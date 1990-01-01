c4ce9f389e7989e2ee77d3314c3d58b3f8cc9378
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1989-12-31 23:30:47 -0800

    use vfinddev to find vnode as b_vp may represent a file instead of the block device supporting the memory file system
    SCCS-vsn: sys/ufs/mfs/mfs_vnops.c 7.12
    
    Former-commit-id: 09cd801192177f28d2bc6d7bdd4ac40b404a0206

--- usr/src/sys/ufs/mfs/mfs_vnops.c
+++ usr/src/sys/ufs/mfs/mfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)mfs_vnops.c	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)mfs_vnops.c	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -417,14 +417,44 @@ name|struct
 name|mfsnode
 modifier|*
 name|mfsp
-init|=
-name|VTOMFS
+decl_stmt|;
+name|struct
+name|vnode
+modifier|*
+name|vp
+decl_stmt|;
+if|if
+condition|(
+name|vfinddev
 argument_list|(
 name|bp
 operator|->
-name|b_vp
+name|b_dev
+argument_list|,
+name|VBLK
+argument_list|,
+operator|&
+name|vp
 argument_list|)
-decl_stmt|;
+operator|||
+name|vp
+operator|->
+name|v_usecount
+operator|==
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"mfs_strategy: bad dev"
+argument_list|)
+expr_stmt|;
+name|mfsp
+operator|=
+name|VTOMFS
+argument_list|(
+name|vp
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|mfsp
@@ -469,9 +499,7 @@ argument_list|(
 operator|(
 name|caddr_t
 operator|)
-name|bp
-operator|->
-name|b_vp
+name|vp
 argument_list|)
 expr_stmt|;
 block|}
