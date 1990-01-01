8c8a7e51073f94dc5ee7c6d3b38e5e74e8f31825
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-01 00:20:32 -0800

    I/O routines should use b_blkno, not b_lblkno
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.21
    
    Former-commit-id: ec1b1c6c23de973ddbabe95330f9f3d32bba38a3

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_vnops.c	7.20 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_vnops.c	7.21 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -7562,7 +7562,7 @@ name|uio_offset
 operator|=
 name|bp
 operator|->
-name|b_lblkno
+name|b_blkno
 operator|*
 name|DEV_BSIZE
 expr_stmt|;
@@ -7723,7 +7723,7 @@ name|uio_offset
 operator|=
 name|bp
 operator|->
-name|b_lblkno
+name|b_blkno
 operator|*
 name|DEV_BSIZE
 expr_stmt|;
@@ -7791,7 +7791,7 @@ operator|=
 operator|(
 name|bp
 operator|->
-name|b_lblkno
+name|b_blkno
 operator|*
 name|DEV_BSIZE
 operator|)
