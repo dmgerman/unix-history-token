9ae9567a649232d1b0fe1f63cc2781d7f3fc914f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-04 04:45:14 -0800

    fix reference bug in fchdir
    SCCS-vsn: sys/kern/vfs_syscalls.c 7.36
    
    Former-commit-id: 2b65bb36fe95f56605e8fd2d31211db60f47ac80

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_syscalls.c	7.35 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_syscalls.c	7.36 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1876,6 +1876,20 @@ argument_list|(
 name|vp
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|error
+condition|)
+name|RETURN
+argument_list|(
+name|error
+argument_list|)
+expr_stmt|;
+name|VREF
+argument_list|(
+name|vp
+argument_list|)
+expr_stmt|;
 name|vrele
 argument_list|(
 name|scp
@@ -1891,7 +1905,7 @@ name|vp
 expr_stmt|;
 name|RETURN
 argument_list|(
-name|error
+literal|0
 argument_list|)
 expr_stmt|;
 block|}
