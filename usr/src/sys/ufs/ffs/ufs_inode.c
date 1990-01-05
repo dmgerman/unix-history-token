93d4dd470f54b7c1bcd584813f5856bda0e12da5
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-05 06:17:31 -0800

    initialize i_diroff; proper association of indirect blocks
    SCCS-vsn: sys/ufs/ffs/ffs_inode.c 7.27
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 7.27
    SCCS-vsn: sys/ufs/lfs/lfs_inode.c 7.27
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 7.27
    
    Former-commit-id: 6f122f36f530a1aa6b84142ff8b36c63a2d803ab

--- usr/src/sys/ufs/ffs/ufs_inode.c
+++ usr/src/sys/ufs/ffs/ufs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ufs_inode.c	7.26 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ufs_inode.c	7.27 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -571,6 +571,12 @@ expr_stmt|;
 name|ip
 operator|->
 name|i_mode
+operator|=
+literal|0
+expr_stmt|;
+name|ip
+operator|->
+name|i_diroff
 operator|=
 literal|0
 expr_stmt|;
@@ -2903,28 +2909,6 @@ name|error
 operator|)
 return|;
 block|}
-if|if
-condition|(
-operator|(
-name|bp
-operator|->
-name|b_flags
-operator|&
-name|B_CACHE
-operator|)
-operator|==
-literal|0
-condition|)
-name|reassignbuf
-argument_list|(
-name|bp
-argument_list|,
-name|ITOV
-argument_list|(
-name|ip
-argument_list|)
-argument_list|)
-expr_stmt|;
 name|bap
 operator|=
 name|bp
