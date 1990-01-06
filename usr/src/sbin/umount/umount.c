a6f8a327b536de8ca9be7933f7bb6b11ac5505e4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-06 02:09:28 -0800

    now AT&T free
    SCCS-vsn: sbin/umount/umount.c 5.11
    
    Former-commit-id: 64e4ce5f2a597029948824004d1e902fe509639b

--- usr/src/sbin/umount/umount.c
+++ usr/src/sbin/umount/umount.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*  * Copyright (c) 1980, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms is permitted  * provided that all copyright information, including this notice,  * is retained in all such forms, and that any documentation,  * advertising or other materials related to such distribution and  * use acknowledge that the software was  * developed by the University of California, Berkeley.  The name  * of the University may not be used to endorse or promote products  * derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
 end_comment
 
 begin_ifndef
@@ -14,15 +14,18 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1980 Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1980, 1989 The Regents of the University of California.\n\  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
-endif|not lint
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_ifndef
 ifndef|#
@@ -36,15 +39,18 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)umount.c	5.10 (Berkeley) %G%"
+literal|"@(#)umount.c	5.11 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
-endif|not lint
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_comment
 comment|/*  * umount  */
