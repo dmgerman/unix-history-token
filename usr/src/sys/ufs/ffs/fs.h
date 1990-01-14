1a93531a848b898e94abc2eb370949fe69286f11
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-14 05:05:24 -0800

    new macro to calculate multiplication by fs_bsize
    SCCS-vsn: sys/ufs/ffs/fs.h 7.8
    
    Former-commit-id: 98d048ca28e67d5322c1a53d5c7a69192e077ad3

--- usr/src/sys/ufs/ffs/fs.h
+++ usr/src/sys/ufs/ffs/fs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)fs.h	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)fs.h	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1121,6 +1121,20 @@ parameter_list|)
 comment|/* calculates (loc % fs->fs_fsize) */
 define|\
 value|((loc)& ~(fs)->fs_fmask)
+end_define
+
+begin_define
+define|#
+directive|define
+name|lblktosize
+parameter_list|(
+name|fs
+parameter_list|,
+name|blk
+parameter_list|)
+comment|/* calculates (blk * fs->fs_bsize) */
+define|\
+value|((blk)<< (fs)->fs_bshift)
 end_define
 
 begin_define
