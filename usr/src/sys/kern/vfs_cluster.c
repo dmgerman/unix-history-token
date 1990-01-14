00fe69e4b00e1a95d73d3ea7549789b76831e849
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-14 02:37:20 -0800

    minor optimization
    SCCS-vsn: sys/kern/vfs_bio.c 7.20
    SCCS-vsn: sys/kern/vfs_cluster.c 7.20
    
    Former-commit-id: 7af6e4ac6dc59af3362bf2ceeebce3e2627ba681

--- usr/src/sys/kern/vfs_cluster.c
+++ usr/src/sys/kern/vfs_cluster.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_cluster.c	7.19 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_cluster.c	7.20 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -563,8 +563,6 @@ block|}
 comment|/* 	 * If there's a read-ahead block, start i/o 	 * on it also (as above). 	 */
 if|if
 condition|(
-name|rablkno
-operator|&&
 operator|!
 name|incore
 argument_list|(
