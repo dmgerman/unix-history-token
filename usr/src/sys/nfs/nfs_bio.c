7e81285b4c6450b675ea2a48382cb3fe99e3d627
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-14 18:51:48 -0800

    n_lastr is replaced by v_lastr
    SCCS-vsn: sys/nfs/nfs_bio.c 7.10
    
    Former-commit-id: 2bd8b563fc69abf758514de8476261cd4f78c09e

--- usr/src/sys/nfs/nfs_bio.c
+++ usr/src/sys/nfs/nfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_bio.c	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_bio.c	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -469,9 +469,9 @@ operator|)
 expr_stmt|;
 if|if
 condition|(
-name|np
+name|vp
 operator|->
-name|n_lastr
+name|v_lastr
 operator|+
 literal|1
 operator|==
@@ -524,9 +524,9 @@ operator|&
 name|bp
 argument_list|)
 expr_stmt|;
-name|np
+name|vp
 operator|->
-name|n_lastr
+name|v_lastr
 operator|=
 name|lbn
 expr_stmt|;
