8359b5538e73967281fc31653b239bf97a3f2a73
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-14 18:56:24 -0800

    debugging, add n_lockholder and n_lockwaiter
    SCCS-vsn: sys/nfs/nfsnode.h 7.7
    
    Former-commit-id: c461a7b0297401846c366f5be85a63750905ba1a

--- usr/src/sys/nfs/nfsnode.h
+++ usr/src/sys/nfs/nfsnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfsnode.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfsnode.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -61,6 +61,14 @@ name|int
 name|n_error
 decl_stmt|;
 comment|/* Save write error value */
+name|pid_t
+name|n_lockholder
+decl_stmt|;
+comment|/* holder of nfsnode lock */
+name|pid_t
+name|n_lockwaiter
+decl_stmt|;
+comment|/* most recent waiter for nfsnode lock */
 block|}
 struct|;
 end_struct
