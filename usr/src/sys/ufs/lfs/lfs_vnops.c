c6af43c7bc93c228907a6dff26c68e01f4a20060
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-14 18:46:19 -0800

    debugging, print out information on processes holding and waiting for lock
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.28
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 7.28
    SCCS-vsn: sys/ufs/lfs/lfs_vnops.c 7.28
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 7.28
    
    Former-commit-id: 3de2ff3446ded68170d3de63c24e84d6d0f26f3c

--- usr/src/sys/ufs/lfs/lfs_vnops.c
+++ usr/src/sys/ufs/lfs/lfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vnops.c	7.27 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vnops.c	7.28 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -6698,6 +6698,44 @@ condition|?
 literal|" (LOCKED)"
 else|:
 literal|""
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|ip
+operator|->
+name|i_spare0
+operator|==
+literal|0
+condition|)
+return|return;
+name|printf
+argument_list|(
+literal|"\towner pid %d"
+argument_list|,
+name|ip
+operator|->
+name|i_spare0
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|ip
+operator|->
+name|i_spare1
+condition|)
+name|printf
+argument_list|(
+literal|" waiting pid %d"
+argument_list|,
+name|ip
+operator|->
+name|i_spare1
+argument_list|)
+expr_stmt|;
+name|printf
+argument_list|(
+literal|"\n"
 argument_list|)
 expr_stmt|;
 block|}
