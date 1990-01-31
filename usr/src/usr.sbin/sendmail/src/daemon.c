67b38fd9013087ce18b141ab9d8e4128e3761282
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-30 21:09:04 -0800

    proper cast in printf (4.3BSD-tahoe/usr.lib/10)
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 5.31
    
    Former-commit-id: a8fbb6bd02921600ff31c9a46b52f15a24aef264

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	5.30 (Berkeley) %G% (with daemon mode)"
+literal|"@(#)daemon.c	5.31 (Berkeley) %G% (with daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -54,7 +54,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	5.30 (Berkeley) %G% (without daemon mode)"
+literal|"@(#)daemon.c	5.31 (Berkeley) %G% (without daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
