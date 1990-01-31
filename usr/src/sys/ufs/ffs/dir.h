6451c45892819965c31f67b3e9957a0ddcb0ee66
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-31 00:56:01 -0800

    update for rewinddir
    SCCS-vsn: sys/ufs/ffs/dir.h 7.7
    SCCS-vsn: sys/ufs/ufs/dir.h 7.7
    
    Former-commit-id: 7c3e228b9acd8c1bb992638b0ae595bdfb5b51d7

--- usr/src/sys/ufs/ffs/dir.h
+++ usr/src/sys/ufs/ffs/dir.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)dir.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)dir.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -169,7 +169,7 @@ begin_define
 define|#
 directive|define
 name|DEV_BSIZE
-value|512
+value|1024
 end_define
 
 begin_endif
@@ -269,16 +269,6 @@ parameter_list|()
 function_decl|;
 end_function_decl
 
-begin_define
-define|#
-directive|define
-name|rewinddir
-parameter_list|(
-name|dirp
-parameter_list|)
-value|seekdir((dirp), (long)0)
-end_define
-
 begin_function_decl
 specifier|extern
 name|void
@@ -286,6 +276,24 @@ name|closedir
 parameter_list|()
 function_decl|;
 end_function_decl
+
+begin_decl_stmt
+specifier|extern
+name|long
+name|_rewinddir
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|rewinddir
+parameter_list|(
+name|dirp
+parameter_list|)
+define|\
+value|_seekdir((dirp), _rewinddir); \ 	_rewinddir = telldir(dirp)
+end_define
 
 begin_endif
 endif|#
