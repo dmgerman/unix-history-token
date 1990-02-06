636f3ac15ce327af0374875a69652cd423fe4072
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-06 01:34:04 -0800

    allow rewinddir to work in all contexts
    SCCS-vsn: include/dirent.h 5.7
    
    Former-commit-id: 6567198cb1d8b58a8f8f051c7bdca10c6a606c25

--- usr/src/include/dirent.h
+++ usr/src/include/dirent.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)dirent.h	5.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)dirent.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -202,7 +202,7 @@ parameter_list|(
 name|dirp
 parameter_list|)
 define|\
-value|_seekdir((dirp), _rewinddir); \ 	_rewinddir = telldir(dirp)
+value|_seekdir((dirp), _rewinddir), \ 	_rewinddir = telldir(dirp)
 end_define
 
 begin_endif
