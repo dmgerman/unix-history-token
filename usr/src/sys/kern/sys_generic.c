f34dec42cedb1f07bdf22073a693ff16805fc07e
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-02-07 23:39:53 -0800

    tsleep
    SCCS-vsn: sys/kern/kern_exit.c 7.14
    SCCS-vsn: sys/kern/sys_generic.c 7.13
    
    Former-commit-id: cd4c21433573c600e1601234df08d1c3358d31fa

--- usr/src/sys/kern/sys_generic.c
+++ usr/src/sys/kern/sys_generic.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_generic.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_generic.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2752,7 +2752,7 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
-name|sleep
+name|tsleep
 argument_list|(
 operator|(
 name|caddr_t
@@ -2763,6 +2763,10 @@ argument_list|,
 name|PZERO
 operator|+
 literal|1
+argument_list|,
+name|SLP_SELECT
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 if|if
