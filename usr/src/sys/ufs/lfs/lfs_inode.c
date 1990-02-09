c799656b213d211937493fe2e481bb5700f19994
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-08 21:23:05 -0800

    optimization from Noemi Paciorek <noemi@osf.org>
    SCCS-vsn: sys/ufs/ffs/ffs_inode.c 7.28
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 7.28
    SCCS-vsn: sys/ufs/lfs/lfs_inode.c 7.28
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 7.28
    
    Former-commit-id: f5c4e35395d3bba66fa461a6de39ee631f87cbd0

--- usr/src/sys/ufs/lfs/lfs_inode.c
+++ usr/src/sys/ufs/lfs/lfs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_inode.c	7.27 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_inode.c	7.28 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1233,6 +1233,17 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
+name|IUNLOCK
+argument_list|(
+name|ip
+argument_list|)
+expr_stmt|;
+name|ip
+operator|->
+name|i_flag
+operator|=
+literal|0
+expr_stmt|;
 comment|/* 	 * If we are done with the inode, reclaim it 	 * so that it can be reused immediately. 	 */
 if|if
 condition|(
@@ -1247,28 +1258,7 @@ operator|->
 name|i_mode
 operator|==
 literal|0
-condition|)
-block|{
-name|vinvalbuf
-argument_list|(
-name|vp
-argument_list|,
-literal|0
-argument_list|)
-expr_stmt|;
-name|IUNLOCK
-argument_list|(
-name|ip
-argument_list|)
-expr_stmt|;
-name|ip
-operator|->
-name|i_flag
-operator|=
-literal|0
-expr_stmt|;
-if|if
-condition|(
+operator|&&
 operator|(
 name|vp
 operator|->
@@ -1283,23 +1273,6 @@ name|vgone
 argument_list|(
 name|vp
 argument_list|)
-expr_stmt|;
-return|return
-operator|(
-name|error
-operator|)
-return|;
-block|}
-name|IUNLOCK
-argument_list|(
-name|ip
-argument_list|)
-expr_stmt|;
-name|ip
-operator|->
-name|i_flag
-operator|=
-literal|0
 expr_stmt|;
 return|return
 operator|(
