72912019228cd2b7f65f52c351f02f6535c7d370
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-02-12 20:19:18 -0800

    make time values u_long's -- should eventually be time_t's. yank to 5.0
    SCCS-vsn: include/pwd.h 5.0
    
    Former-commit-id: 800da3a9d1892e0dbb0e8beb0ded891a970f4697

--- usr/src/include/pwd.h
+++ usr/src/include/pwd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)pwd.h	4.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)pwd.h	5.0 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -74,7 +74,7 @@ name|int
 name|pw_gid
 decl_stmt|;
 comment|/* user gid */
-name|long
+name|u_long
 name|pw_change
 decl_stmt|;
 comment|/* password change time */
@@ -98,7 +98,7 @@ modifier|*
 name|pw_shell
 decl_stmt|;
 comment|/* default shell */
-name|long
+name|u_long
 name|pw_expire
 decl_stmt|;
 comment|/* account expiration */
