54a4b0f1a2a0a8fb31e693251c6412870b7bfaa5
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-15 21:27:50 -0800

    just need dinode.h, not vnode, inode, etc; want ufs/dir.h not sys/dir.h
    SCCS-vsn: sbin/dump/dump.h 5.8
    
    Former-commit-id: dc6b24141c2b4776e07fabc1ec0fd0548453a24d

--- usr/src/sbin/dump/dump.h
+++ usr/src/sbin/dump/dump.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dump.h	5.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dump.h	5.8 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -39,25 +39,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/time.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<ufs/fs.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<sys/vnode.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<ufs/inode.h>
+file|<ufs/dinode.h>
 end_include
 
 begin_include
@@ -69,7 +57,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/dir.h>
+file|<ufs/dir.h>
 end_include
 
 begin_include
