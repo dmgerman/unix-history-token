fd17ac2a9dc705f93182be0c907fd1c8ed9677f3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-15 21:28:18 -0800

    need sys/time.h since dump.h no longer includes it
    SCCS-vsn: sbin/dump/optr.c 5.3
    
    Former-commit-id: c52fd33b433c2273149e9f6e7f368dd181b782c7

--- usr/src/sbin/dump/optr.c
+++ usr/src/sbin/dump/optr.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)optr.c	5.2 (Berkeley) %G%"
+literal|"@(#)optr.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -29,6 +29,12 @@ begin_include
 include|#
 directive|include
 file|"dump.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/time.h>
 end_include
 
 begin_include
