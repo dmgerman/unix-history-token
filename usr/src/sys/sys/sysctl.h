7577d3db88bb0be509cb9347692d00e8deb443bf
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-02-17 01:06:43 -0800

    checkpoint state of eproc as of fri morning
    SCCS-vsn: sys/sys/sysctl.h 7.2
    
    Former-commit-id: 3764c0fcc5eedd39fda9e3841ed88c0fed2388b6

--- usr/src/sys/sys/sysctl.h
+++ usr/src/sys/sys/sysctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sysctl.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sysctl.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -116,7 +116,7 @@ comment|/* by real uid */
 end_comment
 
 begin_comment
-comment|/* proc ops return arrays of these */
+comment|/*   * proc ops return arrays of augmented proc structures  */
 end_comment
 
 begin_struct
@@ -128,43 +128,166 @@ name|proc
 name|kp_proc
 decl_stmt|;
 comment|/* proc structure */
+struct|struct
+name|eproc
+block|{
 name|struct
 name|proc
 modifier|*
-name|kp_paddr
+name|e_paddr
 decl_stmt|;
 comment|/* address of proc */
 name|struct
 name|session
 modifier|*
-name|kp_sess
+name|e_sess
 decl_stmt|;
 comment|/* session pointer */
 name|pid_t
-name|kp_pgid
+name|e_pgid
 decl_stmt|;
 comment|/* process group id */
 name|short
-name|kp_jobc
+name|e_jobc
 decl_stmt|;
 comment|/* job control counter */
 name|dev_t
-name|kp_tdev
+name|e_tdev
 decl_stmt|;
 comment|/* controlling tty */
 name|pid_t
-name|kp_tpgid
+name|e_tpgid
 decl_stmt|;
-comment|/* foreground process group id */
+comment|/* tty process group id */
 name|struct
 name|session
 modifier|*
-name|kp_tsess
+name|e_tsess
 decl_stmt|;
-comment|/* terminal session pointer */
+comment|/* tty session pointer */
+define|#
+directive|define
+name|WMESGLEN
+value|7
+name|char
+name|e_wmesg
+index|[
+name|WMESGLEN
+operator|+
+literal|1
+index|]
+decl_stmt|;
+comment|/* wchan message */
+name|size_t
+name|e_xsize
+decl_stmt|;
+comment|/* text size */
+name|short
+name|e_xrssize
+decl_stmt|;
+comment|/* text rss */
+name|short
+name|e_xccount
+decl_stmt|;
+comment|/* text references */
+name|short
+name|e_xswrss
+decl_stmt|;
+name|long
+name|e_spare
+index|[
+literal|8
+index|]
+decl_stmt|;
+block|}
+name|kp_eproc
+struct|;
 block|}
 struct|;
 end_struct
+
+begin_comment
+comment|/*  * Routing table  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ki_af
+parameter_list|(
+name|x
+parameter_list|)
+value|(((x)&0x00ff0000)>> 16)
+end_define
+
+begin_define
+define|#
+directive|define
+name|KINFO_RT
+value|(1<<8)
+end_define
+
+begin_define
+define|#
+directive|define
+name|KINFO_RT_DUMP
+value|(KINFO_RT|1)
+end_define
+
+begin_comment
+comment|/* dump; may limit to a.f. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|KINFO_RT_FLAGS
+value|(KINFO_RT|2)
+end_define
+
+begin_comment
+comment|/* by flags, e.g. RESOLVING */
+end_comment
+
+begin_comment
+comment|/*  * Locking and stats  */
+end_comment
+
+begin_struct
+struct|struct
+name|kinfo_lock
+block|{
+name|int
+name|kl_lock
+decl_stmt|;
+name|int
+name|kl_want
+decl_stmt|;
+name|int
+name|kl_locked
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KERNEL
+end_ifdef
+
+begin_decl_stmt
+specifier|extern
+name|struct
+name|kinfo_lock
+name|kinfo_lock
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
