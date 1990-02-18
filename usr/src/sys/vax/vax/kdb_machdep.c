3a322b54cb86ccfbfac518ce008d3870d9632d6a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-17 18:46:23 -0800

    get rid of dir.h
    SCCS-vsn: sys/tahoe/vba/hd.c 7.9
    SCCS-vsn: sys/vax/vax/kdb_machdep.c 7.4
    
    Former-commit-id: b23854086d8f223e68ad1f4dcbade55bca8a7b3b

--- usr/src/sys/vax/vax/kdb_machdep.c
+++ usr/src/sys/vax/vax/kdb_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	@(#)kdb_machdep.c	7.3 (Berkeley) %G%	*/
+comment|/*	@(#)kdb_machdep.c	7.4 (Berkeley) %G%	*/
 end_comment
 
 begin_include
@@ -13,12 +13,6 @@ begin_include
 include|#
 directive|include
 file|"conf.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"dir.h"
 end_include
 
 begin_include
