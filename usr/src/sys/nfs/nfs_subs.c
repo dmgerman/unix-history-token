4f85228c777861488a27ceec976c20340923eeb5
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-17 18:39:51 -0800

    get rid of unneeded header files
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.28
    SCCS-vsn: sys/nfs/nfs_node.c 7.22
    SCCS-vsn: sys/nfs/nfs_serv.c 7.13
    SCCS-vsn: sys/nfs/nfs_subs.c 7.17
    SCCS-vsn: sys/nfs/nfs_vfsops.c 7.16
    
    Former-commit-id: fc9f6f125e1e3ef45d294b350e51f4d76980de0e

--- usr/src/sys/nfs/nfs_subs.c
+++ usr/src/sys/nfs/nfs_subs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_subs.c	7.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_subs.c	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -10,19 +10,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"strings.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"param.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"systm.h"
 end_include
 
 begin_include
@@ -40,25 +28,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"mount.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"../ufs/dir.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"time.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"errno.h"
+file|"systm.h"
 end_include
 
 begin_include
@@ -70,13 +40,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"malloc.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"mbuf.h"
+file|"mount.h"
 end_include
 
 begin_include
@@ -94,19 +58,19 @@ end_include
 begin_include
 include|#
 directive|include
-file|"uio.h"
+file|"mbuf.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"namei.h"
+file|"errno.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"ucred.h"
+file|"strings.h"
 end_include
 
 begin_include
