087ae7c7dd4ea8c49ba1635f66d08c21c7d968be
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-17 22:46:54 -0800

    get rid of dependencies on ../ufs/inode.h
    SCCS-vsn: sys/nfs/nfsproto.h 7.4
    
    Former-commit-id: 7fdaa5fa2fdad7836e41188abfd4748a3d4fcb8c

--- usr/src/sys/nfs/nfsproto.h
+++ usr/src/sys/nfs/nfsproto.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfsproto.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfsproto.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -385,6 +385,14 @@ begin_comment
 comment|/* Conversion macros */
 end_comment
 
+begin_decl_stmt
+specifier|extern
+name|int
+name|vttoif_tab
+index|[]
+decl_stmt|;
+end_decl_stmt
+
 begin_define
 define|#
 directive|define
@@ -394,7 +402,7 @@ name|t
 parameter_list|,
 name|m
 parameter_list|)
-value|txdr_unsigned(MAKEIMODE(t,m))
+value|txdr_unsigned((int)(vttoif_tab[(int)(t)] | (m)))
 end_define
 
 begin_define
