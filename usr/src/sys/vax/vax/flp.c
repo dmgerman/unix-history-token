93baeba8eed557f699919d9793c19761db5f05fd
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-17 18:48:33 -0800

    get rid of unneeded header files
    SCCS-vsn: sys/vax/vax/cons.c 7.9
    SCCS-vsn: sys/vax/vax/flp.c 7.3
    SCCS-vsn: sys/vax/vax/sys_machdep.c 7.3
    SCCS-vsn: sys/vax/vax/tu.c 7.3
    SCCS-vsn: sys/vax/vax/mem.c 7.3
    SCCS-vsn: sys/vax/vax/crl.c 7.3
    
    Former-commit-id: 7c2156cc917288ed8e67a92c702f0ca0977e22f7

--- usr/src/sys/vax/vax/flp.c
+++ usr/src/sys/vax/vax/flp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)flp.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)flp.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_if
@@ -30,12 +30,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"dir.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"user.h"
 end_include
 
@@ -43,12 +37,6 @@ begin_include
 include|#
 directive|include
 file|"buf.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"uio.h"
 end_include
 
 begin_include
