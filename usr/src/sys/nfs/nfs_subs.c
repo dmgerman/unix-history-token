cfc1cd0813d871af0f3e6fca2bb27ed4c9212707
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-17 23:23:59 -0800

    get rid of bogus `strings.h' include file
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.29
    SCCS-vsn: sys/nfs/nfs_serv.c 7.14
    SCCS-vsn: sys/nfs/nfs_subs.c 7.18
    
    Former-commit-id: 2b4976d3cb7d998bfb4ecbf6fe781086e52666d1

--- usr/src/sys/nfs/nfs_subs.c
+++ usr/src/sys/nfs/nfs_subs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_subs.c	7.17 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_subs.c	7.18 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -65,12 +65,6 @@ begin_include
 include|#
 directive|include
 file|"errno.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"strings.h"
 end_include
 
 begin_include
