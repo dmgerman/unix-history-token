2b10ff51d5024f8de09f3af76b53d7211b4eca64
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-20 00:47:26 -0800

    move definitions out of vnode.h
    SCCS-vsn: sys/kern/vfs_subr.c 7.29
    
    Former-commit-id: 4f51d9e545eb7eb37014ee20f9deab2dfb53df63

--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.28 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.29 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -54,6 +54,24 @@ include|#
 directive|include
 file|"malloc.h"
 end_include
+
+begin_comment
+comment|/*  * Shorthand notation.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|v_hashchain
+value|v_specinfo->si_hashchain
+end_define
+
+begin_define
+define|#
+directive|define
+name|v_specnext
+value|v_specinfo->si_specnext
+end_define
 
 begin_comment
 comment|/*  * Remove a mount point from the list of mounted filesystems.  * Unmount of the root is illegal.  */
