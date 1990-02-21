4048d5b2f19e006cba5a29fe1500ee41c4f0ce11
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-21 06:18:41 -0800

    raise limit on maxoff
    SCCS-vsn: sys/deprecated/kdb/defs.h 7.9
    
    Former-commit-id: d73af4efd27994ddf689cec66abd01b70ca017f0

--- usr/src/sys/deprecated/kdb/defs.h
+++ usr/src/sys/deprecated/kdb/defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)defs.h	7.8 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)defs.h	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -215,7 +215,7 @@ begin_define
 define|#
 directive|define
 name|MAXOFF
-value|1024
+value|65535
 end_define
 
 begin_define
