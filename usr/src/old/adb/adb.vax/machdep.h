d637259ed3baef920d31a2ffbf7b5ceb5821a688
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-02-23 19:18:10 -0800

    remove KERNTEXTOFF (comes from machparam.h)
    SCCS-vsn: old/adb/adb.vax/machdep.h 5.2
    SCCS-vsn: old/adb/adb.tahoe/machdep.h 5.2
    
    Former-commit-id: 6167881b00dac0471f48c9593f21f5558a4282ef

--- usr/src/old/adb/adb.vax/machdep.h
+++ usr/src/old/adb/adb.vax/machdep.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  *	@(#)machdep.h	5.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  *	@(#)machdep.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -241,17 +241,6 @@ end_define
 
 begin_comment
 comment|/* sys space */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|KERNTEXTOFF
-value|KERNBASE
-end_define
-
-begin_comment
-comment|/* start of kernel text */
 end_comment
 
 end_unit
