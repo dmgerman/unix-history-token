d638a9ddcaf9ce557ec475577a3f5109b44ded1f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-24 01:03:47 -0800

    no longer need to munhash, handled by xuntext instead
    SCCS-vsn: sys/nfs/nfs_bio.c 7.12
    
    Former-commit-id: 42ab63b0013f4477927d7aac37060dd4db3457da

--- usr/src/sys/nfs/nfs_bio.c
+++ usr/src/sys/nfs/nfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_bio.c	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_bio.c	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -737,13 +737,9 @@ decl_stmt|,
 name|bn
 decl_stmt|;
 name|int
-name|i
-decl_stmt|,
 name|n
 decl_stmt|,
 name|on
-decl_stmt|,
-name|count
 decl_stmt|,
 name|error
 init|=
@@ -969,41 +965,6 @@ name|NFS_BIOSIZE
 operator|/
 name|DEV_BSIZE
 operator|)
-expr_stmt|;
-name|count
-operator|=
-name|howmany
-argument_list|(
-name|NFS_BIOSIZE
-argument_list|,
-name|CLBYTES
-argument_list|)
-expr_stmt|;
-for|for
-control|(
-name|i
-operator|=
-literal|0
-init|;
-name|i
-operator|<
-name|count
-condition|;
-name|i
-operator|++
-control|)
-name|munhash
-argument_list|(
-name|vp
-argument_list|,
-name|bn
-operator|+
-name|i
-operator|*
-name|CLBYTES
-operator|/
-name|DEV_BSIZE
-argument_list|)
 expr_stmt|;
 name|again
 label|:
