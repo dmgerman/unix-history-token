fcd2f9311af5715c6619f40af5647192ac0e50f6
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-02-23 21:50:06 -0800

    I see no pointer here
    SCCS-vsn: sys/tahoe/vba/vbavar.h 7.4
    
    Former-commit-id: 3e8647898403378d19cfe7a805ba228c5859d8e8

--- usr/src/sys/tahoe/vba/vbavar.h
+++ usr/src/sys/tahoe/vba/vbavar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vbavar.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vbavar.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -488,7 +488,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*  * VERSAbus device address space is mapped by VMEMmap  * into virtual address vmem[][].  */
+comment|/*  * VERSAbus device address space is mapped by VMEMmap  * into virtual address vmem[].  */
 end_comment
 
 begin_decl_stmt
@@ -506,8 +506,9 @@ end_comment
 
 begin_decl_stmt
 specifier|extern
-name|caddr_t
+name|char
 name|vmem
+index|[]
 decl_stmt|;
 end_decl_stmt
 
