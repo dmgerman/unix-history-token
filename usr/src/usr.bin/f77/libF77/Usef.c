f6830692f659c9672c62e5f95721d3d6e00e4fd5
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1990-02-26 02:55:59 -0800

    don't use 'cc -f' with compilers that don't do automatic double to float promotion.
    SCCS-vsn: usr.bin/f77/libF77/Usef.c 1.4
    
    Former-commit-id: b2fc72a5e1da4ddd9ef195d7c583fb7586fc3b46

--- usr/src/usr.bin/f77/libF77/Usef.c
+++ usr/src/usr.bin/f77/libF77/Usef.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)Usef.c	1.3	%G%  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)Usef.c	1.4	%G%  */
 end_comment
 
 begin_comment
