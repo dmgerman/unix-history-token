823b9ceb321340e86c2bcf0bad237b702f538671
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-06 02:10:31 -0800

    reassign vnode ops for FIFO's
    SCCS-vsn: sys/ufs/ffs/ffs_inode.c 7.29
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 7.29
    SCCS-vsn: sys/ufs/lfs/lfs_inode.c 7.29
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 7.29
    
    Former-commit-id: dfb5a22cd38693ca8b8d907f1e8bb64316eea948

--- usr/src/sys/ufs/ffs/ffs_inode.c
+++ usr/src/sys/ufs/ffs/ffs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ffs_inode.c	7.28 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ffs_inode.c	7.29 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -726,6 +726,51 @@ operator|->
 name|i_mode
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|vp
+operator|->
+name|v_type
+operator|==
+name|VFIFO
+condition|)
+block|{
+ifdef|#
+directive|ifdef
+name|FIFO
+specifier|extern
+name|struct
+name|vnodeops
+name|fifo_inodeops
+decl_stmt|;
+name|vp
+operator|->
+name|v_op
+operator|=
+operator|&
+name|fifo_inodeops
+expr_stmt|;
+else|#
+directive|else
+name|iput
+argument_list|(
+name|ip
+argument_list|)
+expr_stmt|;
+operator|*
+name|ipp
+operator|=
+literal|0
+expr_stmt|;
+return|return
+operator|(
+name|EOPNOTSUPP
+operator|)
+return|;
+endif|#
+directive|endif
+comment|/* FIFO */
+block|}
 if|if
 condition|(
 name|vp
