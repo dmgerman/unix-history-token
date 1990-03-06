5d2ee1ca5413b847839a8f4420e71d702a314300
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-06 03:38:19 -0800

    readdir fixes from Rick Macklem
    SCCS-vsn: sys/nfs/nfs.h 7.6
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.32
    SCCS-vsn: sys/nfs/nfs_node.c 7.23
    SCCS-vsn: sys/nfs/nfs_serv.c 7.15
    SCCS-vsn: sys/nfs/nfsnode.h 7.9
    
    Former-commit-id: 499adb0c53d9ab0cb7b4a1ed4dfda9c0c7998ddc

--- usr/src/sys/nfs/nfs_serv.c
+++ usr/src/sys/nfs/nfs_serv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_serv.c	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_serv.c	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -5651,6 +5651,8 @@ decl_stmt|,
 name|cnt
 decl_stmt|,
 name|fullsiz
+decl_stmt|,
+name|eofflag
 decl_stmt|;
 name|u_long
 name|on
@@ -5890,6 +5892,9 @@ operator|&
 name|io
 argument_list|,
 name|cred
+argument_list|,
+operator|&
+name|eofflag
 argument_list|)
 expr_stmt|;
 name|off
@@ -6368,6 +6373,16 @@ name|NFSX_UNSIGNED
 expr_stmt|;
 name|nfsm_clget
 expr_stmt|;
+if|if
+condition|(
+name|eofflag
+condition|)
+operator|*
+name|p
+operator|=
+name|nfs_true
+expr_stmt|;
+else|else
 operator|*
 name|p
 operator|=
