05cf8da0f6038423696e46b4c45aea3e33310712
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-07 07:32:44 -0800

    m_fsid is now m_stat.f_fsid
    SCCS-vsn: sys/nfs/nfs_serv.c 7.16
    
    Former-commit-id: 92b7e5dd98af4a61329d9084fcdc03423738c111

--- usr/src/sys/nfs/nfs_serv.c
+++ usr/src/sys/nfs/nfs_serv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_serv.c	7.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_serv.c	7.16 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1046,7 +1046,9 @@ name|vp
 operator|->
 name|v_mount
 operator|->
-name|m_fsid
+name|m_stat
+operator|.
+name|f_fsid
 expr_stmt|;
 if|if
 condition|(
@@ -3249,7 +3251,9 @@ name|vp
 operator|->
 name|v_mount
 operator|->
-name|m_fsid
+name|m_stat
+operator|.
+name|f_fsid
 expr_stmt|;
 if|if
 condition|(
@@ -5109,7 +5113,9 @@ name|vp
 operator|->
 name|v_mount
 operator|->
-name|m_fsid
+name|m_stat
+operator|.
+name|f_fsid
 expr_stmt|;
 if|if
 condition|(
