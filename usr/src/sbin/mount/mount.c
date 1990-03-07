2f86242326611f480ba8ea2c7a755558e7cb56e1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-07 05:23:58 -0800

    add MNT_NOWAIT parameter to getmntinfo
    SCCS-vsn: sbin/mount/mount.c 5.26
    
    Former-commit-id: 6e173845c5268bb869070f6cfeb3fde67e71c5e6

--- usr/src/sbin/mount/mount.c
+++ usr/src/sbin/mount/mount.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mount.c	5.25 (Berkeley) %G%"
+literal|"@(#)mount.c	5.26 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -689,6 +689,8 @@ name|getmntinfo
 argument_list|(
 operator|&
 name|mntbuf
+argument_list|,
+name|MNT_NOWAIT
 argument_list|)
 operator|)
 operator|==
@@ -2359,6 +2361,8 @@ name|getmntinfo
 argument_list|(
 operator|&
 name|mntbuf
+argument_list|,
+name|MNT_NOWAIT
 argument_list|)
 expr_stmt|;
 for|for
