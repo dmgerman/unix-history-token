9d580d27eae7a91883d47edfd30fc9aeeffe4df1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-07 07:27:40 -0800

    mntname is now maintained in the mount table
    SCCS-vsn: sys/ufs/ffs/ufsmount.h 7.4
    SCCS-vsn: sys/ufs/ufs/ufsmount.h 7.4
    
    Former-commit-id: ad5106fd136f66a272bb5260cf261c9f08f02a0f

--- usr/src/sys/ufs/ffs/ufsmount.h
+++ usr/src/sys/ufs/ffs/ufsmount.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ufsmount.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ufsmount.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -39,13 +39,6 @@ modifier|*
 name|um_qinod
 decl_stmt|;
 comment|/* QUOTA: pointer to quota file */
-name|char
-name|um_mntname
-index|[
-name|MNAMELEN
-index|]
-decl_stmt|;
-comment|/* mounted filesystem */
 block|}
 struct|;
 end_struct
