e5d5fb9967d6f111bf08af324d4e907673c846d2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-09 01:16:05 -0800

    add flags field to specinfo to mark mounted block devices
    SCCS-vsn: sys/sys/vnode.h 7.23
    
    Former-commit-id: c399a9607ef41a32da2153db322f070fc709598a

--- usr/src/sys/sys/vnode.h
+++ usr/src/sys/sys/vnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vnode.h	7.22 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vnode.h	7.23 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1083,6 +1083,9 @@ name|vnode
 modifier|*
 name|si_specnext
 decl_stmt|;
+name|long
+name|si_flags
+decl_stmt|;
 name|dev_t
 name|si_rdev
 decl_stmt|;
@@ -1090,12 +1093,31 @@ block|}
 struct|;
 end_struct
 
+begin_comment
+comment|/*  * Exported shorthand  */
+end_comment
+
 begin_define
 define|#
 directive|define
 name|v_rdev
 value|v_specinfo->si_rdev
 end_define
+
+begin_comment
+comment|/*  * Flags for specinfo  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SI_MOUNTEDON
+value|0x0001
+end_define
+
+begin_comment
+comment|/* block special device is mounted on */
+end_comment
 
 begin_comment
 comment|/*  * flags for ioflag  */
