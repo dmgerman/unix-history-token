9ee7226da2aa76bff18dfcbff395ae77c8949fc5
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-03-12 17:27:15 -0800

    use MAXHOSTNAMELEN
    SCCS-vsn: usr.bin/systat/systat.h 5.4
    
    Former-commit-id: 4107afdb9e6044cbe670c20dc0f2127c703eaa2d

--- usr/src/usr.bin/systat/systat.h
+++ usr/src/usr.bin/systat/systat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)systat.h	5.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)systat.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -400,7 +400,7 @@ name|namp
 decl_stmt|,
 name|hostname
 index|[
-literal|32
+name|MAXHOSTNAMELEN
 index|]
 decl_stmt|;
 end_decl_stmt
