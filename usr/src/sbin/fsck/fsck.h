ca1c36995f5d7a06ca3641978c83ed8fbabf5971
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-28 00:39:40 -0800

    get rid of unused variables
    SCCS-vsn: sbin/fsck/fsck.h 5.13
    
    Former-commit-id: 1a1776ed06be39e4483c3205dd2f6f359f068317

--- usr/src/sbin/fsck/fsck.h
+++ usr/src/sbin/fsck/fsck.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)fsck.h	5.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)fsck.h	5.13 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -749,41 +749,6 @@ end_decl_stmt
 
 begin_comment
 comment|/* ptr to link count table */
-end_comment
-
-begin_decl_stmt
-name|char
-name|pathname
-index|[
-name|BUFSIZ
-index|]
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* current pathname */
-end_comment
-
-begin_decl_stmt
-name|char
-modifier|*
-name|pathp
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* ptr to current position in pathname */
-end_comment
-
-begin_decl_stmt
-name|char
-modifier|*
-name|endpathname
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* ptr to current end of pathname */
 end_comment
 
 begin_decl_stmt
