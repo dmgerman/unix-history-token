ce2fa534d7780abc93c881e60d45451d23265935
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-28 01:06:07 -0800

    add inode and directory buffer pointers
    SCCS-vsn: sbin/fsck/fsck.h 5.14
    
    Former-commit-id: e0f31633266dffa0bfe7cd3b44a12e0f2892b30a

--- usr/src/sbin/fsck/fsck.h
+++ usr/src/sbin/fsck/fsck.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)fsck.h	5.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)fsck.h	5.14 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -252,6 +252,30 @@ end_decl_stmt
 
 begin_comment
 comment|/* cylinder group blocks */
+end_comment
+
+begin_decl_stmt
+name|struct
+name|bufarea
+modifier|*
+name|pdirbp
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* current directory contents */
+end_comment
+
+begin_decl_stmt
+name|struct
+name|bufarea
+modifier|*
+name|pbp
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* current inode block */
 end_comment
 
 begin_function_decl
