31147b209655b930505aa7a83bfbc9d146497c3d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-03-28 02:13:06 -0800

    break specinfo structure out of vnode.h into specdev.h
    SCCS-vsn: sys/miscfs/specfs/spec_vnops.c 7.24
    SCCS-vsn: sys/kern/vfs_bio.c 7.25
    SCCS-vsn: sys/kern/vfs_cluster.c 7.25
    SCCS-vsn: sys/kern/vfs_subr.c 7.35
    SCCS-vsn: sys/vm/vm_swap.c 7.11
    SCCS-vsn: sys/kern/kern_physio.c 7.13
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.38
    SCCS-vsn: sys/ufs/ffs/ufs_vfsops.c 7.38
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.38
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 7.38
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.35
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 7.35
    SCCS-vsn: sys/ufs/lfs/lfs_vnops.c 7.35
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 7.35
    
    Former-commit-id: c0e82829df74401edca8ebdecb1e7b5e7a101eae

--- usr/src/sys/kern/kern_physio.c
+++ usr/src/sys/kern/kern_physio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_physio.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_physio.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -67,6 +67,12 @@ begin_include
 include|#
 directive|include
 file|"vnode.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"specdev.h"
 end_include
 
 begin_include
--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.34 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.35 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -34,6 +34,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"specdev.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"namei.h"
 end_include
 
@@ -54,24 +60,6 @@ include|#
 directive|include
 file|"malloc.h"
 end_include
-
-begin_comment
-comment|/*  * Shorthand notation.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|v_hashchain
-value|v_specinfo->si_hashchain
-end_define
-
-begin_define
-define|#
-directive|define
-name|v_specnext
-value|v_specinfo->si_specnext
-end_define
 
 begin_comment
 comment|/*  * Remove a mount point from the list of mounted filesystems.  * Unmount of the root is illegal.  */
@@ -733,72 +721,6 @@ name|vclean
 parameter_list|()
 function_decl|;
 end_function_decl
-
-begin_define
-define|#
-directive|define
-name|SPECHSZ
-value|64
-end_define
-
-begin_if
-if|#
-directive|if
-operator|(
-operator|(
-name|SPECHSZ
-operator|&
-operator|(
-name|SPECHSZ
-operator|-
-literal|1
-operator|)
-operator|)
-operator|==
-literal|0
-operator|)
-end_if
-
-begin_define
-define|#
-directive|define
-name|SPECHASH
-parameter_list|(
-name|rdev
-parameter_list|)
-value|(((rdev>>5)+(rdev))&(SPECHSZ-1))
-end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_define
-define|#
-directive|define
-name|SPECHASH
-parameter_list|(
-name|rdev
-parameter_list|)
-value|(((unsigned)((rdev>>5)+(rdev)))%SPECHSZ)
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_decl_stmt
-name|struct
-name|vnode
-modifier|*
-name|speclisth
-index|[
-name|SPECHSZ
-index|]
-decl_stmt|;
-end_decl_stmt
 
 begin_comment
 comment|/*  * Initialize the vnode structures and initialize each file system type.  */
