b5b9475c79140d67e2217eacf5863b43d2d7ad16
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-04-03 21:21:32 -0800

    sonewconn has second arg; in_pcb.h requires ip.h
    SCCS-vsn: sys/netinet/tcp_input.c 7.22
    
    Former-commit-id: 86f0628539275f68c13da9ca014f89951c7a5272

--- usr/src/sys/netinet/tcp_input.c
+++ usr/src/sys/netinet/tcp_input.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)tcp_input.c	7.21 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)tcp_input.c	7.22 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -72,12 +72,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"in_pcb.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"in_systm.h"
 end_include
 
@@ -85,6 +79,12 @@ begin_include
 include|#
 directive|include
 file|"ip.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"in_pcb.h"
 end_include
 
 begin_include
@@ -1441,6 +1441,8 @@ operator|=
 name|sonewconn
 argument_list|(
 name|so
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 if|if
