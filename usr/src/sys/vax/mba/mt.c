f8bc0b1b4f748341eadfc193ea9dbfd904f20331
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-04-04 05:19:34 -0800

    close routines must return error
    SCCS-vsn: sys/vax/mba/ht.c 7.9
    SCCS-vsn: sys/vax/mba/mt.c 7.6
    
    Former-commit-id: 7c72ebda19275c50b59d1086f0982a0d63701c63

--- usr/src/sys/vax/mba/mt.c
+++ usr/src/sys/vax/mba/mt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)mt.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)mt.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1225,6 +1225,11 @@ name|sc_openf
 operator|=
 literal|0
 expr_stmt|;
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 block|}
 end_block
 
