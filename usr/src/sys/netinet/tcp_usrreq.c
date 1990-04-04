7c2453961c05747b4ce07dca8bab4751d19edf66
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-04-03 21:34:08 -0800

    in_pcb.h requires ip.h
    SCCS-vsn: sys/netinet/tcp_debug.c 7.4
    SCCS-vsn: sys/netinet/tcp_usrreq.c 7.13
    SCCS-vsn: sys/netinet/tcp_timer.c 7.16
    
    Former-commit-id: b5da5a2262bafed11db7efde1d640cac1f841cd9

--- usr/src/sys/netinet/tcp_usrreq.c
+++ usr/src/sys/netinet/tcp_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)tcp_usrreq.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)tcp_usrreq.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -78,12 +78,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"in_pcb.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"in_systm.h"
 end_include
 
@@ -91,6 +85,12 @@ begin_include
 include|#
 directive|include
 file|"ip.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"in_pcb.h"
 end_include
 
 begin_include
