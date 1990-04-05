bc892a6fdf054b5a70ac0af64a5cba4e2fc2e3ac
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-04-04 23:37:42 -0800

    add needed header files (may have been distributed as revision 7.9)
    SCCS-vsn: sys/stand.att/saio.h 7.14
    
    Former-commit-id: c820e2c2dc75536e9df6480e7592ec6a87f272ba

--- usr/src/sys/stand.att/saio.h
+++ usr/src/sys/stand.att/saio.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1988 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)saio.h	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1988 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)saio.h	7.14 (Berkeley) %G%  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|"../ufs/fs.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"../ufs/dinode.h"
+end_include
 
 begin_include
 include|#
