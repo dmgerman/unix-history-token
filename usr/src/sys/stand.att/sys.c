d434c9ce0230c6c3b71c646cea9168ff75ac8b0c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-04-04 23:13:39 -0800

    do not need includes
    SCCS-vsn: sys/stand.att/sys.c 7.10
    SCCS-vsn: sys/stand.att/dev.c 7.7
    
    Former-commit-id: f51afd3165610bf9331ab6107353662207a0fb25

--- usr/src/sys/stand.att/sys.c
+++ usr/src/sys/stand.att/sys.c
@@ -1,24 +1,12 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sys.c	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sys.c	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"sys/param.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"ufs/dinode.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"ufs/fs.h"
 end_include
 
 begin_include
