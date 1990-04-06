a96eea033ba2c997b2932fcc56d8864e6f847892
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-04-05 18:35:44 -0800

    rights now special case of ancillary data
    SCCS-vsn: sys/net/raw_usrreq.c 7.8
    
    Former-commit-id: 3ed2c2503e30ad05785c4eb1faea631ee09ce77b

--- usr/src/sys/net/raw_usrreq.c
+++ usr/src/sys/net/raw_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)raw_usrreq.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)raw_usrreq.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -477,8 +477,6 @@ argument|m
 argument_list|,
 argument|nam
 argument_list|,
-argument|rights
-argument_list|,
 argument|control
 argument_list|)
 end_macro
@@ -505,9 +503,6 @@ name|m
 decl_stmt|,
 modifier|*
 name|nam
-decl_stmt|,
-modifier|*
-name|rights
 decl_stmt|,
 modifier|*
 name|control
@@ -549,9 +544,9 @@ operator|)
 return|;
 if|if
 condition|(
-name|rights
+name|control
 operator|&&
-name|rights
+name|control
 operator|->
 name|m_len
 condition|)
