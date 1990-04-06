c23dd296b602b86c16febc504f842244b3116787
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-04-05 18:50:19 -0800

    IFA_ROUTE now moved to protocol independent part of ifaddr
    SCCS-vsn: sys/netinet/in_var.h 7.5
    
    Former-commit-id: 6e96c53161ac3bf135946d2ca4c88f6c0a7986fc

--- usr/src/sys/netinet/in_var.h
+++ usr/src/sys/netinet/in_var.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)in_var.h	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1985, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)in_var.h	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -20,6 +20,10 @@ define|#
 directive|define
 name|ia_ifp
 value|ia_ifa.ifa_ifp
+define|#
+directive|define
+name|ia_flags
+value|ia_ifa.ifa_flags
 comment|/* ia_{,sub}net{,mask} in host order */
 name|u_long
 name|ia_net
@@ -37,9 +41,6 @@ name|u_long
 name|ia_subnetmask
 decl_stmt|;
 comment|/* mask of subnet part */
-name|int
-name|ia_flags
-decl_stmt|;
 name|struct
 name|in_addr
 name|ia_netbroadcast
@@ -106,7 +107,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  * Given a pointer to an in_ifaddr (ifaddr),  * return a pointer to the addr as a sockadd_in.  */
+comment|/*  * Given a pointer to an in_ifaddr (ifaddr),  * return a pointer to the addr as a sockaddr_in.  */
 end_comment
 
 begin_define
@@ -118,21 +119,6 @@ name|ia
 parameter_list|)
 value|(&(((struct in_ifaddr *)(ia))->ia_addr))
 end_define
-
-begin_comment
-comment|/*  * ia_flags  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IFA_ROUTE
-value|0x01
-end_define
-
-begin_comment
-comment|/* routing entry installed */
-end_comment
 
 begin_ifdef
 ifdef|#
