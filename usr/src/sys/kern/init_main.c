f9fb8c3ac51cdc3260f739d1e20e7d5f377f3609
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-04-05 21:47:23 -0800

    swapper and pagedaemon have names set in p_comm.
    SCCS-vsn: sys/kern/init_main.c 7.22
    
    Former-commit-id: c81e10c32525e23bff262117bb55d4dfb0056677

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)init_main.c	7.21 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)init_main.c	7.22 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -199,6 +199,20 @@ name|proc
 index|[
 literal|0
 index|]
+expr_stmt|;
+name|bcopy
+argument_list|(
+literal|"swapper"
+argument_list|,
+name|p
+operator|->
+name|p_comm
+argument_list|,
+sizeof|sizeof
+argument_list|(
+literal|"swapper"
+argument_list|)
+argument_list|)
 expr_stmt|;
 name|p
 operator|->
@@ -905,6 +919,23 @@ operator|*
 name|CLSIZE
 operator|*
 name|KLMAX
+expr_stmt|;
+name|bcopy
+argument_list|(
+literal|"pagedaemon"
+argument_list|,
+name|proc
+index|[
+literal|2
+index|]
+operator|.
+name|p_comm
+argument_list|,
+sizeof|sizeof
+argument_list|(
+literal|"pagedaemon"
+argument_list|)
+argument_list|)
 expr_stmt|;
 name|pageout
 argument_list|()
