a0673abab7e8abaf41040f28139b109362a24c6e
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-04-05 18:28:58 -0800

    no longer included in route.c; needed "malloc.h" to stand on own.
    SCCS-vsn: sys/net/radix.c 7.5
    
    Former-commit-id: 37c38f3ddd73f64f8c80759c01df9217e41c7a8f

--- usr/src/sys/net/radix.c
+++ usr/src/sys/net/radix.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1989  Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)radix.c	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1989  Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)radix.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -24,6 +24,19 @@ include|#
 directive|include
 file|"radix.h"
 end_include
+
+begin_include
+include|#
+directive|include
+file|"malloc.h"
+end_include
+
+begin_define
+define|#
+directive|define
+name|M_DONTWAIT
+value|M_NOWAIT
+end_define
 
 begin_endif
 endif|#
