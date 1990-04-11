5446906a8d0c3985be0452592914030be902f183
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-04-11 03:11:20 -0800

    clear the buffer list before vrele
    SCCS-vsn: sys/ufs/mfs/mfs_vfsops.c 7.11
    
    Former-commit-id: 82d3e03f34e717910eb5fff485d57a3ace294b80

--- usr/src/sys/ufs/mfs/mfs_vfsops.c
+++ usr/src/sys/ufs/mfs/mfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)mfs_vfsops.c	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)mfs_vfsops.c	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -463,6 +463,18 @@ name|mp
 argument_list|)
 condition|)
 block|{
+name|mfsp
+operator|->
+name|mfs_buflist
+operator|=
+operator|(
+expr|struct
+name|buf
+operator|*
+operator|)
+operator|-
+literal|1
+expr_stmt|;
 name|vrele
 argument_list|(
 name|devvp
