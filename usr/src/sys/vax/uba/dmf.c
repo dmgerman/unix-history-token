dcb82ef2130b8ac1b4d46bd5e9ddf325a8b96c37
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-04-13 00:58:04 -0800

    no more bk.h
    SCCS-vsn: sys/vax/uba/dh.c 7.12
    SCCS-vsn: sys/vax/uba/dmf.c 7.12
    SCCS-vsn: sys/vax/uba/dmx.c 7.2
    SCCS-vsn: sys/vax/uba/dmz.c 7.9
    
    Former-commit-id: f1f85c10353fd82c4dace45e19af03aba898b85e

--- usr/src/sys/vax/uba/dmf.c
+++ usr/src/sys/vax/uba/dmf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dmf.c	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dmf.c	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -44,12 +44,6 @@ begin_include
 include|#
 directive|include
 file|"machine/pte.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"bk.h"
 end_include
 
 begin_include
--- usr/src/sys/vax/uba/dmz.c
+++ usr/src/sys/vax/uba/dmz.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dmz.c	7.8 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1985, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dmz.c	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -25,12 +25,6 @@ begin_include
 include|#
 directive|include
 file|"machine/pte.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"bk.h"
 end_include
 
 begin_include
