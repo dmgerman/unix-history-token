dd41bb006e8861094e69ddc065e51e5e31f40f2b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-18 20:36:15 -0800

    flock instead of locking files, major security fixes, minor other stuff
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.62
    
    Former-commit-id: 88885c85dd35f101beca802c583cb025f3bbf8a6

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	5.61 (Berkeley) %G%"
+literal|"@(#)version.c	5.62 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"5.61"
+literal|"5.62"
 decl_stmt|;
 end_decl_stmt
 
