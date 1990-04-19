e6ce6fd657e49948aa3fcaf8eae93d3ec46ec0c7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-18 19:16:55 -0800

    `LineNumber' not incremented when reading :include: file bug report 4.3BSD/usr.lib/120
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 5.16
    
    Former-commit-id: 4aeea0dc86e76948122026413ea72b555ac94f83

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)recipient.c	5.15 (Berkeley) %G%"
+literal|"@(#)recipient.c	5.16 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2126,6 +2126,9 @@ argument_list|,
 literal|'\n'
 argument_list|)
 decl_stmt|;
+name|LineNumber
+operator|++
+expr_stmt|;
 if|if
 condition|(
 name|p
