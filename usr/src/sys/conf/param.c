e095f4f3c277ce527b86d94b0a5026e2db392f02
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-04-27 17:16:56 -0800

    no more static quota tables
    SCCS-vsn: sys/conf/param.c 7.10
    
    Former-commit-id: 83b7175b8273c70e6c6a80192621186a0c88c573

--- usr/src/sys/conf/param.c
+++ usr/src/sys/conf/param.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)param.c	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)param.c	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -269,49 +269,6 @@ name|NMBCLUSTERS
 decl_stmt|;
 end_decl_stmt
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|QUOTA
-end_ifdef
-
-begin_decl_stmt
-name|int
-name|nquota
-init|=
-operator|(
-name|MAXUSERS
-operator|*
-literal|9
-operator|)
-operator|/
-literal|7
-operator|+
-literal|3
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|ndquot
-init|=
-name|NVNODE
-operator|+
-operator|(
-name|MAXUSERS
-operator|*
-name|NMOUNT
-operator|)
-operator|/
-literal|4
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_decl_stmt
 name|int
 name|fscale
@@ -417,39 +374,6 @@ modifier|*
 name|ecmap
 decl_stmt|;
 end_decl_stmt
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|QUOTA
-end_ifdef
-
-begin_decl_stmt
-name|struct
-name|quota
-modifier|*
-name|quota
-decl_stmt|,
-modifier|*
-name|quotaNQUOTA
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|struct
-name|dquot
-modifier|*
-name|dquot
-decl_stmt|,
-modifier|*
-name|dquotNDQUOT
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 end_unit
 
