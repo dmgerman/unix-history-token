ffb6e32c996e57c2eab5aecb2e174e477526bef1
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-05-01 18:19:50 -0800

    only ktrace i/o if successful
    SCCS-vsn: sys/kern/sys_generic.c 7.15
    
    Former-commit-id: 431f3b8ca5165dfd23b2625e3da04b7e0f87ddf5

--- usr/src/sys/kern/sys_generic.c
+++ usr/src/sys/kern/sys_generic.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_generic.c	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_generic.c	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -351,6 +351,10 @@ name|u_procp
 argument_list|,
 name|KTR_GENIO
 argument_list|)
+operator|&&
+name|error
+operator|==
+literal|0
 condition|)
 name|ktrgenio
 argument_list|(
@@ -824,6 +828,12 @@ operator|!=
 name|NULL
 condition|)
 block|{
+if|if
+condition|(
+name|error
+operator|==
+literal|0
+condition|)
 name|ktrgenio
 argument_list|(
 name|u
@@ -1174,6 +1184,10 @@ name|u_procp
 argument_list|,
 name|KTR_GENIO
 argument_list|)
+operator|&&
+name|error
+operator|==
+literal|0
 condition|)
 name|ktrgenio
 argument_list|(
@@ -1664,6 +1678,12 @@ operator|!=
 name|NULL
 condition|)
 block|{
+if|if
+condition|(
+name|error
+operator|==
+literal|0
+condition|)
 name|ktrgenio
 argument_list|(
 name|u
