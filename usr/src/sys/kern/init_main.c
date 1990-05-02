70772789977f88f02d084d2eda206e644c352d8c
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-05-01 18:20:05 -0800

    initialize u_start
    SCCS-vsn: sys/kern/init_main.c 7.24
    
    Former-commit-id: c065496f2369eb108e6eb0a1a8eabf748f32f2b8

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)init_main.c	7.23 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)init_main.c	7.24 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -453,6 +453,12 @@ operator|=
 name|u
 operator|.
 name|u_arg
+expr_stmt|;
+name|u
+operator|.
+name|u_start
+operator|=
+name|time
 expr_stmt|;
 name|u
 operator|.
