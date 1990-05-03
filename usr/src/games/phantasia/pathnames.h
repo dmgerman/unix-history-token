d4f6dee7cb27fe2c7f0e93c32f2cdf43a4d91c77
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-03 04:50:59 -0800

    move to /var/games, delete unnecessary items
    SCCS-vsn: games/phantasia/pathnames.h 5.2
    
    Former-commit-id: 489ac3589be01b915ca3981baa212e6a85b6c61c

--- usr/src/games/phantasia/pathnames.h
+++ usr/src/games/phantasia/pathnames.h
@@ -1,90 +1,69 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)pathnames.h	5.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)pathnames.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|_PATH_ENEMY
-value|"/usr/games/lib/phantasia/enemy"
-end_define
-
-begin_define
-define|#
-directive|define
 name|_PATH_GAMEPROG
-value|"/usr/games/phantasia/phantasia"
+value|"/usr/games/phantasia"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_GOLD
-value|"/usr/games/lib/phantasia/gold"
-end_define
-
-begin_define
-define|#
-directive|define
-name|_PATH_HELP
-value|"/usr/games/lib/phantasia/phant.help"
+value|"/var/games/phantasia/gold"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_LASTDEAD
-value|"/usr/games/lib/phantasia/lastdead"
+value|"/var/games/phantasia/lastdead"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_MESS
-value|"/usr/games/lib/phantasia/mess"
+value|"/var/games/phantasia/mess"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_MONST
-value|"/usr/games/lib/phantasia/monsters"
+value|"/var/games/phantasia/monsters"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_MOTD
-value|"/usr/games/lib/phantasia/motd"
+value|"/var/games/phantasia/motd"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_PEOPLE
-value|"/usr/games/lib/phantasia/characs"
-end_define
-
-begin_define
-define|#
-directive|define
-name|_PATH_PHANTDIR
-value|"/usr/games/lib/phantasia"
+value|"/var/games/phantasia/characs"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_SCORE
-value|"/usr/games/lib/phantasia/scoreboard"
+value|"/var/games/phantasia/scoreboard"
 end_define
 
 begin_define
 define|#
 directive|define
 name|_PATH_VOID
-value|"/usr/games/lib/phantasia/void"
+value|"/var/games/phantasia/void"
 end_define
 
 end_unit
