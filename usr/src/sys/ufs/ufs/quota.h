4181c0bcb869d6c219b528682b0b989a2e2b2eb8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-03 02:30:12 -0800

    overhaul to provide for new quota environment
    SCCS-vsn: sys/ufs/ffs/quota.h 7.4
    SCCS-vsn: sys/ufs/ufs/quota.h 7.4
    
    Former-commit-id: 2b416cce57ed648dfb813b4e6f37b6ef0b6f5d27

--- usr/src/sys/ufs/ufs/quota.h
+++ usr/src/sys/ufs/ufs/quota.h
@@ -1,127 +1,104 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)quota.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Robert Elz at The University of Melbourne.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)quota.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * MELBOURNE DISC QUOTAS  *  * Various junk to do with various quotas (etc) imposed upon  * the average user (big brother finally hits UNIX).  *  * The following structure exists in core for each logged on user.  * It contains global junk relevant to that user's quotas.  *  * The u_quota field of each user struct contains a pointer to  * the quota struct relevant to the current process, this is changed  * by 'setuid' sys call,&/or by the Q_SETUID quota() call.  */
+comment|/*  * Definitions for disk quotas imposed on the average user  * (big brother finally hits UNIX).  *  * The following constants define the amount of time given a user  * before the soft limits are treated as hard limits (usually resulting  * in an allocation failure). The timer is started when the user crosses  * their soft limit, it is reset when they go below their soft limit.  */
 end_comment
-
-begin_struct
-struct|struct
-name|quota
-block|{
-name|struct
-name|quota
-modifier|*
-name|q_forw
-decl_stmt|,
-modifier|*
-name|q_back
-decl_stmt|;
-comment|/* hash chain, MUST be first */
-name|short
-name|q_cnt
-decl_stmt|;
-comment|/* ref count (# processes) */
-name|uid_t
-name|q_uid
-decl_stmt|;
-comment|/* real uid of owner */
-name|int
-name|q_flags
-decl_stmt|;
-comment|/* struct management flags */
-define|#
-directive|define
-name|Q_LOCK
-value|0x01
-comment|/* quota struct locked (for disc i/o) */
-define|#
-directive|define
-name|Q_WANT
-value|0x02
-comment|/* issue a wakeup when lock goes off */
-define|#
-directive|define
-name|Q_NEW
-value|0x04
-comment|/* new quota - no proc1 msg sent yet */
-define|#
-directive|define
-name|Q_NDQ
-value|0x08
-comment|/* account has NO disc quota */
-name|struct
-name|quota
-modifier|*
-name|q_freef
-decl_stmt|,
-modifier|*
-modifier|*
-name|q_freeb
-decl_stmt|;
-name|struct
-name|dquot
-modifier|*
-name|q_dq
-index|[
-name|NMOUNT
-index|]
-decl_stmt|;
-comment|/* disc quotas for mounted filesys's */
-block|}
-struct|;
-end_struct
 
 begin_define
 define|#
 directive|define
-name|NOQUOTA
-value|((struct quota *) 0)
+name|MAX_IQ_TIME
+value|(7*24*60*60)
 end_define
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
+begin_comment
+comment|/* 1 week */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAX_DQ_TIME
+value|(7*24*60*60)
+end_define
+
+begin_comment
+comment|/* 1 week */
+end_comment
+
+begin_comment
+comment|/*  * The following constants define the usage of the quota file array  * in the ufsmount structure and dquot array in the inode structure.  * The semantics of the elements of these arrays are defined in the  * routine getinoquota; the remainder of the quota code treats them  * generically and need not be inspected when changing the size of  * the array.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAXQUOTAS
+value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|USRQUOTA
+value|0
+end_define
+
+begin_comment
+comment|/* element used for user quotas */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|GRPQUOTA
+value|1
+end_define
+
+begin_comment
+comment|/* element used for group quotas */
+end_comment
+
+begin_comment
+comment|/*  * Definitions for the default names of the quotas files.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INITQFNAMES
+value|{ \ 	"user",
+comment|/* USRQUOTA */
+value|\ 	"group",
+comment|/* GRPQUOTA */
+value|\ 	"undefined", \ };
+end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
 name|KERNEL
-argument_list|)
-operator|&&
-name|defined
-argument_list|(
-name|QUOTA
-argument_list|)
-end_if
+end_ifndef
 
 begin_decl_stmt
-name|struct
-name|quota
+name|char
 modifier|*
-name|quota
-decl_stmt|,
-modifier|*
-name|quotaNQUOTA
+name|qfname
+init|=
+literal|"quota"
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
-name|nquota
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|struct
-name|quota
+name|char
 modifier|*
-name|getquota
-argument_list|()
-decl_stmt|,
-modifier|*
-name|qfind
-argument_list|()
+name|qfextension
+index|[]
+init|=
+name|INITQFNAMES
 decl_stmt|;
 end_decl_stmt
 
@@ -131,22 +108,104 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * The following structure defines the format of the disc quota file  * (as it appears on disc) - the file is an array of these structures  * indexed by user number.  The setquota sys call establishes the inode  * for each quota file (a pointer is retained in the mount structure).  *  * The following constants define the number of warnings given a user  * before the soft limits are treated as hard limits (usually resulting  * in an allocation failure).  The warnings are normally manipulated  * each time a user logs in through the Q_DOWARN quota call.  If  * the user logs in and is under the soft limit the warning count  * is reset to MAX_*_WARN, otherwise a message is printed and the  * warning count is decremented.  This makes MAX_*_WARN equivalent to  * the number of logins before soft limits are treated as hard limits.  */
+comment|/*  * Command definitions for the 'quotactl' system call.  * The commands are broken into a main command defined below  * and a subcommand that is used to convey the type of  * quota that is being manipulated (see above).  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|MAX_IQ_WARN
-value|3
+name|SUBCMDMASK
+value|0x00ff
 end_define
 
 begin_define
 define|#
 directive|define
-name|MAX_DQ_WARN
-value|3
+name|SUBCMDSHIFT
+value|8
 end_define
+
+begin_define
+define|#
+directive|define
+name|QCMD
+parameter_list|(
+name|cmd
+parameter_list|,
+name|type
+parameter_list|)
+value|(((cmd)<< SUBCMDSHIFT) | ((type)& SUBCMDMASK))
+end_define
+
+begin_define
+define|#
+directive|define
+name|Q_QUOTAON
+value|0x0100
+end_define
+
+begin_comment
+comment|/* enable quotas */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|Q_QUOTAOFF
+value|0x0200
+end_define
+
+begin_comment
+comment|/* disable quotas */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|Q_GETQUOTA
+value|0x0300
+end_define
+
+begin_comment
+comment|/* get limits and usage */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|Q_SETQUOTA
+value|0x0400
+end_define
+
+begin_comment
+comment|/* set limits and usage */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|Q_SETUSE
+value|0x0500
+end_define
+
+begin_comment
+comment|/* set usage */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|Q_SYNC
+value|0x0600
+end_define
+
+begin_comment
+comment|/* sync disk copy of a filesystems quotas */
+end_comment
+
+begin_comment
+comment|/*  * The following structure defines the format of the disk quota file  * (as it appears on disk) - the file is an array of these structures  * indexed by user or group number.  The setquota system call establishes  * the vnode for each quota file (a pointer is retained in the ufsmount  * structure).  */
+end_comment
 
 begin_struct
 struct|struct
@@ -155,41 +214,47 @@ block|{
 name|u_long
 name|dqb_bhardlimit
 decl_stmt|;
-comment|/* absolute limit on disc blks alloc */
+comment|/* absolute limit on disk blks alloc */
 name|u_long
 name|dqb_bsoftlimit
 decl_stmt|;
-comment|/* preferred limit on disc blks */
+comment|/* preferred limit on disk blks */
 name|u_long
 name|dqb_curblocks
 decl_stmt|;
 comment|/* current block count */
-name|u_short
+name|u_long
 name|dqb_ihardlimit
 decl_stmt|;
 comment|/* maximum # allocated inodes + 1 */
-name|u_short
+name|u_long
 name|dqb_isoftlimit
 decl_stmt|;
 comment|/* preferred inode limit */
-name|u_short
+name|u_long
 name|dqb_curinodes
 decl_stmt|;
 comment|/* current # allocated inodes */
-name|u_char
-name|dqb_bwarn
+name|time_t
+name|dqb_btime
 decl_stmt|;
-comment|/* # warnings left about excessive disc use */
-name|u_char
-name|dqb_iwarn
+comment|/* time limit for excessive disk use */
+name|time_t
+name|dqb_itime
 decl_stmt|;
-comment|/* # warnings left about excessive inodes */
+comment|/* time limit for excessive files */
 block|}
 struct|;
 end_struct
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KERNEL
+end_ifdef
+
 begin_comment
-comment|/*  * The following structure records disc usage for a user on a filesystem.  * There is one allocated for each quota that exists on any filesystem  * for the current user. A cache is kept of other recently used entries.  */
+comment|/*  * The following structure records disk usage for a user or group on a  * filesystem. There is one allocated for each quota that exists on any  * filesystem for the current user or group. A cache is kept of recently  * used entries.  */
 end_comment
 
 begin_struct
@@ -205,77 +270,42 @@ modifier|*
 name|dq_back
 decl_stmt|;
 comment|/* MUST be first entry */
-union|union
-block|{
-name|struct
-name|quota
-modifier|*
-name|Dq_own
-decl_stmt|;
-comment|/* the quota that points to this */
-struct|struct
-block|{
-comment|/* free list */
 name|struct
 name|dquot
 modifier|*
-name|Dq_freef
+name|dq_freef
 decl_stmt|,
 modifier|*
 modifier|*
-name|Dq_freeb
+name|dq_freeb
 decl_stmt|;
-block|}
-name|dq_f
-struct|;
-block|}
-name|dq_u
-union|;
+comment|/* free list */
 name|short
 name|dq_flags
 decl_stmt|;
-define|#
-directive|define
-name|DQ_LOCK
-value|0x01
-comment|/* this quota locked (no MODS) */
-define|#
-directive|define
-name|DQ_WANT
-value|0x02
-comment|/* wakeup on unlock */
-define|#
-directive|define
-name|DQ_MOD
-value|0x04
-comment|/* this quota modified since read */
-define|#
-directive|define
-name|DQ_FAKE
-value|0x08
-comment|/* no limits here, just usage */
-define|#
-directive|define
-name|DQ_BLKS
-value|0x10
-comment|/* has been warned about blk limit */
-define|#
-directive|define
-name|DQ_INODS
-value|0x20
-comment|/* has been warned about inode limit */
+comment|/* flags, see below */
 name|short
 name|dq_cnt
 decl_stmt|;
 comment|/* count of active references */
-name|uid_t
-name|dq_uid
+name|short
+name|dq_spare
 decl_stmt|;
-comment|/* user this applies to */
-name|dev_t
-name|dq_dev
+comment|/* unused spare padding */
+name|short
+name|dq_type
 decl_stmt|;
-comment|/* filesystem this relates to */
+comment|/* quota type of this dquot */
+name|u_long
+name|dq_id
+decl_stmt|;
+comment|/* identifier this applies to */
+name|struct
+name|ufsmount
+modifier|*
+name|dq_ump
+decl_stmt|;
+comment|/* filesystem that this is taken from */
 name|struct
 name|dqblk
 name|dq_dqb
@@ -285,26 +315,79 @@ block|}
 struct|;
 end_struct
 
-begin_define
-define|#
-directive|define
-name|dq_own
-value|dq_u.Dq_own
-end_define
+begin_comment
+comment|/*  * Flag values.  */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|dq_freef
-value|dq_u.dq_f.Dq_freef
+name|DQ_LOCK
+value|0x01
 end_define
+
+begin_comment
+comment|/* this quota locked (no MODS) */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|dq_freeb
-value|dq_u.dq_f.Dq_freeb
+name|DQ_WANT
+value|0x02
 end_define
+
+begin_comment
+comment|/* wakeup on unlock */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DQ_MOD
+value|0x04
+end_define
+
+begin_comment
+comment|/* this quota modified since read */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DQ_FAKE
+value|0x08
+end_define
+
+begin_comment
+comment|/* no limits here, just usage */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DQ_BLKS
+value|0x10
+end_define
+
+begin_comment
+comment|/* has been warned about blk limit */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DQ_INODS
+value|0x20
+end_define
+
+begin_comment
+comment|/* has been warned about inode limit */
+end_comment
+
+begin_comment
+comment|/*  * Shorthand notation.  */
+end_comment
 
 begin_define
 define|#
@@ -351,16 +434,20 @@ end_define
 begin_define
 define|#
 directive|define
-name|dq_bwarn
-value|dq_dqb.dqb_bwarn
+name|dq_btime
+value|dq_dqb.dqb_btime
 end_define
 
 begin_define
 define|#
 directive|define
-name|dq_iwarn
-value|dq_dqb.dqb_iwarn
+name|dq_itime
+value|dq_dqb.dqb_itime
 end_define
+
+begin_comment
+comment|/*  * If the system has never checked for a quota for this file,  * then it is set to NODQUOT. Once a write attempt is made  * the inode pointer is set to reference a dquot structure.  */
+end_comment
 
 begin_define
 define|#
@@ -369,64 +456,66 @@ name|NODQUOT
 value|((struct dquot *) 0)
 end_define
 
+begin_comment
+comment|/*  * Flags to chkdq() and chkiq()  */
+end_comment
+
 begin_define
 define|#
 directive|define
-name|LOSTDQUOT
-value|((struct dquot *) 1)
+name|FORCE
+value|0x01
 end_define
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|KERNEL
-argument_list|)
-operator|&&
-name|defined
-argument_list|(
-name|QUOTA
-argument_list|)
-end_if
+begin_comment
+comment|/* force usage changes independent of limits */
+end_comment
 
-begin_decl_stmt
-name|struct
-name|dquot
-modifier|*
-name|dquot
-decl_stmt|,
-modifier|*
-name|dquotNDQUOT
-decl_stmt|;
-end_decl_stmt
+begin_define
+define|#
+directive|define
+name|CHOWN
+value|0x02
+end_define
 
-begin_decl_stmt
-name|int
-name|ndquot
-decl_stmt|;
-end_decl_stmt
+begin_comment
+comment|/* (advisory) change initiated by chown */
+end_comment
 
-begin_decl_stmt
-name|struct
-name|dquot
-modifier|*
-name|discquota
-argument_list|()
-decl_stmt|,
-modifier|*
-name|inoquota
-argument_list|()
-decl_stmt|,
-modifier|*
-name|dqalloc
-argument_list|()
-decl_stmt|,
-modifier|*
-name|dqp
-argument_list|()
-decl_stmt|;
-end_decl_stmt
+begin_comment
+comment|/*  * Macros to avoid subroutine calls to trivial functions.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|DIAGNOSTIC
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|DQREF
+parameter_list|(
+name|dq
+parameter_list|)
+value|(dq)->dq_cnt++
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|DQREF
+parameter_list|(
+name|dq
+parameter_list|)
+value|dqref(dq)
+end_define
 
 begin_endif
 endif|#
@@ -434,121 +523,17 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * Definitions for the 'quota' system call.  */
+comment|/* DIAGNOSTIC */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|Q_SETDLIM
-value|1
-end_define
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
-comment|/* set disc limits& usage */
+comment|/* KERNEL */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|Q_GETDLIM
-value|2
-end_define
-
-begin_comment
-comment|/* get disc limits& usage */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|Q_SETDUSE
-value|3
-end_define
-
-begin_comment
-comment|/* set disc usage only */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|Q_SYNC
-value|4
-end_define
-
-begin_comment
-comment|/* update disc copy of quota usages */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|Q_SETUID
-value|16
-end_define
-
-begin_comment
-comment|/* change proc to use quotas for uid */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|Q_SETWARN
-value|25
-end_define
-
-begin_comment
-comment|/* alter inode/block warning counts */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|Q_DOWARN
-value|26
-end_define
-
-begin_comment
-comment|/* warn user about excessive space/inodes */
-end_comment
-
-begin_comment
-comment|/*  * Used in Q_SETDUSE.  */
-end_comment
-
-begin_struct
-struct|struct
-name|dqusage
-block|{
-name|u_short
-name|du_curinodes
-decl_stmt|;
-name|u_long
-name|du_curblocks
-decl_stmt|;
-block|}
-struct|;
-end_struct
-
-begin_comment
-comment|/*  * Used in Q_SETWARN.  */
-end_comment
-
-begin_struct
-struct|struct
-name|dqwarn
-block|{
-name|u_char
-name|dw_bwarn
-decl_stmt|;
-name|u_char
-name|dw_iwarn
-decl_stmt|;
-block|}
-struct|;
-end_struct
 
 end_unit
 
