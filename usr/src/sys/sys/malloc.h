0780f643c4d6bf9dbc71b563432d8d0437e1e137
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-03 02:21:22 -0800

    add entries for UFS mount table entries and UFS quota structures
    SCCS-vsn: sys/sys/malloc.h 7.14
    
    Former-commit-id: 1ed709e92b36828c45d672cf40d5e17723aa0bab

--- usr/src/sys/sys/malloc.h
+++ usr/src/sys/sys/malloc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)malloc.h	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)malloc.h	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -314,7 +314,29 @@ value|25
 end_define
 
 begin_comment
-comment|/* Dynamically allocated cahe entries */
+comment|/* Dynamically allocated cache entries */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|M_DQUOT
+value|26
+end_define
+
+begin_comment
+comment|/* UFS quota entries */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|M_UFSMNT
+value|27
+end_define
+
+begin_comment
+comment|/* UFS mount structure */
 end_comment
 
 begin_define
