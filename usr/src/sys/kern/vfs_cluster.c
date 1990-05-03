f8b01e7fd25ea8a9efb4a9c88f3751159d56e6f8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-03 02:10:54 -0800

    checks that vfs_busy is being used
    SCCS-vsn: sys/kern/vfs_bio.c 7.26
    SCCS-vsn: sys/kern/vfs_cluster.c 7.26
    
    Former-commit-id: 7a844f1087d0850a4ebc568ff149a35454749ce2

--- usr/src/sys/kern/vfs_cluster.c
+++ usr/src/sys/kern/vfs_cluster.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_cluster.c	7.25 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_cluster.c	7.26 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2694,6 +2694,23 @@ name|vnode
 modifier|*
 name|nvp
 decl_stmt|;
+if|if
+condition|(
+operator|(
+name|mountp
+operator|->
+name|m_flag
+operator|&
+name|M_MPBUSY
+operator|)
+operator|==
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"mntflushbuf: not busy"
+argument_list|)
+expr_stmt|;
 name|loop
 label|:
 for|for
@@ -3016,6 +3033,23 @@ name|dirty
 init|=
 literal|0
 decl_stmt|;
+if|if
+condition|(
+operator|(
+name|mountp
+operator|->
+name|m_flag
+operator|&
+name|M_MPBUSY
+operator|)
+operator|==
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"mntinvalbuf: not busy"
+argument_list|)
+expr_stmt|;
 name|loop
 label|:
 for|for
