7a1d3f2401f44d9af2ec89e601beb2cc693ca307
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-05-04 00:49:37 -0800

    1st level torek changes for now
    SCCS-vsn: sys/vax/vax/kdb_opset.c 7.5
    SCCS-vsn: sys/vax/include/kdbparam.h 7.3
    
    Former-commit-id: 6fba9859ad19c3cccfb52b571a55e13bfc9c4fe8

--- usr/src/sys/vax/include/kdbparam.h
+++ usr/src/sys/vax/include/kdbparam.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	@(#)kdbparam.h	7.2 (Berkeley) %G%  */
+comment|/*  *	@(#)kdbparam.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_define
