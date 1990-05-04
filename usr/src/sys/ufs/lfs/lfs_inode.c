df72603f5632fabf563fd36f48dbb3794af13fbf
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-03 21:41:08 -0800

    have to take off the hash list when bread of inode fails
    SCCS-vsn: sys/ufs/ffs/ffs_inode.c 7.31
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 7.31
    SCCS-vsn: sys/ufs/lfs/lfs_inode.c 7.31
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 7.31
    
    Former-commit-id: 2ec6b551a60550ca4df3de329e2b077ec4f14e3a

--- usr/src/sys/ufs/lfs/lfs_inode.c
+++ usr/src/sys/ufs/lfs/lfs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_inode.c	7.30 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_inode.c	7.31 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -672,6 +672,24 @@ name|bp
 argument_list|)
 condition|)
 block|{
+comment|/* 		 * The inode does not contain anything useful, so it would 		 * be misleading to leave it on its hash chain. 		 * Iput() will take care of putting it back on the free list. 		 */
+name|remque
+argument_list|(
+name|ip
+argument_list|)
+expr_stmt|;
+name|ip
+operator|->
+name|i_forw
+operator|=
+name|ip
+expr_stmt|;
+name|ip
+operator|->
+name|i_back
+operator|=
+name|ip
+expr_stmt|;
 comment|/* 		 * Unlock and discard unneeded inode. 		 */
 name|iput
 argument_list|(
