55f07bea8f54e52956bfd101f8077d1beeef9ac8
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-05-04 22:00:36 -0800

    lflag can change while we sleep in ttread.
    SCCS-vsn: sys/kern/tty.c 7.24
    
    Former-commit-id: 37b3d915d679a4eb58738d3517a60cc9e09ba1f3

--- usr/src/sys/kern/tty.c
+++ usr/src/sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)tty.c	7.23 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)tty.c	7.24 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -6039,6 +6039,12 @@ literal|0
 decl_stmt|;
 name|loop
 label|:
+name|lflag
+operator|=
+name|tp
+operator|->
+name|t_lflag
+expr_stmt|;
 name|s
 operator|=
 name|spltty
