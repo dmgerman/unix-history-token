5dcab6efadc47939bc340b41616ffa48431ca2e7
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-05 23:30:22 -0800

    no longer need to check um_fs to detect busy; vfs_busy ensures single threading
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.42
    SCCS-vsn: sys/ufs/ffs/ufs_vfsops.c 7.42
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.42
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 7.42
    
    Former-commit-id: 19cb88e030df4378be62163149f6681c9a7ac11b

--- usr/src/sys/ufs/lfs/lfs_vfsops.c
+++ usr/src/sys/ufs/lfs/lfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vfsops.c	7.41 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vfsops.c	7.42 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3054,12 +3054,6 @@ name|allerror
 init|=
 literal|0
 decl_stmt|;
-specifier|static
-name|int
-name|updlock
-init|=
-literal|0
-decl_stmt|;
 if|if
 condition|(
 name|syncprt
@@ -3067,39 +3061,11 @@ condition|)
 name|bufstats
 argument_list|()
 expr_stmt|;
-if|if
-condition|(
-name|updlock
-condition|)
-return|return
-operator|(
-name|EBUSY
-operator|)
-return|;
 name|fs
 operator|=
 name|ump
 operator|->
 name|um_fs
-expr_stmt|;
-if|if
-condition|(
-name|fs
-operator|==
-operator|(
-expr|struct
-name|fs
-operator|*
-operator|)
-literal|1
-condition|)
-return|return
-operator|(
-literal|0
-operator|)
-return|;
-name|updlock
-operator|++
 expr_stmt|;
 comment|/* 	 * Write back modified superblock. 	 * Consistency check that the superblock 	 * is still in the buffer cache. 	 */
 if|if
@@ -3286,10 +3252,6 @@ name|vp
 argument_list|)
 expr_stmt|;
 block|}
-name|updlock
-operator|=
-literal|0
-expr_stmt|;
 comment|/* 	 * Force stale file system control information to be flushed. 	 */
 name|vflushbuf
 argument_list|(
