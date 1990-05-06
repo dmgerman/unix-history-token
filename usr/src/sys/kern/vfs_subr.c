58745104e7eefe55fbb2ee7b03139408ccc2d00a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-06 00:11:07 -0800

    do not lock vnodes when copying for kinfo (if the info changes, who cares)
    SCCS-vsn: sys/kern/vfs_subr.c 7.45
    
    Former-commit-id: eec43744f00efb371959fe2f5db550692b02d1a6

--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.44 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.45 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -3820,7 +3820,6 @@ name|mnt_next
 expr_stmt|;
 continue|continue;
 block|}
-comment|/* 		 * A vget can fail if the vnode is being 		 * recycled.  In this (rare) case, we have to start 		 * over with this filesystem.  Also, have to 		 * check that the next vp is still associated 		 * with this filesystem.  RACE: could have been 		 * recycled onto the same filesystem. 		 */
 name|savebp
 operator|=
 name|bp
@@ -3844,6 +3843,7 @@ operator|->
 name|v_mountf
 control|)
 block|{
+comment|/* 			 * Check that the vp is still associated with 			 * this filesystem.  RACE: could have been 			 * recycled onto the same filesystem. 			 */
 if|if
 condition|(
 name|vp
@@ -3861,34 +3861,6 @@ name|printf
 argument_list|(
 literal|"kinfo: vp changed\n"
 argument_list|)
-expr_stmt|;
-name|bp
-operator|=
-name|savebp
-expr_stmt|;
-goto|goto
-name|again
-goto|;
-block|}
-if|if
-condition|(
-name|vget
-argument_list|(
-name|vp
-argument_list|)
-condition|)
-block|{
-if|if
-condition|(
-name|kinfo_vdebug
-condition|)
-name|printf
-argument_list|(
-literal|"kinfo: vget failed\n"
-argument_list|)
-expr_stmt|;
-name|kinfo_vgetfailed
-operator|++
 expr_stmt|;
 name|bp
 operator|=
@@ -3947,28 +3919,16 @@ argument_list|)
 operator|)
 operator|)
 condition|)
-block|{
-name|vput
-argument_list|(
-name|vp
-argument_list|)
-expr_stmt|;
 return|return
 operator|(
 name|error
 operator|)
 return|;
-block|}
 name|bp
 operator|+=
 name|VPTRSZ
 operator|+
 name|VNODESZ
-expr_stmt|;
-name|vput
-argument_list|(
-name|vp
-argument_list|)
 expr_stmt|;
 block|}
 block|omp
