43da6984a8cef41ef958d3958cad07a746e52b11
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-05 23:28:13 -0800

    have to check MNT_UNMOUNT flag after sleeping, not before
    SCCS-vsn: sys/kern/vfs_subr.c 7.43
    
    Former-commit-id: 6008c35dc83ed57bbf40a050f6c71963cef88da8

--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_subr.c	7.43 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -277,19 +277,6 @@ end_expr_stmt
 
 begin_block
 block|{
-if|if
-condition|(
-name|mp
-operator|->
-name|mnt_flag
-operator|&
-name|MNT_UNMOUNT
-condition|)
-return|return
-operator|(
-literal|1
-operator|)
-return|;
 while|while
 condition|(
 name|mp
@@ -319,6 +306,19 @@ name|PVFS
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|mp
+operator|->
+name|mnt_flag
+operator|&
+name|MNT_UNMOUNT
+condition|)
+return|return
+operator|(
+literal|1
+operator|)
+return|;
 name|mp
 operator|->
 name|mnt_flag
