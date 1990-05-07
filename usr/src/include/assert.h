3c2cc70379abd4ea49a1fa953f7fab4e7054ae9b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-06 22:52:12 -0800

    redo from scratch, print out assertion as well as line/file
    SCCS-vsn: include/assert.h 4.3
    
    Former-commit-id: 1bc250879ed3538278d6cb30001835532a393d60

--- usr/src/include/assert.h
+++ usr/src/include/assert.h
@@ -1,32 +1,24 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	assert.h	4.2	85/01/21	*/
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)assert.h	4.3 (Berkeley) %G%  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
+begin_ifdef
+ifdef|#
+directive|ifdef
 name|NDEBUG
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|_assert
-parameter_list|(
-name|ex
-parameter_list|)
-value|{if (!(ex)){fprintf(stderr,"Assertion failed: file \"%s\", line %d\n", __FILE__, __LINE__);exit(1);}}
-end_define
+end_ifdef
 
 begin_define
 define|#
 directive|define
 name|assert
-parameter_list|(
-name|ex
-parameter_list|)
-value|_assert(ex)
+end_define
+
+begin_define
+define|#
+directive|define
+name|_assert
 end_define
 
 begin_else
@@ -37,19 +29,21 @@ end_else
 begin_define
 define|#
 directive|define
-name|_assert
+name|assert
 parameter_list|(
-name|ex
+name|expression
 parameter_list|)
+value|{ \ 	if (!(expression)) { \ 		(void)fprintf(stderr, \ 		    "assertion \"%s\" failed: file \"%s\", line %d\n", \ 		    "expression", __FILE__, __LINE__); \ 		exit(2); \ 	} \ }
 end_define
 
 begin_define
 define|#
 directive|define
-name|assert
+name|_assert
 parameter_list|(
-name|ex
+name|expression
 parameter_list|)
+value|assert(expression)
 end_define
 
 begin_endif
