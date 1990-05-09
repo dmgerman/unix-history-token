e8a0436b9361535eae6aece07d0f2b09519b7d01
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-08 21:39:47 -0800

    when sync'ing, verify that vnode has not been recycled to a new filesystem
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.43
    SCCS-vsn: sys/ufs/ffs/ufs_vfsops.c 7.43
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.43
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 7.43
    
    Former-commit-id: c70e35c7f794ed0bd50e9683630d90787719b5d5

--- usr/src/sys/ufs/lfs/lfs_vfsops.c
+++ usr/src/sys/ufs/lfs/lfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vfsops.c	7.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vfsops.c	7.43 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3042,11 +3042,6 @@ name|fs
 modifier|*
 name|fs
 decl_stmt|;
-name|struct
-name|vnode
-modifier|*
-name|nvp
-decl_stmt|;
 name|int
 name|error
 decl_stmt|,
@@ -3141,15 +3136,23 @@ name|vp
 condition|;
 name|vp
 operator|=
-name|nvp
-control|)
-block|{
-name|nvp
-operator|=
 name|vp
 operator|->
 name|v_mountf
-expr_stmt|;
+control|)
+block|{
+comment|/* 		 * If the vnode that we are about to sync is no longer 		 * associated with this mount point, start over. 		 */
+if|if
+condition|(
+name|vp
+operator|->
+name|v_mount
+operator|!=
+name|mp
+condition|)
+goto|goto
+name|loop
+goto|;
 name|ip
 operator|=
 name|VTOI
