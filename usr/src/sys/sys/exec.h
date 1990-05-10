00921d47b3f74f3fd7bfc439aaaebd001b028beb
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-09 17:46:48 -0800

    update for hp300 merge
    SCCS-vsn: sys/sys/exec.h 7.2
    
    Former-commit-id: ba351b578e6c032bf211aa0568a67735920c3600

--- usr/src/sys/sys/exec.h
+++ usr/src/sys/sys/exec.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)exec.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)exec.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -11,10 +11,37 @@ begin_struct
 struct|struct
 name|exec
 block|{
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|vax
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|tahoe
+argument_list|)
+name|unsigned
+name|short
+name|a_mid
+decl_stmt|;
+comment|/* machine ID */
+name|unsigned
+name|short
+name|a_magic
+decl_stmt|;
+comment|/* magic number */
+else|#
+directive|else
 name|long
 name|a_magic
 decl_stmt|;
 comment|/* magic number */
+endif|#
+directive|endif
 name|unsigned
 name|long
 name|a_text
@@ -57,6 +84,21 @@ end_struct
 begin_define
 define|#
 directive|define
+name|a_machtype
+value|a_mid
+end_define
+
+begin_comment
+comment|/* SUN compatibility */
+end_comment
+
+begin_comment
+comment|/* a_magic */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|OMAGIC
 value|0407
 end_define
@@ -85,6 +127,87 @@ end_define
 
 begin_comment
 comment|/* demand load format */
+end_comment
+
+begin_comment
+comment|/* a_mid */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_ZERO
+value|0
+end_define
+
+begin_comment
+comment|/* unknown - implementation dependent */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_SUN010
+value|1
+end_define
+
+begin_comment
+comment|/* sun 68010/68020 binary */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_SUN020
+value|2
+end_define
+
+begin_comment
+comment|/* sun 68020-only binary */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_HP200
+value|200
+end_define
+
+begin_comment
+comment|/* hp200 (68010) BSD binary */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_HP300
+value|300
+end_define
+
+begin_comment
+comment|/* hp300 (68020+68881) BSD binary */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_HPUX
+value|0x20C
+end_define
+
+begin_comment
+comment|/* hp200/300 HP-UX binary */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MID_HPUX800
+value|0x20B
+end_define
+
+begin_comment
+comment|/* hp800 HP-UX binary */
 end_comment
 
 end_unit
