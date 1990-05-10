dd7834ce819e3de8cf4a2ca89fd4e84e015f367b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-09 17:50:51 -0800

    update for hp300 merge
    SCCS-vsn: sys/sys/ioctl_compat.h 7.2
    
    Former-commit-id: 9fe0919307d3be1d73fbe60fa5788b2aaa6f7985

--- usr/src/sys/sys/ioctl_compat.h
+++ usr/src/sys/sys/ioctl_compat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ioctl_compat.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ioctl_compat.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -936,6 +936,17 @@ end_define
 
 begin_comment
 comment|/* get local special chars*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|OTIOCCONS
+value|_IO('t', 98)
+end_define
+
+begin_comment
+comment|/* for hp300 -- sans int arg */
 end_comment
 
 begin_define
