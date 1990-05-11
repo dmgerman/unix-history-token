7b5d8241dfbaaebab675e0e555690d8e825d2f71
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-10 19:42:21 -0800

    merge in support for hp300
    SCCS-vsn: sys/sys/systm.h 7.8
    
    Former-commit-id: e8087481d7f5a727cae145bd408df12f1cc7511c

--- usr/src/sys/sys/systm.h
+++ usr/src/sys/sys/systm.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)systm.h	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)systm.h	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -248,6 +248,11 @@ operator|||
 name|defined
 argument_list|(
 name|tahoe
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|hp300
 argument_list|)
 end_if
 
