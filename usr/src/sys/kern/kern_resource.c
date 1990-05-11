ad0ae27bba736b3057dd07a2c8b04a4b8b6aba6b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-10 20:04:00 -0800

    get rid of #defines in user.h
    SCCS-vsn: sys/kern/kern_resource.c 7.7
    
    Former-commit-id: 74bfae0a18b46d8491cca2e968b3828202b04b79

--- usr/src/sys/kern/kern_resource.c
+++ usr/src/sys/kern/kern_resource.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_resource.c	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_resource.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -584,7 +584,9 @@ name|u_uid
 operator|&&
 name|u
 operator|.
-name|u_ruid
+name|u_procp
+operator|->
+name|p_ruid
 operator|&&
 name|u
 operator|.
@@ -596,7 +598,9 @@ name|p_uid
 operator|&&
 name|u
 operator|.
-name|u_ruid
+name|u_procp
+operator|->
+name|p_ruid
 operator|!=
 name|p
 operator|->
