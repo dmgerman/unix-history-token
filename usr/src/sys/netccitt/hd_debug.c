ccdf9ea86c99d1b08d1c114d757ab3bda08ec2bd
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-05-11 22:21:48 -0800

    install combined copyright headers using sccs include for terms
    SCCS-vsn: sys/netccitt/ccitt_proto.c 7.2
    SCCS-vsn: sys/netccitt/hd_debug.c 7.2
    
    Former-commit-id: ccc68d2eeb977dc07de99f2597d40b7e09ba94ff

--- usr/src/sys/netccitt/hd_debug.c
+++ usr/src/sys/netccitt/hd_debug.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* Copyright (c) University of British Columbia, 1984 */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)hd_debug.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
