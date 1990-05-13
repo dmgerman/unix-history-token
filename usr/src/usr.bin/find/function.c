e6cf04ae3f2f4692225357a1e0cb0668665b1d7b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-12 22:28:33 -0800

    define strerror() return
    SCCS-vsn: usr.bin/find/function.c 5.2
    
    Former-commit-id: 4a784597685e37ea0570a03c62f41aa305fb6ef7

--- usr/src/usr.bin/find/function.c
+++ usr/src/usr.bin/find/function.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)function.c	5.1 (Berkeley) %G%"
+literal|"@(#)function.c	5.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -86,6 +86,12 @@ begin_include
 include|#
 directive|include
 file|<stdio.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<strings.h>
 end_include
 
 begin_include
