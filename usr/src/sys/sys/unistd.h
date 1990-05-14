91c4ba58682f0247922eccc85a606f9b42c5adf6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-13 22:46:19 -0800

    access flags are really bit fields
    SCCS-vsn: sys/sys/unistd.h 5.5
    
    Former-commit-id: 114d25828de4bd59072d9d94442f42ab8a732b5a

--- usr/src/sys/sys/unistd.h
+++ usr/src/sys/sys/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)unistd.h	5.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)unistd.h	5.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -75,30 +75,19 @@ end_comment
 begin_define
 define|#
 directive|define
-name|R_OK
-value|4
+name|F_OK
+value|0
 end_define
 
 begin_comment
-comment|/* test for read permission */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|W_OK
-value|2
-end_define
-
-begin_comment
-comment|/* test for write permission */
+comment|/* test for existence of file */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|X_OK
-value|1
+value|0x01
 end_define
 
 begin_comment
@@ -108,12 +97,23 @@ end_comment
 begin_define
 define|#
 directive|define
-name|F_OK
-value|0
+name|W_OK
+value|0x02
 end_define
 
 begin_comment
-comment|/* test for existence of file */
+comment|/* test for write permission */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|R_OK
+value|0x04
+end_define
+
+begin_comment
+comment|/* test for read permission */
 end_comment
 
 begin_comment
