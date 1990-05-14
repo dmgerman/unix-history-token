bc788945fef5904e0fcb0275ee36594a9393a1ab
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-14 01:42:45 -0800

    redo from scratch; fix short/char round error in historic version
    SCCS-vsn: sys/hp300/include/varargs.h 5.1
    
    Former-commit-id: bdafa87a22cf23123a30c11952e5ec10648790a3

--- usr/src/sys/hp300/include/varargs.h
+++ usr/src/sys/hp300/include/varargs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	varargs.h	4.1	83/05/03	*/
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)varargs.h	5.1 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
@@ -14,27 +14,12 @@ end_typedef
 begin_define
 define|#
 directive|define
-name|va_dcl
-value|int va_alist;
-end_define
-
-begin_define
-define|#
-directive|define
-name|va_start
+name|__va_round
 parameter_list|(
-name|list
+name|type
 parameter_list|)
-value|list = (char *)&va_alist
-end_define
-
-begin_define
-define|#
-directive|define
-name|va_end
-parameter_list|(
-name|list
-parameter_list|)
+define|\
+value|(((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
 end_define
 
 begin_define
@@ -42,11 +27,39 @@ define|#
 directive|define
 name|va_arg
 parameter_list|(
-name|list
+name|ap
 parameter_list|,
-name|mode
+name|type
 parameter_list|)
-value|((mode *)(list += sizeof(mode)))[-1]
+define|\
+value|((type *)(ap += __va_round(type)))[-1]
+end_define
+
+begin_define
+define|#
+directive|define
+name|va_dcl
+value|int va_alist;
+end_define
+
+begin_define
+define|#
+directive|define
+name|va_end
+parameter_list|(
+name|ap
+parameter_list|)
+end_define
+
+begin_define
+define|#
+directive|define
+name|va_start
+parameter_list|(
+name|ap
+parameter_list|)
+define|\
+value|ap = (char *)&va_alist
 end_define
 
 end_unit
