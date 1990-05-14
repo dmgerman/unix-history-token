9cd89e8cc28b78fb73123c9cc7bc48633ded5e90
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-13 23:58:11 -0800

    correct POSIX #defines, add value for _POSIX_VDISABLE
    SCCS-vsn: sys/sys/unistd.h 5.6
    
    Former-commit-id: 1ab0836226dea2c58fa1b045397e3e1c9fe1caee

--- usr/src/sys/sys/unistd.h
+++ usr/src/sys/sys/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)unistd.h	5.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)unistd.h	5.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -10,27 +10,21 @@ end_comment
 begin_define
 define|#
 directive|define
-name|POSIX_JOB_CONTROL
+name|_POSIX_JOB_CONTROL
 end_define
 
 begin_comment
 comment|/* implementation supports job control */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|POSIX_SAVED_IDS
-end_define
-
 begin_comment
-comment|/* saved set-user-ID and set-group-ID */
+comment|/*#define	_POSIX_SAVED_IDS	/* saved set-user-ID and set-group-ID */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|POSIX_VERSION
+name|_POSIX_VERSION
 value|198808L
 end_define
 
@@ -41,7 +35,7 @@ end_comment
 begin_define
 define|#
 directive|define
-name|POSIX_CHOWN_RESTRICTED
+name|_POSIX_CHOWN_RESTRICTED
 end_define
 
 begin_comment
@@ -51,22 +45,23 @@ end_comment
 begin_define
 define|#
 directive|define
-name|POSIX_NO_TRUNC
+name|_POSIX_NO_TRUNC
 end_define
 
 begin_comment
 comment|/* too-long path components generate errors */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|POSIX_VDISABLE
-end_define
-
 begin_comment
 comment|/* may disable terminal special characters */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|_POSIX_VDISABLE
+value|((unsigned char)'\377')
+end_define
 
 begin_comment
 comment|/* access function */
