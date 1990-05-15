fe24221f22a1eb4fd67616a614a1f01f0857a2db
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-05-14 23:05:07 -0800

    new conventions for obtaining freeing ancillary data
    SCCS-vsn: sys/netiso/cltp_usrreq.c 7.4
    
    Former-commit-id: 3b416c685c397b53065cc1f1570d04b9aab2ea89

--- usr/src/sys/netiso/cltp_usrreq.c
+++ usr/src/sys/netiso/cltp_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)cltp_usrreq.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)cltp_usrreq.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -1388,6 +1388,7 @@ operator|)
 return|;
 if|if
 condition|(
+operator|(
 name|isop
 operator|==
 name|NULL
@@ -1395,6 +1396,15 @@ operator|&&
 name|req
 operator|!=
 name|PRU_ATTACH
+operator|)
+operator|||
+operator|(
+name|control
+operator|&&
+name|control
+operator|->
+name|m_len
+operator|)
 condition|)
 block|{
 name|error
@@ -1769,6 +1779,17 @@ expr_stmt|;
 block|}
 name|release
 label|:
+if|if
+condition|(
+name|control
+operator|!=
+name|NULL
+condition|)
+name|m_freem
+argument_list|(
+name|control
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|m
