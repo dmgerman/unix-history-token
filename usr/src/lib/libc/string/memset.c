6002568220a23c94815c5b2133881e764be646bc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-15 21:39:22 -0800

    ANSI C version from Chris Torek
    SCCS-vsn: lib/libc/string/memset.c 5.5
    
    Former-commit-id: 6e531c327a077e81c06bfeaeab307b1569df1aa9

--- usr/src/lib/libc/string/memset.c
+++ usr/src/lib/libc/string/memset.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Chris Torek.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_if
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)memset.c	5.4 (Berkeley) %G%"
+literal|"@(#)memset.c	5.5 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -37,56 +37,79 @@ begin_comment
 comment|/* LIBC_SCCS and not lint */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/stdc.h>
+end_include
+
 begin_function
-name|char
+name|void
 modifier|*
 name|memset
 parameter_list|(
-name|s
+name|dst
 parameter_list|,
 name|c
 parameter_list|,
 name|n
 parameter_list|)
-specifier|register
-name|char
+name|void
 modifier|*
-name|s
+name|dst
 decl_stmt|;
-function|register c
-operator|,
-function|n;
-end_function
-
-begin_block
+specifier|register
+name|int
+name|c
+decl_stmt|;
+specifier|register
+name|size_t
+name|n
+decl_stmt|;
+block|{
+if|if
+condition|(
+name|n
+operator|!=
+literal|0
+condition|)
 block|{
 specifier|register
 name|char
 modifier|*
-name|p
+name|d
 init|=
-name|s
+name|dst
 decl_stmt|;
-while|while
-condition|(
-operator|--
-name|n
-operator|>=
-literal|0
-condition|)
+do|do
 operator|*
-name|s
+name|d
 operator|++
 operator|=
 name|c
 expr_stmt|;
+do|while
+condition|(
+operator|--
+name|n
+operator|!=
+literal|0
+condition|)
+do|;
+block|}
 return|return
 operator|(
-name|p
+name|dst
 operator|)
 return|;
 block|}
-end_block
+end_function
 
 end_unit
 
