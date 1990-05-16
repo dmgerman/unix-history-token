900458f6322a764091ca8d01917a01a4f50e81bb
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-15 21:28:49 -0800

    lint
    SCCS-vsn: sys/miscfs/specfs/spec_vnops.c 7.27
    
    Former-commit-id: 81962ece995be619dd9ae9082e2cf8fd37b2ae35

--- usr/src/sys/miscfs/specfs/spec_vnops.c
+++ usr/src/sys/miscfs/specfs/spec_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)spec_vnops.c	7.26 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)spec_vnops.c	7.27 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -552,6 +552,10 @@ begin_comment
 comment|/*  * Vnode op for read  */
 end_comment
 
+begin_comment
+comment|/* ARGSUSED */
+end_comment
+
 begin_expr_stmt
 name|spec_read
 argument_list|(
@@ -1040,6 +1044,10 @@ end_block
 
 begin_comment
 comment|/*  * Vnode op for write  */
+end_comment
+
+begin_comment
+comment|/* ARGSUSED */
 end_comment
 
 begin_expr_stmt
