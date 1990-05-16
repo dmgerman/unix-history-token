d2a07726563d97ef629677d8226b62b51054365d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-16 05:05:41 -0800

    use proper include files
    SCCS-vsn: sys/hp/hpux/hpux_compat.c 7.2
    
    Former-commit-id: a730f63972680715a9e6452d81c0b863ad2f6fc0

--- usr/src/sys/hp/hpux/hpux_compat.c
+++ usr/src/sys/hp/hpux/hpux_compat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hpux_compat.c 1.33 89/08/23$  *  *	@(#)hpux_compat.c	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hpux_compat.c 1.33 89/08/23$  *  *	@(#)hpux_compat.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -5253,7 +5253,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"../h/times.h"
+file|"../sys/times.h"
 end_include
 
 begin_comment
