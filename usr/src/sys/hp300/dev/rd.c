337a42f15ccbcb7403f2d48ec59fdf9ef782d50d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-16 05:04:42 -0800

    use proper include files
    SCCS-vsn: sys/hp300/dev/rd.c 7.2
    SCCS-vsn: sys/hp300/dev/sd.c 7.2
    
    Former-commit-id: 218f68a60756adb048ccb0f464a45e141b0e8e27

--- usr/src/sys/hp300/dev/rd.c
+++ usr/src/sys/hp300/dev/rd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: rd.c 1.30 89/09/17$  *  *	@(#)rd.c	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: rd.c 1.30 89/09/17$  *  *	@(#)rd.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -6051,7 +6051,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"../h/vmmac.h"
+file|"../sys/vmmac.h"
 end_include
 
 begin_comment
