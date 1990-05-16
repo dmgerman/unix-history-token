13bdb4d04f66f2df9a40cca70dbf88e609cdd9ad
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-15 21:07:27 -0800

    fix from Hibler
    SCCS-vsn: sys/kern/kern_malloc.c 7.17
    
    Former-commit-id: 09427f347be57d45490d9fcd2c9e8a5011a07678

--- usr/src/sys/kern/kern_malloc.c
+++ usr/src/sys/kern/kern_malloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)kern_malloc.c	7.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)kern_malloc.c	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1455,9 +1455,9 @@ name|ks_limit
 operator|=
 name|npg
 operator|*
-name|CLBYTES
+name|NBPG
 operator|*
-literal|8
+literal|6
 operator|/
 literal|10
 expr_stmt|;
