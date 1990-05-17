3fe03907987ea8f5d835df35539cf650d376e10c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-17 03:15:54 -0800

    mounttab is gone!
    SCCS-vsn: sys/hp300/hp300/vm_machdep.c 7.2
    
    Former-commit-id: 3f34f33434a56fec0308b769ece66f4479d74887

--- usr/src/sys/hp300/hp300/vm_machdep.c
+++ usr/src/sys/hp300/hp300/vm_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_machdep.c 1.18 89/08/23$  *  *	@(#)vm_machdep.c	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_machdep.c 1.18 89/08/23$  *  *	@(#)vm_machdep.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -37,12 +37,6 @@ begin_include
 include|#
 directive|include
 file|"mount.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"../ufs/ufsmount.h"
 end_include
 
 begin_include
@@ -468,23 +462,12 @@ condition|(
 name|c
 operator|->
 name|c_blkno
-operator|&&
-name|c
-operator|->
-name|c_mdev
-operator|!=
-name|MSWAPX
 condition|)
 name|munhash
 argument_list|(
-name|mounttab
-index|[
 name|c
 operator|->
-name|c_mdev
-index|]
-operator|.
-name|um_devvp
+name|c_vp
 argument_list|,
 operator|(
 name|daddr_t
