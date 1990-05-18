ccd11248006132a33fe937b00e391a456b7650ea
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-18 15:44:25 -0800

    krb.h needs des.h
    SCCS-vsn: usr.bin/su/su.c 5.18
    
    Former-commit-id: 667b1ba0d85bc909b4321d66c3273797f8f4c51e

--- usr/src/usr.bin/su/su.c
+++ usr/src/usr.bin/su/su.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)su.c	5.17 (Berkeley) %G%"
+literal|"@(#)su.c	5.18 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -117,6 +117,12 @@ ifdef|#
 directive|ifdef
 name|KERBEROS
 end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<kerberosIV/des.h>
+end_include
 
 begin_include
 include|#
