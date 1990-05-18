2161509deefdd9c0d5f1e3d10b069cae83d871e6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-17 16:52:44 -0800

    move label_t to machine/machtypes
    SCCS-vsn: sys/sys/types.h 7.10
    
    Former-commit-id: 889d0ee0a53adc5024047172f3a026cdb1864355

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)types.h	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)types.h	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -15,9 +15,12 @@ directive|define
 name|_TYPES_
 end_define
 
-begin_comment
-comment|/*  * Basic system types and major/minor device constructing/busting macros.  */
-end_comment
+begin_typedef
+typedef|typedef
+name|short
+name|dev_t
+typedef|;
+end_typedef
 
 begin_comment
 comment|/* major part of a device */
@@ -104,103 +107,55 @@ typedef|;
 end_typedef
 
 begin_comment
-comment|/* sys III compat */
+comment|/* sys V compatibility */
 end_comment
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|vax
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|tahoe
-argument_list|)
-end_if
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KERNEL
+end_ifdef
 
-begin_typedef
-typedef|typedef
-struct|struct
-name|_physadr
-block|{
-name|int
-name|r
-index|[
-literal|1
-index|]
-decl_stmt|;
-block|}
-typedef|*
-name|physadr
-typedef|;
-end_typedef
+begin_include
+include|#
+directive|include
+file|"machine/machtypes.h"
+end_include
 
-begin_typedef
-typedef|typedef
-struct|struct
-name|label_t
-block|{
-name|int
-name|val
-index|[
-literal|14
-index|]
-decl_stmt|;
-block|}
-name|label_t
-typedef|;
-end_typedef
+begin_else
+else|#
+directive|else
+end_else
+
+begin_include
+include|#
+directive|include
+file|<machine/machtypes.h>
+end_include
 
 begin_endif
 endif|#
 directive|endif
 end_endif
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|hp300
-argument_list|)
-end_if
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|_SIZE_T_
+end_ifdef
 
 begin_typedef
 typedef|typedef
-struct|struct
-name|_physadr
-block|{
-name|short
-name|r
-index|[
-literal|1
-index|]
-decl_stmt|;
-block|}
-typedef|*
-name|physadr
+name|_SIZE_T_
+name|size_t
 typedef|;
 end_typedef
 
-begin_typedef
-typedef|typedef
-struct|struct
-name|label_t
-block|{
-name|int
-name|val
-index|[
-literal|15
-index|]
-decl_stmt|;
-comment|/* consistent with HP-UX */
-block|}
-name|label_t
-typedef|;
-end_typedef
+begin_undef
+undef|#
+directive|undef
+name|_SIZE_T_
+end_undef
 
 begin_endif
 endif|#
@@ -243,6 +198,18 @@ end_typedef
 begin_typedef
 typedef|typedef
 name|long
+modifier|*
+name|qaddr_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* should be typedef quad * qaddr_t; */
+end_comment
+
+begin_typedef
+typedef|typedef
+name|long
 name|daddr_t
 typedef|;
 end_typedef
@@ -254,18 +221,6 @@ modifier|*
 name|caddr_t
 typedef|;
 end_typedef
-
-begin_typedef
-typedef|typedef
-name|long
-modifier|*
-name|qaddr_t
-typedef|;
-end_typedef
-
-begin_comment
-comment|/* should be typedef quad * qaddr_t; */
-end_comment
 
 begin_typedef
 typedef|typedef
@@ -299,13 +254,6 @@ begin_typedef
 typedef|typedef
 name|u_long
 name|clock_t
-typedef|;
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|short
-name|dev_t
 typedef|;
 end_typedef
 
@@ -370,7 +318,7 @@ comment|/* number of bits in a byte */
 end_comment
 
 begin_comment
-comment|/*  * Select uses bit masks of file descriptors in longs.  * These macros manipulate such bit fields (the filesystem macros use chars).  * FD_SETSIZE may be defined by the user, but the default here  * should be>= NOFILE (param.h).  */
+comment|/*  * Select uses bit masks of file descriptors in longs.  These macros  * manipulate such bit fields (the filesystem macros use chars).  * FD_SETSIZE may be defined by the user, but the default here should  * be>= NOFILE (param.h).  */
 end_comment
 
 begin_ifndef
