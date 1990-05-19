49c3af59a15c7467a326c44bfe584fa458f7f40d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-18 19:25:10 -0800

    ANSI doesn't allow promotions for the user
    SCCS-vsn: include/stdarg.h 5.2
    
    Former-commit-id: 30c176febc5db592c4bbcc92ee2dde4b3fb7e8aa

--- usr/src/include/stdarg.h
+++ usr/src/include/stdarg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
@@ -11,16 +11,9 @@ name|va_list
 typedef|;
 end_typedef
 
-begin_define
-define|#
-directive|define
-name|__va_round
-parameter_list|(
-name|type
-parameter_list|)
-define|\
-value|(((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
-end_define
+begin_comment
+comment|/*  * ANSI says: "If there is no actual next argument, or if type is not  * compatible with the type of the actual next argument (as promoted  * according to the default argument promotions), the behavior is  * undefined."  We read this to mean that we're not allowed to do the  * promotion for the user, so shorts and chars drop core.  */
+end_comment
 
 begin_define
 define|#
@@ -32,7 +25,7 @@ parameter_list|,
 name|type
 parameter_list|)
 define|\
-value|((type *)(ap += __va_round(type)))[-1]
+value|((type *)(ap += sizeof(type)< sizeof(int) ? \ 		abort() : sizeof(type)))[-1]
 end_define
 
 begin_define
@@ -47,6 +40,17 @@ end_define
 begin_define
 define|#
 directive|define
+name|__va_promote
+parameter_list|(
+name|type
+parameter_list|)
+define|\
+value|(((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+end_define
+
+begin_define
+define|#
+directive|define
 name|va_start
 parameter_list|(
 name|ap
@@ -54,7 +58,7 @@ parameter_list|,
 name|last
 parameter_list|)
 define|\
-value|(ap = ((char *)&(last) + __va_round(last)))
+value|(ap = ((char *)&(last) + __va_promote(last)))
 end_define
 
 end_unit
