ef4a665dc8ce86ef9a9f6c56c0ee08f13f4d3372
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-05-22 23:09:20 -0800

    date and time created 90/05/22 16:09:20 by marc
    SCCS-vsn: sys/sys/tprintf.h 1.1
    
    Former-commit-id: 2311098711cd84ea04a3830628574d8b39e46dbd

---
+++ usr/src/sys/sys/tprintf.h
@@ -0,0 +1,23 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tprintf.h	1.1 (Berkeley) %G%  */
+end_comment
+
+begin_typedef
+typedef|typedef
+name|struct
+name|session
+modifier|*
+name|tpr_t
+typedef|;
+end_typedef
+
+begin_function_decl
+name|tpr_t
+name|tpr_open
+parameter_list|()
+function_decl|;
+end_function_decl
+
+end_unit
+
