5483a337e87fc5eccc392d4284d053f02162f890
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-25 17:29:58 -0800

    integrate fts(3), don't need lots of this stuff
    SCCS-vsn: usr.sbin/mtree/mtree.h 5.3
    
    Former-commit-id: fa5f0016e198b87fe2bf4406a221e7081c983b4d

--- usr/src/usr.sbin/mtree/mtree.h
+++ usr/src/usr.sbin/mtree/mtree.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mtree.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mtree.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -18,83 +18,8 @@ end_include
 begin_typedef
 typedef|typedef
 struct|struct
-name|_id
-block|{
-name|struct
-name|_id
-modifier|*
-name|next
-decl_stmt|;
-name|u_int
-name|id
-decl_stmt|;
-name|u_long
-name|cnt
-decl_stmt|;
-block|}
-name|ID
-typedef|;
-end_typedef
-
-begin_typedef
-typedef|typedef
-struct|struct
 name|_info
 block|{
-define|#
-directive|define
-name|F_CKSUM
-value|0x001
-comment|/* check sum */
-define|#
-directive|define
-name|F_DMODE
-value|0x002
-comment|/* directory mode */
-define|#
-directive|define
-name|F_FMODE
-value|0x004
-comment|/* directory mode */
-define|#
-directive|define
-name|F_GROUP
-value|0x008
-comment|/* group */
-define|#
-directive|define
-name|F_MODE
-value|0x010
-comment|/* directory mode */
-define|#
-directive|define
-name|F_NLINK
-value|0x020
-comment|/* number of links */
-define|#
-directive|define
-name|F_OWNER
-value|0x040
-comment|/* owner */
-define|#
-directive|define
-name|F_SIZE
-value|0x080
-comment|/* size */
-define|#
-directive|define
-name|F_SLINK
-value|0x100
-comment|/* link count */
-define|#
-directive|define
-name|F_TYPE
-value|0x200
-comment|/* file type */
-name|u_int
-name|flags
-decl_stmt|;
-comment|/* items set */
 define|#
 directive|define
 name|F_BLOCK
@@ -125,29 +50,87 @@ directive|define
 name|F_SOCK
 value|0x020
 comment|/* socket */
-name|u_int
+define|#
+directive|define
+name|F_NONE
+value|0x040
+comment|/* unknown */
+name|u_short
 name|type
 decl_stmt|;
 comment|/* file type */
+define|#
+directive|define
+name|F_CKSUM
+value|0x001
+comment|/* check sum */
+define|#
+directive|define
+name|F_GROUP
+value|0x002
+comment|/* group */
+define|#
+directive|define
+name|F_IGN
+value|0x004
+comment|/* ignore */
+define|#
+directive|define
+name|F_MODE
+value|0x008
+comment|/* mode */
+define|#
+directive|define
+name|F_NLINK
+value|0x010
+comment|/* number of links */
+define|#
+directive|define
+name|F_OWNER
+value|0x020
+comment|/* owner */
+define|#
+directive|define
+name|F_SIZE
+value|0x040
+comment|/* size */
+define|#
+directive|define
+name|F_SLINK
+value|0x080
+comment|/* link count */
+define|#
+directive|define
+name|F_TYPE
+value|0x100
+comment|/* file type */
+name|u_short
+name|flags
+decl_stmt|;
+comment|/* items set */
+name|off_t
+name|st_size
+decl_stmt|;
+name|u_long
+name|cksum
+decl_stmt|;
+comment|/* check sum */
 name|uid_t
 name|st_uid
 decl_stmt|;
 name|gid_t
 name|st_gid
 decl_stmt|;
-name|off_t
-name|st_size
-decl_stmt|;
+define|#
+directive|define
+name|MBITS
+value|(S_ISUID|S_ISGID|S_ISTXT|S_IRWXU|S_IRWXG|S_IRWXO)
 name|mode_t
 name|st_mode
 decl_stmt|;
 name|nlink_t
 name|st_nlink
 decl_stmt|;
-name|u_long
-name|cksum
-decl_stmt|;
-comment|/* check sum */
 name|char
 modifier|*
 name|slink
@@ -166,15 +149,21 @@ block|{
 name|struct
 name|_entry
 modifier|*
-name|next
-decl_stmt|,
-modifier|*
 name|child
 decl_stmt|,
 modifier|*
 name|parent
 decl_stmt|;
-comment|/* tree links */
+comment|/* up, down */
+name|struct
+name|_entry
+modifier|*
+name|next
+decl_stmt|,
+modifier|*
+name|prev
+decl_stmt|;
+comment|/* left, right */
 name|INFO
 name|info
 decl_stmt|;
@@ -186,15 +175,10 @@ value|0x01
 comment|/* directory done */
 define|#
 directive|define
-name|F_IGN
-value|0x02
-comment|/* ignore this node on down */
-define|#
-directive|define
 name|F_VISIT
-value|0x04
+value|0x02
 comment|/* visited this node */
-name|u_int
+name|u_char
 name|flags
 decl_stmt|;
 comment|/* flags */
@@ -203,14 +187,20 @@ modifier|*
 name|name
 decl_stmt|;
 comment|/* node name */
-name|int
-name|nlen
-decl_stmt|;
-comment|/* name length */
 block|}
 name|ENTRY
 typedef|;
 end_typedef
+
+begin_define
+define|#
+directive|define
+name|RP
+parameter_list|(
+name|p
+parameter_list|)
+value|(p->fts_path + 2)
+end_define
 
 end_unit
 
