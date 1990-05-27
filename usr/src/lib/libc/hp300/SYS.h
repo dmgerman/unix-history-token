210bd624a5f3fdbd5d1dde4d2824434231627f0e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-26 18:54:58 -0800

    add .even to guarantee jmp instruction is on even address
    SCCS-vsn: lib/libc/hp300/SYS.h 5.2
    
    Former-commit-id: 5402a940a4969f8df1cc5ac285441e015a0bde5f

--- usr/src/lib/libc/hp300/SYS.h
+++ usr/src/lib/libc/hp300/SYS.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)SYS.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)SYS.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -124,7 +124,7 @@ name|SYSCALL
 parameter_list|(
 name|x
 parameter_list|)
-value|err: jmp cerror; ENTRY(x); movl #SYS_
+value|.even; err: jmp cerror; ENTRY(x); movl #SYS_
 comment|/**/
 value|x,d0; \ 			trap #0; jcs err
 end_define
