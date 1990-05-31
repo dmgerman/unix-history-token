b9c0e0d5ec4481bc7469d2fe06c7710fdd29f400
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-30 19:20:02 -0800

    add ASMSTR #define
    SCCS-vsn: lib/libc/hp300/SYS.h 5.3
    SCCS-vsn: lib/libc/i386/SYS.h 5.2
    SCCS-vsn: lib/libc/vax/SYS.h 5.5
    SCCS-vsn: lib/libc/tahoe/SYS.h 5.5
    
    Former-commit-id: d817ee2b8dc729593cd97f5a75b0c9228e2ec4e6

--- usr/src/lib/libc/hp300/SYS.h
+++ usr/src/lib/libc/hp300/SYS.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)SYS.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)SYS.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -141,6 +141,13 @@ parameter_list|)
 value|ENTRY(x); movl #SYS_
 comment|/**/
 value|y,d0; trap #0;
+end_define
+
+begin_define
+define|#
+directive|define
+name|ASMSTR
+value|.asciz
 end_define
 
 begin_expr_stmt
