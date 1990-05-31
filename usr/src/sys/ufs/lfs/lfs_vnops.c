7a4604547c4e94c110e22112865171d6ca4aea36
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-31 02:19:48 -0800

    have to write out inode if IO_SYNC is specified
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.43
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 7.43
    SCCS-vsn: sys/ufs/lfs/lfs_vnops.c 7.43
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 7.43
    
    Former-commit-id: b376841310991e25b4b39ee5f5006447efa58220

--- usr/src/sys/ufs/lfs/lfs_vnops.c
+++ usr/src/sys/ufs/lfs/lfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vnops.c	7.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)lfs_vnops.c	7.43 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3973,6 +3973,32 @@ operator|=
 name|resid
 expr_stmt|;
 block|}
+if|if
+condition|(
+operator|!
+name|error
+operator|&&
+operator|(
+name|ioflag
+operator|&
+name|IO_SYNC
+operator|)
+condition|)
+name|error
+operator|=
+name|iupdat
+argument_list|(
+name|ip
+argument_list|,
+operator|&
+name|time
+argument_list|,
+operator|&
+name|time
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 name|error
