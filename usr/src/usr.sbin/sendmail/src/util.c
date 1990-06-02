1ca9693308b2fb9172b6fad4f9c084c6126377c7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-02 02:07:05 -0800

    new copyright notice
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 5.15
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.64
    SCCS-vsn: usr.sbin/sendmail/src/util.c 5.18
    
    Former-commit-id: b84fd9103a812ec48280496d0450f4765663a55d

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that this notice is preserved and that due credit is given  * to the University of California at Berkeley. The name of the University  * may not be used to endorse or promote products derived from this  * software without specific prior written permission. This software  * is provided ``as is'' without express or implied warranty.  *  *  Sendmail  *  Copyright (c) 1983  Eric P. Allman  *  Berkeley, California  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)util.c	5.17 (Berkeley) %G%"
+literal|"@(#)util.c	5.18 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
