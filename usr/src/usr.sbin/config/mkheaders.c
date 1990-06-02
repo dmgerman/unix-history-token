c9ee29624b75f376b86214579811543b2d2e58a0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-02 01:41:36 -0800

    new copyright notice
    SCCS-vsn: usr.bin/users/users.c 5.9
    SCCS-vsn: usr.bin/uudecode/uudecode.c 5.10
    SCCS-vsn: usr.bin/uuencode/uuencode.c 5.9
    SCCS-vsn: usr.bin/vacation/vacation.c 5.17
    SCCS-vsn: usr.bin/vmstat/vmstat.c 5.20
    SCCS-vsn: usr.bin/wc/wc.c 5.6
    SCCS-vsn: usr.bin/what/what.c 5.4
    SCCS-vsn: usr.bin/who/who.c 5.11
    SCCS-vsn: old/whoami/whoami.c 5.4
    SCCS-vsn: usr.bin/whois/whois.c 5.9
    SCCS-vsn: usr.bin/write/write.c 4.22
    SCCS-vsn: usr.bin/yes/yes.c 5.4
    SCCS-vsn: usr.sbin/accton/accton.c 4.3
    SCCS-vsn: old/arff/pathnames.h 5.2
    SCCS-vsn: usr.sbin/arp/arp.c 5.11
    SCCS-vsn: usr.sbin/bad144/bad144.c 5.18
    SCCS-vsn: usr.sbin/chown/chown.c 5.15
    SCCS-vsn: usr.sbin/chroot/chroot.c 5.8
    SCCS-vsn: usr.sbin/config/config.h 5.10
    SCCS-vsn: usr.sbin/config/config.y 5.10
    SCCS-vsn: usr.sbin/diskpart/diskpart.c 5.11
    SCCS-vsn: usr.sbin/config/main.c 5.12
    SCCS-vsn: old/dlmpcc/dlmpcc.c 5.5
    SCCS-vsn: usr.sbin/config/mkglue.c 5.8
    SCCS-vsn: old/dlmpcc/pathnames.h 5.2
    SCCS-vsn: usr.sbin/config/mkheaders.c 5.6
    SCCS-vsn: old/dlmpcc/scnhdr.h 5.3
    SCCS-vsn: usr.sbin/config/mkioconf.c 5.14
    SCCS-vsn: usr.sbin/config/mkmakefile.c 5.28
    SCCS-vsn: usr.sbin/config/mkswapconf.c 5.8
    SCCS-vsn: usr.sbin/edquota/edquota.c 5.13
    SCCS-vsn: usr.sbin/edquota/pathnames.h 5.3
    SCCS-vsn: old/flcopy/flcopy.c 5.3
    SCCS-vsn: old/gettable/gettable.c 5.5
    SCCS-vsn: old/htable/htable.c 5.9
    SCCS-vsn: old/htable/htable.h 5.4
    SCCS-vsn: old/implog/implog.c 5.13
    SCCS-vsn: old/htable/parse.y 5.5
    SCCS-vsn: old/implog/pathnames.h 5.5
    SCCS-vsn: old/implogd/implogd.c 5.9
    SCCS-vsn: usr.sbin/inetd/inetd.c 5.22
    SCCS-vsn: usr.sbin/inetd/pathnames.h 5.3
    SCCS-vsn: usr.sbin/kgmon/kgmon.c 5.10
    
    Former-commit-id: 41a08f97b282efdf6d9fb0d8a9bcb0882f4b9532

--- usr/src/usr.sbin/config/mkheaders.c
+++ usr/src/usr.sbin/config/mkheaders.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mkheaders.c	5.5 (Berkeley) %G%"
+literal|"@(#)mkheaders.c	5.6 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
