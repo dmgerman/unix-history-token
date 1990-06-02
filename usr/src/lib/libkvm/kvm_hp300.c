50efa4a53d887a994399d96d760da1acd0c0329b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-01 22:48:45 -0800

    needed sccs id's
    SCCS-vsn: lib/libkvm/kvm.c 5.7
    SCCS-vsn: lib/libkvm/kvm_hp300.c 5.7
    SCCS-vsn: lib/libkvm/kvm_proc.c 5.7
    
    Former-commit-id: 4e74a45537367b16a586ff953cc898f9dd3c2af8

--- usr/src/lib/libkvm/kvm_hp300.c
+++ usr/src/lib/libkvm/kvm_hp300.c
@@ -1,6 +1,40 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms is permitted  * provided that all copyright information, including this notice,  * is retained in all such forms, and that any documentation,  * advertising or other materials related to such distribution and  * use acknowledge that the software was  * developed by the University of California, Berkeley.  The name  * of the University may not be used to endorse or promote products  * derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
+comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|LIBC_SCCS
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|lint
+argument_list|)
+end_if
+
+begin_decl_stmt
+specifier|static
+name|char
+name|sccsid
+index|[]
+init|=
+literal|"@(#)kvm_hp300.c	5.7 (Berkeley) %G%"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* LIBC_SCCS and not lint */
 end_comment
 
 begin_include
