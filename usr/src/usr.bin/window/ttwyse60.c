818477563a7ea77d7324558cf542b164f9872a13
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-02 22:26:16 -0800

    new copyright notice
    SCCS-vsn: usr.bin/window/alias.h 3.7
    SCCS-vsn: usr.bin/window/char.c 3.6
    SCCS-vsn: usr.bin/window/char.h 3.8
    SCCS-vsn: usr.bin/window/cmd.c 3.39
    SCCS-vsn: usr.bin/window/cmd1.c 3.35
    SCCS-vsn: usr.bin/window/cmd2.c 3.39
    SCCS-vsn: usr.bin/window/cmd3.c 3.17
    SCCS-vsn: usr.bin/window/cmd4.c 3.17
    SCCS-vsn: usr.bin/window/cmd5.c 3.17
    SCCS-vsn: usr.bin/window/cmd6.c 3.16
    SCCS-vsn: usr.bin/window/cmd7.c 3.6
    SCCS-vsn: usr.bin/window/compress.c 3.4
    SCCS-vsn: usr.bin/window/context.c 3.11
    SCCS-vsn: usr.bin/window/context.h 3.10
    SCCS-vsn: usr.bin/window/defs.h 3.20
    SCCS-vsn: usr.bin/window/error.c 3.15
    SCCS-vsn: usr.bin/window/lcmd.c 3.33
    SCCS-vsn: usr.bin/window/lcmd.h 3.8
    SCCS-vsn: usr.bin/window/lcmd1.c 3.35
    SCCS-vsn: usr.bin/window/lcmd2.c 3.21
    SCCS-vsn: usr.bin/window/local.h 3.10
    SCCS-vsn: usr.bin/window/main.c 3.40
    SCCS-vsn: usr.bin/window/mloop.c 3.16
    SCCS-vsn: usr.bin/window/parser.h 3.8
    SCCS-vsn: usr.bin/window/parser1.c 3.21
    SCCS-vsn: usr.bin/window/parser2.c 3.13
    SCCS-vsn: usr.bin/window/parser3.c 3.8
    SCCS-vsn: usr.bin/window/parser4.c 3.7
    SCCS-vsn: usr.bin/window/parser5.c 3.12
    SCCS-vsn: usr.bin/window/scanner.c 3.13
    SCCS-vsn: usr.bin/window/startup.c 3.24
    SCCS-vsn: usr.bin/window/string.c 3.12
    SCCS-vsn: usr.bin/window/string.h 3.8
    SCCS-vsn: usr.bin/window/token.h 3.8
    SCCS-vsn: usr.bin/window/tt.h 3.26
    SCCS-vsn: usr.bin/window/ttf100.c 3.11
    SCCS-vsn: usr.bin/window/ttgeneric.c 3.44
    SCCS-vsn: usr.bin/window/tth19.c 3.24
    SCCS-vsn: usr.bin/window/tth29.c 3.9
    SCCS-vsn: usr.bin/window/ttinit.c 3.25
    SCCS-vsn: usr.bin/window/ttoutput.c 3.8
    SCCS-vsn: usr.bin/window/tttermcap.c 3.11
    SCCS-vsn: usr.bin/window/tttvi925.c 3.5
    SCCS-vsn: usr.bin/window/ttwyse60.c 3.3
    SCCS-vsn: usr.bin/window/ttwyse75.c 3.3
    SCCS-vsn: usr.bin/window/ttzapple.c 3.9
    SCCS-vsn: usr.bin/window/ttzentec.c 3.4
    SCCS-vsn: usr.bin/window/value.h 3.8
    SCCS-vsn: usr.bin/window/var.c 3.11
    SCCS-vsn: usr.bin/window/var.h 3.10
    SCCS-vsn: usr.bin/window/win.c 3.23
    SCCS-vsn: usr.bin/window/ww.h 3.59
    SCCS-vsn: usr.bin/window/wwadd.c 3.13
    SCCS-vsn: usr.bin/window/wwalloc.c 3.10
    SCCS-vsn: usr.bin/window/wwbox.c 3.7
    SCCS-vsn: usr.bin/window/wwchild.c 3.11
    SCCS-vsn: usr.bin/window/wwclose.c 3.17
    SCCS-vsn: usr.bin/window/wwclreol.c 3.19
    SCCS-vsn: usr.bin/window/wwclreos.c 3.10
    SCCS-vsn: usr.bin/window/wwcursor.c 3.11
    SCCS-vsn: usr.bin/window/wwdata.c 3.17
    SCCS-vsn: usr.bin/window/wwdelchar.c 3.15
    SCCS-vsn: usr.bin/window/wwdelete.c 3.18
    SCCS-vsn: usr.bin/window/wwdelline.c 3.11
    SCCS-vsn: usr.bin/window/wwdump.c 3.14
    SCCS-vsn: usr.bin/window/wwend.c 3.14
    SCCS-vsn: usr.bin/window/wwenviron.c 3.25
    SCCS-vsn: usr.bin/window/wwerror.c 3.7
    SCCS-vsn: usr.bin/window/wwflush.c 3.11
    SCCS-vsn: usr.bin/window/wwframe.c 3.19
    SCCS-vsn: usr.bin/window/wwgets.c 3.14
    SCCS-vsn: usr.bin/window/wwinit.c 3.38
    SCCS-vsn: usr.bin/window/wwinschar.c 3.19
    SCCS-vsn: usr.bin/window/wwinsline.c 3.11
    SCCS-vsn: usr.bin/window/wwiomux.c 3.24
    SCCS-vsn: usr.bin/window/wwlabel.c 3.16
    SCCS-vsn: usr.bin/window/wwmisc.c 3.13
    SCCS-vsn: usr.bin/window/wwmove.c 3.10
    SCCS-vsn: usr.bin/window/wwopen.c 3.28
    SCCS-vsn: usr.bin/window/wwprintf.c 3.10
    SCCS-vsn: usr.bin/window/wwpty.c 3.17
    SCCS-vsn: usr.bin/window/wwputc.c 3.7
    SCCS-vsn: usr.bin/window/wwputs.c 3.7
    SCCS-vsn: usr.bin/window/wwredraw.c 3.12
    SCCS-vsn: usr.bin/window/wwredrawwin.c 3.15
    SCCS-vsn: usr.bin/window/wwrint.c 3.9
    SCCS-vsn: usr.bin/window/wwscroll.c 3.23
    SCCS-vsn: usr.bin/window/wwsize.c 3.8
    SCCS-vsn: usr.bin/window/wwspawn.c 3.14
    SCCS-vsn: usr.bin/window/wwsuspend.c 3.12
    SCCS-vsn: usr.bin/window/wwtty.c 3.15
    SCCS-vsn: usr.bin/window/wwunframe.c 3.19
    SCCS-vsn: usr.bin/window/wwupdate.c 3.28
    SCCS-vsn: usr.bin/window/wwwrite.c 3.32
    SCCS-vsn: usr.bin/window/xx.c 3.5
    SCCS-vsn: usr.bin/window/xx.h 3.4
    SCCS-vsn: usr.bin/window/xxflush.c 3.5
    
    Former-commit-id: 069ce51097233ac3501389e17867fa9d363cb903

--- usr/src/usr.bin/window/ttwyse60.c
+++ usr/src/usr.bin/window/ttwyse60.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright 1987 by David C. Elliott, MIPS Computer Systems.  *  * U
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * David C. Elliott.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
+comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * David C. Elliott.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -19,7 +19,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)ttwyse60.c	3.2 (Berkeley) %G%"
+literal|"@(#)ttwyse60.c	3.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
