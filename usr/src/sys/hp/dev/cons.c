a24d6750ba740c7e89b485c59743aff20d7f8ae2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-06 23:36:45 -0800

    need flags arguments
    SCCS-vsn: sys/hp/dev/cons.c 7.3
    
    Former-commit-id: 029ae025e054728b0f29a7b9b9b9e53ec6dc3937

--- usr/src/sys/hp/dev/cons.c
+++ usr/src/sys/hp/dev/cons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: cons.c 1.4 88/12/03$  *  *	@(#)cons.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: cons.c 1.4 88/12/03$  *  *	@(#)cons.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -492,6 +492,8 @@ argument_list|(
 argument|dev
 argument_list|,
 argument|uio
+argument_list|,
+argument|flag
 argument_list|)
 end_macro
 
@@ -546,6 +548,8 @@ operator|(
 name|dev
 operator|,
 name|uio
+operator|,
+name|flag
 operator|)
 operator|)
 return|;
@@ -558,6 +562,8 @@ argument_list|(
 argument|dev
 argument_list|,
 argument|uio
+argument_list|,
+argument|flag
 argument_list|)
 end_macro
 
@@ -612,6 +618,8 @@ operator|(
 name|dev
 operator|,
 name|uio
+operator|,
+name|flag
 operator|)
 operator|)
 return|;
