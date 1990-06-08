9d12531560c7fddded31c5d66d458697e65a8f48
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-07 23:41:22 -0800

    damn.  TCSAFLUSH was misspelled as TCSADFLUSH and got propagated
    SCCS-vsn: sys/sys/termios.h 7.10
    
    Former-commit-id: ebf84df8f5f3dd28bf83edc877e08c626c503466

--- usr/src/sys/sys/termios.h
+++ usr/src/sys/sys/termios.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)termios.h	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)termios.h	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1145,12 +1145,23 @@ end_comment
 begin_define
 define|#
 directive|define
-name|TCSADFLUSH
+name|TCSAFLUSH
 value|2
 end_define
 
 begin_comment
 comment|/* drain output, flush input */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TCSADFLUSH
+value|TCSAFLUSH
+end_define
+
+begin_comment
+comment|/* XXX (was a typo, to be removed) */
 end_comment
 
 begin_comment
