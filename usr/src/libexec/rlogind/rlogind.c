9955a46594d91dbfa6cacccb5ae731b892961b7a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-07 17:36:35 -0800

    add FD_ZERO of ebits
    SCCS-vsn: libexec/rlogind/rlogind.c 5.44
    
    Former-commit-id: 1c29c747c4def7fbedc4cb464db42f58e6d7f7a9

--- usr/src/libexec/rlogind/rlogind.c
+++ usr/src/libexec/rlogind/rlogind.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)rlogind.c	5.43 (Berkeley) %G%"
+literal|"@(#)rlogind.c	5.44 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2053,6 +2053,12 @@ decl_stmt|,
 modifier|*
 name|omask
 decl_stmt|;
+name|FD_ZERO
+argument_list|(
+operator|&
+name|ebits
+argument_list|)
+expr_stmt|;
 name|FD_ZERO
 argument_list|(
 operator|&
