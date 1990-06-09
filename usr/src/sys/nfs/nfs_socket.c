8c065bcc64281c0b7b9ba512e8ab5b338b4f4b22
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-08 22:13:32 -0800

    new parameter to tprintf
    SCCS-vsn: sys/nfs/nfs_socket.c 7.15
    
    Former-commit-id: c6edf4f5160a2415a4e60e2b297afab247216477

--- usr/src/sys/nfs/nfs_socket.c
+++ usr/src/sys/nfs/nfs_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_socket.c	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_socket.c	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -983,8 +983,6 @@ operator|->
 name|r_procp
 operator|->
 name|p_session
-operator|->
-name|s_ttyvp
 argument_list|,
 literal|"Nfs server %s, trying reconnect\n"
 argument_list|,
@@ -1000,7 +998,7 @@ expr_stmt|;
 else|else
 name|tprintf
 argument_list|(
-name|NULLVP
+name|NULL
 argument_list|,
 literal|"Nfs server %s, trying a reconnect\n"
 argument_list|,
@@ -1084,8 +1082,6 @@ operator|->
 name|r_procp
 operator|->
 name|p_session
-operator|->
-name|s_ttyvp
 argument_list|,
 literal|"Nfs server %s, reconnected\n"
 argument_list|,
@@ -1101,7 +1097,7 @@ expr_stmt|;
 else|else
 name|tprintf
 argument_list|(
-name|NULLVP
+name|NULL
 argument_list|,
 literal|"Nfs server %s, reconnected\n"
 argument_list|,
@@ -3808,8 +3804,6 @@ operator|->
 name|r_procp
 operator|->
 name|p_session
-operator|->
-name|s_ttyvp
 argument_list|,
 literal|"Nfs server %s, is alive again\n"
 argument_list|,
@@ -3827,7 +3821,7 @@ expr_stmt|;
 else|else
 name|tprintf
 argument_list|(
-name|NULLVP
+name|NULL
 argument_list|,
 literal|"Nfs server %s, is alive again\n"
 argument_list|,
@@ -5478,8 +5472,6 @@ operator|->
 name|r_procp
 operator|->
 name|p_session
-operator|->
-name|s_ttyvp
 argument_list|,
 literal|"Nfs server %s, not responding\n"
 argument_list|,
@@ -5495,7 +5487,7 @@ expr_stmt|;
 else|else
 name|tprintf
 argument_list|(
-name|NULLVP
+name|NULL
 argument_list|,
 literal|"Nfs server %s, not responding\n"
 argument_list|,
