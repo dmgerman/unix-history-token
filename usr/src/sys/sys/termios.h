ff28143a16024b087f62fe6cd2efcbae7354022b
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-14 20:23:32 -0800

    VFLUSHO -> VDISCARD
    SCCS-vsn: sys/kern/tty.c 7.30
    SCCS-vsn: sys/sys/termios.h 7.11
    
    Former-commit-id: 21aec318c7ca12b80c74cb0ae67b12f17aad06c0

--- usr/src/sys/sys/termios.h
+++ usr/src/sys/sys/termios.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)termios.h	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)termios.h	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -234,23 +234,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|VFLUSHO
+name|VDISCARD
 value|15
 end_define
 
 begin_comment
 comment|/* IEXTEN */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|VFLUSH
-value|VFLUSHO
-end_define
-
-begin_comment
-comment|/* for sun */
 end_comment
 
 begin_endif
@@ -977,7 +966,7 @@ value|0x00000400
 end_define
 
 begin_comment
-comment|/* enable FLUSHO and LNEXT */
+comment|/* enable DISCARD and LNEXT */
 end_comment
 
 begin_define
