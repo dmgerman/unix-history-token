b0451f995859b89c446af068812a1d90d158c766
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-20 02:25:08 -0800

    make connection reset a notice, not an error
    SCCS-vsn: sbin/nfsd/nfsd.c 5.7
    
    Former-commit-id: c0bf10baa333138a90fea313bc689edf3af65a31

--- usr/src/sbin/nfsd/nfsd.c
+++ usr/src/sbin/nfsd/nfsd.c
@@ -36,7 +36,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)nfsd.c	5.6 (Berkeley) %G%"
+literal|"@(#)nfsd.c	5.7 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1451,7 +1451,7 @@ literal|0
 condition|)
 name|syslog
 argument_list|(
-name|LOG_ERR
+name|LOG_NOTICE
 argument_list|,
 literal|"Nfssvc STREAM Failed"
 argument_list|)
