4c47c505f13707fe2cb7058916a34399b0aeba55
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-20 00:11:01 -0800

    mark inode dirty to force size update
    SCCS-vsn: sys/ufs/ffs/ufs_lookup.c 7.22
    SCCS-vsn: sys/ufs/ufs/ufs_lookup.c 7.22
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.44
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 7.44
    SCCS-vsn: sys/ufs/lfs/lfs_vnops.c 7.44
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 7.44
    
    Former-commit-id: e8a36d434a81d0eda55c76e6518228067ea003b5

--- usr/src/sys/ufs/ffs/ufs_vnops.c
+++ usr/src/sys/ufs/ffs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ufs_vnops.c	7.43 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)ufs_vnops.c	7.44 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -6146,19 +6146,29 @@ name|i_fs
 operator|->
 name|fs_fsize
 condition|)
+block|{
 name|panic
 argument_list|(
 literal|"mkdir: blksize"
 argument_list|)
 expr_stmt|;
 comment|/* XXX - should grow w/balloc() */
+block|}
 else|else
+block|{
 name|ip
 operator|->
 name|i_size
 operator|=
 name|DIRBLKSIZ
 expr_stmt|;
+name|ip
+operator|->
+name|i_flag
+operator||=
+name|ICHG
+expr_stmt|;
+block|}
 comment|/* 	 * Directory all set up, now 	 * install the entry for it in 	 * the parent directory. 	 */
 name|error
 operator|=
