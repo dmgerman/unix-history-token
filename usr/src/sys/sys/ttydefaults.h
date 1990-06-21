e6271ca5d2938cd12a3cdd603e22c1379b5cfdbe
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-21 01:15:25 -0800

    CINFO -> CSTATYS
    SCCS-vsn: sys/sys/ttydefaults.h 7.4
    
    Former-commit-id: 05ae970b989557f94a2b7c861a8c86d6d9ed1a7b

--- usr/src/sys/sys/ttydefaults.h
+++ usr/src/sys/sys/ttydefaults.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)ttydefaults.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)ttydefaults.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -107,7 +107,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|CINFO
+name|CSTATUS
 value|((unsigned)'\377')
 end_define
 
@@ -295,7 +295,7 @@ name|CMIN
 block|,
 name|CTIME
 block|,
-name|CINFO
+name|CSTATUS
 block|,
 name|_POSIX_VDISABLE
 block|}
