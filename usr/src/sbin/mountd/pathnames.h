59a2852398d610bd2949834abece5658875b4347
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-21 16:08:34 -0800

    mountdtab goes in /var/run, not /var
    SCCS-vsn: sbin/mountd/pathnames.h 6.4
    
    Former-commit-id: 139742921a7a83b30aaed56771a672fc2eec6e72

--- usr/src/sbin/mountd/pathnames.h
+++ usr/src/sbin/mountd/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	6.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	6.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -20,7 +20,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_RMOUNTLIST
-value|"/var/mountdtab"
+value|"/var/run/mountdtab"
 end_define
 
 begin_define
