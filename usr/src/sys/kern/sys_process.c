f213d7c02aaeb697bc0f2b3babd8fd7c9dfef519
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-22 00:44:48 -0800

    chgprot returns error code
    SCCS-vsn: sys/kern/sys_process.c 7.9
    
    Former-commit-id: 5ff39750b4b341e083fdb96c76dfff00239cad90

--- usr/src/sys/kern/sys_process.c
+++ usr/src/sys/kern/sys_process.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sys_process.c	7.8 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sys_process.c	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -695,6 +695,7 @@ condition|)
 block|{
 if|if
 condition|(
+operator|!
 name|chgprot
 argument_list|(
 operator|(
@@ -707,6 +708,7 @@ argument_list|,
 name|RW
 argument_list|)
 operator|&&
+operator|!
 name|chgprot
 argument_list|(
 operator|(
