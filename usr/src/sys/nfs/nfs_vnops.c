aa552b0b8f491b2ba04eff5a65b2ae1126e11ab8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-21 19:15:56 -0800

    have to check for "." when releasing parent directory
    SCCS-vsn: sys/nfs/nfs_serv.c 7.25
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.43
    
    Former-commit-id: 3b3adeccfd200dacb48ac9aba8dcc8d62b24b9d1

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_vnops.c	7.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_vnops.c	7.43 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -5047,6 +5047,24 @@ argument_list|(
 name|tndp
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|tndp
+operator|->
+name|ni_dvp
+operator|==
+name|tndp
+operator|->
+name|ni_vp
+condition|)
+name|vrele
+argument_list|(
+name|tndp
+operator|->
+name|ni_dvp
+argument_list|)
+expr_stmt|;
+else|else
 name|vput
 argument_list|(
 name|tndp
