56950c99f67abc6b60d8a5bdfc59b4447bdd8293
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-22 23:21:31 -0800

    add reference
    SCCS-vsn: sys/nfs/nfs_srvcache.c 7.8
    
    Former-commit-id: 0ef7542a4dd0aa343bd29dab1e05c4d82b1c4f49

--- usr/src/sys/nfs/nfs_srvcache.c
+++ usr/src/sys/nfs/nfs_srvcache.c
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_srvcache.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)nfs_srvcache.c	7.8 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/*  * Reference: Chet Juszczak, "Improving the Performance and Correctness  *            of an NFS Server", in Proc. Winter 1989 USENIX Conference,  *            pages 53-63. San Diego, February 1989.  */
 end_comment
 
 begin_include
