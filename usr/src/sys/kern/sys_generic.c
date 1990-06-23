84d3b3042ba0876ae2b7ecee094216281a280600
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-23 00:19:26 -0800

    have to pass error to ktrace
    SCCS-vsn: sys/kern/sys_generic.c 7.20
    
    Former-commit-id: 278f41d9400737ca924683f9ebb4701c5ef09c62

--- usr/src/sys/kern/sys_generic.c
+++ usr/src/sys/kern/sys_generic.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_generic.c	7.19 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_generic.c	7.20 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -370,6 +370,8 @@ operator|&
 name|ktriov
 argument_list|,
 name|cnt
+argument_list|,
+name|error
 argument_list|)
 expr_stmt|;
 endif|#
@@ -859,6 +861,8 @@ argument_list|,
 name|ktriov
 argument_list|,
 name|cnt
+argument_list|,
+name|error
 argument_list|)
 expr_stmt|;
 name|FREE
@@ -1206,6 +1210,8 @@ operator|&
 name|ktriov
 argument_list|,
 name|cnt
+argument_list|,
+name|error
 argument_list|)
 expr_stmt|;
 endif|#
@@ -1710,6 +1716,8 @@ argument_list|,
 name|ktriov
 argument_list|,
 name|cnt
+argument_list|,
+name|error
 argument_list|)
 expr_stmt|;
 name|FREE
