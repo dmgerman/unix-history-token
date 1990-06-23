08c7ca8395da3dfc9ebb9bea73c7c6ac809b5d21
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1990-06-22 19:00:02 -0800

    cleanup
    SCCS-vsn: sys/hp300/dev/dcmreg.h 7.3
    
    Former-commit-id: 5f5ad42565c68a73ae4f7acbfe4d79afbabca050

--- usr/src/sys/hp300/dev/dcmreg.h
+++ usr/src/sys/hp300/dev/dcmreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: dcmreg.h 1.3 89/08/23$  *  *	@(#)dcmreg.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: dcmreg.h 1.3 89/08/23$  *  *	@(#)dcmreg.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_struct
@@ -1069,15 +1069,8 @@ value|0xff
 end_define
 
 begin_comment
-comment|/*  * WARNING: Serial console is assumed to be at SC13  * and CONUNIT must be 1, signaled by REMOTE/LOCAL switch on card  */
+comment|/*  * WARNING: Serial console is assumed to be the lowest select-code card  * and that card must be logical unit 0 in the kernel.  Also, CONUNIT must  * be 1, the port affected by the REMOTE/LOCAL switch.  */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|CONADDR
-value|((struct dcmdevice *)(IOV(EXTIOBASE + (13 * IOCARDSIZE))))
-end_define
 
 begin_define
 define|#
