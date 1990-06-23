6e7eb5b5ace95875813572c89130bf343d106986
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-06-23 00:17:19 -0800

    remove PABORT
    SCCS-vsn: sys/sys/param.h 7.15
    
    Former-commit-id: b954c3e3a8e8fa8f93edfbd83597ae6cd6cdff48

--- usr/src/sys/sys/param.h
+++ usr/src/sys/sys/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)param.h	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)param.h	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -216,17 +216,6 @@ end_define
 
 begin_comment
 comment|/* or'd with pri for tsleep to check signals */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|PABORT
-value|0x200
-end_define
-
-begin_comment
-comment|/* or'd with pri for tsleep to longjmp XXX */
 end_comment
 
 begin_define
