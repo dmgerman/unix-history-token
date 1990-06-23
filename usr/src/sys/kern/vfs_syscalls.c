a68b6b972f4ef02902f8d88a1aa7b7846adfe0d1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-23 00:20:54 -0800

    p_devtmp => p_dupfd; eliminate u.u_error from RETURN macro
    SCCS-vsn: sys/kern/vfs_syscalls.c 7.54
    
    Former-commit-id: 88b25d9eadd4ee5e91a578f11f3f03222d0c9c3b

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_syscalls.c	7.53 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vfs_syscalls.c	7.54 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -69,13 +69,6 @@ directive|include
 file|"malloc.h"
 end_include
 
-begin_define
-define|#
-directive|define
-name|p_devtmp
-value|p_logname[11]
-end_define
-
 begin_undef
 undef|#
 directive|undef
@@ -89,7 +82,7 @@ name|RETURN
 parameter_list|(
 name|val
 parameter_list|)
-value|{ u.u_error = (val); if (u.u_spare[0] != 0) panic("lock count"); return (u.u_error); }
+value|{if (u.u_spare[0] != 0) panic("lock count"); return (val);}
 end_define
 
 begin_comment
@@ -2789,7 +2782,7 @@ name|fname
 expr_stmt|;
 name|p
 operator|->
-name|p_devtmp
+name|p_dupfd
 operator|=
 operator|-
 literal|1
@@ -2830,7 +2823,7 @@ operator|&&
 comment|/* XXX from fdopen */
 name|p
 operator|->
-name|p_devtmp
+name|p_dupfd
 operator|>=
 literal|0
 operator|&&
@@ -2843,7 +2836,7 @@ name|indx
 argument_list|,
 name|p
 operator|->
-name|p_devtmp
+name|p_dupfd
 argument_list|,
 name|fmode
 argument_list|)
