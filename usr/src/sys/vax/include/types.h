cfc0c7aef867fbb110488463708cf269ec6a6bff
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-25 07:49:39 -0800

    sys/types.h includes this, so can't export anything but *_t to POSIX
    SCCS-vsn: sys/vax/include/types.h 7.4
    SCCS-vsn: sys/hp300/include/types.h 7.4
    SCCS-vsn: sys/i386/include/types.h 7.4
    
    Former-commit-id: d20aabc21cf270d832ab28c30e180a74758ee970

--- usr/src/sys/vax/include/types.h
+++ usr/src/sys/vax/include/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -85,11 +85,21 @@ begin_comment
 comment|/* wchar_t */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
 name|_ANSI_SOURCE
-end_ifndef
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|_POSIX_SOURCE
+argument_list|)
+end_if
 
 begin_typedef
 typedef|typedef
