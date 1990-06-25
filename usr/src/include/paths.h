42461810a32dc2c913d8f360811ad6e5fa51ca08
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-24 18:33:15 -0800

    add _PATH_MAILDIR
    SCCS-vsn: include/paths.h 5.7
    
    Former-commit-id: 224328bff430b6eb8f5fbb1e1a2fda555913bdbc

--- usr/src/include/paths.h
+++ usr/src/include/paths.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)paths.h	5.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)paths.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -50,6 +50,13 @@ define|#
 directive|define
 name|_PATH_KMEM
 value|"/dev/kmem"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_MAILDIR
+value|"/var/mail"
 end_define
 
 begin_define
