0ac4360a0ab02b2ca6ce0acce212c8f463b7e5e4
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-06-24 23:02:15 -0800

    use kernel MAXLOGNAME instead of UT_NAMESIZE (kernel's is bigger)
    SCCS-vsn: lib/libc/gen/getlogin.c 5.8
    
    Former-commit-id: 660520b100e35c80632bc4cb29330e23cf16d410

--- usr/src/lib/libc/gen/getlogin.c
+++ usr/src/lib/libc/gen/getlogin.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)getlogin.c	5.7 (Berkeley) %G%"
+literal|"@(#)getlogin.c	5.8 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -40,7 +40,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<sys/types.h>
+file|<sys/param.h>
 end_include
 
 begin_include
@@ -81,7 +81,7 @@ specifier|static
 name|char
 name|logname
 index|[
-name|UT_NAMESIZE
+name|MAXLOGNAME
 operator|+
 literal|1
 index|]
