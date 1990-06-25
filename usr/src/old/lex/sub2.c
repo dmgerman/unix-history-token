0bcbdf5963169c3fabdd6a91f2b57141c8421598
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-24 19:42:14 -0800

    cfree -> free
    SCCS-vsn: old/lex/sub2.c 4.3
    
    Former-commit-id: 4e7489d0c44722296f0cc2743e51750fd5dbabb4

--- usr/src/old/lex/sub2.c
+++ usr/src/old/lex/sub2.c
@@ -11,7 +11,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)sub2.c	4.2 (Berkeley) %G%"
+literal|"@(#)sub2.c	4.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -5739,12 +5739,6 @@ block|}
 name|cfree
 argument_list|(
 name|fbarr
-argument_list|,
-literal|2
-operator|*
-name|NCH
-argument_list|,
-literal|1
 argument_list|)
 expr_stmt|;
 block|}
