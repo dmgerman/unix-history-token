d3f9cbdcbd8790689072fbb946342eb98e44e7c5
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-06-25 18:00:17 -0800

    copy SS_ASYNC to sockbuf
    SCCS-vsn: sys/kern/sys_socket.c 7.7
    
    Former-commit-id: 23969d654731aa421f5a18e65f9a82f6164f3398

--- usr/src/sys/kern/sys_socket.c
+++ usr/src/sys/kern/sys_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_socket.c	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sys_socket.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -391,13 +391,32 @@ operator|*
 operator|)
 name|data
 condition|)
+block|{
 name|so
 operator|->
 name|so_state
 operator||=
 name|SS_ASYNC
 expr_stmt|;
+name|so
+operator|->
+name|so_rcv
+operator|.
+name|sb_flags
+operator||=
+name|SB_ASYNC
+expr_stmt|;
+name|so
+operator|->
+name|so_snd
+operator|.
+name|sb_flags
+operator||=
+name|SB_ASYNC
+expr_stmt|;
+block|}
 else|else
+block|{
 name|so
 operator|->
 name|so_state
@@ -405,6 +424,25 @@ operator|&=
 operator|~
 name|SS_ASYNC
 expr_stmt|;
+name|so
+operator|->
+name|so_rcv
+operator|.
+name|sb_flags
+operator|&=
+operator|~
+name|SB_ASYNC
+expr_stmt|;
+name|so
+operator|->
+name|so_snd
+operator|.
+name|sb_flags
+operator|&=
+operator|~
+name|SB_ASYNC
+expr_stmt|;
+block|}
 return|return
 operator|(
 literal|0
