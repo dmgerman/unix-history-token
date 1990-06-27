a80a12bf1f6525057b6b41f9211ef1961feb1327
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-26 21:22:27 -0800

    new copyright, STDC prototypes, new flags
    SCCS-vsn: include/vis.h 5.4
    
    Former-commit-id: b5b9a76be8e40fd6401598a5f6d48a29c127ad85

--- usr/src/include/vis.h
+++ usr/src/include/vis.h
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)vis.h	5.3 (Berkeley) %G%  */
-end_comment
-
-begin_comment
-comment|/*  * vis flags  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vis.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -30,18 +26,29 @@ value|0x02
 end_define
 
 begin_comment
-comment|/* use \[nrft...] where appropiate */
+comment|/* use \[nrft0..] where appropiate */
 end_comment
 
 begin_comment
-comment|/*  * to alter set of characters encoded  * (default is to encode all non-graphic except space, tab, and newline)  */
+comment|/*  * to alter set of characters encoded (default is to encode all  * non-graphic except space, tab, and newline).  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VIS_SP
+value|0x04
+end_define
+
+begin_comment
+comment|/* also encode space */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|VIS_TAB
-value|0x04
+value|0x08
 end_define
 
 begin_comment
@@ -51,19 +58,26 @@ end_comment
 begin_define
 define|#
 directive|define
-name|VIS_WHITE
-value|0x08
+name|VIS_NL
+value|0x10
 end_define
 
 begin_comment
-comment|/* also encode space, tab, and newline */
+comment|/* also encode newline */
 end_comment
 
 begin_define
 define|#
 directive|define
+name|VIS_WHITE
+value|(VIS_SP | VIS_TAB | VIS_NL)
+end_define
+
+begin_define
+define|#
+directive|define
 name|VIS_SAFE
-value|0x10
+value|0x20
 end_define
 
 begin_comment
@@ -78,94 +92,37 @@ begin_define
 define|#
 directive|define
 name|VIS_NOSLASH
-value|0x20
+value|0x40
 end_define
 
 begin_comment
 comment|/* inhibit printing '\' */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|VIS_NEXTC
-value|0x40
-end_define
-
 begin_comment
-comment|/* nextc character is valid */
-end_comment
-
-begin_function_decl
-specifier|extern
-name|char
-modifier|*
-name|vis
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_comment
-comment|/*  * unvis flags  */
+comment|/*  * unvis return codes  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|UNVIS_HAT
-value|0x01
-end_define
-
-begin_comment
-comment|/* treat ^ as special */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|UNVIS_END
-value|0x02
-end_define
-
-begin_comment
-comment|/* reset decode machine */
-end_comment
-
-begin_comment
-comment|/*  * return codes  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|UNVIS_NEEDMORE
-value|0
-end_define
-
-begin_comment
-comment|/* not done, feed another char */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|UNVIS_OK
+name|UNVIS_VALID
 value|1
 end_define
 
 begin_comment
-comment|/* done, character ready */
+comment|/* character valid */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|UNVIS_OKPUSH
+name|UNVIS_VALIDPUSH
 value|2
 end_define
 
 begin_comment
-comment|/* done, push back passed char */
+comment|/* character valid, push back passed char */
 end_comment
 
 begin_define
@@ -176,7 +133,7 @@ value|3
 end_define
 
 begin_comment
-comment|/* done, no character ready (hidden newline) */
+comment|/* valid sequence, no character produced */
 end_comment
 
 begin_define
@@ -189,6 +146,123 @@ end_define
 begin_comment
 comment|/* unrecognized escape sequence */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|UNVIS_ERROR
+value|-2
+end_define
+
+begin_comment
+comment|/* decoder in unknown state (unrecoverable) */
+end_comment
+
+begin_comment
+comment|/*  * unvis flags  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|UNVIS_END
+value|1
+end_define
+
+begin_comment
+comment|/* no more characters */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|__STDC__
+operator|||
+name|c_plusplus
+end_if
+
+begin_function_decl
+name|char
+modifier|*
+name|vis
+parameter_list|(
+name|char
+modifier|*
+parameter_list|,
+name|char
+parameter_list|,
+name|char
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|strvis
+parameter_list|(
+name|char
+modifier|*
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|strvisx
+parameter_list|(
+name|char
+modifier|*
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+parameter_list|,
+name|size_t
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_function_decl
+name|char
+modifier|*
+name|vis
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|strvis
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|strvisx
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
