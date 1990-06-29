39ca11476582d36e83783db3ada64f1fe62cb93b
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-06-28 21:57:07 -0800

    protocol notify routines set errors, so can be soft
    SCCS-vsn: sys/netinet/in_pcb.c 7.12
    
    Former-commit-id: 3850c59fd5a10b3ddc65902733c5e5630f47b10f

--- usr/src/sys/netinet/in_pcb.c
+++ usr/src/sys/netinet/in_pcb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)in_pcb.c	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)in_pcb.c	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1776,18 +1776,6 @@ name|inp_next
 expr_stmt|;
 continue|continue;
 block|}
-if|if
-condition|(
-name|errno
-condition|)
-name|inp
-operator|->
-name|inp_socket
-operator|->
-name|so_error
-operator|=
-name|errno
-expr_stmt|;
 name|oinp
 operator|=
 name|inp
@@ -1808,6 +1796,8 @@ name|notify
 call|)
 argument_list|(
 name|oinp
+argument_list|,
+name|errno
 argument_list|)
 expr_stmt|;
 block|}
