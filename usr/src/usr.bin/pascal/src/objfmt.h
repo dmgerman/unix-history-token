061b1bcd7a5edc0a20fcfe91768d99d439462f55
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-29 14:49:51 -0800

    update size of px_header
    SCCS-vsn: usr.bin/pascal/src/objfmt.h 5.3
    
    Former-commit-id: 07d61b239f1af6c94e32d5de24961b53b383ddcd

--- usr/src/usr.bin/pascal/src/objfmt.h
+++ usr/src/usr.bin/pascal/src/objfmt.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)objfmt.h	5.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)objfmt.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -575,7 +575,7 @@ begin_define
 define|#
 directive|define
 name|HEADER_BYTES
-value|2048
+value|2560
 end_define
 
 begin_comment
