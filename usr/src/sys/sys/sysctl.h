e41e1952e6fdcb2a8105b790a397be36d66fd09f
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-28 22:23:45 -0800

    (no message)
    SCCS-vsn: sys/sys/sysctl.h 7.3
    
    Former-commit-id: d4aafc46cf4fb685fa46cb218cef14d720673fcf

--- usr/src/sys/sys/sysctl.h
+++ usr/src/sys/sys/sysctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sysctl.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)sysctl.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -116,97 +116,6 @@ comment|/* by real uid */
 end_comment
 
 begin_comment
-comment|/*   * proc ops return arrays of augmented proc structures  */
-end_comment
-
-begin_struct
-struct|struct
-name|kinfo_proc
-block|{
-name|struct
-name|proc
-name|kp_proc
-decl_stmt|;
-comment|/* proc structure */
-struct|struct
-name|eproc
-block|{
-name|struct
-name|proc
-modifier|*
-name|e_paddr
-decl_stmt|;
-comment|/* address of proc */
-name|struct
-name|session
-modifier|*
-name|e_sess
-decl_stmt|;
-comment|/* session pointer */
-name|pid_t
-name|e_pgid
-decl_stmt|;
-comment|/* process group id */
-name|short
-name|e_jobc
-decl_stmt|;
-comment|/* job control counter */
-name|dev_t
-name|e_tdev
-decl_stmt|;
-comment|/* controlling tty */
-name|pid_t
-name|e_tpgid
-decl_stmt|;
-comment|/* tty process group id */
-name|struct
-name|session
-modifier|*
-name|e_tsess
-decl_stmt|;
-comment|/* tty session pointer */
-define|#
-directive|define
-name|WMESGLEN
-value|7
-name|char
-name|e_wmesg
-index|[
-name|WMESGLEN
-operator|+
-literal|1
-index|]
-decl_stmt|;
-comment|/* wchan message */
-name|size_t
-name|e_xsize
-decl_stmt|;
-comment|/* text size */
-name|short
-name|e_xrssize
-decl_stmt|;
-comment|/* text rss */
-name|short
-name|e_xccount
-decl_stmt|;
-comment|/* text references */
-name|short
-name|e_xswrss
-decl_stmt|;
-name|long
-name|e_spare
-index|[
-literal|8
-index|]
-decl_stmt|;
-block|}
-name|kp_eproc
-struct|;
-block|}
-struct|;
-end_struct
-
-begin_comment
 comment|/*  * Routing table  */
 end_comment
 
@@ -248,6 +157,17 @@ end_define
 begin_comment
 comment|/* by flags, e.g. RESOLVING */
 end_comment
+
+begin_comment
+comment|/*  * vnodes  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|KINFO_VNODE
+value|(2<<8)
+end_define
 
 begin_comment
 comment|/*  * Locking and stats  */
