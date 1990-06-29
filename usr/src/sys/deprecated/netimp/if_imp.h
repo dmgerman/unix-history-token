79217031f990092c10466ecb2439439131b58a21
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-06-29 00:31:07 -0800

    move link numbers from <netinet/in.h>
    SCCS-vsn: sys/deprecated/netimp/if_imp.h 7.6
    
    Former-commit-id: 50745eab0cd2e6b3e10b47fcaecf73c09e467125

--- usr/src/sys/deprecated/netimp/if_imp.h
+++ usr/src/sys/deprecated/netimp/if_imp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)if_imp.h	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	@(#)if_imp.h	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -352,6 +352,31 @@ end_define
 begin_comment
 comment|/* ready for next message */
 end_comment
+
+begin_comment
+comment|/*  * Link numbers  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IMPLINK_IP
+value|155
+end_define
+
+begin_define
+define|#
+directive|define
+name|IMPLINK_LOWEXPER
+value|156
+end_define
+
+begin_define
+define|#
+directive|define
+name|IMPLINK_HIGHEXPER
+value|158
+end_define
 
 begin_comment
 comment|/*  * IMPTYPE_DOWN subtypes, in link number field.  */
