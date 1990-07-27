3295269a40c74b961cb92305485e1317db087a1a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-07-27 02:14:08 -0800

    NFS_MAXDATA changed, and this can confuse Ultrix 2.2 clients from Rick Macklem
    SCCS-vsn: sys/nfs/nfs_serv.c 7.27
    
    Former-commit-id: fe16c387f18e2b2b3679f42877948bf67238d0e3

--- usr/src/sys/nfs/nfs_serv.c
+++ usr/src/sys/nfs/nfs_serv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_serv.c	7.26 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_serv.c	7.27 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -7344,7 +7344,7 @@ name|sf_tsize
 operator|=
 name|txdr_unsigned
 argument_list|(
-name|NFS_MAXDATA
+name|NFS_MAXDGRAMDATA
 argument_list|)
 expr_stmt|;
 name|sfp
