82b5617c58cdefccffdcb8722044708072c69e17
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-07-28 05:43:02 -0800

    (by karels) typo in TIOCSETAF, was always flushing; can't use 0 for no pgrp; ttyrub newline could maybe happen???
    SCCS-vsn: sys/kern/tty.c 7.35
    
    Former-commit-id: 7d34c7b06f49041f4b840b75b0919c5106b105af

--- usr/src/sys/kern/tty.c
+++ usr/src/sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)tty.c	7.34 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)tty.c	7.35 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2558,7 +2558,7 @@ name|t_pgrp
 operator|->
 name|pg_id
 else|:
-literal|0
+name|NO_PID
 expr_stmt|;
 break|break;
 case|case
@@ -7372,6 +7372,10 @@ case|:
 case|case
 name|RETURN
 case|:
+case|case
+name|NEWLINE
+case|:
+comment|/* XXX can't happen ? */
 if|if
 condition|(
 name|tp
