93bd275f404574e2d94fd9c692fa9e9e421770b3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-08-22 15:30:19 -0800

    use vn_open on accounting file to ensure that quotas get set up (from Torek)
    SCCS-vsn: sys/kern/kern_acct.c 7.14
    
    Former-commit-id: 990026cd5b76eb092991548633738cd6a14c820f

--- usr/src/sys/kern/kern_acct.c
+++ usr/src/sys/kern/kern_acct.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_acct.c	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_acct.c	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -67,6 +67,12 @@ begin_include
 include|#
 directive|include
 file|"kernel.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"file.h"
 end_include
 
 begin_include
@@ -312,14 +318,6 @@ return|;
 block|}
 name|ndp
 operator|->
-name|ni_nameiop
-operator|=
-name|LOOKUP
-operator||
-name|FOLLOW
-expr_stmt|;
-name|ndp
-operator|->
 name|ni_segflg
 operator|=
 name|UIO_USERSPACE
@@ -336,9 +334,13 @@ if|if
 condition|(
 name|error
 operator|=
-name|namei
+name|vn_open
 argument_list|(
 name|ndp
+argument_list|,
+name|FWRITE
+argument_list|,
+literal|0644
 argument_list|)
 condition|)
 return|return
@@ -369,28 +371,6 @@ expr_stmt|;
 return|return
 operator|(
 name|EACCES
-operator|)
-return|;
-block|}
-if|if
-condition|(
-name|vp
-operator|->
-name|v_mount
-operator|->
-name|mnt_flag
-operator|&
-name|MNT_RDONLY
-condition|)
-block|{
-name|vrele
-argument_list|(
-name|vp
-argument_list|)
-expr_stmt|;
-return|return
-operator|(
-name|EROFS
 operator|)
 return|;
 block|}
