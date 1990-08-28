b702987f88bf57e21ba9f5ee670ea5f9e5341039
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-08-28 03:31:28 -0800

    rewrite to look like times()
    SCCS-vsn: lib/libc/gen/clock.c 5.4
    
    Former-commit-id: e524119015cd65f8e141b36794a5056ef18b70f8

--- usr/src/lib/libc/gen/clock.c
+++ usr/src/lib/libc/gen/clock.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)clock.c	5.3 (Berkeley) %G%"
+literal|"@(#)clock.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -40,13 +40,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<machine/machlimits.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/types.h>
+file|<sys/param.h>
 end_include
 
 begin_include
@@ -61,6 +55,20 @@ directive|include
 file|<sys/resource.h>
 end_include
 
+begin_comment
+comment|/*  * Convert usec to clock ticks; could do (usec * CLK_TCK) / 1000000,  * but this would overflow if we switch to nanosec.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CONVTCK
+parameter_list|(
+name|r
+parameter_list|)
+value|(r.tv_sec * CLK_TCK + r.tv_usec / (1000000 / CLK_TCK))
+end_define
+
 begin_function
 name|clock_t
 name|clock
@@ -68,10 +76,7 @@ parameter_list|()
 block|{
 name|struct
 name|rusage
-name|rusage
-decl_stmt|;
-name|clock_t
-name|val
+name|ru
 decl_stmt|;
 if|if
 condition|(
@@ -80,7 +85,7 @@ argument_list|(
 name|RUSAGE_SELF
 argument_list|,
 operator|&
-name|rusage
+name|ru
 argument_list|)
 condition|)
 return|return
@@ -92,50 +97,28 @@ operator|-
 literal|1
 operator|)
 return|;
-name|val
-operator|=
-operator|(
-name|rusage
-operator|.
-name|ru_utime
-operator|.
-name|tv_sec
-operator|+
-name|rusage
-operator|.
-name|ru_stime
-operator|.
-name|tv_sec
-operator|)
-operator|*
-name|CLK_TCK
-expr_stmt|;
-comment|/* 	 * Convert usec to clock ticks; could do (usec * CLK_TCK) / 1000000, 	 * but this would overflow if we switch to nanosec. 	 */
-name|val
-operator|+=
-operator|(
-name|rusage
-operator|.
-name|ru_utime
-operator|.
-name|tv_usec
-operator|+
-name|rusage
-operator|.
-name|ru_stime
-operator|.
-name|tv_usec
-operator|)
-operator|/
-operator|(
-literal|1000000
-operator|/
-name|CLK_TCK
-operator|)
-expr_stmt|;
 return|return
 operator|(
-name|val
+call|(
+name|clock_t
+call|)
+argument_list|(
+operator|(
+name|CONVTCK
+argument_list|(
+name|ru
+operator|.
+name|ru_utime
+argument_list|)
+operator|+
+name|CONVTCK
+argument_list|(
+name|ru
+operator|.
+name|ru_stime
+argument_list|)
+operator|)
+argument_list|)
 operator|)
 return|;
 block|}
