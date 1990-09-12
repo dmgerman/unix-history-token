dfcdf7c0f90b541878334dc6000fd166ab80dbc8
Paul Borman <borman@ucbvax.Berkeley.EDU>
1990-09-11 22:53:50 -0800

    Fix for TIOCSIG: pgsignal() was being called with one too few arguments.  Add special case for SIGINFO to call ttyinfo().
    SCCS-vsn: sys/kern/tty_pty.c 7.16
    
    Former-commit-id: aa76b2ad984776c811a84ab691891539dcd88e6a

--- usr/src/sys/kern/tty_pty.c
+++ usr/src/sys/kern/tty_pty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tty_pty.c	7.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tty_pty.c	7.16 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -4015,6 +4015,39 @@ name|int
 operator|*
 operator|)
 name|data
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|(
+operator|*
+operator|(
+name|unsigned
+name|int
+operator|*
+operator|)
+name|data
+operator|==
+name|SIGINFO
+operator|)
+operator|&&
+operator|(
+operator|(
+name|tp
+operator|->
+name|t_lflag
+operator|&
+name|NOKERNINFO
+operator|)
+operator|==
+literal|0
+operator|)
+condition|)
+name|ttyinfo
+argument_list|(
+name|tp
 argument_list|)
 expr_stmt|;
 return|return
