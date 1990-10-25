b27139378fe0cd83c916cbbb6f83474ebf4a6a1e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-10-24 19:37:56 -0800

    typo in comment
    SCCS-vsn: sys/ufs/ffs/inode.h 7.14
    SCCS-vsn: sys/ufs/ufs/inode.h 7.14
    
    Former-commit-id: c4211308bebcc3779780c4e9ff6417e9edda21e5

--- usr/src/sys/ufs/ffs/inode.h
+++ usr/src/sys/ufs/ffs/inode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)inode.h	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)inode.h	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -86,7 +86,7 @@ index|[
 name|MAXQUOTAS
 index|]
 decl_stmt|;
-comment|/* pointer to dquot strauctures */
+comment|/* pointer to dquot structures */
 name|long
 name|i_diroff
 decl_stmt|;
