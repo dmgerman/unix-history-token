fb27bc33697b971af49dd1f00514efb0de34bd47
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-11-15 20:23:55 -0800

    make status variables consistent
    SCCS-vsn: usr.bin/find/find.h 5.2
    
    Former-commit-id: 404e7b2a9119afe8fda70de4c2d8270f2b74e43a

--- usr/src/usr.bin/find/find.h
+++ usr/src/usr.bin/find/find.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Cimarron D. Taylor of the University of California, Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)find.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Cimarron D. Taylor of the University of California, Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)find.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -383,16 +383,22 @@ end_define
 begin_decl_stmt
 specifier|extern
 name|int
-name|errno
+name|ftsoptions
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
 specifier|extern
 name|int
-name|ftsoptions
+name|isdeprecated
 decl_stmt|,
-name|output_specified
+name|isdepth
+decl_stmt|,
+name|isoutput
+decl_stmt|,
+name|isrelative
+decl_stmt|,
+name|isstopdnx
 decl_stmt|;
 end_decl_stmt
 
