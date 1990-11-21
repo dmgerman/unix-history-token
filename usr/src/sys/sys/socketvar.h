3fa11a2ad2b8ac5fc1f8b55409e5317657587a9f
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-11-20 22:06:40 -0800

    almost forgot about this: Aug. fix from torek, wrong parenthesization in sblock
    SCCS-vsn: sys/sys/socketvar.h 7.12
    
    Former-commit-id: fc7df91892e9b05dacea61e13c321fae7504d1e3

--- usr/src/sys/sys/socketvar.h
+++ usr/src/sys/sys/socketvar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)socketvar.h	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)socketvar.h	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -417,7 +417,7 @@ name|sblock
 parameter_list|(
 name|sb
 parameter_list|)
-value|((sb)->sb_flags& SB_LOCK ? sb_lock(sb) : \ 		((sb)->sb_flags |= SB_LOCK), 0)
+value|((sb)->sb_flags& SB_LOCK ? sb_lock(sb) : \ 		((sb)->sb_flags |= SB_LOCK, 0))
 end_define
 
 begin_comment
