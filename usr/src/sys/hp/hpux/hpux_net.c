abf4b259159990dd61801561758c966d55dc842f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-12-17 06:16:15 -0800

    fix include paths
    SCCS-vsn: sys/hp/hpux/hpux_net.c 7.6
    SCCS-vsn: sys/hp/hpux/hpux_sig.c 7.7
    
    Former-commit-id: 0a975ac7d25e07f5193863a66c9dc466007ecbe3

--- usr/src/sys/hp/hpux/hpux_net.c
+++ usr/src/sys/hp/hpux/hpux_net.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hpux_net.c 1.33 89/08/23$  *  *	@(#)hpux_net.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hpux_net.c 1.33 89/08/23$  *  *	@(#)hpux_net.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -88,7 +88,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"sys/hpux.h"
+file|"hpux.h"
 end_include
 
 begin_define
