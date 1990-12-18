cbdd0de2dff58501f4e99b7eacfdb1da77bff21d
William Nesheim <william@ucbvax.Berkeley.EDU>
1990-12-18 00:58:45 -0800

    386 byte sex; really should be ENDIAN et al someday...
    SCCS-vsn: include/mp.h 5.2
    
    Former-commit-id: dde1b45f1e35adcc62d0065bb35e06c7aac7f850

--- usr/src/include/mp.h
+++ usr/src/include/mp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)mp.h	5.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)mp.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -89,11 +89,21 @@ endif|#
 directive|endif
 end_endif
 
-begin_ifndef
-ifndef|#
-directive|ifndef
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
 name|vax
-end_ifndef
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|i386
+argument_list|)
+end_if
 
 begin_struct
 struct|struct
