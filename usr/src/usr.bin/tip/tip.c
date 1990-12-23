64b587ef544083b2d6b20c22e13af6852c88cf26
Thomas Ferrin <tef@ucbvax.Berkeley.EDU>
1990-12-22 21:44:09 -0800

    Fix error message when attempting to set unknown parity
    SCCS-vsn: usr.bin/tip/tip.c 5.14
    
    Former-commit-id: 7528ab494ce365afe76d080c7459c1b42b0f4478

--- usr/src/usr.bin/tip/tip.c
+++ usr/src/usr.bin/tip/tip.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)tip.c	5.13 (Berkeley) %G%"
+literal|"@(#)tip.c	5.14 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2719,9 +2719,9 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"%s: unknown parity value\n"
+literal|"%s: unknown parity value\r\n"
 argument_list|,
-name|PA
+name|parity
 argument_list|)
 expr_stmt|;
 name|fflush
