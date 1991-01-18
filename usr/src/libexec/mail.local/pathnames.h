cc2580b4ed8865737e180e194136245f44b71b75
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-17 22:39:32 -0800

    from scratch -- /bin/mail was a security problem, and not worth fixing
    SCCS-vsn: libexec/mail.local/Makefile 5.3
    SCCS-vsn: libexec/mail.local/mail.local.8 6.7
    SCCS-vsn: libexec/mail.local/mail.local.c 4.38
    SCCS-vsn: libexec/mail.local/pathnames.h 5.3
    
    Former-commit-id: 6ccd96c36bef5c486e86af1377a173e02276a625

--- usr/src/libexec/mail.local/pathnames.h
+++ usr/src/libexec/mail.local/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -9,17 +9,11 @@ directive|include
 file|<paths.h>
 end_include
 
-begin_undef
-undef|#
-directive|undef
-name|_PATH_TMP
-end_undef
-
 begin_define
 define|#
 directive|define
-name|_PATH_TMP
-value|"/tmp/maXXXXX"
+name|_PATH_LOCTMP
+value|"/tmp/local.XXXXXX"
 end_define
 
 end_unit
