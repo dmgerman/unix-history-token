1d39157a2552f60c86c24e4f12c9e419284d8ddf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-18 02:23:04 -0800

    new version
    SCCS-vsn: usr.bin/ar/ar.c 5.6
    SCCS-vsn: include/ar.h 5.2
    
    Former-commit-id: 8f39408e074dd39ccf59077e445ecd398d2fea98

--- usr/src/include/ar.h
+++ usr/src/include/ar.h
@@ -1,7 +1,25 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)ar.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Hugh Smith at The University of Guelph.  *  *	@(#)ar.h	5.2 (Berkeley) %G%  */
 end_comment
+
+begin_comment
+comment|/* Pre-4BSD archives had these magic numbers in them. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|OARMAG1
+value|0177555
+end_define
+
+begin_define
+define|#
+directive|define
+name|OARMAG2
+value|0177545
+end_define
 
 begin_define
 define|#
@@ -10,6 +28,10 @@ name|ARMAG
 value|"!<arch>\n"
 end_define
 
+begin_comment
+comment|/* ar "magic number" */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -17,12 +39,20 @@ name|SARMAG
 value|8
 end_define
 
+begin_comment
+comment|/* strlen(ARMAG); */
+end_comment
+
 begin_define
 define|#
 directive|define
-name|ARFMAG
-value|"`\n"
+name|AR_EFMT1
+value|"#1/"
 end_define
+
+begin_comment
+comment|/* extended format #1 */
+end_comment
 
 begin_struct
 struct|struct
@@ -34,42 +64,53 @@ index|[
 literal|16
 index|]
 decl_stmt|;
+comment|/* name */
 name|char
 name|ar_date
 index|[
 literal|12
 index|]
 decl_stmt|;
+comment|/* modification time */
 name|char
 name|ar_uid
 index|[
 literal|6
 index|]
 decl_stmt|;
+comment|/* user id */
 name|char
 name|ar_gid
 index|[
 literal|6
 index|]
 decl_stmt|;
+comment|/* group id */
 name|char
 name|ar_mode
 index|[
 literal|8
 index|]
 decl_stmt|;
+comment|/* octal file permissions */
 name|char
 name|ar_size
 index|[
 literal|10
 index|]
 decl_stmt|;
+comment|/* size in bytes */
+define|#
+directive|define
+name|ARFMAG
+value|"`\n"
 name|char
 name|ar_fmag
 index|[
 literal|2
 index|]
 decl_stmt|;
+comment|/* consistency check */
 block|}
 struct|;
 end_struct
