a073bc93061e9bb17f33debcf923a2081a2471f9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-17 21:54:07 -0800

    kernel reorg, make strmode take an int.
    SCCS-vsn: include/string.h 5.5
    
    Former-commit-id: 23023f34ff5c63deb7b3106c7f33bcff464d30c4

--- usr/src/include/string.h
+++ usr/src/include/string.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)string.h	5.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)string.h	5.5 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -18,7 +18,7 @@ end_define
 begin_include
 include|#
 directive|include
-file|<machine/machtypes.h>
+file|<machine/types.h>
 end_include
 
 begin_ifdef
@@ -734,7 +734,7 @@ begin_function_decl
 name|void
 name|strmode
 parameter_list|(
-name|mode_t
+name|int
 parameter_list|,
 name|char
 modifier|*
