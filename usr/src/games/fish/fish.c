5882e10b2b778ba4167ac3571b05516f41a96ef1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-19 07:24:24 -0800

    extra newline makes it more readable
    SCCS-vsn: games/fish/fish.c 5.4
    
    Former-commit-id: dbb8029c036e6b5f2f90472beea8e4d99c81c256

--- usr/src/games/fish/fish.c
+++ usr/src/games/fish/fish.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)fish.c	5.3 (Berkeley) %G%"
+literal|"@(#)fish.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -485,7 +485,7 @@ name|void
 operator|)
 name|printf
 argument_list|(
-literal|"Your hand is:"
+literal|"\nYour hand is:"
 argument_list|)
 expr_stmt|;
 name|printhand
