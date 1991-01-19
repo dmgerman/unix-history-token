43fc0ad4c6db6c30fe9ce53e4d368f899c2efb2b
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-01-19 01:36:39 -0800

    amended initflag initialization, now done for all processor types
    SCCS-vsn: sys/kern/init_main.c 7.33
    
    Former-commit-id: a15790a225a80e452f5a138eee1b1c446d7695b3

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)init_main.c	7.32 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)init_main.c	7.33 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -149,15 +149,6 @@ parameter_list|()
 function_decl|;
 end_function_decl
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|i386
-argument_list|)
-end_if
-
 begin_decl_stmt
 specifier|extern
 name|char
@@ -165,11 +156,6 @@ name|initflags
 index|[]
 decl_stmt|;
 end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/*  * Initialization code.  * Called from cold start routine as  * soon as a stack and segmentation  * have been established.  * Functions:  *	clear and free user core  *	turn on clock  *	hand craft 0th process  *	call all initialization routines  *	fork - process 0 to schedule  *	     - process 1 execute bootstrap  *	     - process 2 to page out  */
@@ -206,62 +192,44 @@ name|filedesc
 modifier|*
 name|fdp
 decl_stmt|;
+name|char
+modifier|*
+name|ip
+init|=
+name|initflags
+decl_stmt|;
 name|int
 name|s
 decl_stmt|;
 name|rqinit
 argument_list|()
 expr_stmt|;
-if|#
-directive|if
-name|defined
-argument_list|(
-name|i386
-argument_list|)
 comment|/* 	 * set boot flags 	 */
+operator|*
+name|ip
+operator|++
+operator|=
+literal|'-'
+expr_stmt|;
 if|if
 condition|(
 name|boothowto
 operator|&
 name|RB_SINGLE
 condition|)
-name|bcopy
-argument_list|(
-literal|"-s"
-argument_list|,
-name|initflags
-argument_list|,
-literal|3
-argument_list|)
+operator|*
+name|ip
+operator|++
+operator|=
+literal|'s'
 expr_stmt|;
-elseif|else
-if|if
-condition|(
-name|boothowto
-operator|&
-name|RB_ASKNAME
-condition|)
-name|bcopy
-argument_list|(
-literal|"-a"
-argument_list|,
-name|initflags
-argument_list|,
-literal|3
-argument_list|)
+comment|/* if (boothowto&RB_FASTBOOT) 		*ip++ = 'f'; */
+operator|*
+name|ip
+operator|++
+operator|=
+literal|'\0'
 expr_stmt|;
-else|else
-name|bcopy
-argument_list|(
-literal|"-"
-argument_list|,
-name|initflags
-argument_list|,
-literal|2
-argument_list|)
-expr_stmt|;
-endif|#
-directive|endif
 if|#
 directive|if
 name|defined
