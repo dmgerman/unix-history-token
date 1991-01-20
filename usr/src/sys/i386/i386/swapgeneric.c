f0e9bf0f463c90c1517cbacd9a145b658afff551
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-01-19 20:46:36 -0800

    kernel reorg
    SCCS-vsn: sys/i386/i386/swapgeneric.c 5.4
    
    Former-commit-id: 151dd175a07f6c727e730275c5ddb0647c404de4

--- usr/src/sys/i386/i386/swapgeneric.c
+++ usr/src/sys/i386/i386/swapgeneric.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.386.c%  *  *	@(#)swapgeneric.c	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.386.c%  *  *	@(#)swapgeneric.c	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -10,43 +10,43 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"../machine/pte.h"
+file|"machine/pte.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"param.h"
+file|"sys/param.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"conf.h"
+file|"sys/conf.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"buf.h"
+file|"sys/buf.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"vm.h"
+file|"sys/vm.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"systm.h"
+file|"sys/systm.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"reboot.h"
+file|"sys/reboot.h"
 end_include
 
 begin_comment
