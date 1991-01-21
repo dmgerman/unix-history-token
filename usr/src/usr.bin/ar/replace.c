07307dfda19f03e2f23e3f0389818dd5678a7d69
Chris Torek <torek@ucbvax.Berkeley.EDU>
1991-01-21 08:43:06 -0800

    temporary files are already padded: do not pad them again
    SCCS-vsn: usr.bin/ar/replace.c 5.3
    
    Former-commit-id: bad350279cdbb1e3b2f4ff525ec08ca5c9cbcda0

--- usr/src/usr.bin/ar/replace.c
+++ usr/src/usr.bin/ar/replace.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)replace.c	5.2 (Berkeley) %G%"
+literal|"@(#)replace.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -725,9 +725,7 @@ name|afd
 argument_list|,
 name|archive
 argument_list|,
-name|RPAD
-operator||
-name|WPAD
+literal|0
 argument_list|)
 expr_stmt|;
 if|if
