4c9c803ee0a2e2ab67771bcae68e4978f8c31047
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-01-29 05:41:35 -0800

    label_t must be defined even if not just kernel
    SCCS-vsn: sys/hp300/include/types.h 7.8
    
    Former-commit-id: c291b14b42fd9329fb46eb4bd42d8ea366ca59ed

--- usr/src/sys/hp300/include/types.h
+++ usr/src/sys/hp300/include/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.7 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -88,9 +88,16 @@ end_comment
 begin_if
 if|#
 directive|if
+operator|!
 name|defined
 argument_list|(
-name|KERNEL
+name|_ANSI_SOURCE
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|_POSIX_SOURCE
 argument_list|)
 end_if
 
