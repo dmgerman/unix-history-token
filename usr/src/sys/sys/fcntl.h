660f9d9392abbcbbfa345df1667724fcef5e738e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-02-04 06:51:52 -0800

    add include of types.h
    SCCS-vsn: sys/sys/fcntl.h 5.9
    
    Former-commit-id: 1d19046cff708c2d0cdff8f42066387001f34de6

--- usr/src/sys/sys/fcntl.h
+++ usr/src/sys/sys/fcntl.h
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1983, 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fcntl.h	5.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1983, 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fcntl.h	5.9 (Berkeley) %G%  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|"types.h"
+end_include
 
 begin_ifndef
 ifndef|#
