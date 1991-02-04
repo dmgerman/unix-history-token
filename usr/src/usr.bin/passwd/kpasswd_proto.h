a1996e32b10572472b3f39e77d064e3a8dded71a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-03 21:35:48 -0800

    fix copyright notice
    SCCS-vsn: usr.bin/passwd/kpasswd_proto.h 5.1
    
    Former-commit-id: 7e3c5eedbb5661381ce3525336f49bddc9f5ac3e

--- usr/src/usr.bin/passwd/kpasswd_proto.h
+++ usr/src/usr.bin/passwd/kpasswd_proto.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
+comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kpasswd_proto.h	5.1 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * kpasswd_proto  *  * definitions for the kpasswd "protocol"  * (We hope this to be temporary until a real admin protocol is worked out)  */
+comment|/*  * kpasswd_proto  *  * definitions for the kpasswd "protocol"  * (We hope this to be temporary until a real admin protocol is worked out.)  */
 end_comment
 
 begin_struct
@@ -55,6 +55,7 @@ begin_define
 define|#
 directive|define
 name|SECURE_STRING
+define|\
 value|"Kerberos password update program -- 12/9/88 UC Berkeley"
 end_define
 
