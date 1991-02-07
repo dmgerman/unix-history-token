da1d2e58655c1bf78c30559c57ea3c5592beae04
Chris Torek <torek@ucbvax.Berkeley.EDU>
1991-02-07 06:52:50 -0800

    fprintf takes a FILE pointer too
    SCCS-vsn: usr.bin/gprof/lookup.c 5.5
    
    Former-commit-id: 4d9f0c5d136b296acb344043fdf5689393d2132b

--- usr/src/usr.bin/gprof/lookup.c
+++ usr/src/usr.bin/gprof/lookup.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)lookup.c	5.4 (Berkeley) %G%"
+literal|"@(#)lookup.c	5.5 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -245,6 +245,8 @@ condition|)
 block|{
 name|fprintf
 argument_list|(
+name|stderr
+argument_list|,
 literal|"[arclookup] parentp == 0 || childp == 0\n"
 argument_list|)
 expr_stmt|;
