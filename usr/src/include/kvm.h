c161c9306bf42b31bcb60ba334aa43640f9fc385
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-13 04:13:26 -0800

    don't need to prepend key with type byte any more; should never have included sys/kinfo.h; use <paths.h> for /var/run pathname; wordsmith function prototypes
    SCCS-vsn: include/kvm.h 5.2
    
    Former-commit-id: 4afb7182752ec3ef8d70ba0557ef3439c40766cd

--- usr/src/include/kvm.h
+++ usr/src/include/kvm.h
@@ -1,13 +1,7 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kvm.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kvm.h	5.2 (Berkeley) %G%  */
 end_comment
-
-begin_include
-include|#
-directive|include
-file|<sys/kinfo.h>
-end_include
 
 begin_include
 include|#
@@ -16,40 +10,40 @@ file|<sys/cdefs.h>
 end_include
 
 begin_comment
-comment|/*  * type byte prepending key  */
+comment|/* Default version symbol. */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|KVMDB_NLIST
-value|1
+name|VRS_SYM
+value|"_version"
 end_define
 
 begin_define
 define|#
 directive|define
-name|KVMDB_DEVS
-value|2
-end_define
-
-begin_define
-define|#
-directive|define
-name|KVMDBDIR
-value|"/var/run"
+name|VRS_KEY
+value|"VERSION"
 end_define
 
 begin_decl_stmt
 name|__BEGIN_DECLS
-name|struct
-name|proc
+name|char
 modifier|*
-name|kvm_nextproc
+name|kvm_getargs
 name|__P
 argument_list|(
 operator|(
-name|void
+specifier|const
+expr|struct
+name|proc
+operator|*
+operator|,
+specifier|const
+expr|struct
+name|user
+operator|*
 operator|)
 argument_list|)
 decl_stmt|;
@@ -73,6 +67,19 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+name|char
+modifier|*
+name|kvm_geterr
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 name|struct
 name|user
 modifier|*
@@ -90,30 +97,10 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|char
-modifier|*
-name|kvm_getargs
-name|__P
-argument_list|(
-operator|(
-specifier|const
-expr|struct
+name|struct
 name|proc
-operator|*
-operator|,
-specifier|const
-expr|struct
-name|user
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|char
 modifier|*
-name|kvm_geterr
+name|kvm_nextproc
 name|__P
 argument_list|(
 operator|(
