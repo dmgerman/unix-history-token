804b1ca4415271a484bc7a4cb6ea14ee5766b2ff
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-15 21:43:34 -0800

    done at DEC; add Berkeley specific copyright notice
    SCCS-vsn: sys/sys/dkbad.h 7.2
    
    Former-commit-id: c659f380e7098ca553420455bd34960328513976

--- usr/src/sys/sys/dkbad.h
+++ usr/src/sys/sys/dkbad.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dkbad.h	7.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dkbad.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Definitions needed to perform bad sector  * revectoring ala DEC STD 144.  *  * The bad sector information is located in the  * first 5 even numbered sectors of the last  * track of the disk pack.  There are five  * identical copies of the information, described  * by the dkbad structure.  *  * Replacement sectors are allocated starting with  * the first sector before the bad sector information  * and working backwards towards the beginning of  * the disk.  A maximum of 126 bad sectors are supported.  * The position of the bad sector in the bad sector table  * determines which replacement sector it corresponds to.  *  * The bad sector information and replacement sectors  * are conventionally only accessible through the  * 'c' file system partition of the disk.  If that  * partition is used for a file system, the user is  * responsible for making sure that it does not overlap  * the bad sector information or any replacement sector.s  */
+comment|/*  * Definitions needed to perform bad sector revectoring ala DEC STD 144.  *  * The bad sector information is located in the first 5 even numbered  * sectors of the last track of the disk pack.  There are five identical  * copies of the information, described by the dkbad structure.  *  * Replacement sectors are allocated starting with the first sector before  * the bad sector information and working backwards towards the beginning of  * the disk.  A maximum of 126 bad sectors are supported.  The position of  * the bad sector in the bad sector table determines which replacement sector  * it corresponds to.  *  * The bad sector information and replacement sectors are conventionally  * only accessible through the 'c' file system partition of the disk.  If  * that partition is used for a file system, the user is responsible for  * making sure that it does not overlap the bad sector information or any  * replacement sectors.  */
 end_comment
 
 begin_struct
