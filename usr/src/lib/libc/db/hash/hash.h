8ecd6f7a700dc291f369f7f99883e6a6b1940b11
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-19 18:31:09 -0800

    add "pinning" to the buffer pool
    SCCS-vsn: lib/libc/db/hash/hash.h 5.2
    
    Former-commit-id: c3393df28041423d0c698d97b21a9caf27bd24e7

--- usr/src/lib/libc/db/hash/hash.h
+++ usr/src/lib/libc/db/hash/hash.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * %sccs.include.redist.c%  *  *	@(#)hash.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * %sccs.include.redist.c%  *  *	@(#)hash.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -70,35 +70,25 @@ comment|/* Actual page data */
 name|char
 name|flags
 decl_stmt|;
-comment|/* Combination of BUF_MOD, BUF_DISK, BUF_BUCKET */
-block|}
-struct|;
-end_struct
-
-begin_comment
-comment|/* Flag Values */
-end_comment
-
-begin_define
 define|#
 directive|define
 name|BUF_MOD
 value|0x0001
-end_define
-
-begin_define
 define|#
 directive|define
 name|BUF_DISK
 value|0x0002
-end_define
-
-begin_define
 define|#
 directive|define
 name|BUF_BUCKET
 value|0x0004
-end_define
+define|#
+directive|define
+name|BUF_PIN
+value|0x0008
+block|}
+struct|;
+end_struct
 
 begin_define
 define|#
