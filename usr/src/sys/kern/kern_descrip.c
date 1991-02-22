228705d0ac45f923384955ddbed502ec140a988d
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-02-22 00:16:22 -0800

    changes to fflags FCNTLCANT => FCNTLFLAGS, FFLAGS/OFLAGS
    SCCS-vsn: sys/kern/kern_descrip.c 7.19
    
    Former-commit-id: 7aae7b10f6f7c4abe14d4d023b629072289a8641

--- usr/src/sys/kern/kern_descrip.c
+++ usr/src/sys/kern/kern_descrip.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_descrip.c	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_descrip.c	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -973,11 +973,12 @@ case|:
 operator|*
 name|retval
 operator|=
+name|OFLAGS
+argument_list|(
 name|fp
 operator|->
 name|f_flag
-operator|+
-name|FOPEN
+argument_list|)
 expr_stmt|;
 return|return
 operator|(
@@ -991,22 +992,21 @@ name|fp
 operator|->
 name|f_flag
 operator|&=
-name|FCNTLCANT
+operator|~
+name|FCNTLFLAGS
 expr_stmt|;
 name|fp
 operator|->
 name|f_flag
 operator||=
-operator|(
+name|FFLAGS
+argument_list|(
 name|uap
 operator|->
 name|arg
-operator|-
-name|FOPEN
-operator|)
+argument_list|)
 operator|&
-operator|~
-name|FCNTLCANT
+name|FCNTLFLAGS
 expr_stmt|;
 if|if
 condition|(
