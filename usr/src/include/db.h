b7d6c2628400ec7189d5797f07855ac58956a87b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-22 18:08:54 -0800

    make safe for c++
    SCCS-vsn: include/db.h 5.4
    
    Former-commit-id: 0c7d247c581140a5f6695dd7bdce229c1127fbc5

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -165,7 +165,7 @@ function_decl|;
 name|int
 function_decl|(
 modifier|*
-name|delete
+name|del
 function_decl|)
 parameter_list|()
 function_decl|;
