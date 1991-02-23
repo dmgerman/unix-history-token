3d2b9f111b34e6077b465a62b9c1395895e7e6e8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-22 16:18:55 -0800

    need stdio for DEBUG and HASH_STATISTICS
    SCCS-vsn: lib/libc/db/hash/hash.c 5.8
    
    Former-commit-id: 98df35361c22a22e0acacca2f091657ebd3a9d7c

--- usr/src/lib/libc/db/hash/hash.c
+++ usr/src/lib/libc/db/hash/hash.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)hash.c	5.7 (Berkeley) %G%"
+literal|"@(#)hash.c	5.8 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -77,6 +77,12 @@ begin_include
 include|#
 directive|include
 file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<stdio.h>
 end_include
 
 begin_include
