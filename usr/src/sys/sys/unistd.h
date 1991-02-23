97b1bada4ec5e1ba7e561e70eb184805be21a7de
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-02-22 17:46:26 -0800

    move old lseek values here from sys/file.h; #ifdef KERNEL prototypes
    SCCS-vsn: sys/sys/unistd.h 5.12
    
    Former-commit-id: c5068411bf570dff5a88c96338385b763c179f29

--- usr/src/sys/sys/unistd.h
+++ usr/src/sys/sys/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.12 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -124,7 +124,7 @@ comment|/* test for read permission */
 end_comment
 
 begin_comment
-comment|/* lseek function */
+comment|/* whence values for lseek(2) */
 end_comment
 
 begin_define
@@ -159,6 +159,42 @@ end_define
 begin_comment
 comment|/* set file offset to EOF plus offset */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_POSIX_SOURCE
+end_ifndef
+
+begin_comment
+comment|/* whence values for lseek(2); renamed by POSIX 1003.1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|L_SET
+value|SEEK_SET
+end_define
+
+begin_define
+define|#
+directive|define
+name|L_INCR
+value|SEEK_CUR
+end_define
+
+begin_define
+define|#
+directive|define
+name|L_XTND
+value|SEEK_END
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/* map a stream pointer to a file descriptor */
@@ -431,6 +467,12 @@ end_typedef
 begin_comment
 comment|/* what read() returns */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|KERNEL
+end_ifndef
 
 begin_include
 include|#
@@ -1134,6 +1176,15 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* KERNEL */
+end_comment
 
 begin_macro
 name|__END_DECLS
