0acd1421bacee3bcf4030aa0e999bcbb1eef3307
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-02-22 16:30:03 -0800

    Remove prototype parameter names, both because they didn't begin with underscores and because they contained three of the four uses of C++ keywords.
    SCCS-vsn: include/db.h 5.3
    
    Former-commit-id: 2dd2fb82802ed01fcad2597351bd1d3ef140de79

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -454,18 +454,14 @@ operator|(
 specifier|const
 name|char
 operator|*
-name|file
 operator|,
 name|int
-name|flags
 operator|,
 name|int
-name|mode
 operator|,
 specifier|const
 name|BTREEINFO
 operator|*
-name|private
 operator|)
 argument_list|)
 decl_stmt|;
@@ -481,18 +477,14 @@ operator|(
 specifier|const
 name|char
 operator|*
-name|file
 operator|,
 name|int
-name|flags
 operator|,
 name|int
-name|mode
 operator|,
 specifier|const
 name|HASHINFO
 operator|*
-name|private
 operator|)
 argument_list|)
 decl_stmt|;
@@ -508,18 +500,14 @@ operator|(
 specifier|const
 name|char
 operator|*
-name|file
 operator|,
 name|int
-name|flags
 operator|,
 name|int
-name|mode
 operator|,
 specifier|const
 name|RECNOINFO
 operator|*
-name|private
 operator|)
 argument_list|)
 decl_stmt|;
