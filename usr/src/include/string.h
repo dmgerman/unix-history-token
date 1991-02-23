7259e81bad3a4ff9f27283a662bb4e827a42540f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-22 18:18:54 -0800

    b{cmp,copy,zero} all take void *, not char *
    SCCS-vsn: include/string.h 5.8
    
    Former-commit-id: 4fed9e484a546ad8e60ec0713bf5e4892c6ad98d

--- usr/src/include/string.h
+++ usr/src/include/string.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)string.h	5.7 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)string.h	5.8 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -488,11 +488,11 @@ name|__P
 argument_list|(
 operator|(
 specifier|const
-name|char
+name|void
 operator|*
 operator|,
 specifier|const
-name|char
+name|void
 operator|*
 operator|,
 name|size_t
@@ -508,10 +508,10 @@ name|__P
 argument_list|(
 operator|(
 specifier|const
-name|char
+name|void
 operator|*
 operator|,
-name|char
+name|void
 operator|*
 operator|,
 name|size_t
@@ -526,7 +526,7 @@ name|bzero
 name|__P
 argument_list|(
 operator|(
-name|char
+name|void
 operator|*
 operator|,
 name|size_t
