eb0716a798846bcee028b5ba1edfeede172e7fb0
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-02-22 17:22:17 -0800

    Declarations for link_addr and link_ntoa.
    SCCS-vsn: sys/net/if_dl.h 7.2
    
    Former-commit-id: 5ef614c6ac36d6f5260b1a1ce21f5a973f18ae7f

--- usr/src/sys/net/if_dl.h
+++ usr/src/sys/net/if_dl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_dl.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_dl.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -63,6 +63,66 @@ name|s
 parameter_list|)
 value|((caddr_t)((s)->sdl_data + (s)->sdl_nlen))
 end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|KERNEL
+end_ifndef
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_decl_stmt
+name|__BEGIN_DECLS
+name|void
+name|link_addr
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+expr|struct
+name|sockaddr_dl
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|link_ntoa
+name|__P
+argument_list|(
+operator|(
+specifier|const
+expr|struct
+name|sockaddr_dl
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_macro
+name|__END_DECLS
+end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !KERNEL */
+end_comment
 
 end_unit
 
