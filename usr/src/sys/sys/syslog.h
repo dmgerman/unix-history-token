b229aa9cdd82680bfd0031db11c4e0f6959c9203
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-22 22:01:18 -0800

    use stdarg, not varargs
    SCCS-vsn: sys/sys/syslog.h 7.19
    
    Former-commit-id: 453515dd363df0e4b3b5a51f1f672d64bb0c2c9d

--- usr/src/sys/sys/syslog.h
+++ usr/src/sys/sys/syslog.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)syslog.h	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)syslog.h	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -758,7 +758,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<varargs.h>
+file|<stdarg.h>
 end_include
 
 begin_decl_stmt
