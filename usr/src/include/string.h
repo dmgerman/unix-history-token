21cea9fb540746172b63af4e8ee6243e3b92fefb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-24 22:09:09 -0800

    swab takes void *'s, not char *'s
    SCCS-vsn: include/string.h 5.9
    
    Former-commit-id: a49a741e5b3fec098a7d806cbc23c28bb283c22c

--- usr/src/include/string.h
+++ usr/src/include/string.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)string.h	5.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)string.h	5.9 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -697,10 +697,10 @@ name|__P
 argument_list|(
 operator|(
 specifier|const
-name|char
+name|void
 operator|*
 operator|,
-name|char
+name|void
 operator|*
 operator|,
 name|size_t
