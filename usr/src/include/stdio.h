cfc54e77d85043d3fd1f41704b3a4c0546fdb36b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-24 22:16:22 -0800

    move P_tmpdir from /usr/tmp to /var/tmp add prototype for tempnam(3)
    SCCS-vsn: include/stdio.h 5.12
    
    Former-commit-id: 559ad91ce22b6233cbbcc197f3eafa85b5aec3c0

--- usr/src/include/stdio.h
+++ usr/src/include/stdio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Chris Torek.  *  * %sccs.include.redist.c%  *  *	@(#)stdio.h	5.11 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Chris Torek.  *  * %sccs.include.redist.c%  *  *	@(#)stdio.h	5.12 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -537,7 +537,7 @@ begin_define
 define|#
 directive|define
 name|P_tmpdir
-value|"/usr/tmp/"
+value|"/var/tmp/"
 end_define
 
 begin_endif
@@ -1503,6 +1503,25 @@ name|__P
 argument_list|(
 operator|(
 name|FILE
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|tempnam
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+specifier|const
+name|char
 operator|*
 operator|)
 argument_list|)
