79f72efd063a59b98a443f1992974ccfe31faa24
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-26 19:56:42 -0800

    getgroups takes a pointer to int's for now
    SCCS-vsn: include/unistd.h 5.7
    
    Former-commit-id: e81517799f6f69712fbd7ad949704c78d249b159

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.6 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -462,12 +462,16 @@ argument_list|(
 operator|(
 name|int
 operator|,
-name|gid_t
+name|int
 operator|*
 operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_comment
+comment|/* XXX (gid_t *) */
+end_comment
 
 begin_decl_stmt
 name|char
