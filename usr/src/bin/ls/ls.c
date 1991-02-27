fcdec59fd5e90566c3a376dc36c8f5e5fce4eee8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-26 23:06:04 -0800

    ANSI makes strings unwriteable
    SCCS-vsn: bin/ls/ls.c 5.44
    
    Former-commit-id: 8bd1e19077f076d93d9c967bb072b6b8056b2899

--- usr/src/bin/ls/ls.c
+++ usr/src/bin/ls/ls.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)ls.c	5.43 (Berkeley) %G%"
+literal|"@(#)ls.c	5.44 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -905,6 +905,13 @@ operator|!
 name|argc
 condition|)
 block|{
+name|char
+name|dot
+index|[]
+init|=
+literal|"."
+decl_stmt|;
+comment|/* ANSI says it's not writable.*/
 name|argc
 operator|=
 literal|1
@@ -914,7 +921,7 @@ index|[
 literal|0
 index|]
 operator|=
-literal|"."
+name|dot
 expr_stmt|;
 name|argv
 index|[
