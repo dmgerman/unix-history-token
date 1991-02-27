a776c1633f1847e30e5359aa78f0bfcfed104854
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-27 00:06:12 -0800

    ANSI fix
    SCCS-vsn: sbin/XNSrouted/trace.c 5.11
    
    Former-commit-id: bf7bf5c01375892648cd3a4273161cfd3a883cd5

--- usr/src/sbin/XNSrouted/trace.c
+++ usr/src/sbin/XNSrouted/trace.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)trace.c	5.10 (Berkeley) %G%"
+literal|"@(#)trace.c	5.11 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -37,6 +37,12 @@ define|#
 directive|define
 name|RIPCMDS
 end_define
+
+begin_include
+include|#
+directive|include
+file|<stdlib.h>
+end_include
 
 begin_include
 include|#
