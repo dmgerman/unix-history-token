1c9c716aa3d1dfe9eb2a08fdbe1e749762f791d9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-28 01:36:14 -0800

    move boglog into /var/games
    SCCS-vsn: old/boggle/pathnames.h 5.2
    
    Former-commit-id: e3088d2d669a0cf67437b44f6e84403364e17669

--- usr/src/old/boggle/pathnames.h
+++ usr/src/old/boggle/pathnames.h
@@ -1,7 +1,14 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.2 (Berkeley) %G%  */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|_PATH_BOGLOG
+value|"/var/games/boglog"
+end_define
 
 begin_define
 define|#
