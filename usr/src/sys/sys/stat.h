c3831d7851ccaa0476a5712c83d29242721ea53f
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-04 00:28:07 -0800

    Put the fchmod() declarationwith the chmod() declaration.
    SCCS-vsn: sys/sys/stat.h 7.11
    
    Former-commit-id: b2db40418eaeae5d1975bf0dbceaf8f57faf9b65

--- usr/src/sys/sys/stat.h
+++ usr/src/sys/sys/stat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stat.h	7.10 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stat.h	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_struct
@@ -641,6 +641,20 @@ ifndef|#
 directive|ifndef
 name|_POSIX_SOURCE
 end_ifndef
+
+begin_decl_stmt
+name|int
+name|fchmod
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+name|mode_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|int
