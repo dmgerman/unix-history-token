de7e16ad664bf2bd3f0ff14db78c7dfc65e00dfb
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-04 00:28:29 -0800

    Move the fchmod() declaration to stat.h.
    SCCS-vsn: include/unistd.h 5.8
    
    Former-commit-id: 953acb41405ab9c61526deb8c3fbc23d1e496973

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.7 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -983,20 +983,6 @@ operator|(
 name|int
 operator|,
 name|long
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|fchmod
-name|__P
-argument_list|(
-operator|(
-name|int
-operator|,
-name|mode_t
 operator|)
 argument_list|)
 decl_stmt|;
