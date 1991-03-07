de2ae1e9ab61db06ffa022f4317669b56b17aeeb
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-03-07 00:33:59 -0800

    fix winsize (extra indirection)
    SCCS-vsn: lib/libutil/pty.c 5.4
    
    Former-commit-id: 085da79eac916926b8d6e281911d09776c644818

--- usr/src/lib/libutil/pty.c
+++ usr/src/lib/libutil/pty.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)pty.c	5.3 (Berkeley) %G%"
+literal|"@(#)pty.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -399,7 +399,6 @@ operator|(
 name|char
 operator|*
 operator|)
-operator|&
 name|winp
 argument_list|)
 expr_stmt|;
