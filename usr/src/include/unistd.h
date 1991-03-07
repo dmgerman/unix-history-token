27410b25dc215da0f167a4fd76846dc177232948
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-06 21:51:55 -0800

    add des_cipher(3), des_setkey(3)
    SCCS-vsn: include/unistd.h 5.9
    
    Former-commit-id: 631f8d70f345de03d538ee1a18cc5289cc92b6c8

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.9 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -906,6 +906,42 @@ operator|,
 specifier|const
 name|char
 operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|des_cipher
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+name|char
+operator|*
+operator|,
+name|u_long
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|des_setkey
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+name|key
 operator|)
 argument_list|)
 decl_stmt|;
