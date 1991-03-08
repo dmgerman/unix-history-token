826fde46dfc7fe120fdda87bd51402c4b4f51280
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-07 19:45:28 -0800

    routine arg to glob(3) is a const
    SCCS-vsn: include/glob.h 5.4
    
    Former-commit-id: 74736d55cc9cbf5a18d0655adc6531ab52ba4e52

--- usr/src/include/glob.h
+++ usr/src/include/glob.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Guido van Rossum.  *  * %sccs.include.redist.c%  *  *	@(#)glob.h	5.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Guido van Rossum.  *  * %sccs.include.redist.c%  *  *	@(#)glob.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
@@ -167,7 +167,6 @@ operator|*
 operator|,
 name|int
 operator|,
-specifier|const
 name|int
 argument_list|(
 operator|*
