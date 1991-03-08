6470f12522c0f1b026a93ae8909942901bd9ebc4
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-08 01:08:17 -0800

    #define's don't have to end in ';'...
    SCCS-vsn: libexec/bugfiler/pathnames.h 5.4
    
    Former-commit-id: 43a0eb2b40fd02958df9963e6c2bdd70cd2dc0ee

--- usr/src/libexec/bugfiler/pathnames.h
+++ usr/src/libexec/bugfiler/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -20,7 +20,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_TMP
-value|"/tmp/BUG_XXXXXX";
+value|"/tmp/BUG_XXXXXX"
 end_define
 
 end_unit
