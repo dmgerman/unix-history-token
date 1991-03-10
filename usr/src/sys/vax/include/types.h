5222b3cd72c8e3f9717e34a523c9c1f0f82932b7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-10 02:05:29 -0800

    create {hp300,i386,tahoe,vax}/include/ansi.h
    SCCS-vsn: sys/vax/include/types.h 7.5
    
    Former-commit-id: 553d3d9072901440026838072a040e112b7ba50e

--- usr/src/sys/vax/include/types.h
+++ usr/src/sys/vax/include/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -14,92 +14,6 @@ define|#
 directive|define
 name|_MACHTYPES_H_
 end_define
-
-begin_comment
-comment|/*  * Types which are fundamental to the implementation and may appear in  * more than one standard header are defined here.  Standard headers  * then use:  *	#ifdef	_SIZE_T_  *	typedef	_SIZE_T_ size_t;  *	#undef	_SIZE_T_  *	#endif  *  * Thanks, ANSI!  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_CLOCK_T_
-value|unsigned long
-end_define
-
-begin_comment
-comment|/* clock() */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_PTRDIFF_T_
-value|int
-end_define
-
-begin_comment
-comment|/* ptr1 - ptr2 */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_SIZE_T_
-value|unsigned int
-end_define
-
-begin_comment
-comment|/* sizeof() */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_TIME_T_
-value|long
-end_define
-
-begin_comment
-comment|/* time() */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_VA_LIST_
-value|char *
-end_define
-
-begin_comment
-comment|/* va_list */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_WCHAR_T_
-value|unsigned short
-end_define
-
-begin_comment
-comment|/* wchar_t */
-end_comment
-
-begin_if
-if|#
-directive|if
-operator|!
-name|defined
-argument_list|(
-name|_ANSI_SOURCE
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
-name|_POSIX_SOURCE
-argument_list|)
-end_if
 
 begin_typedef
 typedef|typedef
@@ -133,11 +47,6 @@ block|}
 name|label_t
 typedef|;
 end_typedef
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_endif
 endif|#
