332a52709b327eed622f10a5933e4e3056b3683a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-10 23:37:04 -0800

    ANSI
    SCCS-vsn: usr.bin/ar/misc.c 5.2
    
    Former-commit-id: ce7d91e7d3f76ec550ea6c474e846595a560f2c9

--- usr/src/usr.bin/ar/misc.c
+++ usr/src/usr.bin/ar/misc.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)misc.c	5.1 (Berkeley) %G%"
+literal|"@(#)misc.c	5.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -37,13 +37,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/signal.h>
+file|<sys/errno.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<sys/errno.h>
+file|<signal.h>
 end_include
 
 begin_include
@@ -55,19 +55,25 @@ end_include
 begin_include
 include|#
 directive|include
+file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<stdio.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|"pathnames.h"
+file|"archive.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"archive.h"
+file|"pathnames.h"
 end_include
 
 begin_decl_stmt
