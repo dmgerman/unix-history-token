941b35b874fd10616c1bf7e5eea453335da1fe59
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-12 00:06:17 -0800

    change hash "bytes to cache" to cachesize for consistency
    SCCS-vsn: include/db.h 5.7
    
    Former-commit-id: 88b2239708c8bb90ba624d1f61737991e87fd662

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.6 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -381,7 +381,7 @@ name|nelem
 decl_stmt|;
 comment|/* number of elements */
 name|int
-name|ncached
+name|cachesize
 decl_stmt|;
 comment|/* bytes to cache */
 name|int
