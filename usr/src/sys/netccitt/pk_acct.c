5740c36b8102e44f358b5ea620c52522c1961a1c
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-03-12 23:09:51 -0800

    old cpp forgave missing double quotes at end of #include line, gcc doesn't
    SCCS-vsn: sys/netccitt/pk_acct.c 7.4
    
    Former-commit-id: 93e2cda2c2e6ed9a4f9705780518caad5789c914

--- usr/src/sys/netccitt/pk_acct.c
+++ usr/src/sys/netccitt/pk_acct.c
@@ -1,12 +1,12 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk_acct.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk_acct.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
-file|"param.h
+file|"param.h"
 end_include
 
 begin_include
