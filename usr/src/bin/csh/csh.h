00d00dcab3dc4830de49dccb90880df3a273b071
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-14 20:31:45 -0800

    lost the SCCS id's...
    SCCS-vsn: bin/csh/csh.h 5.8
    
    Former-commit-id: dd86129ea2175845727b547616ea7dbb1aaa4991

--- usr/src/bin/csh/csh.h
+++ usr/src/bin/csh/csh.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley Software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sh.h	5.6 (Berkeley) 2/25/91  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley Software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)csh.h	5.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
