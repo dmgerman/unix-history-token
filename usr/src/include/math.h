d609081326652e5d00e2dc077b117c749362f18f
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-18 19:03:17 -0800

    Prevent re-inclusions
    SCCS-vsn: include/math.h 5.4
    
    Former-commit-id: 63c4f7c953d663e728c60860424c2096c6448ad2

--- usr/src/include/math.h
+++ usr/src/include/math.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * All recipients should regard themselves as participants in an ongoing  * research project and hence should feel obligated to report their  * experiences (good or bad) with these elementary function codes, using  * the sendbug(8) program, to the authors.  *  *	@(#)math.h	5.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1985 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * All recipients should regard themselves as participants in an ongoing  * research project and hence should feel obligated to report their  * experiences (good or bad) with these elementary function codes, using  * the sendbug(8) program, to the authors.  *  *	@(#)math.h	5.4 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_MATH_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_MATH_H_
+end_define
 
 begin_if
 if|#
@@ -717,6 +729,15 @@ end_endif
 begin_macro
 name|__END_DECLS
 end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _MATH_H_ */
+end_comment
 
 end_unit
 
