be4ce2e21d3069deb15768b7733e0a7b7e0198ab
Chris Torek <torek@ucbvax.Berkeley.EDU>
1991-03-23 10:17:17 -0800

    fix #ifndef TRACE definition of `trace' (gcc complains about repeated b)
    SCCS-vsn: sys/sys/trace.h 7.5
    
    Former-commit-id: 87640d4999701f4435edf54d0bedf8794f62e400

--- usr/src/sys/sys/trace.h
+++ usr/src/sys/sys/trace.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)trace.h	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)trace.h	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -502,7 +502,7 @@ name|a
 parameter_list|,
 name|b
 parameter_list|,
-name|b
+name|c
 parameter_list|)
 value|;
 end_define
