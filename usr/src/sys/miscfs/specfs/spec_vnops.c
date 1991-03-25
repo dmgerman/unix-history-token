1ccb22c479b5bdcb03bf722e452ceed30cc5f276
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-03-24 23:25:52 -0800

    new device calling conventions, consolidate nullops
    SCCS-vsn: sys/miscfs/specfs/spec_vnops.c 7.32
    
    Former-commit-id: cd2dc6cb3dc5c4c6e448088346890ebd37109cbc

--- usr/src/sys/miscfs/specfs/spec_vnops.c
+++ usr/src/sys/miscfs/specfs/spec_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)spec_vnops.c	7.31 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)spec_vnops.c	7.32 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -12,13 +12,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|"systm.h"
+file|"proc.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"user.h"
+file|"systm.h"
 end_include
 
 begin_include
