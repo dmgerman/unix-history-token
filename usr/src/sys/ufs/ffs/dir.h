ca8a6b24ed6b87e21d2995590f0fafdbb954fff0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-26 01:28:22 -0800

    _DIR_ => _DIR_H
    SCCS-vsn: sys/ufs/ffs/dir.h 7.10
    SCCS-vsn: sys/ufs/ufs/dir.h 7.10
    
    Former-commit-id: c43e78b5c85c5940c82b8e41bab4bb6a4b2ce950

--- usr/src/sys/ufs/ffs/dir.h
+++ usr/src/sys/ufs/ffs/dir.h
@@ -1,18 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dir.h	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dir.h	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_DIR_
+name|_DIR_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_DIR_
+name|_DIR_H_
 end_define
 
 begin_comment
@@ -127,7 +127,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _DIR_ */
+comment|/* !_DIR_H_ */
 end_comment
 
 end_unit
