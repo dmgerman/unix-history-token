37a6d212e8cbec6d077091d8c58186c1d93b363f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-03-31 18:36:13 -0800

    initialize error return to zero (4.3BSD-reno/sys/6)
    SCCS-vsn: sys/vax/uba/dz.c 7.12
    
    Former-commit-id: f358643b79e8bf7966aa657e1c946e19ce8c75ed

--- usr/src/sys/vax/uba/dz.c
+++ usr/src/sys/vax/uba/dz.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dz.c	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dz.c	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -846,6 +846,8 @@ name|unit
 decl_stmt|;
 name|int
 name|error
+init|=
+literal|0
 decl_stmt|,
 name|dzparam
 argument_list|()
