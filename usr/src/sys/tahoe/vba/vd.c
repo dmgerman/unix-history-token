5cee2f7a575566b50712ed8ba45f1b00cf48a729
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-01 19:09:16 -0800

    fix fault in vdreset_ctlr when unit isn't configured (null pointer)
    SCCS-vsn: sys/tahoe/vba/vd.c 7.14
    
    Former-commit-id: 3329b68b526167ad015a2da35a1768d2dcdb106d

--- usr/src/sys/tahoe/vba/vd.c
+++ usr/src/sys/tahoe/vba/vd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Computer Consoles Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vd.c	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Computer Consoles Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vd.c	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -7357,14 +7357,14 @@ operator|++
 control|)
 if|if
 condition|(
-operator|(
 name|vi
 operator|=
 name|vddinfo
 index|[
 name|unit
 index|]
-operator|)
+operator|&&
+name|vi
 operator|->
 name|ui_mi
 operator|==
