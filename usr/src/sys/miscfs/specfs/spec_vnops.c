730c58b18b78130404945d26f97531a742753b49
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-02 17:06:34 -0800

    ansification: need prototype for function pointer
    SCCS-vsn: sys/miscfs/specfs/spec_vnops.c 7.33
    
    Former-commit-id: 1bb56e5d49892d4c454b82f78db878fb940fac50

--- usr/src/sys/miscfs/specfs/spec_vnops.c
+++ usr/src/sys/miscfs/specfs/spec_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)spec_vnops.c	7.32 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)spec_vnops.c	7.33 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2096,12 +2096,24 @@ operator|->
 name|v_rdev
 decl_stmt|;
 name|int
-function_decl|(
-modifier|*
-name|cfunc
-function_decl|)
-parameter_list|()
-function_decl|;
+argument_list|(
+argument|*cfunc
+argument_list|)
+name|__P
+argument_list|(
+operator|(
+name|dev_t
+operator|,
+name|int
+operator|,
+name|int
+operator|,
+expr|struct
+name|proc
+operator|*
+operator|)
+argument_list|)
+expr_stmt|;
 name|int
 name|mode
 decl_stmt|;
