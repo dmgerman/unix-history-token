97ec263e017c6329b863e0218b49b9a53c7fea50
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-04 00:46:09 -0800

    one version for tahoe, hp300; miscellaneous cleanups
    SCCS-vsn: sys/hp300/include/endian.h 7.7
    
    Former-commit-id: e9b2b4d1bc8634e0b66e8d807e4ca10d393138cb

--- usr/src/sys/hp300/include/endian.h
+++ usr/src/sys/hp300/include/endian.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)endian.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)endian.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Definitions for byte order,  * according to byte significance from low address to high.  */
+comment|/*  * Definitions for byte order, according to byte significance from low  * address to high.  */
 end_comment
 
 begin_define
@@ -15,7 +15,7 @@ value|1234
 end_define
 
 begin_comment
-comment|/* least-significant byte first (vax) */
+comment|/* LSB first: i386, vax */
 end_comment
 
 begin_define
@@ -26,7 +26,7 @@ value|4321
 end_define
 
 begin_comment
-comment|/* most-significant byte first (IBM, net) */
+comment|/* MSB first: 68000, ibm, net */
 end_comment
 
 begin_define
@@ -37,7 +37,7 @@ value|3412
 end_define
 
 begin_comment
-comment|/* LSB first in word, MSW first in long (pdp) */
+comment|/* LSB first in word, MSW first in long */
 end_comment
 
 begin_define
@@ -46,6 +46,84 @@ directive|define
 name|BYTE_ORDER
 value|BIG_ENDIAN
 end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|KERNEL
+end_ifndef
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_decl_stmt
+name|__BEGIN_DECLS
+name|unsigned
+name|long
+name|htonl
+name|__P
+argument_list|(
+operator|(
+name|unsigned
+name|long
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|unsigned
+name|short
+name|htons
+name|__P
+argument_list|(
+operator|(
+name|unsigned
+name|short
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|unsigned
+name|long
+name|ntohl
+name|__P
+argument_list|(
+operator|(
+name|unsigned
+name|long
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|unsigned
+name|short
+name|ntohs
+name|__P
+argument_list|(
+operator|(
+name|unsigned
+name|short
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_macro
+name|__END_DECLS
+end_macro
 
 begin_comment
 comment|/*  * Macros for network/external number representation conversion.  */
@@ -150,28 +228,6 @@ else|#
 directive|else
 end_else
 
-begin_decl_stmt
-name|unsigned
-name|short
-name|ntohs
-argument_list|()
-decl_stmt|,
-name|htons
-argument_list|()
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|unsigned
-name|long
-name|ntohl
-argument_list|()
-decl_stmt|,
-name|htonl
-argument_list|()
-decl_stmt|;
-end_decl_stmt
-
 begin_define
 define|#
 directive|define
@@ -179,7 +235,7 @@ name|NTOHL
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = ntohl(x)
+value|(x) = ntohl((u_long)x)
 end_define
 
 begin_define
@@ -189,7 +245,7 @@ name|NTOHS
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = ntohs(x)
+value|(x) = ntohs((u_short)x)
 end_define
 
 begin_define
@@ -199,7 +255,7 @@ name|HTONL
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = htonl(x)
+value|(x) = htonl((u_long)x)
 end_define
 
 begin_define
@@ -209,7 +265,7 @@ name|HTONS
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = htons(x)
+value|(x) = htons((u_short)x)
 end_define
 
 begin_endif
