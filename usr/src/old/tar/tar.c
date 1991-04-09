750388d3a476a43ba6fb7b1bfbc08ebe4c08252d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-08 23:46:06 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: old/tar/tar.1 6.10
    SCCS-vsn: old/tar/tar.c 5.17
    SCCS-vsn: old/tar/tarformat.5 6.3
    SCCS-vsn: bin/test/test.1 6.4
    SCCS-vsn: old/test/test.1 6.4
    SCCS-vsn: old/test/test.c 5.1
    
    Former-commit-id: ae33c18bdeae47225e82f0b3ccb3f171624c839b

--- usr/src/old/tar/tar.c
+++ usr/src/old/tar/tar.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  */
 end_comment
 
 begin_ifndef
@@ -14,7 +14,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1980 Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1991 The Regents of the University of California.\n\  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)tar.c	5.16 (Berkeley) %G%"
+literal|"@(#)tar.c	5.17 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
