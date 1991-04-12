7358de0a1c4f7b151a0d031d10bb6b92976f1113
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-04-11 18:20:02 -0800

    include vaxuba -> vax/uba + -I/sys
    SCCS-vsn: old/rxformat/rxformat.c 5.6
    
    Former-commit-id: b9f2568af204c42d42dfdd53ccc77b601cadde1d

--- usr/src/old/rxformat/rxformat.c
+++ usr/src/old/rxformat/rxformat.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)rxformat.c	5.5 (Berkeley) %G%"
+literal|"@(#)rxformat.c	5.6 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -61,7 +61,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<vaxuba/rxreg.h>
+file|<vax/uba/rxreg.h>
 end_include
 
 begin_include
