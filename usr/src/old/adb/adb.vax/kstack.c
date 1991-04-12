f5b621d064eee698c49f33ddb6fb175fab07eb77
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-04-11 18:49:15 -0800

    kernel reorg
    SCCS-vsn: old/adb/adb.vax/kstack.c 5.3
    
    Former-commit-id: 09d87bdd648365c2415a50d7835e375250919f1b

--- usr/src/old/adb/adb.vax/kstack.c
+++ usr/src/old/adb/adb.vax/kstack.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)kstack.c	5.2 (Berkeley) %G%"
+literal|"@(#)kstack.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -59,7 +59,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/rpb.h>
+file|<vax/vax/rpb.h>
 end_include
 
 begin_decl_stmt
