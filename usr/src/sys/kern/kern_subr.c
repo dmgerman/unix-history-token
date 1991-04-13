79e787072bfb63ad32b6915848f0f75ff4257724
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-12 23:04:41 -0800

    delete user.h
    SCCS-vsn: sys/kern/kern_subr.c 7.6
    
    Former-commit-id: a512c8e06b2c51e9e889c222b2f3f2d5fe40b358

--- usr/src/sys/kern/kern_subr.c
+++ usr/src/sys/kern/kern_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1991 Regents of the University of California.  * All rights reserved.   *  * %sccs.include.redist.c%  *  *	@(#)kern_subr.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1991 Regents of the University of California.  * All rights reserved.   *  * %sccs.include.redist.c%  *  *	@(#)kern_subr.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -13,12 +13,6 @@ begin_include
 include|#
 directive|include
 file|"systm.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"user.h"
 end_include
 
 begin_expr_stmt
