c92d41d6fc90820b617ad6a39fb2e9e27eb2ca70
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 21:36:55 -0800

    date and time created 91/04/12 14:36:55 by bostic
    SCCS-vsn: usr.bin/f77/include.vax/version.h 5.1
    
    Former-commit-id: 5432a61a70816cea63177207f442b5a1ed0ac3d2

---
+++ usr/src/usr.bin/f77/include.vax/version.h
@@ -0,0 +1,18 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)version.h	5.1 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/* Current version of compiler */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VERSIONNUMBER
+value|"1.1"
+end_define
+
+end_unit
+
