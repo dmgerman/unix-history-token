73959e9270dbaf25469adee140df0f99b8f1c90c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-04-16 06:34:03 -0800

    add prototypes for dead vnode ops; add appropriate proc pointers
    SCCS-vsn: sys/miscfs/deadfs/dead_vnops.c 7.13
    
    Former-commit-id: 61c4e701ca5886c8bc617e79fc7ca9a38596b810

--- usr/src/sys/miscfs/deadfs/dead_vnops.c
+++ usr/src/sys/miscfs/deadfs/dead_vnops.c
@@ -1,12 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dead_vnops.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dead_vnops.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"param.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"systm.h"
 end_include
 
 begin_include
@@ -39,48 +45,427 @@ directive|include
 file|"buf.h"
 end_include
 
+begin_comment
+comment|/*  * Prototypes for dead operations on vnodes.  */
+end_comment
+
 begin_decl_stmt
 name|int
-name|dead_lookup
-argument_list|()
-decl_stmt|,
-name|dead_open
-argument_list|()
-decl_stmt|,
-name|dead_read
-argument_list|()
-decl_stmt|,
-name|dead_write
-argument_list|()
-decl_stmt|,
-name|dead_strategy
-argument_list|()
-decl_stmt|,
-name|dead_ioctl
-argument_list|()
-decl_stmt|,
-name|dead_select
-argument_list|()
-decl_stmt|,
-name|dead_lock
-argument_list|()
-decl_stmt|,
-name|dead_bmap
-argument_list|()
-decl_stmt|,
-name|dead_print
+name|dead_badop
 argument_list|()
 decl_stmt|,
 name|dead_ebadf
 argument_list|()
-decl_stmt|,
-name|dead_badop
-argument_list|()
-decl_stmt|,
-name|dead_nullop
-argument_list|()
 decl_stmt|;
 end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|dead_lookup
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+expr|struct
+name|nameidata
+operator|*
+name|ndp
+operator|,
+expr|struct
+name|proc
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|dead_create
+value|((int (*) __P(( \ 		struct nameidata *ndp, \ 		struct vattr *vap, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_mknod
+value|((int (*) __P(( \ 		struct nameidata *ndp, \ 		struct vattr *vap, \ 		struct ucred *cred, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_decl_stmt
+name|int
+name|dead_open
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+name|int
+name|mode
+operator|,
+expr|struct
+name|ucred
+operator|*
+name|cred
+operator|,
+expr|struct
+name|proc
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|dead_close
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		int fflag, \ 		struct ucred *cred, \ 		struct proc *p))) nullop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_access
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		int mode, \ 		struct ucred *cred, \ 		struct proc *p))) dead_ebadf)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_getattr
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		struct vattr *vap, \ 		struct ucred *cred, \ 		struct proc *p))) dead_ebadf)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_setattr
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		struct vattr *vap, \ 		struct ucred *cred, \ 		struct proc *p))) dead_ebadf)
+end_define
+
+begin_decl_stmt
+name|int
+name|dead_read
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+expr|struct
+name|uio
+operator|*
+name|uio
+operator|,
+name|int
+name|ioflag
+operator|,
+expr|struct
+name|ucred
+operator|*
+name|cred
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|dead_write
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+expr|struct
+name|uio
+operator|*
+name|uio
+operator|,
+name|int
+name|ioflag
+operator|,
+expr|struct
+name|ucred
+operator|*
+name|cred
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|dead_ioctl
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+name|int
+name|command
+operator|,
+name|caddr_t
+name|data
+operator|,
+name|int
+name|fflag
+operator|,
+expr|struct
+name|ucred
+operator|*
+name|cred
+operator|,
+expr|struct
+name|proc
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|dead_select
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+name|int
+name|which
+operator|,
+name|int
+name|fflags
+operator|,
+expr|struct
+name|ucred
+operator|*
+name|cred
+operator|,
+expr|struct
+name|proc
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|dead_mmap
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		int fflags, \ 		struct ucred *cred, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_fsync
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		int fflags, \ 		struct ucred *cred, \ 		int waitfor, \ 		struct proc *p))) nullop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_seek
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		off_t oldoff, \ 		off_t newoff, \ 		struct ucred *cred))) nullop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_remove
+value|((int (*) __P(( \ 		struct nameidata *ndp, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_link
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		struct nameidata *ndp, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_rename
+value|((int (*) __P(( \ 		struct nameidata *fndp, \ 		struct nameidata *tdnp, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_mkdir
+value|((int (*) __P(( \ 		struct nameidata *ndp, \ 		struct vattr *vap, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_rmdir
+value|((int (*) __P(( \ 		struct nameidata *ndp, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_symlink
+value|((int (*) __P(( \ 		struct nameidata *ndp, \ 		struct vattr *vap, \ 		char *target, \ 		struct proc *p))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_readdir
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		struct uio *uio, \ 		struct ucred *cred, \ 		int *eofflagp))) dead_ebadf)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_readlink
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		struct uio *uio, \ 		struct ucred *cred))) dead_ebadf)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_abortop
+value|((int (*) __P(( \ 		struct nameidata *ndp))) dead_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_inactive
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		struct proc *p))) nullop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_reclaim
+value|((int (*) __P(( \ 		struct vnode *vp))) nullop)
+end_define
+
+begin_decl_stmt
+name|int
+name|dead_lock
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|dead_unlock
+value|((int (*) __P(( \ 		struct vnode *vp))) nullop)
+end_define
+
+begin_decl_stmt
+name|int
+name|dead_bmap
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|,
+name|daddr_t
+name|bn
+operator|,
+expr|struct
+name|vnode
+operator|*
+operator|*
+name|vpp
+operator|,
+name|daddr_t
+operator|*
+name|bnp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|dead_strategy
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|buf
+operator|*
+name|bp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|dead_print
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vnode
+operator|*
+name|vp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|dead_islocked
+value|((int (*) __P(( \ 		struct vnode *vp))) nullop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|dead_advlock
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		caddr_t id, \ 		int op, \ 		struct flock *fl, \ 		int flags))) dead_ebadf)
+end_define
 
 begin_decl_stmt
 name|struct
@@ -91,25 +476,25 @@ block|{
 name|dead_lookup
 block|,
 comment|/* lookup */
-name|dead_badop
+name|dead_create
 block|,
 comment|/* create */
-name|dead_badop
+name|dead_mknod
 block|,
 comment|/* mknod */
 name|dead_open
 block|,
 comment|/* open */
-name|dead_nullop
+name|dead_close
 block|,
 comment|/* close */
-name|dead_ebadf
+name|dead_access
 block|,
 comment|/* access */
-name|dead_ebadf
+name|dead_getattr
 block|,
 comment|/* getattr */
-name|dead_ebadf
+name|dead_setattr
 block|,
 comment|/* setattr */
 name|dead_read
@@ -124,52 +509,52 @@ comment|/* ioctl */
 name|dead_select
 block|,
 comment|/* select */
-name|dead_badop
+name|dead_mmap
 block|,
 comment|/* mmap */
-name|dead_nullop
+name|dead_fsync
 block|,
 comment|/* fsync */
-name|dead_nullop
+name|dead_seek
 block|,
 comment|/* seek */
-name|dead_badop
+name|dead_remove
 block|,
 comment|/* remove */
-name|dead_badop
+name|dead_link
 block|,
 comment|/* link */
-name|dead_badop
+name|dead_rename
 block|,
 comment|/* rename */
-name|dead_badop
+name|dead_mkdir
 block|,
 comment|/* mkdir */
-name|dead_badop
+name|dead_rmdir
 block|,
 comment|/* rmdir */
-name|dead_badop
+name|dead_symlink
 block|,
 comment|/* symlink */
-name|dead_ebadf
+name|dead_readdir
 block|,
 comment|/* readdir */
-name|dead_ebadf
+name|dead_readlink
 block|,
 comment|/* readlink */
-name|dead_badop
+name|dead_abortop
 block|,
 comment|/* abortop */
-name|dead_nullop
+name|dead_inactive
 block|,
 comment|/* inactive */
-name|dead_nullop
+name|dead_reclaim
 block|,
 comment|/* reclaim */
 name|dead_lock
 block|,
 comment|/* lock */
-name|dead_nullop
+name|dead_unlock
 block|,
 comment|/* unlock */
 name|dead_bmap
@@ -181,10 +566,10 @@ comment|/* strategy */
 name|dead_print
 block|,
 comment|/* print */
-name|dead_nullop
+name|dead_islocked
 block|,
 comment|/* islocked */
-name|dead_ebadf
+name|dead_advlock
 block|,
 comment|/* advlock */
 block|}
@@ -195,12 +580,18 @@ begin_comment
 comment|/*  * Trivial lookup routine that always fails.  */
 end_comment
 
+begin_comment
+comment|/* ARGSUSED */
+end_comment
+
 begin_macro
 name|dead_lookup
 argument_list|(
 argument|vp
 argument_list|,
 argument|ndp
+argument_list|,
+argument|p
 argument_list|)
 end_macro
 
@@ -217,6 +608,14 @@ name|struct
 name|nameidata
 modifier|*
 name|ndp
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|proc
+modifier|*
+name|p
 decl_stmt|;
 end_decl_stmt
 
@@ -258,6 +657,8 @@ argument_list|,
 argument|mode
 argument_list|,
 argument|cred
+argument_list|,
+argument|p
 argument_list|)
 end_macro
 
@@ -280,6 +681,14 @@ name|struct
 name|ucred
 modifier|*
 name|cred
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|proc
+modifier|*
+name|p
 decl_stmt|;
 end_decl_stmt
 
@@ -471,6 +880,8 @@ argument_list|,
 argument|fflag
 argument_list|,
 argument|cred
+argument_list|,
+argument|p
 argument_list|)
 end_macro
 
@@ -509,6 +920,14 @@ name|cred
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+name|struct
+name|proc
+modifier|*
+name|p
+decl_stmt|;
+end_decl_stmt
+
 begin_block
 block|{
 if|if
@@ -537,6 +956,8 @@ argument_list|,
 name|fflag
 argument_list|,
 name|cred
+argument_list|,
+name|p
 argument_list|)
 operator|)
 return|;
@@ -557,6 +978,8 @@ argument_list|,
 argument|fflags
 argument_list|,
 argument|cred
+argument_list|,
+argument|p
 argument_list|)
 end_macro
 
@@ -581,6 +1004,14 @@ name|struct
 name|ucred
 modifier|*
 name|cred
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|proc
+modifier|*
+name|p
 decl_stmt|;
 end_decl_stmt
 
