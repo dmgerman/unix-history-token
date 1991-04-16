465fe8b784af055b09a10a057435631a2774066b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-04-16 07:25:58 -0800

    add constant for NFS_DIRBLKSIZ
    SCCS-vsn: sys/nfs/nfs.h 7.10
    
    Former-commit-id: 4a0164959750175735948973a1593233815e2d15

--- usr/src/sys/nfs/nfs.h
+++ usr/src/sys/nfs/nfs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs.h	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs.h	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -177,6 +177,17 @@ end_define
 
 begin_comment
 comment|/* Max. number async_daemons runable */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NFS_DIRBLKSIZ
+value|1024
+end_define
+
+begin_comment
+comment|/* Size of an NFS directory block */
 end_comment
 
 begin_define
