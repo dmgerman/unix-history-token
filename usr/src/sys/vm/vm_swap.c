23da1df9396e9d92080a646b2bccbd3cb237e7dc
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-04-16 07:22:23 -0800

    add proc pointers to appropriate VOP operations
    SCCS-vsn: sys/vm/vm_swap.c 7.17
    
    Former-commit-id: eb29516fb5185ab7955c02d28c372a7c763b4f5e

--- usr/src/sys/vm/vm_swap.c
+++ usr/src/sys/vm/vm_swap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vm_swap.c	7.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vm_swap.c	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_include
