e1853be88729d025fec50ca6b5ea5245df7a8e56
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-04-18 21:39:25 -0800

    date and time created 91/04/18 14:39:25 by donn
    SCCS-vsn: usr.bin/gprof/hp300.h 5.1
    
    Former-commit-id: 7ed815975f785abeafb717be32dd59b373b05df8

---
+++ usr/src/usr.bin/gprof/hp300.h
@@ -0,0 +1,42 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)hp300.h	5.1 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/*      *	offset (in bytes) of the code from the entry address of a routine.      *	(see asgnsamples for use and explanation.)      */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|OFFSET_OF_CODE
+value|0
+end_define
+
+begin_define
+define|#
+directive|define
+name|UNITS_TO_CODE
+value|(OFFSET_OF_CODE / sizeof(UNIT))
+end_define
+
+begin_enum
+enum|enum
+name|opermodes
+block|{
+name|dummy
+block|}
+enum|;
+end_enum
+
+begin_typedef
+typedef|typedef
+name|enum
+name|opermodes
+name|operandenum
+typedef|;
+end_typedef
+
+end_unit
+
