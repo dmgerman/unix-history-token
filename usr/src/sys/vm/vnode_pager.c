a5beaf1ad2a092402ddb836d0d6fc1417e1153fa
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-04-20 00:15:58 -0800

    lint
    SCCS-vsn: sys/vm/vnode_pager.c 7.4
    
    Former-commit-id: 16078fbbc137e481384fccf3117bf3ab307c2c47

--- usr/src/sys/vm/vnode_pager.c
+++ usr/src/sys/vm/vnode_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)vnode_pager.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)vnode_pager.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1009,6 +1009,7 @@ argument_list|,
 operator|(
 expr|struct
 name|vnode
+operator|*
 operator|*
 operator|)
 literal|0
