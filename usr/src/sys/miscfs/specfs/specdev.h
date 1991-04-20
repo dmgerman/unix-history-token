f5d76a29530af66cce19361455f4031963d426f9
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-04-20 00:55:07 -0800

    add struct stubs to keep prototypes happy
    SCCS-vsn: sys/miscfs/specfs/specdev.h 7.4
    
    Former-commit-id: c6700fd521798acf3d89245835432b06e781cfcf

--- usr/src/sys/miscfs/specfs/specdev.h
+++ usr/src/sys/miscfs/specfs/specdev.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)specdev.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)specdev.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -152,6 +152,36 @@ end_decl_stmt
 begin_comment
 comment|/*  * Prototypes for special file operations on vnodes.  */
 end_comment
+
+begin_struct_decl
+struct_decl|struct
+name|nameidata
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|ucred
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|flock
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|buf
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|uio
+struct_decl|;
+end_struct_decl
 
 begin_decl_stmt
 name|int
