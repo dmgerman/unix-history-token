8dc023700757455bf871be65dc6da57f74098906
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-20 02:12:33 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libcompat/4.1/ftime.c 5.3
    SCCS-vsn: lib/libcompat/4.1/getpw.c 5.3
    SCCS-vsn: lib/libcompat/4.1/gtty.c 5.3
    SCCS-vsn: lib/libcompat/4.1/stty.c 5.3
    SCCS-vsn: lib/libcompat/4.1/tell.c 5.3
    SCCS-vsn: lib/libcompat/4.1/vlimit.c 5.3
    SCCS-vsn: lib/libcompat/4.1/vtimes.c 5.4
    SCCS-vsn: lib/libcurses/curses.ext 5.6
    SCCS-vsn: lib/libmp/gcd.c 5.4
    SCCS-vsn: lib/libmp/madd.c 5.3
    SCCS-vsn: lib/libmp/mdiv.c 5.3
    SCCS-vsn: lib/libmp/mout.c 5.4
    SCCS-vsn: lib/libmp/mp.3 6.6
    SCCS-vsn: lib/libmp/msqrt.c 5.3
    SCCS-vsn: lib/libmp/mult.c 5.3
    SCCS-vsn: lib/libmp/pow.c 5.3
    SCCS-vsn: lib/libmp/util.c 5.3
    
    Former-commit-id: 555fd45d3440f7d847a211462ade75351e60d8cf

--- usr/src/lib/libmp/gcd.c
+++ usr/src/lib/libmp/gcd.c
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * %sccs.include.proprietary.c%  */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -11,15 +15,18 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)gcd.c	5.3 (Berkeley) %G%"
+literal|"@(#)gcd.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
-endif|not lint
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_include
 include|#
