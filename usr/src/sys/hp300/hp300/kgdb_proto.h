e52c1b6521ff901e01007b003b72da5b973019b6
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-21 00:35:57 -0800

    add KGDB_EXEC, KGDB_SEQ
    SCCS-vsn: sys/hp300/hp300/kgdb_proto.h 1.2
    
    Former-commit-id: 5631fa97dce33fc88b365ed9e0cbff327f160fd0

--- usr/src/sys/hp300/hp300/kgdb_proto.h
+++ usr/src/sys/hp300/hp300/kgdb_proto.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Lawrence Berkeley Laboratory,  * Berkeley, CA.  The name of the University may not be used to  * endorse or promote products derived from this software without  * specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * @(#) $Header: kgdb_proto.h,v 1.1 91/01/10 16:49:43 mccanne Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Lawrence Berkeley Laboratory,  * Berkeley, CA.  The name of the University may not be used to  * endorse or promote products derived from this software without  * specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * @(#) $Header: kgdb_proto.h,v 1.3 91/03/12 22:06:43 mccanne Exp $ (LBL)  */
 end_comment
 
 begin_comment
@@ -66,6 +66,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|KGDB_EXEC
+value|0x09
+end_define
+
+begin_define
+define|#
+directive|define
 name|KGDB_CMD
 parameter_list|(
 name|x
@@ -96,6 +103,13 @@ define|#
 directive|define
 name|KGDB_MORE
 value|0x20
+end_define
+
+begin_define
+define|#
+directive|define
+name|KGDB_SEQ
+value|0x10
 end_define
 
 end_unit
