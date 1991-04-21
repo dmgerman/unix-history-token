1dd27479dd3ae63f33b0812a6ea6a399485acfea
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-20 22:08:05 -0800

    fix includes
    SCCS-vsn: sys/kern/kern_malloc.c 7.23
    
    Former-commit-id: 5964423d2f31544fe4c0f71f383319022d05a8e5

--- usr/src/sys/kern/kern_malloc.c
+++ usr/src/sys/kern/kern_malloc.c
@@ -1,24 +1,12 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_malloc.c	7.22 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_malloc.c	7.23 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"param.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"cmap.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"time.h"
 end_include
 
 begin_include
@@ -48,19 +36,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|"../vm/vm_param.h"
+file|"vm/vm.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"../vm/vm_map.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"../vm/vm_kern.h"
+file|"vm/vm_kern.h"
 end_include
 
 begin_decl_stmt
