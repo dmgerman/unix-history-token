ab9b0ef4db9eab116a69ba0e4fa5f567c3271676
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-20 22:19:15 -0800

    set the new limit copy, not the old!
    SCCS-vsn: sys/kern/kern_resource.c 7.11
    
    Former-commit-id: b5ee9122bae4d2528428461fa975814597755781

--- usr/src/sys/kern/kern_resource.c
+++ usr/src/sys/kern/kern_resource.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_resource.c	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kern_resource.c	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1028,8 +1028,14 @@ name|maxdmap
 expr_stmt|;
 break|break;
 block|}
-operator|*
-name|alimp
+name|p
+operator|->
+name|p_rlimit
+index|[
+name|uap
+operator|->
+name|which
+index|]
 operator|=
 name|alim
 expr_stmt|;
