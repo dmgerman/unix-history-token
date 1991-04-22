4bb2a6310a3407258dc73c290dc972050412c4e2
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-04-21 22:03:15 -0800

    des_cipher takes a long, not u_long; see rev 5.6 of libc/gen/crypt.c
    SCCS-vsn: include/unistd.h 5.11
    
    Former-commit-id: d7b0afd693c2356ec5b272bfe4fa26ca9a78fb6d

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.10 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.11 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -936,7 +936,7 @@ operator|,
 name|char
 operator|*
 operator|,
-name|u_long
+name|long
 operator|,
 name|int
 operator|)
