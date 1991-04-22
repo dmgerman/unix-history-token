75c16cdbc05f6204c698c6e2d0dbf98ff9e970b1
Craig Leres <leres@ucbvax.Berkeley.EDU>
1991-04-21 20:33:39 -0800

    swapper is pid 0, not 1.
    SCCS-vsn: usr.bin/systat/fetch.c 5.4
    
    Former-commit-id: 7f37d1eadec0a3eddaf058a58bb36a21f35aae9e

--- usr/src/usr.bin/systat/fetch.c
+++ usr/src/usr.bin/systat/fetch.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)fetch.c	5.3 (Berkeley) %G%"
+literal|"@(#)fetch.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -341,7 +341,7 @@ if|if
 condition|(
 name|pid
 operator|==
-literal|1
+literal|0
 condition|)
 return|return
 operator|(
