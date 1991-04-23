79a5a9418308255118858eb904eab3fc2186e4c1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-22 22:22:41 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libplot/grn/arc.c 6.2
    SCCS-vsn: lib/libplot/grn/box.c 6.2
    SCCS-vsn: lib/libplot/grn/circle.c 6.2
    SCCS-vsn: lib/libplot/grn/close.c 6.2
    SCCS-vsn: lib/libplot/grn/cont.c 6.2
    SCCS-vsn: lib/libplot/grn/dot.c 6.2
    SCCS-vsn: lib/libplot/grn/erase.c 6.2
    SCCS-vsn: lib/libplot/grn/grnplot.h 6.2
    SCCS-vsn: lib/libplot/grn/label.c 6.2
    SCCS-vsn: lib/libplot/grn/line.c 6.2
    SCCS-vsn: lib/libplot/grn/linemod.c 6.2
    SCCS-vsn: lib/libplot/grn/move.c 6.2
    SCCS-vsn: lib/libplot/grn/open.c 6.2
    SCCS-vsn: lib/libplot/grn/point.c 6.2
    SCCS-vsn: lib/libplot/grn/space.c 6.2
    SCCS-vsn: lib/libplot/grn/subr.c 6.2
    
    Former-commit-id: 035ff89f617673acd6a86e6a7876e138da70ea45

--- usr/src/lib/libplot/grn/grnplot.h
+++ usr/src/lib/libplot/grn/grnplot.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)grnplot.h	6.1 (Berkeley) %G%  *	modified to grnplot by Brad Rubenstein 8/29/86  */
+comment|/*-  * Copyright (c) 1980, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)grnplot.h	6.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Given a plot file, produces a grn file  */
+comment|/*  * Given a plot file, produces a grn file.  *  * Modified to grnplot by Brad Rubenstein 8/29/86.  */
 end_comment
 
 begin_include
