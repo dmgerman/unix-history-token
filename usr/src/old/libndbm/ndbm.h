37093d10e4ad73eecd0e746f72518a15d6074dcd
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-22 21:49:51 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: old/libndbm/ndbm.h 5.2
    SCCS-vsn: old/libndbm/ndbm.c 5.7
    
    Former-commit-id: 1823be9d924e336137fdb7abfad9c9fcbaaaf188

--- usr/src/old/libndbm/ndbm.h
+++ usr/src/old/libndbm/ndbm.h
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)ndbm.h	5.3 (Berkeley) 5/29/90  */
+comment|/*-  * Copyright (c) 1983 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)ndbm.h	5.2 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/*  * Derived from:  *  *	@(#)ndbm.h	5.3 (Berkeley) 5/29/90  */
 end_comment
 
 begin_comment
