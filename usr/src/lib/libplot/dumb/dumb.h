cb480b7807f02f75eb5634a1335c0d8b51e6674d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-22 22:03:08 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libplot/bitgraph/arc.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/bg.h 5.2
    SCCS-vsn: lib/libplot/bitgraph/box.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/circle.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/close.c 5.4
    SCCS-vsn: lib/libplot/bitgraph/cont.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/dot.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/erase.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/label.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/line.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/linemod.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/move.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/open.c 5.4
    SCCS-vsn: lib/libplot/bitgraph/point.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/space.c 5.3
    SCCS-vsn: lib/libplot/dumb/arc.c 5.2
    SCCS-vsn: lib/libplot/dumb/box.c 5.2
    SCCS-vsn: lib/libplot/dumb/circle.c 5.2
    SCCS-vsn: lib/libplot/dumb/close.c 5.3
    SCCS-vsn: lib/libplot/dumb/cont.c 5.2
    SCCS-vsn: lib/libplot/dumb/dot.c 5.2
    SCCS-vsn: lib/libplot/dumb/dumb.h 5.2
    SCCS-vsn: lib/libplot/dumb/erase.c 5.2
    SCCS-vsn: lib/libplot/dumb/label.c 5.2
    SCCS-vsn: lib/libplot/dumb/line.c 5.2
    SCCS-vsn: lib/libplot/dumb/linemod.c 5.2
    SCCS-vsn: lib/libplot/dumb/move.c 5.2
    SCCS-vsn: lib/libplot/dumb/open.c 5.3
    SCCS-vsn: lib/libplot/dumb/point.c 5.2
    SCCS-vsn: lib/libplot/dumb/space.c 5.2
    SCCS-vsn: lib/libplot/dumb/subr.c 5.2
    SCCS-vsn: lib/libplot/f77plot/arc.c 5.2
    SCCS-vsn: lib/libplot/f77plot/box.c 5.3
    SCCS-vsn: lib/libplot/f77plot/circle.c 5.2
    SCCS-vsn: lib/libplot/f77plot/clospl.c 5.3
    SCCS-vsn: lib/libplot/f77plot/cont.c 5.2
    SCCS-vsn: lib/libplot/f77plot/dot.c 5.2
    SCCS-vsn: lib/libplot/f77plot/erase.c 5.2
    SCCS-vsn: lib/libplot/f77plot/label.c 5.2
    SCCS-vsn: lib/libplot/f77plot/line.c 5.2
    SCCS-vsn: lib/libplot/f77plot/linemd.c 5.2
    SCCS-vsn: lib/libplot/f77plot/move.c 5.2
    SCCS-vsn: lib/libplot/f77plot/open.c 5.3
    SCCS-vsn: lib/libplot/f77plot/point.c 5.2
    SCCS-vsn: lib/libplot/f77plot/space.c 5.2
    
    Former-commit-id: b9cceb63d9ad739f99b32a83ad2268ef77d897bd

--- usr/src/lib/libplot/dumb/dumb.h
+++ usr/src/lib/libplot/dumb/dumb.h
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dumb.h	5.1 (Berkeley) %G%  *  *  * This accepts plot file formats and produces the appropriate plots  * for dumb terminals.  It can also be used for printing terminals and  * lineprinter listings, although there is no way to specify number of  * lines and columns different from your terminal.  This would be easy  * to change, and is left as an exercise for the reader.  */
+comment|/*-  * Copyright (c) 1980 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)dumb.h	5.2 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/*  * This accepts plot file formats and produces the appropriate plots  * for dumb terminals.  It can also be used for printing terminals and  * lineprinter listings, although there is no way to specify number of  * lines and columns different from your terminal.  This would be easy  * to change, and is left as an exercise for the reader.  */
 end_comment
 
 begin_include
--- usr/src/lib/libplot/dumb/open.c
+++ usr/src/lib/libplot/dumb/open.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1980 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  */
 end_comment
 
 begin_ifndef
@@ -15,15 +15,18 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)open.c	5.2 (Berkeley) %G%"
+literal|"@(#)open.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
-endif|not lint
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_comment
 comment|/*  * This accepts plot file formats and produces the appropriate plots  * for dumb terminals.  It can also be used for printing terminals and  * lineprinter listings, although there is no way to specify number of  * lines and columns different from your terminal.  This would be easy  * to change, and is left as an exercise for the reader.  */
