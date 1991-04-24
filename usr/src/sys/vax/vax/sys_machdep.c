1b0b7bde7e9846da5a54fbcf0436f2269df3d019
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-04-23 19:05:34 -0800

    dkio.h defined and obsolete ioctl only of interest to hp
    SCCS-vsn: sys/vax/vax/sys_machdep.c 7.7
    
    Former-commit-id: f1c665326295540e132f3ead1e3a695a1b3c1ce2

--- usr/src/sys/vax/vax/sys_machdep.c
+++ usr/src/sys/vax/vax/sys_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sys_machdep.c	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)sys_machdep.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -61,12 +61,6 @@ begin_include
 include|#
 directive|include
 file|"sys/trace.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"dkio.h"
 end_include
 
 begin_include
