75c3cced7d8ebade84d5cc0e7ff4f2d47c4042a1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-25 15:34:11 -0800

    had to move the additional info down, scripts couldn't find the copyright
    SCCS-vsn: usr.sbin/amd/config/os-next.h 5.3
    
    Former-commit-id: 4848e4427142ac19bd8f79c7a2b1d68059aeede5

--- usr/src/usr.sbin/amd/config/os-next.h
+++ usr/src/usr.sbin/amd/config/os-next.h
@@ -1,10 +1,14 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/*  * Copyright (c) 1990 Jan-Simon Pendry  * Copyright (c) 1990 Imperial College of Science, Technology& Medicine  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry at Imperial College, London.  *  * %sccs.include.redist.c%  *  *	@(#)os-next.h	5.3 (Berkeley) %G%  */
+end_comment
+
+begin_comment
 comment|/* $Id: os-next.h,v 5.2.1.1 90/10/21 22:31:00 jsp Exp $ */
 end_comment
 
 begin_comment
-comment|/*  * NeXT OS definitions for Amd (automounter)  *  * By Bill Trost, Reed College  * trost%reed@cse.ogi.edu,  *  * Derived from the Sun 3.2 definitions for Amd (os-sos3.h).  *  * Copyright (c) 1990 Jan-Simon Pendry  * Copyright (c) 1990 Imperial College of Science, Technology& Medicine  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry at Imperial College, London.  *  * %sccs.include.redist.c%  *  *	@(#)os-next.h	5.2 (Berkeley) %G%  */
+comment|/*  * NeXT OS definitions for Amd (automounter)  *  * By Bill Trost, Reed College  * trost%reed@cse.ogi.edu,  *  * Derived from the Sun 3.2 definitions for Amd (os-sos3.h).  */
 end_comment
 
 begin_comment
