3ad5faa500ea31f5c56a2d6a01b763da19dda3f3
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-24 21:29:25 -0800

    original author Tom Truscott; add Berkeley copyright notice
    SCCS-vsn: usr.bin/uucp/uupoll/uupoll.c 5.7
    
    Former-commit-id: 8929002b5a92f2f4e8c32ca0be08cc8557105b82

--- usr/src/usr.bin/uucp/uupoll/uupoll.c
+++ usr/src/usr.bin/uucp/uupoll/uupoll.c
@@ -1,4 +1,32 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+name|char
+name|copyright
+index|[]
+init|=
+literal|"@(#) Copyright (c) 1986, 1991 The Regents of the University of California.\n\  All rights reserved.\n"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -11,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)uupoll.c	5.6	(Berkeley) %G%"
+literal|"@(#)uupoll.c	5.7 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -19,6 +47,10 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_comment
 comment|/*  * Poll named system(s).  *  * The poll occurs even if recent attempts have failed,  * but not if L.sys prohibits the call (e.g. wrong time of day).  *  * Original Author: Tom Truscott (rti!trt)  */
