6faa09cf08d5c0ea6148503638937b9837d1f7ad
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-04-29 00:18:51 -0800

    redist
    SCCS-vsn: sys/i386/stand/prf.c 7.3
    
    Former-commit-id: 89e78ec01792b0ebc3f0ec3f07e0f0486e20e9cc

--- usr/src/sys/i386/stand/prf.c
+++ usr/src/sys/i386/stand/prf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.noredist.c%  *  *	@(#)prf.c	7.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.redist.c%  *  *	@(#)prf.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
