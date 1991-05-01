128a12552e9472e9da41c86fbe1ef727ce153109
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-30 21:32:59 -0800

    move term/tab into /usr/old
    SCCS-vsn: old/roff/common_source/pathnames.h 5.5
    
    Former-commit-id: 3658c4e7fd9e9a0763c88d94a2d320cd1165f15e

--- usr/src/old/roff/common_source/pathnames.h
+++ usr/src/old/roff/common_source/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.5 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -21,7 +21,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_TERM
-value|"/usr/libdata/term/tab"
+value|"/usr/old/libdata/term/tab"
 end_define
 
 begin_define
