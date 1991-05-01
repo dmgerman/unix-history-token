6c82b661b239e87b553d8fa86e013f78b98adf7d
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-04-30 23:34:17 -0800

    DIAGNOSTIC is already defined for general error catching
    SCCS-vsn: sys/netccitt/pk.h 7.8
    SCCS-vsn: sys/netccitt/pk_subr.c 7.11
    
    Former-commit-id: ac56d03e916a540e74b5ab755b6a460fa8aacd05

--- usr/src/sys/netccitt/pk.h
+++ usr/src/sys/netccitt/pk.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk.h	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk.h	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -745,7 +745,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|DIAGNOSTIC
+name|DIAG_TYPE
 value|14 * MAXSTATES
 end_define
 
--- usr/src/sys/netccitt/pk_subr.c
+++ usr/src/sys/netccitt/pk_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk_subr.c	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk_subr.c	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3600,7 +3600,7 @@ name|X25_DIAGNOSTIC
 case|:
 name|type
 operator|=
-name|DIAGNOSTIC
+name|DIAG_TYPE
 expr_stmt|;
 break|break;
 default|default:
