6f9773fffb882f3ed847d7fac84168ede1de97e1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-04 18:02:09 -0800

    kernel has no abort() function
    SCCS-vsn: sys/hp300/include/stdarg.h 7.2
    
    Former-commit-id: fd172b29a10b8509e1463dc3b81d74c6b3d45721

--- usr/src/sys/hp300/include/stdarg.h
+++ usr/src/sys/hp300/include/stdarg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	7.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
@@ -10,6 +10,30 @@ modifier|*
 name|va_list
 typedef|;
 end_typedef
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KERNEL
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|va_arg
+parameter_list|(
+name|ap
+parameter_list|,
+name|type
+parameter_list|)
+define|\
+value|((type *)(ap += sizeof(type)))[-1]
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
 
 begin_define
 define|#
@@ -23,6 +47,11 @@ parameter_list|)
 define|\
 value|((type *)(ap += sizeof(type)< sizeof(int) ? \ 		(abort(), 0) : sizeof(type)))[-1]
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
