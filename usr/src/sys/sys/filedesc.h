f86e24415ce5179ea3eb5be30fa8d8c7cc2601cb
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-04 22:08:06 -0800

    (since 7.2:) change filedesc to point to one array of files/flags; initial allocation is in filedesc0 along with header, and expanded tables replace that one rather than augmenting it
    SCCS-vsn: sys/sys/filedesc.h 7.4
    
    Former-commit-id: b412c1aa368efe597d73c4a49f0d23e98f89a6b2

--- usr/src/sys/sys/filedesc.h
+++ usr/src/sys/sys/filedesc.h
@@ -1,33 +1,28 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)filedesc.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)filedesc.h	7.4 (Berkeley) %G%  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|_FILEDESC_H_
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|_FILEDESC_H_
-end_define
-
 begin_comment
-comment|/*  * This structure is used for the management of descriptors.  * It may be shared by multiple threads.  *  * A process is initially started out with NDFILE worth of  * descriptors, selected to be enough for typical applications  * based on the historic limit of 20 open files. Additional  * descriptors may be allocated up to a process' resource limit.  * The initial expansion is set to NOEXTENT; each time it runs out,  * it is doubled until the resource limit is reached. NOEXTENT should  * be selected to be the biggest multiple of OFILESIZE (see below)  * that will fit in a power-of-two sized piece of memory.  */
+comment|/*  * This structure is used for the management of descriptors.  It may be  * shared by multiple processes.  *  * A process is initially started out with NDFILE descriptors stored within  * this structure, selected to be enough for typical applications based on  * the historical limit of 20 open files (and the usage of descriptors by  * shells).  If these descriptors are exhausted, a larger descriptor table  * may be allocated, up to a process' resource limit; the internal arrays  * are then unused.  The initial expansion is set to NDEXTENT; each time  * it runs out, it is doubled until the resource limit is reached. NDEXTENT  * should be selected to be the biggest multiple of OFILESIZE (see below)  * that will fit in a power-of-two sized piece of memory.  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|NOEXTENT
-value|25
+name|NDFILE
+value|20
+end_define
+
+begin_define
+define|#
+directive|define
+name|NDEXTENT
+value|50
 end_define
 
 begin_comment
-comment|/* 125 bytes in 128-byte alloc. */
+comment|/* 250 bytes in 256-byte alloc. */
 end_comment
 
 begin_struct
@@ -62,11 +57,11 @@ name|int
 name|fd_nfiles
 decl_stmt|;
 comment|/* number of open files allocated */
-name|int
+name|u_short
 name|fd_lastfile
 decl_stmt|;
 comment|/* high-water mark of fd_ofiles */
-name|int
+name|u_short
 name|fd_freefile
 decl_stmt|;
 comment|/* approx. next free file */
@@ -78,6 +73,37 @@ name|u_short
 name|fd_refcnt
 decl_stmt|;
 comment|/* reference count */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * Basic allocation of descriptors:  * one of the above, plus arrays for NDFILE descriptors.  */
+end_comment
+
+begin_struct
+struct|struct
+name|filedesc0
+block|{
+name|struct
+name|filedesc
+name|fd_fd
+decl_stmt|;
+comment|/* 	 * These arrays are used when the number of open files is 	 *<= NDFILE, and are then pointed to by the pointers above. 	 */
+name|struct
+name|file
+modifier|*
+name|fd_dfiles
+index|[
+name|NDFILE
+index|]
+decl_stmt|;
+name|char
+name|fd_dfileflags
+index|[
+name|NDFILE
+index|]
+decl_stmt|;
 block|}
 struct|;
 end_struct
@@ -109,32 +135,8 @@ comment|/* mapped from device */
 end_comment
 
 begin_comment
-comment|/*  * Data structure access macros.  */
+comment|/*  * Storage required per open file descriptor.  */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|OFILE
-parameter_list|(
-name|fd
-parameter_list|,
-name|indx
-parameter_list|)
-value|((fd)->fd_ofiles[indx])
-end_define
-
-begin_define
-define|#
-directive|define
-name|OFILEFLAGS
-parameter_list|(
-name|fd
-parameter_list|,
-name|indx
-parameter_list|)
-value|((fd)->fd_ofileflags[indx])
-end_define
 
 begin_define
 define|#
@@ -143,28 +145,117 @@ name|OFILESIZE
 value|(sizeof(struct file *) + sizeof(char))
 end_define
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KERNEL
+end_ifdef
+
 begin_comment
 comment|/*  * Kernel global variables and routines.  */
 end_comment
 
-begin_function_decl
-specifier|extern
+begin_decl_stmt
+name|int
+name|fdalloc
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+name|p
+operator|,
+name|int
+name|want
+operator|,
+name|int
+operator|*
+name|result
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|fdavail
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+name|p
+operator|,
+name|int
+name|n
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|falloc
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+name|p
+operator|,
+expr|struct
+name|file
+operator|*
+operator|*
+name|resultfp
+operator|,
+name|int
+operator|*
+name|resultfd
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 name|struct
 name|filedesc
 modifier|*
 name|fdcopy
-parameter_list|()
-function_decl|;
-end_function_decl
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|fdfree
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* !_FILEDESC_H_ */
-end_comment
 
 end_unit
 
