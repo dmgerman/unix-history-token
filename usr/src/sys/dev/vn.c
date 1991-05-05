88ec37a234318fcded5d280327c496b53d7ee8bb
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-05 02:43:54 -0800

    mark as broken
    SCCS-vsn: sys/dev/vn.c 7.4
    
    Former-commit-id: 1ec68f14ad6ff64506c09390cd96225c8764a05f

--- usr/src/sys/dev/vn.c
+++ usr/src/sys/dev/vn.c
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: fd.c 1.1 90/07/09$  *  *	@(#)vn.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: fd.c 1.1 90/07/09$  *  *	@(#)vn.c	7.4 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/*  * CURRENTLY BROKEN; the name "fd" collides with /dev/fd/xxx.  * This would need to be converted to the new proc/user layout as well.  */
 end_comment
 
 begin_comment
