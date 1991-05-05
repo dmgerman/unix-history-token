2fe066a52d85db6c51f60da3b084883d187aa510
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-05 02:50:35 -0800

    don't lose bits from unit number
    SCCS-vsn: sys/hp300/dev/rd.c 7.8
    
    Former-commit-id: a9440ef765181072d62b5ae7b41a8525b63e705a

--- usr/src/sys/hp300/dev/rd.c
+++ usr/src/sys/hp300/dev/rd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: rd.c 1.3 90/10/12$  *  *	@(#)rd.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: rd.c 1.3 90/10/12$  *  *	@(#)rd.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2192,7 +2192,7 @@ name|rdunit
 parameter_list|(
 name|x
 parameter_list|)
-value|((minor(x)>> 3)& 0xf)
+value|(minor(x)>> 3)
 end_define
 
 begin_define
