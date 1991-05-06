848e50aa78bee1977e5fb719d4146ab64cc5c88c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-05 20:55:54 -0800

    nodev not referenced
    SCCS-vsn: sys/hp300/stand/cons.c 7.4
    
    Former-commit-id: 933663fadff851471b7d1904ca3ce9b2fcf959f6

--- usr/src/sys/hp300/stand/cons.c
+++ usr/src/sys/hp300/stand/cons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: cons.c 1.5 89/08/22$  *  *	@(#)cons.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: cons.c 1.5 89/08/22$  *  *	@(#)cons.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -20,13 +20,6 @@ include|#
 directive|include
 file|"../hp300/cons.h"
 end_include
-
-begin_function_decl
-name|int
-name|nodev
-parameter_list|()
-function_decl|;
-end_function_decl
 
 begin_ifdef
 ifdef|#
