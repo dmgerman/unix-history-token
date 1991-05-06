9472802cd26b330bdc89ccf3ea29f767b50dc1bc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-06 01:06:03 -0800

    from scratch, Berkeley specific copyright
    SCCS-vsn: sys/sys/timeb.h 7.2
    
    Former-commit-id: 0aab285c24e3394263bd4de29e1cdd9acda6e979

--- usr/src/sys/sys/timeb.h
+++ usr/src/sys/sys/timeb.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)timeb.h	7.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)timeb.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Structure returned by ftime system call  */
+comment|/* The ftime(2) system call structure -- deprecated. */
 end_comment
 
 begin_struct
@@ -14,16 +14,20 @@ block|{
 name|time_t
 name|time
 decl_stmt|;
+comment|/* seconds since the Epoch */
 name|unsigned
 name|short
 name|millitm
 decl_stmt|;
+comment|/* + milliseconds since the Epoch */
 name|short
 name|timezone
 decl_stmt|;
+comment|/* minutes west of CUT */
 name|short
 name|dstflag
 decl_stmt|;
+comment|/* DST == non-zero */
 block|}
 struct|;
 end_struct
