d504fb1b33400c3c58655160b4a56d1dbd5631fa
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-05 16:01:21 -0800

    proc 0 has no p_pptr
    SCCS-vsn: sys/kern/kern_sysctl.c 7.15
    
    Former-commit-id: b82dc542717d1a08ebae79e3c4f045c9d4d6998b

--- usr/src/sys/kern/kern_sysctl.c
+++ usr/src/sys/kern/kern_sysctl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_sysctl.c	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_sysctl.c	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -925,6 +925,12 @@ name|p
 operator|->
 name|p_vmspace
 expr_stmt|;
+if|if
+condition|(
+name|p
+operator|->
+name|p_pptr
+condition|)
 name|ep
 operator|->
 name|e_ppid
@@ -934,6 +940,13 @@ operator|->
 name|p_pptr
 operator|->
 name|p_pid
+expr_stmt|;
+else|else
+name|ep
+operator|->
+name|e_ppid
+operator|=
+literal|0
 expr_stmt|;
 name|ep
 operator|->
