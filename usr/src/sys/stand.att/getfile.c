1b7db1863f5d632dc5252fd74a3d3519e37b7cb1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-05 21:27:11 -0800

    fix includes
    SCCS-vsn: sys/stand.att/getfile.c 7.6
    
    Former-commit-id: 4eca5c8f5f69bc617503a07a86b5e7b93f434e80

--- usr/src/sys/stand.att/getfile.c
+++ usr/src/sys/stand.att/getfile.c
@@ -1,18 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)getfile.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)getfile.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
-file|"sys/param.h"
+file|<sys/param.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|"sys/time.h"
+file|<sys/time.h>
 end_include
 
 begin_include
