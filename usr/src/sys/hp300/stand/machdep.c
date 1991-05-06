376debba9127211594adfe4cb71b09741146c411
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-05 20:55:29 -0800

    use common nodev() routne -- note, it returns -1
    SCCS-vsn: sys/hp300/stand/machdep.c 7.4
    
    Former-commit-id: 220e80eefa8fbf40c75ba206fb57f0f88893b45d

--- usr/src/sys/hp300/stand/machdep.c
+++ usr/src/sys/hp300/stand/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.6 88/05/24$  *  *	@(#)machdep.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.6 88/05/24$  *  *	@(#)machdep.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -350,21 +350,6 @@ name|intrap
 operator|=
 literal|0
 expr_stmt|;
-block|}
-end_block
-
-begin_macro
-name|nodev
-argument_list|()
-end_macro
-
-begin_block
-block|{
-return|return
-operator|(
-literal|0
-operator|)
-return|;
 block|}
 end_block
 
