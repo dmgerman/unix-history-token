63160b949514b110cd7a30b53f0574757ecd349d
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-06 17:48:15 -0800

    argdev is gone
    SCCS-vsn: sys/nfs/nfs_vfsops.c 7.31
    
    Former-commit-id: 6428ed698a5cfb190aea32d94e0dbcc4bc090d4b

--- usr/src/sys/nfs/nfs_vfsops.c
+++ usr/src/sys/nfs/nfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vfsops.c	7.30 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vfsops.c	7.31 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -977,15 +977,6 @@ literal|0
 index|]
 operator|.
 name|sw_vp
-operator|=
-name|vp
-expr_stmt|;
-name|VREF
-argument_list|(
-name|vp
-argument_list|)
-expr_stmt|;
-name|argdev_vp
 operator|=
 name|vp
 expr_stmt|;
