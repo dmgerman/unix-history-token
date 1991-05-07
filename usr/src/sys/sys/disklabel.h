9a2f57900b922484248f2271456ea4ed9b1283f4
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-07 14:09:14 -0800

    MAXPARTITIONS enforced differently on i386; no need for bigger const
    SCCS-vsn: sys/sys/disklabel.h 7.19
    
    Former-commit-id: dd710b982c77ec3eaff70b1ff9c81f5c54593c73

--- usr/src/sys/sys/disklabel.h
+++ usr/src/sys/sys/disklabel.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)disklabel.h	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)disklabel.h	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -60,13 +60,6 @@ end_define
 begin_comment
 comment|/* offset of label in sector */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|MAXPARTITIONS
-value|32
-end_define
 
 begin_endif
 endif|#
