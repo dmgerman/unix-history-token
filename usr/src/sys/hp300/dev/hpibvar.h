4fccdb3833020d9da3ac7b5a2fa8048f277885db
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1991-05-07 16:50:11 -0800

    internalhpib now caddr_t
    SCCS-vsn: sys/hp300/dev/hpibvar.h 7.3
    
    Former-commit-id: 475ead000da0ce7855a6c81a7df0f93505921e8e

--- usr/src/sys/hp300/dev/hpibvar.h
+++ usr/src/sys/hp300/dev/hpibvar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)hpibvar.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)hpibvar.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -245,7 +245,7 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|extern
-name|int
+name|caddr_t
 name|internalhpib
 decl_stmt|;
 end_decl_stmt
