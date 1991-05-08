b2e6fe3244b47c28d97f94e6d7f8a48903a63734
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-07 19:06:34 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/net/slcompress.c 7.7
    
    Former-commit-id: 2ff3e9215436ff54a2a3634f0c07bc08297dccd4

--- usr/src/sys/net/slcompress.c
+++ usr/src/sys/net/slcompress.c
@@ -1,38 +1,11 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	slcompress.c	7.6	90/06/28	*/
+comment|/*-  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)slcompress.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Routines to compress and uncompess tcp packets (for transmission  * over low speed serial lines.  *  * Copyright (c) 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	Van Jacobson (van@helios.ee.lbl.gov), Dec 31, 1989:  *	- Initial distribution.  */
+comment|/*  * Routines to compress and uncompess tcp packets (for transmission  * over low speed serial lines.  *  * Van Jacobson (van@helios.ee.lbl.gov), Dec 31, 1989:  *	- Initial distribution.  *  * static char rcsid[] =  * "$Header: slcompress.c,v 1.19 89/12/31 08:52:59 van Exp $";  */
 end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
-begin_decl_stmt
-specifier|static
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$Header: slcompress.c,v 1.19 89/12/31 08:52:59 van Exp $"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_include
-include|#
-directive|include
-file|<sys/types.h>
-end_include
 
 begin_include
 include|#
