006a62ea96071e412302fda6bf7d9eef63b85f17
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-07 21:32:20 -0800

    errno doesn't need to be masked to u_char
    SCCS-vsn: sys/hp300/hp300/trap.c 7.13
    
    Former-commit-id: 99fde840f29de16d06f8febf6079efc171a4b22a

--- usr/src/sys/hp300/hp300/trap.c
+++ usr/src/sys/hp300/hp300/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.c 1.32 91/04/06$  *  *	@(#)trap.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.c 1.32 91/04/06$  *  *	@(#)trap.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1870,9 +1870,6 @@ index|[
 name|D0
 index|]
 operator|=
-operator|(
-name|u_char
-operator|)
 name|error
 expr_stmt|;
 name|frame
@@ -2070,9 +2067,6 @@ index|[
 name|D0
 index|]
 operator|=
-operator|(
-name|u_char
-operator|)
 name|error
 expr_stmt|;
 name|frame
