c62daf05b7f9ce3b8c197f95e4165d2baa60bccf
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1991-05-07 17:01:44 -0800

    get rid of dumplo definition (this may come back)
    SCCS-vsn: sys/hp300/hp300/swapgeneric.c 7.5
    
    Former-commit-id: 344204408dcc48c5fe098efec41309b648b6deef

--- usr/src/sys/hp300/hp300/swapgeneric.c
+++ usr/src/sys/hp300/hp300/swapgeneric.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)swapgeneric.c	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)swapgeneric.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -97,12 +97,6 @@ block|,
 literal|0
 block|}
 block|, }
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|long
-name|dumplo
 decl_stmt|;
 end_decl_stmt
 
