b4d31bda883eb884b4a00ff383469ad8e3ef861f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-08 21:45:08 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/vax/consolerl/Makefile 7.2
    SCCS-vsn: sys/vax/include/clock.h 7.3
    SCCS-vsn: sys/vax/include/frame.h 7.2
    SCCS-vsn: sys/vax/include/kdbparam.h 7.4
    SCCS-vsn: sys/vax/include/mtpr.h 7.6
    SCCS-vsn: sys/vax/include/param.h 7.19
    SCCS-vsn: sys/vax/include/pcb.h 7.2
    SCCS-vsn: sys/vax/include/psl.h 7.3
    SCCS-vsn: sys/vax/include/pte.h 7.7
    SCCS-vsn: sys/vax/include/reg.h 7.3
    SCCS-vsn: sys/vax/include/trap.h 7.4
    SCCS-vsn: sys/vax/include/vmparam.h 7.4
    SCCS-vsn: sys/tahoe/include/kdbparam.h 7.9
    SCCS-vsn: sys/vax/inline/inline.h 7.2
    SCCS-vsn: sys/vax/inline/langpats.c 7.3
    SCCS-vsn: sys/vax/inline/libcpats.c 7.2
    SCCS-vsn: sys/vax/inline/machdep.c 7.2
    SCCS-vsn: sys/vax/inline/machpats.c 7.4
    SCCS-vsn: sys/vax/inline/main.c 7.2
    SCCS-vsn: sys/vax/mba/hp.c 7.23
    SCCS-vsn: sys/vax/mba/hpreg.h 7.2
    SCCS-vsn: sys/vax/mba/ht.c 7.14
    SCCS-vsn: sys/vax/mba/htreg.h 7.3
    SCCS-vsn: sys/vax/mba/mba.c 7.6
    SCCS-vsn: sys/vax/mba/mbareg.h 7.2
    SCCS-vsn: sys/vax/mba/mbavar.h 7.2
    SCCS-vsn: sys/vax/mba/mt.c 7.10
    SCCS-vsn: sys/vax/mba/mtreg.h 7.3
    
    Former-commit-id: cec9bd4b6e7f77a10f976e35a7bd3ac839888c2c

--- usr/src/sys/vax/inline/main.c
+++ usr/src/sys/vax/inline/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1984, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1984, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -14,15 +14,18 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1984, 1986 Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1984, 1986 The Regents of the University of California.\n\  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
-endif|not lint
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_ifndef
 ifndef|#
@@ -36,15 +39,18 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)main.c	7.1 (Berkeley) %G%"
+literal|"@(#)main.c	7.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
 begin_endif
 endif|#
 directive|endif
-endif|not lint
 end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_include
 include|#
