682e2d9c02f928abbef5313c952193aa0d5e373c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-09 02:45:27 -0800

    new format for prototypes; more precise comments
    SCCS-vsn: sys/ufs/ffs/ufsmount.h 7.9
    SCCS-vsn: sys/ufs/ufs/ufsmount.h 7.9
    
    Former-commit-id: 7de0b0697a67c7d128b08e51d36e492224fd3d88

--- usr/src/sys/ufs/ufs/ufsmount.h
+++ usr/src/sys/ufs/ufs/ufsmount.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufsmount.h	7.8 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufsmount.h	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Mount structure.  * One allocated on every mount.  * Used to find the super block.  */
+comment|/*  * This structure describes the UFS specific mount structure data.  */
 end_comment
 
 begin_struct
