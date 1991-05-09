a49158544ea5b4e79df099977c9efb3630c75e8d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-08 21:27:49 -0800

    sccs id got lost along the way
    SCCS-vsn: sys/tahoe/vba/cy.c 7.8
    
    Former-commit-id: b7b5a02511b59ec3a768b80d01467eb46d77222f

--- usr/src/sys/tahoe/vba/cy.c
+++ usr/src/sys/tahoe/vba/cy.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Computer Consoles Inc.  *  * %sccs.include.redist.c%  *  *	@(#)cy.c	7.4 (Berkeley) 5/5/89  */
+comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Computer Consoles Inc.  *  * %sccs.include.redist.c%  *  *  *	@(#)cy.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
