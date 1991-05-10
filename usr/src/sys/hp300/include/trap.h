9d79f87dcd29b9a2dec538a3f19e1d04a69add4e
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-10 01:27:07 -0800

    add T_USER
    SCCS-vsn: sys/hp300/include/trap.h 7.5
    
    Former-commit-id: 9854fda64890d611386116ddd16cdbf204b8ca28

--- usr/src/sys/hp300/include/trap.h
+++ usr/src/sys/hp300/include/trap.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.h 1.1 90/07/09$  *  *	@(#)trap.h	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.h 1.1 90/07/09$  *  *	@(#)trap.h	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -111,6 +111,17 @@ directive|define
 name|T_TRAP15
 value|15
 end_define
+
+begin_define
+define|#
+directive|define
+name|T_USER
+value|0x80
+end_define
+
+begin_comment
+comment|/* user-mode flag or'ed with type */
+end_comment
 
 end_unit
 
