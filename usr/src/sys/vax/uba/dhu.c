8d72b38d2d4877eefb3b524bdd2906ea31318b6a
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-05-09 20:09:01 -0800

    /sys/sys/bkmac.h is gone
    SCCS-vsn: sys/vax/uba/dhu.c 7.15
    SCCS-vsn: sys/vax/uba/dh.c 7.16
    SCCS-vsn: sys/vax/uba/dmf.c 7.14
    SCCS-vsn: sys/vax/uba/dmx.c 7.6
    SCCS-vsn: sys/vax/uba/dmz.c 7.11
    SCCS-vsn: sys/vax/uba/dz.c 7.13
    
    Former-commit-id: b475b85b6f03149e5f60dbaf01c3d31518003ded

--- usr/src/sys/vax/uba/dhu.c
+++ usr/src/sys/vax/uba/dhu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dhu.c	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1985, 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)dhu.c	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -125,12 +125,6 @@ begin_include
 include|#
 directive|include
 file|"dhureg.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"sys/bkmac.h"
 end_include
 
 begin_include
