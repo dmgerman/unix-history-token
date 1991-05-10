cb51301d7c4cf94584679d63496540ca21f3ecde
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-05-09 18:46:27 -0800

    change name of file
    SCCS-vsn: sys/netiso/clnp_debug.c 7.7
    
    Former-commit-id: dc0fc193bf426a6b617cac527fa3a0eeea591a71

--- usr/src/sys/netiso/clnp_debug.c
+++ usr/src/sys/netiso/clnp_debug.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)clnp_debug.c	7.6 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)clnp_debug.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1141,7 +1141,7 @@ end_function
 begin_include
 include|#
 directive|include
-file|"subr_prf.sprintf"
+file|"clnp_sprintf.c"
 end_include
 
 begin_endif
