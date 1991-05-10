c2e8bc0e026890e105cf8b23afb1d8ec870cd274
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-10 02:26:23 -0800

    (no message)
    SCCS-vsn: sys/i386/i386/mem.c 7.2
    
    Former-commit-id: 34bbb61978b5c97578ccd61bd7a314fd302c74c3

--- usr/src/sys/i386/i386/mem.c
+++ usr/src/sys/i386/i386/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * %sccs.include.noredist.c%  *  * from: Utah $Hdr: mem.c 1.13 89/10/08$  *	@(#)mem.c	7.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: mem.c 1.13 89/10/08$  *	@(#)mem.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
