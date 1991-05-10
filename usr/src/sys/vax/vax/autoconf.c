ffacc134a6b93042735ef701f2cb3b2fbabe0b44
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-10 03:11:16 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/vax/vax/autoconf.c 7.20
    SCCS-vsn: sys/vax/vax/conf.c 7.18
    SCCS-vsn: sys/vax/vax/cpudata.c 7.7
    SCCS-vsn: sys/vax/vax/crl.c 7.5
    SCCS-vsn: sys/vax/vax/dkbad.c 7.3
    SCCS-vsn: sys/vax/vax/genassym.c 7.11
    SCCS-vsn: sys/vax/vax/ioa.h 7.3
    SCCS-vsn: sys/vax/vax/ka630.c 7.8
    SCCS-vsn: sys/vax/vax/ka630.h 7.4
    SCCS-vsn: sys/vax/vax/ka730.c 7.4
    SCCS-vsn: sys/vax/vax/ka750.c 7.4
    SCCS-vsn: sys/vax/vax/ka780.c 7.4
    SCCS-vsn: sys/vax/vax/nexus.h 7.3
    SCCS-vsn: sys/vax/vax/rsp.h 7.2
    SCCS-vsn: sys/vax/vax/swapgeneric.c 7.11
    
    Former-commit-id: 393e7d70ed3c61f0dad7f03ae2d1c59b4f147817

--- usr/src/sys/vax/vax/autoconf.c
+++ usr/src/sys/vax/vax/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982,1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)autoconf.c	7.19 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)autoconf.c	7.20 (Berkeley) %G%  */
 end_comment
 
 begin_comment
