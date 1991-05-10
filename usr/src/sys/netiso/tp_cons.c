2eeae5eb41498c83a3c95fe73625a152179081e7
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-05-10 04:16:09 -0800

    stupid mistake concering (a & b) == 0 forgetting () needed.
    SCCS-vsn: sys/netiso/tp_cons.c 7.8
    
    Former-commit-id: 498ab5725c8919300f34cce3036d59ede1a63e2c

--- usr/src/sys/netiso/tp_cons.c
+++ usr/src/sys/netiso/tp_cons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tp_cons.c	7.7 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tp_cons.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -737,11 +737,13 @@ literal|0
 return|;
 if|if
 condition|(
+operator|(
 name|m
 operator|->
 name|m_flags
 operator|&
 name|M_PKTHDR
+operator|)
 operator|==
 literal|0
 condition|)
