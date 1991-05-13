4b112fdb015ad44e410e17be064aa64cb7133802
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-13 00:30:15 -0800

    stack location wrong (still uses non-mach "exclusive" bound
    SCCS-vsn: sys/i386/include/vmparam.h 5.9
    
    Former-commit-id: d4c2915e05fad33cc3dca9a239cda14f25ac26ad

--- usr/src/sys/i386/include/vmparam.h
+++ usr/src/sys/i386/include/vmparam.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.redist.c%  *  *	@(#)vmparam.h	5.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.redist.c%  *  *	@(#)vmparam.h	5.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -22,7 +22,7 @@ begin_define
 define|#
 directive|define
 name|USRSTACK
-value|0xFDBFD000
+value|0xFDBFE000
 end_define
 
 begin_define
