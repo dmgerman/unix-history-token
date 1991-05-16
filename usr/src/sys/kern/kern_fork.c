10cead62cf5f862779e68899271c67089bef9c0e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-16 04:02:44 -0800

    need to set reference count on copied credential
    SCCS-vsn: sys/kern/kern_fork.c 7.29
    
    Former-commit-id: 31e368b90aead64c1215cb8d60a9c6bb68d0b99c

--- usr/src/sys/kern/kern_fork.c
+++ usr/src/sys/kern/kern_fork.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_fork.c	7.28 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_fork.c	7.29 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -736,6 +736,14 @@ operator|->
 name|p_cred
 argument_list|)
 argument_list|)
+expr_stmt|;
+name|p2
+operator|->
+name|p_cred
+operator|->
+name|p_refcnt
+operator|=
+literal|1
 expr_stmt|;
 name|crhold
 argument_list|(
