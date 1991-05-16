4a0e1cb6759fc423afd8bc86757213aa1fffbf05
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-16 03:28:57 -0800

    add global variable that can be patched to let buufer pool be read
    SCCS-vsn: sys/vm/vm_glue.c 7.8
    
    Former-commit-id: 2fecbff5fd980f439b453ae1106ba2abef36db27

--- usr/src/sys/vm/vm_glue.c
+++ usr/src/sys/vm/vm_glue.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_glue.c	7.7 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_glue.c	7.8 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_include
@@ -79,6 +79,18 @@ end_decl_stmt
 
 begin_comment
 comment|/* XXX */
+end_comment
+
+begin_decl_stmt
+name|int
+name|readbuffers
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* XXX allow kgdb to read kernel buffer pool */
 end_comment
 
 begin_macro
@@ -161,6 +173,9 @@ expr_stmt|;
 comment|/* 	 * XXX there are still some things (e.g. the buffer cache) that 	 * are managed behind the VM system's back so even though an 	 * address is accessible in the mind of the VM system, there may 	 * not be physical pages where the VM thinks there is.  This can 	 * lead to bogus allocation of pages in the kernel address space 	 * or worse, inconsistencies at the pmap level.  We only worry 	 * about the buffer cache for now. 	 */
 if|if
 condition|(
+operator|!
+name|readbuffers
+operator|&&
 name|rv
 operator|&&
 operator|(
