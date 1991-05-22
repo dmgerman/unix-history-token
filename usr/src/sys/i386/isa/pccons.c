088290b45f86407db113a8e9d07f00d6ba10b3bb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-21 17:43:14 -0800

    format polic
    SCCS-vsn: sys/i386/isa/pccons.c 5.11
    
    Former-commit-id: a32b85060032c14dc41e6b99f3fc6810c80ebf12

--- usr/src/sys/i386/isa/pccons.c
+++ usr/src/sys/i386/isa/pccons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Added support for ibmpc term type and improved keyboard support. -Don Ahn  *  * %sccs.include.redist.c%  *  *	@(#)pccons.c	5.10 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * %sccs.include.redist.c%  *  *	@(#)pccons.c	5.11 (Berkeley) %G%  */
 end_comment
 
 begin_comment
