479d435bfa5245435ade02bf223a417ea4bc15ab
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-29 00:34:17 -0800

    add check for lock count
    SCCS-vsn: sys/hp300/hp300/trap.c 7.14.1.2
    
    Former-commit-id: debb50f95fa44cfa79caef67a1bbe5ce343679cc

--- usr/src/sys/hp300/hp300/trap.c
+++ usr/src/sys/hp300/hp300/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.c 1.32 91/04/06$  *  *	@(#)trap.c	7.14.1.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.c 1.32 91/04/06$  *  *	@(#)trap.c	7.14.1.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2035,6 +2035,20 @@ condition|)
 name|panic
 argument_list|(
 literal|"syscall: STARTSAVE"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|curproc
+operator|->
+name|p_spare
+index|[
+literal|2
+index|]
+condition|)
+name|panic
+argument_list|(
+literal|"syscall: LOCK COUNT"
 argument_list|)
 expr_stmt|;
 endif|#
