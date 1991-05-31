97ef38f1ac9728d06a0a2bd467c5a8aec47edbe3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-30 22:27:32 -0800

    make file flags consistent
    SCCS-vsn: sys/hp/dev/hil.c 7.8
    
    Former-commit-id: 3bc91d48a1c760425a5de0ae433e8d152285226c

--- usr/src/sys/hp/dev/hil.c
+++ usr/src/sys/hp/dev/hil.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hil.c 1.33 89/12/22$  *  *	@(#)hil.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hil.c 1.33 89/12/22$  *  *	@(#)hil.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -623,7 +623,7 @@ if|if
 condition|(
 name|flags
 operator|&
-name|FNDELAY
+name|FNONBLOCK
 condition|)
 name|dptr
 operator|->
