f3f9928cec1ab6287fb3477d913bd90248398b44
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-06-07 02:39:39 -0800

    this version now supports IP over X.25; changes include setting the X25_MBS_HOLD  flag and preserving flags through incoming call cloning and aassociation
    SCCS-vsn: sys/netccitt/if_x25subr.c 7.13
    SCCS-vsn: sys/netccitt/pk_subr.c 7.16
    SCCS-vsn: sys/netccitt/pk_input.c 7.13
    
    Former-commit-id: daccb196776dd6fe5a24f4540b6acb0cee65563a

--- usr/src/sys/netccitt/pk_input.c
+++ usr/src/sys/netccitt/pk_input.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk_input.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)pk_input.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3476,6 +3476,7 @@ name|x25_opts
 operator|.
 name|op_flags
 operator||=
+operator|(
 name|sxp
 operator|->
 name|x25_opts
@@ -3484,6 +3485,11 @@ name|op_flags
 operator|&
 operator|~
 name|X25_REVERSE_CHARGE
+operator|)
+operator||
+name|l
+operator|->
+name|lcd_flags
 expr_stmt|;
 name|pk_assoc
 argument_list|(
