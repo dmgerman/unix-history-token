761f9041e7d5c3e2f91831f62dd3f0980b816044
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-06-21 17:54:58 -0800

    quotaoff now needs a proc pointer
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.55
    SCCS-vsn: sys/ufs/ffs/ufs_vfsops.c 7.55
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.55
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 7.55
    
    Former-commit-id: 6e09a20dceddf72a983ae564e4983715b6a49f1e

--- usr/src/sys/ufs/ffs/ufs_vfsops.c
+++ usr/src/sys/ufs/ffs/ufs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	7.54 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	7.55 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2241,6 +2241,8 @@ condition|)
 continue|continue;
 name|quotaoff
 argument_list|(
+name|p
+argument_list|,
 name|mp
 argument_list|,
 name|i
@@ -2646,6 +2648,8 @@ name|error
 operator|=
 name|quotaoff
 argument_list|(
+name|p
+argument_list|,
 name|mp
 argument_list|,
 name|type
