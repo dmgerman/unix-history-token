d76e58441efd27f01dcecc064ae208d9c7b4b21b
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-06-26 05:25:49 -0800

    arp now runs at splnet() to merge arp and routing tables.
    SCCS-vsn: sys/hp300/hp300/machdep.c 7.17
    
    Former-commit-id: b27a7dc8a55312b67ddd935d5dc4b071b38c80ae

--- usr/src/sys/hp300/hp300/machdep.c
+++ usr/src/sys/hp300/hp300/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.63 91/04/24$  *  *	@(#)machdep.c	7.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.63 91/04/24$  *  *	@(#)machdep.c	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -4925,6 +4925,30 @@ block|{
 ifdef|#
 directive|ifdef
 name|INET
+if|if
+condition|(
+name|netisr
+operator|&
+operator|(
+literal|1
+operator|<<
+name|NETISR_ARP
+operator|)
+condition|)
+block|{
+name|netisr
+operator|&=
+operator|~
+operator|(
+literal|1
+operator|<<
+name|NETISR_ARP
+operator|)
+expr_stmt|;
+name|arpintr
+argument_list|()
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|netisr
