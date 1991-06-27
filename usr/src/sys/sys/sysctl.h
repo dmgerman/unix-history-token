eecd8a3ca80cec173742e25e4bc00f2a7347379e
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1991-06-26 20:58:53 -0800

    KINFO_FILE
    SCCS-vsn: sys/sys/sysctl.h 7.9
    
    Former-commit-id: 4c63bffcef9061596456e50956bcd0f251b02b4e

--- usr/src/sys/sys/sysctl.h
+++ usr/src/sys/sys/sysctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sysctl.h	7.8 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sysctl.h	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -167,6 +167,17 @@ define|#
 directive|define
 name|KINFO_VNODE
 value|(2<<8)
+end_define
+
+begin_comment
+comment|/*  * file structures  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|KINFO_FILE
+value|(3<<8)
 end_define
 
 begin_comment
