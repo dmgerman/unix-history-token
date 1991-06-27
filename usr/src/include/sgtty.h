8ac571fc686f092413edd604704b2f647ba7d52a
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1991-06-26 18:40:35 -0800

    don't test for _IOCTL_H_
    SCCS-vsn: include/sgtty.h 5.2
    
    Former-commit-id: 1d392d18d12921bbd68b3d40593cee75a641fe11

--- usr/src/include/sgtty.h
+++ usr/src/include/sgtty.h
@@ -1,13 +1,7 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sgtty.h	5.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1985 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sgtty.h	5.2 (Berkeley) %G%  */
 end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|_IOCTL_H_
-end_ifndef
 
 begin_ifndef
 ifndef|#
@@ -31,15 +25,6 @@ include|#
 directive|include
 file|<sys/ioctl.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* !_IOCTL_H_ */
-end_comment
 
 end_unit
 
