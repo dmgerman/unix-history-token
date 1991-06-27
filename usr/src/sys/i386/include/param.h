f6091e13e46a2f1270ac63e6e59595d2d619d5d0
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-06-26 17:52:06 -0800

    add align macro
    SCCS-vsn: sys/i386/include/param.h 5.7
    
    Former-commit-id: e64b874a4dd2285b68e6eb1290180127b8153874

--- usr/src/sys/i386/include/param.h
+++ usr/src/sys/i386/include/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.redist.c%  *  *	@(#)param.h	5.6 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * %sccs.include.redist.c%  *  *	@(#)param.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -12,6 +12,20 @@ define|#
 directive|define
 name|MACHINE
 value|"i386"
+end_define
+
+begin_comment
+comment|/*  * Round p (pointer or byte index) up to a correctly-aligned value  * for all data types (int, long, ...).   The result is u_int and  * must be cast to any desired pointer type.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ALIGN
+parameter_list|(
+name|p
+parameter_list|)
+value|(((u_int)(p) + (sizeof(int) - 1))&~ (sizeof(int) - 1))
 end_define
 
 begin_define
