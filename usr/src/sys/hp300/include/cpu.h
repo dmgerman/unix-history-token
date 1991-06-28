6f74a70f16685c6e6f0a554f57171b42f09630b1
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-06-28 01:01:47 -0800

    add cpu_wait()
    SCCS-vsn: sys/hp300/include/cpu.h 7.7
    
    Former-commit-id: 3a86d3938f6040be4ef63eb172742b6007ec5362

--- usr/src/sys/hp300/include/cpu.h
+++ usr/src/sys/hp300/include/cpu.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: cpu.h 1.16 91/03/25$  *  *	@(#)cpu.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: cpu.h 1.16 91/03/25$  *  *	@(#)cpu.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -69,6 +69,19 @@ begin_define
 define|#
 directive|define
 name|cpu_exec
+parameter_list|(
+name|p
+parameter_list|)
+end_define
+
+begin_comment
+comment|/* nothing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|cpu_wait
 parameter_list|(
 name|p
 parameter_list|)
