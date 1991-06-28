881054b82b5c8e1d6e109440194c4eb6ff6cdaf8
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-06-28 06:02:18 -0800

    make compile under new and old vm system
    SCCS-vsn: sys/netiso/if_eon.c 7.16
    
    Former-commit-id: faac5fc39f1f2768ae69d1ebd18b219f4cb25deb

--- usr/src/sys/netiso/if_eon.c
+++ usr/src/sys/netiso/if_eon.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_eon.c	7.15 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_eon.c	7.16 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -201,6 +201,14 @@ specifier|extern
 name|struct
 name|timeval
 name|time
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|struct
+name|ifnet
+name|loif
 decl_stmt|;
 end_decl_stmt
 
@@ -619,11 +627,6 @@ block|{
 name|struct
 name|mbuf
 name|mhead
-decl_stmt|;
-specifier|extern
-name|struct
-name|ifnet
-name|loif
 decl_stmt|;
 specifier|register
 name|struct
