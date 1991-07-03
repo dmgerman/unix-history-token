422d0f6dd7765d1cd744730288366c979611e233
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-07-03 01:21:59 -0800

    initial rewritten version from Donn Seeley
    SCCS-vsn: sbin/init/init.c 6.1
    SCCS-vsn: sbin/init/pathnames.h 6.1
    
    Former-commit-id: f5e371e1d4c71e4a2ccc6884e6d4075de247ead7

--- usr/src/sbin/init/pathnames.h
+++ usr/src/sbin/init/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Donn Seeley at UUNET Technologies, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	6.1 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -12,15 +12,15 @@ end_include
 begin_define
 define|#
 directive|define
-name|_PATH_RC
-value|"/etc/rc"
+name|_PATH_SLOGGER
+value|"/sbin/session_logger"
 end_define
 
 begin_define
 define|#
 directive|define
-name|_PATH_RCDOWN
-value|"/etc/rc.shutdown"
+name|_PATH_RUNCOM
+value|"/etc/rc"
 end_define
 
 end_unit
