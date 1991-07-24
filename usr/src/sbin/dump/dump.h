9d84f53716df1b8154d5f7b776745967acc9c8ab
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-07-24 04:09:16 -0800

    use POSIX constant NAME_MAX rather than MAXNAMLEN from dir.h
    SCCS-vsn: sbin/dump/dump.h 5.17
    
    Former-commit-id: 8982369c472f31e329c36065b0d3fb1e2c7f9dc2

--- usr/src/sbin/dump/dump.h
+++ usr/src/sbin/dump/dump.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1980 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dump.h	5.16 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1980 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dump.h	5.17 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -692,7 +692,7 @@ block|{
 name|char
 name|dd_name
 index|[
-name|MAXNAMLEN
+name|NAME_MAX
 operator|+
 literal|3
 index|]
