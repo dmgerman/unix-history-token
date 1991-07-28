507e11fc3dd3c7a92f56627e268d5b281adccf3d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-07-27 17:55:20 -0800

    add Lance Visser to contrib list
    SCCS-vsn: bin/dd/args.c 5.2
    SCCS-vsn: bin/dd/conv.c 5.2
    SCCS-vsn: bin/dd/conv_tab.c 5.2
    SCCS-vsn: bin/dd/dd.c 5.5
    SCCS-vsn: bin/dd/dd.h 5.2
    SCCS-vsn: bin/dd/extern.h 5.2
    SCCS-vsn: bin/dd/misc.c 5.2
    SCCS-vsn: bin/dd/position.c 5.2
    
    Former-commit-id: e26bf078c293dd2435572676f91d6b725e8def8b

--- usr/src/bin/dd/dd.c
+++ usr/src/bin/dd/dd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego and Lance  * Visser of Convex Computer Corporation.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)dd.c	5.4 (Berkeley) %G%"
+literal|"@(#)dd.c	5.5 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
