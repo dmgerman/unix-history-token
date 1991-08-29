7d22c2184b1dc273a8378edbd6588bf927d3c73d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-08-28 22:40:28 -0800

    vm_stat is merged into vmmeter structure
    SCCS-vsn: sys/vm/vm_page.c 7.8
    SCCS-vsn: sys/vm/vm_fault.c 7.8
    SCCS-vsn: sys/vm/vm_param.h 7.4
    
    Former-commit-id: 472b516c2896800c6295a440266b3e9c22c330a0

--- usr/src/sys/vm/vm_fault.c
+++ usr/src/sys/vm/vm_fault.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_fault.c	7.7 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_fault.c	7.8 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -122,9 +122,9 @@ decl_stmt|;
 name|vm_object_t
 name|next_object
 decl_stmt|;
-name|vm_stat
+name|cnt
 operator|.
-name|faults
+name|v_vm_faults
 operator|++
 expr_stmt|;
 comment|/* needs lock XXX */
@@ -500,14 +500,14 @@ name|inactive
 operator|=
 name|FALSE
 expr_stmt|;
-name|vm_stat
+name|cnt
 operator|.
-name|inactive_count
+name|v_inactive_count
 operator|--
 expr_stmt|;
-name|vm_stat
+name|cnt
 operator|.
-name|reactivations
+name|v_reactivated
 operator|++
 expr_stmt|;
 block|}
@@ -536,9 +536,9 @@ name|active
 operator|=
 name|FALSE
 expr_stmt|;
-name|vm_stat
+name|cnt
 operator|.
-name|active_count
+name|v_active_count
 operator|--
 expr_stmt|;
 block|}
@@ -678,9 +678,9 @@ argument_list|,
 name|offset
 argument_list|)
 expr_stmt|;
-name|vm_stat
+name|cnt
 operator|.
-name|pageins
+name|v_pageins
 operator|++
 expr_stmt|;
 name|m
@@ -818,9 +818,9 @@ argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
-name|vm_stat
+name|cnt
 operator|.
-name|zero_fill_count
+name|v_zfod
 operator|++
 expr_stmt|;
 name|m
@@ -977,9 +977,9 @@ name|object
 argument_list|)
 expr_stmt|;
 comment|/* 			 *	Only use the new page below... 			 */
-name|vm_stat
+name|cnt
 operator|.
-name|cow_faults
+name|v_cow_faults
 operator|++
 expr_stmt|;
 name|m
