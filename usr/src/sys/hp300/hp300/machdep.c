70955d20a439675aa5db59b6853a308e0dce90d0
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-08-28 22:49:21 -0800

    vm_stat is merged into vmmeter structure
    SCCS-vsn: sys/hp300/hp300/machdep.c 7.19
    
    Former-commit-id: b9315d60f8571721373e652c08e0693b3519a822

--- usr/src/sys/hp300/hp300/machdep.c
+++ usr/src/sys/hp300/hp300/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.63 91/04/24$  *  *	@(#)machdep.c	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.63 91/04/24$  *  *	@(#)machdep.c	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1087,9 +1087,9 @@ literal|"avail mem = %d\n"
 argument_list|,
 name|ptoa
 argument_list|(
-name|vm_stat
+name|cnt
 operator|.
-name|free_count
+name|v_free_count
 argument_list|)
 argument_list|)
 expr_stmt|;
