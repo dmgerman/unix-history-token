3ab78ffc784a1fa5b232e76bde1ef7f756436f89
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-08-29 00:07:47 -0800

    use PAGE_MASK macro
    SCCS-vsn: sys/vm/vm_mmap.c 7.8
    
    Former-commit-id: ff72ce7a6a6a4c7534214811021ab21e5d08e195

--- usr/src/sys/vm/vm_mmap.c
+++ usr/src/sys/vm/vm_mmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_mmap.c 1.3 90/01/21$  *  *	@(#)vm_mmap.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_mmap.c 1.3 90/01/21$  *  *	@(#)vm_mmap.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -469,7 +469,7 @@ operator|&&
 operator|(
 name|addr
 operator|&
-name|page_mask
+name|PAGE_MASK
 operator|)
 operator|||
 name|uap
@@ -922,7 +922,7 @@ name|uap
 operator|->
 name|addr
 operator|&
-name|page_mask
+name|PAGE_MASK
 operator|)
 operator|||
 name|uap
@@ -1263,7 +1263,7 @@ condition|(
 operator|(
 name|addr
 operator|&
-name|page_mask
+name|PAGE_MASK
 operator|)
 operator|||
 name|uap
@@ -1504,7 +1504,7 @@ condition|(
 operator|(
 name|addr
 operator|&
-name|page_mask
+name|PAGE_MASK
 operator|)
 operator|||
 name|uap
