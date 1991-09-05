f52a23963f084b6d108e1254cbae6e8a75819207
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-04 19:39:48 -0800

    Rev #2 (after ANSI/POSIX changes to hash)
    SCCS-vsn: lib/libc/db/test/hash.tests/tcreat3.c 5.2
    SCCS-vsn: lib/libc/db/test/hash.tests/tdel.c 5.3
    SCCS-vsn: lib/libc/db/test/hash.tests/thash4.c 5.3
    SCCS-vsn: lib/libc/db/test/hash.tests/tread2.c 5.3
    SCCS-vsn: lib/libc/db/test/hash.tests/tseq.c 5.2
    SCCS-vsn: lib/libc/db/test/hash.tests/tverify.c 5.3
    
    Former-commit-id: 142a61252ea94e22658ce534796dcd4ef7dd28c2

--- usr/src/lib/libc/db/test/hash.tests/tread2.c
+++ usr/src/lib/libc/db/test/hash.tests/tread2.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)tread2.c	5.2 (Berkeley) %G%"
+literal|"@(#)tread2.c	5.2 (Berkeley) 3/12/91"
 decl_stmt|;
 end_decl_stmt
 
@@ -113,7 +113,7 @@ begin_decl_stmt
 name|char
 name|wp1
 index|[
-literal|256
+literal|8192
 index|]
 decl_stmt|;
 end_decl_stmt
@@ -122,7 +122,7 @@ begin_decl_stmt
 name|char
 name|wp2
 index|[
-literal|256
+literal|8192
 index|]
 decl_stmt|;
 end_decl_stmt
@@ -255,7 +255,7 @@ name|fgets
 argument_list|(
 name|wp1
 argument_list|,
-literal|256
+literal|8192
 argument_list|,
 name|stdin
 argument_list|)
@@ -264,7 +264,7 @@ name|fgets
 argument_list|(
 name|wp2
 argument_list|,
-literal|256
+literal|8192
 argument_list|,
 name|stdin
 argument_list|)
@@ -309,6 +309,8 @@ name|key
 argument_list|,
 operator|&
 name|res
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 if|if
