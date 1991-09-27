d37e88cfce97adac0687648db27210521125dd4b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-26 23:43:44 -0800

    minimize the dependencies on <sys/types.h
    SCCS-vsn: include/db.h 5.16
    
    Former-commit-id: 96a43b4a1fc7d137fe8a8254399ffe24e1806fe5

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.15 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.16 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -412,7 +412,8 @@ directive|define
 name|R_DUP
 value|0x01
 comment|/* duplicate keys */
-name|u_long
+name|unsigned
+name|long
 name|flags
 decl_stmt|;
 name|int
@@ -553,7 +554,8 @@ directive|define
 name|R_SNAPSHOT
 value|0x04
 comment|/* snapshot the input */
-name|u_long
+name|unsigned
+name|long
 name|flags
 decl_stmt|;
 name|int
@@ -568,7 +570,8 @@ name|size_t
 name|reclen
 decl_stmt|;
 comment|/* record length (fixed-length records) */
-name|u_char
+name|unsigned
+name|char
 name|bval
 decl_stmt|;
 comment|/* delimiting byte (variable-length records */
@@ -585,13 +588,16 @@ begin_typedef
 typedef|typedef
 struct|struct
 block|{
-name|u_long
+name|unsigned
+name|long
 name|number
 decl_stmt|;
-name|u_long
+name|unsigned
+name|long
 name|offset
 decl_stmt|;
-name|u_long
+name|unsigned
+name|long
 name|length
 decl_stmt|;
 define|#
@@ -609,7 +615,8 @@ directive|define
 name|R_OFFSET
 value|0x04
 comment|/* offset is valid */
-name|u_char
+name|unsigned
+name|char
 name|valid
 decl_stmt|;
 block|}
@@ -628,7 +635,7 @@ name|BLSWAP
 parameter_list|(
 name|a
 parameter_list|)
-value|{ \ 	u_long _tmp = a; \ 	((char *)&a)[0] = ((char *)&_tmp)[3]; \ 	((char *)&a)[1] = ((char *)&_tmp)[2]; \ 	((char *)&a)[2] = ((char *)&_tmp)[1]; \ 	((char *)&a)[3] = ((char *)&_tmp)[0]; \ }
+value|{ \ 	unsigned long _tmp = a; \ 	((char *)&a)[0] = ((char *)&_tmp)[3]; \ 	((char *)&a)[1] = ((char *)&_tmp)[2]; \ 	((char *)&a)[2] = ((char *)&_tmp)[1]; \ 	((char *)&a)[3] = ((char *)&_tmp)[0]; \ }
 end_define
 
 begin_define
@@ -638,7 +645,7 @@ name|BLPSWAP
 parameter_list|(
 name|a
 parameter_list|)
-value|{ \ 	u_long _tmp = *(u_long *)a; \ 	((char *)a)[0] = ((char *)&_tmp)[3]; \ 	((char *)a)[1] = ((char *)&_tmp)[2]; \ 	((char *)a)[2] = ((char *)&_tmp)[1]; \ 	((char *)a)[3] = ((char *)&_tmp)[0]; \ }
+value|{ \ 	unsigned long _tmp = *(unsigned long *)a; \ 	((char *)a)[0] = ((char *)&_tmp)[3]; \ 	((char *)a)[1] = ((char *)&_tmp)[2]; \ 	((char *)a)[2] = ((char *)&_tmp)[1]; \ 	((char *)a)[3] = ((char *)&_tmp)[0]; \ }
 end_define
 
 begin_define
@@ -664,7 +671,7 @@ name|BSSWAP
 parameter_list|(
 name|a
 parameter_list|)
-value|{ \ 	u_short _tmp = a; \ 	((char *)&a)[0] = ((char *)&_tmp)[1]; \ 	((char *)&a)[1] = ((char *)&_tmp)[0]; \ }
+value|{ \ 	unsigned short _tmp = a; \ 	((char *)&a)[0] = ((char *)&_tmp)[1]; \ 	((char *)&a)[1] = ((char *)&_tmp)[0]; \ }
 end_define
 
 begin_define
@@ -674,7 +681,7 @@ name|BSPSWAP
 parameter_list|(
 name|a
 parameter_list|)
-value|{ \ 	u_short _tmp = *(u_short *)a; \ 	((char *)a)[0] = ((char *)&_tmp)[1]; \ 	((char *)a)[1] = ((char *)&_tmp)[0]; \ }
+value|{ \ 	unsigned short _tmp = *(unsigned short *)a; \ 	((char *)a)[0] = ((char *)&_tmp)[1]; \ 	((char *)a)[1] = ((char *)&_tmp)[0]; \ }
 end_define
 
 begin_define
