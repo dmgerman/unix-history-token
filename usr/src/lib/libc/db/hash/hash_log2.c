894df75d76592e79db2716aa236e0555c299caae
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-26 23:17:30 -0800

    doesn't need all of these includes
    SCCS-vsn: lib/libc/db/hash/hash_log2.c 5.4
    
    Former-commit-id: f14a00b096b9088b4d4cadbd2daf4dc346e80a00

--- usr/src/lib/libc/db/hash/hash_log2.c
+++ usr/src/lib/libc/db/hash/hash_log2.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)hash_log2.c	5.3 (Berkeley) %G%"
+literal|"@(#)hash_log2.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -41,30 +41,6 @@ begin_include
 include|#
 directive|include
 file|<sys/types.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<db.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|"hash.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"page.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"extern.h"
 end_include
 
 begin_function
