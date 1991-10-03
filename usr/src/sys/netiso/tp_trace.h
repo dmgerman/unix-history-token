e756185de830b3b1a95bea985a2034e32175fa3a
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-10-03 03:44:33 -0800

    get rid of struct tp_param, which contained no interesting info.
    SCCS-vsn: sys/netiso/tp_trace.h 7.6
    
    Former-commit-id: 47b8edb3d3b839c0298d1809273d5f905d1e4c1b

--- usr/src/sys/netiso/tp_trace.h
+++ usr/src/sys/netiso/tp_trace.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tp_trace.h	7.5 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tp_trace.h	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -232,7 +232,7 @@ name|tpt_Tpdu
 decl_stmt|;
 comment|/* header*/
 name|struct
-name|tp_param
+name|tp_refinfo
 name|tpt_Param
 decl_stmt|;
 comment|/* ?? bytes, make sure< 128??*/
