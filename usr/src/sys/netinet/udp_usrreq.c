c799b495504f48b42c9effaa370d9445db25b4dd
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-10-03 06:41:29 -0800

    for now, redundantly include errno.h to share source with sys.oldvm
    SCCS-vsn: sys/netinet/udp_usrreq.c 7.22
    
    Former-commit-id: 94241e68e3645bb72074644c796486fc44960098

--- usr/src/sys/netinet/udp_usrreq.c
+++ usr/src/sys/netinet/udp_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)udp_usrreq.c	7.21 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)udp_usrreq.c	7.22 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -37,6 +37,12 @@ begin_include
 include|#
 directive|include
 file|"socketvar.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"errno.h"
 end_include
 
 begin_include
