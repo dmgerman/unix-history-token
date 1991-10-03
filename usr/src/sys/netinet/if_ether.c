3f5ea3260b7bd8e852ecd8279f79b64023fe4b28
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-10-03 06:43:06 -0800

    bug pointed out by ralph campbell
    SCCS-vsn: sys/netinet/if_ether.c 7.15
    
    Former-commit-id: 9baf8518e342a97eec0116d798ef2b244d663a5b

--- usr/src/sys/netinet/if_ether.c
+++ usr/src/sys/netinet/if_ether.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_ether.c	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_ether.c	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1808,6 +1808,8 @@ name|struct
 name|llinfo_arp
 modifier|*
 name|la
+init|=
+literal|0
 decl_stmt|;
 specifier|register
 name|struct
