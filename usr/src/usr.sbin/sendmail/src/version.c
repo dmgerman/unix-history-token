3be5ed18373a5c688d80ad0f69ae2deceebca1ad
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-10-05 18:09:10 -0800

    allow catching of local names after aliasing but before local name resolution; have name server return indication that it matched.
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 5.21
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 5.14
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.76
    
    Former-commit-id: bea690c73e671e95f705676482b233caf22d99ea

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	5.75 (Berkeley) %G%"
+literal|"@(#)version.c	5.76 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"5.75"
+literal|"5.76"
 decl_stmt|;
 end_decl_stmt
 
