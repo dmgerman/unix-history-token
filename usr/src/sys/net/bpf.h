1e6e47fabe5f091b42f8dbca1762b57d5f0b92d1
Steven McCanne <mccanne@ucbvax.Berkeley.EDU>
1991-10-28 05:30:09 -0800

    add DLT_NULL; BIOCGFLEN no longer exists
    SCCS-vsn: sys/net/bpf.h 7.2
    
    Former-commit-id: 06f290ca7500de6c85d5ea12144ea9034c25dbf9

--- usr/src/sys/net/bpf.h
+++ usr/src/sys/net/bpf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from the Stanford/CMU enet packet filter,  * (net/enet.c) distributed as part of 4.3BSD, and code contributed  * to Berkeley by Steven McCanne of Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)bpf.h	7.1 (Berkeley) %G%  *  * @(#) $Header: bpf.h,v 1.20 91/04/24 22:06:24 mccanne Locked $ (LBL)  */
+comment|/*-  * Copyright (c) 1990-1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from the Stanford/CMU enet packet filter,  * (net/enet.c) distributed as part of 4.3BSD, and code contributed  * to Berkeley by Steven McCanne and Van Jacobson both of Lawrence   * Berkeley Laboratory.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by the University of  *      California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      @(#)bpf.h       7.1 (Berkeley) 5/7/91  *  * @(#) $Header: bpf.h,v 1.24 91/10/27 21:22:32 mccanne Exp $ (LBL)  */
 end_comment
 
 begin_comment
@@ -85,10 +85,17 @@ end_comment
 begin_if
 if|#
 directive|if
+operator|(
 name|defined
 argument_list|(
 name|sun
 argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|ibm032
+argument_list|)
+operator|)
 operator|&&
 operator|!
 name|defined
@@ -100,15 +107,15 @@ end_if
 begin_define
 define|#
 directive|define
-name|BIOCGFLEN
-value|_IOR(B,101, u_int)
+name|BIOCGBLEN
+value|_IOR(B,102, u_int)
 end_define
 
 begin_define
 define|#
 directive|define
-name|BIOCGBLEN
-value|_IOR(B,102, u_int)
+name|BIOCSBLEN
+value|_IOWR(B,102, u_int)
 end_define
 
 begin_define
@@ -189,15 +196,15 @@ end_else
 begin_define
 define|#
 directive|define
-name|BIOCGFLEN
-value|_IOR('B',101, u_int)
+name|BIOCGBLEN
+value|_IOR('B',102, u_int)
 end_define
 
 begin_define
 define|#
 directive|define
-name|BIOCGBLEN
-value|_IOR('B',102, u_int)
+name|BIOCSBLEN
+value|_IOWR('B',102, u_int)
 end_define
 
 begin_define
@@ -328,6 +335,17 @@ end_endif
 
 begin_comment
 comment|/*  * Data-link level type codes.  * Currently, only DLT_EN10MB and DLT_SLIP are supported.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DLT_NULL
+value|0
+end_define
+
+begin_comment
+comment|/* no link-layer encapsulation */
 end_comment
 
 begin_define
