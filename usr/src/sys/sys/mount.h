43dfb415d8a97493f2401a297b22527dc2f5291a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 00:33:08 -0800

    lint in vfs_quotactl function prototype
    SCCS-vsn: sys/sys/mount.h 7.25
    
    Former-commit-id: 3b089eddec9334c7694765457add80ccfe301595

--- usr/src/sys/sys/mount.h
+++ usr/src/sys/sys/mount.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mount.h	7.24 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mount.h	7.25 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
@@ -578,7 +578,7 @@ operator|,
 name|int
 name|cmds
 operator|,
-name|int
+name|u_int
 name|uid
 operator|,
 name|caddr_t
