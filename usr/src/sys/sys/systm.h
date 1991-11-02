709e762bb27d39220fd8067fa3e7724fb72b77d1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 00:31:23 -0800

    make panic take printf(3) style arguments
    SCCS-vsn: sys/sys/systm.h 7.18
    
    Former-commit-id: 2b94c0c3f83a41e30ed1586fda1ccdfce051436c

--- usr/src/sys/sys/systm.h
+++ usr/src/sys/sys/systm.h
@@ -1,10 +1,11 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)systm.h	7.17 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)systm.h	7.18 (Berkeley) %G%  */
 end_comment
 
 begin_decl_stmt
 specifier|extern
+specifier|const
 name|char
 modifier|*
 name|panicstr
@@ -372,8 +373,11 @@ name|panic
 name|__P
 argument_list|(
 operator|(
+specifier|const
 name|char
 operator|*
+operator|,
+operator|...
 operator|)
 argument_list|)
 decl_stmt|;
