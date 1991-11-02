f0c08dd83da5a822f5391e8f91fda834a242f214
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 00:49:37 -0800

    rename bufstats to ufs_bufstats
    SCCS-vsn: sys/nfs/nfs_vfsops.c 7.32
    
    Former-commit-id: 68cb9fc3d99d3244d2d9a35cc8d81b7c0cc7b232

--- usr/src/sys/nfs/nfs_vfsops.c
+++ usr/src/sys/nfs/nfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vfsops.c	7.31 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vfsops.c	7.32 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2661,7 +2661,7 @@ if|if
 condition|(
 name|syncprt
 condition|)
-name|bufstats
+name|ufs_bufstats
 argument_list|()
 expr_stmt|;
 comment|/* 	 * Force stale buffer cache information to be flushed. 	 */
