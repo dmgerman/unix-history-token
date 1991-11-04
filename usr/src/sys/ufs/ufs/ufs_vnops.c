d851d39b731566cc25be0c9fc65df57255175a2c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-03 22:10:11 -0800

    have to grab information before the vnode is released
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 7.66
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 7.66
    
    Former-commit-id: cf26ffcf549b5f62ebd4fa89e5b2b0edf14b5bdf

--- usr/src/sys/ufs/ufs/ufs_vnops.c
+++ usr/src/sys/ufs/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	7.65 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	7.66 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -5410,6 +5410,17 @@ operator|->
 name|ni_dvp
 argument_list|)
 expr_stmt|;
+name|ump
+operator|=
+name|VFSTOUFS
+argument_list|(
+name|ndp
+operator|->
+name|ni_dvp
+operator|->
+name|v_mount
+argument_list|)
+expr_stmt|;
 comment|/* 	 * No rmdir "." please. 	 */
 if|if
 condition|(
@@ -5522,17 +5533,6 @@ operator|->
 name|i_nlink
 operator|-=
 literal|2
-expr_stmt|;
-name|ump
-operator|=
-name|VFSTOUFS
-argument_list|(
-name|ndp
-operator|->
-name|ni_dvp
-operator|->
-name|v_mount
-argument_list|)
 expr_stmt|;
 name|error
 operator|=
