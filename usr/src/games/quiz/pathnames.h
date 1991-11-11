2f707e1ccb53e86d3513dead94271a6b882c80c4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-10 22:24:27 -0800

    Jim Oldroyd's version of quiz
    SCCS-vsn: games/quiz/Makefile 5.1
    SCCS-vsn: games/quiz/pathnames.h 5.2
    SCCS-vsn: games/quiz/quiz.6 6.3
    SCCS-vsn: games/quiz/quiz.c 5.1
    
    Former-commit-id: 50e0ac84e2ac2aa0d1febddf67ba358d1ca3a569

--- usr/src/games/quiz/pathnames.h
+++ usr/src/games/quiz/pathnames.h
@@ -1,12 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|_PATH_INDEX
+name|_PATH_PAGER
+value|"/usr/bin/more"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_QUIZIDX
 value|"/usr/share/games/quiz.db/index"
 end_define
 
