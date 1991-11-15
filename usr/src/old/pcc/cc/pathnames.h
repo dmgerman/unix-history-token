81ff449b99082a758c194ca112958fca13881d30
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-14 18:22:21 -0800

    mv ld to /usr/old/bin
    SCCS-vsn: old/pcc/cc/pathnames.h 5.7
    
    Former-commit-id: bb4b5ae064a90de2b8dc763b8e5f477708960452

--- usr/src/old/pcc/cc/pathnames.h
+++ usr/src/old/pcc/cc/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -55,7 +55,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_LD
-value|"/usr/bin/ld"
+value|"/usr/old/bin/ld"
 end_define
 
 begin_define
