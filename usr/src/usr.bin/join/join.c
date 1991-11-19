9b8c89d6f103e0cce72b673563336df503d6b7f0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-19 00:16:34 -0800

    fix Steve Haymans organization
    SCCS-vsn: usr.bin/join/join.c 5.2
    
    Former-commit-id: 325bc91d9e3f65671f4bd833133b81a728547b9a

--- usr/src/usr.bin/join/join.c
+++ usr/src/usr.bin/join/join.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Steve Hayman of Indiana University, Michiro Hikida and David  * Goodenough.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Steve Hayman of the Computer Science Department, Indiana University,  * Michiro Hikida and David Goodenough.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)join.c	5.1 (Berkeley) %G%"
+literal|"@(#)join.c	5.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
