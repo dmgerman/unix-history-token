673b010c62cab9192ad3077614638c401531f567
Warren Toomey <wkt@csadfa.cs.adfa.oz.au>
1991-11-28 00:00:00 +0000

    COMPAT_43 KERNEL DEPENDENCY CLEANUP
    	 Here is a patch for the use of COMPAT_43 in the 386BSD kernel.
    This is an extremely LOW priority patch, because lots of applications
    (init, csh, vi)	depend on this being defined, so you can't use a kernel
    with this option removed. The only application that does cope with this
    option missing is my own shell which is POSIX-compliant. But you might
    want to add it in now rather than have to rediscover it at a later
    stage.
    
    AUTHOR:		Warren Toomey
    386BSD-Patchkit: patch00062
    
    Former-commit-id: ab7225b27fa2e81d56730e194d65db31892ebdac

--- usr/src/sys.386bsd/kern/tty_pty.c
+++ usr/src/sys.386bsd/kern/tty_pty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_pty.c	7.21 (Berkeley) 5/30/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00061  * --------------------         -----   ----------------------  *  * 11 Dec 92	Williams Jolitz		Fixed tty handling  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_pty.c	7.21 (Berkeley) 5/30/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00062  * --------------------         -----   ----------------------  *  * 11 Dec 92	Williams Jolitz		Fixed tty handling  *  * 28 Nov 1991	Warren Toomey		Cleaned up the use of COMPAT_43  *					in the 386BSD kernel.	   */
 end_comment
 
 begin_decl_stmt
@@ -3535,12 +3535,18 @@ operator|(
 literal|0
 operator|)
 return|;
+ifdef|#
+directive|ifdef
+name|COMPAT_43
+comment|/* wkt */
 case|case
 name|TIOCSETP
 case|:
 case|case
 name|TIOCSETN
 case|:
+endif|#
+directive|endif
 case|case
 name|TIOCSETD
 case|:
@@ -3887,15 +3893,16 @@ case|:
 case|case
 name|TIOCSETAF
 case|:
+ifdef|#
+directive|ifdef
+name|COMPAT_43
+comment|/* wkt */
 case|case
 name|TIOCSETP
 case|:
 case|case
 name|TIOCSETN
 case|:
-ifdef|#
-directive|ifdef
-name|COMPAT_43
 case|case
 name|TIOCSETC
 case|:
