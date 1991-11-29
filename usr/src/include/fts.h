b6d79bc7f296b19ded139c53cd325c82d6626cc4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-28 21:56:42 -0800

    pointers to pointers, not pointers
    SCCS-vsn: include/fts.h 5.15
    
    Former-commit-id: 37c60f9c9580db03e183c3f5e3b4b5ab959d39ed

--- usr/src/include/fts.h
+++ usr/src/include/fts.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fts.h	5.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fts.h	5.15 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -350,9 +350,11 @@ argument_list|(
 specifier|const
 name|FTSENT
 operator|*
+operator|*
 argument_list|,
 specifier|const
 name|FTSENT
+operator|*
 operator|*
 argument_list|)
 operator|)
