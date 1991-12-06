f57c13a2427ef5ec7f0e43ff92d7457ee558de53
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-05 16:41:38 -0800

    mount.h needs param.h and ucred.h, now
    SCCS-vsn: usr.bin/find/function.c 5.19
    
    Former-commit-id: 4404ee91af91b17d50eaffa48d1123f298d13637

--- usr/src/usr.bin/find/function.c
+++ usr/src/usr.bin/find/function.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)function.c	5.18 (Berkeley) %G%"
+literal|"@(#)function.c	5.19 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -32,6 +32,12 @@ begin_include
 include|#
 directive|include
 file|<sys/param.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/ucred.h>
 end_include
 
 begin_include
