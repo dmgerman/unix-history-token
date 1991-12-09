1de6fd9005db8239f4ab5bcdd154181f9d2c1a8d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-12-09 00:42:29 -0800

    include LOCKF compilation variable
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 5.21
    
    Former-commit-id: a8d0baaeb34246db8b3a6c15d7d6cf99892617e6

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	5.20 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	5.21 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -263,6 +263,10 @@ end_define
 
 begin_comment
 comment|/* look in user database */
+end_comment
+
+begin_comment
+comment|/* # define LOCKF	1	/* use System V lockf instead of flock */
 end_comment
 
 begin_comment
