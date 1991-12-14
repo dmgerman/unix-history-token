87f8217f294c7c58799ca91f1064327178e0ea6c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-14 00:06:04 -0800

    update the superblock
    SCCS-vsn: sys/ufs/lfs/lfs_segment.c 7.5
    
    Former-commit-id: c702e42b96b208cc892160f8d7a7aa15df63a3ee

--- usr/src/sys/ufs/lfs/lfs_segment.c
+++ usr/src/sys/ufs/lfs/lfs_segment.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_segment.c	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_segment.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3449,7 +3449,6 @@ operator|*
 operator|)
 argument_list|)
 expr_stmt|;
-return|return;
 ifdef|#
 directive|ifdef
 name|VERBOSE
