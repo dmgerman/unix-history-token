7b729be48b9dfc45f33d3b0fb99b9d3d4ed3bf25
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-16 00:10:43 -0800

    don't make routines static
    SCCS-vsn: sys/ufs/lfs/lfs_balloc.c 7.24
    
    Former-commit-id: a986674b98514f4b0b36c09c335847bb17b415cd

--- usr/src/sys/ufs/lfs/lfs_balloc.c
+++ usr/src/sys/ufs/lfs/lfs_balloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_balloc.c	7.23 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_balloc.c	7.24 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -82,7 +82,6 @@ file|<ufs/lfs/lfs_extern.h>
 end_include
 
 begin_decl_stmt
-specifier|static
 name|int
 name|lfs_getlbns
 name|__P
