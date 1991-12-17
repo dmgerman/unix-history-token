e0fdbfa0c796cc7d9403e2faa03f1d0398b9b0b8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-17 01:46:46 -0800

    M_SUPERBLK rolled into M_UFSMNT
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.68
    
    Former-commit-id: a7c8f83e8bfa543e4950e5dea56c55c1c42401ea

--- usr/src/sys/ufs/lfs/lfs_vfsops.c
+++ usr/src/sys/ufs/lfs/lfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_vfsops.c	7.67 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_vfsops.c	7.68 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1205,7 +1205,7 @@ expr|struct
 name|lfs
 argument_list|)
 argument_list|,
-name|M_SUPERBLK
+name|M_UFSMNT
 argument_list|,
 name|M_WAITOK
 argument_list|)
@@ -1659,7 +1659,7 @@ name|ump
 operator|->
 name|um_lfs
 argument_list|,
-name|M_SUPERBLK
+name|M_UFSMNT
 argument_list|)
 expr_stmt|;
 name|free
