0886197782d666e83f2d334bba4450f3c2021ca2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-16 22:01:57 -0800

    don't bother with the macros, performance isn't an issue here
    SCCS-vsn: lib/libc/db/btree/bt_open.c 5.16
    
    Former-commit-id: 30c8145f0d148b128aee71c7a3d6411c6fb8b4db

--- usr/src/lib/libc/db/btree/bt_open.c
+++ usr/src/lib/libc/db/btree/bt_open.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)bt_open.c	5.15 (Berkeley) %G%"
+literal|"@(#)bt_open.c	5.16 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -730,7 +730,7 @@ name|m
 operator|.
 name|m_lorder
 operator|=
-name|NTOHL
+name|ntohl
 argument_list|(
 name|m
 operator|.
