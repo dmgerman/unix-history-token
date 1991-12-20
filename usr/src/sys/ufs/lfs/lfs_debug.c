a5216056cbb23245085d01ddb581f61b1b5e401f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-19 20:58:28 -0800

    must cast di_size passed to printf
    SCCS-vsn: sys/ufs/lfs/lfs_debug.c 7.3
    
    Former-commit-id: 244c1df13c87c65f9ac95cdaa10f5dad7f6787cd

--- usr/src/sys/ufs/lfs/lfs_debug.c
+++ usr/src/sys/ufs/lfs/lfs_debug.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_debug.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_debug.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -444,6 +444,9 @@ name|di_gid
 argument_list|,
 literal|"size  "
 argument_list|,
+operator|(
+name|u_long
+operator|)
 name|dip
 operator|->
 name|di_size
