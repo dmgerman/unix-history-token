0b0f099d524ce0b76bc0eb13728c260382485f60
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-19 20:32:02 -0800

    fsid_t is not really a quad, it is two longs
    SCCS-vsn: sys/sys/mount.h 7.29
    
    Former-commit-id: decda8c68cd12e6278ad4c43a480221a2ade065e

--- usr/src/sys/sys/mount.h
+++ usr/src/sys/sys/mount.h
@@ -1,11 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mount.h	7.28 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mount.h	7.29 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
 typedef|typedef
-name|quad
+struct|struct
+block|{
+name|long
+name|val
+index|[
+literal|2
+index|]
+decl_stmt|;
+block|}
 name|fsid_t
 typedef|;
 end_typedef
