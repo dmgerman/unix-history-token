1bc61b23a6fb926efda452f6950e2eb4d33848f9
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-19 20:29:55 -0800

    add panic for uncleaned vnode private areas
    SCCS-vsn: sys/kern/vfs_subr.c 7.64
    
    Former-commit-id: 8c44897683af209a7eda0d3a65a9dd798db423cf

--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_subr.c	7.63 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_subr.c	7.64 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -960,6 +960,17 @@ condition|)
 name|vgone
 argument_list|(
 name|vp
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|vp
+operator|->
+name|v_data
+condition|)
+name|panic
+argument_list|(
+literal|"cleaned vnode isn't"
 argument_list|)
 expr_stmt|;
 name|vp
