041fec13d50165177bfa9d47ccd0937b5603811f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-24 01:24:16 -0800

    fts_dfd unused
    SCCS-vsn: include/fts.h 5.16
    
    Former-commit-id: 40545837315773822c4d29961f0d3f67f2449e39

--- usr/src/include/fts.h
+++ usr/src/include/fts.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fts.h	5.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fts.h	5.16 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -53,10 +53,6 @@ modifier|*
 name|fts_path
 decl_stmt|;
 comment|/* path for this descent */
-name|int
-name|fts_dfd
-decl_stmt|;
-comment|/* fd for directories */
 name|int
 name|fts_rfd
 decl_stmt|;
