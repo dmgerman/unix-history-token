f9c76b6e0ea13909eab6e56f5f5fa5adf143e1e5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-31 22:55:07 -0800

    delete LFS_IRELEASE, just do a brelse
    SCCS-vsn: sys/ufs/lfs/lfs_subr.c 7.6
    
    Former-commit-id: 3bba916b243ed28ad087d0b7c5acf047d6c91b1a

--- usr/src/sys/ufs/lfs/lfs_subr.c
+++ usr/src/sys/ufs/lfs/lfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_subr.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_subr.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -371,10 +371,8 @@ name|ifp
 operator|->
 name|if_daddr
 expr_stmt|;
-name|LFS_IRELEASE
+name|brelse
 argument_list|(
-name|fs
-argument_list|,
 name|bp
 argument_list|)
 expr_stmt|;
