91e0cc408b066cefff5045c89bbee5f7c6e87608
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-01-14 20:00:15 -0800

    breada generalized to breadn
    SCCS-vsn: sys/sys/buf.h 7.14
    
    Former-commit-id: 2fa70fbde15b459d20810006c54f055528a119bf

--- usr/src/sys/sys/buf.h
+++ usr/src/sys/sys/buf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)buf.h	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)buf.h	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -613,7 +613,7 @@ end_decl_stmt
 
 begin_decl_stmt
 name|int
-name|breada
+name|breadn
 name|__P
 argument_list|(
 operator|(
@@ -626,6 +626,10 @@ operator|,
 name|int
 operator|,
 name|daddr_t
+operator|*
+operator|,
+name|int
+operator|*
 operator|,
 name|int
 operator|,
