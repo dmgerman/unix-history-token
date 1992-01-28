f8a01af511a1771ab8b6f3bba1d81d56511ba6c7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-01-27 23:15:42 -0800

    put on SCCS headers
    SCCS-vsn: usr.sbin/pstat/pstat.c 5.38
    
    Former-commit-id: 58d63a0fcd953fdb1dcba05ab38a613d17645c7c

--- usr/src/usr.sbin/pstat/pstat.c
+++ usr/src/usr.sbin/pstat/pstat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1980, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  */
 end_comment
 
 begin_ifndef
@@ -14,7 +14,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1980 Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1980, 1991 The Regents of the University of California.\n\  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)pstat.c	5.35 (Berkeley) 6/26/91"
+literal|"@(#)pstat.c	5.38 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -50,10 +50,6 @@ end_endif
 
 begin_comment
 comment|/* not lint */
-end_comment
-
-begin_comment
-comment|/*  * Print system stuff  */
 end_comment
 
 begin_include
