3a99c63ad0bd1524be8ecfeb16d795d9341dc6cd
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-01-30 22:07:56 -0800

    lint changes for sparc port, bug fix on default case
    SCCS-vsn: sys/netinet/in.c 7.20
    
    Former-commit-id: 1a11863de1883969cedc38ccf0e65f3e08685132

--- usr/src/sys/netinet/in.c
+++ usr/src/sys/netinet/in.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)in.c	7.19 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)in.c	7.20 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2483,6 +2483,12 @@ name|sin
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+name|int
+name|scrub
+decl_stmt|;
+end_decl_stmt
+
 begin_block
 block|{
 specifier|register
@@ -2555,6 +2561,9 @@ name|ifp
 argument_list|,
 name|SIOCSIFADDR
 argument_list|,
+operator|(
+name|caddr_t
+operator|)
 name|ia
 argument_list|)
 operator|)
