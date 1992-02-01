0c2d11321f856c5fb505071bbbd882ae32fc154b
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-01-31 21:02:49 -0800

    fix arg to arpwhohas to match prototype
    SCCS-vsn: sys/netinet/if_ether.c 7.19
    
    Former-commit-id: 8d8af19c2dbc33d84c744f03a1120e7c3e605b4b

--- usr/src/sys/netinet/if_ether.c
+++ usr/src/sys/netinet/if_ether.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_ether.c	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_ether.c	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -907,7 +907,7 @@ modifier|*
 name|ac
 decl_stmt|;
 name|struct
-name|inaddr
+name|in_addr
 modifier|*
 name|addr
 decl_stmt|;
