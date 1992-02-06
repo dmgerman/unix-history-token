e69015cc23742514487523dd477678a007eea535
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-02-05 19:39:00 -0800

    don't pass chars to functions
    SCCS-vsn: include/vis.h 5.10
    
    Former-commit-id: a6a598d960a388ddf2e44ccb3b3524693c52cf97

--- usr/src/include/vis.h
+++ usr/src/include/vis.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vis.h	5.9 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vis.h	5.10 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -202,11 +202,11 @@ operator|(
 name|char
 operator|*
 operator|,
-name|char
+name|int
 operator|,
 name|int
 operator|,
-name|char
+name|int
 operator|)
 argument_list|)
 decl_stmt|;
@@ -278,7 +278,7 @@ operator|(
 name|char
 operator|*
 operator|,
-name|char
+name|int
 operator|,
 name|int
 operator|*
