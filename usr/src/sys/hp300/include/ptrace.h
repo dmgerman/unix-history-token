c4b055caeafbc7590d43b6f9ae409ce8e8f449e1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-02-06 06:40:56 -0800

    date and time created 92/02/05 22:40:56 by mckusick
    SCCS-vsn: sys/hp300/include/ptrace.h 7.1
    
    Former-commit-id: 8339a09c36a3a80d0c2f5063883fbfb4ca3e0c81

---
+++ usr/src/sys/hp300/include/ptrace.h
@@ -0,0 +1,11 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ptrace.h	7.1 (Berkeley) %G%  */
+end_comment
+
+begin_comment
+comment|/*  * Machine dependent trace commands.  *  * None for the hp300 at this time.  */
+end_comment
+
+end_unit
+
