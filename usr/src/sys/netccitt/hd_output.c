094a761650d6be79093606436a047a55450f835e
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-02-07 00:32:35 -0800

    comparison wrongly takes precedence over bitwise-and
    SCCS-vsn: sys/netccitt/pk_subr.c 7.20
    SCCS-vsn: sys/netccitt/hd_output.c 7.7
    
    Former-commit-id: 90bf70aa6fdb32fefc0b97eefb2d1f605a486792

--- usr/src/sys/netccitt/hd_output.c
+++ usr/src/sys/netccitt/hd_output.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)hd_output.c	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Laboratory for Computation Vision and the Computer Science Department  * of the University of British Columbia.  *  * %sccs.include.redist.c%  *  *	@(#)hd_output.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -145,11 +145,13 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+operator|(
 name|m
 operator|->
 name|m_flags
 operator|&
 name|M_PKTHDR
+operator|)
 operator|==
 literal|0
 condition|)
