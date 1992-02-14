7c93edc13c4b6155c33fd858960f629ae2cf8bde
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-02-14 02:13:03 -0800

    lint
    SCCS-vsn: sys/kern/uipc_socket.c 7.32
    
    Former-commit-id: f82d8971a35b0f03ea251f2e69f7544ff9e214b5

--- usr/src/sys/kern/uipc_socket.c
+++ usr/src/sys/kern/uipc_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)uipc_socket.c	7.31 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)uipc_socket.c	7.32 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -89,6 +89,12 @@ argument_list|,
 argument|proto
 argument_list|)
 end_macro
+
+begin_decl_stmt
+name|int
+name|dom
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|struct
