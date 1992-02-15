6f1080f77a30f950a0c3d4cfbe7c333fbf1c67ee
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-02-14 22:15:45 -0800

    fix bug
    SCCS-vsn: sys/netiso/if_eon.c 7.18
    
    Former-commit-id: cda5ff2261fc3b90f7d6f68bedfafe1a6ccf5000

--- usr/src/sys/netiso/if_eon.c
+++ usr/src/sys/netiso/if_eon.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_eon.c	7.17 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_eon.c	7.18 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1156,7 +1156,7 @@ name|gate
 argument_list|)
 operator|->
 name|sdl_alen
-operator|=
+operator|==
 literal|1
 condition|)
 name|el
