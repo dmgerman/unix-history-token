137d77f13dd1d9025f8afe27b582645f21b1bba0
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-02-16 01:37:58 -0800

    have to include proc before socketvar.h
    SCCS-vsn: sys/nfs/nfs_srvcache.c 7.13
    
    Former-commit-id: 9d2db31e32b578a42f2f35974b6e059252a2feca

--- usr/src/sys/nfs/nfs_srvcache.c
+++ usr/src/sys/nfs/nfs_srvcache.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_srvcache.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_srvcache.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -35,6 +35,12 @@ begin_include
 include|#
 directive|include
 file|"systm.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"proc.h"
 end_include
 
 begin_include
