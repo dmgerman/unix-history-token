89cac4cddac784c942a4c6f11b224efea4a644a6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-02-18 17:25:48 -0800

    protect include files included by other include files with #ifndef _NAME_H_
    SCCS-vsn: sys/sys/select.h 7.3
    
    Former-commit-id: 401d6bd3bf32fe5b091cc7513c0fb92e3f887388

--- usr/src/sys/sys/select.h
+++ usr/src/sys/sys/select.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)select.h	7.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)select.h	7.3 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_SELECT_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_SELECT_H_
+end_define
 
 begin_comment
 comment|/*  * Used to maintain information about processes that wish to be  * notified when I/O becomes possible.  */
@@ -66,6 +78,15 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_SELECT_H_ */
+end_comment
 
 end_unit
 
