2f879c67de4604746af0b8cba5488b995ed25c0a
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-02-18 23:49:36 -0800

    describing routing message headers for add/delete/state change change on interfaces; collect stats in common structure, so that will be useful for getkerninfo
    SCCS-vsn: sys/net/if.h 7.16
    
    Former-commit-id: fa4fe438ef5e9469c7f588c7457ea5608179a279

--- usr/src/sys/net/if.h
+++ usr/src/sys/net/if.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if.h	7.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if.h	7.16 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -72,6 +72,10 @@ directive|endif
 end_endif
 
 begin_comment
+comment|/*  * Structure describing information about an interface  * which may be of interest to management entities.  */
+end_comment
+
+begin_comment
 comment|/*  * Structure defining a queue for a network interface.  *  * (Would like to call this struct ``if'', but C isn't PL/1.)  */
 end_comment
 
@@ -84,58 +88,119 @@ modifier|*
 name|if_name
 decl_stmt|;
 comment|/* name, e.g. ``en'' or ``lo'' */
-name|short
-name|if_unit
+name|struct
+name|ifnet
+modifier|*
+name|if_next
 decl_stmt|;
-comment|/* sub-unit for lower level driver */
-name|short
-name|if_mtu
-decl_stmt|;
-comment|/* maximum transmission unit */
-name|short
-name|if_flags
-decl_stmt|;
-comment|/* up/down, broadcast, etc. */
-name|short
-name|if_timer
-decl_stmt|;
-comment|/* time 'til if_watchdog called */
-name|int
-name|if_metric
-decl_stmt|;
-comment|/* routing metric (external only) */
+comment|/* all struct ifnets are chained */
 name|struct
 name|ifaddr
 modifier|*
 name|if_addrlist
 decl_stmt|;
 comment|/* linked list of addresses per if */
+name|int
+name|if_pcount
+decl_stmt|;
+comment|/* number of promiscuous listeners */
+name|u_short
+name|if_index
+decl_stmt|;
+comment|/* numeric abbreviation for this if  */
+name|short
+name|if_unit
+decl_stmt|;
+comment|/* sub-unit for lower level driver */
+name|short
+name|if_timer
+decl_stmt|;
+comment|/* time 'til if_watchdog called */
+name|short
+name|if_flags
+decl_stmt|;
+comment|/* up/down, broadcast, etc. */
 struct|struct
-name|ifqueue
+name|if_data
 block|{
+comment|/* generic interface information */
+name|short
+name|ifi_mtu
+decl_stmt|;
+comment|/* maximum transmission unit */
+name|u_char
+name|ifi_type
+decl_stmt|;
+comment|/* ethernet, tokenring, etc */
+name|u_char
+name|ifi_addrlen
+decl_stmt|;
+comment|/* media address length */
+name|u_char
+name|ifi_hdrlen
+decl_stmt|;
+comment|/* media header length */
+name|int
+name|ifi_metric
+decl_stmt|;
+comment|/* routing metric (external only) */
+name|int
+name|ifi_baudrate
+decl_stmt|;
+comment|/* linespeed */
+comment|/* volatile statistics */
+name|int
+name|ifi_ipackets
+decl_stmt|;
+comment|/* packets received on interface */
+name|int
+name|ifi_ierrors
+decl_stmt|;
+comment|/* input errors on interface */
+name|int
+name|ifi_opackets
+decl_stmt|;
+comment|/* packets sent on interface */
+name|int
+name|ifi_oerrors
+decl_stmt|;
+comment|/* output errors on interface */
+name|int
+name|ifi_collisions
+decl_stmt|;
+comment|/* collisions on csma interfaces */
+name|int
+name|ifi_ibytes
+decl_stmt|;
+comment|/* total number of octets received */
+name|int
+name|ifi_obytes
+decl_stmt|;
+comment|/* total number of octets sent */
+name|int
+name|ifi_imcasts
+decl_stmt|;
+comment|/* packets received via multicast */
+name|int
+name|ifi_omcasts
+decl_stmt|;
+comment|/* packets sent via multicast */
+name|int
+name|ifi_iqdrops
+decl_stmt|;
+comment|/* dropped on input, this interface */
+name|int
+name|ifi_noproto
+decl_stmt|;
+comment|/* destined for unsupported protocol */
 name|struct
-name|mbuf
-modifier|*
-name|ifq_head
+name|timeval
+name|ifi_lastchange
 decl_stmt|;
-name|struct
-name|mbuf
-modifier|*
-name|ifq_tail
-decl_stmt|;
-name|int
-name|ifq_len
-decl_stmt|;
-name|int
-name|ifq_maxlen
-decl_stmt|;
-name|int
-name|ifq_drops
-decl_stmt|;
+comment|/* last updated */
 block|}
-name|if_snd
+name|if_data
 struct|;
-comment|/* output queue */
 comment|/* procedure handles */
 name|int
 argument_list|(
@@ -249,90 +314,161 @@ name|int
 operator|)
 argument_list|)
 expr_stmt|;
-comment|/* generic interface statistics */
-name|int
-name|if_ipackets
-decl_stmt|;
-comment|/* packets received on interface */
-name|int
-name|if_ierrors
-decl_stmt|;
-comment|/* input errors on interface */
-name|int
-name|if_opackets
-decl_stmt|;
-comment|/* packets sent on interface */
-name|int
-name|if_oerrors
-decl_stmt|;
-comment|/* output errors on interface */
-name|int
-name|if_collisions
-decl_stmt|;
-comment|/* collisions on csma interfaces */
-comment|/* end statistics */
+struct|struct
+name|ifqueue
+block|{
 name|struct
-name|ifnet
+name|mbuf
 modifier|*
-name|if_next
+name|ifq_head
 decl_stmt|;
-name|u_char
-name|if_type
-decl_stmt|;
-comment|/* ethernet, tokenring, etc */
-name|u_char
-name|if_addrlen
-decl_stmt|;
-comment|/* media address length */
-name|u_char
-name|if_hdrlen
-decl_stmt|;
-comment|/* media header length */
-name|u_char
-name|if_index
-decl_stmt|;
-comment|/* numeric abbreviation for this if  */
-comment|/* more statistics here to avoid recompiling netstat */
 name|struct
-name|timeval
-name|if_lastchange
+name|mbuf
+modifier|*
+name|ifq_tail
 decl_stmt|;
-comment|/* last updated */
 name|int
-name|if_ibytes
+name|ifq_len
 decl_stmt|;
-comment|/* total number of octets received */
 name|int
-name|if_obytes
+name|ifq_maxlen
 decl_stmt|;
-comment|/* total number of octets sent */
 name|int
-name|if_imcasts
+name|ifq_drops
 decl_stmt|;
-comment|/* packets received via multicast */
-name|int
-name|if_omcasts
-decl_stmt|;
-comment|/* packets sent via multicast */
-name|int
-name|if_iqdrops
-decl_stmt|;
-comment|/* dropped on input, this interface */
-name|int
-name|if_noproto
-decl_stmt|;
-comment|/* destined for unsupported protocol */
-name|int
-name|if_baudrate
-decl_stmt|;
-comment|/* linespeed */
-name|int
-name|if_pcount
-decl_stmt|;
-comment|/* number of promiscuous listeners */
+block|}
+name|if_snd
+struct|;
+comment|/* output queue */
 block|}
 struct|;
 end_struct
+
+begin_define
+define|#
+directive|define
+name|if_mtu
+value|if_data.ifi_mtu
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_type
+value|if_data.ifi_type
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_addrlen
+value|if_data.ifi_addrlen
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_hdrlen
+value|if_data.ifi_hdrlen
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_metric
+value|if_data.ifi_metric
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_baudrate
+value|if_data.ifi_baudrate
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_ipackets
+value|if_data.ifi_ipackets
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_ierrors
+value|if_data.ifi_ierrors
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_opackets
+value|if_data.ifi_opackets
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_oerrors
+value|if_data.ifi_oerrors
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_collisions
+value|if_data.ifi_collisions
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_ibytes
+value|if_data.ifi_ibytes
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_obytes
+value|if_data.ifi_obytes
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_imcasts
+value|if_data.ifi_imcasts
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_omcasts
+value|if_data.ifi_omcasts
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_iqdrops
+value|if_data.ifi_iqdrops
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_noproto
+value|if_data.ifi_noproto
+end_define
+
+begin_define
+define|#
+directive|define
+name|if_lastchange
+value|if_data.ifi_lastchange
+end_define
 
 begin_define
 define|#
@@ -644,12 +780,6 @@ function_decl|)
 parameter_list|()
 function_decl|;
 comment|/* check or clean routes (+ or -)'d */
-name|struct
-name|rtentry
-modifier|*
-name|ifa_rt
-decl_stmt|;
-comment|/* ??? for ROUTETOIF */
 name|u_short
 name|ifa_flags
 decl_stmt|;
@@ -658,6 +788,11 @@ name|short
 name|ifa_refcnt
 decl_stmt|;
 comment|/* extra to malloc for link info */
+name|int
+name|ifa_metric
+decl_stmt|;
+comment|/* cost of going out this interface */
+comment|/*	struct	rtentry *ifa_rt;	/* XXXX for ROUTETOIF ????? */
 block|}
 struct|;
 end_struct
@@ -672,6 +807,87 @@ end_define
 begin_comment
 comment|/* route installed */
 end_comment
+
+begin_comment
+comment|/*  * Message format for use in obtaining information about interfaces  * from getkerninfo and the routing socket  */
+end_comment
+
+begin_struct
+struct|struct
+name|if_msghdr
+block|{
+name|u_short
+name|ifm_msglen
+decl_stmt|;
+comment|/* to skip over non-understood messages */
+name|u_char
+name|ifm_version
+decl_stmt|;
+comment|/* future binary compatability */
+name|u_char
+name|ifm_type
+decl_stmt|;
+comment|/* message type */
+name|int
+name|ifm_addrs
+decl_stmt|;
+comment|/* like rtm_addrs */
+name|int
+name|ifm_flags
+decl_stmt|;
+comment|/* value of if_flags */
+name|u_short
+name|ifm_index
+decl_stmt|;
+comment|/* index for associated ifp */
+name|struct
+name|if_data
+name|ifm_data
+decl_stmt|;
+comment|/* statistics and other data about if */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * Message format for use in obtaining information about interface addresses  * from getkerninfo and the routing socket  */
+end_comment
+
+begin_struct
+struct|struct
+name|ifa_msghdr
+block|{
+name|u_short
+name|ifam_msglen
+decl_stmt|;
+comment|/* to skip over non-understood messages */
+name|u_char
+name|ifam_version
+decl_stmt|;
+comment|/* future binary compatability */
+name|u_char
+name|ifam_type
+decl_stmt|;
+comment|/* message type */
+name|int
+name|ifam_addrs
+decl_stmt|;
+comment|/* like rtm_addrs */
+name|int
+name|ifam_flags
+decl_stmt|;
+comment|/* value of ifa_flags */
+name|u_short
+name|ifam_index
+decl_stmt|;
+comment|/* index for associated ifp */
+name|int
+name|ifam_metric
+decl_stmt|;
+comment|/* value of ifa_metric */
+block|}
+struct|;
+end_struct
 
 begin_comment
 comment|/*  * Interface request structure used for socket  * ioctl's.  All interface ioctl's must have parameter  * definitions which begin with ifr_name.  The  * remainder may be interface specific.  */
