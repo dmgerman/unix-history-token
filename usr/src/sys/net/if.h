e68956e23e1b0fda4760e45fae98389bab956f2f
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-02-19 19:28:29 -0800

    stop stupid warningsL`struct mbuf' declared inside parameter list
    SCCS-vsn: sys/net/if.h 7.17
    
    Former-commit-id: cbe2f1129f5b6037c443c29f6e2395553e78bd62

--- usr/src/sys/net/if.h
+++ usr/src/sys/net/if.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if.h	7.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if.h	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -63,6 +63,12 @@ end_comment
 begin_struct_decl
 struct_decl|struct
 name|rtentry
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|mbuf
 struct_decl|;
 end_struct_decl
 
