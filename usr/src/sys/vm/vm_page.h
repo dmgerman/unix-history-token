7dc5ae89e0fb57dbdcb5e11e8d99f4bba0584757
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-02-20 02:05:01 -0800

    define VM_PAGE_INIT as vm_page_init was only used in one place
    SCCS-vsn: sys/vm/vm_page.h 7.6
    
    Former-commit-id: be74736436f41eee1926168e2a492e1b1ca7da4c

--- usr/src/sys/vm/vm_page.h
+++ usr/src/sys/vm/vm_page.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_page.h	7.5 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_page.h	7.6 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -422,25 +422,6 @@ end_decl_stmt
 
 begin_decl_stmt
 name|void
-name|vm_page_init
-name|__P
-argument_list|(
-operator|(
-name|vm_page_t
-name|mem
-operator|,
-name|vm_object_t
-name|object
-operator|,
-name|vm_offset_t
-name|offset
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|void
 name|vm_page_free
 name|__P
 argument_list|(
@@ -614,6 +595,55 @@ parameter_list|(
 name|m
 parameter_list|)
 value|{ (m)->clean = FALSE; }
+end_define
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|DEBUG
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|VM_PAGE_DEBUG_INIT
+parameter_list|(
+name|m
+parameter_list|)
+value|((m)->pagerowned = 0, (m)->ptpage = 0)
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|VM_PAGE_DEBUG_INIT
+parameter_list|(
+name|m
+parameter_list|)
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_define
+define|#
+directive|define
+name|VM_PAGE_INIT
+parameter_list|(
+name|mem
+parameter_list|,
+name|object
+parameter_list|,
+name|offset
+parameter_list|)
+value|{ \ 	(mem)->busy = TRUE; \ 	(mem)->tabled = FALSE; \ 	vm_page_insert((mem), (object), (offset)); \ 	(mem)->absent = FALSE; \ 	(mem)->fictitious = FALSE; \ 	(mem)->page_lock = VM_PROT_NONE; \ 	(mem)->unlock_request = VM_PROT_NONE; \ 	(mem)->laundry = FALSE; \ 	(mem)->active = FALSE; \ 	(mem)->inactive = FALSE; \ 	(mem)->wire_count = 0; \ 	(mem)->clean = TRUE; \ 	(mem)->copy_on_write = FALSE; \ 	(mem)->fake = TRUE; \ 	VM_PAGE_DEBUG_INIT(mem); \ }
 end_define
 
 begin_endif
