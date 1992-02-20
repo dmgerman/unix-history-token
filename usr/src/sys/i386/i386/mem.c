56581dd6cde833c8153ee0a05f6304a5e8be5bc6
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-02-20 02:30:04 -0800

    eliminate function pmap_kernel
    SCCS-vsn: sys/i386/i386/machdep.c 7.9
    SCCS-vsn: sys/i386/i386/mem.c 7.5
    
    Former-commit-id: 68190708e6aaf045d7da0cd957c832b9f04e5fe5

--- usr/src/sys/i386/i386/mem.c
+++ usr/src/sys/i386/i386/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: mem.c 1.13 89/10/08$  *	@(#)mem.c	7.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: mem.c 1.13 89/10/08$  *	@(#)mem.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -221,8 +221,7 @@ name|uio_offset
 expr_stmt|;
 name|pmap_enter
 argument_list|(
-name|pmap_kernel
-argument_list|()
+name|kernel_pmap
 argument_list|,
 name|vmmap
 argument_list|,
@@ -325,8 +324,7 @@ argument_list|)
 expr_stmt|;
 name|pmap_remove
 argument_list|(
-name|pmap_kernel
-argument_list|()
+name|kernel_pmap
 argument_list|,
 name|vmmap
 argument_list|,
