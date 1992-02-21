88dcb2f4ec41d1390ea28cf501bea4239b790768
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-02-20 21:47:44 -0800

    proc.h not necessary
    SCCS-vsn: sys/netinet/in_pcb.c 7.17
    
    Former-commit-id: e84018a53205658fed0d09c9d8cee858453a307e

--- usr/src/sys/netinet/in_pcb.c
+++ usr/src/sys/netinet/in_pcb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)in_pcb.c	7.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)in_pcb.c	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -13,12 +13,6 @@ begin_include
 include|#
 directive|include
 file|"systm.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"proc.h"
 end_include
 
 begin_include
