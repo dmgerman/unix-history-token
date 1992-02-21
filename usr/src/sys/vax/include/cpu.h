42f731bac2e5d8129c399b7ecaf09b39925b28f3
Mike Karels <karels@ucbvax.Berkeley.EDU>
1992-02-20 23:22:01 -0800

    (by sklower) /sys/net files included mtpr.h which was vax and tahoe specific; now they include cpu.h, which in tern includes mtpr.h ...
    SCCS-vsn: sys/vax/include/cpu.h 7.7
    
    Former-commit-id: 685e2db5f6619b00bfd727cd2f244096060433c5

--- usr/src/sys/vax/include/cpu.h
+++ usr/src/sys/vax/include/cpu.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)cpu.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)cpu.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -650,6 +650,23 @@ comment|/* "unibus" IO page address */
 block|}
 struct|;
 end_struct
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_MTPR_H_
+end_ifndef
+
+begin_include
+include|#
+directive|include
+file|"mtpr.h"
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifdef
 ifdef|#
