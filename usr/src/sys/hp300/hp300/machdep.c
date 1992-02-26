58d3bc4013b1f6dafa1448235a7bdd81721cd535
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-02-26 00:34:53 -0800

    lint
    SCCS-vsn: sys/hp300/hp300/machdep.c 7.23
    
    Former-commit-id: fb7cb70dc3fb37e27a8cc3f4011e01a713af80fc

--- usr/src/sys/hp300/hp300/machdep.c
+++ usr/src/sys/hp300/hp300/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.63 91/04/24$  *  *	@(#)machdep.c	7.22 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.63 91/04/24$  *  *	@(#)machdep.c	7.23 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -821,6 +821,7 @@ name|kernel_map
 argument_list|,
 operator|(
 name|vm_offset_t
+operator|*
 operator|)
 operator|&
 name|buffers
@@ -1029,6 +1030,7 @@ name|kernel_map
 argument_list|,
 operator|(
 name|vm_offset_t
+operator|*
 operator|)
 operator|&
 name|mbutl
