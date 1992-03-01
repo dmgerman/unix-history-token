52e5b9ba11fc318a57ebd1163bc373bc15d82439
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-02-29 22:46:47 -0800

    removed kvtophys; not used.
    SCCS-vsn: sys/pmax/include/pte.h 7.2
    
    Former-commit-id: 605957b68334be745f83df1f892f2cadeed1e9a6

--- usr/src/sys/pmax/include/pte.h
+++ usr/src/sys/pmax/include/pte.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: pte.h 1.11 89/09/03$  *  *	@(#)pte.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: pte.h 1.11 89/09/03$  *  *	@(#)pte.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -252,7 +252,7 @@ value|(((x)& PG_FRAME)>> PG_SHIFT)
 end_define
 
 begin_comment
-comment|/*  * Kernel virtual address to page table entry and to physical address.  */
+comment|/*  * Kernel virtual address to page table entry and visa versa.  */
 end_comment
 
 begin_define
@@ -275,17 +275,6 @@ name|pte
 parameter_list|)
 define|\
 value|((((pt_entry_t *)(pte) - PMAP_HASH_KADDR)<< PGSHIFT) + \ 	VM_MIN_KERNEL_ADDRESS)
-end_define
-
-begin_define
-define|#
-directive|define
-name|kvtophys
-parameter_list|(
-name|va
-parameter_list|)
-define|\
-value|((kvtopte(va)->pt_entry& PG_FRAME) | ((unsigned)(va)& PGOFSET))
 end_define
 
 end_unit
