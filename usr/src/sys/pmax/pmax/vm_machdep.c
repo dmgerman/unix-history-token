37a217c6252f1860a925f4e3347be84bc6c2b47e
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-01 01:54:18 -0800

    remove p_regs
    SCCS-vsn: sys/pmax/pmax/vm_machdep.c 7.3
    
    Former-commit-id: d3c6d3848a0f2d06d2bb38f71a00ebbfbd6dc77e

--- usr/src/sys/pmax/pmax/vm_machdep.c
+++ usr/src/sys/pmax/pmax/vm_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_machdep.c 1.21 91/04/06$  *  *	@(#)vm_machdep.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_machdep.c 1.21 91/04/06$  *  *	@(#)vm_machdep.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -114,7 +114,9 @@ name|machFPCurProcPtr
 decl_stmt|;
 name|p2
 operator|->
-name|p_regs
+name|p_md
+operator|.
+name|md_regs
 operator|=
 name|up
 operator|->
