f6b5404d82b9410755dd96f376ceda939b6ee056
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-02 04:55:03 -0800

    don't include mtpr.h unless needed
    SCCS-vsn: sys/ufs/mfs/mfs_vnops.c 7.27
    
    Former-commit-id: 1e5286a20e95257eff5305957c49537ffdafb697

--- usr/src/sys/ufs/mfs/mfs_vnops.c
+++ usr/src/sys/ufs/mfs/mfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mfs_vnops.c	7.26 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mfs_vnops.c	7.27 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -63,11 +63,25 @@ directive|include
 file|<machine/vmparam.h>
 end_include
 
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|tahoe
+argument_list|)
+end_if
+
 begin_include
 include|#
 directive|include
 file|<machine/mtpr.h>
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
