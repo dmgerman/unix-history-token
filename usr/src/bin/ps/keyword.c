a8f53970cdc5d306a6369aaf184d32d80292d5f5
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-02 04:53:11 -0800

    don't bsearch() the null entry.
    SCCS-vsn: bin/ps/keyword.c 5.11
    
    Former-commit-id: 14c5f3791ba385d48a2dc9f4bcf7af925d55051c

--- usr/src/bin/ps/keyword.c
+++ usr/src/bin/ps/keyword.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)keyword.c	5.10 (Berkeley) %G%"
+literal|"@(#)keyword.c	5.11 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3649,6 +3649,8 @@ sizeof|sizeof
 argument_list|(
 name|VAR
 argument_list|)
+operator|-
+literal|1
 argument_list|,
 sizeof|sizeof
 argument_list|(
