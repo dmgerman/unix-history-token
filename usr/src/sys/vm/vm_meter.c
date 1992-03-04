df9729d356e8335f7e88ff7d3a694136e8b495f1
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-03-04 00:08:43 -0800

    avoid NULL object pointer dereference in vmtotal
    SCCS-vsn: sys/vm/vm_meter.c 7.14
    
    Former-commit-id: 36bbc0aa00421c3f6c3576882ebfacf7beafc514

--- usr/src/sys/vm/vm_meter.c
+++ usr/src/sys/vm/vm_meter.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vm_meter.c	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vm_meter.c	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -929,6 +929,14 @@ operator|||
 name|entry
 operator|->
 name|is_sub_map
+operator|||
+name|entry
+operator|->
+name|object
+operator|.
+name|vm_object
+operator|==
+name|NULL
 condition|)
 continue|continue;
 name|entry
