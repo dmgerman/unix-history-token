2b2d2118a026143b4cfee27c883e70049b28953f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-07 19:10:23 -0800

    break ufs up
    SCCS-vsn: usr.sbin/mkproto/mkproto.c 5.8
    
    Former-commit-id: 6574a836e73ef3eea87581a7532fc2c78ab8785b

--- usr/src/usr.sbin/mkproto/mkproto.c
+++ usr/src/usr.sbin/mkproto/mkproto.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mkproto.c	5.7 (Berkeley) %G%"
+literal|"@(#)mkproto.c	5.8 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -71,13 +71,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<ufs/dinode.h>
+file|<ufs/ufs/dinode.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<ufs/fs.h>
+file|<ufs/ffs/fs.h>
 end_include
 
 begin_include
