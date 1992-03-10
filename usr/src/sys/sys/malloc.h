1d7e4c968e42415f778d856e1994fa17401eb561
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-03-10 00:56:22 -0800

    typo
    SCCS-vsn: sys/sys/malloc.h 7.30
    
    Former-commit-id: b31b6bec7778adb05970541bf613748526eef64a

--- usr/src/sys/sys/malloc.h
+++ usr/src/sys/sys/malloc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)malloc.h	7.29 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)malloc.h	7.30 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -954,7 +954,7 @@ argument_list|)
 operator|||
 name|defined
 argument_list|(
-name|DIAGNOSTICS
+name|DIAGNOSTIC
 argument_list|)
 end_if
 
