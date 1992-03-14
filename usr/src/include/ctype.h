dade7558a0c078b056a445df114b3788e061d925
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-03-13 22:06:37 -0800

    __ctype_ should be __ctype
    SCCS-vsn: include/ctype.h 5.7
    
    Former-commit-id: 83aa644a7c85c118bc1e0a0c3b9fac57b8e828d5

--- usr/src/include/ctype.h
+++ usr/src/include/ctype.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ctype.h	5.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ctype.h	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -74,7 +74,7 @@ end_define
 begin_decl_stmt
 specifier|extern
 name|char
-name|__ctype_
+name|__ctype
 index|[]
 decl_stmt|,
 name|__maplower
