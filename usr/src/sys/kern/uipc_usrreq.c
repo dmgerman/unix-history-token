9269970223b6f1f5a43352f4e575f57a47647c07
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-03-15 21:35:57 -0800

    must include systm.h to get decl for panic (for gcc)
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.32
    
    Former-commit-id: ef1eb472665b7afd07d18635a1c42b8318207a6c

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,12 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  * %sccs.include.redist.c%  *  *	@(#)uipc_usrreq.c	7.31 (Berkeley) %G%  */
+comment|/*  *  * %sccs.include.redist.c%  *  *	@(#)uipc_usrreq.c	7.32 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"param.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"systm.h"
 end_include
 
 begin_include
