69956644220294064ea31b6c202ca585ed6bba09
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-03-16 23:27:44 -0800

    fix from Rick Macklem for multiple free's of NFSD structures
    SCCS-vsn: sys/nfs/nfs.h 7.15
    SCCS-vsn: sys/nfs/nfs_socket.c 7.28
    SCCS-vsn: sys/nfs/nfs_subs.c 7.48
    
    Former-commit-id: 5c8e6291bbf5fb7dc9d07f647296f8bd377c63ab

--- usr/src/sys/nfs/nfs.h
+++ usr/src/sys/nfs/nfs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs.h	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs.h	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1045,6 +1045,20 @@ end_endif
 begin_comment
 comment|/* KERNEL */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|SLP_INIT
+value|0x20
+end_define
+
+begin_define
+define|#
+directive|define
+name|SLP_WANTINIT
+value|0x40
+end_define
 
 end_unit
 
--- usr/src/sys/nfs/nfs_subs.c
+++ usr/src/sys/nfs/nfs_subs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_subs.c	7.47 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_subs.c	7.48 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -3261,6 +3261,12 @@ name|nfs_nhinit
 argument_list|()
 expr_stmt|;
 comment|/* Init the nfsnode table */
+name|nfsrv_init
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
+comment|/* Init server data structures */
 name|nfsrv_initcache
 argument_list|()
 expr_stmt|;
