1099a9873a518c0c2250924a683066ee5f78fa22
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-03-19 02:05:41 -0800

    define boottime
    SCCS-vsn: sys/kern/init_main.c 7.47
    
    Former-commit-id: a3b03c241dea094f0245b45698885ed6445b24d9

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)init_main.c	7.46 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)init_main.c	7.47 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -243,6 +243,13 @@ end_decl_stmt
 begin_decl_stmt
 name|int
 name|boothowto
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|timeval
+name|boottime
 decl_stmt|;
 end_decl_stmt
 
