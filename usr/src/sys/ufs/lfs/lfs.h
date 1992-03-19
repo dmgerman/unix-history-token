870159e18a028d32950bec7b3545076bbbff41b9
Carl Staelin <staelin@ucbvax.Berkeley.EDU>
1992-03-18 20:59:55 -0800

    typo
    SCCS-vsn: sys/ufs/lfs/lfs.h 7.11
    
    Former-commit-id: c64df349f1b7faac6455687ee26f02ae17283fd7

--- usr/src/sys/ufs/lfs/lfs.h
+++ usr/src/sys/ufs/lfs/lfs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs.h	7.10 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs.h	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_typedef
@@ -515,7 +515,7 @@ name|D_INDIR
 parameter_list|(
 name|fs
 parameter_list|)
-value|(S_INDIR - NINDIR(fs) - 1)
+value|(S_INDIR(fs) - NINDIR(fs) - 1)
 end_define
 
 begin_define
@@ -525,7 +525,7 @@ name|T_INDIR
 parameter_list|(
 name|fs
 parameter_list|)
-value|(D_INDIR - NINDIR(fs) * NINDIR(fs) - 1)
+value|(D_INDIR(fs) - NINDIR(fs) * NINDIR(fs) - 1)
 end_define
 
 begin_comment
