719e0b9ea98b013892ddf9ed6b3cb513079e9868
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-23 04:40:57 -0800

    added support for memory mini-root.
    SCCS-vsn: sys/sys/reboot.h 7.7
    
    Former-commit-id: a724617402779d7939f2d4938dabd74c5b3d5452

--- usr/src/sys/sys/reboot.h
+++ usr/src/sys/sys/reboot.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)reboot.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)reboot.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -115,6 +115,17 @@ end_define
 
 begin_comment
 comment|/* dump kernel memory before reboot */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RB_MINIROOT
+value|0x200
+end_define
+
+begin_comment
+comment|/* mini-root present in memory at boot time */
 end_comment
 
 begin_comment
