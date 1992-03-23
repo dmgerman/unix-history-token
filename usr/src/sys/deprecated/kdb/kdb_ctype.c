f8f0ac1028a473fc6562de275fb714ac6a70890b
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-23 04:39:37 -0800

    fix name of __ctype to match ctype.h
    SCCS-vsn: sys/deprecated/kdb/kdb_ctype.c 7.3
    
    Former-commit-id: 45c291965f658d2372cb25f0a1543510b8de6442

--- usr/src/sys/deprecated/kdb/kdb_ctype.c
+++ usr/src/sys/deprecated/kdb/kdb_ctype.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kdb_ctype.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1986 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)kdb_ctype.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -11,7 +11,7 @@ end_include
 
 begin_decl_stmt
 name|char
-name|_ctype_
+name|__ctype
 index|[
 literal|1
 operator|+
