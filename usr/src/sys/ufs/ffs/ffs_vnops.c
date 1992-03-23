d4b27d3b399cb2d8c8c7b6d2404bf05d3f89a8a8
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-23 04:38:06 -0800

    remove ITOV and use vnode pointer instead.
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.70
    SCCS-vsn: sys/ufs/ffs/ffs_balloc.c 7.16
    
    Former-commit-id: a5e1f92b8f5ee72b996ef86f1416236702e69a4c

--- usr/src/sys/ufs/ffs/ffs_vnops.c
+++ usr/src/sys/ufs/ffs/ffs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	7.69 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	7.70 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -851,10 +851,7 @@ name|error
 operator|=
 name|breadn
 argument_list|(
-name|ITOV
-argument_list|(
-name|ip
-argument_list|)
+name|vp
 argument_list|,
 name|lbn
 argument_list|,
@@ -880,10 +877,7 @@ name|error
 operator|=
 name|bread
 argument_list|(
-name|ITOV
-argument_list|(
-name|ip
-argument_list|)
+name|vp
 argument_list|,
 name|lbn
 argument_list|,
