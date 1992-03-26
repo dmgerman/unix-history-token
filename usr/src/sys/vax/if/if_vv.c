6ee69a01ebe64006bc7afa407aa1db10f168ae8a
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-03-25 18:53:27 -0800

    M_PREPEND has already freed if no mbufs available.
    SCCS-vsn: sys/vax/if/if_vv.c 7.11
    
    Former-commit-id: 2f07e8b7c5972aa3570d9a0b5976fcb99bb72189

--- usr/src/sys/vax/if/if_vv.c
+++ usr/src/sys/vax/if/if_vv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_vv.c	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_vv.c	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -4428,15 +4428,11 @@ name|m
 operator|==
 literal|0
 condition|)
-block|{
-name|error
-operator|=
+return|return
+operator|(
 name|ENOBUFS
-expr_stmt|;
-goto|goto
-name|bad
-goto|;
-block|}
+operator|)
+return|;
 name|vv
 operator|=
 name|mtod
