afdf54651219f8cdc498dc92aa693eb8ccb1ac75
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-03-28 01:39:19 -0800

    define doff_t for size of directory offsets
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 7.83
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 7.83
    SCCS-vsn: sys/ufs/ffs/ufs_extern.h 7.4
    SCCS-vsn: sys/ufs/ufs/ufs_extern.h 7.4
    
    Former-commit-id: 31e069bef372a2a927de1d0b1753cb2fd0633d08

--- usr/src/sys/ufs/ffs/ufs_extern.h
+++ usr/src/sys/ufs/ffs/ufs_extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_extern.h	7.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_extern.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -410,7 +410,7 @@ expr|struct
 name|inode
 operator|*
 operator|,
-name|off_t
+name|doff_t
 operator|,
 name|char
 operator|*
--- usr/src/sys/ufs/ffs/ufs_vnops.c
+++ usr/src/sys/ufs/ffs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	7.82 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	7.83 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -4855,6 +4855,9 @@ name|ufs_dirbad
 argument_list|(
 name|xp
 argument_list|,
+operator|(
+name|doff_t
+operator|)
 literal|12
 argument_list|,
 literal|"rename: mangled dir"
