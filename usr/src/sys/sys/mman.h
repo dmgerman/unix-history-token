01379a8d34e0239bbb545924625cf15815fe8040
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-30 15:47:20 -0800

    fix spelling MAP_HASSEMPHORE -> MAP_HASSEMAPHORE
    SCCS-vsn: sys/sys/mman.h 7.6
    
    Former-commit-id: bb27720d978d0badd314bf42e747044214ff7f94

--- usr/src/sys/sys/mman.h
+++ usr/src/sys/sys/mman.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mman.h	7.5 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mman.h	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -143,7 +143,7 @@ end_comment
 begin_define
 define|#
 directive|define
-name|MAP_HASSEMPHORE
+name|MAP_HASSEMAPHORE
 value|0x0400
 end_define
 
