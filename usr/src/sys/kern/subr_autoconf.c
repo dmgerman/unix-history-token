c36546cb1d68e9986d2ecf88543900ef870a8599
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-04-02 01:13:12 -0800

    fix LBL contrib notice
    SCCS-vsn: sys/sys/device.h 7.2
    SCCS-vsn: sys/sys/fbio.h 7.2
    SCCS-vsn: sys/kern/subr_autoconf.c 7.2
    
    Former-commit-id: 6931296fb02c6d619ae10f6b6b80fbe0b711ce74

--- usr/src/sys/kern/subr_autoconf.c
+++ usr/src/sys/kern/subr_autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Computer Systems Engineering group at Lawrence Berkeley  * Laboratory under DARPA contract BG 91-66.  *  * %sccs.include.redist.c%  *  *	@(#)subr_autoconf.c	7.1 (Berkeley) %G%  *  * from: $Header: subr_autoconf.c,v 1.3 91/11/23 00:53:49 torek Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software developed by the Computer Systems  * Engineering group at Lawrence Berkeley Laboratory under DARPA  * contract BG 91-66 and contributed to Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)subr_autoconf.c	7.2 (Berkeley) %G%  *  * from: $Header: subr_autoconf.c,v 1.3 91/11/23 00:53:49 torek Exp $ (LBL)  */
 end_comment
 
 begin_include
--- usr/src/sys/sys/device.h
+++ usr/src/sys/sys/device.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Computer Systems Engineering group at Lawrence Berkeley  * Laboratory under DARPA contract BG 91-66.  *  * %sccs.include.redist.c%  *  *	@(#)device.h	7.1 (Berkeley) %G%  *  * from: $Header: device.h,v 1.3 92/01/15 18:25:53 torek Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software developed by the Computer Systems  * Engineering group at Lawrence Berkeley Laboratory under DARPA  * contract BG 91-66 and contributed to Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)device.h	7.2 (Berkeley) %G%  *  * from: $Header: device.h,v 1.3 92/01/15 18:25:53 torek Exp $ (LBL)  */
 end_comment
 
 begin_comment
