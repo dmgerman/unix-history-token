d7fd8b0964d7c5005eb150f63146753c65a89d58
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-04-04 14:39:34 -0800

    386BSD 0.1 development Work on file usr/src/usr.bin/diff/dir.c Work on file usr/src/usr.bin/diff/ed.c Work on file usr/src/usr.bin/join/getopt.h Work on file usr/src/usr.bin/tail/getopt.h Work on file usr/src/usr.bin/diff/getopt.h Work on file usr/src/usr.bin/pr/getopt.h Work on file usr/src/usr.bin/join/getopt1.c Work on file usr/src/usr.bin/diff/getopt1.c Work on file usr/src/usr.bin/tail/getopt1.c Work on file usr/src/usr.bin/diff/ifdef.c Work on file usr/src/usr.bin/diff/limits.h Work on file usr/src/usr.bin/diff/normal.c Work on file usr/src/usr.bin/diff/version.c
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1
    
    Former-commit-id: 38d057644d64d871ab0075d6ca7e6398507a9316

---
+++ usr/src/usr.bin/diff/version.c
@@ -0,0 +1,16 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* Version number of GNU diff.  */
+end_comment
+
+begin_decl_stmt
+name|char
+modifier|*
+name|version_string
+init|=
+literal|"1.15"
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
