6438ef39666eaefc6b6b565164a64d92728f341f
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-04-16 00:27:59 -0800

    split out trace points into default and complete list
    SCCS-vsn: usr.bin/ktrace/ktrace.h 5.2
    
    Former-commit-id: 43d04becf47e55b1bbf4a87a319769564add5183

--- usr/src/usr.bin/ktrace/ktrace.h
+++ usr/src/usr.bin/ktrace/ktrace.h
@@ -1,13 +1,20 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ktrace.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ktrace.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|ALL_POINTS
+name|DEF_POINTS
 value|(KTRFAC_SYSCALL | KTRFAC_SYSRET | KTRFAC_NAMEI | \ 		  KTRFAC_GENIO | KTRFAC_PSIG)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ALL_POINTS
+value|(DEF_POINTS | KTRFAC_CSW)
 end_define
 
 begin_define
