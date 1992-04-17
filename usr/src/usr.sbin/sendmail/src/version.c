126160bb25a463f68cd6518a84cadefb2d71cf78
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-04-17 01:40:56 -0800

    properly set up envelope when returning email; try to eliminate some of the CurEnv globals
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.96
    
    Former-commit-id: 164d9411f4e4f4f2e2a406c2125948a858bca149

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	5.95 (Berkeley) %G%"
+literal|"@(#)version.c	5.96 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"5.95"
+literal|"5.96"
 decl_stmt|;
 end_decl_stmt
 
