b2dba6df41c9a129ad31d7dff0dabc973d5429ca
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-04-16 21:36:58 -0800

    check if root has dot in PATH and issue warning
    SCCS-vsn: bin/sh/exec.c 5.3
    
    Former-commit-id: 0722de78fee4019510ada238f7194455a21c06b3

--- usr/src/bin/sh/exec.c
+++ usr/src/bin/sh/exec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)exec.c	5.2 (Berkeley) %G%"
+literal|"@(#)exec.c	5.2 (Berkeley) 3/13/91"
 decl_stmt|;
 end_decl_stmt
 
@@ -2597,6 +2597,9 @@ decl_stmt|;
 name|int
 name|bltin
 decl_stmt|;
+name|int
+name|hasdot
+decl_stmt|;
 name|old
 operator|=
 name|pathval
@@ -2613,12 +2616,24 @@ expr_stmt|;
 comment|/* assume no change */
 name|index
 operator|=
+name|hasdot
+operator|=
 literal|0
 expr_stmt|;
 name|bltin
 operator|=
 operator|-
 literal|1
+expr_stmt|;
+if|if
+condition|(
+operator|*
+name|new
+operator|==
+literal|':'
+condition|)
+name|hasdot
+operator|++
 expr_stmt|;
 for|for
 control|(
@@ -2710,7 +2725,55 @@ operator|==
 literal|':'
 condition|)
 block|{
+name|char
+name|c
+init|=
+operator|*
+operator|(
+name|new
+operator|+
+literal|1
+operator|)
+decl_stmt|;
 name|index
+operator|++
+expr_stmt|;
+if|if
+condition|(
+name|c
+operator|==
+literal|':'
+operator|||
+name|c
+operator|==
+literal|'\0'
+operator|||
+operator|(
+name|c
+operator|==
+literal|'.'
+operator|&&
+operator|(
+operator|(
+name|c
+operator|=
+operator|*
+operator|(
+name|new
+operator|+
+literal|2
+operator|)
+operator|)
+operator|==
+literal|':'
+operator|||
+name|c
+operator|==
+literal|'\0'
+operator|)
+operator|)
+condition|)
+name|hasdot
 operator|++
 expr_stmt|;
 block|}
@@ -2721,6 +2784,20 @@ name|old
 operator|++
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|hasdot
+operator|&&
+name|geteuid
+argument_list|()
+operator|==
+literal|0
+condition|)
+name|out2str
+argument_list|(
+literal|"sh: warning: running as root with dot in PATH\n"
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|builtinloc
