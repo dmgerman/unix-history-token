174a3bc93022565e02446dbceb446a2b43bc608d
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-04-19 15:43:34 -0800

    dkstat.h is not used, removed.
    SCCS-vsn: sys/kern/tty_compat.c 7.14
    
    Former-commit-id: 3b17a5a2ba8b1abe8b7514859664542de9b8c80e

--- usr/src/sys/kern/tty_compat.c
+++ usr/src/sys/kern/tty_compat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tty_compat.c	7.13 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tty_compat.c	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -67,12 +67,6 @@ begin_include
 include|#
 directive|include
 file|"conf.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"dkstat.h"
 end_include
 
 begin_include
