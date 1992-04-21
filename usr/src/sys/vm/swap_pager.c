819a9093a3180b9ab32256c0e3b72619b6bd83ed
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-04-20 21:51:24 -0800

    set dirty offset (from Macklem)
    SCCS-vsn: sys/vm/swap_pager.c 7.6
    
    Former-commit-id: 26cec08035d97b5f9559dcc9ae58b10d94ee712c

--- usr/src/sys/vm/swap_pager.c
+++ usr/src/sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2628,11 +2628,25 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
+name|bp
+operator|->
+name|b_dirtyoff
+operator|=
+literal|0
+expr_stmt|;
+name|bp
+operator|->
+name|b_dirtyend
+operator|=
+name|PAGE_SIZE
+expr_stmt|;
 name|swapdev_vp
 operator|->
 name|v_numoutput
 operator|++
 expr_stmt|;
+block|}
 comment|/* 	 * If this is an async write we set up additional buffer fields 	 * and place a "cleaning" entry on the inuse queue. 	 */
 if|if
 condition|(
