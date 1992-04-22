1a91cb7ef1fa938f592d9875faaad02800025e31
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-04-21 17:55:25 -0800

    ensure proper types for local variables and parameters
    SCCS-vsn: sys/nfs/nfsnode.h 7.18
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.70
    
    Former-commit-id: f69f4867a1c8fdba88269894ac35589f6b44a8cb

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.69 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.70 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -10661,7 +10661,7 @@ name|vnode
 modifier|*
 name|vp
 decl_stmt|;
-name|u_long
+name|off_t
 name|length
 decl_stmt|;
 name|int
--- usr/src/sys/nfs/nfsnode.h
+++ usr/src/sys/nfs/nfsnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfsnode.h	7.17 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfsnode.h	7.18 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1218,7 +1218,7 @@ name|vnode
 operator|*
 name|vp
 operator|,
-name|u_long
+name|off_t
 name|length
 operator|,
 name|int
