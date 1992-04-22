bdce15de58ec076d2d21e54488a4040b6a8702e4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-04-21 17:25:38 -0800

    vop_truncate takes off_t rather than u_long length op
    SCCS-vsn: sys/miscfs/fifofs/fifo.h 7.4
    SCCS-vsn: sys/miscfs/specfs/specdev.h 7.7
    
    Former-commit-id: 9c22e0d5b08254f06d4e76e2ab46d5c2ae3cab89

--- usr/src/sys/miscfs/fifofs/fifo.h
+++ usr/src/sys/miscfs/fifofs/fifo.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -507,7 +507,7 @@ begin_define
 define|#
 directive|define
 name|fifo_truncate
-value|((int (*) __P(( \ 		struct vnode *vp, \ 		u_long length, \ 		int flags))) nullop)
+value|((int (*) __P(( \ 		struct vnode *vp, \ 		off_t length, \ 		int flags))) nullop)
 end_define
 
 begin_define
