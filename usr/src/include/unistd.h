f23ff5f34937d0fc4f1fa1ad2d60004814c5e0c6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-04-22 18:34:22 -0800

    for now lseek takes and returns a long
    SCCS-vsn: include/unistd.h 5.16
    
    Former-commit-id: fdfab67b6632d44d8ff627b9d861ed9ef09ec435

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.15 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.16 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -566,14 +566,14 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|off_t
+name|long
 name|lseek
 name|__P
 argument_list|(
 operator|(
 name|int
 operator|,
-name|off_t
+name|long
 operator|,
 name|int
 operator|)
