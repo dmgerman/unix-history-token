fd9d95755d0f8233f9e46005a88cb720f130216c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-04-29 15:19:50 -0800

    add /etc/shells for the C libarry
    SCCS-vsn: include/paths.h 5.17
    
    Former-commit-id: bd0aaa9c4b2384f43f2be6c438ee7b9fb097c538

--- usr/src/include/paths.h
+++ usr/src/include/paths.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)paths.h	5.16 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)paths.h	5.17 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -108,6 +108,13 @@ define|#
 directive|define
 name|_PATH_SENDMAIL
 value|"/usr/sbin/sendmail"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_SHELLS
+value|"/etc/shells"
 end_define
 
 begin_define
