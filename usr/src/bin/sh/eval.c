d11160f245dd7bc24a80f611dda30ef5787650ac
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-04-30 22:07:52 -0800

    add support for tilde expansion
    SCCS-vsn: bin/sh/eval.c 5.5
    
    Former-commit-id: dbf602210762401b6527afa445043ac1e5e47f29

--- usr/src/bin/sh/eval.c
+++ usr/src/bin/sh/eval.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)eval.c	5.4 (Berkeley) %G%"
+literal|"@(#)eval.c	5.4 (Berkeley) 4/16/92"
 decl_stmt|;
 end_decl_stmt
 
@@ -1383,7 +1383,9 @@ argument_list|,
 operator|&
 name|arglist
 argument_list|,
-literal|1
+name|EXP_FULL
+operator||
+name|EXP_TILDE
 argument_list|)
 expr_stmt|;
 if|if
@@ -1565,7 +1567,7 @@ argument_list|,
 operator|&
 name|arglist
 argument_list|,
-literal|0
+name|EXP_TILDE
 argument_list|)
 expr_stmt|;
 for|for
@@ -1872,7 +1874,9 @@ argument_list|,
 operator|&
 name|fn
 argument_list|,
-literal|0
+name|EXP_TILDE
+operator||
+name|EXP_REDIR
 argument_list|)
 expr_stmt|;
 name|redir
@@ -2691,7 +2695,7 @@ argument_list|,
 operator|&
 name|varlist
 argument_list|,
-literal|0
+name|EXP_VARTILDE
 argument_list|)
 expr_stmt|;
 continue|continue;
@@ -2704,7 +2708,9 @@ argument_list|,
 operator|&
 name|arglist
 argument_list|,
-literal|1
+name|EXP_FULL
+operator||
+name|EXP_TILDE
 argument_list|)
 expr_stmt|;
 name|varflag
@@ -2791,6 +2797,18 @@ name|sp
 operator|->
 name|next
 control|)
+block|{
+name|TRACE
+argument_list|(
+operator|(
+literal|"evalcommand arg: %s\n"
+operator|,
+name|sp
+operator|->
+name|text
+operator|)
+argument_list|)
+expr_stmt|;
 operator|*
 name|argv
 operator|++
@@ -2799,6 +2817,7 @@ name|sp
 operator|->
 name|text
 expr_stmt|;
+block|}
 operator|*
 name|argv
 operator|=
