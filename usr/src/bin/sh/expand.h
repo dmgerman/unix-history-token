a556f1a8571a4d10ed5ddd440ae336a3b269a419
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-04-30 22:09:58 -0800

    add support for tilde
    SCCS-vsn: bin/sh/expand.h 5.2
    
    Former-commit-id: f233889880fd5b9478729287e608561b518358d3

--- usr/src/bin/sh/expand.h
+++ usr/src/bin/sh/expand.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  *  *	@(#)expand.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  *  *	@(#)expand.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_struct
@@ -38,6 +38,54 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+
+begin_comment
+comment|/*  * expandarg() flags  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|EXP_FULL
+value|0x1
+end_define
+
+begin_comment
+comment|/* perform word splitting& file globbing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|EXP_TILDE
+value|0x2
+end_define
+
+begin_comment
+comment|/* do normal tilde expansion */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|EXP_VARTILDE
+value|0x4
+end_define
+
+begin_comment
+comment|/* expand tildes in an assignment */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|EXP_REDIR
+value|0x8
+end_define
+
+begin_comment
+comment|/* file glob for a redirection (1 match only) */
+end_comment
 
 begin_ifdef
 ifdef|#
