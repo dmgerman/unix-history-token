dae01df1fd67d3eacabeb7d8714a51809912f088
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-05 00:23:20 -0800

    some compilers can't handle the appended increment (MIPS)
    SCCS-vsn: sys/ufs/lfs/lfs_cksum.c 7.3
    
    Former-commit-id: e76306279d5870f8e44ee59f3967b57be51bce0f

--- usr/src/sys/ufs/lfs/lfs_cksum.c
+++ usr/src/sys/ufs/lfs/lfs_cksum.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_cksum.c	7.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_cksum.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -74,18 +74,24 @@ argument_list|(
 name|u_short
 argument_list|)
 control|)
+block|{
 name|sum
 operator|^=
 operator|*
-operator|(
 operator|(
 name|u_short
 operator|*
 operator|)
 name|str
-operator|)
-operator|++
 expr_stmt|;
+operator|++
+operator|(
+name|u_short
+operator|*
+operator|)
+name|str
+expr_stmt|;
+block|}
 return|return
 operator|(
 name|sum
