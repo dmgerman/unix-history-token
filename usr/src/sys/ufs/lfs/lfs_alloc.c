0a301674b5696a3555d8d2e7497ccc549acbdcaa
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-04 23:22:23 -0800

    lint
    SCCS-vsn: sys/ufs/ffs/ffs_alloc.c 7.33
    SCCS-vsn: sys/ufs/ffs/ffs_balloc.c 7.18
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.75
    SCCS-vsn: sys/ufs/lfs/lfs_alloc.c 7.42
    SCCS-vsn: sys/ufs/lfs/lfs_vnops.c 7.80
    
    Former-commit-id: 73f0a163250ecd92fd7ec0a3c792b90c72c2d641

--- usr/src/sys/ufs/lfs/lfs_alloc.c
+++ usr/src/sys/ufs/lfs/lfs_alloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_alloc.c	7.41 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_alloc.c	7.42 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -43,6 +43,12 @@ begin_include
 include|#
 directive|include
 file|<sys/malloc.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/vm.h>
 end_include
 
 begin_include
@@ -463,6 +469,9 @@ name|vnode_pager_setsize
 argument_list|(
 name|vp
 argument_list|,
+operator|(
+name|u_long
+operator|)
 name|ip
 operator|->
 name|i_size
--- usr/src/sys/ufs/lfs/lfs_vnops.c
+++ usr/src/sys/ufs/lfs/lfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_vnops.c	7.79 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_vnops.c	7.80 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -91,6 +91,12 @@ begin_include
 include|#
 directive|include
 file|<sys/malloc.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/vm.h>
 end_include
 
 begin_include
