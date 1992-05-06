cd013dac59e7a2c06a448db04fef85efc4a654bb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-05 18:52:28 -0800

    typo
    SCCS-vsn: include/fts.h 5.24
    
    Former-commit-id: 72bbb26cb99b111f3753cee56c6a49f12c1d9376

--- usr/src/include/fts.h
+++ usr/src/include/fts.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fts.h	5.23 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fts.h	5.24 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -285,7 +285,7 @@ comment|/* don't chdir .. to the parent */
 define|#
 directive|define
 name|FTS_SYMFOLLOW
-value|0x01
+value|0x02
 comment|/* followed a symlink to get here */
 name|u_short
 name|fts_flags
