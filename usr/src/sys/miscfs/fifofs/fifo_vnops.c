a822d127f26c563fe75741e1eab67689f38eebd2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-12 00:52:49 -0800

    missing flags argument to sosend (from forys@snake.cs.utah.edu)
    SCCS-vsn: sys/miscfs/fifofs/fifo_vnops.c 7.12
    
    Former-commit-id: f431bd2decbb337dd99ceb2ca4612af20fc77778

--- usr/src/sys/miscfs/fifofs/fifo_vnops.c
+++ usr/src/sys/miscfs/fifofs/fifo_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo_vnops.c	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo_vnops.c	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1179,6 +1179,8 @@ expr|struct
 name|mbuf
 operator|*
 operator|)
+literal|0
+argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
