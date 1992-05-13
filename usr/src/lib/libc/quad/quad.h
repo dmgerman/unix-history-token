40afc1d6edd6cf87e538505dd3b2b1c6229b76d4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-13 01:15:26 -0800

    use BSD conventions for determining ENDIAN; clean up longlong union
    SCCS-vsn: lib/libc/quad/quad.h 5.3
    
    Former-commit-id: af1bbbb91410e59d43272e4d9b7917c4fc3c208a

--- usr/src/lib/libc/quad/quad.h
+++ usr/src/lib/libc/quad/quad.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)quad.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)quad.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -17,82 +17,12 @@ directive|include
 file|<sys/param.h>
 end_include
 
-begin_include
-include|#
-directive|include
-file|<stddef.h>
-end_include
-
 begin_define
 define|#
 directive|define
 name|BITS_PER_WORD
 value|(NBBY * sizeof(long))
 end_define
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|SItype
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|SItype
-value|long int
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* long long ints are pairs of long ints in the order determined by    WORDS_BIG_ENDIAN.  */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|WORDS_BIG_ENDIAN
-end_ifdef
-
-begin_struct
-struct|struct
-name|longlong
-block|{
-name|long
-name|high
-decl_stmt|,
-name|low
-decl_stmt|;
-block|}
-struct|;
-end_struct
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_struct
-struct|struct
-name|longlong
-block|{
-name|long
-name|low
-decl_stmt|,
-name|high
-decl_stmt|;
-block|}
-struct|;
-end_struct
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/* We need this union to unpack/pack longlongs, since we don't have    any arithmetic yet.  Incoming long long parameters are stored    into the `ll' field, and the unpacked result is read from the struct    longlong.  */
@@ -102,34 +32,56 @@ begin_typedef
 typedef|typedef
 union|union
 block|{
-name|struct
-name|longlong
-name|s
-decl_stmt|;
 name|long
 name|long
 name|ll
 decl_stmt|;
-name|SItype
-name|i
-index|[
-literal|2
-index|]
-decl_stmt|;
-name|unsigned
-name|SItype
-name|ui
+struct|struct
+block|{
+name|long
+name|val
 index|[
 literal|2
 index|]
 decl_stmt|;
 block|}
+name|s
+struct|;
+block|}
 name|long_long
 typedef|;
 end_typedef
 
+begin_define
+define|#
+directive|define
+name|high
+value|val[_QUAD_HIGHWORD]
+end_define
+
+begin_define
+define|#
+directive|define
+name|low
+value|val[_QUAD_LOWWORD]
+end_define
+
+begin_define
+define|#
+directive|define
+name|HIGH
+value|_QUAD_HIGHWORD
+end_define
+
+begin_define
+define|#
+directive|define
+name|LOW
+value|_QUAD_LOWWORD
+end_define
+
 begin_comment
-comment|/* Internally, long long ints are strings of unsigned shorts in the    order determined by BYTES_BIG_ENDIAN.  */
+comment|/* Internally, long long ints are strings of unsigned shorts in the    order determined by BYTE_ORDER.  */
 end_comment
 
 begin_define
@@ -146,29 +98,13 @@ name|low16
 value|(B - 1)
 end_define
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|BYTES_BIG_ENDIAN
-end_ifdef
-
-begin_comment
-comment|/* Note that HIGH and LOW do not describe the order    of words in a long long.  They describe the order of words    in vectors ordered according to the byte order.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|HIGH
-value|0
-end_define
-
-begin_define
-define|#
-directive|define
-name|LOW
-value|1
-end_define
+begin_if
+if|#
+directive|if
+name|BYTE_ORDER
+operator|==
+name|BIG_ENDIAN
+end_if
 
 begin_define
 define|#
@@ -238,20 +174,6 @@ begin_else
 else|#
 directive|else
 end_else
-
-begin_define
-define|#
-directive|define
-name|LOW
-value|0
-end_define
-
-begin_define
-define|#
-directive|define
-name|HIGH
-value|1
-end_define
 
 begin_define
 define|#
