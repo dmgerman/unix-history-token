10745b715c1a8fb1741c5c394724e133f9267b14
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-13 22:00:31 -0800

    fix prototypes
    SCCS-vsn: sys/ufs/ffs/ufs_extern.h 7.6
    SCCS-vsn: sys/ufs/ufs/ufs_extern.h 7.6
    
    Former-commit-id: 868664e81a04b4a37eee8efdaea57a34657f376f

--- usr/src/sys/ufs/ffs/ufs_extern.h
+++ usr/src/sys/ufs/ffs/ufs_extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_extern.h	7.5 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_extern.h	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -341,9 +341,9 @@ expr|struct
 name|vnode
 operator|*
 operator|,
-name|u_int
+name|uid_t
 operator|,
-name|u_int
+name|gid_t
 operator|,
 expr|struct
 name|ucred
