5bf3820e4752904d675ad66a5b2aa997172cf4f1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-15 21:19:31 -0800

    reformat to KNF, redo tracing interface, new version for vi
    SCCS-vsn: lib/libcurses/addch.c 5.6
    SCCS-vsn: lib/libcurses/box.c 5.5
    SCCS-vsn: lib/libcurses/clear.c 5.5
    SCCS-vsn: lib/libcurses/clrtobot.c 5.6
    SCCS-vsn: lib/libcurses/clrtoeol.c 5.5
    SCCS-vsn: lib/libcurses/cr_put.c 5.6
    SCCS-vsn: lib/libcurses/curses.c 5.8
    
    Former-commit-id: 1ebb0199242a055bad604232cb58c681eef6498e

--- usr/src/lib/libcurses/addch.c
+++ usr/src/lib/libcurses/addch.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)addch.c	5.5 (Berkeley) %G%"
+literal|"@(#)addch.c	5.6 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
--- usr/src/lib/libcurses/curses.c
+++ usr/src/lib/libcurses/curses.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)curses.c	5.7 (Berkeley) %G%"
+literal|"@(#)curses.c	5.8 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
