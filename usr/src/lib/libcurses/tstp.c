dd6b9f25dcd38faffa540000a3e97c4a5f59a628
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-15 22:35:36 -0800

    rewrite to POSIX signal and termios interfaces reformat to KNF, redo tracing interface, new version for vi
    SCCS-vsn: lib/libcurses/tstp.c 5.7
    
    Former-commit-id: 838642492cb4fcd871ef197b3d77ca8ce5fe059b

--- usr/src/lib/libcurses/tstp.c
+++ usr/src/lib/libcurses/tstp.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)tstp.c	5.6 (Berkeley) %G%"
+literal|"@(#)tstp.c	5.7 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -41,7 +41,7 @@ file|"curses.ext"
 end_include
 
 begin_comment
-comment|/*  * handle stop and start signals  *  * @(#)tstp.c	5.6 (Berkeley) %G%  */
+comment|/*  * handle stop and start signals  *  * @(#)tstp.c	5.7 (Berkeley) %G%  */
 end_comment
 
 begin_function
