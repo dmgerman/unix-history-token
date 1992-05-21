13392b68353526ecea7299a3721103e1df66a153
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-21 00:37:39 -0800

    close should return 0
    SCCS-vsn: sys/kern/subr_log.c 7.14
    
    Former-commit-id: e192585e47036f77d4f9b5d71bb361e09aafae45

--- usr/src/sys/kern/subr_log.c
+++ usr/src/sys/kern/subr_log.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)subr_log.c	7.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)subr_log.c	7.14 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -279,6 +279,11 @@ name|sc_state
 operator|=
 literal|0
 expr_stmt|;
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 block|}
 end_block
 
