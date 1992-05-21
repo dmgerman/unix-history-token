2484620f8835f9762872edf4b436db7227a0e8b6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-20 23:50:59 -0800

    Don't clobber the state of the uart too much in probe, in case we are using the serial console; from Pace Willisson (pace@blitz.com)
    SCCS-vsn: sys/i386/isa/com.c 7.8
    
    Former-commit-id: 107192c7ff8ab8e9a10f94a952111b85d3c6b636

--- usr/src/sys/i386/isa/com.c
+++ usr/src/sys/i386/isa/com.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)com.c	7.7 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)com.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -456,7 +456,17 @@ name|id_iobase
 operator|+
 name|com_cfcr
 argument_list|,
-literal|0
+name|inb
+argument_list|(
+name|dev
+operator|->
+name|id_iobase
+operator|+
+name|com_cfcr
+argument_list|)
+operator|&
+operator|~
+literal|0x80
 argument_list|)
 expr_stmt|;
 name|outb
