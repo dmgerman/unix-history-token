f75ea48536204bc15bf290215208576bc23e1bc1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-28 03:21:03 -0800

    programs that include this file get new truncate and lseek interface
    SCCS-vsn: include/unistd.h 5.17
    
    Former-commit-id: 54763d01f37ff06588b4eef84d97941e3d2b6e76

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.16 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.17 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -565,6 +565,13 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
+begin_define
+define|#
+directive|define
+name|lseek
+value|__lseek
+end_define
+
 begin_decl_stmt
 name|long
 name|lseek
@@ -573,7 +580,7 @@ argument_list|(
 operator|(
 name|int
 operator|,
-name|long
+name|off_t
 operator|,
 name|int
 operator|)
@@ -1072,6 +1079,13 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|ftruncate
+value|__ftruncate
+end_define
 
 begin_decl_stmt
 name|int
@@ -1762,6 +1776,13 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|truncate
+value|__truncate
+end_define
 
 begin_decl_stmt
 name|int
