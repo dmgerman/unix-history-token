447b13851a68695b9578356bb9e427493e7b83e9
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-02 06:46:57 -0800

    have to cast off_t to int
    SCCS-vsn: sbin/newfs/mkfs.c 6.24
    
    Former-commit-id: c19618c94ce0cf694990a6235feac8166df429e0

--- usr/src/sbin/newfs/mkfs.c
+++ usr/src/sbin/newfs/mkfs.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mkfs.c	6.23 (Berkeley) %G%"
+literal|"@(#)mkfs.c	6.24 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3758,6 +3758,9 @@ name|utime
 expr_stmt|;
 name|wtfs
 argument_list|(
+operator|(
+name|int
+operator|)
 name|SBOFF
 operator|/
 name|sectorsize
