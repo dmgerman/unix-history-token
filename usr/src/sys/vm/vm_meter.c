49ad01c9bbddc1060d7602748c34102e19703c1b
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-06-03 19:00:11 -0800

    remove misplaced simple_unlock call
    SCCS-vsn: sys/vm/vm_meter.c 7.16
    
    Former-commit-id: 6831c966c6a38a9a6a5e1a94fb1ee7780f72adc0

--- usr/src/sys/vm/vm_meter.c
+++ usr/src/sys/vm/vm_meter.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vm_meter.c	7.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vm_meter.c	7.16 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -654,12 +654,6 @@ name|totalp
 argument_list|)
 expr_stmt|;
 comment|/* 	 * Mark all objects as inactive. 	 */
-name|simple_unlock
-argument_list|(
-operator|&
-name|vm_object_list_lock
-argument_list|)
-expr_stmt|;
 name|simple_lock
 argument_list|(
 operator|&
