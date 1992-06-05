924e08946bdf60c2a68d1d22bbc2a1563572fce8
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-06-05 00:08:10 -0800

    change include path for HP code reorg
    SCCS-vsn: sys/kern/kern_exec.c 7.59
    SCCS-vsn: sys/kern/sysv_shm.c 7.17
    
    Former-commit-id: 13f48176551f2594fa0377bd99405b6299f0c2c4

--- usr/src/sys/kern/kern_exec.c
+++ usr/src/sys/kern/kern_exec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)kern_exec.c	7.58 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)kern_exec.c	7.59 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -166,7 +166,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"hp300/hpux/hpux_exec.h"
+file|"hp/hpux/hpux_exec.h"
 end_include
 
 begin_endif
