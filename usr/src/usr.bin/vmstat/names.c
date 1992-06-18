e3724b7f9f63efea8ed4ba09308bf85ec045472b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-17 22:53:56 -0800

    hpux stuff moved
    SCCS-vsn: usr.bin/vmstat/names.c 5.3
    
    Former-commit-id: 0def09352bfd8de7ec1078ab2484734e6094e08c

--- usr/src/usr.bin/vmstat/names.c
+++ usr/src/usr.bin/vmstat/names.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)names.c	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)names.c	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_if
@@ -51,7 +51,7 @@ end_ifdef
 begin_include
 include|#
 directive|include
-file|<hp300/dev/device.h>
+file|<hp/dev/device.h>
 end_include
 
 begin_decl_stmt
