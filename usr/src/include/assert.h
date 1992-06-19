81f5254755b1c5f35f57c6f6c6437c1753ec40de
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-18 18:36:08 -0800

    add _assert for backward compatibility
    SCCS-vsn: include/assert.h 5.2
    
    Former-commit-id: 32b5d557e9b8a6d35b6d04d14489d92886677f08

--- usr/src/include/assert.h
+++ usr/src/include/assert.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)assert.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)assert.h	5.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -29,10 +29,30 @@ parameter_list|)
 value|((void)0)
 end_define
 
+begin_define
+define|#
+directive|define
+name|_assert
+parameter_list|(
+name|e
+parameter_list|)
+value|((void)0)
+end_define
+
 begin_else
 else|#
 directive|else
 end_else
+
+begin_define
+define|#
+directive|define
+name|_assert
+parameter_list|(
+name|e
+parameter_list|)
+value|assert(e)
+end_define
 
 begin_ifdef
 ifdef|#
