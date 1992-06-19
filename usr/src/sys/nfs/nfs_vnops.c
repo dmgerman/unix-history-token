41c4969a3ed3b2e87e4010a5f0da2f7d434c4fca
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-06-18 21:22:42 -0800

    remove spurious vrele in nfs_link
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.82
    
    Former-commit-id: bbe1f0033711ca21d0b8766b4932c84555f8f352

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.81 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.82 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -6484,11 +6484,6 @@ else|else
 name|vput
 argument_list|(
 name|vp
-argument_list|)
-expr_stmt|;
-name|vrele
-argument_list|(
-name|tdvp
 argument_list|)
 expr_stmt|;
 return|return
