184d080ccc5b353e097d9d512c0f99383269128e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-18 18:49:21 -0800

    It's an ANSI kind of morning!  (Getopt, too.)
    SCCS-vsn: bin/rmdir/rmdir.c 5.4
    
    Former-commit-id: 5732575f4470cd1571fa34ab368e0c040f8fc47a

--- usr/src/bin/rmdir/rmdir.c
+++ usr/src/bin/rmdir/rmdir.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -14,7 +14,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1983 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1992 The Regents of the University of California.\n\  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)rmdir.c	5.3 (Berkeley) %G%"
+literal|"@(#)rmdir.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -52,9 +52,23 @@ begin_comment
 comment|/* not lint */
 end_comment
 
-begin_comment
-comment|/*  * Remove directory  */
-end_comment
+begin_include
+include|#
+directive|include
+file|<errno.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<stdlib.h>
+end_include
 
 begin_include
 include|#
@@ -62,7 +76,26 @@ directive|include
 file|<stdio.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<string.h>
+end_include
+
+begin_decl_stmt
+name|void
+name|usage
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_function
+name|int
 name|main
 parameter_list|(
 name|argc
@@ -74,33 +107,62 @@ name|argc
 decl_stmt|;
 name|char
 modifier|*
-modifier|*
 name|argv
+index|[]
 decl_stmt|;
 block|{
 name|int
+name|ch
+decl_stmt|,
 name|errors
 decl_stmt|;
+while|while
+condition|(
+operator|(
+name|ch
+operator|=
+name|getopt
+argument_list|(
+name|argc
+argument_list|,
+name|argv
+argument_list|,
+literal|""
+argument_list|)
+operator|)
+operator|!=
+name|EOF
+condition|)
+switch|switch
+condition|(
+name|ch
+condition|)
+block|{
+case|case
+literal|'?'
+case|:
+default|default:
+name|usage
+argument_list|()
+expr_stmt|;
+block|}
+name|argc
+operator|-=
+name|optind
+expr_stmt|;
+name|argv
+operator|+=
+name|optind
+expr_stmt|;
 if|if
 condition|(
 name|argc
-operator|<
-literal|2
+operator|==
+literal|0
 condition|)
-block|{
-name|fprintf
-argument_list|(
-name|stderr
-argument_list|,
-literal|"usage: rmdir directory ...\n"
-argument_list|)
+name|usage
+argument_list|()
 expr_stmt|;
-name|exit
-argument_list|(
-literal|1
-argument_list|)
-expr_stmt|;
-block|}
 for|for
 control|(
 name|errors
@@ -108,9 +170,10 @@ operator|=
 literal|0
 init|;
 operator|*
-operator|++
 name|argv
 condition|;
+operator|++
+name|argv
 control|)
 if|if
 condition|(
@@ -123,17 +186,22 @@ operator|<
 literal|0
 condition|)
 block|{
+operator|(
+name|void
+operator|)
 name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"rmdir: "
-argument_list|)
-expr_stmt|;
-name|perror
-argument_list|(
+literal|"rmdir: %s: %s\n"
+argument_list|,
 operator|*
 name|argv
+argument_list|,
+name|strerror
+argument_list|(
+name|errno
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|errors
@@ -144,6 +212,29 @@ block|}
 name|exit
 argument_list|(
 name|errors
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
+begin_function
+name|void
+name|usage
+parameter_list|()
+block|{
+operator|(
+name|void
+operator|)
+name|fprintf
+argument_list|(
+name|stderr
+argument_list|,
+literal|"usage: rmdir directory ...\n"
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+literal|1
 argument_list|)
 expr_stmt|;
 block|}
