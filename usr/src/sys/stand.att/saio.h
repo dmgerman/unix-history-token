aaae2b3030c50051062069ed752316683192378b
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-06-19 04:04:12 -0800

    include time.h
    SCCS-vsn: sys/stand.att/saio.h 7.18
    
    Former-commit-id: 4b1ef606d3c716e5a57ac9a6d636788235635b5d

--- usr/src/sys/stand.att/saio.h
+++ usr/src/sys/stand.att/saio.h
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)saio.h	7.17 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)saio.h	7.18 (Berkeley) %G%  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/time.h>
+end_include
 
 begin_include
 include|#
