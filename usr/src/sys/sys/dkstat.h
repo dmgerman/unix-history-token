1a16bcaef758e5107bc55a8e4ce9a6d65d904620
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-20 05:17:57 -0800

    add CP_INTR
    SCCS-vsn: sys/sys/dkstat.h 7.6
    
    Former-commit-id: 46f75a850e6316d3e5f5425e9f61b8012b4810e3

--- usr/src/sys/sys/dkstat.h
+++ usr/src/sys/sys/dkstat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dkstat.h	7.5 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dkstat.h	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -27,15 +27,22 @@ end_define
 begin_define
 define|#
 directive|define
-name|CP_IDLE
+name|CP_INTR
 value|3
 end_define
 
 begin_define
 define|#
 directive|define
-name|CPUSTATES
+name|CP_IDLE
 value|4
+end_define
+
+begin_define
+define|#
+directive|define
+name|CPUSTATES
+value|5
 end_define
 
 begin_define
