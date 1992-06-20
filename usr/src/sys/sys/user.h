cdc1d3d8ffdcbd897c2d68809361880e622f735e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-20 06:28:26 -0800

    add md_coredump
    SCCS-vsn: sys/sys/user.h 7.21
    
    Former-commit-id: a3170c0e957fecceeb3c329b64c72987cb9e91ae

--- usr/src/sys/sys/user.h
+++ usr/src/sys/sys/user.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)user.h	7.20 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)user.h	7.21 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -110,6 +110,11 @@ name|kinfo_proc
 name|u_kproc
 decl_stmt|;
 comment|/* proc + eproc */
+name|struct
+name|md_coredump
+name|u_md
+decl_stmt|;
+comment|/* machine dependent glop */
 block|}
 struct|;
 end_struct
