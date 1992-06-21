a3a3c30a69d00813ca969fd0c9f87eca713bc975
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-20 22:19:45 -0800

    add DR_NEWINODEFMT to indicate new format inodes on tape
    SCCS-vsn: include/protocols/dumprestore.h 5.8
    
    Former-commit-id: 8e36f25f60b7b02c7a78f39089fd7aee4cf9dc3c

--- usr/src/include/protocols/dumprestore.h
+++ usr/src/include/protocols/dumprestore.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dumprestore.h	5.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dumprestore.h	5.8 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -283,11 +283,22 @@ begin_define
 define|#
 directive|define
 name|DR_NEWHEADER
-value|1
+value|0x0001
 end_define
 
 begin_comment
 comment|/* new format tape header */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DR_NEWINODEFMT
+value|0x0002
+end_define
+
+begin_comment
+comment|/* new format inodes on tape */
 end_comment
 
 begin_define
