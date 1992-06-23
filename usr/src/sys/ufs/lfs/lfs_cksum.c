a47444b64aa1504732d4cd9431cf6d0b66a982e7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-23 05:53:13 -0800

    fix the includes
    SCCS-vsn: sys/ufs/lfs/lfs_cksum.c 7.5
    
    Former-commit-id: 8ee55c2aecb5dd3068d01ff1b85b24a25d872025

--- usr/src/sys/ufs/lfs/lfs_cksum.c
+++ usr/src/sys/ufs/lfs/lfs_cksum.c
@@ -1,30 +1,12 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_cksum.c	7.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_cksum.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
-file|<sys/param.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/vnode.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<ufs/lfs/lfs.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<ufs/lfs/lfs_extern.h>
+file|<sys/types.h>
 end_include
 
 begin_comment
