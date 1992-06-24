c6ac76d6567006037521b90727b05057c8e4ea96
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-24 00:28:37 -0800

    move systm.h into KERNEL only defines
    SCCS-vsn: sys/ufs/ffs/ffs_subr.c 7.25
    
    Former-commit-id: 74f17bd66c5c71ddc7b8937737494fe6a9c1594d

--- usr/src/sys/ufs/ffs/ffs_subr.c
+++ usr/src/sys/ufs/ffs/ffs_subr.c
@@ -1,18 +1,12 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_subr.c	7.24 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_subr.c	7.25 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|<sys/param.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/systm.h>
 end_include
 
 begin_include
@@ -26,6 +20,12 @@ ifdef|#
 directive|ifdef
 name|KERNEL
 end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/systm.h>
+end_include
 
 begin_include
 include|#
