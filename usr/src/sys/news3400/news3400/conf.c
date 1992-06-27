656bc3ba4f03b99c108183dcb72f4d2cc63ed776
Kazumasa Utashiro <utashiro@ucbvax.Berkeley.EDU>
1992-06-27 01:22:01 -0800

    use NBM for bitmap console
    SCCS-vsn: sys/news3400/news3400/conf.c 7.3
    
    Former-commit-id: 35c2551d09ad1a1d4b49a05b2ce7c7696c72ffeb

--- usr/src/sys/news3400/news3400/conf.c
+++ usr/src/sys/news3400/news3400/conf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)conf.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)conf.c	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -630,6 +630,12 @@ name|vc
 argument_list|)
 expr_stmt|;
 end_expr_stmt
+
+begin_include
+include|#
+directive|include
+file|"bm.h"
+end_include
 
 begin_expr_stmt
 name|cdev_decl
@@ -1339,12 +1345,12 @@ block|,
 comment|/* 21: rd */
 name|cdev_tty_init
 argument_list|(
-literal|1
+name|NBM
 argument_list|,
 name|cn
 argument_list|)
 block|,
-comment|/* 22: console display device*/
+comment|/* 22: console display device */
 name|cdev_notdef
 argument_list|()
 block|,
