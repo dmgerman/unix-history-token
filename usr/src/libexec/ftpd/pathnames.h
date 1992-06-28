aa4692736bc3ac1c49c2f7394680ecebc1b2eea1
Andrew Cherenson <andrew@ucbvax.Berkeley.EDU>
1992-06-27 23:52:12 -0800

    add paths for message files
    SCCS-vsn: libexec/ftpd/pathnames.h 5.3
    
    Former-commit-id: 1fa496f9ea4dd9cbeeb8964d5d6d5d07f4f088bb

--- usr/src/libexec/ftpd/pathnames.h
+++ usr/src/libexec/ftpd/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -14,6 +14,20 @@ define|#
 directive|define
 name|_PATH_FTPUSERS
 value|"/etc/ftpusers"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_FTPWELCOME
+value|"/etc/ftpwelcome"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_FTPLOGINMESG
+value|"/etc/motd"
 end_define
 
 end_unit
