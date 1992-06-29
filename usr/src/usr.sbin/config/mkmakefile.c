58f2a11fec5a9f8cda7244b7886903ecd4d3e037
Chris G. Demetriou <cgd@postgres.berkeley.edu>
1992-06-29 00:00:00 +0000

    PATCH NEEDED FOR KERNEL REBUILD TO SUCCEED
    Many people who have made their own kernel configurations have discovered
    that their kernel won't build until they change their Makefile to include
    a reference to vers.o (missing symbol _version).
    
    Also, the version number was not updated each time the kernel was rebuilt.
    
    AUTHOR:		Chris G. Demetriou (cgd@berkeley.edu)
    386BSD-Patchkit: patch00001
    
    Former-commit-id: c3fef9fd536f136ce866fb4c527af3ed4d8d45d8

--- usr/src/usr.sbin/config/mkmakefile.c
+++ usr/src/usr.sbin/config/mkmakefile.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980,1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1980,1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00001  * --------------------         -----   ----------------------  *  * 29 Jun 92	Chris G. Demetriou	Fix Version number update  */
 end_comment
 
 begin_ifndef
@@ -488,6 +488,19 @@ literal|1
 argument_list|)
 expr_stmt|;
 block|}
+name|fprintf
+argument_list|(
+name|ofp
+argument_list|,
+literal|"KERN_IDENT=%s\n"
+argument_list|,
+name|raise
+argument_list|(
+name|ident
+argument_list|)
+argument_list|)
+expr_stmt|;
+comment|/* 29 Jun 92*/
 name|fprintf
 argument_list|(
 name|ofp
@@ -2898,6 +2911,18 @@ operator|->
 name|f_fn
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|first
+condition|)
+comment|/* 29 Jun 92*/
+name|fprintf
+argument_list|(
+name|f
+argument_list|,
+literal|" newvers"
+argument_list|)
+expr_stmt|;
 name|fprintf
 argument_list|(
 name|f
@@ -3011,11 +3036,14 @@ name|name
 argument_list|)
 expr_stmt|;
 else|else
+comment|/* 29 Jun 92*/
 name|fprintf
 argument_list|(
 name|f
 argument_list|,
-literal|"swapgeneric.o: ../%s/swapgeneric.c\n"
+literal|"swapgeneric.o: ../../%s/%s/swapgeneric.c\n"
+argument_list|,
+name|machinename
 argument_list|,
 name|machinename
 argument_list|)
