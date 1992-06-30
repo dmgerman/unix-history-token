4f037a2b45ae40a7316792f3e84c3679e43e9797
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-29 18:48:03 -0800

    add iruserok
    SCCS-vsn: include/unistd.h 5.20
    
    Former-commit-id: 9287e8dd6ae775373011275aebec3776cd2f2dac

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.19 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.20 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -1189,6 +1189,28 @@ name|char
 operator|*
 operator|,
 name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|iruserok
+name|__P
+argument_list|(
+operator|(
+name|u_long
+operator|,
+name|int
+operator|,
+specifier|const
+name|char
+operator|*
+operator|,
+specifier|const
+name|char
+operator|*
 operator|)
 argument_list|)
 decl_stmt|;
