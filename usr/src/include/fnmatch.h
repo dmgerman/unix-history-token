42b17da9667ae55224833655918b99bf83cbd6bc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-29 21:43:42 -0800

    move fnmatch function declaration from unistd.h
    SCCS-vsn: include/fnmatch.h 5.2
    
    Former-commit-id: 2e88c381dab6f326298f31efe2e117d0ba4bb32d

--- usr/src/include/fnmatch.h
+++ usr/src/include/fnmatch.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fnmatch.h	5.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fnmatch.h	5.2 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_FNMATCH_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_FNMATCH_H_
+end_define
 
 begin_define
 define|#
@@ -45,6 +57,54 @@ end_define
 
 begin_comment
 comment|/* Period must be matched by period. */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_decl_stmt
+name|__BEGIN_DECLS
+ifndef|#
+directive|ifndef
+name|_POSIX_SOURCE
+name|int
+name|fnmatch
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+specifier|const
+name|char
+operator|*
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_macro
+name|__END_DECLS
+end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_FNMATCH_H_ */
 end_comment
 
 end_unit
