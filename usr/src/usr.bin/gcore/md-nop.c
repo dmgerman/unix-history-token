771d3baa0c932a6821cc59898756a3b9a20a08d1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-30 01:57:36 -0800

    put in keywords
    SCCS-vsn: usr.bin/gcore/md-nop.c 5.3
    
    Former-commit-id: a9bafab5e645218a2e70bfac59e5c4d9e012a027

--- usr/src/usr.bin/gcore/md-nop.c
+++ usr/src/usr.bin/gcore/md-nop.c
@@ -3,6 +3,31 @@ begin_comment
 comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+specifier|static
+name|char
+name|sccsid
+index|[]
+init|=
+literal|"@(#)md-nop.c	5.3 (Berkeley) %G%"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
+
 begin_include
 include|#
 directive|include
