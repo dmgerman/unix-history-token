7d2d2ba356d0083bd203bc3383dfc899a2e91ddd
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:45:25 -0800

    ffs_vget moves from VOP to VFS
    SCCS-vsn: sys/ufs/ffs/ffs_extern.h 7.13
    
    Former-commit-id: d206bd55869e26ba3b4aec5693c16ef0b111070c

--- usr/src/sys/ufs/ffs/ffs_extern.h
+++ usr/src/sys/ufs/ffs/ffs_extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_extern.h	7.12 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_extern.h	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -608,7 +608,14 @@ name|__P
 argument_list|(
 operator|(
 expr|struct
-name|vop_vget_args
+name|mount
+operator|*
+operator|,
+name|ino_t
+operator|,
+expr|struct
+name|vnode
+operator|*
 operator|*
 operator|)
 argument_list|)
