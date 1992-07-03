fdb1e03e71571ba72f1618193293e931ecba682e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:53:41 -0800

    nfs_vget moves from vop to vfs
    SCCS-vsn: sys/nfs/nfsnode.h 7.26
    
    Former-commit-id: b6a61d7e5faceb97ddbda3c8e9daf0302c4f0226

--- usr/src/sys/nfs/nfsnode.h
+++ usr/src/sys/nfs/nfsnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfsnode.h	7.25 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfsnode.h	7.26 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -929,7 +929,14 @@ name|__P
 argument_list|(
 operator|(
 expr|struct
-name|vop_vget_args
+name|mount
+operator|*
+operator|,
+name|ino_t
+operator|,
+expr|struct
+name|vnode
+operator|*
 operator|*
 operator|)
 argument_list|)
