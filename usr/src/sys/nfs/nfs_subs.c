9bdd37a47b1ccba96be8f86de1e4164d38e4b45e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:54:03 -0800

    delete USES
    SCCS-vsn: sys/nfs/nfs_subs.c 7.57
    
    Former-commit-id: 2757b249a01b6c5a2de9a7f15809f843b5d482f0

--- usr/src/sys/nfs/nfs_subs.c
+++ usr/src/sys/nfs/nfs_subs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_subs.c	7.56 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_subs.c	7.57 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -5526,8 +5526,6 @@ end_decl_stmt
 
 begin_block
 block|{
-name|USES_VOP_UNLOCK
-expr_stmt|;
 specifier|register
 name|struct
 name|mount
