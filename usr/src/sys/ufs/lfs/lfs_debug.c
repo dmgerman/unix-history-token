a6c3ae3d940b26572b8a9079e3f7444d3ec394c5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-05 21:36:37 -0800

    needs <sys/mount.h>
    SCCS-vsn: sys/ufs/lfs/lfs_debug.c 7.7
    
    Former-commit-id: 6c138d8d466acb3277fa68b83b74ef383ca2e257

--- usr/src/sys/ufs/lfs/lfs_debug.c
+++ usr/src/sys/ufs/lfs/lfs_debug.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_debug.c	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_debug.c	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -25,6 +25,12 @@ begin_include
 include|#
 directive|include
 file|<sys/vnode.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mount.h>
 end_include
 
 begin_include
