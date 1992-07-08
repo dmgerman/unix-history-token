3edde4feff2275e706de541b68f8ae9c54be0da0
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-08 08:02:41 -0800

    whitespace hacking; add splstatclock
    SCCS-vsn: sys/hp300/include/param.h 7.12
    
    Former-commit-id: 0d89e2fcfd8c2071587f232c42fd4beea3b81c49

--- usr/src/sys/hp300/include/param.h
+++ usr/src/sys/hp300/include/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machparam.h 1.12 91/01/18$  *  *	@(#)param.h	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machparam.h 1.12 91/01/18$  *  *	@(#)param.h	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -589,6 +589,14 @@ begin_define
 define|#
 directive|define
 name|splclock
+parameter_list|()
+value|spl6()
+end_define
+
+begin_define
+define|#
+directive|define
+name|splstatclock
 parameter_list|()
 value|spl6()
 end_define
