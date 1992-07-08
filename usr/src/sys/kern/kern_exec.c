00cd5bbfcce563ba2d111910ea1923a3c14f2d13
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-08 07:52:11 -0800

    p_prof.pr_scale replaced by SPROFIL to show profiling in use
    SCCS-vsn: sys/kern/kern_exec.c 7.64
    
    Former-commit-id: 5b02e6bd87478a1aa89c11f05836a2e68f3bfcf9

--- usr/src/sys/kern/kern_exec.c
+++ usr/src/sys/kern/kern_exec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)kern_exec.c	7.63 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  *  *	@(#)kern_exec.c	7.64 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3737,16 +3737,6 @@ name|btoc
 argument_list|(
 name|ssize
 argument_list|)
-expr_stmt|;
-name|p
-operator|->
-name|p_stats
-operator|->
-name|p_prof
-operator|.
-name|pr_scale
-operator|=
-literal|0
 expr_stmt|;
 if|if
 condition|(
