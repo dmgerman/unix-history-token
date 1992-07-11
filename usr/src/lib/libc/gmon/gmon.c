5afed04c55aa2fa8003c1f6f0123b3dd95dc1b27
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-11 00:15:36 -0800

    update headers in preparation for moving into libc
    SCCS-vsn: lib/libc/gmon/gmon.c 5.11
    
    Former-commit-id: e5173bb877469803a77a803a250cb1cde7e4275b

--- usr/src/lib/libc/gmon/gmon.c
+++ usr/src/lib/libc/gmon/gmon.c
@@ -1,13 +1,22 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1983 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1983, 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
 name|lint
-end_ifndef
+argument_list|)
+operator|&&
+name|defined
+argument_list|(
+name|LIBC_SCCS
+argument_list|)
+end_if
 
 begin_decl_stmt
 specifier|static
@@ -15,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)gmon.c	5.10 (Berkeley) %G%"
+literal|"@(#)gmon.c	5.11 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -23,10 +32,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
 
 begin_include
 include|#
