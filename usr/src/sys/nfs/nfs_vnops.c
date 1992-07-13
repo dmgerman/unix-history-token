7fd2289662535df837137f071af21fadc10219d8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-12 22:52:44 -0800

    new location for includes
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.88
    
    Former-commit-id: 613596ced71e0097935493192dc87f3a38fab339

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.87 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.88 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -76,18 +76,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/specdev.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/fifo.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/map.h>
 end_include
 
@@ -101,6 +89,18 @@ begin_include
 include|#
 directive|include
 file|<vm/vm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<miscfs/specfs/specdev.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<miscfs/fifofs/fifo.h>
 end_include
 
 begin_include
