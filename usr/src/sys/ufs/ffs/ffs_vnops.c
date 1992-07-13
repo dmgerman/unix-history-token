9397edca7601dc6e9b07d8a44f59badab6f6f685
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-12 21:06:22 -0800

    put back in size check to avoid problems with quad math and negative sizes
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.88
    
    Former-commit-id: 3f97a4c6ed5846892916aa0fb83abe0c91fe1bcf

--- usr/src/sys/ufs/ffs/ffs_vnops.c
+++ usr/src/sys/ufs/ffs/ffs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	7.87 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	7.88 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1447,6 +1447,24 @@ name|ip
 operator|->
 name|i_fs
 expr_stmt|;
+if|if
+condition|(
+operator|(
+name|u_quad_t
+operator|)
+name|uio
+operator|->
+name|uio_offset
+operator|>
+name|fs
+operator|->
+name|fs_maxfilesize
+condition|)
+return|return
+operator|(
+name|EFBIG
+operator|)
+return|;
 name|ip
 operator|->
 name|i_flag
