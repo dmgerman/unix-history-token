09b6ff410cc9436a74f44a428592469dd33c13dd
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-07-13 00:38:15 -0800

    need to include "systm.h"
    SCCS-vsn: sys/net/radix.c 7.16
    SCCS-vsn: sys/net/rtsock.c 7.30
    
    Former-commit-id: 2efc92f83599de1b7043b54a52c4dc0dcd511484

--- usr/src/sys/net/rtsock.c
+++ usr/src/sys/net/rtsock.c
@@ -1,12 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)rtsock.c	7.29 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)rtsock.c	7.30 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"param.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"systm.h"
 end_include
 
 begin_include
