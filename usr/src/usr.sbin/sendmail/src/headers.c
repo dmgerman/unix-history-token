af5c776abd16acdbb0fa42fb076b77c7eec17679
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-12 19:55:56 -0800

    fix latent bug in received logging printing
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 5.19
    
    Former-commit-id: 4beef711a2b8349a3f9eda60daaac30a09183619

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)headers.c	5.18 (Berkeley) %G%"
+literal|"@(#)headers.c	5.19 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1613,7 +1613,7 @@ name|sprintf
 argument_list|(
 name|hbuf
 argument_list|,
-literal|"%.90s (%s)"
+literal|"%.80s (%s)"
 argument_list|,
 name|RealHostName
 argument_list|,
