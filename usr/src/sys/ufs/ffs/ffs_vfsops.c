d4f5fe458287d2f772c048efec44f6df28fc87a8
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-07-13 00:13:51 -0800

    MAX -> max
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.74
    
    Former-commit-id: c26a94df9eafa3a635989e316430a01fcd369e56

--- usr/src/sys/ufs/ffs/ffs_vfsops.c
+++ usr/src/sys/ufs/ffs/ffs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	7.73 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	7.74 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1876,7 +1876,7 @@ name|fs
 operator|->
 name|fs_npsect
 operator|=
-name|MAX
+name|max
 argument_list|(
 name|fs
 operator|->
@@ -1898,7 +1898,7 @@ name|fs
 operator|->
 name|fs_interleave
 operator|=
-name|MAX
+name|max
 argument_list|(
 name|fs
 operator|->
