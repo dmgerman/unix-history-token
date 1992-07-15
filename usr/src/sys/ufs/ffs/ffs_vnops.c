771608adfd559cda6eb2be207a6c0fee6d1eba36
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-15 00:20:15 -0800

    be paranoid about big file sizes
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.91
    
    Former-commit-id: e8940a66c42b4e88b74d38511970ee8d4efd4eb0

--- usr/src/sys/ufs/ffs/ffs_vnops.c
+++ usr/src/sys/ufs/ffs/ffs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	7.90 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	7.91 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1950,6 +1950,15 @@ name|i_fs
 expr_stmt|;
 if|if
 condition|(
+name|uio
+operator|->
+name|uio_offset
+operator|<
+literal|0
+operator|||
+operator|(
+name|u_quad_t
+operator|)
 name|uio
 operator|->
 name|uio_offset
