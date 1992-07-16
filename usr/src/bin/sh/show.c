399414ca967e0768dd1ddd9a93328392dda7c8bb
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-07-15 16:10:41 -0800

    trace to ./trace not $HOME/.trace
    SCCS-vsn: bin/sh/show.c 5.3
    
    Former-commit-id: 19e3dc601c81d05355473005db8eddc27387c47f

--- usr/src/bin/sh/show.c
+++ usr/src/bin/sh/show.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)show.c	5.2 (Berkeley) %G%"
+literal|"@(#)show.c	5.2 (Berkeley) 4/12/91"
 decl_stmt|;
 end_decl_stmt
 
@@ -1609,6 +1609,9 @@ operator|!
 name|debug
 condition|)
 return|return;
+ifdef|#
+directive|ifdef
+name|not_this_way
 if|if
 condition|(
 operator|(
@@ -1625,7 +1628,7 @@ condition|)
 block|{
 if|if
 condition|(
-name|getuid
+name|geteuid
 argument_list|()
 operator|==
 literal|0
@@ -1654,6 +1657,18 @@ argument_list|,
 literal|"/trace"
 argument_list|)
 expr_stmt|;
+else|#
+directive|else
+name|scopy
+argument_list|(
+literal|"./trace"
+argument_list|,
+name|s
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* not_this_way */
 if|if
 condition|(
 operator|(
@@ -1734,6 +1749,7 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+comment|/* DEBUG */
 block|}
 end_block
 
