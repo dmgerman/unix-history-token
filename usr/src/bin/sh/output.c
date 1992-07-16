cbbc02322d08b70077dfe52c487878a6b3af217d
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-07-15 16:12:34 -0800

    always flush out2
    SCCS-vsn: bin/sh/output.c 5.3
    
    Former-commit-id: 99341d32649c8632350d918f4016d22044fc8e95

--- usr/src/bin/sh/output.c
+++ usr/src/bin/sh/output.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)output.c	5.2 (Berkeley) %G%"
+literal|"@(#)output.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -444,6 +444,17 @@ operator|*
 name|p
 operator|++
 argument_list|,
+name|file
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|file
+operator|==
+name|out2
+condition|)
+name|flushout
+argument_list|(
 name|file
 argument_list|)
 expr_stmt|;
