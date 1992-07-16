c5c9e97b662bdb1d24b7496b6acb480f3a10a14d
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-07-15 17:01:34 -0800

    always include var.h
    SCCS-vsn: bin/sh/main.c 5.4
    
    Former-commit-id: c97025bc87807b3ee43176218bad7f0a2f216d3f

--- usr/src/bin/sh/main.c
+++ usr/src/bin/sh/main.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)main.c	5.3 (Berkeley) %G%"
+literal|"@(#)main.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -130,22 +130,11 @@ directive|include
 file|"trap.h"
 end_include
 
-begin_if
-if|#
-directive|if
-name|ATTY
-end_if
-
 begin_include
 include|#
 directive|include
 file|"var.h"
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
