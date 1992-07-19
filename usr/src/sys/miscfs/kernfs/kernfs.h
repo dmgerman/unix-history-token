f2ea648da592c5a81ec6f284b5d32ef4049e48cc
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-07-19 11:56:06 -0800

    added raw root device support to kernfs
    SCCS-vsn: sys/miscfs/kernfs/kernfs.h 7.2
    SCCS-vsn: sys/miscfs/kernfs/kernfs_vfsops.c 7.3
    SCCS-vsn: sys/miscfs/kernfs/kernfs_vnops.c 7.4
    
    Former-commit-id: 43aae396e973afaf4f0a8d54b05018b0f62bb0b6

--- usr/src/sys/miscfs/kernfs/kernfs.h
+++ usr/src/sys/miscfs/kernfs/kernfs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)kernfs.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)kernfs.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -73,6 +73,15 @@ specifier|extern
 name|struct
 name|vfsops
 name|kernfs_vfsops
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|struct
+name|vnode
+modifier|*
+name|rrootvp
 decl_stmt|;
 end_decl_stmt
 
