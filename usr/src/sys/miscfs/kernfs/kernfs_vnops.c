bd248b80556cca765941b478457f09a2c0828bab
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-07-19 15:13:41 -0800

    added boottime
    SCCS-vsn: sys/miscfs/kernfs/kernfs_vnops.c 7.5
    
    Former-commit-id: 7f9658728423e7094f08adef5572b65b1e7ef272

--- usr/src/sys/miscfs/kernfs/kernfs_vnops.c
+++ usr/src/sys/miscfs/kernfs/kernfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)kernfs_vnops.c	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)kernfs_vnops.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -216,6 +216,21 @@ block|,
 name|VREAD
 block|,
 name|VDIR
+block|}
+block|,
+block|{
+literal|"boottime"
+block|,
+operator|&
+name|boottime
+operator|.
+name|tv_sec
+block|,
+name|KTT_INT
+block|,
+name|VREAD
+block|,
+name|VREG
 block|}
 block|,
 block|{
