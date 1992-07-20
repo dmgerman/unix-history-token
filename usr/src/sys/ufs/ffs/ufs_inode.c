475a7d11156c4a04b569181d89d47c4550a46e25
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-20 07:31:30 -0800

    remque => ufs_ihashrem
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 7.49
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 7.49
    
    Former-commit-id: 30f83ccedb488a65050d7b084ba0e811c3e4943f

--- usr/src/sys/ufs/ffs/ufs_inode.c
+++ usr/src/sys/ufs/ffs/ufs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_inode.c	7.48 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_inode.c	7.49 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -277,7 +277,7 @@ argument_list|(
 name|vp
 argument_list|)
 expr_stmt|;
-name|remque
+name|ufs_ihashrem
 argument_list|(
 name|ip
 argument_list|)
