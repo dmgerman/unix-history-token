405ab5e0b382519c6dc6514c4a7d5d276db2fb32
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-20 15:23:40 -0800

    put in standard Berkeley header
    SCCS-vsn: usr.sbin/sendmail/EPA/mldistrib/mldistrib.c 5.6
    
    Former-commit-id: 995b74c629cf367c2d5d8459877bfd60043d2249

--- usr/src/usr.sbin/sendmail/EPA/mldistrib/mldistrib.c
+++ usr/src/usr.sbin/sendmail/EPA/mldistrib/mldistrib.c
@@ -1,6 +1,40 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* @(#)mldistrib.c	5.5 (Berkeley) %G% */
+comment|/*  * Copyright (c) 1992 Eric P. Allman  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+name|char
+name|copyright
+index|[]
+init|=
+literal|"@(#) Copyright (c) 1992 The Regents of the University of California.\n\  All rights reserved.\n"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|char
+name|sccsid
+index|[]
+init|=
+literal|"@(#)mldistrib.c	5.6 (Berkeley) %G%"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
 end_comment
 
 begin_include
