08a04b68b7d70b5d117bb58f690286d0000da7da
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-20 06:46:05 -0800

    decrement process ownership count on exit
    SCCS-vsn: sys/kern/kern_exit.c 7.49
    
    Former-commit-id: d5995a7d47f0a93a69bbd7634c14e1dd56513893

--- usr/src/sys/kern/kern_exit.c
+++ usr/src/sys/kern/kern_exit.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_exit.c	7.48 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_exit.c	7.49 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1570,6 +1570,23 @@ argument_list|,
 name|M_ZOMBIE
 argument_list|)
 expr_stmt|;
+comment|/* 			 * Decrement the count of procs running with this uid. 			 */
+operator|(
+name|void
+operator|)
+name|chgproccnt
+argument_list|(
+name|p
+operator|->
+name|p_cred
+operator|->
+name|p_ruid
+argument_list|,
+operator|-
+literal|1
+argument_list|)
+expr_stmt|;
+comment|/* 			 * Free up credentials. 			 */
 if|if
 condition|(
 operator|--
