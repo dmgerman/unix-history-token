dd8e212fc81fcbfe6eb54fba13b8f376c60a8c05
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-23 23:24:18 -0800

    Do block accounting in terms of disk sectors, not file system blocks.
    SCCS-vsn: sys/ufs/lfs/lfs_inode.c 7.75
    
    Former-commit-id: 5d2865b9bb96c9fad27101ba4685b2df7a38ff80

--- usr/src/sys/ufs/lfs/lfs_inode.c
+++ usr/src/sys/ufs/lfs/lfs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_inode.c	7.74 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_inode.c	7.75 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1315,6 +1315,19 @@ name|ip
 operator|->
 name|i_blocks
 operator|-=
+name|btodb
+argument_list|(
+name|blocksreleased
+operator|<<
+name|fs
+operator|->
+name|lfs_bshift
+argument_list|)
+expr_stmt|;
+name|fs
+operator|->
+name|lfs_bfree
+operator|+=
 name|btodb
 argument_list|(
 name|blocksreleased
