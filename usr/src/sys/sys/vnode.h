d74ee7dd792f887fee4b337c3b33c39016dc57af
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-24 00:53:15 -0800

    predeclare strust stat
    SCCS-vsn: sys/sys/vnode.h 7.59
    
    Former-commit-id: 7e2799cf548269538234c5c3332b21aab8661624

--- usr/src/sys/sys/vnode.h
+++ usr/src/sys/sys/vnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	7.58 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	7.59 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -1382,6 +1382,12 @@ end_struct_decl
 begin_struct_decl
 struct_decl|struct
 name|proc
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|stat
 struct_decl|;
 end_struct_decl
 
