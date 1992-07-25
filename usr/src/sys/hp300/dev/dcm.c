5f2adc8b5cc620231e3391760ee36b4d54f776b0
Steven McCanne <mccanne@ucbvax.Berkeley.EDU>
1992-07-24 23:00:47 -0800

    new kgdb framing---changed FRAME_START now identifies a packet
    SCCS-vsn: sys/hp300/dev/dcm.c 7.19
    
    Former-commit-id: 155ca911190d0976ec2fbb5c854f7af365af1ac0

--- usr/src/sys/hp300/dev/dcm.c
+++ usr/src/sys/hp300/dev/dcm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from Utah: $Hdr: dcm.c 1.29 92/01/21$  *  *	@(#)dcm.c	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from Utah: $Hdr: dcm.c 1.29 92/01/21$  *  *	@(#)dcm.c	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -3173,7 +3173,7 @@ index|]
 operator|.
 name|data_char
 operator|==
-name|FRAME_END
+name|FRAME_START
 condition|)
 block|{
 name|pp
