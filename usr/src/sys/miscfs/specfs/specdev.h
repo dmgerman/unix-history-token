98981b0fc8d55e81f536092bde8f43afa866465b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-26 03:31:08 -0800

    add declaration for spec_vnodeop_p()
    SCCS-vsn: sys/miscfs/specfs/specdev.h 7.12
    
    Former-commit-id: 1d68332eec70e706025b837fb263098600c8ec21

--- usr/src/sys/miscfs/specfs/specdev.h
+++ usr/src/sys/miscfs/specfs/specdev.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)specdev.h	7.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)specdev.h	7.12 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -152,6 +152,18 @@ end_decl_stmt
 begin_comment
 comment|/*  * Prototypes for special file operations on vnodes.  */
 end_comment
+
+begin_function_decl
+specifier|extern
+name|int
+function_decl|(
+modifier|*
+modifier|*
+name|spec_vnodeop_p
+function_decl|)
+parameter_list|()
+function_decl|;
+end_function_decl
 
 begin_struct_decl
 struct_decl|struct
