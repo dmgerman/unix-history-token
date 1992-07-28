4c2daf1f26ead6481618e0afafce026e1e389f39
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-07-28 04:19:29 -0800

    use min() not MIN()
    SCCS-vsn: sys/pmax/dev/rz.c 7.5
    
    Former-commit-id: 2e8b62b2a011b45409f0b1539e62019533a4de3d

--- usr/src/sys/pmax/dev/rz.c
+++ usr/src/sys/pmax/dev/rz.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Van Jacobson of Lawrence Berkeley Laboratory and Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)rz.c	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Van Jacobson of Lawrence Berkeley Laboratory and Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)rz.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2580,7 +2580,7 @@ operator|++
 expr_stmt|;
 name|count
 operator|=
-name|MIN
+name|min
 argument_list|(
 name|resid
 argument_list|,
