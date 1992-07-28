4e4c0761122f0e63b543e577f4078c8f19aeb4d1
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-07-28 04:31:38 -0800

    added md_coredump struct
    SCCS-vsn: sys/pmax/include/pcb.h 7.2
    
    Former-commit-id: 8f03d146f89a25040fffefeba7be25eb767dad89

--- usr/src/sys/pmax/include/pcb.h
+++ usr/src/sys/pmax/include/pcb.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: pcb.h 1.13 89/04/23$  *  *	@(#)pcb.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: pcb.h 1.13 89/04/23$  *  *	@(#)pcb.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -26,6 +26,24 @@ name|int
 name|pcb_onfault
 decl_stmt|;
 comment|/* for copyin/copyout faults */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * The pcb is augmented with machine-dependent additional data for  * core dumps. For the PMAX, there is nothing to add.  */
+end_comment
+
+begin_struct
+struct|struct
+name|md_coredump
+block|{
+name|long
+name|md_pad
+index|[
+literal|8
+index|]
+decl_stmt|;
 block|}
 struct|;
 end_struct
