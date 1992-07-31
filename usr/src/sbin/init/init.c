f7639d520fc78cd9b8d1d6a11a9d2a616d2b9210
Christoph Robitschko <chmr@edvz.tu-graz.ac.at>
1992-07-31 00:00:00 +0000

    CHANGE RUN LEVEL (SINGLE USER/MULTIUSER/HALT)
    	/sbin/init did not set some internal flags correctly (Reboot, drain)
    This caused it to not execute /etc/rc again if the system was brought down
    to singleuser and the back up again.  It also tried to start multiuser mode
    when you tried halt or reboot in singleuser (when the singleuser shell was
    terminated by halt/reboot).
    
    AUTHOR:		Christoph Robitschko (chmr@edvz.tu-graz.ac.at)
    386BSD-Patchkit: patch00022
    
    Former-commit-id: 68d348d88ca776e66f6d68b500f1b9cae8603bfb

--- usr/src/sbin/init/init.c
+++ usr/src/sbin/init/init.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1986, 1987, 1992 Daniel D. Lanciani.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by  *	Daniel D. Lanciani.  * 4. The name of the author may not  *    be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Daniel D. Lanciani ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Daniel D. Lanciani BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00014  * --------------------         -----   ----------------------  *  * 04 Sep 92	Paul Kranenburg		Fixed kill -1 and kill -15 for  *					daemons started from /etc/rc.  */
+comment|/*  * Copyright (c) 1986, 1987, 1992 Daniel D. Lanciani.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by  *	Daniel D. Lanciani.  * 4. The name of the author may not  *    be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Daniel D. Lanciani ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Daniel D. Lanciani BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00022  * --------------------         -----   ----------------------  *  * 31 Jul 92	Christoph Robitschko	Fixed run level change code  * 04 Sep 92	Paul Kranenburg		Fixed kill -1 and kill -15 for  *					daemons started from /etc/rc.  */
 end_comment
 
 begin_include
@@ -740,6 +740,11 @@ literal|127
 argument_list|)
 expr_stmt|;
 block|}
+name|Reboot
+operator|=
+literal|0
+expr_stmt|;
+comment|/* 31 Jul 92*/
 while|while
 condition|(
 name|wait
@@ -879,6 +884,14 @@ argument_list|)
 operator|!=
 name|pid
 condition|)
+while|while
+condition|(
+name|drain
+condition|)
+comment|/* 31 Jul 92*/
+name|pause
+argument_list|()
+expr_stmt|;
 goto|goto
 name|top
 goto|;
