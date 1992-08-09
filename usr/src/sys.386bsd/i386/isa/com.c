5a8cb977d001a58e0ffbc4821e5adeec61cf2be0
Christoph Robitschko <chmr@edvz.tu-graz.ac.at>
1992-08-09 00:00:00 +0000

    CORRECT MINOR NUMBER FOR COM PORT (SELECT FAILS)
    I had the problem with select() not working on com ports. I got no
    response from a post regarding this one, so I started to look into it
    myself. I found out that the com driver calculates unit = minor(dev) -1;
    This has the following implications:
            /dev/com1 corresponds to COM0, /dev/com2 to COM1 (very confusing
                    in kernel messages)
            It is incompatible with the config file entries com1 at..., com2 at...
            Unpredictible results will occur if someone puts a com0 at.. in
                    his config file.
            It is incompatible with the DOS usage of COM1, COM2 (But who cares 8-)
            ttselect() calculates unit = minor(dev), and uses this as an index in
                    com_tty. Because this index is different from that used
                    in the com driver, select() on /dev/com1 looks at
                    /dev/com2 and select() on /dev/com2 looks at an undefined
                    entry in com_tty and returns always true.
    
    WARNING: RE-mknod /dev/com1, /dev/com2, and modify your kernel config!
    
    AUTHOR:		Christoph Robitschko (chmr@fstgds01.tu-graz.ac.at)
    386BSD-Patchkit: patch00018
    
    Former-commit-id: c9f2ac51ae04ab8ed606d70fa8cc523defb928f6

--- usr/src/sys.386bsd/i386/isa/com.c
+++ usr/src/sys.386bsd/i386/isa/com.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)com.c	7.5 (Berkeley) 5/16/91  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)com.c	7.5 (Berkeley) 5/16/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00018  * --------------------         -----   ----------------------  *  * 09 Aug 92	Christoph Robitschko	Correct minor number on com ports  */
 end_comment
 
 begin_decl_stmt
@@ -442,7 +442,7 @@ name|UNIT
 parameter_list|(
 name|x
 parameter_list|)
-value|(minor(x)-1)
+value|(minor(x))
 end_define
 
 begin_macro
@@ -552,8 +552,6 @@ operator|=
 name|isdp
 operator|->
 name|id_unit
-operator|-
-literal|1
 expr_stmt|;
 if|if
 condition|(
@@ -667,8 +665,6 @@ argument_list|(
 name|commajor
 argument_list|,
 name|unit
-operator|+
-literal|1
 argument_list|)
 condition|)
 block|{
@@ -1229,8 +1225,6 @@ argument_list|(
 name|commajor
 argument_list|,
 name|unit
-operator|+
-literal|1
 argument_list|)
 condition|)
 endif|#
@@ -1472,7 +1466,6 @@ modifier|*
 name|tp
 decl_stmt|;
 name|unit
-operator|--
 expr_stmt|;
 name|com
 operator|=
@@ -1533,7 +1526,7 @@ directive|define
 name|RCVBYTE
 parameter_list|()
 define|\
-value|code = inb(com+com_data); \ 			if ((tp->t_state& TS_ISOPEN) == 0) { \ 				if (kgdb_dev == makedev(commajor, unit+1)&& \ 				    code == FRAME_END) \ 					kgdb_connect(0);
+value|code = inb(com+com_data); \ 			if ((tp->t_state& TS_ISOPEN) == 0) { \ 				if (kgdb_dev == makedev(commajor, unit)&& \ 				    code == FRAME_END) \ 					kgdb_connect(0);
 comment|/* trap into kgdb */
 value|\ 			} else \ 				(*linesw[tp->t_line].l_rint)(code, tp)
 else|#
@@ -1800,8 +1793,6 @@ argument_list|(
 name|commajor
 argument_list|,
 name|unit
-operator|+
-literal|1
 argument_list|)
 operator|&&
 name|c
@@ -3237,8 +3228,6 @@ argument_list|(
 name|commajor
 argument_list|,
 name|unit
-operator|+
-literal|1
 argument_list|)
 expr_stmt|;
 name|cp
