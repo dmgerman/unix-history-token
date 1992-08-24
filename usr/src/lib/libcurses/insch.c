a862c826c92d6809ff08434381b86c32cd5a1628
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-23 18:10:45 -0800

    KNF
    SCCS-vsn: lib/libcurses/insch.c 5.5
    SCCS-vsn: lib/libcurses/insertln.c 5.5
    
    Former-commit-id: 5f51e63324cfded5b99762a963b2cc2c90ae7bbb

--- usr/src/lib/libcurses/insch.c
+++ usr/src/lib/libcurses/insch.c
@@ -31,50 +31,40 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"curses.ext"
+file|<curses.h>
 end_include
 
 begin_comment
-comment|/*  *	This routine performs an insert-char on the line, leaving  * (_cury,_curx) unchanged.  *  */
+comment|/*  * winsch --  *	Do an insert-char on the line, leaving (_cury,_curx) unchanged.  */
 end_comment
 
-begin_macro
+begin_function
+name|int
 name|winsch
-argument_list|(
-argument|win
-argument_list|,
-argument|c
-argument_list|)
-end_macro
-
-begin_decl_stmt
-name|reg
+parameter_list|(
+name|win
+parameter_list|,
+name|ch
+parameter_list|)
+specifier|register
 name|WINDOW
 modifier|*
 name|win
 decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|char
-name|c
+name|int
+name|ch
 decl_stmt|;
-end_decl_stmt
-
-begin_block
 block|{
-name|reg
+specifier|register
 name|char
+modifier|*
+name|end
+decl_stmt|,
 modifier|*
 name|temp1
 decl_stmt|,
 modifier|*
 name|temp2
-decl_stmt|;
-name|reg
-name|char
-modifier|*
-name|end
 decl_stmt|;
 name|end
 operator|=
@@ -135,7 +125,7 @@ expr_stmt|;
 operator|*
 name|temp1
 operator|=
-name|c
+name|ch
 expr_stmt|;
 name|touchline
 argument_list|(
@@ -207,13 +197,17 @@ expr_stmt|;
 block|}
 else|else
 return|return
+operator|(
 name|ERR
+operator|)
 return|;
 return|return
+operator|(
 name|OK
+operator|)
 return|;
 block|}
-end_block
+end_function
 
 end_unit
 
