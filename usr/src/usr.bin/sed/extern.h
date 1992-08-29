9947c0dad9288f270cb6f6d04a57ecc7a3b47d37
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-28 22:26:15 -0800

    change calling convention of mf_fgets, cspace; new externs match and maxnsub
    SCCS-vsn: usr.bin/sed/extern.h 5.4
    
    Former-commit-id: 05c50d080a64965cd8f3b4490968423351c0a101

--- usr/src/usr.bin/sed/extern.h
+++ usr/src/usr.bin/sed/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Diomidis Spinellis.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Diomidis Spinellis of Imperial College, University of London.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 Diomidis Spinellis.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Diomidis Spinellis of Imperial College, University of London.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_decl_stmt
@@ -18,6 +18,21 @@ name|struct
 name|s_appends
 modifier|*
 name|appends
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|regmatch_t
+modifier|*
+name|match
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|size_t
+name|maxnsub
 decl_stmt|;
 end_decl_stmt
 
@@ -108,14 +123,16 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|char
-modifier|*
+name|int
 name|mf_fgets
 name|__P
 argument_list|(
 operator|(
-name|size_t
+name|SPACE
 operator|*
+operator|,
+expr|enum
+name|e_spflag
 operator|)
 argument_list|)
 decl_stmt|;
@@ -187,6 +204,27 @@ operator|(
 expr|struct
 name|s_command
 operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|cspace
+name|__P
+argument_list|(
+operator|(
+name|SPACE
+operator|*
+operator|,
+name|char
+operator|*
+operator|,
+name|size_t
+operator|,
+expr|enum
+name|e_spflag
 operator|)
 argument_list|)
 decl_stmt|;
