6f546a4f53a6d7e95a044a88cbfdcd27c6b69c62
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-08-31 17:25:38 -0800

    Added termios.h to incldues.
    SCCS-vsn: games/worm/worm.c 5.9
    
    Former-commit-id: b7488d67baf8122c15ea427d540ab495ec4eb641

--- usr/src/games/worm/worm.c
+++ usr/src/games/worm/worm.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)worm.c	5.8 (Berkeley) %G%"
+literal|"@(#)worm.c	5.9 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -72,6 +72,12 @@ begin_include
 include|#
 directive|include
 file|<signal.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<termios.h>
 end_include
 
 begin_define
