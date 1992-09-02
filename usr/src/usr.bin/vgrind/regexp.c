bebf74c1cdd842fc81c3a405ba63189416e3b792
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-01 19:10:46 -0800

    Prototyped.
    SCCS-vsn: usr.bin/vgrind/regexp.c 5.4
    
    Former-commit-id: ef5aab7c29cadabebbf87ee98b6642584f671a7c

--- usr/src/usr.bin/vgrind/regexp.c
+++ usr/src/usr.bin/vgrind/regexp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+comment|/*  * Copyright (c) 1980 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)regexp.c	5.3 (Berkeley) %G%"
+literal|"@(#)regexp.c	5.4 (Berkeley) 8/3/92"
 decl_stmt|;
 end_decl_stmt
 
@@ -34,19 +34,23 @@ directive|include
 file|<ctype.h>
 end_include
 
-begin_typedef
-typedef|typedef
-name|int
-name|boolean
-typedef|;
-end_typedef
+begin_include
+include|#
+directive|include
+file|<stdlib.h>
+end_include
 
-begin_define
-define|#
-directive|define
-name|TRUE
-value|1
-end_define
+begin_include
+include|#
+directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|"extern.h"
+end_include
 
 begin_define
 define|#
@@ -58,9 +62,50 @@ end_define
 begin_define
 define|#
 directive|define
+name|TRUE
+value|!(FALSE)
+end_define
+
+begin_define
+define|#
+directive|define
 name|NIL
 value|0
 end_define
+
+begin_decl_stmt
+specifier|static
+name|void
+name|expconv
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|boolean
+name|_escaped
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* true if we are currently _escaped */
+end_comment
+
+begin_decl_stmt
+name|char
+modifier|*
+name|_start
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* start of string */
+end_comment
 
 begin_decl_stmt
 name|boolean
@@ -86,24 +131,25 @@ begin_comment
 comment|/*  STRNCMP -	like strncmp except that we convert the  *	 	first string to lower case before comparing  *		if l_onecase is set.  */
 end_comment
 
-begin_expr_stmt
+begin_function
+name|int
 name|STRNCMP
-argument_list|(
+parameter_list|(
 name|s1
-argument_list|,
+parameter_list|,
 name|s2
-argument_list|,
+parameter_list|,
 name|len
-argument_list|)
+parameter_list|)
 specifier|register
 name|char
-operator|*
+modifier|*
 name|s1
-operator|,
-operator|*
+decl_stmt|,
+decl|*
 name|s2
-expr_stmt|;
-end_expr_stmt
+decl_stmt|;
+end_function
 
 begin_decl_stmt
 specifier|register
@@ -377,17 +423,7 @@ value|16
 end_define
 
 begin_decl_stmt
-name|char
-modifier|*
-name|ure
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* pointer current position in unconverted exp */
-end_comment
-
-begin_decl_stmt
+specifier|static
 name|char
 modifier|*
 name|ccre
@@ -398,13 +434,17 @@ begin_comment
 comment|/* pointer to current position in converted exp*/
 end_comment
 
-begin_function_decl
+begin_decl_stmt
+specifier|static
 name|char
 modifier|*
-name|malloc
-parameter_list|()
-function_decl|;
-end_function_decl
+name|ure
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* pointer current position in unconverted exp */
+end_comment
 
 begin_function
 name|char
@@ -510,12 +550,11 @@ return|;
 block|}
 end_function
 
-begin_macro
+begin_function
+specifier|static
+name|void
 name|expconv
-argument_list|()
-end_macro
-
-begin_block
+parameter_list|()
 block|{
 specifier|register
 name|char
@@ -895,7 +934,7 @@ name|cs
 expr_stmt|;
 comment|/* offset to next symbol */
 break|break;
-comment|/* return from a recursion */
+comment|/* reurn from a recursion */
 case|case
 literal|')'
 case|:
@@ -1135,7 +1174,7 @@ expr_stmt|;
 block|}
 return|return;
 block|}
-end_block
+end_function
 
 begin_comment
 comment|/* end of convertre */
@@ -1143,27 +1182,6 @@ end_comment
 
 begin_comment
 comment|/*  *	The following routine recognises an irregular expresion  *	with the following special characters:  *  *		\?	-	means last match was optional  *		\a	-	matches any number of characters  *		\d	-	matches any number of spaces and tabs  *		\p	-	matches any number of alphanumeric  *				characters. The  *				characters matched will be copied into  *				the area pointed to by 'name'.  *		\|	-	alternation  *		\( \)	-	grouping used mostly for alternation and  *				optionality  *  *	The irregular expression must be translated to internal form  *	prior to calling this routine  *  *	The value returned is the pointer to the first non \a   *	character matched.  */
-end_comment
-
-begin_decl_stmt
-name|boolean
-name|_escaped
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* true if we are currently _escaped */
-end_comment
-
-begin_decl_stmt
-name|char
-modifier|*
-name|_start
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* start of string */
 end_comment
 
 begin_function
