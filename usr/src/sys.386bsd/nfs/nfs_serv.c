363cf6aade45a1ad4cfc51f75e07cab77ab7784c
Rick "gopher I <>
1992-09-08 00:00:00 +0000

    NFS KERNEL PATCH SET
    	Unofficial fixes for several nfs related problems:
    1 - RFC1094 does not clarify what the semantics of a create rpc should be
        on the server when the file already exists. It appears that some recent
        versions of SunOS expect the file to be truncated to length 0 only if
        the size field of the setattr has been specified. (This semantic has
        not been clarified by anyone within Sun ONC engineering, although I
        have tried mailing them.)
    
    2 - IBM AIX3.2 NFS clients expect a server to reply RPCPROG_MISMATCH to
        an rpc request sent to the nfs port (2049), but not nfs, in order to
        identify an AIX server. (The 386bsd.01 server simply dropped these
        requests on the floor, confusing AIX clients.)
    
    3 - Some servers require that the nfs client use a reserved port number,
        which the 386bsd.01 client did not. (The is a crock, since requiring
        a reserved port number does not enhance nfs security significantly.)
    
    AUTHOR:		Rick "gopher I" (root@snowhite.cis.uoguelph.ca)
    386BSD-Patchkit: patch00053
    
    Former-commit-id: c37b691af65d59a616d3487448a31ca4d4891f34

--- usr/src/sys.386bsd/nfs/nfs_serv.c
+++ usr/src/sys.386bsd/nfs/nfs_serv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfs_serv.c	7.40 (Berkeley) 5/15/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00016  * --------------------         -----   ----------------------  *  * 28 Aug 92	Arne Henrik Juul	Fixed NFS "create" bug  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfs_serv.c	7.40 (Berkeley) 5/15/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00053  * --------------------         -----   ----------------------  *  * 08 Sep 92	Rick "gopher I"		Fix "truncate" (conflicting?)  * 28 Aug 92	Arne Henrik Juul	Fixed NFS "create" bug  */
 end_comment
 
 begin_comment
@@ -3727,8 +3727,17 @@ operator|)
 argument_list|)
 expr_stmt|;
 comment|/* 28 Aug 92*/
+comment|/* 08 Sep 92*/
 if|if
 condition|(
+name|vap
+operator|->
+name|va_size
+operator|!=
+operator|-
+literal|1
+operator|&&
+operator|(
 name|error
 operator|=
 name|VOP_SETATTR
@@ -3741,6 +3750,7 @@ name|cred
 argument_list|,
 name|p
 argument_list|)
+operator|)
 condition|)
 block|{
 name|vput
@@ -8059,9 +8069,8 @@ name|bpos
 decl_stmt|;
 name|int
 name|error
-init|=
-literal|0
 decl_stmt|;
+comment|/* 08 Sep 92*/
 name|struct
 name|mbuf
 modifier|*
@@ -8070,6 +8079,18 @@ decl_stmt|,
 modifier|*
 name|mreq
 decl_stmt|;
+if|if
+condition|(
+operator|*
+name|repstat
+condition|)
+comment|/* 08 Sep 92*/
+name|error
+operator|=
+operator|*
+name|repstat
+expr_stmt|;
+else|else
 name|error
 operator|=
 name|EPROCUNAVAIL
