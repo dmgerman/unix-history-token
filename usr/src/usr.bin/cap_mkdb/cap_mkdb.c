c38b0f95183239faba78412553541de6b91c97e6
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-16 17:58:46 -0800

    copyright[] -> static.
    SCCS-vsn: usr.bin/cap_mkdb/cap_mkdb.c 1.10
    
    Former-commit-id: 65b4ebf20b961dbe76397735590de57eabc02605

--- usr/src/usr.bin/cap_mkdb/cap_mkdb.c
+++ usr/src/usr.bin/cap_mkdb/cap_mkdb.c
@@ -10,6 +10,7 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
@@ -39,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)cap_mkdb.c	1.9 (Berkeley) %G%"
+literal|"@(#)cap_mkdb.c	1.10 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
