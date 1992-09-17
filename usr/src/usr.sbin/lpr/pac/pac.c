4e847789353070924f45d7202a15471777f7f4b5
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-16 17:56:17 -0800

    copyright[] -> static.
    SCCS-vsn: usr.sbin/lpr/pac/pac.c 5.9
    SCCS-vsn: usr.bin/tip/tip.c 5.17
    SCCS-vsn: usr.bin/tip/remote.c 5.9
    
    Former-commit-id: 9fce194f40c16d6e6824b98aa63c1f63e5be7e9d

--- usr/src/usr.sbin/lpr/pac/pac.c
+++ usr/src/usr.sbin/lpr/pac/pac.c
@@ -10,6 +10,7 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
@@ -39,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)pac.c	5.8 (Berkeley) %G%"
+literal|"@(#)pac.c	5.9 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
