83ea907c372af2a8474750c01f94e035dc37859b
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-16 17:51:23 -0800

    copyright[] -> static.
    SCCS-vsn: usr.sbin/lpr/lpc/lpc.c 5.15
    SCCS-vsn: usr.sbin/lpr/lpc/cmds.c 5.11
    
    Former-commit-id: 58de6444325cd9294993f91c38399af48cf12856

--- usr/src/usr.sbin/lpr/lpc/cmds.c
+++ usr/src/usr.sbin/lpr/lpc/cmds.c
@@ -10,6 +10,7 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
@@ -39,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)cmds.c	5.10 (Berkeley) %G%"
+literal|"@(#)cmds.c	5.11 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
