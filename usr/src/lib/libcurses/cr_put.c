4ba1c23fdd5b44e05f442eeeda0994196dd28a9b
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-22 20:38:07 -0800

    Fixed standout bug.
    SCCS-vsn: lib/libcurses/cr_put.c 5.10
    
    Former-commit-id: b21a6b90a078d2aa1b66175f66aa4d1cc1d8b8d6

--- usr/src/lib/libcurses/cr_put.c
+++ usr/src/lib/libcurses/cr_put.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)cr_put.c	5.9 (Berkeley) %G%"
+literal|"@(#)cr_put.c	5.10 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1411,7 +1411,7 @@ name|curscr
 operator|->
 name|flags
 operator|&
-name|__STANDOUT
+name|__WSTANDOUT
 operator|)
 condition|)
 name|putchar
