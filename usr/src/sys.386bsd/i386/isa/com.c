34b4eff3ed95002bf8995849d1772eb5a26e55f9
Rodney W. Grimes <rgrimes@cdrom.com>
1992-09-23 00:00:00 +0000

    FIXES SILO OVERFLOWS IF A 16550 IS INSTALLED
    Here is a patch to fix silo overflows if you have 16550 uarts.  The
    problem is due to the FIFO trigger level being set to 14 bytes.  This
    only leaves 2 bytes after the interupt before it overflows.  This is
    not enought time at high speeds.  By changing the trigger level to
    4 bytes I have had a 38400 baud line running full speed with out any
    silo overflows.
    
    AUTHOR:		Rodney W. Grimes (rgrimes@agora.rain.com)
    386BSD-Patchkit: patch00045
    
    Former-commit-id: c112fef36eac2e90fa1bb18443f21ee7a9e320d8

--- usr/src/sys.386bsd/i386/isa/com.c
+++ usr/src/sys.386bsd/i386/isa/com.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)com.c	7.5 (Berkeley) 5/16/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00019  * --------------------         -----   ----------------------  *  * 30 Aug 92	Poul-Henning Kamp	Stabilize SLIP on lossy lines/UARTS  * 09 Aug 92	Christoph Robitschko	Correct minor number on com ports  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)com.c	7.5 (Berkeley) 5/16/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         3       00045  * --------------------         -----   ----------------------  *  * 23 Sep 92	Rodney W. Grimes	Fix SILO overflow on 16550 UARTS  * 30 Aug 92	Poul-Henning Kamp	Stabilize SLIP on lossy lines/UARTS  * 09 Aug 92	Christoph Robitschko	Correct minor number on com ports  */
 end_comment
 
 begin_decl_stmt
@@ -597,7 +597,7 @@ name|FIFO_RCV_RST
 operator||
 name|FIFO_XMT_RST
 operator||
-name|FIFO_TRIGGER_14
+name|FIFO_TRIGGER_4
 argument_list|)
 expr_stmt|;
 name|DELAY
@@ -2677,7 +2677,7 @@ name|com_fifo
 argument_list|,
 name|FIFO_ENABLE
 operator||
-name|FIFO_TRIGGER_14
+name|FIFO_TRIGGER_4
 argument_list|)
 expr_stmt|;
 return|return
@@ -3443,7 +3443,7 @@ name|FIFO_RCV_RST
 operator||
 name|FIFO_XMT_RST
 operator||
-name|FIFO_TRIGGER_14
+name|FIFO_TRIGGER_4
 argument_list|)
 expr_stmt|;
 name|stat
