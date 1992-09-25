9d0fb3fee3fce53a2ed7abf6e1eca5097db9e9b9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-09-25 04:52:02 -0800

    delete antique code to check bfreelist[0].b_forw (no longer used)
    SCCS-vsn: sys/hp300/hp300/machdep.c 7.32
    
    Former-commit-id: b0ed70f49809db146612be649649a2d115b2dcf4

--- usr/src/sys/hp300/hp300/machdep.c
+++ usr/src/sys/hp300/hp300/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.68 92/01/20$  *  *	@(#)machdep.c	7.31 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.68 92/01/20$  *  *	@(#)machdep.c	7.32 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -4102,13 +4102,6 @@ operator|&&
 name|waittime
 operator|<
 literal|0
-operator|&&
-name|bfreelist
-index|[
-literal|0
-index|]
-operator|.
-name|b_forw
 condition|)
 block|{
 specifier|register
