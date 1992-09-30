c3d34725dde5fb5ddfcf984632fd599c8af3657c
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-09-30 00:38:27 -0800

    BSD 4_4 development Work on file usr/src/contrib/gdb-4.7.lbl/gdb/nm-sun4os4.h Work on file usr/src/contrib/gdb-4.7.lbl/gdb/nm.h Work on file usr/src/contrib/gdb-4.7.lbl/gdb/nm-trash.h
    Synthesized-from: CSRG/cd3/4.4
    
    Former-commit-id: 3812561a07f862b5e1d0a9be8b207d2ca7060a92

---
+++ usr/src/contrib/gdb-4.7.lbl/gdb/nm-trash.h
@@ -0,0 +1,7 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* this file is temporary scaffolding until all hosts have the    native/target/host split in place.  FIXME.  */
+end_comment
+
+end_unit
+
