99860e93bdbc02761968dcbfc721007fdd96accb
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-10-01 23:05:31 -0800

    quit
    SCCS-vsn: lib/libcurses/ctrace.c 5.3
    
    Former-commit-id: 9379f4c7459c282b44626318ffb6f52e049b42b7

--- usr/src/lib/libcurses/ctrace.c
+++ usr/src/lib/libcurses/ctrace.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)ctrace.c	5.2 (Berkeley) %G%"
+literal|"@(#)ctrace.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -27,6 +27,12 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|DEBUG
+end_define
 
 begin_ifdef
 ifdef|#
