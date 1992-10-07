853b31ac3b485694fa9b319ee90cde48b898185d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-10-07 08:11:12 -0800

    use a more efficient queue implementation
    SCCS-vsn: sys/sys/queue.h 7.4
    
    Former-commit-id: a922ff0c1538c6e4b7840503daa1126a0142523e

--- usr/src/sys/sys/queue.h
+++ usr/src/sys/sys/queue.h
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)queue.h	7.3 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Author: Avadis Tevanian, Jr.  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
-end_comment
-
-begin_comment
-comment|/*  *	Type definitions for generic queues.  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)queue.h	7.4 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Author: Avadis Tevanian, Jr.  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_ifndef
@@ -19,25 +15,186 @@ directive|define
 name|_QUEUE_H_
 end_define
 
+begin_comment
+comment|/*  * This file defines two types of data structures, lists and queues.  *  * A list is headed by a single forward pointer (or an array of forward  * pointers for a hash table header). The elements are doubly linked  * so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list after  * an existing element or at the head of the list.  *  * A queue is headed by a pair of pointers, one to the head of the list  * and the other to the tail of the list. The elements are doubly linked  * so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list after  * an existing element, at the head of the list, or at the end of  * the list.  *  * Note that the implementation used here avoids the need to special  * case inserting into an empty list, deleting the last element from  * a list, or inserting at the beginning or end of a list. The drawback  * to this method is that it is not possible to traverse a list or  * queue backwards.  */
+end_comment
+
 begin_struct
 struct|struct
 name|queue_entry
 block|{
-name|struct
-name|queue_entry
+name|void
 modifier|*
 name|next
 decl_stmt|;
 comment|/* next element */
-name|struct
-name|queue_entry
+name|void
+modifier|*
 modifier|*
 name|prev
 decl_stmt|;
-comment|/* previous element */
+comment|/* address of previous next element */
 block|}
 struct|;
 end_struct
+
+begin_struct
+struct|struct
+name|list_entry
+block|{
+name|void
+modifier|*
+name|next
+decl_stmt|;
+comment|/* next element */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * Value for pointers on removed entries.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NOLIST
+value|(void *)0xdead
+end_define
+
+begin_comment
+comment|/*  * Queue functions.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|queue_init
+parameter_list|(
+name|head
+parameter_list|)
+value|((head)->next = 0, (head)->prev =&(head)->next)
+end_define
+
+begin_define
+define|#
+directive|define
+name|queue_enter_tail
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	(elm)->field.next = 0; \ 	(elm)->field.prev = (head)->prev; \ 	*(head)->prev = (elm); \ 	(head)->prev =&(elm)->field.next; \ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|queue_enter_head
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	type queue_ptr; \ 	if (queue_ptr = (head)->next) \ 		queue_ptr->field.prev =&(elm)->field.next; \ 	else \ 		(head)->prev =&(elm)->field.next; \ 	(head)->next = (elm); \ 	(elm)->field.next = queue_ptr; \ 	(elm)->field.prev =&(head)->next; \ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|queue_insert_after
+parameter_list|(
+name|head
+parameter_list|,
+name|listelm
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	type queue_ptr; \ 	if (queue_ptr = (listelm)->next) \ 		queue_ptr->field.prev =&(elm)->field.next; \ 	else \ 		(head)->prev =&(elm)->field.next; \ 	(listelm)->next = (elm); \ 	(elm)->field.next = queue_ptr; \ 	(elm)->field.prev =&(listelm)->next; \ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|queue_remove
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	type queue_ptr; \ 	if (queue_ptr = (elm)->field.next) \ 		queue_ptr->field.prev = (elm)->field.prev; \ 	else \ 		(head)->prev = (elm)->field.prev; \ 	*(elm)->field.prev = queue_ptr; \ 	(elm)->field.next = NOLIST; \ 	(elm)->field.prev = NOLIST; \ }
+end_define
+
+begin_comment
+comment|/*  * List functions.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|list_enter_head
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	type queue_ptr; \ 	if (queue_ptr = (head)->next) \ 		queue_ptr->field.prev =&(elm)->field.next; \ 	(head)->next = (elm); \ 	(elm)->field.next = queue_ptr; \ 	(elm)->field.prev =&(head)->next; \ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|list_insert_after
+parameter_list|(
+name|listelm
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	type queue_ptr; \ 	if (queue_ptr = (listelm)->next) \ 		queue_ptr->field.prev =&(elm)->field.next; \ 	(listelm)->next = (elm); \ 	(elm)->field.next = queue_ptr; \ 	(elm)->field.prev =&(listelm)->next; \ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|list_remove
+parameter_list|(
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{ \ 	type queue_ptr; \ 	if (queue_ptr = (elm)->field.next) \ 		queue_ptr->field.prev = (elm)->field.prev; \ 	*(elm)->field.prev = queue_ptr; \ 	(elm)->field.next = NOLIST; \ 	(elm)->field.prev = NOLIST; \ }
+end_define
+
+begin_comment
+comment|/*  * Compatibility with old MACH VM code.  */
+end_comment
 
 begin_typedef
 typedef|typedef
@@ -76,89 +233,11 @@ end_typedef
 begin_define
 define|#
 directive|define
-name|round_queue
-parameter_list|(
-name|size
-parameter_list|)
-value|(((size)+7)& (~7))
-end_define
-
-begin_define
-define|#
-directive|define
-name|enqueue
-parameter_list|(
-name|queue
-parameter_list|,
-name|elt
-parameter_list|)
-value|enqueue_tail(queue, elt)
-end_define
-
-begin_define
-define|#
-directive|define
-name|dequeue
-parameter_list|(
-name|queue
-parameter_list|)
-value|dequeue_head(queue)
-end_define
-
-begin_define
-define|#
-directive|define
-name|enqueue_head
-parameter_list|(
-name|queue
-parameter_list|,
-name|elt
-parameter_list|)
-value|insque(elt,queue)
-end_define
-
-begin_define
-define|#
-directive|define
-name|enqueue_tail
-parameter_list|(
-name|queue
-parameter_list|,
-name|elt
-parameter_list|)
-value|insque(elt,(queue)->prev)
-end_define
-
-begin_define
-define|#
-directive|define
-name|remqueue
-parameter_list|(
-name|queue
-parameter_list|,
-name|elt
-parameter_list|)
-value|remque(elt)
-end_define
-
-begin_define
-define|#
-directive|define
-name|queue_init
-parameter_list|(
-name|q
-parameter_list|)
-value|((q)->next = (q)->prev = q)
-end_define
-
-begin_define
-define|#
-directive|define
 name|queue_first
 parameter_list|(
-name|q
+name|head
 parameter_list|)
-value|((q)->next)
+value|((head)->next)
 end_define
 
 begin_define
@@ -166,21 +245,9 @@ define|#
 directive|define
 name|queue_next
 parameter_list|(
-name|qc
+name|elm
 parameter_list|)
-value|((qc)->next)
-end_define
-
-begin_define
-define|#
-directive|define
-name|queue_end
-parameter_list|(
-name|q
-parameter_list|,
-name|qe
-parameter_list|)
-value|((q) == (qe))
+value|((elm)->next)
 end_define
 
 begin_define
@@ -188,9 +255,21 @@ define|#
 directive|define
 name|queue_empty
 parameter_list|(
-name|q
+name|head
 parameter_list|)
-value|queue_end((q), queue_first(q))
+value|((head)->next == 0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|queue_end
+parameter_list|(
+name|elm
+parameter_list|,
+name|head
+parameter_list|)
+value|((elm) == 0)
 end_define
 
 begin_define
@@ -206,30 +285,14 @@ name|type
 parameter_list|,
 name|field
 parameter_list|)
-value|{			\ 	if (queue_empty((head))) {				\ 		(head)->next = (queue_entry_t) elt;		\ 		(head)->prev = (queue_entry_t) elt;		\ 		(elt)->field.next = head;			\ 		(elt)->field.prev = head;			\ 	} else {						\ 		register queue_entry_t prev = (head)->prev;	\ 		(elt)->field.prev = prev;			\ 		(elt)->field.next = head;			\ 		(head)->prev = (queue_entry_t)(elt);		\ 		((type)prev)->field.next = (queue_entry_t)(elt);\ 	}							\ }
-end_define
-
-begin_define
-define|#
-directive|define
-name|queue_field
-parameter_list|(
-name|head
-parameter_list|,
-name|thing
-parameter_list|,
-name|type
-parameter_list|,
-name|field
-parameter_list|)
 define|\
-value|(((head) == (thing)) ? (head) :&((type)(thing))->field)
+value|queue_enter_tail(head, elt, type, field)
 end_define
 
 begin_define
 define|#
 directive|define
-name|queue_remove
+name|queue_remove_first
 parameter_list|(
 name|head
 parameter_list|,
@@ -239,39 +302,7 @@ name|type
 parameter_list|,
 name|field
 parameter_list|)
-value|{			\ 	register queue_entry_t next = (elt)->field.next;	\ 	register queue_entry_t prev = (elt)->field.prev;	\ 	queue_field((head), next, type, field)->prev = prev;	\ 	queue_field((head), prev, type, field)->next = next;	\ }
-end_define
-
-begin_define
-define|#
-directive|define
-name|queue_assign
-parameter_list|(
-name|to
-parameter_list|,
-name|from
-parameter_list|,
-name|type
-parameter_list|,
-name|field
-parameter_list|)
-value|{			\ 	((type)((from)->prev))->field.next = (to);		\ 	((type)((from)->next))->field.prev = (to);		\ 	*to = *from;						\ }
-end_define
-
-begin_define
-define|#
-directive|define
-name|queue_remove_first
-parameter_list|(
-name|h
-parameter_list|,
-name|e
-parameter_list|,
-name|t
-parameter_list|,
-name|f
-parameter_list|)
-value|{			\ 	e = (t) queue_first((h));				\ 	queue_remove((h), (e), t, f);				\ }
+value|{ \ 	elt = queue_first(head); \ 	queue_remove(head, elt, type, field) \ }
 end_define
 
 begin_endif
