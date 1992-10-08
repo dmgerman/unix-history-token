f83ca0acfc5ce725cf4f4426ae05125f6a48a5e8
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-08 00:56:37 -0800

    Add fields to ufsmount structure for integration of ffs/lfs_bmap.
    SCCS-vsn: sys/ufs/ffs/ufsmount.h 7.15
    SCCS-vsn: sys/ufs/ufs/ufsmount.h 7.15
    
    Former-commit-id: ccf078b6f9e1ecac2880f4009ea8a2abfd5b4bb6

--- usr/src/sys/ufs/ffs/ufsmount.h
+++ usr/src/sys/ufs/ffs/ufsmount.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufsmount.h	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufsmount.h	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -136,6 +136,18 @@ name|MAXQUOTAS
 index|]
 decl_stmt|;
 comment|/* quota file access cred */
+name|u_long
+name|um_nindir
+decl_stmt|;
+comment|/* indirect ptrs per block */
+name|u_long
+name|um_bptrtodb
+decl_stmt|;
+comment|/* indir ptr to disk block */
+name|u_long
+name|um_seqinc
+decl_stmt|;
+comment|/* inc between seq blocks */
 name|time_t
 name|um_btime
 index|[
