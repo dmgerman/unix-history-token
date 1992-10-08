3a62aa55afa5a55ece88c46a58667ea1e87b1115
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-08 01:14:57 -0800

    Add comment for extra argument in bmap call.
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.95
    
    Former-commit-id: 94a12d7ad17828f85d835ee6190cd2e40244c91c

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.94 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.95 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -10979,7 +10979,7 @@ name|ap
 parameter_list|)
 name|struct
 name|vop_bmap_args
-comment|/* { 		struct vnode *a_vp; 		daddr_t  a_bn; 		struct vnode **a_vpp; 		daddr_t *a_bnp; 	} */
+comment|/* { 		struct vnode *a_vp; 		daddr_t  a_bn; 		struct vnode **a_vpp; 		daddr_t *a_bnp; 		int *a_runp; 	} */
 modifier|*
 name|ap
 decl_stmt|;
