faa0ea587ee52965db4400a3e9164a6ba11df36f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-10-08 08:05:31 -0800

    use new queue routines for buffer lists hanging off vnode
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.81
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 7.94
    SCCS-vsn: sys/nfs/nfs_nqlease.c 7.13
    SCCS-vsn: sys/nfs/nfs_vfsops.c 7.47
    
    Former-commit-id: 63bc2fe26e38e298d834c5961a216f0e98b0dc23

--- usr/src/sys/nfs/nfs_nqlease.c
+++ usr/src/sys/nfs/nfs_nqlease.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_nqlease.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_nqlease.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -5749,6 +5749,8 @@ operator|&&
 name|vp
 operator|->
 name|v_dirtyblkhd
+operator|.
+name|le_next
 operator|&&
 name|vget
 argument_list|(
