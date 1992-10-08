06516adf2b5f3a8d2619619a2b9862a2cf236717
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-08 01:15:48 -0800

    Add an argument to bmap.
    SCCS-vsn: sys/dev/vn.c 7.13
    
    Former-commit-id: c213a55b4c938b1be57c75bcbceaf224e5fcf2ee

--- usr/src/sys/dev/vn.c
+++ usr/src/sys/dev/vn.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: fd.c 1.3 89/12/03$  *  *	@(#)vn.c	7.12 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: fd.c 1.3 89/12/03$  *  *	@(#)vn.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -636,6 +636,8 @@ name|vp
 argument_list|,
 operator|&
 name|nbn
+argument_list|,
+name|NULL
 argument_list|)
 expr_stmt|;
 ifdef|#
