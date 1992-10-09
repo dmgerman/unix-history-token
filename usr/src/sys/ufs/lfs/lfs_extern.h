8e1e9c8e68d701bfc3afea7ef949500d9b7e8ad4
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-08 21:45:14 -0800

    Remove lfs_bmap and lfs_bmaparray calls (use ufs ones instead).
    SCCS-vsn: sys/ufs/lfs/lfs_extern.h 7.22
    
    Former-commit-id: 049ad6493816c593d3ed44d143d627dca5e1c671

--- usr/src/sys/ufs/lfs/lfs_extern.h
+++ usr/src/sys/ufs/lfs/lfs_extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_extern.h	7.21 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_extern.h	7.22 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -108,45 +108,6 @@ argument_list|(
 operator|(
 expr|struct
 name|vop_blkatoff_args
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|lfs_bmap
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|vop_bmap_args
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|lfs_bmaparray
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|vnode
-operator|*
-operator|,
-name|daddr_t
-operator|,
-name|daddr_t
-operator|*
-operator|,
-name|INDIR
-operator|*
-operator|,
-name|int
 operator|*
 operator|)
 argument_list|)
