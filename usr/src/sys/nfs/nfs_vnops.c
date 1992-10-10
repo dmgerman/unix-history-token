0255e23db721a62990a4eae7178fa5d3d86f8e78
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-10-10 06:44:33 -0800

    have to dup, not copy credentials in nfs_doio
    SCCS-vsn: sys/nfs/nfs_vnops.c 7.97
    
    Former-commit-id: 938e16f71270eb7c7ac3fb714bacb6a6d7e3f39e

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.96 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	7.97 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -11334,7 +11334,7 @@ name|pageproc
 expr_stmt|;
 name|cr
 operator|=
-name|crcopy
+name|crdup
 argument_list|(
 name|uiop
 operator|->
