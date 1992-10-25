fda21e66716190408daf85b4eaa2cea482f6c24d
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-24 18:00:44 -0800

    Check all flags, not just B_CACHE to determine if block was in cache.
    SCCS-vsn: sys/ufs/lfs/lfs_syscalls.c 7.26
    
    Former-commit-id: 9b1b7b1dd5ff2e3f8ebb927894c3450100274e7e

--- usr/src/sys/ufs/lfs/lfs_syscalls.c
+++ usr/src/sys/ufs/lfs/lfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_syscalls.c	7.25 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_syscalls.c	7.26 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -838,7 +838,13 @@ name|bp
 operator|->
 name|b_flags
 operator|&
+operator|(
+name|B_DELWRI
+operator||
+name|B_DONE
+operator||
 name|B_CACHE
+operator|)
 operator|)
 operator|&&
 operator|(
