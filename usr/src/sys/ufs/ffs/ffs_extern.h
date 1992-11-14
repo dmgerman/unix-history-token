47e6610ffbb47dcf4dbb4be03142d5b2104b991f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-11-14 00:53:57 -0800

    ffs_inactive moves to ufs_inactive
    SCCS-vsn: sys/ufs/ffs/ffs_extern.h 7.15
    SCCS-vsn: sys/ufs/ffs/ufs_extern.h 7.17
    SCCS-vsn: sys/ufs/ufs/ufs_extern.h 7.17
    
    Former-commit-id: 3fdb5c402962dd228ea82a063edfc891eda5d6d3

--- usr/src/sys/ufs/ffs/ffs_extern.h
+++ usr/src/sys/ufs/ffs/ffs_extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_extern.h	7.14 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_extern.h	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -288,20 +288,6 @@ argument_list|(
 operator|(
 expr|struct
 name|vop_fsync_args
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|ffs_inactive
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|vop_inactive_args
 operator|*
 operator|)
 argument_list|)
--- usr/src/sys/ufs/ffs/ufs_extern.h
+++ usr/src/sys/ufs/ffs/ufs_extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_extern.h	7.16 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_extern.h	7.17 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -615,6 +615,20 @@ argument_list|(
 operator|(
 expr|struct
 name|inode
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|ufs_inactive
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vop_inactive_args
 operator|*
 operator|)
 argument_list|)
