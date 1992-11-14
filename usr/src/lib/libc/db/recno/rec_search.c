7bef295019e59a7558d88de11195445e2fb9cc36
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-11-14 01:31:50 -0800

    prettiness police
    SCCS-vsn: lib/libc/db/recno/rec_search.c 5.5
    
    Former-commit-id: 8cf2c974d8d29fc464fa99d02576bd954bb2571e

--- usr/src/lib/libc/db/recno/rec_search.c
+++ usr/src/lib/libc/db/recno/rec_search.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)rec_search.c	5.4 (Berkeley) %G%"
+literal|"@(#)rec_search.c	5.5 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -46,13 +46,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<errno.h>
+file|<db.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<db.h>
+file|<errno.h>
 end_include
 
 begin_include
