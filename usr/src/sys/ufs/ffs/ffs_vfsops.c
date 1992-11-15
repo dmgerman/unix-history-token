a25d73148ef75bccd30f60c5dd168790c379cf26
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-11-15 06:58:22 -0800

    ufs_iput obsoleted by vput
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.82
    
    Former-commit-id: ea0e1e70a44b99afa920b231d1ce18e778df3e29

--- usr/src/sys/ufs/ffs/ffs_vfsops.c
+++ usr/src/sys/ufs/ffs/ffs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	7.81 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	7.82 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3212,16 +3212,16 @@ name|bp
 argument_list|)
 condition|)
 block|{
-comment|/* 		 * The inode does not contain anything useful, so it would 		 * be misleading to leave it on its hash chain. It will be 		 * returned to the free list by ufs_iput(). 		 */
+comment|/* 		 * The inode does not contain anything useful, so it would 		 * be misleading to leave it on its hash chain. It will be 		 * returned to the free list by vput(). 		 */
 name|ufs_ihashrem
 argument_list|(
 name|ip
 argument_list|)
 expr_stmt|;
 comment|/* Unlock and discard unneeded inode. */
-name|ufs_iput
+name|vput
 argument_list|(
-name|ip
+name|vp
 argument_list|)
 expr_stmt|;
 name|brelse
@@ -3287,9 +3287,9 @@ name|vp
 argument_list|)
 condition|)
 block|{
-name|ufs_iput
+name|vput
 argument_list|(
-name|ip
+name|vp
 argument_list|)
 expr_stmt|;
 operator|*
