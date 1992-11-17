94751284e44b675d0b5547e42550309d2423b865
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-11-16 19:30:45 -0800

    add MF_FOLDCASE flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 5.43
    
    Former-commit-id: b966434b724657eda95104f2f02ca1fc4330a10d

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	5.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	5.43 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	5.42		%G%"
+literal|"@(#)sendmail.h	5.43		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1986,6 +1986,17 @@ end_define
 
 begin_comment
 comment|/* don't complain if map not found */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MF_FOLDCASE
+value|00010
+end_define
+
+begin_comment
+comment|/* fold case in keys */
 end_comment
 
 begin_escape
