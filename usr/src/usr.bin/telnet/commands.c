c92f03cdebb510eca6b03a7dea10fd43251a4c4a
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-11-17 07:07:59 -0800

    rm bogus variable to fix "send" command (jonathan@CS.Stanford.EDU)
    SCCS-vsn: usr.bin/telnet/commands.c 5.7
    SCCS-vsn: usr.bin/telnet/commands.c 5.7
    
    Former-commit-id: 9ad196018ce62be99c25e196cea4aafb3c05b1db

--- usr/src/usr.bin/telnet/commands.c
+++ usr/src/usr.bin/telnet/commands.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)commands.c	5.6 (Berkeley) %G%"
+literal|"@(#)commands.c	5.7 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1492,10 +1492,6 @@ name|argv
 decl_stmt|;
 block|{
 name|int
-name|what
-decl_stmt|;
-comment|/* what we are sending this time */
-name|int
 name|count
 decl_stmt|;
 comment|/* how many bytes we are going to need to send */
@@ -1870,6 +1866,8 @@ name|NET2ADD
 argument_list|(
 name|IAC
 argument_list|,
+name|s
+operator|->
 name|what
 argument_list|)
 expr_stmt|;
@@ -1879,6 +1877,8 @@ literal|"SENT"
 argument_list|,
 name|IAC
 argument_list|,
+name|s
+operator|->
 name|what
 argument_list|)
 expr_stmt|;
