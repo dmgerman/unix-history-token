b635c3b72de6cee87a72686686c8bb4c21c09fe3
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-11-17 01:56:19 -0800

    add event counters; expand dv_xname for external use; fix copyright
    SCCS-vsn: sys/sys/device.h 7.5
    
    Former-commit-id: f7316ab2ba4023e63871aecf9c839ff55dfcd044

--- usr/src/sys/sys/device.h
+++ usr/src/sys/sys/device.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)device.h	7.4 (Berkeley) %G%  *  * from: $Header: device.h,v 1.6 92/06/11 17:56:45 torek Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)device.h	7.5 (Berkeley) %G%  *  * from: $Header: device.h,v 1.6 92/06/11 17:56:45 torek Exp $ (LBL)  */
 end_comment
 
 begin_comment
