abb3d49c9c56999eb7b4992973fce541c1b36bf1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-11-17 21:45:16 -0800

    add rindex()
    SCCS-vsn: sys/libkern/libkern.h 7.5
    
    Former-commit-id: 8d94c005b8da526c34dab55bd0969996b9e66658

--- usr/src/sys/libkern/libkern.h
+++ usr/src/sys/libkern/libkern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)libkern.h	7.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)libkern.h	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -309,6 +309,23 @@ name|__P
 argument_list|(
 operator|(
 name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|rindex
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+name|int
 operator|)
 argument_list|)
 decl_stmt|;
