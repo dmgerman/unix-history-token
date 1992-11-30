7010e817c31de04644e2bbdf66c4bbe89de17088
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-11-29 22:05:35 -0800

    typo, missing comma
    SCCS-vsn: bin/sh/error.c 5.2
    
    Former-commit-id: eca7fa828699b69bb8b6ae2f98f96e6cc3537347

--- usr/src/bin/sh/error.c
+++ usr/src/bin/sh/error.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)error.c	5.1 (Berkeley) %G%"
+literal|"@(#)error.c	5.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -585,6 +585,7 @@ block|,
 name|ALL
 block|,
 literal|"remote access failed"
+block|,
 endif|#
 directive|endif
 ifdef|#
