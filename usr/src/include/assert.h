09d9e19f682fd105676c8ff00ec0ee48a44bc973
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-12-04 01:20:09 -0800

    avoid warnings about _assert redeclaration
    SCCS-vsn: include/assert.h 5.3
    
    Former-commit-id: af2e78f2123aec0250051fd859164267721c064a

--- usr/src/include/assert.h
+++ usr/src/include/assert.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)assert.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)assert.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -11,6 +11,12 @@ begin_undef
 undef|#
 directive|undef
 name|assert
+end_undef
+
+begin_undef
+undef|#
+directive|undef
+name|_assert
 end_undef
 
 begin_ifdef
