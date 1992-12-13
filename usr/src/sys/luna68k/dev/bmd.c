a895e833fa4a08bb404cd207b812cab859eb7132
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1992-12-13 11:51:48 -0800

    make kernel includes standard
    SCCS-vsn: sys/luna68k/dev/bmd.c 7.2
    SCCS-vsn: sys/luna68k/stand/bmd.c 7.2
    
    Former-commit-id: f55750e4dc0fe8ae629adffa864c1828e2c7a2a2

--- usr/src/sys/luna68k/dev/bmd.c
+++ usr/src/sys/luna68k/dev/bmd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)bmd.c	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)bmd.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -10,13 +10,13 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"param.h"
+file|<sys/param.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|"systm.h"
+file|<sys/systm.h>
 end_include
 
 begin_decl_stmt
