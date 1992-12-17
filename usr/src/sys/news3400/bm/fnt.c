0f5a91db8b4c9097835dacea52c688d1cb6909e2
Kazumasa Utashiro <utashiro@ucbvax.Berkeley.EDU>
1992-12-17 05:10:31 -0800

    fixed include file name
    SCCS-vsn: sys/news3400/bm/fnt.c 7.2
    
    Former-commit-id: 106ff1a9c595b1b7321e9d50220c86c2ef9fb89f

--- usr/src/sys/news3400/bm/fnt.c
+++ usr/src/sys/news3400/bm/fnt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  * from: $Hdr: fnt.c,v 4.300 91/06/09 06:14:43 root Rel41 $ SONY  *  *	@(#)fnt.c	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  * from: $Hdr: fnt.c,v 4.300 91/06/09 06:14:43 root Rel41 $ SONY  *  *	@(#)fnt.c	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -49,7 +49,7 @@ end_ifdef
 begin_include
 include|#
 directive|include
-file|"newsipc/newsipc.h"
+file|"../ipc/newsipc.h"
 end_include
 
 begin_ifdef
