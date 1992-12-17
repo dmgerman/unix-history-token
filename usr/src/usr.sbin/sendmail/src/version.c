dc1ffe6e84cf69ddd561465f5861f8762bcc58b5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-12-17 01:24:43 -0800

    clearly mark the version number as an alpha
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.135
    
    Former-commit-id: 39f26914f188ad4c2befc5c8d72e41aea70c6ca9

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	5.134 (Berkeley) %G%"
+literal|"@(#)version.c	5.135 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"5.134"
+literal|"ALPHA-5.135"
 decl_stmt|;
 end_decl_stmt
 
