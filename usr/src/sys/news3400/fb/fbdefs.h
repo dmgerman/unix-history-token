d630a929e0f7d731c305c38838e75c656fbe0dff
Kazumasa Utashiro <utashiro@ucbvax.Berkeley.EDU>
1992-12-17 05:56:22 -0800

    WB_FLUSH macro calls MachEmptyWriteBuffer()
    SCCS-vsn: sys/news3400/fb/fbdefs.h 7.2
    
    Former-commit-id: 0ef6f6f8e845f062af146f912d1182e28ed097bb

--- usr/src/sys/news3400/fb/fbdefs.h
+++ usr/src/sys/news3400/fb/fbdefs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  * from: $Hdr: fbdefs.h,v 4.300 91/06/09 06:33:54 root Rel41 $ SONY  *  *	@(#)fbdefs.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  * from: $Hdr: fbdefs.h,v 4.300 91/06/09 06:33:54 root Rel41 $ SONY  *  *	@(#)fbdefs.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -223,7 +223,7 @@ begin_define
 define|#
 directive|define
 name|WB_FLUSH
-value|wb_flush()
+value|MachEmptyWriteBuffer()
 end_define
 
 begin_else
