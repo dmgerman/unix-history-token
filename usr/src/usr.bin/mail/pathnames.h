218589155b552ea22fbae71df526cee3c8101f58
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-17 20:05:05 -0800

    mail doesn't need to log who sends mail, does it?
    SCCS-vsn: usr.bin/mail/send.c 5.25
    SCCS-vsn: usr.bin/mail/pathnames.h 5.16
    
    Former-commit-id: 3d77fdb4c4e6da833881f37151106ebdeeeb9e01

--- usr/src/usr.bin/mail/pathnames.h
+++ usr/src/usr.bin/mail/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.16 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -28,13 +28,6 @@ define|#
 directive|define
 name|_PATH_TILDE
 value|"/usr/share/misc/mail.tildehelp"
-end_define
-
-begin_define
-define|#
-directive|define
-name|_PATH_MAIL_LOG
-value|"/var/log/maillog"
 end_define
 
 begin_define
