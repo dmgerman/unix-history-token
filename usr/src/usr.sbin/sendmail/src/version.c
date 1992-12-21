508b7aba7f9ac8b50d5480cfe36efd2e731c7f6d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-12-20 19:13:42 -0800

    change random constants to be #defines; getopt-ify main.c
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.142
    
    Former-commit-id: 2f498e25350e09142c9bdfec8de375fd6526d3e2

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	5.141 (Berkeley) %G%"
+literal|"@(#)version.c	5.142 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-5.141"
+literal|"ALPHA-5.142"
 decl_stmt|;
 end_decl_stmt
 
