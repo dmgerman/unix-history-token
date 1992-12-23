986a9ee04f93136eb34995883a1c90a9f3a39b58
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-22 21:39:17 -0800

    err if tcgetattr fails
    SCCS-vsn: lib/libcurses/tty.c 5.4
    
    Former-commit-id: 83033b2e53ff3571b5d5834e44830bdef2551e8b

--- usr/src/lib/libcurses/tty.c
+++ usr/src/lib/libcurses/tty.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)tty.c	5.3 (Berkeley) %G%"
+literal|"@(#)tty.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -103,7 +103,7 @@ argument_list|)
 condition|)
 return|return
 operator|(
-name|OK
+name|ERR
 operator|)
 return|;
 name|GT
