2cf588de5ae361d384db2aaffc061e0a0f6f3a77
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-12-27 17:28:14 -0800

    many MD flags moved here from sys/proc.h and include/pcb.h
    SCCS-vsn: sys/hp300/include/proc.h 7.4
    
    Former-commit-id: 584e140b7b7b541915aedd962d757f78928a7020

--- usr/src/sys/hp300/include/proc.h
+++ usr/src/sys/hp300/include/proc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)proc.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)proc.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -37,6 +37,61 @@ end_define
 
 begin_comment
 comment|/* async trap pending */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MDP_HPUX
+value|0x0002
+end_define
+
+begin_comment
+comment|/* HP-UX process */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MDP_HPUXTRACE
+value|0x0004
+end_define
+
+begin_comment
+comment|/* being traced by HP-UX process */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MDP_HPUXMMAP
+value|0x0008
+end_define
+
+begin_comment
+comment|/* VA space is multiply mapped */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MDP_CCBDATA
+value|0x0010
+end_define
+
+begin_comment
+comment|/* copyback caching of data (68040) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MDP_CCBSTACK
+value|0x0020
+end_define
+
+begin_comment
+comment|/* copyback caching of stack (68040) */
 end_comment
 
 end_unit
