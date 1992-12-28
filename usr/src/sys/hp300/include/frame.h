20b7e9ee9a367524430ed125477d7cd52c538220
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-12-27 17:25:39 -0800

    new stack alignment
    SCCS-vsn: sys/hp300/include/frame.h 7.4
    
    Former-commit-id: 9b5c0da30e14cddbe4ff0f6fc71e242cf6822dc6

--- usr/src/sys/hp300/include/frame.h
+++ usr/src/sys/hp300/include/frame.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: frame.h 1.7 91/03/25$  *  *	@(#)frame.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: frame.h 1.8 92/12/20$  *  *	@(#)frame.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_struct
@@ -12,6 +12,9 @@ name|f_regs
 index|[
 literal|16
 index|]
+decl_stmt|;
+name|short
+name|f_pad
 decl_stmt|;
 name|short
 name|f_stackadj
