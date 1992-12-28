8c8a0fc63b2ab6c33bc95ccf17a05ac52f210887
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-12-27 17:53:41 -0800

    SHPUX has moved
    SCCS-vsn: sys/hp300/hp300/sys_machdep.c 7.11
    
    Former-commit-id: 5ab1e9889ed96da1671f7f1f74e50698fae9e8c2

--- usr/src/sys/hp300/hp300/sys_machdep.c
+++ usr/src/sys/hp300/hp300/sys_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sys_machdep.c	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sys_machdep.c	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -505,9 +505,11 @@ condition|(
 operator|(
 name|curproc
 operator|->
-name|p_flag
+name|p_md
+operator|.
+name|md_flags
 operator|&
-name|SHPUX
+name|MDP_HPUX
 operator|)
 operator|&&
 name|len
