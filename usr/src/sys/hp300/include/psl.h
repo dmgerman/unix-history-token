55f3b26f0adc039812ae4d465fd1acdb936b1f71
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-12-27 17:28:49 -0800

    treat PSL as longword for MBZ bits
    SCCS-vsn: sys/hp300/include/psl.h 7.4
    
    Former-commit-id: 66aee761f6ad1447996823ef76d7e574c4f6c7ea

--- usr/src/sys/hp300/include/psl.h
+++ usr/src/sys/hp300/include/psl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)psl.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)psl.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -236,7 +236,7 @@ begin_define
 define|#
 directive|define
 name|PSL_MBZ
-value|0x58E0
+value|0xFFFF58E0
 end_define
 
 begin_comment
