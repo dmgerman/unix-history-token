9120c49b6baed73a72ce261071b7b5e25e3307fa
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1992-12-27 17:12:59 -0800

    add ITE_CURSORON
    SCCS-vsn: sys/hp/dev/itevar.h 7.4
    
    Former-commit-id: 8aa89c723bb9b88e147571148370d5ee4e3c8201

--- usr/src/sys/hp/dev/itevar.h
+++ usr/src/sys/hp/dev/itevar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: itevar.h 1.14 92/01/21$  *  *	@(#)itevar.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: itevar.h 1.15 92/12/20$  *  *	@(#)itevar.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -285,6 +285,17 @@ end_define
 
 begin_comment
 comment|/* device in use as non-ITE */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ITE_CURSORON
+value|0x40
+end_define
+
+begin_comment
+comment|/* cursor being tracked */
 end_comment
 
 begin_define
