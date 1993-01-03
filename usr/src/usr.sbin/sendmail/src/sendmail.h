6f43bfb7d4ec336db999273fcccd48643f4dc00e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-01-02 21:25:04 -0800

    add definitions for FileMailer and InclMailer
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.3
    
    Former-commit-id: 1e8d675c08126eb6852ce1ef1fce813dfae6a9fc

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.2		%G%"
+literal|"@(#)sendmail.h	6.3		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -832,6 +832,30 @@ end_decl_stmt
 
 begin_comment
 comment|/* ptr to program mailer */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|MAILER
+modifier|*
+name|FileMailer
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* ptr to *file* mailer */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|MAILER
+modifier|*
+name|InclMailer
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* ptr to *include* mailer */
 end_comment
 
 begin_escape
