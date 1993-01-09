384c2e10470bef50711f66fdaf9d74bf73eedebf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-01-09 01:40:16 -0800

    lint
    SCCS-vsn: lib/libc/stdlib/atof.c 5.3
    
    Former-commit-id: 9a202e745f214337d529f2580812c8493fed61f6

--- usr/src/lib/libc/stdlib/atof.c
+++ usr/src/lib/libc/stdlib/atof.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)atof.c	5.2 (Berkeley) %G%"
+literal|"@(#)atof.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -55,6 +55,7 @@ name|atof
 parameter_list|(
 name|ascii
 parameter_list|)
+specifier|const
 name|char
 modifier|*
 name|ascii
@@ -66,11 +67,6 @@ name|strtod
 argument_list|(
 name|ascii
 argument_list|,
-operator|(
-name|char
-operator|*
-operator|*
-operator|)
 name|NULL
 argument_list|)
 operator|)
