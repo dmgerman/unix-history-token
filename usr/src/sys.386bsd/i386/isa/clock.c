405bc08f32d73983a41109e2bb7cd4e3662d1d50
Bruce Evans <bde@runx.oz.au>
1993-01-17 00:00:00 +0000

    CLOCK CALCULATION FIX
    	The patch fixes years >= 2000.  The times were off by 14 sec
    from about 1988 onwards using the 0.0 time database.  I think the 0.1
    database has been hacked to avoid leap seconds.  There are a couple of
    versions of the 0.1 database.  The default one has no leap seconds.  The
    "right" one gives different differences (11 sec?).
    
    AUTHOR:		Bruce Evans (bde@runx.oz.au)
    386BSD-Patchkit: patch00071
    
    Former-commit-id: f0445b3b9d47ab8226db64f3067662a7241a47fd

--- usr/src/sys.386bsd/i386/isa/clock.c
+++ usr/src/sys.386bsd/i386/isa/clock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)clock.c	7.2 (Berkeley) 5/12/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00044  * --------------------         -----   ----------------------  *  * 14 Aug 92	Arne Henrik Juul	Added code for the kernel to  *					allow for DST in the BIOS.  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)clock.c	7.2 (Berkeley) 5/12/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00071  * --------------------         -----   ----------------------  *  * 14 Aug 92	Arne Henrik Juul	Added code in the kernel to  *					allow for DST in the BIOS.  * 17 Jan 93	Bruce Evans		Fixed leap year and second  *					calculations  */
 end_comment
 
 begin_comment
@@ -11,6 +11,12 @@ begin_include
 include|#
 directive|include
 file|"param.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"systm.h"
 end_include
 
 begin_include
@@ -249,17 +255,11 @@ name|ret
 operator|=
 literal|0
 expr_stmt|;
-name|y
-operator|=
-name|y
-operator|-
-literal|70
-expr_stmt|;
 for|for
 control|(
 name|i
 operator|=
-literal|0
+literal|1970
 init|;
 name|i
 operator|<
@@ -526,6 +526,18 @@ argument_list|(
 name|RTC_YEAR
 argument_list|)
 argument_list|)
+operator|+
+literal|1900
+expr_stmt|;
+if|if
+condition|(
+name|sec
+operator|<
+literal|1970
+condition|)
+name|sec
+operator|+=
+literal|100
 expr_stmt|;
 name|leap
 operator|=
@@ -537,7 +549,7 @@ literal|4
 operator|)
 expr_stmt|;
 name|sec
-operator|+=
+operator|=
 name|ytos
 argument_list|(
 name|sec
@@ -640,15 +652,6 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 comment|/* seconds */
-name|sec
-operator|-=
-literal|24
-operator|*
-literal|60
-operator|*
-literal|60
-expr_stmt|;
-comment|/* XXX why ??? */
 comment|/* XXX off by one? Need to calculate DST on SUNDAY */
 comment|/* Perhaps we should have the RTC hold GMT time to save */
 comment|/* us the bother of converting. */
