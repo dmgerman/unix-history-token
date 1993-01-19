ccab73f26a82efd23fe58a1ee1a5ab3576fd6b44
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-01-19 03:11:58 -0800

    add some member to struct KernInter
    SCCS-vsn: sys/luna68k/include/stinger.h 7.3
    
    Former-commit-id: b142b2768bb6af1d6a0c0ce27111dafbaace1f81

--- usr/src/sys/luna68k/include/stinger.h
+++ usr/src/sys/luna68k/include/stinger.h
@@ -1,23 +1,11 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)stinger.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)stinger.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * stinger.h -- Stinger Kernel Interface Definitions  * by A.Fujita, MAR-14-1992  *  * This file should be remade.  */
+comment|/*  * stinger.h -- Stinger Kernel Interface Definitions  *   remade by A.Fujita, JAN-12-1993  */
 end_comment
-
-begin_include
-include|#
-directive|include
-file|<machine/rdvar.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<machine/memory.h>
-end_include
 
 begin_struct
 struct|struct
@@ -26,26 +14,34 @@ block|{
 name|caddr_t
 name|maxaddr
 decl_stmt|;
-name|struct
-name|prgmem
-name|prg
+name|u_int
+name|dipsw
 decl_stmt|;
 name|int
-name|argc
-decl_stmt|;
-name|char
-modifier|*
-modifier|*
-name|argv
-decl_stmt|;
-name|struct
-name|rd_partition
-modifier|*
-name|rd
+name|plane
 decl_stmt|;
 block|}
 struct|;
 end_struct
+
+begin_decl_stmt
+specifier|extern
+name|struct
+name|KernInter
+name|KernInter
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|KIFF_DIPSW_NOBM
+value|0x0002
+end_define
+
+begin_comment
+comment|/* not use bitmap display as console */
+end_comment
 
 end_unit
 
