9e8348b540ce5a65b50be00a0b615647947afc36
Kazumasa Utashiro <utashiro@ucbvax.Berkeley.EDU>
1993-01-20 08:28:08 -0800

    Fixed wrong headers.
    SCCS-vsn: sys/news3400/if/if_news.c 7.4
    
    Former-commit-id: 11d4113caa79ac2beb6a1f29b948a0e46ff735b7

--- usr/src/sys/news3400/if/if_news.c
+++ usr/src/sys/news3400/if/if_news.c
@@ -1,12 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  * from: $Hdr: if_news.c,v 4.300 91/06/09 06:26:01 root Rel41 $ SONY  *  *	@(#)if_news.c	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.  *  * %sccs.include.redist.c%  *  * from: $Hdr: if_news.c,v 4.300 91/06/09 06:26:01 root Rel41 $ SONY  *  *	@(#)if_news.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
 include|#
 directive|include
-file|<sys/types.h" #include<machine/fix_machine_type.h>
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/fix_machine_type.h>
 end_include
 
 begin_include
@@ -66,7 +72,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<news3400/net/if.h>
+file|<net/if.h>
 end_include
 
 begin_include
