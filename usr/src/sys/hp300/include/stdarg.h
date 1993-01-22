55df97d7bd8bdd65addc12a513cdefad0c25a1ef
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-01-21 23:28:22 -0800

    protect against multiple inclusion
    SCCS-vsn: sys/hp300/include/stdarg.h 7.4
    SCCS-vsn: sys/pmax/include/stdarg.h 7.3
    
    Former-commit-id: 929b472e208c9e698188fcfb5ba6adbeafa1e5bf

--- usr/src/sys/hp300/include/stdarg.h
+++ usr/src/sys/hp300/include/stdarg.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	7.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	7.4 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_STDARG_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_STDARG_H_
+end_define
 
 begin_typedef
 typedef|typedef
@@ -85,6 +97,15 @@ parameter_list|(
 name|ap
 parameter_list|)
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_STDARG_H_ */
+end_comment
 
 end_unit
 
