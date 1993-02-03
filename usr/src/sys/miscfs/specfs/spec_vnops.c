bfe88256c3ee7e169cd44c3086d1680132a42c2d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-02-03 05:02:39 -0800

    vinvalbuf and getblk now take parameters to interrupt
    SCCS-vsn: sys/miscfs/specfs/spec_vnops.c 7.52
    
    Former-commit-id: 3ed78d0fc23ce7e0adf9332218f8dcf73f5307e4

--- usr/src/sys/miscfs/specfs/spec_vnops.c
+++ usr/src/sys/miscfs/specfs/spec_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)spec_vnops.c	7.51 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)spec_vnops.c	7.52 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1634,6 +1634,10 @@ argument_list|,
 name|bn
 argument_list|,
 name|bsize
+argument_list|,
+literal|0
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 else|else
@@ -2556,6 +2560,10 @@ argument_list|,
 name|ap
 operator|->
 name|a_p
+argument_list|,
+literal|0
+argument_list|,
+literal|0
 argument_list|)
 condition|)
 return|return
