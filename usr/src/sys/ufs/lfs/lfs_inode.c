bbd7cea0ef10e60196dfacdc035932812ee6496b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-02-03 05:06:39 -0800

    vinvalbuf takes parameter to indicate interrupt
    SCCS-vsn: sys/ufs/lfs/lfs_inode.c 7.84
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.90
    
    Former-commit-id: 8a271ebb7b7abc36699de957c3f4821ac0157ac3

--- usr/src/sys/ufs/lfs/lfs_inode.c
+++ usr/src/sys/ufs/lfs/lfs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_inode.c	7.83 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_inode.c	7.84 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1557,6 +1557,10 @@ argument_list|,
 name|ap
 operator|->
 name|a_p
+argument_list|,
+literal|0
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 name|e2
