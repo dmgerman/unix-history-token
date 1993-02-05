385b3ec91699a2c0ed564efadeded85f467ad79a
Dave A. Borman <dab@ucbvax.Berkeley.EDU>
1993-02-04 21:26:53 -0800

    Fix message for "skiprc" so that the message that gets printed when it is changed is correct. (It used to print the exact opposite of what was really being done...)
    SCCS-vsn: usr.bin/telnet/commands.c 5.9
    
    Former-commit-id: f7176e8ded501cef1a4362b3832582a6f26c8668

--- usr/src/usr.bin/telnet/commands.c
+++ usr/src/usr.bin/telnet/commands.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)commands.c	5.8 (Berkeley) %G%"
+literal|"@(#)commands.c	5.9 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3221,7 +3221,7 @@ block|,
 operator|&
 name|skiprc
 block|,
-literal|"read ~/.telnetrc file"
+literal|"skip reading of ~/.telnetrc file"
 block|}
 block|,
 block|{
