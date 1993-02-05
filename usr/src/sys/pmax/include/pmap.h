97ce454f56fae53e30d67008bdc391a9ec49d111
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-02-05 03:37:45 -0800

    added more kernel virtual address space
    SCCS-vsn: sys/pmax/include/pmap.h 7.6
    
    Former-commit-id: e32c46b3d30f91470fa32c8950c21f11112e9631

--- usr/src/sys/pmax/include/pmap.h
+++ usr/src/sys/pmax/include/pmap.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1987 Carnegie-Mellon University  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)pmap.h	7.5 (Berkeley) %G%  */
+comment|/*   * Copyright (c) 1987 Carnegie-Mellon University  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)pmap.h	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -30,7 +30,7 @@ begin_define
 define|#
 directive|define
 name|PMAP_HASH_KPAGES
-value|4
+value|5
 end_define
 
 begin_define
