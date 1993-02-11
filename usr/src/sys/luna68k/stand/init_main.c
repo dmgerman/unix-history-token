8b0e767cb7fd091a10f48eca3ce8f1dafec961f7
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-02-10 23:50:53 -0800

    local version update
    SCCS-vsn: sys/luna68k/stand/init_main.c 7.6
    
    Former-commit-id: e42601d9e7c8f7b4f1855676b5564e6196888a27

--- usr/src/sys/luna68k/stand/init_main.c
+++ usr/src/sys/luna68k/stand/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)init_main.c	7.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)init_main.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -67,7 +67,7 @@ begin_define
 define|#
 directive|define
 name|VERS_LOCAL
-value|"Phase-29"
+value|"Phase-30"
 end_define
 
 begin_decl_stmt
