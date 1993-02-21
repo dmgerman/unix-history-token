a1f72429db98cfc0a96acbca6f65ff0a894f2238
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-20 22:54:54 -0800

    optimization: don't expand aliases if you are in queue-only mode
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.13
    
    Former-commit-id: 45b5096e18b41e3c5b48dec8420fdcc50d99c9d8

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	6.12 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	6.13 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	6.12 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	6.13 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -1202,6 +1202,19 @@ expr_stmt|;
 name|LogUsrErrs
 operator|=
 name|TRUE
+expr_stmt|;
+comment|/* optimization -- if queueing, don't expand aliases */
+if|if
+condition|(
+name|SendMode
+operator|==
+name|SM_QUEUE
+condition|)
+name|e
+operator|->
+name|e_flags
+operator||=
+name|EF_VRFYONLY
 expr_stmt|;
 name|p
 operator|=
