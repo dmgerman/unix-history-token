fec19ba99c092db0a1983d18577f3c1f39508b23
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-20 23:14:02 -0800

    close cached connections on forks and mailer execs; fix bug in QSELFREF code; add option p (privacy flags) and b (minimum blocks free); VRFY just verifies, EXPN really expands
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.25
    
    Former-commit-id: 55d5227f75f4d15a306caa53d4279e5fca2e5d67

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.24 (Berkeley) %G%"
+literal|"@(#)version.c	6.25 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.24"
+literal|"ALPHA-6.25"
 decl_stmt|;
 end_decl_stmt
 
