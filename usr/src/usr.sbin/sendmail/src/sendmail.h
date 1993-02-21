32708e6a045545876eb5d8196e703180d29b9416
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-20 22:33:34 -0800

    add EF_VRFYONLY flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.15
    
    Former-commit-id: 06e3abc74d67e4e16705a96ede895d261828924d

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.14		%G%"
+literal|"@(#)sendmail.h	6.15		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1395,6 +1395,17 @@ end_define
 
 begin_comment
 comment|/* this message is being forwarded */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|EF_VRFYONLY
+value|001000
+end_define
+
+begin_comment
+comment|/* verify only (don't expand aliases) */
 end_comment
 
 begin_decl_stmt
