94ebe38400b425bf99540b2debdb2c7f78a092d5
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-02-21 21:57:34 -0800

    define MCOUNT properly. Still need to define kernel entry & exit.
    SCCS-vsn: sys/pmax/include/profile.h 7.2
    
    Former-commit-id: b8ad7231e7863156fa135b568765b538df39a760

--- usr/src/sys/pmax/include/profile.h
+++ usr/src/sys/pmax/include/profile.h
@@ -1,19 +1,21 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)profile.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)profile.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|_MCOUNT_DECL
-value|static inline void _mcount
+value|static void _mcount
 end_define
 
 begin_define
 define|#
 directive|define
 name|MCOUNT
+define|\
+value|asm(".globl mcount"); \ 	asm("mcount:"); \ 	asm(".set noreorder"); \ 	asm(".set noat"); \ 	asm("sw $31,4($29);"); \ 	asm("jal _mcount"); \ 	asm("sw $1,0($29);"); \ 	asm("lw $31,4($29)"); \ 	asm("lw $1,0($29)"); \ 	asm("addu $29,$29,8"); \ 	asm("j $31"); \ 	asm("move $31,$1"); \ 	asm(".set reorder"); \ 	asm(".set at");
 end_define
 
 begin_ifdef
