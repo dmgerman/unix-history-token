be7480a45747d75c1b69fe0738c70c80eace6d31
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-21 17:57:27 -0800

    don't "lose" queue runs; completely reset state on RSET; send errors from <> and don't reply to such messages; explicitly check for [a.b.c.d] syntax referencing yourself; generalize read timeouts
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.26
    
    Former-commit-id: 2971dcc40000212adde339ebc145041d3549778d

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.25 (Berkeley) %G%"
+literal|"@(#)version.c	6.26 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.25"
+literal|"ALPHA-6.26"
 decl_stmt|;
 end_decl_stmt
 
