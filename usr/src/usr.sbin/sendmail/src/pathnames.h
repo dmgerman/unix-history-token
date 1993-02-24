eec3e4cb1913f1c78d88e3b7a323af8c26fd75a5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-23 18:09:37 -0800

    allow compile time specification of sendmail.cf and .fc too
    SCCS-vsn: usr.sbin/sendmail/src/pathnames.h 6.5
    
    Former-commit-id: c017d9f55b1cc1064224e618617c1acb169e70ba

--- usr/src/usr.sbin/sendmail/src/pathnames.h
+++ usr/src/usr.sbin/sendmail/src/pathnames.h
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	6.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	6.5 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_PATH_SENDMAILCF
+end_ifndef
 
 begin_define
 define|#
@@ -10,12 +16,28 @@ name|_PATH_SENDMAILCF
 value|"/etc/sendmail.cf"
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_PATH_SENDMAILFC
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|_PATH_SENDMAILFC
 value|"/etc/sendmail.fc"
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
