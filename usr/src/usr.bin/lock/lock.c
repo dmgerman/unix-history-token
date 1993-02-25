70d58ebc33624bfac231cfae47bb7b70e02e6270
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-02-25 00:48:37 -0800

    add contrib notice; Bob Toxen
    SCCS-vsn: usr.bin/lock/lock.c 5.14
    
    Former-commit-id: 7fe07cf3a39d77c1d5d52fd4ca23d438aec87261

--- usr/src/usr.bin/lock/lock.c
+++ usr/src/usr.bin/lock/lock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1987 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+comment|/*  * Copyright (c) 1980, 1987 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Bob Toxen.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)lock.c	5.13 (Berkeley) %G%"
+literal|"@(#)lock.c	5.14 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
