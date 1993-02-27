4c306cd1bf830c1d6ab6695a16cb760d797c6d2e
Charles Hannum <mycroft@ai.mit.edu>
1993-02-27 00:00:00 +0000

    BETTER BYTE SWAPPING FOR GCC1 & GCC2
    The previous byte-swapping macros were rather inefficient, and didn't
    take full advantage of the i486 architecture, when available.  It
    also didn't work well with gcc2, which is where we're eventually heading.
    
    AUTHOR:	Charles Hannum
    386BSD-Patchkit: patch00093
    
    Former-commit-id: 20e512ee8d1e96673f2f69ddf3e6991f2b05924d

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)db.h	5.10 (Berkeley) 4/2/91  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)db.h	5.10 (Berkeley) 4/2/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00093  * --------------------         -----   ----------------------  *  * 27 Feb 93    Charles Hannum		Better byte-swapping macros for  *					i386/i486.  */
 end_comment
 
 begin_ifndef
@@ -14,6 +14,12 @@ define|#
 directive|define
 name|_DB_H_
 end_define
+
+begin_include
+include|#
+directive|include
+file|<machine/endian.h>
+end_include
 
 begin_include
 include|#
@@ -495,9 +501,9 @@ define|#
 directive|define
 name|BLSWAP
 parameter_list|(
-name|a
+name|X
 parameter_list|)
-value|{ \ 	u_long _tmp = a; \ 	((char *)&a)[0] = ((char *)&_tmp)[3]; \ 	((char *)&a)[1] = ((char *)&_tmp)[2]; \ 	((char *)&a)[2] = ((char *)&_tmp)[1]; \ 	((char *)&a)[3] = ((char *)&_tmp)[0]; \ }
+value|{(X) = __byte_swap_long(X);}
 end_define
 
 begin_define
@@ -505,11 +511,11 @@ define|#
 directive|define
 name|BLSWAP_COPY
 parameter_list|(
-name|a
+name|X
 parameter_list|,
-name|b
+name|Y
 parameter_list|)
-value|{ \ 	((char *)&(b))[0] = ((char *)&(a))[3]; \ 	((char *)&(b))[1] = ((char *)&(a))[2]; \ 	((char *)&(b))[2] = ((char *)&(a))[1]; \ 	((char *)&(b))[3] = ((char *)&(a))[0]; \ }
+value|{(Y) = __byte_swap_long(X);}
 end_define
 
 begin_comment
@@ -521,9 +527,9 @@ define|#
 directive|define
 name|BSSWAP
 parameter_list|(
-name|a
+name|X
 parameter_list|)
-value|{ \ 	u_short _tmp = a; \ 	((char *)&a)[0] = ((char *)&_tmp)[1]; \ 	((char *)&a)[1] = ((char *)&_tmp)[0]; \ }
+value|{(X) = __byte_swap_word(X);}
 end_define
 
 begin_define
@@ -531,11 +537,11 @@ define|#
 directive|define
 name|BSSWAP_COPY
 parameter_list|(
-name|a
+name|X
 parameter_list|,
-name|b
+name|Y
 parameter_list|)
-value|{ \ 	((char *)&(b))[0] = ((char *)&(a))[1]; \ 	((char *)&(b))[1] = ((char *)&(a))[0]; \ }
+value|{(Y) = __byte_swap_word(X);}
 end_define
 
 begin_decl_stmt
--- usr/src/sys.386bsd/i386/include/endian.h
+++ usr/src/sys.386bsd/i386/include/endian.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)endian.h	7.8 (Berkeley) 4/3/91  */
+comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)endian.h	7.8 (Berkeley) 4/3/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00093  * --------------------         -----   ----------------------  *  * 27 Feb 93    Charles Hannum		Better byte-swapping macros for  *					i386/i486.  */
 end_comment
 
 begin_comment
@@ -64,66 +64,86 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-name|__BEGIN_DECLS
-name|unsigned
-name|long
-name|htonl
-name|__P
-argument_list|(
-operator|(
-name|unsigned
-name|long
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+begin_define
+define|#
+directive|define
+name|__word_swap_long
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|({ register u_long X = (x); \    asm ("rorl $16, %1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
+end_define
 
-begin_decl_stmt
-name|unsigned
-name|short
-name|htons
-name|__P
-argument_list|(
-operator|(
-name|unsigned
-name|short
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+begin_if
+if|#
+directive|if
+name|__GNUC__
+operator|>=
+literal|2
+end_if
 
-begin_decl_stmt
-name|unsigned
-name|long
-name|ntohl
-name|__P
-argument_list|(
-operator|(
-name|unsigned
-name|long
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+begin_define
+define|#
+directive|define
+name|__byte_swap_long
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|({ register u_long X = (x); \    asm ("xchgb %h1, %b1\n\trorl $16, %1\n\txchgb %h1, %b1" \ 	: "=q" (X) \ 	: "0" (X)); \    X; })
+end_define
 
-begin_decl_stmt
-name|unsigned
-name|short
-name|ntohs
-name|__P
-argument_list|(
-operator|(
-name|unsigned
-name|short
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+begin_define
+define|#
+directive|define
+name|__byte_swap_word
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|({ register u_short X = (x); \    asm ("xchgb %h1, %b1" \ 	: "=q" (X) \ 	: "0" (X)); \    X; })
+end_define
 
-begin_macro
-name|__END_DECLS
-end_macro
+begin_else
+else|#
+directive|else
+end_else
+
+begin_comment
+comment|/* __GNUC__>= 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__byte_swap_long
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|({ register u_long X = (x); \    asm ("rorw $8, %w1\n\trorl $16, %1\n\trorw $8, %w1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
+end_define
+
+begin_define
+define|#
+directive|define
+name|__byte_swap_word
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|({ register u_short X = (x); \    asm ("rorw $8, %w1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __GNUC__>= 2 */
+end_comment
 
 begin_comment
 comment|/*  * Macros for network/external number representation conversion.  */
@@ -227,6 +247,34 @@ begin_else
 else|#
 directive|else
 end_else
+
+begin_define
+define|#
+directive|define
+name|ntohl
+value|__byte_swap_long
+end_define
+
+begin_define
+define|#
+directive|define
+name|ntohs
+value|__byte_swap_word
+end_define
+
+begin_define
+define|#
+directive|define
+name|htonl
+value|__byte_swap_long
+end_define
+
+begin_define
+define|#
+directive|define
+name|htons
+value|__byte_swap_word
+end_define
 
 begin_define
 define|#
