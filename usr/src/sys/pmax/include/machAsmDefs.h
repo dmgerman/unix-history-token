700adb96ca3c2a38a2e308d253aa690d5a2643f6
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-02-27 03:11:46 -0800

    fix profiling
    SCCS-vsn: sys/pmax/include/profile.h 7.3
    SCCS-vsn: sys/pmax/include/machAsmDefs.h 7.4
    
    Former-commit-id: a63af8801405d2f430e1ce1eccc5509e983bdc0c

--- usr/src/sys/pmax/include/machAsmDefs.h
+++ usr/src/sys/pmax/include/machAsmDefs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)machAsmDefs.h	7.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)machAsmDefs.h	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -39,23 +39,8 @@ begin_define
 define|#
 directive|define
 name|MCOUNT
-value|.set noreorder; \ 		.set noat; \ 		move $1,$31; \ 		jal _mount; \ 		subu sp,sp,8;
+value|.set noreorder; \ 		.set noat; \ 		move $1,$31; \ 		jal _mcount; \ 		subu sp,sp,8; \ 		.set reorder; \ 		.set at;
 end_define
-
-begin_expr_stmt
-operator|.
-name|set
-name|reorder
-expr_stmt|;
-end_expr_stmt
-
-begin_expr_stmt
-unit|\
-operator|.
-name|set
-name|at
-expr_stmt|;
-end_expr_stmt
 
 begin_else
 else|#
@@ -86,7 +71,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|.globl x; \ 	.ent x, 0; \ x: ; \ 	.frame sp, 0, ra \ 	MCOUNT
+value|.globl x; \ 	.ent x, 0; \ x: ; \ 	.frame sp, 0, ra; \ 	MCOUNT
 end_define
 
 begin_comment
@@ -135,7 +120,7 @@ parameter_list|,
 name|retpc
 parameter_list|)
 define|\
-value|.globl x; \ 	.ent x, 0; \ x: ; \ 	.frame sp, fsize, retpc \ 	MCOUNT
+value|.globl x; \ 	.ent x, 0; \ x: ; \ 	.frame sp, fsize, retpc; \ 	MCOUNT
 end_define
 
 begin_comment
--- usr/src/sys/pmax/include/profile.h
+++ usr/src/sys/pmax/include/profile.h
@@ -1,13 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)profile.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)profile.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|_MCOUNT_DECL
-value|static void _mcount
+value|static void __mcount
 end_define
 
 begin_define
@@ -15,7 +15,7 @@ define|#
 directive|define
 name|MCOUNT
 define|\
-value|asm(".globl mcount"); \ 	asm("mcount:"); \ 	asm(".set noreorder"); \ 	asm(".set noat"); \ 	asm("sw $31,4($29);"); \ 	asm("jal _mcount"); \ 	asm("sw $1,0($29);"); \ 	asm("lw $31,4($29)"); \ 	asm("lw $1,0($29)"); \ 	asm("addu $29,$29,8"); \ 	asm("j $31"); \ 	asm("move $31,$1"); \ 	asm(".set reorder"); \ 	asm(".set at");
+value|asm(".globl _mcount;" \ 	"_mcount:;" \ 	".set noreorder;" \ 	".set noat;" \ 	"sw $4,8($29);" \ 	"sw $5,12($29);" \ 	"sw $6,16($29);" \ 	"sw $7,20($29);" \ 	"sw $1,0($29);" \ 	"sw $31,4($29);" \ 	"move $5,$31;" \ 	"jal __mcount;" \ 	"move $4,$1;" \ 	"lw $4,8($29);" \ 	"lw $5,12($29);" \ 	"lw $6,16($29);" \ 	"lw $7,20($29);" \ 	"lw $31,4($29);" \ 	"lw $1,0($29);" \ 	"addu $29,$29,8;" \ 	"j $31;" \ 	"move $31,$1;" \ 	".set reorder;" \ 	".set at");
 end_define
 
 begin_ifdef
