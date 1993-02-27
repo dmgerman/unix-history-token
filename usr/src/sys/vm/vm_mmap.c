0b12815b3663d8cbba58588f2e92d4eb721bc023
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-02-27 00:50:31 -0800

    check for anonymous objects in the cache (4.4BSD-alpha/sys/4)
    SCCS-vsn: sys/vm/vm_mmap.c 7.22
    
    Former-commit-id: 122341f0f0e03c4edd25d317bca0667d6d961a54

--- usr/src/sys/vm/vm_mmap.c
+++ usr/src/sys/vm/vm_mmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	7.21 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	7.22 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2304,6 +2304,20 @@ goto|goto
 name|out
 goto|;
 block|}
+comment|/* 		 * The object of unnamed anonymous regions was just created 		 * find it for pager_cache. 		 */
+if|if
+condition|(
+name|handle
+operator|==
+name|NULL
+condition|)
+name|object
+operator|=
+name|vm_object_lookup
+argument_list|(
+name|pager
+argument_list|)
+expr_stmt|;
 comment|/* 		 * Don't cache anonymous objects. 		 * Loses the reference gained by vm_pager_allocate. 		 */
 operator|(
 name|void
