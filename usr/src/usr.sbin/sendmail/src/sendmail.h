ee9e51579d3453e5ce754f1b72b02a8e4099f44e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-01 14:47:34 -0800

    clean up header files
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.26
    
    Former-commit-id: 9c809c9a1070b867096ce455cde194e5bf5ce71e

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.25 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.26 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.25		%G%"
+literal|"@(#)sendmail.h	6.26		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -65,6 +65,12 @@ end_endif
 begin_comment
 comment|/* _DEFINE */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<unistd.h>
+end_include
 
 begin_include
 include|#
@@ -118,6 +124,12 @@ begin_include
 include|#
 directive|include
 file|<errno.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
 end_include
 
 begin_include
