7c5e96a151c59595aefbf42363c0b067508469d7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-01 04:00:13 -0800

    fix another owner- bug; improve logging; run all programs without ctladdrs as daemon; add -qXstring to limit queue run; hooks for ESMTP ; improve syntax checking in readcf
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.31
    
    Former-commit-id: f6be0c7f695190412070b758511c8a11243e9741

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.30 (Berkeley) %G%"
+literal|"@(#)version.c	6.31 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.30"
+literal|"ALPHA-6.31"
 decl_stmt|;
 end_decl_stmt
 
