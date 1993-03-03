63d841e9a952dcf88619c9fb47087780d3a9e1d5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-02 22:27:53 -0800

    fix some uninitialized variables; <unistd.h> everywhere; logging changes; accept host name inside [...] to avoid MX lookup
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.32
    
    Former-commit-id: e7a12f890723643f57bd9a1e70ed620fbdc5780a

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.31 (Berkeley) %G%"
+literal|"@(#)version.c	6.32 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.31"
+literal|"ALPHA-6.32"
 decl_stmt|;
 end_decl_stmt
 
