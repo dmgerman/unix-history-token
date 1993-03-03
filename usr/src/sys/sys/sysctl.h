49c1609f683e816b09cf989993247857bd789d51
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-03-03 02:21:19 -0800

    need proc parameter to be passed through
    SCCS-vsn: sys/sys/sysctl.h 7.15
    
    Former-commit-id: 1059b60581247c774018ee773354b385461e701c

--- usr/src/sys/sys/sysctl.h
+++ usr/src/sys/sys/sysctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)sysctl.h	7.14 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)sysctl.h	7.15 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -551,6 +551,10 @@ name|void
 operator|*
 operator|,
 name|u_int
+operator|,
+expr|struct
+name|proc
+operator|*
 operator|)
 argument_list|)
 expr_stmt|;
