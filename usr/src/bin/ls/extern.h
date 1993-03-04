ad6bdb233ffbdf34f36da337f1540edca0c6553b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-03-04 04:36:44 -0800

    add flags_to_string declaration
    SCCS-vsn: bin/ls/extern.h 5.4
    
    Former-commit-id: 3789616a78d05f315b6510964bda52df21efae2f

--- usr/src/bin/ls/extern.h
+++ usr/src/bin/ls/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_decl_stmt
@@ -160,6 +160,22 @@ name|char
 operator|*
 operator|,
 operator|...
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|flags_to_string
+name|__P
+argument_list|(
+operator|(
+name|u_long
+operator|,
+name|char
+operator|*
 operator|)
 argument_list|)
 decl_stmt|;
