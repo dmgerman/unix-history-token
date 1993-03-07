32743d5c725a3a0528bb892c98630d64c192a351
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-03-07 07:42:02 -0800

    stop if we reach out of band mark while peeking; otherwise we will go into an infinite loop copying 0 bytes each time.  (From Tom Truscott <trt@cs.duke.edu>)
    SCCS-vsn: sys/kern/uipc_socket.c 7.39
    
    Former-commit-id: 7851abd57084891dbd0e09939f76484d1f3b4589

--- usr/src/sys/kern/uipc_socket.c
+++ usr/src/sys/kern/uipc_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)uipc_socket.c	7.38 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)uipc_socket.c	7.39 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3776,10 +3776,21 @@ break|break;
 block|}
 block|}
 else|else
+block|{
 name|offset
 operator|+=
 name|len
 expr_stmt|;
+if|if
+condition|(
+name|offset
+operator|==
+name|so
+operator|->
+name|so_oobmark
+condition|)
+break|break;
+block|}
 block|}
 if|if
 condition|(
