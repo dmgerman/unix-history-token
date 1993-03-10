9e7f90ab907721372cda2b0f449307bb9b75799e
Herb Peyerl <hpeyerl@novatel.cuc.ab.ca>
1993-03-10 00:00:00 +0000

    ALLOW SELECTION OF 3COM 3C503 AUI/BNC FROM IFCONFIG
    Herb Peyerl:
    	There have been questions about the configuration of the AUI/BNC
    connectors on the 3Com 3c503 card.  The way the selection is made is to
    check for IFF_LLC0 being set in the "ifnet" struct.  If set, the driver
    uses AUI, else BNC.  The ifconfig program now allows the following syntax:
    
    ifconfig ec0 inet $hostname aui
    
    ifconfig ec0 inet $hostname bnc
    
    Default with no options yields BNC operation.
    
    Rodney Grimes:
    	Added llc[0-2] and -llc[0-2] and MORE OUTPUT BITS TO IFCONFIG
    
    AUTHOR:		Herb Peyerl (hpeyerl@novatel.cuc.ab.ca
    AUTHOR:		Rodney W. Grimes (rgrimes@agora.rain.com)
    386BSD-Patchkit: patch00102
    
    Former-commit-id: 8f5b48828984de2ff6fc00caaa07512514e4feb2

--- usr/src/sbin/ifconfig/ifconfig.c
+++ usr/src/sbin/ifconfig/ifconfig.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00102  * --------------------         -----   ----------------------  *  * 06 Sep 92	Herb Peyerl		Added "aui"/"bnc" options to ifconfig  *					for 3COM 3C503 port selection  * 10 Mar 93	Rodney W. Grimes	Made the aui/bnc more general, you now  *					also have llc[0-2] and -llc[0-2].  *					Added the rest of the output flag bits.  */
 end_comment
 
 begin_ifndef
@@ -442,6 +442,76 @@ literal|"-debug"
 block|,
 operator|-
 name|IFF_DEBUG
+block|,
+name|setifflags
+block|}
+block|,
+block|{
+literal|"aui"
+block|,
+name|IFF_LLC0
+block|,
+name|setifflags
+block|}
+block|,
+comment|/* 06 Sep 92*/
+block|{
+literal|"bnc"
+block|,
+operator|-
+name|IFF_LLC0
+block|,
+name|setifflags
+block|}
+block|,
+block|{
+literal|"llc0"
+block|,
+name|IFF_LLC0
+block|,
+name|setifflags
+block|}
+block|,
+comment|/* 10 Mar 93 */
+block|{
+literal|"-llc0"
+block|,
+operator|-
+name|IFF_LLC0
+block|,
+name|setifflags
+block|}
+block|,
+block|{
+literal|"llc1"
+block|,
+name|IFF_LLC1
+block|,
+name|setifflags
+block|}
+block|,
+block|{
+literal|"-llc1"
+block|,
+operator|-
+name|IFF_LLC1
+block|,
+name|setifflags
+block|}
+block|,
+block|{
+literal|"llc2"
+block|,
+name|IFF_LLC2
+block|,
+name|setifflags
+block|}
+block|,
+block|{
+literal|"-llc2"
+block|,
+operator|-
+name|IFF_LLC2
 block|,
 name|setifflags
 block|}
@@ -1941,7 +2011,7 @@ define|#
 directive|define
 name|IFFBITS
 define|\
-value|"\020\1UP\2BROADCAST\3DEBUG\4LOOPBACK\5POINTOPOINT\6NOTRAILERS\7RUNNING\10NOARP\ "
+value|"\020\1UP\2BROADCAST\3DEBUG\4LOOPBACK\5POINTOPOINT\6NOTRAILERS\7RUNNING\10NOARP\ \11PROMISC\12ALLMULTI\13OACTIVE\14SIMPLEX\15LLC0\16LLC1\17LLC2\ "
 end_define
 
 begin_comment
