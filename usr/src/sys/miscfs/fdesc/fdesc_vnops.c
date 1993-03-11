34f63da4a0481e27ab5911dcfbc1257b030290ef
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1993-03-11 15:44:53 -0800

    ignore setattr on sockets, instead of reporting an error.
    SCCS-vsn: sys/miscfs/fdesc/fdesc_vnops.c 7.3
    
    Former-commit-id: d080cab8b062657e053aacd5e7a0db11178dd965

--- usr/src/sys/miscfs/fdesc/fdesc_vnops.c
+++ usr/src/sys/miscfs/fdesc/fdesc_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)fdesc_vnops.c	7.2 (Berkeley) %G%  *  * $Id: fdesc_vnops.c,v 1.7 1992/05/30 10:05:34 jsp Exp jsp $  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)fdesc_vnops.c	7.3 (Berkeley) %G%  *  * $Id: fdesc_vnops.c,v 1.7 1992/05/30 10:05:34 jsp Exp jsp $  */
 end_comment
 
 begin_comment
@@ -1129,7 +1129,7 @@ name|DTYPE_SOCKET
 case|:
 name|error
 operator|=
-name|EOPNOTSUPP
+literal|0
 expr_stmt|;
 break|break;
 default|default:
