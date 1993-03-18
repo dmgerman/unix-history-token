0567389aee13333a4721e310e086a14e4f671064
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-17 18:36:28 -0800

    early-bind headers; minor logging changes; handle temp errors better; HPUX/SystemV portability enhancements
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.38
    
    Former-commit-id: 3535fac53af3faf5c5d238b26ddee6443802c1d3

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.37 (Berkeley) %G%"
+literal|"@(#)version.c	6.38 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.37"
+literal|"ALPHA-6.38"
 decl_stmt|;
 end_decl_stmt
 
