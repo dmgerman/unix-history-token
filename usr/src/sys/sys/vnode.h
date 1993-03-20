5f9ecb9e2427e128712a997ec129b2454541d4c0
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-03-19 22:34:26 -0800

    add another long to vattr struct to keep it quad aligned
    SCCS-vsn: sys/sys/vnode.h 7.68
    
    Former-commit-id: 9e3d14ebc5c3002b0280143fe0097846dc02ee43

--- usr/src/sys/sys/vnode.h
+++ usr/src/sys/sys/vnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	7.67 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	7.68 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -463,6 +463,10 @@ name|u_int
 name|va_vaflags
 decl_stmt|;
 comment|/* operations flags, see below */
+name|long
+name|va_spare
+decl_stmt|;
+comment|/* remain quad aligned */
 block|}
 struct|;
 end_struct
