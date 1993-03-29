fe9ce44d7d0855dd8489f45a74d7c8cc2a307e0b
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-03-29 02:10:57 -0800

    lint
    SCCS-vsn: sys/kern/vfs_syscalls.c 7.105
    
    Former-commit-id: 1e016ba5e65654ced9d5ff5991558dfd9a1f49c9

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	7.104 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	7.105 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -8030,8 +8030,6 @@ name|vattr
 name|vattr
 decl_stmt|;
 name|int
-name|s
-decl_stmt|,
 name|error
 decl_stmt|;
 name|struct
