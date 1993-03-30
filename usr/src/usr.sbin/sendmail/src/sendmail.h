8b75609a0bb286f77ca29feeb665994916b7d918
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-29 18:49:22 -0800

    add FallBackMX
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.39
    
    Former-commit-id: 3017acd609931c91df53b506046220368ed4130b

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.38 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.39 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.38		%G%"
+literal|"@(#)sendmail.h	6.39		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3801,6 +3801,18 @@ end_decl_stmt
 
 begin_comment
 comment|/* limit queue runs to this id */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|char
+modifier|*
+name|FallBackMX
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* fall back MX host */
 end_comment
 
 begin_comment
