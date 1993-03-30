872d4c80717fc64c9520703827c2d08b83bdf2f4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-30 00:23:46 -0800

    give a nicer message on message acceptance
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.32
    
    Former-commit-id: 5534727619d65b2032453f831653a42274bf68bc

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	6.31 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	6.32 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	6.31 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	6.32 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -1844,7 +1844,7 @@ name|HoldErrs
 condition|)
 name|message
 argument_list|(
-literal|"250 %s OK"
+literal|"250 %s Message accepted for delivery"
 argument_list|,
 name|id
 argument_list|)
