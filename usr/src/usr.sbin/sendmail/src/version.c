9bce9f2f94e91e53733e551e01dcba983c2ec0e8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-30 22:56:52 -0800

    fix incorrect infinite loop diagnosis & improve diagnostics; fix bogus CurHostAddr printing; don't warn about -f if setting sender to yourself; "probe" SMTP connections using RSET instead of NOOP
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.48
    
    Former-commit-id: bbd7ccb742ddfe86c889a13c4ff9947fbec8870c

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.47 (Berkeley) %G%"
+literal|"@(#)version.c	6.48 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.47"
+literal|"ALPHA-6.48"
 decl_stmt|;
 end_decl_stmt
 
