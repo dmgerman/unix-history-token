b5e498b99d0da160a37763296edc61a5aefb9adc
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-30 19:04:22 -0800

    fix typo
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.34
    
    Former-commit-id: e92b91d33ed2871367433aa05ba4c6bf0529a8a0

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	6.33 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	6.34 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	6.33 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	6.34 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -1510,7 +1510,7 @@ name|e
 operator|->
 name|e_sendmode
 operator|!=
-name|SM_INTERACTIVE
+name|SM_DELIVER
 condition|)
 name|e
 operator|->
