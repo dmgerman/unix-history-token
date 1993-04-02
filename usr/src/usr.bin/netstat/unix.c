7dce3ad824f0f2784e25037f7def01ed2ebb3623
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-04-01 22:25:26 -0800

    convert kinfo to sysctl
    SCCS-vsn: usr.bin/netstat/unix.c 5.16
    SCCS-vsn: usr.bin/netstat/route.c 5.26
    
    Former-commit-id: fed3b82df83c9e90b96ae11d4f6f4eae636835b5

--- usr/src/usr.bin/netstat/unix.c
+++ usr/src/usr.bin/netstat/unix.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)unix.c	5.15 (Berkeley) %G%"
+literal|"@(#)unix.c	5.16 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -71,7 +71,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/kinfo.h>
+file|<sys/sysctl.h>
 end_include
 
 begin_include
@@ -230,7 +230,7 @@ name|kvm_getfiles
 argument_list|(
 name|kvmd
 argument_list|,
-name|KINFO_FILE
+name|KERN_FILE
 argument_list|,
 literal|0
 argument_list|,
