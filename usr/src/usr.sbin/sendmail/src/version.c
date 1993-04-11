daeaf24619c51ca7b2ce4dac9ba146faf5f02660
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-10 17:27:11 -0800

    fix major bug in reply() that trashes all error messages; add IDA-style $&x handling; don't lose $s value in srvrsmtp; random POSIX compatibility and similar cleanups
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.52
    
    Former-commit-id: d949292e319089feeb43abb1b92a9aeb3df9b5d3

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.51 (Berkeley) %G%"
+literal|"@(#)version.c	6.52 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.51"
+literal|"ALPHA-6.52"
 decl_stmt|;
 end_decl_stmt
 
