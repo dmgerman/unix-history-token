4d9c8954749ec8512b96f30fcca1b0f96bb68a6b
Keith Muller <muller@ucbvax.Berkeley.EDU>
1993-04-13 00:46:10 -0800

    put the wrong prototype name in for ar_drain().
    SCCS-vsn: bin/pax/extern.h 1.5
    
    Former-commit-id: 9f7f5613a04c8f575989d8f436497838f825c2b6

--- usr/src/bin/pax/extern.h
+++ usr/src/bin/pax/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Keith Muller.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	1.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 Keith Muller.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	1.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -52,7 +52,7 @@ end_decl_stmt
 
 begin_decl_stmt
 name|void
-name|ar_flush
+name|ar_drain
 name|__P
 argument_list|(
 operator|(
