3db4ebabaf3abb9d9ad514b58bf664347513877f
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-04-14 02:53:58 -0800

    fix frame buffer size
    SCCS-vsn: sys/pmax/dev/mfb.c 7.5
    
    Former-commit-id: 84b5aea7d96eb28fb41823571b276eb612453ae1

--- usr/src/sys/pmax/dev/mfb.c
+++ usr/src/sys/pmax/dev/mfb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)mfb.c	7.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)mfb.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -519,7 +519,7 @@ begin_define
 define|#
 directive|define
 name|MFB_FB_SIZE
-value|0x100000
+value|0x200000
 end_define
 
 begin_comment
