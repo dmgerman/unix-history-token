8a03ef57a7459f4a3a8e37ff3fd5146e4a98fd1e
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-04-14 03:06:27 -0800

    added NNON_LEAF for non-profiled non-leaf entry points.
    SCCS-vsn: sys/pmax/include/machAsmDefs.h 7.5
    
    Former-commit-id: 52071c9c7a69239b0587cac8ea8bb4901bd7efc6

--- usr/src/sys/pmax/include/machAsmDefs.h
+++ usr/src/sys/pmax/include/machAsmDefs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)machAsmDefs.h	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)machAsmDefs.h	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -121,6 +121,25 @@ name|retpc
 parameter_list|)
 define|\
 value|.globl x; \ 	.ent x, 0; \ x: ; \ 	.frame sp, fsize, retpc; \ 	MCOUNT
+end_define
+
+begin_comment
+comment|/*  * NNON_LEAF(x)  *  *	Declare a non-profiled non-leaf routine  *	(a routine that makes other C calls).  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NNON_LEAF
+parameter_list|(
+name|x
+parameter_list|,
+name|fsize
+parameter_list|,
+name|retpc
+parameter_list|)
+define|\
+value|.globl x; \ 	.ent x, 0; \ x: ; \ 	.frame sp, fsize, retpc
 end_define
 
 begin_comment
