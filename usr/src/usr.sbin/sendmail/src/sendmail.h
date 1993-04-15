a0a2b8371ed6dee56d79bdb51b1eca110d7c32e0
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-14 23:52:12 -0800

    add e_bodytype
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.46
    
    Former-commit-id: d50dae380f6b45581f9182fb4814f8bcdf1a9dc6

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.45 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.46 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.45		%G%"
+literal|"@(#)sendmail.h	6.46		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1327,6 +1327,11 @@ modifier|*
 name|e_sibling
 decl_stmt|;
 comment|/* the next envelope of interest */
+name|char
+modifier|*
+name|e_bodytype
+decl_stmt|;
+comment|/* type of message body */
 name|char
 modifier|*
 name|e_df
