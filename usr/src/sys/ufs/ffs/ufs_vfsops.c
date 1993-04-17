c9e3a8bc7dff30b69b525704268c0d73af443f4f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-04-16 22:01:09 -0800

    ufs_free_netcred needs a return value
    SCCS-vsn: sys/ufs/ffs/ufs_vfsops.c 7.68
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 7.68
    
    Former-commit-id: dbf79152f01d2ffe0291754e29999cf811a7a413

--- usr/src/sys/ufs/ffs/ufs_vfsops.c
+++ usr/src/sys/ufs/ffs/ufs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	7.67 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	7.68 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -997,6 +997,11 @@ argument_list|,
 name|M_NETADDR
 argument_list|)
 expr_stmt|;
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 block|}
 end_function
 
