18408d0062604bf2ea0c322479bb21ef14a4a427
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-18 10:41:40 -0800

    fix broken label handling (rewrite label code completely)
    SCCS-vsn: usr.bin/sed/compile.c 5.8
    SCCS-vsn: usr.bin/sed/defs.h 5.5
    
    Former-commit-id: d7362c396b6ccb13b06a2bc3491d19e4e4856da8

--- usr/src/usr.bin/sed/defs.h
+++ usr/src/usr.bin/sed/defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Diomidis Spinellis.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Diomidis Spinellis of Imperial College, University of London.  *  * %sccs.include.redist.c%  *  *	@(#)defs.h	5.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 Diomidis Spinellis.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Diomidis Spinellis of Imperial College, University of London.  *  * %sccs.include.redist.c%  *  *	@(#)defs.h	5.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -172,12 +172,6 @@ range|:
 literal|1
 decl_stmt|;
 comment|/* True if in range */
-name|u_int
-name|lused
-range|:
-literal|1
-decl_stmt|;
-comment|/* True if label used. */
 block|}
 struct|;
 end_struct
