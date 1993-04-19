338840e89c09e1edde84a466b239706bce332a42
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-18 16:35:29 -0800

    use UCB standard prototype string
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 6.32
    
    Former-commit-id: 4b22cb84bfffb36fbbd6964d4eaf303d64086835

--- usr/src/usr.sbin/sendmail/src/alias.c
+++ usr/src/usr.sbin/sendmail/src/alias.c
@@ -81,7 +81,7 @@ name|char
 name|sccsid
 index|[]
 operator|=
-literal|"@(#)alias.c	6.31 (Berkeley) %G% (with NEWDB and NDBM)"
+literal|"@(#)alias.c	6.32 (Berkeley) %G% (with NEWDB and NDBM)"
 expr_stmt|;
 end_expr_stmt
 
@@ -96,7 +96,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)alias.c	6.31 (Berkeley) %G% (with NEWDB)"
+literal|"@(#)alias.c	6.32 (Berkeley) %G% (with NEWDB)"
 decl_stmt|;
 end_decl_stmt
 
@@ -122,7 +122,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)alias.c	6.31 (Berkeley) %G% (with NDBM)"
+literal|"@(#)alias.c	6.32 (Berkeley) %G% (with NDBM)"
 decl_stmt|;
 end_decl_stmt
 
@@ -137,7 +137,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)alias.c	6.31 (Berkeley) %G% (without NEWDB or NDBM)"
+literal|"@(#)alias.c	6.32 (Berkeley) %G% (without NEWDB or NDBM)"
 decl_stmt|;
 end_decl_stmt
 
@@ -3124,7 +3124,7 @@ name|YPCOMPAT
 specifier|static
 name|void
 name|nis_magic
-name|P
+name|__P
 argument_list|(
 operator|(
 name|DBM
