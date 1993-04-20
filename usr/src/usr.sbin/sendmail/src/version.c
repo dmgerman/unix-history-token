5cd4929cc64d6bd6885e94f6ad90f43feb0e4d38
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-19 23:53:18 -0800

    properly handle \! hack; improve error message printing; XLA extensions; additional hooks for tempfail detection
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.54
    
    Former-commit-id: f7c20584b60d59838f5127836673d6014c20465c

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.53 (Berkeley) %G%"
+literal|"@(#)version.c	6.54 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.53"
+literal|"ALPHA-6.54"
 decl_stmt|;
 end_decl_stmt
 
