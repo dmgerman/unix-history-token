4cef73a08b05d80baec12fe1a2e115a5a22ae119
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-20 00:58:06 -0800

    terminate swap list with NODEV, not 0: 0 is a valid (if unusual) dev
    SCCS-vsn: usr.sbin/config/mkswapconf.c 5.11
    
    Former-commit-id: 4644a6bbd20c31f9d36a2d29a32d62eeb1783ade

--- usr/src/usr.sbin/config/mkswapconf.c
+++ usr/src/usr.sbin/config/mkswapconf.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mkswapconf.c	5.10 (Berkeley) %G%"
+literal|"@(#)mkswapconf.c	5.11 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -387,7 +387,7 @@ name|fprintf
 argument_list|(
 name|fp
 argument_list|,
-literal|"\t{ 0, 0, 0 }\n"
+literal|"\t{ NODEV, 0, 0 }\n"
 argument_list|)
 expr_stmt|;
 name|fprintf
