3e5edff50d1794ef9307ca1ed7558bcfc637552a
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-20 12:46:21 -0800

    van's changes (const for SunOS; add backlog control)
    SCCS-vsn: sys/sparc/dev/bsd_audiovar.h 7.3
    
    Former-commit-id: 0ca84ae911fe1946d817d96c16fb0ce4f364b359

--- usr/src/sys/sparc/dev/bsd_audiovar.h
+++ usr/src/sys/sparc/dev/bsd_audiovar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratories.  *  * %sccs.include.redist.c%  *  *	@(#)bsd_audiovar.h	7.2 (Berkeley) %G%  *  * from: $Header: bsd_audiovar.h,v 1.4 92/07/03 23:23:12 mccanne Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)bsd_audiovar.h	7.3 (Berkeley) %G%  *  * from: $Header: bsd_audiovar.h,v 1.6 92/11/21 20:46:49 van Exp $ (LBL)  */
 end_comment
 
 begin_define
@@ -154,6 +154,33 @@ endif|#
 directive|endif
 end_endif
 
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|__STDC__
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+specifier|const
+argument_list|)
+end_if
+
+begin_define
+define|#
+directive|define
+name|const
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_struct
 struct|struct
 name|auio
@@ -181,6 +208,10 @@ name|int
 name|au_blksize
 decl_stmt|;
 comment|/* recv block (chunk) size */
+name|int
+name|au_backlog
+decl_stmt|;
+comment|/* # samples of xmit backlog to gen. */
 name|struct
 name|aucb
 name|au_rb
