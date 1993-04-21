48643c084fad56cd1d43315451e98cafc1acb4e5
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-21 04:12:50 -0800

    LED_ON and LED_OFF were reversed
    SCCS-vsn: sys/sparc/sparc/auxreg.h 7.3
    
    Former-commit-id: 40140445c8a76d562bdb0f5aa01e74d9eb390ce8

--- usr/src/sys/sparc/sparc/auxreg.h
+++ usr/src/sys/sparc/sparc/auxreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratories.  *  * %sccs.include.redist.c%  *  *	@(#)auxreg.h	7.2 (Berkeley) %G%  *  * from: $Header: auxreg.h,v 1.5 92/06/17 05:21:55 torek Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)auxreg.h	7.3 (Berkeley) %G%  *  * from: $Header: auxreg.h,v 1.8 92/11/26 03:04:45 torek Exp $ (LBL)  */
 end_comment
 
 begin_comment
@@ -99,14 +99,14 @@ begin_define
 define|#
 directive|define
 name|LED_ON
-value|*AUXIO_REG = AUXIO_MB1|AUXIO_FEJ
+value|*AUXIO_REG = AUXIO_MB1|AUXIO_FEJ|AUXIO_LED
 end_define
 
 begin_define
 define|#
 directive|define
 name|LED_OFF
-value|*AUXIO_REG = AUXIO_MB1|AUXIO_FEJ|AUXIO_LED
+value|*AUXIO_REG = AUXIO_MB1|AUXIO_FEJ
 end_define
 
 begin_define
