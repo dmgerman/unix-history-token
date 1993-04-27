41db2c1f1fd6eddfb997397d198dae2d0102f139
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-27 14:40:06 -0800

    update for dma chip rev 3, not that we have any yet
    SCCS-vsn: sys/sparc/sbus/dmareg.h 7.4
    
    Former-commit-id: 1ce1d275553d2e0b2984fa875f4cb40205191867

--- usr/src/sys/sparc/sbus/dmareg.h
+++ usr/src/sys/sparc/sbus/dmareg.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)dmareg.h	7.3 (Berkeley) %G%  *  * from: $Header: dmareg.h,v 1.5 92/11/26 02:28:08 torek Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)dmareg.h	7.4 (Berkeley) %G%  *  * from: $Header: dmareg.h,v 1.6 93/04/27 14:39:17 torek Exp $ (LBL)  */
 end_comment
 
 begin_comment
-comment|/*  * Sun-4c Sbus slot 0 DMA registers.  */
+comment|/*  * Sun-4c Sbus slot 0 DMA registers.  *  * Note that the dma_addr on the rev 1 gate array cannot increment  * across a 16 MB boundary (the counter is only 24 bits wide; the  * top 8 bits are simply latched).  */
 end_comment
 
 begin_struct
@@ -32,7 +32,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  * Bits in dma_csr.  *  * Notes in [brackets]:  *	1: not self-clearing, must be reset after being set.  *	2: `drain' is like Unibus `bdp purge', i.e., it tells  *	   the chip to finish up, because there is no more data  *	   going into the buffer register.  Needed only in rev  *	   1 dma chips.  Self-clearing (hence write-only).  *	3: only in rev 1 dma chips.  *	4: only in rev 2 dma chips.  *	5: also enables scsi interrupts.  */
+comment|/*  * Bits in dma_csr.  * DMA chip revision numbers are confusing (rev 3 = `DMA2'?!).  * Not my fault; we just live with what they give us....  *  * Notes in [brackets]:  *	1: used this way in rev 1 (`DMA') chips.  *	2: used this way in rev 2 (`DMA+') chips.  *	3: used this way in rev 3 (`DMA2') chips.  *	R: not self-clearing, must be reset after being set.  *	D: `drain' is like Unibus `bdp purge', i.e., it tells  *	   the chip to finish up, because there is no more data  *	   going into the buffer register.  Supposed to be needed  *	   only in rev 1, but apparently needed or harmless in all  *	   revs.  Self-clearing (hence write-only).  *	I: also enables scsi interrupts.  */
 end_comment
 
 begin_define
@@ -57,7 +57,7 @@ value|0x8
 end_define
 
 begin_comment
-comment|/* device id = rev 1 DMA */
+comment|/* rev 1 DMA gate array */
 end_comment
 
 begin_define
@@ -68,7 +68,18 @@ value|0x9
 end_define
 
 begin_comment
-comment|/* device id = rev 2 DMA */
+comment|/* rev 2 DMA gate array (`DMA+') */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMAREV_3
+value|0xa
+end_define
+
+begin_comment
+comment|/* rev 3 DMA gate array (`DMA2') */
 end_comment
 
 begin_define
@@ -79,7 +90,7 @@ value|0x0fff0000
 end_define
 
 begin_comment
-comment|/* unused; reads as zero [3] */
+comment|/* unused; reads as zero [1] */
 end_comment
 
 begin_define
@@ -90,7 +101,7 @@ value|0x08000000
 end_define
 
 begin_comment
-comment|/* next address loaded [4] (ro) */
+comment|/* next address loaded [2] (ro) */
 end_comment
 
 begin_define
@@ -101,7 +112,7 @@ value|0x04000000
 end_define
 
 begin_comment
-comment|/* address loaded [4] (ro) */
+comment|/* address loaded [2] (ro) */
 end_comment
 
 begin_define
@@ -112,7 +123,7 @@ value|0x02000000
 end_define
 
 begin_comment
-comment|/* working [4] (ro) */
+comment|/* working [2] (ro) */
 end_comment
 
 begin_define
@@ -123,7 +134,7 @@ value|0x01000000
 end_define
 
 begin_comment
-comment|/* next-address enable [4] (rw) */
+comment|/* next-address enable [2] (rw) */
 end_comment
 
 begin_define
@@ -134,7 +145,7 @@ value|0x00800000
 end_define
 
 begin_comment
-comment|/* disable DMA_TC intr [4] (rw) */
+comment|/* disable DMA_TC intr [2] (rw) */
 end_comment
 
 begin_define
@@ -145,7 +156,7 @@ value|0x00400000
 end_define
 
 begin_comment
-comment|/* faster 53C90A mode [4] (rw) */
+comment|/* faster 53C90A mode [2] (rw) */
 end_comment
 
 begin_define
@@ -156,7 +167,18 @@ value|0x00200000
 end_define
 
 begin_comment
-comment|/* LANCE error [4] (ro) */
+comment|/* LANCE error [2] (ro) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_TWOCYCLE
+value|0x00200000
+end_define
+
+begin_comment
+comment|/* two cycle mode [3] (rw?) */
 end_comment
 
 begin_define
@@ -167,7 +189,7 @@ value|0x00100000
 end_define
 
 begin_comment
-comment|/* LANCE addr latch ena [4] (rw) */
+comment|/* LANCE addr latch ena [2] (rw) */
 end_comment
 
 begin_define
@@ -178,7 +200,51 @@ value|0x000f0000
 end_define
 
 begin_comment
-comment|/* unused; reads as zero [4] */
+comment|/* unused; reads as zero [2] */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_BURSTMASK
+value|0x000c0000
+end_define
+
+begin_comment
+comment|/* burst mode mask [3] */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_BURST16
+value|0x00000000
+end_define
+
+begin_comment
+comment|/* 16-byte bursts (default) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_BURST32
+value|0x00040000
+end_define
+
+begin_comment
+comment|/* 32-byte bursts */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_BURST0
+value|0x00080000
+end_define
+
+begin_comment
+comment|/* no bursts */
 end_comment
 
 begin_define
@@ -189,7 +255,7 @@ value|0x00008000
 end_define
 
 begin_comment
-comment|/* set for new AMD ethernet chip */
+comment|/* for new AMD ethernet chip [1,2] */
 end_comment
 
 begin_define
@@ -200,7 +266,7 @@ value|0x00004000
 end_define
 
 begin_comment
-comment|/* terminal count: dma_bc ran out */
+comment|/* terminal cnt: dma_bc ran out [1,2] */
 end_comment
 
 begin_define
@@ -222,7 +288,7 @@ value|0x00001800
 end_define
 
 begin_comment
-comment|/* byte offset (ro) */
+comment|/* byte offset [1] (ro) */
 end_comment
 
 begin_define
@@ -233,7 +299,7 @@ value|0x00000400
 end_define
 
 begin_comment
-comment|/* request pending (ro) */
+comment|/* busy, plz don't flush [1] (ro) */
 end_comment
 
 begin_define
@@ -266,7 +332,7 @@ value|0x00000080
 end_define
 
 begin_comment
-comment|/* reset dma chip [1] */
+comment|/* reset dma chip [R] */
 end_comment
 
 begin_define
@@ -277,18 +343,18 @@ value|0x00000040
 end_define
 
 begin_comment
-comment|/* drain buffered data [2,3] (wo) */
+comment|/* drain buffered data [D,1] (wo) */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|DMA_ERR
+name|DMA_SLAVEERR
 value|0x00000040
 end_define
 
 begin_comment
-comment|/* slave error [4] (ro) */
+comment|/* slave error [2,3] (ro) */
 end_comment
 
 begin_define
@@ -299,7 +365,7 @@ value|0x00000020
 end_define
 
 begin_comment
-comment|/* clear PC, EP, and TC [4] (wo) */
+comment|/* clear PC, EP, and TC [2,3] (wo) */
 end_comment
 
 begin_define
@@ -310,7 +376,7 @@ value|0x00000010
 end_define
 
 begin_comment
-comment|/* interrupt enable [4,5] */
+comment|/* interrupt enable [2,3,I] */
 end_comment
 
 begin_define
@@ -321,7 +387,18 @@ value|0x0000000c
 end_define
 
 begin_comment
-comment|/* bytes in pack reg (ro) */
+comment|/* bytes in pack reg [1] (ro) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_DRAINING
+value|0x0000000c
+end_define
+
+begin_comment
+comment|/* nonzero => draining [2,3] (ro) */
 end_comment
 
 begin_define
@@ -349,9 +426,33 @@ end_comment
 begin_define
 define|#
 directive|define
-name|DMA_BITS
+name|DMA_REV1_BITS
 define|\
-value|"\20\34NAL\33AL\32ON\31NAE\30DTCI\27TURBO\26LERR\25ALE\ \20ILACC\17TC\16BCE\13RP\12ENA\11READ\10RESET\7DRAIN/ERR\6FLUSH\5IE\2EP\1IP"
+value|"\20\20ILACC\17TC\16BCE\13RP\12ENA\11READ\10RESET\7DRAIN\2EP\1IP"
+end_define
+
+begin_comment
+comment|/* Look ma, second system syndrome! */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_REV2_BITS
+define|\
+value|"\20\34NAL\33AL\32ON\31NAE\30DTCI\27TURBO\26LERR\25ALE\ \20ILACC\17TC\16BCE\12ENA\11READ\10RESET\7SLAVEERR\6FLUSH\5IE\2EP\1IP"
+end_define
+
+begin_comment
+comment|/* (Note how most of the crap has been discarded now.) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DMA_REV3_BITS
+define|\
+value|"\20\26TWOCYCLE\16BCE\12ENA\11READ\10RESET\7SLAVEERR\6FLUSH\5IE\2EP\1IP"
 end_define
 
 begin_comment
