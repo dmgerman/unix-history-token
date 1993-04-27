2bdcf4ec2802320eeb0e2dc3e749f9714edfbd78
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-26 18:07:15 -0800

    type char can be a problem in prototypes on some systems
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.50
    
    Former-commit-id: 9acb4f5d7b715273f6a4beb150ab765c36030b11

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.49 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.50 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.49		%G%"
+literal|"@(#)sendmail.h	6.50		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -4239,7 +4239,7 @@ operator|*
 operator|,
 name|int
 operator|,
-name|char
+name|int
 operator|,
 name|char
 operator|*
@@ -4356,7 +4356,7 @@ operator|(
 name|ENVELOPE
 operator|*
 operator|,
-name|char
+name|int
 operator|)
 argument_list|)
 decl_stmt|;
