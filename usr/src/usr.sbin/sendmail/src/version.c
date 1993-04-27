f4a6f33335a703497a694b8b6bd97bf285fd458d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-27 14:16:57 -0800

    adjust ctladdr on Q runs; fix lost CNAME problem; getmxrr match against $=w instead of $j; "a" mailer flag + ESMTP enhancements; extend "b" option to include max message size; NeXT & HPUX compat changes; up default buffer sizes; fix backslash scanning problem; misc cleanup
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.55
    
    Former-commit-id: 1105d388ce7ee1aab0b4e3ca822d2d3162ebf403

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.54 (Berkeley) %G%"
+literal|"@(#)version.c	6.55 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.54"
+literal|"ALPHA-6.55"
 decl_stmt|;
 end_decl_stmt
 
