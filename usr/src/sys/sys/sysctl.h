6ddfd5881a88c766058a3e40673ff12f5fb7ff87
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-27 21:45:43 -0800

    protect against multiple inclusion (user.h includes it) unbalanced endif's
    SCCS-vsn: sys/sys/sysctl.h 7.22
    
    Former-commit-id: ae2faa964079d3b7b783ad2d1afc185233264bff

--- usr/src/sys/sys/sysctl.h
+++ usr/src/sys/sys/sysctl.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)sysctl.h	7.21 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)sysctl.h	7.22 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_SYS_SYSCTL_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_SYS_SYSCTL_H_
+end_define
 
 begin_comment
 comment|/*  * These are for the eproc structure defined below.  */
@@ -1177,6 +1189,15 @@ end_endif
 
 begin_comment
 comment|/* KERNEL */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_SYS_SYSCTL_H_ */
 end_comment
 
 end_unit
