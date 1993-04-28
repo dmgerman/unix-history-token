6de477e77e8066a740a31bf236a0b9ae7d009612
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-04-27 22:07:00 -0800

    kinfo_proc merged into user.h
    SCCS-vsn: sys/sys/user.h 7.22
    
    Former-commit-id: d93afb868c2fe9c1d834db60457fa894d79569ae

--- usr/src/sys/sys/user.h
+++ usr/src/sys/sys/user.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)user.h	7.21 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)user.h	7.22 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -79,7 +79,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<sys/kinfo_proc.h>
+file|<sys/sysctl.h>
 end_include
 
 begin_comment
