83debb512385e64245c0e9cc074571fc2d1b2dcf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 01:24:07 -0800

    declare u_pop_n_swap
    SCCS-vsn: contrib/ed/extern.h 5.3
    
    Former-commit-id: 79d96fedda4057f3f5a5b7d80c903ccdbbd3252a

--- usr/src/contrib/ed/extern.h
+++ usr/src/contrib/ed/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -788,6 +788,20 @@ name|__P
 argument_list|(
 operator|(
 name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|u_pop_n_swap
+name|__P
+argument_list|(
+operator|(
+name|LINE
+operator|*
+operator|*
 operator|)
 argument_list|)
 decl_stmt|;
