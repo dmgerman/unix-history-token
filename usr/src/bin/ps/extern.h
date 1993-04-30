51c6494acab87e1d57d5742524cc38d0056ab751
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 20:19:12 -0800

    use err/warn from the C library
    SCCS-vsn: bin/ps/extern.h 5.4
    
    Former-commit-id: bfc9503ef8fd3ee9e80163c688581bab628bfc0c

--- usr/src/bin/ps/extern.h
+++ usr/src/bin/ps/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_struct_decl
@@ -98,22 +98,6 @@ name|__P
 argument_list|(
 operator|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|void
-name|err
-name|__P
-argument_list|(
-operator|(
-specifier|const
-name|char
-operator|*
-operator|,
-operator|...
 operator|)
 argument_list|)
 decl_stmt|;
