e99c8296db57f42bf1c2d6ce10b1832f95435f74
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-04-30 03:35:49 -0800

    kmem_alloc*() can return zero if no more kernel virtual memory.
    SCCS-vsn: sys/vm/vm_glue.c 7.22
    
    Former-commit-id: b79eec3aaebe6119a641ef3057c5811980e0e936

--- usr/src/sys/vm/vm_glue.c
+++ usr/src/sys/vm/vm_glue.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_glue.c	7.21 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_glue.c	7.22 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_include
@@ -626,6 +626,17 @@ name|UPAGES
 argument_list|)
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|addr
+operator|==
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"vm_fork: no more kernel virtual memory\n"
+argument_list|)
+expr_stmt|;
 name|vm_map_pageable
 argument_list|(
 name|kernel_map
@@ -655,6 +666,17 @@ name|ctob
 argument_list|(
 name|UPAGES
 argument_list|)
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|addr
+operator|==
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"vm_fork: no more kernel virtual memory\n"
 argument_list|)
 expr_stmt|;
 endif|#
