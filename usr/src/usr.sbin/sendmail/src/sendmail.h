0194b33a5da38394d7159d5f1f23a0322419e7c6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-29 23:36:15 -0800

    add M_BLANKEND
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.53
    
    Former-commit-id: 1b52dae17db2c7394a72880ede532a8bdbb0227c

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.52 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.53 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.52		%G%"
+literal|"@(#)sendmail.h	6.53		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -684,6 +684,17 @@ end_define
 
 begin_comment
 comment|/* run Extended SMTP protocol */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|M_BLANKEND
+value|'b'
+end_define
+
+begin_comment
+comment|/* ensure blank line at end of message */
 end_comment
 
 begin_define
