17426943afe5faef8168020e54506cc82717389b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-30 00:40:06 -0800

    update to 4.4BSD release values (32 days to go...)
    SCCS-vsn: sys/sys/param.h 7.28
    
    Former-commit-id: e3e60d3404209cdd844d88a6bf0a22f125fdb361

--- usr/src/sys/sys/param.h
+++ usr/src/sys/sys/param.h
@@ -1,13 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)param.h	7.27 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)param.h	7.28 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|BSD
-value|199207
+value|199306
 end_define
 
 begin_comment
@@ -25,7 +25,7 @@ begin_define
 define|#
 directive|define
 name|BSD4_4
-value|0.5
+value|1
 end_define
 
 begin_ifndef
