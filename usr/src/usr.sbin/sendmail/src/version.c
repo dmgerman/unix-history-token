1e15aacb705a3122b8d335f3db16b023d7af6c71
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-01 16:07:13 -0800

    fix Postmaster alias in fallback mail return; fix flock problem for /dev/null; add "b" and "g" mailer flags; don't change case on header field names; improved error message reporting for queue conditions; HPUX tweaking; misc fixes
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.56
    
    Former-commit-id: e3a496f4b2628880f873d131f32287630bbb0c1b

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.55 (Berkeley) %G%"
+literal|"@(#)version.c	6.56 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.55"
+literal|"ALPHA-6.56"
 decl_stmt|;
 end_decl_stmt
 
