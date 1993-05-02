d7e4ee4ef1a033cf39eb0e13b683a5431c195e30
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-05-02 12:43:33 -0800

    add PORT related macro
    SCCS-vsn: sys/luna68k/dev/sioreg.h 7.3
    
    Former-commit-id: 69b611a8e8a36129995f373c87aea8ce3dae43fd

--- usr/src/sys/luna68k/dev/sioreg.h
+++ usr/src/sys/luna68k/dev/sioreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)sioreg.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)sioreg.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -937,6 +937,27 @@ end_define
 begin_comment
 comment|/* build-in serial-interface address */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|NPORT
+value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|SIO_PORT
+value|0
+end_define
+
+begin_define
+define|#
+directive|define
+name|BMC_PORT
+value|1
+end_define
 
 end_unit
 
