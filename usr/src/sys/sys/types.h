118b53d40b1c45d51c7ba50bf773c7c5e66e1c66
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-02 18:12:24 -0800

    add prototype for lseek to make programs casting second arg to off_t work
    SCCS-vsn: sys/sys/types.h 7.27
    
    Former-commit-id: 1a0ccd7fe5f03f60bb87ffade2550746b9b1e550

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.26 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.27 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -271,6 +271,49 @@ end_typedef
 begin_comment
 comment|/* user id */
 end_comment
+
+begin_comment
+comment|/*  * This belongs in unistd.h, but is placed here to ensure that programs  * casting the second parameter of lseek to off_t will get the correct  * version of lseek.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|KERNEL
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|lseek
+value|__lseek
+end_define
+
+begin_decl_stmt
+name|__BEGIN_DECLS
+name|off_t
+name|lseek
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+name|off_t
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_macro
+name|__END_DECLS
+end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
