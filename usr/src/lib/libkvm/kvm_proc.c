b08da568f33a39eb0ef9832f695d5d28ca7f63de
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-03 12:51:49 -0800

    kinfo_proc moved into sysctl.h
    SCCS-vsn: lib/libkvm/kvm_proc.c 5.29
    
    Former-commit-id: ab2cebe74b137f436531a6918eb2be72bdb3f85d

--- usr/src/lib/libkvm/kvm_proc.c
+++ usr/src/lib/libkvm/kvm_proc.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)kvm_proc.c	5.28 (Berkeley) %G%"
+literal|"@(#)kvm_proc.c	5.29 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -123,12 +123,6 @@ begin_include
 include|#
 directive|include
 file|<sys/sysctl.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/kinfo_proc.h>
 end_include
 
 begin_include
