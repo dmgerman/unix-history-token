f0937d798fe8ca0bb6613b3f9c4dd47eaff7c859
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-03 22:26:20 -0800

    add fpathconf, pathconf, sysconf to the C library
    SCCS-vsn: include/unistd.h 5.28
    
    Former-commit-id: 38b1f89da4c14228db49016a67cdfe56ac9eeefa

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.27 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.28 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -350,10 +350,6 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
-begin_comment
-comment|/* not yet */
-end_comment
-
 begin_decl_stmt
 name|char
 modifier|*
@@ -566,10 +562,6 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
-begin_comment
-comment|/* not yet */
-end_comment
-
 begin_decl_stmt
 name|int
 name|pause
@@ -701,10 +693,6 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
-
-begin_comment
-comment|/* not yet */
-end_comment
 
 begin_decl_stmt
 name|pid_t
