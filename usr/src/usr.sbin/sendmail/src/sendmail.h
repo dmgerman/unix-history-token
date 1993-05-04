c1ac6eb19c5df3bea67bb06fbefbe8a1aa298bba
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 02:05:21 -0800

    change EightBit to SevenBit and invert
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.59
    
    Former-commit-id: d8f6a62824985f23d0ba97bd6b56a63589f83e63

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.58 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.59 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.58		%G%"
+literal|"@(#)sendmail.h	6.59		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3518,12 +3518,12 @@ end_comment
 begin_decl_stmt
 name|EXTERN
 name|bool
-name|EightBit
+name|SevenBit
 decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/* try to preserve 8-bit data */
+comment|/* force 7-bit data */
 end_comment
 
 begin_decl_stmt
