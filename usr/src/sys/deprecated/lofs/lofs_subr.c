52b5e91e0ff526126feab4c091082ead3cec6300
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1993-05-05 12:22:31 -0800

    add missing return value.
    SCCS-vsn: sys/deprecated/lofs/lofs_subr.c 7.2
    
    Former-commit-id: e0247c34c35b5562f21cae2de0cf6a454f9f9396

--- usr/src/sys/deprecated/lofs/lofs_subr.c
+++ usr/src/sys/deprecated/lofs/lofs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)lofs_subr.c	7.1 (Berkeley) %G%  *  * $Id: lofs_subr.c,v 1.11 1992/05/30 10:05:43 jsp Exp jsp $  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)lofs_subr.c	7.2 (Berkeley) %G%  *  * $Id: lofs_subr.c,v 1.11 1992/05/30 10:05:43 jsp Exp jsp $  */
 end_comment
 
 begin_include
@@ -714,7 +714,11 @@ name|newvpp
 operator|=
 name|targetvp
 expr_stmt|;
-return|return;
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 block|}
 name|ap
 operator|=
