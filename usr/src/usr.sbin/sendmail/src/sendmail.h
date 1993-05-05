05a46b7f689ae8db3c08987ce7c139f6c2e984f5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 19:17:52 -0800

    putbody takes one more parameter
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.62
    
    Former-commit-id: 4dd6b9c431d92f80f66f11675a6de700f7676135

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.61 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.62 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.61		%G%"
+literal|"@(#)sendmail.h	6.62		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1372,6 +1372,9 @@ name|MAILER
 operator|*
 operator|,
 name|ENVELOPE
+operator|*
+operator|,
+name|char
 operator|*
 operator|)
 argument_list|)
