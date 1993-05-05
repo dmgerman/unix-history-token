6ea917a8d7d491427ed03434f925011ba31e8b13
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 19:24:55 -0800

    make NIS set NIS_ALIASES and NIS_MAP
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 6.28
    
    Former-commit-id: 585de065dc61d1a20771cb59cdb0b26a570d3c63

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	6.27 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	6.28 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -345,6 +345,39 @@ end_define
 
 begin_comment
 comment|/* enable HASH mapping type (requires NEWDB) */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|NIS
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|NIS_ALIASES
+value|1
+end_define
+
+begin_comment
+comment|/* include NIS support for aliases */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NIS_MAP
+value|1
+end_define
+
+begin_comment
+comment|/* include NIS mapping type */
 end_comment
 
 begin_endif
