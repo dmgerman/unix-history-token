5bab3e480d15e2043ec209cd2a4a71b4854a9371
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-07 17:10:49 -0800

    make __cputchar visible, the back compatibility stuff uses it bug report 4.4BSD-alpha/include/0
    SCCS-vsn: lib/libcurses/curses.h 5.32
    
    Former-commit-id: 3607b96ca44af099e8067f4e40000dfc7edd1de5

--- usr/src/lib/libcurses/curses.h
+++ usr/src/lib/libcurses/curses.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1981 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)curses.h	5.31 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1981 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)curses.h	5.32 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -2286,6 +2286,22 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+name|void
+name|__cputchar
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* Public: backward compatibility */
+end_comment
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -2307,18 +2323,6 @@ name|char
 operator|*
 operator|,
 operator|...
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|void
-name|__cputchar
-name|__P
-argument_list|(
-operator|(
-name|int
 operator|)
 argument_list|)
 decl_stmt|;
