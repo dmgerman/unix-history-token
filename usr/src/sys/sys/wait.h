d52c660d4bd7857f52a383c0852cf887b092d2f5
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-07 21:53:21 -0800

    endian.h now protects itself
    SCCS-vsn: sys/sys/wait.h 7.19
    
    Former-commit-id: c60cd1fca82194a48a1b910fb61b5894d0dfd5bd

--- usr/src/sys/sys/wait.h
+++ usr/src/sys/sys/wait.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)wait.h	7.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)wait.h	7.19 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -244,22 +244,11 @@ begin_comment
 comment|/* any process in my process group */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|BYTE_ORDER
-end_ifndef
-
 begin_include
 include|#
 directive|include
 file|<machine/endian.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/*  * Deprecated:  * Structure of the information in the status word returned by wait4.  * If w_stopval==WSTOPPED, then the second structure describes  * the information returned, else the first.  */
