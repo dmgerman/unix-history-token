c951892588235d952f82eb19573423f489e3065f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-07 16:54:28 -0800

    shut gcc up about large integer constants
    SCCS-vsn: sys/hp300/include/limits.h 7.7
    
    Former-commit-id: 79d61c2a1e3981333162010225b85e563bcfafda

--- usr/src/sys/hp300/include/limits.h
+++ usr/src/sys/hp300/include/limits.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)limits.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)limits.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -37,7 +37,7 @@ comment|/* ticks per second */
 end_comment
 
 begin_comment
-comment|/*  * According to ANSI (section 2.2.4.2), the values below must be usable by  * #if preprocessing directives.  Additionally, the expression must have the  * same type as would an expression that is an object of the corresponding  * type converted according to the integral promotions.  The subtraction for  * INT_MIN and LONG_MIN is so the value is not unsigned; 2147483648 is an  * unsigned int for 32-bit two's complement ANSI compilers (section 3.1.3.2).  * These numbers work for pcc as well.  */
+comment|/*  * According to ANSI (section 2.2.4.2), the values below must be usable by  * #if preprocessing directives.  Additionally, the expression must have the  * same type as would an expression that is an object of the corresponding  * type converted according to the integral promotions.  The subtraction for  * INT_MIN and LONG_MIN is so the value is not unsigned; 2147483648 is an  * unsigned int for 32-bit two's complement ANSI compilers (section 3.1.3.2).  * These numbers work for pcc as well.  The UINT_MAX and ULONG_MAX values  * are written as hex so that GCC will be quiet about large integer constants.  */
 end_comment
 
 begin_define
@@ -132,7 +132,7 @@ begin_define
 define|#
 directive|define
 name|UINT_MAX
-value|4294967295
+value|0xffffffff
 end_define
 
 begin_comment
@@ -165,7 +165,7 @@ begin_define
 define|#
 directive|define
 name|ULONG_MAX
-value|4294967295
+value|0xffffffff
 end_define
 
 begin_comment
