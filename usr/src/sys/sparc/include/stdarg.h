4615756e87024588c836f217890cc62b8a331b52
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-07 18:13:43 -0800

    handle C++ and struct-values
    SCCS-vsn: sys/sparc/include/stdarg.h 7.5
    SCCS-vsn: sys/sparc/include/varargs.h 7.5
    
    Former-commit-id: f41bb6636c93d74de5f6cd412be2e01d037eaccf

--- usr/src/sys/sparc/include/stdarg.h
+++ usr/src/sys/sparc/include/stdarg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	7.4 (Berkeley) %G%  *  * from: $Header: stdarg.h,v 1.7 92/11/26 02:04:47 torek Exp $  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)stdarg.h	7.5 (Berkeley) %G%  *  * from: $Header: stdarg.h,v 1.8 93/05/07 18:10:14 torek Exp $  */
 end_comment
 
 begin_comment
@@ -80,7 +80,51 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * va_arg picks up the next argument of type `ty'.  Appending an  * asterisk to ty must produce a pointer to ty (i.e., ty may not be,  * e.g., `int (*)()').  In addition, ty must not be any type which  * undergoes promotion to some other type (e.g., char): it must  * be the promoted type instead.  *  * Gcc-2.x tries to use ldd/std for double and quad_t values, but Sun's  * brain-damaged calling convention does not quad-align these.  Thus,  * for 8-byte arguments, we have to pick up the actual value four bytes  * at a time, and use type punning (i.e., a union) to produce the result.  * (We could also do this with a libc function, actually, by returning  * 8 byte integers in %o0+%o1 and the same 8 bytes as a double in %f0+%f1.)  */
+comment|/*  * va_arg picks up the next argument of type `ty'.  Appending an  * asterisk to ty must produce a pointer to ty (i.e., ty may not be,  * e.g., `int (*)()').  In addition, ty must not be any type which  * undergoes promotion to some other type (e.g., char): it must  * be the promoted type instead.  *  * Gcc-2.x tries to use ldd/std for double and quad_t values, but Sun's  * brain-damaged calling convention does not quad-align these.  Thus,  * for 8-byte arguments, we have to pick up the actual value four bytes  * at a time, and use type punning (i.e., a union) to produce the result.  * (We could also do this with a libc function, actually, by returning  * 8 byte integers in %o0+%o1 and the same 8 bytes as a double in %f0+%f1.)  *  * Note: we cannot use the union trick (which generates better code) for  * C++, since `ty' might be a type with a constructor (these may not appear  * in a union).  *  * The extraneous casts through `void *' avoid gcc alignment warnings.  */
+end_comment
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__cplusplus
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|__va_8byte
+parameter_list|(
+name|ap
+parameter_list|,
+name|ty
+parameter_list|)
+value|({ \ 	int __va_i[2]; \ 	__va_i[0] = ((int *)(void *)(ap))[0]; \ 	__va_i[1] = ((int *)(void *)(ap))[1]; \ 	(ap) += 8; *(ty *)(void *)__va_i; })
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|__va_8byte
+parameter_list|(
+name|ap
+parameter_list|,
+name|ty
+parameter_list|)
+value|({ \ 	union { ty __d; int __i[2]; } __va_u; \ 	__va_u.__i[0] = ((int *)(void *)(ap))[0]; \ 	__va_u.__i[1] = ((int *)(void *)(ap))[1]; \ 	(ap) += 8; __va_u.__d; })
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __cplusplus */
 end_comment
 
 begin_define
@@ -92,8 +136,9 @@ name|ap
 parameter_list|,
 name|ty
 parameter_list|)
-define|\
-value|(sizeof(ty) == 8 ? __extension__ ({ \ 	union { ty __d; int __i[2]; } __u; \ 	__u.__i[0] = ((int *)(ap))[0]; \ 	__u.__i[1] = ((int *)(ap))[1]; \ 	(ap) += 8; \ 	__u.__d; }) : \     ((ty *)(ap += sizeof(ty)))[-1])
+value|__extension__ ({ \     ty __va_temp;
+comment|/* to check for invisible-ptr struct-valued args */
+value|\     __builtin_classify_type(__va_temp)>= 12 ? \ 	((ty **)(void *)((ap) += sizeof(ty *)))[-1][0] : \     sizeof(ty) == 8 ? __va_8byte(ap, ty) : \ 	((ty *)(void *)(ap += sizeof(ty)))[-1]; })
 end_define
 
 begin_endif
--- usr/src/sys/sparc/include/varargs.h
+++ usr/src/sys/sparc/include/varargs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)varargs.h	7.4 (Berkeley) %G%  *  * from: $Header: varargs.h,v 1.6 92/11/26 02:04:48 torek Exp $  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)varargs.h	7.5 (Berkeley) %G%  *  * from: $Header: varargs.h,v 1.7 93/05/07 18:10:36 torek Exp $  */
 end_comment
 
 begin_ifndef
@@ -14,6 +14,14 @@ define|#
 directive|define
 name|_MACHINE_VARARGS_H_
 end_define
+
+begin_typedef
+typedef|typedef
+name|char
+modifier|*
+name|va_list
+typedef|;
+end_typedef
 
 begin_comment
 comment|/* See<machine/stdarg.h> for comments. */
@@ -33,25 +41,33 @@ directive|define
 name|__extension__
 end_define
 
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_typedef
-typedef|typedef
-name|char
-modifier|*
-name|va_list
-typedef|;
-end_typedef
-
 begin_define
 define|#
 directive|define
 name|va_dcl
 value|int va_alist;
 end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_comment
+comment|/* gcc2 wants to see the '...' token */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|va_dcl
+value|int va_alist; ...
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
@@ -66,19 +82,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|va_arg
-parameter_list|(
-name|ap
-parameter_list|,
-name|ty
-parameter_list|)
-define|\
-value|(sizeof(ty) == 8 ? __extension__ ({ \ 	union { ty __d; int __i[2]; } __u; \ 	__u.__i[0] = ((int *)(ap))[0]; \ 	__u.__i[1] = ((int *)(ap))[1]; \ 	(ap) += 8; \ 	__u.__d; }) : \     ((ty *)(ap += sizeof(ty)))[-1])
-end_define
-
-begin_define
-define|#
-directive|define
 name|va_end
 parameter_list|(
 name|ap
@@ -88,6 +91,34 @@ end_define
 begin_comment
 comment|/* empty */
 end_comment
+
+begin_comment
+comment|/* Note, we can assume C code here; C++ does not use<varargs.h>. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__va_8byte
+parameter_list|(
+name|ap
+parameter_list|,
+name|ty
+parameter_list|)
+value|({ \ 	union { ty __d; int __i[2]; } __va_u; \ 	__va_u.__i[0] = ((int *)(void *)(ap))[0]; \ 	__va_u.__i[1] = ((int *)(void *)(ap))[1]; \ 	(ap) += 8; __va_u.__d; })
+end_define
+
+begin_define
+define|#
+directive|define
+name|va_arg
+parameter_list|(
+name|ap
+parameter_list|,
+name|ty
+parameter_list|)
+value|__extension__ ({ \     ty __va_temp; \     __builtin_classify_type(__va_temp)>= 12 ? \ 	((ty **)(void *)((ap) += sizeof(ty *)))[-1][0] : \     sizeof(ty) == 8 ? __va_8byte(ap, ty) : \ 	((ty *)(void *)(ap += sizeof(ty)))[-1]; })
+end_define
 
 begin_endif
 endif|#
