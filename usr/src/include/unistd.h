0b9362c73a648f95e2e79f1254a98d260de6358f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-07 16:39:25 -0800

    make the sys_ arrays all `const char *const []' (for consistency, and for compatibility with GNU/Cygnus)
    SCCS-vsn: include/unistd.h 5.30
    
    Former-commit-id: dcd24df64d5f1cec58d077cd259d9ab3cb79fe45

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.29 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	5.30 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -1263,6 +1263,7 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|extern
+specifier|const
 name|char
 modifier|*
 specifier|const
