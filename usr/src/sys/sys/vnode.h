ea57a5158c3095f13171db30f7570daafae78811
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-07 21:52:59 -0800

    no need to include endian.h as it is brought in by types.h
    SCCS-vsn: sys/sys/vnode.h 7.69
    
    Former-commit-id: a4d895d88043082ae2f3dbceafc49a64c2392cd1

--- usr/src/sys/sys/vnode.h
+++ usr/src/sys/sys/vnode.h
@@ -1,24 +1,7 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	7.68 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	7.69 (Berkeley) %G%  */
 end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|KERNEL
-end_ifndef
-
-begin_include
-include|#
-directive|include
-file|<machine/endian.h>
-end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
