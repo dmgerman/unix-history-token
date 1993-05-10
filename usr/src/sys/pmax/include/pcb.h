4c32ce9b7ca9bab152b58a84e9327bc6f5439a05
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-05-09 23:09:40 -0800

    new improved pmap changes
    SCCS-vsn: sys/pmax/include/pcb.h 7.3
    
    Former-commit-id: 56b8ee56ea937af50b31f67814517a2f1320a11f

--- usr/src/sys/pmax/include/pcb.h
+++ usr/src/sys/pmax/include/pcb.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: pcb.h 1.13 89/04/23$  *  *	@(#)pcb.h	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: pcb.h 1.13 89/04/23$  *  *	@(#)pcb.h	7.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -26,6 +26,11 @@ name|int
 name|pcb_onfault
 decl_stmt|;
 comment|/* for copyin/copyout faults */
+name|void
+modifier|*
+name|pcb_segtab
+decl_stmt|;
+comment|/* copy of pmap pm_segtab */
 block|}
 struct|;
 end_struct
