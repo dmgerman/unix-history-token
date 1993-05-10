c85b885674657d673ddf59eaa1dd582d6ffc55c0
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-05-10 00:48:05 -0800

    delete extra space
    SCCS-vsn: sys/pmax/pmax/autoconf.c 7.11
    
    Former-commit-id: 75df866afae2de3c6fd868f206021ff8e5f92afb

--- usr/src/sys/pmax/pmax/autoconf.c
+++ usr/src/sys/pmax/pmax/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: autoconf.c 1.31 91/01/21$  *  *	@(#)autoconf.c	7.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: autoconf.c 1.31 91/01/21$  *  *	@(#)autoconf.c	7.11 (Berkeley) %G%  */
 end_comment
 
 begin_comment
