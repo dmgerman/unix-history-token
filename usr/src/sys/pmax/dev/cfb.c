dc151240e253088f5bb615f09d69d2f9d9b7ad20
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-05-09 22:50:36 -0800

    fix cursor bit order
    SCCS-vsn: sys/pmax/dev/cfb.c 7.9
    
    Former-commit-id: e835d1be7d3d26f71362ce8f5283aff25ffb45ab

--- usr/src/sys/pmax/dev/cfb.c
+++ usr/src/sys/pmax/dev/cfb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)cfb.c	7.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)cfb.c	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1545,9 +1545,9 @@ name|i
 operator|++
 control|)
 block|{
-ifdef|#
-directive|ifdef
-name|CURSOR_EL
+ifndef|#
+directive|ifndef
+name|CURSOR_EB
 name|out
 operator|=
 operator|(
