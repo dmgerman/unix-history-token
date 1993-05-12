c2f3de641eff73ef025340542c681de9920db27b
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-05-12 04:04:22 -0800

    add luna68k define
    SCCS-vsn: sys/kern/kern_exit.c 7.53
    SCCS-vsn: sys/kern/sys_process.c 7.37
    
    Former-commit-id: 2c82d65cf175a66741bec66809c8289b9024040b

--- usr/src/sys/kern/kern_exit.c
+++ usr/src/sys/kern/kern_exit.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_exit.c	7.52 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_exit.c	7.53 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -989,11 +989,19 @@ directive|ifdef
 name|COMPAT_43
 end_ifdef
 
-begin_ifdef
-ifdef|#
-directive|ifdef
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
 name|hp300
-end_ifdef
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|luna68k
+argument_list|)
+end_if
 
 begin_include
 include|#
