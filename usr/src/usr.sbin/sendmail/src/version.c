8c4684e617f4594eff661e234713836c2a789bb3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-14 00:38:05 -0800

    fix nasty bug that causes core dumps on fallback MXes
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.59
    
    Former-commit-id: 1cbf8c960c3564668907411927549b8838e22c34

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.58 (Berkeley) %G%"
+literal|"@(#)version.c	6.59 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.58"
+literal|"ALPHA-6.59"
 decl_stmt|;
 end_decl_stmt
 
