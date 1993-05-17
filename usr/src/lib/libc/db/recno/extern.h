bbf87ddf3948969f91d4122ca98c390cecb4df55
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-16 22:44:46 -0800

    sync now takes a flags argument
    SCCS-vsn: lib/libc/db/recno/extern.h 5.4
    
    Former-commit-id: e29bb4a6d5323b5e1e3587989d279e14ef23e986

--- usr/src/lib/libc/db/recno/extern.h
+++ usr/src/lib/libc/db/recno/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	5.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -245,6 +245,8 @@ operator|(
 specifier|const
 name|DB
 operator|*
+operator|,
+name|u_int
 operator|)
 argument_list|)
 decl_stmt|;
