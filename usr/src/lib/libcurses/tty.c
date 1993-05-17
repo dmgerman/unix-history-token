f4c1a05db7c6e6cce8a02fc15cc5f5b1896e618f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-17 04:40:45 -0800

    messed up the translation, though
    SCCS-vsn: lib/libcurses/tty.c 5.13
    
    Former-commit-id: 36e00b1fe33342ffe01a4313514ca572768021e6

--- usr/src/lib/libcurses/tty.c
+++ usr/src/lib/libcurses/tty.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)tty.c	5.12 (Berkeley) %G%"
+literal|"@(#)tty.c	5.13 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -694,6 +694,8 @@ operator|->
 name|maxy
 operator|-
 literal|1
+argument_list|,
+literal|0
 argument_list|,
 literal|0
 argument_list|)
