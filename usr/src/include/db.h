41522f23d3df2d77e54148786d5611f65206810d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-16 23:09:53 -0800

    add psize to recno, passed to btree
    SCCS-vsn: include/db.h 5.24
    
    Former-commit-id: 40113cc2164b89b65573cb94c2846649a6534a60

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.23 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)db.h	5.24 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -598,6 +598,10 @@ name|int
 name|cachesize
 decl_stmt|;
 comment|/* bytes to cache */
+name|int
+name|psize
+decl_stmt|;
+comment|/* page size */
 name|int
 name|lorder
 decl_stmt|;
