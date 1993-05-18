d93b5922e7e6a4645a073beb3c661b2cbc1aa7f4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-17 17:45:02 -0800

    clean up function prototypes
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.65
    
    Former-commit-id: 6ffc824334f2a21a39468294712ffd9925717715

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.64 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.65 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.64		%G%"
+literal|"@(#)sendmail.h	6.65		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -4676,6 +4676,7 @@ parameter_list|(
 name|ENVELOPE
 modifier|*
 parameter_list|,
+specifier|const
 name|char
 modifier|*
 parameter_list|,
@@ -4689,6 +4690,7 @@ specifier|extern
 name|void
 name|syserr
 parameter_list|(
+specifier|const
 name|char
 modifier|*
 parameter_list|,
@@ -4702,6 +4704,7 @@ specifier|extern
 name|void
 name|usrerr
 parameter_list|(
+specifier|const
 name|char
 modifier|*
 parameter_list|,
@@ -4715,6 +4718,7 @@ specifier|extern
 name|void
 name|message
 parameter_list|(
+specifier|const
 name|char
 modifier|*
 parameter_list|,
@@ -4728,6 +4732,7 @@ specifier|extern
 name|void
 name|nmessage
 parameter_list|(
+specifier|const
 name|char
 modifier|*
 parameter_list|,
