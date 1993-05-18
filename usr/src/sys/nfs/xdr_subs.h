dac9077ce146107ce1df05bed53af15b618eb2ed
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-17 21:10:27 -0800

    update from Torek/Macklem for machines requiring quad alignment
    SCCS-vsn: sys/nfs/xdr_subs.h 7.7
    
    Former-commit-id: db830462db6a64617f2e474919b25c4ec4810677

--- usr/src/sys/nfs/xdr_subs.h
+++ usr/src/sys/nfs/xdr_subs.h
@@ -1,19 +1,11 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)xdr_subs.h	7.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)xdr_subs.h	7.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
-comment|/*  * Macros used for conversion to/from xdr representation by nfs...  * These use the MACHINE DEPENDENT routines ntohl, htonl  * As defined by "XDR: External Data Representation Standard" RFC1014  */
+comment|/*  * Macros used for conversion to/from xdr representation by nfs...  * These use the MACHINE DEPENDENT routines ntohl, htonl  * As defined by "XDR: External Data Representation Standard" RFC1014  *  * To simplify the implementation, we use ntohl/htonl even on big-endian  * machines, and count on them being `#define'd away.  Some of these  * might be slightly more efficient as quad_t copies on a big-endian,  * but we cannot count on their alignment anyway.  */
 end_comment
-
-begin_if
-if|#
-directive|if
-name|BYTE_ORDER
-operator|==
-name|LITTLE_ENDIAN
-end_if
 
 begin_define
 define|#
@@ -30,80 +22,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|fxdr_nfstime
-parameter_list|(
-name|f
-parameter_list|,
-name|t
-parameter_list|)
-value|{ \ 	(t)->ts_sec = \ 		ntohl(((struct nfsv2_time *)(f))->nfs_sec); \ 	(t)->ts_nsec = 1000 * \ 		ntohl(((struct nfsv2_time *)(f))->nfs_usec); \ }
-end_define
-
-begin_define
-define|#
-directive|define
-name|fxdr_nqtime
-parameter_list|(
-name|f
-parameter_list|,
-name|t
-parameter_list|)
-value|{ \ 	(t)->ts_sec = \ 		ntohl(((struct nqnfs_time *)(f))->nq_sec); \ 	(t)->ts_nsec = \ 		ntohl(((struct nqnfs_time *)(f))->nq_nsec); \ }
-end_define
-
-begin_comment
-comment|/*  * To handle quad conversions, define a struct of two longs and use  * ntohl and htonl. Maybe someday there should be ntohq and htonq?  */
-end_comment
-
-begin_union
-union|union
-name|_hq
-block|{
-name|quad_t
-name|hq
-decl_stmt|;
-struct|struct
-block|{
-name|long
-name|val
-index|[
-literal|2
-index|]
-decl_stmt|;
-block|}
-name|lq
-struct|;
-block|}
-union|;
-end_union
-
-begin_define
-define|#
-directive|define
-name|fxdr_hyper
-parameter_list|(
-name|f
-parameter_list|,
-name|t
-parameter_list|)
-value|{ \ 	((union _hq *)(t))->lq.val[1] = ntohl(((union _hq *)(f))->lq.val[0]); \ 	((union _hq *)(t))->lq.val[0] = ntohl(((union _hq *)(f))->lq.val[1]); \ }
-end_define
-
-begin_define
-define|#
-directive|define
-name|txdr_hyper
-parameter_list|(
-name|f
-parameter_list|,
-name|t
-parameter_list|)
-value|{ \ 	((union _hq *)(t))->lq.val[0] = htonl(((union _hq *)(f))->lq.val[1]); \ 	((union _hq *)(t))->lq.val[1] = htonl(((union _hq *)(f))->lq.val[0]); \ }
-end_define
-
-begin_define
-define|#
-directive|define
 name|txdr_unsigned
 parameter_list|(
 name|v
@@ -114,13 +32,13 @@ end_define
 begin_define
 define|#
 directive|define
-name|txdr_nqtime
+name|fxdr_nfstime
 parameter_list|(
 name|f
 parameter_list|,
 name|t
 parameter_list|)
-value|{ \ 	((struct nqnfs_time *)(t))->nq_sec = \ 		htonl((f)->ts_sec); \ 	((struct nqnfs_time *)(t))->nq_nsec = \ 		htonl((f)->ts_nsec); \ }
+value|{ \ 	(t)->ts_sec = ntohl(((struct nfsv2_time *)(f))->nfs_sec); \ 	(t)->ts_nsec = 1000 * ntohl(((struct nfsv2_time *)(f))->nfs_usec); \ }
 end_define
 
 begin_define
@@ -132,28 +50,7 @@ name|f
 parameter_list|,
 name|t
 parameter_list|)
-value|{ \ 	((struct nfsv2_time *)(t))->nfs_sec = \ 		htonl((f)->ts_sec); \ 	((struct nfsv2_time *)(t))->nfs_usec = \ 		htonl((f)->ts_nsec) / 1000; \ }
-end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_comment
-comment|/* BIG_ENDIAN */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|fxdr_unsigned
-parameter_list|(
-name|t
-parameter_list|,
-name|v
-parameter_list|)
-value|((t)(v))
+value|{ \ 	((struct nfsv2_time *)(t))->nfs_sec = htonl((f)->ts_sec); \ 	((struct nfsv2_time *)(t))->nfs_usec = htonl((f)->ts_nsec) / 1000; \ }
 end_define
 
 begin_define
@@ -165,43 +62,7 @@ name|f
 parameter_list|,
 name|t
 parameter_list|)
-define|\
-value|*(t) = *((struct timespec *)(f))
-end_define
-
-begin_define
-define|#
-directive|define
-name|fxdr_nfstime
-parameter_list|(
-name|f
-parameter_list|,
-name|t
-parameter_list|)
-value|{ \ 	(t)->ts_sec = ((struct nfsv2_time *)(f))->nfs_sec; \ 	(t)->ts_nsec = ((struct nfsv2_time *)(f))->nfs_usec * 1000; \ }
-end_define
-
-begin_define
-define|#
-directive|define
-name|fxdr_hyper
-parameter_list|(
-name|f
-parameter_list|,
-name|t
-parameter_list|)
-define|\
-value|*((quad_t *)(t)) = *((quad_t *)(f))
-end_define
-
-begin_define
-define|#
-directive|define
-name|txdr_unsigned
-parameter_list|(
-name|v
-parameter_list|)
-value|((long)(v))
+value|{ \ 	(t)->ts_sec = ntohl(((struct nqnfs_time *)(f))->nq_sec); \ 	(t)->ts_nsec = ntohl(((struct nqnfs_time *)(f))->nq_nsec); \ }
 end_define
 
 begin_define
@@ -213,20 +74,19 @@ name|f
 parameter_list|,
 name|t
 parameter_list|)
-define|\
-value|*(t) = *((struct nqnfs_time *)(f))
+value|{ \ 	((struct nqnfs_time *)(t))->nq_sec = htonl((f)->ts_sec); \ 	((struct nqnfs_time *)(t))->nq_nsec = htonl((f)->ts_nsec); \ }
 end_define
 
 begin_define
 define|#
 directive|define
-name|txdr_nfstime
+name|fxdr_hyper
 parameter_list|(
 name|f
 parameter_list|,
 name|t
 parameter_list|)
-value|{ \ 	((struct nfsv2_time *)(t))->nfs_sec = (f)->ts_sec; \ 	((struct nfsv2_time *)(t))->nfs_usec = (f)->ts_nsec / 1000; \ }
+value|{ \ 	((long *)(t))[_QUAD_HIGHWORD] = ntohl(((long *)(f))[0]); \ 	((long *)(t))[_QUAD_LOWWORD] = ntohl(((long *)(f))[1]); \ }
 end_define
 
 begin_define
@@ -238,18 +98,8 @@ name|f
 parameter_list|,
 name|t
 parameter_list|)
-define|\
-value|*((quad_t *)(t)) = *((quad_t *)(f))
+value|{ \ 	((long *)(t))[0] = htonl(((long *)(f))[_QUAD_HIGHWORD]); \ 	((long *)(t))[1] = htonl(((long *)(f))[_QUAD_LOWWORD]); \ }
 end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* ENDIAN */
-end_comment
 
 end_unit
 
