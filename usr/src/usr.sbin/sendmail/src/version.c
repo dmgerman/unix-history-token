357bf5b3eb2d2094ac6fee8c7287ff4b7b10214e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-22 16:42:50 -0800

    merge alias database with maps, expanding and changing interface; announce mailer/host/user triple on -bv flag; don't send unsolicited multiline HELO replies -- it breaks Pony Express; AIX portability changes
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.60
    
    Former-commit-id: d8dbeb7d796ac52213fc518bfdac8be6d64c557a

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	6.59 (Berkeley) %G%"
+literal|"@(#)version.c	6.60 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"ALPHA-6.59"
+literal|"ALPHA-6.60"
 decl_stmt|;
 end_decl_stmt
 
