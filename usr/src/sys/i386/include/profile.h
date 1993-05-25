79df907b2e505ad7490e668dcefa61ec9f991b30
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-25 07:12:03 -0800

    lint
    SCCS-vsn: sys/i386/include/profile.h 7.2
    
    Former-commit-id: 18a20fd5e5b5794bcb49177777b00f4e19403f53

--- usr/src/sys/i386/include/profile.h
+++ usr/src/sys/i386/include/profile.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)profile.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)profile.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -19,7 +19,7 @@ value|extern void mcount() asm("mcount"); void mcount() { \ 	int selfpc, frompci
 comment|/* \ 	 * find the return address for mcount, \ 	 * and the return address for mcount's caller. \ 	 * \ 	 * selfpc = pc pushed by mcount call \ 	 */
 value|\ 	asm("movl 4(%%ebp),%0" : "=r" (selfpc)); \
 comment|/* \ 	 * frompcindex = pc pushed by jsr into self. \ 	 * In GCC the caller's stack frame has already been built so we \ 	 * have to chase a6 to find caller's raddr. \ 	 */
-value|\ 	asm("movl (%%ebp),%0" : "=r" (frompcindex)); \ 	frompcindex = ((unsigned short **)frompcindex)[1]; \ 	_mcount(frompcindex, selfpc); \ }
+value|\ 	asm("movl (%%ebp),%0" : "=r" (frompcindex)); \ 	frompcindex = ((int *)frompcindex)[1]; \ 	_mcount(frompcindex, selfpc); \ }
 end_define
 
 end_unit
