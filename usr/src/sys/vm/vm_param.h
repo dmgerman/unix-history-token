481b7a8fddb96987c8a882f39b5fa87b5ba8d59f
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-25 06:15:35 -0800

    pedantically ANSI, i.e., nothing after #else/#endif
    SCCS-vsn: sys/vm/vm_map.h 7.6
    SCCS-vsn: sys/vm/vm_object.h 7.9
    SCCS-vsn: sys/vm/vm_inherit.h 7.4
    SCCS-vsn: sys/vm/vm_prot.h 7.3
    SCCS-vsn: sys/vm/vm_param.h 7.9
    
    Former-commit-id: 432735edb0d1a8f3f29178ed9d0925178636b959

--- usr/src/sys/vm/vm_param.h
+++ usr/src/sys/vm/vm_param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_param.h	7.8 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_param.h	7.9 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -234,17 +234,11 @@ name|KERN_NO_ACCESS
 value|8
 end_define
 
-begin_ifdef
-ifdef|#
-directive|ifdef
+begin_ifndef
+ifndef|#
+directive|ifndef
 name|ASSEMBLER
-end_ifdef
-
-begin_else
-else|#
-directive|else
-else|ASSEMBLER
-end_else
+end_ifndef
 
 begin_comment
 comment|/*  *	Convert addresses to pages and vice versa.  *	No rounding is used.  */
@@ -276,21 +270,9 @@ parameter_list|)
 value|((vm_offset_t)((x)<< PAGE_SHIFT))
 end_define
 
-begin_endif
-endif|#
-directive|endif
-endif|KERNEL
-end_endif
-
 begin_comment
-comment|/*  *	Round off or truncate to the nearest page.  These will work  *	for either addresses or counts.  (i.e. 1 byte rounds to 1 page  *	bytes.  */
+comment|/*  * Round off or truncate to the nearest page.  These will work  * for either addresses or counts (i.e., 1 byte rounds to 1 page).  */
 end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|KERNEL
-end_ifdef
 
 begin_define
 define|#
@@ -325,46 +307,6 @@ define|\
 value|((vm_offset_t)((((vm_offset_t)(x)) + PAGE_MASK)>> PAGE_SHIFT))
 end_define
 
-begin_else
-else|#
-directive|else
-else|KERNEL
-end_else
-
-begin_define
-define|#
-directive|define
-name|round_page
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|((((vm_offset_t)(x) + (vm_page_size - 1)) / vm_page_size) * vm_page_size)
-end_define
-
-begin_define
-define|#
-directive|define
-name|trunc_page
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|((((vm_offset_t)(x)) / vm_page_size) * vm_page_size)
-end_define
-
-begin_endif
-endif|#
-directive|endif
-endif|KERNEL
-end_endif
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|KERNEL
-end_ifdef
-
 begin_decl_stmt
 specifier|extern
 name|vm_size_t
@@ -398,23 +340,63 @@ begin_comment
 comment|/* last physical page */
 end_comment
 
-begin_endif
-endif|#
-directive|endif
-endif|KERNEL
-end_endif
+begin_else
+else|#
+directive|else
+end_else
+
+begin_comment
+comment|/* out-of-kernel versions of round_page and trunc_page */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|round_page
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|((((vm_offset_t)(x) + (vm_page_size - 1)) / vm_page_size) * vm_page_size)
+end_define
+
+begin_define
+define|#
+directive|define
+name|trunc_page
+parameter_list|(
+name|x
+parameter_list|)
+define|\
+value|((((vm_offset_t)(x)) / vm_page_size) * vm_page_size)
+end_define
 
 begin_endif
 endif|#
 directive|endif
-endif|ASSEMBLER
 end_endif
+
+begin_comment
+comment|/* KERNEL */
+end_comment
 
 begin_endif
 endif|#
 directive|endif
-endif|_VM_PARAM_
 end_endif
+
+begin_comment
+comment|/* ASSEMBLER */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _VM_PARAM_ */
+end_comment
 
 end_unit
 
