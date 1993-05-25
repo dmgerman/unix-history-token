dc9284ea7349a34ae0c1f2c39b25bf86d0d703d7
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-25 09:55:03 -0800

    raise maximum memory size to 64M
    SCCS-vsn: sys/sparc/include/vmparam.h 7.4
    
    Former-commit-id: 7e210f01b0d32958421dc7b855b6b25627978443

--- usr/src/sys/sparc/include/vmparam.h
+++ usr/src/sys/sparc/include/vmparam.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)vmparam.h	7.3 (Berkeley) %G%  *  * from: $Header: vmparam.h,v 1.7 92/11/26 02:04:49 torek Exp $  */
+comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)vmparam.h	7.4 (Berkeley) %G%  *  * from: $Header: vmparam.h,v 1.8 93/05/25 09:52:16 torek Exp $  */
 end_comment
 
 begin_comment
@@ -47,7 +47,7 @@ begin_define
 define|#
 directive|define
 name|MAXTSIZ
-value|(6*1024*1024)
+value|(8*1024*1024)
 end_define
 
 begin_comment
@@ -69,7 +69,7 @@ begin_define
 define|#
 directive|define
 name|DFLDSIZ
-value|(8*1024*1024)
+value|(16*1024*1024)
 end_define
 
 begin_comment
@@ -91,7 +91,7 @@ begin_define
 define|#
 directive|define
 name|MAXDSIZ
-value|(16*1024*1024)
+value|(64*1024*1024)
 end_define
 
 begin_comment
