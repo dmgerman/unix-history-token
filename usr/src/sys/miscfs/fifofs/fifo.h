27280599d7c10b8bb48e97dcbc716efc8b5d43ff
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-25 22:32:19 -0800

    add fifo_pathconf
    SCCS-vsn: sys/miscfs/fifofs/fifo.h 7.8
    SCCS-vsn: sys/miscfs/fifofs/fifo_vnops.c 7.18
    
    Former-commit-id: 005992ce8ddc2ea08b0d16142d721590822e3c3f

--- usr/src/sys/miscfs/fifofs/fifo.h
+++ usr/src/sys/miscfs/fifofs/fifo.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -323,6 +323,20 @@ directive|define
 name|fifo_islocked
 value|((int (*) __P((struct  vop_islocked_args *)))nullop)
 end_define
+
+begin_decl_stmt
+name|int
+name|fifo_pathconf
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vop_pathconf_args
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|int
