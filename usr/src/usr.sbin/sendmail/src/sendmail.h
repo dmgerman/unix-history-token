4ab7989488a16ab4dd68a1043b743545e51fa0ac
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-28 04:48:23 -0800

    fix stabapply prototype
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.68
    
    Former-commit-id: 56d9b6dc97b0d7422278d7548c574624fe9b2530

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.67 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	6.68 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	6.67		%G%"
+literal|"@(#)sendmail.h	6.68		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2909,6 +2909,8 @@ argument_list|)
 argument_list|(
 name|STAB
 operator|*
+argument_list|,
+name|int
 argument_list|)
 operator|)
 argument_list|)
