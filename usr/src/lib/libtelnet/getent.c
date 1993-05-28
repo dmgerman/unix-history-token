4dba26405cea61141440c8dc3806fafe59241a5c
Dave A. Borman <dab@ucbvax.Berkeley.EDU>
1993-05-27 19:50:15 -0800

    Change libtelnet so that instead of picking up the gettytab.c module from getty, it now uses its own version of getent() and getstr(), that use the newer library routines cgetent() and cgetstr().
    SCCS-vsn: lib/libtelnet/Makefile 5.8
    SCCS-vsn: lib/libtelnet/getent.c 5.4
    
    Former-commit-id: cc973405954b610b605f6116171e92f4f055795a

--- usr/src/lib/libtelnet/getent.c
+++ usr/src/lib/libtelnet/getent.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)getent.c	5.3 (Berkeley) %G%"
+literal|"@(#)getent.c	5.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -27,6 +27,14 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_decl_stmt
+specifier|static
+name|char
+modifier|*
+name|area
+decl_stmt|;
+end_decl_stmt
 
 begin_comment
 comment|/*ARGSUSED*/
@@ -53,8 +61,45 @@ end_decl_stmt
 
 begin_block
 block|{
+name|char
+modifier|*
+name|dba
+index|[
+literal|2
+index|]
+decl_stmt|;
+name|dba
+index|[
+literal|0
+index|]
+operator|=
+literal|"/etc/gettytab"
+expr_stmt|;
+name|dba
+index|[
+literal|1
+index|]
+operator|=
+literal|0
+expr_stmt|;
 return|return
 operator|(
+operator|(
+name|cgetent
+argument_list|(
+operator|&
+name|area
+argument_list|,
+name|dba
+argument_list|,
+name|name
+argument_list|)
+operator|==
+literal|0
+operator|)
+condition|?
+literal|1
+else|:
 literal|0
 operator|)
 return|;
@@ -76,13 +121,13 @@ name|char
 modifier|*
 name|getstr
 parameter_list|(
-name|cp
+name|id
 parameter_list|,
 name|cpp
 parameter_list|)
 name|char
 modifier|*
-name|cp
+name|id
 decl_stmt|,
 decl|*
 modifier|*
@@ -92,8 +137,28 @@ end_function
 
 begin_block
 block|{
+name|char
+modifier|*
+name|answer
+decl_stmt|;
 return|return
 operator|(
+operator|(
+name|cgetstr
+argument_list|(
+name|area
+argument_list|,
+name|id
+argument_list|,
+operator|&
+name|answer
+argument_list|)
+operator|>
+literal|0
+operator|)
+condition|?
+name|answer
+else|:
 literal|0
 operator|)
 return|;
