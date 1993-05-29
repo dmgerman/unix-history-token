0e251012fc4b962e652201c1c6d372a30e78d1f9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-29 05:44:11 -0800

    make pid_t signed for safety's sake!
    SCCS-vsn: sys/sys/types.h 7.30
    
    Former-commit-id: 63ce114628720382d304a83f5e74971536b51bce

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.29 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	7.30 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -229,7 +229,6 @@ end_comment
 
 begin_typedef
 typedef|typedef
-name|unsigned
 name|long
 name|pid_t
 typedef|;
