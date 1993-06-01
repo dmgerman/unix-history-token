b5b9a7557ccbb47e9fa8748aedf3b91081c74944
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:31:48 -0800

    add an SCCS/copyright header
    SCCS-vsn: bin/sh/histedit.c 5.2
    
    Former-commit-id: e78dc8e9375348bb6ab0b545addea87b4ff9f907

--- usr/src/bin/sh/histedit.c
+++ usr/src/bin/sh/histedit.c
@@ -1,5 +1,34 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+specifier|static
+name|char
+name|sccsid
+index|[]
+init|=
+literal|"@(#)histedit.c	5.2 (Berkeley) %G%"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
+
+begin_comment
 comment|/*  * Editline and history functions (and glue).  */
 end_comment
 
