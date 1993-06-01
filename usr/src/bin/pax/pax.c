859ba07ce8c9c99987c4a3e2615da2c9c0bc92db
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 21:53:46 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: bin/pax/ar_io.c 8.1
    SCCS-vsn: bin/pax/ar_subs.c 8.1
    SCCS-vsn: bin/pax/buf_subs.c 8.1
    SCCS-vsn: bin/pax/cache.c 8.1
    SCCS-vsn: bin/pax/cache.h 8.1
    SCCS-vsn: bin/pax/cpio.c 8.1
    SCCS-vsn: bin/pax/cpio.h 8.1
    SCCS-vsn: bin/pax/extern.h 8.1
    SCCS-vsn: bin/pax/file_subs.c 8.1
    SCCS-vsn: bin/pax/ftree.c 8.1
    SCCS-vsn: bin/pax/ftree.h 8.1
    SCCS-vsn: bin/pax/gen_subs.c 8.1
    SCCS-vsn: bin/pax/options.c 8.1
    SCCS-vsn: bin/pax/options.h 8.1
    SCCS-vsn: bin/pax/pat_rep.c 8.1
    SCCS-vsn: bin/pax/pat_rep.h 8.1
    SCCS-vsn: bin/pax/pax.1 8.1
    SCCS-vsn: bin/pax/pax.c 8.1
    SCCS-vsn: bin/pax/pax.h 8.1
    SCCS-vsn: bin/pax/sel_subs.c 8.1
    SCCS-vsn: bin/pax/sel_subs.h 8.1
    SCCS-vsn: bin/pax/tables.c 8.1
    SCCS-vsn: bin/pax/tables.h 8.1
    SCCS-vsn: bin/pax/tar.c 8.1
    SCCS-vsn: bin/pax/tar.h 8.1
    SCCS-vsn: bin/pax/tty_subs.c 8.1
    
    Former-commit-id: 2362579a79839ff935f844ca6f8917e5f95fced8

--- usr/src/bin/pax/pax.c
+++ usr/src/bin/pax/pax.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Keith Muller.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1992 Keith Muller.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1992 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1992, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -40,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)pax.c	1.3 (Berkeley) %G%"
+literal|"@(#)pax.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
