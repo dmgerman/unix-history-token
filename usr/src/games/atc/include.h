eec6849ec9594f08d672b361a2e3e3c6e1ab2827
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 23:04:24 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/atc/def.h 8.1
    SCCS-vsn: games/atc/extern.c 8.1
    SCCS-vsn: games/atc/extern.h 8.1
    SCCS-vsn: games/atc/grammar.y 8.1
    SCCS-vsn: games/atc/graphics.c 8.1
    SCCS-vsn: games/atc/include.h 8.1
    SCCS-vsn: games/atc/input.c 8.1
    SCCS-vsn: games/atc/list.c 8.1
    SCCS-vsn: games/atc/log.c 8.1
    SCCS-vsn: games/atc/main.c 8.1
    SCCS-vsn: games/atc/pathnames.h 8.1
    SCCS-vsn: games/atc/struct.h 8.1
    SCCS-vsn: games/atc/tunable.c 8.1
    SCCS-vsn: games/atc/tunable.h 8.1
    SCCS-vsn: games/atc/update.c 8.1
    SCCS-vsn: games/atc/atc.6 8.1
    
    Former-commit-id: fb523cffcb42005ea8cad684edb45f2fca5e91e6

--- usr/src/games/atc/include.h
+++ usr/src/games/atc/include.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ed James.  *  * %sccs.include.redist.c%  *  *	@(#)include.h	5.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ed James.  *  * %sccs.include.redist.c%  *  *	@(#)include.h	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_comment
