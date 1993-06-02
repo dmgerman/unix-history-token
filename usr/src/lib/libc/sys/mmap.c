a93a45a4cf2960abdc433caa3536fc576bb485d0
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-06-01 23:53:58 -0800

    use __indir to maintain double alignment on stack for 'offset'.
    SCCS-vsn: lib/libc/sys/mmap.c 5.2
    
    Former-commit-id: 7e623527ea3330b3013bba4878089fb3fb18e641

--- usr/src/lib/libc/sys/mmap.c
+++ usr/src/lib/libc/sys/mmap.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mmap.c	5.1 (Berkeley) %G%"
+literal|"@(#)mmap.c	5.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -99,7 +99,7 @@ operator|(
 operator|(
 name|caddr_t
 operator|)
-name|indir
+name|__indir
 argument_list|(
 name|SYS_mmap
 argument_list|,
