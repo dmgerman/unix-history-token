6c63717cde472fed07f47ae8484ab33c35a7ac61
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 23:44:51 -0800

    date and time created 93/06/01 16:44:51 by bostic
    SCCS-vsn: contrib/sort/pathnames.h 5.1
    
    Former-commit-id: b37d9ee6e3dcc3f8f430f890ee6fb0e2d318076b

---
+++ usr/src/contrib/sort/pathnames.h
@@ -0,0 +1,21 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Peter McIlroy.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.1 (Berkeley) %G%  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_PATH_STDIN
+value|"/dev/stdin"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_SORTTMP
+value|"/var/tmp/sort.XXXXXXXX"
+end_define
+
+end_unit
+
