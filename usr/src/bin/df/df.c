6d6453608714cbea247c0aecce03200dd9051a67
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-03 01:01:29 -0800

    getbsize moves to libc
    SCCS-vsn: bin/df/df.c 5.36
    SCCS-vsn: bin/df/Makefile 5.5
    
    Former-commit-id: 52b1e5aa11cf566448d6e81c6fe0937091320fb6

--- usr/src/bin/df/df.c
+++ usr/src/bin/df/df.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)df.c	5.35 (Berkeley) %G%"
+literal|"@(#)df.c	5.36 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -351,23 +351,6 @@ name|void
 operator|*
 operator|,
 name|int
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|char
-modifier|*
-name|getbsize
-name|__P
-argument_list|(
-operator|(
-name|int
-operator|*
-operator|,
-name|long
-operator|*
 operator|)
 argument_list|)
 decl_stmt|;
