bff327de189c0ce9e3337234a38d1cc01d3044a3
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-06-02 16:56:36 -0800

    remove unnecessary include
    SCCS-vsn: sys/luna68k/dev/fb.c 7.4
    
    Former-commit-id: bb9ea96e9711c849592b202b35be6565d0b99a67

--- usr/src/sys/luna68k/dev/fb.c
+++ usr/src/sys/luna68k/dev/fb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1990,1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * from: hp/dev/grf.c		7.13 (Berkeley) 7/12/92  *  *	@(#)fb.c	7.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1990,1992 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * from: hp/dev/grf.c		7.13 (Berkeley) 7/12/92  *  *	@(#)fb.c	7.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -29,12 +29,6 @@ begin_include
 include|#
 directive|include
 file|<luna68k/dev/fbio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|"bmc.h"
 end_include
 
 begin_decl_stmt
