706dcb644a1e885998de854b6966d3581f7df045
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-03 02:24:19 -0800

    add prototypes for mlock and munlock
    SCCS-vsn: sys/sys/mman.h 7.8
    
    Former-commit-id: 4bbda292aacedb37c1445d9fb16492d933a0498d

--- usr/src/sys/sys/mman.h
+++ usr/src/sys/sys/mman.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mman.h	7.7 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mman.h	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -290,6 +290,34 @@ end_decl_stmt
 begin_decl_stmt
 name|int
 name|msync
+name|__P
+argument_list|(
+operator|(
+name|caddr_t
+operator|,
+name|size_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|mlock
+name|__P
+argument_list|(
+operator|(
+name|caddr_t
+operator|,
+name|size_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|munlock
 name|__P
 argument_list|(
 operator|(
