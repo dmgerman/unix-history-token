248da929c725e1eab13eab74aadd0c3eaa21af8f
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1993-06-03 23:40:22 -0800

    get rid of unnecessary includes
    SCCS-vsn: sys/hp300/hp300/in_cksum.c 7.5
    
    Former-commit-id: 13e4dc3305103087c2de5c605f6dfd03c73ce532

--- usr/src/sys/hp300/hp300/in_cksum.c
+++ usr/src/sys/hp300/hp300/in_cksum.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: in_cksum.c 1.1 90/07/09$  *  *	@(#)in_cksum.c	7.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: in_cksum.c 1.1 90/07/09$  *  *	@(#)in_cksum.c	7.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -17,18 +17,6 @@ begin_include
 include|#
 directive|include
 file|<sys/mbuf.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<netinet/in.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<netinet/in_systm.h>
 end_include
 
 begin_function_decl
