9a4a4fa992fa73041178143df6764c2f208df431
Paul Kranenburg <pk@cs.few.eur.nl>
1993-06-04 00:00:00 +0000

    Fix dangling pointer in lockf structure
    >The problem is a dangling pointer left in the lockf structure belonging to
    >the current lock holder. The offending process frees its lock structure
    >after breaking out of sleep() as a result of a signal. Possible fix:
    >scan the list of waiting locks to remove the lock that isn't going to be
    >used.
    
    Unfortunately, the fix that went with it was totally bogus. This one might
    do a better job:
    
    Date: Mon, 26 Apr 1993 17:05:01 GMT
    
    AUTHOR:	Paul Kranenburg (pk@cs.few.eur.nl)
    386BSD-Patchkit: patch00169
    
    Former-commit-id: 940b54ee0a7c97a4be33d9861dabc8af8a94b68c

--- usr/src/sys.386bsd/ufs/ufs_lockf.c
+++ usr/src/sys.386bsd/ufs/ufs_lockf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Scooter Morris at Genentech Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_lockf.c	7.7 (Berkeley) 7/2/91  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Scooter Morris at Genentech Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_lockf.c	7.7 (Berkeley) 7/2/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00169  * --------------------         -----   ----------------------  *  * 04 Jun 93	Paul Kranenburg		Fix dangling pointer in lockf struct  *  */
 end_comment
 
 begin_include
@@ -507,6 +507,69 @@ literal|0
 argument_list|)
 condition|)
 block|{
+comment|/* Don't leave a dangling pointer in block list */
+if|if
+condition|(
+name|lf_getblock
+argument_list|(
+name|lock
+argument_list|)
+operator|==
+name|block
+condition|)
+block|{
+name|struct
+name|lockf
+modifier|*
+modifier|*
+name|prev
+decl_stmt|;
+comment|/* Still there, find us on list */
+name|prev
+operator|=
+operator|&
+name|block
+operator|->
+name|lf_block
+expr_stmt|;
+while|while
+condition|(
+operator|(
+name|block
+operator|=
+name|block
+operator|->
+name|lf_block
+operator|)
+operator|!=
+name|NOLOCKF
+condition|)
+block|{
+if|if
+condition|(
+name|block
+operator|==
+name|lock
+condition|)
+block|{
+operator|*
+name|prev
+operator|=
+name|block
+operator|->
+name|lf_block
+expr_stmt|;
+break|break;
+block|}
+name|prev
+operator|=
+operator|&
+name|block
+operator|->
+name|lf_block
+expr_stmt|;
+block|}
+block|}
 name|free
 argument_list|(
 name|lock
