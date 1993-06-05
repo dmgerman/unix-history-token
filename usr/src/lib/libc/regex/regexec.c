6e28270a817ff2f63a8534740f73a9124d5bed1b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 19:58:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/regex/regexec.c 8.1
    SCCS-vsn: lib/libc/regex/regfree.c 8.1
    SCCS-vsn: lib/libc/regex/utils.h 8.1
    
    Former-commit-id: 72f19babfa1452759df51a45364bc6d9f1de3b72

--- usr/src/lib/libc/regex/regexec.c
+++ usr/src/lib/libc/regex/regexec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Henry Spencer.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Henry Spencer of the University of Toronto.  *  * %sccs.include.redist.c%  *  *	@(#)regexec.c	5.4 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992 Henry Spencer.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Henry Spencer of the University of Toronto.  *  * %sccs.include.redist.c%  *  *	@(#)regexec.c	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_if
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)regexec.c	5.4 (Berkeley) %G%"
+literal|"@(#)regexec.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
