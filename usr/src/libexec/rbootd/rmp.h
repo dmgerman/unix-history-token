981db398ea2fafbeda28745a8331bb41a5086422
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 01:58:44 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/rbootd/bpf.c 8.1
    SCCS-vsn: libexec/rbootd/conf.c 8.1
    SCCS-vsn: libexec/rbootd/defs.h 8.1
    SCCS-vsn: libexec/rbootd/parseconf.c 8.1
    SCCS-vsn: libexec/rbootd/pathnames.h 8.1
    SCCS-vsn: libexec/rbootd/rbootd.8 8.1
    SCCS-vsn: libexec/rexecd/rexecd.8 8.1
    SCCS-vsn: libexec/rbootd/rbootd.c 8.1
    SCCS-vsn: libexec/rexecd/rexecd.c 8.1
    SCCS-vsn: libexec/rbootd/rmp.h 8.1
    
    Former-commit-id: f78b02d31404248849a867a841a57fe824084435

--- usr/src/libexec/rbootd/rmp.h
+++ usr/src/libexec/rbootd/rmp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Regents of the University of California.  * Copyright (c) 1988, 1992 The University of Utah and the Center  *	for Software Science (CSS).  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Center for Software Science of the University of Utah Computer  * Science Department.  CSS requests users of this software to return  * to css-dist@cs.utah.edu any improvements that they make and grant  * CSS redistribution rights.  *  * %sccs.include.redist.c%  *  *	@(#)rmp.h	5.1 (Berkeley) %G%  *  * Utah $Hdr: rmp.h 3.1 92/07/06$  * Author: Jeff Forys, University of Utah CSS  */
+comment|/*  * Copyright (c) 1992 Regents of the University of California.  * Copyright (c) 1988, 1992 The University of Utah and the Center  *	for Software Science (CSS).  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Center for Software Science of the University of Utah Computer  * Science Department.  CSS requests users of this software to return  * to css-dist@cs.utah.edu any improvements that they make and grant  * CSS redistribution rights.  *  * %sccs.include.redist.c%  *  *	@(#)rmp.h	8.1 (Berkeley) %G%  *  * Utah $Hdr: rmp.h 3.1 92/07/06$  * Author: Jeff Forys, University of Utah CSS  */
 end_comment
 
 begin_comment
