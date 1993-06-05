fdddbf8fe7c06d57618d53beaac17036604ee74a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 20:02:56 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/sparc/DEFS.h 8.1
    SCCS-vsn: lib/libc/sparc/SYS.h 8.1
    SCCS-vsn: lib/libc/sparc/sys/Ovfork.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/brk.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/cerror.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/exect.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/fork.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/pipe.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/ptrace.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/reboot.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/sbrk.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/setlogin.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/sigpending.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/sigprocmask.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/sigreturn.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/sigsuspend.s 8.1
    SCCS-vsn: lib/libc/sparc/sys/syscall.s 8.1
    
    Former-commit-id: 13610c37344b5d2dd5011e0ee20e4a992095c121

--- usr/src/lib/libc/sparc/SYS.h
+++ usr/src/lib/libc/sparc/SYS.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)SYS.h	5.3 (Berkeley) %G%  *  * from: $Header: SYS.h,v 1.2 92/07/03 18:57:00 torek Exp $  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * %sccs.include.redist.c%  *  *	@(#)SYS.h	8.1 (Berkeley) %G%  *  * from: $Header: SYS.h,v 1.2 92/07/03 18:57:00 torek Exp $  */
 end_comment
 
 begin_include
