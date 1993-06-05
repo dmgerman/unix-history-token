90ec1e467df33ac78d7641163e3ce5e80a0a851a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:06:11 -0800

    add a Berkeley copyright
    SCCS-vsn: lib/libc/stdlib/strtod.c 5.3
    
    Former-commit-id: d797ed597ec5409ac51ea8946c53a6f3f029893b

--- usr/src/lib/libc/stdlib/strtod.c
+++ usr/src/lib/libc/stdlib/strtod.c
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  */
+end_comment
+
 begin_if
 if|#
 directive|if
@@ -20,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)strtod.c	5.2 (Berkeley) %G%"
+literal|"@(#)strtod.c	5.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
