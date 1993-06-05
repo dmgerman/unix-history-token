dfa56474ed2074b6ec072d0c0e225787c1c778c4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 02:06:06 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/rbootd/rbootd.c 8.1
    SCCS-vsn: libexec/rshd/rshd.8 8.1
    SCCS-vsn: libexec/rshd/rshd.c 8.1
    
    Former-commit-id: cf85527959e51b75748fa66bbe460872f339b868

--- usr/src/libexec/rbootd/rbootd.c
+++ usr/src/libexec/rbootd/rbootd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1992 The University of Utah and the Center  *	for Software Science (CSS).  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Center for Software Science of the University of Utah Computer  * Science Department.  CSS requests users of this software to return  * to css-dist@cs.utah.edu any improvements that they make and grant  * CSS redistribution rights.  *  * %sccs.include.redist.c%  *  *	@(#)rbootd.c	5.4 (Berkeley) %G%  *  * Utah $Hdr: rbootd.c 3.1 92/07/06$  * Author: Jeff Forys, University of Utah CSS  */
+comment|/*  * Copyright (c) 1988, 1992 The University of Utah and the Center  *	for Software Science (CSS).  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Center for Software Science of the University of Utah Computer  * Science Department.  CSS requests users of this software to return  * to css-dist@cs.utah.edu any improvements that they make and grant  * CSS redistribution rights.  *  * %sccs.include.redist.c%  *  *	@(#)rbootd.c	8.1 (Berkeley) %G%  *  * Utah $Hdr: rbootd.c 3.1 92/07/06$  * Author: Jeff Forys, University of Utah CSS  */
 end_comment
 
 begin_ifndef
@@ -10,11 +10,12 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1993 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1992, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -39,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)rbootd.c	5.4 (Berkeley) %G%"
+literal|"@(#)rbootd.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
