7a4fb11f8986a83b1811a442f2f01d4fccf90ba9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 22:20:22 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/string/bcmp.3 8.1
    SCCS-vsn: lib/libc/string/bcmp.c 8.1
    SCCS-vsn: lib/libc/string/bcopy.3 8.1
    SCCS-vsn: lib/libc/string/bcopy.c 8.1
    SCCS-vsn: lib/libc/string/bstring.3 8.1
    SCCS-vsn: lib/libc/string/bzero.3 8.1
    SCCS-vsn: lib/libc/string/ffs.3 8.1
    SCCS-vsn: lib/libc/string/ffs.c 8.1
    SCCS-vsn: lib/libc/string/index.3 8.1
    SCCS-vsn: lib/libc/string/index.c 8.1
    SCCS-vsn: lib/libc/string/memccpy.3 8.1
    SCCS-vsn: lib/libc/string/memccpy.c 8.1
    SCCS-vsn: lib/libc/string/memchr.3 8.1
    SCCS-vsn: lib/libc/string/memchr.c 8.1
    SCCS-vsn: lib/libc/string/memcmp.3 8.1
    SCCS-vsn: lib/libc/string/memcmp.c 8.1
    SCCS-vsn: lib/libc/string/memcpy.3 8.1
    SCCS-vsn: lib/libc/string/memmove.3 8.1
    SCCS-vsn: lib/libc/string/memset.3 8.1
    SCCS-vsn: lib/libc/string/memset.c 8.1
    SCCS-vsn: lib/libc/string/rindex.3 8.1
    SCCS-vsn: lib/libc/string/rindex.c 8.1
    SCCS-vsn: lib/libc/string/strcasecmp.3 8.1
    SCCS-vsn: lib/libc/string/strcasecmp.c 8.1
    SCCS-vsn: lib/libc/string/strcat.3 8.1
    SCCS-vsn: lib/libc/string/strcat.c 8.1
    SCCS-vsn: lib/libc/string/strchr.3 8.1
    SCCS-vsn: lib/libc/string/strcmp.3 8.1
    SCCS-vsn: lib/libc/string/strcmp.c 8.1
    SCCS-vsn: lib/libc/string/strcoll.3 8.1
    SCCS-vsn: lib/libc/string/strcoll.c 8.1
    SCCS-vsn: lib/libc/string/strcpy.3 8.1
    SCCS-vsn: lib/libc/string/strcpy.c 8.1
    SCCS-vsn: lib/libc/string/strcspn.3 8.1
    SCCS-vsn: lib/libc/string/strcspn.c 8.1
    SCCS-vsn: lib/libc/string/strdup.3 8.1
    SCCS-vsn: lib/libc/string/strdup.c 8.1
    SCCS-vsn: lib/libc/string/strerror.3 8.1
    SCCS-vsn: lib/libc/string/strerror.c 8.1
    SCCS-vsn: lib/libc/string/strftime.3 8.1
    SCCS-vsn: lib/libc/string/strftime.c 8.1
    SCCS-vsn: lib/libc/string/string.3 8.1
    SCCS-vsn: lib/libc/string/strlen.3 8.1
    SCCS-vsn: lib/libc/string/strlen.c 8.1
    SCCS-vsn: lib/libc/string/strmode.3 8.1
    SCCS-vsn: lib/libc/string/strmode.c 8.1
    SCCS-vsn: lib/libc/string/strncat.c 8.1
    SCCS-vsn: lib/libc/string/strncmp.c 8.1
    SCCS-vsn: lib/libc/string/strncpy.c 8.1
    SCCS-vsn: lib/libc/string/strpbrk.3 8.1
    SCCS-vsn: lib/libc/string/strpbrk.c 8.1
    SCCS-vsn: lib/libc/string/strrchr.3 8.1
    SCCS-vsn: lib/libc/string/strsep.3 8.1
    SCCS-vsn: lib/libc/string/strsep.c 8.1
    SCCS-vsn: lib/libc/string/strspn.3 8.1
    SCCS-vsn: lib/libc/string/strspn.c 8.1
    SCCS-vsn: lib/libc/string/strstr.3 8.1
    SCCS-vsn: lib/libc/string/strstr.c 8.1
    SCCS-vsn: lib/libc/string/strtok.3 8.1
    SCCS-vsn: lib/libc/string/strtok.c 8.1
    SCCS-vsn: lib/libc/string/strxfrm.3 8.1
    SCCS-vsn: lib/libc/string/strxfrm.c 8.1
    SCCS-vsn: lib/libc/string/swab.3 8.1
    SCCS-vsn: lib/libc/string/swab.c 8.1
    SCCS-vsn: lib/libc/db/btree/bt_close.c 8.1
    SCCS-vsn: lib/libc/db/btree/bt_conv.c 8.1
    SCCS-vsn: lib/libc/db/btree/bt_debug.c 8.1
    SCCS-vsn: lib/libc/db/btree/bt_delete.c 8.1
    SCCS-vsn: lib/libc/db/btree/bt_get.c 8.1
    
    Former-commit-id: 01a7177693207148c67154a6fae4fb53555713bc

--- usr/src/lib/libc/string/swab.c
+++ usr/src/lib/libc/string/swab.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jeffrey Mogul.  *  * %sccs.include.redist.c%  */
+comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jeffrey Mogul.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_if
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)swab.c	5.10 (Berkeley) %G%"
+literal|"@(#)swab.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
