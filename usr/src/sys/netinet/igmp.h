85307fb26488f0df5482e13f379b6c75939d0e1b
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1993-06-05 00:24:25 -0800

    prototype everything
    SCCS-vsn: sys/netinet/igmp.h 7.2
    
    Former-commit-id: ce30e41e852187cebe92f1c807ad50b75ddae0d3

--- usr/src/sys/netinet/igmp.h
+++ usr/src/sys/netinet/igmp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * %sccs.include.redist.c%  *  *	@(#)igmp.h	7.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * %sccs.include.redist.c%  *  *	@(#)igmp.h	7.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -89,10 +89,6 @@ end_define
 
 begin_comment
 comment|/* max delay for response to    */
-end_comment
-
-begin_comment
-comment|/*  query (in seconds)          */
 end_comment
 
 end_unit
