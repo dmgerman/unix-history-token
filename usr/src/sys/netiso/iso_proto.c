a21fcdb2fee5477397310899315881f902b0d20d
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1993-06-05 00:02:40 -0800

    fix #endif something to #endif /* something */
    SCCS-vsn: sys/netiso/iso_proto.c 7.13
    SCCS-vsn: sys/netiso/iso_snpac.h 7.9
    
    Former-commit-id: 97d021fe54cf66441ce408e6b980dbd080b0a323

--- usr/src/sys/netiso/iso_proto.c
+++ usr/src/sys/netiso/iso_proto.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)iso_proto.c	7.12 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)iso_proto.c	7.13 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -606,8 +606,11 @@ end_decl_stmt
 begin_endif
 endif|#
 directive|endif
-endif|ISO
 end_endif
+
+begin_comment
+comment|/* ISO */
+end_comment
 
 end_unit
 
--- usr/src/sys/netiso/iso_snpac.h
+++ usr/src/sys/netiso/iso_snpac.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)iso_snpac.h	7.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)iso_snpac.h	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -211,8 +211,11 @@ end_comment
 begin_endif
 endif|#
 directive|endif
-endif|KERNEL
 end_endif
+
+begin_comment
+comment|/* KERNEL */
+end_comment
 
 end_unit
 
