f7217c4decfcbdfd4b55f5726fa75bbaa9a1f4be
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 17:56:57 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/fastboot/fastboot.sh 8.1
    SCCS-vsn: sbin/fastboot/fasthalt.sh 8.1
    SCCS-vsn: sbin/fsck/dir.c 8.1
    SCCS-vsn: sbin/fsck/fsck.8 8.1
    SCCS-vsn: sbin/fsck/fsck.h 8.1
    SCCS-vsn: sbin/fsck/inode.c 8.1
    SCCS-vsn: sbin/fsck/main.c 8.1
    SCCS-vsn: sbin/fsck/pass1.c 8.1
    SCCS-vsn: sbin/fsck/pass1b.c 8.1
    SCCS-vsn: sbin/fsck/pass2.c 8.1
    SCCS-vsn: sbin/fsck/pass3.c 8.1
    SCCS-vsn: sbin/fsck/pass4.c 8.1
    SCCS-vsn: sbin/fsck/pass5.c 8.1
    SCCS-vsn: sbin/fsck/preen.c 8.1
    SCCS-vsn: sbin/fsck/setup.c 8.1
    SCCS-vsn: sbin/fsck/utilities.c 8.1
    
    Former-commit-id: fe3df01f7cd6c395807b2fbf55c89c8f07d8e6ce

--- usr/src/sbin/fsck/fsck.h
+++ usr/src/sbin/fsck/fsck.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fsck.h	5.20 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fsck.h	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_define
