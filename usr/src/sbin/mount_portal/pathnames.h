c8b551424c8f40923beac691c21c8883f7acd25c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:04:09 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/mount_portal/activate.c 8.1
    SCCS-vsn: sbin/mount_portal/conf.c 8.1
    SCCS-vsn: sbin/mount_portal/mount_portal.8 8.1
    SCCS-vsn: sbin/mount_portal/mount_portal.c 8.1
    SCCS-vsn: sbin/mount_portal/pathnames.h 8.1
    SCCS-vsn: sbin/mount_portal/portald.h 8.1
    SCCS-vsn: sbin/mount_portal/pt_conf.c 8.1
    SCCS-vsn: sbin/mount_portal/pt_exec.c 8.1
    SCCS-vsn: sbin/mount_portal/pt_file.c 8.1
    SCCS-vsn: sbin/mount_portal/pt_tcp.c 8.1
    
    Former-commit-id: b68e79dfb6b3eb71145a88ab193e00bfaf685ea4

--- usr/src/sbin/mount_portal/pathnames.h
+++ usr/src/sbin/mount_portal/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California  * Copyright (c) 1990, 1992 Jan-Simon Pendry  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.1 (Berkeley) %G%  *  * $Id: pathnames.h,v 1.2 1992/05/27 07:09:27 jsp Exp jsp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	8.1 (Berkeley) %G%  *  * $Id: pathnames.h,v 1.2 1992/05/27 07:09:27 jsp Exp jsp $  */
 end_comment
 
 begin_include
