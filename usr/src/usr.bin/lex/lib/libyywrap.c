e5a857f12e1fd51f1f94c73017b38b019a368dfe
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-06-06 00:07:54 -0800

    BSD 4_4 development Work on file usr/src/usr.bin/lex/lib/libmain.c Work on file usr/src/usr.bin/lex/lib/libyywrap.c
    Synthesized-from: CSRG/cd3/4.4
    
    Former-commit-id: 1c8dd5f711991b5c50f0584ff5933d832b284dbf

---
+++ usr/src/usr.bin/lex/lib/libyywrap.c
@@ -0,0 +1,18 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* libyywrap - flex run-time support library "yywrap" function */
+end_comment
+
+begin_function
+name|int
+name|yywrap
+parameter_list|()
+block|{
+return|return
+literal|1
+return|;
+block|}
+end_function
+
+end_unit
+
