780d875b9e3cda3c7d4a613f02fcafe61560d714
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:02:42 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/mkproto/mkproto.8 8.1
    SCCS-vsn: usr.sbin/mkproto/mkproto.c 8.1
    
    Former-commit-id: 314544fb3c03be7c438650c0668649d5c4def75e

--- usr/src/usr.sbin/mkproto/mkproto.c
+++ usr/src/usr.sbin/mkproto/mkproto.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1983 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  */
+comment|/*-  * Copyright (c) 1983, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.proprietary.c%  */
 end_comment
 
 begin_ifndef
@@ -10,11 +10,12 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1983 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1983, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -39,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)mkproto.c	5.10 (Berkeley) %G%"
+literal|"@(#)mkproto.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
