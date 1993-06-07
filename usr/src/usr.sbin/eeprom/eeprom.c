c69ff2c11ebc66af3de4f1cea219627b309e1acf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:57:13 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/eeprom/eeprom.8 8.1
    SCCS-vsn: usr.sbin/eeprom/eeprom.c 8.1
    SCCS-vsn: usr.sbin/eeprom/pathnames.h 8.1
    SCCS-vsn: usr.sbin/inetd/inetd.8 8.1
    
    Former-commit-id: d7ed929fc243f4f99d11876993262637ea43019c

--- usr/src/usr.sbin/eeprom/eeprom.c
+++ usr/src/usr.sbin/eeprom/eeprom.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -10,11 +10,12 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1993 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -39,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)eeprom.c	5.2 (Berkeley) %G%"
+literal|"@(#)eeprom.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
