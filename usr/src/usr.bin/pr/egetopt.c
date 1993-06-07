ccaa611183b874b3955771d76f1581182a5af7d4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:57:09 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pr/egetopt.c 8.1
    SCCS-vsn: usr.bin/pr/extern.h 8.1
    SCCS-vsn: usr.bin/pr/pr.1 8.1
    SCCS-vsn: usr.bin/pr/pr.c 8.1
    SCCS-vsn: usr.bin/pr/pr.h 8.1
    
    Former-commit-id: 374cfc8403a4157743e009c3c5f0af2ea2093d5b

--- usr/src/usr.bin/pr/egetopt.c
+++ usr/src/usr.bin/pr/egetopt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 Keith Muller.  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  */
+comment|/*-  * Copyright (c) 1991 Keith Muller.  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)egetopt.c	5.2 (Berkeley) %G%"
+literal|"@(#)egetopt.c	8.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
