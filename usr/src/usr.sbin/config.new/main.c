ac51bfb76ee1cd1dc1859dc8902ffb66c8eeca72
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:55:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/config.new/config.8 8.1
    SCCS-vsn: usr.sbin/config.new/config.h 8.1
    SCCS-vsn: usr.sbin/config.new/files.c 8.1
    SCCS-vsn: usr.sbin/config.new/gram.y 8.1
    SCCS-vsn: usr.sbin/config.new/hash.c 8.1
    SCCS-vsn: usr.sbin/config.new/main.c 8.1
    SCCS-vsn: usr.sbin/config.new/mkheaders.c 8.1
    SCCS-vsn: usr.sbin/config.new/mkioconf.c 8.1
    SCCS-vsn: usr.sbin/config.new/mkmakefile.c 8.1
    SCCS-vsn: usr.sbin/config.new/mkswap.c 8.1
    SCCS-vsn: usr.sbin/config.new/pack.c 8.1
    SCCS-vsn: usr.sbin/config.new/scan.l 8.1
    SCCS-vsn: usr.sbin/config.new/sem.c 8.1
    SCCS-vsn: usr.sbin/config.new/sem.h 8.1
    SCCS-vsn: usr.sbin/config.new/util.c 8.1
    
    Former-commit-id: 28ce7ac0803c16f62e4e67767b648460f1fcfbd4

--- usr/src/usr.sbin/config.new/main.c
+++ usr/src/usr.sbin/config.new/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratories.  *  * %sccs.include.redist.c%  *  *	@(#)main.c	5.2 (Berkeley) %G%  */
+comment|/*   * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratories.  *  * %sccs.include.redist.c%  *  *	@(#)main.c	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -10,11 +10,12 @@ name|lint
 end_ifndef
 
 begin_decl_stmt
+specifier|static
 name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1992 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1992, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
