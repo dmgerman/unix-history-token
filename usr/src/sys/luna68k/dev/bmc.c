d94267a0b0c6449a28d86ba3e2f76776c6c156a3
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-06-08 17:37:53 -0800

    return code of bmcprob is changed
    SCCS-vsn: sys/luna68k/dev/bmc.c 7.8
    
    Former-commit-id: c984de3d64a406801db5d13909ba17fd845693a3

--- usr/src/sys/luna68k/dev/bmc.c
+++ usr/src/sys/luna68k/dev/bmc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)bmc.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * OMRON Corporation.  *  * %sccs.include.redist.c%  *  *	@(#)bmc.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1510,7 +1510,7 @@ argument_list|)
 expr_stmt|;
 return|return
 operator|(
-literal|0
+literal|1
 operator|)
 return|;
 block|}
