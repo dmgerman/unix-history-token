68a619123e2932f49a68fdaa8606b5a8f0026579
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-06-09 08:08:00 -0800

    BSD 4_4 development Work on file usr/src/old/lisp/franz/h/aout.h Work on file usr/src/old/lisp/franz/h/lispo.h
    Synthesized-from: CSRG/cd3/4.4
    
    Former-commit-id: d51e147ed8081c3c54976e19c10746bc31affb2d

---
+++ usr/src/old/lisp/franz/h/aout.h
@@ -0,0 +1,16 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_expr_stmt
+operator|/
+name|usr
+operator|/
+name|include
+operator|/
+name|a
+operator|.
+name|out
+operator|.
+name|h
+end_expr_stmt
+
+end_unit
+
