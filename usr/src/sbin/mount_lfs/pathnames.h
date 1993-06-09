c0178470aa35490eb0926799cf77246c8483827c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-09 01:48:15 -0800

    date and time created 93/06/08 18:48:15 by bostic
    SCCS-vsn: sbin/mount_lfs/pathnames.h 5.1
    
    Former-commit-id: 4c8c5baefd8892564309290835c98e180bb396ad

---
+++ usr/src/sbin/mount_lfs/pathnames.h
@@ -0,0 +1,14 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	5.1 (Berkeley) %G%  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_PATH_LFS_CLEANERD
+value|"/usr/libexec/lfs_cleanerd"
+end_define
+
+end_unit
+
