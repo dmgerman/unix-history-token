1f324ba014231245674542a31754d86bfc53c68b
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-06-09 20:32:55 -0800

    correct FP reg offsets
    SCCS-vsn: old/adb/adb.hp300/defs.h 5.4
    
    Former-commit-id: 4164ab1995100f205ac3e2409d4f56642b40b7f6

--- usr/src/old/adb/adb.hp300/defs.h
+++ usr/src/old/adb/adb.hp300/defs.h
@@ -693,81 +693,111 @@ name|P1LR
 value|72
 end_define
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|BSD4_4
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|FPBASE
+value|280
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|FPBASE
+value|332
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_define
 define|#
 directive|define
 name|FP0
-value|332
+value|(FPBASE+0*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP1
-value|344
+value|(FPBASE+1*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP2
-value|356
+value|(FPBASE+2*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP3
-value|368
+value|(FPBASE+3*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP4
-value|380
+value|(FPBASE+4*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP5
-value|392
+value|(FPBASE+5*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP6
-value|404
+value|(FPBASE+6*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FP7
-value|416
+value|(FPBASE+7*12)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FPCR
-value|428
+value|(FPBASE+8*12+0)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FPSR
-value|432
+value|(FPBASE+8*12+4)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FPIAR
-value|436
+value|(FPBASE+8*12+8)
 end_define
 
 begin_endif
