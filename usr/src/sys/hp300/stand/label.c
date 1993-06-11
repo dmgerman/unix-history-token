b6e6ffa60d851b6d1e1f7054f564d9b8a786e40c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 04:47:58 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/hp300/stand/ite_subr.c 8.1
    SCCS-vsn: sys/hp300/stand/ite_tc.c 8.1
    SCCS-vsn: sys/hp300/stand/label.c 8.1
    SCCS-vsn: sys/hp300/stand/machdep.c 8.1
    SCCS-vsn: sys/hp300/stand/mkboot.c 8.1
    SCCS-vsn: sys/hp300/stand/nhpib.c 8.1
    SCCS-vsn: sys/hp300/stand/prf.c 8.1
    SCCS-vsn: sys/hp300/stand/rd.c 8.1
    SCCS-vsn: sys/hp300/stand/rominfo.h 8.1
    SCCS-vsn: sys/hp300/stand/samachdep.h 8.1
    SCCS-vsn: sys/hp300/stand/scsi.c 8.1
    SCCS-vsn: sys/hp300/stand/scsivar.h 8.1
    SCCS-vsn: sys/hp300/stand/sd.c 8.1
    SCCS-vsn: sys/hp300/stand/srt0.c 8.1
    SCCS-vsn: sys/hp300/stand/volhdr.h 8.1
    
    Former-commit-id: fdf05dd25b7f27999f4a52357ec54eb1f95d3f66

--- usr/src/sys/hp300/stand/label.c
+++ usr/src/sys/hp300/stand/label.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 University of Utah.  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: label.c 1.1 92/12/05$  *  *	@(#)label.c	7.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 University of Utah.  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: label.c 1.1 92/12/05$  *  *	@(#)label.c	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_comment
--- usr/src/sys/hp300/stand/srt0.c
+++ usr/src/sys/hp300/stand/srt0.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: srt0.c 1.18 92/12/21$  *  *	@(#)srt0.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: srt0.c 1.18 92/12/21$  *  *	@(#)srt0.c	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_comment
