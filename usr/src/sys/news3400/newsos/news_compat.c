846efcec23c9abb8c16eacfaf87565fdc03736af
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 22:04:17 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/news3400/newsos/news_compat.c 8.1
    
    Former-commit-id: 5f372cf33e873ddc0fa10dcfe58debb10a7492b7

--- usr/src/sys/news3400/newsos/news_compat.c
+++ usr/src/sys/news3400/newsos/news_compat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  *	%sccs.include.redist.c%  *  *	@(#)news_compat.c	7.1 (Berkeley) %G%  *  * from: $Hdr: sun_misc.c,v 1.12 92/07/12 13:26:10 torek Exp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  *	%sccs.include.redist.c%  *  *	@(#)news_compat.c	8.1 (Berkeley) %G%  *  * from: $Hdr: sun_misc.c,v 1.12 92/07/12 13:26:10 torek Exp $  */
 end_comment
 
 begin_comment
