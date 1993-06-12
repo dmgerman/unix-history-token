3dd90de7c97bbf109fe2efd7fce423b6249530cd
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-06-12 15:49:42 -0800

    set the version to 8.1B -- 8.1 for 4.4BSD, plus parseaddr.c fix
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.67
    
    Former-commit-id: 88b52b65e9acf6f3bb4936ebc7455a45413d3416

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	8.1 (Berkeley) %G%"
+literal|"@(#)version.c	6.67 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"8.1"
+literal|"8.1B"
 decl_stmt|;
 end_decl_stmt
 
