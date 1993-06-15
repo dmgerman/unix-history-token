9e1bb2251acfc14a6007d45cbaece72a385e2a9e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-14 22:25:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/kern/kern_exit.c 8.1
    SCCS-vsn: sys/vm/vm_mmap.c 8.1
    
    Former-commit-id: dee059f49a48c0aceffb98ae8de4955205028a09

--- usr/src/sys/vm/vm_mmap.c
+++ usr/src/sys/vm/vm_mmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	7.31 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.1 (Berkeley) %G%  */
 end_comment
 
 begin_comment
