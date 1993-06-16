85b35fd9da6f417d03dadd91606617064b35c96d
Nate Willams <nate@FreeBSD.org>
1993-06-16 12:02:15 +0000

    From: Juha Nurmela <Juha.Nurmela@lut.fi> Subject: Select fixes for /dev/klog
    Former-commit-id: 4885bf0561862267fee1e020af875dbf225470e4

--- sys/kern/subr_log.c
+++ sys/kern/subr_log.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_log.c	7.11 (Berkeley) 3/17/91  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_log.c	7.11 (Berkeley) 3/17/91  *  * PATCHES MAGIC		LEVEL	PATCH THAT GOT US HERE  * --------------------		-----	----------------------  * CURRENT PATCH LEVEL:		1	XXXXX  * --------------------		-----	----------------------  *  * 16 Jun 93	Juha Nurmela		select uses pids, not pointers  */
 end_comment
 
 begin_comment
@@ -72,12 +72,10 @@ name|int
 name|sc_state
 decl_stmt|;
 comment|/* see above for possibilities */
-name|struct
-name|proc
-modifier|*
-name|sc_selp
+name|pid_t
+name|sc_sel
 decl_stmt|;
-comment|/* process waiting on select call */
+comment|/* pid of process waiting on select call 16 Jun 93 */
 name|int
 name|sc_pgid
 decl_stmt|;
@@ -262,10 +260,11 @@ literal|0
 expr_stmt|;
 name|logsoftc
 operator|.
-name|sc_selp
+name|sc_sel
 operator|=
 literal|0
 expr_stmt|;
+comment|/* 16 Jun 93 */
 block|}
 end_block
 
@@ -603,10 +602,13 @@ return|;
 block|}
 name|logsoftc
 operator|.
-name|sc_selp
+name|sc_sel
 operator|=
 name|p
+operator|->
+name|p_pid
 expr_stmt|;
+comment|/* 16 Jun 93 */
 break|break;
 block|}
 name|splx
@@ -644,21 +646,22 @@ if|if
 condition|(
 name|logsoftc
 operator|.
-name|sc_selp
+name|sc_sel
 condition|)
 block|{
+comment|/* 16 Jun 93 */
 name|selwakeup
 argument_list|(
 name|logsoftc
 operator|.
-name|sc_selp
+name|sc_sel
 argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
 name|logsoftc
 operator|.
-name|sc_selp
+name|sc_sel
 operator|=
 literal|0
 expr_stmt|;
