44e6020d57b9a64885ec742b64a90340e2acde59
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-16 23:42:39 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/sparc/sparc/trap.c 8.1
    
    Former-commit-id: 3b3726965c50dc1e0e818d3784901f0d7cfda41b

--- usr/src/sys/sparc/sparc/trap.c
+++ usr/src/sys/sparc/sparc/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)trap.c	7.6 (Berkeley) %G%  *  * from: $Header: trap.c,v 1.34 93/05/28 04:34:50 torek Exp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)trap.c	8.1 (Berkeley) %G%  *  * from: $Header: trap.c,v 1.34 93/05/28 04:34:50 torek Exp $  */
 end_comment
 
 begin_include
