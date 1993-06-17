f539563b16bb180e05260d57541cccdc44e0f191
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-17 02:26:56 -0800

    need to add NODEV to end of list
    SCCS-vsn: sys/hp300/hp300/swapgeneric.c 7.9
    
    Former-commit-id: e439a5bf9d6b26ee7295bb7920e3e424bd388dae

--- usr/src/sys/hp300/hp300/swapgeneric.c
+++ usr/src/sys/hp300/hp300/swapgeneric.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)swapgeneric.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)swapgeneric.c	7.9 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -90,7 +90,7 @@ literal|0
 block|}
 block|,
 block|{
-literal|0
+name|NODEV
 block|,
 literal|0
 block|,
