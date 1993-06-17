8537cfbaf441703a0ffc546c07cfcea85c0e3288
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-17 02:31:02 -0800

    have to terminate swapdev list with NODEV
    SCCS-vsn: sys/luna68k/luna68k/swapgeneric.c 7.6
    
    Former-commit-id: b262bf83c9e18cf0aa2f4439c48b7c8f64c18d10

--- usr/src/sys/luna68k/luna68k/swapgeneric.c
+++ usr/src/sys/luna68k/luna68k/swapgeneric.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1982, 1986, 1992 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * from: hp300/hp300/swapgeneric.c	7.8 (Berkeley) 10/11/92  *  *	@(#)swapgeneric.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1982, 1986, 1992 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  * from: hp300/hp300/swapgeneric.c	7.8 (Berkeley) 10/11/92  *  *	@(#)swapgeneric.c	7.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -90,7 +90,7 @@ literal|0
 block|}
 block|,
 block|{
-literal|0
+name|NODEV
 block|,
 literal|0
 block|,
