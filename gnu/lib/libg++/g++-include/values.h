3e22c7235d2c46fed6d88c0c72698a12bef00326
Rick Macklem <root@snowhite.cis.uoguelph.ca>
1993-06-21 12:25:14 +0000

    New libg++
    Former-commit-id: fec9486d76850955a92f970a323867fa8c6e904d

--- gnu/lib/libg++/g++-include/values.h
+++ gnu/lib/libg++/g++-include/values.h
@@ -4,7 +4,7 @@ comment|// This may look like C code, but it is really -*- C++ -*-
 end_comment
 
 begin_comment
-comment|/*  Copyright (C) 1988 Free Software Foundation     written by Doug Lea (dl@rocky.oswego.edu)  This file is part of GNU CC.  GNU CC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.  No author or distributor accepts responsibility to anyone for the consequences of using it or for whether it serves any particular purpose or works at all, unless he says so in writing.  Refer to the GNU CC General Public License for full details.  Everyone is granted permission to copy, modify and redistribute GNU CC, but only under the conditions described in the GNU CC General Public License.   A copy of this license is supposed to have been given to you along with GNU CC so you can know your rights and responsibilities.  It should be in a file named COPYING.  Among other things, the copyright notice and this notice must be preserved on all copies.   */
+comment|/*  Copyright (C) 1988 Free Software Foundation     written by Doug Lea (dl@rocky.oswego.edu)  This file is part of the GNU C++ Library.  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for more details. You should have received a copy of the GNU Library General Public License along with this library; if not, write to the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
 end_comment
 
 begin_ifndef
@@ -12,29 +12,6 @@ ifndef|#
 directive|ifndef
 name|_values_h
 end_ifndef
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__GNUG__
-end_ifdef
-
-begin_pragma
-pragma|#
-directive|pragma
-name|once
-end_pragma
-
-begin_pragma
-pragma|#
-directive|pragma
-name|interface
-end_pragma
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_define
 define|#
@@ -186,6 +163,11 @@ operator|||
 name|defined
 argument_list|(
 name|masscomp
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|sgi
 argument_list|)
 end_if
 
