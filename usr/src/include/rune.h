ef3be5319555f085976c1087a564e8bc2909ed44
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-28 02:24:00 -0800

    new f* functions need stdio.h
    SCCS-vsn: include/rune.h 5.3
    
    Former-commit-id: aedc8531e64c7c19bd4f8825f3d43ca315dea19b

--- usr/src/include/rune.h
+++ usr/src/include/rune.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Paul Borman at Krystal Technologies.  *  * %sccs.include.redist.c%  *  *	@(#)rune.h	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Paul Borman at Krystal Technologies.  *  * %sccs.include.redist.c%  *  *	@(#)rune.h	5.3 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -19,6 +19,12 @@ begin_include
 include|#
 directive|include
 file|<runetype.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<stdio.h>
 end_include
 
 begin_define
