3db145675c4ab1b7c83d59e3c0ba49bd1c57d347
Nate Willams <nate@FreeBSD.org>
1993-06-30 23:25:41 +0000

    Added new 4.4 assert that Chris Torek posted to the net.
    Former-commit-id: d05f271e756002e7db5af00e2a88af5e8bf35420

--- include/assert.h
+++ include/assert.h
@@ -1,19 +1,17 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)assert.h	4.4 (Berkeley) 4/3/91  */
+comment|/*-  * Copyright (c) 1992 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)assert.h	5.2 (Berkeley) 6/18/92  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|_ASSERT_H_
-end_ifndef
+begin_comment
+comment|/*  * Unlike other ANSI header files,<assert.h> may usefully be included  * multiple times, with and without NDEBUG defined.  */
+end_comment
 
-begin_define
-define|#
-directive|define
-name|_ASSERT_H_
-end_define
+begin_undef
+undef|#
+directive|undef
+name|assert
+end_undef
 
 begin_ifdef
 ifdef|#
@@ -25,12 +23,20 @@ begin_define
 define|#
 directive|define
 name|assert
+parameter_list|(
+name|e
+parameter_list|)
+value|((void)0)
 end_define
 
 begin_define
 define|#
 directive|define
 name|_assert
+parameter_list|(
+name|e
+parameter_list|)
+value|((void)0)
 end_define
 
 begin_else
@@ -41,21 +47,46 @@ end_else
 begin_define
 define|#
 directive|define
-name|assert
+name|_assert
 parameter_list|(
-name|expression
+name|e
 parameter_list|)
-value|{ \ 	if (!(expression)) { \ 		(void)fprintf(stderr, \ 		    "assertion \"%s\" failed: file \"%s\", line %d\n", \ 		    "expression", __FILE__, __LINE__); \ 		exit(2); \ 	} \ }
+value|assert(e)
 end_define
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__STDC__
+end_ifdef
 
 begin_define
 define|#
 directive|define
-name|_assert
+name|assert
 parameter_list|(
-name|expression
+name|e
 parameter_list|)
-value|assert(expression)
+value|((e) ? (void)0 : __assert(__FILE__, __LINE__, #e))
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_comment
+comment|/* PCC */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|assert
+parameter_list|(
+name|e
+parameter_list|)
+value|((e) ? (void)0 : __assert(__FILE__, __LINE__, "e"))
 end_define
 
 begin_endif
@@ -68,9 +99,36 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* !_ASSERT_H_ */
-end_comment
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_decl_stmt
+name|__BEGIN_DECLS
+name|void
+name|__assert
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+name|int
+operator|,
+specifier|const
+name|char
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_macro
+name|__END_DECLS
+end_macro
 
 end_unit
 
