a602aa5f65d6c47845a89f7599b2dbfdaf077546
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-16 17:23:05 -0800

    avoid null pointer dereference
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.4
    
    Former-commit-id: d99aa5a3e2b043f3455a664e3a5a2e785e42243f

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)util.c	8.3 (Berkeley) %G%"
+literal|"@(#)util.c	8.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3071,6 +3071,10 @@ decl_stmt|;
 block|{
 if|if
 condition|(
+name|s
+operator|==
+name|NULL
+operator|||
 operator|*
 name|s
 operator|==
