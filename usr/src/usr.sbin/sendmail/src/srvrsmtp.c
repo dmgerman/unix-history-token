2448cefdf13546b169432edb4977df7aa20fbced
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-17 20:46:53 -0800

    detect self-destructing alias loop more generally for better diagnostics
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.4
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.5
    
    Former-commit-id: bf7f280e60b75ed7043f54db6af8a5545a6909c3

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.4 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	8.5 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.4 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	8.5 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
