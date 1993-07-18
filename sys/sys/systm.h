9bcb1f1976cda1e8ea780b77630d45f42205ae9e
Paul Richards <paul@isl.cf.ac.uk>
1993-07-18 20:59:46 +0000

    Added prototypes for kexit,cpu_exit and swtch. Moved warning about returning from kexit() to cpu_exit() but it's still not solved. This may prove impossible.
    Former-commit-id: 662dd4d274dfe9bc814f21e8c00ab42dc38d7d95

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -3,6 +3,54 @@ begin_comment
 comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)systm.h	7.17 (Berkeley) 5/25/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00158  * --------------------         -----   ----------------------  *  * 11 Dec 92	Williams Jolitz		Fixed and tty handling  * 25 Apr 93	Bruce Evans		Support for intr-0.1  * 16 May 93	Rodney W. Grimes	Added prototype for spinwait  *					Disabled prototype for wakeup, timeout  */
 end_comment
 
+begin_comment
+comment|/* Prototypes I needed to fix that kern_exit warning     ---- this really the first step in the work that's           been done on sun-lamp to add kernel function          prototypes.                                 */
+end_comment
+
+begin_decl_stmt
+specifier|volatile
+name|void
+name|kexit
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|__volatile
+name|void
+name|cpu_exit
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|swtch
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_decl_stmt
 specifier|extern
 name|char
@@ -284,21 +332,6 @@ end_define
 begin_comment
 comment|/*  * General function declarations.  */
 end_comment
-
-begin_decl_stmt
-specifier|volatile
-name|void
-name|cpu_exit
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|proc
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
 
 begin_decl_stmt
 name|int
