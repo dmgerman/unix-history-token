579118e50ffa51bd2a66edde9d1b89f3abfd7e58
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-23 21:53:01 -0800

    command line mail to bad recipients didn't return error message; don't call getcanonname from getmxrr if you know it will fail; don't claim that all messages were received from localhost in error messages
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.5
    
    Former-commit-id: 344945219b96d466d3f7c2c1702bc5beffb776ea

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	8.4 (Berkeley) %G%"
+literal|"@(#)version.c	8.5 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"8.4"
+literal|"8.5"
 decl_stmt|;
 end_decl_stmt
 
