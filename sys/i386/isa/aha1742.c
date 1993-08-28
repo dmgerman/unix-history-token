1a901fddcbda2f31fbe8d422f6e2a4d0a302e469
Rodney Grimes <rgrimes@FreeBSD.org>
1993-08-28 03:07:44 +0000

    Revised drivers from Julian.
    Former-commit-id: 86ccd1bbd6cea154ccb246722f5534c8b03f9b8e

--- sys/i386/isa/aha1742.c
+++ sys/i386/isa/aha1742.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *	$Id: aha1742.c,v 1.7 1993/08/21 20:01:31 rgrimes Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *	$Id: aha1742.c,v 1.7 93/08/26 21:12:21 julian Exp Locker: julian $  */
 end_comment
 
 begin_include
@@ -1603,8 +1603,6 @@ name|scsi_switch
 name|ahb_switch
 init|=
 block|{
-literal|"ahb"
-block|,
 name|ahb_scsi_cmd
 block|,
 name|ahbminphys
@@ -1615,7 +1613,7 @@ literal|0
 block|,
 name|ahb_adapter_info
 block|,
-literal|0
+literal|"ahb"
 block|,
 literal|0
 block|,
