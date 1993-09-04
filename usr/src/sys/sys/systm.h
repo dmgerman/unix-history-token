76b7c2d8806ec9fc5b5bb56ac5eed980a6c18eb8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-09-03 23:10:26 -0800

    curpri => more descriptive curpriority
    SCCS-vsn: sys/sys/systm.h 8.2
    
    Former-commit-id: d23041979b37e6b202e33744f7666fb525b13a05

--- usr/src/sys/sys/systm.h
+++ usr/src/sys/sys/systm.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)systm.h	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)systm.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -113,7 +113,7 @@ end_comment
 begin_decl_stmt
 specifier|extern
 name|u_char
-name|curpri
+name|curpriority
 decl_stmt|;
 end_decl_stmt
 
