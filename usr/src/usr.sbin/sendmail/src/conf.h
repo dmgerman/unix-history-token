bc37af88fdd8d5f776da8881fe1f19663c30f31c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-05 19:31:56 -0800

    comments only
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.30
    
    Former-commit-id: 536d1c8dc104c18ae98d89cd2ae206b3d3c41c94

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.29 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.30 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1927,7 +1927,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* **  Remaining definitions should never have to be changed.  They are **  primarily to provide back compatibility for older systems -- for **  example, it includes some POSIX compatibility definitions */
+comment|/********************************************************************** **  Remaining definitions should never have to be changed.  They are **  primarily to provide back compatibility for older systems -- for **  example, it includes some POSIX compatibility definitions **********************************************************************/
 end_comment
 
 begin_comment
