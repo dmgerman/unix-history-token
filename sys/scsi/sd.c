aa14d57ceb148d3d3b93db50be664286a7ca3996
Rod Grimes <rgrimes@FreeBSD.org>
1993-09-09 01:30:46 +0000

    Changed the delay from 100 seconds to 20 seconds if you don't have scsi dump code.
    Former-commit-id: 838c47cfa6390398e50046352dfd665dd397ff90

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *	$Id: sd.c,v 1.10 93/08/26 21:09:44 julian Exp Locker: julian $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *	$Id: sd.c,v 1.6 1993/08/28 03:08:56 rgrimes Exp $  */
 end_comment
 
 begin_define
@@ -6574,10 +6574,10 @@ argument_list|)
 expr_stmt|;
 name|DELAY
 argument_list|(
-literal|100000000
+literal|20000000
 argument_list|)
 expr_stmt|;
-comment|/* 100 seconds */
+comment|/* 20 seconds */
 return|return
 operator|(
 operator|-
