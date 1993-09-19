acb800fc7d08eafcdb052b9f0e9c6d7c74b50d97
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-18 22:10:07 -0800

    add BrokenSmtpPeers
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.19
    
    Former-commit-id: 5f8bf717a23244c30a88fa9d16e5ead3cb3ad2b9

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.18 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.19 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.18		%G%"
+literal|"@(#)sendmail.h	8.19		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -4475,6 +4475,17 @@ end_decl_stmt
 
 begin_comment
 comment|/* don't prune source routes */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|bool
+name|BrokenSmtpPeers
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* peers can't handle 2-line greeting */
 end_comment
 
 begin_decl_stmt
