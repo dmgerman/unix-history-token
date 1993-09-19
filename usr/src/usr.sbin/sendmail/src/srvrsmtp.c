675292fc54fd0eb12e7a827ebff07d47f10d16b7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-19 05:08:16 -0800

    hooks to handle broken SMTP peers that can't handle multi-line 220 greeting messages
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.15
    
    Former-commit-id: 7a66d80e5ccaa7a2d4ab24375071f00d23302bae

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.14 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	8.15 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.14 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	8.15 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -526,6 +526,21 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|BrokenSmtpPeers
+condition|)
+block|{
+name|message
+argument_list|(
+literal|"220 %s"
+argument_list|,
+name|inp
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
 name|message
 argument_list|(
 literal|"220-%s"
@@ -538,6 +553,7 @@ argument_list|(
 literal|"220 ESMTP spoken here"
 argument_list|)
 expr_stmt|;
+block|}
 name|protocol
 operator|=
 name|NULL
