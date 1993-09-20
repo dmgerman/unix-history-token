6d022629254b543cf42f5f14443af36db7291455
Rodney Grimes <rgrimes@FreeBSD.org>
1993-09-20 06:28:13 +0000

    Increase the timeout from 2000 counts to 6000 counts, reported by someone on the net (I lost the mail).  This should fix some units not showing up during probes.
    Former-commit-id: 3a12b47ad6653e1aa6bbf35bd245693188fb9e6a

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *	$Id: sd.c,v 1.8 1993/09/10 18:03:21 ats Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *	$Id: sd.c,v 1.9 1993/09/20 06:27:04 rgrimes Exp $  */
 end_comment
 
 begin_define
@@ -3094,7 +3094,7 @@ argument_list|(
 name|rdcap
 argument_list|)
 argument_list|,
-literal|2000
+literal|6000
 argument_list|,
 name|NULL
 argument_list|,
