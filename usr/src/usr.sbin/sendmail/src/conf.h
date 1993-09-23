0db7eac180021a59ec786fb154e8d9d27e5433ea
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-22 17:45:28 -0800

    NeXT also uses union wait
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.36
    
    Former-commit-id: dc856eb78c81a04bc1a1218fe89d6cdb076ac9c4

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.35 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.36 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -983,6 +983,17 @@ end_define
 
 begin_comment
 comment|/* has the statfs(2) syscall */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|WAITUNION
+value|1
+end_define
+
+begin_comment
+comment|/* use "union wait" as wait argument type */
 end_comment
 
 begin_define
