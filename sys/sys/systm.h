cb9c74d7073da23f3933cebb4a81e17cf7e14648
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-08 20:59:39 +0000

    ALL: 	Remove patch kit headers, add $Id$, add multiple inclusion protection.
    msgbuf.h:
    	put the msgbufmapped flag in here along with the pointer to the
    	buffer so that they are togeather.
    
    param.h
    	remove define of __386BSDREL__, this is no longer 386BSDREL 0.1
    	increase MAXINTERP from 32 chars to 64 chars.
    
    system.h
    	panicstr is now const char.
    	panic now takes a const char and is __dead
    	addlog now returns int to match printf
    	printf now returns int
    
    
    Former-commit-id: 49bff415bbdf25d2265bf966cf1d015314f87c35

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,7 +1,43 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)systm.h	7.17 (Berkeley) 5/25/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00158  * --------------------         -----   ----------------------  *  * 11 Dec 92	Williams Jolitz		Fixed and tty handling  * 25 Apr 93	Bruce Evans		Support for intr-0.1  * 16 May 93	Rodney W. Grimes	Added prototype for spinwait  *					Disabled prototype for wakeup, timeout  */
+comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)systm.h	7.17 (Berkeley) 5/25/91  *	$Id$  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_SYS_SYSTM_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_SYS_SYSTM_H_
+end_define
+
+begin_struct
+specifier|extern
+struct|struct
+name|sysent
+block|{
+comment|/* system call table */
+name|int
+name|sy_narg
+decl_stmt|;
+comment|/* number of arguments */
+name|int
+function_decl|(
+modifier|*
+name|sy_call
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* implementing function */
+block|}
+name|sysent
+index|[]
+struct|;
+end_struct
 
 begin_comment
 comment|/* Prototypes I needed to fix that kern_exit warning     ---- this really the first step in the work that's           been done on sun-lamp to add kernel function          prototypes.                                 */
@@ -51,6 +87,7 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|extern
+specifier|const
 name|char
 modifier|*
 name|panicstr
@@ -242,30 +279,6 @@ end_decl_stmt
 begin_comment
 comment|/* vnode equivalent to above */
 end_comment
-
-begin_struct
-specifier|extern
-struct|struct
-name|sysent
-block|{
-comment|/* system call table */
-name|int
-name|sy_narg
-decl_stmt|;
-comment|/* number of arguments */
-name|int
-function_decl|(
-modifier|*
-name|sy_call
-function_decl|)
-parameter_list|()
-function_decl|;
-comment|/* implementing function */
-block|}
-name|sysent
-index|[]
-struct|;
-end_struct
 
 begin_decl_stmt
 specifier|extern
@@ -459,11 +472,13 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+name|__dead
 name|void
 name|panic
 name|__P
 argument_list|(
 operator|(
+specifier|const
 name|char
 operator|*
 operator|)
@@ -485,7 +500,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|void
+name|int
 name|addlog
 name|__P
 argument_list|(
@@ -519,7 +534,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|void
+name|int
 name|printf
 name|__P
 argument_list|(
@@ -1424,6 +1439,15 @@ include|#
 directive|include
 file|<machine/cpufunc.h>
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _SYS_SYSTM_H_ */
+end_comment
 
 end_unit
 
