1401337300a7bc460a61252da862743bc23c0444
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-08 20:51:00 +0000

    All: 	Remove patch kit headers, and add $Id$ 	This is mostly to align some more code with NetBSD.
    cpu.h:
    	Remove the old function vs. include configuration stuff that was
    	ifdefed out when we went to inline functions.
    	Remove the define of resettodr that made it a nop, there is
    	already a function that makes it a nop, no need to #define one.
    	Remove the #defines of processor types, they are now defined
    	in cputypes.h, #include that file.
    	Add struct cpu_nameclass for support of cpu types.
    
    frame.h:
    	include sys/signal.h, it will be needed in the future.
    	put the sigframe structure here that was in machdep.c
    
    pcb.h:
    	Add multiple inclusion protection.
    	Add pcb_ldt and pcb_ldt_len to pcb structure, this is for the
    	user mode ldt.
    
    
    Former-commit-id: 61d89acd0c9a56d99c92cd1cc6a5202a6a09636d

--- sys/i386/include/frame.h
+++ sys/i386/include/frame.h
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)frame.h	5.2 (Berkeley) 1/18/91  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)frame.h	5.2 (Berkeley) 1/18/91  *	$Id$  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/signal.h>
+end_include
 
 begin_comment
 comment|/*  * System stack frames.  */
@@ -143,6 +149,45 @@ name|if_esp
 decl_stmt|;
 name|int
 name|if_ss
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * Signal frame  */
+end_comment
+
+begin_struct
+struct|struct
+name|sigframe
+block|{
+name|int
+name|sf_signum
+decl_stmt|;
+name|int
+name|sf_code
+decl_stmt|;
+name|struct
+name|sigcontext
+modifier|*
+name|sf_scp
+decl_stmt|;
+name|sig_t
+name|sf_handler
+decl_stmt|;
+name|int
+name|sf_eax
+decl_stmt|;
+name|int
+name|sf_edx
+decl_stmt|;
+name|int
+name|sf_ecx
+decl_stmt|;
+name|struct
+name|sigcontext
+name|sf_sc
 decl_stmt|;
 block|}
 struct|;
