406ad882964a36c821ca28805d120b1b80a414df
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-12 04:57:33 +0000

    Remove patch kit header, add $Id$
    Add new line discs from Terry Lambert and other places, fix some formatting.
    
    
    Former-commit-id: 1531107026420feba6f722577e58280aee58c5d5

--- sys/sys/ioctl.h
+++ sys/sys/ioctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ioctl.h	7.19 (Berkeley) 6/26/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         2       00163  * --------------------         -----   ----------------------  *  * 27 Nov 1992	Scott Reynolds		Completed V7 ioctl compatablity  * 27 May 93	Guido van Rooij		Comcontrol ioctls  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ioctl.h	7.19 (Berkeley) 6/26/91  *	$Id$  */
 end_comment
 
 begin_ifndef
@@ -949,12 +949,34 @@ end_comment
 begin_define
 define|#
 directive|define
+name|NETLDISC
+value|1
+end_define
+
+begin_comment
+comment|/* line discip for berk net */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NTTYDISC
+value|2
+end_define
+
+begin_comment
+comment|/* new tty discipline */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|TABLDISC
 value|3
 end_define
 
 begin_comment
-comment|/* tablet discipline */
+comment|/* hitachi tablet discipline */
 end_comment
 
 begin_define
@@ -982,12 +1004,34 @@ end_comment
 begin_define
 define|#
 directive|define
-name|MOUSEDISC
+name|MOUSELDISC
 value|6
 end_define
 
 begin_comment
-comment|/* Mouse discipline */
+comment|/* mouse discipline */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|KBDLDISC
+value|7
+end_define
+
+begin_comment
+comment|/* up/down keyboard trans (console) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NTABLDISC
+value|8
+end_define
+
+begin_comment
+comment|/* gtco tablet discipline */
 end_comment
 
 begin_define
