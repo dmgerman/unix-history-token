80a4c214dd081119c74a58db531dbbc8e1c37c66
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-13 15:59:30 +0000

    ALL:
    Removed patch kit headers and rcsid strings, add $Id$.
    
    isa.c:
    
    Removed old #ifdef notyet isa_configure code, since it will never be
    used, and I have done 90% of what it attempted to.
    
    Add conflict checking code that searchs back through the devtab's looking
    for any device that has already been found that may conflict with what
    we are about to probe.  Checks are mode for I/O address, memory address,
    IRQ, and DRQ.  This should stop the screwing up of any device that has
    alread been found by other device probes.
    Print out messages when we are not going to probe a device due to
    a conflict so the user knows WHY something was not found.  For example:
    
    aha0 not probed due to irq conflict with ahb0 at 11
    
    Now print out a message when a device is not found so the user knows
    that it was probed for, but could not be found.  For example:
    
    ed1 not found at 0x320
    
    For devices that have I/O address < 0x100 say that they are on the
    motherboard, not on isa!  The 0x100 magic number is per ISA spec.  It
    may seem funny that pc0 and sc0 report as being on the motherboard, but
    this is due to the fact that the I/O address used is that of the keyboard
    controller which IS on the motherboard.  We really need to split the
    keyboard probe from the display probe.  It is completly legal to build
    a pc with out one or the other, or even with out both!
    
    npx.c:
    
    Return -1 from the probe routine if we are using the Emulator so
    that the i/o addresses are not printed, this is the same trick used
    for 486's.
    
    Do not print the ``Errors reported via Exception 16'', and
    ``Errors reported via IRQ 13'' messages any more, since these just lead
    to more user confusion that anything.  It still prints the message
    ``Error reporting broken, using 387 emulator'' so that the person is
    aware that there mother board is ill.
    
    
    Former-commit-id: 39e87bbb7d7a94a42a6aea949d204136f6f11db8

--- sys/i386/isa/npx.c
+++ sys/i386/isa/npx.c
@@ -1,17 +1,7 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 William Jolitz.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)npx.c	7.2 (Berkeley) 5/12/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         1       00154  * --------------------         -----   ----------------------  *  * 20 Apr 93	Bruce Evans		New npx-0.5 code  * 23 May 93	Rodney W. Grimes	Return a special value of -1 from  *					the probe code to keep isa_config from  *					printing out the I/O address when we  *					are using trap 16 handling.  *  */
+comment|/*-  * Copyright (c) 1990 William Jolitz.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)npx.c	7.2 (Berkeley) 5/12/91  *	$Id$  */
 end_comment
-
-begin_decl_stmt
-specifier|static
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$Header: /a/cvs/386BSD/src/sys.386bsd/i386/isa/npx.c,v 1.1.1.1 1993/06/12 14:58:00 rgrimes Exp $"
-decl_stmt|;
-end_decl_stmt
 
 begin_include
 include|#
@@ -1141,9 +1131,11 @@ name|id_irq
 operator|=
 literal|0
 expr_stmt|;
+comment|/* 	 * special return value to flag that we do not 	 * actually use any I/O registers 	 */
 return|return
 operator|(
-name|IO_NPXSIZE
+operator|-
+literal|1
 operator|)
 return|;
 block|}
@@ -1167,32 +1159,13 @@ decl_stmt|;
 block|{
 if|if
 condition|(
+operator|!
 name|npx_ex16
-condition|)
-name|printf
-argument_list|(
-literal|"npx%d: Errors reported via Exception 16\n"
-argument_list|,
-name|dvp
-operator|->
-name|id_unit
-argument_list|)
-expr_stmt|;
-elseif|else
-if|if
-condition|(
+operator|&&
+operator|!
 name|npx_irq13
 condition|)
-name|printf
-argument_list|(
-literal|"npx%d: Errors reported via IRQ 13\n"
-argument_list|,
-name|dvp
-operator|->
-name|id_unit
-argument_list|)
-expr_stmt|;
-elseif|else
+block|{
 if|if
 condition|(
 name|npx_exists
@@ -1216,6 +1189,7 @@ operator|->
 name|id_unit
 argument_list|)
 expr_stmt|;
+block|}
 name|npxinit
 argument_list|(
 name|__INITIAL_NPXCW__
