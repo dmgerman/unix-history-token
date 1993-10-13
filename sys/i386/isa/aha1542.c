99e991eae70216afe83aec55286864cc18c96b69
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-13 16:34:10 +0000

    Increase the DELAY that supposedly makes the Bustek 545 work from 1ms to 10ms.  I have my doubts, but oh well.
    Former-commit-id: 60336a901c61ae874c399d2552b50cf4e414bb37

--- sys/i386/isa/aha1542.c
+++ sys/i386/isa/aha1542.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *	$Id: aha1542.c,v 1.8 1993/08/28 03:07:38 rgrimes Exp $  */
+comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *	$Id: aha1542.c,v 1.9 1993/10/12 07:15:28 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -3919,7 +3919,7 @@ directive|endif
 endif|__386BSD__
 name|DELAY
 argument_list|(
-literal|1000
+literal|10000
 argument_list|)
 expr_stmt|;
 comment|/* for Bustek 545 */
