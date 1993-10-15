606e8b5e48703b6ba5c082bfaa057601e0329400
Rodney Grimes <rgrimes@FreeBSD.org>
1993-10-15 10:07:45 +0000

    param.h:
    Mark the fact that PGSHIFT and PDRSHIFT are really the same as
    PG_SHIFT and PD_SHIFT, these should be collapsed some day soon.
    
    Document that KERNBASE should really be KPTDPTDI << PDRSHIFT, for
    now leave it as the constant 0xFE000000 until I make a seperate
    common header file for this stuff (vmaddresses.h?)
    
    Remove NKMEMCLUSTERS define, it was only being used to define
    VM_KMEM_SIZE, so why have all the indirection.  Besides who wants
    to work in CLBYTE sizes chuncks.
    
    
    pmap.h:
    
    Fix $Id$ and some other minor format clean ups.
    
    Remove the XXX comment about NKPDE, since it now has the correct value
    of 7.
    
    Remove unused LASTPTDI and move the APTD into the very end of memory to
    free up 4MB of kernel virtual address space.
    Remove unused RSVDPTDI and free up 12MB of kernel virtual address space.
    
    
    vmparam.h
    
    Fix $Id$.
    
    Increase SHMMAXPGS to 512 (2MB) now that there is room for it to be
    bigger.  The XXX comment stays until the kernel moves down in memory
    to free up enough space to use the proper default of 4MB.
    
    VM_KMEM_SIZE is now a direct constant stating the size of the kernel
    malloc region.  Increased the value from 3MB to 16MB.
    
    
    Former-commit-id: c2064228863dd91f2a52cd8e0829bca055f75a8a

--- sys/i386/include/param.h
+++ sys/i386/include/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)param.h	5.8 (Berkeley) 6/28/91  *	$Id: param.h,v 1.5 1993/10/12 07:13:12 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)param.h	5.8 (Berkeley) 6/28/91  *	$Id: param.h,v 1.6 1993/10/12 12:08:16 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -42,6 +42,10 @@ parameter_list|)
 value|(((u_int)(p) + ALIGNBYTES)&~ ALIGNBYTES)
 end_define
 
+begin_comment
+comment|/* XXX PGSHIFT and PG_SHIFT are two names for the same thing */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -82,6 +86,10 @@ name|NPTEPG
 value|(NBPG/(sizeof (struct pte)))
 end_define
 
+begin_comment
+comment|/* XXX PDRSHIFT and PD_SHIFT are two names for the same thing */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -113,6 +121,10 @@ end_define
 
 begin_comment
 comment|/* byte offset into page dir */
+end_comment
+
+begin_comment
+comment|/*  * XXX This should really be KPTDPTDI<< PDRSHIFT, but since KPTDPTDI is  * defined in pmap.h which is included after this we can't do that  * (YET!)  */
 end_comment
 
 begin_define
@@ -365,28 +377,6 @@ end_endif
 begin_comment
 comment|/* NMBCLUSTERS */
 end_comment
-
-begin_comment
-comment|/*  * Size of kernel malloc arena in CLBYTES-sized logical pages  */
-end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|NKMEMCLUSTERS
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|NKMEMCLUSTERS
-value|(3072*1024/CLBYTES)
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/*  * Some macros for units conversion  */
