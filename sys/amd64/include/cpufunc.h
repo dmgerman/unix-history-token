31fb45b23ea043756ab7573dcd9a961742aa82fb
Rodney Grimes <rgrimes@FreeBSD.org>
1993-10-16 14:40:57 +0000

    Removed all patch kit headers, sccsid and rcsid strings, put $Id$ in, some minor cleanup.  Added $Id$ to files that did not have any version info, etc
    Former-commit-id: 9de84f5eb2aa40c04fa4ba0eaccf7b72303abe18

--- sys/amd64/include/cpufunc.h
+++ sys/amd64/include/cpufunc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Functions to provide access to special i386 instructions.  * XXX - bezillions more are defined in locore.s but are not declared anywhere.  */
+comment|/*  * Functions to provide access to special i386 instructions.  * XXX - bezillions more are defined in locore.s but are not declared anywhere.  *  *	$Id$  */
 end_comment
 
 begin_include
