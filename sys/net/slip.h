c8466d524c0b7ba3c76db9884f90a12f0fab62bb
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-16 17:43:44 +0000

    Removed all patch kit headers, sccsid and rcsid strings, put $Id$ in, some minor cleanup.  Added $Id$ to files that did not have any version info, etc
    Former-commit-id: c3ac20e3baf3aa4ba70a6c0673d7ae321a451787

--- sys/net/slip.h
+++ sys/net/slip.h
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Definitions that user level programs might need to know to interact  * with serial line IP (slip) lines.   * @(#) $Header: slip.h,v 1.4 92/02/02 17:26:02 leres Exp $ (LBL)  *  * Copyright (c) 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  */
+comment|/*  * Copyright (c) 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the University of California, Berkeley.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * 	from: unknown  *	$Id$  */
+end_comment
+
+begin_comment
+comment|/*  * Definitions that user level programs might need to know to interact  * with serial line IP (slip) lines.  */
 end_comment
 
 begin_comment
