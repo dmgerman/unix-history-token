2a8ba03bb8a7cb26143274bea5c3b5c90cf05d51
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-16 12:50:22 +0000

    >From: Gerd Truschinski <gt@g386bsd.first.gmd.de> Date: Fri, 15 Oct 1993 19:01:24 +0100 Description: 	Every time I press the CAPS LOCK key my keyboard goes 	with other machines. This problem was in the first 	version of 386BSD up to now in FreeBSD and NetBSD.
    Repeat-By:
    	Simply press the CAPS LOCK key or the NUM LOCK key.
    
    Fix:
    	add a delay between the only two 'kbd_cmd's one after
    	another. If I take 1000 as a counter it will fail also
    	on a 33MHz/386 machine. I may try other fixes if you want
    	it. Please mention Jan <pernik@varda.ics.muni.cz> because
    	he had send me the fix (it was an older version of pccons.c,
    	but it took only five minutes to find a way for the new pccons.c)
    
    From: Rod Grimes
    	Try this one instead: (it is CPU speed independent.)
    	Change the for spin loop into a call to DELAY(10):
    
    >From: Gerd Truschinski <gt@g386bsd.first.gmd.de>
    Date: Sat, 16 Oct 1993 02:15:11 +0100 (MET)
    I tried it - edit, compile, reboot, edit, compile, reboot, edit....
    
    I need a DELAY(10000), a DELAY(5000) didn't work.
    A DELAY(10) is definatly (where is my dictionary?) to short.
    May be it is better to add a new flag.
    
    Editors Note:
    	Removed patch kit headers, rccsid, added $Id$.
    
    
    Former-commit-id: 23694f8d26ef9990cf4f4cbec482853620c71ce3

--- sys/i386/isa/pccons.c
+++ sys/i386/isa/pccons.c
@@ -1,17 +1,7 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)pccons.c	5.11 (Berkeley) 5/21/91  *  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE  * --------------------         -----   ----------------------  * CURRENT PATCH LEVEL:         6       00162  * --------------------         -----   ----------------------  *  * 15 Aug 92	Pace Willisson		Patches for X server  * 21 Aug 92	Frank Maclachlan	Fixed back-scroll system crash  * 28 Nov 92	Terry Lee		Fixed LED's in X mode  * 09 Feb 93	Rich Murphey		Added 'BELL' mode in X  * 14 Mar 93	Bruce Evans		Added keyboard timeout in pcprobe  * 					Fixed color/mono test and mono  *					kernel color.  Added check for  *					minor.   * 14 Mar 93	Chris G. Demetriou	Moved pg() to i386/cons.c, code  *					cleanup, removed ctl-alt-del.  * 22 Apr 93	Holger Veit		Added cons_highlight/cons_normal  *					to eliminate ESC sequence in   *					init_main.c  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)pccons.c	5.11 (Berkeley) 5/21/91  *	$Id$  */
 end_comment
-
-begin_decl_stmt
-specifier|static
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$Header: /a/cvs/386BSD/src/sys/i386/isa/pccons.c,v 1.4 1993/08/28 13:26:28 rgrimes Exp $"
-decl_stmt|;
-end_decl_stmt
 
 begin_comment
 comment|/*  * code to work keyboard& display for PC-style console  */
@@ -6767,12 +6757,21 @@ argument_list|)
 operator|!=
 literal|0
 condition|)
+block|{
 name|printf
 argument_list|(
 literal|"Timeout for keyboard LED command\n"
 argument_list|)
 expr_stmt|;
-elseif|else
+block|}
+else|else
+block|{
+comment|/* 		 * XXX This is quite questionable, but seems to fix 		 * the problem reported. 		 * some keyboard controllers need some time after they 		 * get a command.  Without this the keyboard 'hangs'. 		 * This seems to be the only place where two commands 		 * are just one behind another.  		 */
+name|DELAY
+argument_list|(
+literal|10000
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|kbd_cmd
@@ -6799,6 +6798,7 @@ argument_list|(
 literal|"Timeout for keyboard LED data\n"
 argument_list|)
 expr_stmt|;
+block|}
 if|#
 directive|if
 literal|0
