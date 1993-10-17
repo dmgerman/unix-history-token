584ac961c64b10811f47cc3b37764736dbe4d0ac
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-17 01:48:37 +0000

    Fix reference no ni_rdir, new name is ni_rootdir, these where inside #ifdef PARNOIA, so there have been overlooked for a long time!
    Former-commit-id: d43137c2d5afc8a0159efeaef3dd5d0209f470e0

--- sys/pcfs/pcfs_lookup.c
+++ sys/pcfs/pcfs_lookup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  Written by Paul Popelka (paulp@uts.amdahl.com)  *  *  You can do anything you want with this software,  *    just don't say you wrote it,  *    and don't remove this notice.  *  *  This software is provided "as is".  *  *  The author supplies this software to be publicly  *  redistributed on the understanding that the author  *  is not responsible for the correct functioning of  *  this software in any circumstances and is not liable  *  for any damages caused by this software.  *  *  October 1992  *  *	$Id$  */
+comment|/*  *  Written by Paul Popelka (paulp@uts.amdahl.com)  *  *  You can do anything you want with this software,  *    just don't say you wrote it,  *    and don't remove this notice.  *  *  This software is provided "as is".  *  *  The author supplies this software to be publicly  *  redistributed on the understanding that the author  *  is not responsible for the correct functioning of  *  this software in any circumstances and is not liable  *  for any damages caused by this software.  *  *  October 1992  *  *	$Id: pcfs_lookup.c,v 1.3 1993/10/16 19:29:35 rgrimes Exp $  */
 end_comment
 
 begin_include
@@ -331,7 +331,7 @@ name|vdp
 operator|==
 name|ndp
 operator|->
-name|ni_rdir
+name|ni_rootdir
 operator|&&
 name|ndp
 operator|->
