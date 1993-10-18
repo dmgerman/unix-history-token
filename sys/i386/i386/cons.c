9c43d8dfe5751287f5c473756b8061225f56ab0a
David Greenman <dg@FreeBSD.org>
1993-10-18 14:21:48 +0000

    Yank out Christoph Robitschko's hack for the hanging console problem as it didn't actually fix it, and because starting the getty on /dev/console instead of /dev/vga is a good work-around.
    Former-commit-id: 87b98d1e47125b414cf99264f04cba1563f2c007

--- sys/i386/i386/cons.c
+++ sys/i386/i386/cons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91  *	$Id$  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91  *	$Id: cons.c,v 1.3 1993/10/16 14:14:49 rgrimes Exp $  */
 end_comment
 
 begin_include
@@ -55,12 +55,6 @@ begin_include
 include|#
 directive|include
 file|"sys/conf.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"sys/vnode.h"
 end_include
 
 begin_include
@@ -304,17 +298,6 @@ expr_stmt|;
 block|}
 end_block
 
-begin_decl_stmt
-specifier|static
-name|struct
-name|vnode
-modifier|*
-name|cnopenvp
-init|=
-name|NULLVP
-decl_stmt|;
-end_decl_stmt
-
 begin_macro
 name|cnopen
 argument_list|(
@@ -352,9 +335,6 @@ end_decl_stmt
 
 begin_block
 block|{
-name|int
-name|error
-decl_stmt|;
 if|if
 condition|(
 name|cn_tab
@@ -372,42 +352,6 @@ name|cn_tab
 operator|->
 name|cn_dev
 expr_stmt|;
-if|if
-condition|(
-name|cnopenvp
-operator|==
-name|NULLVP
-condition|)
-if|if
-condition|(
-operator|(
-name|error
-operator|=
-name|getdevvp
-argument_list|(
-name|dev
-argument_list|,
-operator|&
-name|cnopenvp
-argument_list|,
-name|VCHR
-argument_list|)
-operator|)
-condition|)
-block|{
-name|printf
-argument_list|(
-literal|"cnopen: getdevvp returned %d !\n"
-argument_list|,
-name|error
-argument_list|)
-expr_stmt|;
-return|return
-operator|(
-name|error
-operator|)
-return|;
-block|}
 return|return
 operator|(
 operator|(
@@ -473,9 +417,6 @@ end_decl_stmt
 
 begin_block
 block|{
-name|int
-name|error
-decl_stmt|;
 if|if
 condition|(
 name|cn_tab
@@ -493,17 +434,8 @@ name|cn_tab
 operator|->
 name|cn_dev
 expr_stmt|;
-if|if
-condition|(
-name|vcount
-argument_list|(
-name|cnopenvp
-argument_list|)
-operator|<=
-literal|1
-condition|)
-name|error
-operator|=
+return|return
+operator|(
 operator|(
 operator|*
 name|cdevsw
@@ -525,34 +457,8 @@ name|mode
 operator|,
 name|p
 operator|)
-expr_stmt|;
-else|else
-name|error
-operator|=
-literal|0
-expr_stmt|;
-if|if
-condition|(
-name|error
-operator|==
-literal|0
-condition|)
-block|{
-name|vrele
-argument_list|(
-name|cnopenvp
-argument_list|)
-expr_stmt|;
-name|cnopenvp
-operator|=
-name|NULLVP
-expr_stmt|;
-return|return
-operator|(
-name|error
 operator|)
 return|;
-block|}
 block|}
 end_block
 
