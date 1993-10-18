66c2fa3a4e4569a566e0774b8b2c9515bc55fa8f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-18 03:57:13 -0800

    portability for A/UX
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.41
    
    Former-commit-id: 15d50945838a1245871899ed8cd35b40e869b205

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.40 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.41 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1783,6 +1783,12 @@ ifdef|#
 directive|ifdef
 name|_AUX_SOURCE
 end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/sysmacros.h>
+end_include
 
 begin_define
 define|#
