649a953bc6cb42d54e615e51ea6a334e0b99f114
David Greenman <davidg@Root.COM>
1993-10-19 09:05:51 +0000

    fix bugs in the accounting code that was just added. The Mach VM system keeps track of things as pages, not K bytes, and the rusage stats want Kbytes.
    Former-commit-id: 3d57f4a007541088b23500fb67be50c9e6580dec

--- sys/kern/kern_clock.c
+++ sys/kern/kern_clock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)kern_clock.c	7.16 (Berkeley) 5/9/91  *	$Id: kern_clock.c,v 1.2 1993/10/16 15:24:09 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)kern_clock.c	7.16 (Berkeley) 5/9/91  *	$Id: kern_clock.c,v 1.3 1993/10/19 01:05:36 nate Exp $  */
 end_comment
 
 begin_include
@@ -346,6 +346,10 @@ operator|+=
 name|vm
 operator|->
 name|vm_tsize
+operator|*
+name|NBPG
+operator|/
+literal|1024
 expr_stmt|;
 name|ru
 operator|->
@@ -354,6 +358,10 @@ operator|+=
 name|vm
 operator|->
 name|vm_dsize
+operator|*
+name|NBPG
+operator|/
+literal|1024
 expr_stmt|;
 name|ru
 operator|->
@@ -362,9 +370,14 @@ operator|+=
 name|vm
 operator|->
 name|vm_ssize
+operator|*
+name|NBPG
+operator|/
+literal|1024
 expr_stmt|;
 if|if
 condition|(
+operator|(
 name|vm
 operator|->
 name|vm_pmap
@@ -372,11 +385,17 @@ operator|.
 name|pm_stats
 operator|.
 name|resident_count
+operator|*
+name|NBPG
+operator|/
+literal|1024
+operator|)
 operator|>
 name|ru
 operator|->
 name|ru_maxrss
 condition|)
+block|{
 name|ru
 operator|->
 name|ru_maxrss
@@ -388,7 +407,12 @@ operator|.
 name|pm_stats
 operator|.
 name|resident_count
+operator|*
+name|NBPG
+operator|/
+literal|1024
 expr_stmt|;
+block|}
 block|}
 comment|/* 	 * If the cpu is currently scheduled to a process, then 	 * charge it with resource utilization for a tick, updating 	 * statistics which run in (user+system) virtual time, 	 * such as the cpu time limit and profiling timers. 	 * This assumes that the current process has been running 	 * the entire last tick. 	 */
 if|if
