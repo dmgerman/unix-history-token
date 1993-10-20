33a59882f3f9f9f06d8ef9d40526ff2665cd00a0
Rod Grimes <rgrimes@FreeBSD.org>
1993-10-20 15:49:02 +0000

    >From: "Andrew A. Chernov, Black Mage" <ache@astral.msk.su> Date: Wed, 20 Oct 1993 18:28:44 +0300
    Editors Note:
    	Line ended with comma (``,'') instead of semicolon (``;''),
    	this should not have effected the code.
    	Submitter made no comments other than it was ``simple''.
    
    
    Former-commit-id: ab9d664e0b065434dc5addcf468c97b45a467709

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  * major changes by Julian Elischer (julian@jules.dialix.oz.au) May 1993  *  *	$Id: st.c,v 1.10 1993/10/11 04:53:23 rgrimes Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  * major changes by Julian Elischer (julian@jules.dialix.oz.au) May 1993  *  *	$Id: st.c,v 1.11 1993/10/16 17:21:10 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -3512,7 +3512,7 @@ operator|->
 name|b_actf
 operator|=
 name|NULL
-operator|,
+expr_stmt|;
 comment|/*******************************************************\ 	* Tell the device to get going on the transfer if it's	* 	* not doing anything, otherwise just wait for completion* 	* (All a bit silly if we're only allowing 1 open but..) * 	\*******************************************************/
 name|ststart
 argument_list|(
