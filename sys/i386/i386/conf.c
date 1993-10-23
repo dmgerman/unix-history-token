883e5fe0828b7170577ef152677719d88bff32b8
Jordan K. Hubbard <jkh@FreeBSD.org>
1993-10-23 22:13:08 +0000

    Moved sound driver from major 21 to major 30
    Former-commit-id: 8caafd526b3f176a6ff7fe0f2b430034048c0a37

--- sys/i386/i386/conf.c
+++ sys/i386/i386/conf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.10 1993/10/11 07:39:13 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.11 1993/10/23 10:49:24 jkh Exp $  */
 end_comment
 
 begin_include
@@ -2870,29 +2870,29 @@ name|NULL
 block|}
 block|,
 block|{
-name|sndopen
+name|enodev
 block|,
-name|sndclose
+name|enodev
 block|,
-name|sndread
+name|enodev
 block|,
-name|sndwrite
+name|enodev
 block|,
 comment|/*21*/
-name|sndioctl
-block|,
 name|enodev
 block|,
 name|enodev
+block|,
+name|nullop
 block|,
 name|NULL
 block|,
-comment|/* sound driver */
-name|sndselect
+comment|/* psm */
+name|enodev
 block|,
 name|enodev
 block|,
-name|NULL
+name|enodev
 block|}
 block|,
 block|{
@@ -3103,7 +3103,33 @@ block|,
 name|mcdstrategy
 block|}
 block|,
-comment|/*  * If you need a cdev major number, please contact the 386bsd patchkit   * coordinator by sending mail to "patches@cs.montana.edu".  * If you assign one yourself it may then conflict with someone else.  */
+block|{
+name|sndopen
+block|,
+name|sndclose
+block|,
+name|sndread
+block|,
+name|sndwrite
+block|,
+comment|/*30*/
+name|sndioctl
+block|,
+name|enodev
+block|,
+name|enodev
+block|,
+name|NULL
+block|,
+comment|/* sound driver */
+name|sndselect
+block|,
+name|enodev
+block|,
+name|NULL
+block|}
+block|,
+comment|/*  * If you need a cdev major number, please contact the FreeBSD team  * by sending mail to `freebsd-hackers@freefall.cdrom.com'.  * If you assign one yourself it may then conflict with someone else.  */
 block|}
 decl_stmt|;
 end_decl_stmt
