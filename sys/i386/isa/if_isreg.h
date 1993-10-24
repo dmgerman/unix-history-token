ee799ae6cb66cc49ac8477413836259609c7db81
Paul Richards <paul@isl.cf.ac.uk>
1993-10-24 04:27:00 +0000

    Moved allocation of memory into is_attach so that malloc gets called once when card is attached. Solved problem with driver getting hosed when a reset takes place.
    Removed init_block array -- now part of malloced memory. No more
    static declarations left.
    
    Added code so that debug ioctl actually does something.
    ifconfig is0 debug will now switch on debugging code.
    
    Other general cleanups.
    
    
    Former-commit-id: ab11aea159febe48fa7272b27bbd3fffaf5d07d0

--- sys/i386/isa/if_isreg.h
+++ sys/i386/isa/if_isreg.h
@@ -1,7 +1,43 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Isolan AT 4141-0 Ethernet driver header file  * Isolink 4110   *  * By Paul Richards   *  * Copyright (C) 1993, Paul Richards. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  *	$Id$  */
+comment|/*  * Isolan AT 4141-0 Ethernet driver header file  * Isolink 4110   *  * By Paul Richards   *  * Copyright (C) 1993, Paul Richards. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  *	$Id: if_isreg.h,v 1.2 1993/10/16 13:45:55 rgrimes Exp $  */
 end_comment
+
+begin_comment
+comment|/*  * Initialize multicast address hashing registers to accept  * all multicasts (only used when in promiscuous mode)  */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|NBPFILTER
+operator|>
+literal|0
+end_if
+
+begin_define
+define|#
+directive|define
+name|MULTI_INIT_ADDR
+value|0xff
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|MULTI_INIT_ADDR
+value|0
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/* Declarations specific to this driver */
