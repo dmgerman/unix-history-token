67d878136f8869e715445fc6aaf0730bf879f13d
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-10-24 20:59:08 -0800

    new improved version: dynamic allocation of fake pages
    SCCS-vsn: sys/vm/device_pager.h 8.2
    SCCS-vsn: sys/vm/device_pager.c 8.2
    
    Former-commit-id: 7bd521b0bf376d5738bf579c4498aa5177a60c51

--- usr/src/sys/vm/device_pager.h
+++ usr/src/sys/vm/device_pager.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)device_pager.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)device_pager.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -25,25 +25,9 @@ struct|struct
 name|devpager
 block|{
 name|queue_head_t
-name|devp_list
+name|devp_pglist
 decl_stmt|;
-comment|/* list of managed devices */
-name|dev_t
-name|devp_dev
-decl_stmt|;
-comment|/* devno of device */
-name|vm_page_t
-name|devp_pages
-decl_stmt|;
-comment|/* page structs for device */
-name|int
-name|devp_npages
-decl_stmt|;
-comment|/* size of device in pages */
-name|int
-name|devp_count
-decl_stmt|;
-comment|/* reference count */
+comment|/* list of pages allocated */
 name|vm_object_t
 name|devp_object
 decl_stmt|;
@@ -60,13 +44,6 @@ modifier|*
 name|dev_pager_t
 typedef|;
 end_typedef
-
-begin_define
-define|#
-directive|define
-name|DEV_PAGER_NULL
-value|((dev_pager_t)0)
-end_define
 
 begin_endif
 endif|#
