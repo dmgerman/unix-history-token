6ddc4af79e1e6573667f4434fb7193a954f3ee17
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-28 14:51:27 -0800

    Linux portability patches
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.43
    
    Former-commit-id: fbaf1c42f04ac1030256b5a3c73d6c1ec66db70a

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.43 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1777,6 +1777,12 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_include
+include|#
+directive|include
+file|<sys/sysmacros.h>
+end_include
 
 begin_endif
 endif|#
