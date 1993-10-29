b6099ff0a46dfdab5939373d325c836f6be0e821
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-29 14:44:28 -0800

    Linux portability changes
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.44
    
    Former-commit-id: 70a0145e1252716798c658c7d3f389afece1331d

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.43 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.44 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1713,7 +1713,7 @@ end_comment
 begin_ifdef
 ifdef|#
 directive|ifdef
-name|linux
+name|__linux__
 end_ifdef
 
 begin_define
