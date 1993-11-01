cd4fbe8ca0d1791254039e1f3adb509d7f041e3d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-31 19:34:45 -0800

    see RELEASE_NOTES -- includes some security fixes
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.6.4.1
    
    Former-commit-id: 7a408e174a68ceb3f4d859428c93ee0ffde2fa5c

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	8.6.3.1 (Berkeley) %G%"
+literal|"@(#)version.c	8.6.4.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"8.6.3"
+literal|"8.6.4"
 decl_stmt|;
 end_decl_stmt
 
