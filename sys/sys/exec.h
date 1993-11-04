3c3432b802cedbd5b005e9965f4c1af9850f439a
Paul Richards <paul@isl.cf.ac.uk>
1993-11-04 03:05:00 +0000

    Modified exec header to midmag format. Added #define a_midmag a_magic so existing kernel will work with new format.
    Former-commit-id: dff9e00529e2d53107613b51f1de65e81a0ace28

--- sys/sys/exec.h
+++ sys/sys/exec.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)exec.h	7.5 (Berkeley) 2/15/91  *	$Id$  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)exec.h	7.5 (Berkeley) 2/15/91  *	$Id: exec.h,v 1.3 1993/10/16 17:16:42 rgrimes Exp $  */
 end_comment
 
 begin_ifndef
@@ -16,50 +16,18 @@ name|_EXEC_H_
 end_define
 
 begin_comment
-comment|/* Header prepended to each a.out file. */
+comment|/*  * Header prepended to each a.out file.  * only manipulate the a_midmag field via the  * N_SETMAGIC/N_GET{MAGIC,MID,FLAG} macros in a.out.h  */
 end_comment
 
 begin_struct
 struct|struct
 name|exec
 block|{
-if|#
-directive|if
-operator|!
-name|defined
-argument_list|(
-name|vax
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
-name|tahoe
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
-name|i386
-argument_list|)
 name|unsigned
-name|short
-name|a_mid
-decl_stmt|;
-comment|/* machine ID */
-name|unsigned
-name|short
-name|a_magic
-decl_stmt|;
-comment|/* magic number */
-else|#
-directive|else
 name|long
-name|a_magic
+name|a_midmag
 decl_stmt|;
-comment|/* magic number */
-endif|#
-directive|endif
+comment|/* htonl(flags<<26 | mid<<16 | magic) */
 name|unsigned
 name|long
 name|a_text
@@ -102,12 +70,12 @@ end_struct
 begin_define
 define|#
 directive|define
-name|a_machtype
-value|a_mid
+name|a_magic
+value|a_midmag
 end_define
 
 begin_comment
-comment|/* SUN compatibility */
+comment|/* XXX Hack to work with current kern_execve.c */
 end_comment
 
 begin_comment
@@ -198,6 +166,17 @@ end_comment
 begin_define
 define|#
 directive|define
+name|MID_I386
+value|134
+end_define
+
+begin_comment
+comment|/* i386 BSD binary */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MID_HP200
 value|200
 end_define
@@ -237,6 +216,21 @@ end_define
 
 begin_comment
 comment|/* hp800 HP-UX binary */
+end_comment
+
+begin_comment
+comment|/*  * a_flags  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|EX_DYNAMIC
+value|0x20
+end_define
+
+begin_comment
+comment|/* a.out contains run-time link-edit info */
 end_comment
 
 begin_endif
