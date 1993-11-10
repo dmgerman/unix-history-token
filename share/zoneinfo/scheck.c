5c0e6704d2bdc4046729b1795d5edca35d493229
Garrett Wollman <wollman@FreeBSD.org>
1993-11-10 04:43:35 +0000

    Fall 93 release from Arthur David Olson (as a vendor branch)
    Former-commit-id: cdcc3b8c503954ab960fd8550d5f84dc2fa65a75

--- share/zoneinfo/scheck.c
+++ share/zoneinfo/scheck.c
@@ -1,38 +1,15 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Arthur David Olson of the National Cancer Institute.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
-end_comment
-
 begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
 
-begin_decl_stmt
-specifier|static
-name|char
-name|sccsid
-index|[]
-init|=
-literal|"@(#)scheck.c	5.3 (Berkeley) 4/20/91"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|notdef
-end_ifdef
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|NOID
+end_ifndef
 
 begin_decl_stmt
 specifier|static
@@ -40,7 +17,7 @@ name|char
 name|elsieid
 index|[]
 init|=
-literal|"@(#)scheck.c	8.9"
+literal|"@(#)scheck.c	8.11"
 decl_stmt|;
 end_decl_stmt
 
@@ -48,6 +25,19 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* !defined lint */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !defined NOID */
+end_comment
 
 begin_comment
 comment|/*LINTLIBRARY*/
@@ -56,26 +46,38 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<stdio.h>
+file|"private.h"
 end_include
 
-begin_include
-include|#
-directive|include
-file|<ctype.h>
-end_include
+begin_decl_stmt
+specifier|extern
+name|char
+modifier|*
+name|imalloc
+name|P
+argument_list|(
+operator|(
+name|int
+name|n
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
-begin_include
-include|#
-directive|include
-file|<string.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<stdlib.h>
-end_include
+begin_decl_stmt
+specifier|extern
+name|void
+name|ifree
+name|P
+argument_list|(
+operator|(
+name|char
+operator|*
+name|p
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_function
 name|char
@@ -127,9 +129,16 @@ decl_stmt|;
 name|char
 name|dummy
 decl_stmt|;
+specifier|static
+name|char
+name|nada
+index|[
+literal|1
+index|]
+decl_stmt|;
 name|result
 operator|=
-literal|""
+name|nada
 expr_stmt|;
 if|if
 condition|(
@@ -146,7 +155,7 @@ name|result
 return|;
 name|fbuf
 operator|=
-name|malloc
+name|imalloc
 argument_list|(
 literal|2
 operator|*
@@ -345,10 +354,6 @@ if|if
 condition|(
 name|sscanf
 argument_list|(
-operator|(
-name|char
-operator|*
-operator|)
 name|string
 argument_list|,
 name|fbuf
