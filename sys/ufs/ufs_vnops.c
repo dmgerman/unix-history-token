e430de8a9bfdb7e0fc34ae79d9d775b3786b2ff7
Garrett Wollman <wollman@FreeBSD.org>
1993-11-10 05:33:56 +0000

    Add some support for _POSIX_CHOWN_RESTRICTED disabling.  If this symbol is not defined in sys/unistd.h, we allow users to give files away.  With luck, this will even work in the presence of QUOTAs.  Still need to add a VOP_PATHCONF() entry point to allow for correct pathconf()/fpathconf() functionality.  (By default, the symbol should be left defined to get the traditional BSD behavior.)
    Former-commit-id: 1cf9a435d245351184d63ea0674f1e976f573894

--- sys/ufs/ufs_vnops.c
+++ sys/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_vnops.c	7.64 (Berkeley) 5/16/91  *	$Id: ufs_vnops.c,v 1.4 1993/10/16 18:18:03 rgrimes Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_vnops.c	7.64 (Berkeley) 5/16/91  *	$Id: ufs_vnops.c,v 1.5 1993/10/20 07:31:42 davidg Exp $  */
 end_comment
 
 begin_include
@@ -122,6 +122,20 @@ include|#
 directive|include
 file|"fs.h"
 end_include
+
+begin_comment
+comment|/* Get the current value of _POSIX_CHOWN_RESTRICTED */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|"sys/unistd.h"
+end_include
+
+begin_comment
+comment|/* make sure to get sys/ version. */
+end_comment
 
 begin_comment
 comment|/*  * Create a regular file  */
@@ -1873,6 +1887,9 @@ operator|->
 name|i_gid
 expr_stmt|;
 comment|/* 	 * If we don't own the file, are trying to change the owner 	 * of the file, or are not a member of the target group, 	 * the caller must be superuser or the call fails. 	 */
+ifdef|#
+directive|ifdef
+name|_POSIX_CHOWN_RESTRICTED
 if|if
 condition|(
 operator|(
@@ -1921,6 +1938,50 @@ operator|(
 name|error
 operator|)
 return|;
+else|#
+directive|else
+if|if
+condition|(
+operator|(
+name|cred
+operator|->
+name|cr_uid
+operator|!=
+name|ip
+operator|->
+name|i_uid
+operator|||
+operator|!
+name|groupmember
+argument_list|(
+operator|(
+name|gid_t
+operator|)
+name|gid
+argument_list|,
+name|cred
+argument_list|)
+operator|)
+operator|&&
+operator|(
+name|error
+operator|=
+name|suser
+argument_list|(
+name|cred
+argument_list|,
+operator|&
+name|p
+operator|->
+name|p_acflag
+argument_list|)
+operator|)
+condition|)
+return|return
+name|error
+return|;
+endif|#
+directive|endif
 name|ouid
 operator|=
 name|ip
