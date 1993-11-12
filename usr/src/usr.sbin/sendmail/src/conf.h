c2f6afc389eea9378f00cd8696c13c25df61f70b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-11-12 00:52:27 -0800

    Solaris portability changes
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.47
    
    Former-commit-id: d4a221ffe991a7752b2a754d1796c206efb3dfdf

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.46 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.47 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -710,7 +710,7 @@ begin_define
 define|#
 directive|define
 name|gethostbyname
-value|__switch_gethostbyname
+value|solaris_gethostbyname
 end_define
 
 begin_comment
@@ -721,7 +721,7 @@ begin_define
 define|#
 directive|define
 name|gethostbyaddr
-value|__switch_gethostbyaddr
+value|solaris_gethostbyaddr
 end_define
 
 begin_comment
