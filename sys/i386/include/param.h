0ea396278366040934631c1c64da567f2b835076
David Greenman <dg@FreeBSD.org>
1993-11-13 02:25:21 +0000

    First steps in rewriting locore.s, and making info useful when the machine panics.
    i386/i386/locore.s:
    1) got rid of most .set directives that were being used like
    	#define's, and replaced them with appropriate #define's in
    	the appropriate header files (accessed via genassym).
    2) added comments to header inclusions and global definitions,
    	and global variables
    3) replaced some hardcoded constants with cpp defines (such as
    	PDESIZE and others)
    4) aligned all comments to the same column to make them easier to
    	read
    5) moved macro definitions for ENTRY, ALIGN, NOP, etc. to
    	/sys/i386/include/asmacros.h
    6) added #ifdef BDE_DEBUGGER around all of Bruce's debugger code
    7) added new global '_KERNend' to store last location+1 of kernel
    8) cleaned up zeroing of bss so that only bss is zeroed
    9) fix zeroing of page tables so that it really does zero them all
    	- not just if they follow the bss.
    10) rewrote page table initialization code so that 1) works correctly
    	and 2) write protects the kernel text by default
    11) properly initialize the kernel page directory, upages, p0stack PT,
    	and page tables. The previous scheme was more than a bit
    	screwy.
    12) change allocation of virtual area of IO hole so that it is
    	fixed at KERNBASE + 0xa0000. The previous scheme put it
    	right after the kernel page tables and then later expected
    	it to be at KERNBASE +0xa0000
    13) change multiple bogus settings of user read/write of various
    	areas of kernel VM - including the IO hole; we should never
    	be accessing the IO hole in user mode through the kernel
    	page tables
    14) split kernel support routines such as bcopy, bzero, copyin,
    	copyout, etc. into a seperate file 'support.s'
    15) split swtch and related routines into a seperate 'swtch.s'
    16) split routines related to traps, syscalls, and interrupts
    	into a seperate file 'exception.s'
    17) remove some unused global variables from locore that got
    	inserted by Garrett when he pulled them out of some .h
    	files.
    
    i386/isa/icu.s:
    1) clean up global variable declarations
    2) move in declaration of astpending and netisr
    
    i386/i386/pmap.c:
    1) fix calculation of virtual_avail. It previously was calculated
    	to be right in the middle of the kernel page tables - not
    	a good place to start allocating kernel VM.
    2) properly allocate kernel page dir/tables etc out of kernel map
    	- previously only took out 2 pages.
    
    i386/i386/machdep.c:
    1) modify boot() to print a warning that the system will reboot in
    	PANIC_REBOOT_WAIT_TIME amount of seconds, and let the user
    	abort with a key on the console. The machine will wait for
    	ever if a key is typed before the reboot. The default is
    	15 seconds, but can be set to 0 to mean don't wait at all,
    	-1 to mean wait forever, or any positive value to wait for
    	that many seconds.
    2) print "Rebooting..." just before doing it.
    
    kern/subr_prf.c:
    1) remove PANICWAIT as it is deprecated by the change to machdep.c
    
    i386/i386/trap.c:
    1) add table of trap type strings and use it to print a real trap/
    	panic message rather than just a number. Lot's of work to
    	be done here, but this is the first step. Symbolic traceback
    	is in the TODO.
    
    i386/i386/Makefile.i386:
    1) add support in to build support.s, exception.s and swtch.s
    
    ...and various changes to various header files to make all of the
    	above happen.
    
    
    Former-commit-id: 33c2759cc2562d43ba6b78550d2f177fd52c7acf

--- sys/i386/include/param.h
+++ sys/i386/include/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)param.h	5.8 (Berkeley) 6/28/91  *	$Id: param.h,v 1.7 1993/10/15 10:07:43 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)param.h	5.8 (Berkeley) 6/28/91  *	$Id: param.h,v 1.8 1993/11/07 17:42:58 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -157,17 +157,6 @@ directive|define
 name|BTOPKERNBASE
 value|((u_long)KERNBASE>> PGSHIFT)
 end_define
-
-begin_define
-define|#
-directive|define
-name|KERNSIZE
-value|0x00C00000
-end_define
-
-begin_comment
-comment|/* size of kernel virtual */
-end_comment
 
 begin_define
 define|#
