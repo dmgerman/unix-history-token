c53efa4ad4731bf1a964eb19b8a46167ffd354b2
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-11-14 22:20:28 -0800

    use faster copypage primative in pmap_copy_page
    SCCS-vsn: sys/hp300/hp300/pmap.c 8.2
    
    Former-commit-id: f20531f20d105b46fbba6cd0f4a4d5e0b3138654

--- usr/src/sys/hp300/hp300/pmap.c
+++ usr/src/sys/hp300/hp300/pmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)pmap.c	8.1 (Berkeley) %G%  */
+comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)pmap.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -5833,7 +5833,7 @@ argument_list|,
 name|TRUE
 argument_list|)
 expr_stmt|;
-name|bcopy
+name|copypage
 argument_list|(
 operator|(
 name|caddr_t
@@ -5844,8 +5844,6 @@ operator|(
 name|caddr_t
 operator|)
 name|dkva
-argument_list|,
-name|PAGE_SIZE
 argument_list|)
 expr_stmt|;
 comment|/* CADDR1 and CADDR2 are virtually contiguous */
