91da2a173149b8d20ec7ec84cf6e6998077ee30d
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-11-14 21:54:44 -0800

    eliminate unneeded cast
    SCCS-vsn: sys/vm/vnode_pager.c 8.3
    
    Former-commit-id: 0ba5705f9a4322773b72648196ab3e9d820e1392

--- usr/src/sys/vm/vnode_pager.c
+++ usr/src/sys/vm/vnode_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)vnode_pager.c	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  *	@(#)vnode_pager.c	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -631,9 +631,6 @@ name|pager
 operator|->
 name|pg_data
 operator|=
-operator|(
-name|caddr_t
-operator|)
 name|vnp
 expr_stmt|;
 name|vp
