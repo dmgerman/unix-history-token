e57cac53e6e8f27f803846c0aa89ff759883a8ff
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-11-14 23:42:42 -0800

    IN_CHANGE should set flags not mode
    SCCS-vsn: sys/ufs/ffs/ufs_vnops.c 8.6
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 8.6
    
    Former-commit-id: c5a097c68fd6fd859324337ed898df330440c531

--- usr/src/sys/ufs/ufs/ufs_vnops.c
+++ usr/src/sys/ufs/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1988,7 +1988,11 @@ name|mode
 operator|&
 name|ALLPERMS
 operator|)
-operator||
+expr_stmt|;
+name|ip
+operator|->
+name|i_flag
+operator||=
 name|IN_CHANGE
 expr_stmt|;
 if|if
