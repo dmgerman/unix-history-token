cc6e02b0f693302a3ac69c8f56b4b880497e693e
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1993-11-15 21:32:50 -0800

    af.h has been retired
    SCCS-vsn: sys/net/route.c 8.2
    SCCS-vsn: sys/net/rtsock.c 8.2
    
    Former-commit-id: c4c1d3d0c0f3e62609fd3a164ff19ec130829ddd

--- usr/src/sys/net/route.c
+++ usr/src/sys/net/route.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)route.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1980, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)route.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -61,12 +61,6 @@ begin_include
 include|#
 directive|include
 file|<net/if.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<net/af.h>
 end_include
 
 begin_include
