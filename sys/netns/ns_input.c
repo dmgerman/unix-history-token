09ed69ddc4edd89ce3d1c425f898874f6b6026c4
Garrett Wollman <wollman@FreeBSD.org>
1993-11-18 00:10:53 +0000

    Updated to work with changes for new ICMP and PMTU Discovery.
    Former-commit-id: 86805192898df460efc487df2a3f770fa8328463

--- sys/netns/ns_input.c
+++ sys/netns/ns_input.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1984, 1985, 1986, 1987 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ns_input.c	7.8 (Berkeley) 6/27/91  *	$Id$  */
+comment|/*  * Copyright (c) 1984, 1985, 1986, 1987 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ns_input.c	7.8 (Berkeley) 6/27/91  *	$Id: ns_input.c,v 1.2 1993/10/16 19:54:23 rgrimes Exp $  */
 end_comment
 
 begin_include
@@ -975,43 +975,90 @@ init|=
 block|{
 name|ECONNABORTED
 block|,
+comment|/* ifdown */
 name|ECONNABORTED
 block|,
+comment|/* routedead */
 literal|0
 block|,
+comment|/* #2 */
 literal|0
 block|,
+comment|/* quench2 */
 literal|0
 block|,
-literal|0
-block|,
-name|EHOSTDOWN
-block|,
-name|EHOSTUNREACH
-block|,
-name|ENETUNREACH
-block|,
-name|EHOSTUNREACH
-block|,
-name|ECONNREFUSED
-block|,
-name|ECONNREFUSED
-block|,
+comment|/* quench */
 name|EMSGSIZE
 block|,
+comment|/* msgsize */
+name|EHOSTDOWN
+block|,
+comment|/* hostdead */
+name|EHOSTUNREACH
+block|,
+comment|/* hostunreach */
+name|ENETUNREACH
+block|,
+comment|/* unreachnet */
+name|EHOSTUNREACH
+block|,
+comment|/* unreachhost */
+name|ECONNREFUSED
+block|,
+comment|/* unreachproto */
+name|ECONNREFUSED
+block|,
+comment|/* unreachport */
+name|EMSGSIZE
+block|,
+comment|/* old needfrag */
 literal|0
 block|,
+comment|/* srcfail */
 literal|0
 block|,
+comment|/* netunknown */
 literal|0
 block|,
+comment|/* hostunknown */
 literal|0
 block|,
+comment|/* isolated */
 literal|0
 block|,
+comment|/* net admin. prohibited */
 literal|0
 block|,
+comment|/* host admin. prohibited */
 literal|0
+block|,
+comment|/* tos net unreach */
+literal|0
+block|,
+comment|/* tos host unreach */
+literal|0
+block|,
+comment|/* redirect net */
+literal|0
+block|,
+comment|/* redirect host */
+literal|0
+block|,
+comment|/* redirect tosnet */
+literal|0
+block|,
+comment|/* redirect toshost */
+literal|0
+block|,
+comment|/* time exceeded */
+literal|0
+block|,
+comment|/* reassembly timeout */
+literal|0
+block|,
+comment|/* parameter problem */
+literal|0
+comment|/* required option missing */
 block|}
 decl_stmt|;
 end_decl_stmt
