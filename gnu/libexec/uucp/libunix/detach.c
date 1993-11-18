bf0416901904a43aed714ca2408bc06442fbb3e4
Andrey Chernov <ache@FreeBSD.org>
1993-11-18 00:59:17 +0000

    From Ian Taylor. Fix POSIX detach behaviour, based on setsid() call. stpgrp(0,0) not used for POSIX now.
    Former-commit-id: 5e29decd3ad76188956d434a7b32c30b414e76b4

--- gnu/libexec/uucp/libunix/detach.c
+++ gnu/libexec/uucp/libunix/detach.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* detach.c    Detach from the controlling terminal.     Copyright (C) 1992 Ian Lance Taylor     This file is part of the Taylor UUCP package.     This program is free software; you can redistribute it and/or    modify it under the terms of the GNU General Public License as    published by the Free Software Foundation; either version 2 of the    License, or (at your option) any later version.     This program is distributed in the hope that it will be useful, but    WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.     The author of the program may be contacted at ian@airs.com or    c/o Infinity Development Systems, P.O. Box 520, Waltham, MA 02254.    */
+comment|/* detach.c    Detach from the controlling terminal.     Copyright (C) 1992, 1993 Ian Lance Taylor     This file is part of the Taylor UUCP package.     This program is free software; you can redistribute it and/or    modify it under the terms of the GNU General Public License as    published by the Free Software Foundation; either version 2 of the    License, or (at your option) any later version.     This program is distributed in the hope that it will be useful, but    WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.     The author of the program may be contacted at ian@airs.com or    c/o Infinity Development Systems, P.O. Box 520, Waltham, MA 02254.    */
 end_comment
 
 begin_include
@@ -151,6 +151,30 @@ endif|#
 directive|endif
 end_endif
 
+begin_if
+if|#
+directive|if
+name|HAVE_BROKEN_SETSID
+end_if
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE_SETSID
+end_undef
+
+begin_define
+define|#
+directive|define
+name|HAVE_SETSID
+value|0
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_escape
 end_escape
 
@@ -163,17 +187,10 @@ name|void
 name|usysdep_detach
 parameter_list|()
 block|{
-if|#
-directive|if
-operator|!
-name|HAVE_BSD_PGRP
-operator|||
-operator|!
-name|HAVE_TIOCNOTTY
 name|pid_t
 name|igrp
 decl_stmt|;
-comment|/* First make sure we are not a process group leader.  If we have      TIOCNOTTY, this doesn't matter, since TIOCNOTTY sets our process      group to 0 anyhow.  */
+comment|/* Make sure we are not a process group leader.  */
 if|#
 directive|if
 name|HAVE_BSD_PGRP
@@ -295,13 +312,13 @@ name|NULL
 argument_list|)
 expr_stmt|;
 block|}
-endif|#
-directive|endif
-comment|/* ! HAVE_BSD_PGRP || ! HAVE_TIOCNOTTY */
 if|#
 directive|if
+operator|!
+name|HAVE_SETSID
+operator|&&
 name|HAVE_TIOCNOTTY
-comment|/* Lose the original controlling terminal.  If standard input has      been reopened to /dev/null, this will do no harm.  If another      port has been opened to become the controlling terminal, it      should have been detached when it was closed.  */
+comment|/* Lose the original controlling terminal as well as our process      group.  If standard input has been reopened to /dev/null, this      will do no harm.  If another port has been opened to become the      controlling terminal, it should have been detached when it was      closed.  */
 operator|(
 name|void
 operator|)
@@ -320,6 +337,7 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+comment|/* ! HAVE_SETSID&& HAVE_TIOCNOTTY */
 comment|/* Close stdin, stdout and stderr and reopen them on /dev/null, to      make sure we have no connection at all to the terminal.  */
 operator|(
 name|void
@@ -400,8 +418,43 @@ argument_list|)
 expr_stmt|;
 if|#
 directive|if
-name|HAVE_BSD_PGRP
-comment|/* Make sure our process group ID is set to 0.  On BSD TIOCNOTTY      should already have set it 0, so this will do no harm.  On System      V we presumably did not execute the TIOCNOTTY call, but the      System V setpgrp will detach the controlling terminal anyhow.      This lets us use the same code on both BSD and System V, provided      it compiles correctly, which life easier for the configure      script.  We don't output an error if we got EPERM because some      BSD variants don't permit this usage of setpgrp (which means they      don't provide any way to pick up a new controlling terminal).  */
+name|HAVE_SETSID
+comment|/* Under POSIX the setsid call creates a new session for which we      are the process group leader.  It also detaches us from our      controlling terminal.  */
+if|if
+condition|(
+name|setsid
+argument_list|()
+operator|<
+literal|0
+condition|)
+name|ulog
+argument_list|(
+name|LOG_ERROR
+argument_list|,
+literal|"setsid: %s"
+argument_list|,
+name|strerror
+argument_list|(
+name|errno
+argument_list|)
+argument_list|)
+expr_stmt|;
+else|#
+directive|else
+comment|/* ! HAVE_SETSID */
+if|#
+directive|if
+operator|!
+name|HAVE_SETPGRP
+error|#
+directive|error
+error|Cannot detach from controlling terminal
+endif|#
+directive|endif
+comment|/* If we don't have setsid, we must use setpgrp.  On an old System V      system setpgrp will make us the leader of a new process group and      detach the controlling terminal.  On an old BSD system the call      setpgrp (0, 0) will set our process group to 0 so that we can      acquire a new controlling terminal (TIOCNOTTY may or may not have      already done that anyhow).  */
+if|#
+directive|if
+name|HAVE_BSD_SETPGRP
 if|if
 condition|(
 name|setpgrp
@@ -413,7 +466,19 @@ argument_list|)
 operator|<
 literal|0
 condition|)
+else|#
+directive|else
+if|if
+condition|(
+name|setpgrp
+argument_list|()
+operator|<
+literal|0
+condition|)
+endif|#
+directive|endif
 block|{
+comment|/* Some systems seem to give EPERM errors inappropriately.  */
 if|if
 condition|(
 name|errno
@@ -433,60 +498,9 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
-else|#
-directive|else
-comment|/* ! HAVE_BSD_PGRP */
-if|#
-directive|if
-name|HAVE_SETSID
-comment|/* Under POSIX the setsid call creates a new session for which we      are the process group leader.  It also detaches us from our      controlling terminal.  I'm using the BSD setpgrp call first      because they should be equivalent for my purposes, but it turns      out that on Ultrix 4.0 setsid prevents us from ever acquiring      another controlling terminal (it does not change our process      group, and Ultrix 4.0 prevents us from setting our process group      to 0).  */
-operator|(
-name|void
-operator|)
-name|setsid
-argument_list|()
-expr_stmt|;
-else|#
-directive|else
-comment|/* ! HAVE_SETSID */
-if|#
-directive|if
-name|HAVE_SETPGRP
-comment|/* Now we assume we have the System V setpgrp, which takes no      arguments, and we couldn't compile the HAVE_BSD_PGRP code above      because there was a prototype somewhere in scope.  On System V      setpgrp makes us the leader of a new process group and also      detaches the controlling terminal.  */
-if|if
-condition|(
-name|setpgrp
-argument_list|()
-operator|<
-literal|0
-condition|)
-name|ulog
-argument_list|(
-name|LOG_ERROR
-argument_list|,
-literal|"setpgrp: %s"
-argument_list|,
-name|strerror
-argument_list|(
-name|errno
-argument_list|)
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
-comment|/* ! HAVE_SETPGRP */
-error|#
-directive|error
-error|Must detach from controlling terminal
-endif|#
-directive|endif
-comment|/* HAVE_SETPGRP */
 endif|#
 directive|endif
 comment|/* ! HAVE_SETSID */
-endif|#
-directive|endif
-comment|/* ! HAVE_BSD_PGRP */
 comment|/* At this point we have completely detached from our controlling      terminal.  The next terminal device we open will probably become      our controlling terminal.  */
 block|}
 end_function
