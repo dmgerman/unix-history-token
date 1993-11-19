aed0c506a6e51b9364b23205cc976f5076619db6
David Greenman <davidg@Root.COM>
1993-11-19 06:30:02 +0000

    added 5us delay before checking BUSY flag to conform to ATA spec - as suggested by John Dyson (dyson@implode.rain.com)
    Former-commit-id: cfa61942d4adbac0e9674559f9e426a4351b2266

--- sys/i386/isa/wd.c
+++ sys/i386/isa/wd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.12 1993/11/17 23:25:20 wollman Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.13 1993/11/18 05:02:22 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -1962,6 +1962,12 @@ name|timeout
 operator|=
 literal|0
 expr_stmt|;
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
+argument_list|)
+expr_stmt|;
 while|while
 condition|(
 name|inb
@@ -2578,6 +2584,12 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
+argument_list|)
+expr_stmt|;
 while|while
 condition|(
 operator|(
@@ -3871,6 +3883,12 @@ name|b_active
 operator|=
 literal|1
 expr_stmt|;
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
+argument_list|)
+expr_stmt|;
 comment|/* wait for drive and controller to become ready */
 for|for
 control|(
@@ -4109,6 +4127,12 @@ name|du
 operator|->
 name|dk_port
 expr_stmt|;
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
+argument_list|)
+expr_stmt|;
 while|while
 condition|(
 operator|(
@@ -4153,6 +4177,12 @@ operator|+
 name|wd_command
 argument_list|,
 name|cmd
+argument_list|)
+expr_stmt|;
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
 argument_list|)
 expr_stmt|;
 while|while
@@ -5989,6 +6019,12 @@ name|i
 operator|=
 literal|100000
 expr_stmt|;
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
+argument_list|)
+expr_stmt|;
 while|while
 condition|(
 operator|(
@@ -6034,6 +6070,12 @@ argument_list|,
 name|WDCC_RESTORE
 operator||
 name|WD_STEP
+argument_list|)
+expr_stmt|;
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
 argument_list|)
 expr_stmt|;
 while|while
@@ -6485,6 +6527,12 @@ name|EIO
 operator|)
 return|;
 comment|/* wait for completion */
+comment|/* must delay 5us to conform to ATA spec */
+name|DELAY
+argument_list|(
+literal|5
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|i
