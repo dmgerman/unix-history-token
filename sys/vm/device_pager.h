02fbc4f75abb5a3e1f38ba689e5b12a72f897851
David Greenman <davidg@Root.COM>
1993-11-22 09:49:54 +0000

    patches necessary to allow "linear" memory mapping for localbus video cards. Included in this is a 4.4 device_pager from Mike Hibler as well as changes from myself and Julian Elischer.
    Former-commit-id: d6d8ae7e1be8b46222457aa64e965258f2276d77

--- sys/vm/device_pager.h
+++ sys/vm/device_pager.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)device_pager.h	7.1 (Berkeley) 12/5/90  *	$Id: device_pager.h,v 1.2 1993/10/16 16:20:12 rgrimes Exp $  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)device_pager.h	8.1 (Berkeley) 6/11/93  */
 end_comment
 
 begin_ifndef
@@ -25,25 +25,9 @@ struct|struct
 name|devpager
 block|{
 name|queue_head_t
-name|devp_list
+name|devp_pglist
 decl_stmt|;
-comment|/* list of managed devices */
-name|dev_t
-name|devp_dev
-decl_stmt|;
-comment|/* devno of device */
-name|vm_page_t
-name|devp_pages
-decl_stmt|;
-comment|/* page structs for device */
-name|int
-name|devp_npages
-decl_stmt|;
-comment|/* size of device in pages */
-name|int
-name|devp_count
-decl_stmt|;
-comment|/* reference count */
+comment|/* list of pages allocated */
 name|vm_object_t
 name|devp_object
 decl_stmt|;
@@ -60,74 +44,6 @@ modifier|*
 name|dev_pager_t
 typedef|;
 end_typedef
-
-begin_define
-define|#
-directive|define
-name|DEV_PAGER_NULL
-value|((dev_pager_t)0)
-end_define
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|KERNEL
-end_ifdef
-
-begin_function_decl
-name|void
-name|dev_pager_init
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|vm_pager_t
-name|dev_pager_alloc
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
-name|dev_pager_dealloc
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_decl_stmt
-name|boolean_t
-name|dev_pager_getpage
-argument_list|()
-decl_stmt|,
-name|dev_pager_putpage
-argument_list|()
-decl_stmt|;
-end_decl_stmt
-
-begin_function_decl
-name|boolean_t
-name|dev_pager_haspage
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|pagerops
-name|devicepagerops
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* KERNEL */
-end_comment
 
 begin_endif
 endif|#
