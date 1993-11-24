20fca6eb3fa3eca618e22e62ac7d29f943a6f0d3
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-11-24 07:41:28 -0800

    fix comment, Pinnacle is no longer secret
    SCCS-vsn: sys/sparc/sparc/cpu.c 8.5
    
    Former-commit-id: ee7fa857db68ac75d6f1c7fdb1c72d671aeeba38

--- usr/src/sys/sparc/sparc/cpu.c
+++ usr/src/sys/sparc/sparc/cpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)cpu.c	8.4 (Berkeley) %G%  *  * from: $Header: cpu.c,v 1.18 93/10/31 05:28:13 torek Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)cpu.c	8.5 (Berkeley) %G%  *  * from: $Header: cpu.c,v 1.18 93/10/31 05:28:13 torek Exp $ (LBL)  */
 end_comment
 
 begin_include
@@ -978,7 +978,7 @@ block|,
 literal|"on-chip"
 block|}
 block|,
-comment|/* Pinnacle (shhh) */
+comment|/* Pinnacle */
 block|{
 literal|1
 block|,
