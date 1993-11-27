6c7c1fc009a06aa84f4685536238f963dbbd355d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-11-26 19:11:35 -0800

    convert to new <sys/queue.h> structures
    SCCS-vsn: usr.bin/man/config.h 8.3
    
    Former-commit-id: a525bd2fc27ee9a1e47e56b77d66d833d1a338d8

--- usr/src/usr.bin/man/config.h
+++ usr/src/usr.bin/man/config.h
@@ -1,23 +1,55 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)config.h	8.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)config.h	8.3 (Berkeley) %G%  */
 end_comment
+
+begin_typedef
+typedef|typedef
+struct|struct
+name|_tag
+block|{
+name|TAILQ_ENTRY
+argument_list|(
+argument|_tag
+argument_list|)
+name|q
+expr_stmt|;
+comment|/* Queue of tags. */
+name|TAILQ_HEAD
+argument_list|(
+argument|tqh
+argument_list|,
+argument|_entry
+argument_list|)
+name|list
+expr_stmt|;
+comment|/* Queue of entries. */
+name|char
+modifier|*
+name|s
+decl_stmt|;
+comment|/* Associated string. */
+name|size_t
+name|len
+decl_stmt|;
+comment|/* Length of 's'. */
+block|}
+name|TAG
+typedef|;
+end_typedef
 
 begin_typedef
 typedef|typedef
 struct|struct
 name|_entry
 block|{
-name|struct
-name|queue_entry
-name|list
-decl_stmt|;
-comment|/* List of items. */
-name|struct
-name|queue_entry
-name|tags
-decl_stmt|;
-comment|/* List of tags. */
+name|TAILQ_ENTRY
+argument_list|(
+argument|_entry
+argument_list|)
+name|q
+expr_stmt|;
+comment|/* Queue of entries. */
 name|char
 modifier|*
 name|s
@@ -32,16 +64,26 @@ name|ENTRY
 typedef|;
 end_typedef
 
+begin_expr_stmt
+name|TAILQ_HEAD
+argument_list|(
+name|_head
+argument_list|,
+name|_tag
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_decl_stmt
 specifier|extern
 name|struct
-name|queue_entry
+name|_head
 name|head
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|ENTRY
+name|TAG
 modifier|*
 name|addlist
 name|__P
@@ -81,7 +123,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|ENTRY
+name|TAG
 modifier|*
 name|getlist
 name|__P
