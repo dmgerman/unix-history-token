548b11f78e681a1f939475b09c9c741baf356a12
Andrey Chernov <ache@FreeBSD.org>
1993-11-29 14:50:27 +0000

    Weird bug with kern_kinfo now fixed!  (applauds) Missing argument to thread_block() caused this. Added vm.h for prototype and add string argument to thread_block
    Former-commit-id: 00da6cc863871ca9c12d58cd004e0a8f54b3fc12

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *	from: @(#)swap_pager.c	7.4 (Berkeley) 5/7/91  *	$Id: swap_pager.c,v 1.4 1993/11/07 21:48:34 wollman Exp $  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *	from: @(#)swap_pager.c	7.4 (Berkeley) 5/7/91  *	$Id: swap_pager.c,v 1.5 1993/11/25 01:38:56 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -66,43 +66,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|"queue.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"rlist.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"vm_param.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"queue.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"lock.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"vm_prot.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"vm_object.h"
+file|"vm.h"
 end_include
 
 begin_include
@@ -1480,7 +1450,9 @@ name|swp
 argument_list|)
 expr_stmt|;
 name|thread_block
-argument_list|()
+argument_list|(
+literal|"swpgde"
+argument_list|)
 expr_stmt|;
 block|}
 name|splx
@@ -2182,7 +2154,9 @@ name|swp
 argument_list|)
 expr_stmt|;
 name|thread_block
-argument_list|()
+argument_list|(
+literal|"swpgio"
+argument_list|)
 expr_stmt|;
 block|}
 else|#
@@ -3108,7 +3082,9 @@ name|bp
 argument_list|)
 expr_stmt|;
 name|thread_block
-argument_list|()
+argument_list|(
+literal|"swpgio"
+argument_list|)
 expr_stmt|;
 block|}
 ifdef|#
