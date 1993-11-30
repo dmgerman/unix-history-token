f72ceaa66f46ab92340a0209dd472b90a6e5fbfd
Jordan K. Hubbard <jkh@FreeBSD.org>
1993-11-30 21:04:34 +0000

    More recent changes from Paul K.
    Former-commit-id: 259600e1d261f63816b6abd477abaf936c60fefd

--- gnu/usr.bin/cc/lib/tm.h
+++ gnu/usr.bin/cc/lib/tm.h
@@ -319,12 +319,6 @@ begin_comment
 comment|/* Write the extra assembler code needed to declare a function properly.    Some svr4 assemblers need to also have something extra said about the    function's return value.  We allow for that here.  */
 end_comment
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|notyet
-end_ifdef
-
 begin_define
 define|#
 directive|define
@@ -337,7 +331,7 @@ parameter_list|,
 name|DECL
 parameter_list|)
 define|\
-value|do {                         \     fprintf (FILE, "\t%s\t ", TYPE_ASM_OP);            \     assemble_name (FILE, NAME);                  \     putc (',', FILE);                   \     fprintf (FILE, TYPE_OPERAND_FMT, "function");         \     putc ('\n', FILE);                     \     ASM_DECLARE_RESULT (FILE, DECL_RESULT (DECL));        \     ASM_OUTPUT_LABEL(FILE, NAME);             \   } while (0)
+value|do {									\     fprintf (FILE, "\t%s\t ", TYPE_ASM_OP);				\     assemble_name (FILE, NAME);						\     putc (',', FILE);							\     fprintf (FILE, TYPE_OPERAND_FMT, "function");			\     putc ('\n', FILE);							\     ASM_DECLARE_RESULT (FILE, DECL_RESULT (DECL));			\     ASM_OUTPUT_LABEL(FILE, NAME);					\   } while (0)
 end_define
 
 begin_comment
@@ -356,7 +350,7 @@ parameter_list|,
 name|DECL
 parameter_list|)
 define|\
-value|do {                         \     fprintf (FILE, "\t%s\t ", TYPE_ASM_OP);            \     assemble_name (FILE, NAME);                  \     putc (',', FILE);                   \     fprintf (FILE, TYPE_OPERAND_FMT, "object");           \     putc ('\n', FILE);                     \     if (!flag_inhibit_size_directive)               \       {                           \  fprintf (FILE, "\t%s\t ", SIZE_ASM_OP);            \  assemble_name (FILE, NAME);               \  fprintf (FILE, ",%d\n",  int_size_in_bytes (TREE_TYPE (decl)));   \       }                           \     ASM_OUTPUT_LABEL(FILE, NAME);             \   } while (0)
+value|do {									\     fprintf (FILE, "\t%s\t ", TYPE_ASM_OP);				\     assemble_name (FILE, NAME);						\     putc (',', FILE);							\     fprintf (FILE, TYPE_OPERAND_FMT, "object");				\     putc ('\n', FILE);							\     if (!flag_inhibit_size_directive)					\       {									\ 	fprintf (FILE, "\t%s\t ", SIZE_ASM_OP);				\ 	assemble_name (FILE, NAME);					\ 	fprintf (FILE, ",%d\n",  int_size_in_bytes (TREE_TYPE (decl)));	\       }									\     ASM_OUTPUT_LABEL(FILE, NAME);					\   } while (0)
 end_define
 
 begin_comment
@@ -375,27 +369,7 @@ parameter_list|,
 name|DECL
 parameter_list|)
 define|\
-value|do {                         \     if (!flag_inhibit_size_directive)               \       {                           \         char label[256];                \  static int labelno;                 \  labelno++;                    \  ASM_GENERATE_INTERNAL_LABEL (label, "Lfe", labelno);     \  ASM_OUTPUT_INTERNAL_LABEL (FILE, "Lfe", labelno);     \  fprintf (FILE, "\t%s\t ", SIZE_ASM_OP);            \  assemble_name (FILE, (FNAME));               \         fprintf (FILE, ",");                  \  assemble_name (FILE, label);              \         fprintf (FILE, "-");                  \  assemble_name (FILE, (FNAME));               \  putc ('\n', FILE);                  \       }                           \   } while (0)
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_define
-define|#
-directive|define
-name|ASM_DECLARE_OBJECT_NAME
-parameter_list|(
-name|FILE
-parameter_list|,
-name|NAME
-parameter_list|,
-name|DECL
-parameter_list|)
-define|\
-value|do {                         \     if (!flag_inhibit_size_directive)               \       {                           \  fprintf (FILE, "\t%s\t ", SIZE_ASM_OP);            \  assemble_name (FILE, NAME);               \  fprintf (FILE, ",%d\n",  int_size_in_bytes (TREE_TYPE (decl)));   \       }                           \     ASM_OUTPUT_LABEL(FILE, NAME);             \   } while (0)
+value|do {									\     if (!flag_inhibit_size_directive)					\       {									\         char label[256];						\ 	static int labelno;						\ 	labelno++;							\ 	ASM_GENERATE_INTERNAL_LABEL (label, "Lfe", labelno);		\ 	ASM_OUTPUT_INTERNAL_LABEL (FILE, "Lfe", labelno);		\ 	fprintf (FILE, "\t%s\t ", SIZE_ASM_OP);				\ 	assemble_name (FILE, (FNAME));					\         fprintf (FILE, ",");						\ 	assemble_name (FILE, label);					\         fprintf (FILE, "-");						\ 	assemble_name (FILE, (FNAME));					\ 	putc ('\n', FILE);						\       }									\   } while (0)
 end_define
 
 end_unit
