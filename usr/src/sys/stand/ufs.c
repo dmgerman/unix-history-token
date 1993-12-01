b74adc348754a14ab85244108e278c192290696d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-12-01 00:25:02 -0800

    update for USL
    SCCS-vsn: sys/stand/ufs.c 8.2
    SCCS-vsn: sys/sys/kernel.h 8.2
    
    Former-commit-id: 172e62b8aa796af893daa0c08d9025e68089c870

--- usr/src/sys/stand/ufs.c
+++ usr/src/sys/stand/ufs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)ufs.c	8.1 (Berkeley) %G%  *    *  * Copyright (c) 1990, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Author: David Golub  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)ufs.c	8.2 (Berkeley) %G%  *    *  * Copyright (c) 1990, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Author: David Golub  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -81,7 +81,7 @@ name|NIADDR
 index|]
 decl_stmt|;
 comment|/* buffer for indirect block at 					   level i */
-name|u_long
+name|u_int
 name|f_blksize
 index|[
 name|NIADDR
@@ -199,7 +199,7 @@ name|fsbtodb
 argument_list|(
 name|fs
 argument_list|,
-name|itod
+name|ino_to_fsba
 argument_list|(
 name|fs
 argument_list|,
@@ -263,7 +263,7 @@ name|f_di
 operator|=
 name|dp
 index|[
-name|itoo
+name|ino_to_fsbo
 argument_list|(
 name|fs
 argument_list|,
