16a441475386eea19c50ff4dac60b0ea1288e897
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-12-07 04:39:45 -0800

    sync up to hp300/include/pmap.h 7.13 (Berkeley) 6/2/93
    SCCS-vsn: sys/luna68k/include/pmap.h 8.2
    
    Former-commit-id: e3b1104a4298dc1fbd7c2ef200dfcc617414effa

--- usr/src/sys/luna68k/include/pmap.h
+++ usr/src/sys/luna68k/include/pmap.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1987 Carnegie-Mellon University  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: hp300/include/pmap.h	7.11 (Berkeley) 12/27/92  *  *	@(#)pmap.h	8.1 (Berkeley) %G%  */
+comment|/*   * Copyright (c) 1987 Carnegie-Mellon University  * Copyright (c) 1992 OMRON Corporation.  * Copyright (c) 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: hp300/include/pmap.h	7.13 (Berkeley) 6/2/93  *  *	@(#)pmap.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -409,11 +409,18 @@ endif|#
 directive|endif
 end_endif
 
+begin_comment
+comment|/* KERNEL */
+end_comment
+
 begin_endif
 endif|#
 directive|endif
-endif|_PMAP_MACHINE_
 end_endif
+
+begin_comment
+comment|/* _PMAP_MACHINE_ */
+end_comment
 
 end_unit
 
