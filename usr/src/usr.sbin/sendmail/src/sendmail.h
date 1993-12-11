ceffea75c0ad684b724566410b53f54f0a396720
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-11 00:03:07 -0800

    add SF_ANYFILE
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.30
    
    Former-commit-id: bf402e4385fede583a4d8c89aa0007fcb14e2eac

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.29 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.30 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.29		%G%"
+literal|"@(#)sendmail.h	8.30		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3614,6 +3614,17 @@ end_define
 
 begin_comment
 comment|/* **  Flags passed to safefile. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SF_ANYFILE
+value|0
+end_define
+
+begin_comment
+comment|/* no special restrictions */
 end_comment
 
 begin_define
