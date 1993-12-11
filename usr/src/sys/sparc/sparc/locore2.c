8cad207f809c7607a687fe93babb8e9c10f1796b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-12-11 02:10:37 -0800

    lint
    SCCS-vsn: sys/sparc/sparc/locore2.c 8.4
    SCCS-vsn: sys/sparc/sparc/mem.c 8.2
    
    Former-commit-id: 3f0888232c101660338bbb4a3cfb4dcff1374f24

--- usr/src/sys/sparc/sparc/locore2.c
+++ usr/src/sys/sparc/sparc/locore2.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)locore2.c	8.3 (Berkeley) %G%  *  * from: $Header: locore2.c,v 1.8 92/11/26 03:05:01 mccanne Exp $ (LBL)  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)locore2.c	8.4 (Berkeley) %G%  *  * from: $Header: locore2.c,v 1.8 92/11/26 03:05:01 mccanne Exp $ (LBL)  */
 end_comment
 
 begin_comment
@@ -35,20 +35,18 @@ begin_comment
 comment|/*  * Put process p on the run queue indicated by its priority.  * Calls should be made at splstatclock(), and p->p_stat should be SRUN.  */
 end_comment
 
-begin_expr_stmt
+begin_function
+name|void
 name|setrunqueue
-argument_list|(
+parameter_list|(
 name|p
-argument_list|)
+parameter_list|)
 specifier|register
-expr|struct
+name|struct
 name|proc
-operator|*
+modifier|*
 name|p
-expr_stmt|;
-end_expr_stmt
-
-begin_block
+decl_stmt|;
 block|{
 specifier|register
 name|struct
@@ -133,7 +131,7 @@ operator|=
 name|p
 expr_stmt|;
 block|}
-end_block
+end_function
 
 begin_comment
 comment|/*  * Remove process p from its run queue, which should be the one  * indicated by its priority.  Calls should be made at splstatclock().  */
