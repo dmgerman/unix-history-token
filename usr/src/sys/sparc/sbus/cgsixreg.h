9df288b5a50df4eb9d5bf86bbcd99a3cb78f11c5
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-12-12 23:46:12 -0800

    accomodate bugs in rev 1 cg6 boards
    SCCS-vsn: sys/sparc/sbus/cgsix.c 8.2
    SCCS-vsn: sys/sparc/sbus/cgsixreg.h 8.2
    
    Former-commit-id: 133a4f0355430b2070e7b1b8b1b45f8cd7ef1769

--- usr/src/sys/sparc/sbus/cgsixreg.h
+++ usr/src/sys/sparc/sbus/cgsixreg.h
@@ -1,11 +1,285 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)cgsixreg.h	8.1 (Berkeley) %G%  *  * from: $Header: cgsixreg.h,v 1.1 93/10/12 15:29:24 torek Exp $  */
+comment|/*  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * All advertising materials mentioning features or use of this software  * must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Lawrence Berkeley Laboratory.  *  * %sccs.include.redist.c%  *  *	@(#)cgsixreg.h	8.2 (Berkeley) %G%  *  * from: $Header: cgsixreg.h,v 1.1 93/10/12 15:29:24 torek Exp $  */
 end_comment
 
 begin_comment
-comment|/*  * CG6 display registers.  (Note, I got tired of writing `cgsix' about  * halfway through and changed everything to cg6, but I probably missed  * some.  Unfortunately, the way config works, we need to spell out `six'  * in some places anyway.)  *  * The cg6 is a complicated beastie.  We have been unable to extract any  * documentation and most of the following are guesses based on a limited  * amount of reverse engineering.  *  * A cg6 is composed of numerous groups of control registers, all with TLAs:  *	FBC - frame buffer control?  *	FHC - fbc hardware configuration?  *	DHC - ???  *	TEC - transform engine control?  *	THC - TEC Hardware Configuration (this is where our action goes)  *	ROM - a 64Kbyte ROM with who knows what in it.  *	colormap - see below  *	frame buffer memory (video RAM)  *	possible other stuff  *  * Like the cg3, the cg6 uses a Brooktree Video DAC (see btreg.h).  */
+comment|/*  * CG6 display registers.  (Note, I got tired of writing `cgsix' about  * halfway through and changed everything to cg6, but I probably missed  * some.  Unfortunately, the way config works, we need to spell out `six'  * in some places anyway.)  *  * The cg6 is a complicated beastie.  We have been unable to extract any  * documentation and most of the following are guesses based on a limited  * amount of reverse engineering.  *  * A cg6 is composed of numerous groups of control registers, all with TLAs:  *	FBC - frame buffer control?  *	FHC - fbc hardware configuration / control? register (32 bits)  *	DHC - ???  *	TEC - transform engine control?  *	THC - TEC Hardware Configuration (this is where our action goes)  *	ROM - a 64Kbyte ROM with who knows what in it.  *	colormap - see below  *	frame buffer memory (video RAM)  *	possible other stuff  *  * Like the cg3, the cg6 uses a Brooktree Video DAC (see btreg.h).  *  * Various revisions of the cgsix have various hardware bugs.  So far,  * we have only seen rev 1& 2.  */
 end_comment
+
+begin_comment
+comment|/* bits in FHC register */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_FBID_MASK
+value|0xff000000
+end_define
+
+begin_comment
+comment|/* bits 24..31 are frame buffer ID */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_FBID_SHIFT
+value|24
+end_define
+
+begin_define
+define|#
+directive|define
+name|FHC_REV_MASK
+value|0x00f00000
+end_define
+
+begin_comment
+comment|/* bits 20..23 are revision */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_REV_SHIFT
+value|20
+end_define
+
+begin_define
+define|#
+directive|define
+name|FHC_FROP_DISABLE
+value|0x00080000
+end_define
+
+begin_comment
+comment|/* disable fast/font? rasterops */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_ROW_DISABLE
+value|0x00040000
+end_define
+
+begin_comment
+comment|/* ??? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_SRC_DISABLE
+value|0x00020000
+end_define
+
+begin_comment
+comment|/* ??? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_DST_DISABLE
+value|0x00010000
+end_define
+
+begin_comment
+comment|/* disable destination cache */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_RESET
+value|0x00008000
+end_define
+
+begin_comment
+comment|/* ??? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_XXX0
+value|0x00004000
+end_define
+
+begin_comment
+comment|/* ??? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_LEBO
+value|0x00002000
+end_define
+
+begin_comment
+comment|/* set little endian byte order? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_RES_MASK
+value|0x00001800
+end_define
+
+begin_comment
+comment|/* bits 11&12 are resolution */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_RES_1024
+value|0x00000000
+end_define
+
+begin_comment
+comment|/* res = 1024x768 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_RES_1152
+value|0x00000800
+end_define
+
+begin_comment
+comment|/* res = 1152x900 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_RES_1280
+value|0x00001000
+end_define
+
+begin_comment
+comment|/* res = 1280x1024 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_RES_1600
+value|0x00001800
+end_define
+
+begin_comment
+comment|/* res = 1600x1200 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_CPU_MASK
+value|0x00000600
+end_define
+
+begin_comment
+comment|/* bits 9&10 are cpu type */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_CPU_SPARC
+value|0x00000000
+end_define
+
+begin_comment
+comment|/* cpu = sparc */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_CPU_68020
+value|0x00000200
+end_define
+
+begin_comment
+comment|/* cpu = 68020 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_CPU_386
+value|0x00000400
+end_define
+
+begin_comment
+comment|/* cpu = 80386 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_CPU_XXX
+value|0x00000600
+end_define
+
+begin_comment
+comment|/* ??? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_TEST
+value|0x00000100
+end_define
+
+begin_comment
+comment|/* ??? test window ??? */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_TESTX_MASK
+value|0x000000f0
+end_define
+
+begin_comment
+comment|/* bits 4..7 are test window X */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_TESTX_SHIFT
+value|4
+end_define
+
+begin_define
+define|#
+directive|define
+name|FHC_TESTY_MASK
+value|0x0000000f
+end_define
+
+begin_comment
+comment|/* bits 0..3 are test window Y */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FHC_TESTY_SHIFT
+value|0
+end_define
 
 begin_comment
 comment|/*  * The layout of the THC.  */
@@ -319,9 +593,12 @@ decl_stmt|;
 block|}
 name|cg6_alt_un
 union|;
-comment|/* FHC, whatever that is, at 0x300000 */
+comment|/* FHC register at 0x300000 */
 union|union
 block|{
+name|int
+name|un_fhc
+decl_stmt|;
 name|char
 name|un_pad
 index|[
