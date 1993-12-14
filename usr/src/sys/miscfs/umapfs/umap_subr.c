3c1dcb6e220a0e0e57f0e21464db9e87f75764a6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-12-14 08:44:21 -0800

    new vget parameter
    SCCS-vsn: sys/miscfs/umapfs/umap_subr.c 8.2
    
    Former-commit-id: 31a15fe1f9f3ce3fa91112a84bf0bfc093227498

--- usr/src/sys/miscfs/umapfs/umap_subr.c
+++ usr/src/sys/miscfs/umapfs/umap_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)umap_subr.c	8.1 (Berkeley) %G%  *  * $Id: lofs_subr.c,v 1.11 1992/05/30 10:05:43 jsp Exp jsp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)umap_subr.c	8.2 (Berkeley) %G%  *  * $Id: lofs_subr.c,v 1.11 1992/05/30 10:05:43 jsp Exp jsp $  */
 end_comment
 
 begin_include
@@ -332,9 +332,11 @@ expr_stmt|;
 comment|/* 			 * We need vget for the VXLOCK 			 * stuff, but we don't want to lock 			 * the lower node. 			 */
 if|if
 condition|(
-name|vget_nolock
+name|vget
 argument_list|(
 name|vp
+argument_list|,
+literal|0
 argument_list|)
 condition|)
 block|{
