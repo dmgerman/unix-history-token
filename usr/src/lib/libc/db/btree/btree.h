48b8ca321c76631d2675ed69de605fb4774b98aa
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-12-18 19:22:52 -0800

    typo
    SCCS-vsn: lib/libc/db/btree/btree.h 8.4
    
    Former-commit-id: 551ffe96388bc8acfdf2d367ab3533bafe149096

--- usr/src/lib/libc/db/btree/btree.h
+++ usr/src/lib/libc/db/btree/btree.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Olson.  *  * %sccs.include.redist.c%  *  *	@(#)btree.h	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Olson.  *  * %sccs.include.redist.c%  *  *	@(#)btree.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -752,7 +752,7 @@ block|,
 name|BACK
 block|,
 name|FORWARD
-block|, }
+block|}
 name|bt_order
 enum|;
 name|EPGNO
