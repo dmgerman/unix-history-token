fe778db031da2124fb647dc18f0ab9f9172112f0
David Greenman <davidg@Root.COM>
1993-12-21 21:28:31 +0000

    Remove type cast for fillw's second arg; it messed up the pointer arith and broke pccons.
    Former-commit-id: 803291954147640cb52551a925512d3d3bb07157

--- sys/i386/isa/pccons.c
+++ sys/i386/isa/pccons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)pccons.c	5.11 (Berkeley) 5/21/91  *	$Id: pccons.c,v 1.9 1993/11/25 01:31:46 wollman Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)pccons.c	5.11 (Berkeley) 5/21/91  *	$Id: pccons.c,v 1.10 1993/12/19 00:50:44 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -2941,9 +2941,6 @@ operator|)
 operator||
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|crtat
 argument_list|,
 name|COL
@@ -3543,9 +3540,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|crtat
 argument_list|,
 name|Crtat
@@ -3581,9 +3575,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|Crtat
 argument_list|,
 name|crtat
@@ -3613,9 +3604,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|Crtat
 argument_list|,
 name|vs
@@ -3669,9 +3657,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|crtat
 argument_list|,
 name|vs
@@ -3709,9 +3694,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|crtat
 operator|-
 operator|(
@@ -3759,9 +3741,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|crtat
 operator|-
 operator|(
@@ -3948,9 +3927,6 @@ operator|+
 literal|' '
 argument_list|,
 operator|(
-operator|(
-name|caddr_t
-operator|)
 name|Crtat
 operator|+
 name|vs
@@ -4057,9 +4033,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|Crtat
 argument_list|,
 name|vs
@@ -4415,9 +4388,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|Crtat
 argument_list|,
 name|vs
@@ -4659,9 +4629,6 @@ operator|)
 operator|+
 literal|' '
 argument_list|,
-operator|(
-name|caddr_t
-operator|)
 name|Crtat
 operator|+
 name|vs
