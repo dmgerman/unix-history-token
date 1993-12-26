750c499f7858d2ffe51fd01b9ab9c455dff1438e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-26 15:46:54 -0800

    SCO UNIX doesn't have getusershell(3)
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.70
    
    Former-commit-id: 1020ba7df31a25c891aedbf7fc4f0c007800227f

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.69 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.70 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2138,6 +2138,17 @@ end_define
 
 begin_comment
 comment|/* has the statfs(2) syscall */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HASGETUSERSHELL
+value|0
+end_define
+
+begin_comment
+comment|/* does not have getusershell(3) call */
 end_comment
 
 begin_define
