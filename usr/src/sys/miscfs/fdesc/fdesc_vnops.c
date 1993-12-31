2610ecb401cceceedcc7a9b6595b69b65ea347b6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-12-31 00:17:21 -0800

    vget now locks optionally
    SCCS-vsn: sys/miscfs/fdesc/fdesc_vnops.c 8.4
    
    Former-commit-id: 865faf22564da63c8567de7f935554efc2b3e07c

--- usr/src/sys/miscfs/fdesc/fdesc_vnops.c
+++ usr/src/sys/miscfs/fdesc/fdesc_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)fdesc_vnops.c	8.3 (Berkeley) %G%  *  * $Id: fdesc_vnops.c,v 1.12 1993/04/06 16:17:17 jsp Exp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)fdesc_vnops.c	8.4 (Berkeley) %G%  *  * $Id: fdesc_vnops.c,v 1.12 1993/04/06 16:17:17 jsp Exp $  */
 end_comment
 
 begin_comment
@@ -284,6 +284,8 @@ name|vget
 argument_list|(
 operator|*
 name|nvpp
+argument_list|,
+literal|1
 argument_list|)
 condition|)
 goto|goto
