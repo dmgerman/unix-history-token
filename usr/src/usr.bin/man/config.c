33b7b6249a6c7c8b7aa15abe4d3f3974b10e614f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-02 18:26:02 -0800

    len == 1 for empty lines, now
    SCCS-vsn: usr.bin/man/config.c 8.6
    
    Former-commit-id: 96e015873538b8dacff312d797b1347b77406335

--- usr/src/usr.bin/man/config.c
+++ usr/src/usr.bin/man/config.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)config.c	8.5 (Berkeley) %G%"
+literal|"@(#)config.c	8.6 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -201,8 +201,9 @@ control|)
 block|{
 if|if
 condition|(
-operator|!
 name|len
+operator|==
+literal|1
 condition|)
 comment|/* Skip empty lines. */
 continue|continue;
