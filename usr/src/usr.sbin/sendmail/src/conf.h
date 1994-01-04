72df9d484d19673da330d605918f3c5c9710cad4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-03 17:32:25 -0800

    HP-UX portability
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.72
    
    Former-commit-id: b905f9442ed2dd3efe56de7d15c3cfeeb5ea517c

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.71 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.72 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -467,9 +467,34 @@ end_define
 begin_define
 define|#
 directive|define
+name|GIDSET_T
+value|gid_t
+end_define
+
+begin_define
+define|#
+directive|define
 name|_PATH_UNIX
 value|"/hp-ux"
 end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_PATH_SENDMAILCF
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_PATH_SENDMAILCF
+value|"/usr/lib/sendmail.cf"
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
