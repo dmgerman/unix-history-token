f9a363d4dd666afbce88c668cfe5052056ce58ff
Nate Willams <nate@FreeBSD.org>
1994-01-04 20:05:26 +0000

    New wd driver, based on Bruce Evans 'wx/altwd' driver which has passed enough tests to be considered more stable than current driver.
    Lots of work by Bruce, David G., and Guido have gone into this version, and
    more is to come in the future.
    
    Support for multiple controllers is in, but doesn't work correctly with
    different controllers (IDE AND MFM), but multiple alike controllers appears
    to work.
    
    Most of the stray interrupts problems should be fixed, although you will
    get a couple 'extra interrupts' when disklabeling and on startup.
    
    
    Former-commit-id: d699ef0247e5f5482c25e7f7b8115f9a878750af

--- sys/i386/isa/wdreg.h
+++ sys/i386/isa/wdreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wdreg.h	7.1 (Berkeley) 5/9/91  *	$Id: wdreg.h,v 1.4 1993/10/16 13:46:33 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wdreg.h	7.1 (Berkeley) 5/9/91  *	$Id: wxreg.h,v 1.1 1993/10/26 22:26:39 nate Exp $  */
 end_comment
 
 begin_comment
@@ -557,34 +557,13 @@ comment|/*  * wd driver entry points  */
 end_comment
 
 begin_function_decl
-name|int
-name|wdprobe
-parameter_list|(
-name|struct
-name|isa_device
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|wdattach
-parameter_list|(
-name|struct
-name|isa_device
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|void
 name|wdstrategy
 parameter_list|(
 name|struct
 name|buf
 modifier|*
+name|bp
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -593,8 +572,8 @@ begin_function_decl
 name|void
 name|wdintr
 parameter_list|(
-name|struct
-name|intrframe
+name|int
+name|unit
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -604,14 +583,18 @@ name|int
 name|wdopen
 parameter_list|(
 name|dev_t
+name|dev
 parameter_list|,
 name|int
+name|flags
 parameter_list|,
 name|int
+name|fmt
 parameter_list|,
 name|struct
 name|proc
 modifier|*
+name|p
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -637,34 +620,34 @@ name|int
 name|wdioctl
 parameter_list|(
 name|dev_t
+name|dev
 parameter_list|,
 name|int
+name|cmd
 parameter_list|,
 name|caddr_t
+name|addr
 parameter_list|,
 name|int
+name|flag
 parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_comment
-comment|/* int wdformat(struct buf *bp); */
-end_comment
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|B_FORMAT
+end_ifdef
 
 begin_function_decl
 name|int
-name|wdsize
+name|wdformat
 parameter_list|(
-name|dev_t
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|wddump
-parameter_list|(
-name|dev_t
+name|struct
+name|buf
+modifier|*
+name|bp
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -674,9 +657,31 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* KERNEL */
-end_comment
+begin_function_decl
+name|int
+name|wdsize
+parameter_list|(
+name|dev_t
+name|dev
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|wddump
+parameter_list|(
+name|dev_t
+name|dev
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+endif|KERNEL
+end_endif
 
 end_unit
 
