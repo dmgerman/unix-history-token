fd124d52786d7e9fe621b8fee88f8f89bf1ae512
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 22:53:05 -0800

    add _POSIX_SSIZE_MAX, _POSIX_STREAM_MAX, _POSIX_TZNAME_MAX
    SCCS-vsn: include/limits.h 8.2
    
    Former-commit-id: 64772600f75299ca0080d7d0f1108b0c842dbcfc

--- usr/src/include/limits.h
+++ usr/src/include/limits.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)limits.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)limits.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -83,6 +83,27 @@ define|#
 directive|define
 name|_POSIX_PIPE_BUF
 value|512
+end_define
+
+begin_define
+define|#
+directive|define
+name|_POSIX_SSIZE_MAX
+value|32767
+end_define
+
+begin_define
+define|#
+directive|define
+name|_POSIX_STREAM_MAX
+value|8
+end_define
+
+begin_define
+define|#
+directive|define
+name|_POSIX_TZNAME_MAX
+value|3
 end_define
 
 begin_define
