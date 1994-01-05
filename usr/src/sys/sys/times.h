b0bd6c8d3c26f2b1d372a0bbbb23e0353b06c82a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 23:52:42 -0800

    protect against reinclusion
    SCCS-vsn: sys/sys/times.h 8.2
    
    Former-commit-id: 8778617a7b8f73d911a7e91aba40c3cf7b56333f

--- usr/src/sys/sys/times.h
+++ usr/src/sys/sys/times.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)times.h	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)times.h	8.2 (Berkeley) %G%  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_TIMES_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_TIMES_H_
+end_define
 
 begin_include
 include|#
@@ -92,6 +104,15 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_TIMES_H_ */
+end_comment
 
 end_unit
 
