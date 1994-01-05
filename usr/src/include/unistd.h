dc0a4f07bbfeacf3bf9ffd04cb1b9705e3a29cae
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 21:28:13 -0800

    getgroups now takes gid_t's, they are 32-bits
    SCCS-vsn: include/unistd.h 8.4
    
    Former-commit-id: c38b2ee2296f76ebd1aa15e78a9cf83994927b3f

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)unistd.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -427,16 +427,12 @@ argument_list|(
 operator|(
 name|int
 operator|,
-name|int
-operator|*
+name|gid_t
+index|[]
 operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
-
-begin_comment
-comment|/* XXX (gid_t *) */
-end_comment
 
 begin_decl_stmt
 name|char
