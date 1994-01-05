fc71f958bcc745e8c9ef59c6ff6660af789da98e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-01-05 01:14:33 -0800

    do not need to do anything for cpu_set_init_frame() as the PMAX stores registers in u.u_pcb rather than on stack (i.e., fork() sets p_md.md_regs)
    SCCS-vsn: sys/pmax/include/cpu.h 8.4
    
    Former-commit-id: 48e2a3849eeeff8a456bb4d9520d3fc77fb241fd

--- usr/src/sys/pmax/include/cpu.h
+++ usr/src/sys/pmax/include/cpu.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)cpu.h	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)cpu.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -87,8 +87,11 @@ name|p
 parameter_list|,
 name|fp
 parameter_list|)
-value|(p)->p_md.md_regs = fp
 end_define
+
+begin_comment
+comment|/* nothing */
+end_comment
 
 begin_comment
 comment|/*  * Arguments to hardclock and gatherstats encapsulate the previous  * machine state in an opaque clockframe.  */
