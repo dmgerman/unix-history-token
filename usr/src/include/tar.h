4d0ac450811660778b1459021df8849258ef33c0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 21:50:59 -0800

    update the copyright notice
    SCCS-vsn: include/tar.h 8.2
    
    Former-commit-id: cbac671b7b2ddb8a584302b500f9dbe304bcfdcf

--- usr/src/include/tar.h
+++ usr/src/include/tar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994  *	The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Chuck Karish of Mindcraft, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)tar.h	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Chuck Karish of Mindcraft, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)tar.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
