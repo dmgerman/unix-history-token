89a5439b03382446f46662e9639f440b6c63b42c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-01-05 01:32:58 -0800

    minor typo that didn't make it into the previous version (from ralph)
    SCCS-vsn: sys/pmax/dev/tz.c 8.3
    
    Former-commit-id: 14fba7783db8df9c2f75961bf78154b505ecf314

--- usr/src/sys/pmax/dev/tz.c
+++ usr/src/sys/pmax/dev/tz.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)tz.c	8.2 (Berkeley) %G%  *  * from: $Header: /sprite/src/kernel/dev/RCS/devSCSITape.c,  *	v 8.14 89/07/31 17:26:13 mendel Exp $ SPRITE (Berkeley)  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)tz.c	8.3 (Berkeley) %G%  *  * from: $Header: /sprite/src/kernel/dev/RCS/devSCSITape.c,  *	v 8.14 89/07/31 17:26:13 mendel Exp $ SPRITE (Berkeley)  */
 end_comment
 
 begin_comment
@@ -840,7 +840,7 @@ elseif|else
 if|if
 condition|(
 name|i
-operator|==
+operator|>=
 literal|5
 operator|&&
 name|inqbuf
