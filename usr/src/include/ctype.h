aee731993b8d4818e30991de367d462a625a490d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 23:44:27 -0800

    runetype.h is ANSI safe, need to include it so that __isctype works
    SCCS-vsn: include/ctype.h 8.3
    
    Former-commit-id: 92da8b865cb84ccce41e219095b489441c6fccd6

--- usr/src/include/ctype.h
+++ usr/src/include/ctype.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Paul Borman at Krystal Technologies.  *  * %sccs.include.redist.c%  *  *	@(#)ctype.h	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Paul Borman at Krystal Technologies.  *  * %sccs.include.redist.c%  *  *	@(#)ctype.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -15,22 +15,11 @@ directive|define
 name|_CTYPE_H_
 end_define
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|_ANSI_SOURCE
-end_ifndef
-
 begin_include
 include|#
 directive|include
 file|<runetype.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_define
 define|#
