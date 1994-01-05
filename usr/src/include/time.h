e3f458c45a0dff5b145afd8c25efa9302f61ad98
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 21:39:37 -0800

    include <machine/limits.h>; POSIX.1 requires time.h to have CLK_TCK
    SCCS-vsn: include/time.h 8.2
    
    Former-commit-id: 679fe4ba94dffb868eff1a1ba4e42ca325600f76

--- usr/src/include/time.h
+++ usr/src/include/time.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)time.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)time.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -163,6 +163,16 @@ comment|/* timezone abbreviation */
 block|}
 struct|;
 end_struct
+
+begin_include
+include|#
+directive|include
+file|<machine/limits.h>
+end_include
+
+begin_comment
+comment|/* Include file containing CLK_TCK. */
+end_comment
 
 begin_include
 include|#
