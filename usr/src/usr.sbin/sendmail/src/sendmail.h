864ded4f93d0e4ba4cf4cddcd3d84e8482f6f1bf
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-05 16:34:31 -0800

    add QUNSAFEADDR mailer flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.38
    
    Former-commit-id: 232b80f536f23a22a51cef3b6d568b39a224bc4b

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.37 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.38 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.37		%G%"
+literal|"@(#)sendmail.h	8.38		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -616,6 +616,17 @@ end_define
 
 begin_comment
 comment|/* this entry has an invalid shell listed */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|QUNSAFEADDR
+value|004000
+end_define
+
+begin_comment
+comment|/* address aquired through an unsafe path */
 end_comment
 
 begin_define
