5dfb91c6904adb89de7cef87cd629522be1fa65e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-05 18:20:12 -0800

    syslog portability for HP-UX; fix typo for OSF/1 pid file
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.73
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.61
    
    Former-commit-id: 5d4faee1097c2fe9789e606698104bbb6e8c7efc

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.72 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.73 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -517,6 +517,13 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_define
+define|#
+directive|define
+name|syslog
+value|hard_syslog
+end_define
 
 begin_endif
 endif|#
@@ -1407,7 +1414,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_SENDMAILPID
-value|"/var/run/sendmial.pid"
+value|"/var/run/sendmail.pid"
 end_define
 
 begin_endif
