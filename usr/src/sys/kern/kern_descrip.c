e58fcc1fec8e48d8c45e28d1ff50bba220bb957c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-01-07 00:24:30 -0800

    off by one error (from karels@bsdi.com)
    SCCS-vsn: sys/kern/kern_descrip.c 8.4
    
    Former-commit-id: ded8eb6362390dcaaaf357e1098c3e8d30df63b3

--- usr/src/sys/kern/kern_descrip.c
+++ usr/src/sys/kern/kern_descrip.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_descrip.c	8.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)kern_descrip.c	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3618,7 +3618,7 @@ operator|*
 name|NDEXTENT
 operator|&&
 name|i
-operator|>=
+operator|>
 name|newfdp
 operator|->
 name|fd_lastfile
