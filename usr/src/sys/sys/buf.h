ca754cd6d43d73b5834101f88129b48bd46e6d5e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-01-07 06:43:54 -0800

    need to define NOLIST for b_vnbufs list
    SCCS-vsn: sys/sys/buf.h 8.6
    
    Former-commit-id: c41a99263d6bf2b55cbfdab8f504890f4e7d794b

--- usr/src/sys/sys/buf.h
+++ usr/src/sys/sys/buf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)buf.h	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)buf.h	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -20,6 +20,13 @@ include|#
 directive|include
 file|<sys/queue.h>
 end_include
+
+begin_define
+define|#
+directive|define
+name|NOLIST
+value|((struct buf *)0x87654321)
+end_define
 
 begin_comment
 comment|/*  * The buffer header describes an I/O operation in the kernel.  */
