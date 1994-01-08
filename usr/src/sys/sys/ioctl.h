80749ddd355fb3f9cf94e2bc4e66ccd8e1d83c01
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-08 01:36:35 -0800

    IOCBASECMD was masking out 13 bits of command, instead of the 13 bits of length From BSDI users mailing list Chris Demetriou and Mike Karels
    SCCS-vsn: sys/sys/ioctl.h 8.3
    
    Former-commit-id: 5afbced5f02e7fe866e8033edadea0359ea749d2

--- usr/src/sys/sys/ioctl.h
+++ usr/src/sys/sys/ioctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c)  *	1982, 1986, 1990, 1993  The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ioctl.h	8.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c)  *	1982, 1986, 1990, 1993  The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ioctl.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -121,7 +121,7 @@ name|IOCBASECMD
 parameter_list|(
 name|x
 parameter_list|)
-value|((x)& ~IOCPARM_MASK)
+value|((x)& ~(IOCPARM_MASK<< 16))
 end_define
 
 begin_define
