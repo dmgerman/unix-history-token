9b45067d6b1071c95d79aa75888bcef70ed971e6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-09 01:26:53 -0800

    HP-UX portability changes
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.75
    
    Former-commit-id: 68af329176b40ba4523732359ece0210ebbb6ca4

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.74 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.75 (Berkeley) %G%  */
 end_comment
 
 begin_comment
