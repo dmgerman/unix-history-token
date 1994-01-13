b9bd4a96a9497b206533a0ed6b369872e73d7b31
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-13 14:42:08 -0800

    version 8.6.5 -- see RELEASE_NOTES for details
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.6.5.1
    SCCS-vsn: usr.sbin/sendmail/cf/m4/version.m4 8.6.5.1
    
    Former-commit-id: 7869a570e53780e60ee45406b8ded91f36f16ad1

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	8.6.4.1 (Berkeley) %G%"
+literal|"@(#)version.c	8.6.5.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"8.6.4"
+literal|"8.6.5"
 decl_stmt|;
 end_decl_stmt
 
