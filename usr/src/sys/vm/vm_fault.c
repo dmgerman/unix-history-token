311b722498f7a71f52c7f46ba7d06cdcc33ee229
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-01-12 23:55:52 -0800

    keep track of a few more vmmeter statistics.
    SCCS-vsn: sys/vm/vm_fault.c 8.4
    
    Former-commit-id: d52598a7ba7a4eab4a599cd28060eb371c31a015

--- usr/src/sys/vm/vm_fault.c
+++ usr/src/sys/vm/vm_fault.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_fault.c	8.3 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_fault.c	8.4 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -116,7 +116,7 @@ name|next_object
 decl_stmt|;
 name|cnt
 operator|.
-name|v_vm_faults
+name|v_faults
 operator|++
 expr_stmt|;
 comment|/* needs lock XXX */
@@ -325,6 +325,11 @@ argument_list|)
 expr_stmt|;
 name|UNLOCK_THINGS
 expr_stmt|;
+name|cnt
+operator|.
+name|v_intrans
+operator|++
+expr_stmt|;
 name|thread_block
 argument_list|()
 expr_stmt|;
@@ -504,6 +509,11 @@ expr_stmt|;
 comment|/* 			 *	Call the pager to retrieve the data, if any, 			 *	after releasing the lock on the map. 			 */
 name|UNLOCK_MAP
 expr_stmt|;
+name|cnt
+operator|.
+name|v_pageins
+operator|++
+expr_stmt|;
 name|rv
 operator|=
 name|vm_pager_get
@@ -543,7 +553,7 @@ argument_list|)
 expr_stmt|;
 name|cnt
 operator|.
-name|v_pageins
+name|v_pgpgin
 operator|++
 expr_stmt|;
 name|m
@@ -884,10 +894,8 @@ else|else
 block|{
 name|prot
 operator|&=
-operator|(
 operator|~
 name|VM_PROT_WRITE
-operator|)
 expr_stmt|;
 name|m
 operator|->
