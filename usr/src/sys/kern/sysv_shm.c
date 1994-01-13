b06d756f18b9814a696fcc0f834570b7a33f78b9
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-01-12 22:41:57 -0800

    entries_pageable set TRUE for shm_map (see vm_map.c change).
    SCCS-vsn: sys/kern/sysv_shm.c 8.5
    
    Former-commit-id: 947c7811aac076a5c316b343cac8e2185b35e6f3

--- usr/src/sys/kern/sysv_shm.c
+++ usr/src/sys/kern/sysv_shm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department. Originally from University of Wisconsin.  *  * %sccs.include.proprietary.c%  *  * from: Utah $Hdr: uipc_shm.c 1.11 92/04/23$  *  *	@(#)sysv_shm.c	8.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department. Originally from University of Wisconsin.  *  * %sccs.include.proprietary.c%  *  * from: Utah $Hdr: uipc_shm.c 1.11 92/04/23$  *  *	@(#)sysv_shm.c	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -209,7 +209,7 @@ name|shmall
 operator|*
 name|NBPG
 argument_list|,
-name|FALSE
+name|TRUE
 argument_list|)
 expr_stmt|;
 if|if
