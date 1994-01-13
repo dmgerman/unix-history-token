a2e77e5ec0570e9e481574e7db41f396ac24d9d7
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-01-12 23:56:25 -0800

    reduce number of kernel map entries, add vm_map_clean, get rid of vm_map_is_allocated.
    SCCS-vsn: sys/vm/vm_map.h 8.2
    
    Former-commit-id: 2121e53c5951b11847d7fbfb5053f378d1e806b5

--- usr/src/sys/vm/vm_map.h
+++ usr/src/sys/vm/vm_map.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_map.h	8.1 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_map.h	8.2 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -319,7 +319,7 @@ begin_define
 define|#
 directive|define
 name|MAX_KMAPENT
-value|500
+value|250
 end_define
 
 begin_ifdef
@@ -597,24 +597,6 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|boolean_t
-name|vm_map_is_allocated
-name|__P
-argument_list|(
-operator|(
-name|vm_map_t
-operator|,
-name|vm_offset_t
-operator|,
-name|vm_offset_t
-operator|,
-name|boolean_t
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 name|int
 name|vm_map_lookup
 name|__P
@@ -691,6 +673,26 @@ operator|,
 name|vm_offset_t
 operator|,
 name|vm_offset_t
+operator|,
+name|boolean_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|vm_map_clean
+name|__P
+argument_list|(
+operator|(
+name|vm_map_t
+operator|,
+name|vm_offset_t
+operator|,
+name|vm_offset_t
+operator|,
+name|boolean_t
 operator|,
 name|boolean_t
 operator|)
