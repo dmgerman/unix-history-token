16b247d2e2ad14abea65c0b09abf7eaa313d3195
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-01-15 18:27:21 +0000

    From Rodney Volz:
    In FreeBSD/ufs, files are always created with their group-id
    set to the group-id of their parent-directory. This behaviour
    is neither POSIX nor BSD-conforming.
    
    Posix says, that the group-id of newly created files should be set to
    the group-id of the creator. An optional Posix feature is, that
    if the parent directory has a group S-bit set, the group-id
    of the new file should be set to the group-id of its parent
    directory.
    
    
    Former-commit-id: f58c9082550b35df5d9c2a105e3878b3d1913d35

--- sys/ufs/ufs_vnops.c
+++ sys/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_vnops.c	7.64 (Berkeley) 5/16/91  *	$Id: ufs_vnops.c,v 1.10 1993/12/19 23:23:38 wollman Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_vnops.c	7.64 (Berkeley) 5/16/91  *	$Id: ufs_vnops.c,v 1.11 1994/01/14 16:26:06 davidg Exp $  */
 end_comment
 
 begin_include
@@ -8156,6 +8156,21 @@ name|ni_cred
 operator|->
 name|cr_uid
 expr_stmt|;
+if|#
+directive|if
+literal|0
+block|ip->i_gid = pdir->i_gid;
+else|#
+directive|else
+comment|/* This behaviour should be POSIX conform. -Rodney Volz */
+if|if
+condition|(
+name|pdir
+operator|->
+name|i_mode
+operator|&
+name|S_ISGID
+condition|)
 name|ip
 operator|->
 name|i_gid
@@ -8164,6 +8179,19 @@ name|pdir
 operator|->
 name|i_gid
 expr_stmt|;
+else|else
+name|ip
+operator|->
+name|i_gid
+operator|=
+name|ndp
+operator|->
+name|ni_cred
+operator|->
+name|cr_gid
+expr_stmt|;
+endif|#
+directive|endif
 ifdef|#
 directive|ifdef
 name|QUOTA
