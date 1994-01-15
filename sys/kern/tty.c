be845dabde08b1585a7ad6672ab2263074599f21
Andrey Chernov <ache@FreeBSD.org>
1994-01-15 01:36:38 +0000

    Fix bug when RB_LEN == 0 && VMIN == 0 too.
    Former-commit-id: 767c0495a1a200de10d553491a1489aeb9e99ec5

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty.c	7.44 (Berkeley) 5/28/91  *	$Id: tty.c,v 1.13 1994/01/08 12:09:47 ache Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty.c	7.44 (Berkeley) 5/28/91  *	$Id: tty.c,v 1.14 1994/01/11 18:09:36 ache Exp $  */
 end_comment
 
 begin_include
@@ -6569,6 +6569,8 @@ init|=
 literal|0
 decl_stmt|,
 name|not_enough
+decl_stmt|,
+name|rblen
 decl_stmt|;
 name|loop
 label|:
@@ -6745,13 +6747,21 @@ index|]
 operator|-
 literal|1
 expr_stmt|;
-comment|/* 	 * If there is no input, sleep on rawq 	 * awaiting hardware receipt and notification. 	 * If we have data, we don't need to check for carrier. 	 */
-if|if
-condition|(
+name|rblen
+operator|=
 name|RB_LEN
 argument_list|(
 name|qp
 argument_list|)
+expr_stmt|;
+comment|/* 	 * If there is no input, sleep on rawq 	 * awaiting hardware receipt and notification. 	 * If we have data, we don't need to check for carrier. 	 */
+if|if
+condition|(
+name|rblen
+operator|==
+literal|0
+operator|||
+name|rblen
 operator|<=
 name|not_enough
 condition|)
@@ -6809,18 +6819,8 @@ operator|&
 name|IO_NDELAY
 operator|)
 operator|||
-operator|!
-operator|(
-name|lflag
-operator|&
-name|ICANON
-operator|)
-operator|&&
-name|cc
-index|[
-name|VMIN
-index|]
-operator|==
+name|not_enough
+operator|<
 literal|0
 condition|)
 block|{
