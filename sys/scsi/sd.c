a1c259161a08cf119cb2d11efafc4fd3f1476f16
Rod Grimes <rgrimes@FreeBSD.org>
1994-01-22 11:05:16 +0000

    Remove bogus and missleading comment, this has been fixed.
    -       /* blkcnt = initialise_me; */
    
    
    Former-commit-id: 56e44a639c005d6aab311a2acdb8f363a2a347e5

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.14 1993/12/19 00:54:57 wollman Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.15 1994/01/06 18:08:03 rgrimes Exp $  */
 end_comment
 
 begin_define
@@ -4048,7 +4048,6 @@ name|dumplo
 operator|+
 name|blkoff
 expr_stmt|;
-comment|/* blkcnt = initialise_me; */
 while|while
 condition|(
 name|num
