578b46bf07936f14d608e59e6811e6ed7df48660
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-22 00:48:41 -0800

    add USL's coypright notice
    SCCS-vsn: bin/df/df.c 8.3
    
    Former-commit-id: f55411f48c2815092646decaf086b06b5fae787e

--- usr/src/bin/df/df.c
+++ usr/src/bin/df/df.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  */
+comment|/*  * Copyright (c) 1980, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -40,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)df.c	8.2 (Berkeley) %G%"
+literal|"@(#)df.c	8.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
