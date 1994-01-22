ef742017cb3ab2ac6ab41aa61cda93e9b47d6418
Rod Grimes <rgrimes@FreeBSD.org>
1994-01-22 07:46:25 +0000

    >From: julian@TFS.COM (Julian Elischer) Date: Mon, 17 Jan 94 20:52:14 PST
    As reported on the -bugs list by some one, and confirmed by Julian:
    
    > I increased AHA_RESET_TIMEOUT in aha1542.c from 1000 to 10000
    > and now my 1542C is found everytime.
    
    
    Former-commit-id: a1c7feee2d87a4edc7755cb69f1fdfaf6626076e

--- sys/i386/isa/aha1542.c
+++ sys/i386/isa/aha1542.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.17 1993/12/19 00:50:25 wollman Exp $  */
+comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.18 1994/01/09 18:11:21 ats Exp $  */
 end_comment
 
 begin_comment
@@ -1695,7 +1695,7 @@ begin_define
 define|#
 directive|define
 name|AHA_RESET_TIMEOUT
-value|1000
+value|10000
 end_define
 
 begin_comment
