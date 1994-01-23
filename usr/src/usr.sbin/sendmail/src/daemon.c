61c3d4530709b0ca09ad985adcb1e96dd2560a8d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-23 00:23:57 -0800

    allow setting of ident timeout to zero to turn it off
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.32
    
    Former-commit-id: fca447c5011113845e5f50f9afaab2bdfe2ee131

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.31 (Berkeley) %G% (with daemon mode)"
+literal|"@(#)daemon.c	8.32 (Berkeley) %G% (with daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -54,7 +54,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.31 (Berkeley) %G% (without daemon mode)"
+literal|"@(#)daemon.c	8.32 (Berkeley) %G% (without daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -2111,6 +2111,17 @@ expr_stmt|;
 if|#
 directive|if
 name|IDENTPROTO
+if|if
+condition|(
+name|TimeOuts
+operator|.
+name|to_ident
+operator|==
+literal|0
+condition|)
+goto|goto
+name|noident
+goto|;
 name|lalen
 operator|=
 sizeof|sizeof
