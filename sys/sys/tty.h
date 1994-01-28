2958fb36903dcc895d937ba21f1bd423a50291ae
Andrey Chernov <ache@FreeBSD.org>
1994-01-28 23:15:17 +0000

    1) Remove external (really static) decls from tty.c.
    2) From bde:
    	o t_addr and t_mask aren't used.
    	o Changed most short and char types to int.  gcc warned a lot about
    	  the t_line being used as an array index.  It was worried about
    	  unportable sign extensions; I was worried about inefficiency.
    
    
    Former-commit-id: a9df182c3180c7d9d6c31c66bbb4131b8ecdb084

--- sys/sys/tty.h
+++ sys/sys/tty.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty.h	7.10 (Berkeley) 6/26/91  *	$Id: tty.h,v 1.6 1993/11/25 01:38:07 wollman Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty.h	7.10 (Berkeley) 6/26/91  *	$Id: tty.h,v 1.7 1993/12/19 00:55:28 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -176,10 +176,13 @@ name|caddr_t
 name|T_LINEP
 decl_stmt|;
 comment|/* XXX */
-name|caddr_t
-name|t_addr
-decl_stmt|;
+if|#
+directive|if
+literal|0
+block|caddr_t	t_addr;
 comment|/* ??? */
+endif|#
+directive|endif
 name|dev_t
 name|t_dev
 decl_stmt|;
@@ -204,25 +207,25 @@ modifier|*
 name|t_pgrp
 decl_stmt|;
 comment|/* foreground process group */
-name|char
+name|int
 name|t_line
 decl_stmt|;
 comment|/* glue */
-name|short
+name|int
 name|t_col
 decl_stmt|;
 comment|/* tty */
-name|short
+name|int
 name|t_rocount
 decl_stmt|,
 name|t_rocol
 decl_stmt|;
 comment|/* tty */
-name|short
+name|int
 name|t_hiwat
 decl_stmt|;
 comment|/* hi water mark */
-name|short
+name|int
 name|t_lowat
 decl_stmt|;
 comment|/* low water mark */
@@ -282,14 +285,17 @@ decl_stmt|;
 name|long
 name|t_outcc
 decl_stmt|;
-name|short
+name|int
 name|t_gen
 decl_stmt|;
 comment|/* generation number */
-name|short
-name|t_mask
-decl_stmt|;
+if|#
+directive|if
+literal|0
+block|int     t_mask;
 comment|/* interrupt mask */
+endif|#
+directive|endif
 name|struct
 name|ringb
 name|t_raw
@@ -953,30 +959,6 @@ end_function_decl
 begin_function_decl
 specifier|extern
 name|void
-name|ttyblock
-parameter_list|(
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|ttyunblock
-parameter_list|(
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
 name|ttstart
 parameter_list|(
 name|struct
@@ -1119,33 +1101,7 @@ end_function_decl
 begin_function_decl
 specifier|extern
 name|void
-name|ttypend
-parameter_list|(
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
 name|ttyinput
-parameter_list|(
-name|int
-parameter_list|,
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|int
-name|ttyoutput
 parameter_list|(
 name|int
 parameter_list|,
@@ -1202,75 +1158,6 @@ name|uio
 modifier|*
 parameter_list|,
 name|int
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|ttyrub
-parameter_list|(
-name|int
-parameter_list|,
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|ttyrubo
-parameter_list|(
-name|struct
-name|tty
-modifier|*
-parameter_list|,
-name|int
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|ttyretype
-parameter_list|(
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|ttyecho
-parameter_list|(
-name|int
-parameter_list|,
-name|struct
-name|tty
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|ttyoutstr
-parameter_list|(
-name|char
-modifier|*
-parameter_list|,
-name|struct
-name|tty
-modifier|*
 parameter_list|)
 function_decl|;
 end_function_decl
