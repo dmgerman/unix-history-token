09f98df8066728ac1a94fe1195d0271ba998f5b6
David Greenman <davidg@Root.COM>
1994-01-29 12:10:20 +0000

    Patch from me to fix bug with Nagel congestion avoidance. Always send ACK when a packet len of < mss is received.
    Former-commit-id: a8d9656dda237257e83a880d24a3a68c41caefd1

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tcp_input.c	7.25 (Berkeley) 6/30/90  *	$Id: tcp_input.c,v 1.5 1994/01/24 05:12:32 davidg Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tcp_input.c	7.25 (Berkeley) 6/30/90  *	$Id: tcp_input.c,v 1.6 1994/01/24 07:17:08 davidg Exp $  */
 end_comment
 
 begin_include
@@ -4585,6 +4585,32 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+comment|/* 	 * If this is a small packet, then ACK now - with Nagel 	 *	congestion avoidance sender won't send more until 	 *	he gets an ACK. 	 */
+if|if
+condition|(
+name|ti
+operator|->
+name|ti_len
+operator|&&
+operator|(
+operator|(
+name|unsigned
+operator|)
+name|ti
+operator|->
+name|ti_len
+operator|<
+name|tp
+operator|->
+name|t_maxseg
+operator|)
+condition|)
+name|tp
+operator|->
+name|t_flags
+operator||=
+name|TF_ACKNOW
+expr_stmt|;
 comment|/* 	 * Return any desired output. 	 */
 if|if
 condition|(
@@ -5859,7 +5885,6 @@ operator|=
 name|bufsize
 expr_stmt|;
 else|else
-block|{
 name|bufsize
 operator|=
 name|min
@@ -5886,7 +5911,6 @@ argument_list|,
 name|bufsize
 argument_list|)
 expr_stmt|;
-block|}
 name|tp
 operator|->
 name|t_maxseg
@@ -5923,10 +5947,14 @@ expr_stmt|;
 if|if
 condition|(
 name|bufsize
-operator|>
+operator|<
 name|mss
 condition|)
-block|{
+name|bufsize
+operator|=
+name|mss
+expr_stmt|;
+else|else
 name|bufsize
 operator|=
 name|min
@@ -5953,7 +5981,6 @@ argument_list|,
 name|bufsize
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 name|tp
 operator|->
