efa46b1d749a3d8929ea6d95b72ccce1ac538f92
Rod Grimes <rgrimes@FreeBSD.org>
1994-01-29 10:30:41 +0000

    Patch from Julian.  Commit message by me.
    cd.c:
    Initialize channel info in CDIOCSETVOL ioctl.
    Correct CDIOCSTOP and CDIOCEJCET ioctls to use scsi_stop_unit
    instead of scsi_start_unit.
    Add CDIOCALLOW and CDIOCPREVENT ioctls.
    
    ch.h:
    Return EBUSY instead of ENXIO if the device is already in use.
    
    scsi_base.c:
    Add scsi_stop_unit routine.
    
    sd.c:
    Add mising indirection through sc_link to sd_get_parms routine
    when checking for media loaded.
    
    st.c:
    Return EBUSY instead of ENXIO if the device is already in use.
    Clear the SDEV_WAITING flag in ststart if we do the wakeup call.
    
    
    Former-commit-id: 4b4b6a0e1a769f87322f0b93ee02535d6e73f794

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.15 1994/01/06 18:08:03 rgrimes Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.16 1994/01/22 11:05:16 rgrimes Exp $  */
 end_comment
 
 begin_define
@@ -3294,6 +3294,8 @@ comment|/* 	 * First check if we have it all loaded 	 */
 if|if
 condition|(
 name|sd
+operator|->
+name|sc_link
 operator|->
 name|flags
 operator|&
