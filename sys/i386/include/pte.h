8c7cc69142dd26844fe121d0cf742036b7215e50
David Greenman <davidg@Root.COM>
1994-01-31 06:52:41 +0000

    Fix broken bit definitions for pte/pde.
    Former-commit-id: d9b850e24bf0d48fc3f4c020ef9cbef10871363d

--- sys/i386/include/pte.h
+++ sys/i386/include/pte.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)pte.h	5.5 (Berkeley) 5/9/91  *	$Id: pte.h,v 1.2 1993/10/16 14:39:27 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)pte.h	5.5 (Berkeley) 5/9/91  *	$Id: pte.h,v 1.3 1993/11/07 17:43:05 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -42,16 +42,22 @@ range|:
 literal|2
 decl_stmt|,
 comment|/* access control */
-name|pd_mbz1
+name|pd_ncpwt
 range|:
-literal|2
+literal|1
 decl_stmt|,
-comment|/* reserved, must be zero */
+comment|/* page cache write through */
+name|pd_ncpcd
+range|:
+literal|1
+decl_stmt|,
+comment|/* page cache disable */
 name|pd_u
 range|:
 literal|1
 decl_stmt|,
 comment|/* hardware maintained 'used' bit */
+name|pd_m
 range|:
 literal|1
 decl_stmt|,
@@ -90,11 +96,16 @@ range|:
 literal|2
 decl_stmt|,
 comment|/* access control */
-name|pg_mbz1
+name|pg_ncpwt
 range|:
-literal|2
+literal|1
 decl_stmt|,
-comment|/* reserved, must be zero */
+comment|/* page cache write through */
+name|pg_ncpcd
+range|:
+literal|1
+decl_stmt|,
+comment|/* page cache disable */
 name|pg_u
 range|:
 literal|1
@@ -110,82 +121,15 @@ range|:
 literal|2
 decl_stmt|,
 comment|/* reserved, must be zero */
-name|pg_fod
 range|:
-literal|1
+literal|3
 decl_stmt|,
-comment|/* is fill on demand (=0) */
-range|:
-literal|1
-decl_stmt|,
-comment|/* must write back to swap (unused) */
-name|pg_nc
-range|:
-literal|1
-decl_stmt|,
-comment|/* 'uncacheable page' bit */
+comment|/* (unused) */
 name|pg_pfnum
 range|:
 literal|20
 decl_stmt|;
 comment|/* physical page frame number */
-block|}
-struct|;
-end_struct
-
-begin_struct
-struct|struct
-name|hpte
-block|{
-name|unsigned
-name|int
-name|pg_high
-range|:
-literal|12
-decl_stmt|,
-comment|/* special for clustering */
-name|pg_pfnum
-range|:
-literal|20
-decl_stmt|;
-block|}
-struct|;
-end_struct
-
-begin_struct
-struct|struct
-name|fpte
-block|{
-name|unsigned
-name|int
-name|pg_v
-range|:
-literal|1
-decl_stmt|,
-comment|/* valid bit */
-name|pg_prot
-range|:
-literal|2
-decl_stmt|,
-comment|/* access control */
-range|:
-literal|5
-decl_stmt|,
-name|pg_fileno
-range|:
-literal|1
-decl_stmt|,
-comment|/* file mapped from or TEXT or ZERO */
-name|pg_fod
-range|:
-literal|1
-decl_stmt|,
-comment|/* is fill on demand (=1) */
-name|pg_blkno
-range|:
-literal|22
-decl_stmt|;
-comment|/* file system block number */
 block|}
 struct|;
 end_struct
@@ -238,22 +182,30 @@ end_comment
 begin_define
 define|#
 directive|define
-name|PG_FOD
-value|0x00000200
+name|PG_NC_PWT
+value|0x00000008
 end_define
+
+begin_comment
+comment|/* page cache write through */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|PG_SWAPM
-value|0x00000400
+name|PG_NC_PCD
+value|0x00000010
 end_define
+
+begin_comment
+comment|/* page cache disable */
+end_comment
 
 begin_define
 define|#
 directive|define
 name|PG_N
-value|0x00000800
+value|0x00000018
 end_define
 
 begin_comment
@@ -263,19 +215,23 @@ end_comment
 begin_define
 define|#
 directive|define
-name|PG_M
-value|0x00000040
-end_define
-
-begin_define
-define|#
-directive|define
 name|PG_U
 value|0x00000020
 end_define
 
 begin_comment
-comment|/* not currently used */
+comment|/* page was accessed */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PG_M
+value|0x00000040
+end_define
+
+begin_comment
+comment|/* page was modified */
 end_comment
 
 begin_define
@@ -283,27 +239,6 @@ define|#
 directive|define
 name|PG_FRAME
 value|0xfffff000
-end_define
-
-begin_define
-define|#
-directive|define
-name|PG_FZERO
-value|0
-end_define
-
-begin_define
-define|#
-directive|define
-name|PG_FTEXT
-value|1
-end_define
-
-begin_define
-define|#
-directive|define
-name|PG_FMAX
-value|(PG_FTEXT)
 end_define
 
 begin_define
@@ -346,6 +281,27 @@ define|#
 directive|define
 name|PG_UW
 value|0x00000006
+end_define
+
+begin_define
+define|#
+directive|define
+name|PG_FZERO
+value|0
+end_define
+
+begin_define
+define|#
+directive|define
+name|PG_FTEXT
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|PG_FMAX
+value|(PG_FTEXT)
 end_define
 
 begin_comment
