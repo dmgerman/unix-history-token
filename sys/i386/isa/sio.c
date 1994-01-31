1abe3889c128964f741d670b3fcd501a227159cb
David Greenman <davidg@Root.COM>
1994-01-31 06:12:18 +0000

    Patch from Brian Smith (modified a little by me) to allow kernel config file override to disable fifo on 16550s:
    I bought a board with two 16550's, but one of those ports has a mouse
    on it.  The sio driver always enables the fifo, which is a bad thing
    for mice and X.  The mouse is jerky and hard to use.  The simple thing
    is be to treat one of the ports as a non-fifo'ed UART, and I use the
    flags option in my config file.
    
    So, my config file has:
    device      sio0    at isa? port "IO_COM1" tty irq 4 flags 0x2 vector siointr
    device      sio1    at isa? port "IO_COM2" tty irq 3 vector siointr
    
    (patch deleted)
    
    
    Former-commit-id: a894b132eba4ce2d0d5c8207951dbbba988563f4

--- sys/i386/isa/sio.c
+++ sys/i386/isa/sio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.21 1994/01/02 10:17:29 ache Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.22 1994/01/11 18:31:45 ache Exp $  */
 end_comment
 
 begin_include
@@ -285,6 +285,16 @@ end_endif
 begin_comment
 comment|/* COM_MULTIPORT */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|COM_NOFIFO
+parameter_list|(
+name|dev
+parameter_list|)
+value|((dev)->id_flags& 0x02)
+end_define
 
 begin_ifndef
 ifndef|#
@@ -2222,17 +2232,34 @@ break|break;
 case|case
 name|FIFO_TRIGGER_14
 case|:
+name|printf
+argument_list|(
+literal|" 16550A"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|COM_NOFIFO
+argument_list|(
+name|isdp
+argument_list|)
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|" fifo software disabled"
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
 name|com
 operator|->
 name|hasfifo
 operator|=
 name|TRUE
 expr_stmt|;
-name|printf
-argument_list|(
-literal|" 16550A"
-argument_list|)
-expr_stmt|;
+block|}
 break|break;
 block|}
 name|outb
