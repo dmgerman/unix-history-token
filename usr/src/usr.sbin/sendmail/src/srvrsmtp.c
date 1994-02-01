ad5e40ba39c9038598e6ffc4fdcfe9b4ee366b46
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-31 22:47:40 -0800

    eliminate HELO checking -- almost noone does it right
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.26
    
    Former-commit-id: 589e09835bbc841aa3ef8942772816dc400aba33

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.25 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	8.26 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.25 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	8.26 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -1127,6 +1127,9 @@ name|RealHostName
 argument_list|)
 expr_stmt|;
 block|}
+ifdef|#
+directive|ifdef
+name|PICKY_HELO_CHECK
 if|if
 condition|(
 name|strcasecmp
@@ -1171,6 +1174,8 @@ name|sendinghost
 argument_list|)
 expr_stmt|;
 block|}
+endif|#
+directive|endif
 if|if
 condition|(
 name|protocol
