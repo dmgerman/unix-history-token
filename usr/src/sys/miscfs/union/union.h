b8ee1a65e54259f4744001fb772ab81fb990c41e
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-02-02 01:11:11 -0800

    checkpoint
    SCCS-vsn: sys/miscfs/union/union.h 1.2
    SCCS-vsn: sys/miscfs/union/union_subr.c 1.2
    SCCS-vsn: sys/miscfs/union/union_vnops.c 1.2
    SCCS-vsn: sys/miscfs/union/union_vfsops.c 1.2
    
    Former-commit-id: 958510196c054f0daae5b32b30d5b67606c81f73

--- usr/src/sys/miscfs/union/union.h
+++ usr/src/sys/miscfs/union/union.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 The Regents of the University of California.  * Copyright (c) 1994 Jan-Simon Pendry.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union.h	1.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1994 The Regents of the University of California.  * Copyright (c) 1994 Jan-Simon Pendry.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union.h	1.2 (Berkeley) %G%  */
 end_comment
 
 begin_struct
