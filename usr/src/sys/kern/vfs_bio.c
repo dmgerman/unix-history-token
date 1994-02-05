720c132b49a332a4145eb0b7a4cc60a8de6970db
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-02-04 22:23:57 -0800

    typo in printf
    SCCS-vsn: sys/kern/vfs_bio.c 8.10
    
    Former-commit-id: 68aa2b14117bf2ec90ff24a88123429001ff89c7

--- usr/src/sys/kern/vfs_bio.c
+++ usr/src/sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * This code is derived from software contributed to Berkeley by  * Berkeley Software Design Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_bio.c	8.9 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * This code is derived from software contributed to Berkeley by  * Berkeley Software Design Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_bio.c	8.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2440,7 +2440,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"getblk: stray size"
+literal|"getblk: stray size\n"
 argument_list|)
 expr_stmt|;
 name|bp
