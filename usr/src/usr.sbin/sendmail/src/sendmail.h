b76827b5edb17a83b887120ad840c7633e6840c3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-06 22:07:19 -0800

    add DisConnected global variable
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.41
    
    Former-commit-id: 9ff2f2b67eca400006cc4609f70a0b2d8d7f3305

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.40 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.41 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.40		%G%"
+literal|"@(#)sendmail.h	8.41		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -4416,6 +4416,17 @@ end_decl_stmt
 
 begin_comment
 comment|/* true if running in an SMTP subprocess */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|bool
+name|DisConnected
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* running with OutChannel redirected to xf */
 end_comment
 
 begin_decl_stmt
