0e97e7354185873700568e3c1e1add2c47d7aeeb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-09 15:39:55 -0800

    DG/UX doesn't have getusershell(3)
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.86
    
    Former-commit-id: fe6abd30ab48fc1e8c8af7b124a2aa5ec05dc56b

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.85 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.86 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1196,6 +1196,17 @@ end_define
 
 begin_comment
 comment|/* has initgroups(3) call */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HASGETUSERSHELL
+value|0
+end_define
+
+begin_comment
+comment|/* does not have getusershell(3) */
 end_comment
 
 begin_ifndef
