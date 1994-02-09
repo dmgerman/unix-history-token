616869633ed0c8cee4400347ec77c5343cf108b9
David Greenman <davidg@Root.COM>
1994-02-09 07:03:10 +0000

    Fixes from John Dyson:
    vm_page.c
    	vm_page_deactivate had a window that interrupts were
    	not disabled, but should have been.
    
    vm_object.c
    	in 3 places, a page was being protected VM_PROT_NONE, which
    	if the page is pager mapped, can cause a system failure
    	because a pager mapping could disappear.  Now, we block when
    	a page is busy.
    
    swap_pager.c
    	a deadlock could occur because a process could be waiting on
    	a swap clean entry, and that case was not handled correctly.
    	now, user processes dont call swap_pager_clean and the
    	normal user process wakes-up the pagedaemon when no swap clean
    	entries are available.
    
    
    Former-commit-id: ceea494f7501f0ebead311d7a7b5a3af96085f20

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91  *	$Id: vm_page.c,v 1.10 1994/01/31 04:21:10 davidg Exp $  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91  *	$Id: vm_page.c,v 1.11 1994/01/31 04:32:41 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -2182,7 +2182,7 @@ expr_stmt|;
 comment|/* 	 *	Only move active pages -- ignore locked or already 	 *	inactive ones. 	 * 	 *	XXX: sometimes we get pages which aren't wired down 	 *	or on any queue - we need to put them on the inactive 	 *	queue also, otherwise we lose track of them. 	 *	Paul Mackerras (paulus@cs.anu.edu.au) 9-Jan-93. 	 */
 name|spl
 operator|=
-name|vm_disable_intr
+name|splhigh
 argument_list|()
 expr_stmt|;
 name|m
@@ -2267,11 +2267,6 @@ name|flags
 operator||=
 name|PG_INACTIVE
 expr_stmt|;
-name|vm_set_intr
-argument_list|(
-name|spl
-argument_list|)
-expr_stmt|;
 name|vm_page_inactive_count
 operator|++
 expr_stmt|;
@@ -2304,14 +2299,11 @@ operator||=
 name|PG_LAUNDRY
 expr_stmt|;
 block|}
-else|else
-block|{
-name|vm_set_intr
+name|splx
 argument_list|(
 name|spl
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 end_function
 
