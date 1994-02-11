66d854182c4573e5f2dbbd370cd5e1428e1147da
Guido van Rooij <guido@gvr.win.tue.nl>
1994-02-11 21:14:52 +0000

    ps works inside ddb now.
    Former-commit-id: 87a9eeed245d4263e3a7288f052c3510997d939b

--- sys/ddb/db_command.c
+++ sys/ddb/db_command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_command.c,v 1.2 1993/10/16 16:47:10 rgrimes Exp $  */
+comment|/*   * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_command.c,v 1.3 1993/11/25 01:30:04 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -1000,6 +1000,14 @@ end_decl_stmt
 begin_function_decl
 specifier|extern
 name|void
+name|db_ps
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|void
 name|ipc_port_print
 parameter_list|()
 function_decl|;
@@ -1022,9 +1030,19 @@ block|{
 if|#
 directive|if
 literal|0
-block|{ "threads",	db_show_all_threads,0,	0 },
+block|{ "threads",	db_show_all_threads,	0,	0 },
 endif|#
 directive|endif
+block|{
+literal|"procs"
+block|,
+name|db_ps
+block|,
+literal|0
+block|,
+literal|0
+block|}
+block|,
 block|{
 operator|(
 name|char
@@ -1454,6 +1472,16 @@ block|,
 literal|0
 block|,
 name|db_show_cmds
+block|}
+block|,
+block|{
+literal|"ps"
+block|,
+name|db_ps
+block|,
+literal|0
+block|,
+literal|0
 block|}
 block|,
 block|{
