519afd6cf7d6eaa49a0e6118f9842a2a7184bdc3
Mike Karels <karels@ucbvax.Berkeley.EDU>
1994-02-12 17:59:36 -0800

    fix race in exit of session leader: if tty is revoked by spec_close or other revoke while we wait for output to drain, ttyvp will be released and null
    SCCS-vsn: sys/kern/kern_exit.c 8.7
    
    Former-commit-id: 76d3309a2fc38d87d07a2bd51e2a73a6d1f1af7c

--- usr/src/sys/kern/kern_exit.c
+++ usr/src/sys/kern/kern_exit.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)kern_exit.c	8.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)kern_exit.c	8.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -509,6 +509,13 @@ operator|->
 name|s_ttyp
 argument_list|)
 expr_stmt|;
+comment|/* 				 * The tty could have been revoked 				 * if we blocked. 				 */
+if|if
+condition|(
+name|sp
+operator|->
+name|s_ttyvp
+condition|)
 name|vgoneall
 argument_list|(
 name|sp
@@ -517,6 +524,12 @@ name|s_ttyvp
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|sp
+operator|->
+name|s_ttyvp
+condition|)
 name|vrele
 argument_list|(
 name|sp
