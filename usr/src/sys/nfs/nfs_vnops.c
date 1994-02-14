64e43d33cad83ed3acb0411a89dc80076a86ba3c
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-02-13 19:35:49 -0800

    add null nfs_reallocblks routine (is this needed?)
    SCCS-vsn: sys/nfs/nfs_vnops.c 8.5
    
    Former-commit-id: 964a01b6828a74061863347df611c3f38f447210

--- usr/src/sys/nfs/nfs_vnops.c
+++ usr/src/sys/nfs/nfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	8.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfs_vnops.c	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -488,6 +488,14 @@ block|,
 comment|/* valloc */
 block|{
 operator|&
+name|vop_reallocblks_desc
+block|,
+name|nfs_reallocblks
+block|}
+block|,
+comment|/* reallocblks */
+block|{
+operator|&
 name|vop_vfree_desc
 block|,
 name|nfs_vfree
@@ -869,6 +877,14 @@ name|spec_valloc
 block|}
 block|,
 comment|/* valloc */
+block|{
+operator|&
+name|vop_reallocblks_desc
+block|,
+name|spec_reallocblks
+block|}
+block|,
+comment|/* reallocblks */
 block|{
 operator|&
 name|vop_vfree_desc
@@ -1254,6 +1270,14 @@ name|fifo_valloc
 block|}
 block|,
 comment|/* valloc */
+block|{
+operator|&
+name|vop_reallocblks_desc
+block|,
+name|fifo_reallocblks
+block|}
+block|,
+comment|/* reallocblks */
 block|{
 operator|&
 name|vop_vfree_desc
