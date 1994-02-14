c6da8f1c7578cf17a64148e448c20f1d97f31d06
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-02-13 19:35:19 -0800

    add define for null reallocblks routine (is this needed?)
    SCCS-vsn: sys/nfs/nfsnode.h 8.4
    
    Former-commit-id: 4bc59a7b7c5b00a6afd15f5fac6b0f88ca34c3e6

--- usr/src/sys/nfs/nfsnode.h
+++ usr/src/sys/nfs/nfsnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfsnode.h	8.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * %sccs.include.redist.c%  *  *	@(#)nfsnode.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -938,6 +938,14 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|nfs_reallocblks
+define|\
+value|((int (*) __P((struct  vop_reallocblks_args *)))eopnotsupp)
+end_define
 
 begin_decl_stmt
 name|int
