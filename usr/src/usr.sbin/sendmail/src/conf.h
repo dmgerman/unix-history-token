94cd0e36cd31e32fbbbd485db139964bc9f43bc1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-17 17:24:46 -0800

    don't use IDENTPROTO on Sequent
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.89
    
    Former-commit-id: 8afdb704dc171ad6d3ceb2a68b1f046192cc754a

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.88 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.89 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -3437,6 +3437,28 @@ end_define
 begin_ifndef
 ifndef|#
 directive|ifndef
+name|IDENTPROTO
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|IDENTPROTO
+value|0
+end_define
+
+begin_comment
+comment|/* TCP/IP implementation is broken */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
 name|_PATH_UNIX
 end_ifndef
 
@@ -3570,6 +3592,28 @@ undef|#
 directive|undef
 name|SETPROCTITLE
 end_undef
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|IDENTPROTO
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|IDENTPROTO
+value|0
+end_define
+
+begin_comment
+comment|/* TCP/IP implementation is broken */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
