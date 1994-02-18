2b5f34af6bb911c755ea5d753516af18cc1f088c
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-02-18 00:36:00 -0800

    implement new access policy fixup locking for UNMNT_BELOW.
    SCCS-vsn: sys/miscfs/union/union.h 8.2
    SCCS-vsn: sys/miscfs/union/union_subr.c 8.4
    SCCS-vsn: sys/miscfs/union/union_vnops.c 8.6
    SCCS-vsn: sys/miscfs/union/union_vfsops.c 8.4
    
    Former-commit-id: b1ef3f3af7f9b8a8176dae144fc6417a8b527a73

--- usr/src/sys/miscfs/union/union_subr.c
+++ usr/src/sys/miscfs/union/union_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 Jan-Simon Pendry  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union_subr.c	8.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1994 Jan-Simon Pendry  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union_subr.c	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2164,6 +2164,23 @@ name|cnp
 operator|->
 name|cn_proc
 expr_stmt|;
+if|if
+condition|(
+name|um
+operator|->
+name|um_op
+operator|==
+name|UNMNT_ABOVE
+condition|)
+name|cn
+operator|.
+name|cn_cred
+operator|=
+name|cnp
+operator|->
+name|cn_cred
+expr_stmt|;
+else|else
 name|cn
 operator|.
 name|cn_cred
