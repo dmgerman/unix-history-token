3c1780a6be4b06a6340b29cbe14d69b7129e8392
Nate Willams <nate@FreeBSD.org>
1994-02-22 21:54:03 +0000

    Replaced version w/out copyright with something I whipped up based on hostname(1) and id(1).  This file has a BSD copyright since it's based on those sources.
    Former-commit-id: 56c823839679189bfb98724d691434fbac77cb5b

--- bin/domainname/domainname.c
+++ bin/domainname/domainname.c
@@ -1,4 +1,32 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (c) 1983, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+name|char
+name|copyright
+index|[]
+init|=
+literal|"@(#) Copyright (c) 1983, 1988 Regents of the University of California.\n\  All rights reserved.\n"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -8,10 +36,30 @@ end_ifndef
 begin_decl_stmt
 specifier|static
 name|char
+name|sccsid1
+index|[]
+init|=
+literal|"From@(#)hostname.c	5.4 (Berkeley) 5/31/90"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|char
+name|sccsid2
+index|[]
+init|=
+literal|"From@(#)id.c	5.1 (Berkeley) 6/29/91"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id$"
+literal|"$Id: domainname.c,v 1.2 1993/10/25 03:12:32 rgrimes Exp $ "
 decl_stmt|;
 end_decl_stmt
 
@@ -33,13 +81,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stdlib.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<string.h>
+file|<sys/param.h>
 end_include
 
 begin_include
@@ -51,11 +93,10 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/param.h>
+file|<stdlib.h>
 end_include
 
 begin_decl_stmt
-specifier|static
 name|void
 name|usage
 name|__P
@@ -84,7 +125,7 @@ name|argv
 decl_stmt|;
 block|{
 name|char
-name|dom
+name|domainname
 index|[
 name|MAXHOSTNAMELEN
 index|]
@@ -95,12 +136,9 @@ name|argc
 operator|>
 literal|2
 condition|)
-block|{
 name|usage
 argument_list|()
 expr_stmt|;
-comment|/* NOTREACHED */
-block|}
 if|if
 condition|(
 name|argc
@@ -117,6 +155,7 @@ index|[
 literal|1
 index|]
 argument_list|,
+operator|(
 name|strlen
 argument_list|(
 name|argv
@@ -126,10 +165,8 @@ index|]
 argument_list|)
 operator|+
 literal|1
+operator|)
 argument_list|)
-operator|==
-operator|-
-literal|1
 condition|)
 block|{
 name|perror
@@ -150,16 +187,13 @@ if|if
 condition|(
 name|getdomainname
 argument_list|(
-name|dom
+name|domainname
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|dom
+name|domainname
 argument_list|)
 argument_list|)
-operator|==
-operator|-
-literal|1
 condition|)
 block|{
 name|perror
@@ -177,7 +211,7 @@ name|printf
 argument_list|(
 literal|"%s\n"
 argument_list|,
-name|dom
+name|domainname
 argument_list|)
 expr_stmt|;
 block|}
@@ -190,7 +224,6 @@ block|}
 end_function
 
 begin_function
-specifier|static
 name|void
 name|usage
 parameter_list|()
