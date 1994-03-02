afa5ed7f7fb2418fefc9cc65797b8ef4faba2fbe
David Greenman <davidg@Root.COM>
1994-03-02 05:50:01 +0000

    Minor comment change.
    Former-commit-id: b823dad48b09963fa342538c1eddbd0e218dcefc

--- sys/i386/isa/if_ed.c
+++ sys/i386/isa/if_ed.c
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * Thanks to Charles Hannum for proving to me with example code that the  *	NE1000/2000 support could be added with minimal impact. Without  *	this, I wouldn't have proceeded in this direction.  *	  */
+comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  */
 end_comment
 
 begin_comment
-comment|/*  * $Id: if_ed.c,v 1.33 1994/02/03 11:50:58 davidg Exp $  */
+comment|/*  * $Id: if_ed.c,v 1.34 1994/03/01 12:23:33 davidg Exp $  */
 end_comment
 
 begin_include
