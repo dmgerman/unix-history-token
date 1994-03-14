963869982fbf2357d47a0c9186af7a6d2e5e5878
Andreas Schulz <ats@FreeBSD.org>
1994-03-14 23:09:34 +0000

    Put a safety check in for the secsiz divide for the MB printf. Else we get a panic integer divide fault in the kernel if the drive returns a bogus zero secsize.
    Former-commit-id: 1a85ad2d9c9edbf4220e321251640deafc74c3e9

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.17 1994/01/29 10:30:39 rgrimes Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.18 1994/01/29 11:17:10 davidg Exp $  */
 end_comment
 
 begin_define
@@ -860,6 +860,10 @@ name|unit
 argument_list|,
 name|dp
 operator|->
+name|secsiz
+condition|?
+name|dp
+operator|->
 name|disksize
 operator|/
 operator|(
@@ -873,6 +877,8 @@ name|dp
 operator|->
 name|secsiz
 operator|)
+else|:
+literal|0
 argument_list|,
 name|dp
 operator|->
