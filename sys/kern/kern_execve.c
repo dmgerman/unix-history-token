13955d257b4a3a2f1d0e207749f27d26898621ef
Garrett Wollman <wollman@FreeBSD.org>
1994-03-15 03:55:35 +0000

    Forgot to set SUGID in set-id exec.
    Former-commit-id: 611aa79354dfd8f97b980f3e81706bcefd5b3013

--- sys/kern/kern_execve.c
+++ sys/kern/kern_execve.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman  * 4. The name of the developer may be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_execve.c,v 1.16 1994/03/15 01:58:29 wollman Exp $  */
+comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman  * 4. The name of the developer may be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_execve.c,v 1.17 1994/03/15 02:48:35 wollman Exp $  */
 end_comment
 
 begin_include
@@ -862,6 +862,13 @@ argument_list|)
 expr_stmt|;
 block|}
 comment|/* implement set userid/groupid */
+name|p
+operator|->
+name|p_flag
+operator|&=
+operator|~
+name|SUGID
+expr_stmt|;
 comment|/* 	 * Turn off kernel tracing for set-id programs, except for 	 * root. 	 */
 if|if
 condition|(
@@ -956,6 +963,12 @@ name|attr
 operator|.
 name|va_uid
 expr_stmt|;
+name|p
+operator|->
+name|p_flag
+operator||=
+name|SUGID
+expr_stmt|;
 block|}
 if|if
 condition|(
@@ -1001,6 +1014,12 @@ operator|=
 name|attr
 operator|.
 name|va_gid
+expr_stmt|;
+name|p
+operator|->
+name|p_flag
+operator||=
+name|SUGID
 expr_stmt|;
 block|}
 comment|/* 	 * Implement correct POSIX saved uid behavior. 	 */
@@ -1060,14 +1079,6 @@ name|p_acflag
 operator|&=
 operator|~
 name|AFORK
-expr_stmt|;
-comment|/* clear `set id since last exec' flag */
-name|p
-operator|->
-name|p_flag
-operator|&=
-operator|~
-name|SUGID
 expr_stmt|;
 comment|/* Set entry address */
 name|setregs
