d4d9fdcdaf3e6c8041de35192bd62fda3ee86639
Garrett Wollman <wollman@FreeBSD.org>
1994-03-15 02:48:51 +0000

    Implement 4.4's SUGID flag, which keeps track of processes which call set*id() (reset on exec()).  Also, implement 4.4's logic for setre[ug]id(), which is much simpler and closer to being the correct behavior (given that we're stuck with setre[ug]id() for a variety of reasons).
    Former-commit-id: 00044d2fe189cd34d268989f60e7f790c13cdd05

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1986, 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)proc.h	7.28 (Berkeley) 5/30/91  *	$Id: proc.h,v 1.5 1993/12/19 00:55:20 wollman Exp $  */
+comment|/*-  * Copyright (c) 1986, 1989, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)proc.h	7.28 (Berkeley) 5/30/91  *	$Id: proc.h,v 1.6 1994/01/14 16:25:46 davidg Exp $  */
 end_comment
 
 begin_ifndef
@@ -535,7 +535,7 @@ begin_define
 define|#
 directive|define
 name|SLOAD
-value|0x0000001
+value|0x00000001
 end_define
 
 begin_comment
@@ -546,7 +546,7 @@ begin_define
 define|#
 directive|define
 name|SSYS
-value|0x0000002
+value|0x00000002
 end_define
 
 begin_comment
@@ -557,7 +557,7 @@ begin_define
 define|#
 directive|define
 name|SSINTR
-value|0x0000004
+value|0x00000004
 end_define
 
 begin_comment
@@ -568,7 +568,7 @@ begin_define
 define|#
 directive|define
 name|SCTTY
-value|0x0000008
+value|0x00000008
 end_define
 
 begin_comment
@@ -579,7 +579,7 @@ begin_define
 define|#
 directive|define
 name|SPPWAIT
-value|0x0000010
+value|0x00000010
 end_define
 
 begin_comment
@@ -590,7 +590,7 @@ begin_define
 define|#
 directive|define
 name|SEXEC
-value|0x0000020
+value|0x00000020
 end_define
 
 begin_comment
@@ -601,7 +601,7 @@ begin_define
 define|#
 directive|define
 name|STIMO
-value|0x0000040
+value|0x00000040
 end_define
 
 begin_comment
@@ -612,7 +612,7 @@ begin_define
 define|#
 directive|define
 name|SSEL
-value|0x0000080
+value|0x00000080
 end_define
 
 begin_comment
@@ -623,7 +623,7 @@ begin_define
 define|#
 directive|define
 name|SWEXIT
-value|0x0000100
+value|0x00000100
 end_define
 
 begin_comment
@@ -634,7 +634,8 @@ begin_define
 define|#
 directive|define
 name|SNOCLDSTOP
-value|0x0000200
+define|\
+value|0x00000200
 end_define
 
 begin_comment
@@ -649,7 +650,7 @@ begin_define
 define|#
 directive|define
 name|SLOCK
-value|0x0000400
+value|0x00000400
 end_define
 
 begin_comment
@@ -660,7 +661,7 @@ begin_define
 define|#
 directive|define
 name|SKEEP
-value|0x0000800
+value|0x00000800
 end_define
 
 begin_comment
@@ -671,7 +672,7 @@ begin_define
 define|#
 directive|define
 name|SPHYSIO
-value|0x0001000
+value|0x00001000
 end_define
 
 begin_comment
@@ -682,7 +683,7 @@ begin_define
 define|#
 directive|define
 name|STRC
-value|0x0004000
+value|0x00004000
 end_define
 
 begin_comment
@@ -693,7 +694,7 @@ begin_define
 define|#
 directive|define
 name|SWTED
-value|0x0008000
+value|0x00008000
 end_define
 
 begin_comment
@@ -704,7 +705,7 @@ begin_define
 define|#
 directive|define
 name|SADVLCK
-value|0x0040000
+value|0x00040000
 end_define
 
 begin_comment
@@ -719,7 +720,7 @@ begin_define
 define|#
 directive|define
 name|SOWEUPC
-value|0x0002000
+value|0x00002000
 end_define
 
 begin_comment
@@ -736,7 +737,7 @@ begin_define
 define|#
 directive|define
 name|SHPUX
-value|0x0010000
+value|0x00010000
 end_define
 
 begin_comment
@@ -764,26 +765,23 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* not currently in use (never set) */
-end_comment
-
 begin_define
 define|#
 directive|define
-name|SPAGE
-value|0x0020000
+name|SUGID
+value|0x00020000
 end_define
 
 begin_comment
-comment|/* process in page wait state */
+comment|/* process has changed [ug]id since exec */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|SPAGEDAEMON
-value|0x0080000
+define|\
+value|0x00080000
 end_define
 
 begin_comment
