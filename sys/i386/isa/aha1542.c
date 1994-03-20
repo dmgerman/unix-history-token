1ba32b327b80dfbf1f390c72428be02a56dc2dd6
Garrett Wollman <wollman@FreeBSD.org>
1994-03-20 00:30:04 +0000

    Delete some references to sleep() that somehow crept in.
    Former-commit-id: 0a6e7702f1d6c076fe247b5445f1a01219920d5b

--- sys/i386/isa/aha1542.c
+++ sys/i386/isa/aha1542.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.20 1994/01/22 21:54:10 rgrimes Exp $  */
+comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.21 1994/03/01 16:06:37 ats Exp $  */
 end_comment
 
 begin_comment
@@ -3081,14 +3081,21 @@ operator|)
 operator|)
 condition|)
 block|{
-name|sleep
+name|tsleep
 argument_list|(
+operator|(
+name|caddr_t
+operator|)
 operator|&
 name|aha
 operator|->
 name|aha_ccb_free
 argument_list|,
 name|PRIBIO
+argument_list|,
+literal|"ahaccb"
+argument_list|,
+literal|0
 argument_list|)
 expr_stmt|;
 block|}
