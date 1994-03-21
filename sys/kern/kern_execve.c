ea19cf4ae099af80eb1eb07264d501b6174fda43
David Greenman <davidg@Root.COM>
1994-03-21 09:35:30 +0000

    Changed dynamic stack grow code to grow by "SGROWSIZ" amount. Initially allocate SGROWSIZ amount of stack. Also set vm_ssize to the initial stack VM size. Increased DFLSSIZ stack rlimit default to 8MB.
    Former-commit-id: b9df38ae7dee52d7d6d06a8f4a705e95cb319a14

--- sys/kern/kern_execve.c
+++ sys/kern/kern_execve.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman  * 4. The name of the developer may be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_execve.c,v 1.17 1994/03/15 02:48:35 wollman Exp $  */
+comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman  * 4. The name of the developer may be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_execve.c,v 1.18 1994/03/15 03:55:35 wollman Exp $  */
 end_comment
 
 begin_include
@@ -740,31 +740,6 @@ operator|*
 operator|)
 name|stack_base
 expr_stmt|;
-name|p
-operator|->
-name|p_vmspace
-operator|->
-name|vm_ssize
-operator|=
-operator|(
-operator|(
-operator|(
-name|caddr_t
-operator|)
-name|USRSTACK
-operator|-
-operator|(
-name|char
-operator|*
-operator|)
-name|stack_base
-operator|)
-operator|>>
-name|PAGE_SHIFT
-operator|)
-operator|+
-literal|1
-expr_stmt|;
 comment|/* 	 * Stuff argument count as first item on stack 	 */
 operator|*
 operator|(
@@ -1296,7 +1271,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Destroy old address space, and allocate a new stack  *	The new stack is only DFLSSIZ large because it is grown  *	automatically in trap.c.  */
+comment|/*  * Destroy old address space, and allocate a new stack  *	The new stack is only SGROWSIZ large because it is grown  *	automatically in trap.c.  */
 end_comment
 
 begin_function
@@ -1334,7 +1309,7 @@ call|)
 argument_list|(
 name|USRSTACK
 operator|-
-name|DFLSSIZ
+name|SGROWSIZ
 argument_list|)
 decl_stmt|;
 name|iparams
@@ -1373,7 +1348,7 @@ operator|)
 operator|&
 name|stack_addr
 argument_list|,
-name|DFLSSIZ
+name|SGROWSIZ
 argument_list|,
 name|FALSE
 argument_list|)
@@ -1387,6 +1362,14 @@ operator|(
 name|error
 operator|)
 return|;
+name|vmspace
+operator|->
+name|vm_ssize
+operator|=
+name|SGROWSIZ
+operator|>>
+name|PAGE_SHIFT
+expr_stmt|;
 comment|/* Initialize maximum stack address */
 name|vmspace
 operator|->
