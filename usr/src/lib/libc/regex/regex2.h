7821cc1ab5813d4543e246fdc61d9e2a8683e362
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-20 22:35:10 -0800

    delete UofT; requested change from Henry Spencer <henry@zoo.toronto.edu>
    SCCS-vsn: lib/libc/regex/cclass.h 8.3
    SCCS-vsn: lib/libc/regex/cname.h 8.3
    SCCS-vsn: lib/libc/regex/engine.c 8.5
    SCCS-vsn: lib/libc/regex/re_format.7 8.3
    SCCS-vsn: lib/libc/regex/regcomp.c 8.5
    SCCS-vsn: lib/libc/regex/regerror.c 8.4
    SCCS-vsn: lib/libc/regex/regex.3 8.4
    SCCS-vsn: lib/libc/regex/regex2.h 8.4
    SCCS-vsn: lib/libc/regex/regexec.c 8.3
    SCCS-vsn: lib/libc/regex/regfree.c 8.3
    SCCS-vsn: lib/libc/regex/utils.h 8.3
    
    Former-commit-id: 6607e29b98eb4f939a81f59887c4745a862bf038

--- usr/src/lib/libc/regex/regex2.h
+++ usr/src/lib/libc/regex/regex2.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993, 1994 Henry Spencer.  * Copyright (c) 1992, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Henry Spencer of the University of Toronto.  *  * %sccs.include.redist.c%  *  *	@(#)regex2.h	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992, 1993, 1994 Henry Spencer.  * Copyright (c) 1992, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Henry Spencer.  *  * %sccs.include.redist.c%  *  *	@(#)regex2.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
