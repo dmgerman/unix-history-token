a4c9408841c9a8cf1f3d1e516a66c344c6fc6e41
Andrey Chernov <ache@FreeBSD.org>
1994-03-21 14:53:08 +0000

    Fix printf for root system mounted on second floppy
    Former-commit-id: d014e1f76482a1e2788fdae0734926f72c4624f6

--- sys/amd64/amd64/autoconf.c
+++ sys/amd64/amd64/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.8 1994/01/31 23:47:24 davidg Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.9 1994/03/21 14:37:01 ache Exp $  */
 end_comment
 
 begin_comment
@@ -441,6 +441,13 @@ name|PARTITIONSHIFT
 value|3
 end_define
 
+begin_define
+define|#
+directive|define
+name|FDUNITSHIFT
+value|6
+end_define
+
 begin_comment
 comment|/*  * Attempt to find the device from which we were booted.  * If we can do so, and not instructed not to do so,  * change rootdev to correspond to the load device.  */
 end_comment
@@ -531,16 +538,6 @@ operator|)
 operator|&
 name|B_ADAPTORMASK
 expr_stmt|;
-name|part
-operator|=
-operator|(
-name|bootdev
-operator|>>
-name|B_PARTITIONSHIFT
-operator|)
-operator|&
-name|B_PARTITIONMASK
-expr_stmt|;
 name|unit
 operator|=
 operator|(
@@ -557,13 +554,30 @@ name|majdev
 operator|==
 name|FDMAJOR
 condition|)
+block|{
+name|part
+operator|=
+literal|0
+expr_stmt|;
 name|mindev
 operator|=
 name|unit
 operator|<<
-literal|6
+name|FDUNITSHIFT
 expr_stmt|;
+block|}
 else|else
+block|{
+name|part
+operator|=
+operator|(
+name|bootdev
+operator|>>
+name|B_PARTITIONSHIFT
+operator|)
+operator|&
+name|B_PARTITIONMASK
+expr_stmt|;
 name|mindev
 operator|=
 operator|(
@@ -574,6 +588,7 @@ operator|)
 operator|+
 name|part
 expr_stmt|;
+block|}
 name|orootdev
 operator|=
 name|rootdev
@@ -617,7 +632,15 @@ index|]
 argument_list|,
 name|mindev
 operator|>>
+operator|(
+name|majdev
+operator|==
+name|FDMAJOR
+condition|?
+name|FDUNITSHIFT
+else|:
 name|PARTITIONSHIFT
+operator|)
 argument_list|,
 name|part
 operator|+
