6672faee7540daa74aec0c2eaee9e97ff061e031
David Greenman <davidg@Root.COM>
1994-03-21 09:46:12 +0000

    Modified swap space allocation policy to be more conservative when dealing with small objects. This is a first-cut change, and will likely change again. Lowered size of 'window' of pages to watch ("MINSCAN") in pageout daemon to 256, and fixed a minor bug that resulted in a benign console message.
    Former-commit-id: b525b1157b711bafccbd7da20be769806ca1c6fc

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  * from: @(#)swap_pager.c	7.4 (Berkeley) 5/7/91  *  * $Id: swap_pager.c,v 1.20 1994/03/08 21:44:38 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  * from: @(#)swap_pager.c	7.4 (Berkeley) 5/7/91  *  * $Id: swap_pager.c,v 1.21 1994/03/14 21:54:21 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -5076,13 +5076,55 @@ name|SWB_NPAGES
 else|:
 literal|1
 expr_stmt|;
+comment|/* 			 * this code is alittle conservative, but works 			 * (the intent of this code is to allocate small chunks 			 *  for small objects) 			 */
+if|if
+condition|(
+operator|(
+name|m
+index|[
+name|j
+index|]
+operator|->
+name|offset
+operator|==
+literal|0
+operator|)
+operator|&&
+operator|(
+name|ntoget
+operator|*
+name|NBPG
+operator|>
+name|object
+operator|->
+name|size
+operator|)
+condition|)
+block|{
+name|ntoget
+operator|=
+operator|(
+name|object
+operator|->
+name|size
+operator|+
+operator|(
+name|NBPG
+operator|-
+literal|1
+operator|)
+operator|)
+operator|/
+name|NBPG
+expr_stmt|;
+block|}
 name|retrygetspace
 label|:
 if|if
 condition|(
 name|ntoget
-operator|==
-name|SWB_NPAGES
+operator|>
+literal|1
 operator|&&
 name|rlist_alloc
 argument_list|(
@@ -5753,6 +5795,7 @@ argument_list|,
 name|foff
 argument_list|)
 expr_stmt|;
+comment|/* 		 * set the valid bit 		 */
 name|swb
 index|[
 name|i
@@ -5766,6 +5809,7 @@ operator|<<
 name|off
 operator|)
 expr_stmt|;
+comment|/* 		 * and unlock the data structure 		 */
 operator|--
 name|swb
 index|[
