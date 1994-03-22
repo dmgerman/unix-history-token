1741fc1bafd50cdfd68b69cf6d8d36d368655abe
David Greenman <davidg@Root.COM>
1994-03-22 02:02:25 +0000

    From mrl@teleport.com (Mostyn R. Lewis) >PPP and SLIP users - deleting a route can be deadly! > >Yes this can cause a kernel page fault or random hang. ... >you stand a good chance of a kernel page fault or a random hang because >the routing table for default still has a pointer to the deleted kernel >ifaddr structure. Depending on the re-use state of that memory you live >or die. Death is caused by a function pointer dereference. > >A fix for this, in route.c (/usr/src/sys/net) is to scan the linked list >of ifaddr entries for the interface to see if the pointer is still valid. >The diffs for the fix are appended. There are two versions; mute and >verbose. The verbose fix prints information the the system log to verify >states.
    Former-commit-id: d946eb6c056dc3161ff3d8680a4fe0f86a633c66

--- sys/net/route.c
+++ sys/net/route.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)route.c	7.22 (Berkeley) 6/27/91  *	$Id: route.c,v 1.4 1993/11/25 01:34:10 wollman Exp $  */
+comment|/*  * Copyright (c) 1980, 1986, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)route.c	7.22 (Berkeley) 6/27/91  *	$Id: route.c,v 1.5 1993/12/19 00:52:06 wollman Exp $  */
 end_comment
 
 begin_include
@@ -1790,6 +1790,11 @@ modifier|*
 name|rnh
 decl_stmt|;
 name|struct
+name|ifnet
+modifier|*
+name|ifp
+decl_stmt|;
+name|struct
 name|ifaddr
 modifier|*
 name|ifa
@@ -1978,6 +1983,45 @@ name|ifa
 operator|->
 name|ifa_rtrequest
 condition|)
+block|{
+if|if
+condition|(
+name|ifp
+operator|=
+name|rt
+operator|->
+name|rt_ifp
+condition|)
+block|{
+name|struct
+name|ifaddr
+modifier|*
+name|tifa
+decl_stmt|;
+for|for
+control|(
+name|tifa
+operator|=
+name|ifp
+operator|->
+name|if_addrlist
+init|;
+name|tifa
+condition|;
+name|tifa
+operator|=
+name|tifa
+operator|->
+name|ifa_next
+control|)
+block|{
+if|if
+condition|(
+name|tifa
+operator|==
+name|ifa
+condition|)
+block|{
 name|ifa
 operator|->
 name|ifa_rtrequest
@@ -1992,6 +2036,11 @@ literal|0
 argument_list|)
 argument_list|)
 expr_stmt|;
+break|break;
+block|}
+block|}
+block|}
+block|}
 name|rttrash
 operator|++
 expr_stmt|;
