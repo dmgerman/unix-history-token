a9dbb308cbd7b4d5f1cc3fc7b2b39e4cf5911769
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-22 15:34:37 -0800

    put an AT&T copyright on it
    SCCS-vsn: sys/vax/stand/conf.c 7.10
    SCCS-vsn: sys/vax/stand/confhpup.c 7.8
    
    Former-commit-id: 63097fc64660eb250e04c0404af7edb432a5633e

--- usr/src/sys/vax/stand/conf.c
+++ usr/src/sys/vax/stand/conf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.c	7.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.c	7.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
--- usr/src/sys/vax/stand/confhpup.c
+++ usr/src/sys/vax/stand/confhpup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)confhpup.c	7.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)confhpup.c	7.8 (Berkeley) %G%  */
 end_comment
 
 begin_include
