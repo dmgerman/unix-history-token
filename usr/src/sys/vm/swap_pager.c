7f2f3523122ca183a86d1c64087705dff6fc6eca
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-03-22 00:37:50 -0800

    lint
    SCCS-vsn: sys/vm/swap_pager.c 8.9
    
    Former-commit-id: a4607b0da532330a5e8e68dfd662146248a72a06

--- usr/src/sys/vm/swap_pager.c
+++ usr/src/sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.8 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -3837,6 +3837,11 @@ expr_stmt|;
 break|break;
 block|}
 block|}
+name|splx
+argument_list|(
+name|s
+argument_list|)
+expr_stmt|;
 comment|/* 		 * No operations done, thats all we can do for now. 		 */
 if|if
 condition|(
@@ -3844,19 +3849,7 @@ name|spc
 operator|==
 name|NULL
 condition|)
-block|{
-name|splx
-argument_list|(
-name|s
-argument_list|)
-expr_stmt|;
 break|break;
-block|}
-name|splx
-argument_list|(
-name|s
-argument_list|)
-expr_stmt|;
 comment|/* 		 * Found a completed operation so finish it off. 		 * Note: no longer at splbio since entry is off the list. 		 */
 name|m
 operator|=
