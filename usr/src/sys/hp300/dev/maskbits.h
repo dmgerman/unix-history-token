a28d161ae18b06a51334a4db4a8ca80aaa1c3b30
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-21 21:50:27 -0800

    add add a copyright  noitce
    SCCS-vsn: sys/hp300/dev/maskbits.h 8.2
    
    Former-commit-id: bb39abb895c2e1199a66fe9a123cd78068ba2fb0

--- usr/src/sys/hp300/dev/maskbits.h
+++ usr/src/sys/hp300/dev/maskbits.h
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/*-  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)maskbits.h	8.2 (Berkeley) %G%  */
+end_comment
+
+begin_comment
 comment|/*  * Derived from X11R4  */
 end_comment
 
