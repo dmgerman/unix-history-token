525c69dec814fc0f865ef0b08348b2592a0a81a9
David Greenman <davidg@Root.COM>
1994-03-24 02:23:00 +0000

    Enabled bounce buffers by default. This should be done via a probe for the specific controller type, so this change is temporary.
    Former-commit-id: cb138093217e8e3a5067a67491dbbe1fd34d7db4

--- sys/i386/isa/ultra14f.c
+++ sys/i386/isa/ultra14f.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Ported for use with the UltraStor 14f by Gary Close (gclose@wvnvms.wvnet.edu)  * Slight fixes to timeouts to run with the 34F  * Thanks to Julian Elischer for advice and help with this port.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  * slight mod to make work with 34F as well: Wed Jun  2 18:05:48 WST 1993  *  *      $Id: ultra14f.c,v 1.15 1994/01/29 10:29:14 rgrimes Exp $  */
+comment|/*  * Ported for use with the UltraStor 14f by Gary Close (gclose@wvnvms.wvnet.edu)  * Slight fixes to timeouts to run with the 34F  * Thanks to Julian Elischer for advice and help with this port.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  * slight mod to make work with 34F as well: Wed Jun  2 18:05:48 WST 1993  *  *      $Id: ultra14f.c,v 1.16 1994/03/20 00:30:04 wollman Exp $  */
 end_comment
 
 begin_include
@@ -2130,6 +2130,14 @@ name|device
 operator|=
 operator|&
 name|uha_dev
+expr_stmt|;
+name|uha
+operator|->
+name|sc_link
+operator|.
+name|flags
+operator|=
+name|SDEV_BOUNCE
 expr_stmt|;
 comment|/* 	 * ask the adapter what subunits are present 	 */
 name|scsi_attachdevs
