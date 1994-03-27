a5ba35ce22b5b9cc42d9685eb3195eada4601ffd
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-03-27 14:44:04 -0800

    remove _PATH_EXECDIR, add _PATH_SBIN, _PATH_USRSBIN
    SCCS-vsn: sbin/mount/pathnames.h 8.2
    
    Former-commit-id: 81dabaae9858067a410921346f20ece4001e3ad5

--- usr/src/sbin/mount/pathnames.h
+++ usr/src/sbin/mount/pathnames.h
@@ -1,13 +1,20 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pathnames.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|_PATH_EXECDIR
+name|_PATH_SBIN
 value|"/sbin"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_USRSBIN
+value|"/usr/sbin"
 end_define
 
 begin_define
