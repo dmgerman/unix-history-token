b0ba77441413e6f3642d1663edc61b599397521d
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-03-27 20:44:54 -0800

    fix typo
    SCCS-vsn: sbin/mount_portal/conf.c 8.2
    
    Former-commit-id: 9cc8e4beddfcc41726f6de0f38b9a9f2079e4b47

--- usr/src/sbin/mount_portal/conf.c
+++ usr/src/sbin/mount_portal/conf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)conf.c	8.1 (Berkeley) %G%  *  * $Id: conf.c,v 1.2 1992/05/27 07:09:27 jsp Exp jsp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)conf.c	8.2 (Berkeley) %G%  *  * $Id: conf.c,v 1.2 1992/05/27 07:09:27 jsp Exp jsp $  */
 end_comment
 
 begin_include
@@ -900,7 +900,7 @@ name|qelem
 modifier|*
 name|q
 init|=
-name|q
+name|q0
 operator|->
 name|q_forw
 decl_stmt|;
