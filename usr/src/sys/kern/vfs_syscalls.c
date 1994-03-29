ccfcc5c950390b134c7e4a2fb4fbb90db443e37b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-28 20:21:19 -0800

    change chown(2) to follow symbolic links
    SCCS-vsn: sys/kern/vfs_syscalls.c 8.12
    
    Former-commit-id: 049a1168579058155e0826ea6234b210b93c06e9

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	8.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	8.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -7937,7 +7937,7 @@ name|nd
 argument_list|,
 name|LOOKUP
 argument_list|,
-name|NOFOLLOW
+name|FOLLOW
 argument_list|,
 name|UIO_USERSPACE
 argument_list|,
