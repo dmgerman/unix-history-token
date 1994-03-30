c6c7bf803f02de9407b2e4f79d03cda35c1a4885
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-03-30 09:56:23 +0000

    Update this too.
    Former-commit-id: 1a2ce353852504b5190a60a261179d0e3a37488d

--- sys/net/ppp.h
+++ sys/net/ppp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ppp.h - PPP global declarations.  *  * Copyright (c) 1989 Carnegie Mellon University.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by Carnegie Mellon University.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *	from: unknown  *	$Id$  */
+comment|/*  * ppp.h - PPP global declarations.  *  * Copyright (c) 1989 Carnegie Mellon University.  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by Carnegie Mellon University.  The name of the  * University may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ppp.h,v 1.1 1994/03/30 09:38:17 jkh Exp $  */
 end_comment
 
 begin_ifndef
@@ -18,7 +18,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|NPPP
+name|_NPPP
 value|1
 end_define
 
@@ -94,6 +94,17 @@ end_define
 
 begin_comment
 comment|/* Crytpographic Handshake Protocol */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|LQR
+value|0xc025
+end_define
+
+begin_comment
+comment|/* Link Quality Report protocol */
 end_comment
 
 begin_define
