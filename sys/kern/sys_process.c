4faf74b6b993c08426da97f43a92c3dbf019c011
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-04-02 23:03:01 +0000

    Add a little extra sanity checking so that end profil requests  don't generate spurious faults.  I'm not sure how long this has been broken,  but profiling sure won't work as it is before this patch.
    Former-commit-id: ee4e2cd49f85f32015c596db0a59c28813b81922

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)sys_process.c	7.22 (Berkeley) 5/11/91  *	$Id: sys_process.c,v 1.9 1993/12/19 00:51:35 wollman Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)sys_process.c	7.22 (Berkeley) 5/11/91  *	$Id: sys_process.c,v 1.10 1994/02/26 07:17:53 davidg Exp $  */
 end_comment
 
 begin_include
@@ -1670,9 +1670,17 @@ name|retval
 decl_stmt|;
 block|{
 comment|/* from looking at man pages, and include files, looks like 	 * this just sets up the fields of p->p_stats->p_prof... 	 * and those fields come straight from the args. 	 * only thing *we* have to do is check the args for validity... 	 * 	 * cgd 	 */
-comment|/* check to make sure that the buffer is OK.  addupc (in locore) 	 * checks for faults, but would one be generated, say, writing to 	 * kernel space?  probably not -- it just uses "movl"... 	 * 	 * so we've gotta check to make sure that the info set up for 	 * addupc is set right... it's gotta be writable by the user... 	 */
+comment|/* check to make sure that the buffer is OK.  addupc (in locore) 	 * checks for faults, but would one be generated, say, writing to 	 * kernel space?  probably not -- it just uses "movl"... 	 * 	 * so we've gotta check to make sure that the info set up for 	 * addupc is set right... it's gotta be writable by the user... 	 * 	 * Add a little extra sanity checking so that end profil requests 	 * don't generate spurious faults.	-jkh 	 */
 if|if
 condition|(
+name|uap
+operator|->
+name|bufbase
+operator|&&
+name|uap
+operator|->
+name|bufsize
+operator|&&
 name|useracc
 argument_list|(
 operator|(
