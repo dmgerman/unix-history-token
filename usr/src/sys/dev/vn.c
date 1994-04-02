2cebaf3d175d49f3955836a91a3ce8d37c64efeb
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-04-01 23:47:12 -0800

    oops, forgot to update the Utah id
    SCCS-vsn: sys/dev/vn.c 8.6
    
    Former-commit-id: 1425fd6154fc17ee9f85076819dd40d0ebdc94a9

--- usr/src/sys/dev/vn.c
+++ usr/src/sys/dev/vn.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vn.c 1.8 92/12/20$  *  *	@(#)vn.c	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: vn.c 1.13 94/04/02$  *  *	@(#)vn.c	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
