0d7f0fd3a0df7887bdd5f13c76f6c917dfa9bbbe
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-02 22:19:10 -0800

    put redistributable notice on it, I'm putting it in 4.4BSD-Lite
    SCCS-vsn: old/talk/talk/ctl.c 1.5
    SCCS-vsn: old/talk/talk/ctl_transact.c 1.4
    SCCS-vsn: old/talk/talk/display.c 8.2
    SCCS-vsn: old/talk/talk/get_addrs.c 8.2
    SCCS-vsn: old/talk/talk/get_names.c 8.2
    SCCS-vsn: old/talk/talk/init_disp.c 1.4
    SCCS-vsn: old/talk/talk/invite.c 5.3
    SCCS-vsn: old/talk/talk/io.c 5.3
    SCCS-vsn: old/talk/talk/look_up.c 6.4
    SCCS-vsn: old/talk/talk/msgs.c 5.3
    SCCS-vsn: old/talk/talk/talk.c 8.2
    SCCS-vsn: old/talk/talk/talk.h 5.3
    SCCS-vsn: old/talk/talk/talk_ctl.h 8.2
    
    Former-commit-id: a16a922f20d01719acb88cffade4804b29f72ad6

--- usr/src/old/talk/talk/look_up.c
+++ usr/src/old/talk/talk/look_up.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1983, 1985  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_comment
@@ -19,7 +19,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)look_up.c	6.3 (Berkeley) %G%"
+literal|"@(#)look_up.c	6.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
--- usr/src/old/talk/talk/talk_ctl.h
+++ usr/src/old/talk/talk/talk_ctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  *  *	@(#)talk_ctl.h	5.1 (Berkeley) 6/6/85  */
+comment|/*-  * Copyright (c) 1983, 1985  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)talk_ctl.h	5.1 (Berkeley) 6/6/85  */
 end_comment
 
 begin_include
