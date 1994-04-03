e1ae100c8f9c06b022f545b3626858c9cfb77168
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-02 16:53:07 -0800

    copyright cleanup
    SCCS-vsn: bin/ed/ed.c 5.2
    
    Former-commit-id: 2e696468b38c38861d4e351043a20b401cac4b02

--- usr/src/bin/ed/ed.c
+++ usr/src/bin/ed/ed.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * %sccs.include.proprietary.c%  */
+comment|/*-  * Copyright (c) 1991  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.proprietary.c%  */
 end_comment
 
 begin_ifndef
@@ -14,7 +14,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1991 The Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1991\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
