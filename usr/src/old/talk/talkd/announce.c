5bb3e19a1114eb3110e047d1d6c4ac58a88af8a5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-02 22:23:58 -0800

    put redistributable notice on it, I'm putting it in 4.4BSD-Lite
    SCCS-vsn: old/talk/common_source/ctl.h 8.2
    SCCS-vsn: old/talk/talkd/announce.c 5.3
    SCCS-vsn: old/talk/talkd/print.c 5.3
    SCCS-vsn: old/talk/talkd/process.c 5.3
    SCCS-vsn: old/talk/talkd/table.c 5.3
    SCCS-vsn: old/talk/talkd/talkd.c 5.3
    
    Former-commit-id: d29f1461ffc72909b6b21c397e754a3847f0854d

--- usr/src/old/talk/talkd/announce.c
+++ usr/src/old/talk/talkd/announce.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1983, 1985  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
--- usr/src/old/talk/talkd/talkd.c
+++ usr/src/old/talk/talkd/talkd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Regents of the University of California.  * All rights reserved.  The Berkeley software License Agreement  * specifies the terms and conditions for redistribution.  */
+comment|/*-  * Copyright (c) 1983, 1985  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -14,7 +14,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1983 Regents of the University of California.\n\  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1983, 1985\n\ 	Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
