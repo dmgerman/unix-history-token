63f1e6757fa3cd961f88e9e5efa8eab544e6d04a
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-04-02 16:10:06 -0800

    date and time created 94/04/02 09:10:06 by pendry
    SCCS-vsn: usr.bin/chpass/pw_copy.h 8.1
    
    Former-commit-id: 7c5fd01f57c90cfa37afcd66aadce629f6a7395e

---
+++ usr/src/usr.bin/chpass/pw_copy.h
@@ -0,0 +1,25 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)pw_copy.h	8.1 (Berkeley) %G%  */
+end_comment
+
+begin_decl_stmt
+name|void
+name|pw_copy
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+name|int
+operator|,
+expr|struct
+name|passwd
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
