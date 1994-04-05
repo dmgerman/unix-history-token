6cf63aae8dfad564a93e06161b66286ad6f9eb16
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-04-04 19:36:06 -0800

    eliminate local version of glob (cf ftp) clean up yacc grammar.  prettiness police.
    SCCS-vsn: libexec/ftpd/Makefile 8.2
    SCCS-vsn: libexec/ftpd/popen.c 8.2
    SCCS-vsn: libexec/ftpd/ftpd.c 8.2
    SCCS-vsn: libexec/ftpd/ftpcmd.y 8.2
    SCCS-vsn: libexec/ftpd/extern.h 8.2
    
    Former-commit-id: 7103499f1c6de0bc3285d5331646c44f2cee3153

--- usr/src/libexec/ftpd/extern.h
+++ usr/src/libexec/ftpd/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_decl_stmt
@@ -107,21 +107,6 @@ operator|(
 name|char
 operator|*
 operator|,
-name|char
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|char
-modifier|*
-modifier|*
-name|ftpglob
-name|__P
-argument_list|(
-operator|(
 name|char
 operator|*
 operator|)
@@ -439,8 +424,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|char
-modifier|*
+name|void
 name|yyerror
 name|__P
 argument_list|(
