93620acccf3906cd0bcf7fa356f0820cb1c7ab3f
Andrey Chernov <ache@FreeBSD.org>
1994-04-07 00:30:35 +0000

    Fix error with path like /a/./b (from N. Dudorov)
    Former-commit-id: b905a5f3f39275932cd5a6ee1d5705fbc772c3cf

--- sys/pcfs/pcfs_conv.c
+++ sys/pcfs/pcfs_conv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  Written by Paul Popelka (paulp@uts.amdahl.com)  *  *  You can do anything you want with this software,  *    just don't say you wrote it,  *    and don't remove this notice.  *  *  This software is provided "as is".  *  *  The author supplies this software to be publicly  *  redistributed on the understanding that the author  *  is not responsible for the correct functioning of  *  this software in any circumstances and is not liable  *  for any damages caused by this software.  *  *  October 1992  *  *	$Id: pcfs_conv.c,v 1.3 1993/11/25 01:37:08 wollman Exp $  */
+comment|/*  *  Written by Paul Popelka (paulp@uts.amdahl.com)  *  *  You can do anything you want with this software,  *    just don't say you wrote it,  *    and don't remove this notice.  *  *  This software is provided "as is".  *  *  The author supplies this software to be publicly  *  redistributed on the understanding that the author  *  is not responsible for the correct functioning of  *  this software in any circumstances and is not liable  *  for any damages caused by this software.  *  *  October 1992  *  *	$Id: pcfs_conv.c,v 1.4 1993/12/19 00:54:27 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -1036,12 +1036,9 @@ index|]
 operator|==
 literal|'.'
 operator|&&
-name|un
-index|[
-literal|1
-index|]
+name|unlen
 operator|==
-literal|'\0'
+literal|1
 condition|)
 block|{
 name|dn
@@ -1069,12 +1066,9 @@ index|]
 operator|==
 literal|'.'
 operator|&&
-name|un
-index|[
-literal|2
-index|]
+name|unlen
 operator|==
-literal|'\0'
+literal|2
 condition|)
 block|{
 name|dn
