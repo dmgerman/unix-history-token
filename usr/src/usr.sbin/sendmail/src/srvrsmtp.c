53a47b33e375126f6bb015e92864adc26c40fa1e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-12 21:38:14 -0800

    make HELO and EHLO response consistent
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.34
    
    Former-commit-id: 21dc77f3664d5f084d04a773f748424de868fcd2

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.33 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	8.34 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.33 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	8.34 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
