beadb1b96fd5a1c83fe267df9da2e07ba1822b22
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-12 21:18:58 -0800

    SCO support for fsync
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.101
    SCCS-vsn: usr.sbin/sendmail/src/Makefiles/Makefile.SCO 8.4
    
    Former-commit-id: 0cc938f3e35daf92de86e039391b411dbd757868

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.100 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.101 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2460,6 +2460,17 @@ end_define
 
 begin_comment
 comment|/* has setreuid(2) call */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NEEDFSYNC
+value|1
+end_define
+
+begin_comment
+comment|/* needs the fsync(2) call stub */
 end_comment
 
 begin_define
