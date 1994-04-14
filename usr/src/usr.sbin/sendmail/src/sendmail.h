e50063e14fac8b78179ae66708c8768cc269d6bc
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-14 15:31:20 -0800

    add PRIV_NORECEIPTS
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.43
    
    Former-commit-id: 146624ba93d478940e721e0b5aea2ee292e4d496

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.42 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.43 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.42		%G%"
+literal|"@(#)sendmail.h	8.43		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3532,6 +3532,17 @@ end_define
 
 begin_comment
 comment|/* flag possible authorization probs */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PRIV_NORECEIPTS
+value|00100
+end_define
+
+begin_comment
+comment|/* disallow return receipts */
 end_comment
 
 begin_define
