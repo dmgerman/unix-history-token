839e617c002f45688a71698e85a298522ba81a8b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-16 19:18:45 -0800

    If compressed TCP packets were being received on a SLIP link while it was first configured, the system could crash if a packet with a compressed connection ID was received. BSDI's patch SK110-004
    SCCS-vsn: sys/net/slcompress.c 8.2
    
    Former-commit-id: 4c99674827a8ccbfa24c18f5936d03ac886ab786

--- usr/src/sys/net/slcompress.c
+++ usr/src/sys/net/slcompress.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)slcompress.c	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1989, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)slcompress.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -257,6 +257,12 @@ operator|->
 name|last_xmit
 operator|=
 literal|255
+expr_stmt|;
+name|comp
+operator|->
+name|flags
+operator|=
+name|SLF_TOSS
 expr_stmt|;
 block|}
 end_function
