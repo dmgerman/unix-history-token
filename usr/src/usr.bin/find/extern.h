5559fb4036a7066418cd1906a1eabca6ce17feb9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-16 20:56:14 -0800

    option() is now static to option.c
    SCCS-vsn: usr.bin/find/extern.h 8.3
    
    Former-commit-id: 76cb1e499886fc0d2551cbca1caca57368ce2860

--- usr/src/usr.bin/find/extern.h
+++ usr/src/usr.bin/find/extern.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	8.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)extern.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -101,20 +101,6 @@ name|__P
 argument_list|(
 operator|(
 name|PLAN
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|OPTION
-modifier|*
-name|option
-name|__P
-argument_list|(
-operator|(
-name|char
 operator|*
 operator|)
 argument_list|)
