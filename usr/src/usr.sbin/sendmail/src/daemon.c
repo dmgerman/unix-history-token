98f202278ccba11202c745f065aa595b5c36e0c2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-18 13:48:20 -0800

    sleep between retries
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.48
    
    Former-commit-id: 54890b8dcd2b0d7469de74cebed89c44e742271e

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.47 (Berkeley) %G% (with daemon mode)"
+literal|"@(#)daemon.c	8.48 (Berkeley) %G% (with daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -54,7 +54,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.47 (Berkeley) %G% (without daemon mode)"
+literal|"@(#)daemon.c	8.48 (Berkeley) %G% (without daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
