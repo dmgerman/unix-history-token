b9889f98e91fe93b6f2244f9da1032dd6fd028e3
Andrey Chernov <ache@FreeBSD.org>
1994-04-20 18:37:10 +0000

    1) Fix from Bruce: Somethimes rb_hd, rb_tl can point to different addresses, so change tsleep/wakeup to use constant addresses 2) Change tsleep wait channel from tty* to ptc* to avoid collisions for debug purposes.
    Former-commit-id: edd313517cbcf283c1587a0f35096e6af6b804ca

--- sys/kern/tty_pty.c
+++ sys/kern/tty_pty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty_pty.c	7.21 (Berkeley) 5/30/91  *	$Id: tty_pty.c,v 1.11 1994/04/03 19:40:01 ache Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty_pty.c	7.21 (Berkeley) 5/30/91  *	$Id: tty_pty.c,v 1.12 1994/04/05 10:12:41 ache Exp $  */
 end_comment
 
 begin_comment
@@ -127,6 +127,26 @@ end_define
 begin_comment
 comment|/* Chunk size iomoved to/from user */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|PTCREAD_SLEEP_ADDR
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)&tp->t_out + 1)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PTCWRITE_SLEEP_ADDR
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)&tp->t_raw + 1)
+end_define
 
 begin_function_decl
 specifier|static
@@ -1307,15 +1327,10 @@ expr_stmt|;
 block|}
 name|wakeup
 argument_list|(
-operator|(
-name|caddr_t
-operator|)
-operator|&
+name|PTCREAD_SLEEP_ADDR
+argument_list|(
 name|tp
-operator|->
-name|t_out
-operator|->
-name|rb_tl
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
@@ -1362,15 +1377,10 @@ expr_stmt|;
 block|}
 name|wakeup
 argument_list|(
-operator|(
-name|caddr_t
-operator|)
-operator|&
+name|PTCWRITE_SLEEP_ADDR
+argument_list|(
 name|tp
-operator|->
-name|t_raw
-operator|->
-name|rb_hd
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
@@ -1989,21 +1999,16 @@ name|error
 operator|=
 name|tsleep
 argument_list|(
-operator|(
-name|caddr_t
-operator|)
-operator|&
+name|PTCREAD_SLEEP_ADDR
+argument_list|(
 name|tp
-operator|->
-name|t_out
-operator|->
-name|rb_tl
+argument_list|)
 argument_list|,
 name|TTIPRI
 operator||
 name|PCATCH
 argument_list|,
-name|ttyin
+literal|"ptcin"
 argument_list|,
 literal|0
 argument_list|)
@@ -3260,21 +3265,16 @@ name|error
 operator|=
 name|tsleep
 argument_list|(
-operator|(
-name|caddr_t
-operator|)
-operator|&
+name|PTCWRITE_SLEEP_ADDR
+argument_list|(
 name|tp
-operator|->
-name|t_raw
-operator|->
-name|rb_hd
+argument_list|)
 argument_list|,
 name|TTOPRI
 operator||
 name|PCATCH
 argument_list|,
-name|ttyout
+literal|"ptcout"
 argument_list|,
 literal|0
 argument_list|)
