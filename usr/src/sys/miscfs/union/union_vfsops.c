c975d9885e37f7aa3aed5e4ccbb0795172042977
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-04-24 15:45:45 -0800

    VROOT flag is set in union_allocvp
    SCCS-vsn: sys/miscfs/union/union_vfsops.c 8.8
    
    Former-commit-id: 5b94b9329e46097d19a85ee10edf19fc1cdcdbc5

--- usr/src/sys/miscfs/union/union_vfsops.c
+++ usr/src/sys/miscfs/union/union_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 The Regents of the University of California.  * Copyright (c) 1994 Jan-Simon Pendry.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union_vfsops.c	8.7 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1994 The Regents of the University of California.  * Copyright (c) 1994 Jan-Simon Pendry.  * All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union_vfsops.c	8.8 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1332,15 +1332,6 @@ expr_stmt|;
 block|}
 else|else
 block|{
-operator|(
-operator|*
-name|vpp
-operator|)
-operator|->
-name|v_flag
-operator||=
-name|VROOT
-expr_stmt|;
 if|if
 condition|(
 name|loselock
