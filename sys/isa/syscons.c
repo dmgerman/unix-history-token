3626afb88234383ee410825a6c904140ef0d733f
Soren Schmidt <sos@FreeBSD.org>
1994-04-26 09:09:57 +0000

    Fixed missing bounds check in scroll up/down sequence, that could cause a panic (and did).
    Former-commit-id: 14b6346765e001b26877ac27307c3da764558d0a

--- sys/isa/syscons.c
+++ sys/isa/syscons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992-1994 Søren Schmidt  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:@(#)syscons.c	1.3 940129  *	$Id: syscons.c,v 1.43 1994/04/12 00:05:23 ache Exp $  *  */
+comment|/*-  * Copyright (c) 1992-1994 Søren Schmidt  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:@(#)syscons.c	1.3 940129  *	$Id: syscons.c,v 1.44 1994/04/21 14:22:26 sos Exp $  *  */
 end_comment
 
 begin_if
@@ -4951,11 +4951,11 @@ expr_stmt|;
 comment|/* enable counter 2 output to speaker */
 name|outb
 argument_list|(
-literal|0x61
+name|IO_PPI
 argument_list|,
 name|inb
 argument_list|(
-literal|0x61
+name|IO_PPI
 argument_list|)
 operator||
 literal|3
@@ -4967,11 +4967,11 @@ block|{
 comment|/* disable counter 2 output to speaker */
 name|outb
 argument_list|(
-literal|0x61
+name|IO_PPI
 argument_list|,
 name|inb
 argument_list|(
-literal|0x61
+name|IO_PPI
 argument_list|)
 operator|&
 literal|0xFC
@@ -10134,6 +10134,20 @@ name|n
 operator|=
 literal|1
 expr_stmt|;
+if|if
+condition|(
+name|n
+operator|>
+name|scp
+operator|->
+name|ypos
+condition|)
+name|n
+operator|=
+name|scp
+operator|->
+name|ypos
+expr_stmt|;
 name|bcopy
 argument_list|(
 name|scp
@@ -10230,6 +10244,28 @@ name|n
 operator|=
 literal|1
 expr_stmt|;
+if|if
+condition|(
+name|n
+operator|>
+name|scp
+operator|->
+name|ysize
+operator|-
+name|scp
+operator|->
+name|ypos
+condition|)
+name|n
+operator|=
+name|scp
+operator|->
+name|ysize
+operator|-
+name|scp
+operator|->
+name|ypos
+expr_stmt|;
 name|bcopy
 argument_list|(
 name|scp
@@ -10313,6 +10349,28 @@ condition|)
 name|n
 operator|=
 literal|1
+expr_stmt|;
+if|if
+condition|(
+name|n
+operator|>
+name|scp
+operator|->
+name|xsize
+operator|-
+name|scp
+operator|->
+name|xpos
+condition|)
+name|n
+operator|=
+name|scp
+operator|->
+name|xsize
+operator|-
+name|scp
+operator|->
+name|xpos
 expr_stmt|;
 name|fillw
 argument_list|(
