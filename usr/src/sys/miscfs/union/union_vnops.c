4fb57709933eb2c65e9560e31629cad764256a14
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-05-10 07:05:57 -0800

    fix ref panic in link - same as previous panic but covering the case where a lower layer link is requested.  need to do a copyup to fix this right.
    SCCS-vsn: sys/miscfs/union/union_vnops.c 8.12
    
    Former-commit-id: 08a9a40130949f3b0f8547d37b2b719d45283518

--- usr/src/sys/miscfs/union/union_vnops.c
+++ usr/src/sys/miscfs/union/union_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993, 1994 The Regents of the University of California.  * Copyright (c) 1992, 1993, 1994 Jan-Simon Pendry.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union_vnops.c	8.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992, 1993, 1994 The Regents of the University of California.  * Copyright (c) 1992, 1993, 1994 Jan-Simon Pendry.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)union_vnops.c	8.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -3439,13 +3439,6 @@ argument_list|(
 name|ap
 operator|->
 name|a_vp
-argument_list|)
-expr_stmt|;
-name|vrele
-argument_list|(
-name|ap
-operator|->
-name|a_tdvp
 argument_list|)
 expr_stmt|;
 name|error
