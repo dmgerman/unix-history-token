0e0b291ce3688522cd8ccb7969e3dac1f0073b62
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-05-12 14:00:16 -0800

    portability fix for Solaris 2
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.105
    
    Former-commit-id: 9e01a8a3a46e98537c973d073aeab6893f6765ce

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.104 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.105 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -938,7 +938,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_UNIX
-value|"/kernel/unix"
+value|"/dev/ksyms"
 end_define
 
 begin_endif
