6d2b48cd44e6620c3acdc89461ec70c98a266123
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-05-17 15:12:22 -0800

    make VRFY 252 response more polite
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.39
    
    Former-commit-id: 9daa765356ccd30f8b0e7546f4e9034091f3ee73

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.38 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	8.39 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.38 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	8.39 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -2429,7 +2429,7 @@ name|vrfy
 condition|)
 name|message
 argument_list|(
-literal|"252 Who's to say?"
+literal|"252 Cannot VRFY user; try RCPT to attempt delivery (or try finger)"
 argument_list|)
 expr_stmt|;
 else|else
