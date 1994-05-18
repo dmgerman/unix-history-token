3e10a27ad83b2bf4084323d1d4d48d767b9d8c7a
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-05-18 16:22:04 +0000

    Sigh....
    I'm going to reverse myself on the earlier decision to disable vat
    compatibility by default.  Why?  Three reasons:
    
    1. The "incompatibility" wasn't anywhere near as bad as I thought.
       When ftp/rlogin/et al go to setsockopt() their TOS (Type Of Service)
       and it fails, they don't abort or anything drastic, they simply whinge
       to syslog and continue.  I can live with the occasional message on this,
       especially when a make world for the person in question will fix it.
    
    2. My attempt to make this conditional with IOP_VAT_COMPAT was bogus; I
       simply wasn't thinking!  Making it a kernel config option won't do squat
       for things like ftp and rlogin unless I go and add the option to _their_
       Makefiles too, which would be obviously evil and bad.  Conservative is all
       well and good, but this was just stupid.  There's really no easy way to do
       this for the minor error message it would avoid.  Forget it.
    
    3. I can avoid a doc hit on turning this on for 1.1.5 users - just setting
       the multicast options and recompiling the kernel is easy, remaking the world
       would have been a bit much to ask (and folks getting 1.1.5 bindist won't
       get the syslogd warnings anyway).
    
    
    Former-commit-id: 18278aef9921dfaba074b80a877ce09f4a40a5f7

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)in.h	7.11 (Berkeley) 4/20/91  *	$Id: in.h,v 1.5 1994/05/17 22:31:04 jkh Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)in.h	7.11 (Berkeley) 4/20/91  *	$Id: in.h,v 1.6 1994/05/18 00:04:14 jkh Exp $  */
 end_comment
 
 begin_ifndef
@@ -17,6 +17,14 @@ end_define
 
 begin_comment
 comment|/*  * Constants and structures defined by the internet system,  * Per RFC 790, September 1981.  */
+end_comment
+
+begin_comment
+comment|/* Enable this if you don't want to be IP_TOS compatible with BSDI/NetBSD */
+end_comment
+
+begin_comment
+comment|/* #define NO_VAT_COMPAT	1 */
 end_comment
 
 begin_comment
@@ -565,11 +573,11 @@ begin_comment
 comment|/* buf/ip_opts; set/get IP per-packet options */
 end_comment
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|IP_VAT_COMPAT
-end_ifdef
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|NO_VAT_COMPAT
+end_ifndef
 
 begin_define
 define|#
