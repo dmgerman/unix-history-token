82b896b19f1ce50c58ff670f2a9017ab24c3474c
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-05-19 08:17:53 +0000

    Don't give up on 24 bit card (Bt545 et al) case, just suggest that the aha driver may be necessary.
    Former-commit-id: e0024ed3996f206b3e4e894daaa6e8cfa0a4c9e9

--- sys/i386/isa/bt742a.c
+++ sys/i386/isa/bt742a.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt742a.c,v 1.131 1994/05/18 00:00:00 amurai Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt742a.c,v 1.17 1994/05/17 20:02:12 jkh Exp $  */
 end_comment
 
 begin_comment
@@ -4453,19 +4453,9 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"Try aha1542 driver instead! "
+literal|"You may have to use the aha1542 driver instead! "
 argument_list|)
 expr_stmt|;
-name|printf
-argument_list|(
-literal|"[giving up]\n"
-argument_list|)
-expr_stmt|;
-return|return
-operator|(
-name|ENXIO
-operator|)
-return|;
 break|break;
 case|case
 name|BT_BUS_TYPE_32bit
