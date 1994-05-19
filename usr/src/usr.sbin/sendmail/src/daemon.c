db161634e0afa64556b245a41331d35955ac3011
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-05-18 17:45:19 -0800

    close daemon socket early to limit possible race condition
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.51
    
    Former-commit-id: 70792d85c178d177e8461910fa5347e1cd28becd

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.50 (Berkeley) %G% (with daemon mode)"
+literal|"@(#)daemon.c	8.51 (Berkeley) %G% (with daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -54,7 +54,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.50 (Berkeley) %G% (without daemon mode)"
+literal|"@(#)daemon.c	8.51 (Berkeley) %G% (without daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
