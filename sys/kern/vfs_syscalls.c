28be996f78613a9c0005d2fe520c322592763c45
Andrey Chernov <ache@FreeBSD.org>
1994-05-22 22:31:52 +0000

    lseek don't check offset beyond beginning of file
    Former-commit-id: 27416041c16f8d3321e55d050e474bd99e4e6b73

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vfs_syscalls.c	7.74 (Berkeley) 6/21/91  *	$Id: vfs_syscalls.c,v 1.11 1994/03/15 05:55:08 davidg Exp $  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vfs_syscalls.c	7.74 (Berkeley) 6/21/91  *	$Id: vfs_syscalls.c,v 1.12 1994/03/16 23:32:58 davidg Exp $  */
 end_comment
 
 begin_include
@@ -4630,6 +4630,9 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|off_t
+name|offset
+decl_stmt|;
 if|if
 condition|(
 operator|(
@@ -4676,6 +4679,12 @@ operator|(
 name|ESPIPE
 operator|)
 return|;
+name|offset
+operator|=
+name|fp
+operator|->
+name|f_offset
+expr_stmt|;
 switch|switch
 condition|(
 name|uap
@@ -4686,9 +4695,7 @@ block|{
 case|case
 name|L_INCR
 case|:
-name|fp
-operator|->
-name|f_offset
+name|offset
 operator|+=
 name|uap
 operator|->
@@ -4726,9 +4733,7 @@ operator|(
 name|error
 operator|)
 return|;
-name|fp
-operator|->
-name|f_offset
+name|offset
 operator|=
 name|uap
 operator|->
@@ -4742,9 +4747,7 @@ break|break;
 case|case
 name|L_SET
 case|:
-name|fp
-operator|->
-name|f_offset
+name|offset
 operator|=
 name|uap
 operator|->
@@ -4758,12 +4761,25 @@ name|EINVAL
 operator|)
 return|;
 block|}
+if|if
+condition|(
+name|offset
+operator|<
+literal|0
+condition|)
+return|return
+operator|(
+name|EINVAL
+operator|)
+return|;
 operator|*
 name|retval
 operator|=
 name|fp
 operator|->
 name|f_offset
+operator|=
+name|offset
 expr_stmt|;
 return|return
 operator|(
