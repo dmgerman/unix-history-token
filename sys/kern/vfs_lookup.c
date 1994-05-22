80204c152ef8e240d8ec341f644c462908f52344
Andrey Chernov <ache@FreeBSD.org>
1994-05-22 23:08:06 +0000

    From Bruce:
    POSIX.1-1990 says:
    
    "2.2.2.57 pathname: ... If the pathname refers to a directory, it may also
    have one or more trailing slashes. ..."
    
    Here are my fixes to support trailing slashes on directories.  They
    include several other (much simpler) fixes:
    
    	o don't allow empty pathnames.
    	o allow names of length NAME_MAX.
    	o don't do anything except return successfullyt for
    	  rename(L1, L2) where L1 and L2 are links to the
    	  same file.
    
    
    Former-commit-id: 6a13a7fc0c58aa59bce291b261ba3253daeabff3

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright (c) UNIX System Laboratories, Inc.  All or some portions
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vfs_lookup.c	7.32 (Berkeley) 5/21/91  *	$Id: vfs_lookup.c,v 1.5 1993/11/25 01:33:41 wollman Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vfs_lookup.c	7.32 (Berkeley) 5/21/91  *	$Id: vfs_lookup.c,v 1.6 1994/05/04 08:27:11 rgrimes Exp $  */
 end_comment
 
 begin_include
@@ -252,6 +252,23 @@ name|ndp
 operator|->
 name|ni_pathlen
 argument_list|)
+expr_stmt|;
+comment|/* 	 * Don't allow empty pathname. 	 */
+if|if
+condition|(
+operator|!
+name|error
+operator|&&
+operator|*
+name|ndp
+operator|->
+name|ni_pnbuf
+operator|==
+literal|'\0'
+condition|)
+name|error
+operator|=
+name|ENOENT
 expr_stmt|;
 if|if
 condition|(
@@ -865,6 +882,9 @@ name|rdonly
 decl_stmt|;
 comment|/* mounted read-only flag bit(s) */
 name|int
+name|trailing_slash
+decl_stmt|;
+name|int
 name|error
 init|=
 literal|0
@@ -985,7 +1005,7 @@ init|;
 operator|*
 name|cp
 operator|!=
-literal|0
+literal|'\0'
 operator|&&
 operator|*
 name|cp
@@ -1021,7 +1041,7 @@ condition|(
 name|ndp
 operator|->
 name|ni_namelen
-operator|>=
+operator|>
 name|NAME_MAX
 condition|)
 block|{
@@ -1073,6 +1093,71 @@ name|ndp
 operator|->
 name|ni_namelen
 expr_stmt|;
+name|ndp
+operator|->
+name|ni_next
+operator|=
+name|cp
+expr_stmt|;
+comment|/* 	 * Replace multiple slashes by a single slash and trailing slashes 	 * by a null.  This must be done before VOP_LOOKUP() because the 	 * some fs's don't know about trailing slashes.  Trailing slashes 	 * will be disallowed later if the file turns out not to be a 	 * directory. 	 */
+name|trailing_slash
+operator|=
+literal|0
+expr_stmt|;
+while|while
+condition|(
+operator|*
+name|cp
+operator|==
+literal|'/'
+operator|&&
+operator|(
+name|cp
+index|[
+literal|1
+index|]
+operator|==
+literal|'/'
+operator|||
+name|cp
+index|[
+literal|1
+index|]
+operator|==
+literal|'\0'
+operator|)
+condition|)
+block|{
+name|cp
+operator|++
+expr_stmt|;
+name|ndp
+operator|->
+name|ni_pathlen
+operator|--
+expr_stmt|;
+if|if
+condition|(
+operator|*
+name|cp
+operator|==
+literal|'\0'
+condition|)
+block|{
+name|trailing_slash
+operator|=
+literal|1
+expr_stmt|;
+operator|*
+name|ndp
+operator|->
+name|ni_next
+operator|=
+literal|'\0'
+expr_stmt|;
+comment|/* XXX for direnter() ... */
+block|}
+block|}
 name|ndp
 operator|->
 name|ni_next
@@ -1383,7 +1468,23 @@ operator|||
 operator|*
 name|cp
 operator|!=
-literal|0
+literal|'\0'
+operator|||
+operator|*
+name|cp
+operator|==
+literal|'\0'
+operator|&&
+name|trailing_slash
+operator|&&
+operator|!
+operator|(
+name|ndp
+operator|->
+name|ni_nameiop
+operator|&
+name|WILLBEDIR
+operator|)
 condition|)
 goto|goto
 name|bad
@@ -1498,6 +1599,26 @@ operator|(
 literal|0
 operator|)
 return|;
+block|}
+comment|/* 	 * Check for bogus trailing slashes. 	 */
+if|if
+condition|(
+name|trailing_slash
+operator|&&
+name|dp
+operator|->
+name|v_type
+operator|!=
+name|VDIR
+condition|)
+block|{
+name|error
+operator|=
+name|ENOTDIR
+expr_stmt|;
+goto|goto
+name|bad2
+goto|;
 block|}
 comment|/* 	 * Check to see if the vnode has been mounted on; 	 * if so find the root of the mounted file system. 	 */
 name|mntloop
