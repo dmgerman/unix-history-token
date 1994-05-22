4176423d8ccded9e5efb07f5e38f9fa0c764036d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-05-21 21:19:04 -0800

    check for ENOTDIR before EISDIR in null pathname case
    SCCS-vsn: sys/kern/vfs_lookup.c 8.5
    
    Former-commit-id: 4785895504ebf1716c8f52b59ebc86c519bfb28d

--- usr/src/sys/kern/vfs_lookup.c
+++ usr/src/sys/kern/vfs_lookup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_lookup.c	8.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_lookup.c	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1223,23 +1223,6 @@ condition|)
 block|{
 if|if
 condition|(
-name|cnp
-operator|->
-name|cn_nameiop
-operator|!=
-name|LOOKUP
-condition|)
-block|{
-name|error
-operator|=
-name|EISDIR
-expr_stmt|;
-goto|goto
-name|bad
-goto|;
-block|}
-if|if
-condition|(
 name|dp
 operator|->
 name|v_type
@@ -1250,6 +1233,23 @@ block|{
 name|error
 operator|=
 name|ENOTDIR
+expr_stmt|;
+goto|goto
+name|bad
+goto|;
+block|}
+if|if
+condition|(
+name|cnp
+operator|->
+name|cn_nameiop
+operator|!=
+name|LOOKUP
+condition|)
+block|{
+name|error
+operator|=
+name|EISDIR
 expr_stmt|;
 goto|goto
 name|bad
