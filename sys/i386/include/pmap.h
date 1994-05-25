907006f3996165337c8611f67bc79727dd43e4c3
David Greenman <davidg@Root.COM>
1994-05-25 11:18:50 +0000

    Fixed broken pte bit definitions. I fixed this long ago in pte.h, but sigh, the definitions are duplicated in here, too.
    Former-commit-id: 7957566d73a9156bc690fe2fd5ce0c79b3337423

--- sys/i386/include/pmap.h
+++ sys/i386/include/pmap.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * Derived from hp300 version by Mike Hibler, this version by William  * Jolitz uses a recursive map [a pde points to the page directory] to  * map the page tables using the pagetables themselves. This is done to  * reduce the impact on kernel virtual memory for lots of sparse address  * space, and to reduce the cost of memory to each process.  *  *	from: hp300: @(#)pmap.h	7.2 (Berkeley) 12/16/90  *	from: @(#)pmap.h	7.4 (Berkeley) 5/12/91  * 	$Id: pmap.h,v 1.12 1994/03/24 23:12:48 davidg Exp $  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * Derived from hp300 version by Mike Hibler, this version by William  * Jolitz uses a recursive map [a pde points to the page directory] to  * map the page tables using the pagetables themselves. This is done to  * reduce the impact on kernel virtual memory for lots of sparse address  * space, and to reduce the cost of memory to each process.  *  *	from: hp300: @(#)pmap.h	7.2 (Berkeley) 12/16/90  *	from: @(#)pmap.h	7.4 (Berkeley) 5/12/91  * 	$Id: pmap.h,v 1.13 1994/03/25 22:08:59 davidg Exp $  */
 end_comment
 
 begin_ifndef
@@ -42,11 +42,16 @@ range|:
 literal|2
 decl_stmt|,
 comment|/* access control */
-name|pd_mbz1
+name|pd_ncpwt
 range|:
-literal|2
+literal|1
 decl_stmt|,
-comment|/* reserved, must be zero */
+comment|/* page cache write through */
+name|pd_ncpcd
+range|:
+literal|1
+decl_stmt|,
+comment|/* page cache disable */
 name|pd_u
 range|:
 literal|1
@@ -134,11 +139,16 @@ range|:
 literal|2
 decl_stmt|,
 comment|/* access control */
-name|pg_mbz1
+name|pg_ncpwt
 range|:
-literal|2
+literal|1
 decl_stmt|,
-comment|/* reserved, must be zero */
+comment|/* page cache write through */
+name|pg_ncpcd
+range|:
+literal|1
+decl_stmt|,
+comment|/* page cache disable */
 name|pg_u
 range|:
 literal|1
@@ -219,20 +229,42 @@ end_comment
 begin_define
 define|#
 directive|define
-name|PG_W
-value|0x00000200
+name|PG_NC_PWT
+value|0x00000008
 end_define
+
+begin_comment
+comment|/* page cache write through */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PG_NC_PCD
+value|0x00000010
+end_define
+
+begin_comment
+comment|/* page cache disable */
+end_comment
 
 begin_define
 define|#
 directive|define
 name|PG_N
-value|0x00000800
+value|0x00000018
 end_define
 
 begin_comment
 comment|/* Non-cacheable */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|PG_U
+value|0x00000020
+end_define
 
 begin_define
 define|#
@@ -244,8 +276,8 @@ end_define
 begin_define
 define|#
 directive|define
-name|PG_U
-value|0x00000020
+name|PG_W
+value|0x00000200
 end_define
 
 begin_define
