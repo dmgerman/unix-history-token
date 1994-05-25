79fe7e7eb812414eaf1152ea69140167857b0636
Andreas Schulz <ats@g386bsd.first.bmd.de>
1994-05-25 23:21:46 +0000

    prevent an integer divide in kernel mode if secsiz came back as zero from a get_params from a disk. Found by Donal.Tobin@cs.tcd.ie .
    Former-commit-id: cb82e1b6de09e6d26749962242bf0dc91b9f012d

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.22 1994/04/05 03:23:32 davidg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.23 1994/04/20 07:06:57 davidg Exp $  */
 end_comment
 
 begin_define
@@ -851,6 +851,21 @@ name|SCSI_NOSLEEP
 operator||
 name|SCSI_NOMASK
 argument_list|)
+expr_stmt|;
+comment|/* check if secsiz is zero and set it to something reasonable 	  to prevent a divide fault. */
+if|if
+condition|(
+name|dp
+operator|->
+name|secsiz
+operator|==
+literal|0
+condition|)
+name|dp
+operator|->
+name|secsiz
+operator|=
+literal|512
 expr_stmt|;
 name|printf
 argument_list|(
