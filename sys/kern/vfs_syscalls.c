15828acd94ddc16db92d34a68731e7bd744668f4
Andrey Chernov <ache@FreeBSD.org>
1994-05-26 05:32:05 +0000

    Remove rest of "trailing slashes" change
    Former-commit-id: f474e2f4d6ca6ab19cea659ec2b57c6e23b0296b

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vfs_syscalls.c	7.74 (Berkeley) 6/21/91  *	$Id: vfs_syscalls.c,v 1.15 1994/05/23 01:00:03 ache Exp $  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vfs_syscalls.c	7.74 (Berkeley) 6/21/91  *	$Id: vfs_syscalls.c,v 1.16 1994/05/26 04:46:47 ache Exp $  */
 end_comment
 
 begin_include
@@ -7853,27 +7853,48 @@ name|error
 operator|=
 name|EINVAL
 expr_stmt|;
-if|#
-directive|if
-literal|0
 comment|/* 	 * If source is the same as the destination (that is the 	 * same inode number with the same name in the same directory), 	 * then there is nothing to do. 	 */
-block|if (fvp == tvp&& fromnd.ni_dvp == tdvp&& 	    fromnd.ni_namelen == tond.ni_namelen&& 	    !bcmp(fromnd.ni_ptr, tond.ni_ptr, fromnd.ni_namelen)) 		error = -1;
-else|#
-directive|else
-comment|/* 	 * If the source is the same as the destination (that is, if they 	 * are links to the same vnode), then there is nothing to do. 	 * POSIX standard. 	 */
 if|if
 condition|(
 name|fvp
 operator|==
 name|tvp
+operator|&&
+name|fromnd
+operator|.
+name|ni_dvp
+operator|==
+name|tdvp
+operator|&&
+name|fromnd
+operator|.
+name|ni_namelen
+operator|==
+name|tond
+operator|.
+name|ni_namelen
+operator|&&
+operator|!
+name|bcmp
+argument_list|(
+name|fromnd
+operator|.
+name|ni_ptr
+argument_list|,
+name|tond
+operator|.
+name|ni_ptr
+argument_list|,
+name|fromnd
+operator|.
+name|ni_namelen
+argument_list|)
 condition|)
 name|error
 operator|=
 operator|-
 literal|1
 expr_stmt|;
-endif|#
-directive|endif
 name|out
 label|:
 if|if
