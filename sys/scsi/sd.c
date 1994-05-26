0368c1c5e86b1f22e73aac3b5751b0a0c4036683
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-05-26 00:23:24 +0000

    Argh, I am a bozo!  Use the right variable name in calculating the secsiz check, it might help! :-) [should have let the compile finish BEFORE committing the change, parallelism is not always optimal!]
    Former-commit-id: 428468c48f2440423f6f50e16e689d9ea4d5fc3e

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.24 1994/05/25 23:21:46 ats Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.25 1994/05/25 23:43:26 jkh Exp $  */
 end_comment
 
 begin_define
@@ -3662,13 +3662,13 @@ block|}
 comment|/* keep secsiz sane too - we may divide by it later */
 if|if
 condition|(
-name|dp
+name|disk_parms
 operator|->
 name|secsiz
 operator|==
 literal|0
 condition|)
-name|dp
+name|disk_parms
 operator|->
 name|secsiz
 operator|=
