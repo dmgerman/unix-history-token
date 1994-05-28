21c6747dd96320082cadc17520946a0293a3d31d
Rodney Grimes <rgrimes@FreeBSD.org>
1994-05-28 04:20:59 +0000

    Must now include vm/vm.h and vm/vm_param.h due to the way we define KERNBASE. Reviewed by:	David Greenman
    Former-commit-id: ba8ec442333508b8c86164a97f5761aab0a61ad2

--- usr.sbin/kvm_mkdb/nlist.c
+++ usr.sbin/kvm_mkdb/nlist.c
@@ -37,6 +37,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/vm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/vm_param.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<a.out.h>
 end_include
 
