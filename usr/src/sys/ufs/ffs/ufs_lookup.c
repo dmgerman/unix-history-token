23915a6464742030b807cac1d2d900aad7ab3f5c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-05-29 16:53:24 -0800

    don't check name of directory entries with zero'ed ino (from mycroft@sun-lamp.cs.berkeley.edu)
    SCCS-vsn: sys/ufs/ffs/ufs_lookup.c 8.7
    SCCS-vsn: sys/ufs/ufs/ufs_lookup.c 8.7
    
    Former-commit-id: 0ac7b8c4379a839632e2706ea39ac6510ed4c152

--- usr/src/sys/ufs/ffs/ufs_lookup.c
+++ usr/src/sys/ufs/ffs/ufs_lookup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_lookup.c	8.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_lookup.c	8.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2216,6 +2216,19 @@ goto|goto
 name|bad
 goto|;
 block|}
+if|if
+condition|(
+name|ep
+operator|->
+name|d_ino
+operator|==
+literal|0
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 for|for
 control|(
 name|i
