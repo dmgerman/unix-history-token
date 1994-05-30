17fd96d2b934d35d1a5445ec2f3e36150ee7df63
Andrey Chernov <ache@FreeBSD.org>
1994-05-30 03:33:37 +0000

    From Bde: 	o Removed decl of ttydefaults.  ttydefaults doesn't exist; we use 	  ttydefchars[] and ttychars(). 	o Split TS_ASLEEP into TS_SO_OLOWAT and TS_SO_OCOMPLETE. 	  TS_SO_OLOWAT means the same as TS_ASLSEEP did, but I want to 	  break anything using the old flags so that it gets fixed. 	o Fixed whitespace in CAN_DO_IO().  Please be careful not to 	  make regressive changes to whitespace. 	o Removed decls of tty sleep message strings. 	o Define sleep addresses centrally to ensure that they are unique. 	  Abbreviations: TSA = Tty Sleep Address.  TS_SO = Tty State Sleep 	  On.  We needed 2 new sleep addresses: old tp->t_out becomes 	  TSA_OCOMPLETE(tp) and TSA_OLOWAT; old tp->t_raw becomes 	  TSA_CARR_ON(tp) and TSA_HUP_OR_INPUT(tp).  Later we will need 	  TSA_MODEM_STATUS_CHANGE(tp) to handle RI, etc. 	o Declare new functions; fix old ones function; add struct decls 	  so that <sys/tty.h> is closer to being self-contained.
    Former-commit-id: 4b17f149f762464b5bf2f97e4746d98df4a053f5

--- sys/sys/tty.h
+++ sys/sys/tty.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty.h	7.10 (Berkeley) 6/26/91  *	$Id: tty.h,v 1.9 1994/03/02 20:29:03 guido Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)tty.h	7.10 (Berkeley) 6/26/91  *	$Id: tty.h,v 1.10 1994/03/23 01:58:30 ache Exp $  */
 end_comment
 
 begin_ifndef
@@ -397,14 +397,6 @@ name|TTMINLOWAT
 value|32
 end_define
 
-begin_decl_stmt
-specifier|extern
-name|struct
-name|ttychars
-name|ttydefaults
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
 endif|#
 directive|endif
@@ -427,17 +419,6 @@ end_define
 
 begin_comment
 comment|/* delay timeout in progress */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|TS_WOPEN
-value|0x000002UL
-end_define
-
-begin_comment
-comment|/* waiting for open to complete */
 end_comment
 
 begin_define
@@ -487,12 +468,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|TS_ASLEEP
+name|TS_SO_OLOWAT
 value|0x000040UL
 end_define
 
 begin_comment
-comment|/* wakeup when output done */
+comment|/* wake up when output<= low water */
 end_comment
 
 begin_define
@@ -559,6 +540,17 @@ end_define
 
 begin_comment
 comment|/* collision in write select */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TS_SO_OCOMPLETE
+value|0x002000UL
+end_define
+
+begin_comment
+comment|/* wake up when output complete */
 end_comment
 
 begin_define
@@ -886,39 +878,81 @@ name|DMGET
 value|3
 end_define
 
+begin_comment
+comment|/*  * Sleep addresses.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TSA_CARR_ON
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)(tp) + 0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|TSA_HUP_OR_INPUT
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)(tp) + 1)
+end_define
+
+begin_define
+define|#
+directive|define
+name|TSA_OCOMPLETE
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)(tp) + 2)
+end_define
+
+begin_define
+define|#
+directive|define
+name|TSA_OLOWAT
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)(tp) + 3)
+end_define
+
+begin_define
+define|#
+directive|define
+name|TSA_PTC_READ
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)(tp) + 4)
+end_define
+
+begin_define
+define|#
+directive|define
+name|TSA_PTC_WRITE
+parameter_list|(
+name|tp
+parameter_list|)
+value|((caddr_t)(tp) + 5)
+end_define
+
 begin_ifdef
 ifdef|#
 directive|ifdef
 name|KERNEL
 end_ifdef
 
-begin_comment
-comment|/* symbolic sleep message strings */
-end_comment
-
-begin_decl_stmt
-specifier|extern
-specifier|const
-name|char
-name|ttyin
-index|[]
-decl_stmt|,
-name|ttyout
-index|[]
-decl_stmt|,
-name|ttopen
-index|[]
-decl_stmt|,
-name|ttclos
-index|[]
-decl_stmt|,
-name|ttybg
-index|[]
-decl_stmt|,
-name|ttybuf
-index|[]
-decl_stmt|;
-end_decl_stmt
+begin_struct_decl
+struct_decl|struct
+name|proc
+struct_decl|;
+end_struct_decl
 
 begin_struct_decl
 struct_decl|struct
@@ -929,6 +963,18 @@ end_struct_decl
 begin_comment
 comment|/* From tty.c: */
 end_comment
+
+begin_function_decl
+specifier|extern
+name|void
+name|termioschars
+parameter_list|(
+name|struct
+name|termios
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_function_decl
 specifier|extern
@@ -945,7 +991,7 @@ end_function_decl
 begin_function_decl
 specifier|extern
 name|int
-name|ttwflush
+name|ttywflush
 parameter_list|(
 name|struct
 name|tty
@@ -1194,6 +1240,18 @@ begin_function_decl
 specifier|extern
 name|void
 name|ttwakeup
+parameter_list|(
+name|struct
+name|tty
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|void
+name|ttwwakeup
 parameter_list|(
 name|struct
 name|tty
