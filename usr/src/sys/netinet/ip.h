a9723a9f4de696d4d0dcdca8838fc9bf373f778d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-06-01 16:50:31 -0800

    IPTOS_PREC_ROUTINE should have a value of 0x00, not 0x10 (from honig)
    SCCS-vsn: sys/netinet/ip.h 8.2
    
    Former-commit-id: ce98d2d0e9aa82cbd9f005ae0d67eeb39ecbad51

--- usr/src/sys/netinet/ip.h
+++ usr/src/sys/netinet/ip.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ip.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ip.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -288,7 +288,7 @@ begin_define
 define|#
 directive|define
 name|IPTOS_PREC_ROUTINE
-value|0x10
+value|0x00
 end_define
 
 begin_comment
