267c17c8b8c96e0da8ceab1162b47f08da2fc43b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-06-05 00:04:30 -0800

    since size_t is unsigned, testing iov_len < 0 is futile
    SCCS-vsn: sys/kern/uipc_syscalls.c 8.5
    SCCS-vsn: sys/kern/sys_generic.c 8.6
    
    Former-commit-id: 0bb40ca6b30c3eb95bb7492ff55596fac8d5a04c

--- usr/src/sys/kern/uipc_syscalls.c
+++ usr/src/sys/kern/uipc_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  * %sccs.include.redist.c%  *  *	@(#)uipc_syscalls.c	8.4 (Berkeley) %G%  */
+comment|/*  *  * %sccs.include.redist.c%  *  *	@(#)uipc_syscalls.c	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
