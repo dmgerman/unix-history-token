92e82885ab70b6741b094a2a54e31c38f41ccbeb
Karl Lehenbauer <karl@NeoSoft.com>
1994-06-11 23:33:53 +0000

    Added an access check to the NFS server's create routine.  Without it, clients can zero out any file on the exported filesystem, just by creat(2)'ing it. Problem noticed using NFS on SunOS4.1.1.  This is fixed in BSD 4.4, and the fix is the same.
    Former-commit-id: c45d508e5ba266b45299d41184e64697ee29e882

--- sys/nfs/nfs_serv.c
+++ sys/nfs/nfs_serv.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	From:	@(#)nfs_serv.c	7.40 (Berkeley) 5/15/91  *	$Id: nfs_serv.c,v 1.4 1993/11/25 01:36:50 wollman Exp $  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	From:	@(#)nfs_serv.c	7.40 (Berkeley) 5/15/91  *	$Id: nfs_serv.c,v 1.5 1993/12/19 00:54:12 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -3731,7 +3731,6 @@ operator|)
 argument_list|)
 expr_stmt|;
 comment|/* 28 Aug 92*/
-comment|/* 08 Sep 92*/
 if|if
 condition|(
 name|vap
@@ -3740,21 +3739,22 @@ name|va_size
 operator|!=
 operator|-
 literal|1
-operator|&&
-operator|(
+condition|)
+block|{
+if|if
+condition|(
 name|error
 operator|=
-name|VOP_SETATTR
+name|nfsrv_access
 argument_list|(
 name|vp
 argument_list|,
-name|vap
+name|VWRITE
 argument_list|,
 name|cred
 argument_list|,
 name|p
 argument_list|)
-operator|)
 condition|)
 block|{
 name|vput
@@ -3767,6 +3767,34 @@ argument_list|(
 literal|0
 argument_list|)
 expr_stmt|;
+block|}
+if|if
+condition|(
+name|error
+operator|=
+name|VOP_SETATTR
+argument_list|(
+name|vp
+argument_list|,
+name|vap
+argument_list|,
+name|cred
+argument_list|,
+name|p
+argument_list|)
+condition|)
+block|{
+name|vput
+argument_list|(
+name|vp
+argument_list|)
+expr_stmt|;
+name|nfsm_reply
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 block|}
 name|bzero
