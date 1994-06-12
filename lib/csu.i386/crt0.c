7bec6fdba882502650cb70278cc57d29a7292056
Andrey Chernov <ache@FreeBSD.org>
1994-06-12 10:51:01 +0000

    Add (#ifdef'ed by STARTUP_LOCALE) following line to crt0.c
    	(void) setlocale(LC_ALL, "");
    
    It will be easiest way now to make national chars available
    for all ctype-oriented programs at once by simple
    
    setenv LANG Your_National_Charset
    (currently only Russian.koi8-r available)
    
    Default case (without "LANG" environment
    variable) will be fully ANSI compatible (got "C" locale).
    
    If "LANG" variable present, extention becomes active.
    
    Effect of this extention is great: in one time all ctype
    oriented programs can accept/print national characters
    without any touching source/binary code, it is big win, IMHO.
    
    This method is fully compatible with ISO8859-* and russian koi8-r
    too (in general -- with all 8-bit character sets). I think
    it is very useful.
    I got this idea from Xenix locale implementation.
    
    This extention is even never compiled in, unless you set
    	setenv STARTUP_LOCALE
    before rebuilding crt0.c
    
    
    Former-commit-id: f84c7538d7dfc5287bcd73b9beb5d30afaab305f

--- lib/csu.i386/crt0.c
+++ lib/csu.i386/crt0.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: crt0.c,v 1.8 1994/02/13 20:53:11 jkh Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: crt0.c,v 1.9 1994/02/16 19:26:39 nate Exp $  */
 end_comment
 
 begin_if
@@ -57,6 +57,27 @@ include|#
 directive|include
 file|<sys/param.h>
 end_include
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|STARTUP_LOCALE
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<locale.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* STARTUP_LOCALE */
+end_comment
 
 begin_ifdef
 ifdef|#
@@ -730,7 +751,23 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-endif|MCRT0
+comment|/* MCRT0 */
+ifdef|#
+directive|ifdef
+name|STARTUP_LOCALE
+operator|(
+name|void
+operator|)
+name|setlocale
+argument_list|(
+name|LC_ALL
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* STARTUP_LOCALE */
 asm|asm ("__callmain:");
 comment|/* Defined for the benefit of debuggers */
 name|exit
