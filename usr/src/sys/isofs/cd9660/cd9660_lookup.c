4ddda4b9403fa7ea33c74934ecee52f8d808d97e
Marshall Kirk McKusick <mkm@ucbvax.Berkeley.EDU>
1994-06-14 18:52:15 -0800

    formatting
    SCCS-vsn: sys/isofs/cd9660/cd9660_lookup.c 8.3
    
    Former-commit-id: 2a30521e78905337eda1fc754ddc683d1d85dd1a

--- usr/src/sys/isofs/cd9660/cd9660_lookup.c
+++ usr/src/sys/isofs/cd9660/cd9660_lookup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley  * by Pace Willisson (pace@blitz.com).  The Rock Ridge Extension  * Support code is derived from software contributed to Berkeley  * by Atsushi Murai (amurai@spec.co.jp).  *  * %sccs.include.redist.c%  *  *	from: @(#)ufs_lookup.c	7.33 (Berkeley) 5/19/91  *  *	@(#)cd9660_lookup.c	8.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1989, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley  * by Pace Willisson (pace@blitz.com).  The Rock Ridge Extension  * Support code is derived from software contributed to Berkeley  * by Atsushi Murai (amurai@spec.co.jp).  *  * %sccs.include.redist.c%  *  *	from: @(#)ufs_lookup.c	7.33 (Berkeley) 5/19/91  *  *	@(#)cd9660_lookup.c	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
