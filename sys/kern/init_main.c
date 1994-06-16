c3e0e6ed02744a5c1752625640c69c1869b5cc6c
David Greenman <davidg@Root.COM>
1994-06-16 02:55:28 +0000

    Disable pagescan daemon for now. It's an interesting idea, but consumes lots of CPU time on slow machines. The downside of disabling this is that compiles on 4MB machines will slow down by about 10% on fast CPU machines.
    Former-commit-id: ca5c1cfacf9f1585fc496e93942b6387e4ebc7d3

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)init_main.c	7.41 (Berkeley) 5/15/91  *	$Id: init_main.c,v 1.16 1994/04/14 07:50:06 davidg Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)init_main.c	7.41 (Berkeley) 5/15/91  *	$Id: init_main.c,v 1.17 1994/06/07 23:27:52 phk Exp $  */
 end_comment
 
 begin_include
@@ -1568,69 +1568,9 @@ comment|/*NOTREACHED*/
 block|}
 if|#
 directive|if
-literal|1
+literal|0
 comment|/* 	 * Start page scanner daemon (process 3). 	 */
-if|if
-condition|(
-name|fork
-argument_list|(
-name|p
-argument_list|,
-operator|(
-name|void
-operator|*
-operator|)
-name|NULL
-argument_list|,
-name|rval
-argument_list|)
-condition|)
-name|panic
-argument_list|(
-literal|"failed fork page scanner daemon"
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|rval
-index|[
-literal|1
-index|]
-condition|)
-block|{
-name|p
-operator|=
-name|curproc
-expr_stmt|;
-name|pagescanproc
-operator|=
-name|p
-expr_stmt|;
-name|p
-operator|->
-name|p_flag
-operator||=
-name|SLOAD
-operator||
-name|SSYS
-expr_stmt|;
-name|bcopy
-argument_list|(
-literal|"pagescan"
-argument_list|,
-name|p
-operator|->
-name|p_comm
-argument_list|,
-sizeof|sizeof
-argument_list|(
-literal|"pagescan"
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|vm_pagescan
-argument_list|()
-expr_stmt|;
+block|if (fork(p, (void *) NULL, rval)) 		panic("failed fork page scanner daemon"); 	if (rval[1]) { 		p = curproc; 		pagescanproc = p; 		p->p_flag |= SLOAD|SSYS; 		bcopy("pagescan", p->p_comm, sizeof("pagescan")); 		vm_pagescan();
 comment|/*NOTREACHED*/
 block|}
 endif|#
