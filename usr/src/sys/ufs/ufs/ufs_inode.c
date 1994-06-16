6022c335ec9875d01540f8e3bd8b0a176c0c7dba
Marshall Kirk McKusick <mkm@ucbvax.Berkeley.EDU>
1994-06-15 23:26:41 -0800

    dqinit needs to be ifdef QUOTA
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 8.5
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 8.5
    
    Former-commit-id: 0c83aec8e1d1d9632fe9f3b8c323480d4b8196b4

--- usr/src/sys/ufs/ufs/ufs_inode.c
+++ usr/src/sys/ufs/ufs/ufs_inode.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_inode.c	8.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_inode.c	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -153,9 +153,14 @@ directive|endif
 name|ufs_ihashinit
 argument_list|()
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|QUOTA
 name|dqinit
 argument_list|()
 expr_stmt|;
+endif|#
+directive|endif
 return|return
 operator|(
 literal|0
