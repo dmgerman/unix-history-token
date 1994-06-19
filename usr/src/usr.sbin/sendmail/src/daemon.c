47a215fa0e3e5a7dbd20c7b45168e770b7627e27
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-06-18 16:46:55 -0800

    fall back to gethostbyname even if DNS lookup fails
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.55
    
    Former-commit-id: c36c23d2532dae04aa138b52868680fae33b3fa0

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.54 (Berkeley) %G% (with daemon mode)"
+literal|"@(#)daemon.c	8.55 (Berkeley) %G% (with daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -54,7 +54,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.54 (Berkeley) %G% (without daemon mode)"
+literal|"@(#)daemon.c	8.55 (Berkeley) %G% (without daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -3173,10 +3173,17 @@ name|statp
 expr_stmt|;
 if|if
 condition|(
+operator|(
 operator|*
 name|statp
 operator|!=
 name|EX_TEMPFAIL
+operator|&&
+operator|*
+name|statp
+operator|!=
+name|EX_NOHOST
+operator|)
 operator|||
 name|UseNameServer
 condition|)
