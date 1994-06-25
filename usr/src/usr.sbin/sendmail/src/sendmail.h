37c378dbd00e917fbbb06f017469e920fc532e5b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-06-24 19:26:52 -0800

    add MF_UNSAFEDB flag for future use
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.44
    
    Former-commit-id: 71bbfe176564f1a0b7aa72a4efd4cb76c13cd049

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.43 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.44 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.43		%G%"
+literal|"@(#)sendmail.h	8.44		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2672,6 +2672,17 @@ end_define
 
 begin_comment
 comment|/* implicit: underlying NDBM database */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MF_UNSAFEDB
+value|0x4000
+end_define
+
+begin_comment
+comment|/* this map is world writable */
 end_comment
 
 begin_comment
