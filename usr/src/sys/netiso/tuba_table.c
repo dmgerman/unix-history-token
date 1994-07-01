55bdb0a9f7b49fef7355a9bb369b491e5c552ffb
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-07-01 02:08:44 -0800

    bug fix from Doug Montgomery at NIST.  Forgot to compensate for changes to rn_match conventions.
    SCCS-vsn: sys/netiso/tuba_table.c 8.3
    
    Former-commit-id: e85bbbfdebf8f15d1ba7ba4e6c632b48ab9c86be

--- usr/src/sys/netiso/tuba_table.c
+++ usr/src/sys/netiso/tuba_table.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tuba_table.c	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tuba_table.c	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -359,8 +359,6 @@ operator|->
 name|siso_addr
 argument_list|,
 name|tuba_tree
-operator|->
-name|rnh_treetop
 argument_list|)
 operator|)
 operator|&&
