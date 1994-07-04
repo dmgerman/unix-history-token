5f7f48b4e1bdc9d6af4303dad234e7aceec69047
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-07-03 21:45:35 -0800

    handle SCSI II devices (from Ralph)
    SCCS-vsn: sys/pmax/dev/rz.c 8.2
    
    Former-commit-id: 7332c8f61aa8c3311854b7361020059c4c819d7e

--- usr/src/sys/pmax/dev/rz.c
+++ usr/src/sys/pmax/dev/rz.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Van Jacobson of Lawrence Berkeley Laboratory and Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)rz.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Van Jacobson of Lawrence Berkeley Laboratory and Ralph Campbell.  *  * %sccs.include.redist.c%  *  *	@(#)rz.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2005,7 +2005,7 @@ name|inqbuf
 operator|.
 name|version
 operator|>
-literal|1
+literal|2
 operator|||
 name|i
 operator|<
