507f1cb0ccb74d304dd4ed6ea1f445ad7f8bd9f3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-07-03 21:51:20 -0800

    minor debug checking code (from Ralph)
    SCCS-vsn: sys/pmax/pmax/trap.c 8.6
    SCCS-vsn: sys/pmax/pmax/locore.s 8.6
    
    Former-commit-id: 09729851b2e2ccb703d98cdede9846e1799d444e

--- usr/src/sys/pmax/pmax/trap.c
+++ usr/src/sys/pmax/pmax/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.c 1.32 91/04/06$  *  *	@(#)trap.c	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: trap.c 1.32 91/04/06$  *  *	@(#)trap.c	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -459,6 +459,16 @@ init|=
 name|trapdebug
 struct|;
 end_struct
+
+begin_decl_stmt
+name|u_int
+name|intr_level
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* number of nested interrupts */
+end_comment
 
 begin_endif
 endif|#
@@ -3721,6 +3731,9 @@ name|trp
 operator|=
 name|trapdebug
 expr_stmt|;
+name|intr_level
+operator|++
+expr_stmt|;
 endif|#
 directive|endif
 name|cnt
@@ -3964,6 +3977,14 @@ block|}
 endif|#
 directive|endif
 block|}
+ifdef|#
+directive|ifdef
+name|DEBUG
+name|intr_level
+operator|--
+expr_stmt|;
+endif|#
+directive|endif
 block|}
 end_block
 
