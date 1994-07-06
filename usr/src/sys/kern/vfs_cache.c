08a962255980754c6ab69fd73791a0c5e5dade38
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-07-05 22:12:59 -0800

    turn off MAKEENTRY flag when doingcache is disabled (from cgd)
    SCCS-vsn: sys/kern/vfs_cache.c 8.2
    
    Former-commit-id: b3153b167b5326403ead0f5c94583a6ff08b0deb

--- usr/src/sys/kern/vfs_cache.c
+++ usr/src/sys/kern/vfs_cache.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_cache.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_cache.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -176,11 +176,20 @@ condition|(
 operator|!
 name|doingcache
 condition|)
+block|{
+name|cnp
+operator|->
+name|cn_flags
+operator|&=
+operator|~
+name|MAKEENTRY
+expr_stmt|;
 return|return
 operator|(
 literal|0
 operator|)
 return|;
+block|}
 if|if
 condition|(
 name|cnp
