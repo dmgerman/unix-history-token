9c1c346464f6e132546f29cfb671ee065641e785
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-07-11 05:59:11 -0800

    fix comment
    SCCS-vsn: sys/sys/wait.h 8.2
    
    Former-commit-id: adcc0d4bddcbcc0953a89414efa4189228568dea

--- usr/src/sys/sys/wait.h
+++ usr/src/sys/sys/wait.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)wait.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)wait.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -194,7 +194,7 @@ value|1
 end_define
 
 begin_comment
-comment|/* dont hang in wait */
+comment|/* don't hang in wait */
 end_comment
 
 begin_define
