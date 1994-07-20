af1a4f179765141fe0b4894b3715e4e1bc403f63
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-07-19 17:36:07 -0800

    isop_laddr is a pointer to a sockaddr_iso, not the thing itself; change line about 231 to isop->laddr->siso_tlen
    SCCS-vsn: sys/netiso/iso_pcb.c 8.3
    
    Former-commit-id: e4f62df5243a107ef87d40001076641c9dbbdbe2

--- usr/src/sys/netiso/iso_pcb.c
+++ usr/src/sys/netiso/iso_pcb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)iso_pcb.c	8.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)iso_pcb.c	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -796,7 +796,7 @@ expr_stmt|;
 name|isop
 operator|->
 name|isop_laddr
-operator|.
+operator|->
 name|siso_tlen
 operator|=
 literal|2
