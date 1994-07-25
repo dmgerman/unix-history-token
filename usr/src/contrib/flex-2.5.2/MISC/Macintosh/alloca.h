34cd2c31fd2aae81d92ef133d45945d1ede0f3b3
CSRG <csrg@ucbvax.Berkeley.EDU>
1994-07-25 00:16:44 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/flex-2.5.2/MISC/Macintosh/alloca.h Work on file usr/src/contrib/flex-2.5.2/MISC/Macintosh/xmalloc.c
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2
    
    Former-commit-id: 2bfbd151b63c9489aa72a81c39d0487eee34d066

---
+++ usr/src/contrib/flex-2.5.2/MISC/Macintosh/alloca.h
@@ -0,0 +1,25 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/**************** ** alloca.h ** ** header for alloca() *****************/
+end_comment
+
+begin_typedef
+typedef|typedef
+name|void
+modifier|*
+name|pointer
+typedef|;
+end_typedef
+
+begin_function_decl
+name|pointer
+name|alloca
+parameter_list|(
+name|unsigned
+name|size
+parameter_list|)
+function_decl|;
+end_function_decl
+
+end_unit
+
