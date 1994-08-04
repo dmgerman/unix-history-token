a48993862a165c4b5bcb388a7c9358f83e191e59
David Greenman <dg@FreeBSD.org>
1994-08-04 19:43:13 +0000

    Fixed bug that would cause free memory reserves to be depleted and cause a panic in some cases. Submitted by:	John Dyson
    Former-commit-id: b5176b60c89b7f4b26cef2872fa619a737c89f58

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id$  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.4 1994/08/02 07:43:13 davidg Exp $  */
 end_comment
 
 begin_include
@@ -3088,6 +3088,23 @@ name|pa
 decl_stmt|;
 name|tryagain
 label|:
+if|if
+condition|(
+name|cnt
+operator|.
+name|v_free_count
+operator|<=
+name|cnt
+operator|.
+name|v_free_reserved
+condition|)
+block|{
+name|VM_WAIT
+expr_stmt|;
+goto|goto
+name|tryagain
+goto|;
+block|}
 name|p
 operator|=
 name|vm_page_alloc
