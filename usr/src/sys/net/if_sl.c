bf5a1457d545c3087798a51e892aa9fe5d13a270
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-08-05 23:44:39 -0800

    delete redundant declaration of time (from cgd)
    SCCS-vsn: sys/net/if_sl.c 8.7
    
    Former-commit-id: 0a97bc1352eba7fb6dd11f131f90643aeca48a19

--- usr/src/sys/net/if_sl.c
+++ usr/src/sys/net/if_sl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1989, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_sl.c	8.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1987, 1989, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)if_sl.c	8.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -301,14 +301,6 @@ end_define
 begin_comment
 comment|/* transposed frame esc */
 end_comment
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|timeval
-name|time
-decl_stmt|;
-end_decl_stmt
 
 begin_decl_stmt
 specifier|static
