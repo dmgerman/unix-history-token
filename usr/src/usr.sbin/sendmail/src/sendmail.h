25dc55b4042eeff995bb9d3498d1dcfa2a099477
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-07 17:25:01 -0800

    fix typo
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.49
    
    Former-commit-id: ebecdab5411a054395fb4029cd70cb8e00f398cc

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.48 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.49 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.48		%G%"
+literal|"@(#)sendmail.h	8.49		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1012,7 +1012,7 @@ end_comment
 begin_define
 define|#
 directive|define
-name|M_SPECFIC_UID
+name|M_SPECIFIC_UID
 value|'S'
 end_define
 
