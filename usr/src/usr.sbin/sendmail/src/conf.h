049fb8334ad7ceae056d1a3e0993c89481895fb0
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-07 19:06:41 -0800

    defaults for TZ_TYPE -- this will probably require some tuning
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.114
    
    Former-commit-id: 9e321d483f76d4282953b9b0c1946b25886672ba

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.113 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.114 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2679,6 +2679,17 @@ begin_comment
 comment|/* use<sys/statfs.h> statfs() impl */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|TZ_TYPE
+value|TZ_USE_TM_NAME
+end_define
+
+begin_comment
+comment|/* use tm->tm_name */
+end_comment
+
 begin_undef
 undef|#
 directive|undef
@@ -4674,6 +4685,28 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|TZ_TYPE
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|TZ_TYPE
+value|TZ_USE_TM_ZONE
+end_define
+
+begin_comment
+comment|/* use tm->tm_zone variable */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_endif
 endif|#
 directive|endif
@@ -4944,6 +4977,28 @@ end_define
 
 begin_comment
 comment|/* use System V ustat(2) syscall */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|TZ_TYPE
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|TZ_TYPE
+value|TZ_USE_TZNAME
+end_define
+
+begin_comment
+comment|/* use tzname[] vector */
 end_comment
 
 begin_endif
