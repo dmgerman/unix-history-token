734d1462c51dcc3c055c8f1838ac7977c3bbe775
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-07 22:45:20 -0800

    BrokenSmtpPeers is now an option
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.51
    
    Former-commit-id: 560b6dc263ca4364ba11af41a23dd000e7e41c2a

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.50 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.51 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.50		%G%"
+literal|"@(#)sendmail.h	8.51		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -4892,7 +4892,7 @@ comment|/* don't prune source routes */
 end_comment
 
 begin_decl_stmt
-specifier|extern
+name|EXTERN
 name|bool
 name|BrokenSmtpPeers
 decl_stmt|;
