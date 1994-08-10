3ee2e55932092c14242fcc57e315b83819f8ffbd
David Greenman <dg@FreeBSD.org>
1994-08-10 03:09:37 +0000

    Fixed vm_page_deactivate to deal with getting called with a page that's not on any queue. This is an old patchkit days fix.
    Reviewed by:	John Dyson and David Greenman
    Submitted by:	originally by Paul Mackerras
    
    
    Former-commit-id: 7a593c00565d10510ccf3445780e2f6890623fa9

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91  *	$Id: vm_page.c,v 1.3 1994/08/01 11:25:44 davidg Exp $  */
+comment|/*   * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91  *	$Id: vm_page.c,v 1.4 1994/08/07 13:10:41 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -2169,58 +2169,6 @@ expr_stmt|;
 block|}
 end_function
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
-begin_comment
-comment|/*  *	vm_page_deactivate:  *  *	Returns the given page to the inactive list,  *	indicating that no physical maps have access  *	to this page.  [Used by the physical mapping system.]  *  *	The page queues must be locked.  */
-end_comment
-
-begin_comment
-unit|void vm_page_deactivate(m) 	register vm_page_t	m; { 	int spl; 	VM_PAGE_CHECK(m);
-comment|/* 	 *	Only move active pages -- ignore locked or already 	 *	inactive ones. 	 * 	 *	XXX: sometimes we get pages which aren't wired down 	 *	or on any queue - we need to put them on the inactive 	 *	queue also, otherwise we lose track of them. 	 *	Paul Mackerras (paulus@cs.anu.edu.au) 9-Jan-93. 	 */
-end_comment
-
-begin_define
-unit|spl = splhigh(); 	if (!(m->flags& PG_INACTIVE)&& m->wire_count == 0&& 		m->hold_count == 0) {  		pmap_clear_reference(VM_PAGE_TO_PHYS(m)); 		if (m->flags& PG_ACTIVE) { 			TAILQ_REMOVE(&vm_page_queue_active, m, pageq); 			m->flags&= ~PG_ACTIVE; 			cnt.v_active_count--; 		} 		TAILQ_INSERT_TAIL(&vm_page_queue_inactive, m, pageq); 		m->flags |= PG_INACTIVE; 		cnt.v_inactive_count++;
-define|#
-directive|define
-name|NOT_DEACTIVATE_PROTECTS
-end_define
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|NOT_DEACTIVATE_PROTECTS
-end_ifndef
-
-begin_else
-unit|pmap_page_protect(VM_PAGE_TO_PHYS(m), VM_PROT_NONE);
-else|#
-directive|else
-end_else
-
-begin_endif
-unit|if ((m->flags& PG_CLEAN)&& 			pmap_is_modified(VM_PAGE_TO_PHYS(m))) 			m->flags&= ~PG_CLEAN;
-endif|#
-directive|endif
-end_endif
-
-begin_endif
-unit|if ((m->flags& PG_CLEAN) == 0) 			m->flags |= PG_LAUNDRY; 	}  	splx(spl); }
-endif|#
-directive|endif
-end_endif
-
-begin_if
-if|#
-directive|if
-literal|1
-end_if
-
 begin_comment
 comment|/*  *	vm_page_deactivate:  *  *	Returns the given page to the inactive list,  *	indicating that no physical maps have access  *	to this page.  [Used by the physical mapping system.]  *  *	The page queues must be locked.  */
 end_comment
@@ -2237,36 +2185,41 @@ name|m
 decl_stmt|;
 block|{
 name|int
-name|s
+name|spl
 decl_stmt|;
 name|VM_PAGE_CHECK
 argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
-name|s
+comment|/* 	 *	Only move active pages -- ignore locked or already 	 *	inactive ones. 	 * 	 *	XXX: sometimes we get pages which aren't wired down 	 *	or on any queue - we need to put them on the inactive 	 *	queue also, otherwise we lose track of them. 	 *	Paul Mackerras (paulus@cs.anu.edu.au) 9-Jan-93. 	 */
+name|spl
 operator|=
 name|splhigh
 argument_list|()
 expr_stmt|;
-comment|/* 	 *	Only move active pages -- ignore locked or already 	 *	inactive ones. 	 */
 if|if
 condition|(
+operator|!
 operator|(
 name|m
 operator|->
 name|flags
 operator|&
-name|PG_ACTIVE
+name|PG_INACTIVE
 operator|)
 operator|&&
-operator|(
+name|m
+operator|->
+name|wire_count
+operator|==
+literal|0
+operator|&&
 name|m
 operator|->
 name|hold_count
 operator|==
 literal|0
-operator|)
 condition|)
 block|{
 name|pmap_clear_reference
@@ -2277,20 +2230,19 @@ name|m
 argument_list|)
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|m
+operator|->
+name|flags
+operator|&
+name|PG_ACTIVE
+condition|)
+block|{
 name|TAILQ_REMOVE
 argument_list|(
 operator|&
 name|vm_page_queue_active
-argument_list|,
-name|m
-argument_list|,
-name|pageq
-argument_list|)
-expr_stmt|;
-name|TAILQ_INSERT_TAIL
-argument_list|(
-operator|&
-name|vm_page_queue_inactive
 argument_list|,
 name|m
 argument_list|,
@@ -2304,16 +2256,27 @@ operator|&=
 operator|~
 name|PG_ACTIVE
 expr_stmt|;
+name|cnt
+operator|.
+name|v_active_count
+operator|--
+expr_stmt|;
+block|}
+name|TAILQ_INSERT_TAIL
+argument_list|(
+operator|&
+name|vm_page_queue_inactive
+argument_list|,
+name|m
+argument_list|,
+name|pageq
+argument_list|)
+expr_stmt|;
 name|m
 operator|->
 name|flags
 operator||=
 name|PG_INACTIVE
-expr_stmt|;
-name|cnt
-operator|.
-name|v_active_count
-operator|--
 expr_stmt|;
 name|cnt
 operator|.
@@ -2340,6 +2303,14 @@ else|#
 directive|else
 if|if
 condition|(
+operator|(
+name|m
+operator|->
+name|flags
+operator|&
+name|PG_CLEAN
+operator|)
+operator|&&
 name|pmap_is_modified
 argument_list|(
 name|VM_PAGE_TO_PHYS
@@ -2359,20 +2330,16 @@ endif|#
 directive|endif
 if|if
 condition|(
+operator|(
 name|m
 operator|->
 name|flags
 operator|&
 name|PG_CLEAN
+operator|)
+operator|==
+literal|0
 condition|)
-name|m
-operator|->
-name|flags
-operator|&=
-operator|~
-name|PG_LAUNDRY
-expr_stmt|;
-else|else
 name|m
 operator|->
 name|flags
@@ -2382,16 +2349,11 @@ expr_stmt|;
 block|}
 name|splx
 argument_list|(
-name|s
+name|spl
 argument_list|)
 expr_stmt|;
 block|}
 end_function
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/*  *	vm_page_activate:  *  *	Put the specified page on the active list (if appropriate).  *  *	The page queues must be locked.  */
