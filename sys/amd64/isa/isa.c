995f9f5fd70b713f7d576c7d6569b3b054a4c592
Garrett Wollman <wollman@FreeBSD.org>
1994-08-10 04:39:52 +0000

    Handle NMI's in accordance with data in van Gilluwe book.
    Former-commit-id: 404f0a98e8644ba2e06bb235bbea2cdf1483f155

--- sys/amd64/isa/isa.c
+++ sys/amd64/isa/isa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: isa.c,v 1.16 1994/04/02 20:43:25 ache Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: isa.c,v 1.18 1994/05/25 08:59:24 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -2744,6 +2744,41 @@ block|}
 block|}
 end_function
 
+begin_define
+define|#
+directive|define
+name|NMI_PARITY
+value|(1<< 7)
+end_define
+
+begin_define
+define|#
+directive|define
+name|NMI_IOCHAN
+value|(1<< 6)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENMI_WATCHDOG
+value|(1<< 7)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENMI_BUSTIMER
+value|(1<< 6)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENMI_IOSTATUS
+value|(1<< 5)
+end_define
+
 begin_comment
 comment|/*  * Handle a NMI, possibly a machine check.  * return true to panic system, false to ignore.  */
 end_comment
@@ -2758,21 +2793,100 @@ name|int
 name|cd
 decl_stmt|;
 block|{
-name|log
-argument_list|(
-name|LOG_CRIT
-argument_list|,
-literal|"\nNMI port 61 %x, port 70 %x\n"
-argument_list|,
+name|int
+name|isa_port
+init|=
 name|inb
 argument_list|(
 literal|0x61
 argument_list|)
-argument_list|,
+decl_stmt|;
+name|int
+name|eisa_port
+init|=
 name|inb
 argument_list|(
-literal|0x70
+literal|0x461
 argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|isa_port
+operator|&
+name|NMI_PARITY
+condition|)
+block|{
+name|panic
+argument_list|(
+literal|"RAM parity error, likely hardware failure."
+argument_list|)
+expr_stmt|;
+block|}
+elseif|else
+if|if
+condition|(
+name|isa_port
+operator|&
+name|NMI_IOCHAN
+condition|)
+block|{
+name|panic
+argument_list|(
+literal|"I/O channel check, likely hardware failure."
+argument_list|)
+expr_stmt|;
+block|}
+elseif|else
+if|if
+condition|(
+name|eisa_port
+operator|&
+name|ENMI_WATCHDOG
+condition|)
+block|{
+name|panic
+argument_list|(
+literal|"EISA watchdog timer expired, likely hardware failure."
+argument_list|)
+expr_stmt|;
+block|}
+elseif|else
+if|if
+condition|(
+name|eisa_port
+operator|&
+name|ENMI_BUSTIMER
+condition|)
+block|{
+name|panic
+argument_list|(
+literal|"EISA bus timeout, likely hardware failure."
+argument_list|)
+expr_stmt|;
+block|}
+elseif|else
+if|if
+condition|(
+name|eisa_port
+operator|&
+name|ENMI_IOSTATUS
+condition|)
+block|{
+name|panic
+argument_list|(
+literal|"EISA I/O port status error."
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|printf
+argument_list|(
+literal|"\nNMI ISA %x, EISA %x\n"
+argument_list|,
+name|isa_port
+argument_list|,
+name|eisa_port
 argument_list|)
 expr_stmt|;
 return|return
@@ -2780,6 +2894,7 @@ operator|(
 literal|0
 operator|)
 return|;
+block|}
 block|}
 end_function
 
