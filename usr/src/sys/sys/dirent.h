f6ff2e27e071b2c7dd02a005d3efed23b041002f
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-08-11 06:17:35 -0800

    add whiteout entry type
    SCCS-vsn: sys/sys/dirent.h 8.3
    
    Former-commit-id: 1e897e1b849c4a8e1f51c0ba73cd95876aa7a051

--- usr/src/sys/sys/dirent.h
+++ usr/src/sys/sys/dirent.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dirent.h	8.2 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)dirent.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -118,6 +118,13 @@ define|#
 directive|define
 name|DT_SOCK
 value|12
+end_define
+
+begin_define
+define|#
+directive|define
+name|DT_WHT
+value|14
 end_define
 
 begin_comment
