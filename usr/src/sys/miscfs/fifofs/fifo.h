873474d50acf242e062f7eb5fb49abd597bf25ef
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-08-11 06:41:14 -0800

    add fifo_lease_check
    SCCS-vsn: sys/miscfs/fifofs/fifo.h 8.3
    
    Former-commit-id: 025cc87e97bee5abbbc76360c94d54345477573c

--- usr/src/sys/miscfs/fifofs/fifo.h
+++ usr/src/sys/miscfs/fifofs/fifo.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -127,6 +127,13 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|fifo_lease_check
+value|((int (*) __P((struct  vop_lease_args *)))nullop)
+end_define
 
 begin_decl_stmt
 name|int
