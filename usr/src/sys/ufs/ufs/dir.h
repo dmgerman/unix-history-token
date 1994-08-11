ed6eb19de84faca2e7a1e5b9888382c4dc24fa77
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-08-11 06:54:05 -0800

    add whiteout type
    SCCS-vsn: sys/ufs/ufs/dir.h 8.4
    
    Former-commit-id: baee2d247b8543fab1dfd85e006b7d66939ca200

--- usr/src/sys/ufs/ufs/dir.h
+++ usr/src/sys/ufs/ufs/dir.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dir.h	8.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dir.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -124,6 +124,13 @@ define|#
 directive|define
 name|DT_SOCK
 value|12
+end_define
+
+begin_define
+define|#
+directive|define
+name|DT_WHT
+value|14
 end_define
 
 begin_comment
