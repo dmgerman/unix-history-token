6243ca991b2b5bb088283fe001003e9b8e3d0f60
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-08-11 06:38:30 -0800

    add entries for leases
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 8.10
    
    Former-commit-id: d6f371daba8530485003b0cf96d99b05c50215ca

--- usr/src/sys/ufs/ffs/ffs_vnops.c
+++ usr/src/sys/ufs/ffs/ffs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	8.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	8.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -252,6 +252,14 @@ name|ffs_write
 block|}
 block|,
 comment|/* write */
+block|{
+operator|&
+name|vop_lease_desc
+block|,
+name|ufs_lease_check
+block|}
+block|,
+comment|/* lease */
 block|{
 operator|&
 name|vop_ioctl_desc
@@ -639,6 +647,14 @@ name|ufsspec_write
 block|}
 block|,
 comment|/* write */
+block|{
+operator|&
+name|vop_lease_desc
+block|,
+name|spec_lease_check
+block|}
+block|,
+comment|/* lease */
 block|{
 operator|&
 name|vop_ioctl_desc
@@ -1032,6 +1048,14 @@ name|ufsfifo_write
 block|}
 block|,
 comment|/* write */
+block|{
+operator|&
+name|vop_lease_desc
+block|,
+name|fifo_lease_check
+block|}
+block|,
+comment|/* lease */
 block|{
 operator|&
 name|vop_ioctl_desc
