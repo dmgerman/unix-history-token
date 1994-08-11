ae285cf31d9b4c647f56449b771d3927e96247c3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-08-11 06:42:19 -0800

    add spec_lease_check
    SCCS-vsn: sys/miscfs/specfs/specdev.h 8.3
    
    Former-commit-id: cc5fea87df2a60722b6f8269bdba05cdcc5956ab

--- usr/src/sys/miscfs/specfs/specdev.h
+++ usr/src/sys/miscfs/specfs/specdev.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)specdev.h	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)specdev.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -315,6 +315,13 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|spec_lease_check
+value|((int (*) __P((struct  vop_lease_args *)))nullop)
+end_define
 
 begin_decl_stmt
 name|int
