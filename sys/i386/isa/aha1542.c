e3cbab54619b67de632b07b96e0f381bf70cefe8
Andreas Schulz <ats@FreeBSD.org>
1994-08-14 21:06:00 +0000

    Reviewed by: Added code to check for an adaptec 1542B Version 3.20 Board. This was the first board that supports >1Gb drives and has the extended bios. So we need also to disable the exbios like it is done for the 1542C/CF boards.
    Former-commit-id: ac7235c4cf83aa15d6873be9cff336c483de9779

--- sys/i386/isa/aha1542.c
+++ sys/i386/isa/aha1542.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.27 1994/05/25 08:57:57 rgrimes Exp $  */
+comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.28 1994/08/13 03:49:52 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -3796,7 +3796,7 @@ expr_stmt|;
 endif|#
 directive|endif
 comment|/* AHADEBUG */
-comment|/* 	 * If we are a 1542C or 1542CF disable the extended bios so that the 	 * mailbox interface is unlocked. 	 * No need to check the extended bios flags as some of the 	 * extensions that cause us problems are not flagged in that byte. 	 */
+comment|/* 	 * If we are a 1542C or 1542CF disable the extended bios so that the 	 * mailbox interface is unlocked. 	 * This is also true for the 1542B Version 3.20. First Adaptec 	 * board that supports>1Gb drives. 	 * No need to check the extended bios flags as some of the 	 * extensions that cause us problems are not flagged in that byte. 	 */
 if|if
 condition|(
 operator|(
@@ -3821,6 +3821,26 @@ operator|.
 name|boardid
 operator|==
 literal|0x45
+operator|)
+operator|||
+operator|(
+name|inquire
+operator|.
+name|boardid
+operator|==
+literal|0x41
+operator|&&
+name|inquire
+operator|.
+name|revision_1
+operator|==
+literal|0x31
+operator|&&
+name|inquire
+operator|.
+name|revision_2
+operator|==
+literal|0x34
 operator|)
 condition|)
 block|{
