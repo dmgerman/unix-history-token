2aac901ebe5ca59e10a6dc6e952b5ffe0b8b8188
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-08-15 04:56:41 -0800

    cannot get new controlling tty when you already have one
    SCCS-vsn: sys/kern/tty.c 8.10
    
    Former-commit-id: 21ea5d5a320bc4e79e795f70dcdc2e66216060e0

--- usr/src/sys/kern/tty.c
+++ usr/src/sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)tty.c	8.9 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)tty.c	8.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -4250,19 +4250,17 @@ argument_list|(
 name|p
 argument_list|)
 operator|||
-operator|(
 name|p
 operator|->
 name|p_session
 operator|->
 name|s_ttyvp
 operator|||
+operator|(
 name|tp
 operator|->
 name|t_session
-operator|)
 operator|&&
-operator|(
 name|tp
 operator|->
 name|t_session
