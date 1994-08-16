35b79636394529f6f96d1c8e165b09c555836be8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-08-15 18:59:19 -0800

    release the old controlling terminal if setting a new one
    SCCS-vsn: sys/kern/vfs_vnops.c 8.4
    
    Former-commit-id: 884560ac1feb7ef88b6e7cf589923168ba69c22c

--- usr/src/sys/kern/vfs_vnops.c
+++ usr/src/sys/kern/vfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1895,6 +1895,23 @@ operator|==
 name|TIOCSCTTY
 condition|)
 block|{
+if|if
+condition|(
+name|p
+operator|->
+name|p_session
+operator|->
+name|s_ttyvp
+condition|)
+name|vrele
+argument_list|(
+name|p
+operator|->
+name|p_session
+operator|->
+name|s_ttyvp
+argument_list|)
+expr_stmt|;
 name|p
 operator|->
 name|p_session
