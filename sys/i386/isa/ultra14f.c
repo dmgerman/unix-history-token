059776e121f3c34ac49709a206024778d6c61c60
Poul-Henning Kamp <phk@FreeBSD.org>
1994-08-18 23:36:40 +0000

    Added {} around two initializers for spare[2] in some structs.  This calms down -Wall a little bit.
    Former-commit-id: e820a34e2dfafe13c6a05968db9837955cad68be

--- sys/i386/isa/ultra14f.c
+++ sys/i386/isa/ultra14f.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Ported for use with the UltraStor 14f by Gary Close (gclose@wvnvms.wvnet.edu)  * Slight fixes to timeouts to run with the 34F  * Thanks to Julian Elischer for advice and help with this port.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  * slight mod to make work with 34F as well: Wed Jun  2 18:05:48 WST 1993  *  *      $Id: ultra14f.c,v 1.19 1994/05/27 04:02:08 rgrimes Exp $  */
+comment|/*  * Ported for use with the UltraStor 14f by Gary Close (gclose@wvnvms.wvnet.edu)  * Slight fixes to timeouts to run with the 34F  * Thanks to Julian Elischer for advice and help with this port.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  * slight mod to make work with 34F as well: Wed Jun  2 18:05:48 WST 1993  *  *      $Id: ultra14f.c,v 1.20 1994/08/13 03:50:17 wollman Exp $  */
 end_comment
 
 begin_include
@@ -1336,9 +1336,11 @@ name|uha_adapter_info
 block|,
 literal|"uha"
 block|,
+block|{
 literal|0
 block|,
 literal|0
+block|}
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -1369,9 +1371,11 @@ literal|"uha"
 block|,
 literal|0
 block|,
+block|{
 literal|0
 block|,
 literal|0
+block|}
 block|}
 decl_stmt|;
 end_decl_stmt
