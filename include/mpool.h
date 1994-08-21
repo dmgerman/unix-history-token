3a3f75d8e3557b8052c8630b64608cb73c797bd1
Paul Richards <paul@FreeBSD.org>
1994-08-21 04:03:58 +0000

    Made them idempotent. Reviewed by: Submitted by:
    Former-commit-id: 33099b39bee78d5b4e0dfbf532820cd5bdab270b

--- include/mpool.h
+++ include/mpool.h
@@ -3,6 +3,18 @@ begin_comment
 comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)mpool.h	8.1 (Berkeley) 6/2/93  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_MPOOL_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_MPOOL_H_
+end_define
+
 begin_comment
 comment|/*  * The memory pool scheme is a simple one.  Each in memory page is referenced  * by a bucket which is threaded in three ways.  All active pages are threaded  * on a hash chain (hashed by the page number) and an lru chain.  Inactive  * pages are threaded on a free chain.  Each reference to a memory pool is  * handed an MPOOL which is the opaque cookie passed to all of the memory  * routines.  */
 end_comment
@@ -485,6 +497,11 @@ end_endif
 begin_macro
 name|__END_DECLS
 end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
