3904a5788020b3fd38eead28b7e3e8fbb2b5efda
David Greenman <dg@FreeBSD.org>
1994-08-22 08:21:51 +0000

    Fixed minor typo in diagnostic message.
    Former-commit-id: 7986354282a784ec12b338ac63c1b4ea55e549d8

--- sys/dev/ed/if_ed.c
+++ sys/dev/ed/if_ed.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * $Id: if_ed.c,v 1.44 1994/08/13 03:50:03 wollman Exp $  */
+comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * $Id: if_ed.c,v 1.45 1994/08/18 22:34:52 wollman Exp $  */
 end_comment
 
 begin_include
@@ -5742,7 +5742,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"ed: packets buffers, but transmitter idle\n"
+literal|"ed: packets buffered, but transmitter idle\n"
 argument_list|)
 expr_stmt|;
 name|ed_xmit
