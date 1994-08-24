77a980eea9a1cb113958dc3e778e4f3c4f7c2143
David Greenman <dg@FreeBSD.org>
1994-08-24 10:53:53 +0000

    Pay attention to *all* errors from copyinstr(). This patch fixes a bug that causes a no-panic instant reboot when bogus argv/envvs are fed to execve().
    Former-commit-id: 5b8ae656009658e01ce10f792451efbb69612534

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman  * 4. The name of the developer may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_exec.c,v 1.3 1994/08/06 09:06:31 davidg Exp $  */
+comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman  * 4. The name of the developer may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_exec.c,v 1.4 1994/08/18 22:34:59 wollman Exp $  */
 end_comment
 
 begin_include
@@ -1454,6 +1454,8 @@ modifier|*
 name|envp
 decl_stmt|;
 name|int
+name|error
+decl_stmt|,
 name|length
 decl_stmt|;
 comment|/* 	 * extract arguments first 	 */
@@ -1469,6 +1471,7 @@ if|if
 condition|(
 name|argv
 condition|)
+block|{
 while|while
 condition|(
 name|argp
@@ -1500,6 +1503,8 @@ operator|)
 return|;
 if|if
 condition|(
+name|error
+operator|=
 name|copyinstr
 argument_list|(
 name|argp
@@ -1515,6 +1520,11 @@ argument_list|,
 operator|&
 name|length
 argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|error
 operator|==
 name|ENAMETOOLONG
 condition|)
@@ -1523,6 +1533,12 @@ operator|(
 name|E2BIG
 operator|)
 return|;
+return|return
+operator|(
+name|error
+operator|)
+return|;
+block|}
 name|iparams
 operator|->
 name|stringspace
@@ -1541,6 +1557,7 @@ name|argc
 operator|++
 expr_stmt|;
 block|}
+block|}
 comment|/* 	 * extract environment strings 	 */
 name|envv
 operator|=
@@ -1554,6 +1571,7 @@ if|if
 condition|(
 name|envv
 condition|)
+block|{
 while|while
 condition|(
 name|envp
@@ -1585,6 +1603,8 @@ operator|)
 return|;
 if|if
 condition|(
+name|error
+operator|=
 name|copyinstr
 argument_list|(
 name|envp
@@ -1600,6 +1620,11 @@ argument_list|,
 operator|&
 name|length
 argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|error
 operator|==
 name|ENAMETOOLONG
 condition|)
@@ -1608,6 +1633,12 @@ operator|(
 name|E2BIG
 operator|)
 return|;
+return|return
+operator|(
+name|error
+operator|)
+return|;
+block|}
 name|iparams
 operator|->
 name|stringspace
@@ -1625,6 +1656,7 @@ operator|->
 name|envc
 operator|++
 expr_stmt|;
+block|}
 block|}
 return|return
 operator|(
