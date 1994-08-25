f102a35e8eda8471e7529801a9afe2cba10dc569
Bruce Evans <bde@FreeBSD.org>
1994-08-25 10:01:00 +0000

    Support speeds 57600 and 115200. Privatize functions.
    Former-commit-id: 14572884724d5a5d488840cfadbe4cd78fea58ba

--- sys/kern/tty_compat.c
+++ sys/kern/tty_compat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_compat.c	8.1 (Berkeley) 6/10/93  * $Id$  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_compat.c	8.1 (Berkeley) 6/10/93  * $Id: tty_compat.c,v 1.3 1994/08/02 07:42:48 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -82,6 +82,23 @@ file|<sys/syslog.h>
 end_include
 
 begin_decl_stmt
+specifier|static
+name|int
+name|ttcompatgetflags
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|tty
+operator|*
+name|tp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
 name|void
 name|ttcompatsetflags
 name|__P
@@ -90,16 +107,19 @@ operator|(
 expr|struct
 name|tty
 operator|*
+name|tp
 operator|,
 expr|struct
 name|termios
 operator|*
+name|t
 operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+specifier|static
 name|void
 name|ttcompatsetlflags
 name|__P
@@ -108,10 +128,12 @@ operator|(
 expr|struct
 name|tty
 operator|*
+name|tp
 operator|,
 expr|struct
 name|termios
 operator|*
+name|t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -133,6 +155,22 @@ name|compatspeeds
 index|[]
 init|=
 block|{
+define|#
+directive|define
+name|MAX_SPEED
+value|17
+block|{
+literal|115200
+block|,
+literal|17
+block|}
+block|,
+block|{
+literal|57600
+block|,
+literal|16
+block|}
+block|,
 block|{
 literal|38400
 block|,
@@ -244,9 +282,7 @@ begin_decl_stmt
 specifier|static
 name|int
 name|compatspcodes
-index|[
-literal|16
-index|]
+index|[]
 init|=
 block|{
 literal|0
@@ -280,6 +316,10 @@ block|,
 literal|19200
 block|,
 literal|38400
+block|,
+literal|57600
+block|,
+literal|115200
 block|, }
 decl_stmt|;
 end_decl_stmt
@@ -372,7 +412,7 @@ operator|-
 literal|1
 operator|)
 condition|?
-literal|15
+name|MAX_SPEED
 else|:
 name|speed
 expr_stmt|;
@@ -416,7 +456,7 @@ operator|-
 literal|1
 operator|)
 condition|?
-literal|15
+name|MAX_SPEED
 else|:
 name|speed
 expr_stmt|;
@@ -493,7 +533,7 @@ operator|->
 name|sg_ispeed
 operator|)
 operator|>
-literal|15
+name|MAX_SPEED
 operator|||
 name|speed
 operator|<
@@ -525,7 +565,7 @@ operator|->
 name|sg_ospeed
 operator|)
 operator|>
-literal|15
+name|MAX_SPEED
 operator|||
 name|speed
 operator|<
@@ -1223,6 +1263,7 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|int
 name|ttcompatgetflags
 parameter_list|(
@@ -1538,6 +1579,7 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|void
 name|ttcompatsetflags
 parameter_list|(
@@ -1909,6 +1951,7 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|void
 name|ttcompatsetlflags
 parameter_list|(
