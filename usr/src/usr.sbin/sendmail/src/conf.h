45d7cac3e7810bb0744eaa5f45c5230ebe8ff4da
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-29 15:51:57 -0800

    UnixWare updates
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.119
    
    Former-commit-id: 65dc3bbb611029a65010b996c6dc59d76c9cc104

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.118 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.119 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -4109,7 +4109,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* **  UnixWare ** **	From Evan Champion<evanc@spatial.synapse.org>. */
+comment|/* **  UnixWare 1.1.2. ** **	From Evan Champion<evanc@spatial.synapse.org>. */
 end_comment
 
 begin_ifdef
@@ -4125,12 +4125,6 @@ name|SYSTEM5
 value|1
 end_define
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|HASGETUSERSHELL
-end_ifndef
-
 begin_define
 define|#
 directive|define
@@ -4142,23 +4136,46 @@ begin_comment
 comment|/* does not have getusershell(3) call */
 end_comment
 
-begin_endif
-endif|#
-directive|endif
-end_endif
+begin_define
+define|#
+directive|define
+name|HASGETDTABLESIZE
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|HASSETREUID
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|HASSETSID
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|HASINITGROUPS
+value|1
+end_define
 
 begin_define
 define|#
 directive|define
 name|GIDSET_T
-value|int
+value|gid_t
 end_define
 
 begin_define
 define|#
 directive|define
 name|SLEEP_T
-value|int
+value|unsigned
 end_define
 
 begin_define
