a7ae6759cc4c2f89bfea28425aed4deb8bc77b65
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-08-30 00:12:11 +0000

    Make decl/define for Debugger() match reality. Submitted by:	jkh
    Former-commit-id: cc0ee945a656de5d929f04ca9cccf99ef268818a

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.25 1994/08/27 16:14:34 davidg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.26 1994/08/29 21:37:49 ache Exp $  */
 end_comment
 
 begin_define
@@ -154,7 +154,11 @@ end_ifdef
 begin_function_decl
 name|int
 name|Debugger
-parameter_list|()
+parameter_list|(
+specifier|const
+name|char
+modifier|*
+parameter_list|)
 function_decl|;
 end_function_decl
 
@@ -171,7 +175,9 @@ begin_define
 define|#
 directive|define
 name|Debugger
-parameter_list|()
+parameter_list|(
+name|x
+parameter_list|)
 end_define
 
 begin_endif
