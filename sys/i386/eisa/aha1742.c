c528e1e522981677f26b81e9c8f572c439819c49
Stefan Esser <se@FreeBSD.org>
1994-08-31 23:32:32 +0000

    Reviewed by:	Stefan Esser <se> Debugger() takes an (char*) argument, changed macro definition accordingly.
    Former-commit-id: ee101954db46d5b3f085fcb65444798e3a392cab

--- sys/i386/eisa/aha1742.c
+++ sys/i386/eisa/aha1742.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.19 1994/08/23 07:52:04 paul Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.20 1994/08/27 16:14:17 davidg Exp $  */
 end_comment
 
 begin_include
@@ -143,7 +143,9 @@ begin_define
 define|#
 directive|define
 name|Debugger
-parameter_list|()
+parameter_list|(
+name|x
+parameter_list|)
 value|panic("should call debugger here (adaptec.c)")
 end_define
 
