566fb757f10af5ce056e7bf3a90c9946a75f3090
Bruce Evans <bde@FreeBSD.org>
1994-09-08 11:49:04 +0000

    Remove <machine/eflags.h> and all dependencies on it.  eflags.h is just the Mach/i386 version of the BSD/vax(?) <machine/psl.h>.  The Mach version has slightly better names for many macros but is now out of date and little used.  It was originally used even less (for spelling PSL_T as EFL_TF in <machine/db_machdep.h>).
    Former-commit-id: ab16a329117e9d8bda60d06b6de7d5119b5bd1b0

--- sys/amd64/include/db_machdep.h
+++ sys/amd64/include/db_machdep.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	$Id: db_machdep.h,v 1.2 1993/10/16 14:39:10 rgrimes Exp $  */
+comment|/*   * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	$Id: db_machdep.h,v 1.3 1993/11/07 17:42:50 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -76,12 +76,8 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<machine/eflags.h>
+file|<machine/psl.h>
 end_include
-
-begin_comment
-comment|/* from Mach... */
-end_comment
 
 begin_comment
 comment|/* #include<i386/trap.h> */
@@ -99,10 +95,6 @@ directive|define
 name|i386_saved_state
 value|trapframe
 end_define
-
-begin_comment
-comment|/* end of mangling */
-end_comment
 
 begin_typedef
 typedef|typedef
@@ -208,7 +200,7 @@ name|db_clear_single_step
 parameter_list|(
 name|regs
 parameter_list|)
-value|((regs)->tf_eflags&= ~EFL_TF)
+value|((regs)->tf_eflags&= ~PSL_T)
 end_define
 
 begin_define
@@ -218,7 +210,7 @@ name|db_set_single_step
 parameter_list|(
 name|regs
 parameter_list|)
-value|((regs)->tf_eflags |=  EFL_TF)
+value|((regs)->tf_eflags |=  PSL_T)
 end_define
 
 begin_comment
@@ -230,7 +222,7 @@ comment|/* #define IS_WATCHPOINT_TRAP(type, code)	((type) == T_WATCHPOINT) */
 end_comment
 
 begin_comment
-comment|/* using the 386bsd values, rather than the Mach ones: */
+comment|/* using the FreeBSD values, rather than the Mach ones: */
 end_comment
 
 begin_define
