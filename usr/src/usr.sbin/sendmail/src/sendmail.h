88fa9bf2fb2e776abe96f1bcc6f86290f07a9edb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-09-08 18:16:53 -0800

    add M_UDBENVELOPE flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.61
    
    Former-commit-id: bcf53719c134e2a57e3803f07aa2f5683f0c9d2a

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.60 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.61 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.60		%G%"
+literal|"@(#)sendmail.h	8.61		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -874,6 +874,17 @@ end_define
 
 begin_comment
 comment|/* MAIL11V3: preview headers */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|M_UDBENVELOPE
+value|'i'
+end_define
+
+begin_comment
+comment|/* do udbsender rewriting on envelope */
 end_comment
 
 begin_define
