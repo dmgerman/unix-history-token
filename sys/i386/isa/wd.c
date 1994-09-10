0bbc2859b32fba57de00a4b1c2dc511df0eedddd
David Greenman <dg@FreeBSD.org>
1994-09-10 03:19:49 +0000

    Brought over two fixes from 1.1.5; this now makes this driver "up to date" with 1.1.5:
      revision 1.40
      date: 1994/06/17 16:57:03;  author: pst;  state: Exp;  lines: +4 -2
      From: Gill Kloepfer Jr. <gil@limbic.ssdl.com>
      Verified by: pst
    
      > The DIOCSBAD ioctl sets a bad block table (is almost suredly called by
      > the bad144 utility) and changes the memory-resident bad block table.  The
      > problem is that bad144intern() is not called after the "disk" structure has
      > been changed, so that the internal bad144 table will become out-of-sync with
      > the one in the disk structure.
      ----------------------------
      revision 1.39
      date: 1994/06/07 01:36:39;  author: phk;  state: Exp;  lines: +3 -2
      another place option !defined(DISKLABEL_UNPROTECTED) was needed.
    
    
    Former-commit-id: a4227c77b6d47d46aad392c2e7bdb3864e18f8af

--- sys/i386/isa/wd.c
+++ sys/i386/isa/wd.c
@@ -15,7 +15,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.44 1994/08/29 21:35:16 ache Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.45 1994/08/30 14:26:13 ache Exp $  */
 end_comment
 
 begin_comment
@@ -1617,6 +1617,13 @@ goto|goto
 name|done
 goto|;
 block|}
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|DISKLABEL_UNPROTECTED
+argument_list|)
 comment|/* "soft" write protect check */
 if|if
 condition|(
@@ -1655,6 +1662,9 @@ goto|goto
 name|done
 goto|;
 block|}
+endif|#
+directive|endif
+comment|/* !defined(DISKLABEL_UNPROTECTED) */
 comment|/* 	 * Do bounds checking, adjust transfer, and set b_cylin. 	 */
 if|if
 condition|(
@@ -6044,6 +6054,7 @@ operator|=
 name|EBADF
 expr_stmt|;
 else|else
+block|{
 name|du
 operator|->
 name|dk_bad
@@ -6056,6 +6067,12 @@ operator|*
 operator|)
 name|addr
 expr_stmt|;
+name|bad144intern
+argument_list|(
+name|du
+argument_list|)
+expr_stmt|;
+block|}
 break|break;
 case|case
 name|DIOCGDINFO
