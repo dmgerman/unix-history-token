d5c67f208748458c8eee2800be2dedd08792a84a
Geoff Rehmet <csgr@FreeBSD.org>
1994-09-10 20:03:14 +0000

    Change "asm" to "__asm" - makes it possible to compile code with "-ansi" compiler switch set.
    Former-commit-id: 3afcae4e21cc685ae2092d97236d62a049fead66

--- sys/i386/include/endian.h
+++ sys/i386/include/endian.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)endian.h	7.8 (Berkeley) 4/3/91  *	$Id: endian.h,v 1.3 1993/11/07 17:42:52 wollman Exp $  */
+comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)endian.h	7.8 (Berkeley) 4/3/91  *	$Id: endian.h,v 1.4 1993/12/19 05:14:45 alm Exp $  */
 end_comment
 
 begin_ifndef
@@ -103,7 +103,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|({ register u_long X = (x); \    asm ("rorl $16, %1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
+value|({ register u_long X = (x); \    __asm ("rorl $16, %1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
 end_define
 
 begin_if
@@ -122,7 +122,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|({ register u_long X = (x); \    asm ("xchgb %h1, %b1\n\trorl $16, %1\n\txchgb %h1, %b1" \ 	: "=q" (X) \ 	: "0" (X)); \    X; })
+value|({ register u_long X = (x); \    __asm ("xchgb %h1, %b1\n\trorl $16, %1\n\txchgb %h1, %b1" \ 	: "=q" (X) \ 	: "0" (X)); \    X; })
 end_define
 
 begin_define
@@ -133,7 +133,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|({ register u_short X = (x); \    asm ("xchgb %h1, %b1" \ 	: "=q" (X) \ 	: "0" (X)); \    X; })
+value|({ register u_short X = (x); \    __asm ("xchgb %h1, %b1" \ 	: "=q" (X) \ 	: "0" (X)); \    X; })
 end_define
 
 begin_else
@@ -153,7 +153,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|({ register u_long X = (x); \    asm ("rorw $8, %w1\n\trorl $16, %1\n\trorw $8, %w1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
+value|({ register u_long X = (x); \    __asm ("rorw $8, %w1\n\trorl $16, %1\n\trorw $8, %w1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
 end_define
 
 begin_define
@@ -164,7 +164,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|({ register u_short X = (x); \    asm ("rorw $8, %w1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
+value|({ register u_short X = (x); \    __asm ("rorw $8, %w1" \ 	: "=r" (X) \ 	: "0" (X)); \    X; })
 end_define
 
 begin_endif
