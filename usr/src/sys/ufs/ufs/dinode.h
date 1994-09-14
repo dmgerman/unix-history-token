6413564d784768268fa6f3c399a775c661f8072f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-09-13 18:37:53 -0800

    add whiteout type
    SCCS-vsn: sys/ufs/ufs/dinode.h 8.6
    
    Former-commit-id: b09f03da4f14f9a3c93dfb5112d016a84abe231e

--- usr/src/sys/ufs/ufs/dinode.h
+++ usr/src/sys/ufs/ufs/dinode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dinode.h	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dinode.h	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -349,6 +349,17 @@ end_define
 
 begin_comment
 comment|/* UNIX domain socket. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IFWHT
+value|0160000
+end_define
+
+begin_comment
+comment|/* Whiteout. */
 end_comment
 
 end_unit
