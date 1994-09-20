103f342e8c1b9cd4b958790ecc7310f3263712d1
Bruce Evans <bde@FreeBSD.org>
1994-09-20 22:26:37 +0000

    Don't provide bogus source operands in some asms.  This probably shouldn't matter, but similar bogusness in npx.c causes compiling without -O to fail.
    Use __volatile in all asms.
    
    Parenthesize macro args.
    
    Change the names of the macros to avoid namespace pollution.
    
    Remove unnecessary "#ifdef __i386__".
    
    Sort #defines.
    
    Add comments.
    
    
    Former-commit-id: 4f148193944af1f44e7b05beea639e8f29833b75

--- sys/amd64/include/floatingpoint.h
+++ sys/amd64/include/floatingpoint.h
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 Andrew Moore, Talke Studio  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#) floatingpoint.h	1.0 (Berkeley) 9/23/93  *	$Id: floatingpoint.h,v 1.4 1993/11/07 17:42:55 wollman Exp $  */
-end_comment
-
-begin_comment
-comment|/*  *	IEEE floating point structure and function definitions  */
+comment|/*-  * Copyright (c) 1993 Andrew Moore, Talke Studio  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#) floatingpoint.h	1.0 (Berkeley) 9/23/93  *	$Id: floatingpoint.h,v 1.5 1994/08/04 19:16:36 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -18,6 +14,14 @@ define|#
 directive|define
 name|_FLOATINGPOINT_H_
 end_define
+
+begin_comment
+comment|/*  * IEEE floating point structure and function definitions  */
+end_comment
+
+begin_comment
+comment|/*-  * XXX the following undocumented pollution is exported:  *	fpsetsticky().  *	FP*FLD, FP*OFF and FP*REG from<machine/ieeefp.h>  */
+end_comment
 
 begin_include
 include|#
@@ -37,50 +41,44 @@ directive|ifdef
 name|__GNUC__
 end_ifdef
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__i386__
-end_ifdef
-
 begin_define
 define|#
 directive|define
-name|fnstcw
+name|__fldenv
 parameter_list|(
 name|addr
 parameter_list|)
-value|__asm("fnstcw %0" : "=m" (*addr) : "0" (*addr))
+value|__asm __volatile("fldenv %0" : : "m" (*(addr)))
 end_define
 
 begin_define
 define|#
 directive|define
-name|fnstsw
+name|__fnstenv
 parameter_list|(
 name|addr
 parameter_list|)
-value|__asm("fnstsw %0" : "=m" (*addr) : "0" (*addr))
+value|__asm __volatile("fnstenv %0" : "=m" (*(addr)))
 end_define
 
 begin_define
 define|#
 directive|define
-name|fnstenv
+name|__fnstcw
 parameter_list|(
 name|addr
 parameter_list|)
-value|__asm("fnstenv %0" : "=m" (*addr) : "0" (*addr))
+value|__asm __volatile("fnstcw %0" : "=m" (*(addr)))
 end_define
 
 begin_define
 define|#
 directive|define
-name|fldenv
+name|__fnstsw
 parameter_list|(
 name|addr
 parameter_list|)
-value|__asm("fldenv %0" : : "m" (*addr))
+value|__asm __volatile("fnstsw %0" : "=m" (*(addr)))
 end_define
 
 begin_comment
@@ -101,13 +99,14 @@ name|unsigned
 name|short
 name|_mem
 decl_stmt|;
+comment|/*- 	 * This is more efficient than it looks.  The switch gets optimized 	 * away if _reg is constant. 	 * 	 * The default case only supports _reg == 0.  We could handle more 	 * registers (e.g., tags) using fnstenv, but the interface doesn't 	 * support more. 	 */
 switch|switch
 condition|(
 name|_reg
 condition|)
 block|{
 default|default:
-name|fnstcw
+name|__fnstcw
 argument_list|(
 operator|&
 name|_mem
@@ -117,7 +116,7 @@ break|break;
 case|case
 name|FP_STKY_REG
 case|:
-name|fnstsw
+name|__fnstsw
 argument_list|(
 operator|&
 name|_mem
@@ -163,7 +162,8 @@ decl_stmt|;
 name|unsigned
 name|_p
 decl_stmt|;
-name|fnstenv
+comment|/* 	 * _reg == 0 could be handled better using fnstcw/fldcw. 	 */
+name|__fnstenv
 argument_list|(
 name|_env
 argument_list|)
@@ -204,7 +204,7 @@ operator|&
 name|_fld
 operator|)
 expr_stmt|;
-name|fldenv
+name|__fldenv
 argument_list|(
 name|_env
 argument_list|)
@@ -214,15 +214,6 @@ name|_p
 return|;
 block|}
 end_function
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* __i386__ */
-end_comment
 
 begin_endif
 endif|#
