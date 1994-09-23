9604dcb42a5a81c0f80ce9b4e1e6ce3b90bbb4c8
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-09-23 02:06:17 -0800

    patch from cgd
    SCCS-vsn: sys/netns/ns_input.c 8.2
    
    Former-commit-id: 62466a0d367abdb192606e37cc2d6f192cefa90e

--- usr/src/sys/netns/ns_input.c
+++ usr/src/sys/netns/ns_input.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1984, 1985, 1986, 1987, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ns_input.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1984, 1985, 1986, 1987, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ns_input.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -240,11 +240,6 @@ end_macro
 
 begin_block
 block|{
-specifier|extern
-name|struct
-name|timeval
-name|time
-decl_stmt|;
 name|ns_broadhost
 operator|=
 operator|*
