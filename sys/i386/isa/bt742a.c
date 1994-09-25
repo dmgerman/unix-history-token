209ce090b7c890c204e80a8422a43939256b6394
Poul-Henning Kamp <phk@FreeBSD.org>
1994-09-25 07:11:37 +0000

    Made the kernel 256 bytes smaller by removing a unused 	u_char  bt_scratch_buf[256]; doesn't affect the file-size though :-(
    Former-commit-id: 2e365e90752b682d7d20697bf5d79f75c05f0133

--- sys/i386/isa/bt742a.c
+++ sys/i386/isa/bt742a.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt742a.c,v 1.21 1994/08/23 07:52:07 paul Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt742a.c,v 1.22 1994/08/27 16:14:19 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -1586,15 +1586,6 @@ directive|define
 name|INVALIDATE_CACHE
 value|{asm volatile( ".byte	0x0F ;.byte 0x08" ); }
 end_define
-
-begin_decl_stmt
-name|u_char
-name|bt_scratch_buf
-index|[
-literal|256
-index|]
-decl_stmt|;
-end_decl_stmt
 
 begin_struct
 struct|struct
