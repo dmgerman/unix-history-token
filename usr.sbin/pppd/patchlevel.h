34ea4ede903a7e2e8cc86d78d8301d61b2c4b961
Garrett Wollman <wollman@FreeBSD.org>
1994-09-25 02:11:26 +0000

    Initial revision
    Former-commit-id: d8266afb835f94f4a3118a6a6ebd2ed4bb6cd493

---
+++ usr.sbin/pppd/patchlevel.h
@@ -0,0 +1,28 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $Id: patchlevel.h,v 1.10 1994/06/09 01:51:10 paulus Exp $ */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PATCHLEVEL
+value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|VERSION
+value|"2.1"
+end_define
+
+begin_define
+define|#
+directive|define
+name|DATE
+value|"9 June 94"
+end_define
+
+end_unit
+
