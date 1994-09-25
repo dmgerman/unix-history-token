223953d663ea259f01c76125b824c3fabafbf798
David Greenman <dg@FreeBSD.org>
1994-09-25 04:02:10 +0000

    Disabled swap anti-fragmentation code. It reduces swap paging performance by 20% in my tests, and it appears to be the cause of a swap leak.
    Submitted by:	John Dyson
    
    
    Former-commit-id: 44646a8b1441eb3fc2db58c9cdb6e8bb4daf11bd

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) 3/21/94  * $Id: swap_pager.c,v 1.7 1994/08/18 22:36:00 wollman Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) 3/21/94  * $Id: swap_pager.c,v 1.8 1994/08/29 06:23:18 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -1398,6 +1398,9 @@ modifier|*
 name|rtval
 parameter_list|)
 block|{
+ifdef|#
+directive|ifdef
+name|EXP
 name|unsigned
 name|tmpalloc
 decl_stmt|;
@@ -1475,6 +1478,8 @@ return|return
 literal|1
 return|;
 block|}
+endif|#
+directive|endif
 if|if
 condition|(
 operator|!
@@ -1526,6 +1531,9 @@ decl_stmt|;
 name|unsigned
 name|tmpalloc
 decl_stmt|;
+ifdef|#
+directive|ifdef
+name|EXP
 if|if
 condition|(
 operator|(
@@ -1541,19 +1549,8 @@ operator|>=
 name|nblocksfrag
 condition|)
 block|{
-while|while
-condition|(
-operator|(
-name|from
-operator|+
-name|nblocksfrag
-operator|)
-operator|<=
-name|to
-operator|+
-literal|1
-condition|)
-block|{
+endif|#
+directive|endif
 name|rlist_free
 argument_list|(
 operator|&
@@ -1561,26 +1558,14 @@ name|swapmap
 argument_list|,
 name|from
 argument_list|,
-name|from
-operator|+
-name|nblocksfrag
-operator|-
-literal|1
+name|to
 argument_list|)
 expr_stmt|;
-name|from
-operator|+=
-name|nblocksfrag
-expr_stmt|;
-block|}
-block|}
-if|if
-condition|(
-name|from
-operator|>=
-name|to
-condition|)
+ifdef|#
+directive|ifdef
+name|EXP
 return|return;
+block|}
 name|rlist_free
 argument_list|(
 operator|&
@@ -1620,6 +1605,8 @@ literal|1
 argument_list|)
 expr_stmt|;
 block|}
+endif|#
+directive|endif
 block|}
 end_function
 
