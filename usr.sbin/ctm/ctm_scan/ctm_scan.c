44c67dfa8a14ad6ae1aad2e3063713563aad1fe8
Poul-Henning Kamp <phk@FreeBSD.org>
1994-09-26 06:01:01 +0000

    A couple of nit-picks.
    Former-commit-id: 8df09930909e463ef1e0976ffd181c23d71433c3

--- usr.sbin/ctm/ctm_scan/ctm_scan.c
+++ usr.sbin/ctm/ctm_scan/ctm_scan.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id$  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: ctm_scan.c,v 1.5 1994/09/25 20:45:55 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -493,6 +493,12 @@ block|{
 case|case
 name|S_IFDIR
 case|:
+if|if
+condition|(
+operator|!
+name|CheckMode
+condition|)
+block|{
 name|i
 operator|=
 name|printf
@@ -530,6 +536,7 @@ operator|-
 literal|1
 argument_list|)
 expr_stmt|;
+block|}
 name|ret
 operator||=
 name|Do
