a6eb3dbf7a8b88abfba2ed4fa06ebbe4e7c2e500
Jordan K. Hubbard <jkh@FreeBSD.org>
1994-09-28 16:23:13 +0000

    Make bt_timeout() match proper calling conventions - thank you -W!
    Former-commit-id: bf9045753c248c0b7ee6a782fefeed17b3474645

--- sys/i386/isa/bt742a.c
+++ sys/i386/isa/bt742a.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt742a.c,v 1.23 1994/09/25 07:11:37 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt742a.c,v 1.24 1994/09/28 15:55:35 jkh Exp $  */
 end_comment
 
 begin_comment
@@ -1923,9 +1923,8 @@ begin_function_decl
 name|void
 name|bt_timeout
 parameter_list|(
-name|caddr_t
-parameter_list|,
-name|int
+name|void
+modifier|*
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -6929,12 +6928,7 @@ block|{
 comment|/* 		 * We timed out, so call the timeout handler manually, 		 * accounting for the fact that the clock is not running yet 		 * by taking out the clock queue entry it makes. 		 */
 name|bt_timeout
 argument_list|(
-operator|(
-name|caddr_t
-operator|)
 name|ccb
-argument_list|,
-literal|0
 argument_list|)
 expr_stmt|;
 comment|/* 		 * because we are polling, take out the timeout entry 		 * bt_timeout made 		 */
@@ -7009,12 +7003,7 @@ block|{
 comment|/* 			 * We timed out again...  This is bad.  Notice that 			 * this time there is no clock queue entry to remove. 			 */
 name|bt_timeout
 argument_list|(
-operator|(
-name|caddr_t
-operator|)
 name|ccb
-argument_list|,
-literal|0
 argument_list|)
 expr_stmt|;
 block|}
@@ -7042,11 +7031,9 @@ begin_function
 name|void
 name|bt_timeout
 parameter_list|(
-name|caddr_t
+name|void
+modifier|*
 name|arg1
-parameter_list|,
-name|int
-name|arg2
 parameter_list|)
 block|{
 name|struct
