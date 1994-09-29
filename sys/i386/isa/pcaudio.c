904391069d693d0a99887022aa098e6769ace9d3
Soren Schmidt <sos@FreeBSD.org>
1994-09-29 21:11:29 +0000

    Oops, forgot one patch for the changed selwakeup, fixed.
    Former-commit-id: 2d625f69e5e04acecc33d3cfdac6d253332ce67f

--- sys/i386/isa/pcaudio.c
+++ sys/i386/isa/pcaudio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: pcaudio.c,v 1.7 1994/09/16 13:33:47 davidg Exp $   */
+comment|/*-  * Copyright (c) 1994 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: pcaudio.c,v 1.8 1994/09/29 08:24:43 sos Exp $   */
 end_comment
 
 begin_include
@@ -180,14 +180,11 @@ name|char
 name|timer_on
 decl_stmt|;
 comment|/* is playback running */
-name|char
-name|coll
-decl_stmt|;
-comment|/* select collision */
-name|pid_t
+name|struct
+name|selinfo
 name|wsel
 decl_stmt|;
-comment|/* pid of select'ing proc */
+comment|/* select status */
 block|}
 name|pca_status
 struct|;
@@ -1801,28 +1798,38 @@ condition|(
 name|pca_status
 operator|.
 name|wsel
+operator|.
+name|si_pid
 condition|)
 block|{
 name|selwakeup
 argument_list|(
+operator|(
+expr|struct
+name|selinfo
+operator|*
+operator|)
+operator|&
 name|pca_status
 operator|.
 name|wsel
-argument_list|,
-name|pca_status
 operator|.
-name|coll
+name|si_pid
 argument_list|)
 expr_stmt|;
 name|pca_status
 operator|.
 name|wsel
+operator|.
+name|si_pid
 operator|=
 literal|0
 expr_stmt|;
 name|pca_status
 operator|.
-name|coll
+name|wsel
+operator|.
+name|si_flags
 operator|=
 literal|0
 expr_stmt|;
@@ -1901,6 +1908,8 @@ condition|(
 name|pca_status
 operator|.
 name|wsel
+operator|.
+name|si_pid
 operator|&&
 operator|(
 name|p1
@@ -1910,6 +1919,8 @@ argument_list|(
 name|pca_status
 operator|.
 name|wsel
+operator|.
+name|si_pid
 argument_list|)
 operator|)
 operator|&&
@@ -1925,14 +1936,18 @@ name|selwait
 condition|)
 name|pca_status
 operator|.
-name|coll
+name|wsel
+operator|.
+name|si_flags
 operator|=
-literal|1
+name|SI_COLL
 expr_stmt|;
 else|else
 name|pca_status
 operator|.
 name|wsel
+operator|.
+name|si_pid
 operator|=
 name|p
 operator|->
