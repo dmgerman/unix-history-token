1dc3791e2e9f3a2423431cf758e0aa9f514f8c37
Rodney Grimes <rgrimes@FreeBSD.org>
1994-10-04 06:39:27 +0000

    Pull in 1.1.5.1 changes to sd.c from 1.23 to 1.27 (basically the fixes for drives that return sector counts of 0 and cause division by 0 traps during the probe). Reviewed by:	Dave <root@hclb.demon.co.uk> Obtained from:	FreeBSD 1.1.5.1
    Former-commit-id: fb5305e281c0f2275c4ce86a1699c8203cef33af

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.30 1994/09/02 04:12:23 davidg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.31 1994/09/28 20:16:44 se Exp $  */
 end_comment
 
 begin_define
@@ -3725,10 +3725,6 @@ block|}
 else|else
 block|{
 comment|/* set it to something reasonable */
-name|sectors
-operator|=
-literal|32
-expr_stmt|;
 name|disk_parms
 operator|->
 name|heads
@@ -3747,7 +3743,26 @@ operator|*
 literal|32
 operator|)
 expr_stmt|;
+name|sectors
+operator|=
+literal|32
+expr_stmt|;
 block|}
+comment|/* keep secsiz sane too - we may divide by it later */
+if|if
+condition|(
+name|disk_parms
+operator|->
+name|secsiz
+operator|==
+literal|0
+condition|)
+name|disk_parms
+operator|->
+name|secsiz
+operator|=
+name|SECSIZE
+expr_stmt|;
 name|disk_parms
 operator|->
 name|sectors
