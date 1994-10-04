c8f4b29b116cf517ad49a3336798b5ad687f4cd3
Rodney Grimes <rgrimes@FreeBSD.org>
1994-10-04 06:45:57 +0000

    In sd_get_params() the timeout for the MODE_SENSE command was too short. I changed it from 2000 to 4000 and now my disk works fine.
    Submitted by:	John Hay <jhay@mikom.csir.co.za>
    
    
    Former-commit-id: 7a39fe1d1629ba8a94915e3c2299560c55882bf9

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.31 1994/09/28 20:16:44 se Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.32 1994/10/04 06:39:27 rgrimes Exp $  */
 end_comment
 
 begin_define
@@ -3501,7 +3501,7 @@ argument_list|)
 argument_list|,
 name|SD_RETRIES
 argument_list|,
-literal|2000
+literal|4000
 argument_list|,
 name|NULL
 argument_list|,
