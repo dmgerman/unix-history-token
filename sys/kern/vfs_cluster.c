c6c18c46f5fa657419dc44639655c4390b82d213
Poul-Henning Kamp <phk@FreeBSD.org>
1994-10-08 22:33:43 +0000

    Cosmetics: added ()'s and fixed prinf-formats to make gcc silent.
    Former-commit-id: a992dc2e2da94b5da703b26700c069e859713e9b

--- sys/kern/vfs_cluster.c
+++ sys/kern/vfs_cluster.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_cluster.c	8.7 (Berkeley) 2/13/94  * $Id: vfs_cluster.c,v 1.4 1994/08/08 09:11:31 davidg Exp $  */
+comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_cluster.c	8.7 (Berkeley) 2/13/94  * $Id: vfs_cluster.c,v 1.5 1994/09/24 18:31:45 davidg Exp $  */
 end_comment
 
 begin_include
@@ -2955,6 +2955,7 @@ argument_list|,
 name|start_lbn
 argument_list|)
 operator|||
+operator|(
 name|last_bp
 operator|==
 name|NULL
@@ -2962,6 +2963,7 @@ operator|&&
 name|start_lbn
 operator|==
 name|lbn
+operator|)
 condition|)
 break|break;
 comment|/* 		 * Get the desired block buffer (unless it is the final 		 * sequential block whose buffer was passed in explictly 		 * as last_bp). 		 */
@@ -3044,8 +3046,11 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"Clustered Block: %d addr %x bufsize: %d\n"
+literal|"Clustered Block: %lu addr %lx bufsize: %ld\n"
 argument_list|,
+operator|(
+name|u_long
+operator|)
 name|bp
 operator|->
 name|b_lblkno
@@ -3061,8 +3066,11 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"Child Block: %d addr: %x\n"
+literal|"Child Block: %lu addr: %lx\n"
 argument_list|,
+operator|(
+name|u_long
+operator|)
 name|tbp
 operator|->
 name|b_lblkno
