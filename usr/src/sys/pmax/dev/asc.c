7f06f72e6a3d551a3bb6a5a1de23f1643e21f508
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-10-10 05:06:49 -0800

    Fix for the case when DEBUG isn't defined (from Ralph)
    SCCS-vsn: sys/pmax/dev/asc.c 8.4
    
    Former-commit-id: f35003980c791c53c40b4d3ce61f75fa9d848dfb

--- usr/src/sys/pmax/dev/asc.c
+++ usr/src/sys/pmax/dev/asc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)asc.c	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Ralph Campbell and Rick Macklem.  *  * %sccs.include.redist.c%  *  *	@(#)asc.c	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -4725,12 +4725,17 @@ name|data
 argument_list|)
 expr_stmt|;
 comment|/* XXX */
+ifdef|#
+directive|ifdef
+name|DEBUG
 name|asc_DumpLog
 argument_list|(
 literal|"get_status"
 argument_list|)
 expr_stmt|;
 comment|/* XXX */
+endif|#
+directive|endif
 if|if
 condition|(
 name|data
