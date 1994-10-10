bafd5ae8b139362845c8485ce652ff23d9f9c9c3
Poul-Henning Kamp <phk@FreeBSD.org>
1994-10-10 01:12:27 +0000

    Cosmetics.  Silence gcc -Wall.  Much more to do here :-(
    Former-commit-id: 81ea705e73d68a66d0948ccdba102ca9748160b8

--- sys/i386/isa/aha1542.c
+++ sys/i386/isa/aha1542.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.34 1994/08/27 16:14:16 davidg Exp $  */
+comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.35 1994/09/21 18:17:56 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -1695,6 +1695,38 @@ begin_comment
 comment|/* time to wait for reset (mSec) */
 end_comment
 
+begin_decl_stmt
+name|int
+name|aha_poll
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+expr|struct
+name|scsi_xfer
+operator|*
+operator|,
+expr|struct
+name|aha_ccb
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|aha_init
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -3166,16 +3198,6 @@ name|ccb
 decl_stmt|;
 block|{
 name|struct
-name|aha_data
-modifier|*
-name|aha
-init|=
-name|ahadata
-index|[
-name|unit
-index|]
-decl_stmt|;
-name|struct
 name|scsi_sense_data
 modifier|*
 name|s1
@@ -4473,14 +4495,6 @@ name|unit
 index|]
 decl_stmt|;
 name|struct
-name|scsi_sense_data
-modifier|*
-name|s1
-decl_stmt|,
-modifier|*
-name|s2
-decl_stmt|;
-name|struct
 name|aha_ccb
 modifier|*
 name|ccb
@@ -4494,16 +4508,6 @@ name|int
 name|seg
 decl_stmt|;
 comment|/* scatter gather seg being worked on */
-name|int
-name|i
-init|=
-literal|0
-decl_stmt|;
-name|int
-name|rc
-init|=
-literal|0
-decl_stmt|;
 name|int
 name|thiskv
 decl_stmt|;
@@ -4520,11 +4524,6 @@ decl_stmt|,
 name|datalen
 decl_stmt|,
 name|flags
-decl_stmt|;
-name|struct
-name|iovec
-modifier|*
-name|iovp
 decl_stmt|;
 name|int
 name|s
@@ -5348,11 +5347,6 @@ name|ahadata
 index|[
 name|unit
 index|]
-decl_stmt|;
-name|int
-name|done
-init|=
-literal|0
 decl_stmt|;
 name|int
 name|count
