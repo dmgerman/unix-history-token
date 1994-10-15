2ecb314e0dbbe558ee952e956dc71f2337088695
David Greenman <dg@FreeBSD.org>
1994-10-15 13:33:09 +0000

    1) Some of the counters in the vmmeter struct don't fit well into the Mach VM    scheme of things, so I've changed them to be more appropriate. page in/ous    are now associated with the pager that did them. Nuked v_fault as the    only fault of interest that wouldn't be already counted in v_trap is a VM    fault, and this is counted seperately. 2) Implemented most of the remaining counters and corrected the counting of    some that were done wrong. They are all almost correct now...just a few    minor ones left to fix.
    Former-commit-id: d02e8b0bfd1f880a476795d31572c8d222180c25

--- sys/sys/vmmeter.h
+++ sys/sys/vmmeter.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vmmeter.h	8.1 (Berkeley) 6/2/93  * $Id: vmmeter.h,v 1.3 1994/08/02 07:54:12 davidg Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vmmeter.h	8.1 (Berkeley) 6/2/93  * $Id: vmmeter.h,v 1.4 1994/08/21 04:42:14 paul Exp $  */
 end_comment
 
 begin_ifndef
@@ -44,10 +44,6 @@ name|unsigned
 name|v_soft
 decl_stmt|;
 comment|/* software interrupts */
-name|unsigned
-name|v_faults
-decl_stmt|;
-comment|/* total faults taken */
 comment|/* 	 * Virtual memory activity. 	 */
 name|unsigned
 name|v_lookups
@@ -66,37 +62,37 @@ name|v_cow_faults
 decl_stmt|;
 comment|/* number of copy-on-writes */
 name|unsigned
-name|v_swpin
+name|v_swapin
 decl_stmt|;
-comment|/* swapins */
+comment|/* swap pager pageins */
 name|unsigned
-name|v_swpout
+name|v_swapout
 decl_stmt|;
-comment|/* swapouts */
+comment|/* swap pager pageouts */
 name|unsigned
-name|v_pswpin
+name|v_swappgsin
 decl_stmt|;
-comment|/* pages swapped in */
+comment|/* swap pager pages paged in */
 name|unsigned
-name|v_pswpout
+name|v_swappgsout
 decl_stmt|;
-comment|/* pages swapped out */
+comment|/* swap pager pages paged out */
 name|unsigned
-name|v_pageins
+name|v_vnodein
 decl_stmt|;
-comment|/* number of pageins */
+comment|/* vnode pager pageins */
 name|unsigned
-name|v_pageouts
+name|v_vnodeout
 decl_stmt|;
-comment|/* number of pageouts */
+comment|/* vnode pager pageouts */
 name|unsigned
-name|v_pgpgin
+name|v_vnodepgsin
 decl_stmt|;
-comment|/* pages paged in */
+comment|/* vnode_pager pages paged in */
 name|unsigned
-name|v_pgpgout
+name|v_vnodepgsout
 decl_stmt|;
-comment|/* pages paged out */
+comment|/* vnode pager pages paged out */
 name|unsigned
 name|v_intrans
 decl_stmt|;
