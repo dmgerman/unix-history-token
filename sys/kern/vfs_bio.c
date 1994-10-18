d62abc264a7c73ebd7fd143f6f60263d1793694f
David Greenman <dg@FreeBSD.org>
1994-10-18 06:55:57 +0000

    Removed references to bclnlist which we don't use/support/need.
    Former-commit-id: 3b5e24fee9b827b0286fb383b73375d253279485

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.13 1994/10/04 03:10:47 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.14 1994/10/05 09:48:21 davidg Exp $  */
 end_comment
 
 begin_include
@@ -109,18 +109,6 @@ name|swqueue
 name|bswlist
 decl_stmt|;
 end_decl_stmt
-
-begin_decl_stmt
-name|struct
-name|buf
-modifier|*
-name|bclnlist
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* Head of cleaned page list. */
-end_comment
 
 begin_decl_stmt
 specifier|extern
