90c4a415fe0683e5125ff6f8a2c828fb1c0e40f5
Garrett Wollman <wollman@FreeBSD.org>
1994-10-19 00:10:10 +0000

    kdc_datalen is now a number, not a function pointer.
    Former-commit-id: bfb282431d68749eb1aa85aefb96ffda61f426ee

--- sys/sys/devconf.h
+++ sys/sys/devconf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994, Garrett A. Wollman.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sysctl.h,v 1.17 1994/10/10 00:58:34 phk Exp $  */
+comment|/*  * Copyright (c) 1994, Garrett A. Wollman.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: devconf.h,v 1.1 1994/10/16 03:52:59 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -145,25 +145,6 @@ function_decl|;
 end_typedef
 
 begin_comment
-comment|/*  * DATALEN: calculate the length of the externalized form  */
-end_comment
-
-begin_typedef
-typedef|typedef
-name|size_t
-function_decl|(
-modifier|*
-name|kdc_datalen_t
-function_decl|)
-parameter_list|(
-name|struct
-name|kern_devconf
-modifier|*
-parameter_list|)
-function_decl|;
-end_typedef
-
-begin_comment
 comment|/*  * GOAWAY: shut the device down, if possible, and prepare to exit.  */
 end_comment
 
@@ -228,12 +209,12 @@ name|kdc_internalize_t
 name|kdc_internalize
 decl_stmt|;
 comment|/* filled in by driver */
-name|kdc_datalen_t
-name|kdc_datalen
-decl_stmt|;
-comment|/* filled in by driver */
 name|kdc_goaway_t
 name|kdc_goaway
+decl_stmt|;
+comment|/* filled in by driver */
+name|size_t
+name|kdc_datalen
 decl_stmt|;
 comment|/* filled in by driver */
 block|}
