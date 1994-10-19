b6db606f533507d354fc0e3cac5320ff7faae67a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-10-18 23:39:59 -0800

    unwhiteout => undelete
    SCCS-vsn: sbin/restore/utilities.c 8.4
    
    Former-commit-id: 855bf3d570a12545c0053c0ce213c059563b53b0

--- usr/src/sbin/restore/utilities.c
+++ usr/src/sbin/restore/utilities.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)utilities.c	8.3 (Berkeley) %G%"
+literal|"@(#)utilities.c	8.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1086,7 +1086,7 @@ condition|(
 operator|!
 name|Nflag
 operator|&&
-name|unwhiteout
+name|undelete
 argument_list|(
 name|name
 argument_list|)
