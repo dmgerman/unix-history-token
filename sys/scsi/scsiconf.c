f497a50980c9b3fecd86d19e0a2b3979a7a0508f
Garrett Wollman <wollman@FreeBSD.org>
1994-10-19 20:34:15 +0000

    Implement scsi_externalize(), again.  (This was supposed to be committed in the last batch of changes but somehow got lost in the shuffle.)
    Former-commit-id: 1be15b9b0ece907962c2bc00dff666a27e17c6b0

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: scsiconf.c,v 1.9 1994/09/28 20:16:40 se Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: scsiconf.c,v 1.10 1994/10/08 22:26:38 phk Exp $  */
 end_comment
 
 begin_include
@@ -4018,6 +4018,58 @@ operator|)
 return|;
 block|}
 end_block
+
+begin_function
+name|int
+name|scsi_externalize
+parameter_list|(
+name|struct
+name|scsi_link
+modifier|*
+name|sl
+parameter_list|,
+name|void
+modifier|*
+name|userp
+parameter_list|,
+name|size_t
+modifier|*
+name|lenp
+parameter_list|)
+block|{
+if|if
+condition|(
+operator|*
+name|lenp
+operator|<
+sizeof|sizeof
+expr|*
+name|sl
+condition|)
+return|return
+name|ENOMEM
+return|;
+operator|*
+name|lenp
+operator|-=
+sizeof|sizeof
+expr|*
+name|sl
+expr_stmt|;
+return|return
+name|copyout
+argument_list|(
+name|sl
+argument_list|,
+name|userp
+argument_list|,
+sizeof|sizeof
+expr|*
+name|sl
+argument_list|)
+return|;
+block|}
+end_function
 
 end_unit
 
