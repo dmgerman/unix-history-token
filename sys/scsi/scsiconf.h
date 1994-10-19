47ec2d063cae393c643a28da87d8d570910a33d1
Garrett Wollman <wollman@FreeBSD.org>
1994-10-19 01:49:23 +0000

    Define SCSI_EXTERNALLEN outside of #ifdef KERNEL, for lsdev.
    Former-commit-id: 6048bc73a45d53049b18908be4a3cb880ad1f2ed

--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.10 1994/09/28 20:16:41 se Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.11 1994/10/19 01:20:44 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -1333,13 +1333,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_define
-define|#
-directive|define
-name|SCSI_EXTERNALLEN
-value|(sizeof(struct scsi_link))
-end_define
-
 begin_function_decl
 specifier|extern
 name|int
@@ -1381,6 +1374,13 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_define
+define|#
+directive|define
+name|SCSI_EXTERNALLEN
+value|(sizeof(struct scsi_link))
+end_define
 
 begin_ifdef
 ifdef|#
