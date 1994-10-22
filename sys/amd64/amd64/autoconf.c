89789fbaa40e15446f1b4ab60d9ef95ee825d857
Poul-Henning Kamp <phk@FreeBSD.org>
1994-10-22 17:51:46 +0000

    Autoconf is the one to realize that we are booted disk-less and start the ball rolling.  locore is just moving some data from the boot-program.
    Former-commit-id: f1255fef98ad50608f182363c3575ef76144caf5

--- sys/amd64/amd64/autoconf.c
+++ sys/amd64/amd64/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.13 1994/08/29 21:47:11 ache Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.14 1994/08/31 23:36:56 se Exp $  */
 end_comment
 
 begin_comment
@@ -144,12 +144,6 @@ directive|endif
 if|#
 directive|if
 name|GENERICxxx
-operator|&&
-operator|!
-name|defined
-argument_list|(
-name|DISKLESS
-argument_list|)
 if|if
 condition|(
 operator|(
@@ -168,33 +162,98 @@ argument_list|()
 expr_stmt|;
 else|#
 directive|else
-ifndef|#
-directive|ifndef
-name|DISKLESS
+ifdef|#
+directive|ifdef
+name|NFS
+block|{
+specifier|extern
+name|int
+name|nfs_diskless_valid
+decl_stmt|;
+extern|extern int (*mountroot
+block|)
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+expr_stmt|;
+specifier|extern
+name|int
+name|nfs_mountroot
+argument_list|(
+name|void
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|nfs_diskless_valid
+condition|)
+name|mountroot
+operator|=
+name|nfs_mountroot
+expr_stmt|;
+else|else
 name|setroot
 argument_list|()
-expr_stmt|;
-endif|#
-directive|endif
-endif|#
-directive|endif
-comment|/* 	 * Configure swap area and related system 	 * parameter based on device(s) used. 	 */
-name|swapconf
-argument_list|()
-expr_stmt|;
-name|cold
-operator|=
-literal|0
 expr_stmt|;
 block|}
 end_function
 
+begin_else
+else|#
+directive|else
+end_else
+
 begin_comment
+comment|/* !NFS */
+end_comment
+
+begin_expr_stmt
+name|setroot
+argument_list|()
+expr_stmt|;
+end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* NFS */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* 	 * Configure swap area and related system 	 * parameter based on device(s) used. 	 */
+end_comment
+
+begin_expr_stmt
+name|swapconf
+argument_list|()
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|cold
+operator|=
+literal|0
+expr_stmt|;
+end_expr_stmt
+
+begin_comment
+unit|}
 comment|/*  * Configure swap space and related parameters.  */
 end_comment
 
 begin_function
-specifier|static
+unit|static
 name|void
 name|swapconf
 parameter_list|()
