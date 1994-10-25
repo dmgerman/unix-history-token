d44981ffbc8022254c66a7f06de386db3e6ff325
David Greenman <dg@FreeBSD.org>
1994-10-25 11:27:51 +0000

    Changed some variable names in lf_addblock to make the code both understandable and conform to other conventions used in the file.
    Former-commit-id: 831e7eb101b56546226e4eade93a68daed9ef30d

--- sys/kern/kern_lockf.c
+++ sys/kern/kern_lockf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Scooter Morris at Genentech Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_lockf.c	8.3 (Berkeley) 1/6/94  * $Id: kern_lockf.c,v 1.1 1994/08/08 17:30:48 davidg Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Scooter Morris at Genentech Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_lockf.c	8.3 (Berkeley) 1/6/94  * $Id: kern_lockf.c,v 1.2 1994/09/25 19:33:37 phk Exp $  */
 end_comment
 
 begin_include
@@ -2444,19 +2444,19 @@ begin_function
 name|void
 name|lf_addblock
 parameter_list|(
-name|lock
+name|blocklist
 parameter_list|,
-name|blocked
+name|lock
 parameter_list|)
 name|struct
 name|lockf
 modifier|*
-name|lock
+name|blocklist
 decl_stmt|;
 name|struct
 name|lockf
 modifier|*
-name|blocked
+name|lock
 decl_stmt|;
 block|{
 specifier|register
@@ -2467,7 +2467,7 @@ name|lf
 decl_stmt|;
 if|if
 condition|(
-name|blocked
+name|lock
 operator|==
 name|NOLOCKF
 condition|)
@@ -2486,14 +2486,14 @@ name|lf_print
 argument_list|(
 literal|"addblock: adding"
 argument_list|,
-name|blocked
+name|lock
 argument_list|)
 expr_stmt|;
 name|lf_print
 argument_list|(
 literal|"to blocked list of"
 argument_list|,
-name|lock
+name|blocklist
 argument_list|)
 expr_stmt|;
 block|}
@@ -2505,7 +2505,7 @@ condition|(
 operator|(
 name|lf
 operator|=
-name|lock
+name|blocklist
 operator|->
 name|lf_block
 operator|)
@@ -2513,11 +2513,11 @@ operator|==
 name|NOLOCKF
 condition|)
 block|{
-name|lock
+name|blocklist
 operator|->
 name|lf_block
 operator|=
-name|blocked
+name|lock
 expr_stmt|;
 return|return;
 block|}
@@ -2539,7 +2539,7 @@ name|lf
 operator|->
 name|lf_block
 operator|=
-name|blocked
+name|lock
 expr_stmt|;
 return|return;
 block|}
