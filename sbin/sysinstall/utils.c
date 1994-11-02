aa1077b9428897809d2dcd4fd056c78d0ed8f677
Poul-Henning Kamp <phk@FreeBSD.org>
1994-11-02 22:06:24 +0000

    a missing star.
    Former-commit-id: 76b406cec8df97bee49fdc701be37e5c01dda8a3

--- sbin/sysinstall/utils.c
+++ sbin/sysinstall/utils.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: utils.c,v 1.17 1994/11/02 07:34:01 ache Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: utils.c,v 1.18 1994/11/02 08:52:15 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -1040,6 +1040,7 @@ name|char
 modifier|*
 name|p
 decl_stmt|,
+modifier|*
 name|path
 init|=
 name|StrAlloc
