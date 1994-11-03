c9c10b4c88fa6da0e3b5a5881e18010546e11431
Joerg Wunsch <joerg@FreeBSD.org>
1994-11-03 18:20:15 +0000

    |Both, the wd and the sd driver do not reject invalid request of odd |sizes. They simply pass them to the disks, which usually causes |fatal errors then.
    Former-commit-id: b948fbf5bfc4caff93e3ef137a013e01f589f0a2

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.38 1994/10/27 20:45:04 jkh Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.39 1994/10/31 23:45:40 phk Exp $  */
 end_comment
 
 begin_define
@@ -1884,6 +1884,28 @@ condition|)
 block|{
 goto|goto
 name|done
+goto|;
+block|}
+comment|/* 	 * Odd number of bytes 	 */
+if|if
+condition|(
+name|bp
+operator|->
+name|b_bcount
+operator|%
+name|DEV_BSIZE
+operator|!=
+literal|0
+condition|)
+block|{
+name|bp
+operator|->
+name|b_error
+operator|=
+name|EINVAL
+expr_stmt|;
+goto|goto
+name|bad
 goto|;
 block|}
 comment|/* 	 * Decide which unit and partition we are talking about 	 * only raw is ok if no label 	 */
