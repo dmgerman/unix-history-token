074d56b19dbbbdb0dcf9c566a4acd6d990994ed2
Paul Traina <pst@FreeBSD.org>
1994-11-03 17:49:01 +0000

    Assign character device 20 to be the user reserved device.
    Former-commit-id: f377a6b99cd97fd2f320583ef429e58afd6e8258

--- sys/i386/i386/conf.c
+++ sys/i386/i386/conf.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright (c) UNIX System Laboratories, Inc.  All or some portions
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.39 1994/10/31 17:20:12 joerg Exp $  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.40 1994/11/01 01:47:22 pst Exp $  */
 end_comment
 
 begin_include
@@ -3952,7 +3952,7 @@ name|nullreset
 block|,
 name|NULL
 block|,
-comment|/* scsi 'generic' */
+comment|/* scsi */
 name|seltrue
 block|,
 name|nommap
@@ -3960,6 +3960,7 @@ block|,
 name|nostrat
 block|}
 block|,
+comment|/* 'generic' */
 block|{
 name|twopen
 block|,
@@ -3986,6 +3987,8 @@ block|,
 name|nostrat
 block|}
 block|,
+comment|/*  * If you need a cdev major number for a driver that you intend to donate  * back to the group or release publically, please contact the FreeBSD team  * by sending mail to "FreeBSD-hackers@freefall.cdrom.com".  * If you assign one yourself it may conflict with someone else.  * Otherwise, simply use the one reserved for local use.  */
+comment|/* character device 20 is reserved for local use */
 block|{
 operator|(
 name|d_open_t
@@ -4024,7 +4027,6 @@ operator|*
 operator|)
 name|enxio
 block|,
-comment|/* unused */
 operator|(
 name|d_reset_t
 operator|*
@@ -4300,7 +4302,7 @@ name|nullreset
 block|,
 name|NULL
 block|,
-comment|/* sound driver */
+comment|/* sound */
 name|sndselect
 block|,
 name|nommap
@@ -4554,7 +4556,7 @@ name|nullreset
 block|,
 name|NULL
 block|,
-comment|/* cortex framegrabber */
+comment|/* cortex */
 name|seltrue
 block|,
 name|nommap
@@ -4562,6 +4564,7 @@ block|,
 name|NULL
 block|}
 block|,
+comment|/*framegrabber*/
 block|{
 name|sockopen
 block|,
@@ -4587,69 +4590,6 @@ name|nommap
 block|,
 name|NULL
 block|}
-block|,
-comment|/* character device 42 is reserved for local use */
-block|{
-operator|(
-name|d_open_t
-operator|*
-operator|)
-name|enxio
-block|,
-operator|(
-name|d_close_t
-operator|*
-operator|)
-name|enxio
-block|,
-operator|(
-name|d_rdwr_t
-operator|*
-operator|)
-name|enxio
-block|,
-comment|/*42*/
-operator|(
-name|d_rdwr_t
-operator|*
-operator|)
-name|enxio
-block|,
-operator|(
-name|d_ioctl_t
-operator|*
-operator|)
-name|enxio
-block|,
-operator|(
-name|d_stop_t
-operator|*
-operator|)
-name|enxio
-block|,
-operator|(
-name|d_reset_t
-operator|*
-operator|)
-name|enxio
-block|,
-name|NULL
-block|,
-operator|(
-name|d_select_t
-operator|*
-operator|)
-name|enxio
-block|,
-operator|(
-name|d_mmap_t
-operator|*
-operator|)
-name|enxio
-block|,
-name|NULL
-block|}
-comment|/*  * If you need a cdev major number for a driver that you intend to donate  * back to the group or release publically, please contact the FreeBSD team  * by sending mail to "FreeBSD-hackers@freefall.cdrom.com".  * If you assign one yourself it may conflict with someone else.  * Otherwise, simply use the one reserved for local use.  */
 block|}
 decl_stmt|;
 end_decl_stmt
