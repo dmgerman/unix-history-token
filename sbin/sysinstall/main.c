aeff555a9fd83db18092c2613aaa3db98dd1767b
Poul-Henning Kamp <phk@FreeBSD.org>
1994-11-06 04:05:45 +0000

    label.c	Make sure we always show 8 disk-partitions. main.c	sanitize the logic of what we do when:
    if(getpid()!=1) do stage0 & stage1  (very useful actually)
    else if (floppy-marker-file is there) stage0-2, reboot
    else stage3-5
    
    
    Former-commit-id: 1353e0686f1e24095e1123a33bfe11afae2655af

--- sbin/sysinstall/main.c
+++ sbin/sysinstall/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: main.c,v 1.9 1994/10/29 10:01:34 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: main.c,v 1.10 1994/11/01 10:10:24 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -237,19 +237,24 @@ argument_list|(
 literal|"No memory\n"
 argument_list|)
 expr_stmt|;
-name|setjmp
-argument_list|(
-name|jmp_restart
-argument_list|)
-expr_stmt|;
-comment|/* XXX Allow people to "restart" */
 if|if
 condition|(
-name|getenv
-argument_list|(
-literal|"STAGE0"
-argument_list|)
-operator|||
+name|getpid
+argument_list|()
+operator|!=
+literal|1
+condition|)
+block|{
+name|stage0
+argument_list|()
+expr_stmt|;
+name|stage1
+argument_list|()
+expr_stmt|;
+block|}
+elseif|else
+if|if
+condition|(
 operator|!
 name|access
 argument_list|(
@@ -267,6 +272,13 @@ argument_list|()
 expr_stmt|;
 name|stage2
 argument_list|()
+expr_stmt|;
+name|end_dialog
+argument_list|()
+expr_stmt|;
+name|dialog_active
+operator|=
+literal|0
 expr_stmt|;
 name|reboot
 argument_list|(
