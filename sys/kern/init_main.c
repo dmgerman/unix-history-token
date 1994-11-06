ad4c177794a2e38a0b7d3db65c9db820f319f8b8
David Greenman <dg@FreeBSD.org>
1994-11-06 05:01:58 +0000

    Added support for starting the experimental "vmdaemon" system process. Enabled via REL2_1.
    Submitted by:	John Dyson
    
    
    Former-commit-id: 86a018dfb97b0ec1dcdf9a843becd08808e180ba

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)init_main.c	8.9 (Berkeley) 1/21/94  * $Id: init_main.c,v 1.14 1994/10/20 00:08:26 phk Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)init_main.c	8.9 (Berkeley) 1/21/94  * $Id: init_main.c,v 1.15 1994/10/26 03:34:20 phk Exp $  */
 end_comment
 
 begin_include
@@ -244,6 +244,9 @@ name|pageproc
 decl_stmt|,
 modifier|*
 name|updateproc
+decl_stmt|,
+modifier|*
+name|vmproc
 decl_stmt|;
 end_decl_stmt
 
@@ -1603,8 +1606,92 @@ comment|/* NOTREACHED */
 block|}
 end_if
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|REL2_1
+end_ifdef
+
 begin_comment
-comment|/* 	 * Start update daemon (process 3). 	 */
+comment|/* 	 * Start high level vm daemon (process 3). 	 */
+end_comment
+
+begin_if
+if|if
+condition|(
+name|fork
+argument_list|(
+name|p
+argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
+name|NULL
+argument_list|,
+name|rval
+argument_list|)
+condition|)
+name|panic
+argument_list|(
+literal|"failed fork vm daemon"
+argument_list|)
+expr_stmt|;
+end_if
+
+begin_if
+if|if
+condition|(
+name|rval
+index|[
+literal|1
+index|]
+condition|)
+block|{
+name|p
+operator|=
+name|curproc
+expr_stmt|;
+name|vmproc
+operator|=
+name|p
+expr_stmt|;
+name|p
+operator|->
+name|p_flag
+operator||=
+name|P_INMEM
+operator||
+name|P_SYSTEM
+expr_stmt|;
+name|bcopy
+argument_list|(
+literal|"vmdaemon"
+argument_list|,
+name|p
+operator|->
+name|p_comm
+argument_list|,
+sizeof|sizeof
+argument_list|(
+literal|"vmdaemon"
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|vm_daemon
+argument_list|()
+expr_stmt|;
+comment|/*NOTREACHED*/
+block|}
+end_if
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* 	 * Start update daemon (process 4). 	 */
 end_comment
 
 begin_if
