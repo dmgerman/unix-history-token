2742037523b940dfc80dcbfe6e7df2b89db854ba
Joerg Wunsch <joerg@FreeBSD.org>
1994-11-07 20:09:35 +0000

    Avoid a division by zero if the actual sector size isn't known during the device announcement; assume a sector size of 512 instead (likely to be right at all). This case happens when booting with a removable disk device attached (e.g. an MOD), but no medium inserted.
    Former-commit-id: d2e026cc408def3772e361684d4d2d595f00c9ec

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.39 1994/10/31 23:45:40 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.40 1994/11/03 18:20:15 joerg Exp $  */
 end_comment
 
 begin_define
@@ -1046,6 +1046,21 @@ name|SCSI_NOSLEEP
 operator||
 name|SCSI_NOMASK
 argument_list|)
+expr_stmt|;
+comment|/* 	 * if we don't have actual parameters, assume 512 bytes/sec 	 * (could happen on removable media - MOD) 	 * -- this avoids the division below from falling over 	 */
+if|if
+condition|(
+name|dp
+operator|->
+name|secsiz
+operator|==
+literal|0
+condition|)
+name|dp
+operator|->
+name|secsiz
+operator|=
+literal|512
 expr_stmt|;
 name|printf
 argument_list|(
