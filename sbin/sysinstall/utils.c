d34fb123b24697af24ffa0c81ec442fcf90dbe16
Poul-Henning Kamp <phk@FreeBSD.org>
1994-11-07 04:53:23 +0000

    Always clear before we "TellEm()"
    Former-commit-id: b45748a8d1c64b2dceaec4393bca9d44a9ad2eab

--- sbin/sysinstall/utils.c
+++ sbin/sysinstall/utils.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: utils.c,v 1.23 1994/11/05 07:34:02 jkh Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: utils.c,v 1.24 1994/11/05 23:09:26 ache Exp $  *  */
 end_comment
 
 begin_include
@@ -459,6 +459,9 @@ literal|">\n\r"
 argument_list|,
 literal|3
 argument_list|)
+expr_stmt|;
+name|dialog_clear
+argument_list|()
 expr_stmt|;
 name|dialog_msgbox
 argument_list|(
