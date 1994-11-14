029615a2b4feb6899efb8255c249acd2d79ff67d
Andreas Schulz <ats@FreeBSD.org>
1994-11-14 23:39:33 +0000

    scsiconf.h: Change the sc_link structure from 8 targets with 8 lun's to 16 targets with 8 lun's. Wide scsi and the ncr 825 does support 16 targets. scsiconf.c: move the addition of the trailing NUL's out of a then clause, they are also needed in the else clause for an unknown device.
    Former-commit-id: 8165a88928c73a093891b9fdf905341ad2a638ec

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: scsiconf.c,v 1.12 1994/10/23 21:27:55 wollman Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: scsiconf.c,v 1.13 1994/11/12 17:13:23 ats Exp $  */
 end_comment
 
 begin_include
@@ -3262,13 +3262,6 @@ argument_list|,
 literal|8
 argument_list|)
 expr_stmt|;
-name|manu
-index|[
-literal|8
-index|]
-operator|=
-literal|0
-expr_stmt|;
 name|strncpy
 argument_list|(
 name|model
@@ -3280,13 +3273,6 @@ argument_list|,
 literal|16
 argument_list|)
 expr_stmt|;
-name|model
-index|[
-literal|16
-index|]
-operator|=
-literal|0
-expr_stmt|;
 name|strncpy
 argument_list|(
 name|version
@@ -3297,13 +3283,6 @@ name|revision
 argument_list|,
 literal|4
 argument_list|)
-expr_stmt|;
-name|version
-index|[
-literal|4
-index|]
-operator|=
-literal|0
 expr_stmt|;
 block|}
 else|else
@@ -3341,6 +3320,27 @@ literal|4
 argument_list|)
 expr_stmt|;
 block|}
+name|manu
+index|[
+literal|8
+index|]
+operator|=
+literal|0
+expr_stmt|;
+name|model
+index|[
+literal|16
+index|]
+operator|=
+literal|0
+expr_stmt|;
+name|version
+index|[
+literal|4
+index|]
+operator|=
+literal|0
+expr_stmt|;
 name|printf
 argument_list|(
 literal|"%s%d targ %d lun %d: type %ld(%s) %s SCSI%d\n"
