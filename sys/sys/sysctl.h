ff107b75e12db226a9daf9b10995dfd7b29fe18a
Bruce Evans <bde@FreeBSD.org>
1994-11-14 13:59:09 +0000

    Update stupid declaration in comment to match code.
    Declare all public syscntlfn's here.
    
    
    Former-commit-id: 537f002077d64f1db012b3fcf3da7de1e163bb34

--- sys/sys/sysctl.h
+++ sys/sys/sysctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)sysctl.h	8.1 (Berkeley) 6/2/93  * $Id: sysctl.h,v 1.17 1994/10/10 00:58:34 phk Exp $  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)sysctl.h	8.1 (Berkeley) 6/2/93  * $Id: sysctl.h,v 1.18 1994/10/16 03:53:00 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -1380,7 +1380,7 @@ comment|/* DEBUG */
 end_comment
 
 begin_comment
-comment|/*  * Internal sysctl function calling convention:  *  *	(*sysctlfn)(name, namelen, oldval, oldlenp, newval, newlen);  *  * The name parameter points at the next component of the name to be  * interpreted.  The namelen parameter is the number of integers in  * the name.  */
+comment|/*  * Internal sysctl function calling convention:  *  *	(*sysctlfn)(name, namelen, oldval, oldlenp, newval, newlen, p);  *  * The name parameter points at the next component of the name to be  * interpreted.  The namelen parameter is the number of integers in  * the name.  */
 end_comment
 
 begin_typedef
@@ -1415,6 +1415,54 @@ operator|)
 argument_list|)
 expr_stmt|;
 end_typedef
+
+begin_decl_stmt
+name|sysctlfn
+name|cpu_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|dev_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|fs_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|hw_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|kern_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|net_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|ntp_sysctl
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|sysctlfn
+name|vm_sysctl
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|int
@@ -1617,12 +1665,6 @@ name|size_t
 operator|*
 operator|)
 argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|sysctlfn
-name|dev_sysctl
 decl_stmt|;
 end_decl_stmt
 
