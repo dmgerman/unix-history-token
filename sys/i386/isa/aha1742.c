3331f63ed12acbc030dcf4dc953978694aabe769
Bruce Evans <bde@FreeBSD.org>
1994-11-15 14:53:09 +0000

    Remove bogus declaration of Debugger().  Call Debugger() even if DDB is not defined, but still call panic() after Debugger() returns, although most other SCSI drivers just call Debugger().
    Former-commit-id: 8d5c896c03b245454749cb6cb56b204a5942806a

--- sys/i386/isa/aha1742.c
+++ sys/i386/isa/aha1742.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.23 1994/10/19 01:58:51 wollman Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.24 1994/10/23 21:27:05 wollman Exp $  */
 end_comment
 
 begin_include
@@ -79,6 +79,18 @@ directive|include
 file|<i386/isa/isa_device.h>
 end_include
 
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|NAHB
+value|1
+end_define
+
 begin_endif
 endif|#
 directive|endif
@@ -111,77 +123,33 @@ comment|/*
 comment|*/
 end_comment
 
-begin_ifdef
-ifdef|#
-directive|ifdef
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
 name|KERNEL
-end_ifdef
-
-begin_ifdef
-ifdef|#
-directive|ifdef
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
 name|DDB
-end_ifdef
+argument_list|)
+end_if
 
-begin_function_decl
-name|int
-name|Debugger
+begin_define
+define|#
+directive|define
+name|fatal_if_no_DDB
 parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_comment
-comment|/* DDB */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|Debugger
-parameter_list|(
-name|x
-parameter_list|)
-value|panic("should call debugger here (adaptec.c)")
+value|panic("panic for historical reasons")
 end_define
 
 begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* DDB */
-end_comment
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_comment
-comment|/* KERNEL */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|NAHB
-value|1
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* kernel */
-end_comment
 
 begin_typedef
 typedef|typedef
@@ -1775,6 +1743,9 @@ argument_list|(
 literal|"aha1742"
 argument_list|)
 expr_stmt|;
+name|fatal_if_no_DDB
+argument_list|()
+expr_stmt|;
 block|}
 name|outl
 argument_list|(
@@ -2050,6 +2021,9 @@ name|Debugger
 argument_list|(
 literal|"aha1742"
 argument_list|)
+expr_stmt|;
+name|fatal_if_no_DDB
+argument_list|()
 expr_stmt|;
 block|}
 name|outl
