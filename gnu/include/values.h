5632d73e32aad5a2a4ac276ed195b2c5e9cc319a
Andrey Chernov <ache@FreeBSD.org>
1994-11-15 13:32:35 +0000

    Add comment about SYSV-ism
    Former-commit-id: 0f14f3331f11dfb980983cd0368da52f997ea6ee

--- gnu/include/values.h
+++ gnu/include/values.h
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* This file is SYSV-ism (for compatibility only) */
+end_comment
+
+begin_comment
 comment|/*  Copyright (C) 1988 Free Software Foundation     written by Doug Lea (dl@rocky.oswego.edu)  This file is part of the GNU C++ Library.  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for more details. You should have received a copy of the GNU Library General Public License along with this library; if not, write to the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
 end_comment
 
