aafb1e53004c9d12c35fefe0d2cdaaa80f3521e9
David Greenman <dg@FreeBSD.org>
1994-11-17 14:42:27 +0000

    Fixed rather serious bug where the interface wasn't started after it was stopped in ed_setrcr(). I'm amazed that the driver worked at all. Thanks to John Hay for pointing this out.
    Former-commit-id: 1373475fb20ee8d69e380f8451204442cff6c347

--- sys/i386/isa/if_ed.c
+++ sys/i386/isa/if_ed.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * $Id: if_ed.c,v 1.54 1994/10/23 21:27:16 wollman Exp $  */
+comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * $Id: if_ed.c,v 1.55 1994/11/13 07:17:46 davidg Exp $  */
 end_comment
 
 begin_include
@@ -9364,6 +9364,22 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+comment|/* 	 * Start interface. 	 */
+name|outb
+argument_list|(
+name|sc
+operator|->
+name|nic_addr
+operator|+
+name|ED_P0_CR
+argument_list|,
+name|sc
+operator|->
+name|cr_proto
+operator||
+name|ED_CR_STA
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
