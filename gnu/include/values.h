cd1496685c4fd731da2129fdd7e6666f993590ad
Andrey Chernov <ache@FreeBSD.org>
1994-11-17 11:20:44 +0000

    Put #warning "this file is obsoleted ... etc."
    Former-commit-id: dde76b00c11977f6bd513fd830b361198a9b7a21

--- gnu/include/values.h
+++ gnu/include/values.h
@@ -1,7 +1,20 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_comment
-comment|/* This file is SYSV-ism (for compatibility only) */
-end_comment
+begin_if
+if|#
+directive|if
+name|__GNUC__
+end_if
+
+begin_warning
+warning|#
+directive|warning
+literal|"this file includes<values.h> which is obsoleted, use<limits.h> or<float.h> instead"
+end_warning
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/*  Copyright (C) 1988 Free Software Foundation     written by Doug Lea (dl@rocky.oswego.edu)  This file is part of the GNU C++ Library.  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for more details. You should have received a copy of the GNU Library General Public License along with this library; if not, write to the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
