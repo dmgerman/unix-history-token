eafddd56951819191eaf64f9bdbbd17dd6e3744e
Justin T. Gibbs <gibbs@FreeBSD.org>
1994-11-18 09:14:14 +0000

    Change the include for the sequencer program to point back to the right place:
    include "../../sys/gnu/misc/aic7770/aic7770_seq.h"
    
    
    Former-commit-id: 0bd18e28c4d9cc195f9ec29bbf76134cda7b88fa

--- sys/i386/isa/aic7770.c
+++ sys/i386/isa/aic7770.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Driver for the 27/284X series adaptec SCSI controllers written by   * Justin T. Gibbs.  Much of this driver was taken from Julian Elischer's  * 1742 driver, so it bears his copyright.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7770.c,v 1.2 1994/11/18 08:19:21 jkh Exp $  */
+comment|/*  * Driver for the 27/284X series adaptec SCSI controllers written by   * Justin T. Gibbs.  Much of this driver was taken from Julian Elischer's  * 1742 driver, so it bears his copyright.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7770.c,v 1.3 1994/11/18 09:03:09 jkh Exp $  */
 end_comment
 
 begin_comment
@@ -6408,11 +6408,17 @@ name|int
 name|port
 decl_stmt|;
 block|{
-specifier|extern
+specifier|static
 name|unsigned
 name|char
 name|seqprog
 index|[]
+init|=
+block|{
+include|#
+directive|include
+file|"../../sys/gnu/misc/aic7770/aic7770_seq.h"
+block|}
 decl_stmt|;
 name|outb
 argument_list|(
