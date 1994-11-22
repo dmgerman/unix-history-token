eb6b01a6d645bac6691081ea94c70f491fbcdbe4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-11-21 22:38:23 -0800

    add VISTTY flag to identify tty vnodes
    SCCS-vsn: sys/sys/vnode.h 8.11
    
    Former-commit-id: 5e0b5ae8d216e065b2d95759f563035697084659

--- usr/src/sys/sys/vnode.h
+++ usr/src/sys/sys/vnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	8.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vnode.h	8.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -329,6 +329,17 @@ end_define
 
 begin_comment
 comment|/* vnode being used by kernel */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VISTTY
+value|0x0008
+end_define
+
+begin_comment
+comment|/* vnode represents a tty */
 end_comment
 
 begin_define
