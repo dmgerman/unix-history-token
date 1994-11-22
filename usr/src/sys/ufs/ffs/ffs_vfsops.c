0352e70aea26ddf70a1422e0401bbe0c1d0b3723
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-11-21 22:33:14 -0800

    FSCRED goes away; clean up use of NOCRED
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 8.14
    
    Former-commit-id: d02d562b869312e5022087c0fa81286f306505ff

--- usr/src/sys/ufs/ffs/ffs_vfsops.c
+++ usr/src/sys/ufs/ffs/ffs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	8.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	8.14 (Berkeley) %G%  */
 end_comment
 
 begin_include
