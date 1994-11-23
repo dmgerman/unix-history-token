03ba83621643310da419c3fe95cf2f22bc10aed2
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-11-22 19:47:37 -0800

    get rid of usecount mount restriction (needed for union mounts)
    SCCS-vsn: sys/kern/vfs_syscalls.c 8.26
    
    Former-commit-id: 674c8be74a2187585975587d88661fc4ac1d2aeb

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	8.25 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	8.26 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -678,12 +678,6 @@ return|;
 block|}
 if|if
 condition|(
-name|vp
-operator|->
-name|v_usecount
-operator|>
-literal|1
-operator|||
 name|vp
 operator|->
 name|v_mountedhere
