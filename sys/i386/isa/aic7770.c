20776f01dbba10f12cc70916c7802b725c4994d8
Andreas Schulz <ats@FreeBSD.org>
1994-11-25 22:25:15 +0000

    Changed the MAX_SLOTS constant from 8 to 16. The EISA bus can have up to 16 slots.
    Former-commit-id: f70231a585cd3ec9e0f948c134be5cab4561e2c1

--- sys/i386/isa/aic7770.c
+++ sys/i386/isa/aic7770.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Driver for the 27/284X series adaptec SCSI controllers written by   * Justin T. Gibbs.  Much of this driver was taken from Julian Elischer's  * 1742 driver, so it bears his copyright.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7770.c,v 1.4 1994/11/18 09:14:14 gibbs Exp $  */
+comment|/*  * Driver for the 27/284X series adaptec SCSI controllers written by   * Justin T. Gibbs.  Much of this driver was taken from Julian Elischer's  * 1742 driver, so it bears his copyright.  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7770.c,v 1.5 1994/11/18 20:34:30 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -311,11 +311,11 @@ begin_define
 define|#
 directive|define
 name|MAX_SLOTS
-value|8
+value|16
 end_define
 
 begin_comment
-comment|/* XXX should this be 16?? Need EISA spec */
+comment|/* max slots on the EISA bus */
 end_comment
 
 begin_expr_stmt
