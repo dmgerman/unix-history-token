586b2675eb3d582b0d629ab9f4dce6cb98a64dd7
Andrey Chernov <ache@FreeBSD.org>
1994-12-03 04:34:46 +0000

    Move ncurses test here (also nice game)
    Former-commit-id: c5716404f657559144afb86c4e5bc0d213947bfa

--- .ref-BSD-4_4_Lite1/usr/src/contrib/jove-4.14.6/style.h
+++
@@ -1,7 +0,0 @@
-begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_comment
-comment|/***************************************************************************  * This program is Copyright (C) 1986, 1987, 1988 by Jonathan Payne.  JOVE *  * is provided to you without charge, and with no warranty.  You may give  *  * away copies of JOVE, including sources, provided that this notice is    *  * included in all the files.                                              *  ***************************************************************************/
-end_comment
-
-end_unit
-
