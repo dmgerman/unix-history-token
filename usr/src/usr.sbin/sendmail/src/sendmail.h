5c8ee94d0fbe96c0264ec362832fd6a33663d69d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-12-05 16:43:01 -0800

    add DialDelay
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.78
    
    Former-commit-id: ff33d489f9598cbcfe600ebb658720360a58618d

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.77 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.78 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.77		%G%"
+literal|"@(#)sendmail.h	8.78		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -5206,6 +5206,17 @@ end_decl_stmt
 
 begin_comment
 comment|/* min delivery interval */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|time_t
+name|DialDelay
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* delay between dial-on-demand tries */
 end_comment
 
 begin_decl_stmt
