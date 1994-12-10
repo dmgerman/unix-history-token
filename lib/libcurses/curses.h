9a6e9adfec5808c9e199c3c9844ef5bbdac1a3bb
Andrey Chernov <ache@FreeBSD.org>
1994-12-10 23:02:33 +0000

    tputs: (char) -> (int)
    Former-commit-id: 1dfaa4f27dcc5561d580ce0d92b84ed0445bf5d1

--- lib/libcurses/curses.h
+++ lib/libcurses/curses.h
@@ -2924,7 +2924,7 @@ argument_list|(
 operator|*
 argument_list|)
 argument_list|(
-name|char
+name|int
 argument_list|)
 operator|)
 argument_list|)
@@ -2953,7 +2953,7 @@ argument_list|(
 operator|*
 argument_list|)
 argument_list|(
-name|char
+name|int
 argument_list|)
 operator|)
 argument_list|)
