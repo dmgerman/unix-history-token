4f8acb72d97ca80a6c7dc887f180279ca2d24945
Bruce Evans <bde@FreeBSD.org>
1994-12-11 23:20:50 +0000

    disklabel.h: Prepare for disk slices - more macros for handling disk device numbers, version of readdisklabel() without DOS goop. Clean up prototypes. Uniformize idempotency ifdef.
    diskslice.h:
    New file.
    
    dkbad.h:
    Define more magic numbers.
    Declare internalized version of dkbad struct and functions to operate on it.
    Uniformize idempotency ifdef.
    
    
    Former-commit-id: cc5382be884105592bef705cf7c919c1c6c796e6

--- sys/sys/dkbad.h
+++ sys/sys/dkbad.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)dkbad.h	8.1 (Berkeley) 6/2/93  * $Id: dkbad.h,v 1.2 1994/08/02 07:52:50 davidg Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)dkbad.h	8.1 (Berkeley) 6/2/93  * $Id: dkbad.h,v 1.3 1994/08/21 04:41:40 paul Exp $  */
 end_comment
 
 begin_ifndef
@@ -17,6 +17,39 @@ end_define
 
 begin_comment
 comment|/*  * Definitions needed to perform bad sector revectoring ala DEC STD 144.  *  * The bad sector information is located in the first 5 even numbered  * sectors of the last track of the disk pack.  There are five identical  * copies of the information, described by the dkbad structure.  *  * Replacement sectors are allocated starting with the first sector before  * the bad sector information and working backwards towards the beginning of  * the disk.  A maximum of 126 bad sectors are supported.  The position of  * the bad sector in the bad sector table determines which replacement sector  * it corresponds to.  *  * The bad sector information and replacement sectors are conventionally  * only accessible through the 'c' file system partition of the disk.  If  * that partition is used for a file system, the user is responsible for  * making sure that it does not overlap the bad sector information or any  * replacement sectors.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DKBAD_MAGIC
+value|0x4321
+end_define
+
+begin_comment
+comment|/* normal value for bt_flag */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DKBAD_MAXBAD
+value|126
+end_define
+
+begin_comment
+comment|/* maximum bad sectors supported */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DKBAD_NOCYL
+value|0xffff
+end_define
+
+begin_comment
+comment|/* cylinder to mark end of disk table */
 end_comment
 
 begin_struct
@@ -49,7 +82,7 @@ comment|/* track and sector number */
 block|}
 name|bt_bad
 index|[
-literal|126
+name|DKBAD_MAXBAD
 index|]
 struct|;
 block|}
@@ -84,10 +117,141 @@ name|CONT
 value|3
 end_define
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KERNEL
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/conf.h>
+end_include
+
+begin_define
+define|#
+directive|define
+name|DKBAD_NOSECT
+value|(-1)
+end_define
+
+begin_comment
+comment|/* sector to mark end of core table */
+end_comment
+
+begin_struct
+struct|struct
+name|dkbad_intern
+block|{
+name|daddr_t
+name|bi_maxspare
+decl_stmt|;
+comment|/* last spare sector */
+name|u_int
+name|bi_nbad
+decl_stmt|;
+comment|/* actual dimension of bi_badsect[] */
+name|long
+name|bi_bad
+index|[
+name|DKBAD_MAXBAD
+operator|+
+literal|1
+index|]
+decl_stmt|;
+comment|/* actually usually less */
+block|}
+struct|;
+end_struct
+
+begin_struct_decl
+struct_decl|struct
+name|disklabel
+struct_decl|;
+end_struct_decl
+
+begin_decl_stmt
+name|struct
+name|dkbad_intern
+modifier|*
+name|internbad144
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|dkbad
+operator|*
+name|btp
+operator|,
+expr|struct
+name|disklabel
+operator|*
+name|lp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|readbad144
+name|__P
+argument_list|(
+operator|(
+name|dev_t
+name|dev
+operator|,
+name|d_strategy_t
+operator|*
+name|strat
+operator|,
+expr|struct
+name|disklabel
+operator|*
+name|lp
+operator|,
+expr|struct
+name|dkbad
+operator|*
+name|btp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|daddr_t
+name|transbad144
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|dkbad_intern
+operator|*
+name|bip
+operator|,
+name|daddr_t
+name|blkno
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_SYS_DKBAD_H_ */
+end_comment
 
 end_unit
 
