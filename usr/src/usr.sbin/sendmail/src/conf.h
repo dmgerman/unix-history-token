7ce0ea1138f2dde602569a32b69d0338abf92453
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-12-11 22:04:59 -0800

    change Paragon e-mail address
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.126
    
    Former-commit-id: b43ca34b71c76edc7a1f8f0e525b270630df38bd

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.125 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.126 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1617,7 +1617,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* **  OSF/1 for Intel Paragon. ** **	Contributed by Jeff A. Earickson<jeff@ithaca.ccs.ornl.gov> **	of Oak Ridge National Lab. */
+comment|/* **  OSF/1 for Intel Paragon. ** **	Contributed by Jeff A. Earickson<jeff@ssd.intel.com> **	of Intel Scalable Systems Divison. */
 end_comment
 
 begin_ifdef
