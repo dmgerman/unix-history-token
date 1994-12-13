503fc0187b39411b2c2655baba1f4c2751c69681
Garrett Wollman <wollman@FreeBSD.org>
1994-12-13 22:32:17 +0000

    Update calls to rtalloc1() to match.
    Former-commit-id: 6b2e9b03d6fcf641303a6deb29f7d37f5ca0a966

--- sys/netccitt/if_x25subr.c
+++ sys/netccitt/if_x25subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_x25subr.c	8.1 (Berkeley) 6/10/93  * $Id$  */
+comment|/*  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_x25subr.c	8.1 (Berkeley) 6/10/93  * $Id: if_x25subr.c,v 1.2 1994/08/02 07:47:14 davidg Exp $  */
 end_comment
 
 begin_include
@@ -1179,6 +1179,8 @@ name|lcd_faddr
 argument_list|)
 argument_list|,
 literal|0
+argument_list|,
+literal|0UL
 argument_list|)
 expr_stmt|;
 if|if
@@ -1260,6 +1262,8 @@ operator|->
 name|rt_gateway
 argument_list|,
 literal|0
+argument_list|,
+literal|0UL
 argument_list|)
 expr_stmt|;
 name|rtfree
@@ -1512,6 +1516,8 @@ argument_list|(
 name|dst
 argument_list|,
 literal|1
+argument_list|,
+literal|0UL
 argument_list|)
 operator|)
 operator|==
@@ -2205,6 +2211,8 @@ operator|->
 name|rt_gateway
 argument_list|,
 literal|1
+argument_list|,
+literal|0UL
 argument_list|)
 else|:
 literal|0
@@ -2494,6 +2502,8 @@ argument_list|(
 name|sa
 argument_list|,
 literal|0
+argument_list|,
+literal|0UL
 argument_list|)
 operator|)
 operator|==
@@ -3420,6 +3430,8 @@ name|dst
 argument_list|)
 argument_list|,
 literal|0
+argument_list|,
+literal|0UL
 argument_list|)
 condition|)
 block|{
@@ -3449,6 +3461,8 @@ argument_list|(
 name|sa
 argument_list|,
 literal|1
+argument_list|,
+literal|0UL
 argument_list|)
 operator|)
 condition|)
@@ -3804,6 +3818,8 @@ argument_list|(
 name|dst
 argument_list|,
 literal|0
+argument_list|,
+literal|0UL
 argument_list|)
 decl_stmt|;
 specifier|register
