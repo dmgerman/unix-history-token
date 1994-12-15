40ae974ebf5bb61654c790a940172fb954148378
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-12-15 14:28:59 -0800

    compile glitch
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.54
    
    Former-commit-id: 57ad219969956985d57d82bfdb1e42671ca1b983

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)readcf.c	8.53 (Berkeley) %G%"
+literal|"@(#)readcf.c	8.54 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3623,7 +3623,7 @@ argument_list|,
 literal|"[TCP]"
 argument_list|)
 operator|==
-name|NULL
+literal|0
 operator|)
 condition|)
 name|m
