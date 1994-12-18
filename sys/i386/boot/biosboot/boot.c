e90206bffedf87131e113cdd4d2b3811e8c2a2d6
Bruce Evans <bde@FreeBSD.org>
1994-12-18 19:14:19 +0000

    Pass the slice number of the boot device to the kernel in the previously unused bitfields for the adaptor and the controller.  It should go in the bitfield for the partition but that would not be backwards compatible.
    Former-commit-id: 9d0bbba209d822bb996e54718e569b33e59ed3e6

--- sys/i386/boot/biosboot/boot.c
+++ sys/i386/boot/biosboot/boot.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, [92/04/03  16:51:14  rvb]  *	$Id: boot.c,v 1.26 1994/11/18 13:40:19 jkh Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *   * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *   *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *   * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, [92/04/03  16:51:14  rvb]  *	$Id: boot.c,v 1.27 1994/11/26 09:08:48 phk Exp $  */
 end_comment
 
 begin_comment
@@ -782,22 +782,27 @@ argument_list|,
 name|total
 argument_list|)
 expr_stmt|;
+comment|/* 	 * For backwards compatibility, use the previously-unused adaptor 	 * and controller bitfields to hold the slice number. 	 */
 name|bootdev
 operator|=
-operator|(
 name|MAKEBOOTDEV
 argument_list|(
 name|maj
 argument_list|,
-literal|0
+operator|(
+name|slice
+operator|>>
+literal|4
+operator|)
 argument_list|,
-literal|0
+name|slice
+operator|&
+literal|0xf
 argument_list|,
 name|unit
 argument_list|,
 name|part
 argument_list|)
-operator|)
 expr_stmt|;
 comment|/****************************************************************/
 comment|/* copy that first page and overwrite any BIOS variables	*/
