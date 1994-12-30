ba33a123b8a36b0a5038b92c98cce2a71f85b82c
Andrey Chernov <ache@FreeBSD.org>
1994-12-30 16:09:31 +0000

    Add check for writeable terminal Submitted by: mark@grondar.za (and slightly modified)
    Former-commit-id: 96e169111abeeb94f35b55b6cbfe8d754ff02ef1

--- usr.bin/talk/talk.c
+++ usr.bin/talk/talk.c
@@ -60,7 +60,7 @@ file|"talk.h"
 end_include
 
 begin_comment
-comment|/*  * talk:	A visual form of write. Using sockets, a two way   *		connection is set up between the two people talking.   *		With the aid of curses, the screen is split into two   *		windows, and each users text is added to the window,  *		one character at a time...  *  *		Written by Kipp Hickman  *		  *		Modified to run under 4.1a by Clem Cole and Peter Moore  *		Modified to run between hosts by Peter Moore, 8/19/82  *		Modified to run under 4.1c by Peter Moore 3/17/83  */
+comment|/*  * talk:	A visual form of write. Using sockets, a two way   *		connection is set up between the two people talking.   *		With the aid of curses, the screen is split into two   *		windows, and each users text is added to the window,  *		one character at a time...  *  *		Written by Kipp Hickman  *		  *		Modified to run under 4.1a by Clem Cole and Peter Moore  *		Modified to run between hosts by Peter Moore, 8/19/82  *		Modified to run under 4.1c by Peter Moore 3/17/83  *		Fixed to not run with unwriteable terminals MRVM 28/12/94  */
 end_comment
 
 begin_function
@@ -85,6 +85,9 @@ name|argc
 argument_list|,
 name|argv
 argument_list|)
+expr_stmt|;
+name|check_writeable
+argument_list|()
 expr_stmt|;
 name|init_display
 argument_list|()
