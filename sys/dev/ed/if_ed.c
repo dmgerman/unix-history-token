c3949b0fa75af0874e759ec54f9db1d1641b92cf
David Greenman <dg@FreeBSD.org>
1995-01-01 03:54:34 +0000

    Get rid of unused variable.
    Former-commit-id: 9580f854053d93214e5dc390607260a1274f8722

--- sys/dev/ed/if_ed.c
+++ sys/dev/ed/if_ed.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * $Id: if_ed.c,v 1.59 1994/12/22 21:56:03 wollman Exp $  */
+comment|/*  * Device driver for National Semiconductor DS8390/WD83C690 based ethernet  *   adapters. By David Greenman, 29-April-1993  *  * Copyright (C) 1993, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * Currently supports the Western Digital/SMC 8003 and 8013 series,  *   the SMC Elite Ultra (8216), the 3Com 3c503, the NE1000 and NE2000,  *   and a variety of similar clones.  *  * $Id: if_ed.c,v 1.60 1994/12/31 17:09:56 jkh Exp $  */
 end_comment
 
 begin_include
@@ -7975,10 +7975,6 @@ name|short
 name|amount
 decl_stmt|;
 block|{
-name|unsigned
-name|short
-name|tmp_amount
-decl_stmt|;
 comment|/* select page 0 registers */
 name|outb
 argument_list|(
@@ -7994,10 +7990,6 @@ name|ED_CR_STA
 argument_list|)
 expr_stmt|;
 comment|/* round up to a word */
-name|tmp_amount
-operator|=
-name|amount
-expr_stmt|;
 if|if
 condition|(
 name|amount
