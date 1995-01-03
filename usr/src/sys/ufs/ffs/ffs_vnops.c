65002a469f9ee344cf451121c351a5df828eb292
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-01-03 07:23:58 -0800

    make sure to free correct allocation type if ffs_reclaim (from mycroft)
    SCCS-vsn: sys/ufs/ffs/ffs_vnops.c 8.11
    
    Former-commit-id: 822382e0dd9fd4667184ab0af86f9e76c54eef46

--- usr/src/sys/ufs/ffs/ffs_vnops.c
+++ usr/src/sys/ufs/ffs/ffs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	8.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vnops.c	8.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1751,6 +1751,21 @@ name|vp
 operator|->
 name|v_data
 argument_list|,
+name|VFSTOUFS
+argument_list|(
+name|vp
+operator|->
+name|v_mount
+argument_list|)
+operator|->
+name|um_devvp
+operator|->
+name|v_tag
+operator|==
+name|VT_MFS
+condition|?
+name|M_MFSNODE
+else|:
 name|M_FFSNODE
 argument_list|)
 expr_stmt|;
