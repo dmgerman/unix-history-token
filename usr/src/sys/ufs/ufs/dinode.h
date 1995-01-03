1c4a15b26934d3e5dcb35a32d023ab88cd67744c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-01-03 07:40:37 -0800

    quad => int64_t
    SCCS-vsn: sys/ufs/ufs/dinode.h 8.7
    
    Former-commit-id: 6b65fc5f81d72f545732b17fb0a7b5fa74912dfa

--- usr/src/sys/ufs/ufs/dinode.h
+++ usr/src/sys/ufs/ufs/dinode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dinode.h	8.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dinode.h	8.7 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -79,7 +79,7 @@ comment|/*   4: Lfs: inode number. */
 block|}
 name|di_u
 union|;
-name|u_quad_t
+name|u_int64_t
 name|di_size
 decl_stmt|;
 comment|/*   8: File byte count. */
