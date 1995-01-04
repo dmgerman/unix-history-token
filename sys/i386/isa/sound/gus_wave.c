b0af6a2aec5e3ca59cb2a5d3dff2a38967d5fe4d
Paul Traina <pst@FreeBSD.org>
1995-01-04 20:07:27 +0000

    Fix the GUS driver so that it will initialize properly from power-on... no more DOS boots to start it up.
    Simply did a localized nuke of the OUTB macro in this file.  This is
    a kludge, since it seems it may actually be necessary in other GUS
    files (tbd).
    
    Thanks to: Amancio Hasty & Ken Hornstein
    
    
    Former-commit-id: 5fb3916cf9273af4b06853f2e5543183e9c2d655

--- sys/i386/isa/sound/gus_wave.c
+++ sys/i386/isa/sound/gus_wave.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * sound/gus_wave.c  *  * Driver for the Gravis UltraSound wave table synth.  *  * Copyright by Hannu Savolainen 1993, 1994  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met: 1. Redistributions of source code must retain the above copyright  * notice, this list of conditions and the following disclaimer. 2.  * Redistributions in binary form must reproduce the above copyright notice,  * this list of conditions and the following disclaimer in the documentation  * and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: gus_wave.c,v 1.9 1994/08/02 07:40:02 davidg Exp $  */
+comment|/*  * sound/gus_wave.c  *  * Driver for the Gravis UltraSound wave table synth.  *  * Copyright by Hannu Savolainen 1993, 1994  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met: 1. Redistributions of source code must retain the above copyright  * notice, this list of conditions and the following disclaimer. 2.  * Redistributions in binary form must reproduce the above copyright notice,  * this list of conditions and the following disclaimer in the documentation  * and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: gus_wave.c,v 1.10 1994/10/01 02:16:42 swallace Exp $  */
 end_comment
 
 begin_include
@@ -42,6 +42,24 @@ include|#
 directive|include
 file|"gus_hw.h"
 end_include
+
+begin_undef
+undef|#
+directive|undef
+name|OUTB
+end_undef
+
+begin_define
+define|#
+directive|define
+name|OUTB
+parameter_list|(
+name|val
+parameter_list|,
+name|port
+parameter_list|)
+value|outb(port, val)
+end_define
 
 begin_if
 if|#
