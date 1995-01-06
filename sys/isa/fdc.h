528a181d31995f10fecaf86bc26387153d708868
Joerg Wunsch <joerg@FreeBSD.org>
1995-01-06 15:20:00 +0000

    Peter's work to work around one of the most annoying bugs in the floppy driver (or in the hardware?).  It turned out to be caused by spurious interrupts, right after an FDC reset.
    Also major cleanup in the low-level structure, there are now functions
    performing error-checks for the FDC I/O.
    
    Submitted by:	(mostly) Peter Dufault <dufault@FreeBSD.org>
    
    
    Former-commit-id: 33bb8314fc6af012008972534319eb4d24c76433

--- sys/isa/fdc.h
+++ sys/isa/fdc.h
@@ -1,7 +1,25 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)fd.c	7.4 (Berkeley) 5/25/91  *	$Id: fdc.h,v 1.3 1994/09/17 16:56:07 davidg Exp $  *  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)fd.c	7.4 (Berkeley) 5/25/91  *	$Id: fdc.h,v 1.4 1994/10/10 01:12:26 phk Exp $  *  */
 end_comment
+
+begin_enum
+enum|enum
+name|fdc_type
+block|{
+name|FDC_NE765
+block|,
+name|FDC_I82077
+block|,
+name|FDC_NE72065
+block|,
+name|FDC_UNKNOWN
+init|=
+operator|-
+literal|1
+block|}
+enum|;
+end_enum
 
 begin_comment
 comment|/***********************************************************************\ * Per controller structure.						* \***********************************************************************/
@@ -36,6 +54,10 @@ define|#
 directive|define
 name|FDC_TAPE_BUSY
 value|0x04
+define|#
+directive|define
+name|FDC_STAT_VALID
+value|0x08
 name|struct
 name|fd_data
 modifier|*
@@ -62,11 +84,20 @@ literal|7
 index|]
 decl_stmt|;
 comment|/* copy of the registers */
+name|enum
+name|fdc_type
+name|fdct
+decl_stmt|;
+comment|/* chip version of FDC */
+name|int
+name|fdc_errs
+decl_stmt|;
+comment|/* number of logged errors */
 name|struct
 name|buf
 name|head
 decl_stmt|;
-comment|/* Head of buf chain      */
+comment|/* Head of buf chain	  */
 block|}
 struct|;
 end_struct
@@ -111,6 +142,14 @@ name|struct
 name|fdc_data
 modifier|*
 name|fdc_p
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+name|enum
+name|fdc_type
+name|fdc_t
 typedef|;
 end_typedef
 
