f940457a9aa90c5ee9ee04e9aeb98bcf20b5c7be
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-01-07 01:05:06 -0800

    reset IN_RENAME flag if doing directory rename and it fails (from mycroft)
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 8.16
    
    Former-commit-id: 37e5c5661874bde2addca667f59a5e9ad04ec238

--- usr/src/sys/ufs/ufs/ufs_vnops.c
+++ usr/src/sys/ufs/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	8.15 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	8.16 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -5756,6 +5756,17 @@ argument_list|)
 expr_stmt|;
 name|out
 label|:
+if|if
+condition|(
+name|doingdirectory
+condition|)
+name|ip
+operator|->
+name|i_flag
+operator|&=
+operator|~
+name|IN_RENAME
+expr_stmt|;
 if|if
 condition|(
 name|VOP_LOCK
