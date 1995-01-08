0b30e192905fe46269cad2228289415e2917c138
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-01-07 19:18:43 -0800

    enable load average for AIX3
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.129
    
    Former-commit-id: 573435c6a192ac4f9a9319bcfaa9f05908da3516

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.128 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.129 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -679,6 +679,13 @@ end_define
 begin_comment
 comment|/* pad process title with nulls */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|LA_TYPE
+value|LA_INT
+end_define
 
 begin_endif
 endif|#
