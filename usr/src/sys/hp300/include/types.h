1b930ae8363122afb720ef1af6d2e045509a5b51
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-01-10 02:26:50 -0800

    add register_t for impending syscall args changes
    SCCS-vsn: sys/hp300/include/types.h 8.4
    
    Former-commit-id: 3a439df7f6e06e71e97081c43458ff034a9eff68

--- usr/src/sys/hp300/include/types.h
+++ usr/src/sys/hp300/include/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -150,6 +150,13 @@ name|unsigned
 name|long
 name|long
 name|u_int64_t
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+name|int32_t
+name|register_t
 typedef|;
 end_typedef
 
