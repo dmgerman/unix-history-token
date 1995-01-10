21140444584d96a12151f7c539d83b2e22870154
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-01-10 01:52:28 -0800

    pk_control takes a u_long command (64-bit changes
    SCCS-vsn: sys/netccitt/pk_usrreq.c 8.2
    
    Former-commit-id: 556ade2f2ad3b033c3b93c9930fd7a99d0d6b123

--- usr/src/sys/netccitt/pk_usrreq.c
+++ usr/src/sys/netccitt/pk_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (C) Computer Science Department IV,   * 		 University of Erlangen-Nuremberg, Germany, 1992  * Copyright (c) 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by the  * Laboratory for Computation Vision and the Computer Science Department  * of the the University of British Columbia and the Computer Science  * Department (IV) of the University of Erlangen-Nuremberg, Germany.  *  * %sccs.include.redist.c%  *  *	@(#)pk_usrreq.c	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) University of British Columbia, 1984  * Copyright (C) Computer Science Department IV,   * 		 University of Erlangen-Nuremberg, Germany, 1992  * Copyright (c) 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by the  * Laboratory for Computation Vision and the Computer Science Department  * of the the University of British Columbia and the Computer Science  * Department (IV) of the University of Erlangen-Nuremberg, Germany.  *  * %sccs.include.redist.c%  *  *	@(#)pk_usrreq.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1105,7 +1105,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
+name|u_long
 name|cmd
 decl_stmt|;
 end_decl_stmt
