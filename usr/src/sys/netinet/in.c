e2fda950c4fbe946df2d8cb47f5f1fdf70cf9624
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-01-10 02:19:45 -0800

    ioctl cmd arg is u_long
    SCCS-vsn: sys/netinet/in_var.h 8.2
    SCCS-vsn: sys/netinet/in.c 8.4
    SCCS-vsn: sys/netinet/tcp_usrreq.c 8.3
    SCCS-vsn: sys/netinet/ip_var.h 8.2
    SCCS-vsn: sys/netinet/udp_usrreq.c 8.5
    
    Former-commit-id: 9642b78759d2e367ea12b8581b41a8c42dc9cf69

--- usr/src/sys/netinet/in.c
+++ usr/src/sys/netinet/in.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)in.c	8.3 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)in.c	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -596,7 +596,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
+name|u_long
 name|cmd
 decl_stmt|;
 end_decl_stmt
