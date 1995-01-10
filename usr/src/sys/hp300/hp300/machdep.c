e77fc5772f934d7a6654b40dd67de9de015182e9
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-01-10 02:28:18 -0800

    signal handler code is u_long, pull in vm_extern.h and fix grow() args.
    SCCS-vsn: sys/hp300/hp300/machdep.c 8.11
    
    Former-commit-id: 4c0061d2c9df09c0d9c13d844c1e5cccbd24205a

--- usr/src/sys/hp300/hp300/machdep.c
+++ usr/src/sys/hp300/hp300/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.74 92/12/20$  *  *	@(#)machdep.c	8.10 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machdep.c 1.74 92/12/20$  *  *	@(#)machdep.c	8.11 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -215,6 +215,12 @@ end_define
 begin_comment
 comment|/* XXX - from cmap.h */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<vm/vm_extern.h>
+end_include
 
 begin_include
 include|#
@@ -2562,7 +2568,7 @@ name|sig
 decl_stmt|,
 name|mask
 decl_stmt|;
-name|unsigned
+name|u_long
 name|code
 decl_stmt|;
 block|{
@@ -2795,7 +2801,7 @@ argument_list|(
 name|p
 argument_list|,
 operator|(
-name|unsigned
+name|vm_offset_t
 operator|)
 name|fp
 argument_list|)
