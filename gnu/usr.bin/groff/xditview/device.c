147bf9b41be0a0954cce45ddf98fce13d2c43558
Paul Traina <pst@FreeBSD.org>
1995-01-15 08:30:25 +0000

    Layer in all FreeBSD patches
    Former-commit-id: 549ac9ab513088e87c2b3edce11020fd95e49cb2

--- gnu/usr.bin/groff/xditview/device.c
+++ gnu/usr.bin/groff/xditview/device.c
@@ -124,6 +124,12 @@ begin_comment
 comment|/* Minimum and maximum values a `signed int' can hold.  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|INT_MIN
+end_ifndef
+
 begin_define
 define|#
 directive|define
@@ -131,12 +137,28 @@ name|INT_MIN
 value|(-INT_MAX-1)
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|INT_MAX
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|INT_MAX
 value|2147483647
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
