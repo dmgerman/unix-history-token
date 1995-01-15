d18f13b3f675cfed717da5ab098285be99101a2f
David Greenman <dg@FreeBSD.org>
1995-01-15 09:35:58 +0000

    Attempt to close a hole using splhigh/splx. There still appears to be a serious one in the same area that I don't have time to fix.
    Former-commit-id: 27c1685fc9047feed582a9fc5eb0c05fb9628aac

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.19 1995/01/10 09:20:34 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.20 1995/01/11 01:53:18 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -5512,6 +5512,11 @@ argument_list|)
 expr_stmt|;
 continue|continue;
 block|}
+name|s
+operator|=
+name|splhigh
+argument_list|()
+expr_stmt|;
 name|m
 operator|=
 name|vm_page_lookup
@@ -5527,6 +5532,11 @@ operator|!
 name|m
 condition|)
 block|{
+name|splx
+argument_list|(
+name|s
+argument_list|)
+expr_stmt|;
 name|m
 operator|=
 name|vm_page_alloc
@@ -5683,6 +5693,11 @@ name|bufferdestroyed
 init|=
 literal|0
 decl_stmt|;
+name|splx
+argument_list|(
+name|s
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|j
@@ -5854,6 +5869,11 @@ block|{
 name|int
 name|j
 decl_stmt|;
+name|splx
+argument_list|(
+name|s
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|j
@@ -6016,6 +6036,11 @@ operator|->
 name|flags
 operator||=
 name|PG_BUSY
+expr_stmt|;
+name|splx
+argument_list|(
+name|s
+argument_list|)
 expr_stmt|;
 block|}
 name|bp
