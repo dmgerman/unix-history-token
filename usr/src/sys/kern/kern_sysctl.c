b69275ab56ad09c9d96c39f06281813a33c6fee6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-01-18 23:27:01 -0800

    always lock except when doing vnodes
    SCCS-vsn: sys/kern/kern_sysctl.c 8.6
    
    Former-commit-id: 7f61ae90d35ba2c6f4390fa4f0d7c4418bd9e54a

--- usr/src/sys/kern/kern_sysctl.c
+++ usr/src/sys/kern/kern_sysctl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)kern_sysctl.c	8.5 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)kern_sysctl.c	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -343,7 +343,7 @@ name|name
 index|[
 literal|2
 index|]
-operator|!=
+operator|==
 name|KERN_VNODE
 condition|)
 comment|/* XXX */
