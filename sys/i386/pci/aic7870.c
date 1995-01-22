8f53ad7ae17e2c8b447a2711ee708fe006a14a56
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-01-22 00:47:50 +0000

    Change the string returned in the aic7870 motherboard probe case.  Change #define to a more appropriate name.
    Former-commit-id: cfc3a64a3d4113fa801e51ba8a66da06861e9e13

--- sys/i386/pci/aic7870.c
+++ sys/i386/pci/aic7870.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  *      294X and aic7870 motherboard SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id$  */
+comment|/*  * Product specific probe and attach routines for:  *      294X and aic7870 motherboard SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7870.c,v 1.2 1995/01/16 16:31:57 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -70,7 +70,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|PCI_DEVICE_ID_ADAPTEC_2940_MB
+name|PCI_DEVICE_ID_ADAPTEC_AIC7870
 value|0x70789004ul
 end_define
 
@@ -159,12 +159,11 @@ operator|)
 return|;
 break|break;
 case|case
-name|PCI_DEVICE_ID_ADAPTEC_2940_MB
+name|PCI_DEVICE_ID_ADAPTEC_AIC7870
 case|:
 return|return
 operator|(
 literal|"Adaptec aic7870 SCSI host adapter"
-literal|": on Motherboard"
 operator|)
 return|;
 break|break;
