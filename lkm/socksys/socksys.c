11501befbe9e6e7c747c7e63ca82b8c2a278e27d
Bruce Evans <bde@FreeBSD.org>
1995-01-24 04:22:19 +0000

    Remove typedefs that are now in <sys/conf.h>.
    Former-commit-id: 4f46f5ed62249ac52be275c7a8edca9000a576c6

--- lkm/socksys/socksys.c
+++ lkm/socksys/socksys.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer   *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1994 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer   *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: socksys.c,v 1.1 1994/10/16 20:38:50 sos Exp $  */
 end_comment
 
 begin_include
@@ -44,126 +44,6 @@ include|#
 directive|include
 file|<sys/errno.h>
 end_include
-
-begin_comment
-comment|/* typedefs from sys/i386/i386/conf.c */
-end_comment
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_open_t
-name|__P
-typedef|((
-name|dev_t
-typedef|,
-name|int
-typedef|,
-name|int
-typedef|, struct
-name|proc
-modifier|*
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_close_t
-name|__P
-typedef|((
-name|dev_t
-typedef|,
-name|int
-typedef|,
-name|int
-typedef|, struct
-name|proc
-modifier|*
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_ioctl_t
-name|__P
-typedef|((
-name|dev_t
-typedef|,
-name|int
-typedef|,
-name|caddr_t
-typedef|,
-name|int
-typedef|, struct
-name|proc
-modifier|*
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_rdwr_t
-name|__P
-typedef|((
-name|dev_t
-typedef|, struct
-name|uio
-modifier|*
-typedef|,
-name|int
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_stop_t
-name|__P
-typedef|((struct
-name|tty
-modifier|*
-typedef|,
-name|int
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_reset_t
-name|__P
-typedef|((
-name|int
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_select_t
-name|__P
-typedef|((
-name|dev_t
-typedef|,
-name|int
-typedef|, struct
-name|proc
-modifier|*
-typedef|));
-end_typedef
-
-begin_typedef
-typedef|typedef
-name|int
-name|d_mmap_t
-name|__P
-typedef|((
-comment|/* XXX */
-typedef|));
-end_typedef
 
 begin_function_decl
 name|int
