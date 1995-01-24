d89a8c90f305217e7027a54e18a682e3030d893a
Peter Dufault <dufault@FreeBSD.org>
1995-01-24 12:04:56 +0000

    1. Reject obviously broken CDB command lengths 2. Use "uprintf" (instead of printf) to log the "tape not ready" message
    Former-commit-id: 9c9b242b0ebe531dfe96cb1dcf08f2beeb08488f

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.23 1994/12/16 06:03:26 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.24 1995/01/08 13:38:36 dufault Exp $  */
 end_comment
 
 begin_comment
@@ -2728,7 +2728,7 @@ argument_list|)
 operator|)
 condition|)
 block|{
-name|printf
+name|uprintf
 argument_list|(
 literal|"st%d: not ready\n"
 argument_list|,
