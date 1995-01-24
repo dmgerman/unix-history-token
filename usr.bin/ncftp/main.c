9629be815822aefd830cabe36fd3f5fa568ea564
Andrey Chernov <ache@FreeBSD.org>
1995-01-24 00:37:11 +0000

    Upgrade. My recent fix applied by author.
    Former-commit-id: 22a288e5ccc5f697663c94be94126c6639c4dd5d

--- usr.bin/ncftp/main.c
+++ usr.bin/ncftp/main.c
@@ -13,7 +13,7 @@ begin_define
 define|#
 directive|define
 name|FTP_VERSION
-value|"1.9.1 (January 1, 1995)"
+value|"1.9.2 (January 20, 1995)"
 end_define
 
 begin_comment
@@ -1786,16 +1786,24 @@ name|eventnumber
 operator|=
 literal|0L
 expr_stmt|;
-operator|(
-name|void
-operator|)
+if|if
+condition|(
 name|cmdOpen
 argument_list|(
 name|margc
 argument_list|,
 name|margv
 argument_list|)
+operator|!=
+name|NOERR
+condition|)
+block|{
+name|exit
+argument_list|(
+literal|1
+argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|eventnumber
 operator|=
