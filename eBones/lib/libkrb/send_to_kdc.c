8133337b792e9d25c4370d2eca8cabde963617ba
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-01-25 06:37:33 +0000

    Less expensive fix for the freeing of uninitialized fields in the hostdata struct.  Simply test the "no_host" variable, which if true implies that the hostdata contains bogus data, and act accordingly.
    Former-commit-id: 876164c8e5979cb38a37c07903aeb3d2155dbc69

--- eBones/lib/libkrb/send_to_kdc.c
+++ eBones/lib/libkrb/send_to_kdc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright 1987, 1988 by the Massachusetts Institute of Technology.  * For copying and distribution information, please see the file  *<Copyright.MIT>.  *  *	from: send_to_kdc.c,v 4.20 90/01/02 13:40:37 jtkohl Exp $  *	$Id: send_to_kdc.c,v 1.1.1.1 1994/09/30 14:50:03 csgr Exp $  */
+comment|/*  * Copyright 1987, 1988 by the Massachusetts Institute of Technology.  * For copying and distribution information, please see the file  *<Copyright.MIT>.  *  *	from: send_to_kdc.c,v 4.20 90/01/02 13:40:37 jtkohl Exp $  *	$Id: send_to_kdc.c,v 1.2 1995/01/25 05:40:00 gibbs Exp $  */
 end_comment
 
 begin_ifndef
@@ -441,17 +441,6 @@ comment|/*errno */
 name|SKDC_CANT
 operator|)
 return|;
-name|bzero
-argument_list|(
-name|hostlist
-argument_list|,
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|hostent
-argument_list|)
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 operator|(
@@ -1007,6 +996,12 @@ condition|(
 name|hostlist
 condition|)
 block|{
+if|if
+condition|(
+operator|!
+name|no_host
+condition|)
+block|{
 specifier|register
 name|struct
 name|hostent
@@ -1104,6 +1099,7 @@ block|}
 endif|#
 directive|endif
 comment|/* ULTRIX022 || SunOS */
+block|}
 name|free
 argument_list|(
 operator|(
