3d0e15f3337bcd862631401bd9141fce1af6df3e
David Greenman <dg@FreeBSD.org>
1995-01-26 03:34:31 +0000

    Fix problem with freeing busy pages reported by Nick Sayer.
    Submitted by:	 John Dyson
    
    
    Former-commit-id: 29daae7e3e3ee67d0a1d96b87e4d2483e4b03299

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.24 1995/01/21 06:32:26 ache Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.25 1995/01/24 10:00:43 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -4580,7 +4580,7 @@ operator|=
 name|splhigh
 argument_list|()
 expr_stmt|;
-if|if
+while|while
 condition|(
 operator|(
 name|m
@@ -4663,6 +4663,15 @@ argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|m
+operator|->
+name|valid
+operator|==
+literal|0
+condition|)
+block|{
 name|pmap_page_protect
 argument_list|(
 name|VM_PAGE_TO_PHYS
@@ -4678,6 +4687,7 @@ argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|bp
 operator|->
