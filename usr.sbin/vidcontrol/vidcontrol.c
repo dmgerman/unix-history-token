0afa838e29c7a19da331c6add4d2fb9d363255dc
Soren Schmidt <sos@FreeBSD.org>
1995-01-30 21:44:07 +0000

    Added 80x30 & 80x60 experimental modes officially.
    Former-commit-id: 08db92d85ba8b080883bfa5911efe14be552d9b3

--- usr.sbin/vidcontrol/vidcontrol.c
+++ usr.sbin/vidcontrol/vidcontrol.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: vidcontrol.c,v 1.4 1995/01/12 11:43:52 sos Exp $  */
+comment|/*-  * Copyright (c) 1994-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: vidcontrol.c,v 1.5 1995/01/28 22:18:05 sos Exp $  */
 end_comment
 
 begin_include
@@ -90,10 +90,6 @@ end_decl_stmt
 begin_decl_stmt
 name|int
 name|number
-decl_stmt|,
-name|verbose
-init|=
-literal|0
 decl_stmt|;
 end_decl_stmt
 
@@ -1988,6 +1984,7 @@ argument_list|,
 literal|"Usage: vidcontrol mode             (available modes: VGA_40x25, VGA_80x25,\n"
 literal|"                                                     VGA_80x50, VGA_320x200,\n"
 literal|"                                                     EGA_80x25, EGA_80x43)\n"
+literal|"                                     (experimental)  VGA_80x30, VGA_80x60)\n"
 literal|"                  show               (show available colors)\n"
 literal|"                  fgcol bgcol        (set fore-& background colors)\n"
 literal|"                  -r fgcol bgcol     (set reverse fore-& background colors)\n"
@@ -2076,7 +2073,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"b:c:df:l:Lr:s:t:vx"
+literal|"b:c:df:l:Lr:s:t:x"
 argument_list|)
 operator|)
 operator|!=
@@ -2180,14 +2177,6 @@ name|set_screensaver_timeout
 argument_list|(
 name|optarg
 argument_list|)
-expr_stmt|;
-break|break;
-case|case
-literal|'v'
-case|:
-name|verbose
-operator|=
-literal|1
 expr_stmt|;
 break|break;
 case|case
