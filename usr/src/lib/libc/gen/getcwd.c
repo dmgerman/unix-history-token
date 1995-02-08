db806637e08ab9880459da0b02b3b7b2054b2a4a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-02-07 16:49:58 -0800

    wrong termination statement, could cause the environment to be trashed
    SCCS-vsn: lib/libc/gen/getcwd.c 8.5
    
    Former-commit-id: cca9fdd966a0d4de3cacdac280153fa8d8172226

--- usr/src/lib/libc/gen/getcwd.c
+++ usr/src/lib/libc/gen/getcwd.c
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)getcwd.c	8.4 (Berkeley) %G%"
+literal|"@(#)getcwd.c	8.5 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -284,7 +284,7 @@ argument_list|,
 name|pwdlen
 argument_list|)
 expr_stmt|;
-name|pwd
+name|pt
 index|[
 name|pwdlen
 index|]
