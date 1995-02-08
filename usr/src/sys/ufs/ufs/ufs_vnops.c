0f68317c2de50a84b266c9e7e06640cc4451c6fe
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-02-07 18:52:33 -0800

    give error when user tries to set high 16-bits of inode flags word
    SCCS-vsn: sys/ufs/ufs/ufs_vnops.c 8.18
    
    Former-commit-id: 83ed316f82a14a24048c7f14f0bdcd83ee68fbce

--- usr/src/sys/ufs/ufs/ufs_vnops.c
+++ usr/src/sys/ufs/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	8.17 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vnops.c	8.18 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1456,6 +1456,18 @@ name|SF_IMMUTABLE
 operator||
 name|SF_APPEND
 operator|)
+operator|||
+operator|(
+name|vap
+operator|->
+name|va_flags
+operator|&
+name|UF_SETTABLE
+operator|)
+operator|!=
+name|vap
+operator|->
+name|va_flags
 condition|)
 return|return
 operator|(
