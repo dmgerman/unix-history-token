cf931d577895e669afdc2befb20140d9fdd9428b
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-02-09 11:59:40 +0000

    The whole NEW_CONF_C_SYNTAX was bogus; David's right, it can't be optional at all.  Make it non-optional. Submitted by:	davidg
    Former-commit-id: a84521309992eea7dae31035b2ebc239751b8b5b

--- sys/i386/i386/conf.c
+++ sys/i386/i386/conf.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright (c) UNIX System Laboratories, Inc.  All or some portions
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.58 1995/02/06 23:20:03 jkh Exp $  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.59 1995/02/09 11:13:09 jkh Exp $  */
 end_comment
 
 begin_include
@@ -4419,24 +4419,8 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * This stuff from Heikki is kind of bizarre, and I'm not sure how I feel  * about it yet (I think I like it about as much as I can like anything in  * here), but I'm intrigued enough for now to enable it by default. -jkh  *  * If you want to use the Cyclades driver, you MUST have this on for now  * (or convert Heikki's syntax to the old one).  */
+comment|/*  * This stuff from Heikki is kind of bizarre, and I'm not sure how I feel  * about it yet (I think I like it about as much as I can like anything in  * here). -jkh  */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|NEW_CONF_C_SYNTAX
-value|yes
-end_define
-
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|NEW_CONF_C_SYNTAX
-argument_list|)
-end_if
 
 begin_ifndef
 ifndef|#
@@ -4582,6 +4566,10 @@ endif|#
 directive|endif
 end_endif
 
+begin_comment
+comment|/* Cyclades serial driver */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -4595,15 +4583,6 @@ name|cy
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* NEW_CONF_C_SYNTAX */
-end_comment
 
 begin_comment
 comment|/* open, close, read, write, ioctl, stop, reset, ttys, select, mmap, strat */
@@ -5897,12 +5876,6 @@ block|,
 name|NULL
 block|}
 block|,
-if|#
-directive|if
-name|defined
-argument_list|(
-name|NEW_CONF_C_SYNTAX
-argument_list|)
 name|cdev_tty_init
 argument_list|(
 name|NCY
@@ -5911,8 +5884,6 @@ name|cy
 argument_list|)
 block|,
 comment|/* cyclades */
-endif|#
-directive|endif
 block|{
 name|sscopen
 block|,
