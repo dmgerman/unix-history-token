0eacf955d055e0f4eb7f9acb10d9c32f101efcb2
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-09 18:42:41 -0800

    casts to quiet compiler.
    SCCS-vsn: sys/netiso/esis.c 8.2
    
    Former-commit-id: de17e3923786465d0d07498b0bdd97d837cc081d

--- usr/src/sys/netiso/esis.c
+++ usr/src/sys/netiso/esis.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)esis.c	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)esis.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -4221,6 +4221,11 @@ name|rcb_socket
 operator|->
 name|so_rcv
 argument_list|,
+operator|(
+expr|struct
+name|sockaddr
+operator|*
+operator|)
 operator|&
 name|esis_dl
 argument_list|,
@@ -4280,6 +4285,11 @@ name|rcb_socket
 operator|->
 name|so_rcv
 argument_list|,
+operator|(
+expr|struct
+name|sockaddr
+operator|*
+operator|)
 operator|&
 name|esis_dl
 argument_list|,
