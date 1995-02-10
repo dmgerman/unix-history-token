37b941c3a02eafa453321cedc2c58d89ff4e1949
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-09 18:45:00 -0800

    don't check for <0 on unsigneds.  (kill comp. warning.)
    SCCS-vsn: sys/netiso/tp_output.c 8.2
    
    Former-commit-id: 4fd347e0b1f7cbf9a03507461789b94d5d9bacb4

--- usr/src/sys/netiso/tp_output.c
+++ usr/src/sys/netiso/tp_output.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tp_output.c	8.1 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)tp_output.c	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -330,21 +330,11 @@ expr_stmt|;
 name|ENDDEBUG
 if|if
 condition|(
-operator|(
-name|param
-operator|->
-name|p_netservice
-operator|<
-literal|0
-operator|)
-operator|||
-operator|(
 name|param
 operator|->
 name|p_netservice
 operator|>
 name|TP_MAX_NETSERVICES
-operator|)
 condition|)
 block|{
 name|error
