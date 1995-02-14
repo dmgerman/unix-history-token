152010816effce29f6f8462978665c377754d19e
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-02-14 22:33:10 +0000

    Julian E. says the code is out of date, but I can at least make the includes look right until we get a chance to fix the rest of this.
    Former-commit-id: 273d874983c2b0a02228a4406941f8168145a06f

--- sys/gnu/scsi/nic5000.c
+++ sys/gnu/scsi/nic5000.c
@@ -5,12 +5,12 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"@(#)$Id: nic5000.c,v 1.1 1995/01/25 14:06:18 jkr Exp jkr $"
+literal|"@(#)$Id: nic5000.c,v 1.1 1995/02/14 15:00:37 jkh Exp $"
 decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*******************************************************************************  *  II - Version 0.1 $Revision: 1.1 $   $State: Exp $  *  * Copyright 1994 Dietmar Friede  *******************************************************************************  * Bug reports, patches, comments, suggestions should be sent to:  *  *	jkr@saarlink.de or jkrause@guug.de  *  *******************************************************************************  * $Log: nic5000.c,v $  *  ******************************************************************************/
+comment|/*******************************************************************************  *  II - Version 0.1 $Revision: 1.1 $   $State: Exp $  *  * Copyright 1994 Dietmar Friede  *******************************************************************************  * Bug reports, patches, comments, suggestions should be sent to:  *  *	jkr@saarlink.de or jkrause@guug.de  *  *******************************************************************************  * $Log: nic5000.c,v $  * Revision 1.1  1995/02/14  15:00:37  jkh  * An ISDN driver that supports the EDSS1 and the 1TR6 ISDN interfaces.  * EDSS1 is the "Euro-ISDN", 1TR6 is the soon obsolete german ISDN Interface.  * Obtained from: Dietmar Friede<dfriede@drnhh.neuhaus.de> and  * 	Juergen Krause<jkr@saarlink.de>  *  * This is only one part - the rest to follow in a couple of hours.  * This part is a benign import, since it doesn't affect anything else.  *  *  ******************************************************************************/
 end_comment
 
 begin_comment
@@ -97,19 +97,19 @@ end_include
 begin_include
 include|#
 directive|include
-file|"isdn/isdn_ioctl.h"
+file|"gnu/isdn/isdn_ioctl.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"i386/isa/niccyreg.h"
+file|"gnu/i386/isa/niccyreg.h"
 end_include
 
 begin_include
 include|#
 directive|include
-file|"scsi/scsi_nic.h"
+file|"gnu/scsi/scsi_nic.h"
 end_include
 
 begin_comment
