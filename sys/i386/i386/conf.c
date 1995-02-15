b2843e3c964e6e4149f2ee8f2a6574970caa2e64
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-02-15 04:51:10 +0000

    Boy, no sooner do I clean it up then dirty it again!  Clean up the enxio stuff I should have caught earlier.
    Former-commit-id: 9a21627fecd1c2a4811144a5763e4ef4bfbb9fb4

--- sys/i386/i386/conf.c
+++ sys/i386/i386/conf.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright (c) UNIX System Laboratories, Inc.  All or some portions
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.62 1995/02/14 21:16:43 ugen Exp $  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)conf.c	5.8 (Berkeley) 5/12/91  *	$Id: conf.c,v 1.63 1995/02/14 22:36:49 jkh Exp $  */
 end_comment
 
 begin_include
@@ -4697,7 +4697,7 @@ begin_define
 define|#
 directive|define
 name|ityreset
-value|enxio
+value|nxreset
 end_define
 
 begin_decl_stmt
@@ -4718,56 +4718,56 @@ begin_define
 define|#
 directive|define
 name|ityopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|ityclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|ityread
-value|enxio
+value|nxread
 end_define
 
 begin_define
 define|#
 directive|define
 name|itywrite
-value|enxio
+value|nxwrite
 end_define
 
 begin_define
 define|#
 directive|define
 name|ityioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_define
 define|#
 directive|define
 name|ityreset
-value|enxio
+value|nxreset
 end_define
 
 begin_define
 define|#
 directive|define
 name|ityselect
-value|enxio
+value|nxselect
 end_define
 
 begin_define
 define|#
 directive|define
 name|ity_tty
-value|NULL
+value|(struct tty *)NULL
 end_define
 
 begin_endif
@@ -4811,21 +4811,21 @@ begin_define
 define|#
 directive|define
 name|nicopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|nicclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|nicioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_endif
@@ -4869,21 +4869,21 @@ begin_define
 define|#
 directive|define
 name|nnicopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|nnicclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|nnicioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_endif
@@ -4927,21 +4927,21 @@ begin_define
 define|#
 directive|define
 name|snicopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|snicclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|snicioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_endif
@@ -4988,28 +4988,28 @@ begin_define
 define|#
 directive|define
 name|isdnopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|isdnclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|isdnread
-value|enxio
+value|nxread
 end_define
 
 begin_define
 define|#
 directive|define
 name|isdnioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_endif
@@ -5059,35 +5059,35 @@ begin_define
 define|#
 directive|define
 name|itelopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|itelclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|itelread
-value|enxio
+value|nxread
 end_define
 
 begin_define
 define|#
 directive|define
 name|itelwrite
-value|enxio
+value|nxwrite
 end_define
 
 begin_define
 define|#
 directive|define
 name|itelioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_endif
@@ -5137,35 +5137,35 @@ begin_define
 define|#
 directive|define
 name|ispyopen
-value|enxio
+value|nxopen
 end_define
 
 begin_define
 define|#
 directive|define
 name|ispyclose
-value|enxio
+value|nxclose
 end_define
 
 begin_define
 define|#
 directive|define
 name|ispyread
-value|enxio
+value|nxread
 end_define
 
 begin_define
 define|#
 directive|define
 name|ispywrite
-value|enxio
+value|nxwrite
 end_define
 
 begin_define
 define|#
 directive|define
 name|ispyioctl
-value|enxio
+value|nxioctl
 end_define
 
 begin_endif
