687dff17a5eebe4bc71bcbce0258b37d9953f329
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-14 18:44:56 -0800

    check pointer != NULL rather than cast.
    SCCS-vsn: sys/kern/vfs_cluster.c 8.9
    
    Former-commit-id: 7d765f1bd2c731393a9d0a41ffeabd32089f14a2

--- usr/src/sys/kern/vfs_cluster.c
+++ usr/src/sys/kern/vfs_cluster.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_cluster.c	8.8 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_cluster.c	8.9 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -473,15 +473,14 @@ operator|)
 expr_stmt|;
 name|alreadyincore
 operator|=
-operator|(
-name|int
-operator|)
 name|incore
 argument_list|(
 name|vp
 argument_list|,
 name|ioblkno
 argument_list|)
+operator|!=
+name|NULL
 expr_stmt|;
 name|bp
 operator|=
