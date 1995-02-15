c6ed6dfa97fd69ae77f3ce09e00f077bdd8763df
Andrey Chernov <ache@FreeBSD.org>
1995-02-15 22:25:51 +0000

    Restore deleted in second time my & bde fixes. UGEN STOP IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    Former-commit-id: d02b82344a05634b5dcb2127f33c996f8e4f4d55

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.25 1995/02/14 21:21:25 ugen Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.27 1995/02/15 18:41:56 ugen Exp $  */
 end_comment
 
 begin_include
@@ -1585,6 +1585,10 @@ parameter_list|)
 define|\
 value|((c) == '\n' || (((c) == cc[VEOF] ||				\ 	(c) == cc[VEOL] || (c) == cc[VEOL2])&& (c) != _POSIX_VDISABLE))
 end_define
+
+begin_comment
+comment|/*-  * TODO:  *	o Fix races for sending the start char in ttyflush().  *	o Handle inter-byte timeout for "MIN> 0, TIME> 0" in ttselect().  *	  With luck, there will be MIN chars before select() returns().  *	o Handle CLOCAL consistently for ptys.  Perhaps disallow setting it.  *	o Don't allow input in TS_ZOMBIE case.  It would be visible through  *	  FIONREAD.  *	o Do the new sio locking stuff here and use it to avoid special  *	  case for EXTPROC?  *	o Lock PENDIN too?  *	o Move EXTPROC and/or PENDIN to t_state?  *	o Wrap most of ttioctl in spltty/splx.  *	o Implement TIOCNOTTY or remove it from<sys/ioctl.h>.  */
+end_comment
 
 begin_comment
 comment|/*  * Process input of a single character received on a tty.  */
@@ -5824,6 +5828,32 @@ condition|)
 break|break;
 block|}
 block|}
+if|if
+condition|(
+operator|!
+name|error
+operator|&&
+operator|(
+name|tp
+operator|->
+name|t_outq
+operator|.
+name|c_cc
+operator|||
+name|ISSET
+argument_list|(
+name|tp
+operator|->
+name|t_state
+argument_list|,
+name|TS_BUSY
+argument_list|)
+operator|)
+condition|)
+name|error
+operator|=
+name|EIO
+expr_stmt|;
 name|splx
 argument_list|(
 name|s
@@ -6380,9 +6410,16 @@ decl_stmt|;
 block|{
 if|if
 condition|(
+operator|(
 name|flag
 operator|&
 name|IO_NDELAY
+operator|)
+operator|||
+name|ttywflush
+argument_list|(
+name|tp
+argument_list|)
 condition|)
 name|ttyflush
 argument_list|(
@@ -6391,12 +6428,6 @@ argument_list|,
 name|FREAD
 operator||
 name|FWRITE
-argument_list|)
-expr_stmt|;
-else|else
-name|ttywflush
-argument_list|(
-name|tp
 argument_list|)
 expr_stmt|;
 return|return
@@ -10866,7 +10897,11 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Allocate a tty structure and its associated buffers.  */
+comment|/*  * XXX this is usable but not useful or used.  ttselect() requires an array  * of tty structs.  Most tty drivers have ifdefs for using ttymalloc() but  * assume a different interface.  */
+end_comment
+
+begin_comment
+comment|/*  * Allocate a tty struct.  Clists in the struct will be allocated by  * ttyopen().  */
 end_comment
 
 begin_function
@@ -10881,19 +10916,13 @@ name|tty
 modifier|*
 name|tp
 decl_stmt|;
-name|MALLOC
-argument_list|(
 name|tp
-argument_list|,
-expr|struct
-name|tty
-operator|*
-argument_list|,
-sizeof|sizeof
+operator|=
+name|malloc
 argument_list|(
-expr|struct
-name|tty
-argument_list|)
+sizeof|sizeof
+expr|*
+name|tp
 argument_list|,
 name|M_TTYS
 argument_list|,
@@ -10909,45 +10938,6 @@ expr|*
 name|tp
 argument_list|)
 expr_stmt|;
-comment|/*          * Initialize or restore a cblock allocation policy suitable for          * the standard line discipline.          */
-name|clist_alloc_cblocks
-argument_list|(
-operator|&
-name|tp
-operator|->
-name|t_canq
-argument_list|,
-name|TTYHOG
-argument_list|,
-literal|512
-argument_list|)
-expr_stmt|;
-name|clist_alloc_cblocks
-argument_list|(
-operator|&
-name|tp
-operator|->
-name|t_outq
-argument_list|,
-name|TTMAXHIWAT
-operator|+
-literal|200
-argument_list|,
-literal|512
-argument_list|)
-expr_stmt|;
-name|clist_alloc_cblocks
-argument_list|(
-operator|&
-name|tp
-operator|->
-name|t_rawq
-argument_list|,
-name|TTYHOG
-argument_list|,
-literal|512
-argument_list|)
-expr_stmt|;
 return|return
 operator|(
 name|tp
@@ -10956,55 +10946,29 @@ return|;
 block|}
 end_function
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
 begin_comment
-comment|/*  * Free a tty structure and its buffers.  */
+comment|/* XXX not yet usable: session leader holds a ref (see kern_exit.c). */
 end_comment
 
-begin_function
-name|void
-name|ttyfree
-parameter_list|(
-name|tp
-parameter_list|)
-name|struct
-name|tty
-modifier|*
-name|tp
-decl_stmt|;
-block|{
-name|clist_free_cblocks
-argument_list|(
-operator|&
-name|tp
-operator|->
-name|t_canq
-argument_list|)
-expr_stmt|;
-name|clist_free_cblocks
-argument_list|(
-operator|&
-name|tp
-operator|->
-name|t_outq
-argument_list|)
-expr_stmt|;
-name|clist_free_cblocks
-argument_list|(
-operator|&
-name|tp
-operator|->
-name|t_rawq
-argument_list|)
-expr_stmt|;
-name|FREE
-argument_list|(
-name|tp
-argument_list|,
-name|M_TTYS
-argument_list|)
-expr_stmt|;
-block|}
-end_function
+begin_comment
+comment|/*  * Free a tty struct.  Clists in the struct should have been freed by  * ttyclose().  */
+end_comment
+
+begin_endif
+unit|void ttyfree(tp) 	struct tty *tp; {         free(tp, M_TTYS); }
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* 0 */
+end_comment
 
 end_unit
 
