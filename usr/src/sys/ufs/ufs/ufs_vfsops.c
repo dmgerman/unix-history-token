1c7cb0738fad0a1119f4c457ee099df35765dec5
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-02-15 20:08:39 -0800

    SYNC may be done by anyone; select quota type properly (from karels)
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 8.5
    
    Former-commit-id: 508558d0d5743e6257f7b51ce1a858e744d5fb6a

--- usr/src/sys/ufs/ufs/ufs_vfsops.c
+++ usr/src/sys/ufs/ufs/ufs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	8.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -294,10 +294,11 @@ name|cmd
 condition|)
 block|{
 case|case
-name|Q_GETQUOTA
-case|:
-case|case
 name|Q_SYNC
+case|:
+break|break;
+case|case
+name|Q_GETQUOTA
 case|:
 if|if
 condition|(
@@ -336,7 +337,7 @@ return|;
 block|}
 name|type
 operator|=
-name|cmd
+name|cmds
 operator|&
 name|SUBCMDMASK
 expr_stmt|;
