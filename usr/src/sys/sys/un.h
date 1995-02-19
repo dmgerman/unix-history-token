cee5a12d38ea24d5769431788ba6689ee04dcf03
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-19 15:50:24 -0800

    fix return type for unp_detach()
    SCCS-vsn: sys/sys/un.h 8.3
    
    Former-commit-id: 70d5185bcb93b2bf058cb48eb16c37ebbe221239

--- usr/src/sys/sys/un.h
+++ usr/src/sys/sys/un.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)un.h	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)un.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -161,7 +161,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
+name|void
 name|unp_detach
 name|__P
 argument_list|(
