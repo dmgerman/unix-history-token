e667a9ce49daf8ee1222e6f034595d668153d161
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-19 15:49:02 -0800

    add key_t, so it can be used in syscall arg descriptions.
    SCCS-vsn: sys/sys/types.h 8.6
    
    Former-commit-id: 6a140b2cca793137c3e5d41d7024cd27c6d2519e

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	8.5 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)types.h	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -189,6 +189,17 @@ end_typedef
 
 begin_comment
 comment|/* inode number */
+end_comment
+
+begin_typedef
+typedef|typedef
+name|long
+name|key_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* IPC key (for Sys V IPC) */
 end_comment
 
 begin_typedef
