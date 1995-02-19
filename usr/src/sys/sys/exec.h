c2ee67bc9b891c77f4703af6960d8304729e776e
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-19 15:48:27 -0800

    execve_args now goes in auto-generated syscallargs.h file.
    SCCS-vsn: sys/sys/exec.h 8.4
    
    Former-commit-id: bbfbbfccab36d7f0f2129277df2ccb6a14729dc0

--- usr/src/sys/sys/exec.h
+++ usr/src/sys/sys/exec.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)exec.h	8.3 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)exec.h	8.4 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -50,32 +50,6 @@ name|PS_STRINGS
 define|\
 value|((struct ps_strings *)(USRSTACK - sizeof(struct ps_strings)))
 end_define
-
-begin_comment
-comment|/*  * Arguments to the exec system call.  */
-end_comment
-
-begin_struct
-struct|struct
-name|execve_args
-block|{
-name|char
-modifier|*
-name|fname
-decl_stmt|;
-name|char
-modifier|*
-modifier|*
-name|argp
-decl_stmt|;
-name|char
-modifier|*
-modifier|*
-name|envp
-decl_stmt|;
-block|}
-struct|;
-end_struct
 
 end_unit
 
