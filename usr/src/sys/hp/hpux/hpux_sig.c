fbc966332f02aae4a83d6c282306852895c6c8cf
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1995-02-19 16:00:25 -0800

    ohpuxssig -> compat_43_hpuxssig
    SCCS-vsn: sys/hp/hpux/hpux_sig.c 8.3
    
    Former-commit-id: 0f564a12f4ab01345090059053c12027df96df19

--- usr/src/sys/hp/hpux/hpux_sig.c
+++ usr/src/sys/hp/hpux/hpux_sig.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hpux_sig.c 1.4 92/01/20$  *  *	@(#)hpux_sig.c	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: hpux_sig.c 1.4 92/01/20$  *  *	@(#)hpux_sig.c	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1826,7 +1826,7 @@ struct|;
 end_struct
 
 begin_macro
-name|ohpuxssig
+name|compat_43_hpuxssig
 argument_list|(
 argument|p
 argument_list|,
