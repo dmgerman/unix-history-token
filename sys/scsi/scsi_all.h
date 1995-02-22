a512002399ae11cc5e2701bfd46eff962e5facd3
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-02-22 01:44:21 +0000

    Add a define for the SCSI_QUEUE_FULL sense message
    Former-commit-id: a6aa76a7fc76944b5469ea5864d20f861abfb6a2

--- sys/scsi/scsi_all.h
+++ sys/scsi/scsi_all.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Largely written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with   * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsi_all.h,v 1.7 1994/08/02 07:52:32 davidg Exp $  */
+comment|/*  * Largely written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with   * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsi_all.h,v 1.8 1995/01/08 13:38:30 dufault Exp $  */
 end_comment
 
 begin_comment
@@ -1149,6 +1149,13 @@ define|#
 directive|define
 name|SCSI_INTERM
 value|0x10
+end_define
+
+begin_define
+define|#
+directive|define
+name|SCSI_QUEUE_FULL
+value|0x28
 end_define
 
 begin_endif
