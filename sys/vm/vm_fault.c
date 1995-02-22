f29c9895e32a4d60137db7883c9a66e452742c56
David Greenman <dg@FreeBSD.org>
1995-02-22 09:15:35 +0000

    Only do object paging_in_progress wakeups if someone is waiting on this condition.
    Submitted by:	John Dyson
    
    
    Former-commit-id: cdadfb626a15d70a719f0ef223ea0e45076eeefe

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_fault.c	8.4 (Berkeley) 1/12/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_fault.c,v 1.17 1995/01/26 01:40:04 davidg Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_fault.c	8.4 (Berkeley) 1/12/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_fault.c,v 1.18 1995/02/02 09:08:17 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -253,7 +253,7 @@ value|{				\ 	if (lookup_still_valid) {			\ 		vm_map_lookup_done(map, entry);		\
 define|#
 directive|define
 name|UNLOCK_THINGS
-value|{				\ 	object->paging_in_progress--;			\ 	if (object->paging_in_progress == 0)		\ 		wakeup((caddr_t)object);		\ 	vm_object_unlock(object);			\ 	if (object != first_object) {			\ 		vm_object_lock(first_object);		\ 		FREE_PAGE(first_m);			\ 		first_object->paging_in_progress--;	\ 		if (first_object->paging_in_progress == 0) \ 			wakeup((caddr_t)first_object);	\ 		vm_object_unlock(first_object);		\ 	}						\ 	UNLOCK_MAP;					\ }
+value|{				\ 	object->paging_in_progress--;			\ 	if ((object->paging_in_progress == 0)&& \ 	    (object->flags& OBJ_PIPWNT))	{ \ 		object->flags&= ~OBJ_PIPWNT;	\ 		wakeup((caddr_t)object);		\ 	} \ 	vm_object_unlock(object);			\ 	if (object != first_object) {			\ 		vm_object_lock(first_object);		\ 		FREE_PAGE(first_m);			\ 		first_object->paging_in_progress--;	\ 		if ((first_object->paging_in_progress == 0)&& \ 		    (first_object->flags& OBJ_PIPWNT)) { \ 			first_object->flags&= ~OBJ_PIPWNT; \ 			wakeup((caddr_t)first_object);	\ 		} \ 		vm_object_unlock(first_object);		\ 	}						\ 	UNLOCK_MAP;					\ }
 define|#
 directive|define
 name|UNLOCK_AND_DEALLOCATE
@@ -947,7 +947,23 @@ operator|->
 name|paging_in_progress
 operator|==
 literal|0
+operator|&&
+operator|(
+name|object
+operator|->
+name|flags
+operator|&
+name|OBJ_PIPWNT
+operator|)
 condition|)
+block|{
+name|object
+operator|->
+name|flags
+operator|&=
+operator|~
+name|OBJ_PIPWNT
+expr_stmt|;
 name|wakeup
 argument_list|(
 operator|(
@@ -956,6 +972,7 @@ operator|)
 name|object
 argument_list|)
 expr_stmt|;
+block|}
 name|vm_object_unlock
 argument_list|(
 name|object
@@ -1027,7 +1044,23 @@ operator|->
 name|paging_in_progress
 operator|==
 literal|0
+operator|&&
+operator|(
+name|object
+operator|->
+name|flags
+operator|&
+name|OBJ_PIPWNT
+operator|)
 condition|)
+block|{
+name|object
+operator|->
+name|flags
+operator|&=
+operator|~
+name|OBJ_PIPWNT
+expr_stmt|;
 name|wakeup
 argument_list|(
 operator|(
@@ -1036,6 +1069,7 @@ operator|)
 name|object
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|vm_object_unlock
 argument_list|(
@@ -1163,7 +1197,23 @@ operator|->
 name|paging_in_progress
 operator|==
 literal|0
+operator|&&
+operator|(
+name|object
+operator|->
+name|flags
+operator|&
+name|OBJ_PIPWNT
+operator|)
 condition|)
+block|{
+name|object
+operator|->
+name|flags
+operator|&=
+operator|~
+name|OBJ_PIPWNT
+expr_stmt|;
 name|wakeup
 argument_list|(
 operator|(
@@ -1172,6 +1222,7 @@ operator|)
 name|object
 argument_list|)
 expr_stmt|;
+block|}
 name|vm_object_unlock
 argument_list|(
 name|object
@@ -1214,7 +1265,23 @@ operator|->
 name|paging_in_progress
 operator|==
 literal|0
+operator|&&
+operator|(
+name|object
+operator|->
+name|flags
+operator|&
+name|OBJ_PIPWNT
+operator|)
 condition|)
+block|{
+name|object
+operator|->
+name|flags
+operator|&=
+operator|~
+name|OBJ_PIPWNT
+expr_stmt|;
 name|wakeup
 argument_list|(
 operator|(
@@ -1223,6 +1290,7 @@ operator|)
 name|object
 argument_list|)
 expr_stmt|;
+block|}
 name|vm_object_collapse
 argument_list|(
 name|object
