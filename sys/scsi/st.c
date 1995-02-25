ba7997cbfcac31f44976007e0b340b136e25a327
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-02-25 19:11:11 +0000

    iodone->biodone; get rid of older call syntax.
    Former-commit-id: 7fe5a9b4127a376786aa0aa013e208398d70351e

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.25 1995/01/24 12:04:56 dufault Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.26 1995/01/31 11:41:47 dufault Exp $  */
 end_comment
 
 begin_comment
@@ -4391,7 +4391,7 @@ expr_stmt|;
 name|done
 label|:
 comment|/* 	 * Correctly set the buf to indicate a completed xfer 	 */
-name|iodone
+name|biodone
 argument_list|(
 name|bp
 argument_list|)
