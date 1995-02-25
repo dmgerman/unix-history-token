4c6b309978cf2764ad98ff562b6d633a23d8caf3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-24 18:09:10 -0800

    ISC Unix hacks
    SCCS-vsn: usr.sbin/sendmail/makemap/makemap.c 8.10
    
    Former-commit-id: b2e92165385c3edc5ba28b4ecf9cc11092a0140d

--- usr/src/usr.sbin/sendmail/makemap/makemap.c
+++ usr/src/usr.sbin/sendmail/makemap/makemap.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)makemap.c	8.9 (Berkeley) %G%"
+literal|"@(#)makemap.c	8.10 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -49,12 +49,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/file.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<ctype.h>
 end_include
 
@@ -69,6 +63,23 @@ include|#
 directive|include
 file|<sys/errno.h>
 end_include
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|ISC_UNIX
+end_ifndef
+
+begin_include
+include|#
+directive|include
+file|<sys/file.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
