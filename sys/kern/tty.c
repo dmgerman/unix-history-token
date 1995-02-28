69597e17562c701f8eed257a99f9b9c017154dc7
Andrey Chernov <ache@FreeBSD.org>
1995-02-28 23:21:33 +0000

    Workaround IXOFF bug when output queue is full && RTS control is on
    Former-commit-id: 793f5f794fe123ed240de0f607883de999ace962

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.33 1995/02/27 19:47:31 ugen Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.34 1995/02/28 00:21:03 pst Exp $  */
 end_comment
 
 begin_include
@@ -6163,6 +6163,11 @@ operator|/
 literal|5
 condition|)
 block|{
+name|int
+name|queue_full
+init|=
+literal|0
+decl_stmt|;
 if|if
 condition|(
 name|ISSET
@@ -6183,6 +6188,9 @@ index|]
 operator|!=
 name|_POSIX_VDISABLE
 operator|&&
+operator|(
+name|queue_full
+operator|=
 name|putc
 argument_list|(
 name|tp
@@ -6197,6 +6205,7 @@ name|tp
 operator|->
 name|t_outq
 argument_list|)
+operator|)
 operator|==
 literal|0
 operator|||
@@ -6222,6 +6231,20 @@ expr_stmt|;
 name|ttstart
 argument_list|(
 name|tp
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|queue_full
+condition|)
+comment|/* try again */
+name|SET
+argument_list|(
+name|tp
+operator|->
+name|t_state
+argument_list|,
+name|TS_TBLOCK
 argument_list|)
 expr_stmt|;
 block|}
@@ -6343,6 +6366,11 @@ literal|0
 operator|)
 condition|)
 block|{
+name|int
+name|queue_full
+init|=
+literal|0
+decl_stmt|;
 if|if
 condition|(
 name|ISSET
@@ -6363,6 +6391,9 @@ index|]
 operator|!=
 name|_POSIX_VDISABLE
 operator|&&
+operator|(
+name|queue_full
+operator|=
 name|putc
 argument_list|(
 name|tp
@@ -6377,6 +6408,7 @@ name|tp
 operator|->
 name|t_outq
 argument_list|)
+operator|)
 operator|==
 literal|0
 operator|||
@@ -6402,6 +6434,20 @@ expr_stmt|;
 name|ttstart
 argument_list|(
 name|tp
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|queue_full
+condition|)
+comment|/* try again */
+name|CLR
+argument_list|(
+name|tp
+operator|->
+name|t_state
+argument_list|,
+name|TS_TBLOCK
 argument_list|)
 expr_stmt|;
 block|}
@@ -8033,6 +8079,11 @@ operator|/
 literal|5
 condition|)
 block|{
+name|int
+name|queue_full
+init|=
+literal|0
+decl_stmt|;
 if|if
 condition|(
 name|ISSET
@@ -8051,6 +8102,9 @@ index|]
 operator|!=
 name|_POSIX_VDISABLE
 operator|&&
+operator|(
+name|queue_full
+operator|=
 name|putc
 argument_list|(
 name|cc
@@ -8063,6 +8117,7 @@ name|tp
 operator|->
 name|t_outq
 argument_list|)
+operator|)
 operator|==
 literal|0
 operator|||
@@ -8088,6 +8143,20 @@ expr_stmt|;
 name|ttstart
 argument_list|(
 name|tp
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|queue_full
+condition|)
+comment|/* try again */
+name|SET
+argument_list|(
+name|tp
+operator|->
+name|t_state
+argument_list|,
+name|TS_TBLOCK
 argument_list|)
 expr_stmt|;
 block|}
