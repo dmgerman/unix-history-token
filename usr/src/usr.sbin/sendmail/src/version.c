3b1549188b711f3fcb548c7bc59d330718684f14
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-28 19:10:57 -0800

    multiple glitches in 8.6.10 -- see ../RELEASE_NOTES for details
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.6.11.1
    
    Former-commit-id: 859ee11d5b6e88b5cf30e94c95d3252d99cd889f

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)version.c	8.6.10.1 (Berkeley) %G%"
+literal|"@(#)version.c	8.6.11.1 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -33,7 +33,7 @@ name|char
 name|Version
 index|[]
 init|=
-literal|"8.6.10"
+literal|"8.6.11"
 decl_stmt|;
 end_decl_stmt
 
