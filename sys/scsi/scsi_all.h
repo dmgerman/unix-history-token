c9e6a952f0cd79af6c77bc6b51bcd765afbd4ff9
Bruce Evans <bde@FreeBSD.org>
1995-03-04 12:36:55 +0000

    Remove unused common variable `extended'.
    Former-commit-id: b2d2803210e6407cd175d3ba8542ebcf4d4c9ae2

--- sys/scsi/scsi_all.h
+++ sys/scsi/scsi_all.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Largely written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with   * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsi_all.h,v 1.9 1995/02/22 01:44:21 gibbs Exp $  */
+comment|/*  * Largely written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with   * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsi_all.h,v 1.10 1995/03/01 22:24:41 dufault Exp $  */
 end_comment
 
 begin_comment
@@ -1012,7 +1012,6 @@ literal|14
 index|]
 decl_stmt|;
 block|}
-name|extended
 struct|;
 end_struct
 
