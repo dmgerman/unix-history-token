05052337d4ff8ef0de8e7519e1e704043e41f5f8
David Greenman <dg@FreeBSD.org>
1995-03-04 15:16:07 +0000

    Added some more of John's "anti-chatter" fixes - set the page activation count to 0 after activating the page; the previous behavior biased the pages too high in some cases.
    Submitted by:	John Dyson
    
    
    Former-commit-id: 241a4e3d2e6d8e99ba5093a687b08184334131bb

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.32 1995/03/01 22:08:55 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.33 1995/03/03 22:13:00 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -2165,11 +2165,19 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
 name|vm_page_activate
 argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
+name|m
+operator|->
+name|act_count
+operator|=
+literal|0
+expr_stmt|;
+block|}
 block|}
 block|}
 name|bufspace
@@ -4928,11 +4936,19 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
 name|vm_page_activate
 argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
+name|m
+operator|->
+name|act_count
+operator|=
+literal|0
+expr_stmt|;
+block|}
 continue|continue;
 block|}
 name|m
@@ -5073,6 +5089,12 @@ name|vm_page_activate
 argument_list|(
 name|m
 argument_list|)
+expr_stmt|;
+name|m
+operator|->
+name|act_count
+operator|=
+literal|0
 expr_stmt|;
 block|}
 elseif|else
@@ -5444,11 +5466,19 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
 name|vm_page_activate
 argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
+name|m
+operator|->
+name|act_count
+operator|=
+literal|0
+expr_stmt|;
+block|}
 name|m
 operator|->
 name|flags
