614434a990304743bcb30dd08025efc018beabdb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-05 22:23:11 -0800

    integrate changes from *Hobbit*
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.94
    
    Former-commit-id: 0fda9437dd2606eae460871ff9cbf5cb7771778c

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.93 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.94 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.93		%G%"
+literal|"@(#)sendmail.h	8.94		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -5350,6 +5350,18 @@ end_decl_stmt
 
 begin_comment
 comment|/* delay between dial-on-demand tries */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|char
+modifier|*
+name|SafeFileEnv
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* chroot location for file delivery */
 end_comment
 
 begin_decl_stmt
