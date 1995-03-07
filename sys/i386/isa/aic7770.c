438accf08aa7a4e5ae88274082ded7574a9fbd2d
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-03-07 08:58:22 +0000

    Compile cleanly -Wall.
    Former-commit-id: 192c17448b2b9d0dea377dd227e0a227e4ee9cfb

--- sys/i386/isa/aic7770.c
+++ sys/i386/isa/aic7770.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  * 	27/284X and aic7770 motherboard SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id$  */
+comment|/*  * Product specific probe and attach routines for:  * 	27/284X and aic7770 motherboard SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7770.c,v 1.10 1995/01/16 16:31:34 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -376,25 +376,32 @@ index|[]
 init|=
 block|{
 comment|/* Entries of other tested adaptors should be added here */
+block|{
 name|AHC_274
 block|,
 literal|0x71
+block|}
 block|,
 comment|/*274x, Card*/
+block|{
 name|AHC_274
 block|,
 literal|0x70
+block|}
 block|,
 comment|/*274x, Motherboard*/
+block|{
 name|AHC_284
 block|,
 literal|0x56
+block|}
 block|,
 comment|/*284x, BIOS enabled*/
+block|{
 name|AHC_284
 block|,
 literal|0x57
-block|,
+block|}
 comment|/*284x, BIOS disabled*/
 block|}
 decl_stmt|;
