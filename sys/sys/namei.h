e5574c84d441f76f07c6d33c16f1bdbb08eadbde
Poul-Henning Kamp <phk@FreeBSD.org>
1995-03-09 20:27:21 +0000

    Clean up and improve the namecache.
    1. We always keep one 16th of the vnodes on the freelist, so that the
    namecache doesn't get trashed.  It used to be that it wasn't a problem, but
    the only vnodes getting released these days are directories and things which
    Clean up and improve the namecache.
    
    1. We always keep one 16th of the vnodes on the freelist, so that the
    namecache doesn't get trashed.  It used to be that it wasn't a problem, but
    the only vnodes getting released these days are directories and things which
    gets forced out of the VM/cache.  The latter is not numerous enough to keep
    the pool of vnodes needed for the namecache sufficiently big.
    
    2. Purge invalid entries in the namecache as soon as we notice them.  This
    avoids a stale entry pushing out a valid entry on the LRU list.
    
    3. Speed up the lookup in the namecache by avoid a special case branch.
    
    4. Make the cache purge routines do the thing they're supposed to, and in
    a decently efficient manner.
    
    5. Make the size of the namecache follow the number of vnodes, so that we
    can always point to all the vnodes we have in core.
    
    6. Readability has gone way up.
    
    7. Added a "options NCH_STATISTICS" feature that will gather more
    detailed statistics on the performance of the namecache.
    
    Reviewed by:    davidg
    
    
    Former-commit-id: 2e57bfbc24d46dc8d0cb1cccbf2d9183a73a3ee2

--- sys/sys/namei.h
+++ sys/sys/namei.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)namei.h	8.2 (Berkeley) 1/4/94  * $Id: namei.h,v 1.3 1994/09/27 20:33:41 phk Exp $  */
+comment|/*  * Copyright (c) 1985, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)namei.h	8.2 (Berkeley) 1/4/94  * $Id: namei.h,v 1.4 1995/03/06 06:45:47 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -431,6 +431,28 @@ begin_comment
 comment|/*  * This structure describes the elements in the cache of recent  * names looked up by namei. NCHNAMLEN is sized to make structure  * size a power of two to optimize malloc's. Minimum reasonable  * size is 15.  */
 end_comment
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|NCH_STATISTICS
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|NCHNAMLEN
+value|23
+end_define
+
+begin_comment
+comment|/* maximum name segment length we bother with */
+end_comment
+
+begin_else
+else|#
+directive|else
+end_else
+
 begin_define
 define|#
 directive|define
@@ -441,6 +463,11 @@ end_define
 begin_comment
 comment|/* maximum name segment length we bother with */
 end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_struct
 struct|struct
@@ -480,6 +507,19 @@ name|u_long
 name|nc_vpid
 decl_stmt|;
 comment|/* capability number of nc_vp */
+ifdef|#
+directive|ifdef
+name|NCH_STATISTICS
+name|u_long
+name|nc_nbr
+decl_stmt|;
+comment|/* a serial number */
+name|u_long
+name|nc_hits
+decl_stmt|;
+comment|/* how many times we got hit */
+endif|#
+directive|endif
 name|char
 name|nc_nlen
 decl_stmt|;
@@ -504,6 +544,14 @@ end_ifdef
 begin_decl_stmt
 name|u_long
 name|nextvnodeid
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|u_long
+name|numvnodes
+decl_stmt|,
+name|numcache
 decl_stmt|;
 end_decl_stmt
 
