56889f6c1fb8edec5060ed1b032d810cb4096a01
Poul-Henning Kamp <phk@FreeBSD.org>
1995-03-19 21:26:00 +0000

    Make extra arguments be subdirs to go through.
    Former-commit-id: 98dc75718a72312e7c98af445ddb2c96d13ef157

--- usr.sbin/ctm/ctm_scan/ctm_scan.c
+++ usr.sbin/ctm/ctm_scan/ctm_scan.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: ctm_scan.c,v 1.6 1994/09/26 06:00:55 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: ctm_scan.c,v 1.7 1994/12/03 22:54:04 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -938,8 +938,45 @@ return|return
 literal|2
 return|;
 block|}
+name|argc
+operator|--
+expr_stmt|;
+name|argv
+operator|++
+expr_stmt|;
 block|}
 comment|/*       * Scan the directories recursively.      */
+if|if
+condition|(
+name|argc
+operator|>
+literal|1
+condition|)
+block|{
+while|while
+condition|(
+name|argc
+operator|>
+literal|1
+condition|)
+block|{
+name|Do
+argument_list|(
+name|argv
+index|[
+literal|1
+index|]
+argument_list|)
+expr_stmt|;
+name|argc
+operator|--
+expr_stmt|;
+name|argv
+operator|++
+expr_stmt|;
+block|}
+block|}
+else|else
 return|return
 name|Do
 argument_list|(
