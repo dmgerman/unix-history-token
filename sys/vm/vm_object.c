9fe17b910cf184b44e24f69294008f466aa0d2a7
David Greenman <dg@FreeBSD.org>
1995-03-22 12:24:11 +0000

    Removed unused fifth argument to vm_object_page_clean(). Fixed bug with VTEXT not always getting cleared when it is supposed to. Added check to make sure that vm_object_remove() isn't called with a NULL pager or for a pager for an OBJ_INTERNAL object (neither of which will be on the hash list). Clear OBJ_CANPERSIST if we decide to terminate it because of no resident pages.
    Former-commit-id: 05e05219d96094a7dbafcc82a786875ead44d8f1

--- sys/vm/vm_object.c
+++ sys/vm/vm_object.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_object.c	8.5 (Berkeley) 3/22/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_object.c,v 1.35 1995/03/21 01:11:42 davidg Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_object.c	8.5 (Berkeley) 3/22/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_object.c,v 1.36 1995/03/22 08:08:44 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -540,6 +540,9 @@ block|{
 name|vm_object_t
 name|temp
 decl_stmt|;
+name|vm_pager_t
+name|pager
+decl_stmt|;
 while|while
 condition|(
 name|object
@@ -793,33 +796,23 @@ argument_list|()
 expr_stmt|;
 return|return;
 block|}
-comment|/* 		 * See if this object can persist and has some resident 		 * pages.  If so, enter it in the cache. 		 */
+name|pager
+operator|=
+name|object
+operator|->
+name|pager
+expr_stmt|;
 if|if
 condition|(
-operator|(
-name|object
-operator|->
-name|flags
-operator|&
-name|OBJ_CANPERSIST
-operator|)
+name|pager
 operator|&&
-operator|(
-name|object
+name|pager
 operator|->
-name|resident_page_count
-operator|!=
-literal|0
-operator|)
+name|pg_type
+operator|==
+name|PG_VNODE
 condition|)
 block|{
-name|vm_pager_t
-name|pager
-init|=
-name|object
-operator|->
-name|pager
-decl_stmt|;
 name|vn_pager_t
 name|vnp
 init|=
@@ -830,15 +823,6 @@ name|pager
 operator|->
 name|pg_data
 decl_stmt|;
-if|if
-condition|(
-name|pager
-operator|->
-name|pg_type
-operator|==
-name|PG_VNODE
-condition|)
-block|{
 name|vnp
 operator|->
 name|vnp_vp
@@ -849,6 +833,25 @@ operator|~
 name|VTEXT
 expr_stmt|;
 block|}
+comment|/* 		 * See if this object can persist and has some resident 		 * pages.  If so, enter it in the cache. 		 */
+if|if
+condition|(
+name|object
+operator|->
+name|flags
+operator|&
+name|OBJ_CANPERSIST
+condition|)
+block|{
+if|if
+condition|(
+name|object
+operator|->
+name|resident_page_count
+operator|!=
+literal|0
+condition|)
+block|{
 name|TAILQ_INSERT_TAIL
 argument_list|(
 operator|&
@@ -875,6 +878,17 @@ argument_list|()
 expr_stmt|;
 return|return;
 block|}
+else|else
+block|{
+name|object
+operator|->
+name|flags
+operator|&=
+operator|~
+name|OBJ_CANPERSIST
+expr_stmt|;
+block|}
+block|}
 comment|/* 		 * Make sure no one can look us up now. 		 */
 name|object
 operator|->
@@ -882,10 +896,24 @@ name|flags
 operator||=
 name|OBJ_DEAD
 expr_stmt|;
-name|vm_object_remove
-argument_list|(
+if|if
+condition|(
+name|pager
+operator|!=
+name|NULL
+operator|&&
+operator|(
 name|object
 operator|->
+name|flags
+operator|&
+name|OBJ_INTERNAL
+operator|)
+operator|==
+literal|0
+condition|)
+name|vm_object_remove
+argument_list|(
 name|pager
 argument_list|)
 expr_stmt|;
@@ -1208,9 +1236,6 @@ argument_list|(
 name|vp
 argument_list|)
 expr_stmt|;
-operator|(
-name|void
-operator|)
 name|vm_object_page_clean
 argument_list|(
 name|object
@@ -1218,8 +1243,6 @@ argument_list|,
 literal|0
 argument_list|,
 literal|0
-argument_list|,
-name|TRUE
 argument_list|,
 name|TRUE
 argument_list|)
@@ -1372,11 +1395,11 @@ block|}
 end_function
 
 begin_comment
-comment|/*  *	vm_object_page_clean  *  *	Clean all dirty pages in the specified range of object.  *	Leaves page on whatever queue it is currently on.  *  *	Odd semantics: if start == end, we clean everything.  *  *	The object must be locked.  *	Returns true  */
+comment|/*  *	vm_object_page_clean  *  *	Clean all dirty pages in the specified range of object.  *	Leaves page on whatever queue it is currently on.  *  *	Odd semantics: if start == end, we clean everything.  *  *	The object must be locked.  */
 end_comment
 
 begin_function
-name|boolean_t
+name|void
 name|vm_object_page_clean
 parameter_list|(
 name|object
@@ -1386,8 +1409,6 @@ parameter_list|,
 name|end
 parameter_list|,
 name|syncio
-parameter_list|,
-name|de_queue
 parameter_list|)
 specifier|register
 name|vm_object_t
@@ -1403,9 +1424,6 @@ name|end
 decl_stmt|;
 name|boolean_t
 name|syncio
-decl_stmt|;
-name|boolean_t
-name|de_queue
 decl_stmt|;
 block|{
 specifier|register
@@ -1640,17 +1658,7 @@ goto|;
 block|}
 block|}
 block|}
-name|wakeup
-argument_list|(
-operator|(
-name|caddr_t
-operator|)
-name|object
-argument_list|)
-expr_stmt|;
-return|return
-literal|1
-return|;
+return|return;
 block|}
 end_function
 
