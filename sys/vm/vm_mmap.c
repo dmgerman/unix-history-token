70f23e12bbb8c2a920393f8bb02114867b3137e1
David Greenman <dg@FreeBSD.org>
1995-03-25 16:55:46 +0000

    Added "flags" argument to msync, and implemented MS_ASYNC and MS_INVALIDATE. The MS_ASYNC flag doesn't current work, and MS_INVALIDATE will only toss out the pages in the address space (not all pages in the shadow chain).
    Former-commit-id: 57a5b8256d6bb8785aaa3065fc47725d48239716

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.4 (Berkeley) 1/12/94  * $Id: vm_mmap.c,v 1.17 1995/03/21 10:15:52 davidg Exp $  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.4 (Berkeley) 1/12/94  * $Id: vm_mmap.c,v 1.18 1995/03/22 05:08:41 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -1159,6 +1159,9 @@ decl_stmt|;
 name|int
 name|len
 decl_stmt|;
+name|int
+name|flags
+decl_stmt|;
 block|}
 struct|;
 end_struct
@@ -1194,16 +1197,14 @@ decl_stmt|;
 name|vm_size_t
 name|size
 decl_stmt|;
+name|int
+name|flags
+decl_stmt|;
 name|vm_map_t
 name|map
 decl_stmt|;
 name|int
 name|rv
-decl_stmt|;
-name|boolean_t
-name|syncio
-decl_stmt|,
-name|invalidate
 decl_stmt|;
 ifdef|#
 directive|ifdef
@@ -1294,6 +1295,12 @@ name|uap
 operator|->
 name|len
 expr_stmt|;
+name|flags
+operator|=
+name|uap
+operator|->
+name|flags
+expr_stmt|;
 comment|/* 	 * XXX Gak!  If size is zero we are supposed to sync "all modified 	 * pages with the region containing addr".  Unfortunately, we don't 	 * really keep track of individual mmaps so we approximate by flushing 	 * the range of the map entry containing addr. This can be incorrect 	 * if the region splits or is coalesced with a neighbor. 	 */
 if|if
 condition|(
@@ -1377,16 +1384,6 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-comment|/* 	 * Could pass this in as a third flag argument to implement Sun's 	 * MS_ASYNC. 	 */
-name|syncio
-operator|=
-name|TRUE
-expr_stmt|;
-comment|/* 	 * XXX bummer, gotta flush all cached pages to ensure consistency with 	 * the file system cache.  Otherwise, we could pass this in to 	 * implement Sun's MS_INVALIDATE. 	 */
-name|invalidate
-operator|=
-name|TRUE
-expr_stmt|;
 comment|/* 	 * Clean the pages and interpret the return value. 	 */
 name|rv
 operator|=
@@ -1400,9 +1397,21 @@ name|addr
 operator|+
 name|size
 argument_list|,
-name|syncio
+operator|(
+name|flags
+operator|&
+name|MS_ASYNC
+operator|)
+operator|!=
+literal|0
 argument_list|,
-name|invalidate
+operator|(
+name|flags
+operator|&
+name|MS_INVALIDATE
+operator|)
+operator|!=
+literal|0
 argument_list|)
 expr_stmt|;
 switch|switch
