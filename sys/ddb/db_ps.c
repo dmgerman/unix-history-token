b85d622c79f751dd195b34a50dec59dfafde07c1
David Greenman <dg@FreeBSD.org>
1995-03-28 23:29:52 +0000

    Fixed DDB's "ps" output formatting.
    Former-commit-id: 02847c86cfbb2e69c84a1f752f2d48c42785925b

--- sys/ddb/db_ps.c
+++ sys/ddb/db_ps.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: db_ps.c,v 1.2 1994/09/27 20:35:55 phk Exp $  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: db_ps.c,v 1.3 1994/09/28 19:16:24 phk Exp $  */
 end_comment
 
 begin_include
@@ -70,7 +70,7 @@ name|allproc
 expr_stmt|;
 name|db_printf
 argument_list|(
-literal|"  pid  proc    addr     uid     ppid  pgrp   flag stat comm         wchan\n"
+literal|"  pid   proc     addr    uid  ppid  pgrp  flag stat wmesg   wchan   cmd\n"
 argument_list|)
 expr_stmt|;
 while|while
@@ -133,7 +133,7 @@ condition|)
 block|{
 name|db_printf
 argument_list|(
-literal|"%5d %06x %06x %3d %5d %5d  %06x  %d  %s   "
+literal|"%5d %06x %06x %4d %5d %5d %06x  %d  %6s %06x %s\n"
 argument_list|,
 name|p
 operator|->
@@ -171,44 +171,21 @@ name|p_stat
 argument_list|,
 name|p
 operator|->
+name|p_wmesg
+condition|?
+name|p
+operator|->
+name|p_wmesg
+else|:
+literal|""
+argument_list|,
+name|p
+operator|->
+name|p_wchan
+argument_list|,
+name|p
+operator|->
 name|p_comm
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|p
-operator|->
-name|p_wchan
-condition|)
-block|{
-if|if
-condition|(
-name|p
-operator|->
-name|p_wmesg
-condition|)
-name|db_printf
-argument_list|(
-literal|"%s "
-argument_list|,
-name|p
-operator|->
-name|p_wmesg
-argument_list|)
-expr_stmt|;
-name|db_printf
-argument_list|(
-literal|"%x"
-argument_list|,
-name|p
-operator|->
-name|p_wchan
-argument_list|)
-expr_stmt|;
-block|}
-name|db_printf
-argument_list|(
-literal|"\n"
 argument_list|)
 expr_stmt|;
 block|}
