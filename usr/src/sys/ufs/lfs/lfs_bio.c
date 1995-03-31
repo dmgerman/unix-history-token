115cec78f5c7a22efc1a64ac03841345d8420212
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-03-30 21:20:58 -0800

    MOUNT_LFS becomes lfs_mount_type
    SCCS-vsn: sys/ufs/lfs/lfs_bio.c 8.6
    
    Former-commit-id: e0b6922a41833985b25536479a027ce1b91c9255

--- usr/src/sys/ufs/lfs/lfs_bio.c
+++ usr/src/sys/ufs/lfs/lfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_bio.c	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)lfs_bio.c	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -433,7 +433,7 @@ name|mnt_stat
 operator|.
 name|f_type
 operator|==
-name|MOUNT_LFS
+name|lfs_mount_type
 operator|&&
 operator|(
 name|mp
