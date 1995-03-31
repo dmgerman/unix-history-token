e37f864634d56583f7fe6d1ed77d01ba2482f41a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-03-30 21:22:26 -0800

    fix declaration for mfs_init
    SCCS-vsn: sys/ufs/mfs/mfs_vnops.c 8.10
    
    Former-commit-id: 80f6ea08b0a595e81bf2aeffe7237ac6bbb9453b

--- usr/src/sys/ufs/mfs/mfs_vnops.c
+++ usr/src/sys/ufs/mfs/mfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mfs_vnops.c	8.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)mfs_vnops.c	8.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1308,8 +1308,18 @@ end_comment
 
 begin_macro
 name|mfs_init
-argument_list|()
+argument_list|(
+argument|vfsp
+argument_list|)
 end_macro
+
+begin_decl_stmt
+name|struct
+name|vfsconf
+modifier|*
+name|vfsp
+decl_stmt|;
+end_decl_stmt
 
 begin_block
 block|{
