e66b85841450a02c11ab3e7f6edeb2c7496814e5
Joerg Wunsch <joerg@FreeBSD.org>
1995-04-02 16:14:51 +0000

    Attempt to fix the `you can log into console only once' problem (PR #179).  The fix implements a ttyhalfclose() (sort of), resetting the session and pgrp pointers when the physical device is about to be closed.
    Suggested by: bde
    
    
    Former-commit-id: 447e2f4eb77108f46be42285dbe79410a6a8efc3

--- sys/kern/tty_cons.c
+++ sys/kern/tty_cons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91  *	$Id: cons.c,v 1.22 1995/02/25 20:09:04 pst Exp $  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91  *	$Id: cons.c,v 1.23 1995/02/26 03:15:36 bde Exp $  */
 end_comment
 
 begin_include
@@ -281,6 +281,19 @@ begin_comment
 comment|/* physical console device info */
 end_comment
 
+begin_decl_stmt
+specifier|static
+name|struct
+name|tty
+modifier|*
+name|cn_tp
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* physical console tty struct */
+end_comment
+
 begin_function
 name|void
 name|cninit
@@ -398,6 +411,20 @@ operator|->
 name|d_open
 operator|=
 name|cnopen
+expr_stmt|;
+name|cn_tp
+operator|=
+call|(
+modifier|*
+name|cdp
+operator|->
+name|d_devtotty
+call|)
+argument_list|(
+name|cn_tab
+operator|->
+name|cn_dev
+argument_list|)
 expr_stmt|;
 comment|/* 	 * Turn on console 	 */
 name|cn_tty
@@ -594,6 +621,7 @@ operator|==
 name|cndev
 condition|)
 block|{
+comment|/* the physical device is about to be closed */
 name|cn_phys_is_open
 operator|=
 literal|0
@@ -602,11 +630,33 @@ if|if
 condition|(
 name|cn_is_open
 condition|)
+block|{
+if|if
+condition|(
+name|cn_tp
+condition|)
+block|{
+comment|/* perform a ttyhalfclose() */
+comment|/* reset session and proc group */
+name|cn_tp
+operator|->
+name|t_pgrp
+operator|=
+name|NULL
+expr_stmt|;
+name|cn_tp
+operator|->
+name|t_session
+operator|=
+name|NULL
+expr_stmt|;
+block|}
 return|return
 operator|(
 literal|0
 operator|)
 return|;
+block|}
 block|}
 elseif|else
 if|if
@@ -622,6 +672,7 @@ name|cndev
 argument_list|)
 condition|)
 block|{
+comment|/* the logical console is about to be closed */
 name|cn_is_open
 operator|=
 literal|0
