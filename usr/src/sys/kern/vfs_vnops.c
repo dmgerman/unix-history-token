b2e76d5d5601d1d3a12704c0a90ffcd98560e929
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-04-04 01:15:05 -0800

    do synchronous writes when MNT_SYNCHRONOUS is set
    SCCS-vsn: sys/kern/vfs_vnops.c 8.10
    
    Former-commit-id: b7d19ec191a5383f20f4ecce42889449e5a84a23

--- usr/src/sys/kern/vfs_vnops.c
+++ usr/src/sys/kern/vfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.9 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.10 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1333,11 +1333,23 @@ name|IO_NDELAY
 expr_stmt|;
 if|if
 condition|(
+operator|(
 name|fp
 operator|->
 name|f_flag
 operator|&
 name|O_FSYNC
+operator|)
+operator|||
+operator|(
+name|vp
+operator|->
+name|v_mount
+operator|->
+name|mnt_flag
+operator|&
+name|MNT_SYNCHRONOUS
+operator|)
 condition|)
 name|ioflag
 operator||=
