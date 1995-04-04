ef6d15022f0f21ddb6e90eb8cb35191e597535fe
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-04-04 06:17:44 -0800

    add revoke operator
    SCCS-vsn: sys/miscfs/portal/portal_vnops.c 8.11
    SCCS-vsn: sys/miscfs/procfs/procfs.h 8.8
    SCCS-vsn: sys/miscfs/procfs/procfs_vnops.c 8.14
    
    Former-commit-id: f71ba5fb0961023b7e307634706d9adf975ee4dc

--- usr/src/sys/miscfs/portal/portal_vnops.c
+++ usr/src/sys/miscfs/portal/portal_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)portal_vnops.c	8.10 (Berkeley) %G%  *  * $Id: portal_vnops.c,v 1.4 1992/05/30 10:05:24 jsp Exp jsp $  */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)portal_vnops.c	8.11 (Berkeley) %G%  *  * $Id: portal_vnops.c,v 1.4 1992/05/30 10:05:24 jsp Exp jsp $  */
 end_comment
 
 begin_comment
@@ -2288,6 +2288,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|portal_revoke
+value|vop_revoke
+end_define
+
+begin_define
+define|#
+directive|define
 name|portal_fsync
 value|((int (*) __P((struct  vop_fsync_args *)))nullop)
 end_define
@@ -2567,6 +2574,14 @@ name|portal_mmap
 block|}
 block|,
 comment|/* mmap */
+block|{
+operator|&
+name|vop_revoke_desc
+block|,
+name|portal_revoke
+block|}
+block|,
+comment|/* revoke */
 block|{
 operator|&
 name|vop_fsync_desc
--- usr/src/sys/miscfs/procfs/procfs.h
+++ usr/src/sys/miscfs/procfs/procfs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Jan-Simon Pendry  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)procfs.h	8.7 (Berkeley) %G%  *  * From:  *	$Id: procfs.h,v 3.2 1993/12/15 09:40:17 jsp Exp $  */
+comment|/*  * Copyright (c) 1993 Jan-Simon Pendry  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * %sccs.include.redist.c%  *  *	@(#)procfs.h	8.8 (Berkeley) %G%  *  * From:  *	$Id: procfs.h,v 3.2 1993/12/15 09:40:17 jsp Exp $  */
 end_comment
 
 begin_comment
@@ -879,6 +879,13 @@ define|#
 directive|define
 name|procfs_mmap
 value|((int (*) __P((struct vop_mmap_args *))) procfs_badop)
+end_define
+
+begin_define
+define|#
+directive|define
+name|procfs_revoke
+value|vop_revoke
 end_define
 
 begin_define
