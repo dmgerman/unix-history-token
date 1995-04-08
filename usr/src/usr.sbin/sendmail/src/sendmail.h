c9ac4ddfc047fd7d253310e1b60f5d9b3f141e1f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-08 14:09:36 -0800

    change comment to be more accurate
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.117
    
    Former-commit-id: 42926eea91b3743b5685e4a875bba78d1f41ea37

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.116 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.117 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.116		%G%"
+literal|"@(#)sendmail.h	8.117		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -132,7 +132,7 @@ name|EX_OK
 end_undef
 
 begin_comment
-comment|/* for IRIX 5.3 and up */
+comment|/* for SVr4.2 SMP */
 end_comment
 
 begin_endif
