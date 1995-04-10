2a4734f5f1dd45ae6b947f10ca7c21b519edfd8d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-09 20:08:32 -0800

    add another global flag (sigh)
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.118
    
    Former-commit-id: cdac6b9251dab1e5aff56f20e5159f457f567065

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.117 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.118 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.117		%G%"
+literal|"@(#)sendmail.h	8.118		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -5295,6 +5295,17 @@ end_decl_stmt
 
 begin_comment
 comment|/* running with OutChannel redirected to xf */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|bool
+name|ColonOkInAddr
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* single colon legal in address */
 end_comment
 
 begin_decl_stmt
