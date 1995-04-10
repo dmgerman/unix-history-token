76e6d0b0fb422c3a329cd839075b3bcd8c46ed19
Garrett Wollman <wollman@FreeBSD.org>
1995-04-10 18:42:10 +0000

    there is a endian problem when setting up tunnel and phyint boundaries in the mrouted. inet_parse returns network byte ordered address, but there are a couple comaprisons that need to be done on the addresses and the comparisons are done in host order. I left the comparisions for 0xffffffff alone, because this value is the same in network and host orders.
    Submitted by:	Mark Tinguely <tinguely@plains.NoDak.edu>
    
    
    Former-commit-id: c5c0a5ab85dbef0bbb235680835c7fbbfd25104d

--- usr.sbin/mrouted/config.c
+++ usr.sbin/mrouted/config.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The mrouted program is covered by the license in the accompanying file  * named "LICENSE".  Use of the mrouted program represents acceptance of  * the terms and conditions listed in that file.  *  * The mrouted program is COPYRIGHT 1989 by The Board of Trustees of  * Leland Stanford Junior University.  *  *  * $Id: config.c,v 1.6 1994/08/24 23:52:54 thyagara Exp $  */
+comment|/*  * The mrouted program is covered by the license in the accompanying file  * named "LICENSE".  Use of the mrouted program represents acceptance of  * the terms and conditions listed in that file.  *  * The mrouted program is COPYRIGHT 1989 by The Board of Trustees of  * Leland Stanford Junior University.  *  *  * $Id: config.c,v 1.2 1994/09/08 02:51:12 wollman Exp $  */
 end_comment
 
 begin_include
@@ -1461,7 +1461,10 @@ operator|==
 literal|0xffffffff
 operator|||
 operator|(
+name|ntohl
+argument_list|(
 name|baddr
+argument_list|)
 operator|&
 literal|0xff000000
 operator|)
@@ -2250,7 +2253,10 @@ operator|==
 literal|0xffffffff
 operator|||
 operator|(
+name|ntohl
+argument_list|(
 name|baddr
+argument_list|)
 operator|&
 literal|0xef000000
 operator|)
@@ -3402,7 +3408,10 @@ operator|==
 literal|0xffffffff
 operator|||
 operator|(
+name|ntohl
+argument_list|(
 name|baddr
+argument_list|)
 operator|&
 literal|0xef000000
 operator|)
