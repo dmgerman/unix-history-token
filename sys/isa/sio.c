d0b79f4fb124298b8e2c6adcf7aa712e434a20a0
Bruce Evans <bde@FreeBSD.org>
1995-04-23 12:41:57 +0000

    Move declarations of console functions to cons.h (they should be config(8)ed).
    Update other misplaced prototypes.
    
    
    Former-commit-id: 2e0dcb7a9d4f181ac054964fa8f94cc6377f613d

--- sys/isa/sio.c
+++ sys/isa/sio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.94 1995/04/13 15:03:29 wollman Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.95 1995/04/15 21:45:16 bde Exp $  */
 end_comment
 
 begin_include
@@ -789,7 +789,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  * The public functions in the com module ought to be declared in a com-driver  * system header.  */
+comment|/*  * XXX public functions in drivers should be declared in headers produced  * by `config', not here.  */
 end_comment
 
 begin_comment
@@ -799,6 +799,19 @@ end_comment
 begin_decl_stmt
 name|void
 name|siointr
+name|__P
+argument_list|(
+operator|(
+name|int
+name|unit
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|siointrts
 name|__P
 argument_list|(
 operator|(
@@ -968,21 +981,15 @@ value|noreset
 end_define
 
 begin_decl_stmt
-name|int
-name|sioselect
+name|struct
+name|tty
+modifier|*
+name|siodevtotty
 name|__P
 argument_list|(
 operator|(
 name|dev_t
 name|dev
-operator|,
-name|int
-name|rw
-operator|,
-expr|struct
-name|proc
-operator|*
-name|p
 operator|)
 argument_list|)
 decl_stmt|;
@@ -1001,88 +1008,6 @@ directive|define
 name|siostrategy
 value|nostrategy
 end_define
-
-begin_comment
-comment|/* Console device entry points. */
-end_comment
-
-begin_decl_stmt
-name|int
-name|siocncheckc
-name|__P
-argument_list|(
-operator|(
-name|dev_t
-name|dev
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|siocngetc
-name|__P
-argument_list|(
-operator|(
-name|dev_t
-name|dev
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_struct_decl
-struct_decl|struct
-name|consdev
-struct_decl|;
-end_struct_decl
-
-begin_decl_stmt
-name|void
-name|siocninit
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|consdev
-operator|*
-name|cp
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|void
-name|siocnprobe
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|consdev
-operator|*
-name|cp
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|void
-name|siocnputc
-name|__P
-argument_list|(
-operator|(
-name|dev_t
-name|dev
-operator|,
-name|int
-name|c
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
 
 begin_decl_stmt
 specifier|static
