2815602a5ee43986f514a5c7a85b1455749f1ebe
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-22 18:07:06 -0800

    allow error mailer to take DSN status code
    SCCS-vsn: usr.sbin/sendmail/src/sysexits.c 8.3
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.66
    
    Former-commit-id: d67a99ceedce07b2a44917769bfb938823b836bb

--- usr/src/usr.sbin/sendmail/src/sysexits.c
+++ usr/src/usr.sbin/sendmail/src/sysexits.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)sysexits.c	8.2 (Berkeley) %G%"
+literal|"@(#)sysexits.c	8.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -111,6 +111,304 @@ index|]
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_escape
+end_escape
+
+begin_comment
+comment|/* **  DSNTOEXITSTAT -- convert DSN-style error code to EX_ style. ** **	Parameters: **		dsncode -- the text of the DSN-style code. ** **	Returns: **		The corresponding exit status. */
+end_comment
+
+begin_function
+name|int
+name|dsntoexitstat
+parameter_list|(
+name|dsncode
+parameter_list|)
+name|char
+modifier|*
+name|dsncode
+decl_stmt|;
+block|{
+name|int
+name|code2
+decl_stmt|,
+name|code3
+decl_stmt|;
+comment|/* first the easy cases.... */
+if|if
+condition|(
+operator|*
+name|dsncode
+operator|==
+literal|'2'
+condition|)
+return|return
+name|EX_OK
+return|;
+if|if
+condition|(
+operator|*
+name|dsncode
+operator|==
+literal|'4'
+condition|)
+return|return
+name|EX_TEMPFAIL
+return|;
+comment|/* now decode the other two field parts */
+if|if
+condition|(
+operator|*
+operator|++
+name|dsncode
+operator|==
+literal|'.'
+condition|)
+name|dsncode
+operator|++
+expr_stmt|;
+name|code2
+operator|=
+name|atoi
+argument_list|(
+name|dsncode
+argument_list|)
+expr_stmt|;
+while|while
+condition|(
+operator|*
+name|dsncode
+operator|!=
+literal|'\0'
+operator|&&
+operator|*
+name|dsncode
+operator|!=
+literal|'.'
+condition|)
+name|dsncode
+operator|++
+expr_stmt|;
+if|if
+condition|(
+operator|*
+name|dsncode
+operator|!=
+literal|'\0'
+condition|)
+name|dsncode
+operator|++
+expr_stmt|;
+name|code3
+operator|=
+name|atoi
+argument_list|(
+name|dsncode
+argument_list|)
+expr_stmt|;
+comment|/* and do a nested switch to work them out */
+switch|switch
+condition|(
+name|code2
+condition|)
+block|{
+case|case
+literal|0
+case|:
+comment|/* Other or Undefined status */
+return|return
+name|EX_UNAVAILABLE
+return|;
+case|case
+literal|1
+case|:
+comment|/* Address Status */
+switch|switch
+condition|(
+name|code3
+condition|)
+block|{
+case|case
+literal|0
+case|:
+comment|/* Other Address Status */
+return|return
+name|EX_DATAERR
+return|;
+case|case
+literal|1
+case|:
+comment|/* Bad mailbox address */
+case|case
+literal|6
+case|:
+comment|/* Mailbox has moved, No forwarding address */
+return|return
+name|EX_NOUSER
+return|;
+case|case
+literal|2
+case|:
+comment|/* Bad system address */
+return|return
+name|EX_NOHOST
+return|;
+case|case
+literal|3
+case|:
+comment|/* Bad mailbox address syntax */
+return|return
+name|EX_USAGE
+return|;
+case|case
+literal|4
+case|:
+comment|/* Mailbox address ambiguous */
+return|return
+name|EX_UNAVAILABLE
+return|;
+case|case
+literal|5
+case|:
+comment|/* Address valid */
+return|return
+name|EX_OK
+return|;
+block|}
+break|break;
+case|case
+literal|2
+case|:
+comment|/* Mailbox Status */
+switch|switch
+condition|(
+name|code3
+condition|)
+block|{
+case|case
+literal|0
+case|:
+comment|/* Other or Undefined mailbox status */
+case|case
+literal|1
+case|:
+comment|/* Mailbox disabled, not acccepting messages */
+case|case
+literal|2
+case|:
+comment|/* Mailbox full */
+case|case
+literal|4
+case|:
+comment|/* Mailing list expansion problem */
+return|return
+name|EX_UNAVAILABLE
+return|;
+case|case
+literal|3
+case|:
+comment|/* Message length exceeds administrative lim */
+return|return
+name|EX_DATAERR
+return|;
+block|}
+break|break;
+case|case
+literal|3
+case|:
+comment|/* System Status */
+return|return
+name|EX_OSERR
+return|;
+case|case
+literal|4
+case|:
+comment|/* Network and Routing Status */
+switch|switch
+condition|(
+name|code3
+condition|)
+block|{
+case|case
+literal|0
+case|:
+comment|/* Other or undefined network or routing stat */
+return|return
+name|EX_IOERR
+return|;
+case|case
+literal|1
+case|:
+comment|/* No answer from host */
+case|case
+literal|3
+case|:
+comment|/* Routing server failure */
+case|case
+literal|5
+case|:
+comment|/* Network congestion */
+return|return
+name|EX_TEMPFAIL
+return|;
+case|case
+literal|2
+case|:
+comment|/* Bad connection */
+return|return
+name|EX_IOERR
+return|;
+case|case
+literal|4
+case|:
+comment|/* Unable to route */
+return|return
+name|EX_PROTOCOL
+return|;
+case|case
+literal|6
+case|:
+comment|/* Routing loop detected */
+return|return
+name|EX_CONFIG
+return|;
+case|case
+literal|7
+case|:
+comment|/* Delivery time expired */
+return|return
+name|EX_UNAVAILABLE
+return|;
+block|}
+break|break;
+case|case
+literal|5
+case|:
+comment|/* Protocol Status */
+return|return
+name|EX_PROTOCOL
+return|;
+case|case
+literal|6
+case|:
+comment|/* Message Content or Media Status */
+return|return
+name|EX_UNAVAILABLE
+return|;
+case|case
+literal|7
+case|:
+comment|/* Security Status */
+return|return
+name|EX_DATAERR
+return|;
+block|}
+return|return
+name|EX_CONFIG
+return|;
+block|}
+end_function
 
 end_unit
 
