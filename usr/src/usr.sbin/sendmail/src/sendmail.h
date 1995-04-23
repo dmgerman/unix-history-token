e50b91e299aeca0f91d58cf45c67363d4be8cabc
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-23 14:46:15 -0800

    add M_EBCDIC
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.126
    
    Former-commit-id: 048f19a85ea6c0eb8c28d901e166af4e1c6f3700

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.125 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.126 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.125		%G%"
+literal|"@(#)sendmail.h	8.126		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1232,6 +1232,17 @@ end_define
 
 begin_comment
 comment|/* use hidden-dot algorithm */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|M_EBCDIC
+value|'3'
+end_define
+
+begin_comment
+comment|/* extend Q-P encoding for EBCDIC */
 end_comment
 
 begin_define
