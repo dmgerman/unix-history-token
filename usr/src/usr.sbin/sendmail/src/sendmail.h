fc6e81163a4afd83a3f1764c43170d3150bde5f3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-22 21:21:22 -0800

    add e_status
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.125
    
    Former-commit-id: 075fa325b789d25acb0b76ed8ba2a9fdca095330

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.124 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.125 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.124		%G%"
+literal|"@(#)sendmail.h	8.125		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2150,6 +2150,11 @@ modifier|*
 name|e_envid
 decl_stmt|;
 comment|/* envelope id from MAIL FROM: line */
+name|char
+modifier|*
+name|e_status
+decl_stmt|;
+comment|/* DSN status for this message */
 name|time_t
 name|e_dtime
 decl_stmt|;
