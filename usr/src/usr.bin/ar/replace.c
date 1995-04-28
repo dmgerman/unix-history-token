37fbb2c224581a36783212a1985522caac02037e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-27 23:33:20 -0800

    file descriptor leak From: Charles Hannum <mycroft@NetBSD.ORG>
    SCCS-vsn: usr.bin/ar/replace.c 8.4
    
    Former-commit-id: fd23c4872f30a9d6de342e5756c334ad5fa80229

--- usr/src/usr.bin/ar/replace.c
+++ usr/src/usr.bin/ar/replace.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)replace.c	8.3 (Berkeley) %G%"
+literal|"@(#)replace.c	8.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -297,9 +297,19 @@ name|chdr
 operator|.
 name|date
 condition|)
+block|{
+operator|(
+name|void
+operator|)
+name|close
+argument_list|(
+name|sfd
+argument_list|)
+expr_stmt|;
 goto|goto
 name|useold
 goto|;
+block|}
 if|if
 condition|(
 name|options
