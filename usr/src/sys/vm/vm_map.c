894cdf41fa4fda76f91dee622d585ca0db4c3601
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-04-27 21:48:03 -0800

    lockmanager takes an interlock and a pid rather than a proc ptr
    SCCS-vsn: sys/vm/vm_map.c 8.6
    
    Former-commit-id: a42c8baddf2c75387bf2781454c734061cb8c762

--- usr/src/sys/vm/vm_map.c
+++ usr/src/sys/vm/vm_map.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_map.c	8.5 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*   * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * %sccs.include.redist.c%  *  *	@(#)vm_map.c	8.6 (Berkeley) %G%  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
 begin_comment
@@ -3784,7 +3784,13 @@ name|lock
 argument_list|,
 name|LK_DOWNGRADE
 argument_list|,
-name|curproc
+operator|(
+name|void
+operator|*
+operator|)
+literal|0
+argument_list|,
+name|LOCKPID
 argument_list|)
 expr_stmt|;
 block|}
@@ -7063,7 +7069,13 @@ name|lock
 argument_list|,
 name|LK_EXCLUPGRADE
 argument_list|,
-name|curproc
+operator|(
+name|void
+operator|*
+operator|)
+literal|0
+argument_list|,
+name|LOCKPID
 argument_list|)
 condition|)
 block|{
@@ -7125,7 +7137,13 @@ name|lock
 argument_list|,
 name|LK_DOWNGRADE
 argument_list|,
-name|curproc
+operator|(
+name|void
+operator|*
+operator|)
+literal|0
+argument_list|,
+name|LOCKPID
 argument_list|)
 expr_stmt|;
 block|}
@@ -7164,7 +7182,13 @@ name|lock
 argument_list|,
 name|LK_EXCLUPGRADE
 argument_list|,
-name|curproc
+operator|(
+name|void
+operator|*
+operator|)
+literal|0
+argument_list|,
+name|LOCKPID
 argument_list|)
 condition|)
 block|{
@@ -7220,7 +7244,13 @@ name|lock
 argument_list|,
 name|LK_DOWNGRADE
 argument_list|,
-name|curproc
+operator|(
+name|void
+operator|*
+operator|)
+literal|0
+argument_list|,
+name|LOCKPID
 argument_list|)
 expr_stmt|;
 block|}
