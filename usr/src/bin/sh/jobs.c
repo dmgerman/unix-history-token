32d8250c96af81957778d3248a1af4c010861b0b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-27 21:46:31 -0800

    getpgrp no longer takes an argument the copyright notice was messed up quite a bit
    SCCS-vsn: bin/sh/jobs.c 8.3
    
    Former-commit-id: 4a1e5420ed17e8379d76b450cffdef102f6f4f15

--- usr/src/bin/sh/jobs.c
+++ usr/src/bin/sh/jobs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)jobs.c	8.2 (Berkeley) %G%"
+literal|"@(#)jobs.c	8.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -484,9 +484,7 @@ condition|)
 name|initialpgrp
 operator|=
 name|getpgrp
-argument_list|(
-literal|0
-argument_list|)
+argument_list|()
 expr_stmt|;
 elseif|else
 if|if
@@ -494,9 +492,7 @@ condition|(
 name|initialpgrp
 operator|!=
 name|getpgrp
-argument_list|(
-literal|0
-argument_list|)
+argument_list|()
 condition|)
 block|{
 name|killpg
@@ -2954,9 +2950,7 @@ name|int
 name|mypgrp
 init|=
 name|getpgrp
-argument_list|(
-literal|0
-argument_list|)
+argument_list|()
 decl_stmt|;
 endif|#
 directive|endif
