1fa7af7fcc2be9e551cf4afccdbb9d3bfc26a210
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-04-27 18:46:56 -0800

    move in definition of doff_t and MAXDIRSIZE
    SCCS-vsn: sys/ufs/ufs/dir.h 8.5
    
    Former-commit-id: 4a79001d1748ecd86d63cd240cad6a051e997ca2

--- usr/src/sys/ufs/ufs/dir.h
+++ usr/src/sys/ufs/ufs/dir.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dir.h	8.4 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)dir.h	8.5 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -13,6 +13,24 @@ begin_define
 define|#
 directive|define
 name|_DIR_H_
+end_define
+
+begin_comment
+comment|/*  * Theoretically, directories can be more than 2Gb in length, however, in  * practice this seems unlikely. So, we define the type doff_t as a 32-bit  * quantity to keep down the cost of doing lookup on a 32-bit machine.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|doff_t
+value|int32_t
+end_define
+
+begin_define
+define|#
+directive|define
+name|MAXDIRSIZE
+value|(0x7fffffff)
 end_define
 
 begin_comment
