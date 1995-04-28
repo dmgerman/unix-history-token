87e16c7faf91e23e31af14ad21b3052a96477731
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-27 21:50:19 -0800

    script messed up the copyright notices
    SCCS-vsn: usr.sbin/bad144/bad144.c 8.2
    
    Former-commit-id: 5e5a66d192414bb8d1b937adacda9c74f8fcedf9

--- usr/src/usr.sbin/bad144/bad144.c
+++ usr/src/usr.sbin/bad144/bad144.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993, 198019861988  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  */
+comment|/*  * Copyright (c) 1980, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|copyright
 index|[]
 init|=
-literal|"@(#) Copyright (c) 1993, 198019861988\n\ 	The Regents of the University of California.  All rights reserved.\n"
+literal|"@(#) Copyright (c) 1980, 1986, 1988, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -37,7 +37,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)bad144.c	8.1 (Berkeley) %G%"
+literal|"@(#)bad144.c	8.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
