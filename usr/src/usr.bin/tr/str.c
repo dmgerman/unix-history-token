ff1256284974c45b8d5aa6a45662eec8c11f0f02
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-28 21:11:17 -0800

    We lost when dealing with mappings with unequal ranges, for example 'tr A-Z N-ZA-M', because we didn't skip over the terminating character of the range when processing it.  This works when the ranges line up since they map terminating character to terminating character correctly, but fails when one range maps into two, or with other odd combinations. From: Chris Torek <torek@BSDI.COM>
    SCCS-vsn: usr.bin/tr/str.c 8.2
    
    Former-commit-id: fb3065469f67a1505b56bdb505c04a876b10da1f

--- usr/src/usr.bin/tr/str.c
+++ usr/src/usr.bin/tr/str.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)str.c	8.1 (Berkeley) %G%"
+literal|"@(#)str.c	8.2 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -1264,11 +1264,15 @@ operator|*
 name|s
 operator|->
 name|str
+operator|++
 expr_stmt|;
 if|if
 condition|(
 name|stopval
 operator|<
+operator|(
+name|u_char
+operator|)
 name|s
 operator|->
 name|lastch
