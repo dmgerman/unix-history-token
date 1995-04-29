b70908620b320066e4a7ad7ea0b913f970e4e0a5
Thomas Ferrin <tef@ucbvax.Berkeley.EDU>
1995-04-28 22:41:59 -0800

    Better portability to other systems.
    SCCS-vsn: usr.sbin/lpr/lpr/lpr.c 8.4
    
    Former-commit-id: b69dfb353306a9120d8fe18eaef300046ef28b4f

--- usr/src/usr.sbin/lpr/lpr/lpr.c
+++ usr/src/usr.sbin/lpr/lpr/lpr.c
@@ -53,7 +53,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)lpr.c	8.3 (Berkeley) %G%"
+literal|"@(#)lpr.c	8.4 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -80,6 +80,12 @@ begin_include
 include|#
 directive|include
 file|<sys/stat.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/file.h>
 end_include
 
 begin_include
