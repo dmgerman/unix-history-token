0e281b1d9841c3e20638f523b9c6c2733998e840
David Greenman <dg@FreeBSD.org>
1995-04-30 05:09:13 +0000

    Check for curproc != NULL before dereferencing it.
    Former-commit-id: 0343bd281c85733f6264e26c69e566b5f59a9795

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.41 1995/04/16 11:45:30 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.42 1995/04/16 12:02:42 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -624,12 +624,9 @@ block|{
 if|if
 condition|(
 name|curproc
-operator|&&
-name|curproc
-operator|->
-name|p_stats
+operator|!=
+name|NULL
 condition|)
-comment|/* count block I/O */
 name|curproc
 operator|->
 name|p_stats
@@ -811,12 +808,9 @@ block|{
 if|if
 condition|(
 name|curproc
-operator|&&
-name|curproc
-operator|->
-name|p_stats
+operator|!=
+name|NULL
 condition|)
-comment|/* count block I/O */
 name|curproc
 operator|->
 name|p_stats
@@ -952,10 +946,8 @@ block|{
 if|if
 condition|(
 name|curproc
-operator|&&
-name|curproc
-operator|->
-name|p_stats
+operator|!=
+name|NULL
 condition|)
 name|curproc
 operator|->
@@ -1178,6 +1170,12 @@ argument_list|,
 literal|1
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|curproc
+operator|!=
+name|NULL
+condition|)
 name|curproc
 operator|->
 name|p_stats
