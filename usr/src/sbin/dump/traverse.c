dd794528e4e28e39b1fa10705760a9ff59751932
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-02 07:11:12 -0800

    lint
    SCCS-vsn: sbin/dump/traverse.c 8.6
    
    Former-commit-id: 5576975afd63da818a37922c561d19d67887a61c

--- usr/src/sbin/dump/traverse.c
+++ usr/src/sbin/dump/traverse.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)traverse.c	8.5 (Berkeley) %G%"
+literal|"@(#)traverse.c	8.6 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2539,6 +2539,9 @@ name|loop
 label|:
 if|if
 condition|(
+operator|(
+name|int
+operator|)
 name|lseek
 argument_list|(
 name|diskfd
@@ -2729,6 +2732,9 @@ control|)
 block|{
 if|if
 condition|(
+operator|(
+name|int
+operator|)
 name|lseek
 argument_list|(
 name|diskfd
