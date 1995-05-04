2d238a29199ab90b41d5721900092d652f37a097
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-05-03 18:33:25 -0800

    externs.h -> extern.h
    SCCS-vsn: games/sail/player.h 8.2
    
    Former-commit-id: 12cfa5a50de6f4cb9383049d0d86801b817e8a3f

--- usr/src/games/sail/player.h
+++ usr/src/games/sail/player.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)player.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)player.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -12,7 +12,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"externs.h"
+file|"extern.h"
 end_include
 
 begin_comment
