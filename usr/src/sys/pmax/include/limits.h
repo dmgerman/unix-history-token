906357fe5e7446e3dd3e2a64da3d66a17e9a7ba7
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-04 22:22:44 -0800

    CLK_TCK => 64 (from Ralph)
    SCCS-vsn: sys/pmax/include/limits.h 8.2
    
    Former-commit-id: 7b827d7eea158e3604d7936f518bed33c89139a5

--- usr/src/sys/pmax/include/limits.h
+++ usr/src/sys/pmax/include/limits.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)limits.h	8.1 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)limits.h	8.2 (Berkeley) %G%  */
 end_comment
 
 begin_define
@@ -29,7 +29,7 @@ begin_define
 define|#
 directive|define
 name|CLK_TCK
-value|60
+value|64
 end_define
 
 begin_comment
