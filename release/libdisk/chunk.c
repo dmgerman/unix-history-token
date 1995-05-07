ab82a90d913a02868c53cb716271e897a7b581ff
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-07 01:25:22 +0000

    Always zero new chunks - keeps things like the private structures from having bogus data in them.
    Former-commit-id: 0df4bdad56d96ae22468e6ce73fdb8b26b9ab763

--- release/libdisk/chunk.c
+++ release/libdisk/chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: chunk.c,v 1.7 1995/05/05 07:07:43 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: chunk.c,v 1.8 1995/05/06 03:28:27 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -50,7 +50,7 @@ define|#
 directive|define
 name|new_chunk
 parameter_list|()
-value|malloc(sizeof(struct chunk))
+value|memset(malloc(sizeof(struct chunk), 0, sizeof(struct chunk)))
 end_define
 
 begin_comment
