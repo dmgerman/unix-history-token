b711c54353a9b453cb2a6c7eb85e3962484a132e
John Dyson <dyson@FreeBSD.org>
1995-05-07 03:48:54 +0000

    Fixed a calculation that would once-in-a-while cause the swap_pager to emit spurious page outside of object type messages.  It is not a fatal condition anyway, so the message will be omitted for release.  Also, the code that "clips" the allocation size, associated with the above problem, was fixed.
    Former-commit-id: 829c803b9aa7a8c7ac9a39d38e5996eafa495e73

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) 3/21/94  * $Id: swap_pager.c,v 1.34 1995/04/16 13:58:42 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) 3/21/94  * $Id: swap_pager.c,v 1.35 1995/04/19 10:32:11 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -5337,9 +5337,19 @@ expr_stmt|;
 comment|/* 			 * this code is alittle conservative, but works (the 			 * intent of this code is to allocate small chunks for 			 * small objects) 			 */
 if|if
 condition|(
+operator|(
+name|i
+operator|!=
+literal|1
+operator|)
+operator|&&
+operator|(
+name|foff
+operator|+
 name|ntoget
 operator|*
 name|PAGE_SIZE
+operator|)
 operator|>
 name|object
 operator|->
@@ -5348,6 +5358,7 @@ condition|)
 block|{
 name|ntoget
 operator|=
+operator|(
 operator|(
 name|object
 operator|->
@@ -5359,40 +5370,12 @@ operator|-
 literal|1
 operator|)
 operator|)
+operator|-
+name|foff
+operator|)
 operator|/
 name|PAGE_SIZE
 expr_stmt|;
-comment|/* 				 * make sure that we include the needed page 				 */
-if|if
-condition|(
-name|ntoget
-operator|<=
-name|off
-condition|)
-block|{
-name|printf
-argument_list|(
-literal|"swap_pager_output: page outside of object -- %d, %d\n"
-argument_list|,
-name|m
-index|[
-name|j
-index|]
-operator|->
-name|offset
-argument_list|,
-name|object
-operator|->
-name|size
-argument_list|)
-expr_stmt|;
-name|ntoget
-operator|=
-name|off
-operator|+
-literal|1
-expr_stmt|;
-block|}
 block|}
 name|retrygetspace
 label|:
