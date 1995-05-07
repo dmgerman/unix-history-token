9b0fc79dd3a5bee87ba6fa98433bc82e9534986f
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-07 01:28:16 +0000

    Whoops!  An extra paren somehow got in there.  Fixed.
    Former-commit-id: 6555ab5a6b270d64aa9448bc61a8aa58a1d18417

--- release/libdisk/chunk.c
+++ release/libdisk/chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: chunk.c,v 1.8 1995/05/06 03:28:27 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: chunk.c,v 1.9 1995/05/07 01:25:22 jkh Exp $  *  */
 end_comment
 
 begin_include
@@ -50,7 +50,7 @@ define|#
 directive|define
 name|new_chunk
 parameter_list|()
-value|memset(malloc(sizeof(struct chunk), 0, sizeof(struct chunk)))
+value|memset(malloc(sizeof(struct chunk)), 0, sizeof(struct chunk))
 end_define
 
 begin_comment
