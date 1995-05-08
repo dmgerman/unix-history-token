b57682d521ca6d5649f9707564cb514453564931
Poul-Henning Kamp <phk@FreeBSD.org>
1995-05-08 01:26:47 +0000

    I bet Jordan is tearing his hair out, trying to figure the previous patch out... here is the real thing.
    Former-commit-id: 790c2798e7db43c024aff747a5e38c27b61c93b9

--- lib/libdisk/create_chunk.c
+++ lib/libdisk/create_chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.10 1995/05/06 03:28:28 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.11 1995/05/08 00:44:46 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -294,10 +294,9 @@ control|)
 block|{
 if|if
 condition|(
-operator|!
 name|strcmp
 argument_list|(
-name|c
+name|c1
 operator|->
 name|name
 argument_list|,
