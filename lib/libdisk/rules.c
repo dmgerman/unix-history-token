5c0073cfbba38bc13c496e71f71487cb6221b131
Poul-Henning Kamp <phk@FreeBSD.org>
1995-05-08 01:34:31 +0000

    Add check for rootfs > cyl 1024.
    Former-commit-id: 75a6c974c235ade2ee9d50dc3bf684b924623373

--- lib/libdisk/rules.c
+++ lib/libdisk/rules.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: rules.c,v 1.7 1995/05/05 07:07:45 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: rules.c,v 1.8 1995/05/06 03:28:31 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -928,9 +928,31 @@ name|flags
 operator|&
 name|CHUNK_IS_ROOT
 condition|)
+block|{
 name|k
 operator|++
 expr_stmt|;
+if|if
+condition|(
+name|c1
+operator|->
+name|flags
+operator|&
+name|CHUNK_PAST_1024
+condition|)
+name|sprintf
+argument_list|(
+name|msg
+operator|+
+name|strlen
+argument_list|(
+name|msg
+argument_list|)
+argument_list|,
+literal|"Root filesystem extends past cylinder 1024, and cannot be booted from\n"
+argument_list|)
+expr_stmt|;
+block|}
 name|i
 operator|++
 expr_stmt|;
