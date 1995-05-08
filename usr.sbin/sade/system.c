7098f632e5e62a2f1ec1c56493a2cf30beae4384
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-08 06:06:30 +0000

    Ok, we should now create all filesystems, mount them and extract the cpio floppy at this point.
    Former-commit-id: 4cefac3d2fe03e04594fbc32ccdffe042298ecd8

--- usr.sbin/sade/system.c
+++ usr.sbin/sade/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.6 1995/05/06 09:34:22 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE"  * license, so buy him a beer if you like it!  Buy him a beer for me, too!  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.7 1995/05/07 03:38:03 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE"  * license, so buy him a beer if you like it!  Buy him a beer for me, too!  */
 end_comment
 
 begin_include
@@ -1070,18 +1070,7 @@ argument_list|)
 expr_stmt|;
 name|vsnprintf
 argument_list|(
-operator|(
-name|char
-operator|*
-operator|)
-operator|(
 name|cmd
-operator|+
-name|strlen
-argument_list|(
-name|cmd
-argument_list|)
-operator|)
 argument_list|,
 name|FILENAME_MAX
 argument_list|,
@@ -1107,6 +1096,15 @@ operator|=
 name|system
 argument_list|(
 name|cmd
+argument_list|)
+expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"Command `%s' returns status of %d\n"
+argument_list|,
+name|cmd
+argument_list|,
+name|i
 argument_list|)
 expr_stmt|;
 name|free
