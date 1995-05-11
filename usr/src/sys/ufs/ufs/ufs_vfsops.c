24c273eb1154e54c78b8d0486b68a4681eb1f647
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-10 16:52:09 -0800

    doforce is gone
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 8.7
    
    Former-commit-id: 129717c0f316f65d90606412c587476fc1c2e610

--- usr/src/sys/ufs/ufs/ufs_vfsops.c
+++ usr/src/sys/ufs/ufs/ufs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	8.6 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)ufs_vfsops.c	8.7 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -92,18 +92,6 @@ include|#
 directive|include
 file|<ufs/ufs/ufs_extern.h>
 end_include
-
-begin_comment
-comment|/*  * Flag to permit forcible unmounting.  */
-end_comment
-
-begin_decl_stmt
-name|int
-name|doforce
-init|=
-literal|1
-decl_stmt|;
-end_decl_stmt
 
 begin_comment
 comment|/*  * Make a filesystem operational.  * Nothing to do at the moment.  */
