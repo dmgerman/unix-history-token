a8c88334dfe6b0d51cfd361354acd6834b00f969
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1995-05-10 18:19:42 -0800

    don't call VFS_SYNC for read-only filesystems
    SCCS-vsn: sys/kern/vfs_syscalls.c 8.36
    
    Former-commit-id: 23f1f1788087ab198bd5fe2413d87f79d023728e

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	8.35 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_syscalls.c	8.36 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1753,6 +1753,15 @@ comment|/* remove cache entries for this file sys */
 if|if
 condition|(
 operator|(
+operator|(
+name|mp
+operator|->
+name|mnt_flag
+operator|&
+name|MNT_RDONLY
+operator|)
+operator|||
+operator|(
 name|error
 operator|=
 name|VFS_SYNC
@@ -1770,6 +1779,7 @@ argument_list|)
 operator|)
 operator|==
 literal|0
+operator|)
 operator|||
 operator|(
 name|flags
