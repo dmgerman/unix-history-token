e40d507464590cb7ff90bd7ab3184d0edc0214f6
Bruce Evans <bde@FreeBSD.org>
1995-05-11 07:44:40 +0000

    Add variable `idelayed' and macros setdelayed() and schedsofttty() to access it.  setdelayed() actually ORs the bits in `idelayed' into `ipending' and clears `idelayed'.
    Call setdelayed() every (normal) clock tick to convert delayed
    interrupts into pending ones.
    
    Drivers can set bits in `idelayed' at any time to schedule an interrupt
    at the next clock tick.  This is more efficient than calling timeout().
    Currently only software interrupts can be scheduled.
    
    
    Former-commit-id: 1088afdc521831e5b29bdad64b50e8ced9e5973e

--- sys/i386/include/spl.h
+++ sys/i386/include/spl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: spl.h,v 1.6 1994/12/30 12:42:15 bde Exp $  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: spl.h,v 1.7 1995/05/11 00:13:01 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -160,6 +160,18 @@ begin_decl_stmt
 specifier|extern
 specifier|volatile
 name|unsigned
+name|idelayed
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* interrupts to become pending */
+end_comment
+
+begin_decl_stmt
+specifier|extern
+specifier|volatile
+name|unsigned
 name|ipending
 decl_stmt|;
 end_decl_stmt
@@ -208,6 +220,14 @@ end_comment
 begin_define
 define|#
 directive|define
+name|setdelayed
+parameter_list|()
+value|(*(unsigned *)&ipending |= loadandclear(&idelayed))
+end_define
+
+begin_define
+define|#
+directive|define
 name|setsoftast
 parameter_list|()
 value|(*(unsigned *)&ipending |= SWI_AST_PENDING)
@@ -235,6 +255,14 @@ directive|define
 name|setsofttty
 parameter_list|()
 value|(*(unsigned *)&ipending |= SWI_TTY_PENDING)
+end_define
+
+begin_define
+define|#
+directive|define
+name|schedsofttty
+parameter_list|()
+value|(*(unsigned *)&idelayed |= SWI_TTY_PENDING)
 end_define
 
 begin_decl_stmt
