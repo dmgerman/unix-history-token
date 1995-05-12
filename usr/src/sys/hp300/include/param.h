c0feebdc6b40874a0cf62688a0fdd39ed6764e90
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-11 21:46:28 -0800

    add simple locking primatives
    SCCS-vsn: sys/hp300/include/param.h 8.3
    
    Former-commit-id: b44f6cf86bfeb6d696761825ac51f71140fc9d8e

--- usr/src/sys/hp300/include/param.h
+++ usr/src/sys/hp300/include/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machparam.h 1.16 92/12/20$  *  *	@(#)param.h	8.2 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * %sccs.include.redist.c%  *  * from: Utah $Hdr: machparam.h 1.16 92/12/20$  *  *	@(#)param.h	8.3 (Berkeley) %G%  */
 end_comment
 
 begin_comment
