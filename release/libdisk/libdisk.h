0633170bd0bd82137ab55b0c4893c5a6ae0dc24f
Poul-Henning Kamp <phk@FreeBSD.org>
1995-05-12 18:50:00 +0000

    Added MakeDevDisk(struct disk*, char *path); fixed bug in extended slice naming.
    Former-commit-id: 1aefbfdebd4ce986870d8c1b40e6a26955bdd097

--- release/libdisk/libdisk.h
+++ release/libdisk/libdisk.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: libdisk.h,v 1.14 1995/05/08 02:08:30 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: libdisk.h,v 1.15 1995/05/10 05:57:03 phk Exp $  *  */
 end_comment
 
 begin_define
@@ -659,6 +659,26 @@ end_function_decl
 
 begin_comment
 comment|/* This one creates a partition inside the given parent of the given 	 * size, and returns a pointer to it.  The first unused chunk big 	 * enough is used. 	 */
+end_comment
+
+begin_function_decl
+name|void
+name|MakeDevDisk
+parameter_list|(
+name|struct
+name|disk
+modifier|*
+name|d
+parameter_list|,
+name|char
+modifier|*
+name|path
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_comment
+comment|/* Make device nodes for all chunks on this disk */
 end_comment
 
 begin_comment
--- release/libdisk/tst01.c
+++ release/libdisk/tst01.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: tst01.c,v 1.11 1995/05/10 05:57:04 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: tst01.c,v 1.12 1995/05/11 05:22:54 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -2805,6 +2805,27 @@ argument_list|,
 literal|0
 argument_list|)
 argument_list|)
+argument_list|)
+expr_stmt|;
+continue|continue;
+block|}
+if|if
+condition|(
+operator|!
+name|strcasecmp
+argument_list|(
+operator|*
+name|cmds
+argument_list|,
+literal|"mknod"
+argument_list|)
+condition|)
+block|{
+name|MakeDevDisk
+argument_list|(
+name|d
+argument_list|,
+literal|"/tmp"
 argument_list|)
 expr_stmt|;
 continue|continue;
