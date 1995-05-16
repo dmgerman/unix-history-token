7be9f9b4f7a46f8aa5e3b1d52a0fe94b5466a511
David Greenman <dg@FreeBSD.org>
1995-05-16 01:28:29 +0000

    These diffs modify the behaviour of multicast clients to conform with the IGMPv2 spec.  This fixes the following bugs:
    o ntohs() on a char provides silly results
    o timer needs to be scaled to units of PR_FASTHZ; this was being done
      inconsistenly so now it gets done when it is initialized.
    
    Reviewed by:	Garrett Wollman
    Submitted by:	Bill Fenner <fenner@parc.xerox.com>
    
    
    Former-commit-id: 5707e9d13a2965a267a150948324500c6c572f50

--- sys/netinet/igmp.c
+++ sys/netinet/igmp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)igmp.c	8.1 (Berkeley) 7/19/93  * $Id: igmp.c,v 1.8 1995/03/16 18:14:49 bde Exp $  */
+comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)igmp.c	8.1 (Berkeley) 7/19/93  * $Id: igmp.c,v 1.9 1995/04/26 18:10:53 pst Exp $  */
 end_comment
 
 begin_comment
@@ -559,7 +559,6 @@ name|router_info
 modifier|*
 name|rti
 decl_stmt|;
-specifier|static
 name|int
 name|timer
 decl_stmt|;
@@ -720,12 +719,13 @@ argument_list|)
 expr_stmt|;
 name|timer
 operator|=
-name|ntohs
-argument_list|(
 name|igmp
 operator|->
 name|igmp_code
-argument_list|)
+operator|*
+name|PR_FASTHZ
+operator|/
+name|IGMP_TIMER_SCALE
 expr_stmt|;
 name|rti
 operator|=
@@ -1008,10 +1008,6 @@ operator|->
 name|inm_timer
 operator|>
 name|timer
-operator|*
-name|PR_FASTHZ
-operator|/
-name|IGMP_TIMER_SCALE
 operator|)
 operator|&&
 name|inm
@@ -1171,10 +1167,6 @@ operator|->
 name|inm_timer
 operator|>
 name|timer
-operator|*
-name|PR_FASTHZ
-operator|/
-name|IGMP_TIMER_SCALE
 operator|)
 condition|)
 block|{
