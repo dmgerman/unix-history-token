c75dda4c327cca63adff561ea5f688c4cdccaa4a
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1995-05-18 04:19:48 -0800

    accounting for locks and unlocks
    SCCS-vsn: sys/sys/proc.h 8.14
    
    Former-commit-id: a15c3368f02d378ce34f810f762794028b2c55f0

--- usr/src/sys/sys/proc.h
+++ usr/src/sys/sys/proc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)proc.h	8.13 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)proc.h	8.14 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -326,10 +326,14 @@ modifier|*
 name|p_textvp
 decl_stmt|;
 comment|/* Vnode of executable. */
+name|int
+name|p_locks
+decl_stmt|;
+comment|/* DEBUG: lockmgr count of held locks */
 name|long
 name|p_spare
 index|[
-literal|3
+literal|2
 index|]
 decl_stmt|;
 comment|/* pad to 256, avoid shifting eproc. */
