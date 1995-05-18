917f42c4d90917a526097eaba16fe53f448cbcc1
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-18 15:58:32 +0000

    Clear the screen after changing the font.
    Former-commit-id: 2332b2ab7d1c5e143ba8b20f8c719b368bb34350

--- release/sysinstall/system.c
+++ release/sysinstall/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.14 1995/05/18 09:02:02 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE"  * license, so buy him a beer if you like it!  Buy him a beer for me, too!  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.15 1995/05/18 15:29:46 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE"  * license, so buy him a beer if you like it!  Buy him a beer for me, too!  */
 end_comment
 
 begin_include
@@ -1002,6 +1002,9 @@ expr_stmt|;
 name|noecho
 argument_list|()
 expr_stmt|;
+name|dialog_clear
+argument_list|()
+expr_stmt|;
 block|}
 else|else
 block|{
@@ -1038,6 +1041,9 @@ name|cbreak
 argument_list|()
 expr_stmt|;
 name|noecho
+argument_list|()
+expr_stmt|;
+name|dialog_clear
 argument_list|()
 expr_stmt|;
 block|}
