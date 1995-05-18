bf2a7e9a263429e0272c4a706d5cf63b8db0d2c9
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-18 02:33:25 -0800

    get rid of attempt to detect hanging
    SCCS-vsn: sys/kern/vfs_vnops.c 8.12
    
    Former-commit-id: c04a37cddcc8a4c9a60470a36bb6757c467cf1d4

--- usr/src/sys/kern/vfs_vnops.c
+++ usr/src/sys/kern/vfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.11 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.12 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -2213,22 +2213,6 @@ operator|(
 name|error
 operator|)
 return|;
-ifdef|#
-directive|ifdef
-name|DEBUG
-if|if
-condition|(
-name|error
-operator|==
-name|EWOULDBLOCK
-condition|)
-name|panic
-argument_list|(
-literal|"vn_lock: hung lock"
-argument_list|)
-expr_stmt|;
-endif|#
-directive|endif
 block|}
 name|flags
 operator|&=
