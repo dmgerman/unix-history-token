9dcba27cb364aeb9ce5c7f849fc64ec3a711fb22
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-19 04:17:38 +0000

    Whoops!  Put the helpful message telling you to look at the second screen on the first screen, and not the second.. :-)
    Former-commit-id: 7616a97ebce2b916d294e368ef2833366d3cdd28

--- usr.sbin/sysinstall/system.c
+++ usr.sbin/sysinstall/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.16 1995/05/18 15:58:32 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE"  * license, so buy him a beer if you like it!  Buy him a beer for me, too!  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.17 1995/05/18 17:49:09 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE"  * license, so buy him a beer if you like it!  Buy him a beer for me, too!  */
 end_comment
 
 begin_include
@@ -1196,6 +1196,11 @@ operator|-
 literal|1
 condition|)
 block|{
+name|msgInfo
+argument_list|(
+literal|"Command output is on debugging screen - type ALT-F2 to see it"
+argument_list|)
+expr_stmt|;
 name|dup2
 argument_list|(
 name|DebugFD
@@ -1215,11 +1220,6 @@ argument_list|(
 name|DebugFD
 argument_list|,
 literal|2
-argument_list|)
-expr_stmt|;
-name|msgInfo
-argument_list|(
-literal|"Command output is on debugging screen - type ALT-F2 to see it"
 argument_list|)
 expr_stmt|;
 block|}
