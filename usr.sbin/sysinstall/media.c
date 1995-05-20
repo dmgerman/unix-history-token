0fca47ccc44d60f99ed3741e096cfcce43a807c0
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-20 11:10:35 +0000

    Fix the DOS discovery code to now re-allocate another virtual device if it's called multiple times in a row.  Add a new device type "DEVICE_TYPE_DOS" so that we can look up an previous results.
    Former-commit-id: 1d035e2e45f463f28940f23995a99114ee27c49a

--- usr.sbin/sysinstall/media.c
+++ usr.sbin/sysinstall/media.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated to essentially a complete rewrite.  *  * $Id: media.c,v 1.8 1995/05/20 03:49:09 gpalmer Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,   *    verbatim and that no modifications are made prior to this   *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated to essentially a complete rewrite.  *  * $Id: media.c,v 1.9 1995/05/20 10:33:06 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,   *    verbatim and that no modifications are made prior to this   *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -407,6 +407,33 @@ name|deviceFind
 argument_list|(
 name|NULL
 argument_list|,
+name|DEVICE_TYPE_DOS
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|devs
+condition|)
+block|{
+comment|/* XXX If count> 1 then at some point then we should put up a menu and allow the user to choose XXX */
+name|mediaDevice
+operator|=
+name|devs
+index|[
+literal|0
+index|]
+expr_stmt|;
+return|return
+literal|1
+return|;
+block|}
+else|else
+name|devs
+operator|=
+name|deviceFind
+argument_list|(
+name|NULL
+argument_list|,
 name|DEVICE_TYPE_DISK
 argument_list|)
 expr_stmt|;
@@ -415,11 +442,17 @@ condition|(
 operator|!
 name|devs
 condition|)
+block|{
 name|msgConfirm
 argument_list|(
 literal|"No disk devices found!"
 argument_list|)
 expr_stmt|;
+return|return
+literal|0
+return|;
+block|}
+comment|/* Now go chewing through looking for a DOS FAT partition */
 for|for
 control|(
 name|i
@@ -435,17 +468,6 @@ name|i
 operator|++
 control|)
 block|{
-if|if
-condition|(
-operator|!
-name|devs
-index|[
-name|i
-index|]
-operator|->
-name|enabled
-condition|)
-continue|continue;
 name|d
 operator|=
 operator|(
@@ -505,7 +527,7 @@ name|c1
 operator|->
 name|name
 argument_list|,
-name|DEVICE_TYPE_DISK
+name|DEVICE_TYPE_DOS
 argument_list|,
 name|TRUE
 argument_list|,
