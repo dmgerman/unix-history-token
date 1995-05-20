d986086bf04162bcc14e060f01cc60256d03b9c8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-20 00:09:43 -0800

    vget no longer returns a reference on failure
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 8.30
    
    Former-commit-id: 0a92e88a4668962f1c10882ebd2cf0ea6b979c91

--- usr/src/sys/ufs/ffs/ffs_vfsops.c
+++ usr/src/sys/ufs/ffs/ffs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	8.29 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1989, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)ffs_vfsops.c	8.30 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -4093,11 +4093,6 @@ condition|(
 name|error
 condition|)
 block|{
-name|vrele
-argument_list|(
-name|vp
-argument_list|)
-expr_stmt|;
 name|simple_lock
 argument_list|(
 operator|&
