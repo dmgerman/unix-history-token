5210467e23c98f12f3f273f2469bc60df3e92010
Gary Palmer <gpalmer@FreeBSD.org>
1995-05-21 20:41:28 +0000

    1. Correct the path for distributions on the CDROM
    2. Put msgDebug() calls into the genericGteDist routine to see
       why it is locking.
    
    
    Former-commit-id: 380268e506b73be99bc71758d1f0453abf5cfa0f

--- release/sysinstall/media_strategy.c
+++ release/sysinstall/media_strategy.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated to essentially a complete rewrite.  *  * $Id: media_strategy.c,v 1.8 1995/05/21 19:29:17 gpalmer Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,   *    verbatim and that no modifications are made prior to this   *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated to essentially a complete rewrite.  *  * $Id: media_strategy.c,v 1.9 1995/05/21 19:51:50 gpalmer Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,   *    verbatim and that no modifications are made prior to this   *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -848,6 +848,13 @@ literal|"pieces"
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"Attempting to extract distribution from %u files\n"
+argument_list|,
+name|numchunks
+argument_list|)
+expr_stmt|;
 name|pipe
 argument_list|(
 name|pfd
@@ -931,6 +938,13 @@ literal|26
 operator|)
 argument_list|)
 expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"Opening %s\n"
+argument_list|,
+name|buf
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|(
@@ -959,6 +973,15 @@ name|fd
 argument_list|,
 operator|&
 name|sb
+argument_list|)
+expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"mmap()ing %s (%d)\n"
+argument_list|,
+name|buf
+argument_list|,
+name|fd
 argument_list|)
 expr_stmt|;
 name|memory
@@ -1003,6 +1026,11 @@ name|errno
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"writing out mmap() space\n"
+argument_list|)
+expr_stmt|;
 name|write
 argument_list|(
 literal|1
@@ -1014,6 +1042,13 @@ operator|.
 name|st_size
 argument_list|)
 expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"munmapping %s\n"
+argument_list|,
+name|buf
+argument_list|)
+expr_stmt|;
 name|munmap
 argument_list|(
 name|memory
@@ -1021,6 +1056,13 @@ argument_list|,
 name|sb
 operator|.
 name|st_size
+argument_list|)
+expr_stmt|;
+name|msgDebug
+argument_list|(
+literal|"closing %s\n"
+argument_list|,
+name|buf
 argument_list|)
 expr_stmt|;
 name|close
@@ -1266,7 +1308,7 @@ name|buf
 argument_list|,
 name|PATH_MAX
 argument_list|,
-literal|"/mnt/%s"
+literal|"/mnt/dists/%s"
 argument_list|,
 name|dist
 argument_list|)
