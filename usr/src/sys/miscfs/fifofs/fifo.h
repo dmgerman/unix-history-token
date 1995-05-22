e7ca5f428689273957d711be61c71a8c6e8cfead
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-22 01:09:34 -0800

    fifo_inactive becomes real
    SCCS-vsn: sys/miscfs/fifofs/fifo.h 8.6
    
    Former-commit-id: 2dffda6a9e4de2228c7930106cd0d8373e500b78

--- usr/src/sys/miscfs/fifofs/fifo.h
+++ usr/src/sys/miscfs/fifofs/fifo.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)fifo.h	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_ifdef
@@ -254,12 +254,19 @@ name|fifo_abortop
 value|((int (*) __P((struct  vop_abortop_args *)))fifo_badop)
 end_define
 
-begin_define
-define|#
-directive|define
+begin_decl_stmt
+name|int
 name|fifo_inactive
-value|((int (*) __P((struct  vop_inactive_args *)))nullop)
-end_define
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vop_inactive_args
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_define
 define|#
