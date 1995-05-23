d34ceba55f2fedae4687bc9f2fcefeeef85e9264
Mike Karels <karels@ucbvax.Berkeley.EDU>
1995-05-23 15:25:21 -0800

    Always check the checksum on incoming packets that have them, redefining udpcksum to control only generation of checksums
    SCCS-vsn: sys/netinet/udp_usrreq.c 8.6
    
    Former-commit-id: ac430b5fee7debdfcb40ef6b8e51aaa4725325d4

--- usr/src/sys/netinet/udp_usrreq.c
+++ usr/src/sys/netinet/udp_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)udp_usrreq.c	8.5 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)udp_usrreq.c	8.6 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -482,8 +482,6 @@ expr_stmt|;
 comment|/* 	 * Checksum extended UDP header and data. 	 */
 if|if
 condition|(
-name|udpcksum
-operator|&&
 name|uh
 operator|->
 name|uh_sum
