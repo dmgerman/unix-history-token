9f1c97c09aaf32ab1a66efd6a6165bc18176a213
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-24 22:39:42 -0800

    include BSD ioctl calls in Solaris
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.166
    
    Former-commit-id: 278587da847f66aa5fc70304e4acb23a8fb05125

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.165 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.166 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -1271,6 +1271,17 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_define
+define|#
+directive|define
+name|BSD_COMP
+value|1
+end_define
+
+begin_comment
+comment|/* get BSD ioctl calls */
+end_comment
 
 begin_include
 include|#
