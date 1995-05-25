d84365f508b199a8b94e436f7261e52f4448fd13
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-05-25 01:52:04 +0000

    Cleanup.
    Former-commit-id: b420da148a3fc3c74c8159e313649345106ccb30

--- usr.sbin/sysinstall/system.c
+++ usr.sbin/sysinstall/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.31 1995/05/24 23:36:50 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.32 1995/05/24 23:43:59 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
 end_comment
 
 begin_include
@@ -397,7 +397,7 @@ argument_list|)
 expr_stmt|;
 name|execlp
 argument_list|(
-literal|"/stand/sh"
+literal|"sh"
 argument_list|,
 literal|"-sh"
 argument_list|,
