022941eb6bf10656e0a9d09b82eebf02f36b9231
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-25 19:46:09 -0800

    patch for NeXTstep
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.171
    
    Former-commit-id: 2ff0650a39dfd2ed83aab1c5925a759d7b80d434

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.170 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.171 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -2168,6 +2168,17 @@ end_define
 
 begin_comment
 comment|/* has initgroups(3) call */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NEEDPUTENV
+value|1
+end_define
+
+begin_comment
+comment|/* need putenv(3) call */
 end_comment
 
 begin_ifndef
