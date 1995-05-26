d22223fa026d185a513a0cd8d7a5f26d1cc77bd7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-26 14:10:56 -0800

    add MF_KEEPQUOTES flag to turn off default quote stripping
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.134
    
    Former-commit-id: 1eff768b183b89f2933c2a23c8e791f663388043

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.133 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.134 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.133		%G%"
+literal|"@(#)sendmail.h	8.134		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3268,6 +3268,17 @@ end_define
 
 begin_comment
 comment|/* append new entry on rebuiled */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MF_KEEPQUOTES
+value|0x00010000
+end_define
+
+begin_comment
+comment|/* don't dequote key before lookup */
 end_comment
 
 begin_comment
