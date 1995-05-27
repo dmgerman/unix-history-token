09cb251a5b4928fa1bb28eb5af2d94f0400bbeac
David Greenman <dg@FreeBSD.org>
1995-05-27 04:37:24 +0000

    Added a fix for a bug which caused the wrong interface to be selected for broadcasts if point-to-point links shared the same IP address as the ethernet. The fix must be enabled with P2P_LOCALADDR_SHARE option in the kernel config file. This will someday likely be standard, but there isn't sufficient time before release to determine if there are any interoperability problems with routed and/or gated.
    Reviewed by:	Garrett Wollman, and me
    Submitted by:	Peter Wemm
    
    
    Former-commit-id: d89959d76b8f53cc79616e29beb484b0af516fa6

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.c	8.3 (Berkeley) 1/4/94  * $Id: if.c,v 1.12 1994/12/30 06:46:20 davidg Exp $  */
+comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.c	8.3 (Berkeley) 1/4/94  * $Id: if.c,v 1.13 1995/02/24 11:47:31 davidg Exp $  */
 end_comment
 
 begin_include
@@ -1188,6 +1188,7 @@ name|ifp
 operator|->
 name|if_next
 control|)
+block|{
 for|for
 control|(
 name|ifa
@@ -1235,6 +1236,40 @@ condition|)
 name|next
 label|:
 continue|continue;
+ifdef|#
+directive|ifdef
+name|P2P_LOCALADDR_SHARE
+if|if
+condition|(
+name|ifp
+operator|->
+name|if_flags
+operator|&
+name|IFF_POINTOPOINT
+condition|)
+block|{
+if|if
+condition|(
+name|equal
+argument_list|(
+name|addr
+argument_list|,
+name|ifa
+operator|->
+name|ifa_addr
+argument_list|)
+condition|)
+return|return
+operator|(
+name|ifa
+operator|)
+return|;
+block|}
+else|else
+endif|#
+directive|endif
+comment|/* P2P_LOCALADDR_SHARE */
+block|{
 name|cp
 operator|=
 name|addr_data
@@ -1323,6 +1358,8 @@ name|ifa_maybe
 operator|=
 name|ifa
 expr_stmt|;
+block|}
+block|}
 block|}
 return|return
 operator|(
@@ -1566,6 +1603,40 @@ operator|)
 return|;
 continue|continue;
 block|}
+ifdef|#
+directive|ifdef
+name|P2P_LOCALADDR_SHARE
+if|if
+condition|(
+name|ifp
+operator|->
+name|if_flags
+operator|&
+name|IFF_POINTOPOINT
+condition|)
+block|{
+if|if
+condition|(
+name|equal
+argument_list|(
+name|addr
+argument_list|,
+name|ifa
+operator|->
+name|ifa_dstaddr
+argument_list|)
+condition|)
+return|return
+operator|(
+name|ifa
+operator|)
+return|;
+block|}
+else|else
+endif|#
+directive|endif
+comment|/* P2P_LOCALADDR_SHARE */
+block|{
 name|cp
 operator|=
 name|addr
@@ -1641,6 +1712,7 @@ operator|(
 name|ifa
 operator|)
 return|;
+block|}
 block|}
 return|return
 operator|(
