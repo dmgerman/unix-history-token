238164deeefd6efb332a9e0a6516554d8650af86
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-29 16:03:24 -0800

    allow trusted users to use -bs without a warning
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.76
    
    Former-commit-id: 6e2cf3c08b9e28af6fa5e774102c050a6f0199e7

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -27,7 +27,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.75 (Berkeley) %G% (with SMTP)"
+literal|"@(#)srvrsmtp.c	8.76 (Berkeley) %G% (with SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -42,7 +42,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)srvrsmtp.c	8.75 (Berkeley) %G% (without SMTP)"
+literal|"@(#)srvrsmtp.c	8.76 (Berkeley) %G% (without SMTP)"
 decl_stmt|;
 end_decl_stmt
 
@@ -1594,6 +1594,14 @@ operator|&&
 name|OpMode
 operator|==
 name|MD_SMTP
+operator|&&
+operator|!
+name|wordinclass
+argument_list|(
+name|RealUserName
+argument_list|,
+literal|'t'
+argument_list|)
 operator|&&
 operator|!
 name|bitnset
