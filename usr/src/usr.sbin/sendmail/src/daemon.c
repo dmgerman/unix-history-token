0602143dcbef90b6e35722d5d93c1b4092ca7a43
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-31 00:25:41 -0800

    move setproctitle() to be before every accept to fix problem that looses process title after fork() in BSD-OS 2.0
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.94
    
    Former-commit-id: 48322ed8a335712f1707f66e62fbd77491fe3455

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -39,7 +39,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.93 (Berkeley) %G% (with daemon mode)"
+literal|"@(#)daemon.c	8.94 (Berkeley) %G% (with daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
@@ -54,7 +54,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)daemon.c	8.93 (Berkeley) %G% (without daemon mode)"
+literal|"@(#)daemon.c	8.94 (Berkeley) %G% (without daemon mode)"
 decl_stmt|;
 end_decl_stmt
 
