884b343145ef034fc08111a61fc5a98d167fca5a
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-06-06 09:27:10 +0000

    What was I thinking?  That umask() was totally bogus!
    Former-commit-id: 84e5f022510b9bb7c48f126599f7f9ad779961ef

--- release/sysinstall/system.c
+++ release/sysinstall/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.43.2.9 1995/06/05 03:15:49 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.43.2.10 1995/06/05 18:53:54 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
 end_comment
 
 begin_include
@@ -200,12 +200,6 @@ literal|0
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* Don't be overly conservative */
-name|umask
-argument_list|(
-literal|0755
-argument_list|)
-expr_stmt|;
 for|for
 control|(
 name|i
