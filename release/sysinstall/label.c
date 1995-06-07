bb2aa57f38a4d913a034c12f1c4a7e009cebdca2
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-06-07 06:38:11 +0000

    Don't toggle newfs state to N every time with each mount, only the first one.
    Former-commit-id: bebefb6ed9b35362b95d8e73ef7865e53624071c

--- release/sysinstall/label.c
+++ release/sysinstall/label.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: label.c,v 1.31.2.2 1995/05/31 22:02:37 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: label.c,v 1.31.2.3 1995/06/05 08:34:55 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -679,6 +679,15 @@ name|target
 decl_stmt|,
 name|divisor
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|mpoint
+condition|)
+name|mpoint
+operator|=
+literal|"/change_me"
+expr_stmt|;
 name|ret
 operator|=
 operator|(
@@ -1986,6 +1995,9 @@ decl_stmt|;
 name|PartInfo
 modifier|*
 name|p
+decl_stmt|,
+modifier|*
+name|oldp
 decl_stmt|;
 name|PartType
 name|type
@@ -2813,6 +2825,17 @@ case|:
 case|case
 name|PART_FILESYSTEM
 case|:
+name|oldp
+operator|=
+name|label_chunk_info
+index|[
+name|here
+index|]
+operator|.
+name|c
+operator|->
+name|private
+expr_stmt|;
 name|p
 operator|=
 name|get_mountpoint
@@ -2830,6 +2853,11 @@ condition|(
 name|p
 condition|)
 block|{
+if|if
+condition|(
+operator|!
+name|oldp
+condition|)
 name|p
 operator|->
 name|newfs
@@ -2977,15 +3005,6 @@ operator|.
 name|type
 operator|==
 name|PART_FILESYSTEM
-operator|&&
-name|label_chunk_info
-index|[
-name|here
-index|]
-operator|.
-name|c
-operator|->
-name|private
 condition|)
 block|{
 name|PartInfo
@@ -3019,13 +3038,21 @@ operator|=
 name|new_part
 argument_list|(
 name|pi
+condition|?
+name|pi
 operator|->
 name|mountpoint
+else|:
+name|NULL
 argument_list|,
+name|pi
+condition|?
 operator|!
 name|pi
 operator|->
 name|newfs
+else|:
+name|TRUE
 argument_list|,
 name|label_chunk_info
 index|[
