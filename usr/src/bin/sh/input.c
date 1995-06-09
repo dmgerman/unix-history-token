e3bb661acd3ec69c6c5f8a9b9201e3ad99acdd2c
Christos Zoulas <christos@ucbvax.Berkeley.EDU>
1995-06-09 04:47:21 -0800

    added string.h
    SCCS-vsn: bin/sh/input.c 8.3
    
    Former-commit-id: fa900f47b27777ba4cf02813a39b204c38bdd7f5

--- usr/src/bin/sh/input.c
+++ usr/src/bin/sh/input.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)input.c	8.2 (Berkeley) %G%"
+literal|"@(#)input.c	8.3 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -60,6 +60,12 @@ begin_include
 include|#
 directive|include
 file|<stdlib.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<string.h>
 end_include
 
 begin_comment
