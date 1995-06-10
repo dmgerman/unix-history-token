9af76843a6012c0a759190a261803307503a0e4c
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-06-10 07:54:04 +0000

    Fix a bug that resulted in my optimization of the single-piece case.
    Former-commit-id: c56788002ee485e3d9a3949d02a2a2ac2c8424ad

--- release/sysinstall/dist.c
+++ release/sysinstall/dist.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: dist.c,v 1.35.2.30 1995/06/09 11:51:21 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: dist.c,v 1.35.2.31 1995/06/09 12:19:12 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -1097,6 +1097,8 @@ name|DIST_XF86_BIN
 operator||
 name|DIST_XF86_LIB
 operator||
+name|DIST_XF86_PROG
+operator||
 name|DIST_XF86_MAN
 operator||
 name|DIST_XF86_SERVER
@@ -1850,114 +1852,7 @@ argument_list|,
 name|numchunks
 argument_list|)
 expr_stmt|;
-comment|/* Optimize the single-chunk case */
-if|if
-condition|(
-name|numchunks
-operator|==
-literal|1
-condition|)
-block|{
-name|snprintf
-argument_list|(
-name|buf
-argument_list|,
-literal|512
-argument_list|,
-literal|"%s/%s.aa"
-argument_list|,
-name|path
-argument_list|,
-name|dist
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|isDebug
-argument_list|()
-condition|)
-name|msgDebug
-argument_list|(
-literal|"Trying for single-piece: %s\n"
-argument_list|,
-name|buf
-argument_list|)
-expr_stmt|;
-name|fd
-operator|=
-call|(
-modifier|*
-name|mediaDevice
-operator|->
-name|get
-call|)
-argument_list|(
-name|mediaDevice
-argument_list|,
-name|buf
-argument_list|,
-name|dist_attr
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|fd
-operator|<
-literal|0
-condition|)
-return|return
-name|FALSE
-return|;
-name|msgNotify
-argument_list|(
-literal|"Extracting %s into %s directory..."
-argument_list|,
-name|me
-index|[
-name|i
-index|]
-operator|.
-name|my_name
-argument_list|,
-name|me
-index|[
-name|i
-index|]
-operator|.
-name|my_dir
-argument_list|)
-expr_stmt|;
-name|status
-operator|=
-name|mediaExtractDist
-argument_list|(
-name|me
-index|[
-name|i
-index|]
-operator|.
-name|my_dir
-argument_list|,
-name|fd
-argument_list|)
-expr_stmt|;
-call|(
-modifier|*
-name|mediaDevice
-operator|->
-name|close
-call|)
-argument_list|(
-name|mediaDevice
-argument_list|,
-name|fd
-argument_list|)
-expr_stmt|;
-goto|goto
-name|done
-goto|;
-block|}
-comment|/* We have multiple chunks, go pick them up */
+comment|/* We have one or more chunks, go pick them up */
 name|mediaExtractDistBegin
 argument_list|(
 name|me
@@ -2286,11 +2181,32 @@ name|TRUE
 expr_stmt|;
 else|else
 block|{
+if|if
+condition|(
+name|me
+index|[
+name|i
+index|]
+operator|.
+name|my_dist
+condition|)
+block|{
+name|msgConfirm
+argument_list|(
+literal|"Unable to transfer all components of the %s distribution.\nIf this is a CDROM install, it may be because export restrictions prohibit\nDES code from being shipped from the U.S.  Try to get this code from a\nlocal FTP site instead!"
+argument_list|)
+expr_stmt|;
+name|status
+operator|=
+name|TRUE
+expr_stmt|;
+block|}
+else|else
 name|status
 operator|=
 name|msgYesNo
 argument_list|(
-literal|"Unable to transfer the %s distribution from %s.\nDo you want to retry this distribution later?"
+literal|"Unable to transfer the %s distribution from %s.\nDo you want to try to retrieve it again?"
 argument_list|,
 name|me
 index|[
@@ -2303,30 +2219,6 @@ name|mediaDevice
 operator|->
 name|name
 argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|status
-operator|&&
-operator|!
-name|msgYesNo
-argument_list|(
-literal|"Would you like to clear all distributions from the parent\ngroup of %s?"
-argument_list|,
-name|path
-argument_list|)
-condition|)
-operator|*
-operator|(
-name|me
-index|[
-name|i
-index|]
-operator|.
-name|my_mask
-operator|)
-operator|=
-literal|0
 expr_stmt|;
 block|}
 block|}
@@ -2413,7 +2305,7 @@ name|Dists
 condition|)
 name|msgConfirm
 argument_list|(
-literal|"Couldn't extract all of the dists.  Residue: %0x"
+literal|"Couldn't extract all of the distributions.  This may\nbe because the specified distributions are not available from the\ninstallation media you've chosen (residue: %0x)"
 argument_list|,
 name|Dists
 argument_list|)
