f1da07a353c4ff355aba839a1c7567e9709e1c52
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-10 18:29:18 -0800

    add MaxChildren and CurChildren
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.139
    
    Former-commit-id: cf8228702794ed66b3421abc939bdfcd509e3081

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.138 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.139 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.138		%G%"
+literal|"@(#)sendmail.h	8.139		%G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -6299,6 +6299,28 @@ end_decl_stmt
 
 begin_comment
 comment|/* maximum number of jobs in one queue run */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|int
+name|MaxChildren
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* maximum number of daemonic children */
+end_comment
+
+begin_decl_stmt
+name|EXTERN
+name|int
+name|CurChildren
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* current number of daemonic children */
 end_comment
 
 begin_decl_stmt
