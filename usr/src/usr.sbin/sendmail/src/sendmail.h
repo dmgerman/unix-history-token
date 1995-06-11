81cc3cc6c16716bcb0ca587925ae9b75f550a6f8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-11 00:51:46 -0800

    add DontExpandCnames
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.140
    
    Former-commit-id: ccbb38472f262aa9fec482e20bca447a55d881f4

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.139 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)sendmail.h	8.140 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -31,7 +31,7 @@ name|char
 name|SmailSccsId
 index|[]
 init|=
-literal|"@(#)sendmail.h	8.139		%G%"
+literal|"@(#)sendmail.h	8.140		%G%"
 decl_stmt|;
 end_decl_stmt
 
