cdece951dc56d25010299bcece6d5cfef107a531
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-13 00:26:37 -0800

    keep looking for a dotted address in text_ and nis_getcanonname
    SCCS-vsn: usr.sbin/sendmail/src/map.c 8.76
    
    Former-commit-id: 2f9f94c4668a3a95f963f15c78ef28aa7d684268

--- usr/src/usr.sbin/sendmail/src/map.c
+++ usr/src/usr.sbin/sendmail/src/map.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)map.c	8.75 (Berkeley) %G%"
+literal|"@(#)map.c	8.76 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -2332,8 +2332,6 @@ name|i
 operator|=
 literal|1
 init|;
-operator|!
-name|found
 condition|;
 name|i
 operator|++
