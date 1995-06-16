3aaad66a280f6fa460a3bc849c268ae2db1b5046
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-06-16 04:47:22 -0800

    null pointer bug (from ralph)
    SCCS-vsn: sys/kern/vfs_vnops.c 8.14
    
    Former-commit-id: ade6b4cc232159ce9efa9c87e9375747c830904d

--- usr/src/sys/kern/vfs_vnops.c
+++ usr/src/sys/kern/vfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.13 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)vfs_vnops.c	8.14 (Berkeley) %G%  */
 end_comment
 
 begin_include
@@ -1354,10 +1354,16 @@ operator|(
 name|vp
 operator|->
 name|v_mount
+operator|&&
+operator|(
+name|vp
+operator|->
+name|v_mount
 operator|->
 name|mnt_flag
 operator|&
 name|MNT_SYNCHRONOUS
+operator|)
 operator|)
 condition|)
 name|ioflag
