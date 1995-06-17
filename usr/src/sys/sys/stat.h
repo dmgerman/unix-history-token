a75b937edd6e66d68f75aab35bcbeb89bff3f7a5
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-06-16 20:16:02 -0800

    make consistent with time.h
    SCCS-vsn: sys/sys/stat.h 8.12
    
    Former-commit-id: d92ef8556fd3b0f0782958818cadc176a9ca06d9

--- usr/src/sys/sys/stat.h
+++ usr/src/sys/sys/stat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)stat.h	8.11 (Berkeley) %G%  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * %sccs.include.redist.c%  *  *	@(#)stat.h	8.12 (Berkeley) %G%  */
 end_comment
 
 begin_ifndef
@@ -163,7 +163,7 @@ name|time_t
 name|st_atime
 decl_stmt|;
 comment|/* time of last access */
-name|int32_t
+name|long
 name|st_atimensec
 decl_stmt|;
 comment|/* nsec of last access */
@@ -171,7 +171,7 @@ name|time_t
 name|st_mtime
 decl_stmt|;
 comment|/* time of last data modification */
-name|int32_t
+name|long
 name|st_mtimensec
 decl_stmt|;
 comment|/* nsec of last data modification */
@@ -179,7 +179,7 @@ name|time_t
 name|st_ctime
 decl_stmt|;
 comment|/* time of last file status change */
-name|int32_t
+name|long
 name|st_ctimensec
 decl_stmt|;
 comment|/* nsec of last file status change */
