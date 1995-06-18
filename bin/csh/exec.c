743fe6e22fa1bb5a5aef3b607bd611c8da1b3e05
Andrey Chernov <ache@FreeBSD.org>
1995-06-18 14:20:16 +0000

    Fix error: which /bin/ls .//bin/ls Obtained from: NetBSD
    Former-commit-id: 9286ace5dd93ed95b71cfcff6920f65de97798ed

--- bin/csh/exec.c
+++ bin/csh/exec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1980, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: exec.c,v 1.2 1994/09/24 02:53:59 davidg Exp $  */
+comment|/*-  * Copyright (c) 1980, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: exec.c,v 1.3 1995/05/30 00:06:32 rgrimes Exp $  */
 end_comment
 
 begin_ifndef
@@ -3022,6 +3022,9 @@ name|s1
 decl_stmt|,
 modifier|*
 name|s2
+decl_stmt|,
+modifier|*
+name|cmd
 decl_stmt|;
 name|Char
 name|qc
@@ -3258,6 +3261,21 @@ comment|/* we save and then restore this */
 return|return;
 block|}
 block|}
+name|sp
+operator|->
+name|word
+operator|=
+name|cmd
+operator|=
+name|globone
+argument_list|(
+name|sp
+operator|->
+name|word
+argument_list|,
+name|G_IGNORE
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|(
@@ -3370,6 +3388,12 @@ name|STRdot
 argument_list|)
 condition|)
 block|{
+if|if
+condition|(
+operator|!
+name|slash
+condition|)
+block|{
 name|sp
 operator|->
 name|word
@@ -3400,6 +3424,15 @@ operator|->
 name|word
 argument_list|)
 expr_stmt|;
+block|}
+else|else
+name|prlex
+argument_list|(
+name|cshout
+argument_list|,
+name|lex
+argument_list|)
+expr_stmt|;
 name|sp
 operator|->
 name|word
@@ -3407,6 +3440,14 @@ operator|=
 name|s0
 expr_stmt|;
 comment|/* we save and then restore this */
+name|xfree
+argument_list|(
+operator|(
+name|ptr_t
+operator|)
+name|cmd
+argument_list|)
+expr_stmt|;
 return|return;
 block|}
 name|s1
@@ -3489,6 +3530,14 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
+name|xfree
+argument_list|(
+operator|(
+name|ptr_t
+operator|)
+name|cmd
+argument_list|)
+expr_stmt|;
 name|sp
 operator|->
 name|word
