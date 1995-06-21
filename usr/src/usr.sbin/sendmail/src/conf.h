3a17c527b8c582e2d46abc2ba32292563d539679
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-21 13:42:29 -0800

    ping-ponging ISC_UNIX changes from J.J.Bailey <jjb@jagware.bcc.com>
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.196
    
    Former-commit-id: cec2c05c7d7ec4e3184a84d8cb8b08fbde45a811

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.195 (Berkeley) %G%  */
+comment|/*  * Copyright (c) 1983, 1995 Eric P. Allman  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * %sccs.include.redist.c%  *  *	@(#)conf.h	8.196 (Berkeley) %G%  */
 end_comment
 
 begin_comment
@@ -3538,13 +3538,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_typedef
-typedef|typedef
-name|short
-name|pid_t
-typedef|;
-end_typedef
 
 begin_endif
 endif|#
