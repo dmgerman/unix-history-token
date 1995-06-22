010bd659e964f99bff7839ab5b55473767a7adc1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-21 17:54:49 -0800

    missing parameter to strtorwset
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.111
    
    Former-commit-id: 0d2a66556d9b3ceffffb421567f602cbdf8ba87e

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)readcf.c	8.110 (Berkeley) %G%"
+literal|"@(#)readcf.c	8.111 (Berkeley) %G%"
 decl_stmt|;
 end_decl_stmt
 
@@ -3185,6 +3185,8 @@ argument_list|(
 name|p
 argument_list|,
 name|NULL
+argument_list|,
+name|ST_ENTER
 argument_list|)
 expr_stmt|;
 if|if
