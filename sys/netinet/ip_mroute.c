23ee432b322515feb2f76fcc6ba979a4a4fac7df
Garrett Wollman <wollman@FreeBSD.org>
1995-06-26 16:15:49 +0000

    From Bill Fenner:
    > Also, I don't remember if I sent you this; it affects PIM assert processing.
    
    Submitted by:	Bill Fenner <fenner@parc.xerox.com>
    
    
    Former-commit-id: f8d6d82803263cc807e551eab5afe0e0df34f7ee

--- sys/netinet/ip_mroute.c
+++ sys/netinet/ip_mroute.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * IP multicast forwarding procedures  *  * Written by David Waitzman, BBN Labs, August 1988.  * Modified by Steve Deering, Stanford, February 1989.  * Modified by Mark J. Steiglitz, Stanford, May, 1991  * Modified by Van Jacobson, LBL, January 1993  * Modified by Ajit Thyagarajan, PARC, August 1993  * Modified by Bill Fenner, PARC, April 1995  *  * MROUTING Revision: 3.5  */
+comment|/*  * IP multicast forwarding procedures  *  * Written by David Waitzman, BBN Labs, August 1988.  * Modified by Steve Deering, Stanford, February 1989.  * Modified by Mark J. Steiglitz, Stanford, May, 1991  * Modified by Van Jacobson, LBL, January 1993  * Modified by Ajit Thyagarajan, PARC, August 1993  * Modified by Bill Fenner, PARC, April 1995  *  * MROUTING Revision: 3.5  * $Id$  */
 end_comment
 
 begin_include
@@ -6854,7 +6854,7 @@ name|socket_send
 argument_list|(
 name|ip_mrouter
 argument_list|,
-name|m
+name|mm
 argument_list|,
 operator|&
 name|k_igmpsrc
