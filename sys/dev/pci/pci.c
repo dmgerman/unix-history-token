b4aa161e90a0b955d947d56d5fad1c5d9aef2269
Stefan Esser <se@FreeBSD.org>
1995-06-28 15:59:04 +0000

    Failure of the consistency checks for BIOS assigned mappings of busses connected via PCI to PCI bridges is considered non fatal for now.
    Former-commit-id: 23f74b8b50751f6dbc71d6e9d8f5610ff4236699

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: pci.c,v 1.22 1995/05/04 06:57:11 davidg Exp $ ** **  General subroutines for the PCI bus. **  pci_configure () ** **  FreeBSD ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: pci.c,v 1.23 1995/05/30 08:13:09 rgrimes Exp $ ** **  General subroutines for the PCI bus. **  pci_configure () ** **  FreeBSD ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
 end_comment
 
 begin_define
@@ -2049,6 +2049,32 @@ argument_list|,
 name|subordinate
 argument_list|)
 expr_stmt|;
+name|printf
+argument_list|(
+literal|"\tmapping regs: io:%08x mem:%08x pmem:%08x"
+argument_list|,
+name|pci_conf_read
+argument_list|(
+name|tag
+argument_list|,
+name|PCI_PCI_BRIDGE_IO_REG
+argument_list|)
+argument_list|,
+name|pci_conf_read
+argument_list|(
+name|tag
+argument_list|,
+name|PCI_PCI_BRIDGE_MEM_REG
+argument_list|)
+argument_list|,
+name|pci_conf_read
+argument_list|(
+name|tag
+argument_list|,
+name|PCI_PCI_BRIDGE_PMEM_REG
+argument_list|)
+argument_list|)
+expr_stmt|;
 endif|#
 directive|endif
 comment|/* 			**	check for uninitialized bridge. 			*/
@@ -2336,17 +2362,13 @@ argument_list|)
 expr_stmt|;
 name|mask
 operator|=
-operator|(
-literal|0xFFFF0000
+literal|0xFFFFFFFF
 operator|^
 operator|(
 name|data
 operator|&
 literal|0xFFFF0000
 operator|)
-operator|)
-operator||
-literal|0xFFFF
 expr_stmt|;
 name|this
 operator|->
@@ -2421,17 +2443,13 @@ argument_list|)
 expr_stmt|;
 name|mask
 operator|=
-operator|(
-literal|0xFFFF0000
+literal|0xFFFFFFFF
 operator|^
 operator|(
 name|data
 operator|&
 literal|0xFFFF0000
 operator|)
-operator|)
-operator||
-literal|0xFFFF
 expr_stmt|;
 name|this
 operator|->
@@ -3512,11 +3530,8 @@ operator|->
 name|pcicb_memlimit
 argument_list|)
 expr_stmt|;
-return|return
-operator|(
-literal|0
-operator|)
-return|;
+comment|/*		return (0);*/
+comment|/* ACHTUNG: Ist der Code richtig, wenn eine PCI-PCI-Bridge fuer  * die PCI-Slots verwendet wird, aber die Onboard-Devices direkt   * an der CPU-PCI-Bridge haengen (Siehe Compaq Prolinea Problem) ???  */
 block|}
 name|pcibus
 operator|->
@@ -5098,6 +5113,7 @@ name|VendorTable
 index|[]
 init|=
 block|{
+comment|/*	{0x0e11, "? 0x0e11"},*/
 block|{
 literal|0x1002
 block|,
@@ -5117,17 +5133,25 @@ literal|"NCR"
 block|}
 block|,
 block|{
+literal|0x1022
+block|,
+literal|"AMD"
+block|}
+block|,
+block|{
 literal|0x102B
 block|,
 literal|"MATROX"
 block|}
 block|,
+comment|/*	{0x1039, "? 0x1039"},*/
 block|{
 literal|0x1045
 block|,
 literal|"OPTI"
 block|}
 block|,
+comment|/*	{0x1095, "? 0x1095"},*/
 block|{
 literal|0x5333
 block|,
