5666a2e3cb528258553520f97e6470bca18b8990
Andreas Schulz <ats@FreeBSD.org>
1995-07-08 22:09:11 +0000

    Correct a typo in a comment.
    Former-commit-id: c8e0478b3481b1833e716729837d1e482866724b

--- sys/i386/scsi/aic7xxx.c
+++ sys/i386/scsi/aic7xxx.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/isa/aic7770.c	27/284X and aic7770 motherboard controllers  * /pci/aic7870.c	294x and aic7870 motherboard controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.29 1995/06/11 19:31:29 rgrimes Exp $  */
+comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/isa/aic7770.c	27/284X and aic7770 motherboard controllers  * /pci/aic7870.c	294x and aic7870 motherboard controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.30 1995/07/04 21:14:44 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -810,7 +810,7 @@ comment|/* Our ID mask */
 end_comment
 
 begin_comment
-comment|/*  * SCSI Transfer Count (pp. 3-19,20)  * These registers count down the number of bytes transfered  * across the SCSI bus.  The counter is decrimented only once  * the data has been safely transfered.  SDONE in SSTAT0 is  * set when STCNT goes to 0  */
+comment|/*  * SCSI Transfer Count (pp. 3-19,20)  * These registers count down the number of bytes transfered  * across the SCSI bus.  The counter is decremented only once  * the data has been safely transfered.  SDONE in SSTAT0 is  * set when STCNT goes to 0  */
 end_comment
 
 begin_define
