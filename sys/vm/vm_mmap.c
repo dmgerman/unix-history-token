93374f8b5487de5fc337c05c774ac49cacb69d13
David Greenman <dg@FreeBSD.org>
1995-07-09 06:58:03 +0000

    Moved call to VOP_GETATTR() out of vnode_pager_alloc() and into the places that call vnode_pager_alloc() so that a failure return can be dealt with. This fixes a panic seen on NFS clients when a file being opened is deleted on the server before the open completes.
    Former-commit-id: dca6d3feb0b8e34d79b384974b539c78540f831b

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.4 (Berkeley) 1/12/94  * $Id: vm_mmap.c,v 1.23 1995/05/18 02:59:24 davidg Exp $  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.4 (Berkeley) 1/12/94  * $Id: vm_mmap.c,v 1.24 1995/05/30 08:16:09 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -2539,6 +2539,16 @@ name|rv
 init|=
 name|KERN_SUCCESS
 decl_stmt|;
+name|vm_size_t
+name|objsize
+decl_stmt|;
+name|struct
+name|proc
+modifier|*
+name|p
+init|=
+name|curproc
+decl_stmt|;
 if|if
 condition|(
 name|size
@@ -2550,6 +2560,8 @@ operator|(
 literal|0
 operator|)
 return|;
+name|objsize
+operator|=
 name|size
 operator|=
 name|round_page
@@ -2557,6 +2569,18 @@ argument_list|(
 name|size
 argument_list|)
 expr_stmt|;
+comment|/* 	 * We currently can only deal with page aligned file offsets. 	 * The check is here rather than in the syscall because the 	 * kernel calls this function internally for other mmaping 	 * operations (such as in exec) and non-aligned offsets will 	 * cause pmap inconsistencies...so we want to be sure to 	 * disallow this in all cases. 	 */
+if|if
+condition|(
+name|foff
+operator|&
+name|PAGE_MASK
+condition|)
+return|return
+operator|(
+name|EINVAL
+operator|)
+return|;
 if|if
 condition|(
 operator|(
@@ -2621,18 +2645,6 @@ name|size
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * We currently can only deal with page aligned file offsets. 	 * The check is here rather than in the syscall because the 	 * kernel calls this function internally for other mmaping 	 * operations (such as in exec) and non-aligned offsets will 	 * cause pmap inconsistencies...so we want to be sure to 	 * disallow this in all cases. 	 */
-if|if
-condition|(
-name|foff
-operator|&
-name|PAGE_MASK
-condition|)
-return|return
-operator|(
-name|EINVAL
-operator|)
-return|;
 comment|/* 	 * Lookup/allocate pager.  All except an unnamed anonymous lookup gain 	 * a reference to ensure continued existance of the object. (XXX the 	 * exception is to appease the pageout daemon) 	 */
 if|if
 condition|(
@@ -2692,10 +2704,50 @@ name|v_rdev
 expr_stmt|;
 block|}
 else|else
+block|{
+name|struct
+name|vattr
+name|vat
+decl_stmt|;
+name|int
+name|error
+decl_stmt|;
+name|error
+operator|=
+name|VOP_GETATTR
+argument_list|(
+name|vp
+argument_list|,
+operator|&
+name|vat
+argument_list|,
+name|p
+operator|->
+name|p_ucred
+argument_list|,
+name|p
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|error
+condition|)
+return|return
+operator|(
+name|error
+operator|)
+return|;
+name|objsize
+operator|=
+name|vat
+operator|.
+name|va_size
+expr_stmt|;
 name|type
 operator|=
 name|PG_VNODE
 expr_stmt|;
+block|}
 block|}
 name|pager
 operator|=
@@ -2705,7 +2757,7 @@ name|type
 argument_list|,
 name|handle
 argument_list|,
-name|size
+name|objsize
 argument_list|,
 name|prot
 argument_list|,
