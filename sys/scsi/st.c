ac65a447bdb18c652a904e9ff6f0deeddd3459da
Joerg Wunsch <joerg@FreeBSD.org>
1995-07-09 08:14:24 +0000

    PR #kern/572:
    >Synopsis:       Booting w/scsi tape in drive causes first use to fail
    
    Booting with a tape in a SCSI tape drive will cause the first
    use of the tape to fail with the following message:
    
    st0: bad request, must be between 0 and 0.
    
    Submitted by:	mpp@legarto.minn.net (Mike Pritchard)
    
    
    Former-commit-id: 9a80a1036d1a9d935a494a99f55744c593c4319b

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.35 1995/05/03 18:09:19 dufault Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.36 1995/05/30 08:13:54 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -1452,6 +1452,17 @@ end_define
 
 begin_comment
 comment|/* Device is presently mounted */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ST_SENSE_READ
+value|0x4000
+end_define
+
+begin_comment
+comment|/* mode sense read from drive */
 end_comment
 
 begin_define
@@ -3301,7 +3312,6 @@ name|flags
 operator||=
 name|SDEV_MEDIA_LOADED
 expr_stmt|;
-comment|/* move earlier? */
 return|return
 literal|0
 return|;
@@ -4358,7 +4368,7 @@ name|bp
 operator|->
 name|b_actf
 expr_stmt|;
-comment|/* 		 * if the device has been unmounted byt the user 		 * then throw away all requests until done 		 */
+comment|/* 		 * if the device has been unmounted by the user 		 * then throw away all requests until done 		 */
 if|if
 condition|(
 operator|(
@@ -6134,6 +6144,13 @@ name|sc_link
 operator|->
 name|sd
 decl_stmt|;
+name|st
+operator|->
+name|flags
+operator|&=
+operator|~
+name|ST_SENSE_READ
+expr_stmt|;
 comment|/* 	 * Check if we need to use a default page.. 	 */
 if|if
 condition|(
@@ -6404,11 +6421,11 @@ name|pagelen
 argument_list|)
 expr_stmt|;
 block|}
-name|sc_link
+name|st
 operator|->
 name|flags
 operator||=
-name|SDEV_MEDIA_LOADED
+name|ST_SENSE_READ
 expr_stmt|;
 return|return
 literal|0
@@ -8462,11 +8479,11 @@ operator|)
 operator|&&
 operator|!
 operator|(
-name|sc_link
+name|st
 operator|->
 name|flags
 operator|&
-name|SDEV_MEDIA_LOADED
+name|ST_SENSE_READ
 operator|)
 condition|)
 block|{
@@ -8702,11 +8719,11 @@ operator|)
 operator|&&
 operator|!
 operator|(
-name|sc_link
+name|st
 operator|->
 name|flags
 operator|&
-name|SDEV_MEDIA_LOADED
+name|ST_SENSE_READ
 operator|)
 condition|)
 block|{
