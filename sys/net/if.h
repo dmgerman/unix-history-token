8e9978c2f45c4b57a9bf3a825c2c9eec50351416
Joerg Wunsch <joerg@FreeBSD.org>
1995-07-09 08:12:27 +0000

    Move some struct definitions outside of struct's, so their scopes for C++ will match the scopes for C.
    Submitted by:	Warner Losh
    
    
    Former-commit-id: e7300550c9a50ae6ef486283c0901af123ecf404

--- sys/net/if.h
+++ sys/net/if.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.h	8.1 (Berkeley) 6/10/93  * $Id: if.h,v 1.17 1995/04/26 18:10:44 pst Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.h	8.1 (Berkeley) 6/10/93  * $Id: if.h,v 1.18 1995/05/30 08:07:59 rgrimes Exp $  */
 end_comment
 
 begin_ifndef
@@ -95,59 +95,7 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/*  * Structure describing information about an interface  * which may be of interest to management entities.  */
-end_comment
-
-begin_comment
-comment|/*  * Structure defining a queue for a network interface.  *  * (Would like to call this struct ``if'', but C isn't PL/1.)  */
-end_comment
-
 begin_struct
-struct|struct
-name|ifnet
-block|{
-name|char
-modifier|*
-name|if_name
-decl_stmt|;
-comment|/* name, e.g. ``en'' or ``lo'' */
-name|struct
-name|ifnet
-modifier|*
-name|if_next
-decl_stmt|;
-comment|/* all struct ifnets are chained */
-name|struct
-name|ifaddr
-modifier|*
-name|if_addrlist
-decl_stmt|;
-comment|/* linked list of addresses per if */
-name|int
-name|if_pcount
-decl_stmt|;
-comment|/* number of promiscuous listeners */
-name|caddr_t
-name|if_bpf
-decl_stmt|;
-comment|/* packet filter structure */
-name|u_short
-name|if_index
-decl_stmt|;
-comment|/* numeric abbreviation for this if  */
-name|short
-name|if_unit
-decl_stmt|;
-comment|/* sub-unit for lower level driver */
-name|short
-name|if_timer
-decl_stmt|;
-comment|/* time 'til if_watchdog called */
-name|short
-name|if_flags
-decl_stmt|;
-comment|/* up/down, broadcast, etc. */
 struct|struct
 name|if_data
 block|{
@@ -231,8 +179,93 @@ name|ifi_lastchange
 decl_stmt|;
 comment|/* last updated */
 block|}
-name|if_data
 struct|;
+end_struct
+
+begin_comment
+comment|/*  * Structure defining a queue for a network interface.  *  * (Would like to call this struct ``if'', but C isn't PL/1.)  */
+end_comment
+
+begin_struct
+struct|struct
+name|ifqueue
+block|{
+name|struct
+name|mbuf
+modifier|*
+name|ifq_head
+decl_stmt|;
+name|struct
+name|mbuf
+modifier|*
+name|ifq_tail
+decl_stmt|;
+name|int
+name|ifq_len
+decl_stmt|;
+name|int
+name|ifq_maxlen
+decl_stmt|;
+name|int
+name|ifq_drops
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * Structure describing information about an interface  * which may be of interest to management entities.  */
+end_comment
+
+begin_struct
+struct|struct
+name|ifnet
+block|{
+name|char
+modifier|*
+name|if_name
+decl_stmt|;
+comment|/* name, e.g. ``en'' or ``lo'' */
+name|struct
+name|ifnet
+modifier|*
+name|if_next
+decl_stmt|;
+comment|/* all struct ifnets are chained */
+name|struct
+name|ifaddr
+modifier|*
+name|if_addrlist
+decl_stmt|;
+comment|/* linked list of addresses per if */
+name|int
+name|if_pcount
+decl_stmt|;
+comment|/* number of promiscuous listeners */
+name|caddr_t
+name|if_bpf
+decl_stmt|;
+comment|/* packet filter structure */
+name|u_short
+name|if_index
+decl_stmt|;
+comment|/* numeric abbreviation for this if  */
+name|short
+name|if_unit
+decl_stmt|;
+comment|/* sub-unit for lower level driver */
+name|short
+name|if_timer
+decl_stmt|;
+comment|/* time 'til if_watchdog called */
+name|short
+name|if_flags
+decl_stmt|;
+comment|/* up/down, broadcast, etc. */
+name|struct
+name|if_data
+name|if_data
+decl_stmt|;
 comment|/* procedure handles */
 name|void
 argument_list|(
@@ -343,31 +376,10 @@ name|int
 operator|)
 argument_list|)
 expr_stmt|;
-struct|struct
+name|struct
 name|ifqueue
-block|{
-name|struct
-name|mbuf
-modifier|*
-name|ifq_head
-decl_stmt|;
-name|struct
-name|mbuf
-modifier|*
-name|ifq_tail
-decl_stmt|;
-name|int
-name|ifq_len
-decl_stmt|;
-name|int
-name|ifq_maxlen
-decl_stmt|;
-name|int
-name|ifq_drops
-decl_stmt|;
-block|}
 name|if_snd
-struct|;
+decl_stmt|;
 comment|/* output queue */
 block|}
 struct|;
