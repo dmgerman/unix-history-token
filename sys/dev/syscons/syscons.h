2268794392e306412bb3fe5a86f72f8dc7b503bc
Bruce Evans <bde@FreeBSD.org>
1995-07-11 18:34:30 +0000

    Fix races in scstart().  q_to_b() wasn't called at spltty(), so there were two races: - q_to_b() might unexpectedly return 0 (e.g, after a keyboard signal   flushes the output queue and isn't echoed).  ansi_put() interprets   0 bytes as 4GB... - more output (e.g. for echoes) might arrive afer q_to_b() returns 0.   Then scstart() returns presumably and the new output might not be   handled for a long time.
    Remove unused function scxint().
    
    Fix prototypes (foo() isn't a prototype).
    
    
    Former-commit-id: 7198cfa31bb02bf087c74ce270fdbea6bb836e53

--- sys/dev/syscons/syscons.h
+++ sys/dev/syscons/syscons.h
@@ -1,18 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: syscons.h,v 1.8 1995/04/23 10:15:38 bde Exp $  */
+comment|/*-  * Copyright (c) 1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: syscons.h,v 1.9 1995/05/30 08:03:15 rgrimes Exp $  */
 end_comment
 
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|SYSCONS_H
+name|_I386_ISA_SYSCONS_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|SYSCONS_H
+name|_I386_ISA_SYSCONS_H_
 end_define
 
 begin_comment
@@ -882,16 +882,6 @@ end_function_decl
 
 begin_function_decl
 name|void
-name|scxint
-parameter_list|(
-name|dev_t
-name|dev
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
 name|scstart
 parameter_list|(
 name|struct
@@ -978,7 +968,9 @@ specifier|static
 name|scr_stat
 modifier|*
 name|alloc_scp
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 function_decl|;
 end_function_decl
 
@@ -998,7 +990,9 @@ begin_function_decl
 specifier|static
 name|int
 name|get_scr_num
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 function_decl|;
 end_function_decl
 
@@ -1006,7 +1000,9 @@ begin_function_decl
 specifier|static
 name|void
 name|scrn_timer
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 function_decl|;
 end_function_decl
 
@@ -1238,7 +1234,9 @@ begin_function_decl
 specifier|static
 name|void
 name|set_font_mode
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 function_decl|;
 end_function_decl
 
@@ -1246,7 +1244,9 @@ begin_function_decl
 specifier|static
 name|void
 name|set_normal_mode
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 function_decl|;
 end_function_decl
 
@@ -1350,7 +1350,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* SYSCONS_H */
+comment|/* !_I386_ISA_SYSCONS_H_ */
 end_comment
 
 end_unit
