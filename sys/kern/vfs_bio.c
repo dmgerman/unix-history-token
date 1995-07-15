29bab985582922db6e674321ec6f727fe06d5576
David Greenman <dg@FreeBSD.org>
1995-07-15 16:01:46 +0000

    Resize buffers if they aren't the correct size. Several months ago we made a change to NFS that caused buffers at EOF to be variable size. This had the undesired side-effect of breaking delayed writes on NFS. This fixes it.
    Submitted by:	John Dyson
    
    
    Former-commit-id: ab8eef71b82f76ee6b5320329081e1bda45ef37b

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.46 1995/05/30 08:06:27 rgrimes Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.47 1995/06/28 12:00:54 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -3844,40 +3844,13 @@ operator|!=
 name|size
 condition|)
 block|{
-if|#
-directive|if
-name|defined
+name|allocbuf
 argument_list|(
-name|VFS_BIO_DEBUG
-argument_list|)
-name|printf
-argument_list|(
-literal|"getblk: invalid buffer size: %ld\n"
+name|bp
 argument_list|,
-name|bp
-operator|->
-name|b_bcount
+name|size
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
-name|bp
-operator|->
-name|b_flags
-operator||=
-name|B_NOCACHE
-expr_stmt|;
-operator|(
-name|void
-operator|)
-name|VOP_BWRITE
-argument_list|(
-name|bp
-argument_list|)
-expr_stmt|;
-goto|goto
-name|loop
-goto|;
 block|}
 name|splx
 argument_list|(
