e86ca54a9647bd3ddc89d387f4612ec35d08a53e
David Greenman <dg@FreeBSD.org>
1995-07-23 19:37:52 +0000

    Added some additional diagnostic information output when panicing in biodone().
    Former-commit-id: ad95e42564fc826bb69112e95dca30742426c0dd

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.50 1995/07/21 04:55:45 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.51 1995/07/23 18:49:48 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -5758,7 +5758,7 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|" iosize: %ld, lblkno: %ld\n"
+literal|" iosize: %ld, lblkno: %ld, flags: 0x%x, npages: %d\n"
 argument_list|,
 name|bp
 operator|->
@@ -5773,6 +5773,14 @@ argument_list|,
 name|bp
 operator|->
 name|b_lblkno
+argument_list|,
+name|bp
+operator|->
+name|b_flags
+argument_list|,
+name|bp
+operator|->
+name|b_npages
 argument_list|)
 expr_stmt|;
 name|printf
