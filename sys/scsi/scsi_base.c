9af2d03665eae96eb1247f97b1a46d38326efc46
Bruce Evans <bde@FreeBSD.org>
1995-07-25 22:09:06 +0000

    Change memcmp() to bcmp().  memcmp() isn't declared or implemented for the kernel, but gcc provides an inline version of it if the kernel is compiled with -O.
    The inline memcmp() is OK for small compares and is better than
    the dumb kernel bcmp() in all cases, but it has been hiding the
    library memcmp() which is 4 times faster for large compares.
    
    
    Former-commit-id: 7dfdd4f8b678f0274754b6e0a2a7eaf29511abf8

--- sys/scsi/scsi_base.c
+++ sys/scsi/scsi_base.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written By Julian ELischer  * Copyright julian Elischer 1993.  * Permission is granted to use or redistribute this file in any way as long  * as this notice remains. Julian Elischer does not guarantee that this file  * is totally correct for any given task and users of this file must  * accept responsibility for any damage that occurs from the application of this  * file.  *  * Written by Julian Elischer (julian@dialix.oz.au)  *      $Id: scsi_base.c,v 1.28 1995/04/23 22:07:50 gibbs Exp $  */
+comment|/*  * Written By Julian ELischer  * Copyright julian Elischer 1993.  * Permission is granted to use or redistribute this file in any way as long  * as this notice remains. Julian Elischer does not guarantee that this file  * is totally correct for any given task and users of this file must  * accept responsibility for any damage that occurs from the application of this  * file.  *  * Written by Julian Elischer (julian@dialix.oz.au)  *      $Id: scsi_base.c,v 1.29 1995/05/30 08:13:27 rgrimes Exp $  */
 end_comment
 
 begin_define
@@ -3225,7 +3225,7 @@ condition|)
 block|{
 if|if
 condition|(
-name|memcmp
+name|bcmp
 argument_list|(
 name|ext
 operator|->
