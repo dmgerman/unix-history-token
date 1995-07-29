cb55d2013f4cf3f937bd2b1a4cab6880c0cfde1a
David Greenman <dg@FreeBSD.org>
1995-07-29 18:48:44 +0000

    Add connection drop capability for persist timeouts.
    Reviewed by:	Andras Olah
    Obtained from:	4.4BSD-lite2 via W. Richard Stevens
    
    
    Former-commit-id: 5e32a780abaf5700e5a62a8e5e7e58daa79d7ab3

--- sys/netinet/tcp_timer.c
+++ sys/netinet/tcp_timer.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tcp_timer.c	8.1 (Berkeley) 6/10/93  * $Id: tcp_timer.c,v 1.6 1995/04/12 06:49:56 davidg Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tcp_timer.c	8.1 (Berkeley) 6/10/93  * $Id: tcp_timer.c,v 1.7 1995/05/30 08:09:59 rgrimes Exp $  */
 end_comment
 
 begin_ifndef
@@ -160,6 +160,22 @@ end_decl_stmt
 begin_decl_stmt
 name|int
 name|tcp_maxidle
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|tcp_maxpersistidle
+init|=
+name|TCPTV_KEEP_IDLE
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|tcp_totbackoff
+init|=
+literal|511
 decl_stmt|;
 end_decl_stmt
 
@@ -900,6 +916,51 @@ operator|.
 name|tcps_persisttimeo
 operator|++
 expr_stmt|;
+comment|/* 		 * Hack: if the peer is dead/unreachable, we do not 		 * time out if the window is closed.  After a full 		 * backoff, drop the connection if the idle time 		 * (no responses to probes) reaches the maximum 		 * backoff that we would use if retransmitting. 		 */
+if|if
+condition|(
+name|tp
+operator|->
+name|t_rxtshift
+operator|==
+name|TCP_MAXRXTSHIFT
+operator|&&
+operator|(
+name|tp
+operator|->
+name|t_idle
+operator|>=
+name|tcp_maxpersistidle
+operator|||
+name|tp
+operator|->
+name|t_idle
+operator|>=
+name|TCP_REXMTVAL
+argument_list|(
+name|tp
+argument_list|)
+operator|*
+name|tcp_totbackoff
+operator|)
+condition|)
+block|{
+name|tcpstat
+operator|.
+name|tcps_persistdrop
+operator|++
+expr_stmt|;
+name|tp
+operator|=
+name|tcp_drop
+argument_list|(
+name|tp
+argument_list|,
+name|ETIMEDOUT
+argument_list|)
+expr_stmt|;
+break|break;
+block|}
 name|tcp_setpersist
 argument_list|(
 name|tp
