8a5f9046967a65ca7bd1abc134ec1e0461356d91
David Greenman <dg@FreeBSD.org>
1995-07-30 18:22:43 +0000

    Brought in change from rev 1.5: correct typo.
    Former-commit-id: 5706efbffecdd028d6a94c6bd8df477df1afe5de

--- usr.sbin/ppp/hdlc.c
+++ usr.sbin/ppp/hdlc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	     PPP High Level Link Control (HDLC) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: hdlc.c,v 1.2 1995/02/26 12:17:30 amurai Exp $  *  *	TODO:  */
+comment|/*  *	     PPP High Level Link Control (HDLC) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: hdlc.c,v 1.3 1995/05/30 03:50:33 rgrimes Exp $  *  *	TODO:  */
 end_comment
 
 begin_include
@@ -1648,7 +1648,7 @@ name|LogPrintf
 argument_list|(
 name|LOG_PHASE
 argument_list|,
-literal|"HDLC errros -> FCS: %u ADDR: %u COMD: %u PROTO: %u\n"
+literal|"HDLC errors -> FCS: %u ADDR: %u COMD: %u PROTO: %u\n"
 argument_list|,
 name|hp
 operator|->
