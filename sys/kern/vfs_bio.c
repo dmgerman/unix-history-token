158ac8ae1c429142a2d5e8683ff18e4449a77f20
David Greenman <dg@FreeBSD.org>
1995-08-06 12:10:39 +0000

    Resize both VMIO and non-VMIO buffers if the size changes.
    Former-commit-id: 277df863f299e0753166af805ea82e0cb9b0a520

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.55 1995/07/25 05:41:57 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.56 1995/07/29 11:40:18 bde Exp $  */
 end_comment
 
 begin_comment
@@ -3837,15 +3837,6 @@ operator|!=
 name|size
 condition|)
 block|{
-if|if
-condition|(
-name|bp
-operator|->
-name|b_flags
-operator|&
-name|B_VMIO
-condition|)
-block|{
 name|allocbuf
 argument_list|(
 name|bp
@@ -3853,24 +3844,6 @@ argument_list|,
 name|size
 argument_list|)
 expr_stmt|;
-block|}
-else|else
-block|{
-name|bp
-operator|->
-name|b_flags
-operator||=
-name|B_NOCACHE
-expr_stmt|;
-name|VOP_BWRITE
-argument_list|(
-name|bp
-argument_list|)
-expr_stmt|;
-goto|goto
-name|loop
-goto|;
-block|}
 block|}
 name|splx
 argument_list|(
