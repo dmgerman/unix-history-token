8a58e3215fd102f6fa8d22debb6451decf8fed1e
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-08-23 23:03:17 +0000

    Remove hard coded assumption that SCSI busses have 7 targets. Change some leading spaces to tabs.
    This change forces the controller drivers to allocate a scsibus_data struct
    via a call to scsi_alloc_bus(), fill in the adapter_link field, and optionally
    modify any other fields of the struct.  Scsi_alloc_bus() initializes all fields
    to the default, so the changes in most drivers are very minimal.  For drivers
    that support Wide controllers, the maxtarg field will have to be updated to
    allow probing of all targets (for an example, look at the aic7xxx driver).
    
    Scsi_attachdevs() now takes a scsibus_data* as its argument instead of an
    sc_link*.  This allows us to expand the role of the scsibus_data struct for
    other bus level configuration setings (max number of transactions, current
    transaction opennings, etc for better tagged queuing support).
    
    Reviewed by: Rodney Grimes <rgrimes>, Peter Dufault <dufault>, Julian Elischer <julian>
    
    
    Former-commit-id: d6e8b403f8619c06e24c6c2973d422a0ec6689d1

--- sys/i386/scsi/aic7xxx.c
+++ sys/i386/scsi/aic7xxx.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/isa/aic7770.c	27/284X and aic7770 motherboard controllers  * /pci/aic7870.c	294x and aic7870 motherboard controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.35 1995/08/14 08:29:15 gibbs Exp $  */
+comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/isa/aic7770.c	27/284X and aic7770 motherboard controllers  * /pci/aic7870.c	294x and aic7870 motherboard controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.36 1995/08/15 08:54:21 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -3228,7 +3228,12 @@ index|[
 name|unit
 index|]
 decl_stmt|;
-comment|/*          * fill in the prototype scsi_link.          */
+name|struct
+name|scsibus_data
+modifier|*
+name|scbus
+decl_stmt|;
+comment|/* 	 * fill in the prototype scsi_link. 	 */
 name|ahc
 operator|->
 name|sc_link
@@ -3289,7 +3294,44 @@ name|fordriver
 operator|=
 literal|0
 expr_stmt|;
-comment|/*          * ask the adapter what subunits are present          */
+comment|/* 	 * Prepare the scsibus_data area for the upperlevel 	 * scsi code. 	 */
+name|scbus
+operator|=
+name|scsi_alloc_bus
+argument_list|()
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|scbus
+condition|)
+return|return
+literal|0
+return|;
+name|scbus
+operator|->
+name|adapter_link
+operator|=
+operator|&
+name|ahc
+operator|->
+name|sc_link
+expr_stmt|;
+if|if
+condition|(
+name|ahc
+operator|->
+name|type
+operator|&
+name|AHC_WIDE
+condition|)
+name|scbus
+operator|->
+name|maxtarg
+operator|=
+literal|15
+expr_stmt|;
+comment|/* 	 * ask the adapter what subunits are present 	 */
 name|printf
 argument_list|(
 literal|"ahc%d: Probing channel A\n"
@@ -3299,14 +3341,14 @@ argument_list|)
 expr_stmt|;
 name|scsi_attachdevs
 argument_list|(
-operator|&
-operator|(
-name|ahc
-operator|->
-name|sc_link
-operator|)
+name|scbus
 argument_list|)
 expr_stmt|;
+name|scbus
+operator|=
+name|NULL
+expr_stmt|;
+comment|/* Upper-level SCSI code owns this now */
 if|if
 condition|(
 name|ahc
@@ -3355,6 +3397,42 @@ operator|*
 operator|)
 name|SELBUSB
 expr_stmt|;
+name|scbus
+operator|=
+name|scsi_alloc_bus
+argument_list|()
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|scbus
+condition|)
+return|return
+literal|0
+return|;
+name|scbus
+operator|->
+name|adapter_link
+operator|=
+operator|&
+name|ahc
+operator|->
+name|sc_link_b
+expr_stmt|;
+if|if
+condition|(
+name|ahc
+operator|->
+name|type
+operator|&
+name|AHC_WIDE
+condition|)
+name|scbus
+operator|->
+name|maxtarg
+operator|=
+literal|15
+expr_stmt|;
 name|printf
 argument_list|(
 literal|"ahc%d: Probing Channel B\n"
@@ -3364,14 +3442,14 @@ argument_list|)
 expr_stmt|;
 name|scsi_attachdevs
 argument_list|(
-operator|&
-operator|(
-name|ahc
-operator|->
-name|sc_link_b
-operator|)
+name|scbus
 argument_list|)
 expr_stmt|;
+name|scbus
+operator|=
+name|NULL
+expr_stmt|;
+comment|/* Upper-level SCSI code owns this now */
 block|}
 return|return
 literal|1
