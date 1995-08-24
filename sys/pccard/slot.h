35500d06669a579c44ba8cde76954034e048a76b
Poul-Henning Kamp <phk@FreeBSD.org>
1995-08-24 08:56:20 +0000

    Andrew McRae's pcmcia/pccard code, the kernel part.
    This is still very green, but I have managed to get my modem working.
    Lots of work still to do, but now at least we can commit it. /phk
    
    Reviewed by:	phk
    Submitted by:	Andrew McRae <andrew@mega.com.au>
    
    
    Former-commit-id: 9fe1cdf47c58529a8e96998c3a239c635056b3d5

---
+++ sys/pccard/slot.h
@@ -0,0 +1,405 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  *	Slot structures for PC-CARD interface.  *	Each slot has a controller specific structure  *	attached to it. A slot number allows  *	mapping from the character device to the  *	slot structure. This is separate to the  *	controller slot number to allow multiple controllers  *	to be accessed.  *-------------------------------------------------------------------------  *  * Copyright (c) 1995 Andrew McRae.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
+end_comment
+
+begin_comment
+comment|/*  *	Controller data - Specific to each slot controller.  */
+end_comment
+
+begin_struct
+struct|struct
+name|slot_cont
+block|{
+name|int
+function_decl|(
+modifier|*
+name|mapmem
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* Map memory */
+name|int
+function_decl|(
+modifier|*
+name|mapio
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* Map io */
+name|void
+function_decl|(
+modifier|*
+name|reset
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* init */
+name|void
+function_decl|(
+modifier|*
+name|disable
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* Disable slot */
+name|int
+function_decl|(
+modifier|*
+name|power
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* Set power values */
+name|int
+function_decl|(
+modifier|*
+name|ioctl
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* ioctl to lower level */
+name|void
+function_decl|(
+modifier|*
+name|mapirq
+function_decl|)
+parameter_list|()
+function_decl|;
+comment|/* Map interrupt number */
+name|int
+name|extra
+decl_stmt|;
+comment|/* Controller specific size */
+name|int
+name|maxmem
+decl_stmt|;
+comment|/* Number of allowed memory windows */
+name|int
+name|maxio
+decl_stmt|;
+comment|/* Number of allowed I/O windows */
+name|int
+name|irqs
+decl_stmt|;
+comment|/* IRQ's that are allowed */
+name|char
+modifier|*
+name|name
+decl_stmt|;
+comment|/* controller name */
+comment|/*  *	The rest is maintained by the mainline PC-CARD code.  */
+name|struct
+name|slot_cont
+modifier|*
+name|next
+decl_stmt|;
+comment|/* Allows linked list of controllers */
+name|int
+name|slots
+decl_stmt|;
+comment|/* Slots available */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  *	Driver structure - each driver registers itself  *	with the mainline PC-CARD code. These drivers are  *	then available for linking to the devices.  */
+end_comment
+
+begin_struct_decl
+struct_decl|struct
+name|pccard_dev
+struct_decl|;
+end_struct_decl
+
+begin_struct
+struct|struct
+name|pccard_drv
+block|{
+name|char
+modifier|*
+name|name
+decl_stmt|;
+comment|/* Driver name */
+name|int
+function_decl|(
+modifier|*
+name|handler
+function_decl|)
+parameter_list|(
+name|struct
+name|pccard_dev
+modifier|*
+parameter_list|)
+function_decl|;
+comment|/* Interrupt handler */
+name|void
+function_decl|(
+modifier|*
+name|unload
+function_decl|)
+parameter_list|(
+name|struct
+name|pccard_dev
+modifier|*
+parameter_list|)
+function_decl|;
+comment|/* Disable driver */
+name|void
+function_decl|(
+modifier|*
+name|suspend
+function_decl|)
+parameter_list|(
+name|struct
+name|pccard_dev
+modifier|*
+parameter_list|)
+function_decl|;
+comment|/* Suspend driver */
+name|int
+function_decl|(
+modifier|*
+name|init
+function_decl|)
+parameter_list|(
+name|struct
+name|pccard_dev
+modifier|*
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+comment|/* init device */
+name|int
+name|attr
+decl_stmt|;
+comment|/* driver attributes */
+name|unsigned
+name|int
+modifier|*
+name|imask
+decl_stmt|;
+comment|/* Interrupt mask ptr */
+name|struct
+name|pccard_drv
+modifier|*
+name|next
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  *	Device structure for cards. Each card may have one  *	or more drivers attached to it; each driver is assumed  *	to require at most one interrupt handler, one I/O block  *	and one memory block. This structure is used to link the different  *	devices together.  */
+end_comment
+
+begin_struct
+struct|struct
+name|pccard_dev
+block|{
+name|struct
+name|pccard_dev
+modifier|*
+name|next
+decl_stmt|;
+comment|/* List of drivers */
+name|struct
+name|isa_device
+name|isahd
+decl_stmt|;
+comment|/* Device details */
+name|struct
+name|pccard_drv
+modifier|*
+name|drv
+decl_stmt|;
+name|void
+modifier|*
+name|arg
+decl_stmt|;
+comment|/* Device argument */
+name|struct
+name|slot
+modifier|*
+name|sp
+decl_stmt|;
+comment|/* Back pointer to slot */
+name|int
+name|running
+decl_stmt|;
+comment|/* Current state of driver */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  *	Per-slot structure.  */
+end_comment
+
+begin_struct
+struct|struct
+name|slot
+block|{
+name|struct
+name|slot
+modifier|*
+name|next
+decl_stmt|;
+comment|/* Master list */
+name|int
+name|slot
+decl_stmt|;
+comment|/* Slot number */
+name|int
+name|flags
+decl_stmt|;
+comment|/* Slot flags (see below) */
+name|int
+name|rwmem
+decl_stmt|;
+comment|/* Read/write flags */
+name|int
+name|ex_sel
+decl_stmt|;
+comment|/* PID for select */
+name|int
+name|irq
+decl_stmt|;
+comment|/* IRQ allocated (0 = none) */
+name|int
+name|irqref
+decl_stmt|;
+comment|/* Reference count of driver IRQs */
+name|struct
+name|pccard_dev
+modifier|*
+name|devices
+decl_stmt|;
+comment|/* List of drivers attached */
+comment|/*  *	flags.  */
+name|unsigned
+name|int
+name|insert_timeout
+range|:
+literal|1
+decl_stmt|;
+comment|/* Insert timeout active */
+name|enum
+name|cardstate
+name|state
+decl_stmt|,
+name|laststate
+decl_stmt|;
+comment|/* Current/last card states */
+name|struct
+name|selinfo
+name|selp
+decl_stmt|;
+comment|/* Info for select */
+name|struct
+name|mem_desc
+name|mem
+index|[
+name|NUM_MEM_WINDOWS
+index|]
+decl_stmt|;
+comment|/* Memory windows */
+name|struct
+name|io_desc
+name|io
+index|[
+name|NUM_IO_WINDOWS
+index|]
+decl_stmt|;
+comment|/* I/O windows */
+name|struct
+name|power
+name|pwr
+decl_stmt|;
+comment|/* Power values */
+name|struct
+name|slot_cont
+modifier|*
+name|cinfo
+decl_stmt|;
+comment|/* Per-controller data */
+name|void
+modifier|*
+name|cdata
+decl_stmt|;
+comment|/* Controller specific data */
+block|}
+struct|;
+end_struct
+
+begin_enum
+enum|enum
+name|card_event
+block|{
+name|card_removed
+block|,
+name|card_inserted
+block|}
+enum|;
+end_enum
+
+begin_function_decl
+name|struct
+name|slot
+modifier|*
+name|pccard_alloc_slot
+parameter_list|(
+name|struct
+name|slot_cont
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|pccard_event
+parameter_list|(
+name|struct
+name|slot
+modifier|*
+parameter_list|,
+name|enum
+name|card_event
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|pccard_remove_controller
+parameter_list|(
+name|struct
+name|slot_cont
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
+name|pccard_alloc_intr
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|pccard_add_driver
+parameter_list|(
+name|struct
+name|pccard_drv
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+end_unit
+
