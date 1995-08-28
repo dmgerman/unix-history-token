e4d57ebdb6b71a432d91b2ed209207b872ae3311
David Greenman <dg@FreeBSD.org>
1995-08-28 11:21:23 +0000

    Brought in changes from rev 1.2: don't use tzfile.h
    Former-commit-id: 0c4d0ceddf40654ef5bb5fc13c14617efcade615

--- usr.bin/script/script.c
+++ usr.bin/script/script.c
@@ -134,12 +134,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<tzfile.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<unistd.h>
 end_include
 
@@ -773,7 +767,7 @@ name|it_interval
 operator|.
 name|tv_sec
 operator|=
-name|SECSPERMIN
+literal|60
 operator|/
 literal|2
 expr_stmt|;
