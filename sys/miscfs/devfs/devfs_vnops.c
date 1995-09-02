16e7171e1552e664d95b8c16c15eddd6c023f59a
Julian Elischer <julian@FreeBSD.org>
1995-09-02 07:09:01 +0000

    Submitted by:	Julian changes to make it compile again
    Former-commit-id: 185f68e3b9260255d8cc3f2f4f4022c19a47a668

--- sys/miscfs/devfs/devfs_vnops.c
+++ sys/miscfs/devfs/devfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  Written by Julian Elischer (julian@DIALix.oz.au)  *  *	$Header: /home/ncvs/src/sys/miscfs/devfs/devfs_vnops.c,v 1.5 1995/05/30 08:06:53 rgrimes Exp $  *  * symlinks can wait 'til later.  */
+comment|/*  *  Written by Julian Elischer (julian@DIALix.oz.au)  *  *	$Header: /home/ncvs/src/sys/miscfs/devfs/devfs_vnops.c,v 1.6 1995/08/01 18:50:51 davidg Exp $  *  * symlinks can wait 'til later.  */
 end_comment
 
 begin_include
@@ -77,12 +77,14 @@ directive|include
 file|<sys/vnode.h>
 end_include
 
-begin_comment
-comment|/*#include<miscfs/specfs/specdev.h>*/
-end_comment
+begin_include
+include|#
+directive|include
+file|<miscfs/specfs/specdev.h>
+end_include
 
 begin_comment
-comment|/* plimit structure in the proc struct */
+comment|/* definitions of spec functions we use */
 end_comment
 
 begin_include
@@ -114,10 +116,6 @@ include|#
 directive|include
 file|"devfsdefs.h"
 end_include
-
-begin_comment
-comment|/*extern struct timeval time,boottime;*/
-end_comment
 
 begin_comment
 comment|/*  * Insert description here  */
@@ -1784,7 +1782,7 @@ name|file_node
 operator|->
 name|ctime
 operator|.
-name|tv_sec
+name|ts_sec
 condition|)
 block|{
 name|vap
@@ -1818,7 +1816,7 @@ name|file_node
 operator|->
 name|mtime
 operator|.
-name|tv_sec
+name|ts_sec
 condition|)
 block|{
 name|vap
@@ -1852,7 +1850,7 @@ name|file_node
 operator|->
 name|atime
 operator|.
-name|tv_sec
+name|ts_sec
 condition|)
 block|{
 name|vap
@@ -4555,16 +4553,6 @@ end_comment
 
 begin_comment
 comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)spec_vnops.c	8.6 (Berkeley) 4/9/94  * spec_vnops.c,v 1.9 1994/11/14 13:22:52 bde Exp  */
-end_comment
-
-begin_include
-include|#
-directive|include
-file|"../specfs/specdev.h"
-end_include
-
-begin_comment
-comment|/* for all the definitions and prototypes */
 end_comment
 
 begin_function_decl
