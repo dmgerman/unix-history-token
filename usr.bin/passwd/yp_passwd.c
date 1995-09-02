4382063f5a726558ec1b542bacd597da07a5339f
Bill Paul <wpaul@FreeBSD.org>
1995-09-02 04:02:28 +0000

    Bug fix: use the use_yp() function in the chpass(1) code to determine correctly whether a user is local or NIS (or both, or neither). If you have a user that exists locally but not in NIS, passwd(1) could get confused and try to submit the password change to NIS. (Fortunately, yppasswdd is smart enough to spot the error and reject the change.)
    Bug reported by: Charles Owens <owensc@enc.edu>
    
    
    Former-commit-id: 619cc7513fe666b2790c361ad3c9603d744b7875

--- usr.bin/passwd/yp_passwd.c
+++ usr.bin/passwd/yp_passwd.c
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992/3 Theo de Raadt<deraadt@fsa.ca>  * Copyright (c) 1994 Olaf Kirch<okir@monad.swb.de>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1992/3 Theo de Raadt<deraadt@fsa.ca>  * Copyright (c) 1994 Olaf Kirch<okir@monad.swb.de>  * Copyright (c) 1995 Bill Paul<wpaul@ctr.columbia.edu>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|YP
+end_ifdef
 
 begin_include
 include|#
@@ -85,6 +91,12 @@ begin_include
 include|#
 directive|include
 file|<rpcsvc/yppasswd.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<pw_yp.h>
 end_include
 
 begin_decl_stmt
@@ -442,6 +454,17 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+comment|/* Use the correct password */
+name|pw
+operator|=
+operator|(
+expr|struct
+name|passwd
+operator|*
+operator|)
+operator|&
+name|yp_password
+expr_stmt|;
 comment|/* Initialize password information */
 name|yppasswd
 operator|.
@@ -773,6 +796,15 @@ argument_list|)
 expr_stmt|;
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* YP */
+end_comment
 
 end_unit
 
