002859707e73bf46b025d972a2378b4a730dc18d
David Greenman <dg@FreeBSD.org>
1995-09-08 03:19:47 +0000

    1) Really print 'real' memory - use Maxmem, not physmem. 2) Output K bytes instead of pages as this means something to more people. 3) Moved printf of avail memory to after vm_bounce_init() call so that    bounce buffers are included in the figure. 4) Killed initcpu(); it's an unused vestige from the VAX.
    Former-commit-id: 378ef08d22c985cc16791beef5a4837c456ebb79

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.138 1995/09/03 05:43:02 julian Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.139 1995/09/06 16:13:31 wpaul Exp $  */
 end_comment
 
 begin_include
@@ -466,16 +466,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-specifier|static
-name|void
-name|initcpu
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_decl_stmt
 name|char
 name|machine
@@ -909,14 +899,19 @@ argument_list|()
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"real memory  = %d (%d pages)\n"
+literal|"real memory  = %d (%dK bytes)\n"
 argument_list|,
 name|ptoa
 argument_list|(
-name|physmem
+name|Maxmem
 argument_list|)
 argument_list|,
-name|physmem
+name|ptoa
+argument_list|(
+name|Maxmem
+argument_list|)
+operator|/
+literal|1024
 argument_list|)
 expr_stmt|;
 comment|/* 	 * Display any holes after the first chunk of extended memory. 	 */
@@ -1667,22 +1662,6 @@ condition|)
 name|userconfig
 argument_list|()
 expr_stmt|;
-name|printf
-argument_list|(
-literal|"avail memory = %d (%d pages)\n"
-argument_list|,
-name|ptoa
-argument_list|(
-name|cnt
-operator|.
-name|v_free_count
-argument_list|)
-argument_list|,
-name|cnt
-operator|.
-name|v_free_count
-argument_list|)
-expr_stmt|;
 ifdef|#
 directive|ifdef
 name|BOUNCE_BUFFERS
@@ -1692,9 +1671,26 @@ argument_list|()
 expr_stmt|;
 endif|#
 directive|endif
-comment|/* 	 * Set up CPU-specific registers, cache, etc. 	 */
-name|initcpu
-argument_list|()
+name|printf
+argument_list|(
+literal|"avail memory = %d (%dK bytes)\n"
+argument_list|,
+name|ptoa
+argument_list|(
+name|cnt
+operator|.
+name|v_free_count
+argument_list|)
+argument_list|,
+name|ptoa
+argument_list|(
+name|cnt
+operator|.
+name|v_free_count
+argument_list|)
+operator|/
+literal|1024
+argument_list|)
 expr_stmt|;
 comment|/* 	 * Set up buffers, so they can be used to read disk labels. 	 */
 name|bufinit
@@ -4250,14 +4246,6 @@ expr_stmt|;
 break|break;
 block|}
 block|}
-end_function
-
-begin_function
-specifier|static
-name|void
-name|initcpu
-parameter_list|()
-block|{ }
 end_function
 
 begin_comment
