2ecc137709a6ccb68d225293259e15b3f9bc4767
Julian Elischer <julian@FreeBSD.org>
1995-09-08 04:46:14 +0000

    added the dev_link external registration
    used to produce a second link to an already exixting device
    e.g.
    dev_add /disks/sd0
    dev_link /scsi/bus0/targ0/lun0 to /disks/sd0
    (not real syntax)
    
    
    Former-commit-id: c5340305ee496288fb694f9775ea10669686938a

--- sys/sys/devfsext.h
+++ sys/sys/devfsext.h
@@ -1,4 +1,29 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* usual BSD style copyright here */
+end_comment
+
+begin_comment
+comment|/* Written by Julian Elischer (julian@dialix.oz.au)*/
+end_comment
+
+begin_comment
+comment|/*  * $Id:$  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_SYS_DEVFSECT_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_SYS_DEVFSECT_H_
+value|1
+end_define
+
 begin_function_decl
 name|void
 modifier|*
@@ -34,6 +59,30 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+modifier|*
+name|dev_link
+parameter_list|(
+name|char
+modifier|*
+name|path
+parameter_list|,
+name|char
+modifier|*
+name|name
+parameter_list|,
+name|void
+modifier|*
+name|original
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_comment
+comment|/* the result of a previous dev_link 					or dev_add operation */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -54,6 +103,15 @@ directive|define
 name|DV_DEV
 value|2
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/*_SYS_DEVFSECT_H_*/
+end_comment
 
 end_unit
 
