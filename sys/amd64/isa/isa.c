5abdfc79b6c6bb7e4f5b196df2971f55f6c40e5d
Bruce Evans <bde@FreeBSD.org>
1995-09-10 21:36:12 +0000

    Make pcvt and syscons live in the same kernel.  If both are enabled, then the first one in the config has priority.  They can be switched using userconfig().
    i386/i386/conf.c:
    Initialize the shared syscons/pcvt cdevsw entry to `nx'.
    
    Add cdevsw registration functions.
    
    Use devsw functions of the correct type if they exist.
    
    i386/i386/cons.c:
    Add renamed syscons entry points to constab.
    
    i386/i386/cons.h:
    Declare the renamed syscons entry points.
    
    i386/i386/machdep.c:
    Repeat console initialization after userconfig() in case the current
    console has become wrong.  This depends on cn functions not wiring down
    anything important.
    
    sys/conf.h:
    Declare new functions.
    
    i386/isa/isa.[ch]:
    Add a function to decide which display driver has priority.  Should be
    done better.
    
    i386/isa/syscons.c:
    Rename pccn* -> sccn*.
    
    Initialize CRTC start address in case the previous driver has moved it.
    
    i386/isa/syscons.c, i386/isa/pcvt/*
    Initialize the bogusly shared variable Crtat dynamically in case the
    stored value was changed by the previous driver.
    
    Initialize cdevsw table from a template.
    
    Don't grab the console if another display driver has priority.
    
    i386/isa/syscons.h, i386/isa/pcvt/pcvt_hdr.h:
    Don't externally declare now-static cdevsw functions.
    
    i386/isa/pcvt/pcvt_hdr.h:
    Set the sensitive hardware flag so that pcvt doesn't always have lower
    priority than syscons.  This also fixes the "stupid" detection of the
    display after filling the display with text.
    
    i386/isa/pcvt/pcvt_out.c:
    Don't be confused the off-screen cursor offset 0xffff set by syscons.
    
    kern/subr_xxx.c:
    Add enough nxio/nodev/null devsw functions of the correct type for syscons
    and pcvt.
    
    
    Former-commit-id: 8b80af897e25cdbbd010cfa6a483bfe5bb458d78

--- sys/amd64/isa/isa.c
+++ sys/amd64/isa/isa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: isa.c,v 1.49 1995/05/13 00:09:38 jkh Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: isa.c,v 1.50 1995/05/30 08:02:35 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -4288,6 +4288,56 @@ argument_list|,
 name|d
 argument_list|)
 expr_stmt|;
+block|}
+end_function
+
+begin_comment
+comment|/*  * Find the highest priority enabled display device.  Since we can't  * distinguish display devices from ttys, depend on display devices  * being before serial ttys in the table.  */
+end_comment
+
+begin_function
+name|struct
+name|isa_device
+modifier|*
+name|find_display
+parameter_list|()
+block|{
+name|struct
+name|isa_device
+modifier|*
+name|dvp
+decl_stmt|;
+for|for
+control|(
+name|dvp
+operator|=
+name|isa_devtab_tty
+init|;
+name|dvp
+operator|->
+name|id_driver
+operator|!=
+name|NULL
+condition|;
+name|dvp
+operator|++
+control|)
+if|if
+condition|(
+name|dvp
+operator|->
+name|id_enabled
+condition|)
+return|return
+operator|(
+name|dvp
+operator|)
+return|;
+return|return
+operator|(
+name|NULL
+operator|)
+return|;
 block|}
 end_function
 
