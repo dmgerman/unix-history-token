cca073b30ff03a1f50d5af0e564155722d4544eb
Bruce Evans <bde@FreeBSD.org>
1995-09-10 12:22:59 +0000

    Call output process in slstart() whether or not there is any output.  As in pppstart(), the output process may be overloaded to handle hardware flow control and hardware output completions.
    Don't wait for output to drain in slclose().  Discard output immediately.
    New output is not prevented while processes are waiting for output to
    drain (this is a bug), so the wait was sometimes forever.  Infinite
    waits are also possible when CCTS_OFLOW is enabled and CTS is down.
    Infinite waits were also caused by the above bug in slstart().
    
    Start changing new init stuff yet again: rename unused arg `dummy'.
    
    Reviewed by:	davidg
    
    
    Former-commit-id: f0c115f7a30d72a99567d7f6291c2baa8eef23de

--- sys/net/if_sl.c
+++ sys/net/if_sl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1989, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_sl.c	8.6 (Berkeley) 2/1/94  * $Id: if_sl.c,v 1.29 1995/08/30 00:33:20 bde Exp $  */
+comment|/*  * Copyright (c) 1987, 1989, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_sl.c	8.6 (Berkeley) 2/1/94  * $Id: if_sl.c,v 1.30 1995/09/09 18:10:24 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -523,11 +523,11 @@ specifier|static
 name|void
 name|slattach
 parameter_list|(
-name|udata
+name|dummy
 parameter_list|)
 name|void
 modifier|*
-name|udata
+name|dummy
 decl_stmt|;
 block|{
 specifier|register
@@ -1069,13 +1069,6 @@ decl_stmt|;
 name|int
 name|s
 decl_stmt|;
-if|if
-condition|(
-name|ttywflush
-argument_list|(
-name|tp
-argument_list|)
-condition|)
 name|ttyflush
 argument_list|(
 name|tp
@@ -1918,7 +1911,17 @@ init|;
 condition|;
 control|)
 block|{
-comment|/* 		 * If there is more in the output queue, just send it now. 		 * We are being called in lieu of ttstart and must do what 		 * it would. 		 */
+comment|/* 		 * Call output process whether or not there is more in the 		 * output queue.  We are being called in lieu of ttstart 		 * and must do what it would. 		 */
+call|(
+modifier|*
+name|tp
+operator|->
+name|t_oproc
+call|)
+argument_list|(
+name|tp
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|tp
@@ -1942,16 +1945,6 @@ name|sc_flags
 operator|&=
 operator|~
 name|SC_OUTWAIT
-expr_stmt|;
-call|(
-modifier|*
-name|tp
-operator|->
-name|t_oproc
-call|)
-argument_list|(
-name|tp
-argument_list|)
 expr_stmt|;
 if|if
 condition|(
