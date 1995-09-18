7873e2a15783fdd612ead18dfa227313923b6374
Garrett Wollman <wollman@FreeBSD.org>
1995-09-18 15:51:40 +0000

    Initial back-end support for IP MTU discovery, gated on MTUDISC.  The support for TCP has yet to be written.
    Former-commit-id: 15ba9ac3d9c2c06402e04baee0b02096b176c2da

--- sys/netinet/tcp_timewait.c
+++ sys/netinet/tcp_timewait.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tcp_subr.c	8.1 (Berkeley) 6/10/93  * $Id: tcp_subr.c,v 1.12 1995/06/19 16:45:33 wollman Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tcp_subr.c	8.1 (Berkeley) 6/10/93  * $Id: tcp_subr.c,v 1.13 1995/06/29 18:11:23 wollman Exp $  */
 end_comment
 
 begin_include
@@ -2011,6 +2011,38 @@ expr_stmt|;
 block|}
 end_function
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|MTUDISC
+end_ifdef
+
+begin_decl_stmt
+specifier|static
+name|void
+name|tcp_mtudisc
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|inpcb
+operator|*
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* MTUDISC */
+end_comment
+
 begin_function
 name|void
 name|tcp_ctlinput
@@ -2069,6 +2101,23 @@ name|notify
 operator|=
 name|tcp_quench
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|MTUDISC
+elseif|else
+if|if
+condition|(
+name|cmd
+operator|==
+name|PRC_MSGSIZE
+condition|)
+name|notify
+operator|=
+name|tcp_mtudisc
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* MTUDISC */
 elseif|else
 if|if
 condition|(
@@ -2214,6 +2263,63 @@ name|t_maxseg
 expr_stmt|;
 block|}
 end_function
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|MTUDISC
+end_ifdef
+
+begin_comment
+comment|/*  * When `need fragmentation' ICMP is received, update our idea of the MSS  * based on the new value in the route.  Also nudge TCP to send something,  * since we know the packet we just sent was dropped.  */
+end_comment
+
+begin_function
+specifier|static
+name|void
+name|tcp_mtudisc
+parameter_list|(
+name|inp
+parameter_list|,
+name|errno
+parameter_list|)
+name|struct
+name|inpcb
+modifier|*
+name|inp
+decl_stmt|;
+name|int
+name|errno
+decl_stmt|;
+block|{
+name|struct
+name|tcpcb
+modifier|*
+name|tp
+init|=
+name|intotcpcb
+argument_list|(
+name|inp
+argument_list|)
+decl_stmt|;
+if|#
+directive|if
+literal|0
+block|if (tp) 		;
+comment|/* XXX implement */
+endif|#
+directive|endif
+block|}
+end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* MTUDISC */
+end_comment
 
 begin_comment
 comment|/*  * Look-up the routing entry to the peer of this inpcb.  If no route  * is found and it cannot be allocated the return NULL.  This routine  * is called by TCP routines that access the rmx structure and by tcp_mss  * to get the interface MTU.  */
