e7b2211ed16666ab45bf9de94c78b0bbe4ed218e
Bruce Evans <bde@FreeBSD.org>
1995-09-19 13:31:04 +0000

    Generate prototypes for syscall-implementing functions.  Put them in <sys/sysproto.h> and use them (so far only) in kern/init_sysent.c.
    Don't put $Id in generated files.
    
    kern/syscalls.master:
    I had to add some new fields to describe some non-orthogonal names.
    E.g., the args struct for the syscall-implementing function foo()
    is usually named `foo_args', but for getpid() it is named `args'.
    
    sys/sysent.h:
    sy_call_t is still incomplete to hide a couple of warnings.
    
    
    Former-commit-id: dd3d12dc6d37ade5f987ca204f318bb3da87cafa

--- sys/sys/sysent.h
+++ sys/sys/sysent.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sysent.h,v 1.3 1995/02/14 19:23:20 sos Exp $  */
+comment|/*-  * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sysent.h,v 1.4 1995/03/16 18:16:29 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -16,6 +16,16 @@ name|_SYS_SYSENT_H_
 value|1
 end_define
 
+begin_typedef
+typedef|typedef
+name|int
+name|sy_call_t
+name|__P
+typedef|((
+comment|/* struct proc *, bogusly_various *, int * */
+typedef|));
+end_typedef
+
 begin_struct
 struct|struct
 name|sysent
@@ -25,17 +35,20 @@ name|int
 name|sy_narg
 decl_stmt|;
 comment|/* number of arguments */
-name|int
-function_decl|(
+name|sy_call_t
 modifier|*
 name|sy_call
-function_decl|)
-parameter_list|()
-function_decl|;
+decl_stmt|;
 comment|/* implementing function */
 block|}
 struct|;
 end_struct
+
+begin_struct_decl
+struct_decl|struct
+name|image_params
+struct_decl|;
+end_struct_decl
 
 begin_struct
 struct|struct
@@ -75,12 +88,22 @@ name|sv_errtbl
 decl_stmt|;
 comment|/* errno translation table */
 name|int
-function_decl|(
-modifier|*
-name|sv_fixup
-function_decl|)
-parameter_list|()
-function_decl|;
+argument_list|(
+argument|*sv_fixup
+argument_list|)
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|*
+operator|*
+operator|,
+expr|struct
+name|image_params
+operator|*
+operator|)
+argument_list|)
+expr_stmt|;
 comment|/* stack fixup function */
 block|}
 struct|;
