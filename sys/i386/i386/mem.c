bf82b79c5eeda796956eb6b40a1db42927ea2371
David Greenman <dg@FreeBSD.org>
1995-09-20 13:04:07 +0000

    Brought in changes from revs 1.13-1.14: fix "CMAP busy" panic.
    Former-commit-id: cfab8fe3ecf45248e6449ffd22a97dfd58b4793b

--- sys/i386/i386/mem.c
+++ sys/i386/i386/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.9 1994/08/06 10:25:34 davidg Exp $  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.9.8.1 1995/09/14 07:09:00 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -513,12 +513,64 @@ comment|/* minor device 1 is kernel memory */
 case|case
 literal|1
 case|:
+block|{
+name|vm_offset_t
+name|addr
+decl_stmt|,
+name|eaddr
+decl_stmt|;
 name|c
 operator|=
 name|iov
 operator|->
 name|iov_len
 expr_stmt|;
+comment|/* 			 * Make sure that all of the pages are currently resident so 			 * that we don't create any zero-fill pages. 			 */
+name|addr
+operator|=
+name|trunc_page
+argument_list|(
+name|uio
+operator|->
+name|uio_offset
+argument_list|)
+expr_stmt|;
+name|eaddr
+operator|=
+name|round_page
+argument_list|(
+name|uio
+operator|->
+name|uio_offset
+operator|+
+name|c
+argument_list|)
+expr_stmt|;
+for|for
+control|(
+init|;
+name|addr
+operator|<
+name|eaddr
+condition|;
+name|addr
+operator|+=
+name|PAGE_SIZE
+control|)
+if|if
+condition|(
+name|pmap_extract
+argument_list|(
+name|kernel_pmap
+argument_list|,
+name|addr
+argument_list|)
+operator|==
+literal|0
+condition|)
+return|return
+name|EFAULT
+return|;
 if|if
 condition|(
 operator|!
@@ -575,6 +627,7 @@ name|uio
 argument_list|)
 expr_stmt|;
 continue|continue;
+block|}
 comment|/* minor device 2 is EOF/RATHOLE */
 case|case
 literal|2
