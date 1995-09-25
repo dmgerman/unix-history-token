fef61c7f27a08e4b81ce85959c2fc96ae19aa5a7
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-09-25 00:52:15 +0000

    Great big batch-o-fixes.
    jkh:
    	Change the fixit floppy paradigm somewhat.  Rather than having
    	a fixit floppy be a stand-alone boot floppy with emasculated
    	command set due to space crunch, make boot floppy a filesystem
    	of nice utilities and *no* kernel.  The idea is that you boot
    	the standard boot floppy and choose the "Fixit" item off the top
    	menu.  It then asks for and mounts the fixit floppy, adds the mount
    	point to $PATH and then forks a shell with the inherited PATH.  You
    	can use the shell to traipse around the filesystem, tweaking things
    	as desired.
    
    	Mutate the useless shell option in first menu to far more useful fixit.
    
    	Fix the bogus `ifconfig sl0' case once and for all with a special case
    	hack for cuaa0.
    
    gpalmer:
    	Fix benign-yet-bogus incorrect `password' to anon ftp sites.
    
    joerg:
    	Allow cpio to guess at input format again.
    Submitted by:	jkh, gpalmer, joerg
    
    Former-commit-id: b2b14a983e7f4cec7a56069bd084f3afdd3f54e1

--- release/sysinstall/network.c
+++ release/sysinstall/network.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated to essentially a complete rewrite.  *  * $Id: network.c,v 1.7.2.2 1995/07/21 10:57:33 rgrimes Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated to essentially a complete rewrite.  *  * $Id: network.c,v 1.7.2.3 1995/09/18 17:00:24 peter Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Jordan Hubbard  *	for the FreeBSD Project.  * 4. The name of Jordan Hubbard or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_comment
@@ -78,6 +78,12 @@ modifier|*
 name|cp
 decl_stmt|,
 name|ifconfig
+index|[
+literal|64
+index|]
+decl_stmt|;
+name|char
+name|ifname
 index|[
 literal|64
 index|]
@@ -240,7 +246,24 @@ name|FALSE
 return|;
 block|}
 block|}
+name|strcpy
+argument_list|(
+name|ifname
+argument_list|,
+literal|"sl0"
+argument_list|)
+expr_stmt|;
 block|}
+else|else
+name|strcpy
+argument_list|(
+name|ifname
+argument_list|,
+name|dev
+operator|->
+name|name
+argument_list|)
+expr_stmt|;
 name|snprintf
 argument_list|(
 name|ifconfig
@@ -251,9 +274,7 @@ literal|"%s%s"
 argument_list|,
 name|VAR_IFCONFIG
 argument_list|,
-name|dev
-operator|->
-name|name
+name|ifname
 argument_list|)
 expr_stmt|;
 name|cp
@@ -272,6 +293,8 @@ block|{
 name|msgConfirm
 argument_list|(
 literal|"The %s device is not configured.  You will need to do so\nin the Networking configuration menu before proceeding."
+argument_list|,
+name|ifname
 argument_list|)
 expr_stmt|;
 return|return
@@ -284,7 +307,7 @@ name|vsystem
 argument_list|(
 literal|"ifconfig %s %s"
 argument_list|,
-literal|"sl0"
+name|ifname
 argument_list|,
 name|cp
 argument_list|)
@@ -298,9 +321,7 @@ name|msgConfirm
 argument_list|(
 literal|"Unable to configure the %s interface!\nThis installation method cannot be used."
 argument_list|,
-name|dev
-operator|->
-name|name
+name|ifname
 argument_list|)
 expr_stmt|;
 return|return
