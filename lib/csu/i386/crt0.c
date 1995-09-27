a6de9c9de07077d82c61644a9d036825a5788799
Nate Williams <nate@FreeBSD.org>
1995-09-27 23:13:33 +0000

    Fixup the "ld.so failed" message for the case when ld.so finds undefined symbols.
    An easy example to see this is to develop an X program which links
    against Xt, but doesn't add -lX11 to the link line.  It will link fine,
    but cause run-time errors by ld.so because of missing symbols used by Xt
    defined in X11.  This patch makes the errors more readable.
    
    Submitted by:	jdp@polstra.com (John Polstra)
    
    
    Former-commit-id: 49b0cab688b18712a694ed323d4b07c512723d1d

--- lib/csu/i386/crt0.c
+++ lib/csu/i386/crt0.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: crt0.c,v 1.16 1995/02/24 07:51:13 phk Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: crt0.c,v 1.17 1995/06/27 09:53:27 dfr Exp $  */
 end_comment
 
 begin_if
@@ -584,12 +584,33 @@ end_endif
 begin_define
 define|#
 directive|define
+name|_PUTNMSG
+parameter_list|(
+name|str
+parameter_list|,
+name|len
+parameter_list|)
+value|write(2, (str), (len))
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PUTMSG
+parameter_list|(
+name|str
+parameter_list|)
+value|_PUTNMSG((str), sizeof (str) - 1)
+end_define
+
+begin_define
+define|#
+directive|define
 name|_FATAL
 parameter_list|(
 name|str
 parameter_list|)
-define|\
-value|write(2, str, sizeof(str) - 1), \ 	_exit(1);
+value|( _PUTMSG(str), _exit(1) )
 end_define
 
 begin_macro
@@ -1238,9 +1259,79 @@ operator|-
 literal|1
 condition|)
 block|{
+name|_PUTMSG
+argument_list|(
+literal|"ld.so failed"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|_DYNAMIC
+operator|.
+name|d_entry
+operator|!=
+name|NULL
+condition|)
+block|{
+name|char
+modifier|*
+name|msg
+init|=
+call|(
+name|_DYNAMIC
+operator|.
+name|d_entry
+operator|->
+name|dlerror
+call|)
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|msg
+operator|!=
+name|NULL
+condition|)
+block|{
+name|char
+modifier|*
+name|endp
+decl_stmt|;
+name|_PUTMSG
+argument_list|(
+literal|": "
+argument_list|)
+expr_stmt|;
+for|for
+control|(
+name|endp
+operator|=
+name|msg
+init|;
+operator|*
+name|endp
+operator|!=
+literal|'\0'
+condition|;
+operator|++
+name|endp
+control|)
+empty_stmt|;
+comment|/* Find the end */
+name|_PUTNMSG
+argument_list|(
+name|msg
+argument_list|,
+name|endp
+operator|-
+name|msg
+argument_list|)
+expr_stmt|;
+block|}
+block|}
 name|_FATAL
 argument_list|(
-literal|"ld.so failed\n"
+literal|"\n"
 argument_list|)
 expr_stmt|;
 block|}
