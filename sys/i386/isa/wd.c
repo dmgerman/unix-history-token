0109e48bc25ab185204fa7facb098055c64f29bb
David Greenman <dg@FreeBSD.org>
1995-09-30 15:19:44 +0000

    Make ATAPI support completely optional - if ATAPI isn't defined, the original code/algorithm is used completely (with one bugfix). This makes it "safe" to bring these changes into -stable.
    Former-commit-id: 878bbde2d95ed65cc1c8ddce0d0d436493594d90

--- sys/i386/isa/wd.c
+++ sys/i386/isa/wd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.84 1995/09/07 08:20:18 swallace Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.85 1995/09/30 00:11:19 jkh Exp $  */
 end_comment
 
 begin_comment
@@ -2770,6 +2770,9 @@ decl_stmt|;
 name|int
 name|count
 decl_stmt|;
+ifdef|#
+directive|ifdef
+name|ATAPI
 if|if
 condition|(
 name|wdtab
@@ -2800,6 +2803,8 @@ operator|.
 name|b_active
 condition|)
 return|return;
+endif|#
+directive|endif
 name|loop
 label|:
 comment|/* is there a drive for the controller to do a transfer with? */
@@ -8894,6 +8899,8 @@ name|int
 name|wdc
 decl_stmt|,
 name|err
+init|=
+literal|0
 decl_stmt|;
 name|wdc
 operator|=
@@ -8940,10 +8947,9 @@ argument_list|,
 name|WDCTL_IDS
 argument_list|)
 expr_stmt|;
-name|err
-operator|=
-literal|0
-expr_stmt|;
+ifdef|#
+directive|ifdef
+name|ATAPI
 if|if
 condition|(
 name|wdwait
@@ -8988,9 +8994,6 @@ operator|=
 literal|1
 expr_stmt|;
 comment|/* the drive is incompatible */
-ifdef|#
-directive|ifdef
-name|ATAPI
 if|if
 condition|(
 name|err
@@ -9033,6 +9036,43 @@ literal|0
 expr_stmt|;
 comment|/* ATAPI drive detected */
 block|}
+else|#
+directive|else
+if|if
+condition|(
+name|wdwait
+argument_list|(
+name|du
+argument_list|,
+name|WDCS_READY
+operator||
+name|WDCS_SEEKCMPLT
+argument_list|,
+name|TIMEOUT
+argument_list|)
+operator|!=
+literal|0
+operator|||
+operator|(
+name|du
+operator|->
+name|dk_error
+operator|=
+name|inb
+argument_list|(
+name|wdc
+operator|+
+name|wd_error
+argument_list|)
+operator|)
+operator|!=
+literal|0x01
+condition|)
+return|return
+operator|(
+literal|1
+operator|)
+return|;
 endif|#
 directive|endif
 name|outb
