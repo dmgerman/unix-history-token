d3827231f89d31675f03b57160818da36ad3d2fd
Julian Elischer <julian@FreeBSD.org>
1995-10-04 08:58:00 +0000

    fix REALLY STUPID bug/typo don't look at the diffs.. it's too embarassing :)
    Former-commit-id: c8ec8627b0609f75cf131c4d5fd1aea93b1f1b0a

--- sys/kern/kern_conf.c
+++ sys/kern/kern_conf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Parts Copyright (c) 1995 Terrence R. Lambert  * Copyright (c) 1995 Julian R. Elischer  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY Julian R. Elischer ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kern_conf.c,v 1.1 1995/10/02 09:24:44 julian Exp $  */
+comment|/*-  * Parts Copyright (c) 1995 Terrence R. Lambert  * Copyright (c) 1995 Julian R. Elischer  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY Julian R. Elischer ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kern_conf.c,v 1.2 1995/10/02 10:15:40 julian Exp $  */
 end_comment
 
 begin_include
@@ -48,7 +48,7 @@ parameter_list|,
 name|NXXXDEV
 parameter_list|)
 define|\
-value|int TTYPE##_add(dev_t *descrip,						\ 		struct TTYPE *cdeventry,				\ 		struct TTYPE *oldentry)					\ {									\ 	int i ;								\ 	if ( (int)*descrip == -1) {
+value|int TTYPE##_add(dev_t *descrip,						\ 		struct TTYPE *newentry,					\ 		struct TTYPE *oldentry)					\ {									\ 	int i ;								\ 	if ( (int)*descrip == -1) {
 comment|/* auto (0 is valid) */
 value|\
 comment|/*							\ 		 * Search the table looking for a slot...		\ 		 */
@@ -59,18 +59,18 @@ comment|/* out of allocable slots? */
 value|\ 		if (i == NXXXDEV) {					\ 			return ENFILE;					\ 		}							\ 	} else {
 comment|/* assign */
 value|\ 		i = major(descrip);					\ 		if (i< 0 || i>= NXXXDEV) {				\ 			return EINVAL;					\ 		}							\ 	}								\ 									\
-comment|/* save old */
+comment|/* maybe save old */
 value|\         if (oldentry) {							\ 		bcopy(&TTYPE[i], oldentry, sizeof(struct TTYPE));	\ 	}								\
 comment|/* replace with new */
-value|\ 	bcopy(cdeventry,&TTYPE[i], sizeof(struct TTYPE));		\ 									\
+value|\ 	bcopy(newentry,&TTYPE[i], sizeof(struct TTYPE));		\ 									\
 comment|/* done! */
-value|\ 	*descrip = makedev(i,0);					\ 	return 0;							\ } \  ADDENTRY(cdevsw, nblkdev)
+value|\ 	*descrip = makedev(i,0);					\ 	return 0;							\ } \  ADDENTRY(bdevsw, nblkdev)
 end_define
 
 begin_macro
 name|ADDENTRY
 argument_list|(
-argument|bdevsw
+argument|cdevsw
 argument_list|,
 argument|nchrdev
 argument_list|)
