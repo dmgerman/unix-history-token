17f7ae232ef0623bade9f4d341e424e46f70af53
David Greenman <dg@FreeBSD.org>
1995-10-07 04:28:06 +0000

    Sync with main branch: various bugfixes.
    Former-commit-id: 30bebf0dfdb962d82afd0d510e1d0bbf19c9ee2a

--- sys/gnu/i386/isa/dgreg.h
+++ sys/gnu/i386/isa/dgreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  *  dgreg.h $Id: dgreg.h,v 1.4 1995/07/13 08:07:46 root Exp root $  *  *  Copyright (C) 1995 by Serge Babkin<babkin@hq.icb.chel.su>  *  *  Digiboard driver.  *  *  Stage 1. "Better than nothing".  *  *  Based on sio driver by Bruce Evans and on Linux driver by Troy   *  De Jongh<troyd@digibd.com> or<troyd@skypoint.com>   *  which is under GNU General Public License version 2 so this driver   *  is forced to be under GPL 2 too.  *  *	Serge Babkin does not guarantee that this file is totally correct  *  for any given task and users of this file must accept responsibility  *  for any damage that occurs from the application of this file.  *  *  Written by Serge Babkin,  *      Joint Stock Commercial Bank "Chelindbank"  *      (Chelyabinsk, Russia)  *      babkin@hq.icb.chel.su  */
+comment|/*-  *  dgreg.h $Id: dgreg.h,v 1.2 1995/10/04 21:51:26 jkh Exp $  *  *  Digiboard driver.  *  *  Stage 1. "Better than nothing".  *  *  Based on sio driver by Bruce Evans and on Linux driver by Troy   *  De Jongh<troyd@digibd.com> or<troyd@skypoint.com>   *  which is under GNU General Public License version 2 so this driver   *  is forced to be under GPL 2 too.  *  *  Written by Serge Babkin,  *      Joint Stock Commercial Bank "Chelindbank"  *      (Chelyabinsk, Russia)  *      babkin@hq.icb.chel.su  */
 end_comment
 
 begin_define
