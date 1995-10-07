de868aa2bbcc6ddf81050c2b9e33a141d1824d8b
David Greenman <dg@FreeBSD.org>
1995-10-07 20:17:31 +0000

    Brought in changes from rev 1.22: bugfix for IP_HDRINCL.
    Former-commit-id: 4d2083843775323b52e6d97a9442ff756839ae3f

--- sys/netinet/raw_ip.c
+++ sys/netinet/raw_ip.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)raw_ip.c	8.2 (Berkeley) 1/4/94  * $Id: raw_ip.c,v 1.21 1995/07/24 16:33:51 wollman Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)raw_ip.c	8.7 (Berkeley) 5/15/95  *	$Id: raw_ip.c,v 1.22 1995/09/21 19:59:43 wollman Exp $  */
 end_comment
 
 begin_include
@@ -808,15 +808,15 @@ block|{
 case|case
 name|IP_HDRINCL
 case|:
+name|error
+operator|=
+literal|0
+expr_stmt|;
 if|if
 condition|(
 name|op
 operator|==
 name|PRCO_SETOPT
-operator|||
-name|op
-operator|==
-name|PRCO_GETOPT
 condition|)
 block|{
 if|if
@@ -842,18 +842,11 @@ argument_list|(
 name|int
 argument_list|)
 condition|)
-return|return
-operator|(
+name|error
+operator|=
 name|EINVAL
-operator|)
-return|;
-if|if
-condition|(
-name|op
-operator|==
-name|PRCO_SETOPT
-condition|)
-block|{
+expr_stmt|;
+elseif|else
 if|if
 condition|(
 operator|*
@@ -880,6 +873,11 @@ operator|&=
 operator|~
 name|INP_HDRINCL
 expr_stmt|;
+if|if
+condition|(
+operator|*
+name|m
+condition|)
 operator|(
 name|void
 operator|)
@@ -892,6 +890,16 @@ expr_stmt|;
 block|}
 else|else
 block|{
+operator|*
+name|m
+operator|=
+name|m_get
+argument_list|(
+name|M_WAIT
+argument_list|,
+name|MT_SOOPTS
+argument_list|)
+expr_stmt|;
 operator|(
 operator|*
 name|m
@@ -923,11 +931,9 @@ expr_stmt|;
 block|}
 return|return
 operator|(
-literal|0
+name|error
 operator|)
 return|;
-block|}
-break|break;
 case|case
 name|IP_FW_ADD
 case|:
