23859776b1af304f63cbeeee6f2761cfd4a51185
Atsushi Murai <amurai@FreeBSD.org>
1995-10-08 14:57:32 +0000

    1. Add a settable redial timer and logging of the process id in a file.    A settable redial timer helps to avoid the problem where both ends    of a link want to dial at the same time and the line winds up busy    for both ends. The process id is logged in /var/run/PPP.system where    system is the name of the called system.  When both ends of a link    are running in demand dial mode, you need an easy way to get the pid    of the ppp on the called end so it can be killed and re-started with    -direct or pppd started to handle the incoming ppp session.
    2. Add secret description for "set timeout" to man.
    
    Reviewed by:	Atsushi Murai <amurai@spec.co.jp>
    Submitted by:	John Capo <jc@irbs.com>
    
    
    Former-commit-id: bbdf998da4a0aa4c5d4ec7d37a90990d3c19d921

--- usr.sbin/ppp/vars.c
+++ usr.sbin/ppp/vars.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP configuration variables  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: vars.c,v 1.3 1995/05/30 03:51:01 rgrimes Exp $  *  */
+comment|/*  *		PPP configuration variables  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: vars.c,v 1.5 1995/09/02 17:20:54 amurai Exp $  *  */
 end_comment
 
 begin_include
@@ -59,7 +59,7 @@ name|char
 name|VarLocalVersion
 index|[]
 init|=
-literal|"$Date$"
+literal|"$Date: 1995/09/02 17:20:54 $"
 decl_stmt|;
 end_decl_stmt
 
@@ -164,6 +164,10 @@ block|,
 literal|30
 block|,
 literal|3
+block|,
+name|REDIAL_PERIOD
+block|,
+literal|1
 block|,
 name|MODEM_DEV
 block|,
