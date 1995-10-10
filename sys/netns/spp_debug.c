d2499506eb21e639783b6e125955db93d1a72767
Bruce Evans <bde@FreeBSD.org>
1995-10-10 05:23:17 +0000

    Fix TCP_DEBUG support so that LINT compiles again (deReiserize).
    Former-commit-id: c3fc06ace3833233adcddaedf83b4b63e372669c

--- sys/netns/spp_debug.c
+++ sys/netns/spp_debug.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1984, 1985, 1986, 1987, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)spp_debug.c	8.1 (Berkeley) 6/10/93  * $Id: spp_debug.c,v 1.2 1994/08/02 07:51:57 davidg Exp $  */
+comment|/*  * Copyright (c) 1984, 1985, 1986, 1987, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)spp_debug.c	8.1 (Berkeley) 6/10/93  * $Id: spp_debug.c,v 1.3 1995/07/29 11:41:57 bde Exp $  */
 end_comment
 
 begin_include
@@ -570,9 +570,7 @@ name|pf
 parameter_list|(
 name|f
 parameter_list|)
-value|{ if (flags&SP_
-comment|/**/
-value|f) { printf("%s%s", cp, "f"); cp = ","; } }
+value|{ if (flags& __CONCAT(SP_,f)) \ 		{ printf("%s%s", cp, __STRING(f)); cp = ","; } }
 name|pf
 argument_list|(
 name|SP
@@ -616,9 +614,7 @@ name|p2
 parameter_list|(
 name|f
 parameter_list|)
-value|{ printf("%s = %x, ", "f", si->si_
-comment|/**/
-value|f); }
+value|{ printf("%s = %x, ", __STRING(f), si->__CONCAT(si_,f)); }
 name|p2
 argument_list|(
 name|sid
@@ -759,9 +755,7 @@ name|p3
 parameter_list|(
 name|f
 parameter_list|)
-value|{ printf("%s = %x, ", "f", sp->s_
-comment|/**/
-value|f); }
+value|{ printf("%s = %x, ", __STRING(f), sp->__CONCAT(s_,f)); }
 name|printf
 argument_list|(
 literal|"\t"
