15fa3abf190254ac7ecfc7a742f740257559656c
David Greenman <dg@FreeBSD.org>
1995-10-11 02:41:13 +0000

    Fix probe to work properly with the Cyclades cyclom-16Ye.
    Former-commit-id: f21bcd2cba1c0ed711389fdb6c06f4c0db0b3bbd

--- sys/dev/cy/cy.c
+++ sys/dev/cy/cy.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * cyclades cyclom-y serial driver  *	Andrew Herbert<andrew@werple.apana.org.au>, 17 August 1993  *  * Copyright (c) 1993 Andrew Herbert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name Andrew Herbert may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN  * NO EVENT SHALL I BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: cy.c,v 1.13 1995/07/29 08:33:06 bde Exp $  */
+comment|/*-  * cyclades cyclom-y serial driver  *	Andrew Herbert<andrew@werple.apana.org.au>, 17 August 1993  *  * Copyright (c) 1993 Andrew Herbert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name Andrew Herbert may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN  * NO EVENT SHALL I BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: cy.c,v 1.14 1995/07/31 18:29:48 bde Exp $  */
 end_comment
 
 begin_include
@@ -2037,6 +2037,15 @@ decl_stmt|;
 name|int
 name|unit
 decl_stmt|;
+name|iobase
+operator|=
+operator|(
+name|cy_addr
+operator|)
+name|dev
+operator|->
+name|id_maddr
+expr_stmt|;
 name|unit
 operator|=
 name|dev
@@ -2072,12 +2081,7 @@ expr_stmt|;
 comment|/* Cyclom-16Y hardware reset (Cyclom-8Ys don't care) */
 name|cy_inb
 argument_list|(
-operator|(
-name|cy_addr
-operator|)
-name|dev
-operator|->
-name|id_maddr
+name|iobase
 argument_list|,
 name|CY16_RESET
 argument_list|)
@@ -2089,20 +2093,26 @@ literal|500
 argument_list|)
 expr_stmt|;
 comment|/* wait for the board to get its act together */
+comment|/* this is needed to get the board out of reset */
+name|cy_outb
+argument_list|(
+name|iobase
+argument_list|,
+name|CY_CLEAR_INTR
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+name|DELAY
+argument_list|(
+literal|500
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|cyu
 operator|=
 literal|0
-operator|,
-name|iobase
-operator|=
-operator|(
-name|cy_addr
-operator|)
-name|dev
-operator|->
-name|id_maddr
 init|;
 name|cyu
 operator|<
@@ -2128,11 +2138,10 @@ literal|0
 init|;
 name|i
 operator|<
-literal|100
+literal|10
 condition|;
 name|i
-operator|+=
-literal|50
+operator|++
 control|)
 block|{
 name|DELAY
@@ -2183,11 +2192,10 @@ literal|0
 init|;
 name|i
 operator|<
-literal|1000
+literal|200
 condition|;
 name|i
-operator|+=
-literal|50
+operator|++
 control|)
 block|{
 name|DELAY
@@ -2207,9 +2215,13 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+operator|(
 name|firmware_version
-operator|!=
-literal|0
+operator|&
+literal|0xf0
+operator|)
+operator|==
+literal|0x40
 condition|)
 break|break;
 block|}
@@ -2219,7 +2231,7 @@ condition|(
 operator|(
 name|firmware_version
 operator|&
-literal|0xF0
+literal|0xf0
 operator|)
 operator|!=
 literal|0x40
