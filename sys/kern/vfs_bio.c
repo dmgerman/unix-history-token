03edaf71a39bc588c5c919fca537ba9b1d9206c9
John Dyson <dyson@FreeBSD.org>
1995-10-19 23:48:25 +0000

    If we clear the B_CACHE flag because a buffer isn't composed fully of valid bytes, we must also clear the B_DONE flag.  Some filesystems depend on this (incl NFS) and is probably the cause of the biodone error and subsequent crash.  Anyway this change needs to be made.
    Former-commit-id: aab3d621dc037ff2c18ec83d27c794c165619053

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.65 1995/10/01 05:50:27 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.66 1995/10/08 00:06:08 swallace Exp $  */
 end_comment
 
 begin_comment
@@ -4160,7 +4160,11 @@ operator|->
 name|b_flags
 operator|&=
 operator|~
+operator|(
 name|B_CACHE
+operator||
+name|B_DONE
+operator|)
 expr_stmt|;
 break|break;
 block|}
@@ -4205,7 +4209,11 @@ operator|->
 name|b_flags
 operator|&=
 operator|~
+operator|(
 name|B_CACHE
+operator||
+name|B_DONE
+operator|)
 expr_stmt|;
 break|break;
 block|}
