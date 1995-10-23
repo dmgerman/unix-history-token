2789fe20c7508b0e5c86f94b6f132dcf529035af
Steven Wallace <swallace@FreeBSD.org>
1995-10-23 19:44:38 +0000

    Remove COMPAT_IBCS2 option. Ibcs2 emulator no longer depends on owait() or any other hack to wait4().
    Former-commit-id: 66b5ae388dd1612906f9a99ef948afe17a97ef26

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_exit.c	8.7 (Berkeley) 2/12/94  * $Id: kern_exit.c,v 1.17 1995/10/08 00:06:03 swallace Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_exit.c	8.7 (Berkeley) 2/12/94  * $Id: kern_exit.c,v 1.18 1995/10/23 15:42:09 bde Exp $  */
 end_comment
 
 begin_include
@@ -1011,11 +1011,6 @@ name|defined
 argument_list|(
 name|COMPAT_43
 argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|COMPAT_IBCS2
-argument_list|)
 end_if
 
 begin_if
@@ -1219,7 +1214,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* defined(COMPAT_43) || defined(COMPAT_IBCS2) */
+comment|/* defined(COMPAT_43) */
 end_comment
 
 begin_function
@@ -1315,8 +1310,6 @@ name|int
 name|status
 decl_stmt|,
 name|error
-decl_stmt|,
-name|sig
 decl_stmt|;
 if|if
 condition|(
@@ -1410,78 +1403,6 @@ continue|continue;
 name|nfound
 operator|++
 expr_stmt|;
-if|#
-directive|if
-name|defined
-argument_list|(
-name|COMPAT_43
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|COMPAT_IBCS2
-argument_list|)
-if|if
-condition|(
-name|q
-operator|->
-name|p_sysent
-operator|->
-name|sv_sigtbl
-condition|)
-block|{
-if|if
-condition|(
-operator|(
-name|p
-operator|->
-name|p_xstat
-operator|&
-literal|0xff
-operator|)
-operator|<
-name|q
-operator|->
-name|p_sysent
-operator|->
-name|sv_sigsize
-condition|)
-name|sig
-operator|=
-name|q
-operator|->
-name|p_sysent
-operator|->
-name|sv_sigtbl
-index|[
-name|p
-operator|->
-name|p_xstat
-operator|&
-literal|0xff
-index|]
-expr_stmt|;
-else|else
-name|sig
-operator|=
-name|q
-operator|->
-name|p_sysent
-operator|->
-name|sv_sigsize
-operator|+
-literal|1
-expr_stmt|;
-block|}
-else|else
-name|sig
-operator|=
-name|p
-operator|->
-name|p_xstat
-expr_stmt|;
-endif|#
-directive|endif
 if|if
 condition|(
 name|p
@@ -1534,11 +1455,6 @@ name|defined
 argument_list|(
 name|COMPAT_43
 argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|COMPAT_IBCS2
-argument_list|)
 if|if
 condition|(
 name|compat
@@ -1548,7 +1464,9 @@ index|[
 literal|1
 index|]
 operator|=
-name|sig
+name|p
+operator|->
+name|p_xstat
 expr_stmt|;
 elseif|else
 endif|#
@@ -1958,11 +1876,6 @@ name|defined
 argument_list|(
 name|COMPAT_43
 argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|COMPAT_IBCS2
-argument_list|)
 if|if
 condition|(
 name|compat
@@ -1975,7 +1888,9 @@ index|]
 operator|=
 name|W_STOPCODE
 argument_list|(
-name|sig
+name|p
+operator|->
+name|p_xstat
 argument_list|)
 expr_stmt|;
 name|error
