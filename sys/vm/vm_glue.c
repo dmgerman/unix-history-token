f8a1f552ea8a5e362e61789fef0bbe9229678a37
John Dyson <dyson@FreeBSD.org>
1995-10-23 05:35:48 +0000

    Get rid of machine-dependent NBPG and replace with PAGE_SIZE.
    Former-commit-id: e05581f9eae0023c79ef282276867de88ce8bb8d

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_glue.c	8.6 (Berkeley) 1/5/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_glue.c,v 1.27 1995/09/24 19:51:50 dyson Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_glue.c	8.6 (Berkeley) 1/5/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_glue.c,v 1.28 1995/10/16 05:45:49 dyson Exp $  */
 end_comment
 
 begin_include
@@ -613,7 +613,7 @@ name|UPT_MIN_ADDRESS
 operator|-
 name|UPAGES
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|VM_MAX_ADDRESS
 argument_list|,
@@ -716,7 +716,7 @@ name|ptaddr
 argument_list|,
 name|ptaddr
 operator|+
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|FALSE
 argument_list|)
@@ -745,7 +745,7 @@ name|addr
 operator|+
 name|UPAGES
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|FALSE
 argument_list|)
@@ -775,7 +775,7 @@ name|u_map
 argument_list|,
 name|UPAGES
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|)
 expr_stmt|;
 if|if
@@ -817,7 +817,7 @@ operator|)
 name|up
 operator|)
 operator|+
-name|NBPG
+name|PAGE_SIZE
 operator|*
 name|i
 argument_list|,
@@ -829,7 +829,7 @@ name|pmap
 argument_list|,
 name|addr
 operator|+
-name|NBPG
+name|PAGE_SIZE
 operator|*
 name|i
 argument_list|)
@@ -1158,7 +1158,7 @@ name|ptaddr
 argument_list|,
 name|ptaddr
 operator|+
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|FALSE
 argument_list|)
@@ -1193,7 +1193,7 @@ name|kstack
 operator|+
 name|UPAGES
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|FALSE
 argument_list|)
@@ -1229,7 +1229,7 @@ name|off
 init|=
 name|i
 operator|*
-name|NBPG
+name|PAGE_SIZE
 decl_stmt|;
 name|vm_offset_t
 name|pa
@@ -1896,7 +1896,7 @@ operator|)
 operator|+
 name|UPAGES
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|)
 expr_stmt|;
 name|vm_map_pageable
@@ -1915,7 +1915,7 @@ name|kstack
 operator|+
 name|UPAGES
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|TRUE
 argument_list|)
@@ -1941,7 +1941,7 @@ name|ptaddr
 argument_list|,
 name|ptaddr
 operator|+
-name|NBPG
+name|PAGE_SIZE
 argument_list|,
 name|TRUE
 argument_list|)
