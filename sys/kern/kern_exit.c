3665939db6dec543daba14cd1dc01e0788ba3477
Bruce Evans <bde@FreeBSD.org>
1995-10-23 15:42:12 +0000

    Simplify the pseudo-argument removal changes by not optimizing for the !COMPAT_43 case - use a common function even when there is no `old' function.  The diffs for this are large because of code motion to restore the function order to what it was before the pseudo-argument changes.
    Include <sys/sysproto.h> to get correct args structs and prototypes.
    The diffs for this are large because the declarations of the args structs
    were moved to become comments in the function headers.  The comments may
    actually match the automatically generated declarations right now.
    
    Add prototypes.
    
    
    Former-commit-id: 65f7cc8b1523904025ff6672c076c06ce0fe4006

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_exit.c	8.7 (Berkeley) 2/12/94  * $Id: kern_exit.c,v 1.15 1995/05/30 08:05:25 rgrimes Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_exit.c	8.7 (Berkeley) 2/12/94  * $Id: kern_exit.c,v 1.17 1995/10/08 00:06:03 swallace Exp $  */
 end_comment
 
 begin_include
@@ -13,6 +13,12 @@ begin_include
 include|#
 directive|include
 file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/sysproto.h>
 end_include
 
 begin_include
@@ -164,20 +170,33 @@ directive|include
 file|<vm/vm_kern.h>
 end_include
 
+begin_decl_stmt
+specifier|static
+name|int
+name|wait1
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+operator|,
+expr|struct
+name|wait_args
+operator|*
+operator|,
+name|int
+index|[]
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_comment
 comment|/*  * exit --  *	Death of process.  */
 end_comment
-
-begin_struct
-struct|struct
-name|rexit_args
-block|{
-name|int
-name|rval
-decl_stmt|;
-block|}
-struct|;
-end_struct
 
 begin_function
 name|__dead
@@ -197,6 +216,7 @@ name|p
 decl_stmt|;
 name|struct
 name|rexit_args
+comment|/* { 		int	rval; 	} */
 modifier|*
 name|uap
 decl_stmt|;
@@ -984,29 +1004,6 @@ expr_stmt|;
 block|}
 end_function
 
-begin_struct
-struct|struct
-name|wait_args
-block|{
-name|int
-name|pid
-decl_stmt|;
-name|int
-modifier|*
-name|status
-decl_stmt|;
-name|int
-name|options
-decl_stmt|;
-name|struct
-name|rusage
-modifier|*
-name|rusage
-decl_stmt|;
-block|}
-struct|;
-end_struct
-
 begin_if
 if|#
 directive|if
@@ -1071,38 +1068,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_function_decl
-specifier|static
-name|int
-name|wait1
-parameter_list|(
-name|struct
-name|proc
-modifier|*
-parameter_list|,
-name|struct
-name|wait_args
-modifier|*
-parameter_list|,
-name|int
-index|[]
-parameter_list|,
-name|int
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_struct
-struct|struct
-name|owait_args
-block|{
-name|int
-name|dummy
-decl_stmt|;
-block|}
-struct|;
-end_struct
-
 begin_function
 name|int
 name|owait
@@ -1121,6 +1086,7 @@ decl_stmt|;
 specifier|register
 name|struct
 name|owait_args
+comment|/* { 		int     dummy; 	} */
 modifier|*
 name|uap
 decl_stmt|;
@@ -1247,6 +1213,15 @@ return|;
 block|}
 end_function
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* defined(COMPAT_43) || defined(COMPAT_IBCS2) */
+end_comment
+
 begin_function
 name|int
 name|wait4
@@ -1289,62 +1264,18 @@ return|;
 block|}
 end_function
 
-begin_decl_stmt
+begin_function
 specifier|static
 name|int
 name|wait1
-argument_list|(
-name|q
-argument_list|,
-name|uap
-argument_list|,
-name|retval
-argument_list|,
-name|compat
-argument_list|)
-decl|register struct
-name|proc
-modifier|*
-name|q
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|register
-name|struct
-name|wait_args
-modifier|*
-name|uap
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|retval
-index|[]
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|compat
-decl_stmt|;
-end_decl_stmt
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_function
-name|int
-name|wait4
 parameter_list|(
 name|q
 parameter_list|,
 name|uap
 parameter_list|,
 name|retval
+parameter_list|,
+name|compat
 parameter_list|)
 specifier|register
 name|struct
@@ -1355,6 +1286,7 @@ decl_stmt|;
 specifier|register
 name|struct
 name|wait_args
+comment|/* { 		int pid; 		int *status; 		int options; 		struct rusage *rusage; 	} */
 modifier|*
 name|uap
 decl_stmt|;
@@ -1362,8 +1294,9 @@ name|int
 name|retval
 index|[]
 decl_stmt|;
-endif|#
-directive|endif
+name|int
+name|compat
+decl_stmt|;
 block|{
 specifier|register
 name|int
