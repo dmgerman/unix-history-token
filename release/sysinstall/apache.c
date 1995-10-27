d7dac66e429a4307e67c85532800899233af7ad1
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-10-27 17:31:03 +0000

    Some last few tweaks - now the sample home page comes up properly by default.
    Former-commit-id: cc283a434e418e35b43c99348b92062043261cc6

--- release/sysinstall/apache.c
+++ release/sysinstall/apache.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: apache.c,v 1.6 1995/10/27 02:12:38 jkh Exp $  *  * Copyright (c) 1995  *	Coranth Gryphon.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Coranth Gryphon  *	for the FreeBSD Project.  * 4. The name of Coranth Gryphon or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY CORANTH GRYPHON ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL CORANTH GRYPHON OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: apache.c,v 1.7 1995/10/27 05:16:46 jkh Exp $  *  * Copyright (c) 1995  *	Coranth Gryphon.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Coranth Gryphon  *	for the FreeBSD Project.  * 4. The name of Coranth Gryphon or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY CORANTH GRYPHON ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL CORANTH GRYPHON OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -1008,7 +1008,7 @@ name|tconf
 operator|.
 name|docroot
 argument_list|,
-literal|"%s/data"
+literal|"%s/htdocs"
 argument_list|,
 name|APACHE_BASE
 argument_list|)
@@ -1565,11 +1565,6 @@ name|file
 index|[
 literal|128
 index|]
-decl_stmt|,
-name|cmd
-index|[
-literal|256
-index|]
 decl_stmt|;
 name|char
 modifier|*
@@ -1800,11 +1795,8 @@ operator|.
 name|docroot
 argument_list|)
 condition|)
-block|{
-name|sprintf
+name|vsystem
 argument_list|(
-name|cmd
-argument_list|,
 literal|"mkdir -p %s"
 argument_list|,
 name|tconf
@@ -1812,12 +1804,6 @@ operator|.
 name|docroot
 argument_list|)
 expr_stmt|;
-name|system
-argument_list|(
-name|cmd
-argument_list|)
-expr_stmt|;
-block|}
 if|if
 condition|(
 name|directoryExists
@@ -1832,7 +1818,7 @@ name|sprintf
 argument_list|(
 name|file
 argument_list|,
-literal|"%s/welcome.html"
+literal|"%s/index.html"
 argument_list|,
 name|tconf
 operator|.
@@ -2002,7 +1988,7 @@ name|fprintf
 argument_list|(
 name|fptr
 argument_list|,
-literal|"    For questions or comments, send mail to:\n"
+literal|"    For questions or comments, please send mail to:\n"
 argument_list|)
 expr_stmt|;
 name|fprintf
@@ -2109,11 +2095,8 @@ argument_list|(
 name|file
 argument_list|)
 condition|)
-block|{
-name|sprintf
+name|vsystem
 argument_list|(
-name|cmd
-argument_list|,
 literal|"mv -f %s %s.ORIG"
 argument_list|,
 name|file
@@ -2121,12 +2104,6 @@ argument_list|,
 name|file
 argument_list|)
 expr_stmt|;
-name|system
-argument_list|(
-name|cmd
-argument_list|)
-expr_stmt|;
-block|}
 name|fptr
 operator|=
 name|fopen
@@ -2212,11 +2189,8 @@ argument_list|(
 name|file
 argument_list|)
 condition|)
-block|{
-name|sprintf
+name|vsystem
 argument_list|(
-name|cmd
-argument_list|,
 literal|"mv -f %s %s.ORIG"
 argument_list|,
 name|file
@@ -2224,12 +2198,6 @@ argument_list|,
 name|file
 argument_list|)
 expr_stmt|;
-name|system
-argument_list|(
-name|cmd
-argument_list|)
-expr_stmt|;
-block|}
 name|fptr
 operator|=
 name|fopen
@@ -2366,11 +2334,8 @@ argument_list|(
 name|file
 argument_list|)
 condition|)
-block|{
-name|sprintf
+name|vsystem
 argument_list|(
-name|cmd
-argument_list|,
 literal|"mv -f %s %s.ORIG"
 argument_list|,
 name|file
@@ -2378,12 +2343,6 @@ argument_list|,
 name|file
 argument_list|)
 expr_stmt|;
-name|system
-argument_list|(
-name|cmd
-argument_list|)
-expr_stmt|;
-block|}
 name|fptr
 operator|=
 name|fopen
