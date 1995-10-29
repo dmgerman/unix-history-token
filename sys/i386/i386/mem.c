d3b6e47218a8d67aad1d3a6fedfa649076873f15
Bruce Evans <bde@FreeBSD.org>
1995-10-29 11:37:56 +0000

    Fix mmioctl() for !DEVRANDOM case.  mmioctl() is a function, not a pointer to a function.
    Former-commit-id: 3f16fdf092ec024b2996e410a02a63d1d4b2f5e2

--- sys/i386/i386/mem.c
+++ sys/i386/i386/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.14 1995/09/20 13:01:17 davidg Exp $  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.15 1995/10/28 16:57:55 markm Exp $  */
 end_comment
 
 begin_comment
@@ -58,6 +58,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/random.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/psl.h>
 end_include
 
@@ -89,12 +95,6 @@ begin_include
 include|#
 directive|include
 file|<vm/pmap.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<machine/random.h>
 end_include
 
 begin_ifdef
@@ -1548,12 +1548,6 @@ block|}
 block|}
 end_function
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|DEVRANDOM
-end_ifdef
-
 begin_comment
 comment|/*  * Allow userland to select which interrupts will be used in the muck  * gathering business.  */
 end_comment
@@ -1562,24 +1556,37 @@ begin_function
 name|int
 name|mmioctl
 parameter_list|(
-name|dev_t
 name|dev
 parameter_list|,
-name|int
 name|cmd
 parameter_list|,
-name|caddr_t
 name|cmdarg
 parameter_list|,
-name|int
 name|flags
 parameter_list|,
+name|p
+parameter_list|)
+name|dev_t
+name|dev
+decl_stmt|;
+name|int
+name|cmd
+decl_stmt|;
+name|caddr_t
+name|cmdarg
+decl_stmt|;
+name|int
+name|flags
+decl_stmt|;
 name|struct
 name|proc
 modifier|*
 name|p
-parameter_list|)
+decl_stmt|;
 block|{
+ifdef|#
+directive|ifdef
+name|DEVRANDOM
 if|if
 condition|(
 name|minor
@@ -1680,27 +1687,18 @@ operator|(
 literal|0
 operator|)
 return|;
-block|}
-end_function
-
-begin_else
 else|#
 directive|else
-end_else
-
-begin_decl_stmt
-name|d_ioctl_t
-modifier|*
-name|mmioctl
-init|=
-name|enodev
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
+return|return
+operator|(
+name|ENODEV
+operator|)
+return|;
 endif|#
 directive|endif
-end_endif
+comment|/* DEVRANDOM */
+block|}
+end_function
 
 end_unit
 
