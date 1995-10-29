db4db0630120b7279b992ee59e42a680cccaf1fe
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-10-29 05:57:48 +0000

    Initialize SXFRCTL0 with DFON|SPIOEN|ULTRAEN.
    Former-commit-id: 218f11904dc99356fb0c7f126ad8945312f9059e

--- sys/i386/scsi/aic7xxx.c
+++ sys/i386/scsi/aic7xxx.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/isa/aic7770.c	27/284X and aic7770 motherboard controllers  * /pci/aic7870.c	3940, 2940, aic7870 and aic7850 controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.39 1995/10/26 23:57:16 gibbs Exp $  */
+comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/isa/aic7770.c	27/284X and aic7770 motherboard controllers  * /pci/aic7870.c	3940, 2940, aic7870 and aic7850 controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.40 1995/10/28 17:27:21 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -8381,7 +8381,23 @@ name|SXFRCTL0
 operator|+
 name|iobase
 argument_list|,
+name|DFON
+operator||
+name|SPIOEN
+operator||
 name|ULTRAEN
+argument_list|)
+expr_stmt|;
+else|else
+name|outb
+argument_list|(
+name|SXFRCTL0
+operator|+
+name|iobase
+argument_list|,
+name|DFON
+operator||
+name|SPIOEN
 argument_list|)
 expr_stmt|;
 comment|/* Reset the bus */
@@ -8485,7 +8501,23 @@ name|SXFRCTL0
 operator|+
 name|iobase
 argument_list|,
+name|DFON
+operator||
+name|SPIOEN
+operator||
 name|ULTRAEN
+argument_list|)
+expr_stmt|;
+else|else
+name|outb
+argument_list|(
+name|SXFRCTL0
+operator|+
+name|iobase
+argument_list|,
+name|DFON
+operator||
+name|SPIOEN
 argument_list|)
 expr_stmt|;
 comment|/* Reset the bus */
