82a9903daccf50b11bb4b69ecc695bd5d54d2259
Peter Wemm <peter@FreeBSD.org>
1995-11-02 08:37:22 +0000

    Make putc and b_to_q deal with clists with no reserved blocks in a more civilised manner than panicing.  This only happens as a result of another state botch somewhere else, eg: from a tty driver calling putc or b_to_q on a closed device.  Apparently, it's also been implicated in a panic with a status (^T) event on ptys.
    This change should pretty well be in it's final form now.
    
    
    Former-commit-id: 8927645bee38eeb5c6586804aa2d8bf81e44ded6

--- sys/kern/tty_subr.c
+++ sys/kern/tty_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 1994, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * $Id: tty_subr.c,v 1.15 1995/10/31 19:00:00 peter Exp $  */
+comment|/*  * Copyright (C) 1994, David Greenman. This software may be used, modified,  *   copied, distributed, and sold, in both source and binary form provided  *   that the above copyright and these terms are retained. Under no  *   circumstances is the author responsible for the proper functioning  *   of this software, nor does the author assume any responsibility  *   for damages incurred with its use.  *  * $Id: tty_subr.c,v 1.16 1995/11/01 15:59:55 peter Exp $  */
 end_comment
 
 begin_comment
@@ -1431,25 +1431,16 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"putc to a clist with no reserved cblocks: data discarded.\n"
+literal|"putc to a clist with no reserved cblocks\n"
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|DIAGNOSTIC
-name|Debugger
-argument_list|(
-literal|"putc to clist with no reserved cblocks"
-argument_list|)
-expr_stmt|;
-endif|#
-directive|endif
-comment|/* black-hole the character */
 return|return
 operator|(
-literal|0
+operator|-
+literal|1
 operator|)
 return|;
+comment|/* nothing done */
 block|}
 name|cblockp
 operator|=
@@ -1784,25 +1775,15 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"b_to_q to a clist with no reserved cblocks: data discarded.\n"
+literal|"b_to_q to a clist with no reserved cblocks.\n"
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|DIAGNOSTIC
-name|Debugger
-argument_list|(
-literal|"b_to_q to clist with no reserved cblocks"
-argument_list|)
-expr_stmt|;
-endif|#
-directive|endif
-comment|/* black-hole the characters */
 return|return
 operator|(
-literal|0
+name|amount
 operator|)
 return|;
+comment|/* nothing done */
 block|}
 name|cblockp
 operator|=
