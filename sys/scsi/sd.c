7cb4004ea2457330a1af56217a294cdc5a326065
David Greenman <dg@FreeBSD.org>
1995-11-06 08:19:24 +0000

    Initialize the 3 remaining elements of the scsi_cmd struct rather than bzeroing the whole thing in sdstart().
    Former-commit-id: f42c41a8a953810d57139c8e5c1b294fc684d0f7

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.67 1995/10/12 02:01:56 julian Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.68 1995/10/21 23:13:09 phk Exp $  */
 end_comment
 
 begin_define
@@ -1973,17 +1973,6 @@ operator|>>
 literal|9
 expr_stmt|;
 comment|/* 		 *  Fill out the scsi command 		 */
-name|bzero
-argument_list|(
-operator|&
-name|cmd
-argument_list|,
-sizeof|sizeof
-argument_list|(
-name|cmd
-argument_list|)
-argument_list|)
-expr_stmt|;
 name|cmd
 operator|.
 name|op_code
@@ -2065,6 +2054,20 @@ name|nblk
 operator|&
 literal|0xff
 operator|)
+expr_stmt|;
+name|cmd
+operator|.
+name|byte2
+operator|=
+name|cmd
+operator|.
+name|reserved
+operator|=
+name|cmd
+operator|.
+name|control
+operator|=
+literal|0
 expr_stmt|;
 comment|/* 		 * Call the routine that chats with the adapter. 		 * Note: we cannot sleep as we may be an interrupt 		 */
 if|if
