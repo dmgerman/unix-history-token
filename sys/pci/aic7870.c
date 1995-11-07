0b07d4da525f7970d2e84d5c45cf9ca40f6e785b
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-11-07 05:33:27 +0000

    Remove uneccessary increment of the unit number.
    Former-commit-id: de777e8c440e61cb9cacb9c1e8391dca5a9a6167

--- sys/pci/aic7870.c
+++ sys/pci/aic7870.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  *      3940, 2940, aic7870, and aic7850 SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7870.c,v 1.17 1995/11/04 14:43:20 bde Exp $  */
+comment|/*  * Product specific probe and attach routines for:  *      3940, 2940, aic7870, and aic7850 SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7870.c,v 1.18 1995/11/05 04:51:58 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -672,9 +672,6 @@ expr_stmt|;
 return|return;
 comment|/* XXX PCI code should take return status */
 block|}
-name|ahc_unit
-operator|++
-expr_stmt|;
 name|ahc_attach
 argument_list|(
 name|unit
