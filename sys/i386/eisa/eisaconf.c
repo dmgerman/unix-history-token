2d19879e2dbccd7aa2bc4f263fa2a6ff70a6e136
Justin T. Gibbs <gibbs@FreeBSD.org>
1995-11-09 22:43:25 +0000

    Convert Adaptec 1742 driver to new eisaconf interface.
    Former-commit-id: b0759cb234fade4f050658ac2684539e382b839e

--- sys/i386/eisa/eisaconf.c
+++ sys/i386/eisa/eisaconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * EISA bus probe and attach routines   *  * Copyright (c) 1995 Justin T. Gibbs.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: eisaconf.c,v 1.4 1995/11/06 05:20:59 gibbs Exp $  */
+comment|/*  * EISA bus probe and attach routines   *  * Copyright (c) 1995 Justin T. Gibbs.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: eisaconf.c,v 1.5 1995/11/09 07:14:11 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -405,7 +405,7 @@ operator|*
 operator|)
 name|malloc
 argument_list|(
-literal|14
+literal|10
 operator|*
 sizeof|sizeof
 argument_list|(
@@ -437,7 +437,7 @@ name|e_dev
 operator|->
 name|full_name
 argument_list|,
-literal|"%c%c%c%x rev %x"
+literal|"%c%c%c%x%x"
 argument_list|,
 name|EISA_MFCTR_CHAR0
 argument_list|(
