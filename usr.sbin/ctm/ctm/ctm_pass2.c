606e200d7c60999eaef09a9aa2ac3a6ddc0b25ab
Poul-Henning Kamp <phk@FreeBSD.org>
1995-11-10 12:17:23 +0000

    Fix a core-dump.
    Submitted by:	Simon Marlow <simonm@dcs.gla.ac.uk>
    
    
    Former-commit-id: c85e05fdeb23cdd4547786ce38531467d4fe157e

--- usr.sbin/ctm/ctm/ctm_pass2.c
+++ usr.sbin/ctm/ctm/ctm_pass2.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: ctm_pass2.c,v 1.8 1995/05/30 03:47:24 rgrimes Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: ctm_pass2.c,v 1.9 1995/07/12 09:16:10 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -622,6 +622,10 @@ operator|&
 name|CTM_Q_MD5_Before
 condition|)
 block|{
+name|char
+modifier|*
+name|tmp
+decl_stmt|;
 name|GETFIELD
 argument_list|(
 name|p
@@ -641,14 +645,22 @@ operator|)
 operator|==
 name|S_IFREG
 operator|&&
-name|strcmp
-argument_list|(
+operator|(
+name|tmp
+operator|=
 name|MD5File
 argument_list|(
 name|name
 argument_list|,
 name|md5_1
 argument_list|)
+operator|)
+operator|!=
+name|NULL
+operator|&&
+name|strcmp
+argument_list|(
+name|tmp
 argument_list|,
 name|p
 argument_list|)
