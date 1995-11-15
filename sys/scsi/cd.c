59c04d6e2c63a848186155a3c52d01746d0a9d2d
Satoshi Asami <asami@FreeBSD.org>
1995-11-15 03:27:14 +0000

    Minor aesthetics (changed "." to space in "cd present.[a x b records]" and removed extraneous newline after "can't get the size").
    Former-commit-id: 071cd40c82d141bcb5be5578d08c4669f4447507

--- sys/scsi/cd.c
+++ sys/scsi/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.41 1995/05/30 08:13:20 rgrimes Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.42 1995/10/21 23:13:06 phk Exp $  */
 end_comment
 
 begin_define
@@ -1037,7 +1037,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"cd present.[%ld x %ld byte records]"
+literal|"cd present [%ld x %ld byte records]"
 argument_list|,
 name|cd
 operator|->
@@ -1057,7 +1057,7 @@ else|else
 block|{
 name|printf
 argument_list|(
-literal|"can't get the size\n"
+literal|"can't get the size"
 argument_list|)
 expr_stmt|;
 block|}
