c339d3aadd20a68c2300d91e6f549273d4f06900
Bruce Evans <bde@FreeBSD.org>
1995-11-16 10:35:29 +0000

    Fixed the type of ity_input().  A trailing arg was missing.
    Completed function declarations.
    
    Added prototypes.
    
    Removed some useless includes.
    
    
    Former-commit-id: 47140c7d73699decd61e3381833ab5a622cc6a73

--- sys/gnu/isdn/iitty.c
+++ sys/gnu/isdn/iitty.c
@@ -5,12 +5,12 @@ name|char
 name|_ittyid
 index|[]
 init|=
-literal|"@(#)$Id: iitty.c,v 1.10 1995/07/31 21:01:03 bde Exp $"
+literal|"@(#)$Id: iitty.c,v 1.11 1995/07/31 21:28:42 bde Exp $"
 decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*******************************************************************************  *  II - Version 0.1 $Revision: 1.10 $   $State: Exp $  *  * Copyright 1994 Dietmar Friede  *******************************************************************************  * Bug reports, patches, comments, suggestions should be sent to:  *  *	jkr@saarlink.de or jkrause@guug.de  *  *******************************************************************************  * $Log: iitty.c,v $  * Revision 1.10  1995/07/31  21:01:03  bde  * Obtained from:	partly from ancient patches of mine via 1.1.5  *  * Introduce TS_CONNECTED and TS_ZOMBIE states.  TS_CONNECTED is set  * while a connection is established.  It is set while (TS_CARR_ON or  * CLOCAL is set) and TS_ZOMBIE is clear.  TS_ZOMBIE is set for on to  * off transitions of TS_CARR_ON that occur when CLOCAL is clear and  * is cleared for off to on transitions of CLOCAL.  I/o can only occur  * while TS_CONNECTED is set.  TS_ZOMBIE prevents further i/o.  *  * Split the input-event sleep address TSA_CARR_ON(tp) into TSA_CARR_ON(tp)  * and TSA_HUP_OR_INPUT(tp).  The former address is now used only for  * off to on carrier transitions and equivalent CLOCAL transitions.  * The latter is used for all input events, all carrier transitions  * and certain CLOCAL transitions.  There are some harmless extra  * wakeups for rare connection- related events.  Previously there were  * too many extra wakeups for non-rare input events.  *  * Drivers now call l_modem() instead of setting TS_CARR_ON directly  * to handle even the initial off to on transition of carrier.  They  * should always have done this.  l_modem() now handles TS_CONNECTED  * and TS_ZOMBIE as well as TS_CARR_ON.  *  * gnu/isdn/iitty.c:  * Set TS_CONNECTED for first open ourself to go with bogusly setting  * CLOCAL.  *  * i386/isa/syscons.c, i386/isa/pcvt/pcvt_drv.c:  * We fake carrier, so don't also fake CLOCAL.  *  * kern/tty.c:  * Testing TS_CONNECTED instead of TS_CARR_ON fixes TIOCCONS forgetting to  * test CLOCAL.  TS_ISOPEN was tested instead, but that broke when we disabled  * the clearing of TS_ISOPEN for certain transitions of CLOCAL.  *  * Testing TS_CONNECTED fixes ttyselect() returning false success for output  * to devices in state !TS_CARR_ON&& !CLOCAL.  *  * Optimize the other selwakeup() call (this is not related to the other  * changes).  *  * kern/tty_pty.c:  * ptcopen() can be declared in traditional C now that dev_t isn't short.  *  * Revision 1.9  1995/07/22  16:44:26  bde  * Obtained from:	partly from ancient patches of mine via 1.1.5  *  * Give names to the magic tty i/o sleep addresses and use them.  This makes  * it easier to remember what the addresses are for and to keep them unique.  *  * Revision 1.8  1995/07/22  01:29:28  bde  * Move the inline code for waking up writers to a new function  * ttwwakeup().  The conditions for doing the wakeup will soon become  * more complicated and I don't want them duplicated in all drivers.  *  * It's probably not worth making ttwwakeup() a macro or an inline  * function.  The cost of the function call is relatively small when  * there is a process to wake up.  There is usually a process to wake  * up for large writes and the system call overhead dwarfs the function  * call overhead for small writes.  *  * Revision 1.7  1995/07/21  20:52:21  bde  * Obtained from:	partly from ancient patches by ache and me via 1.1.5  *  * Nuke `symbolic sleep message strings'.  Use unique literal messages so that  * `ps l' shows unambiguously where processes are sleeping.  *  * Revision 1.6  1995/07/21  16:30:37  bde  * Obtained from:	partly from an ancient patch of mine via 1.1.5  *  * Temporarily nuke TS_WOPEN.  It was only used for the obscure MDMBUF  * flow control option in the kernel and for informational purposes  * in `pstat -t'.  The latter worked properly only for ptys.  In  * general there may be multiple processes sleeping in open() and  * multiple processes that successfully opened the tty by opening it  * in O_NONBLOCK mode or during a window when CLOCAL was set.  tty.c  * doesn't have enough information to maintain the flag but always  * cleared it in ttyopen().  *  * TS_WOPEN should be restored someday just so that `pstat -t' can  * display it (MDMBUF is already fixed).  Fixing it requires counting  * of processes sleeping in open() in too many serial drivers.  *  * Revision 1.5  1995/03/28  07:54:43  bde  * Add and move declarations to fix all of the warnings from `gcc -Wimplicit'  * (except in netccitt, netiso and netns) that I didn't notice when I fixed  * "all" such warnings before.  *  * Revision 1.4  1995/02/28  00:20:30  pst  * Incorporate bde's code-review comments.  *  * (a) bring back ttselect, now that we have xxxdevtotty() it isn't dangerous.  * (b) remove all of the wrappers that have been replaced by ttselect  * (c) fix formatting in syscons.c and definition in syscons.h  * (d) add cxdevtotty  *  * NOT DONE:  * (e) make pcvt work... it was already broken...when someone fixes pcvt to  * 	link properly, just rename get_pccons to xxxdevtotty and we're done  *  * Revision 1.3  1995/02/25  20:08:52  pst  * (a) remove the pointer to each driver's tty structure array from cdevsw  * (b) add a function callback vector to tty drivers that will return a pointer  *     to a valid tty structure based upon a dev_t  * (c) make syscons structures the same size whether or not APM is enabled so  *     utilities don't crash if NAPM changes (and make the damn kernel compile!)  * (d) rewrite /dev/snp ioctl interface so that it is device driver and i386  *     independant  *  * Revision 1.2  1995/02/15  06:28:28  jkh  * Fix up include paths, nuke some warnings.  *  * Revision 1.1  1995/02/14  15:00:32  jkh  * An ISDN driver that supports the EDSS1 and the 1TR6 ISDN interfaces.  * EDSS1 is the "Euro-ISDN", 1TR6 is the soon obsolete german ISDN Interface.  * Obtained from: Dietmar Friede<dfriede@drnhh.neuhaus.de> and  * 	Juergen Krause<jkr@saarlink.de>  *  * This is only one part - the rest to follow in a couple of hours.  * This part is a benign import, since it doesn't affect anything else.  *  *  ******************************************************************************/
+comment|/*******************************************************************************  *  II - Version 0.1 $Revision: 1.11 $   $State: Exp $  *  * Copyright 1994 Dietmar Friede  *******************************************************************************  * Bug reports, patches, comments, suggestions should be sent to:  *  *	jkr@saarlink.de or jkrause@guug.de  *  *******************************************************************************  * $Log: iitty.c,v $  * Revision 1.11  1995/07/31  21:28:42  bde  * Use tsleep() instead of ttysleep() to wait for carrier since a generation  * change isn't an error.  *  * Revision 1.10  1995/07/31  21:01:03  bde  * Obtained from:	partly from ancient patches of mine via 1.1.5  *  * Introduce TS_CONNECTED and TS_ZOMBIE states.  TS_CONNECTED is set  * while a connection is established.  It is set while (TS_CARR_ON or  * CLOCAL is set) and TS_ZOMBIE is clear.  TS_ZOMBIE is set for on to  * off transitions of TS_CARR_ON that occur when CLOCAL is clear and  * is cleared for off to on transitions of CLOCAL.  I/o can only occur  * while TS_CONNECTED is set.  TS_ZOMBIE prevents further i/o.  *  * Split the input-event sleep address TSA_CARR_ON(tp) into TSA_CARR_ON(tp)  * and TSA_HUP_OR_INPUT(tp).  The former address is now used only for  * off to on carrier transitions and equivalent CLOCAL transitions.  * The latter is used for all input events, all carrier transitions  * and certain CLOCAL transitions.  There are some harmless extra  * wakeups for rare connection- related events.  Previously there were  * too many extra wakeups for non-rare input events.  *  * Drivers now call l_modem() instead of setting TS_CARR_ON directly  * to handle even the initial off to on transition of carrier.  They  * should always have done this.  l_modem() now handles TS_CONNECTED  * and TS_ZOMBIE as well as TS_CARR_ON.  *  * gnu/isdn/iitty.c:  * Set TS_CONNECTED for first open ourself to go with bogusly setting  * CLOCAL.  *  * i386/isa/syscons.c, i386/isa/pcvt/pcvt_drv.c:  * We fake carrier, so don't also fake CLOCAL.  *  * kern/tty.c:  * Testing TS_CONNECTED instead of TS_CARR_ON fixes TIOCCONS forgetting to  * test CLOCAL.  TS_ISOPEN was tested instead, but that broke when we disabled  * the clearing of TS_ISOPEN for certain transitions of CLOCAL.  *  * Testing TS_CONNECTED fixes ttyselect() returning false success for output  * to devices in state !TS_CARR_ON&& !CLOCAL.  *  * Optimize the other selwakeup() call (this is not related to the other  * changes).  *  * kern/tty_pty.c:  * ptcopen() can be declared in traditional C now that dev_t isn't short.  *  * Revision 1.9  1995/07/22  16:44:26  bde  * Obtained from:	partly from ancient patches of mine via 1.1.5  *  * Give names to the magic tty i/o sleep addresses and use them.  This makes  * it easier to remember what the addresses are for and to keep them unique.  *  * Revision 1.8  1995/07/22  01:29:28  bde  * Move the inline code for waking up writers to a new function  * ttwwakeup().  The conditions for doing the wakeup will soon become  * more complicated and I don't want them duplicated in all drivers.  *  * It's probably not worth making ttwwakeup() a macro or an inline  * function.  The cost of the function call is relatively small when  * there is a process to wake up.  There is usually a process to wake  * up for large writes and the system call overhead dwarfs the function  * call overhead for small writes.  *  * Revision 1.7  1995/07/21  20:52:21  bde  * Obtained from:	partly from ancient patches by ache and me via 1.1.5  *  * Nuke `symbolic sleep message strings'.  Use unique literal messages so that  * `ps l' shows unambiguously where processes are sleeping.  *  * Revision 1.6  1995/07/21  16:30:37  bde  * Obtained from:	partly from an ancient patch of mine via 1.1.5  *  * Temporarily nuke TS_WOPEN.  It was only used for the obscure MDMBUF  * flow control option in the kernel and for informational purposes  * in `pstat -t'.  The latter worked properly only for ptys.  In  * general there may be multiple processes sleeping in open() and  * multiple processes that successfully opened the tty by opening it  * in O_NONBLOCK mode or during a window when CLOCAL was set.  tty.c  * doesn't have enough information to maintain the flag but always  * cleared it in ttyopen().  *  * TS_WOPEN should be restored someday just so that `pstat -t' can  * display it (MDMBUF is already fixed).  Fixing it requires counting  * of processes sleeping in open() in too many serial drivers.  *  * Revision 1.5  1995/03/28  07:54:43  bde  * Add and move declarations to fix all of the warnings from `gcc -Wimplicit'  * (except in netccitt, netiso and netns) that I didn't notice when I fixed  * "all" such warnings before.  *  * Revision 1.4  1995/02/28  00:20:30  pst  * Incorporate bde's code-review comments.  *  * (a) bring back ttselect, now that we have xxxdevtotty() it isn't dangerous.  * (b) remove all of the wrappers that have been replaced by ttselect  * (c) fix formatting in syscons.c and definition in syscons.h  * (d) add cxdevtotty  *  * NOT DONE:  * (e) make pcvt work... it was already broken...when someone fixes pcvt to  * 	link properly, just rename get_pccons to xxxdevtotty and we're done  *  * Revision 1.3  1995/02/25  20:08:52  pst  * (a) remove the pointer to each driver's tty structure array from cdevsw  * (b) add a function callback vector to tty drivers that will return a pointer  *     to a valid tty structure based upon a dev_t  * (c) make syscons structures the same size whether or not APM is enabled so  *     utilities don't crash if NAPM changes (and make the damn kernel compile!)  * (d) rewrite /dev/snp ioctl interface so that it is device driver and i386  *     independant  *  * Revision 1.2  1995/02/15  06:28:28  jkh  * Fix up include paths, nuke some warnings.  *  * Revision 1.1  1995/02/14  15:00:32  jkh  * An ISDN driver that supports the EDSS1 and the 1TR6 ISDN interfaces.  * EDSS1 is the "Euro-ISDN", 1TR6 is the soon obsolete german ISDN Interface.  * Obtained from: Dietmar Friede<dfriede@drnhh.neuhaus.de> and  * 	Juergen Krause<jkr@saarlink.de>  *  * This is only one part - the rest to follow in a couple of hours.  * This part is a benign import, since it doesn't affect anything else.  *  *  ******************************************************************************/
 end_comment
 
 begin_include
@@ -42,6 +42,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"conf.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"ioctl.h"
 end_include
 
@@ -61,18 +67,6 @@ begin_include
 include|#
 directive|include
 file|"proc.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"user.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"conf.h"
 end_include
 
 begin_include
@@ -111,19 +105,42 @@ directive|include
 file|"gnu/isdn/isdn_ioctl.h"
 end_include
 
-begin_function_decl
+begin_decl_stmt
+specifier|extern
 name|int
 name|ityparam
-parameter_list|()
-function_decl|;
-end_function_decl
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|tty
+operator|*
+name|tp
+operator|,
+expr|struct
+name|termios
+operator|*
+name|t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
-begin_function_decl
+begin_decl_stmt
+specifier|extern
 name|void
 name|itystart
-parameter_list|()
-function_decl|;
-end_function_decl
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|tty
+operator|*
+name|tp
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|int
@@ -788,6 +805,9 @@ parameter_list|,
 name|char
 modifier|*
 name|buf
+parameter_list|,
+name|int
+name|dir
 parameter_list|)
 block|{
 specifier|register
