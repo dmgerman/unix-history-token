941d41c617999b4b2c7dbdc45d11665994b3c807
Bruce Evans <bde@FreeBSD.org>
1995-11-16 10:22:29 +0000

    Fixed the type of itel_input().  A trailing arg was missing.
    Included "conf.h" to get some prototypes.
    
    Removed some useless includes.
    
    
    Former-commit-id: 4001b5bcb19b2f1233b6f008431581ea3fc9106e

--- sys/gnu/isdn/iitel.c
+++ sys/gnu/isdn/iitel.c
@@ -5,12 +5,12 @@ name|char
 name|_itelid
 index|[]
 init|=
-literal|"@(#)$Id: iitel.c,v 1.4 1995/07/16 10:11:10 bde Exp $"
+literal|"@(#)$Id: iitel.c,v 1.5 1995/09/08 11:06:57 bde Exp $"
 decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*******************************************************************************  *  II - Version 0.1 $Revision: 1.4 $   $State: Exp $  *  * Copyright 1994 Dietmar Friede  *******************************************************************************  * Bug reports, patches, comments, suggestions should be sent to:  *  *	jkr@saarlink.de or jkrause@guug.de  *  *******************************************************************************  * $Log: iitel.c,v $  * Revision 1.4  1995/07/16  10:11:10  bde  * Don't include<sys/tty.h> in drivers that aren't tty drivers or in general  * files that don't depend on the internals of<sys/tty.h>  *  * Revision 1.3  1995/03/28  07:54:41  bde  * Add and move declarations to fix all of the warnings from `gcc -Wimplicit'  * (except in netccitt, netiso and netns) that I didn't notice when I fixed  * "all" such warnings before.  *  * Revision 1.2  1995/02/15  06:28:27  jkh  * Fix up include paths, nuke some warnings.  *  * Revision 1.1  1995/02/14  15:00:30  jkh  * An ISDN driver that supports the EDSS1 and the 1TR6 ISDN interfaces.  * EDSS1 is the "Euro-ISDN", 1TR6 is the soon obsolete german ISDN Interface.  * Obtained from: Dietmar Friede<dfriede@drnhh.neuhaus.de> and  * 	Juergen Krause<jkr@saarlink.de>  *  * This is only one part - the rest to follow in a couple of hours.  * This part is a benign import, since it doesn't affect anything else.  *  *  ******************************************************************************/
+comment|/*******************************************************************************  *  II - Version 0.1 $Revision: 1.5 $   $State: Exp $  *  * Copyright 1994 Dietmar Friede  *******************************************************************************  * Bug reports, patches, comments, suggestions should be sent to:  *  *	jkr@saarlink.de or jkrause@guug.de  *  *******************************************************************************  * $Log: iitel.c,v $  * Revision 1.5  1995/09/08  11:06:57  bde  * Fix benign type mismatches in devsw functions.  82 out of 299 devsw  * functions were wrong.  *  * Revision 1.4  1995/07/16  10:11:10  bde  * Don't include<sys/tty.h> in drivers that aren't tty drivers or in general  * files that don't depend on the internals of<sys/tty.h>  *  * Revision 1.3  1995/03/28  07:54:41  bde  * Add and move declarations to fix all of the warnings from `gcc -Wimplicit'  * (except in netccitt, netiso and netns) that I didn't notice when I fixed  * "all" such warnings before.  *  * Revision 1.2  1995/02/15  06:28:27  jkh  * Fix up include paths, nuke some warnings.  *  * Revision 1.1  1995/02/14  15:00:30  jkh  * An ISDN driver that supports the EDSS1 and the 1TR6 ISDN interfaces.  * EDSS1 is the "Euro-ISDN", 1TR6 is the soon obsolete german ISDN Interface.  * Obtained from: Dietmar Friede<dfriede@drnhh.neuhaus.de> and  * 	Juergen Krause<jkr@saarlink.de>  *  * This is only one part - the rest to follow in a couple of hours.  * This part is a benign import, since it doesn't affect anything else.  *  *  ******************************************************************************/
 end_comment
 
 begin_include
@@ -48,6 +48,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"conf.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"ioctl.h"
 end_include
 
@@ -55,12 +61,6 @@ begin_include
 include|#
 directive|include
 file|"proc.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"user.h"
 end_include
 
 begin_include
@@ -272,6 +272,9 @@ parameter_list|,
 name|char
 modifier|*
 name|buf
+parameter_list|,
+name|int
+name|dir
 parameter_list|)
 block|{
 name|struct
