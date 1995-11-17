55d052a6762cec2e6f4e501b709fd19826462f95
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-11-17 23:49:45 +0000

    Ack.  How embarassing.  The PCNFSD and GATED config items were essentially no-ops up to now, and the samba configuration didn't bother to add the package either.  Whoops!
    Former-commit-id: 8d81f09e38e6108beda173169d1a33f11923cf3f

--- release/sysinstall/installFinal.c
+++ release/sysinstall/installFinal.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: installFinal.c,v 1.19 1995/11/12 07:27:58 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard& Coranth Gryphon.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the authors listed above  *	for the FreeBSD Project.  * 4. The names of the authors or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE FREEBSD PROJECT ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR THEIR PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: installFinal.c,v 1.20 1995/11/12 11:12:25 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard& Coranth Gryphon.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the authors listed above  *	for the FreeBSD Project.  * 4. The names of the authors or the FreeBSD project may not be used to  *    endorse or promote products derived from this software without specific  *    prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE FREEBSD PROJECT ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR THEIR PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -154,7 +154,7 @@ value|"./smb.conf"
 end_define
 
 begin_comment
-comment|/* Load gated package and maybe even seek to configure or explain it a little */
+comment|/* Load gated package */
 end_comment
 
 begin_function
@@ -166,9 +166,53 @@ modifier|*
 name|unused
 parameter_list|)
 block|{
+if|if
+condition|(
+name|package_add
+argument_list|(
+literal|"gated-3.5a11"
+argument_list|)
+operator|==
+name|RET_SUCCESS
+condition|)
 name|variable_set2
 argument_list|(
 literal|"gated"
+argument_list|,
+literal|"YES"
+argument_list|)
+expr_stmt|;
+return|return
+name|RET_SUCCESS
+return|;
+block|}
+end_function
+
+begin_comment
+comment|/* Load pcnfsd package */
+end_comment
+
+begin_function
+name|int
+name|configPCNFSD
+parameter_list|(
+name|char
+modifier|*
+name|unused
+parameter_list|)
+block|{
+if|if
+condition|(
+name|package_add
+argument_list|(
+literal|"pcnfsd-93.02.16"
+argument_list|)
+operator|==
+name|RET_SUCCESS
+condition|)
+name|variable_set2
+argument_list|(
+literal|"pcnfsd"
 argument_list|,
 literal|"YES"
 argument_list|)
@@ -201,6 +245,20 @@ argument_list|(
 operator|&
 name|MenuSamba
 argument_list|)
+condition|)
+name|i
+operator|=
+name|RET_FAIL
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+name|package_add
+argument_list|(
+literal|"samba-1.9.14"
+argument_list|)
+operator|!=
+name|RET_SUCCESS
 condition|)
 name|i
 operator|=
