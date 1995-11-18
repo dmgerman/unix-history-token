acf028ef3ecfe401513b6102a7e15a06619da8e5
Bruce Evans <bde@FreeBSD.org>
1995-11-18 13:01:19 +0000

    Added bogus casts to avoid warnings.
    Continued cleaning up sysinit stuff.
    
    
    Former-commit-id: 1f893593bc85f8feced715ff12dc3e3890dbc2db

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.c	8.3 (Berkeley) 1/4/94  * $Id: if.c,v 1.20 1995/09/22 17:57:46 wollman Exp $  */
+comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.c	8.3 (Berkeley) 1/4/94  * $Id: if.c,v 1.21 1995/09/27 15:00:49 wollman Exp $  */
 end_comment
 
 begin_include
@@ -160,13 +160,12 @@ begin_function
 name|void
 name|ifinit
 parameter_list|(
-name|udata
+name|dummy
 parameter_list|)
 name|void
 modifier|*
-name|udata
+name|dummy
 decl_stmt|;
-comment|/* not used*/
 block|{
 specifier|register
 name|struct
@@ -2972,10 +2971,25 @@ name|so
 argument_list|,
 name|PRU_CONTROL
 argument_list|,
+operator|(
+expr|struct
+name|mbuf
+operator|*
+operator|)
 name|cmd
 argument_list|,
+operator|(
+expr|struct
+name|mbuf
+operator|*
+operator|)
 name|data
 argument_list|,
+operator|(
+expr|struct
+name|mbuf
+operator|*
+operator|)
 name|ifp
 argument_list|)
 operator|)
@@ -3121,10 +3135,26 @@ name|so
 argument_list|,
 name|PRU_CONTROL
 argument_list|,
+comment|/* 			   * XXX callees reverse the following bogus casts, 			   * but it would be easier to use a separate 			   * interface that is guaranteed to work. 			   */
+operator|(
+expr|struct
+name|mbuf
+operator|*
+operator|)
 name|cmd
 argument_list|,
+operator|(
+expr|struct
+name|mbuf
+operator|*
+operator|)
 name|data
 argument_list|,
+operator|(
+expr|struct
+name|mbuf
+operator|*
+operator|)
 name|ifp
 argument_list|)
 operator|)
