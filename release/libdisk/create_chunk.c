f6115ed0632a4362eee2668d7f5ed6f03d19860e
Jordan K. Hubbard <jkh@FreeBSD.org>
1995-11-18 10:02:10 +0000

    *really* shut this thing up. Debugging output was permanantly stapled to "on". Fix that.
    Former-commit-id: f4557b1752065e61edacaa866a0dd9de3207c2f6

--- release/libdisk/create_chunk.c
+++ release/libdisk/create_chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.21.2.4 1995/10/20 07:00:40 jkh Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.21.2.5 1995/11/17 23:04:04 jkh Exp $  *  */
 end_comment
 
 begin_include
@@ -101,7 +101,7 @@ specifier|static
 name|int
 name|debug
 init|=
-literal|1
+literal|0
 decl_stmt|;
 comment|/* Allow debugger to tweak it */
 return|return
