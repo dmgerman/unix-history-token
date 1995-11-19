54f6d2533c406b8b0e29469643e5915197944a92
Bruce Evans <bde@FreeBSD.org>
1995-11-19 00:59:22 +0000

    Fixed settimeofday(): - don't allow invalid timevals. - normalize timevals as they are built - don't call timevaladd() with   a possibly invalid timeval and normalize the result.
    Fixed a warning.
    
    
    Former-commit-id: 91b5536f0eff286aafa76e7232db409217805d8e

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.10 1995/06/29 07:07:00 davidg Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.11 1995/11/12 06:43:02 bde Exp $  */
 end_comment
 
 begin_include
@@ -354,6 +354,25 @@ operator|)
 return|;
 if|if
 condition|(
+name|atv
+operator|.
+name|tv_usec
+operator|<
+literal|0
+operator|||
+name|atv
+operator|.
+name|tv_usec
+operator|>=
+literal|1000000
+condition|)
+return|return
+operator|(
+name|EINVAL
+operator|)
+return|;
+if|if
+condition|(
 name|uap
 operator|->
 name|tzp
@@ -430,12 +449,19 @@ name|time
 operator|=
 name|atv
 expr_stmt|;
+comment|/* XXX should avoid skew in tv_usec */
 operator|(
 name|void
 operator|)
 name|splsoftclock
 argument_list|()
 expr_stmt|;
+name|timevalfix
+argument_list|(
+operator|&
+name|delta
+argument_list|)
+expr_stmt|;
 name|timevaladd
 argument_list|(
 operator|&
@@ -445,12 +471,6 @@ operator|&
 name|delta
 argument_list|)
 expr_stmt|;
-name|timevalfix
-argument_list|(
-operator|&
-name|boottime
-argument_list|)
-expr_stmt|;
 name|timevaladd
 argument_list|(
 operator|&
@@ -458,12 +478,6 @@ name|runtime
 argument_list|,
 operator|&
 name|delta
-argument_list|)
-expr_stmt|;
-name|timevalfix
-argument_list|(
-operator|&
-name|runtime
 argument_list|)
 expr_stmt|;
 name|LEASE_UPDATETIME
@@ -1156,6 +1170,11 @@ name|getitimer
 argument_list|(
 name|p
 argument_list|,
+operator|(
+expr|struct
+name|getitimer_args
+operator|*
+operator|)
 name|uap
 argument_list|,
 name|retval
