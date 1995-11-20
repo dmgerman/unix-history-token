4d04effbb443bcdd8020ae14c3a655c106cc4acf
David Greenman <dg@FreeBSD.org>
1995-11-20 02:12:34 +0000

    Trim #includes to those that are necessary; other cosmetic changes.
    Former-commit-id: c0c221b93b834451d7d246e8bbe7efec8bb2840e

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.69 1995/11/06 08:19:24 davidg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.70 1995/11/19 22:22:28 dyson Exp $  */
 end_comment
 
 begin_define
@@ -9,12 +9,6 @@ directive|define
 name|SPLSD
 value|splbio
 end_define
-
-begin_include
-include|#
-directive|include
-file|<sys/types.h>
-end_include
 
 begin_include
 include|#
@@ -43,24 +37,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/conf.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/file.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/stat.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/ioctl.h>
 end_include
 
@@ -73,25 +49,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/uio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/malloc.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/errno.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/dkstat.h>
+file|<sys/devconf.h>
 end_include
 
 begin_include
@@ -104,6 +62,24 @@ begin_include
 include|#
 directive|include
 file|<sys/diskslice.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/dkstat.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/errno.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/malloc.h>
 end_include
 
 begin_include
@@ -128,18 +104,6 @@ begin_include
 include|#
 directive|include
 file|<vm/vm.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/devconf.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/dkstat.h>
 end_include
 
 begin_include
@@ -1839,7 +1803,7 @@ name|buf
 modifier|*
 name|bp
 init|=
-literal|0
+name|NULL
 decl_stmt|;
 name|struct
 name|scsi_rw_big
