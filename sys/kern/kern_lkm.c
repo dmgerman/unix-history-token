99185f6cc54ad66a0c780df9d29c225962188f16
Poul-Henning Kamp <phk@FreeBSD.org>
1995-11-20 12:08:08 +0000

    Close the "unused" warning for things in linker-sets. This will also allow us to catch typos in the setname by running a nm through a grep.
    Former-commit-id: 67101ce47246820fcf140bf247ae932bb25d766b

--- sys/kern/kern_lkm.c
+++ sys/kern/kern_lkm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1994 Christopher G. Demetriou  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY TERRENCE R. LAMBERT ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kern_lkm.c,v 1.17 1995/11/06 00:35:50 bde Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1994 Christopher G. Demetriou  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY TERRENCE R. LAMBERT ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kern_lkm.c,v 1.18 1995/11/09 09:43:32 bde Exp $  */
 end_comment
 
 begin_comment
@@ -3520,12 +3520,61 @@ return|;
 block|}
 end_function
 
+begin_comment
+comment|/* XXX: This is bogus.  we should find a better method RSN! */
+end_comment
+
 begin_decl_stmt
 specifier|static
 specifier|const
 name|struct
 name|execsw
-name|lkm_exec_dummy
+name|lkm_exec_dummy1
+init|=
+block|{
+name|NULL
+block|,
+literal|"lkm"
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|struct
+name|execsw
+name|lkm_exec_dummy2
+init|=
+block|{
+name|NULL
+block|,
+literal|"lkm"
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|struct
+name|execsw
+name|lkm_exec_dummy3
+init|=
+block|{
+name|NULL
+block|,
+literal|"lkm"
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|struct
+name|execsw
+name|lkm_exec_dummy4
 init|=
 block|{
 name|NULL
@@ -3540,7 +3589,7 @@ name|TEXT_SET
 argument_list|(
 name|execsw_set
 argument_list|,
-name|lkm_exec_dummy
+name|lkm_exec_dummy1
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -3550,7 +3599,7 @@ name|TEXT_SET
 argument_list|(
 name|execsw_set
 argument_list|,
-name|lkm_exec_dummy
+name|lkm_exec_dummy2
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -3560,7 +3609,7 @@ name|TEXT_SET
 argument_list|(
 name|execsw_set
 argument_list|,
-name|lkm_exec_dummy
+name|lkm_exec_dummy3
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -3570,7 +3619,7 @@ name|TEXT_SET
 argument_list|(
 name|execsw_set
 argument_list|,
-name|lkm_exec_dummy
+name|lkm_exec_dummy4
 argument_list|)
 expr_stmt|;
 end_expr_stmt
