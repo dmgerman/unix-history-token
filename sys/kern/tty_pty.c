2a98d2f7d4624ddc6733a956d4d98b9e8cb0057d
Julian Elischer <julian@FreeBSD.org>
1995-12-02 07:30:19 +0000

    Submitted by:	Heikki Suonsivu
    Made the pty devices creates in devfs match the present MAKEDEV naming
    
    
    Former-commit-id: 0eb456b1058f91410f40b3cbbddf3b1a5e96e7a0

--- sys/kern/tty_pty.c
+++ sys/kern/tty_pty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_pty.c	8.2 (Berkeley) 9/23/93  * $Id: tty_pty.c,v 1.25 1995/11/29 10:48:28 julian Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_pty.c	8.2 (Berkeley) 9/23/93  * $Id: tty_pty.c,v 1.26 1995/11/29 14:40:36 julian Exp $  */
 end_comment
 
 begin_comment
@@ -192,7 +192,7 @@ comment|/* Chunk size iomoved to/from user */
 end_comment
 
 begin_comment
-comment|/*  * pts == /dev/tty[pqrs]?  * ptc == /dev/pty[pqrs]?  */
+comment|/*  * pts == /dev/tty[pqrsPQRS][0123456789abcdefghijklmnopqrstuv]  * ptc == /dev/pty[pqrsPQRS][0123456789abcdefghijklmnopqrstuv]  */
 end_comment
 
 begin_decl_stmt
@@ -3938,12 +3938,12 @@ name|char
 name|jnames
 index|[]
 init|=
-literal|"pqrstu"
+literal|"pqrsPQRS"
 name|char
 name|knames
 index|[]
 operator|=
-literal|"0123456789abcdef"
+literal|"0123456789abcdefghijklmnopqrstuv"
 name|char
 name|devname
 index|[
@@ -3953,7 +3953,7 @@ decl_stmt|;
 define|#
 directive|define
 name|MAXUNITS
-value|(6 * 16)
+value|(8 * 32)
 endif|#
 directive|endif
 name|dev_t
@@ -4051,13 +4051,13 @@ name|j
 operator|=
 name|i
 operator|/
-literal|16
+literal|32
 expr_stmt|;
 name|k
 operator|=
 name|i
 operator|%
-literal|16
+literal|32
 expr_stmt|;
 name|sprintf
 argument_list|(
