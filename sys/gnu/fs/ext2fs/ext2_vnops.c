a7e8048398dc13b7ad97ac0ed25904a034fde989
Bruce Evans <bde@FreeBSD.org>
1995-12-10 14:52:10 +0000

    Restored used includes of <vm/vm_extern.h>.
    Former-commit-id: f3ff517fb19a6007d84903b4b3b60c71de5c2ffa

--- sys/gnu/fs/ext2fs/ext2_vnops.c
+++ sys/gnu/fs/ext2fs/ext2_vnops.c
@@ -115,6 +115,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/vm_extern.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<miscfs/specfs/specdev.h>
 end_include
 
