8b96454e7f9025dc45cc0444dc5e8545bd33958c
Bruce Evans <bde@FreeBSD.org>
1995-12-14 22:51:13 +0000

    Nuked ambiguous sleep message strings: 	old:				new: 	netcls[] = "netcls"		"soclos" 	netcon[] = "netcon"		"accept", "connec" 	netio[] = "netio"		"sblock", "sbwait"
    Former-commit-id: 851c2384240657347da09f2f80f0340c53a0a115

--- sys/kern/uipc_sockbuf.c
+++ sys/kern/uipc_sockbuf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket2.c	8.1 (Berkeley) 6/10/93  * $Id: uipc_socket2.c,v 1.5 1995/05/30 08:06:22 rgrimes Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket2.c	8.1 (Berkeley) 6/10/93  * $Id: uipc_socket2.c,v 1.6 1995/11/03 18:33:46 wollman Exp $  */
 end_comment
 
 begin_include
@@ -90,37 +90,6 @@ end_include
 begin_comment
 comment|/*  * Primitive routines for operating on sockets and socket buffers  */
 end_comment
-
-begin_comment
-comment|/* strings for sleep message: */
-end_comment
-
-begin_decl_stmt
-name|char
-name|netio
-index|[]
-init|=
-literal|"netio"
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|char
-name|netcon
-index|[]
-init|=
-literal|"netcon"
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|char
-name|netcls
-index|[]
-init|=
-literal|"netcls"
-decl_stmt|;
-end_decl_stmt
 
 begin_decl_stmt
 name|u_long
@@ -1110,7 +1079,7 @@ name|PSOCK
 operator||
 name|PCATCH
 argument_list|,
-name|netio
+literal|"sbwait"
 argument_list|,
 name|sb
 operator|->
@@ -1182,7 +1151,7 @@ name|PSOCK
 operator||
 name|PCATCH
 argument_list|,
-name|netio
+literal|"sblock"
 argument_list|,
 literal|0
 argument_list|)
