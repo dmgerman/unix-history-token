22031efff6b67494c3518bd4ee5c9756afc0b196
Bruce Evans <bde@FreeBSD.org>
1995-12-17 06:59:36 +0000

    Updated to match 1TB filesize changes.  Some pindexes were still offsets and weren't converted.  ptrace() was broken.
    Former-commit-id: 6966a6c9ac29468f6f2e54cb2ea689eb1b495dfe

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994, Sean Eric Fagan  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Sean Eric Fagan.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sys_process.c,v 1.17 1995/12/07 12:46:54 davidg Exp $  */
+comment|/*  * Copyright (c) 1994, Sean Eric Fagan  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Sean Eric Fagan.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sys_process.c,v 1.18 1995/12/16 21:43:47 bde Exp $  */
 end_comment
 
 begin_include
@@ -184,8 +184,8 @@ name|wired
 decl_stmt|,
 name|single_use
 decl_stmt|;
-name|vm_offset_t
-name|off
+name|vm_pindex_t
+name|pindex
 decl_stmt|;
 comment|/* Map page into kernel space */
 name|map
@@ -235,7 +235,7 @@ operator|&
 name|object
 argument_list|,
 operator|&
-name|off
+name|pindex
 argument_list|,
 operator|&
 name|out_prot
@@ -272,7 +272,10 @@ name|kernel_map
 argument_list|,
 name|object
 argument_list|,
-name|off
+name|IDX_TO_OFF
+argument_list|(
+name|pindex
+argument_list|)
 argument_list|,
 operator|&
 name|kva
@@ -408,8 +411,8 @@ name|wired
 decl_stmt|,
 name|single_use
 decl_stmt|;
-name|vm_offset_t
-name|off
+name|vm_pindex_t
+name|pindex
 decl_stmt|;
 name|boolean_t
 name|fix_prot
@@ -518,7 +521,7 @@ operator|&
 name|object
 argument_list|,
 operator|&
-name|off
+name|pindex
 argument_list|,
 operator|&
 name|out_prot
@@ -633,7 +636,10 @@ name|kernel_map
 argument_list|,
 name|object
 argument_list|,
-name|off
+name|IDX_TO_OFF
+argument_list|(
+name|pindex
+argument_list|)
 argument_list|,
 operator|&
 name|kva
