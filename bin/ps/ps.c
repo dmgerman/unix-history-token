a4ac697ce886cd9efdf652400bb9d0f969a864a8
Peter Wemm <peter@FreeBSD.org>
1995-12-26 03:38:55 +0000

    Implement a new option to ps..  `-U username'.  This allows you to list the processes belonging to a particular user without having to use `-u' and grepping for the username.  Basically you can now get a short `ps -x' like list (with more space for the command) for other users.
    Former-commit-id: 82fe0a701dcecc30edee74c193254933c3498c78

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ps.c,v 1.8 1995/10/23 21:06:31 ache Exp $  */
+comment|/*-  * Copyright (c) 1990, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ps.c,v 1.9 1995/10/26 10:57:52 ache Exp $  */
 end_comment
 
 begin_ifndef
@@ -171,6 +171,12 @@ begin_include
 include|#
 directive|include
 file|<locale.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<pwd.h>
 end_include
 
 begin_include
@@ -504,6 +510,11 @@ name|struct
 name|winsize
 name|ws
 decl_stmt|;
+name|struct
+name|passwd
+modifier|*
+name|pwd
+decl_stmt|;
 name|dev_t
 name|ttydev
 decl_stmt|;
@@ -703,7 +714,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"aCeghjLlM:mN:O:o:p:rSTt:uvW:wx"
+literal|"aCeghjLlM:mN:O:o:p:rSTt:U:uvW:wx"
 argument_list|)
 operator|)
 operator|!=
@@ -1059,6 +1070,45 @@ expr_stmt|;
 break|break;
 block|}
 case|case
+literal|'U'
+case|:
+name|pwd
+operator|=
+name|getpwnam
+argument_list|(
+name|optarg
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|pwd
+operator|==
+name|NULL
+condition|)
+name|errx
+argument_list|(
+literal|1
+argument_list|,
+literal|"%s: no such user"
+argument_list|,
+name|optarg
+argument_list|)
+expr_stmt|;
+name|uid
+operator|=
+name|pwd
+operator|->
+name|pw_uid
+expr_stmt|;
+name|endpwent
+argument_list|()
+expr_stmt|;
+name|xflg
+operator|++
+expr_stmt|;
+comment|/* XXX: intuitive? */
+break|break;
+case|case
 literal|'u'
 case|:
 name|parsefmt
@@ -1269,6 +1319,7 @@ argument_list|(
 name|dfmt
 argument_list|)
 expr_stmt|;
+comment|/* XXX - should be cleaner */
 if|if
 condition|(
 operator|!
@@ -1282,8 +1333,15 @@ name|pid
 operator|==
 operator|-
 literal|1
+operator|&&
+name|uid
+operator|==
+operator|(
+name|uid_t
+operator|)
+operator|-
+literal|1
 condition|)
-comment|/* XXX - should be cleaner */
 name|uid
 operator|=
 name|getuid
