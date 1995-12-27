5ef08ef48c8c4955794dc20ca76e9c814a1e8af7
Paul Traina <pst@FreeBSD.org>
1995-12-27 17:07:12 +0000

    Add NEC CDR 210 to list of devices with broken LUN support
    Former-commit-id: 92b817498d11f1938f8b454761e2a11bbe89ac80

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.43 1995/12/14 19:44:29 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.44 1995/12/26 21:08:39 peter Exp $  */
 end_comment
 
 begin_include
@@ -1368,6 +1368,25 @@ block|,
 literal|"NEC"
 block|,
 literal|"CD-ROM DRIVE:55"
+block|,
+literal|"*"
+block|,
+literal|"cd"
+block|,
+name|SC_ONE_LU
+block|}
+block|,
+comment|/* 	 * Same with the OEM version of this drive (1.0 firmware). 	 * -Paul 	 */
+block|{
+name|T_READONLY
+block|,
+name|T_READONLY
+block|,
+name|T_REMOV
+block|,
+literal|"NEC"
+block|,
+literal|"CD-ROM DRIVE:210"
 block|,
 literal|"*"
 block|,
