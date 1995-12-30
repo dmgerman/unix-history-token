a806d53beff581a88949349537de86efa704507a
Joerg Wunsch <joerg@FreeBSD.org>
1995-12-30 13:56:28 +0000

    Use a medium_type field of 0 (Default), instead of the value that was obtained by the MOSE SENSE command.  SONY drives are too stupid to eat their own food.
    Submitted by:	stu@solaris.com (Stu Phillips)
    
    While i was at it, i've removed two bogus channel numbers in the``set
    mono'' command, that kept my Toshiba drive complaining.
    
    Also remove Julian's misspelling of `stereo'.
    
    
    Former-commit-id: 18c654b66b84d46dd9954d034fc5ee128a55f7a2

--- sys/scsi/cd.c
+++ sys/scsi/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.53 1995/12/14 09:54:18 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.54 1995/12/20 12:02:43 dufault Exp $  */
 end_comment
 
 begin_define
@@ -4093,10 +4093,6 @@ operator|=
 name|LEFT_CHANNEL
 operator||
 name|RIGHT_CHANNEL
-operator||
-literal|4
-operator||
-literal|8
 expr_stmt|;
 name|data
 operator|.
@@ -4163,7 +4159,7 @@ break|break;
 block|}
 break|break;
 case|case
-name|CDIOCSETSTERIO
+name|CDIOCSETSTEREO
 case|:
 block|{
 name|struct
@@ -5471,6 +5467,15 @@ operator|->
 name|header
 operator|.
 name|data_length
+operator|=
+literal|0
+expr_stmt|;
+comment|/* 	 * SONY drives do not allow a mode select with a medium_type 	 * value that has just been returned by a mode sense; use a 	 * medium_type of 0 (Default) instead. 	 */
+name|data
+operator|->
+name|header
+operator|.
+name|medium_type
 operator|=
 literal|0
 expr_stmt|;
