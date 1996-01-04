2792c501435b6925d727a7358d6a4683395b53c4
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-01-04 08:54:55 +0000

    Merge with HEAD.  Much improved aic7xxx driver and reorganized bt driver.
    Former-commit-id: e79533e149afec9a560cbb1730597a1e751d976e

--- sys/i386/scsi/aic7xxx.h
+++ sys/i386/scsi/aic7xxx.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Interface to the generic driver for the aic7xxx based adaptec  * SCSI controllers.  This is used to implement product specific  * probe and attach routines.  *  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7xxx.h,v 1.10.2.3 1995/09/21 02:11:20 davidg Exp $  */
+comment|/*  * Interface to the generic driver for the aic7xxx based adaptec  * SCSI controllers.  This is used to implement product specific  * probe and attach routines.  *  * Copyright (c) 1994, 1995 Justin T. Gibbs.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7xxx.h,v 1.17 1996/01/03 06:32:12 gibbs Exp $  */
 end_comment
 
 begin_ifndef
@@ -44,7 +44,7 @@ value|255
 end_define
 
 begin_comment
-comment|/* 				 * Up to 255 SCBs on some types of aic7xxx 				 * based boards.  The aic7870 have 16 internal 				 * SCBs, but external SRAM bumps this to 255. 				 * The aic7770 family have only 4, and the  				 * aic7850 have only 3. 				 */
+comment|/* 				 * Up to 255 SCBs on some types of aic7xxx 				 * based boards.  The aic7870 have 16 internal 				 * SCBs, but external SRAM bumps this to 255. 				 * The aic7770 family have only 4, and the  				 * aic7850 has only 3. 				 */
 end_comment
 
 begin_comment
@@ -69,7 +69,7 @@ end_typedef
 
 begin_decl_stmt
 specifier|extern
-name|int
+name|u_long
 name|ahc_unit
 decl_stmt|;
 end_decl_stmt
@@ -183,6 +183,11 @@ name|AHC_RUNNING
 init|=
 literal|0x02
 block|,
+name|AHC_USEDEFAULTS
+init|=
+literal|0x04
+block|,
+comment|/* 					 * For cards without an seeprom 					 * or a BIOS to initialize the chip's 					 * SRAM, we use the default chip and 					 * target settings. 					 */
 name|AHC_EXTSCB
 init|=
 literal|0x10
@@ -199,7 +204,7 @@ typedef|;
 end_typedef
 
 begin_comment
-comment|/*  * The driver keeps up to MAX_SCB scb structures per card in memory.  Only the  * first 26 bytes of the structure are valid for the hardware, the rest used  * for driver level bookeeping.  The "__attribute ((packed))" tags ensure that  * gcc does not attempt to pad the long ints in the structure to word  * boundaries since the first 26 bytes of this structure must have the correct  * offsets for the hardware to find them.  The driver is further optimized  * so that we only have to download the first 19 bytes since as long  * as we always use S/G, the last fields should be zero anyway.  */
+comment|/*  * The driver keeps up to MAX_SCB scb structures per card in memory.  Only the  * first 26 bytes of the structure need to be transfered to the card during  * normal operation.  The remaining fields (next_waiting and host_scb) are  * initialized the first time an SCB is allocated in get_scb().  The fields  * starting at byte 32 are used for kernel level bookeeping.    */
 end_comment
 
 begin_struct
@@ -207,89 +212,56 @@ struct|struct
 name|scb
 block|{
 comment|/* ------------    Begin hardware supported fields    ---------------- */
-comment|/*1*/
+comment|/*0*/
 name|u_char
 name|control
 decl_stmt|;
-define|#
-directive|define
-name|SCB_NEEDWDTR
-value|0x80
-comment|/* Initiate Wide Negotiation */
-define|#
-directive|define
-name|SCB_DISCENB
-value|0x40
-comment|/* Disconnection Enable */
-define|#
-directive|define
-name|SCB_TE
-value|0x20
-comment|/* Tag enable */
-define|#
-directive|define
-name|SCB_NEEDSDTR
-value|0x10
-comment|/* Initiate Sync Negotiation */
-define|#
-directive|define
-name|SCB_NEEDDMA
-value|0x08
-comment|/* Refresh SCB from host ram */
-define|#
-directive|define
-name|SCB_DIS
-value|0x04
-define|#
-directive|define
-name|SCB_TAG_TYPE
-value|0x03
-define|#
-directive|define
-name|SIMPLE_QUEUE
-value|0x00
-define|#
-directive|define
-name|HEAD_QUEUE
-value|0x01
-define|#
-directive|define
-name|OR_QUEUE
-value|0x02
-comment|/*2*/
+comment|/*1*/
 name|u_char
 name|target_channel_lun
 decl_stmt|;
 comment|/* 4/1/3 bits */
+comment|/*2*/
+name|u_char
+name|target_status
+decl_stmt|;
 comment|/*3*/
 name|u_char
 name|SG_segment_count
 decl_stmt|;
-comment|/*7*/
+comment|/*4*/
 name|physaddr
 name|SG_list_pointer
-name|__attribute__
-argument_list|(
-operator|(
-name|packed
-operator|)
-argument_list|)
 decl_stmt|;
-comment|/*11*/
-name|physaddr
-name|cmdpointer
-name|__attribute__
-argument_list|(
-operator|(
-name|packed
-operator|)
-argument_list|)
+comment|/*8*/
+name|u_char
+name|residual_SG_segment_count
+decl_stmt|;
+comment|/*9*/
+name|u_char
+name|residual_data_count
+index|[
+literal|3
+index|]
 decl_stmt|;
 comment|/*12*/
+name|physaddr
+name|data
+decl_stmt|;
+comment|/*16*/
+name|u_long
+name|datalen
+decl_stmt|;
+comment|/* Really only three bits, but its 					 * faster to treat it as a long on 					 * a quad boundary. 					 */
+comment|/*20*/
+name|physaddr
+name|cmdpointer
+decl_stmt|;
+comment|/*24*/
 name|u_char
 name|cmdlen
 decl_stmt|;
-comment|/*14*/
+comment|/*25*/
 name|u_char
 name|RESERVED
 index|[
@@ -297,75 +269,24 @@ literal|2
 index|]
 decl_stmt|;
 comment|/* must be zero */
-comment|/*15*/
-name|u_char
-name|target_status
-decl_stmt|;
-comment|/*18*/
-name|u_char
-name|residual_data_count
-index|[
-literal|3
-index|]
-decl_stmt|;
-comment|/*19*/
-name|u_char
-name|residual_SG_segment_count
-decl_stmt|;
-comment|/*23*/
-name|physaddr
-name|data
-name|__attribute__
-argument_list|(
-operator|(
-name|packed
-operator|)
-argument_list|)
-decl_stmt|;
-comment|/*26*/
-name|u_char
-name|datalen
-index|[
-literal|3
-index|]
-decl_stmt|;
 define|#
 directive|define
-name|SCB_DOWN_SIZE
+name|SCB_PIO_TRANSFER_SIZE
 value|26
-comment|/* amount to actually download */
-define|#
-directive|define
-name|SCB_UP_SIZE
-value|26
-comment|/* 					 * amount we need to upload to perform 					 * a request sense. 					 */
-comment|/*30*/
-name|physaddr
-name|host_scb
-name|__attribute__
-argument_list|(
-operator|(
-name|packed
-operator|)
-argument_list|)
-decl_stmt|;
-comment|/*31*/
+comment|/* 					 * amount we need to upload/download 					 * via rep in/outsb to perform 					 * a request sense.  The second 					 * RESERVED byte is initialized to 					 * 0 in get_scb(). 					 */
+comment|/*27*/
 name|u_char
 name|next_waiting
 decl_stmt|;
 comment|/* Used to thread SCBs awaiting 					 * selection 					 */
+comment|/*28*/
+name|physaddr
+name|host_scb
+decl_stmt|;
 define|#
 directive|define
-name|SCB_LIST_NULL
-value|0xff
-comment|/* SCB list equivelent to NULL */
-if|#
-directive|if
-literal|0
-comment|/* 	 *  No real point in transferring this to the 	 *  SCB registers. 	*/
-block|unsigned char RESERVED[1];
-endif|#
-directive|endif
+name|SCB_HARDWARE_SIZE
+value|32
 comment|/*-----------------end of hardware supported fields----------------*/
 name|struct
 name|scb
@@ -436,6 +357,9 @@ begin_struct
 struct|struct
 name|ahc_data
 block|{
+name|int
+name|unit
+decl_stmt|;
 name|ahc_type
 name|type
 decl_stmt|;
@@ -536,6 +460,108 @@ block|}
 struct|;
 end_struct
 
+begin_comment
+comment|/* Different debugging levels used when AHC_DEBUG is defined */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AHC_SHOWMISC
+value|0x0001
+end_define
+
+begin_define
+define|#
+directive|define
+name|AHC_SHOWCMDS
+value|0x0002
+end_define
+
+begin_define
+define|#
+directive|define
+name|AHC_SHOWSCBS
+value|0x0004
+end_define
+
+begin_define
+define|#
+directive|define
+name|AHC_SHOWABORTS
+value|0x0008
+end_define
+
+begin_define
+define|#
+directive|define
+name|AHC_SHOWSENSE
+value|0x0010
+end_define
+
+begin_define
+define|#
+directive|define
+name|AHC_SHOWSCBCNT
+value|0x0020
+end_define
+
+begin_comment
+comment|/* #define AHC_DEBUG */
+end_comment
+
+begin_decl_stmt
+specifier|extern
+name|int
+name|ahc_debug
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* Initialized in i386/scsi/aic7xxx.c */
+end_comment
+
+begin_comment
+comment|/*  * Since the sequencer can disable pausing in a critical section, we  * must loop until it actually stops.  * XXX Should add a timeout in here??  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PAUSE_SEQUENCER
+parameter_list|(
+name|ahc
+parameter_list|)
+define|\
+value|outb(HCNTRL + ahc->baseport, ahc->pause);   \ 				\         while ((inb(HCNTRL + ahc->baseport)& PAUSE) == 0)             \                         ;
+end_define
+
+begin_define
+define|#
+directive|define
+name|UNPAUSE_SEQUENCER
+parameter_list|(
+name|ahc
+parameter_list|)
+define|\
+value|outb( HCNTRL + ahc->baseport, ahc->unpause )
+end_define
+
+begin_comment
+comment|/*  * Restart the sequencer program from address zero  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RESTART_SEQUENCER
+parameter_list|(
+name|ahc
+parameter_list|)
+define|\
+value|do {                                    \                         outb( SEQCTL + ahc->baseport, SEQRESET|FASTMODE );    \                 } while (inb(SEQADDR0 + ahc->baseport) != 0&&   \ 			 inb(SEQADDR1 + ahc->baseport != 0));     \                                                         \                 UNPAUSE_SEQUENCER(ahc);
+end_define
+
 begin_decl_stmt
 specifier|extern
 name|struct
@@ -549,8 +575,23 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
-name|ahcprobe
+name|void
+name|ahc_reset
+name|__P
+argument_list|(
+operator|(
+name|u_long
+name|iobase
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|ahc_data
+modifier|*
+name|ahc_alloc
 name|__P
 argument_list|(
 operator|(
@@ -571,24 +612,74 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
-name|ahc_attach
+name|void
+name|ahc_free
 name|__P
 argument_list|(
 operator|(
-name|int
-name|unit
+expr|struct
+name|ahc_data
+operator|*
 operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
 
-begin_function_decl
+begin_decl_stmt
+name|int
+name|ahc_init
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|ahc_data
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|ahc_attach
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|ahc_data
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|ahc_eisa_intr
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|*
+name|arg
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 name|int
 name|ahcintr
-parameter_list|()
-function_decl|;
-end_function_decl
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|*
+name|arg
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_endif
 endif|#
