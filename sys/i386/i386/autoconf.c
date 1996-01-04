68e0d9faf01bd4ed08162341eab8d6250b7d0344
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-01-04 08:52:42 +0000

    Merge with HEAD.  Probe the PCI and Eisa busses before the ISA bus and enable interrupts before any bus configuration is performed.
    Former-commit-id: 4e6a1afb750e8bb02312751cccb1b34ad68ca12d

--- sys/i386/i386/autoconf.c
+++ sys/i386/i386/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.32 1995/05/29 04:08:13 davidg Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.33 1995/05/30 07:59:14 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -66,6 +66,16 @@ include|#
 directive|include
 file|<machine/pte.h>
 end_include
+
+begin_include
+include|#
+directive|include
+file|<i386/isa/icu.h>
+end_include
+
+begin_comment
+comment|/* For interrupts */
+end_comment
 
 begin_function_decl
 specifier|static
@@ -250,16 +260,10 @@ directive|include
 file|"eisa.h"
 end_include
 
-begin_include
-include|#
-directive|include
-file|"isa.h"
-end_include
-
 begin_if
 if|#
 directive|if
-name|NISA
+name|NEISA
 operator|>
 literal|0
 end_if
@@ -267,7 +271,7 @@ end_if
 begin_include
 include|#
 directive|include
-file|<i386/isa/isa_device.h>
+file|<i386/eisa/eisaconf.h>
 end_include
 
 begin_endif
@@ -293,6 +297,31 @@ begin_include
 include|#
 directive|include
 file|<pci/pcivar.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_include
+include|#
+directive|include
+file|"isa.h"
+end_include
+
+begin_if
+if|#
+directive|if
+name|NISA
+operator|>
+literal|0
+end_if
+
+begin_include
+include|#
+directive|include
+file|<i386/isa/isa_device.h>
 end_include
 
 begin_endif
@@ -538,6 +567,15 @@ block|{
 name|configure_start
 argument_list|()
 expr_stmt|;
+comment|/* Allow all routines to decide for themselves if they want intrs */
+name|enable_intr
+argument_list|()
+expr_stmt|;
+name|INTREN
+argument_list|(
+name|IRQ_SLAVE
+argument_list|)
+expr_stmt|;
 if|#
 directive|if
 name|NEISA
@@ -550,20 +588,20 @@ endif|#
 directive|endif
 if|#
 directive|if
-name|NISA
+name|NPCI
 operator|>
 literal|0
-name|isa_configure
+name|pci_configure
 argument_list|()
 expr_stmt|;
 endif|#
 directive|endif
 if|#
 directive|if
-name|NPCI
+name|NISA
 operator|>
 literal|0
-name|pci_configure
+name|isa_configure
 argument_list|()
 expr_stmt|;
 endif|#
