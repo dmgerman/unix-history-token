267e7171a6ea37b4001272e9ab809c06da826a22
Garrett Wollman <wollman@FreeBSD.org>
1996-01-05 20:12:53 +0000

    Convert BOUNCE_BUFFERS and BOUNCEPAGES to new option scheme.
    Former-commit-id: 368ee4dd1daa7841421102d41f2b8c2d2d07fa03

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.54 1995/12/10 19:52:59 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.55 1995/12/14 09:54:33 phk Exp $  */
 end_comment
 
 begin_comment
@@ -10,6 +10,12 @@ end_comment
 begin_comment
 comment|/*  * To do:  * work out some better way of guessing what a good timeout is going  * to be depending on whether we expect to retension or not.  *  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|"opt_bounce.h"
+end_include
 
 begin_include
 include|#
