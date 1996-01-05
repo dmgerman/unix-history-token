638fcc0dec8e1b305a6067a2012d5827b9e0ed5b
Poul-Henning Kamp <phk@FreeBSD.org>
1996-01-05 23:30:41 +0000

    Fix a fencepost error. Found by: Lars Fredriksen <fredriks@mcs.com>
    Former-commit-id: 3f569f841e428197f3862f63b710cabea69e6fec

--- lib/libc/stdlib/malloc.c
+++ lib/libc/stdlib/malloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: malloc.c,v 1.7 1995/12/11 14:28:12 peter Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: malloc.c,v 1.8 1995/12/18 12:03:54 phk Exp $  *  */
 end_comment
 
 begin_comment
@@ -1414,9 +1414,6 @@ name|map_pages
 parameter_list|(
 name|int
 name|pages
-parameter_list|,
-name|int
-name|update
 parameter_list|)
 block|{
 name|caddr_t
@@ -1488,9 +1485,11 @@ name|tail
 expr_stmt|;
 if|if
 condition|(
-name|update
-operator|&&
+operator|(
 name|last_index
+operator|+
+literal|1
+operator|)
 operator|>=
 name|malloc_ninfo
 operator|&&
@@ -2600,8 +2599,6 @@ operator|=
 name|map_pages
 argument_list|(
 name|size
-argument_list|,
-literal|1
 argument_list|)
 expr_stmt|;
 if|if
