409b543cd5c65cec33b195e2949920720eb20891
Bill Paul <wpaul@FreeBSD.org>
1996-01-06 20:28:06 +0000

    Sync with my sources at home (these are really tiny changes):
    - Fix a SEGV condition in ypxfr_main.c that reared its ugly head while I
      was working on the 'parallel jobs' feature of the new yppush. After we've
      completed the map transfer and created a local temporary copy, we check
      the order number of the map on ypserv again to make sure it didn't change
      while the transfer was in progress (map skew). If for some reason we flat
      out fail to get the order number from the server, we flag this as an
      error and bail, telling ypxfr_exit() to clean up our temporary files
      for us. However, ypxfr_exit() tries to close the database before unkining
      it, not realizing that it has already been closed prior to the skew check.
      The second attempt to close the database causes a SEGV somewhere inside
      the DB code.
    
      (Well, it does on my 2.0.5 machine anyway. I haven't seen anyone modify
      the DB library code in ages, so the condition is probably still there.)
    
      To work around this, we deliberately set dbp to NULL after closing the
      database and check for the condition in ypxfr_exit(), being careful to
      avoid the second close if we see the NULL.
    
    - In yp_dbwrite.c, make yp_open_db_rw() open the database with O_EXLOCK
      flag set. This probably won't affect much of anything, but I feel better
      having it there.
    
    
    Former-commit-id: 7be3a29d00fcbb5f0aef07450e780d79165a944c

--- libexec/ypxfr/ypxfr_main.c
+++ libexec/ypxfr/ypxfr_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ypxfr_main.c,v 1.11 1995/12/25 02:53:33 wpaul Exp $  */
+comment|/*  * Copyright (c) 1995  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ypxfr_main.c,v 1.13 1996/01/06 19:59:41 wpaul Exp $  */
 end_comment
 
 begin_include
@@ -113,7 +113,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: ypxfr_main.c,v 1.11 1995/12/25 02:53:33 wpaul Exp $"
+literal|"$Id: ypxfr_main.c,v 1.13 1996/01/06 19:59:41 wpaul Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -235,6 +235,12 @@ operator|!=
 name|NULL
 condition|)
 block|{
+if|if
+condition|(
+name|dbp
+operator|!=
+name|NULL
+condition|)
 call|(
 name|void
 call|)
@@ -1240,11 +1246,16 @@ condition|)
 block|{
 name|yp_error
 argument_list|(
-literal|"failed to find master of %s in domain %s"
+literal|"failed to find master of %s in domain %s: %s"
 argument_list|,
 name|ypxfr_mapname
 argument_list|,
 name|ypxfr_source_domain
+argument_list|,
+name|ypxfrerr_string
+argument_list|(
+name|yp_errno
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|ypxfr_exit
@@ -1289,9 +1300,14 @@ condition|)
 block|{
 name|yp_error
 argument_list|(
-literal|"failed to get order number of %s"
+literal|"failed to get order number of %s: %s"
 argument_list|,
 name|ypxfr_mapname
+argument_list|,
+name|ypxfrerr_string
+argument_list|(
+name|yp_errno
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|ypxfr_exit
@@ -1862,6 +1878,11 @@ argument_list|(
 name|dbp
 argument_list|)
 expr_stmt|;
+name|dbp
+operator|=
+name|NULL
+expr_stmt|;
+comment|/*<- yes, it seems this is necessary. */
 comment|/* Peek at the order number again and check for skew. */
 if|if
 condition|(
@@ -1885,9 +1906,14 @@ condition|)
 block|{
 name|yp_error
 argument_list|(
-literal|"failed to get order number of %s"
+literal|"failed to get order number of %s: %s"
 argument_list|,
 name|ypxfr_mapname
+argument_list|,
+name|ypxfrerr_string
+argument_list|(
+name|yp_errno
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|ypxfr_exit
