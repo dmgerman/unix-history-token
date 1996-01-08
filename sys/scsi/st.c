9517479565e201cf8d84c470a3cbc80c57018243
Joerg Wunsch <joerg@FreeBSD.org>
1996-01-08 12:26:15 +0000

    Implement the MTRETENS command.
    Former-commit-id: b25f615c69cb75d017db8dddbe28c9da79f24b35

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.55 1995/12/14 09:54:33 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.56 1996/01/05 20:12:51 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -5406,6 +5406,24 @@ argument_list|(
 name|unit
 argument_list|,
 name|FALSE
+argument_list|,
+name|flags
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|MTRETENS
+case|:
+comment|/* re-tension tape */
+name|errcode
+operator|=
+name|st_load
+argument_list|(
+name|unit
+argument_list|,
+name|LD_LOAD
+operator||
+name|LD_RETEN
 argument_list|,
 name|flags
 argument_list|)
