e749de06d72bd3472ce5c968cd4ad6cb7e850e4d
Stefan Esser <se@FreeBSD.org>
1996-01-09 19:57:49 +0000

    Add back nncr and ncrp for now. ncrcontrol relies on them. (To be replaced by sysctl accesses some time ...)
    Remove a backward jump from the NCR script, which allowed a SCSI target
    to receive any number of NOP messages it desired. If a target indeed
    does such a silly thing, make it fail at the next instruction, instead
    of causing a timeout a few seconds later.
    
    
    Former-commit-id: 8275fb27d675ad7961c76f49784b0517c602b0cc

--- sys/pci/ncr.c
+++ sys/pci/ncr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: ncr.c,v 1.53 1995/12/28 13:04:03 se Exp $ ** **  Device driver for the   NCR 53C810   PCI-SCSI-Controller. ** **  FreeBSD / NetBSD ** **------------------------------------------------------------------------- ** **  Written for 386bsd and FreeBSD by **	Wolfgang Stanglmeier<wolf@cologne.de> **	Stefan Esser<se@mi.Uni-Koeln.de> ** **  Ported to NetBSD by **	Charles M. Hannum<mycroft@gnu.ai.mit.edu> ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: ncr.c,v 1.54 1996/01/07 19:26:12 gibbs Exp $ ** **  Device driver for the   NCR 53C810   PCI-SCSI-Controller. ** **  FreeBSD / NetBSD ** **------------------------------------------------------------------------- ** **  Written for 386bsd and FreeBSD by **	Wolfgang Stanglmeier<wolf@cologne.de> **	Stefan Esser<se@mi.Uni-Koeln.de> ** **  Ported to NetBSD by **	Charles M. Hannum<mycroft@gnu.ai.mit.edu> ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
 end_comment
 
 begin_define
@@ -2234,7 +2234,7 @@ decl_stmt|;
 name|ncrcmd
 name|select
 index|[
-literal|24
+literal|22
 index|]
 decl_stmt|;
 name|ncrcmd
@@ -3123,7 +3123,7 @@ name|char
 name|ident
 index|[]
 init|=
-literal|"\n$Id: ncr.c,v 1.53 1995/12/28 13:04:03 se Exp $\n"
+literal|"\n$Id: ncr.c,v 1.54 1996/01/07 19:26:12 gibbs Exp $\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -3187,6 +3187,33 @@ ifdef|#
 directive|ifdef
 name|KERNEL
 end_ifdef
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|int
+name|nncr
+init|=
+name|MAX_UNITS
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* XXX to be replaced by SYSCTL */
+end_comment
+
+begin_decl_stmt
+name|ncb_p
+name|ncrp
+index|[
+name|MAX_UNITS
+index|]
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* XXX to be replaced by SYSCTL */
+end_comment
 
 begin_decl_stmt
 specifier|static
@@ -3845,6 +3872,10 @@ argument_list|,
 name|smsg
 argument_list|)
 block|,
+ifdef|#
+directive|ifdef
+name|undef
+comment|/* XXX better fail than try to deal with this ... */
 name|SCR_JUMPR
 operator|^
 name|IFTRUE
@@ -3858,6 +3889,8 @@ block|,
 operator|-
 literal|16
 block|,
+endif|#
+directive|endif
 name|SCR_CLR
 argument_list|(
 name|SCR_ATN
