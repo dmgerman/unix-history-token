e1c17741646010667e3140fbc668f599c86355b0
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-01-09 16:19:27 +0000

    Merge with HEAD. Don't set DSCommand.
    Former-commit-id: 93d29a836202b340db4f9c4a2651bcbeb4514a12

--- sys/pci/aic7870.c
+++ sys/pci/aic7870.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  *      3940, 2940, aic7870, and aic7850 SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7870.c,v 1.11.2.6 1996/01/04 08:55:14 gibbs Exp $  */
+comment|/*  * Product specific probe and attach routines for:  *      3940, 2940, aic7870, and aic7850 SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7870.c,v 1.11.2.7 1996/01/08 02:54:20 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -1029,20 +1029,6 @@ name|csize_lattime
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* Enable cache sized transfers, memory, and data parity checking */
-name|outb
-argument_list|(
-name|DSCOMMAND
-operator|+
-name|io_port
-argument_list|,
-name|CACHETHEN
-operator||
-name|DPARCKEN
-operator||
-name|MPARCKEN
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 operator|!
