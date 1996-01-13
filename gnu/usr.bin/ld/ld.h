221e1175749b1bb550c80a43dd13281636ba2248
John Polstra <jdp@FreeBSD.org>
1996-01-13 00:14:53 +0000

    Split up the code so that a single directory can be searched, to support some changes in the dynamic linker.  (This code is shared by the dynamic linker.)
    Reviewed by:	nate@freebsd.org
    
    
    Former-commit-id: 0cc513dc9e08f1c44f46d7ac48e268af77ebf480

--- gnu/usr.bin/ld/ld.h
+++ gnu/usr.bin/ld/ld.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	$Id: ld.h,v 1.13 1995/03/04 17:46:07 nate Exp $  */
+comment|/*  *	$Id: ld.h,v 1.14 1995/05/30 05:01:45 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -2405,6 +2405,31 @@ name|findshlib
 name|__P
 argument_list|(
 operator|(
+name|char
+operator|*
+operator|,
+name|int
+operator|*
+operator|,
+name|int
+operator|*
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|search_lib_dir
+name|__P
+argument_list|(
+operator|(
+name|char
+operator|*
+operator|,
 name|char
 operator|*
 operator|,
