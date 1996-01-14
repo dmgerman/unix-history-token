5d49d64bbdafb2fc4bd0cbce3c7718aebe95b51c
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-01-14 02:19:42 +0000

    Register our softc at attach time.  I forgot to do this in my last commit, causing a panic.
    Former-commit-id: 2052ecdc508a475b0f8590ef637dc68144a17f13

--- sys/i386/eisa/aha1742.c
+++ sys/i386/eisa/aha1742.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.46 1996/01/04 21:10:39 wollman Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.47 1996/01/07 19:20:59 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -3102,6 +3102,14 @@ operator|=
 name|ahb
 operator|->
 name|our_id
+expr_stmt|;
+name|ahb
+operator|->
+name|sc_link
+operator|.
+name|adapter_softc
+operator|=
+name|ahb
 expr_stmt|;
 name|ahb
 operator|->
