4dabe5b9313cf88255c491e6491060c54a9bf82d
Joerg Wunsch <joerg@FreeBSD.org>
1996-01-14 16:29:01 +0000

    Bump the timeout in st_load() from 5 to 15 minutes.  My Tandberg TDC3620 takes 4.5 minutes to retenstion a QIC-250 cartridge, so the 5 minutes seem to be too tight.
    Former-commit-id: f44e2f395e3547275fbdf748e5f30d79aad32a61

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.56 1996/01/05 20:12:51 wollman Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.57 1996/01/08 12:25:06 joerg Exp $  */
 end_comment
 
 begin_comment
@@ -7992,9 +7992,9 @@ literal|0
 argument_list|,
 name|ST_RETRIES
 argument_list|,
-literal|300000
+literal|900000
 argument_list|,
-comment|/* 5 min */
+comment|/* 15 min */
 name|NULL
 argument_list|,
 name|flags
