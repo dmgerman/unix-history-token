e567b321e7aa321a9aeab229c778101a0186f242
Poul-Henning Kamp <phk@FreeBSD.org>
1996-01-18 10:23:02 +0000

    Make %.*s work.
    Former-commit-id: 01c7b11a347eec6cd7c19b2cb2502d514db0e0ae

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1986, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_prf.c	8.3 (Berkeley) 1/21/94  * $Id: subr_prf.c,v 1.21 1996/01/15 22:40:43 phk Exp $  */
+comment|/*-  * Copyright (c) 1986, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_prf.c	8.3 (Berkeley) 1/21/94  * $Id: subr_prf.c,v 1.22 1996/01/16 18:08:57 phk Exp $  */
 end_comment
 
 begin_include
@@ -1630,6 +1630,9 @@ name|sign
 decl_stmt|,
 name|dot
 decl_stmt|;
+name|int
+name|dwidth
+decl_stmt|;
 name|char
 name|padc
 decl_stmt|;
@@ -1737,6 +1740,10 @@ name|dot
 operator|=
 literal|0
 expr_stmt|;
+name|dwidth
+operator|=
+literal|0
+expr_stmt|;
 name|reswitch
 label|:
 switch|switch
@@ -1837,6 +1844,12 @@ goto|;
 case|case
 literal|'0'
 case|:
+if|if
+condition|(
+operator|!
+name|dot
+condition|)
+block|{
 name|padc
 operator|=
 literal|'0'
@@ -1844,6 +1857,7 @@ expr_stmt|;
 goto|goto
 name|reswitch
 goto|;
+block|}
 case|case
 literal|'1'
 case|:
@@ -1873,7 +1887,7 @@ literal|'9'
 case|:
 for|for
 control|(
-name|width
+name|n
 operator|=
 literal|0
 init|;
@@ -1882,9 +1896,9 @@ operator|++
 name|fmt
 control|)
 block|{
-name|width
+name|n
 operator|=
-name|width
+name|n
 operator|*
 literal|10
 operator|+
@@ -1909,6 +1923,19 @@ literal|'9'
 condition|)
 break|break;
 block|}
+if|if
+condition|(
+name|dot
+condition|)
+name|dwidth
+operator|=
+name|n
+expr_stmt|;
+else|else
+name|width
+operator|=
+name|n
+expr_stmt|;
 goto|goto
 name|reswitch
 goto|;
@@ -2287,7 +2314,7 @@ literal|0
 init|;
 name|n
 operator|<
-name|width
+name|dwidth
 operator|&&
 name|p
 index|[
