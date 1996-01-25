97b4e7a1bfdd2901e85dc8cc723de8ed52f507da
Joerg Wunsch <joerg@FreeBSD.org>
1996-01-25 23:03:07 +0000

    Correctly initialize the `btprobing' variable.  It's always been 0, causing a bogus error message to appear if no board has been fond.
    Former-commit-id: c0f5f3765ab7eab29f5d828b40376aaf52e76e68

--- sys/i386/scsi/bt.c
+++ sys/i386/scsi/bt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt.c,v 1.3 1995/12/14 14:19:16 peter Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: bt.c,v 1.4 1996/01/07 19:24:36 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -1394,7 +1394,7 @@ specifier|static
 name|int
 name|btprobing
 init|=
-literal|0
+literal|1
 decl_stmt|;
 end_decl_stmt
 
