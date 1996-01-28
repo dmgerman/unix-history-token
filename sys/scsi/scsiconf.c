befdcd082d692bda73aa9f10f747a3d06c7ea27c
Julian Elischer <julian@FreeBSD.org>
1996-01-28 22:41:44 +0000

    Submitted by:	 rich (which one?)
    Another special case for probing multi-CD cdrom drives.
    
    2nd try at this patch.
    
    
    Former-commit-id: 3a907fb24f914a0f661da8fc7f68451ffd2653e4

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.45 1995/12/27 17:07:12 pst Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.47 1996/01/28 21:51:57 jkh Exp $  */
 end_comment
 
 begin_include
@@ -1337,6 +1337,24 @@ block|,
 name|SC_MORE_LUS
 block|,
 name|CD_Q_NO_TOUCH
+block|}
+block|,
+block|{
+name|T_READONLY
+block|,
+name|T_READONLY
+block|,
+name|T_REMOV
+block|,
+literal|"NRC"
+block|,
+literal|"MBR-7"
+block|,
+literal|"*"
+block|,
+literal|"cd"
+block|,
+name|SC_MORE_LUS
 block|}
 block|,
 block|{
