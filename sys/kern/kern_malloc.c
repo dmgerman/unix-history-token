b00d6535b4cb518e17b92c6009440a627716fe68
David Greenman <dg@FreeBSD.org>
1996-01-29 09:58:34 +0000

    Fixed two bugs in the calculation of the malloc area (kmem_map) size:
    1) The calculation didn't account for NMBCLUSTERS, so if a large number of
       clusters was specified, it would leave little or no space for kernel
       malloc.
    2) It was bogusly restricted to v_page_count. This doesn't take into
       account the sparseness of the malloc area and would have caused
       problems on machines with small amounts of memory. It should probably
       instead be changed to set the malloc limit to be constrained by
       the amount of memory, but I didn't do this.
    
    
    Former-commit-id: 422fedbd4e114caaab567a1bc7fcc102714d6c3a

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_malloc.c	8.3 (Berkeley) 1/4/94  * $Id: kern_malloc.c,v 1.16 1995/12/07 12:46:44 davidg Exp $  */
+comment|/*  * Copyright (c) 1987, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_malloc.c	8.3 (Berkeley) 1/4/94  * $Id: kern_malloc.c,v 1.17 1995/12/14 08:31:28 phk Exp $  */
 end_comment
 
 begin_include
@@ -31,6 +31,12 @@ begin_include
 include|#
 directive|include
 file|<sys/malloc.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mbuf.h>
 end_include
 
 begin_include
@@ -1845,23 +1851,15 @@ endif|#
 directive|endif
 name|npg
 operator|=
+operator|(
+name|nmbclusters
+operator|*
+name|MCLBYTES
+operator|+
 name|VM_KMEM_SIZE
+operator|)
 operator|/
-name|NBPG
-expr_stmt|;
-if|if
-condition|(
-name|npg
-operator|>
-name|cnt
-operator|.
-name|v_page_count
-condition|)
-name|npg
-operator|=
-name|cnt
-operator|.
-name|v_page_count
+name|PAGE_SIZE
 expr_stmt|;
 name|kmemusage
 operator|=
@@ -1914,7 +1912,7 @@ call|)
 argument_list|(
 name|npg
 operator|*
-name|NBPG
+name|PAGE_SIZE
 argument_list|)
 argument_list|,
 name|FALSE
@@ -2002,6 +2000,7 @@ condition|;
 name|indx
 operator|++
 control|)
+block|{
 name|kmemstats
 index|[
 name|indx
@@ -2009,14 +2008,21 @@ index|]
 operator|.
 name|ks_limit
 operator|=
+operator|(
 name|npg
 operator|*
-name|NBPG
+name|PAGE_SIZE
+operator|-
+name|nmbclusters
+operator|*
+name|MCLBYTES
+operator|)
 operator|*
 literal|6
 operator|/
 literal|10
 expr_stmt|;
+block|}
 endif|#
 directive|endif
 block|}
