0cbc17d3e469fa84b6e7acab2c27c1d178d0091c
Mike Pritchard <mpp@FreeBSD.org>
1996-01-30 02:56:08 +0000

    Fix a typo in a comment.
    Former-commit-id: 0a39c474ced517744e7b37c140b0c7e6dcaed1ea

--- sys/sys/cdio.h
+++ sys/sys/cdio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * 16 Feb 93	Julian Elischer	(julian@dialix.oz.au)  *  *	$Id: cdio.h,v 1.7 1995/04/09 15:50:48 jkh Exp $  */
+comment|/*  * 16 Feb 93	Julian Elischer	(julian@dialix.oz.au)  *  *	$Id: cdio.h,v 1.8 1995/05/30 08:14:11 rgrimes Exp $  */
 end_comment
 
 begin_comment
@@ -792,7 +792,7 @@ define|#
 directive|define
 name|CDSETPATCH
 value|0x00008000
-comment|/*<2>Direct routing countrol allowed*/
+comment|/*<2>Direct routing control allowed*/
 name|u_long
 name|special_function
 decl_stmt|;
