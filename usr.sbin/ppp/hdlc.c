1988e69721d9809b53802bce8618f6f86285e419
Poul-Henning Kamp <phk@FreeBSD.org>
1996-01-30 20:04:34 +0000

    Use libmd's MD5. inline hdlc checksum calculation. make big tables const.
    Former-commit-id: 496df134f86b69bac09ebef4320d8bb300a90137

--- usr.sbin/ppp/hdlc.c
+++ usr.sbin/ppp/hdlc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	     PPP High Level Link Control (HDLC) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: hdlc.c,v 1.6 1996/01/10 21:27:45 phk Exp $  *  *	TODO:  */
+comment|/*  *	     PPP High Level Link Control (HDLC) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: hdlc.c,v 1.7 1996/01/11 17:48:46 phk Exp $  *  *	TODO:  */
 end_comment
 
 begin_include
@@ -192,6 +192,7 @@ end_struct
 begin_decl_stmt
 specifier|static
 name|u_short
+specifier|const
 name|fcstab
 index|[
 literal|256
@@ -774,6 +775,7 @@ comment|/*  *  HDLC FCS computation. Read RFC 1171 Appendix B and CCITT X.25 sec
 end_comment
 
 begin_function
+specifier|inline
 name|u_short
 name|HdlcFcs
 parameter_list|(
