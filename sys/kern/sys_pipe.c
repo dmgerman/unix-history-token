1e40d1d85abd7b48a800cb10df72db27312c0e9c
John Dyson <dyson@FreeBSD.org>
1996-01-31 06:00:45 +0000

    Fix another problem with the new pipe code, pointed out by Bruce Evans. This one fixes a problem with interactions with signals.
    Former-commit-id: 72308a99464262fae5d14c8b73f7c0c6a9662b2d

--- sys/kern/sys_pipe.c
+++ sys/kern/sys_pipe.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: sys_pipe.c,v 1.2 1996/01/29 02:57:33 dyson Exp $  */
+comment|/*  * Copyright (c) 1996 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: sys_pipe.c,v 1.3 1996/01/31 02:05:12 dyson Exp $  */
 end_comment
 
 begin_ifndef
@@ -920,6 +920,9 @@ modifier|*
 name|cpipe
 decl_stmt|;
 block|{
+name|int
+name|error
+decl_stmt|;
 while|while
 condition|(
 name|cpipe
@@ -937,6 +940,8 @@ name|PIPE_LWANT
 expr_stmt|;
 if|if
 condition|(
+name|error
+operator|=
 name|tsleep
 argument_list|(
 operator|&
@@ -955,7 +960,7 @@ argument_list|)
 condition|)
 block|{
 return|return
-name|ERESTART
+name|error
 return|;
 block|}
 block|}
@@ -1378,6 +1383,8 @@ name|PIPE_WANTR
 expr_stmt|;
 if|if
 condition|(
+name|error
+operator|=
 name|tsleep
 argument_list|(
 name|rpipe
@@ -1392,10 +1399,6 @@ literal|0
 argument_list|)
 condition|)
 block|{
-name|error
-operator|=
-name|ERESTART
-expr_stmt|;
 break|break;
 block|}
 block|}
@@ -1860,6 +1863,8 @@ name|PIPE_WANTW
 expr_stmt|;
 if|if
 condition|(
+name|error
+operator|=
 name|tsleep
 argument_list|(
 name|wpipe
@@ -1878,10 +1883,6 @@ literal|0
 argument_list|)
 condition|)
 block|{
-name|error
-operator|=
-name|ERESTART
-expr_stmt|;
 break|break;
 block|}
 comment|/* 			 * If read side wants to go away, we just issue a signal 			 * to ourselves. 			 */
