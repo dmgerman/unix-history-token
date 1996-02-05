c3382c41328bf50d5be493dfca930c2f5493a901
Garrett Wollman <wollman@FreeBSD.org>
1996-02-05 19:34:27 +0000

    Make me feel a little better by filling in reasonable values for rmx_sendpipe and rmx_recvpipe.  This has no demonstrable effect on performance. (ttcp reports about 44 Mbit/s for all the buffer sizes I tried between 16384 and 65536.)
    Former-commit-id: 0ab35a4966e6a36d57c7e17e55f736b0b9a42a63

--- sys/net/if_loop.c
+++ sys/net/if_loop.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_loop.c	8.1 (Berkeley) 6/10/93  * $Id: if_loop.c,v 1.15 1995/12/02 17:11:11 bde Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_loop.c	8.1 (Berkeley) 6/10/93  * $Id: if_loop.c,v 1.16 1995/12/09 20:47:13 phk Exp $  */
 end_comment
 
 begin_comment
@@ -919,14 +919,38 @@ if|if
 condition|(
 name|rt
 condition|)
+block|{
 name|rt
 operator|->
 name|rt_rmx
 operator|.
 name|rmx_mtu
 operator|=
+name|rt
+operator|->
+name|rt_ifp
+operator|->
+name|if_mtu
+expr_stmt|;
+comment|/* for ISO */
+comment|/* 		 * For optimal performance, the send and receive buffers 		 * should be at least twice the MTU plus a little more for 		 * overhead. 		 */
+name|rt
+operator|->
+name|rt_rmx
+operator|.
+name|rmx_recvpipe
+operator|=
+name|rt
+operator|->
+name|rt_rmx
+operator|.
+name|rmx_sendpipe
+operator|=
+literal|3
+operator|*
 name|LOMTU
 expr_stmt|;
+block|}
 block|}
 end_function
 
@@ -1010,20 +1034,6 @@ operator|*
 operator|)
 name|data
 expr_stmt|;
-if|if
-condition|(
-name|ifa
-operator|!=
-literal|0
-operator|&&
-name|ifa
-operator|->
-name|ifa_addr
-operator|->
-name|sa_family
-operator|==
-name|AF_ISO
-condition|)
 name|ifa
 operator|->
 name|ifa_rtrequest
