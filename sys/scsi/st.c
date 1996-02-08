f15f3a1a59e1c4708bfa2682e2a3d7c1215c80bd
Paul Traina <pst@FreeBSD.org>
1996-02-08 06:23:49 +0000

    Close kern/614 - wait up to an hour for a tape operation like "fsf" (slow drives)
    Former-commit-id: 7c0dbfcd775b6ec19cd9ee316f3b824b117de3d4

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.59 1996/01/29 03:19:23 gibbs Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.60 1996/02/03 13:31:13 joerg Exp $  */
 end_comment
 
 begin_comment
@@ -7486,9 +7486,9 @@ argument_list|,
 literal|0
 argument_list|,
 comment|/* no retries please , just fail */
-literal|600000
+literal|3600000
 argument_list|,
-comment|/* 10 mins enough? */
+comment|/* wait up to an hour for completion */
 name|NULL
 argument_list|,
 name|flags
