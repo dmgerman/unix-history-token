1e2308867bfa504c6ffd3ceb201a850cf879da3e
Poul-Henning Kamp <phk@FreeBSD.org>
1996-02-11 18:12:42 +0000

    Make a scaffold libgnumalloc.so.#.# unless there is a packeage called "gnumalloc*"
    Former-commit-id: 0592f80c1b97b3f4014a6119774b58015c1f63d9

---
+++ lib/libgnumalloc/cfree.c
@@ -0,0 +1,14 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_function
+name|int
+name|fake_a_gnumalloc_lib
+parameter_list|()
+block|{
+return|return
+literal|1
+return|;
+block|}
+end_function
+
+end_unit
+
