fa7d78cbc57e7c2aa13c8a188b3ed92a617d2ea8
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-02-12 17:03:27 +0000

    Correct PCI ID for the Buslogic 946.
    Former-commit-id: 0de720d233499f8f2b89b7a92d6c3a2bdc3d5286

--- sys/pci/bt9xx.c
+++ sys/pci/bt9xx.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  *      Buslogic BT946 and BT956 SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: bt9xx.c,v 1.2 1995/12/14 09:53:56 phk Exp $  */
+comment|/*  * Product specific probe and attach routines for:  *      Buslogic BT946 and BT956 SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: bt9xx.c,v 1.3 1995/12/14 14:19:19 peter Exp $  */
 end_comment
 
 begin_include
@@ -86,7 +86,7 @@ begin_define
 define|#
 directive|define
 name|PCI_DEVICE_ID_BUSLOGIC_946
-value|0x104B1040ul
+value|0x1040104Bul
 end_define
 
 begin_decl_stmt
