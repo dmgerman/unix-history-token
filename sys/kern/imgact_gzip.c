b238e1059dac2544e8e1ceb0323d104d0c13940d
Poul-Henning Kamp <phk@FreeBSD.org>
1996-02-13 14:16:36 +0000

    rewrap some long lines.
    Former-commit-id: d8ba8f0a9f551d2fd48761b927f857c61d6c3f72

--- sys/kern/imgact_gzip.c
+++ sys/kern/imgact_gzip.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: imgact_gzip.c,v 1.17 1995/12/07 12:46:35 davidg Exp $  *  * This module handles execution of a.out files which have been run through  * "gzip".  This saves diskspace, but wastes cpu-cycles and VM.  *  * TODO:  *	text-segments should be made R/O after being filled  *	is the vm-stuff safe ?  * 	should handle the entire header of gzip'ed stuff.  *	inflate isn't quite reentrant yet...  *	error-handling is a mess...  *	so is the rest...  *	tidy up unnecesary includes  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dkuug.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: imgact_gzip.c,v 1.18 1996/01/19 03:57:56 dyson Exp $  *  * This module handles execution of a.out files which have been run through  * "gzip".  This saves diskspace, but wastes cpu-cycles and VM.  *  * TODO:  *	text-segments should be made R/O after being filled  *	is the vm-stuff safe ?  * 	should handle the entire header of gzip'ed stuff.  *	inflate isn't quite reentrant yet...  *	error-handling is a mess...  *	so is the rest...  *	tidy up unnecesary includes  */
 end_comment
 
 begin_include
@@ -1083,7 +1083,7 @@ operator|!=
 literal|0
 condition|)
 block|{
-comment|/* 		 * Allocate demand-zeroed area for uninitialized data "bss" = 'block 		 * started by symbol' - named after the IBM 7090 instruction of the 		 * same name. 		 */
+comment|/* 		 * Allocate demand-zeroed area for uninitialized data. 		 * "bss" = 'block started by symbol' - named after the  		 * IBM 7090 instruction of the same name. 		 */
 name|vmaddr
 operator|=
 name|gz
