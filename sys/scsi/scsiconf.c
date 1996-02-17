aa57e2b75b4c68a8dddfc4d438ed2b6eb955fc29
Stefan Esser <se@FreeBSD.org>
1996-02-17 23:59:24 +0000

    Add: CDU-8003A aka Apple CDROM-300
    Submitted by:	Michael Reifenberger <root@totum.plaut.de>
    
    
    Former-commit-id: da48350b849f50c939369fa4ae3b3b626b6e7d27

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.52 1996/02/03 13:31:11 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.53 1996/02/12 23:22:33 ache Exp $  */
 end_comment
 
 begin_include
@@ -1388,6 +1388,25 @@ ifndef|#
 directive|ifndef
 name|UKTEST
 comment|/* make cdroms unrecognised to test the uk driver */
+comment|/* 	* CDU-8003A aka Apple CDROM-300. 	*/
+block|{
+name|T_READONLY
+block|,
+name|T_READONLY
+block|,
+name|T_REMOV
+block|,
+literal|"SONY"
+block|,
+literal|"CD-ROM CDU-8003A"
+block|,
+literal|"1.9a"
+block|,
+literal|"cd"
+block|,
+name|SC_ONE_LU
+block|}
+block|,
 block|{
 name|T_READONLY
 block|,
