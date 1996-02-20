93fc93e16ab0a6e0b9188758aed5d84e08aa54fe
John Polstra <jdp@FreeBSD.org>
1996-02-20 04:07:26 +0000

    Changed the dimensions of __CTOR_LIST__ and __DTOR_LIST__ from 0 to 2.  This makes them agree with the declarations in libgcc, and clears the way once again for linking c++rt0.o into all libraries, and eliminating CPLUSPLUSLIB from <bsd.lib.mk>.  (I have not made that change yet, because there is still a bootstrapping problem for "make world".)
    Also, removed a check which ensured that the constructor count in
    the first word of __CTOR_LIST__ was greater than zero before
    traversing the list.  I had added that check earlier, but it is no
    longer necessary, now that there is guaranteed to be at least 2
    words in __CTOR_LIST__.
    
    
    Former-commit-id: 192c788465dc0587ad6527049c5e7ea456ace1f8

--- lib/csu/i386/c++rt0.c
+++ lib/csu/i386/c++rt0.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: c++rt0.c,v 1.3 1995/06/27 09:53:24 dfr Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: c++rt0.c,v 1.4 1996/01/15 17:53:25 jdp Exp $  */
 end_comment
 
 begin_comment
@@ -13,7 +13,7 @@ function_decl|(
 modifier|*
 name|__CTOR_LIST__
 index|[
-literal|0
+literal|2
 index|]
 function_decl|)
 parameter_list|(
@@ -28,7 +28,7 @@ function_decl|(
 modifier|*
 name|__DTOR_LIST__
 index|[
-literal|0
+literal|2
 index|]
 function_decl|)
 parameter_list|(
@@ -97,21 +97,6 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
-comment|/* 	 * If the shared library doesn't have any static constructors in it, 	 * then __CTOR_LIST__ will come out as a simple COMMON region of 	 * 4 bytes.  That is why we have to check the count in the first 	 * word. 	 */
-if|if
-condition|(
-operator|(
-name|unsigned
-name|long
-operator|)
-name|__CTOR_LIST__
-index|[
-literal|0
-index|]
-operator|>
-literal|0
-condition|)
-block|{
 name|void
 function_decl|(
 modifier|*
@@ -140,7 +125,6 @@ operator|)
 operator|(
 operator|)
 expr_stmt|;
-block|}
 block|}
 end_function
 
