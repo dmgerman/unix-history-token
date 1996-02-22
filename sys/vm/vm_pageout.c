c0a93dd29c9bb44ed06f8ed1e5ad098adee6c269
David Greenman <dg@FreeBSD.org>
1996-02-22 10:57:37 +0000

    Add a "NO_SWAPPING" option to disable swapping. This was originally done to help diagnose a problem on wcarchive (where the kernel stack was sometimes not present), but is useful in its own right since swapping actually reduces performance on some systems (such as wcarchive). Note: swapping in this context means making the U pages pageable and has nothing to do with generic VM paging, which is unaffected by this option.
    Reviewed by:	 <dyson>
    
    
    Former-commit-id: 7ffc9ee603e2b445c4c1439c097a70b168375762

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_pageout.c	7.4 (Berkeley) 5/7/91  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_pageout.c,v 1.64 1996/01/19 04:00:23 dyson Exp $  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_pageout.c	7.4 (Berkeley) 5/7/91  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_pageout.c,v 1.65 1996/01/31 12:44:33 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -228,6 +228,12 @@ argument|&page_kp
 argument_list|)
 end_macro
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|NO_SWAPPING
+end_ifndef
+
 begin_comment
 comment|/* the kernel process "vm_daemon"*/
 end_comment
@@ -285,6 +291,15 @@ argument_list|,
 argument|&vm_kp
 argument_list|)
 end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !NO_SWAPPING */
+end_comment
 
 begin_decl_stmt
 name|int
@@ -1907,53 +1922,6 @@ return|return;
 block|}
 end_function
 
-begin_function
-specifier|static
-name|void
-name|vm_req_vmdaemon
-parameter_list|()
-block|{
-specifier|static
-name|int
-name|lastrun
-init|=
-literal|0
-decl_stmt|;
-if|if
-condition|(
-operator|(
-name|ticks
-operator|>
-operator|(
-name|lastrun
-operator|+
-name|hz
-operator|/
-literal|10
-operator|)
-operator|)
-operator|||
-operator|(
-name|ticks
-operator|<
-name|lastrun
-operator|)
-condition|)
-block|{
-name|wakeup
-argument_list|(
-operator|&
-name|vm_daemon_needed
-argument_list|)
-expr_stmt|;
-name|lastrun
-operator|=
-name|ticks
-expr_stmt|;
-block|}
-block|}
-end_function
-
 begin_comment
 comment|/*  *	vm_pageout_scan does the dirty work for the pageout daemon.  */
 end_comment
@@ -2990,6 +2958,9 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+ifndef|#
+directive|ifndef
+name|NO_SWAPPING
 comment|/* 		 * now swap processes out if we are in low memory conditions 		 */
 if|if
 condition|(
@@ -3011,7 +2982,12 @@ name|vm_req_vmdaemon
 argument_list|()
 expr_stmt|;
 block|}
+endif|#
+directive|endif
 block|}
+ifndef|#
+directive|ifndef
+name|NO_SWAPPING
 if|if
 condition|(
 operator|(
@@ -3043,6 +3019,8 @@ name|vm_req_vmdaemon
 argument_list|()
 expr_stmt|;
 block|}
+endif|#
+directive|endif
 comment|/* 	 * make sure that we have swap space -- if we are low on memory and 	 * swap -- then kill the biggest process. 	 */
 if|if
 condition|(
@@ -3532,6 +3510,59 @@ block|}
 block|}
 end_function
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|NO_SWAPPING
+end_ifndef
+
+begin_function
+specifier|static
+name|void
+name|vm_req_vmdaemon
+parameter_list|()
+block|{
+specifier|static
+name|int
+name|lastrun
+init|=
+literal|0
+decl_stmt|;
+if|if
+condition|(
+operator|(
+name|ticks
+operator|>
+operator|(
+name|lastrun
+operator|+
+name|hz
+operator|/
+literal|10
+operator|)
+operator|)
+operator|||
+operator|(
+name|ticks
+operator|<
+name|lastrun
+operator|)
+condition|)
+block|{
+name|wakeup
+argument_list|(
+operator|&
+name|vm_daemon_needed
+argument_list|)
+expr_stmt|;
+name|lastrun
+operator|=
+name|ticks
+expr_stmt|;
+block|}
+block|}
+end_function
+
 begin_function
 specifier|static
 name|void
@@ -3793,6 +3824,15 @@ block|}
 block|}
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !NO_SWAPPING */
+end_comment
 
 end_unit
 
