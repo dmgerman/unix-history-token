d6d8f00be12df784898e14026062b19310f3d7e5
Poul-Henning Kamp <phk@FreeBSD.org>
1996-02-23 15:26:13 +0000

    Big sweep over the IPFIREWALL and IPACCT code.
    Close the ip-fragment hole.
    Waste less memory.
    Rewrite to contemporary more readable style.
    Kill separate IPACCT facility, use "accept" rules in IPFIREWALL.
    Filter incoming >and< outgoing packets.
    Replace "policy" by sticky "deny all" rule.
    Rules have numbers used for ordering and deletion.
    Remove "rerorder" code entirely.
    Count packet & bytecount matches for rules.
    
    
    Former-commit-id: 57d1e70eceda34fa566b1ffcedf044d4d5debc3f

--- sys/netinet/ip_fw.h
+++ sys/netinet/ip_fw.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.h,v 1.13 1995/07/23 05:36:30 davidg Exp $  */
+comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.h,v 1.14 1995/10/01 21:52:50 ugen Exp $  */
 end_comment
 
 begin_comment
@@ -23,12 +23,12 @@ begin_struct
 struct|struct
 name|ip_fw
 block|{
-name|struct
-name|ip_fw
-modifier|*
-name|fw_next
+name|u_long
+name|fw_pcnt
+decl_stmt|,
+name|fw_bcnt
 decl_stmt|;
-comment|/* Next firewall on chain */
+comment|/* Packet and byte counters */
 name|struct
 name|in_addr
 name|fw_src
@@ -43,7 +43,6 @@ decl_stmt|,
 name|fw_dmsk
 decl_stmt|;
 comment|/* Mask for src and dest IP addr */
-comment|/* 	 * This union keeps all "via" information. 	 * If ever fu_via_ip is 0,or IP_FW_F_IFNAME set and 	 * fu_via_name[0] is 0 - match any packet. 	 */
 union|union
 block|{
 name|struct
@@ -72,6 +71,21 @@ struct|;
 block|}
 name|fu_via_un
 union|;
+define|#
+directive|define
+name|fw_via_ip
+value|fu_via_un.fu_via_ip
+define|#
+directive|define
+name|fw_via_name
+value|fu_via_un.fu_via_if.fu_via_name
+define|#
+directive|define
+name|fw_via_unit
+value|fu_via_un.fu_via_if.fu_via_unit
+name|u_short
+name|fw_number
+decl_stmt|;
 name|u_short
 name|fw_flg
 decl_stmt|;
@@ -97,40 +111,40 @@ name|IP_FW_MAX_PORTS
 index|]
 decl_stmt|;
 comment|/* Array of port numbers to match */
-name|u_long
-name|fw_pcnt
+name|u_char
+name|fw_ipopt
 decl_stmt|,
-name|fw_bcnt
+name|fw_ipnopt
 decl_stmt|;
-comment|/* Packet and byte counters */
+comment|/* IP options set/unset */
+name|u_char
+name|fw_tcpf
+decl_stmt|,
+name|fw_tcpnf
+decl_stmt|;
+comment|/* TCP flags sen/unset */
 block|}
 struct|;
 end_struct
 
-begin_comment
-comment|/*  * Definitions to make expressions  * for "via" stuff shorter.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|fw_via_ip
-value|fu_via_un.fu_via_ip
-end_define
-
-begin_define
-define|#
-directive|define
-name|fw_via_name
-value|fu_via_un.fu_via_if.fu_via_name
-end_define
-
-begin_define
-define|#
-directive|define
-name|fw_via_unit
-value|fu_via_un.fu_via_if.fu_via_unit
-end_define
+begin_struct
+struct|struct
+name|ip_fw_chain
+block|{
+name|LIST_ENTRY
+argument_list|(
+argument|ip_fw_chain
+argument_list|)
+name|chain
+expr_stmt|;
+name|struct
+name|ip_fw
+modifier|*
+name|rule
+decl_stmt|;
+block|}
+struct|;
+end_struct
 
 begin_comment
 comment|/*  * Values for "flags" field .  */
@@ -144,7 +158,7 @@ value|0x000
 end_define
 
 begin_comment
-comment|/* This is a universal packet firewall*/
+comment|/* This is a universal packet rule    */
 end_comment
 
 begin_define
@@ -155,7 +169,7 @@ value|0x001
 end_define
 
 begin_comment
-comment|/* This is a TCP packet firewall      */
+comment|/* This is a TCP packet rule          */
 end_comment
 
 begin_define
@@ -166,7 +180,7 @@ value|0x002
 end_define
 
 begin_comment
-comment|/* This is a UDP packet firewall      */
+comment|/* This is a UDP packet rule          */
 end_comment
 
 begin_define
@@ -177,7 +191,7 @@ value|0x003
 end_define
 
 begin_comment
-comment|/* This is a ICMP packet firewall     */
+comment|/* This is a ICMP packet rule         */
 end_comment
 
 begin_define
@@ -188,7 +202,7 @@ value|0x003
 end_define
 
 begin_comment
-comment|/* Mask to isolate firewall kind      */
+comment|/* Mask to isolate rule kind          */
 end_comment
 
 begin_define
@@ -199,69 +213,25 @@ value|0x004
 end_define
 
 begin_comment
-comment|/* This is an accept firewall (as     * 				 *         opposed to a deny firewall)* 				 *                                    */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_SRNG
-value|0x008
-end_define
-
-begin_comment
-comment|/* The first two src ports are a min  * 				 * and max range (stored in host byte * 				 * order).                            * 				 *                                    */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_DRNG
-value|0x010
-end_define
-
-begin_comment
-comment|/* The first two dst ports are a min  * 				 * and max range (stored in host byte * 				 * order).                            * 				 * (ports[0]<= port<= ports[1])     * 				 *                                    */
+comment|/* This is an accept rule	      */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_PRN
-value|0x020
+value|0x008
 end_define
 
 begin_comment
-comment|/* In verbose mode print this firewall*/
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_BIDIR
-value|0x040
-end_define
-
-begin_comment
-comment|/* For accounting-count two way       */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_TCPSYN
-value|0x080
-end_define
-
-begin_comment
-comment|/* For tcp packets-check SYN only     */
+comment|/* Print if this rule matches	      */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_ICMPRPL
-value|0x100
+value|0x010
 end_define
 
 begin_comment
@@ -271,8 +241,30 @@ end_comment
 begin_define
 define|#
 directive|define
+name|IP_FW_F_SRNG
+value|0x020
+end_define
+
+begin_comment
+comment|/* The first two src ports are a min  * 				 * and max range (stored in host byte * 				 * order).                            */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_DRNG
+value|0x040
+end_define
+
+begin_comment
+comment|/* The first two dst ports are a min  * 				 * and max range (stored in host byte * 				 * order).                            */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|IP_FW_F_IFNAME
-value|0x200
+value|0x080
 end_define
 
 begin_comment
@@ -283,12 +275,90 @@ begin_define
 define|#
 directive|define
 name|IP_FW_F_MASK
-value|0x3FF
+value|0x0FF
 end_define
 
 begin_comment
 comment|/* All possible flag bits mask        */
 end_comment
+
+begin_comment
+comment|/*  * Definitions for IP option names.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_IPOPT_LSRR
+value|0x01
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_IPOPT_SSRR
+value|0x02
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_IPOPT_RR
+value|0x04
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_IPOPT_TS
+value|0x08
+end_define
+
+begin_comment
+comment|/*  * Definitions for TCP flags.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_TCPF_FIN
+value|TH_FIN
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_TCPF_SYN
+value|TH_SYN
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_TCPF_RST
+value|TH_RST
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_TCPF_PUSH
+value|TH_PUSH
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_TCPF_ACK
+value|TH_ACK
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_TCPF_URG
+value|TH_URG
+end_define
 
 begin_comment
 comment|/*  * New IP firewall options for [gs]etsockopt at the RAW IP level.  */
@@ -298,70 +368,35 @@ begin_define
 define|#
 directive|define
 name|IP_FW_BASE_CTL
-value|53
+value|50
 end_define
 
 begin_define
 define|#
 directive|define
 name|IP_FW_ADD
-value|(IP_FW_BASE_CTL)
+value|(IP_FW_BASE_CTL+0)
 end_define
 
 begin_define
 define|#
 directive|define
 name|IP_FW_DEL
-value|(IP_FW_BASE_CTL+4)
+value|(IP_FW_BASE_CTL+1)
 end_define
 
 begin_define
 define|#
 directive|define
 name|IP_FW_FLUSH
-value|(IP_FW_BASE_CTL+6)
+value|(IP_FW_BASE_CTL+2)
 end_define
 
 begin_define
 define|#
 directive|define
-name|IP_FW_POLICY
-value|(IP_FW_BASE_CTL+7)
-end_define
-
-begin_define
-define|#
-directive|define
-name|IP_ACCT_ADD
-value|(IP_FW_BASE_CTL+10)
-end_define
-
-begin_define
-define|#
-directive|define
-name|IP_ACCT_DEL
-value|(IP_FW_BASE_CTL+11)
-end_define
-
-begin_define
-define|#
-directive|define
-name|IP_ACCT_FLUSH
-value|(IP_FW_BASE_CTL+12)
-end_define
-
-begin_define
-define|#
-directive|define
-name|IP_ACCT_ZERO
-value|(IP_FW_BASE_CTL+13)
-end_define
-
-begin_define
-define|#
-directive|define
-name|IP_ACCT_CLR
-value|(IP_FW_BASE_CTL+14)
+name|IP_FW_ZERO
+value|(IP_FW_BASE_CTL+3)
 end_define
 
 begin_comment
@@ -385,8 +420,15 @@ end_define
 begin_define
 define|#
 directive|define
+name|IP_FW_P_MBIPO
+value|0x04
+end_define
+
+begin_define
+define|#
+directive|define
 name|IP_FW_P_MASK
-value|0x03
+value|0x07
 end_define
 
 begin_comment
@@ -398,35 +440,6 @@ ifdef|#
 directive|ifdef
 name|KERNEL
 end_ifdef
-
-begin_comment
-comment|/*  * Variables/chain.  */
-end_comment
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|ip_fw
-modifier|*
-name|ip_fw_chain
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|u_short
-name|ip_fw_policy
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|ip_fw
-modifier|*
-name|ip_acct_chain
-decl_stmt|;
-end_decl_stmt
 
 begin_comment
 comment|/*  * Function pointers.  */
@@ -452,9 +465,8 @@ name|struct
 name|ifnet
 modifier|*
 parameter_list|,
-name|struct
-name|ip_fw
-modifier|*
+name|int
+name|dir
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -476,118 +488,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-specifier|extern
-name|void
-function_decl|(
-modifier|*
-name|ip_acct_cnt_ptr
-function_decl|)
-parameter_list|(
-name|struct
-name|ip
-modifier|*
-parameter_list|,
-name|struct
-name|ifnet
-modifier|*
-parameter_list|,
-name|struct
-name|ip_fw
-modifier|*
-parameter_list|,
-name|int
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|int
-function_decl|(
-modifier|*
-name|ip_acct_ctl_ptr
-function_decl|)
-parameter_list|(
-name|int
-parameter_list|,
-name|struct
-name|mbuf
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_comment
 comment|/*  * Function definitions.  */
 end_comment
 
 begin_function_decl
-name|int
-name|ip_fw_chk
-parameter_list|(
-name|struct
-name|mbuf
-modifier|*
-parameter_list|,
-name|struct
-name|ip
-modifier|*
-parameter_list|,
-name|struct
-name|ifnet
-modifier|*
-parameter_list|,
-name|struct
-name|ip_fw
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|ip_fw_ctl
-parameter_list|(
-name|int
-parameter_list|,
-name|struct
-name|mbuf
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|void
-name|ip_acct_cnt
+name|ip_fw_init
 parameter_list|(
-name|struct
-name|ip
-modifier|*
-parameter_list|,
-name|struct
-name|ifnet
-modifier|*
-parameter_list|,
-name|struct
-name|ip_fw
-modifier|*
-parameter_list|,
-name|int
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|ip_acct_ctl
-parameter_list|(
-name|int
-parameter_list|,
-name|struct
-name|mbuf
-modifier|*
+name|void
 parameter_list|)
 function_decl|;
 end_function_decl
