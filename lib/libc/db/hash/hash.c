b464caea015da10b2fb6de34e649e37edb2ec076
Paul Traina <pst@FreeBSD.org>
1996-02-27 19:42:00 +0000

    Fix conflicts and merge into mainline
    Former-commit-id: 227aff28a98b9e4119afac488687273352385644

--- lib/libc/db/hash/hash.c
+++ lib/libc/db/hash/hash.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1990, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_if
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)hash.c	8.7 (Berkeley) 2/21/94"
+literal|"@(#)hash.c	8.9 (Berkeley) 6/16/94"
 decl_stmt|;
 end_decl_stmt
 
@@ -207,7 +207,7 @@ specifier|const
 name|DBT
 operator|*
 operator|,
-name|u_int
+name|u_int32_t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -246,7 +246,7 @@ operator|,
 name|DBT
 operator|*
 operator|,
-name|u_int
+name|u_int32_t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -270,7 +270,7 @@ specifier|const
 name|DBT
 operator|*
 operator|,
-name|u_int
+name|u_int32_t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -313,7 +313,7 @@ operator|,
 name|DBT
 operator|*
 operator|,
-name|u_int
+name|u_int32_t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -330,7 +330,7 @@ specifier|const
 name|DB
 operator|*
 operator|,
-name|u_int
+name|u_int32_t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -492,7 +492,7 @@ name|HASH_STATISTICS
 end_ifdef
 
 begin_decl_stmt
-name|long
+name|int
 name|hash_accesses
 decl_stmt|,
 name|hash_collisions
@@ -1046,7 +1046,7 @@ name|bpages
 operator|*
 sizeof|sizeof
 argument_list|(
-name|u_long
+name|u_int32_t
 operator|*
 argument_list|)
 argument_list|)
@@ -1913,7 +1913,7 @@ expr_stmt|;
 comment|/* First bitmap page is at: splitpoint l2 page offset 1 */
 if|if
 condition|(
-name|__init_bitmap
+name|__ibitmap
 argument_list|(
 name|hashp
 argument_list|,
@@ -2340,7 +2340,7 @@ name|DB
 modifier|*
 name|dbp
 decl_stmt|;
-name|u_int
+name|u_int32_t
 name|flags
 decl_stmt|;
 block|{
@@ -2718,7 +2718,7 @@ name|DBT
 modifier|*
 name|data
 decl_stmt|;
-name|u_int
+name|u_int32_t
 name|flag
 decl_stmt|;
 block|{
@@ -2803,7 +2803,7 @@ name|DBT
 modifier|*
 name|data
 decl_stmt|;
-name|u_int
+name|u_int32_t
 name|flag
 decl_stmt|;
 block|{
@@ -2923,7 +2923,7 @@ name|DBT
 modifier|*
 name|key
 decl_stmt|;
-name|u_int
+name|u_int32_t
 name|flag
 decl_stmt|;
 comment|/* Ignored */
@@ -3061,7 +3061,7 @@ modifier|*
 name|save_bufp
 decl_stmt|;
 specifier|register
-name|u_short
+name|u_int16_t
 modifier|*
 name|bp
 decl_stmt|;
@@ -3080,7 +3080,7 @@ name|char
 modifier|*
 name|kp
 decl_stmt|;
-name|u_short
+name|u_int16_t
 name|pageno
 decl_stmt|;
 ifdef|#
@@ -3159,7 +3159,7 @@ control|(
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|rbufp
@@ -3292,7 +3292,7 @@ comment|/* FOR LOOP INIT */
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|rbufp
@@ -3426,7 +3426,7 @@ comment|/* FOR LOOP INIT */
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|rbufp
@@ -3568,7 +3568,7 @@ case|:
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|rbufp
@@ -3762,7 +3762,7 @@ decl_stmt|;
 end_function
 
 begin_decl_stmt
-name|u_int
+name|u_int32_t
 name|flag
 decl_stmt|;
 end_decl_stmt
@@ -3770,7 +3770,7 @@ end_decl_stmt
 begin_block
 block|{
 specifier|register
-name|u_int
+name|u_int32_t
 name|bucket
 decl_stmt|;
 specifier|register
@@ -3782,7 +3782,7 @@ name|HTAB
 modifier|*
 name|hashp
 decl_stmt|;
-name|u_short
+name|u_int16_t
 modifier|*
 name|bp
 decl_stmt|,
@@ -3954,7 +3954,7 @@ expr_stmt|;
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|bufp
@@ -4005,7 +4005,7 @@ else|else
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|hashp
@@ -4078,7 +4078,7 @@ return|;
 name|bp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 operator|(
@@ -4307,7 +4307,7 @@ modifier|*
 name|hashp
 decl_stmt|;
 block|{
-name|u_int
+name|u_int32_t
 name|old_bucket
 decl_stmt|,
 name|new_bucket
@@ -4644,7 +4644,7 @@ end_function
 
 begin_function
 specifier|extern
-name|u_int
+name|u_int32_t
 name|__call_hash
 parameter_list|(
 name|hashp
--- lib/libc/db/recno/rec_seq.c
+++ lib/libc/db/recno/rec_seq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -15,7 +15,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)rec_seq.c	8.2 (Berkeley) 9/7/93"
+literal|"@(#)rec_seq.c	8.3 (Berkeley) 7/14/94"
 decl_stmt|;
 end_decl_stmt
 
@@ -192,11 +192,14 @@ name|R_NEXT
 case|:
 if|if
 condition|(
-name|ISSET
+name|F_ISSET
 argument_list|(
+operator|&
 name|t
+operator|->
+name|bt_cursor
 argument_list|,
-name|B_SEQINIT
+name|CURS_INIT
 argument_list|)
 condition|)
 block|{
@@ -204,7 +207,9 @@ name|nrec
 operator|=
 name|t
 operator|->
-name|bt_rcursor
+name|bt_cursor
+operator|.
+name|rcursor
 operator|+
 literal|1
 expr_stmt|;
@@ -224,11 +229,14 @@ name|R_PREV
 case|:
 if|if
 condition|(
-name|ISSET
+name|F_ISSET
 argument_list|(
+operator|&
 name|t
+operator|->
+name|bt_cursor
 argument_list|,
-name|B_SEQINIT
+name|CURS_INIT
 argument_list|)
 condition|)
 block|{
@@ -239,7 +247,9 @@ name|nrec
 operator|=
 name|t
 operator|->
-name|bt_rcursor
+name|bt_cursor
+operator|.
+name|rcursor
 operator|-
 literal|1
 operator|)
@@ -260,7 +270,7 @@ case|:
 if|if
 condition|(
 operator|!
-name|ISSET
+name|F_ISSET
 argument_list|(
 name|t
 argument_list|,
@@ -323,7 +333,7 @@ block|{
 if|if
 condition|(
 operator|!
-name|ISSET
+name|F_ISSET
 argument_list|(
 name|t
 argument_list|,
@@ -396,16 +406,21 @@ operator|(
 name|RET_ERROR
 operator|)
 return|;
-name|SET
+name|F_SET
 argument_list|(
+operator|&
 name|t
+operator|->
+name|bt_cursor
 argument_list|,
-name|B_SEQINIT
+name|CURS_INIT
 argument_list|)
 expr_stmt|;
 name|t
 operator|->
-name|bt_rcursor
+name|bt_cursor
+operator|.
+name|rcursor
 operator|=
 name|nrec
 expr_stmt|;
@@ -426,7 +441,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|ISSET
+name|F_ISSET
 argument_list|(
 name|t
 argument_list|,
