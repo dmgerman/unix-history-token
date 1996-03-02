d6952581a97eb7b634064e168e353245481170ff
Peter Wemm <peter@FreeBSD.org>
1996-03-02 18:55:41 +0000

    This file is "obsolete" and no longer used or referenced.
    Former-commit-id: 9ba6a8849932c7637afe2451d1b60fa766e3de2d

--- sys/i386/i386/conf.c
+++
@@ -1,7 +0,0 @@
-begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_comment
-comment|/*  * SURPRISE! this file is now empty  * I'll delete it soon, as soon as most people ar e LIKELY to have  * done a new config...  */
-end_comment
-
-end_unit
-
