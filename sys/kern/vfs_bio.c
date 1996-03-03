66170d07382f076b7f23926294eb8fc68b5b46ef
John Dyson <dyson@FreeBSD.org>
1996-03-03 01:04:28 +0000

    Fix the buffer queue problem differently.  The previous fix could panic with a buffer not on queue panic.
    Former-commit-id: f0f0ced83298a73d4533daf9db93a7e9b1e27b1b

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.85 1996/03/02 03:45:04 dyson Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.86 1996/03/02 04:40:55 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -3549,12 +3549,18 @@ goto|goto
 name|start
 goto|;
 block|}
-comment|/* 			 * Make sure that the buffer is flagged as not being on a 			 * queue. 			 */
+name|TAILQ_INSERT_TAIL
+argument_list|(
+operator|&
+name|bufqueues
+index|[
+name|QUEUE_LRU
+index|]
+argument_list|,
 name|bp
-operator|->
-name|b_qindex
-operator|=
-name|QUEUE_NONE
+argument_list|,
+name|b_freelist
+argument_list|)
 expr_stmt|;
 block|}
 block|}
