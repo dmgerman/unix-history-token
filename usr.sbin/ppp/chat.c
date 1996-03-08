56a4170e208f786cabb917be744281b4a317fc1c
Andrey Chernov <ache@FreeBSD.org>
1996-03-08 12:34:40 +0000

    Log phone as LOG_PHASE, it is valuable enough for multi-phone environment. Add missing return when terminal mode can't be re-established due to modem not opened.
    Former-commit-id: 478601d155ef786f86a6a38dc9d775f6d0479d9c

--- usr.sbin/ppp/chat.c
+++ usr.sbin/ppp/chat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  *  Most of codes are derived from chat.c by Karl Fox (karl@MorningStar.Com).  *  *	Chat -- a program for automatic session establishment (i.e. dial  *		the phone and log in).  *  *	This software is in the public domain.  *  *	Please send all bug reports, requests for information, etc. to:  *  *		Karl Fox<karl@MorningStar.Com>  *		Morning Star Technologies, Inc.  *		1760 Zollinger Road  *		Columbus, OH  43221  *		(614)451-1883  *  * $Id: chat.c,v 1.5 1995/09/02 17:20:50 amurai Exp $  *  *  TODO:  *	o Support more UUCP compatible control sequences.  *	o Dialing shoud not block monitor process.  *	o Reading modem by select should be unified into main.c  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  *  Most of codes are derived from chat.c by Karl Fox (karl@MorningStar.Com).  *  *	Chat -- a program for automatic session establishment (i.e. dial  *		the phone and log in).  *  *	This software is in the public domain.  *  *	Please send all bug reports, requests for information, etc. to:  *  *		Karl Fox<karl@MorningStar.Com>  *		Morning Star Technologies, Inc.  *		1760 Zollinger Road  *		Columbus, OH  43221  *		(614)451-1883  *  * $Id: chat.c,v 1.6 1996/03/08 09:03:03 ache Exp $  *  *  TODO:  *	o Support more UUCP compatible control sequences.  *	o Dialing shoud not block monitor process.  *	o Reading modem by select should be unified into main.c  */
 end_comment
 
 begin_include
@@ -621,6 +621,15 @@ condition|)
 name|fprintf
 argument_list|(
 name|stderr
+argument_list|,
+literal|"Phone: %s\n"
+argument_list|,
+name|phone
+argument_list|)
+expr_stmt|;
+name|LogPrintf
+argument_list|(
+name|LOG_PHASE
 argument_list|,
 literal|"Phone: %s\n"
 argument_list|,
