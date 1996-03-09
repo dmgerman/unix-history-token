d86094a52504c6bf4c48320996044d79dd154108
John Dyson <dyson@FreeBSD.org>
1996-03-09 06:46:51 +0000

    Correct handling of dirty pages in I/O buffers.  The case where pages residing in a buffer that had been dirtied by a process was being handled incorrectly.  The pages were mistakenly placed into the cache queue.  This would likely have the effect of mmaped page modifications being lost when I/O system calls were being used simultaneously to the same locations in a file. Submitted by: davidg
    Former-commit-id: d0ee33a4db1ec0d8ffe2b05614c7d7f31b90300d

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.86 1996/03/02 04:40:55 dyson Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.87 1996/03/03 01:04:28 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -2752,6 +2752,19 @@ operator|->
 name|valid
 condition|)
 block|{
+if|if
+condition|(
+name|m
+operator|->
+name|dirty
+operator|==
+literal|0
+condition|)
+name|vm_page_test_dirty
+argument_list|(
+name|m
+argument_list|)
+expr_stmt|;
 comment|/* 				 * this keeps pressure off of the process memory 				 */
 if|if
 condition|(
@@ -2771,11 +2784,27 @@ operator|.
 name|v_free_min
 operator|)
 condition|)
+block|{
+if|if
+condition|(
+name|m
+operator|->
+name|dirty
+operator|==
+literal|0
+condition|)
 name|vm_page_cache
 argument_list|(
 name|m
 argument_list|)
 expr_stmt|;
+else|else
+name|vm_page_deactivate
+argument_list|(
+name|m
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 elseif|else
 if|if
