2a1c972f8098ccc278c9684687826db48765861d
Bruce Evans <bde@FreeBSD.org>
1996-03-10 00:50:18 +0000

    Brought in fixes for restoring segment registers in prot_to_real() from asm.S (rev 1.7) and table.c (rev 1.9).
    Former-commit-id: 57e49a089d62671f3999d24073530742cfda5d7c

--- sys/i386/boot/biosboot/table.c
+++ sys/i386/boot/biosboot/table.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:36:43  rpd  *	$Id: table.c,v 1.7 1995/05/30 07:58:36 rgrimes Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:36:43  rpd  *	$Id: table.c,v 1.7.4.1 1995/08/23 04:38:47 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -159,13 +159,25 @@ literal|0x0
 block|}
 block|,
 comment|/* 0x28 : boot code, 16 bits */
+block|{
+literal|0xFFFF
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x92
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|/* 0x30 : boot data, 16 bits */
 ifdef|#
 directive|ifdef
 name|BDE_DEBUGGER
 comment|/* More for bdb. */
-block|{}
-block|,
-comment|/* BIOS_CS_INDEX = 6 : null */
 block|{}
 block|,
 comment|/* BIOS_TMP_INDEX = 7 : null */
