dde4c757f2a8d7f04114b3a66f5d125049d72bf3
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-03-10 07:04:27 +0000

    aha1742.c: 	Cleanse the SCSI subsystem of its internally defined types 		u_int32, u_int16, u_int8, int32, int16, int8. 		Use the system defined *_t types instead.
    eisaconf.c:
    	Cosmetic formatting chagnes.
    
    
    Former-commit-id: b6e0ab249412d5093dab1689654f4f312a389b15

--- sys/i386/eisa/aha1742.c
+++ sys/i386/eisa/aha1742.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.50 1996/01/31 18:02:16 gibbs Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.51 1996/02/26 01:01:38 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -1156,7 +1156,7 @@ end_struct
 
 begin_decl_stmt
 specifier|static
-name|u_int32
+name|u_int32_t
 name|ahb_adapter_info
 name|__P
 argument_list|(
@@ -1481,7 +1481,7 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|static
-name|int32
+name|int32_t
 name|ahb_scsi_cmd
 name|__P
 argument_list|(
@@ -3186,7 +3186,7 @@ end_comment
 
 begin_function
 specifier|static
-name|u_int32
+name|u_int32_t
 name|ahb_adapter_info
 parameter_list|(
 name|unit
@@ -4482,7 +4482,7 @@ end_comment
 
 begin_function
 specifier|static
-name|int32
+name|int32_t
 name|ahb_scsi_cmd
 parameter_list|(
 name|xs
