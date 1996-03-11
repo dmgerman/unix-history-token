33df2c645db16a0bfe6e312c6e8fef73c8b76d0b
Jeffrey Hsu <hsu@FreeBSD.org>
1996-03-11 02:21:04 +0000

    Merge in Lite2: add function prototypes 		remove bogus function prototype for issig()---no such function 		fix comment Lite2 changed type of ps_code from int to long.  We change it to u_long   to make it consistent w/ its usage in kern_sig.c. Did not accept type change to ps_addr field.  Delete it instead as it is   not used anywhere. Reviewed by:	davidg & bde
    Former-commit-id: 225a1a55e95d7c8c812b9016866ce11449a3cc84

--- sys/sys/signalvar.h
+++ sys/sys/signalvar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)signalvar.h	8.3 (Berkeley) 1/4/94  * $Id: signalvar.h,v 1.9 1996/01/31 12:44:31 davidg Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)signalvar.h	8.6 (Berkeley) 2/19/95  * $Id: signalvar.h,v 1.10 1996/03/02 19:38:16 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -70,12 +70,8 @@ name|int
 name|ps_sig
 decl_stmt|;
 comment|/* for core dump/debugger XXX */
-name|int
+name|u_long
 name|ps_code
-decl_stmt|;
-comment|/* for core dump/debugger XXX */
-name|int
-name|ps_addr
 decl_stmt|;
 comment|/* for core dump/debugger XXX */
 name|sigset_t
@@ -155,7 +151,7 @@ value|(p->p_sigacts->ps_sigact[(sig)])
 end_define
 
 begin_comment
-comment|/*  * Determine signal that should be delivered to process p, the current  * process, 0 if none.  If there is a pending stop signal with default  * action, the process stops in issig().  */
+comment|/*  * Determine signal that should be delivered to process p, the current  * process, 0 if none.  If there is a pending stop signal with default  * action, the process stops in issignal().  */
 end_comment
 
 begin_define
@@ -166,7 +162,7 @@ parameter_list|(
 name|p
 parameter_list|)
 define|\
-value|(((p)->p_siglist == 0 ||					\ 	    (((p)->p_flag& P_TRACED) == 0&&				\ 	    ((p)->p_siglist& ~(p)->p_sigmask) == 0)) ?			\ 	    0 : issignal(p))
+value|(((p)->p_siglist == 0 ||					\ 	    (((p)->p_flag& P_TRACED) == 0&&				\ 	     ((p)->p_siglist& ~(p)->p_sigmask) == 0)) ?		\ 	    0 : issignal(p))
 end_define
 
 begin_comment
@@ -482,21 +478,6 @@ end_decl_stmt
 
 begin_decl_stmt
 name|int
-name|issig
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|proc
-operator|*
-name|p
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
 name|issignal
 name|__P
 argument_list|(
@@ -519,9 +500,11 @@ operator|(
 expr|struct
 name|proc
 operator|*
+name|p
 operator|,
 name|char
 operator|*
+name|why
 operator|)
 argument_list|)
 decl_stmt|;
@@ -588,12 +571,15 @@ operator|(
 expr|struct
 name|proc
 operator|*
+name|p
 operator|,
 name|int
+name|signum
 operator|,
 expr|struct
 name|sigaction
 operator|*
+name|sa
 operator|)
 argument_list|)
 decl_stmt|;
@@ -608,8 +594,10 @@ operator|(
 expr|struct
 name|proc
 operator|*
+name|p
 operator|,
 name|int
+name|signum
 operator|)
 argument_list|)
 decl_stmt|;
@@ -644,7 +632,7 @@ operator|,
 name|int
 name|sig
 operator|,
-name|unsigned
+name|u_long
 name|code
 operator|)
 argument_list|)
@@ -670,7 +658,7 @@ operator|,
 name|int
 name|returnmask
 operator|,
-name|unsigned
+name|u_long
 name|code
 operator|)
 argument_list|)
