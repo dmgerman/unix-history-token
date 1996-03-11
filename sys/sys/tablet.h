c24d948d3bbaa360d743767282534150de536d91
Jeffrey Hsu <hsu@FreeBSD.org>
1996-03-11 02:15:32 +0000

    Merge in Lite2: use fixed-width types. Reviewed by:	davidg & bde
    Former-commit-id: e1f7a90f584dfac4a8979f9468838b71a7be467a

--- sys/sys/tablet.h
+++ sys/sys/tablet.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1985, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tablet.h	8.3 (Berkeley) 1/4/94  * $Id$  */
+comment|/*-  * Copyright (c) 1985, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tablet.h	8.4 (Berkeley) 7/10/94  * $Id: tablet.h,v 1.3 1996/02/24 11:21:19 hsu Exp $  */
 end_comment
 
 begin_ifndef
@@ -15,75 +15,72 @@ directive|define
 name|_SYS_TABLET_H_
 end_define
 
-begin_comment
-comment|/*  * Tablet line discipline.  */
-end_comment
-
-begin_include
+begin_expr_stmt
+operator|*
+name|Tablet
+name|line
+name|discipline
+operator|.
+modifier|*
+expr|/
 include|#
 directive|include
 file|<sys/ioctl.h>
-end_include
-
-begin_comment
 comment|/*  * Reads on the tablet return one of the following structures, depending on  * the underlying tablet type.  The first two are defined such that a read of  * sizeof (gtcopos) on a non-gtco tablet will return meaningful info.  The  * in-proximity bit is simulated where the tablet does not directly provide  * the information.  */
-end_comment
-
-begin_struct
-struct|struct
+expr|struct
 name|tbpos
 block|{
-name|int
+name|int32_t
 name|xpos
-decl_stmt|,
+block|,
 name|ypos
-decl_stmt|;
+block|;
 comment|/* raw x-y coordinates */
-name|short
+name|int16_t
 name|status
-decl_stmt|;
+block|;
 comment|/* buttons/pen down */
 define|#
 directive|define
 name|TBINPROX
 value|0100000
 comment|/* pen in proximity of tablet */
-name|short
+name|int16_t
 name|scount
-decl_stmt|;
+block|;
 comment|/* sample count */
 block|}
-struct|;
-end_struct
+expr_stmt|;
+end_expr_stmt
 
 begin_struct
 struct|struct
 name|gtcopos
 block|{
-name|int
+name|int32_t
 name|xpos
 decl_stmt|,
 name|ypos
 decl_stmt|;
 comment|/* raw x-y coordinates */
-name|short
+name|int16_t
 name|status
 decl_stmt|;
 comment|/* as above */
-name|short
+name|int16_t
 name|scount
 decl_stmt|;
 comment|/* sample count */
-name|short
+name|int16_t
 name|xtilt
 decl_stmt|,
 name|ytilt
 decl_stmt|;
 comment|/* raw tilt */
-name|short
+name|int16_t
 name|pressure
 decl_stmt|;
-name|short
+name|int16_t
 name|pad
 decl_stmt|;
 comment|/* pad to longword boundary */
@@ -95,7 +92,7 @@ begin_struct
 struct|struct
 name|polpos
 block|{
-name|short
+name|int16_t
 name|p_x
 decl_stmt|,
 name|p_y
@@ -103,7 +100,7 @@ decl_stmt|,
 name|p_z
 decl_stmt|;
 comment|/* raw 3-space coordinates */
-name|short
+name|int16_t
 name|p_azi
 decl_stmt|,
 name|p_pit
@@ -111,7 +108,7 @@ decl_stmt|,
 name|p_rol
 decl_stmt|;
 comment|/* azimuth, pitch, and roll */
-name|short
+name|int16_t
 name|p_stat
 decl_stmt|;
 comment|/* status, as above */
