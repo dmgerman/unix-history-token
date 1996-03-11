d5e2b732f0f83b3f82b97377802815bfc0701432
Jeffrey Hsu <hsu@FreeBSD.org>
1996-03-11 02:15:04 +0000

    Merge in Lite2: use fixed-width types 		add POSIX compatibility 		add union fs defines Reviewed by:	davidg & bde
    Former-commit-id: e3124839d8cccee4602cb43b7607d140b544c561

--- sys/sys/stat.h
+++ sys/sys/stat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)stat.h	8.6 (Berkeley) 3/8/94  * $Id: stat.h,v 1.5 1994/10/02 17:24:57 phk Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)stat.h	8.12 (Berkeley) 6/16/95  * $Id: stat.h,v 1.7 1996/02/24 11:19:43 hsu Exp $  */
 end_comment
 
 begin_ifndef
@@ -35,8 +35,7 @@ begin_struct
 struct|struct
 name|ostat
 block|{
-name|unsigned
-name|short
+name|u_int16_t
 name|st_dev
 decl_stmt|;
 comment|/* inode's device */
@@ -52,22 +51,19 @@ name|nlink_t
 name|st_nlink
 decl_stmt|;
 comment|/* number of hard links */
-name|unsigned
-name|short
+name|u_int16_t
 name|st_uid
 decl_stmt|;
 comment|/* user ID of the file's owner */
-name|unsigned
-name|short
+name|u_int16_t
 name|st_gid
 decl_stmt|;
 comment|/* group ID of the file's group */
-name|unsigned
-name|short
+name|u_int16_t
 name|st_rdev
 decl_stmt|;
 comment|/* device type */
-name|long
+name|int32_t
 name|st_size
 decl_stmt|;
 comment|/* file size, in bytes */
@@ -86,21 +82,19 @@ name|timespec
 name|st_ctimespec
 decl_stmt|;
 comment|/* time of last file status change */
-name|long
+name|int32_t
 name|st_blksize
 decl_stmt|;
 comment|/* optimal blocksize for I/O */
-name|long
+name|int32_t
 name|st_blocks
 decl_stmt|;
 comment|/* blocks allocated for file */
-name|unsigned
-name|long
+name|u_int32_t
 name|st_flags
 decl_stmt|;
 comment|/* user defined flags for file */
-name|unsigned
-name|long
+name|u_int32_t
 name|st_gen
 decl_stmt|;
 comment|/* file generation number */
@@ -149,6 +143,9 @@ name|dev_t
 name|st_rdev
 decl_stmt|;
 comment|/* device type */
+ifndef|#
+directive|ifndef
+name|_POSIX_SOURCE
 name|struct
 name|timespec
 name|st_atimespec
@@ -164,33 +161,58 @@ name|timespec
 name|st_ctimespec
 decl_stmt|;
 comment|/* time of last file status change */
+else|#
+directive|else
+name|time_t
+name|st_atime
+decl_stmt|;
+comment|/* time of last access */
+name|long
+name|st_atimensec
+decl_stmt|;
+comment|/* nsec of last access */
+name|time_t
+name|st_mtime
+decl_stmt|;
+comment|/* time of last data modification */
+name|long
+name|st_mtimensec
+decl_stmt|;
+comment|/* nsec of last data modification */
+name|time_t
+name|st_ctime
+decl_stmt|;
+comment|/* time of last file status change */
+name|long
+name|st_ctimensec
+decl_stmt|;
+comment|/* nsec of last file status change */
+endif|#
+directive|endif
 name|off_t
 name|st_size
 decl_stmt|;
 comment|/* file size, in bytes */
-name|quad_t
+name|int64_t
 name|st_blocks
 decl_stmt|;
 comment|/* blocks allocated for file */
-name|unsigned
-name|long
+name|u_int32_t
 name|st_blksize
 decl_stmt|;
 comment|/* optimal blocksize for I/O */
-name|unsigned
-name|long
+name|u_int32_t
 name|st_flags
 decl_stmt|;
 comment|/* user defined flags for file */
-name|unsigned
-name|long
+name|u_int32_t
 name|st_gen
 decl_stmt|;
 comment|/* file generation number */
-name|long
+name|int32_t
 name|st_lspare
 decl_stmt|;
-name|quad_t
+name|int64_t
 name|st_qspare
 index|[
 literal|2
@@ -199,6 +221,12 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_POSIX_SOURCE
+end_ifndef
 
 begin_define
 define|#
@@ -220,6 +248,11 @@ directive|define
 name|st_ctime
 value|st_ctimespec.ts_sec
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
@@ -526,6 +559,17 @@ end_comment
 begin_define
 define|#
 directive|define
+name|S_IFWHT
+value|0160000
+end_define
+
+begin_comment
+comment|/* whiteout */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|S_ISVTX
 value|0001000
 end_define
@@ -643,6 +687,20 @@ begin_comment
 comment|/* fifo or socket */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|S_ISWHT
+parameter_list|(
+name|m
+parameter_list|)
+value|(((m)& 0170000) == 0160000)
+end_define
+
+begin_comment
+comment|/* whiteout */
+end_comment
+
 begin_endif
 endif|#
 directive|endif
@@ -746,6 +804,17 @@ begin_comment
 comment|/* writes to file may only append */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|UF_OPAQUE
+value|0x00000008
+end_define
+
+begin_comment
+comment|/* directory is opaque wrt. union */
+end_comment
+
 begin_comment
 comment|/*  * Super-user changeable flags.  */
 end_comment
@@ -803,6 +872,13 @@ end_ifdef
 begin_comment
 comment|/*  * Shorthand abbreviations of above.  */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|OPAQUE
+value|(UF_OPAQUE)
+end_define
 
 begin_define
 define|#
