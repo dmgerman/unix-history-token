c6f3d4fb0c927f2ceef948a3ca4f27aecc9efd53
Jeffrey Hsu <hsu@FreeBSD.org>
1996-03-11 02:22:40 +0000

    Merge in Lite2: use machine-independent types. Reviewed by:	davidg & bde
    Former-commit-id: 46a84a2f2020c815be99c9a73a25977b40e22ec3

--- sys/sys/vmmeter.h
+++ sys/sys/vmmeter.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vmmeter.h	8.1 (Berkeley) 6/2/93  * $Id: vmmeter.h,v 1.11 1995/07/29 10:47:05 bde Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vmmeter.h	8.2 (Berkeley) 7/10/94  * $Id: vmmeter.h,v 1.13 1996/02/25 08:50:38 hsu Exp $  */
 end_comment
 
 begin_ifndef
@@ -24,157 +24,157 @@ struct|struct
 name|vmmeter
 block|{
 comment|/* 	 * General system activity. 	 */
-name|unsigned
+name|u_int
 name|v_swtch
 decl_stmt|;
 comment|/* context switches */
-name|unsigned
+name|u_int
 name|v_trap
 decl_stmt|;
 comment|/* calls to trap */
-name|unsigned
+name|u_int
 name|v_syscall
 decl_stmt|;
 comment|/* calls to syscall() */
-name|unsigned
+name|u_int
 name|v_intr
 decl_stmt|;
 comment|/* device interrupts */
-name|unsigned
+name|u_int
 name|v_soft
 decl_stmt|;
 comment|/* software interrupts */
 comment|/* 	 * Virtual memory activity. 	 */
-name|unsigned
+name|u_int
 name|v_vm_faults
 decl_stmt|;
 comment|/* number of address memory faults */
-name|unsigned
+name|u_int
 name|v_cow_faults
 decl_stmt|;
 comment|/* number of copy-on-writes */
-name|unsigned
+name|u_int
 name|v_zfod
 decl_stmt|;
 comment|/* pages zero filled on demand */
-name|unsigned
+name|u_int
 name|v_swapin
 decl_stmt|;
 comment|/* swap pager pageins */
-name|unsigned
+name|u_int
 name|v_swapout
 decl_stmt|;
 comment|/* swap pager pageouts */
-name|unsigned
+name|u_int
 name|v_swappgsin
 decl_stmt|;
 comment|/* swap pager pages paged in */
-name|unsigned
+name|u_int
 name|v_swappgsout
 decl_stmt|;
 comment|/* swap pager pages paged out */
-name|unsigned
+name|u_int
 name|v_vnodein
 decl_stmt|;
 comment|/* vnode pager pageins */
-name|unsigned
+name|u_int
 name|v_vnodeout
 decl_stmt|;
 comment|/* vnode pager pageouts */
-name|unsigned
+name|u_int
 name|v_vnodepgsin
 decl_stmt|;
 comment|/* vnode_pager pages paged in */
-name|unsigned
+name|u_int
 name|v_vnodepgsout
 decl_stmt|;
 comment|/* vnode pager pages paged out */
-name|unsigned
+name|u_int
 name|v_intrans
 decl_stmt|;
 comment|/* intransit blocking page faults */
-name|unsigned
+name|u_int
 name|v_reactivated
 decl_stmt|;
 comment|/* number of pages reactivated from free list */
-name|unsigned
+name|u_int
 name|v_pdwakeups
 decl_stmt|;
 comment|/* number of times daemon has awaken from sleep */
-name|unsigned
+name|u_int
 name|v_pdpages
 decl_stmt|;
 comment|/* number of pages analyzed by daemon */
-name|unsigned
+name|u_int
 name|v_dfree
 decl_stmt|;
 comment|/* pages freed by daemon */
-name|unsigned
+name|u_int
 name|v_pfree
 decl_stmt|;
 comment|/* pages freed by exiting processes */
-name|unsigned
+name|u_int
 name|v_tfree
 decl_stmt|;
 comment|/* total pages freed */
 comment|/* 	 * Distribution of page usages. 	 */
-name|unsigned
+name|u_int
 name|v_page_size
 decl_stmt|;
 comment|/* page size in bytes */
-name|unsigned
+name|u_int
 name|v_page_count
 decl_stmt|;
 comment|/* total number of pages in system */
-name|unsigned
+name|u_int
 name|v_free_reserved
 decl_stmt|;
 comment|/* number of pages reserved for deadlock */
-name|unsigned
+name|u_int
 name|v_free_target
 decl_stmt|;
 comment|/* number of pages desired free */
-name|unsigned
+name|u_int
 name|v_free_min
 decl_stmt|;
 comment|/* minimum number of pages desired free */
-name|unsigned
+name|u_int
 name|v_free_count
 decl_stmt|;
 comment|/* number of pages free */
-name|unsigned
+name|u_int
 name|v_wire_count
 decl_stmt|;
 comment|/* number of pages wired down */
-name|unsigned
+name|u_int
 name|v_active_count
 decl_stmt|;
 comment|/* number of pages active */
-name|unsigned
+name|u_int
 name|v_inactive_target
 decl_stmt|;
 comment|/* number of pages desired inactive */
-name|unsigned
+name|u_int
 name|v_inactive_count
 decl_stmt|;
 comment|/* number of pages inactive */
-name|unsigned
+name|u_int
 name|v_cache_count
 decl_stmt|;
 comment|/* number of pages on buffer cache queue */
-name|unsigned
+name|u_int
 name|v_cache_min
 decl_stmt|;
 comment|/* min number of pages desired on cache queue */
-name|unsigned
+name|u_int
 name|v_cache_max
 decl_stmt|;
 comment|/* max number of pages in cached obj */
-name|unsigned
+name|u_int
 name|v_pageout_free_min
 decl_stmt|;
 comment|/* min number pages reserved for kernel */
-name|unsigned
+name|u_int
 name|v_interrupt_free_min
 decl_stmt|;
 comment|/* reserved number of pages for int code */
@@ -209,59 +209,59 @@ begin_struct
 struct|struct
 name|vmtotal
 block|{
-name|short
+name|int16_t
 name|t_rq
 decl_stmt|;
 comment|/* length of the run queue */
-name|short
+name|int16_t
 name|t_dw
 decl_stmt|;
 comment|/* jobs in ``disk wait'' (neg priority) */
-name|short
+name|int16_t
 name|t_pw
 decl_stmt|;
 comment|/* jobs in page wait */
-name|short
+name|int16_t
 name|t_sl
 decl_stmt|;
 comment|/* jobs sleeping in core */
-name|short
+name|int16_t
 name|t_sw
 decl_stmt|;
 comment|/* swapped out runnable/short block jobs */
-name|long
+name|int32_t
 name|t_vm
 decl_stmt|;
 comment|/* total virtual memory */
-name|long
+name|int32_t
 name|t_avm
 decl_stmt|;
 comment|/* active virtual memory */
-name|long
+name|int32_t
 name|t_rm
 decl_stmt|;
 comment|/* total real memory in use */
-name|long
+name|int32_t
 name|t_arm
 decl_stmt|;
 comment|/* active real memory */
-name|long
+name|int32_t
 name|t_vmshr
 decl_stmt|;
 comment|/* shared virtual memory */
-name|long
+name|int32_t
 name|t_avmshr
 decl_stmt|;
 comment|/* active shared virtual memory */
-name|long
+name|int32_t
 name|t_rmshr
 decl_stmt|;
 comment|/* shared real memory */
-name|long
+name|int32_t
 name|t_armshr
 decl_stmt|;
 comment|/* active shared real memory */
-name|long
+name|int32_t
 name|t_free
 decl_stmt|;
 comment|/* free memory pages */
@@ -354,8 +354,7 @@ comment|/* data and stack size distribution counters */
 end_comment
 
 begin_decl_stmt
-name|unsigned
-name|int
+name|u_int
 name|dmon
 index|[
 name|NDMON
@@ -366,8 +365,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|unsigned
-name|int
+name|u_int
 name|smon
 index|[
 name|NSMON
@@ -382,8 +380,7 @@ comment|/* page in time distribution counters */
 end_comment
 
 begin_decl_stmt
-name|unsigned
-name|int
+name|u_int
 name|pmon
 index|[
 name|NPMON
@@ -398,8 +395,7 @@ comment|/* reclaim time distribution counters */
 end_comment
 
 begin_decl_stmt
-name|unsigned
-name|int
+name|u_int
 name|rmon
 index|[
 name|NRMON
@@ -434,7 +430,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|unsigned
+name|u_int
 name|rectime
 decl_stmt|;
 end_decl_stmt
@@ -444,7 +440,7 @@ comment|/* accumulator for reclaim times */
 end_comment
 
 begin_decl_stmt
-name|unsigned
+name|u_int
 name|pgintime
 decl_stmt|;
 end_decl_stmt
