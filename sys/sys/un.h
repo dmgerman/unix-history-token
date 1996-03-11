3f744914f02d9b0d032d7ed8d8dcfc560b18253d
Jeffrey Hsu <hsu@FreeBSD.org>
1996-03-11 02:12:57 +0000

    Merge in Lite2: add function prototypes. Did not accept prototypes for unp_attach(), unp_bind(), unp_connect(),   unp_detach(), unp_discard(), unp_disconnect(), unp_drop(),   np_gc(), unp_mark(), unp_scan(), and unp_shutdown(). They are used only in uipc_usrreq.c and declared static there. Reviewed by:	davidg & bde
    Former-commit-id: 5ae678e1853e8b7f871ea6f7726bee641f58bb50

--- sys/sys/un.h
+++ sys/sys/un.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)un.h	8.1 (Berkeley) 6/2/93  * $Id: un.h,v 1.7 1995/11/21 12:55:15 bde Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)un.h	8.3 (Berkeley) 2/19/95  * $Id: un.h,v 1.11 1996/03/10 10:36:30 hsu Exp $  */
 end_comment
 
 begin_ifndef
@@ -69,6 +69,12 @@ directive|ifdef
 name|KERNEL
 end_ifdef
 
+begin_struct_decl
+struct_decl|struct
+name|unpcb
+struct_decl|;
+end_struct_decl
+
 begin_decl_stmt
 name|int
 name|uipc_usrreq
@@ -78,20 +84,25 @@ operator|(
 expr|struct
 name|socket
 operator|*
+name|so
 operator|,
 name|int
+name|req
 operator|,
 expr|struct
 name|mbuf
 operator|*
+name|m
 operator|,
 expr|struct
 name|mbuf
 operator|*
+name|nam
 operator|,
 expr|struct
 name|mbuf
 operator|*
+name|control
 operator|)
 argument_list|)
 decl_stmt|;
@@ -106,10 +117,12 @@ operator|(
 expr|struct
 name|socket
 operator|*
+name|so
 operator|,
 expr|struct
 name|socket
 operator|*
+name|so2
 operator|)
 argument_list|)
 decl_stmt|;
@@ -124,6 +137,7 @@ operator|(
 expr|struct
 name|mbuf
 operator|*
+name|m
 operator|)
 argument_list|)
 decl_stmt|;
@@ -138,6 +152,7 @@ operator|(
 expr|struct
 name|mbuf
 operator|*
+name|rights
 operator|)
 argument_list|)
 decl_stmt|;
@@ -149,7 +164,7 @@ directive|else
 end_else
 
 begin_comment
-comment|/* KERNEL */
+comment|/* !KERNEL */
 end_comment
 
 begin_comment
@@ -172,10 +187,18 @@ endif|#
 directive|endif
 end_endif
 
+begin_comment
+comment|/* KERNEL */
+end_comment
+
 begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* !_SYS_UN_H_ */
+end_comment
 
 end_unit
 
