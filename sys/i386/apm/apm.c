1ba861535436770e6b9bf6ef2b71962852313e18
Nate Williams <nate@FreeBSD.org>
1996-03-12 06:09:34 +0000

    Always initialize master_softc.  This avoids panics in the idle loop due to unitialized data if the APM support is compiled in and the probe fails.
    Former-commit-id: ff5f8618dffd597a78e1c2b9d6f5712db84ff56d

--- sys/i386/apm/apm.c
+++ sys/i386/apm/apm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.12.4.1 1995/09/14 07:08:57 davidg Exp $  */
+comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.12.4.2 1996/03/05 05:50:51 nate Exp $  */
 end_comment
 
 begin_include
@@ -2319,17 +2319,15 @@ name|dvp
 operator|->
 name|id_unit
 decl_stmt|;
-name|struct
-name|apm_softc
-modifier|*
-name|sc
-init|=
+comment|/* 	 * XXX - This is necessary here so that we don't panic in the idle 	 * loop because master_softc is unitialized. 	 */
+name|master_softc
+operator|=
 operator|&
 name|apm_softc
 index|[
 name|unit
 index|]
-decl_stmt|;
+expr_stmt|;
 switch|switch
 condition|(
 name|apm_version
@@ -2584,11 +2582,6 @@ index|[
 name|unit
 index|]
 decl_stmt|;
-name|master_softc
-operator|=
-name|sc
-expr_stmt|;
-comment|/* XXX */
 name|sc
 operator|->
 name|initialized
