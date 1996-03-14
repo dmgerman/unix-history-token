cb19262c21906a8617cc5ba92bc5fd613c1cea9e
Bill Fenner <fenner@FreeBSD.org>
1996-03-14 16:59:20 +0000

    IGMPv2 routines rewritten, to be more compact and to fully comply with the IGMPv2 Internet Draft (including Router Alert IP option)
    Former-commit-id: 1d20be95f28a935d9ded871b44004b535dbd7c08

--- sys/netinet/igmp.h
+++ sys/netinet/igmp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)igmp.h	8.1 (Berkeley) 6/10/93  *	$Id: igmp.h,v 1.4 1994/09/06 22:42:17 wollman Exp $  */
+comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)igmp.h	8.1 (Berkeley) 6/10/93  *	$Id: igmp.h,v 1.6 1995/06/13 17:51:06 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -16,7 +16,7 @@ name|_NETINET_IGMP_H_
 end_define
 
 begin_comment
-comment|/*  * Internet Group Management Protocol (IGMP) definitions.  *  * Written by Steve Deering, Stanford, May 1988.  *  * MULTICAST Revision: 3.3.1.2  */
+comment|/*  * Internet Group Management Protocol (IGMP) definitions.  *  * Written by Steve Deering, Stanford, May 1988.  *  * MULTICAST Revision: 3.5.1.2  */
 end_comment
 
 begin_comment
@@ -66,23 +66,45 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IGMP_HOST_MEMBERSHIP_QUERY
+name|IGMP_MEMBERSHIP_QUERY
 value|0x11
 end_define
 
 begin_comment
-comment|/* Host membership query    */
+comment|/* membership query         */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|IGMP_HOST_MEMBERSHIP_REPORT
+name|IGMP_V1_MEMBERSHIP_REPORT
 value|0x12
 end_define
 
 begin_comment
-comment|/* Old membership report    */
+comment|/* Ver. 1 membership report */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IGMP_V2_MEMBERSHIP_REPORT
+value|0x16
+end_define
+
+begin_comment
+comment|/* Ver. 2 membership report */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IGMP_V2_LEAVE_GROUP
+value|0x17
+end_define
+
+begin_comment
+comment|/* Leave-group message	    */
 end_comment
 
 begin_define
@@ -110,34 +132,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IGMP_HOST_NEW_MEMBERSHIP_REPORT
-value|0x16
-end_define
-
-begin_comment
-comment|/* New membership report    */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IGMP_HOST_LEAVE_MESSAGE
-value|0x17
-end_define
-
-begin_comment
-comment|/* Leave-group message	    */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IGMP_MTRACE_RESP
 value|0x1e
 end_define
 
 begin_comment
-comment|/* traceroute resp. (to sender) */
+comment|/* traceroute resp.(to sender)*/
 end_comment
 
 begin_define
@@ -148,7 +148,7 @@ value|0x1f
 end_define
 
 begin_comment
-comment|/* mcast traceroute messages    */
+comment|/* mcast traceroute messages  */
 end_comment
 
 begin_define
@@ -159,11 +159,15 @@ value|10
 end_define
 
 begin_comment
-comment|/* max delay for response to    */
+comment|/* max delay for response to     */
 end_comment
 
 begin_comment
-comment|/* query (in seconds)	    */
+comment|/*  query (in seconds) according */
+end_comment
+
+begin_comment
+comment|/*  to RFC1112                   */
 end_comment
 
 begin_define
@@ -174,79 +178,43 @@ value|10
 end_define
 
 begin_comment
-comment|/* denotes that the igmp->timer filed */
+comment|/* denotes that the igmp code field */
 end_comment
 
 begin_comment
-comment|/*specifies time in tenths of seconds */
+comment|/* specifies time in 10th of seconds*/
 end_comment
 
 begin_comment
-comment|/*  * States for the IGMPv2 state table  */
+comment|/*  * The following four defininitions are for backwards compatibility.  * They should be removed as soon as all applications are updated to  * use the new constant names.  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|IGMP_DELAYING_MEMBER
-value|1
+name|IGMP_HOST_MEMBERSHIP_QUERY
+value|IGMP_MEMBERSHIP_QUERY
 end_define
 
 begin_define
 define|#
 directive|define
-name|IGMP_IDLE_MEMBER
-value|2
+name|IGMP_HOST_MEMBERSHIP_REPORT
+value|IGMP_V1_MEMBERSHIP_REPORT
 end_define
 
 begin_define
 define|#
 directive|define
-name|IGMP_LAZY_MEMBER
-value|3
+name|IGMP_HOST_NEW_MEMBERSHIP_REPORT
+value|IGMP_V2_MEMBERSHIP_REPORT
 end_define
 
 begin_define
 define|#
 directive|define
-name|IGMP_SLEEPING_MEMBER
-value|4
-end_define
-
-begin_define
-define|#
-directive|define
-name|IGMP_AWAKENING_MEMBER
-value|5
-end_define
-
-begin_comment
-comment|/*  * We must remember whether the querier is an old or a new router.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IGMP_OLD_ROUTER
-value|0
-end_define
-
-begin_define
-define|#
-directive|define
-name|IGMP_NEW_ROUTER
-value|1
-end_define
-
-begin_comment
-comment|/*  * Revert to new router if we haven't heard from an old router in  * this amount of time.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IGMP_AGE_THRESHOLD
-value|540
+name|IGMP_HOST_LEAVE_MESSAGE
+value|IGMP_V2_LEAVE_GROUP
 end_define
 
 begin_endif
--- sys/netinet/igmp_var.h
+++ sys/netinet/igmp_var.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)igmp_var.h	8.1 (Berkeley) 7/19/93  * $Id: igmp_var.h,v 1.7 1995/11/14 20:33:54 phk Exp $  */
+comment|/*  * Copyright (c) 1988 Stephen Deering.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Stephen Deering of Stanford University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)igmp_var.h	8.1 (Berkeley) 7/19/93  * $Id: igmp_var.h,v 1.8 1996/01/30 22:58:22 mpp Exp $  */
 end_comment
 
 begin_ifndef
@@ -16,7 +16,7 @@ name|_NETINET_IGMP_VAR_H_
 end_define
 
 begin_comment
-comment|/*  * Internet Group Management Protocol (IGMP),  * implementation-specific definitions.  *  * Written by Steve Deering, Stanford, May 1988.  *  * MULTICAST Revision: 3.3.1.1  */
+comment|/*  * Internet Group Management Protocol (IGMP),  * implementation-specific definitions.  *  * Written by Steve Deering, Stanford, May 1988.  *  * MULTICAST Revision: 3.5.1.3  */
 end_comment
 
 begin_struct
@@ -85,6 +85,53 @@ parameter_list|(
 name|X
 parameter_list|)
 value|(random() % (X) + 1)
+end_define
+
+begin_comment
+comment|/*  * States for IGMPv2's leave processing  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IGMP_OTHERMEMBER
+value|0
+end_define
+
+begin_define
+define|#
+directive|define
+name|IGMP_IREPORTEDLAST
+value|1
+end_define
+
+begin_comment
+comment|/*  * We must remember what version the subnet's querier is.  * We conveniently use the IGMP message type for the proper  * membership report to keep this state.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IGMP_V1_ROUTER
+value|IGMP_V1_MEMBERSHIP_REPORT
+end_define
+
+begin_define
+define|#
+directive|define
+name|IGMP_V2_ROUTER
+value|IGMP_V2_MEMBERSHIP_REPORT
+end_define
+
+begin_comment
+comment|/*  * Revert to new router if we haven't heard from an old router in  * this amount of time.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IGMP_AGE_THRESHOLD
+value|540
 end_define
 
 begin_decl_stmt
