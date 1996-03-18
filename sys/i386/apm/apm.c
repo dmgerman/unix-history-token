257c63e13650fb4bf37933aa7c43c51e85999399
Nate Williams <nate@FreeBSD.org>
1996-03-18 23:30:14 +0000

    Minor bugfixes from the recent PC-CARD release.
    Submitted by:	hosokawa@mt.cs.keio.ac.jp and the rest of the Nomads
    
    
    Former-commit-id: e0245aac4756d6455f5e9bbc5ec9323bbaf8b4d6

--- sys/i386/apm/apm.c
+++ sys/i386/apm/apm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.30 1996/03/18 22:40:57 nate Exp $  */
+comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.31 1996/03/18 22:47:16 nate Exp $  */
 end_comment
 
 begin_include
@@ -2272,6 +2272,15 @@ argument_list|(
 name|sc
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|sc
+operator|->
+name|active
+operator|==
+literal|1
+condition|)
+block|{
 name|timeout
 argument_list|(
 name|apm_timeout
@@ -2288,6 +2297,7 @@ literal|1
 argument_list|)
 expr_stmt|;
 comment|/* More than 1 Hz */
+block|}
 block|}
 end_function
 
@@ -3495,9 +3505,24 @@ block|{
 case|case
 name|APMIO_SUSPEND
 case|:
+if|if
+condition|(
+name|sc
+operator|->
+name|active
+condition|)
+block|{
 name|apm_suspend
 argument_list|()
 expr_stmt|;
+block|}
+else|else
+block|{
+name|error
+operator|=
+name|EINVAL
+expr_stmt|;
+block|}
 break|break;
 case|case
 name|APMIO_GETINFO
