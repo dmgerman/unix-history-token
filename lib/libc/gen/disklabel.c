6cddf04727689abaaee2abc58f0c4e326d60049b
Bruce Evans <bde@FreeBSD.org>
1996-03-19 13:44:57 +0000

    Updated a type to match Lite2's fixed-width type changes.
    Added $Id$.
    
    Obtained from:	4.4BSD-Lite2
    
    
    Former-commit-id: 2cc5879cea95dd1dc6e1334b269fb6e68e57831f

--- lib/libc/gen/disklabel.c
+++ lib/libc/gen/disklabel.c
@@ -9,13 +9,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)disklabel.c	8.2 (Berkeley) 5/3/95";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)disklabel.c	8.1 (Berkeley) 6/4/93"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
@@ -201,7 +214,7 @@ index|[
 literal|3
 index|]
 decl_stmt|;
-name|u_long
+name|u_int32_t
 modifier|*
 name|dx
 decl_stmt|;
