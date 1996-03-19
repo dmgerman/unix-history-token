9f978259ca2049218721222d09c20d6825dae90c
Nate Williams <nate@FreeBSD.org>
1996-03-19 04:40:03 +0000

    Always enable interrupts before calling the APM idle/busy routines.
    Suggested by:	phk@FreeBSD.org
    
    
    Former-commit-id: 812ee0e343ce8029a41fc14c2ba5e140b26508cf

--- sys/i386/bios/apm.c
+++ sys/i386/bios/apm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.31 1996/03/18 22:47:16 nate Exp $  */
+comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.32 1996/03/18 23:30:14 nate Exp $  */
 end_comment
 
 begin_include
@@ -2205,7 +2205,7 @@ operator|->
 name|halt_cpu
 condition|)
 block|{
-asm|__asm("sti ; hlt");
+asm|__asm("hlt");
 comment|/* wait for interrupt */
 block|}
 block|}
