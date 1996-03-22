0df11fbd815cc07892a2e9890dab8ac7fa812733
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-03-22 16:26:26 +0000

    Disable SCSI parity checking until we figure out why the aic78X0 generates them incorrectly in some Wide scenarios.
    Former-commit-id: bf0bc34656c017aa703791730cf2009e09326906

--- sys/i386/scsi/aic7xxx.c
+++ sys/i386/scsi/aic7xxx.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995, 1996 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/eisa/aic7770.c	27/284X and aic7770 motherboard controllers  * pci/aic7870.c	3940, 2940, aic7870 and aic7850 controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.56 1996/03/10 07:11:43 gibbs Exp $  */
+comment|/*  * Generic driver for the aic7xxx based adaptec SCSI controllers  * Copyright (c) 1994, 1995, 1996 Justin T. Gibbs.  * All rights reserved.  *  * Product specific probe and attach routines can be found in:  * i386/eisa/aic7770.c	27/284X and aic7770 motherboard controllers  * pci/aic7870.c	3940, 2940, aic7870 and aic7850 controllers  *  * Portions of this driver are based on the FreeBSD 1742 Driver:  *  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aic7xxx.c,v 1.57 1996/03/11 02:48:41 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -5490,8 +5490,7 @@ name|iobase
 argument_list|)
 operator|&
 operator|(
-name|ENSPCHK
-operator||
+comment|/*ENSPCHK|*/
 name|STIMESEL
 operator|)
 expr_stmt|;
@@ -5610,8 +5609,7 @@ name|iobase
 argument_list|)
 operator|&
 operator|(
-name|ENSPCHK
-operator||
+comment|/*ENSPCHK|*/
 name|STIMESEL
 operator|)
 expr_stmt|;
