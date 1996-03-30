5b63da4397a56023737c8d83ff8269f0d43fec75
Peter Wemm <peter@FreeBSD.org>
1996-03-30 15:02:58 +0000

    Because of the way that ptrace() now calls procfs routines to read/write the process's memory, it was possible for the procfs_domem() call to return a residual leftover, but with no errno.  Since this is no good for ptrace which ignored the the residual, remap a leftover amount into an errno rather than fooling the caller into thinking it was successful when in fact it was not.
    Submitted by: bde (a very long time ago :-)
    
    
    Former-commit-id: ce097ab3ed65a76a14b61e41550b890a9844267e

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994, Sean Eric Fagan  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Sean Eric Fagan.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sys_process.c,v 1.20 1996/01/19 03:58:04 dyson Exp $  */
+comment|/*  * Copyright (c) 1994, Sean Eric Fagan  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Sean Eric Fagan.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sys_process.c,v 1.21 1996/01/24 18:29:00 peter Exp $  */
 end_comment
 
 begin_include
@@ -997,8 +997,8 @@ name|uio_procp
 operator|=
 name|p
 expr_stmt|;
-return|return
-operator|(
+name|error
+operator|=
 name|procfs_domem
 argument_list|(
 name|curp
@@ -1010,6 +1010,40 @@ argument_list|,
 operator|&
 name|uio
 argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|uio
+operator|.
+name|uio_resid
+operator|!=
+literal|0
+condition|)
+block|{
+comment|/* 			 * XXX procfs_domem() doesn't currently return ENOSPC, 			 * so I think write() can bogusly return 0. 			 * XXX what happens for short writes?  We don't want 			 * to write partial data. 			 * XXX procfs_domem() returns EPERM for other invalid 			 * addresses.  Convert this to EINVAL.  Does this 			 * clobber returns of EPERM for other reasons? 			 */
+if|if
+condition|(
+name|error
+operator|==
+literal|0
+operator|||
+name|error
+operator|==
+name|ENOSPC
+operator|||
+name|error
+operator|==
+name|EPERM
+condition|)
+name|error
+operator|=
+name|EINVAL
+expr_stmt|;
+comment|/* EOF */
+block|}
+return|return
+operator|(
+name|error
 operator|)
 return|;
 case|case
