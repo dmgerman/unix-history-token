88d26a663f5530c52e21f5e92bb9b97197bbf5b2
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-03-31 03:04:38 +0000

    Don't set the number of SCBs here.  aic7xxx.c determines the number of SCBs by walking them.
    Former-commit-id: 380001a945ff1965d32490604e6f4dd50297d659

--- sys/i386/eisa/aic7770.c
+++ sys/i386/eisa/aic7770.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  * 	27/284X and aic7770 motherboard SCSI controllers  *  * Copyright (c) 1995, 1996 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7770.c,v 1.24 1996/01/31 18:02:17 gibbs Exp $  */
+comment|/*  * Product specific probe and attach routines for:  * 	27/284X and aic7770 motherboard SCSI controllers  *  * Copyright (c) 1995, 1996 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: aic7770.c,v 1.25 1996/02/26 01:01:40 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -916,13 +916,6 @@ name|id_string
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * Only four SCBs on these cards.  If we ever do SCB paging, 	 * we could support 255 on the Rev E cards. 	 */
-name|ahc
-operator|->
-name|maxscbs
-operator|=
-literal|0x4
-expr_stmt|;
 comment|/* Setup the FIFO threshold and the bus off time */
 if|if
 condition|(
