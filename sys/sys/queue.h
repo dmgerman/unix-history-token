b9233dcdeaa8f516a19148a005bd4fee408a7d81
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-03-31 03:21:45 +0000

    Implement the SLIST and the STAILQ macros.  This gives a program all the aesthetics of using the 4.4 queue macros without paying undo space or time in scenartios where a singly-linked list works fine.
    From queue.h:
    /*
     * A singly-linked list is headed by a single forward pointer. The elements
     * are singly linked for minimum space and pointer manipulation overhead at
     * the expense of O(n) removal for arbitrary elements. New elements can be
     * added to the list after an existing element or at the head of the list.
     * Elements being removed from the head of the list should use the explicit
     * macro for this purpose for optimum efficiency. A singly-linked list may
     * only be traversed in the forward direction.  Singly-linked lists are ideal
     * for applications with large datasets and few or no removals or for
     * implementing a LIFO queue.
     *
     * A singly-linked tail queue is headed by a pair of pointers, one to the
     * head of the list and the other to the tail of the list. The elements are
     * singly linked for minimum space and pointer manipulation overhead at the
     * expense of O(n) removal for arbitrary elements. New elements can be added
     * to the list after an existing element, at the head of the list, or at the
     * end of the list. Elements being removed from the head of the tail queue
     * should use the explicit macro for this purpose for optimum efficiency.
     * A singly-linked tail queue may only be traversed in the forward direction.
     * Singly-linked tail queues are ideal for applications with large datasets
     * and few or no removals or for implementing a FIFO queue.
     */
    
    
    Former-commit-id: 1b92c6dcbdd8084674018258c047102be41408b3

--- sys/sys/queue.h
+++ sys/sys/queue.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)queue.h	8.5 (Berkeley) 8/20/94  * $Id: queue.h,v 1.7 1996/02/24 10:58:08 hsu Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)queue.h	8.5 (Berkeley) 8/20/94  * $Id: queue.h,v 1.7 1996/03/11 02:14:38 hsu Exp $  */
 end_comment
 
 begin_ifndef
@@ -16,8 +16,230 @@ name|_SYS_QUEUE_H_
 end_define
 
 begin_comment
-comment|/*  * This file defines three types of data structures: lists, tail queues,  * and circular queues.  *  * A list is headed by a single forward pointer (or an array of forward  * pointers for a hash table header). The elements are doubly linked  * so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list before  * or after an existing element or at the head of the list. A list  * may only be traversed in the forward direction.  *  * A tail queue is headed by a pair of pointers, one to the head of the  * list and the other to the tail of the list. The elements are doubly  * linked so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list before or  * after an existing element, at the head of the list, or at the end of  * the list. A tail queue may only be traversed in the forward direction.  *  * A circle queue is headed by a pair of pointers, one to the head of the  * list and the other to the tail of the list. The elements are doubly  * linked so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list before or after  * an existing element, at the head of the list, or at the end of the list.  * A circle queue may be traversed in either direction, but has a more  * complex end of list detection.  *  * For details on the use of these macros, see the queue(3) manual page.  */
+comment|/*  * This file defines five types of data structures: singly-linked lists,  * slingly-linked tail queues, lists, tail queues, and circular queues.  *  * A singly-linked list is headed by a single forward pointer. The elements  * are singly linked for minimum space and pointer manipulation overhead at  * the expense of O(n) removal for arbitrary elements. New elements can be  * added to the list after an existing element or at the head of the list.  * Elements being removed from the head of the list should use the explicit  * macro for this purpose for optimum efficiency. A singly-linked list may  * only be traversed in the forward direction.  Singly-linked lists are ideal  * for applications with large datasets and few or no removals or for  * implementing a LIFO queue.  *  * A singly-linked tail queue is headed by a pair of pointers, one to the  * head of the list and the other to the tail of the list. The elements are  * singly linked for minimum space and pointer manipulation overhead at the  * expense of O(n) removal for arbitrary elements. New elements can be added  * to the list after an existing element, at the head of the list, or at the  * end of the list. Elements being removed from the head of the tail queue  * should use the explicit macro for this purpose for optimum efficiency.  * A singly-linked tail queue may only be traversed in the forward direction.  * Singly-linked tail queues are ideal for applications with large datasets  * and few or no removals or for implementing a FIFO queue.  *  * A list is headed by a single forward pointer (or an array of forward  * pointers for a hash table header). The elements are doubly linked  * so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list before  * or after an existing element or at the head of the list. A list  * may only be traversed in the forward direction.  *  * A tail queue is headed by a pair of pointers, one to the head of the  * list and the other to the tail of the list. The elements are doubly  * linked so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list before or  * after an existing element, at the head of the list, or at the end of  * the list. A tail queue may only be traversed in the forward direction.  *  * A circle queue is headed by a pair of pointers, one to the head of the  * list and the other to the tail of the list. The elements are doubly  * linked so that an arbitrary element can be removed without a need to  * traverse the list. New elements can be added to the list before or after  * an existing element, at the head of the list, or at the end of the list.  * A circle queue may be traversed in either direction, but has a more  * complex end of list detection.  *  * For details on the use of these macros, see the queue(3) manual page.  */
 end_comment
+
+begin_comment
+comment|/*  * Singly-linked List definitions.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SLIST_HEAD
+parameter_list|(
+name|name
+parameter_list|,
+name|type
+parameter_list|)
+define|\
+value|struct name {								\ 	struct type *slh_first;
+comment|/* first element */
+value|\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|SLIST_ENTRY
+parameter_list|(
+name|type
+parameter_list|)
+define|\
+value|struct {								\ 	struct type *sle_next;
+comment|/* next element */
+value|\ }
+end_define
+
+begin_comment
+comment|/*  * Singly-linked List functions.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SLIST_INIT
+parameter_list|(
+name|head
+parameter_list|)
+value|{						\ 	(head)->slh_first = NULL;					\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|SLIST_INSERT_AFTER
+parameter_list|(
+name|slistelm
+parameter_list|,
+name|elm
+parameter_list|,
+name|field
+parameter_list|)
+value|{			\ 	(elm)->field.sle_next = (slistelm)->field.sle_next;		\ 	(slistelm)->field.sle_next = (elm);				\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|SLIST_INSERT_HEAD
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	(elm)->field.sle_next = (head)->slh_first;			\ 	(head)->slh_first = (elm);					\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|SLIST_REMOVE_HEAD
+parameter_list|(
+name|head
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	(head)->slh_first = (head)->slh_first->field.sle_next;		\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|SLIST_REMOVE
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	if ((head)->slh_first == (elm)) {				\ 		SLIST_REMOVE_HEAD((head), field);			\ 	}								\ 	else {								\ 		struct type *curelm = (head)->slh_first;		\ 		while( curelm->field.sle_next != (elm) )		\ 			curelm = curelm->field.sle_next;		\ 		curelm->field.sle_next =				\ 		    curelm->field.sle_next->field.sle_next;		\ 	}								\ }
+end_define
+
+begin_comment
+comment|/*  * Singly-linked Tail queue definitions.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STAILQ_HEAD
+parameter_list|(
+name|name
+parameter_list|,
+name|type
+parameter_list|)
+define|\
+value|struct name {								\ 	struct type *stqh_first;
+comment|/* first element */
+value|\ 	struct type **stqh_last;
+comment|/* addr of last next element */
+value|\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|STAILQ_ENTRY
+parameter_list|(
+name|type
+parameter_list|)
+define|\
+value|struct {								\ 	struct type *stqe_next;
+comment|/* next element */
+value|\ }
+end_define
+
+begin_comment
+comment|/*  * Singly-linked Tail queue functions.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STAILQ_INIT
+parameter_list|(
+name|head
+parameter_list|)
+value|{						\ 	(head)->stqh_first = NULL;					\ 	(head)->stqh_last =&(head)->stqh_first;			\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|STAILQ_INSERT_HEAD
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	if (((elm)->field.stqe_next = (head)->stqh_first) == NULL)	\ 		(head)->stqh_last =&(elm)->field.stqe_next;		\ 	(head)->stqh_first = (elm);					\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|STAILQ_INSERT_TAIL
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	(elm)->field.stqe_next = NULL;					\ 	*(head)->stqh_last = (elm);					\ 	(head)->stqh_last =&(elm)->field.stqe_next;			\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|STAILQ_INSERT_AFTER
+parameter_list|(
+name|head
+parameter_list|,
+name|tqelm
+parameter_list|,
+name|elm
+parameter_list|,
+name|field
+parameter_list|)
+value|{			\ 	if (((elm)->field.stqe_next = (tqelm)->field.stqe_next) == NULL)\ 		(head)->stqh_last =&(elm)->field.stqe_next;		\ 	(tqelm)->field.stqe_next = (elm);				\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|STAILQ_REMOVE_HEAD
+parameter_list|(
+name|head
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	if (((head)->stqh_first =					\ 	     (head)->stqh_first->field.stqe_next) == NULL)		\ 		(head)->stqh_last =&(head)->stqh_first;		\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|STAILQ_REMOVE
+parameter_list|(
+name|head
+parameter_list|,
+name|elm
+parameter_list|,
+name|type
+parameter_list|,
+name|field
+parameter_list|)
+value|{				\ 	if ((head)->stqh_first == (elm)) {				\ 		STAILQ_REMOVE_HEAD(head, field);			\ 	}								\ 	else {								\ 		struct type *curelm = (head)->stqh_first;		\ 		while( curelm->field.stqe_next != (elm) )		\ 			curelm = curelm->field.stqe_next;		\ 		if((curelm->field.stqe_next =				\ 		    curelm->field.stqe_next->field.stqe_next) == NULL)	\ 			(head)->stqh_last =&(curelm)->field.stqe_next;	\ 	}								\ }
+end_define
 
 begin_comment
 comment|/*  * List definitions.  */
