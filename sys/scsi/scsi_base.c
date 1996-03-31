ce71be5b9c631390ed76f6b5b7063885eea72731
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-03-31 03:19:09 +0000

    Implement the XS_SELTIMEOUT error code.  This causes the SCSI code to not retry again and should be used when a device times out during selection (ie is not on the bus).  This should speed up the boot sequence.
    Former-commit-id: 3a1677dd043db7375d021ec966915d83ab307d05

--- sys/scsi/scsi_base.c
+++ sys/scsi/scsi_base.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written By Julian ELischer  * Copyright julian Elischer 1993.  * Permission is granted to use or redistribute this file in any way as long  * as this notice remains. Julian Elischer does not guarantee that this file  * is totally correct for any given task and users of this file must  * accept responsibility for any damage that occurs from the application of this  * file.  *  * Written by Julian Elischer (julian@dialix.oz.au)  *      $Id: scsi_base.c,v 1.35 1996/01/05 20:12:45 wollman Exp $  */
+comment|/*  * Written By Julian ELischer  * Copyright julian Elischer 1993.  * Permission is granted to use or redistribute this file in any way as long  * as this notice remains. Julian Elischer does not guarantee that this file  * is totally correct for any given task and users of this file must  * accept responsibility for any damage that occurs from the application of this  * file.  *  * Written by Julian Elischer (julian@dialix.oz.au)  *      $Id: scsi_base.c,v 1.36 1996/03/10 07:13:07 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -2937,6 +2937,9 @@ name|SCSIRET_DO_RETRY
 argument_list|)
 return|;
 comment|/* fall through */
+case|case
+name|XS_SELTIMEOUT
+case|:
 case|case
 name|XS_DRIVER_STUFFUP
 case|:
--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.39 1996/02/03 13:31:12 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.40 1996/03/10 07:13:11 gibbs Exp $  */
 end_comment
 
 begin_ifndef
@@ -1596,7 +1596,18 @@ value|0x09
 end_define
 
 begin_comment
-comment|/* Illegal length (over/under run)	*/
+comment|/* Illegal length (over/under run)	  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|XS_SELTIMEOUT
+value|0x10
+end_define
+
+begin_comment
+comment|/* Device failed to respond to selection  */
 end_comment
 
 begin_ifdef
