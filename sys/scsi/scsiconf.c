e905767da45400814e5732b393db75adb2afef2b
Rich Murphey <rich@FreeBSD.org>
1996-04-04 22:43:59 +0000

    add Nakamichi MBR-7 CD changer to knowndevs. Each platter is on a separate LUN, all sharing one scsi ID. Reviewed by:	davidg, julian Submitted by:	rich Obtained from:  rich
    Former-commit-id: 915139e418d236f1787fdfe3e7596e8478f6a509

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.30.4.4 1995/10/10 00:42:02 davidg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.30.4.5 1996/04/01 00:26:10 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -1190,6 +1190,22 @@ name|T_READONLY
 block|,
 name|T_REMOV
 block|,
+literal|"NRC"
+block|,
+literal|"MBR-7"
+block|,
+literal|"*"
+block|,
+literal|"cd"
+block|,
+name|SC_MORE_LUS
+block|}
+block|,
+block|{
+name|T_READONLY
+block|,
+name|T_REMOV
+block|,
 literal|"CHINON"
 block|,
 literal|"CD-ROM CDS-535"
@@ -1402,6 +1418,22 @@ operator|,
 literal|"PIONEER"
 operator|,
 literal|"CD-ROM DRM-602X"
+operator|,
+literal|"any"
+operator|,
+literal|"cd"
+operator|,
+name|SC_MORE_LUS
+block|}
+operator|,
+block|{
+name|T_READONLY
+operator|,
+name|T_REMOV
+operator|,
+literal|"NRC"
+operator|,
+literal|"MBR-7"
 operator|,
 literal|"any"
 operator|,
