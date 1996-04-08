f22b3ce71db4c807faec5dc63035a06786869c97
Garrett Wollman <wollman@FreeBSD.org>
1996-04-08 16:41:06 +0000

    Added a $Id$ keyword.  Bruce still needs to put a copyright notice on this file.
    Former-commit-id: 3ab61895f369e0a17d3080af207089d9ba0d51d4

--- sys/i386/isa/prof_machdep.c
+++ sys/i386/isa/prof_machdep.c
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * NEED A COPYRIGHT NOPTICE HERE  *  *	$Id$  */
+end_comment
+
 begin_include
 include|#
 directive|include
