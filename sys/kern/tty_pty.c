3734938f4e2bbac5e2565b5bcbe2b49e52d1fbee
Bruce Evans <bde@FreeBSD.org>
1996-04-11 18:43:37 +0000

    Fixed the PF_REMOTE case of ptcwrite().  I broke it in rev.1.22.  Output was duplicated until the canq filled up, and write() normally returned 0. This case is apparently rare.  It was reported for Jove's shell buffer in PR 1130.
    Former-commit-id: 9894b75b3fff5971ea52cdf379d83af6a834d77c

--- sys/kern/tty_pty.c
+++ sys/kern/tty_pty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_pty.c	8.4 (Berkeley) 2/20/95  * $Id: tty_pty.c,v 1.38 1996/02/29 00:18:11 hsu Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_pty.c	8.4 (Berkeley) 2/20/95  * $Id: tty_pty.c,v 1.37 1996/03/11 02:24:39 hsu Exp $  */
 end_comment
 
 begin_comment
@@ -2839,9 +2839,12 @@ block|}
 if|if
 condition|(
 name|cc
+operator|>
+literal|0
 condition|)
+block|{
 name|cc
-operator|-=
+operator|=
 name|b_to_q
 argument_list|(
 operator|(
@@ -2858,6 +2861,15 @@ operator|->
 name|t_canq
 argument_list|)
 expr_stmt|;
+comment|/* 				 * XXX we don't guarantee that the canq size 				 * is>= TTYHOG, so the above b_to_q() may 				 * leave some bytes uncopied.  However, space 				 * is guaranteed for the null terminator if 				 * we don't fail here since (TTYHOG - 1) is 				 * not a multiple of CBSIZE. 				 */
+if|if
+condition|(
+name|cc
+operator|>
+literal|0
+condition|)
+break|break;
+block|}
 block|}
 comment|/* adjust for data copied in but not written */
 name|uio
@@ -3081,7 +3093,7 @@ operator|)
 return|;
 name|block
 label|:
-comment|/* 	 * Come here to wait for slave to open, for space 	 * in outq, or space in rawq. 	 */
+comment|/* 	 * Come here to wait for slave to open, for space 	 * in outq, or space in rawq, or an empty canq. 	 */
 if|if
 condition|(
 operator|(
