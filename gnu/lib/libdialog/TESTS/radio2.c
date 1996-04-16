a72d7bbbd7fc4b75faaeb7e9d633ce6e2fb015d2
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-04-16 12:17:27 +0000

    More changes to attempt to make this whole new dialog scheme more robust.  The new "fire" actions, while affording signficantly more interactivity to libdialog, come at a cost - if the fire action trashes the screen then you're not going to be in Kansas anymore when you come back to the menu and there had better be considerable extra smarts in place for coping with such a situation.  These changes are my attempt to do just that.
    Former-commit-id: a4df22e719d649976ab52c37054f626e20c50970

--- gnu/lib/libdialog/TESTS/radio2.c
+++ gnu/lib/libdialog/TESTS/radio2.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * small test-driver for new dialog functionality  *  * Copyright (c) 1995, Jordan Hubbard  *  * All rights reserved.  *  * This source code may be used, modified, copied, distributed, and  * sold, in both source and binary form provided that the above  * copyright and these terms are retained, verbatim, as the first  * lines of this file.  Under no circumstances is the author  * responsible for the proper functioning of the software nor does  * the author assume any responsibility for damages incurred with  * its use.  *  * $Id: radio2.c,v 1.1 1996/01/01 03:45:29 jkh Exp $  */
+comment|/*  * small test-driver for new dialog functionality  *  * Copyright (c) 1995, Jordan Hubbard  *  * All rights reserved.  *  * This source code may be used, modified, copied, distributed, and  * sold, in both source and binary form provided that the above  * copyright and these terms are retained, verbatim, as the first  * lines of this file.  Under no circumstances is the author  * responsible for the proper functioning of the software nor does  * the author assume any responsibility for damages incurred with  * its use.  *  * $Id: radio2.c,v 1.2 1996/04/07 03:20:59 jkh Exp $  */
 end_comment
 
 begin_include
@@ -102,6 +102,8 @@ name|prompt
 argument_list|)
 expr_stmt|;
 return|return
+name|DITEM_SUCCESS
+operator||
 name|DITEM_REDRAW
 return|;
 block|}
@@ -151,6 +153,8 @@ name|prompt
 argument_list|)
 expr_stmt|;
 return|return
+name|DITEM_SUCCESS
+operator||
 name|DITEM_REDRAW
 return|;
 block|}
