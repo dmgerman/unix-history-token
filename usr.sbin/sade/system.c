00631f88dbc658a48f759041a198288d4f086d91
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-05-01 03:31:08 +0000

    Micro tweak for getting the Holographic shell message to stay on the screen longer.
    Former-commit-id: 29ec0e3a86556713b45647a9160170533c93cf1c

--- usr.sbin/sade/system.c
+++ usr.sbin/sade/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.56 1996/04/28 20:54:09 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.57 1996/04/29 21:15:44 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
 end_comment
 
 begin_include
@@ -1276,7 +1276,7 @@ argument_list|)
 expr_stmt|;
 name|sleep
 argument_list|(
-literal|1
+literal|2
 argument_list|)
 expr_stmt|;
 block|}
