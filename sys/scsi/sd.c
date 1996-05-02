f79f2c14856e0fdd8b1bdcbbbf781fd1bdb9bc83
Poul-Henning Kamp <phk@FreeBSD.org>
1996-05-02 22:20:52 +0000

    Don't fiddle page tables by hand, use pmap_enter. (copied from wd.c)
    Former-commit-id: af508f40fcf4517bd4de5f360689e1a47482cf72

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.87 1996/03/27 18:50:09 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.88 1996/05/02 14:20:54 phk Exp $  */
 end_comment
 
 begin_include
@@ -149,6 +149,12 @@ begin_include
 include|#
 directive|include
 file|<vm/vm_param.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/vm_prot.h>
 end_include
 
 begin_include
@@ -4037,25 +4043,24 @@ operator|>
 literal|0
 condition|)
 block|{
-operator|*
+name|pmap_enter
+argument_list|(
+name|kernel_pmap
+argument_list|,
 operator|(
-name|int
-operator|*
+name|vm_offset_t
 operator|)
-name|CMAP1
-operator|=
-comment|/* XXX use pmap_enter() */
-name|PG_V
-operator||
-name|PG_KW
-operator||
+name|CADDR1
+argument_list|,
 name|trunc_page
 argument_list|(
 name|addr
 argument_list|)
-expr_stmt|;
-name|pmap_update
-argument_list|()
+argument_list|,
+name|VM_PROT_READ
+argument_list|,
+name|TRUE
+argument_list|)
 expr_stmt|;
 comment|/* 		 *  Fill out the scsi command 		 */
 name|bzero
