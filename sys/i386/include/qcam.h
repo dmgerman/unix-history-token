7dc11173dd55fc65b3aa6ef8b690a9c698b9ee0d
Paul Traina <pst@FreeBSD.org>
1996-05-04 07:05:55 +0000

    Update to 1.1beta5d
    Former-commit-id: 067c6d06dfb69967da223f95496308f16487c455

--- sys/i386/include/qcam.h
+++ sys/i386/include/qcam.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 1996 by Thomas Davis  * Copyright (C) 1996 by Scott Laird  *  * Permission is hereby granted, free of charge, to any person obtaining  * a copy of this software and associated documentation files (the  * "Software"), to deal in the Software without restriction, including  * without limitation the rights to use, copy, modify, merge, publish,  * distribute, sublicense, and/or sell copies of the Software, and to  * permit persons to whom the Software is furnished to do so, subject to  * the following conditions:  *  * The above copyright notice and this permission notice shall be  * included in all copies or substantial portions of the Software.  *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  * IN NO EVENT SHALL SCOTT LAIRD BE LIABLE FOR ANY CLAIM, DAMAGES OR  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  * OTHER DEALINGS IN THE SOFTWARE.  *  * NOTE: this file contains the sole public interface between the driver  * and user applications.  Every effort should be made to retain comaptibility  * with the decided upon standard interface shared between Linux and  * FreeBSD.  Currently, FreeBSD uses a different (richer) interface than  * the Linux.  *  * WARNING WARNING: The contents of this structure is in flux,  *		    recompile often, the driver will change the version  *		    number when the interface changes for now...  */
+comment|/*  * Connectix QuickCam parallel-port camera video capture driver.  * Copyright (c) 1996, Paul Traina.  *  * This driver is based in part on work  * Copyright (c) 1996, Thomas Davis.  *  * QuickCam(TM) is a registered trademark of Connectix Inc.  * Use this driver at your own risk, it is not warranted by  * Connectix or the authors.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * NOTE: this file contains the sole public interface between the driver  * and user applications.  Every effort should be made to retain comaptibility  * with the decided upon standard interface shared between Linux and  * FreeBSD.  Currently, FreeBSD uses a different (richer) interface than  * the Linux.  *  * WARNING WARNING: The contents of this structure is in flux,  *		    recompile often, the driver will change the version  *		    number when the interface changes for now...  */
 end_comment
 
 begin_ifndef
@@ -15,6 +15,49 @@ directive|define
 name|_QUICKCAM_H
 value|1
 end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_IOR
+end_ifndef
+
+begin_comment
+comment|/* SCO doesn't have _IOR/_IOW for ioctls, so fake it out to keep things clean*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_IOR
+parameter_list|(
+name|cat
+parameter_list|,
+name|func
+parameter_list|,
+name|data
+parameter_list|)
+value|(((cat)<< 8) | (func))
+end_define
+
+begin_define
+define|#
+directive|define
+name|_IOW
+parameter_list|(
+name|cat
+parameter_list|,
+name|func
+parameter_list|,
+name|data
+parameter_list|)
+value|(((cat)<< 8) | (func))
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/*  * ioctls  */
