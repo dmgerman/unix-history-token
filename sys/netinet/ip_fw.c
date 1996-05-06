dc4624042e998aa1b348236a64dd04748cc597ec
Poul-Henning Kamp <phk@FreeBSD.org>
1996-05-06 20:32:01 +0000

    Merge from head.
    Former-commit-id: 90a3e59e65f2cef3aad7c3f8bc9df57437b6b1a5

--- sys/netinet/ip_fw.c
+++ sys/netinet/ip_fw.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.32 1996/02/24 13:38:26 phk Exp $  */
+comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.14.4.6 1996/02/26 15:23:32 phk Exp $  */
 end_comment
 
 begin_comment
@@ -2151,11 +2151,6 @@ decl_stmt|;
 name|int
 name|s
 decl_stmt|;
-name|s
-operator|=
-name|splnet
-argument_list|()
-expr_stmt|;
 name|fwc
 operator|=
 name|malloc
@@ -2220,11 +2215,6 @@ argument_list|,
 name|M_IPFW
 argument_list|)
 expr_stmt|;
-name|splx
-argument_list|(
-name|s
-argument_list|)
-expr_stmt|;
 return|return
 operator|(
 name|ENOSPC
@@ -2261,6 +2251,11 @@ operator|->
 name|rule
 operator|=
 name|ftmp
+expr_stmt|;
+name|s
+operator|=
+name|splnet
+argument_list|()
 expr_stmt|;
 if|if
 condition|(
@@ -2501,18 +2496,14 @@ operator|->
 name|rule
 operator|->
 name|fw_number
-operator|==
+operator|!=
 operator|(
 name|u_short
 operator|)
 operator|-
 literal|1
 condition|)
-return|return
-operator|(
-name|EINVAL
-operator|)
-return|;
+block|{
 for|for
 control|(
 init|;
@@ -2571,6 +2562,7 @@ expr_stmt|;
 return|return
 literal|0
 return|;
+block|}
 block|}
 block|}
 name|splx
@@ -2995,6 +2987,12 @@ name|ip_fw_chain
 operator|.
 name|lh_first
 decl_stmt|;
+name|int
+name|s
+init|=
+name|splnet
+argument_list|()
+decl_stmt|;
 name|LIST_REMOVE
 argument_list|(
 name|ip_fw_chain
@@ -3002,6 +3000,11 @@ operator|.
 name|lh_first
 argument_list|,
 name|chain
+argument_list|)
+expr_stmt|;
+name|splx
+argument_list|(
+name|s
 argument_list|)
 expr_stmt|;
 name|free
@@ -3046,6 +3049,12 @@ operator|==
 name|IP_FW_ZERO
 condition|)
 block|{
+name|int
+name|s
+init|=
+name|splnet
+argument_list|()
+decl_stmt|;
 name|struct
 name|ip_fw_chain
 modifier|*
@@ -3082,6 +3091,11 @@ operator|->
 name|fw_pcnt
 operator|=
 literal|0
+expr_stmt|;
+name|splx
+argument_list|(
+name|s
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
