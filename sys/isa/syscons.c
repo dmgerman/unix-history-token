8ec3bae3a6324b2d68b05b45e6d867f02707b34f
Joerg Wunsch <joerg@FreeBSD.org>
1996-05-11 23:16:23 +0000

    Give up waiting for a successful keyboard reset after some unreasonable time.  I've got a PCI mainboard that simply doesn't grok it, so continuing with a warning (and a keyboard that's working nevertheless :) seems to be better than spin-looping forever.
    Former-commit-id: c649e5d297c35eeefabb504ebeed7c859ce64ab3

--- sys/isa/syscons.c
+++ sys/isa/syscons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.146 1996/05/01 03:58:21 bde Exp $  */
+comment|/*-  * Copyright (c) 1992-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.147 1996/05/02 21:47:50 ache Exp $  */
 end_comment
 
 begin_include
@@ -1173,6 +1173,8 @@ block|{
 name|int
 name|i
 decl_stmt|,
+name|j
+decl_stmt|,
 name|retries
 init|=
 literal|5
@@ -1306,6 +1308,11 @@ argument_list|)
 expr_stmt|;
 else|else
 block|{
+name|i
+operator|=
+literal|10
+expr_stmt|;
+comment|/* At most 10 retries. */
 name|gotack
 label|:
 name|DELAY
@@ -1313,6 +1320,11 @@ argument_list|(
 literal|10
 argument_list|)
 expr_stmt|;
+name|j
+operator|=
+literal|1000
+expr_stmt|;
+comment|/* Wait at most 10 ms (supposedly). */
 while|while
 condition|(
 operator|(
@@ -1324,6 +1336,11 @@ operator|&
 name|KB_BUF_FULL
 operator|)
 operator|==
+literal|0
+operator|&&
+operator|--
+name|j
+operator|>
 literal|0
 condition|)
 name|DELAY
@@ -1348,6 +1365,11 @@ condition|(
 name|val
 operator|==
 name|KB_ACK
+operator|&&
+operator|--
+name|i
+operator|>
+literal|0
 condition|)
 goto|goto
 name|gotack
@@ -1360,7 +1382,7 @@ name|KB_RESET_DONE
 condition|)
 name|printf
 argument_list|(
-literal|"scprobe: keyboard RESET failed %02x\n"
+literal|"scprobe: keyboard RESET failed (result = 0x%02x)\n"
 argument_list|,
 name|val
 argument_list|)
