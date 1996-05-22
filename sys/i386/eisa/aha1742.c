8267980c4a87168468b1a97d4e63b445fff3c22d
Dima Ruban <dima@FreeBSD.org>
1996-05-22 00:04:12 +0000

    "board not responding" -> "board is not responding" Reviewed by:	gibbs
    Former-commit-id: 2e27725381d182d87b68f1c428bc2e116098fb6f

--- sys/i386/eisa/aha1742.c
+++ sys/i386/eisa/aha1742.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.51 1996/02/26 01:01:38 gibbs Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * commenced: Sun Sep 27 18:14:01 PDT 1992  *  *      $Id: aha1742.c,v 1.52 1996/03/10 07:04:26 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -1948,7 +1948,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"ahb%d: board not responding\n"
+literal|"ahb%d: board is not responding\n"
 argument_list|,
 name|ahb
 operator|->
@@ -2062,7 +2062,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"ahb%d: board not responding\n"
+literal|"ahb%d: board is not responding\n"
 argument_list|,
 name|ahb
 operator|->
@@ -2228,7 +2228,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"ahb%d: board not responding\n"
+literal|"ahb%d: board is not responding\n"
 argument_list|,
 name|ahb
 operator|->
