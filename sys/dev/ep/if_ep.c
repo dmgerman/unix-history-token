b7e2cf837bb05e45ad15da55ff5c5ae3ba5bd9f6
Justin T. Gibbs <gibbs@FreeBSD.org>
1996-05-24 15:22:36 +0000

    Properly determine the offset of the remaining bytes we need to transfer after outsl'ing all the long words we can during transmission in 32bit mode.
    Closes PR:i386/1243
    Submitted by:	flash@eru.tubank.msk.su
    
    
    Former-commit-id: 3d8ddbfef3dfaec404ef68c8b6c16b05daf8c7fa

--- sys/dev/ep/if_ep.c
+++ sys/dev/ep/if_ep.c
@@ -8,7 +8,7 @@ comment|/*  *	Modified from the FreeBSD 1.1.5.1 version by:  *		 	Andres Vega Ga
 end_comment
 
 begin_comment
-comment|/*  *  $Id: if_ep.c,v 1.42 1996/02/26 01:05:34 gibbs Exp $  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
+comment|/*  *  $Id: if_ep.c,v 1.43 1996/02/28 17:19:03 gibbs Exp $  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
 end_comment
 
 begin_include
@@ -3494,11 +3494,16 @@ argument_list|,
 name|caddr_t
 argument_list|)
 operator|+
+operator|(
 name|m
 operator|->
 name|m_len
-operator|/
-literal|4
+operator|&
+operator|(
+operator|~
+literal|3
+operator|)
+operator|)
 argument_list|,
 name|m
 operator|->
