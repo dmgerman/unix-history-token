425e4124713b015eb2e1e6efa58c91dcf5a3f140
Peter Wemm <peter@FreeBSD.org>
1996-05-24 16:19:23 +0000

    Dont allow directories to be link()ed or unlink()ed, even for root (returns EPERM always, the errno is specified by POSIX).
    If you really have a desperate need to link or unlink a directory, you
    can use fsdb. :-)
    
    This should stop any chance of ftpd, rdist, "rm -rf", etc from
    bugging out and damaging the filesystem structure or loosing races
    with malicious users.
    
    Reviewed by: davidg, bde
    
    
    Former-commit-id: 7557e820b331042c14c36cf58daec8c5e2492ace

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_syscalls.c	8.13 (Berkeley) 4/15/94  * $Id: vfs_syscalls.c,v 1.46 1996/01/16 13:07:14 davidg Exp $  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_syscalls.c	8.13 (Berkeley) 4/15/94  * $Id: vfs_syscalls.c,v 1.47 1996/05/11 04:39:53 bde Exp $  */
 end_comment
 
 begin_comment
@@ -4339,27 +4339,15 @@ condition|(
 name|vp
 operator|->
 name|v_type
-operator|!=
+operator|==
 name|VDIR
-operator|||
-operator|(
+condition|)
 name|error
 operator|=
-name|suser
-argument_list|(
-name|p
-operator|->
-name|p_ucred
-argument_list|,
-operator|&
-name|p
-operator|->
-name|p_acflag
-argument_list|)
-operator|)
-operator|==
-literal|0
-condition|)
+name|EPERM
+expr_stmt|;
+comment|/* POSIX */
+else|else
 block|{
 name|NDINIT
 argument_list|(
@@ -4378,22 +4366,6 @@ name|link
 argument_list|,
 name|p
 argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|vp
-operator|->
-name|v_type
-operator|==
-name|VDIR
-condition|)
-name|nd
-operator|.
-name|ni_cnd
-operator|.
-name|cn_flags
-operator||=
-name|WILLBEDIR
 expr_stmt|;
 name|error
 operator|=
@@ -4947,29 +4919,17 @@ condition|(
 name|vp
 operator|->
 name|v_type
-operator|!=
+operator|==
 name|VDIR
-operator|||
-operator|(
+condition|)
 name|error
 operator|=
-name|suser
-argument_list|(
-name|p
-operator|->
-name|p_ucred
-argument_list|,
-operator|&
-name|p
-operator|->
-name|p_acflag
-argument_list|)
-operator|)
-operator|==
-literal|0
-condition|)
+name|EPERM
+expr_stmt|;
+comment|/* POSIX */
+else|else
 block|{
-comment|/* 		 * The root of a mounted filesystem cannot be deleted. 		 */
+comment|/* 		 * The root of a mounted filesystem cannot be deleted. 		 * 		 * XXX: can this only be a VDIR case? 		 */
 if|if
 condition|(
 name|vp
