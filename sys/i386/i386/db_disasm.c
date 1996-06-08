20f8314b2a218c6d979a76097e495c71962b821f
Bruce Evans <bde@FreeBSD.org>
1996-06-08 10:15:48 +0000

    Removed bogus `altfmt' code.  No alternative formats are supported, but altfmt was abused to sometimes screw up the disassembly of the bytes following unconditional jump instructions.  Gas doesn't pad to a longword boundary like the comment said - that is the programmer's responsibility.
    Former-commit-id: c6ac7c5735d18a20c47c98831a21565cf0c75134

--- sys/i386/i386/db_disasm.c
+++ sys/i386/i386/db_disasm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_disasm.c,v 1.13 1996/01/15 22:40:17 phk Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_disasm.c,v 1.14 1996/03/28 20:35:03 wollman Exp $  */
 end_comment
 
 begin_comment
@@ -11734,46 +11734,6 @@ name|imm
 argument_list|)
 expr_stmt|;
 break|break;
-block|}
-block|}
-if|if
-condition|(
-name|altfmt
-operator|==
-literal|0
-condition|)
-block|{
-if|if
-condition|(
-name|inst
-operator|==
-literal|0xe9
-operator|||
-name|inst
-operator|==
-literal|0xeb
-condition|)
-block|{
-comment|/* 		 * GAS pads to longword boundary after unconditional jumps. 		 */
-name|loc
-operator|=
-operator|(
-name|loc
-operator|+
-operator|(
-literal|4
-operator|-
-literal|1
-operator|)
-operator|)
-operator|&
-operator|~
-operator|(
-literal|4
-operator|-
-literal|1
-operator|)
-expr_stmt|;
 block|}
 block|}
 name|db_printf
