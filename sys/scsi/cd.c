f24091c9ccac4393fbdff73f293f149fc4345d96
Joerg Wunsch <joerg@FreeBSD.org>
1996-06-08 11:38:16 +0000

    Merge the changes from rev 1.55 into -stable.  Required in particular by some SONY drives.
    Former-commit-id: 938190b58a24eaa955b60bebfbe1874d103d61de

--- sys/scsi/cd.c
+++ sys/scsi/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.41.4.1 1995/12/21 12:01:50 davidg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.41.4.2 1996/04/01 00:25:57 gibbs Exp $  */
 end_comment
 
 begin_define
@@ -3703,10 +3703,6 @@ operator|=
 name|LEFT_CHANNEL
 operator||
 name|RIGHT_CHANNEL
-operator||
-literal|4
-operator||
-literal|8
 expr_stmt|;
 name|data
 operator|.
@@ -3773,7 +3769,7 @@ break|break;
 block|}
 break|break;
 case|case
-name|CDIOCSETSTERIO
+name|CDIOCSETSTEREO
 case|:
 block|{
 name|struct
@@ -5078,6 +5074,15 @@ operator|->
 name|header
 operator|.
 name|data_length
+operator|=
+literal|0
+expr_stmt|;
+comment|/* 	 * SONY drives do not allow a mode select with a medium_type 	 * value that has just been returned by a mode sense; use a 	 * medium_type of 0 (Default) instead. 	 */
+name|data
+operator|->
+name|header
+operator|.
+name|medium_type
 operator|=
 literal|0
 expr_stmt|;
