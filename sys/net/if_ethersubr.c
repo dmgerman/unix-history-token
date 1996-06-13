2f40ef8cacb0302144d8267d4c536233238df01a
David Greenman <dg@FreeBSD.org>
1996-06-13 02:54:19 +0000

    Keep ether_type in network order for BPF to be consistent with other systems.
    Submitted by:	Ted Lemon, Matt Thomas, and others. Retrofitted for
    		-current by me.
    
    
    Former-commit-id: 861db8c8ff823b71ef512688c161147fb4b06866

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ethersubr.c	8.1 (Berkeley) 6/10/93  * $Id: if_ethersubr.c,v 1.18 1996/06/10 23:07:30 gpalmer Exp $  */
+comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ethersubr.c	8.1 (Berkeley) 6/10/93  * $Id: if_ethersubr.c,v 1.19 1996/06/12 05:10:13 gpalmer Exp $  */
 end_comment
 
 begin_include
@@ -747,7 +747,10 @@ name|m_len
 expr_stmt|;
 name|type
 operator|=
+name|htons
+argument_list|(
 name|ETHERTYPE_IP
+argument_list|)
 expr_stmt|;
 break|break;
 endif|#
@@ -760,7 +763,10 @@ name|AF_IPX
 case|:
 name|type
 operator|=
+name|htons
+argument_list|(
 name|ETHERTYPE_IPX
+argument_list|)
 expr_stmt|;
 name|bcopy
 argument_list|(
@@ -1050,18 +1056,24 @@ argument_list|)
 expr_stmt|;
 name|type
 operator|=
+name|htons
+argument_list|(
 name|m
 operator|->
 name|m_pkthdr
 operator|.
 name|len
+argument_list|)
 expr_stmt|;
 block|}
 else|else
 block|{
 name|type
 operator|=
+name|htons
+argument_list|(
 name|ETHERTYPE_AT
+argument_list|)
 expr_stmt|;
 block|}
 break|break;
@@ -1076,7 +1088,10 @@ name|AF_NS
 case|:
 name|type
 operator|=
+name|htons
+argument_list|(
 name|ETHERTYPE_NS
+argument_list|)
 expr_stmt|;
 name|bcopy
 argument_list|(
@@ -1430,11 +1445,14 @@ operator|)
 return|;
 name|type
 operator|=
+name|htons
+argument_list|(
 name|m
 operator|->
 name|m_pkthdr
 operator|.
 name|len
+argument_list|)
 expr_stmt|;
 name|l
 operator|=
@@ -1689,11 +1707,14 @@ block|}
 block|}
 name|type
 operator|=
+name|htons
+argument_list|(
 name|m
 operator|->
 name|m_pkthdr
 operator|.
 name|len
+argument_list|)
 expr_stmt|;
 ifdef|#
 directive|ifdef
@@ -1897,16 +1918,6 @@ argument_list|,
 expr|struct
 name|ether_header
 operator|*
-argument_list|)
-expr_stmt|;
-name|type
-operator|=
-name|htons
-argument_list|(
-operator|(
-name|u_short
-operator|)
-name|type
 argument_list|)
 expr_stmt|;
 operator|(
