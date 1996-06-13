ff358bcad04e963ca14d08675bc1c3e6208e5c40
Gary Palmer <gpalmer@FreeBSD.org>
1996-06-13 17:35:28 +0000

    Don't try to include opt_ipfw.h in LKMs
    Submitted by:	Ollivier Robert <roberto@keltia.freenix.fr>
    
    
    Former-commit-id: 93abc0a4214f3da4cb9c684989e96da6b389089e

--- sys/netinet/ip_fw.c
+++ sys/netinet/ip_fw.c
@@ -1,17 +1,28 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 Alex Nash  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.37 1996/06/09 23:46:20 alex Exp $  */
+comment|/*  * Copyright (c) 1996 Alex Nash  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.38 1996/06/12 19:34:33 gpalmer Exp $  */
 end_comment
 
 begin_comment
 comment|/*  * Implement IP packet firewall  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|ACTUALLY_LKM_NOT_KERNEL
+end_ifndef
+
 begin_include
 include|#
 directive|include
 file|"opt_ipfw.h"
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
