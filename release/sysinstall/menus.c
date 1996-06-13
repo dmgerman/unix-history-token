465ae90fbf9249db92f757032f6dbbbbb58271f1
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-06-13 17:07:41 +0000

    Uh, this is what I was really thinking when I made the device registration changes - honest! :-)
    Former-commit-id: e04b1634ebdcaf8a1073bfa03e192458d7f3b947

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: menus.c,v 1.65 1996/06/12 14:20:18 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: menus.c,v 1.66 1996/06/12 17:09:32 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -305,6 +305,30 @@ return|;
 block|}
 end_function
 
+begin_define
+define|#
+directive|define
+name|IS_DEVELOPER
+parameter_list|(
+name|dist
+parameter_list|,
+name|extra
+parameter_list|)
+value|(((dist) == (_DIST_DEVELOPER | (extra))) || ((dist) == (_DIST_DEVELOPER | DIST_DES | (extra))))
+end_define
+
+begin_define
+define|#
+directive|define
+name|IS_USER
+parameter_list|(
+name|dist
+parameter_list|,
+name|extra
+parameter_list|)
+value|(((dist) == (_DIST_USER | (extra))) || ((dist) == (_DIST_USER | DIST_DES | (extra))))
+end_define
+
 begin_function
 specifier|static
 name|int
@@ -317,9 +341,12 @@ parameter_list|)
 block|{
 return|return
 operator|(
+name|IS_DEVELOPER
+argument_list|(
 name|Dists
-operator|==
-name|_DIST_DEVELOPER
+argument_list|,
+literal|0
+argument_list|)
 operator|&&
 name|SrcDists
 operator|==
@@ -341,13 +368,12 @@ parameter_list|)
 block|{
 return|return
 operator|(
+name|IS_DEVELOPER
+argument_list|(
 name|Dists
-operator|==
-operator|(
-name|_DIST_DEVELOPER
-operator||
+argument_list|,
 name|DIST_XF86
-operator|)
+argument_list|)
 operator|&&
 name|SrcDists
 operator|==
@@ -369,9 +395,12 @@ parameter_list|)
 block|{
 return|return
 operator|(
+name|IS_DEVELOPER
+argument_list|(
 name|Dists
-operator|==
-name|_DIST_DEVELOPER
+argument_list|,
+literal|0
+argument_list|)
 operator|&&
 name|SrcDists
 operator|==
@@ -393,9 +422,12 @@ parameter_list|)
 block|{
 return|return
 operator|(
+name|IS_USER
+argument_list|(
 name|Dists
-operator|==
-name|_DIST_USER
+argument_list|,
+literal|0
+argument_list|)
 operator|)
 return|;
 block|}
@@ -413,13 +445,12 @@ parameter_list|)
 block|{
 return|return
 operator|(
+name|IS_USER
+argument_list|(
 name|Dists
-operator|==
-operator|(
-name|_DIST_USER
-operator||
+argument_list|,
 name|DIST_XF86
-operator|)
+argument_list|)
 operator|)
 return|;
 block|}
