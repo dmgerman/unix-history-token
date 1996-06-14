3fba410b14dcc892518cc780bd432d58e03e0998
Nate Williams <nate@FreeBSD.org>
1996-06-14 22:11:38 +0000

    At long last, we know have support for the 3C589 in a FreeBSD release using the existing files using the existing PCCARD support.  Now that this is in place I would like to fixup the PCCARD hooks and remove the if_zp driver.  At this point, we support everything we used to support *AND MORE* with the PCCARD code.
    Submitted by:	Naoki Hamada <nao@sbl.cl.nec.co.jp> (via the Nomad release)
    
    [ This works on both my 3C589B and 3C589C ]
    
    
    Former-commit-id: d3b82c518ae49667d502546283f0338ec94b57fb

--- sys/dev/ep/if_epreg.h
+++ sys/dev/ep/if_epreg.h
@@ -4,7 +4,11 @@ comment|/*  * Copyright (c) 1993 Herb Peyerl (hpeyerl@novatel.ca) All rights res
 end_comment
 
 begin_comment
-comment|/*  *  $Id: if_epreg.h,v 1.12 1996/02/26 01:05:36 gibbs Exp $  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
+comment|/*  *  $Id: if_epreg.h,v 1.13 1996/02/28 17:19:04 gibbs Exp $  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
+end_comment
+
+begin_comment
+comment|/*  * Pccard support for 3C589 by:  *		HAMADA Naoki  *		nao@tom-yam.or.jp  */
 end_comment
 
 begin_comment
@@ -89,6 +93,10 @@ name|int
 name|stat
 decl_stmt|;
 comment|/* some flags */
+name|int
+name|gone
+decl_stmt|;
+comment|/* adapter is not present (for PCCARD) */
 define|#
 directive|define
 name|F_RX_FIRST
