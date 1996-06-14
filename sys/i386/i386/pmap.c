f65f0ffffa918b28987f0687b8a43769c921c912
David Greenman <dg@FreeBSD.org>
1996-06-14 04:34:35 +0000

    Brought in a fix from main branch: close a window in pmap_object_init_pt() that has to do with the PG_MAPPED flag not being set at the right time, and PG_BUSY the page rather than doing a vm_page_hold/unhold. This fixes a potential problem where some mapped pages might go unnoticed by other parts of the vm system, which can be a real problem if the page(s) are reclaimed. Reviewed by:	dyson
    Former-commit-id: ecb6d6c0caea0b7e31f3f30ec9a8d8ded38c1a78

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	$Id: pmap.c,v 1.58.4.1 1995/09/12 05:36:42 davidg Exp $  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	$Id: pmap.c,v 1.58.4.4 1996/06/09 08:54:57 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -5119,16 +5119,11 @@ operator|==
 literal|0
 condition|)
 block|{
-name|vm_page_hold
-argument_list|(
-name|p
-argument_list|)
-expr_stmt|;
 name|p
 operator|->
 name|flags
 operator||=
-name|PG_MAPPED
+name|PG_BUSY
 expr_stmt|;
 name|pmap_enter_quick
 argument_list|(
@@ -5144,7 +5139,13 @@ name|p
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|vm_page_unhold
+name|p
+operator|->
+name|flags
+operator||=
+name|PG_MAPPED
+expr_stmt|;
+name|PAGE_WAKEUP
 argument_list|(
 name|p
 argument_list|)
@@ -5250,16 +5251,11 @@ operator|==
 literal|0
 condition|)
 block|{
-name|vm_page_hold
-argument_list|(
-name|p
-argument_list|)
-expr_stmt|;
 name|p
 operator|->
 name|flags
 operator||=
-name|PG_MAPPED
+name|PG_BUSY
 expr_stmt|;
 name|pmap_enter_quick
 argument_list|(
@@ -5275,7 +5271,13 @@ name|p
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|vm_page_unhold
+name|p
+operator|->
+name|flags
+operator||=
+name|PG_MAPPED
+expr_stmt|;
+name|PAGE_WAKEUP
 argument_list|(
 name|p
 argument_list|)
