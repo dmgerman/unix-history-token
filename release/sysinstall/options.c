614bc479535c455d701c77fc3449bb039c4436bf
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-06-14 14:34:03 +0000

    Don't use some of the low-contrast text attributes I was using before - they apparently look bad on some LCD screens.  While I'm in there, tweak and adjust a number of other minor interface details which have been bothering me for awhile.
    Former-commit-id: 14ad74935fc2d5fba7184af25670d42e449a7af0

--- release/sysinstall/options.c
+++ release/sysinstall/options.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated for what's essentially a complete rewrite.  *  * $Id: options.c,v 1.37 1996/06/08 08:01:52 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last attempt in the `sysinstall' line, the next  * generation being slated for what's essentially a complete rewrite.  *  * $Id: options.c,v 1.38 1996/06/08 09:08:45 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -56,7 +56,7 @@ operator|!
 name|cp
 condition|)
 return|return
-literal|"<default setting>"
+literal|"NO"
 return|;
 return|return
 name|cp
@@ -434,7 +434,7 @@ name|OPT_IS_VAR
 block|,
 name|PKG_PROMPT
 block|,
-literal|"PKG_TMPDIR"
+name|VAR_PKG_TMPDIR
 block|,
 name|varCheck
 block|}
@@ -845,7 +845,7 @@ name|mvprintw
 argument_list|(
 name|OPT_END_ROW
 operator|+
-literal|0
+literal|1
 argument_list|,
 literal|0
 argument_list|,
@@ -856,7 +856,7 @@ name|mvprintw
 argument_list|(
 name|OPT_END_ROW
 operator|+
-literal|1
+literal|2
 argument_list|,
 literal|0
 argument_list|,
@@ -967,14 +967,14 @@ expr_stmt|;
 block|}
 name|attrset
 argument_list|(
-name|item_attr
+name|ATTR_TITLE
 argument_list|)
 expr_stmt|;
 name|mvaddstr
 argument_list|(
 name|OPT_END_ROW
 operator|+
-literal|3
+literal|4
 argument_list|,
 literal|0
 argument_list|,
@@ -1044,9 +1044,25 @@ operator|--
 name|currOpt
 expr_stmt|;
 else|else
-name|beep
-argument_list|()
-expr_stmt|;
+for|for
+control|(
+name|currOpt
+operator|=
+literal|0
+init|;
+name|Options
+index|[
+name|currOpt
+operator|+
+literal|1
+index|]
+operator|.
+name|name
+condition|;
+name|currOpt
+operator|++
+control|)
+empty_stmt|;
 continue|continue;
 case|case
 name|KEY_DOWN
@@ -1066,8 +1082,9 @@ operator|++
 name|currOpt
 expr_stmt|;
 else|else
-name|beep
-argument_list|()
+name|currOpt
+operator|=
+literal|0
 expr_stmt|;
 continue|continue;
 case|case
