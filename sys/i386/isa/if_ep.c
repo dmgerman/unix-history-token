c8b114c6328282676333f6be74ba80777d2ddf95
Nate Williams <nate@FreeBSD.org>
1996-06-15 00:43:03 +0000

    Whoops, I accidentally deleted a necessary ":" which is part of the parameters to printf() using the "D" format.  (Why this even worked on my box during testing I don't know, but as soon as I powered it on/off it quite working.)
    Former-commit-id: a58d8a42b4cf7db82ffd6aa84ad920f22f07f096

--- sys/i386/isa/if_ep.c
+++ sys/i386/isa/if_ep.c
@@ -8,7 +8,7 @@ comment|/*  *	Modified from the FreeBSD 1.1.5.1 version by:  *		 	Andres Vega Ga
 end_comment
 
 begin_comment
-comment|/*  *  $Id: if_ep.c,v 1.46 1996/06/14 21:28:35 nate Exp $  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
+comment|/*  *  $Id: if_ep.c,v 1.47 1996/06/14 22:11:35 nate Exp $  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
 end_comment
 
 begin_comment
@@ -3074,6 +3074,8 @@ operator|->
 name|arpcom
 operator|.
 name|ac_enaddr
+argument_list|,
+literal|":"
 argument_list|)
 expr_stmt|;
 name|ifp
