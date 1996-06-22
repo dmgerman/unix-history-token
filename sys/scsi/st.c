37f3a58e24c00042beea03106e520531e5c0a2da
Joerg Wunsch <joerg@FreeBSD.org>
1996-06-22 15:00:00 +0000

    Merge the fix from rev 1.69: bump the timeout for st_erase().
    Former-commit-id: c617258ed9b463b6bbbc956f13ff1f48b906b7dd

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.36.4.2 1996/01/05 06:19:36 pst Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.36.4.3 1996/04/01 00:26:16 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -7992,9 +7992,15 @@ name|immed
 condition|?
 literal|5000
 else|:
-literal|300000
+operator|(
+literal|200
+operator|*
+literal|60
+operator|*
+literal|1000
+operator|)
 argument_list|,
-comment|/* 5 sec or 5 min */
+comment|/* 5 sec or 200 min */
 name|NULL
 argument_list|,
 name|flags
