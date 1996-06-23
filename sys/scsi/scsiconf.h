8a5f66820f05324ff410e6f36c4d8e7ffb69e837
Joerg Wunsch <joerg@FreeBSD.org>
1996-06-23 14:48:39 +0000

    Merge the worm driver into -stable, untested, but eyeball-reviewed.
    The existing worm driver there was entirely useless, you couldn't do
    anything with it.  Thus i believe that shipping a beta-quality driver
    is still better.  (The only other option would have been to remove it
    from the branch.)
    
    
    Former-commit-id: b3688a31594d297841fd773fb8c5bd198d56de7a

--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.26.4.4 1996/03/31 18:10:25 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.26.4.5 1996/04/01 00:26:12 gibbs Exp $  */
 end_comment
 
 begin_ifndef
@@ -2452,6 +2452,13 @@ end_comment
 begin_define
 define|#
 directive|define
+name|SCSI_FIXED_MASK
+value|0x40000000
+end_define
+
+begin_define
+define|#
+directive|define
 name|SCSI_FIXED
 parameter_list|(
 name|DEV
@@ -2462,8 +2469,8 @@ end_define
 begin_define
 define|#
 directive|define
-name|SCSI_FIXED_MASK
-value|0x40000000
+name|SCSI_CONTROL_MASK
+value|0x20000000
 end_define
 
 begin_define
@@ -2473,7 +2480,7 @@ name|SCSI_CONTROL
 parameter_list|(
 name|DEV
 parameter_list|)
-value|(((DEV)& 0x20000000))
+value|(((DEV)& SCSI_CONTROL_MASK))
 end_define
 
 begin_define
