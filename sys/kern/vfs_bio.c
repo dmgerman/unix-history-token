ec3f3f1f99f410d32d87416f77280c181da3e543
David Greenman <dg@FreeBSD.org>
1996-06-30 05:23:41 +0000

    Brought in fix from rev 1.94: make sure pages involved in I/O are not mapped into any processes. Also set b_pages[]=0 when the pages are removed from the buffer - this is to satisfy my paranoia by making sure that nothing bogusly messes with the pages after the buffer has been reclaimed. A similar change was made in the main branch some time ago.
    Former-commit-id: aef1c821dfd2b5c08d4ba6fe7ea7101ec50de8ae

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.46.4.7 1995/11/19 01:42:34 davidg Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.46.4.8 1996/01/07 00:03:00 davidg Exp $  */
 end_comment
 
 begin_comment
@@ -2138,6 +2138,15 @@ expr_stmt|;
 block|}
 block|}
 block|}
+name|bp
+operator|->
+name|b_pages
+index|[
+name|i
+index|]
+operator|=
+literal|0
+expr_stmt|;
 block|}
 name|bufspace
 operator|-=
@@ -6524,18 +6533,18 @@ operator|->
 name|busy
 operator|++
 expr_stmt|;
+name|vm_page_protect
+argument_list|(
+name|m
+argument_list|,
+name|VM_PROT_NONE
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|clear_modify
 condition|)
 block|{
-name|vm_page_protect
-argument_list|(
-name|m
-argument_list|,
-name|VM_PROT_READ
-argument_list|)
-expr_stmt|;
 name|vm_page_set_valid
 argument_list|(
 name|m
