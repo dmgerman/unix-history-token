a76a26fcca0cea4b9f5f9537fab3226947598b00
David Greenman <dg@FreeBSD.org>
1996-06-30 05:17:08 +0000

    Fixed a major bug that caused various pmap related panics, hangs, and reboots.
    The i386 pmap module uses a special area of kernel virtual memory for mapping
    of page tables pages when it needs to modify another process's virtual
    address space. It's called the 'alternate page table map'. There is only one
    of them and it's expected that only one process will be using it at once and
    that the operation is atomic.
    When the merged VM/buffer cache was implemented over a year ago, it became
    necessary to rundown VM pages at I/O completion. The unfortunate and
    unforeseen side effect of this is that pmap functions are now called at bio
    interrupt time. If there happend to be a process using the alternate page
    table map when this I/O completion occurred, it was possible for a different
    process's address space to be switched into the alternate page table map -
    leaving the current pmap process with the wrong address space mapped when
    the interrupt completed. This resulted in BAD things happening like pages
    being mapped or removed from the wrong address space, etc.. Since a very
    common case of a process modifying another process's address space is during
    fork when the kernel stack is inserted, one of the most common manifestations
    of this bug was the kernel stack not being mapped properly, resulting in a
    silent hang or reboot. This made it VERY difficult to troubleshoot this bug
    (I've been trying to figure out the cause of this for >6 months). Fortunately,
    the set of conditions that must be true before this problem occurs is
    sufficiently rare enough that most people never saw the bug occur. As I/O
    rates increase, however, so does the frequency of the crashes. This problem
    used to kill wcarchive about every 10 days, but in more recent times when
    the traffic exceeded >100GB/day, the machine could barely manage 6 hours of
    uptime.
    The fix is to make certain that no process has the pages mapped that are
    involved in the I/O, before the I/O is started. The pages are made busy, so
    no process will be able to map them, either, until the I/O has finished.
    This side-steps the issue by still allowing the pmap functions to be called
    at interrupt time, but also assuring that the alternate page table map won't
    be switched.
    Unfortunately, this appears to not be the only cause of this problem. :-(
    
    Reviewed by:	dyson
    
    
    Former-commit-id: f56440191826ce229439902b13204884fdd3d50c

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.92 1996/05/31 00:41:37 dyson Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.93 1996/06/14 11:01:27 asami Exp $  */
 end_comment
 
 begin_comment
@@ -7581,18 +7581,18 @@ name|busy
 operator|++
 expr_stmt|;
 block|}
+name|vm_page_protect
+argument_list|(
+name|m
+argument_list|,
+name|VM_PROT_NONE
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|clear_modify
 condition|)
 block|{
-name|vm_page_protect
-argument_list|(
-name|m
-argument_list|,
-name|VM_PROT_READ
-argument_list|)
-expr_stmt|;
 name|vm_page_set_validclean
 argument_list|(
 name|m
