54e62ae7cc906bac30d77d74b59836c769fbcf06
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-07-03 01:31:18 +0000

    Merge with HEAD.
    Former-commit-id: 47a67bc11934a678c8880b2052c197bf3e69a3e7

--- release/sysinstall/install.c
+++ release/sysinstall/install.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: install.c,v 1.71.2.93 1996/06/26 09:31:50 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: install.c,v 1.71.2.94 1996/06/29 03:50:24 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -1280,6 +1280,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuDistributions
+argument_list|,
+name|FALSE
 argument_list|)
 operator|&&
 operator|!
@@ -1309,6 +1311,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuMedia
+argument_list|,
+name|FALSE
 argument_list|)
 operator|||
 operator|!
@@ -1471,6 +1475,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuDistributions
+argument_list|,
+name|FALSE
 argument_list|)
 operator|&&
 operator|!
@@ -1516,6 +1522,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuMedia
+argument_list|,
+name|FALSE
 argument_list|)
 operator|||
 operator|!
@@ -1723,6 +1731,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuSyscons
+argument_list|,
+name|FALSE
 argument_list|)
 expr_stmt|;
 name|restorescr
@@ -1781,6 +1791,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuMouse
+argument_list|,
+name|FALSE
 argument_list|)
 expr_stmt|;
 name|restorescr
@@ -2154,6 +2166,8 @@ name|dmenuOpenSimple
 argument_list|(
 operator|&
 name|MenuConfigure
+argument_list|,
+name|FALSE
 argument_list|)
 expr_stmt|;
 name|restorescr
@@ -3410,13 +3424,6 @@ argument_list|(
 name|VAR_BROWSER_BINARY
 argument_list|,
 literal|"/usr/local/bin/lynx"
-argument_list|)
-expr_stmt|;
-name|variable_set2
-argument_list|(
-name|VAR_CONFIG_FILE
-argument_list|,
-literal|"freebsd.cfg"
 argument_list|)
 expr_stmt|;
 name|variable_set2
