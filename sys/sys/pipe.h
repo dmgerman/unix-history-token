e95d7c9c18173dbadfb74200a3013d37415adf3e
Bruce Evans <bde@FreeBSD.org>
1996-07-12 05:44:30 +0000

    Removed obsolete definition of PIPE_NBIO.
    Former-commit-id: fc786a3eb4e48338c91d60c8ab2fddca8c840736

--- sys/sys/pipe.h
+++ sys/sys/pipe.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: pipe.h,v 1.3 1996/02/04 19:56:14 dyson Exp $  */
+comment|/*  * Copyright (c) 1996 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: pipe.h,v 1.4 1996/05/01 04:18:20 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -198,17 +198,6 @@ end_struct
 
 begin_comment
 comment|/*  * Bits in pipe_state.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|PIPE_NBIO
-value|0x001
-end_define
-
-begin_comment
-comment|/* Non-blocking I/O. */
 end_comment
 
 begin_define
