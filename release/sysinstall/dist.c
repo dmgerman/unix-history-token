940b2990c6d96b86938da1c4bdda95a72192befc
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-07-13 05:45:41 +0000

    Mergicate.
    Former-commit-id: 36a2e4e6292886ffe4775a82a7b0cf7275f71526

--- release/sysinstall/dist.c
+++ release/sysinstall/dist.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: dist.c,v 1.36.2.37 1996/07/10 11:48:05 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: dist.c,v 1.67 1996/07/13 05:44:51 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -2307,6 +2307,22 @@ name|total
 operator|=
 literal|0
 expr_stmt|;
+operator|(
+name|void
+operator|)
+name|gettimeofday
+argument_list|(
+operator|&
+name|start
+argument_list|,
+operator|(
+expr|struct
+name|timezone
+operator|*
+operator|)
+literal|0
+argument_list|)
+expr_stmt|;
 comment|/* We have one or more chunks, go pick them up */
 name|mediaExtractDistBegin
 argument_list|(
@@ -2346,8 +2362,6 @@ control|)
 block|{
 name|int
 name|n
-decl_stmt|,
-name|chunktotal
 decl_stmt|,
 name|retval
 decl_stmt|;
@@ -2402,26 +2416,6 @@ argument_list|,
 name|numchunks
 argument_list|,
 name|buf
-argument_list|)
-expr_stmt|;
-name|chunktotal
-operator|=
-literal|0
-expr_stmt|;
-operator|(
-name|void
-operator|)
-name|gettimeofday
-argument_list|(
-operator|&
-name|start
-argument_list|,
-operator|(
-expr|struct
-name|timezone
-operator|*
-operator|)
-literal|0
 argument_list|)
 expr_stmt|;
 name|fd
@@ -2539,10 +2533,6 @@ break|break;
 name|total
 operator|+=
 name|n
-operator|,
-name|chunktotal
-operator|+=
-name|n
 expr_stmt|;
 comment|/* Print statistics about how we're doing */
 operator|(
@@ -2629,7 +2619,7 @@ literal|1
 expr_stmt|;
 name|msgInfo
 argument_list|(
-literal|"%10d bytes read from %s dist, chunk %2d of %2d @ %4.1f KB/sec."
+literal|"%10d bytes read from %s dist, chunk %2d of %2d @ %.1f KB/sec."
 argument_list|,
 name|total
 argument_list|,
@@ -2642,7 +2632,7 @@ argument_list|,
 name|numchunks
 argument_list|,
 operator|(
-name|chunktotal
+name|total
 operator|/
 name|seconds
 operator|)
