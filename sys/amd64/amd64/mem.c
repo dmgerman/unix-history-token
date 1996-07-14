1a22a2fa882aa8e9f8df6e3a56c15c25148a56ea
John Dyson <dyson@FreeBSD.org>
1996-07-14 06:05:53 +0000

    Almost gratuitious improvement of the performance of reading /dev/zero.
    Former-commit-id: 3cdb3be49bc64cb5de0cb7cff2486d43faf58a51

--- sys/amd64/amd64/mem.c
+++ sys/amd64/amd64/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.33 1996/04/07 14:59:26 bde Exp $  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.34 1996/05/02 10:43:05 phk Exp $  */
 end_comment
 
 begin_comment
@@ -526,6 +526,12 @@ begin_comment
 comment|/* poor name! */
 end_comment
 
+begin_decl_stmt
+name|caddr_t
+name|zbuf
+decl_stmt|;
+end_decl_stmt
+
 begin_function
 specifier|static
 name|int
@@ -611,6 +617,26 @@ operator|~
 name|PSL_IOPL
 expr_stmt|;
 break|break;
+case|case
+literal|12
+case|:
+if|if
+condition|(
+name|zbuf
+condition|)
+block|{
+name|free
+argument_list|(
+name|zbuf
+argument_list|,
+name|M_TEMP
+argument_list|)
+expr_stmt|;
+name|zbuf
+operator|=
+name|NULL
+expr_stmt|;
+block|}
 default|default:
 break|break;
 block|}
@@ -1331,12 +1357,12 @@ break|break;
 block|}
 if|if
 condition|(
-name|buf
+name|zbuf
 operator|==
 name|NULL
 condition|)
 block|{
-name|buf
+name|zbuf
 operator|=
 operator|(
 name|caddr_t
@@ -1352,7 +1378,7 @@ argument_list|)
 expr_stmt|;
 name|bzero
 argument_list|(
-name|buf
+name|zbuf
 argument_list|,
 name|PAGE_SIZE
 argument_list|)
@@ -1373,7 +1399,7 @@ name|error
 operator|=
 name|uiomove
 argument_list|(
-name|buf
+name|zbuf
 argument_list|,
 operator|(
 name|int
