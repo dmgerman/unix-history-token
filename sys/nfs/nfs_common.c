7cfcba27f8c0795f61a96e873d2748ae83fcace4
Doug Rabson <dfr@FreeBSD.org>
1996-07-16 10:19:45 +0000

    Various fixes from frank@fwi.uva.nl (Frank van der Linden) via rick@snowhite.cis.uoguelph.ca:
    1. Clear B_NEEDCOMMIT in nfs_write to make sure that dirty data is
    correctly send to the server.  If a buffer was dirtied when it was in
    the B_DELWRI+B_NEEDCOMMIT state, the state of the buffer was left
    unchanged and when the buffer was later cleaned, just a commit rpc was
    made to the server to complete the previous write.  Clearing
    B_NEEDCOMMIT ensures that another write is made to the server.
    
    2. If a server returned a server (for whatever reason) returned an
    answer to a write RPC that implied that fewer bytes than requested
    were written, bad things would happen.
    
    3. The setattr operation passed on the atime in stead of the mtime to
    the server. The fix is trivial.
    
    4. XIDs always started at 0, but this caused some servers (older DEC
    OSF/1 3.0 so I've been told) who had very long-lasting XID caches to
    get confused if, after a reboot of a BSD client, RPCs came in with a
    XID that had in the past been used before from that client. Patch is
    to use the current time in seconds as a starting point for XIDs. The
    patch below is not perfect, because it requires the root fs to be
    mounted first. This is because of the check BSD systems do, comparing
    FS time to system time.
    
    Reviewed by:	Bruce Evans, Terry Lambert.
    Obtained from:  frank@fwi.uva.nl (Frank van der Linden) via rick@snowhite.cis.uoguelph.ca
    
    
    Former-commit-id: 683808d8d56126550336976b3e76e91b361a92f1

--- sys/nfs/nfs_common.c
+++ sys/nfs/nfs_common.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfs_subs.c	8.3 (Berkeley) 1/4/94  * $Id: nfs_subs.c,v 1.29 1996/06/14 11:13:21 phk Exp $  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfs_subs.c	8.3 (Berkeley) 1/4/94  * $Id: nfs_subs.c,v 1.30 1996/06/23 17:19:25 bde Exp $  */
 end_comment
 
 begin_comment
@@ -1963,6 +1963,14 @@ name|grpsiz
 decl_stmt|,
 name|authsiz
 decl_stmt|;
+name|struct
+name|timeval
+name|tv
+decl_stmt|;
+specifier|static
+name|u_long
+name|base
+decl_stmt|;
 name|authsiz
 operator|=
 name|nfsm_rndup
@@ -2070,6 +2078,37 @@ operator|*
 name|NFSX_UNSIGNED
 argument_list|)
 expr_stmt|;
+comment|/* 	 * derive initial xid from system time 	 * XXX time is invalid if root not yet mounted 	 */
+if|if
+condition|(
+operator|!
+name|base
+operator|&&
+operator|(
+name|rootvp
+operator|)
+condition|)
+block|{
+name|microtime
+argument_list|(
+operator|&
+name|tv
+argument_list|)
+expr_stmt|;
+name|base
+operator|=
+name|tv
+operator|.
+name|tv_sec
+operator|<<
+literal|12
+expr_stmt|;
+name|nfs_xid
+operator|=
+name|base
+expr_stmt|;
+block|}
+comment|/* 	 * Skip zero xid if it should ever happen. 	 */
 if|if
 condition|(
 operator|++
@@ -3134,7 +3173,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * copies a uio scatter/gather list to an mbuf chain...  */
+comment|/*  * copies a uio scatter/gather list to an mbuf chain.  * NOTE: can ony handle iovcnt == 1  */
 end_comment
 
 begin_function
@@ -3204,6 +3243,19 @@ name|cp
 decl_stmt|;
 if|if
 condition|(
+name|uiop
+operator|->
+name|uio_iovcnt
+operator|!=
+literal|1
+condition|)
+name|panic
+argument_list|(
+literal|"nfsm_uiotombuf: iovcnt != 1"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
 name|siz
 operator|>
 name|MLEN
@@ -3241,25 +3293,6 @@ operator|>
 literal|0
 condition|)
 block|{
-if|if
-condition|(
-name|uiop
-operator|->
-name|uio_iovcnt
-operator|<=
-literal|0
-operator|||
-name|uiop
-operator|->
-name|uio_iov
-operator|==
-name|NULL
-condition|)
-return|return
-operator|(
-name|EINVAL
-operator|)
-return|;
 name|left
 operator|=
 name|uiop
@@ -3483,30 +3516,6 @@ operator|-=
 name|xfer
 expr_stmt|;
 block|}
-if|if
-condition|(
-name|uiop
-operator|->
-name|uio_iov
-operator|->
-name|iov_len
-operator|<=
-name|siz
-condition|)
-block|{
-name|uiop
-operator|->
-name|uio_iovcnt
-operator|--
-expr_stmt|;
-name|uiop
-operator|->
-name|uio_iov
-operator|++
-expr_stmt|;
-block|}
-else|else
-block|{
 name|uiop
 operator|->
 name|uio_iov
@@ -3523,7 +3532,6 @@ name|iov_len
 operator|-=
 name|uiosiz
 expr_stmt|;
-block|}
 name|siz
 operator|-=
 name|uiosiz
