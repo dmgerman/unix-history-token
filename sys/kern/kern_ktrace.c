6fdb5fb6259c8ba0b0de74febb989b5d8cc24de5
Poul-Henning Kamp <phk@FreeBSD.org>
1996-08-04 20:13:08 +0000

    Add separate kmalloc classes for BIO buffers and Ktrace info.
    Former-commit-id: 0ae661db23c94ec928b3365ea471aae48aa29ad1

--- sys/kern/kern_ktrace.c
+++ sys/kern/kern_ktrace.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_ktrace.c	8.2 (Berkeley) 9/23/93  * $Id: kern_ktrace.c,v 1.10 1996/01/03 21:42:04 wollman Exp $  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_ktrace.c	8.2 (Berkeley) 9/23/93  * $Id: kern_ktrace.c,v 1.11 1996/03/11 06:03:23 hsu Exp $  */
 end_comment
 
 begin_include
@@ -224,7 +224,7 @@ expr|struct
 name|ktr_header
 argument_list|)
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|,
 name|M_WAITOK
 argument_list|)
@@ -369,7 +369,7 @@ operator|*
 argument_list|,
 name|len
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|,
 name|M_WAITOK
 argument_list|)
@@ -454,14 +454,14 @@ name|FREE
 argument_list|(
 name|ktp
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|FREE
 argument_list|(
 name|kth
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|p
@@ -585,7 +585,7 @@ name|FREE
 argument_list|(
 name|kth
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|p
@@ -668,7 +668,7 @@ name|FREE
 argument_list|(
 name|kth
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|p
@@ -801,7 +801,7 @@ argument_list|)
 operator|+
 name|len
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|,
 name|M_WAITOK
 argument_list|)
@@ -925,14 +925,14 @@ name|FREE
 argument_list|(
 name|kth
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|FREE
 argument_list|(
 name|ktp
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|p
@@ -1064,7 +1064,7 @@ name|FREE
 argument_list|(
 name|kth
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|p
@@ -1171,7 +1171,7 @@ name|FREE
 argument_list|(
 name|kth
 argument_list|,
-name|M_TEMP
+name|M_KTRACE
 argument_list|)
 expr_stmt|;
 name|p
