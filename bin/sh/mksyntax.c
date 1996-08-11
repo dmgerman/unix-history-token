0960273378921bdec8e4f4766a82d09215841e19
Andrey Chernov <ache@FreeBSD.org>
1996-08-11 22:51:00 +0000

    Localize it
    Former-commit-id: f07c1b6041774e4f0e64fcc62003b6bf9c853148

--- bin/sh/mksyntax.c
+++ bin/sh/mksyntax.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mksyntax.c,v 1.2 1994/09/24 02:57:57 davidg Exp $  */
 end_comment
 
 begin_ifndef
@@ -512,6 +512,13 @@ operator|=
 literal|0
 expr_stmt|;
 block|}
+name|fputs
+argument_list|(
+literal|"#include<ctype.h>\n"
+argument_list|,
+name|hfile
+argument_list|)
+expr_stmt|;
 name|fputs
 argument_list|(
 literal|"#include<sys/cdefs.h>\n"
@@ -1482,11 +1489,11 @@ init|=
 block|{
 literal|"#define is_digit(c)\t((is_type+SYNBASE)[c]& ISDIGIT)"
 block|,
-literal|"#define is_alpha(c)\t((is_type+SYNBASE)[c]& (ISUPPER|ISLOWER))"
+literal|"#define is_alpha(c)\t((c) != PEOF&& isalpha((unsigned char) (c)))"
 block|,
-literal|"#define is_name(c)\t((is_type+SYNBASE)[c]& (ISUPPER|ISLOWER|ISUNDER))"
+literal|"#define is_name(c)\t((c) != PEOF&& ((c) == '_' || isalpha((unsigned char) (c))))"
 block|,
-literal|"#define is_in_name(c)\t((is_type+SYNBASE)[c]& (ISUPPER|ISLOWER|ISUNDER|ISDIGIT))"
+literal|"#define is_in_name(c)\t((c) != PEOF&& ((c) == '_' || isdigit((unsigned char) (c)) || isalpha((unsigned char) (c))))"
 block|,
 literal|"#define is_special(c)\t((is_type+SYNBASE)[c]& (ISSPECL|ISDIGIT))"
 block|,
