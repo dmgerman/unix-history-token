a86930ba9d0947b4ee12477aa5d24059c9c39c3f
Paul Traina <pst@FreeBSD.org>
1996-08-19 20:28:25 +0000

    Update to match definitions in LBL June 96 release
    Former-commit-id: 56b3d4327ae602044c59ac7870a37fd0007ba91f

--- sys/net/bpf.h
+++ sys/net/bpf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from the Stanford/CMU enet packet filter,  * (net/enet.c) distributed as part of 4.3BSD, and code contributed  * to Berkeley by Steven McCanne and Van Jacobson both of Lawrence  * Berkeley Laboratory.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      @(#)bpf.h	8.1 (Berkeley) 6/10/93  *  * $Id: bpf.h,v 1.8 1996/01/30 22:57:38 mpp Exp $  */
+comment|/*  * Copyright (c) 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from the Stanford/CMU enet packet filter,  * (net/enet.c) distributed as part of 4.3BSD, and code contributed  * to Berkeley by Steven McCanne and Van Jacobson both of Lawrence  * Berkeley Laboratory.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      @(#)bpf.h	8.1 (Berkeley) 6/10/93  *	@(#)bpf.h	1.34 (LBL)     6/16/96  *  * $Id: bpf.h,v 1.9 1996/02/06 18:51:06 wollman Exp $  */
 end_comment
 
 begin_ifndef
@@ -16,6 +16,31 @@ name|_NET_BPF_H_
 end_define
 
 begin_comment
+comment|/* BSD style release date */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|BPF_RELEASE
+value|199606
+end_define
+
+begin_typedef
+typedef|typedef
+name|int32_t
+name|bpf_int32
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+name|u_int32_t
+name|bpf_u_int32
+typedef|;
+end_typedef
+
+begin_comment
 comment|/*  * Alignment macros.  BPF_WORDALIGN rounds up to the next  * even multiple of BPF_ALIGNMENT.  */
 end_comment
 
@@ -23,7 +48,7 @@ begin_define
 define|#
 directive|define
 name|BPF_ALIGNMENT
-value|sizeof(long)
+value|sizeof(bpf_int32)
 end_define
 
 begin_define
@@ -116,7 +141,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/* Current version number. */
+comment|/* Current version number of filter architecture. */
 end_comment
 
 begin_define
@@ -251,11 +276,11 @@ name|timeval
 name|bh_tstamp
 decl_stmt|;
 comment|/* time stamp */
-name|u_long
+name|bpf_u_int32
 name|bh_caplen
 decl_stmt|;
 comment|/* length of captured portion */
-name|u_long
+name|bpf_u_int32
 name|bh_datalen
 decl_stmt|;
 comment|/* original length of packet */
@@ -290,7 +315,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * Data-link level type codes.  * Currently, only DLT_EN10MB and DLT_SLIP are supported.  */
+comment|/*  * Data-link level type codes.  */
 end_comment
 
 begin_define
@@ -412,6 +437,17 @@ end_define
 
 begin_comment
 comment|/* FDDI */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DLT_ATM_RFC1483
+value|11
+end_define
+
+begin_comment
+comment|/* LLC/SNAP encapsulated atm */
 end_comment
 
 begin_comment
@@ -777,7 +813,7 @@ decl_stmt|;
 name|u_char
 name|jf
 decl_stmt|;
-name|long
+name|bpf_u_int32
 name|k
 decl_stmt|;
 block|}
