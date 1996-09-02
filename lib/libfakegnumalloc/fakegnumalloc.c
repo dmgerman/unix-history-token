b55936ca4723833083849459c1a2994faef6cd55
Poul-Henning Kamp <phk@FreeBSD.org>
1996-09-02 13:15:01 +0000

    Add cfree, Move to usr/lib/compat
    Former-commit-id: 052836e7ec618a5096bc9e2dae2ad6a6140da7f4

--- lib/libfakegnumalloc/fakegnumalloc.c
+++ lib/libfakegnumalloc/fakegnumalloc.c
@@ -10,5 +10,22 @@ return|;
 block|}
 end_function
 
+begin_function
+name|void
+name|cfree
+parameter_list|(
+name|void
+modifier|*
+name|foo
+parameter_list|)
+block|{
+name|free
+argument_list|(
+name|foo
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
 end_unit
 
