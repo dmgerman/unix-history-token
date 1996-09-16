13427ff13e9d9fddebeb46b27c52b07b2423c05a
Rodney Grimes <rgrimes@FreeBSD.org>
1996-09-16 08:56:39 +0000

    Re-enable conf82371fb2 now that I have verified that it works, even if it only prints 2 bits out of hundreds.  (Minimizing the diff between -head and 2.1.5.)
    Former-commit-id: 8583700ab936d091523a3c2daef1536ef709d3d4

--- sys/pci/pcisupport.c
+++ sys/pci/pcisupport.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: pcisupport.c,v 1.38 1996/09/06 23:09:01 phk Exp $ ** **  Device driver for DEC/INTEL PCI chipsets. ** **  FreeBSD ** **------------------------------------------------------------------------- ** **  Written for FreeBSD by **	wolf@cologne.de 	Wolfgang Stanglmeier **	se@mi.Uni-Koeln.de	Stefan Esser ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994,1995 Stefan Esser.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: pcisupport.c,v 1.39 1996/09/09 06:09:45 rgrimes Exp $ ** **  Device driver for DEC/INTEL PCI chipsets. ** **  FreeBSD ** **------------------------------------------------------------------------- ** **  Written for FreeBSD by **	wolf@cologne.de 	Wolfgang Stanglmeier **	se@mi.Uni-Koeln.de	Stefan Esser ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994,1995 Stefan Esser.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
 end_comment
 
 begin_include
@@ -3931,31 +3931,83 @@ block|}
 decl_stmt|;
 end_decl_stmt
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
-begin_comment
-comment|/* xxx not used */
-end_comment
-
-begin_comment
-unit|static const struct condmsg conf82371fb2[] = {
+begin_decl_stmt
+specifier|static
+specifier|const
+name|struct
+name|condmsg
+name|conf82371fb2
+index|[]
+init|=
+block|{
 comment|/* IDETM -- IDE Timing Register */
-end_comment
-
-begin_comment
-unit|{ 0x00, 0x00, 0x00, M_TR, "\tPrimary IDE: " },     { 0x41, 0x80, 0x80, M_EN, 0 },     { 0x00, 0x00, 0x00, M_TR, "\n\tSecondary IDE: " },     { 0x43, 0x80, 0x80, M_EN, 0 },     { 0x00, 0x00, 0x00, M_TR, "\n" },
+block|{
+literal|0x00
+block|,
+literal|0x00
+block|,
+literal|0x00
+block|,
+name|M_TR
+block|,
+literal|"\tPrimary IDE: "
+block|}
+block|,
+block|{
+literal|0x41
+block|,
+literal|0x80
+block|,
+literal|0x80
+block|,
+name|M_EN
+block|,
+literal|0
+block|}
+block|,
+block|{
+literal|0x00
+block|,
+literal|0x00
+block|,
+literal|0x00
+block|,
+name|M_TR
+block|,
+literal|"\n\tSecondary IDE: "
+block|}
+block|,
+block|{
+literal|0x43
+block|,
+literal|0x80
+block|,
+literal|0x80
+block|,
+name|M_EN
+block|,
+literal|0
+block|}
+block|,
+block|{
+literal|0x00
+block|,
+literal|0x00
+block|,
+literal|0x00
+block|,
+name|M_TR
+block|,
+literal|"\n"
+block|}
+block|,
 comment|/* end of list */
-end_comment
-
-begin_endif
-unit|{ 0 } };
-endif|#
-directive|endif
-end_endif
+block|{
+literal|0
+block|}
+block|}
+decl_stmt|;
+end_decl_stmt
 
 begin_function
 specifier|static
@@ -4445,12 +4497,17 @@ name|conf82371fb
 argument_list|)
 expr_stmt|;
 break|break;
-if|#
-directive|if
-literal|0
-block|case 0x12308086: 		writeconfig (config_id, conf82371fb2); 		break;
-endif|#
-directive|endif
+case|case
+literal|0x12308086
+case|:
+name|writeconfig
+argument_list|(
+name|config_id
+argument_list|,
+name|conf82371fb2
+argument_list|)
+expr_stmt|;
+break|break;
 if|#
 directive|if
 literal|0
