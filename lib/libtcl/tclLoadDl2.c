c328fafa8164787e1ff9d6d1989df06503208870
Poul-Henning Kamp <phk@FreeBSD.org>
1996-09-18 14:29:21 +0000

    Update to tcl7.5u1. Add two scaffold files to (attempt) to make a tclsh compiled -static work.
    Former-commit-id: 9eb5656a8e3b8d1d32d02511eeb9a139c212d6ec

---
+++ lib/libtcl/tclLoadDl2.c
@@ -0,0 +1,24 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * XXX: We don't have dlopen& friends in statically linked programs  * XXX: so we avoid using them.  */
+end_comment
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|PIC
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|"../../../contrib/tcl/unix/tclLoadDl2.c"
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+end_unit
+
