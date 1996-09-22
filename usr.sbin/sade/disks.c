7cdcbb11e2cf1ef2a62147e245c3d70071047d5a
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-09-22 00:48:55 +0000

    Remove some gratutious screen saves.
    Former-commit-id: aecedd001d415fe9283fc3d6b5970d034d9557f3

--- usr.sbin/sade/disks.c
+++ usr.sbin/sade/disks.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: disks.c,v 1.62 1996/08/03 05:29:24 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: disks.c,v 1.63 1996/08/03 10:10:44 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -1161,13 +1161,6 @@ decl_stmt|;
 name|chunk_e
 name|partitiontype
 decl_stmt|;
-name|WINDOW
-modifier|*
-name|save
-init|=
-name|savescr
-argument_list|()
-decl_stmt|;
 name|snprintf
 argument_list|(
 name|tmp
@@ -1344,11 +1337,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|restorescr
-argument_list|(
-name|save
-argument_list|)
-expr_stmt|;
 block|}
 break|break;
 case|case
@@ -1595,22 +1583,12 @@ literal|"Are you absolutely sure you want to do this now?"
 argument_list|)
 condition|)
 block|{
-name|WINDOW
-modifier|*
-name|save
-init|=
-name|savescr
-argument_list|()
-decl_stmt|;
 name|variable_set2
 argument_list|(
 name|DISK_PARTITIONED
 argument_list|,
 literal|"yes"
 argument_list|)
-expr_stmt|;
-name|clear
-argument_list|()
 expr_stmt|;
 comment|/* Don't trash the MBR if the first (and therefore only) chunk is marked for a truly dedicated 		 * disk (i.e., the disklabel starts at sector 0), even in cases where the user has requested 		 * booteasy or a "standard" MBR -- both would be fatal in this case. 		 */
 if|if
@@ -1670,11 +1648,6 @@ else|else
 name|msgConfirm
 argument_list|(
 literal|"Wrote FDISK partition information out successfully."
-argument_list|)
-expr_stmt|;
-name|restorescr
-argument_list|(
-name|save
 argument_list|)
 expr_stmt|;
 block|}
