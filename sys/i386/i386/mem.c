34ca2fe8bf5fd2ddb1a500cf6a542312ee506b08
Peter Wemm <peter@FreeBSD.org>
1996-09-27 13:25:13 +0000

    I've been meaning to commit this for months.  Implement select() for /dev/random and /dev/urandom.  Both are always writable, urandom is always readable, and /dev/random is readable when >= 8 bits are in the pool.
    Former-commit-id: 7fbfbd76fd55a4b941f9b40008a58479299b39b5

--- sys/i386/i386/mem.c
+++ sys/i386/i386/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.36 1996/07/15 05:23:04 bde Exp $  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.37 1996/07/15 18:22:07 nate Exp $  */
 end_comment
 
 begin_comment
@@ -188,6 +188,13 @@ name|memmmap
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|static
+name|d_select_t
+name|mmselect
+decl_stmt|;
+end_decl_stmt
+
 begin_define
 define|#
 directive|define
@@ -220,7 +227,7 @@ block|,
 name|nodevtotty
 block|,
 comment|/* memory */
-name|seltrue
+name|mmselect
 block|,
 name|memmmap
 block|,
@@ -2125,6 +2132,69 @@ operator|(
 literal|0
 operator|)
 return|;
+block|}
+end_function
+
+begin_function
+name|int
+name|mmselect
+parameter_list|(
+name|dev
+parameter_list|,
+name|rw
+parameter_list|,
+name|p
+parameter_list|)
+name|dev_t
+name|dev
+decl_stmt|;
+name|int
+name|rw
+decl_stmt|;
+name|struct
+name|proc
+modifier|*
+name|p
+decl_stmt|;
+block|{
+switch|switch
+condition|(
+name|minor
+argument_list|(
+name|dev
+argument_list|)
+condition|)
+block|{
+case|case
+literal|3
+case|:
+comment|/* /dev/random */
+return|return
+name|random_select
+argument_list|(
+name|dev
+argument_list|,
+name|rw
+argument_list|,
+name|p
+argument_list|)
+return|;
+case|case
+literal|4
+case|:
+comment|/* /dev/urandom */
+default|default:
+return|return
+name|seltrue
+argument_list|(
+name|dev
+argument_list|,
+name|rw
+argument_list|,
+name|p
+argument_list|)
+return|;
+block|}
 block|}
 end_function
 
