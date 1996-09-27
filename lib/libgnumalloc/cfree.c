0191ab03f70c1f9d2567af7356aaf9a8d7f50253
Peter Wemm <peter@FreeBSD.org>
1996-09-27 06:58:05 +0000

    Repository copy libfakegnumalloc to libgnumalloc and remove the symlink stuff and detection for the "gnumalloc" port which doesn't exist and nobody has cared enough about for the past 6 months or so to implement.
    As has been pointed out to me (quite a few times) in email, the people
    that had been bitten by the changes had failed to follow the instructions
    about updating /etc/rc.
    
    Bruce also pointed out that after my last commit, it was no longer
    removing /usr/lib/libgnumalloc.so.2.0 as it should have been.
    
    Hopefully this (and the comments in the Makefile) should defuse the problem
    a bit.
    
    
    Former-commit-id: 03ed5e1b440cddc25edfdd8d39047822d07d38fd

--- lib/libgnumalloc/cfree.c
+++ lib/libgnumalloc/cfree.c
@@ -1,16 +1,5 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_function
-name|int
-name|fake_a_gnumalloc_lib
-parameter_list|()
-block|{
-return|return
-literal|1
-return|;
-block|}
-end_function
-
-begin_function
 name|void
 name|cfree
 parameter_list|(
