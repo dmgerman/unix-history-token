317960e7051c57a4d2bbe7c967c580bf6184ed50
Bruce Evans <bde@FreeBSD.org>
1996-09-29 18:35:07 +0000

    Added "memory" to clobber list in invlpg().  It needs it if invltlb() needs it.
    Fixed style in invlpg().
    
    Sorted recently renamed functions.
    
    Added prototypes in the non-gcc section for recently added/renamed
    functions.
    
    
    Former-commit-id: b7e2d94bca3a003100522f5ebd9e252148738872

--- sys/amd64/include/cpufunc.h
+++ sys/amd64/include/cpufunc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cpufunc.h,v 1.57 1996/09/28 04:22:46 dyson Exp $  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cpufunc.h,v 1.58 1996/09/28 22:37:57 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -453,6 +453,40 @@ end_function
 begin_function
 unit|}  static
 name|__inline
+name|void
+name|invlpg
+parameter_list|(
+name|u_int
+name|addr
+parameter_list|)
+block|{
+asm|__asm __volatile("invlpg (%0)" : : "r" (addr) : "memory");
+block|}
+end_function
+
+begin_function
+specifier|static
+name|__inline
+name|void
+name|invltlb
+parameter_list|(
+name|void
+parameter_list|)
+block|{
+name|u_long
+name|temp
+decl_stmt|;
+comment|/* 	 * This should be implemented as load_cr3(rcr3()) when load_cr3() 	 * is inlined. 	 */
+asm|__asm __volatile("movl %%cr3, %0; movl %0, %%cr3" : "=r" (temp)
+block|: :
+literal|"memory"
+block|)
+function|;
+end_function
+
+begin_function
+unit|}  static
+name|__inline
 name|u_short
 name|inw
 parameter_list|(
@@ -690,40 +724,6 @@ name|data
 parameter_list|)
 block|{
 asm|__asm __volatile("outw %0,%%dx" : : "a" (data), "d" (port));
-block|}
-end_function
-
-begin_function
-specifier|static
-name|__inline
-name|void
-name|invltlb
-parameter_list|(
-name|void
-parameter_list|)
-block|{
-name|u_long
-name|temp
-decl_stmt|;
-comment|/* 	 * This should be implemented as load_cr3(rcr3()) when load_cr3() 	 * is inlined. 	 */
-asm|__asm __volatile("movl %%cr3, %0; movl %0, %%cr3" : "=r" (temp)
-block|: :
-literal|"memory"
-block|)
-function|;
-end_function
-
-begin_function
-unit|}  static
-name|__inline
-name|void
-name|invlpg
-parameter_list|(
-name|u_long
-name|addr
-parameter_list|)
-block|{
-asm|__asm __volatile("invlpg (%0)": :"r"(addr));
 block|}
 end_function
 
@@ -1010,6 +1010,31 @@ name|addr
 operator|,
 name|size_t
 name|cnt
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|invlpg
+name|__P
+argument_list|(
+operator|(
+name|u_int
+name|addr
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|invltlb
+name|__P
+argument_list|(
+operator|(
+name|void
 operator|)
 argument_list|)
 decl_stmt|;
