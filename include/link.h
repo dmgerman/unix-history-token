49a42fc12ab8fecadcd60e3b88074df4ec27977b
Peter Wemm <peter@FreeBSD.org>
1996-10-01 00:25:32 +0000

    Add support for storing a -R path in ld in the section dispatch table, support LD_HINTS_VERSION_2 that has the ldconfig pathname stored in the ld.so.hints file (ie: a new library can be installed and used without needing to run ldconfig -m first)
    Reviewed by: nate, jdp
    Obtained from: NetBSD (mostly)
    
    
    Former-commit-id: ee39699307cba3481070b780a5995d03aa9599ac

--- include/link.h
+++ include/link.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: link.h,v 1.5 1995/06/27 09:52:59 dfr Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: link.h,v 1.6 1996/01/30 23:30:20 mpp Exp $  */
 end_comment
 
 begin_comment
@@ -239,9 +239,9 @@ name|sdt_sods
 decl_stmt|;
 comment|/* List of shared objects descriptors */
 name|long
-name|sdt_filler1
+name|sdt_paths
 decl_stmt|;
-comment|/* Unused (was: search rules) */
+comment|/* Library search paths */
 name|long
 name|sdt_got
 decl_stmt|;
@@ -712,6 +712,16 @@ end_define
 begin_define
 define|#
 directive|define
+name|LD_PATHS
+parameter_list|(
+name|x
+parameter_list|)
+value|((x)->d_un.d_sdt->sdt_paths)
+end_define
+
+begin_define
+define|#
+directive|define
 name|LD_GOTSZ
 parameter_list|(
 name|x
@@ -901,6 +911,10 @@ define|#
 directive|define
 name|LD_HINTS_VERSION_1
 value|1
+define|#
+directive|define
+name|LD_HINTS_VERSION_2
+value|2
 name|long
 name|hh_hashtab
 decl_stmt|;
@@ -921,6 +935,10 @@ name|long
 name|hh_ehints
 decl_stmt|;
 comment|/* End of hints (max offset in file) */
+name|long
+name|hh_dirlist
+decl_stmt|;
+comment|/* Colon-separated list of srch dirs */
 block|}
 struct|;
 end_struct
