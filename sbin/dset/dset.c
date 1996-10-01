eeeb70b1324d19207abe4dcbe0dafa5abbd240dc
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-10-01 14:42:36 +0000

    Add support for dset'ing an arbitrary kernel (experimental).
    Former-commit-id: f4ca0a833853997d0eee17661a8a27259782f160

--- sbin/dset/dset.c
+++ sbin/dset/dset.c
@@ -313,7 +313,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: %s [-qtv]\n"
+literal|"usage: %s [-qtv] [-k kernel]\n"
 argument_list|,
 name|title
 argument_list|)
@@ -403,6 +403,8 @@ specifier|const
 name|char
 modifier|*
 name|kernel
+init|=
+name|NULL
 decl_stmt|;
 specifier|extern
 name|char
@@ -433,7 +435,7 @@ name|ac
 argument_list|,
 name|av
 argument_list|,
-literal|"qtv"
+literal|"qtvk:"
 argument_list|)
 operator|)
 operator|!=
@@ -461,6 +463,14 @@ name|TRUE
 expr_stmt|;
 comment|/* In test mode we want to be verbose */
 case|case
+literal|'k'
+case|:
+name|kernel
+operator|=
+name|optarg
+expr_stmt|;
+break|break;
+case|case
 literal|'v'
 case|:
 name|verbose
@@ -486,6 +496,11 @@ literal|1
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+operator|!
+name|kernel
+condition|)
 name|kernel
 operator|=
 name|getbootfile
