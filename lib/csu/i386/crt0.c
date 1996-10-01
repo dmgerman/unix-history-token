e3382b6ec8fa33f016c6360514f126acc2b8203e
Peter Wemm <peter@FreeBSD.org>
1996-10-01 00:54:48 +0000

    Support crt0 <-> ld.so interface version 4.  This should be both backwards and forwards compatable with version 3.  This is needed to enable storing a run-time library path in the dynamic linking headers.  The crt startup tries version 4 first, and falls back to version 3, so an executable that is linked on -current will work with the ld.so on 2.1.x and less.
    Reviewed by: nate, jdp
    Obtained from: NetBSD
    
    
    Former-commit-id: cc117c790b9797686f5d2d57ae1c54eb88288a22

--- lib/csu/i386/crt0.c
+++ lib/csu/i386/crt0.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: crt0.c,v 1.21 1995/11/02 12:42:42 ache Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: crt0.c,v 1.23 1996/01/30 05:55:20 nate Exp $  */
 end_comment
 
 begin_include
@@ -1120,6 +1120,12 @@ name|crt_prog
 operator|=
 name|__progname
 expr_stmt|;
+name|crt
+operator|.
+name|crt_ldentry
+operator|=
+name|NULL
+expr_stmt|;
 name|entry
 operator|=
 operator|(
@@ -1145,12 +1151,51 @@ modifier|*
 name|entry
 call|)
 argument_list|(
+name|CRT_VERSION_BSD_4
+argument_list|,
+operator|&
+name|crt
+argument_list|)
+expr_stmt|;
+name|ld_entry
+operator|=
+name|crt
+operator|.
+name|crt_ldentry
+expr_stmt|;
+if|if
+condition|(
+name|ret
+operator|==
+operator|-
+literal|1
+operator|&&
+name|ld_entry
+operator|==
+name|NULL
+condition|)
+block|{
+comment|/* if version 4 not recognised, try version 3 */
+name|ret
+operator|=
+call|(
+modifier|*
+name|entry
+call|)
+argument_list|(
 name|CRT_VERSION_BSD_3
 argument_list|,
 operator|&
 name|crt
 argument_list|)
 expr_stmt|;
+name|ld_entry
+operator|=
+name|_DYNAMIC
+operator|.
+name|d_entry
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|ret
@@ -1166,9 +1211,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|_DYNAMIC
-operator|.
-name|d_entry
+name|ld_entry
 operator|!=
 name|NULL
 condition|)
@@ -1178,9 +1221,7 @@ modifier|*
 name|msg
 init|=
 call|(
-name|_DYNAMIC
-operator|.
-name|d_entry
+name|ld_entry
 operator|->
 name|dlerror
 call|)
@@ -1235,12 +1276,6 @@ literal|"\n"
 argument_list|)
 expr_stmt|;
 block|}
-name|ld_entry
-operator|=
-name|_DYNAMIC
-operator|.
-name|d_entry
-expr_stmt|;
 if|if
 condition|(
 name|ret
