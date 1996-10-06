a09938affd73b1ef7552fb8409474b3152d51a64
John Dyson <dyson@FreeBSD.org>
1996-10-06 22:26:13 +0000

    Make the default cache size optim to be 256K, the old default was 64K.  The change has essentially neutral effect on those machines with little or no cache, and has a positive effect on "normal" machines with 256K or more cache.
    Former-commit-id: b3df8c1b8e1f68c40ff801de9151dbd6e8c98037

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.h	8.2 (Berkeley) 12/13/93  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_page.h,v 1.31 1996/07/30 03:08:17 dyson Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.h	8.2 (Berkeley) 12/13/93  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_page.h,v 1.32 1996/09/08 20:44:46 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -198,75 +198,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|PQ_MEDIUMCACHE
-argument_list|)
-end_if
-
-begin_define
-define|#
-directive|define
-name|PQ_PRIME1
-value|13
-end_define
-
-begin_comment
-comment|/* Prime number somewhat less than PQ_HASH_SIZE */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|PQ_PRIME2
-value|7
-end_define
-
-begin_comment
-comment|/* Prime number somewhat less than PQ_HASH_SIZE */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|PQ_PRIME3
-value|5
-end_define
-
-begin_comment
-comment|/* Prime number somewhat less than PQ_HASH_SIZE */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|PQ_L2_SIZE
-value|64
-end_define
-
-begin_comment
-comment|/* A number of colors opt for 256K cache */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|PQ_L1_SIZE
-value|2
-end_define
-
-begin_comment
-comment|/* Two page L1 cache */
-end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_comment
 comment|/*  * Use 'options PQ_NOOPT' to disable page coloring  */
 end_comment
@@ -327,12 +258,6 @@ name|defined
 argument_list|(
 name|PQ_NORMALCACHE
 argument_list|)
-operator|||
-operator|!
-name|defined
-argument_list|(
-name|PQ_L2_SIZE
-argument_list|)
 end_if
 
 begin_define
@@ -377,6 +302,81 @@ end_define
 
 begin_comment
 comment|/* A reasonable number of colors (opt for 64K cache) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PQ_L1_SIZE
+value|2
+end_define
+
+begin_comment
+comment|/* Two page L1 cache */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|PQ_MEDIUMCACHE
+argument_list|)
+operator|||
+operator|!
+name|defined
+argument_list|(
+name|PQ_L2_SIZE
+argument_list|)
+end_if
+
+begin_define
+define|#
+directive|define
+name|PQ_PRIME1
+value|13
+end_define
+
+begin_comment
+comment|/* Prime number somewhat less than PQ_HASH_SIZE */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PQ_PRIME2
+value|7
+end_define
+
+begin_comment
+comment|/* Prime number somewhat less than PQ_HASH_SIZE */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PQ_PRIME3
+value|5
+end_define
+
+begin_comment
+comment|/* Prime number somewhat less than PQ_HASH_SIZE */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PQ_L2_SIZE
+value|64
+end_define
+
+begin_comment
+comment|/* A number of colors opt for 256K cache */
 end_comment
 
 begin_define
