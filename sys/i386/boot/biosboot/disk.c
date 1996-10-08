6f69a0fe088d57940dd0b7c020b238af21de0c64
Bruce Evans <bde@FreeBSD.org>
1996-10-08 22:25:22 +0000

    Removed unused arg to badsect().  It wasted 16 bytes.
    Staticized badsect().
    
    Avoid warning for benign signed vs unsigned comparison.
    
    
    Former-commit-id: 4dc301cff9346de22136e29f3f96855dead2f83b

--- sys/i386/boot/biosboot/disk.c
+++ sys/i386/boot/biosboot/disk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:35:49  rpd  *	$Id: disk.c,v 1.19 1996/09/11 19:23:10 phk Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:35:49  rpd  *	$Id: disk.c,v 1.20 1996/09/14 07:40:57 bde Exp $  */
 end_comment
 
 begin_comment
@@ -201,6 +201,17 @@ name|int
 name|ra_first
 decl_stmt|;
 end_decl_stmt
+
+begin_function_decl
+specifier|static
+name|int
+name|badsect
+parameter_list|(
+name|int
+name|sector
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_function
 name|int
@@ -675,6 +686,9 @@ name|i
 operator|<
 literal|10
 operator|&&
+operator|(
+name|unsigned
+operator|)
 name|i
 operator|<
 name|dl
@@ -772,8 +786,6 @@ name|dosdev_copy
 argument_list|,
 name|badsect
 argument_list|(
-name|dosdev_copy
-argument_list|,
 name|sector
 operator|++
 argument_list|)
@@ -988,12 +1000,10 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|int
 name|badsect
 parameter_list|(
-name|int
-name|dosdev
-parameter_list|,
 name|int
 name|sector
 parameter_list|)
