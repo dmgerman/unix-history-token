d38ec182b0f0c8949b1360bee282224e38d1aa9f
Paul Traina <pst@FreeBSD.org>
1996-10-11 19:26:42 +0000

    Fix two bugs I accidently put into the syn code at the last minute (yes I had tested the hell out of this).
    I've also temporarily disabled the code so that it behaves as it previously
    did (tail drop's the syns) pending discussion with fenner about some socket
    state flags that I don't fully understand.
    
    Submitted by:	fenner
    
    
    Former-commit-id: 9c154a8977a971094d225963a91a796d5fa5d59c

--- sys/kern/uipc_socket2.c
+++ sys/kern/uipc_socket2.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket2.c	8.1 (Berkeley) 6/10/93  * $Id: uipc_socket2.c,v 1.14 1996/09/19 00:54:36 pst Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket2.c	8.1 (Berkeley) 6/10/93  * $Id: uipc_socket2.c,v 1.15 1996/10/07 04:32:27 pst Exp $  */
 end_comment
 
 begin_include
@@ -260,17 +260,17 @@ argument_list|,
 name|so_list
 argument_list|)
 expr_stmt|;
+name|head
+operator|->
+name|so_incqlen
+operator|--
+expr_stmt|;
 name|so
 operator|->
 name|so_state
 operator|&=
 operator|~
 name|SS_INCOMP
-expr_stmt|;
-name|so
-operator|->
-name|so_incqlen
-operator|--
 expr_stmt|;
 name|TAILQ_INSERT_TAIL
 argument_list|(
@@ -451,7 +451,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Return a random connection that hasn't been serviced yet and  * is eligible for discard.  *  * This may be used in conjunction with protocol specific queue  * congestion routines.  */
+comment|/*  * Return a random connection that hasn't been serviced yet and  * is eligible for discard.  There is a one in qlen chance that  * we will return a null, saying that there are no dropable  * requests.  In this case, the protocol specific code should drop  * the new request.  This insures fairness.  *  * This may be used in conjunction with protocol specific queue  * congestion routines.  */
 end_comment
 
 begin_function
@@ -498,32 +498,6 @@ name|cur_cnt
 decl_stmt|,
 name|old_cnt
 decl_stmt|;
-name|so
-operator|=
-name|TAILQ_FIRST
-argument_list|(
-operator|&
-name|head
-operator|->
-name|so_incomp
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-operator|!
-name|so
-condition|)
-return|return
-operator|(
-name|so
-operator|)
-return|;
-name|qlen
-operator|=
-name|head
-operator|->
-name|so_incqlen
-expr_stmt|;
 if|if
 condition|(
 operator|(
@@ -558,6 +532,32 @@ operator|=
 literal|0
 expr_stmt|;
 block|}
+name|so
+operator|=
+name|TAILQ_FIRST
+argument_list|(
+operator|&
+name|head
+operator|->
+name|so_incomp
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|so
+condition|)
+return|return
+operator|(
+name|so
+operator|)
+return|;
+name|qlen
+operator|=
+name|head
+operator|->
+name|so_incqlen
+expr_stmt|;
 if|if
 condition|(
 operator|++
