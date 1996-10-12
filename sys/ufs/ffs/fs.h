6b47628b74a519c54565e621990309cf32f49f4d
Bruce Evans <bde@FreeBSD.org>
1996-10-12 22:12:51 +0000

    Fixed lblktosize().  It overflowed at 2G.  This bug only affected ufs_read() and ufs_write().
    Found by: looking at warnings for comparing the result of lblktosize()
    (which is usually daddr_t = long) with file sizes (which are u_quad_t
    for ufs).  File sizes should probably be off_t's to avoid warnings
    when the are compared with file offsets, so the fixed lblktosize()
    casts to off_t instead of u_quad_t.
    
    Added definition of smalllblksize().  It is the same as the old
    lblksize() and is more efficient for small block numbers on 32-bit
    machines.
    
    Use smalllblktosize() instead of its expansion in blksize() and
    dblksize().  This keeps the line length short and makes it more
    obvious that the shift can't overflow.
    
    
    Former-commit-id: 82ca0526bcf07f99095ec2eae0467e9ca9ca2380

--- sys/ufs/ffs/fs.h
+++ sys/ufs/ffs/fs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)fs.h	8.7 (Berkeley) 4/19/94  * $Id: fs.h,v 1.5 1995/05/30 08:15:07 rgrimes Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)fs.h	8.7 (Berkeley) 4/19/94  * $Id: fs.h,v 1.6 1996/01/30 23:02:01 mpp Exp $  */
 end_comment
 
 begin_ifndef
@@ -1250,6 +1250,24 @@ name|fs
 parameter_list|,
 name|blk
 parameter_list|)
+comment|/* calculates ((off_t)blk * fs->fs_bsize) */
+define|\
+value|((off_t)(blk)<< (fs)->fs_bshift)
+end_define
+
+begin_comment
+comment|/* Use this only when `blk' is known to be small, e.g.,< NDADDR. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|smalllblktosize
+parameter_list|(
+name|fs
+parameter_list|,
+name|blk
+parameter_list|)
 comment|/* calculates (blk * fs->fs_bsize) */
 define|\
 value|((blk)<< (fs)->fs_bshift)
@@ -1400,7 +1418,7 @@ parameter_list|,
 name|lbn
 parameter_list|)
 define|\
-value|(((lbn)>= NDADDR || (ip)->i_size>= ((lbn) + 1)<< (fs)->fs_bshift) \ 	    ? (fs)->fs_bsize \ 	    : (fragroundup(fs, blkoff(fs, (ip)->i_size))))
+value|(((lbn)>= NDADDR || (ip)->i_size>= smalllblktosize(fs, (lbn) + 1)) \ 	    ? (fs)->fs_bsize \ 	    : (fragroundup(fs, blkoff(fs, (ip)->i_size))))
 end_define
 
 begin_define
@@ -1415,7 +1433,7 @@ parameter_list|,
 name|lbn
 parameter_list|)
 define|\
-value|(((lbn)>= NDADDR || (dip)->di_size>= ((lbn) + 1)<< (fs)->fs_bshift) \ 	    ? (fs)->fs_bsize \ 	    : (fragroundup(fs, blkoff(fs, (dip)->di_size))))
+value|(((lbn)>= NDADDR || (dip)->di_size>= smalllblktosize(fs, (lbn) + 1)) \ 	    ? (fs)->fs_bsize \ 	    : (fragroundup(fs, blkoff(fs, (dip)->di_size))))
 end_define
 
 begin_comment
