66e06f0128f2e2d05bb5bc86c405c7b6f05ec015
David Greenman <dg@FreeBSD.org>
1996-10-13 01:30:32 +0000

    Change DEVFS device naming convention for cuac*, ttyc*.
    Former-commit-id: e62e1deed26d811d3ce613c45167ecb0b76d73ad

--- sys/dev/cy/cy_isa.c
+++ sys/dev/cy/cy_isa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * cyclades cyclom-y serial driver  *	Andrew Herbert<andrew@werple.apana.org.au>, 17 August 1993  *  * Copyright (c) 1993 Andrew Herbert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name Andrew Herbert may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN  * NO EVENT SHALL I BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: cy.c,v 1.37 1996/10/04 14:17:32 davidg Exp $  */
+comment|/*-  * cyclades cyclom-y serial driver  *	Andrew Herbert<andrew@werple.apana.org.au>, 17 August 1993  *  * Copyright (c) 1993 Andrew Herbert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name Andrew Herbert may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN  * NO EVENT SHALL I BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: cy.c,v 1.38 1996/10/13 01:09:16 davidg Exp $  */
 end_comment
 
 begin_include
@@ -2555,9 +2555,13 @@ name|GID_WHEEL
 argument_list|,
 literal|0600
 argument_list|,
-literal|"ttyc%n"
+literal|"ttyc%n%n"
+argument_list|,
+name|adapter
 argument_list|,
 name|unit
+operator|%
+name|CY_MAX_PORTS
 argument_list|)
 expr_stmt|;
 name|com
@@ -2581,9 +2585,13 @@ name|GID_WHEEL
 argument_list|,
 literal|0600
 argument_list|,
-literal|"ttyic%n"
+literal|"ttyic%n%n"
+argument_list|,
+name|adapter
 argument_list|,
 name|unit
+operator|%
+name|CY_MAX_PORTS
 argument_list|)
 expr_stmt|;
 name|com
@@ -2607,9 +2615,13 @@ name|GID_WHEEL
 argument_list|,
 literal|0600
 argument_list|,
-literal|"ttylc%n"
+literal|"ttylc%n%n"
+argument_list|,
+name|adapter
 argument_list|,
 name|unit
+operator|%
+name|CY_MAX_PORTS
 argument_list|)
 expr_stmt|;
 name|com
@@ -2633,9 +2645,13 @@ name|GID_DIALER
 argument_list|,
 literal|0660
 argument_list|,
-literal|"cuac%n"
+literal|"cuac%n%n"
+argument_list|,
+name|adapter
 argument_list|,
 name|unit
+operator|%
+name|CY_MAX_PORTS
 argument_list|)
 expr_stmt|;
 name|com
@@ -2661,9 +2677,13 @@ name|GID_DIALER
 argument_list|,
 literal|0660
 argument_list|,
-literal|"cuaic%n"
+literal|"cuaic%n%n"
+argument_list|,
+name|adapter
 argument_list|,
 name|unit
+operator|%
+name|CY_MAX_PORTS
 argument_list|)
 expr_stmt|;
 name|com
@@ -2689,9 +2709,13 @@ name|GID_DIALER
 argument_list|,
 literal|0660
 argument_list|,
-literal|"cualc%n"
+literal|"cualc%n%n"
+argument_list|,
+name|adapter
 argument_list|,
 name|unit
+operator|%
+name|CY_MAX_PORTS
 argument_list|)
 expr_stmt|;
 endif|#
