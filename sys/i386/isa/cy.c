93744a9df12c338009cbbcc11ee230130ab027fd
David Greenman <dg@FreeBSD.org>
1996-10-14 16:10:00 +0000

    Make this compile again for the CyDebug case.
    Former-commit-id: 6eab513e5be85d32c6614f20b89c89b22f25e850

--- sys/i386/isa/cy.c
+++ sys/i386/isa/cy.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * cyclades cyclom-y serial driver  *	Andrew Herbert<andrew@werple.apana.org.au>, 17 August 1993  *  * Copyright (c) 1993 Andrew Herbert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name Andrew Herbert may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN  * NO EVENT SHALL I BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: cy.c,v 1.38 1996/10/13 01:09:16 davidg Exp $  */
+comment|/*-  * cyclades cyclom-y serial driver  *	Andrew Herbert<andrew@werple.apana.org.au>, 17 August 1993  *  * Copyright (c) 1993 Andrew Herbert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name Andrew Herbert may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN  * NO EVENT SHALL I BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: cy.c,v 1.39 1996/10/13 01:30:32 davidg Exp $  */
 end_comment
 
 begin_include
@@ -11317,6 +11317,10 @@ argument_list|(
 name|iobase
 argument_list|,
 name|CD1400_SRER
+argument_list|,
+name|com
+operator|->
+name|cy_align
 argument_list|)
 argument_list|,
 name|com
@@ -11333,6 +11337,10 @@ argument_list|(
 name|iobase
 argument_list|,
 name|CD1400_SVRR
+argument_list|,
+name|com
+operator|->
+name|cy_align
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -11345,6 +11353,10 @@ argument_list|(
 name|iobase
 argument_list|,
 name|CD1400_MSVR2
+argument_list|,
+name|com
+operator|->
+name|cy_align
 argument_list|)
 argument_list|,
 name|com
@@ -11361,6 +11373,10 @@ argument_list|(
 name|iobase
 argument_list|,
 name|CD1400_RIR
+argument_list|,
+name|com
+operator|->
+name|cy_align
 argument_list|)
 argument_list|,
 name|cd_inb
@@ -11368,6 +11384,10 @@ argument_list|(
 name|iobase
 argument_list|,
 name|CD1400_TIR
+argument_list|,
+name|com
+operator|->
+name|cy_align
 argument_list|)
 argument_list|,
 name|cd_inb
@@ -11375,6 +11395,10 @@ argument_list|(
 name|iobase
 argument_list|,
 name|CD1400_MIR
+argument_list|,
+name|com
+operator|->
+name|cy_align
 argument_list|)
 argument_list|)
 expr_stmt|;
