956604a8dd0e53309e2be153cc0d3b00827db401
Peter Wemm <peter@FreeBSD.org>
1996-10-19 01:34:25 +0000

    Back out most of the last backout. :-)  Guido removed the 1.1.1.1->1.1.1.2 changes from the vendor branch as well, backing the db-1.82 changes.
    This file should now be the same as it was in rev 1.1.1.2.
    
    
    Former-commit-id: 025a738e2a5f2e53f70c73141418499f8998838c

--- lib/libc/db/hash/hash_buf.c
+++ lib/libc/db/hash/hash_buf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1990, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Margo Seltzer.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_if
@@ -24,7 +24,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)hash_buf.c	8.2 (Berkeley) 2/21/94"
+literal|"@(#)hash_buf.c	8.5 (Berkeley) 7/15/94"
 decl_stmt|;
 end_decl_stmt
 
@@ -51,6 +51,12 @@ begin_include
 include|#
 directive|include
 file|<errno.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<stddef.h>
 end_include
 
 begin_include
@@ -117,7 +123,7 @@ operator|(
 name|HTAB
 operator|*
 operator|,
-name|u_int
+name|u_int32_t
 operator|,
 name|BUFHEAD
 operator|*
@@ -212,7 +218,7 @@ name|HTAB
 modifier|*
 name|hashp
 decl_stmt|;
-name|u_int
+name|u_int32_t
 name|addr
 decl_stmt|;
 name|BUFHEAD
@@ -230,7 +236,7 @@ modifier|*
 name|bp
 decl_stmt|;
 specifier|register
-name|u_int
+name|u_int32_t
 name|is_disk_mask
 decl_stmt|;
 specifier|register
@@ -420,7 +426,7 @@ operator|*
 operator|)
 operator|(
 operator|(
-name|u_int
+name|ptrdiff_t
 operator|)
 name|bp
 operator||
@@ -469,7 +475,7 @@ name|HTAB
 modifier|*
 name|hashp
 decl_stmt|;
-name|u_int
+name|u_int32_t
 name|addr
 decl_stmt|;
 name|BUFHEAD
@@ -500,7 +506,7 @@ decl_stmt|;
 name|int
 name|segment_ndx
 decl_stmt|;
-name|u_short
+name|u_int16_t
 name|oaddr
 decl_stmt|,
 modifier|*
@@ -556,6 +562,23 @@ operator|(
 name|NULL
 operator|)
 return|;
+ifdef|#
+directive|ifdef
+name|PURIFY
+name|memset
+argument_list|(
+name|bp
+argument_list|,
+literal|0xff
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|BUFHEAD
+argument_list|)
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 if|if
 condition|(
 operator|(
@@ -589,6 +612,24 @@ name|NULL
 operator|)
 return|;
 block|}
+ifdef|#
+directive|ifdef
+name|PURIFY
+name|memset
+argument_list|(
+name|bp
+operator|->
+name|page
+argument_list|,
+literal|0xff
+argument_list|,
+name|hashp
+operator|->
+name|BSIZE
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 if|if
 condition|(
 name|hashp
@@ -633,7 +674,7 @@ comment|/* 			 * Set oaddr before __put_page so that you get it 			 * before byt
 name|shortp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|bp
@@ -846,7 +887,7 @@ break|break;
 name|shortp
 operator|=
 operator|(
-name|u_short
+name|u_int16_t
 operator|*
 operator|)
 name|xbp
