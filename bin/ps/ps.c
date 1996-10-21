256e664efa3088ac9cd27a1e63d0e8d667045d2f
Peter Wemm <peter@FreeBSD.org>
1996-10-21 07:30:26 +0000

    Implement a -c option to ps to display the short command name instead of the full argument vector.
    I've bumped into a few things that expected this switch to be present,
    the most recent was the snmp package in ports.  I'm not 100% sure of the
    origins of this, but Linux has it, so does the "BSD-compatable" version
    of ps on our SVR4 systems (so I assume SunOS has it too).
    
    
    Former-commit-id: 8a3ce4a59cb9fb37fcbcefded646afe75ddc0839

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ps.c,v 1.11 1996/01/12 08:49:43 peter Exp $  */
+comment|/*-  * Copyright (c) 1990, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ps.c,v 1.12 1996/01/20 10:43:54 mpp Exp $  */
 end_comment
 
 begin_ifndef
@@ -234,6 +234,16 @@ end_decl_stmt
 
 begin_comment
 comment|/* exit value */
+end_comment
+
+begin_decl_stmt
+name|int
+name|cflag
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* -c */
 end_comment
 
 begin_decl_stmt
@@ -720,7 +730,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"aCeghjLlM:mN:O:o:p:rSTt:U:uvW:wx"
+literal|"aCceghjLlM:mN:O:o:p:rSTt:U:uvW:wx"
 argument_list|)
 operator|)
 operator|!=
@@ -743,18 +753,26 @@ literal|1
 expr_stmt|;
 break|break;
 case|case
-literal|'e'
+literal|'C'
 case|:
-comment|/* XXX set ufmt */
-name|needenv
+name|rawcpu
 operator|=
 literal|1
 expr_stmt|;
 break|break;
 case|case
-literal|'C'
+literal|'c'
 case|:
-name|rawcpu
+name|cflag
+operator|=
+literal|1
+expr_stmt|;
+break|break;
+case|case
+literal|'e'
+case|:
+comment|/* XXX set ufmt */
+name|needenv
 operator|=
 literal|1
 expr_stmt|;
