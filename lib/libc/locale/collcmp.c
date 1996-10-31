9db487d22a0698bb9248d264b29f1a55f110a476
Andrey Chernov <ache@FreeBSD.org>
1996-10-31 04:25:14 +0000

    Rename collate_range_cmp to __collate_range_cmp for internal usage inside libc. Add collate_range_cmp as alias to __collate_range_cmp for temp. backward compatibility. collate_range_cmp will be replaced with direct code for each external program for compatibility with the rest of world
    Former-commit-id: 0aed2b96b19032a506b38cbbd1beddb75d7847ca

--- lib/libc/locale/collcmp.c
+++ lib/libc/locale/collcmp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 1996 by Andrey A. Chernov, Moscow, Russia.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: collcmp.c,v 1.6 1996/09/17 19:27:06 ache Exp $  */
+comment|/*  * Copyright (C) 1996 by Andrey A. Chernov, Moscow, Russia.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: collcmp.c,v 1.7 1996/10/23 15:35:46 ache Exp $  */
 end_comment
 
 begin_define
@@ -22,13 +22,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<limits.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<locale.h>
+file|"collate.h"
 end_include
 
 begin_ifndef
@@ -49,12 +43,41 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * Compare two characters converting collate information  * into ASCII-compatible range, it allows to handle  * "[a-z]"-type ranges with national characters.  */
+comment|/* Temporary backward compatibility */
 end_comment
 
 begin_function
 name|int
 name|collate_range_cmp
+parameter_list|(
+name|c1
+parameter_list|,
+name|c2
+parameter_list|)
+name|int
+name|c1
+decl_stmt|,
+name|c2
+decl_stmt|;
+block|{
+return|return
+name|__collate_range_cmp
+argument_list|(
+name|c1
+argument_list|,
+name|c2
+argument_list|)
+return|;
+block|}
+end_function
+
+begin_comment
+comment|/*  * Compare two characters converting collate information  * into ASCII-compatible range, it allows to handle  * "[a-z]"-type ranges with national characters.  */
+end_comment
+
+begin_function
+name|int
+name|__collate_range_cmp
 parameter_list|(
 name|c1
 parameter_list|,
