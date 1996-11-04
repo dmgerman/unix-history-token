e92bf5038199a32d37bcd091072db1fed03bb943
Bruce Evans <bde@FreeBSD.org>
1996-11-04 17:40:43 +0000

    Don't clobber max_ncyls, ending up guessing thetotal number of cylinders as the number in the 4th slot.
    Fixes PR 1893.
    
    Should be in 2.1.5 and 2.2.
    
    
    Former-commit-id: f3cee0102a58203cced869e47828ab11b03f1965

--- sys/kern/subr_diskmbr.c
+++ sys/kern/subr_diskmbr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994 Bruce D. Evans.  * All rights reserved.  *  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $  *	$Id: diskslice_machdep.c,v 1.20 1996/04/07 17:32:09 bde Exp $  */
+comment|/*-  * Copyright (c) 1994 Bruce D. Evans.  * All rights reserved.  *  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $  *	$Id: diskslice_machdep.c,v 1.21 1996/09/20 17:39:11 bde Exp $  */
 end_comment
 
 begin_include
@@ -1204,12 +1204,15 @@ operator|++
 control|)
 block|{
 name|int
+name|ncyls
+decl_stmt|;
+name|int
 name|nsectors
 decl_stmt|;
 name|int
 name|ntracks
 decl_stmt|;
-name|max_ncyls
+name|ncyls
 operator|=
 name|DPCYL
 argument_list|(
@@ -1221,16 +1224,18 @@ name|dp
 operator|->
 name|dp_esect
 argument_list|)
+operator|+
+literal|1
 expr_stmt|;
 if|if
 condition|(
 name|max_ncyls
 operator|<
-name|max_ncyls
+name|ncyls
 condition|)
 name|max_ncyls
 operator|=
-name|max_ncyls
+name|ncyls
 expr_stmt|;
 name|nsectors
 operator|=
@@ -1270,20 +1275,11 @@ operator|=
 name|ntracks
 expr_stmt|;
 block|}
-comment|/* Check the geometry. */
+comment|/* 	 * Check that we have guessed the geometry right by checking the 	 * partition entries. 	 */
 comment|/* 	 * TODO: 	 * As above. 	 * Check for overlaps. 	 * Check against d_secperunit if the latter is reliable. 	 */
 name|error
 operator|=
 literal|0
-expr_stmt|;
-name|secpercyl
-operator|=
-operator|(
-name|u_long
-operator|)
-name|max_nsectors
-operator|*
-name|max_ntracks
 expr_stmt|;
 for|for
 control|(
@@ -1386,6 +1382,15 @@ goto|goto
 name|done
 goto|;
 comment|/* 	 * Accept the DOS partition table. 	 * First adjust the label (we have been careful not to change it 	 * before we can guarantee success). 	 */
+name|secpercyl
+operator|=
+operator|(
+name|u_long
+operator|)
+name|max_nsectors
+operator|*
+name|max_ntracks
+expr_stmt|;
 if|if
 condition|(
 name|secpercyl
