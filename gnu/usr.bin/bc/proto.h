a4724f91e4adff49b43514bd4a49340b0a44cf34
Steve Price <steve@FreeBSD.org>
1996-11-04 04:36:31 +0000

    Get rid of bogus #ifdef's.
    Former-commit-id: d2dfc6d14c389227a0aba781788ef1f038feff44

--- gnu/usr.bin/bc/proto.h
+++ gnu/usr.bin/bc/proto.h
@@ -75,50 +75,17 @@ begin_comment
 comment|/* Include the standard library header files. */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|NO_UNISTD
-end_ifndef
-
 begin_include
 include|#
 directive|include
 file|<unistd.h>
 end_include
 
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|NO_STDLIB
-end_ifndef
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__STDC__
-end_ifdef
-
 begin_include
 include|#
 directive|include
 file|<stdlib.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/* Define the _PROTOTYPE macro if it is needed. */
