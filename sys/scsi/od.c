0e9a3e397fe0099302aeed224773fe617230c7b2
Joerg Wunsch <joerg@FreeBSD.org>
1996-11-06 17:31:14 +0000

    Fix the sequence at odopen() time so the driver actually notices if a medium with another size is being inserted.  Right now, this case was broken and led to a situation where a medium could only be replaced with another one of the same size.
    Closes PR #kern/1830: Can't mount optical disk...
    
    Submitted by:	akiyama@kme.mei.co.jp (Shunsuke Akiyama)
    
    
    Former-commit-id: 30a7c401926d74e34866be5cab67b773a9d98cf1

--- sys/scsi/od.c
+++ sys/scsi/od.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995,1996 Shunsuke Akiyama.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Shunsuke Akiyama.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Shunsuke Akiyama AND CONTRIBUTORS ``AS IS''  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Shunsuke Akiyama OR CONTRIBUTORS BE  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: od.c,v 1.21 1996/08/17 20:50:26 joerg Exp $  */
+comment|/*  * Copyright (c) 1995,1996 Shunsuke Akiyama.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Shunsuke Akiyama.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Shunsuke Akiyama AND CONTRIBUTORS ``AS IS''  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Shunsuke Akiyama OR CONTRIBUTORS BE  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: od.c,v 1.22 1996/09/06 23:09:11 phk Exp $  */
 end_comment
 
 begin_comment
@@ -1288,13 +1288,11 @@ argument_list|)
 operator|)
 argument_list|)
 expr_stmt|;
-comment|/* 	 * Try to start the drive (ignore failure). 	 */
+comment|/* 	 * Try to start the drive, and try to clear "Unit Attention" 	 * condition, when media had been changed before. 	 * This operation also clears the SDEV_MEDIA_LOADED flag in its 	 * error handling routine. 	 */
 name|scsi_start_unit
 argument_list|(
 name|sc_link
 argument_list|,
-name|SCSI_ERR_OK
-operator||
 name|SCSI_SILENT
 argument_list|)
 expr_stmt|;
@@ -1309,12 +1307,12 @@ operator||
 name|SCSI_SILENT
 argument_list|)
 expr_stmt|;
-comment|/* 	 * Try to clear "Unit Attention" condition, when media had 	 * been changed	before. 	 * This operation also clears the SDEV_MEDIA_LOADED flag in its 	 * error handling routine. 	 */
+comment|/* 	 * Make sure the drive is ready. 	 */
 name|scsi_test_unit_ready
 argument_list|(
 name|sc_link
 argument_list|,
-name|SCSI_SILENT
+literal|0
 argument_list|)
 expr_stmt|;
 name|SC_DEBUG
@@ -1462,14 +1460,9 @@ case|:
 case|case
 literal|1024
 case|:
-ifdef|#
-directive|ifdef
-name|notyet
 case|case
 literal|2048
 case|:
-endif|#
-directive|endif
 break|break;
 default|default :
 name|printf
