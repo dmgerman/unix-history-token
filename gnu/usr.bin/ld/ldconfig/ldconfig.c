79e16d77cbff4adde5e7968447283ad854162a6b
Joerg Wunsch <joerg@FreeBSD.org>
1996-11-08 23:20:38 +0000

    Merge from rev 1.16: fix the ldconfig handling for non-existant directories.
    Former-commit-id: 7b857073881790f62d33c553f4db896f79ed13e9

--- gnu/usr.bin/ld/ldconfig/ldconfig.c
+++ gnu/usr.bin/ld/ldconfig/ldconfig.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993,1995 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: ldconfig.c,v 1.14 1996/10/01 01:31:51 peter Exp $  */
+comment|/*  * Copyright (c) 1993,1995 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: ldconfig.c,v 1.16 1996/11/08 02:12:40 jdp Exp $  */
 end_comment
 
 begin_include
@@ -521,6 +521,13 @@ condition|)
 name|std_search_path
 argument_list|()
 expr_stmt|;
+if|if
+condition|(
+operator|!
+name|justread
+condition|)
+block|{
+comment|/* Add any directories from the command line */
 for|for
 control|(
 name|i
@@ -534,6 +541,41 @@ condition|;
 name|i
 operator|++
 control|)
+block|{
+if|if
+condition|(
+name|access
+argument_list|(
+name|argv
+index|[
+name|i
+index|]
+argument_list|,
+name|F_OK
+argument_list|)
+operator|==
+operator|-
+literal|1
+condition|)
+block|{
+comment|/* Doesn't exist */
+name|warn
+argument_list|(
+literal|"%s"
+argument_list|,
+name|argv
+index|[
+name|i
+index|]
+argument_list|)
+expr_stmt|;
+name|rval
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+block|}
+else|else
 name|add_search_path
 argument_list|(
 name|argv
@@ -542,6 +584,8 @@ name|i
 index|]
 argument_list|)
 expr_stmt|;
+block|}
+block|}
 for|for
 control|(
 name|i
@@ -690,13 +734,16 @@ condition|)
 block|{
 if|if
 condition|(
-operator|!
 name|silent
-operator|||
+operator|&&
 name|errno
-operator|!=
+operator|==
 name|ENOENT
 condition|)
+comment|/* Ignore the error */
+return|return
+literal|0
+return|;
 name|warn
 argument_list|(
 literal|"%s"
