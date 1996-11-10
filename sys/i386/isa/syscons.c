099e635e46041d3d0c9b944cde8a5e889fa119ec
Nate Williams <nate@FreeBSD.org>
1996-11-10 16:57:43 +0000

    From -current:
    Allow us to enable the 'XT_KEYBOARD' code using a configuration flag.
    This allows the user to add modify syscons's configuration flags using
    UserConfig that will allow older/quirky hardware (most notably older IBM
    ThinkPad laptops) to work with the standard boot kernel.
    
    (note: I used the same flag as in -current so the documentation would be
    the same)
    
    Inspired by:    The Nomads
    
    
    Former-commit-id: 23a106756e8d164dd831a3b5e95a531ad62e3e88

--- sys/i386/isa/syscons.c
+++ sys/i386/isa/syscons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.117.4.11 1996/06/25 20:19:48 markm Exp $  */
+comment|/*-  * Copyright (c) 1992-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.117.4.12 1996/06/27 21:39:55 joerg Exp $  */
 end_comment
 
 begin_include
@@ -1075,9 +1075,33 @@ name|val
 argument_list|)
 expr_stmt|;
 block|}
+comment|/*      * Allow us to set the XT_KEYBD flag in UserConfig so that keyboards      * such as those on the IBM ThinkPad laptop computers can be used      * with the standard console driver.      */
+if|if
+condition|(
+name|dev
+operator|->
+name|id_flags
+operator|&
+name|XT_KEYBD
+condition|)
+name|xt_keyboard
+operator|=
+literal|1
+expr_stmt|;
 ifdef|#
 directive|ifdef
 name|XT_KEYBOARD
+name|xt_keyboard
+operator|=
+literal|1
+expr_stmt|;
+endif|#
+directive|endif
+if|if
+condition|(
+name|xt_keyboard
+condition|)
+block|{
 name|kbd_wait
 argument_list|()
 expr_stmt|;
@@ -1101,9 +1125,7 @@ expr_stmt|;
 name|kbd_wait
 argument_list|()
 expr_stmt|;
-endif|#
-directive|endif
-comment|/* XT_KEYBOARD */
+block|}
 return|return
 operator|(
 name|IO_KBDSIZE
