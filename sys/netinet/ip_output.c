af06b20fac71c5643dad9eaebdea0ee7ffc7ecd5
Bill Fenner <fenner@FreeBSD.org>
1996-11-11 04:56:32 +0000

    Add the IP_RECVIF socket option, which supplies a packet's incoming interface using a sockaddr_dl.
    Fix the other packet-information socket options (SO_TIMESTAMP, IP_RECVDSTADDR)
    to work for multicast UDP and raw sockets as well.  (They previously only
    worked for unicast UDP).
    
    
    Former-commit-id: 30cd4ac5d53fc840b0e3e6fa2cf34b7f61e59237

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ip_output.c	8.3 (Berkeley) 1/21/94  *	$Id: ip_output.c,v 1.43 1996/10/07 19:21:46 wollman Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ip_output.c	8.3 (Berkeley) 1/21/94  *	$Id: ip_output.c,v 1.44 1996/10/22 22:26:02 sos Exp $  */
 end_comment
 
 begin_define
@@ -3047,6 +3047,9 @@ case|:
 case|case
 name|IP_RECVDSTADDR
 case|:
+case|case
+name|IP_RECVIF
+case|:
 if|if
 condition|(
 name|m
@@ -3140,6 +3143,15 @@ case|:
 name|OPTSET
 argument_list|(
 name|INP_RECVDSTADDR
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|IP_RECVIF
+case|:
+name|OPTSET
+argument_list|(
+name|INP_RECVIF
 argument_list|)
 expr_stmt|;
 break|break;
@@ -3403,6 +3415,9 @@ case|:
 case|case
 name|IP_RECVDSTADDR
 case|:
+case|case
+name|IP_RECVIF
+case|:
 operator|*
 name|mp
 operator|=
@@ -3490,6 +3505,17 @@ operator|=
 name|OPTBIT
 argument_list|(
 name|INP_RECVDSTADDR
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|IP_RECVIF
+case|:
+name|optval
+operator|=
+name|OPTBIT
+argument_list|(
+name|INP_RECVIF
 argument_list|)
 expr_stmt|;
 break|break;
