63b66dcb072a8a4fc1bddd2fe0bc2042de6f2f24
Stefan Esser <se@FreeBSD.org>
1996-11-12 22:20:22 +0000

    Fix PCI to PCI bridge register bit field masks.
    Thanks to "Mike Durian" <durian@plutotech.com> for the very good
    problem report and his support as a beta tester of this patch.
    
    
    Former-commit-id: 6aea4188f418d290b5a367da7acba2f18fbd0042

--- sys/pci/pcireg.h
+++ sys/pci/pcireg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: pcireg.h,v 1.5.4.1 1996/01/19 19:15:36 se Exp $ ** **  Names for PCI configuration space registers. ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: pcireg.h,v 1.5.4.2 1996/06/26 04:36:50 davidg Exp $ ** **  Names for PCI configuration space registers. ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
 end_comment
 
 begin_ifndef
@@ -712,7 +712,7 @@ name|PCI_PPB_IOBASE_EXTRACT
 parameter_list|(
 name|x
 parameter_list|)
-value|(((x)<< 8)& 0xFF00)
+value|(((x)<< 8)& 0xF000)
 end_define
 
 begin_define
@@ -722,7 +722,7 @@ name|PCI_PPB_IOLIMIT_EXTRACT
 parameter_list|(
 name|x
 parameter_list|)
-value|(((x)<< 0)& 0xFF00)
+value|(((x)<< 0)& 0xF000 | 0x0FFF)
 end_define
 
 begin_define
@@ -732,7 +732,7 @@ name|PCI_PPB_MEMBASE_EXTRACT
 parameter_list|(
 name|x
 parameter_list|)
-value|(((x)<< 16)& 0xFFFF0000)
+value|(((x)<< 16)& 0xFFF00000)
 end_define
 
 begin_define
@@ -742,7 +742,7 @@ name|PCI_PPB_MEMLIMIT_EXTRACT
 parameter_list|(
 name|x
 parameter_list|)
-value|(((x)<<  0)& 0xFFFF0000)
+value|(((x)<<  0)& 0xFFF00000 | 0x000FFFFF)
 end_define
 
 begin_comment
