be4d7eed5b1e76afa25f7517362ad8338ad3cdc4
Bill Fenner <fenner@FreeBSD.org>
1996-11-15 18:50:31 +0000

    Reword two messages:
    duplicate ip address 204.162.228.7! sent from ethernet address: 08:00:20:09:7b:1d
    changed to
    arp: 08:00:20:09:7b:1d is using my IP address 204.162.228.7!
    
    and
    
    arp info overwritten for 204.162.228.2 by 08:00:20:09:7b:1d
    changed to
    arp: 204.162.228.2 moved from 08:00:20:07:b6:a0 to 08:00:20:09:7b:1d
    
    I think the new wordings are more clear and could save some support
    questions.
    
    
    Former-commit-id: cdc35bdb5df736e178021df5549622cb963ec51d

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ether.c	8.1 (Berkeley) 6/10/93  * $Id: if_ether.c,v 1.33 1996/06/21 21:45:58 peter Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ether.c	8.1 (Berkeley) 6/10/93  * $Id: if_ether.c,v 1.34 1996/10/12 19:49:32 bde Exp $  */
 end_comment
 
 begin_comment
@@ -2576,18 +2576,18 @@ name|log
 argument_list|(
 name|LOG_ERR
 argument_list|,
-literal|"duplicate IP address %s! sent from ethernet address: %6D\n"
-argument_list|,
-name|inet_ntoa
-argument_list|(
-name|isaddr
-argument_list|)
+literal|"arp: %6D is using my IP address %s!\n"
 argument_list|,
 name|ea
 operator|->
 name|arp_sha
 argument_list|,
 literal|":"
+argument_list|,
+name|inet_ntoa
+argument_list|(
+name|isaddr
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|itaddr
@@ -2670,12 +2670,23 @@ name|log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"arp info overwritten for %s by %6D\n"
+literal|"arp: %s moved from %6D to %6D\n"
 argument_list|,
 name|inet_ntoa
 argument_list|(
 name|isaddr
 argument_list|)
+argument_list|,
+operator|(
+name|u_char
+operator|*
+operator|)
+name|LLADDR
+argument_list|(
+name|sdl
+argument_list|)
+argument_list|,
+literal|":"
 argument_list|,
 name|ea
 operator|->
