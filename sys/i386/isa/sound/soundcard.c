74e0421d799f1d80cf1f4ac4b69b11514ffadd3d
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-11-15 18:35:35 +0000

    Break my own rule again - I didn't think this stuff was going to come back with the copyright stuff fixed so soon (this should be merged into 2.2 when you have a chance, Poul).
    This is the new AWE32 driver, with support for the AWE32's fancy MIDI
    synthesizer.  The utilities for this will appear as port submissions soon
    afterwards, according to the submitter.
    
    Submitted-By: Randall Hopper <rhh@ct.picker.com>
    Written-By: Takashi Iwai <iwai@dragon.mm.t.u-tokyo.ac.jp>
    
    Former-commit-id: 950ea2dda87e787a26d7375393656e2ae27bde60

--- sys/i386/isa/sound/soundcard.c
+++ sys/i386/isa/sound/soundcard.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * sound/386bsd/soundcard.c  *   * Soundcard driver for FreeBSD.  *   * Copyright by Hannu Savolainen 1993  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: soundcard.c,v 1.43 1996/09/10 08:26:06 bde Exp $  */
+comment|/*  * sound/386bsd/soundcard.c  *   * Soundcard driver for FreeBSD.  *   * Copyright by Hannu Savolainen 1993  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: soundcard.c,v 1.44 1996/11/02 10:38:04 asami Exp $  */
 end_comment
 
 begin_include
@@ -348,6 +348,21 @@ block|,
 name|sndattach
 block|,
 literal|"sbmidi"
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|isa_driver
+name|awedriver
+init|=
+block|{
+name|sndprobe
+block|,
+name|sndattach
+block|,
+literal|"awe"
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -1313,6 +1328,19 @@ condition|)
 return|return
 operator|(
 name|SNDCARD_SB16MIDI
+operator|)
+return|;
+elseif|else
+if|if
+condition|(
+name|driver
+operator|==
+operator|&
+name|awedriver
+condition|)
+return|return
+operator|(
+name|SNDCARD_AWE32
 operator|)
 return|;
 elseif|else
