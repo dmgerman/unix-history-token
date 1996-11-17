9c7bdec0d47045806fd2a2a0516cff94c9d2f8b4
John Dyson <dyson@FreeBSD.org>
1996-11-17 02:11:01 +0000

    Improve the caching of small files like directories, while not substantially increasing buffer space.  Specifically, we double the number of buffers, but allocate only half the amount of memory per buffer.  Note that VDIR files aren't cached unless instantiated in a buffer.  This will significantly improve caching.
    Former-commit-id: 557ad6a473edcf93bf4a668173d6c25e105dfa11

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.103 1996/10/06 07:50:05 dyson Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.104 1996/10/17 03:04:43 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -608,8 +608,6 @@ block|}
 comment|/*  * maxbufspace is currently calculated to support all filesystem blocks  * to be 8K.  If you happen to use a 16K filesystem, the size of the buffer  * cache is still the same as it would be for 8K filesystems.  This  * keeps the size of the buffer cache "in check" for big block filesystems.  */
 name|maxbufspace
 operator|=
-literal|2
-operator|*
 operator|(
 name|nbuf
 operator|+
