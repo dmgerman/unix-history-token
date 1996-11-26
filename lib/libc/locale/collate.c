bb89cba7f97d4a4296c8093d84750a2b74902225
Andrey Chernov <ache@FreeBSD.org>
1996-11-26 02:49:53 +0000

    PATH_LOCALE: use this non-standard env variable first time only, i.e. strdup() it to prevent unsetenv() or setenv() effects. Check its length to not allow user to overflow internal locale buffer. Move PATH_LOCALE handling code into one place.
    POSIX: make better stub for LC_MONETARY & LC_NUMERIC, now it check
    locale directory existance instead of refusing all non-C non-POSIX
    locales. POSIX treats empty locale env variable as unset variable
    while our old code treats it as "C" locale, fix it. Implement previous locale
    restoring, if locale setting fails. Old code assumes success if some
    of LC_ALL subset is successed even other fails, POSIX treats it as
    failure with previous locale restoring, fix it.
    
    Remove unneccessary length checking in currentlocale()
    
    
    Former-commit-id: 8aad534f9aaed1983ac7dfdcfafabff1be544286

--- lib/libc/locale/collate.c
+++ lib/libc/locale/collate.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1995 Alex Tatmanjants<alex@elvisti.kiev.ua>  *		at Electronni Visti IA, Kiev, Ukraine.  *			All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: collate.c,v 1.7 1996/10/15 21:53:22 ache Exp $  */
+comment|/*-  * Copyright (c) 1995 Alex Tatmanjants<alex@elvisti.kiev.ua>  *		at Electronni Visti IA, Kiev, Ukraine.  *			All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: collate.c,v 1.8 1996/10/23 15:35:43 ache Exp $  */
 end_comment
 
 begin_include
@@ -194,10 +194,6 @@ block|}
 if|if
 condition|(
 operator|!
-operator|*
-name|encoding
-operator|||
-operator|!
 name|strcmp
 argument_list|(
 name|encoding
@@ -220,21 +216,17 @@ if|if
 condition|(
 operator|!
 name|_PathLocale
-operator|&&
-operator|!
-operator|(
-name|_PathLocale
-operator|=
-name|getenv
-argument_list|(
-literal|"PATH_LOCALE"
-argument_list|)
-operator|)
 condition|)
-name|_PathLocale
+block|{
+name|__collate_load_error
 operator|=
-name|_PATH_LOCALE
+name|save_load_error
 expr_stmt|;
+return|return
+operator|-
+literal|1
+return|;
+block|}
 name|strcpy
 argument_list|(
 name|buf
