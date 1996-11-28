2f80c0f5e0f4c31076c84c2765a68108415ef9ee
Poul-Henning Kamp <phk@FreeBSD.org>
1996-11-28 08:09:56 +0000

    YAMFC
    Former-commit-id: 394fcca0d246dfbee474482de10507a204498079

--- lib/libftpio/ftpio.h
+++ lib/libftpio/ftpio.h
@@ -30,7 +30,7 @@ file|<time.h>
 end_include
 
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * Major Changelog:  *  * Jordan K. Hubbard  * 17 Jan 1996  *  * Turned inside out. Now returns xfers as new file ids, not as a special  * `state' of FTP_t  *  * $Id: ftpio.h,v 1.7 1996/08/21 01:12:11 jkh Exp $  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * Major Changelog:  *  * Jordan K. Hubbard  * 17 Jan 1996  *  * Turned inside out. Now returns xfers as new file ids, not as a special  * `state' of FTP_t  *  * $Id: ftpio.h,v 1.8 1996/09/19 17:28:34 peter Exp $  */
 end_comment
 
 begin_comment
@@ -46,6 +46,8 @@ block|{
 name|init
 block|,
 name|isopen
+block|,
+name|quit
 block|}
 name|con_state
 enum|;
@@ -146,6 +148,10 @@ name|port
 parameter_list|,
 name|int
 name|verbose
+parameter_list|,
+name|int
+modifier|*
+name|retcode
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -303,6 +309,10 @@ parameter_list|,
 name|char
 modifier|*
 name|passwd
+parameter_list|,
+name|int
+modifier|*
+name|retcode
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -324,6 +334,10 @@ parameter_list|,
 name|char
 modifier|*
 name|passwd
+parameter_list|,
+name|int
+modifier|*
+name|retcode
 parameter_list|)
 function_decl|;
 end_function_decl
