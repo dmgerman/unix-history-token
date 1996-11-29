06441a51f1a8952e424fad5c77a364a0f6e50417
Bruce Evans <bde@FreeBSD.org>
1996-11-29 15:06:17 +0000

    Fixed handling of non-POSIX control characters.  They must not do anything special unless IEXTEN is set.
    Found by:	NIST-PCTS
    
    
    Former-commit-id: c1563cec70b388fd821c7bf36cb2bcfaafa3ed1b

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.83 1996/08/28 18:45:09 bde Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.84 1996/08/31 16:52:26 bde Exp $  */
 end_comment
 
 begin_comment
@@ -1729,9 +1729,11 @@ directive|define
 name|TTBREAKC
 parameter_list|(
 name|c
+parameter_list|,
+name|lflag
 parameter_list|)
 define|\
-value|((c) == '\n' || (((c) == cc[VEOF] ||				\ 	(c) == cc[VEOL] || (c) == cc[VEOL2])&& (c) != _POSIX_VDISABLE))
+value|((c) == '\n' || (((c) == cc[VEOF] ||				\ 	  (c) == cc[VEOL] || ((c) == cc[VEOL2]&& lflag& IEXTEN))&&	\ 	 (c) != _POSIX_VDISABLE))
 end_define
 
 begin_comment
@@ -2894,6 +2896,13 @@ index|]
 argument_list|,
 name|c
 argument_list|)
+operator|&&
+name|ISSET
+argument_list|(
+name|lflag
+argument_list|,
+name|IEXTEN
+argument_list|)
 condition|)
 block|{
 name|int
@@ -3088,6 +3097,13 @@ index|]
 argument_list|,
 name|c
 argument_list|)
+operator|&&
+name|ISSET
+argument_list|(
+name|lflag
+argument_list|,
+name|IEXTEN
+argument_list|)
 condition|)
 block|{
 name|ttyretype
@@ -3110,6 +3126,13 @@ name|VSTATUS
 index|]
 argument_list|,
 name|c
+argument_list|)
+operator|&&
+name|ISSET
+argument_list|(
+name|lflag
+argument_list|,
+name|IEXTEN
 argument_list|)
 condition|)
 block|{
@@ -3312,6 +3335,8 @@ condition|(
 name|TTBREAKC
 argument_list|(
 name|c
+argument_list|,
+name|lflag
 argument_list|)
 condition|)
 block|{
@@ -8296,8 +8321,16 @@ name|ISSET
 argument_list|(
 name|lflag
 argument_list|,
+name|IEXTEN
+operator||
 name|ISIG
 argument_list|)
+operator|==
+operator|(
+name|IEXTEN
+operator||
+name|ISIG
+operator|)
 condition|)
 block|{
 name|pgsignal
@@ -8455,6 +8488,8 @@ operator|&&
 name|TTBREAKC
 argument_list|(
 name|c
+argument_list|,
+name|lflag
 argument_list|)
 condition|)
 break|break;
