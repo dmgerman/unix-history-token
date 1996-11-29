df103e9af97075c9a85948f493b426d5c9574b0d
John Dyson <dyson@FreeBSD.org>
1996-11-29 16:22:22 +0000

    Relax the constraints on the bswap opcode (it works on non-byte registers.)  Also clean up some namespace pollution, and remove gcc-1 support (nothing really works with it anymore anyway.) Submitted by:	Bruce Evans <bde@freebsd.org> and me.
    Former-commit-id: ba3863e54f09e8cb9f1c70f98a87859363685ebb

--- sys/i386/include/endian.h
+++ sys/i386/include/endian.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)endian.h	7.8 (Berkeley) 4/3/91  *	$Id: endian.h,v 1.7 1996/10/21 17:15:05 nate Exp $  */
+comment|/*  * Copyright (c) 1987, 1991 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)endian.h	7.8 (Berkeley) 4/3/91  *	$Id: endian.h,v 1.8 1996/11/29 07:04:03 dyson Exp $  */
 end_comment
 
 begin_ifndef
@@ -13,7 +13,6 @@ begin_define
 define|#
 directive|define
 name|_MACHINE_ENDIAN_H_
-value|1
 end_define
 
 begin_comment
@@ -103,16 +102,8 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|({ register u_long __X = (x); \    __asm ("rorl $16, %1" \ 	: "=r" (__X) \ 	: "0" (__X)); \    __X; })
+value|__extension__ ({ register u_long __X = (x); \    __asm ("rorl $16, %1" \ 	: "=r" (__X) \ 	: "0" (__X)); \    __X; })
 end_define
-
-begin_if
-if|#
-directive|if
-name|__GNUC__
-operator|>=
-literal|2
-end_if
 
 begin_if
 if|#
@@ -137,7 +128,7 @@ parameter_list|(
 name|x
 parameter_list|)
 define|\
-value|__extension__ ({ register u_long __X = (x); \    __asm ("bswap %0" \ 	: "=q" (__X) \ 	: "0" (__X)); \    __X; })
+value|__extension__ ({ register u_long __X = (x); \    __asm ("bswap %0" \ 	: "=r" (__X) \ 	: "0" (__X)); \    __X; })
 end_define
 
 begin_else
@@ -171,46 +162,6 @@ parameter_list|)
 define|\
 value|__extension__ ({ register u_short __X = (x); \    __asm ("xchgb %h1, %b1" \ 	: "=q" (__X) \ 	: "0" (__X)); \    __X; })
 end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_comment
-comment|/* __GNUC__>= 2 */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|__byte_swap_long
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|({ register u_long __X = (x); \    __asm ("rorw $8, %w1\n\trorl $16, %1\n\trorw $8, %w1" \ 	: "=r" (__X) \ 	: "0" (__X)); \    __X; })
-end_define
-
-begin_define
-define|#
-directive|define
-name|__byte_swap_word
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|({ register u_short __X = (x); \    __asm ("rorw $8, %w1" \ 	: "=r" (__X) \ 	: "0" (__X)); \    __X; })
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* __GNUC__>= 2 */
-end_comment
 
 begin_comment
 comment|/*  * Macros for network/external number representation conversion.  */
@@ -315,6 +266,12 @@ else|#
 directive|else
 end_else
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__GNUC__
+end_ifdef
+
 begin_define
 define|#
 directive|define
@@ -343,6 +300,11 @@ name|htons
 value|__byte_swap_word
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_define
 define|#
 directive|define
@@ -350,7 +312,7 @@ name|NTOHL
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = ntohl((u_long)x)
+value|((x) = ntohl((u_long)(x)))
 end_define
 
 begin_define
@@ -360,7 +322,7 @@ name|NTOHS
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = ntohs((u_short)x)
+value|((x) = ntohs((u_short)(x)))
 end_define
 
 begin_define
@@ -370,7 +332,7 @@ name|HTONL
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = htonl((u_long)x)
+value|((x) = htonl((u_long)(x)))
 end_define
 
 begin_define
@@ -380,7 +342,7 @@ name|HTONS
 parameter_list|(
 name|x
 parameter_list|)
-value|(x) = htons((u_short)x)
+value|((x) = htons((u_short)(x)))
 end_define
 
 begin_endif
@@ -394,7 +356,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _MACHINE_ENDIAN_H_ */
+comment|/* !_MACHINE_ENDIAN_H_ */
 end_comment
 
 end_unit
