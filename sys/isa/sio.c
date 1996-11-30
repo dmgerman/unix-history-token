fcde44c2e1dc3ddeeba4d5ac064acace6604011a
Bruce Evans <bde@FreeBSD.org>
1996-11-30 14:51:04 +0000

    Cleaned up CONSPEED changes.  `comdefaultrate' gives the default speed for the "com" console, not for general purpose "com" ports, so there was no need to split it into comdefaultrate and condefaultrate.
    Former-commit-id: 54246eaca80000861fd44fec974d67395b3e025d

--- sys/isa/sio.c
+++ sys/isa/sio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.148 1996/11/02 02:26:01 pst Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.149 1996/11/13 18:31:51 bde Exp $  */
 end_comment
 
 begin_include
@@ -1331,15 +1331,6 @@ begin_decl_stmt
 specifier|static
 name|speed_t
 name|comdefaultrate
-init|=
-name|TTYDEF_SPEED
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
-name|speed_t
-name|condefaultrate
 init|=
 name|CONSPEED
 decl_stmt|;
@@ -3362,7 +3353,7 @@ name|it_in
 operator|.
 name|c_ospeed
 operator|=
-name|condefaultrate
+name|comdefaultrate
 expr_stmt|;
 block|}
 else|else
@@ -3378,7 +3369,7 @@ name|it_in
 operator|.
 name|c_ospeed
 operator|=
-name|comdefaultrate
+name|TTYDEF_SPEED
 expr_stmt|;
 name|termioschars
 argument_list|(
@@ -10836,7 +10827,7 @@ decl_stmt|;
 name|Port_t
 name|iobase
 decl_stmt|;
-comment|/* 	 * Save all the device control registers except the fifo register 	 * and set our default ones (cs8 -parenb speed=condefaultrate). 	 * We can't save the fifo register since it is read-only. 	 */
+comment|/* 	 * Save all the device control registers except the fifo register 	 * and set our default ones (cs8 -parenb speed=comdefaultrate). 	 * We can't save the fifo register since it is read-only. 	 */
 name|iobase
 operator|=
 name|siocniobase
@@ -10914,7 +10905,7 @@ name|divisor
 operator|=
 name|ttspeedtab
 argument_list|(
-name|condefaultrate
+name|comdefaultrate
 argument_list|,
 name|comspeedtab
 argument_list|)
