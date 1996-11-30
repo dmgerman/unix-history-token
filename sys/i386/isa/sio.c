b6256b618487a467fb613bad7b861f3c65df1d2c
Bruce Evans <bde@FreeBSD.org>
1996-11-30 15:29:31 +0000

    Reset h/w fifos (if any) in siostop().  Now ttyflush() works fairly well with sio devices (not perfectly, since there is no way to flush the tx holding register on 8250-16450's.  I'm not sure if resetting the fifos flushes the tx shift register).
    Reminded by:	NIST-PCTS
    
    
    Former-commit-id: 1c461bbeaa3f3e96e7269703ee81d41106d76dcf

--- sys/i386/isa/sio.c
+++ sys/i386/isa/sio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.151 1996/11/30 15:03:05 bde Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.152 1996/11/30 15:19:19 bde Exp $  */
 end_comment
 
 begin_include
@@ -9797,6 +9797,28 @@ operator|&
 name|FWRITE
 condition|)
 block|{
+if|if
+condition|(
+name|com
+operator|->
+name|hasfifo
+condition|)
+comment|/* XXX does this flush everything? */
+name|outb
+argument_list|(
+name|com
+operator|->
+name|iobase
+operator|+
+name|com_fifo
+argument_list|,
+name|FIFO_XMT_RST
+operator||
+name|com
+operator|->
+name|fifo_image
+argument_list|)
+expr_stmt|;
 name|com
 operator|->
 name|obufs
@@ -9859,6 +9881,28 @@ operator|&
 name|FREAD
 condition|)
 block|{
+if|if
+condition|(
+name|com
+operator|->
+name|hasfifo
+condition|)
+comment|/* XXX does this flush everything? */
+name|outb
+argument_list|(
+name|com
+operator|->
+name|iobase
+operator|+
+name|com_fifo
+argument_list|,
+name|FIFO_RCV_RST
+operator||
+name|com
+operator|->
+name|fifo_image
+argument_list|)
+expr_stmt|;
 name|com_events
 operator|-=
 operator|(
@@ -9888,7 +9932,6 @@ argument_list|(
 name|tp
 argument_list|)
 expr_stmt|;
-comment|/* XXX should clear h/w fifos too. */
 block|}
 end_function
 
