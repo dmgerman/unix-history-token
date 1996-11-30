19a1094ce687e3cd28c72bed7048394f614ce88c
Torsten Blum <torstenb@FreeBSD.org>
1996-11-30 12:00:25 +0000

    add flag to allow only anonymous ftp logins
    Reviewed by:	pst
    
    
    Former-commit-id: ccb969463bea88bd7b35ee5e5871ccc32474863a

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1985, 1988, 1990, 1992, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ftpd.c,v 1.25 1996/10/18 17:09:25 ache Exp $  */
+comment|/*  * Copyright (c) 1985, 1988, 1990, 1992, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ftpd.c,v 1.26 1996/11/20 22:13:51 pst Exp $  */
 end_comment
 
 begin_if
@@ -465,6 +465,18 @@ end_decl_stmt
 
 begin_comment
 comment|/* be extra careful about security */
+end_comment
+
+begin_decl_stmt
+name|int
+name|anon_only
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* Only anonymous ftp allowed */
 end_comment
 
 begin_decl_stmt
@@ -1375,7 +1387,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"dlDSUt:T:u:va:p:"
+literal|"AdlDSUt:T:u:va:p:"
 argument_list|)
 operator|)
 operator|!=
@@ -1548,6 +1560,14 @@ name|val
 expr_stmt|;
 break|break;
 block|}
+case|case
+literal|'A'
+case|:
+name|anon_only
+operator|=
+literal|1
+expr_stmt|;
+break|break;
 case|case
 literal|'v'
 case|:
@@ -3017,6 +3037,22 @@ argument_list|,
 literal|"ANONYMOUS FTP LOGIN REFUSED FROM %s"
 argument_list|,
 name|remotehost
+argument_list|)
+expr_stmt|;
+return|return;
+block|}
+if|if
+condition|(
+name|anon_only
+operator|!=
+literal|0
+condition|)
+block|{
+name|reply
+argument_list|(
+literal|530
+argument_list|,
+literal|"Sorry, only anonymous ftp allowed."
 argument_list|)
 expr_stmt|;
 return|return;
