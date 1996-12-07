b86105f14a4dce57e919c2be09403e0e14cbf1f1
Joerg Wunsch <joerg@FreeBSD.org>
1996-12-07 22:45:15 +0000

    Default the CD-ROM devices to a single LUN only.  Too many of them are broken and respond to multiple LUNs.
    Reviewed by:	rv@groa.uct.ac.za (Russell Vincent)
    
    
    Former-commit-id: a3fb460d429b0225b6cecd4fe6e385390769ca2f

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.68 1996/11/28 09:51:03 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.69 1996/11/30 07:39:37 joerg Exp $  */
 end_comment
 
 begin_include
@@ -1565,6 +1565,24 @@ name|SC_ONE_LU
 block|}
 block|,
 comment|/* 	 * Doobe-doo-be doooo 	 * -Mary 	 */
+block|{
+name|T_READONLY
+block|,
+name|T_READONLY
+block|,
+name|T_REMOV
+block|,
+literal|"*"
+block|,
+literal|"*"
+block|,
+literal|"*"
+block|,
+literal|"cd"
+block|,
+name|SC_ONE_LU
+block|}
+block|,
 endif|#
 directive|endif
 comment|/* !UKTEST */
