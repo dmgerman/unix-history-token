bd1340d9792ea7c2ce8b3fc84a513d631859adde
Joerg Wunsch <joerg@FreeBSD.org>
1996-12-11 15:17:10 +0000

    YAMFC
    Former-commit-id: 6dc4631d443e72550ba868d98fdaf2a45bb2d644

--- usr.sbin/pw/pw_user.c
+++ usr.sbin/pw/pw_user.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 1996  *	David L. Nugent.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY DAVID L. NUGENT AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL DAVID L. NUGENT OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: pw_user.c,v 1.1.1.1.2.1 1996/12/10 00:32:02 joerg Exp $  */
+comment|/*-  * Copyright (C) 1996  *	David L. Nugent.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY DAVID L. NUGENT AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL DAVID L. NUGENT OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: pw_user.c,v 1.1.1.1.2.2 1996/12/11 00:14:17 joerg Exp $  */
 end_comment
 
 begin_include
@@ -5253,7 +5253,7 @@ name|gecos
 condition|?
 literal|":"
 else|:
-literal|" :+-&#%$^()!@~*?<>=|\\/\""
+literal|" ,\t:+-&#%$^()!@~*?<>=|\\/\""
 decl_stmt|;
 while|while
 condition|(
@@ -5283,12 +5283,41 @@ name|l
 index|]
 operator|<
 literal|' '
+operator|||
+name|name
+index|[
+name|l
+index|]
+operator|>
+literal|126
 condition|)
 name|cmderr
 argument_list|(
 name|EX_DATAERR
 argument_list|,
+operator|(
+name|name
+index|[
+name|l
+index|]
+operator|<
+literal|' '
+operator|||
+operator|(
+name|unsigned
+name|char
+operator|)
+name|name
+index|[
+name|l
+index|]
+operator|>
+literal|126
+operator|)
+condition|?
 literal|"invalid character `%c' in field\n"
+else|:
+literal|"invalid character 0x$02x in field\n"
 argument_list|,
 name|name
 index|[
@@ -5307,7 +5336,7 @@ name|gecos
 operator|&&
 name|l
 operator|>
-literal|8
+name|MAXLOGNAME
 condition|)
 name|cmderr
 argument_list|(
