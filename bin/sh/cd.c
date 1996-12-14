d453dc1944543cf66a9ef2c778608c447bac5555
Steve Price <steve@FreeBSD.org>
1996-12-14 06:20:03 +0000

    Merge in NetBSD mods and -Wall cleaning.
    Obtained from: NetBSD, me
    
    
    Former-commit-id: 21fc335b107782e51c2cbfc8b422887e3887af51

--- bin/sh/cd.c
+++ bin/sh/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cd.c,v 1.5 1995/11/14 01:04:52 peter Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cd.c,v 1.6 1996/09/01 10:19:47 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -12,6 +12,7 @@ end_ifndef
 begin_decl_stmt
 specifier|static
 name|char
+specifier|const
 name|sccsid
 index|[]
 init|=
@@ -44,6 +45,12 @@ begin_include
 include|#
 directive|include
 file|<stdlib.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<string.h>
 end_include
 
 begin_include
@@ -117,6 +124,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"exec.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"redir.h"
 end_include
 
@@ -130,6 +143,12 @@ begin_include
 include|#
 directive|include
 file|"show.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"cd.h"
 end_include
 
 begin_decl_stmt
@@ -177,22 +196,11 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|STATIC
-name|void
-name|getpwd
-name|__P
-argument_list|(
-operator|(
-name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 name|char
 modifier|*
 name|curdir
+init|=
+name|NULL
 decl_stmt|;
 end_decl_stmt
 
@@ -252,11 +260,6 @@ name|struct
 name|stat
 name|statb
 decl_stmt|;
-name|char
-modifier|*
-name|padvance
-parameter_list|()
-function_decl|;
 name|int
 name|print
 init|=
@@ -503,6 +506,17 @@ name|int
 name|print
 decl_stmt|;
 block|{
+name|TRACE
+argument_list|(
+operator|(
+literal|"docd(\"%s\", %d) called\n"
+operator|,
+name|dest
+operator|,
+name|print
+operator|)
+argument_list|)
+expr_stmt|;
 name|INTOFF
 expr_stmt|;
 if|if
@@ -1412,10 +1426,6 @@ return|;
 block|}
 end_function
 
-begin_comment
-comment|/*  * Run /bin/pwd to find out what the current directory is.  We suppress  * interrupts throughout most of this, but the user can still break out  * of it by killing the pwd program.  If we already know the current  * directory, this routine returns immediately.  */
-end_comment
-
 begin_define
 define|#
 directive|define
@@ -1423,8 +1433,11 @@ name|MAXPWD
 value|256
 end_define
 
+begin_comment
+comment|/*  * Run /bin/pwd to find out what the current directory is.  We suppress  * interrupts throughout most of this, but the user can still break out  * of it by killing the pwd program.  If we already know the current  * directory, this routine returns immediately.  */
+end_comment
+
 begin_function
-name|STATIC
 name|void
 name|getpwd
 parameter_list|()
@@ -1575,6 +1588,9 @@ index|]
 argument_list|)
 expr_stmt|;
 block|}
+operator|(
+name|void
+operator|)
 name|execl
 argument_list|(
 name|pwd_bin
@@ -1596,6 +1612,9 @@ name|pwd_bin
 argument_list|)
 expr_stmt|;
 block|}
+operator|(
+name|void
+operator|)
 name|close
 argument_list|(
 name|pip
@@ -1663,6 +1682,9 @@ operator|+=
 name|i
 expr_stmt|;
 block|}
+operator|(
+name|void
+operator|)
 name|close
 argument_list|(
 name|pip
--- bin/sh/var.h
+++ bin/sh/var.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)var.h	8.2 (Berkeley) 5/4/95  *	$Id: var.h,v 1.2 1994/09/24 02:58:23 davidg Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)var.h	8.2 (Berkeley) 5/4/95  *	$Id: var.h,v 1.3 1996/09/01 10:21:54 peter Exp $  */
 end_comment
 
 begin_comment
@@ -15,7 +15,7 @@ begin_define
 define|#
 directive|define
 name|VEXPORT
-value|01
+value|0x01
 end_define
 
 begin_comment
@@ -26,7 +26,7 @@ begin_define
 define|#
 directive|define
 name|VREADONLY
-value|02
+value|0x02
 end_define
 
 begin_comment
@@ -37,7 +37,7 @@ begin_define
 define|#
 directive|define
 name|VSTRFIXED
-value|04
+value|0x04
 end_define
 
 begin_comment
@@ -48,7 +48,7 @@ begin_define
 define|#
 directive|define
 name|VTEXTFIXED
-value|010
+value|0x08
 end_define
 
 begin_comment
@@ -59,7 +59,7 @@ begin_define
 define|#
 directive|define
 name|VSTACK
-value|020
+value|0x10
 end_define
 
 begin_comment
@@ -70,11 +70,22 @@ begin_define
 define|#
 directive|define
 name|VUNSET
-value|040
+value|0x20
 end_define
 
 begin_comment
 comment|/* the variable is not set */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VNOFUNC
+value|0x40
+end_define
+
+begin_comment
+comment|/* don't call the callback function */
 end_comment
 
 begin_struct
@@ -96,6 +107,21 @@ modifier|*
 name|text
 decl_stmt|;
 comment|/* name=value */
+name|void
+argument_list|(
+argument|*func
+argument_list|)
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|)
+argument_list|)
+expr_stmt|;
+comment|/* function to be called when  */
+comment|/* the variable gets set/unset */
 block|}
 struct|;
 end_struct
@@ -223,6 +249,25 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|NO_HISTORY
+end_ifndef
+
+begin_decl_stmt
+specifier|extern
+name|struct
+name|var
+name|vhistsize
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_comment
 comment|/*  * The following macros access the values of the above variables.  * They have to skip over the name.  They return the null string  * for unset variables.  */
 end_comment
@@ -287,6 +332,33 @@ directive|define
 name|termval
 parameter_list|()
 value|(vterm.text + 5)
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_define
+define|#
+directive|define
+name|optindval
+parameter_list|()
+value|(voptind.text + 7)
+end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|NO_HISTORY
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|histsizeval
+parameter_list|()
+value|(vhistsize.text + 9)
 end_define
 
 begin_endif
@@ -542,6 +614,37 @@ operator|,
 name|char
 operator|*
 operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|unsetvar
+name|__P
+argument_list|(
+operator|(
+name|char
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|setvarsafe
+name|__P
+argument_list|(
+operator|(
+name|char
+operator|*
+operator|,
+name|char
+operator|*
+operator|,
+name|int
 operator|)
 argument_list|)
 decl_stmt|;
