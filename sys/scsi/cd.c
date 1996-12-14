57758add496f2ef703b9597fae13d7e68a3c4af3
Joerg Wunsch <joerg@FreeBSD.org>
1996-12-14 14:52:48 +0000

    YAMFC
    Former-commit-id: 48499748dccac5c04f138e0fe1d1108024684e7c

--- sys/scsi/cd.c
+++ sys/scsi/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.72 1996/07/23 21:52:18 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.73 1996/09/06 23:09:06 phk Exp $  */
 end_comment
 
 begin_include
@@ -1109,6 +1109,8 @@ argument_list|,
 name|SCSI_NOSLEEP
 operator||
 name|SCSI_NOMASK
+operator||
+name|SCSI_SILENT
 argument_list|)
 expr_stmt|;
 block|}
