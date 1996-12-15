aa2fd91cf86e440dacf4af4522a1857085e577c8
Bruce Evans <bde@FreeBSD.org>
1996-12-15 14:52:14 +0000

    YAMFC (give up early for empty partitions).
    Former-commit-id: 337d54bb34197bd7479aeac7bf54294decce949c

--- sys/i386/boot/biosboot/disk.c
+++ sys/i386/boot/biosboot/disk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:35:49  rpd  *	$Id: disk.c,v 1.21 1996/10/08 22:25:22 bde Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:35:49  rpd  *	$Id: disk.c,v 1.22 1996/10/08 22:31:31 bde Exp $  */
 end_comment
 
 begin_comment
@@ -412,7 +412,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"bad disklabel"
+literal|"bad disklabel\n"
 argument_list|)
 expr_stmt|;
 return|return
@@ -487,7 +487,6 @@ name|p_offset
 operator|+
 name|sector
 expr_stmt|;
-comment|/* This is a good idea for all disks */
 name|bsize
 operator|=
 name|dl
@@ -499,6 +498,22 @@ index|]
 operator|.
 name|p_size
 expr_stmt|;
+if|if
+condition|(
+name|bsize
+operator|==
+literal|0
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|"empty partition\n"
+argument_list|)
+expr_stmt|;
+return|return
+literal|1
+return|;
+block|}
 ifdef|#
 directive|ifdef
 name|DO_BAD144
