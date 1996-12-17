5681b9f67bf40179b385bcac1fc4ca0b7b53b590
Garrett Wollman <wollman@FreeBSD.org>
1996-12-17 21:11:59 +0000

    Merge from vendor branch.
    Former-commit-id: 85dad0b1b7624a36901b31d5e2b4ddf852a515f3

--- sbin/routed/rtquery/rtquery.c
+++ sbin/routed/rtquery/rtquery.c
@@ -498,7 +498,7 @@ function_decl|;
 end_function_decl
 
 begin_function
-name|int
+name|void
 name|main
 parameter_list|(
 name|int
@@ -1156,7 +1156,7 @@ name|stderr
 argument_list|,
 literal|"%s: [-np1v] [-r tgt_rt] [-w wtime]"
 literal|" [-a type=passwd] host1 [host2 ...]\n"
-literal|"or\t-t {on=filename|more|off|on=dump/../table}"
+literal|"or\t-t {on=filename|more|off|dump}"
 literal|" host1 [host2 ...]\n"
 argument_list|,
 name|pgmname
