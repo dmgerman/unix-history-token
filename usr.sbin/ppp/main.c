f653f0280af6766f8381ca0536cdab387cf896e4
Nate Williams <nate@FreeBSD.org>
1996-12-19 00:41:42 +0000

    Fixed prototypes of PacketAliasIn/Out.  (cosmetic)
    Submitted by:	Brian Somers <brian@awfulhak.demon.co.uk>
    
    
    Former-commit-id: 8ad987c40bb84c55f08028b67ea6be465461c23a

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.23 1996/12/03 21:38:48 nate Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
+comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.24 1996/12/12 14:39:45 jkh Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
 end_comment
 
 begin_include
@@ -3702,6 +3702,11 @@ condition|)
 block|{
 name|PacketAliasOut
 argument_list|(
+operator|(
+expr|struct
+name|ip
+operator|*
+operator|)
 name|rbuff
 argument_list|)
 expr_stmt|;
@@ -3766,6 +3771,11 @@ condition|)
 block|{
 name|PacketAliasOut
 argument_list|(
+operator|(
+expr|struct
+name|ip
+operator|*
+operator|)
 name|rbuff
 argument_list|)
 expr_stmt|;
