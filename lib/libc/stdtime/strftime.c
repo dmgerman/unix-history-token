56aca1f2aa6430adf27f8652ce1c53ae8d5d28ed
Andrey Chernov <ache@FreeBSD.org>
1996-12-28 05:04:24 +0000

    Add comment that range checking is already done at upper level Kill snprintf left in collate.c from previous backout
    Should go in 2.2
    
    
    Former-commit-id: 1dae1af748fbbd0e5c96c280b142164818fe8383

--- lib/libc/stdtime/strftime.c
+++ lib/libc/stdtime/strftime.c
@@ -16,7 +16,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: strftime.c,v 1.11 1996/07/19 15:17:44 wollman Exp $"
+literal|"$Id: strftime.c,v 1.12 1996/11/26 02:52:52 ache Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -2118,6 +2118,7 @@ condition|)
 goto|goto
 name|no_locale
 goto|;
+comment|/* Range checking already done at upper level caller */
 name|strcpy
 argument_list|(
 name|filename
