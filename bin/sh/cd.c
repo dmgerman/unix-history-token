bc6e074de9f9bb3ee3c452ee129baacf5784a963
Steve Price <steve@FreeBSD.org>
1997-01-04 19:14:29 +0000

    Make sh(1) think and be in the same place at the same time.  This closes PR#2331: strange output of sh's pwd on symlinked directories.
    Former-commit-id: 37a1adcda7e7c89d640fb7957f85ab61a104e705

--- bin/sh/cd.c
+++ bin/sh/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cd.c,v 1.10 1996/12/23 22:16:35 steve Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cd.c,v 1.11 1996/12/23 22:29:03 steve Exp $  */
 end_comment
 
 begin_ifndef
@@ -63,12 +63,6 @@ begin_include
 include|#
 directive|include
 file|<errno.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<limits.h>
 end_include
 
 begin_comment
@@ -517,11 +511,17 @@ argument_list|)
 expr_stmt|;
 name|INTOFF
 expr_stmt|;
+name|updatepwd
+argument_list|(
+name|dest
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|chdir
 argument_list|(
-name|dest
+name|stackblock
+argument_list|()
 argument_list|)
 operator|<
 literal|0
@@ -534,9 +534,29 @@ operator|-
 literal|1
 return|;
 block|}
-name|updatepwd
+name|hashcd
+argument_list|()
+expr_stmt|;
+comment|/* update command hash table */
+if|if
+condition|(
+name|prevdir
+condition|)
+name|ckfree
 argument_list|(
-name|dest
+name|prevdir
+argument_list|)
+expr_stmt|;
+name|prevdir
+operator|=
+name|curdir
+expr_stmt|;
+name|curdir
+operator|=
+name|savestr
+argument_list|(
+name|stackblock
+argument_list|()
 argument_list|)
 expr_stmt|;
 name|INTON
@@ -646,7 +666,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Update curdir (the name of the current directory) in response to a  * cd command.  We also call hashcd to let the routines in exec.c know  * that the current directory has changed.  */
+comment|/*  * Determine the new working directory, but don't actually enforce  * any changes.  */
 end_comment
 
 begin_function
@@ -669,10 +689,6 @@ name|char
 modifier|*
 name|p
 decl_stmt|;
-name|hashcd
-argument_list|()
-expr_stmt|;
-comment|/* update command hash table */
 name|cdcomppath
 operator|=
 name|stalloc
@@ -847,31 +863,6 @@ name|STACKSTRNUL
 argument_list|(
 name|new
 argument_list|)
-expr_stmt|;
-name|INTOFF
-expr_stmt|;
-if|if
-condition|(
-name|prevdir
-condition|)
-name|ckfree
-argument_list|(
-name|prevdir
-argument_list|)
-expr_stmt|;
-name|prevdir
-operator|=
-name|curdir
-expr_stmt|;
-name|curdir
-operator|=
-name|savestr
-argument_list|(
-name|stackblock
-argument_list|()
-argument_list|)
-expr_stmt|;
-name|INTON
 expr_stmt|;
 block|}
 end_function
