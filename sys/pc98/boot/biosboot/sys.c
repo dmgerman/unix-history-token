3cd222d1c3174239d6e51d8c697a179a0ab5b2b1
Takenori KATO <kato@FreeBSD.org>
1997-01-04 16:19:16 +0000

    YAMFC (clean up).
    Former-commit-id: 495aa3e36a87bd447bff66f0041fd3882b09855a

--- sys/pc98/boot/biosboot/sys.c
+++ sys/pc98/boot/biosboot/sys.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:36:34  rpd  *	$Id: sys.c,v 1.6 1996/10/29 08:36:11 asami Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:36:34  rpd  *	$Id: sys.c,v 1.6.2.1 1996/11/09 21:12:13 phk Exp $  */
 end_comment
 
 begin_comment
@@ -1297,13 +1297,13 @@ condition|(
 name|maj
 condition|)
 block|{
+ifdef|#
+directive|ifdef
+name|PC98
 case|case
 literal|4
 case|:
 comment|/* sd */
-ifdef|#
-directive|ifdef
-name|PC98
 name|dosdev_copy
 operator|=
 name|unit
@@ -1359,6 +1359,12 @@ expr_stmt|;
 else|#
 directive|else
 comment|/* IBM-PC */
+case|case
+literal|0
+case|:
+case|case
+literal|4
+case|:
 name|dosdev_copy
 operator|=
 name|biosdrive
@@ -1368,15 +1374,15 @@ expr_stmt|;
 endif|#
 directive|endif
 break|break;
+ifdef|#
+directive|ifdef
+name|PC98
 case|case
 literal|0
 case|:
 case|case
 literal|2
 case|:
-ifdef|#
-directive|ifdef
-name|PC98
 name|dosdev_copy
 operator|=
 operator|(
@@ -1391,6 +1397,9 @@ literal|0x80
 expr_stmt|;
 else|#
 directive|else
+case|case
+literal|2
+case|:
 name|dosdev_copy
 operator|=
 name|biosdrive
@@ -1398,9 +1407,7 @@ expr_stmt|;
 endif|#
 directive|endif
 break|break;
-case|case
-literal|3
-case|:
+default|default:
 name|printf
 argument_list|(
 literal|"Unknown device\n"
