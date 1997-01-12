7f5cc9edb7b8cbfd81f4703d0d0889466fd1cbc5
Bill Paul <wpaul@FreeBSD.org>
1997-01-12 08:18:17 +0000

    Ack. When I moved the getsockopt() calls, I forgot to move the lines that initialize the getsockopt() args (type and len) too.
    Former-commit-id: e9d6d16acedf21b0901d7d1167f8d5b7ce054430

--- usr.sbin/ypserv/yp_dnslookup.c
+++ usr.sbin/ypserv/yp_dnslookup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995, 1996  *	Bill Paul<wpaul@ctr.columbia.edu>. All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: yp_dnslookup.c,v 1.13 1997/01/07 04:48:52 wpaul Exp $  */
+comment|/*  * Copyright (c) 1995, 1996  *	Bill Paul<wpaul@ctr.columbia.edu>. All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: yp_dnslookup.c,v 1.15 1997/01/12 08:09:28 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -134,7 +134,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: yp_dnslookup.c,v 1.13 1997/01/07 04:48:52 wpaul Exp $"
+literal|"$Id: yp_dnslookup.c,v 1.15 1997/01/12 08:09:28 wpaul Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -1745,6 +1745,18 @@ decl_stmt|,
 name|len
 decl_stmt|;
 comment|/* Check for SOCK_DGRAM or SOCK_STREAM -- we need to know later */
+name|type
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+name|len
+operator|=
+sizeof|sizeof
+argument_list|(
+name|type
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|getsockopt
@@ -1855,18 +1867,6 @@ operator|=
 name|rqstp
 operator|->
 name|rq_vers
-expr_stmt|;
-name|type
-operator|=
-operator|-
-literal|1
-expr_stmt|;
-name|len
-operator|=
-sizeof|sizeof
-argument_list|(
-name|type
-argument_list|)
 expr_stmt|;
 name|q
 operator|->
@@ -2058,6 +2058,18 @@ decl_stmt|,
 name|len
 decl_stmt|;
 comment|/* Check for SOCK_DGRAM or SOCK_STREAM -- we need to know later */
+name|type
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+name|len
+operator|=
+sizeof|sizeof
+argument_list|(
+name|type
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|getsockopt
@@ -2233,18 +2245,6 @@ operator|->
 name|domain
 operator|=
 name|NULL
-expr_stmt|;
-name|type
-operator|=
-operator|-
-literal|1
-expr_stmt|;
-name|len
-operator|=
-sizeof|sizeof
-argument_list|(
-name|type
-argument_list|)
 expr_stmt|;
 name|q
 operator|->
