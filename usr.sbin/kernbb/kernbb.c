cca0a9e09e5906e842dbe478128b5ea186c4054a
Bruce Evans <bde@FreeBSD.org>
1997-01-14 10:35:10 +0000

    YAMFC (fixed misapplied patch in rev.1.3 (not in rev.1.2 like the HEAD log says)).
    Former-commit-id: f1be3387aa1748a556248bfc4f1157e1a37a522a

--- usr.sbin/kernbb/kernbb.c
+++ usr.sbin/kernbb/kernbb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: kernbb.c,v 1.2 1995/04/28 04:58:19 phk Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: kernbb.c,v 1.3 1995/10/22 19:45:28 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -454,15 +454,6 @@ literal|0
 index|]
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-operator|!
-name|counts
-index|[
-name|i
-index|]
-condition|)
-continue|continue;
 for|for
 control|(
 name|i
@@ -479,6 +470,15 @@ name|i
 operator|++
 control|)
 block|{
+if|if
+condition|(
+operator|!
+name|counts
+index|[
+name|i
+index|]
+condition|)
+continue|continue;
 if|if
 condition|(
 operator|!
