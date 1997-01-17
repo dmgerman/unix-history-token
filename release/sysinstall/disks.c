cd60b8ec4791673d0f8063ee17b7aa78bb8a5976
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-01-17 08:53:50 +0000

    Merge last 3 days worth of -current changes.
    Former-commit-id: 2cb338b6e021c88d3b199028c41036305e47eeae

--- release/sysinstall/disks.c
+++ release/sysinstall/disks.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id$  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: disks.c,v 1.70.2.6 1997/01/15 07:31:20 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -820,6 +820,10 @@ literal|80
 index|]
 decl_stmt|;
 comment|/* Now print our overall state */
+if|if
+condition|(
+name|d
+condition|)
 name|print_chunks
 argument_list|(
 name|d
@@ -1563,13 +1567,36 @@ literal|"Are you SURE you want to Undo everything?"
 argument_list|)
 condition|)
 block|{
+name|char
+name|cp
+index|[
+name|BUFSIZ
+index|]
+decl_stmt|;
+name|sstrncpy
+argument_list|(
+name|cp
+argument_list|,
+name|d
+operator|->
+name|name
+argument_list|,
+sizeof|sizeof
+name|cp
+argument_list|)
+expr_stmt|;
+name|Free_Disk
+argument_list|(
+name|dev
+operator|->
+name|private
+argument_list|)
+expr_stmt|;
 name|d
 operator|=
 name|Open_Disk
 argument_list|(
-name|d
-operator|->
-name|name
+name|cp
 argument_list|)
 expr_stmt|;
 if|if
@@ -1577,26 +1604,11 @@ condition|(
 operator|!
 name|d
 condition|)
-block|{
 name|msgConfirm
 argument_list|(
 literal|"Can't reopen disk %s! Internal state is probably corrupted"
 argument_list|,
-name|d
-operator|->
-name|name
-argument_list|)
-expr_stmt|;
-name|clear
-argument_list|()
-expr_stmt|;
-break|break;
-block|}
-name|Free_Disk
-argument_list|(
-name|dev
-operator|->
-name|private
+name|cp
 argument_list|)
 expr_stmt|;
 name|dev
@@ -1615,6 +1627,10 @@ argument_list|(
 name|DISK_LABELLED
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|d
+condition|)
 name|record_chunks
 argument_list|(
 name|d
