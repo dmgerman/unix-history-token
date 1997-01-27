fa16106ee1b723728fdd63178c598806f9019462
Bruce Evans <bde@FreeBSD.org>
1997-01-27 12:48:15 +0000

    Fixed EXTRAVNODES ifdef. Updated warnings about obsolete options.
    Former-commit-id: fd2b11f614169f2d86839d2b1f453aacb57d2041

--- sys/kern/kern_opt.c
+++ sys/kern/kern_opt.c
@@ -14,7 +14,7 @@ end_ifdef
 begin_warning
 warning|#
 directive|warning
-literal|"obsolete option CHILD_MAX - rename it to PROC0_RLIMIT_NPROC"
+literal|"obsolete option CHILD_MAX - use /etc/login.conf"
 end_warning
 
 begin_endif
@@ -25,7 +25,7 @@ end_endif
 begin_ifdef
 ifdef|#
 directive|ifdef
-name|GATEWAY
+name|EXTRAVNODES
 end_ifdef
 
 begin_warning
@@ -65,7 +65,7 @@ end_ifdef
 begin_warning
 warning|#
 directive|warning
-literal|"obsolete option OPEN_MAX - rename it to PROC0_RLIMIT_NOFILE"
+literal|"obsolete option OPEN_MAX - use /etc/login.conf"
 end_warning
 
 begin_endif
