1caa9e2678c848788ebac63c3c96d950002322d7
Bruce Evans <bde@FreeBSD.org>
1997-01-29 14:20:16 +0000

    YAMFC (removed `Debugger("sdopen: no slices")').
    Former-commit-id: 2d727bd97d972b7365114bfd628f0b2c559c622e

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.94 1996/09/06 23:09:18 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.95 1996/09/14 04:31:09 bde Exp $  */
 end_comment
 
 begin_include
@@ -1293,6 +1293,12 @@ name|flags
 operator|&
 name|SDEV_MEDIA_LOADED
 operator|)
+operator|&&
+name|sd
+operator|->
+name|dk_slices
+operator|!=
+name|NULL
 condition|)
 block|{
 comment|/* 		 * If somebody still has it open, then forbid re-entry. 		 */
@@ -1314,20 +1320,6 @@ goto|goto
 name|bad
 goto|;
 block|}
-if|if
-condition|(
-name|sd
-operator|->
-name|dk_slices
-operator|==
-name|NULL
-condition|)
-name|Debugger
-argument_list|(
-literal|"sdopen: no slices"
-argument_list|)
-expr_stmt|;
-else|else
 name|dsgone
 argument_list|(
 operator|&
