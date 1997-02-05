310f28a0b91c5de1f361a50120e43333eac8a0ff
David Greenman <dg@FreeBSD.org>
1997-02-05 09:56:14 +0000

    Brought in change from rev 1.14: nuke getenv(PATH_LOCALE)
    Former-commit-id: c47fd86cfe700d7e30192706d22975bed74577f8

--- lib/libc/locale/setlocale.c
+++ lib/libc/locale/setlocale.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Paul Borman at Krystal Technologies.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: setlocale.c,v 1.11 1996/11/27 22:30:44 ache Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Paul Borman at Krystal Technologies.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: setlocale.c,v 1.8.2.1 1996/11/28 21:47:18 phk Exp $  */
 end_comment
 
 begin_ifdef
@@ -16,7 +16,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: setlocale.c,v 1.11 1996/11/27 22:30:44 ache Exp $"
+literal|"$Id: setlocale.c,v 1.8.2.1 1996/11/28 21:47:18 phk Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -1159,63 +1159,10 @@ condition|(
 operator|!
 name|_PathLocale
 condition|)
-block|{
-if|if
-condition|(
-operator|!
-operator|(
-name|ret
-operator|=
-name|getenv
-argument_list|(
-literal|"PATH_LOCALE"
-argument_list|)
-operator|)
-operator|||
-name|getuid
-argument_list|()
-operator|!=
-name|geteuid
-argument_list|()
-operator|||
-name|getgid
-argument_list|()
-operator|!=
-name|getegid
-argument_list|()
-condition|)
 name|_PathLocale
 operator|=
 name|_PATH_LOCALE
 expr_stmt|;
-elseif|else
-if|if
-condition|(
-name|strlen
-argument_list|(
-name|ret
-argument_list|)
-operator|+
-literal|45
-operator|>
-name|PATH_MAX
-operator|||
-operator|!
-operator|(
-name|_PathLocale
-operator|=
-name|strdup
-argument_list|(
-name|ret
-argument_list|)
-operator|)
-condition|)
-return|return
-operator|(
-name|NULL
-operator|)
-return|;
-block|}
 if|if
 condition|(
 name|strcmp
