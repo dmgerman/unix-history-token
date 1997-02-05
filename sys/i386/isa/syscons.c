4de58cbb1882ffa39befb9e66fb7bdcd68ab186c
David Greenman <dg@FreeBSD.org>
1997-02-05 05:41:50 +0000

    Brought in the important parts of rev 1.186: Add spltty around call to close Disallow IO access to everyone if securelevel > 0 Don't corrupt memory if the cursor isn't in the 80x25 area.
    Former-commit-id: 010fdf01bc89511ab7bb649185da6ac5552b73d8

--- sys/i386/isa/syscons.c
+++ sys/i386/isa/syscons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.117.4.14 1996/11/10 19:12:48 jkh Exp $  */
+comment|/*-  * Copyright (c) 1992-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.117.4.15 1996/11/15 23:36:39 jkh Exp $  */
 end_comment
 
 begin_include
@@ -2244,6 +2244,9 @@ expr_stmt|;
 endif|#
 directive|endif
 block|}
+name|spltty
+argument_list|()
+expr_stmt|;
 operator|(
 operator|*
 name|linesw
@@ -2265,6 +2268,9 @@ name|ttyclose
 argument_list|(
 name|tp
 argument_list|)
+expr_stmt|;
+name|spl0
+argument_list|()
 expr_stmt|;
 return|return
 operator|(
@@ -4715,6 +4721,15 @@ literal|0
 condition|)
 return|return
 name|error
+return|;
+if|if
+condition|(
+name|securelevel
+operator|>
+literal|0
+condition|)
+return|return
+name|EPERM
 return|;
 name|fp
 operator|=
@@ -12047,6 +12062,25 @@ name|crtc_addr
 operator|+
 literal|1
 argument_list|)
+expr_stmt|;
+comment|/*      * Validate cursor location.  It may be off the screen.  Then we must      * not use it for the initial buffer offset.      */
+if|if
+condition|(
+name|hw_cursor
+operator|>=
+name|ROW
+operator|*
+name|COL
+condition|)
+name|hw_cursor
+operator|=
+operator|(
+name|ROW
+operator|-
+literal|1
+operator|)
+operator|*
+name|COL
 expr_stmt|;
 comment|/* move hardware cursor out of the way */
 name|outb
