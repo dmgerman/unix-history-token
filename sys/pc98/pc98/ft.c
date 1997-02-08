6b2c784329855899c7ec8faf910ff4c8e2677a31
Takenori KATO <kato@FreeBSD.org>
1997-02-08 10:01:29 +0000

    YAMFC (revision 1.10; hardcoded unit number).
    Former-commit-id: ee94bf2e497aea6c49d040d3a6b6395bbd5e9032

--- sys/pc98/pc98/ft.c
+++ sys/pc98/pc98/ft.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  Copyright (c) 1993, 1994 Steve Gerakines  *  *  This is freely redistributable software.  You may do anything you  *  wish with it, so long as the above notice stays intact.  *  *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS  *  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  *  DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT,  *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  *  POSSIBILITY OF SUCH DAMAGE.  *  *  ft.c - QIC-40/80 floppy tape driver  *  $Id: ft.c,v 1.5.2.2 1996/12/04 16:00:59 phk Exp $  *  *  01/19/95 ++sg  *  Cleaned up recalibrate/seek code at attach time for FreeBSD 2.x.  *  *  06/07/94 v0.9 ++sg  *  Tape stuck on segment problem should be gone.  Re-wrote buffering  *  scheme.  Added support for drives that do not automatically perform  *  seek load point.  Can handle more wakeup types now and should correctly  *  report most manufacturer names.  Fixed places where unit 0 was being  *  sent to the fdc instead of the actual unit number.  Added ioctl support  *  for an in-core badmap.  *  *  01/26/94 v0.3b - Jim Babb  *  Got rid of the hard coded device selection.  Moved (some of) the  *  static variables into a structure for support of multiple devices.  *  ( still has a way to go for 2 controllers - but closer )  *  Changed the interface with fd.c so we no longer 'steal' it's  *  driver routine vectors.  *  *  10/30/93 v0.3  *  Fixed a couple more bugs.  Reading was sometimes looping when an  *  an error such as address-mark-missing was encountered.  Both  *  reading and writing was having more backup-and-retries than was  *  necessary.  Added support to get hardware info.  Updated for use  *  with FreeBSD.  *  *  09/15/93 v0.2 pl01  *  Fixed a bunch of bugs:  extra isa_dmadone() in async_write() (shouldn't  *  matter), fixed double buffering in async_req(), changed tape_end() in  *  set_fdcmode() to reduce unexpected interrupts, changed end of track  *  processing in async_req(), protected more of ftreq_rw() with an  *  splbio().  Changed some of the ftreq_*() functions so that they wait  *  for inactivity and then go, instead of aborting immediately.  *  *  08/07/93 v0.2 release  *  Shifted from ftstrat to ioctl support for I/O.  Streaming is now much  *  more reliable.  Added internal support for error correction, QIC-40,  *  and variable length tapes.  Random access of segments greatly  *  improved.  Formatting and verification support is close but still  *  incomplete.  *  *  06/03/93 v0.1 Alpha release  *  Hopefully the last re-write.  Many bugs fixed, many remain.  */
+comment|/*  *  Copyright (c) 1993, 1994 Steve Gerakines  *  *  This is freely redistributable software.  You may do anything you  *  wish with it, so long as the above notice stays intact.  *  *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS  *  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  *  DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT,  *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  *  POSSIBILITY OF SUCH DAMAGE.  *  *  ft.c - QIC-40/80 floppy tape driver  *  $Id: ft.c,v 1.5.2.3 1997/01/10 06:54:36 kato Exp $  *  *  01/19/95 ++sg  *  Cleaned up recalibrate/seek code at attach time for FreeBSD 2.x.  *  *  06/07/94 v0.9 ++sg  *  Tape stuck on segment problem should be gone.  Re-wrote buffering  *  scheme.  Added support for drives that do not automatically perform  *  seek load point.  Can handle more wakeup types now and should correctly  *  report most manufacturer names.  Fixed places where unit 0 was being  *  sent to the fdc instead of the actual unit number.  Added ioctl support  *  for an in-core badmap.  *  *  01/26/94 v0.3b - Jim Babb  *  Got rid of the hard coded device selection.  Moved (some of) the  *  static variables into a structure for support of multiple devices.  *  ( still has a way to go for 2 controllers - but closer )  *  Changed the interface with fd.c so we no longer 'steal' it's  *  driver routine vectors.  *  *  10/30/93 v0.3  *  Fixed a couple more bugs.  Reading was sometimes looping when an  *  an error such as address-mark-missing was encountered.  Both  *  reading and writing was having more backup-and-retries than was  *  necessary.  Added support to get hardware info.  Updated for use  *  with FreeBSD.  *  *  09/15/93 v0.2 pl01  *  Fixed a bunch of bugs:  extra isa_dmadone() in async_write() (shouldn't  *  matter), fixed double buffering in async_req(), changed tape_end() in  *  set_fdcmode() to reduce unexpected interrupts, changed end of track  *  processing in async_req(), protected more of ftreq_rw() with an  *  splbio().  Changed some of the ftreq_*() functions so that they wait  *  for inactivity and then go, instead of aborting immediately.  *  *  08/07/93 v0.2 release  *  Shifted from ftstrat to ioctl support for I/O.  Streaming is now much  *  more reliable.  Added internal support for error correction, QIC-40,  *  and variable length tapes.  Random access of segments greatly  *  improved.  Formatting and verification support is close but still  *  incomplete.  *  *  06/03/93 v0.1 Alpha release  *  Hopefully the last re-write.  Many bugs fixed, many remain.  */
 end_comment
 
 begin_include
@@ -2664,29 +2664,6 @@ name|i
 operator|=
 literal|1
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-if|if
-condition|(
-operator|!
-name|i
-operator|&&
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-operator|<
-literal|0
-condition|)
-name|i
-operator|=
-literal|1
-expr_stmt|;
-else|#
-directive|else
 if|if
 condition|(
 operator|!
@@ -2705,8 +2682,6 @@ name|i
 operator|=
 literal|1
 expr_stmt|;
-endif|#
-directive|endif
 if|if
 condition|(
 operator|!
@@ -2994,18 +2969,6 @@ argument_list|,
 name|NE7CMD_SENSED
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -3013,8 +2976,6 @@ argument_list|,
 name|ftu
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
 name|st3
 operator|=
 name|in_fdc
@@ -3626,18 +3587,6 @@ literal|0x4a
 argument_list|)
 expr_stmt|;
 comment|/* READ_ID */
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -3645,8 +3594,6 @@ argument_list|,
 name|ftu
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
 break|break;
 case|case
 literal|1
@@ -5828,18 +5775,6 @@ literal|0x66
 argument_list|)
 expr_stmt|;
 comment|/* read */
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -5848,8 +5783,6 @@ name|ftu
 argument_list|)
 expr_stmt|;
 comment|/* unit */
-endif|#
-directive|endif
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -6585,18 +6518,6 @@ literal|0x45
 argument_list|)
 expr_stmt|;
 comment|/* write */
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -6605,8 +6526,6 @@ name|ftu
 argument_list|)
 expr_stmt|;
 comment|/* unit */
-endif|#
-directive|endif
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -7945,18 +7864,6 @@ argument_list|,
 name|NE7CMD_RECAL
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -7964,8 +7871,6 @@ argument_list|,
 name|ftu
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
 if|if
 condition|(
 name|ftintr_wait
@@ -8303,18 +8208,6 @@ argument_list|,
 name|NE7CMD_SEEK
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -8322,8 +8215,6 @@ argument_list|,
 name|ftu
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -10615,18 +10506,6 @@ argument_list|,
 name|NE7CMD_SENSED
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -10634,8 +10513,6 @@ argument_list|,
 name|ftu
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
 name|st3
 operator|=
 name|in_fdc
@@ -10722,18 +10599,6 @@ argument_list|,
 name|NE7CMD_SENSED
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-name|out_fdc
-argument_list|(
-name|fdcu
-argument_list|,
-literal|3
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
 name|out_fdc
 argument_list|(
 name|fdcu
@@ -10741,8 +10606,6 @@ argument_list|,
 name|ftu
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
 name|st3
 operator|=
 name|in_fdc
