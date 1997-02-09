c8b782709c0b1c32ff2d7316498452028b30876a
Joerg Wunsch <joerg@FreeBSD.org>
1997-02-09 20:38:03 +0000

    YAMFC (add bradelf.1, and merge rev 1.3 of brandelf.c)
    Former-commit-id: dc32c3395189894114062bd397785a01d61f47a4

--- usr.bin/brandelf/brandelf.c
+++ usr.bin/brandelf/brandelf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1996 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id$  */
+comment|/*-  * Copyright (c) 1996 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: brandelf.c,v 1.1 1996/10/16 18:16:22 sos Exp $  */
 end_comment
 
 begin_include
@@ -18,6 +18,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<fcntl.h>
 end_include
 
@@ -30,11 +42,14 @@ end_include
 begin_function_decl
 name|int
 name|usage
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 function_decl|;
 end_function_decl
 
 begin_function
+name|int
 name|main
 parameter_list|(
 name|int
@@ -46,22 +61,17 @@ modifier|*
 name|argv
 parameter_list|)
 block|{
-specifier|extern
+specifier|const
 name|char
 modifier|*
-name|optarg
-decl_stmt|;
-specifier|extern
-name|int
-name|optind
-decl_stmt|;
-name|char
 name|type
-index|[
-literal|10
-index|]
 init|=
 literal|"FreeBSD"
+decl_stmt|;
+name|int
+name|retval
+init|=
+literal|0
 decl_stmt|;
 name|int
 name|ch
@@ -111,12 +121,9 @@ name|change
 operator|=
 literal|1
 expr_stmt|;
-name|strcpy
-argument_list|(
 name|type
-argument_list|,
+operator|=
 name|optarg
-argument_list|)
 expr_stmt|;
 break|break;
 default|default:
@@ -210,6 +217,10 @@ literal|0
 index|]
 argument_list|)
 expr_stmt|;
+name|retval
+operator|=
+literal|1
+expr_stmt|;
 goto|goto
 name|fail
 goto|;
@@ -239,6 +250,10 @@ index|[
 literal|0
 index|]
 argument_list|)
+expr_stmt|;
+name|retval
+operator|=
+literal|1
 expr_stmt|;
 goto|goto
 name|fail
@@ -286,6 +301,10 @@ index|[
 literal|0
 index|]
 argument_list|)
+expr_stmt|;
+name|retval
+operator|=
+literal|1
 expr_stmt|;
 goto|goto
 name|fail
@@ -411,6 +430,10 @@ literal|0
 index|]
 argument_list|)
 expr_stmt|;
+name|retval
+operator|=
+literal|1
+expr_stmt|;
 goto|goto
 name|fail
 goto|;
@@ -425,19 +448,29 @@ name|argv
 operator|++
 expr_stmt|;
 block|}
+return|return
+name|retval
+return|;
 block|}
 end_function
 
 begin_function
 name|int
 name|usage
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
 literal|"Usage: brandelf [-t string] file ...\n"
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+literal|1
 argument_list|)
 expr_stmt|;
 block|}
