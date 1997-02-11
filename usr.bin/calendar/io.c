cabc6909df2e3a7d5aa57d498ac3e0c5d771329b
Mike Pritchard <mpp@FreeBSD.org>
1997-02-11 03:45:14 +0000

    Fix a couple of typos.
    Submitted by:	bde
    
    
    Former-commit-id: a8e39a9b83d7eb8ce0c7c4c88b270ff21e054144

--- usr.bin/calendar/io.c
+++ usr.bin/calendar/io.c
@@ -1496,7 +1496,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"calendar: setegid failed\n"
+literal|"calendar: setgid failed\n"
 argument_list|)
 expr_stmt|;
 name|_exit
@@ -1519,7 +1519,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"caelndar: setuid failed\n"
+literal|"calendar: setuid failed\n"
 argument_list|)
 expr_stmt|;
 name|_exit
@@ -1874,7 +1874,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"calendar: setegid failed\n"
+literal|"calendar: setgid failed\n"
 argument_list|)
 expr_stmt|;
 name|_exit
@@ -1897,7 +1897,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"caelndar: setuid failed\n"
+literal|"calendar: setuid failed\n"
 argument_list|)
 expr_stmt|;
 name|_exit
