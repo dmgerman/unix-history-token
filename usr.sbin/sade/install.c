58099628cb868a5ce2b09a7f5328cf8dea30a31f
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-02-11 02:13:06 +0000

    -#include <ufs/ufs/ufsmount.h>
    This was a temporary measure I took to deal with John's 4.4L2 changes
    which should not have crept in.
    
    
    Former-commit-id: 34bbf4e63af4fe670335e6f228cd1866170bb055

--- usr.sbin/sade/install.c
+++ usr.sbin/sade/install.c
@@ -87,12 +87,6 @@ directive|include
 file|<unistd.h>
 end_include
 
-begin_include
-include|#
-directive|include
-file|<ufs/ufs/ufsmount.h>
-end_include
-
 begin_function_decl
 specifier|static
 name|void
