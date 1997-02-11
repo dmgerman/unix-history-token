772170026efcd7a28c4c3e193fd9e0c81a7c50dc
Garrett Wollman <wollman@FreeBSD.org>
1997-02-11 20:46:06 +0000

    Fix remote request for HTTP proxies.  Should close PR#2670.
    Former-commit-id: 249bc2748ad479a6c317f04c7c4e58af73838f10

--- usr.bin/fetch/http.c
+++ usr.bin/fetch/http.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright 1997 Massachusetts Institute of Technology  *  * Permission to use, copy, modify, and distribute this software and  * its documentation for any purpose and without fee is hereby  * granted, provided that both the above copyright notice and this  * permission notice appear in all copies, that both the above  * copyright notice and this permission notice appear in all  * supporting documentation, and that the name of M.I.T. not be used  * in advertising or publicity pertaining to distribution of the  * software without specific, written prior permission.  M.I.T. makes  * no representations about the suitability of this software for any  * purpose.  It is provided "as is" without express or implied  * warranty.  *   * THIS SOFTWARE IS PROVIDED BY M.I.T. ``AS IS''.  M.I.T. DISCLAIMS  * ALL EXPRESS OR IMPLIED WARRANTIES WITH REGARD TO THIS SOFTWARE,  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT  * SHALL M.I.T. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: http.c,v 1.2 1997/01/31 19:55:50 wollman Exp $  */
+comment|/*-  * Copyright 1997 Massachusetts Institute of Technology  *  * Permission to use, copy, modify, and distribute this software and  * its documentation for any purpose and without fee is hereby  * granted, provided that both the above copyright notice and this  * permission notice appear in all copies, that both the above  * copyright notice and this permission notice appear in all  * supporting documentation, and that the name of M.I.T. not be used  * in advertising or publicity pertaining to distribution of the  * software without specific, written prior permission.  M.I.T. makes  * no representations about the suitability of this software for any  * purpose.  It is provided "as is" without express or implied  * warranty.  *   * THIS SOFTWARE IS PROVIDED BY M.I.T. ``AS IS''.  M.I.T. DISCLAIMS  * ALL EXPRESS OR IMPLIED WARRANTIES WITH REGARD TO THIS SOFTWARE,  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT  * SHALL M.I.T. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: http.c,v 1.3 1997/02/05 19:59:14 wollman Exp $  */
 end_comment
 
 begin_include
@@ -878,8 +878,6 @@ block|}
 name|p
 operator|=
 name|slash
-operator|+
-literal|1
 expr_stmt|;
 name|https
 operator|=
@@ -943,6 +941,7 @@ argument_list|(
 name|hosthdr
 argument_list|)
 expr_stmt|;
+comment|/* 	 * NB: HTTP/1.1 servers MUST also accept a full URI. 	 * However, HTTP/1.0 servers will ONLY accept a trimmed URI. 	 */
 name|https
 operator|->
 name|http_remote_request
@@ -951,6 +950,9 @@ name|safe_strdup
 argument_list|(
 name|p
 argument_list|)
+expr_stmt|;
+name|p
+operator|++
 expr_stmt|;
 name|ques
 operator|=
@@ -2239,7 +2241,7 @@ name|iov
 argument_list|,
 name|n
 argument_list|,
-literal|"GET /"
+literal|"GET "
 argument_list|)
 expr_stmt|;
 name|addstr
@@ -4064,6 +4066,7 @@ name|restart_from
 argument_list|)
 expr_stmt|;
 comment|/* XXX truncation */
+comment|/* 	 * Eventually this loop will be separated out as http_suck(), and 	 * there will be a separate http_suck_chunked() to deal with that 	 * Transfer-Encoding. 	 */
 do|do
 block|{
 name|alarm
