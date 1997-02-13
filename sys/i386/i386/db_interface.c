f95ee9a9dbafbb767e1d3ca50fe4466d9c4bae3b
Bruce Evans <bde@FreeBSD.org>
1997-02-13 07:06:53 +0000

    YAMFC (fixed benign type mismatch; fixed writing just above etext);
    Former-commit-id: f16c10146845053220b0599dcd79dfe51fe91628

--- sys/i386/i386/db_interface.c
+++ sys/i386/i386/db_interface.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_interface.c,v 1.22 1996/08/30 17:03:45 pst Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_interface.c,v 1.23 1996/09/28 22:37:28 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -579,36 +579,30 @@ name|char
 modifier|*
 name|dst
 decl_stmt|;
-specifier|register
-name|pt_entry_t
+name|unsigned
 modifier|*
 name|ptep0
 init|=
-literal|0
+name|NULL
 decl_stmt|;
-name|pt_entry_t
+name|unsigned
 name|oldmap0
 init|=
-block|{
 literal|0
-block|}
 decl_stmt|;
 name|vm_offset_t
 name|addr1
 decl_stmt|;
-specifier|register
-name|pt_entry_t
+name|unsigned
 modifier|*
 name|ptep1
 init|=
-literal|0
+name|NULL
 decl_stmt|;
-name|pt_entry_t
+name|unsigned
 name|oldmap1
 init|=
-block|{
 literal|0
-block|}
 decl_stmt|;
 name|db_nofault
 operator|=
@@ -623,11 +617,14 @@ name|VM_MIN_KERNEL_ADDRESS
 operator|&&
 name|addr
 operator|<=
+name|round_page
+argument_list|(
 operator|(
 name|vm_offset_t
 operator|)
 operator|&
 name|etext
+argument_list|)
 condition|)
 block|{
 name|ptep0
@@ -645,13 +642,8 @@ operator|*
 name|ptep0
 expr_stmt|;
 operator|*
-operator|(
-name|int
-operator|*
-operator|)
 name|ptep0
 operator||=
-comment|/* INTEL_PTE_WRITE */
 name|PG_RW
 expr_stmt|;
 name|addr1
@@ -691,13 +683,8 @@ operator|*
 name|ptep1
 expr_stmt|;
 operator|*
-operator|(
-name|int
-operator|*
-operator|)
 name|ptep1
 operator||=
-comment|/* INTEL_PTE_WRITE */
 name|PG_RW
 expr_stmt|;
 block|}
