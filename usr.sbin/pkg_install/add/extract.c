7bc238ba6835f51814c9f33eef3afe89d25448fa
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-02-14 01:54:12 +0000

    YAMFC.
    Former-commit-id: e5674d1f9f78baaa3b4652a70851c4510e9d259c

--- usr.sbin/pkg_install/add/extract.c
+++ usr.sbin/pkg_install/add/extract.c
@@ -12,7 +12,7 @@ name|char
 modifier|*
 name|rcsid
 init|=
-literal|"$Id: extract.c,v 1.6 1994/12/06 00:51:32 jkh Exp $"
+literal|"$Id: extract.c,v 1.7 1995/05/19 22:40:54 jkh Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -63,7 +63,7 @@ name|todir
 parameter_list|)
 comment|/* push out string */
 define|\
-value|if (strlen(where_args)> sizeof(STARTSTRING)-1) { \ 		    strcat(where_args, "|tar xf - -C "); \ 		    strcat(where_args, todir); \ 		    if (system(where_args)) \ 			barf("can't invoke tar pipeline"); \ 		    strcpy(where_args, STARTSTRING); \ 		    where_count = sizeof(STARTSTRING)-1; \ 	} \ 	if (perm_count) { \ 		    if (!isdir(todir)) apply_perms(todir, perm_args); \ 		    perm_args[0] = 0;\ 		    perm_count = 0; \ 	}
+value|if (strlen(where_args)> sizeof(STARTSTRING)-1) { \ 		    strcat(where_args, "|tar xf - -C "); \ 		    strcat(where_args, todir); \ 		    if (system(where_args)) \ 			barf("can't invoke tar pipeline"); \ 		    strcpy(where_args, STARTSTRING); \ 		    where_count = sizeof(STARTSTRING)-1; \ 	} \ 	if (perm_count) { \ 		    apply_perms(todir, perm_args); \ 		    perm_args[0] = 0;\ 		    perm_count = 0; \ 	}
 end_define
 
 begin_function
