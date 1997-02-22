b136f5f4e5c29831e1a2e126502b451fa970e8ab
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-02-22 07:34:11 +0000

    Commit a very minor typo fix to a prototype. Submitted-By: Bill Richter <richterb@binkley.wellsfargo.com>
    Former-commit-id: d643c675ce6f02e40395cfe6ad9f46fca0175877

--- usr.sbin/newsyslog/newsyslog.c
+++ usr.sbin/newsyslog/newsyslog.c
@@ -23,7 +23,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: newsyslog.c,v 1.6 1996/09/06 06:36:07 jkh Exp $"
+literal|"$Id: newsyslog.c,v 1.7 1996/09/09 15:32:26 jkh Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -585,7 +585,7 @@ name|int
 name|numdays
 parameter_list|,
 name|int
-name|falgs
+name|flags
 parameter_list|,
 name|int
 name|perm
