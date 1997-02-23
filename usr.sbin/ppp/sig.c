e4f1679288b94decf840ba4983648cd254b36047
Brian Somers <brian@FreeBSD.org>
1997-02-23 20:01:20 +0000

    Requested by:	Elvind Eklund Remove usage of __sighandler_t as it's not available in 2.1*
    Former-commit-id: 30f6c7983037a594a90a1c1b4e72d122a82ccb84

--- usr.sbin/ppp/sig.c
+++ usr.sbin/ppp/sig.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997  *	Brian Somers<brian@awfulhak.demon.co.uk>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id$  *  *  TODO:  *  */
+comment|/*-  * Copyright (c) 1997  *	Brian Somers<brian@awfulhak.demon.co.uk>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: sig.c,v 1.2 1997/02/22 16:10:51 peter Exp $  *  *  TODO:  *  */
 end_comment
 
 begin_include
@@ -51,8 +51,7 @@ end_comment
 
 begin_decl_stmt
 specifier|static
-name|__sighandler_t
-modifier|*
+name|sig_type
 name|handler
 index|[
 name|__MAXSIG
@@ -107,20 +106,17 @@ comment|/*     set up signal_recorder, and record handler as the function to ult
 end_comment
 
 begin_function
-name|__sighandler_t
-modifier|*
+name|sig_type
 name|pending_signal
 parameter_list|(
 name|int
 name|sig
 parameter_list|,
-name|__sighandler_t
-modifier|*
+name|sig_type
 name|fn
 parameter_list|)
 block|{
-name|__sighandler_t
-modifier|*
+name|sig_type
 name|Result
 decl_stmt|;
 if|if
@@ -181,8 +177,7 @@ literal|1
 index|]
 operator|=
 operator|(
-name|__sighandler_t
-operator|*
+name|sig_type
 operator|)
 literal|0
 expr_stmt|;
