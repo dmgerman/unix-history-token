f0f7e0f646e97afb77ab7d057828a6180a0614fc
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-03-09 22:25:49 +0000

    Add a registration screen so we can finally start counting our little user puppies.
    Former-commit-id: 97084205d115c834a9d894d0ce3d210dc81f43a7

--- usr.sbin/sade/menus.c
+++ usr.sbin/sade/menus.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: menus.c,v 1.119 1997/02/22 14:12:05 peter Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: menus.c,v 1.120 1997/03/08 12:57:45 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -1206,6 +1206,34 @@ block|,
 name|NULL
 block|,
 literal|"pcnfsd"
+block|}
+block|,
+block|{
+literal|"Register"
+block|,
+literal|"Register yourself or company as a FreeBSD user."
+block|,
+name|dmenuVarCheck
+block|,
+name|configRegister
+block|,
+name|NULL
+block|,
+literal|"registered"
+block|}
+block|,
+block|{
+literal|"Root Password"
+block|,
+literal|"Set the system manager's password."
+block|,
+name|NULL
+block|,
+name|dmenuSystemCommand
+block|,
+name|NULL
+block|,
+literal|"passwd root"
 block|}
 block|,
 block|{
@@ -6582,7 +6610,7 @@ name|docBrowser
 block|}
 block|,
 block|{
-literal|"B XFree86"
+literal|"X XFree86"
 block|,
 literal|"Configure XFree86"
 block|,
@@ -6622,7 +6650,31 @@ name|diskPartitionEditor
 block|}
 block|,
 block|{
-literal|"Exit"
+literal|"S Set Root Password"
+block|,
+literal|"Set the system manager's password."
+block|,
+name|NULL
+block|,
+name|dmenuSystemCommand
+block|,
+name|NULL
+block|,
+literal|"passwd root"
+block|}
+block|,
+block|{
+literal|"R Register"
+block|,
+literal|"Register yourself or company as a FreeBSD user."
+block|,
+name|NULL
+block|,
+name|configRegister
+block|}
+block|,
+block|{
+literal|"E Exit"
 block|,
 literal|"Exit this menu (returning to previous)"
 block|,
