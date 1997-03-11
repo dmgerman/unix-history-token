186f5be328bc2a53fd18f31b1c5b6c1adf447a05
Peter Wemm <peter@FreeBSD.org>
1997-03-11 15:57:44 +0000

    Update to work under Lite2 includes
    Former-commit-id: 6c5d2bebb701508dabdd58766e7d4d8d8a728b40

--- usr.sbin/fdcontrol/fdcontrol.c
+++ usr.sbin/fdcontrol/fdcontrol.c
@@ -18,6 +18,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/ioctl_fd.h>
 end_include
 
--- usr.sbin/qcamcontrol/qcamcontrol.c
+++ usr.sbin/qcamcontrol/qcamcontrol.c
@@ -18,6 +18,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/fcntl.h>
 end_include
 
