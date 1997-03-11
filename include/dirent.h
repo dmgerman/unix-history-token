5bfa506217de56045e3e86c76093a21e3332db58
Peter Wemm <peter@FreeBSD.org>
1997-03-11 11:15:46 +0000

    Merge Lite2 changes onto mainline:  - declare opendir2(), which is used to control how readdir() handles  whiteouts etc.
    Former-commit-id: b9261210c8efeb8c31f93b23361cf27b3d22127f

--- include/dirent.h
+++ include/dirent.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)dirent.h	8.1 (Berkeley) 6/8/93  */
+comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)dirent.h	8.2 (Berkeley) 7/28/94  */
 end_comment
 
 begin_ifndef
@@ -104,6 +104,10 @@ name|long
 name|dd_rewind
 decl_stmt|;
 comment|/* magic cookie for rewinding */
+name|int
+name|dd_flags
+decl_stmt|;
+comment|/* flags for readdir */
 block|}
 name|DIR
 typedef|;
@@ -118,6 +122,54 @@ name|dirp
 parameter_list|)
 value|((dirp)->dd_fd)
 end_define
+
+begin_comment
+comment|/* flags for opendir2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DTF_HIDEW
+value|0x0001
+end_define
+
+begin_comment
+comment|/* hide whiteout entries */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DTF_NODUP
+value|0x0002
+end_define
+
+begin_comment
+comment|/* don't return duplicate names */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DTF_REWIND
+value|0x0004
+end_define
+
+begin_comment
+comment|/* rewind after reading union stack */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__DTF_READALL
+value|0x0008
+end_define
+
+begin_comment
+comment|/* everything has been read */
+end_comment
 
 begin_ifndef
 ifndef|#
@@ -220,6 +272,23 @@ ifndef|#
 directive|ifndef
 name|_POSIX_SOURCE
 end_ifndef
+
+begin_decl_stmt
+name|DIR
+modifier|*
+name|__opendir2
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|long
