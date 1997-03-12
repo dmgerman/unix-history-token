77642202383ff3839f89e95722afc95bedc70197
Bruce Evans <bde@FreeBSD.org>
1997-03-12 16:49:28 +0000

    Finished (?) merging with Lite2: cleaned up #include mess.
    Fixed misformatting in a comment.
    
    
    Former-commit-id: 4a13e68af8277425dbb430c374b923881edbad05

--- sbin/fsck_ffs/dir.c
+++ sbin/fsck_ffs/dir.c
@@ -62,18 +62,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stdio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<stdlib.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<err.h>
 end_include
 
