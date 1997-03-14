250ea646692dda8bc26c27f9fd1d791f0d052cc9
Garrett Wollman <wollman@FreeBSD.org>
1997-03-14 16:51:28 +0000

    Add protoswitch entries for shortcut send/receive.  Correct a few misleading comments, and move allthe struct tag forward declarations to be in one place.
    Former-commit-id: de378946826fdf598e4df8730f6c02dd2ca92169

--- sys/sys/protosw.h
+++ sys/sys/protosw.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)protosw.h	8.1 (Berkeley) 6/2/93  * $Id$  */
+comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)protosw.h	8.1 (Berkeley) 6/2/93  *	$Id: protosw.h,v 1.15 1997/02/22 09:45:42 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -14,6 +14,16 @@ define|#
 directive|define
 name|_SYS_PROTOSW_H_
 end_define
+
+begin_comment
+comment|/* Forward declare these structures referenced from prototypes below. */
+end_comment
+
+begin_struct_decl
+struct_decl|struct
+name|ifnet
+struct_decl|;
+end_struct_decl
 
 begin_struct_decl
 struct_decl|struct
@@ -36,6 +46,18 @@ end_struct_decl
 begin_struct_decl
 struct_decl|struct
 name|sockproto
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|stat
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|uio
 struct_decl|;
 end_struct_decl
 
@@ -676,20 +698,8 @@ begin_comment
 comment|/* users shouldn't see this decl */
 end_comment
 
-begin_struct_decl
-struct_decl|struct
-name|stat
-struct_decl|;
-end_struct_decl
-
-begin_struct_decl
-struct_decl|struct
-name|ifnet
-struct_decl|;
-end_struct_decl
-
 begin_comment
-comment|/*  * If the ordering here looks odd, that's because it's alphabetical.  */
+comment|/*  * If the ordering here looks odd, that's because it's alphabetical.  * Having this structure separated out from the main protoswitch is actually  * a big (12 cycles per call) lose on high-end CPUs.  We will eventually  * migrate this stuff back into the main structure.  */
 end_comment
 
 begin_struct
@@ -928,7 +938,6 @@ name|flags
 operator|)
 argument_list|)
 expr_stmt|;
-comment|/* 	 * The `m' parameter here is almost certainly going to become a 	 * `struct uio' at some point in the future.  Similar changes 	 * will probably happen for the receive entry points. 	 */
 name|int
 argument_list|(
 argument|*pru_send
@@ -1018,6 +1027,85 @@ expr|struct
 name|mbuf
 operator|*
 name|nam
+operator|)
+argument_list|)
+expr_stmt|;
+comment|/* 	 * These two added later, so they are out of order.  They are used 	 * for shortcutting (fast path input/output) in some protocols. 	 * XXX - that's a lie, they are not implemented yet 	 */
+name|int
+argument_list|(
+argument|*pru_sosend
+argument_list|)
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|socket
+operator|*
+name|so
+operator|,
+expr|struct
+name|mbuf
+operator|*
+name|addr
+operator|,
+expr|struct
+name|uio
+operator|*
+name|uio
+operator|,
+expr|struct
+name|mbuf
+operator|*
+name|top
+operator|,
+expr|struct
+name|mbuf
+operator|*
+name|control
+operator|,
+name|int
+name|flags
+operator|)
+argument_list|)
+expr_stmt|;
+name|int
+argument_list|(
+argument|*pru_soreceive
+argument_list|)
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|socket
+operator|*
+name|so
+operator|,
+expr|struct
+name|mbuf
+operator|*
+operator|*
+name|paddr
+operator|,
+expr|struct
+name|uio
+operator|*
+name|uio
+operator|,
+expr|struct
+name|mbuf
+operator|*
+operator|*
+name|mp0
+operator|,
+expr|struct
+name|mbuf
+operator|*
+operator|*
+name|controlp
+operator|,
+name|int
+operator|*
+name|flagsp
 operator|)
 argument_list|)
 expr_stmt|;
