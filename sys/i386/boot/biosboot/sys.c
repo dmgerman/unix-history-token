f944ba8f3ffa6687a949333aafbd560a20964162
Bruce Evans <bde@FreeBSD.org>
1997-03-15 16:49:51 +0000

    Don't attempt to read past EOF.
    Saved enough bytes to make room for this bloat (testing poff < inode.i_size
    takes 56 bytes!) by copying fs to a local variable.
    
    
    Former-commit-id: 77ae8c63f2496febf2e3ee7f9dd2f5a45dc522f1

--- sys/i386/boot/biosboot/sys.c
+++ sys/i386/boot/biosboot/sys.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:36:34  rpd  *	$Id$  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:36:34  rpd  *	$Id: sys.c,v 1.18 1997/02/22 09:30:14 peter Exp $  */
 end_comment
 
 begin_include
@@ -237,16 +237,33 @@ name|cnt2
 decl_stmt|,
 name|bnum2
 decl_stmt|;
+name|struct
+name|fs
+modifier|*
+name|fs_copy
+decl_stmt|;
 while|while
 condition|(
 name|count
+operator|>
+literal|0
+operator|&&
+name|poff
+operator|<
+name|inode
+operator|.
+name|i_size
 condition|)
 block|{
+name|fs_copy
+operator|=
+name|fs
+expr_stmt|;
 name|off
 operator|=
 name|blkoff
 argument_list|(
-name|fs
+name|fs_copy
 argument_list|,
 name|poff
 argument_list|)
@@ -255,7 +272,7 @@ name|logno
 operator|=
 name|lblkno
 argument_list|(
-name|fs
+name|fs_copy
 argument_list|,
 name|poff
 argument_list|)
@@ -266,7 +283,7 @@ name|size
 operator|=
 name|blksize
 argument_list|(
-name|fs
+name|fs_copy
 argument_list|,
 operator|&
 name|inode
@@ -278,7 +295,7 @@ name|bnum2
 operator|=
 name|fsbtodb
 argument_list|(
-name|fs
+name|fs_copy
 argument_list|,
 name|block_map
 argument_list|(
