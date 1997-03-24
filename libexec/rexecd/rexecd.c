2666b21b248478df1f049ccf18de2d6c429bb374
Warner Losh <imp@FreeBSD.org>
1997-03-24 05:58:57 +0000

    YAMFC: 1.14 chdir as user
    Former-commit-id: cead173274cbdd2048608a2ab286536f30124a91

--- libexec/rexecd/rexecd.c
+++ libexec/rexecd/rexecd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: rexecd.c,v 1.8.2.2 1996/11/23 08:25:36 phk Exp $  */
+comment|/*  * Copyright (c) 1983, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: rexecd.c,v 1.8.2.3 1997/02/09 04:40:52 imp Exp $  */
 end_comment
 
 begin_ifndef
@@ -170,7 +170,11 @@ begin_decl_stmt
 name|char
 name|username
 index|[
-literal|20
+name|MAXLOGNAME
+operator|+
+literal|5
+operator|+
+literal|1
 index|]
 init|=
 literal|"USER="
@@ -181,7 +185,11 @@ begin_decl_stmt
 name|char
 name|homedir
 index|[
-literal|64
+name|MAXPATHLEN
+operator|+
+literal|5
+operator|+
+literal|1
 index|]
 init|=
 literal|"HOME="
@@ -192,7 +200,11 @@ begin_decl_stmt
 name|char
 name|shell
 index|[
-literal|64
+name|MAXPATHLEN
+operator|+
+literal|6
+operator|+
+literal|1
 index|]
 init|=
 literal|"SHELL="
@@ -1131,29 +1143,6 @@ argument_list|,
 name|user
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|chdir
-argument_list|(
-name|pwd
-operator|->
-name|pw_dir
-argument_list|)
-operator|<
-literal|0
-condition|)
-block|{
-name|error
-argument_list|(
-literal|"No remote directory.\n"
-argument_list|)
-expr_stmt|;
-name|exit
-argument_list|(
-literal|1
-argument_list|)
-expr_stmt|;
-block|}
 operator|(
 name|void
 operator|)
@@ -1664,6 +1653,29 @@ name|pwd
 operator|->
 name|pw_shell
 expr_stmt|;
+if|if
+condition|(
+name|chdir
+argument_list|(
+name|pwd
+operator|->
+name|pw_dir
+argument_list|)
+operator|<
+literal|0
+condition|)
+block|{
+name|error
+argument_list|(
+literal|"No remote directory.\n"
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
+block|}
 name|execl
 argument_list|(
 name|pwd
