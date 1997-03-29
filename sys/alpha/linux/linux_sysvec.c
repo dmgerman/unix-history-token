333e2e139035d18c4c21737e77bad9b46a0ce6be
Peter Wemm <peter@FreeBSD.org>
1997-03-29 10:50:27 +0000

    Fix the warning when compiling the SYSINIT hook due to function types.
    Former-commit-id: 4d176953f766078dddb35771605f96638f049dbe

--- sys/alpha/linux/linux_sysvec.c
+++ sys/alpha/linux/linux_sysvec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994-1996 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer   *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id$  */
+comment|/*-  * Copyright (c) 1994-1996 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer   *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: linux_sysvec.c,v 1.11 1997/02/22 09:38:26 peter Exp $  */
 end_comment
 
 begin_comment
@@ -290,6 +290,21 @@ name|mask
 operator|,
 name|u_long
 name|code
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|void
+name|linux_elf_init
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|*
+name|dummy
 operator|)
 argument_list|)
 decl_stmt|;
@@ -2208,6 +2223,46 @@ begin_comment
 comment|/*  * XXX: this is WRONG, it needs to be SI_SUB_EXEC, but this is just at the  * "proof of concept" stage and will be fixed shortly  */
 end_comment
 
+begin_function
+specifier|static
+name|void
+name|linux_elf_init
+parameter_list|(
+name|dummy
+parameter_list|)
+name|void
+modifier|*
+name|dummy
+decl_stmt|;
+block|{
+if|if
+condition|(
+name|elf_insert_brand_entry
+argument_list|(
+operator|&
+name|linux_brand
+argument_list|)
+operator|<
+literal|0
+condition|)
+name|printf
+argument_list|(
+literal|"cannot insert Linux elf brand handler\n"
+argument_list|)
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+name|bootverbose
+condition|)
+name|printf
+argument_list|(
+literal|"Linux-ELF exec handler installed\n"
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
 begin_expr_stmt
 name|SYSINIT
 argument_list|(
@@ -2217,10 +2272,9 @@ name|SI_SUB_VFS
 argument_list|,
 name|SI_ORDER_ANY
 argument_list|,
-name|elf_insert_brand_entry
+name|linux_elf_init
 argument_list|,
-operator|&
-name|linux_brand
+name|NULL
 argument_list|)
 expr_stmt|;
 end_expr_stmt
