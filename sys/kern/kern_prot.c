dd27f59ea652838e531c32a8c65a8fbc06a92087
Peter Wemm <peter@FreeBSD.org>
1997-03-31 13:41:49 +0000

    Do not set the P_SUGID flag in seteuid()/setegid() if nothing has changed. (for supporting issetugid())
    Former-commit-id: f034dd540a8a74d39c2513a30f6a9263c0db7e28

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_prot.c	8.6 (Berkeley) 1/21/94  * $Id: kern_prot.c,v 1.27 1997/03/31 13:21:37 peter Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_prot.c	8.6 (Berkeley) 1/21/94  * $Id: kern_prot.c,v 1.28 1997/03/31 13:36:46 peter Exp $  */
 end_comment
 
 begin_comment
@@ -1545,12 +1545,14 @@ name|pc
 operator|->
 name|p_ruid
 operator|&&
+comment|/* allow seteuid(getuid()) */
 name|euid
 operator|!=
 name|pc
 operator|->
 name|p_svuid
 operator|&&
+comment|/* allow seteuid(saved uid) */
 operator|(
 name|error
 operator|=
@@ -1573,6 +1575,17 @@ name|error
 operator|)
 return|;
 comment|/* 	 * Everything's okay, do it.  Copy credentials so other references do 	 * not see our changes. 	 */
+if|if
+condition|(
+name|pc
+operator|->
+name|pc_ucred
+operator|->
+name|cr_uid
+operator|!=
+name|euid
+condition|)
+block|{
 name|pc
 operator|->
 name|pc_ucred
@@ -1598,6 +1611,7 @@ name|p_flag
 operator||=
 name|P_SUGID
 expr_stmt|;
+block|}
 return|return
 operator|(
 literal|0
@@ -1963,12 +1977,14 @@ name|pc
 operator|->
 name|p_rgid
 operator|&&
+comment|/* allow setegid(getgid()) */
 name|egid
 operator|!=
 name|pc
 operator|->
 name|p_svgid
 operator|&&
+comment|/* allow setegid(saved gid) */
 operator|(
 name|error
 operator|=
@@ -1990,6 +2006,20 @@ operator|(
 name|error
 operator|)
 return|;
+if|if
+condition|(
+name|pc
+operator|->
+name|pc_ucred
+operator|->
+name|cr_groups
+index|[
+literal|0
+index|]
+operator|!=
+name|egid
+condition|)
+block|{
 name|pc
 operator|->
 name|pc_ucred
@@ -2018,6 +2048,7 @@ name|p_flag
 operator||=
 name|P_SUGID
 expr_stmt|;
+block|}
 return|return
 operator|(
 literal|0
