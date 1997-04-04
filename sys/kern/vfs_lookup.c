4acf7a62571880efdccc41f7cec06f50f53f7270
Doug Rabson <dfr@FreeBSD.org>
1997-04-04 17:46:21 +0000

    Add a function vop_sharedlock which a copy of vop_nolock without the implementation #ifdef out.  This can be used for now by NFS.  As soon as all the other filesystems' locking is fixed, this can go away.
    Print the vnode address in vprint for easier debugging.
    
    
    Former-commit-id: 3f1fc0b4fb3175251b041d555e3477d7a35c1825

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_lookup.c	8.4 (Berkeley) 2/16/94  * $Id$  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_lookup.c	8.4 (Berkeley) 2/16/94  * $Id: vfs_lookup.c,v 1.16 1997/02/22 09:39:33 peter Exp $  */
 end_comment
 
 begin_include
@@ -1585,6 +1585,13 @@ name|ni_vp
 operator|=
 name|NULL
 expr_stmt|;
+name|ASSERT_VOP_LOCKED
+argument_list|(
+name|dp
+argument_list|,
+literal|"lookup"
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|error
@@ -1786,6 +1793,15 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+name|ASSERT_VOP_LOCKED
+argument_list|(
+name|ndp
+operator|->
+name|ni_vp
+argument_list|,
+literal|"lookup"
+argument_list|)
+expr_stmt|;
 comment|/* 	 * Take into account any additional components consumed by 	 * the underlying filesystem. 	 */
 if|if
 condition|(
@@ -2016,6 +2032,27 @@ name|ndp
 operator|->
 name|ni_pathlen
 operator|--
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|ndp
+operator|->
+name|ni_dvp
+operator|!=
+name|ndp
+operator|->
+name|ni_vp
+condition|)
+block|{
+name|ASSERT_VOP_UNLOCKED
+argument_list|(
+name|ndp
+operator|->
+name|ni_dvp
+argument_list|,
+literal|"lookup"
+argument_list|)
 expr_stmt|;
 block|}
 name|vrele
