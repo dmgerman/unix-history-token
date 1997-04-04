98854e99a5fa0dc870bd97f0db93177d274a0310
Doug Rabson <dfr@FreeBSD.org>
1997-04-04 17:43:32 +0000

    Add some debugging macros for tracing VFS locking bugs. Declare (hopefully short-lived) vop_sharedlock.
    Former-commit-id: 0f52fb9f3714c0570faebbf6d23aa6d52866a0dc

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vnode.h	8.7 (Berkeley) 2/4/94  * $Id$  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vnode.h	8.7 (Berkeley) 2/4/94  * $Id: vnode.h,v 1.42 1997/02/22 09:46:29 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -1630,6 +1630,88 @@ block|}
 struct|;
 end_struct
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|DEBUG_VFS_LOCKS
+end_ifdef
+
+begin_comment
+comment|/*  * Macros to aid in tracing VFS locking problems.  Not totally  * reliable since if the process sleeps between changing the lock  * state and checking it with the assert, some other process could  * change the state.  They are good enough for debugging a single  * filesystem using a single-threaded test.  I find that 'cvs co src'  * is a pretty good test.  */
+end_comment
+
+begin_comment
+comment|/*  * [dfr] Kludge until I get around to fixing all the vfs locking.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IS_LOCKING_VFS
+parameter_list|(
+name|vp
+parameter_list|)
+value|((vp)->v_tag == VT_UFS		\ 				 || (vp)->v_tag == VT_MFS	\ 				 || (vp)->v_tag == VT_NFS	\ 				 || (vp)->v_tag == VT_LFS	\ 				 || (vp)->v_tag == VT_ISOFS	\ 				 || (vp)->v_tag == VT_MSDOSFS	\ 				 || (vp)->v_tag == VT_DEVFS)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ASSERT_VOP_LOCKED
+parameter_list|(
+name|vp
+parameter_list|,
+name|str
+parameter_list|)
+define|\
+value|if ((vp)&& IS_LOCKING_VFS(vp)&& !VOP_ISLOCKED(vp)) {	\ 	panic("%s: %x is not locked but should be", str, vp);	\     }
+end_define
+
+begin_define
+define|#
+directive|define
+name|ASSERT_VOP_UNLOCKED
+parameter_list|(
+name|vp
+parameter_list|,
+name|str
+parameter_list|)
+define|\
+value|if ((vp)&& IS_LOCKING_VFS(vp)&& VOP_ISLOCKED(vp)) {	\ 	panic("%s: %x is locked but shouldn't be", str, vp);	\     }
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|ASSERT_VOP_LOCKED
+parameter_list|(
+name|vp
+parameter_list|,
+name|str
+parameter_list|)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ASSERT_VOP_UNLOCKED
+parameter_list|(
+name|vp
+parameter_list|,
+name|str
+parameter_list|)
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_comment
 comment|/*  * VOCALL calls an op given an ops vector.  We break it out because BSD's  * vclean changes the ops vector and then wants to call ops with the old  * vector.  */
 end_comment
@@ -2426,6 +2508,20 @@ argument_list|(
 operator|(
 expr|struct
 name|vop_unlock_args
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|vop_sharedlock
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|vop_lock_args
 operator|*
 operator|)
 argument_list|)
