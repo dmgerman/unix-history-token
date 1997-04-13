460db85013178abaf7f097f21dcb9e911bae1454
John Dyson <dyson@FreeBSD.org>
1997-04-13 03:35:30 +0000

    The pmap code was too generous in the allocation of kva space for the pv entries.  This problem has become obvious due to the increase in the size of the pv entries.  We need to create a more intelligent policy for pv entry management eventually. Submitted by:	David Greenman <dg@freebsd.org>
    Former-commit-id: 253c968bb3d7a8f8fef4442ba93627d6e8314e62

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	$Id: pmap.c,v 1.139 1997/04/07 07:15:52 peter Exp $  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	$Id: pmap.c,v 1.140 1997/04/13 01:48:08 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -175,6 +175,13 @@ begin_define
 define|#
 directive|define
 name|PMAP_KEEP_PDIRS
+end_define
+
+begin_define
+define|#
+directive|define
+name|PMAP_SHPGPERPROC
+value|200
 end_define
 
 begin_if
@@ -5491,13 +5498,6 @@ begin_comment
 comment|/*  * init the pv_entry allocation system  */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|PVSPERPAGE
-value|64
-end_define
-
 begin_function
 name|void
 name|init_pv_entries
@@ -5508,14 +5508,16 @@ name|int
 name|npg
 decl_stmt|;
 block|{
-comment|/* 	 * allocate enough kvm space for PVSPERPAGE entries per page (lots) 	 * kvm space is fairly cheap, be generous!!!  (the system can panic if 	 * this is too small.) 	 */
+comment|/* 	 * Allocate enough kvm space for one entry per page, and 	 * each process having PMAP_SHPGPERPROC pages shared with other 	 * processes.  (The system can panic if this is too small, but also 	 * can fail on bootup if this is too big.) 	 * XXX The pv management mechanism needs to be fixed so that systems 	 * with lots of shared mappings amongst lots of processes will still 	 * work.  The fix will likely be that once we run out of pv entries 	 * we will free other entries (and the associated mappings), with 	 * some policy yet to be determined. 	 */
 name|npvvapg
 operator|=
 operator|(
 operator|(
-name|npg
+name|PMAP_SHPGPERPROC
 operator|*
-name|PVSPERPAGE
+name|maxproc
+operator|+
+name|npg
 operator|)
 operator|*
 sizeof|sizeof
