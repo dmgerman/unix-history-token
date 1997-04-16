c79d715671f1d28b1686d13a6c4a8dc1b6f69ab1
Takenori KATO <kato@FreeBSD.org>
1997-04-16 16:24:24 +0000

    Do not set the uppervp to NULLVP in union_removed_upper.  If lowervp is NULLVP, union node will have neither uppervp nor lowervp.  This causes page fault trap.
    The union_removed_upper just remove union node from cache and it
    doesn't set uppervp to NULLVP.  Since union node is removed from
    cache, it will not be referenced.
    
    The code that remove union node from cache was copied from
    union_inactive.
    
    
    Former-commit-id: 8f81efa30bdc679e88f686d1cb5df725c08420dd

--- sys/fs/unionfs/union_subr.c
+++ sys/fs/unionfs/union_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 Jan-Simon Pendry  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)union_subr.c	8.20 (Berkeley) 5/20/95  * $Id: union_subr.c,v 1.16 1997/04/13 06:25:03 phk Exp $  */
+comment|/*  * Copyright (c) 1994 Jan-Simon Pendry  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)union_subr.c	8.20 (Berkeley) 5/20/95  * $Id: union_subr.c,v 1.17 1997/04/13 06:27:09 phk Exp $  */
 end_comment
 
 begin_include
@@ -4033,13 +4033,66 @@ init|=
 name|curproc
 decl_stmt|;
 comment|/* XXX */
-name|union_newupper
-argument_list|(
+name|struct
+name|vnode
+modifier|*
+modifier|*
+name|vpp
+decl_stmt|;
+comment|/* 	 * Do not set the uppervp to NULLVP.  If lowervp is NULLVP, 	 * union node will have neither uppervp nor lowervp.  We romove 	 * the union node from cache, so that it will not be referrenced. 	 */
+if|#
+directive|if
+literal|0
+block|union_newupper(un, NULLVP);
+endif|#
+directive|endif
+if|if
+condition|(
 name|un
-argument_list|,
+operator|->
+name|un_dircache
+operator|!=
+literal|0
+condition|)
+block|{
+for|for
+control|(
+name|vpp
+operator|=
+name|un
+operator|->
+name|un_dircache
+init|;
+operator|*
+name|vpp
+operator|!=
 name|NULLVP
+condition|;
+name|vpp
+operator|++
+control|)
+name|vrele
+argument_list|(
+operator|*
+name|vpp
 argument_list|)
 expr_stmt|;
+name|free
+argument_list|(
+name|un
+operator|->
+name|un_dircache
+argument_list|,
+name|M_TEMP
+argument_list|)
+expr_stmt|;
+name|un
+operator|->
+name|un_dircache
+operator|=
+literal|0
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|un
