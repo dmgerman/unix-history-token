f36921cd991c401afa3715feda204d55b5dec403
Takenori KATO <kato@FreeBSD.org>
1997-04-16 12:11:37 +0000

    Use reset port before clearing page table in cpu_reset if PC98 is defined.  Clearing page table could hang some new PC-98.
    Former-commit-id: e585781837ea4bd1aee4ea311604c927191c7fff

--- sys/amd64/amd64/vm_machdep.c
+++ sys/amd64/amd64/vm_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * Copyright (c) 1989, 1990 William Jolitz  * Copyright (c) 1994 John Dyson  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_machdep.c	7.3 (Berkeley) 5/13/91  *	Utah $Hdr: vm_machdep.c 1.16.1.1 89/06/23$  *	$Id: vm_machdep.c,v 1.77 1997/03/29 04:35:26 bde Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986 The Regents of the University of California.  * Copyright (c) 1989, 1990 William Jolitz  * Copyright (c) 1994 John Dyson  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_machdep.c	7.3 (Berkeley) 5/13/91  *	Utah $Hdr: vm_machdep.c 1.16.1.1 89/06/23$  *	$Id: vm_machdep.c,v 1.78 1997/04/07 07:15:56 peter Exp $  */
 end_comment
 
 begin_include
@@ -3045,6 +3045,37 @@ name|void
 name|cpu_reset
 parameter_list|()
 block|{
+ifdef|#
+directive|ifdef
+name|PC98
+comment|/* 	 * Attempt to do a CPU reset via CPU reset port. 	 */
+asm|asm("cli");
+name|outb
+argument_list|(
+literal|0x37
+argument_list|,
+literal|0x0f
+argument_list|)
+expr_stmt|;
+comment|/* SHUT0 = 0. */
+name|outb
+argument_list|(
+literal|0x37
+argument_list|,
+literal|0x0b
+argument_list|)
+expr_stmt|;
+comment|/* SHUT1 = 0. */
+name|outb
+argument_list|(
+literal|0xf0
+argument_list|,
+literal|0x00
+argument_list|)
+expr_stmt|;
+comment|/* Reset. */
+else|#
+directive|else
 comment|/* 	 * Attempt to do a CPU reset via the keyboard controller, 	 * do not turn of the GateA20, as any machine that fails 	 * to do the reset here would then end up in no man's land. 	 */
 if|#
 directive|if
@@ -3052,12 +3083,6 @@ operator|!
 name|defined
 argument_list|(
 name|BROKEN_KEYBOARD_RESET
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
-name|PC98
 argument_list|)
 name|outb
 argument_list|(
@@ -3087,6 +3112,9 @@ expr_stmt|;
 comment|/* wait 1 sec for printf to complete */
 endif|#
 directive|endif
+endif|#
+directive|endif
+comment|/* PC98 */
 comment|/* force a shutdown by unmapping entire address space ! */
 name|bzero
 argument_list|(
@@ -3103,36 +3131,6 @@ name|invltlb
 argument_list|()
 expr_stmt|;
 comment|/* NOTREACHED */
-ifdef|#
-directive|ifdef
-name|PC98
-asm|asm("   cli ");
-name|outb
-argument_list|(
-literal|0x37
-argument_list|,
-literal|0x0f
-argument_list|)
-expr_stmt|;
-comment|/* SHUT 0 = 0 */
-name|outb
-argument_list|(
-literal|0x37
-argument_list|,
-literal|0x0b
-argument_list|)
-expr_stmt|;
-comment|/* SHUT 1 = 0 */
-name|outb
-argument_list|(
-literal|0xf0
-argument_list|,
-literal|0x00
-argument_list|)
-expr_stmt|;
-comment|/* reset port */
-endif|#
-directive|endif
 while|while
 condition|(
 literal|1
