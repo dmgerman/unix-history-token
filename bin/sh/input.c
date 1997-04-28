41e2223d860d4d9cde30a6346a6fe97529e739bc
Steve Price <steve@FreeBSD.org>
1997-04-28 03:11:52 +0000

    Nuke register keyword usage, rename pread to preadfd, and open input files FD_CLOEXEC.
    Obtained from: NetBSD
    
    
    Former-commit-id: 7b2564426aaf933382eb123412fa5327867f3481

--- bin/sh/input.c
+++ bin/sh/input.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: input.c,v 1.9 1997/02/22 13:58:28 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -377,7 +377,7 @@ end_decl_stmt
 begin_decl_stmt
 specifier|static
 name|int
-name|pread
+name|preadfd
 name|__P
 argument_list|(
 operator|(
@@ -470,7 +470,6 @@ name|int
 name|len
 decl_stmt|;
 block|{
-specifier|register
 name|char
 modifier|*
 name|p
@@ -560,7 +559,7 @@ end_function
 begin_function
 specifier|static
 name|int
-name|pread
+name|preadfd
 parameter_list|()
 block|{
 name|int
@@ -574,6 +573,9 @@ name|buf
 expr_stmt|;
 name|retry
 label|:
+ifndef|#
+directive|ifndef
+name|NO_HISTORY
 if|if
 condition|(
 name|parsefile
@@ -626,7 +628,8 @@ expr_stmt|;
 block|}
 block|}
 else|else
-block|{
+endif|#
+directive|endif
 name|nr
 operator|=
 name|read
@@ -642,7 +645,6 @@ operator|-
 literal|1
 argument_list|)
 expr_stmt|;
-block|}
 if|if
 condition|(
 name|nr
@@ -841,7 +843,7 @@ condition|(
 operator|(
 name|parselleft
 operator|=
-name|pread
+name|preadfd
 argument_list|()
 operator|)
 operator|==
@@ -1403,6 +1405,18 @@ decl_stmt|,
 name|push
 decl_stmt|;
 block|{
+operator|(
+name|void
+operator|)
+name|fcntl
+argument_list|(
+name|fd
+argument_list|,
+name|F_SETFD
+argument_list|,
+name|FD_CLOEXEC
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|push
