b156b251e85d7dbeb9432d785357fcdeab4b81bb
Steve Passe <fsmp@FreeBSD.org>
1997-05-03 18:05:31 +0000

    added declaration for get_isa_apic_mask().
    Submitted by:	"John S. Dyson" <toor@dyson.iquest.net>
    
    
    Former-commit-id: 8baa7e74bc7efdb13dc01f1e177f159c0f884709

--- sys/i386/include/smp.h
+++ sys/i386/include/smp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: smp.h,v 1.3 1997/04/30 19:04:26 fsmp Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: smp.h,v 1.4 1997/05/01 19:33:06 fsmp Exp $  *  */
 end_comment
 
 begin_ifndef
@@ -336,6 +336,18 @@ end_decl_stmt
 begin_decl_stmt
 name|int
 name|get_isa_apic_irq
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|get_isa_apic_mask
 name|__P
 argument_list|(
 operator|(
