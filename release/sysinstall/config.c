f85a5d70beeab96531527ec843ab1644ab2b38da
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-05-05 06:41:36 +0000

    Update to rc.conf routing glue (YAMFC)
    Former-commit-id: 940653be397cab3c207ce3364c09dd84d4826f94

--- release/sysinstall/config.c
+++ release/sysinstall/config.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: config.c,v 1.51.2.42 1997/04/28 10:34:39 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: config.c,v 1.51.2.43 1997/04/29 09:14:05 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -2011,7 +2011,7 @@ index|[
 name|i
 index|]
 argument_list|,
-literal|"%s=\"%s\"\t\t%s\n"
+literal|"%s=\"%s\"\t\t%s"
 argument_list|,
 name|v
 operator|->
@@ -3153,16 +3153,16 @@ block|{
 name|char
 modifier|*
 name|cp
-decl_stmt|;
-name|cp
-operator|=
+init|=
 name|variable_get
 argument_list|(
 name|VAR_ROUTER
 argument_list|)
-expr_stmt|;
+decl_stmt|;
 if|if
 condition|(
+name|cp
+operator|&&
 name|strcmp
 argument_list|(
 name|cp
@@ -3171,6 +3171,13 @@ literal|"NO"
 argument_list|)
 condition|)
 block|{
+name|variable_set2
+argument_list|(
+name|VAR_ROUTER_ENABLE
+argument_list|,
+literal|"YES"
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|!
@@ -3200,15 +3207,34 @@ argument_list|(
 literal|"Unable to load gated package.  Falling back to no router."
 argument_list|)
 expr_stmt|;
-name|variable_set2
+name|variable_unset
 argument_list|(
 name|VAR_ROUTER
+argument_list|)
+expr_stmt|;
+name|variable_unset
+argument_list|(
+name|VAR_ROUTERFLAGS
+argument_list|)
+expr_stmt|;
+name|variable_set2
+argument_list|(
+name|VAR_ROUTER_ENABLE
 argument_list|,
 literal|"NO"
 argument_list|)
 expr_stmt|;
+name|cp
+operator|=
+name|NULL
+expr_stmt|;
 block|}
 block|}
+if|if
+condition|(
+name|cp
+condition|)
+block|{
 comment|/* Now get the flags, if they chose a router */
 name|ret
 operator|=
@@ -3230,10 +3256,21 @@ name|ret
 operator|!=
 name|DITEM_SUCCESS
 condition|)
-block|{
 name|variable_unset
 argument_list|(
-name|VAR_ROUTER
+name|VAR_ROUTERFLAGS
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+else|else
+block|{
+comment|/* No router case */
+name|variable_set2
+argument_list|(
+name|VAR_ROUTER_ENABLE
+argument_list|,
+literal|"NO"
 argument_list|)
 expr_stmt|;
 name|variable_unset
@@ -3241,7 +3278,11 @@ argument_list|(
 name|VAR_ROUTERFLAGS
 argument_list|)
 expr_stmt|;
-block|}
+name|variable_unset
+argument_list|(
+name|VAR_ROUTER
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 return|return
