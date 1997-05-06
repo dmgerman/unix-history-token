b5dd9736951ca712799fa3e1202856e072a0c079
Alexander Langer <alex@FreeBSD.org>
1997-05-06 02:12:18 +0000

    Create the default rule with flags IP_FW_F_IN | IP_FW_F_OUT. Closes PR#3100.
    Former-commit-id: 07f35f2ba09b7a4312e19ad02229767a54c6020d

--- sys/netinet/ip_fw.c
+++ sys/netinet/ip_fw.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 Alex Nash  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.55 1997/02/22 09:41:32 peter Exp $  */
+comment|/*  * Copyright (c) 1996 Alex Nash  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.56 1997/04/06 11:09:03 dufault Exp $  */
 end_comment
 
 begin_comment
@@ -4071,6 +4071,14 @@ name|u_short
 operator|)
 operator|-
 literal|1
+expr_stmt|;
+name|deny
+operator|.
+name|fw_flg
+operator|=
+name|IP_FW_F_IN
+operator||
+name|IP_FW_F_OUT
 expr_stmt|;
 name|add_entry
 argument_list|(
