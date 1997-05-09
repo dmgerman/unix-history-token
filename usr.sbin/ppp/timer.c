93980af86ebceb867e7af91b0234af074599fe13
Brian Somers <brian@FreeBSD.org>
1997-05-09 20:48:21 +0000

    Set setitimer() arg correctly.
    Former-commit-id: 1d82a0fd7e6921b82b93e22d2d69e95ea621edab

--- usr.sbin/ppp/timer.c
+++ usr.sbin/ppp/timer.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP Timer Processing Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: timer.c,v 1.13 1997/03/09 20:09:17 ache Exp $  *  *  TODO:  */
+comment|/*  *		PPP Timer Processing Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: timer.c,v 1.14 1997/03/13 12:45:26 brian Exp $  *  *  TODO:  */
 end_comment
 
 begin_include
@@ -1180,6 +1180,20 @@ name|struct
 name|itimerval
 name|itimer
 decl_stmt|;
+name|itimer
+operator|.
+name|it_interval
+operator|.
+name|tv_usec
+operator|=
+name|itimer
+operator|.
+name|it_interval
+operator|.
+name|tv_sec
+operator|=
+literal|0
+expr_stmt|;
 name|itimer
 operator|.
 name|it_value
