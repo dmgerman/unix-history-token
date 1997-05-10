c377809b5752cec0b80444a080774a47f8dbdc85
Bruce Evans <bde@FreeBSD.org>
1997-05-10 08:01:13 +0000

    Cleaned up #includes.  Lite2 cleaned up <sys/mount.h> so no kludges are required for NFS now.
    Ifdefed SMP #defines.
    
    
    Former-commit-id: 55d81dd8a43e65e249d7518a5b298fcaac63b9fa

--- sys/i386/i386/genassym.c
+++ sys/i386/i386/genassym.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)genassym.c	5.11 (Berkeley) 5/10/91  *	$Id: genassym.c,v 1.44 1997/04/07 07:15:48 peter Exp $  */
+comment|/*-  * Copyright (c) 1982, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)genassym.c	5.11 (Berkeley) 5/10/91  *	$Id: genassym.c,v 1.45 1997/04/26 11:45:06 peter Exp $  */
 end_comment
 
 begin_include
@@ -24,57 +24,19 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/errno.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/proc.h>
 end_include
 
-begin_comment
-comment|/* XXX This is just real ugly to have to do, but it is what you have to do! */
-end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|NFS
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|NFS
-end_define
-
 begin_include
 include|#
 directive|include
 file|<sys/mount.h>
-end_include
-
-begin_undef
-undef|#
-directive|undef
-name|NFS
-end_undef
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_include
-include|#
-directive|include
-file|<sys/mount.h>
-end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_include
-include|#
-directive|include
-file|<sys/mbuf.h>
 end_include
 
 begin_include
@@ -86,31 +48,19 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/msgbuf.h>
+file|<sys/resource.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<machine/cpu.h>
+file|<sys/resourcevar.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<machine/trap.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<machine/psl.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<machine/reg.h>
+file|<machine/frame.h>
 end_include
 
 begin_include
@@ -123,12 +73,6 @@ begin_include
 include|#
 directive|include
 file|<machine/tss.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/syscall.h>
 end_include
 
 begin_include
@@ -167,11 +111,27 @@ directive|include
 file|<vm/vm_map.h>
 end_include
 
+begin_define
+define|#
+directive|define
+name|KERNEL
+end_define
+
+begin_comment
+comment|/* XXX avoid user headers */
+end_comment
+
 begin_include
 include|#
 directive|include
 file|<sys/user.h>
 end_include
+
+begin_undef
+undef|#
+directive|undef
+name|KERNEL
+end_undef
 
 begin_include
 include|#
@@ -487,6 +447,9 @@ operator|->
 name|p_pid
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|SMP
 name|printf
 argument_list|(
 literal|"#define\tP_ONCPU %p\n"
@@ -507,6 +470,8 @@ operator|->
 name|p_lastcpu
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 name|printf
 argument_list|(
 literal|"#define\tSSLEEP %d\n"
@@ -746,6 +711,9 @@ operator|->
 name|pcb_ldt
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|SMP
 name|printf
 argument_list|(
 literal|"#define\tPCB_MPNEST %p\n"
@@ -756,6 +724,8 @@ operator|->
 name|pcb_mpnest
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 name|printf
 argument_list|(
 literal|"#define\tU_PROF %p\n"
