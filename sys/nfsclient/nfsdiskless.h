0e19e66b71615e28c07cbebb9e711ff21c543408
Tor Egge <tegge@FreeBSD.org>
1997-05-12 19:02:56 +0000

    Use the old nfs arguments in the nfs_diskless structure, to be compatible with boot proms made from the 2.2 source. Convert the nfs arguments when copying to the new diskless structure. Copy the gateway field in the diskless structure.
    Former-commit-id: a2dd4f9012ff2278e635e129cfd8f3b07c989b15

--- sys/nfsclient/nfsdiskless.h
+++ sys/nfsclient/nfsdiskless.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfsdiskless.h	8.2 (Berkeley) 3/30/95  * $Id$  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfsdiskless.h	8.2 (Berkeley) 3/30/95  * $Id: nfsdiskless.h,v 1.8 1997/02/22 09:42:47 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -117,6 +117,90 @@ block|}
 struct|;
 end_struct
 
+begin_comment
+comment|/*  * Old arguments to mount NFS  */
+end_comment
+
+begin_struct
+struct|struct
+name|onfs_args
+block|{
+name|struct
+name|sockaddr
+modifier|*
+name|addr
+decl_stmt|;
+comment|/* file server address */
+name|int
+name|addrlen
+decl_stmt|;
+comment|/* length of address */
+name|int
+name|sotype
+decl_stmt|;
+comment|/* Socket type */
+name|int
+name|proto
+decl_stmt|;
+comment|/* and Protocol */
+name|u_char
+modifier|*
+name|fh
+decl_stmt|;
+comment|/* File handle to be mounted */
+name|int
+name|fhsize
+decl_stmt|;
+comment|/* Size, in bytes, of fh */
+name|int
+name|flags
+decl_stmt|;
+comment|/* flags */
+name|int
+name|wsize
+decl_stmt|;
+comment|/* write size in bytes */
+name|int
+name|rsize
+decl_stmt|;
+comment|/* read size in bytes */
+name|int
+name|readdirsize
+decl_stmt|;
+comment|/* readdir size in bytes */
+name|int
+name|timeo
+decl_stmt|;
+comment|/* initial timeout in .1 secs */
+name|int
+name|retrans
+decl_stmt|;
+comment|/* times to retry send */
+name|int
+name|maxgrouplist
+decl_stmt|;
+comment|/* Max. size of group list */
+name|int
+name|readahead
+decl_stmt|;
+comment|/* # of blocks to readahead */
+name|int
+name|leaseterm
+decl_stmt|;
+comment|/* Term (sec) of lease */
+name|int
+name|deadthresh
+decl_stmt|;
+comment|/* Retrans threshold */
+name|char
+modifier|*
+name|hostname
+decl_stmt|;
+comment|/* server's name */
+block|}
+struct|;
+end_struct
+
 begin_struct
 struct|struct
 name|nfs_diskless
@@ -132,7 +216,7 @@ name|mygateway
 decl_stmt|;
 comment|/* Default gateway */
 name|struct
-name|nfs_args
+name|onfs_args
 name|swap_args
 decl_stmt|;
 comment|/* Mount args for swap file */
@@ -165,7 +249,7 @@ name|swap_ucred
 decl_stmt|;
 comment|/* Swap credentials */
 name|struct
-name|nfs_args
+name|onfs_args
 name|root_args
 decl_stmt|;
 comment|/* Mount args for root fs */
