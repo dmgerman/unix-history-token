c213da141eefb0247aa898d34bd07371fbbe5391
Doug Rabson <dfr@FreeBSD.org>
1997-05-13 10:23:47 +0000

    Changes to support the kernel linker:
    	Add a -Bforcedynamic option which generates a dynamic object even
    	if no shared libraries were given in the link.
    
    	Make RRS in text section warnings conditional on "-assert pure-text"
    	so that I can link non-PIC kernel modules without tons of link
    	errors.  Changes to bsd.lib.mk to follow.
    
    	Fix a couple of bugs exposed by the fact that the kernel is not
    	linked at zero.
    
    Reviewed by:	jdp
    
    
    Former-commit-id: 6e69ab0390f9c3ede77db32c6100f0c6a8b39aed

--- gnu/usr.bin/ld/rrs.c
+++ gnu/usr.bin/ld/rrs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: rrs.c,v 1.20 1997/02/22 15:46:23 peter Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: rrs.c,v 1.21 1997/04/25 15:26:12 jdp Exp $  */
 end_comment
 
 begin_include
@@ -960,6 +960,12 @@ operator|<
 name|text_start
 operator|+
 name|text_size
+operator|&&
+operator|(
+name|link_mode
+operator|&
+name|WARNRRSTEXT
+operator|)
 condition|)
 name|warnx
 argument_list|(
@@ -2681,7 +2687,13 @@ condition|(
 name|number_of_shobjs
 operator|==
 literal|0
-comment|/*&& !(link_mode& DYNAMIC)*/
+operator|&&
+operator|!
+operator|(
+name|link_mode
+operator|&
+name|FORCEDYNAMIC
+operator|)
 condition|)
 block|{
 comment|/* 		 * First slots in both tables are reserved 		 * hence the "> 1" condition 		 */
@@ -3741,17 +3753,12 @@ name|pos
 operator|=
 name|rrs_data_start
 operator|+
-operator|(
-name|N_DATOFF
+name|N_TXTOFF
 argument_list|(
 name|outheader
 argument_list|)
 operator|-
-name|DATA_START
-argument_list|(
-name|outheader
-argument_list|)
-operator|)
+name|text_start
 expr_stmt|;
 if|if
 condition|(
@@ -3987,17 +3994,12 @@ name|pos
 operator|=
 name|rrs_text_start
 operator|+
-operator|(
 name|N_TXTOFF
 argument_list|(
 name|outheader
 argument_list|)
 operator|-
-name|TEXT_START
-argument_list|(
-name|outheader
-argument_list|)
-operator|)
+name|text_start
 expr_stmt|;
 if|if
 condition|(
