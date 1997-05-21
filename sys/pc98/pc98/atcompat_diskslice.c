4f558d3f638f6ef3964d2831d61bba44cac280e1
Takenori KATO <kato@FreeBSD.org>
1997-05-21 00:39:08 +0000

    Synchronize with sys/i386/isa/diskslice_machdep.c revision 1.21.2.2.
    Former-commit-id: abcdaeafb976f89d9af766487928d064454eaa61

--- sys/pc98/pc98/atcompat_diskslice.c
+++ sys/pc98/pc98/atcompat_diskslice.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994 Bruce D. Evans.  * All rights reserved.  *  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $  *	$Id: atcompat_diskslice.c,v 1.2 1996/10/09 21:46:08 asami Exp $  */
+comment|/*-  * Copyright (c) 1994 Bruce D. Evans.  * All rights reserved.  *  * Copyright (c) 1982, 1986, 1988 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)ufs_disksubr.c	7.16 (Berkeley) 5/4/91  *	from: ufs_disksubr.c,v 1.8 1994/06/07 01:21:39 phk Exp $  *	$Id: atcompat_diskslice.c,v 1.2.2.1 1996/11/09 21:13:34 phk Exp $  */
 end_comment
 
 begin_comment
@@ -425,7 +425,7 @@ name|dp
 operator|->
 name|dp_start
 expr_stmt|;
-comment|/* 	 * If ssector1 is on a cylinder>= 1024, then ssector can't be right. 	 * Allow the C/H/S for it to be 1023/ntracks-1/nsectors, or correct 	 * apart from the cylinder being reduced modulo 1024. 	 */
+comment|/* 	 * If ssector1 is on a cylinder>= 1024, then ssector can't be right. 	 * Allow the C/H/S for it to be 1023/ntracks-1/nsectors, or correct 	 * apart from the cylinder being reduced modulo 1024.  Always allow 	 * 1023/255/63. 	 */
 if|if
 condition|(
 name|ssector
@@ -452,6 +452,11 @@ literal|1023
 operator|)
 operator|||
 operator|(
+name|secpercyl
+operator|!=
+literal|0
+operator|&&
+operator|(
 name|ssector1
 operator|-
 name|ssector
@@ -464,6 +469,7 @@ name|secpercyl
 operator|)
 operator|==
 literal|0
+operator|)
 operator|)
 operator|||
 operator|(
@@ -588,6 +594,11 @@ literal|1023
 operator|)
 operator|||
 operator|(
+name|secpercyl
+operator|!=
+literal|0
+operator|&&
+operator|(
 name|esector1
 operator|-
 name|esector
@@ -600,6 +611,7 @@ name|secpercyl
 operator|)
 operator|==
 literal|0
+operator|)
 operator|)
 operator|||
 operator|(
