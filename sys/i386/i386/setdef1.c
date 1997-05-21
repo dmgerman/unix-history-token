68fab3982453dc982d78fa413547d932087fb106
John Polstra <jdp@FreeBSD.org>
1997-05-21 23:21:30 +0000

    This commit affects ELF kernels only.
    Remove "setdefs.h" and arrange to generate it automatically at
    ELF kernel build time.
    
    "gensetdefs.c" is a utility which scans a set of ELF object files
    and outputs a line ``DEFINE_SET(name, length);'' for each linker
    set that it finds.  When generating an ELF kernel, this is run just
    before the final link to generate "setdefs.h".
    
    Remove the init_sets() function from "setdef0.c", and its call from
    "machdep.c".  Since "gensetdefs.c" calculates the length of each
    set, it is no longer necessary in an ELF kernel to count the set
    elements at kernel initialization time.  Also remove "set_of_sets"
    which was used for this purpose.
    
    Link "setdef0" and "setdef1" into the kernel only if building for
    ELF.  Since init_sets() is no longer used, there is no need to link
    them into an a.out kernel.
    
    
    Former-commit-id: 79bc261f322c775646e5509342b6dbcfcd1d868e

--- sys/i386/i386/setdef1.c
+++ sys/i386/i386/setdef1.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 John D. Polstra  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id$  */
+comment|/*-  * Copyright (c) 1997 John D. Polstra  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: setdef1.c,v 1.1 1997/04/22 06:55:28 jdp Exp $  */
 end_comment
 
 begin_ifdef
@@ -10,19 +10,8 @@ name|__ELF__
 end_ifdef
 
 begin_comment
-comment|/*  * END_SET emits the NULL terminator for a set.  */
+comment|/*  * DEFINE_SET emits the NULL terminator for a set.  */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|END_SET
-parameter_list|(
-name|set
-parameter_list|)
-define|\
-value|__asm__(".section .set." #set ",\"aw\"");	\ 	__asm__(".long 0");				\ 	__asm__(".previous")
-end_define
 
 begin_define
 define|#
@@ -30,36 +19,31 @@ directive|define
 name|DEFINE_SET
 parameter_list|(
 name|set
+parameter_list|,
+name|count
 parameter_list|)
-value|END_SET(set)
+define|\
+value|__asm__(".section .set." #set ",\"aw\"");	\ 	__asm__(".long 0");				\ 	__asm__(".previous")
 end_define
 
 begin_include
 include|#
 directive|include
-file|<i386/i386/setdefs.h>
+file|"setdefs.h"
 end_include
 
 begin_comment
 comment|/* Contains a `DEFINE_SET' for each set */
 end_comment
 
-begin_expr_stmt
-name|END_SET
-argument_list|(
-name|set_of_sets
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_comment
-comment|/* Terminate the mother of all sets */
-end_comment
-
 begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* __ELF__ */
+end_comment
 
 end_unit
 
