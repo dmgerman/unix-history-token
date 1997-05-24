414271af81623eb94f08c161cfd059b1db69f773
John-Mark Gurney <jmg@FreeBSD.org>
1997-05-24 22:19:16 +0000

    YAMFC: merge in support for cdrom drives that return BCD track numbers
    Former-commit-id: 70461cbe6af52915bd6529c290096ae8b364c858

--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.45 1996/09/08 10:44:17 phk Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.46 1996/09/10 23:37:52 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -960,6 +960,13 @@ define|#
 directive|define
 name|CD_Q_NO_TOUCH
 value|0x0001
+end_define
+
+begin_define
+define|#
+directive|define
+name|CD_Q_BCD_TRACKS
+value|0x0002
 end_define
 
 begin_comment
