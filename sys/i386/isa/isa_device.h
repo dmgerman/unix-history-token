24d02fb85c2a7b589c69a11281772eb5c1911b3b
Stefan Esser <se@FreeBSD.org>
1997-05-26 14:42:24 +0000

    Add support for shared interrupts to the kernel. This code is meant be (eventually) architecture independent. It provides an emulation of the ISA interrupt registration function register_intr(), but that function does no longer manipulated the interrupt controller and interrupt descriptor table, but calls the architecture dependent function setup_icu() for that purpose.
    After the ISA/EISA bus code has been modified to directly call the new
    interrupt registartion functions (intr_create() and intr_connect()),
    the emulation of register_intr() should be dropped.
    
    The C level interrupt handler function should take a (void*) argument,
    and the function pointer type (inthand2_t) should defined in some  other
    place than isa_device.h.
    
    This commit is a pre-requisite for the removal of the PCI specific shared
    interrupt code.
    
    Reviewed by:    dfr,bde
    
    
    Former-commit-id: c1c5452f1759ed9ef1b0fa661ca8dac3c5999b42

--- sys/i386/isa/isa_device.h
+++ sys/i386/isa/isa_device.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa_device.h	7.1 (Berkeley) 5/9/91  *	$Id: isa_device.h,v 1.38 1997/04/26 19:26:37 peter Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa_device.h	7.1 (Berkeley) 5/9/91  *	$Id: isa_device.h,v 1.39 1997/04/27 21:18:58 fsmp Exp $  */
 end_comment
 
 begin_ifndef
@@ -869,7 +869,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/* this functions ONLY exists in an SMP/APIC_IO kernel: */
+comment|/* this function ONLY exists in an SMP/APIC_IO kernel: */
 end_comment
 
 begin_decl_stmt
@@ -920,8 +920,8 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
-name|register_intr
+name|void
+name|update_intrname
 name|__P
 argument_list|(
 operator|(
@@ -930,20 +930,6 @@ name|intr
 operator|,
 name|int
 name|device_id
-operator|,
-name|u_int
-name|flags
-operator|,
-name|inthand2_t
-operator|*
-name|handler
-operator|,
-name|u_int
-operator|*
-name|maskptr
-operator|,
-name|int
-name|unit
 operator|)
 argument_list|)
 decl_stmt|;
@@ -951,7 +937,35 @@ end_decl_stmt
 
 begin_decl_stmt
 name|int
-name|unregister_intr
+name|icu_setup
+name|__P
+argument_list|(
+operator|(
+name|int
+name|intr
+operator|,
+name|inthand2_t
+operator|*
+name|func
+operator|,
+name|void
+operator|*
+name|arg
+operator|,
+name|u_int
+operator|*
+name|maskptr
+operator|,
+name|int
+name|flags
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|icu_unset
 name|__P
 argument_list|(
 operator|(
