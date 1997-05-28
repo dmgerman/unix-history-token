8c3a077c1d07ead164ce6c5b16510a9dfcdb390e
Bill Paul <wpaul@FreeBSD.org>
1997-05-28 16:38:35 +0000

    Fix other small things that got lost in the merge:
    - bde's change to includes section in getrpcent.3
    - Lost comment in svc_run.c (the code here was actually the same since
      I had fixed the 'fds + 1' bug in my stuff at home before mailing
      Peter about it, but I didn't notce that he'd made a change to the
      comment right above the changed line).
    
    Also pointed out by the ever vigilant: bde
    
    
    Former-commit-id: 3016a05ada25401e301f7cb78e224c44a5ebe95d

--- lib/libc/rpc/svc_run.c
+++ lib/libc/rpc/svc_run.c
@@ -32,7 +32,7 @@ name|char
 modifier|*
 name|rcsid
 init|=
-literal|"$Id: svc_run.c,v 1.4 1996/12/30 15:14:29 peter Exp $"
+literal|"$Id: svc_run.c,v 1.7 1997/02/22 15:01:32 peter Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -243,7 +243,7 @@ argument_list|)
 expr_stmt|;
 continue|continue;
 default|default:
-comment|/* XXX What the hell?? what if fds == NULL?? */
+comment|/* if fds == NULL, select() can't return a result */
 name|svc_getreqset2
 argument_list|(
 name|fds
