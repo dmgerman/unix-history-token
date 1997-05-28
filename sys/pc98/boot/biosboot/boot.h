4c68f1c7f7c8d490e11f7fd6a23f51b93fa3b5c5
Takenori KATO <kato@FreeBSD.org>
1997-05-28 09:23:00 +0000

    Synchronize with following files:
    >  Revision  Changes    Path
    >  1.65      +3 -3      src/sys/i386/boot/biosboot/boot.c
    >  1.18      +1 -5      src/sys/i386/boot/biosboot/boot.h
    >  1.26      +7 -6      src/sys/i386/boot/biosboot/disk.c
    >  1.22      +4 -2      src/sys/i386/boot/biosboot/io.c
    >  1.20      +12 -9     src/sys/i386/boot/biosboot/sys.c
    
    
    Former-commit-id: 148df7699e327495fc8332f7a8e89be5469b1aca

--- sys/pc98/boot/biosboot/boot.h
+++ sys/pc98/boot/biosboot/boot.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:35:03  rpd  *	$Id$  */
+comment|/*  * Mach Operating System  * Copyright (c) 1992, 1991 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie Mellon  * the rights to redistribute these changes.  *  *	from: Mach, Revision 2.2  92/04/04  11:35:03  rpd  *	$Id: boot.h,v 1.8 1997/02/22 09:43:06 peter Exp $  */
 end_comment
 
 begin_include
@@ -338,20 +338,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|char
-modifier|*
-name|Bread
-parameter_list|(
-name|int
-name|dosdev
-parameter_list|,
-name|int
-name|sector
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_comment
 comment|/* io.c */
 end_comment
@@ -385,16 +371,6 @@ name|putchar
 parameter_list|(
 name|int
 name|c
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|getchar
-parameter_list|(
-name|int
-name|in_buf
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -566,27 +542,6 @@ name|buffer
 parameter_list|,
 name|int
 name|count
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|find
-parameter_list|(
-name|char
-modifier|*
-name|path
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|block_map
-parameter_list|(
-name|int
-name|file_block
 parameter_list|)
 function_decl|;
 end_function_decl
