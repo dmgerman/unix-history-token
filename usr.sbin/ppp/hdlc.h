9a73b227df07f37c1db5f937d646b37c75e0a7e0
Brian Somers <brian@FreeBSD.org>
1997-06-01 01:14:38 +0000

    YAMFC: Implement "set mtu" command to allow the client to        reduce the interface mtu.        Allow max mru spec of 16k.        Add "show m[rt]u" ability.
    Former-commit-id: ce4a085fc5d77e61b2d1257527a6d6a5b340b312

--- usr.sbin/ppp/hdlc.h
+++ usr.sbin/ppp/hdlc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: hdlc.h,v 1.3 1996/01/11 17:48:48 phk Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: hdlc.h,v 1.4 1996/01/30 11:08:31 dfr Exp $  *  *	TODO:  */
 end_comment
 
 begin_ifndef
@@ -103,13 +103,38 @@ begin_define
 define|#
 directive|define
 name|MAX_MRU
-value|2000
+value|16384
 end_define
 
 begin_define
 define|#
 directive|define
 name|MIN_MRU
+value|296
+end_define
+
+begin_define
+define|#
+directive|define
+name|DEF_MTU
+value|0
+end_define
+
+begin_comment
+comment|/* whatever peer says */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAX_MTU
+value|16384
+end_define
+
+begin_define
+define|#
+directive|define
+name|MIN_MTU
 value|296
 end_define
 
