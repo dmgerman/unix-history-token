874ca55a783b880670d939afc60c254491c3b3c0
Julian Elischer <julian@FreeBSD.org>
1997-06-02 06:24:52 +0000

    Submitted by:	Whistle Communications (archie Cobbs)
    These changes add the ability to specify that a UFS file/directory
    cannot be unlinked. This is basically a scaled back version
    of the IMMUTABLE flag. The reason is to allow an administrator
    to create a directory hierarchy that a group of users
    can arbitrarily add/delete files from, but that the hierarchy
    itself is safe from removal by them.
    If the NOUNLINK definition is set to 0
    then this results in no change to what happens normally.
    (and results in identical binary (in the kernel)).
    It can be proven that if this bit is never set by the admin,
    no new behaviour is introduced..
    Several "good idea" comments from reviewers plus one grumble
    about creeping featurism.
    
    This code is in production in 2.2 based systems
    
    
    Former-commit-id: 1ca5f777ff830e3bee8bf864dc407ed5a88990ab

--- lib/libc/gen/strtofflags.c
+++ lib/libc/gen/strtofflags.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: stat_flags.c,v 1.5 1997/02/22 14:04:02 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -132,6 +132,17 @@ if|if
 condition|(
 name|flags
 operator|&
+name|UF_NOUNLINK
+condition|)
+name|SAPPEND
+argument_list|(
+literal|"uunlnk"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|flags
+operator|&
 name|UF_NODUMP
 condition|)
 name|SAPPEND
@@ -181,6 +192,17 @@ condition|)
 name|SAPPEND
 argument_list|(
 literal|"schg"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|flags
+operator|&
+name|SF_NOUNLINK
+condition|)
+name|SAPPEND
+argument_list|(
+literal|"sunlnk"
 argument_list|)
 expr_stmt|;
 return|return
@@ -459,6 +481,24 @@ argument_list|,
 name|SF_IMMUTABLE
 argument_list|)
 expr_stmt|;
+name|TEST
+argument_list|(
+name|p
+argument_list|,
+literal|"sunlnk"
+argument_list|,
+name|SF_NOUNLINK
+argument_list|)
+expr_stmt|;
+name|TEST
+argument_list|(
+name|p
+argument_list|,
+literal|"sunlink"
+argument_list|,
+name|SF_NOUNLINK
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 literal|1
@@ -510,6 +550,24 @@ argument_list|,
 literal|"uimmutable"
 argument_list|,
 name|UF_IMMUTABLE
+argument_list|)
+expr_stmt|;
+name|TEST
+argument_list|(
+name|p
+argument_list|,
+literal|"uunlnk"
+argument_list|,
+name|UF_NOUNLINK
+argument_list|)
+expr_stmt|;
+name|TEST
+argument_list|(
+name|p
+argument_list|,
+literal|"uunlink"
+argument_list|,
+name|UF_NOUNLINK
 argument_list|)
 expr_stmt|;
 comment|/* FALLTHROUGH */
