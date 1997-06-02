9bcbff7fdc0cd75ebaea6e90642ad3b0aea95b81
Julian Elischer <julian@FreeBSD.org>
1997-06-02 05:02:37 +0000

    Submitted by:	Whistle Communications (archie Cobbs)
    these are quite extensive additions to the ipfw code.
    they include a change to the API because the old method was
    broken, but the user view is kept the same.
    
    The new code allows a particular match to skip forward to a particular
    line number, so that blocks of rules can be
    used without checking all the intervening rules.
    There are also many more ways of rejecting
    connections especially TCP related, and
    many many more ...
    
    see the man page for a complete description.
    
    
    Former-commit-id: aadbf7f88cfbd9cedd513a642b1013787b76fe31

--- sys/netinet/ip_divert.c
+++ sys/netinet/ip_divert.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ip_divert.c,v 1.10 1997/05/26 03:33:48 peter Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ip_divert.c,v 1.11 1997/06/01 15:58:44 peter Exp $  */
 end_comment
 
 begin_include
@@ -317,28 +317,16 @@ name|int
 name|hlen
 parameter_list|)
 block|{
-specifier|register
 name|struct
 name|ip
 modifier|*
 name|ip
-init|=
-name|mtod
-argument_list|(
-name|m
-argument_list|,
-expr|struct
-name|ip
-operator|*
-argument_list|)
 decl_stmt|;
-specifier|register
 name|struct
 name|inpcb
 modifier|*
 name|inp
 decl_stmt|;
-specifier|register
 name|struct
 name|socket
 modifier|*
@@ -353,7 +341,51 @@ literal|0
 condition|)
 name|panic
 argument_list|(
-literal|"div_input"
+literal|"div_input: port is 0"
+argument_list|)
+expr_stmt|;
+comment|/* Assure header */
+if|if
+condition|(
+name|m
+operator|->
+name|m_len
+operator|<
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|ip
+argument_list|)
+operator|&&
+operator|(
+name|m
+operator|=
+name|m_pullup
+argument_list|(
+name|m
+argument_list|,
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|ip
+argument_list|)
+argument_list|)
+operator|)
+operator|==
+literal|0
+condition|)
+block|{
+return|return;
+block|}
+name|ip
+operator|=
+name|mtod
+argument_list|(
+name|m
+argument_list|,
+expr|struct
+name|ip
+operator|*
 argument_list|)
 expr_stmt|;
 comment|/* Record divert port */
@@ -406,6 +438,28 @@ name|ifaddr
 modifier|*
 name|ifa
 decl_stmt|;
+ifdef|#
+directive|ifdef
+name|DIAGNOSTIC
+comment|/* Sanity check */
+if|if
+condition|(
+operator|!
+operator|(
+name|m
+operator|->
+name|m_flags
+operator|&
+name|M_PKTHDR
+operator|)
+condition|)
+name|panic
+argument_list|(
+literal|"div_input: no pkt hdr"
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 comment|/* More fields affected by ip_input() */
 name|HTONS
 argument_list|(
--- sys/netinet/ip_fw.h
+++ sys/netinet/ip_fw.h
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id$  */
-end_comment
-
-begin_comment
-comment|/*  * Format of an IP firewall descriptor  *  * fw_src, fw_dst, fw_smsk, fw_dmsk are always stored in network byte order.  * fw_flg and fw_n*p are stored in host byte order (of course).  * Port numbers are stored in HOST byte order.  */
+comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.h,v 1.1 1997/05/09 17:46:45 archie Exp $  */
 end_comment
 
 begin_ifndef
@@ -18,6 +14,48 @@ define|#
 directive|define
 name|_IP_FW_H
 end_define
+
+begin_comment
+comment|/*  * This union structure identifies an interface, either explicitly  * by name or implicitly by IP address. The flags IP_FW_F_IIFNAME  * and IP_FW_F_OIFNAME say how to interpret this structure. An  * interface unit number of -1 matches any unit number, while an  * IP address of 0.0.0.0 indicates matches any interface.  *  * The receive and transmit interfaces are only compared against the  * the packet if the corresponding bit (IP_FW_F_IIFACE or IP_FW_F_OIFACE)  * is set. Note some packets lack a receive or transmit interface  * (in which case the missing "interface" never matches).  */
+end_comment
+
+begin_union
+union|union
+name|ip_fw_if
+block|{
+name|struct
+name|in_addr
+name|fu_via_ip
+decl_stmt|;
+comment|/* Specified by IP address */
+struct|struct
+block|{
+comment|/* Specified by interface name */
+define|#
+directive|define
+name|FW_IFNLEN
+value|6
+comment|/* To keep structure on 2^x boundary */
+name|char
+name|name
+index|[
+name|FW_IFNLEN
+index|]
+decl_stmt|;
+name|short
+name|unit
+decl_stmt|;
+comment|/* -1 means match any unit */
+block|}
+name|fu_via_if
+struct|;
+block|}
+union|;
+end_union
+
+begin_comment
+comment|/*  * Format of an IP firewall descriptor  *  * fw_src, fw_dst, fw_smsk, fw_dmsk are always stored in network byte order.  * fw_flg and fw_n*p are stored in host byte order (of course).  * Port numbers are stored in HOST byte order.  * Warning: setsockopt() will fail if sizeof(struct ip_fw)> MLEN (108)  */
+end_comment
 
 begin_struct
 struct|struct
@@ -43,64 +81,14 @@ decl_stmt|,
 name|fw_dmsk
 decl_stmt|;
 comment|/* Mask for src and dest IP addr */
-union|union
-block|{
-name|struct
-name|in_addr
-name|fu_via_ip
-decl_stmt|;
-comment|/* Specified by IP address */
-struct|struct
-block|{
-comment|/* Specified by interface name */
-define|#
-directive|define
-name|FW_IFNLEN
-value|6
-comment|/* To keep structure on 2^x boundary */
-name|char
-name|fu_via_name
-index|[
-name|FW_IFNLEN
-index|]
-decl_stmt|;
-name|short
-name|fu_via_unit
-decl_stmt|;
-block|}
-name|fu_via_if
-struct|;
-block|}
-name|fu_via_un
-union|;
-define|#
-directive|define
-name|fw_via_ip
-value|fu_via_un.fu_via_ip
-define|#
-directive|define
-name|fw_via_name
-value|fu_via_un.fu_via_if.fu_via_name
-define|#
-directive|define
-name|fw_via_unit
-value|fu_via_un.fu_via_if.fu_via_unit
 name|u_short
 name|fw_number
 decl_stmt|;
+comment|/* Rule number */
 name|u_short
 name|fw_flg
 decl_stmt|;
 comment|/* Flags word */
-name|u_short
-name|fw_nsp
-decl_stmt|,
-name|fw_ndp
-decl_stmt|;
-comment|/* N'of src ports and # of dst ports */
-comment|/* in ports array (dst ports follow */
-comment|/* src ports; max of 10 ports in all; */
-comment|/* count of 0 means match all ports) */
 define|#
 directive|define
 name|IP_FW_MAX_PORTS
@@ -140,17 +128,109 @@ name|long
 name|timestamp
 decl_stmt|;
 comment|/* timestamp (tv_sec) of last match */
-name|u_short
-name|fw_divert_port
+name|union
+name|ip_fw_if
+name|fw_in_if
+decl_stmt|,
+name|fw_out_if
 decl_stmt|;
-comment|/* Divert port (options IPDIVERT) */
+comment|/* Incoming and outgoing interfaces */
+union|union
+block|{
+name|u_short
+name|fu_divert_port
+decl_stmt|;
+comment|/* Divert/tee port (options IPDIVERT) */
+name|u_short
+name|fu_skipto_rule
+decl_stmt|;
+comment|/* SKIPTO command rule number */
+name|u_short
+name|fu_reject_code
+decl_stmt|;
+comment|/* REJECT response code */
+block|}
+name|fw_un
+union|;
 name|u_char
 name|fw_prot
 decl_stmt|;
 comment|/* IP protocol */
+name|u_char
+name|fw_nports
+decl_stmt|;
+comment|/* N'of src ports and # of dst ports */
+comment|/* in ports array (dst ports follow */
+comment|/* src ports; max of 10 ports in all; */
+comment|/* count of 0 means match all ports) */
 block|}
 struct|;
 end_struct
+
+begin_define
+define|#
+directive|define
+name|IP_FW_GETNSRCP
+parameter_list|(
+name|rule
+parameter_list|)
+value|((rule)->fw_nports& 0x0f)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_SETNSRCP
+parameter_list|(
+name|rule
+parameter_list|,
+name|n
+parameter_list|)
+value|do {				\ 					  (rule)->fw_nports&= ~0x0f;	\ 					  (rule)->fw_nports |= (n);	\ 					} while (0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_GETNDSTP
+parameter_list|(
+name|rule
+parameter_list|)
+value|((rule)->fw_nports>> 4)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IP_FW_SETNDSTP
+parameter_list|(
+name|rule
+parameter_list|,
+name|n
+parameter_list|)
+value|do {				\ 					  (rule)->fw_nports&= ~0xf0;	\ 					  (rule)->fw_nports |= (n)<< 4;\ 					} while (0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|fw_divert_port
+value|fw_un.fu_divert_port
+end_define
+
+begin_define
+define|#
+directive|define
+name|fw_skipto_rule
+value|fw_un.fu_skipto_rule
+end_define
+
+begin_define
+define|#
+directive|define
+name|fw_reject_code
+value|fw_un.fu_reject_code
+end_define
 
 begin_struct
 struct|struct
@@ -178,89 +258,56 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IP_FW_F_INVSRC
+name|IP_FW_F_IN
 value|0x0001
 end_define
 
 begin_comment
-comment|/* Invert sense of src check	      */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_INVDST
-value|0x0002
-end_define
-
-begin_comment
-comment|/* Invert sense of dst check	      */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_IN
-value|0x0004
-end_define
-
-begin_comment
-comment|/* Inbound 			      */
+comment|/* Check inbound packets		*/
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_OUT
+value|0x0002
+end_define
+
+begin_comment
+comment|/* Check outbound packets		*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_IIFACE
+value|0x0004
+end_define
+
+begin_comment
+comment|/* Apply inbound interface test		*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_OIFACE
 value|0x0008
 end_define
 
 begin_comment
-comment|/* Outbound			      */
+comment|/* Apply outbound interface test	*/
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_COMMAND
-value|0x0030
+value|0x0070
 end_define
 
 begin_comment
-comment|/* Mask for type of chain entry:      */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_ACCEPT
-value|0x0010
-end_define
-
-begin_comment
-comment|/* This is an accept rule	      */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_COUNT
-value|0x0020
-end_define
-
-begin_comment
-comment|/* This is a count rule		      */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_DIVERT
-value|0x0030
-end_define
-
-begin_comment
-comment|/* This is a divert rule	      */
+comment|/* Mask for type of chain entry:	*/
 end_comment
 
 begin_define
@@ -271,29 +318,84 @@ value|0x0000
 end_define
 
 begin_comment
-comment|/* This is a deny rule	              */
+comment|/* This is a deny rule			*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_REJECT
+value|0x0010
+end_define
+
+begin_comment
+comment|/* Deny and send a response packet	*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_ACCEPT
+value|0x0020
+end_define
+
+begin_comment
+comment|/* This is an accept rule		*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_COUNT
+value|0x0030
+end_define
+
+begin_comment
+comment|/* This is a count rule			*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_DIVERT
+value|0x0040
+end_define
+
+begin_comment
+comment|/* This is a divert rule		*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_TEE
+value|0x0050
+end_define
+
+begin_comment
+comment|/* This is a tee rule			*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_SKIPTO
+value|0x0060
+end_define
+
+begin_comment
+comment|/* This is a skipto rule		*/
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_PRN
-value|0x0040
-end_define
-
-begin_comment
-comment|/* Print if this rule matches	      */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_ICMPRPL
 value|0x0080
 end_define
 
 begin_comment
-comment|/* Send back icmp unreachable packet  */
+comment|/* Print if this rule matches		*/
 end_comment
 
 begin_define
@@ -304,7 +406,7 @@ value|0x0100
 end_define
 
 begin_comment
-comment|/* The first two src ports are a min  * 				 * and max range (stored in host byte * 				 * order).                            */
+comment|/* The first two src ports are a min	* 				 * and max range (stored in host byte	* 				 * order).				*/
 end_comment
 
 begin_define
@@ -315,62 +417,110 @@ value|0x0200
 end_define
 
 begin_comment
-comment|/* The first two dst ports are a min  * 				 * and max range (stored in host byte * 				 * order).                            */
+comment|/* The first two dst ports are a min	* 				 * and max range (stored in host byte	* 				 * order).				*/
 end_comment
 
 begin_define
 define|#
 directive|define
-name|IP_FW_F_IFNAME
+name|IP_FW_F_IIFNAME
 value|0x0400
 end_define
 
 begin_comment
-comment|/* Use interface name/unit (not IP)   */
+comment|/* In interface by name/unit (not IP)	*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_OIFNAME
+value|0x0800
+end_define
+
+begin_comment
+comment|/* Out interface by name/unit (not IP)	*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_INVSRC
+value|0x1000
+end_define
+
+begin_comment
+comment|/* Invert sense of src check		*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_F_INVDST
+value|0x2000
+end_define
+
+begin_comment
+comment|/* Invert sense of dst check		*/
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_FRAG
-value|0x0800
+value|0x4000
 end_define
 
 begin_comment
-comment|/* Fragment			      */
+comment|/* Fragment				*/
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_ICMPBIT
-value|0x1000
+value|0x8000
 end_define
 
 begin_comment
-comment|/* ICMP type bitmap is valid          */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IP_FW_F_IFUWILD
-value|0x2000
-end_define
-
-begin_comment
-comment|/* Match all interface units          */
+comment|/* ICMP type bitmap is valid		*/
 end_comment
 
 begin_define
 define|#
 directive|define
 name|IP_FW_F_MASK
-value|0x3FFF
+value|0xFFFF
 end_define
 
 begin_comment
-comment|/* All possible flag bits mask        */
+comment|/* All possible flag bits mask		*/
+end_comment
+
+begin_comment
+comment|/*  * For backwards compatibility with rules specifying "via iface" but  * not restricted to only "in" or "out" packets, we define this combination  * of bits to represent this configuration.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IF_FW_F_VIAHACK
+value|(IP_FW_F_IN|IP_FW_F_OUT|IP_FW_F_IIFACE|IP_FW_F_OIFACE)
+end_define
+
+begin_comment
+comment|/*  * Definitions for REJECT response codes.  * Values less than 256 correspond to ICMP unreachable codes.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_REJECT_RST
+value|0x0100
+end_define
+
+begin_comment
+comment|/* TCP packets: send RST */
 end_comment
 
 begin_comment
