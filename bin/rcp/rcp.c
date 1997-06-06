9b7ca5de2d9bf97dd0285fae9509ac144a85de1e
Philippe Charnier <charnier@FreeBSD.org>
1997-06-06 16:14:34 +0000

    MFC: Correct usage string.
    Former-commit-id: a67a7f411eca1c9d2e36452a42dd1f136b69f50d

--- bin/rcp/rcp.c
+++ bin/rcp/rcp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1990, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*  * Copyright (c) 1983, 1990, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: rcp.c,v 1.8.2.1 1997/05/09 16:01:22 joerg Exp $  */
 end_comment
 
 begin_ifndef
@@ -4946,11 +4946,11 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"%s\n\t%s\n"
+literal|"%s\n%s\n"
 argument_list|,
 literal|"usage: rcp [-Kpx] [-k realm] f1 f2"
 argument_list|,
-literal|"or: rcp [-Kprx] [-k realm] f1 ... fn directory"
+literal|"       rcp [-Kprx] [-k realm] f1 ... fn directory"
 argument_list|)
 expr_stmt|;
 else|#
@@ -4962,11 +4962,11 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"%s\n\t%s\n"
+literal|"%s\n%s\n"
 argument_list|,
 literal|"usage: rcp [-Kp] [-k realm] f1 f2"
 argument_list|,
-literal|"or: rcp [-Kpr] [-k realm] f1 ... fn directory"
+literal|"       rcp [-Kpr] [-k realm] f1 ... fn directory"
 argument_list|)
 expr_stmt|;
 endif|#
@@ -4980,7 +4980,11 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: rcp [-p] f1 f2; or: rcp [-pr] f1 ... fn directory\n"
+literal|"%s\n%s\n"
+argument_list|,
+literal|"usage: rcp [-p] f1 f2"
+argument_list|,
+literal|"       rcp [-pr] f1 ... fn directory"
 argument_list|)
 expr_stmt|;
 endif|#
