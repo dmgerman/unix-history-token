eb7f52c915545590fa5cdb870811d4776a0a634a
Brian Somers <brian@FreeBSD.org>
1997-06-13 02:08:58 +0000

    YAMFC: Add ppp.linkdown file to compliment ppp.linkup.
           Submitted by:	Forgotten
           Passed on by:	Terry Dwyer 61 8 9491 5161 <tdwyer@io.telstra.com.au>
    
           Also remove extraneous setuid(0) - it's only undone by
           the subsequent call to SelectSystem().
    
    
    Former-commit-id: 4bcad4a49ebc45289e5eb0eb9e6608e04b0c89be

--- usr.sbin/ppp/defs.h
+++ usr.sbin/ppp/defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.15 1997/06/09 03:27:18 brian Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.4.2.5 1997/06/10 09:43:20 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_ifndef
@@ -220,15 +220,15 @@ end_define
 begin_define
 define|#
 directive|define
-name|LINKFILE
+name|LINKUPFILE
 value|"ppp.linkup"
 end_define
 
 begin_define
 define|#
 directive|define
-name|ETHERFILE
-value|"ppp.etherup"
+name|LINKDOWNFILE
+value|"ppp.linkdown"
 end_define
 
 begin_define
