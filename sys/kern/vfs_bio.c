e69ae682cc1df66da379cd07d42c76d8ad432755
Bruce Evans <bde@FreeBSD.org>
1997-06-13 08:30:40 +0000

    Fixed livelock in getnewbuf().
    It is possible for multiple process to sleep concurrently waiting
    for a buffer.  When the buffer shortage is a shortage of space but
    not a shortage of buffer headers, the processes took turns creating
    empty buffers and waking each other to advertise the brelse() of
    the empties; progress was never made because tsleep() always found
    another high-priority process to run and everything was done at
    splbio(), so vfs_update never had a chance to flush delayed writes,
    not to mention that i/o never had a chance to complete.
    
    The problem seems to be rare in practice, but it can easily be
    reproduced by misusing block devices, at least for sufficently slow
    devices on machines with a sufficiently small buffer cache.  E.g.,
    `tar cvf /dev/fd0 /kernel' on an 8MB system with no disk in fd0
    causes the problem quickly; the same command with a disk in fd0
    causes the problem not quite as quickly; and people have reported
    problems newfs'ing file systems on block devices.
    
    Block devices only cause this problem indirectly.  They are pessimized
    for time and space, and the space pessimization causes the shortage
    (it manifests as internal fragmentation in buffer_map).
    
    This should be fixed in 2.2.
    
    
    Former-commit-id: 1d00dd045d2f3ba20428baa23c9b8c3703f0716a

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.118 1997/06/03 09:42:43 dfr Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. This work was done expressly for inclusion into FreeBSD.  Other use  *    is allowed if this notation is included.  * 5. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: vfs_bio.c,v 1.119 1997/06/06 09:04:28 dfr Exp $  */
 end_comment
 
 begin_comment
@@ -3777,6 +3777,7 @@ name|needsbuffer
 operator|=
 literal|1
 expr_stmt|;
+do|do
 name|tsleep
 argument_list|(
 operator|&
@@ -3795,6 +3796,11 @@ argument_list|,
 name|slptimeo
 argument_list|)
 expr_stmt|;
+do|while
+condition|(
+name|needsbuffer
+condition|)
+do|;
 return|return
 operator|(
 literal|0
