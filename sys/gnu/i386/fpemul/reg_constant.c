9e8717259d88acaddb143bcc6a1d5e0dbf6cc6f0
Bruce Evans <bde@FreeBSD.org>
1997-06-14 15:11:07 +0000

    Removed unused #includes.
    Former-commit-id: 70e71e8c937a3ad8041d4203df2a29e8e609aa1e

--- sys/gnu/i386/fpemul/reg_constant.c
+++ sys/gnu/i386/fpemul/reg_constant.c
@@ -18,18 +18,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/systm.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<machine/cpu.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/pcb.h>
 end_include
 
@@ -49,12 +37,6 @@ begin_include
 include|#
 directive|include
 file|<gnu/i386/fpemul/status_w.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<gnu/i386/fpemul/reg_constant.h>
 end_include
 
 begin_decl_stmt
