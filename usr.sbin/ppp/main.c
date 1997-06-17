22c94e1f2e647b730c5837c0c2b3c34549f8b497
Brian Somers <brian@FreeBSD.org>
1997-06-17 01:47:10 +0000

    YAMFC: Handle redial tries correctly under -ddial
    Former-commit-id: 5beeb6ce1fc015e882ef649477230e09cc1b4d34

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.22.2.24 1997/06/16 13:26:26 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
+comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.22.2.25 1997/06/16 20:02:26 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
 end_comment
 
 begin_include
@@ -3647,6 +3647,13 @@ operator|++
 expr_stmt|;
 if|if
 condition|(
+operator|!
+operator|(
+name|mode
+operator|&
+name|MODE_DDIAL
+operator|)
+operator|&&
 name|VarDialTries
 condition|)
 name|LogPrintf
@@ -3672,6 +3679,13 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+operator|!
+operator|(
+name|mode
+operator|&
+name|MODE_DDIAL
+operator|)
+operator|&&
 name|VarDialTries
 operator|&&
 name|tries
@@ -3723,6 +3737,13 @@ expr_stmt|;
 comment|/* Tries are per number, not per list of numbers. */
 if|if
 condition|(
+operator|!
+operator|(
+name|mode
+operator|&
+name|MODE_DDIAL
+operator|)
+operator|&&
 name|VarDialTries
 condition|)
 name|LogPrintf
@@ -3814,6 +3835,13 @@ block|}
 elseif|else
 if|if
 condition|(
+operator|!
+operator|(
+name|mode
+operator|&
+name|MODE_DDIAL
+operator|)
+operator|&&
 name|VarDialTries
 operator|&&
 name|tries
