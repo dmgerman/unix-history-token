0988522282c9f0bccff6b977e171e7b1965aaaf2
Brian Somers <brian@FreeBSD.org>
1997-06-22 02:22:17 +0000

    YAMFC: Fix this damn mbuf with a negative m_len.  It turns        out to be a problem with VJ header compression.        davidg spotted this in usr.sbin/ppp/slcompress.c        a while ago, but I believe gave the wrong reasons -        it's too easy to reproduce !  The only scenario that        I've been able to reproduce the problem under is when        m_len is *exactly* 40 !  So go figure !
           PR:             3749
           Submitted elsewhere by: davidg
           Obtained from: usr.sbin/ppp/slcompress.c
    
    
    Former-commit-id: e22f2fe4d676fdea626ce9986bcf21d55d43e9a3

--- sys/net/ppp_tty.c
+++ sys/net/ppp_tty.c
@@ -4,7 +4,7 @@ comment|/*  * ppp_tty.c - Point-to-Point Protocol (PPP) driver for asynchronous 
 end_comment
 
 begin_comment
-comment|/* $Id: ppp_tty.c,v 1.11.2.1 1997/06/09 04:15:09 brian Exp $ */
+comment|/* $Id: ppp_tty.c,v 1.11.2.2 1997/06/18 02:52:16 brian Exp $ */
 end_comment
 
 begin_comment
@@ -3194,31 +3194,6 @@ name|spltty
 argument_list|()
 expr_stmt|;
 comment|/* in case.. do not want netisrs to preempt us */
-name|m
-operator|=
-name|sc
-operator|->
-name|sc_outm
-expr_stmt|;
-if|if
-condition|(
-name|m
-operator|&&
-name|m
-operator|->
-name|m_len
-operator|<
-literal|0
-condition|)
-name|MFREE
-argument_list|(
-name|m
-argument_list|,
-name|sc
-operator|->
-name|sc_outm
-argument_list|)
-expr_stmt|;
 name|idle
 operator|=
 literal|0
