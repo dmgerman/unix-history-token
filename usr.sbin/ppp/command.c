334de9076b6cff78ff833a51476f0ac1c540475b
Brian Somers <brian@FreeBSD.org>
1997-06-23 23:10:13 +0000

    o Fix uptime for direct connections. o Style police o Make hangup abort the current connection, not   necessarily exiting (-auto/-ddial). o Trap HUP and INT during DoChat and abort the   connection attempt.  This means you can now   type "dial" and change your mind with ^C, or   HUP the process to stop it dialing.
    Slapped into doing it by: Chuck Robey <chuckr@glue.umd.edu>
    
    
    Former-commit-id: 44c843c98bf3588cd256c0a8989d7e3c11d3153f

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.59 1997/06/17 02:04:51 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.60 1997/06/23 19:18:13 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -672,6 +672,9 @@ block|{
 name|int
 name|tries
 decl_stmt|;
+name|int
+name|res
+decl_stmt|;
 if|if
 condition|(
 name|LcpFsm
@@ -805,8 +808,12 @@ break|break;
 block|}
 if|if
 condition|(
+operator|(
+name|res
+operator|=
 name|DialModem
 argument_list|()
+operator|)
 operator|==
 name|EX_DONE
 condition|)
@@ -824,6 +831,16 @@ argument_list|()
 expr_stmt|;
 break|break;
 block|}
+elseif|else
+if|if
+condition|(
+name|res
+operator|==
+name|EX_SIG
+condition|)
+return|return
+literal|1
+return|;
 block|}
 do|while
 condition|(
