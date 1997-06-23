1af09fa09bb847e12e5ac0a27bb516344911c316
Brian Somers <brian@FreeBSD.org>
1997-06-23 19:19:28 +0000

    YAMFC: Fix "set parity"
           PR:		3881
           Actual problem found by:	shocking@mailbox.uq.edu.au
    
    
    Former-commit-id: 5d899897b3ab082c1fef444e9b74c048c7ce5217

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.21 1997/06/13 04:01:39 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.22 1997/06/17 02:06:50 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -4152,49 +4152,17 @@ modifier|*
 name|argv
 decl_stmt|;
 block|{
-name|int
-name|parity
-decl_stmt|;
-if|if
-condition|(
+return|return
 name|argc
 operator|>
 literal|0
-condition|)
-block|{
-name|parity
-operator|=
+condition|?
 name|ChangeParity
 argument_list|(
 operator|*
 name|argv
 argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|parity
-operator|<
-literal|0
-condition|)
-name|LogPrintf
-argument_list|(
-name|LogWARN
-argument_list|,
-literal|"Invalid parity.\n"
-argument_list|)
-expr_stmt|;
-else|else
-block|{
-name|VarParity
-operator|=
-name|parity
-expr_stmt|;
-return|return
-literal|0
-return|;
-block|}
-block|}
-return|return
+else|:
 operator|-
 literal|1
 return|;
