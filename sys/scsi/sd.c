e8476d23834005eac360056f0d3b5e6e38dbb573
Tor Egge <tegge@FreeBSD.org>
1997-06-25 19:07:43 +0000

    Introduce an advisory exclusive lock on the scsi link structure. Change sd_open, sd_close and sd_ioctl to use this lock to ensure serialization of some critical operations, thus avoiding some race conditions. Ideas picked from NetBSD (ccd and sd devices). This fixes one of the problems noted in PR kern/3688. Reviewed by:	"Justin T. Gibbs" <gibbs@plutotech.com>
    Former-commit-id: 1acda99ec3e5cf258483c40dfea7ae97176985da

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.104 1997/03/24 11:25:02 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.105 1997/05/01 19:15:38 sos Exp $  */
 end_comment
 
 begin_include
@@ -1281,6 +1281,20 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
+name|errcode
+operator|=
+name|scsi_device_lock
+argument_list|(
+name|sc_link
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|errcode
+condition|)
+return|return
+name|errcode
+return|;
 comment|/* 	 * If it's been invalidated, then forget the label 	 */
 name|sc_link
 operator|->
@@ -1631,6 +1645,11 @@ name|sdqueues
 operator|)
 argument_list|)
 expr_stmt|;
+name|scsi_device_unlock
+argument_list|(
+name|sc_link
+argument_list|)
+expr_stmt|;
 return|return
 literal|0
 return|;
@@ -1666,6 +1685,11 @@ operator|~
 name|SDEV_OPEN
 expr_stmt|;
 block|}
+name|scsi_device_unlock
+argument_list|(
+name|sc_link
+argument_list|)
+expr_stmt|;
 return|return
 name|errcode
 return|;
@@ -1716,12 +1740,29 @@ name|scsi_data
 modifier|*
 name|sd
 decl_stmt|;
+name|errval
+name|errcode
+decl_stmt|;
 name|sd
 operator|=
 name|sc_link
 operator|->
 name|sd
 expr_stmt|;
+name|errcode
+operator|=
+name|scsi_device_lock
+argument_list|(
+name|sc_link
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|errcode
+condition|)
+return|return
+name|errcode
+return|;
 name|dsclose
 argument_list|(
 name|dev
@@ -1763,6 +1804,11 @@ operator|~
 name|SDEV_OPEN
 expr_stmt|;
 block|}
+name|scsi_device_unlock
+argument_list|(
+name|sc_link
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 literal|0
@@ -2682,6 +2728,20 @@ return|;
 comment|/* XXX */
 name|error
 operator|=
+name|scsi_device_lock
+argument_list|(
+name|sc_link
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|error
+condition|)
+return|return
+name|error
+return|;
+name|error
+operator|=
 name|dsioctl
 argument_list|(
 literal|"sd"
@@ -2706,6 +2766,11 @@ name|ds_setgeom_t
 operator|*
 operator|)
 name|NULL
+argument_list|)
+expr_stmt|;
+name|scsi_device_unlock
+argument_list|(
+name|sc_link
 argument_list|)
 expr_stmt|;
 if|if
