1a6adf822a82f6e6b19e01ae2f0d8628ada445be
Steve Price <steve@FreeBSD.org>
1997-06-29 06:55:02 +0000

    Import of GNU patch version 2.4.
    Former-commit-id: 511633a2fe6a28e37e7c367915d4b6c131ded5a1

---
+++ contrib/patch/version.h
@@ -0,0 +1,23 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* Print the version number.  */
+end_comment
+
+begin_comment
+comment|/* $Id: version.h,v 1.3 1997/04/07 01:07:00 eggert Exp $ */
+end_comment
+
+begin_decl_stmt
+name|void
+name|version
+name|PARAMS
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
