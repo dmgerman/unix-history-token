ee07c1fd0d1cdded138731789aec7460c990d729
Bruce Evans <bde@FreeBSD.org>
1997-07-06 03:03:10 +0000

    Merge from Lite2 (make the command `sum' an alias for `cksum -o 1', and reject -o args other than "1" or "2").
    Former-commit-id: 273bfaba7800bf9b7653353203c2605d09644932

--- usr.bin/cksum/cksum.c
+++ usr.bin/cksum/cksum.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * James W. Williams of NASA Goddard Space Flight Center.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * James W. Williams of NASA Goddard Space Flight Center.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cksum.c,v 1.3 1997/06/25 07:02:03 charnier Exp $  */
 end_comment
 
 begin_ifndef
@@ -40,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)cksum.c	8.1 (Berkeley) 6/6/93"
+literal|"@(#)cksum.c	8.2 (Berkeley) 4/28/95"
 decl_stmt|;
 end_decl_stmt
 
@@ -143,15 +143,6 @@ modifier|*
 name|argv
 decl_stmt|;
 block|{
-specifier|extern
-name|int
-name|optind
-decl_stmt|;
-name|u_long
-name|len
-decl_stmt|,
-name|val
-decl_stmt|;
 specifier|register
 name|int
 name|ch
@@ -160,9 +151,17 @@ name|fd
 decl_stmt|,
 name|rval
 decl_stmt|;
+name|u_long
+name|len
+decl_stmt|,
+name|val
+decl_stmt|;
 name|char
 modifier|*
 name|fn
+decl_stmt|,
+modifier|*
+name|p
 decl_stmt|;
 name|int
 argument_list|(
@@ -201,6 +200,43 @@ name|long
 operator|)
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|(
+name|p
+operator|=
+name|rindex
+argument_list|(
+name|argv
+index|[
+literal|0
+index|]
+argument_list|,
+literal|'/'
+argument_list|)
+operator|)
+operator|==
+name|NULL
+condition|)
+name|p
+operator|=
+name|argv
+index|[
+literal|0
+index|]
+expr_stmt|;
+else|else
+operator|++
+name|p
+expr_stmt|;
+if|if
+condition|(
+operator|*
+name|p
+operator|==
+literal|'c'
+condition|)
+block|{
 name|cfncn
 operator|=
 name|crc
@@ -209,6 +245,18 @@ name|pfncn
 operator|=
 name|pcrc
 expr_stmt|;
+block|}
+else|else
+block|{
+name|cfncn
+operator|=
+name|csum1
+expr_stmt|;
+name|pfncn
+operator|=
+name|psum1
+expr_stmt|;
+block|}
 while|while
 condition|(
 operator|(
@@ -237,10 +285,13 @@ literal|'o'
 case|:
 if|if
 condition|(
-operator|*
+operator|!
+name|strcmp
+argument_list|(
 name|optarg
-operator|==
-literal|'1'
+argument_list|,
+literal|"1"
+argument_list|)
 condition|)
 block|{
 name|cfncn
@@ -255,10 +306,13 @@ block|}
 elseif|else
 if|if
 condition|(
-operator|*
+operator|!
+name|strcmp
+argument_list|(
 name|optarg
-operator|==
-literal|'2'
+argument_list|,
+literal|"2"
+argument_list|)
 condition|)
 block|{
 name|cfncn
