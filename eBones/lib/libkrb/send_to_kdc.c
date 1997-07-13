50dbe5f4154a9d272c67cea0372eb6ac722eaeb4
Satoshi Asami <asami@FreeBSD.org>
1997-07-13 21:29:50 +0000

    Merge 1.13.
    Former-commit-id: 9c81f55d038a3e6f190151d6a8e3874ba23a628d

--- eBones/lib/libkrb/send_to_kdc.c
+++ eBones/lib/libkrb/send_to_kdc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright 1987, 1988 by the Massachusetts Institute of Technology.  * For copying and distribution information, please see the file  *<Copyright.MIT>.  *  *	from: send_to_kdc.c,v 4.20 90/01/02 13:40:37 jtkohl Exp $  *	$Id: send_to_kdc.c,v 1.9 1995/09/16 23:11:25 gibbs Exp $  */
+comment|/*  * Copyright 1987, 1988 by the Massachusetts Institute of Technology.  * For copying and distribution information, please see the file  *<Copyright.MIT>.  *  *	from: send_to_kdc.c,v 4.20 90/01/02 13:40:37 jtkohl Exp $  *	$Id: send_to_kdc.c,v 1.10 1995/10/05 21:29:45 gibbs Exp $  */
 end_comment
 
 begin_if
@@ -202,23 +202,6 @@ begin_decl_stmt
 specifier|extern
 name|int
 name|krb_debug
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|char
-modifier|*
-name|malloc
-argument_list|()
-decl_stmt|,
-modifier|*
-name|calloc
-argument_list|()
-decl_stmt|,
-modifier|*
-name|realloc
-argument_list|()
 decl_stmt|;
 end_decl_stmt
 
