7bd783aec8328fcfce75ded3a97756a306af4614
Stefan Esser <se@FreeBSD.org>
1997-07-18 19:33:56 +0000

    Fix "unexpected phase change" interrupt handler: Do not access the dstat register twice, pass the value read the first time to the fixup code instead.
    Submitted by:	Gerard Roudier <groudier@club-internet.fr>
    
    
    Former-commit-id: d5f644dfd5f1aaeccd8da43e982b2eab8ef77db0

--- sys/pci/ncr.c
+++ sys/pci/ncr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: ncr.c,v 1.97 1997/04/20 15:48:17 bde Exp $ ** **  Device driver for the   NCR 53C810   PCI-SCSI-Controller. ** **  FreeBSD / NetBSD ** **------------------------------------------------------------------------- ** **  Written for 386bsd and FreeBSD by **	Wolfgang Stanglmeier<wolf@cologne.de> **	Stefan Esser<se@mi.Uni-Koeln.de> ** **  Ported to NetBSD by **	Charles M. Hannum<mycroft@gnu.ai.mit.edu> ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: ncr.c,v 1.98 1997/06/11 22:36:02 se Exp $ ** **  Device driver for the   NCR 53C810   PCI-SCSI-Controller. ** **  FreeBSD / NetBSD ** **------------------------------------------------------------------------- ** **  Written for 386bsd and FreeBSD by **	Wolfgang Stanglmeier<wolf@cologne.de> **	Stefan Esser<se@mi.Uni-Koeln.de> ** **  Ported to NetBSD by **	Charles M. Hannum<mycroft@gnu.ai.mit.edu> ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
 end_comment
 
 begin_define
@@ -2726,6 +2726,9 @@ name|ncr_int_ma
 parameter_list|(
 name|ncb_p
 name|np
+parameter_list|,
+name|u_char
+name|dstat
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -3111,7 +3114,7 @@ name|char
 name|ident
 index|[]
 init|=
-literal|"\n$Id: ncr.c,v 1.97 1997/04/20 15:48:17 bde Exp $\n"
+literal|"\n$Id: ncr.c,v 1.98 1997/06/11 22:36:02 se Exp $\n"
 decl_stmt|;
 end_decl_stmt
 
@@ -15469,6 +15472,8 @@ block|{
 name|ncr_int_ma
 argument_list|(
 name|np
+argument_list|,
+name|dstat
 argument_list|)
 expr_stmt|;
 return|return;
@@ -16645,6 +16650,9 @@ name|ncr_int_ma
 parameter_list|(
 name|ncb_p
 name|np
+parameter_list|,
+name|u_char
+name|dstat
 parameter_list|)
 block|{
 name|u_long
@@ -16764,10 +16772,7 @@ if|if
 condition|(
 operator|!
 operator|(
-name|INB
-argument_list|(
-name|nc_dstat
-argument_list|)
+name|dstat
 operator|&
 name|DFE
 operator|)
@@ -17499,6 +17504,16 @@ name|newcmd
 argument_list|)
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|(
+name|cmd
+operator|&
+literal|7
+operator|)
+operator|==
+literal|0
+condition|)
 name|OUTL
 argument_list|(
 name|nc_dsp
@@ -17508,6 +17523,19 @@ argument_list|(
 name|np
 argument_list|,
 name|dispatch
+argument_list|)
+argument_list|)
+expr_stmt|;
+else|else
+name|OUTL
+argument_list|(
+name|nc_dsp
+argument_list|,
+name|NCB_SCRIPT_PHYS
+argument_list|(
+name|np
+argument_list|,
+name|checkatn
 argument_list|)
 argument_list|)
 expr_stmt|;
