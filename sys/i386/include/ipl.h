71a32f00c4a5380ef97a7ce0634f2e4199b57e39
Bruce Evans <bde@FreeBSD.org>
1997-07-20 10:35:50 +0000

    Hide definitions that depend on the APIC_IO option from LKMs so that LKMs break properly if they use these definitions.
    Use '#ifdef notyet' instead of '#if 0' to hide temporarily misplaced
    declarations.
    
    
    Former-commit-id: 5a5cada329b5753564143272134e8af2ac26b9bc

--- sys/i386/include/ipl.h
+++ sys/i386/include/ipl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ipl.h,v 1.9 1997/05/31 09:16:36 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -14,6 +14,21 @@ define|#
 directive|define
 name|_MACHINE_IPL_H_
 end_define
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|KERNEL
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|ACTUALLY_LKM_NOT_KERNEL
+argument_list|)
+end_if
 
 begin_ifdef
 ifdef|#
@@ -146,6 +161,15 @@ name|SWI_MASK
 value|(~HWI_MASK)
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* KERNEL&& !ACTUALLY_LKM_NOT_KERNEL */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -156,14 +180,24 @@ begin_comment
 comment|/*  * cpl is preserved by interrupt handlers so it is effectively nonvolatile.  * ipending and idelayed are changed by interrupt handlers so they are  * volatile.  */
 end_comment
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|notyet
+end_ifdef
 
 begin_comment
-unit|extern	unsigned bio_imask;
+comment|/* in<sys/interrupt.h> until pci drivers stop hacking on them */
+end_comment
+
+begin_decl_stmt
+specifier|extern
+name|unsigned
+name|bio_imask
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
 comment|/* group of interrupts masked with splbio() */
 end_comment
 
@@ -207,24 +241,46 @@ begin_comment
 comment|/* active interrupts masked by cpl */
 end_comment
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|notyet
+end_ifdef
 
 begin_comment
-unit|extern	unsigned net_imask;
+comment|/* in<sys/interrupt.h> until pci drivers stop hacking on them */
+end_comment
+
+begin_decl_stmt
+specifier|extern
+name|unsigned
+name|net_imask
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
 comment|/* group of interrupts masked with splimp() */
 end_comment
 
+begin_decl_stmt
+specifier|extern
+name|unsigned
+name|stat_imask
+decl_stmt|;
+end_decl_stmt
+
 begin_comment
-unit|extern	unsigned stat_imask;
 comment|/* interrupts masked with splstatclock() */
 end_comment
 
+begin_decl_stmt
+specifier|extern
+name|unsigned
+name|tty_imask
+decl_stmt|;
+end_decl_stmt
+
 begin_comment
-unit|extern	unsigned tty_imask;
 comment|/* group of interrupts masked with spltty() */
 end_comment
 
