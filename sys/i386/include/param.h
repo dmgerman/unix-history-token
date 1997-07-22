8707a8c1d7aaa1ba05acca3cfc58c079703f3695
Steve Passe <fsmp@FreeBSD.org>
1997-07-22 23:19:48 +0000

    Added a missing semicolon, compile failed when I turned on simple_lock(). Evidently this has never been compiled before, NOT a good sign...
    Former-commit-id: deed5e4a7b2ccc3dbe3f38524ae55ef2879f04f4

--- sys/i386/include/param.h
+++ sys/i386/include/param.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)param.h	5.8 (Berkeley) 6/28/91  *	$Id: param.h,v 1.26 1997/02/22 09:34:54 peter Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)param.h	5.8 (Berkeley) 6/28/91  *	$Id: param.h,v 1.27 1997/04/07 09:27:21 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -441,13 +441,11 @@ name|__inline
 name|void
 name|simple_lock_init
 parameter_list|(
-name|lkp
-parameter_list|)
 name|struct
 name|simplelock
 modifier|*
 name|lkp
-decl_stmt|;
+parameter_list|)
 block|{
 name|lkp
 operator|->
@@ -464,14 +462,12 @@ name|__inline
 name|void
 name|simple_lock
 parameter_list|(
-name|lkp
-parameter_list|)
 name|__volatile
 name|struct
 name|simplelock
 modifier|*
 name|lkp
-decl_stmt|;
+parameter_list|)
 block|{
 while|while
 condition|(
@@ -493,14 +489,12 @@ name|__inline
 name|int
 name|simple_lock_try
 parameter_list|(
-name|lkp
-parameter_list|)
 name|__volatile
 name|struct
 name|simplelock
 modifier|*
 name|lkp
-decl_stmt|;
+parameter_list|)
 block|{
 return|return
 operator|(
@@ -513,6 +507,7 @@ operator|->
 name|lock_data
 argument_list|)
 operator|)
+return|;
 block|}
 end_function
 
@@ -522,14 +517,12 @@ name|__inline
 name|void
 name|simple_unlock
 parameter_list|(
-name|lkp
-parameter_list|)
 name|__volatile
 name|struct
 name|simplelock
 modifier|*
 name|lkp
-decl_stmt|;
+parameter_list|)
 block|{
 name|lkp
 operator|->
