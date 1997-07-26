c71997e355d4040a6f1fb7fe0ceb281e6cb326a9
Takenori KATO <kato@FreeBSD.org>
1997-07-26 13:52:47 +0000

    Synchronize with sys/i386/isa/clock.c revision 1.98.
    Former-commit-id: 0689566ff9f0fa5ed2558b9a223128652009b3cd

--- sys/pc98/cbus/clock.c
+++ sys/pc98/cbus/clock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.29 1997/07/21 13:12:01 kato Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.30 1997/07/23 11:28:39 kato Exp $  */
 end_comment
 
 begin_comment
@@ -117,7 +117,7 @@ file|<machine/smptests.h>
 end_include
 
 begin_comment
-comment|/** TEST_ALTTIMER, APIC_PIN0_TIMER */
+comment|/** NEW_STRATEGY (,SMP_TIMER_NC) */
 end_comment
 
 begin_endif
@@ -4734,6 +4734,16 @@ name|APIC_IO
 ifdef|#
 directive|ifdef
 name|NEW_STRATEGY
+ifdef|#
+directive|ifdef
+name|SMP_TIMER_NC
+error|#
+directive|error
+literal|'options SMP_TIMER_NC'
+error|no longer used, remove& reconfig.
+endif|#
+directive|endif
+comment|/** XXX SMP_TIMER_NC */
 comment|/* 1st look for ExtInt on pin 0 */
 if|if
 condition|(
@@ -4764,7 +4774,7 @@ argument_list|,
 name|NRSVIDT
 argument_list|)
 expr_stmt|;
-comment|/* start vector */
+comment|/* start vector (unused) */
 name|outb
 argument_list|(
 name|IO_ICU1
@@ -4816,7 +4826,7 @@ name|x
 operator|=
 literal|0
 expr_stmt|;
-comment|/** if (bootverbose */
+comment|/* XXX if (bootverbose) */
 name|printf
 argument_list|(
 literal|"APIC_IO: routing 8254 via 8259 on pin 0\n"
@@ -4839,7 +4849,7 @@ name|x
 operator|=
 literal|2
 expr_stmt|;
-comment|/** if (bootverbose */
+comment|/* XXX if (bootverbose) */
 name|printf
 argument_list|(
 literal|"APIC_IO: routing 8254 via pin 2\n"
@@ -4853,180 +4863,36 @@ argument_list|(
 literal|"neither pin 0 or pin 2 works for 8254"
 argument_list|)
 expr_stmt|;
-comment|/* setup the vectors for the chosen method */
-name|vec
-index|[
-name|x
-index|]
-operator|=
-operator|(
-name|u_int
-operator|)
-name|vec8254
-expr_stmt|;
-name|Xintr8254
-operator|=
-operator|(
-name|u_int
-operator|)
-name|ivectors
-index|[
-name|x
-index|]
-expr_stmt|;
-name|mask8254
-operator|=
-operator|(
-literal|1
-operator|<<
-name|x
-operator|)
-expr_stmt|;
-name|register_intr
-argument_list|(
-comment|/* irq */
-name|x
-argument_list|,
-comment|/* XXX id */
-literal|0
-argument_list|,
-comment|/* flags */
-literal|0
-argument_list|,
-comment|/* XXX */
-operator|(
-name|inthand2_t
-operator|*
-operator|)
-name|clkintr
-argument_list|,
-operator|&
-name|clk_imask
-argument_list|,
-comment|/* unit */
-literal|0
-argument_list|)
-expr_stmt|;
-name|INTREN
-argument_list|(
-name|mask8254
-argument_list|)
-expr_stmt|;
 else|#
 directive|else
 comment|/** NEW_STRATEGY */
-ifdef|#
-directive|ifdef
-name|APIC_PIN0_TIMER
-comment|/* 	 * Allow 8254 timer to INTerrupt 8259: 	 *  re-initialize master 8259: 	 *   reset; prog 4 bytes, single ICU, edge triggered 	 */
-name|outb
+comment|/* 8254 is traditionally on ISA IRQ0 */
+if|#
+directive|if
+name|defined
 argument_list|(
-name|IO_ICU1
-argument_list|,
-literal|0x13
+name|SMP_TIMER_NC
 argument_list|)
-expr_stmt|;
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
+name|x
+operator|=
+operator|-
 literal|1
-argument_list|,
-name|NRSVIDT
-argument_list|)
-expr_stmt|;
-comment|/* start vector */
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-literal|0x00
-argument_list|)
-expr_stmt|;
-comment|/* ignore slave */
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-literal|0x03
-argument_list|)
-expr_stmt|;
-comment|/* auto EOI, 8086 */
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-literal|0xfe
-argument_list|)
-expr_stmt|;
-comment|/* unmask INT0 */
-comment|/* program IO APIC for type 3 INT on INT0 */
-if|if
-condition|(
-name|ext_int_setup
-argument_list|(
-literal|0
-argument_list|,
-literal|0
-argument_list|)
-operator|<
-literal|0
-condition|)
-name|panic
-argument_list|(
-literal|"8254 redirect via APIC pin0 impossible!"
-argument_list|)
-expr_stmt|;
-name|register_intr
-argument_list|(
-comment|/* irq */
-literal|0
-argument_list|,
-comment|/* XXX id */
-literal|0
-argument_list|,
-comment|/* flags */
-literal|0
-argument_list|,
-comment|/* XXX */
-operator|(
-name|inthand2_t
-operator|*
-operator|)
-name|clkintr
-argument_list|,
-operator|&
-name|clk_imask
-argument_list|,
-comment|/* unit */
-literal|0
-argument_list|)
-expr_stmt|;
-name|INTREN
-argument_list|(
-name|APIC_IRQ0
-argument_list|)
 expr_stmt|;
 else|#
 directive|else
-comment|/* APIC_PIN0_TIMER */
-comment|/* 8254 is traditionally on ISA IRQ0 */
-if|if
-condition|(
-operator|(
 name|x
 operator|=
 name|isa_apic_pin
 argument_list|(
 literal|0
 argument_list|)
-operator|)
+expr_stmt|;
+endif|#
+directive|endif
+comment|/** XXX SMP_TIMER_NC */
+if|if
+condition|(
+name|x
 operator|<
 literal|0
 condition|)
@@ -5042,59 +4908,6 @@ literal|"APIC missing 8254 connection\n"
 argument_list|)
 expr_stmt|;
 comment|/* allow 8254 timer to INTerrupt 8259 */
-ifdef|#
-directive|ifdef
-name|TEST_ALTTIMER
-comment|/* 		 * re-initialize master 8259: 		 * reset; prog 4 bytes, single ICU, edge triggered 		 */
-name|outb
-argument_list|(
-name|IO_ICU1
-argument_list|,
-literal|0x13
-argument_list|)
-expr_stmt|;
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-name|NRSVIDT
-argument_list|)
-expr_stmt|;
-comment|/* start vector */
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-literal|0x00
-argument_list|)
-expr_stmt|;
-comment|/* ignore slave */
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-literal|0x03
-argument_list|)
-expr_stmt|;
-comment|/* auto EOI, 8086 */
-name|outb
-argument_list|(
-name|IO_ICU1
-operator|+
-literal|1
-argument_list|,
-literal|0xfe
-argument_list|)
-expr_stmt|;
-comment|/* unmask INT0 */
-else|#
-directive|else
 name|x
 operator|=
 name|inb
@@ -5121,9 +4934,6 @@ name|x
 argument_list|)
 expr_stmt|;
 comment|/* write new mask */
-endif|#
-directive|endif
-comment|/* TEST_ALTTIMER */
 comment|/* program IO APIC for type 3 INT on INT0 */
 if|if
 condition|(
@@ -5147,6 +4957,10 @@ literal|0
 expr_stmt|;
 comment|/* 8259 is on 0 */
 block|}
+endif|#
+directive|endif
+comment|/** NEW_STRATEGY */
+comment|/* setup the vectors */
 name|vec
 index|[
 name|x
@@ -5167,7 +4981,6 @@ index|[
 name|x
 index|]
 expr_stmt|;
-comment|/* XXX might need Xfastintr# */
 name|mask8254
 operator|=
 operator|(
@@ -5206,12 +5019,6 @@ argument_list|(
 name|mask8254
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
-comment|/* APIC_PIN0_TIMER */
-endif|#
-directive|endif
-comment|/** NEW_STRATEGY */
 else|#
 directive|else
 comment|/* APIC_IO */
