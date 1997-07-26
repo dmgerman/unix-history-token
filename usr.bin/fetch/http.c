a6be68c239346bc7b0e036d4f9bda7b8a674e26d
Garrett Wollman <wollman@FreeBSD.org>
1997-07-26 20:00:05 +0000

    Fix the error message for when mirroring and restarting are rejected due to the output not being a regular file.  Also split the error message in any error case here to be on two lines to lessen the likelihood of it being too long to fit on just one.
    Former-commit-id: d07aa0c4dbcf6aa57bcf8657e0a450f201b87b66

--- usr.bin/fetch/http.c
+++ usr.bin/fetch/http.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright 1997 Massachusetts Institute of Technology  *  * Permission to use, copy, modify, and distribute this software and  * its documentation for any purpose and without fee is hereby  * granted, provided that both the above copyright notice and this  * permission notice appear in all copies, that both the above  * copyright notice and this permission notice appear in all  * supporting documentation, and that the name of M.I.T. not be used  * in advertising or publicity pertaining to distribution of the  * software without specific, written prior permission.  M.I.T. makes  * no representations about the suitability of this software for any  * purpose.  It is provided "as is" without express or implied  * warranty.  *   * THIS SOFTWARE IS PROVIDED BY M.I.T. ``AS IS''.  M.I.T. DISCLAIMS  * ALL EXPRESS OR IMPLIED WARRANTIES WITH REGARD TO THIS SOFTWARE,  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT  * SHALL M.I.T. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: http.c,v 1.7 1997/07/25 19:35:43 wollman Exp $  */
+comment|/*-  * Copyright 1997 Massachusetts Institute of Technology  *  * Permission to use, copy, modify, and distribute this software and  * its documentation for any purpose and without fee is hereby  * granted, provided that both the above copyright notice and this  * permission notice appear in all copies, that both the above  * copyright notice and this permission notice appear in all  * supporting documentation, and that the name of M.I.T. not be used  * in advertising or publicity pertaining to distribution of the  * software without specific, written prior permission.  M.I.T. makes  * no representations about the suitability of this software for any  * purpose.  It is provided "as is" without express or implied  * warranty.  *   * THIS SOFTWARE IS PROVIDED BY M.I.T. ``AS IS''.  M.I.T. DISCLAIMS  * ALL EXPRESS OR IMPLIED WARRANTIES WITH REGARD TO THIS SOFTWARE,  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT  * SHALL M.I.T. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: http.c,v 1.8 1997/07/26 19:25:56 wollman Exp $  */
 end_comment
 
 begin_include
@@ -2667,15 +2667,44 @@ condition|(
 name|errno
 operator|!=
 literal|0
+operator|||
+operator|!
+name|S_ISREG
+argument_list|(
+name|stab
+operator|.
+name|st_mode
+argument_list|)
 condition|)
 block|{
+if|if
+condition|(
+name|errno
+operator|!=
+literal|0
+condition|)
 name|warn
 argument_list|(
-literal|"%s: cannot mirror; will retrieve anew"
+literal|"%s"
 argument_list|,
 name|fs
 operator|->
 name|fs_outputfile
+argument_list|)
+expr_stmt|;
+else|else
+name|warnx
+argument_list|(
+literal|"%s: not a regular file"
+argument_list|,
+name|fs
+operator|->
+name|fs_outputfile
+argument_list|)
+expr_stmt|;
+name|warnx
+argument_list|(
+literal|"cannot mirror; will retrieve anew"
 argument_list|)
 expr_stmt|;
 block|}
@@ -2798,27 +2827,35 @@ condition|(
 name|errno
 operator|!=
 literal|0
+operator|||
+operator|!
+name|S_ISREG
+argument_list|(
+name|stab
+operator|.
+name|st_mode
+argument_list|)
 condition|)
 block|{
+if|if
+condition|(
+name|errno
+operator|!=
+literal|0
+condition|)
 name|warn
 argument_list|(
-literal|"%s: cannot restart; will retrieve anew"
+literal|"%s"
 argument_list|,
 name|fs
 operator|->
 name|fs_outputfile
 argument_list|)
 expr_stmt|;
-name|restarting
-operator|=
-literal|0
-expr_stmt|;
-block|}
 else|else
-block|{
 name|warnx
 argument_list|(
-literal|"%s: cannot restart; will retrieve anew"
+literal|"%s: not a regular file"
 argument_list|,
 name|fs
 operator|->
@@ -2828,6 +2865,11 @@ expr_stmt|;
 name|restarting
 operator|=
 literal|0
+expr_stmt|;
+name|warnx
+argument_list|(
+literal|"cannot restart; will retrieve anew"
+argument_list|)
 expr_stmt|;
 block|}
 block|}
