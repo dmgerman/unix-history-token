ea93db88ce6e0ee800733cf2a9ce98627e286c7e
John Dyson <dyson@FreeBSD.org>
1997-07-27 04:44:12 +0000

    Fix a very subtile problem that causes unnessary numbers of objects backing a single logical object. Submitted by:	Alan Cox <alc@cs.rice.edu>
    Former-commit-id: 65920f9c7bf926285c0d444b8ab87d73ba4fb3e1

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_map.c	8.3 (Berkeley) 1/12/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_map.c,v 1.77 1997/06/15 23:33:52 dyson Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_map.c	8.3 (Berkeley) 1/12/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_map.c,v 1.78 1997/06/23 21:51:03 tegge Exp $  */
 end_comment
 
 begin_comment
@@ -3339,6 +3339,54 @@ argument_list|,
 name|entry
 argument_list|)
 expr_stmt|;
+comment|/* 	 * If there is no object backing this entry, we might as well create 	 * one now.  If we defer it, an object can get created after the map 	 * is clipped, and individual objects will be created for the split-up 	 * map.  This is a bit of a hack, but is also about the best place to 	 * put this improvement. 	 */
+if|if
+condition|(
+name|entry
+operator|->
+name|object
+operator|.
+name|vm_object
+operator|==
+name|NULL
+condition|)
+block|{
+name|vm_object_t
+name|object
+decl_stmt|;
+name|object
+operator|=
+name|vm_object_allocate
+argument_list|(
+name|OBJT_DEFAULT
+argument_list|,
+name|OFF_TO_IDX
+argument_list|(
+name|entry
+operator|->
+name|end
+operator|-
+name|entry
+operator|->
+name|start
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|entry
+operator|->
+name|object
+operator|.
+name|vm_object
+operator|=
+name|object
+expr_stmt|;
+name|entry
+operator|->
+name|offset
+operator|=
+literal|0
+expr_stmt|;
+block|}
 name|new_entry
 operator|=
 name|vm_map_entry_create
@@ -3472,6 +3520,54 @@ specifier|register
 name|vm_map_entry_t
 name|new_entry
 decl_stmt|;
+comment|/* 	 * If there is no object backing this entry, we might as well create 	 * one now.  If we defer it, an object can get created after the map 	 * is clipped, and individual objects will be created for the split-up 	 * map.  This is a bit of a hack, but is also about the best place to 	 * put this improvement. 	 */
+if|if
+condition|(
+name|entry
+operator|->
+name|object
+operator|.
+name|vm_object
+operator|==
+name|NULL
+condition|)
+block|{
+name|vm_object_t
+name|object
+decl_stmt|;
+name|object
+operator|=
+name|vm_object_allocate
+argument_list|(
+name|OBJT_DEFAULT
+argument_list|,
+name|OFF_TO_IDX
+argument_list|(
+name|entry
+operator|->
+name|end
+operator|-
+name|entry
+operator|->
+name|start
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|entry
+operator|->
+name|object
+operator|.
+name|vm_object
+operator|=
+name|object
+expr_stmt|;
+name|entry
+operator|->
+name|offset
+operator|=
+literal|0
+expr_stmt|;
+block|}
 comment|/* 	 * Create a new entry and insert it AFTER the specified entry 	 */
 name|new_entry
 operator|=
@@ -7769,7 +7865,7 @@ break|break;
 case|case
 name|VM_INHERIT_SHARE
 case|:
-comment|/*                          * Clone the entry, creating the shared object if necessary.                          */
+comment|/* 			 * Clone the entry, creating the shared object if necessary. 			 */
 name|object
 operator|=
 name|old_entry
