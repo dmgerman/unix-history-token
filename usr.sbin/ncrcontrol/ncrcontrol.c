2c07afe1b2db964847f2df3cfeda1309a6d598bf
Stefan Esser <se@FreeBSD.org>
1997-07-28 21:33:45 +0000

    Make compile with rev. 1.101 of ncr.c, which added support for loading of the SCIPTS microcode into the NCR on-chip RAM.
    Former-commit-id: 3c65564f362bd3391ba580c9875e1a46b64d8b85

--- usr.sbin/ncrcontrol/ncrcontrol.c
+++ usr.sbin/ncrcontrol/ncrcontrol.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: ncrcontrol.c,v 1.16 1997/02/22 16:08:15 peter Exp $ ** **  Utility for NCR 53C810 device driver. ** **  386bsd / FreeBSD / NetBSD ** **------------------------------------------------------------------------- ** **  Written for 386bsd and FreeBSD by **	wolf@dentaro.gun.de	Wolfgang Stanglmeier **	se@mi.Uni-Koeln.de	Stefan Esser ** **  Ported to NetBSD by **	mycroft@gnu.ai.mit.edu ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: ncrcontrol.c,v 1.17 1997/07/25 20:46:39 se Exp $ ** **  Utility for NCR 53C810 device driver. ** **  386bsd / FreeBSD / NetBSD ** **------------------------------------------------------------------------- ** **  Written for 386bsd and FreeBSD by **	wolf@dentaro.gun.de	Wolfgang Stanglmeier **	se@mi.Uni-Koeln.de	Stefan Esser ** **  Ported to NetBSD by **	mycroft@gnu.ai.mit.edu ** **------------------------------------------------------------------------- ** ** Copyright (c) 1994 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** ** *************************************************************************** */
 end_comment
 
 begin_include
@@ -4129,7 +4129,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|tryloop
 argument_list|)
@@ -4571,7 +4571,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|msg_parity
 argument_list|)
@@ -4597,7 +4597,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|msg_reject
 argument_list|)
@@ -4623,7 +4623,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|msg_extended
 argument_list|)
@@ -4649,7 +4649,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|msg_sdtr
 argument_list|)
@@ -4909,7 +4909,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|msg_out_abort
 argument_list|)
@@ -4935,7 +4935,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|getcc
 argument_list|)
@@ -4961,7 +4961,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|getcc1
 argument_list|)
@@ -4987,7 +4987,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|getcc2
 argument_list|)
@@ -5247,7 +5247,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|aborttag
 argument_list|)
@@ -5273,7 +5273,7 @@ operator|-
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|abort
 argument_list|)
@@ -8036,7 +8036,7 @@ operator|+
 name|offsetof
 argument_list|(
 expr|struct
-name|script
+name|scripth
 argument_list|,
 name|tryloop
 argument_list|)
