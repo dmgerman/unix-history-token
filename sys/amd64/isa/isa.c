2f43ff8b1894b36adcb6d82f0eae2fac8d40a86b
Michael Smith <msmith@FreeBSD.org>
1997-07-29 05:24:36 +0000

    Return to using disable/enable_intr() for guarding DMA register access. Mask the read value from the count register in order to return zero correctly after TC, as per intel datasheet : "If it is not autoinitialised, this register will have a count of FFFFH after TC"
    Former-commit-id: 77093f81503f2ff0bdaeb8453f9db965e278ea24

--- sys/amd64/isa/isa.c
+++ sys/amd64/isa/isa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: isa.c,v 1.97 1997/07/28 07:49:40 msmith Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: isa.c,v 1.98 1997/07/28 09:13:11 msmith Exp $  */
 end_comment
 
 begin_comment
@@ -4027,12 +4027,6 @@ operator|+
 literal|2
 expr_stmt|;
 block|}
-name|ef
-operator|=
-name|read_eflags
-argument_list|()
-expr_stmt|;
-comment|/* get current flags */
 name|disable_intr
 argument_list|()
 expr_stmt|;
@@ -4066,7 +4060,7 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
-comment|/* clear again (paranoia?) */
+comment|/* clear again */
 name|low2
 operator|=
 name|inb
@@ -4081,13 +4075,11 @@ argument_list|(
 name|waport
 argument_list|)
 expr_stmt|;
-name|write_eflags
-argument_list|(
-name|ef
-argument_list|)
+name|enable_intr
+argument_list|()
 expr_stmt|;
-comment|/* restore flags */
-comment|/* now decide if a wrap has tried to skew our results */
+comment|/* enable interrupts again */
+comment|/*  	 * Now decide if a wrap has tried to skew our results. 	 * Note that after TC, the count will read 0xffff, while we want  	 * to return zero, so we add and then mask to compensate. 	 */
 if|if
 condition|(
 name|low1
@@ -4097,6 +4089,7 @@ condition|)
 block|{
 name|cnt
 operator|=
+operator|(
 name|low1
 operator|+
 operator|(
@@ -4106,12 +4099,16 @@ literal|8
 operator|)
 operator|+
 literal|1
+operator|)
+operator|&
+literal|0xffff
 expr_stmt|;
 block|}
 else|else
 block|{
 name|cnt
 operator|=
+operator|(
 name|low2
 operator|+
 operator|(
@@ -4121,6 +4118,9 @@ literal|8
 operator|)
 operator|+
 literal|1
+operator|)
+operator|&
+literal|0xffff
 expr_stmt|;
 block|}
 if|if
