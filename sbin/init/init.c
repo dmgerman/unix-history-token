18ec250d61233d8b9f3dad00814baea5910f5f73
Andrey Chernov <ache@FreeBSD.org>
1997-08-06 16:07:52 +0000

    rc.shutdown fixes: 1) revoke -> HUP 2) controlling terminal already present 3) add missing setprocresources call
    Former-commit-id: 7c0044aca6c7dd0cd7d5b79bafdb4a3cb17831c3

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Donn Seeley at Berkeley Software Design, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *		$Id: init.c,v 1.23 1997/07/08 11:51:11 ache Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Donn Seeley at Berkeley Software Design, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *		$Id: init.c,v 1.24 1997/08/02 00:22:49 davidn Exp $  */
 end_comment
 
 begin_ifndef
@@ -6063,10 +6063,6 @@ name|sp
 decl_stmt|;
 specifier|register
 name|int
-name|rcdown
-decl_stmt|;
-specifier|register
-name|int
 name|i
 decl_stmt|;
 name|pid_t
@@ -6117,20 +6113,20 @@ name|se_flags
 operator||=
 name|SE_SHUTDOWN
 expr_stmt|;
-operator|(
-name|void
-operator|)
-name|revoke
+name|kill
 argument_list|(
 name|sp
 operator|->
-name|se_device
+name|se_process
+argument_list|,
+name|SIGHUP
 argument_list|)
 expr_stmt|;
 block|}
 comment|/* Try to run the rc.shutdown script within a period of time */
-name|rcdown
-operator|=
+operator|(
+name|void
+operator|)
 name|runshutdown
 argument_list|()
 expr_stmt|;
@@ -6299,9 +6295,7 @@ block|{
 name|int
 name|fd
 decl_stmt|;
-name|setsid
-argument_list|()
-expr_stmt|;
+comment|/* Assume that init already grab console as ctty before */
 name|sigemptyset
 argument_list|(
 operator|&
@@ -6358,7 +6352,6 @@ operator|)
 literal|0
 argument_list|)
 expr_stmt|;
-comment|/* 		 * Clean our descriptor table to be sure of 		 * getting /dev/console as control terminal. 		 */
 for|for
 control|(
 name|fd
@@ -6369,7 +6362,7 @@ init|;
 name|fd
 operator|--
 operator|>
-literal|0
+literal|2
 condition|;
 control|)
 operator|(
@@ -6405,29 +6398,14 @@ argument_list|)
 expr_stmt|;
 else|else
 block|{
-if|if
-condition|(
-name|ioctl
+operator|(
+name|void
+operator|)
+name|dup2
 argument_list|(
 name|fd
 argument_list|,
-name|TIOCSCTTY
-argument_list|,
-operator|(
-name|char
-operator|*
-operator|)
-name|NULL
-argument_list|)
-operator|==
-operator|-
-literal|1
-condition|)
-name|warning
-argument_list|(
-literal|"can't get %s for controlling terminal: %m"
-argument_list|,
-name|_PATH_CONSOLE
+literal|0
 argument_list|)
 expr_stmt|;
 operator|(
@@ -6448,6 +6426,17 @@ argument_list|(
 name|fd
 argument_list|,
 literal|2
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|fd
+operator|>
+literal|2
+condition|)
+name|close
+argument_list|(
+name|fd
 argument_list|)
 expr_stmt|;
 block|}
@@ -6489,6 +6478,16 @@ operator|)
 literal|0
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|LOGIN_CAP
+name|setprocresources
+argument_list|(
+name|RESOURCE_RC
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 name|execv
 argument_list|(
 name|_PATH_BSHELL
