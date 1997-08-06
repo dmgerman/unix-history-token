22e6cf32c6d31d747f48da059311c13f49831023
Peter Wemm <peter@FreeBSD.org>
1997-08-06 11:08:01 +0000

    printf does not understand %hd in the kernel
    Former-commit-id: 6d32fba4262b2ca2eb1347f5fe182b07e0f3c3f3

--- sys/i386/i386/bios.c
+++ sys/i386/i386/bios.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Michael Smith  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: bios.c,v 1.2 1997/08/04 03:29:05 msmith Exp $  */
+comment|/*-  * Copyright (c) 1997 Michael Smith  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: bios.c,v 1.3 1997/08/05 01:38:19 msmith Exp $  */
 end_comment
 
 begin_comment
@@ -478,20 +478,29 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"Table at 0x%x, %hd entries, %hd bytes, largest entry %hd bytes\n"
+literal|"Table at 0x%x, %d entries, %d bytes, largest entry %d bytes\n"
 argument_list|,
 name|dmit
 operator|->
 name|st_base
 argument_list|,
+operator|(
+name|int
+operator|)
 name|dmit
 operator|->
 name|st_entries
 argument_list|,
+operator|(
+name|int
+operator|)
 name|dmit
 operator|->
 name|st_size
 argument_list|,
+operator|(
+name|int
+operator|)
 name|sbt
 operator|->
 name|st_maxsize
@@ -626,16 +635,22 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"Table at 0x%x, %hd entries, %hd bytes\n"
+literal|"Table at 0x%x, %d entries, %d bytes\n"
 argument_list|,
 name|dmit
 operator|->
 name|st_base
 argument_list|,
+operator|(
+name|int
+operator|)
 name|dmit
 operator|->
 name|st_entries
 argument_list|,
+operator|(
+name|int
+operator|)
 name|dmit
 operator|->
 name|st_size
