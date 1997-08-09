4e7c686111315ca364708c2083def22f4f7c62ac
Julian Elischer <julian@FreeBSD.org>
1997-08-09 01:44:25 +0000

    Teach both disk drivers how to cope with a hardware watchdog while dumping core.. I'm tired of getting 1/2 of a core-dump
    conditional on -DHW_WDOG for now
    this will migrate to 2.2 as that's where I need it.
    
    
    Former-commit-id: fdd3fdf314df036a8f4e27929a500ec93d3bbf9b

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.105 1997/05/01 19:15:38 sos Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.106 1997/06/25 19:07:43 tegge Exp $  */
 end_comment
 
 begin_include
@@ -4420,6 +4420,7 @@ operator|)
 return|;
 comment|/* we said not to sleep! */
 block|}
+comment|/* 		 * If we are dumping core, it may take a while. 		 * So reassure the user and hold off any watchdogs. 		 */
 if|if
 condition|(
 operator|(
@@ -4435,6 +4436,23 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
+ifdef|#
+directive|ifdef
+name|HW_WDOG
+if|if
+condition|(
+name|wdog_tickler
+condition|)
+call|(
+modifier|*
+name|wdog_tickler
+call|)
+argument_list|()
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* HW_WDOG */
 name|printf
 argument_list|(
 literal|"%ld "
@@ -4444,6 +4462,7 @@ operator|/
 literal|2048
 argument_list|)
 expr_stmt|;
+block|}
 comment|/* update block count */
 name|num
 operator|-=
