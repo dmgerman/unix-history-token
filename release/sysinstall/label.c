88a3e3775780c59815fee183967cfec816f68d61
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-08-11 13:15:25 +0000

    YAMFC
    Former-commit-id: 02b9ff9f45dbdf503a5c1f311c4778e2aafd1c87

--- release/sysinstall/label.c
+++ release/sysinstall/label.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: label.c,v 1.63.2.5 1997/03/11 17:48:39 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: label.c,v 1.63.2.6 1997/06/06 13:01:05 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -4920,6 +4920,38 @@ operator|==
 name|PART_SLICE
 condition|)
 block|{
+name|char
+name|name
+index|[
+literal|512
+index|]
+decl_stmt|;
+name|int
+name|entries
+init|=
+literal|1
+decl_stmt|;
+while|while
+condition|(
+name|entries
+condition|)
+block|{
+name|snprintf
+argument_list|(
+name|name
+argument_list|,
+sizeof|sizeof
+name|name
+argument_list|,
+literal|"%s-%d"
+argument_list|,
+name|c1
+operator|->
+name|name
+argument_list|,
+name|entries
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|(
@@ -4927,8 +4959,6 @@ name|cp
 operator|=
 name|variable_get
 argument_list|(
-name|c1
-operator|->
 name|name
 argument_list|)
 operator|)
@@ -5142,11 +5172,23 @@ name|DITEM_SUCCESS
 expr_stmt|;
 block|}
 block|}
+name|entries
+operator|++
+expr_stmt|;
+block|}
+else|else
+block|{
+comment|/* No more matches, leave the loop */
+name|entries
+operator|=
+literal|0
+expr_stmt|;
+block|}
 block|}
 block|}
 else|else
 block|{
-comment|/* Must be something we can set a mountpoint */
+comment|/* Must be something we can set a mountpoint for */
 name|cp
 operator|=
 name|variable_get
@@ -5167,7 +5209,7 @@ index|[
 literal|50
 index|]
 decl_stmt|,
-name|nwfs
+name|do_newfs
 index|[
 literal|8
 index|]
@@ -5177,7 +5219,7 @@ name|newfs
 init|=
 name|FALSE
 decl_stmt|;
-name|nwfs
+name|do_newfs
 index|[
 literal|0
 index|]
@@ -5194,7 +5236,7 @@ literal|"%s %s"
 argument_list|,
 name|mpoint
 argument_list|,
-name|nwfs
+name|do_newfs
 argument_list|)
 operator|!=
 literal|2
@@ -5224,7 +5266,7 @@ name|newfs
 operator|=
 name|toupper
 argument_list|(
-name|nwfs
+name|do_newfs
 index|[
 literal|0
 index|]
