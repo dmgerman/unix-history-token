1e34777fd9df0a335844fef5cd845119e622fc65
Julian Elischer <julian@FreeBSD.org>
1997-08-11 02:04:14 +0000

    YAMCF: 	sd.c 1.106->1.107 	wd.c 1.134->1.135 	kern_shutdown.c 1.17->1.19 	sys/systm.h 1.54->1.55
    Obtained from: Whistle production build
    teach disk drives how to hold off a hardware watchdog while they
    are busy saving a core-dump. 1/2 a core dump is REALLY annoying..
    Conditional on HW_WDOG. (I should add this to LINT too I guess)
    
    
    Former-commit-id: 097f1fb28885fd93ce917fbcd5b8ff496a518060

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.95.2.1 1997/01/29 14:20:16 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.95.2.2 1997/02/05 19:02:22 kato Exp $  */
 end_comment
 
 begin_include
@@ -4169,6 +4169,7 @@ operator|)
 return|;
 comment|/* we said not to sleep! */
 block|}
+comment|/* 		 * If we are dumping core, it may take a while. 		 * So reassure the user and hold off any watchdogs. 		 */
 if|if
 condition|(
 operator|(
@@ -4184,6 +4185,23 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
+ifdef|#
+directive|ifdef
+name|HW_WDOG
+if|if
+condition|(
+name|wdog_tickler
+condition|)
+call|(
+modifier|*
+name|wdog_tickler
+call|)
+argument_list|()
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* HW_WDOG */
 name|printf
 argument_list|(
 literal|"%ld "
@@ -4193,6 +4211,7 @@ operator|/
 literal|2048
 argument_list|)
 expr_stmt|;
+block|}
 comment|/* update block count */
 name|num
 operator|-=
