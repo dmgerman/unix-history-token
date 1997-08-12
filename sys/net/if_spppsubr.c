64c0e5b547120fb4cd0f19a3665586560a2941c0
Kenjiro Cho <kjc@FreeBSD.org>
1997-08-12 05:22:54 +0000

    Fix a traceroute problem in the CISCO HDLC mode. (cisco routers not returning ICMP_TIMXCEED)
    use CISCO_UNICAST instead of CISCO_MULTICAST to send normal packets.
    this is needed for packets to get processed by a cisco router,
    but doesn't matter if a packet is just forwarded.
    
    Reviewed by:itojun@itojun.org
    
    
    Former-commit-id: f2b5181122eb004c1d87506eb805cf296d9da3c9

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994 Cronyx Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 1.9, Wed Oct  4 18:58:15 MSK 1995  *  * $Id: if_spppsubr.c,v 1.23 1997/08/06 01:43:09 itojun Exp $  */
+comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994 Cronyx Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 1.9, Wed Oct  4 18:58:15 MSK 1995  *  * $Id: if_spppsubr.c,v 1.24 1997/08/10 14:28:16 joerg Exp $  */
 end_comment
 
 begin_include
@@ -3160,9 +3160,9 @@ name|h
 operator|->
 name|address
 operator|=
-name|CISCO_MULTICAST
+name|CISCO_UNICAST
 expr_stmt|;
-comment|/* broadcast address */
+comment|/* unicast address */
 name|h
 operator|->
 name|control
