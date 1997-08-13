eef8550bd3945ceb83225fd2c9b80e4778fe1a8b
Philippe Charnier <charnier@FreeBSD.org>
1997-08-13 06:46:57 +0000

    Remove trailing \n in warn() string.
    Former-commit-id: 73ad7e062c32333491a5b8ef747017c80e4c8c58

--- usr.bin/tail/reverse.c
+++ usr.bin/tail/reverse.c
@@ -9,13 +9,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)reverse.c	8.1 (Berkeley) 6/6/93";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)reverse.c	8.1 (Berkeley) 6/6/93"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
@@ -760,7 +773,7 @@ condition|)
 block|{
 name|warnx
 argument_list|(
-literal|"warning: %ld bytes discarded\n"
+literal|"warning: %ld bytes discarded"
 argument_list|,
 name|enomem
 argument_list|)
