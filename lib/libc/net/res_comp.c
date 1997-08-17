a9a0ebe33efcef4c6287820712305721a48386e8
Joerg Wunsch <joerg@FreeBSD.org>
1997-08-17 15:04:56 +0000

    MFC: rev 1.11, check for MAXHOSTNAMELEN
    Former-commit-id: 422004cfcc10f0864519617e6399850d3876fdf1

--- lib/libc/net/res_comp.c
+++ lib/libc/net/res_comp.c
@@ -44,7 +44,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: res_comp.c,v 1.12 1997/06/27 08:22:02 peter Exp $"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
@@ -228,6 +228,20 @@ expr_stmt|;
 name|cp
 operator|=
 name|comp_dn
+expr_stmt|;
+if|if
+condition|(
+name|length
+operator|>
+name|MAXHOSTNAMELEN
+operator|-
+literal|1
+condition|)
+name|length
+operator|=
+name|MAXHOSTNAMELEN
+operator|-
+literal|1
 expr_stmt|;
 name|eom
 operator|=
