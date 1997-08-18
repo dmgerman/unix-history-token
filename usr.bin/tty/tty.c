fb1c356f19ea657aa3f43f161628c84c8905725c
Philippe Charnier <charnier@FreeBSD.org>
1997-08-18 07:30:15 +0000

    Add usage().
    Former-commit-id: 8aeb41dc9178f56dba080c6b5febf59a34fc08c3

--- usr.bin/tty/tty.c
+++ usr.bin/tty/tty.c
@@ -11,6 +11,7 @@ end_ifndef
 
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
 name|copyright
 index|[]
@@ -34,13 +35,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)tty.c	8.1 (Berkeley) 6/6/93";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)tty.c	8.1 (Berkeley) 6/6/93"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
@@ -59,7 +73,27 @@ directive|include
 file|<stdio.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<unistd.h>
+end_include
+
+begin_decl_stmt
+specifier|static
+name|void
+name|usage
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_function
+name|int
 name|main
 parameter_list|(
 name|argc
@@ -130,17 +164,8 @@ case|case
 literal|'?'
 case|:
 default|default:
-name|fputs
-argument_list|(
-literal|"usage: tty [-s]\n"
-argument_list|,
-name|stderr
-argument_list|)
-expr_stmt|;
-name|exit
-argument_list|(
-literal|2
-argument_list|)
+name|usage
+argument_list|()
 expr_stmt|;
 block|}
 name|t
@@ -171,6 +196,27 @@ condition|?
 literal|0
 else|:
 literal|1
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
+begin_function
+specifier|static
+name|void
+name|usage
+parameter_list|()
+block|{
+name|fprintf
+argument_list|(
+name|stderr
+argument_list|,
+literal|"usage: tty [-s]\n"
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+literal|2
 argument_list|)
 expr_stmt|;
 block|}
