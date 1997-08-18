db3df172b57c7bd29f4cff77e2f71f69ec92439d
Takenori KATO <kato@FreeBSD.org>
1997-08-18 11:30:05 +0000

    Synchronize with sys/i386/i386/machdep.c, trapc and sys/i386/isa/npx.c revisions 1.256, 1.105 and 1.51, respectively.
    Former-commit-id: e2d4b30f93c9d2a3025030e934942206b0205f5e

--- sys/pc98/i386/trap.c
+++ sys/pc98/i386/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 1994, David Greenman  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the University of Utah, and William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)trap.c	7.4 (Berkeley) 5/13/91  *	$Id: trap.c,v 1.28 1997/08/10 10:53:49 kato Exp $  */
+comment|/*-  * Copyright (C) 1994, David Greenman  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the University of Utah, and William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)trap.c	7.4 (Berkeley) 5/13/91  *	$Id: trap.c,v 1.29 1997/08/13 11:00:09 kato Exp $  */
 end_comment
 
 begin_comment
@@ -775,16 +775,12 @@ directive|endif
 comment|/* CPU_BUGGY_CYRIX */
 if|if
 condition|(
-operator|(
-name|ISPL
+name|CS_SECURE
 argument_list|(
 name|frame
 operator|.
 name|tf_cs
 argument_list|)
-operator|==
-name|SEL_UPL
-operator|)
 operator|||
 operator|(
 name|frame
@@ -2583,14 +2579,12 @@ name|PSL_VM
 condition|?
 literal|"vm86"
 else|:
-name|ISPL
+name|CS_SECURE
 argument_list|(
 name|frame
 operator|->
 name|tf_cs
 argument_list|)
-operator|==
-name|SEL_UPL
 condition|?
 literal|"user"
 else|:
@@ -2670,16 +2664,12 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-operator|(
-name|ISPL
+name|CS_SECURE
 argument_list|(
 name|frame
 operator|->
 name|tf_cs
 argument_list|)
-operator|==
-name|SEL_UPL
-operator|)
 operator|||
 operator|(
 name|frame
@@ -3313,14 +3303,13 @@ name|p_sticks
 expr_stmt|;
 if|if
 condition|(
-name|ISPL
+operator|!
+name|CS_SECURE
 argument_list|(
 name|frame
 operator|.
 name|tf_cs
 argument_list|)
-operator|!=
-name|SEL_UPL
 condition|)
 name|panic
 argument_list|(
