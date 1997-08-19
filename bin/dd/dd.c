c617ccb63c60e654aa3fcfc0e469877a5e90e0fc
Jonathan Lemon <jlemon@FreeBSD.org>
1997-08-19 19:46:18 +0000

    Pad the input buffer whenever sync is used, not just if the noerror flag     is also set. Change osync to not to tack on an empty block if the input buffer is null,     or an even multiple of the blocksize. Also change osync to pad the output with nulls/spaces depending whether    this is a block-oriented conversion or not (same as sync).
    PR:		3818
    
    Former-commit-id: ba99463cb5012a2772dba82d979480e268ad7f8e

--- bin/dd/dd.c
+++ bin/dd/dd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego and Lance  * Visser of Convex Computer Corporation.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Keith Muller of the University of California, San Diego and Lance  * Visser of Convex Computer Corporation.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: dd.c,v 1.10 1997/02/22 14:02:44 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -1206,15 +1206,10 @@ name|dd_in
 parameter_list|()
 block|{
 name|int
-name|flags
-decl_stmt|,
 name|n
 decl_stmt|;
 for|for
 control|(
-name|flags
-operator|=
-name|ddflags
 init|;
 condition|;
 control|)
@@ -1236,28 +1231,16 @@ operator|>=
 name|cpy_cnt
 condition|)
 return|return;
-comment|/* 		 * Zero the buffer first if trying to recover from errors so 		 * lose the minimum amount of data.  If doing block operations 		 * use spaces. 		 */
+comment|/* 		 * Zero the buffer first if sync; If doing block operations 		 * use spaces. 		 */
 if|if
 condition|(
-operator|(
-name|flags
+name|ddflags
 operator|&
-operator|(
-name|C_NOERROR
-operator||
 name|C_SYNC
-operator|)
-operator|)
-operator|==
-operator|(
-name|C_NOERROR
-operator||
-name|C_SYNC
-operator|)
 condition|)
 if|if
 condition|(
-name|flags
+name|ddflags
 operator|&
 operator|(
 name|C_BLOCK
@@ -1337,7 +1320,7 @@ if|if
 condition|(
 operator|!
 operator|(
-name|flags
+name|ddflags
 operator|&
 name|C_NOERROR
 operator|)
@@ -1640,12 +1623,44 @@ operator|&&
 name|out
 operator|.
 name|dbcnt
+operator|&&
+name|out
+operator|.
+name|dbcnt
 operator|<
 name|out
 operator|.
 name|dbsz
 condition|)
 block|{
+if|if
+condition|(
+name|ddflags
+operator|&
+operator|(
+name|C_BLOCK
+operator||
+name|C_UNBLOCK
+operator|)
+condition|)
+name|memset
+argument_list|(
+name|out
+operator|.
+name|dbp
+argument_list|,
+literal|' '
+argument_list|,
+name|out
+operator|.
+name|dbsz
+operator|-
+name|out
+operator|.
+name|dbcnt
+argument_list|)
+expr_stmt|;
+else|else
 name|memset
 argument_list|(
 name|out
