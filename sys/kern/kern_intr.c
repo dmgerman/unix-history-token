63b8f6b7066a0e7efd0619732a0c43e13413846f
Steve Passe <fsmp@FreeBSD.org>
1997-08-21 06:39:41 +0000

    Added a half dozen casts to eliminate annoying warnings.
    Former-commit-id: 65fad553f089cdf316126fd6c2d04affc5a4ecd8

--- sys/kern/kern_intr.c
+++ sys/kern/kern_intr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, Stefan Esser<se@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: kern_intr.c,v 1.9 1997/08/02 14:31:27 bde Exp $  *  */
+comment|/*  * Copyright (c) 1997, Stefan Esser<se@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: kern_intr.c,v 1.4 1997/08/21 06:36:02 smp Exp smp $  *  */
 end_comment
 
 begin_include
@@ -202,6 +202,9 @@ name|p
 operator|->
 name|handler
 argument_list|(
+operator|(
+name|int
+operator|)
 name|p
 operator|->
 name|argument
@@ -603,6 +606,9 @@ name|update_intrname
 argument_list|(
 name|irq
 argument_list|,
+operator|(
+name|int
+operator|)
 name|idesc
 operator|->
 name|devdata
@@ -682,6 +688,10 @@ name|icu_setup
 argument_list|(
 name|irq
 argument_list|,
+operator|(
+name|inthand2_t
+operator|*
+operator|)
 name|intr_mux
 argument_list|,
 name|head
@@ -1003,6 +1013,10 @@ name|icu_unset
 argument_list|(
 name|irq
 argument_list|,
+operator|(
+name|inthand2_t
+operator|*
+operator|)
 name|intr_mux
 argument_list|)
 expr_stmt|;
@@ -1037,6 +1051,10 @@ name|icu_setup
 argument_list|(
 name|irq
 argument_list|,
+operator|(
+name|inthand2_t
+operator|*
+operator|)
 name|intr_mux
 argument_list|,
 name|head
@@ -1097,6 +1115,9 @@ name|update_intrname
 argument_list|(
 name|irq
 argument_list|,
+operator|(
+name|int
+operator|)
 name|head
 operator|->
 name|devdata
