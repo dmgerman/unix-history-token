d64aaf2f14b8a0a501506096ccb442d24290a607
Brian Somers <brian@FreeBSD.org>
1997-08-21 17:08:44 +0000

    YAMFC: Allow the use of HISADDR & MYADDR in ppp.conf.
    Former-commit-id: 5e3a8dfe80cdff7a8e55b9edd0e93b26f64f807f

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.29 1997/08/14 01:49:04 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.30 1997/08/21 01:21:13 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -5354,6 +5354,30 @@ operator|=
 literal|0
 expr_stmt|;
 block|}
+name|IpcpInfo
+operator|.
+name|want_ipaddr
+operator|.
+name|s_addr
+operator|=
+name|DefMyAddress
+operator|.
+name|ipaddr
+operator|.
+name|s_addr
+expr_stmt|;
+name|IpcpInfo
+operator|.
+name|his_ipaddr
+operator|.
+name|s_addr
+operator|=
+name|DefHisAddress
+operator|.
+name|ipaddr
+operator|.
+name|s_addr
+expr_stmt|;
 if|if
 condition|(
 operator|(
