31f6eb0a21eb45f78fea5ac4437ef27e7863e018
Jonathan Lemon <jlemon@FreeBSD.org>
1997-08-21 22:47:42 +0000

    MFC: Bugfixes from rev 1.20; -p/-u conflict, release of resources.
    Former-commit-id: 19b5bcb898e2ea61e2796b45922b37e14c63d0e7

--- sbin/modload/modload.c
+++ sbin/modload/modload.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Terrence R. Lambert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY TERRENCE R. LAMBERT ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: modload.c,v 1.13 1996/08/13 00:50:59 pst Exp $  */
+comment|/*  * Copyright (c) 1993 Terrence R. Lambert.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY TERRENCE R. LAMBERT ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
 end_comment
 
 begin_include
@@ -551,6 +551,10 @@ name|close
 argument_list|(
 name|modfd
 argument_list|)
+expr_stmt|;
+name|fileopen
+operator|=
+literal|0
 expr_stmt|;
 block|}
 end_function
@@ -1403,6 +1407,24 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|dounlink
+operator|&&
+name|unlink
+argument_list|(
+name|out
+argument_list|)
+condition|)
+name|err
+argument_list|(
+literal|17
+argument_list|,
+literal|"unlink(%s)"
+argument_list|,
+name|out
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
 name|post
 condition|)
 block|{
@@ -1490,6 +1512,10 @@ name|offset
 argument_list|)
 expr_stmt|;
 comment|/* XXX the modload docs say that drivers can install bdevsw& 	       cdevsw, but the interface only supports one at a time.  sigh. */
+comment|/* Free resources before calling post-install program */
+name|cleanup
+argument_list|()
+expr_stmt|;
 name|execl
 argument_list|(
 name|post
@@ -1514,30 +1540,6 @@ argument_list|,
 name|post
 argument_list|)
 expr_stmt|;
-block|}
-if|if
-condition|(
-name|dounlink
-condition|)
-block|{
-if|if
-condition|(
-name|unlink
-argument_list|(
-name|out
-argument_list|)
-condition|)
-block|{
-name|err
-argument_list|(
-literal|17
-argument_list|,
-literal|"unlink(%s)"
-argument_list|,
-name|out
-argument_list|)
-expr_stmt|;
-block|}
 block|}
 return|return
 literal|0
