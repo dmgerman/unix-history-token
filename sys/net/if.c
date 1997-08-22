55de7bef4c557b0856abd88dfaf93de15515babc
Julian Elischer <julian@FreeBSD.org>
1997-08-22 22:47:27 +0000

    add some comments while trying to understand why appletalk gets some things wrong. (part of my continuing "comment it as you understand it" effort :)
    Former-commit-id: 619ba253fca540353e895415fd9dfdded3859021

--- sys/net/if.c
+++ sys/net/if.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.c	8.3 (Berkeley) 1/4/94  *	$Id: if.c,v 1.48 1997/05/03 21:07:13 peter Exp $  */
+comment|/*  * Copyright (c) 1980, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if.c	8.3 (Berkeley) 1/4/94  *	$Id: if.c,v 1.49 1997/07/07 17:36:06 julian Exp $  */
 end_comment
 
 begin_include
@@ -1139,6 +1139,7 @@ decl_stmt|,
 modifier|*
 name|cplim
 decl_stmt|;
+comment|/* 	 * AF_LINK addresses can be looked up directly by their index number, 	 * so do that if we can. 	 */
 if|if
 condition|(
 name|af
@@ -1184,6 +1185,7 @@ index|]
 operator|)
 return|;
 block|}
+comment|/*  	 * Scan though each interface, looking for ones that have 	 * addresses in this address family. 	 */
 for|for
 control|(
 name|ifp
@@ -1257,6 +1259,7 @@ operator|&
 name|IFF_POINTOPOINT
 condition|)
 block|{
+comment|/* 				 * This is a bit broken as it doesn't  				 * take into account that the remote end may  				 * be a single node in the network we are 				 * looking for. 				 * The trouble is that we don't know the  				 * netmask for the remote end. 				 */
 if|if
 condition|(
 name|ifa
@@ -1282,6 +1285,7 @@ return|;
 block|}
 else|else
 block|{
+comment|/* 				 * Scan all the bits in the ifa's address. 				 * If a bit dissagrees with what we are 				 * looking for, mask it with the netmask 				 * to see if it really matters. 				 * (A byte at a time) 				 */
 if|if
 condition|(
 name|ifa
@@ -1352,6 +1356,8 @@ condition|)
 goto|goto
 name|next
 goto|;
+comment|/* next address! */
+comment|/* 				 * If the netmask of what we just found 				 * is more specific than what we had before 				 * (if we had one) then remember the new one 				 * before continuing to search 				 * for an even better one. 				 */
 if|if
 condition|(
 name|ifa_maybe
