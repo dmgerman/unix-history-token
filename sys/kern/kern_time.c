b52d2ed6f87214a5580cc653f66aaeb6f733ebfc
Bruce Evans <bde@FreeBSD.org>
1997-08-26 00:40:04 +0000

    Don't return EINVAL for negative timespecs in the nanosleep functions. Negative timespecs are perfectly valid.  Just return 0 immediately for them.  Also, return 0 immediately for zero timespecs.
    Fixed some style bugs.
    
    
    Former-commit-id: 5d8e2fd364476b36e498e7e0655342db40ce2221

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.31 1997/08/13 17:55:11 ache Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.32 1997/08/14 08:15:12 ache Exp $  */
 end_comment
 
 begin_include
@@ -101,6 +101,32 @@ end_comment
 begin_decl_stmt
 specifier|static
 name|int
+name|nanosleep1
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+name|p
+operator|,
+expr|struct
+name|timespec
+operator|*
+name|rqt
+operator|,
+expr|struct
+name|timespec
+operator|*
+name|rmt
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|int
 name|settime
 name|__P
 argument_list|(
@@ -123,32 +149,6 @@ operator|(
 expr|struct
 name|timeval
 operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
-name|int
-name|nanosleep1
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|proc
-operator|*
-name|p
-operator|,
-expr|struct
-name|timespec
-operator|*
-name|rqt
-operator|,
-expr|struct
-name|timespec
-operator|*
-name|rmt
 operator|)
 argument_list|)
 decl_stmt|;
@@ -896,12 +896,6 @@ if|if
 condition|(
 name|rqt
 operator|->
-name|tv_sec
-operator|<
-literal|0
-operator|||
-name|rqt
-operator|->
 name|tv_nsec
 operator|<
 literal|0
@@ -915,6 +909,31 @@ condition|)
 return|return
 operator|(
 name|EINVAL
+operator|)
+return|;
+if|if
+condition|(
+name|rqt
+operator|->
+name|tv_sec
+operator|<
+literal|0
+operator|||
+name|rqt
+operator|->
+name|tv_sec
+operator|==
+literal|0
+operator|&&
+name|rqt
+operator|->
+name|tv_nsec
+operator|==
+literal|0
+condition|)
+return|return
+operator|(
+literal|0
 operator|)
 return|;
 name|TIMESPEC_TO_TIMEVAL
@@ -1561,7 +1580,7 @@ operator|(
 name|error
 operator|)
 return|;
-comment|/* See kern_sig.c:sigsuspend() for explanation */
+comment|/* See kern_sig.c:sigsuspend() for explanation. */
 name|ps
 operator|->
 name|ps_oldmask
@@ -1598,6 +1617,7 @@ operator|&
 name|rmt
 argument_list|)
 expr_stmt|;
+comment|/* See kern_sig.c:sigsuspend() again. */
 name|p
 operator|->
 name|p_sigmask
