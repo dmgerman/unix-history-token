1aa6130b41999dfbacf48af5de70962d7d26f35a
Peter Wemm <peter@FreeBSD.org>
1997-08-26 18:36:15 +0000

    Correct some things I forgot about until it was too late with smp_active. smp_active = 1 used to indicate that the system had frozen previously started AP's, while smp_active = 0 was "AP's not yet started".  I have split this into smp_started (which is set when the AP's come online), and smp_active is left for turning on/off AP scheduling.
    Former-commit-id: 10f90a600c27c1a0f8e891b55be47361209565b6

--- sys/i386/i386/mptable.c
+++ sys/i386/i386/mptable.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.45 1997/08/25 21:28:08 bde Exp $  */
+comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.46 1997/08/26 18:10:31 peter Exp $  */
 end_comment
 
 begin_include
@@ -959,6 +959,17 @@ modifier|*
 name|my_idlePTD
 decl_stmt|;
 end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|int
+name|smp_started
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* has the system started? */
+end_comment
 
 begin_comment
 comment|/*  * Local data and functions.  */
@@ -7090,7 +7101,7 @@ name|APIC_IO
 argument_list|)
 if|if
 condition|(
-name|smp_active
+name|smp_started
 operator|&&
 name|invltlb_ok
 condition|)
@@ -7143,7 +7154,7 @@ block|{
 if|if
 condition|(
 operator|!
-name|smp_active
+name|smp_started
 condition|)
 return|return
 literal|0
@@ -7186,7 +7197,7 @@ block|{
 if|if
 condition|(
 operator|!
-name|smp_active
+name|smp_started
 condition|)
 return|return
 literal|0
@@ -7425,6 +7436,11 @@ name|invltlb_ok
 operator|=
 literal|1
 expr_stmt|;
+name|smp_started
+operator|=
+literal|1
+expr_stmt|;
+comment|/* enable IPI's, tlb shootdown, freezes etc */
 name|smp_active
 operator|=
 literal|1
