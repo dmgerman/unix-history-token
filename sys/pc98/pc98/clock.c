eb4f031dddd086b3c04afba646f04eb1ed0d8d98
Takenori KATO <kato@FreeBSD.org>
1997-09-01 10:44:06 +0000

    Synchronize with sys/i386/isa/clock.c revision 1.102.
    Former-commit-id: f5f85879c191c37e113ddbed3bcab4b05d31888f

--- sys/pc98/pc98/clock.c
+++ sys/pc98/pc98/clock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.33 1997/08/22 08:12:14 kato Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.34 1997/08/30 15:47:46 kato Exp $  */
 end_comment
 
 begin_comment
@@ -209,116 +209,20 @@ directive|ifdef
 name|SMP
 end_ifdef
 
-begin_include
-include|#
-directive|include
-file|<machine/smptests.h>
-end_include
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|SIMPLE_MPINTRLOCK
-end_ifdef
-
 begin_define
 define|#
 directive|define
-name|DISABLE_INTR
+name|disable_intr
 parameter_list|()
-define|\
-value|__asm __volatile("cli" : : : "memory");		\  	s_lock(&clock_lock);
+value|CLOCK_DISABLE_INTR()
 end_define
 
 begin_define
 define|#
 directive|define
-name|ENABLE_INTR
+name|enable_intr
 parameter_list|()
-define|\
-value|s_unlock(&clock_lock);				\ 	__asm __volatile("sti");
-end_define
-
-begin_define
-define|#
-directive|define
-name|CLOCK_UNLOCK
-parameter_list|()
-define|\
-value|s_unlock(&clock_lock);
-end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_comment
-comment|/* SIMPLE_MPINTRLOCK */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DISABLE_INTR
-parameter_list|()
-value|disable_intr()
-end_define
-
-begin_define
-define|#
-directive|define
-name|ENABLE_INTR
-parameter_list|()
-value|enable_intr()
-end_define
-
-begin_define
-define|#
-directive|define
-name|CLOCK_UNLOCK
-parameter_list|()
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* SIMPLE_MPINTRLOCK */
-end_comment
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_comment
-comment|/* SMP */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DISABLE_INTR
-parameter_list|()
-value|disable_intr()
-end_define
-
-begin_define
-define|#
-directive|define
-name|ENABLE_INTR
-parameter_list|()
-value|enable_intr()
-end_define
-
-begin_define
-define|#
-directive|define
-name|CLOCK_UNLOCK
-parameter_list|()
+value|CLOCK_ENABLE_INTR()
 end_define
 
 begin_endif
@@ -1046,7 +950,7 @@ name|timer0_max_count
 operator|-
 name|TIMER0_LATCH_COUNT
 expr_stmt|;
-name|DISABLE_INTR
+name|disable_intr
 argument_list|()
 expr_stmt|;
 name|outb
@@ -1078,7 +982,7 @@ operator|>>
 literal|8
 argument_list|)
 expr_stmt|;
-name|ENABLE_INTR
+name|enable_intr
 argument_list|()
 expr_stmt|;
 name|timer0_prescaler_count
@@ -1127,7 +1031,7 @@ name|timer0_max_count
 operator|-
 name|TIMER0_LATCH_COUNT
 expr_stmt|;
-name|DISABLE_INTR
+name|disable_intr
 argument_list|()
 expr_stmt|;
 name|outb
@@ -1159,7 +1063,7 @@ operator|>>
 literal|8
 argument_list|)
 expr_stmt|;
-name|ENABLE_INTR
+name|enable_intr
 argument_list|()
 expr_stmt|;
 comment|/* 			 * See microtime.s for this magic. 			 */
@@ -1817,7 +1721,7 @@ operator|=
 name|read_eflags
 argument_list|()
 expr_stmt|;
-name|DISABLE_INTR
+name|disable_intr
 argument_list|()
 expr_stmt|;
 comment|/* Select timer0 and latch counter value. */
@@ -2327,7 +2231,7 @@ operator|)
 return|;
 comment|/* XXX Should be EBUSY, but nobody cares anyway. */
 block|}
-name|DISABLE_INTR
+name|disable_intr
 argument_list|()
 expr_stmt|;
 name|outb
@@ -2348,7 +2252,7 @@ literal|8
 operator|)
 argument_list|)
 expr_stmt|;
-name|ENABLE_INTR
+name|enable_intr
 argument_list|()
 expr_stmt|;
 if|if
@@ -3046,7 +2950,7 @@ operator|=
 name|read_eflags
 argument_list|()
 expr_stmt|;
-name|DISABLE_INTR
+name|disable_intr
 argument_list|()
 expr_stmt|;
 name|timer_freq
@@ -5506,7 +5410,7 @@ operator|=
 name|read_eflags
 argument_list|()
 expr_stmt|;
-name|DISABLE_INTR
+name|disable_intr
 argument_list|()
 expr_stmt|;
 name|i586_ctr_freq
