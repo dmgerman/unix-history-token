5b96a91d9523cd75a1ed05e5e5b470987bb6ce68
David Greenman <dg@FreeBSD.org>
1997-09-04 15:23:33 +0000

    Cosmetic change to last commit: speculative_mtest -> speculative_mprobe.
    Former-commit-id: de89b32294caaabaa82a5c6c30a77204a266af03

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.261 1997/09/02 20:05:28 bde Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.262 1997/09/04 15:14:48 davidg Exp $  */
 end_comment
 
 begin_include
@@ -4687,7 +4687,7 @@ name|int
 name|off
 decl_stmt|;
 name|int
-name|speculative_mtest
+name|speculative_mprobe
 decl_stmt|;
 name|proc0
 operator|.
@@ -5783,19 +5783,19 @@ literal|0x100000
 operator|/
 name|PAGE_SIZE
 expr_stmt|;
-comment|/* 	 * Indicate that we wish to do a speculative search for memory beyond 	 * the end of the reported size if the indicated amount is 64MB (0x4000 	 * pages) - which is the largest amount that the BIOS/bootblocks can 	 * currently report. If a specific amount of memory is indicated via 	 * the MAXMEM option or the npx0 "msize", then don't do the speculative 	 * memory test. 	 */
+comment|/* 	 * Indicate that we wish to do a speculative search for memory beyond 	 * the end of the reported size if the indicated amount is 64MB (0x4000 	 * pages) - which is the largest amount that the BIOS/bootblocks can 	 * currently report. If a specific amount of memory is indicated via 	 * the MAXMEM option or the npx0 "msize", then don't do the speculative 	 * memory probe. 	 */
 if|if
 condition|(
 name|Maxmem
 operator|==
 literal|0x4000
 condition|)
-name|speculative_mtest
+name|speculative_mprobe
 operator|=
 name|TRUE
 expr_stmt|;
 else|else
-name|speculative_mtest
+name|speculative_mprobe
 operator|=
 name|FALSE
 expr_stmt|;
@@ -5808,7 +5808,7 @@ name|MAXMEM
 operator|/
 literal|4
 expr_stmt|;
-name|speculative_mtest
+name|speculative_mprobe
 operator|=
 name|FALSE
 expr_stmt|;
@@ -5852,7 +5852,7 @@ name|id_msize
 operator|/
 literal|4
 expr_stmt|;
-name|speculative_mtest
+name|speculative_mprobe
 operator|=
 name|FALSE
 expr_stmt|;
@@ -6133,7 +6133,7 @@ name|PAGE_SIZE
 expr_stmt|;
 if|if
 condition|(
-name|speculative_mtest
+name|speculative_mprobe
 operator|==
 name|TRUE
 operator|&&
