8c6ce40db20cde77f7dd89643485ef3d6eacdeee
David Greenman <dg@FreeBSD.org>
1997-09-05 10:23:58 +0000

    Changes to support NetBSD and the new ifmedia extensions. Submitted by:	Jason Thorpe <thorpej@netbsd.org>
    Former-commit-id: c81372f69c69a169d81a037fb73381313c4906d5

--- sys/dev/fxp/if_fxpreg.h
+++ sys/dev/fxp/if_fxpreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: if_fxpreg.h,v 1.8 1997/03/21 08:00:13 davidg Exp $  */
+comment|/*  * Copyright (c) 1995, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
 end_comment
 
 begin_define
@@ -31,58 +31,112 @@ name|FXP_PCI_IOBA
 value|0x14
 end_define
 
-begin_struct
-struct|struct
-name|fxp_csr
-block|{
-specifier|volatile
-name|u_int8_t
-operator|:
-literal|2
-operator|,
-name|scb_rus
-operator|:
-literal|4
-operator|,
-name|scb_cus
-operator|:
-literal|2
-expr_stmt|;
-specifier|volatile
-name|u_int8_t
-name|scb_statack
-decl_stmt|;
-specifier|volatile
-name|u_int8_t
-name|scb_command
-decl_stmt|;
-specifier|volatile
-name|u_int8_t
-name|scb_intrcntl
-decl_stmt|;
-specifier|volatile
-name|u_int32_t
-name|scb_general
-decl_stmt|;
-specifier|volatile
-name|u_int32_t
-name|port
-decl_stmt|;
-specifier|volatile
-name|u_int16_t
-name|flash_control
-decl_stmt|;
-specifier|volatile
-name|u_int16_t
-name|eeprom_control
-decl_stmt|;
-specifier|volatile
-name|u_int32_t
-name|mdi_control
-decl_stmt|;
-block|}
-struct|;
-end_struct
+begin_comment
+comment|/*  * Control/status registers.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_SCB_RUSCUS
+value|0
+end_define
+
+begin_comment
+comment|/* scb_rus/scb_cus (1 byte) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_SCB_STATACK
+value|1
+end_define
+
+begin_comment
+comment|/* scb_statack (1 byte) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_SCB_COMMAND
+value|2
+end_define
+
+begin_comment
+comment|/* scb_command (1 byte) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_SCB_INTRCNTL
+value|3
+end_define
+
+begin_comment
+comment|/* scb_intrcntl (1 byte) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_SCB_GENERAL
+value|4
+end_define
+
+begin_comment
+comment|/* scb_general (4 bytes) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_PORT
+value|8
+end_define
+
+begin_comment
+comment|/* port (4 bytes) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_FLASHCONTROL
+value|12
+end_define
+
+begin_comment
+comment|/* flash control (2 bytes) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_EEPROMCONTROL
+value|14
+end_define
+
+begin_comment
+comment|/* eeprom control (2 bytes) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FXP_CSR_MDICONTROL
+value|16
+end_define
+
+begin_comment
+comment|/* mdi control (4 bytes) */
+end_comment
+
+begin_comment
+comment|/*  * FOR REFERENCE ONLY, the old definition of FXP_CSR_SCB_RUSCUS:  *  *	volatile u_int8_t	:2,  *				scb_rus:4,  *				scb_cus:2;  */
+end_comment
 
 begin_define
 define|#
