fe49c5d17895b67582d434556ed829209d794b67
Joerg Wunsch <joerg@FreeBSD.org>
1997-09-07 10:08:23 +0000

    Silence the PREVENT ALLOW MEDIUM REMOVAL call.
    Submitted by:	jmattson@wco.com (Jim Mattson)
    
    
    Former-commit-id: 13284882c865c4440373976aa07cb492200c50dd

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.79 1997/08/23 21:05:32 steve Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.80 1997/09/02 20:06:38 bde Exp $  */
 end_comment
 
 begin_comment
@@ -2149,16 +2149,18 @@ name|flags
 operator||=
 name|ST_WRITTEN
 expr_stmt|;
+comment|/* PREVENT ALLOW MEDIUM REMOVAL is optional per the SCSI-2 specs */
 name|scsi_prevent
 argument_list|(
 name|sc_link
 argument_list|,
 name|PR_PREVENT
 argument_list|,
-literal|0
+name|SCSI_SILENT
+operator||
+name|SCSI_ERR_OK
 argument_list|)
 expr_stmt|;
-comment|/* who cares if it fails? */
 name|SC_DEBUG
 argument_list|(
 name|sc_link
