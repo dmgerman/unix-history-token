e553f3ac4f4241163951cf64cad2eb9721d5f369
Joerg Wunsch <joerg@FreeBSD.org>
1997-09-07 15:06:08 +0000

    Add a quirk flag for CD drives that jam when seeing a START STOP UNIT command.
    PR:		2388
    Submitted by:	nsayer@quack.kfu.com (Nick Sayer) [basically]
    
    Former-commit-id: 5ef81a1dc10ddb2a3776330b8f1810c955b09bbc

--- sys/scsi/cd.c
+++ sys/scsi/cd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.83 1997/05/04 15:24:22 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *      $Id: cd.c,v 1.84 1997/09/02 20:06:30 bde Exp $  */
 end_comment
 
 begin_include
@@ -1447,7 +1447,20 @@ name|ENXIO
 operator|)
 return|;
 block|}
-comment|/* 	 * Start the drive, and take notice of error returns. 	 */
+comment|/* 	 * Start the drive, and take notice of error returns. 	 * Some (arguably broken) drives go crazy on this attempt, we can 	 * handle it. 	 */
+if|if
+condition|(
+operator|(
+name|sc_link
+operator|->
+name|quirks
+operator|&
+name|CD_Q_NO_START
+operator|)
+operator|==
+literal|0
+condition|)
+block|{
 name|scsi_start_unit
 argument_list|(
 name|sc_link
@@ -1466,6 +1479,7 @@ literal|"'start' attempted "
 operator|)
 argument_list|)
 expr_stmt|;
+block|}
 name|sc_link
 operator|->
 name|flags
