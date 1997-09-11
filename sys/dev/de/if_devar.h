61b2d598f12294127b27b7e1e4ce10f30e5b660d
Peter Wemm <peter@FreeBSD.org>
1997-09-11 15:27:35 +0000

    malloc() the rx and tx descriptors seperately rather than as part of the large (over 4KB) softc struct.  The descriptor array is accessed by busmaster dma and must be physically contiguous in memory.  malloc() of a block greater than a page is only virtually contiguous, and not necessarily physically contigious.
    contigmalloc() could do this, but that is a bit on the overkill side.
    
    I'm not sure of the origins of the problem report and diagnosis, I learned
    of the problem via mail forwarded from  Jim Shankland <jas@flyingfox.com>.
    
    Jim said that Matt Thomas's workaround was to reduce the number of
    transmit descriptors from 128 to 32, but I was concerned that it might
    cost performance.  Anyway, this change is my fault, not Jim's. :-)
    
    Reviewed by: davidg
    
    
    Former-commit-id: e4a2cace48d196c09b8238d8e30db6e8aefb434f

--- sys/dev/de/if_devar.h
+++ sys/dev/de/if_devar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994-1997 Matt Thomas (matt@3am-software.com)  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: if_devar.h,v 1.28 1997/07/03 16:55:07 thomas Exp $  */
+comment|/*-  * Copyright (c) 1994-1997 Matt Thomas (matt@3am-software.com)  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: if_devar.h,v 1.1.1.2 1997/08/03 12:17:38 peter Exp $  */
 end_comment
 
 begin_if
@@ -2274,6 +2274,22 @@ decl_stmt|;
 name|tulip_srom_connection_t
 name|tulip_conntype
 decl_stmt|;
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__FreeBSD__
+argument_list|)
+name|tulip_desc_t
+modifier|*
+name|tulip_rxdescs
+decl_stmt|;
+name|tulip_desc_t
+modifier|*
+name|tulip_txdescs
+decl_stmt|;
+else|#
+directive|else
 name|tulip_desc_t
 name|tulip_rxdescs
 index|[
@@ -2286,6 +2302,8 @@ index|[
 name|TULIP_TXDESCS
 index|]
 decl_stmt|;
+endif|#
+directive|endif
 block|}
 struct|;
 end_struct
