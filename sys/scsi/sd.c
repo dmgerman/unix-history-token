6cf5fcd407b72b8889c1f3a829513ce5a8c199ef
Joerg Wunsch <joerg@FreeBSD.org>
1997-09-13 16:19:14 +0000

    MFC: introduce a function is_physical_memory(), and use it to exclude the ``ISA memory hole'' from kernel coredumps.
    Former-commit-id: c16a10345151f27bb0d0ca8b7b70cde780591be5

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.95.2.2 1997/02/05 19:02:22 kato Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.95.2.3 1997/08/11 02:04:09 julian Exp $  */
 end_comment
 
 begin_include
@@ -3920,6 +3920,16 @@ operator|>
 literal|0
 condition|)
 block|{
+if|if
+condition|(
+name|is_physical_memory
+argument_list|(
+operator|(
+name|vm_offset_t
+operator|)
+name|addr
+argument_list|)
+condition|)
 name|pmap_enter
 argument_list|(
 name|kernel_pmap
@@ -3932,6 +3942,26 @@ argument_list|,
 name|trunc_page
 argument_list|(
 name|addr
+argument_list|)
+argument_list|,
+name|VM_PROT_READ
+argument_list|,
+name|TRUE
+argument_list|)
+expr_stmt|;
+else|else
+name|pmap_enter
+argument_list|(
+name|kernel_pmap
+argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
+name|CADDR1
+argument_list|,
+name|trunc_page
+argument_list|(
+literal|0
 argument_list|)
 argument_list|,
 name|VM_PROT_READ
