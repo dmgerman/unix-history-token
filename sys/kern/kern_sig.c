32b4b46a4f3d653264e804f47905a8b77213a92a
Joerg Wunsch <joerg@FreeBSD.org>
1997-09-13 19:42:29 +0000

    Implement SA_NOCLDWAIT.
    The implementation is done (unlike what i've originally been
    contemplating) by reparenting kids of processes that have the
    appropriate bit set to PID 1, and let PID 1 handle the zombie.  This
    is far less problematical than what would seem to be ``doing it
    right'', for a number of reasons.
    
    Of our currently shipping PID-1-intended programs, 50 % fail the above
    assumption. ;-)  (Read this: sysinstall doesn't do it right.  This is
    no problem as long as no program called by sysinstall actually uses
    SA_NOCLDWAIT.)
    
    ToDo:		. clarify the correct SA_* flag inheritance, compared
    		  to other systems,
    		. decide whether the compat cruft (osigvec(9)) should
    		  deal with new system additions or not,
    		. merge OpenBSD's SA_SIGINFO implementation. ;)
    Reviewed by:	bde
    
    Former-commit-id: 7a21b93aa093f4083032e760ee2494411819a7f6

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_sig.c	8.7 (Berkeley) 4/18/94  * $Id: kern_sig.c,v 1.32 1997/08/26 00:31:04 bde Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_sig.c	8.7 (Berkeley) 4/18/94  * $Id: kern_sig.c,v 1.33 1997/09/02 20:05:41 bde Exp $  */
 end_comment
 
 begin_include
@@ -507,6 +507,24 @@ name|SA_NOCLDSTOP
 expr_stmt|;
 if|if
 condition|(
+name|signum
+operator|==
+name|SIGCHLD
+operator|&&
+name|p
+operator|->
+name|p_flag
+operator|&
+name|P_NOCLDWAIT
+condition|)
+name|sa
+operator|->
+name|sa_flags
+operator||=
+name|SA_NOCLDWAIT
+expr_stmt|;
+if|if
+condition|(
 operator|(
 name|error
 operator|=
@@ -843,6 +861,47 @@ name|p_flag
 operator|&=
 operator|~
 name|P_NOCLDSTOP
+expr_stmt|;
+if|if
+condition|(
+name|sa
+operator|->
+name|sa_flags
+operator|&
+name|SA_NOCLDWAIT
+condition|)
+block|{
+comment|/* 			 * Paranoia: since SA_NOCLDWAIT is implemented by 			 * reparenting the dying child to PID 1 (and 			 * trust it to reap the zombie), PID 1 itself is 			 * forbidden to set SA_NOCLDWAIT. 			 */
+if|if
+condition|(
+name|p
+operator|->
+name|p_pid
+operator|==
+literal|1
+condition|)
+name|p
+operator|->
+name|p_flag
+operator|&=
+operator|~
+name|P_NOCLDWAIT
+expr_stmt|;
+else|else
+name|p
+operator|->
+name|p_flag
+operator||=
+name|P_NOCLDWAIT
+expr_stmt|;
+block|}
+else|else
+name|p
+operator|->
+name|p_flag
+operator|&=
+operator|~
+name|P_NOCLDWAIT
 expr_stmt|;
 block|}
 comment|/* 	 * Set bit in p_sigignore for signals that are set to SIG_IGN, 	 * and for signals set to SIG_DFL where the default is to ignore. 	 * However, don't put SIGCONT in p_sigignore, 	 * as we have to restart the process. 	 */
