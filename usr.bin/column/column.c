9c8593faa74e64ab5a19685afb7ea1782feba252
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-09-14 23:19:57 +0000

    MFC: doc & cosmetic fixes.
    Former-commit-id: 8ac58af34b19f3a1a23827bf7e6501519a5e16e5

--- usr.bin/column/column.c
+++ usr.bin/column/column.c
@@ -40,7 +40,7 @@ name|char
 name|sccsid
 index|[]
 init|=
-literal|"@(#)column.c	8.3 (Berkeley) 4/2/94"
+literal|"@(#)column.c	8.4 (Berkeley) 5/4/95"
 decl_stmt|;
 end_decl_stmt
 
@@ -99,6 +99,12 @@ begin_include
 include|#
 directive|include
 file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<unistd.h>
 end_include
 
 begin_decl_stmt
