dd150881d124dcafb8dfe491e81adce56fe58630
Peter Wemm <peter@FreeBSD.org>
1997-09-14 02:46:44 +0000

    Extend select hook to support poll
    Former-commit-id: c154a3daeb74bef37da8f750fe1a7561c9b1d9bf

--- sys/kern/subr_log.c
+++ sys/kern/subr_log.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_log.c	8.1 (Berkeley) 6/10/93  * $Id: subr_log.c,v 1.21 1997/03/23 03:36:22 bde Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_log.c	8.1 (Berkeley) 6/10/93  * $Id: subr_log.c,v 1.22 1997/03/24 11:52:25 bde Exp $  */
 end_comment
 
 begin_comment
@@ -71,6 +71,12 @@ begin_include
 include|#
 directive|include
 file|<sys/kernel.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/poll.h>
 end_include
 
 begin_ifdef
@@ -145,8 +151,8 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|static
-name|d_select_t
-name|logselect
+name|d_poll_t
+name|logpoll
 decl_stmt|;
 end_decl_stmt
 
@@ -182,7 +188,7 @@ block|,
 name|nodevtotty
 block|,
 comment|/* klog */
-name|logselect
+name|logpoll
 block|,
 name|nommap
 block|,
@@ -593,11 +599,11 @@ end_comment
 begin_function
 specifier|static
 name|int
-name|logselect
+name|logpoll
 parameter_list|(
 name|dev
 parameter_list|,
-name|rw
+name|events
 parameter_list|,
 name|p
 parameter_list|)
@@ -605,7 +611,7 @@ name|dev_t
 name|dev
 decl_stmt|;
 name|int
-name|rw
+name|events
 decl_stmt|;
 name|struct
 name|proc
@@ -615,18 +621,27 @@ decl_stmt|;
 block|{
 name|int
 name|s
+decl_stmt|;
+name|int
+name|revents
 init|=
+literal|0
+decl_stmt|;
+name|s
+operator|=
 name|splhigh
 argument_list|()
-decl_stmt|;
-switch|switch
+expr_stmt|;
+if|if
 condition|(
-name|rw
+name|events
+operator|&
+operator|(
+name|POLLIN
+operator||
+name|POLLRDNORM
+operator|)
 condition|)
-block|{
-case|case
-name|FREAD
-case|:
 if|if
 condition|(
 name|msgbufp
@@ -637,18 +652,17 @@ name|msgbufp
 operator|->
 name|msg_bufx
 condition|)
-block|{
-name|splx
-argument_list|(
-name|s
-argument_list|)
-expr_stmt|;
-return|return
+name|revents
+operator||=
+name|events
+operator|&
 operator|(
-literal|1
+name|POLLIN
+operator||
+name|POLLRDNORM
 operator|)
-return|;
-block|}
+expr_stmt|;
+else|else
 name|selrecord
 argument_list|(
 name|p
@@ -659,8 +673,6 @@ operator|.
 name|sc_selp
 argument_list|)
 expr_stmt|;
-break|break;
-block|}
 name|splx
 argument_list|(
 name|s
@@ -668,7 +680,7 @@ argument_list|)
 expr_stmt|;
 return|return
 operator|(
-literal|0
+name|revents
 operator|)
 return|;
 block|}
