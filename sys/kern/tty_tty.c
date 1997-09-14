17cc3d8c49cdca9691128fb20c7be28712c5aa75
Peter Wemm <peter@FreeBSD.org>
1997-09-14 02:40:46 +0000

    Extend to use poll backend.  If memory serves correctly, most of this was adapted from NetBSD..  However, there are some differences in the tty system that are big enough to cause their code to not fit comfortably.
    Obtained from:  NetBSD (I think)
    
    
    Former-commit-id: d8e19e41d2ba60ccd1fce20212bbb6ebe9e464d5

--- sys/kern/tty_tty.c
+++ sys/kern/tty_tty.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_tty.c	8.2 (Berkeley) 9/23/93  * $Id: tty_tty.c,v 1.16 1997/03/24 11:24:38 bde Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty_tty.c	8.2 (Berkeley) 9/23/93  * $Id: tty_tty.c,v 1.17 1997/09/02 20:05:56 bde Exp $  */
 end_comment
 
 begin_comment
@@ -11,6 +11,12 @@ begin_include
 include|#
 directive|include
 file|<sys/param.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/systm.h>
 end_include
 
 begin_include
@@ -100,8 +106,8 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|static
-name|d_select_t
-name|cttyselect
+name|d_poll_t
+name|cttypoll
 decl_stmt|;
 end_decl_stmt
 
@@ -140,7 +146,7 @@ block|,
 name|nodevtotty
 block|,
 comment|/* tty */
-name|cttyselect
+name|cttypoll
 block|,
 name|nommap
 block|,
@@ -645,11 +651,11 @@ end_comment
 begin_function
 specifier|static
 name|int
-name|cttyselect
+name|cttypoll
 parameter_list|(
 name|dev
 parameter_list|,
-name|flag
+name|events
 parameter_list|,
 name|p
 parameter_list|)
@@ -657,7 +663,7 @@ name|dev_t
 name|dev
 decl_stmt|;
 name|int
-name|flag
+name|events
 decl_stmt|;
 name|struct
 name|proc
@@ -681,25 +687,30 @@ name|ttyvp
 operator|==
 name|NULL
 condition|)
-return|return
-operator|(
-literal|1
-operator|)
-return|;
 comment|/* try operation to get EOF/failure */
 return|return
 operator|(
-name|VOP_SELECT
+name|seltrue
+argument_list|(
+name|dev
+argument_list|,
+name|events
+argument_list|,
+name|p
+argument_list|)
+operator|)
+return|;
+return|return
+operator|(
+name|VOP_POLL
 argument_list|(
 name|ttyvp
 argument_list|,
-name|flag
+name|events
 argument_list|,
-name|FREAD
-operator||
-name|FWRITE
-argument_list|,
-name|NOCRED
+name|p
+operator|->
+name|p_ucred
 argument_list|,
 name|p
 argument_list|)
