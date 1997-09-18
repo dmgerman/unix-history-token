575e1478c2dcdb4480d7eecc39f483cc8d6324e2
Poul-Henning Kamp <phk@FreeBSD.org>
1997-09-18 18:19:45 +0000

    Fix bad assumptions about types. PR:		1649 Reviewed by:	phk Submitted by:	Jason Thorpe <thorpej@nas.nasa.gov>
    Former-commit-id: 99ea54481328b71d78eb0c978211be28d45e6308

--- sbin/md5/global.h
+++ sbin/md5/global.h
@@ -38,15 +38,40 @@ name|POINTER
 typedef|;
 end_typedef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_comment
+comment|/* UINT2 defines a two byte word */
+end_comment
+
+begin_comment
+unit|typedef unsigned short int UINT2;
+comment|/* UINT4 defines a four byte word */
+end_comment
+
+begin_else
+unit|typedef unsigned long int UINT4;
+else|#
+directive|else
+end_else
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
+end_include
+
 begin_comment
 comment|/* UINT2 defines a two byte word */
 end_comment
 
 begin_typedef
 typedef|typedef
-name|unsigned
-name|short
-name|int
+name|u_int16_t
 name|UINT2
 typedef|;
 end_typedef
@@ -57,12 +82,19 @@ end_comment
 
 begin_typedef
 typedef|typedef
-name|unsigned
-name|long
-name|int
+name|u_int32_t
 name|UINT4
 typedef|;
 end_typedef
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* 0 */
+end_comment
 
 begin_comment
 comment|/* PROTO_LIST is defined depending on how PROTOTYPES is defined above. If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it   returns an empty list.  */
