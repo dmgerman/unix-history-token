7c7e709dd4bc8878f4f8d55bc2c2792d40270d1c
Brian Somers <brian@FreeBSD.org>
1997-09-19 09:42:03 +0000

    MFC: Don't close(1) in direct mode and then proceed to      isatty(1) !  Keep 0 open for this till the modem's      been set up by either dup()ing 0 or by opening      ctermid(NULL) (if isatty(0)).
         Reviewed by: Angelo Turetta <ATuretta@stylo.it>
    
    
    Former-commit-id: f6f59cd77d5a3ccefb3777abeb0a80923222d5a7

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.22.2.36 1997/09/05 23:07:26 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
+comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.22.2.37 1997/09/16 23:20:18 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
 end_comment
 
 begin_include
@@ -2313,7 +2313,7 @@ expr_stmt|;
 comment|/* We know it's currently stdout */
 name|close
 argument_list|(
-literal|0
+literal|1
 argument_list|)
 expr_stmt|;
 name|close
@@ -2356,7 +2356,7 @@ argument_list|()
 expr_stmt|;
 name|close
 argument_list|(
-literal|1
+literal|0
 argument_list|)
 expr_stmt|;
 block|}
@@ -3210,6 +3210,11 @@ argument_list|(
 name|LogPHASE
 argument_list|,
 literal|"Packet mode enabled\n"
+argument_list|)
+expr_stmt|;
+name|close
+argument_list|(
+literal|0
 argument_list|)
 expr_stmt|;
 name|PacketMode
