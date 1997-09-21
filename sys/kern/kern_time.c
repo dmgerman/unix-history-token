23e261cd71cf9b328dcb75525b35fb0b583fb27a
Justin T. Gibbs <gibbs@FreeBSD.org>
1997-09-21 22:00:25 +0000

    init_main.c subr_autoconf.c: 	Add support for "interrupt driven configuration hooks". 	A component of the kernel can register a hook, most likely 	during auto-configuration, and receive a callback once 	interrupt services are available.  This callback will occur before 	the root and dump devices are configured, so the configuration 	task can affect the selection of those two devices or complete 	any tasks that need to be performed prior to launching init. 	System boot is posponed so long as a hook is registered.  The 	hook owner is responsible for removing the hook once their task 	is complete or the system boot can continue.
    kern_acct.c kern_clock.c kern_exit.c kern_synch.c kern_time.c:
    	Change the interface and implementation for the kernel callout
    	service.  The new implemntaion is based on the work of
    	Adam M. Costello and George Varghese, published in a technical
    	report entitled "Redesigning the BSD Callout and Timer Facilities".
    	The interface used in FreeBSD is a little different than the one
    	outlined in the paper.  The new function prototypes are:
    
    	struct callout_handle timeout(void (*func)(void *),
    				      void *arg, int ticks);
    
    	void untimeout(void (*func)(void *), void *arg,
    		       struct callout_handle handle);
    
    	If a client wishes to remove a timeout, it must store the
    	callout_handle returned by timeout and pass it to untimeout.
    
    	The new implementation gives 0(1) insert and removal of callouts
    	making this interface scale well even for applications that
    	keep 100s of callouts outstanding.
    
    	See the updated timeout.9 man page for more details.
    
    
    Former-commit-id: c04caffb0762182d94b703272075950252471991

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.33 1997/08/26 00:40:04 bde Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.34 1997/09/02 20:05:49 bde Exp $  */
 end_comment
 
 begin_include
@@ -2786,6 +2786,18 @@ operator|==
 name|ITIMER_REAL
 condition|)
 block|{
+if|if
+condition|(
+name|timerisset
+argument_list|(
+operator|&
+name|p
+operator|->
+name|p_realtimer
+operator|.
+name|it_value
+argument_list|)
+condition|)
 name|untimeout
 argument_list|(
 name|realitexpire
@@ -2794,6 +2806,10 @@ operator|(
 name|caddr_t
 operator|)
 name|p
+argument_list|,
+name|p
+operator|->
+name|p_ithandle
 argument_list|)
 expr_stmt|;
 if|if
@@ -2818,6 +2834,10 @@ operator|&
 name|time
 argument_list|)
 expr_stmt|;
+name|p
+operator|->
+name|p_ithandle
+operator|=
 name|timeout
 argument_list|(
 name|realitexpire
@@ -2983,6 +3003,10 @@ operator|>
 argument_list|)
 condition|)
 block|{
+name|p
+operator|->
+name|p_ithandle
+operator|=
 name|timeout
 argument_list|(
 name|realitexpire
