48048393ce717dbb56684914b79184e077a3ea51
Brian Somers <brian@FreeBSD.org>
1997-09-21 13:11:24 +0000

    MFC: Typo police      Submitted by:	Mark Cammidge <mark@gmtunx.ee.uct.ac.za>
    Former-commit-id: 28daf175ac917543a023a4906f31cfa67df9cf59

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.38 1997/09/09 23:57:55 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.39 1997/09/16 23:20:16 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -2047,7 +2047,7 @@ name|ShowCommand
 block|,
 name|LOCAL_AUTH
 block|,
-literal|"Show status and statictics"
+literal|"Show status and statistics"
 block|,
 literal|"show var"
 block|}
@@ -3053,7 +3053,7 @@ name|ReportCompress
 block|,
 name|LOCAL_AUTH
 block|,
-literal|"Show compression statictics"
+literal|"Show compression statistics"
 block|,
 literal|"show compress"
 block|}
