b6a9d74b645112950691026e866bfb23c6f66c51
Brian Somers <brian@FreeBSD.org>
1997-09-21 20:27:46 +0000

    MFC: It turns out that the following:        close(1);        close(2);        x = open(ctermid(NULL), O_RDWR|O_NONBLOCK);        close(0)      on a tty causes select() to return an exception for descriptor x !      This is the case in RELENG_2_2, but not in 2.2.2.  I'm not sure why.      Instead of doing the x=open() and close(0), we just do x=0 now.      Problem pointed out by: Greg Lehey <grog@lemis.com>                              Tomi Vainio <tomppa@fidata.fi>
    Former-commit-id: 1b0efefbef1537d3de201b3f39088dfebbfd093c

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.22.2.37 1997/09/16 23:20:18 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
+comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.22.2.38 1997/09/19 09:42:02 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
 end_comment
 
 begin_include
@@ -3210,11 +3210,6 @@ argument_list|(
 name|LogPHASE
 argument_list|,
 literal|"Packet mode enabled\n"
-argument_list|)
-expr_stmt|;
-name|close
-argument_list|(
-literal|0
 argument_list|)
 expr_stmt|;
 name|PacketMode
