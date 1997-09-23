677caa45f48fdc7933be83e0def4b4d64c31b2e8
Brian Somers <brian@FreeBSD.org>
1997-09-23 19:52:14 +0000

    Undo botched commit (local changes got in)
    Former-commit-id: 8098a9bf6949101bcf5c0709debb5a6bcd25a27d

--- usr.sbin/ppp/lcp.c
+++ usr.sbin/ppp/lcp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	      PPP Link Control Protocol (LCP) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: lcp.c,v 1.32 1997/09/22 02:51:24 brian Exp $  *  * TODO:  *      o Validate magic number received from peer.  *	o Limit data field length by MRU  */
+comment|/*  *	      PPP Link Control Protocol (LCP) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: lcp.c,v 1.33 1997/09/22 23:59:14 brian Exp $  *  * TODO:  *      o Validate magic number received from peer.  *	o Limit data field length by MRU  */
 end_comment
 
 begin_include
@@ -1564,11 +1564,7 @@ operator|*
 name|cp
 operator|++
 operator|=
-name|VarEncMD4
-condition|?
-literal|0x80
-else|:
-literal|0x05
+literal|5
 expr_stmt|;
 comment|/* Use MD4/MD5 */
 break|break;
@@ -2552,21 +2548,12 @@ argument_list|(
 name|ConfChap
 argument_list|)
 operator|&&
-operator|(
 name|cp
 index|[
 literal|4
 index|]
 operator|==
 literal|5
-operator|||
-name|cp
-index|[
-literal|4
-index|]
-operator|==
-literal|0x80
-operator|)
 condition|)
 block|{
 name|LcpInfo
@@ -2587,15 +2574,6 @@ expr_stmt|;
 name|ackp
 operator|+=
 name|length
-expr_stmt|;
-name|VarEncMD4
-operator|=
-name|cp
-index|[
-literal|4
-index|]
-operator|==
-literal|0x80
 expr_stmt|;
 block|}
 elseif|else
