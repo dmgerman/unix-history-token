6af3c32859c630f2fff749a57a3b899cb771a2d7
Andrey Chernov <ache@FreeBSD.org>
1997-09-24 20:48:39 +0000

    Revoke privs before shell exit Includes cleanup Use getlogin() Fix names in FILES section Convert to random()
    Former-commit-id: f60459976f66dc44a470d1a536b07f7c27f6c84f

--- games/rogue/rogue.h
+++ games/rogue/rogue.h
@@ -3,6 +3,12 @@ begin_comment
 comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Timoth C. Stoehr.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)rogue.h	8.1 (Berkeley) 5/31/93  */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<string.h>
+end_include
+
 begin_comment
 comment|/*  * rogue.h  *  * This source herein may be modified and/or distributed by anybody who  * so desires, with the following restrictions:  *    1.)  This notice shall not be removed.  *    2.)  Credit shall not be taken for the creation of this source.  *    3.)  This code is not to be traded, sold, or used for personal  *         gain or profit.  */
 end_comment
@@ -2113,30 +2119,6 @@ end_comment
 begin_function_decl
 name|char
 modifier|*
-name|strcpy
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|char
-modifier|*
-name|strncpy
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|char
-modifier|*
-name|strcat
-parameter_list|()
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|char
-modifier|*
 name|mon_name
 parameter_list|()
 function_decl|;
@@ -2342,12 +2324,22 @@ parameter_list|()
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|long
+begin_define
+define|#
+directive|define
 name|rrandom
-parameter_list|()
-function_decl|;
-end_function_decl
+value|random
+end_define
+
+begin_define
+define|#
+directive|define
+name|srrandom
+parameter_list|(
+name|x
+parameter_list|)
+value|srandomdev()
+end_define
 
 begin_function_decl
 name|long
