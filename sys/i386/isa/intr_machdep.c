c580442863b59ad4e353f0dfc4fedacfbf9dfad6
Stephen McKay <mckay@FreeBSD.org>
1997-09-28 15:48:34 +0000

    Add a small hack to support the strange antics of the Unisys ELI 4003.  This machine generates an NMI for each floating point error, just like an old XT. Since it is ISA only, reading the EISA status port yields 0xff, which would give a spurious EISA panic.  The simplest thing to do is to ignore the 0xff.
    Former-commit-id: 86814125da338a35079dd8a4636daee0638d3e8e

--- sys/i386/isa/intr_machdep.c
+++ sys/i386/isa/intr_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: intr_machdep.c,v 1.5 1997/08/29 18:45:19 fsmp Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)isa.c	7.2 (Berkeley) 5/13/91  *	$Id: intr_machdep.c,v 1.6 1997/08/30 08:08:04 fsmp Exp $  */
 end_comment
 
 begin_include
@@ -786,71 +786,67 @@ name|isa_port
 operator|&
 name|NMI_PARITY
 condition|)
-block|{
 name|panic
 argument_list|(
 literal|"RAM parity error, likely hardware failure."
 argument_list|)
 expr_stmt|;
-block|}
-elseif|else
 if|if
 condition|(
 name|isa_port
 operator|&
 name|NMI_IOCHAN
 condition|)
-block|{
 name|panic
 argument_list|(
 literal|"I/O channel check, likely hardware failure."
 argument_list|)
 expr_stmt|;
-block|}
-elseif|else
+comment|/* 	 * On a real EISA machine, this will never happen.  However it can 	 * happen on ISA machines which implement XT style floating point 	 * error handling (very rare).  Save them from a meaningless panic. 	 */
+if|if
+condition|(
+name|eisa_port
+operator|==
+literal|0xff
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 if|if
 condition|(
 name|eisa_port
 operator|&
 name|ENMI_WATCHDOG
 condition|)
-block|{
 name|panic
 argument_list|(
 literal|"EISA watchdog timer expired, likely hardware failure."
 argument_list|)
 expr_stmt|;
-block|}
-elseif|else
 if|if
 condition|(
 name|eisa_port
 operator|&
 name|ENMI_BUSTIMER
 condition|)
-block|{
 name|panic
 argument_list|(
 literal|"EISA bus timeout, likely hardware failure."
 argument_list|)
 expr_stmt|;
-block|}
-elseif|else
 if|if
 condition|(
 name|eisa_port
 operator|&
 name|ENMI_IOSTATUS
 condition|)
-block|{
 name|panic
 argument_list|(
 literal|"EISA I/O port status error."
 argument_list|)
 expr_stmt|;
-block|}
-else|else
-block|{
 name|printf
 argument_list|(
 literal|"\nNMI ISA %x, EISA %x\n"
@@ -865,7 +861,6 @@ operator|(
 literal|0
 operator|)
 return|;
-block|}
 endif|#
 directive|endif
 block|}
