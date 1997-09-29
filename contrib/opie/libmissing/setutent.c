170be43819a20a517996f6531a736287751716dd
Andrey Chernov <ache@FreeBSD.org>
1997-09-29 11:31:22 +0000

    Add setutent.c
    Former-commit-id: 9f83d7d372f42fab2daae8024a4cbdb0d39813dd

---
+++ contrib/opie/libmissing/setutent.c
@@ -0,0 +1,26 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* setutent.c: A replacement for the setutent function  %%% copyright-cmetz-96 This software is Copyright 1996-1997 by Craig Metz, All Rights Reserved. The Inner Net License Version 2 applies to this software. You should have received a copy of the license with this software. If you didn't get a copy, you may request one from<license@inner.net>.          History:  	Created by cmetz for OPIE 2.31. */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|"opie_cfg.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"opie.h"
+end_include
+
+begin_decl_stmt
+name|VOIDRET
+name|setutent
+name|FUNCTION_NOARGS
+block|{ }
+end_decl_stmt
+
+end_unit
+
