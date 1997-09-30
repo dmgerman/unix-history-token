0248c703578ba3dd2c35d90186d118a917adb348
Bill Paul <wpaul@FreeBSD.org>
1997-09-30 18:08:11 +0000

    Putting records with zero-length keys into a Berkeley DB hash database is asking for trouble (sequential database enumerations can get caught in an infinite loop). The yp_mkdb(8) utility avoids putting such records into a database, but ypxfr does not. Today I got bit by a NULL entry in one of the amd maps on my network, which is served by a SunOS master. The map was transfered successfully to my FreeBSD slave, but attempting to dump it with ypcat(1) caused ypserv(8) to transmit the same record over and over again, making the map appear to be infinitely large. I finally noticed the problem while testing a new version of amd under development at the Columbia CS department, which began gobbling up insane amounts of memory while trying to swallow the map.
    To deal with this problem, I'm modifying ypxfr to watch for records
    with zero-length keys and turn them into something less destructive
    before writing them to the database.
    
    
    Former-commit-id: 8c4f3f8ab7e52f43e652ec8d4e9044924a55789e

--- libexec/ypxfr/ypxfr_main.c
+++ libexec/ypxfr/ypxfr_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ypxfr_main.c,v 1.8 1997/02/22 14:22:48 peter Exp $  */
+comment|/*  * Copyright (c) 1995  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ypxfr_main.c,v 1.9 1997/03/28 15:48:21 imp Exp $  */
 end_comment
 
 begin_include
@@ -125,7 +125,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: ypxfr_main.c,v 1.8 1997/02/22 14:22:48 peter Exp $"
+literal|"$Id: ypxfr_main.c,v 1.9 1997/03/28 15:48:21 imp Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -514,6 +514,12 @@ operator|(
 name|status
 operator|)
 return|;
+comment|/* 	 * XXX Do not attempt to write zero-length keys or 	 * data into a Berkeley DB hash database. It causes a 	 * strange failure mode where sequential searches get 	 * caught in an infinite loop. 	 */
+if|if
+condition|(
+name|keylen
+condition|)
+block|{
 name|dbkey
 operator|.
 name|data
@@ -526,6 +532,27 @@ name|size
 operator|=
 name|keylen
 expr_stmt|;
+block|}
+else|else
+block|{
+name|dbkey
+operator|.
+name|data
+operator|=
+literal|""
+expr_stmt|;
+name|dbkey
+operator|.
+name|size
+operator|=
+literal|1
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|vallen
+condition|)
+block|{
 name|dbval
 operator|.
 name|data
@@ -538,6 +565,22 @@ name|size
 operator|=
 name|vallen
 expr_stmt|;
+block|}
+else|else
+block|{
+name|dbval
+operator|.
+name|data
+operator|=
+literal|""
+expr_stmt|;
+name|dbval
+operator|.
+name|size
+operator|=
+literal|1
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|yp_put_record
