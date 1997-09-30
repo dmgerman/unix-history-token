886f20351c017618f795284fc4111a3aa81acc2c
David Greenman <dg@FreeBSD.org>
1997-09-30 12:29:00 +0000

    Brought in changes from rev 1.28: support for SIOCSIFMTU in ether_ioctl().
    Former-commit-id: 9db7129f565543da29cecf83985942248fbf0afd

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ethersubr.c	8.1 (Berkeley) 6/10/93  * $Id: if_ethersubr.c,v 1.26.2.1 1996/11/21 16:43:55 phk Exp $  */
+comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ethersubr.c	8.1 (Berkeley) 6/10/93  * $Id: if_ethersubr.c,v 1.26.2.2 1997/06/28 09:22:07 jhay Exp $  */
 end_comment
 
 begin_include
@@ -4459,7 +4459,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_function
-name|void
+name|int
 name|ether_ioctl
 parameter_list|(
 name|struct
@@ -4497,6 +4497,11 @@ name|ifreq
 operator|*
 operator|)
 name|data
+decl_stmt|;
+name|int
+name|error
+init|=
+literal|0
 decl_stmt|;
 switch|switch
 condition|(
@@ -4825,8 +4830,42 @@ argument_list|)
 expr_stmt|;
 block|}
 break|break;
+case|case
+name|SIOCSIFMTU
+case|:
+comment|/* 		 * Set the interface MTU. 		 */
+if|if
+condition|(
+name|ifr
+operator|->
+name|ifr_mtu
+operator|>
+name|ETHERMTU
+condition|)
+block|{
+name|error
+operator|=
+name|EINVAL
+expr_stmt|;
 block|}
-return|return;
+else|else
+block|{
+name|ifp
+operator|->
+name|if_mtu
+operator|=
+name|ifr
+operator|->
+name|ifr_mtu
+expr_stmt|;
+block|}
+break|break;
+block|}
+return|return
+operator|(
+name|error
+operator|)
+return|;
 block|}
 end_function
 
