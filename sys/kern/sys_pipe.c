d2a28d43163d9f424c23ab4194b3460e04657b5e
Peter Wemm <peter@FreeBSD.org>
1997-10-06 08:30:08 +0000

    Ack!  Fix excessive cut/paste blunder during poll mods.  Who had the pointy hat last? :-]
    When one is selecting (or polling) for write, it helps if we use the
    write side of the pipe when requesting wakeups instead of the read side.
    This broke ghostview (at least) - I'm suprised it wasn't noticed for
    so long.
    
    Reviewed by:  Greg Lehey <grog@lemis.com>
    
    
    Former-commit-id: 93a8caa722fec1b18ff77bf11a0b74bb31a13ca6

--- sys/kern/sys_pipe.c
+++ sys/kern/sys_pipe.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: sys_pipe.c,v 1.32 1997/09/02 20:05:53 bde Exp $  */
+comment|/*  * Copyright (c) 1996 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: sys_pipe.c,v 1.33 1997/09/14 02:43:25 peter Exp $  */
 end_comment
 
 begin_comment
@@ -4445,12 +4445,12 @@ argument_list|(
 name|p
 argument_list|,
 operator|&
-name|rpipe
+name|wpipe
 operator|->
 name|pipe_sel
 argument_list|)
 expr_stmt|;
-name|rpipe
+name|wpipe
 operator|->
 name|pipe_state
 operator||=
