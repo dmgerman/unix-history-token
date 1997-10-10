097fb0ed478245b7c3ab28604844e449751bc5f6
Poul-Henning Kamp <phk@FreeBSD.org>
1997-10-10 18:14:23 +0000

    Rename "struct kmemstats" to "struct malloc_type" it makes more sense now.
    Fix type argument to hashinit() and phashinit()
    
    
    Former-commit-id: 6fbc1c02b0e7c16e806d6501f2110c08fd6ea50e

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_malloc.c	8.3 (Berkeley) 1/4/94  * $Id: kern_malloc.c,v 1.30 1997/09/16 13:51:58 bde Exp $  */
+comment|/*  * Copyright (c) 1987, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_malloc.c	8.3 (Berkeley) 1/4/94  * $Id: kern_malloc.c,v 1.31 1997/10/10 14:06:34 phk Exp $  */
 end_comment
 
 begin_include
@@ -109,7 +109,7 @@ name|__P
 argument_list|(
 operator|(
 expr|struct
-name|kmemstats
+name|malloc_type
 operator|*
 operator|)
 argument_list|)
@@ -133,7 +133,7 @@ end_macro
 
 begin_decl_stmt
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|kmemstatistics
 init|=
@@ -263,7 +263,7 @@ name|long
 name|spare0
 decl_stmt|;
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|type
 decl_stmt|;
@@ -326,7 +326,7 @@ name|long
 name|size
 decl_stmt|;
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|type
 decl_stmt|;
@@ -390,7 +390,7 @@ endif|#
 directive|endif
 specifier|register
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|ksp
 init|=
@@ -854,7 +854,7 @@ name|type
 operator|=
 operator|(
 expr|struct
-name|kmemstats
+name|malloc_type
 operator|*
 operator|)
 name|WEIRD_ADDR
@@ -874,7 +874,7 @@ name|type
 operator|=
 operator|(
 expr|struct
-name|kmemstats
+name|malloc_type
 operator|*
 operator|)
 name|WEIRD_ADDR
@@ -1125,7 +1125,7 @@ modifier|*
 name|addr
 decl_stmt|;
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|type
 decl_stmt|;
@@ -1177,7 +1177,7 @@ endif|#
 directive|endif
 specifier|register
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|ksp
 init|=
@@ -1992,7 +1992,7 @@ parameter_list|(
 name|type
 parameter_list|)
 name|struct
-name|kmemstats
+name|malloc_type
 modifier|*
 name|type
 decl_stmt|;
