5ffc0d4cd2f848e614735b2b67c18aa19926b4aa
Andrey Chernov <ache@FreeBSD.org>
1997-10-17 09:40:08 +0000

    Add $Id
    Former-commit-id: 7421faad74e73cdd99e0e4d378bd4d834bad5f71

--- lib/libc/gen/usleep.c
+++ lib/libc/gen/usleep.c
@@ -18,13 +18,25 @@ name|lint
 argument_list|)
 end_if
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)usleep.c	8.1 (Berkeley) 6/4/93";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)usleep.c	8.1 (Berkeley) 6/4/93"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
