75b84b3fbfcdb79452acc2abe6ec9dde8cc7d114
Joerg Wunsch <joerg@FreeBSD.org>
1997-10-19 09:37:49 +0000

    Make the Sun OEM version of the DLT a known multi-LUN device so changers will work.
    PR:		kern/4802
    Submitted by:	dawes@physics.usyd.edu.au (David Dawes)
    
    Former-commit-id: a46301095b619d8ed7cefba0016411fbc83b2249

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.94 1997/09/18 00:15:55 msmith Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.95 1997/10/05 13:38:17 joerg Exp $  */
 end_comment
 
 begin_include
@@ -1366,6 +1366,26 @@ block|,
 name|T_REMOV
 block|,
 literal|"Quantum"
+block|,
+literal|"DLT*"
+block|,
+literal|"*"
+block|,
+literal|"st"
+block|,
+name|SC_MORE_LUS
+block|,
+literal|0
+block|}
+block|,
+block|{
+name|T_SEQUENTIAL
+block|,
+name|T_SEQUENTIAL
+block|,
+name|T_REMOV
+block|,
+literal|"SUN"
 block|,
 literal|"DLT*"
 block|,
