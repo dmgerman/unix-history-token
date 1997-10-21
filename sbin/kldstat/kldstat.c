6099af2041d50a7b9151c7ee50a8db775ebf79fc
John-Mark Gurney <jmg@FreeBSD.org>
1997-10-21 09:59:26 +0000

    a few code style changes:     o start function names in column 1     o sort order of flags in getopt and switch     o don't try to reference progname     o unspam some changes introduced by a 2.2.1-R build box instead of a 	-current build box
    doc changes:
        o document when these commands first appeared
        o put email address in angle brakets
        o minor mdoc clean up
    
    
    Former-commit-id: 61aa3c3b276c44454cac02c5a0a7bb7351819fa9

--- sbin/kldstat/kldstat.c
+++ sbin/kldstat/kldstat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kldstat.c,v 1.1 1997/05/07 18:19:54 dfr Exp $  */
+comment|/*-  * Copyright (c) 1997 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kldstat.c,v 1.2 1997/10/19 11:15:44 jmg Exp $  */
 end_comment
 
 begin_include
@@ -50,29 +50,6 @@ include|#
 directive|include
 file|<sys/linker.h>
 end_include
-
-begin_decl_stmt
-specifier|extern
-name|char
-modifier|*
-name|optarg
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|optind
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
-name|char
-modifier|*
-name|progname
-decl_stmt|;
-end_decl_stmt
 
 begin_function
 specifier|static
@@ -255,15 +232,15 @@ begin_function
 specifier|static
 name|void
 name|usage
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: %s [-v]\n"
-argument_list|,
-name|progname
+literal|"usage: kldstat [-v]\n"
 argument_list|)
 expr_stmt|;
 name|exit
@@ -306,13 +283,6 @@ name|filename
 init|=
 literal|0
 decl_stmt|;
-name|progname
-operator|=
-name|argv
-index|[
-literal|0
-index|]
-expr_stmt|;
 while|while
 condition|(
 operator|(
@@ -324,7 +294,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"vi:n:"
+literal|"i:n:v"
 argument_list|)
 operator|)
 operator|!=
@@ -336,14 +306,6 @@ condition|(
 name|c
 condition|)
 block|{
-case|case
-literal|'v'
-case|:
-name|verbose
-operator|=
-literal|1
-expr_stmt|;
-break|break;
 case|case
 literal|'i'
 case|:
@@ -361,6 +323,14 @@ case|:
 name|filename
 operator|=
 name|optarg
+expr_stmt|;
+break|break;
+case|case
+literal|'v'
+case|:
+name|verbose
+operator|=
+literal|1
 expr_stmt|;
 break|break;
 default|default:
@@ -390,16 +360,16 @@ condition|(
 name|filename
 condition|)
 block|{
+if|if
+condition|(
+operator|(
 name|fileid
 operator|=
 name|kldfind
 argument_list|(
 name|filename
 argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|fileid
+operator|)
 operator|<
 literal|0
 condition|)
