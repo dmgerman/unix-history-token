c3ba94a68d4af926dda5a4fe6efe2abb1e8d9869
Andrey Chernov <ache@FreeBSD.org>
1997-10-23 21:32:48 +0000

    Restore back non-interruptable sleep/usleep just redefine them to not mix with standard library functions
    Former-commit-id: e61bdb92fca1a81425461c945c62647313100ad1

--- usr.sbin/ppp/defs.h
+++ usr.sbin/ppp/defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.18 1997/08/25 00:29:10 brian Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.19 1997/09/10 02:20:28 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_ifndef
@@ -31,6 +31,12 @@ begin_include
 include|#
 directive|include
 file|<unistd.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<signal.h>
 end_include
 
 begin_include
@@ -527,6 +533,51 @@ directive|define
 name|FALSE
 value|(0)
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|SIGALRM
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|sleep
+value|nointr_sleep
+end_define
+
+begin_define
+define|#
+directive|define
+name|usleep
+value|nointr_usleep
+end_define
+
+begin_function_decl
+name|u_int
+name|sleep
+parameter_list|(
+name|u_int
+name|sec
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|usleep
+parameter_list|(
+name|u_int
+name|usec
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_endif
 endif|#
