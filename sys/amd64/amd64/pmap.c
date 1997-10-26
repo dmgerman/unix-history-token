a10b1a5eefbe5908b6e1133072f10101b08bea2d
John Dyson <dyson@FreeBSD.org>
1997-10-26 12:33:39 +0000

    Change the initial amount of memory allocated for pv_entries to be proportional to the amount of system memory.  Also, clean-up some of the new pv_entry mgmt code.
    Former-commit-id: 94a16a6ac618bcdaa6a07b1d3ba5a9619523fe43

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	$Id: pmap.c,v 1.165 1997/10/25 02:41:51 dyson Exp $  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	$Id: pmap.c,v 1.166 1997/10/25 04:49:01 dyson Exp $  */
 end_comment
 
 begin_comment
@@ -235,6 +235,13 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_define
+define|#
+directive|define
+name|MINPV
+value|2048
+end_define
 
 begin_if
 if|#
@@ -599,20 +606,11 @@ literal|0
 decl_stmt|;
 end_decl_stmt
 
-begin_define
-define|#
-directive|define
-name|NPVINIT
-value|8192
-end_define
-
 begin_decl_stmt
 name|struct
 name|pv_entry
+modifier|*
 name|pvinit
-index|[
-name|NPVINIT
-index|]
 decl_stmt|;
 end_decl_stmt
 
@@ -2077,6 +2075,9 @@ decl_stmt|;
 name|int
 name|i
 decl_stmt|;
+name|int
+name|initial_pvs
+decl_stmt|;
 comment|/* 	 * calculate the number of pv_entries needed 	 */
 name|vm_first_phys
 operator|=
@@ -2223,10 +2224,44 @@ argument_list|)
 expr_stmt|;
 block|}
 comment|/* 	 * init the pv free list 	 */
+name|initial_pvs
+operator|=
+name|pv_npg
+expr_stmt|;
+if|if
+condition|(
+name|initial_pvs
+operator|<
+name|MINPV
+condition|)
+name|initial_pvs
+operator|=
+name|MINPV
+expr_stmt|;
 name|pvzone
 operator|=
 operator|&
 name|pvzone_store
+expr_stmt|;
+name|pvinit
+operator|=
+operator|(
+expr|struct
+name|pv_entry
+operator|*
+operator|)
+name|kmem_alloc
+argument_list|(
+name|kernel_map
+argument_list|,
+name|initial_pvs
+operator|*
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|pv_entry
+argument_list|)
+argument_list|)
 expr_stmt|;
 name|zbootinit
 argument_list|(
@@ -2236,15 +2271,13 @@ literal|"PV ENTRY"
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|pvinit
-index|[
-literal|0
-index|]
+expr|struct
+name|pv_entry
 argument_list|)
 argument_list|,
 name|pvinit
 argument_list|,
-name|NPVINIT
+name|pv_npg
 argument_list|)
 expr_stmt|;
 comment|/* 	 * Now it is safe to enable pv_table recording. 	 */
@@ -2254,6 +2287,10 @@ name|TRUE
 expr_stmt|;
 block|}
 end_function
+
+begin_comment
+comment|/*  * Initialize the address space (zone) for the pv_entries.  Set a  * high water mark so that the system can recover from excessive  * numbers of pv entries.  */
+end_comment
 
 begin_function
 name|void
@@ -6291,7 +6328,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"pmap_collect: collecting pv entries -- increase PMAP_SHPGPERPROC"
+literal|"pmap_collect: collecting pv entries -- suggest increasing PMAP_SHPGPERPROC\n"
 argument_list|)
 expr_stmt|;
 name|warningdone
