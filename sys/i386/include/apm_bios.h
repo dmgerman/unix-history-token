e9d1c6d335f35ebf48cb6bbb01a69f2fe21304ca
Nate Williams <nate@FreeBSD.org>
1997-11-04 19:02:35 +0000

    MFC: - Driver cleanup (update copyrights, un-necessary parameters, white-space) - remove un-necessary include, and the NAPM check since it's done already in   apmprobe. - Run the apm_resume hooks if apm_suspend fails due to the BIOS. - Add a better display/suspend function
    [ The last requires that apm(8) be recompiled ]
    
    
    Former-commit-id: b547a56e24c2e4e99d603137eb312260d26e54c5

--- sys/i386/include/apm_bios.h
+++ sys/i386/include/apm_bios.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Aug, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm_bios.h,v 1.12 1996/09/21 14:57:49 bde Exp $  */
+comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@mt.cs.keio.ac.jp>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Aug, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm_bios.h,v 1.12.2.1 1997/03/09 16:52:49 kato Exp $  */
 end_comment
 
 begin_ifndef
@@ -1091,8 +1091,8 @@ end_define
 begin_define
 define|#
 directive|define
-name|APMIO_DISPLAYOFF
-value|_IO('P', 9)
+name|APMIO_DISPLAY
+value|_IOW('P', 9, int)
 end_define
 
 begin_endif
