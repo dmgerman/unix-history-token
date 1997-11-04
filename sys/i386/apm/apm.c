21cdbc4051e3ae59053a978e841f53ec490e4ad8
Nate Williams <nate@FreeBSD.org>
1997-11-04 17:37:52 +0000

    - If the APM BIOS fails to suspend the system after running the   suspend hooks, run the resume hooks to re-configure the system back   to where it was.
    Former-commit-id: c6e1903d7702ffcad032ca86c6c724f06bbaa319

--- sys/i386/apm/apm.c
+++ sys/i386/apm/apm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@jp.FreeBSD.org>  * Copyright (c) 1996 Nate Williams<nate@FreeBSD.org>  * Copyright (c) 1997 Poul-Henning Kamp<phk@FreeBSD.org>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.60 1997/09/21 21:33:10 gibbs Exp $  */
+comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@jp.FreeBSD.org>  * Copyright (c) 1996 Nate Williams<nate@FreeBSD.org>  * Copyright (c) 1997 Poul-Henning Kamp<phk@FreeBSD.org>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.62 1997/10/23 04:18:49 nate Exp $  */
 end_comment
 
 begin_include
@@ -1774,11 +1774,25 @@ name|APM_HOOK_SUSPEND
 index|]
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
 name|apm_suspend_system
 argument_list|()
-expr_stmt|;
+operator|==
+literal|0
+condition|)
 name|apm_processevent
 argument_list|()
+expr_stmt|;
+else|else
+comment|// Failure, 'resume' the system again
+name|apm_execute_hook
+argument_list|(
+name|hook
+index|[
+name|APM_HOOK_RESUME
+index|]
+argument_list|)
 expr_stmt|;
 block|}
 block|}
