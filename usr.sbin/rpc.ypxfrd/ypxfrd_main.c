6d1d7b44198758ac737db837e4cffb76106e5140
Philippe Charnier <charnier@FreeBSD.org>
1997-11-06 07:32:22 +0000

    MFC: Use err(3).
    Former-commit-id: d4573e99c49a237609d756a9f213a62a8de1fe5d

--- usr.sbin/rpc.ypxfrd/ypxfrd_main.c
+++ usr.sbin/rpc.ypxfrd/ypxfrd_main.c
@@ -1,12 +1,50 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995, 1996  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ypxfrd_main.c,v 1.1.1.1 1996/06/05 04:36:54 wpaul Exp $  */
+comment|/*  * Copyright (c) 1995, 1996  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|char
+name|rcsid
+index|[]
+init|=
+literal|"$Id$"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"ypxfrd.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|<err.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<errno.h>
 end_include
 
 begin_include
@@ -122,12 +160,6 @@ directive|include
 file|<sys/wait.h>
 end_include
 
-begin_include
-include|#
-directive|include
-file|<errno.h>
-end_include
-
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -183,7 +215,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: ypxfrd_main.c,v 1.1.1.1 1996/06/05 04:36:54 wpaul Exp $"
+literal|"$Id: ypxfrd_main.c,v 1.1.1.1.2.1 1997/08/29 05:15:58 imp Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -296,14 +328,9 @@ name|msg
 argument_list|)
 expr_stmt|;
 else|else
-operator|(
-name|void
-operator|)
-name|fprintf
+name|warnx
 argument_list|(
-name|stderr
-argument_list|,
-literal|"%s\n"
+literal|"%s"
 argument_list|,
 name|msg
 argument_list|)
@@ -553,7 +580,7 @@ condition|)
 block|{
 continue|continue;
 block|}
-name|perror
+name|warn
 argument_list|(
 literal|"svc_run: - select failed"
 argument_list|)
@@ -670,9 +697,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"%s [-p path]\n"
-argument_list|,
-name|progname
+literal|"usage: rpc.ypxfrd [-p path]\n"
 argument_list|)
 expr_stmt|;
 name|exit
@@ -684,6 +709,7 @@ block|}
 end_function
 
 begin_function
+name|int
 name|main
 parameter_list|(
 name|argc
@@ -703,12 +729,16 @@ specifier|register
 name|SVCXPRT
 modifier|*
 name|transp
+init|=
+name|NULL
 decl_stmt|;
 name|int
 name|sock
 decl_stmt|;
 name|int
 name|proto
+init|=
+literal|0
 decl_stmt|;
 name|struct
 name|sockaddr_in
@@ -884,18 +914,13 @@ name|pid
 operator|<
 literal|0
 condition|)
-block|{
-name|perror
-argument_list|(
-literal|"cannot fork"
-argument_list|)
-expr_stmt|;
-name|exit
+name|err
 argument_list|(
 literal|1
+argument_list|,
+literal|"fork"
 argument_list|)
 expr_stmt|;
-block|}
 if|if
 condition|(
 name|pid
