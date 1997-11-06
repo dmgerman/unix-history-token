a016aa76e509d840512e5b240f87da2a9db9f6bc
Andrey Chernov <ache@FreeBSD.org>
1997-11-06 01:00:03 +0000

    Merge changes
    Former-commit-id: be1cebad0b73aaa557497f39e46f55429469d93c

--- contrib/libreadline/complete.c
+++ contrib/libreadline/complete.c
@@ -2944,6 +2944,9 @@ block|{
 comment|/* If there is a single match, see if we need to quote it.          This also checks whether the common prefix of several 	 matches needs to be quoted. */
 name|should_quote
 operator|=
+name|rl_filename_quote_characters
+condition|?
+operator|(
 name|rl_strpbrk
 argument_list|(
 name|match
@@ -2951,6 +2954,9 @@ argument_list|,
 name|rl_filename_quote_characters
 argument_list|)
 operator|!=
+literal|0
+operator|)
+else|:
 literal|0
 expr_stmt|;
 name|do_replace
@@ -5941,21 +5947,6 @@ name|char
 modifier|*
 name|scan
 decl_stmt|;
-if|if
-condition|(
-name|string2
-operator|==
-name|NULL
-condition|)
-return|return
-operator|(
-operator|(
-name|char
-operator|*
-operator|)
-name|NULL
-operator|)
-return|;
 for|for
 control|(
 init|;
