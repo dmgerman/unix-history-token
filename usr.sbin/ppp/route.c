5bb3d824e97fec92841506876ab87810a7f40900
Brian Somers <brian@FreeBSD.org>
1997-11-09 03:22:49 +0000

    Don't include RTA_GATEWAY in rtm_addrs unless it's specified.
    Former-commit-id: 52e3ef9336c3924181434212685df1e95aa52d3f

--- usr.sbin/ppp/route.c
+++ usr.sbin/ppp/route.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	      PPP Routing related Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1994, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: route.c,v 1.20 1997/10/26 01:03:37 brian Exp $  *  */
+comment|/*  *	      PPP Routing related Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1994, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: route.c,v 1.21 1997/11/08 00:28:11 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -280,8 +280,6 @@ operator|=
 name|RTA_DST
 operator||
 name|RTA_NETMASK
-operator||
-name|RTA_GATEWAY
 expr_stmt|;
 name|rtmes
 operator|.
@@ -396,6 +394,14 @@ expr_stmt|;
 name|cp
 operator|+=
 literal|16
+expr_stmt|;
+name|rtmes
+operator|.
+name|m_rtm
+operator|.
+name|rtm_addrs
+operator||=
+name|RTA_GATEWAY
 expr_stmt|;
 block|}
 if|if
