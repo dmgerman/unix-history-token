62741e473cb20da7d4347baeb4e2ce02e6aa9f56
Nate Williams <nate@FreeBSD.org>
1997-11-10 14:38:08 +0000

    - Convert c++ comment to c comment.
    Noted by: 	Bruce
    
    
    Former-commit-id: 13e3474ad7acd341152d8815238fdf91d89fb468

--- sys/i386/apm/apm.c
+++ sys/i386/apm/apm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@jp.FreeBSD.org>  * Copyright (c) 1996 Nate Williams<nate@FreeBSD.org>  * Copyright (c) 1997 Poul-Henning Kamp<phk@FreeBSD.org>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.62 1997/10/23 04:18:49 nate Exp $  */
+comment|/*  * APM (Advanced Power Management) BIOS Device Driver  *  * Copyright (c) 1994 UKAI, Fumitoshi.  * Copyright (c) 1994-1995 by HOSOKAWA, Tatsumi<hosokawa@jp.FreeBSD.org>  * Copyright (c) 1996 Nate Williams<nate@FreeBSD.org>  * Copyright (c) 1997 Poul-Henning Kamp<phk@FreeBSD.org>  *  * This software may be used, modified, copied, and distributed, in  * both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep, 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm.c,v 1.63 1997/11/04 17:37:52 nate Exp $  */
 end_comment
 
 begin_include
@@ -1785,7 +1785,7 @@ name|apm_processevent
 argument_list|()
 expr_stmt|;
 else|else
-comment|// Failure, 'resume' the system again
+comment|/* Failure, 'resume' the system again */
 name|apm_execute_hook
 argument_list|(
 name|hook
