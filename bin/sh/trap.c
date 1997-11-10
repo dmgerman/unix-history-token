adcdd8567092ef263c4ab631ff732201558e3c57
Bruce Evans <bde@FreeBSD.org>
1997-11-10 11:32:24 +0000

    Fixed some type and value mismatches.  setsignal() returned a bogusly cast value that was always ignored.  Rev.1.9 of trap.c made this more bogus by returning a semantically different value after calling siginterrupt().  Avoid these problems by not returning a value.
    Former-commit-id: 10aab0881c991cc14d6988ec2e17d57d80717f3c

--- bin/sh/trap.c
+++ bin/sh/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: trap.c,v 1.8 1997/02/22 13:58:46 peter Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: trap.c,v 1.9 1997/11/05 23:33:58 ache Exp $  */
 end_comment
 
 begin_ifndef
@@ -855,7 +855,7 @@ comment|/*  * Set the signal handler for the specified signal.  The routine figu
 end_comment
 
 begin_function
-name|long
+name|void
 name|setsignal
 parameter_list|(
 name|signo
@@ -868,6 +868,8 @@ name|int
 name|action
 decl_stmt|;
 name|sig_t
+name|sig
+decl_stmt|,
 name|sigact
 init|=
 name|SIG_DFL
@@ -875,9 +877,6 @@ decl_stmt|;
 name|char
 modifier|*
 name|t
-decl_stmt|;
-name|long
-name|sig
 decl_stmt|;
 if|if
 condition|(
@@ -1023,9 +1022,7 @@ argument_list|)
 condition|)
 block|{
 comment|/* 			 * Pretend it worked; maybe we should give a warning 			 * here, but other shells don't. We don't alter 			 * sigmode, so that we retry every time. 			 */
-return|return
-literal|0
-return|;
+return|return;
 block|}
 if|if
 condition|(
@@ -1089,9 +1086,7 @@ name|t
 operator|==
 name|action
 condition|)
-return|return
-literal|0
-return|;
+return|return;
 switch|switch
 condition|(
 name|action
@@ -1129,9 +1124,6 @@ name|action
 expr_stmt|;
 name|sig
 operator|=
-operator|(
-name|long
-operator|)
 name|signal
 argument_list|(
 name|signo
@@ -1146,15 +1138,12 @@ if|if
 condition|(
 name|sig
 operator|!=
-operator|-
-literal|1
+name|SIG_ERR
 operator|&&
 name|action
 operator|==
 name|S_CATCH
 condition|)
-name|sig
-operator|=
 name|siginterrupt
 argument_list|(
 name|signo
@@ -1164,9 +1153,6 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-return|return
-name|sig
-return|;
 block|}
 end_function
 
