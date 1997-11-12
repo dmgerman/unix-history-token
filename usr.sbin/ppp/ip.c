758eb004aba7707988a82571b3f8370b6bd67983
Brian Somers <brian@FreeBSD.org>
1997-11-12 21:04:21 +0000

    Understand networks in the filter rules.  We don't insist on x.x.x.0/24 - x.x.x.x/24 is ok too.
    Former-commit-id: 69e76fade29674ed58b9cfb802bf6f1a08c65a88

--- usr.sbin/ppp/ip.c
+++ usr.sbin/ppp/ip.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP IP Protocol Interface  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ip.c,v 1.27 1997/10/26 12:42:10 brian Exp $  *  *	TODO:  *		o Return ICMP message for filterd packet  *		  and optionaly record it into log.  */
+comment|/*  *		PPP IP Protocol Interface  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ip.c,v 1.28 1997/11/12 19:48:45 brian Exp $  *  *	TODO:  *		o Return ICMP message for filterd packet  *		  and optionaly record it into log.  */
 end_comment
 
 begin_include
@@ -731,11 +731,19 @@ operator|.
 name|s_addr
 operator|)
 operator|==
+operator|(
 name|fp
 operator|->
 name|saddr
 operator|.
 name|s_addr
+operator|&
+name|fp
+operator|->
+name|smask
+operator|.
+name|s_addr
+operator|)
 operator|&&
 operator|(
 name|pip
@@ -751,11 +759,19 @@ operator|.
 name|s_addr
 operator|)
 operator|==
+operator|(
 name|fp
 operator|->
 name|daddr
 operator|.
 name|s_addr
+operator|&
+name|fp
+operator|->
+name|dmask
+operator|.
+name|s_addr
+operator|)
 condition|)
 block|{
 if|if
