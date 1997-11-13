0f69e26512b51e83011d97b3ba74007c038c25a9
Jordan K. Hubbard <jkh@FreeBSD.org>
1997-11-13 11:49:53 +0000

    MFC: PR#4894
    Former-commit-id: 2cb3a3e73611164fa4f4f2b7e60ff5a6ed396a2c

--- usr.sbin/pkg_install/create/perform.c
+++ usr.sbin/pkg_install/create/perform.c
@@ -12,7 +12,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: perform.c,v 1.32.2.3 1997/10/09 07:09:01 charnier Exp $"
+literal|"$Id: perform.c,v 1.32.2.4 1997/10/13 15:06:11 jkh Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -379,35 +379,6 @@ argument_list|,
 name|pkg_in
 argument_list|)
 expr_stmt|;
-comment|/* Prefix should override the packing list */
-if|if
-condition|(
-name|Prefix
-condition|)
-block|{
-name|delete_plist
-argument_list|(
-operator|&
-name|plist
-argument_list|,
-name|FALSE
-argument_list|,
-name|PLIST_CWD
-argument_list|,
-name|NULL
-argument_list|)
-expr_stmt|;
-name|add_plist_top
-argument_list|(
-operator|&
-name|plist
-argument_list|,
-name|PLIST_CWD
-argument_list|,
-name|Prefix
-argument_list|)
-expr_stmt|;
-block|}
 comment|/*      * Run down the list and see if we've named it, if not stick in a name      * at the top.      */
 if|if
 condition|(
