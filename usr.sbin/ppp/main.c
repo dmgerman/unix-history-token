fb8671e0666c799e92a62367837c68452cec78ba
Brian Somers <brian@FreeBSD.org>
1997-11-13 14:44:06 +0000

    Log a warning when a -direct invocation is denied.
    Former-commit-id: c7a5cc123ecb35e2a7c721c030222d585e2a03b6

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.95 1997/11/13 12:10:50 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
+comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.96 1997/11/13 14:43:18 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
 end_comment
 
 begin_include
@@ -1660,6 +1660,50 @@ name|stderr
 argument_list|,
 literal|"You may not use ppp in this mode with this label\n"
 argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|mode
+operator|&
+name|MODE_DIRECT
+condition|)
+block|{
+specifier|const
+name|char
+modifier|*
+name|l
+decl_stmt|;
+if|if
+condition|(
+operator|(
+name|l
+operator|=
+name|GetLabel
+argument_list|()
+operator|)
+operator|==
+name|NULL
+condition|)
+name|l
+operator|=
+literal|"default"
+expr_stmt|;
+name|VarTerm
+operator|=
+literal|0
+expr_stmt|;
+name|LogPrintf
+argument_list|(
+name|LogWARN
+argument_list|,
+literal|"Label %s rejected -direct connection\n"
+argument_list|,
+name|l
+argument_list|)
+expr_stmt|;
+block|}
+name|LogClose
+argument_list|()
 expr_stmt|;
 return|return
 literal|1
