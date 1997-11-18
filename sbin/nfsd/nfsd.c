1f22c5c1523ed400bbc4741f7f2272a2c3e6f041
John Polstra <jdp@FreeBSD.org>
1997-11-18 03:14:53 +0000

    Fix two missing arguments detected by "-Wformat".
    Former-commit-id: fa978d1ceb854ca93a50e6d45fb262c9e690815f

--- sbin/nfsd/nfsd.c
+++ sbin/nfsd/nfsd.c
@@ -849,6 +849,8 @@ name|warnx
 argument_list|(
 literal|"nfsd count %d; reset to %d"
 argument_list|,
+name|nfsdcnt
+argument_list|,
 name|DEFNFSDCNT
 argument_list|)
 expr_stmt|;
@@ -988,6 +990,8 @@ block|{
 name|warnx
 argument_list|(
 literal|"nfsd count %d; reset to %d"
+argument_list|,
+name|nfsdcnt
 argument_list|,
 name|DEFNFSDCNT
 argument_list|)
