0b286d0a1227211b5b2fa9cab3edb849f66c65c3
John Polstra <jdp@FreeBSD.org>
1997-11-18 05:35:48 +0000

    Merge from main branch 1.18 -> 1.19: Fix erroneous format string.
    Former-commit-id: 2de26465b3320f7f4cc425cd3c84d86bac42f120

--- lib/libdisk/chunk.c
+++ lib/libdisk/chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: chunk.c,v 1.15 1995/06/11 19:29:32 rgrimes Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: chunk.c,v 1.16 1996/03/24 18:55:36 joerg Exp $  *  */
 end_comment
 
 begin_include
@@ -309,7 +309,7 @@ return|;
 default|default:
 name|warn
 argument_list|(
-literal|"Unsupported mother (0x%x) in Find_Mother_Chunk"
+literal|"Unsupported mother type in Find_Mother_Chunk"
 argument_list|)
 expr_stmt|;
 return|return
