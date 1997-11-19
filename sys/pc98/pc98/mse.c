1b70807bd9ad59ca3b5e700054bbc0555743efb5
Takenori KATO <kato@FreeBSD.org>
1997-11-19 11:35:52 +0000

    Sync with sys/i386/isa/mse.c revision 1.35.
    Former-commit-id: 2bb25ddb002aba33e466b41fc1257d7c571ceb74

--- sys/pc98/pc98/mse.c
+++ sys/pc98/pc98/mse.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright 1992 by the University of Guelph  *  * Permission to use, copy and modify this  * software and its documentation for any purpose and without  * fee is hereby granted, provided that the above copyright  * notice appear in all copies and that both that copyright  * notice and this permission notice appear in supporting  * documentation.  * University of Guelph makes no representations about the suitability of  * this software for any purpose.  It is provided "as is"  * without express or implied warranty.  *  * $Id: mse.c,v 1.11 1997/07/21 13:11:06 kato Exp $  */
+comment|/*  * Copyright 1992 by the University of Guelph  *  * Permission to use, copy and modify this  * software and its documentation for any purpose and without  * fee is hereby granted, provided that the above copyright  * notice appear in all copies and that both that copyright  * notice and this permission notice appear in supporting  * documentation.  * University of Guelph makes no representations about the suitability of  * this software for any purpose.  It is provided "as is"  * without express or implied warranty.  *  * $Id: mse.c,v 1.12 1997/09/14 16:27:31 kato Exp $  */
 end_comment
 
 begin_comment
@@ -46,12 +46,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/proc.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/kernel.h>
 end_include
 
@@ -59,6 +53,12 @@ begin_include
 include|#
 directive|include
 file|<sys/poll.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/select.h>
 end_include
 
 begin_include
