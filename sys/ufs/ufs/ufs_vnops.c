69f62ec5e970395f506e8cc76e0347b1ef48541a
Bruce Evans <bde@FreeBSD.org>
1997-11-20 16:08:56 +0000

    Fixed marking of access time for special files and fifos (don't do it if the file system is mounted noatime).  Not fixed: the access time is marked at the start of a read() and not marked on successful completion.  I think this should be handled at the vfs level.
    Print a better panic message for missing vops.  Don't use printf()
    before panic(), since the printf()ed part isn't shown by gdb.
    This actually loses a little with the current gdb, since gdb just
    prints the fmt arg to panic, so %'s aren't expanded.  gdb should
    fetch the full message from the message buffer if possible.
    
    Fixed default vop function for vop_getpages_desc.  It needs to
    just return EOPNOTSUPP so that the vnode pager can get the pages
    in using a general method.  Panicing broke exec'ing of files on
    ext2fs file systems.  ffs works because it doesn't use the default.
    
    Fixed nearby style bugs.
    
    
    Former-commit-id: b5f270572095838696d55b0e5a46a3d133f7c33a

--- sys/ufs/ufs/ufs_vnops.c
+++ sys/ufs/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_vnops.c	8.27 (Berkeley) 5/27/95  * $Id: ufs_vnops.c,v 1.67 1997/11/13 01:48:30 julian Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_vnops.c	8.27 (Berkeley) 5/27/95  * $Id: ufs_vnops.c,v 1.68 1997/11/18 14:20:09 phk Exp $  */
 end_comment
 
 begin_include
@@ -8265,6 +8265,21 @@ name|ap
 decl_stmt|;
 block|{
 comment|/* 	 * Set access flag. 	 */
+if|if
+condition|(
+operator|!
+operator|(
+name|ap
+operator|->
+name|a_vp
+operator|->
+name|v_mount
+operator|->
+name|mnt_flag
+operator|&
+name|MNT_NOATIME
+operator|)
+condition|)
 name|VTOI
 argument_list|(
 name|ap
@@ -8452,6 +8467,21 @@ name|ap
 decl_stmt|;
 block|{
 comment|/* 	 * Set access flag. 	 */
+if|if
+condition|(
+operator|!
+operator|(
+name|ap
+operator|->
+name|a_vp
+operator|->
+name|v_mount
+operator|->
+name|mnt_flag
+operator|&
+name|MNT_NOATIME
+operator|)
+condition|)
 name|VTOI
 argument_list|(
 name|ap
@@ -9556,20 +9586,15 @@ modifier|*
 name|ap
 decl_stmt|;
 block|{
-name|printf
+name|panic
 argument_list|(
-literal|"Missing VOP function (%s) in UFS child"
+literal|"no vop function for %s in ufs child"
 argument_list|,
 name|ap
 operator|->
 name|a_desc
 operator|->
 name|vdesc_name
-argument_list|)
-expr_stmt|;
-name|panic
-argument_list|(
-literal|"Missing VOP function in UFS"
 argument_list|)
 expr_stmt|;
 return|return
@@ -9614,17 +9639,6 @@ block|,
 block|{
 operator|&
 name|vop_fsync_desc
-block|,
-operator|(
-name|vop_t
-operator|*
-operator|)
-name|ufs_missingop
-block|}
-block|,
-block|{
-operator|&
-name|vop_getpages_desc
 block|,
 operator|(
 name|vop_t
