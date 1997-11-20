aefba0dd718f8a92875758353e4c5230c474c753
Bruce Evans <bde@FreeBSD.org>
1997-11-20 18:24:52 +0000

    Fixed wrong limits for the kernel text in db_numargs().  The interval [VM_MIN_KERNEL_ADDRESS, etext] was used instead of [btext, etext).  Added a comment about this being completely wrong for LKMs.  This only affects interpreting the instructions after the return to attempt decide the number of args.  The attempt usually fails anyway.
    Former-commit-id: 023ab6dd07f26df3e69c79a79c46b04539ea9668

--- sys/i386/i386/db_trace.c
+++ sys/i386/i386/db_trace.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_trace.c,v 1.25 1997/08/21 06:32:37 charnier Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_trace.c,v 1.26 1997/10/27 17:23:18 bde Exp $  */
 end_comment
 
 begin_include
@@ -456,6 +456,7 @@ argument_list|,
 name|FALSE
 argument_list|)
 expr_stmt|;
+comment|/* 	 * XXX etext is wrong for LKMs.  We should attempt to interpret 	 * the instruction at the return address in all cases.  This 	 * may require better fault handling. 	 */
 if|if
 condition|(
 name|argp
@@ -464,10 +465,10 @@ operator|(
 name|int
 operator|*
 operator|)
-name|VM_MIN_KERNEL_ADDRESS
+name|btext
 operator|||
 name|argp
-operator|>
+operator|>=
 operator|(
 name|int
 operator|*
