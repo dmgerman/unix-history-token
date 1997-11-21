80fbaf570fa586a8033eb5994122aced13004e37
Jonathan Lemon <jlemon@FreeBSD.org>
1997-11-21 22:33:52 +0000

    Correct CPU_CYRIX_NO_LOCK fix. PR:		5121 Pointed out by:	Matthew Hunt
    Former-commit-id: 012815ec8ce809ad653e45bd2f5fda73415c13cb

--- sys/amd64/amd64/initcpu.c
+++ sys/amd64/amd64/initcpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) KATO Takenori, 1997.  *   * All rights reserved.  Unpublished rights reserved under the copyright  * laws of Japan.  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *   * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *		$Id: initcpu.c,v 1.8 1997/10/06 08:08:41 kato Exp $  */
+comment|/*  * Copyright (c) KATO Takenori, 1997.  *   * All rights reserved.  Unpublished rights reserved under the copyright  * laws of Japan.  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *   * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *		$Id: initcpu.c,v 1.9 1997/10/28 15:58:09 bde Exp $  */
 end_comment
 
 begin_include
@@ -980,11 +980,11 @@ directive|ifdef
 name|CPU_CYRIX_NO_LOCK
 name|write_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|,
 name|read_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|)
 operator||
 name|CCR1_NO_LOCK
@@ -997,11 +997,11 @@ directive|ifdef
 name|FAILSAFE
 name|write_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|,
 name|read_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|)
 operator|&
 operator|~
@@ -1305,11 +1305,11 @@ directive|ifdef
 name|CPU_CYRIX_NO_LOCK
 name|write_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|,
 name|read_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|)
 operator||
 name|CCR1_NO_LOCK
@@ -1322,11 +1322,11 @@ directive|ifdef
 name|FAILSAFE
 name|write_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|,
 name|read_cyrix_reg
 argument_list|(
-name|CCR0
+name|CCR1
 argument_list|)
 operator|&
 operator|~
