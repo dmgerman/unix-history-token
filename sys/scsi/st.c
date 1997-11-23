07eea7ed369b3736abc21881fab4d7770954108f
Bruce Evans <bde@FreeBSD.org>
1997-11-23 11:56:18 +0000

    Fixed misuse of O_ACCMODE.  Cosmetic.
    Not fixed in:	sound driver
    
    
    Former-commit-id: be244e5da2c56ef3297189144a1292ed1c511cf8

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.81 1997/09/07 10:08:23 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.82 1997/09/21 22:03:19 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -2135,12 +2135,8 @@ block|}
 comment|/* 	 * Make sure that a tape opened in write-only mode will have 	 * file marks written on it when closed, even if not written to. 	 * This is for SUN compatibility 	 */
 if|if
 condition|(
-operator|(
 name|flags
 operator|&
-name|O_ACCMODE
-operator|)
-operator|==
 name|FWRITE
 condition|)
 name|st
