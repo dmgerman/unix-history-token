0a11827d96c1b9ddbade71adbc99fbb5d9739951
Bruce Evans <bde@FreeBSD.org>
1997-11-23 10:30:50 +0000

    Fixed some style bugs in the poll() code.
    Removed dead code to "Avoid inadvertently sleeping forever".  hzto()
    never returns 0.
    
    
    Former-commit-id: 4e5e02b79141741ed0636d5d24e720789584aa22

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)sys_generic.c	8.5 (Berkeley) 1/21/94  * $Id: sys_generic.c,v 1.31 1997/10/12 20:24:02 phk Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)sys_generic.c	8.5 (Berkeley) 1/21/94  * $Id: sys_generic.c,v 1.32 1997/11/06 19:29:20 phk Exp $  */
 end_comment
 
 begin_include
@@ -163,6 +163,27 @@ end_expr_stmt
 begin_decl_stmt
 specifier|static
 name|int
+name|pollscan
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|proc
+operator|*
+operator|,
+expr|struct
+name|pollfd
+operator|*
+operator|,
+name|int
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|int
 name|selscan
 name|__P
 argument_list|(
@@ -177,27 +198,6 @@ operator|*
 operator|,
 name|fd_mask
 operator|*
-operator|*
-operator|,
-name|int
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
-name|int
-name|pollscan
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|proc
-operator|*
-operator|,
-expr|struct
-name|pollfd
 operator|*
 operator|,
 name|int
@@ -3124,17 +3124,6 @@ operator|&
 name|atv
 argument_list|)
 expr_stmt|;
-comment|/* 		 * Avoid inadvertently sleeping forever. 		 */
-if|if
-condition|(
-name|timo
-operator|==
-literal|0
-condition|)
-name|timo
-operator|=
-literal|1
-expr_stmt|;
 name|splx
 argument_list|(
 name|s
@@ -3474,7 +3463,7 @@ name|n
 init|=
 literal|0
 decl_stmt|;
-comment|/* Note; backend also returns POLLHUP/POLLERR if appropriate */
+comment|/* Note: backend also returns POLLHUP/POLLERR if appropriate. */
 specifier|static
 name|int
 name|flag
@@ -3930,17 +3919,6 @@ operator|&
 name|atv
 argument_list|)
 expr_stmt|;
-comment|/* 		 * Avoid inadvertently sleeping forever. 		 */
-if|if
-condition|(
-name|timo
-operator|==
-literal|0
-condition|)
-name|timo
-operator|=
-literal|1
-expr_stmt|;
 name|splx
 argument_list|(
 name|s
@@ -4303,7 +4281,7 @@ expr_stmt|;
 block|}
 else|else
 block|{
-comment|/* Note: backend also returns POLLHUP and 				 * POLLERR if appropriate */
+comment|/* 				 * Note: backend also returns POLLHUP and 				 * POLLERR if appropriate. 				 */
 name|fds
 operator|->
 name|revents
