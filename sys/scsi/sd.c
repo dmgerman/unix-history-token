8fe57837b59be1bc7fe94eb8e4e454af143ba682
Bruce Evans <bde@FreeBSD.org>
1997-11-23 11:30:42 +0000

    Fixed the sector size frobbing in sd_strategy() at least not to break for the usual sector size.  dscheck() adjusts b_bcount to handle EOF, so we must scale it and not preserve it.  i/o beyond the end of partitions has been broken since rev.1.96.
    Not fixed in:	od driver
    
    
    Former-commit-id: 7a68bd5784cad3b6e217f2407e88dee373aa3d87

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.112 1997/10/12 08:54:47 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.113 1997/11/07 08:53:32 phk Exp $  */
 end_comment
 
 begin_include
@@ -2000,7 +2000,6 @@ name|secsize
 operator|/
 name|DEV_BSIZE
 decl_stmt|;
-comment|/* save original block number and size */
 name|int
 name|b_blkno
 init|=
@@ -2008,14 +2007,7 @@ name|bp
 operator|->
 name|b_blkno
 decl_stmt|;
-name|int
-name|b_bcount
-init|=
-name|bp
-operator|->
-name|b_bcount
-decl_stmt|;
-comment|/* replace with scaled values */
+comment|/* Replace blkno and count with scaled values. */
 name|bp
 operator|->
 name|b_blkno
@@ -2040,7 +2032,7 @@ operator|->
 name|dk_slices
 argument_list|)
 expr_stmt|;
-comment|/* prevent bad side effects in block system */
+comment|/* 		 * Restore blkno and unscale the values set by dscheck(), 		 * except for b_pblkno. 		 */
 name|bp
 operator|->
 name|b_blkno
@@ -2050,10 +2042,9 @@ expr_stmt|;
 name|bp
 operator|->
 name|b_bcount
-operator|=
-name|b_bcount
+operator|*=
+name|sec_blk_ratio
 expr_stmt|;
-comment|/* scale resid */
 name|bp
 operator|->
 name|b_resid
