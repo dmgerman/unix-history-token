224507c0fc8a4908809345d684e28da4aafe5c5e
Takenori KATO <kato@FreeBSD.org>
1997-11-25 09:54:36 +0000

    Sync with sys/i386/i386/machdep.c revision up to 1.274.
    Former-commit-id: 06fbe45895ce1035300fcf59fa411af57a6263e7

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.60 1997/10/13 09:21:16 kato Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.61 1997/11/07 12:53:48 kato Exp $  */
 end_comment
 
 begin_include
@@ -352,6 +352,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/ipl.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/md_var.h>
 end_include
 
@@ -617,6 +623,19 @@ argument|NULL
 argument_list|)
 end_macro
 
+begin_expr_stmt
+specifier|static
+name|MALLOC_DEFINE
+argument_list|(
+name|M_MBUF
+argument_list|,
+literal|"mbuf"
+argument_list|,
+literal|"mbuf"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -653,21 +672,6 @@ ifdef|#
 directive|ifdef
 name|BOUNCE_BUFFERS
 end_ifdef
-
-begin_decl_stmt
-specifier|extern
-name|char
-modifier|*
-name|bouncememory
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|maxbkva
-decl_stmt|;
-end_decl_stmt
 
 begin_ifdef
 ifdef|#
@@ -711,13 +715,6 @@ comment|/* BOUNCE_BUFFERS */
 end_comment
 
 begin_decl_stmt
-specifier|extern
-name|int
-name|freebufspace
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 name|int
 name|msgbufmapped
 init|=
@@ -742,19 +739,6 @@ name|u_int
 name|atdevbase
 decl_stmt|;
 end_decl_stmt
-
-begin_expr_stmt
-specifier|static
-name|MALLOC_DEFINE
-argument_list|(
-name|M_MBUF
-argument_list|,
-literal|"mbuf"
-argument_list|,
-literal|"mbuf"
-argument_list|)
-expr_stmt|;
-end_expr_stmt
 
 begin_decl_stmt
 name|int
@@ -892,10 +876,6 @@ end_expr_stmt
 
 begin_decl_stmt
 name|int
-name|boothowto
-init|=
-literal|0
-decl_stmt|,
 name|bootverbose
 init|=
 literal|0
@@ -928,13 +908,6 @@ end_endif
 begin_decl_stmt
 name|long
 name|dumplo
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|bootdev
 decl_stmt|;
 end_decl_stmt
 
@@ -4862,6 +4835,11 @@ decl_stmt|;
 name|int
 name|speculative_mprobe
 decl_stmt|;
+comment|/* 	 * Prevent lowering of the ipl if we call tsleep() early. 	 */
+name|safepri
+operator|=
+name|cpl
+expr_stmt|;
 name|proc0
 operator|.
 name|p_addr
