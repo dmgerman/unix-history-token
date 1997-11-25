ac9877f1566588e4dd75610b60f66089279cd0eb
Julian Elischer <julian@FreeBSD.org>
1997-11-25 07:07:48 +0000

    Shift a few SYSINT() calls around. this results in a few functions becoming static, and the SYSINITs being close to the code they are related to. setting up the dump device is with dumpsys() and kicking off the scheduler is with the scheduler. Mounting root is with the code that does it.
    Reviewed by: phk
    
    
    Former-commit-id: 361b3fb3053dd2e50406328dd7c8630e0406a864

--- sys/kern/vfs_conf.c
+++ sys/kern/vfs_conf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  * Copyright (c) 1995 Artisoft, Inc.  All Rights Reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_conf.c	8.8 (Berkeley) 3/31/94  * $Id: vfs_conf.c,v 1.16 1997/10/16 07:32:14 julian Exp $  */
+comment|/*  * Copyright (c) 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  * Copyright (c) 1995 Artisoft, Inc.  All Rights Reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_conf.c	8.8 (Berkeley) 3/31/94  * $Id: vfs_conf.c,v 1.17 1997/11/07 08:53:10 phk Exp $  */
 end_comment
 
 begin_comment
@@ -16,6 +16,12 @@ end_include
 begin_comment
 comment|/* dev_t (types.h)*/
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/kernel.h>
+end_include
 
 begin_include
 include|#
@@ -144,6 +150,7 @@ comment|/*  * vfs_mountrootfs  *  * Common entry point for root mounts  *  * PAR
 end_comment
 
 begin_function
+specifier|static
 name|int
 name|vfs_mountrootfs
 parameter_list|(
@@ -291,6 +298,109 @@ operator|)
 return|;
 block|}
 end_function
+
+begin_comment
+comment|/* ARGSUSED*/
+end_comment
+
+begin_decl_stmt
+specifier|static
+name|void
+name|xxx_vfs_mountroot
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|*
+name|fsnamep
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|BOOTP
+end_ifdef
+
+begin_decl_stmt
+specifier|extern
+name|void
+name|bootpc_init
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_function
+specifier|static
+name|void
+name|xxx_vfs_mountroot
+parameter_list|(
+name|fsnamep
+parameter_list|)
+name|void
+modifier|*
+name|fsnamep
+decl_stmt|;
+block|{
+comment|/* XXX Add a separate SYSINIT entry */
+ifdef|#
+directive|ifdef
+name|BOOTP
+name|bootpc_init
+argument_list|()
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* Mount the root file system. */
+if|if
+condition|(
+name|vfs_mountrootfs
+argument_list|(
+operator|*
+operator|(
+operator|(
+name|char
+operator|*
+operator|*
+operator|)
+name|fsnamep
+operator|)
+argument_list|)
+condition|)
+name|panic
+argument_list|(
+literal|"cannot mount root"
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
+begin_macro
+name|SYSINIT
+argument_list|(
+argument|mountroot
+argument_list|,
+argument|SI_SUB_MOUNT_ROOT
+argument_list|,
+argument|SI_ORDER_FIRST
+argument_list|,
+argument|xxx_vfs_mountroot
+argument_list|,
+argument|&mountrootfsname
+argument_list|)
+end_macro
 
 end_unit
 
