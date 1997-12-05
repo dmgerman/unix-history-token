d101c058d5ae7adc0a84bbc208ce04082ebf52ce
Bruce Evans <bde@FreeBSD.org>
1997-12-05 19:14:36 +0000

    Removed one `const' from the declaration of `ks_shortdesc'.  The pointer isn't actually const in vmstat.
    Fixed pedantic syntax errors caused by trailing semicolons in macro
    definitions.
    
    Fixed style bugs and typos in revisions 1.26-1.33.
    
    
    Former-commit-id: 8da287e0b463d8560ef6f7d3a17efe564620e8ab

--- sys/sys/malloc.h
+++ sys/sys/malloc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1987, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)malloc.h	8.5 (Berkeley) 5/3/95  * $Id: malloc.h,v 1.33 1997/12/05 05:36:49 dyson Exp $  */
+comment|/*  * Copyright (c) 1987, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)malloc.h	8.5 (Berkeley) 5/3/95  * $Id: malloc.h,v 1.34 1997/12/05 18:58:13 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -66,7 +66,7 @@ name|malloc_type
 modifier|*
 name|ks_next
 decl_stmt|;
-comment|/* Next pointer */
+comment|/* next in list */
 name|long
 name|ks_memuse
 decl_stmt|;
@@ -94,14 +94,13 @@ comment|/* maximum number ever used */
 name|u_long
 name|ks_magic
 decl_stmt|;
-comment|/* If if's not magic, don't touch it */
+comment|/* if it's not magic, don't touch it */
 specifier|const
 name|char
 modifier|*
-specifier|const
 name|ks_shortdesc
 decl_stmt|;
-comment|/* Short description */
+comment|/* short description */
 name|u_short
 name|ks_limblocks
 decl_stmt|;
@@ -126,7 +125,7 @@ parameter_list|,
 name|longdesc
 parameter_list|)
 define|\
-value|struct malloc_type type[1] = { { NULL, 0, 0, 0, 0, 0, 0, \ 					 M_MAGIC, shortdesc, 0, 0 } }; \ 	struct __hack
+value|struct malloc_type type[1] = { \ 		{ NULL, 0, 0, 0, 0, 0, 0, M_MAGIC, shortdesc, 0, 0 } \ 	}; \ 	struct __hack
 end_define
 
 begin_define
@@ -158,7 +157,7 @@ parameter_list|,
 name|longdesc
 parameter_list|)
 define|\
-value|MALLOC_DEFINE(type, shortdesc, longdesc);
+value|MALLOC_DEFINE(type, shortdesc, longdesc)
 end_define
 
 begin_else
@@ -178,7 +177,7 @@ parameter_list|,
 name|longdesc
 parameter_list|)
 define|\
-value|MALLOC_DECLARE(type);
+value|MALLOC_DECLARE(type)
 end_define
 
 begin_endif
