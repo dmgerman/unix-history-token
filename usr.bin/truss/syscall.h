9fab3fbe87a7228148312c938d7d9e7f8b4de74e
Sean Eric Fagan <sef@FreeBSD.org>
1997-12-06 06:51:14 +0000

    First cut at printing out ioctl names intelligently.  Note that this doesn't handle linux ioctls (yet?).  This uses the mkioctl script from kdump, bless its little heart.
    Reviewed by:	Mike Smith
    
    
    Former-commit-id: 6be0de99c435a7bb6787f14c1ea3db5e5028f4ff

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * System call arguments come in several flavours:  * Hex -- values that should be printed in hex (addresses)  * Octal -- Same as above, but octal  * Int -- normal integer values (file descriptors, for example)  * String -- pointers to sensible data.  Note that we treat read() and  *	write() arguments as such, even though they may *not* be  *	printable data.  * Ptr -- pointer to some specific structure.  Just print as hex for now.  * Quad -- a double-word value.  e.g., lseek(int, offset_t, int)  * Stat -- a pointer to a stat buffer.  Currently unused.  *  * In addition, the pointer types (String, Ptr) may have OUT masked in --  * this means that the data is set on *return* from the system call -- or  * IN (meaning that the data is passed *into* the system call).  */
+comment|/*  * System call arguments come in several flavours:  * Hex -- values that should be printed in hex (addresses)  * Octal -- Same as above, but octal  * Int -- normal integer values (file descriptors, for example)  * String -- pointers to sensible data.  Note that we treat read() and  *	write() arguments as such, even though they may *not* be  *	printable data.  * Ptr -- pointer to some specific structure.  Just print as hex for now.  * Quad -- a double-word value.  e.g., lseek(int, offset_t, int)  * Stat -- a pointer to a stat buffer.  Currently unused.  * Ioctl -- an ioctl command.  Woefully limited.  *  * In addition, the pointer types (String, Ptr) may have OUT masked in --  * this means that the data is set on *return* from the system call -- or  * IN (meaning that the data is passed *into* the system call).  */
 end_comment
 
 begin_comment
-comment|/*  * $Id$  */
+comment|/*  * $Id: syscall.h,v 1.1 1997/12/06 05:23:07 sef Exp $  */
 end_comment
 
 begin_enum
@@ -26,6 +26,8 @@ block|,
 name|Ptr
 block|,
 name|Stat
+block|,
+name|Ioctl
 block|,
 name|Quad
 block|}
