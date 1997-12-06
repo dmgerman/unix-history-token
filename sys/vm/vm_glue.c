6848b19c4d5c1aa0348e19fdf03140221e24b531
John Dyson <dyson@FreeBSD.org>
1997-12-06 02:23:36 +0000

    Support an optional, sysctl enabled feature of idle process swapout.  This is apparently useful for large shell systems, or systems  with long running idle processes.  To enable the feature:
    	sysctl -w vm.swap_idle_enabled=1
    
    Please note that some of the other vm sysctl variables have been renamed
    to be more accurate.
    Submitted by:	Much of it from Matt Dillon <dillon@best.net>
    
    
    Former-commit-id: da1097d5b36593024565df4d824561b66d34f4bd

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_glue.c	8.6 (Berkeley) 1/5/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_glue.c,v 1.66 1997/09/01 03:17:16 bde Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_glue.c	8.6 (Berkeley) 1/5/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_glue.c,v 1.67 1997/11/07 08:53:42 phk Exp $  */
 end_comment
 
 begin_include
@@ -49,6 +49,12 @@ begin_include
 include|#
 directive|include
 file|<sys/vmmeter.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/sysctl.h>
 end_include
 
 begin_include
@@ -1227,13 +1233,82 @@ value|(((p)->p_lock == 0)&& \ 		((p)->p_flag& (P_TRACED|P_NOSWAP|P_SYSTEM|P_INME
 end_define
 
 begin_comment
+comment|/*  * Swap_idle_threshold1 is the guaranteed swapped in time for a process  */
+end_comment
+
+begin_decl_stmt
+name|int
+name|swap_idle_threshold1
+init|=
+literal|2
+decl_stmt|;
+end_decl_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_vm
+argument_list|,
+name|OID_AUTO
+argument_list|,
+name|swap_idle_threshold1
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|swap_idle_threshold1
+argument_list|,
+literal|0
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_comment
+comment|/*  * Swap_idle_threshold2 is the time that a process can be idle before  * it will be swapped out, if idle swapping is enabled.  */
+end_comment
+
+begin_decl_stmt
+name|int
+name|swap_idle_threshold2
+init|=
+literal|10
+decl_stmt|;
+end_decl_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_vm
+argument_list|,
+name|OID_AUTO
+argument_list|,
+name|swap_idle_threshold2
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|swap_idle_threshold2
+argument_list|,
+literal|0
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_comment
 comment|/*  * Swapout is driven by the pageout daemon.  Very simple, we find eligible  * procs and unwire their u-areas.  We try to always "swap" at least one  * process in case we need the room for a swapin.  * If any procs have been sleeping/stopped for at least maxslp seconds,  * they are swapped.  Else, we swap the longest-sleeping or stopped process,  * if any, otherwise the longest-resident process.  */
 end_comment
 
 begin_function
 name|void
 name|swapout_procs
-parameter_list|()
+parameter_list|(
+name|int
+name|action
+parameter_list|)
 block|{
 specifier|register
 name|struct
@@ -1341,7 +1416,7 @@ operator|==
 name|RTP_PRIO_REALTIME
 condition|)
 continue|continue;
-comment|/* 			 * do not swapout a process waiting on a critical 			 * event of some kind 			 */
+comment|/* 			 * Do not swapout a process waiting on a critical 			 * event of some kind.  Also guarantee swap_idle_threshold1 			 * time in memory. 			 */
 if|if
 condition|(
 operator|(
@@ -1360,8 +1435,42 @@ operator|(
 name|p
 operator|->
 name|p_slptime
-operator|<=
-literal|10
+operator|<
+name|swap_idle_threshold1
+operator|)
+condition|)
+continue|continue;
+comment|/* 			 * If the system is under memory stress, or if we are swapping 			 * idle processes>= swap_idle_threshold2, then swap the process 			 * out. 			 */
+if|if
+condition|(
+operator|(
+operator|(
+name|action
+operator|&
+name|VM_SWAP_NORMAL
+operator|)
+operator|==
+literal|0
+operator|)
+operator|&&
+operator|(
+operator|(
+operator|(
+name|action
+operator|&
+name|VM_SWAP_IDLE
+operator|)
+operator|==
+literal|0
+operator|)
+operator|||
+operator|(
+name|p
+operator|->
+name|p_slptime
+operator|<
+name|swap_idle_threshold2
+operator|)
 operator|)
 condition|)
 continue|continue;
@@ -1428,6 +1537,31 @@ name|vm_map
 argument_list|)
 expr_stmt|;
 comment|/* 			 * If the process has been asleep for awhile and had 			 * most of its pages taken away already, swap it out. 			 */
+if|if
+condition|(
+operator|(
+name|action
+operator|&
+name|VM_SWAP_NORMAL
+operator|)
+operator|||
+operator|(
+operator|(
+name|action
+operator|&
+name|VM_SWAP_IDLE
+operator|)
+operator|&&
+operator|(
+name|p
+operator|->
+name|p_slptime
+operator|>
+name|swap_idle_threshold2
+operator|)
+operator|)
+condition|)
+block|{
 name|swapout
 argument_list|(
 name|p
@@ -1452,6 +1586,7 @@ expr_stmt|;
 goto|goto
 name|retry
 goto|;
+block|}
 block|}
 block|}
 comment|/* 	 * If we swapped something out, and another process needed memory, 	 * then wakeup the sched process. 	 */
