c5dd08c88f6028bbb0f086878d93f419b2764d0a
Wolfgang Helbig <helbig@FreeBSD.org>
1997-12-07 19:04:14 +0000

    Added easterog() and easteroj() which compute orthodox easter for Gregorian and Julian Calendar. Suggested by: Andrey
    Former-commit-id: d9b5c0bead67e1a64b05a631a456787497a96097

--- lib/libcalendar/easter.c
+++ lib/libcalendar/easter.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Wolfgang Helbig  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1997 Wolfgang Helbig  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: easter.c,v 1.1.1.1 1997/12/04 10:41:49 helbig Exp $  */
 end_comment
 
 begin_include
@@ -8,6 +8,17 @@ include|#
 directive|include
 file|"calendar.h"
 end_include
+
+begin_function_decl
+specifier|static
+name|int
+name|easterodn
+parameter_list|(
+name|int
+name|y
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_comment
 comment|/* Compute Easter Sunday in Gregorian Calendar */
@@ -202,13 +213,13 @@ block|}
 end_function
 
 begin_comment
-comment|/* Compute Easter Sunday in Julian Calendar */
+comment|/* Compute the Gregorian date of Easter Sunday in Julian Calendar */
 end_comment
 
 begin_function
 name|date
 modifier|*
-name|easterj
+name|easterog
 parameter_list|(
 name|int
 name|y
@@ -216,6 +227,68 @@ parameter_list|,
 name|date
 modifier|*
 name|dt
+parameter_list|)
+block|{
+return|return
+operator|(
+name|gdate
+argument_list|(
+name|easterodn
+argument_list|(
+name|y
+argument_list|)
+argument_list|,
+name|dt
+argument_list|)
+operator|)
+return|;
+block|}
+end_function
+
+begin_comment
+comment|/* Compute the Julian date of Easter Sunday in Julian Calendar */
+end_comment
+
+begin_function
+name|date
+modifier|*
+name|easteroj
+parameter_list|(
+name|int
+name|y
+parameter_list|,
+name|date
+modifier|*
+name|dt
+parameter_list|)
+block|{
+return|return
+operator|(
+name|jdate
+argument_list|(
+name|easterodn
+argument_list|(
+name|y
+argument_list|)
+argument_list|,
+name|dt
+argument_list|)
+operator|)
+return|;
+block|}
+end_function
+
+begin_comment
+comment|/* Compute the day number of Easter Sunday in Julian Calendar */
+end_comment
+
+begin_function
+specifier|static
+name|int
+name|easterodn
+parameter_list|(
+name|int
+name|y
 parameter_list|)
 block|{
 comment|/* 	 * Table for the easter limits in one metonic (19-year) cycle. 21 	 * to 31 is in March, 1 through 18 in April. Easter is the first 	 * sunday after the easter limit. 	 */
@@ -284,12 +357,15 @@ block|,
 literal|7
 block|}
 decl_stmt|;
+name|date
+name|dt
+decl_stmt|;
 name|int
 name|dn
 decl_stmt|;
-comment|/* Assign the easter limit of y to *dt */
+comment|/* Assign the easter limit of y to dt */
 name|dt
-operator|->
+operator|.
 name|d
 operator|=
 name|mc
@@ -302,40 +378,43 @@ expr_stmt|;
 if|if
 condition|(
 name|dt
-operator|->
+operator|.
 name|d
 operator|<
 literal|21
 condition|)
 name|dt
-operator|->
+operator|.
 name|m
 operator|=
 literal|4
 expr_stmt|;
 else|else
 name|dt
-operator|->
+operator|.
 name|m
 operator|=
 literal|3
 expr_stmt|;
 name|dt
-operator|->
+operator|.
 name|y
 operator|=
 name|y
 expr_stmt|;
-comment|/* Compute the next sunday after the easter limit */
+comment|/* Return the next sunday after the easter limit */
 name|dn
 operator|=
 name|ndaysj
 argument_list|(
+operator|&
 name|dt
 argument_list|)
 expr_stmt|;
+return|return
+operator|(
 name|dn
-operator|+=
+operator|+
 name|ns
 index|[
 name|weekday
@@ -343,15 +422,6 @@ argument_list|(
 name|dn
 argument_list|)
 index|]
-expr_stmt|;
-return|return
-operator|(
-name|jdate
-argument_list|(
-name|dn
-argument_list|,
-name|dt
-argument_list|)
 operator|)
 return|;
 block|}
