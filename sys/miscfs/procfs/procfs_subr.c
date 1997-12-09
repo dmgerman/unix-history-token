2abed1e00340dc627482f04d7ec7159f25e7ba8b
Sean Eric Fagan <sef@FreeBSD.org>
1997-12-09 05:03:41 +0000

    Code to prevent a panic caused by procfs_exit().  Note that i don't know what is teh root cause -- but, sometimes, a procfs vnode in pfshead is apparantly corrupt (or a UFS vnode instead).  Without this patch, I can get it to panic by doing (in csh)
    	while (1)
    		ps auxwww
    	end
    
    and it will panic when the PID's wrap.  With it, it does not panic.
    Yes -- I know that this is NOT the right way to fix it.  But I haven't
    been able to get it to panic yet (which confuses me).  I am going to
    be looking into the vgone() code now, as that may be a part of it.
    
    
    Former-commit-id: 1f4541c55382ee63bb0bee5ac7ff9ecfe75707ea

--- sys/miscfs/procfs/procfs_subr.c
+++ sys/miscfs/procfs/procfs_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Jan-Simon Pendry  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)procfs_subr.c	8.6 (Berkeley) 5/14/95  *  *	$Id: procfs_subr.c,v 1.18 1997/12/06 04:11:12 sef Exp $  */
+comment|/*  * Copyright (c) 1993 Jan-Simon Pendry  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)procfs_subr.c	8.6 (Berkeley) 5/14/95  *  *	$Id: procfs_subr.c,v 1.19 1997/12/08 01:06:22 sef Exp $  */
 end_comment
 
 begin_include
@@ -1277,6 +1277,17 @@ operator|->
 name|pfs_next
 control|)
 block|{
+name|struct
+name|vnode
+modifier|*
+name|vp
+init|=
+name|PFSTOV
+argument_list|(
+name|pfs
+argument_list|)
+decl_stmt|;
+comment|/* 		 * XXX - this is probably over-paranoid here -- 		 * for some reason, occasionally the v_tag is 		 * not VT_PROCFS; this results in a panic.  I'm 		 * not sure *why* that is happening. 		 */
 if|if
 condition|(
 name|pfs
@@ -1284,13 +1295,20 @@ operator|->
 name|pfs_pid
 operator|==
 name|pid
+operator|&&
+name|vp
+operator|->
+name|v_usecount
+operator|&&
+name|vp
+operator|->
+name|v_tag
+operator|==
+name|VT_PROCFS
 condition|)
 name|vgone
 argument_list|(
-name|PFSTOV
-argument_list|(
-name|pfs
-argument_list|)
+name|vp
 argument_list|)
 expr_stmt|;
 block|}
