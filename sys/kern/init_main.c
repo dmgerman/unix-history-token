f6804d96783604adcb55846063c27911457d9dd8
John Dyson <dyson@FreeBSD.org>
1997-12-12 04:00:59 +0000

    We have had support for running the kernel daemons as threads for quite a while, but forgot to do so.  For now, this code supports most daemons  running as kernel threads in UP kernels, and as full processes in SMP.  We will soon be able to run them as threads in SMP, but not yet.
    Former-commit-id: 4a08202a4538b02031f0f6c37afedb929e878ece

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995 Terrence R. Lambert  * All rights reserved.  *  * Copyright (c) 1982, 1986, 1989, 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)init_main.c	8.9 (Berkeley) 1/21/94  * $Id: init_main.c,v 1.76 1997/11/25 07:07:41 julian Exp $  */
+comment|/*  * Copyright (c) 1995 Terrence R. Lambert  * All rights reserved.  *  * Copyright (c) 1982, 1986, 1989, 1991, 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)init_main.c	8.9 (Berkeley) 1/21/94  * $Id: init_main.c,v 1.77 1997/12/06 04:11:09 sef Exp $  */
 end_comment
 
 begin_include
@@ -97,6 +97,12 @@ begin_include
 include|#
 directive|include
 file|<sys/vmmeter.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/unistd.h>
 end_include
 
 begin_include
@@ -685,15 +691,76 @@ break|break;
 case|case
 name|SI_TYPE_KTHREAD
 case|:
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|SMP
+argument_list|)
 comment|/* kernel thread*/
 if|if
 condition|(
-name|fork
+name|fork1
 argument_list|(
 operator|&
 name|proc0
 argument_list|,
-name|NULL
+name|RFMEM
+operator||
+name|RFFDG
+operator||
+name|RFPROC
+argument_list|)
+condition|)
+name|panic
+argument_list|(
+literal|"fork kernel thread"
+argument_list|)
+expr_stmt|;
+name|cpu_set_fork_handler
+argument_list|(
+name|pfind
+argument_list|(
+name|proc0
+operator|.
+name|p_retval
+index|[
+literal|0
+index|]
+argument_list|)
+argument_list|,
+operator|(
+operator|*
+name|sipp
+operator|)
+operator|->
+name|func
+argument_list|,
+operator|(
+operator|*
+name|sipp
+operator|)
+operator|->
+name|udata
+argument_list|)
+expr_stmt|;
+break|break;
+endif|#
+directive|endif
+case|case
+name|SI_TYPE_KPROCESS
+case|:
+if|if
+condition|(
+name|fork1
+argument_list|(
+operator|&
+name|proc0
+argument_list|,
+name|RFFDG
+operator||
+name|RFPROC
 argument_list|)
 condition|)
 name|panic
@@ -1842,7 +1909,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_macro
-name|SYSINIT_KT
+name|SYSINIT_KP
 argument_list|(
 argument|init
 argument_list|,
