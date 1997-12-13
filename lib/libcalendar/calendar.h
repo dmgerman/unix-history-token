ceafa358a419553a1c9bb30aae812d7260a7b06c
Wolfgang Helbig <helbig@FreeBSD.org>
1997-12-13 11:51:16 +0000

    Delete "typedef ... date" (see style(9)). In the man page Use ".Pp" instead of blank lines, adopt English and stress that the Julian->Gregorian switch took place at different dates in different countries. Suggested by: Garrett.
    Former-commit-id: fed32592efe11e8c6a39669b0637279ba0b67a47

--- lib/libcalendar/calendar.h
+++ lib/libcalendar/calendar.h
@@ -1,10 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Wolfgang Helbig  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: calendar.h,v 1.1.1.1 1997/12/04 10:41:49 helbig Exp $  */
+comment|/*-  * Copyright (c) 1997 Wolfgang Helbig  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: calendar.h,v 1.2 1997/12/07 19:04:11 helbig Exp $  */
 end_comment
 
-begin_typedef
-typedef|typedef
+begin_struct
 struct|struct
 name|date
 block|{
@@ -21,11 +20,11 @@ name|d
 decl_stmt|;
 comment|/* day */
 block|}
-name|date
-typedef|;
-end_typedef
+struct|;
+end_struct
 
 begin_function_decl
+name|struct
 name|date
 modifier|*
 name|easterg
@@ -33,6 +32,7 @@ parameter_list|(
 name|int
 name|_year
 parameter_list|,
+name|struct
 name|date
 modifier|*
 name|_dt
@@ -41,6 +41,7 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|struct
 name|date
 modifier|*
 name|easterog
@@ -48,6 +49,7 @@ parameter_list|(
 name|int
 name|_year
 parameter_list|,
+name|struct
 name|date
 modifier|*
 name|_dt
@@ -56,6 +58,7 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|struct
 name|date
 modifier|*
 name|easteroj
@@ -63,6 +66,7 @@ parameter_list|(
 name|int
 name|_year
 parameter_list|,
+name|struct
 name|date
 modifier|*
 name|_dt
@@ -71,6 +75,7 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|struct
 name|date
 modifier|*
 name|gdate
@@ -78,6 +83,7 @@ parameter_list|(
 name|int
 name|_nd
 parameter_list|,
+name|struct
 name|date
 modifier|*
 name|_dt
@@ -86,6 +92,7 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|struct
 name|date
 modifier|*
 name|jdate
@@ -93,6 +100,7 @@ parameter_list|(
 name|int
 name|_nd
 parameter_list|,
+name|struct
 name|date
 modifier|*
 name|_dt
@@ -104,6 +112,7 @@ begin_function_decl
 name|int
 name|ndaysg
 parameter_list|(
+name|struct
 name|date
 modifier|*
 name|_dt
@@ -115,6 +124,7 @@ begin_function_decl
 name|int
 name|ndaysj
 parameter_list|(
+name|struct
 name|date
 modifier|*
 name|_dt
