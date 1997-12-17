7da7f00c018d567b37e2feea7cea49051c4bbd9f
Brian Somers <brian@FreeBSD.org>
1997-12-17 21:21:47 +0000

    If we've got a descriptor table that's bigger than an fd_set, make sure that all descriptors >2 are closed when we start - otherwise we're asking for a dump in FD_SET().
    Problem pointed out by: Theo de Raadt <deraadt@cvs.openbsd.org>
    
    
    Former-commit-id: a3e57c2488e7b497e1ccf733bd68369b14fb3abf

--- usr.sbin/ppp/main.c
+++ usr.sbin/ppp/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.106 1997/12/03 10:23:50 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
+comment|/*  *			User Process PPP  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: main.c,v 1.107 1997/12/13 02:37:27 brian Exp $  *  *	TODO:  *		o Add commands for traffic summary, version display, etc.  *		o Add signal handler for misc controls.  */
 end_comment
 
 begin_include
@@ -1672,6 +1672,33 @@ decl_stmt|,
 modifier|*
 name|label
 decl_stmt|;
+name|int
+name|nfds
+decl_stmt|;
+name|nfds
+operator|=
+name|getdtablesize
+argument_list|()
+expr_stmt|;
+if|if
+condition|(
+name|nfds
+operator|>=
+name|FD_SETSIZE
+condition|)
+comment|/*      * If we've got loads of file descriptors, make sure they're all      * closed.  If they aren't, we may end up with a seg fault when our      * `fd_set's get too big when select()ing !      */
+while|while
+condition|(
+operator|--
+name|nfds
+operator|>
+literal|2
+condition|)
+name|close
+argument_list|(
+name|nfds
+argument_list|)
+expr_stmt|;
 name|VarTerm
 operator|=
 literal|0
