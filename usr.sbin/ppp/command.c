cbdc33b48ed63e4cfb524f54adccca89d0408993
Brian Somers <brian@FreeBSD.org>
1997-12-19 18:11:05 +0000

    Reset our "ifaddr" to zero if the "set ifaddr" fails in -auto mode.  This makes ppp correctly exit if you kick it off twice in auto mode with the same label (read: interface).
    Former-commit-id: de41471db5df457e968bad0f144fd9b2d62f6799

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.113 1997/12/18 00:28:35 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.114 1997/12/18 01:10:13 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -6889,9 +6889,25 @@ argument_list|)
 operator|<
 literal|0
 condition|)
+block|{
+name|DefMyAddress
+operator|.
+name|ipaddr
+operator|.
+name|s_addr
+operator|=
+name|DefHisAddress
+operator|.
+name|ipaddr
+operator|.
+name|s_addr
+operator|=
+literal|0L
+expr_stmt|;
 return|return
 literal|4
 return|;
+block|}
 block|}
 return|return
 literal|0
