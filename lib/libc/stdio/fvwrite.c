d412e22cb1bef5245985bd3facc6d3f6f83e85ca
Andrey Chernov <ache@FreeBSD.org>
1997-12-24 13:17:13 +0000

    Remove wrong comment about snprintf: "return the number of bytes needed, rather the number used"
    According to Single Unix specs:
    
    Upon successful completion, these functions return the number of bytes
    transmitted excluding the terminating null
    
    
    Former-commit-id: ae3afd963b74232cac17bba59d99e0fe8f367194

--- lib/libc/stdio/fvwrite.c
+++ lib/libc/stdio/fvwrite.c
@@ -37,7 +37,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id$"
+literal|"$Id: fvwrite.c,v 1.5 1997/02/22 15:02:08 peter Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -289,7 +289,7 @@ operator|==
 literal|0
 condition|)
 block|{
-comment|/* 		 * Fully buffered: fill partially full buffer, if any, 		 * and then flush.  If there is no partial buffer, write 		 * one _bf._size byte chunk directly (without copying). 		 * 		 * String output is a special case: write as many bytes 		 * as fit, but pretend we wrote everything.  This makes 		 * snprintf() return the number of bytes needed, rather 		 * than the number used, and avoids its write function 		 * (so that the write function can be invalid). 		 */
+comment|/* 		 * Fully buffered: fill partially full buffer, if any, 		 * and then flush.  If there is no partial buffer, write 		 * one _bf._size byte chunk directly (without copying). 		 * 		 * String output is a special case: write as many bytes 		 * as fit, but pretend we wrote everything.  This avoids 		 * snprintf() write function (so that the write function 		 * can be invalid). 		 */
 do|do
 block|{
 name|GETIOV
