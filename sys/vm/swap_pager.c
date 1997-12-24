c0cea511b19ff47737d52ea54a79bb2ca31f9fa1
John Dyson <dyson@FreeBSD.org>
1997-12-24 15:05:25 +0000

    Support running with inadequate swap space.  Additionally, the code will complain with a suggestion of increasing it.
    Former-commit-id: 3bfba2e600d258c9211bab9e0f33ba8ac19cd853

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) 3/21/94  * $Id: swap_pager.c,v 1.78 1997/09/01 03:17:13 bde Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * Copyright (c) 1990 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: swap_pager.c 1.4 91/04/30$  *  *	@(#)swap_pager.c	8.9 (Berkeley) 3/21/94  * $Id: swap_pager.c,v 1.79 1997/12/02 21:07:19 phk Exp $  */
 end_comment
 
 begin_comment
@@ -150,6 +150,15 @@ end_decl_stmt
 begin_decl_stmt
 specifier|static
 name|int
+name|suggest_more_swap
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|int
 name|no_swap_space
 init|=
 literal|1
@@ -167,7 +176,7 @@ begin_define
 define|#
 directive|define
 name|MAX_PAGEOUT_CLUSTER
-value|16
+value|8
 end_define
 
 begin_expr_stmt
@@ -1596,6 +1605,51 @@ name|vm_swap_size
 operator|-=
 name|amount
 expr_stmt|;
+if|if
+condition|(
+operator|!
+name|suggest_more_swap
+operator|&&
+operator|(
+name|vm_swap_size
+operator|<
+name|btodb
+argument_list|(
+name|cnt
+operator|.
+name|v_page_count
+operator|*
+name|PAGE_SIZE
+argument_list|)
+operator|)
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|"swap_pager: suggest more swap space: %d MB\n"
+argument_list|,
+operator|(
+literal|2
+operator|*
+name|cnt
+operator|.
+name|v_page_count
+operator|*
+operator|(
+name|PAGE_SIZE
+operator|/
+literal|1024
+operator|)
+operator|)
+operator|/
+literal|1000
+argument_list|)
+expr_stmt|;
+name|suggest_more_swap
+operator|=
+literal|1
+expr_stmt|;
+block|}
 if|if
 condition|(
 operator|!
@@ -4836,6 +4890,25 @@ comment|/* 			 * If we're out of swap space, then attempt to free 			 * some whe
 if|if
 condition|(
 name|SWAPLOW
+operator|||
+operator|(
+name|vm_swap_size
+operator|<
+name|btodb
+argument_list|(
+operator|(
+name|cnt
+operator|.
+name|v_page_count
+operator|-
+name|cnt
+operator|.
+name|v_wire_count
+operator|)
+argument_list|)
+operator|*
+name|PAGE_SIZE
+operator|)
 condition|)
 block|{
 for|for
@@ -7141,7 +7214,14 @@ argument_list|(
 name|bp
 argument_list|)
 expr_stmt|;
-comment|/* 	if (bp->b_flags& B_WANTED) */
+if|if
+condition|(
+name|bp
+operator|->
+name|b_flags
+operator|&
+name|B_WANTED
+condition|)
 name|wakeup
 argument_list|(
 name|bp
