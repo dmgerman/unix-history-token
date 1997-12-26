33e990c479504d42ac1042af246cc8367560cff0
Alexander Langer <alex@FreeBSD.org>
1997-12-26 03:24:26 +0000

    Put the return value of getopt into an int, not a char.
    Former-commit-id: 781b9287f7eb01136448604475db08e72e942764

--- sbin/ipfw/ipfw.c
+++ sbin/ipfw/ipfw.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 Alex Nash, Paul Traina, Poul-Henning Kamp  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Idea and grammar partially left from:  * Copyright (c) 1993 Daniel Boulet  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  * NEW command line interface for IP firewall facility  *  * $Id: ipfw.c,v 1.47 1997/08/08 14:36:28 alex Exp $  *  */
+comment|/*  * Copyright (c) 1996 Alex Nash, Paul Traina, Poul-Henning Kamp  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Idea and grammar partially left from:  * Copyright (c) 1993 Daniel Boulet  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  * NEW command line interface for IP firewall facility  *  * $Id: ipfw.c,v 1.48 1997/12/05 02:43:26 julian Exp $  *  */
 end_comment
 
 begin_include
@@ -6497,7 +6497,7 @@ modifier|*
 name|av
 decl_stmt|;
 block|{
-name|char
+name|int
 name|ch
 decl_stmt|;
 specifier|extern
