4166775e4e5f48476d916af8ee19e3b4a5a56cd5
Bruce Evans <bde@FreeBSD.org>
1997-12-28 01:01:13 +0000

    Restored used include of <sys/malloc.h>.  malloc() is not used here, but kmem_malloc() is used and it takes the same "flags" as malloc().
    Use the mbuf allocation "flags" M_WAIT and M_DONTWAIT consistently.
    There is really only one boolean flag, M_DONTWAIT, but the "flags"
    were always treated as enum-like values, except in some places here
    where the values are tacitly converted to boolean flags.  Treat
    them as enum-like values everywhere, except where we tacitly assume
    that there are only two values in order to convert them to the
    corresponding two kmem_malloc() "flags".
    
    
    Former-commit-id: 852f5386a62faba0986ba54c214db1e72bede889

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_mbuf.c	8.2 (Berkeley) 1/4/94  *	$Id: uipc_mbuf.c,v 1.31 1997/10/12 20:24:10 phk Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_mbuf.c	8.2 (Berkeley) 1/4/94  *	$Id: uipc_mbuf.c,v 1.32 1997/10/28 15:58:22 bde Exp $  */
 end_comment
 
 begin_include
@@ -13,6 +13,12 @@ begin_include
 include|#
 directive|include
 file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/malloc.h>
 end_include
 
 begin_include
@@ -394,7 +400,7 @@ name|m_clalloc
 argument_list|(
 literal|16
 argument_list|,
-literal|0
+name|M_WAIT
 argument_list|)
 operator|==
 literal|0
@@ -434,14 +440,14 @@ name|m_mballoc
 parameter_list|(
 name|nmb
 parameter_list|,
-name|nowait
+name|how
 parameter_list|)
 specifier|register
 name|int
 name|nmb
 decl_stmt|;
 name|int
-name|nowait
+name|how
 decl_stmt|;
 block|{
 specifier|register
@@ -494,8 +500,9 @@ name|p
 operator|==
 literal|0
 operator|&&
-operator|!
-name|nowait
+name|how
+operator|==
+name|M_WAIT
 condition|)
 block|{
 name|mbstat
@@ -518,7 +525,7 @@ name|M_WAITOK
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * Either the map is now full, or this is nowait and there 	 * are no pages left. 	 */
+comment|/* 	 * Either the map is now full, or `how' is M_NOWAIT and there 	 * are no pages left. 	 */
 if|if
 condition|(
 name|p
@@ -649,7 +656,7 @@ name|m_clalloc
 argument_list|(
 literal|1
 argument_list|,
-literal|0
+name|M_WAIT
 argument_list|)
 operator|==
 literal|0
@@ -726,14 +733,14 @@ name|m_clalloc
 parameter_list|(
 name|ncl
 parameter_list|,
-name|nowait
+name|how
 parameter_list|)
 specifier|register
 name|int
 name|ncl
 decl_stmt|;
 name|int
-name|nowait
+name|how
 decl_stmt|;
 block|{
 specifier|register
@@ -771,7 +778,9 @@ operator|>
 name|PAGE_SIZE
 if|if
 condition|(
-name|nowait
+name|how
+operator|!=
+name|M_WAIT
 condition|)
 block|{
 name|i_want_my_mcl
@@ -841,7 +850,9 @@ argument_list|(
 name|npg
 argument_list|)
 argument_list|,
-name|nowait
+name|how
+operator|!=
+name|M_WAIT
 condition|?
 name|M_NOWAIT
 else|:
@@ -858,7 +869,7 @@ name|MCLBYTES
 expr_stmt|;
 endif|#
 directive|endif
-comment|/* 	 * Either the map is now full, or this is nowait and there 	 * are no pages left. 	 */
+comment|/* 	 * Either the map is now full, or `how' is M_NOWAIT and there 	 * are no pages left. 	 */
 if|if
 condition|(
 name|p
@@ -1179,12 +1190,12 @@ name|mbuf
 modifier|*
 name|m_get
 parameter_list|(
-name|nowait
+name|how
 parameter_list|,
 name|type
 parameter_list|)
 name|int
-name|nowait
+name|how
 decl_stmt|,
 name|type
 decl_stmt|;
@@ -1199,7 +1210,7 @@ name|MGET
 argument_list|(
 name|m
 argument_list|,
-name|nowait
+name|how
 argument_list|,
 name|type
 argument_list|)
@@ -1218,12 +1229,12 @@ name|mbuf
 modifier|*
 name|m_gethdr
 parameter_list|(
-name|nowait
+name|how
 parameter_list|,
 name|type
 parameter_list|)
 name|int
-name|nowait
+name|how
 decl_stmt|,
 name|type
 decl_stmt|;
@@ -1238,7 +1249,7 @@ name|MGETHDR
 argument_list|(
 name|m
 argument_list|,
-name|nowait
+name|how
 argument_list|,
 name|type
 argument_list|)
@@ -1257,12 +1268,12 @@ name|mbuf
 modifier|*
 name|m_getclr
 parameter_list|(
-name|nowait
+name|how
 parameter_list|,
 name|type
 parameter_list|)
 name|int
-name|nowait
+name|how
 decl_stmt|,
 name|type
 decl_stmt|;
@@ -1277,7 +1288,7 @@ name|MGET
 argument_list|(
 name|m
 argument_list|,
-name|nowait
+name|how
 argument_list|,
 name|type
 argument_list|)
