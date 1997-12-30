497e25d15760423f6d0cf2d9ffe1b7bd07ed5f31
Brian Somers <brian@FreeBSD.org>
1997-12-30 03:46:58 +0000

    MFC: Make the super scsi device available (/dev/ssc).
    Former-commit-id: e064ebfe0fae7780ddbe80926c1a10db2ca70337

--- sys/scsi/scsiconf.h
+++ sys/scsi/scsiconf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.46.2.1 1997/05/24 22:19:16 jmg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  *	$Id: scsiconf.h,v 1.46.2.2 1997/07/14 12:48:32 kato Exp $  */
 end_comment
 
 begin_ifndef
@@ -2529,8 +2529,10 @@ parameter_list|,
 name|T
 parameter_list|,
 name|L
+parameter_list|,
+name|P
 parameter_list|)
-value|( \          ((B)<< 24) | \          ((T)<< 20) | \          ((L)<< 16) | \          SCSI_FIXED_MASK )
+value|( \          ((B)<< 24) | \          ((T)<< 20) | \          ((L)<< 16) | \          (P) | \          SCSI_FIXED_MASK )
 end_define
 
 begin_endif
