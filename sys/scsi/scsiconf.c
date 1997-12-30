9a642a11534da77c4164824de757d0fe8f6bde26
Julian Elischer <julian@FreeBSD.org>
1997-12-30 04:19:54 +0000

    move punchline of joke up to rest of joke. (got moved by useful edits)
    Former-commit-id: 27bf8c390a18127e0cf2f4dfee3783777c872f4b

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.98 1997/12/20 00:28:49 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.99 1997/12/20 23:05:14 joerg Exp $  */
 end_comment
 
 begin_include
@@ -1706,6 +1706,7 @@ block|,
 name|SC_ONE_LU
 block|}
 block|,
+comment|/* 	 * Doobe-doo-be doooo 	 * -Mary 	 */
 block|{
 name|T_READONLY
 block|,
@@ -1726,7 +1727,6 @@ block|,
 name|CD_Q_NO_START
 block|}
 block|,
-comment|/* 	 * Doobe-doo-be doooo 	 * -Mary 	 */
 block|{
 name|T_READONLY
 block|,
