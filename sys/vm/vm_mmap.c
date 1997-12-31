1f630210ac1b6285781db5c46196d4f6301aefa0
Alexander Langer <alex@FreeBSD.org>
1997-12-31 02:35:29 +0000

    caddr_t --> void *
    Former-commit-id: 036c6f47c7114a731bd3d86b611db1525f63e7d3

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.4 (Berkeley) 1/12/94  * $Id: vm_mmap.c,v 1.69 1997/11/06 19:29:54 phk Exp $  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_mmap.c 1.6 91/10/21$  *  *	@(#)vm_mmap.c	8.4 (Berkeley) 1/12/94  * $Id: vm_mmap.c,v 1.70 1997/12/16 17:40:42 eivind Exp $  */
 end_comment
 
 begin_comment
@@ -370,7 +370,8 @@ begin_struct
 struct|struct
 name|mmap_args
 block|{
-name|caddr_t
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -454,7 +455,8 @@ name|prot
 decl_stmt|,
 name|maxprot
 decl_stmt|;
-name|caddr_t
+name|void
+modifier|*
 name|handle
 decl_stmt|;
 name|int
@@ -888,7 +890,8 @@ expr_stmt|;
 name|handle
 operator|=
 operator|(
-name|caddr_t
+name|void
+operator|*
 operator|)
 name|vp
 expr_stmt|;
@@ -1235,7 +1238,8 @@ begin_struct
 struct|struct
 name|msync_args
 block|{
-name|caddr_t
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|int
@@ -1523,7 +1527,8 @@ begin_struct
 struct|struct
 name|munmap_args
 block|{
-name|caddr_t
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -1770,7 +1775,9 @@ begin_struct
 struct|struct
 name|mprotect_args
 block|{
-name|caddr_t
+specifier|const
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -1957,7 +1964,8 @@ begin_struct
 struct|struct
 name|minherit_args
 block|{
-name|caddr_t
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -2122,7 +2130,8 @@ begin_struct
 struct|struct
 name|madvise_args
 block|{
-name|caddr_t
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -2330,7 +2339,9 @@ begin_struct
 struct|struct
 name|mincore_args
 block|{
-name|caddr_t
+specifier|const
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -2906,7 +2917,9 @@ begin_struct
 struct|struct
 name|mlock_args
 block|{
-name|caddr_t
+specifier|const
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -3226,7 +3239,9 @@ begin_struct
 struct|struct
 name|munlock_args
 block|{
-name|caddr_t
+specifier|const
+name|void
+modifier|*
 name|addr
 decl_stmt|;
 name|size_t
@@ -3415,10 +3430,10 @@ parameter_list|,
 name|int
 name|flags
 parameter_list|,
-name|caddr_t
+name|void
+modifier|*
 name|handle
 parameter_list|,
-comment|/* XXX should be vp */
 name|vm_ooffset_t
 name|foff
 parameter_list|)
@@ -3604,7 +3619,8 @@ expr_stmt|;
 name|handle
 operator|=
 operator|(
-name|caddr_t
+name|void
+operator|*
 operator|)
 name|vp
 operator|->
