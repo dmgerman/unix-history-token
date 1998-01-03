a619e2501d87c85777268d497333a6e4485026f3
John Polstra <jdp@FreeBSD.org>
1998-01-03 05:21:50 +0000

    Merge from main branch 1.47 -> 1.48: accept comments and blank lines.
    Former-commit-id: b9954b3613d1c0f97ef3fa072cb7411f95fa7400

--- sbin/ipfw/ipfw.c
+++ sbin/ipfw/ipfw.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 Alex Nash, Paul Traina, Poul-Henning Kamp  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Idea and grammar partially left from:  * Copyright (c) 1993 Daniel Boulet  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  * NEW command line interface for IP firewall facility  *  * $Id: ipfw.c,v 1.34.2.6 1997/07/25 03:15:04 brian Exp $  *  */
+comment|/*  * Copyright (c) 1996 Alex Nash, Paul Traina, Poul-Henning Kamp  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Idea and grammar partially left from:  * Copyright (c) 1993 Daniel Boulet  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  * NEW command line interface for IP firewall facility  *  * $Id: ipfw.c,v 1.34.2.7 1997/08/21 01:30:21 alex Exp $  *  */
 end_comment
 
 begin_include
@@ -7129,6 +7129,14 @@ index|]
 operator|=
 name|linename
 expr_stmt|;
+if|if
+condition|(
+operator|*
+name|buf
+operator|==
+literal|'#'
+condition|)
+continue|continue;
 for|for
 control|(
 name|i
@@ -7169,6 +7177,13 @@ index|]
 operator|=
 name|a
 expr_stmt|;
+if|if
+condition|(
+name|i
+operator|==
+literal|1
+condition|)
+continue|continue;
 if|if
 condition|(
 name|i
