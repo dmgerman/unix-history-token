f4d0c4ded5c3510d4bee22022497a2adf1ba2c46
Brian Somers <brian@FreeBSD.org>
1998-01-04 20:25:41 +0000

    Initialize CcpInfo protocols to -1 (none, not OUI). Don't Call Term() for an algorithm that hasn't been Init()d.
    Former-commit-id: c4f4b0ef980ad6607cee645c3901ddcf0c8c58b0

--- usr.sbin/ppp/ccp.h
+++ usr.sbin/ppp/ccp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ccp.h,v 1.10 1997/12/03 10:23:45 brian Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ccp.h,v 1.11 1997/12/03 23:27:55 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_define
@@ -162,6 +162,14 @@ name|u_long
 name|my_reject
 decl_stmt|;
 comment|/* Request codes I have rejected */
+name|int
+name|out_init
+decl_stmt|;
+comment|/* Init called for out algorithm */
+name|int
+name|in_init
+decl_stmt|;
+comment|/* Init called for in algorithm */
 name|u_long
 name|uncompout
 decl_stmt|,
