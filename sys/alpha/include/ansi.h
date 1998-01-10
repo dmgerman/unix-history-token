a212428b5e1b02b3da0944cc058deb6987c6c40e
John Birrell <jb@FreeBSD.org>
1998-01-10 10:13:16 +0000

    These are a few of the alpha machine dependent header files - the first referenced by the build of user-space libraries. These files were obtained from NetBSD (with ansi.h being modified to reflect the FreeBSD off_t and pid_t implementation).
    Former-commit-id: d414a2de938d8e6c6a4ee7b295e7c656279c3b33

---
+++ sys/alpha/include/ansi.h
@@ -0,0 +1,243 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $Id$ */
+end_comment
+
+begin_comment
+comment|/* From: NetBSD: ansi.h,v 1.9 1997/11/23 20:20:53 kleink Exp */
+end_comment
+
+begin_comment
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ansi.h	8.2 (Berkeley) 1/4/94  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ANSI_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_ANSI_H_
+end_define
+
+begin_comment
+comment|/*  * Types which are fundamental to the implementation and may appear in  * more than one standard header are defined here.  Standard headers  * then use:  *	#ifdef	_BSD_SIZE_T_  *	typedef	_BSD_SIZE_T_ size_t;  *	#undef	_BSD_SIZE_T_  *	#endif  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_CLOCK_T_
+value|int
+end_define
+
+begin_comment
+comment|/* clock() */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_PTRDIFF_T_
+value|long
+end_define
+
+begin_comment
+comment|/* ptr1 - ptr2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_SIZE_T_
+value|unsigned long
+end_define
+
+begin_comment
+comment|/* sizeof() */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_SSIZE_T_
+value|long
+end_define
+
+begin_comment
+comment|/* byte count or error */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_TIME_T_
+value|int
+end_define
+
+begin_comment
+comment|/* time() */
+end_comment
+
+begin_typedef
+typedef|typedef
+struct|struct
+block|{
+name|char
+modifier|*
+name|__base
+decl_stmt|;
+name|int
+name|__offset
+decl_stmt|;
+name|int
+name|__pad
+decl_stmt|;
+block|}
+name|__va_list
+typedef|;
+end_typedef
+
+begin_define
+define|#
+directive|define
+name|_BSD_VA_LIST_
+value|__va_list
+end_define
+
+begin_comment
+comment|/* va_list */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_CLOCKID_T_
+value|int
+end_define
+
+begin_comment
+comment|/* clockid_t */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_TIMER_T_
+value|int
+end_define
+
+begin_comment
+comment|/* timer_t */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_SUSECONDS_T_
+value|int
+end_define
+
+begin_comment
+comment|/* suseconds_t */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_USECONDS_T_
+value|unsigned int
+end_define
+
+begin_comment
+comment|/* useconds_t */
+end_comment
+
+begin_comment
+comment|/*  * Types which are fundamental to the implementation and must be used  * in more than one standard header although they are only declared in  * one (perhaps nonstandard) header are defined here.  Standard headers  * use _BSD_XXX_T_ without undef'ing it.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_CT_RUNE_T_
+value|int
+end_define
+
+begin_comment
+comment|/* arg type for ctype funcs */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_OFF_T_
+value|long
+end_define
+
+begin_comment
+comment|/* file offset */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_PID_T_
+value|int
+end_define
+
+begin_comment
+comment|/* process [group] */
+end_comment
+
+begin_comment
+comment|/*  * Runes (wchar_t) is declared to be an ``int'' instead of the more natural  * ``unsigned long'' or ``long''.  Two things are happening here.  It is not  * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,  * it looks like 10646 will be a 31 bit standard.  This means that if your  * ints cannot hold 32 bits, you will be in trouble.  The reason an int was  * chosen over a long is that the is*() and to*() routines take ints (says  * ANSI C), but they use _RUNE_T_ instead of int.  By changing it here, you  * lose a bit of ANSI conformance, but your programs will still work.  *      * Note that _WCHAR_T_ and _RUNE_T_ must be of the same type.  When wchar_t  * and rune_t are typedef'd, _WCHAR_T_ will be undef'd, but _RUNE_T remains  * defined for ctype.h.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_WCHAR_T_
+value|int
+end_define
+
+begin_comment
+comment|/* wchar_t */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_WINT_T_
+value|int
+end_define
+
+begin_comment
+comment|/* wint_t */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_RUNE_T_
+value|int
+end_define
+
+begin_comment
+comment|/* rune_t */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _ANSI_H_ */
+end_comment
+
+end_unit
+
---
+++ sys/alpha/include/signal.h
@@ -0,0 +1,134 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $Id$ */
+end_comment
+
+begin_comment
+comment|/* From: NetBSD: signal.h,v 1.3 1997/04/06 08:47:43 cgd Exp */
+end_comment
+
+begin_comment
+comment|/*  * Copyright (c) 1994, 1995 Carnegie-Mellon University.  * All rights reserved.  *  * Author: Chris G. Demetriou  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ALPHA_SIGNAL_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_ALPHA_SIGNAL_H_
+end_define
+
+begin_typedef
+typedef|typedef
+name|long
+name|sig_atomic_t
+typedef|;
+end_typedef
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ANSI_SOURCE
+end_ifndef
+
+begin_comment
+comment|/*  * Information pushed on stack when a signal is delivered.  * This is used by the kernel to restore state following  * execution of the signal handler.  It is also made available  * to the handler to allow it to restore state properly if  * a non-standard exit is performed.  *  * Note that sc_regs[] and sc_fpregs[]+sc_fpcr are inline  * representations of 'struct reg' and 'struct fpreg', respectively.  */
+end_comment
+
+begin_struct
+struct|struct
+name|sigcontext
+block|{
+name|long
+name|sc_onstack
+decl_stmt|;
+comment|/* sigstack state to restore */
+name|long
+name|sc_mask
+decl_stmt|;
+comment|/* signal mask to restore */
+name|long
+name|sc_pc
+decl_stmt|;
+comment|/* pc to restore */
+name|long
+name|sc_ps
+decl_stmt|;
+comment|/* ps to restore */
+name|unsigned
+name|long
+name|sc_regs
+index|[
+literal|32
+index|]
+decl_stmt|;
+comment|/* integer register set (see above) */
+define|#
+directive|define
+name|sc_sp
+value|sc_regs[R_SP]
+name|long
+name|sc_ownedfp
+decl_stmt|;
+comment|/* fp has been used */
+name|unsigned
+name|long
+name|sc_fpregs
+index|[
+literal|32
+index|]
+decl_stmt|;
+comment|/* FP register set (see above) */
+name|unsigned
+name|long
+name|sc_fpcr
+decl_stmt|;
+comment|/* FP control register (see above) */
+name|unsigned
+name|long
+name|sc_fp_control
+decl_stmt|;
+comment|/* FP software control word */
+name|long
+name|sc_reserved
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* XXX */
+name|long
+name|sc_xxx
+index|[
+literal|8
+index|]
+decl_stmt|;
+comment|/* XXX */
+block|}
+struct|;
+end_struct
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_ANSI_SOURCE */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_ALPHA_SIGNAL_H_*/
+end_comment
+
+end_unit
+
