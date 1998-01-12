e67bf73e43d585d40f215b0bfa53720c0bbbd77e
David E. O'Brien <obrien@FreeBSD.org>
1998-01-12 06:21:25 +0000

    Used Peters suggestion of "cvs update -A -p foo > foo". Problem is my cvs wrapper isn't quiet.  Thus the spam.
    Former-commit-id: 1af83628a6ff0bac53f578679c75cfc824e09621

--- usr.bin/ncal/ncal.c
+++ usr.bin/ncal/ncal.c
@@ -1,32 +1,24 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_expr_stmt
-operator|...
-name|using
-name|script
-operator|...
-name|CVS
-operator|:
-name|up
-operator|-
-name|A
-operator|-
-name|p
-name|ncal
-operator|.
-name|c
+begin_comment
 comment|/*-  * Copyright (c) 1997 Wolfgang Helbig  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+end_comment
+
+begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
+end_ifndef
+
+begin_decl_stmt
 specifier|static
 specifier|const
 name|char
 name|rcsid
 index|[]
-operator|=
-literal|"$Id: ncal.c,v 1.5 1998/01/07 07:46:33 charnier Exp $"
-expr_stmt|;
-end_expr_stmt
+init|=
+literal|"$Id: ncal.c,v 1.5.2.1 1998/01/12 05:11:06 obrien Exp $"
+decl_stmt|;
+end_decl_stmt
 
 begin_endif
 endif|#
