45160ab0f51682a83d4ef207d6335bc93823a7ea
Bruce Evans <bde@FreeBSD.org>
1998-01-16 11:46:05 +0000

    Added a forward struct declaration so that this file is less self-insufficent.
    Former-commit-id: a0c95087f126ed5f29d6e789f9fd357c062f02ed

--- include/rpc/svc.h
+++ include/rpc/svc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  *  *	from: @(#)svc.h 1.20 88/02/08 SMI  *	from: @(#)svc.h	2.2 88/07/29 4.0 RPCSRC  *	$Id: svc.h,v 1.6 1996/12/30 13:59:40 peter Exp $  */
+comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  *  *	from: @(#)svc.h 1.20 88/02/08 SMI  *	from: @(#)svc.h	2.2 88/07/29 4.0 RPCSRC  *	$Id: svc.h,v 1.12 1997/05/28 04:45:13 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -41,6 +41,12 @@ name|XPRT_IDLE
 block|}
 enum|;
 end_enum
+
+begin_struct_decl
+struct_decl|struct
+name|rpc_msg
+struct_decl|;
+end_struct_decl
 
 begin_comment
 comment|/*  * Server side transport handle  */
--- include/rpc/svc_auth.h
+++ include/rpc/svc_auth.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  *  *	from: @(#)svc_auth.h 1.6 86/07/16 SMI  *	from: @(#)svc_auth.h	2.1 88/07/29 4.0 RPCSRC  *	$Id: svc_auth.h,v 1.4 1996/01/30 23:32:36 mpp Exp $  */
+comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  *  *	from: @(#)svc_auth.h 1.6 86/07/16 SMI  *	from: @(#)svc_auth.h	2.1 88/07/29 4.0 RPCSRC  *	$Id: svc_auth.h,v 1.9 1997/05/28 04:45:14 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -18,6 +18,18 @@ define|#
 directive|define
 name|_RPC_SVCAUTH_H
 end_define
+
+begin_struct_decl
+struct_decl|struct
+name|rpc_msg
+struct_decl|;
+end_struct_decl
+
+begin_struct_decl
+struct_decl|struct
+name|svc_req
+struct_decl|;
+end_struct_decl
 
 begin_comment
 comment|/*  * Server side authenticator  */
@@ -51,7 +63,6 @@ name|svc_auth_reg
 name|__P
 argument_list|(
 operator|(
-specifier|register
 name|int
 operator|,
 expr|enum
@@ -59,7 +70,15 @@ name|auth_stat
 argument_list|(
 operator|*
 argument_list|)
-argument_list|()
+argument_list|(
+expr|struct
+name|svc_req
+operator|*
+argument_list|,
+expr|struct
+name|rpc_msg
+operator|*
+argument_list|)
 operator|)
 argument_list|)
 decl_stmt|;
