c4bb5a09eff405f7194dc89845b4dcd281d426a1
Bruce Evans <bde@FreeBSD.org>
1998-01-17 13:39:29 +0000

    Fixed syntax error in normally-unconfigured code in previous commit.
    Former-commit-id: 5f75e80b1be13515b16d6954603d95fc7819be18

--- bin/cp/utils.c
+++ bin/cp/utils.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: utils.c,v 1.14 1997/10/03 18:11:14 wosch Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: utils.c,v 1.15 1998/01/16 07:37:05 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -507,6 +507,7 @@ name|wresid
 operator|-=
 name|wcount
 control|)
+block|{
 name|wcount
 operator|=
 name|write
@@ -582,9 +583,6 @@ expr_stmt|;
 block|}
 block|}
 block|}
-end_function
-
-begin_else
 else|else
 endif|#
 directive|endif
@@ -695,13 +693,7 @@ literal|1
 expr_stmt|;
 block|}
 block|}
-end_else
-
-begin_comment
 comment|/* 	 * Don't remove the target even after an error.  The target might 	 * not be a regular file, or its attributes might be important, 	 * or its contents might be irreplacable.  It would only be safe 	 * to remove it if we created it and its length is 0. 	 */
-end_comment
-
-begin_if
 if|if
 condition|(
 name|pflag
@@ -807,9 +799,6 @@ operator|=
 literal|1
 expr_stmt|;
 block|}
-end_if
-
-begin_expr_stmt
 operator|(
 name|void
 operator|)
@@ -818,9 +807,6 @@ argument_list|(
 name|from_fd
 argument_list|)
 expr_stmt|;
-end_expr_stmt
-
-begin_if
 if|if
 condition|(
 name|close
@@ -843,40 +829,29 @@ operator|=
 literal|1
 expr_stmt|;
 block|}
-end_if
-
-begin_return
 return|return
 operator|(
 name|rval
 operator|)
 return|;
-end_return
+block|}
+end_function
 
-begin_macro
-unit|}  int
+begin_function
+name|int
 name|copy_link
-argument_list|(
-argument|p
-argument_list|,
-argument|exists
-argument_list|)
-end_macro
-
-begin_decl_stmt
+parameter_list|(
+name|p
+parameter_list|,
+name|exists
+parameter_list|)
 name|FTSENT
 modifier|*
 name|p
 decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 name|int
 name|exists
 decl_stmt|;
-end_decl_stmt
-
-begin_block
 block|{
 name|int
 name|len
@@ -991,7 +966,7 @@ literal|0
 operator|)
 return|;
 block|}
-end_block
+end_function
 
 begin_function
 name|int
