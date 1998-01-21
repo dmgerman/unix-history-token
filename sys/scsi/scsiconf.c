c0898dfbfe0aa75c3f8db2a380abb6bfea2f1715
Andrey Chernov <ache@FreeBSD.org>
1998-01-21 08:04:47 +0000

    MFC: SCSI-SCSI RAID from Infotrand
    Former-commit-id: 5f7f40cfed7c96cf36472a3581c4ac9e495d8a79

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.64.2.21 1997/11/06 08:31:21 joerg Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.64.2.22 1997/12/30 22:27:58 nate Exp $  */
 end_comment
 
 begin_include
@@ -1182,6 +1182,24 @@ block|,
 literal|"EMULEX"
 block|,
 literal|"MD21*"
+block|,
+literal|"*"
+block|,
+literal|"sd"
+block|,
+name|SC_MORE_LUS
+block|}
+block|,
+block|{
+name|T_DIRECT
+block|,
+name|T_DIRECT
+block|,
+name|T_FIXED
+block|,
+literal|"IFT"
+block|,
+literal|"*"
 block|,
 literal|"*"
 block|,
