d35c333e5f6e1c993559ecc08b9541283fc693df
Michael Smith <msmith@FreeBSD.org>
1998-01-29 06:42:12 +0000

    Correct usage of unit to t->lun.  This fixes the DEVFS case and the "buggy Zip" message. Suggested by:	Pedro A M Vazquez <vazquez@IQM.Unicamp.BR>
    Former-commit-id: 0e8617c924a63027d03c7e8ce44ef69ebb402322

--- sys/pc98/pc98/wfd.c
+++ sys/pc98/pc98/wfd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997,1998  Junichi Satoh<junichi@astec.co.jp>  *   All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY Junichi Satoh ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL Junichi Satoh BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *      $Id: wfd.c,v 1.2 1998/01/16 22:43:33 pst Exp $  */
+comment|/*  * Copyright (c) 1997,1998  Junichi Satoh<junichi@astec.co.jp>  *   All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY Junichi Satoh ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL Junichi Satoh BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *      $Id: wfd.c,v 1.3 1998/01/27 16:49:35 msmith Exp $  */
 end_comment
 
 begin_comment
@@ -1271,7 +1271,9 @@ name|printf
 argument_list|(
 literal|"wfd%d: buggy Zip drive, 64-block transfer limit set\n"
 argument_list|,
-name|unit
+name|t
+operator|->
+name|lun
 argument_list|)
 expr_stmt|;
 name|t
@@ -1298,7 +1300,9 @@ name|mynor
 operator|=
 name|dkmakeminor
 argument_list|(
-name|unit
+name|t
+operator|->
+name|lun
 argument_list|,
 name|WHOLE_DISK_SLICE
 argument_list|,
@@ -1326,7 +1330,9 @@ literal|0640
 argument_list|,
 literal|"wfd%d"
 argument_list|,
-name|unit
+name|t
+operator|->
+name|lun
 argument_list|)
 expr_stmt|;
 name|t
@@ -1350,7 +1356,9 @@ literal|0640
 argument_list|,
 literal|"rwfd%d"
 argument_list|,
-name|unit
+name|t
+operator|->
+name|lun
 argument_list|)
 expr_stmt|;
 endif|#
