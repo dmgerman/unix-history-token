13eaec5e523685dfeaf1976dda81b870fcb46225
Poul-Henning Kamp <phk@FreeBSD.org>
1998-01-30 11:34:06 +0000

    Retire LFS.
    If you want to play with it, you can find the final version of the
    code in the repository the tag LFS_RETIREMENT.
    
    If somebody makes LFS work again, adding it back is certainly
    desireable, but as it is now nobody seems to care much about it,
    and it has suffered considerable bitrot since its somewhat haphazard
    integration.
    
    R.I.P
    
    
    Former-commit-id: b206700683a316e2d2250dbac7ff338ce3216ff0

--- sys/amd64/amd64/autoconf.c
+++ sys/amd64/amd64/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.83 1998/01/09 03:20:53 eivind Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.84 1998/01/24 02:54:12 eivind Exp $  */
 end_comment
 
 begin_comment
@@ -23,12 +23,6 @@ begin_include
 include|#
 directive|include
 file|"opt_cd9660.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"opt_lfs.h"
 end_include
 
 begin_include
@@ -1176,57 +1170,6 @@ name|printf
 argument_list|(
 literal|"Considering FFS root f/s.\n"
 argument_list|)
-expr_stmt|;
-comment|/* 		 * Ignore the -a flag if this kernel isn't compiled 		 * with a generic root/swap configuration: if we skip 		 * setroot() and we aren't a generic kernel, chaos 		 * will ensue because setconf() will be a no-op. 		 * (rootdev is always initialized to NODEV in a 		 * generic configuration, so we test for that.) 		 */
-if|if
-condition|(
-operator|(
-name|boothowto
-operator|&
-name|RB_ASKNAME
-operator|)
-operator|==
-literal|0
-operator|||
-name|rootdev
-operator|!=
-name|NODEV
-condition|)
-name|setroot
-argument_list|()
-expr_stmt|;
-block|}
-endif|#
-directive|endif
-if|#
-directive|if
-name|defined
-argument_list|(
-name|LFS
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|LFS_ROOT
-argument_list|)
-if|if
-condition|(
-operator|!
-name|mountrootfsname
-condition|)
-block|{
-if|if
-condition|(
-name|bootverbose
-condition|)
-name|printf
-argument_list|(
-literal|"Considering LFS root f/s.\n"
-argument_list|)
-expr_stmt|;
-name|mountrootfsname
-operator|=
-literal|"lfs"
 expr_stmt|;
 comment|/* 		 * Ignore the -a flag if this kernel isn't compiled 		 * with a generic root/swap configuration: if we skip 		 * setroot() and we aren't a generic kernel, chaos 		 * will ensue because setconf() will be a no-op. 		 * (rootdev is always initialized to NODEV in a 		 * generic configuration, so we test for that.) 		 */
 if|if
