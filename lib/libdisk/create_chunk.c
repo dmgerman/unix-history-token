1f7654cbdc8b5af33cea0d2956b3044c98e690e1
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-02-10 17:19:12 +0000

    Be more verbose if we can't determine device major/minor.
    Former-commit-id: 88844c1065d15c09de2094148a055fbf90083de9

--- lib/libdisk/create_chunk.c
+++ lib/libdisk/create_chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.27 1997/02/22 15:06:31 peter Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.28 1998/01/16 12:50:36 jkh Exp $  *  */
 end_comment
 
 begin_include
@@ -1596,6 +1596,13 @@ literal|13
 expr_stmt|;
 else|else
 block|{
+name|msgDebug
+argument_list|(
+literal|"MakeDev: Unknown major/minor for devtype %s\n"
+argument_list|,
+name|p
+argument_list|)
+expr_stmt|;
 return|return
 literal|0
 return|;
