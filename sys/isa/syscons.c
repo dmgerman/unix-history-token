c2c71465c9af795e4f1b71791c59fa2127d2b2f9
Poul-Henning Kamp <phk@FreeBSD.org>
1998-02-13 17:54:53 +0000

    Fix a couple of lines that dropped out in my merge yesterday.
    Complained about by:	ache
    
    
    Former-commit-id: 7eba95338ed6ff0900733aa0828304560bacaa4c

--- sys/isa/syscons.c
+++ sys/isa/syscons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992-1997 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.254 1998/02/12 22:05:05 phk Exp $  */
+comment|/*-  * Copyright (c) 1992-1997 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: syscons.c,v 1.255 1998/02/13 11:31:34 phk Exp $  */
 end_comment
 
 begin_include
@@ -3195,6 +3195,51 @@ name|ISMAPPED
 argument_list|(
 name|pa
 argument_list|,
+sizeof|sizeof
+argument_list|(
+name|u_long
+argument_list|)
+argument_list|)
+condition|)
+block|{
+name|segoff
+operator|=
+operator|*
+operator|(
+name|u_long
+operator|*
+operator|)
+name|pa_to_va
+argument_list|(
+name|pa
+argument_list|)
+expr_stmt|;
+name|pa
+operator|=
+operator|(
+operator|(
+operator|(
+name|segoff
+operator|&
+literal|0xffff0000
+operator|)
+operator|>>
+literal|12
+operator|)
+operator|+
+operator|(
+name|segoff
+operator|&
+literal|0xffff
+operator|)
+operator|)
+expr_stmt|;
+if|if
+condition|(
+name|ISMAPPED
+argument_list|(
+name|pa
+argument_list|,
 name|MODE_PARAM_SIZE
 argument_list|)
 condition|)
@@ -3209,6 +3254,7 @@ argument_list|(
 name|pa
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 block|}
 return|return
