a5b7e6056b0f0c94562f37cbfb749498067881c8
Takenori KATO <kato@FreeBSD.org>
1998-02-13 09:34:42 +0000

    Use RDMSR instruction instead of WRMSR.
    Former-commit-id: b64687140a847eab4089712b09a7d63d0f98dbd4

--- sys/amd64/amd64/identcpu.c
+++ sys/amd64/amd64/identcpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.41 1998/01/25 23:45:33 kato Exp $  */
+comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.42 1998/01/27 08:12:09 kato Exp $  */
 end_comment
 
 begin_include
@@ -2043,12 +2043,12 @@ begin_decl_stmt
 specifier|static
 specifier|volatile
 name|u_int
-name|trap_by_wrmsr
+name|trap_by_rdmsr
 decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*  * Special exception 6 handler.  * The wrmsr instruction generates invalid opcodes fault on 486-class  * Cyrix CPU.  Stacked eip register points the wrmsr instruction in the  * function identblue() when this handler is called.  Stacked eip should  * be advanced.  */
+comment|/*  * Special exception 6 handler.  * The rdmsr instruction generates invalid opcodes fault on 486-class  * Cyrix CPU.  Stacked eip register points the rdmsr instruction in the  * function identblue() when this handler is called.  Stacked eip should  * be advanced.  */
 end_comment
 
 begin_decl_stmt
@@ -2058,7 +2058,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_asm
-asm|asm (" 	.text 	.p2align 2,0x90 " __XSTRING(CNAME(bluetrap6)) ": 	ss 	movl	$0xa8c1d," __XSTRING(CNAME(trap_by_wrmsr)) " 	addl	$2, (%esp)		  # I know wrmsr is a 2-bytes instruction. 	iret ");
+asm|asm (" 	.text 	.p2align 2,0x90 " __XSTRING(CNAME(bluetrap6)) ": 	ss 	movl	$0xa8c1d," __XSTRING(CNAME(trap_by_rdmsr)) " 	addl	$2, (%esp)		  # I know rdmsr is a 2-bytes instruction. 	iret ");
 end_asm
 
 begin_comment
@@ -2072,7 +2072,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_asm
-asm|asm (" 	.text 	.p2align 2,0x90 " __XSTRING(CNAME(bluetrap13)) ": 	ss 	movl	$0xa89c4," __XSTRING(CNAME(trap_by_wrmsr)) " 	popl	%eax				# discard errorcode. 	addl	$2, (%esp)			# I know wrmsr is a 2-bytes instruction. 	iret ");
+asm|asm (" 	.text 	.p2align 2,0x90 " __XSTRING(CNAME(bluetrap13)) ": 	ss 	movl	$0xa89c4," __XSTRING(CNAME(trap_by_rdmsr)) " 	popl	%eax				# discard errorcode. 	addl	$2, (%esp)			# I know rdmsr is a 2-bytes instruction. 	iret ");
 end_asm
 
 begin_comment
@@ -2087,11 +2087,11 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
-name|trap_by_wrmsr
+name|trap_by_rdmsr
 operator|=
 literal|0
 expr_stmt|;
-comment|/*  	 * Cyrix 486-class CPU does not support wrmsr instruction. 	 * The wrmsr instruction generates invalid opcode fault, and exception 	 * will be trapped by bluetrap6() on Cyrix 486-class CPU.  The 	 * bluetrap6() set the magic number to trap_by_wrmsr. 	 */
+comment|/*  	 * Cyrix 486-class CPU does not support rdmsr instruction. 	 * The rdmsr instruction generates invalid opcode fault, and exception 	 * will be trapped by bluetrap6() on Cyrix 486-class CPU.  The 	 * bluetrap6() set the magic number to trap_by_rdmsr. 	 */
 name|setidt
 argument_list|(
 literal|6
@@ -2110,7 +2110,7 @@ name|SEL_KPL
 argument_list|)
 argument_list|)
 expr_stmt|;
-comment|/* 	 * Certain BIOS disables cpuid instructnion of Cyrix 6x86MX CPU. 	 * In this case, wrmsr generates general protection fault, and 	 * exception will be trapped by bluetrap13(). 	 */
+comment|/* 	 * Certain BIOS disables cpuid instructnion of Cyrix 6x86MX CPU. 	 * In this case, rdmsr generates general protection fault, and 	 * exception will be trapped by bluetrap13(). 	 */
 name|setidt
 argument_list|(
 literal|13
@@ -2129,17 +2129,15 @@ name|SEL_KPL
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|wrmsr
+name|rdmsr
 argument_list|(
 literal|0x1002
-argument_list|,
-literal|0x03000000LL
 argument_list|)
 expr_stmt|;
 comment|/* Cyrix CPU generates fault. */
 if|if
 condition|(
-name|trap_by_wrmsr
+name|trap_by_rdmsr
 operator|==
 literal|0xa8c1d
 condition|)
@@ -2149,7 +2147,7 @@ return|;
 elseif|else
 if|if
 condition|(
-name|trap_by_wrmsr
+name|trap_by_rdmsr
 operator|==
 literal|0xa89c4
 condition|)
