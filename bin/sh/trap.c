e3ac6451aa9217c2d2c09d8a8904fd36f70189c5
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-02-15 11:32:27 +0000

    MFC: timeout support, fixes to signal handling.  If there's more contraversy over the latter, I can either continue to track it in -current or back it out.
    Former-commit-id: 5520d898fa180b19a85b92a6f85b0c4c3fc8f005

--- bin/sh/trap.c
+++ bin/sh/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: trap.c,v 1.4 1996/09/01 10:21:47 peter Exp $  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: trap.c,v 1.4.2.1 1997/08/25 09:10:46 jkh Exp $  */
 end_comment
 
 begin_ifndef
@@ -855,7 +855,7 @@ comment|/*  * Set the signal handler for the specified signal.  The routine figu
 end_comment
 
 begin_function
-name|long
+name|void
 name|setsignal
 parameter_list|(
 name|signo
@@ -868,6 +868,8 @@ name|int
 name|action
 decl_stmt|;
 name|sig_t
+name|sig
+decl_stmt|,
 name|sigact
 init|=
 name|SIG_DFL
@@ -1020,9 +1022,7 @@ argument_list|)
 condition|)
 block|{
 comment|/* 			 * Pretend it worked; maybe we should give a warning 			 * here, but other shells don't. We don't alter 			 * sigmode, so that we retry every time. 			 */
-return|return
-literal|0
-return|;
+return|return;
 block|}
 if|if
 condition|(
@@ -1086,9 +1086,7 @@ name|t
 operator|==
 name|action
 condition|)
-return|return
-literal|0
-return|;
+return|return;
 switch|switch
 condition|(
 name|action
@@ -1124,17 +1122,37 @@ name|t
 operator|=
 name|action
 expr_stmt|;
-return|return
-operator|(
-name|long
-operator|)
+name|sig
+operator|=
 name|signal
 argument_list|(
 name|signo
 argument_list|,
 name|sigact
 argument_list|)
-return|;
+expr_stmt|;
+ifdef|#
+directive|ifdef
+name|BSD
+if|if
+condition|(
+name|sig
+operator|!=
+name|SIG_ERR
+operator|&&
+name|action
+operator|==
+name|S_CATCH
+condition|)
+name|siginterrupt
+argument_list|(
+name|signo
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 block|}
 end_function
 
@@ -1332,6 +1350,9 @@ end_decl_stmt
 
 begin_block
 block|{
+ifndef|#
+directive|ifndef
+name|BSD
 name|signal
 argument_list|(
 name|signo
@@ -1339,6 +1360,8 @@ argument_list|,
 name|onsig
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 if|if
 condition|(
 name|signo
