153a97e9d1b8cf10968662bc217835a0bbd1e5ec
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-02-17 19:48:19 +0000

    MFC: I am a merging machine.  Let's just hope I don't die of boredom before I finish this.
    Former-commit-id: 728a8e6e7190689c2ac6ea548259df15f6d407ad

--- lib/libc/sys/truncate.c
+++ lib/libc/sys/truncate.c
@@ -49,6 +49,12 @@ directive|include
 file|<sys/syscall.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<unistd.h>
+end_include
+
 begin_comment
 comment|/*  * This function provides 64-bit offset padding that  * is not supplied by GCC 1.X but is supplied by GCC 2.X.  */
 end_comment
