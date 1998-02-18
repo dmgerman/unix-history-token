eac034802a1be753bcb79ec9d24adcebc84b793a
Michael Smith <msmith@FreeBSD.org>
1998-02-18 09:40:54 +0000

    Put the SC_Q_NO_TAGS value in the correct field for the Iomega Jaz quirk entry, and move it down with the other NO_TAGS device. Submitted by:	bde
    Former-commit-id: 1b31203118e952938a9e549fd16389ca4708d6a2

--- sys/scsi/scsiconf.c
+++ sys/scsi/scsiconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.102 1998/01/21 08:03:37 ache Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992  *  * New configuration setup: dufault@hda.com  *  *      $Id: scsiconf.c,v 1.103 1998/02/16 11:15:35 msmith Exp $  */
 end_comment
 
 begin_include
@@ -1214,24 +1214,6 @@ block|,
 name|SC_MORE_LUS
 block|}
 block|,
-block|{
-name|T_DIRECT
-block|,
-name|T_DIRECT
-block|,
-name|T_REMOV
-block|,
-literal|"iomega"
-block|,
-literal|"jaz*"
-block|,
-literal|"*"
-block|,
-literal|"sd"
-block|,
-name|SD_Q_NO_TAGS
-block|}
-block|,
 endif|#
 directive|endif
 comment|/* NSD */
@@ -2024,6 +2006,26 @@ block|,
 literal|"HP"
 block|,
 literal|"C372*"
+block|,
+literal|"*"
+block|,
+literal|"sd"
+block|,
+name|SC_ONE_LU
+block|,
+name|SD_Q_NO_TAGS
+block|}
+block|,
+block|{
+name|T_DIRECT
+block|,
+name|T_DIRECT
+block|,
+name|T_REMOV
+block|,
+literal|"iomega"
+block|,
+literal|"jaz*"
 block|,
 literal|"*"
 block|,
