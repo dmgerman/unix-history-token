531e4d7c0b1aa0aa37de9cdae6abd5a3afc74b6c
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-02-18 09:30:31 +0000

    Support for FAT32 partitions. Submitted by:	Dmitrij Tejblum <dima@tejblum.dnttm.rssi.ru> Obtained from:	NetBSD
    Former-commit-id: 176885cea084ab6456a63a749062c70375d581b3

--- sbin/mount_msdos/mount_msdos.c
+++ sbin/mount_msdos/mount_msdos.c
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/*	$NetBSD: mount_msdos.c,v 1.18 1997/09/16 12:24:18 lukem Exp $	*/
+end_comment
+
+begin_comment
 comment|/*  * Copyright (c) 1994 Christopher G. Demetriou  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Christopher G. Demetriou.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
@@ -16,7 +20,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: mount_msdos.c,v 1.9 1997/03/29 03:32:25 imp Exp $"
+literal|"$Id: mount_msdos.c,v 1.10 1997/08/25 20:23:16 bde Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -274,6 +278,12 @@ name|args
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|args
+operator|.
+name|magic
+operator|=
+name|MSDOSFS_ARGSMAGIC
+expr_stmt|;
 while|while
 condition|(
 operator|(
@@ -285,7 +295,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"u:g:m:o:"
+literal|"sl9u:g:m:o:"
 argument_list|)
 operator|)
 operator|!=
@@ -298,6 +308,51 @@ condition|(
 name|c
 condition|)
 block|{
+ifdef|#
+directive|ifdef
+name|MSDOSFSMNT_GEMDOSFS
+case|case
+literal|'G'
+case|:
+name|args
+operator|.
+name|flags
+operator||=
+name|MSDOSFSMNT_GEMDOSFS
+expr_stmt|;
+break|break;
+endif|#
+directive|endif
+case|case
+literal|'s'
+case|:
+name|args
+operator|.
+name|flags
+operator||=
+name|MSDOSFSMNT_SHORTNAME
+expr_stmt|;
+break|break;
+case|case
+literal|'l'
+case|:
+name|args
+operator|.
+name|flags
+operator||=
+name|MSDOSFSMNT_LONGNAME
+expr_stmt|;
+break|break;
+case|case
+literal|'9'
+case|:
+name|args
+operator|.
+name|flags
+operator||=
+name|MSDOSFSMNT_NOWIN95
+expr_stmt|;
+break|break;
 case|case
 literal|'u'
 case|:
@@ -920,6 +975,11 @@ name|done
 operator|=
 literal|0
 expr_stmt|;
+name|rv
+operator|=
+operator|-
+literal|1
+expr_stmt|;
 if|if
 condition|(
 operator|*
@@ -988,7 +1048,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: mount_msdos [-F flags] [-u user] [-g group] [-m mask] bdev dir\n"
+literal|"usage: mount_msdos [-o options] [-u user] [-g group] [-m mask] bdev dir\n"
 argument_list|)
 expr_stmt|;
 name|exit
