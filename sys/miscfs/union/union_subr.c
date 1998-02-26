48401c82148dad8f9b1b1203d2e3aa6a1a743e3c
Takenori KATO <kato@FreeBSD.org>
1998-02-26 03:23:56 +0000

    Deleted KLOCK-hack.
    Former-commit-id: 520a8d10e375d8ee86620a829acc0ac2b88e9f50

--- sys/miscfs/union/union_subr.c
+++ sys/miscfs/union/union_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 Jan-Simon Pendry  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)union_subr.c	8.20 (Berkeley) 5/20/95  * $Id: union_subr.c,v 1.27 1998/02/06 12:13:44 eivind Exp $  */
+comment|/*  * Copyright (c) 1994 Jan-Simon Pendry  * Copyright (c) 1994  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)union_subr.c	8.20 (Berkeley) 5/20/95  * $Id: union_subr.c,v 1.28 1998/02/10 03:32:05 kato Exp $  */
 end_comment
 
 begin_include
@@ -1502,47 +1502,6 @@ name|mp
 operator|)
 condition|)
 block|{
-comment|/* 				 * Do not assume that vget() does not 				 * lock the vnode even though flags 				 * argument is 0. 				 */
-if|if
-condition|(
-operator|(
-name|un
-operator|->
-name|un_uppervp
-operator|!=
-name|NULLVP
-operator|)
-operator|&&
-operator|(
-operator|(
-name|un
-operator|->
-name|un_flags
-operator|&
-name|UN_KLOCK
-operator|)
-operator|==
-literal|0
-operator|)
-condition|)
-block|{
-name|SETGLOCK
-argument_list|(
-name|un
-argument_list|)
-expr_stmt|;
-name|klocked
-operator|=
-literal|1
-expr_stmt|;
-block|}
-else|else
-block|{
-name|klocked
-operator|=
-literal|0
-expr_stmt|;
-block|}
 if|if
 condition|(
 name|vget
@@ -1564,15 +1523,6 @@ name|NULL
 argument_list|)
 condition|)
 block|{
-if|if
-condition|(
-name|klocked
-condition|)
-name|CLEARGLOCK
-argument_list|(
-name|un
-argument_list|)
-expr_stmt|;
 name|union_list_unlock
 argument_list|(
 name|hash
@@ -1582,15 +1532,6 @@ goto|goto
 name|loop
 goto|;
 block|}
-if|if
-condition|(
-name|klocked
-condition|)
-name|CLEARGLOCK
-argument_list|(
-name|un
-argument_list|)
-expr_stmt|;
 break|break;
 block|}
 block|}
