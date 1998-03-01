1fa5da86566c3668c251eb9c0d297822857434d0
Bruce Evans <bde@FreeBSD.org>
1998-03-01 06:01:33 +0000

    Fixed syntax error in previous commit.
    Former-commit-id: a9d2772ea6694659ec866677b0b7f49ac0b53c7b

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994-1996 Cronyx Engineering Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 2.4, Thu Apr 30 17:17:21 MSD 1997  *  * $Id: if_spppsubr.c,v 1.32 1998/02/09 06:09:57 eivind Exp $  */
+comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994-1996 Cronyx Engineering Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 2.4, Thu Apr 30 17:17:21 MSD 1997  *  * $Id: if_spppsubr.c,v 1.33 1998/02/28 21:01:09 phk Exp $  */
 end_comment
 
 begin_include
@@ -13231,7 +13231,6 @@ name|desiredaddr
 operator|==
 name|hisaddr
 operator|||
-operator|(
 name|hisaddr
 operator|==
 literal|1
@@ -13239,7 +13238,7 @@ operator|&&
 name|desiredaddr
 operator|!=
 literal|0
-operator|)
+condition|)
 block|{
 comment|/* 				 * Peer's address is same as our value, 				 * or we have set it to 0.0.0.1 to  				 * indicate that we do not really care, 				 * this is agreeable.  Gonna conf-ack 				 * it. 				 */
 if|if
