ec496cabac5d98c6740214b7d11334ea09127310
Tor Egge <tegge@FreeBSD.org>
1998-03-03 20:09:14 +0000

    Reduce timeout before assuming that forwarding of hardclock or softclock failed. Don't complain on forwarding failure, unless BETTER_CLOCK_DIAGNOSTIC is defined.
    Former-commit-id: df5a8e3ea3fa4356cf076e690df832a026c3b063

--- sys/i386/include/mptable.h
+++ sys/i386/include/mptable.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.65 1998/02/09 06:08:13 eivind Exp $  */
+comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.66 1998/03/01 04:18:50 dyson Exp $  */
 end_comment
 
 begin_include
@@ -8909,7 +8909,11 @@ literal|0
 expr_stmt|;
 while|while
 condition|(
+operator|(
 name|checkstate_need_ast
+operator|&
+name|map
+operator|)
 operator|!=
 literal|0
 condition|)
@@ -8922,16 +8926,23 @@ if|if
 condition|(
 name|i
 operator|>
-literal|1000000
+literal|100000
 condition|)
 block|{
+ifdef|#
+directive|ifdef
+name|BETTER_CLOCK_DIAGNOSTIC
 name|printf
 argument_list|(
 literal|"forward_statclock: dropped ast 0x%x\n"
 argument_list|,
 name|checkstate_need_ast
+operator|&
+name|map
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 break|break;
 block|}
 block|}
@@ -9242,7 +9253,11 @@ literal|0
 expr_stmt|;
 while|while
 condition|(
+operator|(
 name|checkstate_need_ast
+operator|&
+name|map
+operator|)
 operator|!=
 literal|0
 condition|)
@@ -9255,16 +9270,23 @@ if|if
 condition|(
 name|i
 operator|>
-literal|1000000
+literal|100000
 condition|)
 block|{
+ifdef|#
+directive|ifdef
+name|BETTER_CLOCK_DIAGNOSTIC
 name|printf
 argument_list|(
 literal|"forward_hardclock: dropped ast 0x%x\n"
 argument_list|,
 name|checkstate_need_ast
+operator|&
+name|map
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 break|break;
 block|}
 block|}
