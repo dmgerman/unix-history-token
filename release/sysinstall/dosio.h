ee9c70532f37dec00ddc1c038aba41076d6acb41
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-03-03 04:29:01 +0000

    Update stand-alone DOS I/O routines to be able to read from FAT32/VFAT partitions.  This should allow sysinstall to function properly in all Win95 installation based scenarios now. Submitted by:	Robert Nordier <rnordier@iafrica.com>
    Former-commit-id: d05ea6b3747cb9b1ceda89dcc2866ce64e7d0a1c

--- release/sysinstall/dosio.h
+++ release/sysinstall/dosio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 Robert Nordier  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1996, 1998 Robert Nordier  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in  *    the documentation and/or other materials provided with the  *    distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER  * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -16,14 +16,14 @@ name|DOSIO_H
 end_define
 
 begin_comment
-comment|/*   * DOS file attributes  */
+comment|/*  * DOS file attributes  */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|FA_RDONLY
-value|0x01
+value|001
 end_define
 
 begin_comment
@@ -34,7 +34,7 @@ begin_define
 define|#
 directive|define
 name|FA_HIDDEN
-value|0x02
+value|002
 end_define
 
 begin_comment
@@ -45,7 +45,7 @@ begin_define
 define|#
 directive|define
 name|FA_SYSTEM
-value|0x04
+value|004
 end_define
 
 begin_comment
@@ -56,7 +56,7 @@ begin_define
 define|#
 directive|define
 name|FA_LABEL
-value|0x08
+value|010
 end_define
 
 begin_comment
@@ -67,7 +67,7 @@ begin_define
 define|#
 directive|define
 name|FA_DIR
-value|0x10
+value|020
 end_define
 
 begin_comment
@@ -78,23 +78,34 @@ begin_define
 define|#
 directive|define
 name|FA_ARCH
-value|0x20
+value|040
 end_define
 
 begin_comment
 comment|/* archive (file modified) */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|FA_XDE
+value|017
+end_define
+
 begin_comment
-comment|/*   * Error number to overload if filesystem errors are detected during  * routine processing  */
+comment|/* extended directory entry */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|EBADFS
-value|EINVAL
+name|FA_MASK
+value|077
 end_define
+
+begin_comment
+comment|/* all attributes */
+end_comment
 
 begin_comment
 comment|/*  * Macros to convert DOS-format 16-bit and 32-bit quantities  */
@@ -107,7 +118,7 @@ name|cv2
 parameter_list|(
 name|p
 parameter_list|)
-value|((u_short)(p)[0] |         \                ((u_short)(p)[1]<< 010))
+value|((u_int16_t)(p)[0] |         \                 ((u_int16_t)(p)[1]<< 010))
 end_define
 
 begin_define
@@ -117,12 +128,57 @@ name|cv4
 parameter_list|(
 name|p
 parameter_list|)
-value|((u_long)(p)[0] |          \                ((u_long)(p)[1]<< 010) |  \                ((u_long)(p)[2]<< 020) |  \                ((u_long)(p)[3]<< 030))
+value|((u_int32_t)(p)[0] |          \                 ((u_int32_t)(p)[1]<< 010) |  \                 ((u_int32_t)(p)[2]<< 020) |  \                 ((u_int32_t)(p)[3]<< 030))
 end_define
 
 begin_comment
-comment|/*  * DOS directory structure  */
+comment|/*  * Directory, filesystem, and file structures.  */
 end_comment
+
+begin_typedef
+typedef|typedef
+struct|struct
+block|{
+name|u_char
+name|x_case
+decl_stmt|;
+comment|/* case */
+name|u_char
+name|c_hsec
+decl_stmt|;
+comment|/* created: secs/100 */
+name|u_char
+name|c_time
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* created: time */
+name|u_char
+name|c_date
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* created: date */
+name|u_char
+name|a_date
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* accessed: date */
+name|u_char
+name|h_clus
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* clus[hi] */
+block|}
+name|DOS_DEX
+typedef|;
+end_typedef
 
 begin_typedef
 typedef|typedef
@@ -146,27 +202,24 @@ name|u_char
 name|attr
 decl_stmt|;
 comment|/* attributes */
-name|u_char
-name|reserved
-index|[
-literal|10
-index|]
+name|DOS_DEX
+name|dex
 decl_stmt|;
-comment|/* reserved */
+comment|/* VFAT/FAT32 only */
 name|u_char
 name|time
 index|[
 literal|2
 index|]
 decl_stmt|;
-comment|/* time */
+comment|/* modified: time */
 name|u_char
 name|date
 index|[
 literal|2
 index|]
 decl_stmt|;
-comment|/* date */
+comment|/* modified: date */
 name|u_char
 name|clus
 index|[
@@ -180,7 +233,7 @@ index|[
 literal|4
 index|]
 decl_stmt|;
-comment|/* file size */
+comment|/* size */
 block|}
 name|DOS_DE
 typedef|;
@@ -191,62 +244,145 @@ typedef|typedef
 struct|struct
 block|{
 name|u_char
-modifier|*
-name|fat
+name|seq
 decl_stmt|;
-comment|/* FAT */
-name|u_long
-name|volid
+comment|/* flags */
+name|u_char
+name|name1
+index|[
+literal|5
+index|]
+index|[
+literal|2
+index|]
 decl_stmt|;
-comment|/* volume id */
-name|u_long
-name|lsnfat
+comment|/* 1st name area */
+name|u_char
+name|attr
 decl_stmt|;
-comment|/* logical sector number: fat */
-name|u_long
-name|lsndir
+comment|/* (see fat_de) */
+name|u_char
+name|res
 decl_stmt|;
-comment|/* logical sector number: dir */
-name|u_long
-name|lsndta
+comment|/* reserved */
+name|u_char
+name|chk
 decl_stmt|;
-comment|/* logical sector number: data area */
-name|short
+comment|/* checksum */
+name|u_char
+name|name2
+index|[
+literal|6
+index|]
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* 2nd name area */
+name|u_char
+name|clus
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* (see fat_de) */
+name|u_char
+name|name3
+index|[
+literal|2
+index|]
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* 3rd name area */
+block|}
+name|DOS_XDE
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+union|union
+block|{
+name|DOS_DE
+name|de
+decl_stmt|;
+comment|/* standard directory entry */
+name|DOS_XDE
+name|xde
+decl_stmt|;
+comment|/* extended directory entry */
+block|}
+name|DOS_DIR
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+struct|struct
+block|{
+name|int
 name|fd
 decl_stmt|;
 comment|/* file descriptor */
-name|short
-name|fat12
+name|u_char
+modifier|*
+name|buf
 decl_stmt|;
-comment|/* 12-bit FAT entries */
-name|u_short
-name|spf
+comment|/* buffer */
+name|u_int
+name|bufsec
 decl_stmt|;
-comment|/* sectors per fat */
-name|u_short
-name|dirents
-decl_stmt|;
-comment|/* root directory entries */
-name|u_short
-name|spc
-decl_stmt|;
-comment|/* sectors per cluster */
-name|u_short
-name|xclus
-decl_stmt|;
-comment|/* maximum cluster number */
-name|u_short
-name|bsize
-decl_stmt|;
-comment|/* cluster size in bytes */
-name|u_short
-name|bshift
-decl_stmt|;
-comment|/* cluster conversion shift */
-name|u_short
+comment|/* buffered sector */
+name|u_int
 name|links
 decl_stmt|;
 comment|/* active links to structure */
+name|u_int
+name|spc
+decl_stmt|;
+comment|/* sectors per cluster */
+name|u_int
+name|bsize
+decl_stmt|;
+comment|/* cluster size in bytes */
+name|u_int
+name|bshift
+decl_stmt|;
+comment|/* cluster conversion shift */
+name|u_int
+name|dirents
+decl_stmt|;
+comment|/* root directory entries */
+name|u_int
+name|spf
+decl_stmt|;
+comment|/* sectors per fat */
+name|u_int
+name|rdcl
+decl_stmt|;
+comment|/* root directory start cluster */
+name|u_int
+name|lsnfat
+decl_stmt|;
+comment|/* start of fat */
+name|u_int
+name|lsndir
+decl_stmt|;
+comment|/* start of root dir */
+name|u_int
+name|lsndta
+decl_stmt|;
+comment|/* start of data area */
+name|u_int
+name|fatsz
+decl_stmt|;
+comment|/* FAT entry size */
+name|u_int
+name|xclus
+decl_stmt|;
+comment|/* maximum cluster number */
 block|}
 name|DOS_FS
 typedef|;
@@ -265,11 +401,11 @@ name|DOS_DE
 name|de
 decl_stmt|;
 comment|/* directory entry */
-name|u_long
+name|u_int
 name|offset
 decl_stmt|;
 comment|/* current offset */
-name|u_short
+name|u_int
 name|c
 decl_stmt|;
 comment|/* last cluster read */
@@ -278,36 +414,16 @@ name|DOS_FILE
 typedef|;
 end_typedef
 
-begin_comment
-comment|/*  * The following variable can be set to the address of an error-handling  * routine which will be invoked when a read() returns EIO.  The handler  * should return 1 to retry the read, otherwise 0.  */
-end_comment
-
-begin_function_decl
-specifier|extern
-name|int
-function_decl|(
-modifier|*
-name|dos_ioerr
-function_decl|)
-parameter_list|(
-name|int
-name|op
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_function_decl
 name|int
 name|dos_mount
 parameter_list|(
 name|DOS_FS
 modifier|*
-name|fs
 parameter_list|,
 specifier|const
 name|char
 modifier|*
-name|devname
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -318,18 +434,6 @@ name|dos_unmount
 parameter_list|(
 name|DOS_FS
 modifier|*
-name|fs
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|u_long
-name|dos_free
-parameter_list|(
-name|DOS_FS
-modifier|*
-name|fs
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -341,112 +445,10 @@ name|dos_open
 parameter_list|(
 name|DOS_FS
 modifier|*
-name|fs
 parameter_list|,
 specifier|const
 name|char
 modifier|*
-name|path
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|dos_close
-parameter_list|(
-name|void
-modifier|*
-name|v
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|fpos_t
-name|dos_seek
-parameter_list|(
-name|void
-modifier|*
-name|v
-parameter_list|,
-name|fpos_t
-name|offset
-parameter_list|,
-name|int
-name|whence
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|dos_read
-parameter_list|(
-name|void
-modifier|*
-name|v
-parameter_list|,
-name|char
-modifier|*
-name|buf
-parameter_list|,
-name|int
-name|nbytes
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|dos_stat
-parameter_list|(
-name|DOS_FS
-modifier|*
-name|fs
-parameter_list|,
-specifier|const
-name|char
-modifier|*
-name|path
-parameter_list|,
-name|struct
-name|stat
-modifier|*
-name|sb
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|dos_fstat
-parameter_list|(
-name|DOS_FILE
-modifier|*
-name|f
-parameter_list|,
-name|struct
-name|stat
-modifier|*
-name|sb
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
-name|dos_cvtime
-parameter_list|(
-name|time_t
-modifier|*
-name|timer
-parameter_list|,
-name|u_short
-name|ddate
-parameter_list|,
-name|u_short
-name|dtime
 parameter_list|)
 function_decl|;
 end_function_decl
