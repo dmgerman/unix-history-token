be12624a3c575876a45a976288353c502e22940b
Stefan Esser <se@FreeBSD.org>
1998-03-07 11:33:25 +0000

    Add PCI LKM support. Drivers that want to be loadable need to call the new function "pci_register_lkm(&pci_device, 0)" from their "load" entry point. The new PCIOCATTACHED ioctl on /dev/pci allows user-land programs to find out, whether a PCI device got a driver attached. The "pciconf" function has been extended to return that information for automatic PCI driver loading from /etc/rc.
    Former-commit-id: b41d21af0f9ede0d6560963347985816d0ba0cfb

--- sys/pci/pcibus.h
+++ sys/pci/pcibus.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/************************************************************************** ** **  $Id: pcibus.h,v 1.3 1995/11/21 12:54:54 bde Exp $ ** **  Declarations for pci bus driver. ** **  FreeBSD ** **------------------------------------------------------------------------- ** ** Copyright (c) 1995 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
+comment|/************************************************************************** ** **  $Id: pcibus.h,v 1.4 1996/01/30 22:59:55 mpp Exp $ ** **  Declarations for pci bus driver. ** **  FreeBSD ** **------------------------------------------------------------------------- ** ** Copyright (c) 1995 Wolfgang Stanglmeier.  All rights reserved. ** ** Redistribution and use in source and binary forms, with or without ** modification, are permitted provided that the following conditions ** are met: ** 1. Redistributions of source code must retain the above copyright **    notice, this list of conditions and the following disclaimer. ** 2. Redistributions in binary form must reproduce the above copyright **    notice, this list of conditions and the following disclaimer in the **    documentation and/or other materials provided with the distribution. ** 3. The name of the author may not be used to endorse or promote products **    derived from this software without specific prior written permission. ** ** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR ** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES ** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, ** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT ** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, ** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY ** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF ** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ** *************************************************************************** */
 end_comment
 
 begin_ifndef
@@ -95,9 +95,6 @@ name|u_long
 name|data
 parameter_list|)
 function_decl|;
-name|unsigned
-name|pb_maxirq
-decl_stmt|;
 name|int
 function_decl|(
 modifier|*
@@ -183,6 +180,21 @@ name|linker_set
 name|pcibus_set
 decl_stmt|;
 end_decl_stmt
+
+begin_function_decl
+name|int
+name|pci_register_lkm
+parameter_list|(
+name|struct
+name|pci_device
+modifier|*
+name|dvp
+parameter_list|,
+name|int
+name|if_revision
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_endif
 endif|#
