1256330bbd5b654875effbec2e3536015cd1e5b2
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-03-08 13:36:24 +0000

    MFC: cosmetic.
    Former-commit-id: f88365e327ea43e0676e863798ce45df644550ed

--- usr.bin/tcopy/tcopy.c
+++ usr.bin/tcopy/tcopy.c
@@ -54,7 +54,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: tcopy.c,v 1.2.2.1 1997/08/18 07:03:43 charnier Exp $"
+literal|"$Id: tcopy.c,v 1.2.2.2 1997/08/29 05:29:58 imp Exp $"
 decl_stmt|;
 end_decl_stmt
 
