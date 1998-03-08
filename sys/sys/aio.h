ab13eb142467db4011abd5bccbd5ae49f1f1a4b6
Peter Dufault <dufault@FreeBSD.org>
1998-03-08 22:21:12 +0000

    Put sigevent and AIO_LISTIO_MAX back in aio.h so that kernels can be built.
    Former-commit-id: 6a7ac39c882c6fc05304d30cc78a55ab61209a11

--- sys/sys/aio.h
+++ sys/sys/aio.h
@@ -12,7 +12,7 @@ name|_AIO_H_
 end_define
 
 begin_comment
-comment|/*  * Copyright (c) 1997 John S. Dyson.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. John S. Dyson's name may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * DISCLAIMER:  This code isn't warranted to do anything useful.  Anything  * bad that happens because of using this software isn't the responsibility  * of the author.  This software is distributed AS-IS.  *  * $Id: aio.h,v 1.3 1997/12/08 02:18:14 dyson Exp $  */
+comment|/*  * Copyright (c) 1997 John S. Dyson.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. John S. Dyson's name may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * DISCLAIMER:  This code isn't warranted to do anything useful.  Anything  * bad that happens because of using this software isn't the responsibility  * of the author.  This software is distributed AS-IS.  *  * $Id: aio.h,v 1.4 1998/03/08 17:25:30 dufault Exp $  */
 end_comment
 
 begin_include
@@ -22,146 +22,164 @@ file|<sys/types.h>
 end_include
 
 begin_comment
-comment|/*  * Returned by aio_cancel:  *  (Note that FreeBSD's aio is not cancellable -- yet.)  */
+comment|/**************************************************************************/
 end_comment
 
-begin_define
-define|#
-directive|define
-name|AIO_CANCELED
-value|0x1
-end_define
-
-begin_define
-define|#
-directive|define
-name|AIO_NOTCANCELED
-value|0x2
-end_define
-
-begin_define
-define|#
-directive|define
-name|AIO_ALLDONE
-value|0x3
-end_define
-
 begin_comment
-comment|/*  * LIO opcodes  */
+comment|/* Additions to signal.h -- hack alert.                                   */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|LIO_NOP
-value|0x0
-end_define
-
-begin_define
-define|#
-directive|define
-name|LIO_WRITE
-value|0x1
-end_define
-
-begin_define
-define|#
-directive|define
-name|LIO_READ
-value|0x2
-end_define
-
 begin_comment
-comment|/*  * LIO modes  */
+comment|/**************************************************************************/
 end_comment
 
-begin_define
-define|#
-directive|define
-name|LIO_NOWAIT
-value|0x0
-end_define
-
-begin_define
-define|#
-directive|define
-name|LIO_WAIT
-value|0x1
-end_define
-
 begin_comment
-comment|/*  * Private mode bit for aio.  * (This bit is set by the library routine  *  to allow the kernel to support sync  *  or async operations in the future.)  */
+comment|/*  * sigval structure:  */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|AIO_PMODE_SYNC
-value|0x1
-end_define
-
-begin_define
-define|#
-directive|define
-name|AIO_PMODE_DONE
-value|0x2
-end_define
-
-begin_define
-define|#
-directive|define
-name|AIO_PMODE_SUSPEND
-value|0x4
-end_define
+begin_union
+union|union
+name|sigval
+block|{
+name|int
+name|sival_int
+decl_stmt|;
+name|void
+modifier|*
+name|sival_ptr
+decl_stmt|;
+block|}
+union|;
+end_union
 
 begin_comment
-comment|/*  * I/O active flag -- used for compat  * with kernel.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|AIO_PMODE_ACTIVE
-value|0x2357c0de
-end_define
-
-begin_comment
-comment|/*  * Private members for aiocb -- don't access  * directly.  */
+comment|/*  * this is the sigevent structure:  */
 end_comment
 
 begin_struct
 struct|struct
+name|sigevent
+block|{
+name|int
+name|sigev_notify
+decl_stmt|;
+comment|/* Notification */
+name|int
+name|sigev_signo
+decl_stmt|;
+comment|/* Signal number */
+name|union
+name|sigval
+name|sigev_value
+decl_stmt|;
+comment|/* Not used yet in FreeBSD */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * values for sigev_notify:  */
+end_comment
+
+begin_expr_stmt
+name|define
+name|SIGEV_NONE
+literal|0
+comment|/* Don't post a signal */
+define|#
+directive|define
+name|SIGEV_SIGNAL
+value|1
+comment|/* Post specified signal */
+comment|/*  * Returned by aio_cancel:  *  (Note that FreeBSD's aio is not cancellable -- yet.)  */
+define|#
+directive|define
+name|AIO_CANCELED
+value|0x1
+define|#
+directive|define
+name|AIO_NOTCANCELED
+value|0x2
+define|#
+directive|define
+name|AIO_ALLDONE
+value|0x3
+comment|/*  * LIO opcodes  */
+define|#
+directive|define
+name|LIO_NOP
+value|0x0
+define|#
+directive|define
+name|LIO_WRITE
+value|0x1
+define|#
+directive|define
+name|LIO_READ
+value|0x2
+comment|/*  * LIO modes  */
+define|#
+directive|define
+name|LIO_NOWAIT
+value|0x0
+define|#
+directive|define
+name|LIO_WAIT
+value|0x1
+comment|/*  * Maximum number of allowed LIO operations  */
+define|#
+directive|define
+name|AIO_LISTIO_MAX
+value|16
+comment|/*  * Private mode bit for aio.  * (This bit is set by the library routine  *  to allow the kernel to support sync  *  or async operations in the future.)  */
+define|#
+directive|define
+name|AIO_PMODE_SYNC
+value|0x1
+define|#
+directive|define
+name|AIO_PMODE_DONE
+value|0x2
+define|#
+directive|define
+name|AIO_PMODE_SUSPEND
+value|0x4
+define|#
+directive|define
+name|AIO_PMODE_ACTIVE
+value|0x2357c0de
+comment|/*  * Private members for aiocb -- don't access  * directly.  */
+expr|struct
 name|__aiocb_private
 block|{
 name|int
 name|status
-decl_stmt|;
+block|;
 name|int
 name|error
-decl_stmt|;
+block|;
 name|int
 name|privatemodes
-decl_stmt|;
+block|;
 name|int
 name|active
-decl_stmt|;
+block|;
 name|int
 name|tid
-decl_stmt|;
+block|;
 name|int
 name|threadinfo
-decl_stmt|;
+block|;
 name|void
-modifier|*
+operator|*
 name|userinfo
-decl_stmt|;
+block|;
 name|void
-modifier|*
+operator|*
 name|kernelinfo
-decl_stmt|;
-block|}
-struct|;
-end_struct
+block|; }
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/*  * I/O control block  */
