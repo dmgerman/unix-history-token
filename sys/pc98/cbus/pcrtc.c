519e6143e2d7eff1031552fd459e9f0e8d7edd05
Takenori KATO <kato@FreeBSD.org>
1998-03-17 08:42:18 +0000

    Sync with sys/i386/isa/clock.c revision 1.117.
    Former-commit-id: 63f97928811b2b1d4cf2c63293aec0e88e0591ac

--- sys/pc98/cbus/pcrtc.c
+++ sys/pc98/cbus/pcrtc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.48 1998/03/07 15:43:43 kato Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.49 1998/03/15 13:35:42 kato Exp $  */
 end_comment
 
 begin_comment
@@ -126,6 +126,31 @@ include|#
 directive|include
 file|<machine/md_var.h>
 end_include
+
+begin_if
+if|#
+directive|if
+name|NAPM
+operator|>
+literal|0
+end_if
+
+begin_include
+include|#
+directive|include
+file|<machine/apm_bios.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<i386/apm/apm_setup.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifdef
 ifdef|#
@@ -3243,26 +3268,6 @@ name|tsc_present
 operator|=
 literal|0
 expr_stmt|;
-ifdef|#
-directive|ifdef
-name|SMP
-name|tsc_present
-operator|=
-literal|0
-expr_stmt|;
-endif|#
-directive|endif
-if|#
-directive|if
-name|NAPM
-operator|>
-literal|0
-name|tsc_present
-operator|=
-literal|0
-expr_stmt|;
-endif|#
-directive|endif
 ifndef|#
 directive|ifndef
 name|PC98
@@ -3485,6 +3490,30 @@ expr_stmt|;
 endif|#
 directive|endif
 block|}
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|SMP
+argument_list|)
+comment|/* 	 * We can not use the TSC in SMP mode, until we figure out a 	 * cheap (impossible), reliable and precise (yeah right!)  way 	 * to synchronize the TSCs of all the CPUs. 	 * Curse Intel for leaving the counter out of the I/O APIC. 	 */
+if|#
+directive|if
+name|NAPM
+operator|>
+literal|0
+comment|/* 	 * We can not use the TSC if we found an APM bios.  Too many 	 * of them lie about their ability&intention to fiddle the CPU 	 * clock for us to rely on this.  Precise timekeeping on an 	 * APM'ed machine is at best a fools pursuit anyway, since  	 * any and all of the time spent in various SMM code can't  	 * be reliably accounted for.  Reading the RTC is your only 	 * source of reliable time info.  The i8254 looses too of course 	 * but we need to have some kind of time... 	 */
+if|if
+condition|(
+name|apm_version
+operator|!=
+name|APMINI_CANTFIND
+condition|)
+return|return;
+endif|#
+directive|endif
+comment|/* NAPM> 0 */
 if|if
 condition|(
 name|tsc_present
@@ -3509,6 +3538,9 @@ name|tsc_timecounter
 argument_list|)
 expr_stmt|;
 block|}
+endif|#
+directive|endif
+comment|/* !defined(SMP) */
 block|}
 end_function
 
