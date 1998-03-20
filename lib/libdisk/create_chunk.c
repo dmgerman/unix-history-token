5514a2b7ade265f96d35bee77cc03caaea7f6d7a
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-03-20 23:43:04 +0000

    MF22: teach about LS-120 devices.
    Former-commit-id: 44d176797fd7a879c1fcb7d17a7b5731e01081f3

--- lib/libdisk/create_chunk.c
+++ lib/libdisk/create_chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.28 1998/01/16 12:50:36 jkh Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.29 1998/02/10 17:19:12 jkh Exp $  *  */
 end_comment
 
 begin_include
@@ -1571,6 +1571,27 @@ operator|,
 name|cmaj
 operator|=
 literal|70
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+operator|!
+name|strncmp
+argument_list|(
+name|p
+argument_list|,
+literal|"wfd"
+argument_list|,
+literal|3
+argument_list|)
+condition|)
+name|bmaj
+operator|=
+literal|1
+operator|,
+name|cmaj
+operator|=
+literal|87
 expr_stmt|;
 elseif|else
 if|if
--- lib/libdisk/disk.c
+++ lib/libdisk/disk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: disk.c,v 1.30 1998/03/07 08:45:46 ache Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: disk.c,v 1.31 1998/03/14 21:14:31 ache Exp $  *  */
 end_comment
 
 begin_include
@@ -1492,6 +1492,8 @@ block|,
 literal|"da"
 block|,
 literal|"od"
+block|,
+literal|"wfd"
 block|,
 literal|0
 block|}
