053fccbd18f3014e0250eee1549093f0fc2dcf51
Peter Wemm <peter@FreeBSD.org>
1998-03-21 18:01:50 +0000

    Import pppd from ppp-2.3.3 onto the vendor branch (on 1.1.2 hopefully)
    Former-commit-id: 5d2708dda071c88abcdc15091ef0d4e9d9f7dcfa

--- usr.sbin/pppd/chap_ms.h
+++ usr.sbin/pppd/chap_ms.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * chap.h - Challenge Handshake Authentication Protocol definitions.  *  * Copyright (c) 1995 Eric Rosenquist, Strata Software Limited.  * http://www.strataware.com/  *  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by Eric Rosenquist.  The name of the author may not be used to  * endorse or promote products derived from this software without  * specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: chap_ms.h,v 1.1 1996/05/28 00:42:31 paulus Exp $  */
+comment|/*  * chap.h - Challenge Handshake Authentication Protocol definitions.  *  * Copyright (c) 1995 Eric Rosenquist, Strata Software Limited.  * http://www.strataware.com/  *  * All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by Eric Rosenquist.  The name of the author may not be used to  * endorse or promote products derived from this software without  * specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: chap_ms.h,v 1.2 1997/11/27 06:08:10 paulus Exp $  */
 end_comment
 
 begin_ifndef
@@ -8,6 +8,17 @@ ifndef|#
 directive|ifndef
 name|__CHAPMS_INCLUDE__
 end_ifndef
+
+begin_define
+define|#
+directive|define
+name|MD4_SIGNATURE_SIZE
+value|16
+end_define
+
+begin_comment
+comment|/* 16 bytes in a MD4 message digest */
+end_comment
 
 begin_define
 define|#
