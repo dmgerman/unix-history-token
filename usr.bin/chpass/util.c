876f19b0c2dd2ef2c8727a6d8984c9253ff3ea16
Philippe Charnier <charnier@FreeBSD.org>
1998-03-23 07:41:49 +0000

    Add rcsid. Correct -Wall warning.
    Former-commit-id: 3d6e55b91059a8d883de1a14f9c95df06d647c21

--- usr.bin/chpass/util.c
+++ usr.bin/chpass/util.c
@@ -9,13 +9,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)util.c	8.4 (Berkeley) 4/2/94";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)util.c	8.4 (Berkeley) 4/2/94"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
@@ -592,10 +605,12 @@ argument_list|()
 expr_stmt|;
 while|while
 condition|(
+operator|(
 name|sh
 operator|=
 name|getusershell
 argument_list|()
+operator|)
 condition|)
 block|{
 if|if
