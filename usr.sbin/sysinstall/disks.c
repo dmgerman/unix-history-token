17a9f99fcfdfe363378bd29f016bd2fe99a91d35
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-03-23 08:36:27 +0000

    MF22: resync.
    Former-commit-id: 72c71ba60551a609d59c524f525669b6f3902ab9

--- usr.sbin/sysinstall/disks.c
+++ usr.sbin/sysinstall/disks.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: disks.c,v 1.96 1998/03/19 15:07:20 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: disks.c,v 1.97 1998/03/23 05:59:10 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -2079,8 +2079,16 @@ operator|=
 name|FALSE
 expr_stmt|;
 comment|/* Don't trash the MBR if the first (and therefore only) chunk is marked for a truly dedicated 	     * disk (i.e., the disklabel starts at sector 0), even in cases where the user has requested 	     * booteasy or a "standard" MBR -- both would be fatal in this case. 	     */
+if|#
+directive|if
+literal|0
+block|if ((d->chunks->part->flags& CHUNK_FORCE_ALL) != CHUNK_FORCE_ALL&& (mbrContents = getBootMgr(d->name)) != NULL) 		Set_Boot_Mgr(d, mbrContents);
+else|#
+directive|else
+comment|/* 	     * Don't offer to update the MBR on this disk if the first "real" chunk looks like 	     * a FreeBSD "all disk" partition, or the disk is entirely FreeBSD.  	     */
 if|if
 condition|(
+operator|(
 operator|(
 name|d
 operator|->
@@ -2088,12 +2096,23 @@ name|chunks
 operator|->
 name|part
 operator|->
-name|flags
-operator|&
-name|CHUNK_FORCE_ALL
-operator|)
+name|type
 operator|!=
-name|CHUNK_FORCE_ALL
+name|freebsd
+operator|)
+operator|||
+operator|(
+name|d
+operator|->
+name|chunks
+operator|->
+name|part
+operator|->
+name|offset
+operator|>
+literal|1
+operator|)
+operator|)
 operator|&&
 operator|(
 name|mbrContents
@@ -2115,6 +2134,8 @@ argument_list|,
 name|mbrContents
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 break|break;
 default|default:
 name|beep
@@ -2644,6 +2665,21 @@ argument_list|(
 name|DISK_PARTITIONED
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|cp
+operator|&&
+operator|!
+name|strcmp
+argument_list|(
+name|cp
+argument_list|,
+literal|"written"
+argument_list|)
+condition|)
+return|return
+name|DITEM_SUCCESS
+return|;
 for|for
 control|(
 name|i
