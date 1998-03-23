c827bcc818af62c77af0d431897f81f6d2634338
Jonathan Lemon <jlemon@FreeBSD.org>
1998-03-23 19:52:59 +0000

    Add the ability to make real-mode BIOS calls from the kernel.  Currently, everything is contained inside #ifdef VM86, so this option must be present in the config file to use this functionality.
    Thanks to Tor Egge, these changes should work on SMP machines.  However,
    it may not be throughly SMP-safe.
    
    Currently, the only BIOS calls made are memory-sizing routines at bootup,
    these replace reading the RTC values.
    
    
    Former-commit-id: 564c864eca85687f5966278b9566a0ef37f65b35

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 1994, David Greenman  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the University of Utah, and William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)trap.c	7.4 (Berkeley) 5/13/91  *	$Id: trap.c,v 1.121 1998/02/04 22:32:12 eivind Exp $  */
+comment|/*-  * Copyright (C) 1994, David Greenman  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the University of Utah, and William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)trap.c	7.4 (Berkeley) 5/13/91  *	$Id: trap.c,v 1.122 1998/02/06 12:13:10 eivind Exp $  */
 end_comment
 
 begin_comment
@@ -795,6 +795,102 @@ name|frame
 operator|.
 name|tf_err
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|VM86
+if|if
+condition|(
+name|in_vm86call
+condition|)
+block|{
+if|if
+condition|(
+name|frame
+operator|.
+name|tf_eflags
+operator|&
+name|PSL_VM
+operator|&&
+operator|(
+name|type
+operator|==
+name|T_PROTFLT
+operator|||
+name|type
+operator|==
+name|T_STKFLT
+operator|)
+condition|)
+block|{
+name|i
+operator|=
+name|vm86_emulate
+argument_list|(
+operator|(
+expr|struct
+name|vm86frame
+operator|*
+operator|)
+operator|&
+name|frame
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|i
+operator|!=
+literal|0
+condition|)
+comment|/* 				 * returns to original process 				 */
+name|vm86_trap
+argument_list|(
+operator|(
+expr|struct
+name|vm86frame
+operator|*
+operator|)
+operator|&
+name|frame
+argument_list|)
+expr_stmt|;
+return|return;
+block|}
+switch|switch
+condition|(
+name|type
+condition|)
+block|{
+comment|/* 			 * these traps want either a process context, or 			 * assume a normal userspace trap. 			 */
+case|case
+name|T_PROTFLT
+case|:
+case|case
+name|T_SEGNPFLT
+case|:
+name|trap_fatal
+argument_list|(
+operator|&
+name|frame
+argument_list|)
+expr_stmt|;
+return|return;
+case|case
+name|T_TRCTRAP
+case|:
+name|type
+operator|=
+name|T_BPTFLT
+expr_stmt|;
+comment|/* kernel breakpoint */
+comment|/* FALL THROUGH */
+block|}
+goto|goto
+name|kernel_trap
+goto|;
+comment|/* normal kernel trap handling */
+block|}
+endif|#
+directive|endif
 if|if
 condition|(
 operator|(
@@ -1267,6 +1363,13 @@ block|}
 block|}
 else|else
 block|{
+ifdef|#
+directive|ifdef
+name|VM86
+name|kernel_trap
+label|:
+endif|#
+directive|endif
 comment|/* kernel trap */
 switch|switch
 condition|(
