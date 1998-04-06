531e25bd980b04c6bb7bb355ef492d6fa0a69352
Poul-Henning Kamp <phk@FreeBSD.org>
1998-04-06 08:26:08 +0000

    Make a kernel version of the timer* functions called timerval* to be more consistent.
    OK'ed by:	bde
    
    
    Former-commit-id: feb1ca7494d6723a739fce10af4764bb431c81c4

--- sys/compat/linux/linux_misc.c
+++ sys/compat/linux/linux_misc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: linux_misc.c,v 1.35 1998/03/30 09:49:34 phk Exp $  */
+comment|/*-  * Copyright (c) 1994-1995 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: linux_misc.c,v 1.36 1998/04/04 18:56:54 phk Exp $  */
 end_comment
 
 begin_include
@@ -268,7 +268,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|timerisset
+name|timevalisset
 argument_list|(
 operator|&
 name|old_it
@@ -278,7 +278,7 @@ argument_list|)
 condition|)
 if|if
 condition|(
-name|timercmp
+name|timevalcmp
 argument_list|(
 operator|&
 name|old_it
@@ -291,7 +291,7 @@ argument_list|,
 operator|<
 argument_list|)
 condition|)
-name|timerclear
+name|timevalclear
 argument_list|(
 operator|&
 name|old_it
@@ -345,7 +345,7 @@ expr_stmt|;
 comment|/* XXX Still needed ? */
 if|if
 condition|(
-name|timerisset
+name|timevalisset
 argument_list|(
 operator|&
 name|p
@@ -377,7 +377,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|timerisset
+name|timevalisset
 argument_list|(
 operator|&
 name|it
@@ -1922,7 +1922,7 @@ name|tv_sec
 operator|<
 literal|0
 condition|)
-name|timerclear
+name|timevalclear
 argument_list|(
 operator|&
 name|utv
@@ -2060,7 +2060,7 @@ name|tv_sec
 operator|<
 literal|0
 condition|)
-name|timerclear
+name|timevalclear
 argument_list|(
 operator|&
 name|utv
@@ -2068,7 +2068,7 @@ argument_list|)
 expr_stmt|;
 block|}
 else|else
-name|timerclear
+name|timevalclear
 argument_list|(
 operator|&
 name|utv
--- sys/sys/timetc.h
+++ sys/sys/timetc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)time.h	8.5 (Berkeley) 5/4/95  * $Id: time.h,v 1.22 1998/03/30 09:55:35 phk Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)time.h	8.5 (Berkeley) 5/4/95  * $Id: time.h,v 1.23 1998/04/04 13:26:16 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -350,7 +350,7 @@ parameter_list|,
 name|uvp
 parameter_list|)
 define|\
-value|do {								\ 		(vvp)->tv_sec += (uvp)->tv_sec;		\ 		(vvp)->tv_nsec += (uvp)->tv_nsec;	\ 		if ((vvp)->tv_nsec>= 1000000000) {			\ 			(vvp)->tv_sec++;				\ 			(vvp)->tv_nsec -= 1000000000;			\ 		}							\ 	} while (0)
+value|do {								\ 		(vvp)->tv_sec += (uvp)->tv_sec;				\ 		(vvp)->tv_nsec += (uvp)->tv_nsec;			\ 		if ((vvp)->tv_nsec>= 1000000000) {			\ 			(vvp)->tv_sec++;				\ 			(vvp)->tv_nsec -= 1000000000;			\ 		}							\ 	} while (0)
 end_define
 
 begin_define
@@ -363,12 +363,32 @@ parameter_list|,
 name|uvp
 parameter_list|)
 define|\
-value|do {								\ 		(vvp)->tv_sec -= (uvp)->tv_sec;		\ 		(vvp)->tv_nsec -= (uvp)->tv_nsec;	\ 		if ((vvp)->tv_nsec< 0) {				\ 			(vvp)->tv_sec--;				\ 			(vvp)->tv_nsec += 1000000000;			\ 		}							\ 	} while (0)
+value|do {								\ 		(vvp)->tv_sec -= (uvp)->tv_sec;				\ 		(vvp)->tv_nsec -= (uvp)->tv_nsec;			\ 		if ((vvp)->tv_nsec< 0) {				\ 			(vvp)->tv_sec--;				\ 			(vvp)->tv_nsec += 1000000000;			\ 		}							\ 	} while (0)
 end_define
 
 begin_comment
 comment|/* Operations on timevals. */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|timevalclear
+parameter_list|(
+name|tvp
+parameter_list|)
+value|(tvp)->tv_sec = (tvp)->tv_usec = 0
+end_define
+
+begin_define
+define|#
+directive|define
+name|timevalisset
+parameter_list|(
+name|tvp
+parameter_list|)
+value|((tvp)->tv_sec || (tvp)->tv_usec)
+end_define
 
 begin_define
 define|#
@@ -385,6 +405,10 @@ define|\
 value|(((tvp)->tv_sec == (uvp)->tv_sec) ?				\ 	    ((tvp)->tv_usec cmp (uvp)->tv_usec) :			\ 	    ((tvp)->tv_sec cmp (uvp)->tv_sec))
 end_define
 
+begin_comment
+comment|/* timevaladd and timevalsub are not inlined */
+end_comment
+
 begin_endif
 endif|#
 directive|endif
@@ -394,8 +418,14 @@ begin_comment
 comment|/* KERNEL */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|KERNEL
+end_ifndef
+
 begin_comment
-comment|/* Operations on timevals. */
+comment|/* NetBSD/OpenBSD compatable interfaces */
 end_comment
 
 begin_define
@@ -432,20 +462,6 @@ parameter_list|)
 define|\
 value|(((tvp)->tv_sec == (uvp)->tv_sec) ?				\ 	    ((tvp)->tv_usec cmp (uvp)->tv_usec) :			\ 	    ((tvp)->tv_sec cmp (uvp)->tv_sec))
 end_define
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|KERNEL
-end_ifndef
-
-begin_comment
-comment|/* use timevaladd/timevalsub in kernel */
-end_comment
-
-begin_comment
-comment|/* NetBSD/OpenBSD compatable interfaces */
-end_comment
 
 begin_define
 define|#
@@ -645,18 +661,6 @@ begin_decl_stmt
 specifier|extern
 name|time_t
 name|time_second
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|void
-name|forward_timecounter
-name|__P
-argument_list|(
-operator|(
-name|void
-operator|)
-argument_list|)
 decl_stmt|;
 end_decl_stmt
 
