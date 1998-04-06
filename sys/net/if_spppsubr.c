f5cd9d53240aaab14f5f68bc6f9a6c876de6b93d
Poul-Henning Kamp <phk@FreeBSD.org>
1998-04-06 09:30:42 +0000

    Make read_random() take a (void *) argument instead of (char *)
    Former-commit-id: 181cc169392f30f82d7d8a8f60ffb72b93f3f1bc

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994-1996 Cronyx Engineering Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 2.4, Thu Apr 30 17:17:21 MSD 1997  *  * $Id: if_spppsubr.c,v 1.35 1998/03/30 09:52:06 phk Exp $  */
+comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994-1996 Cronyx Engineering Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 2.4, Thu Apr 30 17:17:21 MSD 1997  *  * $Id: if_spppsubr.c,v 1.36 1998/04/04 13:26:03 phk Exp $  */
 end_comment
 
 begin_include
@@ -5469,10 +5469,6 @@ expr_stmt|;
 comment|/* Generate new local sequence number */
 name|read_random
 argument_list|(
-operator|(
-name|char
-operator|*
-operator|)
 operator|&
 name|sp
 operator|->
@@ -11331,10 +11327,6 @@ argument_list|)
 expr_stmt|;
 name|read_random
 argument_list|(
-operator|(
-name|char
-operator|*
-operator|)
 operator|&
 name|sp
 operator|->
@@ -12122,10 +12114,6 @@ name|magic
 condition|)
 name|read_random
 argument_list|(
-operator|(
-name|char
-operator|*
-operator|)
 operator|&
 name|sp
 operator|->
