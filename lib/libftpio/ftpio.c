14cd752e3334693f2c254e61817271948e0eee3b
Poul-Henning Kamp <phk@FreeBSD.org>
1998-04-11 07:28:53 +0000

    Correctly figure out that the remove cannot do passive mode.
    PR:		6259
    Reviewed & slightly modified by:	phk
    Submitted by:	Archie Cobbs <archie@whistle.com>
    
    Former-commit-id: 42f5ef4f37ac22aa9956d241838b58a398c388b2

--- lib/libftpio/ftpio.c
+++ lib/libftpio/ftpio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * Major Changelog:  *  * Jordan K. Hubbard  * 17 Jan 1996  *  * Turned inside out. Now returns xfers as new file ids, not as a special  * `state' of FTP_t  *  * $Id: ftpio.c,v 1.28 1997/10/02 23:26:03 fenner Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * Major Changelog:  *  * Jordan K. Hubbard  * 17 Jan 1996  *  * Turned inside out. Now returns xfers as new file ids, not as a special  * `state' of FTP_t  *  * $Id: ftpio.c,v 1.29 1997/12/20 04:06:05 jb Exp $  *  */
 end_comment
 
 begin_include
@@ -1736,6 +1736,15 @@ literal|0
 condition|)
 return|return
 name|i
+return|;
+if|if
+condition|(
+name|i
+operator|!=
+name|FTP_PASSIVE_HAPPY
+condition|)
+return|return
+name|FAILURE
 return|;
 name|ftp
 operator|->
