53bf9574664140cdbcf335b2a350578b5d16ec55
Dag-Erling Smorgrav <des@FreeBSD.org>
1998-04-27 14:17:30 +0000

    Cast return values to the appropriate fp_*_t. Note that the man page incorrectly refers to them as e.g. fp_except rather than fp_except_t.
    PR:		misc/6310
    Submitted by:	Niall Smart
    
    Former-commit-id: 5657349ae13b246e5a77b34a2fa4fbbc5dc1a350

--- sys/amd64/include/floatingpoint.h
+++ sys/amd64/include/floatingpoint.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 Andrew Moore, Talke Studio  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#) floatingpoint.h	1.0 (Berkeley) 9/23/93  *	$Id$  */
+comment|/*-  * Copyright (c) 1993 Andrew Moore, Talke Studio  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#) floatingpoint.h	1.0 (Berkeley) 9/23/93  *	$Id: floatingpoint.h,v 1.8 1997/02/22 09:34:37 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -233,7 +233,7 @@ define|#
 directive|define
 name|fpgetround
 parameter_list|()
-value|((__fpgetreg(FP_RND_REG)& FP_RND_FLD)>> FP_RND_OFF)
+value|((fp_rnd_t) ((__fpgetreg(FP_RND_REG)& FP_RND_FLD)>> FP_RND_OFF))
 end_define
 
 begin_define
@@ -243,7 +243,7 @@ name|fpsetround
 parameter_list|(
 name|m
 parameter_list|)
-value|__fpsetreg((m), FP_RND_REG, FP_RND_FLD, FP_RND_OFF)
+value|((fp_rnd_t) __fpsetreg((m), FP_RND_REG, FP_RND_FLD, FP_RND_OFF))
 end_define
 
 begin_define
@@ -251,7 +251,7 @@ define|#
 directive|define
 name|fpgetprec
 parameter_list|()
-value|((__fpgetreg(FP_PRC_REG)& FP_PRC_FLD)>> FP_PRC_OFF)
+value|((fp_prec_t) ((__fpgetreg(FP_PRC_REG)& FP_PRC_FLD)>> FP_PRC_OFF))
 end_define
 
 begin_define
@@ -261,7 +261,7 @@ name|fpsetprec
 parameter_list|(
 name|m
 parameter_list|)
-value|__fpsetreg((m), FP_PRC_REG, FP_PRC_FLD, FP_PRC_OFF)
+value|((fp_prec_t) __fpsetreg((m), FP_PRC_REG, FP_PRC_FLD, FP_PRC_OFF))
 end_define
 
 begin_define
@@ -269,7 +269,7 @@ define|#
 directive|define
 name|fpgetmask
 parameter_list|()
-value|((~__fpgetreg(FP_MSKS_REG)& FP_MSKS_FLD)>> FP_MSKS_OFF)
+value|((fp_except_t) ((~__fpgetreg(FP_MSKS_REG)& FP_MSKS_FLD)>> FP_MSKS_OFF))
 end_define
 
 begin_define
@@ -279,7 +279,7 @@ name|fpsetmask
 parameter_list|(
 name|m
 parameter_list|)
-value|__fpsetreg(~(m), FP_MSKS_REG, FP_MSKS_FLD, FP_MSKS_OFF)
+value|((fp_except_t) (__fpsetreg(~(m), FP_MSKS_REG, FP_MSKS_FLD, FP_MSKS_OFF)))
 end_define
 
 begin_define
@@ -287,7 +287,7 @@ define|#
 directive|define
 name|fpgetsticky
 parameter_list|()
-value|((__fpgetreg(FP_STKY_REG)& FP_STKY_FLD)>> FP_STKY_OFF)
+value|((fp_except_t) ((__fpgetreg(FP_STKY_REG)& FP_STKY_FLD)>> FP_STKY_OFF))
 end_define
 
 begin_define
@@ -297,7 +297,7 @@ name|fpresetsticky
 parameter_list|(
 name|m
 parameter_list|)
-value|__fpsetreg(0, FP_STKY_REG, (m), FP_STKY_OFF)
+value|((fp_except_t) __fpsetreg(0, FP_STKY_REG, (m), FP_STKY_OFF))
 end_define
 
 begin_define
