e7a2bee2d99b20f1e68bdee2d1115a8b12af9e65
David Greenman <dg@FreeBSD.org>
1998-04-28 07:02:33 +0000

    Oops, revert part of a diff that wasn't supposed to have been committed.
    Former-commit-id: ffac2a549fd8bef96974115afe57c88edf9b4699

--- lib/libutil/setproctitle.c
+++ lib/libutil/setproctitle.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, is permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Peter Wemm.  *  * $Id: setproctitle.c,v 1.5 1997/02/22 15:08:33 peter Exp $  */
+comment|/*  * Copyright (c) 1995 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, is permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Peter Wemm.  *  * $Id: setproctitle.c,v 1.6 1998/04/28 06:59:14 dg Exp $  */
 end_comment
 
 begin_include
@@ -135,7 +135,7 @@ begin_define
 define|#
 directive|define
 name|SPT_BUFSIZE
-value|128
+value|2048
 end_define
 
 begin_comment
