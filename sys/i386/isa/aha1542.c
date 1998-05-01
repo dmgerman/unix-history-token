2bd8afbae448d8bd70140421cb25782d8bda661c
Bruce Evans <bde@FreeBSD.org>
1998-05-01 18:30:02 +0000

    Don't depend on "implicit int".
    Former-commit-id: c10fdcd8e6b1bf2d3d67d59ee7e82fb7310d3b69

--- sys/i386/isa/aha1542.c
+++ sys/i386/isa/aha1542.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.75 1998/02/21 10:11:43 eivind Exp $  */
+comment|/*  * (Mostly) Written by Julian Elischer (julian@tfs.com)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  *      $Id: aha1542.c,v 1.76 1998/04/17 22:36:29 des Exp $  */
 end_comment
 
 begin_comment
@@ -2042,8 +2042,9 @@ name|u_char
 name|data
 decl_stmt|;
 specifier|register
+name|int
 name|i
-expr_stmt|;
+decl_stmt|;
 name|int
 name|sts
 decl_stmt|;
@@ -2831,8 +2832,9 @@ name|char
 name|stat
 decl_stmt|;
 specifier|register
+name|int
 name|i
-expr_stmt|;
+decl_stmt|;
 name|struct
 name|aha_data
 modifier|*
