4c51898eadaea704e29ea61f9fc29dae200ed60d
John Birrell <jb@FreeBSD.org>
1998-05-10 20:51:46 +0000

    Treat all internet addresses as u_int32_t.
    Former-commit-id: 5379e69fb2072b8d30459151cc9b87c48fc96613

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)in.h	8.3 (Berkeley) 1/3/94  * $Id: in.h,v 1.30 1998/02/25 02:35:35 julian Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)in.h	8.3 (Berkeley) 1/3/94  * $Id: in.h,v 1.31 1998/04/19 17:22:27 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -1161,7 +1161,7 @@ begin_struct
 struct|struct
 name|in_addr
 block|{
-name|u_long
+name|u_int32_t
 name|s_addr
 decl_stmt|;
 block|}
@@ -1179,7 +1179,7 @@ name|IN_CLASSA
 parameter_list|(
 name|i
 parameter_list|)
-value|(((long)(i)& 0x80000000) == 0)
+value|(((u_int32_t)(i)& 0x80000000) == 0)
 end_define
 
 begin_define
@@ -1217,7 +1217,7 @@ name|IN_CLASSB
 parameter_list|(
 name|i
 parameter_list|)
-value|(((long)(i)& 0xc0000000) == 0x80000000)
+value|(((u_int32_t)(i)& 0xc0000000) == 0x80000000)
 end_define
 
 begin_define
@@ -1255,7 +1255,7 @@ name|IN_CLASSC
 parameter_list|(
 name|i
 parameter_list|)
-value|(((long)(i)& 0xe0000000) == 0xc0000000)
+value|(((u_int32_t)(i)& 0xe0000000) == 0xc0000000)
 end_define
 
 begin_define
@@ -1286,7 +1286,7 @@ name|IN_CLASSD
 parameter_list|(
 name|i
 parameter_list|)
-value|(((long)(i)& 0xf0000000) == 0xe0000000)
+value|(((u_int32_t)(i)& 0xf0000000) == 0xe0000000)
 end_define
 
 begin_define
@@ -1339,7 +1339,7 @@ name|IN_EXPERIMENTAL
 parameter_list|(
 name|i
 parameter_list|)
-value|(((long)(i)& 0xf0000000) == 0xf0000000)
+value|(((u_int32_t)(i)& 0xf0000000) == 0xf0000000)
 end_define
 
 begin_define
@@ -1349,28 +1349,28 @@ name|IN_BADCLASS
 parameter_list|(
 name|i
 parameter_list|)
-value|(((long)(i)& 0xf0000000) == 0xf0000000)
+value|(((u_int32_t)(i)& 0xf0000000) == 0xf0000000)
 end_define
 
 begin_define
 define|#
 directive|define
 name|INADDR_ANY
-value|(u_long)0x00000000
+value|(u_int32_t)0x00000000
 end_define
 
 begin_define
 define|#
 directive|define
 name|INADDR_LOOPBACK
-value|(u_long)0x7f000001
+value|(u_int32_t)0x7f000001
 end_define
 
 begin_define
 define|#
 directive|define
 name|INADDR_BROADCAST
-value|(u_long)0xffffffff
+value|(u_int32_t)0xffffffff
 end_define
 
 begin_comment
@@ -1403,7 +1403,7 @@ begin_define
 define|#
 directive|define
 name|INADDR_UNSPEC_GROUP
-value|(u_long)0xe0000000
+value|(u_int32_t)0xe0000000
 end_define
 
 begin_comment
@@ -1414,7 +1414,7 @@ begin_define
 define|#
 directive|define
 name|INADDR_ALLHOSTS_GROUP
-value|(u_long)0xe0000001
+value|(u_int32_t)0xe0000001
 end_define
 
 begin_comment
@@ -1425,7 +1425,7 @@ begin_define
 define|#
 directive|define
 name|INADDR_ALLRTRS_GROUP
-value|(u_long)0xe0000002
+value|(u_int32_t)0xe0000002
 end_define
 
 begin_comment
@@ -1436,7 +1436,7 @@ begin_define
 define|#
 directive|define
 name|INADDR_MAX_LOCAL_GROUP
-value|(u_long)0xe00000ff
+value|(u_int32_t)0xe00000ff
 end_define
 
 begin_comment
