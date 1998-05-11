fe11871587a21215cd9fdc27f08733f07ec90d3c
John Dyson <dyson@FreeBSD.org>
1998-05-11 03:55:28 +0000

    Fix the futimes/undelete/utrace conflict with other BSD's.  Note that the only common  usage of utrace (the possible problem with this commit) is with malloc, so this should be a real problem.  Add the various NetBSD syscalls that allow full emulation of their development environment.
    Former-commit-id: 61a2a0ed18ff8f97576656d374e52e9a6d1f681d

--- sys/sys/stat.h
+++ sys/sys/stat.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)stat.h	8.12 (Berkeley) 6/16/95  * $Id: stat.h,v 1.13 1997/06/02 06:24:51 julian Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)stat.h	8.12 (Berkeley) 6/16/95  * $Id: stat.h,v 1.14 1998/02/25 02:00:44 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -242,6 +242,125 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_POSIX_SOURCE
+end_ifndef
+
+begin_struct
+struct|struct
+name|nstat
+block|{
+name|dev_t
+name|st_dev
+decl_stmt|;
+comment|/* inode's device */
+name|ino_t
+name|st_ino
+decl_stmt|;
+comment|/* inode's number */
+name|u_int32_t
+name|st_mode
+decl_stmt|;
+comment|/* inode protection mode */
+name|u_int32_t
+name|st_nlink
+decl_stmt|;
+comment|/* number of hard links */
+name|uid_t
+name|st_uid
+decl_stmt|;
+comment|/* user ID of the file's owner */
+name|gid_t
+name|st_gid
+decl_stmt|;
+comment|/* group ID of the file's group */
+name|dev_t
+name|st_rdev
+decl_stmt|;
+comment|/* device type */
+ifndef|#
+directive|ifndef
+name|_POSIX_SOURCE
+name|struct
+name|timespec
+name|st_atimespec
+decl_stmt|;
+comment|/* time of last access */
+name|struct
+name|timespec
+name|st_mtimespec
+decl_stmt|;
+comment|/* time of last data modification */
+name|struct
+name|timespec
+name|st_ctimespec
+decl_stmt|;
+comment|/* time of last file status change */
+else|#
+directive|else
+name|time_t
+name|st_atime
+decl_stmt|;
+comment|/* time of last access */
+name|long
+name|st_atimensec
+decl_stmt|;
+comment|/* nsec of last access */
+name|time_t
+name|st_mtime
+decl_stmt|;
+comment|/* time of last data modification */
+name|long
+name|st_mtimensec
+decl_stmt|;
+comment|/* nsec of last data modification */
+name|time_t
+name|st_ctime
+decl_stmt|;
+comment|/* time of last file status change */
+name|long
+name|st_ctimensec
+decl_stmt|;
+comment|/* nsec of last file status change */
+endif|#
+directive|endif
+name|off_t
+name|st_size
+decl_stmt|;
+comment|/* file size, in bytes */
+name|int64_t
+name|st_blocks
+decl_stmt|;
+comment|/* blocks allocated for file */
+name|u_int32_t
+name|st_blksize
+decl_stmt|;
+comment|/* optimal blocksize for I/O */
+name|u_int32_t
+name|st_flags
+decl_stmt|;
+comment|/* user defined flags for file */
+name|u_int32_t
+name|st_gen
+decl_stmt|;
+comment|/* file generation number */
+name|int64_t
+name|st_qspare
+index|[
+literal|2
+index|]
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
