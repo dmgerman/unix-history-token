36e9ef4c711f74b92dce971ec04dc8affaa69a41
Peter Wemm <peter@FreeBSD.org>
1998-05-14 10:38:52 +0000

    Commit an old change that has been sitting around for a long while. signanosleep() did not deal with signal masks properly.  This change was based on a discussion with bde some time ago (at least 6 months or more).
    signanosleep() should probably go away since it was never really used for
    more than a few weeks and doesn't appear in released code.  It should
    probably be killed before somebody uses it and it becomes a gratuitous
    nonstandard feature.
    
    
    Former-commit-id: 9274b631e725220a1703ab2983b739ac697b60b9

--- sys/kern/kern_time.c
+++ sys/kern/kern_time.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.51 1998/04/05 12:10:41 phk Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_time.c	8.1 (Berkeley) 6/10/93  * $Id: kern_time.c,v 1.52 1998/04/06 08:26:04 phk Exp $  */
 end_comment
 
 begin_include
@@ -1346,7 +1346,25 @@ operator|(
 name|error
 operator|)
 return|;
-comment|/* change mask for sleep */
+comment|/* 	 * Use the support for sigsuspend() to manage the masks.  Arrange to 	 * have postsig() itself restore the mask after the signal has 	 * been delivered.  This is because postsig() is called just prior 	 * to returning to usermode.  If we had restore the masks early, the 	 * signal would be blocked by the time it should have been processed. 	 */
+name|p
+operator|->
+name|p_sigacts
+operator|->
+name|ps_oldmask
+operator|=
+name|p
+operator|->
+name|p_sigmask
+expr_stmt|;
+name|p
+operator|->
+name|p_sigacts
+operator|->
+name|ps_flags
+operator||=
+name|SAS_OLDMASK
+expr_stmt|;
 name|p
 operator|->
 name|p_sigmask
@@ -1369,6 +1387,34 @@ operator|&
 name|rmt
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|error
+operator|!=
+name|EINTR
+condition|)
+block|{
+comment|/* signal not queued, restore mask ourselves */
+name|p
+operator|->
+name|p_sigmask
+operator|=
+name|p
+operator|->
+name|p_sigacts
+operator|->
+name|ps_oldmask
+expr_stmt|;
+name|p
+operator|->
+name|p_sigacts
+operator|->
+name|ps_flags
+operator|&=
+operator|~
+name|SAS_OLDMASK
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|error
