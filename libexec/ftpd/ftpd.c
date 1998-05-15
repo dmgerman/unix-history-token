adfb9fa331e1efbfcb1ec96773e62d0964481e40
Andrey Chernov <ache@FreeBSD.org>
1998-05-15 15:06:58 +0000

    Do TZ= as first thing, since FTP protocol is unable to tell zone offset in any case.
    It makes no difference for anon account (since chroot already makes it GMT),
    but if you do mirror with special non-anon login, in old variant
    your mirror will be wholy retransmitted twice in the year due to
    time zone changes (/etc/localtime plays bad role here)
    
    
    Former-commit-id: 0288007ed8be363c1db29f192aad37afcfc45af7

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -55,7 +55,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: ftpd.c,v 1.45 1998/02/24 08:45:57 eivind Exp $"
+literal|"$Id: ftpd.c,v 1.46 1998/04/28 03:37:23 dg Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -1491,10 +1491,19 @@ name|FILE
 modifier|*
 name|fd
 decl_stmt|;
+name|setenv
+argument_list|(
+literal|"TZ"
+argument_list|,
+literal|""
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+comment|/* since protocol have no way to tell offset */
 name|tzset
 argument_list|()
 expr_stmt|;
-comment|/* in case no timezone database in ~ftp */
 ifdef|#
 directive|ifdef
 name|OLD_SETPROCTITLE
