ff90d6ed6d09f461afc3b807d6aa0da4e2d76847
Poul-Henning Kamp <phk@FreeBSD.org>
1998-05-19 18:48:30 +0000

    Change a data type internal to the timecounters, and remove the "delta" function.
    Reviewed, but not entirely approved by: bde
    
    
    Former-commit-id: 96d29371b4b2581671bf05858e6d7d92e4f88577

--- sys/amd64/isa/clock.c
+++ sys/amd64/isa/clock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.118 1998/03/30 09:48:47 phk Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.119 1998/04/05 01:04:48 tegge Exp $  */
 end_comment
 
 begin_comment
@@ -672,8 +672,21 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|static
-name|u_int64_t
+name|unsigned
 name|i8254_get_timecount
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|unsigned
+name|tsc_get_timecount
 name|__P
 argument_list|(
 operator|(
@@ -699,35 +712,6 @@ end_function_decl
 
 begin_decl_stmt
 specifier|static
-name|u_int64_t
-name|tsc_get_timecount
-name|__P
-argument_list|(
-operator|(
-name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
-name|u_int32_t
-name|tsc_get_timedelta
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|timecounter
-operator|*
-name|tc
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
 name|struct
 name|timecounter
 name|tsc_timecounter
@@ -736,14 +720,11 @@ literal|3
 index|]
 init|=
 block|{
-name|tsc_get_timedelta
-block|,
-comment|/* get_timedelta */
 name|tsc_get_timecount
 block|,
 comment|/* get_timecount */
 operator|~
-literal|0
+literal|0u
 block|,
 comment|/* counter_mask */
 literal|0
@@ -790,19 +771,11 @@ literal|3
 index|]
 init|=
 block|{
-literal|0
-block|,
-comment|/* get_timedelta */
 name|i8254_get_timecount
 block|,
 comment|/* get_timecount */
-operator|(
-literal|1ULL
-operator|<<
-literal|32
-operator|)
-operator|-
-literal|1
+operator|~
+literal|0u
 block|,
 comment|/* counter_mask */
 literal|0
@@ -4216,13 +4189,13 @@ end_expr_stmt
 
 begin_function
 specifier|static
-name|u_int64_t
+name|u_int
 name|i8254_get_timecount
 parameter_list|(
 name|void
 parameter_list|)
 block|{
-name|u_int32_t
+name|u_int
 name|count
 decl_stmt|;
 name|u_long
@@ -4321,7 +4294,7 @@ end_function
 
 begin_function
 specifier|static
-name|u_int64_t
+name|u_int
 name|tsc_get_timecount
 parameter_list|(
 name|void
@@ -4329,38 +4302,8 @@ parameter_list|)
 block|{
 return|return
 operator|(
-operator|(
-name|u_int64_t
-operator|)
 name|rdtsc
 argument_list|()
-operator|)
-return|;
-block|}
-end_function
-
-begin_function
-specifier|static
-name|u_int32_t
-name|tsc_get_timedelta
-parameter_list|(
-name|struct
-name|timecounter
-modifier|*
-name|tc
-parameter_list|)
-block|{
-return|return
-operator|(
-operator|(
-name|u_int64_t
-operator|)
-name|rdtsc
-argument_list|()
-operator|-
-name|tc
-operator|->
-name|offset_count
 operator|)
 return|;
 block|}
