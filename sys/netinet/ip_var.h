53ca5f5cb9977bdee4a288363ae9dc5106920833
Pierre Beyssac <pb@FreeBSD.org>
1998-05-19 15:53:50 +0000

    Move (private) struct ipflow out of ip_var.h, to reduce dependencies (for ipfw for example) on internal implementation details. Add $Id$ where missing.
    Former-commit-id: 59b33a63b693073a011bf7d5d17e76a2bbcb95bd

--- sys/netinet/ip_var.h
+++ sys/netinet/ip_var.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ip_var.h	8.2 (Berkeley) 1/9/95  *	$Id: ip_var.h,v 1.35 1998/05/19 14:04:36 dg Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ip_var.h	8.2 (Berkeley) 1/9/95  *	$Id: ip_var.h,v 1.36 1998/05/19 14:19:03 dg Exp $  */
 end_comment
 
 begin_ifndef
@@ -375,60 +375,6 @@ name|u_long
 name|ips_notmember
 decl_stmt|;
 comment|/* multicasts for unregistered grps */
-block|}
-struct|;
-end_struct
-
-begin_struct
-struct|struct
-name|ipflow
-block|{
-name|LIST_ENTRY
-argument_list|(
-argument|ipflow
-argument_list|)
-name|ipf_next
-expr_stmt|;
-comment|/* next ipflow in bucket */
-name|struct
-name|in_addr
-name|ipf_dst
-decl_stmt|;
-comment|/* destination address */
-name|struct
-name|in_addr
-name|ipf_src
-decl_stmt|;
-comment|/* source address */
-name|u_int8_t
-name|ipf_tos
-decl_stmt|;
-comment|/* type-of-service */
-name|struct
-name|route
-name|ipf_ro
-decl_stmt|;
-comment|/* associated route entry */
-name|u_long
-name|ipf_uses
-decl_stmt|;
-comment|/* number of uses in this period */
-name|int
-name|ipf_timer
-decl_stmt|;
-comment|/* remaining lifetime of this entry */
-name|u_long
-name|ipf_dropped
-decl_stmt|;
-comment|/* ENOBUFS returned by if_output */
-name|u_long
-name|ipf_errors
-decl_stmt|;
-comment|/* other errors returned by if_output */
-name|u_long
-name|ipf_last_uses
-decl_stmt|;
-comment|/* number of uses in last period */
 block|}
 struct|;
 end_struct
