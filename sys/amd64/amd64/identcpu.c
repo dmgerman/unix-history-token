4629bc17acc5084ac06b64698f401d4178370a6c
Dag-Erling Smorgrav <des@FreeBSD.org>
1998-05-21 22:53:24 +0000

    Correctly identify the precise CPU model within the 686 family: instead of just printing "Pentium Pro", check the model (cpu_id & 0xf0) and print the appropriate information.
    Former-commit-id: 85274231efe67ae4653e4ac433bfb156e193945a

--- sys/amd64/amd64/identcpu.c
+++ sys/amd64/amd64/identcpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.45 1998/04/26 03:18:38 dyson Exp $  */
+comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.46 1998/05/19 19:40:45 peter Exp $  */
 end_comment
 
 begin_include
@@ -390,6 +390,13 @@ name|CPUCLASS_486
 block|}
 block|,
 comment|/* CPU_CY486DX */
+block|{
+literal|"Pentium II"
+block|,
+name|CPUCLASS_686
+block|}
+block|,
+comment|/* CPU_PII */
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -637,11 +644,96 @@ break|break;
 case|case
 literal|0x600
 case|:
+comment|/* Check the particular flavor of 686 */
+if|if
+condition|(
+operator|(
+name|cpu_id
+operator|&
+literal|0xf0
+operator|)
+operator|==
+literal|0x00
+condition|)
+name|strcat
+argument_list|(
+name|cpu_model
+argument_list|,
+literal|"Pentium Pro A-step"
+argument_list|)
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+operator|(
+name|cpu_id
+operator|&
+literal|0xf0
+operator|)
+operator|==
+literal|0x10
+condition|)
 name|strcat
 argument_list|(
 name|cpu_model
 argument_list|,
 literal|"Pentium Pro"
+argument_list|)
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+operator|(
+name|cpu_id
+operator|&
+literal|0xf0
+operator|)
+operator|==
+literal|0x30
+condition|)
+block|{
+name|strcat
+argument_list|(
+name|cpu_model
+argument_list|,
+literal|"Pentium II"
+argument_list|)
+expr_stmt|;
+name|cpu
+operator|=
+name|CPU_PII
+expr_stmt|;
+block|}
+elseif|else
+if|if
+condition|(
+operator|(
+name|cpu_id
+operator|&
+literal|0xf0
+operator|)
+operator|==
+literal|0x50
+condition|)
+block|{
+name|strcat
+argument_list|(
+name|cpu_model
+argument_list|,
+literal|"Pentium II (quarter-micron)"
+argument_list|)
+expr_stmt|;
+name|cpu
+operator|=
+name|CPU_PII
+expr_stmt|;
+block|}
+else|else
+name|strcat
+argument_list|(
+name|cpu_model
+argument_list|,
+literal|"Unknown 80686"
 argument_list|)
 expr_stmt|;
 break|break;
