7abf4bfcf0fdd53f75885594d00748d2fefccd41
Peter Wemm <peter@FreeBSD.org>
1998-05-30 16:33:58 +0000

    When using NFSv3, use the remote server's idea of the maximum file size rather than assuming 2^64.  It may not like files that big. :-) On the nfs server, calculate and report the max file size as the point that the block numbers in the cache would turn negative. (ie: 1099511627775 bytes (1TB)).
    One of the things I'm worried about however, is that directory offsets
    are really cookies on a NFSv3 server and can be rather large, especially
    when/if the server generates the opaque directory cookies by using a local
    filesystem offset in what comes out as the upper 32 bits of the 64 bit
    cookie.  (a server is free to do this, it could save byte swapping
    depending on the native 64 bit byte order)
    
    Obtained from:	NetBSD
    
    
    Former-commit-id: c38694d291b29b848235e3610aedcdc1109f7380

--- sys/nfsclient/nfs_vfsops.c
+++ sys/nfsclient/nfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfs_vfsops.c	8.12 (Berkeley) 5/20/95  * $Id: nfs_vfsops.c,v 1.62 1998/05/20 08:05:45 peter Exp $  */
+comment|/*  * Copyright (c) 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Rick Macklem at The University of Guelph.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)nfs_vfsops.c	8.12 (Berkeley) 5/20/95  * $Id: nfs_vfsops.c,v 1.63 1998/05/24 14:41:56 peter Exp $  */
 end_comment
 
 begin_include
@@ -2105,6 +2105,9 @@ decl_stmt|,
 modifier|*
 name|mb2
 decl_stmt|;
+name|u_int64_t
+name|maxfsize
+decl_stmt|;
 name|nfsstats
 operator|.
 name|rpccnt
@@ -2380,6 +2383,49 @@ operator|=
 name|max
 expr_stmt|;
 block|}
+comment|/* XXX */
+name|nmp
+operator|->
+name|nm_maxfilesize
+operator|=
+operator|(
+name|u_int64_t
+operator|)
+literal|0x80000000
+operator|*
+name|DEV_BSIZE
+operator|-
+literal|1
+expr_stmt|;
+name|fxdr_hyper
+argument_list|(
+operator|&
+name|fsp
+operator|->
+name|fs_maxfilesize
+argument_list|,
+operator|&
+name|maxfsize
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|maxfsize
+operator|>
+literal|0
+operator|&&
+name|maxfsize
+operator|<
+name|nmp
+operator|->
+name|nm_maxfilesize
+condition|)
+name|nmp
+operator|->
+name|nm_maxfilesize
+operator|=
+name|maxfsize
+expr_stmt|;
 name|nmp
 operator|->
 name|nm_state
@@ -4054,6 +4100,25 @@ operator|->
 name|mnt_maxsymlinklen
 operator|=
 literal|1
+expr_stmt|;
+if|if
+condition|(
+operator|(
+name|argp
+operator|->
+name|flags
+operator|&
+name|NFSMNT_NFSV3
+operator|)
+operator|==
+literal|0
+condition|)
+comment|/* 		 * V2 can only handle 32 bit filesizes. For v3, nfs_fsinfo 		 * will fill this in. 		 */
+name|nmp
+operator|->
+name|nm_maxfilesize
+operator|=
+literal|0xffffffffLL
 expr_stmt|;
 name|nmp
 operator|->
