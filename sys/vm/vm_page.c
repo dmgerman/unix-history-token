d9caa458d65a9ba439e22505b3eb0d2e880d052b
John Dyson <dyson@FreeBSD.org>
1998-06-02 05:50:08 +0000

    Cleanup and remove some dead code from the initialization.
    Former-commit-id: 291518fa854636ab4fabe4a027121ad164e28f8c

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91  *	$Id: vm_page.c,v 1.98 1998/04/15 17:47:38 bde Exp $  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91  *	$Id: vm_page.c,v 1.99 1998/05/02 03:02:13 peter Exp $  */
 end_comment
 
 begin_comment
@@ -45,6 +45,12 @@ begin_include
 include|#
 directive|include
 file|<sys/vnode.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/kernel.h>
 end_include
 
 begin_include
@@ -1072,7 +1078,10 @@ argument_list|)
 expr_stmt|;
 name|mapped
 operator|=
+name|round_page
+argument_list|(
 name|vaddr
+argument_list|)
 expr_stmt|;
 name|vaddr
 operator|=
@@ -1093,6 +1102,13 @@ name|start
 operator|=
 name|new_start
 expr_stmt|;
+name|vaddr
+operator|=
+name|round_page
+argument_list|(
+name|vaddr
+argument_list|)
+expr_stmt|;
 name|bzero
 argument_list|(
 operator|(
@@ -1104,10 +1120,6 @@ name|vaddr
 operator|-
 name|mapped
 argument_list|)
-expr_stmt|;
-name|mapped
-operator|=
-name|vaddr
 expr_stmt|;
 for|for
 control|(
@@ -1132,51 +1144,6 @@ name|bucket
 operator|++
 expr_stmt|;
 block|}
-comment|/* 	 * Validate these zone addresses. 	 */
-name|new_start
-operator|=
-name|start
-operator|+
-operator|(
-name|vaddr
-operator|-
-name|mapped
-operator|)
-expr_stmt|;
-name|pmap_map
-argument_list|(
-name|mapped
-argument_list|,
-name|start
-argument_list|,
-name|new_start
-argument_list|,
-name|VM_PROT_READ
-operator||
-name|VM_PROT_WRITE
-argument_list|)
-expr_stmt|;
-name|bzero
-argument_list|(
-operator|(
-name|caddr_t
-operator|)
-name|mapped
-argument_list|,
-operator|(
-name|vaddr
-operator|-
-name|mapped
-operator|)
-argument_list|)
-expr_stmt|;
-name|start
-operator|=
-name|round_page
-argument_list|(
-name|new_start
-argument_list|)
-expr_stmt|;
 comment|/* 	 * Compute the number of pages of memory that will be available for 	 * use (taking into account the overhead of a page structure per 	 * page). 	 */
 name|first_page
 operator|=
@@ -1435,11 +1402,11 @@ name|m
 operator|->
 name|queue
 operator|=
-name|PQ_FREE
-operator|+
 name|m
 operator|->
 name|pc
+operator|+
+name|PQ_FREE
 expr_stmt|;
 name|TAILQ_INSERT_TAIL
 argument_list|(
