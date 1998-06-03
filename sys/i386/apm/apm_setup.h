0044be79e5de55a0d37dc4d1dfa6b127cfa79ec1
Michael Smith <msmith@FreeBSD.org>
1998-06-03 01:59:42 +0000

    If vm86 services are available, use these to perform the APM BIOS probe and intialisation.  This will ultimately remove the grubby (but functional) hack that copies a real-mode function into low memory early in locore.s.
    Former-commit-id: e986917967e42f992a2727f3c64b5169ff328ce9

--- sys/i386/apm/apm_setup.h
+++ sys/i386/apm/apm_setup.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 1994 by HOSOKAWA, Tatsumi<hosokawa@jp.FreeBSD.org>  *  * This software may be used, modified, copied, distributed, and sold,  * in both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep., 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm_setup.h,v 1.7 1997/02/22 09:29:49 peter Exp $  */
+comment|/*  * Copyright (C) 1994 by HOSOKAWA, Tatsumi<hosokawa@jp.FreeBSD.org>  *  * This software may be used, modified, copied, distributed, and sold,  * in both source and binary form provided that the above copyright and  * these terms are retained. Under no circumstances is the author  * responsible for the proper functioning of this software, nor does  * the author assume any responsibility for damages incurred with its  * use.  *  * Sep., 1994	Implemented on FreeBSD 1.1.5.1R (Toshiba AVS001WD)  *  *	$Id: apm_setup.h,v 1.8 1997/03/29 11:06:38 phk Exp $  */
 end_comment
 
 begin_decl_stmt
@@ -41,7 +41,14 @@ end_decl_stmt
 begin_decl_stmt
 specifier|extern
 name|u_short
-name|apm_cs_limit
+name|apm_cs32_limit
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|u_short
+name|apm_cs16_limit
 decl_stmt|;
 end_decl_stmt
 
