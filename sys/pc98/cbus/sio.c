af0e9ddb67c0488785f89acb4b2b56a7820f5c91
Takenori KATO <kato@FreeBSD.org>
1998-06-05 08:31:01 +0000

    Sync with sys/i386/isa/sio.c revision up to 1.205.
    Former-commit-id: c484691cddf7e0ac450656249bb4b209edc7462f

--- sys/pc98/cbus/sio.c
+++ sys/pc98/cbus/sio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.57 1998/05/20 13:39:06 kato Exp $  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)com.c	7.5 (Berkeley) 5/16/91  *	$Id: sio.c,v 1.58 1998/06/01 12:40:24 kato Exp $  */
 end_comment
 
 begin_include
@@ -4271,11 +4271,16 @@ name|dev
 argument_list|)
 argument_list|)
 expr_stmt|;
-return|return
-operator|(
+name|dev
+operator|->
+name|id_irq
+operator|=
 literal|0
-operator|)
-return|;
+expr_stmt|;
+name|idev
+operator|=
+name|dev
+expr_stmt|;
 block|}
 ifndef|#
 directive|ifndef
@@ -4733,6 +4738,12 @@ argument_list|()
 expr_stmt|;
 return|return
 operator|(
+name|iobase
+operator|==
+name|siocniobase
+condition|?
+name|IO_COMSIZE
+else|:
 name|result
 operator|)
 return|;
@@ -4912,7 +4923,6 @@ expr_stmt|;
 name|enable_intr
 argument_list|()
 expr_stmt|;
-comment|/* XXX the magic mask is to discard transient (clock) irqs. */
 name|irqs
 operator|=
 name|irqmap
@@ -4925,28 +4935,32 @@ name|irqmap
 index|[
 literal|0
 index|]
-operator|&
-operator|~
-literal|0x0105
 expr_stmt|;
 if|if
 condition|(
-name|irqs
-operator|!=
 name|idev
 operator|->
 name|id_irq
+operator|!=
+literal|0
+operator|&&
+operator|(
+name|idev
+operator|->
+name|id_irq
+operator|&
+name|irqs
+operator|)
+operator|==
+literal|0
 condition|)
 name|printf
 argument_list|(
-literal|"sio%d: probed irq %d does not match configured irq %d\n"
+literal|"sio%d: configured irq %d not in bitmap of probed irqs %#x\n"
 argument_list|,
-name|ffs
-argument_list|(
-name|irqs
-argument_list|)
-operator|-
-literal|1
+name|dev
+operator|->
+name|id_unit
 argument_list|,
 name|ffs
 argument_list|(
@@ -4956,6 +4970,8 @@ name|id_irq
 argument_list|)
 operator|-
 literal|1
+argument_list|,
+name|irqs
 argument_list|)
 expr_stmt|;
 if|if
@@ -4964,7 +4980,7 @@ name|bootverbose
 condition|)
 name|printf
 argument_list|(
-literal|"sio%d: irq maps: %04x %04x %04x %04x\n"
+literal|"sio%d: irq maps: %#x %#x %#x %#x\n"
 argument_list|,
 name|dev
 operator|->
@@ -5082,6 +5098,12 @@ break|break;
 block|}
 return|return
 operator|(
+name|iobase
+operator|==
+name|siocniobase
+condition|?
+name|IO_COMSIZE
+else|:
 name|result
 operator|)
 return|;
