a06122dba163bb4ff7142cff90d0a9729097b566
Andrey Chernov <ache@FreeBSD.org>
1998-06-05 09:49:51 +0000

    Fix handling more than 1 char with non-C locale Misc. cleanup PR: 6825 Submitted by: Sergey Gershtein <sg@mplik.ru>
    Former-commit-id: dad743045a0f5c96245439b59aebf1cb7b4b779c

--- lib/libc/string/strxfrm.c
+++ lib/libc/string/strxfrm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1995 Alex Tatmanjants<alex@elvisti.kiev.ua>  *		at Electronni Visti IA, Kiev, Ukraine.  *			All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id$  */
+comment|/*-  * Copyright (c) 1995 Alex Tatmanjants<alex@elvisti.kiev.ua>  *		at Electronni Visti IA, Kiev, Ukraine.  *			All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: strxfrm.c,v 1.8 1997/02/22 15:03:31 peter Exp $  */
 end_comment
 
 begin_include
@@ -145,6 +145,12 @@ operator|-
 literal|1
 return|;
 block|}
+name|prim
+operator|=
+name|sec
+operator|=
+literal|0
+expr_stmt|;
 name|ss
 operator|=
 name|s
@@ -153,12 +159,6 @@ name|__collate_substitute
 argument_list|(
 name|src
 argument_list|)
-expr_stmt|;
-name|prim
-operator|=
-name|sec
-operator|=
-literal|0
 expr_stmt|;
 while|while
 condition|(
@@ -215,24 +215,21 @@ expr_stmt|;
 name|len
 operator|--
 expr_stmt|;
-block|}
-block|}
-if|#
-directive|if
-literal|0
-block|s = ss; 	while (*s&& len> 1) { 		while (*s&& !prim) { 			lookup(s,&l,&prim,&sec); 			s += l; 		} 		if (prim&& sec) { 			*d++ = (char)sec; 			len--; 		} 	}
-endif|#
-directive|endif
-comment|/* 0 */
-operator|*
-name|d
+name|prim
 operator|=
-literal|'\0'
+literal|0
 expr_stmt|;
+block|}
+block|}
 name|free
 argument_list|(
 name|ss
 argument_list|)
+expr_stmt|;
+operator|*
+name|d
+operator|=
+literal|'\0'
 expr_stmt|;
 return|return
 name|d
