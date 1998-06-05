b6e8033ec77fad9f441f174e5beffaaed23034a0
David Greenman <dg@FreeBSD.org>
1998-06-05 21:44:20 +0000

    Moved limit frobbing (and the resulting limcopy()) that occurs for accounting to the accounting function so that this isn't needlessly done for some process exits. Reviewed by:	bde,phk
    Former-commit-id: 823ca524b650cb6c9911b1c40b18f3eb1c1a33ce

--- sys/kern/kern_acct.c
+++ sys/kern/kern_acct.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994 Christopher G. Demetriou  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_acct.c	8.1 (Berkeley) 6/14/93  *	$Id: kern_acct.c,v 1.17 1997/09/21 22:00:04 gibbs Exp $  */
+comment|/*-  * Copyright (c) 1994 Christopher G. Demetriou  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_acct.c	8.1 (Berkeley) 6/14/93  *	$Id: kern_acct.c,v 1.18 1997/11/06 19:29:07 phk Exp $  */
 end_comment
 
 begin_include
@@ -847,7 +847,61 @@ name|p
 operator|->
 name|p_acflag
 expr_stmt|;
-comment|/* 	 * Now, just write the accounting information to the file. 	 */
+comment|/* 	 * Eliminate any file size rlimit. 	 */
+if|if
+condition|(
+name|p
+operator|->
+name|p_limit
+operator|->
+name|p_refcnt
+operator|>
+literal|1
+operator|&&
+operator|(
+name|p
+operator|->
+name|p_limit
+operator|->
+name|p_lflags
+operator|&
+name|PL_SHAREMOD
+operator|)
+operator|==
+literal|0
+condition|)
+block|{
+name|p
+operator|->
+name|p_limit
+operator|->
+name|p_refcnt
+operator|--
+expr_stmt|;
+name|p
+operator|->
+name|p_limit
+operator|=
+name|limcopy
+argument_list|(
+name|p
+operator|->
+name|p_limit
+argument_list|)
+expr_stmt|;
+block|}
+name|p
+operator|->
+name|p_rlimit
+index|[
+name|RLIMIT_FSIZE
+index|]
+operator|.
+name|rlim_cur
+operator|=
+name|RLIM_INFINITY
+expr_stmt|;
+comment|/* 	 * Write the accounting information to the file. 	 */
 name|VOP_LEASE
 argument_list|(
 name|vp
