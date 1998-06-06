9e92b6533dbcca502cd7806b2bee4bbc1bde3c56
John Birrell <jb@FreeBSD.org>
1998-06-06 07:27:06 +0000

    Add a warning message for a thread locking against itself. This is not supposed to happen, but I have seen bogus g++ code that causes it.
    Former-commit-id: 0fadb21c86f60ba1f506a071ca74a6091adfa585

--- lib/libpthread/thread/thr_spinlock.c
+++ lib/libpthread/thread/thr_spinlock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997 John Birrell<jb@cimlogic.com.au>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by John Birrell.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: uthread_spinlock.c,v 1.1 1998/04/29 09:59:28 jb Exp $  *  */
+comment|/*  * Copyright (c) 1997 John Birrell<jb@cimlogic.com.au>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by John Birrell.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: uthread_spinlock.c,v 1.2 1998/05/05 21:47:58 jb Exp $  *  */
 end_comment
 
 begin_include
@@ -30,6 +30,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"spinlock.h"
 end_include
 
@@ -50,6 +56,7 @@ parameter_list|(
 specifier|volatile
 name|long
 modifier|*
+specifier|volatile
 name|lck
 parameter_list|)
 block|{
@@ -63,9 +70,60 @@ name|lck
 operator|!=
 literal|0
 condition|)
+block|{
+comment|/* Check if already locked by the running thread: */
+if|if
+condition|(
+operator|*
+name|lck
+operator|==
+operator|(
+name|long
+operator|)
+name|_thread_run
+condition|)
+block|{
+name|char
+name|str
+index|[
+literal|40
+index|]
+decl_stmt|;
+name|snprintf
+argument_list|(
+name|str
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|str
+argument_list|)
+argument_list|,
+literal|"Warning: Thread %p attempted to lock %p which it had already locked!\n"
+argument_list|,
+name|_thread_run
+argument_list|,
+name|lck
+argument_list|)
+expr_stmt|;
+name|_thread_sys_write
+argument_list|(
+literal|2
+argument_list|,
+name|str
+argument_list|,
+name|strlen
+argument_list|(
+name|str
+argument_list|)
+argument_list|)
+expr_stmt|;
+return|return;
+block|}
+comment|/* Give up the time slice: */
 name|sched_yield
 argument_list|()
 expr_stmt|;
+block|}
 comment|/* 	 * Try to grab the lock and loop if another thread grabs 	 * it before we do. 	 */
 block|}
 do|while
