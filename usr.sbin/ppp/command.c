1e19068cd435d2f312983fbfa9e82b95ac9c1b70
Brian Somers <brian@FreeBSD.org>
1998-06-07 17:10:53 +0000

    Use a width of 0 rather than 32 when HISADDR is 0.0.0.0. Reported by:      many Narrowed down by: Lutz Albers <lutz@muc.de>
    Former-commit-id: 59e07baa833378d7b6c294eaa4cc088bbba5f998

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.49 1998/03/13 00:58:07 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.24.2.50 1998/04/25 09:17:39 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -6836,6 +6836,23 @@ name|s_addr
 expr_stmt|;
 if|if
 condition|(
+name|hisaddr
+operator|&&
+operator|!
+name|UseHisaddr
+argument_list|(
+name|hisaddr
+argument_list|,
+name|mode
+operator|&
+name|MODE_AUTO
+argument_list|)
+condition|)
+return|return
+literal|4
+return|;
+if|if
+condition|(
 name|DefHisAddress
 operator|.
 name|ipaddr
@@ -6860,23 +6877,6 @@ operator|=
 literal|0
 expr_stmt|;
 block|}
-if|if
-condition|(
-name|hisaddr
-operator|&&
-operator|!
-name|UseHisaddr
-argument_list|(
-name|hisaddr
-argument_list|,
-name|mode
-operator|&
-name|MODE_AUTO
-argument_list|)
-condition|)
-return|return
-literal|4
-return|;
 return|return
 literal|0
 return|;
