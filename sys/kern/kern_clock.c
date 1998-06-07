8379de5e3465aca3673563cbbc542a93c8fc09bd
Poul-Henning Kamp <phk@FreeBSD.org>
1998-06-07 08:40:53 +0000

    Add a "this" style argument and a "void *private" so timecounters can figure out which instance to wount with.
    Former-commit-id: 17b09e5028a4ec03f45e2aea5e3dcbfb21900644

--- sys/kern/kern_clock.c
+++ sys/kern/kern_clock.c
@@ -10,7 +10,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998 Poul-Henning Kamp<phk@FreeBSD.org>  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_clock.c	8.5 (Berkeley) 1/21/94  * $Id: kern_clock.c,v 1.69 1998/05/19 18:54:38 phk Exp $  */
+comment|/*-  * Copyright (c) 1997, 1998 Poul-Henning Kamp<phk@FreeBSD.org>  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_clock.c	8.5 (Berkeley) 1/21/94  * $Id: kern_clock.c,v 1.70 1998/05/28 09:30:16 phk Exp $  */
 end_comment
 
 begin_include
@@ -1732,7 +1732,9 @@ operator|(
 name|tc
 operator|->
 name|get_timecount
-argument_list|()
+argument_list|(
+name|tc
+argument_list|)
 operator|-
 name|tc
 operator|->
@@ -2425,7 +2427,9 @@ operator|=
 name|tc
 operator|->
 name|get_timecount
-argument_list|()
+argument_list|(
+name|tc
+argument_list|)
 expr_stmt|;
 name|tc
 index|[
@@ -2535,7 +2539,9 @@ control|)
 name|tc
 operator|->
 name|get_timecount
-argument_list|()
+argument_list|(
+name|tc
+argument_list|)
 expr_stmt|;
 name|nanotime
 argument_list|(
@@ -2613,7 +2619,9 @@ operator|=
 name|tc
 operator|->
 name|get_timecount
-argument_list|()
+argument_list|(
+name|tc
+argument_list|)
 expr_stmt|;
 name|nanotime
 argument_list|(
@@ -2746,7 +2754,7 @@ comment|/* Currently unused */
 end_comment
 
 begin_endif
-unit|void switch_timecounter(struct timecounter *newtc) { 	int s; 	struct timecounter *tc; 	struct timespec ts;  	s = splclock(); 	tc = timecounter; 	if (newtc == tc || newtc == tc->other) { 		splx(s); 		return; 	} 	nanotime(&ts); 	newtc->offset_sec = ts.tv_sec; 	newtc->offset_nano = (u_int64_t)ts.tv_nsec<< 32; 	newtc->offset_micro = ts.tv_nsec / 1000; 	newtc->offset_count = newtc->get_timecount(); 	timecounter = newtc; 	splx(s); }
+unit|void switch_timecounter(struct timecounter *newtc) { 	int s; 	struct timecounter *tc; 	struct timespec ts;  	s = splclock(); 	tc = timecounter; 	if (newtc == tc || newtc == tc->other) { 		splx(s); 		return; 	} 	nanotime(&ts); 	newtc->offset_sec = ts.tv_sec; 	newtc->offset_nano = (u_int64_t)ts.tv_nsec<< 32; 	newtc->offset_micro = ts.tv_nsec / 1000; 	newtc->offset_count = newtc->get_timecount(newtc); 	timecounter = newtc; 	splx(s); }
 endif|#
 directive|endif
 end_endif
@@ -3229,6 +3237,8 @@ name|unsigned
 name|dummy_get_timecount
 parameter_list|(
 name|void
+modifier|*
+name|tc
 parameter_list|)
 block|{
 specifier|static
