a9738d247a571abbfca50efb37b314597e72d21e
Peter Hawkins <thepish@FreeBSD.org>
1998-06-07 13:38:16 +0000

    PR:		bin/5730 Submitted by:	James Raynard james@jraynard.demon.co.uk Make config's invalid option warning messages more informative. Corrected by adding the improved error report from -current. Thanks!
    Former-commit-id: 64c92bae6e418e203ea98c71d5e41db61d4281f9

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -28,7 +28,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id$"
+literal|"$Id: mkmakefile.c,v 1.18.2.3 1997/09/16 06:57:10 charnier Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -802,6 +802,21 @@ argument_list|(
 name|ofp
 argument_list|,
 literal|" -D%s"
+argument_list|,
+name|op
+operator|->
+name|op_name
+argument_list|)
+expr_stmt|;
+name|printf
+argument_list|(
+literal|"%s:%d: unknown option \"%s\"\n"
+argument_list|,
+name|PREFIX
+argument_list|,
+name|op
+operator|->
+name|op_line
 argument_list|,
 name|op
 operator|->
