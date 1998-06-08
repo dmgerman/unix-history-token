9c7718de8233da04b36c81ac75f353cbf34033f8
Brian Somers <brian@FreeBSD.org>
1998-06-08 20:23:44 +0000

    Correct ``set server'' usage and add mention that ``set ns'' changed in README.changes. Suggested by: stuart henderson <stuart@internationalschool.co.uk>
    Former-commit-id: 6759f194a20bc2c123c46db6e3cb0367ed17fb70

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.136 1998/05/29 18:32:10 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.137 1998/06/06 23:00:33 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -630,7 +630,7 @@ name|char
 name|VersionDate
 index|[]
 init|=
-literal|"$Date: 1998/05/29 18:32:10 $"
+literal|"$Date: 1998/06/06 23:00:33 $"
 decl_stmt|;
 end_decl_stmt
 
@@ -10133,7 +10133,7 @@ name|LOCAL_AUTH
 block|,
 literal|"server port"
 block|,
-literal|"set server|socket TcpPort|LocalName|none [mask]"
+literal|"set server|socket TcpPort|LocalName|none password [mask]"
 block|}
 block|,
 block|{
