49604f87387f9c043c892f9584a39a4c9b041c84
Bruce Evans <bde@FreeBSD.org>
1998-06-08 02:43:12 +0000

    Fixed an ioctl which grew overnight.
    Former-commit-id: 5668f0b75add55b5be1ac849d941262fa81a3505

--- sys/dev/ppbus/pps.c
+++ sys/dev/ppbus/pps.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: pps.c,v 1.4 1998/02/16 23:51:00 eivind Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: pps.c,v 1.5 1998/06/07 19:44:22 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -812,7 +812,7 @@ parameter_list|(
 name|dev_t
 name|dev
 parameter_list|,
-name|int
+name|u_long
 name|cmd
 parameter_list|,
 name|caddr_t
