27a32dc37daa899822e73b8d81383b86afff955c
Brian Somers <brian@FreeBSD.org>
1998-06-09 18:49:10 +0000

    Show the time that we reached maximum throughput. Submitted by: "Stephane E. Potvin" <sepotvin@videotron.ca>
    Former-commit-id: 8042f8b33ac287ac810890157abc9d60195b8283

--- usr.sbin/ppp/throughput.c
+++ usr.sbin/ppp/throughput.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Brian Somers<brian@Awfulhak.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: throughput.c,v 1.4.4.9 1998/05/01 19:26:04 brian Exp $  */
+comment|/*-  * Copyright (c) 1997 Brian Somers<brian@Awfulhak.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: throughput.c,v 1.5 1998/05/21 21:48:41 brian Exp $  */
 end_comment
 
 begin_include
@@ -121,6 +121,15 @@ operator|->
 name|nSample
 operator|=
 literal|0
+expr_stmt|;
+name|t
+operator|->
+name|BestOctetsPerSecondTime
+operator|=
+name|time
+argument_list|(
+name|NULL
+argument_list|)
 expr_stmt|;
 name|memset
 argument_list|(
@@ -275,11 +284,19 @@ name|prompt_Printf
 argument_list|(
 name|prompt
 argument_list|,
-literal|"  peak      %5d bytes/sec\n"
+literal|"  peak      %5d bytes/sec on %s\n"
 argument_list|,
 name|t
 operator|->
 name|BestOctetsPerSecond
+argument_list|,
+name|ctime
+argument_list|(
+operator|&
+name|t
+operator|->
+name|BestOctetsPerSecondTime
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
@@ -413,7 +430,7 @@ name|log_Printf
 argument_list|(
 name|level
 argument_list|,
-literal|" total %ld bytes/sec, peak %d bytes/sec\n"
+literal|" total %ld bytes/sec, peak %d bytes/sec on %s\n"
 argument_list|,
 operator|(
 name|t
@@ -430,6 +447,14 @@ argument_list|,
 name|t
 operator|->
 name|BestOctetsPerSecond
+argument_list|,
+name|ctime
+argument_list|(
+operator|&
+name|t
+operator|->
+name|BestOctetsPerSecondTime
+argument_list|)
 argument_list|)
 expr_stmt|;
 else|else
@@ -546,6 +571,7 @@ name|t
 operator|->
 name|OctetsPerSecond
 condition|)
+block|{
 name|t
 operator|->
 name|BestOctetsPerSecond
@@ -554,6 +580,16 @@ name|t
 operator|->
 name|OctetsPerSecond
 expr_stmt|;
+name|t
+operator|->
+name|BestOctetsPerSecondTime
+operator|=
+name|time
+argument_list|(
+name|NULL
+argument_list|)
+expr_stmt|;
+block|}
 if|if
 condition|(
 operator|++
