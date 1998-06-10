cc6bbaed0512e646d11e71192b894f347bc312cd
Brian Somers <brian@FreeBSD.org>
1998-06-10 00:16:07 +0000

    Fix a couple of warnings noted with -Wall on FreeBSD-2.1.5. Pointed out by: Charlie Sorsby <crs@hgo.net>
    Former-commit-id: 010d43691546806ad64e6b6ed6fa8a3671483289

--- usr.sbin/ppp/route.c
+++ usr.sbin/ppp/route.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	      PPP Routing related Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1994, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: route.c,v 1.42.2.25 1998/05/16 21:19:00 brian Exp $  *  */
+comment|/*  *	      PPP Routing related Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1994, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: route.c,v 1.45 1998/05/21 21:48:10 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -1709,17 +1709,6 @@ literal|1
 operator|)
 return|;
 block|}
-if|if
-condition|(
-name|needed
-operator|<
-literal|0
-condition|)
-return|return
-operator|(
-literal|1
-operator|)
-return|;
 name|sp
 operator|=
 name|malloc
@@ -2147,13 +2136,6 @@ argument_list|)
 expr_stmt|;
 return|return;
 block|}
-if|if
-condition|(
-name|needed
-operator|<
-literal|0
-condition|)
-return|return;
 name|sp
 operator|=
 name|malloc
