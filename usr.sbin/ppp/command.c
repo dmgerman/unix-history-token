79a1dc4a22f0e2b81b61b19ef505a946ac30596c
Brian Somers <brian@FreeBSD.org>
1998-06-12 17:45:10 +0000

    o Maintain a link-type mask for open datalinks as well as   for all datalinks in a bundle.  Ppp now deals correctly   with link types that are changed while open o When changing the type of the last AUTO link, only clear   the interface if we're not in PHASE_NETWORK.  This allows   us to switch to -ddial mode while we have a connection   without suddenly unexpectedly throttling ourselves by   clearing the interface configuration.   Problem area noted by: Aaron Jeremias Luz <aaron@csh.rit.edu>
    Former-commit-id: fa408c69b76b0e73344fc83403f6b072b28318ad

--- usr.sbin/ppp/command.c
+++ usr.sbin/ppp/command.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.138 1998/06/08 20:23:44 brian Exp $  *  */
+comment|/*  *		PPP User command processing module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: command.c,v 1.139 1998/06/10 00:16:06 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -630,7 +630,7 @@ name|char
 name|VersionDate
 index|[]
 init|=
-literal|"$Date: 1998/06/08 20:23:44 $"
+literal|"$Date: 1998/06/10 00:16:06 $"
 decl_stmt|;
 end_decl_stmt
 
@@ -1620,6 +1620,8 @@ operator|->
 name|bundle
 operator|->
 name|phys_type
+operator|.
+name|all
 argument_list|)
 condition|)
 block|{
@@ -1803,6 +1805,8 @@ operator|->
 name|bundle
 operator|->
 name|phys_type
+operator|.
+name|all
 operator|&
 operator|~
 operator|(
@@ -7387,6 +7391,8 @@ operator|->
 name|bundle
 operator|->
 name|phys_type
+operator|.
+name|all
 operator|&
 name|PHYS_AUTO
 argument_list|)
