f6ddd0156e30cbe47c1190b87482f680bcaf6e84
Steve Price <steve@FreeBSD.org>
1998-06-13 19:36:22 +0000

    Add a macro tweak.
    PR:		6932
    Submitted by:	Nick Sayer <nsayer@quack.kfu.com>
    
    Former-commit-id: 687f93033fd95a2d790830229d42981f9fa3561f

--- sys/i386/isa/si.c
+++ sys/i386/isa/si.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Device driver for Specialix range (SI/XIO) of serial line multiplexors.  *  * Copyright (C) 1990, 1992, 1998 Specialix International,  * Copyright (C) 1993, Andy Rutter<andy@acronym.co.uk>  * Copyright (C) 1995, Peter Wemm<peter@netplex.com.au>  *  * Originally derived from:	SunOS 4.x version  * Ported from BSDI version to FreeBSD by Peter Wemm.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notices, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notices, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Andy Rutter of  *	Advanced Methods and Tools Ltd. based on original information  *	from Specialix International.  * 4. Neither the name of Advanced Methods and Tools, nor Specialix  *    International may be used to endorse or promote products derived from  *    this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN  * NO EVENT SHALL THE AUTHORS BE LIABLE.  *  *	$Id: si.c,v 1.71 1998/06/07 17:10:56 dfr Exp $  */
+comment|/*  * Device driver for Specialix range (SI/XIO) of serial line multiplexors.  *  * Copyright (C) 1990, 1992, 1998 Specialix International,  * Copyright (C) 1993, Andy Rutter<andy@acronym.co.uk>  * Copyright (C) 1995, Peter Wemm<peter@netplex.com.au>  *  * Originally derived from:	SunOS 4.x version  * Ported from BSDI version to FreeBSD by Peter Wemm.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notices, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notices, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Andy Rutter of  *	Advanced Methods and Tools Ltd. based on original information  *	from Specialix International.  * 4. Neither the name of Advanced Methods and Tools, nor Specialix  *    International may be used to endorse or promote products derived from  *    this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN  * NO EVENT SHALL THE AUTHORS BE LIABLE.  *  *	$Id: si.c,v 1.72 1998/06/10 12:49:54 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -3937,21 +3937,12 @@ block|}
 comment|/* OK, now lets download the download code */
 if|if
 condition|(
-operator|(
+name|SI_ISJET
+argument_list|(
 name|sc
 operator|->
 name|sc_type
-operator|==
-name|SIJETISA
-operator|)
-operator|||
-operator|(
-name|sc
-operator|->
-name|sc_type
-operator|==
-name|SIJETPCI
-operator|)
+argument_list|)
 condition|)
 block|{
 name|DPRINT
@@ -4442,21 +4433,12 @@ literal|1
 case|:
 if|if
 condition|(
-operator|(
+name|SI_ISJET
+argument_list|(
 name|sc
 operator|->
 name|sc_type
-operator|==
-name|SIJETISA
-operator|)
-operator|||
-operator|(
-name|sc
-operator|->
-name|sc_type
-operator|==
-name|SIJETPCI
-operator|)
+argument_list|)
 condition|)
 block|{
 comment|/* set throttle to 100 times per second */
