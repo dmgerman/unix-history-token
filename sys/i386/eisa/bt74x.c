f698deeff13ca52ebc02a89557381b0cfcff6262
Justin T. Gibbs <gibbs@FreeBSD.org>
1998-06-14 16:41:57 +0000

    Add support for the Storage Dimensions SDC3222F. PR: 6261 Submitted by: "Matthew N. Dodd" <winter@jurai.net>
    Former-commit-id: e8a8e88e5972481e60e23f285ccd7a21399c652e

--- sys/i386/eisa/bt74x.c
+++ sys/i386/eisa/bt74x.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Product specific probe and attach routines for:  * 	Buslogic BT74x SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: bt74x.c,v 1.11 1997/08/21 19:56:40 bde Exp $  */
+comment|/*  * Product specific probe and attach routines for:  * 	Buslogic BT74x SCSI controllers  *  * Copyright (c) 1995 Justin T. Gibbs  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Justin T. Gibbs.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  *	$Id: bt74x.c,v 1.12 1997/09/21 21:35:22 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -72,6 +72,13 @@ define|#
 directive|define
 name|EISA_DEVICE_ID_AMI_4801
 value|0x05a94801
+end_define
+
+begin_define
+define|#
+directive|define
+name|EISA_DEVICE_ID_BUS4781
+value|0x0ab34781
 end_define
 
 begin_define
@@ -506,6 +513,15 @@ case|:
 return|return
 operator|(
 literal|"AMI Series 48 SCSI host adapter"
+operator|)
+return|;
+break|break;
+case|case
+name|EISA_DEVICE_ID_BUS4781
+case|:
+return|return
+operator|(
+literal|"Storage Dimensions SDC3222F SCSI host adapter"
 operator|)
 return|;
 break|break;
