e9f3119dab5ca685aef27bf42fb938c56102ee0d
Bruce Evans <bde@FreeBSD.org>
1998-06-14 14:00:47 +0000

    Added typedefs for __int64_t and __uint64_t.  These will be used to avoid explicit use of `[unsigned] long long' and `[u]quad_t'.
    Fixed disorder of and lack of comments for the types added in the previous
    two commits.  Improved some comments.  The comments should be elswhere
    (not duplicated in every MD file).
    
    
    Former-commit-id: 8df944320513914c732af09d32b40a8ee62f11b5

--- sys/i386/include/ansi.h
+++ sys/i386/include/ansi.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ansi.h	8.2 (Berkeley) 1/4/94  * $Id: ansi.h,v 1.13 1997/05/08 13:45:43 peter Exp $  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ansi.h	8.2 (Berkeley) 1/4/94  * $Id: ansi.h,v 1.14 1998/01/08 00:27:30 alex Exp $  */
 end_comment
 
 begin_ifndef
@@ -27,7 +27,18 @@ value|unsigned long
 end_define
 
 begin_comment
-comment|/* clock() */
+comment|/* clock()... */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_CLOCKID_T_
+value|int
+end_define
+
+begin_comment
+comment|/* clock_gettime()... */
 end_comment
 
 begin_define
@@ -82,7 +93,51 @@ value|long
 end_define
 
 begin_comment
-comment|/* time() */
+comment|/* time()... */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_TIMER_T_
+value|int
+end_define
+
+begin_comment
+comment|/* timer_gettime()... */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_UINT8_T_
+value|unsigned char
+end_define
+
+begin_comment
+comment|/* unsigned exactly 8 bits */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_UINT16_T_
+value|unsigned short
+end_define
+
+begin_comment
+comment|/* unsigned exactly 16 bits */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_UINT32_T_
+value|unsigned int
+end_define
+
+begin_comment
+comment|/* unsigned exactly 32 bits */
 end_comment
 
 begin_define
@@ -95,41 +150,6 @@ end_define
 begin_comment
 comment|/* wchar_t (see below) */
 end_comment
-
-begin_define
-define|#
-directive|define
-name|_BSD_CLOCKID_T_
-value|int
-end_define
-
-begin_define
-define|#
-directive|define
-name|_BSD_TIMER_T_
-value|int
-end_define
-
-begin_define
-define|#
-directive|define
-name|_BSD_UINT8_T_
-value|unsigned char
-end_define
-
-begin_define
-define|#
-directive|define
-name|_BSD_UINT16_T_
-value|unsigned short
-end_define
-
-begin_define
-define|#
-directive|define
-name|_BSD_UINT32_T_
-value|unsigned int
-end_define
 
 begin_comment
 comment|/*  * Types which are fundamental to the implementation and must be used  * in more than one standard header although they are only declared in  * one (perhaps nonstandard) header are defined here.  Standard headers  * use _BSD_XXX_T_ without undef'ing it.  */
@@ -150,7 +170,7 @@ begin_define
 define|#
 directive|define
 name|_BSD_OFF_T_
-value|long long
+value|__int64_t
 end_define
 
 begin_comment
@@ -200,6 +220,73 @@ directive|define
 name|_BSD_CLOCKS_PER_SEC_
 value|128
 end_define
+
+begin_comment
+comment|/*  * Typedefs for especially magic types.  #define's wouldn't work in the  * __GNUC__ case, since __attribute__(()) only works in certain contexts.  * This is not in<machine/types.h>, since that has too much namespace  * pollution for inclusion in ANSI headers, yet we need __int64_t in at  * least<stdio.h>.  */
+end_comment
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__GNUC__
+end_ifdef
+
+begin_typedef
+typedef|typedef
+name|int
+name|__attribute__
+argument_list|(
+operator|(
+name|__mode__
+argument_list|(
+name|__DI__
+argument_list|)
+operator|)
+argument_list|)
+name|__int64_t
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+name|unsigned
+name|int
+name|__attribute__
+typedef|((
+name|__mode__
+typedef|(
+name|__DI__
+typedef|)))
+name|__uint64_t
+typedef|;
+end_typedef
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_typedef
+typedef|typedef
+name|long
+name|long
+name|__int64_t
+typedef|;
+end_typedef
+
+begin_typedef
+typedef|typedef
+name|unsigned
+name|long
+name|long
+name|__uint64_t
+typedef|;
+end_typedef
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_endif
 endif|#
