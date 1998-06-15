69519a7c08ff2d8834c6edaa4bf15a2e7c010448
Philippe Charnier <charnier@FreeBSD.org>
1998-06-15 07:00:01 +0000

    Add rcsid. Remove unused #includes.
    Former-commit-id: 3af153ec536be1124cc2521f8b3db5d59b54bcba

--- sbin/dumpfs/dumpfs.c
+++ sbin/dumpfs/dumpfs.c
@@ -11,6 +11,7 @@ end_ifndef
 
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
 name|copyright
 index|[]
@@ -34,13 +35,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)dumpfs.c	8.5 (Berkeley) 4/29/95";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)dumpfs.c	8.5 (Berkeley) 4/29/95"
+literal|"$Id$"
 decl_stmt|;
 end_decl_stmt
 
@@ -68,12 +82,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<ufs/ufs/dinode.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<ufs/ffs/fs.h>
 end_include
 
@@ -81,12 +89,6 @@ begin_include
 include|#
 directive|include
 file|<err.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<errno.h>
 end_include
 
 begin_include
@@ -111,12 +113,6 @@ begin_include
 include|#
 directive|include
 file|<stdlib.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<string.h>
 end_include
 
 begin_include
