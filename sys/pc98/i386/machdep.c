7adc990356ae58dfa190aca21429e28c8e1aa34f
Takenori KATO <kato@FreeBSD.org>
1998-06-26 02:49:25 +0000

    Sync with sys/i386/i386/machdep.c revision 1.209.2.24.
    Submitted by:	Takahashi Yoshihiro <nyan@wyvern.cc.kogakuin.ac.jp>
    
    
    Former-commit-id: 13aa5adefb4173160fea1fc699019219eedd43a3

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.11.2.24 1998/04/12 15:04:45 kato Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.11.2.25 1998/05/06 19:11:44 gibbs Exp $  */
 end_comment
 
 begin_include
@@ -31,6 +31,12 @@ begin_include
 include|#
 directive|include
 file|"opt_machdep.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"opt_msgbuf.h"
 end_include
 
 begin_include
@@ -706,18 +712,6 @@ name|int
 name|freebufspace
 decl_stmt|;
 end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|msgbufmapped
-init|=
-literal|0
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* set when safe to use msgbuf */
-end_comment
 
 begin_decl_stmt
 name|int
@@ -5989,11 +5983,7 @@ name|PAGE_SIZE
 operator|+
 name|round_page
 argument_list|(
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|msgbuf
-argument_list|)
+name|MSGBUF_SIZE
 argument_list|)
 operator|>=
 name|phys_avail
@@ -6054,11 +6044,7 @@ index|]
 operator|-=
 name|round_page
 argument_list|(
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|msgbuf
-argument_list|)
+name|MSGBUF_SIZE
 argument_list|)
 expr_stmt|;
 name|avail_end
@@ -6080,11 +6066,7 @@ name|off
 operator|<
 name|round_page
 argument_list|(
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|msgbuf
-argument_list|)
+name|MSGBUF_SIZE
 argument_list|)
 condition|;
 name|off
@@ -6111,9 +6093,12 @@ argument_list|,
 name|TRUE
 argument_list|)
 expr_stmt|;
-name|msgbufmapped
-operator|=
-literal|1
+name|msgbufinit
+argument_list|(
+name|msgbufp
+argument_list|,
+name|MSGBUF_SIZE
+argument_list|)
 expr_stmt|;
 comment|/* make a initial tss so microp can get interrupt stack on syscall! */
 name|proc0
