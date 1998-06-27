6159ff807fbf52aa7e50d59cc17f01400aaa9457
John Polstra <jdp@FreeBSD.org>
1998-06-27 02:01:25 +0000

    Extend the existing hackery to set the disk type to SCSI for the "da" and "od" devices.  Formerly ESDI was used for them, causing problems for fresh installations of CAM systems.
    Former-commit-id: 96bd48f91b57153bbe9b37f08f0e81bb298558ef

--- lib/libdisk/write_disk.c
+++ lib/libdisk/write_disk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id$  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: write_disk.c,v 1.20 1997/02/22 15:06:40 peter Exp $  *  */
 end_comment
 
 begin_include
@@ -519,6 +519,24 @@ literal|0
 index|]
 operator|==
 literal|'s'
+operator|||
+name|new
+operator|->
+name|name
+index|[
+literal|0
+index|]
+operator|==
+literal|'d'
+operator|||
+name|new
+operator|->
+name|name
+index|[
+literal|0
+index|]
+operator|==
+literal|'o'
 condition|?
 name|DTYPE_SCSI
 else|:
