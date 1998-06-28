e7b22d574412d4126c288c74da21269c0bb1e3e2
Doug Rabson <dfr@FreeBSD.org>
1998-06-28 00:52:50 +0000

    Use db_addr_t for the breakpoint address in db_set_single_step.
    Former-commit-id: 33fc31bcdf92340dc7b511604a60f49bfc03c811

--- sys/ddb/db_run.c
+++ sys/ddb/db_run.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_run.c,v 1.13 1997/06/14 11:52:37 bde Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_run.c,v 1.14 1998/06/07 17:09:37 dfr Exp $  */
 end_comment
 
 begin_comment
@@ -872,12 +872,11 @@ name|PC_REGS
 argument_list|(
 name|regs
 argument_list|)
-decl_stmt|;
-specifier|register
-name|unsigned
-name|inst
 decl_stmt|,
 name|brpc
+decl_stmt|;
+name|unsigned
+name|inst
 decl_stmt|;
 comment|/* 	 *	User was stopped at pc, e.g. the instruction 	 *	at pc was not executed. 	 */
 name|inst
