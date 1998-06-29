84b2b28fa84bc752e1835c26a795046813d166b6
Bruce Evans <bde@FreeBSD.org>
1998-06-29 17:25:46 +0000

    Don't assume that time_t is long.
    Former-commit-id: 01df42fc96b50410c8d422adee65b4f6e11af829

--- usr.bin/doscmd/bios.c
+++ usr.bin/doscmd/bios.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993, 1996  *	Berkeley Software Design, Inc.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Berkeley Software  *	Design, Inc.  *  * THIS SOFTWARE IS PROVIDED BY Berkeley Software Design, Inc. ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Berkeley Software Design, Inc. BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	BSDI bios.c,v 2.3 1996/04/08 19:32:19 bostic Exp  *  * $Id: bios.c,v 1.1 1997/08/09 01:42:33 dyson Exp $  */
+comment|/*  * Copyright (c) 1992, 1993, 1996  *	Berkeley Software Design, Inc.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Berkeley Software  *	Design, Inc.  *  * THIS SOFTWARE IS PROVIDED BY Berkeley Software Design, Inc. ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Berkeley Software Design, Inc. BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	BSDI bios.c,v 2.3 1996/04/08 19:32:19 bostic Exp  *  * $Id: bios.c,v 1.2 1997/08/15 23:41:23 jlemon Exp $  */
 end_comment
 
 begin_include
@@ -722,6 +722,9 @@ decl_stmt|;
 name|struct
 name|timeval
 name|tv
+decl_stmt|;
+name|time_t
+name|tv_sec
 decl_stmt|;
 name|struct
 name|timezone
@@ -1489,14 +1492,18 @@ operator|&
 name|tz
 argument_list|)
 expr_stmt|;
+name|tv_sec
+operator|=
+name|tv
+operator|.
+name|tv_sec
+expr_stmt|;
 name|tm
 operator|=
 operator|*
 name|localtime
 argument_list|(
 operator|&
-name|tv
-operator|.
 name|tv_sec
 argument_list|)
 expr_stmt|;
--- usr.bin/doscmd/int1a.c
+++ usr.bin/doscmd/int1a.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993, 1996  *	Berkeley Software Design, Inc.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Berkeley Software  *	Design, Inc.  *  * THIS SOFTWARE IS PROVIDED BY Berkeley Software Design, Inc. ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Berkeley Software Design, Inc. BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	BSDI int1a.c,v 2.2 1996/04/08 19:32:49 bostic Exp  *  * $Id: int1a.c,v 1.3 1996/09/22 15:42:56 miff Exp $  */
+comment|/*  * Copyright (c) 1992, 1993, 1996  *	Berkeley Software Design, Inc.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Berkeley Software  *	Design, Inc.  *  * THIS SOFTWARE IS PROVIDED BY Berkeley Software Design, Inc. ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Berkeley Software Design, Inc. BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	BSDI int1a.c,v 2.2 1996/04/08 19:32:49 bostic Exp  *  * $Id: int1a.c,v 1.1 1997/08/09 01:42:49 dyson Exp $  */
 end_comment
 
 begin_include
@@ -63,6 +63,9 @@ name|tm
 modifier|*
 name|tm
 decl_stmt|;
+name|time_t
+name|tv_sec
+decl_stmt|;
 name|long
 name|value
 decl_stmt|;
@@ -101,13 +104,17 @@ operator|==
 literal|0
 condition|)
 block|{
+name|tv_sec
+operator|=
+name|boot_time
+operator|.
+name|tv_sec
+expr_stmt|;
 name|tm
 operator|=
 name|localtime
 argument_list|(
 operator|&
-name|boot_time
-operator|.
 name|tv_sec
 argument_list|)
 expr_stmt|;
@@ -163,13 +170,17 @@ condition|(
 name|R_AL
 condition|)
 block|{
+name|tv_sec
+operator|=
+name|boot_time
+operator|.
+name|tv_sec
+expr_stmt|;
 name|tm
 operator|=
 name|localtime
 argument_list|(
 operator|&
-name|boot_time
-operator|.
 name|tv_sec
 argument_list|)
 expr_stmt|;
@@ -255,13 +266,17 @@ case|case
 literal|0x01
 case|:
 comment|/* set current clock count */
+name|tv_sec
+operator|=
+name|boot_time
+operator|.
+name|tv_sec
+expr_stmt|;
 name|tm
 operator|=
 name|localtime
 argument_list|(
 operator|&
-name|boot_time
-operator|.
 name|tv_sec
 argument_list|)
 expr_stmt|;
@@ -306,13 +321,17 @@ operator|&
 name|zone
 argument_list|)
 expr_stmt|;
+name|tv_sec
+operator|=
+name|tod
+operator|.
+name|tv_sec
+expr_stmt|;
 name|tm
 operator|=
 name|localtime
 argument_list|(
 operator|&
-name|tod
-operator|.
 name|tv_sec
 argument_list|)
 expr_stmt|;
@@ -356,13 +375,17 @@ operator|&
 name|zone
 argument_list|)
 expr_stmt|;
+name|tv_sec
+operator|=
+name|tod
+operator|.
+name|tv_sec
+expr_stmt|;
 name|tm
 operator|=
 name|localtime
 argument_list|(
 operator|&
-name|tod
-operator|.
 name|tv_sec
 argument_list|)
 expr_stmt|;
