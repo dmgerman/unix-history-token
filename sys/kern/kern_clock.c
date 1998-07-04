c83a5dab666f16dd03633c0df0b3a451dacaea7b
Poul-Henning Kamp <phk@FreeBSD.org>
1998-07-04 19:12:21 +0000

    Change the sign on a race-condition, so that instead of ending up several tens of milliseconds out in the future we end up the right place with a subweeniesecond error.
    Former-commit-id: f0c7eca3f5c0f18665ad734464b9b4cda66c21e8

--- sys/kern/kern_clock.c
+++ sys/kern/kern_clock.c
@@ -10,7 +10,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998 Poul-Henning Kamp<phk@FreeBSD.org>  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_clock.c	8.5 (Berkeley) 1/21/94  * $Id: kern_clock.c,v 1.73 1998/06/09 13:10:52 phk Exp $  */
+comment|/*-  * Copyright (c) 1997, 1998 Poul-Henning Kamp<phk@FreeBSD.org>  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_clock.c	8.5 (Berkeley) 1/21/94  * $Id: kern_clock.c,v 1.74 1998/07/02 21:35:02 phk Exp $  */
 end_comment
 
 begin_include
@@ -2903,31 +2903,25 @@ modifier|*
 name|tc
 decl_stmt|,
 modifier|*
+name|tcn
+decl_stmt|,
+modifier|*
 name|tco
 decl_stmt|;
 name|unsigned
 name|delta
 decl_stmt|;
-if|if
-condition|(
+name|tco
+operator|=
 name|timecounter
-operator|->
-name|tc_poll_pps
-condition|)
-name|timecounter
-operator|->
-name|tc_poll_pps
-argument_list|(
-name|timecounter
-argument_list|)
 expr_stmt|;
 name|tc
 operator|=
-name|timecounter
+name|tco
 operator|->
 name|tc_other
 expr_stmt|;
-name|tco
+name|tcn
 operator|=
 name|tc
 operator|->
@@ -2937,13 +2931,13 @@ operator|*
 name|tc
 operator|=
 operator|*
-name|timecounter
+name|tco
 expr_stmt|;
 name|tc
 operator|->
 name|tc_other
 operator|=
-name|tco
+name|tcn
 expr_stmt|;
 name|delta
 operator|=
@@ -3019,6 +3013,20 @@ name|tc
 operator|=
 name|sync_other_counter
 argument_list|()
+expr_stmt|;
+comment|/* 	 * We may be inducing a tiny error here, the tc_poll_pps() may 	 * process a latched count which happens after the tco_delta() 	 * in sync_other_counter(), which would extend the previous 	 * counters parameters into the domain of this new one. 	 * Since the timewindow is very small for this, the error is 	 * going to be only a few weenieseconds (as Dave Mills would 	 * say), so lets just not talk more about it, OK ? 	 */
+if|if
+condition|(
+name|tc
+operator|->
+name|tc_poll_pps
+condition|)
+name|tc
+operator|->
+name|tc_poll_pps
+argument_list|(
+name|tc
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
