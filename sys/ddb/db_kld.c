4aa6e4006a7e5cecd10cd907209cbd32fb0ee7d4
Doug Rabson <dfr@FreeBSD.org>
1998-07-05 10:11:20 +0000

    Add symtab parameter to X_db_symbol_values.
    Former-commit-id: 405b6d64ea549d67cb4217d3fb09a49d1a0e2fa5

--- sys/ddb/db_kld.c
+++ sys/ddb/db_kld.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id$  *	from db_aout.c,v 1.20 1998/06/07 17:09:36 dfr Exp  */
+comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_kld.c,v 1.1 1998/06/28 00:57:28 dfr Exp $  *	from db_aout.c,v 1.20 1998/06/07 17:09:36 dfr Exp  */
 end_comment
 
 begin_comment
@@ -220,12 +220,18 @@ begin_function
 name|void
 name|X_db_symbol_values
 parameter_list|(
+name|symtab
+parameter_list|,
 name|dbsym
 parameter_list|,
 name|namep
 parameter_list|,
 name|valuep
 parameter_list|)
+name|db_symtab_t
+modifier|*
+name|symtab
+decl_stmt|;
 name|db_sym_t
 name|dbsym
 decl_stmt|;
