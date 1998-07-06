ee6e4123ea58e3d0f00215e7911b8eee353dca55
Brian Somers <brian@FreeBSD.org>
1998-07-06 02:07:29 +0000

    Version 1.65 -> 1.7
    Former-commit-id: 7ec259f3a82a76b5193f182c082f0b4c6ce8181a

--- usr.sbin/ppp/vars.c
+++ usr.sbin/ppp/vars.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP configuration variables  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: vars.c,v 1.9.2.15 1997/09/23 00:01:27 brian Exp $  *  */
+comment|/*  *		PPP configuration variables  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: vars.c,v 1.9.2.16 1998/01/26 20:05:32 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -98,7 +98,7 @@ name|char
 name|VarVersion
 index|[]
 init|=
-literal|"PPP Version 1.65"
+literal|"PPP Version 1.7"
 decl_stmt|;
 end_decl_stmt
 
@@ -107,7 +107,7 @@ name|char
 name|VarLocalVersion
 index|[]
 init|=
-literal|"$Date: 1998/01/21 02:15:31 $"
+literal|"$Date: 1998/01/26 20:05:32 $"
 decl_stmt|;
 end_decl_stmt
 
