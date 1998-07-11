a5045a56c9e60f1c012d4047f357d9c087afa8f7
Bruce Evans <bde@FreeBSD.org>
1998-07-11 04:02:49 +0000

    Don't pretend to support ix86's with 16-bit ints by using longs just to ensure 32-bit variables.  Doing so broke i386's with 64-bit longs.  Use fixed-size integral types instead of plain ints, shorts, chars and pointers since the bootinfo struct layout is a binary interface.  The boot blocks could reasonably be implemented using 16-bit code.
    Former-commit-id: 88896dfbc2a1794c5262fe64adc6a5db334b5337

--- sys/i386/include/bootinfo.h
+++ sys/i386/include/bootinfo.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 1994 by Rodney W. Grimes, Milwaukie, Oregon  97222  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Rodney W. Grimes.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY RODNEY W. GRIMES ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL RODNEY W. GRIMES BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: bootinfo.h,v 1.7 1997/02/22 09:33:57 peter Exp $  */
+comment|/*-  * Copyright (C) 1994 by Rodney W. Grimes, Milwaukie, Oregon  97222  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Rodney W. Grimes.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY RODNEY W. GRIMES ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL RODNEY W. GRIMES BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: bootinfo.h,v 1.8 1997/07/31 08:07:36 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -41,71 +41,60 @@ begin_struct
 struct|struct
 name|bootinfo
 block|{
-name|unsigned
-name|int
+name|u_int32_t
 name|bi_version
 decl_stmt|;
-name|unsigned
-name|char
-modifier|*
+name|u_int32_t
 name|bi_kernelname
 decl_stmt|;
-name|struct
-name|nfs_diskless
-modifier|*
+comment|/* represents a char * */
+name|u_int32_t
 name|bi_nfs_diskless
 decl_stmt|;
+comment|/* struct nfs_diskless * */
 comment|/* End of fields that are always present. */
 define|#
 directive|define
 name|bi_endcommon
 value|bi_n_bios_used
-name|unsigned
-name|int
+name|u_int32_t
 name|bi_n_bios_used
 decl_stmt|;
-name|unsigned
-name|long
+name|u_int32_t
 name|bi_bios_geom
 index|[
 name|N_BIOS_GEOM
 index|]
 decl_stmt|;
-name|unsigned
-name|int
+name|u_int32_t
 name|bi_size
 decl_stmt|;
-name|unsigned
-name|char
+name|u_int8_t
 name|bi_memsizes_valid
 decl_stmt|;
-name|unsigned
-name|char
+name|u_int8_t
 name|bi_pad
 index|[
 literal|1
 index|]
 decl_stmt|;
-name|unsigned
-name|short
+name|u_int16_t
 name|bi_vesa
 decl_stmt|;
-name|unsigned
-name|long
+name|u_int32_t
 name|bi_basemem
 decl_stmt|;
-name|unsigned
-name|long
+name|u_int32_t
 name|bi_extmem
 decl_stmt|;
-name|unsigned
-name|long
+name|u_int32_t
 name|bi_symtab
 decl_stmt|;
-name|unsigned
-name|long
+comment|/* struct symtab * */
+name|u_int32_t
 name|bi_esymtab
 decl_stmt|;
+comment|/* struct symtab * */
 block|}
 struct|;
 end_struct
