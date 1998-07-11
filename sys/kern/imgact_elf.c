a82c82503f7b103c4973d5aa74d0b376994692a3
Bruce Evans <bde@FreeBSD.org>
1998-07-11 10:28:47 +0000

    Fixed printf format errors.
    Former-commit-id: e9bffe5950d797c063b175be7cb1843f890590c5

--- sys/kern/imgact_elf.c
+++ sys/kern/imgact_elf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1995-1996 Søren Schmidt  * Copyright (c) 1996 Peter Wemm  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: imgact_elf.c,v 1.25 1998/06/07 17:11:31 dfr Exp $  */
+comment|/*-  * Copyright (c) 1995-1996 Søren Schmidt  * Copyright (c) 1996 Peter Wemm  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software withough specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: imgact_elf.c,v 1.26 1998/06/08 09:19:35 dfr Exp $  */
 end_comment
 
 begin_include
@@ -1635,7 +1635,7 @@ operator|)
 expr_stmt|;
 name|UPRINTF
 argument_list|(
-literal|".text<%08x,%08x> entry=%08x\n"
+literal|".text<%08lx,%08lx> entry=%08lx\n"
 argument_list|,
 name|text_addr
 argument_list|,
@@ -1696,7 +1696,7 @@ argument_list|)
 expr_stmt|;
 name|UPRINTF
 argument_list|(
-literal|".data<%08x,%08x>\n"
+literal|".data<%08lx,%08lx>\n"
 argument_list|,
 name|data_addr
 argument_list|,
@@ -2267,7 +2267,7 @@ name|e_entry
 expr_stmt|;
 name|UPRINTF
 argument_list|(
-literal|".text<%08x,%08x> entry=%08x\n"
+literal|".text<%08lx,%08lx> entry=%08lx\n"
 argument_list|,
 name|text_addr
 argument_list|,
@@ -2314,7 +2314,7 @@ argument_list|)
 expr_stmt|;
 name|UPRINTF
 argument_list|(
-literal|".data<%08x,%08x>\n"
+literal|".data<%08lx,%08lx>\n"
 argument_list|,
 name|data_addr
 argument_list|,
