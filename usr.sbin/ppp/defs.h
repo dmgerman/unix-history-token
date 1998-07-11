8200d86c52b23f95df83cb1f9eb83e1f7d868936
Brian Somers <brian@FreeBSD.org>
1998-07-11 02:48:36 +0000

    Change MIN_LQRPERIOD from 5 to 2.
    Former-commit-id: dbe46ef66eaed92c39f7b1d0a897586634f8cdbc

--- usr.sbin/ppp/defs.h
+++ usr.sbin/ppp/defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.32 1998/05/29 18:32:11 brian Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.33 1998/06/27 23:48:43 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_comment
@@ -218,7 +218,7 @@ begin_define
 define|#
 directive|define
 name|MIN_LQRPERIOD
-value|5
+value|2
 end_define
 
 begin_comment
@@ -233,7 +233,7 @@ value|30
 end_define
 
 begin_comment
-comment|/* LQR frequency */
+comment|/* Default LQR frequency */
 end_comment
 
 begin_define
