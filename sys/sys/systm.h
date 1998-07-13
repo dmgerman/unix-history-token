029c7fbd9d681626756d5367369ea2622c6dae24
Bruce Evans <bde@FreeBSD.org>
1998-07-13 06:45:23 +0000

    Added macros __printflike() and __scanflike() to <sys/cdefs.h>. Use them to `make gcc -Wformat' check formats for all printf-like and scanf-like functions in /usr/src except for the err()/warn() family.  err() isn't quite printf-like since its format arg can legitimately be NULL.  syslog() isn't quite printf-like, but gcc already accepts %m, even for plain printf() when it shouldn't.
    Former-commit-id: 6c0fbc554c2213a27b5e52165f4de0cc56946abb

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)systm.h	8.7 (Berkeley) 3/29/95  * $Id: systm.h,v 1.71 1998/06/07 17:13:04 dfr Exp $  */
+comment|/*-  * Copyright (c) 1982, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)systm.h	8.7 (Berkeley) 3/29/95  * $Id: systm.h,v 1.72 1998/06/09 12:52:35 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -461,6 +461,12 @@ operator|...
 operator|)
 argument_list|)
 name|__dead2
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|2
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -527,6 +533,12 @@ operator|,
 operator|...
 operator|)
 argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|2
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -559,6 +571,12 @@ operator|,
 name|_BSD_VA_LIST_
 operator|)
 argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|0
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -576,6 +594,12 @@ operator|*
 operator|,
 operator|...
 operator|)
+argument_list|)
+name|__printflike
+argument_list|(
+literal|2
+argument_list|,
+literal|3
 argument_list|)
 decl_stmt|;
 end_decl_stmt
@@ -605,6 +629,12 @@ operator|,
 operator|...
 operator|)
 argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|2
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -625,6 +655,12 @@ operator|,
 operator|...
 operator|)
 argument_list|)
+name|__printflike
+argument_list|(
+literal|2
+argument_list|,
+literal|3
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -641,6 +677,12 @@ operator|,
 operator|...
 operator|)
 argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|2
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -656,6 +698,12 @@ operator|*
 operator|,
 name|_BSD_VA_LIST_
 operator|)
+argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|0
 argument_list|)
 decl_stmt|;
 end_decl_stmt
@@ -676,6 +724,12 @@ operator|*
 operator|,
 operator|...
 operator|)
+argument_list|)
+name|__printflike
+argument_list|(
+literal|2
+argument_list|,
+literal|3
 argument_list|)
 decl_stmt|;
 end_decl_stmt
