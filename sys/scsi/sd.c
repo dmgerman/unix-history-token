8b6b50d43e825f56daecfbc845f025cae68ccadc
Bruce Evans <bde@FreeBSD.org>
1998-07-14 11:34:22 +0000

    If object pointers need to be cast to [unsigned] integers, then cast to [u]intptr_t instead of to [u_]long.
    Don't cast pointers to integers just to do ordinary pointer arithmetic
    on them, especially when the casts use gcc's feature of casting lvalues.
    
    
    Former-commit-id: 1d004741dca6e65858ca9850d349c49f9ce8fd66

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.133 1998/07/11 07:45:59 bde Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.134 1998/07/13 08:23:05 julian Exp $  */
 end_comment
 
 begin_include
@@ -5330,7 +5330,7 @@ comment|/* 		 * If we are dumping core, it may take a while. 		 * So reassure th
 if|if
 condition|(
 operator|(
-name|u_long
+name|uintptr_t
 operator|)
 name|addr
 operator|%
@@ -5383,9 +5383,6 @@ name|blknum
 operator|+=
 name|blkcnt
 expr_stmt|;
-operator|(
-name|long
-operator|)
 name|addr
 operator|+=
 name|blkcnt
