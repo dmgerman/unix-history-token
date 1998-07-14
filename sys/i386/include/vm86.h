8e4de4b8c0a4d16b5ff1893f971c6a8ba43f235d
Michael Smith <msmith@FreeBSD.org>
1998-07-14 17:13:23 +0000

    Add missing register name defines.
    Former-commit-id: 0a05d9dded21fc6f7473ce2422a259b177042d0a

--- sys/i386/include/vm86.h
+++ sys/i386/include/vm86.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Jonathan Lemon  * All rights reserved.  *  * Derived from register.h, which is  *     Copyright (c) 1996 Michael Smith.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: vm86.h,v 1.5 1998/03/23 19:52:57 jlemon Exp $  */
+comment|/*-  * Copyright (c) 1997 Jonathan Lemon  * All rights reserved.  *  * Derived from register.h, which is  *     Copyright (c) 1996 Michael Smith.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: vm86.h,v 1.6 1998/06/08 09:47:35 bde Exp $  */
 end_comment
 
 begin_ifndef
@@ -148,6 +148,14 @@ name|vmf_eax
 value|eax.r_ex
 define|#
 directive|define
+name|vmf_bh
+value|ebx.r_b.r_h
+define|#
+directive|define
+name|vmf_bl
+value|ebx.r_b.r_l
+define|#
+directive|define
 name|vmf_bx
 value|ebx.r_w.r_x
 define|#
@@ -156,12 +164,28 @@ name|vmf_ebx
 value|ebx.r_ex
 define|#
 directive|define
+name|vmf_ch
+value|ecx.r_b.r_h
+define|#
+directive|define
+name|vmf_cl
+value|ecx.r_b.r_l
+define|#
+directive|define
 name|vmf_cx
 value|ecx.r_w.r_x
 define|#
 directive|define
 name|vmf_ecx
 value|ecx.r_ex
+define|#
+directive|define
+name|vmf_dh
+value|edx.r_b.r_h
+define|#
+directive|define
+name|vmf_dl
+value|edx.r_b.r_l
 define|#
 directive|define
 name|vmf_dx
