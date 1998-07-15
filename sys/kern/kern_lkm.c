76e6b98df0f1ca50cc5254749a12d13feef11ace
Bruce Evans <bde@FreeBSD.org>
1998-07-15 06:39:12 +0000

    Cast between u_longs and object pointers via uintptr_t.
    Access the entry address as a uintfptr_t, not as a long, and not
    necessarily as what modload(8) passes (it takes a u_long from the
    exec header and passes a u_int).
    
    
    Former-commit-id: d4ccfca21d913108f8286a83e0c7e41aa3aa5906

--- sys/kern/kern_lkm.c
+++ sys/kern/kern_lkm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1994 Christopher G. Demetriou  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY TERRENCE R. LAMBERT ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kern_lkm.c,v 1.49 1998/06/07 17:11:34 dfr Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1994 Christopher G. Demetriou  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Terrence R. Lambert.  * 4. The name Terrence R. Lambert may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY TERRENCE R. LAMBERT ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kern_lkm.c,v 1.50 1998/06/25 11:27:36 phk Exp $  */
 end_comment
 
 begin_include
@@ -1019,9 +1019,13 @@ name|loadbufp
 operator|->
 name|data
 argument_list|,
-operator|(
+call|(
 name|caddr_t
-operator|)
+call|)
+argument_list|(
+name|uintptr_t
+argument_list|)
+argument_list|(
 name|curp
 operator|->
 name|area
@@ -1029,6 +1033,7 @@ operator|+
 name|curp
 operator|->
 name|offset
+argument_list|)
 argument_list|,
 name|i
 argument_list|)
@@ -1179,9 +1184,13 @@ case|:
 comment|/* The remainder must be bss, so we clear it */
 name|bzero
 argument_list|(
-operator|(
+call|(
 name|caddr_t
-operator|)
+call|)
+argument_list|(
+name|uintptr_t
+argument_list|)
+argument_list|(
 name|curp
 operator|->
 name|area
@@ -1189,6 +1198,7 @@ operator|+
 name|curp
 operator|->
 name|offset
+argument_list|)
 argument_list|,
 name|curp
 operator|->
@@ -1244,12 +1254,10 @@ operator|)
 operator|(
 operator|*
 operator|(
-operator|(
-name|long
+name|uintfptr_t
 operator|*
 operator|)
 name|data
-operator|)
 operator|)
 expr_stmt|;
 comment|/* call entry(load)... (assigns "private" portion) */
@@ -1737,8 +1745,7 @@ operator|->
 name|private
 operator|=
 operator|(
-name|unsigned
-name|long
+name|uintptr_t
 operator|)
 name|curp
 operator|->
