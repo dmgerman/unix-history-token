3a275352e572986658eee90c0ac126bda5bce5b4
Bruce Evans <bde@FreeBSD.org>
1998-07-15 04:43:49 +0000

    Cast function pointers to uintfptr_t before casting them to u_long. Hopefully caddr_t is large enough to hold function pointers.
    Cast object pointers to uintptr_t before casting them to u_long.
    
    Types are wronger than usual for the PT_READ_U case.  ptrace() can
    only return ints, but longs are accessed.
    
    
    Former-commit-id: e276b36f47cb60c03035313e0f33e5d949f63f47

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994, Sean Eric Fagan  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Sean Eric Fagan.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sys_process.c,v 1.37 1998/05/19 00:00:12 tegge Exp $  */
+comment|/*  * Copyright (c) 1994, Sean Eric Fagan  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Sean Eric Fagan.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: sys_process.c,v 1.38 1998/06/07 17:11:39 dfr Exp $  */
 end_comment
 
 begin_include
@@ -723,6 +723,9 @@ argument_list|,
 operator|(
 name|u_long
 operator|)
+operator|(
+name|uintfptr_t
+operator|)
 name|uap
 operator|->
 name|addr
@@ -939,7 +942,7 @@ operator|(
 name|off_t
 operator|)
 operator|(
-name|u_long
+name|uintptr_t
 operator|)
 name|uap
 operator|->
@@ -1032,13 +1035,12 @@ case|:
 if|if
 condition|(
 operator|(
-name|u_long
+name|uintptr_t
 operator|)
 name|uap
 operator|->
 name|addr
 operator|>
-operator|(
 name|UPAGES
 operator|*
 name|PAGE_SIZE
@@ -1047,7 +1049,6 @@ sizeof|sizeof
 argument_list|(
 name|long
 argument_list|)
-operator|)
 condition|)
 block|{
 return|return
@@ -1142,14 +1143,14 @@ operator|*
 operator|)
 operator|(
 operator|(
-name|u_long
+name|uintptr_t
 operator|)
 name|p
 operator|->
 name|p_addr
 operator|+
 operator|(
-name|u_long
+name|uintptr_t
 operator|)
 name|uap
 operator|->
