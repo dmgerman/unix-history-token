f37a4576bfcd505def4b3a9920c5bb72833c7e37
Bruce Evans <bde@FreeBSD.org>
1998-07-15 06:19:33 +0000

    Cast between longs and pointers via intptr_t.  The results of fuword() should be checked before casting.  The results of suword() should be checked.
    Former-commit-id: d1d741cb1b4f8fea9902c5adc6b67c407af22934

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_exec.c,v 1.82 1998/04/17 22:36:50 des Exp $  */
+comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: kern_exec.c,v 1.83 1998/06/07 17:11:33 dfr Exp $  */
 end_comment
 
 begin_include
@@ -1281,6 +1281,9 @@ argument_list|,
 operator|(
 name|u_long
 operator|)
+operator|(
+name|uintptr_t
+operator|)
 name|stack_base
 argument_list|)
 expr_stmt|;
@@ -2051,6 +2054,9 @@ operator|=
 operator|(
 name|caddr_t
 operator|)
+operator|(
+name|intptr_t
+operator|)
 name|fuword
 argument_list|(
 name|argv
@@ -2178,6 +2184,9 @@ operator|=
 operator|(
 name|caddr_t
 operator|)
+operator|(
+name|intptr_t
+operator|)
 name|fuword
 argument_list|(
 name|argv
@@ -2209,6 +2218,9 @@ name|envp
 operator|=
 operator|(
 name|caddr_t
+operator|)
+operator|(
+name|intptr_t
 operator|)
 name|fuword
 argument_list|(
@@ -2543,6 +2555,9 @@ argument_list|,
 operator|(
 name|long
 operator|)
+operator|(
+name|intptr_t
+operator|)
 name|vectp
 argument_list|)
 expr_stmt|;
@@ -2575,6 +2590,9 @@ operator|++
 argument_list|,
 operator|(
 name|long
+operator|)
+operator|(
+name|intptr_t
 operator|)
 name|destp
 argument_list|)
@@ -2613,6 +2631,9 @@ argument_list|,
 operator|(
 name|long
 operator|)
+operator|(
+name|intptr_t
+operator|)
 name|vectp
 argument_list|)
 expr_stmt|;
@@ -2645,6 +2666,9 @@ operator|++
 argument_list|,
 operator|(
 name|long
+operator|)
+operator|(
+name|intptr_t
 operator|)
 name|destp
 argument_list|)
