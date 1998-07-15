25c7a7f60f9fe801a8943dd862437de255f7891f
Doug Rabson <dfr@FreeBSD.org>
1998-07-15 20:07:33 +0000

    Add some debug code.
    Former-commit-id: 62c7c91675cf089f3643c95f6588cc7b552574c1

--- sys/alpha/alpha/pmap.c
+++ sys/alpha/alpha/pmap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	from:	i386 Id: pmap.c,v 1.193 1998/04/19 15:22:48 bde Exp  *		with some ideas from NetBSD's alpha pmap  *	$Id: pmap.c,v 1.2 1998/07/05 11:58:35 dfr Exp $  */
+comment|/*  * Copyright (c) 1991 Regents of the University of California.  * All rights reserved.  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  * Copyright (c) 1994 David Greenman  * All rights reserved.  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and William Jolitz of UUNET Technologies Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)pmap.c	7.7 (Berkeley)	5/12/91  *	from:	i386 Id: pmap.c,v 1.193 1998/04/19 15:22:48 bde Exp  *		with some ideas from NetBSD's alpha pmap  *	$Id: pmap.c,v 1.3 1998/07/12 16:13:54 dfr Exp $  */
 end_comment
 
 begin_comment
@@ -182,6 +182,12 @@ directive|define
 name|PMAP_DIAGNOSTIC
 end_define
 
+begin_define
+define|#
+directive|define
+name|PMAP_DEBUG
+end_define
+
 begin_if
 if|#
 directive|if
@@ -225,7 +231,9 @@ begin_function
 specifier|static
 name|void
 name|pmap_break
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{ }
 end_function
 
@@ -4331,13 +4339,9 @@ operator|.
 name|resident_count
 operator|--
 expr_stmt|;
-if|#
-directive|if
-operator|!
-name|defined
-argument_list|(
-name|MAX_PERF
-argument_list|)
+ifdef|#
+directive|ifdef
+name|PMAP_DEBUG
 if|if
 condition|(
 name|p
@@ -4401,6 +4405,69 @@ name|pm_ptphint
 operator|=
 name|NULL
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|PMAP_DEBUG
+block|{
+name|u_long
+modifier|*
+name|lp
+init|=
+operator|(
+name|u_long
+operator|*
+operator|)
+name|ALPHA_PHYS_TO_K0SEG
+argument_list|(
+name|VM_PAGE_TO_PHYS
+argument_list|(
+name|p
+argument_list|)
+argument_list|)
+decl_stmt|;
+name|u_long
+modifier|*
+name|ep
+init|=
+operator|(
+name|u_long
+operator|*
+operator|)
+operator|(
+operator|(
+name|char
+operator|*
+operator|)
+name|lp
+operator|+
+name|PAGE_SIZE
+operator|)
+decl_stmt|;
+for|for
+control|(
+init|;
+name|lp
+operator|<
+name|ep
+condition|;
+name|lp
+operator|++
+control|)
+if|if
+condition|(
+operator|*
+name|lp
+operator|!=
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"pmap_release_free_page: page not zero"
+argument_list|)
+expr_stmt|;
+block|}
+endif|#
+directive|endif
 name|vm_page_free_zero
 argument_list|(
 name|p
@@ -5134,6 +5201,13 @@ name|s
 operator|=
 name|splhigh
 argument_list|()
+expr_stmt|;
+name|printf
+argument_list|(
+literal|"pmap_growkernel: growing to %lx\n"
+argument_list|,
+name|addr
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
