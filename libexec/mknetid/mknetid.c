990ef14e22dcb9412992ca25142aeabd7e383cc6
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-07-19 10:56:06 +0000

    Whoops - looks like patch flubbed this one; fix.
    Former-commit-id: ef1df84b15f09f6b97ba15139053c7ab85a0257f

--- libexec/mknetid/mknetid.c
+++ libexec/mknetid/mknetid.c
@@ -16,33 +16,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: mknetid.c,v 1.4.2.2 1997/12/15 07:16:15 charnier Exp $"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$Id$"
+literal|"$Id: mknetid.c,v 1.4.2.3 1998/07/19 06:53:02 jkh Exp $"
 decl_stmt|;
 end_decl_stmt
 
