d31c77239ad620ec5cd889e6d7070b5d2868f53e
Julian Elischer <julian@FreeBSD.org>
1998-07-22 19:37:02 +0000

    Don't return an error if the magic number is not present. Failing to probe is not an error, just a negative result.
    Former-commit-id: a1495bddc05f6fc8df322fa25dadb581bd3ed14d

--- sys/dev/slice/mbr.c
+++ sys/dev/slice/mbr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 1997,1998 Julian Elischer.  All rights reserved.  * julian@freebsd.org  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met:  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright notice,  *     this list of conditions and the following disclaimer in the documentation  *     and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE HOLDER OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *   *	$Id: mbr.c,v 1.7 1998/07/13 08:22:55 julian Exp $  */
+comment|/*-  * Copyright (C) 1997,1998 Julian Elischer.  All rights reserved.  * julian@freebsd.org  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met:  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright notice,  *     this list of conditions and the following disclaimer in the documentation  *     and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE HOLDER OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *   *	$Id: mbr.c,v 1.8 1998/07/20 04:12:39 julian Exp $  */
 end_comment
 
 begin_include
@@ -771,6 +771,15 @@ argument_list|(
 name|bp
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|bootverbose
+condition|)
+name|printf
+argument_list|(
+literal|"failed.. IO error "
+argument_list|)
+expr_stmt|;
 goto|goto
 name|nope
 goto|;
@@ -811,9 +820,14 @@ argument_list|(
 name|bp
 argument_list|)
 expr_stmt|;
-name|error
-operator|=
-name|EINVAL
+if|if
+condition|(
+name|bootverbose
+condition|)
+name|printf
+argument_list|(
+literal|"rejected.. bad magic "
+argument_list|)
 expr_stmt|;
 goto|goto
 name|nope
@@ -977,6 +991,10 @@ operator|&
 literal|0x7f
 condition|)
 block|{
+if|if
+condition|(
+name|bootverbose
+condition|)
 name|printf
 argument_list|(
 literal|"rejected.. bad flag "
@@ -1009,6 +1027,10 @@ literal|0
 operator|)
 condition|)
 block|{
+if|if
+condition|(
+name|bootverbose
+condition|)
 name|printf
 argument_list|(
 literal|"rejected.. Slice includes MBR "
@@ -1037,6 +1059,10 @@ operator|>
 literal|1
 condition|)
 block|{
+if|if
+condition|(
+name|bootverbose
+condition|)
 name|printf
 argument_list|(
 literal|"rejected.. multiple active "
@@ -1212,6 +1238,15 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 comment|/* 	 * Handle each of the partitions.  	 * We should check that each makes sense and is legal. 	 * 1/ it should not already have a slice. 	 * 2/ should not be 0 length. 	 * 3/ should not go past end of our slice. 	 * 4/ should not include sector 0. 	 * 5/ should not overlap other slices. 	 * 	 * Be aware that this may queue up one (or more) IO requests 	 * for each subslice created. 	 */
+if|if
+condition|(
+name|bootverbose
+condition|)
+name|printf
+argument_list|(
+literal|"yep \n"
+argument_list|)
+expr_stmt|;
 name|dp
 operator|=
 name|dp0
@@ -1852,6 +1887,11 @@ operator|)
 return|;
 name|nope
 label|:
+name|printf
+argument_list|(
+literal|"nope\n"
+argument_list|)
+expr_stmt|;
 name|mbr_revoke
 argument_list|(
 name|pd
