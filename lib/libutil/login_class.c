fb90e4eb2dc4ae743e1b87ee61dfc15a048c1141
Andrey Chernov <ache@FreeBSD.org>
1998-07-28 01:30:16 +0000

    cast arg to (long) to match format
    Former-commit-id: acae7be5d43aecedf79dd77294e4a5764b6e6647

--- lib/libutil/login_class.c
+++ lib/libutil/login_class.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1996 by  * Sean Eric Fagan<sef@kithrup.com>  * David Nugent<davidn@blaze.net.au>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, is permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. This work was done expressly for inclusion into FreeBSD.  Other use  *    is permitted provided this notation is included.  * 4. Absolutely no warranty of function or purpose is made by the authors.  * 5. Modifications may be freely made to this file providing the above  *    conditions are met.  *  * High-level routines relating to use of the user capabilities database  *  *	$Id: login_class.c,v 1.7 1998/05/25 03:55:23 steve Exp $  */
+comment|/*-  * Copyright (c) 1996 by  * Sean Eric Fagan<sef@kithrup.com>  * David Nugent<davidn@blaze.net.au>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, is permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. This work was done expressly for inclusion into FreeBSD.  Other use  *    is permitted provided this notation is included.  * 4. Absolutely no warranty of function or purpose is made by the authors.  * 5. Modifications may be freely made to this file providing the above  *    conditions are met.  *  * High-level routines relating to use of the user capabilities database  *  *	$Id: login_class.c,v 1.8 1998/06/03 08:12:00 jb Exp $  */
 end_comment
 
 begin_include
@@ -1782,6 +1782,9 @@ name|pwd
 operator|->
 name|pw_name
 argument_list|,
+operator|(
+name|long
+operator|)
 name|pwd
 operator|->
 name|pw_gid
@@ -1894,6 +1897,9 @@ name|LOG_ERR
 argument_list|,
 literal|"setuid(%ld): %m"
 argument_list|,
+operator|(
+name|long
+operator|)
 name|uid
 argument_list|)
 expr_stmt|;
