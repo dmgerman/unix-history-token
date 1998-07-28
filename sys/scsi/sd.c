8111a146f81ef9946a2be4f24fec1f1470569bb4
Bruce Evans <bde@FreeBSD.org>
1998-07-28 18:59:49 +0000

    Set the disk type to SCSI in the in-core label for the whole disk.
    Former-commit-id: 10a944fb85af22c870ad89b2613b0deb4e5d9008

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.134 1998/07/13 08:23:05 julian Exp $  */
+comment|/*  * Written by Julian Elischer (julian@dialix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992  *  *      $Id: sd.c,v 1.135 1998/07/14 11:34:22 bde Exp $  */
 end_comment
 
 begin_include
@@ -2166,6 +2166,12 @@ argument_list|,
 sizeof|sizeof
 name|label
 argument_list|)
+expr_stmt|;
+name|label
+operator|.
+name|d_type
+operator|=
+name|DTYPE_SCSI
 expr_stmt|;
 name|label
 operator|.
