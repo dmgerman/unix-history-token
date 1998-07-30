04c1fe5ea0dfc42dc3ace696453d1c1f3a780381
Bruce Evans <bde@FreeBSD.org>
1998-07-30 17:40:45 +0000

    Fixed printf format errors.
    Former-commit-id: aab348b53543787d8c4ba34149e526376366cb86

--- sys/fs/nullfs/null_vfsops.c
+++ sys/fs/nullfs/null_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)null_vfsops.c	8.2 (Berkeley) 1/21/94  *  * @(#)lofs_vfsops.c	1.2 (Berkeley) 6/18/92  * $Id: null_vfsops.c,v 1.25 1998/03/01 22:46:18 msmith Exp $  */
+comment|/*  * Copyright (c) 1992, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software donated to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)null_vfsops.c	8.2 (Berkeley) 1/21/94  *  * @(#)lofs_vfsops.c	1.2 (Berkeley) 6/18/92  * $Id: null_vfsops.c,v 1.26 1998/05/06 05:29:34 msmith Exp $  */
 end_comment
 
 begin_comment
@@ -438,8 +438,12 @@ directive|ifdef
 name|NULLFS_DIAGNOSTIC
 name|printf
 argument_list|(
-literal|"nullfs_mount(mp = %x)\n"
+literal|"nullfs_mount(mp = %p)\n"
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|mp
 argument_list|)
 expr_stmt|;
@@ -982,8 +986,12 @@ directive|ifdef
 name|NULLFS_DIAGNOSTIC
 name|printf
 argument_list|(
-literal|"nullfs_unmount(mp = %x)\n"
+literal|"nullfs_unmount(mp = %p)\n"
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|mp
 argument_list|)
 expr_stmt|;
@@ -1125,10 +1133,18 @@ directive|ifdef
 name|NULLFS_DIAGNOSTIC
 name|printf
 argument_list|(
-literal|"nullfs_root(mp = %x, vp = %x->%x)\n"
+literal|"nullfs_root(mp = %p, vp = %p->%p)\n"
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|mp
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|MOUNTTONULLMOUNT
 argument_list|(
 name|mp
@@ -1136,6 +1152,10 @@ argument_list|)
 operator|->
 name|nullm_rootvp
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|NULLVPTOLOWERVP
 argument_list|(
 name|MOUNTTONULLMOUNT
@@ -1313,10 +1333,18 @@ directive|ifdef
 name|NULLFS_DIAGNOSTIC
 name|printf
 argument_list|(
-literal|"nullfs_statfs(mp = %x, vp = %x->%x)\n"
+literal|"nullfs_statfs(mp = %p, vp = %p->%p)\n"
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|mp
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|MOUNTTONULLMOUNT
 argument_list|(
 name|mp
@@ -1324,6 +1352,10 @@ argument_list|)
 operator|->
 name|nullm_rootvp
 argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
 name|NULLVPTOLOWERVP
 argument_list|(
 name|MOUNTTONULLMOUNT
