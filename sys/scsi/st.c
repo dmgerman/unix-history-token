680a789da043b4af4c8833886927bba98e0f5e49
Poul-Henning Kamp <phk@FreeBSD.org>
1998-07-31 09:00:39 +0000

    In sys/scsi/st.c, st_close() does not check for errors from calling st_write_filemarks().  This means that it is possible to write a file on the tape for which all the writes and the close return without errors, but the all bits did not make it onto the tape.
    PR:		741
    Reviewed by:	phk
    Submitted by:	Andrew Heybey <ath@niksun.com>
    
    Former-commit-id: 3807e74077c973d2df8177df4583d621514d6602

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.90 1998/07/04 22:30:24 julian Exp $  */
+comment|/*  * Written by Julian Elischer (julian@tfs.com)(now julian@DIALix.oz.au)  * for TRW Financial Systems for use under the MACH(2.5) operating system.  *  * TRW Financial Systems, in accordance with their agreement with Carnegie  * Mellon University, makes this software available to CMU to distribute  * or use in any manner that they see fit as long as this message is kept with  * the software. For this reason TFS also grants any other persons or  * organisations permission to use or modify this software.  *  * TFS supplies this software to be publicly redistributed  * on the understanding that TFS is not responsible for the correct  * functioning of this software in any circumstances.  *  * $Id: st.c,v 1.91 1998/07/11 07:46:01 bde Exp $  */
 end_comment
 
 begin_comment
@@ -2249,6 +2249,11 @@ name|scsi_data
 modifier|*
 name|st
 decl_stmt|;
+name|errval
+name|errcode
+init|=
+literal|0
+decl_stmt|;
 name|unit
 operator|=
 name|STUNIT
@@ -2285,6 +2290,8 @@ operator|)
 operator|==
 name|ST_WRITTEN
 condition|)
+name|errcode
+operator|=
 name|st_write_filemarks
 argument_list|(
 name|unit
@@ -2379,7 +2386,7 @@ name|ST_OPEN
 expr_stmt|;
 return|return
 operator|(
-literal|0
+name|errcode
 operator|)
 return|;
 block|}
