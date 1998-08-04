02d8ad19bbf907cbfdf7f87b71202db6d6d8d38f
Julian Elischer <julian@FreeBSD.org>
1998-08-04 23:17:05 +0000

    fix broken loopback code for ddp (again) Submitted by: Stefan Bethke <stb@hanse.de>
    Former-commit-id: e08ab8537774e137f05cb6dce8050c0ef77bffb2

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ethersubr.c	8.1 (Berkeley) 6/10/93  * $Id: if_ethersubr.c,v 1.50 1998/06/13 02:27:10 julian Exp $  */
+comment|/*  * Copyright (c) 1982, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ethersubr.c	8.1 (Berkeley) 6/10/93  * $Id: if_ethersubr.c,v 1.51 1998/06/14 20:58:14 julian Exp $  */
 end_comment
 
 begin_include
@@ -510,6 +510,10 @@ name|loop_copy
 init|=
 literal|0
 decl_stmt|;
+name|int
+name|hlen
+decl_stmt|;
+comment|/* link layer header lenght */
 name|struct
 name|arpcom
 modifier|*
@@ -720,6 +724,10 @@ name|EHOSTUNREACH
 argument_list|)
 expr_stmt|;
 block|}
+name|hlen
+operator|=
+name|ETHER_HDR_LEN
+expr_stmt|;
 switch|switch
 condition|(
 name|dst
@@ -989,6 +997,16 @@ name|m_pkthdr
 operator|.
 name|len
 argument_list|)
+expr_stmt|;
+name|hlen
+operator|=
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|llc
+argument_list|)
+operator|+
+name|ETHER_HDR_LEN
 expr_stmt|;
 block|}
 else|else
@@ -1940,7 +1958,7 @@ name|n
 argument_list|,
 name|dst
 argument_list|,
-name|ETHER_HDR_LEN
+name|hlen
 argument_list|)
 expr_stmt|;
 block|}
@@ -1974,7 +1992,7 @@ name|m
 argument_list|,
 name|dst
 argument_list|,
-name|ETHER_HDR_LEN
+name|hlen
 argument_list|)
 expr_stmt|;
 return|return
