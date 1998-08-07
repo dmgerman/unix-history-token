1371fa65b3181fb7bd02a7b82ee2190ca8aae3fe
Doug Rabson <dfr@FreeBSD.org>
1998-08-07 08:17:39 +0000

    Add some more useful macros.
    Submitted by: Andrew Gallatin <gallatin@cs.duke.edu>
    
    
    Former-commit-id: a8167e699723c56715cda1567b339a240c535c15

--- sys/alpha/include/swiz.h
+++ sys/alpha/include/swiz.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: swiz.h,v 1.1 1998/07/27 09:40:35 dfr Exp $  */
 end_comment
 
 begin_ifndef
@@ -74,6 +74,42 @@ end_define
 begin_define
 define|#
 directive|define
+name|SPARSE_BYTE_ADDRESS
+parameter_list|(
+name|base
+parameter_list|,
+name|o
+parameter_list|)
+value|((base) + SPARSE_BYTE_OFFSET(o))
+end_define
+
+begin_define
+define|#
+directive|define
+name|SPARSE_WORD_ADDRESS
+parameter_list|(
+name|base
+parameter_list|,
+name|o
+parameter_list|)
+value|((base) + SPARSE_WORD_OFFSET(o))
+end_define
+
+begin_define
+define|#
+directive|define
+name|SPARSE_LONG_ADDRESS
+parameter_list|(
+name|base
+parameter_list|,
+name|o
+parameter_list|)
+value|((base) + SPARSE_LONG_OFFSET(o))
+end_define
+
+begin_define
+define|#
+directive|define
 name|SPARSE_BYTE_EXTRACT
 parameter_list|(
 name|o
@@ -98,6 +134,18 @@ end_define
 begin_define
 define|#
 directive|define
+name|SPARSE_LONG_EXTRACT
+parameter_list|(
+name|o
+parameter_list|,
+name|d
+parameter_list|)
+value|(d)
+end_define
+
+begin_define
+define|#
+directive|define
 name|SPARSE_BYTE_INSERT
 parameter_list|(
 name|o
@@ -117,6 +165,18 @@ parameter_list|,
 name|d
 parameter_list|)
 value|((d)<< (8*((o)& 2)))
+end_define
+
+begin_define
+define|#
+directive|define
+name|SPARSE_LONG_INSERT
+parameter_list|(
+name|o
+parameter_list|,
+name|d
+parameter_list|)
+value|(d)
 end_define
 
 begin_define
