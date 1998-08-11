3b9df71acd50920fc3ce3002c5c6628431a34216
Bruce Evans <bde@FreeBSD.org>
1998-08-11 15:08:13 +0000

    Implemented dynamic registration of software interrupt handlers.  Not used yet.
    Use dummy SWI handlers to avoid some checks for null pointers.
    
    
    Former-commit-id: 3c0c838fe4b381793ad51945bfb19cbc26dc4f02

--- sys/sys/interrupt.h
+++ sys/sys/interrupt.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, Stefan Esser<se@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: interrupt.h,v 1.5 1997/06/08 17:15:31 ache Exp $  */
+comment|/*  * Copyright (c) 1997, Stefan Esser<se@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: interrupt.h,v 1.6 1997/07/09 18:08:15 ache Exp $  */
 end_comment
 
 begin_comment
@@ -28,6 +28,16 @@ end_define
 begin_comment
 comment|/* excl. intr, default is shared */
 end_comment
+
+begin_typedef
+typedef|typedef
+name|void
+name|swihand_t
+name|__P
+typedef|((
+name|void
+typedef|));
+end_typedef
 
 begin_function_decl
 name|struct
@@ -95,6 +105,65 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_decl_stmt
+name|void
+name|register_swi
+name|__P
+argument_list|(
+operator|(
+name|int
+name|intr
+operator|,
+name|swihand_t
+operator|*
+name|handler
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|swi_dispatcher
+name|__P
+argument_list|(
+operator|(
+name|int
+name|intr
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|swihand_t
+name|swi_generic
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|swihand_t
+name|swi_null
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|unregister_swi
+name|__P
+argument_list|(
+operator|(
+name|int
+name|intr
+operator|,
+name|swihand_t
+operator|*
+name|handler
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_comment
 comment|/* XXX emulate old interface for now ... */
 end_comment
@@ -141,6 +210,34 @@ name|handler
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|NHWI
+end_ifdef
+
+begin_comment
+comment|/* XXX type change in middle; MI code uses only the top NSWI entries. */
+end_comment
+
+begin_decl_stmt
+specifier|extern
+name|swihand_t
+modifier|*
+name|ihandlers
+index|[
+name|NHWI
+operator|+
+name|NSWI
+index|]
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
