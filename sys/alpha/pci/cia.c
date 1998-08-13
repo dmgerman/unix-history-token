928e62fcf8f50476040d2e29da007480e6699eb0
Doug Rabson <dfr@FreeBSD.org>
1998-08-13 08:11:27 +0000

    I managed to break sparse configuration accesses with my last commit.
    Pointed out by: Andrew Gallatin <gallatin@cs.duke.edu>
    
    
    Former-commit-id: ab21cb4948a84d9bc06f6452c2fca3a04b0489a9

--- sys/alpha/pci/cia.c
+++ sys/alpha/pci/cia.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cia.c,v 1.6 1998/08/10 07:53:59 dfr Exp $  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cia.c,v 1.7 1998/08/11 08:51:09 dfr Exp $  */
 end_comment
 
 begin_comment
@@ -1717,7 +1717,7 @@ parameter_list|,
 name|type
 parameter_list|)
 define|\
-value|type val = ~0;							\ 	int ipl = 0;							\ 	u_int32_t old_cfg = 0;						\ 	vm_offset_t off = CIA_SWIZ_CFGOFF(b, s, f, r);			\ 	vm_offset_t kv = SPARSE_##width##_ADDRESS(CIA_PCI_CONF, off);	\ 	alpha_mb();							\ 	CIA_TYPE1_SETUP(b,ipl,old_cfg);					\ 	if (!badaddr((caddr_t)kv, sizeof(type))) {			\ 		val = SPARSE_##width##_EXTRACT(off, SPARSE_READ(kv));	\ 	}								\         CIA_TYPE1_TEARDOWN(b,ipl,old_cfg);				\ 	return val;
+value|type val = ~0;							\ 	int ipl = 0;							\ 	u_int32_t old_cfg = 0;						\ 	vm_offset_t off = CIA_SWIZ_CFGOFF(b, s, f, r);			\ 	vm_offset_t kv = SPARSE_##width##_ADDRESS(KV(CIA_PCI_CONF), off); \ 	alpha_mb();							\ 	CIA_TYPE1_SETUP(b,ipl,old_cfg);					\ 	if (!badaddr((caddr_t)kv, sizeof(type))) {			\ 		val = SPARSE_##width##_EXTRACT(off, SPARSE_READ(kv));	\ 	}								\         CIA_TYPE1_TEARDOWN(b,ipl,old_cfg);				\ 	return val;
 end_define
 
 begin_define
@@ -1740,7 +1740,7 @@ parameter_list|,
 name|type
 parameter_list|)
 define|\
-value|int ipl = 0;							\ 	u_int32_t old_cfg = 0;						\ 	vm_offset_t off = CIA_SWIZ_CFGOFF(b, s, f, r);			\ 	vm_offset_t kv = SPARSE_##width##_ADDRESS(CIA_PCI_CONF, off);	\ 	alpha_mb();							\ 	CIA_TYPE1_SETUP(b,ipl,old_cfg);					\ 	if (!badaddr((caddr_t)kv, sizeof(type))) {			\                 SPARSE_WRITE(kv, SPARSE_##width##_INSERT(off, data));	\ 		alpha_wmb();						\ 	}								\         CIA_TYPE1_TEARDOWN(b,ipl,old_cfg);				\ 	return;
+value|int ipl = 0;							\ 	u_int32_t old_cfg = 0;						\ 	vm_offset_t off = CIA_SWIZ_CFGOFF(b, s, f, r);			\ 	vm_offset_t kv = SPARSE_##width##_ADDRESS(KV(CIA_PCI_CONF), off); \ 	alpha_mb();							\ 	CIA_TYPE1_SETUP(b,ipl,old_cfg);					\ 	if (!badaddr((caddr_t)kv, sizeof(type))) {			\                 SPARSE_WRITE(kv, SPARSE_##width##_INSERT(off, data));	\ 		alpha_wmb();						\ 	}								\         CIA_TYPE1_TEARDOWN(b,ipl,old_cfg);				\ 	return;
 end_define
 
 begin_function
