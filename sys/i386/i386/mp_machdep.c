1c2aa303075bdbf7eb820e92ca8fc0992d6bf4af
Bruce Evans <bde@FreeBSD.org>
1998-08-16 00:41:40 +0000

    pmap.c: Cast pointers to (vm_offset_t) instead of to (u_long) (as before) or to (uintptr_t)(void *) (as would be more correct).  Don't cast vm_offset_t's to (u_long) just to do arithmetic on them.
    mp_machdep.c:
    Cast pointers to (uintptr_t) instead of to (u_long).  Don't forget
    to cast pointers to (void *) first or to recover from integral
    possible integral promotions, although this is too much work for
    machine-dependent code.
    
    vm code generally avoids warnings for pointer vs long size mismatches
    by using vm_offset_t to represent pointers; pmap.c often uses plain
    `unsigned int' instead of vm_offset_t and didn't use u_long elsewhere,
    but this style was messed up by code apparently imported from mp_machdep.c.
    
    
    Former-commit-id: 435dd1902429f43b87129e8b8a06ea6d8027c787

--- sys/i386/i386/mp_machdep.c
+++ sys/i386/i386/mp_machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.75 1998/05/17 18:53:17 tegge Exp $  */
+comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.76 1998/05/17 22:12:08 tegge Exp $  */
 end_comment
 
 begin_include
@@ -2295,7 +2295,11 @@ name|PG_RW
 operator||
 operator|(
 operator|(
-name|u_long
+name|uintptr_t
+operator|)
+operator|(
+name|void
+operator|*
 operator|)
 name|KPTphys
 operator|&
@@ -6697,8 +6701,12 @@ index|[
 literal|0
 index|]
 operator|=
+operator|(
+name|void
+operator|*
+operator|)
 call|(
-name|pd_entry_t
+name|uintptr_t
 call|)
 argument_list|(
 name|PG_V
@@ -6707,7 +6715,11 @@ name|PG_RW
 operator||
 operator|(
 operator|(
-name|u_long
+name|uintptr_t
+operator|)
+operator|(
+name|void
+operator|*
 operator|)
 name|KPTphys
 operator|&
