6cf0b9deeb4a3e73b8d8e3f82a80ff8c73ee9040
John Polstra <jdp@FreeBSD.org>
1998-08-16 03:03:38 +0000

    Revamp the ELF include files to better support architecture-independent applications.  Here's how it works.
    The kernel should include <machine/elf.h> to get the definitions
    for the native architecture.  It can reference the various ELF
    structures with generic names like Elf_Sym, Elf_Shdr, etc.  A define
    __ELF_WORD_SIZE is also available with the value 32 or 64 as
    appropriate for the native architecture.
    
    Generic applications should include <elf.h>, which is just a wrapper
    for <machine/elf.h>.
    
    Applications such as object file dumpers that need to deal with
    foreign ELF files can include <sys/elf32.h> and/or <sys/elf64.h>.
    Both can be included from the same source file if desired.  The
    structure names must be referenced using wordsize-specific names
    like Elf32_Sym, Elf64_Shdr, etc.
    
    I haven't change the alpha stuff, but I haven't broken it either.
    
    
    Former-commit-id: a652199763a51c5b126ca295583e0b403972220b

--- sys/i386/include/elf.h
+++ sys/i386/include/elf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1996-1997 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: elf.h,v 1.1 1997/05/21 23:07:28 jdp Exp $  */
+comment|/*-  * Copyright (c) 1996-1997 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: elf.h,v 1.2 1997/08/30 18:59:48 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -29,6 +29,23 @@ end_include
 begin_comment
 comment|/* Definitions common to all 32 bit architectures. */
 end_comment
+
+begin_define
+define|#
+directive|define
+name|__ELF_WORD_SIZE
+value|32
+end_define
+
+begin_comment
+comment|/* Used by<sys/elf_generic.h> */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/elf_generic.h>
+end_include
 
 begin_comment
 comment|/*  * Auxiliary vector entries for passing information to the interpreter.  *  * The i386 supplement to the SVR4 ABI specification names this "auxv_t",  * but POSIX lays claim to all symbols ending with "_t".  */
@@ -71,6 +88,14 @@ block|}
 name|Elf32_Auxinfo
 typedef|;
 end_typedef
+
+begin_expr_stmt
+name|__ElfType
+argument_list|(
+name|Auxinfo
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/* Values for a_type. */
