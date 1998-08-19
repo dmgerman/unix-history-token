7d19d0776390e31044d30b2abb65445188ca6325
Bruce Evans <bde@FreeBSD.org>
1998-08-19 04:01:00 +0000

    A limit of 200000 for the output buffer high watermark was excessive, since (hardware) ttys have too low a bandwidth to benefit significantly from large buffers.  Use twice the old limit for the new-default case and 8 times the old limit for the driver-specifies-watermark case. Nothing uses these cases yet.
    Removed related debugging code.
    
    
    Former-commit-id: 15b23f84d2860f44b1d5f5027413abbf82a09482

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,16 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_decl_stmt
-specifier|static
-specifier|volatile
-name|int
-name|ttyverbose
-init|=
-literal|0
-decl_stmt|;
-end_decl_stmt
-
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.104 1998/06/07 17:11:41 dfr Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1990, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tty.c	8.8 (Berkeley) 1/21/94  * $Id: tty.c,v 1.105 1998/07/11 10:41:15 bde Exp $  */
 end_comment
 
 begin_comment
@@ -10799,17 +10789,6 @@ argument_list|,
 literal|512
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|ttyverbose
-condition|)
-name|printf
-argument_list|(
-literal|"ttsetwater: can: %d, "
-argument_list|,
-name|TTYHOG
-argument_list|)
-expr_stmt|;
 switch|switch
 condition|(
 name|tp
@@ -10901,17 +10880,6 @@ argument_list|,
 name|x
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|ttyverbose
-condition|)
-name|printf
-argument_list|(
-literal|"raw: %d, "
-argument_list|,
-name|x
-argument_list|)
-expr_stmt|;
 comment|/* Output. */
 switch|switch
 condition|(
@@ -10937,7 +10905,9 @@ literal|10
 expr_stmt|;
 name|ttmaxhiwat
 operator|=
-literal|200000
+literal|2
+operator|*
+name|TTMAXHIWAT
 expr_stmt|;
 break|break;
 case|case
@@ -10967,7 +10937,9 @@ literal|10
 expr_stmt|;
 name|ttmaxhiwat
 operator|=
-literal|200000
+literal|8
+operator|*
+name|TTMAXHIWAT
 expr_stmt|;
 break|break;
 block|}
@@ -11053,17 +11025,6 @@ operator|->
 name|t_outq
 argument_list|,
 name|x
-argument_list|,
-name|x
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|ttyverbose
-condition|)
-name|printf
-argument_list|(
-literal|"out: %d\n"
 argument_list|,
 name|x
 argument_list|)
