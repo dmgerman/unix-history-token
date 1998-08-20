b94b0c56599fcebe986a97af8b3fe3c0ea439e14
Doug Rabson <dfr@FreeBSD.org>
1998-08-20 08:27:11 +0000

    Add support for TurboChannel alphas (DEC 3000/300 and 3000/500).
    Obtained from: NetBSD
    Submitted by: Andrew Gallatin <gallatin@cs.duke.edu>
    
    
    Former-commit-id: aabf7dfec3166f2288989c418fb05635ff206de7

--- sys/alpha/alpha/autoconf.c
+++ sys/alpha/alpha/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: autoconf.c,v 1.5 1998/07/22 08:34:19 dfr Exp $  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: autoconf.c,v 1.6 1998/08/10 07:53:58 dfr Exp $  */
 end_comment
 
 begin_include
@@ -89,6 +89,12 @@ begin_include
 include|#
 directive|include
 file|<machine/cpuconf.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/rpb.h>
 end_include
 
 begin_include
@@ -260,15 +266,35 @@ expr_stmt|;
 name|root_bus_configure
 argument_list|()
 expr_stmt|;
+if|if
+condition|(
+operator|(
+name|hwrpb
+operator|->
+name|rpb_type
+operator|!=
+name|ST_DEC_3000_300
+operator|)
+operator|&&
+operator|(
+name|hwrpb
+operator|->
+name|rpb_type
+operator|!=
+name|ST_DEC_3000_500
+operator|)
+condition|)
+block|{
 name|pci_configure
 argument_list|()
 expr_stmt|;
-comment|/* 	 * Probe ISA devices after everything. 	 */
+comment|/* 		 * Probe ISA devices after everything. 		 */
 name|bus_generic_attach
 argument_list|(
 name|isa_bus_device
 argument_list|)
 expr_stmt|;
+block|}
 name|configure_finish
 argument_list|()
 expr_stmt|;
