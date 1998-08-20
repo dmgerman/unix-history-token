b1c34d501f181ea79f0e56d1586facca9b254ad2
Bruce Evans <bde@FreeBSD.org>
1998-08-20 06:10:42 +0000

    Moved `nx' functions to the one place where they are used (su.c). They shouldn't be used there either.  They should have gone away about 3 years ago when the statically initialized devswitches went away, but su.c unfortunately still frobs the cdevswitch in the old way.
    Former-commit-id: 16e440e36683eff6eb2be43e2c478ffd2964f6de

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)conf.h	8.5 (Berkeley) 1/9/95  * $Id: conf.h,v 1.42 1998/06/25 11:27:56 phk Exp $  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)conf.h	8.5 (Berkeley) 1/9/95  * $Id: conf.h,v 1.43 1998/07/04 22:30:26 julian Exp $  */
 end_comment
 
 begin_ifndef
@@ -885,108 +885,6 @@ end_define
 
 begin_comment
 comment|/* one unused function is as good as another */
-end_comment
-
-begin_decl_stmt
-name|d_open_t
-name|nxopen
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|d_close_t
-name|nxclose
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|d_read_t
-name|nxread
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|d_write_t
-name|nxwrite
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|d_ioctl_t
-name|nxioctl
-decl_stmt|;
-end_decl_stmt
-
-begin_define
-define|#
-directive|define
-name|nxstop
-value|nostop
-end_define
-
-begin_comment
-comment|/* one void return is as good as another */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|nxreset
-value|noreset
-end_define
-
-begin_comment
-comment|/* one unused function is as good as another */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|nxdevtotty
-value|nodevtotty
-end_define
-
-begin_comment
-comment|/* one NULL return is as good as another */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|nxmmap
-value|nommap
-end_define
-
-begin_comment
-comment|/* one -1 return is as good as another */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|nxstrategy
-value|nostrategy
-end_define
-
-begin_comment
-comment|/* one NULL value is as good as another */
-end_comment
-
-begin_decl_stmt
-name|d_dump_t
-name|nxdump
-decl_stmt|;
-end_decl_stmt
-
-begin_define
-define|#
-directive|define
-name|nxpsize
-value|nopsize
-end_define
-
-begin_comment
-comment|/* one NULL value is as good as another */
 end_comment
 
 begin_decl_stmt
