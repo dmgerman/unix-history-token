2f809284ca44fb5de7154f0f00392c947fe3fda1
Stephen McKay <mckay@FreeBSD.org>
1998-08-22 15:51:41 +0000

    Pass me the pointy hat with the extra sequins.  Just a moment, while I get it to sit right...
    The __error() hack gave out the wrong address.  It returned the address of
    errno in ld.so instead of the address of errno in the main program.  Oops.
    
    The hack is now correct, just in time to be obsoleted by elf.
    
    
    Former-commit-id: 6059b4cc0f23d2f66d14aae7991373ed7a31cfaa

--- libexec/rtld-aout/rtld.c
+++ libexec/rtld-aout/rtld.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: rtld.c,v 1.54 1998/06/07 03:53:06 brian Exp $  */
+comment|/*  * Copyright (c) 1993 Paul Kranenburg  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Paul Kranenburg.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: rtld.c,v 1.55 1998/06/21 14:22:29 mckay Exp $  */
 end_comment
 
 begin_include
@@ -6830,6 +6830,18 @@ return|;
 block|}
 end_function
 
+begin_decl_stmt
+specifier|static
+name|int
+modifier|*
+name|p_errno
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* Pointer to errno variable in main program. */
+end_comment
+
 begin_comment
 comment|/*  * Help old a.out binaries that are broken by the new errno macro.  They  * can be missing __error() through no fault of their own.  In particular,  * old a.out binaries can link against an old libc that does not contain  * __error(), yet still require __error() because of other libraries that  * have been recompiled since the errno change.  This locates the backward  * compatible work-alike we have hidden here in ld.so.  */
 end_comment
@@ -6885,6 +6897,67 @@ condition|)
 return|return
 name|NULL
 return|;
+comment|/* 	 * Locate errno in the main program.  If it's not there, NULL 	 * will be returned by our __error() substitute, and a core dump 	 * will follow.  That's impossible, of course, since crt0.o always 	 * supplies errno. 	 */
+name|smp
+operator|=
+name|NULL
+expr_stmt|;
+name|np
+operator|=
+name|lookup
+argument_list|(
+literal|"_errno"
+argument_list|,
+operator|&
+name|smp
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|np
+operator|!=
+name|NULL
+operator|&&
+name|smp
+operator|!=
+name|NULL
+condition|)
+block|{
+name|p_errno
+operator|=
+operator|(
+name|int
+operator|*
+operator|)
+operator|(
+name|smp
+operator|->
+name|som_addr
+operator|+
+name|np
+operator|->
+name|nz_value
+operator|)
+expr_stmt|;
+ifdef|#
+directive|ifdef
+name|DEBUG
+name|xprintf
+argument_list|(
+literal|" HACK: _errno at %p in %s\n"
+argument_list|,
+name|p_errno
+argument_list|,
+name|smp
+operator|->
+name|som_path
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
+block|}
 comment|/* 	 * Specifically find the ld.so link map because most routines 	 * skip over it during normal operation. 	 */
 for|for
 control|(
@@ -6911,7 +6984,7 @@ operator|&
 name|RTLD_RTLD
 condition|)
 break|break;
-comment|/* 	 * Actually, ld.so uses errno via the new macro, so it has a copy 	 * of __error() lying around.  The really neat but obscure hack 	 * is to just use that one, but to be really clear about what 	 * is going on, we use an explicit __error() substitute. 	 */
+comment|/* 	 * Find our __error() substitute stashed here in ld.so. 	 */
 name|np
 operator|=
 name|lookup
@@ -6982,8 +7055,7 @@ name|__error_unthreaded_hack
 parameter_list|()
 block|{
 return|return
-operator|&
-name|errno
+name|p_errno
 return|;
 block|}
 end_function
