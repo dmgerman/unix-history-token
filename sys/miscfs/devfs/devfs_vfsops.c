94ee8589df1925bb6d2d3b3736f97c5ad7e1ab27
Bruce Evans <bde@FreeBSD.org>
1998-09-08 16:59:37 +0000

    Oops, don't assume that the environment is normal in devfs_mount(). It isn't for the hidden mountpoint.  The static vfs's haven't been attached then, so mp->mnt_vfc can't be valid.
    Former-commit-id: 962d5f8d14fb53c3a0a69130ccdd1f2581bf71a8

--- sys/miscfs/devfs/devfs_vfsops.c
+++ sys/miscfs/devfs/devfs_vfsops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright 1997,1998 Julian Elischer.  All rights reserved.  * julian@freebsd.org  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met:  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright notice,  *     this list of conditions and the following disclaimer in the documentation  *     and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE HOLDER OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *   *	$Id: devfs_vfsops.c,v 1.31 1998/08/16 01:21:51 bde Exp $  *  */
+comment|/*-  * Copyright 1997,1998 Julian Elischer.  All rights reserved.  * julian@freebsd.org  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met:  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright notice,  *     this list of conditions and the following disclaimer in the documentation  *     and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE HOLDER OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *   *	$Id: devfs_vfsops.c,v 1.32 1998/09/07 13:17:00 bde Exp $  *  */
 end_comment
 
 begin_include
@@ -326,16 +326,6 @@ name|mount
 operator|=
 name|mp
 expr_stmt|;
-name|mp
-operator|->
-name|mnt_data
-operator|=
-operator|(
-name|void
-operator|*
-operator|)
-name|devfs_mp_p
-expr_stmt|;
 comment|/*- 	 *  Fill out some fields 	 */
 name|mp
 operator|->
@@ -346,6 +336,25 @@ name|qaddr_t
 operator|)
 name|devfs_mp_p
 expr_stmt|;
+if|if
+condition|(
+name|mp
+operator|->
+name|mnt_vfc
+operator|==
+name|NULL
+condition|)
+name|mp
+operator|->
+name|mnt_stat
+operator|.
+name|f_type
+operator|=
+operator|-
+literal|1
+expr_stmt|;
+comment|/* doing hidden mountpoint */
+else|else
 name|mp
 operator|->
 name|mnt_stat
@@ -391,9 +400,9 @@ index|]
 operator|=
 name|mp
 operator|->
-name|mnt_vfc
-operator|->
-name|vfc_typenum
+name|mnt_stat
+operator|.
+name|f_type
 expr_stmt|;
 name|mp
 operator|->
