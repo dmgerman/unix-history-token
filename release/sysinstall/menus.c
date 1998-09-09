3759cbc911c41f6d4e66d5f22001b648bb572ea8
Daniel O'Callaghan <danny@FreeBSD.org>
1998-09-09 05:09:55 +0000

    Fix typos.
    Former-commit-id: b722f0731e0f9dc121e3dd3cb0b7b5cce0bb2cc0

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: menus.c,v 1.167 1998/07/20 10:48:11 yokota Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: menus.c,v 1.168 1998/07/21 06:44:40 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -7408,9 +7408,9 @@ literal|"amd_flags"
 block|}
 block|,
 block|{
-literal|"TCP Extentions"
+literal|"TCP Extensions"
 block|,
-literal|"Allow RFC1323 and RFC1644 TCP extentions?"
+literal|"Allow RFC1323 and RFC1644 TCP extensions?"
 block|,
 name|dmenuVarCheck
 block|,
@@ -7418,7 +7418,7 @@ name|dmenuToggleVariable
 block|,
 name|NULL
 block|,
-literal|"tcp_extentions=YES"
+literal|"tcp_extensions=YES"
 block|}
 block|,
 block|{
@@ -7457,7 +7457,7 @@ directive|endif
 block|{
 literal|"Ntpdate"
 block|,
-literal|"Select a clock-syncronization server"
+literal|"Select a clock-synchronization server"
 block|,
 name|dmenuVarCheck
 block|,
@@ -7575,9 +7575,9 @@ name|DMENU_SELECTION_RETURNS
 block|,
 literal|"NTPDATE Server Selection"
 block|,
-literal|"There are a number of time syncronization servers available\n"
+literal|"There are a number of time synchronization servers available\n"
 literal|"for public use around the Internet.  Please select one reasonably\n"
-literal|"close to you to have your system time syncronized accordingly."
+literal|"close to you to have your system time synchronized accordingly."
 block|,
 literal|"These are the primary open-access NTP servers"
 block|,
