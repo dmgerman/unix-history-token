876934d8822c9f573b4ce29a3bcaab35dca7c9a0
Brian Somers <brian@FreeBSD.org>
1998-09-09 00:03:09 +0000

    Initialise lcp::his_mru to the ``set mtu'' value if it's less than DEF_MRU, allowing our interface mtu to be decreased despite negotiation with the peer.
    Former-commit-id: d3d044c2dfb91346db376660c7a754bac248cefd

--- usr.sbin/ppp/lcp.c
+++ usr.sbin/ppp/lcp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	      PPP Link Control Protocol (LCP) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: lcp.c,v 1.62 1998/08/07 18:42:49 brian Exp $  *  * TODO:  *	o Limit data field length by MRU  */
+comment|/*  *	      PPP Link Control Protocol (LCP) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: lcp.c,v 1.63 1998/09/04 18:25:59 brian Exp $  *  * TODO:  *	o Limit data field length by MRU  */
 end_comment
 
 begin_include
@@ -1203,6 +1203,33 @@ name|maxconfig
 operator|=
 literal|10
 expr_stmt|;
+name|lcp
+operator|->
+name|his_mru
+operator|=
+name|lcp
+operator|->
+name|fsm
+operator|.
+name|bundle
+operator|->
+name|cfg
+operator|.
+name|mtu
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|lcp
+operator|->
+name|his_mru
+operator|||
+name|lcp
+operator|->
+name|his_mru
+operator|>
+name|DEF_MRU
+condition|)
 name|lcp
 operator|->
 name|his_mru
