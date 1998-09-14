a842cb4f5cfc86e46922c5802137d281c6ebeefc
David E. O'Brien <obrien@FreeBSD.org>
1998-09-14 08:20:37 +0000

    Support our "nfsv2" option. (originally added to old amd/amd/host_opts.c rev 1.5) The "nfsv2" option is equivant to "proto=udp,vers=2".  It is debatable whether NFS v2 should force "proto=udp" but I figure that is what most people will expect.  Note, I can get the new Amd to mount a 2.2-STABLE box with "proto=tcp,vers=2".  RFC 1094 does not disallow such behavior.
    Former-commit-id: e9ab626e5a2c97b497529935cf9aa29b1e8cbea8

--- contrib/amd/amd/srvr_nfs.c
+++ contrib/amd/amd/srvr_nfs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997-1998 Erez Zadok  * Copyright (c) 1990 Jan-Simon Pendry  * Copyright (c) 1990 Imperial College of Science, Technology& Medicine  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry at Imperial College, London.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by the University of  *      California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      %W% (Berkeley) %G%  *  * $Id: srvr_nfs.c,v 5.2.2.1 1992/02/09 15:09:06 jsp beta $  *  */
+comment|/*  * Copyright (c) 1997-1998 Erez Zadok  * Copyright (c) 1990 Jan-Simon Pendry  * Copyright (c) 1990 Imperial College of Science, Technology& Medicine  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry at Imperial College, London.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by the University of  *      California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      %W% (Berkeley) %G%  *  * $Id: srvr_nfs.c,v 1.1.1.1 1998/08/23 22:07:20 obrien Exp $  *  */
 end_comment
 
 begin_comment
@@ -2320,6 +2320,54 @@ block|}
 endif|#
 directive|endif
 comment|/* MNTTAB_OPT_PROTO */
+comment|/* XXX: do we want to do this unconditionally of #define's? */
+comment|/* #if defined(MNTTAB_OPT_VERS )&& defined( MNTTAB_OPT_PROTO ) */
+block|{
+name|char
+modifier|*
+name|nfsv2_opt
+init|=
+name|hasmntopt
+argument_list|(
+operator|&
+name|mnt
+argument_list|,
+literal|"nfsv2"
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|nfsv2_opt
+condition|)
+block|{
+name|nfs_version
+operator|=
+operator|(
+name|unsigned
+name|long
+operator|)
+literal|2
+expr_stmt|;
+comment|/* nullify any ``vers=X'' statements */
+name|nfs_proto
+operator|=
+literal|"udp"
+expr_stmt|;
+comment|/* nullify any ``proto=tcp'' stmts */
+name|plog
+argument_list|(
+name|XLOG_WARNING
+argument_list|,
+literal|"found compatiblity option \"nfsv2\": set options"
+literal|" vers=2, proto=udp for host %s"
+argument_list|,
+name|host
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+comment|/*#endif*/
+comment|/* MNTTAB_OPT_VERS&& MNTTAB_OPT_PROTO */
 comment|/*    * lookup host address and canonical name    */
 name|hp
 operator|=
