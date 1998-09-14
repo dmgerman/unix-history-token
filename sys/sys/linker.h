f5fd52a16cccd6540c864801a5642b6250c6d702
Michael Smith <msmith@FreeBSD.org>
1998-09-14 18:31:19 +0000

    Add constants associated with preloaded kernel modules.
    Former-commit-id: 594f8b38abe934d0a42f8d5af0d0b2b1ed7bf65f

--- sys/sys/linker.h
+++ sys/sys/linker.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: linker.h,v 1.3 1998/01/01 08:55:37 bde Exp $  */
+comment|/*-  * Copyright (c) 1997 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: linker.h,v 1.4 1998/08/12 08:44:21 dfr Exp $  */
 end_comment
 
 begin_ifndef
@@ -510,6 +510,99 @@ name|deps
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_comment
+comment|/*  * Module information subtypes  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MODINFO_NAME
+value|0x0000
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFO_TYPE
+value|0x0001
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFO_ADDR
+value|0x0002
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFO_SIZE
+value|0x0003
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFO_METADATA
+value|0x8000
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFOMD_AOUTEXEC
+value|0x0001
+end_define
+
+begin_comment
+comment|/* a.out exec header */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MODINFOMD_ELFHDR
+value|0x0002
+end_define
+
+begin_comment
+comment|/* ELF header */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MODINFOMD_NOCOPY
+value|0x8000
+end_define
+
+begin_comment
+comment|/* don't copy this metadata to the kernel */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|KLD_IDENT_SYMNAME
+value|"kld_identifier_"
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFOMD_KLDIDENT
+value|(MODINFOMD_NOCOPY | 0x4000)
+end_define
+
+begin_define
+define|#
+directive|define
+name|MODINFOMD_KLDDEP
+value|(MODINFOMD_NOCOPY | 0x4001)
+end_define
 
 begin_ifdef
 ifdef|#
