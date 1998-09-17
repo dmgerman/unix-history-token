bc028a8f43129cd65dcf99d2490cceb6c72f6e5c
Matthew Jacob <mjacob@FreeBSD.org>
1998-09-17 21:06:38 +0000

    Clean up a comment. Make the default debugging level contingent upon CAMDEBUG.
    Former-commit-id: 0c783a339550b9d26218b31d03146b1add1a61dc

--- sys/dev/isp/isp_freebsd_cam.h
+++ sys/dev/isp/isp_freebsd_cam.h
@@ -4,10 +4,6 @@ comment|/* $FreeBSD$ */
 end_comment
 
 begin_comment
-comment|/* $Id: isp_freebsd_cam.h,v 1.3 1998/09/16 16:42:40 mjacob Exp $ */
-end_comment
-
-begin_comment
 comment|/*  * Qlogic ISP SCSI Host Adapter FreeBSD Wrapper Definitions (CAM version)  *---------------------------------------  * Copyright (c) 1997, 1998 by Matthew Jacob  * NASA/Ames Research Center  * All rights reserved.  *---------------------------------------  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
@@ -231,10 +227,6 @@ name|isp_name
 value|isp_osinfo.name
 end_define
 
-begin_comment
-comment|/*  * XXXX: UNTIL WE PUT CODE IN THAT CHECKS RETURNS FROM MALLOC  * XXXX: FOR CONTIGOUS PAGES, WE LIMIT TO PAGE_SIZE THE SIZE  * XXXX: OF MAILBOXES.  */
-end_comment
-
 begin_define
 define|#
 directive|define
@@ -286,12 +278,35 @@ parameter_list|)
 value|if (isp->isp_dblev>= lev) printf x
 end_define
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|CAMDEBUG
+end_ifdef
+
 begin_define
 define|#
 directive|define
 name|DFLT_DBLEVEL
 value|2
 end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|DFLT_DBLEVEL
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
