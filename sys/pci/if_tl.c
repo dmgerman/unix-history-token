8dca09ab2fc0a6a8a575c2d219ba8aa6c58f0ab9
Bill Paul <wpaul@FreeBSD.org>
1998-09-24 17:14:23 +0000

    Small tweak: force another reset of the adapter after probing for all the PHYs in tl_attach(). This is mainly to suck away any possible stray interrupts.
    This prevents an intermittent problem on some systems where the adapter
    probes correctly but yields a device timeout (and possible subsequent adapter
    check) when configured. When I originally tested the driver, I ifconfig'ed
    the interface after the system had already been booted and didn't notice
    any problems, but when configuring the interface immediately at startup,
    it would occasionally timeout and hang, until an adapter check interrupt
    came along and reset things again. I'm not exactly sure if this is a
    general problem of just something peculiar to this hardware (there are
    three devices, including the tlan, all on IRQ 11) but the extra reset
    shouldn't hurt anything. (It works fine with my 100Mbps Olicom adapter too.)
    
    Thanks to Mark Taylor from Cybernet (mtaylor@cybernet.com) for allowing
    me remote access to a Compaq system for debugging purposes.
    
    
    Former-commit-id: 5b787bf26f117dc23f79ae7817a6a3b37b859cc4

--- sys/pci/if_tl.c
+++ sys/pci/if_tl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_tl.c,v 1.28 1998/09/18 05:22:57 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_tl.c,v 1.30 1998/09/24 16:26:02 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -176,7 +176,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: if_tl.c,v 1.28 1998/09/18 05:22:57 wpaul Exp $"
+literal|"$Id: if_tl.c,v 1.30 1998/09/24 16:26:02 wpaul Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -7076,6 +7076,32 @@ goto|goto
 name|fail
 goto|;
 block|}
+name|tl_intvec_adchk
+argument_list|(
+operator|(
+name|void
+operator|*
+operator|)
+name|sc
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+name|tl_stop
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
+comment|/* 	 * Attempt to clear any stray interrupts 	 * that may be lurking. 	 */
+name|tl_intr
+argument_list|(
+operator|(
+name|void
+operator|*
+operator|)
+name|sc
+argument_list|)
+expr_stmt|;
 comment|/* 	 * Call MI attach routines. 	 */
 name|if_attach
 argument_list|(
@@ -8530,6 +8556,10 @@ name|sc
 operator|=
 name|xsc
 expr_stmt|;
+if|if
+condition|(
+name|type
+condition|)
 name|printf
 argument_list|(
 literal|"tl%d: adapter check: %x\n"
