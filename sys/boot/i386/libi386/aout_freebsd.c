b47554f8aeb773e4288cbf9f8305bb027507f8d4
Peter Wemm <peter@FreeBSD.org>
1998-09-29 09:11:49 +0000

    The bootinfo struct was getting clobbered or not passed through correctly. Presumably VTOP doesn't work for static objects. The easiest way to get it working was to reserve some space after the environment strings and copy the bootinfo struct there. Also, set RB_BOOTINFO, it's needed.
    I got the code to load and run an unmolested kernel OK for the first time
    with this system a few minutes ago - at last!.  I did have to stop it
    looking at the floppy though as BTX was trapping a mode 14 fault when
    it look for /boot/boot.conf when no disk was in the drive. (I'm booting
    from a scsi disk (bios disk 0x80)).
    
    Now to teach it about ELF and modules :-)
    
    
    Former-commit-id: 6491ba6b887c847b110ec1962f4764c0341e96cb

--- sys/boot/i386/libi386/aout_freebsd.c
+++ sys/boot/i386/libi386/aout_freebsd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: aout_freebsd.c,v 1.4 1998/09/17 23:52:07 msmith Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: aout_freebsd.c,v 1.5 1998/09/28 22:01:19 peter Exp $  */
 end_comment
 
 begin_include
@@ -441,15 +441,9 @@ index|[
 literal|5
 index|]
 operator|=
-operator|(
-name|u_int32_t
-operator|)
-name|VTOP
-argument_list|(
-operator|&
-name|bi
-argument_list|)
+literal|0
 expr_stmt|;
+comment|/* physical addr of bootinfo */
 comment|/* find the last module in the chain */
 for|for
 control|(
@@ -522,6 +516,37 @@ argument_list|(
 name|addr
 argument_list|)
 expr_stmt|;
+comment|/* pad to a 4-byte boundary */
+name|addr
+operator|=
+operator|(
+name|addr
+operator|+
+literal|0x3
+operator|)
+operator|&
+operator|~
+literal|0x3
+expr_stmt|;
+comment|/* leave space for bootinfo */
+name|argv
+index|[
+literal|5
+index|]
+operator|=
+operator|(
+name|u_int32_t
+operator|)
+name|addr
+expr_stmt|;
+name|addr
+operator|+=
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|bootinfo
+argument_list|)
+expr_stmt|;
 comment|/* pad to a page boundary */
 name|pad
 operator|=
@@ -571,6 +596,34 @@ name|bi_kernend
 operator|=
 name|addr
 expr_stmt|;
+comment|/* and insert bootinfo struct into reserved spot */
+name|i386_copyin
+argument_list|(
+operator|&
+name|bi
+argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
+name|argv
+index|[
+literal|5
+index|]
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|bi
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|argv
+index|[
+literal|0
+index|]
+operator||=
+name|RB_BOOTINFO
+expr_stmt|;
+comment|/* it's there now */
 name|entry
 operator|=
 name|ehdr
