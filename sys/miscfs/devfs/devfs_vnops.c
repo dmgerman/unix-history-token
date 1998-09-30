b155106607d013193af44a83d6178c1c517ac5d4
Soren Schmidt <sos@FreeBSD.org>
1998-09-30 20:33:46 +0000

    Make devfs update the atime timestamp so that 'w' works when using options DEVFS.
    Former-commit-id: 1b06687dfe4205bb98c633efcd92d15425117e47

--- sys/miscfs/devfs/devfs_vnops.c
+++ sys/miscfs/devfs/devfs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright 1997,1998 Julian Elischer.  All rights reserved.  * julian@freebsd.org  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met:  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright notice,  *     this list of conditions and the following disclaimer in the documentation  *     and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE HOLDER OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *   *	$Id: devfs_vnops.c,v 1.59 1998/08/16 01:21:51 bde Exp $  */
+comment|/*  * Copyright 1997,1998 Julian Elischer.  All rights reserved.  * julian@freebsd.org  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met:  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright notice,  *     this list of conditions and the following disclaimer in the documentation  *     and/or other materials provided with the distribution.  *   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE HOLDER OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *   *	$Id: devfs_vnops.c,v 1.60 1998/09/04 08:06:56 dfr Exp $  */
 end_comment
 
 begin_include
@@ -1366,6 +1366,31 @@ operator|->
 name|atime
 operator|)
 argument_list|)
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|file_node
+operator|->
+name|flags
+operator|&
+name|IN_ACCESS
+condition|)
+block|{
+name|nanotime
+argument_list|(
+operator|&
+name|file_node
+operator|->
+name|atime
+argument_list|)
+expr_stmt|;
+name|file_node
+operator|->
+name|flags
+operator|&=
+operator|~
+name|IN_ACCESS
 expr_stmt|;
 block|}
 name|vap
@@ -4949,11 +4974,7 @@ argument_list|,
 name|p
 argument_list|)
 expr_stmt|;
-return|return
-operator|(
-name|error
-operator|)
-return|;
+break|break;
 case|case
 name|VBLK
 case|:
@@ -5262,11 +5283,7 @@ operator|!=
 literal|0
 condition|)
 do|;
-return|return
-operator|(
-name|error
-operator|)
-return|;
+break|break;
 default|default:
 name|panic
 argument_list|(
@@ -5274,7 +5291,30 @@ literal|"devfs_read type"
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* NOTREACHED */
+if|if
+condition|(
+operator|!
+operator|(
+name|vp
+operator|->
+name|v_mount
+operator|->
+name|mnt_flag
+operator|&
+name|MNT_NOATIME
+operator|)
+condition|)
+name|dnp
+operator|->
+name|flags
+operator||=
+name|IN_ACCESS
+expr_stmt|;
+return|return
+operator|(
+name|error
+operator|)
+return|;
 block|}
 end_function
 
