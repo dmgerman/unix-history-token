3dd5b074554c4a7d09ac06cbf5f5d6f3c474cd53
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-09-30 13:36:53 +0000

    Simplify gunzip usage even more (doh!).
    Former-commit-id: 4c3f23b5bfb5a9195db8fbe40544f5fcc92962fc

--- usr.sbin/sysinstall/system.c
+++ usr.sbin/sysinstall/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.82 1997/07/31 11:08:45 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.83 1998/09/30 11:44:29 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
 end_comment
 
 begin_include
@@ -158,7 +158,7 @@ argument_list|)
 operator|||
 name|vsystem
 argument_list|(
-literal|"%s -c %s> %s"
+literal|"%s< %s> %s"
 argument_list|,
 name|gunzip
 argument_list|,
