fc139e7fabaa2b9d0f372d7500b64005c5ef49ee
David E. O'Brien <obrien@FreeBSD.org>
1998-10-05 11:28:27 +0000

    Now require *FS_ROOT to enable the ability to mount a *FS /. Previously one could config(8) a kernel that would not link.
    Former-commit-id: 7479663b270423117a685adbcf8f632fd4faed91

--- sys/i386/i386/autoconf.c
+++ sys/i386/i386/autoconf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.105 1998/09/14 19:56:38 sos Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)autoconf.c	7.1 (Berkeley) 5/9/91  *	$Id: autoconf.c,v 1.106 1998/09/15 10:03:42 gibbs Exp $  */
 end_comment
 
 begin_comment
@@ -285,6 +285,84 @@ directive|include
 file|<sys/bus.h>
 end_include
 
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|FFS_ROOT
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|FFS
+argument_list|)
+end_if
+
+begin_error
+error|#
+directive|error
+error|``options FFS_ROOT'' requires ``options FFS''
+end_error
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|NFS_ROOT
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|NFS
+argument_list|)
+end_if
+
+begin_error
+error|#
+directive|error
+error|``options NFS_ROOT'' requires ``options NFS''
+end_error
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|CD9660_ROOT
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|CD9660
+argument_list|)
+end_if
+
+begin_error
+error|#
+directive|error
+error|``options CD9660_ROOT'' requires ``options CD9660''
+end_error
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
 name|void
@@ -367,11 +445,14 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|CD9660
-end_ifdef
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|CD9660_ROOT
+argument_list|)
+end_if
 
 begin_include
 include|#
@@ -677,7 +758,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* CD9660 */
+comment|/* CD9660_ROOT */
 end_comment
 
 begin_decl_stmt
@@ -984,11 +1065,6 @@ if|#
 directive|if
 name|defined
 argument_list|(
-name|CD9660
-argument_list|)
-operator|||
-name|defined
-argument_list|(
 name|CD9660_ROOT
 argument_list|)
 if|if
@@ -1110,11 +1186,6 @@ if|#
 directive|if
 name|defined
 argument_list|(
-name|NFS
-argument_list|)
-operator|||
-name|defined
-argument_list|(
 name|NFS_ROOT
 argument_list|)
 if|if
@@ -1144,11 +1215,6 @@ directive|endif
 comment|/* NFS */
 if|#
 directive|if
-name|defined
-argument_list|(
-name|FFS
-argument_list|)
-operator|||
 name|defined
 argument_list|(
 name|FFS_ROOT
