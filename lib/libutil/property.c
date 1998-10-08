7f6688e985f2f4637391d1cc7f01a0d52f8073c9
Jordan K. Hubbard <jkh@FreeBSD.org>
1998-10-08 01:56:49 +0000

    Correct a build error that got past my build test somehow.
    Former-commit-id: a78df5a9127f0abb748dab4ee988a6632b8e2ad8

--- lib/libutil/property.c
+++ lib/libutil/property.c
@@ -12,6 +12,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<stdio.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<stdlib.h>
 end_include
 
@@ -19,6 +25,12 @@ begin_include
 include|#
 directive|include
 file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
 end_include
 
 begin_include
