09b9c39ab5b5c9ef5e7cbd654e34f3cf82b3b3d2
Peter Wemm <peter@FreeBSD.org>
1998-10-09 23:32:03 +0000

    At the moment, the alpha tentatively uses the ddb elf code rather than KLD, because the alpha boot loader hasn't been converted yet, and because it needs the full symbol tables with local symbols in order to make sense of stack tracebacks.  KLD will implement this (using full sybmol table rather than the globals only) shortly.
    Former-commit-id: 676f47d1d8b5533aab50ff74cc08d5674bcf6584

--- sys/ddb/db_elf.c
+++ sys/ddb/db_elf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $Id: db_elf.c,v 1.2 1998/06/28 00:59:26 dfr Exp $ */
+comment|/* $Id: db_elf.c,v 1.3 1998/08/17 08:06:31 dfr Exp $ */
 end_comment
 
 begin_comment
@@ -10,6 +10,20 @@ end_comment
 begin_comment
 comment|/*-  * Copyright (c) 1997 The NetBSD Foundation, Inc.  * All rights reserved.  *  * This code is derived from software contributed to The NetBSD Foundation  * by Jason R. Thorpe of the Numerical Aerospace Simulation Facility,  * NASA Ames Research Center.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the NetBSD  *	Foundation, Inc. and its contributors.  * 4. Neither the name of The NetBSD Foundation nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__ELF__
+argument_list|)
+operator|&&
+name|defined
+argument_list|(
+name|__alpha__
+argument_list|)
+end_if
 
 begin_include
 include|#
@@ -58,36 +72,6 @@ include|#
 directive|include
 file|<ddb/db_output.h>
 end_include
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|DB_ELF_SYMBOLS
-end_ifdef
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|DB_ELFSIZE
-end_ifndef
-
-begin_error
-error|#
-directive|error
-error|Must define DB_ELFSIZE!
-end_error
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_define
-define|#
-directive|define
-name|ELFSIZE
-value|DB_ELFSIZE
-end_define
 
 begin_include
 include|#
@@ -1411,10 +1395,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* DB_ELF_SYMBOLS */
-end_comment
 
 end_unit
 
