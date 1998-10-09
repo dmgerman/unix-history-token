0009b12f1764dfe28e620a6581300cb126804c08
Peter Wemm <peter@FreeBSD.org>
1998-10-09 23:24:55 +0000

    Attempt to at least align MODINFO_* blocks.  IMHO, decoding this stuff should be MD code since one day we'll have to recover pages from deleted preload data.  MI code can't be expected to know how to deal with pmap internals, assuming it gets done via pmap that is. :-)
    Former-commit-id: 92c6bd2e7777a2cff0d5b55bef82ba3267f5858c

--- sys/boot/i386/libi386/bootinfo.c
+++ sys/boot/i386/libi386/bootinfo.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: bootinfo.c,v 1.10 1998/10/07 10:55:46 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: bootinfo.c,v 1.11 1998/10/09 07:11:19 msmith Exp $  */
 end_comment
 
 begin_include
@@ -19,6 +19,12 @@ begin_include
 include|#
 directive|include
 file|<sys/reboot.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/linker.h>
 end_include
 
 begin_include
@@ -507,7 +513,7 @@ name|a
 parameter_list|,
 name|s
 parameter_list|)
-value|{			\     COPY32(t, a);				\     COPY32(strlen(s) + 1, a);			\     i386_copyin(s, a, strlen(s) + 1);		\     a += strlen(s) + 1;				\ }
+value|{			\     COPY32(t, a);				\     COPY32(strlen(s) + 1, a);			\     i386_copyin(s, a, strlen(s) + 1);		\     a += roundup(strlen(s) + 1, sizeof(u_int32_t));\ }
 end_define
 
 begin_define
@@ -545,7 +551,7 @@ name|a
 parameter_list|,
 name|s
 parameter_list|)
-value|{			\     COPY32(t, a);				\     COPY32(sizeof(s), a);			\     i386_copyin(&s, a, sizeof(s));		\     a += sizeof(s);				\ }
+value|{			\     COPY32(t, a);				\     COPY32(sizeof(s), a);			\     i386_copyin(&s, a, sizeof(s));		\     a += roundup(sizeof(s), sizeof(u_int32_t));	\ }
 end_define
 
 begin_define
@@ -581,7 +587,7 @@ name|a
 parameter_list|,
 name|mm
 parameter_list|)
-value|{			\     COPY32(MODINFO_METADATA | mm->md_type, a);	\     COPY32(mm->md_size, a);			\     i386_copyin(mm->md_data, a, mm->md_size);	\     a += mm->md_size;				\ }
+value|{			\     COPY32(MODINFO_METADATA | mm->md_type, a);	\     COPY32(mm->md_size, a);			\     i386_copyin(mm->md_data, a, mm->md_size);	\     a += roundup(mm->md_size, sizeof(u_int32_t));\ }
 end_define
 
 begin_define
