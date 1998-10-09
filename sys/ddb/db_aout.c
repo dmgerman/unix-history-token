4e512aa170e4475310e636c83a0ab0056a07b461
Peter Wemm <peter@FreeBSD.org>
1998-10-09 23:29:44 +0000

    We don't compile this on an elf kernel (and explicitly not on alpha in case it's possible to compile in something like ECOFF) The three db_xxx.c symbol interfaces are "standard" because config isn't flexible enough without forcing the user to know about it.
    Former-commit-id: 80f7bdf44cba72ff9228c1bed017a06f773dd061

--- sys/ddb/db_aout.c
+++ sys/ddb/db_aout.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_aout.c,v 1.20 1998/06/07 17:09:36 dfr Exp $  */
+comment|/*  * Mach Operating System  * Copyright (c) 1991,1990 Carnegie Mellon University  * All Rights Reserved.  *  * Permission to use, copy, modify and distribute this software and its  * documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  *	$Id: db_aout.c,v 1.21 1998/06/28 00:55:02 dfr Exp $  */
 end_comment
 
 begin_comment
@@ -10,6 +10,22 @@ end_comment
 begin_comment
 comment|/*  * Symbol table routines for a.out format files.  */
 end_comment
+
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|__ELF__
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|__alpha__
+argument_list|)
+end_if
 
 begin_include
 include|#
@@ -40,12 +56,6 @@ include|#
 directive|include
 file|<ddb/db_sym.h>
 end_include
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|DB_NO_AOUT
-end_ifndef
 
 begin_define
 define|#
@@ -1437,10 +1447,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* DB_NO_AOUT */
-end_comment
 
 end_unit
 
