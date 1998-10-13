6b29264e2764fb7061ee51d3364e5a79cb83ca24
Ken Merry <ken@FreeBSD.org>
1998-10-13 21:41:32 +0000

    Fix a bug in the error recovery code.  It was possible to have more than one error recovery action oustanding for a given peripheral.
    This is bad for several reasons.  The first problem is that the error
    recovery actions would likely be to fix the same problem.  (e.g., we
    queue 5 CCBs to a disk, and the first one comes back with 0x04,0x02.  We
    start error recovery, and the second one comes back with the same status.
    Then the third one comes back, and so on.  Each one causes the drive to get
    nailed with a start unit, when we really only need one.)
    
    The other problem is that we only have space to store one CCB while we're
    doing error recovery.  The subsequent error recovery actions that got
    started were over-writing the CCBs from previous error recovery actions,
    but we still tried to call the done routine N times for N error recovery
    actions.  Each call to dadone() was done with the same CCB, though.  So on
    the second one, we got a "biodone: buffer not busy" panic, since the buffer
    in question had already been through biodone().
    
    In any case, this fixes things so that any any given time, there's only one
    error recovery action outstanding for any given peripheral driver.
    
    Reviewed by:	gibbs
    Reported by:	Philippe Regnauld <regnauld@deepo.prosa.dk>
    [ Philippe wins the "bug finder of the week" award ]
    
    
    Former-commit-id: 2261f570e51b79848d77c75549e2d1d915aaaaff

--- sys/cam/cam_periph.c
+++ sys/cam/cam_periph.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Common functions for CAM "type" (peripheral) drivers.  *  * Copyright (c) 1997, 1998 Justin T. Gibbs.  * Copyright (c) 1997, 1998 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: cam_periph.c,v 1.2 1998/09/20 07:14:36 gibbs Exp $  */
+comment|/*  * Common functions for CAM "type" (peripheral) drivers.  *  * Copyright (c) 1997, 1998 Justin T. Gibbs.  * Copyright (c) 1997, 1998 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: cam_periph.c,v 1.3 1998/09/29 09:18:08 bde Exp $  */
 end_comment
 
 begin_include
@@ -3897,6 +3897,13 @@ name|ccb
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|periph
+operator|->
+name|flags
+operator|&=
+operator|~
+name|CAM_PERIPH_RECOVERY_INPROG
+expr_stmt|;
 name|xpt_action
 argument_list|(
 name|done_ccb
@@ -4078,6 +4085,13 @@ name|ccb
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|periph
+operator|->
+name|flags
+operator|&=
+operator|~
+name|CAM_PERIPH_RECOVERY_INPROG
+expr_stmt|;
 name|xpt_action
 argument_list|(
 name|done_ccb
@@ -4105,6 +4119,13 @@ name|ccb
 argument_list|)
 argument_list|)
 expr_stmt|;
+name|periph
+operator|->
+name|flags
+operator|&=
+operator|~
+name|CAM_PERIPH_RECOVERY_INPROG
+expr_stmt|;
 name|xpt_action
 argument_list|(
 name|done_ccb
@@ -4129,6 +4150,13 @@ expr|union
 name|ccb
 argument_list|)
 argument_list|)
+expr_stmt|;
+name|periph
+operator|->
+name|flags
+operator|&=
+operator|~
+name|CAM_PERIPH_RECOVERY_INPROG
 expr_stmt|;
 name|xpt_action
 argument_list|(
@@ -4483,6 +4511,28 @@ operator|>
 literal|0
 condition|)
 block|{
+comment|/* 					 * Since error recovery is already 					 * in progress, don't attempt to 					 * process this error.  It is probably 					 * related to the error that caused 					 * the currently active error recovery 					 * action.  Also, we only have 					 * space for one saved CCB, so if we 					 * had two concurrent error recovery 					 * actions, we would end up 					 * over-writing one error recovery 					 * CCB with another one. 					 */
+if|if
+condition|(
+name|periph
+operator|->
+name|flags
+operator|&
+name|CAM_PERIPH_RECOVERY_INPROG
+condition|)
+block|{
+name|error
+operator|=
+name|ERESTART
+expr_stmt|;
+break|break;
+block|}
+name|periph
+operator|->
+name|flags
+operator||=
+name|CAM_PERIPH_RECOVERY_INPROG
+expr_stmt|;
 comment|/* decrement the number of retries */
 if|if
 condition|(
@@ -4644,6 +4694,28 @@ block|{
 name|int
 name|le
 decl_stmt|;
+comment|/* 					 * Only one error recovery action 					 * at a time.  See above. 					 */
+if|if
+condition|(
+name|periph
+operator|->
+name|flags
+operator|&
+name|CAM_PERIPH_RECOVERY_INPROG
+condition|)
+block|{
+name|error
+operator|=
+name|ERESTART
+expr_stmt|;
+break|break;
+block|}
+name|periph
+operator|->
+name|flags
+operator||=
+name|CAM_PERIPH_RECOVERY_INPROG
+expr_stmt|;
 comment|/* decrement the number of retries */
 name|retry
 operator|=
@@ -4816,6 +4888,28 @@ block|{
 name|int
 name|le
 decl_stmt|;
+comment|/* 					 * Only one error recovery action 					 * at a time.  See above. 					 */
+if|if
+condition|(
+name|periph
+operator|->
+name|flags
+operator|&
+name|CAM_PERIPH_RECOVERY_INPROG
+condition|)
+block|{
+name|error
+operator|=
+name|ERESTART
+expr_stmt|;
+break|break;
+block|}
+name|periph
+operator|->
+name|flags
+operator||=
+name|CAM_PERIPH_RECOVERY_INPROG
+expr_stmt|;
 comment|/* decrement the number of retries */
 name|retry
 operator|=
