516a9e04471b1ac4ebaa419e9727e4eea99482fd
Peter Wemm <peter@FreeBSD.org>
1998-10-13 09:25:27 +0000

    Make the ELF load messages cleaner.
    Former-commit-id: e8f75e659ec7fd368337c556be40ef76145710df

--- sys/boot/common/load_elf.c
+++ sys/boot/common/load_elf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: load_elf.c,v 1.3 1998/10/09 23:18:43 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: load_elf.c,v 1.4 1998/10/12 09:13:50 peter Exp $  */
 end_comment
 
 begin_include
@@ -565,6 +565,9 @@ else|:
 name|elf_moduletype
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|ELF_VERBOSE
 if|if
 condition|(
 name|kernel
@@ -582,6 +585,8 @@ operator|)
 name|dest
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 name|mp
 operator|->
 name|m_size
@@ -1024,9 +1029,10 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
 name|printf
 argument_list|(
-literal|" text=0x%lx"
+literal|"text=0x%lx "
 argument_list|,
 operator|(
 name|long
@@ -1039,10 +1045,12 @@ operator|.
 name|p_filesz
 argument_list|)
 expr_stmt|;
+block|}
 else|else
+block|{
 name|printf
 argument_list|(
-literal|" data=0x%lx"
+literal|"data=0x%lx"
 argument_list|,
 operator|(
 name|long
@@ -1055,6 +1063,52 @@ operator|.
 name|p_filesz
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|phdr
+index|[
+name|i
+index|]
+operator|.
+name|p_filesz
+operator|<
+name|phdr
+index|[
+name|i
+index|]
+operator|.
+name|p_memsz
+condition|)
+name|printf
+argument_list|(
+literal|"+0x%lx"
+argument_list|,
+call|(
+name|long
+call|)
+argument_list|(
+name|phdr
+index|[
+name|i
+index|]
+operator|.
+name|p_memsz
+operator|-
+name|phdr
+index|[
+name|i
+index|]
+operator|.
+name|p_filesz
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|printf
+argument_list|(
+literal|" "
+argument_list|)
+expr_stmt|;
+block|}
 endif|#
 directive|endif
 if|if
@@ -1195,32 +1249,6 @@ operator|.
 name|p_memsz
 operator|-
 literal|1
-argument_list|)
-argument_list|)
-expr_stmt|;
-else|#
-directive|else
-name|printf
-argument_list|(
-literal|" bss=0x%lx"
-argument_list|,
-call|(
-name|long
-call|)
-argument_list|(
-name|phdr
-index|[
-name|i
-index|]
-operator|.
-name|p_memsz
-operator|-
-name|phdr
-index|[
-name|i
-index|]
-operator|.
-name|p_filesz
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1425,6 +1453,21 @@ name|ehdr
 operator|->
 name|e_shentsize
 expr_stmt|;
+if|if
+condition|(
+name|chunk
+operator|==
+literal|0
+operator|||
+name|ehdr
+operator|->
+name|e_shoff
+operator|==
+literal|0
+condition|)
+goto|goto
+name|nosyms
+goto|;
 name|shdr
 operator|=
 name|malloc
@@ -1680,7 +1723,7 @@ directive|ifndef
 name|ELF_VERBOSE
 name|printf
 argument_list|(
-literal|" symbols=["
+literal|"syms=["
 argument_list|)
 expr_stmt|;
 endif|#
@@ -1816,6 +1859,17 @@ argument_list|)
 expr_stmt|;
 else|#
 directive|else
+if|if
+condition|(
+name|i
+operator|==
+name|symstrindex
+condition|)
+name|printf
+argument_list|(
+literal|"+"
+argument_list|)
+expr_stmt|;
 name|printf
 argument_list|(
 literal|"0x%x+0x%lx"
