7a81b6053aeac781f35053ef6ef9a3043d5b260a
Ken Merry <ken@FreeBSD.org>
1998-10-14 23:28:26 +0000

    Fix a couple of potential buffer overrun cases.
    Submitted by:	imp
    
    
    Former-commit-id: c7ee2a054dd3b8ac32098b4d9ea6f8ec413903c0

--- lib/libdevstat/devstat.c
+++ lib/libdevstat/devstat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: devstat.c,v 1.2 1998/09/18 02:35:25 ken Exp $  */
+comment|/*  * Copyright (c) 1997, 1998 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: devstat.c,v 1.3 1998/09/20 00:11:09 ken Exp $  */
 end_comment
 
 begin_include
@@ -655,6 +655,7 @@ name|errlen
 expr_stmt|;
 block|}
 else|else
+block|{
 name|strncpy
 argument_list|(
 name|devstat_errbuf
@@ -664,6 +665,16 @@ argument_list|,
 name|DEVSTAT_ERRBUF_SIZE
 argument_list|)
 expr_stmt|;
+name|devstat_errbuf
+index|[
+name|DEVSTAT_ERRBUF_SIZE
+operator|-
+literal|1
+index|]
+operator|=
+literal|'\0'
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|version
@@ -1616,6 +1627,23 @@ index|[
 name|i
 index|]
 operator|.
+name|device_name
+index|[
+name|DEVSTAT_NAME_LEN
+operator|-
+literal|1
+index|]
+operator|=
+literal|'\0'
+expr_stmt|;
+operator|(
+operator|*
+name|dev_select
+operator|)
+index|[
+name|i
+index|]
+operator|.
 name|unit_number
 operator|=
 name|devices
@@ -1734,9 +1762,14 @@ index|[
 literal|80
 index|]
 decl_stmt|;
-name|sprintf
+name|snprintf
 argument_list|(
 name|tmpstr
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|tmpstr
+argument_list|)
 argument_list|,
 literal|"%s%d"
 argument_list|,
@@ -3444,9 +3477,14 @@ literal|1
 operator|)
 condition|)
 block|{
-name|sprintf
+name|snprintf
 argument_list|(
 name|devstat_errbuf
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|devstat_errbuf
+argument_list|)
 argument_list|,
 literal|"%s: unknown match item \"%s\""
 argument_list|,
