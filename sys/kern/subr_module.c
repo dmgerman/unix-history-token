992344cbf050e91c64c30c66cc9f0a4ad2723025
Peter Wemm <peter@FreeBSD.org>
1998-10-14 05:07:23 +0000

    Align to sizeof(long) rather than sizeof(int32_t).  It needs to be long because this code is shared with the alpha.  I hope the alpha can read 32 bit ints at 32 bit alignment (vs. 64 bit alignment).
    Former-commit-id: 0bb615eefbe30a7c87756f018378dfa9cf2d1fc0

--- sys/kern/subr_module.c
+++ sys/kern/subr_module.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: subr_module.c,v 1.2 1998/10/09 23:59:01 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: subr_module.c,v 1.3 1998/10/12 09:03:48 peter Exp $  */
 end_comment
 
 begin_include
@@ -158,7 +158,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -317,7 +317,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -405,7 +405,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -486,7 +486,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -646,7 +646,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -814,7 +814,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -967,7 +967,7 @@ name|next
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|u_int32_t
+name|u_long
 argument_list|)
 argument_list|)
 expr_stmt|;
