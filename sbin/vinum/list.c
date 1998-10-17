678228e3c1bfec4b1ff1befe2068ef04df1ac076
Bruce Evans <bde@FreeBSD.org>
1998-10-17 13:02:46 +0000

    Don't assume that time_t is long.
    Former-commit-id: bd79ffc5d8e1d865e6fc2e93be0ef61916903ee6

--- sbin/vinum/list.c
+++ sbin/vinum/list.c
@@ -4,7 +4,7 @@ comment|/*      list.c: vinum interface program, list routines  */
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *    * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: list.c,v 1.12 1998/08/10 05:15:06 grog Exp grog $  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *    * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: list.c,v 1.1.1.1 1998/09/16 05:57:36 grog Exp $  */
 end_comment
 
 begin_include
@@ -392,6 +392,9 @@ name|int
 name|recurse
 parameter_list|)
 block|{
+name|time_t
+name|t
+decl_stmt|;
 name|get_drive_info
 argument_list|(
 operator|&
@@ -429,6 +432,16 @@ operator|.
 name|devicename
 argument_list|)
 expr_stmt|;
+name|t
+operator|=
+name|drive
+operator|.
+name|label
+operator|.
+name|date_of_birth
+operator|.
+name|tv_sec
+expr_stmt|;
 name|printf
 argument_list|(
 literal|"\t\tCreated on %s at %s"
@@ -442,15 +455,19 @@ argument_list|,
 name|ctime
 argument_list|(
 operator|&
+name|t
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|t
+operator|=
 name|drive
 operator|.
 name|label
 operator|.
-name|date_of_birth
+name|last_update
 operator|.
 name|tv_sec
-argument_list|)
-argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
@@ -460,13 +477,7 @@ comment|/* care: \n at end */
 name|ctime
 argument_list|(
 operator|&
-name|drive
-operator|.
-name|label
-operator|.
-name|last_update
-operator|.
-name|tv_sec
+name|t
 argument_list|)
 argument_list|)
 expr_stmt|;
