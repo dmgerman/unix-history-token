ea6d03f3ae1f26a50f2cc19bd31cb48085611965
Peter Hawkins <thepish@FreeBSD.org>
1998-10-18 04:46:24 +0000

    PR:		7583 Submitted by:	Dave Glowacki <dglo@SSEC.WISC.EDU> Support input via stdin if no input file name specified explicitly
    Former-commit-id: 9a48e7e27be22ee8fb79d12f0feaf2017a74ceb1

--- usr.bin/indent/indent.c
+++ usr.bin/indent/indent.c
@@ -54,7 +54,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id$"
+literal|"$Id: indent.c,v 1.2 1997/07/15 09:50:59 charnier Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -866,8 +866,9 @@ operator|==
 literal|0
 condition|)
 block|{
-name|usage
-argument_list|()
+name|input
+operator|=
+name|stdin
 expr_stmt|;
 block|}
 if|if
@@ -879,6 +880,10 @@ condition|)
 if|if
 condition|(
 name|troff
+operator|||
+name|input
+operator|==
+name|stdin
 condition|)
 name|output
 operator|=
@@ -5256,7 +5261,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: indent file [ outfile ] [ options ]\n"
+literal|"usage: indent [ file [ outfile ] ] [ options ]\n"
 argument_list|)
 expr_stmt|;
 name|exit
