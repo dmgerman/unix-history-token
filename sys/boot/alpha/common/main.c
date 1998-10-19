4bd1289f9f1c21fdae5191be71e7c8bd2c8462d1
Doug Rabson <dfr@FreeBSD.org>
1998-10-19 09:12:41 +0000

    Reclaim the 48k used by the first bootstrap stage by adding it to the malloc pool.
    Former-commit-id: 2f3b0c4a5a4b15cba9c998dda5f585c5d6fd1aa1

--- sys/boot/alpha/common/main.c
+++ sys/boot/alpha/common/main.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998 Doug Rabson<dfr@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: main.c,v 1.4 1998/09/03 02:10:02 msmith Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998 Doug Rabson<dfr@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: main.c,v 1.5 1998/09/26 10:51:36 dfr Exp $  */
 end_comment
 
 begin_include
@@ -226,7 +226,7 @@ index|[
 literal|128
 index|]
 decl_stmt|;
-comment|/*       * Initialise the heap as early as possible.  Once this is done, alloc() is usable.      * The stack is buried inside us, so this is safe       */
+comment|/*       * Initialise the heap as early as possible.  Once this is done,      * alloc() is usable. The stack is buried inside us, so this is      * safe.      */
 name|setheap
 argument_list|(
 operator|(
@@ -242,7 +242,28 @@ operator|)
 literal|0x20040000
 argument_list|)
 expr_stmt|;
-comment|/*       * XXX Chicken-and-egg problem; we want to have console output early, but some      * console attributes may depend on reading from eg. the boot device, which we      * can't do yet.      *      * We can use printf() etc. once this is done.      */
+ifdef|#
+directive|ifdef
+name|LOADER
+comment|/*      * If this is the two stage disk loader, add the memory used by      * the first stage to the heap.      */
+name|free_region
+argument_list|(
+operator|(
+name|void
+operator|*
+operator|)
+name|PRIMARY_LOAD_ADDRESS
+argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
+name|SECONDARY_LOAD_ADDRESS
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
+comment|/*       * XXX Chicken-and-egg problem; we want to have console output      * early, but some console attributes may depend on reading from      * eg. the boot device, which we can't do yet.  We can use      * printf() etc. once this is done.      */
 name|cons_probe
 argument_list|()
 expr_stmt|;
