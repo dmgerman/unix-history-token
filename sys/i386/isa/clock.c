164f234fce18a717449820799765d197647bd883
Poul-Henning Kamp <phk@FreeBSD.org>
1998-10-23 10:46:20 +0000

    Update timecounters to new interface.
    Former-commit-id: 030ff26060e9a0cafd44bdfce67edb5c143cf6d0

--- sys/i386/isa/clock.c
+++ sys/i386/isa/clock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.126 1998/09/20 03:47:54 bde Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz and Don Ahn.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)clock.c	7.2 (Berkeley) 5/12/91  *	$Id: clock.c,v 1.127 1998/09/20 19:56:28 bde Exp $  */
 end_comment
 
 begin_comment
@@ -721,9 +721,6 @@ specifier|static
 name|struct
 name|timecounter
 name|tsc_timecounter
-index|[
-literal|3
-index|]
 init|=
 block|{
 name|tsc_get_timecount
@@ -756,6 +753,7 @@ name|tsc_timecounter
 argument_list|,
 name|CTLFLAG_RD
 argument_list|,
+operator|&
 name|tsc_timecounter
 argument_list|,
 sizeof|sizeof
@@ -775,9 +773,6 @@ specifier|static
 name|struct
 name|timecounter
 name|i8254_timecounter
-index|[
-literal|3
-index|]
 init|=
 block|{
 name|i8254_get_timecount
@@ -810,6 +805,7 @@ name|i8254_timecounter
 argument_list|,
 name|CTLFLAG_RD
 argument_list|,
+operator|&
 name|i8254_timecounter
 argument_list|,
 sizeof|sizeof
@@ -2669,9 +2665,6 @@ name|hz
 argument_list|)
 expr_stmt|;
 name|i8254_timecounter
-index|[
-literal|0
-index|]
 operator|.
 name|tc_frequency
 operator|=
@@ -2679,6 +2672,7 @@ name|timer_freq
 expr_stmt|;
 name|init_timecounter
 argument_list|(
+operator|&
 name|i8254_timecounter
 argument_list|)
 expr_stmt|;
@@ -2787,9 +2781,6 @@ literal|0
 condition|)
 block|{
 name|tsc_timecounter
-index|[
-literal|0
-index|]
 operator|.
 name|tc_frequency
 operator|=
@@ -2797,6 +2788,7 @@ name|tsc_freq
 expr_stmt|;
 name|init_timecounter
 argument_list|(
+operator|&
 name|tsc_timecounter
 argument_list|)
 expr_stmt|;
@@ -4079,9 +4071,6 @@ name|hz
 argument_list|)
 expr_stmt|;
 name|i8254_timecounter
-index|[
-literal|0
-index|]
 operator|.
 name|tc_frequency
 operator|=
@@ -4184,9 +4173,6 @@ operator|=
 name|freq
 expr_stmt|;
 name|tsc_timecounter
-index|[
-literal|0
-index|]
 operator|.
 name|tc_frequency
 operator|=
