e7d51e8d41adca5c1306e077ef609524604bbbc8
Bruce Evans <bde@FreeBSD.org>
1998-10-25 19:24:04 +0000

    Don't follow null bdevsw pointers.  The `major(dev) < nblkdev' test rotted when bdevsw[] became sparse.  We still depend on magic to avoid having to check that (v_rdev) device numbers in vnodes are not NODEV.
    Removed a redundant `major(dev) < nblkdev' test instead of updating it.
    
    Don't follow a garbage bdevsw pointer for attempts to swap on empty
    regular files.  This case currently can't happen.  Swapping on regular
    files is ifdefed out in swapon() and isn't attempted for empty files
    in nfs_mountroot().
    
    
    Former-commit-id: cd3585cce7afe599e267caab082db0684a3a5239

--- sys/vm/vm_swap.c
+++ sys/vm/vm_swap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vm_swap.c	8.5 (Berkeley) 2/17/94  * $Id: vm_swap.c,v 1.55 1998/07/04 20:45:42 julian Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vm_swap.c	8.5 (Berkeley) 2/17/94  * $Id: vm_swap.c,v 1.56 1998/07/04 22:30:26 julian Exp $  */
 end_comment
 
 begin_include
@@ -857,9 +857,6 @@ name|VBLK
 case|:
 name|dev
 operator|=
-operator|(
-name|dev_t
-operator|)
 name|vp
 operator|->
 name|v_rdev
@@ -872,6 +869,16 @@ name|dev
 argument_list|)
 operator|>=
 name|nblkdev
+operator|||
+name|bdevsw
+index|[
+name|major
+argument_list|(
+name|dev
+argument_list|)
+index|]
+operator|==
+name|NULL
 condition|)
 block|{
 name|error
@@ -1037,26 +1044,6 @@ name|EINVAL
 return|;
 name|found
 label|:
-if|if
-condition|(
-name|dev
-operator|!=
-name|NODEV
-operator|&&
-operator|(
-name|major
-argument_list|(
-name|dev
-argument_list|)
-operator|>=
-name|nblkdev
-operator|)
-condition|)
-return|return
-operator|(
-name|ENXIO
-operator|)
-return|;
 name|error
 operator|=
 name|VOP_OPEN
@@ -1088,6 +1075,10 @@ condition|(
 name|nblks
 operator|==
 literal|0
+operator|&&
+name|dev
+operator|!=
+name|NODEV
 operator|&&
 operator|(
 name|bdevsw
