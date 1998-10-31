df0318601df9b558e32a9688147eba1de80984f6
Michael Smith <msmith@FreeBSD.org>
1998-10-31 02:53:12 +0000

    - Add a new command 'lsdev' to list devices which might be likely to host    filesystems.  - New 'help' command and data in the help.* files (not yet installed),    provides topic and subtopic help, indexes, etc.  - Don't crash if the user tries to set an invalid console.  Be helpful    instead.  - Expand tabs (badly) on the i386 video console.  - Some minor cosmetic changes.
    Former-commit-id: 4f204d4a90199f7c2c2e2b05750a6f4cf6aa191a

--- sys/boot/common/boot.c
+++ sys/boot/common/boot.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: boot.c,v 1.6 1998/10/11 10:10:41 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: boot.c,v 1.7 1998/10/14 00:41:17 peter Exp $  */
 end_comment
 
 begin_comment
@@ -758,7 +758,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"\rBooting [%s] in %d seconds..."
+literal|"\rBooting [%s] in %d seconds... "
 argument_list|,
 name|getbootfile
 argument_list|(
@@ -785,6 +785,20 @@ literal|1
 expr_stmt|;
 block|}
 block|}
+if|if
+condition|(
+name|yes
+condition|)
+name|printf
+argument_list|(
+literal|"\rBooting [%s]...               "
+argument_list|,
+name|getbootfile
+argument_list|(
+literal|0
+argument_list|)
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|cr
@@ -943,7 +957,7 @@ name|strchr
 argument_list|(
 name|spec
 argument_list|,
-literal|','
+literal|';'
 argument_list|)
 operator|)
 operator|!=
