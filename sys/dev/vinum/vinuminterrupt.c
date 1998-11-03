bf5e760a42e80fc711d353c1d839bfab695dfeda
Greg Lehey <grog@FreeBSD.org>
1998-11-03 06:37:57 +0000

    Take drive down if the disk driver tells us it's not there
    Former-commit-id: b2f7aced84c7c2145c07ea475d2d16bae575aa0e

--- sys/dev/vinum/vinuminterrupt.c
+++ sys/dev/vinum/vinuminterrupt.c
@@ -4,7 +4,7 @@ comment|/* interrupt.c: bottom half of the driver */
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: interrupt.c,v 1.1 1998/08/13 06:12:27 grog Exp grog $  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: interrupt.c,v 1.2 1998/10/30 06:39:36 grog Exp grog $  */
 end_comment
 
 begin_define
@@ -256,6 +256,30 @@ name|setstate_noupdate
 argument_list|)
 expr_stmt|;
 comment|/* take the subdisk down */
+elseif|else
+if|if
+condition|(
+name|rq
+operator|->
+name|error
+operator|=
+name|ENXIO
+condition|)
+comment|/* or "not configured" (drive down) */
+name|set_drive_state
+argument_list|(
+name|rqe
+operator|->
+name|driveno
+argument_list|,
+comment|/* take the drive down */
+name|drive_down
+argument_list|,
+name|setstate_force
+operator||
+name|setstate_noupdate
+argument_list|)
+expr_stmt|;
 block|}
 comment|/* Now update the statistics */
 if|if
