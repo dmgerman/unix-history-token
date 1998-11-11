cae93082cb0a9d4fc408fe065b79fb09a114e749
Michael Smith <msmith@FreeBSD.org>
1998-11-11 00:02:25 +0000

    Use an at_shutdown hook to drop back to the SRM in the case where the system is being halted, rather than hanging waiting for a keypress.
    Submitted by:	Andrew Gallatin <gallatin@freebsd.org>
    
    
    Former-commit-id: 4d747a36b373e6773e90ba796378411a1b657988

--- sys/alpha/alpha/machdep.c
+++ sys/alpha/alpha/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: machdep.c,v 1.19 1998/10/30 05:41:07 msmith Exp $  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: machdep.c,v 1.20 1998/11/02 00:14:50 alex Exp $  */
 end_comment
 
 begin_comment
@@ -784,6 +784,35 @@ name|member
 parameter_list|)
 value|((size_t)(&((type *)0)->member))
 end_define
+
+begin_comment
+comment|/*  * Hooked into the shutdown chain; if the system is to be halted,  * unconditionally drop back to the SRM console.  */
+end_comment
+
+begin_function
+specifier|static
+name|void
+name|alpha_srm_shutdown
+parameter_list|(
+name|int
+name|howto
+parameter_list|,
+name|void
+modifier|*
+name|junk
+parameter_list|)
+block|{
+if|if
+condition|(
+name|howto
+operator|&
+name|RB_HALT
+condition|)
+name|alpha_pal_halt
+argument_list|()
+expr_stmt|;
+block|}
+end_function
 
 begin_function
 specifier|static
@@ -1597,6 +1626,17 @@ argument_list|()
 expr_stmt|;
 name|vm_pager_bufferinit
 argument_list|()
+expr_stmt|;
+name|at_shutdown_pri
+argument_list|(
+name|alpha_srm_shutdown
+argument_list|,
+literal|0
+argument_list|,
+name|SHUTDOWN_FINAL
+argument_list|,
+name|SHUTDOWN_PRI_LAST
+argument_list|)
 expr_stmt|;
 block|}
 end_function
