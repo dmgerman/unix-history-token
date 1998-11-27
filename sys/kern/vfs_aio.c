2d87a5fdf41fe0e574fda5a97a291502f13faa7e
Tor Egge <tegge@FreeBSD.org>
1998-11-27 01:14:21 +0000

    Don't forget to update the pmap associated with aio daemons when adding new page directory entries for a growing kernel virtual address space.
    Former-commit-id: 447750c8036601dfd0210cc51565e260ea29b952

--- sys/kern/vfs_aio.c
+++ sys/kern/vfs_aio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997 John S. Dyson.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. John S. Dyson's name may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * DISCLAIMER:  This code isn't warranted to do anything useful.  Anything  * bad that happens because of using this software isn't the responsibility  * of the author.  This software is distributed AS-IS.  *  * $Id: vfs_aio.c,v 1.33 1998/08/17 17:28:10 bde Exp $  */
+comment|/*  * Copyright (c) 1997 John S. Dyson.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. John S. Dyson's name may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * DISCLAIMER:  This code isn't warranted to do anything useful.  Anything  * bad that happens because of using this software isn't the responsibility  * of the author.  This software is distributed AS-IS.  *  * $Id: vfs_aio.c,v 1.34 1998/10/25 17:44:52 phk Exp $  */
 end_comment
 
 begin_comment
@@ -1308,7 +1308,6 @@ comment|/*  * Single AIOD vmspace shared amongst all of them  */
 end_comment
 
 begin_decl_stmt
-specifier|static
 name|struct
 name|vmspace
 modifier|*
