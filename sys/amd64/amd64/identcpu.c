8603b55175ef65eeef7652955de0aa7a9be0941b
Takenori KATO <kato@FreeBSD.org>
1998-12-05 16:30:55 +0000

    Print out information for write-allocate of AMD CPUs.
    Submitted by:	Akio Morita <amorita@meadow.scphys.kyoto-u.ac.jp>
    
    
    Former-commit-id: 187b94dac9266987558eda77f61d6411336967f7

--- sys/amd64/amd64/identcpu.c
+++ sys/amd64/amd64/identcpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.51 1998/07/11 07:45:28 bde Exp $  */
+comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.52 1998/10/06 13:16:23 kato Exp $  */
 end_comment
 
 begin_include
@@ -2992,6 +2992,9 @@ index|[
 literal|4
 index|]
 decl_stmt|;
+name|quad_t
+name|amd_whcr
+decl_stmt|;
 name|do_cpuid
 argument_list|(
 literal|0x80000000
@@ -3176,6 +3179,103 @@ operator|&
 literal|0xff
 argument_list|)
 expr_stmt|;
+block|}
+switch|switch
+condition|(
+name|cpu_id
+operator|&
+literal|0xFF0
+condition|)
+block|{
+case|case
+literal|0x560
+case|:
+comment|/* K6 0.35u */
+case|case
+literal|0x570
+case|:
+comment|/* K6 0.25u */
+case|case
+literal|0x580
+case|:
+comment|/* K6-2 */
+case|case
+literal|0x590
+case|:
+comment|/* K6-3 */
+name|amd_whcr
+operator|=
+name|rdmsr
+argument_list|(
+literal|0xc0000082
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+operator|(
+name|amd_whcr
+operator|&
+literal|0x00fe
+operator|)
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|"Write Allocate Disable\n"
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|printf
+argument_list|(
+literal|"Write Allocate Enable Limit: %dM bytes\n"
+argument_list|,
+call|(
+name|u_int32_t
+call|)
+argument_list|(
+name|amd_whcr
+operator|&
+literal|0x00fe
+argument_list|)
+operator|*
+literal|2
+argument_list|)
+expr_stmt|;
+name|printf
+argument_list|(
+literal|"Write Allocate 15-16M bytes: %s\n"
+argument_list|,
+operator|(
+name|amd_whcr
+operator|&
+literal|0x0001
+operator|)
+condition|?
+literal|"Enable"
+else|:
+literal|"Disable"
+argument_list|)
+expr_stmt|;
+name|printf
+argument_list|(
+literal|"Hardware Write Allocate Control: %s\n"
+argument_list|,
+operator|(
+name|amd_whcr
+operator|&
+literal|0x0100
+operator|)
+condition|?
+literal|"Enable"
+else|:
+literal|"Disable"
+argument_list|)
+expr_stmt|;
+block|}
+break|break;
 block|}
 block|}
 end_function
