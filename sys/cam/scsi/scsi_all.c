fda22dedc533869565491efae7493135c6ae3b90
Matthew Jacob <mjacob@FreeBSD.org>
1998-12-06 00:05:47 +0000

    print the appropriate SCSI revision (with CCS as a proper name for the announce message
    Former-commit-id: 278d1eb333fbe086f04c3ae898853a5571515079

--- sys/cam/scsi/scsi_all.c
+++ sys/cam/scsi/scsi_all.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Implementation of Utility functions for all SCSI device types.  *  * Copyright (c) 1997, 1998 Justin T. Gibbs.  * Copyright (c) 1997, 1998 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: scsi_all.c,v 1.6 1998/10/15 19:08:58 ken Exp $  */
+comment|/*  * Implementation of Utility functions for all SCSI device types.  *  * Copyright (c) 1997, 1998 Justin T. Gibbs.  * Copyright (c) 1997, 1998 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: scsi_all.c,v 1.7 1998/12/04 22:54:43 archie Exp $  */
 end_comment
 
 begin_include
@@ -11089,6 +11089,11 @@ name|revision
 index|[
 literal|16
 index|]
+decl_stmt|,
+name|rstr
+index|[
+literal|4
+index|]
 decl_stmt|;
 name|type
 operator|=
@@ -11334,9 +11339,45 @@ name|revision
 argument_list|)
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|SID_ANSI_REV
+argument_list|(
+name|inq_data
+argument_list|)
+operator|==
+name|SCSI_REV_CCS
+condition|)
+name|bcopy
+argument_list|(
+literal|"CCS"
+argument_list|,
+name|rstr
+argument_list|,
+literal|4
+argument_list|)
+expr_stmt|;
+else|else
+name|snprintf
+argument_list|(
+name|rstr
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|rstr
+argument_list|)
+argument_list|,
+literal|"%d"
+argument_list|,
+name|SID_ANSI_REV
+argument_list|(
+name|inq_data
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|printf
 argument_list|(
-literal|"<%s %s %s> %s %s SCSI%d device %s\n"
+literal|"<%s %s %s> %s %s SCSI-%s device %s\n"
 argument_list|,
 name|vendor
 argument_list|,
@@ -11355,10 +11396,7 @@ literal|"Fixed"
 argument_list|,
 name|dtype
 argument_list|,
-name|SID_ANSI_REV
-argument_list|(
-name|inq_data
-argument_list|)
+name|rstr
 argument_list|,
 name|qtype
 argument_list|)
