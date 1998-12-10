a46a309bb3319f2e7825114b802ee9db00a1a0c6
Bill Paul <wpaul@FreeBSD.org>
1998-12-10 16:18:43 +0000

    Silence gcc -Wall -ansi -pedantic and fix minor bug in xl_txeof().
    Pointed out by: Eivind
    
    
    Former-commit-id: 4827002b979ab0fc3bca540fce70beca83ff2e82

--- sys/pci/if_xl.c
+++ sys/pci/if_xl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_xl.c,v 1.18 1998/12/05 02:21:42 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_xl.c,v 1.62 1998/12/10 00:55:04 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -191,7 +191,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: if_xl.c,v 1.18 1998/12/05 02:21:42 wpaul Exp $"
+literal|"$Id: if_xl.c,v 1.62 1998/12/10 00:55:04 wpaul Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -796,9 +796,9 @@ expr|struct
 name|xl_softc
 operator|*
 operator|,
-name|u_int16_t
+name|int
 operator|,
-name|u_int16_t
+name|int
 operator|)
 argument_list|)
 decl_stmt|;
@@ -894,8 +894,7 @@ name|xl_calchash
 name|__P
 argument_list|(
 operator|(
-name|u_int8_t
-operator|*
+name|caddr_t
 operator|)
 argument_list|)
 decl_stmt|;
@@ -1917,10 +1916,10 @@ name|xl_softc
 modifier|*
 name|sc
 decl_stmt|;
-name|u_int16_t
+name|int
 name|reg
 decl_stmt|;
-name|u_int16_t
+name|int
 name|data
 decl_stmt|;
 block|{
@@ -2235,8 +2234,7 @@ name|xl_calchash
 parameter_list|(
 name|addr
 parameter_list|)
-name|u_int8_t
-modifier|*
+name|caddr_t
 name|addr
 decl_stmt|;
 block|{
@@ -8234,17 +8232,6 @@ name|xl_tx_free
 operator|=
 name|cur_tx
 expr_stmt|;
-if|if
-condition|(
-operator|!
-name|cur_tx
-operator|->
-name|xl_ptr
-operator|->
-name|xl_next
-condition|)
-empty_stmt|;
-break|break;
 block|}
 if|if
 condition|(
