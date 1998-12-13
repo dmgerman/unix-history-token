b1b02cb885fe183b53e06dac19bc3421d2dcbfbc
Steve Price <steve@FreeBSD.org>
1998-12-13 23:30:15 +0000

    wcd_read_toc returns zero on success and EIO on failure, so change the test in open to return EIO if non-zero.
    PR:		8316
    Submitted by:	Masatoshi TAMURA <tamrin@shinzan.kuee.kyoto-u.ac.jp>
    
    Former-commit-id: 6e227dda3753c605d7fe443f66ad69a9cc0c00b4

--- sys/i386/isa/wcd.c
+++ sys/i386/isa/wcd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * IDE CD-ROM driver for FreeBSD.  * Supports ATAPI-compatible drives.  *  * Copyright (C) 1995 Cronyx Ltd.  * Author Serge Vakulenko,<vak@cronyx.ru>  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 1.9, Mon Oct  9 20:27:42 MSK 1995  * $Id: wcd.c,v 1.59 1998/09/15 08:15:29 gibbs Exp $  */
+comment|/*  * IDE CD-ROM driver for FreeBSD.  * Supports ATAPI-compatible drives.  *  * Copyright (C) 1995 Cronyx Ltd.  * Author Serge Vakulenko,<vak@cronyx.ru>  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 1.9, Mon Oct  9 20:27:42 MSK 1995  * $Id: wcd.c,v 1.60 1998/10/30 10:57:09 luigi Exp $  */
 end_comment
 
 begin_include
@@ -2906,7 +2906,7 @@ name|wcd_read_toc
 argument_list|(
 name|t
 argument_list|)
-operator|<
+operator|!=
 literal|0
 condition|)
 return|return
