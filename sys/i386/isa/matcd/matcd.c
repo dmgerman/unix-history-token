d197e312eeefeb1799aa5289066e28fec322eb70
Eivind Eklund <eivind@FreeBSD.org>
1998-12-13 23:36:16 +0000

    Fix 3-byte buffer overflow.
    Also, while I'm here, fix up some more after those that haven't
    noticed #endif isn't a comment introducer.
    
    
    Former-commit-id: 66fd3649f43f092786d38b32d2eba11ec5af05da

--- sys/i386/isa/matcd/matcd.c
+++ sys/i386/isa/matcd/matcd.c
@@ -36,7 +36,7 @@ comment|/*	The proceeding strings may not be changed*/
 end_comment
 
 begin_comment
-comment|/* $Id: matcd.c,v 1.35 1998/07/04 22:30:20 julian Exp $ */
+comment|/* $Id: matcd.c,v 1.36 1998/07/15 10:11:21 bde Exp $ */
 end_comment
 
 begin_comment
@@ -473,7 +473,7 @@ name|lc_devfs_token
 decl_stmt|;
 endif|#
 directive|endif
-endif|DEVFS
+comment|/* DEVFS */
 block|}
 name|matcd_data
 index|[
@@ -10042,7 +10042,7 @@ name|unsigned
 name|char
 name|data
 index|[
-literal|5
+literal|8
 index|]
 decl_stmt|;
 name|cd
