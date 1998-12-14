0bb32e2e4b42564bfbafbac84b24dfe689fd10b2
Matthew Dillon <dillon@FreeBSD.org>
1998-12-14 21:17:37 +0000

    fix intermediate overflow in 'quad = int * int' situation by casting     the arguments to the multiply to a quad equivalent.  In this case,     vm_ooffset_t.
    Reviewed by:	Archie Cobbs <archie@whistle.com>
    
    
    Former-commit-id: e64c12dd1943de8bbfe86e4616ee0a9f277de3d1

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.185 1998/11/18 09:00:47 dg Exp $  */
+comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.186 1998/12/07 17:23:45 eivind Exp $  */
 end_comment
 
 begin_comment
@@ -5826,8 +5826,14 @@ name|f_iosize
 expr_stmt|;
 name|off
 operator|=
+operator|(
+name|vm_ooffset_t
+operator|)
 name|blkno
 operator|*
+operator|(
+name|vm_ooffset_t
+operator|)
 name|vp
 operator|->
 name|v_mount
