be697a556ea3423906f7a82d2f2699ff55f1bf1b
Eivind Eklund <eivind@FreeBSD.org>
1998-12-14 01:59:16 +0000

    Make the use of 'disc' vs 'ds' as prefix consistent by making all 'disc'. This fix the conflict of having two functions called 'dsioctl()'.
    Former-commit-id: acea414bd0a9c789dc71b894d0d3ea870e389f18

--- sys/net/if_disc.c
+++ sys/net/if_disc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	From: @(#)if_loop.c	8.1 (Berkeley) 6/10/93  *	$Id: if_disc.c,v 1.19 1998/01/08 23:41:21 eivind Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	From: @(#)if_loop.c	8.1 (Berkeley) 6/10/93  *	$Id: if_disc.c,v 1.20 1998/06/07 17:12:03 dfr Exp $  */
 end_comment
 
 begin_comment
@@ -138,14 +138,14 @@ begin_decl_stmt
 specifier|static
 name|struct
 name|ifnet
-name|dsif
+name|discif
 decl_stmt|;
 end_decl_stmt
 
 begin_function_decl
 specifier|static
 name|int
-name|dsoutput
+name|discoutput
 parameter_list|(
 name|struct
 name|ifnet
@@ -169,7 +169,7 @@ end_function_decl
 begin_function_decl
 specifier|static
 name|void
-name|dsrtrequest
+name|discrtrequest
 parameter_list|(
 name|int
 name|cmd
@@ -190,7 +190,7 @@ end_function_decl
 begin_function_decl
 specifier|static
 name|int
-name|dsioctl
+name|discioctl
 parameter_list|(
 name|struct
 name|ifnet
@@ -226,7 +226,7 @@ modifier|*
 name|ifp
 init|=
 operator|&
-name|dsif
+name|discif
 decl_stmt|;
 name|ifp
 operator|->
@@ -252,13 +252,13 @@ name|ifp
 operator|->
 name|if_ioctl
 operator|=
-name|dsioctl
+name|discioctl
 expr_stmt|;
 name|ifp
 operator|->
 name|if_output
 operator|=
-name|dsoutput
+name|discoutput
 expr_stmt|;
 name|ifp
 operator|->
@@ -308,7 +308,7 @@ end_function
 begin_function
 specifier|static
 name|int
-name|dsoutput
+name|discoutput
 parameter_list|(
 name|ifp
 parameter_list|,
@@ -355,7 +355,7 @@ literal|0
 condition|)
 name|panic
 argument_list|(
-literal|"dsoutput no HDR"
+literal|"discoutput no HDR"
 argument_list|)
 expr_stmt|;
 if|#
@@ -420,7 +420,7 @@ expr_stmt|;
 block|}
 if|if
 condition|(
-name|dsif
+name|discif
 operator|.
 name|if_bpf
 condition|)
@@ -463,7 +463,7 @@ expr_stmt|;
 name|bpf_mtap
 argument_list|(
 operator|&
-name|dsif
+name|discif
 argument_list|,
 operator|&
 name|m0
@@ -513,7 +513,7 @@ end_comment
 begin_function
 specifier|static
 name|void
-name|dsrtrequest
+name|discrtrequest
 parameter_list|(
 name|cmd
 parameter_list|,
@@ -561,7 +561,7 @@ end_comment
 begin_function
 specifier|static
 name|int
-name|dsioctl
+name|discioctl
 parameter_list|(
 name|ifp
 parameter_list|,
@@ -640,7 +640,7 @@ name|ifa
 operator|->
 name|ifa_rtrequest
 operator|=
-name|dsrtrequest
+name|discrtrequest
 expr_stmt|;
 comment|/* 		 * Everything else is done at a higher level. 		 */
 break|break;
