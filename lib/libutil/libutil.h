759c9b60f9ab9a6764ceaebffbf187feb17c0f2f
Bruce Evans <bde@FreeBSD.org>
1998-12-16 17:52:15 +0000

    Declare setproctitle() as printf0-like.
    Former-commit-id: 7d8c7318796631ed06fc9b78ee89fee34a2e6cfe

--- lib/libutil/libutil.h
+++ lib/libutil/libutil.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, is permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. This work was done expressly for inclusion into FreeBSD.  Other use  *    is permitted provided this notation is included.  * 4. Absolutely no warranty of function or purpose is made by the author  *    Peter Wemm.  * 5. Modifications may be freely made to this file providing the above  *    conditions are met.  *  * $Id: libutil.h,v 1.19 1998/10/09 07:28:14 jkh Exp $  */
+comment|/*  * Copyright (c) 1995 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, is permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. This work was done expressly for inclusion into FreeBSD.  Other use  *    is permitted provided this notation is included.  * 4. Absolutely no warranty of function or purpose is made by the author  *    Peter Wemm.  * 5. Modifications may be freely made to this file providing the above  *    conditions are met.  *  * $Id: libutil.h,v 1.20 1998/10/09 07:32:38 jkh Exp $  */
 end_comment
 
 begin_ifndef
@@ -85,6 +85,12 @@ name|_fmt
 operator|,
 operator|...
 operator|)
+argument_list|)
+name|__printf0like
+argument_list|(
+literal|1
+argument_list|,
+literal|2
 argument_list|)
 decl_stmt|;
 end_decl_stmt
