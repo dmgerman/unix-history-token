2edd6619c7e526f4c0c6dd6cc55d3b6ad1c257c4
Bruce Evans <bde@FreeBSD.org>
1998-12-16 18:59:47 +0000

    Declare size_t and ssize_t if they are not already declared, so that <kvm.h> is self-sufficient again.
    Moved typedefs and forward struct declarations out of __BEGIN_DECLS/
    __END_DECLS.
    
    Don't comment out the prototype for kvm_uread().  This was a 4 year
    old kludge for previous breakage of self-sufficiency.  The prototypwe
    was broken instead.
    
    Fixed bitrot (const poisoning) in the type of kvm_uread().
    
    Fixed order of the declaration of kvm_uread().
    
    
    Former-commit-id: 0145ed82184132a1b2ca1aa79491edd8256a8140

--- include/kvm.h
+++ include/kvm.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kvm.h	8.1 (Berkeley) 6/2/93  */
+comment|/*-  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kvm.h	8.1 (Berkeley) 6/2/93  *	$Id$  */
 end_comment
 
 begin_ifndef
@@ -45,9 +45,53 @@ directive|include
 file|<sys/cdefs.h>
 end_include
 
-begin_macro
-name|__BEGIN_DECLS
-end_macro
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|_BSD_SIZE_T_
+end_ifdef
+
+begin_typedef
+typedef|typedef
+name|_BSD_SIZE_T_
+name|size_t
+typedef|;
+end_typedef
+
+begin_undef
+undef|#
+directive|undef
+name|_BSD_SIZE_T_
+end_undef
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|_BSD_SSIZE_T_
+end_ifdef
+
+begin_typedef
+typedef|typedef
+name|_BSD_SSIZE_T_
+name|ssize_t
+typedef|;
+end_typedef
+
+begin_undef
+undef|#
+directive|undef
+name|_BSD_SSIZE_T_
+end_undef
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_typedef
 typedef|typedef
@@ -63,7 +107,14 @@ name|kinfo_proc
 struct_decl|;
 end_struct_decl
 
+begin_struct_decl
+struct_decl|struct
+name|proc
+struct_decl|;
+end_struct_decl
+
 begin_decl_stmt
+name|__BEGIN_DECLS
 name|int
 name|kvm_close
 name|__P
@@ -292,6 +343,32 @@ end_decl_stmt
 
 begin_decl_stmt
 name|ssize_t
+name|kvm_uread
+name|__P
+argument_list|(
+operator|(
+name|kvm_t
+operator|*
+operator|,
+specifier|const
+expr|struct
+name|proc
+operator|*
+operator|,
+name|unsigned
+name|long
+operator|,
+name|char
+operator|*
+operator|,
+name|size_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|ssize_t
 name|kvm_write
 name|__P
 argument_list|(
@@ -311,10 +388,6 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
-
-begin_comment
-comment|/* Comment this out until it can be fixed correctly. ssize_t	  kvm_uread 	    __P((kvm_t *, struct proc *, unsigned long, char *, size_t)); */
-end_comment
 
 begin_macro
 name|__END_DECLS
