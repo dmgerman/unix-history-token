db3d8bd579a63fdd9d0bb27e0d7d4d96766bd219
Matthew Jacob <mjacob@FreeBSD.org>
1998-12-17 19:26:49 +0000

    import logical/hardware block locate ioctls from NetBSD
    Former-commit-id: 87c9bbef7fdf2b3ba458b5cd615c1f73076fb929

--- sys/sys/mtio.h
+++ sys/sys/mtio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)mtio.h	8.1 (Berkeley) 6/2/93  * $Id: mtio.h,v 1.10 1997/02/22 09:45:37 peter Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)mtio.h	8.1 (Berkeley) 6/2/93  * $Id: mtio.h,v 1.11 1998/09/15 10:07:26 gibbs Exp $  */
 end_comment
 
 begin_ifndef
@@ -675,6 +675,54 @@ end_define
 
 begin_comment
 comment|/* enable EOT error */
+end_comment
+
+begin_comment
+comment|/*  * When more SCSI-3 SSC (streaming device) devices are out there  * that support the full 32 byte type 2 structure, we'll have to  * rethink these ioctls to support all the entities they haul into  * the picture (64 bit blocks, logical file record numbers, etc..).  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIOCRDSPOS
+value|_IOR('m', 5, u_int32_t)
+end_define
+
+begin_comment
+comment|/* get logical blk addr */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIOCRDHPOS
+value|_IOR('m', 6, u_int32_t)
+end_define
+
+begin_comment
+comment|/* get hardware blk addr */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIOCSLOCATE
+value|_IOW('m', 5, u_int32_t)
+end_define
+
+begin_comment
+comment|/* seek to logical blk addr */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIOCHLOCATE
+value|_IOW('m', 6, u_int32_t)
+end_define
+
+begin_comment
+comment|/* seek to hardware blk addr */
 end_comment
 
 begin_ifndef
