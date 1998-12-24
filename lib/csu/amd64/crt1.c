7f4ff46d4213439c79976f352f0686a832845de9
Steve Price <steve@FreeBSD.org>
1998-12-24 18:19:47 +0000

    Strip the leading path from __progname as is done in the a.out case.  Also bring in stddef.h so we can use NULL instead of 0 for pointer comparisons.
    Hinted at by:	Bruce Evans
    Reviewed by:	John Polstra
    
    
    Former-commit-id: fc6bb0ad15ae354da12cd953255b5beeacf5959a

--- lib/csu/amd64/crt1.c
+++ lib/csu/amd64/crt1.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright 1996-1998 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *      $Id: crt1.c,v 1.1.1.1 1998/03/07 20:27:10 jdp Exp $  */
+comment|/*-  * Copyright 1996-1998 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *      $Id: crt1.c,v 1.2 1998/09/07 23:31:59 jdp Exp $  */
 end_comment
 
 begin_ifndef
@@ -19,6 +19,12 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_include
+include|#
+directive|include
+file|<stddef.h>
+end_include
 
 begin_include
 include|#
@@ -255,7 +261,19 @@ condition|(
 name|argc
 operator|>
 literal|0
+operator|&&
+name|argv
+index|[
+literal|0
+index|]
+operator|!=
+name|NULL
 condition|)
+block|{
+name|char
+modifier|*
+name|s
+decl_stmt|;
 name|__progname
 operator|=
 name|argv
@@ -263,12 +281,40 @@ index|[
 literal|0
 index|]
 expr_stmt|;
+for|for
+control|(
+name|s
+operator|=
+name|__progname
+init|;
+operator|*
+name|s
+operator|!=
+literal|'\0'
+condition|;
+name|s
+operator|++
+control|)
+if|if
+condition|(
+operator|*
+name|s
+operator|==
+literal|'/'
+condition|)
+name|__progname
+operator|=
+name|s
+operator|+
+literal|1
+expr_stmt|;
+block|}
 if|if
 condition|(
 operator|&
 name|_DYNAMIC
 operator|!=
-literal|0
+name|NULL
 condition|)
 name|atexit
 argument_list|(
