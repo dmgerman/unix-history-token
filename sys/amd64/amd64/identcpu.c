8304d3a0e93ffe5fdd450013322f58671cbfa176
Michael Smith <msmith@FreeBSD.org>
1998-12-27 23:23:26 +0000

    From the submitter:
      CPU_WT_ALLOC does not work correctly for K6-2s of model 8+ and
    probably K6-3s (when they appear on the market soon). In addition,
    print_AMD_info() incorrectly printfs write allocation's size. I've
    fixed them, so they now Do The Right Thing, and added a
    "NO_MEMORY_HOLE" option to easily allow 15-16mb range handling for us
    K6 and K6-2 users.
    
    Submitted by:	Brian Feldman <green@unixhelp.org>
    
    
    Former-commit-id: d1c7d52d57299db1eafc11648ab63778dc448b3c

--- sys/amd64/amd64/identcpu.c
+++ sys/amd64/amd64/identcpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.52 1998/10/06 13:16:23 kato Exp $  */
+comment|/*  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * Copyright (c) 1997 KATO Takenori.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Id: machdep.c,v 1.193 1996/06/18 01:22:04 bde Exp  *	$Id: identcpu.c,v 1.53 1998/12/05 16:30:55 kato Exp $  */
 end_comment
 
 begin_include
@@ -174,6 +174,15 @@ end_function_decl
 begin_function_decl
 name|void
 name|enable_K6_wt_alloc
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|enable_K6_2_wt_alloc
 parameter_list|(
 name|void
 parameter_list|)
@@ -1187,11 +1196,23 @@ operator|>
 literal|3
 operator|)
 condition|)
-block|{
 name|enable_K5_wt_alloc
 argument_list|()
 expr_stmt|;
-block|}
+elseif|else
+if|if
+condition|(
+operator|(
+name|cpu_id
+operator|&
+literal|0x0f0
+operator|)
+operator|>
+literal|0x70
+condition|)
+name|enable_K6_2_wt_alloc
+argument_list|()
+expr_stmt|;
 elseif|else
 if|if
 condition|(
@@ -3195,14 +3216,6 @@ case|case
 literal|0x570
 case|:
 comment|/* K6 0.25u */
-case|case
-literal|0x580
-case|:
-comment|/* K6-2 */
-case|case
-literal|0x590
-case|:
-comment|/* K6-3 */
 name|amd_whcr
 operator|=
 name|rdmsr
@@ -3241,7 +3254,7 @@ operator|&
 literal|0x00fe
 argument_list|)
 operator|*
-literal|2
+literal|4
 argument_list|)
 expr_stmt|;
 name|printf
@@ -3267,6 +3280,88 @@ operator|(
 name|amd_whcr
 operator|&
 literal|0x0100
+operator|)
+condition|?
+literal|"Enable"
+else|:
+literal|"Disable"
+argument_list|)
+expr_stmt|;
+block|}
+break|break;
+case|case
+literal|0x580
+case|:
+comment|/* K6-2 */
+case|case
+literal|0x590
+case|:
+comment|/* K6-3 */
+name|amd_whcr
+operator|=
+name|rdmsr
+argument_list|(
+literal|0xc0000082
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+operator|(
+name|amd_whcr
+operator|&
+operator|(
+literal|0x3ff
+operator|<<
+literal|22
+operator|)
+operator|)
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|"Write Allocate Disable\n"
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|printf
+argument_list|(
+literal|"Write Allocate Enable Limit: %dM bytes\n"
+argument_list|,
+call|(
+name|u_int32_t
+call|)
+argument_list|(
+operator|(
+name|amd_whcr
+operator|&
+operator|(
+literal|0x3ff
+operator|<<
+literal|22
+operator|)
+operator|)
+operator|>>
+literal|22
+argument_list|)
+operator|*
+literal|4
+argument_list|)
+expr_stmt|;
+name|printf
+argument_list|(
+literal|"Write Allocate 15-16M bytes: %s\n"
+argument_list|,
+operator|(
+name|amd_whcr
+operator|&
+operator|(
+literal|1
+operator|<<
+literal|16
+operator|)
 operator|)
 condition|?
 literal|"Enable"
