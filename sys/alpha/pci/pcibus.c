487eee9dc3e7a1a8726d0aa1aa2a12a1a7ecb227
Doug Rabson <dfr@FreeBSD.org>
1998-12-27 18:03:29 +0000

    Fix some 64bit truncation problems which crept into SYSCTL_LONG() with the last cleanup.  Since the oid_arg2 field of struct sysctl_oid is not wide enough to hold a long, the SYSCTL_LONG() macro has been modified to only support exporting long variables by pointer instead of by value.
    Reviewed by: bde
    
    
    Former-commit-id: 04daa6ad19076f4454c727e985d38347fbbe1298

--- sys/alpha/pci/pcibus.c
+++ sys/alpha/pci/pcibus.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, Stefan Esser<se@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: pcibus.c,v 1.6 1998/11/15 18:25:16 dfr Exp $  *  */
+comment|/*  * Copyright (c) 1997, Stefan Esser<se@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: pcibus.c,v 1.7 1998/11/18 23:53:12 dfr Exp $  *  */
 end_comment
 
 begin_include
@@ -197,8 +197,6 @@ argument_list|,
 operator|&
 name|chipset_ports
 argument_list|,
-literal|0
-argument_list|,
 literal|"PCI chipset port address"
 argument_list|)
 expr_stmt|;
@@ -217,8 +215,6 @@ name|CTLFLAG_RD
 argument_list|,
 operator|&
 name|chipset_memory
-argument_list|,
-literal|0
 argument_list|,
 literal|"PCI chipset memory address"
 argument_list|)
@@ -239,8 +235,6 @@ argument_list|,
 operator|&
 name|chipset_dense
 argument_list|,
-literal|0
-argument_list|,
 literal|"PCI chipset dense memory address"
 argument_list|)
 expr_stmt|;
@@ -260,26 +254,10 @@ argument_list|,
 operator|&
 name|chipset_hae_mask
 argument_list|,
-literal|0
-argument_list|,
 literal|"PCI chipset mask for HAE register"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-
-begin_decl_stmt
-specifier|static
-name|int
-name|cfgmech
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|static
-name|int
-name|devmax
-decl_stmt|;
-end_decl_stmt
 
 begin_ifdef
 ifdef|#
--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * Quite extensively rewritten by Poul-Henning Kamp of the FreeBSD  * project, to make these variables more userfriendly.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_sysctl.c	8.4 (Berkeley) 4/14/94  * $Id: kern_sysctl.c,v 1.79 1998/12/04 22:54:51 archie Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Mike Karels at Berkeley Software Design, Inc.  *  * Quite extensively rewritten by Poul-Henning Kamp of the FreeBSD  * project, to make these variables more userfriendly.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_sysctl.c	8.4 (Berkeley) 4/14/94  * $Id: kern_sysctl.c,v 1.80 1998/12/13 07:18:54 truckman Exp $  */
 end_comment
 
 begin_include
@@ -2438,7 +2438,7 @@ comment|/*  * Default "handler" functions.  */
 end_comment
 
 begin_comment
-comment|/*  * Handle an integer, signed or unsigned.  * Two cases:  *     a variable:  point arg1 at it.  *     a constant:  pass it in arg2.  */
+comment|/*  * Handle an int, signed or unsigned.  * Two cases:  *     a variable:  point arg1 at it.  *     a constant:  pass it in arg2.  */
 end_comment
 
 begin_decl_stmt
@@ -2532,7 +2532,7 @@ block|}
 end_decl_stmt
 
 begin_comment
-comment|/*  * Handle an integer, signed or unsigned.  * Two cases:  *     a variable:  point arg1 at it.  *     a constant:  pass it in arg2.  */
+comment|/*  * Handle a long, signed or unsigned.  * Two cases:  *     a variable:  point arg1 at it.  *     a constant:  pass it in arg2.  */
 end_comment
 
 begin_decl_stmt
@@ -2545,10 +2545,6 @@ name|error
 init|=
 literal|0
 decl_stmt|;
-if|if
-condition|(
-name|arg1
-condition|)
 name|error
 operator|=
 name|SYSCTL_OUT
@@ -2556,22 +2552,6 @@ argument_list|(
 name|req
 argument_list|,
 name|arg1
-argument_list|,
-sizeof|sizeof
-argument_list|(
-name|long
-argument_list|)
-argument_list|)
-expr_stmt|;
-else|else
-name|error
-operator|=
-name|SYSCTL_OUT
-argument_list|(
-name|req
-argument_list|,
-operator|&
-name|arg2
 argument_list|,
 sizeof|sizeof
 argument_list|(
@@ -2614,100 +2594,6 @@ argument_list|,
 sizeof|sizeof
 argument_list|(
 name|long
-argument_list|)
-argument_list|)
-expr_stmt|;
-return|return
-operator|(
-name|error
-operator|)
-return|;
-block|}
-end_decl_stmt
-
-begin_comment
-comment|/*  * Handle an integer, signed or unsigned.  * Two cases:  *     a variable:  point arg1 at it.  *     a constant:  pass it in arg2.  */
-end_comment
-
-begin_decl_stmt
-name|int
-name|sysctl_handle_intptr
-name|SYSCTL_HANDLER_ARGS
-block|{
-name|int
-name|error
-init|=
-literal|0
-decl_stmt|;
-if|if
-condition|(
-name|arg1
-condition|)
-name|error
-operator|=
-name|SYSCTL_OUT
-argument_list|(
-name|req
-argument_list|,
-name|arg1
-argument_list|,
-sizeof|sizeof
-argument_list|(
-name|intptr_t
-argument_list|)
-argument_list|)
-expr_stmt|;
-else|else
-name|error
-operator|=
-name|SYSCTL_OUT
-argument_list|(
-name|req
-argument_list|,
-operator|&
-name|arg2
-argument_list|,
-sizeof|sizeof
-argument_list|(
-name|intptr_t
-argument_list|)
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|error
-operator|||
-operator|!
-name|req
-operator|->
-name|newptr
-condition|)
-return|return
-operator|(
-name|error
-operator|)
-return|;
-if|if
-condition|(
-operator|!
-name|arg1
-condition|)
-name|error
-operator|=
-name|EPERM
-expr_stmt|;
-else|else
-name|error
-operator|=
-name|SYSCTL_IN
-argument_list|(
-name|req
-argument_list|,
-name|arg1
-argument_list|,
-sizeof|sizeof
-argument_list|(
-name|intptr_t
 argument_list|)
 argument_list|)
 expr_stmt|;
