41c41e90ef5fd9413261f278c54ff2c817222f4b
Peter Wemm <peter@FreeBSD.org>
1999-01-04 18:37:41 +0000

    Don't forget a trailing \n when loading a kernel that has been stripped. (This might make ELF_VERBOSE look funny, but I'm tempted to delete that  anyway)
    Former-commit-id: 92cbe2389b0517b05ecbb9b1f400bb9fc3ff3287

--- sys/boot/common/load_elf.c
+++ sys/boot/common/load_elf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: load_elf.c,v 1.8 1998/10/16 03:04:15 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998 Peter Wemm<peter@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: load_elf.c,v 1.9 1998/10/17 03:06:38 peter Exp $  */
 end_comment
 
 begin_include
@@ -1602,7 +1602,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"\nelf_loadimage: cannot lseek() to section headers\n"
+literal|"\nelf_loadimage: cannot lseek() to section headers"
 argument_list|)
 expr_stmt|;
 goto|goto
@@ -1625,7 +1625,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"\nelf_loadimage: read section headers failed\n"
+literal|"\nelf_loadimage: read section headers failed"
 argument_list|)
 expr_stmt|;
 goto|goto
@@ -1926,7 +1926,7 @@ directive|ifdef
 name|ELF_VERBOSE
 name|printf
 argument_list|(
-literal|"%s: 0x%lx@0x%lx -> 0x%lx-0x%lx\n"
+literal|"\n%s: 0x%lx@0x%lx -> 0x%lx-0x%lx"
 argument_list|,
 name|secname
 argument_list|,
@@ -2008,7 +2008,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"\nelf_loadimage: could not seek for symbols - skipped!\n"
+literal|"\nelf_loadimage: could not seek for symbols - skipped!"
 argument_list|)
 expr_stmt|;
 name|lastaddr
@@ -2051,7 +2051,7 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"\nelf_loadimage: could not read symbols - skipped!\n"
+literal|"\nelf_loadimage: could not read symbols - skipped!"
 argument_list|)
 expr_stmt|;
 name|lastaddr
@@ -2121,7 +2121,7 @@ directive|ifndef
 name|ELF_VERBOSE
 name|printf
 argument_list|(
-literal|"]\n"
+literal|"]"
 argument_list|)
 expr_stmt|;
 endif|#
@@ -2158,6 +2158,11 @@ argument_list|)
 expr_stmt|;
 name|nosyms
 label|:
+name|printf
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
 name|ret
 operator|=
 name|lastaddr
