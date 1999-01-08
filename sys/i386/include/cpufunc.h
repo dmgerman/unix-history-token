922ef102c1b88aebef2d450baf2baaed5e6a8404
Bruce Evans <bde@FreeBSD.org>
1999-01-08 19:51:02 +0000

    Fixed some style bugs.  Clarified a comment.
    Former-commit-id: 56f99f3f50d85646a433055ece8079aba001e9f0

--- sys/i386/include/cpufunc.h
+++ sys/i386/include/cpufunc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cpufunc.h,v 1.82 1999/01/08 16:29:57 bde Exp $  */
+comment|/*-  * Copyright (c) 1993 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cpufunc.h,v 1.83 1999/01/08 19:17:45 bde Exp $  */
 end_comment
 
 begin_comment
@@ -576,20 +576,22 @@ asm|__asm __volatile("invd");
 block|}
 end_function
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|KERNEL
-end_ifdef
-
-begin_ifdef
-ifdef|#
-directive|ifdef
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
 name|SMP
-end_ifdef
+argument_list|)
+operator|&&
+name|defined
+argument_list|(
+name|KERNEL
+argument_list|)
+end_if
 
 begin_comment
-comment|/*  * When using APIC IPI's, the inlining cost is prohibitive since the call  * executes into the IPI transmission system.  */
+comment|/*  * When using APIC IPI's, invlpg() is not simply the invlpg instruction  * (this is a bug) and the inlining cost is prohibitive since the call  * executes into the IPI transmission system.  */
 end_comment
 
 begin_decl_stmt
@@ -628,7 +630,7 @@ modifier|*
 name|addr
 parameter_list|)
 block|{
-asm|__asm   __volatile("invlpg %0"::"m"(*(char *)addr):"memory");
+asm|__asm __volatile("invlpg %0" : : "m" (*(char *)addr) : "memory");
 block|}
 end_function
 
@@ -679,7 +681,7 @@ directive|else
 end_else
 
 begin_comment
-comment|/* !SMP */
+comment|/* !(SMP&& KERNEL) */
 end_comment
 
 begin_function
@@ -692,7 +694,7 @@ name|u_int
 name|addr
 parameter_list|)
 block|{
-asm|__asm   __volatile("invlpg %0"::"m"(*(char *)addr):"memory");
+asm|__asm __volatile("invlpg %0" : : "m" (*(char *)addr) : "memory");
 block|}
 end_function
 
@@ -716,14 +718,11 @@ block|)
 function|;
 end_function
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
+begin_ifdef
+ifdef|#
+directive|ifdef
 name|SWTCH_OPTIM_STATS
-argument_list|)
-end_if
+end_ifdef
 
 begin_expr_stmt
 operator|++
@@ -743,16 +742,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* SMP */
-end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* KERNEL */
+comment|/* SMP&& KERNEL */
 end_comment
 
 begin_function
@@ -1115,7 +1105,7 @@ name|void
 name|setbits
 parameter_list|(
 specifier|volatile
-name|unsigned
+name|u_int
 modifier|*
 name|addr
 parameter_list|,
@@ -1562,7 +1552,7 @@ name|__P
 argument_list|(
 operator|(
 specifier|volatile
-name|unsigned
+name|u_int
 operator|*
 name|addr
 operator|,
