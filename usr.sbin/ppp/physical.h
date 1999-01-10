558faf5a70a49620236758489674c3a8e8b155d9
Brian Somers <brian@FreeBSD.org>
1999-01-10 02:26:09 +0000

    MFC: Only call isatty() when we open our descriptor, and remember      the answer.
         If we later get a descriptor exception from select(), we know
         that it's a tty (isatty() returns 0 after the exception on a
         tty) and remember to call modem_LogicalClose().
    
         The upshot of it all is that descriptor exceptions dont leave
         the tty locked any more.
    
    
    Former-commit-id: 10ab960539b6923dad5cf896390423d8e95b2403

--- usr.sbin/ppp/physical.h
+++ usr.sbin/ppp/physical.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Written by Eivind Eklund<eivind@yes.no>  *    for Yes Interactive  *  * Copyright (C) 1998, Yes Interactive.  All rights reserved.  *  * Redistribution and use in any form is permitted.  Redistribution in  * source form should include the above copyright and this set of  * conditions, because large sections american law seems to have been  * created by a bunch of jerks on drugs that are now illegal, forcing  * me to include this copyright-stuff instead of placing this in the  * public domain.  The name of of 'Yes Interactive' or 'Eivind Eklund'  * may not be used to endorse or promote products derived from this  * software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *  $Id: physical.h,v 1.3 1998/05/29 18:33:10 brian Exp $  *  */
+comment|/*  * Written by Eivind Eklund<eivind@yes.no>  *    for Yes Interactive  *  * Copyright (C) 1998, Yes Interactive.  All rights reserved.  *  * Redistribution and use in any form is permitted.  Redistribution in  * source form should include the above copyright and this set of  * conditions, because large sections american law seems to have been  * created by a bunch of jerks on drugs that are now illegal, forcing  * me to include this copyright-stuff instead of placing this in the  * public domain.  The name of of 'Yes Interactive' or 'Eivind Eklund'  * may not be used to endorse or promote products derived from this  * software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  *  $Id: physical.h,v 1.4 1998/08/25 17:48:43 brian Exp $  *  */
 end_comment
 
 begin_struct_decl
@@ -44,11 +44,10 @@ name|mbits
 decl_stmt|;
 comment|/* Current DCD status */
 name|unsigned
-name|dev_is_modem
+name|isatty
 range|:
 literal|1
 decl_stmt|;
-comment|/* Is the device an actual modem?                                   Faked for sync devices, though...                                   (Possibly this should be                                   dev_is_not_tcp?) XXX-ML */
 name|struct
 name|mbuf
 modifier|*
@@ -169,18 +168,6 @@ begin_function_decl
 specifier|extern
 name|int
 name|physical_GetFD
-parameter_list|(
-name|struct
-name|physical
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|int
-name|physical_IsATTY
 parameter_list|(
 name|struct
 name|physical
