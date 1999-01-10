492dc2e5d88c0177c1263194f1e2e61e19f3945a
Matthew Jacob <mjacob@FreeBSD.org>
1999-01-10 02:51:06 +0000

    add MEMZERO portability defines
    Former-commit-id: 091eef6fd383985157002ad953af487894586bdf

--- sys/dev/isp/isp_freebsd.h
+++ sys/dev/isp/isp_freebsd.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $Id: $ */
+comment|/* $Id: isp_freebsd.h,v 1.7 1998/12/28 19:22:26 mjacob Exp $ */
 end_comment
 
 begin_comment
-comment|/* release_12_28_98_A */
+comment|/* release_12_28_98_A+ */
 end_comment
 
 begin_comment
@@ -228,6 +228,13 @@ define|#
 directive|define
 name|DFLT_DBLEVEL
 value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|MEMZERO
+value|BZERO
 end_define
 
 begin_define
--- sys/dev/isp/isp_freebsd_cam.h
+++ sys/dev/isp/isp_freebsd_cam.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $Id: $ */
+comment|/* $Id: isp_freebsd_cam.h,v 1.10 1998/12/28 19:22:26 mjacob Exp $ */
 end_comment
 
 begin_comment
-comment|/* release_12_28_98_A */
+comment|/* release_12_28_98_A+ */
 end_comment
 
 begin_comment
@@ -276,6 +276,13 @@ parameter_list|,
 name|x
 parameter_list|)
 value|if (isp->isp_dblev>= lev) printf x
+end_define
+
+begin_define
+define|#
+directive|define
+name|MEMZERO
+value|bzero
 end_define
 
 begin_ifdef
