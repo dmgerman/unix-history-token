b80c70221be7acce65aaebaeefd69583fd56a0e0
Poul-Henning Kamp <phk@FreeBSD.org>
1999-01-14 21:48:13 +0000

    Bring us one step closer to sysinstall'ing on a DOC2k device.
    Former-commit-id: 48565ccb9da9fb38484423e9976d71bcb56b58bf

--- lib/libdisk/create_chunk.c
+++ lib/libdisk/create_chunk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.32 1998/10/27 21:14:03 msmith Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: create_chunk.c,v 1.33 1999/01/08 00:32:19 jkh Exp $  *  */
 end_comment
 
 begin_include
@@ -1604,6 +1604,31 @@ operator|,
 name|cmaj
 operator|=
 literal|87
+operator|,
+name|p
+operator|+=
+literal|3
+expr_stmt|;
+elseif|else
+if|if
+condition|(
+operator|!
+name|strncmp
+argument_list|(
+name|p
+argument_list|,
+literal|"fla"
+argument_list|,
+literal|3
+argument_list|)
+condition|)
+name|bmaj
+operator|=
+literal|28
+operator|,
+name|cmaj
+operator|=
+literal|101
 operator|,
 name|p
 operator|+=
