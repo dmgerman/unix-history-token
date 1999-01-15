ac79dd3ea4fbe778fa4c160d4166975224df4e07
Bruce Evans <bde@FreeBSD.org>
1999-01-15 10:00:12 +0000

    Use a fast interrupt handler for the PCI version of the cy driver if option CY_PCI_FASTINTR is configured and mapping the irq to a fastintr is possible.  Unfortunately, this has to be optional because pci_map_int_right() doesn't handle the INTR_EXCL flag right -- INTR_EXCL is honoured even if the interrupt needs to be non-exclusive for other devices to work.
    Former-commit-id: e1704d4acb867f53ac7cae166a45b0d2a8152c8a

--- sys/pci/cy_pci.c
+++ sys/pci/cy_pci.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cy_pci.c,v 1.8 1999/01/11 23:35:01 bde Exp $  */
+comment|/*  * Copyright (c) 1996, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: cy_pci.c,v 1.9 1999/01/11 23:43:54 bde Exp $  */
 end_comment
 
 begin_comment
@@ -24,6 +24,12 @@ end_if
 begin_include
 include|#
 directive|include
+file|"opt_cy_pci_fastintr.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/param.h>
 end_include
 
@@ -31,6 +37,12 @@ begin_include
 include|#
 directive|include
 file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/interrupt.h>
 end_include
 
 begin_include
@@ -322,8 +334,11 @@ block|}
 comment|/* 	 * Allocate our interrupt. 	 * XXX	Using the ISA interrupt handler directly is a bit of a violation 	 *	since it doesn't actually take the same argument. For PCI, the 	 *	argument is a void * token, but for ISA it is a unit. Since 	 *	there is no overlap in PCI/ISA unit numbers for this driver, and 	 *	since the ISA driver must handle the interrupt anyway, we use 	 *	the unit number as the token even for PCI. 	 */
 if|if
 condition|(
+ifdef|#
+directive|ifdef
+name|CY_PCI_FASTINTR
 operator|!
-name|pci_map_int
+name|pci_map_int_right
 argument_list|(
 name|config_id
 argument_list|,
@@ -341,6 +356,35 @@ name|adapter
 argument_list|,
 operator|&
 name|tty_imask
+argument_list|,
+name|INTR_EXCL
+operator||
+name|INTR_FAST
+argument_list|)
+operator|&&
+endif|#
+directive|endif
+operator|!
+name|pci_map_int_right
+argument_list|(
+name|config_id
+argument_list|,
+operator|(
+name|pci_inthand_t
+operator|*
+operator|)
+name|cyintr
+argument_list|,
+operator|(
+name|void
+operator|*
+operator|)
+name|adapter
+argument_list|,
+operator|&
+name|tty_imask
+argument_list|,
+literal|0
 argument_list|)
 condition|)
 block|{
