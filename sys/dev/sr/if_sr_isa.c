eb748ee597b8369aabce2bb6d0d420b1972abd58
Julian Elischer <julian@FreeBSD.org>
1999-01-18 07:55:02 +0000

    From inspection, ISA interrupts were completely broken.. revert some of the old ISA code. (I can't test this but I'm working on the file.)
    Former-commit-id: 2359424f8ee73e1d7d5754ff17ab0e1374854a8f

--- sys/dev/sr/if_sr_isa.c
+++ sys/dev/sr/if_sr_isa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996 John Hay.  * Copyright (c) 1996 SDL Communications, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: if_sr.c,v 1.17 1998/12/16 18:42:38 phk Exp $  */
+comment|/*  * Copyright (c) 1996 John Hay.  * Copyright (c) 1996 SDL Communications, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: if_sr.c,v 1.18 1999/01/12 01:17:00 eivind Exp $  */
 end_comment
 
 begin_comment
@@ -948,6 +948,13 @@ name|hc
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_decl_stmt
+specifier|static
+name|ointhand2_t
+name|srintr
+decl_stmt|;
+end_decl_stmt
 
 begin_function_decl
 specifier|static
@@ -2694,6 +2701,12 @@ operator|->
 name|id_unit
 index|]
 decl_stmt|;
+name|id
+operator|->
+name|id_ointr
+operator|=
+name|srintr
+expr_stmt|;
 name|outb
 argument_list|(
 name|hc
@@ -3907,6 +3920,45 @@ literal|1
 return|;
 block|}
 end_function
+
+begin_comment
+comment|/*  * Get the ISA interrupts  */
+end_comment
+
+begin_function
+name|void
+name|srintr
+parameter_list|(
+name|void
+modifier|*
+name|cookie
+parameter_list|)
+block|{
+name|struct
+name|sr_hardc
+modifier|*
+name|hc
+decl_stmt|;
+name|hc
+operator|=
+operator|&
+name|sr_hardc
+index|[
+name|unit
+index|]
+expr_stmt|;
+name|srintr_hc
+argument_list|(
+name|hc
+argument_list|)
+expr_stmt|;
+return|return;
+block|}
+end_function
+
+begin_comment
+comment|/*  * PCI interrupts come straight here  */
+end_comment
 
 begin_function
 name|void
