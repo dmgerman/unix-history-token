c8f558c5b893ea8de14fdf877180faa723d7160c
Takenori KATO <kato@FreeBSD.org>
1999-01-18 14:55:38 +0000

    Added copyright.
    Pointed out by:	Kazutaka YOKOTA <yokota@zodiac.mech.utsunomiya-u.ac.jp>
    
    
    Former-commit-id: 70e2901267b90562d9ea858a4083af54f60bf498

--- sys/pc98/pc98/pc98gdc.c
+++ sys/pc98/pc98/pc98gdc.c
@@ -1,31 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_define
-define|#
-directive|define
-name|TEXT_GDC
-value|IO_GDC1
-end_define
-
 begin_comment
-comment|/* 0x60 */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|ROW
-value|25
-end_define
-
-begin_define
-define|#
-directive|define
-name|COL
-value|80
-end_define
-
-begin_comment
-comment|/*-  * $Id: pc98gdc.c,v 1.1 1999/01/18 08:38:07 kato Exp $  */
+comment|/*  * Copyright (c) 1999 FreeBSD(98) port team.  * All rights reserved.  *   * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *   * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id$  */
 end_comment
 
 begin_include
@@ -143,6 +118,31 @@ include|#
 directive|include
 file|<i386/isa/isa_device.h>
 end_include
+
+begin_define
+define|#
+directive|define
+name|TEXT_GDC
+value|IO_GDC1
+end_define
+
+begin_comment
+comment|/* 0x60 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ROW
+value|25
+end_define
+
+begin_define
+define|#
+directive|define
+name|COL
+value|80
+end_define
 
 begin_define
 define|#
