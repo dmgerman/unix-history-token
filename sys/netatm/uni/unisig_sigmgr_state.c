8b8f5fa6488c9c216605604e8f5f451e28e7b12a
Mike Spengler <mks@FreeBSD.org>
1999-01-19 23:14:51 +0000

    Prevent accessing freed signalling instance memory during detach processing.
    Former-commit-id: 569e3d6ec2168065b417528cf1ef935b3d7d1370

--- sys/netatm/uni/unisig_sigmgr_state.c
+++ sys/netatm/uni/unisig_sigmgr_state.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  * ===================================  * HARP  |  Host ATM Research Platform  * ===================================  *  *  * This Host ATM Research Platform ("HARP") file (the "Software") is  * made available by Network Computing Services, Inc. ("NetworkCS")  * "AS IS".  NetworkCS does not provide maintenance, improvements or  * support of any kind.  *  * NETWORKCS MAKES NO WARRANTIES OR REPRESENTATIONS, EXPRESS OR IMPLIED,  * INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY  * AND FITNESS FOR A PARTICULAR PURPOSE, AS TO ANY ELEMENT OF THE  * SOFTWARE OR ANY SUPPORT PROVIDED IN CONNECTION WITH THIS SOFTWARE.  * In no event shall NetworkCS be responsible for any damages, including  * but not limited to consequential damages, arising from or relating to  * any use of the Software or related support.  *  * Copyright 1994-1998 Network Computing Services, Inc.  *  * Copies of this Software may be made, however, the above copyright  * notice must be reproduced on all copies.  *  *	@(#) $Id: unisig_sigmgr_state.c,v 1.2 1998/09/17 09:35:02 phk Exp $  *  */
+comment|/*  *  * ===================================  * HARP  |  Host ATM Research Platform  * ===================================  *  *  * This Host ATM Research Platform ("HARP") file (the "Software") is  * made available by Network Computing Services, Inc. ("NetworkCS")  * "AS IS".  NetworkCS does not provide maintenance, improvements or  * support of any kind.  *  * NETWORKCS MAKES NO WARRANTIES OR REPRESENTATIONS, EXPRESS OR IMPLIED,  * INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY  * AND FITNESS FOR A PARTICULAR PURPOSE, AS TO ANY ELEMENT OF THE  * SOFTWARE OR ANY SUPPORT PROVIDED IN CONNECTION WITH THIS SOFTWARE.  * In no event shall NetworkCS be responsible for any damages, including  * but not limited to consequential damages, arising from or relating to  * any use of the Software or related support.  *  * Copyright 1994-1998 Network Computing Services, Inc.  *  * Copies of this Software may be made, however, the above copyright  * notice must be reproduced on all copies.  *  *	@(#) $Id: unisig_sigmgr_state.c,v 1.3 1998/10/31 20:07:01 phk Exp $  *  */
 end_comment
 
 begin_comment
@@ -64,7 +64,7 @@ end_ifndef
 begin_expr_stmt
 name|__RCSID
 argument_list|(
-literal|"@(#) $Id: unisig_sigmgr_state.c,v 1.2 1998/09/17 09:35:02 phk Exp $"
+literal|"@(#) $Id: unisig_sigmgr_state.c,v 1.3 1998/10/31 20:07:01 phk Exp $"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -2144,13 +2144,6 @@ name|T_ATM_CAUSE_NORMAL_CALL_CLEARING
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * Set the signalling manager state 	 */
-name|usp
-operator|->
-name|us_state
-operator|=
-name|UNISIG_DETACH
-expr_stmt|;
 comment|/* 	 * Close the signalling channel 	 */
 if|if
 condition|(
@@ -2293,7 +2286,16 @@ name|M_DEVBUF
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * Otherwise, wait for protocol instance to be freed 	 * during unisig_free processing for the last queued VCC 	 */
+else|else
+block|{
+comment|/* 		 * Otherwise, set new signalling manager state and 		 * wait for protocol instance to be freed during  		 * unisig_free processing for the last queued VCC 		 */
+name|usp
+operator|->
+name|us_state
+operator|=
+name|UNISIG_DETACH
+expr_stmt|;
+block|}
 return|return
 operator|(
 literal|0
