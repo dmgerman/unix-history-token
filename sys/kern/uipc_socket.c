7418666a4700d44f29dc5c797e0cfa934eca4dec
Bill Fenner <fenner@FreeBSD.org>
1999-01-25 16:58:56 +0000

    Port NetBSD's 19990120-accept bug fix.  This works around the race condition where select(2) can return that a listening socket has a connected socket queued, the connection is broken, and the user calls accept(2), which then blocks because there are no connections queued.
    Reviewed by:	wollman
    Obtained from:	NetBSD
    (ftp://ftp.NetBSD.ORG/pub/NetBSD/misc/security/patches/19990120-accept)
    
    
    Former-commit-id: cb04a74403526d2bd178ad181523f9a1ca32e60a

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket.c	8.3 (Berkeley) 4/15/94  *	$Id: uipc_socket.c,v 1.50 1999/01/20 17:31:54 fenner Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket.c	8.3 (Berkeley) 4/15/94  *	$Id: uipc_socket.c,v 1.51 1999/01/20 17:45:22 fenner Exp $  */
 end_comment
 
 begin_include
@@ -776,18 +776,7 @@ operator|&
 name|SS_COMP
 condition|)
 block|{
-name|TAILQ_REMOVE
-argument_list|(
-operator|&
-name|head
-operator|->
-name|so_comp
-argument_list|,
-name|so
-argument_list|,
-name|so_list
-argument_list|)
-expr_stmt|;
+comment|/* 			 * We must not decommission a socket that's 			 * on the accept(2) queue.  If we do, then 			 * accept(2) may hang after select(2) indicated 			 * that the listening socket was ready. 			 */
 block|}
 else|else
 block|{
@@ -956,6 +945,18 @@ operator|->
 name|so_list
 operator|.
 name|tqe_next
+expr_stmt|;
+name|TAILQ_REMOVE
+argument_list|(
+operator|&
+name|so
+operator|->
+name|so_comp
+argument_list|,
+name|sp
+argument_list|,
+name|so_list
+argument_list|)
 expr_stmt|;
 operator|(
 name|void
@@ -1249,6 +1250,18 @@ operator|&=
 operator|~
 name|SS_NOFDREF
 expr_stmt|;
+if|if
+condition|(
+operator|(
+name|so
+operator|->
+name|so_state
+operator|&
+name|SS_ISDISCONNECTED
+operator|)
+operator|==
+literal|0
+condition|)
 name|error
 operator|=
 call|(
@@ -1267,6 +1280,22 @@ argument_list|,
 name|nam
 argument_list|)
 expr_stmt|;
+else|else
+block|{
+if|if
+condition|(
+name|nam
+condition|)
+operator|*
+name|nam
+operator|=
+literal|0
+expr_stmt|;
+name|error
+operator|=
+literal|0
+expr_stmt|;
+block|}
 name|splx
 argument_list|(
 name|s
