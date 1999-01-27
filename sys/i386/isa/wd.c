1b3c6f0849c47e08beb4d049093ed37b57f8ee11
Julian Elischer <julian@FreeBSD.org>
1999-01-27 19:32:34 +0000

    Patch from Soeren to remove inapropriate interrupt messages also a bit of a BDE patch in there I beleive. Backs out a fix I needed for Cyrix support early on but it turns out that a later fix in the cyrix support made it un-needed.
    Former-commit-id: 5f0c55d67edd0d3432c27215efdbdf2b0299dc7f

--- sys/i386/isa/wd.c
+++ sys/i386/isa/wd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.185 1999/01/16 01:06:23 bde Exp $  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)wd.c	7.2 (Berkeley) 5/9/91  *	$Id: wd.c,v 1.186 1999/01/17 05:46:24 bde Exp $  */
 end_comment
 
 begin_comment
@@ -4728,6 +4728,7 @@ expr_stmt|;
 comment|/* finish off DMA */
 if|if
 condition|(
+operator|(
 name|du
 operator|->
 name|dk_flags
@@ -4735,8 +4736,11 @@ operator|&
 operator|(
 name|DKFL_DMA
 operator||
-name|DKFL_USEDMA
+name|DKFL_SINGLE
 operator|)
+operator|)
+operator|==
+name|DKFL_DMA
 condition|)
 block|{
 comment|/* XXX SMP boxes sometimes generate an early intr.  Why? */
@@ -4759,9 +4763,10 @@ argument_list|)
 operator|&
 name|WDDS_INTERRUPT
 operator|)
-operator|!=
+operator|==
 literal|0
 condition|)
+return|return;
 name|dmastat
 operator|=
 name|wddma
@@ -7112,6 +7117,25 @@ operator|==
 name|WDCC_FEATURES
 condition|)
 block|{
+name|outb
+argument_list|(
+name|wdc
+operator|+
+name|wd_sdh
+argument_list|,
+name|WDSD_IBM
+operator||
+operator|(
+name|du
+operator|->
+name|dk_unit
+operator|<<
+literal|4
+operator|)
+operator||
+name|head
+argument_list|)
+expr_stmt|;
 name|outb
 argument_list|(
 name|wdc
@@ -10786,13 +10810,11 @@ operator|&
 operator|(
 name|DKFL_DMA
 operator||
-name|DKFL_USEDMA
+name|DKFL_SINGLE
 operator|)
 operator|)
-operator|&&
-name|du
-operator|->
-name|dk_dmacookie
+operator|==
+name|DKFL_DMA
 condition|)
 name|wddma
 index|[
