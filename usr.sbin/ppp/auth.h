ad520580d91f4b95516c3b1138a3f845ed422fd4
Brian Somers <brian@FreeBSD.org>
1999-02-01 13:48:24 +0000

    MFC: If we receive no answer from the server when sending PAP      requests, give up (don't sit there indefinitely).
    Former-commit-id: ae4dc4034400b77edccc18fc80c476698c4565b6

--- usr.sbin/ppp/auth.h
+++ usr.sbin/ppp/auth.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: auth.h,v 1.11 1998/05/21 21:44:03 brian Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: auth.h,v 1.12 1998/08/07 18:42:47 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_struct_decl
@@ -31,6 +31,17 @@ modifier|*
 parameter_list|,
 name|int
 parameter_list|,
+name|struct
+name|physical
+modifier|*
+parameter_list|)
+function_decl|;
+name|void
+function_decl|(
+modifier|*
+name|FailedFunc
+function_decl|)
+parameter_list|(
 name|struct
 name|physical
 modifier|*
@@ -115,7 +126,6 @@ parameter_list|,
 name|void
 function_decl|(
 modifier|*
-name|fn
 function_decl|)
 parameter_list|(
 name|struct
@@ -124,6 +134,16 @@ modifier|*
 parameter_list|,
 name|int
 parameter_list|,
+name|struct
+name|physical
+modifier|*
+parameter_list|)
+parameter_list|,
+name|void
+function_decl|(
+modifier|*
+function_decl|)
+parameter_list|(
 name|struct
 name|physical
 modifier|*
