378bd6d478ad50cd7361f89f18d42b252dc82925
Bill Fenner <fenner@FreeBSD.org>
1999-02-02 07:23:28 +0000

    Fix the port of the NetBSD 19990120-accept fix.  I misread a piece of code when examining their fix, which caused my code (in rev 1.52) to: - panic("soaccept: !NOFDREF") - fatal trap 12, with tracebacks going thru soclose and soaccept
    Former-commit-id: 7cd94adb2c3fb3b630746042e50088a573b1dde6

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket.c	8.3 (Berkeley) 4/15/94  *	$Id: uipc_socket.c,v 1.52 1999/01/25 16:58:52 fenner Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket.c	8.3 (Berkeley) 4/15/94  *	$Id: uipc_socket.c,v 1.53 1999/01/27 21:49:57 dillon Exp $  */
 end_comment
 
 begin_include
@@ -777,6 +777,7 @@ name|SS_COMP
 condition|)
 block|{
 comment|/* 			 * We must not decommission a socket that's 			 * on the accept(2) queue.  If we do, then 			 * accept(2) may hang after select(2) indicated 			 * that the listening socket was ready. 			 */
+return|return;
 block|}
 else|else
 block|{
@@ -796,11 +797,7 @@ operator|->
 name|so_state
 operator|&=
 operator|~
-operator|(
 name|SS_INCOMP
-operator||
-name|SS_COMP
-operator|)
 expr_stmt|;
 name|so
 operator|->
@@ -946,6 +943,7 @@ name|so_list
 operator|.
 name|tqe_next
 expr_stmt|;
+comment|/* Dequeue from so_comp since sofree() won't do it */
 name|TAILQ_REMOVE
 argument_list|(
 operator|&
@@ -957,6 +955,24 @@ name|sp
 argument_list|,
 name|so_list
 argument_list|)
+expr_stmt|;
+name|so
+operator|->
+name|so_qlen
+operator|--
+expr_stmt|;
+name|sp
+operator|->
+name|so_state
+operator|&=
+operator|~
+name|SS_COMP
+expr_stmt|;
+name|sp
+operator|->
+name|so_head
+operator|=
+name|NULL
 expr_stmt|;
 operator|(
 name|void
