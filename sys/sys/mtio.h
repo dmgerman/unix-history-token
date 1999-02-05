d2a17271c91e15f9e194bc6a47a0904cc0a365a3
Matthew Jacob <mjacob@FreeBSD.org>
1999-02-05 02:47:30 +0000

    Add driver states that will be reported (for SCSI) in mt_dsreg.
    Former-commit-id: 240f1963cc6290aead3bdf62e604651280a9a2af

--- sys/sys/mtio.h
+++ sys/sys/mtio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)mtio.h	8.1 (Berkeley) 6/2/93  * $Id: mtio.h,v 1.14 1998/12/21 22:14:02 mjacob Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)mtio.h	8.1 (Berkeley) 6/2/93  * $Id: mtio.h,v 1.15 1998/12/22 17:17:17 mjacob Exp $  */
 end_comment
 
 begin_ifndef
@@ -320,6 +320,164 @@ directive|define
 name|MT_COMP_UNSUPP
 value|0xfffffffd
 end_define
+
+begin_comment
+comment|/*  * Values in mt_dsreg that say what the device is doing  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_NIL
+value|0
+end_define
+
+begin_comment
+comment|/* Unknown */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_REST
+value|1
+end_define
+
+begin_comment
+comment|/* Doing Nothing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_RBSY
+value|2
+end_define
+
+begin_comment
+comment|/* Communicating with tape (but no motion) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_WR
+value|20
+end_define
+
+begin_comment
+comment|/* Writing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_FMK
+value|21
+end_define
+
+begin_comment
+comment|/* Writing Filemarks */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_ZER
+value|22
+end_define
+
+begin_comment
+comment|/* Erasing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_RD
+value|30
+end_define
+
+begin_comment
+comment|/* Reading */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_FWD
+value|40
+end_define
+
+begin_comment
+comment|/* Spacing Forward */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_REV
+value|41
+end_define
+
+begin_comment
+comment|/* Spacing Reverse */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_POS
+value|42
+end_define
+
+begin_comment
+comment|/* Hardware Positioning (direction unknown) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_REW
+value|43
+end_define
+
+begin_comment
+comment|/* Rewinding */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_TEN
+value|44
+end_define
+
+begin_comment
+comment|/* Retensioning */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_UNL
+value|45
+end_define
+
+begin_comment
+comment|/* Unloading */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MTIO_DSREG_LD
+value|46
+end_define
+
+begin_comment
+comment|/* Unloading */
+end_comment
 
 begin_endif
 endif|#
