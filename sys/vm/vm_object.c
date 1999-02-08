bd6c047f3113b571a90cbada9bbba69dd805a366
Matthew Dillon <dillon@FreeBSD.org>
1999-02-08 05:15:54 +0000

    General cleanup.  Remove #if 0's and remove useless register qualifiers.
    Former-commit-id: 6c203d0fa3a45fd4967a069ceaa458bd152ef7e7

--- sys/vm/vm_object.c
+++ sys/vm/vm_object.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_object.c	8.5 (Berkeley) 3/22/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_object.c,v 1.145 1999/02/07 08:44:53 dillon Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_object.c	8.5 (Berkeley) 3/22/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_object.c,v 1.146 1999/02/07 21:48:22 dillon Exp $  */
 end_comment
 
 begin_comment
@@ -282,18 +282,6 @@ index|]
 decl_stmt|;
 end_decl_stmt
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
-begin_endif
-unit|static int objidnumber;
-endif|#
-directive|endif
-end_endif
-
 begin_function
 name|void
 name|_vm_object_allocate
@@ -310,7 +298,6 @@ decl_stmt|;
 name|vm_size_t
 name|size
 decl_stmt|;
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
@@ -358,12 +345,6 @@ name|flags
 operator|=
 literal|0
 expr_stmt|;
-if|#
-directive|if
-literal|0
-block|object->id = ++objidnumber;
-endif|#
-directive|endif
 if|if
 condition|(
 operator|(
@@ -487,12 +468,6 @@ name|vm_ooffset_t
 operator|)
 literal|0
 expr_stmt|;
-if|#
-directive|if
-literal|0
-block|object->page_hint = NULL;
-endif|#
-directive|endif
 comment|/* 	 * Try to generate a number that will spread objects out in the 	 * hash table.  We 'wipe' new objects across the hash in 128 page 	 * increments plus 1 more to offset it a little more by the time 	 * it wraps around. 	 */
 name|object
 operator|->
@@ -667,7 +642,6 @@ name|vm_size_t
 name|size
 decl_stmt|;
 block|{
-specifier|register
 name|vm_object_t
 name|result
 decl_stmt|;
@@ -708,7 +682,6 @@ name|vm_object_reference
 parameter_list|(
 name|object
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
@@ -1284,12 +1257,10 @@ name|vm_object_terminate
 parameter_list|(
 name|object
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
 block|{
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|;
@@ -1578,7 +1549,6 @@ name|int
 name|flags
 decl_stmt|;
 block|{
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|,
@@ -1586,7 +1556,6 @@ name|np
 decl_stmt|,
 name|tp
 decl_stmt|;
-specifier|register
 name|vm_offset_t
 name|tstart
 decl_stmt|,
@@ -2482,12 +2451,10 @@ name|vm_object_deactivate_pages
 parameter_list|(
 name|object
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
 block|{
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|,
@@ -2537,22 +2504,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
-begin_comment
-comment|/*  *	vm_object_pmap_copy:  *  *	Makes all physical pages in the specified  *	object range copy-on-write.  No writeable  *	references to these pages should remain.  *  *	The object must *not* be locked.  */
-end_comment
-
-begin_endif
-unit|void vm_object_pmap_copy(object, start, end) 	register vm_object_t object; 	register vm_pindex_t start; 	register vm_pindex_t end; { 	register vm_page_t p;  	if (object == NULL || (object->flags& OBJ_WRITEABLE) == 0) 		return;  	for (p = TAILQ_FIRST(&object->memq); 		p != NULL; 		p = TAILQ_NEXT(p, listq)) { 		vm_page_protect(p, VM_PROT_READ); 	}  	vm_object_clear_flag(object, OBJ_WRITEABLE); }
-endif|#
-directive|endif
-end_endif
-
 begin_comment
 comment|/*  * Same as vm_object_pmap_copy, except range checking really  * works, and is meant for small sections of an object.  *  * This code protects resident pages by making them read-only  * and is typically called on a fork or split when a page  * is converted to copy-on-write.    *  * NOTE: If the page is already at VM_PROT_NONE, calling  * vm_page_protect will have no effect.  */
 end_comment
@@ -2567,15 +2518,12 @@ name|start
 parameter_list|,
 name|end
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|start
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|end
 decl_stmt|;
@@ -2583,7 +2531,6 @@ block|{
 name|vm_pindex_t
 name|idx
 decl_stmt|;
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|;
@@ -2659,20 +2606,16 @@ name|start
 parameter_list|,
 name|end
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|start
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|end
 decl_stmt|;
 block|{
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|;
@@ -2978,12 +2921,6 @@ operator|==
 name|NULL
 condition|)
 continue|continue;
-if|#
-directive|if
-literal|0
-block|if ((tobject == NULL) || (tobject->ref_count != 1)) { 				continue; 			}
-endif|#
-directive|endif
 name|tpindex
 operator|+=
 name|OFF_TO_IDX
@@ -3154,11 +3091,9 @@ name|vm_size_t
 name|length
 decl_stmt|;
 block|{
-specifier|register
 name|vm_object_t
 name|source
 decl_stmt|;
-specifier|register
 name|vm_object_t
 name|result
 decl_stmt|;
@@ -3281,26 +3216,21 @@ name|vm_object_qcollapse
 parameter_list|(
 name|object
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
 block|{
-specifier|register
 name|vm_object_t
 name|backing_object
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|backing_offset_index
 decl_stmt|;
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|,
 name|pp
 decl_stmt|;
-specifier|register
 name|vm_size_t
 name|size
 decl_stmt|;
@@ -4414,15 +4344,12 @@ name|end
 parameter_list|,
 name|clean_only
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|object
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|start
 decl_stmt|;
-specifier|register
 name|vm_pindex_t
 name|end
 decl_stmt|;
@@ -4430,7 +4357,6 @@ name|boolean_t
 name|clean_only
 decl_stmt|;
 block|{
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|,
@@ -4806,7 +4732,6 @@ name|prev_size
 parameter_list|,
 name|next_size
 parameter_list|)
-specifier|register
 name|vm_object_t
 name|prev_object
 decl_stmt|;
@@ -5583,7 +5508,6 @@ name|full
 init|=
 name|have_addr
 decl_stmt|;
-specifier|register
 name|vm_page_t
 name|p
 decl_stmt|;
@@ -5592,7 +5516,6 @@ define|#
 directive|define
 name|count
 value|was_count
-specifier|register
 name|int
 name|count
 decl_stmt|;
