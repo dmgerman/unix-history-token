c0024d2f127df7f294efd76446fa435f725536a8
Guido van Rooij <guido@FreeBSD.org>
1999-02-09 15:53:21 +0000

    Add missing poatch for ibcs2_ipc.h as well. Sorry this took so long but there was a routing problem earlier today.
    Former-commit-id: 60a3e335073a422c7c0891a88326df25d09315ec

--- sys/i386/ibcs2/ibcs2_ipc.h
+++ sys/i386/ibcs2/ibcs2_ipc.h
@@ -1,14 +1,50 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995 Steven Wallace  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Steven Wallace.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: ibcs2_ipc.h,v 1.3 1997/02/22 09:33:20 peter Exp $  */
+comment|/*  * Copyright (c) 1995 Steven Wallace  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Steven Wallace.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: ibcs2_ipc.h,v 1.4 1999/02/07 19:35:40 guido Exp $  */
 end_comment
+
+begin_struct
+struct|struct
+name|ibcs2_ipc_perm
+block|{
+name|ushort
+name|uid
+decl_stmt|;
+comment|/* user id */
+name|ushort
+name|gid
+decl_stmt|;
+comment|/* group id */
+name|ushort
+name|cuid
+decl_stmt|;
+comment|/* creator user id */
+name|ushort
+name|cgid
+decl_stmt|;
+comment|/* creator group id */
+name|ushort
+name|mode
+decl_stmt|;
+comment|/* r/w permission */
+name|ushort
+name|seq
+decl_stmt|;
+comment|/* sequence # (to generate unique msg/sem/shm id) */
+name|key_t
+name|key
+decl_stmt|;
+comment|/* user specified msg/sem/shm key */
+block|}
+struct|;
+end_struct
 
 begin_struct
 struct|struct
 name|ibcs2_msqid_ds
 block|{
 name|struct
-name|ipc_perm
+name|ibcs2_ipc_perm
 name|msg_perm
 decl_stmt|;
 name|struct
@@ -54,7 +90,7 @@ struct|struct
 name|ibcs2_semid_ds
 block|{
 name|struct
-name|ipc_perm
+name|ibcs2_ipc_perm
 name|sem_perm
 decl_stmt|;
 name|struct
@@ -100,7 +136,7 @@ struct|struct
 name|ibcs2_shmid_ds
 block|{
 name|struct
-name|ipc_perm
+name|ibcs2_ipc_perm
 name|shm_perm
 decl_stmt|;
 name|int
