54f0282602e155d5139fceeae3b4b06d64de2163
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-02-14 21:26:52 +0000

    MFC: fix initial state.
    Former-commit-id: 40cf5b6cc09518f517f5489e0362a97b9eced275

--- release/sysinstall/system.c
+++ release/sysinstall/system.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.87.2.1 1999/02/05 22:20:17 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: system.c,v 1.87.2.2 1999/02/09 22:25:59 jkh Exp $  *  * Jordan Hubbard  *  * My contributions are in the public domain.  *  * Parts of this file are also blatently stolen from Poul-Henning Kamp's  * previous version of sysinstall, and as such fall under his "BEERWARE license"  * so buy him a beer if you like it!  Buy him a beer for me, too!  * Heck, get him completely drunk and send me pictures! :-)  */
 end_comment
 
 begin_include
@@ -487,7 +487,7 @@ name|VAR_HOSTNAME
 argument_list|,
 name|hname
 argument_list|,
-literal|1
+literal|0
 argument_list|)
 expr_stmt|;
 block|}
