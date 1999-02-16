5754c39736ed909ebc7db3e76ce23becb4f3c96b
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-02-16 01:58:04 +0000

    Be more forgiving of PnP config saving failures.
    Former-commit-id: 4e4152a44a771352e064ae39dbebc349b25f5a9d

--- usr.sbin/sysinstall/kget.c
+++ usr.sbin/sysinstall/kget.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999 Andrzej Bialecki<abial@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kget.c,v 1.1 1999/01/20 12:31:43 jkh Exp $  */
+comment|/*-  * Copyright (c) 1999 Andrzej Bialecki<abial@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: kget.c,v 1.2 1999/02/15 07:07:37 jkh Exp $  */
 end_comment
 
 begin_include
@@ -469,6 +469,10 @@ name|free
 argument_list|(
 name|buf
 argument_list|)
+operator|,
+name|buf
+operator|=
+name|NULL
 expr_stmt|;
 comment|/* Now, print the changes to PnP override table */
 name|i
@@ -497,10 +501,9 @@ argument_list|(
 literal|"kget: error sizing buffer\n"
 argument_list|)
 expr_stmt|;
-return|return
-operator|-
-literal|1
-return|;
+goto|goto
+name|bail
+goto|;
 block|}
 name|buf
 operator|=
@@ -544,10 +547,9 @@ argument_list|(
 literal|"kget: error retrieving data\n"
 argument_list|)
 expr_stmt|;
-return|return
-operator|-
-literal|1
-return|;
+goto|goto
+name|bail
+goto|;
 block|}
 name|i
 operator|=
@@ -806,6 +808,8 @@ operator|<
 name|len
 condition|)
 do|;
+name|bail
+label|:
 name|fprintf
 argument_list|(
 name|fout
@@ -816,6 +820,15 @@ expr_stmt|;
 name|fclose
 argument_list|(
 name|fout
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|buf
+condition|)
+name|free
+argument_list|(
+name|buf
 argument_list|)
 expr_stmt|;
 return|return
