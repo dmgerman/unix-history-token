76fb5410a159ff433a07ed0aa70a82c4e241c08d
Brian Somers <brian@FreeBSD.org>
1999-02-20 01:12:45 +0000

    Handle empty PAP & CHAP packets (containing only an FSM header). Some CHAP implementations send no welcome message with their SUCCESS/FAILURE packets.  This was being mis-identified as a truncated packet by the new authentication code :-(
    Former-commit-id: ec6be99f3ac50c9fe19cf43a1a63e7e9e9175806

--- usr.sbin/ppp/auth.c
+++ usr.sbin/ppp/auth.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			PPP Secret Key Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1994, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: auth.c,v 1.39 1999/02/18 00:52:12 brian Exp $  *  *	TODO:  *		o Implement check against with registered IP addresses.  */
+comment|/*  *			PPP Secret Key Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1994, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: auth.c,v 1.40 1999/02/19 10:48:42 brian Exp $  *  *	TODO:  *		o Implement check against with registered IP addresses.  */
 end_comment
 
 begin_include
@@ -1726,6 +1726,19 @@ condition|)
 return|return
 name|bp
 return|;
+name|authp
+operator|->
+name|in
+operator|.
+name|hdr
+operator|.
+name|length
+operator|=
+name|htons
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
 name|log_Printf
 argument_list|(
 name|LogWARN
@@ -1748,6 +1761,20 @@ argument_list|)
 expr_stmt|;
 block|}
 else|else
+block|{
+name|authp
+operator|->
+name|in
+operator|.
+name|hdr
+operator|.
+name|length
+operator|=
+name|htons
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
 name|log_Printf
 argument_list|(
 name|LogWARN
@@ -1764,6 +1791,7 @@ argument_list|,
 name|len
 argument_list|)
 expr_stmt|;
+block|}
 name|mbuf_Free
 argument_list|(
 name|bp
--- usr.sbin/ppp/chap.c
+++ usr.sbin/ppp/chap.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *			PPP CHAP Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: chap.c,v 1.45 1999/02/18 19:11:46 brian Exp $  *  *	TODO:  */
+comment|/*  *			PPP CHAP Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: chap.c,v 1.46 1999/02/18 19:45:06 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_include
@@ -3135,10 +3135,25 @@ argument_list|)
 operator|)
 operator|==
 name|NULL
+operator|&&
+name|ntohs
+argument_list|(
+name|chap
+operator|->
+name|auth
+operator|.
+name|in
+operator|.
+name|hdr
+operator|.
+name|length
+argument_list|)
+operator|==
+literal|0
 condition|)
 name|log_Printf
 argument_list|(
-name|LogERROR
+name|LogWARN
 argument_list|,
 literal|"Chap Input: Truncated header !\n"
 argument_list|)
