f3e786e3d252212f305061fde2047c23abef6dbc
Poul-Henning Kamp <phk@FreeBSD.org>
1999-02-23 15:08:44 +0000

    Misplaces brace puts important code into debug section.
    Reviewed by:	phk
    Submitted by:	Stefan Bethke <stefan.bethke@hanse.de>
    
    
    Former-commit-id: 6b16b2f08cd418b84b679b3c0b114e591ef850d6

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994-1996 Cronyx Engineering Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 2.4, Thu Apr 30 17:17:21 MSD 1997  *  * $Id: if_spppsubr.c,v 1.52 1998/12/27 21:30:44 phk Exp $  */
+comment|/*  * Synchronous PPP/Cisco link level subroutines.  * Keepalive protocol implemented in both Cisco and PPP modes.  *  * Copyright (C) 1994-1996 Cronyx Engineering Ltd.  * Author: Serge Vakulenko,<vak@cronyx.ru>  *  * Heavily revamped to conform to RFC 1661.  * Copyright (C) 1997, Joerg Wunsch.  *  * This software is distributed with NO WARRANTIES, not even the implied  * warranties for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *  * Authors grant any other persons or organisations permission to use  * or modify this software as long as this message is kept with the software,  * all derivative works or modified versions.  *  * From: Version 2.4, Thu Apr 30 17:17:21 MSD 1997  *  * $Id: if_spppsubr.c,v 1.53 1999/02/19 13:45:09 phk Exp $  */
 end_comment
 
 begin_include
@@ -13916,6 +13916,7 @@ name|desiredaddr
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
 name|p
 index|[
 literal|2
@@ -13950,7 +13951,6 @@ index|]
 operator|=
 name|hisaddr
 expr_stmt|;
-block|}
 break|break;
 block|}
 comment|/* Add the option to nak'ed list. */
