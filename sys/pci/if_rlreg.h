45ed55a71e72bdc9cfec5fa7276ec85517ccf1dc
Bill Paul <wpaul@FreeBSD.org>
1999-02-23 15:38:25 +0000

    Add support for still more cheapo 10/100 cards: Delta Electronics and Addtron appear to have their own VIA Rhine II and RealTek 8139 boards with custom PCI vendor and device IDs. This commit updates the PCI vendor and device lists in the vr and rl drivers so that we can probe the additional devices.
    Found by: nosing around the PCI vendor and device code list at:
    	http://www.halcyon.com/scripts/jboemler/pci/pcicode
    
    
    Former-commit-id: bd9a738cab8868785cf1d53fdccadfe152849b5e

--- sys/pci/if_rlreg.h
+++ sys/pci/if_rlreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_rlreg.h,v 1.14 1998/12/07 00:16:44 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_rlreg.h,v 1.16 1999/02/23 06:42:42 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -1823,17 +1823,6 @@ value|0x10EC
 end_define
 
 begin_comment
-comment|/*  * Accton PCI vendor ID  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|ACCTON_VENDORID
-value|0x1113
-end_define
-
-begin_comment
 comment|/*  * RealTek chip device IDs.  */
 end_comment
 
@@ -1852,6 +1841,17 @@ value|0x8139
 end_define
 
 begin_comment
+comment|/*  * Accton PCI vendor ID  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ACCTON_VENDORID
+value|0x1113
+end_define
+
+begin_comment
 comment|/*  * Accton MPX 5030/5038 device ID.  */
 end_comment
 
@@ -1860,6 +1860,50 @@ define|#
 directive|define
 name|ACCTON_DEVICEID_5030
 value|0x1211
+end_define
+
+begin_comment
+comment|/*  * Delta Electronics Vendor ID.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DELTA_VENDORID
+value|0x1500
+end_define
+
+begin_comment
+comment|/*  * Delta device IDs.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DELTA_DEVICEID_8139
+value|0x1360
+end_define
+
+begin_comment
+comment|/*  * Addtron vendor ID.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ADDTRON_VENDORID
+value|0x4033
+end_define
+
+begin_comment
+comment|/*  * Addtron device IDs.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ADDTRON_DEVICEID_8139
+value|0x1360
 end_define
 
 begin_comment
