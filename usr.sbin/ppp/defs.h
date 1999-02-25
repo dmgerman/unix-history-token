3b595445988c3045c03f7dad8c1e5037a9903ac1
Brian Somers <brian@FreeBSD.org>
1999-02-25 20:08:11 +0000

    MFC: Parse IP addresses more securely - specifically, don't allow      a bum name to return as 0.0.0.0... we don't want ``delete xxx''      to delete the default route when xxx doesn't resolve.
         Support IP number specifications as the host when specifying
         a tcp-style device (rather than *just* hostnames).
    
    
    Former-commit-id: 2a5586fd8d96c00d4cd9a83f5fb2543eec4c3777

--- usr.sbin/ppp/defs.h
+++ usr.sbin/ppp/defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.35 1998/08/07 18:42:48 brian Exp $  *  *	TODO:  */
+comment|/*  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: defs.h,v 1.36 1998/08/25 17:48:42 brian Exp $  *  *	TODO:  */
 end_comment
 
 begin_comment
@@ -531,6 +531,19 @@ begin_function_decl
 specifier|extern
 name|int
 name|Nam2mode
+parameter_list|(
+specifier|const
+name|char
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|struct
+name|in_addr
+name|GetIpAddr
 parameter_list|(
 specifier|const
 name|char
