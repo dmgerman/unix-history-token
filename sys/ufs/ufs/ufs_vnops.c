aeb3cb2d39b9e9397570f317ca97c409d67b5592
Matthew Dillon <dillon@FreeBSD.org>
1999-02-25 05:35:53 +0000

    Update ufs_vnops code to use new specinfo fields rather then guess.     This is part of general specinfo / d_parms() commit.
    Former-commit-id: cf50105e89bb9bd09750e024cde10f67277d9939

--- sys/ufs/ufs/ufs_vnops.c
+++ sys/ufs/ufs/ufs_vnops.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_vnops.c	8.27 (Berkeley) 5/27/95  * $Id: ufs_vnops.c,v 1.106 1999/01/28 00:57:56 dillon Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_vnops.c	8.27 (Berkeley) 5/27/95  * $Id: ufs_vnops.c,v 1.107 1999/02/13 08:38:28 dillon Exp $  */
 end_comment
 
 begin_include
@@ -1894,7 +1894,7 @@ name|ip
 operator|->
 name|i_gen
 expr_stmt|;
-comment|/* this doesn't belong here */
+comment|/* 	 * Use the information contained in v_specinfo for VBLK and VCHR 	 * vnodes, and in the underlying mount point for (typically) VREG 	 * vnodes.  Note that vp->v_specmountpoint can be NULL. 	 */
 if|if
 condition|(
 name|vp
@@ -1903,12 +1903,18 @@ name|v_type
 operator|==
 name|VBLK
 condition|)
+block|{
 name|vap
 operator|->
 name|va_blocksize
 operator|=
-name|BLKDEV_IOSIZE
+name|vp
+operator|->
+name|v_specinfo
+operator|->
+name|si_bsize_best
 expr_stmt|;
+block|}
 elseif|else
 if|if
 condition|(
@@ -1918,13 +1924,20 @@ name|v_type
 operator|==
 name|VCHR
 condition|)
+block|{
 name|vap
 operator|->
 name|va_blocksize
 operator|=
-name|MAXBSIZE
+name|vp
+operator|->
+name|v_specinfo
+operator|->
+name|si_bsize_max
 expr_stmt|;
+block|}
 else|else
+block|{
 name|vap
 operator|->
 name|va_blocksize
@@ -1937,6 +1950,7 @@ name|mnt_stat
 operator|.
 name|f_iosize
 expr_stmt|;
+block|}
 name|vap
 operator|->
 name|va_bytes
