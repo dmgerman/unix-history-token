c5e15b553e85d47af614ee89542a6043acc20d5b
Julian Elischer <julian@FreeBSD.org>
1999-03-03 02:35:51 +0000

    Slight cleanup of code resurected for union mounts.. Submitted by: Tony Finch <dot@dotat.at>
    Former-commit-id: a09402003e2ba87267c21eef6c778dc19ad3d77f

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_syscalls.c	8.13 (Berkeley) 4/15/94  * $Id: vfs_syscalls.c,v 1.118 1999/02/25 15:54:06 bde Exp $  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_syscalls.c	8.13 (Berkeley) 4/15/94  * $Id: vfs_syscalls.c,v 1.119 1999/02/27 07:06:05 julian Exp $  */
 end_comment
 
 begin_comment
@@ -14622,8 +14622,6 @@ operator|)
 return|;
 if|if
 condition|(
-name|union_dircheckp
-operator|&&
 name|SCARG
 argument_list|(
 name|uap
@@ -14634,6 +14632,11 @@ operator|==
 name|auio
 operator|.
 name|uio_resid
+condition|)
+block|{
+if|if
+condition|(
+name|union_dircheckp
 condition|)
 block|{
 name|error
@@ -14670,19 +14673,6 @@ return|;
 block|}
 if|if
 condition|(
-operator|(
-name|SCARG
-argument_list|(
-name|uap
-argument_list|,
-name|count
-argument_list|)
-operator|==
-name|auio
-operator|.
-name|uio_resid
-operator|)
-operator|&&
 operator|(
 name|vp
 operator|->
@@ -14745,6 +14735,7 @@ expr_stmt|;
 goto|goto
 name|unionread
 goto|;
+block|}
 block|}
 name|error
 operator|=
@@ -15099,8 +15090,6 @@ operator|)
 return|;
 if|if
 condition|(
-name|union_dircheckp
-operator|&&
 name|SCARG
 argument_list|(
 name|uap
@@ -15111,6 +15100,11 @@ operator|==
 name|auio
 operator|.
 name|uio_resid
+condition|)
+block|{
+if|if
+condition|(
+name|union_dircheckp
 condition|)
 block|{
 name|error
@@ -15147,19 +15141,6 @@ return|;
 block|}
 if|if
 condition|(
-operator|(
-name|SCARG
-argument_list|(
-name|uap
-argument_list|,
-name|count
-argument_list|)
-operator|==
-name|auio
-operator|.
-name|uio_resid
-operator|)
-operator|&&
 operator|(
 name|vp
 operator|->
@@ -15222,6 +15203,7 @@ expr_stmt|;
 goto|goto
 name|unionread
 goto|;
+block|}
 block|}
 if|if
 condition|(
