e6ebcb08f3769c5aeed3c1acd9f30dd0352ea547
Alan Cox <alc@FreeBSD.org>
1999-03-04 18:04:40 +0000

    exec_aout_imgact should lock the vm_map before calling vm_map_insert.
    Reviewed by:	Matthew Dillon <dillon@apollo.backplane.com>,
    		"John S. Dyson" <dyson@iquest.net>, and
    		David Greenman <dg@root.com>
    
    
    Former-commit-id: 9c0af481f71fe7d888e58105931f5c9afbf128fe

--- sys/kern/imgact_aout.c
+++ sys/kern/imgact_aout.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: imgact_aout.c,v 1.45 1999/01/29 22:59:43 dillon Exp $  */
+comment|/*  * Copyright (c) 1993, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: imgact_aout.c,v 1.46 1999/02/19 14:25:34 luoqi Exp $  */
 end_comment
 
 begin_include
@@ -259,6 +259,9 @@ name|struct
 name|vnode
 modifier|*
 name|vp
+decl_stmt|;
+name|vm_map_t
+name|map
 decl_stmt|;
 name|vm_object_t
 name|object
@@ -581,6 +584,18 @@ name|imgp
 operator|->
 name|vp
 expr_stmt|;
+name|map
+operator|=
+operator|&
+name|vmspace
+operator|->
+name|vm_map
+expr_stmt|;
+name|vm_map_lock
+argument_list|(
+name|map
+argument_list|)
+expr_stmt|;
 name|object
 operator|=
 name|vp
@@ -632,11 +647,18 @@ if|if
 condition|(
 name|error
 condition|)
+block|{
+name|vm_map_unlock
+argument_list|(
+name|map
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 name|error
 operator|)
 return|;
+block|}
 name|data_end
 operator|=
 name|text_end
@@ -691,11 +713,18 @@ if|if
 condition|(
 name|error
 condition|)
+block|{
+name|vm_map_unlock
+argument_list|(
+name|map
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 name|error
 operator|)
 return|;
+block|}
 block|}
 name|pmap_object_init_pt
 argument_list|(
@@ -762,12 +791,24 @@ if|if
 condition|(
 name|error
 condition|)
+block|{
+name|vm_map_unlock
+argument_list|(
+name|map
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 name|error
 operator|)
 return|;
 block|}
+block|}
+name|vm_map_unlock
+argument_list|(
+name|map
+argument_list|)
+expr_stmt|;
 comment|/* Fill in process VM information */
 name|vmspace
 operator|->
