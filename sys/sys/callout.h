256793029114566edc5b2f3c3c85edc2620d37cf
Garrett Wollman <wollman@FreeBSD.org>
1999-03-06 04:46:20 +0000

    Expose a slightly-lower-level interface to timeouts which allows callers to manage their own memory.  Tested on my machine (make buildworld). I've made analogous changes on the alpha, but don't have a machine to test.
    Not-objected-to by:	dg, gibbs
    
    
    Former-commit-id: a72a6bee9ec4b71ab2e0490fff8960fba4f63905

--- sys/sys/callout.h
+++ sys/sys/callout.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)callout.h	8.2 (Berkeley) 1/21/94  * $Id: callout.h,v 1.10 1997/12/01 05:45:15 davidg Exp $  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)callout.h	8.2 (Berkeley) 1/21/94  * $Id: callout.h,v 1.11 1998/01/11 00:43:51 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -84,9 +84,46 @@ operator|)
 argument_list|)
 expr_stmt|;
 comment|/* function to call */
+name|int
+name|c_flags
+decl_stmt|;
+comment|/* state of this entry */
 block|}
 struct|;
 end_struct
+
+begin_define
+define|#
+directive|define
+name|CALLOUT_LOCAL_ALLOC
+value|0x0001
+end_define
+
+begin_comment
+comment|/* was allocated from callfree */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CALLOUT_PENDING
+value|0x0002
+end_define
+
+begin_comment
+comment|/* callout is currently active */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CALLOUT_FIRED
+value|0x0004
+end_define
+
+begin_comment
+comment|/* callout has been fired */
+end_comment
 
 begin_struct
 struct|struct
@@ -150,6 +187,82 @@ decl_stmt|,
 name|callwheelmask
 decl_stmt|,
 name|softticks
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|callout_fired
+parameter_list|(
+name|c
+parameter_list|)
+value|((c)->c_flags& CALLOUT_FIRED)
+end_define
+
+begin_decl_stmt
+name|void
+name|callout_init
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|callout
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_define
+define|#
+directive|define
+name|callout_pending
+parameter_list|(
+name|c
+parameter_list|)
+value|(((c)->c_flags& CALLOUT_PENDING) ? \ 				 ((c)->c_time - ticks) : 0)
+end_define
+
+begin_decl_stmt
+name|void
+name|callout_reset
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|callout
+operator|*
+operator|,
+name|int
+operator|,
+name|void
+argument_list|(
+operator|*
+argument_list|)
+argument_list|(
+name|void
+operator|*
+argument_list|)
+operator|,
+name|void
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|callout_stop
+name|__P
+argument_list|(
+operator|(
+expr|struct
+name|callout
+operator|*
+operator|)
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
