ea7b1cea32e623e1e85fe82e80b8d149b9b3278f
Doug Rabson <dfr@FreeBSD.org>
1999-03-07 16:06:41 +0000

    * Register sysctl nodes before running sysinits when loading files and   unregister them after sysuninits when unloading. * Add code to vfs_register() to set the oid number of vfs sysctls to   the type number of the filesystem.
    Reviewed by: bde
    
    
    Former-commit-id: a57d9d3051ace50215211f262cfbb81ee71e3a30

--- sys/kern/vfs_init.c
+++ sys/kern/vfs_init.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed  * to Berkeley by John Heidemann of the UCLA Ficus project.  *  * Source: * @(#)i405_init.c 2.10 92/04/27 UCLA Ficus project  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_init.c	8.3 (Berkeley) 1/4/94  * $Id: vfs_init.c,v 1.43 1999/01/28 17:32:00 dillon Exp $  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed  * to Berkeley by John Heidemann of the UCLA Ficus project.  *  * Source: * @(#)i405_init.c 2.10 92/04/27 UCLA Ficus project  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)vfs_init.c	8.3 (Berkeley) 1/4/94  * $Id: vfs_init.c,v 1.44 1999/02/16 10:49:49 dfr Exp $  */
 end_comment
 
 begin_include
@@ -1501,6 +1501,11 @@ modifier|*
 name|l
 decl_stmt|;
 name|struct
+name|sysctl_oid
+modifier|*
+name|oidp
+decl_stmt|;
+name|struct
 name|vfsconf
 modifier|*
 name|vfsp
@@ -1536,7 +1541,6 @@ name|vfc_next
 control|)
 if|if
 condition|(
-operator|!
 name|strcmp
 argument_list|(
 name|vfc
@@ -1547,6 +1551,8 @@ name|vfsp
 operator|->
 name|vfc_name
 argument_list|)
+operator|==
+literal|0
 condition|)
 return|return
 name|EEXIST
@@ -1579,6 +1585,63 @@ name|vfc_next
 operator|=
 name|NULL
 expr_stmt|;
+comment|/* 	 * If this filesystem has a sysctl node under vfs 	 * (i.e. vfs.xxfs), then change the oid number of that node to  	 * match the filesystem's type number.  This allows user code 	 * which uses the type number to read sysctl variables defined 	 * by the filesystem to continue working. Since the oids are 	 * in a sorted list, we need to make sure the order is 	 * preserved by re-registering the oid after modifying its 	 * number. 	 */
+for|for
+control|(
+name|oidp
+operator|=
+name|SLIST_FIRST
+argument_list|(
+operator|&
+name|sysctl__vfs_children
+argument_list|)
+init|;
+name|oidp
+condition|;
+name|oidp
+operator|=
+name|SLIST_NEXT
+argument_list|(
+name|oidp
+argument_list|,
+name|oid_link
+argument_list|)
+control|)
+if|if
+condition|(
+name|strcmp
+argument_list|(
+name|oidp
+operator|->
+name|oid_name
+argument_list|,
+name|vfc
+operator|->
+name|vfc_name
+argument_list|)
+operator|==
+literal|0
+condition|)
+block|{
+name|sysctl_unregister_oid
+argument_list|(
+name|oidp
+argument_list|)
+expr_stmt|;
+name|oidp
+operator|->
+name|oid_number
+operator|=
+name|vfc
+operator|->
+name|vfc_typenum
+expr_stmt|;
+name|sysctl_register_oid
+argument_list|(
+name|oidp
+argument_list|)
+expr_stmt|;
+block|}
 comment|/* 	 * Call init function for this VFS... 	 */
 operator|(
 operator|*
