099dcf0382225774098af36a6723db95f7f29741
Bill Paul <wpaul@FreeBSD.org>
1999-03-07 04:39:25 +0000

    Also add 1000baseSX, 1000baseLX, 1000baseCX and 1000baseTX media types. At this point I don't know if there are any actual gigabit ethernet devices that support media other than 1000baseSX (multi-mode fiber) but who knows.
    Former-commit-id: c1c181bc35fa0fc7f2db8563dbadfd1466652a05

--- sys/net/if_media.h
+++ sys/net/if_media.h
@@ -437,6 +437,50 @@ begin_comment
 comment|/* 10baseFL - Fiber */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|IFM_1000_SX
+value|14
+end_define
+
+begin_comment
+comment|/* 1000BaseSX Multi-mode Fiber */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IFM_1000_LX
+value|15
+end_define
+
+begin_comment
+comment|/* 1000BaseLX Single-mode Fiber */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IFM_1000_CX
+value|16
+end_define
+
+begin_comment
+comment|/* 1000BaseCX 150ohm STP */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IFM_1000_TX
+value|17
+end_define
+
+begin_comment
+comment|/* 1000BaseTX 4 pair cat 5 */
+end_comment
+
 begin_comment
 comment|/*  * Token ring  */
 end_comment
@@ -953,14 +997,14 @@ begin_define
 define|#
 directive|define
 name|IFM_SUBTYPE_ETHERNET_DESCRIPTIONS
-value|{				\ 	{ IFM_10_T,	"10baseT/UTP" },				\ 	{ IFM_10_2,	"10base2/BNC" },				\ 	{ IFM_10_5,	"10base5/AUI" },				\ 	{ IFM_100_TX,	"100baseTX" },					\ 	{ IFM_100_FX,	"100baseFX" },					\ 	{ IFM_100_T4,	"100baseT4" },					\ 	{ IFM_100_VG,	"100baseVG" },					\ 	{ IFM_100_T2,	"100baseT2" },					\ 	{ IFM_1000_FX,	"1000baseFX" },					\ 	{ IFM_10_STP,	"10baseSTP" },					\ 	{ IFM_10_FL,	"10baseFL" },					\ 	{ 0, NULL },							\ }
+value|{				\ 	{ IFM_10_T,	"10baseT/UTP" },				\ 	{ IFM_10_2,	"10base2/BNC" },				\ 	{ IFM_10_5,	"10base5/AUI" },				\ 	{ IFM_100_TX,	"100baseTX" },					\ 	{ IFM_100_FX,	"100baseFX" },					\ 	{ IFM_100_T4,	"100baseT4" },					\ 	{ IFM_100_VG,	"100baseVG" },					\ 	{ IFM_100_T2,	"100baseT2" },					\ 	{ IFM_1000_FX,	"1000baseFX" },					\ 	{ IFM_10_STP,	"10baseSTP" },					\ 	{ IFM_10_FL,	"10baseFL" },					\ 	{ IFM_1000_SX,	"1000baseSX" },					\ 	{ IFM_1000_LX,	"1000baseLX" },					\ 	{ IFM_1000_CX,	"1000baseCX" },					\ 	{ IFM_1000_TX,	"1000baseTX" },					\ 	{ 0, NULL },							\ }
 end_define
 
 begin_define
 define|#
 directive|define
 name|IFM_SUBTYPE_ETHERNET_ALIASES
-value|{					\ 	{ IFM_10_T,	"UTP" },					\ 	{ IFM_10_T,	"10UTP" },					\ 	{ IFM_10_2,	"BNC" },					\ 	{ IFM_10_2,	"10BNC" },					\ 	{ IFM_10_5,	"AUI" },					\ 	{ IFM_10_5,	"10AUI" },					\ 	{ IFM_100_TX,	"100TX" },					\ 	{ IFM_100_FX,	"100FX" },					\ 	{ IFM_100_T4,	"100T4" },					\ 	{ IFM_100_VG,	"100VG" },					\ 	{ IFM_100_T2,	"100T2" },					\ 	{ IFM_1000_FX,	"1000FX" },					\ 	{ IFM_10_STP,	"10STP" },					\ 	{ IFM_10_FL,	"10FL" },					\ 	{ 0, NULL },							\ }
+value|{					\ 	{ IFM_10_T,	"UTP" },					\ 	{ IFM_10_T,	"10UTP" },					\ 	{ IFM_10_2,	"BNC" },					\ 	{ IFM_10_2,	"10BNC" },					\ 	{ IFM_10_5,	"AUI" },					\ 	{ IFM_10_5,	"10AUI" },					\ 	{ IFM_100_TX,	"100TX" },					\ 	{ IFM_100_FX,	"100FX" },					\ 	{ IFM_100_T4,	"100T4" },					\ 	{ IFM_100_VG,	"100VG" },					\ 	{ IFM_100_T2,	"100T2" },					\ 	{ IFM_1000_FX,	"1000FX" },					\ 	{ IFM_10_STP,	"10STP" },					\ 	{ IFM_10_FL,	"10FL" },					\ 	{ IFM_1000_FX,	"1000SX" },					\ 	{ IFM_1000_FX,	"1000LX" },					\ 	{ IFM_1000_FX,	"1000CX" },					\ 	{ IFM_1000_FX,	"1000TX" },					\ 	{ 0, NULL },							\ }
 end_define
 
 begin_define
