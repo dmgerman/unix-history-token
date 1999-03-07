58bd81b531a9e04263cbba2960c4cbd76aae6a9b
Brian Somers <brian@FreeBSD.org>
1999-03-07 18:13:44 +0000

    Support PPTP via libalias (``alias pptp addr'').
    Former-commit-id: 3ab15d53949a7ee48a58703e8dc729329e1291ed

--- usr.sbin/ppp/nat_cmd.h
+++ usr.sbin/ppp/nat_cmd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * The code in this file was written by Eivind Eklund<perhaps@yes.no>,  * who places it in the public domain without restriction.  *  *	$Id: alias_cmd.h,v 1.8 1998/05/21 21:43:45 brian Exp $  */
+comment|/*-  * The code in this file was written by Eivind Eklund<perhaps@yes.no>,  * who places it in the public domain without restriction.  *  *	$Id: alias_cmd.h,v 1.9 1999/03/07 15:02:37 brian Exp $  */
 end_comment
 
 begin_struct_decl
@@ -39,6 +39,19 @@ begin_function_decl
 specifier|extern
 name|int
 name|alias_ProxyRule
+parameter_list|(
+name|struct
+name|cmdargs
+specifier|const
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|int
+name|alias_Pptp
 parameter_list|(
 name|struct
 name|cmdargs
