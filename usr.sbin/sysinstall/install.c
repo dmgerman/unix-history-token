97bb97ce12369c3d6f50aa79892ffde710079504
Jordan K. Hubbard <jkh@FreeBSD.org>
1999-03-11 09:38:06 +0000

    Make proper symlinks for fixit mode and ELF binaries.
    Noticed by:	jdp
    
    
    Former-commit-id: fdfef15d34271fca50a457d8c0f69f7246cbff55

--- usr.sbin/sysinstall/install.c
+++ usr.sbin/sysinstall/install.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: install.c,v 1.228 1999/02/14 21:26:28 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*  * The new sysinstall program.  *  * This is probably the last program in the `sysinstall' line - the next  * generation being essentially a complete rewrite.  *  * $Id: install.c,v 1.229 1999/02/15 00:49:33 jkh Exp $  *  * Copyright (c) 1995  *	Jordan Hubbard.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -1256,7 +1256,12 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-comment|/* Yet another iggly hardcoded pathname. */
+comment|/* Yet more iggly hardcoded pathnames. */
+name|Mkdir
+argument_list|(
+literal|"/usr/libexec"
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|!
@@ -1266,11 +1271,6 @@ literal|"/usr/libexec/ld.so"
 argument_list|)
 condition|)
 block|{
-name|Mkdir
-argument_list|(
-literal|"/usr/libexec"
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|symlink
@@ -1280,15 +1280,55 @@ argument_list|,
 literal|"/usr/libexec/ld.so"
 argument_list|)
 condition|)
+name|msgDebug
+argument_list|(
+literal|"Couldn't link to ld.so - not necessarily a problem for ELF\n"
+argument_list|)
+expr_stmt|;
+block|}
+if|if
+condition|(
+operator|!
+name|file_readable
+argument_list|(
+literal|"/usr/libexec/ld-elf.so.1"
+argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|symlink
+argument_list|(
+literal|"/mnt2/usr/libexec/ld-elf.so.1"
+argument_list|,
+literal|"/usr/libexec/ld-elf.so.1"
+argument_list|)
+condition|)
 block|{
 name|msgConfirm
 argument_list|(
-literal|"Warning: could not create the symlink for ld.so.\n"
+literal|"Warning: could not create the symlink for ld-elf.so.1\n"
 literal|"Dynamic executables from the CDROM likely won't work."
 argument_list|)
 expr_stmt|;
 block|}
 block|}
+comment|/* optional nicety */
+if|if
+condition|(
+operator|!
+name|file_readable
+argument_list|(
+literal|"/usr/bin/vi"
+argument_list|)
+condition|)
+name|symlink
+argument_list|(
+literal|"/mnt2/usr/bin/vi"
+argument_list|,
+literal|"/usr/bin/vi"
+argument_list|)
+expr_stmt|;
 name|fixit_common
 argument_list|()
 expr_stmt|;
