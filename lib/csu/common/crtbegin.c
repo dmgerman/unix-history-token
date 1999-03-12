9885c8a31b784b6bc51cb28f0218c6e707f38ae0
John Polstra <jdp@FreeBSD.org>
1999-03-12 17:33:28 +0000

    Move the code for the ".init" and ".fini" sections outside of a C function so the compiler won't try to emit line numbers for it with "-g", breaking the build.  This has the nice side-effect of making crtbegin.o and crtbeginS.o a little bit smaller.
    Remove "-Wno-unused" from the Makefile.  Replace it with "__unused"
    on particular function and variable declarations.
    
    
    Former-commit-id: a2ce9c70daa19f8400f32ba92cfb7d30fcc84c59

--- lib/csu/common/crtbegin.c
+++ lib/csu/common/crtbegin.c
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright 1996-1998 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *      $Id: crtbegin.c,v 1.3 1996/04/12 02:24:35 jdp Exp $  */
+comment|/*-  * Copyright 1996-1998 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *      $Id: crtbegin.c,v 1.1.1.1 1998/03/07 20:27:10 jdp Exp $  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
 
 begin_typedef
 typedef|typedef
@@ -67,6 +73,28 @@ operator|)
 operator|-
 literal|1
 block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|void
+name|do_ctors
+argument_list|(
+name|void
+argument_list|)
+name|__unused
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|static
+name|void
+name|do_dtors
+argument_list|(
+name|void
+argument_list|)
+name|__unused
 decl_stmt|;
 end_decl_stmt
 
@@ -154,25 +182,13 @@ expr_stmt|;
 block|}
 end_function
 
-begin_function
-specifier|static
-name|void
-name|function_skeleton
-parameter_list|(
-name|void
-parameter_list|)
-block|{
-asm|__asm__(".section .init,\"ax\",@progbits");
-name|do_ctors
-argument_list|()
-expr_stmt|;
-asm|__asm__(".section .fini,\"ax\",@progbits");
-name|do_dtors
-argument_list|()
-expr_stmt|;
-asm|__asm__(".text");
-block|}
-end_function
+begin_asm
+asm|__asm__(".section .init,\"ax\",@progbits; call do_ctors; .previous");
+end_asm
+
+begin_asm
+asm|__asm__(".section .fini,\"ax\",@progbits; call do_dtors; .previous");
+end_asm
 
 end_unit
 
