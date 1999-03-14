04b5e075c08b7d4bcb71af9f5425913e2b1fa8a0
Ken Merry <ken@FreeBSD.org>
1999-03-14 05:17:49 +0000

    MFC, version 1.49
    Disable tagged queueing for IBM DCAS drives, update comment for WD
    Enterprise drives.
    
    PR:		kern/10398
    Submitted by:	Hideaki Okada <hokada@isl.melco.co.jp>
    
    Former-commit-id: 95e11a1f48ade5a91a99b3a6e18f01fed6c710e5

--- sys/cam/cam_xpt.c
+++ sys/cam/cam_xpt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Implementation of the Common Access Method Transport (XPT) layer.  *  * Copyright (c) 1997, 1998, 1999 Justin T. Gibbs.  * Copyright (c) 1997, 1998, 1999 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: cam_xpt.c,v 1.42.2.3 1999/03/07 22:51:15 ken Exp $  */
+comment|/*  * Implementation of the Common Access Method Transport (XPT) layer.  *  * Copyright (c) 1997, 1998, 1999 Justin T. Gibbs.  * Copyright (c) 1997, 1998, 1999 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: cam_xpt.c,v 1.42.2.4 1999/03/11 11:03:00 jkh Exp $  */
 end_comment
 
 begin_include
@@ -1063,6 +1063,30 @@ literal|2
 block|}
 block|,
 block|{
+comment|/* 		 * Slow when tagged queueing is enabled.  Write performance 		 * steadily drops off with more and more concurrent 		 * transactions.  Best sequential write performance with 		 * tagged queueing turned off and write caching turned on. 		 * 		 * PR:  kern/10398 		 * Submitted by:  Hideaki Okada<hokada@isl.melco.co.jp> 		 * Drive:  DCAS-34330 w/ "S65A" firmware. 		 * 		 * The drive with the problem had the "S65A" firmware 		 * revision, and has also been reported (by Stephen J. 		 * Roznowski<sjr@home.net>) for a drive with the "S61A" 		 * firmware revision. 		 * 		 * Although no one has reported problems with the 2 gig 		 * version of the DCAS drive, the assumption is that it 		 * has the same problems as the 4 gig version.  Therefore 		 * this quirk entries disables tagged queueing for all 		 * DCAS drives. 		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_FIXED
+block|,
+literal|"IBM"
+block|,
+literal|"DCAS*"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+literal|0
+block|,
+comment|/*mintags*/
+literal|0
+block|,
+comment|/*maxtags*/
+literal|0
+block|}
+block|,
+block|{
 comment|/* Broken tagged queuing drive */
 block|{
 name|T_DIRECT
@@ -1135,7 +1159,7 @@ literal|0
 block|}
 block|,
 block|{
-comment|/* 		 * Slow when tagged queueing is enabled. (1.5MB/sec versus 		 * 8MB/sec.) 		 * Submitted by: Andrew Gallatin<gallatin@cs.duke.edu> 		 */
+comment|/* 		 * Slow when tagged queueing is enabled. (1.5MB/sec versus 		 * 8MB/sec.) 		 * Submitted by: Andrew Gallatin<gallatin@cs.duke.edu> 		 * Best performance with these drives is achieved with 		 * tagged queueing turned off, and write caching turned on. 		 */
 block|{
 name|T_DIRECT
 block|,
@@ -1159,7 +1183,7 @@ literal|0
 block|}
 block|,
 block|{
-comment|/* 		 * Slow when tagged queueing is enabled. (1.5MB/sec versus 		 * 8MB/sec.) 		 * Submitted by: Andrew Gallatin<gallatin@cs.duke.edu> 		 */
+comment|/* 		 * Slow when tagged queueing is enabled. (1.5MB/sec versus 		 * 8MB/sec.) 		 * Submitted by: Andrew Gallatin<gallatin@cs.duke.edu> 		 * Best performance with these drives is achieved with 		 * tagged queueing turned off, and write caching turned on. 		 */
 block|{
 name|T_DIRECT
 block|,
