01533851108b3014f260d18485da5a97c14cdf36
Jonathan Lemon <jlemon@FreeBSD.org>
1999-03-18 20:13:20 +0000

    Cast (u_char *) to (vm_offset_t) to silence compiler warnings.
    Former-commit-id: 356f3f9448c1a2f4e774890b02f56dbc678d2aff

--- sys/i386/isa/vesa.c
+++ sys/i386/isa/vesa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Kazutaka YOKOTA and Michael Smith  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: vesa.c,v 1.18 1999/02/20 03:05:37 yokota Exp $  */
+comment|/*-  * Copyright (c) 1998 Kazutaka YOKOTA and Michael Smith  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: vesa.c,v 1.19 1999/03/18 04:38:16 jlemon Exp $  */
 end_comment
 
 begin_include
@@ -1071,6 +1071,9 @@ argument_list|(
 operator|&
 name|vesa_vmcontext
 argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
 name|buf
 argument_list|,
 operator|&
@@ -1381,6 +1384,9 @@ argument_list|(
 operator|&
 name|vesa_vmcontext
 argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
 name|p
 argument_list|,
 operator|&
@@ -1685,6 +1691,9 @@ argument_list|(
 operator|&
 name|vesa_vmcontext
 argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
 name|p
 argument_list|,
 operator|&
@@ -1890,6 +1899,9 @@ argument_list|(
 operator|&
 name|vesa_vmcontext
 argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
 name|buf
 argument_list|,
 operator|&
@@ -2450,6 +2462,9 @@ argument_list|(
 operator|&
 name|vesa_vmcontext
 argument_list|,
+operator|(
+name|vm_offset_t
+operator|)
 name|buf
 argument_list|,
 operator|&
