295f2a02a9a99887ab49030bf1d12e2c5757e129
Wes Peters <wes@FreeBSD.org>
1999-03-20 04:51:25 +0000

    Turn on PCI bus mastering in driver attach routine to avoid hanging the system if the BIOS has not initialized the interface.
    PR:		10150
    Reviewed by:	dg
    
    Former-commit-id: d9e745212842376e63163eddc67be3ffaea6b9b8

--- sys/pci/if_fxp.c
+++ sys/pci/if_fxp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995, David Greenman  * All rights reserved.  *  * Modifications to support NetBSD and media selection:  * Copyright (c) 1997 Jason R. Thorpe.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: if_fxp.c,v 1.64 1999/02/12 17:56:23 julian Exp $  */
+comment|/*  * Copyright (c) 1995, David Greenman  * All rights reserved.  *  * Modifications to support NetBSD and media selection:  * Copyright (c) 1997 Jason R. Thorpe.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: if_fxp.c,v 1.65 1999/03/17 16:44:53 luigi Exp $  */
 end_comment
 
 begin_comment
@@ -309,6 +309,16 @@ include|#
 directive|include
 file|<pci/pcivar.h>
 end_include
+
+begin_include
+include|#
+directive|include
+file|<pci/pcireg.h>
+end_include
+
+begin_comment
+comment|/* for PCIM_CMD_xxx */
+end_comment
 
 begin_include
 include|#
@@ -2281,6 +2291,9 @@ decl_stmt|;
 name|int
 name|s
 decl_stmt|;
+name|u_long
+name|val
+decl_stmt|;
 name|sc
 operator|=
 name|malloc
@@ -2326,6 +2339,33 @@ name|s
 operator|=
 name|splimp
 argument_list|()
+expr_stmt|;
+comment|/* 	 * Enable bus mastering. 	 */
+name|val
+operator|=
+name|pci_conf_read
+argument_list|(
+name|config_id
+argument_list|,
+name|PCI_COMMAND_STATUS_REG
+argument_list|)
+expr_stmt|;
+name|val
+operator||=
+operator|(
+name|PCIM_CMD_MEMEN
+operator||
+name|PCIM_CMD_BUSMASTEREN
+operator|)
+expr_stmt|;
+name|pci_conf_write
+argument_list|(
+name|config_id
+argument_list|,
+name|PCI_COMMAND_STATUS_REG
+argument_list|,
+name|val
+argument_list|)
 expr_stmt|;
 comment|/* 	 * Map control/status registers. 	 */
 if|if
