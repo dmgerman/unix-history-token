ace7572055c549c823b18d9c8334c6f1ac5a0167
Matthew Jacob <mjacob@FreeBSD.org>
1999-03-25 19:47:33 +0000

    roll internal tag level
    Former-commit-id: 4b595abe440d8914a015cb5c60a604953f46ee56

--- sys/dev/isp/asm_sbus.h
+++ sys/dev/isp/asm_sbus.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $Id: asm_sbus.h,v 1.5 1999/02/09 01:05:41 mjacob Exp $ */
+comment|/* $Id: asm_sbus.h,v 1.6 1999/03/17 05:04:38 mjacob Exp $ */
 end_comment
 
 begin_comment
-comment|/* release_03_16_99 */
+comment|/* release_03_25_99 */
 end_comment
 
 begin_comment
