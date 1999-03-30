06038f7cde691b795c1e91dca20b6039b61c3a17
cvs2svn <cvs2svn@FreeBSD.org>
1999-03-30 07:36:37 +0000

    This commit was manufactured by cvs2svn to create branch 'VENDOR-gcc'.
    Former-commit-id: f5be10a3f2aa85e65acb42ce49e8dd5c96261931

---
+++ contrib/gcc/config/alpha/gdb.h
@@ -0,0 +1,26 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* Definitions of target machine for GNU compiler, for DEC Alpha, using    encapsulated stabs.    Copyright (C) 1992, 1993 Free Software Foundation, Inc.    Contributed by Peter Schauer (pes@regent.e-technik.tu-muenchen.de).  This file is part of GNU CC.  GNU CC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GNU CC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GNU CC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|"alpha/alpha.h"
+end_include
+
+begin_undef
+undef|#
+directive|undef
+name|PREFERRED_DEBUGGING_TYPE
+end_undef
+
+begin_define
+define|#
+directive|define
+name|PREFERRED_DEBUGGING_TYPE
+value|DBX_DEBUG
+end_define
+
+end_unit
+
