be2f8e13941e9164b57f4a38c6569e22cd9ef25b
Peter Wemm <peter@FreeBSD.org>
1999-04-05 15:43:11 +0000

    Make ppbus compile under egcs.  I'm not sure about this casting-to-union- -containing-the-type stuff, but gcc seemed to do it implicitly..
    Former-commit-id: d01bbeb5cd29d78269283040e115454ea4e13186

--- sys/dev/ppbus/vpoio.c
+++ sys/dev/ppbus/vpoio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Nicolas Souchu  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: vpoio.c,v 1.5 1999/01/10 12:04:55 nsouch Exp $  *  */
+comment|/*-  * Copyright (c) 1998 Nicolas Souchu  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: vpoio.c,v 1.6 1999/01/30 15:35:39 nsouch Exp $  *  */
 end_comment
 
 begin_ifdef
@@ -307,7 +307,7 @@ begin_define
 define|#
 directive|define
 name|INIT_TRIG_MICROSEQ
-value|{						\ 	int i;								\ 	for (i=1; i<= 7; i+=2) {					\ 		disconnect_microseq[i].arg[2] = (void *)d_pulse;	\ 		connect_epp_microseq[i].arg[2] = 			\ 		connect_spp_microseq[i].arg[2] = (void *)c_pulse;	\ 	}								\ }
+value|{						\ 	int i;								\ 	for (i=1; i<= 7; i+=2) {					\ 		disconnect_microseq[i].arg[2] = (union ppb_insarg)d_pulse; \ 		connect_epp_microseq[i].arg[2] = 			\ 		connect_spp_microseq[i].arg[2] = (union ppb_insarg)c_pulse; \ 	}								\ }
 end_define
 
 begin_define
@@ -1730,10 +1730,22 @@ name|vpo_dev
 argument_list|,
 name|MS_OP_PUT
 argument_list|,
+operator|(
+expr|union
+name|ppb_insarg
+operator|)
 name|buffer
 argument_list|,
+operator|(
+expr|union
+name|ppb_insarg
+operator|)
 name|size
 argument_list|,
+operator|(
+expr|union
+name|ppb_insarg
+operator|)
 name|MS_UNKNOWN
 argument_list|,
 operator|&
@@ -1799,10 +1811,22 @@ name|vpo_dev
 argument_list|,
 name|MS_OP_GET
 argument_list|,
+operator|(
+expr|union
+name|ppb_insarg
+operator|)
 name|buffer
 argument_list|,
+operator|(
+expr|union
+name|ppb_insarg
+operator|)
 name|size
 argument_list|,
+operator|(
+expr|union
+name|ppb_insarg
+operator|)
 name|MS_UNKNOWN
 argument_list|,
 operator|&
