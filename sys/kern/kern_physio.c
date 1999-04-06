3349684568949aa599eea112075fbd84baf1d115
Peter Wemm <peter@FreeBSD.org>
1999-04-06 03:04:47 +0000

    Use the reference counted PHOLD()/PRELE() rather than P_PHYSIO.
    Former-commit-id: cc25587d2f88b6ce33a939bc7024ed6e67c9d343

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: kern_physio.c,v 1.29 1998/10/25 17:44:51 phk Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: kern_physio.c,v 1.30 1999/01/21 08:29:04 dillon Exp $  */
 end_comment
 
 begin_include
@@ -178,12 +178,11 @@ name|buf
 modifier|*
 name|bpa
 decl_stmt|;
-comment|/*  * keep the process from being swapped  */
+comment|/* 	 * Keep the process UPAGES from being swapped. (XXX for performance?) 	 */
+name|PHOLD
+argument_list|(
 name|curproc
-operator|->
-name|p_flag
-operator||=
-name|P_PHYSIO
+argument_list|)
 expr_stmt|;
 comment|/* create and build a buffer header for a transfer */
 name|bpa
@@ -690,13 +689,11 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-comment|/*  * allow the process to be swapped  */
+comment|/* 	 * Allow the process UPAGES to be swapped again. 	 */
+name|PRELE
+argument_list|(
 name|curproc
-operator|->
-name|p_flag
-operator|&=
-operator|~
-name|P_PHYSIO
+argument_list|)
 expr_stmt|;
 return|return
 operator|(
