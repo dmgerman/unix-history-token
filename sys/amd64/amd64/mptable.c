b30d63512cc07d32ad2172991405859abadfbe19
Peter Wemm <peter@FreeBSD.org>
1999-04-07 17:08:40 +0000

    Disable the mtrr copy calls, it doesn't work with the i686_mem.c stuff. This should make it compile/link again.
    Former-commit-id: bdee3ebbcbdd9eba7c606f0b442b668b437474ea

--- sys/amd64/amd64/mptable.c
+++ sys/amd64/amd64/mptable.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.92 1999/02/26 03:42:50 tegge Exp $  */
+comment|/*  * Copyright (c) 1996, by Steve Passe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. The name of the developer may NOT be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: mp_machdep.c,v 1.93 1999/03/05 16:38:10 bde Exp $  */
 end_comment
 
 begin_include
@@ -2177,12 +2177,12 @@ operator|)
 name|PTD
 argument_list|)
 expr_stmt|;
-name|putmtrr
-argument_list|()
-expr_stmt|;
-name|pmap_setvidram
-argument_list|()
-expr_stmt|;
+if|#
+directive|if
+literal|0
+block|putmtrr(); 	pmap_setvidram();
+endif|#
+directive|endif
 name|invltlb
 argument_list|()
 expr_stmt|;
@@ -2344,12 +2344,12 @@ decl_stmt|;
 endif|#
 directive|endif
 comment|/* APIC_IO */
-name|getmtrr
-argument_list|()
-expr_stmt|;
-name|pmap_setvidram
-argument_list|()
-expr_stmt|;
+if|#
+directive|if
+literal|0
+block|getmtrr(); 	pmap_setvidram();
+endif|#
+directive|endif
 name|POSTCODE
 argument_list|(
 name|MP_ENABLE_POST
@@ -9174,9 +9174,12 @@ literal|"cpuid mismatch! boom!!"
 argument_list|)
 expr_stmt|;
 block|}
-name|getmtrr
-argument_list|()
-expr_stmt|;
+if|#
+directive|if
+literal|0
+block|getmtrr();
+endif|#
+directive|endif
 comment|/* Init local apic for irq's */
 name|apic_initialize
 argument_list|()
