5e6f5177dcc818832eb0839588ef9f217fc728ef
Hidetoshi Shimokawa <simokawa@FreeBSD.org>
1999-04-11 12:48:15 +0000

    Control unaligned access handling via sysctl.
    Former-commit-id: 641816b94a4fab52e7116e4c097c126682245e22

--- sys/alpha/alpha/machdep.c
+++ sys/alpha/alpha/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: machdep.c,v 1.34 1999/03/06 04:46:10 wollman Exp $  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: machdep.c,v 1.35 1999/04/03 22:19:50 jdp Exp $  */
 end_comment
 
 begin_comment
@@ -529,15 +529,9 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* for cpu_sysctl() */
-end_comment
-
 begin_decl_stmt
 name|int
 name|alpha_unaligned_print
-init|=
-literal|1
 decl_stmt|;
 end_decl_stmt
 
@@ -548,8 +542,6 @@ end_comment
 begin_decl_stmt
 name|int
 name|alpha_unaligned_fix
-init|=
-literal|1
 decl_stmt|;
 end_decl_stmt
 
@@ -560,14 +552,75 @@ end_comment
 begin_decl_stmt
 name|int
 name|alpha_unaligned_sigbus
-init|=
-literal|0
 decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/* don't SIGBUS on fixed-up accesses */
+comment|/* SIGBUS on fixed-up accesses */
 end_comment
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_machdep
+argument_list|,
+name|CPU_UNALIGNED_PRINT
+argument_list|,
+name|unaligned_print
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|alpha_unaligned_print
+argument_list|,
+literal|1
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_machdep
+argument_list|,
+name|CPU_UNALIGNED_FIX
+argument_list|,
+name|unaligned_fix
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|alpha_unaligned_fix
+argument_list|,
+literal|1
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_machdep
+argument_list|,
+name|CPU_UNALIGNED_SIGBUS
+argument_list|,
+name|unaligned_sigbus
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|alpha_unaligned_sigbus
+argument_list|,
+literal|0
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_decl_stmt
 specifier|static
