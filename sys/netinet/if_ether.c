c734a7b0b4ca4dbd1a64c1785008648b9c8fe512
Eivind Eklund <eivind@FreeBSD.org>
1999-04-15 17:58:24 +0000

    Better handling for ARP/source routing on Token Ring
    Submitted by:	Larry Lile <lile@stdio.com>
    
    
    Former-commit-id: a4ed31068e26b55504a0a564e66bd3d1e3aaef96

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ether.c	8.1 (Berkeley) 6/10/93  * $Id: if_ether.c,v 1.55 1999/03/04 04:03:57 wpaul Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)if_ether.c	8.1 (Berkeley) 6/10/93  * $Id: if_ether.c,v 1.56 1999/03/10 10:11:43 julian Exp $  */
 end_comment
 
 begin_comment
@@ -3138,17 +3138,16 @@ name|sdl_rcf
 operator|=
 name|NULL
 expr_stmt|;
-comment|/* Save source routing information for Token-ring interfaces, if available */
+comment|/* 		 * If we receive an arp from a token-ring station over 		 * a token-ring nic then try to save the source 		 * routing info. 		 */
 if|if
 condition|(
-name|ea
+name|ac
 operator|->
-name|arp_hrd
+name|ac_if
+operator|.
+name|if_type
 operator|==
-name|htons
-argument_list|(
-name|ARPHRD_IEEE802
-argument_list|)
+name|IFT_ISO88025
 condition|)
 block|{
 name|th
@@ -3357,6 +3356,15 @@ operator|=
 name|sdl
 operator|->
 name|sdl_rcf
+expr_stmt|;
+block|}
+else|else
+block|{
+name|sdl
+operator|->
+name|sdl_rcf
+operator|=
+name|NULL
 expr_stmt|;
 block|}
 if|if
@@ -3807,16 +3815,15 @@ expr_stmt|;
 comment|/* let's be sure! */
 switch|switch
 condition|(
-name|ntohs
-argument_list|(
-name|ea
+name|ac
 operator|->
-name|arp_hrd
-argument_list|)
+name|ac_if
+operator|.
+name|if_type
 condition|)
 block|{
 case|case
-name|ARPHRD_IEEE802
+name|IFT_ISO88025
 case|:
 comment|/* Re-arrange the source/dest address */
 name|memcpy
@@ -3967,7 +3974,7 @@ literal|0x40
 expr_stmt|;
 break|break;
 case|case
-name|ARPHRD_ETHER
+name|IFT_ETHER
 case|:
 name|eh
 operator|=
