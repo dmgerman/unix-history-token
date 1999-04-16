28ecc98a4ddc5fb47c3a4cb397708e13aa6285e8
Peter Wemm <peter@FreeBSD.org>
1999-04-16 21:28:10 +0000

    Corresponding minimal changes for kernel configuration after new-bus commit.
    Former-commit-id: b2a29bcf94bb13c7fcb7e8ee33ffe3b3b206c784

--- usr.sbin/config/configvers.h
+++ usr.sbin/config/configvers.h
@@ -1,13 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * 6 digits of version.  The most significant are branch indicators  * (eg: RELENG_2_2 = 22, -current presently = 30 etc). The least  * significant digits are incremented for each incompatable change.  *  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE  * and<osreldate.h> system.  *  * $Id: configvers.h,v 1.10 1999/04/13 18:22:57 peter Exp $  */
+comment|/*  * 6 digits of version.  The most significant are branch indicators  * (eg: RELENG_2_2 = 22, -current presently = 30 etc). The least  * significant digits are incremented for each incompatable change.  *  * The numbering scheme is inspired by the sys/conf/newvers.sh RELDATE  * and<osreldate.h> system.  *  * $Id: configvers.h,v 1.11 1999/04/15 14:52:22 bde Exp $  */
 end_comment
 
 begin_define
 define|#
 directive|define
 name|CONFIGVERS
-value|300012
+value|400013
 end_define
 
 end_unit
