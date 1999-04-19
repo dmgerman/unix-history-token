e4ed0dac78f62e7ac70a6fa130f404becfd0b3f7
Doug Rabson <dfr@FreeBSD.org>
1999-04-19 08:42:39 +0000

    Fix some confusion between physical and virtual addresses when recording the driver's choice of memory window.
    Former-commit-id: ef7504b158e3680e1ebc8bccb2afb42769deb09a

--- sys/i386/isa/isa_compat.c
+++ sys/i386/isa/isa_compat.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: isa_compat.c,v 1.1 1999/04/16 21:22:23 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: isa_compat.c,v 1.2 1999/04/17 09:56:35 bde Exp $  */
 end_comment
 
 begin_include
@@ -49,6 +49,30 @@ begin_include
 include|#
 directive|include
 file|<sys/rman.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/vmparam.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/vm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/pmap.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/pmap.h>
 end_include
 
 begin_include
@@ -672,6 +696,10 @@ block|{
 name|int
 name|portsize
 decl_stmt|;
+name|void
+modifier|*
+name|maddr
+decl_stmt|;
 name|isa_compat_alloc_resources
 argument_list|(
 name|dev
@@ -686,9 +714,7 @@ name|res
 operator|.
 name|memory
 condition|)
-name|dvp
-operator|->
-name|id_maddr
+name|maddr
 operator|=
 name|rman_get_virtual
 argument_list|(
@@ -698,11 +724,15 @@ name|memory
 argument_list|)
 expr_stmt|;
 else|else
+name|maddr
+operator|=
+literal|0
+expr_stmt|;
 name|dvp
 operator|->
 name|id_maddr
 operator|=
-literal|0
+name|maddr
 expr_stmt|;
 name|portsize
 operator|=
@@ -818,14 +848,7 @@ name|dvp
 operator|->
 name|id_maddr
 operator|!=
-operator|(
-name|void
-operator|*
-operator|)
-name|isa_get_maddr
-argument_list|(
-name|dev
-argument_list|)
+name|maddr
 condition|)
 name|isa_set_maddr
 argument_list|(
@@ -837,6 +860,8 @@ operator|)
 name|dvp
 operator|->
 name|id_maddr
+operator|-
+name|KERNBASE
 argument_list|)
 expr_stmt|;
 if|if
