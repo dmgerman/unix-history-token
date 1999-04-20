57ba590ecde046cd8d2aac73e415599e23db74b8
Dmitrij Tejblum <dt@FreeBSD.org>
1999-04-20 19:54:20 +0000

    MFC rev.1.206: fix lock up in getnewbuf() with interruptible NFS.
    Former-commit-id: e9afa31917ac761530cc0bd0e5acbfced1aec890

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.193.2.3 1999/01/31 05:30:12 dillon Exp $  */
+comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.193.2.4 1999/03/03 17:43:17 julian Exp $  */
 end_comment
 
 begin_comment
@@ -4442,6 +4442,8 @@ operator||=
 name|VFS_BIO_NEED_ANY
 expr_stmt|;
 do|do
+if|if
+condition|(
 name|tsleep
 argument_list|(
 operator|&
@@ -4459,7 +4461,12 @@ literal|"newbuf"
 argument_list|,
 name|slptimeo
 argument_list|)
-expr_stmt|;
+condition|)
+return|return
+operator|(
+name|NULL
+operator|)
+return|;
 do|while
 condition|(
 name|needsbuffer
