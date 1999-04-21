5dc29950e20eb1fa23451c51b377e21c0f724be0
Brian Somers <brian@FreeBSD.org>
1999-04-21 08:03:35 +0000

    If ioctl TIOCMGET fails, continue.  The device may be a pseudo device created by an interactive version of rlogin/telnet/ssh & friends
    Former-commit-id: 63f6c1bc2f995f2c5cd79403288d82d18b028c18

--- usr.sbin/ppp/modem.c
+++ usr.sbin/ppp/modem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP Modem handling module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: modem.c,v 1.108 1999/04/03 11:54:00 brian Exp $  *  *  TODO:  */
+comment|/*  *		PPP Modem handling module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: modem.c,v 1.109 1999/04/03 12:01:38 brian Exp $  *  *  TODO:  */
 end_comment
 
 begin_include
@@ -1469,28 +1469,28 @@ name|log_Printf
 argument_list|(
 name|LogPHASE
 argument_list|,
-literal|"%s: ioctl error (%s)!\n"
+literal|"%s: Carrier not required (pseudo tty ?)\n"
 argument_list|,
 name|modem
 operator|->
 name|link
 operator|.
 name|name
-argument_list|,
-name|strerror
-argument_list|(
-name|errno
-argument_list|)
 argument_list|)
 expr_stmt|;
-name|datalink_Down
+name|timer_Stop
 argument_list|(
+operator|&
 name|modem
 operator|->
-name|dl
-argument_list|,
-name|CLOSE_NORMAL
+name|Timer
 argument_list|)
+expr_stmt|;
+name|modem
+operator|->
+name|mbits
+operator|=
+name|TIOCM_CD
 expr_stmt|;
 return|return;
 block|}
