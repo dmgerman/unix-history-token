3aa5509807c48671033708b1f3b9ddc9dc813c59
John Hay <jhay@FreeBSD.org>
1999-04-22 14:34:53 +0000

    Fix the display of the "nice" value of processes like ntpd that use the posix sched_setscheduler() to set their priority.
    Noticed by:	Mark Allwright <mallwri@orion.didata.co.za>
    
    
    Former-commit-id: 3e1c8183c7ce16ebe9e59dcf5d2a7364891a5c00

--- usr.bin/top/machine.c
+++ usr.bin/top/machine.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * top - a top users display for Unix  *  * SYNOPSIS:  For FreeBSD-2.x system  *  * DESCRIPTION:  * Originally written for BSD4.4 system by Christos Zoulas.  * Ported to FreeBSD 2.x by Steven Wallace&& Wolfram Schneider  * Order support hacked in from top-3.5beta6/machine/m_aix41.c  *   by Monte Mitzelfelt (for latest top see http://www.groupsys.com/topinfo/)  *  * This is the machine-dependent module for FreeBSD 2.2  * Works for:  *	FreeBSD 2.2, and probably FreeBSD 2.1.x  *  * LIBS: -lkvm  *  * AUTHOR:  Christos Zoulas<christos@ee.cornell.edu>  *          Steven Wallace<swallace@freebsd.org>  *          Wolfram Schneider<wosch@FreeBSD.org>  *  * $Id: machine.c,v 1.22 1999/03/05 16:38:13 bde Exp $  */
+comment|/*  * top - a top users display for Unix  *  * SYNOPSIS:  For FreeBSD-2.x system  *  * DESCRIPTION:  * Originally written for BSD4.4 system by Christos Zoulas.  * Ported to FreeBSD 2.x by Steven Wallace&& Wolfram Schneider  * Order support hacked in from top-3.5beta6/machine/m_aix41.c  *   by Monte Mitzelfelt (for latest top see http://www.groupsys.com/topinfo/)  *  * This is the machine-dependent module for FreeBSD 2.2  * Works for:  *	FreeBSD 2.2, and probably FreeBSD 2.1.x  *  * LIBS: -lkvm  *  * AUTHOR:  Christos Zoulas<christos@ee.cornell.edu>  *          Steven Wallace<swallace@freebsd.org>  *          Wolfram Schneider<wosch@FreeBSD.org>  *  * $Id: machine.c,v 1.23 1999/03/07 06:55:47 bde Exp $  */
 end_comment
 
 begin_include
@@ -2929,6 +2929,8 @@ operator|-
 name|NZERO
 else|:
 operator|(
+name|RTP_PRIO_IS_REALTIME
+argument_list|(
 name|PP
 argument_list|(
 name|pp
@@ -2937,8 +2939,7 @@ name|p_rtprio
 operator|.
 name|type
 argument_list|)
-operator|==
-name|RTP_PRIO_REALTIME
+argument_list|)
 condition|?
 operator|(
 name|PRIO_MIN
