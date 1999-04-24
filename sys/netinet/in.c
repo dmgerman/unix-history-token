d771a077be4839ca2da9a8878ea8ed65e75d7758
Luigi Rizzo <luigi@FreeBSD.org>
1999-04-24 12:28:51 +0000

    postpone the sending of IGMP LEAVE msg to after deleting the mc address from the address list. The latter operation on some hardware resets the card, potentially canceling the pending LEAVE pkt.
    Former-commit-id: 2f4f1c04a8cc2af17e6ca866c8fe826fc3c6a896

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)in.c	8.4 (Berkeley) 1/9/95  *	$Id: in.c,v 1.38 1998/06/07 17:12:13 dfr Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)in.c	8.4 (Berkeley) 1/9/95  *	$Id: in.c,v 1.39 1998/12/07 05:41:10 eivind Exp $  */
 end_comment
 
 begin_include
@@ -2807,12 +2807,23 @@ name|inm
 operator|->
 name|inm_ifma
 decl_stmt|;
+name|struct
+name|in_multi
+name|my_inm
+decl_stmt|;
 name|int
 name|s
 init|=
 name|splnet
 argument_list|()
 decl_stmt|;
+name|my_inm
+operator|.
+name|inm_ifp
+operator|=
+name|NULL
+expr_stmt|;
+comment|/* don't send the leave msg */
 if|if
 condition|(
 name|ifma
@@ -2822,11 +2833,11 @@ operator|==
 literal|1
 condition|)
 block|{
-comment|/* 		 * No remaining claims to this record; let IGMP know that 		 * we are leaving the multicast group. 		 */
-name|igmp_leavegroup
-argument_list|(
+comment|/* 		 * No remaining claims to this record; let IGMP know that 		 * we are leaving the multicast group. 		 * But do it after the if_delmulti() which might reset 		 * the interface and nuke the packet. 		 */
+name|my_inm
+operator|=
+operator|*
 name|inm
-argument_list|)
 expr_stmt|;
 name|ifma
 operator|->
@@ -2859,6 +2870,20 @@ argument_list|,
 name|ifma
 operator|->
 name|ifma_addr
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|my_inm
+operator|.
+name|inm_ifp
+operator|!=
+name|NULL
+condition|)
+name|igmp_leavegroup
+argument_list|(
+operator|&
+name|my_inm
 argument_list|)
 expr_stmt|;
 name|splx
