54ed0a1dec6005f09072a88de57fe854a1590e5d
Luigi Rizzo <luigi@FreeBSD.org>
1999-04-26 14:59:02 +0000

    MFC: make one pass through the firewall the default.
    Former-commit-id: 2f286fa803f76a339a7dc1ded7e147fc1913a47f

--- sys/netinet/ip_fw.c
+++ sys/netinet/ip_fw.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  * Copyright (c) 1996 Alex Nash  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.103.2.1 1999/03/31 14:10:59 nsayer Exp $  */
+comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  * Copyright (c) 1996 Alex Nash  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.c,v 1.103.2.2 1999/04/01 14:56:17 nsayer Exp $  */
 end_comment
 
 begin_comment
@@ -274,13 +274,9 @@ specifier|static
 name|int
 name|fw_one_pass
 init|=
-literal|0
+literal|1
 decl_stmt|;
 end_decl_stmt
-
-begin_comment
-comment|/* XXX */
-end_comment
 
 begin_ifdef
 ifdef|#
