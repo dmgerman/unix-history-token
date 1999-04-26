6cbac5f4d0be2490300805c88fc1e716e915ecc4
Brian Somers <brian@FreeBSD.org>
1999-04-26 08:53:52 +0000

    Add support for NetBSD (history() from libedit is different).
    Former-commit-id: b3a95328d0477ad00387ac40e685a2c2a71d69cd

--- usr.sbin/pppctl/pppctl.c
+++ usr.sbin/pppctl/pppctl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Brian Somers<brian@Awfulhak.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: pppctl.c,v 1.16 1998/03/22 00:43:04 brian Exp $  */
+comment|/*-  * Copyright (c) 1997 Brian Somers<brian@Awfulhak.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: pppctl.c,v 1.17 1999/01/31 12:24:29 brian Exp $  */
 end_comment
 
 begin_include
@@ -1864,6 +1864,22 @@ name|size
 operator|=
 literal|20
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|__NetBSD__
+name|history
+argument_list|(
+name|hist
+argument_list|,
+name|NULL
+argument_list|,
+name|H_SETSIZE
+argument_list|,
+name|size
+argument_list|)
+expr_stmt|;
+else|#
+directive|else
 name|history
 argument_list|(
 name|hist
@@ -1873,6 +1889,8 @@ argument_list|,
 name|size
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 name|edit
 operator|=
 name|el_init
@@ -2000,6 +2018,22 @@ name|len
 operator|>
 literal|1
 condition|)
+ifdef|#
+directive|ifdef
+name|__NetBSD__
+name|history
+argument_list|(
+name|hist
+argument_list|,
+name|NULL
+argument_list|,
+name|H_ENTER
+argument_list|,
+name|l
+argument_list|)
+expr_stmt|;
+else|#
+directive|else
 name|history
 argument_list|(
 name|hist
@@ -2009,6 +2043,8 @@ argument_list|,
 name|l
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 name|write
 argument_list|(
 name|fd
