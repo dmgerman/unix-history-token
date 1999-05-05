528658e8fc7fbcdde07ded6efdb8edd0b724dcd0
Mark Murray <markm@FreeBSD.org>
1999-05-05 20:28:54 +0000

    Incomplete. Back out until I can revisit.
    Former-commit-id: 52e95de5b3207bce554412c730e6c38b06ec4037

--- sbin/routed/rtquery/rtquery.c
+++ sbin/routed/rtquery/rtquery.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgment:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: rtquery.c,v 1.10 1999/05/02 13:14:16 markm Exp $  */
+comment|/*-  * Copyright (c) 1982, 1986, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgment:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id$  */
 end_comment
 
 begin_decl_stmt
@@ -65,12 +65,6 @@ begin_include
 include|#
 directive|include
 file|<arpa/inet.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<err.h>
 end_include
 
 begin_include
@@ -187,7 +181,7 @@ directive|endif
 end_endif
 
 begin_empty
-empty|#ident "$Revision: 1.10 $"
+empty|#ident "$Revision$"
 end_empty
 
 begin_ifndef
@@ -1355,11 +1349,14 @@ operator|<
 literal|0
 condition|)
 block|{
-name|err
+name|perror
+argument_list|(
+literal|"socket"
+argument_list|)
+expr_stmt|;
+name|exit
 argument_list|(
 literal|2
-argument_list|,
-literal|"socket"
 argument_list|)
 expr_stmt|;
 block|}
@@ -1409,7 +1406,7 @@ operator|*
 literal|1024
 condition|)
 block|{
-name|warn
+name|perror
 argument_list|(
 literal|"setsockopt SO_RCVBUF"
 argument_list|)
@@ -1614,11 +1611,14 @@ operator|==
 literal|0
 condition|)
 block|{
-name|err
+name|perror
+argument_list|(
+literal|"bind"
+argument_list|)
+expr_stmt|;
+name|exit
 argument_list|(
 literal|2
-argument_list|,
-literal|"bind"
 argument_list|)
 expr_stmt|;
 block|}
@@ -2160,11 +2160,14 @@ operator|<
 literal|0
 condition|)
 block|{
-name|err
+name|perror
+argument_list|(
+literal|"recvfrom"
+argument_list|)
+expr_stmt|;
+name|exit
 argument_list|(
 literal|1
-argument_list|,
-literal|"recvfrom"
 argument_list|)
 expr_stmt|;
 block|}
@@ -2286,11 +2289,14 @@ operator|==
 name|EINTR
 condition|)
 continue|continue;
-name|err
+name|perror
+argument_list|(
+literal|"select"
+argument_list|)
+expr_stmt|;
+name|exit
 argument_list|(
 literal|1
-argument_list|,
-literal|"select"
 argument_list|)
 expr_stmt|;
 block|}
@@ -2336,11 +2342,14 @@ operator|<
 literal|0
 condition|)
 block|{
-name|err
+name|perror
+argument_list|(
+literal|"gettimeofday(now)"
+argument_list|)
+expr_stmt|;
+name|exit
 argument_list|(
 literal|1
-argument_list|,
-literal|"gettimeofday(now)"
 argument_list|)
 expr_stmt|;
 block|}
@@ -2410,14 +2419,15 @@ operator|<
 literal|0
 condition|)
 block|{
-name|err
+name|perror
 argument_list|(
-operator|-
-literal|1
-argument_list|,
 literal|"gettimeofday(sent)"
 argument_list|)
 expr_stmt|;
+return|return
+operator|-
+literal|1
+return|;
 block|}
 name|memset
 argument_list|(
@@ -2549,14 +2559,15 @@ operator|<
 literal|0
 condition|)
 block|{
-name|err
+name|perror
 argument_list|(
-operator|-
-literal|1
-argument_list|,
 name|host
 argument_list|)
 expr_stmt|;
+return|return
+operator|-
+literal|1
+return|;
 block|}
 return|return
 literal|0
