5b0949d0b19d91cd35ebd3f82f34c53bfb8071e8
Bill Paul <wpaul@FreeBSD.org>
1999-05-06 15:32:52 +0000

    Tweak the Macronix driver to hopefully make it more reliable:
    - Change to the same transmit scheme as the PNIC driver.
    - Dynamically set the cache alignment, and set burst size the same as
      the PNIC driver in mx_init().
    - Enable 'store and forward' mode by default. This is the slowest option
      and it does reduce 100Mbps performance somewhat, but it's the most
      reliable setting I can find. I'm more interested in having the driver
      work reliably than trying to squeeze the best performance out of it.
      The reason I'm doing this is that on *some* systems you may see a lot
      of transmit underruns (which I can't explain: these are *fast* test
      systems) and these errors seem to cause unusual and decidedly
      non-tulip-like behavior. In normal 10Mbps mode, performance is fine
      (you can easily saturate a 10Mbps link).
    
    Also tweak some of the other drivers:
    
    - Increase the size of the TX ring for the Winbond, ASIX, VIA Rhine
      and PNIC drivers.
    - Set a larger value for ifq_maxlen in the ThunderLAN driver. The setting
      of TL_TX_LIST_CNT - 1 is too low (the ThunderLAN driver only allocates
      20 transmit descriptors, and I don't want to fiddle with that now
      because the ThunderLAN's descriptor structure is an oddball size
      compared to the others).
    
    
    Former-commit-id: 7b47ce5936345d4a66d9aad1becdc6cf39f83676

--- sys/pci/if_mxreg.h
+++ sys/pci/if_mxreg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_mxreg.h,v 1.16 1999/04/08 17:35:38 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_mxreg.h,v 1.17 1999/05/06 15:04:00 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -311,8 +311,15 @@ end_define
 begin_define
 define|#
 directive|define
-name|MX_BUSCTL_XMITPOLL
-value|0x00060000
+name|MX_BUSCTL_TXPOLL
+value|0x000E0000
+end_define
+
+begin_define
+define|#
+directive|define
+name|MX_BUSCTL_MUSTBEONE
+value|0x04000000
 end_define
 
 begin_define
@@ -348,6 +355,13 @@ define|#
 directive|define
 name|MX_SKIPLEN_5LONG
 value|0x00000040
+end_define
+
+begin_define
+define|#
+directive|define
+name|MX_CACHEALIGN_NONE
+value|0x00000000
 end_define
 
 begin_define
@@ -2061,7 +2075,7 @@ begin_define
 define|#
 directive|define
 name|MX_TX_LIST_CNT
-value|64
+value|128
 end_define
 
 begin_define
@@ -2138,13 +2152,6 @@ parameter_list|(
 name|x
 parameter_list|)
 value|x->mx_ptr->mx_frag[0].mx_status
-end_define
-
-begin_define
-define|#
-directive|define
-name|MX_UNSENT
-value|0x12341234
 end_define
 
 begin_struct
@@ -2434,7 +2441,7 @@ name|u_int8_t
 name|mx_autoneg
 decl_stmt|;
 name|u_int8_t
-name|mx_singlebuf
+name|mx_cachesize
 decl_stmt|;
 name|caddr_t
 name|mx_ldata_ptr
@@ -2739,6 +2746,13 @@ define|#
 directive|define
 name|MX_PCI_CLASSCODE
 value|0x09
+end_define
+
+begin_define
+define|#
+directive|define
+name|MX_PCI_CACHELEN
+value|0x0C
 end_define
 
 begin_define
