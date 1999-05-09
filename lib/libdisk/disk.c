f9c05b5cbfc2fcd4ea5b8933de0f7957a4d47af4
Doug Rabson <dfr@FreeBSD.org>
1999-05-09 11:36:22 +0000

    MFC: More disklabel tweaks for sysinstall
    Former-commit-id: 4411b78a9f02350108db57bf816b681cc1c13834

--- lib/libdisk/disk.c
+++ lib/libdisk/disk.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: disk.c,v 1.39.2.1 1999/05/08 19:10:14 dfr Exp $  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@login.dknet.dk> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: disk.c,v 1.39.2.2 1999/05/08 21:24:04 dfr Exp $  *  */
 end_comment
 
 begin_include
@@ -1197,13 +1197,6 @@ argument_list|(
 name|pname
 argument_list|,
 name|name
-argument_list|)
-expr_stmt|;
-name|strcat
-argument_list|(
-name|pname
-argument_list|,
-literal|"c"
 argument_list|)
 expr_stmt|;
 name|j
