3f46d9d4d9c02fc6bfdfc5c0e26f1447d40c2d59
Dag-Erling Smorgrav <des@FreeBSD.org>
1999-05-12 06:57:07 +0000

    Brucify.
    Former-commit-id: d4bd7f7171c4c5385d9cd232413d01c408f34a03

--- usr.bin/touch/touch.c
+++ usr.bin/touch/touch.c
@@ -1669,8 +1669,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: touch [-acfm] [-r file]"
-literal|" [-t [[CC]YY]MMDDhhmm[.SS]] file ...\n"
+literal|"usage: touch [-acfm] [-r file] [-t [[CC]YY]MMDDhhmm[.SS]] file ...\n"
 argument_list|)
 expr_stmt|;
 name|exit
