ee167882d4627f2292afc2af12500e96b77349ae
Brian Somers <brian@FreeBSD.org>
1999-05-14 09:35:51 +0000

    Ensure that we're not going to overflow our ``struct tun'' when we mbuf_Read() into it. Add the link name to a few diagnostics.
    Former-commit-id: bd72b04b0e4abd7d37c6d6a226f8f3acf499eac3

--- usr.sbin/ppp/ip.c
+++ usr.sbin/ppp/ip.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *		PPP IP Protocol Interface  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ip.c,v 1.59 1999/05/08 11:06:42 brian Exp $  *  *	TODO:  *		o Return ICMP message for filterd packet  *		  and optionaly record it into log.  */
+comment|/*  *		PPP IP Protocol Interface  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: ip.c,v 1.60 1999/05/09 20:02:19 brian Exp $  *  *	TODO:  *		o Return ICMP message for filterd packet  *		  and optionaly record it into log.  */
 end_comment
 
 begin_include
@@ -2483,6 +2483,48 @@ argument_list|(
 name|bp
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|nb
+operator|>
+sizeof|sizeof
+name|tun
+operator|.
+name|data
+condition|)
+block|{
+name|log_Printf
+argument_list|(
+name|LogWARN
+argument_list|,
+literal|"ip_Input: %s: Packet too large (got %d, max %d)\n"
+argument_list|,
+name|l
+operator|->
+name|name
+argument_list|,
+name|nb
+argument_list|,
+call|(
+name|int
+call|)
+argument_list|(
+sizeof|sizeof
+name|tun
+operator|.
+name|data
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|mbuf_Free
+argument_list|(
+name|bp
+argument_list|)
+expr_stmt|;
+return|return
+name|NULL
+return|;
+block|}
 name|mbuf_Read
 argument_list|(
 name|bp
@@ -2610,7 +2652,11 @@ name|log_Printf
 argument_list|(
 name|LogERROR
 argument_list|,
-literal|"ip_Input: wrote %d, got %s\n"
+literal|"ip_Input: %s: wrote %d, got %s\n"
+argument_list|,
+name|l
+operator|->
+name|name
 argument_list|,
 name|nb
 argument_list|,
@@ -2625,7 +2671,11 @@ name|log_Printf
 argument_list|(
 name|LogERROR
 argument_list|,
-literal|"ip_Input: wrote %d, got %d\n"
+literal|"ip_Input: %s: wrote %d, got %d\n"
+argument_list|,
+name|l
+operator|->
+name|name
 argument_list|,
 name|nb
 argument_list|,
