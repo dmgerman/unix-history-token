e74730c9042004b63a0326858506d40080e55e76
David E. O'Brien <obrien@FreeBSD.org>
1999-05-14 01:53:32 +0000

    Virgin import of Xircom PCCARD driver v1.16.
    v1.16 (1999/03/08) fixed BPF input hang and infinite loop on CE2
    short-packet output.
    
    Developed by:	Scott Mitchell <scott@uk.freebsd.org>
    Obtained from:	http://www.freebsd-uk.eu.org/~scott/xe_drv/
    
    
    Former-commit-id: 9d0a0975b05f270754edafb56694e4cace6a2bb0

--- sys/dev/xe/if_xe.c
+++ sys/dev/xe/if_xe.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998, 1999 Scott Mitchell  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: if_xe.c,v 1.15 1999/03/05 12:11:40 root Exp $  */
+comment|/*-  * Copyright (c) 1998, 1999 Scott Mitchell  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: if_xe.c,v 1.16 1999/03/08 16:28:50 root Exp $  */
 end_comment
 
 begin_comment
@@ -4554,6 +4554,7 @@ name|if_ipackets
 operator|++
 expr_stmt|;
 comment|/* Success! */
+block|}
 name|XE_OUTW
 argument_list|(
 name|XE_DOR
@@ -4562,7 +4563,6 @@ literal|0x8000
 argument_list|)
 expr_stmt|;
 comment|/* skip_rx_packet command */
-block|}
 block|}
 block|}
 elseif|else
@@ -7331,13 +7331,18 @@ name|pad
 operator|>
 literal|0
 condition|)
+block|{
 name|XE_OUTW
 argument_list|(
 name|XE_EDP
 argument_list|,
-literal|0xfeed
+literal|0xdead
 argument_list|)
 expr_stmt|;
+name|pad
+operator|--
+expr_stmt|;
+block|}
 return|return
 literal|0
 return|;
