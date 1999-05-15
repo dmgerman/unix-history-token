a89702688866329fd363bcbe15cb4b63cc437f47
Doug Rabson <dfr@FreeBSD.org>
1999-05-15 18:14:03 +0000

    Make sure sysctls use udev_t, not dev_t.
    Submitted by: Thomas Valentino Crimi <tcrimi+@andrew.cmu.edu>
    
    
    Former-commit-id: 01492b407b4db94b88bfcc0fc6cf52bc68a049dd

--- sys/alpha/alpha/cons.c
+++ sys/alpha/alpha/cons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91  *	$Id: cons.c,v 1.5 1999/04/27 12:20:52 phk Exp $  */
+comment|/*  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91  *	$Id: cons.c,v 1.6 1999/05/08 06:37:51 phk Exp $  */
 end_comment
 
 begin_include
@@ -302,6 +302,13 @@ begin_comment
 comment|/* seems to be never really used */
 end_comment
 
+begin_decl_stmt
+specifier|static
+name|udev_t
+name|cn_udev_t
+decl_stmt|;
+end_decl_stmt
+
 begin_expr_stmt
 name|SYSCTL_OPAQUE
 argument_list|(
@@ -314,10 +321,10 @@ argument_list|,
 name|CTLFLAG_RD
 argument_list|,
 operator|&
-name|cn_dev_t
+name|cn_udev_t
 argument_list|,
 sizeof|sizeof
-name|cn_dev_t
+name|cn_udev_t
 argument_list|,
 literal|"T,dev_t"
 argument_list|,
@@ -655,6 +662,13 @@ name|cn_tp
 operator|->
 name|t_dev
 expr_stmt|;
+name|cn_udev_t
+operator|=
+name|dev2udev
+argument_list|(
+name|cn_dev_t
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
@@ -715,6 +729,13 @@ expr_stmt|;
 name|cn_dev_t
 operator|=
 literal|0
+expr_stmt|;
+name|cn_udev_t
+operator|=
+name|dev2udev
+argument_list|(
+name|cn_dev_t
+argument_list|)
 expr_stmt|;
 block|}
 end_function
