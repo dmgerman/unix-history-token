c5fcc423357b564994c99a40d4d1bbcd472db0b1
Alan Cox <alc@FreeBSD.org>
1999-05-18 05:38:48 +0000

    vm_map_insert: 	General cleanup.  Eliminate coalescing checks that are duplicated 	by vm_object_coalesce.
    Former-commit-id: e063d32b13291dec79317f9ad7e63038e04b590e

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_map.c	8.3 (Berkeley) 1/12/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_map.c,v 1.162 1999/05/16 05:07:31 alc Exp $  */
+comment|/*  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * The Mach Operating System project at Carnegie-Mellon University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)vm_map.c	8.3 (Berkeley) 1/12/94  *  *  * Copyright (c) 1987, 1990 Carnegie-Mellon University.  * All rights reserved.  *  * Authors: Avadis Tevanian, Jr., Michael Wayne Young  *  * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *  * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  *  * $Id: vm_map.c,v 1.163 1999/05/17 00:53:53 alc Exp $  */
 end_comment
 
 begin_comment
@@ -1334,27 +1334,6 @@ decl_stmt|;
 name|u_char
 name|protoeflags
 decl_stmt|;
-if|if
-condition|(
-operator|(
-name|object
-operator|!=
-name|NULL
-operator|)
-operator|&&
-operator|(
-name|cow
-operator|&
-name|MAP_NOFAULT
-operator|)
-condition|)
-block|{
-name|panic
-argument_list|(
-literal|"vm_map_insert: paradoxical MAP_NOFAULT request"
-argument_list|)
-expr_stmt|;
-block|}
 comment|/* 	 * Check that the start and end points are not bogus. 	 */
 if|if
 condition|(
@@ -1458,10 +1437,23 @@ name|cow
 operator|&
 name|MAP_NOFAULT
 condition|)
+block|{
 name|protoeflags
 operator||=
 name|MAP_ENTRY_NOFAULT
 expr_stmt|;
+name|KASSERT
+argument_list|(
+name|object
+operator|==
+name|NULL
+argument_list|,
+operator|(
+literal|"vm_map_insert: paradoxical MAP_NOFAULT request"
+operator|)
+argument_list|)
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|object
@@ -1519,51 +1511,11 @@ name|header
 operator|)
 operator|&&
 operator|(
-operator|(
 name|prev_entry
 operator|->
 name|eflags
-operator|&
-name|MAP_ENTRY_IS_SUB_MAP
-operator|)
 operator|==
-literal|0
-operator|)
-operator|&&
-operator|(
-operator|(
-name|prev_entry
-operator|->
-name|object
-operator|.
-name|vm_object
-operator|==
-name|NULL
-operator|)
-operator|||
-operator|(
-name|prev_entry
-operator|->
-name|object
-operator|.
-name|vm_object
-operator|->
-name|type
-operator|==
-name|OBJT_DEFAULT
-operator|)
-operator|||
-operator|(
-name|prev_entry
-operator|->
-name|object
-operator|.
-name|vm_object
-operator|->
-name|type
-operator|==
-name|OBJT_SWAP
-operator|)
+name|protoeflags
 operator|)
 operator|&&
 operator|(
@@ -1581,23 +1533,16 @@ name|wired_count
 operator|==
 literal|0
 operator|)
-condition|)
-block|{
-if|if
-condition|(
-operator|(
-name|protoeflags
-operator|==
-name|prev_entry
-operator|->
-name|eflags
-operator|)
 operator|&&
 operator|(
 operator|(
-name|cow
-operator|&
-name|MAP_NOFAULT
+name|prev_entry
+operator|->
+name|object
+operator|.
+name|vm_object
+operator|==
+name|NULL
 operator|)
 operator|||
 name|vm_object_coalesce
@@ -1642,7 +1587,7 @@ argument_list|)
 operator|)
 condition|)
 block|{
-comment|/* 			 * We were able to extend the object.  Determine if we 			 * can extend the previous map entry to include the  			 * new range as well. 			 */
+comment|/* 		 * We were able to extend the object.  Determine if we 		 * can extend the previous map entry to include the  		 * new range as well. 		 */
 if|if
 condition|(
 operator|(
@@ -1694,7 +1639,7 @@ name|KERN_SUCCESS
 operator|)
 return|;
 block|}
-comment|/* 			 * If we can extend the object but cannot extend the 			 * map entry, we have to create a new map entry.  We 			 * must bump the ref count on the extended object to 			 * account for it. 			 */
+comment|/* 		 * If we can extend the object but cannot extend the 		 * map entry, we have to create a new map entry.  We 		 * must bump the ref count on the extended object to 		 * account for it. 		 */
 name|object
 operator|=
 name|prev_entry
@@ -1724,7 +1669,6 @@ argument_list|(
 name|object
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 comment|/* 	 * NOTE: if conditionals fail, object can be NULL here.  This occurs 	 * in things like the buffer map where we manage kva but do not manage 	 * backing objects. 	 */
 comment|/* 	 * Create a new entry 	 */
