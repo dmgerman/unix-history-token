ec5cad6847763c8ad7e64a4124685a195b32b7ac
Andrey Chernov <ache@FreeBSD.org>
1999-05-21 15:54:40 +0000

    Realy fix overflow on SO_*TIMEO
    Submitted by: bde
    
    
    Former-commit-id: 8ccf2fe3f353f8118a6ff157286d53a5175a236e

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket.c	8.3 (Berkeley) 4/15/94  *	$Id: uipc_socket.c,v 1.56 1999/04/24 18:22:34 ache Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)uipc_socket.c	8.3 (Berkeley) 4/15/94  *	$Id: uipc_socket.c,v 1.57 1999/05/03 23:57:23 billf Exp $  */
 end_comment
 
 begin_include
@@ -4576,7 +4576,7 @@ name|struct
 name|timeval
 name|tv
 decl_stmt|;
-name|short
+name|u_long
 name|val
 decl_stmt|;
 name|error
@@ -4953,19 +4953,68 @@ condition|)
 goto|goto
 name|bad
 goto|;
+comment|/* assert(hz> 0); */
 if|if
 condition|(
 name|tv
 operator|.
 name|tv_sec
+operator|<
+literal|0
+operator|||
+name|tv
+operator|.
+name|tv_sec
+operator|>
+name|SHRT_MAX
+operator|/
+name|hz
+operator|||
+name|tv
+operator|.
+name|tv_usec
+operator|<
+literal|0
+operator|||
+name|tv
+operator|.
+name|tv_usec
+operator|>=
+literal|1000000
+condition|)
+block|{
+name|error
+operator|=
+name|EDOM
+expr_stmt|;
+goto|goto
+name|bad
+goto|;
+block|}
+comment|/* assert(tick> 0); */
+comment|/* assert(ULONG_MAX - SHRT_MAX>= 1000000); */
+name|val
+operator|=
+call|(
+name|u_long
+call|)
+argument_list|(
+name|tv
+operator|.
+name|tv_sec
 operator|*
 name|hz
+argument_list|)
 operator|+
 name|tv
 operator|.
 name|tv_usec
 operator|/
 name|tick
+expr_stmt|;
+if|if
+condition|(
+name|val
 operator|>
 name|SHRT_MAX
 condition|)
@@ -4978,20 +5027,6 @@ goto|goto
 name|bad
 goto|;
 block|}
-name|val
-operator|=
-name|tv
-operator|.
-name|tv_sec
-operator|*
-name|hz
-operator|+
-name|tv
-operator|.
-name|tv_usec
-operator|/
-name|tick
-expr_stmt|;
 switch|switch
 condition|(
 name|sopt
