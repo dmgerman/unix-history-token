7564274009901fd2dbb887a43018e464c80b62e6
Dmitrij Tejblum <dt@FreeBSD.org>
1999-05-22 20:10:31 +0000

    Don't call calcru() on a swapped-out process. calcru() access p_stats, which is in U-area.
    Former-commit-id: bb6fd1ef31b72af7bba28b3965c0482004f453b4

--- sys/fs/procfs/procfs_status.c
+++ sys/fs/procfs/procfs_status.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Jan-Simon Pendry  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)procfs_status.c	8.4 (Berkeley) 6/15/94  *  * From:  *	$Id: procfs_status.c,v 1.12 1999/01/05 03:53:06 peter Exp $  */
+comment|/*  * Copyright (c) 1993 Jan-Simon Pendry  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Jan-Simon Pendry.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)procfs_status.c	8.4 (Berkeley) 6/15/94  *  * From:  *	$Id: procfs_status.c,v 1.13 1999/04/28 11:37:20 phk Exp $  */
 end_comment
 
 begin_include
@@ -374,41 +374,6 @@ name|p_flag
 operator|&
 name|P_INMEM
 condition|)
-name|ps
-operator|+=
-name|sprintf
-argument_list|(
-name|ps
-argument_list|,
-literal|" %ld,%ld"
-argument_list|,
-name|p
-operator|->
-name|p_stats
-operator|->
-name|p_start
-operator|.
-name|tv_sec
-argument_list|,
-name|p
-operator|->
-name|p_stats
-operator|->
-name|p_start
-operator|.
-name|tv_usec
-argument_list|)
-expr_stmt|;
-else|else
-name|ps
-operator|+=
-name|sprintf
-argument_list|(
-name|ps
-argument_list|,
-literal|" -1,-1"
-argument_list|)
-expr_stmt|;
 block|{
 name|struct
 name|timeval
@@ -427,10 +392,11 @@ operator|&
 name|st
 argument_list|,
 operator|(
-name|void
+expr|struct
+name|timeval
 operator|*
 operator|)
-literal|0
+name|NULL
 argument_list|)
 expr_stmt|;
 name|ps
@@ -439,7 +405,23 @@ name|sprintf
 argument_list|(
 name|ps
 argument_list|,
-literal|" %ld,%ld %ld,%ld"
+literal|" %ld,%ld %ld,%ld %ld,%ld"
+argument_list|,
+name|p
+operator|->
+name|p_stats
+operator|->
+name|p_start
+operator|.
+name|tv_sec
+argument_list|,
+name|p
+operator|->
+name|p_stats
+operator|->
+name|p_start
+operator|.
+name|tv_usec
 argument_list|,
 name|ut
 operator|.
@@ -459,6 +441,16 @@ name|tv_usec
 argument_list|)
 expr_stmt|;
 block|}
+else|else
+name|ps
+operator|+=
+name|sprintf
+argument_list|(
+name|ps
+argument_list|,
+literal|" -1,-1 -1,-1 -1,-1"
+argument_list|)
+expr_stmt|;
 name|ps
 operator|+=
 name|sprintf
