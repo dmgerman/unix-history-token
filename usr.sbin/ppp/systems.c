1fe0d52e67e819228448f2866e1332c42cbed898
Brian Somers <brian@FreeBSD.org>
1999-05-24 09:04:35 +0000

    Don't warn about ``!'' lines being unindented commands in ppp.conf.
    Former-commit-id: 9626acaf3edbfaf6c9f9f670ccbc6b4a5ad024e7

--- usr.sbin/ppp/systems.c
+++ usr.sbin/ppp/systems.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	          System configuration routines  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: systems.c,v 1.43 1999/05/08 11:07:43 brian Exp $  *  *  TODO:  */
+comment|/*  *	          System configuration routines  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: systems.c,v 1.44 1999/05/09 20:02:26 brian Exp $  *  *  TODO:  */
 end_comment
 
 begin_include
@@ -1628,6 +1628,14 @@ name|indent
 condition|)
 block|{
 comment|/* start of next section */
+if|if
+condition|(
+operator|*
+name|cp
+operator|!=
+literal|'!'
+condition|)
+block|{
 name|wp
 operator|=
 name|strchr
@@ -1662,13 +1670,15 @@ name|log_Printf
 argument_list|(
 name|LogWARN
 argument_list|,
-literal|"Unindented command (%s line %d) - ignored\n"
+literal|"Unindented command (%s line %d) -"
+literal|" ignored\n"
 argument_list|,
 name|filename
 argument_list|,
 name|linenum
 argument_list|)
 expr_stmt|;
+block|}
 break|break;
 block|}
 name|len
