eb17da3ffa3a6f6d0fce7ecd00bc9e75c3ad8f4c
Dmitrij Tejblum <dt@FreeBSD.org>
1999-05-25 19:32:53 +0000

    From our i386 counterpart: |revision 1.13 |date: 1995/09/15 23:49:23;  author: davidg;  state: Exp;  lines: +15 -2 |Check for page being resident when doing I/O with /dev/kmem and return |EFAULT if it is not resident. This prevents the system from manufacturing |a zero-fill page for unused but allocated areas of the kernel's VM.
    Former-commit-id: b10f2bb3797f443bc9bd32e48f59d15fb8ae547e

--- sys/alpha/alpha/mem.c
+++ sys/alpha/alpha/mem.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.5 1999/04/23 19:53:38 dt Exp $  */
+comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department, and code derived from software contributed to  * Berkeley by William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: Utah $Hdr: mem.c 1.13 89/10/08$  *	from: @(#)mem.c	7.2 (Berkeley) 5/9/91  *	$Id: mem.c,v 1.6 1999/04/27 11:13:20 phk Exp $  */
 end_comment
 
 begin_comment
@@ -17,12 +17,6 @@ begin_include
 include|#
 directive|include
 file|<sys/param.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/conf.h>
 end_include
 
 begin_include
@@ -62,6 +56,12 @@ begin_include
 include|#
 directive|include
 file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/conf.h>
 end_include
 
 begin_include
@@ -843,6 +843,12 @@ comment|/* minor device 1 is kernel memory */
 case|case
 literal|1
 case|:
+block|{
+name|vm_offset_t
+name|addr
+decl_stmt|,
+name|eaddr
+decl_stmt|;
 name|v
 operator|=
 name|uio
@@ -882,6 +888,48 @@ argument_list|,
 name|MAXPHYS
 argument_list|)
 expr_stmt|;
+comment|/* 			 * Make sure that all of the pages are currently resident so 			 * that we don't create any zero-fill pages. 			 */
+name|addr
+operator|=
+name|trunc_page
+argument_list|(
+name|v
+argument_list|)
+expr_stmt|;
+name|eaddr
+operator|=
+name|round_page
+argument_list|(
+name|v
+operator|+
+name|c
+argument_list|)
+expr_stmt|;
+for|for
+control|(
+init|;
+name|addr
+operator|<
+name|eaddr
+condition|;
+name|addr
+operator|+=
+name|PAGE_SIZE
+control|)
+if|if
+condition|(
+name|pmap_extract
+argument_list|(
+name|kernel_pmap
+argument_list|,
+name|addr
+argument_list|)
+operator|==
+literal|0
+condition|)
+return|return
+name|EFAULT
+return|;
 if|#
 directive|if
 name|defined
@@ -963,6 +1011,7 @@ name|uio
 argument_list|)
 expr_stmt|;
 break|break;
+block|}
 comment|/* minor device 2 is EOF/rathole */
 case|case
 literal|2
