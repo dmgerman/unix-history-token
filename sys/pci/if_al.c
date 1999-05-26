e7e74a57cdb0fa735b0a7138454f24664bb08e09
Bill Paul <wpaul@FreeBSD.org>
1999-05-26 22:56:22 +0000

    Two small tweaks:
    - Clear the IFF_OACTIVE flag when al_txeof() runs down the last TX mbuf chain.
    - Mark the workaround for the transmitter stalling bug with
      #ifdef AL_TX_STALL_WAR/#endif.
    
    
    Former-commit-id: d5fc8b9a2361e456a421342c9602ba6bfedf0b9b

--- sys/pci/if_al.c
+++ sys/pci/if_al.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998, 1999  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_al.c,v 1.2 1999/05/21 04:42:36 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998, 1999  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_al.c,v 1.13 1999/05/26 22:48:16 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -168,6 +168,16 @@ directive|include
 file|<pci/pcivar.h>
 end_include
 
+begin_comment
+comment|/* Enable workaround for small transmitter bug. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AL_TX_STALL_WAR
+end_define
+
 begin_define
 define|#
 directive|define
@@ -197,7 +207,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: if_al.c,v 1.2 1999/05/21 04:42:36 wpaul Exp $"
+literal|"$Id: if_al.c,v 1.13 1999/05/26 22:48:16 wpaul Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -6311,6 +6321,13 @@ name|al_tx_tail
 operator|=
 name|NULL
 expr_stmt|;
+name|ifp
+operator|->
+name|if_flags
+operator|&=
+operator|~
+name|IFF_OACTIVE
+expr_stmt|;
 break|break;
 block|}
 name|sc
@@ -7350,6 +7367,9 @@ argument_list|,
 literal|0xFFFFFFFF
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|AL_TX_STALL_WAR
 comment|/* 		 * Work around some strange behavior in the Comet. For 		 * some reason, the transmitter will sometimes wedge if 		 * we queue up a descriptor chain that wraps from the end 		 * of the transmit list back to the beginning. If we reach 		 * the end of the list and still have more packets to queue, 		 * don't queue them now: end the transmit session here and 		 * then wait until it finishes before sending the other 		 * packets. 		 */
 if|if
 condition|(
@@ -7376,6 +7396,8 @@ name|IFF_OACTIVE
 expr_stmt|;
 break|break;
 block|}
+endif|#
+directive|endif
 block|}
 name|sc
 operator|->
