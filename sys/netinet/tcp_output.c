74c9f771629d046e4861723acfa5abbc6c8c9461
David Greenman <dg@FreeBSD.org>
1999-05-27 12:24:21 +0000

    Added net.inet.tcp.path_mtu_discovery variable which when set to 0 (default 1) disables PMTUD globally. Although PMTUD can be disabled in the standard case by locking the MTU on a static route (including the default route), this method doesn't work in the face of dynamic routing protocols like gated.
    Former-commit-id: 1f91912b4e31529c6455d2ad0640a2696b70657b

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tcp_output.c	8.4 (Berkeley) 5/24/95  *	$Id: tcp_output.c,v 1.32 1999/01/20 17:31:59 fenner Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1988, 1990, 1993, 1995  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)tcp_output.c	8.4 (Berkeley) 5/24/95  *	$Id: tcp_output.c,v 1.33 1999/04/07 22:22:06 julian Exp $  */
 end_comment
 
 begin_include
@@ -25,6 +25,18 @@ begin_include
 include|#
 directive|include
 file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/kernel.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/sysctl.h>
 end_include
 
 begin_include
@@ -166,6 +178,36 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_decl_stmt
+specifier|static
+name|int
+name|path_mtu_discovery
+init|=
+literal|1
+decl_stmt|;
+end_decl_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_net_inet_tcp
+argument_list|,
+name|OID_AUTO
+argument_list|,
+name|path_mtu_discovery
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|path_mtu_discovery
+argument_list|,
+literal|1
+argument_list|,
+literal|"Enable Path MTU Discovery"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/*  * Tcp output routine: figure out what should be sent and send it.  */
@@ -2634,16 +2676,11 @@ operator|+
 name|len
 expr_stmt|;
 block|{
-if|#
-directive|if
-literal|1
 name|struct
 name|rtentry
 modifier|*
 name|rt
 decl_stmt|;
-endif|#
-directive|endif
 operator|(
 operator|(
 expr|struct
@@ -2697,12 +2734,11 @@ operator|->
 name|inp_ip_tos
 expr_stmt|;
 comment|/* XXX */
-if|#
-directive|if
-literal|1
 comment|/* 	 * See if we should do MTU discovery.  We do it only if the following 	 * are true: 	 *	1) we have a valid route to the destination 	 *	2) the MTU is not locked (if it is, then discovery has been 	 *	   disabled) 	 */
 if|if
 condition|(
+name|path_mtu_discovery
+operator|&&
 operator|(
 name|rt
 operator|=
@@ -2747,8 +2783,6 @@ operator||=
 name|IP_DF
 expr_stmt|;
 block|}
-endif|#
-directive|endif
 name|error
 operator|=
 name|ip_output
@@ -2807,9 +2841,6 @@ literal|0
 operator|)
 return|;
 block|}
-if|#
-directive|if
-literal|1
 if|if
 condition|(
 name|error
@@ -2831,8 +2862,6 @@ return|return
 literal|0
 return|;
 block|}
-endif|#
-directive|endif
 if|if
 condition|(
 operator|(
