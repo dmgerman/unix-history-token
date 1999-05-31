be89bd209c4e51ee92439565332fe0c494c42363
Doug Rabson <dfr@FreeBSD.org>
1999-05-31 18:39:17 +0000

    * Change include file locations. * Fix some misunderstandings about the return value of resource_int_value(). * Make it build on alpha (doesn't work yet...)
    Former-commit-id: ff9e98351292ba44be45ffdced8eabd1f44c1c8a

--- sys/dev/fdc/fdc.c
+++ sys/dev/fdc/fdc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Don Ahn.  *  * Libretto PCMCIA floppy support by David Horwitt (dhorwitt@ucsd.edu)  * aided by the Linux floppy driver modifications from David Bateman  * (dbateman@eng.uts.edu.au).  *  * Copyright (c) 1993, 1994 by  *  jc@irbs.UUCP (John Capo)  *  vak@zebub.msk.su (Serge Vakulenko)  *  ache@astral.msk.su (Andrew A. Chernov)  *  * Copyright (c) 1993, 1994, 1995 by  *  joerg_wunsch@uriah.sax.de (Joerg Wunsch)  *  dufault@hda.com (Peter Dufault)  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)fd.c	7.4 (Berkeley) 5/25/91  *	$Id: fd.c,v 1.144 1999/05/30 11:10:54 dfr Exp $  *  */
+comment|/*  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Don Ahn.  *  * Libretto PCMCIA floppy support by David Horwitt (dhorwitt@ucsd.edu)  * aided by the Linux floppy driver modifications from David Bateman  * (dbateman@eng.uts.edu.au).  *  * Copyright (c) 1993, 1994 by  *  jc@irbs.UUCP (John Capo)  *  vak@zebub.msk.su (Serge Vakulenko)  *  ache@astral.msk.su (Andrew A. Chernov)  *  * Copyright (c) 1993, 1994, 1995 by  *  joerg_wunsch@uriah.sax.de (Joerg Wunsch)  *  dufault@hda.com (Peter Dufault)  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from:	@(#)fd.c	7.4 (Berkeley) 5/25/91  *	$Id: fd.c,v 1.145 1999/05/30 16:52:12 phk Exp $  *  */
 end_comment
 
 begin_include
@@ -179,25 +179,25 @@ end_include
 begin_include
 include|#
 directive|include
-file|<i386/isa/isa.h>
+file|<isa/isareg.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<i386/isa/fdreg.h>
+file|<isa/fdreg.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<i386/isa/fdc.h>
+file|<isa/fdc.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<i386/isa/rtc.h>
+file|<isa/rtc.h>
 end_include
 
 begin_ifdef
@@ -2725,6 +2725,19 @@ literal|8
 index|]
 decl_stmt|;
 comment|/* better be long enough */
+comment|/* No pnp support */
+if|if
+condition|(
+name|isa_get_vendorid
+argument_list|(
+name|dev
+argument_list|)
+condition|)
+return|return
+operator|(
+name|ENXIO
+operator|)
+return|;
 name|fdc
 operator|=
 name|device_get_softc
@@ -3450,7 +3463,7 @@ literal|"drive"
 argument_list|,
 name|ivar
 argument_list|)
-operator|==
+operator|!=
 literal|0
 condition|)
 operator|*
@@ -3492,6 +3505,10 @@ operator|&
 name|disabled
 argument_list|)
 operator|==
+literal|0
+operator|&&
+name|disabled
+operator|!=
 literal|0
 condition|)
 name|device_disable
@@ -3732,19 +3749,6 @@ literal|0
 decl_stmt|;
 endif|#
 directive|endif
-comment|/* No pnp support */
-if|if
-condition|(
-name|isa_get_vendorid
-argument_list|(
-name|dev
-argument_list|)
-condition|)
-return|return
-operator|(
-name|ENXIO
-operator|)
-return|;
 name|fdsu
 operator|=
 operator|*
@@ -3811,6 +3815,9 @@ argument_list|(
 name|dev
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|__i386__
 comment|/* look up what bios thinks we have */
 switch|switch
 condition|(
@@ -3878,6 +3885,15 @@ name|RTCFDT_NONE
 expr_stmt|;
 break|break;
 block|}
+else|#
+directive|else
+name|fdt
+operator|=
+name|RTCFDT_144M
+expr_stmt|;
+comment|/* XXX probably */
+endif|#
+directive|endif
 comment|/* is there a unit? */
 if|if
 condition|(
