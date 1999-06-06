0132a5e726c9d1521996c7ae22f24a4eaa2d669c
Archie Cobbs <archie@FreeBSD.org>
1999-06-06 02:44:04 +0000

    MFC (rev. 1.53): ksprintn() race condition fix
    Former-commit-id: dbd6ab8dd2f76658b27220ebb8f7df6fa34c507a

--- sys/kern/subr_prf.c
+++ sys/kern/subr_prf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1986, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_prf.c	8.3 (Berkeley) 1/21/94  * $Id: subr_prf.c,v 1.50 1998/09/06 06:25:04 ache Exp $  */
+comment|/*-  * Copyright (c) 1986, 1988, 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)subr_prf.c	8.3 (Berkeley) 1/21/94  * $Id: subr_prf.c,v 1.51 1998/12/03 04:45:56 archie Exp $  */
 end_comment
 
 begin_include
@@ -92,6 +92,17 @@ define|#
 directive|define
 name|TOLOG
 value|0x04
+end_define
+
+begin_comment
+comment|/* Max number conversion buffer length: a long in base 8, plus NUL byte */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAXNBUF
+value|(sizeof(long) * NBBY / 3 + 2)
 end_define
 
 begin_decl_stmt
@@ -211,6 +222,10 @@ name|ksprintn
 name|__P
 argument_list|(
 operator|(
+name|char
+operator|*
+name|nbuf
+operator|,
 name|u_long
 name|num
 operator|,
@@ -812,6 +827,12 @@ name|char
 modifier|*
 name|p
 decl_stmt|;
+name|char
+name|nbuf
+index|[
+name|MAXNBUF
+index|]
+decl_stmt|;
 name|msglogchar
 argument_list|(
 literal|'<'
@@ -825,6 +846,8 @@ name|p
 operator|=
 name|ksprintn
 argument_list|(
+name|nbuf
+argument_list|,
 operator|(
 name|u_long
 operator|)
@@ -1616,7 +1639,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Put a number (base<= 16) in a buffer in reverse order; return an  * optional length and a pointer to the NULL terminated (preceded?)  * buffer.  */
+comment|/*  * Put a number (base<= 16) in a buffer in reverse order; return an  * optional length and a pointer to the NULL terminated (preceded?)  * buffer. The buffer pointed to by "buf" must have length>= MAXNBUF.  */
 end_comment
 
 begin_function
@@ -1625,12 +1648,18 @@ name|char
 modifier|*
 name|ksprintn
 parameter_list|(
+name|buf
+parameter_list|,
 name|ul
 parameter_list|,
 name|base
 parameter_list|,
 name|lenp
 parameter_list|)
+name|char
+modifier|*
+name|buf
+decl_stmt|;
 specifier|register
 name|u_long
 name|ul
@@ -1647,22 +1676,6 @@ end_function
 begin_block
 block|{
 comment|/* A long in base 8, plus NULL. */
-specifier|static
-name|char
-name|buf
-index|[
-sizeof|sizeof
-argument_list|(
-name|long
-argument_list|)
-operator|*
-name|NBBY
-operator|/
-literal|3
-operator|+
-literal|2
-index|]
-decl_stmt|;
 specifier|register
 name|char
 modifier|*
@@ -1671,6 +1684,11 @@ decl_stmt|;
 name|p
 operator|=
 name|buf
+expr_stmt|;
+operator|*
+name|p
+operator|=
+literal|'\0'
 expr_stmt|;
 do|do
 block|{
@@ -1755,6 +1773,12 @@ parameter_list|(
 name|c
 parameter_list|)
 value|{int cc=(c); if (func) (*func)(cc,arg); else *d++ = cc; retval++; }
+name|char
+name|nbuf
+index|[
+name|MAXNBUF
+index|]
+decl_stmt|;
 name|char
 modifier|*
 name|p
@@ -2163,6 +2187,8 @@ name|q
 operator|=
 name|ksprintn
 argument_list|(
+name|nbuf
+argument_list|,
 name|ul
 argument_list|,
 operator|*
@@ -2766,6 +2792,8 @@ name|p
 operator|=
 name|ksprintn
 argument_list|(
+name|nbuf
+argument_list|,
 name|ul
 argument_list|,
 name|base
