549b5187c5bdc134343bf9e99158ac967204f457
Dmitrij Tejblum <dt@FreeBSD.org>
1999-06-07 16:30:27 +0000

    Ensure that host_addr (which is returned in struct hostent::h_addr_list[0]) is aligned so that pointer to it can be safely casted to struct in_addr *.
    Former-commit-id: 0b1f1a4e5dfd4827b90ff0d1ee4f5efed38f254a

--- lib/libc/net/gethostbyht.c
+++ lib/libc/net/gethostbyht.c
@@ -34,7 +34,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: gethostbyht.c,v 1.9 1997/02/22 15:00:07 peter Exp $"
+literal|"$Id: gethostbyht.c,v 1.10 1997/06/27 08:22:01 peter Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -171,10 +171,10 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|static
-name|u_char
+name|u_int32_t
 name|host_addr
 index|[
-literal|16
+literal|4
 index|]
 decl_stmt|;
 end_decl_stmt
