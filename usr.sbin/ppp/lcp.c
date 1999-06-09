ab90d3cf977568b11dc42cd7af8f334790e12362
Brian Somers <brian@FreeBSD.org>
1999-06-09 20:27:26 +0000

    Initialise `mp'.
    Former-commit-id: 6214c2b6fd21dcb7a19a6a1e6dfcc8de8ce0bca1

--- usr.sbin/ppp/lcp.c
+++ usr.sbin/ppp/lcp.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	      PPP Link Control Protocol (LCP) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: lcp.c,v 1.75 1999/06/02 15:59:02 brian Exp $  *  */
+comment|/*  *	      PPP Link Control Protocol (LCP) Module  *  *	    Written by Toshiharu OHNO (tony-o@iij.ad.jp)  *  *   Copyright (C) 1993, Internet Initiative Japan, Inc. All rights reserverd.  *  * Redistribution and use in source and binary forms are permitted  * provided that the above copyright notice and this paragraph are  * duplicated in all such forms and that any documentation,  * advertising materials, and other materials related to such  * distribution and use acknowledge that the software was developed  * by the Internet Initiative Japan, Inc.  The name of the  * IIJ may not be used to endorse or promote products derived  * from this software without specific prior written permission.  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.  *  * $Id: lcp.c,v 1.76 1999/06/09 16:54:03 brian Exp $  *  */
 end_comment
 
 begin_include
@@ -6249,6 +6249,19 @@ break|break;
 case|case
 name|TY_ENDDISC
 case|:
+name|mp
+operator|=
+operator|&
+name|lcp
+operator|->
+name|fsm
+operator|.
+name|bundle
+operator|->
+name|ncp
+operator|.
+name|mp
+expr_stmt|;
 name|log_Printf
 argument_list|(
 name|LogLCP
