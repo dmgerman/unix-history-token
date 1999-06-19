954ec8312bbc793e8b1ec944d1302f1cec3547c0
Doug Rabson <dfr@FreeBSD.org>
1999-06-19 11:54:35 +0000

    Convert signalling NaNs to quiet NaNs as specified by the Alpha Architecture Reference Manual.
    Submitted by: Hidetoshi Shimokawa <simokawa@sat.t.u-tokyo.ac.jp>
    
    
    Former-commit-id: 68362487ed9b0bc69df865791e2831f86869d62a

--- sys/alpha/alpha/ieee_float.c
+++ sys/alpha/alpha/ieee_float.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ieee_float.c,v 1.3 1999/05/10 16:02:11 peter Exp $  */
+comment|/*-  * Copyright (c) 1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: ieee_float.c,v 1.4 1999/06/14 11:42:30 simokawa Exp $  */
 end_comment
 
 begin_comment
@@ -1483,10 +1483,20 @@ argument_list|(
 name|fa
 argument_list|)
 condition|)
+block|{
 name|result
 operator|=
 name|fa
 expr_stmt|;
+name|result
+operator|.
+name|t
+operator|.
+name|fraction
+operator||=
+name|QNAN_BIT
+expr_stmt|;
+block|}
 comment|/* If either operand is a signalling NaN, trap. */
 if|if
 condition|(
@@ -2082,10 +2092,20 @@ argument_list|(
 name|fa
 argument_list|)
 condition|)
+block|{
 name|result
 operator|=
 name|fa
 expr_stmt|;
+name|result
+operator|.
+name|t
+operator|.
+name|fraction
+operator||=
+name|QNAN_BIT
+expr_stmt|;
+block|}
 comment|/* If either operand is a signalling NaN, trap. */
 if|if
 condition|(
@@ -2810,10 +2830,20 @@ argument_list|(
 name|fa
 argument_list|)
 condition|)
+block|{
 name|result
 operator|=
 name|fa
 expr_stmt|;
+name|result
+operator|.
+name|t
+operator|.
+name|fraction
+operator||=
+name|QNAN_BIT
+expr_stmt|;
+block|}
 comment|/* If either operand is a signalling NaN, trap. */
 if|if
 condition|(
