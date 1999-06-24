6045960a3ce4cab86a8d70e570eaf7bb51b205d9
Kazutaka YOKOTA <yokota@FreeBSD.org>
1999-06-24 09:21:27 +0000

    Fix ESC[P (delete N chars) and ESC[@ (insert N chars).  These deletion and insertion should affect the line the cursor is on only.
    Former-commit-id: 03ac30ce298bd872658976671851942368683a8c

--- sys/dev/syscons/syscons.c
+++ sys/dev/syscons/syscons.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992-1998 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: syscons.c,v 1.305 1999/05/31 11:25:13 phk Exp $  */
+comment|/*-  * Copyright (c) 1992-1998 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: syscons.c,v 1.307 1999/06/22 14:13:31 yokota Exp $  */
 end_comment
 
 begin_include
@@ -14280,7 +14280,7 @@ operator|+
 name|n
 operator|)
 expr_stmt|;
-name|sc_vtb_delete
+name|sc_vtb_move
 argument_list|(
 operator|&
 name|scp
@@ -14290,6 +14290,28 @@ argument_list|,
 name|scp
 operator|->
 name|cursor_pos
+operator|+
+name|n
+argument_list|,
+name|scp
+operator|->
+name|cursor_pos
+argument_list|,
+name|count
+argument_list|)
+expr_stmt|;
+name|sc_vtb_erase
+argument_list|(
+operator|&
+name|scp
+operator|->
+name|vtb
+argument_list|,
+name|scp
+operator|->
+name|cursor_pos
+operator|+
+name|count
 argument_list|,
 name|n
 argument_list|,
@@ -14393,7 +14415,27 @@ operator|+
 name|n
 operator|)
 expr_stmt|;
-name|sc_vtb_ins
+name|sc_vtb_move
+argument_list|(
+operator|&
+name|scp
+operator|->
+name|vtb
+argument_list|,
+name|scp
+operator|->
+name|cursor_pos
+argument_list|,
+name|scp
+operator|->
+name|cursor_pos
+operator|+
+name|n
+argument_list|,
+name|count
+argument_list|)
+expr_stmt|;
+name|sc_vtb_erase
 argument_list|(
 operator|&
 name|scp
