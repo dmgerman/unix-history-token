3f900250968b4764f013d1f032d9ed7bfb954651
Kazutaka YOKOTA <yokota@FreeBSD.org>
1999-06-24 09:06:48 +0000

    Declare the correct size of softc and fix sc_get_softc().
    Former-commit-id: 1b0895ad7e6271c66832ba928d89228ccd4dc26a

--- sys/isa/syscons_isa.c
+++ sys/isa/syscons_isa.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999 Kazutaka YOKOTA<yokota@zodiac.mech.utsunomiya-u.ac.jp>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: syscons_isa.c,v 1.4 1999/05/30 11:12:29 dfr Exp $  */
+comment|/*-  * Copyright (c) 1999 Kazutaka YOKOTA<yokota@zodiac.mech.utsunomiya-u.ac.jp>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer as  *    the first lines of this file unmodified.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: syscons_isa.c,v 1.5 1999/06/22 14:13:41 yokota Exp $  */
 end_comment
 
 begin_include
@@ -249,10 +249,11 @@ name|SC_DRIVER_NAME
 block|,
 name|sc_methods
 block|,
-literal|1
-block|,
-comment|/* XXX */
-block|}
+sizeof|sizeof
+argument_list|(
+name|sc_softc_t
+argument_list|)
+block|, }
 decl_stmt|;
 end_decl_stmt
 
@@ -450,11 +451,14 @@ operator|(
 name|sc_softc_t
 operator|*
 operator|)
-name|devclass_get_softc
+name|device_get_softc
+argument_list|(
+name|devclass_get_device
 argument_list|(
 name|sc_devclass
 argument_list|,
 name|unit
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
@@ -592,11 +596,14 @@ operator|(
 name|sc_softc_t
 operator|*
 operator|)
-name|devclass_get_softc
+name|device_get_softc
+argument_list|(
+name|devclass_get_device
 argument_list|(
 name|sc_devclass
 argument_list|,
 name|i
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
