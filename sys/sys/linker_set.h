399d509c6526ea5328c14de2dd11f6f4e92f60ba
Peter Wemm <peter@FreeBSD.org>
1999-06-26 12:27:01 +0000

    ABS_SET() and BSS_SET() are no longer used.  The sole remaining user of TEXT_SET() is DDB's command tables.
    Former-commit-id: bdd036e6e22983924b849409f2550437599ca94d

--- sys/sys/linker_set.h
+++ sys/sys/linker_set.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999 John D. Polstra  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: linker_set.h,v 1.1 1999/01/14 05:48:46 jdp Exp $  */
+comment|/*-  * Copyright (c) 1999 John D. Polstra  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: linker_set.h,v 1.2 1999/05/08 12:52:26 peter Exp $  */
 end_comment
 
 begin_ifndef
@@ -95,30 +95,6 @@ parameter_list|)
 value|MAKE_SET(set, sym)
 end_define
 
-begin_define
-define|#
-directive|define
-name|BSS_SET
-parameter_list|(
-name|set
-parameter_list|,
-name|sym
-parameter_list|)
-value|MAKE_SET(set, sym)
-end_define
-
-begin_define
-define|#
-directive|define
-name|ABS_SET
-parameter_list|(
-name|set
-parameter_list|,
-name|sym
-parameter_list|)
-value|MAKE_SET(set, sym)
-end_define
-
 begin_else
 else|#
 directive|else
@@ -165,30 +141,6 @@ parameter_list|,
 name|sym
 parameter_list|)
 value|MAKE_SET(set, sym, 25)
-end_define
-
-begin_define
-define|#
-directive|define
-name|BSS_SET
-parameter_list|(
-name|set
-parameter_list|,
-name|sym
-parameter_list|)
-value|MAKE_SET(set, sym, 27)
-end_define
-
-begin_define
-define|#
-directive|define
-name|ABS_SET
-parameter_list|(
-name|set
-parameter_list|,
-name|sym
-parameter_list|)
-value|MAKE_SET(set, sym, 21)
 end_define
 
 begin_endif
