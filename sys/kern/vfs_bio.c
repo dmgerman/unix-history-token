25e38a8bcb39d3fc43fb0fbd0ef49597ca2eca38
Peter Wemm <peter@FreeBSD.org>
1999-06-28 15:32:10 +0000

    Fix a bug that was almost certainly making breadn() fail.  BUF_KERNPROC() was being called on the wrong bp - it should be called on the one that's just about to be fed to VOP_STRATEGY().
    Former-commit-id: c28b23685b21601f77d5812769c8f58f0e20197a

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.216 1999/06/26 02:46:06 mckusick Exp $  */
+comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.217 1999/06/26 14:46:35 peter Exp $  */
 end_comment
 
 begin_comment
@@ -2042,7 +2042,7 @@ argument_list|)
 expr_stmt|;
 name|BUF_KERNPROC
 argument_list|(
-name|bp
+name|rabp
 argument_list|)
 expr_stmt|;
 name|VOP_STRATEGY
