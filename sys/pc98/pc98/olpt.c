552ac2aa0bdafc74ae6f19844652c220e42b7773
Takenori KATO <kato@FreeBSD.org>
1999-06-28 15:31:32 +0000

    MFC: new parallel port driver for PC98.
    Former-commit-id: 231062461421d3ff18ea5a1722601d66dc7e04fa

--- sys/pc98/pc98/olpt.c
+++ sys/pc98/pc98/olpt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1990 William F. Jolitz, TeleMuse  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This software is a component of "386BSD" developed by  *	William F. Jolitz, TeleMuse.  * 4. Neither the name of the developer nor the name "386BSD"  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS A COMPONENT OF 386BSD DEVELOPED BY WILLIAM F. JOLITZ  * AND IS INTENDED FOR RESEARCH AND EDUCATIONAL PURPOSES ONLY. THIS  * SOFTWARE SHOULD NOT BE CONSIDERED TO BE A COMMERCIAL PRODUCT.  * THE DEVELOPER URGES THAT USERS WHO REQUIRE A COMMERCIAL PRODUCT  * NOT MAKE USE OF THIS WORK.  *  * FOR USERS WHO WISH TO UNDERSTAND THE 386BSD SYSTEM DEVELOPED  * BY WILLIAM F. JOLITZ, WE RECOMMEND THE USER STUDY WRITTEN  * REFERENCES SUCH AS THE  "PORTING UNIX TO THE 386" SERIES  * (BEGINNING JANUARY 1991 "DR. DOBBS JOURNAL", USA AND BEGINNING  * JUNE 1991 "UNIX MAGAZIN", GERMANY) BY WILLIAM F. JOLITZ AND  * LYNNE GREER JOLITZ, AS WELL AS OTHER BOOKS ON UNIX AND THE  * ON-LINE 386BSD USER MANUAL BEFORE USE. A BOOK DISCUSSING THE INTERNALS  * OF 386BSD ENTITLED "386BSD FROM THE INSIDE OUT" WILL BE AVAILABLE LATE 1992.  *  * THIS SOFTWARE IS PROVIDED BY THE DEVELOPER ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE DEVELOPER BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: unknown origin, 386BSD 0.1  *	$Id$  */
+comment|/*  * Copyright (c) 1990 William F. Jolitz, TeleMuse  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This software is a component of "386BSD" developed by  *	William F. Jolitz, TeleMuse.  * 4. Neither the name of the developer nor the name "386BSD"  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS A COMPONENT OF 386BSD DEVELOPED BY WILLIAM F. JOLITZ  * AND IS INTENDED FOR RESEARCH AND EDUCATIONAL PURPOSES ONLY. THIS  * SOFTWARE SHOULD NOT BE CONSIDERED TO BE A COMMERCIAL PRODUCT.  * THE DEVELOPER URGES THAT USERS WHO REQUIRE A COMMERCIAL PRODUCT  * NOT MAKE USE OF THIS WORK.  *  * FOR USERS WHO WISH TO UNDERSTAND THE 386BSD SYSTEM DEVELOPED  * BY WILLIAM F. JOLITZ, WE RECOMMEND THE USER STUDY WRITTEN  * REFERENCES SUCH AS THE  "PORTING UNIX TO THE 386" SERIES  * (BEGINNING JANUARY 1991 "DR. DOBBS JOURNAL", USA AND BEGINNING  * JUNE 1991 "UNIX MAGAZIN", GERMANY) BY WILLIAM F. JOLITZ AND  * LYNNE GREER JOLITZ, AS WELL AS OTHER BOOKS ON UNIX AND THE  * ON-LINE 386BSD USER MANUAL BEFORE USE. A BOOK DISCUSSING THE INTERNALS  * OF 386BSD ENTITLED "386BSD FROM THE INSIDE OUT" WILL BE AVAILABLE LATE 1992.  *  * THIS SOFTWARE IS PROVIDED BY THE DEVELOPER ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE DEVELOPER BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: unknown origin, 386BSD 0.1  *	$Id: lpt.c,v 1.24 1998/12/11 08:48:21 kato Exp $  */
 end_comment
 
 begin_comment
@@ -1071,12 +1071,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|PC98
-end_ifndef
-
 begin_function_decl
 specifier|static
 name|int
@@ -1086,11 +1080,6 @@ name|void
 parameter_list|)
 function_decl|;
 end_function_decl
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_function_decl
 specifier|static
@@ -1208,72 +1197,39 @@ name|cdevsw
 name|lpt_cdevsw
 init|=
 block|{
-comment|/* open */
 name|lptopen
 block|,
-comment|/* close */
 name|lptclose
 block|,
-comment|/* read */
 name|noread
 block|,
-comment|/* write */
 name|lptwrite
 block|,
-comment|/* ioctl */
+comment|/*16*/
 name|lptioctl
 block|,
-comment|/* stop */
-name|nostop
+name|nullstop
 block|,
-comment|/* reset */
-name|noreset
+name|nullreset
 block|,
-comment|/* devtotty */
 name|nodevtotty
 block|,
-comment|/* poll */
-name|nopoll
+comment|/* lpt */
+name|seltrue
 block|,
-comment|/* mmap */
 name|nommap
 block|,
-comment|/* strategy */
-name|nostrategy
+name|nostrat
 block|,
-comment|/* name */
 literal|"lpt"
 block|,
-comment|/* parms */
-name|noparms
+name|NULL
 block|,
-comment|/* maj */
-name|CDEV_MAJOR
-block|,
-comment|/* dump */
-name|nodump
-block|,
-comment|/* psize */
-name|nopsize
-block|,
-comment|/* flags */
-literal|0
-block|,
-comment|/* maxio */
-literal|0
-block|,
-comment|/* bmaj */
 operator|-
 literal|1
 block|}
 decl_stmt|;
 end_decl_stmt
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|PC98
-end_ifndef
 
 begin_comment
 comment|/*  * Internal routine to lptprobe to do port tests of one byte value  */
@@ -1367,15 +1323,6 @@ operator|)
 return|;
 block|}
 end_function
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* PC98 */
-end_comment
 
 begin_comment
 comment|/*  * New lpt port probe Geoff Rehmet - Rhodes University - 14/2/94  * Based partially on Rod Grimes' printer probe  *  * Logic:  *	1) If no port address was given, use the bios detected ports  *	   and autodetect what ports the printers are on.  *	2) Otherwise, probe the data port at the address given,  *	   using the method in Rod Grimes' port probe.  *	   (Much code ripped off directly from Rod's probe.)  *  * Comments from Rod's probe:  * Logic:  *	1) You should be able to write to and read back the same value  *	   to the data port.  Do an alternating zeros, alternating ones,  *	   walking zero, and walking one test to check for stuck bits.  *  *	2) You should be able to write to and read back the same value  *	   to the control port lower 5 bits, the upper 3 bits are reserved  *	   per the IBM PC technical reference manauls and different boards  *	   do different things with them.  Do an alternating zeros, alternating  *	   ones, walking zero, and walking one test to check for stuck bits.  *  *	   Some printers drag the strobe line down when the are powered off  * 	   so this bit has been masked out of the control port test.  *  *	   XXX Some printers may not like a fast pulse on init or strobe, I  *	   don't know at this point, if that becomes a problem these bits  *	   should be turned off in the mask byte for the control port test.  *  *	   We are finally left with a mask of 0x14, due to some printers  *	   being adamant about holding other bits high ........  *  *	   Before probing the control port, we write a 0 to the data port -  *	   If not, some printers chuck out garbage when the strobe line  *	   gets toggled.  *  *	3) Set the data and control ports to a value of 0  *  *	This probe routine has been tested on Epson Lx-800, HP LJ3P,  *	Epson FX-1170 and C.Itoh 8510RM  *	printers.  *	Quick exit on fail added.  */
@@ -2011,21 +1958,11 @@ decl_stmt|;
 name|int
 name|s
 decl_stmt|;
-ifdef|#
-directive|ifdef
-name|PC98
-name|int
-name|port
-decl_stmt|;
-else|#
-directive|else
 name|int
 name|trys
 decl_stmt|,
 name|port
 decl_stmt|;
-endif|#
-directive|endif
 name|u_int
 name|unit
 init|=
@@ -2717,9 +2654,6 @@ name|dev
 argument_list|)
 argument_list|)
 decl_stmt|;
-ifndef|#
-directive|ifndef
-name|PC98
 name|int
 name|port
 init|=
@@ -2727,8 +2661,6 @@ name|sc
 operator|->
 name|sc_port
 decl_stmt|;
-endif|#
-directive|endif
 if|if
 condition|(
 name|sc
@@ -3434,9 +3366,6 @@ name|lpt_sc
 operator|+
 name|unit
 decl_stmt|;
-ifndef|#
-directive|ifndef
-name|PC98
 name|int
 name|port
 init|=
@@ -3449,8 +3378,6 @@ decl_stmt|;
 name|int
 name|i
 decl_stmt|;
-endif|#
-directive|endif
 ifdef|#
 directive|ifdef
 name|INET
@@ -3984,12 +3911,6 @@ directive|endif
 block|}
 end_function
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|PC98
-end_ifndef
-
 begin_comment
 comment|/*  * Build the translation tables for the LPIP (BSD unix) protocol.  * We don't want to calculate these nasties in our tight loop, so we  * precalculate them when we initialize.  */
 end_comment
@@ -4244,15 +4165,6 @@ literal|0
 return|;
 block|}
 end_function
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* PC98 */
-end_comment
 
 begin_comment
 comment|/*  * Process an ioctl request.  */
@@ -5702,9 +5614,6 @@ name|sc_port
 operator|+
 name|lpt_status
 decl_stmt|;
-ifndef|#
-directive|ifndef
-name|PC98
 name|int
 name|lpt_ctrl_port
 init|=
@@ -5719,8 +5628,6 @@ name|sc_port
 operator|+
 name|lpt_control
 decl_stmt|;
-endif|#
-directive|endif
 name|int
 name|s
 decl_stmt|,
@@ -6460,12 +6367,13 @@ begin_comment
 comment|/* INET */
 end_comment
 
-begin_decl_stmt
+begin_expr_stmt
 specifier|static
-name|int
 name|lpt_devsw_installed
-decl_stmt|;
-end_decl_stmt
+operator|=
+literal|0
+expr_stmt|;
+end_expr_stmt
 
 begin_function
 specifier|static
@@ -6477,16 +6385,33 @@ modifier|*
 name|unused
 parameter_list|)
 block|{
+name|dev_t
+name|dev
+decl_stmt|;
 if|if
 condition|(
 operator|!
 name|lpt_devsw_installed
 condition|)
 block|{
+name|dev
+operator|=
+name|makedev
+argument_list|(
+name|CDEV_MAJOR
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
 name|cdevsw_add
 argument_list|(
 operator|&
+name|dev
+argument_list|,
+operator|&
 name|lpt_cdevsw
+argument_list|,
+name|NULL
 argument_list|)
 expr_stmt|;
 name|lpt_devsw_installed
