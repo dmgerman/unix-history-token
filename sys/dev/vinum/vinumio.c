3795e4661a631a784ea68ddb26fafb6434ff61ae
Greg Lehey <grog@FreeBSD.org>
1999-06-29 04:08:51 +0000

    close_drive:   Correct race condition between caller and daemon.
      Tripped-over-by:	Zach Heilig <zach@uffdaonline.net>
    			Bernd Walter <ticso@cicely.de>
    			Niels Chr. Bank-Pedersen <ncbp@bank-pedersen.dk>
    
    
    Former-commit-id: 69a25329a61326a5990ac08f0fd70d34c13bb2a0

--- sys/dev/vinum/vinumio.c
+++ sys/dev/vinum/vinumio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *    * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumio.c,v 1.29 1999/06/24 08:55:02 grog Exp $  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *    * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumio.c,v 1.24 1999/03/23 02:00:52 grog Exp grog $  */
 end_comment
 
 begin_include
@@ -853,19 +853,18 @@ modifier|*
 name|drive
 parameter_list|)
 block|{
-if|if
-condition|(
-name|drive
-operator|->
-name|vp
-condition|)
-block|{
 name|LOCKDRIVE
 argument_list|(
 name|drive
 argument_list|)
 expr_stmt|;
 comment|/* keep the daemon out */
+if|if
+condition|(
+name|drive
+operator|->
+name|vp
+condition|)
 name|close_locked_drive
 argument_list|(
 name|drive
@@ -877,7 +876,6 @@ argument_list|(
 name|drive
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 end_function
 
@@ -1179,7 +1177,6 @@ name|b_flags
 operator|=
 name|flag
 expr_stmt|;
-comment|/* get locked */
 name|bp
 operator|->
 name|b_dev
