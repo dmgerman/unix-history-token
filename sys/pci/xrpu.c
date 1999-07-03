abc2abcc38eee15caad339d6c8c4732a3b6ddcc5
Poul-Henning Kamp <phk@FreeBSD.org>
1999-07-03 08:23:00 +0000

    Bail after 5 attempts to read very noisy signals.
    Former-commit-id: 6148d2e213c682e5175be23e478da311f48ec5c5

--- sys/pci/xrpu.c
+++ sys/pci/xrpu.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: xrpu.c,v 1.12 1999/05/31 11:29:04 phk Exp $  *  * A very simple device driver for PCI cards based on Xilinx 6200 series  * FPGA/RPU devices.  Current Functionality is to allow you to open and  * mmap the entire thing into your program.  *  * Hardware currently supported:  *	www.vcc.com HotWorks 1 6216 based card.  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.org> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: xrpu.c,v 1.13 1999/06/22 10:31:30 phk Exp $  *  * A very simple device driver for PCI cards based on Xilinx 6200 series  * FPGA/RPU devices.  Current Functionality is to allow you to open and  * mmap the entire thing into your program.  *  * Hardware currently supported:  *	www.vcc.com HotWorks 1 6216 based card.  *  */
 end_comment
 
 begin_include
@@ -408,6 +408,8 @@ name|tc_priv
 decl_stmt|;
 name|int
 name|i
+decl_stmt|,
+name|j
 decl_stmt|;
 name|unsigned
 name|count1
@@ -454,6 +456,10 @@ name|tc
 operator|->
 name|tc_counter_mask
 expr_stmt|;
+name|j
+operator|=
+literal|0
+expr_stmt|;
 do|do
 block|{
 name|count1
@@ -482,6 +488,11 @@ condition|(
 name|ppscount
 operator|!=
 name|count1
+operator|&&
+operator|++
+name|j
+operator|<
+literal|5
 condition|)
 do|;
 name|pps_event
@@ -512,6 +523,10 @@ name|i
 index|]
 condition|)
 block|{
+name|j
+operator|=
+literal|0
+expr_stmt|;
 name|ppscount
 operator|=
 operator|*
@@ -556,6 +571,11 @@ condition|(
 name|ppscount
 operator|!=
 name|count1
+operator|&&
+operator|++
+name|j
+operator|<
+literal|5
 condition|)
 do|;
 name|pps_event
