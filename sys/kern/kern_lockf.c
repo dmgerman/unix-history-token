218bca21eee5788bb624914c0d01fc852ada3bd2
Bruce Evans <bde@FreeBSD.org>
1999-07-04 14:43:01 +0000

    Fixed corruption of the "blocked" list in lf_setlock() when tsleep() returns 0 after ptrace() attach and/or detach doesn't quite quite deliver a signal.  Perhaps the process shouldn't be woken in this case, but avoiding the problem is easy.
    PR:		12247
    
    Fixed a couple of places where mechanical fixing of compiler warnings
    caused misspelling of NOLOCKF as NULL.
    
    
    Former-commit-id: 85ec3fd75c409bcc431bde8fe501dded876a2d4f

--- sys/kern/kern_lockf.c
+++ sys/kern/kern_lockf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Scooter Morris at Genentech Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_lockf.c	8.3 (Berkeley) 1/6/94  * $Id: kern_lockf.c,v 1.21 1999/01/27 21:49:56 dillon Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Scooter Morris at Genentech Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_lockf.c	8.3 (Berkeley) 1/6/94  * $Id: kern_lockf.c,v 1.22 1999/05/08 22:46:46 dt Exp $  */
 end_comment
 
 begin_include
@@ -1026,9 +1026,6 @@ block|}
 endif|#
 directive|endif
 comment|/* LOCKF_DEBUG */
-if|if
-condition|(
-operator|(
 name|error
 operator|=
 name|tsleep
@@ -1044,16 +1041,15 @@ name|lockstr
 argument_list|,
 literal|0
 argument_list|)
-operator|)
-condition|)
-block|{
-comment|/* 			 * We may have been awakened by a signal (in 			 * which case we must remove ourselves from the 			 * blocked list) and/or by another process 			 * releasing a lock (in which case we have already 			 * been removed from the blocked list and our 			 * lf_next field set to NOLOCKF).                          */
+expr_stmt|;
+comment|/* 		 * We may have been awakened by a signal and/or by a 		 * debugger continuing us (in which cases we must remove 		 * ourselves from the blocked list) and/or by another 		 * process releasing a lock (in which case we have 		 * already been removed from the blocked list and our 		 * lf_next field set to NOLOCKF). 		 */
 if|if
 condition|(
 name|lock
 operator|->
 name|lf_next
 condition|)
+block|{
 name|TAILQ_REMOVE
 argument_list|(
 operator|&
@@ -1068,6 +1064,18 @@ argument_list|,
 name|lf_block
 argument_list|)
 expr_stmt|;
+name|lock
+operator|->
+name|lf_next
+operator|=
+name|NOLOCKF
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|error
+condition|)
+block|{
 name|free
 argument_list|(
 name|lock
@@ -1319,7 +1327,7 @@ operator|.
 name|tqh_first
 operator|)
 operator|!=
-name|NULL
+name|NOLOCKF
 condition|)
 block|{
 name|TAILQ_REMOVE
@@ -2898,7 +2906,7 @@ operator|.
 name|tqh_first
 operator|)
 operator|!=
-name|NULL
+name|NOLOCKF
 condition|)
 block|{
 name|TAILQ_REMOVE
