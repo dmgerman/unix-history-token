7cb4ed6f988cdd202255de863c637063c9756078
Jason Evans <jasone@FreeBSD.org>
1999-07-05 00:35:19 +0000

    Use growable stacks for thread stacks that are the default stack size.
    Cache discarded default thread stacks for use in subsequent thread creations.
    
    Create a red zone at the end of each stack (including the initial thread
    stack), with the hope of causing a segfault if a stack overflows.
    
    To activate these modifications, add -D_PTHREAD_GSTACK to CFLAGS in
    src/lib/libc_r/Makefile.  Since the modifications depend on the VM_STACK
    kernel option, I'm not sure how to safely use growable stacks by default.
    
    Testing, as well as algorithmic and stylistic comments are welcome.
    
    
    Former-commit-id: 9f683f0346c15b13951cda551f6bbe7019757b14

--- lib/libc_r/uthread/uthread_init.c
+++ lib/libc_r/uthread/uthread_init.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1995-1998 John Birrell<jb@cimlogic.com.au>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by John Birrell.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: uthread_init.c,v 1.11 1999/06/20 08:28:28 jb Exp $  */
+comment|/*  * Copyright (c) 1995-1998 John Birrell<jb@cimlogic.com.au>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by John Birrell.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: uthread_init.c,v 1.12 1999/06/23 15:01:21 dt Exp $  */
 end_comment
 
 begin_comment
@@ -72,6 +72,29 @@ include|#
 directive|include
 file|<sys/ttycom.h>
 end_include
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|_PTHREAD_GSTACK
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mman.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifdef
 ifdef|#
@@ -711,6 +734,52 @@ name|_sched_switch_hook
 operator|=
 name|NULL
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|_PTHREAD_GSTACK
+comment|/* Initialize the thread stack cache: */
+name|SLIST_INIT
+argument_list|(
+operator|&
+name|_stackq
+argument_list|)
+expr_stmt|;
+comment|/* Create the red zone for the main stack. */
+if|if
+condition|(
+name|MAP_FAILED
+operator|==
+name|mmap
+argument_list|(
+operator|(
+name|void
+operator|*
+operator|)
+name|PTHREAD_STACK_TOP
+operator|-
+name|PTHREAD_STACK_INITIAL
+argument_list|,
+name|PTHREAD_STACK_GUARD
+argument_list|,
+literal|0
+argument_list|,
+name|MAP_ANON
+argument_list|,
+operator|-
+literal|1
+argument_list|,
+literal|0
+argument_list|)
+condition|)
+block|{
+name|PANIC
+argument_list|(
+literal|"Cannot allocate red zone for initial thread"
+argument_list|)
+expr_stmt|;
+block|}
+endif|#
+directive|endif
 comment|/* 		 * Write a magic value to the thread structure 		 * to help identify valid ones: 		 */
 name|_thread_initial
 operator|->
--- lib/libpthread/thread/thr_gc.c
+++ lib/libpthread/thread/thr_gc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1998 John Birrell<jb@cimlogic.com.au>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by John Birrell.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: uthread_gc.c,v 1.3 1999/03/23 05:07:55 jb Exp $  *  * Garbage collector thread. Frees memory allocated for dead threads.  *  */
+comment|/*  * Copyright (c) 1998 John Birrell<jb@cimlogic.com.au>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by John Birrell.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY JOHN BIRRELL AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: uthread_gc.c,v 1.4 1999/06/20 08:28:25 jb Exp $  *  * Garbage collector thread. Frees memory allocated for dead threads.  *  */
 end_comment
 
 begin_include
@@ -26,6 +26,29 @@ include|#
 directive|include
 file|<sys/types.h>
 end_include
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|_PTHREAD_GSTACK
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mman.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
@@ -287,6 +310,63 @@ operator|!=
 name|NULL
 condition|)
 block|{
+ifdef|#
+directive|ifdef
+name|_PTHREAD_GSTACK
+if|if
+condition|(
+name|pthread
+operator|->
+name|attr
+operator|.
+name|stacksize_attr
+operator|==
+name|PTHREAD_STACK_DEFAULT
+condition|)
+block|{
+comment|/* Default-size stack.  Cache it: */
+name|struct
+name|stack
+modifier|*
+name|spare_stack
+init|=
+operator|(
+name|pthread
+operator|->
+name|stack
+operator|+
+name|PTHREAD_STACK_DEFAULT
+operator|-
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|stack
+argument_list|)
+operator|)
+decl_stmt|;
+name|SLIST_INSERT_HEAD
+argument_list|(
+operator|&
+name|_stackq
+argument_list|,
+name|spare_stack
+argument_list|,
+name|qe
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+comment|/* Non-standard stack size.  free() it outside the locks: */
+name|p_stack
+operator|=
+name|pthread
+operator|->
+name|stack
+expr_stmt|;
+block|}
+else|#
+directive|else
 comment|/* 					 * Point to the stack that must 					 * be freed outside the locks: 					 */
 name|p_stack
 operator|=
@@ -294,6 +374,8 @@ name|pthread
 operator|->
 name|stack
 expr_stmt|;
+endif|#
+directive|endif
 block|}
 comment|/* 				 * Point to the thread structure that must 				 * be freed outside the locks: 				 */
 name|pthread_cln
@@ -321,6 +403,63 @@ operator|!=
 name|NULL
 condition|)
 block|{
+ifdef|#
+directive|ifdef
+name|_PTHREAD_GSTACK
+if|if
+condition|(
+name|pthread
+operator|->
+name|attr
+operator|.
+name|stacksize_attr
+operator|==
+name|PTHREAD_STACK_DEFAULT
+condition|)
+block|{
+comment|/* Default-size stack.  Cache it: */
+name|struct
+name|stack
+modifier|*
+name|spare_stack
+init|=
+operator|(
+name|pthread
+operator|->
+name|stack
+operator|+
+name|PTHREAD_STACK_DEFAULT
+operator|-
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|stack
+argument_list|)
+operator|)
+decl_stmt|;
+name|SLIST_INSERT_HEAD
+argument_list|(
+operator|&
+name|_stackq
+argument_list|,
+name|spare_stack
+argument_list|,
+name|qe
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+comment|/* Non-standard stack size.  free() it outside the locks: */
+name|p_stack
+operator|=
+name|pthread
+operator|->
+name|stack
+expr_stmt|;
+block|}
+else|#
+directive|else
 comment|/* 					 * Point to the stack that must 					 * be freed outside the locks: 					 */
 name|p_stack
 operator|=
@@ -328,6 +467,8 @@ name|pthread
 operator|->
 name|stack
 expr_stmt|;
+endif|#
+directive|endif
 comment|/* 					 * NULL the stack pointer now 					 * that the memory has been freed:  					 */
 name|pthread
 operator|->
