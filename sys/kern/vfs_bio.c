f8174b2f1c10e29cc03bc63a23d35362e2244432
Kirk McKusick <mckusick@FreeBSD.org>
1999-07-08 17:58:55 +0000

    Condition in KASSERT was reversed.
    Former-commit-id: 5ebc99271f2b369955489dd0ac33e35c748aade0

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.220 1999/07/04 00:25:27 mckusick Exp $  */
+comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.221 1999/07/08 06:05:52 mckusick Exp $  */
 end_comment
 
 begin_comment
@@ -1740,7 +1740,7 @@ argument_list|(
 name|bp
 argument_list|)
 operator|==
-literal|0
+literal|1
 argument_list|,
 operator|(
 literal|"bremfree: bp %p not locked"
