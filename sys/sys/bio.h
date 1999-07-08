2cb5f46fbc66cea547e4dbda14ef12b364cba9ce
Kirk McKusick <mckusick@FreeBSD.org>
1999-07-08 06:06:00 +0000

    These changes appear to give us benefits with both small (32MB) and large (1G) memory machine configurations.  I was able to run 'dbench 32' on a 32MB system without bring the machine to a grinding halt.
        * buffer cache hash table now dynamically allocated.  This will
          have no effect on memory consumption for smaller systems and
          will help scale the buffer cache for larger systems.
    
        * minor enhancement to pmap_clearbit().  I noticed that
          all the calls to it used constant arguments.  Making
          it an inline allows the constants to propogate to
          deeper inlines and should produce better code.
    
        * removal of inherent vfs_ioopt support through the emplacement
          of appropriate #ifdef's, with John's permission.  If we do not
          find a use for it by the end of the year we will remove it entirely.
    
        * removal of getnewbufloops* counters & sysctl's - no longer
          necessary for debugging, getnewbuf() is now optimal.
    
        * buffer hash table functions removed from sys/buf.h and localized
          to vfs_bio.c
    
        * VFS_BIO_NEED_DIRTYFLUSH flag and support code added
          ( bwillwrite() ), allowing processes to block when too many dirty
          buffers are present in the system.
    
        * removal of a softdep test in bdwrite() that is no longer necessary
          now that bdwrite() no longer attempts to flush dirty buffers.
    
        * slight optimization added to bqrelse() - there is no reason
          to test for available buffer space on B_DELWRI buffers.
    
        * addition of reverse-scanning code to vfs_bio_awrite().
          vfs_bio_awrite() will attempt to locate clusterable areas
          in both the forward and reverse direction relative to the
          offset of the buffer passed to it.  This will probably not
          make much of a difference now, but I believe we will start
          to rely on it heavily in the future if we decide to shift
          some of the burden of the clustering closer to the actual
          I/O initiation.
    
        * Removal of the newbufcnt and lastnewbuf counters that Kirk
          added.  They do not fix any race conditions that haven't already
          been fixed by the gbincore() test done after the only call
          to getnewbuf().  getnewbuf() is a static, so there is no chance
          of it being misused by other modules.  ( Unless Kirk can think
          of a specific thing that this code fixes.  I went through it
          very carefully and didn't see anything ).
    
        * removal of VOP_ISLOCKED() check in flushbufqueues().  I do not
          think this check is necessary, the buffer should flush properly
          whether the vnode is locked or not. ( yes? ).
    
        * removal of extra arguments passed to getnewbuf() that are not
          necessary.
    
        * missed cluster_wbuild() that had to be a cluster_wbuild_wb() in
          vfs_cluster.c
    
        * vn_write() now calls bwillwrite() *PRIOR* to locking the vnode,
          which should greatly aid flushing operations in heavy load
          situations - both the pageout and update daemons will be able
          to operate more efficiently.
    
        * removal of b_usecount.  We may add it back in later but for now
          it is useless.  Prior implementations of the buffer cache never
          had enough buffers for it to be useful, and current implementations
          which make more buffers available might not benefit relative to
          the amount of sophistication required to implement a b_usecount.
          Straight LRU should work just as well, especially when most things
          are VMIO backed.  I expect that (even though John will not like
          this assumption) directories will become VMIO backed some point soon.
    
    Submitted by:	Matthew Dillon <dillon@backplane.com>
    Reviewed by:	Kirk McKusick <mckusick@mckusick.com>
    
    
    Former-commit-id: 89957cc4e490b97f4b257110a896a9c944fd8b9b

--- sys/sys/bio.h
+++ sys/sys/bio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)buf.h	8.9 (Berkeley) 3/30/95  * $Id: buf.h,v 1.74 1999/06/29 05:59:47 peter Exp $  */
+comment|/*  * Copyright (c) 1982, 1986, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  * (c) UNIX System Laboratories, Inc.  * All or some portions of this file are derived from material licensed  * to the University of California by American Telephone and Telegraph  * Co. or Unix System Laboratories, Inc. and are reproduced herein with  * the permission of UNIX System Laboratories, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)buf.h	8.9 (Berkeley) 3/30/95  * $Id: buf.h,v 1.75 1999/07/04 00:25:32 mckusick Exp $  */
 end_comment
 
 begin_ifndef
@@ -228,9 +228,9 @@ decl_stmt|;
 comment|/* buffer queue index */
 name|unsigned
 name|char
-name|b_usecount
+name|b_unused1
 decl_stmt|;
-comment|/* buffer use count */
+comment|/* unused field */
 name|unsigned
 name|char
 name|b_xflags
@@ -1773,34 +1773,6 @@ comment|/* KERNEL */
 end_comment
 
 begin_comment
-comment|/*  * number of buffer hash entries  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|BUFHSZ
-value|512
-end_define
-
-begin_comment
-comment|/*  * buffer hash table calculation, originally by David Greenman  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|BUFHASH
-parameter_list|(
-name|vnp
-parameter_list|,
-name|bn
-parameter_list|)
-define|\
-value|(&bufhashtbl[(((uintptr_t)(vnp)>> 7)+(int)(bn)) % BUFHSZ])
-end_define
-
-begin_comment
 comment|/*  * Definitions for the buffer free lists.  */
 end_comment
 
@@ -2032,8 +2004,32 @@ struct_decl|;
 end_struct_decl
 
 begin_decl_stmt
+name|vm_offset_t
+name|bufhashinit
+name|__P
+argument_list|(
+operator|(
+name|vm_offset_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 name|void
 name|bufinit
+name|__P
+argument_list|(
+operator|(
+name|void
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|bwillwrite
 name|__P
 argument_list|(
 operator|(
--- sys/ufs/ufs/ufs_readwrite.c
+++ sys/ufs/ufs/ufs_readwrite.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_readwrite.c	8.11 (Berkeley) 5/8/95  * $Id: ufs_readwrite.c,v 1.57 1999/01/28 00:57:56 dillon Exp $  */
+comment|/*-  * Copyright (c) 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ufs_readwrite.c	8.11 (Berkeley) 5/8/95  * $Id: ufs_readwrite.c,v 1.58 1999/04/05 19:38:30 julian Exp $  */
 end_comment
 
 begin_define
@@ -351,9 +351,9 @@ argument_list|(
 name|object
 argument_list|)
 expr_stmt|;
-if|#
-directive|if
-literal|1
+ifdef|#
+directive|ifdef
+name|ENABLE_VFS_IOOPT
 comment|/* 	 * If IO optimisation is turned on, 	 * and we are NOT a VM based IO request,  	 * (i.e. not headed for the buffer cache) 	 * but there IS a vm object associated with it. 	 */
 if|if
 condition|(
@@ -511,9 +511,9 @@ operator|<=
 literal|0
 condition|)
 break|break;
-if|#
-directive|if
-literal|1
+ifdef|#
+directive|ifdef
+name|ENABLE_VFS_IOOPT
 if|if
 condition|(
 operator|(
@@ -903,6 +903,9 @@ operator|=
 name|size
 expr_stmt|;
 block|}
+ifdef|#
+directive|ifdef
+name|ENABLE_VFS_IOOPT
 if|if
 condition|(
 name|vfs_ioopt
@@ -965,6 +968,8 @@ argument_list|)
 expr_stmt|;
 block|}
 else|else
+endif|#
+directive|endif
 block|{
 comment|/* 			 * otherwise use the general form 			 */
 name|error
