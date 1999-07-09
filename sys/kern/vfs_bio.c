776c535cbdb066d24aa7f2678de7984694e248c2
Peter Wemm <peter@FreeBSD.org>
1999-07-09 16:41:19 +0000

    bufhashinit() is called with a caddr_t and is expected to return the same in both the alpha and i386 ports.
    Former-commit-id: 6e94668623a1cd0a188335d6f0ed6e947493626e

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.221 1999/07/08 06:05:52 mckusick Exp $  */
+comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.222 1999/07/08 17:58:55 mckusick Exp $  */
 end_comment
 
 begin_comment
@@ -1244,10 +1244,10 @@ comment|/*  * Initialize buffer headers and related structures.   */
 end_comment
 
 begin_function
-name|vm_offset_t
+name|caddr_t
 name|bufhashinit
 parameter_list|(
-name|vm_offset_t
+name|caddr_t
 name|vaddr
 parameter_list|)
 block|{
