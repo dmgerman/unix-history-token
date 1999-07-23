5a7e7547c65e4748cac14ac00d5fb341651ec7d3
Bill Paul <wpaul@FreeBSD.org>
1999-07-23 02:10:11 +0000

    Convert the Alteon Tigon gigabit ethernet driver to newbus. Also upgrade to the latest firmware release from Alteon (12.3.12).
    Former-commit-id: 6875469874bffe4098b04caf4231745fd88fd5d9

--- sys/pci/if_tireg.h
+++ sys/pci/if_tireg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998, 1999  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_tireg.h,v 1.46 1999/07/05 19:20:31 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998, 1999  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_tireg.h,v 1.46 1999/07/05 19:20:31 wpaul Exp wpaul $  */
 end_comment
 
 begin_comment
@@ -399,7 +399,7 @@ begin_define
 define|#
 directive|define
 name|TI_FIRMWARE_FIX
-value|0xa
+value|0xc
 end_define
 
 begin_comment
@@ -4388,17 +4388,22 @@ comment|/* interface info */
 name|bus_space_handle_t
 name|ti_bhandle
 decl_stmt|;
-ifdef|#
-directive|ifdef
-name|__alpha__
-name|vm_offset_t
-name|ti_vhandle
-decl_stmt|;
-comment|/* alpha dense space vaddr */
-endif|#
-directive|endif
 name|bus_space_tag_t
 name|ti_btag
+decl_stmt|;
+name|void
+modifier|*
+name|ti_intrhand
+decl_stmt|;
+name|struct
+name|resource
+modifier|*
+name|ti_irq
+decl_stmt|;
+name|struct
+name|resource
+modifier|*
+name|ti_res
 decl_stmt|;
 name|struct
 name|ifmedia
@@ -4417,10 +4422,6 @@ name|u_int8_t
 name|ti_linkstat
 decl_stmt|;
 comment|/* Link state */
-name|caddr_t
-name|ti_rdata_ptr
-decl_stmt|;
-comment|/* Raw ring data */
 name|struct
 name|ti_ring_data
 modifier|*
