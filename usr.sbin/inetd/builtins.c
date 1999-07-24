d2a2fb423e25f1aeebf05d86779d378df1bfd876
Brian F. Feldman <green@FreeBSD.org>
1999-07-24 16:24:03 +0000

    Clean up to match style(9) more closely. This should fix the problem of people having ants in their pants ;)
    Former-commit-id: 1d77013c59e8d3f491847c494500443b9aca1c47

--- usr.sbin/inetd/builtins.c
+++ usr.sbin/inetd/builtins.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1983, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: builtins.c,v 1.8 1999/07/24 12:35:50 sheldonh Exp $  *  */
+comment|/*-  * Copyright (c) 1983, 1991, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: builtins.c,v 1.9 1999/07/24 13:02:09 sheldonh Exp $  *  */
 end_comment
 
 begin_include
@@ -1506,11 +1506,6 @@ name|ucred
 name|uc
 decl_stmt|;
 name|struct
-name|passwd
-modifier|*
-name|pw
-decl_stmt|;
-name|struct
 name|timeval
 name|tv
 init|=
@@ -1519,6 +1514,11 @@ literal|10
 block|,
 literal|0
 block|}
+decl_stmt|;
+name|struct
+name|passwd
+modifier|*
+name|pw
 decl_stmt|;
 name|fd_set
 name|fdset
@@ -1607,6 +1607,11 @@ condition|(
 name|argc
 condition|)
 block|{
+name|int
+name|sec
+decl_stmt|,
+name|usec
+decl_stmt|;
 while|while
 condition|(
 operator|(
@@ -1659,12 +1664,6 @@ break|break;
 case|case
 literal|'t'
 case|:
-block|{
-name|int
-name|sec
-decl_stmt|,
-name|usec
-decl_stmt|;
 switch|switch
 condition|(
 name|sscanf
@@ -1712,7 +1711,7 @@ argument_list|)
 expr_stmt|;
 break|break;
 block|}
-block|}
+break|break;
 default|default:
 break|break;
 block|}
