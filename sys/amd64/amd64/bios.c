c6666c98ff2027d3fb69ff380291fbc92e0efdd1
Michael Smith <msmith@FreeBSD.org>
1999-07-29 18:47:39 +0000

    Formatting-only cleanup accidentally omitted from the patch merge in the previous major update.  Bring new code into style alignment with the existing code.  No functional changes.
    Former-commit-id: 1cd5d3a2bfc7514ab87a0031faa1be1096e08c05

--- sys/amd64/amd64/bios.c
+++ sys/amd64/amd64/bios.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 Michael Smith  * Copyright (c) 1998 Jonathan Lemon  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: bios.c,v 1.13 1999/07/29 01:49:17 msmith Exp $  */
+comment|/*-  * Copyright (c) 1997 Michael Smith  * Copyright (c) 1998 Jonathan Lemon  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: bios.c,v 1.14 1999/07/29 06:48:26 msmith Exp $  */
 end_comment
 
 begin_comment
@@ -1574,7 +1574,7 @@ name|flags
 operator||=
 name|BIOSUTIL_FLAG
 expr_stmt|;
-comment|/* FALL THROUGH */
+comment|/* FALLTHROUGH */
 case|case
 literal|'D'
 case|:
@@ -1697,7 +1697,7 @@ operator|==
 name|IdlePTD
 condition|)
 block|{
-comment|/* 		 * no page table, so create one and install it. 		 */
+comment|/* 	 * no page table, so create one and install it. 	 */
 name|pte
 operator|=
 operator|(
@@ -1743,7 +1743,7 @@ expr_stmt|;
 block|}
 else|else
 block|{
-comment|/* 		 * this is a user-level page table  		 */
+comment|/* 	 * this is a user-level page table  	 */
 name|pte
 operator|=
 operator|(
@@ -1754,7 +1754,7 @@ operator|&
 name|PTmap
 expr_stmt|;
 block|}
-comment|/* 	 * install pointer to page 0.  we don't need to flush the tlb, 	 * since there should not be a previous mapping for page 0. 	 */
+comment|/*      * install pointer to page 0.  we don't need to flush the tlb,      * since there should not be a previous mapping for page 0.      */
 operator|*
 name|pte
 operator|=
@@ -2093,7 +2093,7 @@ argument_list|)
 expr_stmt|;
 comment|/* ... and free it */
 block|}
-comment|/* 	 * XXX only needs to be invlpg(0) but that doesn't work on the 386  	 */
+comment|/*      * XXX only needs to be invlpg(0) but that doesn't work on the 386       */
 name|invltlb
 argument_list|()
 expr_stmt|;
