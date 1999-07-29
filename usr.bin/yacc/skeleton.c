00439a6e8febde4d19c5c2542cf53f86e26001fc
David E. O'Brien <obrien@FreeBSD.org>
1999-07-29 09:42:14 +0000

    * Don't assume realloc() can take NULL as first arg.  Yacc needs to   generate portable code... * Correctly define yyparse() (ie, K&R vs. C++/ANSI-C)
    Obtained from:	OpenBSD revs 1.5 & 1.10
    
    
    Former-commit-id: f72261d7a7daee519fbc8b92efe1867c5595d00a

--- usr.bin/yacc/skeleton.c
+++ usr.bin/yacc/skeleton.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Robert Paul Corbett.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: skeleton.c,v 1.15 1999/04/18 13:37:49 peter Exp $  */
+comment|/*  * Copyright (c) 1989 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Robert Paul Corbett.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: skeleton.c,v 1.16 1999/07/29 08:47:30 obrien Exp $  */
 end_comment
 
 begin_ifndef
@@ -256,7 +256,11 @@ literal|"        newsize = YYMAXDEPTH;"
 block|,
 literal|"    i = yyssp - yyss;"
 block|,
-literal|"    if ((newss = (short *)realloc(yyss, newsize * sizeof *newss)) == NULL)"
+literal|"    newss = yyss ? (short *)realloc(yyss, newsize * sizeof *newss) :"
+block|,
+literal|"      (short *)malloc(newsize * sizeof *newss);"
+block|,
+literal|"    if (newss == NULL)"
 block|,
 literal|"        return -1;"
 block|,
@@ -264,7 +268,11 @@ literal|"    yyss = newss;"
 block|,
 literal|"    yyssp = newss + i;"
 block|,
-literal|"    if ((newvs = (YYSTYPE *)realloc(yyvs, newsize * sizeof *newvs)) == NULL)"
+literal|"    newvs = yyvs ? (YYSTYPE *)realloc(yyvs, newsize * sizeof *newvs) :"
+block|,
+literal|"      (YYSTYPE *)malloc(newsize * sizeof *newvs);"
+block|,
+literal|"    if (newvs == NULL)"
 block|,
 literal|"        return -1;"
 block|,
@@ -294,7 +302,15 @@ literal|""
 block|,
 literal|"int"
 block|,
+literal|"#if defined(__cplusplus) || __STDC__"
+block|,
+literal|"yyparse(void)"
+block|,
+literal|"#else"
+block|,
 literal|"yyparse()"
+block|,
+literal|"#endif"
 block|,
 literal|"{"
 block|,
