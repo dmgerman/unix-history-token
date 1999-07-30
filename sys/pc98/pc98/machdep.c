827d719d4cd6e42f2546049a2fc8968f157d6b21
Takenori KATO <kato@FreeBSD.org>
1999-07-30 11:42:05 +0000

    Sync with sys/i386/i386/machdep.c revision 1.357.
    Former-commit-id: 7d7d8ead68894063d694350153a102afec377219

--- sys/pc98/pc98/machdep.c
+++ sys/pc98/pc98/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.126 1999/07/09 12:51:11 kato Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.127 1999/07/26 12:14:00 kato Exp $  */
 end_comment
 
 begin_include
@@ -4688,11 +4688,11 @@ literal|0
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GAPMCODE32_SEL 9 APM BIOS 32-bit interface (32bit Code) */
+comment|/* GBIOSCODE32_SEL 9 BIOS 32-bit interface (32bit Code) */
 block|{
 literal|0
 block|,
-comment|/* segment base address (overwritten by APM)  */
+comment|/* segment base address (overwritten)  */
 literal|0xfffff
 block|,
 comment|/* length */
@@ -4716,11 +4716,11 @@ literal|1
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GAPMCODE16_SEL 10 APM BIOS 32-bit interface (16bit Code) */
+comment|/* GBIOSCODE16_SEL 10 BIOS 32-bit interface (16bit Code) */
 block|{
 literal|0
 block|,
-comment|/* segment base address (overwritten by APM)  */
+comment|/* segment base address (overwritten)  */
 literal|0xfffff
 block|,
 comment|/* length */
@@ -4744,11 +4744,11 @@ literal|1
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GAPMDATA_SEL	11 APM BIOS 32-bit interface (Data) */
+comment|/* GBIOSDATA_SEL 11 BIOS 32-bit interface (Data) */
 block|{
 literal|0
 block|,
-comment|/* segment base address (overwritten by APM) */
+comment|/* segment base address (overwritten) */
 literal|0xfffff
 block|,
 comment|/* length */
@@ -4766,6 +4766,62 @@ block|,
 literal|0
 block|,
 literal|1
+block|,
+comment|/* default 32 vs 16 bit size */
+literal|1
+comment|/* limit granularity (byte/page units)*/
+block|}
+block|,
+comment|/* GBIOSUTIL_SEL 12 BIOS 16-bit interface (Utility) */
+block|{
+literal|0
+block|,
+comment|/* segment base address (overwritten) */
+literal|0xfffff
+block|,
+comment|/* length */
+name|SDT_MEMRWA
+block|,
+comment|/* segment type */
+literal|0
+block|,
+comment|/* segment descriptor priority level */
+literal|1
+block|,
+comment|/* segment descriptor present */
+literal|0
+block|,
+literal|0
+block|,
+literal|0
+block|,
+comment|/* default 32 vs 16 bit size */
+literal|1
+comment|/* limit granularity (byte/page units)*/
+block|}
+block|,
+comment|/* GBIOSARGS_SEL 13 BIOS 16-bit interface (Arguments) */
+block|{
+literal|0
+block|,
+comment|/* segment base address (overwritten) */
+literal|0xfffff
+block|,
+comment|/* length */
+name|SDT_MEMRWA
+block|,
+comment|/* segment type */
+literal|0
+block|,
+comment|/* segment descriptor priority level */
+literal|1
+block|,
+comment|/* segment descriptor present */
+literal|0
+block|,
+literal|0
+block|,
+literal|0
 block|,
 comment|/* default 32 vs 16 bit size */
 literal|1
