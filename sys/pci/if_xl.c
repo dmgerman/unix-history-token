020efb942913fbb6a163cf8e0a31837476e1997a
Bill Paul <wpaul@FreeBSD.org>
1999-08-02 21:07:22 +0000

    MFC: issue an RX and TX reset in xl_reset() to smack the 3c905C to its senses.
    Former-commit-id: 0a1892be030c244bb73b93655392c20a15bcb145

--- sys/pci/if_xl.c
+++ sys/pci/if_xl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_xl.c,v 1.22.2.16 1999/07/08 00:43:29 wpaul Exp $  */
+comment|/*  * Copyright (c) 1997, 1998  *	Bill Paul<wpaul@ctr.columbia.edu>.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Bill Paul.  * 4. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: if_xl.c,v 1.109 1999/08/02 20:24:59 wpaul Exp $  */
 end_comment
 
 begin_comment
@@ -232,7 +232,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"$Id: if_xl.c,v 1.22.2.16 1999/07/08 00:43:29 wpaul Exp $"
+literal|"$Id: if_xl.c,v 1.109 1999/08/02 20:24:59 wpaul Exp $"
 decl_stmt|;
 end_decl_stmt
 
@@ -5255,6 +5255,35 @@ operator|->
 name|xl_unit
 argument_list|)
 expr_stmt|;
+comment|/* Reset TX and RX. */
+name|CSR_WRITE_2
+argument_list|(
+name|sc
+argument_list|,
+name|XL_COMMAND
+argument_list|,
+name|XL_CMD_RX_RESET
+argument_list|)
+expr_stmt|;
+name|xl_wait
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
+name|CSR_WRITE_2
+argument_list|(
+name|sc
+argument_list|,
+name|XL_COMMAND
+argument_list|,
+name|XL_CMD_TX_RESET
+argument_list|)
+expr_stmt|;
+name|xl_wait
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 comment|/* Wait a little while for the chip to get its brains in order. */
 name|DELAY
 argument_list|(
@@ -7722,6 +7751,11 @@ operator|->
 name|ifmedia
 argument_list|,
 name|media
+argument_list|)
+expr_stmt|;
+name|xl_stop
+argument_list|(
+name|sc
 argument_list|)
 expr_stmt|;
 comment|/* 	 * Call MI attach routines. 	 */
@@ -11997,6 +12031,11 @@ name|sc
 argument_list|)
 expr_stmt|;
 name|xl_rxeof
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
+name|xl_reset
 argument_list|(
 name|sc
 argument_list|)
