b399ef3674e439cb5a45ff4688e7070145cfa3a9
Brian F. Feldman <green@FreeBSD.org>
1999-08-04 18:53:50 +0000

    Fix fd race conditions (during shared fd table usage.) Badfileops is now used in f_ops in place of NULL, and modifications to the files are more carefully ordered. f_ops should also be set to &badfileops upon "close" of a file.
    This does not fix other problems mentioned in this PR than the first
    one.
    
    PR:		11629
    Reviewed by:	peter
    
    Former-commit-id: a4675ece55da9ff0aafd30a4c402f3f9df52998f

--- sys/dev/streams/streams.c
+++ sys/dev/streams/streams.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1998 Mark Newton  * Copyright (c) 1994 Christos Zoulas  * Copyright (c) 1997 Todd Vierling  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The names of the authors may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * Stolen from NetBSD /sys/compat/svr4/svr4_net.c.  Pseudo-device driver  * skeleton produced from /usr/share/examples/drivers/make_pseudo_driver.sh  * in 3.0-980524-SNAP then hacked a bit (but probably not enough :-).  *  * $Id$  */
+comment|/*  * Copyright (c) 1998 Mark Newton  * Copyright (c) 1994 Christos Zoulas  * Copyright (c) 1997 Todd Vierling  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The names of the authors may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * Stolen from NetBSD /sys/compat/svr4/svr4_net.c.  Pseudo-device driver  * skeleton produced from /usr/share/examples/drivers/make_pseudo_driver.sh  * in 3.0-980524-SNAP then hacked a bit (but probably not enough :-).  *  * $Id: streams.c,v 1.8 1999/08/01 12:51:06 newton Exp $  */
 end_comment
 
 begin_include
@@ -873,17 +873,20 @@ return|;
 block|}
 name|fp
 operator|->
+name|f_data
+operator|=
+operator|(
+name|caddr_t
+operator|)
+name|so
+expr_stmt|;
+name|fp
+operator|->
 name|f_flag
 operator|=
 name|FREAD
 operator||
 name|FWRITE
-expr_stmt|;
-name|fp
-operator|->
-name|f_type
-operator|=
-name|DTYPE_SOCKET
 expr_stmt|;
 name|fp
 operator|->
@@ -894,12 +897,9 @@ name|svr4_netops
 expr_stmt|;
 name|fp
 operator|->
-name|f_data
+name|f_type
 operator|=
-operator|(
-name|caddr_t
-operator|)
-name|so
+name|DTYPE_SOCKET
 expr_stmt|;
 operator|(
 name|void
@@ -1217,13 +1217,6 @@ operator|->
 name|so_emuldata
 return|;
 comment|/* Allocate a new one. */
-name|fp
-operator|->
-name|f_ops
-operator|=
-operator|&
-name|svr4_netops
-expr_stmt|;
 name|st
 operator|=
 name|malloc
@@ -1276,6 +1269,13 @@ operator|->
 name|so_emuldata
 operator|=
 name|st
+expr_stmt|;
+name|fp
+operator|->
+name|f_ops
+operator|=
+operator|&
+name|svr4_netops
 expr_stmt|;
 return|return
 name|st
