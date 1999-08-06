e69eae829b9b9f59cb927c8b965c6eee129b0ba3
Soren Schmidt <sos@FreeBSD.org>
1999-08-06 17:39:38 +0000

    Add limitted support for the Promise Ultra/66 controller. Its only supported upto UDMA33 like the old Promise, but it works now. More when I have specs ....
    Former-commit-id: 21e2abf04c46a915f9a8642fec89080f850b9e7a

--- sys/dev/ata/ata-all.c
+++ sys/dev/ata/ata-all.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998,1999 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: ata-all.c,v 1.14 1999/05/20 09:12:02 sos Exp $  */
+comment|/*-  * Copyright (c) 1998,1999 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *  $Id: ata-all.c,v 1.15 1999/06/25 09:02:56 sos Exp $  */
 end_comment
 
 begin_include
@@ -822,6 +822,13 @@ name|dev
 argument_list|)
 operator|==
 name|PCIS_STORAGE_RAID
+operator|||
+name|pci_get_subclass
+argument_list|(
+name|dev
+argument_list|)
+operator|==
+name|PCIS_STORAGE_OTHER
 operator|)
 condition|)
 block|{
@@ -856,6 +863,12 @@ literal|0x4d33105a
 case|:
 return|return
 literal|"Promise Ultra/33 IDE controller"
+return|;
+case|case
+literal|0x4d38105a
+case|:
+return|return
+literal|"Promise Ultra/66 IDE controller"
 return|;
 case|case
 literal|0x522910b9
@@ -1040,6 +1053,10 @@ condition|(
 name|type
 operator|==
 literal|0x4d33105a
+operator|||
+name|type
+operator|==
+literal|0x4d38105a
 condition|)
 block|{
 name|iobase_1
@@ -1526,6 +1543,10 @@ condition|(
 name|type
 operator|==
 literal|0x4d33105a
+operator|||
+name|type
+operator|==
+literal|0x4d38105a
 condition|)
 name|bus_setup_intr
 argument_list|(
@@ -1671,6 +1692,10 @@ condition|(
 name|type
 operator|!=
 literal|0x4d33105a
+operator|&&
+name|type
+operator|!=
+literal|0x4d38105a
 condition|)
 block|{
 name|irq
