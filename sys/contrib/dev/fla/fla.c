1278f5ab0bfea695ec1bcab6cc9c3b639ad36fca
Bruce Evans <bde@FreeBSD.org>
1999-08-07 13:11:12 +0000

    Fixed a bogus include pathname which broke compiling in an obj directory.
    Former-commit-id: 3857b968ae7072ab3cae509fccd9e15e3e23859e

--- sys/contrib/dev/fla/fla.c
+++ sys/contrib/dev/fla/fla.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: fla.c,v 1.3 1999/08/06 15:22:09 phk Exp $   *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $Id: fla.c,v 1.1 1999/08/06 15:59:07 phk Exp $   *  */
 end_comment
 
 begin_include
@@ -292,7 +292,7 @@ end_endif
 begin_include
 include|#
 directive|include
-file|<../../contrib/dev/fla/msysosak.h>
+file|<contrib/dev/fla/msysosak.h>
 end_include
 
 begin_expr_stmt
