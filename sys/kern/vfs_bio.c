8c4b35b57866d9ebbc6a185388b860b2315fbd07
Tor Egge <tegge@FreeBSD.org>
1999-08-09 00:48:50 +0000

    MFC: If we still haven't got a sufficient number of free buffers after the call to flushdirtybuffers() then sleep in waitfreebuffers(). PR:		11697
    Former-commit-id: 9dfce2a265ee28bfa52e43e1ae859c95f568897e

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.193.2.4 1999/03/03 17:43:17 julian Exp $  */
+comment|/*  * Copyright (c) 1994,1997 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Absolutely no warranty of function or purpose is made by the author  *		John S. Dyson.  *  * $Id: vfs_bio.c,v 1.193.2.5 1999/04/20 19:54:20 dt Exp $  */
 end_comment
 
 begin_comment
@@ -5499,7 +5499,7 @@ expr_stmt|;
 if|if
 condition|(
 name|numfreebuffers
-operator|<
+operator|>=
 name|hifreebuffers
 condition|)
 break|break;
