708b7b8691f7bee0fc1e25380a274085b9fd3119
Luigi Rizzo <luigi@FreeBSD.org>
1999-08-11 15:34:47 +0000

    Implement probabilistic rule match in ipfw. Each rule can be associated with a match probability to achieve non-deterministic behaviour of the firewall. This can be extremely useful for testing purposes such as simulating random packet drop without having to use dummynet (which already does the same thing), and simulating multipath effects and the associated out-of-order delivery (this time in conjunction with dummynet).
    The overhead on normal rules is just one comparison with 0.
    
    Since it would have been trivial to implement this by just adding
    a field to the ip_fw structure, I decided to do it in a
    backward-compatible way (i.e. struct ip_fw is unchanged, and as a
    consequence you don't need to recompile ipfw if you don't want to
    use this feature), since this was also useful for -STABLE.
    
    When, at some point, someone decides to change struct ip_fw, please
    add a length field and a version number at the beginning, so userland
    apps can keep working even if they are out of sync with the kernel.
    
    
    Former-commit-id: d437e246490bffc7788cf991407de716f5cde360

--- sys/netinet/ip_fw.h
+++ sys/netinet/ip_fw.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.h,v 1.39 1999/07/28 22:22:57 green Exp $  */
+comment|/*  * Copyright (c) 1993 Daniel Boulet  * Copyright (c) 1994 Ugen J.S.Antsilevich  *  * Redistribution and use in source forms, with and without modification,  * are permitted provided that this entire comment appears intact.  *  * Redistribution in binary form may occur without any restrictions.  * Obviously, it would be nice if you gave credit where credit is due  * but requiring it would be too onerous.  *  * This software is provided ``AS IS'' without any warranties of any kind.  *  *	$Id: ip_fw.h,v 1.40 1999/08/01 16:57:16 green Exp $  */
 end_comment
 
 begin_ifndef
@@ -209,6 +209,32 @@ name|u_int64_t
 name|fw_loghighest
 decl_stmt|;
 comment|/* highest number packet to log */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/*  * extended ipfw structure... some fields in the original struct  * can be used to pass parameters up/down, namely pointers  *     void *pipe_ptr  *     void *next_rule_ptr   * some others can be used to pass parameters down, namely counters etc.  *     u_int64_t fw_pcnt,fw_bcnt;  *     long timestamp;  */
+end_comment
+
+begin_struct
+struct|struct
+name|ip_fw_ext
+block|{
+comment|/* extended structure */
+name|struct
+name|ip_fw
+name|rule
+decl_stmt|;
+comment|/* must be at offset 0 */
+name|long
+name|dont_match_prob
+decl_stmt|;
+comment|/* 0x7fffffff means 1.0, always fail */
+name|u_int
+name|param1
+decl_stmt|;
+comment|/* unused at the moment */
 block|}
 struct|;
 end_struct
@@ -593,8 +619,19 @@ end_comment
 begin_define
 define|#
 directive|define
+name|IP_FW_F_RND_MATCH
+value|0x00800000
+end_define
+
+begin_comment
+comment|/* probabilistic rule match		*/
+end_comment
+
+begin_define
+define|#
+directive|define
 name|IP_FW_F_MASK
-value|0x007FFFFF
+value|0x00FFFFFF
 end_define
 
 begin_comment
