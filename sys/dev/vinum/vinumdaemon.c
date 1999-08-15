59dd33a5bb2c446d579c66bfefb22d287a049c21
Greg Lehey <grog@FreeBSD.org>
1999-08-15 02:31:19 +0000

    Add note that a change needs backing out.
    Former-commit-id: 153f2680bf0204e6d36053591db3b2a222e63b6e

--- sys/dev/vinum/vinumdaemon.c
+++ sys/dev/vinum/vinumdaemon.c
@@ -4,7 +4,7 @@ comment|/* daemon.c: kernel part of Vinum daemon */
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *    * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumdaemon.c,v 1.11 1999/08/07 08:06:05 grog Exp $  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumdaemon.c,v 1.5 1999/06/23 03:22:01 grog Exp grog $  */
 end_comment
 
 begin_include
@@ -218,7 +218,7 @@ operator|->
 name|type
 condition|)
 block|{
-comment|/* 		 * We had an I/O error on a request.  Go through the 		 * request and try to salvage it  		 */
+comment|/* 		 * We had an I/O error on a request.  Go through the 		 * request and try to salvage it 		 */
 case|case
 name|daemonrq_ioerror
 case|:
@@ -306,7 +306,7 @@ argument_list|)
 expr_stmt|;
 comment|/* the failed request */
 break|break;
-comment|/* 		 * Write the config to disk.  We could end up with 		 * quite a few of these in a row.  Only honour the 		 * last one  		 */
+comment|/* 		 * Write the config to disk.  We could end up with 		 * quite a few of these in a row.  Only honour the 		 * last one 		 */
 case|case
 name|daemonrq_saveconfig
 case|:
@@ -525,6 +525,7 @@ modifier|*
 name|rq
 parameter_list|)
 block|{
+comment|/*      * This should read:      *      *     vinumstrategy(rq->bp);      *      * Negotiate with phk to get it fixed.      */
 name|BUF_STRATEGY
 argument_list|(
 name|rq
@@ -651,7 +652,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * see if the daemon is running.  Return 0 (no error)  * if it is, ESRCH otherwise   */
+comment|/*  * see if the daemon is running.  Return 0 (no error)  * if it is, ESRCH otherwise  */
 end_comment
 
 begin_function
