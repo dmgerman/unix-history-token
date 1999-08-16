de14643b2389ff7fa3923bf8ebed02b1c6cbe1cd
Nick Hibma <n_hibma@FreeBSD.org>
1999-08-16 21:44:14 +0000

    Change calling of chained event handler from always after the attach/detach to 1) MOD_LOAD before attach 2) MOD_UNLOAD after detach
    The driver specific event handler can now be used to function as
    driver specific init/deinit function (compare to device specific
    init/deinit functions: attach & detach).
    
    
    Former-commit-id: 69aafda0d73203d0b76dc533ec56e0689a92b42d

--- sys/kern/subr_bus.c
+++ sys/kern/subr_bus.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997,1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: subr_bus.c,v 1.37 1999/08/14 13:20:04 n_hibma Exp $  */
+comment|/*-  * Copyright (c) 1997,1998 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: subr_bus.c,v 1.38 1999/08/14 13:32:25 n_hibma Exp $  */
 end_comment
 
 begin_include
@@ -10588,6 +10588,27 @@ block|{
 case|case
 name|MOD_LOAD
 case|:
+if|if
+condition|(
+name|dmd
+operator|->
+name|dmd_chainevh
+condition|)
+name|error
+operator|=
+name|dmd
+operator|->
+name|dmd_chainevh
+argument_list|(
+name|mod
+argument_list|,
+name|what
+argument_list|,
+name|dmd
+operator|->
+name|dmd_chainarg
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|i
@@ -10727,8 +10748,6 @@ index|]
 argument_list|)
 expr_stmt|;
 block|}
-break|break;
-block|}
 if|if
 condition|(
 operator|!
@@ -10753,6 +10772,8 @@ operator|->
 name|dmd_chainarg
 argument_list|)
 expr_stmt|;
+break|break;
+block|}
 return|return
 operator|(
 name|error
