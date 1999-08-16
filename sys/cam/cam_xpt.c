0499e4383104c226b37c263c231d7541616b87c2
Matthew Jacob <mjacob@FreeBSD.org>
1999-08-16 17:48:36 +0000

    MFC: Put in a limit (8) for lun width- do not exceed probing this unless a device is quirked for allowing it. Reviewed by:	gibbs@freebsd.org
    Former-commit-id: e08378e404bce90b4b0c9b55adfa5b3bd79eaefc

--- sys/cam/cam_xpt.c
+++ sys/cam/cam_xpt.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Implementation of the Common Access Method Transport (XPT) layer.  *  * Copyright (c) 1997, 1998, 1999 Justin T. Gibbs.  * Copyright (c) 1997, 1998, 1999 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: cam_xpt.c,v 1.42.2.16 1999/07/11 06:20:46 jmg Exp $  */
+comment|/*  * Implementation of the Common Access Method Transport (XPT) layer.  *  * Copyright (c) 1997, 1998, 1999 Justin T. Gibbs.  * Copyright (c) 1997, 1998, 1999 Kenneth D. Merry.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *      $Id: cam_xpt.c,v 1.42.2.17 1999/07/12 10:01:38 jmg Exp $  */
 end_comment
 
 begin_include
@@ -619,6 +619,10 @@ define|#
 directive|define
 name|CAM_QUIRK_NOSERIAL
 value|0x02
+define|#
+directive|define
+name|CAM_QUIRK_HILUNS
+value|0x04
 name|u_int
 name|mintags
 decl_stmt|;
@@ -628,6 +632,13 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+
+begin_define
+define|#
+directive|define
+name|CAM_SCSI2_MAXLUN
+value|8
+end_define
 
 begin_typedef
 typedef|typedef
@@ -20275,6 +20286,26 @@ operator|)
 condition|)
 block|{
 comment|/* Try the next lun */
+if|if
+condition|(
+name|lun_id
+operator|<
+operator|(
+name|CAM_SCSI2_MAXLUN
+operator|-
+literal|1
+operator|)
+operator|||
+operator|(
+name|device
+operator|->
+name|quirk
+operator|->
+name|quirks
+operator|&
+name|CAM_QUIRK_HILUNS
+operator|)
+condition|)
 name|lun_id
 operator|++
 expr_stmt|;
@@ -20313,6 +20344,26 @@ literal|0
 condition|)
 block|{
 comment|/* Try the next lun */
+if|if
+condition|(
+name|lun_id
+operator|<
+operator|(
+name|CAM_SCSI2_MAXLUN
+operator|-
+literal|1
+operator|)
+operator|||
+operator|(
+name|device
+operator|->
+name|quirk
+operator|->
+name|quirks
+operator|&
+name|CAM_QUIRK_HILUNS
+operator|)
+condition|)
 name|lun_id
 operator|++
 expr_stmt|;
