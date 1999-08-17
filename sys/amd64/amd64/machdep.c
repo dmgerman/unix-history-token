8241e5aa3b96bd93a904fbf747fe61fb62ad9bcc
Michael Smith <msmith@FreeBSD.org>
1999-08-17 07:09:13 +0000

    Mindbogglingly, many BIOS vendors expect to be able to load %ds with 0x40 and then access data stored in real-mode segment 0x40, even when called in protected mode.  Microsoft unfortunately coddle these individuals, and so must we if we want to run their code.
    This change works around GPFs in some APM and PnP BIOS implementations.
    
    Obtained from:	Linux
    
    
    Former-commit-id: 9b8ad9b3eb8188f299fb54e46405b7cbc8367a01

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.357 1999/07/29 01:49:18 msmith Exp $  */
+comment|/*-  * Copyright (c) 1992 Terrence R. Lambert.  * Copyright (c) 1982, 1987, 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)machdep.c	7.4 (Berkeley) 6/3/91  *	$Id: machdep.c,v 1.358 1999/08/09 10:34:43 phk Exp $  */
 end_comment
 
 begin_include
@@ -4572,7 +4572,35 @@ literal|0
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GPANIC_SEL	8 Panic Tss Descriptor */
+comment|/* GBIOSLOWMEM_SEL 8 BIOS access to realmode segment 0x40, must be #8 in GDT */
+block|{
+literal|0x400
+block|,
+comment|/* segment base address */
+literal|0xfffff
+block|,
+comment|/* length */
+name|SDT_MEMRWA
+block|,
+comment|/* segment type */
+literal|0
+block|,
+comment|/* segment descriptor priority level */
+literal|1
+block|,
+comment|/* segment descriptor present */
+literal|0
+block|,
+literal|0
+block|,
+literal|1
+block|,
+comment|/* default 32 vs 16 bit size */
+literal|1
+comment|/* limit granularity (byte/page units)*/
+block|}
+block|,
+comment|/* GPANIC_SEL	9 Panic Tss Descriptor */
 block|{
 operator|(
 name|int
@@ -4610,35 +4638,7 @@ literal|0
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GBIOSCODE32_SEL 9 BIOS 32-bit interface (32bit Code) */
-block|{
-literal|0
-block|,
-comment|/* segment base address (overwritten)  */
-literal|0xfffff
-block|,
-comment|/* length */
-name|SDT_MEMERA
-block|,
-comment|/* segment type */
-literal|0
-block|,
-comment|/* segment descriptor priority level */
-literal|1
-block|,
-comment|/* segment descriptor present */
-literal|0
-block|,
-literal|0
-block|,
-literal|1
-block|,
-comment|/* default 32 vs 16 bit size */
-literal|1
-comment|/* limit granularity (byte/page units)*/
-block|}
-block|,
-comment|/* GBIOSCODE16_SEL 10 BIOS 32-bit interface (16bit Code) */
+comment|/* GBIOSCODE32_SEL 10 BIOS 32-bit interface (32bit Code) */
 block|{
 literal|0
 block|,
@@ -4666,7 +4666,35 @@ literal|1
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GBIOSDATA_SEL 11 BIOS 32-bit interface (Data) */
+comment|/* GBIOSCODE16_SEL 11 BIOS 32-bit interface (16bit Code) */
+block|{
+literal|0
+block|,
+comment|/* segment base address (overwritten)  */
+literal|0xfffff
+block|,
+comment|/* length */
+name|SDT_MEMERA
+block|,
+comment|/* segment type */
+literal|0
+block|,
+comment|/* segment descriptor priority level */
+literal|1
+block|,
+comment|/* segment descriptor present */
+literal|0
+block|,
+literal|0
+block|,
+literal|0
+block|,
+comment|/* default 32 vs 16 bit size */
+literal|1
+comment|/* limit granularity (byte/page units)*/
+block|}
+block|,
+comment|/* GBIOSDATA_SEL 12 BIOS 32-bit interface (Data) */
 block|{
 literal|0
 block|,
@@ -4694,7 +4722,7 @@ literal|1
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GBIOSUTIL_SEL 12 BIOS 16-bit interface (Utility) */
+comment|/* GBIOSUTIL_SEL 13 BIOS 16-bit interface (Utility) */
 block|{
 literal|0
 block|,
@@ -4722,7 +4750,7 @@ literal|1
 comment|/* limit granularity (byte/page units)*/
 block|}
 block|,
-comment|/* GBIOSARGS_SEL 13 BIOS 16-bit interface (Arguments) */
+comment|/* GBIOSARGS_SEL 14 BIOS 16-bit interface (Arguments) */
 block|{
 literal|0
 block|,
--- sys/amd64/include/segments.h
+++ sys/amd64/include/segments.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1989, 1990 William F. Jolitz  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)segments.h	7.1 (Berkeley) 5/9/91  *	$Id: segments.h,v 1.20 1999/06/18 14:32:21 bde Exp $  */
+comment|/*-  * Copyright (c) 1989, 1990 William F. Jolitz  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)segments.h	7.1 (Berkeley) 5/9/91  *	$Id: segments.h,v 1.21 1999/07/29 01:49:19 msmith Exp $  */
 end_comment
 
 begin_ifndef
@@ -1041,8 +1041,19 @@ end_comment
 begin_define
 define|#
 directive|define
-name|GPANIC_SEL
+name|GBIOSLOWMEM_SEL
 value|8
+end_define
+
+begin_comment
+comment|/* BIOS low memory access (must be entry 8) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|GPANIC_SEL
+value|9
 end_define
 
 begin_comment
@@ -1053,7 +1064,7 @@ begin_define
 define|#
 directive|define
 name|GBIOSCODE32_SEL
-value|9
+value|10
 end_define
 
 begin_comment
@@ -1064,7 +1075,7 @@ begin_define
 define|#
 directive|define
 name|GBIOSCODE16_SEL
-value|10
+value|11
 end_define
 
 begin_comment
@@ -1075,7 +1086,7 @@ begin_define
 define|#
 directive|define
 name|GBIOSDATA_SEL
-value|11
+value|12
 end_define
 
 begin_comment
@@ -1086,7 +1097,7 @@ begin_define
 define|#
 directive|define
 name|GBIOSUTIL_SEL
-value|12
+value|13
 end_define
 
 begin_comment
@@ -1097,7 +1108,7 @@ begin_define
 define|#
 directive|define
 name|GBIOSARGS_SEL
-value|13
+value|14
 end_define
 
 begin_comment
@@ -1130,7 +1141,7 @@ begin_define
 define|#
 directive|define
 name|NGDT
-value|14
+value|15
 end_define
 
 begin_endif
