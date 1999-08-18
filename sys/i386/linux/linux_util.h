33b0b03a78e4d7a48ed476f7e345e0b30191b270
Marcel Moolenaar <marcel@FreeBSD.org>
1999-08-18 11:18:45 +0000

    MFC: handle EFAULT cases when translating pathnames. Seems important :-)
    Former-commit-id: 07754eec6603f25001ee93e01a61f87220c72424

--- sys/i386/linux/linux_util.h
+++ sys/i386/linux/linux_util.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 Christos Zoulas  * Copyright (c) 1995 Frank van der Linden  * Copyright (c) 1995 Scott Bartram  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * from: svr4_util.h,v 1.5 1994/11/18 02:54:31 christos Exp  * from: linux_util.h,v 1.2 1995/03/05 23:23:50 fvdl Exp  * $Id: linux_util.h,v 1.6 1998/06/30 08:40:33 jmg Exp $  */
+comment|/*  * Copyright (c) 1994 Christos Zoulas  * Copyright (c) 1995 Frank van der Linden  * Copyright (c) 1995 Scott Bartram  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * from: svr4_util.h,v 1.5 1994/11/18 02:54:31 christos Exp  * from: linux_util.h,v 1.2 1995/03/05 23:23:50 fvdl Exp  * $Id: linux_util.h,v 1.7 1998/12/16 16:28:57 bde Exp $  */
 end_comment
 
 begin_comment
@@ -254,6 +254,23 @@ end_decl_stmt
 begin_define
 define|#
 directive|define
+name|CHECKALT
+parameter_list|(
+name|p
+parameter_list|,
+name|sgp
+parameter_list|,
+name|path
+parameter_list|,
+name|i
+parameter_list|)
+define|\
+value|do {								\ 		int _error;						\ 		_error = linux_emul_find(p, sgp, linux_emul_path, path,	\&path, i);						\ 		if (_error == EFAULT)					\ 			return (_error);				\ 	} while (0)
+end_define
+
+begin_define
+define|#
+directive|define
 name|CHECKALTEXIST
 parameter_list|(
 name|p
@@ -262,8 +279,7 @@ name|sgp
 parameter_list|,
 name|path
 parameter_list|)
-define|\
-value|linux_emul_find(p, sgp, linux_emul_path, path,&(path), 0)
+value|CHECKALT(p, sgp, path, 0)
 end_define
 
 begin_define
@@ -277,8 +293,7 @@ name|sgp
 parameter_list|,
 name|path
 parameter_list|)
-define|\
-value|linux_emul_find(p, sgp, linux_emul_path, path,&(path), 1)
+value|CHECKALT(p, sgp, path, 1)
 end_define
 
 begin_endif
