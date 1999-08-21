89d955d26404bc75e08334c9fd1a1841b6ecac4b
Poul-Henning Kamp <phk@FreeBSD.org>
1999-08-21 06:48:16 +0000

    Detect if the the offset used to read from a raw device loose bits when converted to block number.
    Former-commit-id: f7b6c65065ab895ad3d631b467755224216084f8

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: kern_physio.c,v 1.35 1999/06/26 02:46:02 mckusick Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: kern_physio.c,v 1.36 1999/08/14 11:40:42 phk Exp $  */
 end_comment
 
 begin_include
@@ -228,6 +228,9 @@ decl_stmt|;
 name|caddr_t
 name|sa
 decl_stmt|;
+name|off_t
+name|blockno
+decl_stmt|;
 name|int
 name|bp_alloc
 init|=
@@ -412,17 +415,37 @@ name|b_saveaddr
 operator|=
 name|sa
 expr_stmt|;
+name|blockno
+operator|=
+name|uio
+operator|->
+name|uio_offset
+operator|>>
+name|DEV_BSHIFT
+expr_stmt|;
 name|bp
 operator|->
 name|b_blkno
 operator|=
-name|btodb
-argument_list|(
-name|uio
-operator|->
-name|uio_offset
-argument_list|)
+name|blockno
 expr_stmt|;
+if|if
+condition|(
+name|bp
+operator|->
+name|b_blkno
+operator|!=
+name|blockno
+condition|)
+block|{
+name|error
+operator|=
+name|EINVAL
+expr_stmt|;
+goto|goto
+name|doerror
+goto|;
+block|}
 name|bp
 operator|->
 name|b_offset
