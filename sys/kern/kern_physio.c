6d1315b040bc20700be337d645652454a2544dd3
Poul-Henning Kamp <phk@FreeBSD.org>
1999-08-21 18:22:44 +0000

    Use more compiler friendly test for overflow.
    Submitted by:	bde
    
    
    Former-commit-id: 9b7f1835879ad6c350b82074f7308a85540770fd

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: kern_physio.c,v 1.36 1999/08/14 11:40:42 phk Exp $  */
+comment|/*  * Copyright (c) 1994 John S. Dyson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    John S. Dyson.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $Id: kern_physio.c,v 1.37 1999/08/21 06:48:16 phk Exp $  */
 end_comment
 
 begin_include
@@ -423,17 +423,12 @@ name|uio_offset
 operator|>>
 name|DEV_BSHIFT
 expr_stmt|;
-name|bp
-operator|->
-name|b_blkno
-operator|=
-name|blockno
-expr_stmt|;
 if|if
 condition|(
-name|bp
-operator|->
-name|b_blkno
+operator|(
+name|daddr_t
+operator|)
+name|blockno
 operator|!=
 name|blockno
 condition|)
@@ -446,6 +441,12 @@ goto|goto
 name|doerror
 goto|;
 block|}
+name|bp
+operator|->
+name|b_blkno
+operator|=
+name|blockno
+expr_stmt|;
 name|bp
 operator|->
 name|b_offset
