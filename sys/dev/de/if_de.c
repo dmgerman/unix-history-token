18488971eb8ef1e9c48e8ec94a86db1ffe738bf8
Bill Fumerola <billf@FreeBSD.org>
1999-08-21 21:35:59 +0000

    Add missing include.
    Submitted by:	Mitsuru IWASAKI <iwasaki@jp.FreeBSD.org>
    Reviewed by:	Alex Perel <veers@disturbed.net>
    
    
    Former-commit-id: e92ea79dd67a2d9ad27aeeb10a71f73010407a69

--- sys/dev/de/if_de.c
+++ sys/dev/de/if_de.c
@@ -66,6 +66,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/eventhandler.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/clock.h>
 end_include
 
