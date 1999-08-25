d87c8427820bad73b639e0295083a37fa6ad04de
Marcel Moolenaar <marcel@FreeBSD.org>
1999-08-25 11:19:03 +0000

    Change all UNIMPL syscalls to STD and add them to linux_dummy. Now we always know if and when an unimplemented or obsoleted syscall is being used. Make the message more end-user friendly.
    And as long as we're here, rename some unimplemeted syscalls (linux_phys ->
    linux_umount2, linux_vm86 -> linux_vm86old, linux_new_vm86 -> linux_vm86).
    
    Change prototype for linux_newuname from `struct linux_newuname_t *' into
    `struct linux_new_utsname *'. This change is reflected in linux.h and
    linux_misc.c.
    
    
    Former-commit-id: 363e6e2ce754723168466acc6e0082a2a31f7172

--- sys/i386/linux/linux.h
+++ sys/i386/linux/linux.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1994-1996 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer   *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: linux.h,v 1.33 1999/08/14 17:28:46 marcel Exp $  */
+comment|/*-  * Copyright (c) 1994-1996 Søren Schmidt  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer   *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  *	$Id: linux.h,v 1.34 1999/08/15 17:28:39 marcel Exp $  */
 end_comment
 
 begin_ifndef
@@ -231,6 +231,57 @@ block|}
 name|linux_new_sigaction_t
 typedef|;
 end_typedef
+
+begin_define
+define|#
+directive|define
+name|LINUX_MAX_UTSNAME
+value|65
+end_define
+
+begin_struct
+struct|struct
+name|linux_new_utsname
+block|{
+name|char
+name|sysname
+index|[
+name|LINUX_MAX_UTSNAME
+index|]
+decl_stmt|;
+name|char
+name|nodename
+index|[
+name|LINUX_MAX_UTSNAME
+index|]
+decl_stmt|;
+name|char
+name|release
+index|[
+name|LINUX_MAX_UTSNAME
+index|]
+decl_stmt|;
+name|char
+name|version
+index|[
+name|LINUX_MAX_UTSNAME
+index|]
+decl_stmt|;
+name|char
+name|machine
+index|[
+name|LINUX_MAX_UTSNAME
+index|]
+decl_stmt|;
+name|char
+name|domainname
+index|[
+name|LINUX_MAX_UTSNAME
+index|]
+decl_stmt|;
+block|}
+struct|;
+end_struct
 
 begin_comment
 comment|/*  * The Linux sigcontext, pretty much a standard 386 trapframe.  */
