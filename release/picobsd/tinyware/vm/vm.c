55a466b9c532e5bd3fb426d8ab40cb6358d53a52
Roger Hardiman <roger@FreeBSD.org>
1999-08-26 18:29:48 +0000

    MFC: Obtain VM page size using a sysctl rather than using DEFAULT_PAGE_SIZE
    Former-commit-id: 3c5922e1b9c5175baea93dc945cac5dca337453b

--- release/picobsd/tinyware/vm/vm.c
+++ release/picobsd/tinyware/vm/vm.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Andrzej Bialecki  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: vm.c,v 1.1.1.1 1998/08/27 17:38:45 abial Exp $  */
+comment|/*-  * Copyright (c) 1998 Andrzej Bialecki  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$Id: vm.c,v 1.5 1999/08/26 11:11:48 roger Exp $  */
 end_comment
 
 begin_include
@@ -40,7 +40,7 @@ name|pgtok
 parameter_list|(
 name|a
 parameter_list|)
-value|((a) * (u_int) DEFAULT_PAGE_SIZE>> 10)
+value|((a) * (u_int) pagesize>> 10)
 end_define
 
 begin_function
@@ -68,10 +68,37 @@ literal|0
 decl_stmt|,
 name|len
 decl_stmt|;
+name|int
+name|pagesize
+decl_stmt|,
+name|pagesize_len
+decl_stmt|;
 name|struct
 name|vmtotal
 name|v
 decl_stmt|;
+name|pagesize_len
+operator|=
+sizeof|sizeof
+argument_list|(
+name|int
+argument_list|)
+expr_stmt|;
+name|sysctlbyname
+argument_list|(
+literal|"vm.stats.vm.v_page_size"
+argument_list|,
+operator|&
+name|pagesize
+argument_list|,
+operator|&
+name|pagesize_len
+argument_list|,
+name|NULL
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
 name|len
 operator|=
 sizeof|sizeof
