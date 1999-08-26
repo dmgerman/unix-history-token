4d460d2f9944bf7cec2210b6bb048410a1f8457a
Greg Lehey <grog@FreeBSD.org>
1999-08-26 03:30:44 +0000

    MFC: config_plex: don't allocate a rangelock struct.
    lockrange: correctly expand rangelock struct, including expanding a
    	   null struct.  Previously lockrange would attempt to lock a
    	   NULL pointer under these circumstances.
    
    Reported-by:	Ian Freislich <iang@uunet.co.za>
    
    Former-commit-id: df5babefad7f79a053e2446effe5de4ebfaaa162

--- sys/dev/vinum/vinumconfig.c
+++ sys/dev/vinum/vinumconfig.c
@@ -4,7 +4,7 @@ comment|/*  * To do:  *  * Don't store drive configuration on the config DB: rea
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumconfig.c,v 1.25 1999/08/15 02:30:34 grog Exp $  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumconfig.c,v 1.8.2.5 1999/08/24 04:05:34 grog Exp $  */
 end_comment
 
 begin_define
@@ -6307,61 +6307,6 @@ name|plexsuffix
 argument_list|)
 expr_stmt|;
 comment|/* and add it to the name */
-block|}
-if|if
-condition|(
-name|plex
-operator|->
-name|organization
-operator|==
-name|plex_raid5
-condition|)
-block|{
-comment|/* RAID-5 plex, */
-name|plex
-operator|->
-name|lock
-operator|=
-operator|(
-expr|struct
-name|rangelock
-operator|*
-operator|)
-name|Malloc
-argument_list|(
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|rangelock
-argument_list|)
-operator|*
-name|INITIAL_LOCKS
-argument_list|)
-expr_stmt|;
-comment|/* allocate lock table */
-name|bzero
-argument_list|(
-name|plex
-operator|->
-name|lock
-argument_list|,
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|rangelock
-argument_list|)
-operator|*
-name|INITIAL_LOCKS
-argument_list|)
-expr_stmt|;
-comment|/* zero it */
-name|plex
-operator|->
-name|alloclocks
-operator|=
-name|INITIAL_LOCKS
-expr_stmt|;
-comment|/* and note how many there are */
 block|}
 comment|/* Note the last plex we configured */
 name|current_plex
--- sys/dev/vinum/vinumlock.c
+++ sys/dev/vinum/vinumlock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumlock.c,v 1.12 1999/08/14 06:28:21 grog Exp $  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumlock.c,v 1.5.2.4 1999/08/24 04:05:41 grog Exp $  */
 end_comment
 
 begin_include
@@ -841,6 +841,16 @@ argument_list|,
 name|INITIAL_LOCKS
 argument_list|)
 expr_stmt|;
+name|pos
+operator|=
+operator|&
+name|plex
+operator|->
+name|lock
+index|[
+name|newlock
+index|]
+expr_stmt|;
 while|while
 condition|(
 name|newlock
@@ -862,6 +872,7 @@ operator|=
 literal|0
 expr_stmt|;
 block|}
+else|else
 name|pos
 operator|=
 name|lock
