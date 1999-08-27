267a0701f9dcab825e0aacdb6a63afbcd644bdeb
Peter Wemm <peter@FreeBSD.org>
1999-08-27 06:53:34 +0000

    Don't return 0 for an unknown ioctl (!).  This was breaking ppp(8). Slight tidy up while here.
    Former-commit-id: f02e271f23efc47ae347902840fb480065ba15b4

--- sys/dev/si/si.c
+++ sys/dev/si/si.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Device driver for Specialix range (SI/XIO) of serial line multiplexors.  *  * Copyright (C) 1990, 1992, 1998 Specialix International,  * Copyright (C) 1993, Andy Rutter<andy@acronym.co.uk>  * Copyright (C) 1995, Peter Wemm<peter@netplex.com.au>  *  * Originally derived from:	SunOS 4.x version  * Ported from BSDI version to FreeBSD by Peter Wemm.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notices, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notices, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Andy Rutter of  *	Advanced Methods and Tools Ltd. based on original information  *	from Specialix International.  * 4. Neither the name of Advanced Methods and Tools, nor Specialix  *    International may be used to endorse or promote products derived from  *    this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN  * NO EVENT SHALL THE AUTHORS BE LIABLE.  *  *	$Id: si.c,v 1.88 1999/08/18 17:42:41 nsayer Exp $  */
+comment|/*  * Device driver for Specialix range (SI/XIO) of serial line multiplexors.  *  * Copyright (C) 1990, 1992, 1998 Specialix International,  * Copyright (C) 1993, Andy Rutter<andy@acronym.co.uk>  * Copyright (C) 1995, Peter Wemm<peter@netplex.com.au>  *  * Originally derived from:	SunOS 4.x version  * Ported from BSDI version to FreeBSD by Peter Wemm.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notices, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notices, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Andy Rutter of  *	Advanced Methods and Tools Ltd. based on original information  *	from Specialix International.  * 4. Neither the name of Advanced Methods and Tools, nor Specialix  *    International may be used to endorse or promote products derived from  *    this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN  * NO EVENT SHALL THE AUTHORS BE LIABLE.  *  *	$Id: si.c,v 1.89 1999/08/23 20:58:48 phk Exp $  */
 end_comment
 
 begin_ifndef
@@ -7932,9 +7932,20 @@ name|error
 operator|!=
 name|ENOIOCTL
 condition|)
+block|{
+name|splx
+argument_list|(
+name|oldspl
+argument_list|)
+expr_stmt|;
 goto|goto
-name|outspl
+name|out
 goto|;
+block|}
+name|error
+operator|=
+literal|0
+expr_stmt|;
 switch|switch
 condition|(
 name|cmd
@@ -8097,14 +8108,9 @@ expr_stmt|;
 if|if
 condition|(
 name|error
-operator|!=
+operator|==
 literal|0
 condition|)
-block|{
-goto|goto
-name|outspl
-goto|;
-block|}
 name|pp
 operator|->
 name|sp_dtr_wait
@@ -8146,12 +8152,6 @@ operator|=
 name|ENOTTY
 expr_stmt|;
 block|}
-name|error
-operator|=
-literal|0
-expr_stmt|;
-name|outspl
-label|:
 name|splx
 argument_list|(
 name|oldspl
