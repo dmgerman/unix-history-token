84a66cb3c7b2323d013b47ec435122e8b75689af
Roger Hardiman <roger@FreeBSD.org>
1999-08-31 10:29:42 +0000

    Update to driver release 1.71
    New Features:
    Greatly improved VBI capture support. (mainly for the AleVT port)
    Supports select() on /dev/vbi
    Improved RISC program for RGB+VBI capture to capture both evenodd
    VBI data even when only capturing even only (or odd only) video
    Based on code from Juha Nurmela <Juha.Nurmela@quicknet.inet.fi>
    
    Support for Hauppauge 627 and Temic 4006
    Submitted by: Maurice Castro <maurice@atum.castro.aus.net>
    
    Bug Fix:
    Fix bug in AverMedia card detection.
    
    
    Former-commit-id: 0d2625d2a21ec26e07ec51e7cc6b8fb864d3fd5c

--- sys/dev/bktr/bktr_reg.h
+++ sys/dev/bktr/bktr_reg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1999 Roger Hardiman  * Copyright (c) 1998 Amancio Hasty  * Copyright (c) 1995 Mark Tinguely and Jim Lowe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Mark Tinguely and Jim Lowe  * 4. The name of the author may not be used to endorse or promote products   *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*  * Copyright (c) 1999 Roger Hardiman  * Copyright (c) 1998 Amancio Hasty  * Copyright (c) 1995 Mark Tinguely and Jim Lowe  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Mark Tinguely and Jim Lowe  * 4. The name of the author may not be used to endorse or promote products   *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $Id: brktree_reg.h,v 1.29 1999/06/12 14:54:56 roger Exp $  */
 end_comment
 
 begin_ifndef
@@ -1786,6 +1786,12 @@ decl_stmt|;
 name|bus_dmamap_t
 name|dm_mem
 decl_stmt|;
+name|bus_dmamap_t
+name|dm_vbidata
+decl_stmt|;
+name|bus_dmamap_t
+name|dm_vbibuffer
+decl_stmt|;
 name|vm_offset_t
 name|phys_base
 decl_stmt|;
@@ -1829,6 +1835,12 @@ name|dm_oprog
 decl_stmt|;
 name|bus_dmamap_t
 name|dm_mem
+decl_stmt|;
+name|bus_dmamap_t
+name|dm_vbidata
+decl_stmt|;
+name|bus_dmamap_t
+name|dm_vbibuffer
 decl_stmt|;
 name|size_t
 name|dm_mapsize
@@ -1945,6 +1957,19 @@ name|int
 name|vbisize
 decl_stmt|;
 comment|/* Number of bytes in the circular buffer */
+name|u_long
+name|vbi_sequence_number
+decl_stmt|;
+comment|/* sequence number for VBI */
+name|int
+name|vbi_read_blocked
+decl_stmt|;
+comment|/* user process blocked on read() from /dev/vbi */
+name|struct
+name|selinfo
+name|vbi_select
+decl_stmt|;
+comment|/* Data used by select() on /dev/vbi */
 name|struct
 name|proc
 modifier|*
