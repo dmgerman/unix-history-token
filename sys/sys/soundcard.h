b1b208b800b561163a51732731a185fc3f0434cc
Doug Rabson <dfr@FreeBSD.org>
1999-09-04 14:33:58 +0000

    Make the idempotency ifdefs uniform with the rest of src/sys/sys.
    Former-commit-id: 2028f55c6e00da98028c98fdd339ecbec39f2e57

--- sys/sys/soundcard.h
+++ sys/sys/soundcard.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|SOUNDCARD_H
+name|_SYS_SOUNDCARD_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|SOUNDCARD_H
+name|_SYS_SOUNDCARD_H_
 end_define
 
 begin_comment
@@ -4838,7 +4838,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* SOUNDCARD_H */
+comment|/* !_SYS_SOUNDCARD_H_ */
 end_comment
 
 end_unit
