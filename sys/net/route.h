f60702b29ff6cbe9276d2c094a8d87250146a172
Ruslan Ermilov <ru@FreeBSD.org>
1999-09-14 00:33:23 +0000

    Add comments, fix typos.
    Reviewed by:	wollman
    
    
    Former-commit-id: 7d6e1b1dd471ccb7a7125b3b28c4cb3f107b9343

--- sys/net/route.h
+++ sys/net/route.h
@@ -667,6 +667,10 @@ begin_comment
 comment|/* Up the ante and ignore older versions */
 end_comment
 
+begin_comment
+comment|/*  * Message types.  */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -843,6 +847,10 @@ begin_comment
 comment|/* mcast group membership being deleted */
 end_comment
 
+begin_comment
+comment|/*  * Bitmask values for rtm_inits and rmx_locks.  */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -873,7 +881,7 @@ value|0x4
 end_define
 
 begin_comment
-comment|/* init or lock _hopcount */
+comment|/* init or lock _expire */
 end_comment
 
 begin_define
@@ -932,7 +940,7 @@ comment|/* init or lock _rttvar */
 end_comment
 
 begin_comment
-comment|/*  * Bitmask values for rtm_addr.  */
+comment|/*  * Bitmask values for rtm_addrs.  */
 end_comment
 
 begin_define
