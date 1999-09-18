efc401696cee7258d05212c1d90bcc2c6f1a5642
David E. O'Brien <obrien@FreeBSD.org>
1999-09-18 10:51:31 +0000

    Virgin import of EGCS 1.1.2's libf2c
    Former-commit-id: c1f999a45c30f40a3964639c48761ad29a52c612

---
+++ contrib/libf2c/libU77/acconfig.h
@@ -0,0 +1,13 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* Define as the path of the `chmod' program. */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|CHMOD_PATH
+end_undef
+
+end_unit
+
