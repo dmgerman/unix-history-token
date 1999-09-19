3fb51af3283a3a55c9aa132b2d22771bb22124e3
David E. O'Brien <obrien@FreeBSD.org>
1999-09-19 10:28:57 +0000

    This should get us the same functionality we had with GCC 2.7.2.1.
    *  Support for our dual ELF/a.out building ability
    *  Our custom ASM_* definitions
    *  Our custom debugger and profiling related definitions
    *  Our custom STARTFILE/ENDFILE specs
    
    *  The stock EGCS 1.1.2 freebsd-elf.h file depended on
       egcs-1.1.2/gcc/config/linux.h, which included "svr4.h".  We will
       include "svr4.h" via our "tm.h" definition.  So add the few bits
       from "linux.h" we actually needed.
    
    *  Using our current crtbegin.o/crtend.o we cannot support the DWARF2
       unwinding mechanisms.  In the future we will switch to the
       non-sjlj-exceptions type exception machanism.  However the `make world'
       bootstrap problems with the EGCS crtstuff.c must be overcome first.
    
    *  Our a.out gas doesn't "know" to use NOP's for aligns while in the text
       section.  Thus the a.out alignment generation needed tweaking from
       what we did with GCC 2.7.2.  [from BDE]
    
    *  The definition of SUPPORTS_ONE_ONLY prevents the compiler from trying
       to use "linkonce" sections for a.out.  The definition of
       NO_DBX_FUNCTION_END we had causes it to avoid .stabs symbols that the
       assembler cannot handle for a.out.  [from JDP]
    
    *  The previous "EXCEPTION_SECTION_FUNCTION" is the wrong name for EGCS.
       It also needed tweaking for EGCS.  [from JDP]
    
    Also bump __FreeBSD_cc_version to 400002 in case we need to know we are
    using EGCS at some point.
    
    
    Former-commit-id: 1294d41a9e61c9ba03f61c3449fdabc40675551c

--- contrib/gcc/config/i386/freebsd.h
+++ contrib/gcc/config/i386/freebsd.h
@@ -1,45 +1,65 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* Definitions for Intel 386 running FreeBSD with either a.out or ELF format    Copyright (C) 1994, 1995 Free Software Foundation, Inc.    Contributed by Eric Youngdale.    Modified for stabs-in-ELF by H.J. Lu.    Adapted from Linux version by John Polstra.    Added support for generating "old a.out gas" on the fly by Peter Wemm.  This file is part of GNU CC.  GNU CC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GNU CC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GNU CC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+comment|/* Definitions for Intel 386 running FreeBSD with either a.out or ELF format    Copyright (C) 1996 Free Software Foundation, Inc.    Contributed by Eric Youngdale.    Modified for stabs-in-ELF by H.J. Lu.    Adapted from GNU/Linux version by John Polstra.    Added support for generating "old a.out gas" on the fly by Peter Wemm.  This file is part of GNU CC.  GNU CC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GNU CC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GNU CC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_comment
-comment|/* A lie, I guess, but the general idea behind FreeBSD/ELF is that we are    supposed to be outputting something that will assemble under SVr4.    This gets us pretty close.  */
+comment|/* $FreeBSD$ */
 end_comment
 
-begin_include
-include|#
-directive|include
-file|<i386/i386.h>
-end_include
+begin_undef
+undef|#
+directive|undef
+name|TARGET_VERSION
+end_undef
+
+begin_define
+define|#
+directive|define
+name|TARGET_VERSION
+value|fprintf (stderr, " (i386 FreeBSD/ELF)");
+end_define
 
 begin_comment
-comment|/* Base i386 target machine definitions */
-end_comment
-
-begin_include
-include|#
-directive|include
-file|<i386/att.h>
-end_include
-
-begin_comment
-comment|/* Use the i386 AT&T assembler syntax */
-end_comment
-
-begin_include
-include|#
-directive|include
-file|<linux.h>
-end_include
-
-begin_comment
-comment|/* some common stuff */
+comment|/* The svr4 ABI for the i386 says that records and unions are returned    in memory.  */
 end_comment
 
 begin_comment
-comment|/* Don't assume anything about the header files. */
+comment|/* On FreeBSD, we do not. */
 end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|DEFAULT_PCC_STRUCT_RETURN
+end_undef
+
+begin_define
+define|#
+directive|define
+name|DEFAULT_PCC_STRUCT_RETURN
+value|0
+end_define
+
+begin_comment
+comment|/* This gets defined in tm.h->linux.h->svr4.h, and keeps us from using    libraries compiled with the native cc, so undef it. */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|NO_DOLLAR_IN_LABEL
+end_undef
+
+begin_comment
+comment|/* Don't assume anything about the header files.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|NO_IMPLICIT_EXTERN_C
+end_undef
 
 begin_define
 define|#
@@ -48,7 +68,7 @@ name|NO_IMPLICIT_EXTERN_C
 end_define
 
 begin_comment
-comment|/* This defines which switch letters take arguments.  On svr4, most of    the normal cases (defined in gcc.c) apply, and we also have -h* and    -z* options (for the linker).  We have a slightly different mix.  We    have -R (alias --rpath), no -z, --soname (-h), --assert etc. */
+comment|/* This defines which switch letters take arguments.  On FreeBSD, most of    the normal cases (defined in gcc.c) apply, and we also have -h* and    -z* options (for the linker) (comming from svr4).    We also have -R (alias --rpath), no -z, --soname (-h), --assert etc.  */
 end_comment
 
 begin_undef
@@ -65,7 +85,7 @@ parameter_list|(
 name|CHAR
 parameter_list|)
 define|\
-value|(   (CHAR) == 'D' \    || (CHAR) == 'U' \    || (CHAR) == 'o' \    || (CHAR) == 'e' \    || (CHAR) == 'T' \    || (CHAR) == 'u' \    || (CHAR) == 'I' \    || (CHAR) == 'm' \    || (CHAR) == 'L' \    || (CHAR) == 'A' \    || (CHAR) == 'h' \    || (CHAR) == 'z'
+value|(DEFAULT_SWITCH_TAKES_ARG (CHAR)					\    || (CHAR) == 'h'							\    || (CHAR) == 'z'
 comment|/* ignored by ld */
 value|\    || (CHAR) == 'R')
 end_define
@@ -85,19 +105,6 @@ name|STR
 parameter_list|)
 define|\
 value|(DEFAULT_WORD_SWITCH_TAKES_ARG (STR)					\    || !strcmp (STR, "rpath") || !strcmp (STR, "rpath-link")		\    || !strcmp (STR, "soname") || !strcmp (STR, "defsym") 		\    || !strcmp (STR, "assert") || !strcmp (STR, "dynamic-linker"))
-end_define
-
-begin_undef
-undef|#
-directive|undef
-name|TARGET_VERSION
-end_undef
-
-begin_define
-define|#
-directive|define
-name|TARGET_VERSION
-value|fprintf (stderr, " (i386 FreeBSD)");
 end_define
 
 begin_define
@@ -172,27 +179,6 @@ value|{ "profiler-epilogue",	 MASK_PROFILER_EPILOGUE},	\      { "no-profiler-epi
 end_define
 
 begin_comment
-comment|/* The svr4 ABI for the i386 says that records and unions are returned    in memory.  */
-end_comment
-
-begin_comment
-comment|/* On FreeBSD, we do not. */
-end_comment
-
-begin_undef
-undef|#
-directive|undef
-name|DEFAULT_PCC_STRUCT_RETURN
-end_undef
-
-begin_define
-define|#
-directive|define
-name|DEFAULT_PCC_STRUCT_RETURN
-value|0
-end_define
-
-begin_comment
 comment|/* Prefix for internally generated assembler labels.  If we aren't using     underscores, we are using prefix `.'s to identify labels that should      be ignored, as in `i386/gas.h' --karl@cs.umb.edu  */
 end_comment
 
@@ -263,6 +249,13 @@ define|#
 directive|define
 name|ASM_APP_OFF
 value|"#NO_APP\n"
+end_define
+
+begin_define
+define|#
+directive|define
+name|SET_ASM_OP
+value|".set"
 end_define
 
 begin_comment
@@ -401,6 +394,8 @@ name|ASM_OUTPUT_ADDR_DIFF_ELT
 parameter_list|(
 name|FILE
 parameter_list|,
+name|BODY
+parameter_list|,
 name|VALUE
 parameter_list|,
 name|REL
@@ -425,49 +420,7 @@ parameter_list|,
 name|LOG
 parameter_list|)
 define|\
-value|if ((LOG)!=0) fprintf ((FILE), "\t.p2align %d\n", (LOG))
-end_define
-
-begin_comment
-comment|/* Align labels, etc. at 4-byte boundaries.    For the 486, align to 16-byte boundary for sake of cache.  */
-end_comment
-
-begin_undef
-undef|#
-directive|undef
-name|ASM_OUTPUT_ALIGN_CODE
-end_undef
-
-begin_define
-define|#
-directive|define
-name|ASM_OUTPUT_ALIGN_CODE
-parameter_list|(
-name|FILE
-parameter_list|)
-define|\
-value|fprintf ((FILE), "\t.p2align %d,0x90\n", i386_align_jumps)
-end_define
-
-begin_comment
-comment|/* Align start of loop at 4-byte boundary.  */
-end_comment
-
-begin_undef
-undef|#
-directive|undef
-name|ASM_OUTPUT_LOOP_ALIGN
-end_undef
-
-begin_define
-define|#
-directive|define
-name|ASM_OUTPUT_LOOP_ALIGN
-parameter_list|(
-name|FILE
-parameter_list|)
-define|\
-value|fprintf ((FILE), "\t.p2align %d,0x90\n", i386_align_loops)
+value|if ((LOG)!=0) {							\     if (in_text_section())						\       fprintf ((FILE), "\t.p2align %d,0x90\n", (LOG));			\     else								\       fprintf ((FILE), "\t.p2align %d\n", (LOG));			\   }
 end_define
 
 begin_comment
@@ -485,6 +438,28 @@ define|#
 directive|define
 name|USE_CONST_SECTION
 value|TARGET_ELF
+end_define
+
+begin_comment
+comment|/* The a.out tools do not support "linkonce" sections. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SUPPORTS_ONE_ONLY
+value|TARGET_ELF
+end_define
+
+begin_comment
+comment|/* The a.out tools do not support "Lscope" .stabs symbols. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NO_DBX_FUNCTION_END
+value|TARGET_AOUT
 end_define
 
 begin_comment
@@ -635,6 +610,46 @@ define|\
 value|do {									\   static int sym_lineno = 1;						\   if (TARGET_ELF) {							\     fprintf (file, ".stabn 68,0,%d,.LM%d-", line, sym_lineno);		\     assemble_name (file, XSTR (XEXP (DECL_RTL (current_function_decl), 0), 0));\     fprintf (file, "\n.LM%d:\n", sym_lineno);				\     sym_lineno += 1;							\   } else {								\     fprintf (file, "\t%s %d,0,%d\n", ASM_STABD_OP, N_SLINE, lineno);	\   }									\ } while (0)
 end_define
 
+begin_undef
+undef|#
+directive|undef
+name|DEFAULT_VTABLE_THUNKS
+end_undef
+
+begin_define
+define|#
+directive|define
+name|DEFAULT_VTABLE_THUNKS
+value|1
+end_define
+
+begin_comment
+comment|/* This is BSD, so we want the DBX format.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DBX_DEBUGGING_INFO
+end_define
+
+begin_comment
+comment|/* Use stabs instead of DWARF debug format.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|PREFERRED_DEBUGGING_TYPE
+end_undef
+
+begin_define
+define|#
+directive|define
+name|PREFERRED_DEBUGGING_TYPE
+value|DBX_DEBUG
+end_define
+
 begin_comment
 comment|/* in elf, the function stabs come first, before the relative offsets */
 end_comment
@@ -751,6 +766,7 @@ begin_define
 define|#
 directive|define
 name|JUMP_TABLES_IN_TEXT_SECTION
+value|(flag_pic)
 end_define
 
 begin_comment
@@ -760,9 +776,10 @@ end_comment
 begin_define
 define|#
 directive|define
-name|EXCEPTION_SECTION_FUNCTION
+name|EXCEPTION_SECTION
+parameter_list|()
 define|\
-value|do {									\   if (TARGET_ELF) {							\     named_section (NULL_TREE, ".gcc_except_table");			\   } else {								\     if (flag_pic)							\       data_section ();							\     else								\       readonly_data_section ();						\   }									\ } while (0);
+value|do {									\   if (TARGET_ELF) {							\     named_section (NULL_TREE, ".gcc_except_table", 0);			\   } else {								\     if (flag_pic)							\       data_section ();							\     else								\       readonly_data_section ();						\   }									\ } while (0);
 end_define
 
 begin_comment
@@ -778,17 +795,11 @@ value|do {									\     if (!(TARGET_ELF))							\       emit_library_call (gen
 end_define
 
 begin_comment
-comment|/* Use dollar signs in special g++ assembler names.  */
+comment|/* Copy this from the svr4 specifications... */
 end_comment
 
-begin_undef
-undef|#
-directive|undef
-name|NO_DOLLAR_IN_LABEL
-end_undef
-
 begin_comment
-comment|/* Map i386 registers to the numbers dwarf expects.  Of course this is different    from what stabs expects.  */
+comment|/* Define the register numbers to be used in Dwarf debugging information.    The SVR4 reference port C compiler uses the following register numbers    in its Dwarf output code: 	0 for %eax (gnu regno = 0) 	1 for %ecx (gnu regno = 2) 	2 for %edx (gnu regno = 1) 	3 for %ebx (gnu regno = 3) 	4 for %esp (gnu regno = 7) 	5 for %ebp (gnu regno = 6) 	6 for %esi (gnu regno = 4) 	7 for %edi (gnu regno = 5)    The following three DWARF register numbers are never generated by    the SVR4 C compiler or by the GNU compilers, but SDB on x86/svr4    believes these numbers have these meanings. 	8  for %eip    (no gnu equivalent) 	9  for %eflags (no gnu equivalent) 	10 for %trapno (no gnu equivalent)    It is not at all clear how we should number the FP stack registers    for the x86 architecture.  If the version of SDB on x86/svr4 were    a bit less brain dead with respect to floating-point then we would    have a precedent to follow with respect to DWARF register numbers    for x86 FP registers, but the SDB on x86/svr4 is so completely    broken with respect to FP registers that it is hardly worth thinking    of it as something to strive for compatibility with.    The version of x86/svr4 SDB I have at the moment does (partially)    seem to believe that DWARF register number 11 is associated with    the x86 register %st(0), but that's about all.  Higher DWARF    register numbers don't seem to be associated with anything in    particular, and even for DWARF regno 11, SDB only seems to under-    stand that it should say that a variable lives in %st(0) (when    asked via an `=' command) if we said it was in DWARF regno 11,    but SDB still prints garbage when asked for the value of the    variable in question (via a `/' command).    (Also note that the labels SDB prints for various FP stack regs    when doing an `x' command are all wrong.)    Note that these problems generally don't affect the native SVR4    C compiler because it doesn't allow the use of -O with -g and    because when it is *not* optimizing, it allocates a memory    location for each floating-point variable, and the memory    location is what gets described in the DWARF AT_location    attribute for the variable in question.    Regardless of the severe mental illness of the x86/svr4 SDB, we    do something sensible here and we use the following DWARF    register numbers.  Note that these are all stack-top-relative    numbers. 	11 for %st(0) (gnu regno = 8) 	12 for %st(1) (gnu regno = 9) 	13 for %st(2) (gnu regno = 10) 	14 for %st(3) (gnu regno = 11) 	15 for %st(4) (gnu regno = 12) 	16 for %st(5) (gnu regno = 13) 	17 for %st(6) (gnu regno = 14) 	18 for %st(7) (gnu regno = 15) */
 end_comment
 
 begin_undef
@@ -873,7 +884,7 @@ parameter_list|,
 name|LABELNO
 parameter_list|)
 define|\
-value|{									\   if (flag_pic)								\     fprintf (FILE, "\tcall *%s@GOT(%%ebx)\n",				\       TARGET_AOUT ? "mcount" : ".mcount");				\   else									\     fprintf (FILE, "\tcall %s\n", TARGET_AOUT ? "mcount" : ".mcount");	\ }
+value|{									\   if (flag_pic)								\     {									\       fprintf (FILE, "\tcall *%s@GOT(%%ebx)\n",				\       TARGET_AOUT ? "mcount" : ".mcount");				\     }									\   else									\     {									\       fprintf (FILE, "\tcall %s\n", TARGET_AOUT ? "mcount" : ".mcount");	\     }									\ }
 end_define
 
 begin_define
@@ -946,98 +957,6 @@ name|WCHAR_TYPE_SIZE
 value|BITS_PER_WORD
 end_define
 
-begin_comment
-comment|/* FREEBSD_NATIVE is defined when gcc is integrated into the FreeBSD    source tree so it can be configured appropriately without using    the GNU configure/build mechanism. */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|FREEBSD_NATIVE
-end_ifdef
-
-begin_comment
-comment|/* Look for the include files in the system-defined places.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|GPLUSPLUS_INCLUDE_DIR
-value|"/usr/include/g++"
-end_define
-
-begin_define
-define|#
-directive|define
-name|GCC_INCLUDE_DIR
-value|"/usr/include"
-end_define
-
-begin_comment
-comment|/* FreeBSD has GCC_INCLUDE_DIR first.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|INCLUDE_DEFAULTS
-define|\
-value|{					\     { GCC_INCLUDE_DIR, 0, 0 },		\     { GPLUSPLUS_INCLUDE_DIR, 1, 1 },	\     { 0, 0, 0 }				\   }
-end_define
-
-begin_comment
-comment|/* Under FreeBSD, the normal location of the compiler back ends is the    /usr/libexec directory.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|STANDARD_EXEC_PREFIX
-value|"/usr/libexec/"
-end_define
-
-begin_comment
-comment|/* Under FreeBSD, the normal location of the various *crt*.o files is the    /usr/lib directory.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|STANDARD_STARTFILE_PREFIX
-value|"/usr/lib/"
-end_define
-
-begin_comment
-comment|/* On FreeBSD, gcc is called 'cc' */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|GCC_NAME
-value|"cc"
-end_define
-
-begin_comment
-comment|/* FreeBSD is 4.4BSD derived */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|bsd4_4
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* FREEBSD_NATIVE */
-end_comment
-
 begin_undef
 undef|#
 directive|undef
@@ -1048,7 +967,7 @@ begin_define
 define|#
 directive|define
 name|CPP_PREDEFINES
-value|"-Dunix -Di386 -D__FreeBSD__=4 -D__FreeBSD_cc_version=400001 -Asystem(unix) -Asystem(FreeBSD) -Acpu(i386) -Amachine(i386)"
+value|"-Di386 -Dunix -D__FreeBSD__=4 -D__FreeBSD_cc_version=400002 -Asystem(unix) -Asystem(FreeBSD) -Acpu(i386) -Amachine(i386)"
 end_define
 
 begin_undef
@@ -1150,7 +1069,7 @@ value|"%{!shared:libgcc.a%s}"
 end_define
 
 begin_comment
-comment|/* Provide a LINK_SPEC appropriate for FreeBSD.  Here we provide support    for the special GCC options -static and -shared, which allow us to    link things in one of these three modes by applying the appropriate    combinations of options at link-time. We like to support here for    as many of the other GNU linker options as possible. But I don't    have the time to search for those flags. I am sure how to add    support for -soname shared_object_name. H.J.     When the -shared link option is used a final link is not being    done.  */
+comment|/* Provide a LINK_SPEC appropriate for FreeBSD.  Here we provide support    for the special GCC options -static and -shared, which allow us to    link things in one of these three modes by applying the appropriate    combinations of options at link-time. We like to support here for    as many of the other GNU linker options as possible. But I don't    have the time to search for those flags. I am sure how to add    support for -soname shared_object_name. H.J.     I took out %{v:%{!V:-V}}. It is too much :-(. They can use    -Wl,-V.     When the -shared link option is used a final link is not being    done.  */
 end_comment
 
 begin_undef
@@ -1167,14 +1086,40 @@ value|"\  %{p:%e`-p' not supported; use `-pg' and gprof(1)} \   %{maout: %{share
 end_define
 
 begin_comment
-comment|/* Get perform_* macros to build libgcc.a.  */
+comment|/* A C statement to output to the stdio stream FILE an assembler    command to advance the location counter to a multiple of 1<<LOG    bytes if it is within MAX_SKIP bytes.     This is used to align code labels according to Intel recommendations.  */
 end_comment
 
-begin_include
-include|#
-directive|include
-file|"i386/perform.h"
-end_include
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|HAVE_GAS_MAX_SKIP_P2ALIGN
+end_ifdef
+
+begin_error
+error|#
+directive|error
+literal|"we don't have this for the aout gas"
+end_error
+
+begin_define
+define|#
+directive|define
+name|ASM_OUTPUT_MAX_SKIP_ALIGN
+parameter_list|(
+name|FILE
+parameter_list|,
+name|LOG
+parameter_list|,
+name|MAX_SKIP
+parameter_list|)
+define|\
+value|if ((LOG)!=0) \     if ((MAX_SKIP)==0) fprintf ((FILE), "\t.p2align %d\n", (LOG)); \     else fprintf ((FILE), "\t.p2align %d,,%d\n", (LOG), (MAX_SKIP))
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_undef
 undef|#
@@ -1226,32 +1171,16 @@ directive|define
 name|HAVE_ATEXIT
 end_define
 
+begin_comment
+comment|/* FreeBSD ELF using our home-grown crtbegin.o/crtend.o does not support the    DWARF2 unwinding mechanisms.  Once `make world' bootstraping problems with    the EGCS crtstuff.c is overcome, we will switch to the non-sjlj-exceptions     type exception machanism.  */
+end_comment
+
 begin_define
 define|#
 directive|define
-name|HAVE_PUTENV
+name|DWARF2_UNWIND_INFO
+value|0
 end_define
-
-begin_comment
-comment|/* to assist building libgcc2.c */
-end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|__ELF__
-end_ifndef
-
-begin_undef
-undef|#
-directive|undef
-name|OBJECT_FORMAT_ELF
-end_undef
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 end_unit
 
