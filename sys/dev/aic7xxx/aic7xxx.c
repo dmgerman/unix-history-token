d2c9a76f6ca98ff89770c207e092f9fbee26be75
Justin T. Gibbs <gibbs@FreeBSD.org>
1999-09-20 19:04:22 +0000

    Clear the SELINGO bit after a selection timeout occurs.  SELINGO is usually cleared by a successful selection, but there is no guarantee that a future successful selection will ever occur (e.g. empty bus). The driver never looks at SELINGO, but the busy LED does, so this change has the cosmetic effect of fixing the rare instance where the busy LED was left on, confusing the user.
    Former-commit-id: 2b336f3034e6bb7b456a5c8ab03e740b70783c1a

--- sys/dev/aic7xxx/aic7xxx.c
+++ sys/dev/aic7xxx/aic7xxx.c
@@ -12575,11 +12575,23 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
+comment|/* No more pending messages */
 name|ahc_clear_msg_state
 argument_list|(
 name|ahc
 argument_list|)
 expr_stmt|;
+comment|/* 		 * Although the driver does not care about the 		 * 'Selection in Progress' status bit, the busy 		 * LED does.  SELINGO is only cleared by a sucessful 		 * selection, so we must manually clear it to ensure 		 * the LED turns off just incase no future successful 		 * selections occur (e.g. no devices on the bus). 		 */
+name|ahc_outb
+argument_list|(
+name|ahc
+argument_list|,
+name|CLRSINT0
+argument_list|,
+name|CLRSELINGO
+argument_list|)
+expr_stmt|;
+comment|/* Clear interrupt state */
 name|ahc_outb
 argument_list|(
 name|ahc
@@ -20893,19 +20905,6 @@ name|ccb
 argument_list|)
 expr_stmt|;
 break|break;
-block|}
-if|if
-condition|(
-name|ccb
-operator|->
-name|ccb_h
-operator|.
-name|func_code
-operator|==
-name|XPT_NOTIFY_ACK
-condition|)
-block|{
-comment|/* Clear notification state */
 block|}
 name|SLIST_INSERT_HEAD
 argument_list|(
