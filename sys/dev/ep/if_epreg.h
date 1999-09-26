f52e47a432138c895b63c37952175d7100cfe451
Matthew N. Dodd <mdodd@FreeBSD.org>
1999-09-26 06:42:36 +0000

    - Split out the ISA and PCCARD specific code.
    - Split out the prototypes, externs and struct decls from if_epreg.h into
      if_epvar.h.
    
    - Add support for MCA based Etherlink III (3c529) devices.
    
    None of this code is used right now; the old if_ep driver is still
    in place and used.
    
    I will eventually get around to converting if_ep_isa.c to newbus once I've
    had a talk with Peter and DFR about the DEVICE_IDENTIFY() method.
    
    I have tested this code on my PS/2.  It works.  I would like EISA and ISA
    testers since my example hardware hasn't arrived yet.
    
    Add:
    
    dev/ep/if_ep.c          optional ep
    dev/ep/if_ep_isa.c      optional ep isa
    dev/ep/if_ep_eisa.c     optional ep eisa
    dev/ep/if_ep_mca.c      optional ep mca
    dev/ep/if_ep_pccard.c   optional ep card
    
    to sys/conf/files
    
    Remove:
    
    i386/eisa/3c5x9.c             optional        ep
    i386/isa/if_ep.c              optional        ep
    
    from sys/i386/conf/files.i386
    
    PCCARD testers wanted!
    
    I will switch off and cvs rm the old driver in favor of this copy once
    I've had positive feedback or have the hardware to verify that it works.
    
    
    Former-commit-id: acd5b0d7ce994c8935ccaa3b4cfac4151b9479ea

--- sys/dev/ep/if_epreg.h
+++ sys/dev/ep/if_epreg.h
@@ -1,146 +1,25 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1993 Herb Peyerl (hpeyerl@novatel.ca) All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met: 1. Redistributions of source code must retain the above copyright  * notice, this list of conditions and the following disclaimer. 2. The name  * of the author may not be used to endorse or promote products derived from  * this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO  * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED  * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * if_epreg.h,v 1.4 1994/11/13 10:12:37 gibbs Exp Modified by:  *  October 2, 1994   Modified by: Andres Vega Garcia   INRIA - Sophia Antipolis, France  e-mail: avega@sophia.inria.fr  finger: avega@pax.inria.fr   */
+comment|/*  * Copyright (c) 1993 Herb Peyerl (hpeyerl@novatel.ca) All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions are  * met: 1. Redistributions of source code must retain the above copyright  * notice, this list of conditions and the following disclaimer. 2. The name  * of the author may not be used to endorse or promote products derived from  * this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO  * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED  * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_comment
-comment|/*  * $FreeBSD$  *  *  Promiscuous mode added and interrupt logic slightly changed  *  to reduce the number of adapter failures. Transceiver select  *  logic changed to use value from EEPROM. Autoconfiguration  *  features added.  *  Done by:  *          Serge Babkin  *          Chelindbank (Chelyabinsk, Russia)  *          babkin@hq.icb.chel.su  */
+comment|/*  * DELAY_MULTIPLE: How much to boost "base" delays, except  * for the inter-bit delays in get_eeprom_data.  A cyrix Media GX needed this.  */
 end_comment
 
-begin_comment
-comment|/*  * Pccard support for 3C589 by:  *		HAMADA Naoki  *		nao@tom-yam.or.jp  */
-end_comment
-
-begin_comment
-comment|/*  * Ethernet software status per interface.  */
-end_comment
-
-begin_struct
-struct|struct
-name|ep_softc
-block|{
-name|struct
-name|arpcom
-name|arpcom
-decl_stmt|;
-comment|/* Ethernet common part		 */
-name|int
-name|ep_io_addr
-decl_stmt|;
-comment|/* i/o bus address		 */
-name|struct
-name|mbuf
-modifier|*
-name|top
-decl_stmt|,
-modifier|*
-name|mcur
-decl_stmt|;
-name|short
-name|cur_len
-decl_stmt|;
-name|u_short
-name|ep_connectors
-decl_stmt|;
-comment|/* Connectors on this card.	 */
-name|u_char
-name|ep_connector
-decl_stmt|;
-comment|/* Configured connector.	 */
-name|int
-name|stat
-decl_stmt|;
-comment|/* some flags */
-name|int
-name|gone
-decl_stmt|;
-comment|/* adapter is not present (for PCCARD) */
+begin_define
 define|#
 directive|define
-name|F_RX_FIRST
-value|0x1
-define|#
-directive|define
-name|F_PROMISC
-value|0x8
-define|#
-directive|define
-name|F_ACCESS_32_BITS
-value|0x100
-name|struct
-name|ep_board
-modifier|*
-name|epb
-decl_stmt|;
-name|int
-name|unit
-decl_stmt|;
-ifdef|#
-directive|ifdef
-name|EP_LOCAL_STATS
-name|short
-name|tx_underrun
-decl_stmt|;
-name|short
-name|rx_no_first
-decl_stmt|;
-name|short
-name|rx_no_mbuf
-decl_stmt|;
-name|short
-name|rx_bpf_disc
-decl_stmt|;
-name|short
-name|rx_overrunf
-decl_stmt|;
-name|short
-name|rx_overrunl
-decl_stmt|;
-endif|#
-directive|endif
-block|}
-struct|;
-end_struct
+name|DELAY_MULTIPLE
+value|10
+end_define
 
-begin_struct
-struct|struct
-name|ep_board
-block|{
-name|int
-name|epb_addr
-decl_stmt|;
-comment|/* address of this board */
-name|char
-name|epb_used
-decl_stmt|;
-comment|/* was this entry already used for configuring ? */
-comment|/* data from EEPROM for later use */
-name|u_short
-name|eth_addr
-index|[
-literal|3
-index|]
-decl_stmt|;
-comment|/* Ethernet address */
-name|u_short
-name|prod_id
-decl_stmt|;
-comment|/* product ID */
-name|int
-name|cmd_off
-decl_stmt|;
-comment|/* command offset (bit shift) */
-name|int
-name|mii_trans
-decl_stmt|;
-comment|/* activate MII transiever */
-name|u_short
-name|res_cfg
-decl_stmt|;
-comment|/* resource configuration */
-block|}
-struct|;
-end_struct
+begin_define
+define|#
+directive|define
+name|BIT_DELAY_MULTIPLE
+value|10
+end_define
 
 begin_comment
 comment|/*  * Some global constants  */
@@ -1499,116 +1378,6 @@ directive|define
 name|RX_BYTES_MASK
 value|(u_short) (0x07ff)
 end_define
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|ep_board
-name|ep_board
-index|[]
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|ep_boards
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|u_long
-name|ep_unit
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|ep_softc
-modifier|*
-name|ep_alloc
-name|__P
-argument_list|(
-operator|(
-name|int
-name|unit
-operator|,
-expr|struct
-name|ep_board
-operator|*
-name|epb
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|void
-name|ep_free
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|ep_softc
-operator|*
-name|sc
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|void
-name|ep_intr
-name|__P
-argument_list|(
-operator|(
-name|void
-operator|*
-name|sc
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|ep_attach
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|ep_softc
-operator|*
-name|sc
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|u_int16_t
-name|get_e
-name|__P
-argument_list|(
-operator|(
-expr|struct
-name|ep_softc
-operator|*
-name|sc
-operator|,
-name|int
-name|offset
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
 
 begin_comment
 comment|/*  * Config flags  */
