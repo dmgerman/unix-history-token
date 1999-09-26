e9961b636437deb8f94d8581ec500add949c66e4
Eivind Eklund <eivind@FreeBSD.org>
1999-09-26 18:19:49 +0000

    Move the declaration of panic() from sys/systm.h to sys/param.h. Rationale: Wider access, so we can add assertions to header files. panicstr is still in sys/systm.h
    Suggested by:   phk
    Discussed with: peter
    
    
    Former-commit-id: 7d3c694ad6175b16097404f9601e7859fbb5ccea

--- sys/dev/advansys/adwlib.c
+++ sys/dev/advansys/adwlib.c
@@ -10,7 +10,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<sys/types.h>
+file|<sys/param.h>
 end_include
 
 begin_include
--- sys/i386/i386/mpapic.c
+++ sys/i386/i386/mpapic.c
@@ -12,7 +12,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/types.h>
+file|<sys/param.h>
 end_include
 
 begin_include
