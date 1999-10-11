8f5367bb9e545e2eccbd81724dd1f01d7722c1f8
Poul-Henning Kamp <phk@FreeBSD.org>
1999-10-11 19:12:30 +0000

    remove unused #includes.
    Former-commit-id: 99031bb06bc455861bd94f379e40e45af891e596

--- sys/contrib/dev/fla/fla.c
+++ sys/contrib/dev/fla/fla.c
@@ -72,12 +72,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/bus.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/clock.h>
 end_include
 
@@ -109,12 +103,6 @@ begin_include
 include|#
 directive|include
 file|<sys/bus.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<isa/isareg.h>
 end_include
 
 begin_include
