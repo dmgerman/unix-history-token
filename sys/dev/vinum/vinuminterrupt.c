4c109da9076565ff0393a1b0a7994980a61cd970
Greg Lehey <grog@FreeBSD.org>
1999-10-17 23:56:47 +0000

    sdio_done:   Set the errors in the correct buffer header.
    Inspection-provoked-by: Bernd Walter <ticso@cicely.de>
    
    Former-commit-id: d3a20b802e1a87810219a1a2c738c518531458d6

--- sys/dev/vinum/vinuminterrupt.c
+++ sys/dev/vinum/vinuminterrupt.c
@@ -4,7 +4,7 @@ comment|/* vinuminterrupt.c: bottom half of the driver */
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998, 1999  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinuminterrupt.c,v 1.7 1999/10/12 04:34:50 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998, 1999  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id$  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -1123,12 +1123,17 @@ name|B_ERROR
 condition|)
 block|{
 comment|/* had an error */
+name|sbp
+operator|->
 name|bp
 operator|->
 name|b_flags
 operator||=
 name|B_ERROR
 expr_stmt|;
+comment|/* propagate upwards */
+name|sbp
+operator|->
 name|bp
 operator|->
 name|b_error
@@ -1177,14 +1182,6 @@ operator|.
 name|b_resid
 expr_stmt|;
 comment|/* copy the resid field */
-name|biodone
-argument_list|(
-name|sbp
-operator|->
-name|bp
-argument_list|)
-expr_stmt|;
-comment|/* complete the caller's I/O */
 comment|/* Now update the statistics */
 if|if
 condition|(
@@ -1215,8 +1212,10 @@ index|]
 operator|.
 name|bytes_read
 operator|+=
-name|bp
+name|sbp
 operator|->
+name|b
+operator|.
 name|b_bcount
 expr_stmt|;
 name|SD
@@ -1238,8 +1237,10 @@ index|]
 operator|.
 name|bytes_read
 operator|+=
-name|bp
+name|sbp
 operator|->
+name|b
+operator|.
 name|b_bcount
 expr_stmt|;
 block|}
@@ -1265,8 +1266,10 @@ index|]
 operator|.
 name|bytes_written
 operator|+=
-name|bp
+name|sbp
 operator|->
+name|b
+operator|.
 name|b_bcount
 expr_stmt|;
 name|SD
@@ -1288,11 +1291,21 @@ index|]
 operator|.
 name|bytes_written
 operator|+=
-name|bp
+name|sbp
 operator|->
+name|b
+operator|.
 name|b_bcount
 expr_stmt|;
 block|}
+name|biodone
+argument_list|(
+name|sbp
+operator|->
+name|bp
+argument_list|)
+expr_stmt|;
+comment|/* complete the caller's I/O */
 name|Free
 argument_list|(
 name|sbp
