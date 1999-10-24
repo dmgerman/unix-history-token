4b5145d2fe6ff1689ce036f795c5873d39751070
Matthew N. Dodd <mdodd@FreeBSD.org>
1999-10-24 07:17:26 +0000

    Fix compiling PCCARD kernels.
    This is a hack and I don't like it.
    
    Hopefully the new PCCARD stuff should be in working order in a day or so.
    
    
    Former-commit-id: 8deb637a4260c0fd274968d5cef1fcfd3f4429d2

--- sys/dev/ed/if_ed_isa.c
+++ sys/dev/ed/if_ed_isa.c
@@ -6,6 +6,20 @@ end_comment
 begin_include
 include|#
 directive|include
+file|"card.h"
+end_include
+
+begin_if
+if|#
+directive|if
+name|NCARD
+operator|==
+literal|0
+end_if
+
+begin_include
+include|#
+directive|include
 file|<sys/param.h>
 end_include
 
@@ -593,6 +607,11 @@ literal|0
 argument_list|)
 expr_stmt|;
 end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
