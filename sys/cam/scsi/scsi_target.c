cd1f294752a0c0a82c3330e7c837858209e10774
Matthew Jacob <mjacob@FreeBSD.org>
1999-10-27 21:52:21 +0000

    MFC: Mark path for deallocation only *after* you've successfully allocated it.
    Former-commit-id: 619d92d518710fcf2d614e109117fa3065f84aa6

--- sys/cam/scsi/scsi_target.c
+++ sys/cam/scsi/scsi_target.c
@@ -2764,9 +2764,6 @@ operator|->
 name|lun_id
 argument_list|)
 expr_stmt|;
-name|free_path_on_return
-operator|++
-expr_stmt|;
 if|if
 condition|(
 name|status
@@ -2785,6 +2782,9 @@ goto|goto
 name|fail
 goto|;
 block|}
+name|free_path_on_return
+operator|++
+expr_stmt|;
 name|xpt_setup_ccb
 argument_list|(
 operator|&
