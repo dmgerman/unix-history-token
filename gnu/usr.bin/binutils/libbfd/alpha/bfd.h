3a25d0818da4cbd513ff73dca1e8bdf9bc85e868
Marcel Moolenaar <marcel@FreeBSD.org>
1999-11-12 16:53:58 +0000

    Make binutils for Alpha work on i386 machine, by properly defining whether the host machine has a 64-bit long or not.
    Former-commit-id: f8d3fb0a72fdad2f6d7b5927f0bcca953a3ea882

--- gnu/usr.bin/binutils/libbfd/alpha/bfd.h
+++ gnu/usr.bin/binutils/libbfd/alpha/bfd.h
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/* Main header file for the bfd library -- portable access to object files.    Copyright 1990, 91, 92, 93, 94, 95, 96, 97, 1998    Free Software Foundation, Inc.    Contributed by Cygnus Support.  ** NOTE: bfd.h and bfd-in2.h are GENERATED files.  Don't change them; ** instead, change bfd-in.h or the other BFD source files processed to ** generate these files.  This file is part of BFD, the Binary File Descriptor library.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
@@ -43,10 +47,24 @@ define|#
 directive|define
 name|BFD_ARCH_SIZE
 value|64
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__i386__
+argument_list|)
+define|#
+directive|define
+name|BFD_HOST_64BIT_LONG
+value|0
+else|#
+directive|else
 define|#
 directive|define
 name|BFD_HOST_64BIT_LONG
 value|1
+endif|#
+directive|endif
 if|#
 directive|if
 literal|0
