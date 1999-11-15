7b3116a273a38dcc7bbea3a2b22b60f3d8026865
David E. O'Brien <obrien@FreeBSD.org>
1999-11-15 19:26:58 +0000

    Fix a minor bogon.
    Former-commit-id: ddd99579330778cb8e1bb80b3641123ccd2a6e77

--- contrib/gcc/config/alpha/freebsd.h
+++ contrib/gcc/config/alpha/freebsd.h
@@ -191,7 +191,7 @@ begin_define
 define|#
 directive|define
 name|LINK_SPEC
-value|"-m elf64alpha					\   %{p:%e`-p' not supported; use `-pg' and gprof(1)}		\   %{Wl,*:%*}							\   %{assert*} %{R*} %{rpath*} %{defsym*}				\   %{shared:-Bshareable %{h*} %{soname*}}			\   %{symbolic:-Bsymbolic}					\   %{!shared:							\     %{!static:							\       %{rdynamic:-export-dynamic}				\       %{!dynamic-linker:-dynamic-linker /usr/libexec/ld-elf.so.1} \     %{static:-Bstatic}}"
+value|"-m elf64alpha					\   %{p:%e`-p' not supported; use `-pg' and gprof(1)}		\   %{Wl,*:%*}							\   %{assert*} %{R*} %{rpath*} %{defsym*}				\   %{shared:-Bshareable %{h*} %{soname*}}			\   %{symbolic:-Bsymbolic}					\   %{!shared:							\     %{!static:							\       %{rdynamic:-export-dynamic}				\       %{!dynamic-linker:-dynamic-linker /usr/libexec/ld-elf.so.1}} \     %{static:-Bstatic}}"
 end_define
 
 begin_comment
