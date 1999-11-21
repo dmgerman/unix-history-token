b3618e1c7aa5bd7eab8fb7e5b167bcaa2b3acd9c
Philippe Charnier <charnier@FreeBSD.org>
1999-11-21 16:11:56 +0000

    Correct use of .Nm. Add DIAGNOSTICS section. Add rcsid
    Former-commit-id: fa7d4f9c9cd7d8855dde9c8ff24a0df114d2700a

--- bin/chflags/chflags.c
+++ bin/chflags/chflags.c
@@ -35,14 +35,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)chflags.c	8.5 (Berkeley) 4/1/94";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
 specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)chflags.c	8.5 (Berkeley) 4/1/94"
+literal|"$FreeBSD$"
 decl_stmt|;
 end_decl_stmt
 
