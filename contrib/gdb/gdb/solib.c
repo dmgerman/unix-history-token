8932a72f4ed6b0bc5ff47cbef753031c07d0ca7c
Peter Wemm <peter@FreeBSD.org>
1999-11-22 18:04:53 +0000

    Fix (I believe) Bill Paul's core dump when using gdb on shared executables. This is an addition to rev 1.5.  If lm == NULL, new->lm is uninitialized.
    Former-commit-id: 2285a3674b9babf1ec1fa8f5ac101ce29f36ed31

--- contrib/gdb/gdb/solib.c
+++ contrib/gdb/gdb/solib.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Handle SunOS and SVR4 shared libraries for GDB, the GNU Debugger.    Copyright 1990, 91, 92, 93, 94, 95, 96, 98, 1999    Free Software Foundation, Inc.     This file is part of GDB.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -3704,6 +3708,8 @@ expr_stmt|;
 comment|/* For SVR4 versions, the first entry in the link map is for the 	 inferior executable, so we must ignore it.  For some versions of 	 SVR4, it has no name.  For others (Solaris 2.3 for example), it 	 does have a name, so we can no longer use a missing name to 	 decide when to ignore it. */
 if|if
 condition|(
+name|lm
+operator|&&
 name|new
 operator|&&
 operator|!
