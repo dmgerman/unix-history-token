46d503b8e9ff60f96e2d34a62833ba4d91c4b6dc
Bill Paul <wpaul@FreeBSD.org>
1999-12-05 22:18:29 +0000

    Remove references to old joystick driver from this file, which Peter forgot to do when he converted it to newbus.
    Note: make *sure* to remove any old joy.o object files from your kernel
    build directories in order to be sure the joy driver is properly recompiled.
    
    Noticed by: phk
    
    
    Former-commit-id: 43afeb8d6bf6e1df4cadd3fee14380b44b8161b1

--- sys/i386/isa/isa_compat.h
+++ sys/i386/isa/isa_compat.h
@@ -252,12 +252,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"joy.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"cy.h"
 end_include
 
@@ -698,14 +692,6 @@ specifier|extern
 name|struct
 name|isa_driver
 name|gscdriver
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|isa_driver
-name|joydriver
 decl_stmt|;
 end_decl_stmt
 
@@ -1667,20 +1653,6 @@ name|INTR_TYPE_MISC
 block|,
 operator|&
 name|spigotdriver
-block|}
-block|,
-endif|#
-directive|endif
-if|#
-directive|if
-name|NJOY
-operator|>
-literal|0
-block|{
-name|INTR_TYPE_MISC
-block|,
-operator|&
-name|joydriver
 block|}
 block|,
 endif|#
