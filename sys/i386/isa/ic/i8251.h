ae6c35a8cc102d7f3f2ec49187bd19124c93f00c
Takahashi Yoshihiro <nyan@FreeBSD.org>
1999-12-06 03:28:55 +0000

    MFC: Supported i8251 (internal COM1) FIFO mode.
    Approved by:	Jordan K. Hubbard
    
    
    Former-commit-id: 25b8fa5c8c1e19f64cb0db257f9b3af4781a7024

--- sys/i386/isa/ic/i8251.h
+++ sys/i386/isa/ic/i8251.h
@@ -1,10 +1,14 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ns16550.h	7.1 (Berkeley) 5/9/91  */
+comment|/*-  * Copyright (c) 1991 The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ns16550.h	7.1 (Berkeley) 5/9/91  * $FreeBSD$  */
 end_comment
 
 begin_comment
 comment|/*  * modified for PC9801 by M.Ishii   *			Kyoto University Microcomputer Club (KMC)  */
+end_comment
+
+begin_comment
+comment|/*  * modified for 8251(FIFO) by Seigo TANIMURA<tanimura@FreeBSD.org>  */
 end_comment
 
 begin_comment
@@ -190,6 +194,193 @@ end_comment
 begin_define
 define|#
 directive|define
+name|STS8251F_TxEMP
+value|0x01
+end_define
+
+begin_comment
+comment|/* transmit buffer EMPTY */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STS8251F_TxRDY
+value|0x02
+end_define
+
+begin_comment
+comment|/* transmit READY */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STS8251F_RxRDY
+value|0x04
+end_define
+
+begin_comment
+comment|/* data exists in receive buffer */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STS8251F_OE
+value|0x10
+end_define
+
+begin_comment
+comment|/* overrun error */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STS8251F_PE
+value|0x20
+end_define
+
+begin_comment
+comment|/* perity error */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STS8251F_BD_SD
+value|0x80
+end_define
+
+begin_comment
+comment|/* break detect (async) / sync detect (sync) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_DTCT
+value|0x60
+end_define
+
+begin_comment
+comment|/* FIFO detection mask */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_INTRV
+value|0x0e
+end_define
+
+begin_comment
+comment|/* interrupt event */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_TO
+value|0x0c
+end_define
+
+begin_comment
+comment|/* receive timeout */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_LSTS
+value|0x06
+end_define
+
+begin_comment
+comment|/* line status */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_RxRDY
+value|0x04
+end_define
+
+begin_comment
+comment|/* receive READY */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_TxRDY
+value|0x02
+end_define
+
+begin_comment
+comment|/* transmit READY */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_ISEV
+value|0x01
+end_define
+
+begin_comment
+comment|/* event occured */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INTR8251F_MSTS
+value|0x00
+end_define
+
+begin_comment
+comment|/* modem status */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CTRL8251F_ENABLE
+value|0x01
+end_define
+
+begin_comment
+comment|/* enable FIFO */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CTRL8251F_RCV_RST
+value|0x02
+end_define
+
+begin_comment
+comment|/* reset receive FIFO */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CTRL8251F_XMT_RST
+value|0x04
+end_define
+
+begin_comment
+comment|/* reset transmit FIFO */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MOD8251_5BITS
 value|0x00
 end_define
@@ -295,12 +486,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|CICSCD_CI
-value|0x80
+name|CICSCD_CD
+value|0x20
 end_define
 
 begin_comment
-comment|/* CI */
+comment|/* CD */
 end_comment
 
 begin_define
@@ -317,8 +508,52 @@ end_comment
 begin_define
 define|#
 directive|define
-name|CICSCD_CD
+name|CICSCD_CI
+value|0x80
+end_define
+
+begin_comment
+comment|/* CI */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CICSCDF_CS
+value|0x10
+end_define
+
+begin_comment
+comment|/* CS */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CICSCDF_DR
 value|0x20
+end_define
+
+begin_comment
+comment|/* DR */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CICSCDF_CI
+value|0x40
+end_define
+
+begin_comment
+comment|/* CI */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CICSCDF_CD
+value|0x80
 end_define
 
 begin_comment
