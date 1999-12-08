6d1d0fbfb08d0e7364dc7ce8f918719600facb1c
David E. O'Brien <obrien@FreeBSD.org>
1999-12-08 06:43:26 +0000

    MFC: 1.4 (document "X")
    Former-commit-id: aac423b6f557dd6a3db531a68f06a81f5186f804

--- contrib/isc-dhcp/common/tables.c
+++ contrib/isc-dhcp/common/tables.c
@@ -7,6 +7,10 @@ begin_comment
 comment|/*  * Copyright (c) 1995, 1996 The Internet Software Consortium.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of The Internet Software Consortium nor the names  *    of its contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE INTERNET SOFTWARE CONSORTIUM AND  * CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE INTERNET SOFTWARE CONSORTIUM OR  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * This software has been written for the Internet Software Consortium  * by Ted Lemon<mellon@fugue.com> in cooperation with Vixie  * Enterprises.  To learn more about the Internet Software Consortium,  * see ``http://www.vix.com/isc''.  To learn more about Vixie  * Enterprises, see ``http://www.vix.com''.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -39,7 +43,7 @@ file|"dhcpd.h"
 end_include
 
 begin_comment
-comment|/* DHCP Option names, formats and codes, from RFC1533.     Format codes:     e - end of data    I - IP address    l - 32-bit signed integer    L - 32-bit unsigned integer    s - 16-bit signed integer    S - 16-bit unsigned integer    b - 8-bit signed integer    B - 8-bit unsigned integer    t - ASCII text    f - flag (true or false)    A - array of whatever precedes (e.g., IA means array of IP addresses) */
+comment|/* DHCP Option names, formats and codes, from RFC1533.     Format codes:     e - end of data    I - IP address    l - 32-bit signed integer    L - 32-bit unsigned integer    s - 16-bit signed integer    S - 16-bit unsigned integer    b - 8-bit signed integer    B - 8-bit unsigned integer    t - ASCII text    f - flag (true or false)    A - array of whatever precedes (e.g., IA means array of IP addresses)    X - hex dump */
 end_comment
 
 begin_decl_stmt
