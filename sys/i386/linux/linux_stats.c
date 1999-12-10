9b0f62987c863e929d9e949ca19175c070208536
Marcel Moolenaar <marcel@FreeBSD.org>
1999-12-10 12:36:20 +0000

    Remove unused includes.
    Found by: phk-scan
    
    
    Former-commit-id: 66e49f29e47dc5d97311454a6b0d09654be4e988

--- sys/i386/linux/linux_stats.c
+++ sys/i386/linux/linux_stats.c
@@ -36,18 +36,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/socketvar.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/pipe.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/proc.h>
 end_include
 
