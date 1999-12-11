80ec019ddd9fa352a676219d879a62561a5b7bbf
Peter Wemm <peter@FreeBSD.org>
1999-12-11 15:10:02 +0000

    Unmangle cvs's MD5* calls.
    Former-commit-id: 45931399b82f40e5aca2924bcf9af4d27ac74d0e

--- contrib/cvs/lib/md5.h
+++ contrib/cvs/lib/md5.h
@@ -3,6 +3,10 @@ begin_comment
 comment|/* See md5.c for explanation and copyright information.  */
 end_comment
 
+begin_comment
+comment|/*  * $FreeBSD$  */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -14,6 +18,58 @@ define|#
 directive|define
 name|MD5_H
 end_define
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|cvs_MD5Context
+value|MD5Context
+end_define
+
+begin_define
+define|#
+directive|define
+name|cvs_MD5Init
+value|MD5Init
+end_define
+
+begin_define
+define|#
+directive|define
+name|cvs_MD5Update
+value|MD5Update
+end_define
+
+begin_define
+define|#
+directive|define
+name|cvs_MD5Final
+value|MD5Final
+end_define
+
+begin_define
+define|#
+directive|define
+name|cvs_MD5Transform
+value|MD5Transform
+end_define
+
+begin_include
+include|#
+directive|include
+file|<sys/md5.h>
+end_include
+
+begin_else
+else|#
+directive|else
+end_else
 
 begin_comment
 comment|/* Unlike previous versions of this code, uint32 need not be exactly    32 bits, merely 32 bits or more.  Choosing a data type which is 32    bits instead of 64 is not important; speed is considerably more    important.  ANSI guarantees that "unsigned long" will be big enough,    and always using it seems to have few disadvantages.  */
@@ -138,6 +194,11 @@ operator|)
 argument_list|)
 decl_stmt|;
 end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_endif
 endif|#
