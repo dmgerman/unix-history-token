ff481836f797b367e7c8a1853351d55fa85b7d78
Greg Lehey <grog@FreeBSD.org>
1999-12-17 01:18:44 +0000

    Change location of temporary file from /tmp to /var/tmp.  This is a repeat of an earlier commit which apparently got lost with the last import.  It helps solve the frequently reported problem
       pid 4032 (mail.local), uid 0 on /: file system full
    
    (though there appears to be a lot of space) caused by idiots sending
    30 MB mail messages.
    
    Most-recently-reported-by:  jahanur <jahanur@jjsoft.com>
    
    Add $FreeBSD$ so that I can check the file back in.
    
    Rejected-by:	CVS
    
    Former-commit-id: 654dbcccde079fbd487300c08b68cf477fb3e803

--- contrib/sendmail/mail.local/pathnames.h
+++ contrib/sendmail/mail.local/pathnames.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Sendmail, Inc.  All rights reserved.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * By using this file, you agree to the terms and conditions set  * forth in the LICENSE file which can be found at the top level of  * the sendmail distribution.  *  *  *	@(#)pathnames.h	8.5 (Berkeley) 5/19/1998  */
+comment|/*-  * Copyright (c) 1998 Sendmail, Inc.  All rights reserved.  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * By using this file, you agree to the terms and conditions set  * forth in the LICENSE file which can be found at the top level of  * the sendmail distribution.  *  *  *	@(#)pathnames.h	8.5 (Berkeley) 5/19/1998  *	$FreeBSD$  */
 end_comment
 
 begin_include
@@ -13,7 +13,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_LOCTMP
-value|"/tmp/local.XXXXXX"
+value|"/var/tmp/local.XXXXXX"
 end_define
 
 end_unit
