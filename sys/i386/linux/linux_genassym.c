c632a48dc556f0df5e9eb45ceb07a0f8b676cf3e
Marcel Moolenaar <marcel@FreeBSD.org>
1999-12-23 19:59:47 +0000

    Use genassym(1) and <sys/assym.h> to generate assembler symbols.
    Former-commit-id: fa35815b1e4f8db2bb0b74e133419fcfd9875ec5

--- sys/i386/linux/linux_genassym.c
+++ sys/i386/linux/linux_genassym.c
@@ -6,6 +6,12 @@ end_comment
 begin_include
 include|#
 directive|include
+file|<sys/assym.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/param.h>
 end_include
 
@@ -15,68 +21,12 @@ directive|include
 file|<i386/linux/linux.h>
 end_include
 
-begin_define
-define|#
-directive|define
-name|offsetof
-parameter_list|(
-name|type
-parameter_list|,
-name|member
-parameter_list|)
-value|((size_t)(&((type *)0)->member))
-end_define
-
-begin_define
-define|#
-directive|define
-name|OS
-parameter_list|(
-name|s
-parameter_list|,
-name|m
-parameter_list|)
-value|((u_int)offsetof(struct s, m))
-end_define
-
-begin_decl_stmt
-name|int
-decl|main
-name|__P
+begin_expr_stmt
+name|ASSYM
 argument_list|(
-operator|(
-name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|printf
-name|__P
-argument_list|(
-operator|(
-specifier|const
-name|char
-operator|*
-operator|,
-operator|...
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
-
-begin_function
-name|int
-name|main
-parameter_list|()
-block|{
-name|printf
-argument_list|(
-literal|"#define\tLINUX_SIGF_HANDLER %u\n"
+name|LINUX_SIGF_HANDLER
 argument_list|,
-name|OS
+name|offsetof
 argument_list|(
 name|linux_sigframe
 argument_list|,
@@ -84,11 +34,14 @@ name|sf_handler
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|printf
+end_expr_stmt
+
+begin_expr_stmt
+name|ASSYM
 argument_list|(
-literal|"#define\tLINUX_SIGF_SC %u\n"
+name|LINUX_SIGF_SC
 argument_list|,
-name|OS
+name|offsetof
 argument_list|(
 name|linux_sigframe
 argument_list|,
@@ -96,11 +49,14 @@ name|sf_sc
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|printf
+end_expr_stmt
+
+begin_expr_stmt
+name|ASSYM
 argument_list|(
-literal|"#define\tLINUX_SC_GS %u\n"
+name|LINUX_SC_GS
 argument_list|,
-name|OS
+name|offsetof
 argument_list|(
 name|linux_sigcontext
 argument_list|,
@@ -108,11 +64,14 @@ name|sc_gs
 argument_list|)
 argument_list|)
 expr_stmt|;
-name|printf
+end_expr_stmt
+
+begin_expr_stmt
+name|ASSYM
 argument_list|(
-literal|"#define\tLINUX_SC_EFLAGS %u\n"
+name|LINUX_SC_EFLAGS
 argument_list|,
-name|OS
+name|offsetof
 argument_list|(
 name|linux_sigcontext
 argument_list|,
@@ -120,13 +79,7 @@ name|sc_eflags
 argument_list|)
 argument_list|)
 expr_stmt|;
-return|return
-operator|(
-literal|0
-operator|)
-return|;
-block|}
-end_function
+end_expr_stmt
 
 end_unit
 
