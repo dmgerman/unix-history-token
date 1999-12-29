201c242d38f55062f1e8d580f3daac5dcf2aea1b
Peter Wemm <peter@FreeBSD.org>
1999-12-29 17:50:34 +0000

    Connect fparseln(3) for mailwrapper(8)
    Former-commit-id: b0a11949d6a04895ff9755c0137f98a7c961cdb0

--- lib/libutil/fparseln.c
+++ lib/libutil/fparseln.c
@@ -4,6 +4,10 @@ comment|/*	$NetBSD: fparseln.c,v 1.9 1999/09/20 04:48:06 lukem Exp $	*/
 end_comment
 
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/*  * Copyright (c) 1997 Christos Zoulas.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Christos Zoulas.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
@@ -31,7 +35,7 @@ end_if
 begin_expr_stmt
 name|__RCSID
 argument_list|(
-literal|"$NetBSD: fparseln.c,v 1.9 1999/09/20 04:48:06 lukem Exp $"
+literal|"$FreeBSD$"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -40,6 +44,12 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
+end_include
 
 begin_include
 include|#
@@ -74,7 +84,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<util.h>
+file|<libutil.h>
 end_include
 
 begin_decl_stmt
@@ -139,20 +149,12 @@ decl_stmt|;
 name|size_t
 name|ne
 decl_stmt|;
-name|_DIAGASSERT
-argument_list|(
-name|sp
-operator|!=
-name|NULL
-argument_list|)
-expr_stmt|;
-name|_DIAGASSERT
-argument_list|(
-name|p
-operator|!=
-name|NULL
-argument_list|)
-expr_stmt|;
+if|#
+directive|if
+literal|0
+block|_DIAGASSERT(sp != NULL); 	_DIAGASSERT(p != NULL);
+endif|#
+directive|endif
 comment|/* No escape character */
 if|if
 condition|(
@@ -287,13 +289,12 @@ name|nl
 decl_stmt|,
 name|com
 decl_stmt|;
-name|_DIAGASSERT
-argument_list|(
-name|fp
-operator|!=
-name|NULL
-argument_list|)
-expr_stmt|;
+if|#
+directive|if
+literal|0
+block|_DIAGASSERT(fp != NULL);
+endif|#
+directive|endif
 name|len
 operator|=
 literal|0
