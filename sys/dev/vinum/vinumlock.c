8f77bbd2ccf7dd02782a798660f7754e990f1245
Greg Lehey <grog@FreeBSD.org>
2000-01-05 22:59:36 +0000

    Don't tsleep with PCATCH, it's a wonderful way to hang the system.
    Reported-by:	Bernd Walter <ticso@cicely.de>
    
    Former-commit-id: 35fe87beb4281e875e98ef9bce33bf7bbef7ce64

--- sys/dev/vinum/vinumlock.c
+++ sys/dev/vinum/vinumlock.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumlock.c,v 1.11 1999/10/12 04:35:37 grog Exp grog $  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -162,8 +162,6 @@ operator|&
 name|lockdrive
 argument_list|,
 name|PRIBIO
-operator||
-name|PCATCH
 argument_list|,
 literal|"vindrv"
 argument_list|,
@@ -318,8 +316,6 @@ operator|->
 name|volno
 argument_list|,
 name|PRIBIO
-operator||
-name|PCATCH
 argument_list|,
 literal|"volock"
 argument_list|,
@@ -459,8 +455,6 @@ literal|0
 index|]
 argument_list|,
 name|PRIBIO
-operator||
-name|PCATCH
 argument_list|,
 literal|"plexlk"
 argument_list|,
@@ -758,8 +752,6 @@ operator|->
 name|stripe
 argument_list|,
 name|PRIBIO
-operator||
-name|PCATCH
 argument_list|,
 literal|"vrlock"
 argument_list|,
@@ -1052,8 +1044,6 @@ operator|&
 name|vinum_conf
 argument_list|,
 name|PRIBIO
-operator||
-name|PCATCH
 argument_list|,
 literal|"vincfg"
 argument_list|,
