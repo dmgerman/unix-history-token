8298529686a36af428410ba15f260fbece869215
Greg Lehey <grog@FreeBSD.org>
2000-01-05 06:03:56 +0000

    Set P_SYSTEM in the daemon proc structure to alleviate delays on shutdown.
    Submitted-by:	Alfred Perlstein <bright@wintelcom.net>
    
    Correct printf format for pointers to avoid compilation warnings on
    alpha.
    
    Submitted-by:	Bernd Walter <ticso@cicely.de>
    
    Identify daemon as 'vinum', not 'vinumd', in messages.  This
    corresponds to the name in ps.
    
    
    Former-commit-id: 8072859aac2dfa7745e2b4157afcfecdd467ff17

--- sys/dev/vinum/vinumdaemon.c
+++ sys/dev/vinum/vinumdaemon.c
@@ -4,7 +4,7 @@ comment|/* daemon.c: kernel part of Vinum daemon */
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumdaemon.c,v 1.7 1999/10/12 09:39:48 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumdaemon.c,v 1.8 2000/01/03 05:22:03 grog Exp grog $  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -150,6 +150,15 @@ name|daemonq
 modifier|*
 name|request
 decl_stmt|;
+name|curproc
+operator|->
+name|p_flag
+operator||=
+name|P_INMEM
+operator||
+name|P_SYSTEM
+expr_stmt|;
+comment|/* we're a system process */
 name|daemon_save_config
 argument_list|()
 expr_stmt|;
@@ -200,7 +209,7 @@ name|log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"vinumd: abdicating\n"
+literal|"vinum: abdicating\n"
 argument_list|)
 expr_stmt|;
 return|return;
@@ -281,11 +290,8 @@ name|log
 argument_list|(
 name|LOG_WARNING
 argument_list|,
-literal|"vinumd: recovering I/O request: %x\n%s dev %d.%d, offset 0x%x, length %ld\n"
+literal|"vinum: recovering I/O request: %p\n%s dev %d.%d, offset 0x%x, length %ld\n"
 argument_list|,
-operator|(
-name|u_int
-operator|)
 name|rq
 argument_list|,
 name|rq
@@ -404,7 +410,7 @@ name|log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"vinumd: saving config\n"
+literal|"vinum: saving config\n"
 argument_list|)
 expr_stmt|;
 name|daemon_save_config
@@ -428,7 +434,7 @@ name|log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"vinumd: stopping\n"
+literal|"vinum: stopping\n"
 argument_list|)
 expr_stmt|;
 name|daemon_options
@@ -490,7 +496,7 @@ name|log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"vinumd: ping reply\n"
+literal|"vinum: ping reply\n"
 argument_list|)
 expr_stmt|;
 name|wakeup
