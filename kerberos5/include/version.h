96558f7926dea3e57feb329270f48f2c9feac761
Mark Murray <markm@FreeBSD.org>
2000-01-15 21:38:08 +0000

    Userland build of Kerberos5 (AKA Heimdal). More to come.
    This is not ready for primetime yet! Please hold off on the bug reports.
    
    
    Former-commit-id: 63859e5022328db4605d4c6c5534577297a58823

---
+++ kerberos5/include/version.h
@@ -0,0 +1,43 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_decl_stmt
+name|char
+modifier|*
+name|krb4_long_version
+init|=
+literal|"@(#)$Version: krb4-1.0 by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 19:34:57 SAST 2000 $"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|krb4_version
+init|=
+literal|"krb4-1.0"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|heimdal_long_version
+init|=
+literal|"@(#)$Version: heimdal-0.1l by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 20:56:02 SAST 2000 $"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|char
+modifier|*
+name|heimdal_version
+init|=
+literal|"heimdal-0.1l"
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
