f5c8e6d572b73c69c8843bfc6547a0e51d99b0b3
Peter Wemm <peter@FreeBSD.org>
2000-01-26 16:55:09 +0000

    Revision 1.3 is now taken care of in the base ncurses source.
    Former-commit-id: 960fb2e3f3df600bf4358109d13c931decce57b7

--- contrib/ncurses/ncurses/tinfo/lib_termcap.c
+++ contrib/ncurses/ncurses/tinfo/lib_termcap.c
@@ -7,6 +7,10 @@ begin_comment
 comment|/****************************************************************************  *  Author: Zeyd M. Ben-Halim<zmbenhal@netcom.com> 1992,1995               *  *     and: Eric S. Raymond<esr@snark.thyrsus.com>                         *  ****************************************************************************/
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -40,7 +44,7 @@ end_include
 begin_macro
 name|MODULE_ID
 argument_list|(
-literal|"$Id: lib_termcap.c,v 1.28 1999/02/27 22:12:58 tom Exp $"
+literal|"$Id: lib_termcap.c,v 1.29 1999/09/05 01:06:43 tom Exp $"
 argument_list|)
 end_macro
 
@@ -495,7 +499,6 @@ name|char
 modifier|*
 modifier|*
 name|area
-name|GCC_UNUSED
 parameter_list|)
 block|{
 name|int
@@ -594,22 +597,31 @@ operator|)
 argument_list|)
 expr_stmt|;
 comment|/* setupterm forces cancelled strings to null */
-ifdef|#
-directive|ifdef
-name|FREEBSD_NATIVE
 if|if
 condition|(
+name|area
+operator|!=
+literal|0
+operator|&&
 operator|*
 name|area
+operator|!=
+literal|0
 operator|&&
+name|VALID_STRING
+argument_list|(
 name|tp
 operator|->
 name|Strings
 index|[
 name|i
 index|]
+argument_list|)
 condition|)
 block|{
+operator|(
+name|void
+operator|)
 name|strcpy
 argument_list|(
 operator|*
@@ -628,19 +640,13 @@ name|area
 operator|+=
 name|strlen
 argument_list|(
-name|tp
-operator|->
-name|Strings
-index|[
-name|i
-index|]
+operator|*
+name|area
 argument_list|)
 operator|+
 literal|1
 expr_stmt|;
 block|}
-endif|#
-directive|endif
 name|returnPtr
 argument_list|(
 name|tp
