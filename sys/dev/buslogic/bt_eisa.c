4d0c3ee38f06af022e1f0914e65063ac7aff2a92
Peter Wemm <peter@FreeBSD.org>
2000-01-29 14:27:26 +0000

    Remove #include "eisa.h" and #if NEISA > 0 - this is guaranteed by config.
    Former-commit-id: 3c1661c41bbfa4310c6ef0708a25986bd8756ccc

--- sys/dev/buslogic/bt_eisa.c
+++ sys/dev/buslogic/bt_eisa.c
@@ -6,20 +6,6 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"eisa.h"
-end_include
-
-begin_if
-if|#
-directive|if
-name|NEISA
-operator|>
-literal|0
-end_if
-
-begin_include
-include|#
-directive|include
 file|<sys/param.h>
 end_include
 
@@ -1345,15 +1331,6 @@ literal|0
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* NEISA> 0 */
-end_comment
 
 end_unit
 
