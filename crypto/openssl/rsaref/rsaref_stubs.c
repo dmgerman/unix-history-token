d2e9a10a0a1adce2038e34cee16b7c1173a62ea0
Peter Wemm <peter@FreeBSD.org>
2000-02-25 08:13:50 +0000

    Don't use the dlopen() stubs if comiling with PIC.  This still needs some more thought for the static case.  Should we provide weak error-generating stubs for static binaries if -lrsaref was forgotten?
    Former-commit-id: d62f0c703f66faab8fae06d1304e6e056a02bc2f

--- crypto/openssl/rsaref/rsaref_stubs.c
+++ crypto/openssl/rsaref/rsaref_stubs.c
@@ -3,11 +3,20 @@ begin_comment
 comment|/*  * $FreeBSD$  *  * Copyright (c) 2000  *	Jordan Hubbard.  All rights reserved.  *  * Stub functions for RSA code.  If you link with this code, you will  * get a full set of weak symbol references to the rsaref library  * functions which are required by openssl.  These can then be occluded  * by the real rsaref library by explicitly linking with it or, failing  * that, these stub functions will also attempt to find an appropriate  * rsaref library in the search path and do the link-up at runtime.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer,  *    verbatim and that no modifications are made prior to this  *    point in the file.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY JORDAN HUBBARD ``AS IS'' AND ANY EXPRESS  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL JORDAN HUBBARD OR HIS PETS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, LIFE OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE. SO THERE.  *  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
 name|NO_RSA
-end_ifndef
+argument_list|)
+operator|&&
+name|defined
+argument_list|(
+name|PIC
+argument_list|)
+end_if
 
 begin_include
 include|#
@@ -771,7 +780,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* NO_RSA */
+comment|/* !NO_RSA&& PIC */
 end_comment
 
 end_unit
