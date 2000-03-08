08428390c8686b06a3fc278988f78db5e8720826
Jonathan Lemon <jlemon@FreeBSD.org>
2000-03-08 16:16:31 +0000

    Add support for older EISA compaq cards and newer Smart 4200 cards. Change disk names to `idad' to avoid naming conflicts with the controller, and enable the new disk code to pick up the drives.
    Tested by:  	david.w.james@bt.com  (existing compaq support)
    Reviewed by:	msmith
    Approved by:	jordan
    
    
    Former-commit-id: ccaf4959ed36033e5a66f3ca71e38597fe92d9bc

--- sys/dev/ida/idareg.h
+++ sys/dev/ida/idareg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999 Jonathan Lemon  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1999,2000 Jonathan Lemon  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -8,7 +8,95 @@ comment|/*  * #defines and software structures for the Compaq RAID card  */
 end_comment
 
 begin_comment
-comment|/*  * board register offsets  */
+comment|/*  * defines for older EISA controllers (IDA, IDA-2, IAES, SMART)  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|R_EISA_INT_MASK
+value|0xC89
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_LOCAL_MASK
+value|0xC8C
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_LOCAL_DOORBELL
+value|0xC8D
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_SYSTEM_MASK
+value|0xC8E
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_SYSTEM_DOORBELL
+value|0xC8F
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_LIST_ADDR
+value|0xC90
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_LIST_LEN
+value|0xC94
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_TAG
+value|0xC97
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_COMPLETE_ADDR
+value|0xC98
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_EISA_LIST_STATUS
+value|0xC9E
+end_define
+
+begin_define
+define|#
+directive|define
+name|EISA_CHANNEL_BUSY
+value|0x01
+end_define
+
+begin_define
+define|#
+directive|define
+name|EISA_CHANNEL_CLEAR
+value|0x02
+end_define
+
+begin_comment
+comment|/*  * board register offsets for SMART-2 controllers  */
 end_comment
 
 begin_define
@@ -47,7 +135,7 @@ value|0x14
 end_define
 
 begin_comment
-comment|/*  * interrupt mask values   */
+comment|/*  * interrupt mask values for SMART series  */
 end_comment
 
 begin_define
@@ -62,6 +150,63 @@ define|#
 directive|define
 name|INT_ENABLE
 value|0x01
+end_define
+
+begin_comment
+comment|/*  * board offsets for the 42xx series  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|R_42XX_STATUS
+value|0x30
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_42XX_INT_MASK
+value|0x34
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_42XX_REQUEST
+value|0x40
+end_define
+
+begin_define
+define|#
+directive|define
+name|R_42XX_REPLY
+value|0x44
+end_define
+
+begin_comment
+comment|/*  * interrupt values for 42xx series  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INT_ENABLE_42XX
+value|0x00
+end_define
+
+begin_define
+define|#
+directive|define
+name|INT_DISABLE_42XX
+value|0x08
+end_define
+
+begin_define
+define|#
+directive|define
+name|STATUS_42XX_INT_PENDING
+value|0x08
 end_define
 
 begin_comment
