32b1b725736fb90ecad3c0c9f77fe5214c8d19ff
Matthew Dillon <dillon@FreeBSD.org>
2000-03-09 18:57:17 +0000

    MFC 1.33 - move brelse() so last access of data occurs prior to buffer     release rather then after the buffer is released.
    Former-commit-id: bc083e8b4436392860257dad492a771c3f7c120a

--- sys/ufs/ufs/ufs_lookup.c
+++ sys/ufs/ufs/ufs_lookup.c
@@ -1001,11 +1001,6 @@ name|ep
 operator|->
 name|d_reclen
 expr_stmt|;
-name|brelse
-argument_list|(
-name|bp
-argument_list|)
-expr_stmt|;
 goto|goto
 name|found
 goto|;
@@ -1417,6 +1412,11 @@ operator||
 name|IN_UPDATE
 expr_stmt|;
 block|}
+name|brelse
+argument_list|(
+name|bp
+argument_list|)
+expr_stmt|;
 comment|/* 	 * Found component in pathname. 	 * If the final component of path name, save information 	 * in the cache as to where the entry was found. 	 */
 if|if
 condition|(
