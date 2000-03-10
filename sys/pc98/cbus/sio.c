8b3f6691e83c6754c264630e8043fb6b6299a9c4
Takenori KATO <kato@FreeBSD.org>
2000-03-10 10:34:36 +0000

    Merged from sys/isa/sio.c revisions 1.289 and 1.290.
    Former-commit-id: 15b5c2742e3972710e9475d9aa8b540a7f86e642

--- sys/pc98/cbus/sio.c
+++ sys/pc98/cbus/sio.c
@@ -5265,6 +5265,13 @@ block|}
 block|,
 comment|/* OZO8039 - Zoom 56k flex */
 block|{
+literal|0x3024a341
+block|,
+name|NULL
+block|}
+block|,
+comment|/* PMC2430 - Pace 56 Voice Internal Modem */
+block|{
 literal|0x1000eb49
 block|,
 name|NULL
@@ -8823,13 +8830,6 @@ block|{
 name|enable_intr
 argument_list|()
 expr_stmt|;
-name|free
-argument_list|(
-name|com
-argument_list|,
-name|M_DEVBUF
-argument_list|)
-expr_stmt|;
 comment|/* 		 * Leave i/o resources allocated if this is a `cn'-level 		 * console, so that other devices can't snarf them. 		 */
 if|if
 condition|(
@@ -11618,13 +11618,6 @@ argument_list|,
 sizeof|sizeof
 expr|*
 name|tp
-argument_list|)
-expr_stmt|;
-name|free
-argument_list|(
-name|com
-argument_list|,
-name|M_DEVBUF
 argument_list|)
 expr_stmt|;
 name|splx
