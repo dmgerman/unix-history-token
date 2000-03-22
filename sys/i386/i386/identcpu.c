4f02c8e24b2565c3a6b34297bd64d719036227f1
Takahashi Yoshihiro <nyan@FreeBSD.org>
2000-03-22 05:27:22 +0000

    MFC: Simplify messages of Pentium II, Pentium II Xeon, Celeron, Pentium III      and Pentium III Xeon CPUs.
    Former-commit-id: 2d1ace8d7fd3e92be82cdca7b725c7a4091569d1

--- sys/i386/i386/identcpu.c
+++ sys/i386/i386/identcpu.c
@@ -812,33 +812,9 @@ break|break;
 case|case
 literal|0x30
 case|:
-name|strcat
-argument_list|(
-name|cpu_model
-argument_list|,
-literal|"Pentium II"
-argument_list|)
-expr_stmt|;
-name|cpu
-operator|=
-name|CPU_PII
-expr_stmt|;
-break|break;
 case|case
 literal|0x50
 case|:
-name|strcat
-argument_list|(
-name|cpu_model
-argument_list|,
-literal|"Pentium II/Xeon/Celeron"
-argument_list|)
-expr_stmt|;
-name|cpu
-operator|=
-name|CPU_PII
-expr_stmt|;
-break|break;
 case|case
 literal|0x60
 case|:
@@ -846,7 +822,7 @@ name|strcat
 argument_list|(
 name|cpu_model
 argument_list|,
-literal|"Celeron"
+literal|"Pentium II/Pentium II Xeon/Celeron"
 argument_list|)
 expr_stmt|;
 name|cpu
@@ -857,18 +833,6 @@ break|break;
 case|case
 literal|0x70
 case|:
-name|strcat
-argument_list|(
-name|cpu_model
-argument_list|,
-literal|"Pentium III"
-argument_list|)
-expr_stmt|;
-name|cpu
-operator|=
-name|CPU_PIII
-expr_stmt|;
-break|break;
 case|case
 literal|0x80
 case|:
@@ -876,7 +840,7 @@ name|strcat
 argument_list|(
 name|cpu_model
 argument_list|,
-literal|"Pentium III"
+literal|"Pentium III/Pentium III Xeon"
 argument_list|)
 expr_stmt|;
 name|cpu
