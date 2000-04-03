5a57503e7a647382113f51958ecb7ab89ebb9dfe
Brian Somers <brian@FreeBSD.org>
2000-04-03 20:02:28 +0000

    MFC: Plug two file descriptor leaks
    Former-commit-id: 1d82d06c9feccbacf0692b5f8eaa333ccecce0cc

--- usr.sbin/ppp/systems.c
+++ usr.sbin/ppp/systems.c
@@ -1489,10 +1489,17 @@ condition|(
 operator|!
 name|n
 condition|)
+block|{
+name|fclose
+argument_list|(
+name|fp
+argument_list|)
+expr_stmt|;
 return|return
 literal|0
 return|;
 comment|/* got it */
+block|}
 break|break;
 default|default:
 name|log_Printf
@@ -1578,9 +1585,16 @@ name|how
 operator|==
 name|SYSTEM_EXISTS
 condition|)
+block|{
+name|fclose
+argument_list|(
+name|fp
+argument_list|)
+expr_stmt|;
 return|return
 literal|0
 return|;
+block|}
 while|while
 condition|(
 operator|(
