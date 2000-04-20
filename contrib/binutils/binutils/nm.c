8500387a3304beec763aad89c113b2d4fd115de9
David E. O'Brien <obrien@FreeBSD.org>
2000-04-20 08:58:04 +0000

    Revert this file back to its FSF/Cygnus virgin state. With jb's rev 1.2 commit to usr.bin/lorder/lorder.sh we don't depend on the 4.4BSD's ``nm -o'' behavior.
    Previous to 4.3BSD-Reno, ``nm -o'' did not output the .o's filename on a
    line by itself.  The change occurred between CSRG's nm.c rev 4.8 (1987) and
    rev 5.1 (in 1989), which was "new version from Hans Huebner hans@garp.mit.edu,
    huebner@db0tui6.BITNET".  The Binutils maintainers would rather cater to
    a loud boisterous user of 4.3BSD VAXen which has its own native toolchain,
    rather than a modern Unix with multiple orders of magnitude more users
    and in which Binutils *is* the native toolchain.
    
    
    Former-commit-id: 6135a842ac9e1af8c80ac2dcc05cd8d2b4a1c31a

--- contrib/binutils/binutils/nm.c
+++ contrib/binutils/binutils/nm.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* nm.c -- Describe symbol table of a rel file.    Copyright 1991, 92, 93, 94, 95, 96, 97, 1998 Free Software Foundation, Inc.     This file is part of GNU Binutils.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2 of the License, or    (at your option) any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -5565,6 +5569,9 @@ block|{
 if|if
 condition|(
 name|filename_per_file
+operator|&&
+operator|!
+name|filename_per_symbol
 condition|)
 name|printf
 argument_list|(
