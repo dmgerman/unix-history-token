e949b2ce5b3816f3a5ea397a59432fcdec61fddb
Kazutaka YOKOTA <yokota@FreeBSD.org>
2000-04-21 14:21:39 +0000

    MFC: Reduce the default timeout for the three button emulation from 200 msec to 100 msec.
    Former-commit-id: 5cbfd92cabc57cb0d389298a0ea9ccd5ecfeae06

--- usr.sbin/moused/moused.c
+++ usr.sbin/moused/moused.c
@@ -196,11 +196,11 @@ begin_define
 define|#
 directive|define
 name|DFLT_BUTTON2TIMEOUT
-value|200
+value|100
 end_define
 
 begin_comment
-comment|/* 0.2 second */
+comment|/* 0.1 second */
 end_comment
 
 begin_define
