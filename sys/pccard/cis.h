b650cad716dc1c6b31668a86e8ac816972a3bc9e
Mitsuru IWASAKI <iwasaki@FreeBSD.org>
2000-04-26 15:36:43 +0000

    MFPAO3.  Improvement of of pccard cis tuple parsing capability.  - Fixed bogus CIS tuple dumping (Network node ID, IRQ modes and etc.)  - Include telling drivers ethernet address if Network node ID    tuple is available.  This is usefull for some bogus ehter cards which    can't get correct ethernet address from CIS tupple.
    Obtained from:	PAO3
    
    
    Former-commit-id: f805a51f5df79f64982995038bd710739e77d057

--- sys/pccard/cis.h
+++ sys/pccard/cis.h
@@ -1,11 +1,23 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *	PCMCIA card structures and defines.  *	These defines relate to the user level  *	structures and card information, not  *	driver/process communication.  *-------------------------------------------------------------------------  *  * Copyright (c) 1995 Andrew McRae.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
+comment|/*  *	PCMCIA card structures and defines.  *	These defines relate to the user level  *	structures and card information, not  *	driver/process communication.  *-------------------------------------------------------------------------  *  * Copyright (c) 1995 Andrew McRae.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  *  */
 end_comment
 
 begin_comment
 comment|/*  *	Card Information Structure tuples definitions  *	The structure of a tuple is basically:  *  *		Tuple_code  *		Tuple_data_length  *		Tuple_data ...  *  *	Tuples are contiguous in attribute memory, and  *	are terminated with a 0xFF for the tuple code or  *	the tuple length.  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_PCCARD_CIS_H
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_PCCARD_CIS_H
+end_define
 
 begin_define
 define|#
@@ -27,6 +39,72 @@ end_define
 
 begin_comment
 comment|/* Device descriptor, common memory */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_LONGLINK_CB
+value|0x02
+end_define
+
+begin_comment
+comment|/* Long link to next chain for CardBus */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_INDIRECT
+value|0x03
+end_define
+
+begin_comment
+comment|/* Indirect access */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_CONF_MAP_CB
+value|0x04
+end_define
+
+begin_comment
+comment|/* Card Configuration map for CardBus */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_CONFIG_CB
+value|0x05
+end_define
+
+begin_comment
+comment|/* Card Configuration entry for CardBus */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_LONGLINK_MFC
+value|0x06
+end_define
+
+begin_comment
+comment|/* Long link to next chain for Multi function card */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_BAR
+value|0x07
+end_define
+
+begin_comment
+comment|/* Base address register for CardBus */
 end_comment
 
 begin_define
@@ -644,6 +722,39 @@ directive|define
 name|CIS_MEM_HOST
 value|0x80
 end_define
+
+begin_comment
+comment|/*  *	Misc sub-tuple.  *	Byte 1:  *	Byte 2:  *		0x0c - DMA Request Signal  *                      00 - not support DMA  *                      01 - use SPKR# line  *                      10 - use IOIS16# line  *                      11 - use INPACK# line  *		0x10 - DMA Width  *                      0 - 8 bit DMA  *                      1 - 16 bit DMA  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CIS_MISC_DMA_WIDTH
+parameter_list|(
+name|x
+parameter_list|)
+value|(((x)& 0x10)>> 4)
+end_define
+
+begin_define
+define|#
+directive|define
+name|CIS_MISC_DMA_REQ
+parameter_list|(
+name|x
+parameter_list|)
+value|(((x)>> 2)& 0x3)
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _PCCARD_CIS_H */
+end_comment
 
 end_unit
 
