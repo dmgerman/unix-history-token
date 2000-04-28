6fe2dc410b2fd4ced07ed3aca3a6144138c1a581
Julian Elischer <julian@FreeBSD.org>
2000-04-28 17:43:04 +0000

    OOps forgot to check in this one... API chage for netgraph.
    Former-commit-id: ce2f774dc118acf7a47b80a4431f004a6ce3805c

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -4150,6 +4150,9 @@ name|ng_mesg
 modifier|*
 modifier|*
 name|resp
+parameter_list|,
+name|hook_p
+name|lasthook
 parameter_list|)
 block|{
 name|struct
@@ -4451,6 +4454,16 @@ name|m
 parameter_list|,
 name|meta_p
 name|meta
+parameter_list|,
+name|struct
+name|mbuf
+modifier|*
+modifier|*
+name|ret_m
+parameter_list|,
+name|meta_p
+modifier|*
+name|ret_meta
 parameter_list|)
 block|{
 name|struct
