dddb90fe2862d21293f5fd0d2a4af5d05a621dea
Takahashi Yoshihiro <nyan@FreeBSD.org>
2000-04-29 05:55:33 +0000

    Fixed typo.
    Former-commit-id: c6cb1180be2fb7800b589b3fda4be9e19d5bcd4e

--- sys/pc98/pc98/wormio.h
+++ sys/pc98/pc98/wormio.h
@@ -4,7 +4,29 @@ comment|/* Shared between kernel& process */
 end_comment
 
 begin_comment
-comment|/* $FreeBSD$  #ifndef	_SYS_WORMIO_H_ #define	_SYS_WORMIO_H_  #include<sys/ioccom.h>  /***************************************************************\ * Ioctls for the WORM drive					* \***************************************************************/
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_SYS_WORMIO_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_SYS_WORMIO_H_
+end_define
+
+begin_include
+include|#
+directive|include
+file|<sys/ioccom.h>
+end_include
+
+begin_comment
+comment|/***************************************************************\ * Ioctls for the WORM drive					* \***************************************************************/
 end_comment
 
 begin_comment
