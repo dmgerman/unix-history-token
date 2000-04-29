15179d5edf7597cc5961c9c14e6a550a04e3d8ea
Peter Wemm <peter@FreeBSD.org>
2000-04-29 15:25:56 +0000

    Add a missing MODULE_DEPEND() on miibus..  I was working from KMODDEPS which this driver didn't have.
    Former-commit-id: be9ccc54729bbaeb07d490ae062a422a68f6e11e

--- sys/pci/if_sk.c
+++ sys/pci/if_sk.c
@@ -209,6 +209,22 @@ directive|include
 file|<pci/xmaciireg.h>
 end_include
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|sk
+argument_list|,
+name|miibus
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_comment
 comment|/* "controller miibus0" required.  See GENERIC if you get errors here. */
 end_comment
