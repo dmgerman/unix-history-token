28ec3a186589b5abc25304a5ddb517ac692ef761
Peter Wemm <peter@FreeBSD.org>
2000-05-01 19:54:26 +0000

    Add missing $FreeBSD$
    Former-commit-id: 55f39ce9ff550d9c09c60d2b5eef5540e1cc8870

--- sys/dev/dec/mc146818reg.h
+++ sys/dev/dec/mc146818reg.h
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/*	$NetBSD: mc146818reg.h,v 1.2 1997/03/12 06:53:42 cgd Exp $	*/
 end_comment
 
--- sys/dev/dec/mcclockvar.h
+++ sys/dev/dec/mcclockvar.h
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/* $NetBSD: mcclockvar.h,v 1.4 1997/06/22 08:02:19 jonathan Exp $ */
 end_comment
 
