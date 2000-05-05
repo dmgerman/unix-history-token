cd3a4be933e8a888137b62ab0cbcef7525d2fd2f
Archie Cobbs <archie@FreeBSD.org>
2000-05-05 02:48:39 +0000

    MFC: fix broken multi-link fragment reassembly algorithm.
    Former-commit-id: 61bbf04162ba79cc44e76c17f89445263eef8a31

--- sys/netgraph/ng_ppp.h
+++ sys/netgraph/ng_ppp.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ng_ppp.h  *  * Copyright (c) 1996-1999 Whistle Communications, Inc.  * All rights reserved.  *   * Subject to the following obligations and disclaimer of warranty, use and  * redistribution of this software, in source or object code forms, with or  * without modifications are expressly permitted by Whistle Communications;  * provided, however, that:  * 1. Any and all reproductions of the source or object code must include the  *    copyright notice above and the following disclaimer of warranties; and  * 2. No rights are granted, in any manner or form, to use Whistle  *    Communications, Inc. trademarks, including the mark "WHISTLE  *    COMMUNICATIONS" on advertising, endorsements, or otherwise except as  *    such appears in the above copyright notice or in the software.  *   * THIS SOFTWARE IS BEING PROVIDED BY WHISTLE COMMUNICATIONS "AS IS", AND  * TO THE MAXIMUM EXTENT PERMITTED BY LAW, WHISTLE COMMUNICATIONS MAKES NO  * REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, REGARDING THIS SOFTWARE,  * INCLUDING WITHOUT LIMITATION, ANY AND ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  * WHISTLE COMMUNICATIONS DOES NOT WARRANT, GUARANTEE, OR MAKE ANY  * REPRESENTATIONS REGARDING THE USE OF, OR THE RESULTS OF THE USE OF THIS  * SOFTWARE IN TERMS OF ITS CORRECTNESS, ACCURACY, RELIABILITY OR OTHERWISE.  * IN NO EVENT SHALL WHISTLE COMMUNICATIONS BE LIABLE FOR ANY DAMAGES  * RESULTING FROM OR ARISING OUT OF ANY USE OF THIS SOFTWARE, INCLUDING  * WITHOUT LIMITATION, ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  * PUNITIVE, OR CONSEQUENTIAL DAMAGES, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES, LOSS OF USE, DATA OR PROFITS, HOWEVER CAUSED AND UNDER ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF WHISTLE COMMUNICATIONS IS ADVISED OF THE POSSIBILITY  * OF SUCH DAMAGE.  *  * Author: Archie Cobbs<archie@whistle.com>  *  * $FreeBSD$  * $Whistle: ng_ppp.h,v 1.8 1999/01/25 02:40:02 archie Exp $  */
+comment|/*  * ng_ppp.h  *  * Copyright (c) 1996-2000 Whistle Communications, Inc.  * All rights reserved.  *   * Subject to the following obligations and disclaimer of warranty, use and  * redistribution of this software, in source or object code forms, with or  * without modifications are expressly permitted by Whistle Communications;  * provided, however, that:  * 1. Any and all reproductions of the source or object code must include the  *    copyright notice above and the following disclaimer of warranties; and  * 2. No rights are granted, in any manner or form, to use Whistle  *    Communications, Inc. trademarks, including the mark "WHISTLE  *    COMMUNICATIONS" on advertising, endorsements, or otherwise except as  *    such appears in the above copyright notice or in the software.  *   * THIS SOFTWARE IS BEING PROVIDED BY WHISTLE COMMUNICATIONS "AS IS", AND  * TO THE MAXIMUM EXTENT PERMITTED BY LAW, WHISTLE COMMUNICATIONS MAKES NO  * REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, REGARDING THIS SOFTWARE,  * INCLUDING WITHOUT LIMITATION, ANY AND ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  * WHISTLE COMMUNICATIONS DOES NOT WARRANT, GUARANTEE, OR MAKE ANY  * REPRESENTATIONS REGARDING THE USE OF, OR THE RESULTS OF THE USE OF THIS  * SOFTWARE IN TERMS OF ITS CORRECTNESS, ACCURACY, RELIABILITY OR OTHERWISE.  * IN NO EVENT SHALL WHISTLE COMMUNICATIONS BE LIABLE FOR ANY DAMAGES  * RESULTING FROM OR ARISING OUT OF ANY USE OF THIS SOFTWARE, INCLUDING  * WITHOUT LIMITATION, ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  * PUNITIVE, OR CONSEQUENTIAL DAMAGES, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES, LOSS OF USE, DATA OR PROFITS, HOWEVER CAUSED AND UNDER ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF WHISTLE COMMUNICATIONS IS ADVISED OF THE POSSIBILITY  * OF SUCH DAMAGE.  *  * Author: Archie Cobbs<archie@whistle.com>  *  * $FreeBSD$  * $Whistle: ng_ppp.h,v 1.8 1999/01/25 02:40:02 archie Exp $  */
 end_comment
 
 begin_ifndef
@@ -30,7 +30,7 @@ begin_define
 define|#
 directive|define
 name|NGM_PPP_COOKIE
-value|940897793
+value|940897794
 end_define
 
 begin_comment
@@ -220,6 +220,17 @@ end_comment
 begin_define
 define|#
 directive|define
+name|NG_PPP_HOOK_IPV6
+value|"ipv6"
+end_define
+
+begin_comment
+comment|/* IPv6 packet data */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|NG_PPP_HOOK_LINK_PREFIX
 value|"link"
 end_define
@@ -239,10 +250,10 @@ name|NGM_PPP_SET_CONFIG
 init|=
 literal|1
 block|,
-comment|/* takes struct ng_ppp_bundle_config */
+comment|/* takes struct ng_ppp_node_conf */
 name|NGM_PPP_GET_CONFIG
 block|,
-comment|/* returns ng_ppp_bundle_config */
+comment|/* returns ng_ppp_node_conf */
 name|NGM_PPP_GET_LINK_STATS
 block|,
 comment|/* takes link #, returns stats struct */
@@ -262,7 +273,7 @@ end_comment
 
 begin_struct
 struct|struct
-name|ng_ppp_link_config
+name|ng_ppp_link_conf
 block|{
 name|u_char
 name|enableLink
@@ -300,16 +311,16 @@ begin_define
 define|#
 directive|define
 name|NG_PPP_LINK_TYPE_INFO
-value|{				\ 	{							\ 	  { "enable",&ng_parse_int8_type	},	\ 	  { "protocomp",&ng_parse_int8_type	},	\ 	  { "acfcomp",&ng_parse_int8_type	},	\ 	  { "mru",&ng_parse_int16_type	},	\ 	  { "latency",&ng_parse_int32_type	},	\ 	  { "bandwidth",&ng_parse_int32_type	},	\ 	  { NULL },						\ 	}							\ }
+value|{				\ 	{							\ 	  { "enableLink",&ng_parse_int8_type	},	\ 	  { "enableProtoComp",&ng_parse_int8_type	},	\ 	  { "enableACFComp",&ng_parse_int8_type	},	\ 	  { "mru",&ng_parse_int16_type	},	\ 	  { "latency",&ng_parse_int32_type	},	\ 	  { "bandwidth",&ng_parse_int32_type	},	\ 	  { NULL },						\ 	}							\ }
 end_define
 
 begin_comment
-comment|/* Node config structure */
+comment|/* Bundle config structure */
 end_comment
 
 begin_struct
 struct|struct
-name|ng_ppp_node_config
+name|ng_ppp_bund_conf
 block|{
 name|u_int16_t
 name|mrru
@@ -335,6 +346,10 @@ name|u_char
 name|enableIP
 decl_stmt|;
 comment|/* enable IP data flow */
+name|u_char
+name|enableIPv6
+decl_stmt|;
+comment|/* enable IPv6 data flow */
 name|u_char
 name|enableAtalk
 decl_stmt|;
@@ -367,9 +382,35 @@ name|u_char
 name|enableVJDecompression
 decl_stmt|;
 comment|/* enable VJ decompression */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/* Keep this in sync with the above structure definition */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NG_PPP_BUND_TYPE_INFO
+value|{					\ 	{								\ 	  { "mrru",&ng_parse_int16_type	},	\ 	  { "enableMultilink",&ng_parse_int8_type	},	\ 	  { "recvShortSeq",&ng_parse_int8_type	},	\ 	  { "xmitShortSeq",&ng_parse_int8_type	},	\ 	  { "enableRoundRobin",&ng_parse_int8_type	},	\ 	  { "enableIP",&ng_parse_int8_type	},	\ 	  { "enableIPv6",&ng_parse_int8_type	},	\ 	  { "enableAtalk",&ng_parse_int8_type	},	\ 	  { "enableIPX",&ng_parse_int8_type	},	\ 	  { "enableCompression",&ng_parse_int8_type	},	\ 	  { "enableDecompression",&ng_parse_int8_type	},	\ 	  { "enableEncryption",&ng_parse_int8_type	},	\ 	  { "enableDecryption",&ng_parse_int8_type	},	\ 	  { "enableVJCompression",&ng_parse_int8_type	},	\ 	  { "enableVJDecompression",&ng_parse_int8_type	},	\ 	  { NULL }							\ 	}								\ }
+end_define
+
+begin_comment
+comment|/* Total node config structure */
+end_comment
+
+begin_struct
+struct|struct
+name|ng_ppp_node_conf
+block|{
 name|struct
-name|ng_ppp_link_config
-comment|/* per link config params */
+name|ng_ppp_bund_conf
+name|bund
+decl_stmt|;
+name|struct
+name|ng_ppp_link_conf
 name|links
 index|[
 name|NG_PPP_MAX_LINKS
@@ -388,9 +429,11 @@ define|#
 directive|define
 name|NG_PPP_CONFIG_TYPE_INFO
 parameter_list|(
+name|bctype
+parameter_list|,
 name|arytype
 parameter_list|)
-value|{		\ 	{							\ 	  { "mrru",&ng_parse_int16_type	},	\ 	  { "multilink",&ng_parse_int8_type	},	\ 	  { "recvShortSeq",&ng_parse_int8_type	},	\ 	  { "xmitShortSeq",&ng_parse_int8_type	},	\ 	  { "roundRobin",&ng_parse_int8_type	},	\ 	  { "ip",&ng_parse_int8_type	},	\ 	  { "appletalk",&ng_parse_int8_type	},	\ 	  { "ipx",&ng_parse_int8_type	},	\ 	  { "comp",&ng_parse_int8_type	},	\ 	  { "decomp",&ng_parse_int8_type	},	\ 	  { "encryption",&ng_parse_int8_type	},	\ 	  { "decryption",&ng_parse_int8_type	},	\ 	  { "vjcomp",&ng_parse_int8_type	},	\ 	  { "vjdecomp",&ng_parse_int8_type	},	\ 	  { "links",		(arytype)		},	\ 	  { NULL },						\ 	}							\ }
+value|{	\ 	{							\ 	  { "bund",		(bctype)	},		\ 	  { "links",		(arytype)	},		\ 	  { NULL }						\ 	}							\ }
 end_define
 
 begin_comment
@@ -422,9 +465,17 @@ name|badProtos
 decl_stmt|;
 comment|/* frames rec'd with bogus protocol */
 name|u_int32_t
+name|runts
+decl_stmt|;
+comment|/* Too short MP fragments */
+name|u_int32_t
 name|dupFragments
 decl_stmt|;
 comment|/* MP frames with duplicate seq # */
+name|u_int32_t
+name|dropFragments
+decl_stmt|;
+comment|/* MP fragments we had to drop */
 block|}
 struct|;
 end_struct
@@ -437,7 +488,7 @@ begin_define
 define|#
 directive|define
 name|NG_PPP_STATS_TYPE_INFO
-value|{				\ 	{							\ 	  { "xmitFrames",&ng_parse_int32_type	},	\ 	  { "xmitOctets",&ng_parse_int32_type	},	\ 	  { "recvFrames",&ng_parse_int32_type	},	\ 	  { "recvOctets",&ng_parse_int32_type	},	\ 	  { "badProtos",&ng_parse_int32_type	},	\ 	  { "dupFragments",&ng_parse_int32_type	},	\ 	  { NULL },						\ 	}							\ }
+value|{				\ 	{							\ 	  { "xmitFrames",&ng_parse_int32_type	},	\ 	  { "xmitOctets",&ng_parse_int32_type	},	\ 	  { "recvFrames",&ng_parse_int32_type	},	\ 	  { "recvOctets",&ng_parse_int32_type	},	\ 	  { "badProtos",&ng_parse_int32_type	},	\ 	  { "runts",&ng_parse_int32_type	},	\ 	  { "dupFragments",&ng_parse_int32_type	},	\ 	  { "dropFragments",&ng_parse_int32_type	},	\ 	  { NULL }						\ 	}							\ }
 end_define
 
 begin_endif
