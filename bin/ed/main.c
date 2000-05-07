e73c8702b245873bb4c6d50081b2ea200a0fd576
Josef Karthauser <joe@FreeBSD.org>
2000-05-07 18:06:55 +0000

    MFC: Fix a potential buffer overflow in the command line argument parsing 	with 'ed [MAXPATHLEN + 1 chars]'. {from revision 1.15}
    Former-commit-id: 7b9e04c606ac0b3e0b2e37ba9e2ad123b6cdb128

--- bin/ed/main.c
+++ bin/ed/main.c
@@ -776,12 +776,29 @@ name|argv
 operator|!=
 literal|'!'
 condition|)
-name|strcpy
+if|if
+condition|(
+name|strlcpy
 argument_list|(
 name|old_filename
 argument_list|,
 operator|*
 name|argv
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|old_filename
+argument_list|)
+argument_list|)
+operator|>=
+sizeof|sizeof
+argument_list|(
+name|old_filename
+argument_list|)
+condition|)
+name|quit
+argument_list|(
+literal|2
 argument_list|)
 expr_stmt|;
 block|}
@@ -6513,9 +6530,15 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
-comment|/* assert: no trailing escape */
 while|while
 condition|(
+name|i
+operator|<
+name|filesz
+operator|-
+literal|1
+comment|/* Worry about a possible trailing escape */
+operator|&&
 operator|(
 name|file
 index|[
