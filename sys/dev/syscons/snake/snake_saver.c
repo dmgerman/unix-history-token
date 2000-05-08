b03ac9520fee8c0b1f74005b91bb8eb58f4cb415
Takahashi Yoshihiro <nyan@FreeBSD.org>
2000-05-08 14:55:21 +0000

    Added PC-98 supports.
    Obtained from:	FreeBSD(98) 4.0R-Rev01
    
    
    Former-commit-id: b76b6b280e7a9b555de86ef2fad708f3b0479766

--- sys/dev/syscons/snake/snake_saver.c
+++ sys/dev/syscons/snake/snake_saver.c
@@ -75,6 +75,23 @@ directive|include
 file|<dev/syscons/syscons.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|PC98
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<pc98/pc98/pc98_machdep.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
 name|char
