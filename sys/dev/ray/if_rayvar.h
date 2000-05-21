c3229bbf3e97081958019cb1c2a7ed500fd279ed
Duncan Barclay <dmlb@FreeBSD.org>
2000-05-21 20:43:24 +0000

    MFRELENG_3
    Former-commit-id: 271b46baf03a5401e2ee7594d1c5b4cfa915ab9a

--- sys/dev/ray/if_rayvar.h
+++ sys/dev/ray/if_rayvar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2000  * Dr. Duncan McLennan Barclay, dmlb@ragnet.demon.co.uk.  *  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY DUNCAN BARCLAY AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL DUNCAN BARCLAY OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: if_ray.c,v 1.24 2000/04/24 15:49:20 dmlb Exp $  *  */
+comment|/*  * Copyright (C) 2000  * Dr. Duncan McLennan Barclay, dmlb@ragnet.demon.co.uk.  *  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the author nor the names of any co-contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY DUNCAN BARCLAY AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL DUNCAN BARCLAY OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: if_rayvar.h,v 1.3 2000/05/11 18:53:50 dmlb Exp $  *  */
 end_comment
 
 begin_comment
@@ -186,9 +186,9 @@ name|gone
 decl_stmt|;
 comment|/* 1 = Card bailed out		*/
 name|int
-name|translation
+name|framing
 decl_stmt|;
-comment|/* Packet translation types	*/
+comment|/* Packet framing types		*/
 name|struct
 name|ray_ecf_startup_v5
 name|sc_ecf_startup
@@ -379,59 +379,8 @@ block|}
 struct|;
 end_struct
 
-begin_define
-define|#
-directive|define
-name|RAY_COM_FWOK
-value|0x0001
-end_define
-
 begin_comment
-comment|/* Wakeup on completion	*/
-end_comment
-
-begin_define
-define|#
-directive|define
-name|RAY_COM_FRUNNING
-value|0x0002
-end_define
-
-begin_comment
-comment|/* This one running	*/
-end_comment
-
-begin_define
-define|#
-directive|define
-name|RAY_COM_FCOMPLETED
-value|0x0004
-end_define
-
-begin_comment
-comment|/* This one completed	*/
-end_comment
-
-begin_define
-define|#
-directive|define
-name|RAY_COM_FLAGS_PRINTFB
-define|\
-value|"\020"			\ 	"\001WOK"		\ 	"\002RUNNING"		\ 	"\003COMPLETED"
-end_define
-
-begin_define
-define|#
-directive|define
-name|RAY_COM_NEEDS_TIMO
-parameter_list|(
-name|cmd
-parameter_list|)
-value|(		\ 	 (cmd == RAY_CMD_DOWNLOAD_PARAMS) ||	\ 	 (cmd == RAY_CMD_UPDATE_PARAMS) ||	\ 	 (cmd == RAY_CMD_UPDATE_MCAST)		\ 	)
-end_define
-
-begin_comment
-comment|/*  * Translation types  */
+comment|/*  * Framing types  */
 end_comment
 
 begin_comment
@@ -441,7 +390,7 @@ end_comment
 begin_define
 define|#
 directive|define
-name|SC_TRANSLATE_WEBGEAR
+name|SC_FRAMING_WEBGEAR
 value|0
 end_define
 
@@ -749,6 +698,68 @@ define|\
 value|SRAM_WRITE_REGION((sc), (off) + offsetof(struct s, f), (p), (n))
 end_define
 
+begin_define
+define|#
+directive|define
+name|RAY_COM_FWOK
+value|0x0001
+end_define
+
+begin_comment
+comment|/* Wakeup on completion	*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RAY_COM_FRUNNING
+value|0x0002
+end_define
+
+begin_comment
+comment|/* This one running	*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RAY_COM_FCOMPLETED
+value|0x0004
+end_define
+
+begin_comment
+comment|/* This one completed	*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RAY_COM_FWAIT
+value|0x0008
+end_define
+
+begin_comment
+comment|/* Do not run the queue */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RAY_COM_FLAGS_PRINTFB
+define|\
+value|"\020"			\ 	"\001WOK"		\ 	"\002RUNNING"		\ 	"\003COMPLETED"		\ 	"\004WAIT"
+end_define
+
+begin_define
+define|#
+directive|define
+name|RAY_COM_NEEDS_TIMO
+parameter_list|(
+name|cmd
+parameter_list|)
+value|(		\ 	 (cmd == RAY_CMD_DOWNLOAD_PARAMS) ||	\ 	 (cmd == RAY_CMD_UPDATE_PARAMS) ||	\ 	 (cmd == RAY_CMD_UPDATE_MCAST)		\ 	)
+end_define
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -980,6 +991,8 @@ directive|define
 name|RAY_MBUF_DUMP
 parameter_list|(
 name|sc
+parameter_list|,
+name|mask
 parameter_list|,
 name|m
 parameter_list|,
