117cba66959d6a6f48d39192c69070bd6d9cef56
Kris Kennaway <kris@FreeBSD.org>
2000-06-03 09:58:15 +0000

    Resolve conflicts
    Former-commit-id: df76350183d42dd115c022893ce75c6d7ce66ce5

--- crypto/openssh/servconf.c
+++ crypto/openssh/servconf.c
@@ -12,7 +12,7 @@ end_include
 begin_expr_stmt
 name|RCSID
 argument_list|(
-literal|"$Id: servconf.c,v 1.40 2000/05/08 17:12:15 markus Exp $"
+literal|"$Id: servconf.c,v 1.41 2000/05/22 18:42:01 markus Exp $"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -3199,6 +3199,20 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|cp
+condition|)
+name|fatal
+argument_list|(
+literal|"%s line %d: Missing argument."
+argument_list|,
+name|filename
+argument_list|,
+name|linenum
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
 name|ciphers_valid
 argument_list|(
 name|cp
@@ -3254,6 +3268,20 @@ argument_list|(
 name|NULL
 argument_list|,
 name|WHITESPACE
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|cp
+condition|)
+name|fatal
+argument_list|(
+literal|"%s line %d: Missing argument."
+argument_list|,
+name|filename
+argument_list|,
+name|linenum
 argument_list|)
 expr_stmt|;
 name|value
