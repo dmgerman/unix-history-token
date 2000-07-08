682f5b6916e1c299ee2566a1c210ccbc3263392a
John Polstra <jdp@FreeBSD.org>
2000-07-08 04:10:38 +0000

    Solve the dynamic linker's problems with multithreaded programs once and for all (I hope).  Packages such as wine, JDK, and linuxthreads should no longer have any problems with re-entering the dynamic linker.
    This commit replaces the locking used in the dynamic linker with a
    new spinlock-based reader/writer lock implementation.  Brian
    Fundakowski Feldman <green> argued for this from the very beginning,
    but it took me a long time to come around to his point of view.
    Spinlocks are the only kinds of locks that work with all thread
    packages.  But on uniprocessor systems they can be inefficient,
    because while a contender for the lock is spinning the holder of the
    lock cannot make any progress toward releasing it.  To alleviate
    this disadvantage I have borrowed a trick from Sleepycat's Berkeley
    DB implementation.  When spinning for a lock, the requester does a
    nanosleep() call for 1 usec. each time around the loop.  This will
    generally yield the CPU to other threads, allowing the lock holder
    to finish its business and release the lock.  I chose 1 usec. as the
    minimum sleep which would with reasonable certainty not be rounded
    down to 0.
    
    The formerly machine-independent file "lockdflt.c" has been moved
    into the architecture-specific subdirectories by repository copy.
    It now contains the machine-dependent spinlocking code.  For the
    spinlocks I used the very nifty "simple, non-scalable reader-preference
    lock" which I found at
    
      <http://www.cs.rochester.edu/u/scott/synchronization/pseudocode/rw.html>
    
    on all CPUs except the 80386 (the specific CPU model, not the
    architecture).  The 80386 CPU doesn't support the necessary "cmpxchg"
    instruction, so on that CPU a simple exclusive test-and-set lock
    is used instead.  80386 CPUs are detected at initialization time by
    trying to execute "cmpxchg" and catching the resulting SIGILL
    signal.
    
    To reduce contention for the locks, I have revamped a couple of
    key data structures, permitting all common operations to be done
    under non-exclusive (reader) locking.  The only operations that
    require exclusive locking now are the rare intrusive operations
    such as dlopen() and dlclose().
    
    The dllockinit() interface is now deprecated.  It still exists,
    but only as a do-nothing stub.  I plan to remove it as soon as is
    reasonably possible.  (From the very beginning it was clearly
    labeled as experimental and subject to change.)  As far as I know,
    only the linuxthreads port uses dllockinit().  This interface turned
    out to have several problems.  As one example, when the dynamic
    linker called a client-supplied locking function, that function
    sometimes needed lazy binding, causing re-entry into the dynamic
    linker and a big looping mess.  And in any case, it turned out to be
    too burdensome to require threads packages to register themselves
    with the dynamic linker.
    
    
    Former-commit-id: 7cfc667ac7c1d4a229afbfb9f811712cf229f2d1

--- libexec/rtld-elf/i386/rtld_machdep.h
+++ libexec/rtld-elf/i386/rtld_machdep.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1999, 2000 John D. Polstra.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -48,7 +48,79 @@ define|\
 value|do {						\ 	dbg("reloc_jmpslot: *%p = %p", (void *)(where),	\ 	  (void *)(target));				\ 	(*(Elf_Addr *)(where) = (Elf_Addr)(target));	\     } while (0)
 end_define
 
+begin_function
+specifier|static
+specifier|inline
+name|void
+name|atomic_decr_int
+parameter_list|(
+specifier|volatile
+name|int
+modifier|*
+name|p
+parameter_list|)
+block|{
+asm|__asm __volatile ("lock; decl %0" : "=m"(*p) : "0"(*p) : "cc");
+block|}
+end_function
+
+begin_function
+specifier|static
+specifier|inline
+name|void
+name|atomic_incr_int
+parameter_list|(
+specifier|volatile
+name|int
+modifier|*
+name|p
+parameter_list|)
+block|{
+asm|__asm __volatile ("lock; incl %0" : "=m"(*p) : "0"(*p) : "cc");
+block|}
+end_function
+
+begin_function
+specifier|static
+specifier|inline
+name|void
+name|atomic_add_int
+parameter_list|(
+specifier|volatile
+name|int
+modifier|*
+name|p
+parameter_list|,
+name|int
+name|val
+parameter_list|)
+block|{
+asm|__asm __volatile ("lock; addl %1, %0"
+block|:
+literal|"=m"
+operator|(
+operator|*
+name|p
+operator|)
+operator|:
+literal|"ri"
+operator|(
+name|val
+operator|)
+operator|,
+literal|"0"
+operator|(
+operator|*
+name|p
+operator|)
+operator|:
+literal|"cc"
+block|)
+function|;
+end_function
+
 begin_endif
+unit|}
 endif|#
 directive|endif
 end_endif
