f2043a1d54451cabeeeb321afc31f8b397c6d87e
Daniel C. Sobral <dcs@FreeBSD.org>
2000-07-09 18:13:35 +0000

    Spencer's regex(3) test code.
    Obtained from: BSD/OS
    
    
    Former-commit-id: 43b6cd4f4fbd45d295590090fade9604186036cf

---
+++ lib/libc/regex/grot/stdlib.h
@@ -0,0 +1,25 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$  */
+end_comment
+
+begin_function_decl
+specifier|extern
+name|char
+modifier|*
+name|malloc
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|char
+modifier|*
+name|realloc
+parameter_list|()
+function_decl|;
+end_function_decl
+
+end_unit
+
