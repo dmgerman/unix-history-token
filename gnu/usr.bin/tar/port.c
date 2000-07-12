522077d217c70462c71546678d4b2b4db5ed0dac
Kris Kennaway <kris@FreeBSD.org>
2000-07-12 00:59:32 +0000

    Don't call fprintf() without a format string.
    Former-commit-id: 4b8e15910e0b514651d7ef3a032af4fc63e664bb

--- gnu/usr.bin/tar/port.c
+++ gnu/usr.bin/tar/port.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Supporting routines which may sometimes be missing.    Copyright (C) 1988, 1992 Free Software Foundation  This file is part of GNU Tar.  GNU Tar is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GNU Tar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GNU Tar; see the file COPYING.  If not, write to the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -1990,6 +1994,8 @@ expr_stmt|;
 name|fprintf
 argument_list|(
 name|stderr
+argument_list|,
+literal|"%s"
 argument_list|,
 name|s
 argument_list|)
