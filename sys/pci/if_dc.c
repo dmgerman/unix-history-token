66e6d5ff9e3dda28d78f784704bbcc21143241c2
Bill Paul <wpaul@FreeBSD.org>
2000-07-14 20:48:33 +0000

    MFC: merge in support for Accton EN1217D card. (Macronix chip: just add the PCI IDs.)
    Former-commit-id: 0ff9142e1ba88447848b77ff84196f3d3898a6d7

--- sys/pci/if_dc.c
+++ sys/pci/if_dc.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright (c) 1997, 1998, 1999  *	Bill Paul<wpaul@ee.columbia.edu>
 end_comment
 
 begin_comment
-comment|/*  * DEC "tulip" clone ethernet driver. Supports the DEC/Intel 21143  * series chips and several workalikes including the following:  *  * Macronix 98713/98715/98725 PMAC (www.macronix.com)  * Macronix/Lite-On 82c115 PNIC II (www.macronix.com)  * Lite-On 82c168/82c169 PNIC (www.litecom.com)  * ASIX Electronics AX88140A (www.asix.com.tw)  * ASIX Electronics AX88141 (www.asix.com.tw)  * ADMtek AL981 (www.admtek.com.tw)  * ADMtek AN985 (www.admtek.com.tw)  * Davicom DM9100, DM9102, DM9102A (www.davicom8.com)  *  * Datasheets for the 21143 are available at developer.intel.com.  * Datasheets for the clone parts can be found at their respective sites.  * (Except for the PNIC; see www.freebsd.org/~wpaul/PNIC/pnic.ps.gz.)  * The PNIC II is essentially a Macronix 98715A chip; the only difference  * worth noting is that its multicast hash table is only 128 bits wide  * instead of 512.  *  * Written by Bill Paul<wpaul@ee.columbia.edu>  * Electrical Engineering Department  * Columbia University, New York City  */
+comment|/*  * DEC "tulip" clone ethernet driver. Supports the DEC/Intel 21143  * series chips and several workalikes including the following:  *  * Macronix 98713/98715/98725 PMAC (www.macronix.com)  * Macronix/Lite-On 82c115 PNIC II (www.macronix.com)  * Lite-On 82c168/82c169 PNIC (www.litecom.com)  * ASIX Electronics AX88140A (www.asix.com.tw)  * ASIX Electronics AX88141 (www.asix.com.tw)  * ADMtek AL981 (www.admtek.com.tw)  * ADMtek AN985 (www.admtek.com.tw)  * Davicom DM9100, DM9102, DM9102A (www.davicom8.com)  * Accton EN1217 (www.accton.com)  *  * Datasheets for the 21143 are available at developer.intel.com.  * Datasheets for the clone parts can be found at their respective sites.  * (Except for the PNIC; see www.freebsd.org/~wpaul/PNIC/pnic.ps.gz.)  * The PNIC II is essentially a Macronix 98715A chip; the only difference  * worth noting is that its multicast hash table is only 128 bits wide  * instead of 512.  *  * Written by Bill Paul<wpaul@ee.columbia.edu>  * Electrical Engineering Department  * Columbia University, New York City  */
 end_comment
 
 begin_comment
@@ -369,6 +369,14 @@ block|,
 name|DC_DEVICEID_82C168
 block|,
 literal|"82c169 PNIC 10/100BaseTX"
+block|}
+block|,
+block|{
+name|DC_VENDORID_ACCTON
+block|,
+name|DC_DEVICEID_EN1217
+block|,
+literal|"Accton EN1217 10/100BaseTX"
 block|}
 block|,
 block|{
@@ -7009,6 +7017,9 @@ expr_stmt|;
 break|break;
 case|case
 name|DC_DEVICEID_987x5
+case|:
+case|case
+name|DC_DEVICEID_EN1217
 case|:
 name|sc
 operator|->
