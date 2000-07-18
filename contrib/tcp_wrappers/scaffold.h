149d8e67ba86ed6a2d54876b921b327b851b395f
Hajimu UMEMOTO <ume@FreeBSD.org>
2000-07-18 08:34:57 +0000

    MFC: Add IPv6 scoped address support. It enables us to control link-local connections by interface like this:
        ALL : [fe80::%ed0]/10 : allow
        ALL : [fe80::]/10 : deny
    
    
    Former-commit-id: 4521be30672927eab9f998fc67856fd5169d3975

--- contrib/tcp_wrappers/scaffold.h
+++ contrib/tcp_wrappers/scaffold.h
@@ -1,7 +1,28 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*   * @(#) scaffold.h 1.3 94/12/31 18:19:19   *    * Author: Wietse Venema, Eindhoven University of Technology, The Netherlands.   */
+comment|/*   * @(#) scaffold.h 1.3 94/12/31 18:19:19   *    * Author: Wietse Venema, Eindhoven University of Technology, The Netherlands.   *   * $FreeBSD$   */
 end_comment
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|INET6
+end_ifdef
+
+begin_function_decl
+specifier|extern
+name|struct
+name|addrinfo
+modifier|*
+name|find_inet_addr
+parameter_list|()
+function_decl|;
+end_function_decl
+
+begin_else
+else|#
+directive|else
+end_else
 
 begin_function_decl
 specifier|extern
@@ -12,6 +33,11 @@ name|find_inet_addr
 parameter_list|()
 function_decl|;
 end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_function_decl
 specifier|extern
