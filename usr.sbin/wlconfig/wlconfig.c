f01d63934c8a4746598fcad5b45f08f7f20c68df
Archie Cobbs <archie@FreeBSD.org>
2000-07-19 16:33:02 +0000

    MFC: fix breakage caused by ether_aton() prototype change.
    Former-commit-id: 50ec5c7c74bc6c2b61070564ef86d37c315be884

--- usr.sbin/wlconfig/wlconfig.c
+++ usr.sbin/wlconfig/wlconfig.c
@@ -81,20 +81,6 @@ directive|include
 file|<netinet/if_ether.h>
 end_include
 
-begin_function_decl
-specifier|extern
-name|struct
-name|ether_addr
-modifier|*
-name|ether_aton
-parameter_list|(
-name|char
-modifier|*
-name|a
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_include
 include|#
 directive|include
