a2e1f6d7928c89322cf858310610620fbbb2746a
Kris Kennaway <kris@FreeBSD.org>
2000-07-31 21:37:29 +0000

    Fix a bug in keyed sorting due to malloc abuse.
    Submitted by:	green
    
    
    Former-commit-id: 464cd1f2f14250a9b17ce28690c585728217e478

--- gnu/usr.bin/sort/sort.c
+++ gnu/usr.bin/sort/sort.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* sort - sort lines of text (with all kinds of options).    Copyright (C) 1988, 1991, 1992, 1993, 1994, 1995 Free Software Foundation     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.     Written December 1988 by Mike Haertel.    The author may be reached (Email) at the address mike@gnu.ai.mit.edu,    or (US mail) as Mike Haertel c/o Free Software Foundation. */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -9482,6 +9486,17 @@ sizeof|sizeof
 argument_list|(
 expr|struct
 name|keyfield
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|bzero
+argument_list|(
+name|key
+argument_list|,
+sizeof|sizeof
+argument_list|(
+operator|*
+name|key
 argument_list|)
 argument_list|)
 expr_stmt|;
