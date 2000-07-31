ace0047c275ba1145655fe2954c77c765a963cc7
Andrey Chernov <ache@FreeBSD.org>
2000-07-31 23:41:47 +0000

    Nonexistent <sys/pfil.h> -> <net/pfil.h> Kernel 'make depend' fails otherwise
    Former-commit-id: b66113562afad3ad553a34b451d98eb22a1b1957

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -84,12 +84,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/pfil.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/malloc.h>
 end_include
 
@@ -133,6 +127,12 @@ begin_include
 include|#
 directive|include
 file|<sys/sysctl.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<net/pfil.h>
 end_include
 
 begin_include
