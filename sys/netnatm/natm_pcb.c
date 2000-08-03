dc5b0a74a24fdcc84834cba783dbc33414955fa1
Peter Wemm <peter@FreeBSD.org>
2000-08-03 18:56:28 +0000

    MFC: $FreeBSD$
    Former-commit-id: 75b8b841d205df5ac8a9798fddc28e187458c4fd

--- sys/netnatm/natm_pcb.c
+++ sys/netnatm/natm_pcb.c
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/*	$NetBSD: natm_pcb.c,v 1.4 1996/11/09 03:26:27 chuck Exp $	*/
 end_comment
 
