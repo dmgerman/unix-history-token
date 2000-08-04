0ce121e37ab230f5a562aed1f7d22d29d780d5bb
Michael Smith <msmith@FreeBSD.org>
2000-08-04 06:52:50 +0000

    MFC: support for 2.x/3.x adapters without memory-mapped register windows.
    Former-commit-id: 1aff6b26d9e03f339766dbb0d287b9383cc1c0b5

--- sys/dev/mlx/mlx.c
+++ sys/dev/mlx/mlx.c
@@ -76,6 +76,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/bus_memio.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/bus_pio.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/bus.h>
 end_include
 
