d90eff13e2514e197c6f06f459714198c39aef38
John Baldwin <jhb@FreeBSD.org>
2000-08-23 00:05:29 +0000

    MFC: - Correct string length bounds checking. - Calculate the string length of a u_long at compile-time, instead of using   a hardcoded value. - Add FreeBSD id tag.
    Former-commit-id: 0c77e31d5191189ff889620f293f189c1b8a5fe5

--- lib/libc/rpc/netname.c
+++ lib/libc/rpc/netname.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user or with the express written consent of  * Sun Microsystems, Inc.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  */
+comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user or with the express written consent of  * Sun Microsystems, Inc.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  *  * $FreeBSD$  */
 end_comment
 
 begin_if
@@ -87,13 +87,19 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stdlib.h>
+file|<limits.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<unistd.h>
+file|<stdio.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<stdlib.h>
 end_include
 
 begin_include
@@ -105,7 +111,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stdio.h>
+file|<unistd.h>
 end_include
 
 begin_ifndef
@@ -143,6 +149,41 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_define
+define|#
+directive|define
+name|TYPE_BIT
+parameter_list|(
+name|type
+parameter_list|)
+value|(sizeof (type) * CHAR_BIT)
+end_define
+
+begin_define
+define|#
+directive|define
+name|TYPE_SIGNED
+parameter_list|(
+name|type
+parameter_list|)
+value|(((type) -1)< 0)
+end_define
+
+begin_comment
+comment|/* ** 302 / 1000 is log10(2.0) rounded up. ** Subtract one for the sign bit if the type is signed; ** add one for integer division truncation; ** add one more for a minus sign if the type is signed. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|INT_STRLEN_MAXIMUM
+parameter_list|(
+name|type
+parameter_list|)
+define|\
+value|((TYPE_BIT(type) - TYPE_SIGNED(type)) * 302 / 1000 + 1 + TYPE_SIGNED(type))
+end_define
 
 begin_decl_stmt
 specifier|static
@@ -265,11 +306,6 @@ name|char
 modifier|*
 name|dfltdom
 decl_stmt|;
-define|#
-directive|define
-name|MAXIPRINT
-value|(11)
-comment|/* max length of printed integer */
 if|if
 condition|(
 name|domain
@@ -308,7 +344,17 @@ argument_list|)
 operator|+
 literal|1
 operator|+
-name|MAXIPRINT
+name|INT_STRLEN_MAXIMUM
+argument_list|(
+name|u_long
+argument_list|)
+operator|+
+literal|1
+operator|+
+name|strlen
+argument_list|(
+name|OPSYS
+argument_list|)
 operator|>
 name|MAXNETNAMELEN
 condition|)
@@ -455,6 +501,13 @@ operator|+
 name|strlen
 argument_list|(
 name|host
+argument_list|)
+operator|+
+literal|1
+operator|+
+name|strlen
+argument_list|(
+name|OPSYS
 argument_list|)
 operator|>
 name|MAXNETNAMELEN
