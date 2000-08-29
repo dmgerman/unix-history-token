83f2130d3ca9b21fe2ded510f583fbc773fc6fa8
Andrey Chernov <ache@FreeBSD.org>
2000-08-29 21:04:07 +0000

    strtok() -> strsep() (no strtok() in libraries allowed) small cleanup in nearby area: pointer 0 -> NULL, according to manpages hardcoded constant -> sizeof(buf)
    Former-commit-id: 84dcb353f5c5787f82b27ce8a0a7ebe9ed809ee6

--- lib/libc/rpc/getpublickey.c
+++ lib/libc/rpc/getpublickey.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user or with the express written consent of  * Sun Microsystems, Inc.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  */
+comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user or with the express written consent of  * Sun Microsystems, Inc.  *  * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *  * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *  * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *  * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *  * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  *  * $FreeBSD$  */
 end_comment
 
 begin_if
@@ -282,11 +282,7 @@ if|if
 condition|(
 name|fd
 operator|==
-operator|(
-name|FILE
-operator|*
-operator|)
-literal|0
+name|NULL
 condition|)
 return|return
 operator|(
@@ -305,7 +301,10 @@ name|fgets
 argument_list|(
 name|buf
 argument_list|,
-literal|1024
+sizeof|sizeof
+argument_list|(
+name|buf
+argument_list|)
 argument_list|,
 name|fd
 argument_list|)
@@ -314,7 +313,7 @@ if|if
 condition|(
 name|res
 operator|==
-literal|0
+name|NULL
 condition|)
 block|{
 name|fclose
@@ -490,9 +489,10 @@ else|else
 block|{
 name|mkey
 operator|=
-name|strtok
+name|strsep
 argument_list|(
-name|buf
+operator|&
+name|res
 argument_list|,
 literal|"\t "
 argument_list|)
@@ -517,19 +517,30 @@ argument_list|)
 expr_stmt|;
 continue|continue;
 block|}
+do|do
+block|{
 name|mval
 operator|=
-name|strtok
+name|strsep
 argument_list|(
-operator|(
-name|char
-operator|*
-operator|)
-name|NULL
+operator|&
+name|res
 argument_list|,
 literal|" \t#\n"
 argument_list|)
 expr_stmt|;
+block|}
+do|while
+condition|(
+name|mval
+operator|!=
+name|NULL
+operator|&&
+operator|!
+operator|*
+name|mval
+condition|)
+do|;
 if|if
 condition|(
 name|mval
