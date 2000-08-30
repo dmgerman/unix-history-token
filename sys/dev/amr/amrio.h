bd111f77f02e7702921811ef72a8f1fcbf735479
Michael Smith <msmith@FreeBSD.org>
2000-08-30 07:52:50 +0000

    Major update to the AMI MegaRAID driver.
     - New support for 40LD firmware found in Series 475 and 471 adapters.
     - Better support for 8LD firmware adapters
     - Ioctl passthrough interface for userland utilities.
     - Improved error handling and queueing.
     - Several bugfixes (including the 'still open' shutdown bug and
       closing some small race conditions).
     - Zone-style command allocator, reducing memory wasted under heavy
       load conditions.
     - CAM interface (disabled and not fully working) for SCSI passthrough
       access to non-disk devices
    
    Thanks to AMI for supplying a pile of new adapters and various other
    help in making this happen.
    
    
    Former-commit-id: b1144d4bd297e8b07dd91a3bc33f494ef8f1988f

--- sys/dev/amr/amrio.h
+++ sys/dev/amr/amrio.h
@@ -4,35 +4,88 @@ comment|/*-  * Copyright (c) 1999 Michael Smith  * All rights reserved.  *  * Re
 end_comment
 
 begin_comment
-comment|/*  * Drive status  */
+comment|/*  * ioctl interface  */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/ioccom.h>
+end_include
+
+begin_comment
+comment|/*  * Fetch the driver's interface version.  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|AMRD_OFFLINE
-value|0x0
+name|AMR_IO_VERSION_NUMBER
+value|0x01
 end_define
 
 begin_define
 define|#
 directive|define
-name|AMRD_DEGRADED
-value|0x1
+name|AMR_IO_VERSION
+value|_IOR('A', 0x200, int)
 end_define
+
+begin_comment
+comment|/*  * Pass a command from userspace through to the adapter.  *  * Note that in order to be code-compatible with the Linux  * interface where possible, the formatting of the au_cmd field is  * somewhat Interesting.  *  * For normal commands, the layout is (fields from struct amr_mailbox_ioctl):  *  * 0		mb_command  * 1		mb_channel  * 2		mb_param  * 3		mb_pad[0]  * 4		mb_drive  *  * For SCSI passthrough commands, the layout is:  *  * 0		AMR_CMD_PASS	(0x3)  * 1		reserved, 0  * 2		cdb length  * 3		cdb data  * 3+cdb_len	passthrough control byte (timeout, ars, islogical)  * 4+cdb_len	reserved, 0  * 5+cdb_len	channel  * 6+cdb_len	target  */
+end_comment
+
+begin_struct
+struct|struct
+name|amr_user_ioctl
+block|{
+name|unsigned
+name|char
+name|au_cmd
+index|[
+literal|32
+index|]
+decl_stmt|;
+comment|/* command text from userspace */
+name|void
+modifier|*
+name|au_buffer
+decl_stmt|;
+comment|/* data buffer in userspace */
+name|unsigned
+name|long
+name|au_length
+decl_stmt|;
+comment|/* data buffer size (0 == no data) */
+name|int
+name|au_direction
+decl_stmt|;
+comment|/* data transfer direction */
+define|#
+directive|define
+name|AMR_IO_NODATA
+value|0
+define|#
+directive|define
+name|AMR_IO_READ
+value|1
+define|#
+directive|define
+name|AMR_IO_WRITE
+value|2
+name|int
+name|au_status
+decl_stmt|;
+comment|/* command status returned by adapter */
+block|}
+struct|;
+end_struct
 
 begin_define
 define|#
 directive|define
-name|AMRD_OPTIMAL
-value|0x2
-end_define
-
-begin_define
-define|#
-directive|define
-name|AMRD_DELETED
-value|0x3
+name|AMR_IO_COMMAND
+value|_IOWR('A', 0x201, struct amr_user_ioctl)
 end_define
 
 end_unit
