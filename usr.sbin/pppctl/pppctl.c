80008e40cb0624810905fa86243b200c1f3d3c41
Brian Somers <brian@FreeBSD.org>
2000-09-02 02:01:18 +0000

    libutil is no longer required as setproctitle() has moved to libc
    Former-commit-id: 1aca6027fc3dfc25c53084c4851afb3d77ed2cfb

--- usr.sbin/pppctl/pppctl.c
+++ usr.sbin/pppctl/pppctl.c
@@ -63,23 +63,6 @@ directive|include
 file|<histedit.h>
 end_include
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__FreeBSD__
-end_ifdef
-
-begin_include
-include|#
-directive|include
-file|<libutil.h>
-end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_include
 include|#
 directive|include
