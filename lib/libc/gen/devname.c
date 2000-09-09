f881797e649763a6b6f6e98fc48bfa146905ecce
Poul-Henning Kamp <phk@FreeBSD.org>
2000-09-09 11:39:59 +0000

    Add code to devname(3) so it can find the names of devices which were not present when dev_mkdb(8) was run.
    First the dev_mkdb(8) database is searched, this caters for non-DEVFS
    cases where people have renamed a device.
    
    If that fails we ask the kernel using sysctl kern.devname if the device
    driver has put a name in the dev_t.  This covers DEVFS cloned devices.
    
    If that also fails we format a string which isn't entirely useless.
    
    
    Former-commit-id: 0794ebe94f211cc5e4d611ce23191caa86372b1d

--- lib/libc/gen/devname.c
+++ lib/libc/gen/devname.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_if
@@ -41,6 +41,12 @@ begin_include
 include|#
 directive|include
 file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/sysctl.h>
 end_include
 
 begin_include
@@ -276,13 +282,20 @@ specifier|static
 name|char
 name|buf
 index|[
-literal|20
+literal|30
 index|]
+decl_stmt|;
+comment|/* XXX: pick up from<sys/conf.h> */
+name|int
+name|i
+decl_stmt|,
+name|j
 decl_stmt|;
 name|char
 modifier|*
 name|r
 decl_stmt|;
+comment|/* First check the DB file. */
 name|r
 operator|=
 name|xdevname
@@ -294,10 +307,67 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-operator|!
 name|r
+operator|!=
+name|NULL
+condition|)
+return|return
+operator|(
+name|r
+operator|)
+return|;
+comment|/* Then ask the kernel. */
+if|if
+condition|(
+operator|(
+name|type
+operator|&
+name|S_IFMT
+operator|)
+operator|==
+name|S_IFCHR
 condition|)
 block|{
+name|j
+operator|=
+sizeof|sizeof
+argument_list|(
+name|buf
+argument_list|)
+expr_stmt|;
+name|i
+operator|=
+name|sysctlbyname
+argument_list|(
+literal|"kern.devname"
+argument_list|,
+name|buf
+argument_list|,
+operator|&
+name|j
+argument_list|,
+operator|&
+name|dev
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|dev
+argument_list|)
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|i
+operator|==
+literal|0
+condition|)
+return|return
+operator|(
+name|buf
+operator|)
+return|;
+block|}
+comment|/* Finally just format it */
 name|r
 operator|=
 name|buf
@@ -373,7 +443,6 @@ name|dev
 argument_list|)
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 return|return
 operator|(
