aee004964f75352ab5ac55925378b565ed9bd941
Takenori KATO <kato@FreeBSD.org>
2000-09-15 05:47:56 +0000

    Merged from sys/isa/fd.c revision 1.187.
    Pointed out by:	nyan
    
    
    Former-commit-id: f8e0a3ad557b1061b922867f643ab0311558c80f

--- sys/pc98/pc98/fd.c
+++ sys/pc98/pc98/fd.c
@@ -6821,6 +6821,12 @@ literal|0
 block|int	i; 	int	mynor; 	int	typemynor; 	int	typesize;
 endif|#
 directive|endif
+specifier|static
+name|int
+name|cdevsw_add_done
+init|=
+literal|0
+decl_stmt|;
 name|fd
 operator|=
 name|device_get_softc
@@ -6828,6 +6834,12 @@ argument_list|(
 name|dev
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|!
+name|cdevsw_add_done
+condition|)
+block|{
 name|cdevsw_add
 argument_list|(
 operator|&
@@ -6835,6 +6847,10 @@ name|fd_cdevsw
 argument_list|)
 expr_stmt|;
 comment|/* XXX */
+name|cdevsw_add_done
+operator|++
+expr_stmt|;
+block|}
 name|make_dev
 argument_list|(
 operator|&
