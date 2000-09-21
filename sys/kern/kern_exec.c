ac3f69da0e2780e725bc767cfd67f735aa87991a
Don Lewis <truckman@FreeBSD.org>
2000-09-21 09:04:17 +0000

    Remove unneeded #include that was a remnant of an earlier version of my uidinfo patch.
    Found by:	phk
    
    
    Former-commit-id: 742b9372ac98f44e5841a639c98de3184ff7b3f4

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -108,12 +108,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/resourcevar.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/sysent.h>
 end_include
 
