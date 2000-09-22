eae6ec8b3b42d3cb32bf3ca4223ec2003e32b778
Maxim Sobolev <sobomax@FreeBSD.org>
2000-09-22 06:53:44 +0000

    "zope language" --> "zope platform".
    Requested by:	asami
    
    
    Former-commit-id: bfb5a7c876478348c17ce1b8764e810a38f8af03

--- release/sysinstall/index.c
+++ release/sysinstall/index.c
@@ -490,7 +490,7 @@ literal|"X Window System window managers."
 block|,
 literal|"zope"
 block|,
-literal|"Software related to the Zope language."
+literal|"Software related to the Zope platform."
 block|,
 name|NULL
 block|,
