cb58ffcb7b850ad24b6e5fdef4ba246eeda5f2a5
Justin T. Gibbs <gibbs@FreeBSD.org>
2000-09-22 23:11:41 +0000

    MFC: Add SPI-3 messages and message terms.
    Former-commit-id: 00ba838ba00f4b44076cf3d9bf92adbb3d57483f

--- sys/cam/scsi/scsi_message.h
+++ sys/cam/scsi/scsi_message.h
@@ -1,9 +1,5 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $FreeBSD$ */
-end_comment
-
-begin_comment
 comment|/*  * This file is in the public domain.  * $FreeBSD$  */
 end_comment
 
@@ -24,6 +20,21 @@ end_define
 
 begin_comment
 comment|/* M/M */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_TASK_COMPLETE
+value|0x00
+end_define
+
+begin_comment
+comment|/* M/M */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
 end_comment
 
 begin_define
@@ -95,6 +106,21 @@ end_comment
 begin_define
 define|#
 directive|define
+name|MSG_ABORT_TASK_SET
+value|0x06
+end_define
+
+begin_comment
+comment|/* O/M */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MSG_MESSAGE_REJECT
 value|0x07
 end_define
@@ -161,12 +187,42 @@ end_comment
 begin_define
 define|#
 directive|define
+name|MSG_TARGET_RESET
+value|0x0c
+end_define
+
+begin_comment
+comment|/* O/M */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MSG_ABORT_TAG
 value|0x0d
 end_define
 
 begin_comment
 comment|/* O/O */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_ABORT_TASK
+value|0x0d
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
 end_comment
 
 begin_define
@@ -183,12 +239,31 @@ end_comment
 begin_define
 define|#
 directive|define
+name|MSG_CLEAR_TASK_SET
+value|0x0e
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MSG_INIT_RECOVERY
 value|0x0f
 end_define
 
 begin_comment
 comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* Deprecated in SPI3 */
 end_comment
 
 begin_define
@@ -202,6 +277,10 @@ begin_comment
 comment|/* O/O */
 end_comment
 
+begin_comment
+comment|/* Deprecated in SPI3 */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -211,6 +290,55 @@ end_define
 
 begin_comment
 comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* Deprecated in SPI3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_CLEAR_ACA
+value|0x16
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_LOGICAL_UNIT_RESET
+value|0x17
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_QAS_REQUEST
+value|0x55
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 */
 end_comment
 
 begin_comment
@@ -231,12 +359,42 @@ end_comment
 begin_define
 define|#
 directive|define
+name|MSG_SIMPLE_TASK
+value|0x20
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MSG_HEAD_OF_Q_TAG
 value|0x21
 end_define
 
 begin_comment
 comment|/* O/O */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_HEAD_OF_QUEUE_TASK
+value|0x21
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
 end_comment
 
 begin_define
@@ -253,12 +411,42 @@ end_comment
 begin_define
 define|#
 directive|define
+name|MSG_ORDERED_TASK
+value|0x22
+end_define
+
+begin_comment
+comment|/* O/O */
+end_comment
+
+begin_comment
+comment|/* SPI3 Terminology */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|MSG_IGN_WIDE_RESIDUE
 value|0x23
 end_define
 
 begin_comment
 comment|/* O/O */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_ACA_TASK
+value|0x24
+end_define
+
+begin_comment
+comment|/* 0/0 */
+end_comment
+
+begin_comment
+comment|/* SPI3 */
 end_comment
 
 begin_comment
@@ -309,7 +497,7 @@ begin_define
 define|#
 directive|define
 name|MSG_IDENTIFY_LUNMASK
-value|0x01F
+value|0x03F
 end_define
 
 begin_comment
@@ -363,6 +551,49 @@ define|#
 directive|define
 name|MSG_EXT_WDTR_BUS_32_BIT
 value|0x02
+end_define
+
+begin_comment
+comment|/* Deprecated in SPI3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_EXT_PPR
+value|0x04
+end_define
+
+begin_comment
+comment|/* SPI3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MSG_EXT_PPR_LEN
+value|0x06
+end_define
+
+begin_define
+define|#
+directive|define
+name|MSG_EXT_PPR_QAS_REQ
+value|0x04
+end_define
+
+begin_define
+define|#
+directive|define
+name|MSG_EXT_PPR_DT_REQ
+value|0x02
+end_define
+
+begin_define
+define|#
+directive|define
+name|MSG_EXT_PPR_IU_REQ
+value|0x01
 end_define
 
 end_unit
