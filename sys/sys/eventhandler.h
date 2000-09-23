b26e470fe783888e87e822d8dd776bec156a177e
Michael Smith <msmith@FreeBSD.org>
2000-09-23 00:12:31 +0000

    Don't include proc.h now that mutex.h does it for us.
    Former-commit-id: 1e62e7a9f50652602ca50ace4a4dbeda488f1ea3

--- sys/sys/eventhandler.h
+++ sys/sys/eventhandler.h
@@ -12,16 +12,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/proc.h>
-end_include
-
-begin_comment
-comment|/* XXX should these be prerequisites? */
-end_comment
-
-begin_include
-include|#
-directive|include
 file|<machine/mutex.h>
 end_include
 
