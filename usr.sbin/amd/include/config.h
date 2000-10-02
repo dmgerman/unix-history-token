25c23a0d9a90e380490c71ec7bd6a268ec84a3a6
Peter Wemm <peter@FreeBSD.org>
2000-10-02 08:31:20 +0000

    Really fix the struct callout collision.  AMD does not need to #include <sys/mbuf.h>.  (which #include's <machine/mutex.h> and then <sys/proc.h> and then <sys/callout.h>, leading to the collision). <sys/mbuf.h> is really one of those 'no user servicable parts inside' things.
    Former-commit-id: 3bf24112d2b646f7bcdb6d672a23eaa938e4ef58

--- usr.sbin/amd/include/config.h
+++ usr.sbin/amd/include/config.h
@@ -5444,12 +5444,9 @@ begin_comment
 comment|/* Define if you have the<sys/mbuf.h> header file.  */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|HAVE_SYS_MBUF_H
-value|1
-end_define
+begin_comment
+comment|/* #define HAVE_SYS_MBUF_H 1 XXX NO NO NO!!! */
+end_comment
 
 begin_comment
 comment|/* Define if you have the<sys/mntctl.h> header file.  */
