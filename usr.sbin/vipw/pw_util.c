5793c9eb4bc708bd61c2f085f7fcd771d016e4be
Peter Wemm <peter@FreeBSD.org>
2000-10-04 05:44:33 +0000

    MFC: printf-style format fix.  warn(string) -> warn("%s", string)
    Former-commit-id: 75569aac5ad260c69fdf51205154553c093cb002

--- usr.sbin/vipw/pw_util.c
+++ usr.sbin/vipw/pw_util.c
@@ -9,15 +9,33 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_else
+unit|static char sccsid[] = "@(#)pw_util.c	8.3 (Berkeley) 4/2/94";
+else|#
+directive|else
+end_else
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)pw_util.c	8.3 (Berkeley) 4/2/94"
+literal|"$FreeBSD$"
 decl_stmt|;
 end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_endif
 endif|#
@@ -917,6 +935,8 @@ name|err
 condition|)
 name|warn
 argument_list|(
+literal|"%s"
+argument_list|,
 name|name
 argument_list|)
 expr_stmt|;
