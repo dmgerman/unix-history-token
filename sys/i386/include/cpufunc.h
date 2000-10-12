09091e558bee987fe83cd56d29c05c53dac4178b
Bruce Evans <bde@FreeBSD.org>
2000-10-12 17:05:33 +0000

    Removed unused include of <machine/lock.h>.  The locking interface stopped being (ab)used here in rev.1.97.
    Former-commit-id: ebfdb257684299e7e5cc3a0471bfe376faadd57a

--- sys/i386/include/cpufunc.h
+++ sys/i386/include/cpufunc.h
@@ -94,27 +94,6 @@ end_ifdef
 begin_ifdef
 ifdef|#
 directive|ifdef
-name|SMP
-end_ifdef
-
-begin_include
-include|#
-directive|include
-file|<machine/lock.h>
-end_include
-
-begin_comment
-comment|/* XXX */
-end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_ifdef
-ifdef|#
-directive|ifdef
 name|SWTCH_OPTIM_STATS
 end_ifdef
 
