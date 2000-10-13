3215f07bf5fcdffe93626e06535139bf68388874
David E. O'Brien <obrien@FreeBSD.org>
2000-10-13 12:04:55 +0000

    Virgin import of gperf v2.7.2.
    Former-commit-id: 20ea4fec1d2bbdc73a92e7a0d9da462a841f7492

--- contrib/gperf/lib/getopt.h
+++ contrib/gperf/lib/getopt.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* Declarations for getopt.    Copyright (C) 1989,90,91,92,93,94,96,97 Free Software Foundation, Inc.    This file is part of the GNU C Library.     The GNU C Library is free software; you can redistribute it and/or    modify it under the terms of the GNU Library General Public License as    published by the Free Software Foundation; either version 2 of the    License, or (at your option) any later version.     The GNU C Library is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    Library General Public License for more details.     You should have received a copy of the GNU Library General Public    License along with the GNU C Library; see the file COPYING.LIB.  If not,    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,    Boston, MA 02111-1307, USA.  */
+comment|/* Declarations for getopt.    Copyright (C) 1989-1998, 2000 Free Software Foundation, Inc.    This file is part of the GNU C Library.     The GNU C Library is free software; you can redistribute it and/or    modify it under the terms of the GNU Library General Public License as    published by the Free Software Foundation; either version 2 of the    License, or (at your option) any later version.     The GNU C Library is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    Library General Public License for more details.     You should have received a copy of the GNU Library General Public    License along with the GNU C Library; see the file COPYING.LIB.  If not,    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,    Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_ifndef
@@ -55,12 +55,19 @@ name|option
 block|{
 if|#
 directive|if
+operator|(
 name|defined
 argument_list|(
 name|__STDC__
 argument_list|)
 operator|&&
 name|__STDC__
+operator|)
+operator|||
+name|defined
+argument_list|(
+name|__cplusplus
+argument_list|)
 specifier|const
 name|char
 modifier|*
