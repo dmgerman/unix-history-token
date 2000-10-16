c20e522a2f0a4ea10e70b47e224577d6ad13fe0b
Brian Somers <brian@FreeBSD.org>
2000-10-16 09:14:44 +0000

    Include sys/types.h
    Former-commit-id: 8d88505680f0d7087ee09439ee079c35404b6bcb

--- usr.sbin/amd/include/config.h
+++ usr.sbin/amd/include/config.h
@@ -2664,6 +2664,12 @@ end_comment
 begin_include
 include|#
 directive|include
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/endian.h>
 end_include
 
