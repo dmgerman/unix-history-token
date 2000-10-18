32df0652c2071e1a7a7273a00a0c477e7563d036
Jonathan Chen <jon@FreeBSD.org>
2000-10-18 03:25:13 +0000

    Initial commit of NEWCARD cardbus side (that actually compiles and works)
    Files:
    	dev/cardbus/cardbus.c
    	dev/cardbus/cardbusreg.h
    	dev/cardbus/cardbusvar.h
    	dev/cardbus/cardbus_cis.c
    	dev/cardbus/cardbus_cis.h
    	dev/pccbb/pccbb.c
    	dev/pccbb/pccbbreg.h
    	dev/pccbb/pccbbvar.h
    	dev/pccbb/pccbb_if.m
    
    This should support:
      - cardbus controllers:
        * TI 113X
        * TI 12XX
        * TI 14XX
        * Ricoh 47X
        * Ricoh 46X
        * ToPIC 95
        * ToPIC 97
        * ToPIC 100
        * Cirrus Logic CLPD683x
      - cardbus cards
        * 3c575BT
        * 3c575CT
        * Xircom X3201 (includes IBM, Xircom and, Intel cards)
        [ 3com support already in kernel, Xircom will be committed real soon now]
    
    This doesn't work with 16bit pccards under NEWCARD.
    
    Enable in your config by having "device pccbb" and "device cardbus".
    (A "device pccard" will attach a pccard bus, but it means you system have
    a high chance of panicing when a 16bit card is inserted)
    
    It should be fairly simple to make a driver attach to cardbus under
    NEWCARD -- simply add an entry for attaching to cardbus on a new
    DRIVER_MODULE and add new device IDs as necessary.  You should also make
    sure the card can be detached nicely without the interrupt routine doing
    something weird, like going into an infinite loop.  Usually that should
    entail adding an additional check when a pci register or the bus space is
    read to check if it equals 0xffffffff.
    
    Any problems, please let me know.
    
    Reviewed by: imp
    
    
    Former-commit-id: a96bdc0a345f09499d7e9ae745ddcb3a2a33e4af

--- sys/dev/cardbus/cardbus_cis.h
+++ sys/dev/cardbus/cardbus_cis.h
@@ -4,350 +4,467 @@ comment|/*  * Copyright (c) 2000,2001 Jonathan Chen.  * All rights reserved.  * 
 end_comment
 
 begin_comment
-comment|/*  * Register definitions for the Cardbus Bus  */
+comment|/*  * Cardbus CIS definitions  */
 end_comment
+
+begin_function_decl
+name|int
+name|cardbus_do_cis
+parameter_list|(
+name|device_t
+name|dev
+parameter_list|,
+name|device_t
+name|child
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_define
+define|#
+directive|define
+name|MAXTUPLESIZE
+value|0x400
+end_define
 
 begin_comment
-comment|/* Cardbus bus constants */
+comment|/* CIS TUPLES */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_SLOTMAX
-value|0
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_FUNCMAX
-value|7
-end_define
-
-begin_comment
-comment|/* Cardbus configuration header registers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_BASE0_REG
-value|0x10
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_BASE1_REG
-value|0x14
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_BASE2_REG
-value|0x18
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_BASE3_REG
-value|0x1C
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_BASE4_REG
-value|0x20
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_BASE5_REG
-value|0x24
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_CIS_REG
-value|0x28
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_CIS_ASIMASK
-value|0x07
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_CIS_ADDRMASK
-value|0x0ffffff8
-end_define
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_CIS_ASI_TUPLE
+name|CISTPL_NULL
 value|0x00
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR0
+name|CISTPL_DEVICE
 value|0x01
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR1
+name|CISTPL_LONG_LINK_CB
 value|0x02
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR2
+name|CISTPL_INDIRECT
 value|0x03
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR3
+name|CISTPL_CONFIG_CB
 value|0x04
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR4
+name|CISTPL_CFTABLE_ENTRY_CB
 value|0x05
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR5
+name|CISTPL_LONGLINK_MFC
 value|0x06
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_ROM
+name|CISTPL_BAR
 value|0x07
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_ROM_REG
-value|0x30
-end_define
-
-begin_comment
-comment|/* EXROM offsets for reading CIS */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|CARDBUS_EXROM_SIGNATURE
-value|0x00
+name|CISTPL_PWR_MGMNT
+value|0x08
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_PTR
+name|CISTPL_EXTDEVICE
+value|0x09
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_CHECKSUM
+value|0x10
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_LONGLINK_A
+value|0x11
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_LONGLINK_C
+value|0x12
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_LINKTARGET
+value|0x13
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_NO_LINK
+value|0x14
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_VERS_1
+value|0x15
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_ALTSTR
+value|0x16
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_DEVICE_A
+value|0x17
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_JEDEC_C
 value|0x18
 end_define
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_SIGNATURE
-value|0x00
+name|CISTPL_JEDEC_A
+value|0x19
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_CONFIG
+value|0x1A
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_CFTABLE_ENTRY
+value|0x1B
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_DEVICE_OC
+value|0x1C
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_DEVICE_OA
+value|0x1D
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_DEVICE_GEO
+value|0x1E
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_DEVICE_GEO_A
+value|0x1F
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_MANFID
+value|0x20
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_FUNCID
+value|0x21
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_FUNCE
+value|0x22
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_SWIL
+value|0x23
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_VERS_2
+value|0x40
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_FORMAT
+value|0x41
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_GEOMETRY
+value|0x42
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_BYTEORDER
+value|0x43
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_DATE
+value|0x44
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_BATTERY
+value|0x45
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_ORG
+value|0x46
+end_define
+
+begin_define
+define|#
+directive|define
+name|CISTPL_END
+value|0xFF
 end_define
 
 begin_comment
-comment|/* Signature ("PCIR") */
+comment|/* BAR */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_VENDOR_ID
-value|0x04
+name|TPL_BAR_REG_ASI_MASK
+value|0x07
+end_define
+
+begin_define
+define|#
+directive|define
+name|TPL_BAR_REG_AS
+value|0x08
 end_define
 
 begin_comment
-comment|/* Vendor Identification */
+comment|/* CISTPL_FUNC */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_DEVICE_ID
-value|0x06
+name|TPL_FUNC_MF
+value|0
 end_define
 
 begin_comment
-comment|/* Device Identification */
+comment|/* multi function tuple */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_LENGTH
-value|0x0a
+name|TPL_FUNC_MEM
+value|1
 end_define
 
 begin_comment
-comment|/* PCI Data Structure Length */
+comment|/* memory */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_REV
-value|0x0c
+name|TPL_FUNC_SERIAL
+value|2
 end_define
 
 begin_comment
-comment|/* PCI Data Structure Revision */
+comment|/* serial, including modem and fax */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_CLASS_CODE
-value|0x0d
+name|TPL_FUNC_PARALLEL
+value|3
 end_define
 
 begin_comment
-comment|/* Class Code */
+comment|/* parallel, including printer and SCSI */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_IMAGE_LENGTH
-value|0x10
+name|TPL_FUNC_DISK
+value|4
 end_define
 
 begin_comment
-comment|/* Image Length */
+comment|/* Disk */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_DATA_REV
-value|0x12
+name|TPL_FUNC_VIDEO
+value|5
 end_define
 
 begin_comment
-comment|/* Revision Level of Code/Data */
+comment|/* Video Adaptor */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_CODE_TYPE
-value|0x14
+name|TPL_FUNC_LAN
+value|6
 end_define
 
 begin_comment
-comment|/* Code Type */
+comment|/* LAN Adaptor */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_EXROM_DATA_INDICATOR
-value|0x15
+name|TPL_FUNC_AIMS
+value|7
 end_define
 
 begin_comment
-comment|/* Indicator */
+comment|/* Auto Inclement Mass Strages */
 end_comment
 
 begin_comment
-comment|/* useful macros */
+comment|/* TPL_FUNC_LAN */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ADDR
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|(CARDBUS_CIS_ADDRMASK& (x))
+name|TPL_FUNCE_LAN_TECH
+value|1
 end_define
+
+begin_comment
+comment|/* technology */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_BAR
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|(((CARDBUS_CIS_ASIMASK& (x))-1)*4+0x10)
+name|TPL_FUNCE_LAN_SPEED
+value|2
 end_define
+
+begin_comment
+comment|/* speed */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_CIS_ASI_ROM_IMAGE
-parameter_list|(
-name|x
-parameter_list|)
-define|\
-value|(((x)>> 28)& 0xf)
+name|TPL_FUNCE_LAN_MEDIA
+value|2
 end_define
+
+begin_comment
+comment|/* which media do you use? */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_MAPREG_MEM_ADDR_MASK
-value|0x0ffffff0
+name|TPL_FUNCE_LAN_NID
+value|4
 end_define
+
+begin_comment
+comment|/* node id (address) */
+end_comment
 
 begin_define
 define|#
 directive|define
-name|CARDBUS_MAPREG_MEM_ADDR
-parameter_list|(
-name|mr
-parameter_list|)
-define|\
-value|((mr)& CARDBUS_MAPREG_MEM_ADDR_MASK)
+name|TPL_FUNCE_LAN_CONN
+value|5
 end_define
 
-begin_define
-define|#
-directive|define
-name|CARDBUS_MAPREG_MEM_SIZE
-parameter_list|(
-name|mr
-parameter_list|)
-define|\
-value|(CARDBUS_MAPREG_MEM_ADDR(mr)& -CARDBUS_MAPREG_MEM_ADDR(mr))
-end_define
+begin_comment
+comment|/* connector type (shape) */
+end_comment
 
 end_unit
 
