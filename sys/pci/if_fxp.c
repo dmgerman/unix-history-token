cea83220e870a27f30e806ebbf79bef92e280b24
Warner Losh <imp@FreeBSD.org>
2000-10-22 06:41:46 +0000

    Make usual 1-line cardbus support modification.
    I'm committing this over an Intel PRO-100 CardBus II card.
    
    
    Former-commit-id: 0821803ab5c773c1c26f096a51fa9e564fb9b2e8

--- sys/pci/if_fxp.c
+++ sys/pci/if_fxp.c
@@ -2420,6 +2420,24 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|DRIVER_MODULE
+argument_list|(
+name|if_fxp
+argument_list|,
+name|cardbus
+argument_list|,
+name|fxp_driver
+argument_list|,
+name|fxp_devclass
+argument_list|,
+literal|0
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_comment
 comment|/*  * Do generic parts of attach.  */
 end_comment
