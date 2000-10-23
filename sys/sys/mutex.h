790a208eb1d47475b4facde1d8fda89caae8cb72
Poul-Henning Kamp <phk@FreeBSD.org>
2000-10-23 09:14:20 +0000

    Do not recursively include <sys/proc.h>
    Former-commit-id: bece55ad096e60ed8d632941c3325e343fe0c186

--- sys/sys/mutex.h
+++ sys/sys/mutex.h
@@ -42,16 +42,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/proc.h>
-end_include
-
-begin_comment
-comment|/* Needed for curproc. */
-end_comment
-
-begin_include
-include|#
-directive|include
 file|<machine/atomic.h>
 end_include
 
