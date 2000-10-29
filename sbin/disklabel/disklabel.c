3af42b8be0f26b1224e6f5de26b4d57eb6c0a6b1
Matthew Dillon <dillon@FreeBSD.org>
2000-10-29 01:58:55 +0000

    Implement a new ioctl, DIOCGDVIRGIN, which returns a virgin disklabel for a disk device or slice.
    Fix the disklabel program to use the new ioctl rather then DIOCGDINFO when
    being asked to install a new label with 'auto'.  If the new ioctl fails,
    disklabel will revert to DIOCGDINFO.
    
    Update the disklabel documentation
    
    This allows disklabel to initialize new virgin 'auto' labels on slices.
    
    
    Former-commit-id: c0d1962351d767c12dbbc31f719c8bb7ad151715

--- sbin/disklabel/disklabel.c
+++ sbin/disklabel/disklabel.c
@@ -7071,6 +7071,22 @@ name|NULL
 operator|)
 return|;
 block|}
+comment|/* 	 * Try to use the new get-virgin-label ioctl.  If it fails, 	 * fallback to the old get-disdk-info ioctl. 	 */
+if|if
+condition|(
+name|ioctl
+argument_list|(
+name|f
+argument_list|,
+name|DIOCGDVIRGIN
+argument_list|,
+operator|&
+name|lab
+argument_list|)
+operator|<
+literal|0
+condition|)
+block|{
 if|if
 condition|(
 name|ioctl
@@ -7101,6 +7117,7 @@ operator|(
 name|NULL
 operator|)
 return|;
+block|}
 block|}
 name|close
 argument_list|(
