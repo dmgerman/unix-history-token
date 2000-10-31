e5cc5336f34d259a8d3ffc0038bca47bec153aec
Poul-Henning Kamp <phk@FreeBSD.org>
2000-10-31 10:58:14 +0000

    Deprecate devsw->d_bmaj entirely.
    This removes support for booting current kernels with very old bootblocks.
    
    Device driver writers: Please remove initializations for the d_bmaj
    field in your cdevsw{}.
    
    
    Former-commit-id: 8170f0bbbd7a08ba4b255819802a013d9ac550e2

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -781,8 +781,9 @@ decl_stmt|;
 name|u_int
 name|d_flags
 decl_stmt|;
+comment|/* This following field is deprecated.  Please don't initialize */
 name|int
-name|d_bmaj
+name|d_XXXbmaj
 decl_stmt|;
 block|}
 struct|;
