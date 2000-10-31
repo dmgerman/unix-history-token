c261440c3c28caadd1a2208e4d7bab4226b61723
David E. O'Brien <obrien@FreeBSD.org>
2000-10-31 11:36:33 +0000

    Quiet -Wall.
    Former-commit-id: 82a8726de62cd601f7963315ddd58cf0a5554197

--- gnu/lib/libg2c/MAIN.c
+++ gnu/lib/libg2c/MAIN.c
@@ -1,12 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_macro
-name|MAIN__
-argument_list|()
-end_macro
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
 
-begin_block
-block|{}
-end_block
+begin_function
+name|int
+name|MAIN__
+parameter_list|()
+block|{
+return|return
+literal|0
+return|;
+block|}
+end_function
 
 end_unit
 
