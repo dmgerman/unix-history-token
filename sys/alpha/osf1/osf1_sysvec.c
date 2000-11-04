942284ab5dc83f1ef639e6968211ce7558fd822a
David E. O'Brien <obrien@FreeBSD.org>
2000-11-04 07:35:46 +0000

    MFC: declare a module version so the `linux' module has a way to depend on      the osf1 module.
    Approved by:	JKH
    
    
    Former-commit-id: 53440745cef3ebcd6a8a2f84fff3116f965a30ec

--- sys/alpha/osf1/osf1_sysvec.c
+++ sys/alpha/osf1/osf1_sysvec.c
@@ -108,6 +108,16 @@ directive|include
 file|<alpha/osf1/osf1.h>
 end_include
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|osf1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_decl_stmt
 name|int
 name|osf1_szsigcode
