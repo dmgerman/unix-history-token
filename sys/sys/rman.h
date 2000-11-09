7a9a4ea18c02a00adb9a33f9259c705b0bd5740d
Garrett Wollman <wollman@FreeBSD.org>
2000-11-09 15:42:05 +0000

    Fix a reference to ``Standard C'' to refer specifically to the 1990 version; the requirement it describes is not in C99.
    Former-commit-id: 4847e4ac7212af19eb92113c2c3f31b4f6f11e97

--- sys/sys/rman.h
+++ sys/sys/rman.h
@@ -249,7 +249,7 @@ name|_KERNEL
 end_ifdef
 
 begin_comment
-comment|/*  * We use a linked list rather than a bitmap because we need to be able to  * represent potentially huge objects (like all of a processor's physical  * address space).  That is also why the indices are defined to have type  * `unsigned long' -- that being the largest integral type in Standard C.  */
+comment|/*  * We use a linked list rather than a bitmap because we need to be able to  * represent potentially huge objects (like all of a processor's physical  * address space).  That is also why the indices are defined to have type  * `unsigned long' -- that being the largest integral type in ISO C (1990).  * The 1999 version of C allows `long long'; we may need to switch to that  * at some point in the future, particularly if we want to support 36-bit  * addresses on IA32 hardware.  */
 end_comment
 
 begin_expr_stmt
