937f6b18e46a21114eac9ad104d5075e0b117f2a
John Baldwin <jhb@FreeBSD.org>
2000-11-10 21:52:04 +0000

    Fix SMP kernel compiles by #include'ing machine/globals.h to get the cpuid variable.
    Former-commit-id: be88d68a08be362934f85dafebfebb28267f56ef

--- sys/kern/kern_ktr.c
+++ sys/kern/kern_ktr.c
@@ -58,6 +58,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/globals.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/stdarg.h>
 end_include
 
