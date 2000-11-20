286ace0096c2649a67b198c6e40def6b845ae22b
Poul-Henning Kamp <phk@FreeBSD.org>
2000-11-20 22:00:25 +0000

    Make programs which still #include <machine/{mouse,console}.h> fail at compiletime, with an explanatory error message.  Previously they would only get a warning.
    These files will be finally removed 2001-01-15
    
    
    Former-commit-id: c24540841807497f81aa8e87afba988b19a2662f

--- sys/i386/include/console.h
+++ sys/i386/include/console.h
@@ -3,61 +3,22 @@ begin_comment
 comment|/* $FreeBSD$ */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|_MACHINE_CONSOLE_H_
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|_MACHINE_CONSOLE_H_
-end_define
-
 begin_if
 if|#
 directive|if
 name|__GNUC__
 end_if
 
-begin_warning
-warning|#
-directive|warning
+begin_error
+error|#
+directive|error
 literal|"this file includes<machine/console.h> which is deprecated, use<sys/{kb,cons,fb}io.h> instead"
-end_warning
+end_error
 
 begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_include
-include|#
-directive|include
-file|<sys/fbio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/kbio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/consio.h>
-end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* !_MACHINE_CONSOLE_H_ */
-end_comment
 
 end_unit
 
--- sys/i386/include/mouse.h
+++ sys/i386/include/mouse.h
@@ -9,22 +9,16 @@ directive|if
 name|__GNUC__
 end_if
 
-begin_warning
-warning|#
-directive|warning
+begin_error
+error|#
+directive|error
 literal|"this file includes<machine/mouse.h> which is deprecated, use<sys/mouse.h> instead"
-end_warning
+end_error
 
 begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_include
-include|#
-directive|include
-file|<sys/mouse.h>
-end_include
 
 end_unit
 
