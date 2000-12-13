cddb122a52d9d9a58a2e37074593a3de29ca6a5f
Warner Losh <imp@FreeBSD.org>
2000-12-13 06:25:22 +0000

    Remove unnecessary includes found by phk's script a long time ago.
    Former-commit-id: 9743191274e1801ada297df5bb7115ebf10d9978

--- sys/dev/aha/aha.c
+++ sys/dev/aha/aha.c
@@ -78,18 +78,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<vm/vm.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<vm/pmap.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<dev/aha/ahareg.h>
 end_include
 
