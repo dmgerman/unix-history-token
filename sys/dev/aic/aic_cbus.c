a7f5c927af96cc8f2eb589d7f143e73a06881057
Warner Losh <imp@FreeBSD.org>
2000-12-13 05:46:23 +0000

    Add module dependencies on CAM module.
    Submitted by:	Michael Reifenberger
    
    
    Former-commit-id: 0fb23dc155961772295b69d226fa0fe6dcc573f1

--- sys/dev/aic/aic_cbus.c
+++ sys/dev/aic/aic_cbus.c
@@ -1430,6 +1430,22 @@ decl_stmt|;
 end_decl_stmt
 
 begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|aic
+argument_list|,
+name|cam
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|DRIVER_MODULE
 argument_list|(
 name|aic
