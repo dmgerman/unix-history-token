377ca0c1677d086df51294babbf26a7b7bb8e66c
MIHIRA Sanpei Yoshiro <sanpei@FreeBSD.org>
2000-12-23 14:09:25 +0000

    [MFC]: add PNPID for PnP PCMCIA, SCM SwapBox Classic X2P.
    Former-commit-id: 249e88e96bc310818e014275ee06855d73466d33

--- sys/dev/pcic/i82365reg.h
+++ sys/dev/pcic/i82365reg.h
@@ -1786,5 +1786,16 @@ begin_comment
 comment|/* SCM0469 */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|PCIC_PNP_SCM_SWAPBOX2
+value|0x69046d4c
+end_define
+
+begin_comment
+comment|/* SCM SwapBox Classic X2P */
+end_comment
+
 end_unit
 
