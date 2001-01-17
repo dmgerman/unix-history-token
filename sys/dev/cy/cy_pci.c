d5814b045f2a247bf7a1a39a86c2249c97838f85
Peter Wemm <peter@FreeBSD.org>
2001-01-17 00:47:46 +0000

    Special case for compiling LINT - just give a warning and continue on. At least we have a chance at getting test compile coverage for the rest of the kernel now.
    Former-commit-id: a3b762b1e1d4b87e097183833f4fc8d226f20ee8

--- sys/dev/cy/cy_pci.c
+++ sys/dev/cy/cy_pci.c
@@ -3,6 +3,23 @@ begin_comment
 comment|/*  * Copyright (c) 1996, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice unmodified, this list of conditions, and the following  *    disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|COMPILING_LINT
+end_ifdef
+
+begin_warning
+warning|#
+directive|warning
+literal|"The cy pci driver is broken and is not compiled with LINT"
+end_warning
+
+begin_else
+else|#
+directive|else
+end_else
+
 begin_comment
 comment|/*  * Cyclades Y PCI serial interface driver  */
 end_comment
@@ -486,6 +503,11 @@ comment|/* XXX should release any allocated virtual memory */
 return|return;
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
