39f451b7f2a6f64004c1caa84786976f8755e073
Peter Wemm <peter@FreeBSD.org>
2001-01-21 07:54:10 +0000

    Remove APIC_INTR_DIAGNOSTIC - this has been disabled for some time now. Remove some leftovers of removed SMP options.
    Former-commit-id: 6dd1565522ece8b85c16f854e66cea32273b6881

--- sys/i386/include/smptests.h
+++ sys/i386/include/smptests.h
@@ -45,18 +45,6 @@ directive|define
 name|PUSHDOWN_LEVEL_2
 end_define
 
-begin_define
-define|#
-directive|define
-name|PUSHDOWN_LEVEL_3_NOT
-end_define
-
-begin_define
-define|#
-directive|define
-name|PUSHDOWN_LEVEL_4_NOT
-end_define
-
 begin_comment
 comment|/*  * Debug version of simple_lock.  This will store the CPU id of the  * holding CPU along with the lock.  When a CPU fails to get the lock  * it compares its own id to the holder id.  If they are the same it  * panic()s, as simple locks are binary, and this would cause a deadlock.  *  */
 end_comment
@@ -110,70 +98,6 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * INTR_SIMPLELOCK has been removed, as the interrupt mechanism will likely  * not use this sort of optimization if we move to interrupt threads.  */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|PUSHDOWN_LEVEL_4
-end_ifdef
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/*  * CPL_AND_CML has been removed.  Interrupt threads will eventually not  * use either mechanism so there is no point trying to optimize it.  */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|PUSHDOWN_LEVEL_3
-end_ifdef
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/*  * SPL_DEBUG_POSTCODE/INTR_SPL/SPL_DEBUG - removed  *  * These functions were too expensive for the standard case but, more   * importantly, we should be able to come up with a much cleaner way  * to handle the cpl.  Having to do any locking at all is a mistake  * for something that is modified as often as cpl is.  */
-end_comment
-
-begin_comment
-comment|/*  * FAST_WITHOUTCPL - now made the default (define removed).  Text below   * contains the current discussion.  I am confident we can find a solution  * that does not require us to process softints from a hard int, which can  * kill serial performance due to the lack of true hardware ipl's.  *  ****  *  * Ignore the ipending bits when exiting FAST_INTR() routines.  *  * according to Bruce:  *  * setsoft*() may set ipending.  setsofttty() is actually used in the  * FAST_INTR handler in some serial drivers.  This is necessary to get  * output completions and other urgent events handled as soon as possible.  * The flag(s) could be set in a variable other than ipending, but they  * needs to be checked against cpl to decide whether the software interrupt  * handler can/should run.  *  *  (FAST_INTR used to just return  * in all cases until rev.1.7 of vector.s.  This worked OK provided there  * were no user-mode CPU hogs.  CPU hogs caused an average latency of 1/2  * clock tick for output completions...)  ***  *  * So I need to restore cpl handling someday, but AFTER  *  I finish making spl/cpl MP-safe.  */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|PUSHDOWN_LEVEL_1
-end_ifdef
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/*  * FAST_SIMPLELOCK no longer exists, because it doesn't help us.  The cpu  * is likely to already hold the MP lock and recursive MP locks are now  * very cheap, so we do not need this optimization.  Eventually *ALL*   * interrupts will run in their own thread, so there is no sense complicating  * matters now.  */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|PUSHDOWN_LEVEL_1
-end_ifdef
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
 comment|/*  * Portions of the old TEST_LOPRIO code, back from the grave!  */
 end_comment
 
@@ -214,38 +138,6 @@ ifdef|#
 directive|ifdef
 name|APIC_IO
 end_ifdef
-
-begin_comment
-comment|/*  * Enable extra counters for some selected locations in the interrupt handlers.  * Look in apic_vector.s, apic_ipl.s and ipl.s for APIC_ITRACE or   * APIC_INTR_DIAGNOSTIC.  */
-end_comment
-
-begin_undef
-undef|#
-directive|undef
-name|APIC_INTR_DIAGNOSTIC
-end_undef
-
-begin_comment
-comment|/*  * Add extra tracking of a specific interrupt. Look in apic_vector.s,   * apic_ipl.s and ipl.s for APIC_ITRACE and log_intr_event.  * APIC_INTR_DIAGNOSTIC must be defined for this to work.  */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|APIC_INTR_DIAGNOSTIC
-end_ifdef
-
-begin_define
-define|#
-directive|define
-name|APIC_INTR_DIAGNOSTIC_IRQ
-value|17
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/*  * Don't assume that slow interrupt handler X is called from vector  * X + ICU_OFFSET.  */
