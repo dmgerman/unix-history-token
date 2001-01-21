3350f3cd26a91fa9e3bcbcba0c03179e55546acc
Jake Burkholder <jake@FreeBSD.org>
2001-01-21 03:11:03 +0000

    Implement optimized PCPU accessors.  These are necessary to support a pre-emptable kernel.  For variables of size 4 bytes or less they compile to a single instruction, which does not allow a process to migrate cpus in the middle, and get the value for the "wrong" cpu.
    Former-commit-id: e0f0eae045e06addffb37d9b5d0335a32ff52f86

--- sys/i386/include/globals.h
+++ sys/i386/include/globals.h
@@ -27,71 +27,95 @@ directive|include
 file|<machine/globaldata.h>
 end_include
 
-begin_expr_stmt
-specifier|static
-name|__inline
-expr|struct
-name|globaldata
-operator|*
-name|_global_globaldata
-argument_list|(
-argument|void
-argument_list|)
-block|{ 	struct
-name|globaldata
-operator|*
-name|gd
-block|;
-name|int
-name|offset
-block|;
-name|offset
-operator|=
-name|offsetof
-argument_list|(
-expr|struct
-name|globaldata
-argument_list|,
-name|gd_prvspace
-argument_list|)
-block|;
-asm|__asm __volatile("movl %%fs:%1,%0"
-operator|:
-literal|"=r"
-operator|(
-name|gd
-operator|)
-operator|:
-literal|"m"
-operator|(
-operator|*
-operator|(
-expr|struct
-name|globaldata
-operator|*
-operator|)
-operator|(
-name|offset
-operator|)
-operator|)
-block|)
-expr_stmt|;
-end_expr_stmt
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__GNUC__
+end_ifndef
 
-begin_return
-return|return
-operator|(
-name|gd
-operator|)
-return|;
-end_return
+begin_error
+error|#
+directive|error
+error|gcc is required to use this file
+end_error
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/*  * Evaluates to the byte offset of the per-cpu variable name.  */
+end_comment
 
 begin_define
-unit|}
 define|#
 directive|define
-name|GLOBALDATA
-value|(_global_globaldata())
+name|__pcpu_offset
+parameter_list|(
+name|name
+parameter_list|)
+define|\
+value|__offsetof(struct globaldata, name)
+end_define
+
+begin_comment
+comment|/*  * Evaluates to the type of the per-cpu variable name.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__pcpu_type
+parameter_list|(
+name|name
+parameter_list|)
+define|\
+value|__typeof(((struct globaldata *)0)->name)
+end_define
+
+begin_comment
+comment|/*  * Evaluates to the address of the per-cpu variable name.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__PCPU_PTR
+parameter_list|(
+name|name
+parameter_list|)
+value|({						\ 	__pcpu_type(name) *__p;						\ 									\ 	__asm __volatile("movl %%fs:%1,%0; addl %2,%0"			\ 	    : "=r" (__p)						\ 	    : "m" (*(struct globaldata *)(__pcpu_offset(gd_prvspace))),	\ 	      "i" (__pcpu_offset(name)));				\ 									\ 	__p;								\ })
+end_define
+
+begin_comment
+comment|/*  * Evaluates to the value of the per-cpu variable name.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__PCPU_GET
+parameter_list|(
+name|name
+parameter_list|)
+value|({						\ 	__pcpu_type(name) __result;					\ 									\ 	if (sizeof(__result) == 1) {					\ 		u_char __b;						\ 		__asm __volatile("movb %%fs:%1,%0"			\ 		    : "=r" (__b)					\ 		    : "m" (*(u_char *)(__pcpu_offset(name))));		\ 		__result = *(__pcpu_type(name) *)&__b;			\ 	} else if (sizeof(__result) == 2) {				\ 		u_short __w;						\ 		__asm __volatile("movw %%fs:%1,%0"			\ 		    : "=r" (__w)					\ 		    : "m" (*(u_short *)(__pcpu_offset(name))));		\ 		__result = *(__pcpu_type(name) *)&__w;			\ 	} else if (sizeof(__result) == 4) {				\ 		u_int __i;						\ 		__asm __volatile("movl %%fs:%1,%0"			\ 		    : "=r" (__i)					\ 		    : "m" (*(u_int *)(__pcpu_offset(name))));		\ 		__result = *(__pcpu_type(name) *)&__i;			\ 	} else {							\ 		__result = *__PCPU_PTR(name);				\ 	}								\ 									\ 	__result;							\ })
+end_define
+
+begin_comment
+comment|/*  * Sets the value of the per-cpu variable name to value val.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__PCPU_SET
+parameter_list|(
+name|name
+parameter_list|,
+name|val
+parameter_list|)
+value|({					\ 	__pcpu_type(name) __val = (val);				\ 									\ 	if (sizeof(__val) == 1) {					\ 		u_char __b;						\ 		__b = *(u_char *)&__val;				\ 		__asm __volatile("movb %1,%%fs:%0"			\ 		    : "=m" (*(u_char *)(__pcpu_offset(name)))		\ 		    : "r" (__b));					\ 	} else if (sizeof(__val) == 2) {				\ 		u_short __w;						\ 		__w = *(u_short *)&__val;				\ 		__asm __volatile("movw %1,%%fs:%0"			\ 		    : "=m" (*(u_short *)(__pcpu_offset(name)))		\ 		    : "r" (__w));					\ 	} else if (sizeof(__val) == 4) {				\ 		u_int __i;						\ 		__i = *(u_int *)&__val;					\ 		__asm __volatile("movl %1,%%fs:%0"			\ 		    : "=m" (*(u_int *)(__pcpu_offset(name)))		\ 		    : "r" (__i));					\ 	} else {							\ 		*__PCPU_PTR(name) = __val;				\ 	}								\ })
 end_define
 
 begin_define
@@ -101,7 +125,7 @@ name|PCPU_GET
 parameter_list|(
 name|member
 parameter_list|)
-value|(GLOBALDATA->gd_ ## member)
+value|__PCPU_GET(gd_ ## member)
 end_define
 
 begin_define
@@ -111,7 +135,7 @@ name|PCPU_PTR
 parameter_list|(
 name|member
 parameter_list|)
-value|(&GLOBALDATA->gd_ ## member)
+value|__PCPU_PTR(gd_ ## member)
 end_define
 
 begin_define
@@ -123,7 +147,7 @@ name|member
 parameter_list|,
 name|val
 parameter_list|)
-value|(GLOBALDATA->gd_ ## member = (val))
+value|__PCPU_SET(gd_ ## member, val)
 end_define
 
 begin_define
@@ -138,6 +162,13 @@ define|#
 directive|define
 name|CURTHD
 value|PCPU_GET(curproc)
+end_define
+
+begin_define
+define|#
+directive|define
+name|GLOBALDATA
+value|PCPU_GET(prvspace)
 end_define
 
 begin_define
