b36a938826e3d81745efd0b73d673ba04b128523
Jeroen Ruigrok/Asmodai <asmodai@FreeBSD.org>
2001-01-23 13:02:46 +0000

    MFC:	Reparent a kernel thread to init during kthread_exit() so that the 	zombie can be reaped.
    	This MFC also includes an #include to <sys/ptrace.h> to avoid
    	the implicit declaration of proc_reparent().  It might be that
    	CURRENT at this point has different header files due to SMPng.
    
    
    Former-commit-id: 5223a15c1c7912a056d04a54985b25b771aeef5f

--- sys/kern/kern_kthread.c
+++ sys/kern/kern_kthread.c
@@ -30,6 +30,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/ptrace.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/resourcevar.h>
 end_include
 
@@ -315,6 +321,13 @@ name|int
 name|ecode
 parameter_list|)
 block|{
+name|proc_reparent
+argument_list|(
+name|curproc
+argument_list|,
+name|initproc
+argument_list|)
+expr_stmt|;
 name|exit1
 argument_list|(
 name|curproc
