edb245ba24724fe88eecd912456811738475144f
John Baldwin <jhb@FreeBSD.org>
2001-01-23 22:18:41 +0000

    Tag unused functions with __unused.
    Former-commit-id: c90e184be330d93995a996282abff291b3107b24

--- sys/dev/sound/isa/opl.c
+++ sys/dev/sound/isa/opl.c
@@ -2230,16 +2230,17 @@ begin_comment
 comment|/* These functions are local. */
 end_comment
 
-begin_function_decl
+begin_decl_stmt
 specifier|static
 name|void
 name|opl_startplay
-parameter_list|(
+argument_list|(
 name|sc_p
 name|scp
-parameter_list|)
-function_decl|;
-end_function_decl
+argument_list|)
+name|__unused
+decl_stmt|;
+end_decl_stmt
 
 begin_function_decl
 specifier|static
