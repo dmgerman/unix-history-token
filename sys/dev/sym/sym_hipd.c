c2bbbe8eafe046a0d689d557617390aea3308905
Gerard Roudier <groudier@FreeBSD.org>
2001-02-03 16:16:27 +0000

    MFC: from sym_hipd.c revision 1.23
    Former-commit-id: bb356af6e1f583735f004b88aee9a76e3d5f0e34

--- sys/dev/sym/sym_hipd.c
+++ sys/dev/sym/sym_hipd.c
@@ -7684,8 +7684,11 @@ name|device_id
 operator|==
 name|PCI_ID_LSI53C1010_2
 operator|&&
-comment|/* np->revision_id< 0xff */
-literal|1
+name|np
+operator|->
+name|revision_id
+operator|<
+literal|0x1
 operator|&&
 name|np
 operator|->
@@ -11580,8 +11583,11 @@ name|device_id
 operator|==
 name|PCI_ID_LSI53C1010
 operator|&&
-comment|/* np->revision_id< 0xff */
-literal|1
+name|np
+operator|->
+name|revision_id
+operator|<
+literal|0x2
 condition|)
 name|np
 operator|->
@@ -11981,12 +11987,15 @@ condition|)
 block|{
 if|if
 condition|(
+operator|!
+operator|(
 name|INB
 argument_list|(
 name|nc_gpreg
 argument_list|)
 operator|&
 literal|0x08
+operator|)
 condition|)
 name|np
 operator|->
