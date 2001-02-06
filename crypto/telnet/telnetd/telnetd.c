d8b9f6d799fbec0f42d8d6544f1d14e6fe8ae8d4
Jeroen Ruigrok/Asmodai <asmodai@FreeBSD.org>
2001-02-06 09:32:26 +0000

    Fix typo: wierd -> weird.
    There is no such thing as wierd in the english language.
    
    
    Former-commit-id: abc963bf7e93835926c75a7c89cb1d1ca31d07a8

--- crypto/telnet/telnetd/telnetd.c
+++ crypto/telnet/telnetd/telnetd.c
@@ -432,7 +432,7 @@ file|<sys/ptyvar.h>
 end_include
 
 begin_comment
-comment|/*  * Because of the way ptyibuf is used with streams messages, we need  * ptyibuf+1 to be on a full-word boundary.  The following wierdness  * is simply to make that happen.  */
+comment|/*  * Because of the way ptyibuf is used with streams messages, we need  * ptyibuf+1 to be on a full-word boundary.  The following weirdness  * is simply to make that happen.  */
 end_comment
 
 begin_decl_stmt
