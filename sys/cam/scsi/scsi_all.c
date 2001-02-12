80160ee7b530da40a347db7e76188fe7d18a3124
Matthew Jacob <mjacob@FreeBSD.org>
2001-02-12 01:28:30 +0000

    MFC: fixing SSD_KEY_RECOVERED_ERROR to be the non-error it is and making MFC: sure we're chatty on ABORTED COMMAND errors.
    Former-commit-id: f50a2804c76e53e2546aa0221c9703d72fcd993c

--- sys/cam/scsi/scsi_all.c
+++ sys/cam/scsi/scsi_all.c
@@ -10638,14 +10638,6 @@ comment|/* FALLTHROUGH */
 case|case
 name|SSD_KEY_MISCOMPARE
 case|:
-name|print_sense
-operator|=
-name|FALSE
-expr_stmt|;
-comment|/* FALLTHROUGH */
-case|case
-name|SSD_KEY_RECOVERED_ERROR
-case|:
 comment|/* decrement the number of retries */
 name|retry
 operator|=
@@ -10661,6 +10653,11 @@ if|if
 condition|(
 name|retry
 condition|)
+block|{
+name|error
+operator|=
+name|ERESTART
+expr_stmt|;
 name|ccb
 operator|->
 name|ccb_h
@@ -10668,10 +10665,22 @@ operator|.
 name|retry_count
 operator|--
 expr_stmt|;
+block|}
+else|else
+block|{
+name|error
+operator|=
+name|EIO
+expr_stmt|;
+block|}
+case|case
+name|SSD_KEY_RECOVERED_ERROR
+case|:
 name|error
 operator|=
 literal|0
 expr_stmt|;
+comment|/* not an error */
 break|break;
 case|case
 name|SSD_KEY_ILLEGAL_REQUEST
@@ -10883,6 +10892,9 @@ expr_stmt|;
 block|}
 block|}
 break|break;
+case|case
+name|SSD_KEY_ABORTED_COMMAND
+case|:
 default|default:
 comment|/* decrement the number of retries */
 name|retry
@@ -10951,6 +10963,17 @@ operator|&
 name|SS_ERRMASK
 expr_stmt|;
 block|}
+comment|/* 			 * Make sure ABORTED COMMAND errors get 			 * printed as they're indicative of marginal 			 * SCSI busses that people should address. 			 */
+if|if
+condition|(
+name|sense_key
+operator|==
+name|SSD_KEY_ABORTED_COMMAND
+condition|)
+name|print_sense
+operator|=
+name|TRUE
+expr_stmt|;
 block|}
 break|break;
 block|}
