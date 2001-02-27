5588b9e064423145f7358a06a61c71359a6286a0
Andrey Chernov <ache@FreeBSD.org>
2001-02-27 14:42:19 +0000

    Use formula with better random distribution for rand()
    Even better formula from random() could not be intetgrated because rand_r()
    supposed to store its state in the single variable (but table needed for
    random() algorithm integration).
    
    
    Former-commit-id: d35c375a439e1127856ca3aaefa1087a70600ce2

--- lib/libc/stdlib/rand.c
+++ lib/libc/stdlib/rand.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * Posix rand_r function added May 1999 by Wes Peters<wes@softweyr.com>.  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * Posix rand_r function added May 1999 by Wes Peters<wes@softweyr.com>.  *  * $FreeBSD$  */
 end_comment
 
 begin_if
@@ -81,6 +81,10 @@ modifier|*
 name|ctx
 parameter_list|)
 block|{
+ifdef|#
+directive|ifdef
+name|USE_WEAK_SEEDING
+comment|/*  * Historic implementation compatibility.  * The random sequences do not vary much with the seed,  * even with overflowing.  */
 return|return
 operator|(
 operator|(
@@ -105,6 +109,73 @@ literal|1
 operator|)
 operator|)
 return|;
+else|#
+directive|else
+comment|/* !USE_WEAK_SEEDING */
+comment|/*  * Compute x = (7^5 * x) mod (2^31 - 1)  * wihout overflowing 31 bits:  *      (2^31 - 1) = 127773 * (7^5) + 2836  * From "Random number generators: good ones are hard to find",  * Park and Miller, Communications of the ACM, vol. 31, no. 10,  * October 1988, p. 1195.  */
+name|long
+name|hi
+decl_stmt|,
+name|lo
+decl_stmt|,
+name|x
+decl_stmt|;
+name|hi
+operator|=
+operator|*
+name|ctx
+operator|/
+literal|127773
+expr_stmt|;
+name|lo
+operator|=
+operator|*
+name|ctx
+operator|%
+literal|127773
+expr_stmt|;
+name|x
+operator|=
+literal|16807
+operator|*
+name|lo
+operator|-
+literal|2836
+operator|*
+name|hi
+expr_stmt|;
+if|if
+condition|(
+name|x
+operator|<=
+literal|0
+condition|)
+name|x
+operator|+=
+literal|0x7fffffff
+expr_stmt|;
+return|return
+operator|(
+operator|(
+operator|*
+name|ctx
+operator|=
+name|x
+operator|)
+operator|%
+operator|(
+operator|(
+name|u_long
+operator|)
+name|RAND_MAX
+operator|+
+literal|1
+operator|)
+operator|)
+return|;
+endif|#
+directive|endif
+comment|/* !USE_WEAK_SEEDING */
 block|}
 end_function
 
