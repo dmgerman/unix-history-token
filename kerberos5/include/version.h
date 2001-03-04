d01292e0f84771621fc6aa47a7a4aa41fd0799fc
Mark Murray <markm@FreeBSD.org>
2001-03-04 13:50:12 +0000

    MFC: Heimdal 0.3e build stuff.
    Former-commit-id: c646f4c28f5b7b1d4170e482525a6404fd0254ac

--- kerberos5/include/version.h
+++ kerberos5/include/version.h
@@ -4,38 +4,42 @@ comment|/* $FreeBSD$ */
 end_comment
 
 begin_decl_stmt
+specifier|const
 name|char
 modifier|*
 name|krb4_long_version
 init|=
-literal|"@(#)$Version: krb4-1.0 by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 19:34:57 SAST 2000 $"
+literal|"@(#)$Version: krb4-1.0.5 (FreeBSD) $"
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+specifier|const
 name|char
 modifier|*
 name|krb4_version
 init|=
-literal|"krb4-1.0"
+literal|"krb4-1.0.5"
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+specifier|const
 name|char
 modifier|*
 name|heimdal_long_version
 init|=
-literal|"@(#)$Version: heimdal-0.2o by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 20:56:02 SAST 2000 $"
+literal|"@(#)$Version: heimdal-0.3e (FreeBSD) $"
 decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
+specifier|const
 name|char
 modifier|*
 name|heimdal_version
 init|=
-literal|"heimdal-0.2o"
+literal|"heimdal-0.3e"
 decl_stmt|;
 end_decl_stmt
 
