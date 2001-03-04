920489d583d01cd8c02d8f7567201cfd4088218f
Kris Kennaway <kris@FreeBSD.org>
2001-03-04 08:32:34 +0000

    MFC: r1.32, 1.33: remove extraneous extern, update includes
    Former-commit-id: 055a8660db1f4ea09fee66b8ab95291f035c6cce

--- usr.bin/ftp/ftp.c
+++ usr.bin/ftp/ftp.c
@@ -86,6 +86,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/time.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<netinet/in.h>
 end_include
 
@@ -222,13 +228,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|h_errno
-decl_stmt|;
-end_decl_stmt
 
 begin_decl_stmt
 name|int
