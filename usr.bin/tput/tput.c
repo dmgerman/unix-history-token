7f55ffde0a974ac365a61f1eb747172303b5164a
Kris Kennaway <kris@FreeBSD.org>
2001-03-04 09:17:45 +0000

    MFC: r1.8: remove extraneous externs
    Former-commit-id: 82c662592a5779b965259987e7b741aa9e6c17ab

--- usr.bin/tput/tput.c
+++ usr.bin/tput/tput.c
@@ -34,13 +34,25 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static char sccsid[] = "@(#)tput.c	8.2 (Berkeley) 3/19/94";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)tput.c	8.2 (Berkeley) 3/19/94"
+literal|"$FreeBSD$"
 decl_stmt|;
 end_decl_stmt
 
@@ -169,15 +181,6 @@ modifier|*
 name|argv
 decl_stmt|;
 block|{
-specifier|extern
-name|char
-modifier|*
-name|optarg
-decl_stmt|;
-specifier|extern
-name|int
-name|optind
-decl_stmt|;
 name|int
 name|ch
 decl_stmt|,
