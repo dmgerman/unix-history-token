38225734a053965a8fd239af38ee12931a416684
Kris Kennaway <kris@FreeBSD.org>
2001-03-04 08:54:10 +0000

    MFC: r1.14, r1.15: remove extraneous externs and correct typo in comment
    Former-commit-id: 97905c5197155f4b2adbd62a11647f1cd548766e

--- usr.bin/nm/nm.c
+++ usr.bin/nm/nm.c
@@ -376,10 +376,6 @@ modifier|*
 name|argv
 decl_stmt|;
 block|{
-specifier|extern
-name|int
-name|optind
-decl_stmt|;
 name|int
 name|ch
 decl_stmt|,
@@ -2232,7 +2228,7 @@ argument_list|,
 name|objname
 argument_list|)
 expr_stmt|;
-comment|/* 	 * handle undefined-only format seperately (no space is 	 * left for symbol values, no type field is printed) 	 */
+comment|/* 	 * handle undefined-only format separately (no space is 	 * left for symbol values, no type field is printed) 	 */
 if|if
 condition|(
 name|print_only_undefined_symbols
