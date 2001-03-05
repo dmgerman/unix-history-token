325b45d59c25d834a3cb4d60ba59392c3f737263
Kris Kennaway <kris@FreeBSD.org>
2001-03-05 11:35:42 +0000

    MFC: 1.14, 1.15: strcpy() -> strlcpy(), fix typo
    Former-commit-id: 43720cc788eaf0810d89a513ae9b860e4f6fe698

--- crypto/telnet/telnetd/telnetd.c
+++ crypto/telnet/telnetd/telnetd.c
@@ -432,7 +432,7 @@ file|<sys/ptyvar.h>
 end_include
 
 begin_comment
-comment|/*  * Because of the way ptyibuf is used with streams messages, we need  * ptyibuf+1 to be on a full-word boundary.  The following wierdness  * is simply to make that happen.  */
+comment|/*  * Because of the way ptyibuf is used with streams messages, we need  * ptyibuf+1 to be on a full-word boundary.  The following weirdness  * is simply to make that happen.  */
 end_comment
 
 begin_decl_stmt
@@ -3824,11 +3824,16 @@ operator|>=
 literal|0
 condition|)
 block|{
-name|strcpy
+name|strlcpy
 argument_list|(
 name|line
 argument_list|,
 name|lp
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|line
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|line
@@ -4929,11 +4934,16 @@ condition|)
 operator|(
 name|void
 operator|)
-name|strcpy
+name|strlcpy
 argument_list|(
 name|host_name
 argument_list|,
 name|HN
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|host_name
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
