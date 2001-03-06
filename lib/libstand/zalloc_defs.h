d035a00ed2b6d5fb460cfa0df387e4908efe3778
Doug Rabson <dfr@FreeBSD.org>
2001-03-06 16:11:36 +0000

    A quick and dirty port of libstand to ia64.
    Former-commit-id: 71624a2de6afd38adb086be8d32fdf699a94e916

--- lib/libstand/zalloc_defs.h
+++ lib/libstand/zalloc_defs.h
@@ -168,6 +168,40 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__ia64__
+end_ifdef
+
+begin_typedef
+typedef|typedef
+name|unsigned
+name|long
+name|iaddr_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* unsigned int same size as pointer	*/
+end_comment
+
+begin_typedef
+typedef|typedef
+name|long
+name|saddr_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* signed int same size as pointer	*/
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
