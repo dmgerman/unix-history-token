8681cba7a1a454cc7eb01a3a2abbc7b857db6bb7
John Baldwin <jhb@FreeBSD.org>
2001-03-07 05:29:21 +0000

    Unrevert the pmap_map() changes.  They weren't broken on x86.
    Sense beaten into me by:	peter
    
    
    Former-commit-id: ef2b7b57cd91b89be19f217eca51988391df30e3

--- sys/i386/i386/pmap.c
+++ sys/i386/i386/pmap.c
@@ -2726,7 +2726,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  *	Used to map a range of physical addresses into kernel  *	virtual address space.  *  *	For now, VM is already on, we only need to map the  *	specified memory.  */
+comment|/*  *	Used to map a range of physical addresses into kernel  *	virtual address space.  *  *	The value passed in '*virt' is a suggested virtual address for  *	the mapping. Architectures which can support a direct-mapped  *	physical to virtual region can return the appropriate address  *	within that region, leaving '*virt' unchanged. Other  *	architectures should map the pages starting at '*virt' and  *	update '*virt' with the first usable address after the mapped  *	region.  */
 end_comment
 
 begin_function
@@ -2742,6 +2742,7 @@ parameter_list|,
 name|prot
 parameter_list|)
 name|vm_offset_t
+modifier|*
 name|virt
 decl_stmt|;
 name|vm_offset_t
@@ -2754,6 +2755,17 @@ name|int
 name|prot
 decl_stmt|;
 block|{
+name|vm_offset_t
+name|sva
+init|=
+operator|*
+name|virt
+decl_stmt|;
+name|vm_offset_t
+name|va
+init|=
+name|sva
+decl_stmt|;
 while|while
 condition|(
 name|start
@@ -2763,12 +2775,12 @@ condition|)
 block|{
 name|pmap_kenter
 argument_list|(
-name|virt
+name|va
 argument_list|,
 name|start
 argument_list|)
 expr_stmt|;
-name|virt
+name|va
 operator|+=
 name|PAGE_SIZE
 expr_stmt|;
@@ -2777,9 +2789,14 @@ operator|+=
 name|PAGE_SIZE
 expr_stmt|;
 block|}
+operator|*
+name|virt
+operator|=
+name|va
+expr_stmt|;
 return|return
 operator|(
-name|virt
+name|sva
 operator|)
 return|;
 block|}
