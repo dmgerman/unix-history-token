758732649b8d3f2022ec972a42510e70ee07b77b
Doug Rabson <dfr@FreeBSD.org>
2001-03-09 13:44:53 +0000

    Adjust a comment slightly.
    Former-commit-id: 559c70f141a83c63b578b329c7711bbc0f38b547

--- sys/ia64/ia64/machdep.c
+++ sys/ia64/ia64/machdep.c
@@ -2448,7 +2448,7 @@ literal|1
 expr_stmt|;
 comment|/* 0 used for proc0 pmap */
 block|}
-comment|/* 	 * Initialize the virtual memory system, and set the 	 * page table base register in proc 0's PCB. 	 */
+comment|/* 	 * Initialize the virtual memory system. 	 */
 name|pmap_bootstrap
 argument_list|()
 expr_stmt|;
