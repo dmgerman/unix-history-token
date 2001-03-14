09ce44b5ff507e1226137de98112310c8bddf3fb
Philippe Charnier <charnier@FreeBSD.org>
2001-03-14 19:39:29 +0000

    Typo. Move FreeBSD Id out of copyright section.
    Former-commit-id: c44ddb5a5e2c91e20993826d8df0609610cdc4fd

--- usr.sbin/vnconfig/vnconfig.c
+++ usr.sbin/vnconfig/vnconfig.c
@@ -1,6 +1,32 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  *  * $FreeBSD$  *  */
+comment|/*  * ----------------------------------------------------------------------------  * "THE BEER-WARE LICENSE" (Revision 42):  *<phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you  * can do whatever you want with this stuff. If we meet some day, and you think  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp  * ----------------------------------------------------------------------------  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|char
+name|rcsid
+index|[]
+init|=
+literal|"$FreeBSD$"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
 end_comment
 
 begin_include
@@ -33,7 +59,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\tPlease used mdconfig(8).\n"
+literal|"\tPlease use mdconfig(8).\n"
 argument_list|)
 expr_stmt|;
 name|exit
