06a5a2def5eaff6f66debc38d0c19c650f3a25f9
Poul-Henning Kamp <phk@FreeBSD.org>
2001-03-17 10:00:50 +0000

    Add a new entrypoint to the hashes in libmd:   char *   FooFileChunk(const char *filename, char *buf, off_t offset, off_t length) Which only hashes part of a file. Implement FooFile() in terms of this function.
    Submitted by:	roam
    
    
    Former-commit-id: 098cf23ad02aaf081800ff6857d274c8f20f7fe5

--- lib/libmd/md2c.c
+++ lib/libmd/md2c.c
@@ -10,7 +10,7 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"md2.h"
+file|<sys/types.h>
 end_include
 
 begin_include
@@ -22,7 +22,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/types.h>
+file|"md2.h"
 end_include
 
 begin_typedef
