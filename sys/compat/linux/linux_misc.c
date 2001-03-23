2481a23da4fcec2fe0f14fbd6c6462a61b4ceb33
Andrew Gallatin <gallatin@FreeBSD.org>
2001-03-23 19:22:21 +0000

    fix linux_times() to take into account linux's value of CLK_TCK on the alpha. Previously, results were off by a factor of 10
    Tested by: Yoriaki FUJIMORI <fujimori@grafin.fujimori.cache.waseda.ac.jp>
    
    
    Former-commit-id: 2f2f867acbc625b9d224e590b49d870b1ab624c7

--- sys/compat/linux/linux_misc.c
+++ sys/compat/linux/linux_misc.c
@@ -2702,6 +2702,28 @@ block|}
 struct|;
 end_struct
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__alpha__
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|CLK_TCK
+value|1024
+end_define
+
+begin_comment
+comment|/* Linux uses 1024 on alpha */
+end_comment
+
+begin_else
+else|#
+directive|else
+end_else
+
 begin_define
 define|#
 directive|define
@@ -2712,6 +2734,11 @@ end_define
 begin_comment
 comment|/* Linux uses 100 */
 end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
