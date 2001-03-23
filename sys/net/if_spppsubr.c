58c387e42dd3dcdc455067fcf22f21d30728615f
Joerg Wunsch <joerg@FreeBSD.org>
2001-03-23 19:51:12 +0000

    (MFC candidate, see below).
    When we get an Open event in stopped state, experience shows that this
    is usually means we've somehow missed a previous Down event.  This has
    occasionally bitten people for the IPCP layer with ISDN, apparently a
    previously aborted IPCP negotiation must have caused this.  As a
    bandaid, we quickly pretent a Down event by advancing to starting
    state; this effectively implements the `restart' option mentioned in
    RFC 1663.
    
    While i'm not yet fully convinced this is the best thing to do (and is
    fully compliant with RFC 1661), i've seen a number of reports here on
    the German mailing lists where people have been bitten by the previous
    behaviour which usually causes quickly looping ISDN reconnects (thus
    loss of money...), and where just this patch fixes the problem.
    
    For this, i'd even like to see it MFC'd if possible.
    
    Submitted by:	Helmut Kreft <kreft@zeus.ai-lab.fh-furtwangen.de>
    
    
    Former-commit-id: da8dc7037ffcf2cacea75a07a023ede7dad447d1

--- sys/net/if_spppsubr.c
+++ sys/net/if_spppsubr.c
@@ -9205,6 +9205,26 @@ break|break;
 case|case
 name|STATE_STOPPED
 case|:
+comment|/* 		 * Try escaping stopped state.  This seems to bite 		 * people occasionally, in particular for IPCP, 		 * presumably following previous IPCP negotiation 		 * aborts.  Somehow, we must have missed a Down event 		 * which would have caused a transition into starting 		 * state, so as a bandaid we force the Down event now. 		 * This effectively implements (something like the) 		 * `restart' option mentioned in the state transition 		 * table of RFC 1661. 		 */
+name|sppp_cp_change_state
+argument_list|(
+name|cp
+argument_list|,
+name|sp
+argument_list|,
+name|STATE_STARTING
+argument_list|)
+expr_stmt|;
+call|(
+name|cp
+operator|->
+name|tls
+call|)
+argument_list|(
+name|sp
+argument_list|)
+expr_stmt|;
+break|break;
 case|case
 name|STATE_STOPPING
 case|:
