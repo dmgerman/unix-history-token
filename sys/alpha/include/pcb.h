f685088574bee71e5ec4b5958b96cc265a9cc8d5
John Baldwin <jhb@FreeBSD.org>
2001-04-17 02:51:28 +0000

    - Add appropriate #ifndef/#define/#endif to protect against multiple   inclusions. - Blow away all evidence of a static curpcb as curpcb is a per-CPU variable   and this definition is now bogus.
    Former-commit-id: 786c8a2ea36de2bf2ff79ce9eadae23700c43554

--- sys/alpha/include/pcb.h
+++ sys/alpha/include/pcb.h
@@ -11,6 +11,18 @@ begin_comment
 comment|/*  * Copyright (c) 1994, 1995, 1996 Carnegie-Mellon University.  * All rights reserved.  *  * Author: Chris G. Demetriou  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_MACHINE_PCB_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_MACHINE_PCB_H_
+end_define
+
 begin_include
 include|#
 directive|include
@@ -99,40 +111,14 @@ block|}
 struct|;
 end_struct
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|_KERNEL
-end_ifdef
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|curpcb
-end_ifndef
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|pcb
-modifier|*
-name|curpcb
-decl_stmt|;
-end_decl_stmt
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
-comment|/* our current running pcb */
+comment|/* _MACHINE_PCB_H */
 end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 end_unit
 
