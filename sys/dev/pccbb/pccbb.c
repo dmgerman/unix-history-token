08a3dac59e1cd943ee60b75017b0256f4dd28d73
Warner Losh <imp@FreeBSD.org>
2001-04-18 04:42:11 +0000

    Add support for the OZ6832.
    Submitted by: Martin Nilsson <martin@svenskabutiker.se>
    
    
    Former-commit-id: 9dd9ee193dd1d5ef8ca44f6b6d2629b89689d03a

--- sys/dev/pccbb/pccbb.c
+++ sys/dev/pccbb/pccbb.c
@@ -640,6 +640,16 @@ block|}
 block|,
 comment|/* 02Micro */
 block|{
+name|PCI_DEVICE_ID_PCIC_OZ6832
+block|,
+literal|"O2Mirco OZ6832 PCI-CardBus Bridge"
+block|,
+name|CB_CIRRUS
+block|,
+name|PCCBB_PCIC_MEM_32
+block|}
+block|,
+block|{
 name|PCI_DEVICE_ID_PCIC_OZ6872
 block|,
 literal|"O2Mirco OZ6872 PCI-CardBus Bridge"
