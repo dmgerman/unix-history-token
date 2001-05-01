8536a916bfe47ae8aeb0fbed70c95e49bd0497f2
Mark Murray <markm@FreeBSD.org>
2001-05-01 09:30:02 +0000

    Compensate for header dethreading.
    Former-commit-id: 99be353fe91c837d81b3b78acc8970a92124dde6

--- lib/libc/gen/fstab.c
+++ lib/libc/gen/fstab.c
@@ -69,6 +69,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/lock.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mutex.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/mount.h>
 end_include
 
