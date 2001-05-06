6559e8efa5b79fbc348adae6bb8dc585cf804fd2
Takenori KATO <kato@FreeBSD.org>
2001-05-06 09:18:32 +0000

    Merged from sys/i386/i386/machdep.c revision 1.448.
    Former-commit-id: 6433a6dff140ff9740d142594c210fb38222cf9a

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -3825,17 +3825,6 @@ name|_udatasel
 expr_stmt|;
 name|regs
 operator|->
-name|tf_fs
-operator|=
-name|_udatasel
-expr_stmt|;
-name|load_gs
-argument_list|(
-name|_udatasel
-argument_list|)
-expr_stmt|;
-name|regs
-operator|->
 name|tf_ss
 operator|=
 name|_udatasel
