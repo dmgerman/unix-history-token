96c85dd1cbef4a4ed7e6646a87330159543ba285
Andrew Gallatin <gallatin@FreeBSD.org>
2001-05-20 16:22:46 +0000

    fix alpha-MD compile errors after the vm_mtx commit
    Former-commit-id: add1ccaafd0d39cea2e86ef1c5b6618065a2aad3

--- sys/alpha/alpha/pmap.c
+++ sys/alpha/alpha/pmap.c
@@ -80,6 +80,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/mutex.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<vm/vm_kern.h>
 end_include
 
--- sys/alpha/alpha/promcons.c
+++ sys/alpha/alpha/promcons.c
@@ -86,6 +86,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/mutex.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<vm/vm_kern.h>
 end_include
 
