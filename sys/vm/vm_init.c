4f1b7ab29a88bd717bdfa6a15ce0029f2a3e74f1
John Baldwin <jhb@FreeBSD.org>
2001-05-22 05:35:45 +0000

    Sort includes from previous commit.
    Former-commit-id: cec26453423e8e42cfc82fcd0cbf1f87e9195054

--- sys/vm/vm_init.c
+++ sys/vm/vm_init.c
@@ -28,6 +28,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/mutex.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/proc.h>
 end_include
 
@@ -35,12 +41,6 @@ begin_include
 include|#
 directive|include
 file|<sys/systm.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/mutex.h>
 end_include
 
 begin_include
