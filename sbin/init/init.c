bfe9fb97fb963e58be4b8af877e0978b0da89440
Dirk Froemberg <dirk@FreeBSD.org>
2001-05-22 22:57:56 +0000

    MFC: Typo: single used mode -> single user mode.
    Former-commit-id: 1bd75d58b3a8c591238ede8f27b3b77e80746d85

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -6936,7 +6936,7 @@ argument_list|)
 expr_stmt|;
 name|warning
 argument_list|(
-literal|"timeout expired for %s on %s: %m; going to single used mode"
+literal|"timeout expired for %s on %s: %m; going to single user mode"
 argument_list|,
 name|_PATH_BSHELL
 argument_list|,
