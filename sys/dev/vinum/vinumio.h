7289eb4084a39cfde2c541db6241b1c245896869
Greg Lehey <grog@FreeBSD.org>
2001-05-23 23:24:05 +0000

    Change #if VINUMDEBUG to #ifdef VINUMDEBUG.  This is a flag, not a variable.
    Former-commit-id: e339517eb6ea303c7d307e05867637c970efe0ba

--- sys/dev/vinum/vinumio.h
+++ sys/dev/vinum/vinumio.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumio.h,v 1.19 2000/04/26 04:17:33 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumio.h,v 1.21 2001/05/22 07:29:23 grog Exp grog $  * $FreeBSD$  */
 end_comment
 
 begin_define
@@ -14,35 +14,12 @@ begin_comment
 comment|/* ID letter of our ioctls */
 end_comment
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|VINUMDEBUG
-end_ifdef
-
 begin_define
 define|#
 directive|define
 name|MAX_IOCTL_REPLY
 value|1024
 end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_define
-define|#
-directive|define
-name|MAX_IOCTL_REPLY
-value|256
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_ifdef
 ifdef|#
--- sys/dev/vinum/vinumrequest.c
+++ sys/dev/vinum/vinumrequest.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998, 1999  *  Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumrequest.c,v 1.30 2001/01/09 04:20:55 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998, 1999  *  Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumrequest.c,v 1.32 2001/05/23 23:04:38 grog Exp grog $  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -720,8 +720,8 @@ name|enum
 name|requeststatus
 name|status
 decl_stmt|;
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
@@ -1458,8 +1458,8 @@ operator|=
 name|rq
 expr_stmt|;
 comment|/* hook our request at the front */
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
@@ -1536,8 +1536,8 @@ operator|=
 literal|0
 expr_stmt|;
 comment|/* nothing yet */
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
@@ -2819,8 +2819,8 @@ operator|->
 name|sdoffset
 expr_stmt|;
 comment|/* truncate */
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
@@ -3977,8 +3977,8 @@ name|drive
 modifier|*
 name|drive
 decl_stmt|;
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
@@ -4433,8 +4433,8 @@ expr_stmt|;
 return|return;
 block|}
 block|}
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
@@ -4525,8 +4525,8 @@ operator|=
 name|splbio
 argument_list|()
 expr_stmt|;
-if|#
-directive|if
+ifdef|#
+directive|ifdef
 name|VINUMDEBUG
 if|if
 condition|(
