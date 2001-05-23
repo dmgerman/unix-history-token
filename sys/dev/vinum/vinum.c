e29c3b46bc75d862d8a0ca0eb00410e14a63a915
Greg Lehey <grog@FreeBSD.org>
2001-05-23 05:25:45 +0000

    Set flag VF_HASDEBUG in vinum_conf to indicate that the module was compiled with debug support.  This can be used by userland programs to recognize which ioctls the module supports.
    As a result, remove the gross debug device/non-debug device hack used
    to recognize whether the kernel module was in sync with the userland
    module.
    
    Replace explicit references to major/minor numbers of vinum
    superdevice with the VINUM_SUPERDEV macro written for that purpose.
    
    
    Former-commit-id: eabf0fdda9aefa5462c88f9af1fa2757146e4626

--- sys/dev/vinum/vinum.c
+++ sys/dev/vinum/vinum.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinum.c,v 1.33 2001/01/09 06:19:15 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinum.c,v 1.34 2001/05/22 04:07:22 grog Exp grog $  * $FreeBSD$  */
 end_comment
 
 begin_define
@@ -181,12 +181,6 @@ name|vinum_super_dev
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
-name|dev_t
-name|vinum_debug_super_dev
-decl_stmt|;
-end_decl_stmt
-
 begin_comment
 comment|/*  * Called by main() during pseudo-device attachment.  All we need  * to do is allocate enough space for devices to be configured later, and  * add devsw entries.  */
 end_comment
@@ -225,6 +219,20 @@ argument_list|,
 literal|"vinum: loaded\n"
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|VINUMDEBUG
+name|vinum_conf
+operator|.
+name|flags
+operator||=
+name|VF_LOADED
+operator||
+name|VF_HASDEBUG
+expr_stmt|;
+comment|/* we're loaded now, and we support debug */
+else|#
+directive|else
 name|vinum_conf
 operator|.
 name|flags
@@ -232,6 +240,8 @@ operator||=
 name|VF_LOADED
 expr_stmt|;
 comment|/* we're loaded now */
+endif|#
+directive|endif
 name|daemonq
 operator|=
 name|NULL
@@ -268,35 +278,6 @@ argument_list|,
 literal|"vinum/controld"
 argument_list|)
 expr_stmt|;
-name|vinum_debug_super_dev
-operator|=
-name|make_dev
-argument_list|(
-operator|&
-name|vinum_cdevsw
-argument_list|,
-name|VINUMMINOR
-argument_list|(
-literal|1
-argument_list|,
-literal|0
-argument_list|,
-literal|0
-argument_list|,
-name|VINUM_SUPERDEV_TYPE
-argument_list|)
-argument_list|,
-name|UID_ROOT
-argument_list|,
-name|GID_WHEEL
-argument_list|,
-name|S_IRUSR
-operator||
-name|S_IWUSR
-argument_list|,
-literal|"vinum/Control"
-argument_list|)
-expr_stmt|;
 name|vinum_super_dev
 operator|=
 name|make_dev
@@ -304,16 +285,7 @@ argument_list|(
 operator|&
 name|vinum_cdevsw
 argument_list|,
-name|VINUMMINOR
-argument_list|(
-literal|2
-argument_list|,
-literal|0
-argument_list|,
-literal|0
-argument_list|,
-name|VINUM_SUPERDEV_TYPE
-argument_list|)
+name|VINUM_SUPERDEV
 argument_list|,
 name|UID_ROOT
 argument_list|,
@@ -1206,11 +1178,6 @@ comment|/* daemon device */
 name|destroy_dev
 argument_list|(
 name|vinum_super_dev
-argument_list|)
-expr_stmt|;
-name|destroy_dev
-argument_list|(
-name|vinum_debug_super_dev
 argument_list|)
 expr_stmt|;
 name|cdevsw_remove
