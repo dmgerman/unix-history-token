66cfed7b57f1ed08a1d327b8c58b0ddb873b9298
Kris Kennaway <kris@FreeBSD.org>
2001-05-26 22:39:33 +0000

    Fix some trivial warnings and clamp down with WARNS=2
    MFC after:	1 week
    
    
    Former-commit-id: f9456bc7e208a425f0a908b0d84a20800a609323

--- usr.bin/touch/touch.c
+++ usr.bin/touch/touch.c
@@ -11,6 +11,7 @@ end_ifndef
 
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
 name|copyright
 index|[]
@@ -34,13 +35,26 @@ directive|ifndef
 name|lint
 end_ifndef
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|static const char sccsid[] = "@(#)touch.c	8.1 (Berkeley) 6/6/93";
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|sccsid
+name|rcsid
 index|[]
 init|=
-literal|"@(#)touch.c	8.1 (Berkeley) 6/6/93"
+literal|"$FreeBSD$"
 decl_stmt|;
 end_decl_stmt
 
@@ -118,6 +132,22 @@ include|#
 directive|include
 file|<unistd.h>
 end_include
+
+begin_decl_stmt
+name|int
+decl|main
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+name|char
+operator|*
+index|[]
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 name|int
