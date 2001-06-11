939d952ba93d106cb2bf778e6735b66ee96b9f26
Hajimu UMEMOTO <ume@FreeBSD.org>
2001-06-11 20:14:45 +0000

    Nuke wrongly introduced experimental stuff during KAME merge.
    Following changed was made by previous commit:
    
      - add a pointer to struct mauxtag.  two integer was too restrictive.
      - add m_aux_{add,find}2.
      - make sure to nuke mbuf pointed to m_aux.
    
    
    Former-commit-id: b3f7f8a4b46576997887adff3ac3d6581e043cc9

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -1037,58 +1037,6 @@ name|u_long
 name|m_mhlen
 decl_stmt|;
 comment|/* length of data in a header mbuf */
-name|u_quad_t
-name|m_exthdrget
-decl_stmt|;
-comment|/* # of calls to IP6_EXTHDR_GET */
-name|u_quad_t
-name|m_exthdrget0
-decl_stmt|;
-comment|/* # of calls to IP6_EXTHDR_GET0 */
-name|u_quad_t
-name|m_pulldowns
-decl_stmt|;
-comment|/* # of calls to m_pulldown */
-name|u_quad_t
-name|m_pulldown_copy
-decl_stmt|;
-comment|/* # of mbuf copies in m_pulldown */
-name|u_quad_t
-name|m_pulldown_alloc
-decl_stmt|;
-comment|/* # of mbuf allocs in m_pulldown */
-name|u_quad_t
-name|m_pullups
-decl_stmt|;
-comment|/* # of calls to m_pullup */
-name|u_quad_t
-name|m_pullup_copy
-decl_stmt|;
-comment|/* # of possible m_pullup copies */
-name|u_quad_t
-name|m_pullup_alloc
-decl_stmt|;
-comment|/* # of possible m_pullup mallocs */
-name|u_quad_t
-name|m_pullup_fail
-decl_stmt|;
-comment|/* # of possible m_pullup failures */
-name|u_quad_t
-name|m_pullup2
-decl_stmt|;
-comment|/* # of calls to m_pullup2 */
-name|u_quad_t
-name|m_pullup2_copy
-decl_stmt|;
-comment|/* # of possible m_pullup2 copies */
-name|u_quad_t
-name|m_pullup2_alloc
-decl_stmt|;
-comment|/* # of possible m_pullup2 mallocs */
-name|u_quad_t
-name|m_pullup2_fail
-decl_stmt|;
-comment|/* # of possible m_pullup2 failures */
 block|}
 struct|;
 end_struct
