d71d8fdae88fe878a86812e6842c81f619a2d967
David E. O'Brien <obrien@FreeBSD.org>
2001-06-16 06:17:35 +0000

    style(9) and remove a left over Alpha comment
    Former-commit-id: 009e9b080fb8a1dfbcafc9be59767556961dd830

--- sys/boot/ia64/efi/main.c
+++ sys/boot/ia64/efi/main.c
@@ -1,6 +1,32 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998,2000 Doug Rabson<dfr@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1998 Michael Smith<msmith@freebsd.org>  * Copyright (c) 1998,2000 Doug Rabson<dfr@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_decl_stmt
+specifier|static
+specifier|const
+name|char
+name|rcsid
+index|[]
+init|=
+literal|"$FreeBSD$"
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* not lint */
 end_comment
 
 begin_include
@@ -245,7 +271,6 @@ literal|0
 block|printf("Memory: %ld k\n", memsize() / 1024);
 endif|#
 directive|endif
-comment|/* We're booting from an SRM disk, try to spiff this */
 comment|/* XXX presumes that biosdisk is first in devsw */
 name|currdev
 operator|.
@@ -303,7 +328,7 @@ if|#
 directive|if
 literal|0
 comment|/* Create arc-specific variables */
-block|bootfile = GetEnvironmentVariable(ARCENV_BOOTFILE); 	if (bootfile) 		setenv("bootfile", bootfile, 1);  	env_setenv("currdev", EV_VOLATILE, 		   arc_fmtdev(&currdev), arc_setcurrdev, env_nounset); 	env_setenv("loaddev", EV_VOLATILE, 		   arc_fmtdev(&currdev), env_noset, env_nounset);
+block|bootfile = GetEnvironmentVariable(ARCENV_BOOTFILE); 	if (bootfile) 		setenv("bootfile", bootfile, 1);  	env_setenv("currdev", EV_VOLATILE, arc_fmtdev(&currdev), 	    arc_setcurrdev, env_nounset); 	env_setenv("loaddev", EV_VOLATILE, arc_fmtdev(&currdev), env_noset, 	    env_nounset);
 endif|#
 directive|endif
 name|setenv
@@ -351,7 +376,9 @@ argument_list|()
 expr_stmt|;
 comment|/* doesn't return */
 return|return
+operator|(
 name|EFI_SUCCESS
+operator|)
 return|;
 comment|/* keep compiler happy */
 block|}
