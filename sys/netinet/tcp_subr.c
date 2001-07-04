ebdb8485d868eb14140ceae30273bcb354a1d98a
Hajimu UMEMOTO <ume@FreeBSD.org>
2001-07-04 09:26:40 +0000

    MFC 1.105: made sure to use the correct sa_len for rtalloc(). sizeof(ro_dst) is not necessarily the correct one. this change would also fix the recent path MTU discovery problem for the destination of an incoming TCP connection.
    Former-commit-id: df7e0df833b247a98fe9ab15ce849ab2b7c10dc4

--- sys/netinet/tcp_subr.c
+++ sys/netinet/tcp_subr.c
@@ -6196,9 +6196,8 @@ name|sa_len
 operator|=
 sizeof|sizeof
 argument_list|(
-name|ro
-operator|->
-name|ro_dst
+expr|struct
+name|sockaddr_in
 argument_list|)
 expr_stmt|;
 operator|(
@@ -6339,9 +6338,8 @@ name|sin6_len
 operator|=
 sizeof|sizeof
 argument_list|(
-name|ro6
-operator|->
-name|ro_dst
+operator|*
+name|dst6
 argument_list|)
 expr_stmt|;
 name|dst6
