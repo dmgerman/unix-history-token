4ab087ca7f192a8cbef4a02cbeabda7c5a2672f1
Peter Wemm <peter@FreeBSD.org>
2001-07-12 12:19:11 +0000

    Do not depend on pcb_savefpu backwards compat #define.
    Former-commit-id: 16a6b13352b4e1d53a172ba2279d0a531dc2840a

--- sys/gnu/i386/fpemul/fpu_system.h
+++ sys/gnu/i386/fpemul/fpu_system.h
@@ -27,7 +27,7 @@ begin_define
 define|#
 directive|define
 name|I387
-value|(*(union i387_union *)&(((struct pcb *)curproc->p_addr)->pcb_savefpu))
+value|(*(union i387_union *)&(((struct pcb *)curproc->p_addr)->pcb_save.sv_87))
 end_define
 
 begin_define
