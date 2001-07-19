6c338e8d74dd8069bbb2afb8a3747134b17e8dfd
Joerg Wunsch <joerg@FreeBSD.org>
2001-07-19 13:16:54 +0000

    Merge a small part of the floppy driver changes from -current into RELENG_4.  FDOPT_NOERRLOG and the FD_GSTAT ioctl command are merged, both are fairly minor changes that are easily auditable and thus copmatible with the policy of -stable.
    Former-commit-id: 24a30669795cc3bb44c50a00fb1cab1e0e704b46

--- sys/i386/include/ioctl_fd.h
+++ sys/i386/include/ioctl_fd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 1992-1994 by Joerg Wunsch, Dresden  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT  * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR  * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH  * DAMAGE.  *  * $FreeBSD$  */
+comment|/*  * Copyright (C) 1992-1994,2001 by Joerg Wunsch, Dresden  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT  * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR  * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH  * DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -275,6 +275,20 @@ block|}
 struct|;
 end_struct
 
+begin_struct
+struct|struct
+name|fdc_status
+block|{
+name|u_int
+name|status
+index|[
+literal|7
+index|]
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
 begin_define
 define|#
 directive|define
@@ -333,6 +347,21 @@ name|FD_DEBUG
 value|_IOW('F', 66, int)
 end_define
 
+begin_comment
+comment|/*  * Obtain NE765 status registers.  Only successful if there is  * a valid status stored in fdc->status[].  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FD_GSTAT
+value|_IOR('F', 68, struct fdc_status)
+end_define
+
+begin_comment
+comment|/* Options for FD_GOPTS/FD_SOPTS, cleared on device close */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -341,7 +370,18 @@ value|0x0001
 end_define
 
 begin_comment
-comment|/* no retries on failure (cleared on close) */
+comment|/* no retries on failure */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|FDOPT_NOERRLOG
+value|0x002
+end_define
+
+begin_comment
+comment|/* no "hard error" kernel log messages */
 end_comment
 
 begin_comment
