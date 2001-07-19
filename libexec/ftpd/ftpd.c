8a7bf2ea895a3ec0d70e69dda9ec4f67dbe155b0
Dima Dorfman <dd@FreeBSD.org>
2001-07-19 05:44:08 +0000

    MFC 1.77: compile okay with -DVIRTUAL_HOSTING.
    Former-commit-id: 4bd9b2de20f02d04f6c14b76b68cea71d0da5176

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -750,6 +750,14 @@ name|hostname
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+name|int
+name|epsvall
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -760,14 +768,6 @@ begin_decl_stmt
 name|char
 modifier|*
 name|ftpuser
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|int
-name|epsvall
-init|=
-literal|0
 decl_stmt|;
 end_decl_stmt
 
