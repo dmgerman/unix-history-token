932b5d4bf56c1bcfcf942c9b3634efd0a6c02989
Nick Hibma <n_hibma@FreeBSD.org>
2001-07-20 09:20:43 +0000

    Support for USB floppies based upon SMSC USB FDD controller.
    PR:		28877
    Submitted by:	Larry Baird <lab@gta.com>
    MFC after:	1 week
    
    Former-commit-id: 3337d42e23e30697785e96e5439cf632eba04008

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -709,6 +709,26 @@ block|,
 comment|/*quirks*/
 name|DA_Q_NO_6_BYTE
 block|}
+block|,
+block|{
+comment|/* 		 * The vendor, product and version strings coming from the 		 * controller are null terminated instead of being padded with 		 * spaces. The trailing wildcard character '*' is required. 		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_REMOVABLE
+block|,
+literal|"SMSC*"
+block|,
+literal|"USB FDC*"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+name|DA_Q_NO_6_BYTE
+operator||
+name|DA_Q_NO_SYNC_CACHE
+block|}
 block|}
 decl_stmt|;
 end_decl_stmt
