139c7731c22e84daa4077293a6a98b53de3c24cb
Kris Kennaway <kris@FreeBSD.org>
2001-07-23 21:52:26 +0000

    output_data(), output_datalen() and netflush() didn't actually guarantee to do what they are supposed to: under some circumstances output data would be truncated, or the buffer would not actually be flushed (possibly leading to overflows when the caller assumes the operation succeeded).  Change the semantics so that these functions ensure they complete the operation before returning.
    Comment out diagnostic code enabled by '-D reports' which causes an
    infinite recursion and an eventual crash.
    
    Patch developed with assistance from ru and assar.
    
    
    Former-commit-id: 792229e5eaf1858d722a71d311ed79c2f697712e

--- crypto/telnet/telnetd/ext.h
+++ crypto/telnet/telnetd/ext.h
@@ -1224,7 +1224,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|int
+name|void
 name|output_datalen
 name|__P
 argument_list|(
@@ -1233,7 +1233,7 @@ specifier|const
 name|char
 operator|*
 operator|,
-name|size_t
+name|int
 operator|)
 argument_list|)
 decl_stmt|;
