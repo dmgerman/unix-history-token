7298e31a739012c7d028b0ca67bd6807ff898d3e
Brooks Davis <brooks@FreeBSD.org>
2001-07-27 04:40:04 +0000

    Remove -traditional from CFLAGS and add unistd.h to header.h so this actually compiles.  Hopefully, this code didn't rely on some weird side effect of -traditional.
    Former-commit-id: 2017ce1517af98e03a8cc743c20d2888ff8bfe32

--- usr.sbin/pcvt/vttest/esc.c
+++ usr.sbin/pcvt/vttest/esc.c
@@ -2,12 +2,6 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_include
 include|#
 directive|include
-file|<unistd.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|"header.h"
 end_include
 
--- usr.sbin/pcvt/vttest/header.h
+++ usr.sbin/pcvt/vttest/header.h
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -174,6 +178,12 @@ begin_include
 include|#
 directive|include
 file|<setjmp.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<unistd.h>
 end_include
 
 begin_decl_stmt
