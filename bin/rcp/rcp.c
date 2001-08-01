95ac45c3a961eb83d75fdefb62d0b805d914efb0
David E. O'Brien <obrien@FreeBSD.org>
2001-08-01 05:14:24 +0000

    MFC: MAXPATHLEN -> PATH_MAX, __printflike, fix type of NULL arg to execl()
    Former-commit-id: 3bf2e5f3526657d2ce371500899bb211c04684ff

--- bin/rcp/rcp.c
+++ bin/rcp/rcp.c
@@ -142,6 +142,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<libutil.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<limits.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<netdb.h>
 end_include
 
@@ -185,12 +197,6 @@ begin_include
 include|#
 directive|include
 file|<unistd.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<libutil.h>
 end_include
 
 begin_include
@@ -449,6 +455,12 @@ operator|,
 operator|...
 operator|)
 argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|2
+argument_list|)
 decl_stmt|;
 end_decl_stmt
 
@@ -482,6 +494,28 @@ expr|struct
 name|stat
 operator|*
 operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|run_err
+name|__P
+argument_list|(
+operator|(
+specifier|const
+name|char
+operator|*
+operator|,
+operator|...
+operator|)
+argument_list|)
+name|__printflike
+argument_list|(
+literal|1
+argument_list|,
+literal|2
 argument_list|)
 decl_stmt|;
 end_decl_stmt
@@ -2944,7 +2978,7 @@ index|]
 decl_stmt|,
 name|path
 index|[
-name|MAXPATHLEN
+name|PATH_MAX
 index|]
 decl_stmt|;
 if|if
@@ -3181,9 +3215,10 @@ operator|->
 name|d_name
 argument_list|)
 operator|>=
-name|MAXPATHLEN
-operator|-
-literal|1
+sizeof|sizeof
+argument_list|(
+name|path
+argument_list|)
 condition|)
 block|{
 name|run_err
