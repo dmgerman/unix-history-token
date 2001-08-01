4d210dc1193d59c05a77b952d9423be325b12d26
David E. O'Brien <obrien@FreeBSD.org>
2001-08-01 05:31:04 +0000

    MFC: WARNS=2 and BDECFLAGS
    Former-commit-id: 042cc4794a2eaba1b5c8505b37ba64e3ed4c136d

--- bin/sleep/sleep.c
+++ bin/sleep/sleep.c
@@ -104,6 +104,22 @@ file|<unistd.h>
 end_include
 
 begin_decl_stmt
+name|int
+decl|main
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+name|char
+operator|*
+index|[]
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 name|void
 name|usage
 name|__P
