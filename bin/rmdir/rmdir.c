f96c007a0b93fc5908284e7e599fc3fc73756c31
David E. O'Brien <obrien@FreeBSD.org>
2001-08-01 05:16:47 +0000

    MFC: WARNS=2 and BDECFLAGS
    Former-commit-id: 3ea806a63dab9026997fe6bdeae2597a8ef7c1c5

--- bin/rmdir/rmdir.c
+++ bin/rmdir/rmdir.c
@@ -105,6 +105,22 @@ end_include
 
 begin_decl_stmt
 name|int
+decl|main
+name|__P
+argument_list|(
+operator|(
+name|int
+operator|,
+name|char
+operator|*
+index|[]
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
 name|rm_path
 name|__P
 argument_list|(
