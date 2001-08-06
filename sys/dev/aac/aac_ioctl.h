8b257b694203aeee99eeeddd7254766ac789915c
Scott Long <scottl@FreeBSD.org>
2001-08-06 03:52:40 +0000

    Missed this one in my 80-column sweep.
    Former-commit-id: 72c33cc23570dba45b5e89355a8e3d98552290c7

--- sys/dev/aac/aac_ioctl.h
+++ sys/dev/aac/aac_ioctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2000 Michael Smith  * Copyright (c) 2000 Scott Long  * Copyright (c) 2000 BSDi  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$FreeBSD$  */
+comment|/*-  * Copyright (c) 2000 Michael Smith  * Copyright (c) 2000-2001 Scott Long  * Copyright (c) 2000 BSDi  * Copyright (c) 2001 Adaptec, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$FreeBSD$  */
 end_comment
 
 begin_comment
@@ -116,7 +116,7 @@ name|meth
 parameter_list|,
 name|acc
 parameter_list|)
-value|(((devType)<< 16) | ((acc)<< 14) | ((func)<< 2) | (meth))
+value|(((devType)<< 16) | ((acc)<< 14) \ 					   | ((func)<< 2) | (meth))
 end_define
 
 begin_define
@@ -179,42 +179,42 @@ begin_define
 define|#
 directive|define
 name|FSACTL_SENDFIB
-value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2050, METHOD_BUFFERED, FILE_ANY_ACCESS)
+value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2050, \ 					METHOD_BUFFERED, FILE_ANY_ACCESS)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FSACTL_AIF_THREAD
-value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2127, METHOD_NEITHER, FILE_ANY_ACCESS)
+value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2127, \ 					METHOD_NEITHER, FILE_ANY_ACCESS)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FSACTL_OPEN_GET_ADAPTER_FIB
-value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2100, METHOD_BUFFERED, FILE_ANY_ACCESS)
+value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2100, \ 					METHOD_BUFFERED, FILE_ANY_ACCESS)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FSACTL_GET_NEXT_ADAPTER_FIB
-value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2101, METHOD_BUFFERED, FILE_ANY_ACCESS)
+value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2101, \ 					METHOD_BUFFERED, FILE_ANY_ACCESS)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FSACTL_CLOSE_GET_ADAPTER_FIB
-value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2102, METHOD_BUFFERED, FILE_ANY_ACCESS)
+value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2102, \ 					METHOD_BUFFERED, FILE_ANY_ACCESS)
 end_define
 
 begin_define
 define|#
 directive|define
 name|FSACTL_MINIPORT_REV_CHECK
-value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2107, METHOD_BUFFERED, FILE_ANY_ACCESS)
+value|CTL_CODE(FILE_DEVICE_CONTROLLER, 2107, \ 					METHOD_BUFFERED, FILE_ANY_ACCESS)
 end_define
 
 begin_comment
