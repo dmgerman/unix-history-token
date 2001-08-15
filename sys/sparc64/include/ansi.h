593de42b7a8ab70199cb7683c6bbf8f32e4aa87b
David E. O'Brien <obrien@FreeBSD.org>
2001-08-15 03:55:37 +0000

    Sync up with the latest ansi.h in other platforms -- especially RUNE and wchar bits.
    Former-commit-id: 622506d8259f028474dbb2caa0fdb3f699ca560c

--- sys/sparc64/include/ansi.h
+++ sys/sparc64/include/ansi.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ansi.h	8.2 (Berkeley) 1/4/94  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)ansi.h	8.2 (Berkeley) 1/4/94  *	From: NetBSD: ansi.h,v 1.9 1997/11/23 20:20:53 kleink Exp  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -16,7 +16,7 @@ name|_MACHINE_ANSI_H_
 end_define
 
 begin_comment
-comment|/*  * Types which are fundamental to the implementation and may appear in  * more than one standard header are defined here.  Standard headers  * then use:  *	#ifdef	_BSD_SIZE_T_  *	typedef	_BSD_SIZE_T_ size_t;  *	#undef	_BSD_SIZE_T_  *	#endif  */
+comment|/*  * Types which are fundamental to the implementation and may appear in  * more than one standard header are defined here.  Standard headers  * then use:  *	#ifdef	_BSD_SIZE_T_  *	typedef	_BSD_SIZE_T_	size_t;  *	#undef	_BSD_SIZE_T_  *	#endif  */
 end_comment
 
 begin_define
@@ -38,7 +38,18 @@ value|int
 end_define
 
 begin_comment
-comment|/* clockid_t */
+comment|/* clock_gettime()... */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_MBSTATE_T_
+value|__mbstate_t
+end_define
+
+begin_comment
+comment|/* mbstate_t */
 end_comment
 
 begin_define
@@ -50,6 +61,17 @@ end_define
 
 begin_comment
 comment|/* ptr1 - ptr2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_RUNE_T_
+value|_BSD_CT_RUNE_T_
+end_define
+
+begin_comment
+comment|/* rune_t (see below) */
 end_comment
 
 begin_define
@@ -70,6 +92,10 @@ name|_BSD_SOCKLEN_T_
 value|__uint32_t
 end_define
 
+begin_comment
+comment|/* socklen_t (duh) */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -89,7 +115,7 @@ value|int
 end_define
 
 begin_comment
-comment|/* time() */
+comment|/* time()... */
 end_comment
 
 begin_define
@@ -100,7 +126,29 @@ value|int
 end_define
 
 begin_comment
-comment|/* timer_t */
+comment|/* timer_gettime()... */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_WCHAR_T_
+value|_BSD_CT_RUNE_T_
+end_define
+
+begin_comment
+comment|/* wchar_t (see below) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_BSD_WINT_T_
+value|_BSD_CT_RUNE_T_
+end_define
+
+begin_comment
+comment|/* wint_t (see below) */
 end_comment
 
 begin_comment
@@ -140,6 +188,43 @@ begin_comment
 comment|/* process [group] */
 end_comment
 
+begin_if
+if|#
+directive|if
+name|defined
+name|__GNUC__
+operator|&&
+operator|(
+name|__GNUC__
+operator|>
+literal|2
+operator|||
+name|__GNUC__
+operator|==
+literal|2
+operator|&&
+name|__GNUC_MINOR__
+operator|>
+literal|95
+operator|)
+end_if
+
+begin_define
+define|#
+directive|define
+name|_BSD_VA_LIST_
+value|__builtin_va_list
+end_define
+
+begin_comment
+comment|/* internally known to gcc */
+end_comment
+
+begin_else
+else|#
+directive|else
+end_else
+
 begin_define
 define|#
 directive|define
@@ -151,41 +236,17 @@ begin_comment
 comment|/* va_list */
 end_comment
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_comment
-comment|/*  * Runes (wchar_t) is declared to be an ``int'' instead of the more natural  * ``unsigned long'' or ``long''.  Two things are happening here.  It is not  * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,  * it looks like 10646 will be a 31 bit standard.  This means that if your  * ints cannot hold 32 bits, you will be in trouble.  The reason an int was  * chosen over a long is that the is*() and to*() routines take ints (says  * ANSI C), but they use _RUNE_T_ instead of int.  By changing it here, you  * lose a bit of ANSI conformance, but your programs will still work.  *      * Note that _WCHAR_T_ and _RUNE_T_ must be of the same type.  When wchar_t  * and rune_t are typedef'd, _WCHAR_T_ will be undef'd, but _RUNE_T remains  * defined for ctype.h.  */
+comment|/*__GNUC__*/
 end_comment
 
-begin_define
-define|#
-directive|define
-name|_BSD_WCHAR_T_
-value|int
-end_define
-
 begin_comment
-comment|/* wchar_t */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_BSD_WINT_T_
-value|int
-end_define
-
-begin_comment
-comment|/* wint_t */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|_BSD_RUNE_T_
-value|int
-end_define
-
-begin_comment
-comment|/* rune_t */
+comment|/*  * The rune type above is declared to be an ``int'' instead of the more natural  * ``unsigned long'' or ``long''.  Two things are happening here.  It is not  * unsigned so that EOF (-1) can be naturally assigned to it and used.  Also,  * it looks like 10646 will be a 31 bit standard.  This means that if your  * ints cannot hold 32 bits, you will be in trouble.  The reason an int was  * chosen over a long is that the is*() and to*() routines take ints (says  * ANSI C), but they use _BSD_CT_RUNE_T_ instead of int.  By changing it  * here, you lose a bit of ANSI conformance, but your programs will still  * work.  *  * NOTE: rune_t is not covered by ANSI nor other standards, and should not  * be instantiated outside of lib/libc/locale.  Use wchar_t.  wchar_t and  * rune_t must be the same type.  Also wint_t must be no narrower than  * wchar_t, and should also be able to hold all members of the largest  * character set plus one extra value (WEOF). wint_t must be at least 16 bits.  */
 end_comment
 
 begin_comment
@@ -302,13 +363,36 @@ name|__uintptr_t
 typedef|;
 end_typedef
 
+begin_comment
+comment|/*  * mbstate_t is an opaque object to keep conversion state, during multibyte  * stream conversions.  The content must not be referenced by user programs.  */
+end_comment
+
+begin_typedef
+typedef|typedef
+union|union
+block|{
+name|char
+name|__mbstate8
+index|[
+literal|128
+index|]
+decl_stmt|;
+name|__int64_t
+name|_mbstateL
+decl_stmt|;
+comment|/* for alignment */
+block|}
+name|__mbstate_t
+typedef|;
+end_typedef
+
 begin_endif
 endif|#
 directive|endif
 end_endif
 
 begin_comment
-comment|/* _MACHINE_ANSI_H_ */
+comment|/* !_MACHINE_ANSI_H_ */
 end_comment
 
 end_unit
