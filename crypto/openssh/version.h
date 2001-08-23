56edee475e05af39715b398e379caa5b7ca61229
cvs2svn <cvs2svn@FreeBSD.org>
2001-08-23 22:42:42 +0000

    This commit was manufactured by cvs2svn to create branch 'RELENG_4_4'.
    Former-commit-id: 94c5da2dbce279034a7e68dd26c6db777835f1c3

---
+++ crypto/openssh/version.h
@@ -0,0 +1,18 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
+comment|/* $OpenBSD: version.h,v 1.13 2000/10/16 09:38:45 djm Exp $ */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SSH_VERSION
+value|"OpenSSH_2.3.0 FreeBSD localisations 20010713"
+end_define
+
+end_unit
+
