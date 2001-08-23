b9cdd9781c92c6a05b9d670a4dd0d080a37e4153
Peter Wemm <peter@FreeBSD.org>
2001-08-23 01:43:38 +0000

    Merge i386/machdep.c rev 1.468: make breakpoint/trace interrupt instead of trap gates
    Forgotten by: bde
    
    
    Former-commit-id: 3547dfa2054e07fe3834099fdddce80066a8cf4c

--- sys/pc98/pc98/machdep.c
+++ sys/pc98/pc98/machdep.c
@@ -8132,7 +8132,7 @@ argument_list|(
 name|dbg
 argument_list|)
 argument_list|,
-name|SDT_SYS386TGT
+name|SDT_SYS386IGT
 argument_list|,
 name|SEL_KPL
 argument_list|,
@@ -8176,7 +8176,7 @@ argument_list|(
 name|bpt
 argument_list|)
 argument_list|,
-name|SDT_SYS386TGT
+name|SDT_SYS386IGT
 argument_list|,
 name|SEL_UPL
 argument_list|,
