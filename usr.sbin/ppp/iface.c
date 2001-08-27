098fb013836f75762781b9851fb39a0ca76724b6
Brian Somers <brian@FreeBSD.org>
2001-08-27 19:08:43 +0000

    Some cosmetic changes to sync with OpenBSD
    Former-commit-id: f62d789eaa2995d6c0a22390609ae76e4a31d0d2

--- usr.sbin/ppp/iface.c
+++ usr.sbin/ppp/iface.c
@@ -33,11 +33,22 @@ directive|include
 file|<net/if_dl.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+end_ifdef
+
 begin_include
 include|#
 directive|include
 file|<net/if_var.h>
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
