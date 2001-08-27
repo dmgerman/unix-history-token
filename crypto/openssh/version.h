2c73155ac7513bf890bc7164e466df9e3dc9eacd
Paul Saab <ps@FreeBSD.org>
2001-08-27 10:20:02 +0000

    Correctly spell localizations
    Former-commit-id: ade81e30a23d3dc906e16070b8d4e4c69970ca3a

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -31,7 +31,7 @@ begin_define
 define|#
 directive|define
 name|SSH_VERSION_ADDENDUM
-value|"FreeBSD localisations 20010713"
+value|"FreeBSD localizations 20010713"
 end_define
 
 begin_function_decl
