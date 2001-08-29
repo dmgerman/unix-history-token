dd091267a59ac936b25a46cd08473419bf5a3448
Andrey Chernov <ache@FreeBSD.org>
2001-08-29 13:17:02 +0000

    Add heuristic to detect SSH connection (in the same style as other heuristics already here which not supposed to be secure, just helpers).
    Approved by:	security@ silence
    
    
    Former-commit-id: 1f347c9f870fcf70a31dc3c4e1a88888f34f47f5

--- contrib/opie/libopie/insecure.c
+++ contrib/opie/libopie/insecure.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* insecure.c: The opieinsecure() library function.  %%% portions-copyright-cmetz-96 Portions of this software are Copyright 1996-1998 by Craig Metz, All Rights Reserved. The Inner Net License Version 2 applies to these portions of the software. You should have received a copy of the license with this software. If you didn't get a copy, you may request one from<license@inner.net>.  Portions of this software are Copyright 1995 by Randall Atkinson and Dan McDonald, All Rights Reserved. All Rights under this copyright are assigned to the U.S. Naval Research Laboratory (NRL). The NRL Copyright Notice and License Agreement applies to this software.          History:  	Modified by cmetz for OPIE 2.31. Fixed a logic bug. Call endut[x]ent(). 	Modified by cmetz for OPIE 2.3. Added result caching. Use 	     __opiegetutmpentry(). Ifdef around ut_host check. Eliminate 	     unused variable. 	Modified by cmetz for OPIE 2.2. Use FUNCTION declaration et al.              Allow IP loopback. DISPLAY and ut_host must match exactly,              not just the part before the colon. Added work-around for               Sun CDE dtterm bug. Leave the environment as it was              found. Use uname().         Created at NRL for OPIE 2.2 from opiesubr.c. Fixed pointer              assignment that should have been a comparison. */
+comment|/* insecure.c: The opieinsecure() library function.  %%% portions-copyright-cmetz-96 Portions of this software are Copyright 1996-1998 by Craig Metz, All Rights Reserved. The Inner Net License Version 2 applies to these portions of the software. You should have received a copy of the license with this software. If you didn't get a copy, you may request one from<license@inner.net>.  Portions of this software are Copyright 1995 by Randall Atkinson and Dan McDonald, All Rights Reserved. All Rights under this copyright are assigned to the U.S. Naval Research Laboratory (NRL). The NRL Copyright Notice and License Agreement applies to this software.          History:  	Modified by cmetz for OPIE 2.31. Fixed a logic bug. Call endut[x]ent(). 	Modified by cmetz for OPIE 2.3. Added result caching. Use 	     __opiegetutmpentry(). Ifdef around ut_host check. Eliminate 	     unused variable. 	Modified by cmetz for OPIE 2.2. Use FUNCTION declaration et al.              Allow IP loopback. DISPLAY and ut_host must match exactly,              not just the part before the colon. Added work-around for               Sun CDE dtterm bug. Leave the environment as it was              found. Use uname().         Created at NRL for OPIE 2.2 from opiesubr.c. Fixed pointer              assignment that should have been a comparison.  $FreeBSD$  */
 end_comment
 
 begin_include
@@ -180,6 +180,22 @@ literal|1
 condition|)
 return|return
 name|result
+return|;
+if|if
+condition|(
+name|getenv
+argument_list|(
+literal|"SSH_CLIENT"
+argument_list|)
+operator|!=
+name|NULL
+condition|)
+return|return
+operator|(
+name|result
+operator|=
+literal|0
+operator|)
 return|;
 name|display_name
 operator|=
