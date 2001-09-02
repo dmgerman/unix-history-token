7acbceff857ce1fbb02a58c2f1055e581b3daa60
Nick Hibma <n_hibma@FreeBSD.org>
2001-09-02 20:44:49 +0000

    Add quirk entry for FujiFilm camera.
    Submitted by:		Guido van Rooij <guido@gvr.org>
    
    
    Former-commit-id: 0263f9dc47d407291ffc383dc445fdedca33ac4a

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -789,6 +789,26 @@ name|DA_Q_NO_6_BYTE
 operator||
 name|DA_Q_NO_SYNC_CACHE
 block|}
+block|,
+block|{
+comment|/* 		 * FujiFilm Camera 		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_REMOVABLE
+block|,
+literal|"FUJIFILMUSB-DRIVEUNIT"
+block|,
+literal|"USB-DRIVEUNIT"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+name|DA_Q_NO_6_BYTE
+operator||
+name|DA_Q_NO_SYNC_CACHE
+block|}
 block|}
 decl_stmt|;
 end_decl_stmt
