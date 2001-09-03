7a858352a6156edb41f354e2fb38bd8732fa7803
Orion Hodson <orion@FreeBSD.org>
2001-09-03 14:48:41 +0000

    MFC: Fix return value of the setblocksize functions of these drivers, recording is broken otherwise.
    Approved by:	jkh
    
    
    Former-commit-id: c95643025af12ab2ceaa9d5369e3390f4e969d9e

--- sys/dev/sound/pci/cs4281.c
+++ sys/dev/sound/pci/cs4281.c
@@ -1585,12 +1585,9 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 return|return
-name|sndbuf_getsize
-argument_list|(
 name|ch
 operator|->
-name|buffer
-argument_list|)
+name|blksz
 return|;
 block|}
 end_function
