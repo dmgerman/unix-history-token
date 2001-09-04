34bcc372c35b1eb756db6e2d2edbac5893763d58
Bill Paul <wpaul@FreeBSD.org>
2001-09-04 17:10:11 +0000

    Add support for Conexant LANfinity miniPCI controllers. People who have laptops with this chip should test this and report back as I don't have access to this hardware myself. People with -stable systems should try the patch at:
    	http://www.freebsd.org/~wpaul/conexant.patch.gz
    
    Submitted by:	Phil Kernick <Phil@Kernick.org>
    
    
    Former-commit-id: 1093513e76ac519aa2a378c3e5aa6a56d497a136

--- sys/dev/dc/if_dcreg.h
+++ sys/dev/dc/if_dcreg.h
@@ -314,6 +314,17 @@ end_comment
 begin_define
 define|#
 directive|define
+name|DC_TYPE_CONEXANT
+value|0xC
+end_define
+
+begin_comment
+comment|/* Conexant LANfinity RS7112 */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|DC_IS_MACRONIX
 parameter_list|(
 name|x
@@ -411,6 +422,16 @@ parameter_list|(
 name|x
 parameter_list|)
 value|(x->dc_type == DC_TYPE_XIRCOM)
+end_define
+
+begin_define
+define|#
+directive|define
+name|DC_IS_CONEXANT
+parameter_list|(
+name|x
+parameter_list|)
+value|(x->dc_type == DC_TYPE_CONEXANT)
 end_define
 
 begin_comment
@@ -3608,6 +3629,28 @@ begin_comment
 comment|/* End of PNIC specific registers */
 end_comment
 
+begin_comment
+comment|/*  * CONEXANT specific registers.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DC_CONEXANT_PHYADDR
+value|0x1
+end_define
+
+begin_define
+define|#
+directive|define
+name|DC_CONEXANT_EE_NODEADDR
+value|0x19A
+end_define
+
+begin_comment
+comment|/* End of CONEXANT specific registers */
+end_comment
+
 begin_struct
 struct|struct
 name|dc_softc
@@ -4266,6 +4309,28 @@ define|#
 directive|define
 name|DC_DEVICEID_FE2500
 value|0xAB02
+end_define
+
+begin_comment
+comment|/*  * Conexant vendor ID.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DC_VENDORID_CONEXANT
+value|0x14f1
+end_define
+
+begin_comment
+comment|/*  * Conexant device IDs.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DC_DEVICEID_RS7112
+value|0x1803
 end_define
 
 begin_comment
