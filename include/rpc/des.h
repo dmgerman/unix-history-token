3931cade32fe01f7370d3f56fee76de51c19b6e1
Peter Wemm <peter@FreeBSD.org>
2001-09-10 01:15:48 +0000

    Stop gcc's fixincludes making an exception for this file
    Former-commit-id: af7bc957d6635945cdbedecb298209927e46b79d

--- include/rpc/des.h
+++ include/rpc/des.h
@@ -4,6 +4,10 @@ comment|/*  @(#)des.h	2.2 88/08/10 4.0 RPCSRC; from 2.7 88/02/08 SMI  */
 end_comment
 
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/*  * Sun RPC is a product of Sun Microsystems, Inc. and is provided for  * unrestricted use provided that this legend is included on all tape  * media and as a part of the software program in whole or part.  Users  * may copy or modify Sun RPC without charge, but are not authorized  * to license or distribute it to anyone else except as part of a product or  * program developed by the user.  *   * SUN RPC IS PROVIDED AS IS WITH NO WARRANTIES OF ANY KIND INCLUDING THE  * WARRANTIES OF DESIGN, MERCHANTIBILITY AND FITNESS FOR A PARTICULAR  * PURPOSE, OR ARISING FROM A COURSE OF DEALING, USAGE OR TRADE PRACTICE.  *   * Sun RPC is provided with no support and without any obligation on the  * part of Sun Microsystems, Inc. to assist in its use, correction,  * modification or enhancement.  *   * SUN MICROSYSTEMS, INC. SHALL HAVE NO LIABILITY WITH RESPECT TO THE  * INFRINGEMENT OF COPYRIGHTS, TRADE SECRETS OR ANY PATENTS BY SUN RPC  * OR ANY PART THEREOF.  *   * In no event will Sun Microsystems, Inc. be liable for any lost revenue  * or profits or other special, indirect and consequential damages, even if  * Sun has been advised of the possibility of such damages.  *   * Sun Microsystems, Inc.  * 2550 Garcia Avenue  * Mountain View, California  94043  */
 end_comment
 
@@ -138,7 +142,7 @@ begin_define
 define|#
 directive|define
 name|DESIOCBLOCK
-value|_IOWR(d, 6, struct desparams)
+value|_IOWR('d', 6, struct desparams)
 end_define
 
 begin_comment
@@ -149,7 +153,7 @@ begin_define
 define|#
 directive|define
 name|DESIOCQUICK
-value|_IOWR(d, 7, struct desparams)
+value|_IOWR('d', 7, struct desparams)
 end_define
 
 begin_endif
