c301738d52607a42425a5fc61c00c0fded377651
Robert N. M. Watson <rwatson@FreeBSD.org>
2001-09-25 21:08:33 +0000

    o So, when <dd> e-mailed me and said that the comment was inverted   for securelevel_ge() and securelevel_gt(), I was a little surprised,   but fixed it.  Turns out that it was the code that was inverted, during   a whitespace cleanup in my commit tree.  This commit inverts the   checks, and restores the comment.
    Former-commit-id: 426478c693892c9a0b179e0e95deae6895d75bfb

--- sys/kern/kern_prot.c
+++ sys/kern/kern_prot.c
@@ -5445,7 +5445,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Test securelevel values against passed required securelevel.  * _gt implements (securelevel> level), and _ge implements  * (securelevel>= level).  Returns 0 oer EPERM.  *  * cr is permitted to be NULL for the time being, as there were some  * existing securelevel checks that occurred without a process/credential  * context.  In the future this will be disallowed, so a kernel  * message is displayed.  *  * XXX: The redundant construction below is to facilitate the merging  * of support for per-jail securelevels, which maintain a local  * jail securelevel in the process credential.  */
+comment|/*  * Test securelevel values against passed required securelevel.  * _gt implements (level> securelevel), and _ge implements  * (level>= securelevel).  Returns 0 oer EPERM.  *  * cr is permitted to be NULL for the time being, as there were some  * existing securelevel checks that occurred without a process/credential  * context.  In the future this will be disallowed, so a kernel  * message is displayed.  *  * XXX: The redundant construction below is to facilitate the merging  * of support for per-jail securelevels, which maintain a local  * jail securelevel in the process credential.  */
 end_comment
 
 begin_function
@@ -5475,9 +5475,9 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|securelevel
-operator|>
 name|level
+operator|>
+name|securelevel
 condition|)
 return|return
 operator|(
@@ -5495,9 +5495,9 @@ else|else
 block|{
 if|if
 condition|(
-name|securelevel
-operator|>
 name|level
+operator|>
+name|securelevel
 condition|)
 return|return
 operator|(
@@ -5541,9 +5541,9 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|securelevel
-operator|>=
 name|level
+operator|>=
+name|securelevel
 condition|)
 return|return
 operator|(
@@ -5561,9 +5561,9 @@ else|else
 block|{
 if|if
 condition|(
-name|securelevel
-operator|>=
 name|level
+operator|>=
+name|securelevel
 condition|)
 return|return
 operator|(
