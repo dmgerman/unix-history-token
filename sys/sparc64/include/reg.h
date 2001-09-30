316f4e5d0e919d6f0c1a2d5b0eea2932e82ffc53
Jake Burkholder <jake@FreeBSD.org>
2001-09-30 19:59:07 +0000

    Add contents to struct *reg.
    Former-commit-id: d2b9ce1fe9a2c663e8fd927aac7cfb14ce0504c7

--- sys/sparc64/include/reg.h
+++ sys/sparc64/include/reg.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2001 Jake Burkholder.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1990 The Regents of the University of California.  * All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * William Jolitz.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: @(#)reg.h	5.5 (Berkeley) 1/18/91  *	from: FreeBSD: src/sys/i386/include/reg.h,v 1.23 2000/09/21  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -15,19 +15,66 @@ directive|define
 name|_MACHINE_REG_H_
 end_define
 
+begin_comment
+comment|/*  * Register set accessible via /proc/$pid/regs and PT_{SET,GET}REGS.  */
+end_comment
+
 begin_struct
 struct|struct
 name|reg
-block|{ }
+block|{
+name|u_long
+name|r_global
+index|[
+literal|8
+index|]
+decl_stmt|;
+name|u_long
+name|r_out
+index|[
+literal|8
+index|]
+decl_stmt|;
+name|u_long
+name|r_tstate
+decl_stmt|;
+name|u_long
+name|r_pc
+decl_stmt|;
+name|u_long
+name|r_npc
+decl_stmt|;
+name|u_int
+name|r_y
+decl_stmt|;
+block|}
 struct|;
 end_struct
+
+begin_comment
+comment|/*  * Register set accessible via /proc/$pid/fpregs.  */
+end_comment
 
 begin_struct
 struct|struct
 name|fpreg
-block|{ }
+block|{
+name|u_int
+name|fr_regs
+index|[
+literal|64
+index|]
+decl_stmt|;
+name|u_long
+name|fr_fsr
+decl_stmt|;
+block|}
 struct|;
 end_struct
+
+begin_comment
+comment|/*  * Register set accessible via /proc/$pid/dbregs.  */
+end_comment
 
 begin_struct
 struct|struct
@@ -41,6 +88,10 @@ ifdef|#
 directive|ifdef
 name|_KERNEL
 end_ifdef
+
+begin_comment
+comment|/*  * XXX these interfaces are MI, so they should be declared in a MI place.  */
+end_comment
 
 begin_function_decl
 name|int
@@ -93,23 +144,6 @@ name|s
 parameter_list|,
 name|u_long
 name|ps
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|set_dbregs
-parameter_list|(
-name|struct
-name|thread
-modifier|*
-name|td
-parameter_list|,
-name|struct
-name|dbreg
-modifier|*
-name|dbregs
 parameter_list|)
 function_decl|;
 end_function_decl
