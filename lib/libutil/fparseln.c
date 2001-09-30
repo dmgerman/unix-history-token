1e519e93b04b7c041f4bb9a40f78e80690879d9c
Matthew Dillon <dillon@FreeBSD.org>
2001-09-30 22:35:07 +0000

    Add __FBSDID()s to libutil
    Former-commit-id: adabaa7d4cae7f187a894646313f228d2f3378d2

--- lib/libutil/fparseln.c
+++ lib/libutil/fparseln.c
@@ -13,33 +13,13 @@ directive|include
 file|<sys/cdefs.h>
 end_include
 
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|LIBC_SCCS
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
-name|lint
-argument_list|)
-end_if
-
 begin_expr_stmt
-name|__RCSID
+name|__FBSDID
 argument_list|(
 literal|"$FreeBSD$"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
