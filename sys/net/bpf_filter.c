f01e6e218bb217bd7ef03433da6a7d43e6372768
Doug Rabson <dfr@FreeBSD.org>
2001-10-05 19:04:23 +0000

    Add ia64 to the list of machines which don't do unaligned reads.
    Former-commit-id: 346493685e89f137cde0ff8dbde3a9681534d5ed

--- sys/net/bpf_filter.c
+++ sys/net/bpf_filter.c
@@ -48,6 +48,11 @@ name|defined
 argument_list|(
 name|__alpha__
 argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__ia64__
+argument_list|)
 end_if
 
 begin_define
--- sys/net/if_loop.c
+++ sys/net/if_loop.c
@@ -1419,9 +1419,17 @@ argument_list|,
 name|hlen
 argument_list|)
 expr_stmt|;
-ifdef|#
-directive|ifdef
+if|#
+directive|if
+name|defined
+argument_list|(
 name|__alpha__
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__ia64__
+argument_list|)
 comment|/* The alpha doesn't like unaligned data. 		 * We move data down in the first mbuf */
 if|if
 condition|(
