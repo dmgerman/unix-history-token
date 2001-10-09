c58232d54cf9cbb2449dc6d7731db31259790570
John Baldwin <jhb@FreeBSD.org>
2001-10-09 17:25:30 +0000

    proces -> process in a comment.
    Former-commit-id: 2d7a9200d1242f1a74a7f14814f929aa374b87e5

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1049,7 +1049,7 @@ operator|=
 name|tmp
 expr_stmt|;
 block|}
-comment|/* 	 * For security and other reasons, signal handlers cannot 	 * be shared after an exec. The new proces gets a copy of the old 	 * handlers. In execsigs(), the new process will have its signals 	 * reset. 	 */
+comment|/* 	 * For security and other reasons, signal handlers cannot 	 * be shared after an exec. The new process gets a copy of the old 	 * handlers. In execsigs(), the new process will have its signals 	 * reset. 	 */
 if|if
 condition|(
 name|p
