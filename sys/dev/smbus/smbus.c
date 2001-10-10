3322994f39a9d4ce7a58bd4bc09d88caf855dd29
Murray Stokely <murray@FreeBSD.org>
2001-10-10 12:10:26 +0000

    MFC: SMBus support for the AMD 756 power management unit.
    Former-commit-id: c4418ce642c5f1fee46bef3eedbd09c82cc8cef1

--- sys/dev/smbus/smbus.c
+++ sys/dev/smbus/smbus.c
@@ -490,5 +490,23 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|DRIVER_MODULE
+argument_list|(
+name|smbus
+argument_list|,
+name|amdsmb
+argument_list|,
+name|smbus_driver
+argument_list|,
+name|smbus_devclass
+argument_list|,
+literal|0
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
