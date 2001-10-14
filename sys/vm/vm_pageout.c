4502943d2d602e33c4c8d8d0cd4980bdc3bf8ca8
Tor Egge <tegge@FreeBSD.org>
2001-10-14 20:51:14 +0000

    Don't remove all mappings of a swapped out process if the vm map contained wired entries.  vm_fault_unwire() depends on the mapping being intact.
    Reviewed by:	dillon
    
    
    Former-commit-id: c162aaf2f1226c704b49d857c70225f68ab2e179

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -2080,6 +2080,9 @@ name|obj
 decl_stmt|,
 name|bigobj
 decl_stmt|;
+name|int
+name|nothingwired
+decl_stmt|;
 name|GIANT_REQUIRED
 expr_stmt|;
 if|if
@@ -2110,6 +2113,10 @@ block|}
 name|bigobj
 operator|=
 name|NULL
+expr_stmt|;
+name|nothingwired
+operator|=
+name|TRUE
 expr_stmt|;
 comment|/* 	 * first, search out the biggest object, and try to free pages from 	 * that. 	 */
 name|tmpe
@@ -2192,6 +2199,18 @@ name|obj
 expr_stmt|;
 block|}
 block|}
+if|if
+condition|(
+name|tmpe
+operator|->
+name|wired_count
+operator|>
+literal|0
+condition|)
+name|nothingwired
+operator|=
+name|FALSE
+expr_stmt|;
 name|tmpe
 operator|=
 name|tmpe
@@ -2297,6 +2316,8 @@ condition|(
 name|desired
 operator|==
 literal|0
+operator|&&
+name|nothingwired
 condition|)
 name|pmap_remove
 argument_list|(
