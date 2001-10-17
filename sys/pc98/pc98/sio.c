dd6e1b0197b0962bdb2dff604c4ac8e95004ba55
Takahashi Yoshihiro <nyan@FreeBSD.org>
2001-10-17 14:03:01 +0000

    Merged from sys/isa/sio.c revision 1.291.2.21.
    Former-commit-id: a86a5882449d7415cb0afec70cf78a38cde0ada8

--- sys/pc98/pc98/sio.c
+++ sys/pc98/pc98/sio.c
@@ -10558,7 +10558,7 @@ name|device_printf
 argument_list|(
 name|dev
 argument_list|,
-literal|"unable to activate interrupt in fast mode - using normal mode"
+literal|"unable to activate interrupt in fast mode - using normal mode\n"
 argument_list|)
 expr_stmt|;
 block|}
