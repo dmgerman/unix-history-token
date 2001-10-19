4e0a3e8aa32e001337bc89071bcc8d7c4fc9a471
John Baldwin <jhb@FreeBSD.org>
2001-10-19 19:22:04 +0000

    Move sys/mutex.h include after sys/lock.h to ensure we use the proper versions of the mutex operations.
    Former-commit-id: 74902d2184ce290b110e9c4b98cc4140d2bf124c

--- sys/alpha/alpha/machdep.c
+++ sys/alpha/alpha/machdep.c
@@ -68,12 +68,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/mutex.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/ktr.h>
 end_include
 
@@ -99,6 +93,12 @@ begin_include
 include|#
 directive|include
 file|<sys/lock.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mutex.h>
 end_include
 
 begin_include
