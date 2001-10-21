f4bd1c904244bab105bb6cd4af2ddd6279a9bb69
Bruce Evans <bde@FreeBSD.org>
2001-10-21 06:05:08 +0000

    MFi386: - sys/pc98/pc98/npx.c 1.87 (2001/09/15; author: imp)   I don't think pc98 has acpi at all, so ifdef the acpi attachments for   now.
    This completes merging sys/pc98/pc98/npx.c into sys/i386/isa/npx.c so
    that the former can be removed.
    
    
    Former-commit-id: 1db3d33c2b07030075cb4fcd97c1c569e692297a

--- sys/amd64/isa/npx.c
+++ sys/amd64/isa/npx.c
@@ -3417,6 +3417,12 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|PC98
+end_ifndef
+
 begin_expr_stmt
 name|DRIVER_MODULE
 argument_list|(
@@ -3434,6 +3440,11 @@ literal|0
 argument_list|)
 expr_stmt|;
 end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
