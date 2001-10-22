9275222ca9495f68aa87d6d647c0c592a215870f
Dag-Erling Smorgrav <des@FreeBSD.org>
2001-10-22 15:32:12 +0000

    Back out part of previous commit: remove #include <sys/proc.h>.
    Former-commit-id: 2ddec587b466a7a9050f6f64f3721685d8fb5895

--- usr.bin/truss/main.c
+++ usr.bin/truss/main.c
@@ -54,12 +54,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/proc.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<err.h>
 end_include
 
--- usr.bin/truss/setup.c
+++ usr.bin/truss/setup.c
@@ -54,12 +54,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/proc.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/wait.h>
 end_include
 
