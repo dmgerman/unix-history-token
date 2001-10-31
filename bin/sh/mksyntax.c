a7ee92d7d44514dbb0920f7aae6cd890a296a9de
Tor Egge <tegge@FreeBSD.org>
2001-10-31 23:46:13 +0000

    MFC: Adjust some type checks to include CTLQUOTEMARK in the range of      shell control characters.
         Repair normally unused is_digit() macro.
    
    
    Former-commit-id: 4477563cd6ad2886e2aab73c13109956076b7ea1

--- bin/sh/mksyntax.c
+++ bin/sh/mksyntax.c
@@ -1659,13 +1659,13 @@ name|macro
 index|[]
 init|=
 block|{
-literal|"#define is_digit(c)\t((c>= 0&& is_type+SYNBASE)[c]& ISDIGIT)"
+literal|"#define is_digit(c)\t((is_type+SYNBASE)[c]& ISDIGIT)"
 block|,
-literal|"#define is_alpha(c)\t((c) != PEOF&& ((c)< CTLESC || (c)> CTLENDARI)&& isalpha((unsigned char) (c)))"
+literal|"#define is_alpha(c)\t((c) != PEOF&& ((c)< CTLESC || (c)> CTLQUOTEMARK)&& isalpha((unsigned char) (c)))"
 block|,
-literal|"#define is_name(c)\t((c) != PEOF&& ((c)< CTLESC || (c)> CTLENDARI)&& ((c) == '_' || isalpha((unsigned char) (c))))"
+literal|"#define is_name(c)\t((c) != PEOF&& ((c)< CTLESC || (c)> CTLQUOTEMARK)&& ((c) == '_' || isalpha((unsigned char) (c))))"
 block|,
-literal|"#define is_in_name(c)\t((c) != PEOF&& ((c)< CTLESC || (c)> CTLENDARI)&& ((c) == '_' || isalnum((unsigned char) (c))))"
+literal|"#define is_in_name(c)\t((c) != PEOF&& ((c)< CTLESC || (c)> CTLQUOTEMARK)&& ((c) == '_' || isalnum((unsigned char) (c))))"
 block|,
 literal|"#define is_special(c)\t((is_type+SYNBASE)[c]& (ISSPECL|ISDIGIT))"
 block|,
