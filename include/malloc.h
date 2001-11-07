47109eaa3e1bac9b23e3fa0f8f67a447d6a1924a
David E. O'Brien <obrien@FreeBSD.org>
2001-11-07 23:14:31 +0000

    Traditionally work in the -traditional case.
    Submitted by:	joerg
    
    
    Former-commit-id: c161348a6e7928423533f7ed318c75a429c304af

--- include/malloc.h
+++ include/malloc.h
@@ -3,11 +3,33 @@ begin_comment
 comment|/* $FreeBSD$ */
 end_comment
 
+begin_if
+if|#
+directive|if
+name|__STDC__
+end_if
+
 begin_error
 error|#
 directive|error
 literal|"<malloc.h> has been replaced by<stdlib.h>"
 end_error
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_include
+include|#
+directive|include
+file|<stdlib.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
