003e127edee6b9dce9d8311214a67deffd0149ea
Peter Wemm <peter@FreeBSD.org>
2001-11-19 07:07:09 +0000

    Use some (now) spare space for passing through a pointer to the FPSWA Interface provided by EFI (Floating Point SoftWare Assist).
    Former-commit-id: 2cd3c0520b6d16c66b0452c50d1a851deb1060c4

--- sys/ia64/include/bootinfo.h
+++ sys/ia64/include/bootinfo.h
@@ -33,10 +33,14 @@ comment|/* version 1 */
 name|u_int64_t
 name|bi_spare
 index|[
-literal|8
+literal|7
 index|]
 decl_stmt|;
 comment|/* was: name of booted kernel */
+name|u_int64_t
+name|bi_fpswa
+decl_stmt|;
+comment|/* FPSWA interface */
 name|u_int64_t
 name|bi_boothowto
 decl_stmt|;
