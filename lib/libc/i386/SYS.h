6422443561c84f3f84d7646d73d5b285904b14bb
John Baldwin <jhb@FreeBSD.org>
2001-11-27 20:51:26 +0000

    Use 'mov' instead of 'lea' for setting the syscall number in %eax as that is clearer about what we are actually doing.
    Requested by:	bde
    
    
    Former-commit-id: 19856a910bb1699d530fdbfe5c90fd73c1461f7b

--- lib/libc/i386/SYS.h
+++ lib/libc/i386/SYS.h
@@ -22,7 +22,7 @@ name|SYSCALL
 parameter_list|(
 name|x
 parameter_list|)
-value|2: PIC_PROLOGUE; jmp PIC_PLT(HIDENAME(cerror));	\ 			ENTRY(__CONCAT(__sys_,x));			\ 			.weak CNAME(x);					\ 			.set CNAME(x),CNAME(__CONCAT(__sys_,x));	\ 			.weak CNAME(__CONCAT(_,x));			\ 			.set CNAME(__CONCAT(_,x)),CNAME(__CONCAT(__sys_,x)); \ 			lea __CONCAT(SYS_,x),%eax; KERNCALL; jb 2b
+value|2: PIC_PROLOGUE; jmp PIC_PLT(HIDENAME(cerror));	\ 			ENTRY(__CONCAT(__sys_,x));			\ 			.weak CNAME(x);					\ 			.set CNAME(x),CNAME(__CONCAT(__sys_,x));	\ 			.weak CNAME(__CONCAT(_,x));			\ 			.set CNAME(__CONCAT(_,x)),CNAME(__CONCAT(__sys_,x)); \ 			mov __CONCAT($SYS_,x),%eax; KERNCALL; jb 2b
 end_define
 
 begin_define
@@ -42,7 +42,7 @@ name|PSEUDO
 parameter_list|(
 name|x
 parameter_list|)
-value|ENTRY(__CONCAT(__sys_,x));			\ 			.weak CNAME(__CONCAT(_,x));			\ 			.set CNAME(__CONCAT(_,x)),CNAME(__CONCAT(__sys_,x)); \ 			lea __CONCAT(SYS_,x), %eax; KERNCALL; ret
+value|ENTRY(__CONCAT(__sys_,x));			\ 			.weak CNAME(__CONCAT(_,x));			\ 			.set CNAME(__CONCAT(_,x)),CNAME(__CONCAT(__sys_,x)); \ 			mov __CONCAT($SYS_,x),%eax; KERNCALL; ret
 end_define
 
 begin_comment
