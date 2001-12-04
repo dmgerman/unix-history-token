7fdc007d7fe090cc48eab17e100ef512f395120f
Ian Dowse <iedowse@FreeBSD.org>
2001-12-04 16:53:42 +0000

    When VOP_SYMLINK fails, the value of *vpp is junk, so we must NULL out nd.ni_vp to prevent the resource cleanup code at the end of nfsrv_symlink from trying to vrele it. This fixes a "vrele: negative ref cnt" panic that can occur when a symlink is attempted on an NFS filesystem with no free space. Found locally, but the symptoms correspond to those in the PR referenced below.
    PR:		kern/26878
    MFC after:	3 days
    
    Former-commit-id: 96844bda2ea3cc6d772b6b1a8a9f2d7d79ed8dae

--- sys/nfsserver/nfs_serv.c
+++ sys/nfsserver/nfs_serv.c
@@ -13901,7 +13901,6 @@ name|NDF_ONLY_PNBUF
 argument_list|)
 expr_stmt|;
 else|else
-block|{
 name|vput
 argument_list|(
 name|nd
@@ -13915,7 +13914,6 @@ name|ni_vp
 operator|=
 name|NULL
 expr_stmt|;
-block|}
 comment|/* 	 * releases directory prior to potential lookup op. 	 */
 name|vput
 argument_list|(
