2752f4e4689c56b8b1b428de4342eb17a9e79abf
Mark Murray <markm@FreeBSD.org>
2001-12-11 23:39:38 +0000

    WARNS=2 fixes, use __FBSDID().
    Former-commit-id: 35f42147ea0da973121858a066de221d926973e2

--- usr.bin/tput/tput.c
+++ usr.bin/tput/tput.c
@@ -3,6 +3,20 @@ begin_comment
 comment|/*-  * Copyright (c) 1980, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -11,6 +25,7 @@ end_ifndef
 
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
 name|copyright
 index|[]
@@ -24,35 +39,20 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* not lint */
-end_comment
-
 begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
-begin_endif
-unit|static char sccsid[] = "@(#)tput.c	8.2 (Berkeley) 3/19/94";
-endif|#
-directive|endif
-end_endif
-
 begin_decl_stmt
 specifier|static
+specifier|const
 name|char
-name|rcsid
+name|sccsid
 index|[]
 init|=
-literal|"$FreeBSD$"
+literal|"@(#)tput.c	8.2 (Berkeley) 3/19/94"
 decl_stmt|;
 end_decl_stmt
 
@@ -60,10 +60,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
 
 begin_include
 include|#
@@ -156,6 +152,7 @@ name|process
 name|__P
 argument_list|(
 operator|(
+specifier|const
 name|char
 operator|*
 operator|,
@@ -199,9 +196,6 @@ modifier|*
 name|cptr
 decl_stmt|,
 modifier|*
-name|p
-decl_stmt|,
-modifier|*
 name|term
 decl_stmt|,
 name|buf
@@ -213,6 +207,11 @@ name|tbuf
 index|[
 literal|1024
 index|]
+decl_stmt|;
+specifier|const
+name|char
+modifier|*
+name|p
 decl_stmt|;
 name|term
 operator|=
@@ -577,14 +576,16 @@ name|str
 parameter_list|,
 name|argv
 parameter_list|)
+specifier|const
 name|char
 modifier|*
 name|cap
-decl_stmt|,
-decl|*
+decl_stmt|;
+name|char
+modifier|*
 name|str
 decl_stmt|,
-modifier|*
+decl|*
 modifier|*
 name|argv
 decl_stmt|;
