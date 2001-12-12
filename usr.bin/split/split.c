a82cdd06fc6f3babe1b8eb0f34ff034eadf9243d
Mark Murray <markm@FreeBSD.org>
2001-12-12 23:09:07 +0000

    WARNS=2 fixes, use __FBSDID, remove GCC-specific flags.
    Former-commit-id: e8b3539a00408fb30e92b351252865ece6f53ee7

--- usr.bin/split/split.c
+++ usr.bin/split/split.c
@@ -3,6 +3,20 @@ begin_comment
 comment|/*  * Copyright (c) 1987, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -25,36 +39,20 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* not lint */
-end_comment
-
 begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
-begin_else
-unit|static char sccsid[] = "@(#)split.c	8.2 (Berkeley) 4/16/94";
-else|#
-directive|else
-end_else
-
 begin_decl_stmt
 specifier|static
 specifier|const
 name|char
-name|rcsid
+name|sccsid
 index|[]
 init|=
-literal|"$FreeBSD$"
+literal|"@(#)split.c	8.2 (Berkeley) 4/16/94"
 decl_stmt|;
 end_decl_stmt
 
@@ -62,15 +60,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
 
 begin_include
 include|#
@@ -150,7 +139,7 @@ comment|/* Default num lines per file. */
 end_comment
 
 begin_decl_stmt
-name|size_t
+name|int
 name|bytecnt
 decl_stmt|;
 end_decl_stmt
@@ -809,14 +798,15 @@ parameter_list|()
 block|{
 name|size_t
 name|bcnt
-decl_stmt|,
-name|dist
-decl_stmt|,
-name|len
 decl_stmt|;
 name|char
 modifier|*
 name|C
+decl_stmt|;
+name|int
+name|dist
+decl_stmt|,
+name|len
 decl_stmt|;
 for|for
 control|(
@@ -877,6 +867,9 @@ name|bcnt
 operator|+
 name|len
 operator|>=
+operator|(
+name|u_int
+operator|)
 name|bytecnt
 condition|)
 block|{
@@ -942,9 +935,6 @@ name|ofd
 argument_list|,
 name|C
 argument_list|,
-operator|(
-name|int
-operator|)
 name|bytecnt
 argument_list|)
 operator|!=
