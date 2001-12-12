35629c9807362800364388fbc52fed0e5427c1f1
Alexey Zelkin <phantom@FreeBSD.org>
2001-12-12 15:41:08 +0000

    * style(9)'ify * add K&R support to langinfo.h
    Requested by:	bde (2)
    
    
    Former-commit-id: 1e4f718d6c09abf3e034ef608516446d8b687035

--- include/monetary.h
+++ include/monetary.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_MONETARY_H
+name|_MONETARY_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_MONETARY_H
+name|_MONETARY_H_
 end_define
 
 begin_include
@@ -104,7 +104,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_MONETARY_H */
+comment|/* !_MONETARY_H_ */
 end_comment
 
 end_unit
