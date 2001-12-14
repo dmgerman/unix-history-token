260e851476003af0dfa8d0797df871ade2b15287
Jonathan Lemon <jlemon@FreeBSD.org>
2001-12-14 19:56:19 +0000

    MFC: r1.99; set some values before calling if_attach().
    Former-commit-id: be82875ec6953af2db77119aed5735897f118ec0

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -3281,11 +3281,6 @@ name|sockaddr_dl
 modifier|*
 name|sdl
 decl_stmt|;
-name|if_attach
-argument_list|(
-name|ifp
-argument_list|)
-expr_stmt|;
 name|ifp
 operator|->
 name|if_type
@@ -3303,6 +3298,11 @@ operator|->
 name|if_hdrlen
 operator|=
 literal|14
+expr_stmt|;
+name|if_attach
+argument_list|(
+name|ifp
+argument_list|)
 expr_stmt|;
 name|ifp
 operator|->
