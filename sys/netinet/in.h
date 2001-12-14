483440e03ba9db934ce1e383f586ddaff0064aa3
Jonathan Lemon <jlemon@FreeBSD.org>
2001-12-14 19:53:55 +0000

    MFC: r1.57; centralize macro definitions.
    Former-commit-id: 7c800142bf77d8c819cff2545c4edc8dd91aae3b

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -2450,6 +2450,36 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
+begin_define
+define|#
+directive|define
+name|satosin
+parameter_list|(
+name|sa
+parameter_list|)
+value|((struct sockaddr_in *)(sa))
+end_define
+
+begin_define
+define|#
+directive|define
+name|sintosa
+parameter_list|(
+name|sin
+parameter_list|)
+value|((struct sockaddr *)(sin))
+end_define
+
+begin_define
+define|#
+directive|define
+name|ifatoia
+parameter_list|(
+name|ifa
+parameter_list|)
+value|((struct in_ifaddr *)(ifa))
+end_define
+
 begin_endif
 endif|#
 directive|endif
