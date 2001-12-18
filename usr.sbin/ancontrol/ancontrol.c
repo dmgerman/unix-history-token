dcbb2a7f0d8b04f2a38af94f1ba6f80e59df0e8f
Pierre Beyssac <pb@FreeBSD.org>
2001-12-18 23:28:45 +0000

    Fix typo in usage (power same -> power save).
    Former-commit-id: 220b4b29a456929da98484c4d7f369d90e5dee83

--- usr.sbin/ancontrol/ancontrol.c
+++ usr.sbin/ancontrol/ancontrol.c
@@ -4699,7 +4699,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\t%s -i iface -s 0|1|2|3 (set power same mode)\n"
+literal|"\t%s -i iface -s 0|1|2|3 (set power save mode)\n"
 argument_list|,
 name|p
 argument_list|)
