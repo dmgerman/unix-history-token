e3acdf5c2165fbce83c53853f7b9228b85fcbc9d
Sheldon Hearn <sheldonh@FreeBSD.org>
2001-12-27 01:59:11 +0000

    MFC rev 1.79: add nsmb.conf to resurrection list
    Approved by:	re
    
    
    Former-commit-id: e8ea6f5f655688b9437e81b46331e9e4388ca022

--- release/sysinstall/installUpgrade.c
+++ release/sysinstall/installUpgrade.c
@@ -556,6 +556,16 @@ block|,
 block|{
 name|JUST_COPY
 block|,
+literal|"nsmb.conf"
+block|,
+name|TRUE
+block|,
+name|NULL
+block|}
+block|,
+block|{
+name|JUST_COPY
+block|,
 literal|"pam.conf"
 block|,
 name|TRUE
