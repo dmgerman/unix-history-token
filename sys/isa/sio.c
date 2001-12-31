fc08d3dd438bb0974835142f83273eb8b7e02ab1
Sheldon Hearn <sheldonh@FreeBSD.org>
2001-12-31 09:23:40 +0000

    MFC src/sys/dev/sio/sio_isa.c rev 1.4: add PNP Id for Create Labs Phoneblaster
    Approved by:	re
    
    
    Former-commit-id: d6ee200f4c292cbd441d8ddb1d35ebca60ed71a2

--- sys/isa/sio.c
+++ sys/isa/sio.c
@@ -3266,6 +3266,13 @@ block|}
 block|,
 comment|/* CRD0001 - Cardinal MVP288IV ? */
 block|{
+literal|0x01308c0e
+block|,
+name|NULL
+block|}
+block|,
+comment|/* CTL3001 - Creative Labs Phoneblaster */
+block|{
 literal|0x36033610
 block|,
 name|NULL
