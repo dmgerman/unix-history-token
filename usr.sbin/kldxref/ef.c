247f7ec721cae33188b116dbb7565b006e44ff04
John Baldwin <jhb@FreeBSD.org>
2002-01-03 19:10:49 +0000

    Remove unnecessary machine/bootinfo.h includes.
    Submitted by:	jake
    
    
    Former-commit-id: 6284adc0bd53fba027722f224b7db1a88ef6e9af

--- usr.sbin/kldxref/ef.c
+++ usr.sbin/kldxref/ef.c
@@ -48,12 +48,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/bootinfo.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/elf.h>
 end_include
 
--- usr.sbin/kldxref/kldxref.c
+++ usr.sbin/kldxref/kldxref.c
@@ -84,12 +84,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/bootinfo.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/elf.h>
 end_include
 
