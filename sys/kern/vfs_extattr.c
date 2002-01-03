97323c029d87562c944ecc488403e6b441e1a75d
Stefan Esser <se@FreeBSD.org>
2002-01-03 09:54:24 +0000

    Return EBADF in case some vnode field has been reset to a NULL pointer. (There has been some discussion, whether ENOENT or EBADF is more appropriate. I choose the latter, since the operation is not supported on the file descriptor at that time, even if it was, immediately before.)
    PR:		32681
    Reviewed by:	dillon, iedowse, ...
    Approved by:	nectar
    MFC after:	3 days
    		(pending RE approval)
    
    Former-commit-id: 17f94d1187e27fdae8993e298deaa124f478efc3

--- sys/kern/vfs_extattr.c
+++ sys/kern/vfs_extattr.c
@@ -4040,6 +4040,17 @@ operator|)
 operator|->
 name|v_mount
 expr_stmt|;
+if|if
+condition|(
+name|mp
+operator|==
+name|NULL
+condition|)
+return|return
+operator|(
+name|EBADF
+operator|)
+return|;
 name|sp
 operator|=
 operator|&
