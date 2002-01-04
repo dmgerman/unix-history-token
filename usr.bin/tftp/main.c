4fb322db1423b2c0100e40dc76ff0bdf74ee9812
Guido van Rooij <guido@FreeBSD.org>
2002-01-04 13:42:14 +0000

    Fix the coredump that occurs when, from the tfpt prompt, a 'c' command is issued without an argument.
    Former-commit-id: a851ef79a89ede320fe1b9a5d9f1be807d81c751

--- usr.bin/tftp/main.c
+++ usr.bin/tftp/main.c
@@ -1161,9 +1161,17 @@ expr_stmt|;
 block|}
 if|if
 condition|(
+operator|(
 name|argc
+operator|<
+literal|2
+operator|)
+operator|||
+operator|(
+name|argv
 operator|>
 literal|3
+operator|)
 condition|)
 block|{
 name|printf
