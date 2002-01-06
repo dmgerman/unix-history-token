dcebb06e1f2488be68ae55b979a81151a20bb57a
Mike Barcroft <mike@FreeBSD.org>
2002-01-06 19:57:26 +0000

    Make the utility name in the usage() diagnostic match the actual utility name.
    Submitted by:	3d
    PR:		33496
    
    Former-commit-id: 8fab2259f2ee4b7593e0cec15828f75a6c551b98

--- usr.sbin/diskpart/diskpart.c
+++ usr.sbin/diskpart/diskpart.c
@@ -1978,7 +1978,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: disktab [-p] [-d] [-s size] disk-type\n"
+literal|"usage: diskpart [-p] [-d] [-s size] disk-type\n"
 argument_list|)
 expr_stmt|;
 name|exit
