009b237cf0ff1fa2f73721eb7ab68da7e6f11181
Jake Burkholder <jake@FreeBSD.org>
2002-01-08 05:06:39 +0000

    Prototype pmap_map_tsb().
    Former-commit-id: a77aaaccdb4677cb7fb64e98d66fb4fb67af4aa0

--- sys/sparc64/include/pmap.h
+++ sys/sparc64/include/pmap.h
@@ -269,6 +269,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|pmap_map_tsb
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_define
 define|#
 directive|define
