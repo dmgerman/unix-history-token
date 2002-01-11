0efe9cc95af9eae096e20cc990dc8b57810d0ac6
Ruslan Ermilov <ru@FreeBSD.org>
2002-01-11 19:06:48 +0000

    Don't format lines that look like troff requests, for compatibility with old fmt(1).  New option -n permits formatting of lines beginning with a dot character.
    PR:		bin/31392
    MFC after:	3 days
    
    Former-commit-id: de1bc8222fa00bf65502ec5d0dfd38af581594bf

--- usr.bin/fmt/fmt.c
+++ usr.bin/fmt/fmt.c
@@ -4,7 +4,7 @@ comment|/*	$OpenBSD: fmt.c,v 1.16 2000/06/25 15:35:42 pjanzen Exp $	*/
 end_comment
 
 begin_comment
-comment|/* Sensible version of fmt  *  * Syntax: fmt [ options ] [ goal [ max ] ] [ filename ... ]  *  * Since the documentation for the original fmt is so poor, here  * is an accurate description of what this one does. It's usually  * the same. The *mechanism* used may differ from that suggested  * here. Note that we are *not* entirely compatible with fmt,  * because fmt gets so many things wrong.  *  * 1. Tabs are expanded, assuming 8-space tab stops.  *    If the `-t<n>' option is given, we assume<n>-space  *    tab stops instead.  *    Trailing blanks are removed from all lines.  *    x\b == nothing, for any x other than \b.  *    Other control characters are simply stripped. This  *    includes \r.  * 2. Each line is split into leading whitespace and  *    everything else. Maximal consecutive sequences of  *    lines with the same leading whitespace are considered  *    to form paragraphs, except that a blank line is always  *    a paragraph to itself.  *    If the `-p' option is given then the first line of a  *    paragraph is permitted to have indentation different  *    from that of the other lines.  *    If the `-m' option is given then a line that looks  *    like a mail message header, if it is not immediately  *    preceded by a non-blank non-message-header line, is  *    taken to start a new paragraph, which also contains  *    any subsequent lines with non-empty leading whitespace.  * 3. The "everything else" is split into words; a word  *    includes its trailing whitespace, and a word at the  *    end of a line is deemed to be followed by a single  *    space, or two spaces if it ends with a sentence-end  *    character. (See the `-d' option for how to change that.)  *    If the `-s' option has been given, then a word's trailing  *    whitespace is replaced by what it would have had if it  *    had occurred at end of line.  * 4. Each paragraph is sent to standard output as follows.  *    We output the leading whitespace, and then enough words  *    to make the line length as near as possible to the goal  *    without exceeding the maximum. (If a single word would  *    exceed the maximum, we output that anyway.) Of course  *    the trailing whitespace of the last word is ignored.  *    We then emit a newline and start again if there are any  *    words left.  *    Note that for a blank line this translates as "We emit  *    a newline".  *    If the `-l<n>' option is given, then leading whitespace  *    is modified slightly:<n> spaces are replaced by a tab.  *    Indented paragraphs (see above under `-p') make matters  *    more complicated than this suggests. Actually every paragraph  *    has two `leading whitespace' values; the value for the first  *    line, and the value for the most recent line. (While processing  *    the first line, the two are equal. When `-p' has not been  *    given, they are always equal.) The leading whitespace  *    actually output is that of the first line (for the first  *    line of *output*) or that of the most recent line (for  *    all other lines of output).  *    When `-m' has been given, message header paragraphs are  *    taken as having first-leading-whitespace empty and  *    subsequent-leading-whitespace two spaces.  *  * Multiple input files are formatted one at a time, so that a file  * never ends in the middle of a line.  *  * There's an alternative mode of operation, invoked by giving  * the `-c' option. In that case we just center every line,  * and most of the other options are ignored. This should  * really be in a separate program, but we must stay compatible  * with old `fmt'.  *  * QUERY: Should `-m' also try to do the right thing with quoted text?  * QUERY: `-b' to treat backslashed whitespace as old `fmt' does?  * QUERY: Option meaning `never join lines'?  * QUERY: Option meaning `split in mid-word to avoid overlong lines'?  * (Those last two might not be useful, since we have `fold'.)  *  * Differences from old `fmt':  *  *   - We have many more options. Options that aren't understood  *     generate a lengthy usage message, rather than being  *     treated as filenames.  *   - Even with `-m', our handling of message headers is  *     significantly different. (And much better.)  *   - We don't treat `\ ' as non-word-breaking.  *   - Downward changes of indentation start new paragraphs  *     for us, as well as upward. (I think old `fmt' behaves  *     in the way it does in order to allow indented paragraphs,  *     but this is a broken way of making indented paragraphs  *     behave right.)  *   - Given the choice of going over or under |goal_length|  *     by the same amount, we go over; old `fmt' goes under.  *   - We treat `?' as ending a sentence, and not `:'. Old `fmt'  *     does the reverse.  *   - We return approved return codes. Old `fmt' returns  *     1 for some errors, and *the number of unopenable files*  *     when that was all that went wrong.  *   - We have fewer crashes and more helpful error messages.  *   - We don't turn spaces into tabs at starts of lines unless  *     specifically requested.  *   - New `fmt' is somewhat smaller and slightly faster than  *     old `fmt'.  *  * Bugs:  *  *   None known. There probably are some, though.  *  * Portability:  *  *   I believe this code to be pretty portable. It does require  *   that you have `getopt'. If you need to include "getopt.h"  *   for this (e.g., if your system didn't come with `getopt'  *   and you installed it yourself) then you should arrange for  *   NEED_getopt_h to be #defined.  *  *   Everything here should work OK even on nasty 16-bit  *   machines and nice 64-bit ones. However, it's only really  *   been tested on my FreeBSD machine. Your mileage may vary.  */
+comment|/* Sensible version of fmt  *  * Syntax: fmt [ options ] [ goal [ max ] ] [ filename ... ]  *  * Since the documentation for the original fmt is so poor, here  * is an accurate description of what this one does. It's usually  * the same. The *mechanism* used may differ from that suggested  * here. Note that we are *not* entirely compatible with fmt,  * because fmt gets so many things wrong.  *  * 1. Tabs are expanded, assuming 8-space tab stops.  *    If the `-t<n>' option is given, we assume<n>-space  *    tab stops instead.  *    Trailing blanks are removed from all lines.  *    x\b == nothing, for any x other than \b.  *    Other control characters are simply stripped. This  *    includes \r.  * 2. Each line is split into leading whitespace and  *    everything else. Maximal consecutive sequences of  *    lines with the same leading whitespace are considered  *    to form paragraphs, except that a blank line is always  *    a paragraph to itself.  *    If the `-p' option is given then the first line of a  *    paragraph is permitted to have indentation different  *    from that of the other lines.  *    If the `-m' option is given then a line that looks  *    like a mail message header, if it is not immediately  *    preceded by a non-blank non-message-header line, is  *    taken to start a new paragraph, which also contains  *    any subsequent lines with non-empty leading whitespace.  *    Unless the `-n' option is given, lines beginning with  *    a . (dot) are not formatted.  * 3. The "everything else" is split into words; a word  *    includes its trailing whitespace, and a word at the  *    end of a line is deemed to be followed by a single  *    space, or two spaces if it ends with a sentence-end  *    character. (See the `-d' option for how to change that.)  *    If the `-s' option has been given, then a word's trailing  *    whitespace is replaced by what it would have had if it  *    had occurred at end of line.  * 4. Each paragraph is sent to standard output as follows.  *    We output the leading whitespace, and then enough words  *    to make the line length as near as possible to the goal  *    without exceeding the maximum. (If a single word would  *    exceed the maximum, we output that anyway.) Of course  *    the trailing whitespace of the last word is ignored.  *    We then emit a newline and start again if there are any  *    words left.  *    Note that for a blank line this translates as "We emit  *    a newline".  *    If the `-l<n>' option is given, then leading whitespace  *    is modified slightly:<n> spaces are replaced by a tab.  *    Indented paragraphs (see above under `-p') make matters  *    more complicated than this suggests. Actually every paragraph  *    has two `leading whitespace' values; the value for the first  *    line, and the value for the most recent line. (While processing  *    the first line, the two are equal. When `-p' has not been  *    given, they are always equal.) The leading whitespace  *    actually output is that of the first line (for the first  *    line of *output*) or that of the most recent line (for  *    all other lines of output).  *    When `-m' has been given, message header paragraphs are  *    taken as having first-leading-whitespace empty and  *    subsequent-leading-whitespace two spaces.  *  * Multiple input files are formatted one at a time, so that a file  * never ends in the middle of a line.  *  * There's an alternative mode of operation, invoked by giving  * the `-c' option. In that case we just center every line,  * and most of the other options are ignored. This should  * really be in a separate program, but we must stay compatible  * with old `fmt'.  *  * QUERY: Should `-m' also try to do the right thing with quoted text?  * QUERY: `-b' to treat backslashed whitespace as old `fmt' does?  * QUERY: Option meaning `never join lines'?  * QUERY: Option meaning `split in mid-word to avoid overlong lines'?  * (Those last two might not be useful, since we have `fold'.)  *  * Differences from old `fmt':  *  *   - We have many more options. Options that aren't understood  *     generate a lengthy usage message, rather than being  *     treated as filenames.  *   - Even with `-m', our handling of message headers is  *     significantly different. (And much better.)  *   - We don't treat `\ ' as non-word-breaking.  *   - Downward changes of indentation start new paragraphs  *     for us, as well as upward. (I think old `fmt' behaves  *     in the way it does in order to allow indented paragraphs,  *     but this is a broken way of making indented paragraphs  *     behave right.)  *   - Given the choice of going over or under |goal_length|  *     by the same amount, we go over; old `fmt' goes under.  *   - We treat `?' as ending a sentence, and not `:'. Old `fmt'  *     does the reverse.  *   - We return approved return codes. Old `fmt' returns  *     1 for some errors, and *the number of unopenable files*  *     when that was all that went wrong.  *   - We have fewer crashes and more helpful error messages.  *   - We don't turn spaces into tabs at starts of lines unless  *     specifically requested.  *   - New `fmt' is somewhat smaller and slightly faster than  *     old `fmt'.  *  * Bugs:  *  *   None known. There probably are some, though.  *  * Portability:  *  *   I believe this code to be pretty portable. It does require  *   that you have `getopt'. If you need to include "getopt.h"  *   for this (e.g., if your system didn't come with `getopt'  *   and you installed it yourself) then you should arrange for  *   NEED_getopt_h to be #defined.  *  *   Everything here should work OK even on nasty 16-bit  *   machines and nice 64-bit ones. However, it's only really  *   been tested on my FreeBSD machine. Your mileage may vary.  */
 end_comment
 
 begin_comment
@@ -405,6 +405,19 @@ end_comment
 begin_decl_stmt
 specifier|static
 name|int
+name|format_troff
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* Format troff? */
+end_comment
+
+begin_decl_stmt
+specifier|static
+name|int
 name|n_errors
 init|=
 literal|0
@@ -672,7 +685,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"0123456789cd:hl:mpst:w:"
+literal|"0123456789cd:hl:mnpst:w:"
 argument_list|)
 operator|)
 operator|!=
@@ -688,6 +701,10 @@ case|case
 literal|'c'
 case|:
 name|centerP
+operator|=
+literal|1
+expr_stmt|;
+name|format_troff
 operator|=
 literal|1
 expr_stmt|;
@@ -719,6 +736,14 @@ case|case
 literal|'m'
 case|:
 name|grok_mail_headers
+operator|=
+literal|1
+expr_stmt|;
+continue|continue;
+case|case
+literal|'n'
+case|:
+name|format_troff
 operator|=
 literal|1
 expr_stmt|;
@@ -895,6 +920,7 @@ literal|"Options: -c     center each line instead of formatting\n"
 literal|"         -d<chars> double-space after<chars> at line end\n"
 literal|"         -l<n> turn each<n> spaces at start of line into a tab\n"
 literal|"         -m     try to make sure mail header lines stay separate\n"
+literal|"         -n     format lines beginning with a dot\n"
 literal|"         -p     allow indented paragraphs\n"
 literal|"         -s     coalesce whitespace inside lines\n"
 literal|"         -t<n> have tabs every<n> columns\n"
@@ -1301,12 +1327,24 @@ operator|=
 name|hdr_Continuation
 expr_stmt|;
 block|}
-comment|/* We need a new paragraph if and only if:        *   this line is blank,        *   OR it's a mail header,        *   OR it's not a mail header AND the last line was one,        *   OR the indentation has changed        *      AND the line isn't a mail header continuation line        *      AND this isn't the second line of an indented paragraph.        */
+comment|/* We need a new paragraph if and only if:        *   this line is blank,        *   OR it's a troff request (and we don't format troff),        *   OR it's a mail header,        *   OR it's not a mail header AND the last line was one,        *   OR the indentation has changed        *      AND the line isn't a mail header continuation line        *      AND this isn't the second line of an indented paragraph.        */
 if|if
 condition|(
 name|length
 operator|==
 literal|0
+operator|||
+operator|(
+name|line
+index|[
+literal|0
+index|]
+operator|==
+literal|'.'
+operator|&&
+operator|!
+name|format_troff
+operator|)
 operator|||
 name|header_type
 operator|==
@@ -1381,11 +1419,42 @@ condition|(
 name|length
 operator|==
 literal|0
+operator|||
+operator|(
+name|line
+index|[
+literal|0
+index|]
+operator|==
+literal|'.'
+operator|&&
+operator|!
+name|format_troff
+operator|)
 condition|)
 block|{
+if|if
+condition|(
+name|length
+operator|==
+literal|0
+condition|)
 name|putchar
 argument_list|(
 literal|'\n'
+argument_list|)
+expr_stmt|;
+else|else
+name|printf
+argument_list|(
+literal|"%.*s\n"
+argument_list|,
+operator|(
+name|int
+operator|)
+name|length
+argument_list|,
+name|line
 argument_list|)
 expr_stmt|;
 name|prev_header_type
@@ -2226,6 +2295,11 @@ name|spaces_pending
 init|=
 literal|0
 decl_stmt|;
+name|int
+name|troff
+init|=
+literal|0
+decl_stmt|;
 if|if
 condition|(
 name|buf
@@ -2265,6 +2339,25 @@ condition|)
 block|{
 if|if
 condition|(
+name|len
+operator|+
+name|spaces_pending
+operator|==
+literal|0
+operator|&&
+name|ch
+operator|==
+literal|'.'
+operator|&&
+operator|!
+name|format_troff
+condition|)
+name|troff
+operator|=
+literal|1
+expr_stmt|;
+if|if
+condition|(
 name|ch
 operator|==
 literal|' '
@@ -2275,6 +2368,8 @@ expr_stmt|;
 elseif|else
 if|if
 condition|(
+name|troff
+operator|||
 name|isprint
 argument_list|(
 name|ch
