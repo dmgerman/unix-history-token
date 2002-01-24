a0189e3be8f5565bb5f0a656bd55662910cbf6ea
David Malone <dwmalone@FreeBSD.org>
2002-01-24 22:27:50 +0000

    Make usage message and man page synopsis reflect the fact that -n and -c are mutually exclusive.
    PR:		34233
    Submitted by:	Gary W. Swearingen <swear@blarg.net>
    MFC after:	3 days
    
    Former-commit-id: 56ebc9ab7ae748cb935f9a2d6821bc98ed982de9

--- usr.bin/head/head.c
+++ usr.bin/head/head.c
@@ -819,7 +819,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: head [-n lines] [-c bytes] [file ...]\n"
+literal|"usage: head [-n lines | -c bytes] [file ...]\n"
 argument_list|)
 expr_stmt|;
 name|exit
