20042a2defaa0f36c54d4c028421b2eca0f508a2
David E. O'Brien <obrien@FreeBSD.org>
2002-01-27 22:47:22 +0000

    Change the way the version strings are handled.
    Former-commit-id: 2c4a482bd8bc5ab52cf32fa27915e943a489aa57

--- gnu/usr.bin/binutils/libbfd/i386/bfd.h
+++ gnu/usr.bin/binutils/libbfd/i386/bfd.h
@@ -87,14 +87,8 @@ directive|endif
 endif|#
 directive|endif
 comment|/* #define BFD_VERSION 211930000 */
-define|#
-directive|define
-name|BFD_VERSION_DATE
-value|20020126
-define|#
-directive|define
-name|BFD_VERSION_STRING
-value|"2.11.93 20020126"
+comment|/* #define BFD_VERSION_DATE 20020126 */
+comment|/* #define BFD_VERSION_STRING "2.11.93 20020126" */
 comment|/* The word size used by BFD on the host.  This may be 64 with a 32    bit target if the host is 64 bit, or if other 64 bit targets have    been selected with --enable-targets, or if --enable-64-bit-bfd.  */
 define|#
 directive|define
