bfc62dbac4eedb0613017c006886494d3b93c974
Matthew Dillon <dillon@FreeBSD.org>
2002-01-31 18:39:44 +0000

    GC P_BUFEXHAUST leftovers, we've had a new mechanism to avoid buffer cache lockups for over a year now.
    MFC after:		0 days
    
    
    Former-commit-id: 348ad31d246d7a450a01941b4767a0ccd0c6c5fc

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -6932,12 +6932,6 @@ name|SHUTDOWN_PRI_LAST
 argument_list|)
 expr_stmt|;
 comment|/* 	 * This process is allowed to take the buffer cache to the limit 	 */
-name|curproc
-operator|->
-name|p_flag
-operator||=
-name|P_BUFEXHAUST
-expr_stmt|;
 name|s
 operator|=
 name|splbio
