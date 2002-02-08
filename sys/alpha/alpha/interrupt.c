27669c37f89e81bc1d9b8fb8254b1e7bfc7d8e0a
John Baldwin <jhb@FreeBSD.org>
2002-02-08 18:23:46 +0000

    Turn interrupt thread preemption back on on the alpha.
    Tested by:	mjacob
    
    
    Former-commit-id: dd5b44c3f412a2a80406819b11e786018365de9d

--- sys/alpha/alpha/interrupt.c
+++ sys/alpha/alpha/interrupt.c
@@ -1788,10 +1788,10 @@ name|ithread_schedule
 argument_list|(
 name|ithd
 argument_list|,
-literal|0
+operator|!
+name|cold
 argument_list|)
 expr_stmt|;
-comment|/* XXX:no preemption for now */
 name|KASSERT
 argument_list|(
 name|error
