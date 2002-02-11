3410218920e777f41ce5afaafb4df14ebe4dd2df
Takahashi Yoshihiro <nyan@FreeBSD.org>
2002-02-11 04:50:11 +0000

    MFC: Correct typo.
    Former-commit-id: 6852d295ec3c3824287dbc21474f68a1f5f72dba

--- sys/boot/pc98/libpc98/vidconsole.c
+++ sys/boot/pc98/libpc98/vidconsole.c
@@ -3402,7 +3402,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * This routine uses an inb to an unused port, the time to execute that  * inb is approximately 1.25uS.  This value is pretty constant across  * all CPU's and all buses, with the exception of some PCI implentations  * that do not forward this I/O adress to the ISA bus as they know it  * is not a valid ISA bus address, those machines execute this inb in  * 60 nS :-(.  *  */
+comment|/*  * This routine uses an inb to an unused port, the time to execute that  * inb is approximately 1.25uS.  This value is pretty constant across  * all CPU's and all buses, with the exception of some PCI implentations  * that do not forward this I/O address to the ISA bus as they know it  * is not a valid ISA bus address, those machines execute this inb in  * 60 nS :-(.  *  */
 end_comment
 
 begin_function
