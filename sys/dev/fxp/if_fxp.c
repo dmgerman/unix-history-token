9f0583ac77fe9e3b8179dc88b33ea1e675b61316
Mike Silbersack <silby@FreeBSD.org>
2002-02-11 23:22:43 +0000

    MFC rev 1.128
      Remove mbuf exhaustion warning messages; these are handled by the
      mbuf system in a rate-limited fashion now.
    
    
    Former-commit-id: b4a704755a0301eaafffcc83e39a02e2ed4737bc

--- sys/dev/fxp/if_fxp.c
+++ sys/dev/fxp/if_fxp.c
@@ -7941,15 +7941,6 @@ operator|==
 literal|0
 condition|)
 block|{
-name|device_printf
-argument_list|(
-name|sc
-operator|->
-name|dev
-argument_list|,
-literal|"cluster allocation failed, packet dropped!\n"
-argument_list|)
-expr_stmt|;
 name|m_freem
 argument_list|(
 name|m
@@ -7982,15 +7973,6 @@ block|}
 block|}
 else|else
 block|{
-name|device_printf
-argument_list|(
-name|sc
-operator|->
-name|dev
-argument_list|,
-literal|"mbuf allocation failed, packet dropped!\n"
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|oldm
