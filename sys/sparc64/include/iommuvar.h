50cf393a8e56aeba559340d112136b0cad06ff39
Thomas Moestl <tmm@FreeBSD.org>
2002-02-13 15:59:17 +0000

    Merge r1.42 of iommu.c and r1.9 of iommuvar.h from NetBSD (this adds support for managing both streaming caches on psycho pairs). Use explicit bus space accesses instead of mapping the device memory into kva. Move DVMA allocation to the map creation/dma memory allocation functions.
    Former-commit-id: 68b2a2ea0b45bb09b4658fd4fee349c946ddd084

--- sys/sparc64/include/iommuvar.h
+++ sys/sparc64/include/iommuvar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1999 Matthew R. Green  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: NetBSD: iommuvar.h,v 1.7 2001/07/20 00:07:13 eeh Exp  *  * $FreeBSD$  */
+comment|/*  * Copyright (c) 1999 Matthew R. Green  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: NetBSD: iommuvar.h,v 1.9 2001/07/20 00:07:13 eeh Exp  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -13,6 +13,47 @@ begin_define
 define|#
 directive|define
 name|_MACHINE_IOMMUVAR_H_
+end_define
+
+begin_define
+define|#
+directive|define
+name|IO_PAGE_SIZE
+value|PAGE_SIZE_8K
+end_define
+
+begin_define
+define|#
+directive|define
+name|IO_PAGE_MASK
+value|PAGE_MASK_8K
+end_define
+
+begin_define
+define|#
+directive|define
+name|IO_PAGE_SHIFT
+value|PAGE_SHIFT_8K
+end_define
+
+begin_define
+define|#
+directive|define
+name|round_io_page
+parameter_list|(
+name|x
+parameter_list|)
+value|round_page(x)
+end_define
+
+begin_define
+define|#
+directive|define
+name|trunc_io_page
+parameter_list|(
+name|x
+parameter_list|)
+value|trunc_page(x)
 end_define
 
 begin_comment
@@ -42,7 +83,7 @@ decl_stmt|;
 name|int64_t
 name|is_cr
 decl_stmt|;
-comment|/* IOMMU control regiter value */
+comment|/* IOMMU control register value */
 name|struct
 name|rman
 name|is_dvma_rman
@@ -50,52 +91,66 @@ decl_stmt|;
 comment|/* DVMA map for this instance */
 name|vm_offset_t
 name|is_flushpa
+index|[
+literal|2
+index|]
 decl_stmt|;
-comment|/* used to flush the SBUS */
-comment|/* Needs to be volatile or egcs optimizes away loads */
 specifier|volatile
 name|int64_t
+modifier|*
+name|is_flushva
+index|[
+literal|2
+index|]
+decl_stmt|;
+comment|/* 	 * When a flush is completed, 64 bytes will be stored at the given 	 * location, the first double word being 1, to indicate completion. 	 * The lower 6 address bits are ignored, so the addresses need to be 	 * suitably aligned; over-allocate a large enough margin to be able 	 * to adjust it. 	 * Two such buffers are needed. 	 * Needs to be volatile or egcs optimizes away loads. 	 */
+specifier|volatile
+name|char
 name|is_flush
+index|[
+name|STRBUF_FLUSHSYNC_NBYTES
+operator|*
+literal|3
+operator|-
+literal|1
+index|]
 decl_stmt|;
 comment|/* copies of our parents state, to allow us to be self contained */
 name|bus_space_tag_t
 name|is_bustag
 decl_stmt|;
 comment|/* our bus tag */
-name|struct
-name|iommureg
-modifier|*
+name|bus_space_handle_t
+name|is_bushandle
+decl_stmt|;
+name|bus_addr_t
 name|is_iommu
 decl_stmt|;
 comment|/* IOMMU registers */
-name|struct
-name|iommu_strbuf
-modifier|*
+name|bus_addr_t
 name|is_sb
+index|[
+literal|2
+index|]
 decl_stmt|;
 comment|/* streaming buffer */
-name|u_int64_t
-modifier|*
+name|bus_addr_t
 name|is_dtag
 decl_stmt|;
 comment|/* tag diagnostics access */
-name|u_int64_t
-modifier|*
+name|bus_addr_t
 name|is_ddram
 decl_stmt|;
 comment|/* data ram diag. access */
-name|u_int64_t
-modifier|*
+name|bus_addr_t
 name|is_dqueue
 decl_stmt|;
 comment|/* LRU queue diag. access */
-name|u_int64_t
-modifier|*
+name|bus_addr_t
 name|is_dva
 decl_stmt|;
 comment|/* VA diag. register */
-name|u_int64_t
-modifier|*
+name|bus_addr_t
 name|is_dtcmp
 decl_stmt|;
 comment|/* tag compare diag. access */
@@ -182,7 +237,7 @@ end_decl_stmt
 
 begin_decl_stmt
 name|int
-name|iommu_dvmamem_alloc
+name|iommu_dvmamap_create
 name|__P
 argument_list|(
 operator|(
@@ -190,10 +245,6 @@ name|bus_dma_tag_t
 operator|,
 expr|struct
 name|iommu_state
-operator|*
-operator|,
-name|void
-operator|*
 operator|*
 operator|,
 name|int
@@ -206,8 +257,8 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-name|void
-name|iommu_dvmamem_free
+name|int
+name|iommu_dvmamap_destroy
 name|__P
 argument_list|(
 operator|(
@@ -215,9 +266,6 @@ name|bus_dma_tag_t
 operator|,
 expr|struct
 name|iommu_state
-operator|*
-operator|,
-name|void
 operator|*
 operator|,
 name|bus_dmamap_t
@@ -290,6 +338,52 @@ operator|,
 name|bus_dmamap_t
 operator|,
 name|bus_dmasync_op_t
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|int
+name|iommu_dvmamem_alloc
+name|__P
+argument_list|(
+operator|(
+name|bus_dma_tag_t
+operator|,
+expr|struct
+name|iommu_state
+operator|*
+operator|,
+name|void
+operator|*
+operator|*
+operator|,
+name|int
+operator|,
+name|bus_dmamap_t
+operator|*
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|void
+name|iommu_dvmamem_free
+name|__P
+argument_list|(
+operator|(
+name|bus_dma_tag_t
+operator|,
+expr|struct
+name|iommu_state
+operator|*
+operator|,
+name|void
+operator|*
+operator|,
+name|bus_dmamap_t
 operator|)
 argument_list|)
 decl_stmt|;
