bc5c2f9dd5d3a2206318b61ddbad55317dbd05cd
Brian F. Feldman <green@FreeBSD.org>
2002-02-16 17:44:43 +0000

    (Doing that whole test-immediately-after-commit-thing like obrien sez:) Forgot to include lock.h and mutex.h for GIANT_REQUIRED.
    Former-commit-id: 3dc2e37545b3d5a7e5573ccebe054b7e10a5e238

--- sys/kern/kern_conf.c
+++ sys/kern/kern_conf.c
@@ -24,6 +24,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/lock.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/mutex.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/sysctl.h>
 end_include
 
