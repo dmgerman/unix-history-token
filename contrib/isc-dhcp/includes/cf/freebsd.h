8f763428d8e928f7cde25823b4e7d227e3e7e8ef
Murray Stokely <murray@FreeBSD.org>
2002-02-19 12:22:54 +0000

    Resolve conflicts.
    Former-commit-id: 92ddfb341b72e05640b47c3636504f5c521d27a6

--- contrib/isc-dhcp/includes/cf/freebsd.h
+++ contrib/isc-dhcp/includes/cf/freebsd.h
@@ -4,7 +4,7 @@ comment|/* freebsd.h     System dependencies for FreeBSD... */
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1996, 1998 The Internet Software Consortium.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of The Internet Software Consortium nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE INTERNET SOFTWARE CONSORTIUM AND  * CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL  * THE INTERNET SOFTWARE CONSORTIUM OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED  * OF THE POSSIBILITY OF SUCH DAMAGE.  *  * This software was written for the Internet Software Consortium by Ted Lemon  * under a contract with Vixie Laboratories.  */
+comment|/*  * Copyright (c) 1996-1999 Internet Software Consortium.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of The Internet Software Consortium nor the names  *    of its contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE INTERNET SOFTWARE CONSORTIUM AND  * CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE INTERNET SOFTWARE CONSORTIUM OR  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * This software has been written for the Internet Software Consortium  * by Ted Lemon in cooperation with Vixie Enterprises and Nominum, Inc.  * To learn more about the Internet Software Consortium, see  * ``http://www.isc.org/''.  To learn more about Vixie Enterprises,  * see ``http://www.vix.com''.   To learn more about Nominum, Inc., see  * ``http://www.nominum.com''.  *  * $FreeBSD$  */
 end_comment
 
 begin_define
@@ -274,6 +274,30 @@ directive|define
 name|HAVE_SA_LEN
 end_define
 
+begin_comment
+comment|/* socklen_t was first defined on November 24 in sys/socket.h, and    __FreeBSD_version was changed to 400013 on December 4, so if you    get a compile error on this, and you updated between those dates,    that's why.   Also, it may be that some 3.x version after 3.4 will    have socklen_t, but no such change has been made so far. */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|__FreeBSD_version
+operator|<
+literal|400013
+end_if
+
+begin_define
+define|#
+directive|define
+name|SOCKLEN_T
+value|int
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_if
 if|#
 directive|if
@@ -293,6 +317,132 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_define
+define|#
+directive|define
+name|HAVE_MKSTEMP
+end_define
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|NEED_PRAND_CONF
+end_ifdef
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|HAVE_DEV_RANDOM
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|HAVE_DEV_RANDOM
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* HAVE_DEV_RANDOM */
+end_comment
+
+begin_decl_stmt
+specifier|const
+name|char
+modifier|*
+name|cmds
+index|[]
+init|=
+block|{
+literal|"/bin/ps -axlw 2>&1"
+block|,
+literal|"/usr/sbin/arp -an 2>&1"
+block|,
+literal|"/usr/bin/netstat -an 2>&1"
+block|,
+literal|"/bin/df  2>&1"
+block|,
+literal|"/usr/bin/dig com. soa +ti=1 +retry=0 2>&1"
+block|,
+literal|"/usr/bin/netstat -an 2>&1"
+block|,
+literal|"/usr/bin/dig . soa +ti=1 +retry=0 2>&1"
+block|,
+literal|"/usr/sbin/iostat  2>&1"
+block|,
+literal|"/usr/bin/vmstat  2>&1"
+block|,
+literal|"/usr/bin/w  2>&1"
+block|,
+name|NULL
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|const
+name|char
+modifier|*
+name|dirs
+index|[]
+init|=
+block|{
+literal|"/tmp"
+block|,
+literal|"/usr/tmp"
+block|,
+literal|"."
+block|,
+literal|"/"
+block|,
+literal|"/var/spool"
+block|,
+literal|"/dev"
+block|,
+literal|"/var/mail"
+block|,
+literal|"/home"
+block|,
+literal|"/usr/home"
+block|,
+name|NULL
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|const
+name|char
+modifier|*
+name|files
+index|[]
+init|=
+block|{
+literal|"/var/log/messages"
+block|,
+literal|"/var/log/wtmp"
+block|,
+literal|"/var/log/lastlog"
+block|,
+name|NULL
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* NEED_PRAND_CONF */
+end_comment
 
 end_unit
 
