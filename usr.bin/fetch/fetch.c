c1319172ef9d9d63ed20eaef1e63593ae6914eea
Bruce Evans <bde@FreeBSD.org>
2002-02-25 05:00:39 +0000

    #include <sys/time.h> instead of depending on namespace pollution in <sys/stat.h> for the declaration of struct timeval.
    Removed unused includes (<time.h> doesn't declare anything of interest;
    only <sys/time.h> does).
    
    Sorted includes a bit.
    
    
    Former-commit-id: 9c397fc4e457b9262e5d3c1411c82a9cf0c03d26

--- usr.bin/fetch/fetch.c
+++ usr.bin/fetch/fetch.c
@@ -12,19 +12,19 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/stat.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/socket.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<sys/ioctl.h>
+file|<sys/stat.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/time.h>
 end_include
 
 begin_include
