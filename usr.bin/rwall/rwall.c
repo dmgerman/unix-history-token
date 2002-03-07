979a13f23212733531afe8b30f9c746eeb32dedb
Mark Murray <markm@FreeBSD.org>
2002-03-07 14:11:41 +0000

    Revert/fix the most controversial parts of the last commit:
    1) place __FBSDID() correctly IAW style(9).
    
    2) revert (and slightly correct) a "complex" expression in an if()
       statement.
    
    
    Former-commit-id: 53d7059cc9d49b1d54f5a3a59668d57eb2ba6564

--- usr.bin/rwall/rwall.c
+++ usr.bin/rwall/rwall.c
@@ -3,20 +3,6 @@ begin_comment
 comment|/*  * Copyright (c) 1993 Christopher G. Demetriou  * Copyright (c) 1988, 1990 Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_include
-include|#
-directive|include
-file|<sys/cdefs.h>
-end_include
-
-begin_expr_stmt
-name|__FBSDID
-argument_list|(
-literal|"$FreeBSD$"
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -64,6 +50,20 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/*  * This program is not related to David Wall, whose Stanford Ph.D. thesis  * is entitled "Mechanisms for Broadcast and Selective Broadcast".  */
@@ -442,13 +442,21 @@ argument_list|,
 name|_PATH_TMP
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|(
 name|fd
 operator|=
 name|mkstemp
 argument_list|(
 name|tmpname
 argument_list|)
-expr_stmt|;
+operator|)
+operator|==
+operator|-
+literal|1
+operator|||
+operator|(
 name|fp
 operator|=
 name|fdopen
@@ -457,16 +465,9 @@ name|fd
 argument_list|,
 literal|"r+"
 argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|fd
+operator|)
 operator|==
-operator|-
-literal|1
-operator|||
-operator|!
-name|fp
+name|NULL
 condition|)
 name|err
 argument_list|(
