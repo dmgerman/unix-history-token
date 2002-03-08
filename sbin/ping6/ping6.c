7607851eb4e333a2466e9186ef016ee45cb7592e
Hajimu UMEMOTO <ume@FreeBSD.org>
2002-03-08 09:18:59 +0000

    MFC: Show standard deviation.
    	sbin/ping6/Makefile:	1.9
    	sbin/ping6/ping6.8:	1.17
    	sbin/ping6/ping6.c:	1.16
    
    
    Former-commit-id: 9ea79ef4e44dff9e9ed6602b269b67e08ab624c3

--- sbin/ping6/ping6.c
+++ sbin/ping6/ping6.c
@@ -194,6 +194,11 @@ name|defined
 argument_list|(
 name|__NetBSD__
 argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__FreeBSD__
+argument_list|)
 end_if
 
 begin_include
@@ -966,6 +971,11 @@ operator|||
 name|defined
 argument_list|(
 name|__NetBSD__
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__FreeBSD__
 argument_list|)
 end_if
 
@@ -7486,6 +7496,11 @@ name|defined
 argument_list|(
 name|__NetBSD__
 argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__FreeBSD__
+argument_list|)
 name|tsumsq
 operator|+=
 name|triptime
@@ -11069,6 +11084,11 @@ operator|||
 name|defined
 argument_list|(
 name|__NetBSD__
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__FreeBSD__
 argument_list|)
 name|double
 name|dev
