dda7365c65ff13d9e36aaeadb5ac1c19351fabab
John Hay <jhay@FreeBSD.org>
2002-03-10 18:48:23 +0000

    Make this compile on non i386 archs too.
    Former-commit-id: 2dc9c0880b1b5784cfdca2b553e2b75ab58f6d88

--- sys/isa/sio.c
+++ sys/isa/sio.c
@@ -39,11 +39,22 @@ directive|include
 file|"pci.h"
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__i386__
+end_ifdef
+
 begin_include
 include|#
 directive|include
 file|"puc.h"
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
