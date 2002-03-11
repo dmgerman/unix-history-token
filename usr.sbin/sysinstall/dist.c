74392c8f2fddf7689599494a10e5d4079fc6d792
Murray Stokely <murray@FreeBSD.org>
2002-03-11 13:48:13 +0000

    Update number of ports available.
    Former-commit-id: 2e749ff5e023cc1ad1a011fffaf7ecd8f7e90c18

--- usr.sbin/sysinstall/dist.c
+++ usr.sbin/sysinstall/dist.c
@@ -2231,7 +2231,7 @@ operator|!
 name|msgYesNo
 argument_list|(
 literal|"Would you like to install the FreeBSD ports collection?\n\n"
-literal|"This will give you ready access to over 6300 ported software packages,\n"
+literal|"This will give you ready access to over 6600 ported software packages,\n"
 literal|"at a cost of around 100MB of disk space when \"clean\" and possibly\n"
 literal|"much more than that when a lot of the distribution tarballs are loaded\n"
 literal|"(unless you have the extra discs available from a FreeBSD CD/DVD distribution\n"
