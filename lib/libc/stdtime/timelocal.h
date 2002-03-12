6aca366aa92f3649c5a4e720d3cba24b35e4a952
Alexey Zelkin <phantom@FreeBSD.org>
2002-03-12 17:24:54 +0000

    MFC: sync with -CURRENT's stdtime code
    This MFC brings to -STABLE following changes/features:
    
    . get rid of non-standard %E[Ff] formats
    . add %F per POSIX
    . add %r per POSIX (ampm_fmt)
    . hardcode c_fmt in a different way since used in nl_langinfo now
    . make '%A' and '%a', '%B' and '%b' same per POSIX
    . introduce non-standard md_order (month/day order locale field).
      This field is available to user-land apps via nl_langinfo(3)
      and supposed to replace all usages of %E[Ff] in more correct way.
    . allow POSIX legal data on 3 chars abbrev, name length
      and %c format
    
    WARNING: please beware about this commit. It can trigger some
    hidden or implicit problems with locale/time specific functions or
    related/dependent stuff in userland applications. Please report
    anything unusual!
    
    Following files/revisions were MFCed:
    
    	Makefile.inc	1.11
    	strftime.3	1.25, 1.26
    	strftime.c	1.29, 1.30, 1.32 - 1.34
    	strptime.c	1.20 - 1.25
    	timelocal.c	1.12 - 1.22
    	timelocal.h	1.7 - 1.11
    
    
    Former-commit-id: 801893dc7f268b51ccd1b8bc94b8086d69a70a8f

--- lib/libc/stdtime/timelocal.h
+++ lib/libc/stdtime/timelocal.h
@@ -1,7 +1,19 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997 FreeBSD Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997-2002 FreeBSD Project.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_TIMELOCAL_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_TIMELOCAL_H_
+end_define
 
 begin_comment
 comment|/*  * Private header file for the strftime and strptime localization  * stuff.  */
@@ -58,7 +70,6 @@ name|char
 modifier|*
 name|c_fmt
 decl_stmt|;
-comment|/* not used, just compatibility placeholder */
 specifier|const
 name|char
 modifier|*
@@ -85,47 +96,47 @@ decl_stmt|;
 specifier|const
 name|char
 modifier|*
-name|Ef_fmt
+name|md_order
 decl_stmt|;
 specifier|const
 name|char
 modifier|*
-name|EF_fmt
+name|ampm_fmt
 decl_stmt|;
 block|}
 struct|;
 end_struct
 
-begin_decl_stmt
-specifier|extern
+begin_function_decl
 name|struct
 name|lc_time_T
-name|_time_localebuf
-decl_stmt|;
-end_decl_stmt
+modifier|*
+name|__get_current_time_locale
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_decl_stmt
-specifier|extern
+begin_function_decl
 name|int
-name|_time_using_locale
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
+name|__time_load_locale
+parameter_list|(
 specifier|const
-name|struct
-name|lc_time_T
-name|_C_time_locale
-decl_stmt|;
-end_decl_stmt
+name|char
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_define
-define|#
-directive|define
-name|Locale
-value|(_time_using_locale ?&_time_localebuf :&_C_time_locale)
-end_define
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_TIMELOCAL_H_ */
+end_comment
 
 end_unit
 
