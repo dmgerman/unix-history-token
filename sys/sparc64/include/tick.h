6704fcbe91343cdef53d80e8acd78c22c336fb8c
Jake Burkholder <jake@FreeBSD.org>
2002-03-13 04:38:33 +0000

    Add support for driving the clocks on secondary cpus.
    Submitted by:	tmm
    
    
    Former-commit-id: 1147c876ed60e4680ef44cdcadb4c946f770a6e9

--- sys/sparc64/include/tick.h
+++ sys/sparc64/include/tick.h
@@ -41,6 +41,26 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|SMP
+end_ifdef
+
+begin_function_decl
+name|void
+name|tick_start_ap
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_function_decl
 name|void
 name|tick_stop
