7cf3abe9d00a7d2db5a55af37f37fb07625b9fed
Michael Smith <msmith@FreeBSD.org>
2002-03-16 02:23:30 +0000

    Merge local changes for the 20020308 ACPI CA update.
    Former-commit-id: ed0055589b660c15d89cccd840d2f845c445bcfd

--- sys/contrib/dev/acpica/acconfig.h
+++ sys/contrib/dev/acpica/acconfig.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************  *  * Name: acconfig.h - Global configuration constants  *       $Revision: 90 $  *  *****************************************************************************/
+comment|/******************************************************************************  *  * Name: acconfig.h - Global configuration constants  *       $Revision: 94 $  *  *****************************************************************************/
 end_comment
 
 begin_comment
@@ -39,7 +39,7 @@ begin_define
 define|#
 directive|define
 name|ACPI_CA_VERSION
-value|0x20020214
+value|0x20020308
 end_define
 
 begin_comment
