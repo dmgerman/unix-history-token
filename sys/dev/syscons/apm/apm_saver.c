a7ddc8bfc46adcfdf58e8070d77dbcdb9972be40
Alfred Perlstein <alfred@FreeBSD.org>
2002-03-20 06:48:11 +0000

    Remove __P.
    Former-commit-id: 6f02f3bf09d73057e4325b2e0a3bc6117555fefc

--- sys/dev/syscons/apm/apm_saver.c
+++ sys/dev/syscons/apm/apm_saver.c
@@ -109,19 +109,16 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
+begin_function_decl
 specifier|extern
 name|int
 name|apm_display
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|int
 name|newstate
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_decl_stmt
 specifier|extern
