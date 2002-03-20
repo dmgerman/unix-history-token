c4218ff2c54d65da5ee5413b6a069ebaadf1807e
Warner Losh <imp@FreeBSD.org>
2002-03-20 19:13:13 +0000

    No need to conditionalize on pci being in the kernel for this
    Former-commit-id: 4e8d4d647da79809b77c1672044ad7057f237137

--- sys/dev/ida/ida.c
+++ sys/dev/ida/ida.c
@@ -10,12 +10,6 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<pci.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/param.h>
 end_include
 
@@ -61,24 +55,11 @@ directive|include
 file|<sys/disk.h>
 end_include
 
-begin_if
-if|#
-directive|if
-name|NPCI
-operator|>
-literal|0
-end_if
-
 begin_include
 include|#
 directive|include
 file|<machine/bus_memio.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
--- sys/dev/ida/ida_disk.c
+++ sys/dev/ida/ida_disk.c
@@ -61,24 +61,11 @@ directive|include
 file|<sys/disk.h>
 end_include
 
-begin_if
-if|#
-directive|if
-name|NPCI
-operator|>
-literal|0
-end_if
-
 begin_include
 include|#
 directive|include
 file|<machine/bus_memio.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
