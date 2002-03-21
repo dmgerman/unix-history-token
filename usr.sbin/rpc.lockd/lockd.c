276d73880384fc4eb773735b9c4a1e9400b87329
Alfred Perlstein <alfred@FreeBSD.org>
2002-03-21 22:53:49 +0000

    Remove main() prototype.
    Former-commit-id: 47846e11ec8727e3c9955155318b166036e9de39

--- usr.sbin/rpc.lockd/lockd.c
+++ usr.sbin/rpc.lockd/lockd.c
@@ -191,19 +191,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|int
-name|main
-parameter_list|(
-name|int
-parameter_list|,
-name|char
-modifier|*
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|void
 name|nlm_prog_0
 parameter_list|(
