74d1d06ea0f4040feec67b9349ad6bd8e391443d
Warner Losh <imp@FreeBSD.org>
2002-03-21 13:20:49 +0000

    o remove __P o remove main prototype
    Former-commit-id: 5fe72f20b8810d24cfd8b8ca3d70740a35c061c4

--- sbin/nfsiod/nfsiod.c
+++ sbin/nfsiod/nfsiod.c
@@ -163,41 +163,32 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
+begin_function_decl
 name|void
 name|nonfs
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|int
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_decl_stmt
+begin_function_decl
 name|void
 name|reapchild
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|int
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_decl_stmt
+begin_function_decl
 name|void
 name|usage
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_comment
 comment|/*  * Nfsiod does asynchronous buffered I/O on behalf of the NFS client.  * It does not have to be running for correct operation, but will  * improve throughput.  */
--- sbin/slattach/slattach.c
+++ sbin/slattach/slattach.c
@@ -275,18 +275,15 @@ begin_comment
 comment|/* get tty device as controlling terminal */
 end_comment
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|void
 name|usage
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_decl_stmt
 name|int
