2b32497a7b4286132f465b705b6e6c6f7486639b
David E. O'Brien <obrien@FreeBSD.org>
2002-03-22 19:52:59 +0000

    Update SCM ID.
    Former-commit-id: 57491af9af7004683f213f07d1566a280e79a6d9

--- bin/mv/mv.c
+++ bin/mv/mv.c
@@ -47,17 +47,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
 endif|#
 directive|endif
@@ -66,6 +55,20 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
