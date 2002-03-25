d89d8feaebdb44ea586df4efbe201b18897d41a5
Mike Barcroft <mike@FreeBSD.org>
2002-03-25 09:23:41 +0000

    Include <machine/limits.h> for definition of ULONG_MAX.  Remove a duplicate include.
    Former-commit-id: 6894581f550b38247f3f713e34dace4f57c77bef

--- sys/dev/sound/midi/midi.h
+++ sys/dev/sound/midi/midi.h
@@ -140,16 +140,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/clock.h>
-end_include
-
-begin_comment
-comment|/* for DELAY */
-end_comment
-
-begin_include
-include|#
-directive|include
 file|<machine/resource.h>
 end_include
 
@@ -180,6 +170,12 @@ end_include
 begin_comment
 comment|/* for DELAY */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<machine/limits.h>
+end_include
 
 begin_include
 include|#
