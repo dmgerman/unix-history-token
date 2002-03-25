082c046fcbb9f7f6f38ce78d57574e72152ad981
Thomas Moestl <tmm@FreeBSD.org>
2002-03-25 04:53:18 +0000

    Add missing declarations.
    Former-commit-id: fdc11b415d1269c200c918ed5372dcf17f03b422

--- sys/sparc64/include/clock.h
+++ sys/sparc64/include/clock.h
@@ -15,6 +15,27 @@ directive|define
 name|_MACHINE_CLOCK_H_
 end_define
 
+begin_decl_stmt
+specifier|extern
+name|u_long
+name|tick_increment
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|u_long
+name|tick_freq
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|u_long
+name|tick_MHz
+decl_stmt|;
+end_decl_stmt
+
 begin_endif
 endif|#
 directive|endif
