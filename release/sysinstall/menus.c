2b02ee2b2927663309975d286179ef74e9c68a96
Bruce A. Mah <bmah@FreeBSD.org>
2002-03-25 21:07:07 +0000

    MFC:  Add ERRATA file to the documentation menu and index, renumber menu entries on documentation menu now begin with "1".
    	src/usr.sbin/sysinstall/menus.c	1.337->1.338
    
    
    Former-commit-id: a619d4abc377d85d9681ea3f8db54bd24b6ac048

--- release/sysinstall/menus.c
+++ release/sysinstall/menus.c
@@ -853,6 +853,20 @@ literal|"README"
 block|}
 block|,
 block|{
+literal|" Doc, Errata"
+block|,
+literal|"The distribution errata."
+block|,
+name|NULL
+block|,
+name|dmenuDisplayFile
+block|,
+name|NULL
+block|,
+literal|"ERRATA"
+block|}
+block|,
+block|{
 literal|" Doc, Hardware"
 block|,
 literal|"The distribution hardware guide."
@@ -1787,7 +1801,7 @@ name|dmenuExit
 block|}
 block|,
 block|{
-literal|"2 README"
+literal|"1 README"
 block|,
 literal|"A general description of FreeBSD.  Read this!"
 block|,
@@ -1798,6 +1812,20 @@ block|,
 name|NULL
 block|,
 literal|"README"
+block|}
+block|,
+block|{
+literal|"2 Errata"
+block|,
+literal|"Late-breaking, post-release news."
+block|,
+name|NULL
+block|,
+name|dmenuDisplayFile
+block|,
+name|NULL
+block|,
+literal|"ERRATA"
 block|}
 block|,
 block|{
