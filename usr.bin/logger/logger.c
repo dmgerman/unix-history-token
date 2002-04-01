76ab3692c2a7fc259a0f0025ad8ba9b0503c81a2
David Malone <dwmalone@FreeBSD.org>
2002-04-01 11:06:00 +0000

    1) Use FBSDID for vendor string. 2) Change an int for a size_t, as that's what it is mainly used for. 3) Add some braces to keep gcc happy.
    Former-commit-id: 4943e0d4d42afa5b7a45b412fea308e7497c57e6

--- usr.bin/logger/logger.c
+++ usr.bin/logger/logger.c
@@ -47,17 +47,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
 endif|#
 directive|endif
@@ -66,6 +55,20 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -505,7 +508,7 @@ decl_stmt|,
 modifier|*
 name|endp
 decl_stmt|;
-name|int
+name|size_t
 name|len
 decl_stmt|;
 for|for
@@ -1090,6 +1093,7 @@ name|lsent
 operator|!=
 name|len
 condition|)
+block|{
 if|if
 condition|(
 name|lsent
@@ -1110,6 +1114,7 @@ argument_list|,
 name|lsent
 argument_list|)
 expr_stmt|;
+block|}
 name|free
 argument_list|(
 name|line
