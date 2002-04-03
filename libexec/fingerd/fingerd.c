137984f7d9d355c10d1f5f289b88d0f389f3d53d
Mike Barcroft <mike@FreeBSD.org>
2002-04-03 09:05:23 +0000

    MFC Rev 1.20:   Revert revision 1.11.  FreeBSD/alpha has suppport for T/TCP.
    Former-commit-id: a1d2cfd06d8eab8451b5c56e31eb306ddf04203a

--- libexec/fingerd/fingerd.c
+++ libexec/fingerd/fingerd.c
@@ -341,14 +341,6 @@ expr_stmt|;
 block|}
 comment|/* 	 * Enable server-side Transaction TCP. 	 */
 block|{
-if|#
-directive|if
-operator|!
-name|defined
-argument_list|(
-name|__alpha__
-argument_list|)
-comment|/* XXX FIXME */
 name|int
 name|one
 init|=
@@ -380,8 +372,6 @@ literal|"setsockopt(TCP_NOPUSH) failed: %m"
 argument_list|)
 expr_stmt|;
 block|}
-endif|#
-directive|endif
 block|}
 if|if
 condition|(
