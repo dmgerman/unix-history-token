4980e6d889f266b583a3a8cf06d99688f6b458a1
Jake Burkholder <jake@FreeBSD.org>
2002-04-06 08:29:18 +0000

    segsz_t needs to be 64bit for sparc64.
    Former-commit-id: c638a0cf0195d3fe6a3b16112cc2b1dc3d20cdc1

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -592,11 +592,19 @@ begin_comment
 comment|/* resource limit */
 end_comment
 
-begin_ifdef
-ifdef|#
-directive|ifdef
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
 name|__alpha__
-end_ifdef
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__sparc64__
+argument_list|)
+end_if
 
 begin_comment
 comment|/* XXX should be in<machine/types.h> */
