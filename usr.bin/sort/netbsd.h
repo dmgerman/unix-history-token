875cfa3dcaf2a64882f2db5a42de8aa2f13e0b4b
Dag-Erling Smorgrav <des@FreeBSD.org>
2002-04-06 13:59:43 +0000

    Build a working version of sort(1) from contrib.  Use netbsd.h as a hack to avoid bringing the sources off the vendor branch just to #ifdef out the RCS and SCCS IDs.
    Former-commit-id: d6dad8e275596cc5ff8a982c6f11aca5f50f3284

---
+++ usr.bin/sort/netbsd.h
@@ -0,0 +1,33 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__RCSID
+parameter_list|(
+name|x
+parameter_list|)
+end_define
+
+begin_comment
+comment|/* nothing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|__SCCSID
+parameter_list|(
+name|x
+parameter_list|)
+end_define
+
+begin_comment
+comment|/* nothing */
+end_comment
+
+end_unit
+
