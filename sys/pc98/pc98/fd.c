1c3e27753d2a9dd4926f81eacde10e3cd23aa693
Takahashi Yoshihiro <nyan@FreeBSD.org>
2002-04-06 06:43:55 +0000

    Merged from sys/isa/fd.c revision 1.228.
    Former-commit-id: 81f885fc6f6128f048e78a801758fa5a57446a41

--- sys/pc98/pc98/fd.c
+++ sys/pc98/pc98/fd.c
@@ -15843,12 +15843,7 @@ if|if
 condition|(
 name|readdisklabel
 argument_list|(
-name|dkmodpart
-argument_list|(
 name|dev
-argument_list|,
-name|RAW_PART
-argument_list|)
 argument_list|,
 name|lp
 argument_list|)
