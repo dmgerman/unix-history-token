1f397625431c63e5e10d044ddeaf93f0ce335f48
Josef Karthauser <joe@FreeBSD.org>
2002-04-07 12:29:55 +0000

    Update $NetBSD$ ident.
    Former-commit-id: c96cb3efb4e8b918eddb0c7f8dab7ce36f6b0fa9

--- sys/dev/usb/usbdi.h
+++ sys/dev/usb/usbdi.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	$NetBSD: usbdi.h,v 1.51 2001/04/13 11:19:58 augustss Exp $	*/
+comment|/*	$NetBSD: usbdi.h,v 1.56 2001/12/03 01:47:12 augustss Exp $	*/
 end_comment
 
 begin_comment
