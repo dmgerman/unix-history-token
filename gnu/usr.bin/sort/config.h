9b2c9577649ba81aa2a974ab846ae300ec76e04f
Andrey Chernov <ache@FreeBSD.org>
2002-04-08 12:12:07 +0000

    Revive GNU sort
    Former-commit-id: 469004e8fbaee45caad65f4c014992e2f34df308

---
+++ gnu/usr.bin/sort/config.h
@@ -0,0 +1,365 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* config.h.  Generated automatically by configure.  */
+end_comment
+
+begin_comment
+comment|/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+end_comment
+
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
+comment|/* Define if on AIX 3.    System headers sometimes define this.    We just want to avoid a redefinition error message.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ALL_SOURCE
+end_ifndef
+
+begin_comment
+comment|/* #undef _ALL_SOURCE */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Define if using alloca.c.  */
+end_comment
+
+begin_comment
+comment|/* #undef C_ALLOCA */
+end_comment
+
+begin_comment
+comment|/* Define to empty if the keyword does not work.  */
+end_comment
+
+begin_comment
+comment|/* #undef const */
+end_comment
+
+begin_comment
+comment|/* Define to one of _getb67, GETB67, getb67 for Cray-2 and Cray-YMP systems.    This function is required for alloca.c support on those systems.  */
+end_comment
+
+begin_comment
+comment|/* #undef CRAY_STACKSEG_END */
+end_comment
+
+begin_comment
+comment|/* Define if you have alloca, as a function or macro.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ALLOCA
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have<alloca.h> and it should be used (not on Ultrix).  */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_ALLOCA_H */
+end_comment
+
+begin_comment
+comment|/* Define if you don't have vprintf but do have _doprnt.  */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_DOPRNT */
+end_comment
+
+begin_comment
+comment|/* Define if the `long double' type works.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_LONG_DOUBLE
+value|1
+end_define
+
+begin_comment
+comment|/* Define if your struct stat has st_blksize.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ST_BLKSIZE
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the vprintf function.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_VPRINTF
+value|1
+end_define
+
+begin_comment
+comment|/* Define as __inline if that's what the C compiler calls it.  */
+end_comment
+
+begin_comment
+comment|/* #undef inline */
+end_comment
+
+begin_comment
+comment|/* Define if on MINIX.  */
+end_comment
+
+begin_comment
+comment|/* #undef _MINIX */
+end_comment
+
+begin_comment
+comment|/* Define to `long' if<sys/types.h> doesn't define.  */
+end_comment
+
+begin_comment
+comment|/* #undef off_t */
+end_comment
+
+begin_comment
+comment|/* Define if the system does not provide POSIX.1 features except    with this defined.  */
+end_comment
+
+begin_comment
+comment|/* #undef _POSIX_1_SOURCE */
+end_comment
+
+begin_comment
+comment|/* Define if you need to in order for stat and other things to work.  */
+end_comment
+
+begin_comment
+comment|/* #undef _POSIX_SOURCE */
+end_comment
+
+begin_comment
+comment|/* Define as the return type of signal handlers (int or void).  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RETSIGTYPE
+value|void
+end_define
+
+begin_comment
+comment|/* Define to `unsigned' if<sys/types.h> doesn't define.  */
+end_comment
+
+begin_comment
+comment|/* #undef size_t */
+end_comment
+
+begin_comment
+comment|/* If using the C implementation of alloca, define if you know the    direction of stack growth for your system; otherwise it will be    automatically deduced at run-time. 	STACK_DIRECTION> 0 => grows toward higher addresses 	STACK_DIRECTION< 0 => grows toward lower addresses 	STACK_DIRECTION = 0 => direction of growth unknown  */
+end_comment
+
+begin_comment
+comment|/* #undef STACK_DIRECTION */
+end_comment
+
+begin_comment
+comment|/* Define if the `S_IS*' macros in<sys/stat.h> do not work properly.  */
+end_comment
+
+begin_comment
+comment|/* #undef STAT_MACROS_BROKEN */
+end_comment
+
+begin_comment
+comment|/* Define if you have the ANSI C header files.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STDC_HEADERS
+value|1
+end_define
+
+begin_comment
+comment|/* Define if your processor stores words with the most significant    byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+end_comment
+
+begin_comment
+comment|/* #undef WORDS_BIGENDIAN */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if ANSI function prototypes are usable.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PROTOTYPES
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if GNU regex should be used instead of GNU rx.  */
+end_comment
+
+begin_comment
+comment|/* #undef WITH_REGEX */
+end_comment
+
+begin_comment
+comment|/* Define if you have the isascii function.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ISASCII
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the strchr function.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRCHR
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the strerror function.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRERROR
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the strrchr function.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRRCHR
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the<dirent.h> header file.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_DIRENT_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the<fcntl.h> header file.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_FCNTL_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the<limits.h> header file.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_LIMITS_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the<memory.h> header file.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_MEMORY_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the<ndir.h> header file.  */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_NDIR_H */
+end_comment
+
+begin_comment
+comment|/* Define if you have the<string.h> header file.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRING_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define if you have the<sys/dir.h> header file.  */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_SYS_DIR_H */
+end_comment
+
+begin_comment
+comment|/* Define if you have the<sys/ndir.h> header file.  */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_SYS_NDIR_H */
+end_comment
+
+begin_comment
+comment|/* Define if you have the<unistd.h> header file.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_UNISTD_H
+value|1
+end_define
+
+end_unit
+
---
+++ gnu/usr.bin/sort/version.h
@@ -0,0 +1,16 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_decl_stmt
+specifier|extern
+specifier|const
+name|char
+modifier|*
+name|version_string
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
