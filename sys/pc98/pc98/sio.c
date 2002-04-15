55b0165077e6b780fa0dd6bdbf8902d1549b1b46
Takahashi Yoshihiro <nyan@FreeBSD.org>
2002-04-15 13:39:37 +0000

    Merged from sys/isa/sio.c revision 1.291.2.30.
    Former-commit-id: ee3a981cb4110a90ee93e7fa47c4a2d4b837086f

--- sys/pc98/pc98/sio.c
+++ sys/pc98/pc98/sio.c
@@ -644,18 +644,21 @@ end_define
 begin_define
 define|#
 directive|define
-name|MINOR_MAGIC_MASK
-value|(CALLOUT_MASK | CONTROL_MASK)
+name|MINOR_TO_UNIT
+parameter_list|(
+name|mynor
+parameter_list|)
+value|((((mynor)& ~0xffffU)>> (8 + 3)) \ 				 | ((mynor)& 0x1f))
 end_define
 
 begin_define
 define|#
 directive|define
-name|MINOR_TO_UNIT
+name|UNIT_TO_MINOR
 parameter_list|(
-name|mynor
+name|unit
 parameter_list|)
-value|((mynor)& ~MINOR_MAGIC_MASK)
+value|((((unit)& ~0x1fU)<< (8 + 3)) \ 				 | ((unit)& 0x1f))
 end_define
 
 begin_ifdef
@@ -8203,6 +8206,9 @@ name|Port_t
 name|iobase
 decl_stmt|;
 name|int
+name|minorbase
+decl_stmt|;
+name|int
 name|unit
 decl_stmt|;
 name|u_int
@@ -10253,12 +10259,19 @@ operator|=
 name|TRUE
 expr_stmt|;
 block|}
+name|minorbase
+operator|=
+name|UNIT_TO_MINOR
+argument_list|(
+name|unit
+argument_list|)
+expr_stmt|;
 name|make_dev
 argument_list|(
 operator|&
 name|sio_cdevsw
 argument_list|,
-name|unit
+name|minorbase
 argument_list|,
 name|UID_ROOT
 argument_list|,
@@ -10276,7 +10289,7 @@ argument_list|(
 operator|&
 name|sio_cdevsw
 argument_list|,
-name|unit
+name|minorbase
 operator||
 name|CONTROL_INIT_STATE
 argument_list|,
@@ -10296,7 +10309,7 @@ argument_list|(
 operator|&
 name|sio_cdevsw
 argument_list|,
-name|unit
+name|minorbase
 operator||
 name|CONTROL_LOCK_STATE
 argument_list|,
@@ -10316,7 +10329,7 @@ argument_list|(
 operator|&
 name|sio_cdevsw
 argument_list|,
-name|unit
+name|minorbase
 operator||
 name|CALLOUT_MASK
 argument_list|,
@@ -10336,7 +10349,7 @@ argument_list|(
 operator|&
 name|sio_cdevsw
 argument_list|,
-name|unit
+name|minorbase
 operator||
 name|CALLOUT_MASK
 operator||
@@ -10358,7 +10371,7 @@ argument_list|(
 operator|&
 name|sio_cdevsw
 argument_list|,
-name|unit
+name|minorbase
 operator||
 name|CALLOUT_MASK
 operator||
