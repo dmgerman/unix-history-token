d34d7f7a5c3bf488d4199286c1a2b0b5d41a5e57
Bruce Evans <bde@FreeBSD.org>
2002-04-18 13:02:17 +0000

    Fixed some style bugs: - ifdefs around vendor sccsid were left reversed after switching to   __FBSDID(). - vertical whitespace after some of the non-FALLTHROUGH cases was lost. - too much vertical whitespace before prototypes.
    Former-commit-id: f69857b632f8003b0c8cb6f754629430c31bbd89

--- usr.bin/msgs/msgs.c
+++ usr.bin/msgs/msgs.c
@@ -29,17 +29,17 @@ begin_comment
 comment|/* not lint */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
 begin_if
 if|#
 directive|if
 literal|0
 end_if
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
 
 begin_endif
 unit|static char sccsid[] = "@(#)msgs.c	8.2 (Berkeley) 4/28/95";
@@ -47,14 +47,14 @@ endif|#
 directive|endif
 end_endif
 
+begin_comment
+comment|/* not lint */
+end_comment
+
 begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
 
 begin_include
 include|#
