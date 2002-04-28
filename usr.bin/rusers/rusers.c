2e8033df47e893b0a2f55cc5f0ef51a4fc3b77d7
Mark Murray <markm@FreeBSD.org>
2002-04-28 11:13:20 +0000

    Trivial warnings fixes; staticise and use unsigned.
    Use __FBSDID().
    
    
    Former-commit-id: 59326319ef8c63ca506fb62115b1a83b060afa39

--- usr.bin/rusers/rusers.c
+++ usr.bin/rusers/rusers.c
@@ -3,31 +3,19 @@ begin_comment
 comment|/*-  * Copyright (c) 1993, John Brezak  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -92,7 +80,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<strings.h>
+file|<string.h>
 end_include
 
 begin_include
@@ -154,6 +142,7 @@ struct|;
 end_struct
 
 begin_function
+specifier|static
 name|int
 name|search_host
 parameter_list|(
@@ -222,6 +211,7 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|void
 name|remember_host
 parameter_list|(
@@ -288,6 +278,7 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|int
 name|rusers_reply
 parameter_list|(
@@ -300,9 +291,10 @@ modifier|*
 name|raddrp
 parameter_list|)
 block|{
-name|int
+name|u_int
 name|x
-decl_stmt|,
+decl_stmt|;
+name|int
 name|idle
 decl_stmt|;
 name|char
@@ -806,6 +798,7 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|void
 name|onehost
 parameter_list|(
@@ -987,9 +980,12 @@ block|}
 end_function
 
 begin_function
+specifier|static
 name|void
 name|allhosts
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|utmpidlearr
 name|up
@@ -1068,7 +1064,9 @@ begin_function
 specifier|static
 name|void
 name|usage
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|fprintf
 argument_list|(
