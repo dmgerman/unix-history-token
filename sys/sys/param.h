dbb19bc330f43c29001638c5fa86ee331e2ba6f3
Matthew N. Dodd <mdodd@FreeBSD.org>
2002-04-30 23:55:16 +0000

    Document the location (in the source tree) of the "Porter's Handbook".
    Former-commit-id: 78c50822e074bdae5096bc0801bee89cf247abbb

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -41,7 +41,7 @@ value|1
 end_define
 
 begin_comment
-comment|/*   * __FreeBSD_version numbers are documented in the Porter's Handbook.  * If you bump the version for any reason, you should update the documentation  * there.  */
+comment|/*   * __FreeBSD_version numbers are documented in the Porter's Handbook.  * If you bump the version for any reason, you should update the documentation  * there.  * Currently this lives here:  *  *	doc/en_US.ISO8859-1/books/porters-handbook/book.sgml  */
 end_comment
 
 begin_undef
