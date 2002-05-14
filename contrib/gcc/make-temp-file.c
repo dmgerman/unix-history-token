9ad4f9f2ee908d7f0fda21fbf02d1fddd27a6526
David E. O'Brien <obrien@FreeBSD.org>
2002-05-14 00:33:44 +0000

    Merge choose-temp.c rev 1.2 (use /tmp 1st, and then /var/tmp) into GCC 3.1.
    Former-commit-id: 528959ad9067db90a476eb2a89348fd3f99923c0

--- contrib/gcc/make-temp-file.c
+++ contrib/gcc/make-temp-file.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Utility to pick a temporary filename prefix.    Copyright (C) 1996, 1997, 1998, 2001 Free Software Foundation, Inc.  This file is part of the libiberty library. Libiberty is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.  Libiberty is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for more details.  You should have received a copy of the GNU Library General Public License along with libiberty; see the file COPYING.LIB.  If not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -458,7 +462,16 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-comment|/* Try /var/tmp, /usr/tmp, then /tmp.  */
+comment|/* Try /tmp, /var/tmp, then /usr/tmp.  */
+name|base
+operator|=
+name|try
+argument_list|(
+name|tmp
+argument_list|,
+name|base
+argument_list|)
+expr_stmt|;
 name|base
 operator|=
 name|try
@@ -473,15 +486,6 @@ operator|=
 name|try
 argument_list|(
 name|usrtmp
-argument_list|,
-name|base
-argument_list|)
-expr_stmt|;
-name|base
-operator|=
-name|try
-argument_list|(
-name|tmp
 argument_list|,
 name|base
 argument_list|)
