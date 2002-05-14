dedf6d73715dd0be170222cba1294089c165c5da
David E. O'Brien <obrien@FreeBSD.org>
2002-05-14 01:53:53 +0000

    Merge c-common.c rev 1.10 into GCC 3.1
    Clarify that the "yields only last 2 digits of year in some locales" warning
    does not apply to BSD.
    
    Submitted by:	ache
    
    
    Former-commit-id: 3e7f168b21e30ca21ef48afadbfa2fa9b82ae612

--- contrib/gcc/c-format.c
+++ contrib/gcc/c-format.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Check calls to formatted I/O functions (-Wformat).    Copyright (C) 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,    2001, 2002 Free Software Foundation, Inc.  This file is part of GCC.  GCC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GCC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GCC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -10129,7 +10133,7 @@ name|status_warning
 argument_list|(
 name|status
 argument_list|,
-literal|"`%%%c' yields only last 2 digits of year in some locales"
+literal|"`%%%c' yields only last 2 digits of year in some locales on non-BSD systems"
 argument_list|,
 name|format_char
 argument_list|)
