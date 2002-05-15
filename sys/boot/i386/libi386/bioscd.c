44f6dfe3a2fc9eaae440aac329a519070e8adbd7
John Hay <jhay@FreeBSD.org>
2002-05-15 11:32:53 +0000

    DEV_BSIZE lives in sys/param.h and not in machine/param.h anymore.
    Former-commit-id: 59726484d6b686980efc24264d5f98866fc5e54c

--- sys/boot/i386/libi386/bioscd.c
+++ sys/boot/i386/libi386/bioscd.c
@@ -16,13 +16,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/reboot.h>
+file|<sys/param.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<machine/param.h>
+file|<sys/reboot.h>
 end_include
 
 begin_include
