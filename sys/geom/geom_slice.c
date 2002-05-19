085a092be746d549f10a1f7028daba912b795a1e
Poul-Henning Kamp <phk@FreeBSD.org>
2002-05-19 18:59:39 +0000

    Include needed #include for regression tests.
    Sponsored by: DARPA & NAI Labs.
    
    
    Former-commit-id: 30e145f2e8a896e709d6a4b8b71774b276bdf381

--- sys/geom/geom_slice.c
+++ sys/geom/geom_slice.c
@@ -42,6 +42,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<err.h>
 end_include
 
@@ -1679,7 +1685,6 @@ name|flags
 operator||=
 name|G_GEOM_WITHER
 expr_stmt|;
-comment|/* First prevent any new requests */
 name|error
 operator|=
 name|cp
