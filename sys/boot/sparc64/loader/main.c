6c3878f2f3a14272827f2f313933a600e774caf9
Jake Burkholder <jake@FreeBSD.org>
2002-05-29 19:38:49 +0000

    Add needed include of queue.h.  Remove unneeded include of smp.h.
    Former-commit-id: b1776cbebce08fa02ed64ea140feab899806b966

--- sys/boot/sparc64/loader/main.c
+++ sys/boot/sparc64/loader/main.c
@@ -40,6 +40,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/queue.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/asi.h>
 end_include
 
@@ -71,12 +77,6 @@ begin_include
 include|#
 directive|include
 file|<machine/metadata.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<machine/smp.h>
 end_include
 
 begin_include
