62fb7fae3cf4c158316a52b2cb830d66b1b59a49
David E. O'Brien <obrien@FreeBSD.org>
2002-06-06 03:26:36 +0000

    Tidy do-while.
    Requested by:	bde
    
    
    Former-commit-id: bee4f1a1e71f04e204346391c0041bdbb3e531d6

--- contrib/gcc/config/i386/freebsd.h
+++ contrib/gcc/config/i386/freebsd.h
@@ -578,7 +578,7 @@ parameter_list|,
 name|LOG
 parameter_list|)
 define|\
-value|do {									\   if ((LOG)!=0) {							\     if (in_text_section())						\       fprintf ((FILE), "\t.p2align %d,0x90\n", (LOG));			\     else								\       fprintf ((FILE), "\t.p2align %d\n", (LOG));			\   }									\   } while (0)
+value|do {					     				\     if ((LOG)!=0) {							\       if (in_text_section())						\ 	fprintf ((FILE), "\t.p2align %d,0x90\n", (LOG));		\       else								\ 	fprintf ((FILE), "\t.p2align %d\n", (LOG));			\     }									\   } while (0)
 end_define
 
 begin_undef
