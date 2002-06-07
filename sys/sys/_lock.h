2b57fbcf0693aa8a0a148a249dee19a2b7eb89e1
Bruce Evans <bde@FreeBSD.org>
2002-06-07 14:37:09 +0000

    Renamed the idempotency identifier to match the file name.
    Former-commit-id: 3903f2faeb8c86dca6a85c6bf41439214dc4983d

--- sys/sys/_lock.h
+++ sys/sys/_lock.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_SYS_LOCK_TYPES_H_
+name|_SYS__LOCK_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_SYS_LOCK_TYPES_H_
+name|_SYS__LOCK_H_
 end_define
 
 begin_struct
@@ -61,7 +61,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_SYS_LOCK_TYPES_H_ */
+comment|/* !_SYS__LOCK_H_ */
 end_comment
 
 end_unit
