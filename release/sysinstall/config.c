26a17a50d820e13b10899105967d70e5841fc5e9
David E. O'Brien <obrien@FreeBSD.org>
2002-06-10 04:28:53 +0000

    Change our default XF86Config location from /etc/ to /etc/X11/, following the lead of The XFree86 Project's default.
    Approved by:	Murray
    
    
    Former-commit-id: cc200a3389c78944693a1f2a2a28209125857c57

--- release/sysinstall/config.c
+++ release/sysinstall/config.c
@@ -3338,6 +3338,12 @@ literal|"\"MouseSystems\" as the mouse protocol in the X configuration\n"
 literal|"utility."
 argument_list|)
 expr_stmt|;
+name|Mkdir
+argument_list|(
+literal|"/etc/X11"
+argument_list|)
+expr_stmt|;
+comment|/* XXX:Remove this later after we are happy mtree will have created this for us. */
 name|systemExecute
 argument_list|(
 name|execcmd
@@ -3348,7 +3354,7 @@ condition|(
 operator|!
 name|file_readable
 argument_list|(
-literal|"/etc/XF86Config"
+literal|"/etc/X11/XF86Config"
 argument_list|)
 condition|)
 block|{
