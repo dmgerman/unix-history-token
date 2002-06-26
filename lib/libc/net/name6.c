6d11d0daa6f19e403605df4f9c7d915305dcc443
Warner Losh <imp@FreeBSD.org>
2002-06-26 06:06:43 +0000

    MFC: Fix remove overflow
    Approved by: nectar
    
    
    Former-commit-id: 7fe3986603e760f3a2b98a1ef4dc1d8233f42256

--- lib/libc/net/name6.c
+++ lib/libc/net/name6.c
@@ -5235,6 +5235,9 @@ decl_stmt|,
 modifier|*
 modifier|*
 name|hap
+decl_stmt|,
+modifier|*
+name|obp
 decl_stmt|;
 name|int
 name|type
@@ -6259,6 +6262,11 @@ operator|-=
 name|nn
 expr_stmt|;
 block|}
+name|obp
+operator|=
+name|bp
+expr_stmt|;
+comment|/* ALIGN rounds up */
 name|bp
 operator|=
 operator|(
@@ -6269,6 +6277,14 @@ name|ALIGN
 argument_list|(
 name|bp
 argument_list|)
+expr_stmt|;
+name|buflen
+operator|-=
+operator|(
+name|bp
+operator|-
+name|obp
+operator|)
 expr_stmt|;
 name|DNS_FATAL
 argument_list|(
