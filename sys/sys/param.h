793b86a967dae285af58e26b9ba73df5286e3547
David E. O'Brien <obrien@FreeBSD.org>
2002-06-26 16:49:28 +0000

    MFC: `sed -i'. This also gets deregisterization, de_P, and all others changes between 5-CURRENT and RELENG_4. Bump __FreeBSD_version for bsd.port.mk's use.
    Former-commit-id: 5be424910dd62df167b728deb89cf2db2f789f9a

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -41,7 +41,7 @@ value|1
 end_define
 
 begin_comment
-comment|/*   * __FreeBSD_version numbers are documented in the Porter's Handbook.  * If you bump the version for any reason, you should update the documentation  * there.  */
+comment|/*   * __FreeBSD_version numbers are documented in the Porter's Handbook.  * If you bump the version for any reason, you should update the documentation  * there.  * Currently this lives here:  *	doc/en_US.ISO8859-1/books/porters-handbook/book.sgml  */
 end_comment
 
 begin_undef
@@ -54,7 +54,7 @@ begin_define
 define|#
 directive|define
 name|__FreeBSD_version
-value|460100
+value|460101
 end_define
 
 begin_comment
