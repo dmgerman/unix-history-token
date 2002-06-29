2feb0990ab467784cd188b9a077e61ec5a3967ce
Dag-Erling Smorgrav <des@FreeBSD.org>
2002-06-29 11:25:20 +0000

    OpenBSD lifted this code our tree.  Preserve the original CVS id.
    Former-commit-id: 6fb6d07c23b06ad713e2ff58edfca3a462b532fd

--- crypto/openssh/auth-krb5.c
+++ crypto/openssh/auth-krb5.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *    Kerberos v5 authentication and ticket-passing routines.  *  * $FreeBSD$  */
+comment|/*  *    Kerberos v5 authentication and ticket-passing routines.  *  * $xFreeBSD: src/crypto/openssh/auth-krb5.c,v 1.6 2001/02/13 16:58:04 assar Exp$  */
 end_comment
 
 begin_comment
