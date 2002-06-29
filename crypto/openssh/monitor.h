6b99ed675a47b30fd4fbd8c3886468ba2908f480
Dag-Erling Smorgrav <des@FreeBSD.org>
2002-06-29 10:56:23 +0000

    PAM support, the FreeBSD way.
    Sponsored by:	DARPA, NAI Labs
    
    
    Former-commit-id: e404480cd2b1159e9fe42e094ae1816bb6c50aef

--- crypto/openssh/monitor.h
+++ crypto/openssh/monitor.h
@@ -4,6 +4,10 @@ comment|/*	$OpenBSD: monitor.h,v 1.6 2002/06/11 05:46:20 mpech Exp $	*/
 end_comment
 
 begin_comment
+comment|/*	$FreeBSD$	*/
+end_comment
+
+begin_comment
 comment|/*  * Copyright 2002 Niels Provos<provos@citi.umich.edu>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
@@ -98,6 +102,22 @@ block|,
 name|MONITOR_ANS_RSARESPONSE
 block|,
 name|MONITOR_REQ_PAM_START
+block|,
+name|MONITOR_REQ_PAM_INIT_CTX
+block|,
+name|MONITOR_ANS_PAM_INIT_CTX
+block|,
+name|MONITOR_REQ_PAM_QUERY
+block|,
+name|MONITOR_ANS_PAM_QUERY
+block|,
+name|MONITOR_REQ_PAM_RESPOND
+block|,
+name|MONITOR_ANS_PAM_RESPOND
+block|,
+name|MONITOR_REQ_PAM_FREE_CTX
+block|,
+name|MONITOR_ANS_PAM_FREE_CTX
 block|,
 name|MONITOR_REQ_TERM
 block|}
