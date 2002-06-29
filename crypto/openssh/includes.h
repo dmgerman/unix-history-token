1ddc6e39df0c018e53bfa346bffd9827b90f7575
Dag-Erling Smorgrav <des@FreeBSD.org>
2002-06-29 11:22:20 +0000

    Use our __RCSID().
    Former-commit-id: 977d1c10d7fac1095220f8e9948c51585b0590cb

--- crypto/openssh/includes.h
+++ crypto/openssh/includes.h
@@ -4,6 +4,10 @@ comment|/*	$OpenBSD: includes.h,v 1.17 2002/01/26 16:44:22 stevesk Exp $	*/
 end_comment
 
 begin_comment
+comment|/*	$FreeBSD$	*/
+end_comment
+
+begin_comment
 comment|/*  * Author: Tatu Ylonen<ylo@cs.hut.fi>  * Copyright (c) 1995 Tatu Ylonen<ylo@cs.hut.fi>, Espoo, Finland  *                    All rights reserved  * This file includes most of the needed system headers.  *  * As far as I am concerned, the code I have written for this software  * can be used freely for any purpose.  Any derived versions of this  * software must be clearly marked as such, and if the derived work is  * incompatible with the protocol description in the RFC file, it must be  * called by a name other than "ssh" or "Secure Shell".  */
 end_comment
 
@@ -27,9 +31,7 @@ parameter_list|(
 name|msg
 parameter_list|)
 define|\
-value|static
-comment|/**/
-value|const char *const rcsid[] = { (char *)rcsid, "\100(#)" msg }
+value|__RCSID(msg)
 end_define
 
 begin_include
