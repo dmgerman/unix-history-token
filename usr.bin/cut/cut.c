63036daa8093001790068c8201ce332dd5c0e226
David E. O'Brien <obrien@FreeBSD.org>
2002-06-30 05:34:21 +0000

    Consistently use FBSDID
    Former-commit-id: 0f514998396ad33aaaf5ea87e31e990efa612daa

--- usr.bin/cut/cut.c
+++ usr.bin/cut/cut.c
@@ -31,17 +31,6 @@ literal|"@(#)cut.c	8.3 (Berkeley) 5/4/95"
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
 endif|#
 directive|endif
@@ -50,6 +39,20 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
