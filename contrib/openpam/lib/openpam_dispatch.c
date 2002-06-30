30f1d9bb17e8558cf0cb8f8b98c9fb9dffad36a1
Dag-Erling Smorgrav <des@FreeBSD.org>
2002-06-30 21:30:05 +0000

    Vendor import of OpenPAM Citronella.
    Former-commit-id: bdbe82ccdd803214546109577823ac1cbe4cf8ea

--- contrib/openpam/lib/openpam_dispatch.c
+++ contrib/openpam/lib/openpam_dispatch.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2002 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by ThinkSec AS and  * NAI Labs, the Security Research Division of Network Associates, Inc.  * under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the  * DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/openpam/lib/openpam_dispatch.c#14 $  */
+comment|/*-  * Copyright (c) 2002 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by ThinkSec AS and  * Network Associates Laboratories, the Security Research Division of  * Network Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035  * ("CBOSS"), as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/openpam/lib/openpam_dispatch.c#17 $  */
 end_comment
 
 begin_include
@@ -361,25 +361,36 @@ operator|==
 name|PAM_SUCCESS
 condition|)
 block|{
-comment|/* 			 * For pam_setcred() and pam_chauthtok() with the 			 * PAM_PRELIM_CHECK flag, treat "sufficient" as 			 * "optional". 			 * 			 * Note that Solaris libpam does not terminate 			 * the chain here if a required module has 			 * previously failed.  I'm not sure why. 			 */
+comment|/* 			 * For pam_setcred() and pam_chauthtok() with the 			 * PAM_PRELIM_CHECK flag, treat "sufficient" as 			 * "optional". 			 */
 if|if
 condition|(
+operator|(
 name|chain
 operator|->
 name|flag
 operator|==
 name|PAM_SUFFICIENT
+operator|||
+name|chain
+operator|->
+name|flag
+operator|==
+name|PAM_BINDING
+operator|)
+operator|&&
+operator|!
+name|fail
 operator|&&
 name|primitive
 operator|!=
 name|PAM_SM_SETCRED
 operator|&&
+operator|!
 operator|(
 name|primitive
-operator|!=
+operator|==
 name|PAM_SM_CHAUTHTOK
-operator|||
-operator|!
+operator|&&
 operator|(
 name|flags
 operator|&
@@ -410,11 +421,19 @@ name|r
 expr_stmt|;
 if|if
 condition|(
+operator|(
 name|chain
 operator|->
 name|flag
 operator|==
 name|PAM_REQUIRED
+operator|||
+name|chain
+operator|->
+name|flag
+operator|==
+name|PAM_BINDING
+operator|)
 operator|&&
 operator|!
 name|fail
@@ -464,6 +483,10 @@ if|if
 condition|(
 operator|!
 name|fail
+operator|&&
+name|err
+operator|!=
+name|PAM_NEW_AUTHTOK_REQD
 condition|)
 name|err
 operator|=
