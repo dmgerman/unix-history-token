efe03aba1a7155302102738ccf966f32abd0e388
John Baldwin <jhb@FreeBSD.org>
2002-07-02 21:35:31 +0000

    MFC: Document the DD and Wizard modes.
    Former-commit-id: a1fbcbc62d0a9cc358b19eda413850a4a3131740

--- release/sysinstall/disks.c
+++ release/sysinstall/disks.c
@@ -750,7 +750,7 @@ literal|16
 argument_list|,
 literal|0
 argument_list|,
-literal|"A = Use Entire Disk    G = set Drive Geometry   C = Create Slice"
+literal|"A = Use Entire Disk   G = set Drive Geometry   C = Create Slice   F = `DD' mode"
 argument_list|)
 expr_stmt|;
 name|mvprintw
@@ -759,7 +759,7 @@ literal|17
 argument_list|,
 literal|0
 argument_list|,
-literal|"D = Delete Slice       Z = Toggle Size Units    S = Set Bootable"
+literal|"D = Delete Slice      Z = Toggle Size Units    S = Set Bootable   | = Wizard m."
 argument_list|)
 expr_stmt|;
 name|mvprintw
@@ -768,7 +768,7 @@ literal|18
 argument_list|,
 literal|0
 argument_list|,
-literal|"T = Change Type        U = Undo All Changes     Q = Finish"
+literal|"T = Change Type       U = Undo All Changes     Q = Finish"
 argument_list|)
 expr_stmt|;
 if|if
@@ -780,7 +780,7 @@ name|mvprintw
 argument_list|(
 literal|18
 argument_list|,
-literal|48
+literal|47
 argument_list|,
 literal|"W = Write Changes"
 argument_list|)
