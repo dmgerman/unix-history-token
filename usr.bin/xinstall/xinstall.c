d9f04f52285c4f52e8b5a2fb010ec4d7961da18d
Dag-Erling Smorgrav <des@FreeBSD.org>
2002-07-03 17:00:13 +0000

    Back out previous commit, rev 1.3.2.5 of bsd.sys.mk takes care of it.
    Former-commit-id: 5f4181aa46c988d09b59d75a97293e6ce33e4739

--- usr.bin/xinstall/xinstall.c
+++ usr.bin/xinstall/xinstall.c
@@ -62,12 +62,6 @@ directive|include
 file|<sys/cdefs.h>
 end_include
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__FBSDID
-end_ifdef
-
 begin_expr_stmt
 name|__FBSDID
 argument_list|(
@@ -75,11 +69,6 @@ literal|"$FreeBSD$"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
