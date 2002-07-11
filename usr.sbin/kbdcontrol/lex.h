f46d6d1a96483417a12211d3ad72a63be635944a
Alfred Perlstein <alfred@FreeBSD.org>
2002-07-11 21:13:37 +0000

    de-__P()
    Former-commit-id: 9fe97188179111481d390c729d1156e8dab0618e

--- usr.sbin/kbdcontrol/lex.h
+++ usr.sbin/kbdcontrol/lex.h
@@ -277,18 +277,15 @@ name|yyin
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
+begin_function_decl
 specifier|extern
 name|int
 name|yylex
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 end_unit
 
