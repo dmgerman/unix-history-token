d55b5fd5b651ed18896a3e3dc1a852555a4f9223
Juli Mallett <jmallett@FreeBSD.org>
2002-07-19 07:53:41 +0000

    Reduce differences to CURRENT.
    Former-commit-id: 73cbb6c6e97b18e1f6f77302c4b3e4ab1a15babe

--- include/paths.h
+++ include/paths.h
@@ -226,6 +226,13 @@ name|_PATH_VI
 value|"/usr/bin/vi"
 end_define
 
+begin_define
+define|#
+directive|define
+name|_PATH_WALL
+value|"/usr/bin/wall"
+end_define
+
 begin_comment
 comment|/* Provide trailing slash, since mostly used for building pathnames. */
 end_comment
@@ -283,20 +290,17 @@ begin_comment
 comment|/* How to get the correct name of the kernel. */
 end_comment
 
-begin_decl_stmt
+begin_function_decl
 name|__BEGIN_DECLS
 specifier|const
 name|char
 modifier|*
 name|getbootfile
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_macro
 name|__END_DECLS
