634708babc77812599192d06e6861ff211b3f396
Matthew Jacob <mjacob@FreeBSD.org>
2002-07-25 16:00:24 +0000

    Put MODULE_VERSION back here so that ispfw is happy.
    Former-commit-id: b371d15654412f0d3bc49460122052d8abc0f75e

--- sys/dev/isp/isp_freebsd.c
+++ sys/dev/isp/isp_freebsd.c
@@ -44,6 +44,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/module.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/ioccom.h>
 end_include
 
@@ -52,6 +58,16 @@ include|#
 directive|include
 file|<dev/isp/isp_ioctl.h>
 end_include
+
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|isp
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_decl_stmt
 name|int
