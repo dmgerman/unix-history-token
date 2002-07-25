b65ef5b3eba95ac4fce228676d6287ae3d58e58d
Ruslan Ermilov <ru@FreeBSD.org>
2002-07-25 09:33:17 +0000

    MFC: cross-release support.
    Assorted fixes:
    
    - Changed the way the `catpages' dist is built (all MD catpages
      were missing before).
    
    - Perl manpages now end up in the `manpage' dist.
    
    - Clean up the `dict' dist generation script.
    
    - Diff reduction to -CURRENT.
    
    
    Former-commit-id: e7d2e33f5fdb2d0beb06b180d7aab3c756a06cc4

--- usr.sbin/crunch/crunchide/extern.h
+++ usr.sbin/crunch/crunchide/extern.h
@@ -4,13 +4,17 @@ comment|/*	$NetBSD: extern.h,v 1.5 1998/05/06 13:16:57 mycroft Exp $	*/
 end_comment
 
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/*  * Copyright (c) 1997 Christopher G. Demetriou.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *      This product includes software developed by Christopher G. Demetriou  *	for the NetBSD Project.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
 begin_ifdef
 ifdef|#
 directive|ifdef
-name|__alpha__
+name|arch_alpha
 end_ifdef
 
 begin_define
@@ -33,7 +37,7 @@ end_else
 begin_ifdef
 ifdef|#
 directive|ifdef
-name|__mips__
+name|arch_mips
 end_ifdef
 
 begin_define
@@ -50,7 +54,7 @@ end_else
 begin_ifdef
 ifdef|#
 directive|ifdef
-name|__powerpc__
+name|arch_powerpc
 end_ifdef
 
 begin_define
