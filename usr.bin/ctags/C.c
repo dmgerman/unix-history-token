cedc32e1f069fba5149cccce7e0b25fd691d0455
Tim J. Robbins <tjr@FreeBSD.org>
2002-07-30 00:55:07 +0000

    MFC: Avoid buffer overflows, sort symbol name with locale collating order with -x option, style changes.
    Former-commit-id: 27ff51c192d3f95225a2c0d75b4482fbc3f52586

--- usr.bin/ctags/C.c
+++ usr.bin/ctags/C.c
@@ -3,48 +3,42 @@ begin_comment
 comment|/*  * Copyright (c) 1987, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
 begin_if
 if|#
 directive|if
 literal|0
 end_if
 
-begin_else
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
+begin_endif
 unit|static char sccsid[] = "@(#)C.c	8.4 (Berkeley) 4/2/94";
-else|#
-directive|else
-end_else
+endif|#
+directive|endif
+end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -70,57 +64,45 @@ directive|include
 file|"ctags.h"
 end_include
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|int
 name|func_entry
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|void
 name|hash_entry
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|void
 name|skip_string
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|int
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|int
 name|str_entry
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|int
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_comment
 comment|/*  * c_entries --  *	read .c and .h files and call appropriate routines  */
@@ -129,7 +111,9 @@ end_comment
 begin_function
 name|void
 name|c_entries
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|int
 name|c
@@ -644,6 +628,24 @@ name|c
 argument_list|)
 condition|)
 block|{
+if|if
+condition|(
+name|sp
+operator|==
+name|tok
+operator|+
+sizeof|sizeof
+name|tok
+operator|-
+literal|1
+condition|)
+comment|/* Too long -- truncate it */
+operator|*
+name|sp
+operator|=
+name|EOS
+expr_stmt|;
+else|else
 operator|*
 name|sp
 operator|++
@@ -677,7 +679,9 @@ begin_function
 specifier|static
 name|int
 name|func_entry
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|int
 name|c
@@ -893,7 +897,9 @@ begin_function
 specifier|static
 name|void
 name|hash_entry
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|int
 name|c
@@ -978,6 +984,24 @@ name|c
 argument_list|)
 condition|)
 break|break;
+if|if
+condition|(
+name|sp
+operator|==
+name|tok
+operator|+
+sizeof|sizeof
+name|tok
+operator|-
+literal|1
+condition|)
+comment|/* Too long -- truncate it */
+operator|*
+name|sp
+operator|=
+name|EOS
+expr_stmt|;
+else|else
 operator|*
 name|sp
 operator|++
@@ -1042,6 +1066,24 @@ condition|;
 control|)
 block|{
 comment|/* get next token */
+if|if
+condition|(
+name|sp
+operator|==
+name|tok
+operator|+
+sizeof|sizeof
+name|tok
+operator|-
+literal|1
+condition|)
+comment|/* Too long -- truncate it */
+operator|*
+name|sp
+operator|=
+name|EOS
+expr_stmt|;
+else|else
 operator|*
 name|sp
 operator|++
@@ -1139,12 +1181,10 @@ specifier|static
 name|int
 name|str_entry
 parameter_list|(
-name|c
-parameter_list|)
 name|int
 name|c
-decl_stmt|;
-comment|/* current character */
+parameter_list|)
+comment|/* c is current character */
 block|{
 name|int
 name|curline
@@ -1209,6 +1249,24 @@ condition|;
 control|)
 block|{
 comment|/* get next token */
+if|if
+condition|(
+name|sp
+operator|==
+name|tok
+operator|+
+sizeof|sizeof
+name|tok
+operator|-
+literal|1
+condition|)
+comment|/* Too long -- truncate it */
+operator|*
+name|sp
+operator|=
+name|EOS
+expr_stmt|;
+else|else
 operator|*
 name|sp
 operator|++
@@ -1331,12 +1389,10 @@ begin_function
 name|void
 name|skip_comment
 parameter_list|(
-name|t
-parameter_list|)
 name|int
 name|t
-decl_stmt|;
-comment|/* comment character */
+parameter_list|)
+comment|/* t is comment character */
 block|{
 name|int
 name|c
@@ -1418,11 +1474,9 @@ begin_function
 name|void
 name|skip_string
 parameter_list|(
-name|key
-parameter_list|)
 name|int
 name|key
-decl_stmt|;
+parameter_list|)
 block|{
 name|int
 name|c
@@ -1492,11 +1546,9 @@ begin_function
 name|int
 name|skip_key
 parameter_list|(
-name|key
-parameter_list|)
 name|int
 name|key
-decl_stmt|;
+parameter_list|)
 block|{
 name|int
 name|c
--- usr.bin/ctags/fortran.c
+++ usr.bin/ctags/fortran.c
@@ -3,43 +3,42 @@ begin_comment
 comment|/*  * Copyright (c) 1987, 1993, 1994  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
 begin_if
 if|#
 directive|if
 literal|0
 end_if
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
+
 begin_endif
-unit|static const char sccsid[] = "@(#)fortran.c	8.3 (Berkeley) 4/2/94";
+unit|static char sccsid[] = "@(#)fortran.c	8.3 (Berkeley) 4/2/94";
 endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -71,18 +70,15 @@ directive|include
 file|"ctags.h"
 end_include
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|void
 name|takeprec
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_decl_stmt
 name|char
@@ -432,13 +428,19 @@ expr_stmt|;
 operator|(
 name|void
 operator|)
-name|strcpy
+name|strlcpy
 argument_list|(
 name|tok
 argument_list|,
 name|lbp
+argument_list|,
+sizeof|sizeof
+argument_list|(
+name|tok
+argument_list|)
 argument_list|)
 expr_stmt|;
+comment|/* possible trunc */
 name|getline
 argument_list|()
 expr_stmt|;
@@ -467,12 +469,11 @@ begin_function
 name|int
 name|cicmp
 parameter_list|(
-name|cp
-parameter_list|)
+specifier|const
 name|char
 modifier|*
 name|cp
-decl_stmt|;
+parameter_list|)
 block|{
 name|int
 name|len
@@ -547,7 +548,9 @@ begin_function
 specifier|static
 name|void
 name|takeprec
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 for|for
 control|(
