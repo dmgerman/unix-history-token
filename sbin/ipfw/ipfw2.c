3654fb73d479bab8bd7dafb35e5375835cd36401
Luigi Rizzo <luigi@FreeBSD.org>
2002-07-31 22:31:47 +0000

    Two bugfixes:   + the header file contains two different opcodes (O_IPOPTS and O_IPOPT)     for what is the same thing, and sure enough i used one in the kernel     and the other one in userland. Be consistent!
      + "keep-state" and "limit" must be the last match pattern in a rule,
        so no matter how you enter them move them to the end of the rule.
    
    
    Former-commit-id: 7edc2e630d3b3d4b4a668a39bfc90a6c88f48695

--- sbin/ipfw/ipfw2.c
+++ sbin/ipfw/ipfw2.c
@@ -4749,7 +4749,7 @@ argument_list|)
 expr_stmt|;
 break|break;
 case|case
-name|O_IPOPTS
+name|O_IPOPT
 case|:
 name|print_flags
 argument_list|(
@@ -11093,10 +11093,11 @@ init|=
 literal|0
 decl_stmt|;
 comment|/* set if we have a MAC address */
-name|int
+name|ipfw_insn
+modifier|*
 name|have_state
 init|=
-literal|0
+name|NULL
 decl_stmt|;
 comment|/* check-state or keep-state */
 name|int
@@ -11366,7 +11367,7 @@ name|TOK_CHECKSTATE
 case|:
 name|have_state
 operator|=
-literal|1
+name|action
 expr_stmt|;
 name|action
 operator|->
@@ -11986,15 +11987,10 @@ if|if
 condition|(
 name|have_state
 condition|)
-block|{
-name|have_state
-operator|=
-literal|0
-expr_stmt|;
+comment|/* must be a check-state, we are done */
 goto|goto
 name|done
 goto|;
-block|}
 define|#
 directive|define
 name|OR_START
@@ -13105,7 +13101,7 @@ name|fill_flags
 argument_list|(
 name|cmd
 argument_list|,
-name|O_IPOPTS
+name|O_IPOPT
 argument_list|,
 name|f_ipopts
 argument_list|,
@@ -13132,7 +13128,7 @@ name|fill_flags
 argument_list|(
 name|cmd
 argument_list|,
-name|O_IPOPTS
+name|O_IPTOS
 argument_list|,
 name|f_iptos
 argument_list|,
@@ -13562,13 +13558,13 @@ name|errx
 argument_list|(
 name|EX_USAGE
 argument_list|,
-literal|"only one of check-state "
+literal|"only one of keep-state "
 literal|"and limit is allowed"
 argument_list|)
 expr_stmt|;
 name|have_state
 operator|=
-literal|1
+name|cmd
 expr_stmt|;
 name|fill_cmd
 argument_list|(
@@ -13598,9 +13594,13 @@ name|errx
 argument_list|(
 name|EX_USAGE
 argument_list|,
-literal|"only one of check-state "
+literal|"only one of keep-state "
 literal|"and limit is allowed"
 argument_list|)
+expr_stmt|;
+name|have_state
+operator|=
+name|cmd
 expr_stmt|;
 block|{
 name|ipfw_insn_limit
@@ -13728,10 +13728,6 @@ expr_stmt|;
 name|av
 operator|++
 expr_stmt|;
-name|have_state
-operator|=
-literal|1
-expr_stmt|;
 block|}
 break|break;
 default|default:
@@ -13788,6 +13784,12 @@ comment|/* 	 * generate O_PROBE_STATE if necessary 	 */
 if|if
 condition|(
 name|have_state
+operator|&&
+name|have_state
+operator|->
+name|opcode
+operator|!=
+name|O_CHECK_STATE
 condition|)
 block|{
 name|fill_cmd
@@ -13809,7 +13811,7 @@ name|dst
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * copy all commands but O_LOG 	 */
+comment|/* 	 * copy all commands but O_LOG, O_KEEP_STATE, O_LIMIT 	 */
 for|for
 control|(
 name|src
@@ -13836,15 +13838,24 @@ argument_list|(
 name|src
 argument_list|)
 expr_stmt|;
-if|if
+switch|switch
 condition|(
 name|src
 operator|->
 name|opcode
-operator|!=
-name|O_LOG
 condition|)
 block|{
+case|case
+name|O_LOG
+case|:
+case|case
+name|O_KEEP_STATE
+case|:
+case|case
+name|O_LIMIT
+case|:
+break|break;
+default|default:
 name|bcopy
 argument_list|(
 name|src
@@ -13864,6 +13875,38 @@ operator|+=
 name|i
 expr_stmt|;
 block|}
+block|}
+comment|/* 	 * put back the have_state command as last opcode 	 */
+if|if
+condition|(
+name|have_state
+condition|)
+block|{
+name|i
+operator|=
+name|F_LEN
+argument_list|(
+name|have_state
+argument_list|)
+expr_stmt|;
+name|bcopy
+argument_list|(
+name|have_state
+argument_list|,
+name|dst
+argument_list|,
+name|i
+operator|*
+sizeof|sizeof
+argument_list|(
+name|u_int32_t
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|dst
+operator|+=
+name|i
+expr_stmt|;
 block|}
 comment|/* 	 * start action section 	 */
 name|rule
