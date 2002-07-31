925bcf79a6fc0dc79c31ff6522a870b9dcbb68d6
Jacques Vidrine <nectar@FreeBSD.org>
2002-07-31 15:11:59 +0000

    Sort headers to reduce diffs between branches.
    Former-commit-id: 915364ecabda3af58485d2972a23c551c539f917

--- lib/libc/xdr/xdr_array.c
+++ lib/libc/xdr/xdr_array.c
@@ -80,6 +80,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<limits.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<stdio.h>
 end_include
 
@@ -93,12 +99,6 @@ begin_include
 include|#
 directive|include
 file|<string.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<limits.h>
 end_include
 
 begin_include
