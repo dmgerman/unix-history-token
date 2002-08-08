14070c382af3e484081f15682cfc9c40f4a1ee93
Warner Losh <imp@FreeBSD.org>
2002-08-08 15:52:55 +0000

    Don't bogusly depend on pcic.  pccard should be enough.
    Noticed by: jhay
    Forgotten  about for two years by: imp
    
    
    Former-commit-id: b82fb928eba98315bd829ac036aa22300b2f0e23

--- sys/dev/sn/if_sn_pccard.c
+++ sys/dev/sn/if_sn_pccard.c
@@ -450,25 +450,5 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
-begin_expr_stmt
-name|MODULE_DEPEND
-argument_list|(
-name|if_sn
-argument_list|,
-name|pcic
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_comment
-comment|/* XXX */
-end_comment
-
 end_unit
 
