8402844d4ad4d3f2c34152270d31e45f1a2f2914
Maxime Henrion <mux@FreeBSD.org>
2002-08-13 16:07:39 +0000

    Don't depend on namespace pollution in sys/mount.h and include sys/uio.h.
    Former-commit-id: ce1c0f9b879d330316f582eaf83c9872cba70f17

--- sbin/mount_nullfs/mount_nullfs.c
+++ sbin/mount_nullfs/mount_nullfs.c
@@ -82,6 +82,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/uio.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<err.h>
 end_include
 
--- sbin/mount_udf/mount_udf.c
+++ sbin/mount_udf/mount_udf.c
@@ -34,6 +34,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/uio.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<err.h>
 end_include
 
