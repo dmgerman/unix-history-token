f4ba3ebf2b2be17680f72bf0b8da120e33d84229
Ruslan Ermilov <ru@FreeBSD.org>
2002-08-15 08:45:49 +0000

    Merge gcc.295/cccp.c,v 1.9: don't let gcc(1) hide warnings in system headers.
    Reviewed by:	bde
    
    Original 2.95 change was subject to:
    
    Approved by:	obrien
    
    
    Former-commit-id: 0a3974e56faab79ff3eddff765cc8ea717d78a78

--- contrib/gcc/c-lex.c
+++ contrib/gcc/c-lex.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Lexical analyzer for C and Objective C.    Copyright (C) 1987, 1988, 1989, 1992, 1994, 1995, 1996, 1997    1998, 1999, 2000 Free Software Foundation, Inc.  This file is part of GCC.  GCC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GCC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GCC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -1386,6 +1390,9 @@ operator|->
 name|to_file
 argument_list|)
 expr_stmt|;
+ifndef|#
+directive|ifndef
+name|FREEBSD_NATIVE
 name|in_system_header
 operator|=
 name|new_map
@@ -1394,6 +1401,16 @@ name|sysp
 operator|!=
 literal|0
 expr_stmt|;
+else|#
+directive|else
+comment|/* FREEBSD_NATIVE */
+name|in_system_header
+operator|=
+literal|0
+expr_stmt|;
+endif|#
+directive|endif
+comment|/* FREEBSD_NATIVE */
 name|input_filename
 operator|=
 name|new_map
