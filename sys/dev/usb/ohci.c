8a08c84504c5282e78a881331fb8258263eea840
Josef Karthauser <joe@FreeBSD.org>
2002-08-16 09:10:43 +0000

    MFNetBSD: 	revision 1.127 	date: 2002/08/07 20:03:19;  author: augustss;  lines: +4 -8 	Fix some braindead calls to free memory (only encountered 	under low memory conditions).  From OpenBSD.
    Former-commit-id: 5f5992c3a5be9e700273928bf11125a19e3d2069

--- sys/dev/usb/ohci.c
+++ sys/dev/usb/ohci.c
@@ -8,6 +8,10 @@ comment|/*	$FreeBSD$	*/
 end_comment
 
 begin_comment
+comment|/* Also, already ported:  *	$NetBSD: ohci.c,v 1.127 2002/08/07 20:03:19 augustss Exp $  */
+end_comment
+
+begin_comment
 comment|/*  * Copyright (c) 1998 The NetBSD Foundation, Inc.  * All rights reserved.  *  * This code is derived from software contributed to The NetBSD Foundation  * by Lennart Augustsson (lennart@augustsson.net) at  * Carlstedt Research& Technology.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *        This product includes software developed by the NetBSD  *        Foundation, Inc. and its contributors.  * 4. Neither the name of The NetBSD Foundation nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
@@ -10548,18 +10552,9 @@ name|sitd
 operator|==
 name|NULL
 condition|)
-block|{
-name|ohci_free_sitd
-argument_list|(
-name|sc
-argument_list|,
-name|sitd
-argument_list|)
-expr_stmt|;
 goto|goto
 name|bad1
 goto|;
-block|}
 name|opipe
 operator|->
 name|tail
@@ -10614,18 +10609,9 @@ name|std
 operator|==
 name|NULL
 condition|)
-block|{
-name|ohci_free_std
-argument_list|(
-name|sc
-argument_list|,
-name|std
-argument_list|)
-expr_stmt|;
 goto|goto
 name|bad1
 goto|;
-block|}
 name|opipe
 operator|->
 name|tail
