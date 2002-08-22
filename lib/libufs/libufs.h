4f52e805b5fecba4787bf9331455d42d4ef3edcf
Juli Mallett <jmallett@FreeBSD.org>
2002-08-22 23:35:35 +0000

    Wrap the header to prevent multiple inclusion, and mark the DECLS section.
    Reminded by:	Rachel Hestilow <hestilow@ximian.com>
    
    
    Former-commit-id: bc1e56263fc39d946eb363e4290a32346eef365c

--- lib/libufs/libufs.h
+++ lib/libufs/libufs.h
@@ -3,6 +3,18 @@ begin_comment
 comment|/*  * Copyright (c) 2002 Juli Mallett.  All rights reserved.  *  * This software was written by Juli Mallett<jmallett@FreeBSD.org> for the  * FreeBSD project.  Redistribution and use in source and binary forms, with  * or without modification, are permitted provided that the following  * conditions are met:  *  * 1. Redistribution of source code must retain the above copyright notice,  *    this list of conditions and the following disclaimer.  * 2. Redistribution in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__LIBUFS_H__
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|__LIBUFS_H__
+end_define
+
 begin_comment
 comment|/*  * libufs macros (internal, non-exported).  */
 end_comment
@@ -149,15 +161,10 @@ block|}
 struct|;
 end_struct
 
-begin_comment
-comment|/*  * libufs prototypes.  */
-end_comment
-
-begin_comment
-comment|/*  * block.c  */
-end_comment
-
 begin_function_decl
+name|__BEGIN_DECLS
+comment|/*  * libufs prototypes.  */
+comment|/*  * block.c  */
 name|ssize_t
 name|bread
 parameter_list|(
@@ -300,6 +307,19 @@ modifier|*
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_macro
+name|__END_DECLS
+end_macro
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __LIBUFS_H__ */
+end_comment
 
 end_unit
 
