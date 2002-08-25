626a08a88fdc6d44c168dfd2d7e214bf8df1bc1d
Crist J. Clark <cjc@FreeBSD.org>
2002-08-25 06:05:25 +0000

    Don't give up on a remote log host when we get a EHOSTUNREACH or EHOSTDOWN. These are often transient errors (when the remote host reboots, temporary network problems, etc.), and we'd rather err on the side of caution and keep trying send messages that never arrive than just give up.
    Note that this is not an implementation of the "back-off" methods
    given in the PR. Those just seem too complicated. Why not just keep
    trying each time? Trying and failing doesn't really consume
    significantly more resources than if we were successful for each
    message.
    
    PR:		bin/31029
    MFC after:	1 week
    
    Former-commit-id: f0c73a2089091cac9b1f8bbd10d559c4626bf2cf

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -5702,6 +5702,15 @@ name|send_to_all
 condition|)
 break|break;
 block|}
+name|dprintf
+argument_list|(
+literal|"lsent/l: %d/%d\n"
+argument_list|,
+name|lsent
+argument_list|,
+name|l
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|lsent
@@ -5714,6 +5723,43 @@ name|e
 init|=
 name|errno
 decl_stmt|;
+name|logerror
+argument_list|(
+literal|"sendto"
+argument_list|)
+expr_stmt|;
+name|errno
+operator|=
+name|e
+expr_stmt|;
+switch|switch
+condition|(
+name|errno
+condition|)
+block|{
+case|case
+name|EHOSTUNREACH
+case|:
+case|case
+name|EHOSTDOWN
+case|:
+break|break;
+comment|/* case EBADF: */
+comment|/* case EACCES: */
+comment|/* case ENOTSOCK: */
+comment|/* case EFAULT: */
+comment|/* case EMSGSIZE: */
+comment|/* case EAGAIN: */
+comment|/* case ENOBUFS: */
+comment|/* case ECONNREFUSED: */
+default|default:
+name|dprintf
+argument_list|(
+literal|"removing entry\n"
+argument_list|,
+name|e
+argument_list|)
+expr_stmt|;
 operator|(
 name|void
 operator|)
@@ -5724,21 +5770,14 @@ operator|->
 name|f_file
 argument_list|)
 expr_stmt|;
-name|errno
-operator|=
-name|e
-expr_stmt|;
 name|f
 operator|->
 name|f_type
 operator|=
 name|F_UNUSED
 expr_stmt|;
-name|logerror
-argument_list|(
-literal|"sendto"
-argument_list|)
-expr_stmt|;
+break|break;
+block|}
 block|}
 block|}
 break|break;
