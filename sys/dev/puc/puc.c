03494c1192645ab0034d3bfaa8fbc994449c6b18
Poul-Henning Kamp <phk@FreeBSD.org>
2002-08-31 18:42:31 +0000

    Don't enable PUC_DEBUG by accident
    Former-commit-id: 940af86838f7a515e84a4d9a977d202d0577e180

--- sys/dev/puc/puc.c
+++ sys/dev/puc/puc.c
@@ -1,10 +1,4 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_define
-define|#
-directive|define
-name|PUC_DEBUG
-end_define
-
 begin_comment
 comment|/*	$NetBSD: puc.c,v 1.7 2000/07/29 17:43:38 jlam Exp $	*/
 end_comment
