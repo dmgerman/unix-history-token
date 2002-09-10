8bba10a1ff4939e42db2e650337b62165c4202e2
Bruce Evans <bde@FreeBSD.org>
2002-09-10 12:19:16 +0000

    Fixed namespace pollution in uma changes: - use `struct uma_zone *' instead of uma_zone_t, so that <sys/uma.h> isn't   a prerequisite. - don't include <sys/uma.h>. Namespace pollution makes "opaque" types like uma_zone_t perfectly non-opaque.  Such types should never be used (see style(9)).
    Former-commit-id: 159005f5e302dd8b11f500c76e095591783e2553

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -123,12 +123,6 @@ begin_comment
 comment|/* Machine-dependent proc substruct. */
 end_comment
 
-begin_include
-include|#
-directive|include
-file|<vm/uma.h>
-end_include
-
 begin_comment
 comment|/*  * One structure allocated per session.  *  * List of locks  * (m)		locked by s_mtx mtx  * (e)		locked by proctree_lock sx  * (c)		const until freeing  */
 end_comment
@@ -2980,7 +2974,9 @@ end_comment
 
 begin_decl_stmt
 specifier|extern
-name|uma_zone_t
+name|struct
+name|uma_zone
+modifier|*
 name|proc_zone
 decl_stmt|;
 end_decl_stmt
