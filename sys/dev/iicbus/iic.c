f2cd584b8c13648afa322c04573dc614a795c192
Peter Wemm <peter@FreeBSD.org>
2002-09-19 03:25:46 +0000

    move <machine/iic.h> and <machine/smb.h> to <dev/iicbus/iic.h> and <dev/smbus/smb.h> - there is nothing MD about these ioctl definitions.
    Former-commit-id: ce69df9435797186b5444797a4d0d341e56feecb

--- sys/dev/iicbus/iic.c
+++ sys/dev/iicbus/iic.c
@@ -66,7 +66,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/iic.h>
+file|<dev/iicbus/iic.h>
 end_include
 
 begin_include
--- sys/dev/smbus/smb.c
+++ sys/dev/smbus/smb.c
@@ -66,7 +66,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/smb.h>
+file|<dev/smbus/smb.h>
 end_include
 
 begin_include
