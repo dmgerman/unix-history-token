291946266a0445f336b3d8625263db46c11a8970
Martin Blapp <mbr@FreeBSD.org>
2002-09-20 15:16:06 +0000

    Enable the automatic TX underrun recovery for the ADMtek chips. This solves cvsup update on my laptop which aborts after a while without this patch.
    PR:		34236
    Reviewed by:	phk
    MFC after:	2 weeks
    
    Former-commit-id: 59d05e54e9f66fa4474270036fd491e32bd36081

--- sys/dev/dc/if_dcreg.h
+++ sys/dev/dc/if_dcreg.h
@@ -2827,6 +2827,17 @@ end_comment
 begin_define
 define|#
 directive|define
+name|DC_AL_CR
+value|0x88
+end_define
+
+begin_comment
+comment|/* command register */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|DC_AL_PAR0
 value|0xA4
 end_define
@@ -2943,6 +2954,17 @@ end_define
 
 begin_comment
 comment|/* built in PHY autoneg expansion */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DC_AL_CR_ATUR
+value|0x00000001
+end_define
+
+begin_comment
+comment|/* automatic TX underrun recovery */
 end_comment
 
 begin_define
