893d0bd74c06fb28b9e160fc9d68a8d6709ee05f
Jeff Roberson <jeff@FreeBSD.org>
2002-09-23 00:29:31 +0000

    - Correct the name in the header guards.
    Spotted by:	rwatson
    
    
    Former-commit-id: b16b5840f5816f6c1d18f67768ec0a55290f2679

--- sys/sys/alq.h
+++ sys/sys/alq.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_SYS_ALD_H_
+name|_SYS_ALQ_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_SYS_ALD_H_
+name|_SYS_ALQ_H_
 end_define
 
 begin_comment
@@ -209,7 +209,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _SYS_ALD_H_ */
+comment|/* _SYS_ALQ_H_ */
 end_comment
 
 end_unit
