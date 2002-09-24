0d40957fac87dfa50b1b7bfc25c3873d1f028ac5
Mike Barcroft <mike@FreeBSD.org>
2002-09-24 17:28:12 +0000

    Add forgotten <sys/_types.h> include.  Fix some style bugs while I'm here.
    PR: 43326
    
    Former-commit-id: 088f1111077a6f597f04d72f97dde6783993f3e1

--- include/nl_types.h
+++ include/nl_types.h
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $FreeBSD$ */
-end_comment
-
-begin_comment
-comment|/*********************************************************** Copyright 1990, by Alfalfa Software Incorporated, Cambridge, Massachusetts.                          All Rights Reserved  Permission to use, copy, modify, and distribute this software and its documentation for any purpose and without fee is hereby granted, provided that the above copyright notice appear in all copies and that both that copyright notice and this permission notice appear in supporting documentation, and that Alfalfa's name not be used in advertising or publicity pertaining to distribution of the software without specific, written prior permission.  ALPHALPHA DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL ALPHALPHA BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  If you make any modifications, bugfixes or other changes to this software we'd appreciate it if you could send a copy to us so we can keep things up-to-date.  Many thanks. 				Kee Hinckley 				Alfalfa Software, Inc. 				267 Allston St., #3 				Cambridge, MA 02139  USA 				nazgul@alfalfa.com  ******************************************************************/
+comment|/*-  * Copyright 1990, by Alfalfa Software Incorporated, Cambridge, Massachusetts.  *  *                         All Rights Reserved  *  * Permission to use, copy, modify, and distribute this software and its  * documentation for any purpose and without fee is hereby granted,  * provided that the above copyright notice appear in all copies and that  * both that copyright notice and this permission notice appear in  * supporting documentation, and that Alfalfa's name not be used in  * advertising or publicity pertaining to distribution of the software  * without specific, written prior permission.  *  * ALPHALPHA DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING  * ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL  * ALPHALPHA BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR  * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,  * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,  * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS  * SOFTWARE.  *  * If you make any modifications, bugfixes or other changes to this software  * we'd appreciate it if you could send a copy to us so we can keep things  * up-to-date.  Many thanks.  *				Kee Hinckley  *				Alfalfa Software, Inc.  *				267 Allston St., #3  *				Cambridge, MA 02139  USA  *				nazgul@alfalfa.com  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -25,6 +21,12 @@ directive|include
 file|<sys/cdefs.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<sys/_types.h>
+end_include
+
 begin_define
 define|#
 directive|define
@@ -38,6 +40,14 @@ directive|define
 name|NL_CAT_LOCALE
 value|1
 end_define
+
+begin_typedef
+typedef|typedef
+name|void
+modifier|*
+name|nl_catd
+typedef|;
+end_typedef
 
 begin_ifndef
 ifndef|#
@@ -63,17 +73,8 @@ endif|#
 directive|endif
 end_endif
 
-begin_typedef
-typedef|typedef
-name|void
-modifier|*
-name|nl_catd
-typedef|;
-end_typedef
-
 begin_function_decl
 name|__BEGIN_DECLS
-specifier|extern
 name|nl_catd
 name|catopen
 parameter_list|(
@@ -87,7 +88,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-specifier|extern
 name|char
 modifier|*
 name|catgets
@@ -106,7 +106,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-specifier|extern
 name|int
 name|catclose
 parameter_list|(
@@ -125,7 +124,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _NL_TYPES_H_ */
+comment|/* !_NL_TYPES_H_ */
 end_comment
 
 end_unit
