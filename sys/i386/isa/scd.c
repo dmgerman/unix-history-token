04cf6b32b142e4d4ae60e58ca5faabace1815bd2
Poul-Henning Kamp <phk@FreeBSD.org>
2002-09-30 13:49:20 +0000

    If GEOM is in the kernel, take these three out.  I have no way of testing any modifications to them, they shouldn't even bother with disklabels in the first place and they are just plain obsolete old hardware which should be axed entirely before 5.0-R IMO.
    Sponsored by:	DARPA & NAI Labs.
    
    
    Former-commit-id: c725c72f03fa0305cd0f94a965c53f33b5c569d9

--- sys/i386/isa/scd.c
+++ sys/i386/isa/scd.c
@@ -1,4 +1,16 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_include
+include|#
+directive|include
+file|"opt_geom.h"
+end_include
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|GEOM
+end_ifndef
+
 begin_comment
 comment|/*-  * Copyright (c) 1995 Mikael Hybsch  * All rights reserved.  *  * Portions of this file are copied from mcd.c  * which has the following copyrights:  *  *	Copyright 1993 by Holger Veit (data part)  *	Copyright 1993 by Brian Moore (audio part)  *	Changes Copyright 1993 by Gary Clark II  *	Changes Copyright (C) 1994 by Andrew A. Chernov  *  *	Rewrote probe routine to work on newer Mitsumi drives.  *	Additional changes (C) 1994 by Jordan K. Hubbard  *  *	All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  */
 end_comment
@@ -8204,6 +8216,11 @@ literal|0
 return|;
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
