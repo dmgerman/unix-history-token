0ab7ef87f08a764205814881226f13e081e8dd78
Matthew N. Dodd <mdodd@FreeBSD.org>
2002-10-02 04:42:24 +0000

    Create/destroy memory zones on module load/unload, not on device attach/detach.
    Former-commit-id: 2d7559c140d908cb7eed2a2cf10ab180cd38fb67

--- sys/dev/hea/hea_pci.c
+++ sys/dev/hea/hea_pci.c
@@ -877,5 +877,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|hea
+argument_list|,
+name|hea
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
