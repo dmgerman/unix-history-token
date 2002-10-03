10e1f4b0e92f5efa3ff8b83b1af3b3d3f3252a33
Bruce Evans <bde@FreeBSD.org>
2002-10-03 15:00:07 +0000

    Use a non-bogus type for representing the values of offsets in structs. off_t is for offsets in files, and it is signed so it was no better than the original type of int for avoiding warnings from broken lints, except accidentally on machines like i386's where size_t is smaller than off_t.
    Former-commit-id: df069d48e4a04c8fb5afee83dc0a680f737c830a

--- bin/ps/ps.h
+++ bin/ps/ps.h
@@ -174,7 +174,7 @@ name|width
 decl_stmt|;
 comment|/* printing width */
 comment|/* 	 * The following (optional) elements are hooks for passing information 	 * to the generic output routine pvar (which prints simple elements 	 * from the well known kinfo_proc structure). 	 */
-name|off_t
+name|int
 name|off
 decl_stmt|;
 comment|/* offset in structure */
