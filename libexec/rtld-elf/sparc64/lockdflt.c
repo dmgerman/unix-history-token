27c97391bf5098299a28453677d6cd38530e10ae
Ruslan Ermilov <ru@FreeBSD.org>
2002-10-09 20:20:43 +0000

    <machine/atomic.h> requires <sys/types.h>.
    Reviewed by:	jake, mike
    
    
    Former-commit-id: 3a61e271be2f3c2e0ca66ccf1b5c8b512d33f653

--- libexec/rtld-elf/sparc64/lockdflt.c
+++ libexec/rtld-elf/sparc64/lockdflt.c
@@ -28,12 +28,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/atomic.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|"debug.h"
 end_include
 
--- libexec/rtld-elf/sparc64/rtld_machdep.h
+++ libexec/rtld-elf/sparc64/rtld_machdep.h
@@ -19,6 +19,12 @@ end_define
 begin_include
 include|#
 directive|include
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/atomic.h>
 end_include
 
