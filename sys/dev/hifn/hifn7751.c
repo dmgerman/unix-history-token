9f82b1c86d7a28f999025973335b0307be588a5a
Sam Leffler <sam@FreeBSD.org>
2002-10-15 23:25:56 +0000

    missed additional callout that can run w/o Giant locked
    Former-commit-id: b3e59834f71d485e7dde03cceed17facf1f53ebc

--- sys/dev/hifn/hifn7751.c
+++ sys/dev/hifn/hifn7751.c
@@ -3388,6 +3388,7 @@ name|sc_rnghz
 operator|=
 literal|1
 expr_stmt|;
+comment|/* NB: 1 means the callout runs w/o Giant locked */
 name|callout_init
 argument_list|(
 operator|&
@@ -3395,7 +3396,7 @@ name|sc
 operator|->
 name|sc_rngto
 argument_list|,
-literal|0
+literal|1
 argument_list|)
 expr_stmt|;
 name|callout_reset
