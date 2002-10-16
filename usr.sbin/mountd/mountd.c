ffc74fcf4bc797fb175826f3ddac7eb770c08e7e
Philippe Charnier <charnier@FreeBSD.org>
2002-10-16 16:04:50 +0000

    s/rpc.mountd/mountd/ Add FBSDID Reorder #if/#endif around sccsid to conform style(9)
    Former-commit-id: 103adf90d5c3eb1920130a5a3db609a769c3c0bb

--- usr.sbin/mountd/mountd.c
+++ usr.sbin/mountd/mountd.c
@@ -29,17 +29,17 @@ begin_comment
 comment|/*not lint*/
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
 begin_if
 if|#
 directive|if
 literal|0
 end_if
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|lint
+end_ifndef
 
 begin_endif
 unit|static char sccsid[] = "@(#)mountd.c	8.15 (Berkeley) 5/1/95";
@@ -47,25 +47,28 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
+begin_comment
+comment|/*not lint*/
+end_comment
 
 begin_endif
 endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/*not lint*/
-end_comment
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -1630,7 +1633,7 @@ name|errx
 argument_list|(
 literal|1
 argument_list|,
-literal|"another rpc.mountd is already running. Aborting"
+literal|"another mountd is already running. Aborting"
 argument_list|)
 expr_stmt|;
 name|s
