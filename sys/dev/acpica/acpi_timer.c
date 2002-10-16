16027e0f38fed930a482723f31edafc2a40c7ec2
John Baldwin <jhb@FreeBSD.org>
2002-10-16 17:27:40 +0000

    struct timecounter is defined in sys/time.h on 4-stable.
    Sponsored by:	The Weather Channel
    
    
    Former-commit-id: 73e829ffade3f5666054687733a7f985f5b52ed7

--- sys/dev/acpica/acpi_timer.c
+++ sys/dev/acpica/acpi_timer.c
@@ -33,11 +33,35 @@ directive|include
 file|<sys/sysctl.h>
 end_include
 
+begin_if
+if|#
+directive|if
+name|__FreeBSD_version
+operator|>=
+literal|500000
+end_if
+
 begin_include
 include|#
 directive|include
 file|<sys/timetc.h>
 end_include
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_include
+include|#
+directive|include
+file|<sys/time.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
