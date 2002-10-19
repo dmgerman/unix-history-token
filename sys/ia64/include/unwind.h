8ec37dc935fc2a063386f4deba18fef74f7855f5
Marcel Moolenaar <marcel@FreeBSD.org>
2002-10-19 19:30:38 +0000

    Update the unwind information when modules are loaded and unloaded by using the linker hooks. Since these hooks are called for the kernel as well, we don't need to deal with that with a special SYSINIT. The initialization implicitly performed on the first update of the unwind information is made explicit with a SYSINIT. We now don't need the _ia64_unwind_{start|end} symbols.
    Former-commit-id: 8e671f148d52abe835761050d4e795f421ac6c70

--- sys/ia64/include/unwind.h
+++ sys/ia64/include/unwind.h
@@ -3,41 +3,24 @@ begin_comment
 comment|/*-  * Copyright (c) 2001 Doug Rabson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
-begin_struct_decl
-struct_decl|struct
-name|ia64_unwind_table
-struct_decl|;
-end_struct_decl
-
 begin_function_decl
-name|struct
-name|ia64_unwind_table
-modifier|*
+name|int
 name|ia64_add_unwind_table
 parameter_list|(
-name|u_int64_t
-modifier|*
-name|base
+name|vm_offset_t
 parameter_list|,
-name|u_int64_t
-modifier|*
-name|start
+name|vm_offset_t
 parameter_list|,
-name|u_int64_t
-modifier|*
-name|end
+name|vm_offset_t
 parameter_list|)
 function_decl|;
 end_function_decl
 
 begin_function_decl
 name|void
-name|ia64_free_unwind_table
+name|ia64_delete_unwind_table
 parameter_list|(
-name|struct
-name|ia64_unwind_table
-modifier|*
-name|ut
+name|vm_offset_t
 parameter_list|)
 function_decl|;
 end_function_decl
