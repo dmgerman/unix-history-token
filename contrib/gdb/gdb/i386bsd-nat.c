e2fb305fc26f41ec4647e6cfff7aeecb4d8dc0ea
Poul-Henning Kamp <phk@FreeBSD.org>
2002-10-21 05:33:05 +0000

    Track changed definition of the debug registers.
    Sorry for missing this the first time.
    
    
    Former-commit-id: 0829d33b7cf675eecd66ebb934bb2533fe1da9aa

--- contrib/gdb/gdb/i386bsd-nat.c
+++ contrib/gdb/gdb/i386bsd-nat.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Native-dependent code for modern i386 BSD's.    Copyright 2000, 2001, 2002 Free Software Foundation, Inc.     This file is part of GDB.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2 of the License, or    (at your option) any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 59 Temple Place - Suite 330,    Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -1160,7 +1164,10 @@ expr_stmt|;
 comment|/* For some mysterious reason, some of the reserved bits in the      debug control register get set.  Mask these off, otherwise the      ptrace call below will fail.  */
 name|dbregs
 operator|.
-name|dr7
+name|dr
+index|[
+literal|7
+index|]
 operator|&=
 operator|~
 operator|(
@@ -1340,7 +1347,10 @@ directive|endif
 return|return
 name|dbregs
 operator|.
-name|dr6
+name|dr
+index|[
+literal|6
+index|]
 return|;
 block|}
 end_function
