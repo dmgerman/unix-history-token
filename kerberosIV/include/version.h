62ad818f59170d9f3b653bf912235e72659cdaac
Assar Westerlund <assar@FreeBSD.org>
2002-10-23 13:21:32 +0000

    MFC kadm_ser_wrap.c:1.1.1.4: fix buffer overflow
    MFC updating kerberosIV and kerberos5 versions consistenly
    
    Approved by:	security-officer
    Obtained from:	kth-krb CVS
    
    
    Former-commit-id: 82a8305d302f4cbe3950e224206ff4462043b3b3

--- kerberosIV/include/version.h
+++ kerberosIV/include/version.h
@@ -8,7 +8,7 @@ name|char
 modifier|*
 name|krb4_long_version
 init|=
-literal|"@(#)$Version: krb4-1.0 by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 19:34:57 SAST 2000 $"
+literal|"@(#)$Version: krb4-1.0.5fb1 (FreeBSD) $"
 decl_stmt|;
 end_decl_stmt
 
@@ -17,7 +17,7 @@ name|char
 modifier|*
 name|krb4_version
 init|=
-literal|"krb4-1.0"
+literal|"krb4-1.0.5fb1"
 decl_stmt|;
 end_decl_stmt
 
@@ -26,7 +26,7 @@ name|char
 modifier|*
 name|heimdal_long_version
 init|=
-literal|"@(#)$Version: heimdal-0.1l by root on greenpeace.grondar.za (i386-unknown-freebsd4.0) Sat Jan  8 20:56:02 SAST 2000 $"
+literal|"@(#)$Version: Heimdal 0.3e fb1 (FreeBSD) $"
 decl_stmt|;
 end_decl_stmt
 
@@ -35,7 +35,7 @@ name|char
 modifier|*
 name|heimdal_version
 init|=
-literal|"heimdal-0.1l"
+literal|"Heimdal 0.3e fb1"
 decl_stmt|;
 end_decl_stmt
 
