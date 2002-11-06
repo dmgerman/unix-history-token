cbde79be5580bc009b69f8ef91c52a5b7248d884
Josef Karthauser <joe@FreeBSD.org>
2002-11-06 14:23:20 +0000

    MFC: Remove __P().
    Former-commit-id: 936ab4ba9e3b6f70baa7bd181d16661561177e9d

--- sys/dev/usb/usb_ethersubr.c
+++ sys/dev/usb/usb_ethersubr.c
@@ -129,18 +129,15 @@ name|usbq_tx
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
+begin_function_decl
 name|Static
 name|void
 name|usbintr
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_function
 name|Static
