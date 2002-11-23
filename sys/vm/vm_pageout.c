36be0a761c12ca84f59b9f70db59cce020cd4279
Alan Cox <alc@FreeBSD.org>
2002-11-23 08:08:54 +0000

    Assert that the page queues lock rather than Giant is held in vm_pageout_page_free().
    Approved by:	re
    
    
    Former-commit-id: 62f37905d0dd022210817ace99df9c66f08ddc1b

--- sys/vm/vm_pageout.c
+++ sys/vm/vm_pageout.c
@@ -2419,7 +2419,13 @@ name|object
 operator|->
 name|type
 decl_stmt|;
-name|GIANT_REQUIRED
+name|mtx_assert
+argument_list|(
+operator|&
+name|vm_page_queue_mtx
+argument_list|,
+name|MA_OWNED
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
