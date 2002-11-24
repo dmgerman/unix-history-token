1522f559c160c244f9353501a9a9f2aeefc19b8e
Alan Cox <alc@FreeBSD.org>
2002-11-24 21:13:34 +0000

    Add page queues locking to vunmapbuf().
    Approved by:	re (blanket)
    
    
    Former-commit-id: a12b2fa7d08981e0ea1007715f8fd27174c856e9

--- sys/sparc64/sparc64/vm_machdep.c
+++ sys/sparc64/sparc64/vm_machdep.c
@@ -1487,6 +1487,9 @@ argument_list|,
 name|npages
 argument_list|)
 expr_stmt|;
+name|vm_page_lock_queues
+argument_list|()
+expr_stmt|;
 for|for
 control|(
 name|pidx
@@ -1509,6 +1512,9 @@ index|[
 name|pidx
 index|]
 argument_list|)
+expr_stmt|;
+name|vm_page_unlock_queues
+argument_list|()
 expr_stmt|;
 name|bp
 operator|->
