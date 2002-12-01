7ce7d5a14bbec124bbd90593156f7dd4c8ca0297
Ruslan Ermilov <ru@FreeBSD.org>
2002-12-01 13:38:25 +0000

    Bootstrapping aid from pre-kqueue(2) systems, e.g. 4.0-RELEASE.
    Submitted by:	jmallett
    Approved by:	re (bmah)
    
    
    Former-commit-id: 03c2a32d767836b4b32291343851fb3521d17a49

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -67,11 +67,22 @@ directive|include
 file|<sys/time.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|USE_KQUEUE
+end_ifdef
+
 begin_include
 include|#
 directive|include
 file|<sys/event.h>
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
