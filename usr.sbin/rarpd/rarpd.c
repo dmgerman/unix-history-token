2519b3ad0dcd1984ebcafa47f743d7d523828a58
David Malone <dwmalone@FreeBSD.org>
2002-12-01 19:19:34 +0000

    Sync with -current: Minor man page changes, add -t option and document in comments and usage.
    Former-commit-id: d0b3a6e524d9bc98ce83323c233620c15beb2328

--- usr.sbin/rarpd/rarpd.c
+++ usr.sbin/rarpd/rarpd.c
@@ -56,7 +56,7 @@ comment|/* not lint */
 end_comment
 
 begin_comment
-comment|/*  * rarpd - Reverse ARP Daemon  *  * Usage:	rarpd -a [ -dfsv ] [ hostname ]  *		rarpd [ -dfsv ] interface [ hostname ]  *  * 'hostname' is optional solely for backwards compatibility with Sun's rarpd.  * Currently, the argument is ignored.  */
+comment|/*  * rarpd - Reverse ARP Daemon  *  * Usage:	rarpd -a [ -dfsv ] [-t directory] [ hostname ]  *		rarpd [ -dfsv ] [-t directory] interface [ hostname ]  *  * 'hostname' is optional solely for backwards compatibility with Sun's rarpd.  * Currently, the argument is ignored.  */
 end_comment
 
 begin_include
@@ -890,7 +890,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"adfsv"
+literal|"adfst:v"
 argument_list|)
 operator|)
 operator|!=
@@ -929,6 +929,14 @@ literal|'s'
 case|:
 operator|++
 name|sflag
+expr_stmt|;
+break|break;
+case|case
+literal|'t'
+case|:
+name|tftp_dir
+operator|=
+name|optarg
 expr_stmt|;
 break|break;
 case|case
@@ -2054,7 +2062,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: rarpd [-adfsv] [interface]\n"
+literal|"usage: rarpd [-adfsv] [-t directory] [interface]\n"
 argument_list|)
 expr_stmt|;
 name|exit
