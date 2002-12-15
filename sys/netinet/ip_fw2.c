2d43831fba0b2feaa6dc6cdd37b3f59b7dffb15e
Maxim Konovalov <maxim@FreeBSD.org>
2002-12-15 09:44:02 +0000

    o Fix byte order logging issue: sa.sin_port is already in host byte order.
    PR:		kern/45964
    Submitted by:	Sascha Blank <sblank@tiscali.de>
    Reviewed by:	luigi
    MFC after:	1 week
    
    Former-commit-id: 6e931045393aef9dd8f50924d123656dc87434c7

--- sys/netinet/ip_fw2.c
+++ sys/netinet/ip_fw2.c
@@ -2214,14 +2214,11 @@ argument_list|)
 argument_list|,
 literal|":%d"
 argument_list|,
-name|ntohs
-argument_list|(
 name|sa
 operator|->
 name|sa
 operator|.
 name|sin_port
-argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
