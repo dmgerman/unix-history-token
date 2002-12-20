41ea2394a8cfcc8db9be973e707deeba8101a849
Matthew Dillon <dillon@FreeBSD.org>
2002-12-20 09:57:49 +0000

    MFC 1.183, fix an ack-on-ack tcp buf.
    Former-commit-id: da68d168c47ad6f335ba9944a37ab33eaefc2b79

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -5538,8 +5538,15 @@ operator|.
 name|tcps_pawsdrop
 operator|++
 expr_stmt|;
+if|if
+condition|(
+name|tlen
+condition|)
 goto|goto
 name|dropafterack
+goto|;
+goto|goto
+name|drop
 goto|;
 block|}
 block|}
