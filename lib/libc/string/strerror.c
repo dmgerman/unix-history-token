f154486808522284f03e0798c11a7c7b3d69d4fe
Mike Barcroft <mike@FreeBSD.org>
2002-12-20 05:26:10 +0000

    Stylistic changes: o Fix an English error (comma splice) and poorly worded sentence. o Fix KNF ordering of variables (pointers come before arithmetic types). o Restore hand-optimization of sizeof()-1, instead of strlen(). o Remove unneeded local variables in strerror_r().
    Test by:	strerror regression test
    Requested by:	bde
    Reviewed by:	bde
    
    
    Former-commit-id: 87f150969245be0cb3a57e1dad658b947acfd84f

--- lib/libc/string/strerror.c
+++ lib/libc/string/strerror.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_if
@@ -77,7 +77,7 @@ value|"Unknown error: "
 end_define
 
 begin_comment
-comment|/*  * Define a buffer size big enough to describe a 64-bit signed integer  * converted to ASCII decimal (19 bytes), with an optional leading sign  * (1 byte), finally we get the prefix and a trailing NUL from UPREFIX.  */
+comment|/*  * Define a buffer size big enough to describe a 64-bit signed integer  * converted to ASCII decimal (19 bytes), with an optional leading sign  * (1 byte); finally, we get the prefix and a trailing NUL from UPREFIX.  */
 end_comment
 
 begin_define
@@ -88,7 +88,7 @@ value|(20 + sizeof(UPREFIX))
 end_define
 
 begin_comment
-comment|/* Don't link to stdio(3) to avoid bloat for statically linked binaries. */
+comment|/*  * Doing this by hand instead of linking with stdio(3) avoids bloat for  * statically linked binaries.  */
 end_comment
 
 begin_function
@@ -107,16 +107,16 @@ name|size_t
 name|len
 parameter_list|)
 block|{
-name|unsigned
-name|int
-name|uerr
-decl_stmt|;
 name|char
 modifier|*
 name|p
 decl_stmt|,
 modifier|*
 name|t
+decl_stmt|;
+name|unsigned
+name|int
+name|uerr
 decl_stmt|;
 name|char
 name|tmp
@@ -194,10 +194,12 @@ name|p
 operator|=
 name|buf
 operator|+
-name|strlen
+sizeof|sizeof
 argument_list|(
 name|UPREFIX
 argument_list|)
+operator|-
+literal|1
 init|;
 name|t
 operator|>
@@ -243,13 +245,6 @@ name|size_t
 name|buflen
 parameter_list|)
 block|{
-name|int
-name|retval
-decl_stmt|;
-name|retval
-operator|=
-literal|0
-expr_stmt|;
 if|if
 condition|(
 name|errnum
@@ -270,12 +265,12 @@ argument_list|,
 name|buflen
 argument_list|)
 expr_stmt|;
-name|retval
-operator|=
+return|return
+operator|(
 name|EINVAL
-expr_stmt|;
+operator|)
+return|;
 block|}
-elseif|else
 if|if
 condition|(
 name|strlcpy
@@ -292,13 +287,14 @@ argument_list|)
 operator|>=
 name|buflen
 condition|)
-name|retval
-operator|=
-name|ERANGE
-expr_stmt|;
 return|return
 operator|(
-name|retval
+name|ERANGE
+operator|)
+return|;
+return|return
+operator|(
+literal|0
 operator|)
 return|;
 block|}
