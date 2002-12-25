1f8c2545ea8f7feb61095bd3f14d55c25514f53c
Ruslan Ermilov <ru@FreeBSD.org>
2002-12-25 09:16:58 +0000

    A month after pst@ has committed his revision 1.8, it was incorporated by UCB as revision 8.5.  Do a diff reduction.
    Former-commit-id: e55de3cfef31a08c18b0e0f1ec58a1c0a28eb62e

--- sys/net/radix.c
+++ sys/net/radix.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1988, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)radix.c	8.4 (Berkeley) 11/2/94  * $FreeBSD$  */
+comment|/*  * Copyright (c) 1988, 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)radix.c	8.5 (Berkeley) 5/19/95  * $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -3037,7 +3037,7 @@ operator|)
 condition|)
 break|break;
 block|}
-comment|/* 		 * If the mask is not duplicated, we wouldn't 		 * find it among possible duplicate key entries 		 * anyway, so the above test doesn't hurt. 		 * 		 * We sort the masks for a duplicated key the same way as 		 * in a masklist -- most specific to least specific. 		 * This may require the unfortunate nuisance of relocating 		 * the head of the list. 		 */
+comment|/* 		 * If the mask is not duplicated, we wouldn't 		 * find it among possible duplicate key entries 		 * anyway, so the above test doesn't hurt. 		 * 		 * We sort the masks for a duplicated key the same way as 		 * in a masklist -- most specific to least specific. 		 * This may require the unfortunate nuisance of relocating 		 * the head of the list. 		 * 		 * We also reverse, or doubly link the list through the 		 * parent pointer. 		 */
 if|if
 condition|(
 name|tt
@@ -4136,7 +4136,7 @@ condition|(
 name|dupedkey
 condition|)
 block|{
-comment|/* 		 * at this point, tt is the deletion target and saved_tt 		 * is the head of the dupekey chain 		 */
+comment|/* 		 * Here, tt is the deletion target and 		 * saved_tt is the head of the dupekey chain. 		 */
 if|if
 condition|(
 name|tt
