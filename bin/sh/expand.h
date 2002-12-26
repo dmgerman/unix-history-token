c96849b00b275fa15f4a6c8ec156118d1eab99f3
Tim J. Robbins <tjr@FreeBSD.org>
2002-12-26 14:28:54 +0000

    Add the "wordexp" shell built-in command which will be used to implement the POSIX wordexp() function.
    Former-commit-id: b5ea17e0f4384e3c3050c48561a089c6c4eefe74

--- bin/sh/expand.h
+++ bin/sh/expand.h
@@ -182,5 +182,18 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|int
+name|wordexpcmd
+parameter_list|(
+name|int
+parameter_list|,
+name|char
+modifier|*
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
 end_unit
 
