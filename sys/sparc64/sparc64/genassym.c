896d87c8bbc2d42ef8b02044ab61bf73a0021761
Jake Burkholder <jake@FreeBSD.org>
2002-12-28 23:58:18 +0000

    Forgot this file in previous commit.
    Former-commit-id: 3a05d249c71b9240e1796d001ec2fc50e2115869

--- sys/sparc64/sparc64/genassym.c
+++ sys/sparc64/sparc64/genassym.c
@@ -256,6 +256,16 @@ end_expr_stmt
 begin_expr_stmt
 name|ASSYM
 argument_list|(
+name|VM_MIN_PROM_ADDRESS
+argument_list|,
+name|VM_MIN_PROM_ADDRESS
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|ASSYM
+argument_list|(
 name|EFAULT
 argument_list|,
 name|EFAULT
