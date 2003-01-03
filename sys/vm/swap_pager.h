9ffb502e3f2afa4211905ad4f8ea0a0de58cbd1e
Poul-Henning Kamp <phk@FreeBSD.org>
2003-01-03 14:22:52 +0000

    Use correct _VM_SWAP_PAGER_H_ to check for multiple inclusion.
    Former-commit-id: 193b3224b9643c4064a4df1f905b0de90a29ab66

--- sys/vm/swap_pager.h
+++ sys/vm/swap_pager.h
@@ -10,13 +10,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_SWAP_PAGER_
+name|_VM_SWAP_PAGER_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_SWAP_PAGER_
+name|_VM_SWAP_PAGER_H_
 value|1
 end_define
 
@@ -334,7 +334,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _SWAP_PAGER_ */
+comment|/* _VM_SWAP_PAGER_H_ */
 end_comment
 
 end_unit
