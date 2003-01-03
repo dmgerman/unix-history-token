d92ed4252062e52b6602e088b6b15eac59d374b1
Scott Long <scottl@FreeBSD.org>
2003-01-03 22:36:35 +0000

    As per the 5.0 TODO list, make malloc default to 'aj' instead of 'AJ'. This matches the default for the 4.x releases, and is only going into the RELENG_5_0 branch for now.  Making similar chages to HEAD is up for debate as always.
    Approved by:	re (rwatson)
    
    
    Former-commit-id: 7cf84a5495d51b69c562c033df4bab37e85e02ba

--- lib/libc/stdlib/malloc.c
+++ lib/libc/stdlib/malloc.c
@@ -905,8 +905,6 @@ begin_decl_stmt
 specifier|static
 name|int
 name|malloc_abort
-init|=
-literal|1
 decl_stmt|;
 end_decl_stmt
 
@@ -986,8 +984,6 @@ begin_decl_stmt
 specifier|static
 name|int
 name|malloc_junk
-init|=
-literal|1
 decl_stmt|;
 end_decl_stmt
 
