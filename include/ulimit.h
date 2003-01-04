4c43a5e212abeac809692af6131ec30dbebe4887
Tim J. Robbins <tjr@FreeBSD.org>
2003-01-04 00:32:18 +0000

    Bracket the prototype for ulimit() with __BEGIN_DECLS/__END_DECLS so that it can be called from C++.
    Former-commit-id: fa667414c910716bc95186850d765d293b8f65ba

--- include/ulimit.h
+++ include/ulimit.h
@@ -15,6 +15,12 @@ directive|define
 name|_ULIMIT_H_
 end_define
 
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
 begin_define
 define|#
 directive|define
@@ -30,6 +36,7 @@ value|2
 end_define
 
 begin_function_decl
+name|__BEGIN_DECLS
 name|long
 name|ulimit
 parameter_list|(
@@ -39,6 +46,10 @@ modifier|...
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_macro
+name|__END_DECLS
+end_macro
 
 begin_endif
 endif|#
