0bd918d6d8692cb7a2b782d3053fe13dc11ef7fa
Peter Grehan <grehan@FreeBSD.org>
2003-01-18 11:25:21 +0000

    Allow the MD frame definition to be seen in. Required for truss/ptrace.
    Approved by:  Benno
    
    
    Former-commit-id: f63a34fc9f29fe9ff38cf4353198bff788a1eadd

--- sys/powerpc/include/signal.h
+++ sys/powerpc/include/signal.h
@@ -46,32 +46,17 @@ name|sig_atomic_t
 typedef|;
 end_typedef
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|_KERNEL
-end_ifdef
+begin_if
+if|#
+directive|if
+name|__BSD_VISIBLE
+end_if
 
 begin_include
 include|#
 directive|include
 file|<machine/frame.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* _KERNEL */
-end_comment
-
-begin_if
-if|#
-directive|if
-name|__BSD_VISIBLE
-end_if
 
 begin_struct
 struct|struct
