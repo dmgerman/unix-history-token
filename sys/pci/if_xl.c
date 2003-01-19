bda609a4dd7bd40ca1ce03c57b5e84a5e177b3da
David E. O'Brien <obrien@FreeBSD.org>
2003-01-19 04:47:54 +0000

    Complete the support of the on-board xl(4) on nVidia nForce2 mobo's.
    Submitted by:	Mikko S. Hyvarinen <morphy@morphy.iki.fi>
    
    
    Former-commit-id: ff3fdbecb9712d515960d6086fd48dce1cb234d5

--- sys/pci/if_xl.c
+++ sys/pci/if_xl.c
@@ -4974,6 +4974,10 @@ case|case
 name|TC_DEVICEID_TORNADO_656C
 case|:
 comment|/* 3c656C */
+case|case
+name|TC_DEVICEID_TORNADO_10_100BT_NVIDIA
+case|:
+comment|/* nVidia nForce2 */
 name|sc
 operator|->
 name|xl_media
@@ -5466,6 +5470,21 @@ operator||=
 name|XL_FLAG_INVERT_MII_PWR
 operator||
 name|XL_FLAG_INVERT_LED_PWR
+expr_stmt|;
+if|if
+condition|(
+name|pci_get_device
+argument_list|(
+name|dev
+argument_list|)
+operator|==
+name|TC_DEVICEID_TORNADO_10_100BT_NVIDIA
+condition|)
+name|sc
+operator|->
+name|xl_flags
+operator||=
+name|XL_FLAG_PHYOK
 expr_stmt|;
 comment|/* 	 * If this is a 3c905B, we have to check one extra thing. 	 * The 905B supports power management and may be placed in 	 * a low-power mode (D3 mode), typically by certain operating 	 * systems which shall not be named. The PCI BIOS is supposed 	 * to reset the NIC and bring it out of low-power mode, but 	 * some do not. Consequently, we have to see if this chip 	 * supports power management, and if so, make sure it's not 	 * in low-power mode. If power management is available, the 	 * capid byte will be 0x01. 	 * 	 * I _think_ that what actually happens is that the chip 	 * loses its PCI configuration during the transition from 	 * D3 back to D0; this means that it should be possible for 	 * us to save the PCI iobase, membase and IRQ, put the chip 	 * back in the D0 state, then restore the PCI config ourselves. 	 */
 if|if
