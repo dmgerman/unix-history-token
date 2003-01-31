4a153c74a6f30cc0368d91ac564ee8b500f71111
Poul-Henning Kamp <phk@FreeBSD.org>
2003-01-31 18:31:48 +0000

    Add Apple partitioning test case.
    Use correct name for image data file
    
    
    Former-commit-id: 6d74d0921d32f246e3f6b5fb6d1899b44451d653

--- tools/regression/geom/geom_sim.h
+++ tools/regression/geom/geom_sim.h
@@ -746,6 +746,15 @@ end_function_decl
 
 begin_function_decl
 name|void
+name|g_apple_init
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
 name|g_bsd_init
 parameter_list|(
 name|void
