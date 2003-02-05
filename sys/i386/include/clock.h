d75639961a35dbbef72eddd9c98c2223804f41dd
Poul-Henning Kamp <phk@FreeBSD.org>
2003-02-05 09:20:40 +0000

    i386/i386/tsc.c was repo-copied from i386/isa/clock.c.
    Remove all the stuff that does not relate to the TSC.
    
    Change the calibration to use DELAY(1000000) rather than trying to check
    it against the CMOS RTC, this drastically increases precision:
    
    Using 25 samples on a Athlon 700MHz UP machine I find:
    
                    stddev          min          max        average
    CMOS             22200 Hz    -74980 Hz     34301 Hz   704928721 Hz
    DELAY             1805 Hz     -1984 Hz      2678 Hz   704937583 Hz
    
    (The difference between the two averages is not statistically significant.)
    
    expressed in PPM of the frequency:
                    stddev          min          max
    CMOS             31.49 PPM  -106.37 PPM    48.66 PPM
    DELAY             2.56 PPM     2.81 PPM     3.80 PPM
    
    This code will not be used until a followup commit to sys/isa/clock.c
    and sys/pc98/pc98/clock.c which will only happen after some field testing.
    
    
    Former-commit-id: 944335cf3882d3b1c756d7265ed4f062494a4d4c

--- sys/i386/include/clock.h
+++ sys/i386/include/clock.h
@@ -240,6 +240,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|init_TSC
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
