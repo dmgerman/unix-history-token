bb251d9b9b00463e3b526f0bc4e2f4e71d6adb16
Benno Rice <benno@FreeBSD.org>
2003-02-05 12:09:53 +0000

    Add cpu.c.  This contains one exported function, cpu_setup(), which handles setup of and printing information about cpus.
    Obtained from:	NetBSD (parts)
    
    
    Former-commit-id: 74c44c767c38215c77875368a9161e07d4701864

--- sys/powerpc/include/md_var.h
+++ sys/powerpc/include/md_var.h
@@ -156,6 +156,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|cpu_setup
+parameter_list|(
+name|u_int
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
