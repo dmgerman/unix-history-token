aabc2df7cc22089affaf763933399a2a6e173cf4
Dag-Erling Smorgrav <des@FreeBSD.org>
2003-02-07 15:27:03 +0000

    Sync with P4.  Most of this is debugging code; the only substantial changes are improvements to openpam_{borrow,restore}_cred() (#24779 and #24780).
    Former-commit-id: d94d8cf95671fedff216f6ec7e352ecb0dd1c210

--- contrib/openpam/lib/openpam_impl.h
+++ contrib/openpam/lib/openpam_impl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2001 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by ThinkSec AS and  * Network Associates Laboratories, the Security Research Division of  * Network Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035  * ("CBOSS"), as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/openpam/lib/openpam_impl.h#19 $  */
+comment|/*-  * Copyright (c) 2001 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by ThinkSec AS and  * Network Associates Laboratories, the Security Research Division of  * Network Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035  * ("CBOSS"), as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/openpam/lib/openpam_impl.h#20 $  */
 end_comment
 
 begin_ifndef
@@ -53,6 +53,18 @@ modifier|*
 name|_pam_err_name
 index|[
 name|PAM_NUM_ERRORS
+index|]
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+specifier|const
+name|char
+modifier|*
+name|_pam_item_name
+index|[
+name|PAM_NUM_ITEMS
 index|]
 decl_stmt|;
 end_decl_stmt
@@ -452,6 +464,36 @@ end_define
 begin_define
 define|#
 directive|define
+name|ENTERI
+parameter_list|(
+name|i
+parameter_list|)
+value|do { \ 	if ((i)> 0&& (i)< PAM_NUM_ITEMS) \ 		openpam_log(PAM_LOG_DEBUG, "entering: %s", _pam_item_name[i]); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "entering: %d", (i)); \ } while (0);
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENTERN
+parameter_list|(
+name|n
+parameter_list|)
+value|do { \ 	openpam_log(PAM_LOG_DEBUG, "entering: %d", (n)); \ } while (0);
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENTERS
+parameter_list|(
+name|s
+parameter_list|)
+value|do { \ 	if ((s) == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "entering: NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "entering: '%s'", (s)); \ } while (0);
+end_define
+
+begin_define
+define|#
+directive|define
 name|RETURNV
 parameter_list|()
 value|openpam_log(PAM_LOG_DEBUG, "returning")
@@ -470,11 +512,11 @@ end_define
 begin_define
 define|#
 directive|define
-name|RETURNI
+name|RETURNN
 parameter_list|(
-name|i
+name|n
 parameter_list|)
-value|do { \ 	openpam_log(PAM_LOG_DEBUG, "returning %d", (i)); \ 	return (i); \ } while (0)
+value|do { \ 	openpam_log(PAM_LOG_DEBUG, "returning %d", (n)); \ 	return (n); \ } while (0)
 end_define
 
 begin_define
@@ -512,6 +554,33 @@ end_define
 begin_define
 define|#
 directive|define
+name|ENTERI
+parameter_list|(
+name|i
+parameter_list|)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENTERN
+parameter_list|(
+name|n
+parameter_list|)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ENTERS
+parameter_list|(
+name|s
+parameter_list|)
+end_define
+
+begin_define
+define|#
+directive|define
 name|RETURNV
 parameter_list|()
 value|return
@@ -530,11 +599,11 @@ end_define
 begin_define
 define|#
 directive|define
-name|RETURNI
+name|RETURNN
 parameter_list|(
-name|i
+name|n
 parameter_list|)
-value|return (i)
+value|return (n)
 end_define
 
 begin_define
