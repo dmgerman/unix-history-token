af1973b69d092d63dd0a10af84b218004557dfe2
Martin Blapp <mbr@FreeBSD.org>
2003-02-12 22:19:34 +0000

    MFC Rev. 1.80, 1.81
    Dynamically configure the width of the srom.  This code comes from
    OpenBSD who got the code (or the idea) from the NetBSD tlp driver.
    
    MFC Rev. 1.67
    
    Fix Conexant chips which always reports carrier lost
    on full duplex mode.
    
    Reviewed by:	phk, imp
    
    
    Former-commit-id: 0f4b270b2ecc5c64dc4b904922921b9573ef5f84

--- sys/pci/if_dcreg.h
+++ sys/pci/if_dcreg.h
@@ -3626,6 +3626,9 @@ name|u_int8_t
 name|dc_cachesize
 decl_stmt|;
 name|int
+name|dc_romwidth
+decl_stmt|;
+name|int
 name|dc_pnic_rx_bug_save
 decl_stmt|;
 name|unsigned
@@ -3646,10 +3649,8 @@ name|u_int32_t
 name|dc_txthresh
 decl_stmt|;
 name|u_int8_t
+modifier|*
 name|dc_srom
-index|[
-literal|1024
-index|]
 decl_stmt|;
 name|struct
 name|dc_mediainfo
