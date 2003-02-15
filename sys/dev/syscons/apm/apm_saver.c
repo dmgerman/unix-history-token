cb2abecf5a637d875277e22c11873b562e2226b0
Ruslan Ermilov <ru@FreeBSD.org>
2003-02-15 21:22:19 +0000

    Added dependency on the apm(4) module.
    Former-commit-id: 53a77caeaa956f3b052c62beb0fb21af7fad028f

--- sys/dev/syscons/apm/apm_saver.c
+++ sys/dev/syscons/apm/apm_saver.c
@@ -268,5 +268,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|apm_saver
+argument_list|,
+name|apm
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
