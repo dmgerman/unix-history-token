56e776876b502893a3e4011a3e0a4efc0db21f79
Poul-Henning Kamp <phk@FreeBSD.org>
2003-02-16 13:23:29 +0000

    Declare tty related counters in a tty related .h file.
    Former-commit-id: 84bdd1ef6ccfdea5a33d2dbd513d9454caa5ffe0

--- sys/sys/tty.h
+++ sys/sys/tty.h
@@ -1190,6 +1190,34 @@ begin_comment
 comment|/* Temporary virtual console. */
 end_comment
 
+begin_decl_stmt
+specifier|extern
+name|long
+name|tk_cancc
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|long
+name|tk_nin
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|long
+name|tk_nout
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|long
+name|tk_rawcc
+decl_stmt|;
+end_decl_stmt
+
 begin_function_decl
 name|int
 name|b_to_q
