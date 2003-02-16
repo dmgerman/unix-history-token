999388e9b98d04c6f7e30ead98360d97a522aec8
Warner Losh <imp@FreeBSD.org>
2003-02-16 18:05:54 +0000

    Whatever dumbass wrote sio_pci.c didn't include a detach method. Wearing said pointy hat, correct the oversight and hope nobody notices.
    # this should make xircom modems happier to detach once other bugs with
    # the cardbus layer are fixed.
    
    Noticed by: scottl
    Conical Hat to: imp
    
    
    Former-commit-id: 4d0a5b16ae82464269eb85df32beb8dbb4797639

--- sys/dev/sio/sio_pci.c
+++ sys/dev/sio/sio_pci.c
@@ -148,6 +148,13 @@ argument_list|,
 name|sio_pci_attach
 argument_list|)
 block|,
+name|DEVMETHOD
+argument_list|(
+name|device_detach
+argument_list|,
+name|siodetach
+argument_list|)
+block|,
 block|{
 literal|0
 block|,
