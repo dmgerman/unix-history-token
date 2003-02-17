c5c085d942ee3d4c6d295c674d7fd598a0733604
Warner Losh <imp@FreeBSD.org>
2003-02-17 19:57:32 +0000

    an driver now needs wlan, so put the required explicit depends into the source.
    Reviewed by: ambrisko
    
    
    Former-commit-id: cc3b61768a293f2dffe00d7f1d819047df649d86

--- sys/dev/an/if_an_pccard.c
+++ sys/dev/an/if_an_pccard.c
@@ -339,6 +339,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|if_an
+argument_list|,
+name|wlan
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_decl_stmt
 specifier|static
 specifier|const
--- sys/dev/an/if_an_pci.c
+++ sys/dev/an/if_an_pci.c
@@ -1185,5 +1185,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|if_an
+argument_list|,
+name|wlan
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
