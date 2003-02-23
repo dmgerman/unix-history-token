c756c96f8f496e18a1cf1ca17a20d827908ffcc6
Sam Leffler <sam@FreeBSD.org>
2003-02-23 17:45:28 +0000

    MFC: IPSEC_FILTERGIF config option
    Add a new config option IPSEC_FILTERGIF to control whether or not
    packets coming out of a GIF tunnel are re-processed by ipfw, et. al.
    By default they are not reprocessed.  With the option they are.
    
    This reverts 1.214.  Prior to that change packets were not re-processed.
    After they were which caused problems because packets do not have
    distinguishing characteristics (like a special network if) that allows
    them to be filtered specially.
    
    PR:		48159
    Reviewed by:	Guido van Rooij <guido@gvr.org>
    Approved by:	re (jhb, murray)
    
    Former-commit-id: 3c8b4c73361c1cc6dfae06e06c29ac490b78f02a

--- sys/netinet/ip_input.c
+++ sys/netinet/ip_input.c
@@ -2052,6 +2052,33 @@ name|len
 argument_list|)
 expr_stmt|;
 block|}
+if|#
+directive|if
+name|defined
+argument_list|(
+name|IPSEC
+argument_list|)
+operator|&&
+operator|!
+name|defined
+argument_list|(
+name|IPSEC_FILTERGIF
+argument_list|)
+comment|/* 	 * Bypass packet filtering for packets from a tunnel (gif). 	 */
+if|if
+condition|(
+name|ipsec_gethist
+argument_list|(
+name|m
+argument_list|,
+name|NULL
+argument_list|)
+condition|)
+goto|goto
+name|pass
+goto|;
+endif|#
+directive|endif
 comment|/* 	 * IpHack's section. 	 * Right now when no processing on packet has done 	 * and it is still fresh out of network we do our black 	 * deals with it. 	 * - Firewall: deny/allow/divert 	 * - Xlate: translate packet's addr/port (NAT). 	 * - Pipe: pass pkt through dummynet. 	 * - Wrap: fake packet's addr/port<unimpl.> 	 * - Encapsulate: put it in another IP and send out.<unimp.>  	 */
 name|iphack
 label|:
