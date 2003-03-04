d7043b681b58e6d157b23d1256c661235e02cee7
John Baldwin <jhb@FreeBSD.org>
2003-03-04 20:24:53 +0000

    Wrap the hyperthreading support code with the HTT kernel option. Hyperthreading support is now off unless the HTT option is added.
    MFC-after:	3 days
    
    Former-commit-id: 3187bf80d5c6d582edd7e694d5717bc6a5c481f4

--- sys/i386/i386/mp_machdep.c
+++ sys/i386/i386/mp_machdep.c
@@ -12,6 +12,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"opt_htt.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"opt_kstack_pages.h"
 end_include
 
@@ -905,6 +911,12 @@ name|MP_ANNOUNCE_POST
 value|0x19
 end_define
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|HTT
+end_ifdef
+
 begin_decl_stmt
 specifier|static
 name|int
@@ -918,6 +930,11 @@ name|u_int
 name|logical_cpus
 decl_stmt|;
 end_decl_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/* used to hold the AP's until we are ready to release them */
@@ -1296,6 +1313,12 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|HTT
+end_ifdef
+
 begin_function_decl
 specifier|static
 name|void
@@ -1306,6 +1329,11 @@ name|id_mask
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_function_decl
 specifier|static
@@ -3357,9 +3385,14 @@ decl_stmt|;
 name|int
 name|type
 decl_stmt|;
+ifdef|#
+directive|ifdef
+name|HTT
 name|u_int
 name|id_mask
 decl_stmt|;
+endif|#
+directive|endif
 name|POSTCODE
 argument_list|(
 name|MPTABLE_PASS1_POST
@@ -3407,10 +3440,15 @@ name|nintrs
 operator|=
 literal|0
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|HTT
 name|id_mask
 operator|=
 literal|0
 expr_stmt|;
+endif|#
+directive|endif
 comment|/* check for use of 'default' configuration */
 if|if
 condition|(
@@ -3574,6 +3612,9 @@ expr_stmt|;
 name|mp_maxid
 operator|++
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|HTT
 name|id_mask
 operator||=
 literal|1
@@ -3587,6 +3628,8 @@ operator|)
 operator|->
 name|apic_id
 expr_stmt|;
+endif|#
+directive|endif
 block|}
 break|break;
 case|case
@@ -3700,12 +3743,17 @@ operator|=
 name|MAXCPU
 expr_stmt|;
 block|}
+ifdef|#
+directive|ifdef
+name|HTT
 comment|/* See if we need to fixup HT logical CPUs. */
 name|mptable_hyperthread_fixup
 argument_list|(
 name|id_mask
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 comment|/* 	 * Count the BSP. 	 * This is also used as a counter while starting the APs. 	 */
 name|mp_ncpus
 operator|=
@@ -3730,10 +3778,15 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
+ifdef|#
+directive|ifdef
+name|HTT
 name|struct
 name|PROCENTRY
 name|proc
 decl_stmt|;
+endif|#
+directive|endif
 name|int
 name|x
 decl_stmt|;
@@ -3775,6 +3828,9 @@ argument_list|(
 name|MPTABLE_PASS2_POST
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|HTT
 comment|/* Initialize fake proc entry for use with HT fixup. */
 name|bzero
 argument_list|(
@@ -3799,6 +3855,8 @@ name|cpu_flags
 operator|=
 name|PROCENTRY_FLAG_EN
 expr_stmt|;
+endif|#
+directive|endif
 name|pgeflag
 operator|=
 literal|0
@@ -4316,6 +4374,9 @@ condition|)
 operator|++
 name|cpu
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|HTT
 if|if
 condition|(
 name|need_hyperthreading_fixup
@@ -4370,6 +4431,8 @@ operator|++
 expr_stmt|;
 block|}
 block|}
+endif|#
+directive|endif
 break|break;
 case|case
 literal|1
@@ -4473,6 +4536,12 @@ literal|0
 return|;
 block|}
 end_function
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|HTT
+end_ifdef
 
 begin_comment
 comment|/*  * Check if we should perform a hyperthreading "fix-up" to  * enumerate any logical CPU's that aren't already listed  * in the table.  *  * XXX: We assume that all of the physical CPUs in the  * system have the same number of logical CPUs.  *  * XXX: We assume that APIC ID's are allocated such that  * the APIC ID's for a physical processor are aligned  * with the number of logical CPU's in the processor.  */
@@ -4605,6 +4674,11 @@ name|logical_cpus
 expr_stmt|;
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_function
 name|void
