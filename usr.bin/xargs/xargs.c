5226f18ff75ced9a3fcbf73d6dfabd378576fb84
Maxime Henrion <mux@FreeBSD.org>
2003-03-23 18:29:52 +0000

    Add a new -o option to tell xargs(1) to reopen /dev/tty as stdin in the child process, before executing the command.  This is very useful when you do stuff like ``find ... | xargs interactive_application''. Without -o, the application would inherit the pipe as its stdin, and you thus lose any control over it.
    This flag has been carefully chosen to not conflit with other options
    of other xargs utilities like GNU xargs.
    
    Reviewed by:	jmallett
    
    
    Former-commit-id: b7abd45c8eb656e2b7db5b38a840242c25eb9847

--- usr.bin/xargs/xargs.c
+++ usr.bin/xargs/xargs.c
@@ -94,6 +94,12 @@ directive|include
 file|<errno.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<fcntl.h>
+end_include
+
 begin_if
 if|#
 directive|if
@@ -343,6 +349,8 @@ name|insingle
 decl_stmt|,
 name|indouble
 decl_stmt|,
+name|oflag
+decl_stmt|,
 name|pflag
 decl_stmt|,
 name|tflag
@@ -538,7 +546,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"0E:I:J:L:n:P:pR:s:tx"
+literal|"0E:I:J:L:n:oP:pR:s:tx"
 argument_list|)
 operator|)
 operator|!=
@@ -631,6 +639,14 @@ literal|1
 argument_list|,
 literal|"illegal argument count"
 argument_list|)
+expr_stmt|;
+break|break;
+case|case
+literal|'o'
+case|:
+name|oflag
+operator|=
+literal|1
 expr_stmt|;
 break|break;
 case|case
@@ -2026,6 +2042,36 @@ expr_stmt|;
 case|case
 literal|0
 case|:
+if|if
+condition|(
+name|oflag
+condition|)
+block|{
+name|close
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|open
+argument_list|(
+literal|"/dev/tty"
+argument_list|,
+name|O_RDONLY
+argument_list|)
+operator|==
+operator|-
+literal|1
+condition|)
+name|err
+argument_list|(
+literal|1
+argument_list|,
+literal|"open"
+argument_list|)
+expr_stmt|;
+block|}
 name|execvp
 argument_list|(
 name|argv
@@ -2380,7 +2426,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: xargs [-0pt] [-E eofstr] [-I replstr [-R replacements]] [-J replstr]\n"
+literal|"usage: xargs [-0opt] [-E eofstr] [-I replstr [-R replacements]] [-J replstr]\n"
 literal|"             [-L number] [-n number [-x]] [-P maxprocs] [-s size]\n"
 literal|"             [utility [argument ...]]\n"
 argument_list|)
