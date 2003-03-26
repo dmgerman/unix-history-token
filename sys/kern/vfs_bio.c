3b0b87b48e0eff0c47f982ce1437d983ecfea493
Tor Egge <tegge@FreeBSD.org>
2003-03-26 23:40:42 +0000

    Add support for reading directly from file to userland buffer when the O_DIRECT descriptor status flag is set and both offset and length is a multiple of the physical media sector size.
    Former-commit-id: c5f5e73a910b7744b35d811241ee6ddc186f0669

--- sys/kern/vfs_bio.c
+++ sys/kern/vfs_bio.c
@@ -157,6 +157,18 @@ directive|include
 file|<vm/vm_map.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|"opt_directio.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"opt_swap.h"
+end_include
+
 begin_expr_stmt
 specifier|static
 name|MALLOC_DEFINE
@@ -1374,6 +1386,31 @@ begin_comment
 comment|/* wait for buf space, lo hysteresis */
 end_comment
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|DIRECTIO
+end_ifdef
+
+begin_function_decl
+specifier|extern
+name|void
+name|ffs_rawread_setup
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* DIRECTIO */
+end_comment
+
 begin_comment
 comment|/*  *	numdirtywakeup:  *  *	If someone is blocked due to there being too many dirty buffers,  *	and numdirtybuffers is now reasonable, wake them up.  */
 end_comment
@@ -1950,6 +1987,29 @@ argument_list|,
 literal|16
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|NSWBUF_MIN
+if|if
+condition|(
+name|nswbuf
+operator|<
+name|NSWBUF_MIN
+condition|)
+name|nswbuf
+operator|=
+name|NSWBUF_MIN
+expr_stmt|;
+endif|#
+directive|endif
+ifdef|#
+directive|ifdef
+name|DIRECTIO
+name|ffs_rawread_setup
+argument_list|()
+expr_stmt|;
+endif|#
+directive|endif
 comment|/* 	 * Reserve space for the buffer cache buffers 	 */
 name|swbuf
 operator|=
