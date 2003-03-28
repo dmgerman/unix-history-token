ebcf4884ba78c44d1a00cbc4874e864b16e95f55
Scott Long <scottl@FreeBSD.org>
2003-03-28 20:05:39 +0000

    Late MFC: Remove AAC_COMPAT_LINUX and replace it with a separate aac_linux module.
    Approved by: re(murray)
    
    
    Former-commit-id: 53e03299a5f2b3b21ef3427a3921bfe2df596309

--- sys/dev/aac/aac_linux.c
+++ sys/dev/aac/aac_linux.c
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2002 Scott Long  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$FreeBSD$  */
-end_comment
-
-begin_comment
-comment|/*  * Linux ioctl handler for the aac device driver  */
+comment|/*-  * Copyright (c) 2000 Michael Smith  * Copyright (c) 2001 Scott Long  * Copyright (c) 2000 BSDi  * Copyright (c) 2001 Adaptec, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$FreeBSD$  */
 end_comment
 
 begin_include
@@ -105,7 +101,7 @@ end_decl_stmt
 begin_expr_stmt
 name|SYSINIT
 argument_list|(
-name|aac_linux_register
+name|aac_register
 argument_list|,
 name|SI_SUB_KLD
 argument_list|,
@@ -122,7 +118,7 @@ end_expr_stmt
 begin_expr_stmt
 name|SYSUNINIT
 argument_list|(
-name|aac_linux_unregister
+name|aac_unregister
 argument_list|,
 name|SI_SUB_KLD
 argument_list|,
@@ -176,7 +172,7 @@ end_expr_stmt
 begin_expr_stmt
 name|MODULE_DEPEND
 argument_list|(
-name|aac_linux
+name|aac
 argument_list|,
 name|linux
 argument_list|,
@@ -195,9 +191,9 @@ name|int
 name|aac_linux_ioctl
 parameter_list|(
 name|struct
-name|thread
+name|proc
 modifier|*
-name|td
+name|p
 parameter_list|,
 name|struct
 name|linux_ioctl_args
@@ -213,34 +209,19 @@ decl_stmt|;
 name|u_long
 name|cmd
 decl_stmt|;
-name|int
-name|error
-decl_stmt|;
-if|if
-condition|(
-operator|(
-name|error
+name|fp
 operator|=
-name|fget
-argument_list|(
-name|td
-argument_list|,
+name|p
+operator|->
+name|p_fd
+operator|->
+name|fd_ofiles
+index|[
 name|args
 operator|->
 name|fd
-argument_list|,
-operator|&
-name|fp
-argument_list|)
-operator|)
-operator|!=
-literal|0
-condition|)
-return|return
-operator|(
-name|error
-operator|)
-return|;
+index|]
+expr_stmt|;
 name|cmd
 operator|=
 name|args
@@ -248,8 +229,7 @@ operator|->
 name|cmd
 expr_stmt|;
 comment|/* 	 * Pass the ioctl off to our standard handler. 	 */
-name|error
-operator|=
+return|return
 operator|(
 name|fo_ioctl
 argument_list|(
@@ -264,24 +244,8 @@ name|args
 operator|->
 name|arg
 argument_list|,
-name|td
-operator|->
-name|td_ucred
-argument_list|,
-name|td
+name|p
 argument_list|)
-operator|)
-expr_stmt|;
-name|fdrop
-argument_list|(
-name|fp
-argument_list|,
-name|td
-argument_list|)
-expr_stmt|;
-return|return
-operator|(
-name|error
 operator|)
 return|;
 block|}
