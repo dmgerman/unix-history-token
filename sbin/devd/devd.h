7a04ee13bd9983c9cd3c478dadaaabf0024cc4ea
Warner Losh <imp@FreeBSD.org>
2003-04-26 20:59:04 +0000

    Create a new C++ header file called devd.hh which has all the class definitions in it.  Begin to document the classes that we use, and how they interrelate (using comments that I can use with doxygen to automatically generate docs with).
    Former-commit-id: fe2753a57211ff101e9950a2ad79482c2a9cb5ae

--- sbin/devd/devd.h
+++ sbin/devd/devd.h
@@ -3,6 +3,22 @@ begin_comment
 comment|/*-  * DEVD (Device action daemon)  *  * Copyright (c) 2002 M. Warner Losh<imp@freebsd.org>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|DEVD_H
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|DEVD_H
+end_define
+
+begin_comment
+comment|/** @warning This file needs to be purely 'C' compatible.  */
+end_comment
+
 begin_struct_decl
 struct_decl|struct
 name|event_proc
@@ -186,6 +202,15 @@ directive|define
 name|DEVCTL_MAXBUF
 value|1025
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* DEVD_H */
+end_comment
 
 end_unit
 
