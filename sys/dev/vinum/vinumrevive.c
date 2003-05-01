3154d01703411c32019b1e558d2af6c9631ef6be
Greg Lehey <grog@FreeBSD.org>
2003-05-01 01:34:05 +0000

    Rewrite minor number decoding.  Now we have only three types of object: subdisks, plexes and volumes.  The encoding for plexes and subdisks no longer reflects the object to which they belong.  The super devices are high-order volume numbers.  This gives vastly more potential volumes (4 million instead of 256).
    Correct formats for some error messages.  Don't cast the value to
    match the format.
    
    
    Former-commit-id: dfabaa58af4449d9f61e2cf80fd64066801ada08

--- sys/dev/vinum/vinumrevive.c
+++ sys/dev/vinum/vinumrevive.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998, 1999  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumrevive.c,v 1.15 2001/05/23 23:04:48 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998, 1999  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Parts copyright (c) 1997, 1998 Cybernet Corporation, NetMAX project.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinumrevive.c,v 1.18 2003/04/28 02:54:43 grog Exp $  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -476,6 +476,7 @@ name|plex_disorg
 case|:
 comment|/* to keep the compiler happy */
 break|break;
+comment|/* to keep the pedants happy */
 block|}
 if|if
 condition|(
@@ -602,17 +603,11 @@ name|bp
 operator|->
 name|b_dev
 operator|=
-name|VINUMDEV
+name|VINUM_VOL
 argument_list|(
 name|plex
 operator|->
 name|volno
-argument_list|,
-literal|0
-argument_list|,
-literal|0
-argument_list|,
-name|VINUM_VOLUME_TYPE
 argument_list|)
 expr_stmt|;
 comment|/* create the device number */
@@ -915,10 +910,6 @@ operator|->
 name|b_dev
 argument_list|)
 argument_list|,
-operator|(
-name|long
-name|long
-operator|)
 name|rq
 operator|->
 name|bp
@@ -1332,10 +1323,6 @@ name|msg
 argument_list|,
 literal|"Parity incorrect at offset 0x%llx\n"
 argument_list|,
-operator|(
-name|long
-name|long
-operator|)
 name|errorloc
 argument_list|)
 expr_stmt|;
