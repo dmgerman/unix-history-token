c5ecca4e1ae9c54e2dab91d57fdcadaba114360d
Greg Lehey <grog@FreeBSD.org>
2003-05-01 01:32:08 +0000

    Use strrchr, not rindex.  This is for compatibility with other systems, and requires a #define.
    Former-commit-id: 7088295f94f5c101e4f720a03c9f73522ea3a2fa

--- sys/dev/vinum/vinummemory.c
+++ sys/dev/vinum/vinummemory.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinummemory.c,v 1.28 2001/05/23 23:04:06 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinummemory.c,v 1.30 2003/04/28 02:54:43 grog Exp $  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -14,16 +14,6 @@ ifdef|#
 directive|ifdef
 name|VINUMDEBUG
 end_ifdef
-
-begin_undef
-undef|#
-directive|undef
-name|longjmp
-end_undef
-
-begin_comment
-comment|/* this was defined as LongJmp */
-end_comment
 
 begin_include
 include|#
@@ -60,6 +50,23 @@ end_decl_stmt
 begin_comment
 comment|/* for debugger */
 end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|longjmp
+end_undef
+
+begin_comment
+comment|/* this was defined as LongJmp */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|strrchr
+value|rindex
+end_define
 
 begin_ifdef
 ifdef|#
@@ -164,6 +171,10 @@ else|#
 directive|else
 end_else
 
+begin_comment
+comment|/* not i386 */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -180,10 +191,18 @@ endif|#
 directive|endif
 end_endif
 
+begin_comment
+comment|/* i386 */
+end_comment
+
 begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* VINUMDEBUG */
+end_comment
 
 begin_comment
 comment|/* find the base name of a path name */
@@ -203,7 +222,7 @@ name|char
 modifier|*
 name|f
 init|=
-name|rindex
+name|strrchr
 argument_list|(
 name|file
 argument_list|,
@@ -822,7 +841,7 @@ name|char
 modifier|*
 name|f
 init|=
-name|rindex
+name|strrchr
 argument_list|(
 name|file
 argument_list|,
