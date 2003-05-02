987bf8a33e0200b9066ebcc749ab90db4a7c1b37
David E. O'Brien <obrien@FreeBSD.org>
2003-05-02 06:49:10 +0000

    Use __FBSDID.
    Former-commit-id: f4558289ec1bb4bfb39f3e8b0057d9fbfd6902f2

--- sbin/camcontrol/util.c
+++ sbin/camcontrol/util.c
@@ -7,31 +7,19 @@ begin_comment
 comment|/*  * Taken from the original scsi(8) program.  * from: scsi.c,v 1.17 1998/01/12 07:57:57 charnier Exp $";  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
