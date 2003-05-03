91e1a46f69292bb8f7f6215c00cbd6320bcac2ac
David E. O'Brien <obrien@FreeBSD.org>
2003-05-03 19:09:11 +0000

    Don't shaddow exp(3).
    Former-commit-id: 6b13e58735678a39c29f300b573360f4a3dc4585

--- usr.bin/xargs/xargs.c
+++ usr.bin/xargs/xargs.c
@@ -3,24 +3,20 @@ begin_comment
 comment|/*-  * Copyright (c) 1990, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * John B. Roll Jr.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $xMach: xargs.c,v 1.6 2002/02/23 05:27:47 tim Exp $  */
 end_comment
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
 begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|copyright
-index|[]
-init|=
-literal|"@(#) Copyright (c) 1990, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
+unit|static const char copyright[] = "@(#) Copyright (c) 1990, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n";
 endif|#
 directive|endif
 end_endif
@@ -28,12 +24,6 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
-
-begin_if
-if|#
-directive|if
-literal|0
-end_if
 
 begin_ifndef
 ifndef|#
@@ -276,7 +266,7 @@ name|ep
 decl_stmt|,
 modifier|*
 modifier|*
-name|exp
+name|endxp
 decl_stmt|,
 modifier|*
 modifier|*
@@ -937,7 +927,7 @@ condition|)
 do|;
 block|}
 comment|/* 	 * Set up begin/end/traversing pointers into the array.  The -n 	 * count doesn't include the trailing NULL pointer, so the malloc 	 * added in an extra slot. 	 */
-name|exp
+name|endxp
 operator|=
 operator|(
 name|xp
@@ -1294,7 +1284,7 @@ if|if
 condition|(
 name|xp
 operator|==
-name|exp
+name|endxp
 operator|||
 name|p
 operator|>
@@ -1321,7 +1311,7 @@ name|xflag
 operator|&&
 name|xp
 operator|!=
-name|exp
+name|endxp
 operator|&&
 name|p
 operator|>
