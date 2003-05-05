e8537393a914d15ae3a3f6bcfbe92fae99f5e1fd
Greg Lehey <grog@FreeBSD.org>
2003-05-05 05:28:15 +0000

    free_vinum: Rearrange sequence of actions to avoid potential race condition when shutting down.
    Former-commit-id: 53bd3a3de3dda6fc5714613f7e5f9485103d1cef

--- sys/dev/vinum/vinum.c
+++ sys/dev/vinum/vinum.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinum.c,v 1.41 2003/04/28 06:15:36 grog Exp grog $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1997, 1998  *	Nan Yang Computer Services Limited.  All rights reserved.  *  *  Written by Greg Lehey  *  *  This software is distributed under the so-called ``Berkeley  *  License'':  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by Nan Yang Computer  *      Services Limited.  * 4. Neither the name of the Company nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * This software is provided ``as is'', and any express or implied  * warranties, including, but not limited to, the implied warranties of  * merchantability and fitness for a particular purpose are disclaimed.  * In no event shall the company or contributors be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability, whether  * in contract, strict liability, or tort (including negligence or  * otherwise) arising in any way out of the use of this software, even if  * advised of the possibility of such damage.  *  * $Id: vinum.c,v 1.42 2003/05/04 05:25:14 grog Exp grog $  * $FreeBSD$  */
 end_comment
 
 begin_define
@@ -750,13 +750,54 @@ name|vinum_conf
 operator|.
 name|drives_allocated
 decl_stmt|;
-name|EVENTHANDLER_DEREGISTER
+while|while
+condition|(
+operator|(
+name|vinum_conf
+operator|.
+name|flags
+operator|&
+operator|(
+name|VF_STOPPING
+operator||
+name|VF_DAEMONOPEN
+operator|)
+operator|)
+operator|==
+operator|(
+name|VF_STOPPING
+operator||
+name|VF_DAEMONOPEN
+operator|)
+condition|)
+block|{
+comment|/* at least one daemon open, we're stopping */
+name|queue_daemon_request
 argument_list|(
-name|dev_clone
+name|daemonrq_return
 argument_list|,
-name|dev_clone_tag
+operator|(
+expr|union
+name|daemoninfo
+operator|)
+literal|0
 argument_list|)
 expr_stmt|;
+comment|/* stop the daemon */
+name|tsleep
+argument_list|(
+operator|&
+name|vinumclose
+argument_list|,
+name|PUSER
+argument_list|,
+literal|"vstop"
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+comment|/* and wait for it */
+block|}
 if|if
 condition|(
 name|DRIVE
@@ -822,54 +863,6 @@ argument_list|(
 name|DRIVE
 argument_list|)
 expr_stmt|;
-block|}
-while|while
-condition|(
-operator|(
-name|vinum_conf
-operator|.
-name|flags
-operator|&
-operator|(
-name|VF_STOPPING
-operator||
-name|VF_DAEMONOPEN
-operator|)
-operator|)
-operator|==
-operator|(
-name|VF_STOPPING
-operator||
-name|VF_DAEMONOPEN
-operator|)
-condition|)
-block|{
-comment|/* at least one daemon open, we're stopping */
-name|queue_daemon_request
-argument_list|(
-name|daemonrq_return
-argument_list|,
-operator|(
-expr|union
-name|daemoninfo
-operator|)
-literal|0
-argument_list|)
-expr_stmt|;
-comment|/* stop the daemon */
-name|tsleep
-argument_list|(
-operator|&
-name|vinumclose
-argument_list|,
-name|PUSER
-argument_list|,
-literal|"vstop"
-argument_list|,
-literal|1
-argument_list|)
-expr_stmt|;
-comment|/* and wait for it */
 block|}
 if|if
 condition|(
@@ -1052,6 +1045,13 @@ operator|=
 name|VINUMVERSION
 expr_stmt|;
 comment|/* reinstate version number */
+name|EVENTHANDLER_DEREGISTER
+argument_list|(
+name|dev_clone
+argument_list|,
+name|dev_clone_tag
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
