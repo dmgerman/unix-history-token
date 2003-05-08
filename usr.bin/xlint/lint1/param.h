6f3e72e560b1921981c5515e86a6552815d10ba8
Peter Wemm <peter@FreeBSD.org>
2003-05-08 06:33:49 +0000

    Add amd64 settings.
    Approved by:  re (scottl)
    
    
    Former-commit-id: f797f67c0bf637507cb67bb2827ab0109c0e66b5

--- usr.bin/xlint/lint1/param.h
+++ usr.bin/xlint/lint1/param.h
@@ -81,6 +81,26 @@ end_define
 begin_elif
 elif|#
 directive|elif
+name|__amd64__
+end_elif
+
+begin_define
+define|#
+directive|define
+name|PTRDIFF_IS_LONG
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|SIZEOF_IS_ULONG
+value|1
+end_define
+
+begin_elif
+elif|#
+directive|elif
 name|__alpha__
 end_elif
 
