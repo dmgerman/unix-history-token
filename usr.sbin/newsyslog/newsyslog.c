a9a2fb30e3a57fd85293eecabd37f6474327f799
Garance A. Drosehn <gad@FreeBSD.org>
2003-05-12 23:41:29 +0000

    MFC 1.68:  Use __FBSDID over rcsid[].
    Former-commit-id: b18c28a2e0c3260b49612e54b7100f88cbd46fe7

--- usr.sbin/newsyslog/newsyslog.c
+++ usr.sbin/newsyslog/newsyslog.c
@@ -11,31 +11,19 @@ begin_comment
 comment|/*  * newsyslog - roll over selected logs at the appropriate time, keeping the a  * specified number of backup files around.  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* not lint */
-end_comment
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_define
 define|#
