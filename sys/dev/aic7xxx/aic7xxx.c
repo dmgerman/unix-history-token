017489449477f7c460030ff10346206866b93519
Justin T. Gibbs <gibbs@FreeBSD.org>
2003-05-26 21:20:47 +0000

    Fix disabling of PCI parity error interrupts.  We need to set FAILDIS in the SEQCTL register, not the HCNTRL register.
    aic7xxx.c:
    	Remeber SEQCTL settings in the "seqctl" field of our
    	softc.  seqctl defaults to just having FASTMODE set,
    	but the bus attachments can override this.
    
    aic7xxx.h:
    	Add the seqctl softc field.
    
    aic7xxx_pci.c:
    	Update the seqctl softc field and manually update SEQCTL
    	when to many PCI errors occur
    
    Approved by: RE
    
    
    Former-commit-id: b118812ba155bff256927ce7bba7a4d22a9a2249

--- sys/dev/aic7xxx/aic7xxx.c
+++ sys/dev/aic7xxx/aic7xxx.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Core routines and tables shareable across OS platforms.  *  * Copyright (c) 1994-2002 Justin T. Gibbs.  * Copyright (c) 2000-2002 Adaptec Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification.  * 2. Redistributions in binary form must reproduce at minimum a disclaimer  *    substantially similar to the "NO WARRANTY" disclaimer below  *    ("Disclaimer") and any redistribution must be conditioned upon  *    including a substantially similar Disclaimer requirement for further  *    binary redistribution.  * 3. Neither the names of the above-listed copyright holders nor the names  *    of any contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * Alternatively, this software may be distributed under the terms of the  * GNU General Public License ("GPL") version 2 as published by the Free  * Software Foundation.  *  * NO WARRANTY  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGES.  *  * $Id: //depot/aic7xxx/aic7xxx/aic7xxx.c#128 $  *  * $FreeBSD$  */
+comment|/*  * Core routines and tables shareable across OS platforms.  *  * Copyright (c) 1994-2002 Justin T. Gibbs.  * Copyright (c) 2000-2002 Adaptec Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification.  * 2. Redistributions in binary form must reproduce at minimum a disclaimer  *    substantially similar to the "NO WARRANTY" disclaimer below  *    ("Disclaimer") and any redistribution must be conditioned upon  *    including a substantially similar Disclaimer requirement for further  *    binary redistribution.  * 3. Neither the names of the above-listed copyright holders nor the names  *    of any contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * Alternatively, this software may be distributed under the terms of the  * GNU General Public License ("GPL") version 2 as published by the Free  * Software Foundation.  *  * NO WARRANTY  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGES.  *  * $Id: //depot/aic7xxx/aic7xxx/aic7xxx.c#129 $  *  * $FreeBSD$  */
 end_comment
 
 begin_ifdef
@@ -1522,7 +1522,9 @@ name|ahc
 argument_list|,
 name|SEQCTL
 argument_list|,
-name|FASTMODE
+name|ahc
+operator|->
+name|seqctl
 argument_list|)
 expr_stmt|;
 name|ahc_outb
@@ -6394,12 +6396,9 @@ name|ahc
 argument_list|,
 name|SEQCTL
 argument_list|,
-name|ahc_inb
-argument_list|(
 name|ahc
-argument_list|,
-name|SEQCTL
-argument_list|)
+operator|->
+name|seqctl
 operator||
 name|STEP
 argument_list|)
@@ -6495,15 +6494,9 @@ name|ahc
 argument_list|,
 name|SEQCTL
 argument_list|,
-name|ahc_inb
-argument_list|(
 name|ahc
-argument_list|,
-name|SEQCTL
-argument_list|)
-operator|&
-operator|~
-name|STEP
+operator|->
+name|seqctl
 argument_list|)
 expr_stmt|;
 block|}
@@ -16683,6 +16676,13 @@ operator|->
 name|flags
 operator|=
 name|AHC_FNONE
+expr_stmt|;
+comment|/* 	 * Default to all error reporting enabled with the 	 * sequencer operating at its fastest speed. 	 * The bus attach code may modify this. 	 */
+name|ahc
+operator|->
+name|seqctl
+operator|=
+name|FASTMODE
 expr_stmt|;
 for|for
 control|(
