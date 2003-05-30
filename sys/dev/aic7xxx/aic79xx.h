72a9d5f06d1a9d591792da586a46342dcb95e412
Scott Long <scottl@FreeBSD.org>
2003-05-30 02:14:22 +0000

    Fix a reported case of severe data corruption:
    aic79xx.h:
    aic79xx.reg:
    	Return the SCB_TAG field to 16byte alignment.
    	It seems that on some PCI systems, SCBs are not
    	transferred correctly to the controller with
    	the previous placement of the SCB_TAG field.
    
    Approved by:	re (rwatson)
    
    
    Former-commit-id: f18af63e0635b6cebfc1c022b637bbc8cd2c1777

--- sys/dev/aic7xxx/aic79xx.h
+++ sys/dev/aic7xxx/aic79xx.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Core definitions and data structures shareable across OS platforms.  *  * Copyright (c) 1994-2002 Justin T. Gibbs.  * Copyright (c) 2000-2002 Adaptec Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification.  * 2. Redistributions in binary form must reproduce at minimum a disclaimer  *    substantially similar to the "NO WARRANTY" disclaimer below  *    ("Disclaimer") and any redistribution must be conditioned upon  *    including a substantially similar Disclaimer requirement for further  *    binary redistribution.  * 3. Neither the names of the above-listed copyright holders nor the names  *    of any contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * Alternatively, this software may be distributed under the terms of the  * GNU General Public License ("GPL") version 2 as published by the Free  * Software Foundation.  *  * NO WARRANTY  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGES.  *  * $Id: //depot/aic7xxx/aic7xxx/aic79xx.h#90 $  *  * $FreeBSD$  */
+comment|/*  * Core definitions and data structures shareable across OS platforms.  *  * Copyright (c) 1994-2002 Justin T. Gibbs.  * Copyright (c) 2000-2002 Adaptec Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification.  * 2. Redistributions in binary form must reproduce at minimum a disclaimer  *    substantially similar to the "NO WARRANTY" disclaimer below  *    ("Disclaimer") and any redistribution must be conditioned upon  *    including a substantially similar Disclaimer requirement for further  *    binary redistribution.  * 3. Neither the names of the above-listed copyright holders nor the names  *    of any contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * Alternatively, this software may be distributed under the terms of the  * GNU General Public License ("GPL") version 2 as published by the Free  * Software Foundation.  *  * NO WARRANTY  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGES.  *  * $Id: aic79xx.h,v 1.11 2003/05/26 21:10:58 gibbs Exp $  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -1205,57 +1205,57 @@ directive|define
 name|SG_PTR_MASK
 value|0xFFFFFFF8
 comment|/*16*/
-name|uint64_t
-name|dataptr
-decl_stmt|;
-comment|/*24*/
-name|uint32_t
-name|datacnt
-decl_stmt|;
-comment|/* Byte 3 is spare. */
-comment|/*28*/
-name|uint32_t
-name|sgptr
-decl_stmt|;
-comment|/*32*/
-name|uint32_t
-name|hscb_busaddr
-decl_stmt|;
-comment|/*36*/
-name|uint32_t
-name|next_hscb_busaddr
-decl_stmt|;
-comment|/*40*/
-name|uint8_t
-name|control
-decl_stmt|;
-comment|/* See SCB_CONTROL in aic79xx.reg for details */
-comment|/*41*/
-name|uint8_t
-name|scsiid
-decl_stmt|;
-comment|/* 				 * Selection out Id 				 * Our Id (bits 0-3) Their ID (bits 4-7) 				 */
-comment|/*42*/
-name|uint8_t
-name|lun
-decl_stmt|;
-comment|/*43*/
-name|uint8_t
-name|task_attribute
-decl_stmt|;
-comment|/*44*/
-name|uint8_t
-name|cdb_len
-decl_stmt|;
-comment|/*45*/
-name|uint8_t
-name|task_management
-decl_stmt|;
-comment|/*46*/
 name|uint16_t
 name|tag
 decl_stmt|;
 comment|/* Reused by Sequencer. */
+comment|/*18*/
+name|uint8_t
+name|control
+decl_stmt|;
+comment|/* See SCB_CONTROL in aic79xx.reg for details */
+comment|/*19*/
+name|uint8_t
+name|scsiid
+decl_stmt|;
+comment|/* 				 * Selection out Id 				 * Our Id (bits 0-3) Their ID (bits 4-7) 				 */
+comment|/*20*/
+name|uint8_t
+name|lun
+decl_stmt|;
+comment|/*21*/
+name|uint8_t
+name|task_attribute
+decl_stmt|;
+comment|/*22*/
+name|uint8_t
+name|cdb_len
+decl_stmt|;
+comment|/*23*/
+name|uint8_t
+name|task_management
+decl_stmt|;
+comment|/*24*/
+name|uint64_t
+name|dataptr
+decl_stmt|;
+comment|/*32*/
+name|uint32_t
+name|datacnt
+decl_stmt|;
+comment|/* Byte 3 is spare. */
+comment|/*36*/
+name|uint32_t
+name|sgptr
+decl_stmt|;
+comment|/*40*/
+name|uint32_t
+name|hscb_busaddr
+decl_stmt|;
+comment|/*44*/
+name|uint32_t
+name|next_hscb_busaddr
+decl_stmt|;
 comment|/********** Long lun field only downloaded for full 8 byte lun support ********/
 comment|/*48*/
 name|uint8_t
