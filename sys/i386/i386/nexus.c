207c230605330b028f6d156a88ba0d0bd096a223
Poul-Henning Kamp <phk@FreeBSD.org>
2003-05-31 19:03:20 +0000

    Remove unused variable.
    Found by:       FlexeLint
    
    
    Former-commit-id: 5ae9adc45807239b74534f58902aed086ff89ac4

--- sys/i386/i386/nexus.c
+++ sys/i386/i386/nexus.c
@@ -1970,10 +1970,6 @@ modifier|*
 name|cookiep
 parameter_list|)
 block|{
-name|driver_t
-modifier|*
-name|driver
-decl_stmt|;
 name|int
 name|error
 decl_stmt|;
@@ -2009,13 +2005,6 @@ condition|)
 name|flags
 operator||=
 name|INTR_EXCL
-expr_stmt|;
-name|driver
-operator|=
-name|device_get_driver
-argument_list|(
-name|child
-argument_list|)
 expr_stmt|;
 comment|/* 	 * We depend here on rman_activate_resource() being idempotent. 	 */
 name|error
