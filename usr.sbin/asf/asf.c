a32eb1f7869238e3d8dfab88e651f121101b1517
Greg Lehey <grog@FreeBSD.org>
2003-06-08 07:29:57 +0000

    Add licence.
    Complained about by:	juli
    
    Correct usage() text.
    
    
    Former-commit-id: 00f920ea5eff293e982c59a3b5c9e4ff147cd51a

--- usr.sbin/asf/asf.c
+++ usr.sbin/asf/asf.c
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/*  * Copyright (c) 2002, 2003 Greg Lehey  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * This software is provided by the author ``as is'' and any express  * or implied warranties, including, but not limited to, the implied  * warranties of merchantability and fitness for a particular purpose  * are disclaimed.  In no event shall the author be liable for any  * direct, indirect, incidental, special, exemplary, or consequential  * damages (including, but not limited to, procurement of substitute  * goods or services; loss of use, data, or profits; or business  * interruption) however caused and on any theory of liability,  * whether in contract, strict liability, or tort (including  * negligence or otherwise) arising in any way out of the use of this  * software, even if advised of the possibility of such damage.  */
+end_comment
+
+begin_comment
 comment|/* $Id: asf.c,v 1.4 2003/05/04 02:55:20 grog Exp grog $ */
 end_comment
 
@@ -392,7 +396,7 @@ argument_list|(
 name|stderr
 argument_list|,
 literal|"Usage:\n"
-literal|"%s [-a] [-k] [modules-path [outfile]]\n\n"
+literal|"%s [-a] [-k] [-t] [modules-path [outfile]]\n\n"
 literal|"\t-a\tappend to outfile)\n"
 literal|"\t-k\ttake input from kldstat(8)\n"
 argument_list|,
