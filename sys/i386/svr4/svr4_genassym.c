514a6874f57a9c93a780f0b850fef1ebb7222405
David E. O'Brien <obrien@FreeBSD.org>
2003-06-10 05:05:54 +0000

    Use __FBSDID().
    Former-commit-id: ede0f94804f4482c253c1b96418dcffc92851942

--- sys/i386/svr4/svr4_genassym.c
+++ sys/i386/svr4/svr4_genassym.c
@@ -1,11 +1,21 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $FreeBSD$ */
-end_comment
-
-begin_comment
 comment|/* Derived from:  Id: linux_genassym.c,v 1.8 1998/07/29 15:50:41 bde Exp */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
