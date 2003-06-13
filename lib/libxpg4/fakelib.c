cdd8132e5d670bc7018e483bd9b805ee6341db2e
Tim J. Robbins <tjr@FreeBSD.org>
2003-06-13 07:21:37 +0000

    Mark libxpg4 as OBSOLETE_IN_6: it has been empty since its contents were merged into libc in 4.5-RELEASE.
    Former-commit-id: 9247268df588fdb64138183f2bcce3f9acfef7a5

--- lib/libxpg4/fakelib.c
+++ lib/libxpg4/fakelib.c
@@ -3,6 +3,22 @@ begin_comment
 comment|/* $FreeBSD$ */
 end_comment
 
+begin_comment
+comment|/* libxpg4 is obsolete in FreeBSD 6 -- library has been merged into libc. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|OBSOLETE_IN_6
+end_define
+
+begin_include
+include|#
+directive|include
+file|<sys/param.h>
+end_include
+
 begin_decl_stmt
 specifier|static
 name|int
