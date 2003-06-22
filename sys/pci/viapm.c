033dfcb6e33c0be300fb876a622576c2e58a3c84
Matthew N. Dodd <mdodd@FreeBSD.org>
2003-06-22 06:50:02 +0000

    Add ID for VT8233A.
    PR:		 i386/38299
    Submitted by:	 Rob Schulhof <rrs@there.net>
    
    Former-commit-id: 3524f9fed7a20d16956094aae0f1ef70093439eb

--- sys/pci/viapm.c
+++ sys/pci/viapm.c
@@ -223,6 +223,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|VIA_8233A_PMU_ID
+value|0x31471106
+end_define
+
+begin_define
+define|#
+directive|define
 name|VIAPM_INB
 parameter_list|(
 name|port
@@ -1093,6 +1100,9 @@ name|viapro
 goto|;
 case|case
 name|VIA_8233_PMU_ID
+case|:
+case|case
+name|VIA_8233A_PMU_ID
 case|:
 name|desc
 operator|=
