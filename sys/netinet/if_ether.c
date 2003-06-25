a6d209b8a4a96acc75f1029a7259307d83370227
Robert N. M. Watson <rwatson@FreeBSD.org>
2003-06-25 16:56:05 +0000

    MFC net.link.ether.inet.log_arp_movements sysctl to disable gratuitous log entries when a non-permanent ethernet address of a host changes.
    Requested by:	Georgi Hristov <hristov@iocc.com>
    
    
    Former-commit-id: 80042d39c0820e8c620d242669ddc44ed0270b49

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -2857,6 +2857,15 @@ literal|1
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|static
+name|int
+name|log_arp_movements
+init|=
+literal|1
+decl_stmt|;
+end_decl_stmt
+
 begin_expr_stmt
 name|SYSCTL_INT
 argument_list|(
@@ -2874,6 +2883,27 @@ argument_list|,
 literal|0
 argument_list|,
 literal|"log arp packets arriving on the wrong interface"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_net_link_ether_inet
+argument_list|,
+name|OID_AUTO
+argument_list|,
+name|log_arp_movements
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|log_arp_movements
+argument_list|,
+literal|0
+argument_list|,
+literal|"log arp replies from MACs different than the one in the cache"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -3507,6 +3537,11 @@ name|rt
 operator|->
 name|rt_expire
 condition|)
+block|{
+if|if
+condition|(
+name|log_arp_movements
+condition|)
 name|log
 argument_list|(
 name|LOG_INFO
@@ -3557,6 +3592,7 @@ operator|->
 name|if_unit
 argument_list|)
 expr_stmt|;
+block|}
 else|else
 block|{
 name|log
