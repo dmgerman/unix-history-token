84f89ae8cb28f2de0e5c4a7c42e8cccc973fad61
David E. O'Brien <obrien@FreeBSD.org>
2003-06-25 19:06:40 +0000

    Push the alloca #error warning farther down to play nicer with some out of tree local translator.
    Requested by:	 jmallett
    
    
    Former-commit-id: 837ce066ea59a721ee801645d6e4cadfba563f2a

--- include/stdlib.h
+++ include/stdlib.h
@@ -1344,17 +1344,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_else
-else|#
-directive|else
-end_else
-
-begin_error
-error|#
-directive|error
-error|Please add alloca support on all FreeBSD architectures for this compiler.
-end_error
-
 begin_endif
 endif|#
 directive|endif
