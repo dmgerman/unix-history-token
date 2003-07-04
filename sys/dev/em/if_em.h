2b7769f2d74584341d4e0c70c2a8a82c61d5d1c3
Maxime Henrion <mux@FreeBSD.org>
2003-07-04 10:15:16 +0000

    The em(4) driver has been converted to busdma and doesn't use vtophys() anymore, so remove the alpha hack which defines vtophys() to alpha_XXX_dmamap().
    Former-commit-id: 55f64152c000f0d682eb8ccd530df5a89488f044

--- sys/dev/em/if_em.h
+++ sys/dev/em/if_em.h
@@ -631,37 +631,6 @@ name|EM_MAX_SCATTER
 value|64
 end_define
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__alpha__
-end_ifdef
-
-begin_undef
-undef|#
-directive|undef
-name|vtophys
-end_undef
-
-begin_define
-define|#
-directive|define
-name|vtophys
-parameter_list|(
-name|va
-parameter_list|)
-value|alpha_XXX_dmamap((vm_offset_t)(va))
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* __alpha__ */
-end_comment
-
 begin_comment
 comment|/* ******************************************************************************  * vendor_info_array  *  * This array contains the list of Subvendor/Subdevice IDs on which the driver  * should load.  *  * ******************************************************************************/
 end_comment
