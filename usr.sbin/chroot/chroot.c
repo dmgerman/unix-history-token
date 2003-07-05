e648e4116d6596861e15523003b1912bf763e73b
Mike Makonnen <mtm@FreeBSD.org>
2003-07-05 09:25:17 +0000

    Reduce diffs to HEAD MFC: rev. 1.5 Fix the type of the NULL arg to execl() MFC: rev. 1.7 de-__P() MFC: rev. 1.8 Use __FBSDID over rcsid[].  Protect copyright[] where needed. MFC: rev. 1.9 Explicitly compare the values returned by chdir(2) and 	      chroot(2) to -1.
    Former-commit-id: 2cf66aba04550c69adf1145f43cd54fc7b48a11e

--- usr.sbin/chroot/chroot.c
+++ usr.sbin/chroot/chroot.c
@@ -3,24 +3,20 @@ begin_comment
 comment|/*  * Copyright (c) 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
 begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|copyright
-index|[]
-init|=
-literal|"@(#) Copyright (c) 1988, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
+unit|static const char copyright[] = "@(#) Copyright (c) 1988, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n";
 endif|#
 directive|endif
 end_endif
@@ -34,12 +30,6 @@ ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
-
-begin_if
-if|#
-directive|if
-literal|0
-end_if
 
 begin_endif
 unit|static char sccsid[] = "@(#)chroot.c	8.1 (Berkeley) 6/9/93";
@@ -47,25 +37,28 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
+begin_comment
+comment|/* not lint */
+end_comment
 
 begin_endif
 endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* not lint */
-end_comment
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -109,18 +102,15 @@ directive|include
 file|<unistd.h>
 end_include
 
-begin_decl_stmt
+begin_function_decl
 specifier|static
 name|void
 name|usage
-name|__P
-argument_list|(
-operator|(
+parameter_list|(
 name|void
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_function
 name|int
@@ -204,11 +194,17 @@ index|[
 literal|0
 index|]
 argument_list|)
+operator|==
+operator|-
+literal|1
 operator|||
 name|chroot
 argument_list|(
 literal|"."
 argument_list|)
+operator|==
+operator|-
+literal|1
 condition|)
 name|err
 argument_list|(
@@ -281,6 +277,10 @@ name|shell
 argument_list|,
 literal|"-i"
 argument_list|,
+operator|(
+name|char
+operator|*
+operator|)
 name|NULL
 argument_list|)
 expr_stmt|;
