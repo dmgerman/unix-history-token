d86d76ebb0f0c006bb3b28d85ec149c468131fad
Yar Tikhiy <yar@FreeBSD.org>
2003-07-09 12:46:24 +0000

    Make a malloced copy of "chrootdir" even if it points to an absolute pathname inside "residue" so "chrootdir" can be simply freed later.
    PR:		bin/53435
    Submitted by:	Yutaka Ishihara <yutaka at fandc.co.jp>
    MFC after:	1 week
    
    Former-commit-id: e7e87187aeb92ec88568007c13b21ee9da8641ad

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -7217,7 +7217,10 @@ argument_list|)
 operator|)
 operator|!=
 name|NULL
-operator|&&
+condition|)
+block|{
+if|if
+condition|(
 name|chrootdir
 index|[
 literal|0
@@ -7225,7 +7228,6 @@ index|]
 operator|!=
 literal|'/'
 condition|)
-block|{
 name|asprintf
 argument_list|(
 operator|&
@@ -7240,6 +7242,15 @@ argument_list|,
 name|chrootdir
 argument_list|)
 expr_stmt|;
+else|else
+name|chrootdir
+operator|=
+name|strdup
+argument_list|(
+name|chrootdir
+argument_list|)
+expr_stmt|;
+comment|/* so it can be freed */
 if|if
 condition|(
 name|chrootdir
