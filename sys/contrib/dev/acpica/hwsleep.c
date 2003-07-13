c53d3f868d3f44be906b8bb308e3ed9226096d46
Nate Lawson <njl@FreeBSD.org>
2003-07-13 22:47:01 +0000

    Commit local patches for files off the vendor branch.
    Former-commit-id: 2d17f016f173e3301da70564d5da309d724ec8f2

--- sys/contrib/dev/acpica/hwsleep.c
+++ sys/contrib/dev/acpica/hwsleep.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************  *  * Name: hwsleep.c - ACPI Hardware Sleep/Wake Interface  *              $Revision: 52 $  *  *****************************************************************************/
+comment|/******************************************************************************  *  * Name: hwsleep.c - ACPI Hardware Sleep/Wake Interface  *              $Revision: 56 $  *  *****************************************************************************/
 end_comment
 
 begin_comment
@@ -418,7 +418,7 @@ name|ACPI_BITREG_WAKE_STATUS
 argument_list|,
 literal|1
 argument_list|,
-name|ACPI_MTX_LOCK
+name|ACPI_MTX_DO_NOT_LOCK
 argument_list|)
 expr_stmt|;
 if|if
@@ -438,7 +438,9 @@ block|}
 name|Status
 operator|=
 name|AcpiHwClearAcpiStatus
-argument_list|()
+argument_list|(
+name|ACPI_MTX_DO_NOT_LOCK
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
@@ -463,7 +465,7 @@ name|ACPI_BITREG_ARB_DISABLE
 argument_list|,
 literal|1
 argument_list|,
-name|ACPI_MTX_LOCK
+name|ACPI_MTX_DO_NOT_LOCK
 argument_list|)
 expr_stmt|;
 if|if
@@ -789,31 +791,6 @@ operator|!
 name|InValue
 condition|)
 do|;
-name|Status
-operator|=
-name|AcpiSetRegister
-argument_list|(
-name|ACPI_BITREG_ARB_DISABLE
-argument_list|,
-literal|0
-argument_list|,
-name|ACPI_MTX_DO_NOT_LOCK
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|ACPI_FAILURE
-argument_list|(
-name|Status
-argument_list|)
-condition|)
-block|{
-name|return_ACPI_STATUS
-argument_list|(
-name|Status
-argument_list|)
-expr_stmt|;
-block|}
 name|return_ACPI_STATUS
 argument_list|(
 name|AE_OK
@@ -854,7 +831,9 @@ name|ACPI_MTX_DO_NOT_LOCK
 argument_list|)
 expr_stmt|;
 name|AcpiHwClearAcpiStatus
-argument_list|()
+argument_list|(
+name|ACPI_MTX_DO_NOT_LOCK
+argument_list|)
 expr_stmt|;
 name|AcpiHwDisableNonWakeupGpes
 argument_list|()
@@ -871,7 +850,7 @@ operator|->
 name|SmiCmd
 argument_list|,
 operator|(
-name|ACPI_INTEGER
+name|UINT32
 operator|)
 name|AcpiGbl_FADT
 operator|->
