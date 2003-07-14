3ce4308dbbdaf255c44f0d939006cd885b18657e
Josef Karthauser <joe@FreeBSD.org>
2003-07-14 18:49:18 +0000

    MFNetBSD: revision 1.170     date: 2003/02/19 01:35:04;  author: augustss;     Bail out early in isoc_done if the ii is not on the interrupt list.
    Former-commit-id: 96198be73deb845dfa76584625cd22c249a1b2bb

--- sys/dev/usb/uhci.c
+++ sys/dev/usb/uhci.c
@@ -8,7 +8,7 @@ comment|/*	$FreeBSD$	*/
 end_comment
 
 begin_comment
-comment|/*	Also incorporated from NetBSD:  *	$NetBSD: uhci.c,v 1.162 2002/07/11 21:14:28 augustss Exp $  *	$NetBSD: uhci.c,v 1.163 2002/09/27 15:37:36 provos Exp $  *	$NetBSD: uhci.c,v 1.164 2002/09/29 21:13:01 augustss Exp $  *	$NetBSD: uhci.c,v 1.165 2002/12/31 02:04:49 dsainty Exp $  *	$NetBSD: uhci.c,v 1.166 2002/12/31 02:21:31 dsainty Exp $  *	$NetBSD: uhci.c,v 1.167 2003/01/01 16:25:59 augustss Exp $  *	$NetBSD: uhci.c,v 1.168 2003/02/08 03:32:51 ichiro Exp $  *	$NetBSD: uhci.c,v 1.169 2003/02/16 23:15:28 augustss Exp $  */
+comment|/*	Also already incorporated from NetBSD:  *	$NetBSD: uhci.c,v 1.162 2002/07/11 21:14:28 augustss Exp $  *	$NetBSD: uhci.c,v 1.163 2002/09/27 15:37:36 provos Exp $  *	$NetBSD: uhci.c,v 1.164 2002/09/29 21:13:01 augustss Exp $  *	$NetBSD: uhci.c,v 1.165 2002/12/31 02:04:49 dsainty Exp $  *	$NetBSD: uhci.c,v 1.166 2002/12/31 02:21:31 dsainty Exp $  *	$NetBSD: uhci.c,v 1.167 2003/01/01 16:25:59 augustss Exp $  *	$NetBSD: uhci.c,v 1.168 2003/02/08 03:32:51 ichiro Exp $  *	$NetBSD: uhci.c,v 1.169 2003/02/16 23:15:28 augustss Exp $  *	$NetBSD: uhci.c,v 1.170 2003/02/19 01:35:04 augustss Exp $  */
 end_comment
 
 begin_comment
@@ -13229,6 +13229,15 @@ operator|!=
 name|xfer
 condition|)
 comment|/* Not on interrupt list, ignore it. */
+return|return;
+if|if
+condition|(
+operator|!
+name|uhci_active_intr_info
+argument_list|(
+name|ii
+argument_list|)
+condition|)
 return|return;
 ifdef|#
 directive|ifdef
