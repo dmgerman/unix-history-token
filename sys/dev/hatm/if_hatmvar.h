c63ab05d3b3da7f92b0f4bfc4bb12ae73eb3c2a1
Hartmut Brandt <harti@FreeBSD.org>
2003-08-07 13:42:31 +0000

    Make the driver preserve open connections accross ifconfig down and up commands. When configuring the interface down only the connections that are currently closing are deleted from the connection table. When the interface is configured up, all connections that are in the table are re-opened.
    Former-commit-id: 75594bb0c92aa63e1da68e2c5ba93445a17299f4

--- sys/dev/hatm/if_hatmvar.h
+++ sys/dev/hatm/if_hatmvar.h
@@ -2031,5 +2031,23 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|hatm_load_vc
+parameter_list|(
+name|struct
+name|hatm_softc
+modifier|*
+name|sc
+parameter_list|,
+name|u_int
+name|cid
+parameter_list|,
+name|int
+name|reopen
+parameter_list|)
+function_decl|;
+end_function_decl
+
 end_unit
 
