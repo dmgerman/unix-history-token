2848d6f38e24b9383dceca7106fbb8dadaf352c7
Jacques Vidrine <nectar@FreeBSD.org>
2003-08-10 23:05:37 +0000

    panic() if we try to handle an out-of-range signal number in psignal()/tdsignal().  The test was historically in psignal().  It was changed into a KASSERT, and then later moved to tdsignal() when the latter was introduced.
    Reviewed by:	iedowse, jhb
    
    
    Former-commit-id: b9330e32ac839fcaa64b327aa54baac6a6bbf48a

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -8128,6 +8128,19 @@ decl_stmt|;
 name|int
 name|prop
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|_SIG_VALID
+argument_list|(
+name|sig
+argument_list|)
+condition|)
+name|panic
+argument_list|(
+literal|"psignal(): invalid signal"
+argument_list|)
+expr_stmt|;
 name|PROC_LOCK_ASSERT
 argument_list|(
 name|p
@@ -8319,18 +8332,17 @@ name|sigacts
 modifier|*
 name|ps
 decl_stmt|;
-name|KASSERT
-argument_list|(
+if|if
+condition|(
+operator|!
 name|_SIG_VALID
 argument_list|(
 name|sig
 argument_list|)
-argument_list|,
-operator|(
-literal|"tdsignal(): invalid signal %d\n"
-operator|,
-name|sig
-operator|)
+condition|)
+name|panic
+argument_list|(
+literal|"do_tdsignal(): invalid signal"
 argument_list|)
 expr_stmt|;
 name|p
