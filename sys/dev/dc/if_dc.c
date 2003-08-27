1693bf29aefa61d023e4e75e39008366dc5db69e
Martin Blapp <mbr@FreeBSD.org>
2003-08-27 08:13:34 +0000

    All davicom cards seem to need DC_TX_ALIGN.
    Former-commit-id: de881aac092d46489ce17a64690daf53f136b24f

--- sys/dev/dc/if_dc.c
+++ sys/dev/dc/if_dc.c
@@ -9272,21 +9272,15 @@ name|DC_TX_STORENFWD
 expr_stmt|;
 name|sc
 operator|->
-name|dc_pmode
-operator|=
-name|DC_PMODE_MII
-expr_stmt|;
-if|if
-condition|(
-name|revision
-operator|>=
-name|DC_REVISION_DM9102A
-condition|)
-name|sc
-operator|->
 name|dc_flags
 operator||=
 name|DC_TX_ALIGN
+expr_stmt|;
+name|sc
+operator|->
+name|dc_pmode
+operator|=
+name|DC_PMODE_MII
 expr_stmt|;
 comment|/* Increase the latency timer value. */
 name|command
