13a1af01fb87e9fd57ac1fb9bb14d4b4b67c6b3b
Mark Murray <markm@FreeBSD.org>
2003-09-15 06:41:33 +0000

    Add a module dependancy. Now CAM will autoload when you load this.
    OK'ed by:	mdodd
    
    
    Former-commit-id: 35cd1af2c3a56c3eb14cb5f74392c4e4b66e5bdf

--- sys/dev/isp/isp_freebsd.c
+++ sys/dev/isp/isp_freebsd.c
@@ -79,6 +79,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|isp
+argument_list|,
+name|cam
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_decl_stmt
 name|int
 name|isp_announced
