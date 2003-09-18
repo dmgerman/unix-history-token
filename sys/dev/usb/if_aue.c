9891e4789a3607b4f227378a1e58f4d6f5b60aa2
David E. O'Brien <obrien@FreeBSD.org>
2003-09-18 15:41:07 +0000

    Support the Hawking Technology's UF100 Pegasus II 10/100 USB Ethernet.
    Former-commit-id: 97f3c916a7aa5227b729f286cafccaacd3dbb874

--- sys/dev/usb/if_aue.c
+++ sys/dev/usb/if_aue.c
@@ -654,6 +654,16 @@ block|}
 block|,
 block|{
 block|{
+name|USB_VENDOR_HAWKING
+block|,
+name|USB_PRODUCT_HAWKING_UF100
+block|}
+block|,
+name|PII
+block|}
+block|,
+block|{
+block|{
 name|USB_VENDOR_IODATA
 block|,
 name|USB_PRODUCT_IODATA_USBETTX
