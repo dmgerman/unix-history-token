f93cdb1326ac4077518b2cea2f81f043d0ee3eec
Peter Grehan <grehan@FreeBSD.org>
2003-09-22 11:55:02 +0000

    ATAng requires <sys/taskqueue.h>
    Former-commit-id: fda2758994827e8c1df9606bbce84eb5f33824aa

--- sys/powerpc/powermac/ata_macio.c
+++ sys/powerpc/powermac/ata_macio.c
@@ -52,6 +52,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/taskqueue.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/stdarg.h>
 end_include
 
