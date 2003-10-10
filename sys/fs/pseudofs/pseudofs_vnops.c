284253637752fc4bdfd7029bbf885cc0f62636f5
Jacques Vidrine <nectar@FreeBSD.org>
2003-10-10 20:15:49 +0000

    Repair build. sys/limits.h -> machine/limits.h This should have been committed along with kern_subr.c 1.63.2.2.
    Noticed by:	Chris Grijzen <chrisgrijzen@hotmail.com>
    
    
    Former-commit-id: 7a52a53453a27e0a9eeace278a64fdac4d738337

--- sys/fs/pseudofs/pseudofs_vnops.c
+++ sys/fs/pseudofs/pseudofs_vnops.c
@@ -42,12 +42,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/limits.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/lock.h>
 end_include
 
@@ -97,6 +91,12 @@ begin_include
 include|#
 directive|include
 file|<sys/vnode.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/limits.h>
 end_include
 
 begin_include
