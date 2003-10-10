c9e8c4aec15557bdf858faedbe25094cb87cfc66
Hajimu UMEMOTO <ume@FreeBSD.org>
2003-10-10 15:06:16 +0000

    switch cast128 implementation to implementation by Steve Reid; smaller footprint.
    Obtained from:	KAME
    
    
    Former-commit-id: cccfd34f0ce5818ebfb23e5a1626b09d947034f6

--- sys/crypto/cast128/cast128.h
+++ sys/crypto/cast128/cast128.h
@@ -4,137 +4,106 @@ comment|/*	$FreeBSD$	*/
 end_comment
 
 begin_comment
-comment|/*	$KAME: cast128.h,v 1.7 2001/11/27 09:47:32 sakane Exp $	*/
+comment|/*	$NetBSD: cast128.h,v 1.6 2003/08/26 20:03:57 thorpej Exp $ */
 end_comment
 
 begin_comment
-comment|/*  * heavily modified by Tomomi Suzuki<suzuki@grelot.elec.ryukoku.ac.jp>  */
+comment|/*      $OpenBSD: cast.h,v 1.2 2002/03/14 01:26:51 millert Exp $       */
 end_comment
 
 begin_comment
-comment|/*  * The CAST-128 Encryption Algorithm (RFC 2144)  *  * original implementation<Hideo "Sir MaNMOS" Morisita>  * 1997/08/21  */
-end_comment
-
-begin_comment
-comment|/*  * Copyright (C) 1997 Hideo "Sir MANMOS" Morishita  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY Hideo "Sir MaNMOS" Morishita ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL Hideo "Sir MaNMOS" Morishita BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*  *	CAST-128 in C  *	Written by Steve Reid<sreid@sea-to-sky.net>  *	100% Public Domain - no warranty  *	Released 1997.10.11  */
 end_comment
 
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|RFC2144_CAST_128_H
+name|_CAST128_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|RFC2144_CAST_128_H
+name|_CAST128_H_
 end_define
 
-begin_include
-include|#
-directive|include
-file|<sys/param.h>
-end_include
-
-begin_define
-define|#
-directive|define
-name|CAST128_ENCRYPT
-value|1
-end_define
-
-begin_define
-define|#
-directive|define
-name|CAST128_DECRYPT
-value|0
-end_define
+begin_typedef
+typedef|typedef
+struct|struct
+block|{
+name|u_int32_t
+name|xkey
+index|[
+literal|32
+index|]
+decl_stmt|;
+comment|/* Key, after expansion */
+name|int
+name|rounds
+decl_stmt|;
+comment|/* Number of rounds to use, 12 or 16 */
+block|}
+name|cast128_key
+typedef|;
+end_typedef
 
 begin_function_decl
-specifier|extern
 name|void
-name|set_cast128_subkey
+name|cast128_setkey
 parameter_list|(
-name|u_int32_t
+name|cast128_key
 modifier|*
+name|key
 parameter_list|,
+specifier|const
 name|u_int8_t
 modifier|*
+name|rawkey
 parameter_list|,
 name|int
+name|keybytes
 parameter_list|)
 function_decl|;
 end_function_decl
 
 begin_function_decl
-specifier|extern
 name|void
-name|cast128_encrypt_round16
+name|cast128_encrypt
 parameter_list|(
-name|u_int8_t
+specifier|const
+name|cast128_key
 modifier|*
+name|key
 parameter_list|,
 specifier|const
 name|u_int8_t
 modifier|*
+name|inblock
 parameter_list|,
-name|u_int32_t
+name|u_int8_t
 modifier|*
+name|outblock
 parameter_list|)
 function_decl|;
 end_function_decl
 
 begin_function_decl
-specifier|extern
 name|void
-name|cast128_decrypt_round16
+name|cast128_decrypt
 parameter_list|(
-name|u_int8_t
+specifier|const
+name|cast128_key
 modifier|*
+name|key
 parameter_list|,
 specifier|const
 name|u_int8_t
 modifier|*
+name|inblock
 parameter_list|,
-name|u_int32_t
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|cast128_encrypt_round12
-parameter_list|(
 name|u_int8_t
 modifier|*
-parameter_list|,
-specifier|const
-name|u_int8_t
-modifier|*
-parameter_list|,
-name|u_int32_t
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|extern
-name|void
-name|cast128_decrypt_round12
-parameter_list|(
-name|u_int8_t
-modifier|*
-parameter_list|,
-specifier|const
-name|u_int8_t
-modifier|*
-parameter_list|,
-name|u_int32_t
-modifier|*
+name|outblock
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -143,6 +112,10 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* _CAST128_H_ */
+end_comment
 
 end_unit
 
