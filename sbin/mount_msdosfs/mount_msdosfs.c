a619776552da2cb038a33c85340d80ab6c8495a6
Tom Rhodes <trhodes@FreeBSD.org>
2003-10-22 20:11:42 +0000

    Make WARNS=2 build without error.
    Former-commit-id: 2dfc9fa2b51b83bef345ab6c20b8f9db66ced535

--- sbin/mount_msdosfs/mount_msdosfs.c
+++ sbin/mount_msdosfs/mount_msdosfs.c
@@ -66,6 +66,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/linker.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<fs/msdosfs/msdosfsmount.h>
 end_include
 
