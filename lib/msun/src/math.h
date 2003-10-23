8ef71e03586590cefb50fe86914bfa22911941c9
Dag-Erling Smorgrav <des@FreeBSD.org>
2003-10-23 08:23:38 +0000

    Add prototypes for all long double functions in C99.  Leave them all #if 0'd out, except for fabsl(3) which I've implemented.
    Former-commit-id: e96c754045b342e5b44667f2a6e9d57aecabeeeb

--- lib/msun/src/math.h
+++ lib/msun/src/math.h
@@ -1953,6 +1953,45 @@ begin_comment
 comment|/* __BSD_VISIBLE */
 end_comment
 
+begin_comment
+comment|/*  * long double versions of ISO/POSIX math functions  */
+end_comment
+
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|long double	acoshl(long double); long double	acosl(long double); long double	asinhl(long double); long double	asinl(long double); long double	atan2l(long double, long double); long double	atanhl(long double); long double	atanl(long double); long double	cbrtl(long double); long double	ceill(long double); long double	copysignl(long double, long double); long double	coshl(long double); long double	cosl(long double); long double	erfcl(long double); long double	erfl(long double); long double	exp2l(long double); long double	expl(long double); long double	expm1l(long double);
+endif|#
+directive|endif
+end_endif
+
+begin_function_decl
+name|long
+name|double
+name|fabsl
+parameter_list|(
+name|long
+name|double
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|long double	fdiml(long double, long double); long double	floorl(long double); long double	fmal(long double, long double, long double); long double	fmaxl(long double, long double); long double	fminl(long double, long double); long double	fmodl(long double, long double); long double	frexpl(long double	value, int *); long double	hypotl(long double, long double); int		ilogbl(long double); long double	ldexpl(long double, int); long double	lgammal(long double); long long	llrintl(long double); long long	llroundl(long double); long double	log10l(long double); long double	log1pl(long double); long double	log2l(long double); long double	logbl(long double); long double	logl(long double); long		lrintl(long double); long		lroundl(long double); long double	modfl(long double, long double	*); long double	nanl(const char *); long double	nearbyintl(long double); long double	nextafterl(long double, long double); double		nexttoward(double, long double); float		nexttowardf(float, long double); long double	nexttowardl(long double, long double); long double	powl(long double, long double); long double	remainderl(long double, long double); long double	remquol(long double, long double, int *); long double	rintl(long double); long double	roundl(long double); long double	scalblnl(long double, long); long double	scalbnl(long double, int); long double	sinhl(long double); long double	sinl(long double); long double	sqrtl(long double); long double	tanhl(long double); long double	tanl(long double); long double	tgammal(long double); long double	truncl(long double);
+endif|#
+directive|endif
+end_endif
+
 begin_endif
 endif|#
 directive|endif
