4aa0322e48a56f265ea18d7e1638af5fe8a1947c
Marcel Moolenaar <marcel@FreeBSD.org>
2003-10-23 06:41:59 +0000

    Cleanup. Remove the md_flags for threads. It's not used. The flags we had were bogus. While here, reassign the copyright to the Project. There's nothing in this files that originates from NetBSD, especially now that the FreeBSD/alpha bits have been removed, but even then the amount of inherited code that we actually used was nil.
    Former-commit-id: a36657124e87143a01c9cb81b0566d4b29d4eede

--- sys/ia64/include/proc.h
+++ sys/ia64/include/proc.h
@@ -1,14 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $FreeBSD$ */
-end_comment
-
-begin_comment
-comment|/* From: NetBSD: proc.h,v 1.3 1997/04/06 08:47:36 cgd Exp */
-end_comment
-
-begin_comment
-comment|/*  * Copyright (c) 1994, 1995 Carnegie-Mellon University.  * All rights reserved.  *  * Author: Chris G. Demetriou  *   * Permission to use, copy, modify and distribute this software and  * its documentation is hereby granted, provided that both the copyright  * notice and this permission notice appear in all copies of the  * software, derivative works or modified versions, and any portions  * thereof, and that both notices appear in supporting documentation.  *   * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"   * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND   * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.  *   * Carnegie Mellon requests users of this software to return to  *  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU  *  School of Computer Science  *  Carnegie Mellon University  *  Pittsburgh PA 15213-3890  *  * any improvements or extensions that they make and grant Carnegie the  * rights to redistribute these changes.  */
+comment|/*  * Copyright (c) 2003 The FreeBSD Project  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -23,74 +15,16 @@ directive|define
 name|_MACHINE_PROC_H_
 end_define
 
-begin_comment
-comment|/*  * Machine-dependent part of the proc struct for the Alpha.  */
-end_comment
-
 begin_struct
 struct|struct
 name|mdthread
 block|{
-name|u_long
-name|md_flags
-decl_stmt|;
 name|register_t
 name|md_savecrit
 decl_stmt|;
 block|}
 struct|;
 end_struct
-
-begin_define
-define|#
-directive|define
-name|MDP_FPUSED
-value|0x0001
-end_define
-
-begin_comment
-comment|/* Process used the FPU */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|MDP_UAC_NOPRINT
-value|0x0010
-end_define
-
-begin_comment
-comment|/* Don't print unaligned traps */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|MDP_UAC_NOFIX
-value|0x0020
-end_define
-
-begin_comment
-comment|/* Don't fixup unaligned traps */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|MDP_UAC_SIGBUS
-value|0x0040
-end_define
-
-begin_comment
-comment|/* Deliver SIGBUS upon 					   unaligned access */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|MDP_UAC_MASK
-value|(MDP_UAC_NOPRINT | MDP_UAC_NOFIX | MDP_UAC_SIGBUS)
-end_define
 
 begin_struct
 struct|struct
