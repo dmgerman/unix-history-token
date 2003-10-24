a250beedc8b65f15394d99181fced2dfed78637b
Warner Losh <imp@FreeBSD.org>
2003-10-24 22:02:29 +0000

    Parse the ! lines that will soon be coming from the kernel.  These are a generalized notification mechanism for subsystems wishing to report events.
    Revieded by: njl
    
    # The kernel side seems like it might be causing panics for me, but should
    # be forthcoming shortly.
    
    
    Former-commit-id: e5c8c41755eea8a8639a9a14c893d35d7e44fac4

--- sbin/devd/devd.h
+++ sbin/devd/devd.h
@@ -83,6 +83,19 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|void
+name|add_notify
+parameter_list|(
+name|int
+parameter_list|,
+name|struct
+name|event_proc
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|struct
 name|event_proc
 modifier|*
