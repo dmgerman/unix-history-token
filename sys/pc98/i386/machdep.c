c1b1419763c9bd9e66fcc11bb24a774173331c17
Takahashi Yoshihiro <nyan@FreeBSD.org>
2003-10-31 13:47:43 +0000

    MFi386: revision 1.578.
    Former-commit-id: 963d4a4b2cd0d1c0d4f7b73d967539d55ff75c40

--- sys/pc98/i386/machdep.c
+++ sys/pc98/i386/machdep.c
@@ -6039,16 +6039,6 @@ begin_comment
 comment|/* local descriptor table */
 end_comment
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|SMP
-end_ifdef
-
-begin_comment
-comment|/* table descriptors - used to load tables by microp */
-end_comment
-
 begin_decl_stmt
 name|struct
 name|region_descriptor
@@ -6058,10 +6048,9 @@ name|r_idt
 decl_stmt|;
 end_decl_stmt
 
-begin_endif
-endif|#
-directive|endif
-end_endif
+begin_comment
+comment|/* table descriptors */
+end_comment
 
 begin_decl_stmt
 name|int
@@ -8837,18 +8826,6 @@ name|off
 decl_stmt|,
 name|x
 decl_stmt|;
-ifndef|#
-directive|ifndef
-name|SMP
-comment|/* table descriptors - used to load tables by microp */
-name|struct
-name|region_descriptor
-name|r_gdt
-decl_stmt|,
-name|r_idt
-decl_stmt|;
-endif|#
-directive|endif
 name|struct
 name|pcpu
 modifier|*
