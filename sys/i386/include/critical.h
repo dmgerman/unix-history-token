adfc66c94c27d264e1fc35cb7fae364096c71a37
John Baldwin <jhb@FreeBSD.org>
2003-11-03 21:06:54 +0000

    Revert the critical section implementation to disable interrupts via cli/sti now that we support many more than 32 interrupt sources.
    Former-commit-id: 12efcc650d76c614ea87267e5f705b404687c424

--- sys/i386/include/critical.h
+++ sys/i386/include/critical.h
@@ -19,15 +19,6 @@ begin_function_decl
 name|__BEGIN_DECLS
 comment|/*  * Prototypes - see<arch>/<arch>/critical.c  */
 name|void
-name|cpu_unpend
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
 name|cpu_critical_fork_exit
 parameter_list|(
 name|void
@@ -45,12 +36,26 @@ begin_comment
 comment|/*  *	cpu_critical_enter:  *  *	This routine is called from critical_enter() on the 0->1 transition  *	of td_critnest, prior to it being incremented to 1.  *  *	If new-style critical section handling we do not have to do anything.  *	However, as a side effect any interrupts occuring while td_critnest  *	is non-zero will be deferred.  */
 end_comment
 
-begin_define
-define|#
-directive|define
+begin_function
+specifier|static
+name|__inline
+name|void
 name|cpu_critical_enter
-parameter_list|()
-end_define
+parameter_list|(
+name|void
+parameter_list|)
+block|{
+name|curthread
+operator|->
+name|td_md
+operator|.
+name|md_savecrit
+operator|=
+name|intr_disable
+argument_list|()
+expr_stmt|;
+block|}
+end_function
 
 begin_comment
 comment|/*  *	cpu_critical_exit:  *  *	This routine is called from critical_exit() on a 1->0 transition  *	of td_critnest, after it has been decremented to 0.  We are  *	exiting the last critical section.  *  *	Note that the td->critnest (1->0) transition interrupt race against  *	our int_pending/unpend() check below is handled by the interrupt  *	code for us, so we do not have to do anything fancy.  */
@@ -65,16 +70,14 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
-comment|/* 	 * We may have to schedule pending interrupts.  Create 	 * conditions similar to an interrupt context and call 	 * unpend(). 	 * 	 * note: we do this even if we are in an interrupt 	 * nesting level.  Deep nesting is protected by 	 * critical_*() and if we conditionalized it then we 	 * would have to check int_pending again whenever 	 * we decrement td_intr_nesting_level to 0. 	 */
-if|if
-condition|(
-name|PCPU_GET
+name|intr_restore
 argument_list|(
-name|int_pending
+name|curthread
+operator|->
+name|td_md
+operator|.
+name|md_savecrit
 argument_list|)
-condition|)
-name|cpu_unpend
-argument_list|()
 expr_stmt|;
 block|}
 end_function
