f5eed985af24d6d92e0d00c62871ebe8012e5b47
Tim J. Robbins <tjr@FreeBSD.org>
2003-11-08 02:58:37 +0000

    Remove unused #includes.
    Former-commit-id: 2a74f2b87ee7250e8bb4ea60e7351f6566887d61

--- lib/libc/locale/big5.c
+++ lib/libc/locale/big5.c
@@ -66,18 +66,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stddef.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<stdio.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<stdlib.h>
 end_include
 
