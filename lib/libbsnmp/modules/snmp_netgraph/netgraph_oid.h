952acc6e7dbe5f1f112dcd242670bc737d280c8a
Hartmut Brandt <harti@FreeBSD.org>
2003-11-10 09:17:34 +0000

    Add build infrastructure for libbsnmp and the modules. Add the netgraph module for the SNMP daemon.
    Former-commit-id: cf0280c4781d4a8a45f7bb900076307750d15146

---
+++ lib/libbsnmp/modules/snmp_netgraph/netgraph_oid.h
@@ -0,0 +1,32 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
+comment|/* generated file, don't edit - use ./genfiles */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|OID_begemotNg
+value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|OIDLEN_begemotNg
+value|9
+end_define
+
+begin_define
+define|#
+directive|define
+name|OIDX_begemotNg
+value|{ 9, { 1, 3, 6, 1, 4, 1, 12325, 1, 2, } }
+end_define
+
+end_unit
+
