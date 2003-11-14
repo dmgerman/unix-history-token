96dbeb295b3f1bbeed10e329da22cf35b72e5348
Warner Losh <imp@FreeBSD.org>
2003-11-14 05:37:36 +0000

    Reconst-poison crc routine that was renamed to mchash. re-remove the _ in u_intXX_t
    Former-commit-id: 01f96676325b4b90ec6fd428633bbc1d1ca3ca4e

--- sys/dev/sn/if_sn.c
+++ sys/dev/sn/if_sn.c
@@ -348,10 +348,13 @@ end_function_decl
 
 begin_function_decl
 specifier|static
-name|u_int32_t
+name|uint32_t
 name|sn_mchash
 parameter_list|(
-name|caddr_t
+specifier|const
+name|uint8_t
+modifier|*
+name|addr
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -4885,10 +4888,12 @@ end_function
 
 begin_function
 specifier|static
-name|u_int32_t
+name|uint32_t
 name|sn_mchash
 parameter_list|(
-name|caddr_t
+specifier|const
+name|uint8_t
+modifier|*
 name|addr
 parameter_list|)
 block|{
@@ -4898,7 +4903,7 @@ name|poly
 init|=
 literal|0xedb88320
 decl_stmt|;
-name|u_int32_t
+name|uint32_t
 name|crc
 decl_stmt|;
 name|int
@@ -4906,7 +4911,7 @@ name|idx
 decl_stmt|,
 name|bit
 decl_stmt|;
-name|u_int8_t
+name|uint8_t
 name|data
 decl_stmt|;
 comment|/* Compute CRC for the address value. */
