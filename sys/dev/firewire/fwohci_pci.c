64cfe11eb8ee119c7cf76767a5bc2baf8a8f12a2
Hidetoshi Shimokawa <simokawa@FreeBSD.org>
2003-12-14 03:09:27 +0000

    Put back accidentally removed cardbus support.
    Former-commit-id: da8e2f43b7a41e89cb7338c89d42b3bd5d5ef16d

--- sys/dev/firewire/fwohci_pci.c
+++ sys/dev/firewire/fwohci_pci.c
@@ -2050,5 +2050,23 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|DRIVER_MODULE
+argument_list|(
+name|fwohci
+argument_list|,
+name|cardbus
+argument_list|,
+name|fwohci_driver
+argument_list|,
+name|fwohci_devclass
+argument_list|,
+literal|0
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
