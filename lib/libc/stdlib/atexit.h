71c5387469cd5908f42a1dd47bb1ac13a97bcdab
Alexander Kabaev <kan@FreeBSD.org>
2003-12-19 17:11:21 +0000

    Implement __cxa_atexit/__cxa_finalize as specified by the cross-vendor ó++ ABI document at http://www.codesourcery.com/cxx-abi/abi.html#dso-dtor
    The ABI was initially defined for ia64, but GCC3 and Intel compilers
    have adopted it on other platforms.
    
    This is the patch from PR bin/59552 with a number of changes by
    me.
    
    PR:		bin/59552
    Submitted by:	Bradley T Hughes (bhughes at trolltech dot com)
    
    Former-commit-id: 046624af185ecb726fbfaa0b499d1d8a16517294

--- lib/libc/stdlib/atexit.h
+++ lib/libc/stdlib/atexit.h
@@ -14,47 +14,16 @@ name|ATEXIT_SIZE
 value|32
 end_define
 
-begin_struct
-struct|struct
-name|atexit
-block|{
-name|struct
-name|atexit
-modifier|*
-name|next
-decl_stmt|;
-comment|/* next in list */
-name|int
-name|ind
-decl_stmt|;
-comment|/* next index in this table */
+begin_function_decl
 name|void
-function_decl|(
+name|__cxa_finalize
+parameter_list|(
+name|void
 modifier|*
-name|fns
-index|[
-name|ATEXIT_SIZE
-index|]
-function_decl|)
-parameter_list|()
+name|dso
+parameter_list|)
 function_decl|;
-comment|/* the table itself */
-block|}
-struct|;
-end_struct
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|atexit
-modifier|*
-name|__atexit
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
-comment|/* points to head of LIFO stack */
-end_comment
+end_function_decl
 
 end_unit
 
