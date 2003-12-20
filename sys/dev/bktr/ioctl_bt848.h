21bf6e1bf6e3ed110c0318afa75f412fb5a54fe7
David E. O'Brien <obrien@FreeBSD.org>
2003-12-20 17:12:25 +0000

    Make the multiple include guard correct for this file location. This fixes a bug where the guard conflicted with machine/ioctl_*.
    Former-commit-id: 9cadd89b0475f4122265161e3ee7e010dcfe9e3a

--- sys/dev/bktr/ioctl_bt848.h
+++ sys/dev/bktr/ioctl_bt848.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_MACHINE_IOCTL_BT848_H_
+name|_DEV_BKTR_IOCTL_BT848_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_MACHINE_IOCTL_BT848_H_
+name|_DEV_BKTR_IOCTL_BT848_H_
 end_define
 
 begin_comment
@@ -1456,7 +1456,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _MACHINE_IOCTL_BT848_H_ */
+comment|/* _DEV_BKTR_IOCTL_BT848_H_ */
 end_comment
 
 end_unit
--- sys/dev/bktr/ioctl_meteor.h
+++ sys/dev/bktr/ioctl_meteor.h
@@ -10,13 +10,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_MACHINE_IOCTL_METEOR_H_
+name|_DEV_BKTR_IOCTL_METEOR_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_MACHINE_IOCTL_METEOR_H_
+name|_DEV_BKTR_IOCTL_METEOR_H_
 end_define
 
 begin_ifndef
@@ -1056,7 +1056,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_MACHINE_IOCTL_METEOR_H_ */
+comment|/* !_DEV_BKTR_IOCTL_METEOR_H_ */
 end_comment
 
 end_unit
