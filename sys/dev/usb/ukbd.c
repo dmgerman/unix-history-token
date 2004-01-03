f0a36456968ab7aecdc4d3ddc17528a9de7c8864
MIHIRA Sanpei Yoshiro <sanpei@FreeBSD.org>
2004-01-03 15:01:04 +0000

    Add support SUN TYPE 6 USB KEYBOARD
    Submitted by:	<schley@cf-wnd.de>
    
    
    Former-commit-id: 27acaa089672bccd7579060604843a45d1832e83

--- sys/dev/usb/ukbd.c
+++ sys/dev/usb/ukbd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1998 The NetBSD Foundation, Inc.  * All rights reserved.  *  * This code is derived from software contributed to The NetBSD Foundation  * by Lennart Augustsson (lennart@augustsson.net) at  * Carlstedt Research& Technology.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *        This product includes software developed by the NetBSD  *        Foundation, Inc. and its contributors.  * 4. Neither the name of The NetBSD Foundation nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1998 The NetBSD Foundation, Inc.  * All rights reserved.  *  * This code is derived from software contributed to The NetBSD Foundation  * by Lennart Augustsson (lennart@augustsson.net) at  * Carlstedt Research& Technology.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *        This product includes software developed by the NetBSD  *        Foundation, Inc. and its contributors.  * 4. Neither the name of The NetBSD Foundation nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * Modifications for SUN TYPE 6 USB Keyboard by  *  Jörg Peter Schley (jps@scxnet.de)  */
 end_comment
 
 begin_include
@@ -1526,7 +1526,7 @@ literal|86
 block|,
 literal|107
 block|,
-name|NN
+literal|122
 block|,
 name|NN
 block|,
@@ -1556,35 +1556,35 @@ name|NN
 block|,
 name|NN
 block|,
-name|NN
+literal|115
 block|,
-name|NN
+literal|108
 block|,
-name|NN
+literal|111
 block|,
-name|NN
+literal|113
 block|,
 comment|/* 70 - 77 */
-name|NN
+literal|109
 block|,
-name|NN
+literal|110
 block|,
-name|NN
+literal|112
 block|,
-name|NN
+literal|118
 block|,
-name|NN
+literal|114
 block|,
-name|NN
+literal|116
 block|,
-name|NN
+literal|117
 block|,
-name|NN
+literal|119
 block|,
 comment|/* 78 - 7F */
-name|NN
+literal|121
 block|,
-name|NN
+literal|120
 block|,
 name|NN
 block|,
@@ -7481,7 +7481,38 @@ block|,
 literal|0x5c
 block|,
 literal|0x5d
-block|, 	}
+block|,
+comment|/* SUN TYPE 6 USB KEYBOARD */
+literal|0x68
+block|,
+literal|0x5e
+block|,
+literal|0x5f
+block|,
+literal|0x60
+block|,
+literal|0x61
+block|,
+literal|0x62
+block|,
+literal|0x63
+block|,
+literal|0x64
+block|,
+literal|0x65
+block|,
+literal|0x66
+block|,
+literal|0x67
+block|,
+literal|0x25
+block|,
+literal|0x1f
+block|,
+literal|0x1e
+block|,
+literal|0x20
+block|,  	}
 decl_stmt|;
 name|int
 name|scancode
