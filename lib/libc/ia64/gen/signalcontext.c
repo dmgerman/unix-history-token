3afe73772bdef36ad6c26b231c54ea4731d12818
Jacques Vidrine <nectar@FreeBSD.org>
2004-01-06 19:40:28 +0000

    Add required headers.
    Former-commit-id: 1e0c916d412544c20fea80501e10500e663383dd

--- lib/libc/ia64/gen/signalcontext.c
+++ lib/libc/ia64/gen/signalcontext.c
@@ -50,6 +50,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<stdlib.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<strings.h>
 end_include
 
