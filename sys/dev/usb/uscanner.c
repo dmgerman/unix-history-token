8c0a0fc75c48db5b1afd7c227de49abd3949f0d0
MIHIRA Sanpei Yoshiro <sanpei@FreeBSD.org>
2004-01-12 12:54:34 +0000

    MFC: Add support 	- Canon USB Scanner N676U 	- Canon USB Scanner N1220U
    Former-commit-id: af5ddb198c9d5711c59f9668e194db7e1d21d3ec

--- sys/dev/usb/uscanner.c
+++ sys/dev/usb/uscanner.c
@@ -472,6 +472,26 @@ block|,
 literal|0
 block|}
 block|,
+block|{
+block|{
+name|USB_VENDOR_CANON
+block|,
+name|USB_PRODUCT_CANON_N676U
+block|}
+block|,
+literal|0
+block|}
+block|,
+block|{
+block|{
+name|USB_VENDOR_CANON
+block|,
+name|USB_PRODUCT_CANON_N1220U
+block|}
+block|,
+literal|0
+block|}
+block|,
 comment|/* Kye */
 block|{
 block|{
