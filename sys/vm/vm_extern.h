2940b878bd36074a51151435bb381157740067b6
Alan Cox <alc@FreeBSD.org>
2004-01-14 06:21:38 +0000

    Remove vm_page_alloc_contig().  It's now unused.
    Former-commit-id: d75624ebbb1c2544fa8e9f29e47a1a7578cac198

--- sys/vm/vm_extern.h
+++ sys/vm/vm_extern.h
@@ -506,21 +506,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|vm_offset_t
-name|vm_page_alloc_contig
-parameter_list|(
-name|vm_offset_t
-parameter_list|,
-name|vm_paddr_t
-parameter_list|,
-name|vm_paddr_t
-parameter_list|,
-name|vm_offset_t
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|void
 name|vm_set_page_size
 parameter_list|(
