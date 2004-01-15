488e8d0191966253c39b6940d26187141e5e33d6
Peter Grehan <grehan@FreeBSD.org>
2004-01-15 23:52:32 +0000

    Catch up with ATA UMA changes
    Former-commit-id: 2297db775775a8406bb9722af4d7cae579fbd152

--- sys/powerpc/powermac/ata_macio.c
+++ sys/powerpc/powermac/ata_macio.c
@@ -64,6 +64,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/uma.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/stdarg.h>
 end_include
 
--- sys/powerpc/psim/ata_iobus.c
+++ sys/powerpc/psim/ata_iobus.c
@@ -70,6 +70,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/uma.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/resource.h>
 end_include
 
