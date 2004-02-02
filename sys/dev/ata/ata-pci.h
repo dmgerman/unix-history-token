0dabffa86817cfc2170636557546fdf2d552d592
Soren Schmidt <sos@FreeBSD.org>
2004-02-02 14:05:57 +0000

    Fix support for the Promise TX4 on amd64. Somehow the bridge on there shows up with another PCI id than it does on x86, no idea why...
    Former-commit-id: 312cc37757b3fd2c32fe00d3d7006aa5095fa88c

--- sys/dev/ata/ata-pci.h
+++ sys/dev/ata/ata-pci.h
@@ -322,6 +322,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|ATA_DEC_21150_1
+value|0x00231011
+end_define
+
+begin_define
+define|#
+directive|define
 name|ATA_HIGHPOINT_ID
 value|0x1103
 end_define
