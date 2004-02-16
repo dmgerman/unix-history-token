a88158720b8133bbb0933bb5299c83e1128039bb
Andrey Chernov <ache@FreeBSD.org>
2004-02-16 22:43:05 +0000

    1) Remove files no longer needed. 2) Switch back to regex.h
    Former-commit-id: bf435d7e22f8343c7626bb9a1923494c6c919f7e

--- gnu/usr.bin/grep/dfa.c
+++ gnu/usr.bin/grep/dfa.c
@@ -542,33 +542,11 @@ endif|#
 directive|endif
 end_endif
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__FreeBSD__
-end_ifdef
-
-begin_include
-include|#
-directive|include
-file|<gnuregex.h>
-end_include
-
-begin_else
-else|#
-directive|else
-end_else
-
 begin_include
 include|#
 directive|include
 file|"regex.h"
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
--- gnu/usr.bin/grep/search.c
+++ gnu/usr.bin/grep/search.c
@@ -46,33 +46,11 @@ directive|include
 file|"grep.h"
 end_include
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__FreeBSD__
-end_ifdef
-
-begin_include
-include|#
-directive|include
-file|<gnuregex.h>
-end_include
-
-begin_else
-else|#
-directive|else
-end_else
-
 begin_include
 include|#
 directive|include
 file|"regex.h"
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
