da4f849bab8e7c138bd6a4b186919e6a4d13105d
Andrey Chernov <ache@FreeBSD.org>
2004-02-18 22:09:34 +0000

    Install gnuregex.h as wrapper for gnu/regex.h (with warning)
    Former-commit-id: 062e3d29a1612541f411c7aaa531d105ce0f83bb

---
+++ gnu/lib/libregex/gnuregex.h
@@ -0,0 +1,19 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_warning
+warning|#
+directive|warning
+literal|"<gnuregex.h> has been replaced by<gnu/regex.h>"
+end_warning
+
+begin_include
+include|#
+directive|include
+file|<gnu/regex.h>
+end_include
+
+end_unit
+
