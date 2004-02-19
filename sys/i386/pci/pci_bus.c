4ad52b0cc827bf18f2e7664af2e07f31bf0417f1
John Baldwin <jhb@FreeBSD.org>
2004-02-19 18:34:26 +0000

    Add back an include to fix the build for the CPU_ELAN case.
    Former-commit-id: 7cca398c53ec8ad54851aadac9e1cb87176ea8c7

--- sys/i386/pci/pci_bus.c
+++ sys/i386/pci/pci_bus.c
@@ -77,6 +77,23 @@ directive|include
 file|<isa/isavar.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|CPU_ELAN
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<machine/md_var.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
