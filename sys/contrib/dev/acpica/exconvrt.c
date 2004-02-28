ea6fbff9cf8943c501459df7e1eb4df578d977c5
Nate Lawson <njl@FreeBSD.org>
2004-02-28 20:23:30 +0000

    Vendor import of ACPICA 20040220
    Former-commit-id: f45838efb3e3e26def38d83bbcd346501db4eda6

--- sys/contrib/dev/acpica/exconvrt.c
+++ sys/contrib/dev/acpica/exconvrt.c
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************  *  * Module Name: exconvrt - Object conversion routines  *              $Revision: 49 $  *  *****************************************************************************/
+comment|/******************************************************************************  *  * Module Name: exconvrt - Object conversion routines  *              $Revision: 53 $  *  *****************************************************************************/
 end_comment
 
 begin_comment
-comment|/******************************************************************************  *  * 1. Copyright Notice  *  * Some or all of this work - Copyright (c) 1999 - 2003, Intel Corp.  * All rights reserved.  *  * 2. License  *  * 2.1. This is your license from Intel Corp. under its intellectual property  * rights.  You may have additional license terms from the party that provided  * you this software, covering your right to use that party's intellectual  * property rights.  *  * 2.2. Intel grants, free of charge, to any person ("Licensee") obtaining a  * copy of the source code appearing in this file ("Covered Code") an  * irrevocable, perpetual, worldwide license under Intel's copyrights in the  * base code distributed originally by Intel ("Original Intel Code") to copy,  * make derivatives, distribute, use and display any portion of the Covered  * Code in any form, with the right to sublicense such rights; and  *  * 2.3. Intel grants Licensee a non-exclusive and non-transferable patent  * license (with the right to sublicense), under only those claims of Intel  * patents that are infringed by the Original Intel Code, to make, use, sell,  * offer to sell, and import the Covered Code and derivative works thereof  * solely to the minimum extent necessary to exercise the above copyright  * license, and in no event shall the patent license extend to any additions  * to or modifications of the Original Intel Code.  No other license or right  * is granted directly or by implication, estoppel or otherwise;  *  * The above copyright and patent license is granted only if the following  * conditions are met:  *  * 3. Conditions  *  * 3.1. Redistribution of Source with Rights to Further Distribute Source.  * Redistribution of source code of any substantial portion of the Covered  * Code or modification with rights to further distribute source must include  * the above Copyright Notice, the above License, this list of Conditions,  * and the following Disclaimer and Export Compliance provision.  In addition,  * Licensee must cause all Covered Code to which Licensee contributes to  * contain a file documenting the changes Licensee made to create that Covered  * Code and the date of any change.  Licensee must include in that file the  * documentation of any changes made by any predecessor Licensee.  Licensee  * must include a prominent statement that the modification is derived,  * directly or indirectly, from Original Intel Code.  *  * 3.2. Redistribution of Source with no Rights to Further Distribute Source.  * Redistribution of source code of any substantial portion of the Covered  * Code or modification without rights to further distribute source must  * include the following Disclaimer and Export Compliance provision in the  * documentation and/or other materials provided with distribution.  In  * addition, Licensee may not authorize further sublicense of source of any  * portion of the Covered Code, and must include terms to the effect that the  * license from Licensee to its licensee is limited to the intellectual  * property embodied in the software Licensee provides to its licensee, and  * not to intellectual property embodied in modifications its licensee may  * make.  *  * 3.3. Redistribution of Executable. Redistribution in executable form of any  * substantial portion of the Covered Code or modification must reproduce the  * above Copyright Notice, and the following Disclaimer and Export Compliance  * provision in the documentation and/or other materials provided with the  * distribution.  *  * 3.4. Intel retains all right, title, and interest in and to the Original  * Intel Code.  *  * 3.5. Neither the name Intel nor any other trademark owned or controlled by  * Intel shall be used in advertising or otherwise to promote the sale, use or  * other dealings in products derived from or relating to the Covered Code  * without prior written authorization from Intel.  *  * 4. Disclaimer and Export Compliance  *  * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED  * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE  * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE,  * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY  * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY  * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A  * PARTICULAR PURPOSE.  *  * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES  * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR  * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT,  * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY  * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL  * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS  * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY  * LIMITED REMEDY.  *  * 4.3. Licensee shall not export, either directly or indirectly, any of this  * software or system incorporating such software without first obtaining any  * required license or other approval from the U. S. Department of Commerce or  * any other agency or department of the United States Government.  In the  * event Licensee exports any such software from the United States or  * re-exports any such software from a foreign destination, Licensee shall  * ensure that the distribution and export/re-export of the software is in  * compliance with all laws, regulations, orders, or other restrictions of the  * U.S. Export Administration Regulations. Licensee agrees that neither it nor  * any of its subsidiaries will export/re-export any technical data, process,  * software, or service, directly or indirectly, to any country for which the  * United States government or any agency thereof requires an export license,  * other governmental approval, or letter of assurance, without first obtaining  * such license, approval or letter.  *  *****************************************************************************/
+comment|/******************************************************************************  *  * 1. Copyright Notice  *  * Some or all of this work - Copyright (c) 1999 - 2004, Intel Corp.  * All rights reserved.  *  * 2. License  *  * 2.1. This is your license from Intel Corp. under its intellectual property  * rights.  You may have additional license terms from the party that provided  * you this software, covering your right to use that party's intellectual  * property rights.  *  * 2.2. Intel grants, free of charge, to any person ("Licensee") obtaining a  * copy of the source code appearing in this file ("Covered Code") an  * irrevocable, perpetual, worldwide license under Intel's copyrights in the  * base code distributed originally by Intel ("Original Intel Code") to copy,  * make derivatives, distribute, use and display any portion of the Covered  * Code in any form, with the right to sublicense such rights; and  *  * 2.3. Intel grants Licensee a non-exclusive and non-transferable patent  * license (with the right to sublicense), under only those claims of Intel  * patents that are infringed by the Original Intel Code, to make, use, sell,  * offer to sell, and import the Covered Code and derivative works thereof  * solely to the minimum extent necessary to exercise the above copyright  * license, and in no event shall the patent license extend to any additions  * to or modifications of the Original Intel Code.  No other license or right  * is granted directly or by implication, estoppel or otherwise;  *  * The above copyright and patent license is granted only if the following  * conditions are met:  *  * 3. Conditions  *  * 3.1. Redistribution of Source with Rights to Further Distribute Source.  * Redistribution of source code of any substantial portion of the Covered  * Code or modification with rights to further distribute source must include  * the above Copyright Notice, the above License, this list of Conditions,  * and the following Disclaimer and Export Compliance provision.  In addition,  * Licensee must cause all Covered Code to which Licensee contributes to  * contain a file documenting the changes Licensee made to create that Covered  * Code and the date of any change.  Licensee must include in that file the  * documentation of any changes made by any predecessor Licensee.  Licensee  * must include a prominent statement that the modification is derived,  * directly or indirectly, from Original Intel Code.  *  * 3.2. Redistribution of Source with no Rights to Further Distribute Source.  * Redistribution of source code of any substantial portion of the Covered  * Code or modification without rights to further distribute source must  * include the following Disclaimer and Export Compliance provision in the  * documentation and/or other materials provided with distribution.  In  * addition, Licensee may not authorize further sublicense of source of any  * portion of the Covered Code, and must include terms to the effect that the  * license from Licensee to its licensee is limited to the intellectual  * property embodied in the software Licensee provides to its licensee, and  * not to intellectual property embodied in modifications its licensee may  * make.  *  * 3.3. Redistribution of Executable. Redistribution in executable form of any  * substantial portion of the Covered Code or modification must reproduce the  * above Copyright Notice, and the following Disclaimer and Export Compliance  * provision in the documentation and/or other materials provided with the  * distribution.  *  * 3.4. Intel retains all right, title, and interest in and to the Original  * Intel Code.  *  * 3.5. Neither the name Intel nor any other trademark owned or controlled by  * Intel shall be used in advertising or otherwise to promote the sale, use or  * other dealings in products derived from or relating to the Covered Code  * without prior written authorization from Intel.  *  * 4. Disclaimer and Export Compliance  *  * 4.1. INTEL MAKES NO WARRANTY OF ANY KIND REGARDING ANY SOFTWARE PROVIDED  * HERE.  ANY SOFTWARE ORIGINATING FROM INTEL OR DERIVED FROM INTEL SOFTWARE  * IS PROVIDED "AS IS," AND INTEL WILL NOT PROVIDE ANY SUPPORT,  ASSISTANCE,  * INSTALLATION, TRAINING OR OTHER SERVICES.  INTEL WILL NOT PROVIDE ANY  * UPDATES, ENHANCEMENTS OR EXTENSIONS.  INTEL SPECIFICALLY DISCLAIMS ANY  * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A  * PARTICULAR PURPOSE.  *  * 4.2. IN NO EVENT SHALL INTEL HAVE ANY LIABILITY TO LICENSEE, ITS LICENSEES  * OR ANY OTHER THIRD PARTY, FOR ANY LOST PROFITS, LOST DATA, LOSS OF USE OR  * COSTS OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR FOR ANY INDIRECT,  * SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THIS AGREEMENT, UNDER ANY  * CAUSE OF ACTION OR THEORY OF LIABILITY, AND IRRESPECTIVE OF WHETHER INTEL  * HAS ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.  THESE LIMITATIONS  * SHALL APPLY NOTWITHSTANDING THE FAILURE OF THE ESSENTIAL PURPOSE OF ANY  * LIMITED REMEDY.  *  * 4.3. Licensee shall not export, either directly or indirectly, any of this  * software or system incorporating such software without first obtaining any  * required license or other approval from the U. S. Department of Commerce or  * any other agency or department of the United States Government.  In the  * event Licensee exports any such software from the United States or  * re-exports any such software from a foreign destination, Licensee shall  * ensure that the distribution and export/re-export of the software is in  * compliance with all laws, regulations, orders, or other restrictions of the  * U.S. Export Administration Regulations. Licensee agrees that neither it nor  * any of its subsidiaries will export/re-export any technical data, process,  * software, or service, directly or indirectly, to any country for which the  * United States government or any agency thereof requires an export license,  * other governmental approval, or letter of assurance, without first obtaining  * such license, approval or letter.  *  *****************************************************************************/
 end_comment
 
 begin_define
@@ -46,7 +46,7 @@ argument_list|)
 end_macro
 
 begin_comment
-comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToInteger  *  * PARAMETERS:  *ObjDesc        - Object to be converted.  Must be an  *                                Integer, Buffer, or String  *              WalkState       - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Convert an ACPI Object to an integer.  *  ******************************************************************************/
+comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToInteger  *  * PARAMETERS:  ObjDesc         - Object to be converted.  Must be an  *                                Integer, Buffer, or String  *              ResultDesc      - Where the new Integer object is returned  *              WalkState       - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Convert an ACPI Object to an integer.  *  ******************************************************************************/
 end_comment
 
 begin_function
@@ -341,7 +341,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToBuffer  *  * PARAMETERS:  *ObjDesc        - Object to be converted.  Must be an  *                                Integer, Buffer, or String  *              WalkState       - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Convert an ACPI Object to a Buffer  *  ******************************************************************************/
+comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToBuffer  *  * PARAMETERS:  ObjDesc         - Object to be converted.  Must be an  *                                Integer, Buffer, or String  *              ResultDesc      - Where the new buffer object is returned  *              WalkState       - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Convert an ACPI Object to a Buffer  *  ******************************************************************************/
 end_comment
 
 begin_function
@@ -710,7 +710,7 @@ for|for
 control|(
 name|j
 operator|=
-literal|1
+literal|0
 init|;
 name|j
 operator|<
@@ -741,7 +741,7 @@ block|}
 comment|/* Create the decimal digit */
 if|if
 condition|(
-name|Digit
+name|Remainder
 operator|!=
 literal|0
 condition|)
@@ -864,7 +864,7 @@ break|break;
 default|default:
 break|break;
 block|}
-comment|/*      * Since leading zeros are supressed, we must check for the case where      * the integer equals 0.      *      * Finally, null terminate the string and return the length      */
+comment|/*      * Since leading zeros are supressed, we must check for the case where      * the integer equals 0      *      * Finally, null terminate the string and return the length      */
 if|if
 condition|(
 operator|!
@@ -899,7 +899,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToString  *  * PARAMETERS:  *ObjDesc        - Object to be converted.  Must be an  *                                Integer, Buffer, or String  *              WalkState       - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Convert an ACPI Object to a string  *  ******************************************************************************/
+comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToString  *  * PARAMETERS:  ObjDesc         - Object to be converted.  Must be an  *                                  Integer, Buffer, or String  *              ResultDesc      - Where the string object is returned  *              Base            - 10 or 16  *              MaxLength       - Max length of the returned string  *              WalkState       - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Convert an ACPI Object to a string  *  ******************************************************************************/
 end_comment
 
 begin_function
@@ -930,12 +930,6 @@ name|ACPI_OPERAND_OBJECT
 modifier|*
 name|RetDesc
 decl_stmt|;
-name|UINT32
-name|i
-decl_stmt|;
-name|UINT32
-name|StringLength
-decl_stmt|;
 name|UINT8
 modifier|*
 name|NewBuf
@@ -943,6 +937,12 @@ decl_stmt|;
 name|UINT8
 modifier|*
 name|Pointer
+decl_stmt|;
+name|UINT32
+name|StringLength
+decl_stmt|;
+name|UINT32
+name|i
 decl_stmt|;
 name|ACPI_FUNCTION_TRACE_PTR
 argument_list|(
@@ -1357,7 +1357,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToTargetType  *  * PARAMETERS:  DestinationType     - Current type of the destination  *              SourceDesc          - Source object to be converted.  *              WalkState           - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Implements "implicit conversion" rules for storing an object.  *  ******************************************************************************/
+comment|/*******************************************************************************  *  * FUNCTION:    AcpiExConvertToTargetType  *  * PARAMETERS:  DestinationType     - Current type of the destination  *              SourceDesc          - Source object to be converted.  *              ResultDesc          - Where the converted object is returned  *              WalkState           - Current method state  *  * RETURN:      Status  *  * DESCRIPTION: Implements "implicit conversion" rules for storing an object.  *  ******************************************************************************/
 end_comment
 
 begin_function
@@ -1538,6 +1538,15 @@ argument_list|)
 expr_stmt|;
 break|break;
 default|default:
+name|ACPI_REPORT_ERROR
+argument_list|(
+operator|(
+literal|"Bad destination type during conversion: %X\n"
+operator|,
+name|DestinationType
+operator|)
+argument_list|)
+expr_stmt|;
 name|Status
 operator|=
 name|AE_AML_INTERNAL
@@ -1580,6 +1589,12 @@ argument_list|)
 operator|)
 argument_list|)
 expr_stmt|;
+name|ACPI_REPORT_ERROR
+argument_list|(
+argument|(
+literal|"Bad Target Type (ARGI): %X\n"
+argument|,             GET_CURRENT_ARG_TYPE (WalkState->OpInfo->RuntimeArgs))
+argument_list|)
 name|Status
 operator|=
 name|AE_AML_INTERNAL
