f80c8093165b215ec4932c1aa2976857553f8889
Dag-Erling Smorgrav <des@FreeBSD.org>
2004-03-11 18:04:03 +0000

    MFP4 (#47094): avoid bogus NULL pointer warnings when building with -DDEBUG.
    Former-commit-id: 6bdf62204de70345cd3762e86c94cb30f5717cdc

--- contrib/openpam/lib/openpam_impl.h
+++ contrib/openpam/lib/openpam_impl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2001-2003 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by ThinkSec AS and  * Network Associates Laboratories, the Security Research Division of  * Network Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035  * ("CBOSS"), as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/openpam/lib/openpam_impl.h#28 $  */
+comment|/*-  * Copyright (c) 2001-2003 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by ThinkSec AS and  * Network Associates Laboratories, the Security Research Division of  * Network Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035  * ("CBOSS"), as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote  *    products derived from this software without specific prior written  *    permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/openpam/lib/openpam_impl.h#29 $  */
 end_comment
 
 begin_ifndef
@@ -456,7 +456,7 @@ name|ENTERI
 parameter_list|(
 name|i
 parameter_list|)
-value|do { \ 	if ((i)> 0&& (i)< PAM_NUM_ITEMS) \ 		openpam_log(PAM_LOG_DEBUG, "entering: %s", _pam_item_name[i]); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "entering: %d", (i)); \ } while (0)
+value|do { \ 	int _i = (i); \ 	if (_i> 0&& _i< PAM_NUM_ITEMS) \ 		openpam_log(PAM_LOG_DEBUG, "entering: %s", _pam_item_name[_i]); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "entering: %d", _i); \ } while (0)
 end_define
 
 begin_define
@@ -466,7 +466,7 @@ name|ENTERN
 parameter_list|(
 name|n
 parameter_list|)
-value|do { \ 	openpam_log(PAM_LOG_DEBUG, "entering: %d", (n)); \ } while (0)
+value|do { \ 	int _n = (n); \ 	openpam_log(PAM_LOG_DEBUG, "entering: %d", _n); \ } while (0)
 end_define
 
 begin_define
@@ -476,7 +476,7 @@ name|ENTERS
 parameter_list|(
 name|s
 parameter_list|)
-value|do { \ 	if ((s) == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "entering: NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "entering: '%s'", (s)); \ } while (0)
+value|do { \ 	const char *_s = (s); \ 	if (_s == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "entering: NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "entering: '%s'", _s); \ } while (0)
 end_define
 
 begin_define
@@ -494,7 +494,7 @@ name|RETURNC
 parameter_list|(
 name|c
 parameter_list|)
-value|do { \ 	if ((c)>= 0&& (c)< PAM_NUM_ERRORS) \ 		openpam_log(PAM_LOG_DEBUG, "returning %s", _pam_err_name[c]); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "returning %d!", (c)); \ 	return (c); \ } while (0)
+value|do { \ 	int _c = (c); \ 	if (_c>= 0&& _c< PAM_NUM_ERRORS) \ 		openpam_log(PAM_LOG_DEBUG, "returning %s", _pam_err_name[_c]); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "returning %d!", _c); \ 	return (_c); \ } while (0)
 end_define
 
 begin_define
@@ -504,7 +504,7 @@ name|RETURNN
 parameter_list|(
 name|n
 parameter_list|)
-value|do { \ 	openpam_log(PAM_LOG_DEBUG, "returning %d", (n)); \ 	return (n); \ } while (0)
+value|do { \ 	int _n = (n); \ 	openpam_log(PAM_LOG_DEBUG, "returning %d", _n); \ 	return (_n); \ } while (0)
 end_define
 
 begin_define
@@ -514,7 +514,7 @@ name|RETURNP
 parameter_list|(
 name|p
 parameter_list|)
-value|do { \ 	if ((p) == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "returning NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "returning %p", (p)); \ 	return (p); \ } while (0)
+value|do { \ 	const void *_p = (p); \ 	if (_p == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "returning NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "returning %p", _p); \ 	return (p); \ } while (0)
 end_define
 
 begin_define
@@ -524,7 +524,7 @@ name|RETURNS
 parameter_list|(
 name|s
 parameter_list|)
-value|do { \ 	if ((s) == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "returning NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "returning '%s'", (s)); \ 	return (s); \ } while (0)
+value|do { \ 	const char *_s = (s); \ 	if (_s == NULL) \ 		openpam_log(PAM_LOG_DEBUG, "returning NULL"); \ 	else \ 		openpam_log(PAM_LOG_DEBUG, "returning '%s'", _s); \ 	return (_s); \ } while (0)
 end_define
 
 begin_else
