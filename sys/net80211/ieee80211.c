1434099cb0db631df79e17ad34e4f92930d18aff
Maxime Henrion <mux@FreeBSD.org>
2004-03-16 19:25:55 +0000

    Add explicit dependency on "ether", since we use ether_ifattach().
    Former-commit-id: 88493be4ec8f2391816e86ed5e1f4b823073d403

--- sys/net80211/ieee80211.c
+++ sys/net80211/ieee80211.c
@@ -3978,5 +3978,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|wlan
+argument_list|,
+name|ether
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
