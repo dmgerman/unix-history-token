1a4a77dd9a629f2485f2dce46893868493be9d21
David Malone <dwmalone@FreeBSD.org>
2004-03-23 12:37:02 +0000

    Add support for decoding Timespec, Timeval, Itimerval, Pollfd, Fd_set and Sigaction structures. Use these for printing the arguments to sigaction(), nanosleep(), select(), poll(), gettimeofday(), clock_gettime(), recvfrom(), getitimer() and setitimer().
    This is based on Dan's patch from the PR but I've hacked it for
    style and some other issues. While Dan has checked this patch, any
    goofs are probably my fault.
    
    (The PR also contains support for the dual return values of pipe().
    These will follow once I've ported that support to platforms other
    than i386.)
    
    PR:		52190
    Submitted by:	Dan Nelson <dnelson@allantgroup.com>
    
    Former-commit-id: 80ab60580d152d4ec6c29c269caade4403c77922

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * See i386-fbsd.c for copyright and license terms.  *  * System call arguments come in several flavours:  * Hex -- values that should be printed in hex (addresses)  * Octal -- Same as above, but octal  * Int -- normal integer values (file descriptors, for example)  * String -- pointers to sensible data.  Note that we treat read() and  *	write() arguments as such, even though they may *not* be  *	printable data.  * Ptr -- pointer to some specific structure.  Just print as hex for now.  * Quad -- a double-word value.  e.g., lseek(int, offset_t, int)  * Stat -- a pointer to a stat buffer.  Currently unused.  * Ioctl -- an ioctl command.  Woefully limited.  *  * In addition, the pointer types (String, Ptr) may have OUT masked in --  * this means that the data is set on *return* from the system call -- or  * IN (meaning that the data is passed *into* the system call).  */
+comment|/*  * See i386-fbsd.c for copyright and license terms.  *  * System call arguments come in several flavours:  * Hex -- values that should be printed in hex (addresses)  * Octal -- Same as above, but octal  * Int -- normal integer values (file descriptors, for example)  * String -- pointers to sensible data.  Note that we treat read() and  *	write() arguments as such, even though they may *not* be  *	printable data.  * Ptr -- pointer to some specific structure.  Just print as hex for now.  * Stat -- a pointer to a stat buffer.  Currently unused.  * Ioctl -- an ioctl command.  Woefully limited.  * Quad -- a double-word value.  e.g., lseek(int, offset_t, int)  * Signal -- a signal number.  Prints the signal name (SIGxxx)  * Sockaddr -- a pointer to a struct sockaddr.  Prints symbolic AF, and IP:Port  * StringArray -- a pointer to an array of string pointers.  * Timespec -- a pointer to a struct timespec.  Prints both elements.  * Timeval -- a pointer to a struct timeval.  Prints both elements.  * Itimerval -- a pointer to a struct itimerval.  Prints all elements.  * Pollfd -- a pointer to an array of struct pollfd.  Prints .fd and .events.  * Fd_set -- a pointer to an array of fd_set.  Prints the fds that are set.  * Sigaction -- a pointer to a struct sigaction.  Prints all elements.  *  * In addition, the pointer types (String, Ptr) may have OUT masked in --  * this means that the data is set on *return* from the system call -- or  * IN (meaning that the data is passed *into* the system call).  */
 end_comment
 
 begin_comment
@@ -36,6 +36,18 @@ block|,
 name|Sockaddr
 block|,
 name|StringArray
+block|,
+name|Timespec
+block|,
+name|Timeval
+block|,
+name|Itimerval
+block|,
+name|Pollfd
+block|,
+name|Fd_set
+block|,
+name|Sigaction
 block|,
 name|Fcntl
 block|,
