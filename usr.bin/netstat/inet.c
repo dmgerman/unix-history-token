955bc596a8caf134607cf5d56330cd0e7382a4db
Dag-Erling Smorgrav <des@FreeBSD.org>
2004-03-30 09:18:15 +0000

    MFC (rev 1.61): do not print warning if kernel does not support PIM
    Former-commit-id: 07a7ecc1ff3df2b3d4140534126d8f109c88f17e

--- usr.bin/netstat/inet.c
+++ usr.bin/netstat/inet.c
@@ -3656,6 +3656,12 @@ operator|<
 literal|0
 condition|)
 block|{
+if|if
+condition|(
+name|errno
+operator|!=
+name|ENOENT
+condition|)
 name|warn
 argument_list|(
 literal|"sysctl: net.inet.pim.stats"
