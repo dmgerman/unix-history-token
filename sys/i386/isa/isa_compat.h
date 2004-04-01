5f256bc04914168c903bb3800a4e00aeec35d277
Roman Kurakin <rik@FreeBSD.org>
2004-04-01 16:57:00 +0000

    MFC: New driver doesn't need old compatibily scheme.
    Approved by:	imp (mentor)
    
    
    Former-commit-id: 86b69f017f5909a7e5a2a0aaa342db76701e8ec4

--- sys/i386/isa/isa_compat.h
+++ sys/i386/isa/isa_compat.h
@@ -18,12 +18,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"cx.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"el.h"
 end_include
 
@@ -272,14 +266,6 @@ specifier|extern
 name|struct
 name|isa_driver
 name|wdcdriver
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|struct
-name|isa_driver
-name|cxdriver
 decl_stmt|;
 end_decl_stmt
 
@@ -866,20 +852,6 @@ name|INTR_TYPE_NET
 block|,
 operator|&
 name|lncdriver
-block|}
-block|,
-endif|#
-directive|endif
-if|#
-directive|if
-name|NCX
-operator|>
-literal|0
-block|{
-name|INTR_TYPE_NET
-block|,
-operator|&
-name|cxdriver
 block|}
 block|,
 endif|#
