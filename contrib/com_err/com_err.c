307a88af222cb7613fdeb90f522557fdabf85439
Jacques Vidrine <nectar@FreeBSD.org>
2004-04-03 21:08:58 +0000

    Import libcom_err from Heimdal 0.6.1.
    Former-commit-id: f054c1df3f429775394d0b2d5dbdb14c8a4cd969

--- contrib/com_err/com_err.c
+++ contrib/com_err/com_err.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1997, 1998 Kungliga Tekniska Högskolan  * (Royal Institute of Technology, Stockholm, Sweden).   * All rights reserved.   *  * Redistribution and use in source and binary forms, with or without   * modification, are permitted provided that the following conditions   * are met:   *  * 1. Redistributions of source code must retain the above copyright   *    notice, this list of conditions and the following disclaimer.   *  * 2. Redistributions in binary form must reproduce the above copyright   *    notice, this list of conditions and the following disclaimer in the   *    documentation and/or other materials provided with the distribution.   *  * 3. All advertising materials mentioning features or use of this software   *    must display the following acknowledgement:   *      This product includes software developed by Kungliga Tekniska   *      Högskolan and its contributors.   *  * 4. Neither the name of the Institute nor the names of its contributors   *    may be used to endorse or promote products derived from this software   *    without specific prior written permission.   *  * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND   * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE   * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL   * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY   * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF   * SUCH DAMAGE.   */
+comment|/*  * Copyright (c) 1997 - 2002 Kungliga Tekniska Högskolan  * (Royal Institute of Technology, Stockholm, Sweden).   * All rights reserved.   *  * Redistribution and use in source and binary forms, with or without   * modification, are permitted provided that the following conditions   * are met:   *  * 1. Redistributions of source code must retain the above copyright   *    notice, this list of conditions and the following disclaimer.   *  * 2. Redistributions in binary form must reproduce the above copyright   *    notice, this list of conditions and the following disclaimer in the   *    documentation and/or other materials provided with the distribution.   *  * 3. Neither the name of the Institute nor the names of its contributors   *    may be used to endorse or promote products derived from this software   *    without specific prior written permission.   *  * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND   * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE   * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL   * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY   * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF   * SUCH DAMAGE.   */
 end_comment
 
 begin_ifdef
@@ -18,7 +18,7 @@ end_include
 begin_expr_stmt
 name|RCSID
 argument_list|(
-literal|"$Id: com_err.c,v 1.13 1999/03/12 15:17:08 bg Exp $"
+literal|"$Id: com_err.c,v 1.18 2002/03/10 23:07:01 assar Exp $"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
@@ -63,6 +63,8 @@ name|struct
 name|et_list
 modifier|*
 name|_et_list
+init|=
+name|NULL
 decl_stmt|;
 end_decl_stmt
 
@@ -101,6 +103,23 @@ name|p
 operator|==
 name|NULL
 condition|)
+block|{
+if|if
+condition|(
+name|code
+operator|<
+literal|0
+condition|)
+name|sprintf
+argument_list|(
+name|msg
+argument_list|,
+literal|"Unknown error %ld"
+argument_list|,
+name|code
+argument_list|)
+expr_stmt|;
+else|else
 name|p
 operator|=
 name|strerror
@@ -108,6 +127,7 @@ argument_list|(
 name|code
 argument_list|)
 expr_stmt|;
+block|}
 if|if
 condition|(
 name|p
@@ -197,6 +217,43 @@ literal|0
 return|;
 block|}
 end_function
+
+begin_function_decl
+specifier|static
+name|void
+name|default_proc
+parameter_list|(
+specifier|const
+name|char
+modifier|*
+name|whoami
+parameter_list|,
+name|long
+name|code
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|fmt
+parameter_list|,
+name|va_list
+name|args
+parameter_list|)
+function_decl|__attribute__
+parameter_list|(
+function_decl|(__format__
+parameter_list|(
+name|__printf__
+parameter_list|,
+function_decl|3
+operator|,
+function_decl|0
+end_function_decl
+
+begin_empty_stmt
+unit|)))
+empty_stmt|;
+end_empty_stmt
 
 begin_function
 specifier|static
@@ -557,6 +614,65 @@ operator|(
 name|buf
 operator|)
 return|;
+block|}
+end_function
+
+begin_function
+name|void
+name|add_to_error_table
+parameter_list|(
+name|struct
+name|et_list
+modifier|*
+name|new_table
+parameter_list|)
+block|{
+name|struct
+name|et_list
+modifier|*
+name|et
+decl_stmt|;
+for|for
+control|(
+name|et
+operator|=
+name|_et_list
+init|;
+name|et
+condition|;
+name|et
+operator|=
+name|et
+operator|->
+name|next
+control|)
+block|{
+if|if
+condition|(
+name|et
+operator|->
+name|table
+operator|->
+name|base
+operator|==
+name|new_table
+operator|->
+name|table
+operator|->
+name|base
+condition|)
+return|return;
+block|}
+name|new_table
+operator|->
+name|next
+operator|=
+name|_et_list
+expr_stmt|;
+name|_et_list
+operator|=
+name|new_table
+expr_stmt|;
 block|}
 end_function
 
