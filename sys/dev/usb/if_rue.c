99daec289de776b9ca564d39deb56f2f858a01af
Ian Dowse <iedowse@FreeBSD.org>
2004-04-06 19:32:00 +0000

    Use the correct flag for mbuf allocations (M_DONTWAIT, not M_NOWAIT).
    Former-commit-id: 1826de03510fbd3196b1a01c51b2e59503b8b062

--- sys/dev/usb/if_rue.c
+++ sys/dev/usb/if_rue.c
@@ -3580,7 +3580,7 @@ name|MGETHDR
 argument_list|(
 name|m_new
 argument_list|,
-name|M_NOWAIT
+name|M_DONTWAIT
 argument_list|,
 name|MT_DATA
 argument_list|)
@@ -3612,7 +3612,7 @@ name|MCLGET
 argument_list|(
 name|m_new
 argument_list|,
-name|M_NOWAIT
+name|M_DONTWAIT
 argument_list|)
 expr_stmt|;
 if|if
