7c8b11f3a44044aab189c67b9914ef661ba31916
Tim J. Robbins <tjr@FreeBSD.org>
2004-04-07 11:02:51 +0000

    Reset internal conversion state after triggering conversion errors; the state is undefined according to the standards, and our undefined behaviour has changed.
    Former-commit-id: 8ce41984f57100d6db319ad862b92460239d01e9

--- tools/regression/lib/libc/locale/test-mblen.c
+++ tools/regression/lib/libc/locale/test-mblen.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2002 Tim J. Robbins  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2002-2004 Tim J. Robbins  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_comment
@@ -175,6 +175,18 @@ operator|-
 literal|1
 argument_list|)
 expr_stmt|;
+name|assert
+argument_list|(
+name|mblen
+argument_list|(
+name|NULL
+argument_list|,
+literal|0
+argument_list|)
+operator|==
+literal|0
+argument_list|)
+expr_stmt|;
 comment|/* 	 * Japanese (EUC) locale. 	 */
 name|assert
 argument_list|(
@@ -286,6 +298,18 @@ operator|-
 literal|1
 argument_list|)
 expr_stmt|;
+name|assert
+argument_list|(
+name|mblen
+argument_list|(
+name|NULL
+argument_list|,
+literal|0
+argument_list|)
+operator|==
+literal|0
+argument_list|)
+expr_stmt|;
 comment|/* Incomplete character sequence (truncated double-byte). */
 name|memset
 argument_list|(
@@ -324,6 +348,18 @@ argument_list|)
 operator|==
 operator|-
 literal|1
+argument_list|)
+expr_stmt|;
+name|assert
+argument_list|(
+name|mblen
+argument_list|(
+name|NULL
+argument_list|,
+literal|0
+argument_list|)
+operator|==
+literal|0
 argument_list|)
 expr_stmt|;
 comment|/* Same as above, but complete. */
