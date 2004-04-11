fc4e482af8b635c611e8d2439394ffe8b84d97d9
Warner Losh <imp@FreeBSD.org>
2004-04-11 19:22:25 +0000

    Add note about why we're ignoring the below 1MB bit.
    Former-commit-id: b41afdce3f5934ffe3524008c7a1fbbb4eb60e9b

--- sys/dev/cardbus/cardbus_cis.c
+++ sys/dev/cardbus/cardbus_cis.c
@@ -2110,6 +2110,7 @@ expr_stmt|;
 if|#
 directive|if
 literal|0
+comment|/* 		 * XXX: It appears from a careful reading of the spec 		 * that we're not supposed to honor this when the bridge 		 * is not on the main system bus.  PCI spec doesn't appear 		 * to allow for memory ranges not listed in the bridge's 		 * decode range to be decoded.  The PC Card spec seems to 		 * indicate that this should only be done on x86 based 		 * machines, which seems to imply that on non-x86 machines 		 * the adddresses can be anywhere.  This further implies that 		 * since the hardware can do it on non-x86 machines, it should 		 * be able to do it on x86 machines.  Therefore, we can and 		 * should ignore this hint.  Furthermore, the PC Card spec 		 * recommends always allocating memory above 1MB, contradicting 		 * the other part of the PC Card spec. 		 * 		 * NetBSD ignores this bit, but it also ignores the 		 * prefetchable bit too, so that's not an indication of 		 * correctness. 		 */
 block|if (reg& TPL_BAR_REG_BELOW1MB) 			dinfo->mbelow1mb |= BARBIT(bar);
 endif|#
 directive|endif
