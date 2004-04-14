4a983b63e4bf8e8991cb1fcfdb9b1754650bc420
Nate Lawson <njl@FreeBSD.org>
2004-04-14 16:52:19 +0000

    Even though the patch has been submitted to the vendor, this file is off the vendor branch.  Once more, with feeling!
    Former-commit-id: 9d2a4837b721a4b005c60c9466ceeb27b12ad4f8

--- sys/contrib/dev/acpica/hwsleep.c
+++ sys/contrib/dev/acpica/hwsleep.c
@@ -579,13 +579,6 @@ argument_list|(
 name|ACPI_BITREG_SLEEP_ENABLE
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|SleepState
-operator|!=
-name|ACPI_STATE_S5
-condition|)
-block|{
 comment|/* Clear wake status */
 name|Status
 operator|=
@@ -633,6 +626,13 @@ name|Status
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|SleepState
+operator|!=
+name|ACPI_STATE_S5
+condition|)
+block|{
 comment|/* Disable BM arbitration */
 name|Status
 operator|=
