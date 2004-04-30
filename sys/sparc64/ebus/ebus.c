b87cae4ec217c0aa2ffc5fc50962cca2f40184cc
Thomas Moestl <tmm@FreeBSD.org>
2004-04-30 19:19:55 +0000

    Remove inclusion of opt_ebus.h.
    Former-commit-id: f71cad44bc9ef55b8050a250382dec08264081c9

--- sys/sparc64/ebus/ebus.c
+++ sys/sparc64/ebus/ebus.c
@@ -3,12 +3,6 @@ begin_comment
 comment|/*  * Copyright (c) 1999, 2000 Matthew R. Green  * Copyright (c) 2001 Thomas Moestl<tmm@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	from: NetBSD: ebus.c,v 1.26 2001/09/10 16:27:53 eeh Exp  *  * $FreeBSD$  */
 end_comment
 
-begin_include
-include|#
-directive|include
-file|"opt_ebus.h"
-end_include
-
 begin_comment
 comment|/*  * UltraSPARC 5 and beyond ebus support.  *  * note that this driver is not complete:  *	- ebus2 dma code is completely unwritten  *	- interrupt establish is written and appears to work  *	- bus map code is written and appears to work  * XXX: This is PCI specific, however, there exist SBus-to-EBus bridges...  * XXX: The EBus was designed to allow easy adaption of ISA devices to it - a  * compatability layer for ISA devices might be nice, although probably not  * easily possible because of some cruft (like in[bwl]/out[bwl] and friends).  * Additionally, the existing ISA code is limited to one ISA bus, however,  * there are machines with both ISA and EBus.  */
 end_comment
