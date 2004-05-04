f0ddd4f2f6a78c5f1ae8d87c487ec93b78ca4e9f
John Baldwin <jhb@FreeBSD.org>
2004-05-04 21:17:52 +0000

    - Create a pir0 psuedo device as a child of legacy0 if we attach a legacy   host-PCI bridge device and find a valid $PIR. - Make pci_pir_parse() private to pci_pir.c and have pir0's attach routine   call it instead of having legacy_pcib_attach() call it. - Implement suspend/resume support for the $PIR by giving pir0 a resume   method that calls the BIOS to reroute each link that was already routed   before the machine was suspended. - Dump the state of the routed flag in the links display code. - If a link's IRQ is set by a tunable, then force that link to be re-routed   the first time it is used. - Move the 'Found $PIR' message under bootverbose as the pir0 description   line lists the number of entries already.  The pir0 line also only shows   up if we are actually using the $PIR which is a bonus. - Use BUS_CONFIG_INTR() to ensure that any IRQs used by a PCI link are   set to level/low trigger/polarity.
    Former-commit-id: 02a0c0e601529c3cd773d560020a83dac69f1f4e

--- sys/i386/include/pci_cfgreg.h
+++ sys/i386/include/pci_cfgreg.h
@@ -176,15 +176,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|void
-name|pci_pir_parse
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|int
 name|pci_pir_probe
 parameter_list|(
