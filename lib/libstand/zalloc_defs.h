04d767675f2b044c163713986d95f4262762927d
Olivier Houchard <cognet@FreeBSD.org>
2004-05-14 13:32:13 +0000

    Define iaddr_t and saddr_t for arm.
    Former-commit-id: e2c2ebacd6a2df5a2faf73207a8f631178a27e94

--- lib/libstand/zalloc_defs.h
+++ lib/libstand/zalloc_defs.h
@@ -221,6 +221,40 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__arm__
+end_ifdef
+
+begin_typedef
+typedef|typedef
+name|unsigned
+name|long
+name|iaddr_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* unsigned int same size as pointer	*/
+end_comment
+
+begin_typedef
+typedef|typedef
+name|int
+name|saddr_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* signed int same size as pointer	*/
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
