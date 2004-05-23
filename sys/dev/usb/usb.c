7ced3198c8ccd9365a643f59b8d772e38e914c6b
Lukas Ertl <le@FreeBSD.org>
2004-05-23 14:01:23 +0000

    MFNetBSD 1.80; author: wiz   URL updates, from Jared Yanovich and jmc@openbsd, forwarded by the latter.
    Obtained from:  NetBSD
    
    
    Former-commit-id: e867c36d9ef4539f5f5d221846550c3d5eedfe16

--- sys/dev/usb/usb.c
+++ sys/dev/usb/usb.c
@@ -4,7 +4,7 @@ comment|/*	$NetBSD: usb.c,v 1.68 2002/02/20 20:30:12 christos Exp $	*/
 end_comment
 
 begin_comment
-comment|/* Also already merged from NetBSD:  *	$NetBSD: usb.c,v 1.70 2002/05/09 21:54:32 augustss Exp $  *	$NetBSD: usb.c,v 1.71 2002/06/01 23:51:04 lukem Exp $  *	$NetBSD: usb.c,v 1.73 2002/09/23 05:51:19 simonb Exp $  */
+comment|/* Also already merged from NetBSD:  *	$NetBSD: usb.c,v 1.70 2002/05/09 21:54:32 augustss Exp $  *	$NetBSD: usb.c,v 1.71 2002/06/01 23:51:04 lukem Exp $  *	$NetBSD: usb.c,v 1.73 2002/09/23 05:51:19 simonb Exp $  *	$NetBSD: usb.c,v 1.80 2003/11/07 17:03:25 wiz Exp $  */
 end_comment
 
 begin_include
@@ -26,7 +26,7 @@ comment|/*  * Copyright (c) 1998 The NetBSD Foundation, Inc.  * All rights reser
 end_comment
 
 begin_comment
-comment|/*  * USB specifications and other documentation can be found at  * http://www.usb.org/developers/data/ and  * http://www.usb.org/developers/index.html .  */
+comment|/*  * USB specifications and other documentation can be found at  * http://www.usb.org/developers/docs/ and  * http://www.usb.org/developers/devclass_docs/  */
 end_comment
 
 begin_include
