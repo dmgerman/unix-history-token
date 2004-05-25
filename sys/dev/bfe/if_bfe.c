453c364a0c24ea61e4e3c34b5534bb4bcf49d3ac
Dag-Erling Smorgrav <des@FreeBSD.org>
2004-05-25 11:04:01 +0000

    Set baudrate to 100 Mbps, and advertise our ability to handle extended frames (802.1q).
    Submitted by:	Steinar Haug <sthaug@nethelp.no>
    
    
    Former-commit-id: 2ac5923eb7c181334c9c16dfa863f4076aa87f01

--- sys/dev/bfe/if_bfe.c
+++ sys/dev/bfe/if_bfe.c
@@ -2063,7 +2063,7 @@ name|ifp
 operator|->
 name|if_baudrate
 operator|=
-literal|10000000
+literal|100000000
 expr_stmt|;
 name|ifp
 operator|->
@@ -2136,6 +2136,25 @@ name|sc
 operator|->
 name|bfe_stat_ch
 argument_list|)
+expr_stmt|;
+comment|/* 	 * Tell the upper layer(s) we support long frames. 	 */
+name|ifp
+operator|->
+name|if_data
+operator|.
+name|ifi_hdrlen
+operator|=
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|ether_vlan_header
+argument_list|)
+expr_stmt|;
+name|ifp
+operator|->
+name|if_capabilities
+operator||=
+name|IFCAP_VLAN_MTU
 expr_stmt|;
 comment|/* 	 * Hook interrupt last to avoid having to lock softc 	 */
 name|error
