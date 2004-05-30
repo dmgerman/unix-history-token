087ed1d01df00f1c74eebb95ba432cc7a77e0c37
Poul-Henning Kamp <phk@FreeBSD.org>
2004-05-30 20:00:41 +0000

    Add missing <sys/module.h> includes
    Former-commit-id: 1aeff83329a7c9704531d04e61058db761964622

--- sys/dev/de/if_de.c
+++ sys/dev/de/if_de.c
@@ -82,6 +82,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/module.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/eventhandler.h>
 end_include
 
--- sys/pci/if_sk.c
+++ sys/pci/if_sk.c
@@ -72,6 +72,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/module.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/socket.h>
 end_include
 
