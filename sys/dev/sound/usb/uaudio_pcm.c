b6de830f2b86e248ccced97fa3f044b5121017b2
Takanori Watanabe <takawata@FreeBSD.org>
2004-05-31 11:38:46 +0000

    Devclass have to be shared with same 'pcm' devclass, or unit management will corrupt.
    Former-commit-id: ab7fd70b1a176a54985c1d2c72308b08c047103c

--- sys/dev/sound/usb/uaudio_pcm.c
+++ sys/dev/sound/usb/uaudio_pcm.c
@@ -1547,13 +1547,6 @@ block|, }
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
-specifier|static
-name|devclass_t
-name|pcm_devclass
-decl_stmt|;
-end_decl_stmt
-
 begin_expr_stmt
 name|DRIVER_MODULE
 argument_list|(
