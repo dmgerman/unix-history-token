0bab6282552c92fba250e9b3776d2b1d4295a0a7
Joerg Wunsch <joerg@FreeBSD.org>
2004-06-01 20:18:25 +0000

    Add SVR4-compatible VTOC-style elements to the Sun label.  The FreeBSD kernel doesn't use them but sunlabel(8) shortly will, and both these files are used by sunlabel(8).
    Former-commit-id: 144f6439e204398448614d338d372653ffe46563

--- sys/sys/sun_disklabel.h
+++ sys/sys/sun_disklabel.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)sun_disklabel.h	8.1 (Berkeley) 6/11/93  *	$NetBSD: disklabel.h,v 1.2 1998/08/22 14:55:28 mrg Exp $  *  * $FreeBSD$   */
+comment|/*  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  * Copyright (c) 2004, Joerg Wunsch  *  * This software was developed by the Computer Systems Engineering group  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and  * contributed to Berkeley.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)sun_disklabel.h	8.1 (Berkeley) 6/11/93  *	$NetBSD: disklabel.h,v 1.2 1998/08/22 14:55:28 mrg Exp $  *  * $FreeBSD$   */
 end_comment
 
 begin_ifndef
@@ -47,6 +47,24 @@ name|SUN_SIZE
 value|512
 end_define
 
+begin_define
+define|#
+directive|define
+name|SUN_VTOC_VERSION
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|SUN_VTOC_SANE
+value|0x600DDEEE
+end_define
+
+begin_comment
+comment|/* SVR4-compatible VTOC is "sane". */
+end_comment
+
 begin_comment
 comment|/*  * XXX: I am actually not sure if this should be "16 sectors" or "8192 bytes".  * XXX: Considering that Sun went to the effort of getting 512 byte compatible  * XXX: CDROM drives produced my guess is that Sun computers stand little or  * XXX: even no chance of running, much less booting on !=512 byte media.  * XXX: Define this is in terms of bytes since that is easier for us.  */
 end_comment
@@ -80,6 +98,163 @@ end_struct
 
 begin_struct
 struct|struct
+name|sun_vtoc_info
+block|{
+name|u_int16_t
+name|svtoc_tag
+decl_stmt|;
+comment|/* partition tag */
+name|u_int16_t
+name|svtoc_flag
+decl_stmt|;
+comment|/* partition flags */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/* known partition tag values */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_UNASSIGNED
+value|0x00
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_BOOT
+value|0x01
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_ROOT
+value|0x02
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_SWAP
+value|0x03
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_USR
+value|0x04
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_BACKUP
+value|0x05
+end_define
+
+begin_comment
+comment|/* "c" partition, covers entire disk */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_STAND
+value|0x06
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_VAR
+value|0x07
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_HOME
+value|0x08
+end_define
+
+begin_define
+define|#
+directive|define
+name|VTOC_ALTSCTR
+value|0x09
+end_define
+
+begin_comment
+comment|/* alternate sector partition */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_CACHE
+value|0x0a
+end_define
+
+begin_comment
+comment|/* Solaris cachefs partition */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_VXVM_PUB
+value|0x0e
+end_define
+
+begin_comment
+comment|/* VxVM public region */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_VXVM_PRIV
+value|0x0f
+end_define
+
+begin_comment
+comment|/* VxVM private region */
+end_comment
+
+begin_comment
+comment|/* VTOC partition flags */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_UNMNT
+value|0x01
+end_define
+
+begin_comment
+comment|/* unmountable partition */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|VTOC_RONLY
+value|0x10
+end_define
+
+begin_comment
+comment|/* partition is read/only */
+end_comment
+
+begin_struct
+struct|struct
 name|sun_disklabel
 block|{
 name|char
@@ -88,6 +263,28 @@ index|[
 literal|128
 index|]
 decl_stmt|;
+comment|/* SVR4 VTOC information */
+name|u_int32_t
+name|sl_vtoc_vers
+decl_stmt|;
+comment|/* == SUN_VTOC_VERSION */
+name|u_int16_t
+name|sl_vtoc_nparts
+decl_stmt|;
+comment|/* == SUN_NPART */
+name|struct
+name|sun_vtoc_info
+name|sl_vtoc_map
+index|[
+name|SUN_NPART
+index|]
+decl_stmt|;
+comment|/* partition tag/flag */
+name|u_int32_t
+name|sl_vtoc_sane
+decl_stmt|;
+comment|/* == SUN_VTOC_SANE */
+comment|/* Sun label information */
 name|u_int16_t
 name|sl_rpm
 decl_stmt|;
