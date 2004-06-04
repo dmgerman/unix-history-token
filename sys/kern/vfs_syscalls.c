f77ab4e407973176ae31b3cd000ea6c49fc76432
Tim J. Robbins <tjr@FreeBSD.org>
2004-06-04 11:00:22 +0000

    Remove a stale comment.
    Former-commit-id: 2a09f22509872a449df4aeedc57d37703d0dbeb3

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -9422,7 +9422,7 @@ name|struct
 name|nameidata
 name|nd
 decl_stmt|;
-comment|/* 	 * Create and modify a temporary credential instead of one that 	 * is potentially shared.  This could also mess up socket 	 * buffer accounting which can run in an interrupt context. 	 * 	 * XXX - Depending on how "threads" are finally implemented, it 	 * may be better to explicitly pass the credential to namei() 	 * rather than to modify the potentially shared process structure. 	 */
+comment|/* 	 * Create and modify a temporary credential instead of one that 	 * is potentially shared.  This could also mess up socket 	 * buffer accounting which can run in an interrupt context. 	 */
 name|cred
 operator|=
 name|td
