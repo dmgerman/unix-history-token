91f533e9c392908bd5062dc93fbf539154829272
Warner Losh <imp@FreeBSD.org>
2004-06-13 20:17:44 +0000

    Include vm/vm_param.h to pull in KERNBASE now.  This should fix the pc98 tinderbox breakage.
    Former-commit-id: c8ebdcb96eea032c46875bcdaff0476bcbc170f9

--- sys/pc98/cbus/gdc.c
+++ sys/pc98/cbus/gdc.c
@@ -102,6 +102,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/vm_param.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/md_var.h>
 end_include
 
