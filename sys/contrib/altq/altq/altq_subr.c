ca4f8b6fd58cf1f8d1295de23ec80fdd0edc8415
Max Laier <mlaier@FreeBSD.org>
2004-06-13 22:52:38 +0000

    #if out an old leftover in the KAME code. opt_cpu.h is no longer useful here and breaks build on some arch.
    Found-by:	tinderbox
    
    Former-commit-id: f3584389f0ac10753bf77e39fd70bc8ec728af2d

--- sys/contrib/altq/altq/altq_subr.c
+++ sys/contrib/altq/altq/altq_subr.c
@@ -254,6 +254,14 @@ directive|ifdef
 name|__FreeBSD__
 end_ifdef
 
+begin_if
+if|#
+directive|if
+name|__FreeBSD__
+operator|<
+literal|3
+end_if
+
 begin_include
 include|#
 directive|include
@@ -263,6 +271,11 @@ end_include
 begin_comment
 comment|/* for FreeBSD-2.2.8 to get i586_ctr_freq */
 end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
