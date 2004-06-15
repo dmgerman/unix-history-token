2c42b1f6b7dac91183125c71edab220054a3f377
Nate Lawson <njl@FreeBSD.org>
2004-06-15 02:12:12 +0000

    We only need the devclass_find() result, not the softc.
    Former-commit-id: d223effde5d4e80562a6969bda687f2158f218cc

--- sys/i386/i386/bios.c
+++ sys/i386/i386/bios.c
@@ -2369,14 +2369,9 @@ return|return;
 comment|/* ACPI already active */
 if|if
 condition|(
-name|devclass_get_softc
-argument_list|(
 name|devclass_find
 argument_list|(
 literal|"acpi"
-argument_list|)
-argument_list|,
-literal|0
 argument_list|)
 operator|!=
 name|NULL
