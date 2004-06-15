955187514a007f06dcef5b1469b934f93f619ae6
Diomidis Spinellis <dds@FreeBSD.org>
2004-06-15 17:38:10 +0000

    Partial MFC of 1.7: added some missing defines. These are needed to MFC usr.sbin/ppp/radius.c 1.44
    Submitted by:   Michael Bretterklieber
    PR:             46555
    PR:             bin/61294
    Approved by:	ru
    
    Former-commit-id: d724d6d7581d39de51c4425517e3f70247e8fb0e

--- lib/libradius/radlib.h
+++ lib/libradius/radlib.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright 1998 Juniper Networks, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$FreeBSD$  */
+comment|/*-  * Copyright 1998 Juniper Networks, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *<<<<<<< radlib.h  *	$FreeBSD$ =======  *	$FreeBSD$>>>>>>> 1.7  */
 end_comment
 
 begin_ifndef
@@ -622,6 +622,20 @@ end_comment
 begin_define
 define|#
 directive|define
+name|RAD_ACCT_INPUT_GIGAWORDS
+value|52
+end_define
+
+begin_define
+define|#
+directive|define
+name|RAD_ACCT_OUTPUT_GIGAWORDS
+value|53
+end_define
+
+begin_define
+define|#
+directive|define
 name|RAD_CHAP_CHALLENGE
 value|60
 end_define
@@ -817,6 +831,39 @@ end_comment
 begin_define
 define|#
 directive|define
+name|RAD_EAP_MESSAGE
+value|79
+end_define
+
+begin_comment
+comment|/* Octets */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RAD_MESSAGE_AUTHENTIC
+value|80
+end_define
+
+begin_comment
+comment|/* Octets */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RAD_ACCT_INTERIM_INTERVAL
+value|85
+end_define
+
+begin_comment
+comment|/* Integer */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|RAD_NAS_IPV6_ADDRESS
 value|95
 end_define
@@ -907,6 +954,13 @@ define|#
 directive|define
 name|RAD_STOP
 value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|RAD_UPDATE
+value|3
 end_define
 
 begin_define
