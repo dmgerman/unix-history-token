4e33b343de69b3bedee6f70082cebc53b00589ea
Poul-Henning Kamp <phk@FreeBSD.org>
2004-06-21 21:59:00 +0000

    Be BURN_BRIDGES compliant
    Former-commit-id: 8549627234a9f526a0006190be19836236e8b3bf

--- sys/i4b/driver/i4b_ing.c
+++ sys/i4b/driver/i4b_ing.c
@@ -279,9 +279,7 @@ comment|/* back pointer to node */
 name|char
 name|nodename
 index|[
-name|NG_NODELEN
-operator|+
-literal|1
+name|NG_NODESIZ
 index|]
 decl_stmt|;
 comment|/* store our node name */
