bb981aea307fa098cdcea707862411a319a33a47
David Schultz <das@FreeBSD.org>
2004-06-27 01:58:12 +0000

    Update a stale comment.  The heuristic to swap processes out based on the number of pages already paged out was broken in rev 1.10 and removed in rev 1.11.
    Former-commit-id: ad226a50d71e92f10c898278333af602d31d82cc

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -3721,7 +3721,7 @@ operator|->
 name|kg_slptime
 expr_stmt|;
 block|}
-comment|/* 			 * If the process has been asleep for awhile and had 			 * most of its pages taken away already, swap it out. 			 */
+comment|/* 			 * If the pageout daemon didn't free enough pages, 			 * or if this process is idle and the system is 			 * configured to swap proactively, swap it out. 			 */
 if|if
 condition|(
 operator|(
