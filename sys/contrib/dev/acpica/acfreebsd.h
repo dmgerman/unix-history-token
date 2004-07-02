c22e8494d8da1c6dfb7c40b75a5ad89ad42434d9
Mark Santcroos <marks@FreeBSD.org>
2004-07-02 08:34:19 +0000

    Unchanged files that are off the vendor branch.
    Former-commit-id: 2830e037ed17582e7344d22d76924c16718fd073

--- sys/contrib/dev/acpica/acfreebsd.h
+++ sys/contrib/dev/acpica/acfreebsd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************  *  * Name: acfreebsd.h - OS specific defines, etc.  *       $Revision: 15 $  *  *****************************************************************************/
+comment|/******************************************************************************  *  * Name: acfreebsd.h - OS specific defines, etc.  *       $Revision: 17 $  *  *****************************************************************************/
 end_comment
 
 begin_comment
