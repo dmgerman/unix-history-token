586f077a1f85c406ad5c2a7479e8cff4f6086d00
Bruce M Simpson <bms@FreeBSD.org>
2004-07-06 03:07:50 +0000

    Style.
    Former-commit-id: 2aa1efa193a45c4ff2e8c715070e56cc1a17596c

--- sys/net/if_disc.c
+++ sys/net/if_disc.c
@@ -662,7 +662,9 @@ expr_stmt|;
 break|break;
 block|}
 return|return
+operator|(
 literal|0
+operator|)
 return|;
 block|}
 end_function
@@ -843,7 +845,9 @@ name|m
 argument_list|)
 expr_stmt|;
 return|return
+operator|(
 literal|0
+operator|)
 return|;
 block|}
 end_function
