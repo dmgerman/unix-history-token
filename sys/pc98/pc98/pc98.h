d3ac97cfccb75db5984e9b9b938fc373c114c191
Takahashi Yoshihiro <nyan@FreeBSD.org>
2004-07-08 13:47:56 +0000

    Remove obsolete defines.
    Former-commit-id: ec2167dafbe609adf8e767c6ab8c511403f41bcd

--- sys/pc98/pc98/pc98.h
+++ sys/pc98/pc98/pc98.h
@@ -95,28 +95,6 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IO_DMAPG
-value|0x021
-end_define
-
-begin_comment
-comment|/* DMA Page Registers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_COM1
-value|0x030
-end_define
-
-begin_comment
-comment|/* 8251A RS232C serial I/O (int) */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IO_SYSPORT
 value|0x031
 end_define
@@ -139,17 +117,6 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IO_LPT
-value|0x040
-end_define
-
-begin_comment
-comment|/* 8255A Printer Port */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IO_KBD
 value|0x041
 end_define
@@ -161,89 +128,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IO_NMI
-value|0x050
-end_define
-
-begin_comment
-comment|/* NMI Control */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_WAIT
-value|0x05F
-end_define
-
-begin_comment
-comment|/* WAIT 0.6 us */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_GDC1
-value|0x060
-end_define
-
-begin_comment
-comment|/* 7220 GDC Text Control */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IO_TIMER1
 value|0x071
 end_define
 
 begin_comment
 comment|/* 8253C Timer */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_SASI
-value|0x080
-end_define
-
-begin_comment
-comment|/* SASI Hard Disk Controller */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_FD1
-value|0x090
-end_define
-
-begin_comment
-comment|/* 765A 1MB FDC */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_GDC2
-value|0x0A0
-end_define
-
-begin_comment
-comment|/* 7220 GDC Graphic Control */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_CGROM
-value|0x0A1
-end_define
-
-begin_comment
-comment|/* Character ROM */
 end_comment
 
 begin_define
@@ -282,61 +172,6 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IO_FD2
-value|0x0C8
-end_define
-
-begin_comment
-comment|/* 765A 640KB FDC */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_SIO1
-value|0x0D0
-end_define
-
-begin_comment
-comment|/* MC16550II ext RS232C */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_REEST
-value|0x0F0
-end_define
-
-begin_comment
-comment|/* CPU FPU reset */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_A2OEN
-value|0x0F2
-end_define
-
-begin_comment
-comment|/* A20 enable */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_A20CT
-value|0x0F6
-end_define
-
-begin_comment
-comment|/* A20 control enable/disable */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IO_NPX
 value|0x0F8
 end_define
@@ -348,133 +183,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IO_SOUND
-value|0x188
-end_define
-
-begin_comment
-comment|/* YM2203 FM sound board */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_EGC
-value|0x4A0
-end_define
-
-begin_comment
-comment|/* 7220 GDC Graphic Control */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_SCSI
-value|0xCC0
-end_define
-
-begin_comment
-comment|/* SCSI Controller */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_SIO2
-value|0x8D0
-end_define
-
-begin_comment
-comment|/* MC16550II ext RS232C */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_BEEPF
-value|0x3FDB
-end_define
-
-begin_comment
-comment|/* beep frequency */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_MOUSE
-value|0x7FD9
-end_define
-
-begin_comment
-comment|/* mouse */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_BMS
-value|0x7FD9
-end_define
-
-begin_comment
-comment|/* Bus Mouse */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_MSE
-value|0x7FD9
-end_define
-
-begin_comment
-comment|/* Bus Mouse */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_MOUSETM
-value|0xDFBD
-end_define
-
-begin_comment
-comment|/* mouse timer */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_WD1_NEC
-value|0x640
-end_define
-
-begin_comment
-comment|/* 98note IDE Hard disk controller */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IO_WD1_EPSON
 value|0x80
 end_define
 
 begin_comment
 comment|/* 386note Hard disk controller */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_WD1
-value|IO_WD1_NEC
-end_define
-
-begin_comment
-comment|/* IDE Hard disk controller */
 end_comment
 
 begin_define
@@ -512,105 +226,6 @@ define|#
 directive|define
 name|IO_ISASIZES
 end_define
-
-begin_define
-define|#
-directive|define
-name|IO_ASCSIZE
-value|5
-end_define
-
-begin_comment
-comment|/* AmiScan GI1904-based hand scanner */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_CGASIZE
-value|16
-end_define
-
-begin_comment
-comment|/* CGA controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_DMASIZE
-value|16
-end_define
-
-begin_comment
-comment|/* 8237 DMA controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_DPGSIZE
-value|32
-end_define
-
-begin_comment
-comment|/* 74LS612 DMA page registers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_EISASIZE
-value|4096
-end_define
-
-begin_comment
-comment|/* EISA controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_FDCSIZE
-value|8
-end_define
-
-begin_comment
-comment|/* Nec765 floppy controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_GAMSIZE
-value|16
-end_define
-
-begin_comment
-comment|/* AT compatible game controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_GSCSIZE
-value|8
-end_define
-
-begin_comment
-comment|/* GeniScan GS-4500G hand scanner */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_ICUSIZE
-value|16
-end_define
-
-begin_comment
-comment|/* 8259A interrupt controllers */
-end_comment
 
 begin_define
 define|#
@@ -659,89 +274,12 @@ end_comment
 begin_define
 define|#
 directive|define
-name|IO_MDASIZE
-value|16
-end_define
-
-begin_comment
-comment|/* Monochrome display controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
 name|IO_NPXSIZE
 value|8
 end_define
 
 begin_comment
 comment|/* 80387/80487 NPX registers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_PMPSIZE
-value|2
-end_define
-
-begin_comment
-comment|/* 82347 power management peripheral */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_PSMSIZE
-value|5
-end_define
-
-begin_comment
-comment|/* 8042 Keyboard controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_RTCSIZE
-value|16
-end_define
-
-begin_comment
-comment|/* CMOS real time clock, NMI control */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_TMRSIZE
-value|16
-end_define
-
-begin_comment
-comment|/* 8253 programmable timers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_VGASIZE
-value|16
-end_define
-
-begin_comment
-comment|/* VGA controllers */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|IO_WDCSIZE
-value|8
-end_define
-
-begin_comment
-comment|/* WD compatible disk controllers */
 end_comment
 
 begin_endif
@@ -876,84 +414,6 @@ begin_comment
 comment|/* !RAM_BEGIN */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|PC98
-end_ifndef
-
-begin_comment
-comment|/* IBM-PC */
-end_comment
-
-begin_comment
-comment|/*  * Oddball Physical Memory Addresses  */
-end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|COMPAQ_RAMRELOC
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|COMPAQ_RAMRELOC
-value|0x80C00000
-end_define
-
-begin_comment
-comment|/* Compaq RAM relocation/diag */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|COMPAQ_RAMSETUP
-value|0x80C00002
-end_define
-
-begin_comment
-comment|/* Compaq RAM setup */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|WEITEK_FPU
-value|0xC0000000
-end_define
-
-begin_comment
-comment|/* WTL 2167 */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|CYRIX_EMC
-value|0xC0000000
-end_define
-
-begin_comment
-comment|/* Cyrix EMC */
-end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* !COMPAQ_RAMRELOC */
-end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_define
 define|#
 directive|define
@@ -1001,31 +461,6 @@ define|#
 directive|define
 name|M_8M
 value|0x8000
-end_define
-
-begin_comment
-comment|/*  * Obtained from NetBSD/pc98  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|MADDRUNK
-value|-1
-end_define
-
-begin_define
-define|#
-directive|define
-name|IRQUNK
-value|0
-end_define
-
-begin_define
-define|#
-directive|define
-name|DRQUNK
-value|-1
 end_define
 
 begin_endif
