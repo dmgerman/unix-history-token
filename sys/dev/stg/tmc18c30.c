ddcfdf98d40e911728d90e20c3e5b04bbeb548eb
Marcel Moolenaar <marcel@FreeBSD.org>
2004-07-10 21:14:20 +0000

    Update for the KDB framework: o  Make debugging code conditional upon KDB instead of DDB.
    Former-commit-id: 28dd00aa2c8fedde07f00c4d9ad05c6bf1a27b59

--- sys/dev/stg/tmc18c30.c
+++ sys/dev/stg/tmc18c30.c
@@ -47,12 +47,6 @@ end_expr_stmt
 begin_include
 include|#
 directive|include
-file|"opt_ddb.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/param.h>
 end_include
 
@@ -4887,7 +4881,7 @@ argument_list|)
 expr_stmt|;
 ifdef|#
 directive|ifdef
-name|DDB
+name|KDB
 if|if
 condition|(
 name|stg_debug
@@ -4901,7 +4895,7 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-comment|/* DDB */
+comment|/* KDB */
 block|}
 endif|#
 directive|endif
