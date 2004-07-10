9fc92f7c9a2815e96e5fe4097ec3f05d8f83e3af
Marcel Moolenaar <marcel@FreeBSD.org>
2004-07-10 23:05:38 +0000

    Remove obsolete prototype of kdb_trap().
    Former-commit-id: 36e5273d970ef4e994efd767454308f528d8032d

--- sys/sparc64/include/frame.h
+++ sys/sparc64/include/frame.h
@@ -260,18 +260,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|int
-name|kdb_trap
-parameter_list|(
-name|struct
-name|trapframe
-modifier|*
-name|tf
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_endif
 endif|#
 directive|endif
