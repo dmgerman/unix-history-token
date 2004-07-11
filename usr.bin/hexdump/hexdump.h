5f4dfbba236c688b248b77948583f96a41c30f52
Tim J. Robbins <tjr@FreeBSD.org>
2004-07-11 01:11:12 +0000

    Add POSIX-style support for multibyte characters to od(1): the 'c' conversion interprets input bytes as multibyte sequences and displays printable characters in the area corresponding to their first byte. The remaining bytes are shown as "**".
    Former-commit-id: fc9ff214995394e89772f0d53515cc34e108b184

--- usr.bin/hexdump/hexdump.h
+++ usr.bin/hexdump/hexdump.h
@@ -3,6 +3,12 @@ begin_comment
 comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)hexdump.h	8.1 (Berkeley) 6/6/93  * $FreeBSD$  */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<wchar.h>
+end_include
+
 begin_typedef
 typedef|typedef
 struct|struct
@@ -92,6 +98,14 @@ modifier|*
 name|nospace
 decl_stmt|;
 comment|/* no whitespace version */
+name|int
+name|mbleft
+decl_stmt|;
+comment|/* bytes left of multibyte char. */
+name|mbstate_t
+name|mbstate
+decl_stmt|;
+comment|/* conversion state */
 block|}
 name|PR
 typedef|;
@@ -358,6 +372,8 @@ modifier|*
 parameter_list|,
 name|u_char
 modifier|*
+parameter_list|,
+name|size_t
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -461,6 +477,18 @@ name|char
 modifier|*
 modifier|*
 modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|size_t
+name|peek
+parameter_list|(
+name|u_char
+modifier|*
+parameter_list|,
+name|size_t
 parameter_list|)
 function_decl|;
 end_function_decl
