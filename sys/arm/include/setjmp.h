d90d1e617b43f766e777613097838a118fcb89e4
Olivier Houchard <cognet@FreeBSD.org>
2004-07-12 21:23:58 +0000

    Protect setjmp.h with #ifndef _MACHINE_SETJMP_H_.
    Former-commit-id: c2c35c4b3ae30e8ae16182ff314af7a5ccf92787

--- sys/arm/include/setjmp.h
+++ sys/arm/include/setjmp.h
@@ -11,6 +11,18 @@ begin_comment
 comment|/*  * machine/setjmp.h: machine dependent setjmp-related information.  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_MACHINE_SETJMP_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_MACHINE_SETJMP_H_
+end_define
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -257,6 +269,15 @@ literal|1
 index|]
 typedef|;
 end_typedef
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !_MACHINE_SETJMP_H_ */
+end_comment
 
 end_unit
 
