d57fc4a4483e3ce82ca1100b90ed03b80b40e306
Warner Losh <imp@FreeBSD.org>
2004-07-13 02:42:23 +0000

    Don't depend on implicit include of machine/bus.h in sys/rman.h, but instead explicitly include it.
    Former-commit-id: 4bdb40c65908771f26829ee05420b2b8a0e9581f

--- sys/dev/fdc/fdc.c
+++ sys/dev/fdc/fdc.c
@@ -122,6 +122,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/bus.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/clock.h>
 end_include
 
--- sys/dev/fdc/fdc_isa.c
+++ sys/dev/fdc/fdc_isa.c
@@ -62,6 +62,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/bus.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<dev/fdc/fdcvar.h>
 end_include
 
