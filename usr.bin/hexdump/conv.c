de21a28b1486e52e80801e1127f9a55e3f5a243f
Johan Karlsson <johan@FreeBSD.org>
2004-07-16 11:07:07 +0000

    Include <string.h> to get memset and strcmp prototype.
    Sort includes.
    
    This is now WARNS=2 clean, bump WARNS to keep it clean.
    
    
    Former-commit-id: a1dbaa068ad1da64cd3e3fbddddddb2debeb787b

--- usr.bin/hexdump/conv.c
+++ usr.bin/hexdump/conv.c
@@ -58,12 +58,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stdio.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<ctype.h>
 end_include
 
@@ -76,7 +70,19 @@ end_include
 begin_include
 include|#
 directive|include
+file|<stdio.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<stdlib.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<string.h>
 end_include
 
 begin_include
