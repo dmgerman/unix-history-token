8a6d02af1a020f8dcf6eaddd515e4fe27c58af54
Alexander Kabaev <kan@FreeBSD.org>
2004-07-28 04:23:38 +0000

    -O0 -O1 should optimize alignment for time, not size. Moved from toplev.c.
    Former-commit-id: a95b533cb94b99b3790ccb36c29f21d024ffa6e2

--- contrib/gcc/opts.c
+++ contrib/gcc/opts.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* Command line option handling.    Copyright (C) 2002, 2003, 2004 Free Software Foundation, Inc.    Contributed by Neil Booth.  This file is part of GCC.  GCC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GCC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GCC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -2322,10 +2326,6 @@ expr_stmt|;
 block|}
 if|if
 condition|(
-name|optimize
-operator|<
-literal|2
-operator|||
 name|optimize_size
 condition|)
 block|{
