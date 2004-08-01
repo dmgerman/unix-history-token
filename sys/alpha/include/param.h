d053542dcd31da36b1c8a14a28c389ed1fb1f169
Scott Long <scottl@FreeBSD.org>
2004-08-01 14:31:45 +0000

    Turn off PREEMPTION by default while it gets debugged.  It's been causing 4 weeks of problems including deadlocks and instant panics.  Note that the real bugs are likely in the scheduler.
    Former-commit-id: 590c947b3e0a2df898d3e7ee1baa2f81e201f75a

--- sys/alpha/include/param.h
+++ sys/alpha/include/param.h
@@ -346,11 +346,26 @@ begin_comment
 comment|/* increment of stack/NBPG */
 end_comment
 
+begin_comment
+comment|/* PREEMPTION exposes scheduler bugs that need to be fixed. */
+end_comment
+
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
 begin_define
 define|#
 directive|define
 name|PREEMPTION
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
