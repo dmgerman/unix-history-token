1bcb40016145c3a13094740d43e79ae05b696ec4
Mark Murray <markm@FreeBSD.org>
2004-08-02 20:42:28 +0000

    Add module versions.
    Former-commit-id: bb4fd8220e5e9cdf01adefbd9a6218a68a6d2b3d

--- sys/dev/random/randomdev.c
+++ sys/dev/random/randomdev.c
@@ -915,5 +915,15 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|random
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
