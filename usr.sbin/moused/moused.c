d11a961eb8581d7ef8d4ba8b2c19b9996991872c
Philip Paeps <philip@FreeBSD.org>
2004-08-03 18:43:45 +0000

    Teach moused about Synaptics touchpads.
    While I'm here, document the existence of the '-l' option, which allows
    one to use moused to use psm in some more interesting ways.
    
    Approved by:	njl (mentor)
    
    
    Former-commit-id: 3dcea8add9a0280f5248f7a1a3cc0162fc3a6374

--- usr.sbin/moused/moused.c
+++ usr.sbin/moused/moused.c
@@ -756,6 +756,12 @@ name|MOUSE_MODEL_4DPLUS
 block|}
 block|,
 block|{
+literal|"Synaptics Touchpad"
+block|,
+name|MOUSE_MODEL_SYNAPTICS
+block|}
+block|,
+block|{
 literal|"generic"
 block|,
 name|MOUSE_MODEL_GENERIC
@@ -5574,7 +5580,7 @@ literal|"usage: moused [-DRcdfs] [-I file] [-F rate] [-r resolution] [-S baudrat
 argument_list|,
 literal|"              [-a X [,Y]] [-C threshold] [-m N=M] [-w N] [-z N]"
 argument_list|,
-literal|"              [-t<mousetype>] [-3 [-E timeout]] -p<port>"
+literal|"              [-t<mousetype>] [-l level] [-3 [-E timeout]] -p<port>"
 argument_list|,
 literal|"       moused [-d] -i<port|if|type|model|all> -p<port>"
 argument_list|)
