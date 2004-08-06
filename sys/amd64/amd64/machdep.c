a299c92b1068460648351c6b6436a2d340733a19
Mark Murray <markm@FreeBSD.org>
2004-08-06 07:20:32 +0000

    MFi386: sort out the mem device. Grrrr.
    Former-commit-id: 8e93df71f1d4cd5d396ddf7caac1550d94e771e5

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -158,6 +158,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/memrange.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/mutex.h>
 end_include
 
@@ -710,6 +716,13 @@ begin_decl_stmt
 name|struct
 name|mtx
 name|icu_lock
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|mem_range_softc
+name|mem_range_softc
 decl_stmt|;
 end_decl_stmt
 
