fb6b2b223658db7516002d92dc077a9087ba350f
David Xu <davidxu@FreeBSD.org>
2004-08-08 22:46:22 +0000

    Initialize thread_db module.
    Former-commit-id: 2fe5f6fe4e93b3f23351ff6f7d85a4a496a40e99

--- gnu/usr.bin/gdb/arch/i386/init.c
+++ gnu/usr.bin/gdb/arch/i386/init.c
@@ -762,6 +762,13 @@ end_decl_stmt
 begin_decl_stmt
 specifier|extern
 name|initialize_file_ftype
+name|_initialize_thread_db
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+name|initialize_file_ftype
 name|_initialize_tui_hooks
 decl_stmt|;
 end_decl_stmt
@@ -867,6 +874,9 @@ name|_initialize_gcore
 argument_list|()
 expr_stmt|;
 name|_initialize_fbsd_proc
+argument_list|()
+expr_stmt|;
+name|_initialize_thread_db
 argument_list|()
 expr_stmt|;
 name|_initialize_remote
