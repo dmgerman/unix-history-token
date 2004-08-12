ad1fc6a404ae3b041bb76b82d1fc0a08abce5e6d
Max Laier <mlaier@FreeBSD.org>
2004-08-12 13:54:50 +0000

    Loopback fix from Henning Brauer: 	skip over interface addresses without IFA_ROUTE, fixes some issue 	with pppd
    PR:	misc/69954
    
    Former-commit-id: dece0ca9d5c591c801fa284b11e2abb551c64954

--- sys/contrib/pf/net/pf_if.c
+++ sys/contrib/pf/net/pf_if.c
@@ -8,6 +8,10 @@ comment|/*	$OpenBSD: pf_if.c,v 1.11 2004/03/15 11:38:23 cedric Exp $ */
 end_comment
 
 begin_comment
+comment|/* add	$OpenBSD: pf_if.c,v 1.19 2004/08/11 12:06:44 henning Exp $ */
+end_comment
+
+begin_comment
 comment|/*  * Copyright (c) 2001 Daniel Hartmeier  * Copyright (c) 2003 Cedric Berger  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  *    - Redistributions of source code must retain the above copyright  *      notice, this list of conditions and the following disclaimer.  *    - Redistributions in binary form must reproduce the above  *      copyright notice, this list of conditions and the following  *      disclaimer in the documentation and/or other materials provided  *      with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE  * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
@@ -129,6 +133,12 @@ begin_include
 include|#
 directive|include
 file|<net/if_types.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<net/route.h>
 end_include
 
 begin_include
@@ -3314,6 +3324,18 @@ operator|&&
 name|af
 operator|!=
 name|AF_INET6
+condition|)
+continue|continue;
+if|if
+condition|(
+operator|!
+operator|(
+name|ia
+operator|->
+name|ifa_flags
+operator|&
+name|IFA_ROUTE
+operator|)
 condition|)
 continue|continue;
 if|if
