589cb868bafdba0b5c0f1cbb1f455866a7b20b1d
Warner Losh <imp@FreeBSD.org>
2004-08-23 05:23:16 +0000

    ifndef PC98 the opt_agp.h files, per extensive discussions with nyan-san, obrien, rwatson, and scottl.
    Approved by: re
    
    
    Former-commit-id: 35f082235beee036333ff3d65b15a7434e74e4f7

--- sys/pci/agp_via.c
+++ sys/pci/agp_via.c
@@ -23,11 +23,22 @@ directive|include
 file|"opt_bus.h"
 end_include
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|PC98
+end_ifndef
+
 begin_include
 include|#
 directive|include
 file|"opt_agp.h"
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_include
 include|#
