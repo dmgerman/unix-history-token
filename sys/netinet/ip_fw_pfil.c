cafb0b2b40048bf5123558aa31f9ed6125e081fe
Ruslan Ermilov <ru@FreeBSD.org>
2004-08-26 14:18:30 +0000

    Revert the last change to sys/modules/ipfw/Makefile and fix a standalone module build in a better way.
    Silence from:	andre
    MFC after:	3 days
    
    
    Former-commit-id: 91f0f0231729794e86780b593ccb18eaf76e9781

--- sys/netinet/ip_fw_pfil.c
+++ sys/netinet/ip_fw_pfil.c
@@ -3,6 +3,16 @@ begin_comment
 comment|/*  * Copyright (c) 2004 Andre Oppermann, Internet Business Solutions AG  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
+begin_if
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|KLD_MODULE
+argument_list|)
+end_if
+
 begin_include
 include|#
 directive|include
@@ -60,12 +70,6 @@ directive|if
 operator|!
 name|defined
 argument_list|(
-name|KLD_MODULE
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
 name|PFIL_HOOKS
 argument_list|)
 end_if
@@ -75,6 +79,11 @@ error|#
 directive|error
 error|IPFIREWALL requires PFIL_HOOKS.
 end_error
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_endif
 endif|#
