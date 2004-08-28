49c4a405e163620a7a160f6c4abad8482d8702fd
Tim Kientzle <kientzle@FreeBSD.org>
2004-08-28 03:13:05 +0000

    Recognize and skip 'x' and 'g' pax extension entries.  In particular, this avoids the creation of "PaxHeader" dirs when unpacking pax-format tar archives such as those written by bsdtar.
    MFC after: 3 days
    
    
    Former-commit-id: b7e3a3e1e8240c8e48c2380ce3416cf971b39db9

--- contrib/cpio/tar.c
+++ contrib/cpio/tar.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/* tar.c - read in write tar headers for cpio    Copyright (C) 1992 Free Software Foundation, Inc.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 end_comment
 
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -1408,6 +1412,70 @@ operator|->
 name|c_mode
 operator||=
 name|CP_IFREG
+expr_stmt|;
+break|break;
+case|case
+literal|'x'
+case|:
+case|case
+literal|'g'
+case|:
+comment|/* Ignore pax 'x' and 'g' extension entries. */
+comment|/* Skip body of this entry. */
+while|while
+condition|(
+name|file_hdr
+operator|->
+name|c_filesize
+operator|>
+literal|0
+condition|)
+block|{
+name|tape_buffered_read
+argument_list|(
+operator|(
+operator|(
+name|char
+operator|*
+operator|)
+operator|&
+name|tar_rec
+operator|)
+argument_list|,
+name|in_des
+argument_list|,
+name|TARRECORDSIZE
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|file_hdr
+operator|->
+name|c_filesize
+operator|>
+name|TARRECORDSIZE
+condition|)
+name|file_hdr
+operator|->
+name|c_filesize
+operator|-=
+name|TARRECORDSIZE
+expr_stmt|;
+else|else
+name|file_hdr
+operator|->
+name|c_filesize
+operator|=
+literal|0
+expr_stmt|;
+block|}
+comment|/* Read next header and return that instead. */
+name|read_in_tar_header
+argument_list|(
+name|file_hdr
+argument_list|,
+name|in_des
+argument_list|)
 expr_stmt|;
 break|break;
 block|}
