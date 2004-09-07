145bb42bd0d465780ea9dfeaf6f6e8e25ea84b48
Robert N. M. Watson <rwatson@FreeBSD.org>
2004-09-07 23:29:26 +0000

    Merge nd6.c:1.45 to RELENG_5:
      date: 2004/09/05 17:27:54;  author: rwatson;  state: Exp;  lines: +1 -0
      Call callout_init() on nd6_slowtimo_ch before setting it going; otherwise,
      the flags field will be improperly initialized resulting in inconsistent
      operation (sometimes with Giant, sometimes without, et al).
    
      RELENG_5 candidate.
    
    Approved by:	re (scottl)
    
    
    Former-commit-id: 241d34cf1082b6826329916430cf98174d4825aa

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -616,6 +616,14 @@ operator|=
 literal|1
 expr_stmt|;
 comment|/* start timer */
+name|callout_init
+argument_list|(
+operator|&
+name|nd6_slowtimo_ch
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
 name|callout_reset
 argument_list|(
 operator|&
