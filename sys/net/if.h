e2299e00246acf765c722b2f6ee26547a8d34a7d
Brooks Davis <brooks@FreeBSD.org>
2004-09-07 23:21:10 +0000

    MFC: if.c:1.202 if.h:1.90 ifconfig.c:1.107
    Use a spare byte in struct if_data to store the structure size without
    increasing it.  Add code to ifconfig to use this size to find the
    sockaddr_dl after the struct if_data in the routing message.  This
    allows struct if_data to grow (up to 255 bytes) without breaking
    ifconfig.
    
    Approved by:	re (scottl)
    
    
    Former-commit-id: d8187fc0891750297d231878a625602bed1d3750

--- sys/net/if.h
+++ sys/net/if.h
@@ -184,6 +184,10 @@ name|u_char
 name|ifi_xmitquota
 decl_stmt|;
 comment|/* polling quota for xmit intrs */
+name|u_char
+name|ifi_datalen
+decl_stmt|;
+comment|/* length of this data struct */
 name|u_long
 name|ifi_mtu
 decl_stmt|;
