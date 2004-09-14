6033afc40243a1174cc9ea18a002d9e40adf696e
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2004-09-14 15:33:22 +0000

    - Skip providers with 0 sectorsize. - Make kern.geom.concat.debug sysctl tunable.
    Approved by:	re (scottl)
    
    
    Former-commit-id: d67ca446930a1b75ce4f10ed3746f613a9d713bd

--- sys/geom/concat/g_concat.c
+++ sys/geom/concat/g_concat.c
@@ -132,6 +132,17 @@ decl_stmt|;
 end_decl_stmt
 
 begin_expr_stmt
+name|TUNABLE_INT
+argument_list|(
+literal|"kern.geom.concat.debug"
+argument_list|,
+operator|&
+name|g_concat_debug
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|SYSCTL_UINT
 argument_list|(
 name|_kern_geom_concat
@@ -2706,6 +2717,20 @@ operator|->
 name|name
 argument_list|)
 expr_stmt|;
+comment|/* Skip providers with 0 sectorsize. */
+if|if
+condition|(
+name|pp
+operator|->
+name|sectorsize
+operator|==
+literal|0
+condition|)
+return|return
+operator|(
+name|NULL
+operator|)
+return|;
 name|gp
 operator|=
 name|g_new_geomf
