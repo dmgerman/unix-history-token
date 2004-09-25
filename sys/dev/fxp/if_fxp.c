1c454b83aa797db72434231ed274ebc11071b536
Maxime Henrion <mux@FreeBSD.org>
2004-09-25 22:21:09 +0000

    MFC revision 1.218:   Add support for the fxp(4) based card in ICH6 (i915) chipsets.
      Submitted by:   Mike Tancsa <mike@sentex.net>
    
    Approved by:	re@ (scottl)
    
    
    Former-commit-id: be9e965912e3c1364239a5d24add87cfd7aa6db9

--- sys/dev/fxp/if_fxp.c
+++ sys/dev/fxp/if_fxp.c
@@ -622,6 +622,15 @@ literal|"Intel 82551QM Pro/100 M Mobile Connection"
 block|}
 block|,
 block|{
+literal|0x1064
+block|,
+operator|-
+literal|1
+block|,
+literal|"Intel 82562EZ (ICH6)"
+block|}
+block|,
+block|{
 literal|0x1209
 block|,
 operator|-
