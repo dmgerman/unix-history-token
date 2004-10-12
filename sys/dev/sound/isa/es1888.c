e9da4429ed3af6b35997f323e3c640580ddc7ca9
Nate Lawson <njl@FreeBSD.org>
2004-10-12 01:56:03 +0000

    * Remove the acpi attachment from the es1888.  It has an identify method that conjures up the device node so it isn't true PNP.  Noticed by jhb@.
    * Add an attachment for esscontrol since it too uses ISA_PNP_PROBE.
    
    * Move an attachment from snd_mss to snd_pnpmss.  The latter is the real
      PNP user.
    
    
    Former-commit-id: 6c972a49ac9a575c1191ff9207fcf2e29bdff48f

--- sys/dev/sound/isa/es1888.c
+++ sys/dev/sound/isa/es1888.c
@@ -605,24 +605,6 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
-name|DRIVER_MODULE
-argument_list|(
-name|snd_es1888
-argument_list|,
-name|acpi
-argument_list|,
-name|es1888_driver
-argument_list|,
-name|pcm_devclass
-argument_list|,
-literal|0
-argument_list|,
-literal|0
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_expr_stmt
 name|MODULE_DEPEND
 argument_list|(
 name|snd_es1888
