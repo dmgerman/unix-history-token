287a7c32f04083c18aa0cb5861b460f829889dc6
Nate Lawson <njl@FreeBSD.org>
2004-10-13 07:27:21 +0000

    If flags are present, print them like ISA does.
    MFC after:	1 day
    
    
    Former-commit-id: b8108c659d0d1ebddfb252884e273d8ecfe3a844

--- sys/dev/acpica/acpi.c
+++ sys/dev/acpica/acpi.c
@@ -3130,6 +3130,25 @@ argument_list|,
 name|child
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|device_get_flags
+argument_list|(
+name|child
+argument_list|)
+condition|)
+name|retval
+operator|+=
+name|printf
+argument_list|(
+literal|" flags %#x"
+argument_list|,
+name|device_get_flags
+argument_list|(
+name|child
+argument_list|)
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 name|retval
