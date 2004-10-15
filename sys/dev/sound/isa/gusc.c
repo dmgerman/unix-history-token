8b5357df6d71b0b5d59af3b832f0101fd45965e8
Nate Lawson <njl@FreeBSD.org>
2004-10-15 05:14:10 +0000

    MFC:  Add acpi attachments for the PNP devices.
    Approved by:	re (kensmith)
    
    
    Former-commit-id: 4362a54328be7d65e037c993f681eae1e4f0d7a4

--- sys/dev/sound/isa/gusc.c
+++ sys/dev/sound/isa/gusc.c
@@ -3584,6 +3584,24 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
+name|DRIVER_MODULE
+argument_list|(
+name|snd_gusc
+argument_list|,
+name|acpi
+argument_list|,
+name|gusc_driver
+argument_list|,
+name|gusc_devclass
+argument_list|,
+literal|0
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|MODULE_DEPEND
 argument_list|(
 name|snd_gusc
