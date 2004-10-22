f98caf219239e8c62c4a99a763fa4aa7e629a628
SUZUKI Shinsuke <suz@FreeBSD.org>
2004-10-22 07:31:12 +0000

    removed compilation warning
    PR:	bin/71624
    
    Former-commit-id: 172d9620c75ec039ca487d2af7654265650bb3c8

--- usr.sbin/rtadvd/config.c
+++ usr.sbin/rtadvd/config.c
@@ -128,6 +128,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<search.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<stdlib.h>
 end_include
 
--- usr.sbin/rtadvd/timer.c
+++ usr.sbin/rtadvd/timer.c
@@ -44,6 +44,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<search.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"timer.h"
 end_include
 
