e5566a4ea7c4b835352d1e38dfd7650f81d38f19
Yar Tikhiy <yar@FreeBSD.org>
2004-11-13 13:42:43 +0000

    Using off_t to pass a block size is obvious overkill. The size_t type is better suited for that, particularly because the "blksize" argument is to be passed to malloc() and read(). On 64-bit archs it's more to a style issue, but the good style of coding in C is also important.
    Former-commit-id: a18369b1399f475e36efe9a0a1e9dec616e8d09c

--- libexec/ftpd/ftpd.c
+++ libexec/ftpd/ftpd.c
@@ -1325,7 +1325,7 @@ parameter_list|,
 name|FILE
 modifier|*
 parameter_list|,
-name|off_t
+name|size_t
 parameter_list|,
 name|off_t
 parameter_list|,
@@ -9742,7 +9742,7 @@ name|FILE
 modifier|*
 name|outstr
 parameter_list|,
-name|off_t
+name|size_t
 name|blksize
 parameter_list|,
 name|off_t
