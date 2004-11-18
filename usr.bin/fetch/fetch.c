e2121b8d1d4837de58c3f7f222ffceb149252f26
Colin Percival <cperciva@FreeBSD.org>
2004-11-18 12:06:51 +0000

    FreeBSD-SA-04:16.fetch: MFC revision 1.75 of src/usr.bin/fetch/fetch.c Bump newvers.sh and document in UPDATING.
    Approved by:	so
    
    
    Former-commit-id: 0d4ecd6227f9a818c3611af674cccbe768d25959

--- usr.bin/fetch/fetch.c
+++ usr.bin/fetch/fetch.c
@@ -2551,6 +2551,14 @@ operator|-
 name|count
 operator|<
 name|B_size
+operator|&&
+name|us
+operator|.
+name|size
+operator|-
+name|count
+operator|>=
+literal|0
 condition|)
 name|size
 operator|=
