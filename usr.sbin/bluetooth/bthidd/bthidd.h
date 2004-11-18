08beafe1fd0dea49c24391a5be0784b1a23f6b3f
Maksim Yevmenkin <emax@FreeBSD.org>
2004-11-18 18:05:15 +0000

    Check in updated bthidd(8). This is still work in progress.
    Former-commit-id: 1c33c4fc89cd58ce748b1ee6fb824eb823d09fb9

--- usr.sbin/bluetooth/bthidd/bthidd.h
+++ usr.sbin/bluetooth/bthidd/bthidd.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * bthidd.h  *  * Copyright (c) 2004 Maksim Yevmenkin<m_evmenkin@yahoo.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: bthidd.h,v 1.4 2004/02/26 21:44:20 max Exp $  * $FreeBSD$  */
+comment|/*  * bthidd.h  *  * Copyright (c) 2004 Maksim Yevmenkin<m_evmenkin@yahoo.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: bthidd.h,v 1.6 2004/11/17 21:59:42 max Exp $  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -49,13 +49,32 @@ name|cons
 decl_stmt|;
 comment|/* /dev/consolectl */
 name|int
+name|vkbd
+decl_stmt|;
+comment|/* /dev/vkbdctl */
+name|char
+specifier|const
+modifier|*
+name|script
+decl_stmt|;
+comment|/* keyboard switching script */
+name|int
+name|windex
+decl_stmt|;
+comment|/* wired keyboard index */
+name|bitstr_t
+modifier|*
+name|keys
+decl_stmt|;
+comment|/* pressed keys map */
+name|int
 name|ctrl
 decl_stmt|;
 comment|/* control channel (listen) */
 name|int
 name|intr
 decl_stmt|;
-comment|/* interrupt channel (listen) */
+comment|/* intr. channel (listen) */
 name|int
 name|maxfd
 decl_stmt|;
@@ -136,6 +155,11 @@ define|#
 directive|define
 name|OPEN
 value|3
+name|bitstr_t
+modifier|*
+name|keys
+decl_stmt|;
+comment|/* pressed keys map */
 name|LIST_ENTRY
 argument_list|(
 argument|bthid_session
--- usr.sbin/bluetooth/bthidd/session.c
+++ usr.sbin/bluetooth/bthidd/session.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * session.c  *  * Copyright (c) 2004 Maksim Yevmenkin<m_evmenkin@yahoo.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: session.c,v 1.1 2004/02/12 22:46:59 max Exp $  * $FreeBSD$  */
+comment|/*  * session.c  *  * Copyright (c) 2004 Maksim Yevmenkin<m_evmenkin@yahoo.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: session.c,v 1.2 2004/11/17 21:59:42 max Exp $  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -49,6 +49,12 @@ begin_include
 include|#
 directive|include
 file|"bthidd.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"kbd.h"
 end_include
 
 begin_comment
@@ -133,6 +139,9 @@ name|s
 operator|->
 name|ctrl
 operator|=
+operator|-
+literal|1
+expr_stmt|;
 name|s
 operator|->
 name|intr
@@ -146,6 +155,36 @@ name|state
 operator|=
 name|CLOSED
 expr_stmt|;
+name|s
+operator|->
+name|keys
+operator|=
+name|bit_alloc
+argument_list|(
+name|kbd_maxkey
+argument_list|()
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|s
+operator|->
+name|keys
+operator|==
+name|NULL
+condition|)
+block|{
+name|free
+argument_list|(
+name|s
+argument_list|)
+expr_stmt|;
+name|s
+operator|=
+name|NULL
+expr_stmt|;
+block|}
+else|else
 name|LIST_INSERT_HEAD
 argument_list|(
 operator|&
@@ -469,6 +508,13 @@ name|maxfd
 operator|--
 expr_stmt|;
 block|}
+name|free
+argument_list|(
+name|s
+operator|->
+name|keys
+argument_list|)
+expr_stmt|;
 name|memset
 argument_list|(
 name|s
