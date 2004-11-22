59142c7ec7e98010b830faf2760b351fa55d3f72
Joerg Wunsch <joerg@FreeBSD.org>
2004-11-22 16:08:19 +0000

    [Sorry, forgot to commit my source changes in my previous commit.] Document all options and general usage.
    Implement the -a option to bump the annotation_level.  This improves
    the Emacs gud behaviour.  You can now supply the following function
    
    (defun gud-gdb-massage-args (file args) (cons "-a" args))
    
    (e.g. by evaluating it from the *scratch* buffer) and get the normal
    jump to the source window when browsing the stack.
    
    We should probably eventually supply our own kgdb submode to gud.el.
    
    
    Former-commit-id: ce5c65cded1df928a4d5b8d6c1599417242c65e0

--- gnu/usr.bin/gdb/kgdb/main.c
+++ gnu/usr.bin/gdb/kgdb/main.c
@@ -301,7 +301,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: %s [-v] [-d crashdir] [-c core | -n dumpnr | -r device]\n"
+literal|"usage: %s [-a] [-v] [-d crashdir] [-c core | -n dumpnr | -r device]\n"
 literal|"\t[kernel [core]]\n"
 argument_list|,
 name|getprogname
@@ -923,7 +923,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"c:d:n:r:v"
+literal|"ac:d:n:r:v"
 argument_list|)
 operator|)
 operator|!=
@@ -936,6 +936,13 @@ condition|(
 name|ch
 condition|)
 block|{
+case|case
+literal|'a'
+case|:
+name|annotation_level
+operator|++
+expr_stmt|;
+break|break;
 case|case
 literal|'c'
 case|:
