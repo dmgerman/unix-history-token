281be85dc67b9059db133a41e80cea4e068a83b5
Robert N. M. Watson <rwatson@FreeBSD.org>
2004-11-29 00:29:40 +0000

    Merge tcp_input.c:1.256 from HEAD to RELENG_5:
      date: 2004/11/22 13:16:27;  author: rwatson;  state: Exp;  lines: +0 -3
      Remove "Unlocked read" annotations associated with previously unlocked
      use of socket buffer fields in the TCP input code.  These references
      are now protected by use of the receive socket buffer lock.
    
      MFC after:      1 week
    
    
    Former-commit-id: 10f308037a1d2979a7271b9099ea62fbc4be2e65

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1629,7 +1629,6 @@ argument_list|,
 name|tqe_q
 argument_list|)
 expr_stmt|;
-comment|/* Unlocked read. */
 if|if
 condition|(
 name|so
@@ -5389,7 +5388,6 @@ argument_list|)
 expr_stmt|;
 comment|/* some progress has been done */
 comment|/* #ifdef TCPDEBUG 			if (so->so_options& SO_DEBUG) 				tcp_trace(TA_INPUT, ostate, tp, 				    (void *)tcp_saveipgen,&tcp_savetcp, 0); #endif 			 * Add data to socket buffer. 			 */
-comment|/* Unlocked read. */
 name|SOCKBUF_LOCK
 argument_list|(
 operator|&
@@ -9339,7 +9337,6 @@ argument_list|(
 name|tp
 argument_list|)
 expr_stmt|;
-comment|/* Unlocked read. */
 name|SOCKBUF_LOCK
 argument_list|(
 operator|&
