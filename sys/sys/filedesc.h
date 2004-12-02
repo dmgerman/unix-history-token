1f4dabbd52c0241a1c89554767e12c162b4abb8e
Poul-Henning Kamp <phk@FreeBSD.org>
2004-12-02 12:17:27 +0000

    Drop ffree() as a separate function and incorporate the only place used.
    Former-commit-id: 34de03314a62c302081637eed5b8d083c2882d60

--- sys/sys/filedesc.h
+++ sys/sys/filedesc.h
@@ -535,18 +535,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|void
-name|ffree
-parameter_list|(
-name|struct
-name|file
-modifier|*
-name|fp
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|struct
 name|filedesc_to_leader
 modifier|*
