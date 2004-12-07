aea99dcd44f32b1c1b7049071aab21abf09ddac9
Poul-Henning Kamp <phk@FreeBSD.org>
2004-12-07 07:10:39 +0000

    Remove nfsclnt() prototype.
    Former-commit-id: acd98e024ce201c6cd06b72c27cc5a17c4182a1c

--- include/unistd.h
+++ include/unistd.h
@@ -3601,18 +3601,6 @@ end_endif
 
 begin_function_decl
 name|int
-name|nfsclnt
-parameter_list|(
-name|int
-parameter_list|,
-name|void
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
 name|nfssvc
 parameter_list|(
 name|int
