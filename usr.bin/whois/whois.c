913231a905e5af5a32316405d48bc1e0eec33d69
Marius Strobl <marius@FreeBSD.org>
2004-12-12 23:02:45 +0000

    MFC whois.c: 1.29: Mark a function as __printflike(). 1.30: Leave the `copyright' variable exposed, since the main purpose of       it is to put a copyright in the object file. 1.35: Add support for identifying NORID (norwegian top level registry)       handles. 1.38: Add whois.iana.org to the whois(1) utility, under the -I option. 1.39: Add whois.abuse.net to whois(1). 1.40: Add -b to usage. 1.41: Add special case for the german whois nameserver.
    MFC whois.1:
    1.29: mdoc(7) police: markup polishing.
    1.30: Add whois.iana.org to the whois(1) utility, under the -I option.
    1.31: Date bump.
    1.32: Add whois.abuse.net to whois(1).
    
    
    Former-commit-id: a11e638454f1f8281a15eb360a6ceac2e0330ca0

--- usr.bin/whois/whois.c
+++ usr.bin/whois/whois.c
@@ -3,20 +3,24 @@ begin_comment
 comment|/*  * Copyright (c) 1980, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
 begin_ifndef
 ifndef|#
 directive|ifndef
 name|lint
 end_ifndef
 
+begin_decl_stmt
+specifier|static
+specifier|const
+name|char
+name|copyright
+index|[]
+init|=
+literal|"@(#) Copyright (c) 1980, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n"
+decl_stmt|;
+end_decl_stmt
+
 begin_endif
-unit|static char copyright[] = "@(#) Copyright (c) 1980, 1993\n\ 	The Regents of the University of California.  All rights reserved.\n";
 endif|#
 directive|endif
 end_endif
@@ -24,6 +28,12 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_if
+if|#
+directive|if
+literal|0
+end_if
 
 begin_ifndef
 ifndef|#
@@ -141,6 +151,13 @@ end_include
 begin_define
 define|#
 directive|define
+name|ABUSEHOST
+value|"whois.abuse.net"
+end_define
+
+begin_define
+define|#
+directive|define
 name|NICHOST
 value|"whois.crsnic.net"
 end_define
@@ -227,6 +244,27 @@ define|#
 directive|define
 name|BNICHOST
 value|"whois.registro.br"
+end_define
+
+begin_define
+define|#
+directive|define
+name|NORIDHOST
+value|"whois.norid.no"
+end_define
+
+begin_define
+define|#
+directive|define
+name|IANAHOST
+value|"whois.iana.org"
+end_define
+
+begin_define
+define|#
+directive|define
+name|GERMNICHOST
+value|"de.whois-servers.net"
 end_define
 
 begin_define
@@ -335,8 +373,17 @@ name|format
 parameter_list|,
 modifier|...
 parameter_list|)
-function_decl|;
+function_decl|__printflike
+parameter_list|(
+function_decl|2
+operator|,
+function_decl|3
 end_function_decl
+
+begin_empty_stmt
+unit|)
+empty_stmt|;
+end_empty_stmt
 
 begin_function_decl
 specifier|static
@@ -436,7 +483,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"ac:dgh:ilmpQrR6"
+literal|"abc:dgh:iIlmpQrR6"
 argument_list|)
 operator|)
 operator|!=
@@ -455,6 +502,14 @@ case|:
 name|host
 operator|=
 name|ANICHOST
+expr_stmt|;
+break|break;
+case|case
+literal|'b'
+case|:
+name|host
+operator|=
+name|ABUSEHOST
 expr_stmt|;
 break|break;
 case|case
@@ -495,6 +550,14 @@ case|:
 name|host
 operator|=
 name|INICHOST
+expr_stmt|;
+break|break;
+case|case
+literal|'I'
+case|:
+name|host
+operator|=
+name|IANAHOST
 expr_stmt|;
 break|break;
 case|case
@@ -790,6 +853,58 @@ argument_list|,
 literal|"can't search for a null string"
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|strlen
+argument_list|(
+name|domain
+argument_list|)
+operator|>
+sizeof|sizeof
+argument_list|(
+literal|"-NORID"
+argument_list|)
+operator|-
+literal|1
+operator|&&
+name|strcasecmp
+argument_list|(
+name|domain
+operator|+
+name|strlen
+argument_list|(
+name|domain
+argument_list|)
+operator|-
+sizeof|sizeof
+argument_list|(
+literal|"-NORID"
+argument_list|)
+operator|+
+literal|1
+argument_list|,
+literal|"-NORID"
+argument_list|)
+operator|==
+literal|0
+condition|)
+block|{
+name|s_asprintf
+argument_list|(
+operator|&
+name|retval
+argument_list|,
+literal|"%s"
+argument_list|,
+name|NORIDHOST
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+name|retval
+operator|)
+return|;
+block|}
 while|while
 condition|(
 name|pos
@@ -1221,6 +1336,30 @@ argument_list|,
 literal|"fdopen()"
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|strcmp
+argument_list|(
+name|hostname
+argument_list|,
+name|GERMNICHOST
+argument_list|)
+operator|==
+literal|0
+condition|)
+block|{
+name|fprintf
+argument_list|(
+name|sfo
+argument_list|,
+literal|"-T dn,ace -C US-ASCII %s\r\n"
+argument_list|,
+name|query
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
 name|fprintf
 argument_list|(
 name|sfo
@@ -1230,6 +1369,7 @@ argument_list|,
 name|query
 argument_list|)
 expr_stmt|;
+block|}
 name|fflush
 argument_list|(
 name|sfo
@@ -1608,7 +1748,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: whois [-adgilmpQrR6] [-c country-code | -h hostname] "
+literal|"usage: whois [-abdgiIlmpQrR6] [-c country-code | -h hostname] "
 literal|"name ...\n"
 argument_list|)
 expr_stmt|;
