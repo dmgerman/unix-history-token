bcdd13404925ec2d2bae374a022a0a07c4ae147b
Warner Losh <imp@FreeBSD.org>
2004-12-13 06:26:23 +0000

    sys/vnode.h is inappropriate for a driver.  Use sys/uio.h instead.
    Former-commit-id: f0475e1ac891c57771e4c17ae8677e2a858f3ced

--- sys/dev/usb/uscanner.c
+++ sys/dev/usb/uscanner.c
@@ -164,12 +164,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/vnode.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/poll.h>
 end_include
 
@@ -183,6 +177,12 @@ begin_include
 include|#
 directive|include
 file|<sys/sysctl.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/uio.h>
 end_include
 
 begin_include
