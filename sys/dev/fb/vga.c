7bc79078a011f655117e584f0ab37420f34c4fa2
Ruslan Ermilov <ru@FreeBSD.org>
2004-12-15 13:55:00 +0000

    MFC: 1.33: Fixed compilation warnings with option VGA_NO_MODE_CHANGE.
    Former-commit-id: 9faa53f39d87802665de8fb2f7bad1884a6943db

--- sys/dev/fb/vga.c
+++ sys/dev/fb/vga.c
@@ -3395,23 +3395,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-specifier|static
-name|void
-name|filll_io
-parameter_list|(
-name|int
-name|val
-parameter_list|,
-name|vm_offset_t
-name|d
-parameter_list|,
-name|size_t
-name|size
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -3505,6 +3488,23 @@ ifndef|#
 directive|ifndef
 name|VGA_NO_MODE_CHANGE
 end_ifndef
+
+begin_function_decl
+specifier|static
+name|void
+name|filll_io
+parameter_list|(
+name|int
+name|val
+parameter_list|,
+name|vm_offset_t
+name|d
+parameter_list|,
+name|size_t
+name|size
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_function_decl
 specifier|static
@@ -7928,6 +7928,12 @@ return|;
 block|}
 end_function
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|VGA_NO_MODE_CHANGE
+end_ifndef
+
 begin_if
 if|#
 directive|if
@@ -8033,6 +8039,15 @@ expr_stmt|;
 block|}
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !VGA_NO_MODE_CHANGE */
+end_comment
 
 begin_comment
 comment|/* entry points */
