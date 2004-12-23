af53da53be79c9c77410b2c5212b441274ea452d
Gleb Smirnoff <glebius@FreeBSD.org>
2004-12-23 13:09:37 +0000

    Move systm.h up, since it is required by mbuf.h.
    Requested by:	ru
    
    
    Former-commit-id: dd9c256ea7e21a30ecc41faf5fcb9230136d751b

--- sys/netgraph/ng_base.c
+++ sys/netgraph/ng_base.c
@@ -16,6 +16,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/ctype.h>
 end_include
 
@@ -71,12 +77,6 @@ begin_include
 include|#
 directive|include
 file|<sys/syslog.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/systm.h>
 end_include
 
 begin_include
