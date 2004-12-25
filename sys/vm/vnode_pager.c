9f4e6577bdb75d7921c8337d664be81d60e5c65c
Poul-Henning Kamp <phk@FreeBSD.org>
2004-12-25 21:30:41 +0000

    fix comment
    Former-commit-id: 14815555fb04ad5efa210f9f5675044949ab8e0d

--- sys/vm/vnode_pager.c
+++ sys/vm/vnode_pager.c
@@ -1981,7 +1981,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * old style vnode pager output routine  */
+comment|/*  * old style vnode pager input routine  */
 end_comment
 
 begin_function
