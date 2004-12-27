ccf84bf5f9e58e7dd35e522e4ac354b780209b7f
Nate Lawson <njl@FreeBSD.org>
2004-12-27 05:36:47 +0000

    Remove trailing whitespace.
    Former-commit-id: a2d276c912266dcdc2fac7668decea8b749336db

--- sys/dev/acpica/acpi_ec.c
+++ sys/dev/acpica/acpi_ec.c
@@ -165,7 +165,7 @@ value|((EC_COMMAND) 0x84)
 end_define
 
 begin_comment
-comment|/*   * EC_STATUS:  * ----------  * The encoding of the EC status register is illustrated below.  * Note that a set bit (1) indicates the property is TRUE  * (e.g. if bit 0 is set then the output buffer is full).  * +-+-+-+-+-+-+-+-+  * |7|6|5|4|3|2|1|0|	  * +-+-+-+-+-+-+-+-+  *  | | | | | | | |  *  | | | | | | | +- Output Buffer Full?  *  | | | | | | +--- Input Buffer Full?  *  | | | | | +-----<reserved>  *  | | | | +------- Data Register is Command Byte?  *  | | | +--------- Burst Mode Enabled?  *  | | +----------- SCI Event?  *  | +------------- SMI Event?  *  +---------------<Reserved>  *  */
+comment|/*   * EC_STATUS:  * ----------  * The encoding of the EC status register is illustrated below.  * Note that a set bit (1) indicates the property is TRUE  * (e.g. if bit 0 is set then the output buffer is full).  * +-+-+-+-+-+-+-+-+  * |7|6|5|4|3|2|1|0|  * +-+-+-+-+-+-+-+-+  *  | | | | | | | |  *  | | | | | | | +- Output Buffer Full?  *  | | | | | | +--- Input Buffer Full?  *  | | | | | +-----<reserved>  *  | | | | +------- Data Register is Command Byte?  *  | | | +--------- Burst Mode Enabled?  *  | | +----------- SCI Event?  *  | +------------- SMI Event?  *  +---------------<Reserved>  *  */
 end_comment
 
 begin_typedef
