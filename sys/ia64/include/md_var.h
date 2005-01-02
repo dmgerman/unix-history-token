7d6e24fec163c421d682e94551adf99b00a7ca14
Marcel Moolenaar <marcel@FreeBSD.org>
2005-01-02 00:20:54 +0000

    Further enhance the handling of misaligned loads and stores: o  implement double-extended and single precision loads and stores, o  implement double precision stores, o  replace the machdep.unaligned_print sysctl with debug.unaligned_print    and change the default value to 0, o  replace the machdep.unaligned_sigbus sysctl with debug.unaligned_test, o  Remmove the fillfd() function. The function is trvial enough for    inline assembly.
    The debug.unaligned_test sysctl is used to test the emulation of
    misaligned loads and stores. When PSR.ac is 0, the CPU will handle
    misaligned memory accesses itselfi and we don't get an exception
    for it. When PSR.ac is 1, the process needs to be signalled and we
    should not emulate. The sysctl takes effect when PSR.ac is 1 and
    tells us that we should emulate and not send a signal.
    
    PR: 72268
    MFC after: 1 week
    
    Former-commit-id: d800c6748c861229ef2e9bd9c2ec6e0c1bab374a

--- sys/ia64/include/md_var.h
+++ sys/ia64/include/md_var.h
@@ -429,21 +429,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|void
-name|spillfd
-parameter_list|(
-name|void
-modifier|*
-name|src
-parameter_list|,
-name|void
-modifier|*
-name|dst
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|int
 name|syscall
 parameter_list|(
