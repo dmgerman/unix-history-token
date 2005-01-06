cb697beac140207e6a4006b1c5f46e9545d1d214
Warner Losh <imp@FreeBSD.org>
2005-01-06 18:27:09 +0000

    Add dol FreeBSD dol and /*+ize license
    Former-commit-id: a48324016910aff82647731eb04c001f19f48ab8

--- sys/gnu/dev/sound/pci/emu10k1-alsa.h
+++ sys/gnu/dev/sound/pci/emu10k1-alsa.h
@@ -12,7 +12,11 @@ name|__SOUND_EMU10K1_H
 end_define
 
 begin_comment
-comment|/*  *  Copyright (c) by Jaroslav Kysela<perex@suse.cz>,  *		     Creative Labs, Inc.  *  Definitions for EMU10K1 (SB Live!) chips  *  *  *   This program is free software; you can redistribute it and/or modify  *   it under the terms of the GNU General Public License as published by  *   the Free Software Foundation; either version 2 of the License, or  *   (at your option) any later version.  *  *   This program is distributed in the hope that it will be useful,  *   but WITHOUT ANY WARRANTY; without even the implied warranty of  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *   GNU General Public License for more details.  *  *   You should have received a copy of the GNU General Public License  *   along with this program; if not, write to the Free Software  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *  */
+comment|/*-  *  Copyright (c) by Jaroslav Kysela<perex@suse.cz>,  *		     Creative Labs, Inc.  *  Definitions for EMU10K1 (SB Live!) chips  *  *  *   This program is free software; you can redistribute it and/or modify  *   it under the terms of the GNU General Public License as published by  *   the Free Software Foundation; either version 2 of the License, or  *   (at your option) any later version.  *  *   This program is distributed in the hope that it will be useful,  *   but WITHOUT ANY WARRANTY; without even the implied warranty of  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *   GNU General Public License for more details.  *  *   You should have received a copy of the GNU General Public License  *   along with this program; if not, write to the Free Software  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *  */
+end_comment
+
+begin_comment
+comment|/* $FreeBSD$ */
 end_comment
 
 begin_ifdef
