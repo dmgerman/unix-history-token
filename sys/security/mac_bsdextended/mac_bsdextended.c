3fdaea3ad4092bfeba8efdfe437a7466b9cb8259
Robert N. M. Watson <rwatson@FreeBSD.org>
2005-01-29 20:36:30 +0000

    Merge mac_bsdextended.c:1.22-1.23 and mac_bsdextended.h:1.4-1.5 from HEAD to RELENG_5:
      date: 2004/10/21 11:19:02;  author: rwatson;  state: Exp;  lines: +59 -33
      date: 2004/10/21 11:19:02;  author: rwatson;  state: Exp;  lines: +15 -0
      Modify mac_bsdextended policy so that it defines its own vnode access
      right bits rather than piggy-backing on the V* rights defined in
      vnode.h.  The mac_bsdextended bits are given the same values as the V*
      bits to make the new kernel module binary compatible with the old
      version of libugidfw that uses V* bits.  This avoids leaking kernel
      API/ABI to user management tools, and in particular should remove the
      need for libugidfw to include vnode.h.
    
      Requested by:   phk
    
      date: 2004/10/21 11:29:56;  author: rwatson;  state: Exp;  lines: +1 -1
      date: 2004/10/21 11:29:56;  author: rwatson;  state: Exp;  lines: +1 -1
      Bump copyright dates for NETA on these files.
    
    
    Former-commit-id: 26c615bfc5ea07da39465fb6446934d81596af90

--- sys/security/mac_bsdextended/mac_bsdextended.c
+++ sys/security/mac_bsdextended/mac_bsdextended.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999-2002 Robert N. M. Watson  * Copyright (c) 2001-2003 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed by Robert Watson for the TrustedBSD Project.  *  * This software was developed for the FreeBSD Project in part by Network  * Associates Laboratories, the Security Research Division of Network  * Associates, Inc. under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"),  * as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1999-2002 Robert N. M. Watson  * Copyright (c) 2001-2005 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed by Robert Watson for the TrustedBSD Project.  *  * This software was developed for the FreeBSD Project in part by Network  * Associates Laboratories, the Security Research Division of Network  * Associates, Inc. under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"),  * as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -435,10 +435,10 @@ name|rule
 operator|->
 name|mbr_mode
 operator||
-name|VALLPERM
+name|MBI_ALLPERM
 operator|)
 operator|!=
-name|VALLPERM
+name|MBI_ALLPERM
 condition|)
 return|return
 operator|(
@@ -1229,17 +1229,17 @@ if|if
 condition|(
 name|acc_mode
 operator|&
-name|VAPPEND
+name|MBI_APPEND
 condition|)
 block|{
 name|acc_mode
 operator|&=
 operator|~
-name|VAPPEND
+name|MBI_APPEND
 expr_stmt|;
 name|acc_mode
 operator||=
-name|VWRITE
+name|MBI_WRITE
 expr_stmt|;
 block|}
 name|error
@@ -1360,7 +1360,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 operator|)
 return|;
@@ -1527,7 +1527,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VEXEC
+name|MBI_EXEC
 argument_list|)
 operator|)
 return|;
@@ -1609,7 +1609,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VEXEC
+name|MBI_EXEC
 argument_list|)
 operator|)
 return|;
@@ -1701,7 +1701,7 @@ name|dvap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 operator|)
 return|;
@@ -1798,7 +1798,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 if|if
@@ -1847,7 +1847,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 operator|)
 return|;
@@ -1932,7 +1932,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -2022,7 +2022,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 operator|)
 return|;
@@ -2114,9 +2114,9 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VREAD
+name|MBI_READ
 operator||
-name|VEXEC
+name|MBI_EXEC
 argument_list|)
 operator|)
 return|;
@@ -2201,7 +2201,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VSTAT
+name|MBI_STAT
 argument_list|)
 operator|)
 return|;
@@ -2296,7 +2296,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VREAD
+name|MBI_READ
 argument_list|)
 operator|)
 return|;
@@ -2393,7 +2393,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 if|if
@@ -2442,7 +2442,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 if|if
@@ -2540,7 +2540,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VREAD
+name|MBI_READ
 argument_list|)
 operator|)
 return|;
@@ -2627,7 +2627,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VEXEC
+name|MBI_EXEC
 argument_list|)
 operator|)
 return|;
@@ -2794,7 +2794,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VREAD
+name|MBI_READ
 argument_list|)
 operator|)
 return|;
@@ -2876,7 +2876,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VREAD
+name|MBI_READ
 argument_list|)
 operator|)
 return|;
@@ -2973,7 +2973,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 if|if
@@ -3022,7 +3022,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 return|return
@@ -3126,7 +3126,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 if|if
@@ -3182,7 +3182,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 expr_stmt|;
 block|}
@@ -3269,7 +3269,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -3359,7 +3359,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -3454,7 +3454,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VWRITE
+name|MBI_WRITE
 argument_list|)
 operator|)
 return|;
@@ -3539,7 +3539,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -3624,7 +3624,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -3712,7 +3712,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -3802,7 +3802,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VADMIN
+name|MBI_ADMIN
 argument_list|)
 operator|)
 return|;
@@ -3889,7 +3889,7 @@ name|vap
 operator|.
 name|va_gid
 argument_list|,
-name|VSTAT
+name|MBI_STAT
 argument_list|)
 operator|)
 return|;
--- sys/security/mac_bsdextended/mac_bsdextended.h
+++ sys/security/mac_bsdextended/mac_bsdextended.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999-2002 Robert N. M. Watson  * Copyright (c) 2001-2002 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed by Robert Watson for the TrustedBSD Project.  *  * This software was developed for the FreeBSD Project in part by Network  * Associates Laboratories, the Security Research Division of Network  * Associates, Inc. under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"),  * as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1999-2002 Robert N. M. Watson  * Copyright (c) 2001-2005 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed by Robert Watson for the TrustedBSD Project.  *  * This software was developed for the FreeBSD Project in part by Network  * Associates Laboratories, the Security Research Division of Network  * Associates, Inc. under DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"),  * as part of the DARPA CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -53,6 +53,59 @@ define|#
 directive|define
 name|MBI_BITS
 value|(MBI_UID_DEFINED | MBI_GID_DEFINED | MBI_NEGATED)
+end_define
+
+begin_comment
+comment|/*  * Rights that can be represented in mbr_mode.  These have the same values  * as the V* rights in vnode.h, but in order to avoid sharing user and  * kernel constants, we define them here.  That will also improve ABI  * stability if the in-kernel values change.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MBI_EXEC
+value|000100
+end_define
+
+begin_define
+define|#
+directive|define
+name|MBI_WRITE
+value|000200
+end_define
+
+begin_define
+define|#
+directive|define
+name|MBI_READ
+value|000400
+end_define
+
+begin_define
+define|#
+directive|define
+name|MBI_ADMIN
+value|010000
+end_define
+
+begin_define
+define|#
+directive|define
+name|MBI_STAT
+value|020000
+end_define
+
+begin_define
+define|#
+directive|define
+name|MBI_APPEND
+value|040000
+end_define
+
+begin_define
+define|#
+directive|define
+name|MBI_ALLPERM
+value|(MBI_EXEC | MBI_WRITE | MBI_READ | MBI_ADMIN | \ 			    MBI_STAT | MBI_APPEND)
 end_define
 
 begin_struct
