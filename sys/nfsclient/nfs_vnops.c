7068a4078d039846085d62d7c3b9da5b48c80ea2
Robert N. M. Watson <rwatson@FreeBSD.org>
2005-01-29 23:19:08 +0000

    Style cleanup for O_DIRECT sysctl comment introduced in nfs_vnops.c:1.242.
    Former-commit-id: 374c67729257a40fbdd10f58e023bad1331def14

--- sys/nfsclient/nfs_vnops.c
+++ sys/nfsclient/nfs_vnops.c
@@ -1072,7 +1072,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_comment
-comment|/*  * This sysctl allows other processes to mmap a file that has been opened O_DIRECT   * by a process. In general, having processes mmap the file while Direct IO is in   * progress can lead to Data Inconsistencies. But, we allow this by default to   * prevent DoS attacks - to prevent a malicious user from opening up files O_DIRECT   * preventing other users from mmap'ing these files. "Protected" environments where   * stricter consistency guarantees are required can disable this knob.   * The process that opened the file O_DIRECT cannot mmap() the file, because   * mmap'ed IO on an O_DIRECT open() is not meaningful.  */
+comment|/*  * This sysctl allows other processes to mmap a file that has been opened  * O_DIRECT by a process.  In general, having processes mmap the file while  * Direct IO is in progress can lead to Data Inconsistencies.  But, we allow  * this by default to prevent DoS attacks - to prevent a malicious user from  * opening up files O_DIRECT preventing other users from mmap'ing these  * files.  "Protected" environments where stricter consistency guarantees are  * required can disable this knob.  The process that opened the file O_DIRECT  * cannot mmap() the file, because mmap'ed IO on an O_DIRECT open() is not  * meaningful.  */
 end_comment
 
 begin_decl_stmt
