494255d01caa702055cb71ca50f488a32e074736
Jeff Roberson <jeff@FreeBSD.org>
2005-02-04 19:59:43 +0000

    - MFC r1.26 support for schedgraph.
    Former-commit-id: 7a4409343a8bd21819eff02e0893d4e8f1ca298a

--- sys/sys/ktr.h
+++ sys/sys/ktr.h
@@ -384,8 +384,19 @@ end_comment
 begin_define
 define|#
 directive|define
+name|KTR_SCHED
+value|0x20000000
+end_define
+
+begin_comment
+comment|/* Machine parsed sched info. */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|KTR_ALL
-value|0x1fffffff
+value|0x3fffffff
 end_define
 
 begin_comment
