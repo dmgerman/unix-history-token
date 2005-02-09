d683106a0b3478e0ab842aca93f7d947ba19080b
John-Mark Gurney <jmg@FreeBSD.org>
2005-02-09 00:10:17 +0000

    move pmap.h after vm.h include...  some of the headers from pmap.h depend upon vm.h
    Former-commit-id: d96dcb01ea7189e8579525b46058e139b4b27b09

--- sys/arm/arm/machdep.c
+++ sys/arm/arm/machdep.c
@@ -156,13 +156,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<vm/pmap.h>
+file|<vm/vm.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<vm/vm.h>
+file|<vm/pmap.h>
 end_include
 
 begin_include
