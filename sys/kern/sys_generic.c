1f15a1945b4fdea1d086a416610dedaebab13de5
Colin Percival <cperciva@FreeBSD.org>
2005-02-10 20:19:17 +0000

    Declare "cnt" (a number of bytes to read or write) as an "ssize_t", not as a "long" in dofileread() and dofilewrite().
    Discussed with:	jhb
    
    
    Former-commit-id: 8da1c99feb7a65d40b347c8dfe1e5155cf81f718

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -714,9 +714,10 @@ name|struct
 name|iovec
 name|aiov
 decl_stmt|;
-name|long
+name|ssize_t
 name|cnt
-decl_stmt|,
+decl_stmt|;
+name|long
 name|error
 init|=
 literal|0
@@ -1664,9 +1665,10 @@ name|struct
 name|iovec
 name|aiov
 decl_stmt|;
-name|long
+name|ssize_t
 name|cnt
-decl_stmt|,
+decl_stmt|;
+name|long
 name|error
 init|=
 literal|0
