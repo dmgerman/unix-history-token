33aa55e5ee1afbc835690f662f0935d1ee1f975b
David E. O'Brien <obrien@FreeBSD.org>
2005-02-16 06:48:35 +0000

    va_list style tweaks
    Former-commit-id: 2e56ed041d0319a6bcce0e6b8e95439803125242

--- sbin/dump/optr.c
+++ sbin/dump/optr.c
@@ -973,6 +973,11 @@ argument_list|,
 name|ap
 argument_list|)
 expr_stmt|;
+name|va_end
+argument_list|(
+name|ap
+argument_list|)
+expr_stmt|;
 operator|(
 name|void
 operator|)
@@ -987,11 +992,6 @@ operator|)
 name|fflush
 argument_list|(
 name|stderr
-argument_list|)
-expr_stmt|;
-name|va_end
-argument_list|(
-name|ap
 argument_list|)
 expr_stmt|;
 name|va_start
