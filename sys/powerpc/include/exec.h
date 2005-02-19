d0d7b9bfd0545287ad148d0686569c9e53264e41
Ruslan Ermilov <ru@FreeBSD.org>
2005-02-19 21:16:48 +0000

    Use a common multi-inclusion protection, and add such a protection to alpha/include/exec.h.
    Former-commit-id: c5ea9e1b0bf0b3866eaad06ebd0d09fc549ddd47

--- sys/powerpc/include/exec.h
+++ sys/powerpc/include/exec.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_EXEC_H_
+name|_MACHINE_EXEC_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_EXEC_H_
+name|_MACHINE_EXEC_H_
 end_define
 
 begin_define
@@ -28,7 +28,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_EXEC_H_ */
+comment|/* !_MACHINE_EXEC_H_ */
 end_comment
 
 end_unit
