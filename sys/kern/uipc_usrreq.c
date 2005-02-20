34174285f4366318ceda17664347def0b7841146
Robert N. M. Watson <rwatson@FreeBSD.org>
2005-02-20 04:18:22 +0000

    Move assignment of UNIX domain socket pcb during unp_attach() outside of the global UNIX domain socket mutex: no protection is needed that early in the setup of the UNIX domain socket and socket structures.
    MFC after:	3 days
    
    
    Former-commit-id: 2ae921a9eca1b488e6aa997547b1020efdf75396

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -3191,6 +3191,12 @@ name|unp_socket
 operator|=
 name|so
 expr_stmt|;
+name|so
+operator|->
+name|so_pcb
+operator|=
+name|unp
+expr_stmt|;
 name|UNP_LOCK
 argument_list|()
 expr_stmt|;
@@ -3222,12 +3228,6 @@ name|unp
 argument_list|,
 name|unp_link
 argument_list|)
-expr_stmt|;
-name|so
-operator|->
-name|so_pcb
-operator|=
-name|unp
 expr_stmt|;
 name|UNP_UNLOCK
 argument_list|()
