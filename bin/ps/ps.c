c7204c00cb71d75abfb09cb6fce3486ea624598f
Xin Li <delphij@FreeBSD.org>
2005-02-22 06:50:54 +0000

    Sync ps(1) in RELENG_5 with HEAD:
    MFC ps.1 revision 1.84
    date: 2005/02/14 16:56:15;  author: delphij;  state: Exp;  lines: +22 -10
    Several improvements to ps.1:
    	- Document the fact that empty heading text suppresses the
    	  heading line (e.g. 'ps -o pid='), as this is very useful
    	  in scripts.
    	- Describe logname keyword more completely.
    	- Describe the printing of arguments more completely.
    	- Put lockname in the correct alphabetical order in the list
    	  of all keywords.
    	- Correct sentence in standards section.
    
    Submitted by:	Jilles Tjoelker <jilles at stack nl>
    PR:		docs/73618
    MFC After:	1 week
    
    MFC ps.1 revision 1.83, ps.c revision 1.110
    date: 2005/02/09 17:37:38;  author: ru;  state: Exp;  lines: +2 -3
    Sync program's usage() with manpage's SYNOPSIS.
    
    MFC Makefile revision 1.27
    date: 2005/01/27 13:42:40;  author: delphij;  state: Exp;  lines: +1 -1
    ps(1) is WARNS=6 on all Tier-1 platforms as far as I can test,
    so use default WARNS level from bin/Makefile.inc
    
    MFC ps.1 revision 1.82
    date: 2005/01/10 08:39:24;  author: imp;  state: Exp;  lines: +1 -0
    /*- or .\"- or #- to begin license clauses.
    
    MFC Makefile revision 1.26
    date: 2004/11/13 17:12:22;  author: ru;  state: Exp;  lines: +0 -2
    Removed bitrot.
    
    Discussed with:	ru
    
    
    Former-commit-id: 6f96e4e5f3c6d6e86d0737b10b3d3b2abf6094c6

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -5671,7 +5671,7 @@ block|{
 define|#
 directive|define
 name|SINGLE_OPTS
-value|"[-aCc" OPT_LAZY_f "HhjlmrSTuvwXxZ]"
+value|"[-aCce" OPT_LAZY_f "HhjlmrSTuvwXxZ]"
 operator|(
 name|void
 operator|)
@@ -5683,11 +5683,11 @@ literal|"%s\n%s\n%s\n%s\n"
 argument_list|,
 literal|"usage: ps "
 name|SINGLE_OPTS
-literal|" [-G gid[,gid]] [-O|o fmt]"
-argument_list|,
-literal|"          [-p pid[,pid]] [-t tty[,tty]] [-U user[,user]]"
+literal|" [-O fmt | -o fmt] [-G gid[,gid...]]"
 argument_list|,
 literal|"          [-M core] [-N system]"
+argument_list|,
+literal|"          [-p pid[,pid...]] [-t tty[,tty...]] [-U user[,user...]]"
 argument_list|,
 literal|"       ps [-L]"
 argument_list|)
