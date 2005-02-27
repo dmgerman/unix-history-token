0e9649a06f7384929814c8c4282aeb0a20189a14
Jeff Roberson <jeff@FreeBSD.org>
2005-02-27 02:33:03 +0000

    MFC revision 1.70  - filedesc locking cleanup.
    Former-commit-id: 62ad82e5c3bc3d1dca91bc54854525bd7852eb07

--- sys/fs/unionfs/union_vfsops.c
+++ sys/fs/unionfs/union_vfsops.c
@@ -706,7 +706,7 @@ operator|->
 name|td_ucred
 argument_list|)
 expr_stmt|;
-name|FILEDESC_LOCK
+name|FILEDESC_LOCK_FAST
 argument_list|(
 name|td
 operator|->
@@ -730,7 +730,7 @@ name|p_fd
 operator|->
 name|fd_cmask
 expr_stmt|;
-name|FILEDESC_UNLOCK
+name|FILEDESC_UNLOCK_FAST
 argument_list|(
 name|td
 operator|->
