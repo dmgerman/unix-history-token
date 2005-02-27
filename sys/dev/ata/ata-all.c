9149d7e91fa0c68a864821b467fb48d1ec0f6e4f
Matthew N. Dodd <mdodd@FreeBSD.org>
2005-02-27 20:35:23 +0000

    MFC: 1.235
    Avoid using tsleep() in the resume path as it may result in the
    system hanging if timer interrupts aren't running yet.
    
    This allows my Thinkpad to resume successfully with APM.
    
    
    Former-commit-id: 2dbcdfd8f2e2395971d61a2eeaffe7851bfe7b00

--- sys/dev/ata/ata-all.c
+++ sys/dev/ata/ata-all.c
@@ -398,6 +398,15 @@ literal|1
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|static
+name|int
+name|ata_resuming
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
 begin_comment
 comment|/* sysctl vars */
 end_comment
@@ -2200,6 +2209,10 @@ condition|)
 return|return
 name|ENXIO
 return|;
+name|ata_resuming
+operator|=
+literal|1
+expr_stmt|;
 name|error
 operator|=
 name|ata_reinit
@@ -2211,6 +2224,10 @@ name|ata_start
 argument_list|(
 name|ch
 argument_list|)
+expr_stmt|;
+name|ata_resuming
+operator|=
+literal|0
 expr_stmt|;
 return|return
 name|error
@@ -5162,6 +5179,8 @@ name|hz
 operator|)
 operator|||
 name|ata_delayed_attach
+operator|||
+name|ata_resuming
 condition|)
 name|DELAY
 argument_list|(
