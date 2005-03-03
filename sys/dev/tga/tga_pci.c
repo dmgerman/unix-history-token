62d2874760f79799289021a0e8a2e6f9846fdcc1
David E. O'Brien <obrien@FreeBSD.org>
2005-03-03 04:49:43 +0000

    MFC: rev 1.13: Use BUS_PROBE_DEFAULT in preference to 0.
    Brought to you by the letters:	I-M-P
    
    
    Former-commit-id: 9870a3ae2cf3e400f590326b7dcd443123beb747

--- sys/dev/tga/tga_pci.c
+++ sys/dev/tga/tga_pci.c
@@ -387,12 +387,6 @@ operator|.
 name|d_name
 operator|=
 literal|"tga"
-block|,
-operator|.
-name|d_maj
-operator|=
-operator|-
-literal|1
 block|, }
 decl_stmt|;
 end_decl_stmt
@@ -474,7 +468,7 @@ argument_list|)
 expr_stmt|;
 name|error
 operator|=
-literal|0
+name|BUS_PROBE_DEFAULT
 expr_stmt|;
 break|break;
 block|}
