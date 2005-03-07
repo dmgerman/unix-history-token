d0be3834e2573b0f34eb28ded7b62bfff88b7556
Dag-Erling Smorgrav <des@FreeBSD.org>
2005-03-07 17:03:18 +0000

    MFC: add /rescue/init to default init path, before /stand/sysinstall
    Approved by:	re (kensmith)
    
    
    Former-commit-id: 8c72b960c2736f97bc05c1f47641d83c018383dc

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1937,7 +1937,7 @@ directive|else
 end_else
 
 begin_expr_stmt
-literal|"/sbin/init:/sbin/oinit:/sbin/init.bak:/stand/sysinstall"
+literal|"/sbin/init:/sbin/oinit:/sbin/init.bak:/rescue/init:/stand/sysinstall"
 expr_stmt|;
 end_expr_stmt
 
