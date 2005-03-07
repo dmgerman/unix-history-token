af924c0ac0db601eb6065913945fefe85cbc186a
Stefan Farfeleder <stefanf@FreeBSD.org>
2005-03-07 20:28:26 +0000

    Remove an obsolete sentence from a comment.
    Former-commit-id: 4b6ce6d1be93bf52da25f9d84706c41abc849ea6

--- lib/msun/src/s_rint.c
+++ lib/msun/src/s_rint.c
@@ -45,7 +45,7 @@ file|"math_private.h"
 end_include
 
 begin_comment
-comment|/*  * TWO23 is long double instead of double to avoid a bug in gcc.  Without  * this, gcc thinks that TWO23[sx]+x and w-TWO23[sx] already have double  * precision and doesn't clip them to double precision when they are  * assigned and returned.  Use long double even in the !__STDC__ case in  * case this is compiled with gcc -traditional.  */
+comment|/*  * TWO23 is long double instead of double to avoid a bug in gcc.  Without  * this, gcc thinks that TWO23[sx]+x and w-TWO23[sx] already have double  * precision and doesn't clip them to double precision when they are  * assigned and returned.  */
 end_comment
 
 begin_decl_stmt
