25f974114f8c83c70601212ce488e98090c3525f
Hajimu UMEMOTO <ume@FreeBSD.org>
2005-03-10 11:40:53 +0000

    use cast128 in opencrypto to nuke duplicate code.
    Former-commit-id: 39d46708db7c96936598e8588eeb90a2fb18df25

--- sys/crypto/cast128/cast128.h
+++ sys/crypto/cast128/cast128.h
@@ -3,18 +3,6 @@ begin_comment
 comment|/*	$FreeBSD$	*/
 end_comment
 
-begin_comment
-comment|/*	$NetBSD: cast128.h,v 1.6 2003/08/26 20:03:57 thorpej Exp $ */
-end_comment
-
-begin_comment
-comment|/*      $OpenBSD: cast.h,v 1.2 2002/03/14 01:26:51 millert Exp $       */
-end_comment
-
-begin_comment
-comment|/*  *	CAST-128 in C  *	Written by Steve Reid<sreid@sea-to-sky.net>  *	100% Public Domain - no warranty  *	Released 1997.10.11  */
-end_comment
-
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -27,86 +15,63 @@ directive|define
 name|_CAST128_H_
 end_define
 
-begin_typedef
-typedef|typedef
-struct|struct
-block|{
-name|u_int32_t
-name|xkey
-index|[
-literal|32
-index|]
-decl_stmt|;
-comment|/* Key, after expansion */
-name|int
-name|rounds
-decl_stmt|;
-comment|/* Number of rounds to use, 12 or 16 */
-block|}
-name|cast128_key
-typedef|;
-end_typedef
+begin_include
+include|#
+directive|include
+file|<opencrypto/cast.h>
+end_include
 
-begin_function_decl
-name|void
+begin_define
+define|#
+directive|define
+name|cast128_key
+value|cast_key
+end_define
+
+begin_define
+define|#
+directive|define
 name|cast128_setkey
 parameter_list|(
-name|cast128_key
-modifier|*
 name|key
 parameter_list|,
-specifier|const
-name|u_int8_t
-modifier|*
 name|rawkey
 parameter_list|,
-name|int
 name|keybytes
 parameter_list|)
-function_decl|;
-end_function_decl
+define|\
+value|cast_setkey((key), (rawkey), (keybytes))
+end_define
 
-begin_function_decl
-name|void
+begin_define
+define|#
+directive|define
 name|cast128_encrypt
 parameter_list|(
-specifier|const
-name|cast128_key
-modifier|*
 name|key
 parameter_list|,
-specifier|const
-name|u_int8_t
-modifier|*
 name|inblock
 parameter_list|,
-name|u_int8_t
-modifier|*
 name|outblock
 parameter_list|)
-function_decl|;
-end_function_decl
+define|\
+value|cast_encrypt((key), (inblock), (outblock))
+end_define
 
-begin_function_decl
-name|void
+begin_define
+define|#
+directive|define
 name|cast128_decrypt
 parameter_list|(
-specifier|const
-name|cast128_key
-modifier|*
 name|key
 parameter_list|,
-specifier|const
-name|u_int8_t
-modifier|*
 name|inblock
 parameter_list|,
-name|u_int8_t
-modifier|*
 name|outblock
 parameter_list|)
-function_decl|;
-end_function_decl
+define|\
+value|cast_decrypt((key), (inblock), (outblock))
+end_define
 
 begin_endif
 endif|#
