37ce3dc28bea559f4e520d2f314066739b5f873f
Murray Stokely <murray@FreeBSD.org>
2005-03-19 01:39:13 +0000

    Add device id for nForce 4 audio controller.
    PR:		kern/78482
    Submitted by:	Markus Niemistö <markus.niemisto@iki.fi>
    MFC after:	1 week
    
    Former-commit-id: 1a4daf188cb85ca90387ba5c22704d6497d3d65e

--- sys/dev/sound/pci/ich.c
+++ sys/dev/sound/pci/ich.c
@@ -3231,6 +3231,19 @@ return|return
 name|BUS_PROBE_DEFAULT
 return|;
 case|case
+literal|0x005910de
+case|:
+name|device_set_desc
+argument_list|(
+name|dev
+argument_list|,
+literal|"nVidia nForce4"
+argument_list|)
+expr_stmt|;
+return|return
+literal|0
+return|;
+case|case
 literal|0x74451022
 case|:
 name|device_set_desc
