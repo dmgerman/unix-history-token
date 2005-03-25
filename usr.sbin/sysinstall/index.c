550b2873a3b98dd8693932cb0727aa6ec4e443a0
John Baldwin <jhb@FreeBSD.org>
2005-03-25 21:38:26 +0000

    MFC: Recover gracefully if the user puts the wrong CD in.
    Approved by:	re (kensmith)
    
    
    Former-commit-id: 3d2e0fe5b545b6965d4705c9766b39cf4e4c4bb5

--- usr.sbin/sysinstall/index.c
+++ usr.sbin/sysinstall/index.c
@@ -3605,8 +3605,8 @@ condition|)
 return|return
 name|DITEM_SUCCESS
 return|;
-comment|/*      * What if the package is not available on the current media volume?      *      */
-if|if
+comment|/*      * Prompt user if the package is not available on the current volume.      */
+while|while
 condition|(
 name|id
 operator|->
@@ -3662,7 +3662,6 @@ argument_list|(
 name|mediaDevice
 argument_list|)
 expr_stmt|;
-comment|/* XXX, at this point we check to see if this is the 		 * correct disc, and if not, we loop */
 block|}
 else|else
 block|{
