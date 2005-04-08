a89f6dfec9c3343dc1235ad386e8ebeeea154141
Scott Long <scottl@FreeBSD.org>
2005-04-08 22:51:50 +0000

    Explicity make atapi-cam depend on CAM.
    Submitted by: Michal Mertl
    
    
    Former-commit-id: 59b3bc1c9988ead84a0116b676bb6786c152accb

--- sys/dev/ata/atapi-cam.c
+++ sys/dev/ata/atapi-cam.c
@@ -675,6 +675,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|atapicam
+argument_list|,
+name|cam
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_function
 specifier|static
 name|int
