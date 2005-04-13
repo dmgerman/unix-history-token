d019e6f8b0aa811df966a895eea9d83f503b3b0c
Gleb Smirnoff <glebius@FreeBSD.org>
2005-04-13 21:05:55 +0000

    Fix mss byte order, only affects synproxy code path.
    Submitted by:	John L. Scarfone via OpenBSD
    Reviewed by:	mlaier
    Obtained from:	OpenBSD, rev. 1.483
    MFC after:	2 days
    
    
    Former-commit-id: 11efdc1ffa8ffff64540afcb5b3cdabe5d4fbda6

--- sys/contrib/pf/net/pf.c
+++ sys/contrib/pf/net/pf.c
@@ -12,6 +12,10 @@ comment|/* add	$OpenBSD: pf.c,v 1.448 2004/05/11 07:34:11 dhartmei Exp $ */
 end_comment
 
 begin_comment
+comment|/* add	$OpenBSD: pf.c,v 1.483 2005/03/15 17:38:43 dhartmei Exp $ */
+end_comment
+
+begin_comment
 comment|/*  * Copyright (c) 2001 Daniel Hartmeier  * Copyright (c) 2002,2003 Henning Brauer  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  *    - Redistributions of source code must retain the above copyright  *      notice, this list of conditions and the following disclaimer.  *    - Redistributions in binary form must reproduce the above  *      copyright notice, this list of conditions and the following  *      disclaimer in the documentation and/or other materials provided  *      with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE  * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * Effort sponsored in part by the Defense Advanced Research Projects  * Agency (DARPA) and Air Force Research Laboratory, Air Force  * Materiel Command, USAF, under agreement number F30602-01-2-0537.  *  */
 end_comment
 
@@ -16784,6 +16788,11 @@ operator|&
 name|mss
 argument_list|,
 literal|2
+argument_list|)
+expr_stmt|;
+name|NTOHS
+argument_list|(
+name|mss
 argument_list|)
 expr_stmt|;
 comment|/* FALLTHROUGH */
