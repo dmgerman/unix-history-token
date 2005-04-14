deee134ad8ad0c21f75ddfcc17232b6014bfb8e8
Peter Wemm <peter@FreeBSD.org>
2005-04-14 04:13:27 +0000

    It seems I introduced a new prerequisite for <machine/pcb.h> on i386, which is included from <sys/user.h>.  Add a bandaid for userland.
    Former-commit-id: 5ef3015eb6b388cf3fa81a85598798f8164665ac

--- sys/i386/include/pcb.h
+++ sys/i386/include/pcb.h
@@ -19,6 +19,23 @@ begin_comment
 comment|/*  * Intel 386 process control block  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_KERNEL
+end_ifndef
+
+begin_include
+include|#
+directive|include
+file|<machine/segments.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
