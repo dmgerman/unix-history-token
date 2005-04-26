ce130ed00613400b5974cc5930df7633ddaa8019
Darren Reed <darrenr@FreeBSD.org>
2005-04-26 14:59:26 +0000

    Missing file from vendor branch import.
    Former-commit-id: b4b0362f763f6aa43f6fbd9f2fca617fcf5d7e70

--- contrib/ipfilter/ip_fil.c
+++ contrib/ipfilter/ip_fil.c
@@ -1,9 +1,5 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	$NetBSD$	*/
-end_comment
-
-begin_comment
 comment|/*  * Copyright (C) 1993-2001 by Darren Reed.  *  * See the IPFILTER.LICENCE file for details on licencing.  */
 end_comment
 
@@ -35,7 +31,7 @@ name|char
 name|rcsid
 index|[]
 init|=
-literal|"@(#)Id: ip_fil.c,v 2.133.2.9 2005/01/08 14:22:18 darrenr Exp"
+literal|"@(#)$Id: ip_fil.c,v 2.133.2.9 2005/01/08 14:22:18 darrenr Exp $"
 decl_stmt|;
 end_decl_stmt
 
