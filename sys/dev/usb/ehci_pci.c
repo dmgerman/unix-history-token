73ea71c7a2ba15bd2860d246f8d531c906c926d4
Julian Elischer <julian@FreeBSD.org>
2005-04-26 20:52:07 +0000

    Add missing include.
    Former-commit-id: a782603a6211100b68078f38ace5669a34542850

--- sys/dev/usb/ehci_pci.c
+++ sys/dev/usb/ehci_pci.c
@@ -80,6 +80,16 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/clock.h>
+end_include
+
+begin_comment
+comment|/* for DELAY() */
+end_comment
+
+begin_include
+include|#
+directive|include
 file|<sys/rman.h>
 end_include
 
