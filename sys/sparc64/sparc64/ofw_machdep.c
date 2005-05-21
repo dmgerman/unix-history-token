be95517329ac11a51ef13bb7b1b4568c75417ff7
Marius Strobl <marius@FreeBSD.org>
2005-05-21 20:17:01 +0000

    - Make sure that the OFW address properties that are going to be decode   consist of the expected number of address and size cells (we can't use   dynamic arrays here because at the point in the boot process when this   code is used malloc() doesn't work, yet). This fixes a Fast Data Access   MMU Miss when uart(4) (erroneously) calls OF_decode_addr() to decode   the address of PS/2 keyboards. PS/2 keyboards use a different and also   undocumented scheme at the first parent node than mapping at 'ranges'   properties. It's however not worth implementing that other scheme and   actually also fits atkbdc(4) better to just start at the first parent   node of PS/2 keyboards which is the 8042 controller (I have atkbdc(4)   working that way). - Use FBSDID.
    MFC after:	1 month
    
    
    Former-commit-id: 6c3fc0dc55bd1682c30e39f790edb82a6eab3913

--- sys/sparc64/sparc64/ofw_machdep.c
+++ sys/sparc64/sparc64/ofw_machdep.c
@@ -1,7 +1,21 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2001 by Thomas Moestl<tmm@FreeBSD.org>.  * Copyright (c) 2005 by Marius Strobl<marius@FreeBSD.org>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2001 by Thomas Moestl<tmm@FreeBSD.org>.  * Copyright (c) 2005 by Marius Strobl<marius@FreeBSD.org>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/*  * Some Open Firmware helper functions that are likely machine dependent.  */
@@ -484,6 +498,18 @@ literal|1
 expr_stmt|;
 if|if
 condition|(
+name|addrc
+operator|<
+literal|2
+operator|||
+name|addrc
+operator|>
+literal|3
+operator|||
+name|szc
+operator|<
+literal|1
+operator|||
 name|szc
 operator|>
 literal|2
@@ -796,6 +822,10 @@ expr_stmt|;
 if|if
 condition|(
 name|paddrc
+operator|<
+literal|2
+operator|||
+name|paddrc
 operator|>
 literal|3
 condition|)
@@ -902,6 +932,10 @@ literal|1
 expr_stmt|;
 if|if
 condition|(
+name|szc
+operator|<
+literal|1
+operator|||
 name|szc
 operator|>
 literal|2
