7c8eaf910fd5b654ffa9cff16d9019e645f07c7f
Robert N. M. Watson <rwatson@FreeBSD.org>
2005-05-29 16:11:34 +0000

    For consistency with more system include files, add a trailing '_' to the define guards in audit_kevents.h.
    Former-commit-id: 1ccb1707e8f2f551147159cf9f072e18b43213e7

--- sys/bsm/audit_kevents.h
+++ sys/bsm/audit_kevents.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_BSM_AUDIT_KEVENTS_H
+name|_BSM_AUDIT_KEVENTS_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_BSM_AUDIT_KEVENTS_H
+name|_BSM_AUDIT_KEVENTS_H_
 end_define
 
 begin_comment
@@ -32,7 +32,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_BSM_AUDIT_KEVENTS_H */
+comment|/* !_BSM_AUDIT_KEVENTS_H_ */
 end_comment
 
 end_unit
