6a53d4f4a07546242d48a48cb9b9f06c093c8f3d
Philippe Charnier <charnier@FreeBSD.org>
2005-05-29 15:57:00 +0000

    rscid -> __FBSDID. Mark parameter as __unused when necessary.
    Former-commit-id: f5e86279b8ee323e16c76e3047b9acb8b61ec813

--- sbin/restore/interactive.c
+++ sbin/restore/interactive.c
@@ -21,17 +21,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
 begin_endif
 endif|#
 directive|endif
@@ -40,6 +29,20 @@ end_endif
 begin_comment
 comment|/* not lint */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -3593,6 +3596,7 @@ name|onintr
 parameter_list|(
 name|int
 name|signo
+name|__unused
 parameter_list|)
 block|{
 if|if
