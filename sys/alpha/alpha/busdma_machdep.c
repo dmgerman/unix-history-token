134ba90187bbb5298e07097423caa2cea6629ba6
Marcel Moolenaar <marcel@FreeBSD.org>
2005-06-01 05:14:57 +0000

    MFC 1.51: In bus_dmamap_load(), temporarily point the segments pointer in the DMA tag to a local variable so that we don't dereference a NULL pointer.
    PR: alpha/30486
    
    Former-commit-id: 47af3e86e2ac083e6625eeed735b3e3269fc3a5c

--- sys/alpha/alpha/busdma_machdep.c
+++ sys/alpha/alpha/busdma_machdep.c
@@ -2071,6 +2071,9 @@ name|int
 name|flags
 parameter_list|)
 block|{
+name|bus_dma_segment_t
+name|segment
+decl_stmt|;
 name|vm_offset_t
 name|vaddr
 decl_stmt|;
@@ -2121,6 +2124,13 @@ name|vm_offset_t
 operator|)
 name|buf
 argument_list|)
+expr_stmt|;
+name|dmat
+operator|->
+name|segments
+operator|=
+operator|&
+name|segment
 expr_stmt|;
 name|dmat
 operator|->
@@ -2203,6 +2213,12 @@ literal|1
 argument_list|,
 name|error
 argument_list|)
+expr_stmt|;
+name|dmat
+operator|->
+name|segments
+operator|=
+name|NULL
 expr_stmt|;
 return|return
 operator|(
