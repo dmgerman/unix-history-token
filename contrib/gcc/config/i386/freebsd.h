a9ca4325912549c4500ae7992376d2ee0e8a3a93
Alexander Kabaev <kan@FreeBSD.org>
2005-06-03 05:58:56 +0000

    Fix a typo from previos commit.
    Former-commit-id: d28e240533e6a31943ce57304624478e506147ab

--- contrib/gcc/config/i386/freebsd.h
+++ contrib/gcc/config/i386/freebsd.h
@@ -3,10 +3,6 @@ begin_comment
 comment|/* Definitions for Intel 386 running FreeBSD with ELF format    Copyright (C) 1996, 2000, 2002 Free Software Foundation, Inc.    Contributed by Eric Youngdale.    Modified for stabs-in-ELF by H.J. Lu.    Adapted from GNU/Linux version by John Polstra.    Continued development by David O'Brien<obrien@freebsd.org>  This file is part of GCC.  GCC is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  GCC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with GCC; see the file COPYING.  If not, write to the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
-begin_expr_stmt
-operator|-
-end_expr_stmt
-
 begin_comment
 comment|/* $FreeBSD$ */
 end_comment
