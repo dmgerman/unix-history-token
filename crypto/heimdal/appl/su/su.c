920029cd1686791a1b0a6d4497e87f8bbeed9cae
Ken Smith <kensmith@FreeBSD.org>
2005-07-07 14:16:38 +0000

    This is sort of an MFS.  Peter made these changes to the RELENG_* branches but missed HEAD.  This patch extends his a little bit, setting it up via the Makefiles so that adding _FREEFALL_CONFIG to /etc/make.conf is the only thing needed to cluster-ize things (current setup also requires overriding CFLAGS).
    From Peter's commit to the RELENG_* branches:
    > Add the freebsd.org custer's source modifications under #ifdefs to aid
    > keeping things in sync.  For ksu:
    > * install suid-root by default
    > * don't fall back to asking for a unix password (ie: be pure kerberos)
    > * allow custom user instances for things like www and not just root
    
    The Makefile tweaks will be MFC-ed, the rest is already done.
    
    MFC after:      3 days
    Approved by:    re (dwhite)
    
    
    Former-commit-id: 2c872f045f8fa82f2b16d04407112697449089d2

--- crypto/heimdal/appl/su/su.c
+++ crypto/heimdal/appl/su/su.c
@@ -3,6 +3,10 @@ begin_comment
 comment|/*  * Copyright (c) 1999 - 2003 Kungliga Tekniska Högskolan  * (Royal Institute of Technology, Stockholm, Sweden).   * All rights reserved.   *  * Redistribution and use in source and binary forms, with or without   * modification, are permitted provided that the following conditions   * are met:   *  * 1. Redistributions of source code must retain the above copyright   *    notice, this list of conditions and the following disclaimer.   *  * 2. Redistributions in binary form must reproduce the above copyright   *    notice, this list of conditions and the following disclaimer in the   *    documentation and/or other materials provided with the distribution.   *  * 3. Neither the name of KTH nor the names of its contributors may be  *    used to endorse or promote products derived from this software without  *    specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY KTH AND ITS CONTRIBUTORS ``AS IS'' AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL KTH OR ITS CONTRIBUTORS BE  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR  * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 end_comment
 
+begin_comment
+comment|/*  * $FreeBSD$  */
+end_comment
+
 begin_include
 include|#
 directive|include
@@ -830,10 +834,19 @@ name|p
 argument_list|,
 name|NULL
 argument_list|,
+ifdef|#
+directive|ifdef
+name|_FREEFALL_CONFIG
+name|login_name
+argument_list|,
+else|#
+directive|else
 name|su_info
 operator|->
 name|pw_name
 argument_list|,
+endif|#
+directive|endif
 name|NULL
 argument_list|)
 expr_stmt|;
@@ -846,12 +859,17 @@ literal|1
 return|;
 if|if
 condition|(
+ifndef|#
+directive|ifndef
+name|_FREEFALL_CONFIG
 name|su_info
 operator|->
 name|pw_uid
 operator|!=
 literal|0
 operator|||
+endif|#
+directive|endif
 name|krb5_kuserok
 argument_list|(
 name|context
@@ -2061,6 +2079,9 @@ operator|&&
 name|login_info
 operator|->
 name|pw_uid
+ifndef|#
+directive|ifndef
+name|_FREEFALL_CONFIG
 operator|&&
 name|verify_unix
 argument_list|(
@@ -2068,6 +2089,8 @@ name|su_info
 argument_list|)
 operator|!=
 literal|0
+endif|#
+directive|endif
 condition|)
 block|{
 name|printf
