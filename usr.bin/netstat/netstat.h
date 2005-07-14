bf66bc7764b1f269b8b0f9448b4a3df777c61b7c
Max Laier <mlaier@FreeBSD.org>
2005-07-14 22:42:35 +0000

    Print newly exported pfsync statistics with netstat(8).
    Requested by:	glebius
    MFC after:	1 week
    
    
    Former-commit-id: 65eb5568be9493f97d059f39e029fa59ba0ae73c

--- usr.bin/netstat/netstat.h
+++ usr.bin/netstat/netstat.h
@@ -389,6 +389,21 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|pfsync_stats
+parameter_list|(
+name|u_long
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_ifdef
 ifdef|#
 directive|ifdef
