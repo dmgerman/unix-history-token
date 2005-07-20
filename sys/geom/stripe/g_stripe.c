8911a197298ed5c5968ca0babf5616a57675630a
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2005-07-20 13:15:11 +0000

    MFC:	g_concat.c	1.25 	g_stripe.c	1.26
    Before calling g_orphan_provider(), add G_PF_WITHER flag, so GEOM will know
    to destroy it.
    
    PR:		kern/81758
    Submitted by:	trasz <trasz@buziaczek.pl>
    
    Former-commit-id: b5a640588c636bbb2ff44a07c5f96cea8010488a

--- sys/geom/stripe/g_stripe.c
+++ sys/geom/stripe/g_stripe.c
@@ -795,6 +795,14 @@ operator|!=
 name|NULL
 condition|)
 block|{
+name|sc
+operator|->
+name|sc_provider
+operator|->
+name|flags
+operator||=
+name|G_PF_WITHER
+expr_stmt|;
 name|g_orphan_provider
 argument_list|(
 name|sc
