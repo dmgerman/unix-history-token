e57c2e1eaa1a37bf0940c3a4bb4ad46845dd656e
Hajimu UMEMOTO <ume@FreeBSD.org>
2005-07-25 12:36:43 +0000

    include netinet6/scope6_var.h.
    Former-commit-id: 45f3402487372d041b5e0e20d066e48d4f3cd254

--- sys/netinet/ip_carp.c
+++ sys/netinet/ip_carp.c
@@ -273,6 +273,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<netinet6/scope6_var.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<netinet6/nd6.h>
 end_include
 
