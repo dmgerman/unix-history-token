a3edac99d1e9edcd4fb105a7aba2efddb6d71b09
Hajimu UMEMOTO <ume@FreeBSD.org>
2005-07-26 00:19:58 +0000

    include scope6_var.h for in6_clearscope().
    Former-commit-id: b7b8ef7f7c0acaf8c8a23b0796104c38e515bfe2

--- sys/netinet/ip_fw2.c
+++ sys/netinet/ip_fw2.c
@@ -331,6 +331,23 @@ directive|include
 file|<netinet/icmp6.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|INET6
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<netinet6/scope6_var.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
