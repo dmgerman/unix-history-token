efe3802c5cefb9c2a7efb47f47062d49258e38ce
Soren Schmidt <sos@FreeBSD.org>
2005-08-05 13:05:45 +0000

    Make IOCATARAIDSTATUS IOWR so status can be gotten correctly
    Former-commit-id: e5a0777e7b3343015cb7cfeecf338ebdfe4dd13d

--- sys/sys/ata.h
+++ sys/sys/ata.h
@@ -2211,7 +2211,7 @@ begin_define
 define|#
 directive|define
 name|IOCATARAIDSTATUS
-value|_IOR('a', 202, struct ata_ioc_raid_config)
+value|_IOWR('a', 202, struct ata_ioc_raid_config)
 end_define
 
 begin_define
