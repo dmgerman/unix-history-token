06137ee92a155a022c06725b019b7c618d1da165
Stefan Farfeleder <stefanf@FreeBSD.org>
2005-08-10 13:38:01 +0000

    Submitted to and merged from NetBSD (rev. 1.23 and 1.24): - Don't delete the current line when typing `yy'. - Don't use a possibly stale pointer in cv_paste(). -
    Former-commit-id: fbf3ec05f5c88461ed312163c50d9ab14e8270a5

--- lib/libedit/vi.c
+++ lib/libedit/vi.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Christos Zoulas of Cornell University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$NetBSD: vi.c,v 1.22 2005/08/08 14:05:37 christos Exp $  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Christos Zoulas of Cornell University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$NetBSD: vi.c,v 1.24 2005/08/10 12:46:24 christos Exp $  */
 end_comment
 
 begin_if
@@ -203,6 +203,16 @@ name|pos
 operator|=
 literal|0
 expr_stmt|;
+if|if
+condition|(
+operator|!
+operator|(
+name|c
+operator|&
+name|YANK
+operator|)
+condition|)
+block|{
 name|el
 operator|->
 name|el_line
@@ -227,6 +237,7 @@ name|el_line
 operator|.
 name|buffer
 expr_stmt|;
+block|}
 if|if
 condition|(
 name|c
@@ -300,10 +311,6 @@ name|int
 name|c
 parameter_list|)
 block|{
-name|char
-modifier|*
-name|ptr
-decl_stmt|;
 name|c_kill_t
 modifier|*
 name|k
@@ -395,14 +402,6 @@ operator|.
 name|cursor
 operator|++
 expr_stmt|;
-name|ptr
-operator|=
-name|el
-operator|->
-name|el_line
-operator|.
-name|cursor
-expr_stmt|;
 name|c_insert
 argument_list|(
 name|el
@@ -436,7 +435,11 @@ name|void
 operator|)
 name|memcpy
 argument_list|(
-name|ptr
+name|el
+operator|->
+name|el_line
+operator|.
+name|cursor
 argument_list|,
 name|k
 operator|->
