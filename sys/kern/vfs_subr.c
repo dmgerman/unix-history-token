8452475c959825dc6af230935785ac7a4e7ccf7c
Alexander Kabaev <kan@FreeBSD.org>
2005-08-15 13:50:38 +0000

    MFC r1.639: Do not drop the vnode interlock if vdropl is called on already doomed vnode. vdropl callers expect it to return with interlock still being held.
    Approved by:	re (hrs)
    
    
    Former-commit-id: a16a692c7b3dcd23e49e73261263b1469711d45e

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -10160,14 +10160,7 @@ name|v_iflag
 operator|&
 name|VI_DOOMED
 condition|)
-block|{
-name|VI_UNLOCK
-argument_list|(
-name|vp
-argument_list|)
-expr_stmt|;
 return|return;
-block|}
 name|vp
 operator|->
 name|v_iflag
