a9feed231e84c2d5dc41d9e2de4b1499de5f26e2
Christian S.J. Peron <csjp@FreeBSD.org>
2005-08-19 04:50:08 +0000

    Synch with HEAD:
    MFC revision 1.167
    date: 2005/08/08 21:06:42;  author: csjp;  state: Exp;  lines: +2 -0
    Drop in a WITNESS_WARN into SYSCTL_IN to make sure that we are
    not holding any non-sleep-able-locks locks when copyin is called.
    This gets executed un-conditionally since we have no function
    to wire the buffer in this direction.
    
    MFC revision 1.166
    date: 2005/08/08 18:54:35;  author: csjp;  state: Exp;  lines: +6 -1
    Check to see if we wired the user-supplied buffers in SYSCTL_OUT, if
    the buffer has not been wired and we are holding any non-sleep-able locks,
    drop a witness warning. If the buffer has not been wired, it is possible
    that the writing of the data can sleep, especially if the page is not in
    memory. This can result in a number of different locking issues, including
    dead locks.
    
    Approved by:	re (kensmith)
    
    
    Former-commit-id: 6420141940ea391f2218aac983546ac70d59c273

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -4748,13 +4748,14 @@ operator|(
 literal|0
 operator|)
 return|;
+comment|/* 	 * If we have not wired the user supplied buffer and we are currently 	 * holding locks, drop a witness warning, as it's possible that 	 * write operations to the user page can sleep. 	 */
 if|if
 condition|(
 name|req
 operator|->
 name|lock
-operator|==
-name|REQ_LOCKED
+operator|!=
+name|REQ_WIRED
 condition|)
 name|WITNESS_WARN
 argument_list|(
@@ -4901,6 +4902,17 @@ operator|(
 name|EINVAL
 operator|)
 return|;
+name|WITNESS_WARN
+argument_list|(
+name|WARN_GIANTOK
+operator||
+name|WARN_SLEEPOK
+argument_list|,
+name|NULL
+argument_list|,
+literal|"sysctl_new_user()"
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|copyin
