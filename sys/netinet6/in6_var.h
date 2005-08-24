21f0d3a2f9c15b9aeeb85d19fcac4618c76fcc86
Robert N. M. Watson <rwatson@FreeBSD.org>
2005-08-24 15:18:38 +0000

    Merge in_var.h:1.54, in6_var.h:1.23, mld6.c:1.22 from HEAD to RELENG_6:
      Modify network protocol consumers of the ifnet multicast address lists
      to lock if_addr_mtx.
    
      Problem reported by:    Ed Maste <emaste at phaedrus dot sandvine dot ca>
    
    Approved by:	re (scottl)
    
    
    Former-commit-id: 85c4f8c88b66c7da463d8c5c39aa1fb6d0f9af83

--- sys/netinet6/in6_var.h
+++ sys/netinet6/in6_var.h
@@ -1726,7 +1726,7 @@ comment|/* struct ifnet *ifp; */
 define|\
 comment|/* struct in6_multi *in6m; */
 define|\
-value|do { \ 	struct ifmultiaddr *ifma; \ 	TAILQ_FOREACH(ifma,&(ifp)->if_multiaddrs, ifma_link) { \ 		if (ifma->ifma_addr->sa_family == AF_INET6 \&& IN6_ARE_ADDR_EQUAL(&((struct sockaddr_in6 *)ifma->ifma_addr)->sin6_addr, \&(addr))) \ 			break; \ 	} \ 	(in6m) = (struct in6_multi *)(ifma ? ifma->ifma_protospec : 0); \ } while(0)
+value|do { \ 	struct ifmultiaddr *ifma; \ 	IF_ADDR_LOCK(ifp); \ 	TAILQ_FOREACH(ifma,&(ifp)->if_multiaddrs, ifma_link) { \ 		if (ifma->ifma_addr->sa_family == AF_INET6 \&& IN6_ARE_ADDR_EQUAL(&((struct sockaddr_in6 *)ifma->ifma_addr)->sin6_addr, \&(addr))) \ 			break; \ 	} \ 	(in6m) = (struct in6_multi *)(ifma ? ifma->ifma_protospec : 0); \ 	IF_ADDR_UNLOCK(ifp); \ } while(0)
 end_define
 
 begin_comment
