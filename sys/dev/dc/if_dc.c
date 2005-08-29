ab9547842ae8c9a30c2acdca5bcbaa36ded3f5ef
Warner Losh <imp@FreeBSD.org>
2005-08-29 17:03:31 +0000

    Fix another instance of old info re: miibus
    Former-commit-id: 653b86a0bc81d091a99b00728f189975ef9c38fc

--- sys/dev/dc/if_dc.c
+++ sys/dev/dc/if_dc.c
@@ -282,7 +282,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_comment
-comment|/* "controller miibus0" required.  See GENERIC if you get errors here. */
+comment|/* "device miibus" required.  See GENERIC if you get errors here. */
 end_comment
 
 begin_include
