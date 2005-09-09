389789cebe652160655b7c41b897d09a24a952e9
Stefan Farfeleder <stefanf@FreeBSD.org>
2005-09-09 21:03:34 +0000

    - Provide a reasonable error message for `export -p var'. - Use argv rather than argptr since getopt() is used here.
    Former-commit-id: 91e276191d2ffc6465113ae028ddce67b47af4a6

--- bin/sh/var.c
+++ bin/sh/var.c
@@ -2407,6 +2407,19 @@ name|argv
 operator|+=
 name|optind
 expr_stmt|;
+if|if
+condition|(
+name|values
+operator|&&
+name|argc
+operator|!=
+literal|0
+condition|)
+name|error
+argument_list|(
+literal|"-p requires no arguments"
+argument_list|)
+expr_stmt|;
 name|listsetvar
 argument_list|(
 name|cmdenviron
@@ -2425,7 +2438,7 @@ operator|(
 name|name
 operator|=
 operator|*
-name|argptr
+name|argv
 operator|++
 operator|)
 operator|!=
