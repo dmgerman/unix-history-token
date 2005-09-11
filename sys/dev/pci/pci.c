f851eda0df6336fca7d436aad5fad3078252db2b
Warner Losh <imp@FreeBSD.org>
2005-09-11 19:28:30 +0000

    Change the default of pci_do_powerstate to 0, per request from re@. The number of raid controllers that violate the WHQL seems to be growing in number and not isolated to old versions as previously thought.  Though the numbers of these seen in the wild is still relatively small, they hang the system when parts of their devices are powered down.  The one area that these parts appear often are in the higher end servers.  As such, be conservative about powering down devices that have no driver attached by default.  Until a better approach is proven in current, this is the prudent choice.
    Laptop users wishing the benefits of powering down devices with no
    drivers will now need to set hw.pci.do_powerstate=1 in their
    /boot/loader.conf file.  Some users will have devices that will
    prevent this setting (hence the need to make it default 0).
    
    Approved by: re@ (scottl)
    
    
    Former-commit-id: 8cd9b6613fde1908767fc85ef1e637d03a66c643

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -1008,7 +1008,7 @@ specifier|static
 name|int
 name|pci_do_powerstate
 init|=
-literal|1
+literal|0
 decl_stmt|;
 end_decl_stmt
 
@@ -1037,7 +1037,7 @@ argument_list|,
 operator|&
 name|pci_do_powerstate
 argument_list|,
-literal|1
+literal|0
 argument_list|,
 literal|"Power down devices into D3 state when no driver attaches to them.\n\ Otherwise, leave the device in D0 state when no driver attaches."
 argument_list|)
