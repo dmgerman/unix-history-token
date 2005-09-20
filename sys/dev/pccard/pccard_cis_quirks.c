4806adfe7c4cc9ed86d539cc716554862599cea6
Gleb Smirnoff <glebius@FreeBSD.org>
2005-09-20 10:25:51 +0000

    Fix build.
    Former-commit-id: a6923385e6afc84f3d2fa0902b1c819b2c973f85

--- sys/dev/pccard/pccard_cis_quirks.c
+++ sys/dev/pccard/pccard_cis_quirks.c
@@ -72,6 +72,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<dev/pccard/pccardvarp.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"pccarddevs.h"
 end_include
 
