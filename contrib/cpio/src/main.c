e332efd3b4d06b60852d24a92afda06b94eab370
Tim J. Robbins <tjr@FreeBSD.org>
2005-10-01 05:49:29 +0000

    Merge from old main.c rev 1.2: Don't set the umask until after we have processed the arguments and opened the archive file.
    Former-commit-id: 42ca151abbcd8bc85005dd7c966cb61c2768aeee

--- contrib/cpio/src/main.c
+++ contrib/cpio/src/main.c
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/* main.c - main program and argument processing for cpio.    Copyright (C) 1990, 1991, 1992, 2001, 2003, 2004 Free Software Foundation, Inc.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License along    with this program; if not, write to the Free Software Foundation, Inc.,    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
@@ -3239,11 +3243,6 @@ index|[
 literal|0
 index|]
 expr_stmt|;
-name|umask
-argument_list|(
-literal|0
-argument_list|)
-expr_stmt|;
 ifdef|#
 directive|ifdef
 name|__TURBOC__
@@ -3283,6 +3282,11 @@ argument_list|(
 name|argc
 argument_list|,
 name|argv
+argument_list|)
+expr_stmt|;
+name|umask
+argument_list|(
+literal|0
 argument_list|)
 expr_stmt|;
 name|initialize_buffers
