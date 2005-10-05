718a84b10b9893c58783de398dcbcd4a73f6fbe3
Warner Losh <imp@FreeBSD.org>
2005-10-05 05:21:07 +0000

    MFp4: 	o Add support for Tamarack TC5299J + MII found on SMC 8041TX V.2 	  and corega PCCCCTXD 	o Add support for ISA/PCI RTL80[12]9 chips 	o Improve support for the ax88790 based 	o minor code movement
    Submitted by: (#2) David Madole
    
    
    Former-commit-id: e9b82486e429c75183c38338fe123bcc1bc79389

--- sys/dev/ed/if_edvar.h
+++ sys/dev/ed/if_edvar.h
@@ -728,6 +728,19 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|int
+name|ed_probe_RTL80x9
+parameter_list|(
+name|device_t
+parameter_list|,
+name|int
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_ifdef
 ifdef|#
 directive|ifdef
