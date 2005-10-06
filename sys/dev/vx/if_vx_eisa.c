b3ad5d70ae1a607f84230f32a67d7c458e246ccf
John Baldwin <jhb@FreeBSD.org>
2005-10-06 18:41:31 +0000

    Fix another edge case I just noticed when committing the previous changes: If bus_setup_intr() fails, cleanup the ifnet setup in vx_attach() by calling ether_ifdetach() and if_free().
    MFC after:	1 week
    
    
    Former-commit-id: 8aaa2745a2b8acc530b2a35af085f74b4217047c

--- sys/dev/vx/if_vx_eisa.c
+++ sys/dev/vx/if_vx_eisa.c
@@ -74,6 +74,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<net/ethernet.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<net/if.h>
 end_include
 
@@ -555,6 +561,20 @@ operator|&
 name|sc
 operator|->
 name|vx_mtx
+argument_list|)
+expr_stmt|;
+name|ether_ifdetach
+argument_list|(
+name|sc
+operator|->
+name|vx_ifp
+argument_list|)
+expr_stmt|;
+name|if_free
+argument_list|(
+name|sc
+operator|->
+name|vx_ifp
 argument_list|)
 expr_stmt|;
 name|bad
