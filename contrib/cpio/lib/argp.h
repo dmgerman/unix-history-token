a89db9fbf4da5d344bac22aa6f6d80aeeb615b4e
Tim J. Robbins <tjr@FreeBSD.org>
2005-10-17 12:20:05 +0000

    In __option_is_short(), avoid calling isprint() on key characters outside the range allowed by that function, resulting in undefined behaviour. Our undefined behaviour in multibyte locales happened to differ from glibc's, resulting in errors parsing option strings.
    Obtained from:	Corinna Vinschen (Red Hat)
    
    
    Former-commit-id: 17e41d649a02f1d68b01a91eb25a27ebcc55b6df

--- contrib/cpio/lib/argp.h
+++ contrib/cpio/lib/argp.h
@@ -1,5 +1,9 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/* Hierarchial argument parsing, layered over getopt.    Copyright (C) 1995-1999,2003,2004 Free Software Foundation, Inc.    This file is part of the GNU C Library.    Written by Miles Bader<miles@gnu.ai.mit.edu>.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License along    with this program; if not, write to the Free Software Foundation,    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
@@ -1408,6 +1412,10 @@ return|return
 name|__key
 operator|>
 literal|0
+operator|&&
+name|__key
+operator|<
+literal|256
 operator|&&
 name|isprint
 argument_list|(
