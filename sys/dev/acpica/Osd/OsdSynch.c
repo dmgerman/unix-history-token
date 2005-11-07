810c15bac319062345274fcc14871c0934e3bf0d
David E. O'Brien <obrien@FreeBSD.org>
2005-11-07 09:53:25 +0000

    MFC: canonify the include of acpi.h.
    Former-commit-id: ced1a6f4a9c2b9c0443f5859801e71cd370b98f2

--- sys/dev/acpica/Osd/OsdSynch.c
+++ sys/dev/acpica/Osd/OsdSynch.c
@@ -24,7 +24,7 @@ end_expr_stmt
 begin_include
 include|#
 directive|include
-file|"acpi.h"
+file|<contrib/dev/acpica/acpi.h>
 end_include
 
 begin_include
--- sys/dev/acpica/acpi_package.c
+++ sys/dev/acpica/acpi_package.c
@@ -62,7 +62,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|"acpi.h"
+file|<contrib/dev/acpica/acpi.h>
 end_include
 
 begin_include
