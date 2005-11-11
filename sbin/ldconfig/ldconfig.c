c79a6387069ce6ea7c061b96b289b9bcb8d3522a
John Baldwin <jhb@FreeBSD.org>
2005-11-11 21:24:21 +0000

    Document -32 flag in usage() output as well.
    Requested by:	ru
    MFC after:	1 week
    
    
    Former-commit-id: f07b8107dc0abb5f9a9e561c1218d4d5d017c2f9

--- sbin/ldconfig/ldconfig.c
+++ sbin/ldconfig/ldconfig.c
@@ -968,7 +968,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: ldconfig [-aout | -elf] [-Rimrsv] [-f hints_file] [directory | file ...]\n"
+literal|"usage: ldconfig [-32] [-aout | -elf] [-Rimrsv] [-f hints_file] [directory | file ...]\n"
 argument_list|)
 expr_stmt|;
 name|exit
