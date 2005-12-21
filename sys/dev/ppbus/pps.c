04b013e49f961878121554179d1f8b4f0ae24958
Ruslan Ermilov <ru@FreeBSD.org>
2005-12-21 10:54:47 +0000

    Add missing MODULE_DEPEND() so that ppbus.ko and these .ko's can be loaded dynamically.
    Former-commit-id: b77a897fcab8040234c8e5c606977691f6864dfb

--- sys/dev/ppbus/pps.c
+++ sys/dev/ppbus/pps.c
@@ -1835,5 +1835,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|pps
+argument_list|,
+name|ppbus
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
