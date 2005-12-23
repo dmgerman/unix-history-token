6295470ed61a13e6dafc98061ac9eec6dd69a109
Takahashi Yoshihiro <nyan@FreeBSD.org>
2005-12-23 12:14:55 +0000

    Fix build error.
    Former-commit-id: 17680762da5fe67c07cc0f26858260eb7238ebb7

--- sys/pc98/cbus/clock.c
+++ sys/pc98/cbus/clock.c
@@ -138,6 +138,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/cpu.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/cputypes.h>
 end_include
 
