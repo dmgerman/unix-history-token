b1528bbf044550b61b530b39944ae1a6ff8aea7c
David Xu <davidxu@FreeBSD.org>
2005-12-24 02:59:29 +0000

    Avoid kernel panic when attaching a process which may not be stopped by debugger, e.g process is dumping core. Only access p_xthread if P_STOPPED_TRACE is set, this means thread is ready to exchange signal with debugger, print a warning if P_STOPPED_TRACE is not set due to some bugs in other code, if there is.
    The patch has been tested by Anish Mistry mistry.7 at osu dot edu, and
    is slightly adjusted.
    
    
    Former-commit-id: 2e6b16aed65dc9041630457b873618787592aca0

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -11242,12 +11242,6 @@ name|p_xstat
 operator|=
 name|sig
 expr_stmt|;
-name|p
-operator|->
-name|p_xthread
-operator|=
-name|td
-expr_stmt|;
 name|mtx_lock_spin
 argument_list|(
 operator|&
@@ -12456,12 +12450,6 @@ operator|->
 name|p_xstat
 operator|=
 name|sig
-expr_stmt|;
-name|p
-operator|->
-name|p_xthread
-operator|=
-name|td
 expr_stmt|;
 name|mtx_lock_spin
 argument_list|(
