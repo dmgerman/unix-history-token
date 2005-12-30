187adb25693bf1dd08f6537e3cd0b052cb862364
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2005-12-30 11:45:07 +0000

    Improve memguard a bit: - Provide tunable vm.memguard.desc, so one can specify memory type without   changing the code and recompiling the kernel. - Allow to use memguard for kernel modules by providing sysctl   vm.memguard.desc, which can be changed to short description of memory   type before module is loaded. - Move as much memguard code as possible to memguard.c. - Add sysctl node vm.memguard. and move memguard-specific sysctl there. - Add malloc_desc2type() function for finding memory type based on its   short description (ks_shortdesc field). - Memory type can be changed (via vm.memguard.desc sysctl) only if it   doesn't exist (will be loaded later) or when no memory is allocated yet.   If there is allocated memory for the given memory type, return EBUSY. - Implement two ways of memory types comparsion and make safer/slower the   default.
    Former-commit-id: f3c26c99b5fdec7161f172080250055803c73750

--- sys/sys/malloc.h
+++ sys/sys/malloc.h
@@ -651,6 +651,20 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|struct
+name|malloc_type
+modifier|*
+name|malloc_desc2type
+parameter_list|(
+specifier|const
+name|char
+modifier|*
+name|desc
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
