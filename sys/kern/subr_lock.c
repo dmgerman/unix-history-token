f7471b0211017404f149ebc66b2a5f60f3fc980a
John Baldwin <jhb@FreeBSD.org>
2006-01-17 16:58:32 +0000

    Bah.  Fix 'show lock' to actually be compiled in.  I had just fixed this in p4 but had an older subr_lock.c on the machine I committed to CVS from.
    Former-commit-id: 55ce79dfd6939b25af3ae1f0592f3a2efd0eca6b

--- sys/kern/subr_lock.c
+++ sys/kern/subr_lock.c
@@ -24,6 +24,12 @@ end_expr_stmt
 begin_include
 include|#
 directive|include
+file|"opt_ddb.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/param.h>
 end_include
 
@@ -37,6 +43,12 @@ begin_include
 include|#
 directive|include
 file|<sys/ktr.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/linker_set.h>
 end_include
 
 begin_include
