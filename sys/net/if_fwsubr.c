41867ca4073381148d8bf20bd0ec6bdd4746b705
Brooks Davis <brooks@FreeBSD.org>
2006-01-24 06:30:51 +0000

    Backout ill considered MFC of 1.20.  It doesn't compile.
    Pointy hat:	brooks
    
    
    Former-commit-id: b4aac33beae6896796db620d64461091c017bb9a

--- sys/net/if_fwsubr.c
+++ sys/net/if_fwsubr.c
@@ -2572,16 +2572,10 @@ name|ETHERTYPE_IP
 case|:
 if|if
 condition|(
-operator|(
-name|m
-operator|=
 name|ip_fastforward
 argument_list|(
 name|m
 argument_list|)
-operator|)
-operator|==
-name|NULL
 condition|)
 return|return;
 name|isr
