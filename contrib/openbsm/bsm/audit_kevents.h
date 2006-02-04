7d53f11dd60615dbc5053bcbf6f53357807a39b1
Robert N. M. Watson <rwatson@FreeBSD.org>
2006-02-04 13:17:48 +0000

    Import OpenBSM 1.0 alpha 2, a minor update on alpha 1:
    - Man page formatting improvements.
    - A number of new audit event identifiers for FreeBSD, Linux, and POSIX.1b
      events.
    - Remove 'tfm' class, unused in OpenBSM.
    
    Obtained from:	TrustedBSD Project
    
    
    Former-commit-id: c3c3db77cc239a0763c0c59e62ea7ee68e29279d

--- contrib/openbsm/bsm/audit_kevents.h
+++ contrib/openbsm/bsm/audit_kevents.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 2005 Apple Computer, Inc.  * All rights reserved.  *  * @APPLE_BSD_LICENSE_HEADER_START@  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * @APPLE_BSD_LICENSE_HEADER_END@  *  * $P4: //depot/projects/trustedbsd/openbsm/bsm/audit_kevents.h#29 $  */
+comment|/*  * Copyright (c) 2005 Apple Computer, Inc.  * All rights reserved.  *  * @APPLE_BSD_LICENSE_HEADER_START@  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * @APPLE_BSD_LICENSE_HEADER_END@  *  * $P4: //depot/projects/trustedbsd/openbsm/bsm/audit_kevents.h#34 $  */
 end_comment
 
 begin_ifndef
@@ -2573,6 +2573,281 @@ begin_comment
 comment|/* FreeBSD-specific. */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|AUE_BDFLUSH
+value|381
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SETFSUID
+value|382
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_GETFSUID
+value|383
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_PERSONALITY
+value|384
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_GETSCHEDULER
+value|385
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_SETSCHEDULER
+value|386
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_PRCTL
+value|387
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_GETCWD
+value|388
+end_define
+
+begin_comment
+comment|/* FreeBSD/Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_CAPGET
+value|389
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_CAPSET
+value|390
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_PIVOT_ROOT
+value|391
+end_define
+
+begin_comment
+comment|/* Linux-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_RTPRIO
+value|392
+end_define
+
+begin_comment
+comment|/* FreeBSD-specific. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_GETPARAM
+value|393
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_SETPARAM
+value|394
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_GET_PRIORITY_MAX
+value|395
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_GET_PRIORITY_MIN
+value|396
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_SCHED_RR_GET_INTERVAL
+value|397
+end_define
+
+begin_comment
+comment|/* POSIX.1b. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_GET_FILE
+value|398
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_SET_FILE
+value|399
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_GET_FD
+value|400
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_SET_FD
+value|401
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_DELETE_FILE
+value|402
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_DELETE_FD
+value|403
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_CHECK_FILE
+value|404
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_ACL_CHECK_FD
+value|405
+end_define
+
+begin_comment
+comment|/* FreeBSD. */
+end_comment
+
 begin_comment
 comment|/*  * Darwin BSM uses a number of AUE_O_* definitions, which are aliased to the  * normal Solaris BSM identifiers.  _O_ refers to it being an old, or compat  * interface.  In most cases, Darwin has never implemented these system calls  * but picked up the fields in their system call table from their FreeBSD  * import.  Happily, these have different names than the AUE_O* definitions  * in Solaris BSM.  */
 end_comment
@@ -3475,6 +3750,20 @@ begin_define
 define|#
 directive|define
 name|AUE_PWRITE
+value|AUE_NULL
+end_define
+
+begin_define
+define|#
+directive|define
+name|AUE_PREADV
+value|AUE_NULL
+end_define
+
+begin_define
+define|#
+directive|define
+name|AUE_PWRITEV
 value|AUE_NULL
 end_define
 
