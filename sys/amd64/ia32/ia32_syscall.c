2c8dd0e25024cc4c2603583e946f1bd6207ef71b
Wayne Salamon <wsalamon@FreeBSD.org>
2006-02-04 20:37:20 +0000

    Call the audit syscall enter/exit functions for the amd64 architecture, both 32-bit and 64-bit paths. System calls will now be audited.
    Obtained from: TrustedBSD Project
    Approved by: rwatson (mentor)
    
    
    Former-commit-id: 9c6f9515192282d45d592b4fb8513fcb81a09883

--- sys/amd64/ia32/ia32_syscall.c
+++ sys/amd64/ia32/ia32_syscall.c
@@ -161,6 +161,12 @@ end_endif
 begin_include
 include|#
 directive|include
+file|<security/audit/audit.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<vm/vm.h>
 end_include
 
@@ -679,6 +685,13 @@ argument_list|,
 name|narg
 argument_list|)
 expr_stmt|;
+name|AUDIT_SYSCALL_ENTER
+argument_list|(
+name|code
+argument_list|,
+name|td
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 call|(
@@ -691,6 +704,13 @@ argument_list|(
 name|td
 argument_list|,
 name|args64
+argument_list|)
+expr_stmt|;
+name|AUDIT_SYSCALL_EXIT
+argument_list|(
+name|error
+argument_list|,
+name|td
 argument_list|)
 expr_stmt|;
 block|}
