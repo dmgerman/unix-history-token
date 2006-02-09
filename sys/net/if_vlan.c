36924329072a8edebfe53867d8b88f24de096103
Ed Maste <emaste@FreeBSD.org>
2006-02-09 22:11:58 +0000

    Add a MODULE_VERSION so that other modules (perhaps third-party) can depend on this one.
    Approved by:	rwatson (mentor)
    
    
    Former-commit-id: 4179ed2cde1dbf62ed10f1fdd8d459325c4bc5cc

--- sys/net/if_vlan.c
+++ sys/net/if_vlan.c
@@ -2507,6 +2507,16 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|if_vlan
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|MODULE_DEPEND
 argument_list|(
 name|if_vlan
