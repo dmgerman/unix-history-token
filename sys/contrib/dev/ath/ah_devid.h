4b4a60cbdb2f4a54a16bae6b94773c5327254234
Sam Leffler <sam@FreeBSD.org>
2006-02-24 19:49:50 +0000

    MFC v0.9.16.16 hal
    Approved by:	re (mux)
    
    
    Former-commit-id: 8fbb290da2623f84932bca6c84e736a806890bc9

--- sys/contrib/dev/ath/ah_devid.h
+++ sys/contrib/dev/ath/ah_devid.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2002-2004 Sam Leffler, Errno Consulting, Atheros  * Communications, Inc.  All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the following conditions are met:  * 1. The materials contained herein are unmodified and are used  *    unmodified.  * 2. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following NO  *    ''WARRANTY'' disclaimer below (''Disclaimer''), without  *    modification.  * 3. Redistributions in binary form must reproduce at minimum a  *    disclaimer similar to the Disclaimer below and any redistribution  *    must be conditioned upon including a substantially similar  *    Disclaimer requirement for further binary redistribution.  * 4. Neither the names of the above-listed copyright holders nor the  *    names of any contributors may be used to endorse or promote  *    product derived from this software without specific prior written  *    permission.  *  * NO WARRANTY  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF NONINFRINGEMENT,  * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE  * FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGES.  *  * $Id: ah_devid.h,v 1.12 2004/10/21 22:48:57 sam Exp $  */
+comment|/*-  * Copyright (c) 2002-2006 Sam Leffler, Errno Consulting, Atheros  * Communications, Inc.  All rights reserved.  *  * Redistribution and use in source and binary forms are permitted  * provided that the following conditions are met:  * 1. The materials contained herein are unmodified and are used  *    unmodified.  * 2. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following NO  *    ''WARRANTY'' disclaimer below (''Disclaimer''), without  *    modification.  * 3. Redistributions in binary form must reproduce at minimum a  *    disclaimer similar to the Disclaimer below and any redistribution  *    must be conditioned upon including a substantially similar  *    Disclaimer requirement for further binary redistribution.  * 4. Neither the names of the above-listed copyright holders nor the  *    names of any contributors may be used to endorse or promote  *    product derived from this software without specific prior written  *    permission.  *  * NO WARRANTY  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  * ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  * LIMITED TO, THE IMPLIED WARRANTIES OF NONINFRINGEMENT,  * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE  * FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGES.  *  * $Id: //depot/sw/linuxsrc/src/802_11/madwifi/hal/main/ah_devid.h#19 $  */
 end_comment
 
 begin_ifndef
@@ -229,6 +229,39 @@ begin_comment
 comment|/* AR2313 WMAC (AP43-030) */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|AR5212_AR2315_REV6
+value|0x0086
+end_define
+
+begin_comment
+comment|/* AR2315 WMAC (AP51-Light) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR5212_AR2315_REV7
+value|0x0087
+end_define
+
+begin_comment
+comment|/* AR2315 WMAC (AP51-Full) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR5212_AR2317_REV1
+value|0x0091
+end_define
+
+begin_comment
+comment|/* AR2317 WMAC (AP61) */
+end_comment
+
 begin_comment
 comment|/* AR5212 compatible devid's also attach to 5212 */
 end_comment
@@ -284,6 +317,39 @@ end_define
 
 begin_comment
 comment|/* AR2413 aka Griffin-lite */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR5212_AR5413
+value|0x001b
+end_define
+
+begin_comment
+comment|/* Eagle */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR5212_AR5424
+value|0x001c
+end_define
+
+begin_comment
+comment|/* Condor (PCI express) */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR5212_DEVID_FF19
+value|0xff19
+end_define
+
+begin_comment
+comment|/* XXX PCI express */
 end_comment
 
 begin_comment
