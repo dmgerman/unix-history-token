7ebfe32140b2afddb6d2bb7182e1f9a7ba32625c
Wojciech A. Koszek <wkoszek@FreeBSD.org>
2006-03-01 21:40:14 +0000

    MFC:   Connect zlib to the build.
      This patch fixes a problem, which exists if you have IPSEC in your kernel
      and want to have crypto support loaded as KLD. By moving zlib to separate
      module and adding MODULE_DEPEND directives, it is possible to use such
      configuration without complication. Otherwise, since IPSEC is linked with
      zlib (just like crypto.ko) you'll get following error:
    
              interface zlib.1 already present in the KLD 'kernel'!
    
      Approved by:    cognet (mentor)
    
    Approved by:	re (scottl)
    
    
    Former-commit-id: 8ef33c65ae3a1c8c2dcb8ee7db09bf6d871a2932

--- sys/opencrypto/cryptodev.c
+++ sys/opencrypto/cryptodev.c
@@ -4155,5 +4155,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|cryptodev
+argument_list|,
+name|zlib
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
