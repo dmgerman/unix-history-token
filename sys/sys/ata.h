ffe67383cfd8ef33d1eca4fb6ecb34c520cb1859
Soren Schmidt <sos@FreeBSD.org>
2006-03-05 17:43:13 +0000

    Add two new ATAPI commands.
    Former-commit-id: d54d5ad9d1f17b7f20f23c12a5215af7bdc34f77

--- sys/sys/ata.h
+++ sys/sys/ata.h
@@ -1492,6 +1492,17 @@ end_define
 begin_define
 define|#
 directive|define
+name|ATAPI_INQUIRY
+value|0x12
+end_define
+
+begin_comment
+comment|/* get inquiry data */
+end_comment
+
+begin_define
+define|#
+directive|define
 name|ATAPI_MODE_SELECT
 value|0x15
 end_define
@@ -1849,6 +1860,17 @@ end_define
 
 begin_comment
 comment|/* send CUE sheet */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|ATAPI_SERVICE_ACTION_IN
+value|0x96
+end_define
+
+begin_comment
+comment|/* get service data */
 end_comment
 
 begin_define
