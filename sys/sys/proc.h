9d9fdf0bbb544c87a399094fdbced5183f5ede18
David Xu <davidxu@FreeBSD.org>
2006-03-13 10:37:25 +0000

    Remove unused code.
    Former-commit-id: f5651b57e5f9c9048a10f25390b15079f5fbe60b

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -4876,18 +4876,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|int
-name|thread_sleep_check
-parameter_list|(
-name|struct
-name|thread
-modifier|*
-name|td
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|void
 name|thread_stash
 parameter_list|(
