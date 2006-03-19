68dce123c74c6bb7ace119ec13d89fba95fae191
Joseph Koshy <jkoshy@FreeBSD.org>
2006-03-19 01:48:44 +0000

    MFC {r1.9 libpmc.c, r1.10 pmc.3}: Add an alias 'unhalted-cycles'   denoting cycles where the CPU is not in a halt or sleep state. [1] Increment the manual page date, bring the copyright year upto-date.
    Approved by:	re (mux) [1]
    
    
    Former-commit-id: 01e0ce94313d8d4c953c9e897fdb0f0d19527ad0

--- lib/libpmc/libpmc.c
+++ lib/libpmc/libpmc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2003-2005 Joseph Koshy  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2003-2006 Joseph Koshy  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_include
@@ -1470,6 +1470,13 @@ argument_list|(
 literal|"interrupts"
 argument_list|,
 literal|"k8-fr-taken-hardware-interrupts"
+argument_list|)
+block|,
+name|EV_ALIAS
+argument_list|(
+literal|"unhalted-cycles"
+argument_list|,
+literal|"k8-bu-cpu-clk-unhalted"
 argument_list|)
 block|,
 name|EV_ALIAS
@@ -3311,6 +3318,13 @@ argument_list|(
 literal|"instructions"
 argument_list|,
 literal|"p4-instr-retired,mask=nbogusntag+nbogustag"
+argument_list|)
+block|,
+name|EV_ALIAS
+argument_list|(
+literal|"unhalted-cycles"
+argument_list|,
+literal|"p4-global-power-events"
 argument_list|)
 block|,
 name|EV_ALIAS
@@ -6490,6 +6504,13 @@ argument_list|(
 literal|"interrupts"
 argument_list|,
 literal|"p6-hw-int-rx"
+argument_list|)
+block|,
+name|EV_ALIAS
+argument_list|(
+literal|"unhalted-cycles"
+argument_list|,
+literal|"p6-cpu-clk-unhalted"
 argument_list|)
 block|,
 name|EV_ALIAS
