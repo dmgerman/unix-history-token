439ad0c173581c5330faa36268ee4dd9d480f503
Robert N. M. Watson <rwatson@FreeBSD.org>
2006-03-19 15:37:04 +0000

    Merge Perforce change 93568 from TrustedBSD audit3 branch:
      Normalize nested include guards.
    
    Obtained from:	TrustedBSD Project
    
    
    Former-commit-id: 0034f90b171d08f785b5d0aa787220a98983ad17

--- sys/security/audit/audit.h
+++ sys/security/audit/audit.h
@@ -10,13 +10,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_BSM_AUDIT_KERNEL_H
+name|_SECURITY_AUDIT_KERNEL_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_BSM_AUDIT_KERNEL_H
+name|_SEUCRITY_AUDIT_KERNEL_H_
 end_define
 
 begin_ifndef
@@ -1213,7 +1213,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_BSM_AUDIT_KERNEL_H */
+comment|/* !_SECURITY_AUDIT_KERNEL_H_ */
 end_comment
 
 end_unit
--- sys/security/audit/audit_private.h
+++ sys/security/audit/audit_private.h
@@ -10,13 +10,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_BSM_AUDIT_PRIVATE_H
+name|_SECURITY_AUDIT_PRIVATE_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_BSM_AUDIT_PRIVATE_H
+name|_SECURITY_AUDIT_PRIVATE_H_
 end_define
 
 begin_ifndef
@@ -1036,7 +1036,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* ! _BSM_AUDIT_PRIVATE_H */
+comment|/* ! _SECURITY_AUDIT_PRIVATE_H_ */
 end_comment
 
 end_unit
