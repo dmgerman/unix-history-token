d8e8da42473e2170048fa6e193083779fa9cb180
Joseph Koshy <jkoshy@FreeBSD.org>
2006-03-22 10:25:37 +0000

    MFC {r1.19,r1.21 sys/dev/hwpmc/hwpmc_mod.c; r1.5 hwpmc_logging.c}:  - When a process is de-configuring a log file, also stop all of its    PMCs that require a log file to operate.  This change should fix    PR 90269. [1]  - When deconfiguring a log, only stop PMCs that are in the RUNNING    state. [2]
    Approved by:	re (scottl) [1], (hrs) [2]
    PR:		kern/90269
    
    Former-commit-id: 2572b21dd05ca4fdcdd9143b1ac25b0c6849e8cd

--- sys/dev/hwpmc/hwpmc_mod.c
+++ sys/dev/hwpmc/hwpmc_mod.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2003-2005 Joseph Koshy  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*-  * Copyright (c) 2003-2006 Joseph Koshy  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -9059,6 +9059,11 @@ name|PMC_OP_CONFIGURELOG
 case|:
 block|{
 name|struct
+name|pmc
+modifier|*
+name|pm
+decl_stmt|;
+name|struct
 name|pmc_owner
 modifier|*
 name|po
@@ -9190,6 +9195,34 @@ name|error
 operator|==
 literal|0
 condition|)
+block|{
+name|LIST_FOREACH
+argument_list|(
+argument|pm
+argument_list|,
+argument|&po->po_pmcs
+argument_list|,
+argument|pm_next
+argument_list|)
+if|if
+condition|(
+name|pm
+operator|->
+name|pm_flags
+operator|&
+name|PMC_F_NEEDS_LOGFILE
+operator|&&
+name|pm
+operator|->
+name|pm_state
+operator|==
+name|PMC_STATE_RUNNING
+condition|)
+name|pmc_stop
+argument_list|(
+name|pm
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|pmclog_deconfigure_log
@@ -9197,6 +9230,7 @@ argument_list|(
 name|po
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 else|else
 name|error
