a18314dffb4cbd9a83a17d9f42ccb56172693213
Scott Long <scottl@FreeBSD.org>
2006-04-04 04:39:35 +0000

    Teach sysinstall about MFI.
    Approved by: re
    
    
    Former-commit-id: def89aae041ceeaca2ab3e8bc5a8ad2f067190b5

--- usr.sbin/sysinstall/devices.c
+++ usr.sbin/sysinstall/devices.c
@@ -430,6 +430,22 @@ literal|4
 block|}
 block|,
 block|{
+name|DEVICE_TYPE_DISK
+block|,
+literal|"mfid%d"
+block|,
+literal|"LSI MegaRAID SAS"
+block|,
+literal|254
+block|,
+literal|65538
+block|,
+literal|8
+block|,
+literal|4
+block|}
+block|,
+block|{
 name|DEVICE_TYPE_FLOPPY
 block|,
 literal|"fd%d"
