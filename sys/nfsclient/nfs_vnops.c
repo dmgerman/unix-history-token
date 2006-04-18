10a3afadcce93f9d9954eb4e2976e311d3285596
Jonathan Chen <jon@FreeBSD.org>
2006-04-18 14:15:50 +0000

    MFC 1.261 - fix a crash when an nfsv2 mount fails
    Approved by:	re
    
    
    Former-commit-id: c81d260ac9c6d86a750b23924023c653847989fb

--- sys/nfsclient/nfs_vnops.c
+++ sys/nfsclient/nfs_vnops.c
@@ -4060,6 +4060,11 @@ condition|(
 name|error
 condition|)
 block|{
+if|if
+condition|(
+name|v3
+condition|)
+block|{
 name|nfsm_postop_attr
 argument_list|(
 name|dvp
@@ -4072,6 +4077,7 @@ argument_list|(
 name|mrep
 argument_list|)
 expr_stmt|;
+block|}
 goto|goto
 name|nfsmout
 goto|;
