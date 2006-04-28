59d6e4751979f775e79388b5e0947f7a7db6290b
Colin Percival <cperciva@FreeBSD.org>
2006-04-28 06:54:34 +0000

    MFC: Allow L2 caches to be shared between processors by default.
    Approved by:    re (scottl)
    
    
    Former-commit-id: b21cdccecac60f5ca8e3ab11c5867cbaa82ea4ee

--- sys/i386/i386/mp_machdep.c
+++ sys/i386/i386/mp_machdep.c
@@ -1751,7 +1751,7 @@ operator|>=
 literal|4
 condition|)
 block|{
-comment|/* Ask the processor about up to 32 caches. */
+comment|/* Ask the processor about the L1 cache. */
 for|for
 control|(
 name|i
@@ -1760,7 +1760,7 @@ literal|0
 init|;
 name|i
 operator|<
-literal|32
+literal|1
 condition|;
 name|i
 operator|++
