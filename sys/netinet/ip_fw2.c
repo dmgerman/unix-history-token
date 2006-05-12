0c3fe6b7795ee0478379c2bb98691efa949b5f1e
Max Laier <mlaier@FreeBSD.org>
2006-05-12 04:41:27 +0000

    Reintroduce net.inet6.ip6.fw.enable sysctl to dis/enable the ipv6 processing seperately.  Also use pfil hook/unhook instead of keeping the check functions in pfil just to return there based on the sysctl.  While here fix some whitespace on a nearby SYSCTL_ macro.
    Former-commit-id: cfc1aa9755e7ce22d0da8a7a2477bf3740e37be7

--- sys/netinet/ip_fw2.c
+++ sys/netinet/ip_fw2.c
@@ -652,6 +652,16 @@ begin_comment
 comment|/* bounded to 1..1000 in add_rule() */
 end_comment
 
+begin_function_decl
+specifier|extern
+name|int
+name|ipfw_chg_hook
+parameter_list|(
+name|SYSCTL_HANDLER_ARGS
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -677,7 +687,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
-name|SYSCTL_INT
+name|SYSCTL_PROC
 argument_list|(
 name|_net_inet_ip_fw
 argument_list|,
@@ -685,6 +695,8 @@ name|OID_AUTO
 argument_list|,
 name|enable
 argument_list|,
+name|CTLTYPE_INT
+operator||
 name|CTLFLAG_RW
 operator||
 name|CTLFLAG_SECURE3
@@ -693,6 +705,10 @@ operator|&
 name|fw_enable
 argument_list|,
 literal|0
+argument_list|,
+name|ipfw_chg_hook
+argument_list|,
+literal|"I"
 argument_list|,
 literal|"Enable ipfw"
 argument_list|)
@@ -18087,6 +18103,38 @@ argument_list|,
 literal|0
 argument_list|,
 literal|"Firewall"
+argument_list|)
+expr_stmt|;
+name|SYSCTL_ADD_PROC
+argument_list|(
+operator|&
+name|ip6_fw_sysctl_ctx
+argument_list|,
+name|SYSCTL_CHILDREN
+argument_list|(
+name|ip6_fw_sysctl_tree
+argument_list|)
+argument_list|,
+name|OID_AUTO
+argument_list|,
+literal|"enable"
+argument_list|,
+name|CTLTYPE_INT
+operator||
+name|CTLFLAG_RW
+operator||
+name|CTLFLAG_SECURE3
+argument_list|,
+operator|&
+name|fw6_enable
+argument_list|,
+literal|0
+argument_list|,
+name|ipfw_chg_hook
+argument_list|,
+literal|"I"
+argument_list|,
+literal|"Enable ipfw+6"
 argument_list|)
 expr_stmt|;
 name|SYSCTL_ADD_INT
