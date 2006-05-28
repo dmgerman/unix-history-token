aa278ef5755f3e3b3efdf6a79fe857225226f2ce
Craig Rodrigues <rodrigc@FreeBSD.org>
2006-05-28 20:09:18 +0000

    Modify MNT_UPDATE behavior for nullfs so that it does not return EOPNOTSUPP if an "export" parameter was passed in. This should allow nullfs mounts to be NFS exported.
    PR:		kern/87906
    MFC after:	1 week
    
    Former-commit-id: 3da05956044a87dcb73ee5750a8b0e3e6ff2911e

--- sys/fs/nullfs/null_vfsops.c
+++ sys/fs/nullfs/null_vfsops.c
@@ -261,12 +261,33 @@ operator|&
 name|MNT_UPDATE
 condition|)
 block|{
+comment|/* 		 * Only support update mounts for NFS export. 		 */
+if|if
+condition|(
+name|vfs_flagopt
+argument_list|(
+name|mp
+operator|->
+name|mnt_optnew
+argument_list|,
+literal|"export"
+argument_list|,
+name|NULL
+argument_list|,
+literal|0
+argument_list|)
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
+else|else
 return|return
 operator|(
 name|EOPNOTSUPP
 operator|)
 return|;
-comment|/* return VFS_MOUNT(MOUNTTONULLMOUNT(mp)->nullm_vfs, path, data, ndp, td);*/
 block|}
 comment|/* 	 * Get argument 	 */
 name|error
