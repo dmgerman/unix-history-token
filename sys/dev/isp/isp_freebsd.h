41882c9134aecc7290f0497d43f69bc9e1d8a8b4
Matthew Jacob <mjacob@FreeBSD.org>
2006-06-03 07:19:44 +0000

    allow this to compile cleanly under RELENG_4
    Former-commit-id: 70a9877ffd3c826cc43cabd6f9caeb472433acf5

--- sys/dev/isp/isp_freebsd.h
+++ sys/dev/isp/isp_freebsd.h
@@ -127,6 +127,25 @@ directive|include
 file|<machine/bus.h>
 end_include
 
+begin_if
+if|#
+directive|if
+name|__FreeBSD_version
+operator|<
+literal|500000
+end_if
+
+begin_include
+include|#
+directive|include
+file|<machine/clock.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
