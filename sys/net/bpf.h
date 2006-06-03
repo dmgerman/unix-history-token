29bf3b2fbea38f2aa8aa1d5354d74cbc332d767a
Christian S.J. Peron <csjp@FreeBSD.org>
2006-06-03 18:48:14 +0000

    Back out previous two commits, this caused some problems in the namespace resulting in some build failures. Instead, to fix the problem of bpf not being present, check the pointer before dereferencing it.
    This is a temporary bandaid until we can decide on how we want to handle
    the bpf code not being present. This will be fixed shortly.
    
    
    Former-commit-id: 0427de505bd4c20f9d1b53b2885b92cb023b744c

--- sys/net/bpf.h
+++ sys/net/bpf.h
@@ -1769,18 +1769,6 @@ directive|ifdef
 name|_KERNEL
 end_ifdef
 
-begin_include
-include|#
-directive|include
-file|"opt_bpf.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"opt_netgraph.h"
-end_include
-
 begin_comment
 comment|/*  * Descriptor associated with each attached hardware interface.  */
 end_comment
@@ -1978,19 +1966,10 @@ modifier|*
 name|bpf
 parameter_list|)
 block|{
-if|#
-directive|if
-name|defined
-argument_list|(
-name|DEV_BPF
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|NETGRAPH_BPF
-argument_list|)
-return|return
-operator|(
+if|if
+condition|(
+name|bpf
+operator|&&
 operator|!
 name|LIST_EMPTY
 argument_list|(
@@ -1999,18 +1978,17 @@ name|bpf
 operator|->
 name|bif_dlist
 argument_list|)
+condition|)
+return|return
+operator|(
+literal|1
 operator|)
 return|;
-else|#
-directive|else
 return|return
 operator|(
 literal|0
 operator|)
 return|;
-endif|#
-directive|endif
-comment|/* DEV_BPF || NETGRAPH_BPF */
 block|}
 end_function
 
