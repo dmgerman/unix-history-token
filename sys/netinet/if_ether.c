b4b1b305f73f208d1a1d452ed8c872dd5d61c14d
Andrew Thompson <thompsa@FreeBSD.org>
2006-06-09 00:33:30 +0000

    Enable proxy ARP answers on any of the bridged interfaces if proxy record belongs to another interface within the bridge group.
    PR:		kern/94408
    Submitted by:	Eygene A. Ryabinkin
    MFC after:	1 month
    
    Former-commit-id: e975c02387ea14ac50df54d3c8307b48304f8108

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -4296,7 +4296,7 @@ directive|endif
 block|}
 else|else
 block|{
-comment|/* 			 * Return proxied ARP replies only on the interface 			 * where this network resides. Otherwise we may 			 * conflict with the host we are proxying for. 			 */
+comment|/* 			 * Return proxied ARP replies only on the interface 			 * or bridge cluster where this network resides. 			 * Otherwise we may conflict with the host we are 			 * proxying for. 			 */
 if|if
 condition|(
 name|rt
@@ -4304,6 +4304,24 @@ operator|->
 name|rt_ifp
 operator|!=
 name|ifp
+operator|&&
+operator|(
+name|rt
+operator|->
+name|rt_ifp
+operator|->
+name|if_bridge
+operator|!=
+name|ifp
+operator|->
+name|if_bridge
+operator|||
+name|ifp
+operator|->
+name|if_bridge
+operator|==
+name|NULL
+operator|)
 condition|)
 block|{
 name|RT_UNLOCK
