d6220601b436a6fefb9b188706803bfdaa76646e
John Baldwin <jhb@FreeBSD.org>
2006-06-21 21:06:07 +0000

    MFC: Add two device IDs for Linksys PCMPC200 cards.
    Former-commit-id: 6279c1d779dcc33ae6706048d10a6b7a2e7fcce3

--- sys/pci/if_dcreg.h
+++ sys/pci/if_dcreg.h
@@ -4695,6 +4695,35 @@ name|DC_DEVICEID_MSMN130
 value|0x0002
 end_define
 
+begin_comment
+comment|/*  * Linksys vendor ID.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DC_VENDORID_LINKSYS
+value|0x1737
+end_define
+
+begin_comment
+comment|/*  * Linksys device IDs.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|DC_DEVICEID_PCMPC200_AB08
+value|0xab08
+end_define
+
+begin_define
+define|#
+directive|define
+name|DC_DEVICEID_PCMPC200_AB09
+value|0xab09
+end_define
+
 begin_define
 define|#
 directive|define
