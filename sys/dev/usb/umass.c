a0ddc0a8f017bed2f0794bf2b7ace6c9f4bb2998
Ian Dowse <iedowse@FreeBSD.org>
2006-06-26 00:31:25 +0000

    MFC: implement umass_cam_poll() and add a missing ohci_waitintr() call so that polled operation works.
    PR:		usb/93720
    
    Former-commit-id: ecb4582ee8db53f7852fe714e59f00cd6a78746e

--- sys/dev/usb/umass.c
+++ sys/dev/usb/umass.c
@@ -11360,10 +11360,6 @@ block|}
 block|}
 end_function
 
-begin_comment
-comment|/* umass_cam_poll  *	all requests are handled through umass_cam_action, requests  *	are never pending. So, nothing to do here.  */
-end_comment
-
 begin_function
 name|Static
 name|void
@@ -11375,9 +11371,6 @@ modifier|*
 name|sim
 parameter_list|)
 block|{
-ifdef|#
-directive|ifdef
-name|USB_DEBUG
 name|struct
 name|umass_softc
 modifier|*
@@ -11408,9 +11401,31 @@ argument_list|)
 operator|)
 argument_list|)
 expr_stmt|;
-endif|#
-directive|endif
-comment|/* nop */
+name|usbd_set_polling
+argument_list|(
+name|sc
+operator|->
+name|sc_udev
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+name|usbd_dopoll
+argument_list|(
+name|sc
+operator|->
+name|iface
+argument_list|)
+expr_stmt|;
+name|usbd_set_polling
+argument_list|(
+name|sc
+operator|->
+name|sc_udev
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
