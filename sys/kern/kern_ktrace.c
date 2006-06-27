9e91ff946db98afc61a8eb1efba4ba4f573b0b84
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2006-06-27 11:29:38 +0000

    Use suser_cred(9) instead of checking cr_uid directly.
    Reviewed by:	rwatson
    
    
    Former-commit-id: 743cae8e4a3ccb7c2bef4a1ff9f05bffaf13ba11

--- sys/kern/kern_ktrace.c
+++ sys/kern/kern_ktrace.c
@@ -3669,11 +3669,14 @@ name|facs
 expr_stmt|;
 if|if
 condition|(
+name|suser_cred
+argument_list|(
 name|td
 operator|->
 name|td_ucred
-operator|->
-name|cr_uid
+argument_list|,
+name|SUSER_ALLOWJAIL
+argument_list|)
 operator|==
 literal|0
 condition|)
