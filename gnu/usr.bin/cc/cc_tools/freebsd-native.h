0e8c039de1a4139e62a3ea3ad93ad047810a5b4f
David E. O'Brien <obrien@FreeBSD.org>
2006-07-11 20:02:39 +0000

    MFC: define FBSD_CC_VER simular to FBSD_MAJOR.
    Former-commit-id: 48c50b1170b82cbb35eeda9e0859df73124a46b2

--- gnu/usr.bin/cc/cc_tools/freebsd-native.h
+++ gnu/usr.bin/cc/cc_tools/freebsd-native.h
@@ -25,6 +25,17 @@ name|FBSD_MAJOR
 value|6
 end_define
 
+begin_define
+define|#
+directive|define
+name|FBSD_CC_VER
+value|602001
+end_define
+
+begin_comment
+comment|/* form like __FreeBSD_version */
+end_comment
+
 begin_undef
 undef|#
 directive|undef
