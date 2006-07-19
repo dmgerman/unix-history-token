cf53405d1a873cadaf78033ae79d06c4a8ab0dcd
Konstantin Belousov <kib@FreeBSD.org>
2006-07-19 16:31:12 +0000

    Enable build of ppc(4) module on i386 and amd64 architectures. Add proper module interdependencies.
    Reviewed by:	marcel
    Approved by:	pjd (mentor)
    
    
    Former-commit-id: f1c3fd4d822bc56c6b45a420f80a5d2c6dadccce

--- sys/dev/ppbus/lpt.c
+++ sys/dev/ppbus/lpt.c
@@ -3901,5 +3901,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|lpt
+argument_list|,
+name|ppbus
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
