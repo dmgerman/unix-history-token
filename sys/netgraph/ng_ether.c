89636c9b4cae67dfb79f7b19a1d89d4ff16f85e9
Tai-hwa Liang <avatar@FreeBSD.org>
2006-07-27 06:15:37 +0000

    Fixing compilation bustage: net/if_bridgevar.h depends on net/bridgestp.h.
    Former-commit-id: b0924661498376376ebeb90f677833b072a8d2b8

--- sys/netgraph/ng_ether.c
+++ sys/netgraph/ng_ether.c
@@ -98,6 +98,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<net/bridgestp.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<net/if_bridgevar.h>
 end_include
 
