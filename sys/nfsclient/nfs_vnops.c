979bb4d1b6dc7156b9bc986983d3e12564470bfb
Konstantin Belousov <kib@FreeBSD.org>
2006-08-07 12:33:25 +0000

    MFC rev. 1.267: Always supply curthread as argument to nfs_asyncio and nfs_doio in nfs_strategy. Otherwise, for some buffers, signals would be ignored at the intr mounts.
    Reviewed by:	mohan
    Approved by:	pjd (mentor)
    
    
    Former-commit-id: 62eab26f679183c778126016e9d6ab4475234c5f

--- sys/nfsclient/nfs_vnops.c
+++ sys/nfsclient/nfs_vnops.c
@@ -13729,11 +13729,6 @@ name|ucred
 modifier|*
 name|cr
 decl_stmt|;
-name|struct
-name|thread
-modifier|*
-name|td
-decl_stmt|;
 name|KASSERT
 argument_list|(
 operator|!
@@ -13768,24 +13763,6 @@ name|bp
 operator|)
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|bp
-operator|->
-name|b_flags
-operator|&
-name|B_ASYNC
-condition|)
-name|td
-operator|=
-name|NULL
-expr_stmt|;
-else|else
-name|td
-operator|=
-name|curthread
-expr_stmt|;
-comment|/* XXX */
 if|if
 condition|(
 name|bp
@@ -13835,7 +13812,7 @@ name|bp
 argument_list|,
 name|NOCRED
 argument_list|,
-name|td
+name|curthread
 argument_list|)
 condition|)
 operator|(
@@ -13851,7 +13828,7 @@ name|bp
 argument_list|,
 name|cr
 argument_list|,
-name|td
+name|curthread
 argument_list|)
 expr_stmt|;
 return|return
