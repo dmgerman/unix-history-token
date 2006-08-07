13c526c8f2d9d65aa8ee12f552b279ef5f066f74
Giorgos Keramidas <keramida@FreeBSD.org>
2006-08-07 10:33:07 +0000

    Spell "determine" correctly.
    Reviewed by:	jb
    
    
    Former-commit-id: e38d3a2a7ec675700af1bdb24475643d9dc5946a

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1316,7 +1316,7 @@ value|PAGE_SIZE
 end_define
 
 begin_comment
-comment|/*  * The OpenSolaris version is set according to the version last imported  * from http://dlc.sun.com/osol/on/downloads/current/. In FreeBSD header  * files it can be used to detemine the level of compatibility that the  * FreeBSD headers provide to OpenSolaris code. Perhaps one day there  * will be a really, really Single Unix Specification.  */
+comment|/*  * The OpenSolaris version is set according to the version last imported  * from http://dlc.sun.com/osol/on/downloads/current/. In FreeBSD header  * files it can be used to determine the level of compatibility that the  * FreeBSD headers provide to OpenSolaris code. Perhaps one day there  * will be a really, really Single Unix Specification.  */
 end_comment
 
 begin_define
