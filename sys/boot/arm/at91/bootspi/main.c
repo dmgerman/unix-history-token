9c90b7b319b4396389c07438ba7ffec10cf672e2
Warner Losh <imp@FreeBSD.org>
2006-08-16 23:39:58 +0000

    MFp4: First cut at making spi and/or sd card booting work, needs work
    Former-commit-id: 6b8c12144e4d8c342b5484b86c314b3806fdf8f9

--- sys/boot/arm/at91/bootspi/main.c
+++ sys/boot/arm/at91/bootspi/main.c
@@ -1,12 +1,18 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*******************************************************************************  *  * Filename: main.c  *  * Basic entry points for top-level functions  *  * Revision information:  *  * 20AUG2004	kb_admin	initial creation  * 12JAN2005	kb_admin	cosmetic changes  * 29APR2005	kb_admin	modified boot delay  *  * BEGIN_KBDD_BLOCK  * No warranty, expressed or implied, is included with this software.  It is  * provided "AS IS" and no warranty of any kind including statutory or aspects  * relating to merchantability or fitness for any purpose is provided.  All  * intellectual property rights of others is maintained with the respective  * owners.  This software is not copyrighted and is intended for reference  * only.  * END_BLOCK  *  * $FreeBSD$  ******************************************************************************/
+comment|/*-  * Copyright (c) 2006 M. Warner Losh.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * This software is derived from software provided by kwikbyte without  * copyright as follows:  *  * No warranty, expressed or implied, is included with this software.  It is  * provided "AS IS" and no warranty of any kind including statutory or aspects  * relating to merchantability or fitness for any purpose is provided.  All  * intellectual property rights of others is maintained with the respective  * owners.  This software is not copyrighted and is intended for reference  * only.  *  * $FreeBSD$  */
 end_comment
 
 begin_include
 include|#
 directive|include
 file|"env_vars.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"at91rm9200.h"
 end_include
 
 begin_include
@@ -33,9 +39,11 @@ directive|include
 file|"lib.h"
 end_include
 
-begin_comment
-comment|/*  * .KB_C_FN_DEFINITION_START  * int main(void)  *  This global function waits at least one second, but not more than two   * seconds, for input from the serial port.  If no response is recognized,  * it acts according to the parameters specified by the environment.  For   * example, the function might boot an operating system.  Do not return  * from this function.  * .KB_C_FN_DEFINITION_END  */
-end_comment
+begin_include
+include|#
+directive|include
+file|"spi_flash.h"
+end_include
 
 begin_function
 name|int
@@ -44,16 +52,19 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
+name|printf
+argument_list|(
+literal|"\r\nBoot\r\n"
+argument_list|)
+expr_stmt|;
+name|SPI_InitFlash
+argument_list|()
+expr_stmt|;
 name|EMAC_Init
 argument_list|()
 expr_stmt|;
 name|LoadBootCommands
 argument_list|()
-expr_stmt|;
-name|printf
-argument_list|(
-literal|"\r\nSPI Boot loader.\r\nAutoboot...\r\n"
-argument_list|)
 expr_stmt|;
 if|if
 condition|(
@@ -65,12 +76,19 @@ operator|==
 operator|-
 literal|1
 condition|)
+block|{
+name|start_wdog
+argument_list|(
+literal|30
+argument_list|)
+expr_stmt|;
 name|ExecuteEnvironmentFunctions
 argument_list|()
 expr_stmt|;
+block|}
 name|Bootloader
 argument_list|(
-literal|0
+name|getc
 argument_list|)
 expr_stmt|;
 return|return
