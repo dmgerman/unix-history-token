a919db399ab922dd272daf29b868a3d29b1e2ef1
Stefan Esser <se@FreeBSD.org>
2006-09-01 18:12:10 +0000

    MFC: 1.77 Fix logic for skipping of idle processes.
    Former-commit-id: e6c959f161dbcd82cfced60fbb76817900cb88f4

--- usr.bin/top/machine.c
+++ usr.bin/top/machine.c
@@ -2847,8 +2847,14 @@ operator|||
 name|pp
 operator|->
 name|ki_stat
-operator|!=
-name|SRUN
+operator|==
+name|SSTOP
+operator|||
+name|pp
+operator|->
+name|ki_stat
+operator|==
+name|SIDL
 operator|)
 condition|)
 comment|/* skip idle or non-running processes */
