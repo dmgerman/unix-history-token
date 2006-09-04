e678704abdd2090f52e4ab22b54acfd19f7267d8
Marcel Moolenaar <marcel@FreeBSD.org>
2006-09-04 18:27:14 +0000

    Add SIIG 4 port serial card based on the Oxford OX16PCI954.
    Former-commit-id: 87f4e30f04f5a746e6c826da51e8ff43fe028c1f

--- sys/dev/puc/pucdata.c
+++ sys/dev/puc/pucdata.c
@@ -2106,6 +2106,30 @@ literal|0x1415
 block|,
 literal|0x9501
 block|,
+literal|0x131f
+block|,
+literal|0x2051
+block|,
+literal|"SIIG Cyber 4S PCI 16C650 (20x family)"
+block|,
+name|DEFAULT_RCLK
+operator|*
+literal|10
+block|,
+name|PUC_PORT_4S
+block|,
+literal|0x10
+block|,
+literal|0
+block|,
+literal|8
+block|, 	}
+block|,
+block|{
+literal|0x1415
+block|,
+literal|0x9501
+block|,
 literal|0xffff
 block|,
 literal|0
