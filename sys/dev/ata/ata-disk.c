8f322eeae1a47828313a3e77a484aa185b7c8c45
Soren Schmidt <sos@FreeBSD.org>
2006-09-14 19:12:29 +0000

    If current_heads or current_sectors in the disk cap page are zero, dont try to use the current_ geometry. This avoids a panic with BIOS'n that sets these to zero.
    Former-commit-id: 87afef55fb8670df0046f4232664cad6ca0a3d1d

--- sys/dev/ata/ata-disk.c
+++ sys/dev/ata/ata-disk.c
@@ -437,6 +437,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+operator|(
 name|atadev
 operator|->
 name|param
@@ -444,6 +445,19 @@ operator|.
 name|atavalid
 operator|&
 name|ATA_FLAG_54_58
+operator|)
+operator|&&
+name|atadev
+operator|->
+name|param
+operator|.
+name|current_heads
+operator|&&
+name|atadev
+operator|->
+name|param
+operator|.
+name|current_sectors
 condition|)
 block|{
 name|adp
