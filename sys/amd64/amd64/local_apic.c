067bfa1895bd4a3fc811c90912934623fb0a91a5
Maxim Sobolev <sobomax@FreeBSD.org>
2006-09-27 22:09:35 +0000

    Extend comment explaining why code is conditional at !defined(SCHED_ULE).
    Suggested by:	ru
    
    
    Former-commit-id: 548f61b8a58943640e4c02b6771bd3a9f85de60f

--- sys/amd64/amd64/local_apic.c
+++ sys/amd64/amd64/local_apic.c
@@ -2638,7 +2638,7 @@ name|defined
 argument_list|(
 name|SCHED_ULE
 argument_list|)
-comment|/* 	 * Don't do any accounting for the disabled HTT cores, since it 	 * will provide misleading numbers for the userland. 	 * 	 * No locking is necessary here, since even if we loose the race 	 * when hlt_cpus_mask changes it is not a big deal, really. 	 */
+comment|/* 	 * Don't do any accounting for the disabled HTT cores, since it 	 * will provide misleading numbers for the userland. 	 * 	 * No locking is necessary here, since even if we loose the race 	 * when hlt_cpus_mask changes it is not a big deal, really. 	 * 	 * Don't do that for ULE, since ULE doesn't consider hlt_cpus_mask 	 * and unlike other schedulers it actually schedules threads to 	 * those CPUs. 	 */
 if|if
 condition|(
 operator|(
