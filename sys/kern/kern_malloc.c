d7c8fb69a0dfa4f1b10f6b8d15a936536819a976
Robert N. M. Watson <rwatson@FreeBSD.org>
2006-10-26 10:17:13 +0000

    Increase usefulness of "show malloc" by moving from displaying the basic counters of allocs/frees/use for each malloc type to calculating InUse, MemUse, and Requests as displayed by the userspace vmstat -m.  This is more useful when debugging malloc(9)-related memory leaks, where the count of allocs/frees may not usefully reflect that current memory allocation (i.e., when highly variable size allocations occur with the same malloc type, such as with contigmalloc).
    MFC after:			3 days
    Limitations observed by:	scottl
    
    
    Former-commit-id: 21b9e0d737e97ef5300f9faa80cb31ddbb408aab

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1987, 1991, 1993  *	The Regents of the University of California.  * Copyright (c) 2005 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_malloc.c	8.3 (Berkeley) 1/4/94  */
+comment|/*-  * Copyright (c) 1987, 1991, 1993  *	The Regents of the University of California.  * Copyright (c) 2005-2006 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)kern_malloc.c	8.3 (Berkeley) 1/4/94  */
 end_comment
 
 begin_comment
@@ -3423,20 +3423,25 @@ name|allocs
 decl_stmt|,
 name|frees
 decl_stmt|;
+name|u_int64_t
+name|alloced
+decl_stmt|,
+name|freed
+decl_stmt|;
 name|int
 name|i
 decl_stmt|;
 name|db_printf
 argument_list|(
-literal|"%18s %12s %12s %12s\n"
+literal|"%18s %12s  %12s %12s\n"
 argument_list|,
 literal|"Type"
 argument_list|,
-literal|"Allocs"
+literal|"InUse"
 argument_list|,
-literal|"Frees"
+literal|"MemUse"
 argument_list|,
-literal|"Used"
+literal|"Requests"
 argument_list|)
 expr_stmt|;
 for|for
@@ -3472,6 +3477,14 @@ operator|=
 literal|0
 expr_stmt|;
 name|frees
+operator|=
+literal|0
+expr_stmt|;
+name|alloced
+operator|=
+literal|0
+expr_stmt|;
+name|freed
 operator|=
 literal|0
 expr_stmt|;
@@ -3511,22 +3524,52 @@ index|]
 operator|.
 name|mts_numfrees
 expr_stmt|;
+name|alloced
+operator|+=
+name|mtip
+operator|->
+name|mti_stats
+index|[
+name|i
+index|]
+operator|.
+name|mts_memalloced
+expr_stmt|;
+name|freed
+operator|+=
+name|mtip
+operator|->
+name|mti_stats
+index|[
+name|i
+index|]
+operator|.
+name|mts_memfreed
+expr_stmt|;
 block|}
 name|db_printf
 argument_list|(
-literal|"%18s %12ju %12ju %12ju\n"
+literal|"%18s %12ju %12juK %12ju\n"
 argument_list|,
 name|mtp
 operator|->
 name|ks_shortdesc
 argument_list|,
 name|allocs
-argument_list|,
-name|frees
-argument_list|,
-name|allocs
 operator|-
 name|frees
+argument_list|,
+operator|(
+name|alloced
+operator|-
+name|freed
+operator|+
+literal|1023
+operator|)
+operator|/
+literal|1024
+argument_list|,
+name|allocs
 argument_list|)
 expr_stmt|;
 block|}
