a5b0ad93e51fcf3c7f176a2df6ac99c102cbb013
Gleb Smirnoff <glebius@FreeBSD.org>
2006-10-28 18:15:13 +0000

    MFC rev. 1.93, 1.94:   - Print the number of allocation failures in UMA zones.   - Add comma after REQUESTS field missed in previous commit.   - Widen some columns; make width of header columns less cryptic.
      PR:	kern/102940
    
    Approved by:	re (hrs)
    
    
    Former-commit-id: d180c3f26ccfb08fc84addb54992480a23b9e547

--- usr.bin/vmstat/vmstat.c
+++ usr.bin/vmstat/vmstat.c
@@ -5588,7 +5588,7 @@ block|}
 block|}
 name|printf
 argument_list|(
-literal|"%-15s %-8s %-9s %-7s %-5s %-8s\n\n"
+literal|"%-20s %8s  %8s  %8s  %8s  %8s  %8s\n\n"
 argument_list|,
 literal|"ITEM"
 argument_list|,
@@ -5601,6 +5601,8 @@ argument_list|,
 literal|"FREE"
 argument_list|,
 literal|"REQUESTS"
+argument_list|,
+literal|"FAILURES"
 argument_list|)
 expr_stmt|;
 for|for
@@ -5645,7 +5647,7 @@ argument_list|)
 expr_stmt|;
 name|printf
 argument_list|(
-literal|"%-15s %4llu, %8llu, %7llu, %6llu, %8llu\n"
+literal|"%-20s %8llu, %8llu, %8llu, %8llu, %8llu, %8llu\n"
 argument_list|,
 name|name
 argument_list|,
@@ -5670,6 +5672,11 @@ name|mtp
 argument_list|)
 argument_list|,
 name|memstat_get_numallocs
+argument_list|(
+name|mtp
+argument_list|)
+argument_list|,
+name|memstat_get_failures
 argument_list|(
 name|mtp
 argument_list|)
