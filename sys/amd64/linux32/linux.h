ac02c77047fc973d4be7328f88468c9a1ed43c27
Alexander Leidinger <netchild@FreeBSD.org>
2006-10-29 14:02:39 +0000

    Backout the linux aio stuff. Several problems where identified and the dynamic nature (if no native aio code is available, the linux part returns ENOSYS because of missing requisites) should be solved differently than it is.
    All this will be done in P4.
    
    Not included in this commit is a backout of the changes to the native aio
    code (removing static in some places). Those changes (and some more) will
    also be needed when the reworked linux aio stuff will reenter the tree.
    
    Requested by:	rwatson
    Discussed with:	rwatson
    
    
    Former-commit-id: 1cbde0ef5cb23b6cc52753b5b04fe7814fc28c53

--- sys/amd64/linux32/linux.h
+++ sys/amd64/linux32/linux.h
@@ -3637,12 +3637,6 @@ name|THREADING_FLAGS
 value|(CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND)
 end_define
 
-begin_include
-include|#
-directive|include
-file|<compat/linux/linux_aio.h>
-end_include
-
 begin_endif
 endif|#
 directive|endif
