0a5c2a4d85dca6b83de5ca12c3561fedb23378de
John Birrell <jb@FreeBSD.org>
2006-11-15 21:41:59 +0000

    Get the parent dma tag if one exists. This is required on sun4v. Other arches will default to NULL if they have no parent.
    Reviewed by: mjacob
    
    
    Former-commit-id: ab438787f73b7fe12bf99191ab8df5fe2e43e0b7

--- sys/dev/mpt/mpt_pci.c
+++ sys/dev/mpt/mpt_pci.c
@@ -2803,7 +2803,12 @@ argument_list|(
 name|mpt
 argument_list|,
 comment|/*parent*/
-name|NULL
+name|bus_get_dma_tag
+argument_list|(
+name|mpt
+operator|->
+name|dev
+argument_list|)
 argument_list|,
 comment|/*alignment*/
 literal|1
