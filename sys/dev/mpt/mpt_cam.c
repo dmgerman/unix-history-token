2e6183a835b7739c8b8a771f4203dc5be73fe983
Matthew Jacob <mjacob@FreeBSD.org>
2006-12-10 01:13:56 +0000

    Make mpt_pci depend on pci and mpt_cam depend on CAM.
    PR:		106536
    Suggested by:	Norikatsu Shigemura
    MFC after:	3 days
    
    Former-commit-id: 33742b1507a067ac712d6886405ed7ff9f2833f5

--- sys/dev/mpt/mpt_cam.c
+++ sys/dev/mpt/mpt_cam.c
@@ -725,6 +725,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|mpt_cam
+argument_list|,
+name|cam
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_function
 name|int
 name|mpt_cam_probe
--- sys/dev/mpt/mpt_pci.c
+++ sys/dev/mpt/mpt_pci.c
@@ -592,6 +592,22 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|mpt
+argument_list|,
+name|pci
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|MODULE_VERSION
 argument_list|(
 name|mpt
