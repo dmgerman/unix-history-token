2f5317f887d5593a853b03d853874fa729610d4d
John Baldwin <jhb@FreeBSD.org>
2006-12-14 19:57:06 +0000

    Add a first pass at a way to blacklist MSI on systems where it doesn't work: - A new PCI quirk (PCI_QUIRK_DISABLE_MSI) is added to the quirk table. - A new pci_msi_device_blacklisted() determines if a passed in device   matches an MSI quirk in the quirk table.  This can be overridden (all   quirks ignored) by setting the hw.pci.honor_msi_blacklist to 0. - A global blacklist check is performed in the MI PCI bus code by checking   to see if the device at 0:0:0 is blacklisted.
    Tested by:	jdp
    
    
    Former-commit-id: c30dcd2c9de34dbd1b3e9a15b3be6014d169f9ba

--- sys/dev/pci/pcivar.h
+++ sys/dev/pci/pcivar.h
@@ -1710,6 +1710,16 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|int
+name|pci_msi_device_blacklisted
+parameter_list|(
+name|device_t
+name|dev
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
