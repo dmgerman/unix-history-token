a88aa1c2cdc691ef40f6bcc753106a4910673625
Marcel Moolenaar <marcel@FreeBSD.org>
2006-12-21 05:40:46 +0000

    Unbreak 64-bit little-endian systems that do require alignment. The fix involves using le16dec(), le32dec(), le16enc() and le32enc(). This eliminates invalid casts and duplicated logic.
    Former-commit-id: 6ec7e3dbe38fde6d943a5ab388b15f7e9ee12fab

--- sys/fs/msdosfs/bpb.h
+++ sys/fs/msdosfs/bpb.h
@@ -234,19 +234,9 @@ end_comment
 begin_include
 include|#
 directive|include
-file|<machine/endian.h>
+file|<sys/endian.h>
 end_include
 
-begin_if
-if|#
-directive|if
-operator|(
-name|BYTE_ORDER
-operator|==
-name|LITTLE_ENDIAN
-operator|)
-end_if
-
 begin_define
 define|#
 directive|define
@@ -254,7 +244,7 @@ name|getushort
 parameter_list|(
 name|x
 parameter_list|)
-value|*((u_int16_t *)(x))
+value|le16dec(x)
 end_define
 
 begin_define
@@ -264,7 +254,7 @@ name|getulong
 parameter_list|(
 name|x
 parameter_list|)
-value|*((u_int32_t *)(x))
+value|le32dec(x)
 end_define
 
 begin_define
@@ -276,7 +266,7 @@ name|p
 parameter_list|,
 name|v
 parameter_list|)
-value|(*((u_int16_t *)(p)) = (v))
+value|le16enc(p, v)
 end_define
 
 begin_define
@@ -288,62 +278,8 @@ name|p
 parameter_list|,
 name|v
 parameter_list|)
-value|(*((u_int32_t *)(p)) = (v))
+value|le32enc(p, v)
 end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_define
-define|#
-directive|define
-name|getushort
-parameter_list|(
-name|x
-parameter_list|)
-value|(((u_int8_t *)(x))[0] + (((u_int8_t *)(x))[1]<< 8))
-end_define
-
-begin_define
-define|#
-directive|define
-name|getulong
-parameter_list|(
-name|x
-parameter_list|)
-value|(u_int32_t)(((u_int8_t *)(x))[0] + (((u_int8_t *)(x))[1]<< 8) \ 			 + (((u_int8_t *)(x))[2]<< 16)	\ 			 + (((u_int8_t *)(x))[3]<< 24))
-end_define
-
-begin_define
-define|#
-directive|define
-name|putushort
-parameter_list|(
-name|p
-parameter_list|,
-name|v
-parameter_list|)
-value|(((u_int8_t *)(p))[0] = (v),	\ 			 ((u_int8_t *)(p))[1] = (v)>> 8)
-end_define
-
-begin_define
-define|#
-directive|define
-name|putulong
-parameter_list|(
-name|p
-parameter_list|,
-name|v
-parameter_list|)
-value|(((u_int8_t *)(p))[0] = (v),	\ 			 ((u_int8_t *)(p))[1] = (v)>> 8, \ 			 ((u_int8_t *)(p))[2] = (v)>> 16,\ 			 ((u_int8_t *)(p))[3] = (v)>> 24)
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_comment
 comment|/*  * BIOS Parameter Block (BPB) for DOS 3.3  */
