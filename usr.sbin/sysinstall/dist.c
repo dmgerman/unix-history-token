8f420236bd76bafb836ea05b472b39910c7b2696
Murray Stokely <murray@FreeBSD.org>
2007-01-16 10:29:10 +0000

    Provide a more accurate description of the size of the ports collection.
    Former-commit-id: 17c3242db15506c7b8a0c4be3e22ddde103462c0

--- usr.sbin/sysinstall/dist.c
+++ usr.sbin/sysinstall/dist.c
@@ -1525,7 +1525,7 @@ operator|!
 name|msgYesNo
 argument_list|(
 literal|"Would you like to install the FreeBSD ports collection?\n\n"
-literal|"This will give you ready access to over 13,300 ported software packages,\n"
+literal|"This will give you ready access to over 14,900 ported software packages,\n"
 literal|"at a cost of around 440MB of disk space when \"clean\" and possibly\n"
 literal|"much more than that when a lot of the distribution tarballs are loaded\n"
 literal|"(unless you have the extra discs available from a FreeBSD CD/DVD distribution\n"
