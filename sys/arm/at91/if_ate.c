9d9074801b41b4638b3bf5ff5f4c52c971cc43b9
Kevin Lo <kevlo@FreeBSD.org>
2007-02-03 07:46:26 +0000

    ether_ifattach() sets if_mtu to ETHERMTU, don't bother set it again.
    Approved by: imp, cognet
    
    
    Former-commit-id: d96cb5ea37052ec0a7d279da8acb7c3e4c5d1b61

--- sys/arm/at91/if_ate.c
+++ sys/arm/at91/if_ate.c
@@ -939,12 +939,6 @@ argument_list|)
 expr_stmt|;
 name|ifp
 operator|->
-name|if_mtu
-operator|=
-name|ETHERMTU
-expr_stmt|;
-name|ifp
-operator|->
 name|if_flags
 operator|=
 name|IFF_BROADCAST
--- sys/arm/xscale/ixp425/if_npe.c
+++ sys/arm/xscale/ixp425/if_npe.c
@@ -1560,12 +1560,6 @@ argument_list|)
 expr_stmt|;
 name|ifp
 operator|->
-name|if_mtu
-operator|=
-name|ETHERMTU
-expr_stmt|;
-name|ifp
-operator|->
 name|if_flags
 operator|=
 name|IFF_BROADCAST
