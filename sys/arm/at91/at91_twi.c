da06c0ad1c25897addb156864cbfecd5add51c8a
Warner Losh <imp@FreeBSD.org>
2007-02-06 12:07:14 +0000

    at91_twi depends on the iicbus module to satisfy its symbols when loaded, so make that explicit.  Works for the monolithic kernel case, won't work for the kldload case.
    Former-commit-id: a3dde28fde32ff3ddc1f1c872a7cf1e7fc424de9

--- sys/arm/at91/at91_twi.c
+++ sys/arm/at91/at91_twi.c
@@ -1728,5 +1728,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|at91_twi
+argument_list|,
+name|iicbus
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
