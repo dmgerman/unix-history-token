e1faacbf278999c9c942e0d3db591492f12ce4df
Christian S.J. Peron <csjp@FreeBSD.org>
2007-02-19 00:34:28 +0000

    MFC the AUDIT_SYSCALL_ENTER/EXIT entry points in syscall handling.  This will unbreak auditing for the ARM architecture in RELENG_6
    Former-commit-id: 7a6e8a15a21fc4fb5511375656113913d7c12048

--- sys/arm/arm/trap.c
+++ sys/arm/arm/trap.c
@@ -216,6 +216,12 @@ directive|include
 file|<machine/swi.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<security/audit/audit.h>
+end_include
+
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -3605,6 +3611,13 @@ argument_list|,
 name|S_PT_SCE
 argument_list|)
 expr_stmt|;
+name|AUDIT_SYSCALL_ENTER
+argument_list|(
+name|code
+argument_list|,
+name|td
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 call|(
@@ -3617,6 +3630,13 @@ argument_list|(
 name|td
 argument_list|,
 name|args
+argument_list|)
+expr_stmt|;
+name|AUDIT_SYSCALL_EXIT
+argument_list|(
+name|error
+argument_list|,
+name|td
 argument_list|)
 expr_stmt|;
 block|}
