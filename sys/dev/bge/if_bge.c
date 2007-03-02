4763e08227949ec50e77e2290c094ec39aafdb93
Jung-uk Kim <jkim@FreeBSD.org>
2007-03-02 19:19:31 +0000

    MFC: Use ETHER_BPF_MTAP() instead of BPF_MTAP().
    Former-commit-id: da25a5ba355abe44c612498ab64acd7bc301d841

--- sys/dev/bge/if_bge.c
+++ sys/dev/bge/if_bge.c
@@ -14535,7 +14535,7 @@ operator|++
 name|count
 expr_stmt|;
 comment|/* 		 * If there's a BPF listener, bounce a copy of this frame 		 * to him. 		 */
-name|BPF_MTAP
+name|ETHER_BPF_MTAP
 argument_list|(
 name|ifp
 argument_list|,
