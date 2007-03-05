1d9202e84140968e24644352147b3245846d9a83
Bruce M Simpson <bms@FreeBSD.org>
2007-03-05 12:46:16 +0000

    MFC rev 1.59:   Add ELSA XI330 product.  This is rebadged and sold as SMC 2532W-B and   I/O Data also resells it.  Add an alternative airvast an100 id.
    PR:	kern/92323
    
    Former-commit-id: 1fe0480c29525b643c93b016c3f7b4ef637148e7

--- sys/dev/wi/if_wi_pccard.c
+++ sys/dev/wi/if_wi_pccard.c
@@ -526,6 +526,13 @@ name|PCMCIA_CARD
 argument_list|(
 name|ELSA
 argument_list|,
+name|XI330_IEEE
+argument_list|)
+block|,
+name|PCMCIA_CARD
+argument_list|(
+name|ELSA
+argument_list|,
 name|XI800_IEEE
 argument_list|)
 block|,
