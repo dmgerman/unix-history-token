eec682fa9bf131c1a27ea373200fa276590bf245
Alan Cox <alc@FreeBSD.org>
2007-04-02 05:43:38 +0000

    MFC revision 1.179   Eliminate unnecessary PG_BUSY tests.
    Former-commit-id: 0ea9df7397496e64d174fc168548010a9b2d5e3e

--- sys/ia64/ia64/pmap.c
+++ sys/ia64/ia64/pmap.c
@@ -3281,14 +3281,6 @@ operator|||
 name|m
 operator|->
 name|busy
-operator|||
-operator|(
-name|m
-operator|->
-name|flags
-operator|&
-name|PG_BUSY
-operator|)
 condition|)
 continue|continue;
 name|TAILQ_FOREACH_SAFE
