9771add7cd8684056ba5379868dd87b031d1ce48
Pav Lucistnik <pav@FreeBSD.org>
2007-04-03 08:19:31 +0000

    - Update for new tcl categories
    MFC after:	3 days
    
    
    Former-commit-id: af7fcc2bfa4f529939cd8a05005a83cabeeb9f9b

--- usr.sbin/sysinstall/index.c
+++ usr.sbin/sysinstall/index.c
@@ -461,13 +461,13 @@ literal|"sysutils"
 block|,
 literal|"Various system utilities."
 block|,
+literal|"tcl"
+block|,
+literal|"TCL and packages that depend on it."
+block|,
 literal|"tcl80"
 block|,
 literal|"TCL v8.0 and packages that depend on it."
-block|,
-literal|"tcl81"
-block|,
-literal|"TCL v8.1 and packages that depend on it."
 block|,
 literal|"tcl82"
 block|,
@@ -484,6 +484,10 @@ block|,
 literal|"textproc"
 block|,
 literal|"Text processing/search utilities."
+block|,
+literal|"tk"
+block|,
+literal|"Tk and packages that depend on it."
 block|,
 literal|"tk80"
 block|,
