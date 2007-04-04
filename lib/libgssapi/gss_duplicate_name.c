fa20f0b2c7213a9f9caef62c0decfbbce0641f03
Alexander Kabaev <kan@FreeBSD.org>
2007-04-04 02:40:59 +0000

    Add include directives for string.h and stdlib.h to get proper function prototypess of memset, memcpy and malloc respectively.
    Former-commit-id: d2ead223b56f3cd2daa8558110bdbdd8d5159e21

--- lib/libgssapi/gss_duplicate_name.c
+++ lib/libgssapi/gss_duplicate_name.c
@@ -12,6 +12,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<stdlib.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<errno.h>
 end_include
 
