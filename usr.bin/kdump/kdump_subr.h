b377884face55dc467044f393b03b1e1426b9b53
Ed Maste <emaste@FreeBSD.org>
2007-04-09 22:04:27 +0000

    Add prototype for generated ptraceopname function.
    Former-commit-id: 8d9440205a6a70d7be9d9c4dc358ff72d387d6e2

--- usr.bin/kdump/kdump_subr.h
+++ usr.bin/kdump/kdump_subr.h
@@ -418,5 +418,14 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|ptraceopname
+parameter_list|(
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
 end_unit
 
