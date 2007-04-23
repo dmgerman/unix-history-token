97c77a48649cb3c272681ed22b541a3a1387f461
Gregory Shapiro <gshapiro@FreeBSD.org>
2007-04-23 22:32:30 +0000

    For completeness, also copy over the Copyright year changes from the sendmail.org repo on these modified files.
    Former-commit-id: b85bdb804a0c6a819e7a02f67a6979556936e3f3

--- contrib/sendmail/libmilter/listener.c
+++ contrib/sendmail/libmilter/listener.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  Copyright (c) 1999-2006 Sendmail, Inc. and its suppliers.  *	All rights reserved.  *  * By using this file, you agree to the terms and conditions set  * forth in the LICENSE file which can be found at the top level of  * the sendmail distribution.  *  */
+comment|/*  *  Copyright (c) 1999-2007 Sendmail, Inc. and its suppliers.  *	All rights reserved.  *  * By using this file, you agree to the terms and conditions set  * forth in the LICENSE file which can be found at the top level of  * the sendmail distribution.  *  */
 end_comment
 
 begin_include
