f77bbce97aa60198f1648762a0bf242ce605e143
Konstantin Belousov <kib@FreeBSD.org>
2007-04-26 08:56:56 +0000

    Allow the dounmount() to proceed even for doomed coveredvp.
    In dounmount(), before or while vn_lock(coveredvp) is called, coveredvp
    vnode may be VI_DOOMED due to one of the following:
    - other thread finished unmount and vput()ed it, and vnode was chosen
      for recycling, while vn_lock() slept;
    - forced unmount of the coveredvp->v_mount fs.
    In the first case, next check for changed v_mountedhere or mnt_gen counter
    would be successfull. In the second case, the unmount shall be allowed.
    
    Submitted by:	sobomax
    MFC after:	2 weeks
    
    
    Former-commit-id: 652f1f589c1af106b41401e922dc9fe08d7c0336

--- sys/kern/vfs_mount.c
+++ sys/kern/vfs_mount.c
@@ -5752,8 +5752,6 @@ argument_list|(
 name|coveredvp
 argument_list|)
 expr_stmt|;
-name|error
-operator|=
 name|vn_lock
 argument_list|(
 name|coveredvp
@@ -5761,6 +5759,8 @@ argument_list|,
 name|LK_EXCLUSIVE
 operator||
 name|LK_INTERLOCK
+operator||
+name|LK_RETRY
 argument_list|,
 name|td
 argument_list|)
@@ -5771,15 +5771,6 @@ name|coveredvp
 argument_list|)
 expr_stmt|;
 comment|/* 		 * Check for mp being unmounted while waiting for the 		 * covered vnode lock. 		 */
-if|if
-condition|(
-name|error
-condition|)
-return|return
-operator|(
-name|error
-operator|)
-return|;
 if|if
 condition|(
 name|coveredvp
