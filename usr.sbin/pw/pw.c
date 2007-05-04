1142a76c010bc3f03fa1eaf95b2df15556489443
Lukas Ertl <le@FreeBSD.org>
2007-05-04 17:33:04 +0000

    MFC all changes regarding new option -M.
    Former-commit-id: 08ee554a1d6720ba920bb8c4d958b46ad7c6666c

--- usr.sbin/pw/pw.c
+++ usr.sbin/pw/pw.c
@@ -368,11 +368,11 @@ init|=
 block|{
 block|{
 comment|/* user */
-literal|"V:C:qn:u:c:d:e:p:g:G:mk:s:oL:i:w:h:H:Db:NPy:Y"
+literal|"V:C:qn:u:c:d:e:p:g:G:mM:k:s:oL:i:w:h:H:Db:NPy:Y"
 block|,
 literal|"V:C:qn:u:rY"
 block|,
-literal|"V:C:qn:u:c:d:e:p:g:G:ml:k:s:w:L:h:H:FNPY"
+literal|"V:C:qn:u:c:d:e:p:g:G:mM:l:k:s:w:L:h:H:FNPY"
 block|,
 literal|"V:C:qn:u:FPa7"
 block|,
@@ -432,18 +432,6 @@ end_function
 begin_empty_stmt
 empty_stmt|;
 end_empty_stmt
-
-begin_expr_stmt
-name|umask
-argument_list|(
-literal|0
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_comment
-comment|/* We wish to handle this manually */
-end_comment
 
 begin_expr_stmt
 name|LIST_INIT
@@ -1354,6 +1342,7 @@ literal|"\t-p date        password expiry date\n"
 literal|"\t-g grp         initial group\n"
 literal|"\t-G grp1,grp2   additional groups\n"
 literal|"\t-m [ -k dir ]  create and set up home\n"
+literal|"\t-M mode        home directory permissions\n"
 literal|"\t-s shell       name of login shell\n"
 literal|"\t-o             duplicate uid ok\n"
 literal|"\t-L class       user class\n"
@@ -1371,6 +1360,7 @@ literal|"\t-g group       default group\n"
 literal|"\t-G grp1,grp2   additional groups\n"
 literal|"\t-L class       default user class\n"
 literal|"\t-k dir         default home skeleton\n"
+literal|"\t-M mode        home directory permissions\n"
 literal|"\t-u min,max     set min,max uids\n"
 literal|"\t-i min,max     set min,max gids\n"
 literal|"\t-w method      set default password method\n"
@@ -1400,6 +1390,7 @@ literal|"\t-G grp1,grp2   additional groups\n"
 literal|"\t-l name        new login name\n"
 literal|"\t-L class       user class\n"
 literal|"\t-m [ -k dir ]  create and set up home\n"
+literal|"\t-M mode        home directory permissions\n"
 literal|"\t-s shell       name of login shell\n"
 literal|"\t-w method      set new password using method\n"
 literal|"\t-h fd          read password on fd\n"
