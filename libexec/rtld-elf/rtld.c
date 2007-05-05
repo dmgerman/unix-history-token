dd82a68aee3464aa270a286111dc5e943b6467fd
David Xu <davidxu@FreeBSD.org>
2007-05-05 08:44:59 +0000

    Fix a TLS memory leak.
    PR: threads/112297
    MFC: 1 week
    
    Former-commit-id: fbdee7af00b796777adf3417f476d062ea9c8869

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -14636,6 +14636,15 @@ operator|)
 name|tlsstart
 argument_list|)
 expr_stmt|;
+name|free
+argument_list|(
+operator|(
+name|void
+operator|*
+operator|)
+name|dtv
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
