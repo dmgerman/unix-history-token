e658d5dc101634ae841912a2dc9b18c46e998206
Robert N. M. Watson <rwatson@FreeBSD.org>
2007-05-08 10:22:46 +0000

    Merge uipc_usrreq.c:1.184 from HEAD to RELENG_6:
      Remove now unneeded ENOTCONN clause from SOCK_DGRAM side of uipc_send():
      we have to check it regardless of the target address, so don't check it
      twice.
    
    
    Former-commit-id: fe63cc28016c5e9f001d288a0eac336882f7365c

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -2608,24 +2608,6 @@ name|error
 condition|)
 break|break;
 block|}
-else|else
-block|{
-if|if
-condition|(
-name|unp
-operator|->
-name|unp_conn
-operator|==
-name|NULL
-condition|)
-block|{
-name|error
-operator|=
-name|ENOTCONN
-expr_stmt|;
-break|break;
-block|}
-block|}
 comment|/* 		 * Because connect() and send() are non-atomic in a sendto() 		 * with a target address, it's possible that the socket will 		 * have disconnected before the send() can run.  In that case 		 * return the slightly counter-intuitive but otherwise 		 * correct error that the socket is not connected. 		 */
 if|if
 condition|(
