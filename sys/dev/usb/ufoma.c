71e7560b01dd7632c7be02ea7cdb9d7d38569a41
Alexander Kabaev <kan@FreeBSD.org>
2007-05-19 04:57:15 +0000

    Remove local static redefinition of ucom_devclass.
    Former-commit-id: 2a81e1104af1281976dd12c46095617e216ca6f3

--- sys/dev/usb/ufoma.c
+++ sys/dev/usb/ufoma.c
@@ -984,13 +984,6 @@ block|}
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
-specifier|static
-name|devclass_t
-name|ucom_devclass
-decl_stmt|;
-end_decl_stmt
-
 begin_expr_stmt
 name|DRIVER_MODULE
 argument_list|(
