7fc0b78ce2a2f1adeb69301187fd5e62e4740fec
Alexander Kabaev <kan@FreeBSD.org>
2007-05-19 02:14:13 +0000

    Add FreeBSD suffix to GCC version string.
    Former-commit-id: 9b8d5dd751e83431ded54f5aec83f5fbda2475b2

--- contrib/gcc/version.c
+++ contrib/gcc/version.c
@@ -10,18 +10,15 @@ file|"version.h"
 end_include
 
 begin_comment
-comment|/* This is the string reported as the version number by all components    of the compiler.  If you distribute a modified version of GCC,    please modify this string to indicate that, e.g. by putting your    organization's name in parentheses at the end of the string.  */
+comment|/* This is the trailing component of the string reported as the    version number by all components of the compiler.  For an official    FSF release, it is empty.  If you distribute a modified version of    GCC, please change this string to indicate that.  The suggested    format is a leading space, followed by your organization's name    in parentheses.  You may also wish to include a number indicating    the revision of your modified compiler.  */
 end_comment
 
-begin_decl_stmt
-specifier|const
-name|char
-name|version_string
-index|[]
-init|=
-literal|"3.4.6 [FreeBSD] 20060825"
-decl_stmt|;
-end_decl_stmt
+begin_define
+define|#
+directive|define
+name|VERSUFFIX
+value|" [FreeBSD]"
+end_define
 
 begin_comment
 comment|/* This is the location of the online document giving instructions for    reporting bugs.  If you distribute a modified version of GCC,    please change this to refer to a document giving instructions for    reporting bugs to you, not us.  (You are of course welcome to    forward us bugs reported to you, if you determine that they are    not bugs in your modifications.)  */
@@ -34,6 +31,23 @@ name|bug_report_url
 index|[]
 init|=
 literal|"<URL:http://gcc.gnu.org/bugs.html>"
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* The complete version string, assembled from several pieces.    BASEVER, DATESTAMP, and DEVPHASE are defined by the Makefile.  */
+end_comment
+
+begin_decl_stmt
+specifier|const
+name|char
+name|version_string
+index|[]
+init|=
+name|BASEVER
+name|DATESTAMP
+name|DEVPHASE
+name|VERSUFFIX
 decl_stmt|;
 end_decl_stmt
 
