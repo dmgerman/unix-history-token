af3f17ddc6e8f379d21de94b524f9560c1808a9d
Dag-Erling Smorgrav <des@FreeBSD.org>
2007-05-24 16:09:38 +0000

    MFC: expose vdropl()
    Former-commit-id: 1e1f4e2313e82a56d7910f8b40fecbc25b32956a

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -2715,6 +2715,17 @@ end_function_decl
 
 begin_function_decl
 name|void
+name|vdropl
+parameter_list|(
+name|struct
+name|vnode
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
 name|vfs_add_vnodeops
 parameter_list|(
 specifier|const
