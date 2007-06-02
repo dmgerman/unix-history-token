d312612c1b20bd1e11638f5bc2829c2e04b752cb
JINMEI Tatuya <jinmei@FreeBSD.org>
2007-06-02 08:02:36 +0000

    fixed memory leak for IPv6 multicast membership information associated with interface addresses.
    Approved by:	gnn (mentor)
    MFC after:	1 week
    
    
    Former-commit-id: a752350e98103d61842e887447faf36322d27234

--- sys/netinet6/in6_ifattach.c
+++ sys/netinet6/in6_ifattach.c
@@ -3055,11 +3055,14 @@ name|struct
 name|in6_multi
 modifier|*
 name|in6m
-decl_stmt|;
-name|struct
-name|in6_multi
+decl_stmt|,
 modifier|*
 name|in6m_next
+decl_stmt|;
+name|struct
+name|in6_multi_mship
+modifier|*
+name|imm
 decl_stmt|;
 comment|/* remove neighbor management table */
 name|nd6_purge
@@ -3173,6 +3176,35 @@ operator|*
 operator|)
 name|ifa
 expr_stmt|;
+comment|/* 		 * leave from multicast groups we have joined for the interface 		 */
+while|while
+condition|(
+operator|(
+name|imm
+operator|=
+name|ia
+operator|->
+name|ia6_memberships
+operator|.
+name|lh_first
+operator|)
+operator|!=
+name|NULL
+condition|)
+block|{
+name|LIST_REMOVE
+argument_list|(
+name|imm
+argument_list|,
+name|i6mm_chain
+argument_list|)
+expr_stmt|;
+name|in6_leavegroup
+argument_list|(
+name|imm
+argument_list|)
+expr_stmt|;
+block|}
 comment|/* remove from the routing table */
 if|if
 condition|(
