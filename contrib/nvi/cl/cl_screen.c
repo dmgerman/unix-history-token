76d1d161fbd58ad0ba64bd94b56fe24caa569a5e
Rong-En Fan <rafan@FreeBSD.org>
2007-06-06 11:14:30 +0000

    - Sort the headers per style(9)
    Reminded by:	yar
    Approved by:	delphij (mentor, implicit)
    
    
    Former-commit-id: b95738c3c193068695aef7e30b1389248fa8a319

--- contrib/nvi/cl/cl_screen.c
+++ contrib/nvi/cl/cl_screen.c
@@ -62,12 +62,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<term.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<errno.h>
 end_include
 
@@ -93,6 +87,12 @@ begin_include
 include|#
 directive|include
 file|<string.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<term.h>
 end_include
 
 begin_include
