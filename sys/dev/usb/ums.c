4bb6b3f554d01fa0bec16eaf52f3dd8d6c9f727b
Warner Losh <imp@FreeBSD.org>
2007-06-18 22:30:08 +0000

    Expand usb_port.h macros.
    Former-commit-id: 1990abc9648d21557078a5f6e33c789ed0dac58c

--- sys/dev/usb/ums.c
+++ sys/dev/usb/ums.c
@@ -114,6 +114,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<dev/usb/usb_port.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<dev/usb/usb.h>
 end_include
 
