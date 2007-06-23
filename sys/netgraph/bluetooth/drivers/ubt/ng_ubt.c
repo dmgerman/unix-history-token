cfc4988473bfb7a4c22d0c10d48f90a3433f997a
Warner Losh <imp@FreeBSD.org>
2007-06-23 04:34:38 +0000

    These modules depend on usb, make that explicit
    Approved by: re@
    
    
    Former-commit-id: 0c6f68993f035d798135c56c229919b981b6fa0a

--- sys/netgraph/bluetooth/drivers/ubt/ng_ubt.c
+++ sys/netgraph/bluetooth/drivers/ubt/ng_ubt.c
@@ -916,6 +916,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|ubt
+argument_list|,
+name|usb
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_comment
 comment|/****************************************************************************  ****************************************************************************  **                              USB specific  ****************************************************************************  ****************************************************************************/
 end_comment
