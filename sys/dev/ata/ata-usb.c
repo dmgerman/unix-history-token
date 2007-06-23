5cb04dded5628ca9f9c8c614432eae2ed463e945
Warner Losh <imp@FreeBSD.org>
2007-06-23 21:52:05 +0000

    Include usb_port.h
    Approved by: re@ (blanket and/or cleanup commit approval)
    
    
    Former-commit-id: 87ddd025a89ca9803b807d7b45e46621ea454050

--- sys/dev/ata/ata-usb.c
+++ sys/dev/ata/ata-usb.c
@@ -128,6 +128,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<dev/usb/usb_port.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<dev/usb/usb.h>
 end_include
 
