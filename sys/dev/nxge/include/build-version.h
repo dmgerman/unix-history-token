d482b5dbe0f421d60d7a08b7a2e2128ad4d2d42f
Sam Leffler <sam@FreeBSD.org>
2007-06-29 22:47:18 +0000

    Neterion Xframe 10GbE Server/Storage adapter driver.
    The nxge driver provides support for Neterion Xframe-I and Xframe-II
    adapters. The driver supports TCP Segmentation Offload (TSO/LSO),
    Jumbo frames (5 buffer mode), Header separation (2 and 3 Receive
    buffer modes), VLAN, and Promiscuous mode.
    
    Submitted by:	Neterion
    Reviewed by:	rwatson
    Approved by:	re (kensmith)
    
    
    Former-commit-id: b7155796e6136aa9b2150d76668558177184dbbf

---
+++ sys/dev/nxge/include/build-version.h
@@ -0,0 +1,39 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|BUILD_VERSION_H
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|BUILD_VERSION_H
+end_define
+
+begin_comment
+comment|/* Do not edit! Automatically generated when released.*/
+end_comment
+
+begin_define
+define|#
+directive|define
+name|GENERATED_BUILD_VERSION
+value|"10294"
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* BUILD_VERSION_H */
+end_comment
+
+end_unit
+
