dca0eec86c05d47be25b043db13c220fe7e06710
Robert N. M. Watson <rwatson@FreeBSD.org>
2007-07-03 10:14:13 +0000

    Fix a bug in sblock() that has existed since revision 1.1 from BSD: correctly return an error if M_NOWAIT is passed to sblock() and the operation might block.  This remarkably subtle macro bug appears to be responsible for quite a few undiagnosed socket buffer corruption and mbuf-related kernel panics.
    This bug has already been fixed in 7-CURRENT as part of the move to
    using sx(9) locks to serialize simultaneous socket I/O, but is an
    MFC candidate for all earlier FreeBSD -STABLE branches.
    
    MFC after:	2 weeks
    Found by:	Isilon
    Submitted by:	jeff
    Tested by:	jhb, Yahoo!
    
    
    Former-commit-id: d7b6bdf83a5d280757e805c8a927ba4fe7701868

--- sys/sys/socketvar.h
+++ sys/sys/socketvar.h
@@ -951,7 +951,7 @@ name|sb
 parameter_list|,
 name|wf
 parameter_list|)
-value|((sb)->sb_flags& SB_LOCK ? \ 		(((wf) == M_WAITOK) ? sb_lock(sb) : EWOULDBLOCK) : \ 		((sb)->sb_flags |= SB_LOCK), 0)
+value|((sb)->sb_flags& SB_LOCK ? \ 		(((wf) == M_WAITOK) ? sb_lock(sb) : EWOULDBLOCK) : \ 		((sb)->sb_flags |= SB_LOCK, 0))
 end_define
 
 begin_comment
