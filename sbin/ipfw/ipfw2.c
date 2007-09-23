5ed47d45eb7efa126beb8d6551dbb0cf5e50d46d
Maxim Konovalov <maxim@FreeBSD.org>
2007-09-23 16:29:22 +0000

    o Cosmetic: fix the issue when "ipfw(8) show" produces "not" twice:
    $ ipfw -n add 1 allow layer2 not mac-type ip
    00001 allow ip from any to any layer2 not not mac-type 0x0800
    
    PR:		bin/115372
    Submitted by:	Andrey V. Elsukov
    Approved by:	re (hrs)
    MFC after:	3 weeks
    
    Former-commit-id: ef585c749b99ed741bd6e941924efe3c3ecc031f

--- sbin/ipfw/ipfw2.c
+++ sbin/ipfw/ipfw2.c
@@ -2624,21 +2624,6 @@ name|sep
 decl_stmt|;
 if|if
 condition|(
-name|cmd
-operator|->
-name|o
-operator|.
-name|len
-operator|&
-name|F_NOT
-condition|)
-name|printf
-argument_list|(
-literal|" not"
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
 name|opcode
 operator|!=
 literal|0
@@ -8180,6 +8165,19 @@ condition|)
 name|printf
 argument_list|(
 literal|" {"
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|cmd
+operator|->
+name|len
+operator|&
+name|F_NOT
+condition|)
+name|printf
+argument_list|(
+literal|" not"
 argument_list|)
 expr_stmt|;
 name|print_newports
