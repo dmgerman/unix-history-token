9bb18b0b58a8a45e0db8bc475536f28c6d804fb5
David E. O'Brien <obrien@FreeBSD.org>
2007-10-02 20:06:14 +0000

    Do not imply -ftree-vrp with -O2 and above.  One must implicitly specify '-ftree-vrp' if one wants it. Some bad code generation has been tracked to -ftree-vrp.  jdk1{5,6} are notable examples.
    Approved by:	re(kensmith)
    
    
    Former-commit-id: bf84aeb1f68788728074f4e09b9fa47d6bbbaeba

--- contrib/gcc/opts.c
+++ contrib/gcc/opts.c
@@ -2220,10 +2220,8 @@ name|flag_tree_store_copy_prop
 operator|=
 literal|1
 expr_stmt|;
-name|flag_tree_vrp
-operator|=
-literal|1
-expr_stmt|;
+comment|/* XXX: some issues with ports have been traced to -ftree-vrp.          So remove it from -O2 and above.  Note that jdk1{5,6} are affected          and they build with w/-O3 - so we cannot just move it to -O3. */
+comment|// flag_tree_vrp = 1;
 if|if
 condition|(
 operator|!
