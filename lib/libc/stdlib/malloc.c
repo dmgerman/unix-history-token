18f807b49c740a75a39573f580ca99b83c383880
Ken Smith <kensmith@FreeBSD.org>
2007-10-11 06:35:46 +0000

    Turn on MALLOC_PRODUCTION which turns off some stuff used for debugging support.
    Reminded by:	kris
    Approved by:	re (implicit)
    
    
    Former-commit-id: 665626ec20544fa054522bce843759224cbcc52c

--- lib/libc/stdlib/malloc.c
+++ lib/libc/stdlib/malloc.c
@@ -7,9 +7,11 @@ begin_comment
 comment|/*  * MALLOC_PRODUCTION disables assertions and statistics gathering.  It also  * defaults the A and J runtime options to off.  These settings are appropriate  * for production systems.  */
 end_comment
 
-begin_comment
-comment|/* #define MALLOC_PRODUCTION */
-end_comment
+begin_define
+define|#
+directive|define
+name|MALLOC_PRODUCTION
+end_define
 
 begin_ifndef
 ifndef|#
