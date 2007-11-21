50c45f6a2b15647e68dd78d231024e1663f1e27a
Soren Schmidt <sos@FreeBSD.org>
2007-11-21 21:15:00 +0000

    Update with the latest fixes from -current.
    Approved by: re@
    
    
    Former-commit-id: 915984dc6fc87549fea8383f5a0178ec9911d3cd

--- sys/dev/ata/ata-dma.c
+++ sys/dev/ata/ata-dma.c
@@ -1359,6 +1359,17 @@ argument_list|(
 name|ATA_DMA_EOT
 argument_list|)
 expr_stmt|;
+name|KASSERT
+argument_list|(
+name|nsegs
+operator|<=
+name|ATA_DMA_ENTRIES
+argument_list|,
+operator|(
+literal|"too many DMA segment entries\n"
+operator|)
+argument_list|)
+expr_stmt|;
 name|args
 operator|->
 name|nsegs
