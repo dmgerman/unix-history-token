950d6d329d5258cfefdd247675d0077d8c097ace
John Baldwin <jhb@FreeBSD.org>
2007-11-28 22:20:08 +0000

    Adjust the code to probe for the PCI config mechanism to use. - On amd64, just assume type #1 is always used.  PCI 2.0 mandated   deprecated type #2 and required type #1 for all future bridges which   was well before amd64 existed. - For i386, ignore whatever value was in 0xcf8 before testing for type #1   and instead rely on the other tests to determine if type #1 works.  Some   newer machines leave garbage in 0xcf8 during boot and as a result the   kernel doesn't find PCI at all (which greatly confuses ACPI which expects   PCI to exist when PCI busses are in the namespace).
    MFC after:	3 days
    Discussed with:	scottl
    
    
    Former-commit-id: 5d5012577d7cae30f66359f5b64ce1f229d28303

--- sys/i386/pci/pci_cfgreg.c
+++ sys/i386/pci/pci_cfgreg.c
@@ -1656,6 +1656,7 @@ name|mode2res
 decl_stmt|,
 name|oldval2
 decl_stmt|;
+comment|/* Check for type #1 first. */
 name|oldval1
 operator|=
 name|inl
@@ -1676,17 +1677,6 @@ name|oldval1
 argument_list|)
 expr_stmt|;
 block|}
-if|if
-condition|(
-operator|(
-name|oldval1
-operator|&
-name|CONF1_ENABLE_MSK
-operator|)
-operator|==
-literal|0
-condition|)
-block|{
 name|cfgmech
 operator|=
 name|CFGMECH_1
@@ -1810,7 +1800,7 @@ name|cfgmech
 operator|)
 return|;
 block|}
-block|}
+comment|/* Type #1 didn't work, so try type #2. */
 name|oldval2
 operator|=
 name|inb
@@ -1914,6 +1904,7 @@ operator|)
 return|;
 block|}
 block|}
+comment|/* Nothing worked, so punt. */
 name|cfgmech
 operator|=
 name|CFGMECH_NONE
