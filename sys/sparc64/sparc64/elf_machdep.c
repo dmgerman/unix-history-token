b9e0b16711744dc7372a86a8fd752e498d58d5e5
Marius Strobl <marius@FreeBSD.org>
2007-12-04 20:00:48 +0000

    MFC: libexec/rtld-elf/sparc64/reloc.c 1.16,      sys/sparc64/sparc64/elf_machdep.c 1.24
    - Fix the handling of R_SPARC_OLO10, which is a bit of a special case
      in the way we implement handling of relocations.
      While at it replace some more ELF_R_TYPE which should have been
      ELF64_R_TYPE_ID but didn't cause problems so far.
    - Sync a sanity check between kernel and rtld(1) and change it to be
      maintenance free regarding the type used for the lookup table.
    - Sprinkle const on lookup tables.
    - Use __FBSDID.
    
    
    Former-commit-id: 9aebc9b87542d16347dfd1748284e5b5f48280d9

--- sys/sparc64/sparc64/elf_machdep.c
+++ sys/sparc64/sparc64/elf_machdep.c
@@ -1,7 +1,21 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2001 Jake Burkholder.  * Copyright (c) 2000 Eduardo Horvath.  * Copyright (c) 1999 The NetBSD Foundation, Inc.  * All rights reserved.  *  * This code is derived from software contributed to The NetBSD Foundation  * by Paul Kranenburg.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *        This product includes software developed by the NetBSD  *        Foundation, Inc. and its contributors.  * 4. Neither the name of The NetBSD Foundation nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  *	from: NetBSD: mdreloc.c,v 1.5 2001/04/25 12:24:51 kleink Exp  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2001 Jake Burkholder.  * Copyright (c) 2000 Eduardo Horvath.  * Copyright (c) 1999 The NetBSD Foundation, Inc.  * All rights reserved.  *  * This code is derived from software contributed to The NetBSD Foundation  * by Paul Kranenburg.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *        This product includes software developed by the NetBSD  *        Foundation, Inc. and its contributors.  * 4. Neither the name of The NetBSD Foundation nor the names of its  *    contributors may be used to endorse or promote products derived  *    from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  *	from: NetBSD: mdreloc.c,v 1.5 2001/04/25 12:24:51 kleink Exp  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_include
 include|#
@@ -94,6 +108,7 @@ file|"linker_if.h"
 end_include
 
 begin_decl_stmt
+specifier|static
 name|struct
 name|sysentvec
 name|elf64_freebsd_sysvec
@@ -373,6 +388,7 @@ end_comment
 
 begin_decl_stmt
 specifier|static
+specifier|const
 name|int
 name|reloc_target_flags
 index|[]
@@ -1305,6 +1321,7 @@ end_define
 
 begin_decl_stmt
 specifier|static
+specifier|const
 name|long
 name|reloc_target_bitmask
 index|[]
@@ -1490,7 +1507,7 @@ block|,
 comment|/* _10, _11, _64 */
 name|_BM
 argument_list|(
-literal|10
+literal|13
 argument_list|)
 block|,
 name|_BM
@@ -1676,7 +1693,7 @@ name|data
 expr_stmt|;
 if|if
 condition|(
-name|ELF_R_TYPE
+name|ELF64_R_TYPE_ID
 argument_list|(
 name|rela
 operator|->
@@ -1834,7 +1851,7 @@ name|where
 expr_stmt|;
 name|rtype
 operator|=
-name|ELF_R_TYPE
+name|ELF64_R_TYPE_ID
 argument_list|(
 name|rela
 operator|->
@@ -1876,8 +1893,17 @@ operator|==
 name|R_SPARC_COPY
 operator|||
 name|rtype
-operator|>
-name|R_SPARC_UA16
+operator|>=
+sizeof|sizeof
+argument_list|(
+name|reloc_target_bitmask
+argument_list|)
+operator|/
+sizeof|sizeof
+argument_list|(
+operator|*
+name|reloc_target_bitmask
+argument_list|)
 condition|)
 return|return
 operator|(
@@ -1940,6 +1966,27 @@ operator|+=
 name|addr
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|rtype
+operator|==
+name|R_SPARC_OLO10
+condition|)
+name|value
+operator|=
+operator|(
+name|value
+operator|&
+literal|0x3ff
+operator|)
+operator|+
+name|ELF64_R_TYPE_DATA
+argument_list|(
+name|rela
+operator|->
+name|r_info
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|RELOC_PC_RELATIVE
