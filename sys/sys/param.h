df685255d916b5183dd59e5c89fe1e72b1ba3380
Kip Macy <kmacy@FreeBSD.org>
2007-12-05 23:48:03 +0000

    Fix bit macros to work on multi-byte types
    MFC after: 1 day
    
    
    Former-commit-id: 66f88a689ea2104b5615caa6c8ef883a0b8ac4d1

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -840,7 +840,7 @@ name|a
 parameter_list|,
 name|i
 parameter_list|)
-value|((a)[(i)/NBBY] |= 1<<((i)%NBBY))
+value|(((uint8_t *)(a))[(i)/NBBY] |= 1<<((i)%NBBY))
 end_define
 
 begin_define
@@ -852,7 +852,7 @@ name|a
 parameter_list|,
 name|i
 parameter_list|)
-value|((a)[(i)/NBBY]&= ~(1<<((i)%NBBY)))
+value|(((uint8_t *)(a))[(i)/NBBY]&= ~(1<<((i)%NBBY)))
 end_define
 
 begin_define
@@ -864,7 +864,7 @@ name|a
 parameter_list|,
 name|i
 parameter_list|)
-value|((a)[(i)/NBBY]& (1<<((i)%NBBY)))
+value|(((uint8_t *)(a))[(i)/NBBY]& (1<<((i)%NBBY)))
 end_define
 
 begin_define
@@ -876,7 +876,7 @@ name|a
 parameter_list|,
 name|i
 parameter_list|)
-value|(((a)[(i)/NBBY]& (1<<((i)%NBBY))) == 0)
+value|((((uint8_t *)(a))[(i)/NBBY]& (1<<((i)%NBBY))) == 0)
 end_define
 
 begin_comment
