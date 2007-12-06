1d5277669859eddb874513df760814720b37eaa8
John Baldwin <jhb@FreeBSD.org>
2007-12-06 23:11:27 +0000

    Move several data structure definitions out of freebsd32_misc.c and into freebsd32.h instead.
    MFC after:	1 week
    
    
    Former-commit-id: 33f421f7d99c1ad39b257016cfee50c2075cafc8

--- sys/kern/kern_module.c
+++ sys/kern/kern_module.c
@@ -1830,6 +1830,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/socket.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<compat/freebsd32/freebsd32_util.h>
 end_include
 
