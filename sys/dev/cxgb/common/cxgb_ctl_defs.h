dd032421757d603c890ccbcdcb6305c226c33ca6
Kip Macy <kmacy@FreeBSD.org>
2007-12-06 00:02:00 +0000

    Common code changes for firmware update.
    Approved by: re(kensmith)
    
    
    Former-commit-id: 77a929df60ebca9aa04c34421ad46c23567d0e30

--- sys/dev/cxgb/common/cxgb_ctl_defs.h
+++ sys/dev/cxgb/common/cxgb_ctl_defs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2003-2006 Chelsio Communications.  All rights reserved.  *  * This program is distributed in the hope that it will be useful, but WITHOUT  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or  * FITNESS FOR A PARTICULAR PURPOSE.  See the LICENSE file included in this  * release for licensing terms and conditions.  *  * $FreeBSD$  */
+comment|/*  Copyright (c) 2007, Chelsio Inc. All rights reserved.  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:   1. Redistributions of source code must retain the above copyright notice,     this list of conditions and the following disclaimer.   2. Neither the name of the Chelsio Corporation nor the names of its     contributors may be used to endorse or promote products derived from     this software without specific prior written permission.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -57,14 +57,24 @@ block|,
 name|RDMA_GET_MEM
 block|,
 name|FAILOVER
+init|=
+literal|30
 block|,
 name|FAILOVER_DONE
+init|=
+literal|31
 block|,
 name|FAILOVER_CLEAR
+init|=
+literal|32
 block|,
 name|GET_CPUIDX_OF_QSET
+init|=
+literal|40
 block|,
 name|GET_RX_PAGE_INFO
+init|=
+literal|50
 block|, }
 enum|;
 end_enum
