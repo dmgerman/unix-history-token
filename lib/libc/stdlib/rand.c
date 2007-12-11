2b7af902e5dde496f72d808d7f8fa0a24d348a2b
Andrey Chernov <ache@FreeBSD.org>
2007-12-11 20:39:32 +0000

    Fix typo in the comment
    Former-commit-id: 4ae2067fa1a34fa13069be500b4d45e9529bffd6

--- lib/libc/stdlib/rand.c
+++ lib/libc/stdlib/rand.c
@@ -168,7 +168,7 @@ return|;
 else|#
 directive|else
 comment|/* !USE_WEAK_SEEDING */
-comment|/*  * Compute x = (7^5 * x) mod (2^31 - 1)  * wihout overflowing 31 bits:  *      (2^31 - 1) = 127773 * (7^5) + 2836  * From "Random number generators: good ones are hard to find",  * Park and Miller, Communications of the ACM, vol. 31, no. 10,  * October 1988, p. 1195.  */
+comment|/*  * Compute x = (7^5 * x) mod (2^31 - 1)  * without overflowing 31 bits:  *      (2^31 - 1) = 127773 * (7^5) + 2836  * From "Random number generators: good ones are hard to find",  * Park and Miller, Communications of the ACM, vol. 31, no. 10,  * October 1988, p. 1195.  */
 name|long
 name|hi
 decl_stmt|,
