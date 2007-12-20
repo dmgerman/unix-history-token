4586cabab76cacd2fd98105f5fa81da21fac15df
David Schultz <das@FreeBSD.org>
2007-12-20 03:16:55 +0000

    Grumble. DO declare logbl(), DON'T declare logl() just yet. bde is going to commit logl() Real Soon Now. I'm just trying to slow him down with merge conflicts.
    Noticed by:	bde
    
    
    Former-commit-id: c1abb6ae5d1db90779a0d4e43101a530aee8091b

--- lib/msun/src/math.h
+++ lib/msun/src/math.h
@@ -2818,7 +2818,30 @@ literal|0
 end_if
 
 begin_endif
-unit|long double	log10l(long double); long double	log1pl(long double); long double	log2l(long double); long double	logbl(long double); long double	logl(long double); long		lrintl(long double);
+unit|long double	log10l(long double); long double	log1pl(long double); long double	log2l(long double);
+endif|#
+directive|endif
+end_endif
+
+begin_function_decl
+name|long
+name|double
+name|logbl
+parameter_list|(
+name|long
+name|double
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_if
+if|#
+directive|if
+literal|0
+end_if
+
+begin_endif
+unit|long double	logl(long double); long		lrintl(long double);
 endif|#
 directive|endif
 end_endif
