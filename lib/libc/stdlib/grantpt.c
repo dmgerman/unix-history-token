e571854beef0b104088f407aea2a07b713f77a8e
John Baldwin <jhb@FreeBSD.org>
2007-12-21 21:26:08 +0000

    Clean up some of the pts(4) vs pty(4) stuff in grantpt(3) and friends: - Use PTY* for all pty(4) related constants. - Use PTMX* for all pts(4) related constants. - Consistently use _PATH_DEV PTMX rather than "/dev/ptmx". - Revert 1.7 and properly fix it by using the correct prefix string for   pts(4) masters.
    MFC after:	3 days
    
    
    Former-commit-id: 7f7ba958e50919f36fb45a3ea306ca989c350384

--- lib/libc/stdlib/grantpt.c
+++ lib/libc/stdlib/grantpt.c
@@ -149,31 +149,46 @@ end_include
 begin_define
 define|#
 directive|define
-name|PTM_PREFIX
+name|PTYM_PREFIX
 value|"pty"
 end_define
 
 begin_comment
-comment|/* pseudo tty master naming convention */
+comment|/* pty(4) master naming convention */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|PTS_PREFIX
+name|PTYS_PREFIX
 value|"tty"
 end_define
 
 begin_comment
-comment|/* pseudo tty slave naming convention */
+comment|/* pty(4) slave naming convention */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|NEWPTS_PREFIX
-value|"pts"
+name|PTMXM_PREFIX
+value|"ptc/"
 end_define
+
+begin_comment
+comment|/* pts(4) master naming convention */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PTMXS_PREFIX
+value|"pts/"
+end_define
+
+begin_comment
+comment|/* pts(4) slave naming convention */
+end_comment
 
 begin_define
 define|#
@@ -189,21 +204,21 @@ end_comment
 begin_define
 define|#
 directive|define
-name|PT_MAX
+name|PTY_MAX
 value|256
 end_define
 
 begin_define
 define|#
 directive|define
-name|PT_DEV1
+name|PTY_DEV1
 value|"pqrsPQRSlmnoLMNO"
 end_define
 
 begin_define
 define|#
 directive|define
-name|PT_DEV2
+name|PTY_DEV2
 value|"0123456789abcdefghijklmnopqrstuv"
 end_define
 
@@ -229,7 +244,7 @@ name|ISPTM
 parameter_list|(
 name|x
 parameter_list|)
-value|(S_ISCHR((x).st_mode)&& 			\ 			 minor((x).st_rdev)>= 0&&			\ 			 minor((x).st_rdev)< PT_MAX)
+value|(S_ISCHR((x).st_mode)&& 			\ 			 minor((x).st_rdev)>= 0&&			\ 			 minor((x).st_rdev)< PTY_MAX)
 end_define
 
 begin_function
@@ -315,10 +330,9 @@ if|if
 condition|(
 name|stat
 argument_list|(
-literal|"/dev/ptmx"
+argument|_PATH_DEV PTMX
 argument_list|,
-operator|&
-name|sb
+argument|&sb
 argument_list|)
 operator|!=
 literal|0
@@ -677,7 +691,7 @@ name|master
 index|[]
 init|=
 name|_PATH_DEV
-name|PTM_PREFIX
+name|PTYM_PREFIX
 literal|"XY"
 decl_stmt|;
 specifier|const
@@ -753,7 +767,7 @@ name|master
 operator|+
 name|strlen
 argument_list|(
-argument|_PATH_DEV PTM_PREFIX
+argument|_PATH_DEV PTYM_PREFIX
 argument_list|)
 expr_stmt|;
 name|mc2
@@ -767,7 +781,7 @@ for|for
 control|(
 name|pc1
 operator|=
-name|PT_DEV1
+name|PTY_DEV1
 init|;
 operator|!
 name|bflag
@@ -787,7 +801,7 @@ for|for
 control|(
 name|pc2
 operator|=
-name|PT_DEV2
+name|PTY_DEV2
 init|;
 operator|(
 operator|*
@@ -882,20 +896,20 @@ parameter_list|)
 block|{
 specifier|static
 name|char
-name|slave
+name|pty_slave
 index|[]
 init|=
 name|_PATH_DEV
-name|PTS_PREFIX
+name|PTYS_PREFIX
 literal|"XY"
 decl_stmt|;
 specifier|static
 name|char
-name|new_slave
+name|ptmx_slave
 index|[]
 init|=
 name|_PATH_DEV
-name|NEWPTS_PREFIX
+name|PTMXS_PREFIX
 literal|"4294967295"
 decl_stmt|;
 name|char
@@ -951,19 +965,19 @@ name|void
 operator|)
 name|snprintf
 argument_list|(
-argument|slave
+argument|pty_slave
 argument_list|,
-argument|sizeof(slave)
+argument|sizeof(pty_slave)
 argument_list|,
-argument|_PATH_DEV PTS_PREFIX
+argument|_PATH_DEV PTYS_PREFIX
 literal|"%s"
 argument_list|,
-argument|devname(sbuf.st_rdev, S_IFCHR) + 					       strlen(PTM_PREFIX)
+argument|devname(sbuf.st_rdev, S_IFCHR) + 					       strlen(PTYM_PREFIX)
 argument_list|)
 expr_stmt|;
 name|retval
 operator|=
-name|slave
+name|pty_slave
 expr_stmt|;
 block|}
 else|else
@@ -973,19 +987,19 @@ name|void
 operator|)
 name|snprintf
 argument_list|(
-argument|new_slave
+argument|ptmx_slave
 argument_list|,
-argument|sizeof(new_slave)
+argument|sizeof(ptmx_slave)
 argument_list|,
-argument|_PATH_DEV NEWPTS_PREFIX
+argument|_PATH_DEV PTMXS_PREFIX
 literal|"%s"
 argument_list|,
-argument|devname(sbuf.st_rdev, S_IFCHR) + 					       strlen(PTM_PREFIX)
+argument|devname(sbuf.st_rdev, S_IFCHR) + 					       strlen(PTMXM_PREFIX)
 argument_list|)
 expr_stmt|;
 name|retval
 operator|=
-name|new_slave
+name|ptmx_slave
 expr_stmt|;
 block|}
 block|}
