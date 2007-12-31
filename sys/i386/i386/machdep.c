1853f50291a7297fe8ea9376d23890907a5964b0
John Baldwin <jhb@FreeBSD.org>
2007-12-31 21:12:45 +0000

    Include a "pae" feature if an i386 kernel is built with PAE support.
    Obtained from:	Yahoo!
    
    
    Former-commit-id: 53920f138e6cb80d736fcf524a33786f689929f2

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -949,6 +949,27 @@ literal|0
 decl_stmt|;
 end_decl_stmt
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|PAE
+end_ifdef
+
+begin_expr_stmt
+name|FEATURE
+argument_list|(
+name|pae
+argument_list|,
+literal|"Physical Address Extensions"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_comment
 comment|/*  * The number of PHYSMAP entries must be one less than the number of  * PHYSSEG entries because the PHYSMAP entry that spans the largest  * physical address that is accessible by ISA DMA is split into two  * PHYSSEG entries.  */
 end_comment
