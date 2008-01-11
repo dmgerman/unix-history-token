f46ef19220d829f9fd160037d2af7b61739540a8
Bruce Evans <bde@FreeBSD.org>
2008-01-11 17:54:20 +0000

    MFamd64 1.15-1.18 (cosmetic changes, mainly to comments).  The inline functions haven't been cleaned up here because the amd64 cleanups don't apply directly and the functions here will be merged or rewritten later.
    Former-commit-id: 6554d8df3f750900e21a4ba98165b6981cf7fb2e

--- sys/i386/include/ieeefp.h
+++ sys/i386/include/ieeefp.h
@@ -1,10 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1990 Andrew Moore, Talke Studio  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * 	from: @(#) ieeefp.h 	1.0 (Berkeley) 9/23/93  * $FreeBSD$  */
-end_comment
-
-begin_comment
-comment|/*  *	IEEE floating point type and constant definitions.  */
+comment|/*-  * Copyright (c) 2003 Peter Wemm.  * Copyright (c) 1990 Andrew Moore, Talke Studio  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * 	from: @(#) ieeefp.h 	1.0 (Berkeley) 9/23/93  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -18,6 +14,10 @@ define|#
 directive|define
 name|_MACHINE_IEEEFP_H_
 end_define
+
+begin_comment
+comment|/*  * IEEE floating point type, constant and function definitions.  * XXX: FP*FLD, FP*REG and FP*OFF are undocumented pollution.  */
+end_comment
 
 begin_ifndef
 ifndef|#
@@ -37,7 +37,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * FP rounding modes  */
+comment|/*  * Rounding modes.  */
 end_comment
 
 begin_typedef
@@ -51,10 +51,10 @@ block|,
 comment|/* round to nearest */
 name|FP_RM
 block|,
-comment|/* round down to minus infinity */
+comment|/* round down towards minus infinity */
 name|FP_RP
 block|,
-comment|/* round up to plus infinity */
+comment|/* round up towards plus infinity */
 name|FP_RZ
 comment|/* truncate */
 block|}
@@ -63,7 +63,7 @@ typedef|;
 end_typedef
 
 begin_comment
-comment|/*  * FP precision modes  */
+comment|/*  * Precision (i.e., rounding precision) modes.  */
 end_comment
 
 begin_typedef
@@ -96,7 +96,7 @@ value|int
 end_define
 
 begin_comment
-comment|/*  * FP exception masks  */
+comment|/*  * Exception bit masks.  */
 end_comment
 
 begin_define
@@ -177,7 +177,7 @@ comment|/* stack fault */
 end_comment
 
 begin_comment
-comment|/*  * FP registers  */
+comment|/*  * FPU control and status register numbers (indexes into the env array).  */
 end_comment
 
 begin_define
@@ -225,7 +225,7 @@ comment|/* sticky flags */
 end_comment
 
 begin_comment
-comment|/*  * FP register bit field masks  */
+comment|/*  * FPU control word bit-field masks.  */
 end_comment
 
 begin_define
@@ -258,7 +258,11 @@ value|0xc00
 end_define
 
 begin_comment
-comment|/* round control field */
+comment|/* rounding control field */
+end_comment
+
+begin_comment
+comment|/*  * FPU status word bit-field masks.  */
 end_comment
 
 begin_define
@@ -273,7 +277,7 @@ comment|/* sticky flags field */
 end_comment
 
 begin_comment
-comment|/*  * FP register bit field offsets  */
+comment|/*  * FPU control word bit-field offsets (shift counts).  */
 end_comment
 
 begin_define
@@ -306,7 +310,11 @@ value|10
 end_define
 
 begin_comment
-comment|/* round control offset */
+comment|/* rounding control offset */
+end_comment
+
+begin_comment
+comment|/*  * FPU status word bit-field offsets (shift counts).  */
 end_comment
 
 begin_define
@@ -339,21 +347,21 @@ end_define
 begin_define
 define|#
 directive|define
-name|__fnstenv
-parameter_list|(
-name|addr
-parameter_list|)
-value|__asm __volatile("fnstenv %0" : "=m" (*(addr)))
-end_define
-
-begin_define
-define|#
-directive|define
 name|__fnstcw
 parameter_list|(
 name|addr
 parameter_list|)
 value|__asm __volatile("fnstcw %0" : "=m" (*(addr)))
+end_define
+
+begin_define
+define|#
+directive|define
+name|__fnstenv
+parameter_list|(
+name|addr
+parameter_list|)
+value|__asm __volatile("fnstenv %0" : "=m" (*(addr)))
 end_define
 
 begin_define
