7a762d59cf0a3bf9a03397e2f29d250f51266ae2
David E. O'Brien <obrien@FreeBSD.org>
2008-01-13 06:11:36 +0000

    Merge rev 1.11 ("iso8601" option keyword)  into version 1.11.22. (all our other local changes are OBE)
    Former-commit-id: b10e5473d06f3c8b94df139733eab94429691bb1

--- contrib/cvs/src/rcs.h
+++ contrib/cvs/src/rcs.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1992, Brian Berliner and Jeff Polk  * Copyright (c) 1989-1992, Brian Berliner  *   * You may distribute under the terms of the GNU General Public License as  * specified in the README file that comes with the CVS source distribution.  *   * RCS source control definitions needed by rcs.c and friends  *  * $FreeBSD$  */
+comment|/*  * Copyright (C) 1986-2005 The Free Software Foundation, Inc.  *  * Portions Copyright (C) 1998-2005 Derek Price, Ximbiot<http://ximbiot.com>,  *                                  and others.  *  * Portions Copyright (C) 1992, Brian Berliner and Jeff Polk  * Portions Copyright (C) 1989-1992, Brian Berliner  *   * You may distribute under the terms of the GNU General Public License as  * specified in the README file that comes with the CVS source distribution.  *   * RCS source control definitions needed by rcs.c and friends  *  * $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -1033,6 +1033,9 @@ specifier|const
 name|char
 operator|*
 name|rev
+operator|,
+name|time_t
+name|citime
 operator|,
 name|int
 name|flags
