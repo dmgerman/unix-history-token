6a8a42f356f0f1e6fe9bee91e2141571fee715c8
Kip Macy <kmacy@FreeBSD.org>
2008-01-16 00:26:04 +0000

    put tx queue size back to 1024
    Former-commit-id: 5fc3ea538450092aed4f1d581aec20ab8d644c02

--- sys/dev/cxgb/cxgb_adapter.h
+++ sys/dev/cxgb/cxgb_adapter.h
@@ -499,35 +499,12 @@ name|RSPQ_Q_SIZE
 value|1024
 end_define
 
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
 begin_define
 define|#
 directive|define
 name|TX_ETH_Q_SIZE
 value|1024
 end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_define
-define|#
-directive|define
-name|TX_ETH_Q_SIZE
-value|64
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_enum
 enum|enum
