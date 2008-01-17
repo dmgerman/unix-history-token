f4946cbfb8538e268e3596a1eadebabecefc9e0f
John Baldwin <jhb@FreeBSD.org>
2008-01-17 20:11:58 +0000

    Use pause(9) instead of a tsleep() on a stack variable.
    MFC after:	1 week
    
    
    Former-commit-id: e747b391d11b08fd947ea0ad0b0810c6aa733e85

--- sys/dev/if_ndis/if_ndis.c
+++ sys/dev/if_ndis/if_ndis.c
@@ -14959,13 +14959,8 @@ argument_list|)
 expr_stmt|;
 return|return;
 block|}
-name|tsleep
+name|pause
 argument_list|(
-operator|&
-name|error
-argument_list|,
-name|PWAIT
-argument_list|,
 literal|"ssidscan"
 argument_list|,
 name|hz
