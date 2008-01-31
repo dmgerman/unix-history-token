28d6a0c652081539461cc8ac50cbfb222edb37cc
Daniel Eischen <deischen@FreeBSD.org>
2008-01-31 19:38:26 +0000

    When reinitializing a lockuser, don't assume that the lock is in use.  If it is in use, use the watched request, otherwise use the lockuser's own request.  Only allocate a lockuser request if both requests are null.
    PR:	119920
    Tested by (6.x):	Landon Fuller <landonf -at- bikemonkey -dot- org>
    
    Former-commit-id: 06ab39a7e2f5b9e981e7e3f14d84def6e906b677

--- lib/libkse/sys/lock.c
+++ lib/libkse/sys/lock.c
@@ -538,7 +538,16 @@ operator|-
 literal|1
 operator|)
 return|;
-comment|/* 	 * All lockusers keep their watch request and drop their 	 * own (lu_myreq) request.  Their own request is either 	 * some other lockuser's watch request or is the head of 	 * the lock. 	 */
+if|if
+condition|(
+name|lu
+operator|->
+name|lu_watchreq
+operator|!=
+name|NULL
+condition|)
+block|{
+comment|/* 		 * In this case the lock is active.  All lockusers 		 * keep their watch request and drop their own 		 * (lu_myreq) request.  Their own request is either 		 * some other lockuser's watch request or is the 		 * head of the lock. 		 */
 name|lu
 operator|->
 name|lu_myreq
@@ -547,6 +556,13 @@ name|lu
 operator|->
 name|lu_watchreq
 expr_stmt|;
+name|lu
+operator|->
+name|lu_watchreq
+operator|=
+name|NULL
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|lu
@@ -555,6 +571,7 @@ name|lu_myreq
 operator|==
 name|NULL
 condition|)
+comment|/* 		 * Oops, something isn't quite right.  Try to 		 * allocate one. 		 */
 return|return
 operator|(
 name|_lockuser_init
