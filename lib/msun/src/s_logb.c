32b07acb3b42e9b650d842363292c1d039c97d7e
Bruce Evans <bde@FreeBSD.org>
2008-02-08 01:22:13 +0000

    Oops, fix the fix in rev.1.10.  logb() and logbf() were broken on denormals, and logb() remained broken after 1.10 because the fix for logbf() was incompletely translated.
    Convert to __FBSDID().
    
    
    Former-commit-id: 739ba14660ac602beecefc770d1115748b4e3789

--- lib/msun/src/s_logb.c
+++ lib/msun/src/s_logb.c
@@ -7,26 +7,19 @@ begin_comment
 comment|/*  * ====================================================  * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.  *  * Developed at SunPro, a Sun Microsystems, Inc. business.  * Permission to use, copy, modify, and distribute this  * software is freely granted, provided that this notice  * is preserved.  * ====================================================  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
 
-begin_decl_stmt
-specifier|static
-name|char
-name|rcsid
-index|[]
-init|=
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/*  * double logb(x)  * IEEE 754 logb. Included to pass IEEE test suite. Not recommend.  * Use ilogb instead.  */
@@ -133,7 +126,7 @@ operator|*=
 name|two54
 expr_stmt|;
 comment|/* convert subnormal x to normal */
-name|GET_FLOAT_WORD
+name|GET_HIGH_WORD
 argument_list|(
 name|ix
 argument_list|,
@@ -146,7 +139,7 @@ literal|0x7fffffff
 expr_stmt|;
 return|return
 call|(
-name|float
+name|double
 call|)
 argument_list|(
 operator|(
