0465c1c413c498b57140db555e65d81b402bb42d
Bruce Evans <bde@FreeBSD.org>
2008-02-14 13:44:03 +0000

    Use the expression fabs(x+0.0)+fabs(y+0.0) instad of a+b (where a is |x| or |y| and b is |y| or |x|) when mixing NaN arg(s).
    hypot*() had its own foot shooting for mixing NaNs -- it swaps the
    args so that |x| in bits is largest, but does this before quieting
    signaling NaNs, so on amd64 (where the result of adding NaNs depends
    on the order) it gets inconsistent results if setting the quiet bit
    makes a difference, just like a similar ia64 and i387 hardware comparison.
    The usual fix (see e_powf.c 1.13 for more details) of mixing using
    (a+0.0)+-(b+0.0) doesn't work on amd64 if the args are swapped (since
    the rder makes a difference with SSE). Fortunately, the original args
    are unchanged and don't need to be swapped when we let the hardware
    decide the mixing after quieting them, but we need to take their
    absolute value.
    
    hypotf() doesn't seem to have any real bugs masked by this non-bug.
    On amd64, its maximum error in 2^32 trials on amd64 is now 0.8422 ulps,
    and on i386 the maximum error is unchanged and about the same, except
    with certain CFLAGS it magically drops to 0.5 (perfect rounding).
    
    Convert to __FBSDID().
    
    
    Former-commit-id: b0028c3d59a0ec8325fbcab6e16271faf1f862f7

--- lib/msun/src/e_hypot.c
+++ lib/msun/src/e_hypot.c
@@ -7,26 +7,19 @@ begin_comment
 comment|/*  * ====================================================  * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.  *  * Developed at SunSoft, a Sun Microsystems, Inc. business.  * Permission to use, copy, modify, and distribute this  * software is freely granted, provided that this notice   * is preserved.  * ====================================================  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
 
-begin_decl_stmt
-specifier|static
-name|char
-name|rcsid
-index|[]
-init|=
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
 literal|"$FreeBSD$"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
+argument_list|)
+expr_stmt|;
+end_expr_stmt
 
 begin_comment
 comment|/* __ieee754_hypot(x,y)  *  * Method :                    *	If (assume round-to-nearest) z=x*x+y*y   *	has error less than sqrt(2)/2 ulp, than   *	sqrt(z) has error less than 1 ulp (exercise).  *  *	So, compute sqrt(x*x+y*y) with some care as   *	follows to get the error below 1 ulp:  *  *	Assume x>y>0;  *	(if possible, set rounding to round-to-nearest)  *	1. if x> 2y  use  *		x1*x1+(y*y+(x2*(x+x1))) for x*x+y*y  *	where x1 = x with lower 32 bits cleared, x2 = x-x1; else  *	2. if x<= 2y use  *		t1*y1+((x-y)*(x-y)+(t1*y2+t2*y))  *	where t1 = 2x with lower 32 bits cleared, t2 = 2x-t1,   *	y1= y with lower 32 bits chopped, y2 = y-y1.  *		  *	NOTE: scaling may be necessary if some argument is too   *	      large or too tiny  *  * Special cases:  *	hypot(x,y) is INF if x or y is +INF or -INF; else  *	hypot(x,y) is NAN if x or y is NAN.  *  * Accuracy:  * 	hypot(x,y) returns sqrt(x^2+y^2) with error less   * 	than 1 ulps (units in the last place)   */
@@ -201,13 +194,23 @@ comment|/* Inf or NaN */
 name|u_int32_t
 name|low
 decl_stmt|;
+comment|/* Use original arg order iff result is NaN; quieten sNaNs. */
 name|w
 operator|=
-name|a
+name|fabs
+argument_list|(
+name|x
 operator|+
-name|b
+literal|0.0
+argument_list|)
+operator|+
+name|fabs
+argument_list|(
+name|y
+operator|+
+literal|0.0
+argument_list|)
 expr_stmt|;
-comment|/* for sNaN */
 name|GET_LOW_WORD
 argument_list|(
 name|low
