21785730c9935216bc5dd773a0d399bb1ac35961
Rui Paulo <rpaulo@FreeBSD.org>
2008-02-16 02:00:25 +0000

    Skip validation of the C3 state if we disabled C3 by software (i.e., via quirk).
    Submitted by:	Andriy Gapon <avg at icyb.net.ua>
    Reviewed by:	njl (mentor)
    Approved by:	njl (mentor)
    Requested by:	njl (mentor)
    MFC after:	3 days
    
    
    Former-commit-id: 845216a5119f65c0c6bbac6cc7a70041bd46eea8

--- sys/dev/acpica/acpi_cpu.c
+++ sys/dev/acpica/acpi_cpu.c
@@ -2753,6 +2753,13 @@ operator|.
 name|C3Latency
 operator|<=
 literal|1000
+operator|&&
+operator|!
+operator|(
+name|cpu_quirks
+operator|&
+name|CPU_QUIRK_NO_C3
+operator|)
 condition|)
 block|{
 name|gas
