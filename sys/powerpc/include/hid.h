4bcc949be6d8b5a2443571046d102dccf06112ee
Rafal Jaworowski <raj@FreeBSD.org>
2008-02-25 00:09:23 +0000

    Teach PowerPC CPU identification routines to recognize e500 cores. Fix style issues in this area.
    Approved by:	cognet (mentor)
    MFp4:		e500
    
    Former-commit-id: 72eababd0b2cc0a1e1ba7bc01fd13c074109fdd3

--- sys/powerpc/include/hid.h
+++ sys/powerpc/include/hid.h
@@ -453,6 +453,14 @@ define|\
 value|"\20"								\     "\040EMCP\037b1\036b2\035b3\034b4\033TBEN\032b6\031STEN"		\     "\030HBATEN\027NAP\026SLEEP\025DPM\024b12\023BHTCLR\022XAEN\021NHR"	\     "\020ICE\017DCE\016ILOCK\015DLOCK\014ICFI\013DCFI\012SPD\011XBSEN"	\     "\010SGE\007b25\006BTIC\005LRSTK\004FOLD\003BHT\002NOPDST\001NOPTI"
 end_define
 
+begin_define
+define|#
+directive|define
+name|HID0_E500_BITMASK
+define|\
+value|"\20"								\     "\040EMCP\037b1\036b2\035b3\034b4\033b5\032b6\031b7"		\     "\030DOZE\027NAP\026SLEEP\025b11\024b12\023b13\022b14\021b15"	\     "\020b16\017TBEN\016SEL_TBCLK\015b19\014b20\013b21\012b22\011b23"	\     "\010EN_MAS7_UPDATE\007DCFA\006b26\005b27\004b28\003b29\002b30\001NOPTI"
+end_define
+
 begin_comment
 comment|/*  *  HID0 bit definitions per cpu model  *  * bit	603	604	750	7400	7410   7450    7457  *   0	EMCP	EMCP	EMCP	EMCP	EMCP   -       -  *   1	-	ECP	DBP	-	-      -       -  *   2	EBA	EBA	EBA	EBA	EDA    -       -  *   3	EBD	EBD	EBD	EBD	EBD    -       -  *   4	SBCLK	-	BCLK	BCKL	BCLK   -       -  *   5	EICE	-	-	-	-      TBEN    TBEN  *   6	ECLK	-	ECLK	ECLK	ECLK   -       -  *   7	PAR	PAR	PAR	PAR	PAR    STEN    STEN  *   8	DOZE	-	DOZE	DOZE	DOZE   -       HBATEN  *   9	NAP	-	NAP	NAP	NAP    NAP     NAP  *  10	SLEEP	-	SLEEP	SLEEP	SLEEP  SLEEP   SLEEP  *  11	DPM	-	DPM	DPM	DPM    DPM     DPM  *  12	RISEG	-	-	RISEG	-      -       -  *  13	-	-	-	EIEC	EIEC   BHTCLR  BHTCLR  *  14	-	-	-	-	-      XAEN    XAEN  *  15	-	NHR	NHR	NHR	NHR    NHR     NHR  *  16	ICE	ICE	ICE	ICE	ICE    ICE     ICE  *  17	DCE	DCE	DCE	DCE	DCE    DCE     DCE  *  18	ILOCK	ILOCK	ILOCK	ILOCK	ILOCK  ILOCK   ILOCK  *  19	DLOCK	DLOCK	DLOCK	DLOCK	DLOCK  DLOCK   DLOCK  *  20	ICFI	ICFI	ICFI	ICFI	ICFI   ICFI    ICFI  *  21	DCFI	DCFI	DCFI	DCFI	DCFI   DCFI    DCFI  *  22	-	-	SPD	SPD	SPG    SPD     SPD  *  23	-	-	IFEM	IFTT	IFTT   -       XBSEN  *  24	-	SIE	SGE	SGE	SGE    SGE     SGE  *  25	-	-	DCFA	DCFA	DCFA   -       -  *  26	-	-	BTIC	BTIC	BTIC   BTIC    BTIC  *  27	FBIOB	-	-	-	-      LRSTK   LRSTK  *  28	-	-	ABE	-	-      FOLD    FOLD  *  29	-	BHT	BHT	BHT	BHT    BHT     BHT  *  30	-	-	-	NOPDST	NOPDST NOPDST  NOPDST  *  31	NOOPTI	-	NOOPTI	NOPTI	NOPTI  NOPTI   NOPTI  *  *  604: ECP = Enable cache parity checking  *  604: SIE = Serial instruction execution disable  * 7450: TBEN = Time Base Enable  * 7450: STEN = Software table lookup enable  * 7450: BHTCLR = Branch history clear  * 7450: XAEN = Extended Addressing Enabled  * 7450: LRSTK = Link Register Stack Enable  * 7450: FOLD = Branch folding enable  * 7457: HBATEN = High BAT Enable  * 7457: XBSEN = Extended BAT Block Size Enable  */
 end_comment
