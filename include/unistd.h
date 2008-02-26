8fc2165ebdd2b3c6b52fe858e060a32916df6a62
Ruslan Ermilov <ru@FreeBSD.org>
2008-02-26 20:33:52 +0000

    Added the "restrict" type-qualifier to the readlink() prototype.
    Former-commit-id: 5250d81419cf856c88844737271f4e06405de674

--- include/unistd.h
+++ include/unistd.h
@@ -2738,9 +2738,11 @@ parameter_list|(
 specifier|const
 name|char
 modifier|*
+name|__restrict
 parameter_list|,
 name|char
 modifier|*
+name|__restrict
 parameter_list|,
 name|size_t
 parameter_list|)
