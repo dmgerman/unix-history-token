1ce872b7138c88b4f5b8f2b0ffa2beb35f9fbffb
Pyun YongHyeon <yongari@FreeBSD.org>
2008-02-29 03:38:12 +0000

    Workaround GMAC hardware hang of Yukon II on the receipt of pause frames. This bug seems to happen on certain hardware model/revision (e.g. 88E8053) but it's not identified which hardwares are affected. Revision 1.4 of if_mskreg.h was not enough to workaround the bug. To workaround it, inrease GMAC FIFO threshold by one FIFO word to flush received pause frames.
    Reported by:	das, Kirill Nuzhdin < kirill.nuzhdin AT rad dot chem dot msu dot ru >
    Tested by:	das, Kirill Nuzhdin
    
    
    Former-commit-id: eb0fccaf4023256fc147dc3ea659125332c971f8

--- sys/dev/msk/if_msk.c
+++ sys/dev/msk/if_msk.c
@@ -18822,7 +18822,7 @@ argument_list|,
 name|GMR_FS_ANY_ERR
 argument_list|)
 expr_stmt|;
-comment|/* Set Rx FIFO flush threshold to 64 bytes. */
+comment|/* 	 * Set Rx FIFO flush threshold to 64 bytes + 1 FIFO word 	 * due to hardware hang on receipt of pause frames. 	 */
 name|CSR_WRITE_4
 argument_list|(
 name|sc
@@ -18837,6 +18837,8 @@ name|RX_GMF_FL_THR
 argument_list|)
 argument_list|,
 name|RX_GMF_FL_THR_DEF
+operator|+
+literal|1
 argument_list|)
 expr_stmt|;
 comment|/* Configure Tx MAC FIFO. */
