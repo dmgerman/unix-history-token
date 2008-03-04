1aa5fc5070abb8fe3e1b3e3fde35557415ec0639
David Xu <davidxu@FreeBSD.org>
2008-03-04 03:45:11 +0000

    Include cpuset.h, unbreak compiling.
    Former-commit-id: d4b48d96d83f0039071a3832813b77008a320f47

--- lib/libthr/thread/thr_private.h
+++ lib/libthr/thread/thr_private.h
@@ -46,6 +46,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/param.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/cpuset.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/atomic.h>
 end_include
 
