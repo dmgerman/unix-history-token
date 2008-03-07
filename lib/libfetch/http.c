db7984dfadd5d3ff5726c29ac38d952a83b11197
Remko Lodder <remko@FreeBSD.org>
2008-03-07 15:01:52 +0000

    MFC libfetch from HEAD to RELENG_7
    Approved by:	imp (mentor, implicit), DES
    Glanced at by:	des
    
    
    Former-commit-id: 28b78d2fbda30e8a06f08e00672c0a3146f0b65a

--- lib/libfetch/http.c
+++ lib/libfetch/http.c
@@ -460,6 +460,10 @@ literal|10
 operator|+
 name|tolower
 argument_list|(
+operator|(
+name|unsigned
+name|char
+operator|)
 operator|*
 name|p
 argument_list|)
@@ -1061,8 +1065,12 @@ name|l
 operator|=
 name|len
 expr_stmt|;
-name|bcopy
+name|memcpy
 argument_list|(
+name|buf
+operator|+
+name|pos
+argument_list|,
 name|io
 operator|->
 name|buf
@@ -1070,10 +1078,6 @@ operator|+
 name|io
 operator|->
 name|bufpos
-argument_list|,
-name|buf
-operator|+
-name|pos
 argument_list|,
 name|l
 argument_list|)
@@ -1809,6 +1813,10 @@ name|hdr
 operator|&&
 name|tolower
 argument_list|(
+operator|(
+name|unsigned
+name|char
+operator|)
 operator|*
 name|str
 operator|++
@@ -1816,6 +1824,10 @@ argument_list|)
 operator|==
 name|tolower
 argument_list|(
+operator|(
+name|unsigned
+name|char
+operator|)
 operator|*
 name|hdr
 operator|++
