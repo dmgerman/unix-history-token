add77bf0084c63a78e48efdae55589d7a6de8dd7
Remko Lodder <remko@FreeBSD.org>
2008-03-07 15:23:32 +0000

    MFC rev 1.340 usbdevs MFC rev 1.90 uscanner.c
    Add support for the EPSON CX5400 scanner
    
      PR: usb/120980
      Submitted by: Christophe Etcheverry <cetcheve at gmail dot com>
      MFC after: 3 days
      Approved by: imp (mentor, implicit for trivial changes)
    
    Approved by:	imp (mentor, implicit)
    
    
    Former-commit-id: 4f5b5f2a29639ceac72ac7aa0b10a3a53017ab49

--- sys/dev/usb/uscanner.c
+++ sys/dev/usb/uscanner.c
@@ -1295,6 +1295,16 @@ block|,
 literal|0
 block|}
 block|,
+block|{
+block|{
+name|USB_VENDOR_EPSON
+block|,
+name|USB_PRODUCT_EPSON_CX5400
+block|}
+block|,
+literal|0
+block|}
+block|,
 comment|/* UMAX */
 block|{
 block|{
