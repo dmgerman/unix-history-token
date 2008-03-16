cc67c3e73f42d33f8f6c553487c42565cf2fd9d3
David E. O'Brien <obrien@FreeBSD.org>
2008-03-16 07:20:18 +0000

    MFC: style(9)
    Former-commit-id: 2b44626fee7a6a7e60ddcaa5ec800557cef7493b

--- usr.bin/netstat/if.c
+++ usr.bin/netstat/if.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 1983, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1983, 1988, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_if
@@ -274,7 +274,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*   * Dump pfsync statistics structure.  */
+comment|/*  * Dump pfsync statistics structure.  */
 end_comment
 
 begin_function
@@ -903,6 +903,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnethead
 argument_list|)
+operator|!=
+literal|0
 condition|)
 return|return;
 name|ifnetaddr
@@ -932,6 +934,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnet
 argument_list|)
+operator|!=
+literal|0
 condition|)
 return|return;
 if|if
@@ -1110,6 +1114,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnet
 argument_list|)
+operator|!=
+literal|0
 condition|)
 return|return;
 name|strlcpy
@@ -1145,7 +1151,6 @@ name|interface
 operator|!=
 literal|0
 operator|&&
-operator|(
 name|strcmp
 argument_list|(
 name|name
@@ -1154,7 +1159,6 @@ name|interface
 argument_list|)
 operator|!=
 literal|0
-operator|)
 condition|)
 continue|continue;
 name|cp
@@ -1359,6 +1363,8 @@ argument_list|,
 sizeof|sizeof
 name|ifaddr
 argument_list|)
+operator|!=
+literal|0
 condition|)
 block|{
 name|ifaddraddr
@@ -2207,6 +2213,8 @@ argument_list|,
 sizeof|sizeof
 name|ifma
 argument_list|)
+operator|!=
+literal|0
 condition|)
 break|break;
 name|multiaddr
@@ -2235,6 +2243,8 @@ argument_list|,
 sizeof|sizeof
 name|msa
 argument_list|)
+operator|!=
+literal|0
 condition|)
 break|break;
 if|if
@@ -2578,6 +2588,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnethead
 argument_list|)
+operator|!=
+literal|0
 condition|)
 return|return;
 name|firstifnet
@@ -2677,6 +2689,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnet
 argument_list|)
+operator|!=
+literal|0
 condition|)
 break|break;
 name|strlcpy
@@ -3054,6 +3068,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnet
 argument_list|)
+operator|!=
+literal|0
 condition|)
 block|{
 name|printf
@@ -3381,6 +3397,8 @@ argument_list|,
 sizeof|sizeof
 name|ifnet
 argument_list|)
+operator|!=
+literal|0
 condition|)
 block|{
 name|off
