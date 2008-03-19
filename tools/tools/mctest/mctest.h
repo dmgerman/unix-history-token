90afd06c9b29909dad155d13023e32509ea96901
George V. Neville-Neil <gnn@FreeBSD.org>
2008-03-19 12:44:23 +0000

    A multicast test.  The mctest program acts as both a source and a sink for multicast packets.  Parameters for the interface, packet size, number of packets, and interpacket gap may be given on the command line. The sink records how many packets were missed, and at what time each packet arrived.
    Former-commit-id: 0771de4c524393ee3f539df112e743b47b420e9b

---
+++ tools/tools/mctest/mctest.h
@@ -0,0 +1,143 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Copyright (c) 2008, George V. Neville-Neil
+end_comment
+
+begin_comment
+comment|// All rights reserved.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// Redistribution and use in source and binary forms, with or without
+end_comment
+
+begin_comment
+comment|// modification, are permitted provided that the following conditions
+end_comment
+
+begin_comment
+comment|// are met:
+end_comment
+
+begin_comment
+comment|// 1. Redistributions of source code must retain the above copyright
+end_comment
+
+begin_comment
+comment|//    notice, this list of conditions and the following disclaimer.
+end_comment
+
+begin_comment
+comment|// 2. Redistributions in binary form must reproduce the above copyright
+end_comment
+
+begin_comment
+comment|//    notice, this list of conditions and the following disclaimer in the
+end_comment
+
+begin_comment
+comment|//    documentation and/or other materials provided with the distribution.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+end_comment
+
+begin_comment
+comment|// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+end_comment
+
+begin_comment
+comment|// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+end_comment
+
+begin_comment
+comment|// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+end_comment
+
+begin_comment
+comment|// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+end_comment
+
+begin_comment
+comment|// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+end_comment
+
+begin_comment
+comment|// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+end_comment
+
+begin_comment
+comment|// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+end_comment
+
+begin_comment
+comment|// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+end_comment
+
+begin_comment
+comment|// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+end_comment
+
+begin_comment
+comment|// SUCH DAMAGE.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// $FreeBSD$
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_decl_stmt
+specifier|const
+name|char
+modifier|*
+name|DEFAULT_GROUP
+init|=
+literal|"239.255.255.1"
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|const
+name|int
+name|DEFAULT_PORT
+init|=
+literal|6666
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|const
+name|int
+name|TIMEOUT
+init|=
+literal|10
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
