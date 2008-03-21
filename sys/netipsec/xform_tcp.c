d14682747e030c88acdbbcc031762d98dede55de
Bjoern A. Zeeb <bz@FreeBSD.org>
2008-03-21 18:31:42 +0000

    Add ';' missed with the SYSINIT changes. Not noticed by tb as TCP_SIGNATURE is not in LINT.
    MFC after:	1 month
    
    
    Former-commit-id: 4caf1a1b5aa1be815341c2c0d0f5b7cc45781e68

--- sys/netipsec/xform_tcp.c
+++ sys/netipsec/xform_tcp.c
@@ -489,20 +489,21 @@ expr_stmt|;
 block|}
 end_function
 
-begin_macro
+begin_expr_stmt
 name|SYSINIT
 argument_list|(
-argument|tcpsignature_xform_init
+name|tcpsignature_xform_init
 argument_list|,
-argument|SI_SUB_DRIVERS
+name|SI_SUB_DRIVERS
 argument_list|,
-argument|SI_ORDER_FIRST
+name|SI_ORDER_FIRST
 argument_list|,
-argument|tcpsignature_attach
+name|tcpsignature_attach
 argument_list|,
-argument|NULL
+name|NULL
 argument_list|)
-end_macro
+expr_stmt|;
+end_expr_stmt
 
 end_unit
 
