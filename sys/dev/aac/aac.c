c16350c8ae1ad4fdcc130ac9cc7bb4ba68e03e8c
Ed Maste <emaste@FreeBSD.org>
2008-03-28 18:13:38 +0000

    MFC aac.c r1.133:   Correct data direction flags in aac_bio_command() in the   !AAC_FLAGS_RAW_IO && AAC_FLAGS_SG_64BIT case.
      Submitted by:   Adaptec
    
    
    Former-commit-id: e8c2d9551f9bd019460a134667ecbcb66c1d600b

--- sys/dev/aac/aac.c
+++ sys/dev/aac/aac.c
@@ -5447,7 +5447,7 @@ name|cm
 operator|->
 name|cm_flags
 operator||=
-name|AAC_CMD_DATAOUT
+name|AAC_CMD_DATAIN
 expr_stmt|;
 name|cm
 operator|->
@@ -5550,7 +5550,7 @@ name|cm
 operator|->
 name|cm_flags
 operator||=
-name|AAC_CMD_DATAIN
+name|AAC_CMD_DATAOUT
 expr_stmt|;
 name|cm
 operator|->
