b2df55606f4e9a10ad3159049d6c21c6ebc5d8bb
David Schultz <das@FreeBSD.org>
2008-03-30 20:47:26 +0000

    Implement remquol() based on remquo().
    Former-commit-id: 20dcb4865b7ab16bbdcf7dbc8eb73bd0be7ec69e

--- lib/msun/src/s_remquo.c
+++ lib/msun/src/s_remquo.c
@@ -24,6 +24,12 @@ end_expr_stmt
 begin_include
 include|#
 directive|include
+file|<float.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"math.h"
 end_include
 
@@ -1017,6 +1023,29 @@ name|x
 return|;
 block|}
 end_function
+
+begin_if
+if|#
+directive|if
+name|LDBL_MANT_DIG
+operator|==
+literal|53
+end_if
+
+begin_expr_stmt
+name|__weak_reference
+argument_list|(
+name|remquo
+argument_list|,
+name|remquol
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
