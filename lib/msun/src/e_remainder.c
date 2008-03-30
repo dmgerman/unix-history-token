cdb6698584194103b34be599a668d937d981e66f
David Schultz <das@FreeBSD.org>
2008-03-30 20:47:42 +0000

    Implement remainderl() as a wrapper around remquol(). The extra work remquol() performs to compute the quotient is negligible.
    Former-commit-id: 5cc0bc4e7dda3a8dc09781c033ae7b2cba39ae55

--- lib/msun/src/e_remainder.c
+++ lib/msun/src/e_remainder.c
@@ -28,6 +28,12 @@ end_comment
 begin_include
 include|#
 directive|include
+file|<float.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"math.h"
 end_include
 
@@ -338,6 +344,29 @@ name|x
 return|;
 block|}
 end_function
+
+begin_if
+if|#
+directive|if
+name|LDBL_MANT_DIG
+operator|==
+literal|53
+end_if
+
+begin_expr_stmt
+name|__weak_reference
+argument_list|(
+name|remainder
+argument_list|,
+name|remainderl
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
