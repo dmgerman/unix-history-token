5ea2a96d713e57cb27d462f7fae0fee1dd7c4e2e
Konstantin Belousov <kib@FreeBSD.org>
2008-03-31 11:55:10 +0000

    Add the constant definition needed by the implementation of the openat() and the related syscalls.
    Based on the submission by rdivacky,
    	sponsored by Google Summer of Code 2007
    Reviewed by:	rwatson, rdivacky
    Tested by:	pho
    
    
    Former-commit-id: 4ce12b19094dbebe96b11eeef52fd0de7018c96b

--- sys/sys/fcntl.h
+++ sys/sys/fcntl.h
@@ -350,6 +350,80 @@ directive|endif
 end_endif
 
 begin_comment
+comment|/* Defined by POSIX Extended API Set Part 2 */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|__BSD_VISIBLE
+end_if
+
+begin_comment
+comment|/*  * Magic value that specify the use of the current working directory  * to determine the target of relative file paths in the openat() and  * similar syscalls.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AT_FDCWD
+value|-100
+end_define
+
+begin_comment
+comment|/*  * Miscellaneous flags for the *at() syscalls.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AT_EACCESS
+value|0x100
+end_define
+
+begin_comment
+comment|/* Check access using effective user and group ID */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AT_SYMLINK_NOFOLLOW
+value|0x200
+end_define
+
+begin_comment
+comment|/* Do not follow symbolic links */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AT_SYMLINK_FOLLOW
+value|0x400
+end_define
+
+begin_comment
+comment|/* Follow symbolic link */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AT_REMOVEDIR
+value|0x800
+end_define
+
+begin_comment
+comment|/* Remove directory instead of file */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
 comment|/* Defined by POSIX 1003.1; BSD default, but must be distinct from O_RDONLY. */
 end_comment
 
@@ -379,6 +453,61 @@ define|#
 directive|define
 name|O_DIRECT
 value|0x00010000
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Defined by POSIX Extended API Set Part 2 */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|__BSD_VISIBLE
+end_if
+
+begin_define
+define|#
+directive|define
+name|O_DIRECTORY
+value|0x00020000
+end_define
+
+begin_comment
+comment|/* Fail if not directory */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|O_EXEC
+value|0x00040000
+end_define
+
+begin_comment
+comment|/* Open for execute only */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|_KERNEL
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|FEXEC
+value|O_EXEC
 end_define
 
 begin_endif
@@ -428,7 +557,7 @@ begin_define
 define|#
 directive|define
 name|FMASK
-value|(FREAD|FWRITE|FAPPEND|FASYNC|FFSYNC|FNONBLOCK|O_DIRECT)
+value|(FREAD|FWRITE|FAPPEND|FASYNC|FFSYNC|FNONBLOCK|O_DIRECT|FEXEC)
 end_define
 
 begin_comment
