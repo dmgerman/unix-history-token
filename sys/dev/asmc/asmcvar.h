31782201c717e84ec7963a413aa5590ccb4ddd9d
Rui Paulo <rpaulo@FreeBSD.org>
2008-04-07 11:22:12 +0000

    The SMC is represented on the acpi tables, so we can completely remove dependency on isa. We are now an acpi child.
    Also:
    	* Add compile time debugging activation
    	* Increase the delay for the SMS init flag.
    
    Former-commit-id: bfe7aff02a9ed2fbecb57c1e78bae8e4dbdd680c

--- sys/dev/asmc/asmcvar.h
+++ sys/dev/asmc/asmcvar.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2007 Rui Paulo<rpaulo@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  *  */
+comment|/*-  * Copyright (c) 2007, 2008 Rui Paulo<rpaulo@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  *  */
 end_comment
 
 begin_define
@@ -64,12 +64,20 @@ modifier|*
 name|sc_model
 decl_stmt|;
 name|int
-name|sc_rid
+name|sc_rid_port
+decl_stmt|;
+name|int
+name|sc_rid_irq
 decl_stmt|;
 name|struct
 name|resource
 modifier|*
-name|sc_res
+name|sc_ioport
+decl_stmt|;
+name|struct
+name|resource
+modifier|*
+name|sc_irq
 decl_stmt|;
 name|void
 modifier|*
@@ -92,25 +100,30 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  * The Sudden Motion Sensor is able to generate an interrupt when  * there are certain critical conditions (free fall, high acceleration and  * shocks).  * The following IRQ is used.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|ASMC_IRQ
-value|6
-end_define
-
-begin_comment
 comment|/*  * Data port.  */
 end_comment
 
 begin_define
 define|#
 directive|define
-name|ASMC_DATAPORT
-value|0x300
+name|ASMC_DATAPORT_READ
+parameter_list|(
+name|sc
+parameter_list|)
+value|bus_read_1(sc->sc_ioport, 0x00)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ASMC_DATAPORT_WRITE
+parameter_list|(
+name|sc
+parameter_list|,
+name|val
+parameter_list|)
+define|\
+value|bus_write_1(sc->sc_ioport, 0, val)
 end_define
 
 begin_define
@@ -127,8 +140,24 @@ end_comment
 begin_define
 define|#
 directive|define
-name|ASMC_CMDPORT
-value|0x304
+name|ASMC_CMDPORT_READ
+parameter_list|(
+name|sc
+parameter_list|)
+value|bus_read_1(sc->sc_ioport, 0x04)
+end_define
+
+begin_define
+define|#
+directive|define
+name|ASMC_CMDPORT_WRITE
+parameter_list|(
+name|sc
+parameter_list|,
+name|val
+parameter_list|)
+define|\
+value|bus_write_1(sc->sc_ioport, 4, val)
 end_define
 
 begin_define
@@ -152,8 +181,11 @@ end_comment
 begin_define
 define|#
 directive|define
-name|ASMC_INTPORT
-value|0x31f
+name|ASMC_INTPORT_READ
+parameter_list|(
+name|sc
+parameter_list|)
+value|bus_read_1(sc->sc_ioport, 0x1f)
 end_define
 
 begin_comment
