cb936db416ec0bd29a2bc4cb6958a0354fe29163
Robert N. M. Watson <rwatson@FreeBSD.org>
2008-04-13 21:45:52 +0000

    Make naming of include guards for MAC Framework include files more consistent with other kernel include guards (don't start with _SYS).
    MFC after:	3 days
    
    
    Former-commit-id: 452bf45c31308803e29049b65a55d7c6a599893b

--- sys/security/mac/mac_framework.h
+++ sys/security/mac/mac_framework.h
@@ -10,13 +10,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_SYS_SECURITY_MAC_MAC_FRAMEWORK_H_
+name|_SECURITY_MAC_MAC_FRAMEWORK_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_SYS_SECURITY_MAC_MAC_FRAMEWORK_H_
+name|_SECURITY_MAC_MAC_FRAMEWORK_H_
 end_define
 
 begin_ifndef
@@ -4101,7 +4101,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_SYS_SECURITY_MAC_MAC_FRAMEWORK_H_ */
+comment|/* !_SECURITY_MAC_MAC_FRAMEWORK_H_ */
 end_comment
 
 end_unit
