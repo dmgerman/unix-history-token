31328c1e3fcb9704b292b2681372c4215dd54cdd
Maksim Yevmenkin <emax@FreeBSD.org>
2008-04-18 00:55:03 +0000

    MFC:
    Fix possible buffer overrun on 64-bit arch when generating MAC
    address for tap interface.
    
    Reported by:	Marc Lorner < marc dot loerner at hob dot de >
    Reviewed by:	bms
    MFC after:	3 days
    
    
    Former-commit-id: dd09281471dba30356f4ee76806cb6a1192e24ba

--- sys/net/if_tap.c
+++ sys/net/if_tap.c
@@ -1893,6 +1893,9 @@ name|unsigned
 name|short
 name|macaddr_hi
 decl_stmt|;
+name|uint32_t
+name|macaddr_mid
+decl_stmt|;
 name|int
 name|unit
 decl_stmt|,
@@ -2032,6 +2035,13 @@ argument_list|(
 literal|0x00bd
 argument_list|)
 expr_stmt|;
+name|macaddr_mid
+operator|=
+operator|(
+name|uint32_t
+operator|)
+name|ticks
+expr_stmt|;
 name|bcopy
 argument_list|(
 operator|&
@@ -2048,7 +2058,7 @@ expr_stmt|;
 name|bcopy
 argument_list|(
 operator|&
-name|ticks
+name|macaddr_mid
 argument_list|,
 operator|&
 name|eaddr
@@ -2058,7 +2068,7 @@ index|]
 argument_list|,
 sizeof|sizeof
 argument_list|(
-name|long
+name|uint32_t
 argument_list|)
 argument_list|)
 expr_stmt|;
