920e80d18c536cb373ebc78ed107c8d24141ca81
Antoine Brodin <antoine@FreeBSD.org>
2008-04-20 15:56:59 +0000

    Record the dependency of the iscsi initiator on cam.
    PR:		116745
    Submitted by:	OISHI Masakuni
    Approved by:	rwatson (mentor)
    MFC after:	1 month
    
    Former-commit-id: 229ae933fef8224f6def28e71dfcda8d6d00a848

--- sys/dev/iscsi/initiator/iscsi.c
+++ sys/dev/iscsi/initiator/iscsi.c
@@ -3956,5 +3956,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|iscsi
+argument_list|,
+name|cam
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
