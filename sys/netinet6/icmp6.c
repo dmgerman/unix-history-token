79e1b41b7a3b9342d9795116fc05bae4829bb76c
Robert N. M. Watson <rwatson@FreeBSD.org>
2008-04-21 12:08:40 +0000

    In ICMPv6, read lock rather than write lock the inpcb on receive.
    MFC after:	3 months
    
    
    Former-commit-id: c62d3ee12563fb6c87fb6c03a154f3c923b9917e

--- sys/netinet6/icmp6.c
+++ sys/netinet6/icmp6.c
@@ -8009,7 +8009,7 @@ argument_list|,
 argument|inp_list
 argument_list|)
 block|{
-name|INP_WLOCK
+name|INP_RLOCK
 argument_list|(
 name|in6p
 argument_list|)
@@ -8029,7 +8029,7 @@ condition|)
 block|{
 name|docontinue
 label|:
-name|INP_WUNLOCK
+name|INP_RUNLOCK
 argument_list|(
 name|in6p
 argument_list|)
@@ -8356,7 +8356,7 @@ operator|=
 name|NULL
 expr_stmt|;
 block|}
-name|INP_WUNLOCK
+name|INP_RUNLOCK
 argument_list|(
 name|last
 argument_list|)
@@ -8574,7 +8574,7 @@ operator|->
 name|in6p_socket
 argument_list|)
 expr_stmt|;
-name|INP_WUNLOCK
+name|INP_RUNLOCK
 argument_list|(
 name|last
 argument_list|)
