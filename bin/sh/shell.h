e85d5f24bb732717dc894492e4a1c0d8ea1f14b2
Stefan Farfeleder <stefanf@FreeBSD.org>
2008-04-27 20:43:26 +0000

    Revert, this was supposed to go into HEAD.
    Former-commit-id: 3d0944cebda141da5c6527c42d79bf1a4103b419

--- bin/sh/shell.h
+++ bin/sh/shell.h
@@ -3,24 +3,6 @@ begin_comment
 comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)shell.h	8.2 (Berkeley) 5/4/95  * $FreeBSD$  */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|SHELL_H_
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|SHELL_H_
-end_define
-
-begin_include
-include|#
-directive|include
-file|<inttypes.h>
-end_include
-
 begin_comment
 comment|/*  * The follow should be set to reflect the type of system you have:  *	JOBS -> 1 if you have Berkeley job control, 0 otherwise.  *	define DEBUG=1 to compile in debugging (set global "debug" to turn on)  *	define DEBUG=2 to compile in and turn on debugging.  *  * When debugging is on, debugging info will be written to $HOME/trace and  * a quit signal will generate a core dump.  */
 end_comment
@@ -42,7 +24,7 @@ end_comment
 
 begin_typedef
 typedef|typedef
-name|intmax_t
+name|long
 name|arith_t
 typedef|;
 end_typedef
@@ -51,7 +33,7 @@ begin_define
 define|#
 directive|define
 name|ARITH_FORMAT_STR
-value|"%" PRIdMAX
+value|"%ld"
 end_define
 
 begin_define
@@ -61,7 +43,7 @@ name|atoarith_t
 parameter_list|(
 name|arg
 parameter_list|)
-value|strtoimax(arg, NULL, 0)
+value|strtol(arg, NULL, 0)
 end_define
 
 begin_define
@@ -75,7 +57,7 @@ name|endptr
 parameter_list|,
 name|base
 parameter_list|)
-value|strtoimax(nptr, endptr, base)
+value|strtol(nptr, endptr, base)
 end_define
 
 begin_typedef
@@ -157,15 +139,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* !SHELL_H_ */
-end_comment
 
 end_unit
 
