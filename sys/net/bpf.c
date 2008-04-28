98d06e951ca4f6ad8a261f4d045506b26430ee15
Jung-uk Kim <jkim@FreeBSD.org>
2008-04-28 19:42:11 +0000

    Check packet directions more properly instead of just checking received interface is null.
    PR:		kern/123138
    Submitted by:	Dmitry (hanabana at mail dot ru)
    MFC after:	1 week
    
    Former-commit-id: 26fdb1acb2f2671e6589925edc9f55de348ba425

--- sys/net/bpf.c
+++ sys/net/bpf.c
@@ -6101,10 +6101,12 @@ name|BPF_CHECK_DIRECTION
 parameter_list|(
 name|d
 parameter_list|,
+name|r
+parameter_list|,
 name|i
 parameter_list|)
 define|\
-value|(((d)->bd_direction == BPF_D_IN&& (i) == NULL) ||	\ 	    ((d)->bd_direction == BPF_D_OUT&& (i) != NULL))
+value|(((d)->bd_direction == BPF_D_IN&& (r) != (i)) ||	\ 	    ((d)->bd_direction == BPF_D_OUT&& (r) == (i)))
 end_define
 
 begin_comment
@@ -6212,6 +6214,10 @@ operator|->
 name|m_pkthdr
 operator|.
 name|rcvif
+argument_list|,
+name|bp
+operator|->
+name|bif_ifp
 argument_list|)
 condition|)
 continue|continue;
@@ -6516,6 +6522,10 @@ operator|->
 name|m_pkthdr
 operator|.
 name|rcvif
+argument_list|,
+name|bp
+operator|->
+name|bif_ifp
 argument_list|)
 condition|)
 continue|continue;
