37c423f733aa324572434de1b447561497a653bf
Marius Strobl <marius@FreeBSD.org>
2008-05-02 17:44:18 +0000

    Remove an header which is unused for sun4v.
    MFC after:	3 days
    
    
    Former-commit-id: 4fed4a0179f7d08aff7b0b006dedd8fd67d788a3

--- sys/sun4v/sun4v/nexus.c
+++ sys/sun4v/sun4v/nexus.c
@@ -80,12 +80,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/bus_common.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/intr_machdep.h>
 end_include
 
