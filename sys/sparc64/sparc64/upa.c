d7c34f3c64bd6917f6c8f5f59add6cb62b28a4ba
Marius Strobl <marius@FreeBSD.org>
2008-05-08 20:57:08 +0000

    Use <machine/intr_machdep.h> directly instead of depending on header pollution in the otherwise unused <sys/pcpu.h>.
    Former-commit-id: a03f669203ce076564a6278eb6cfff2ee1e95c08

--- sys/sparc64/sparc64/upa.c
+++ sys/sparc64/sparc64/upa.c
@@ -50,12 +50,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/pcpu.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/resource.h>
 end_include
 
@@ -93,6 +87,12 @@ begin_include
 include|#
 directive|include
 file|<machine/bus_common.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<machine/intr_machdep.h>
 end_include
 
 begin_include
