4272d470ff596bf4b819da884821b212e51475e4
Antoine Brodin <antoine@FreeBSD.org>
2008-05-10 18:50:45 +0000

    Document freebsd extensions to netcat a bit better: - sort the options - document -o and -O everywhere
    Reviewed by:	delphij
    Approved by:	rwatson (mentor)
    
    
    Former-commit-id: 6d9ccda295fa64f9f216b729571b111aa5668d20

--- contrib/netcat/netcat.c
+++ contrib/netcat/netcat.c
@@ -4319,7 +4319,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\tCommand Summary:\n\ 	\t-4		Use IPv4\n\ 	\t-6            Use IPv6\n"
+literal|"\tCommand Summary:\n\ 	\t-4		Use IPv4\n\ 	\t-6		Use IPv6\n\ 	\t-D		Enable the debug socket option\n\ 	\t-d		Detach from stdin\n"
 argument_list|)
 expr_stmt|;
 ifdef|#
@@ -4329,7 +4329,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\ 	\t-e policy     Use specified IPsec policy\n\ 	\t-E            Use IPsec ESP\n"
+literal|"\ 	\t-E		Use IPsec ESP\n\ 	\t-e policy	Use specified IPsec policy\n"
 argument_list|)
 expr_stmt|;
 endif|#
@@ -4338,7 +4338,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\ 	\t-D		Enable the debug socket option\n\ 	\t-d		Detach from stdin\n\ 	\t-h		This help text\n\ 	\t-i secs\t	Delay interval for lines sent, ports scanned\n\ 	\t-k		Keep inbound sockets open for multiple connects\n\ 	\t-l		Listen mode, for inbound connects\n\ 	\t-n		Suppress name/port resolutions\n\ 	\t-O		Disable TCP options\n\ 	\t-P proxyuser\tUsername for proxy authentication\n\ 	\t-p port\t	Specify local port for remote connects\n\ 	\t-r		Randomize remote ports\n\ 	\t-S		Enable the TCP MD5 signature option\n\ 	\t-s addr\t	Local source address\n\ 	\t-T ToS\t	Set IP Type of Service\n\ 	\t-t		Answer TELNET negotiation\n\ 	\t-U		Use UNIX domain socket\n\ 	\t-u		UDP mode\n\ 	\t-v		Verbose\n\ 	\t-w secs\t	Timeout for connects and final net reads\n\ 	\t-X proto	Proxy protocol: \"4\", \"5\" (SOCKS) or \"connect\"\n\ 	\t-x addr[:port]\tSpecify proxy address and port\n\ 	\t-z		Zero-I/O mode [used for scanning]\n\ 	Port numbers can be individual or ranges: lo-hi [inclusive]\n"
+literal|"\ 	\t-h		This help text\n\ 	\t-i secs\t	Delay interval for lines sent, ports scanned\n\ 	\t-k		Keep inbound sockets open for multiple connects\n\ 	\t-l		Listen mode, for inbound connects\n\ 	\t-n		Suppress name/port resolutions\n\ 	\t-O		Disable TCP options\n\ 	\t-o		Terminate on EOF on input\n\ 	\t-P proxyuser\tUsername for proxy authentication\n\ 	\t-p port\t	Specify local port for remote connects\n\ 	\t-r		Randomize remote ports\n\ 	\t-S		Enable the TCP MD5 signature option\n\ 	\t-s addr\t	Local source address\n\ 	\t-T ToS\t	Set IP Type of Service\n\ 	\t-t		Answer TELNET negotiation\n\ 	\t-U		Use UNIX domain socket\n\ 	\t-u		UDP mode\n\ 	\t-v		Verbose\n\ 	\t-w secs\t	Timeout for connects and final net reads\n\ 	\t-X proto	Proxy protocol: \"4\", \"5\" (SOCKS) or \"connect\"\n\ 	\t-x addr[:port]\tSpecify proxy address and port\n\ 	\t-z		Zero-I/O mode [used for scanning]\n\ 	Port numbers can be individual or ranges: lo-hi [inclusive]\n"
 argument_list|)
 expr_stmt|;
 ifdef|#
@@ -4493,7 +4493,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: nc [-46DEdhklnorStUuvz] [-e policy] [-i interval] [-P proxy_username] [-p source_port]\n"
+literal|"usage: nc [-46DdEhklnOorStUuvz] [-e policy] [-i interval] [-P proxy_username] [-p source_port]\n"
 argument_list|)
 expr_stmt|;
 else|#
@@ -4502,7 +4502,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: nc [-46DdhklnorStUuvz] [-i interval] [-P proxy_username] [-p source_port]\n"
+literal|"usage: nc [-46DdhklnOorStUuvz] [-i interval] [-P proxy_username] [-p source_port]\n"
 argument_list|)
 expr_stmt|;
 endif|#
