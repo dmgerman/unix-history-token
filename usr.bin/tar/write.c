812c37444c3c3b0fc0ac7832a15202b441461569
Colin Percival <cperciva@FreeBSD.org>
2008-05-19 18:09:26 +0000

    There's no way for entry to possibly be NULL at the end of write_entry (in fact, there has never been any way for it to be NULL, going all the way back to revision 1.1 of this file), so remove the check and unconditionally free entry.
    Found by:	Coverity Prevent
    
    
    Former-commit-id: 6fa1c74d1d7b120a13b69770aed6e551d1844c7c

--- usr.bin/tar/write.c
+++ usr.bin/tar/write.c
@@ -4232,12 +4232,6 @@ argument_list|(
 name|fd
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|entry
-operator|!=
-name|NULL
-condition|)
 name|archive_entry_free
 argument_list|(
 name|entry
