435c7e9c9d8836f6d7913f852e0de13acb39fa12
Warner Losh <imp@FreeBSD.org>
2008-06-06 17:27:19 +0000

    Tweak a comment and a constant to restore old 30ms upper bound. cs_readreg takes ~2us not ~1us to run.
    Former-commit-id: 5e862328afb1adc786acc95f5c6f59b34e3ef765

--- sys/dev/cs/if_cs.c
+++ sys/dev/cs/if_cs.c
@@ -929,7 +929,7 @@ block|{
 name|int
 name|i
 decl_stmt|;
-comment|/* 	 * From the CS8900A datasheet, section 3.5.2: 	 * "Before issuing any command to the EEPROM, the host must wait 	 * for the SIBUSY bit (Register 16, SelfST, bit 8) to clear.  After 	 * each command has been issued, the host must wait again for SIBUSY 	 * to clear." 	 * 	 * Before we issue the command, we should be !busy, so that will 	 * be fast.  The datasheet suggests that clock out from the part 	 * per word will be on the order of 25us, which is consistant with 	 * the 1MHz serial clock and 16bits...  We should never hit 100, 	 * let alone 30,000 here.  The original code did an unconditional 	 * 30ms DELAY here.  Bad Kharma. 	 */
+comment|/* 	 * From the CS8900A datasheet, section 3.5.2: 	 * "Before issuing any command to the EEPROM, the host must wait 	 * for the SIBUSY bit (Register 16, SelfST, bit 8) to clear.  After 	 * each command has been issued, the host must wait again for SIBUSY 	 * to clear." 	 * 	 * Before we issue the command, we should be !busy, so that will 	 * be fast.  The datasheet suggests that clock out from the part 	 * per word will be on the order of 25us, which is consistant with 	 * the 1MHz serial clock and 16bits...  We should never hit 100, 	 * let alone 15,000 here.  The original code did an unconditional 	 * 30ms DELAY here.  Bad Kharma.  cs_readreg takes ~2us. 	 */
 for|for
 control|(
 name|i
@@ -938,7 +938,7 @@ literal|0
 init|;
 name|i
 operator|<
-literal|30000
+literal|15000
 condition|;
 name|i
 operator|++
