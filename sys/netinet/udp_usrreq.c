0219cc1fafc5379a05c7a78e1eee6d4381359552
Robert N. M. Watson <rwatson@FreeBSD.org>
2008-07-10 16:20:18 +0000

    Slightly rearrange validation of UDP arguments and jail processing in udp_output() so that argument validation occurs before jail processing.
    Add additional comments explaining what's going on when we process
    addresses and binding during udp_output().
    
    MFC after:	3 weeks
    
    
    Former-commit-id: cc5bb256dcb4b18f1170d3be6e5aca5f75622db2

--- sys/netinet/udp_usrreq.c
+++ sys/netinet/udp_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1988, 1990, 1993, 1995  *	The Regents of the University of California.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)udp_usrreq.c	8.6 (Berkeley) 5/23/95  */
+comment|/*-  * Copyright (c) 1982, 1986, 1988, 1990, 1993, 1995  *	The Regents of the University of California.  * Copyright (c) 2008 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)udp_usrreq.c	8.6 (Berkeley) 5/23/95  */
 end_comment
 
 begin_include
@@ -4000,6 +4000,7 @@ goto|goto
 name|release
 goto|;
 block|}
+comment|/* 	 * If a UDP socket has been connected, then a local address/port will 	 * have been selected and bound. 	 * 	 * If a UDP socket has not been connect to, then an explicit 	 * destination address must be used, in which case a local 	 * address/port may not have been selected and bound. 	 */
 if|if
 condition|(
 name|addr
@@ -4016,6 +4017,26 @@ argument_list|(
 name|inp
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|inp
+operator|->
+name|inp_faddr
+operator|.
+name|s_addr
+operator|!=
+name|INADDR_ANY
+condition|)
+block|{
+name|error
+operator|=
+name|EISCONN
+expr_stmt|;
+goto|goto
+name|release
+goto|;
+block|}
+comment|/* 		 * Jail may rewrite the destination address, so let it do 		 * that before we use it. 		 */
 name|sin
 operator|=
 operator|(
@@ -4050,25 +4071,7 @@ operator|.
 name|s_addr
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|inp
-operator|->
-name|inp_faddr
-operator|.
-name|s_addr
-operator|!=
-name|INADDR_ANY
-condition|)
-block|{
-name|error
-operator|=
-name|EISCONN
-expr_stmt|;
-goto|goto
-name|release
-goto|;
-block|}
+comment|/* 		 * If a local address or port hasn't yet been selected, do 		 * that now.  Once a port is selected, we commit the binding 		 * back to the socket; we also commit the binding of the 		 * address if in jail. 		 */
 name|error
 operator|=
 name|in_pcbconnect_setup
