6555f3f9aaaf9d28139b140159f13add21b2dcc1
Jack F Vogel <jfv@FreeBSD.org>
2008-07-31 02:22:53 +0000

    Data type fix
    Former-commit-id: 39d5cb98f85a572ccdbc126d997783d2eadf0fd6

--- sys/dev/e1000/if_igb.c
+++ sys/dev/e1000/if_igb.c
@@ -21211,8 +21211,12 @@ name|device_printf
 argument_list|(
 name|dev
 argument_list|,
-literal|"no descriptors avail event = %lu\n"
+literal|"no descriptors avail event = %lld\n"
 argument_list|,
+operator|(
+name|long
+name|long
+operator|)
 name|txr
 operator|->
 name|no_desc_avail
