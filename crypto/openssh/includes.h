a4bb6f9020f17692b64274b891450796fde0707e
Dag-Erling Smorgrav <des@FreeBSD.org>
2008-08-01 01:32:56 +0000

    More files which no longer have any local changes.
    Former-commit-id: 30c77519e9717649b2c77b8a8d3216bb8edebb11

--- crypto/openssh/includes.h
+++ crypto/openssh/includes.h
@@ -4,10 +4,6 @@ comment|/* $OpenBSD: includes.h,v 1.54 2006/07/22 20:48:23 stevesk Exp $ */
 end_comment
 
 begin_comment
-comment|/* $FreeBSD$	*/
-end_comment
-
-begin_comment
 comment|/*  * Author: Tatu Ylonen<ylo@cs.hut.fi>  * Copyright (c) 1995 Tatu Ylonen<ylo@cs.hut.fi>, Espoo, Finland  *                    All rights reserved  * This file includes most of the needed system headers.  *  * As far as I am concerned, the code I have written for this software  * can be used freely for any purpose.  Any derived versions of this  * software must be clearly marked as such, and if the derived work is  * incompatible with the protocol description in the RFC file, it must be  * called by a name other than "ssh" or "Secure Shell".  */
 end_comment
 
