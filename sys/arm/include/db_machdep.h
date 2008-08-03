91e847a577e40885d293a2156085bf01a2dc74f6
Olivier Houchard <cognet@FreeBSD.org>
2008-08-03 01:51:30 +0000

    Add blx as a branch instruction.
    MFC after:	3 days
    
    
    Former-commit-id: b407e1057140f8a45837f9cd44ae1f63c616a992

--- sys/arm/include/db_machdep.h
+++ sys/arm/include/db_machdep.h
@@ -185,8 +185,12 @@ name|inst_branch
 parameter_list|(
 name|ins
 parameter_list|)
-value|(((ins)& 0x0f000000) == 0x0a000000 || \ 				 ((ins)& 0x0fdffff0) == 0x079ff100 || \ 				 ((ins)& 0x0ff0f000) == 0x0590f000)
+value|(((ins)& 0x0f000000) == 0x0a000000 || \ 				 ((ins)& 0x0fdffff0) == 0x079ff100 || \ 				 ((ins)& 0x0ff0f000) == 0x0590f000 || \ 				 ((ins)& 0x0ffffff0) == 0x012fff30)
 end_define
+
+begin_comment
+comment|/* blx */
+end_comment
 
 begin_define
 define|#
