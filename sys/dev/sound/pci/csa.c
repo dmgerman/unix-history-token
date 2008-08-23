2dc479639831317de7c7a1921a71d8fb1a913559
Warner Losh <imp@FreeBSD.org>
2008-08-23 18:27:10 +0000

    Make sure that we handle errors in device_get_children correctly.
    Former-commit-id: abfbe0cd32308c7a7fccf42b1a327ae71f0c071a

--- sys/dev/sound/pci/csa.c
+++ sys/dev/sound/pci/csa.c
@@ -522,6 +522,8 @@ name|pci_childcount
 operator|=
 literal|0
 expr_stmt|;
+if|if
+condition|(
 name|device_get_children
 argument_list|(
 operator|*
@@ -533,7 +535,8 @@ argument_list|,
 operator|&
 name|pci_childcount
 argument_list|)
-expr_stmt|;
+condition|)
+continue|continue;
 for|for
 control|(
 name|j
