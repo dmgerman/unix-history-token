51a518c958b5f4d0ea49166706808cfa288ef662
Marius Strobl <marius@FreeBSD.org>
2008-08-23 15:20:33 +0000

    Provide and consume module dependency information.
    MFC after:	3 days
    
    
    Former-commit-id: f447f1b10e9afe655959046f94a915e3c6ea7add

--- sys/sparc64/sbus/sbus.c
+++ sys/sparc64/sbus/sbus.c
@@ -782,6 +782,16 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|sbus
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_define
 define|#
 directive|define
