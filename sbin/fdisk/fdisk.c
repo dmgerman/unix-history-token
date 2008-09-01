508b4c75522f54d842bece1cc4c9bba48d0ecb63
David E. O'Brien <obrien@FreeBSD.org>
2008-09-01 23:54:57 +0000

    MFC: r181036: Add a -q[uiet] flag for scripts.
    Former-commit-id: bfa33fba3a98e2c4b5c6edced3cd3f1e8f231e91

--- sbin/fdisk/fdisk.c
+++ sbin/fdisk/fdisk.c
@@ -485,6 +485,19 @@ end_comment
 begin_decl_stmt
 specifier|static
 name|int
+name|q_flag
+init|=
+literal|0
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* Be quiet */
+end_comment
+
+begin_decl_stmt
+specifier|static
+name|int
 name|u_flag
 init|=
 literal|0
@@ -1421,7 +1434,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"BIab:f:ipstuv1234"
+literal|"BIab:f:ipqstuv1234"
 argument_list|)
 operator|)
 operator|!=
@@ -1485,6 +1498,14 @@ case|case
 literal|'p'
 case|:
 name|print_config_flag
+operator|=
+literal|1
+expr_stmt|;
+break|break;
+case|case
+literal|'q'
+case|:
+name|q_flag
 operator|=
 literal|1
 expr_stmt|;
@@ -2311,7 +2332,7 @@ name|stderr
 argument_list|,
 literal|"%s%s"
 argument_list|,
-literal|"usage: fdisk [-BIaipstu] [-b bootcode] [-1234] [disk]\n"
+literal|"usage: fdisk [-BIaipqstu] [-b bootcode] [-1234] [disk]\n"
 argument_list|,
 literal|"       fdisk -f configfile [-itv] [disk]\n"
 argument_list|)
@@ -4261,6 +4282,12 @@ literal|0
 operator|)
 return|;
 block|}
+if|if
+condition|(
+operator|!
+name|q_flag
+condition|)
+comment|/* GEOM errors are benign, not all devices supported */
 name|warnx
 argument_list|(
 literal|"%s"
