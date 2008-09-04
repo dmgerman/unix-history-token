d81ff91f79e303076c07c3585828f086ac4a0aac
Kevin Lo <kevlo@FreeBSD.org>
2008-09-04 10:44:33 +0000

    If the process id specified is invalid, the system call returns ESRCH
    Former-commit-id: 1d5a5cfe1eaaffd39d2519867e4a451b9fd28323

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -5362,7 +5362,9 @@ operator|==
 name|NULL
 condition|)
 return|return
-name|EAGAIN
+operator|(
+name|ESRCH
+operator|)
 return|;
 if|if
 condition|(
@@ -5377,7 +5379,9 @@ name|np
 argument_list|)
 expr_stmt|;
 return|return
-name|EAGAIN
+operator|(
+name|ESRCH
+operator|)
 return|;
 block|}
 name|PROC_UNLOCK
