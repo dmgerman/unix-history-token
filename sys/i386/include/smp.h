90250385136811db4fa7df23a0cb08b9b30f22e6
Kip Macy <kmacy@FreeBSD.org>
2008-09-25 07:09:50 +0000

    add  ipi mapping
    MFC after:	1 month
    
    
    Former-commit-id: 73f3fa1c6b6df3232fa95fc8921d4a53220fa1c3

--- sys/i386/include/smp.h
+++ sys/i386/include/smp.h
@@ -483,6 +483,26 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|XEN
+end_ifdef
+
+begin_function_decl
+name|void
+name|ipi_to_irq_init
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_endif
 endif|#
 directive|endif
