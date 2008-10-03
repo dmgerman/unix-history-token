02d4c5831700217dde16a47aa2621f68d58aef3e
Robert N. M. Watson <rwatson@FreeBSD.org>
2008-10-03 09:01:55 +0000

    Remove or update several stale comments.
    A bit of whitespace/style cleanup.
    
    Update copyright.
    
    MFC after:	3 days (applicable changes)
    
    
    Former-commit-id: cd3fc5b6a06594297e73b211d170ce6b5b618582

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  * Copyright (c) 2004-2007 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	From: @(#)uipc_usrreq.c	8.3 (Berkeley) 1/4/94  */
+comment|/*-  * Copyright (c) 1982, 1986, 1989, 1991, 1993  *	The Regents of the University of California.  * Copyright (c) 2004-2008 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	From: @(#)uipc_usrreq.c	8.3 (Berkeley) 1/4/94  */
 end_comment
 
 begin_comment
@@ -2567,7 +2567,7 @@ name|unp2
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * We hold the global lock, so it's OK to acquire multiple pcb locks 	 * at a time. 	 */
+comment|/* 	 * We hold the global lock exclusively, so it's OK to acquire 	 * multiple pcb locks at a time. 	 */
 while|while
 condition|(
 operator|!
@@ -3359,10 +3359,6 @@ return|;
 block|}
 end_function
 
-begin_comment
-comment|/* pru_rcvoob is EOPNOTSUPP */
-end_comment
-
 begin_function
 specifier|static
 name|int
@@ -3727,7 +3723,6 @@ block|}
 case|case
 name|SOCK_STREAM
 case|:
-comment|/* 		 * Connect if not connected yet. 		 * 		 * Note: A better implementation would complain if not equal 		 * to the peer's address. 		 */
 if|if
 condition|(
 operator|(
@@ -4021,7 +4016,7 @@ literal|"uipc_send unknown socktype"
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * SEND_EOF is equivalent to a SEND followed by a SHUTDOWN. 	 */
+comment|/* 	 * PRUS_EOF is equivalent to pru_send followed by pru_shutdown. 	 */
 if|if
 condition|(
 name|flags
@@ -5600,7 +5595,7 @@ if|if
 condition|(
 name|vfslocked
 condition|)
-comment|/*  		 * Giant has been previously acquired. This means filesystem 		 * isn't MPSAFE. Do it once again. 		 */
+comment|/*  		 * Giant has been previously acquired. This means filesystem 		 * isn't MPSAFE.  Do it once again. 		 */
 name|mtx_lock
 argument_list|(
 operator|&
