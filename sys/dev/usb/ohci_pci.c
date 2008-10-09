f658d0f3fee9c5a58d67b6338519f604285ae180
Nick Hibma <n_hibma@FreeBSD.org>
2008-10-09 19:22:00 +0000

    Add modules for the HCI part of USB. This is convenient when having a UHCI controller in your laptop but inserting a (OHCI-based) Option Cardbus card.
    Former-commit-id: 5cc9b2d2eaee368fe1171fdcc1d88b3fd601a7e0

--- sys/dev/usb/ohci_pci.c
+++ sys/dev/usb/ohci_pci.c
@@ -1776,5 +1776,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|ohci
+argument_list|,
+name|usb
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
