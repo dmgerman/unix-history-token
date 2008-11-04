87df5fd71c47037dae85e13c4bf9440b73c6c48f
Doug Rabson <dfr@FreeBSD.org>
2008-11-04 16:43:02 +0000

    Include <sys/eventhandler.h>.
    Former-commit-id: 1804d4490cdf1df45df6be5368cd5fb64f07cb3a

--- sys/nfsserver/nfs_srvkrpc.c
+++ sys/nfsserver/nfs_srvkrpc.c
@@ -164,6 +164,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/eventhandler.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<netinet/in.h>
 end_include
 
