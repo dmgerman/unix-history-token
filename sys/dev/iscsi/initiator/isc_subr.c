4582593a74065f2eefaec2c9ae16b412f5167abe
Scott Long <scottl@FreeBSD.org>
2008-11-25 07:17:11 +0000

    Big update to the iSCSI initiator code.  Highlights include IPv6 support, many bugs fixes, many more performance improvements.
    Submitted by:	Danny Braniss
    
    M    sbin/iscontrol/iscsi.conf.5
    M    sbin/iscontrol/iscontrol.8
    M    sbin/iscontrol/iscontrol.h
    M    sbin/iscontrol/config.c
    M    sbin/iscontrol/fsm.c
    M    sbin/iscontrol/login.c
    M    sbin/iscontrol/pdu.c
    M    sbin/iscontrol/misc.c
    M    sbin/iscontrol/auth_subr.c
    M    sbin/iscontrol/iscontrol.c
    M    sys/dev/iscsi/initiator/isc_cam.c
    M    sys/dev/iscsi/initiator/iscsi.h
    M    sys/dev/iscsi/initiator/isc_soc.c
    M    sys/dev/iscsi/initiator/iscsi_subr.c
    M    sys/dev/iscsi/initiator/iscsivar.h
    M    sys/dev/iscsi/initiator/isc_subr.c
    M    sys/dev/iscsi/initiator/iscsi.c
    M    sys/dev/iscsi/initiator/isc_sm.c
    
    
    Former-commit-id: eed044fe4c7e75b755634e478d8ddfc02d5a586b

--- sys/dev/iscsi/initiator/isc_subr.c
+++ sys/dev/iscsi/initiator/isc_subr.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2005-2007 Daniel Braniss<danny@cs.huji.ac.il>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*-  * Copyright (c) 2005-2008 Daniel Braniss<danny@cs.huji.ac.il>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_comment
@@ -158,18 +158,6 @@ include|#
 directive|include
 file|<dev/iscsi/initiator/iscsivar.h>
 end_include
-
-begin_expr_stmt
-name|MALLOC_DEFINE
-argument_list|(
-name|M_PDU
-argument_list|,
-literal|"iSCSI pdu"
-argument_list|,
-literal|"iSCSI driver"
-argument_list|)
-expr_stmt|;
-end_expr_stmt
 
 begin_function
 specifier|static
