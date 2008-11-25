705ff0685f92aad93c818675e0a7e8b2c777d95b
Rui Paulo <rpaulo@FreeBSD.org>
2008-11-25 00:39:03 +0000

    Add support for Marvell PATA M88SX6121.
    PR:		129154
    Submitted by:	João Barros <joao.barros at gmail.com>
    MFC after:	1 month
    
    Former-commit-id: 9d8bb2352467618d4277f29094c011194958306d

--- sys/dev/ata/ata-pci.h
+++ sys/dev/ata/ata-pci.h
@@ -1134,6 +1134,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|ATA_M88SX6121
+value|0x612111ab
+end_define
+
+begin_define
+define|#
+directive|define
 name|ATA_M88SX6145
 value|0x614511ab
 end_define
