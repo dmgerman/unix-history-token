64c212d92de6a03b84397862f4f2df5277b80d42
Bjoern A. Zeeb <bz@FreeBSD.org>
2008-11-26 12:24:18 +0000

    Plug a credential leak in case the inpcb is freed by in6_pcbfree() instead of in_pcbfree(); missed in r183606.
    Reviewed by:	rwatson
    MFC after:	3 days (instantly for 7.1-RC?)
    
    
    Former-commit-id: aa62fe7dfc8a8d0d3a7e1a85f2c320ea4668ddb1

--- sys/netinet6/in6_pcb.c
+++ sys/netinet6/in6_pcb.c
@@ -1924,6 +1924,13 @@ name|inp_vflag
 operator|=
 literal|0
 expr_stmt|;
+name|crfree
+argument_list|(
+name|inp
+operator|->
+name|inp_cred
+argument_list|)
+expr_stmt|;
 ifdef|#
 directive|ifdef
 name|MAC
