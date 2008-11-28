e94c5496cd78978784a4fe151fc4744dda138f3f
Doug Rabson <dfr@FreeBSD.org>
2008-11-28 16:25:43 +0000

    Add Xen HVM support.
    Former-commit-id: a2b7ed9267951ff0b816bf63dc0800a8ea55d3eb

--- 6/sys/xen/gnttab.h
+++ 6/sys/xen/gnttab.h
@@ -48,6 +48,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<xen/interface/memory.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/xen/xen-os.h>
 end_include
 
