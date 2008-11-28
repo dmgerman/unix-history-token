550d502f80c206c36cdb69ad8aaad547bc7d3604
Sam Leffler <sam@FreeBSD.org>
2008-11-28 00:03:41 +0000

    virgin import of ath hal
    Former-commit-id: d972528d489f39e17e1c43f85d857b9b5414619c

---
+++ ah_debug.h
@@ -0,0 +1,131 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (c) 2008 Sam Leffler, Errno Consulting  * Copyright (c) 2008 Atheros Communications, Inc.  *  * Permission to use, copy, modify, and/or distribute this software for any  * purpose with or without fee is hereby granted, provided that the above  * copyright notice and this permission notice appear in all copies.  *  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  *  * $Id: ah_debug.h,v 1.1 2008/10/12 16:44:34 sam Exp $  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ATH_AH_DEBUG_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_ATH_AH_DEBUG_H_
+end_define
+
+begin_comment
+comment|/*  * Atheros Device Hardware Access Layer (HAL).  *  * Debug mask definitions.  */
+end_comment
+
+begin_enum
+enum|enum
+block|{
+name|HAL_DEBUG_REGDOMAIN
+init|=
+literal|0x00000001
+block|,
+comment|/* regulatory handling */
+name|HAL_DEBUG_ATTACH
+init|=
+literal|0x00000002
+block|,
+comment|/* work done in attach */
+name|HAL_DEBUG_RESET
+init|=
+literal|0x00000004
+block|,
+comment|/* reset work */
+name|HAL_DEBUG_NFCAL
+init|=
+literal|0x00000008
+block|,
+comment|/* noise floor calibration */
+name|HAL_DEBUG_PERCAL
+init|=
+literal|0x00000010
+block|,
+comment|/* periodic calibration */
+name|HAL_DEBUG_ANI
+init|=
+literal|0x00000020
+block|,
+comment|/* ANI operation */
+name|HAL_DEBUG_PHYIO
+init|=
+literal|0x00000040
+block|,
+comment|/* phy i/o operations */
+name|HAL_DEBUG_REGIO
+init|=
+literal|0x00000080
+block|,
+comment|/* register i/o operations */
+name|HAL_DEBUG_RFPARAM
+init|=
+literal|0x00000100
+block|,
+name|HAL_DEBUG_TXQUEUE
+init|=
+literal|0x00000200
+block|,
+comment|/* tx queue handling */
+name|HAL_DEBUG_TX
+init|=
+literal|0x00000400
+block|,
+name|HAL_DEBUG_TXDESC
+init|=
+literal|0x00000800
+block|,
+name|HAL_DEBUG_RX
+init|=
+literal|0x00001000
+block|,
+name|HAL_DEBUG_RXDESC
+init|=
+literal|0x00002000
+block|,
+name|HAL_DEBUG_KEYCACHE
+init|=
+literal|0x00004000
+block|,
+comment|/* keycache handling */
+name|HAL_DEBUG_EEPROM
+init|=
+literal|0x00008000
+block|,
+name|HAL_DEBUG_BEACON
+init|=
+literal|0x00010000
+block|,
+comment|/* beacon setup work */
+name|HAL_DEBUG_POWER
+init|=
+literal|0x00020000
+block|,
+comment|/* power management */
+name|HAL_DEBUG_INTERRUPT
+init|=
+literal|0x00000080
+block|,
+comment|/* interrupt handling */
+name|HAL_DEBUG_ANY
+init|=
+literal|0xffffffff
+block|}
+enum|;
+end_enum
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _ATH_AH_DEBUG_H_ */
+end_comment
+
+end_unit
+
---
+++ ah_eeprom.h
@@ -0,0 +1,528 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (c) 2002-2008 Sam Leffler, Errno Consulting  * Copyright (c) 2002-2008 Atheros Communications, Inc.  *  * Permission to use, copy, modify, and/or distribute this software for any  * purpose with or without fee is hereby granted, provided that the above  * copyright notice and this permission notice appear in all copies.  *  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  *  * $Id: ah_eeprom.h,v 1.6 2008/11/10 04:08:00 sam Exp $  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ATH_AH_EEPROM_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_ATH_AH_EEPROM_H_
+end_define
+
+begin_comment
+comment|/*  * Version 3 EEPROMs are all 16K.  * 3.1 adds turbo limit, antenna gain, 16 CTL's, 11g info,  *	and 2.4Ghz ob/db for B& G  * 3.2 has more accurate pcdac intercepts and analog chip  *	calibration.  * 3.3 adds ctl in-band limit, 32 ctl's, and frequency  *	expansion  * 3.4 adds xr power, gainI, and 2.4 turbo params  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER3
+value|0x3000
+end_define
+
+begin_comment
+comment|/* Version 3.0; start of 16k EEPROM */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER3_1
+value|0x3001
+end_define
+
+begin_comment
+comment|/* Version 3.1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER3_2
+value|0x3002
+end_define
+
+begin_comment
+comment|/* Version 3.2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER3_3
+value|0x3003
+end_define
+
+begin_comment
+comment|/* Version 3.3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER3_4
+value|0x3004
+end_define
+
+begin_comment
+comment|/* Version 3.4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_0
+value|0x4000
+end_define
+
+begin_comment
+comment|/* Version 4.0 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_1
+value|0x4001
+end_define
+
+begin_comment
+comment|/* Version 4.0 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_2
+value|0x4002
+end_define
+
+begin_comment
+comment|/* Version 4.0 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_3
+value|0x4003
+end_define
+
+begin_comment
+comment|/* Version 4.0 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_6
+value|0x4006
+end_define
+
+begin_comment
+comment|/* Version 4.0 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_7
+value|0x3007
+end_define
+
+begin_comment
+comment|/* Version 4.7 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER4_9
+value|0x4009
+end_define
+
+begin_comment
+comment|/* EEPROM EAR futureproofing */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER5_0
+value|0x5000
+end_define
+
+begin_comment
+comment|/* Adds new 2413 cal powers and added params */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER5_1
+value|0x5001
+end_define
+
+begin_comment
+comment|/* Adds capability values */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER5_3
+value|0x5003
+end_define
+
+begin_comment
+comment|/* Adds spur mitigation table */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER5_4
+value|0x5004
+end_define
+
+begin_comment
+comment|/*  * Version 14 EEPROMs came in with AR5416.  * 14.2 adds txFrameToPaOn, txFrameToDataStart, ht40PowerInc  * 14.3 adds bswAtten, bswMargin, swSettle, and base OpFlags for HT20/40  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_1
+value|0xE001
+end_define
+
+begin_comment
+comment|/* 11n support */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_2
+value|0xE002
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_3
+value|0xE003
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_7
+value|0xE007
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_9
+value|0xE009
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_16
+value|0xE010
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_17
+value|0xE011
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_EEPROM_VER14_19
+value|0xE013
+end_define
+
+begin_enum
+enum|enum
+block|{
+name|AR_EEP_RFKILL
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_AMODE
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_BMODE
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_GMODE
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_TURBO5DISABLE
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_TURBO2DISABLE
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_ISTALON
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_32KHZCRYSTAL
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_MACADDR
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_COMPRESS
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_FASTFRAME
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_AES
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_BURST
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_MAXQCU
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_KCENTRIES
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_NFTHRESH_5
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_NFTHRESH_2
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_REGDMN_0
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_REGDMN_1
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_OPCAP
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_OPMODE
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_RFSILENT
+block|,
+comment|/* uint16_t* */
+name|AR_EEP_OB_5
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_DB_5
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_OB_2
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_DB_2
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_TXMASK
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_RXMASK
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_RXGAIN_TYPE
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_TXGAIN_TYPE
+block|,
+comment|/* uint8_t* */
+name|AR_EEP_OL_PWRCTRL
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_FSTCLK_5G
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+name|AR_EEP_ANTGAINMAX_5
+block|,
+comment|/* int8_t* */
+name|AR_EEP_ANTGAINMAX_2
+block|,
+comment|/* int8_t* */
+name|AR_EEP_WRITEPROTECT
+block|,
+comment|/* use ath_hal_eepromGetFlag */
+block|}
+enum|;
+end_enum
+
+begin_typedef
+typedef|typedef
+struct|struct
+block|{
+name|uint16_t
+name|rdEdge
+decl_stmt|;
+name|uint16_t
+name|twice_rdEdgePower
+decl_stmt|;
+name|HAL_BOOL
+name|flag
+decl_stmt|;
+block|}
+name|RD_EDGES_POWER
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* XXX should probably be version-dependent */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SD_NO_CTL
+value|0xf0
+end_define
+
+begin_define
+define|#
+directive|define
+name|NO_CTL
+value|0xff
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_MODE_M
+value|0x0f
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_11A
+value|0
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_11B
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_11G
+value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_TURBO
+value|3
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_108G
+value|4
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_2GHT20
+value|5
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_5GHT20
+value|6
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_2GHT40
+value|7
+end_define
+
+begin_define
+define|#
+directive|define
+name|CTL_5GHT40
+value|8
+end_define
+
+begin_define
+define|#
+directive|define
+name|AR_NO_SPUR
+value|0x8000
+end_define
+
+begin_comment
+comment|/* XXX exposed to chip code */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAX_RATE_POWER
+value|63
+end_define
+
+begin_function_decl
+name|HAL_STATUS
+name|ath_hal_legacyEepromAttach
+parameter_list|(
+name|struct
+name|ath_hal
+modifier|*
+name|ah
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|HAL_STATUS
+name|ath_hal_v14EepromAttach
+parameter_list|(
+name|struct
+name|ath_hal
+modifier|*
+name|ah
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _ATH_AH_EEPROM_H_ */
+end_comment
+
+end_unit
+
