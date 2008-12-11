feb5cb1b3d8e092d423cb5d66e0536413e12e118
Warner Losh <imp@FreeBSD.org>
2008-12-11 08:15:14 +0000

    Push mips support for as into the tree.
    Former-commit-id: 8b2a3c71d4a76531df1de91b49bbd8d42ecc6e14

---
+++ gnu/usr.bin/binutils/as/mips-freebsd/targ-cpu.h
@@ -0,0 +1,20 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TE_TMIPS
+value|1
+end_define
+
+begin_include
+include|#
+directive|include
+file|"tc-mips.h"
+end_include
+
+end_unit
+
