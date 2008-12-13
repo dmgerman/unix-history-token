a8c4afcdd87510cd9616041d601d1f531fc4f755
Alexander Motin <mav@FreeBSD.org>
2008-12-13 22:26:24 +0000

    Revert rev. 183277:
    Remove ng_rmnode_flags() function.
    ng_rmnode_self() was made to be called only while having node locked.
    When node is properly locked, any function call sent to it will always be
    queued. So turning ng_rmnode_self() into the ng_rmnode_flags() is not just
    meaningless, but incorrent, as it violates node locking when called outside.
    
    No objections:	julian, thompsa
    
    
    Former-commit-id: e6d71bb74a8039324d0b4dc37b719497ee8c0a49

--- sys/netgraph/netgraph.h
+++ sys/netgraph/netgraph.h
@@ -5873,19 +5873,6 @@ end_comment
 
 begin_function_decl
 name|int
-name|ng_rmnode_flags
-parameter_list|(
-name|node_p
-name|here
-parameter_list|,
-name|int
-name|flags
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
 name|ng_rmnode_self
 parameter_list|(
 name|node_p
