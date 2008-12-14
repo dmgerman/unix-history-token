925ca16acd0b7b9d6cbe6e57fa5fb224a023c271
Ken Smith <kensmith@FreeBSD.org>
2008-12-14 06:40:04 +0000

    Clean out the remaining alpha-isms.
    Former-commit-id: 7b481ab7dd2ccf3affb8c9318eda3ea398aefa15

--- usr.sbin/sysinstall/main.c
+++ usr.sbin/sysinstall/main.c
@@ -720,11 +720,6 @@ if|#
 directive|if
 name|defined
 argument_list|(
-name|__alpha__
-argument_list|)
-operator|||
-name|defined
-argument_list|(
 name|__sparc64__
 argument_list|)
 operator|||
--- usr.sbin/sysinstall/sysinstall.h
+++ usr.sbin/sysinstall/sysinstall.h
@@ -107,11 +107,6 @@ argument_list|)
 operator|||
 name|defined
 argument_list|(
-name|__alpha__
-argument_list|)
-operator|||
-name|defined
-argument_list|(
 name|__amd64__
 argument_list|)
 end_if
