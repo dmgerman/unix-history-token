3bca1b232c3b1452722a6e9cfc11ff783c3d047b
Adrian Chadd <adrian@FreeBSD.org>
2009-01-09 17:16:18 +0000

    Comment some potentially confusing logic.
    Nitpicking by: mlaier
    
    MFC after:	2 weeks
    
    
    Former-commit-id: 42d7e3f28fb235b9ccfbc09ef91c014e69997709

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -1676,6 +1676,7 @@ name|sin_zero
 argument_list|)
 argument_list|)
 expr_stmt|;
+comment|/* 			 * Is the address a local IP address?  			 * If INP_NONLOCALOK is set, then the socket may be bound 			 * to any local endpoint address. 			 */
 if|if
 condition|(
 if|#
