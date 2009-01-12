604a32e5e24b3941852088b282f0b9ef04aead35
Jung-uk Kim <jkim@FreeBSD.org>
2009-01-12 19:23:46 +0000

    Connect padlock(4) to amd64 build for VIA Nano processors.
    Former-commit-id: 32e41a9a5da8051db8715f61e9eeb910d2ce503e

--- sys/crypto/via/padlock.c
+++ sys/crypto/via/padlock.c
@@ -70,6 +70,12 @@ if|#
 directive|if
 name|defined
 argument_list|(
+name|__amd64__
+argument_list|)
+operator|||
+operator|(
+name|defined
+argument_list|(
 name|__i386__
 argument_list|)
 operator|&&
@@ -78,6 +84,7 @@ name|defined
 argument_list|(
 name|PC98
 argument_list|)
+operator|)
 end_if
 
 begin_include
@@ -322,6 +329,12 @@ if|#
 directive|if
 name|defined
 argument_list|(
+name|__amd64__
+argument_list|)
+operator|||
+operator|(
+name|defined
+argument_list|(
 name|__i386__
 argument_list|)
 operator|&&
@@ -330,6 +343,7 @@ name|defined
 argument_list|(
 name|PC98
 argument_list|)
+operator|)
 comment|/* If there is no AES support, we has nothing to do here. */
 if|if
 condition|(
