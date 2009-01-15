e373f94740a380dee382c5a1391a4fb9289e1812
Marius Strobl <marius@FreeBSD.org>
2009-01-15 20:13:22 +0000

    MFC: r183281
    - Don't read the identifier string from the VPD if there's no chip
      containing an Ethernet address fitted as this is yet another thing
      that fails in that case in order to avoid the one second delay
      until pci_read_vpd_reg() times out.
    - Const'ify the bge_devs array.
    
    
    Former-commit-id: 0a57168db663136da50281aab27a65d39288c577

--- sys/dev/bge/if_bge.c
+++ sys/dev/bge/if_bge.c
@@ -347,6 +347,7 @@ end_comment
 
 begin_struct
 specifier|static
+specifier|const
 struct|struct
 name|bge_type
 block|{
@@ -9128,6 +9129,7 @@ name|device_t
 name|dev
 parameter_list|)
 block|{
+specifier|const
 name|struct
 name|bge_type
 modifier|*
@@ -9264,6 +9266,11 @@ name|pname
 decl_stmt|;
 if|if
 condition|(
+name|bge_has_eaddr
+argument_list|(
+name|sc
+argument_list|)
+operator|&&
 name|pci_get_vpd_ident
 argument_list|(
 name|dev
