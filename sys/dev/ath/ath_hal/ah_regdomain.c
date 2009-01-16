0bc5ff6417c11f5550861fb6bbca40aa7fe8233d
Sam Leffler <sam@FreeBSD.org>
2009-01-16 20:44:45 +0000

    export PSB frequencies
    Former-commit-id: 829481c7712955c88495b47a347d79bb7399b890

--- sys/dev/ath/ath_hal/ah_regdomain.c
+++ sys/dev/ath/ath_hal/ah_regdomain.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 2002-2008 Sam Leffler, Errno Consulting  * Copyright (c) 2005-2006 Atheros Communications, Inc.  * All rights reserved.  *  * Permission to use, copy, modify, and/or distribute this software for any  * purpose with or without fee is hereby granted, provided that the above  * copyright notice and this permission notice appear in all copies.  *  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  *  * $Id: ah_regdomain.c,v 1.24 2008/11/27 22:29:27 sam Exp $  */
+comment|/*  * Copyright (c) 2002-2008 Sam Leffler, Errno Consulting  * Copyright (c) 2005-2006 Atheros Communications, Inc.  * All rights reserved.  *  * Permission to use, copy, modify, and/or distribute this software for any  * purpose with or without fee is hereby granted, provided that the above  * copyright notice and this permission notice appear in all copies.  *  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  *  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -10659,8 +10659,10 @@ block|,
 operator|.
 name|chan11a
 operator|=
-name|BM3
+name|BM4
 argument_list|(
+name|F1_4950_4980
+argument_list|,
 name|F1_5120_5240
 argument_list|,
 name|F1_5260_5700
@@ -10671,8 +10673,10 @@ block|,
 operator|.
 name|chan11a_half
 operator|=
-name|BM3
+name|BM4
 argument_list|(
+name|F1_4945_4985
+argument_list|,
 name|F2_5120_5240
 argument_list|,
 name|F2_5260_5700
@@ -10683,8 +10687,10 @@ block|,
 operator|.
 name|chan11a_quarter
 operator|=
-name|BM3
+name|BM4
 argument_list|(
+name|F1_4942_4987
+argument_list|,
 name|F3_5120_5240
 argument_list|,
 name|F3_5260_5700
