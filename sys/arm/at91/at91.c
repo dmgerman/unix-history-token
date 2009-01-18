4caf5d5da2776d6883d24ff3baf6133f4df2ecfe
Stanislav Sedov <stas@FreeBSD.org>
2009-01-18 12:12:38 +0000

    - Merge r184309:   Add a missing NULL-pointer check.
    Former-commit-id: 056641445e71b80eacc414291144bdf740aa408d

--- sys/arm/at91/at91.c
+++ sys/arm/at91/at91.c
@@ -2128,6 +2128,15 @@ argument_list|,
 name|child
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|rle
+operator|->
+name|res
+operator|!=
+name|NULL
+condition|)
+block|{
 name|rman_set_bustag
 argument_list|(
 name|rle
@@ -2147,6 +2156,7 @@ argument_list|,
 name|start
 argument_list|)
 expr_stmt|;
+block|}
 break|break;
 block|}
 if|if
