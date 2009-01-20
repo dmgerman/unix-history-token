5942e119556ddafa9bae5f96034af7b9b1c86e8c
Maksim Yevmenkin <emax@FreeBSD.org>
2009-01-20 22:17:05 +0000

    Update (well, actually rewrite mostly) ng_ubt2 driver for USB2.
    Reviewed by:	HPS, alfred
    Blessed by:	HPS
    
    
    Former-commit-id: f8ed06edc975933a2c23bc0884eb029050ccf8e3

--- sys/dev/usb2/bluetooth/ng_ubt2_var.h
+++ sys/dev/usb2/bluetooth/ng_ubt2_var.h
@@ -4,7 +4,7 @@ comment|/*  * ng_ubt_var.h  */
 end_comment
 
 begin_comment
-comment|/*-  * Copyright (c) 2001-2002 Maksim Yevmenkin<m_evmenkin@yahoo.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: ng_ubt_var.h,v 1.2 2003/03/22 23:44:36 max Exp $  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2001-2009 Maksim Yevmenkin<m_evmenkin@yahoo.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $Id: ng_ubt_var.h,v 1.2 2003/03/22 23:44:36 max Exp $  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -17,23 +17,7 @@ begin_define
 define|#
 directive|define
 name|_NG_UBT_VAR_H_
-end_define
-
-begin_comment
-comment|/* pullup wrapper */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|NG_UBT_M_PULLUP
-parameter_list|(
-name|m
-parameter_list|,
-name|s
-parameter_list|)
-define|\
-value|do { \ 		if ((m)->m_len< (s)) \ 			(m) = m_pullup((m), (s)); \ 		if ((m) == NULL) { \ 			NG_UBT_ALERT("%s: %s - m_pullup(%d) failed\n", \ 				__func__, sc->sc_name, (s)); \ 		} \ 	} while (0)
+value|1
 end_define
 
 begin_comment
@@ -43,7 +27,7 @@ end_comment
 begin_define
 define|#
 directive|define
-name|NG_UBT_DEBUG
+name|UBT_DEBUG
 parameter_list|(
 name|level
 parameter_list|,
@@ -53,47 +37,68 @@ name|fmt
 parameter_list|,
 modifier|...
 parameter_list|)
-value|do { \     if ((sc)->sc_debug>= (level)) { \         printf("%s:%s:%d: " fmt, (sc)->sc_name, \ 	       __FUNCTION__, __LINE__,## __VA_ARGS__); \     } \ } while (0)
+define|\
+value|do {								\ 	if ((sc)->sc_debug>= (level))				\ 		printf("%s:%s:%d: " fmt, (sc)->sc_name,		\ 			__FUNCTION__, __LINE__,## __VA_ARGS__);	\ } while (0)
 end_define
 
 begin_define
 define|#
 directive|define
-name|NG_UBT_ALERT
+name|UBT_ALERT
 parameter_list|(
 modifier|...
 parameter_list|)
-value|NG_UBT_DEBUG(NG_UBT_ALERT_LEVEL, __VA_ARGS__)
+value|UBT_DEBUG(NG_UBT_ALERT_LEVEL, __VA_ARGS__)
 end_define
 
 begin_define
 define|#
 directive|define
-name|NG_UBT_ERR
+name|UBT_ERR
 parameter_list|(
 modifier|...
 parameter_list|)
-value|NG_UBT_DEBUG(NG_UBT_ERR_LEVEL, __VA_ARGS__)
+value|UBT_DEBUG(NG_UBT_ERR_LEVEL, __VA_ARGS__)
 end_define
 
 begin_define
 define|#
 directive|define
-name|NG_UBT_WARN
+name|UBT_WARN
 parameter_list|(
 modifier|...
 parameter_list|)
-value|NG_UBT_DEBUG(NG_UBT_WARN_LEVEL, __VA_ARGS__)
+value|UBT_DEBUG(NG_UBT_WARN_LEVEL, __VA_ARGS__)
 end_define
 
 begin_define
 define|#
 directive|define
-name|NG_UBT_INFO
+name|UBT_INFO
 parameter_list|(
 modifier|...
 parameter_list|)
-value|NG_UBT_DEBUG(NG_UBT_INFO_LEVEL, __VA_ARGS__)
+value|UBT_DEBUG(NG_UBT_INFO_LEVEL, __VA_ARGS__)
+end_define
+
+begin_define
+define|#
+directive|define
+name|UBT_MBUFQ_LOCK
+parameter_list|(
+name|sc
+parameter_list|)
+value|mtx_lock(&(sc)->sc_mbufq_mtx)
+end_define
+
+begin_define
+define|#
+directive|define
+name|UBT_MBUFQ_UNLOCK
+parameter_list|(
+name|sc
+parameter_list|)
+value|mtx_unlock(&(sc)->sc_mbufq_mtx)
 end_define
 
 begin_comment
@@ -111,45 +116,69 @@ begin_define
 define|#
 directive|define
 name|UBT_DEFAULT_QLEN
-value|12
+value|64
 end_define
-
-begin_comment
-comment|/* Bluetooth USB defines */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|UBT_IF_0_N_TRANSFER
-value|7
-end_define
-
-begin_comment
-comment|/* units */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|UBT_IF_1_N_TRANSFER
-value|4
-end_define
-
-begin_comment
-comment|/* units */
-end_comment
 
 begin_define
 define|#
 directive|define
 name|UBT_ISOC_NFRAMES
-value|25
+value|32
 end_define
 
 begin_comment
-comment|/* units */
+comment|/* should be factor of 8 */
 end_comment
+
+begin_comment
+comment|/* Bluetooth USB defines */
+end_comment
+
+begin_enum
+enum|enum
+block|{
+comment|/* Interface #0 transfers */
+name|UBT_IF_0_BULK_DT_WR
+init|=
+literal|0
+block|,
+name|UBT_IF_0_BULK_DT_RD
+block|,
+name|UBT_IF_0_INTR_DT_RD
+block|,
+name|UBT_IF_0_CTRL_DT_WR
+block|,
+name|UBT_IF_0_BULK_CS_WR
+block|,
+name|UBT_IF_0_BULK_CS_RD
+block|,
+name|UBT_IF_0_INTR_CS_RD
+block|,
+name|UBT_IF_0_N_TRANSFER
+block|,
+comment|/* number of interface 0's transfers */
+comment|/* Interface #1 transfers */
+name|UBT_IF_1_ISOC_DT_RD1
+init|=
+name|UBT_IF_0_N_TRANSFER
+block|,
+name|UBT_IF_1_ISOC_DT_RD2
+block|,
+name|UBT_IF_1_ISOC_DT_WR1
+block|,
+name|UBT_IF_1_ISOC_DT_WR2
+block|,
+name|UBT_N_TRANSFER
+block|,
+comment|/* total number of transfers */
+name|UBT_IF_1_N_TRANSFER
+init|=
+name|UBT_N_TRANSFER
+operator|-
+name|UBT_IF_1_ISOC_DT_RD1
+block|, }
+enum|;
+end_enum
 
 begin_comment
 comment|/* USB device softc structure */
@@ -159,131 +188,116 @@ begin_struct
 struct|struct
 name|ubt_softc
 block|{
-comment|/* State */
-name|ng_ubt_node_debug_ep
-name|sc_debug
-decl_stmt|;
-comment|/* debug level */
-name|uint32_t
-name|sc_flags
-decl_stmt|;
-comment|/* device flags */
-define|#
-directive|define
-name|UBT_NEED_FRAME_TYPE
-value|(1<< 0)
-comment|/* device required frame type */
-define|#
-directive|define
-name|UBT_HAVE_FRAME_TYPE
-value|UBT_NEED_FRAME_TYPE
-define|#
-directive|define
-name|UBT_FLAG_READ_STALL
-value|(1<< 1)
-comment|/* read transfer has stalled */
-define|#
-directive|define
-name|UBT_FLAG_WRITE_STALL
-value|(1<< 2)
-comment|/* write transfer has stalled */
-define|#
-directive|define
-name|UBT_FLAG_INTR_STALL
-value|(1<< 3)
-comment|/* interrupt transfer has stalled */
-name|ng_ubt_node_stat_ep
-name|sc_stat
-decl_stmt|;
-comment|/* statistic */
-define|#
-directive|define
-name|NG_UBT_STAT_PCKTS_SENT
-parameter_list|(
-name|s
-parameter_list|)
-value|(s).pckts_sent ++
-define|#
-directive|define
-name|NG_UBT_STAT_BYTES_SENT
-parameter_list|(
-name|s
-parameter_list|,
-name|n
-parameter_list|)
-value|(s).bytes_sent += (n)
-define|#
-directive|define
-name|NG_UBT_STAT_PCKTS_RECV
-parameter_list|(
-name|s
-parameter_list|)
-value|(s).pckts_recv ++
-define|#
-directive|define
-name|NG_UBT_STAT_BYTES_RECV
-parameter_list|(
-name|s
-parameter_list|,
-name|n
-parameter_list|)
-value|(s).bytes_recv += (n)
-define|#
-directive|define
-name|NG_UBT_STAT_OERROR
-parameter_list|(
-name|s
-parameter_list|)
-value|(s).oerrors ++
-define|#
-directive|define
-name|NG_UBT_STAT_IERROR
-parameter_list|(
-name|s
-parameter_list|)
-value|(s).ierrors ++
-define|#
-directive|define
-name|NG_UBT_STAT_RESET
-parameter_list|(
-name|s
-parameter_list|)
-value|bzero(&(s), sizeof((s)))
 name|uint8_t
 name|sc_name
 index|[
 literal|16
 index|]
 decl_stmt|;
-name|struct
-name|mtx
-name|sc_mtx
+comment|/* State */
+name|ng_ubt_node_debug_ep
+name|sc_debug
 decl_stmt|;
+comment|/* debug level */
+name|int
+name|sc_flags
+decl_stmt|;
+comment|/* device flags */
+define|#
+directive|define
+name|UBT_FLAG_READ_STALL
+value|(1<< 0)
+comment|/* read transfer has stalled */
+define|#
+directive|define
+name|UBT_FLAG_WRITE_STALL
+value|(1<< 1)
+comment|/* write transfer has stalled */
+define|#
+directive|define
+name|UBT_FLAG_INTR_STALL
+value|(1<< 2)
+comment|/* inter transfer has stalled */
+name|ng_ubt_node_stat_ep
+name|sc_stat
+decl_stmt|;
+comment|/* statistic */
+define|#
+directive|define
+name|UBT_STAT_PCKTS_SENT
+parameter_list|(
+name|sc
+parameter_list|)
+value|(sc)->sc_stat.pckts_sent ++
+define|#
+directive|define
+name|UBT_STAT_BYTES_SENT
+parameter_list|(
+name|sc
+parameter_list|,
+name|n
+parameter_list|)
+value|(sc)->sc_stat.bytes_sent += (n)
+define|#
+directive|define
+name|UBT_STAT_PCKTS_RECV
+parameter_list|(
+name|sc
+parameter_list|)
+value|(sc)->sc_stat.pckts_recv ++
+define|#
+directive|define
+name|UBT_STAT_BYTES_RECV
+parameter_list|(
+name|sc
+parameter_list|,
+name|n
+parameter_list|)
+value|(sc)->sc_stat.bytes_recv += (n)
+define|#
+directive|define
+name|UBT_STAT_OERROR
+parameter_list|(
+name|sc
+parameter_list|)
+value|(sc)->sc_stat.oerrors ++
+define|#
+directive|define
+name|UBT_STAT_IERROR
+parameter_list|(
+name|sc
+parameter_list|)
+value|(sc)->sc_stat.ierrors ++
+define|#
+directive|define
+name|UBT_STAT_RESET
+parameter_list|(
+name|sc
+parameter_list|)
+value|bzero(&(sc)->sc_stat, sizeof((sc)->sc_stat))
 comment|/* USB device specific */
 name|struct
-name|usb2_xfer
-modifier|*
-name|sc_xfer_if_0
+name|mtx
+name|sc_if_mtx
 index|[
-name|UBT_IF_0_N_TRANSFER
+literal|2
 index|]
 decl_stmt|;
+comment|/* interface locks */
 name|struct
 name|usb2_xfer
 modifier|*
-name|sc_xfer_if_1
+name|sc_xfer
 index|[
-name|UBT_IF_1_N_TRANSFER
+name|UBT_N_TRANSFER
 index|]
 decl_stmt|;
-comment|/* Interrupt pipe (HCI events) */
 name|struct
-name|mbuf
-modifier|*
-name|sc_intr_buffer
+name|mtx
+name|sc_mbufq_mtx
 decl_stmt|;
-comment|/* interrupt buffer */
-comment|/* Control pipe (HCI commands) */
+comment|/* lock for all queues */
+comment|/* HCI commands */
 name|struct
 name|ng_bt_mbufq
 name|sc_cmdq
@@ -292,15 +306,13 @@ comment|/* HCI command queue */
 define|#
 directive|define
 name|UBT_CTRL_BUFFER_SIZE
-value|(sizeof(ng_hci_cmd_pkt_t) + NG_HCI_CMD_PKT_SIZE)
-comment|/* Bulk in pipe (ACL data) */
-name|struct
-name|mbuf
-modifier|*
-name|sc_bulk_in_buffer
-decl_stmt|;
-comment|/* bulk-in buffer */
-comment|/* Bulk out pipe (ACL data) */
+value|(sizeof(struct usb2_device_request) +	\ 				 sizeof(ng_hci_cmd_pkt_t) + NG_HCI_CMD_PKT_SIZE)
+define|#
+directive|define
+name|UBT_INTR_BUFFER_SIZE
+value|(MCLBYTES-1)
+comment|/* reserve 1 byte for ID-tag */
+comment|/* ACL data */
 name|struct
 name|ng_bt_mbufq
 name|sc_aclq
@@ -310,23 +322,23 @@ define|#
 directive|define
 name|UBT_BULK_READ_BUFFER_SIZE
 value|(MCLBYTES-1)
-comment|/* reserve one byte for ID-tag */
+comment|/* reserve 1 byte for ID-tag */
 define|#
 directive|define
 name|UBT_BULK_WRITE_BUFFER_SIZE
 value|(MCLBYTES)
-comment|/* Isoc. out pipe (ACL data) */
+comment|/* SCO data */
 name|struct
 name|ng_bt_mbufq
 name|sc_scoq
 decl_stmt|;
 comment|/* SCO data queue */
-comment|/* Isoc. in pipe (ACL data) */
 name|struct
-name|ng_bt_mbufq
-name|sc_sciq
+name|mbuf
+modifier|*
+name|sc_isoc_in_buffer
 decl_stmt|;
-comment|/* SCO data queue */
+comment|/* SCO reassembly buffer */
 comment|/* Netgraph specific */
 name|node_p
 name|sc_node
@@ -336,6 +348,40 @@ name|hook_p
 name|sc_hook
 decl_stmt|;
 comment|/* upstream hook */
+comment|/* Glue */
+name|int
+name|sc_task_flags
+decl_stmt|;
+comment|/* task flags */
+define|#
+directive|define
+name|UBT_FLAG_T_PENDING
+value|(1<< 0)
+comment|/* task pending */
+define|#
+directive|define
+name|UBT_FLAG_T_STOP_ALL
+value|(1<< 1)
+comment|/* stop all xfers */
+define|#
+directive|define
+name|UBT_FLAG_T_START_ALL
+value|(1<< 2)
+comment|/* start all read and isoc 						   write xfers */
+define|#
+directive|define
+name|UBT_FLAG_T_START_CTRL
+value|(1<< 3)
+comment|/* start control xfer (write) */
+define|#
+directive|define
+name|UBT_FLAG_T_START_BULK
+value|(1<< 4)
+comment|/* start bulk xfer (write) */
+name|struct
+name|task
+name|sc_task
+decl_stmt|;
 block|}
 struct|;
 end_struct
