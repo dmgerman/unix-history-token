8f7fcc848a5817be11a39009e2be076e940e0531
Weongyo Jeong <weongyo@FreeBSD.org>
2009-01-31 05:25:14 +0000

    MFC r186919:   fix a typo to set the 48Mbps data rate.
      PR:		kern/130189
      Submitted by:	Paul B. Mahol <onemda_at_gmail.com>
      Reviewed by:	sam
    
    
    Former-commit-id: aca971485adce6d90225285a47d14e0b5fe85468

--- sys/dev/if_ndis/if_ndis.c
+++ sys/dev/if_ndis/if_ndis.c
@@ -3837,7 +3837,7 @@ name|TESTSETRATE
 argument_list|(
 name|IEEE80211_MODE_11G
 argument_list|,
-literal|47
+literal|48
 argument_list|)
 expr_stmt|;
 name|TESTSETRATE
@@ -3879,7 +3879,7 @@ name|TESTSETRATE
 argument_list|(
 name|IEEE80211_MODE_11A
 argument_list|,
-literal|47
+literal|48
 argument_list|)
 expr_stmt|;
 name|TESTSETRATE
