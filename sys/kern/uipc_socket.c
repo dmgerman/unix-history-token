aca3d592961c768febb090969a40c5acc562f8c0
Robert N. M. Watson <rwatson@FreeBSD.org>
2009-02-04 20:00:17 +0000

    Remove written-to but never read local variable 'offset' from soreceive_dgram().
    Submitted by:	Christoph Mallon <christoph dot mallon at gmx dot de>
    MFC after:	1 week
    
    
    Former-commit-id: 25ad7e6ee4a10aa2b8ae247cebf2c03b98b428b6

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -8619,8 +8619,6 @@ decl_stmt|,
 name|len
 decl_stmt|,
 name|error
-decl_stmt|,
-name|offset
 decl_stmt|;
 name|struct
 name|protosw
@@ -9384,10 +9382,6 @@ operator|(
 literal|"soreceive_dgram: !data"
 operator|)
 argument_list|)
-expr_stmt|;
-name|offset
-operator|=
-literal|0
 expr_stmt|;
 while|while
 condition|(
