9f6cbb4d8aa6d63a8b3de585b8d5cd6e4fc5d390
John Baldwin <jhb@FreeBSD.org>
2009-02-12 14:41:23 +0000

    MFC: Drop the kernel linker lock while running SYSUNINIT routines and removing sysctls during a linker file unload.
    Former-commit-id: fc9076ca474938342c79bc896bbd1a5f0d6999b0

--- sys/kern/kern_linker.c
+++ sys/kern/kern_linker.c
@@ -2688,6 +2688,16 @@ operator|&
 name|LINKER_FILE_LINKED
 condition|)
 block|{
+name|file
+operator|->
+name|flags
+operator|&=
+operator|~
+name|LINKER_FILE_LINKED
+expr_stmt|;
+name|KLD_UNLOCK
+argument_list|()
+expr_stmt|;
 name|linker_file_sysuninit
 argument_list|(
 name|file
@@ -2697,6 +2707,9 @@ name|linker_file_unregister_sysctls
 argument_list|(
 name|file
 argument_list|)
+expr_stmt|;
+name|KLD_LOCK
+argument_list|()
 expr_stmt|;
 block|}
 name|TAILQ_REMOVE
