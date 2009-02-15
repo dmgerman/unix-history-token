ad2a8a563c862d2ad57cb60094cffe1cd1c0af03
Bruce M Simpson <bms@FreeBSD.org>
2009-02-15 12:10:05 +0000

    Fix a typo which caused ifmcstat's sysctl path to print the network-layer endpoint address of the group membership, rather than its link-layer mapping as intended. The KVM path is not affected.
    MFC after:	1 week
    
    
    Former-commit-id: a622db65e8e50720db63debdbdfc884a31d5c638

--- usr.sbin/ifmcstat/ifmcstat.c
+++ usr.sbin/ifmcstat/ifmcstat.c
@@ -3783,11 +3783,11 @@ operator|=
 name|getnameinfo
 argument_list|(
 operator|&
-name|pifasa
+name|pllsa
 operator|->
 name|sa
 argument_list|,
-name|pifasa
+name|pllsa
 operator|->
 name|sa
 operator|.
