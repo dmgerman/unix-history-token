b6d255bcfb4964a98d05bdaee7acf96e85386546
Robert N. M. Watson <rwatson@FreeBSD.org>
2009-03-02 13:29:18 +0000

    Merge OpenBSM 1.1 beta 1 from OpenBSM vendor branch to head, both contrib/openbsm (svn merge) and src/sys/{bsm,security/audit} (manual merge).
    OpenBSM history for imported revision below for reference.
    
    MFC after:      1 month
    Sponsored by:   Apple, Inc.
    Obtained from:  TrustedBSD Project
    
    OpenBSM 1.1 beta 1
    
    - The filesz parameter in audit_control(5) now accepts suffixes: 'B' for
      Bytes, 'K' for Kilobytes, 'M' for Megabytes, and 'G' for Gigabytes.
      For legacy support no suffix defaults to bytes.
    - Audit trail log expiration support added.  It is configured in
      audit_control(5) with the expire-after parameter.  If there is no
      expire-after parameter in audit_control(5), the default, then the audit
      trail files are not expired and removed.  See audit_control(5) for
      more information.
    - Change defaults in audit_control: warn at 5% rather than 20% free for audit
      partitions, rotate automatically at 2mb, and set the default policy to
      cnt,argv rather than cnt so that execve(2) arguments are captured if
      AUE_EXECVE events are audited.  These may provide more usable defaults for
      many users.
    - Use au_domain_to_bsm(3) and au_socket_type_to_bsm(3) to convert
      au_to_socket_ex(3) arguments to BSM format.
    - Fix error encoding AUT_IPC_PERM tokens.
    
    
    Former-commit-id: 932ea9cf8a4dbf3cf415a1ba69aa42d37b1e9915

--- sys/bsm/audit_kevents.h
+++ sys/bsm/audit_kevents.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2005 Apple Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * P4: //depot/projects/trustedbsd/openbsm/sys/bsm/audit_kevents.h#4  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2005 Apple Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * P4: //depot/projects/trustedbsd/openbsm/sys/bsm/audit_kevents.h#5  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -4751,6 +4751,28 @@ begin_comment
 comment|/* Darwin. */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|AUE_PREAD
+value|43192
+end_define
+
+begin_comment
+comment|/* Darwin/FreeBSD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|AUE_PWRITE
+value|43193
+end_define
+
+begin_comment
+comment|/* Darwin/FreeBSD. */
+end_comment
+
 begin_comment
 comment|/*  * Darwin BSM uses a number of AUE_O_* definitions, which are aliased to the  * normal Solaris BSM identifiers.  _O_ refers to it being an old, or compat  * interface.  In most cases, Darwin has never implemented these system calls  * but picked up the fields in their system call table from their FreeBSD  * import.  Happily, these have different names than the AUE_O* definitions  * in Solaris BSM.  */
 end_comment
@@ -5400,13 +5422,6 @@ end_comment
 begin_define
 define|#
 directive|define
-name|AUE_ACCESSEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
 name|AUE_ATGETMSG
 value|AUE_NULL
 end_define
@@ -5477,13 +5492,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_CHMODEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
 name|AUE_CHUD
 value|AUE_NULL
 end_define
@@ -5499,13 +5507,6 @@ begin_define
 define|#
 directive|define
 name|AUE_DUP
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
-name|AUE_FCHMODEXTENDED
 value|AUE_NULL
 end_define
 
@@ -5575,13 +5576,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_FSTATEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
 name|AUE_FSTATFS64
 value|AUE_NULL
 end_define
@@ -5603,7 +5597,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_FSTAT64EXTENDED
+name|AUE_FSTAT64_EXTENDED
 value|AUE_NULL
 end_define
 
@@ -5841,13 +5835,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_LSTATEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
 name|AUE_LSTATV
 value|AUE_NULL
 end_define
@@ -5862,7 +5849,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_LSTAT64EXTENDED
+name|AUE_LSTAT64_EXTENDED
 value|AUE_NULL
 end_define
 
@@ -5890,20 +5877,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_MKDIREXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
-name|AUE_MKFIFOEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
 name|AUE_MODWATCH
 value|AUE_NULL
 end_define
@@ -5919,27 +5892,6 @@ begin_define
 define|#
 directive|define
 name|AUE_MSYNC
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
-name|AUE_OPENEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
-name|AUE_PREAD
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
-name|AUE_PWRITE
 value|AUE_NULL
 end_define
 
@@ -6247,13 +6199,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_STATEXTENDED
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
 name|AUE_STATFS64
 value|AUE_NULL
 end_define
@@ -6275,7 +6220,7 @@ end_define
 begin_define
 define|#
 directive|define
-name|AUE_STAT64EXTENDED
+name|AUE_STAT64_EXTENDED
 value|AUE_NULL
 end_define
 
@@ -6297,13 +6242,6 @@ begin_define
 define|#
 directive|define
 name|AUE_TABLE
-value|AUE_NULL
-end_define
-
-begin_define
-define|#
-directive|define
-name|AUE_UMASKEXTENDED
 value|AUE_NULL
 end_define
 
--- sys/security/audit/audit_bsm_token.c
+++ sys/security/audit/audit_bsm_token.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2004-2009 Apple Inc.  * Copyright (c) 2005 SPARTA, Inc.  * All rights reserved.  *  * This code was developed in part by Robert N. M. Watson, Senior Principal  * Scientist, SPARTA, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_token.c#86  */
+comment|/*-  * Copyright (c) 2004-2009 Apple Inc.  * Copyright (c) 2005 SPARTA, Inc.  * All rights reserved.  *  * This code was developed in part by Robert N. M. Watson, Senior Principal  * Scientist, SPARTA, Inc.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_token.c#90  */
 end_comment
 
 begin_include
@@ -437,7 +437,7 @@ name|pad0_16
 init|=
 literal|0
 decl_stmt|;
-name|u_int16_t
+name|u_int32_t
 name|pad0_32
 init|=
 literal|0
@@ -637,7 +637,7 @@ name|pad0_16
 init|=
 literal|0
 decl_stmt|;
-name|u_int16_t
+name|u_int32_t
 name|pad0_32
 init|=
 literal|0
@@ -1535,6 +1535,11 @@ name|t
 argument_list|,
 name|dptr
 argument_list|,
+sizeof|sizeof
+argument_list|(
+name|u_char
+argument_list|)
+operator|+
 literal|12
 operator|*
 sizeof|sizeof
@@ -3505,7 +3510,7 @@ argument_list|(
 name|u_int16_t
 argument_list|)
 operator|+
-literal|16
+literal|8
 operator|*
 sizeof|sizeof
 argument_list|(
@@ -3530,18 +3535,22 @@ name|ADD_U_INT16
 argument_list|(
 name|dptr
 argument_list|,
+name|au_domain_to_bsm
+argument_list|(
 name|so_domain
 argument_list|)
+argument_list|)
 expr_stmt|;
-comment|/* XXXRW: explicitly convert? */
 name|ADD_U_INT16
 argument_list|(
 name|dptr
 argument_list|,
+name|au_socket_type_to_bsm
+argument_list|(
 name|so_type
 argument_list|)
+argument_list|)
 expr_stmt|;
-comment|/* XXXRW: explicitly convert? */
 if|if
 condition|(
 name|so_domain
