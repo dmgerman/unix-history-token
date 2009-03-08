dfe7f0714e70e378a8dfe0553f9f2f4fc43cdff7
Tim Kientzle <kientzle@FreeBSD.org>
2009-03-08 05:34:20 +0000

    Merge r552,r559 from libarchive.googlecode.com:  High-resolution time support on Tru64, AIX, and GNU Hurd, thanks to Björn Jacke.
    Former-commit-id: 6aa3a0d269a7d8fa6a1c96e194de1e7302de6263

--- usr.bin/tar/bsdtar_platform.h
+++ usr.bin/tar/bsdtar_platform.h
@@ -504,16 +504,11 @@ parameter_list|)
 value|(st)->st_mtimespec.tv_nsec
 end_define
 
-begin_else
-else|#
-directive|else
-end_else
-
-begin_if
-if|#
-directive|if
+begin_elif
+elif|#
+directive|elif
 name|HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
-end_if
+end_elif
 
 begin_define
 define|#
@@ -535,6 +530,84 @@ parameter_list|)
 value|(st)->st_mtim.tv_nsec
 end_define
 
+begin_elif
+elif|#
+directive|elif
+name|HAVE_STRUCT_STAT_ST_MTIME_N
+end_elif
+
+begin_define
+define|#
+directive|define
+name|ARCHIVE_STAT_CTIME_NANOS
+parameter_list|(
+name|st
+parameter_list|)
+value|(st)->st_ctime_n
+end_define
+
+begin_define
+define|#
+directive|define
+name|ARCHIVE_STAT_MTIME_NANOS
+parameter_list|(
+name|st
+parameter_list|)
+value|(st)->st_mtime_n
+end_define
+
+begin_elif
+elif|#
+directive|elif
+name|HAVE_STRUCT_STAT_ST_UMTIME
+end_elif
+
+begin_define
+define|#
+directive|define
+name|ARCHIVE_STAT_CTIME_NANOS
+parameter_list|(
+name|st
+parameter_list|)
+value|(st)->st_uctime * 1000
+end_define
+
+begin_define
+define|#
+directive|define
+name|ARCHIVE_STAT_MTIME_NANOS
+parameter_list|(
+name|st
+parameter_list|)
+value|(st)->st_umtime * 1000
+end_define
+
+begin_elif
+elif|#
+directive|elif
+name|HAVE_STRUCT_STAT_ST_MTIME_USEC
+end_elif
+
+begin_define
+define|#
+directive|define
+name|ARCHIVE_STAT_CTIME_NANOS
+parameter_list|(
+name|st
+parameter_list|)
+value|(st)->st_ctime_usec * 1000
+end_define
+
+begin_define
+define|#
+directive|define
+name|ARCHIVE_STAT_MTIME_NANOS
+parameter_list|(
+name|st
+parameter_list|)
+value|(st)->st_mtime_usec * 1000
+end_define
+
 begin_else
 else|#
 directive|else
@@ -559,11 +632,6 @@ name|st
 parameter_list|)
 value|(0)
 end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_endif
 endif|#
