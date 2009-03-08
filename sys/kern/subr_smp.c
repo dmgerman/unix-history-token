84cb2323350c90e71fb680b3f0c724a29b90b9b4
Dmitry Chagin <dchagin@FreeBSD.org>
2009-03-08 11:12:23 +0000

    MFC r189232, r189313: Fix range-check error introduced in r182292. Panic in case the ncpus == 0. it helps to catch bugs in the callers.
    Approved by:	kib (mentor)
    
    
    Former-commit-id: b8c454ab9be24efa4bf06f35ad087e271f2ea357

--- sys/kern/subr_smp.c
+++ sys/kern/subr_smp.c
@@ -1301,7 +1301,7 @@ operator|=
 literal|0
 init|;
 name|i
-operator|<
+operator|<=
 name|mp_maxid
 condition|;
 name|i
@@ -1329,6 +1329,19 @@ argument_list|)
 condition|)
 name|ncpus
 operator|++
+expr_stmt|;
+if|if
+condition|(
+name|ncpus
+operator|==
+literal|0
+condition|)
+name|panic
+argument_list|(
+literal|"ncpus is 0 with map=0x%x"
+argument_list|,
+name|map
+argument_list|)
 expr_stmt|;
 comment|/* obtain rendezvous lock */
 name|mtx_lock_spin
