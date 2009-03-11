ceb0b37a60fe11b23ad34b9eb41cdc61e04709d1
Konstantin Belousov <kib@FreeBSD.org>
2009-03-11 19:45:52 +0000

    Do not double-free the struct inode when insmntque failed. Default insmntque destructor reclaims the vnode, and ufs_reclaim frees the memory.
    Reviewed by:	tegge
    MFC after:	3 days
    
    
    Former-commit-id: 49a3f8f6bc5b00cb6f3a40b3c231882be167b796

--- sys/ufs/ffs/ffs_vfsops.c
+++ sys/ufs/ffs/ffs_vfsops.c
@@ -7335,13 +7335,6 @@ operator|!=
 literal|0
 condition|)
 block|{
-name|uma_zfree
-argument_list|(
-name|uma_inode
-argument_list|,
-name|ip
-argument_list|)
-expr_stmt|;
 operator|*
 name|vpp
 operator|=
