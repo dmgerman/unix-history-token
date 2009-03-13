07107301f04bbb46e6789bc324f481d009b61794
Rafal Jaworowski <raj@FreeBSD.org>
2009-03-13 06:28:20 +0000

    Make MPC85xx LAW handling and reset routines aware of the MPC8548 variant.
    Inspired by discussion with Alexey V Fedorov on freebsd-powerpc@.
    
    
    Former-commit-id: f666ae83e1b506489959276c98cfb2afd8a6250c

--- sys/powerpc/mpc85xx/mpc85xx.h
+++ sys/powerpc/mpc85xx/mpc85xx.h
@@ -70,6 +70,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|int
+name|law_getmax
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
