d6b2a105dd64a0b57a0a3bbf7bd35f33acc21f50
Robert N. M. Watson <rwatson@FreeBSD.org>
2009-03-22 22:07:52 +0000

    Add dtnfsclient, a first cut at an NFSv2/v3 client reuest DTrace provider.  The NFS client exposes 'start' and 'done' probes for NFSv2 and NFSv3 RPCs when using the new RPC implementation, passing in the vnode, mbuf chain, credential, and NFSv2 or NFSv3 procedure number. For 'done' probes, the error number is also available.
    Probes are named in the following way:
    
      ...
      nfsclient:nfs2:write:start
      nfsclient:nfs2:write:done
      ...
      nfsclient:nfs3:access:start
      nfsclient:nfs3:access:done
      ...
    
    Access to the unmarshalled arguments is not easily available at this
    point in the stack, but the passed probe arguments are sufficient to
    to a lot of interesting things in practice.  Technically, these probes
    may cover multiple RPC retransmits, and even transactions if the
    transaction ID change as a result of authentication failure or a
    jukebox error from the server, but usefully capture the intent of a
    single NFS request, such as access, getattr, write, etc.
    
    Typical use might involve profiling RPC latency by system call, number
    of RPCs, how often a getattr leads to a call to access, when failed
    access control checks occur, etc.  More detailed RPC information might
    best be provided by adding a krpc provider.  It would also be useful
    to add NFS client probes for events such as the access cache or
    attribute cache satisfying requests without an RPC.
    
    Sponsored by:	Google, Inc.
    MFC after:	1 month
    
    
    Former-commit-id: e3ff753f7338057b44eabbdd600ecd0422d9c1a3

--- sys/modules/dtrace/dtraceall/dtraceall.c
+++ sys/modules/dtrace/dtraceall/dtraceall.c
@@ -190,6 +190,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|dtraceall
+argument_list|,
+name|dtnfsclient
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_if
 if|#
 directive|if
