7efe31ee6deeaeafa157c31c5b409fff6cd59a02
Warner Losh <imp@FreeBSD.org>
2009-03-31 02:50:41 +0000

    Go back to filtering all PHY addresses above 16 since at least two cards still have issues with them.  Maybe this is a silicon rev?  In any case, doing the filtering only for the AX88790 for the moment.
    Former-commit-id: d4aae0ba027560f144b46a6917fe90bb09c16052

--- sys/dev/ed/if_ed_pccard.c
+++ sys/dev/ed/if_ed_pccard.c
@@ -5648,7 +5648,7 @@ argument_list|(
 name|dev
 argument_list|)
 expr_stmt|;
-comment|/* 	 * The AX88790 has an interesting quirk.  It has an internal phy that 	 * needs a special bit set to access, but can also have additional 	 * external PHYs set for things like HomeNET media.  When accessing 	 * the internal PHY, a bit has to be set, when accessing the external 	 * PHYs, it must be clear.  See Errata 1, page 51, in the AX88790 	 * datasheet for more details. 	 */
+comment|/* 	 * The AX88790 has an interesting quirk.  It has an internal phy that 	 * needs a special bit set to access, but can also have additional 	 * external PHYs set for things like HomeNET media.  When accessing 	 * the internal PHY, a bit has to be set, when accessing the external 	 * PHYs, it must be clear.  See Errata 1, page 51, in the AX88790 	 * datasheet for more details. 	 * 	 * Also, PHYs above 16 appear to be phantoms on some cards, but not 	 * others.  Registers read for this are often the same as prior values 	 * read.  Filter all register requests to 17-31. 	 */
 if|if
 condition|(
 name|sc
@@ -5658,6 +5658,17 @@ operator|==
 name|ED_CHIP_TYPE_AX88790
 condition|)
 block|{
+if|if
+condition|(
+name|phy
+operator|>
+literal|0x10
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 if|if
 condition|(
 name|phy
@@ -5864,7 +5875,7 @@ argument_list|(
 name|dev
 argument_list|)
 expr_stmt|;
-comment|/* 	 * The AX88790 has an interesting quirk.  It has an internal phy that 	 * needs a special bit set to access, but can also have additional 	 * external PHYs set for things like HomeNET media.  When accessing 	 * the internal PHY, a bit has to be set, when accessing the external 	 * PHYs, it must be clear.  See Errata 1, page 51, in the AX88790 	 * datasheet for more details. 	 */
+comment|/* See ed_miibus_readreg for details */
 if|if
 condition|(
 name|sc
@@ -5874,6 +5885,17 @@ operator|==
 name|ED_CHIP_TYPE_AX88790
 condition|)
 block|{
+if|if
+condition|(
+name|phy
+operator|>
+literal|0x10
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 if|if
 condition|(
 name|phy
