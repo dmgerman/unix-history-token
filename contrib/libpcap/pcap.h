335746f471897ee130fd5deac4def79554aaa584
Rui Paulo <rpaulo@FreeBSD.org>
2009-04-02 13:04:17 +0000

    Restore local change to include <sys/bpf.h> inside pcap.h. This fixes remaining ports build problems.
    Former-commit-id: d1bcae00e4f15190590d6eebcf23c1bc9c5a24f9

--- contrib/libpcap/pcap.h
+++ contrib/libpcap/pcap.h
@@ -85,22 +85,11 @@ begin_comment
 comment|/* WIN32/MSDOS/UN*X */
 end_comment
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|PCAP_DONT_INCLUDE_PCAP_BPF_H
-end_ifndef
-
 begin_include
 include|#
 directive|include
-file|<pcap-bpf.h>
+file|<net/bpf.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
