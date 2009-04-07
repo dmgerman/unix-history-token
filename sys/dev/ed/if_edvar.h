4afa6f1d3ee0db1c3f7d238798d85eb66953c696
Warner Losh <imp@FreeBSD.org>
2009-04-07 05:41:38 +0000

    Provide a generic ifmedia set of routines as a fallback.  The DP8390-based cards have no generic way of reporting status of the link or setting the media type.  Some specific versions of these cards do, however, allow for this, and we already support some of them.  Make the 'ed' experience more uniform by providing "autoselect" as the meida and status "active" always.  This won't affect the chips that provide more specific details.
    Former-commit-id: 9765c52202c5db7d16f028de863a26b705138bda

--- sys/dev/ed/if_edvar.h
+++ sys/dev/ed/if_edvar.h
@@ -1056,6 +1056,17 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|ed_gen_ifmedia_init
+parameter_list|(
+name|struct
+name|ed_softc
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_decl_stmt
 name|driver_intr_t
 name|edintr
