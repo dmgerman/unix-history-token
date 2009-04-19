78a090386c27a534c105196db3c6b714e03bf879
Robert N. M. Watson <rwatson@FreeBSD.org>
2009-04-19 15:18:16 +0000

    Vendor import of OpenBSM 1.1, which incorporates the following changes since the last imported OpenBSM release:
    OpenBSM 1.1
    
    - Change auditon(2) parameters and data structures to be 32/64-bit architecture
      independent.  Add more information to man page about auditon(2) parameters.
    - Add wrapper functions for auditon(2) to use legacy commands when the new
      commands are not supported.
    - Add default for 'expire-after' in audit_control to expire trail files when
      the audit directory is more than 10 megabytes ('10M').
    - Interface to convert between local and BSM fcntl(2) command values has been
      added:  au_bsm_to_fcntl_cmd(3) and au_fcntl_cmd_to_bsm(3), along with
      definitions of constants in audit_fcntl.h.
    - A bug, introduced in OpenBSM 1.1 alpha 4, in which AUT_RETURN32 tokens
      generated by audit_submit(3) were improperly encoded has been fixed.
    - Fix example in audit_submit(3) man page.  Also, make it clear that we want
      the audit ID as the argument.
    - A new audit event class 'aa', for post-login authentication and
      authorization events, has been added.
    
    Obtained from:	TrustedBSD Project
    Sponsored by:	Apple, Inc.
    
    
    Former-commit-id: 01fc0b312fa17d64ee75ec14fed31a49f289ad80

--- libbsm/bsm_control.c
+++ libbsm/bsm_control.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2004,2009 Apple Inc.  * Copyright (c) 2006 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_control.c#28 $  */
+comment|/*-  * Copyright (c) 2004, 2009 Apple Inc.  * Copyright (c) 2006 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_control.c#33 $  */
 end_comment
 
 begin_include
@@ -95,6 +95,12 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_include
+include|#
+directive|include
+file|<sys/stat.h>
+end_include
 
 begin_comment
 comment|/*  * Parse the contents of the audit_control file to return the audit control  * parameters.  These static fields are protected by 'mutex'.  */
@@ -776,7 +782,7 @@ begin_function
 name|ssize_t
 name|au_poltostr
 parameter_list|(
-name|long
+name|int
 name|policy
 parameter_list|,
 name|size_t
@@ -919,7 +925,7 @@ name|char
 modifier|*
 name|polstr
 parameter_list|,
-name|long
+name|int
 modifier|*
 name|policy
 parameter_list|)
@@ -1088,6 +1094,16 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
+specifier|static
+name|time_t
+name|lastctime
+init|=
+literal|0
+decl_stmt|;
+name|struct
+name|stat
+name|sbuf
+decl_stmt|;
 name|ptrmoved
 operator|=
 literal|1
@@ -1098,6 +1114,54 @@ name|fp
 operator|!=
 name|NULL
 condition|)
+block|{
+comment|/* 		 * Check to see if the file on disk has changed.  If so, 		 * force a re-read of the file by closing it. 		 */
+if|if
+condition|(
+name|fstat
+argument_list|(
+name|fileno
+argument_list|(
+name|fp
+argument_list|)
+argument_list|,
+operator|&
+name|sbuf
+argument_list|)
+operator|<
+literal|0
+condition|)
+goto|goto
+name|closefp
+goto|;
+if|if
+condition|(
+name|lastctime
+operator|!=
+name|sbuf
+operator|.
+name|st_ctime
+condition|)
+block|{
+name|lastctime
+operator|=
+name|sbuf
+operator|.
+name|st_ctime
+expr_stmt|;
+name|closefp
+label|:
+name|fclose
+argument_list|(
+name|fp
+argument_list|)
+expr_stmt|;
+name|fp
+operator|=
+name|NULL
+expr_stmt|;
+return|return;
+block|}
 name|fseek
 argument_list|(
 name|fp
@@ -1107,6 +1171,7 @@ argument_list|,
 name|SEEK_SET
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 end_function
 
