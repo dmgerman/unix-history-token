f62bf6af39e0150b2564a34d18de004b2930f930
Colin Percival <cperciva@FreeBSD.org>
2009-04-22 14:07:14 +0000

    Don't leak information via uninitialized space in db(3) records. [09:07]
    Sanity-check string lengths in order to stop OpenSSL crashing
    when printing corrupt BMPString or UniversalString objects. [09:08]
    
    Security:	FreeBSD-SA-09:07.libc
    Security:	FreeBSD-SA-09:08.openssl
    Security:	CVE-2009-0590
    Approved by:	re (kensmith)
    Approved by:	so (cperciva)
    
    Former-commit-id: 099ac24cf8cbb5e259dadb4aa6fdcacc502f9834

--- crypto/openssl/crypto/asn1/asn1_err.c
+++ crypto/openssl/crypto/asn1/asn1_err.c
@@ -1102,6 +1102,15 @@ block|,
 block|{
 name|ERR_REASON
 argument_list|(
+name|ASN1_R_BMPSTRING_IS_WRONG_LENGTH
+argument_list|)
+block|,
+literal|"bmpstring is wrong length"
+block|}
+block|,
+block|{
+name|ERR_REASON
+argument_list|(
 name|ASN1_R_BN_LIB
 argument_list|)
 block|,
@@ -1808,6 +1817,15 @@ name|ASN1_R_UNEXPECTED_EOC
 argument_list|)
 block|,
 literal|"unexpected eoc"
+block|}
+block|,
+block|{
+name|ERR_REASON
+argument_list|(
+name|ASN1_R_UNIVERSALSTRING_IS_WRONG_LENGTH
+argument_list|)
+block|,
+literal|"universalstring is wrong length"
 block|}
 block|,
 block|{
