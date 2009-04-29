d7c5ed87576768ddc078da05a799420e732fa0ba
Jeff Roberson <jeff@FreeBSD.org>
2009-04-29 06:54:40 +0000

    - Add support for cpuid leaf 0xb.  This allows us to determine the    topology of nehalem/corei7 based systems.  - Remove the cpu_cores/cpu_logical detection from identcpu.  - Describe the layout of the system in cpu_mp_announce().
    Sponsored by:   Nokia
    
    
    Former-commit-id: c0f8a4d3d8c5fe41979e2f5b3fc4be69eb60db8f

--- sys/i386/include/smp.h
+++ sys/i386/include/smp.h
@@ -200,24 +200,6 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* global data in identcpu.c */
-end_comment
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|cpu_cores
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-name|cpu_logical
-decl_stmt|;
-end_decl_stmt
-
-begin_comment
 comment|/* IPI handlers */
 end_comment
 
