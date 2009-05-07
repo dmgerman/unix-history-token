f9ee04faee76f0211f901aaf576e664f6bde8d54
Dmitry Chagin <dchagin@FreeBSD.org>
2009-05-07 10:01:05 +0000

    Add preliminary KTR(9) support to the linux emulation layer.
    Approved by:	kib (mentor)
    MFC after:	1 month
    
    
    Former-commit-id: 699800837f8dcf9f5f29f815f7a9bb010c7f1379

--- sys/compat/linux/linux_mib.c
+++ sys/compat/linux/linux_mib.c
@@ -1460,11 +1460,19 @@ return|;
 block|}
 end_function
 
-begin_ifdef
-ifdef|#
-directive|ifdef
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
 name|DEBUG
-end_ifdef
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|KTR
+argument_list|)
+end_if
 
 begin_decl_stmt
 name|u_char
@@ -1773,7 +1781,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* DEBUG */
+comment|/* DEBUG || KTR */
 end_comment
 
 end_unit
