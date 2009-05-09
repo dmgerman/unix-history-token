31acf7c171c597c6a9b7c2587fc5a565567de70d
Ed Schouten <ed@FreeBSD.org>
2009-05-09 15:09:40 +0000

    Add support for old TTY ioctls to kdump.
    At first I allowed ioctl_compat.h to be included, but it just returned
    an empty file. I had to do this, to keep kdump happy. I really want to
    raise a compiler error when including this header, so now it will just
    throw an error if you don't set COMPAT_43TTY.
    
    
    Former-commit-id: 270555aa2bb5e0f2a53254041c36a609cff6dc41

--- sys/sys/ioctl_compat.h
+++ sys/sys/ioctl_compat.h
@@ -15,12 +15,6 @@ directive|define
 name|_SYS_IOCTL_COMPAT_H_
 end_define
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|_KERNEL
-end_ifdef
-
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -875,15 +869,6 @@ end_define
 
 begin_comment
 comment|/* for hp300 -- sans int arg */
-end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* _KERNEL */
 end_comment
 
 begin_endif
