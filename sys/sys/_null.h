eb5de9de0b087508df71226691717abae029851f
John Baldwin <jhb@FreeBSD.org>
2009-05-18 17:39:12 +0000

    MFC: Use __null for NULL for GNU C++ 4.x and use the same value for NULL in the kernel as in userland.
    Former-commit-id: 498f916c7b741195772b4af02c6f662fcc3ebfce

--- sys/sys/_null.h
+++ sys/sys/_null.h
@@ -12,11 +12,6 @@ end_ifndef
 begin_if
 if|#
 directive|if
-name|defined
-argument_list|(
-name|_KERNEL
-argument_list|)
-operator|||
 operator|!
 name|defined
 argument_list|(
@@ -29,6 +24,36 @@ define|#
 directive|define
 name|NULL
 value|((void *)0)
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__GNUG__
+argument_list|)
+operator|&&
+name|defined
+argument_list|(
+name|__GNUC__
+argument_list|)
+operator|&&
+name|__GNUC__
+operator|>=
+literal|4
+end_if
+
+begin_define
+define|#
+directive|define
+name|NULL
+value|__null
 end_define
 
 begin_else
@@ -79,7 +104,16 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* _KERNEL || !__cplusplus */
+comment|/* __GNUG__ */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* !__cplusplus */
 end_comment
 
 begin_endif
