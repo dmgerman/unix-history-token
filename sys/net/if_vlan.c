a9d97d4be122d8e797b84ea7fafa30dc166d460c
John Baldwin <jhb@FreeBSD.org>
2009-05-18 21:58:57 +0000

    MFC: Remove dependency of vlan.ko on miibus.ko.
    Former-commit-id: fa5d534ff0d923cb597aff98be6272efd42e4f3a

--- sys/net/if_vlan.c
+++ sys/net/if_vlan.c
@@ -1421,22 +1421,6 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
-begin_expr_stmt
-name|MODULE_DEPEND
-argument_list|(
-name|if_vlan
-argument_list|,
-name|miibus
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
 begin_function
 specifier|static
 name|struct
