4e98e6dd3adca5156c1837eeed844bf0824ac491
Scott Long <scottl@FreeBSD.org>
2009-05-19 01:41:11 +0000

    Updated PCI ID's from the vendor
    Former-commit-id: 1b134e084ea64ec67bae3d8fe1ff833f58fe1112

--- sys/dev/ciss/ciss.c
+++ sys/dev/ciss/ciss.c
@@ -1888,7 +1888,7 @@ literal|0x3237
 block|,
 name|CISS_BOARD_SA5
 block|,
-literal|"HP Smart Array"
+literal|"HP Smart Array E500"
 block|}
 block|,
 block|{
@@ -1944,6 +1944,16 @@ block|,
 block|{
 literal|0x103C
 block|,
+literal|0x323D
+block|,
+name|CISS_BOARD_SA5
+block|,
+literal|"HP Smart Array P700m"
+block|}
+block|,
+block|{
+literal|0x103C
+block|,
 literal|0x3241
 block|,
 name|CISS_BOARD_SA5
@@ -1989,6 +1999,26 @@ block|,
 name|CISS_BOARD_SA5
 block|,
 literal|"HP Smart Array P812"
+block|}
+block|,
+block|{
+literal|0x103C
+block|,
+literal|0x324A
+block|,
+name|CISS_BOARD_SA5
+block|,
+literal|"HP Smart Array P712m"
+block|}
+block|,
+block|{
+literal|0x103C
+block|,
+literal|0x324B
+block|,
+name|CISS_BOARD_SA5
+block|,
+literal|"HP Smart Array"
 block|}
 block|,
 block|{
