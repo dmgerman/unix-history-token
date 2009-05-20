97b33878c04adc5c69be363d07e36555a8dc2b5b
Kip Macy <kmacy@FreeBSD.org>
2009-05-20 22:27:18 +0000

    add vdestroy declaration now that it is no longer static - needed by ZFS
    Former-commit-id: 9d07193ec2d9d38bf8692c70d7a1c2859414ac5e

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -4167,6 +4167,18 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|void
+name|vdestroy
+parameter_list|(
+name|struct
+name|vnode
+modifier|*
+name|vp
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|int
 name|vnode_create_vobject
 parameter_list|(
