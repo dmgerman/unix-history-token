d3eb1df19104cca01dc4e58b10643062cbecdc28
Rick Macklem <rmacklem@FreeBSD.org>
2009-05-22 01:15:07 +0000

    Fix the experimental nfs server so that it depends on the nlm, since it now calls nlm_acquire_next_sysid().
    Approved by:	kib (mentor)
    
    
    Former-commit-id: 40c786920e70fb940dd6838df81c02b2d427e79c

--- sys/fs/nfsserver/nfs_nfsdport.c
+++ sys/fs/nfsserver/nfs_nfsdport.c
@@ -15840,5 +15840,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|nfsd
+argument_list|,
+name|nfslockd
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
