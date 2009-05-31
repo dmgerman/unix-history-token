c4ea8e77979b3a951f2552ce5620a3b4f4050de1
Dmitry Chagin <dchagin@FreeBSD.org>
2009-05-31 06:58:35 +0000

    MFC r191719:
    Reimplement futexes.
    Old implemention used Giant to protect the kernel data structures,
    but at the same time called malloc(M_WAITOK), that could cause the
    calling thread to sleep and lost Giant protection. User-visible
    result was the missed wakeup.
    
    New implementation uses one sx lock per futex. The sx protects
    the futex structures and allows to sleep while copyin or copyout
    are performed.
    
    Unlike linux, we return EINVAL when FUTEX_CMP_REQUEUE operation
    is requested and either caller specified futexes are equial or
    second futex already exists. This is acceptable since the situation
    can only occur from the application error, and glibc falls back to
    old FUTEX_WAKE operation when FUTEX_CMP_REQUEUE returns an error.
    
    Approved by:	kib (mentor)
    
    
    Former-commit-id: 697380cb4f5bc74abed4cbb914d3c72e27b020e3

--- sys/amd64/linux32/linux32_sysvec.c
+++ sys/amd64/linux32/linux32_sysvec.c
@@ -554,8 +554,8 @@ end_expr_stmt
 begin_decl_stmt
 specifier|extern
 name|struct
-name|sx
-name|futex_sx
+name|mtx
+name|futex_mtx
 decl_stmt|;
 end_decl_stmt
 
@@ -5807,12 +5807,16 @@ operator|&
 name|futex_list
 argument_list|)
 expr_stmt|;
-name|sx_init
+name|mtx_init
 argument_list|(
 operator|&
-name|futex_sx
+name|futex_mtx
 argument_list|,
-literal|"futex protection lock"
+literal|"ftllk"
+argument_list|,
+name|NULL
+argument_list|,
+name|MTX_DEF
 argument_list|)
 expr_stmt|;
 name|linux_exit_tag
@@ -6015,10 +6019,10 @@ operator|&
 name|emul_shared_lock
 argument_list|)
 expr_stmt|;
-name|sx_destroy
+name|mtx_destroy
 argument_list|(
 operator|&
-name|futex_sx
+name|futex_mtx
 argument_list|)
 expr_stmt|;
 name|EVENTHANDLER_DEREGISTER
