ca5ec2bac8c4bbeafdd02555b6e8b7609c565270
Bjoern A. Zeeb <bz@FreeBSD.org>
2009-06-10 11:19:34 +0000

    All consumers of in_cksum.h have been properly #ifdefed already, so do not include the file either as it would give as an extra dependency on INET.
    Former-commit-id: 6d82bc3aaf6899f96067fd4c3d2c63aa2fe70c63

--- sys/contrib/pf/net/if_pflog.c
+++ sys/contrib/pf/net/if_pflog.c
@@ -352,6 +352,12 @@ end_include
 begin_ifdef
 ifdef|#
 directive|ifdef
+name|INET
+end_ifdef
+
+begin_ifdef
+ifdef|#
+directive|ifdef
 name|__FreeBSD__
 end_ifdef
 
@@ -360,6 +366,11 @@ include|#
 directive|include
 file|<machine/in_cksum.h>
 end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_endif
 endif|#
