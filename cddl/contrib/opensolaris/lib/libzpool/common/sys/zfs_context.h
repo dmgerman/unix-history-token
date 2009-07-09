93d0d5ca9d681944ed23cd9784de61fa6683267b
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2009-07-09 20:22:05 +0000

    The mutex_owned() macro should operate on kmutex_t and not on mutex_t. This fixes 'zdb <poolname>' crash.
    Reported by:	avg
    Approved by:	re (kib)
    
    
    Former-commit-id: 591be35fd0840540bb133ae2ed51327368a34383

--- cddl/contrib/opensolaris/lib/libzpool/common/sys/zfs_context.h
+++ cddl/contrib/opensolaris/lib/libzpool/common/sys/zfs_context.h
@@ -759,6 +759,13 @@ parameter_list|(
 name|mp
 parameter_list|)
 value|zmutex_destroy((kmutex_t *)(mp))
+define|#
+directive|define
+name|mutex_owned
+parameter_list|(
+name|mp
+parameter_list|)
+value|zmutex_owned((kmutex_t *)(mp))
 specifier|extern
 name|void
 name|zmutex_init
@@ -771,6 +778,15 @@ function_decl|;
 specifier|extern
 name|void
 name|zmutex_destroy
+parameter_list|(
+name|kmutex_t
+modifier|*
+name|mp
+parameter_list|)
+function_decl|;
+specifier|extern
+name|int
+name|zmutex_owned
 parameter_list|(
 name|kmutex_t
 modifier|*
