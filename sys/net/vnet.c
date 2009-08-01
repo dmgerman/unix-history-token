23c02246e84e9c01eaf2b126024efdd4d1266439
Robert N. M. Watson <rwatson@FreeBSD.org>
2009-08-01 22:22:45 +0000

    The colour was red as shall be the letters of this warning to people upon boot if the experimental VIMAGE feature was compiled into the kernel.
    Submitted by:	bz
    Reviewed by:	zec
    Approved by:	re (vimage blanket)
    
    
    Former-commit-id: 98fec30ce6d75311a7e126fd7b1281c01a95437e

--- sys/net/vnet.c
+++ sys/net/vnet.c
@@ -727,6 +727,13 @@ modifier|*
 name|arg
 parameter_list|)
 block|{
+comment|/* Warn people before take off - in case we crash early. */
+name|printf
+argument_list|(
+literal|"WARNING: VIMAGE (virtualized network stack) is a highly "
+literal|"experimental feature.\n"
+argument_list|)
+expr_stmt|;
 comment|/* 	 * We MUST clear curvnet in vi_init_done() before going SMP, 	 * otherwise CURVNET_SET() macros would scream about unnecessary 	 * curvnet recursions. 	 */
 name|curvnet
 operator|=
