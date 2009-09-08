9e82d63bb51fc9c51855a5d50923ee2bfee7424c
Poul-Henning Kamp <phk@FreeBSD.org>
2009-09-08 12:59:56 +0000

    Move multi-include protection back up to the top of the file and name after the physical file rather than the aliased name.
    Former-commit-id: 6bfde3b8a35375a8886733e5d92979b7c7483224

--- sys/amd64/include/param.h
+++ sys/amd64/include/param.h
@@ -3,6 +3,18 @@ begin_comment
 comment|/*-  * Copyright (c) 2002 David E. O'Brien.  All rights reserved.  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department and Ralph Campbell.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	@(#)param.h	8.1 (Berkeley) 6/10/93  * $FreeBSD$  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_AMD64_INCLUDE_PARAM_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_AMD64_INCLUDE_PARAM_H_
+end_define
+
 begin_comment
 comment|/*  * Machine dependent constants for AMD64.  */
 end_comment
@@ -66,18 +78,6 @@ begin_define
 define|#
 directive|define
 name|__PCI_REROUTE_INTERRUPT
-end_define
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|_MACHINE_PARAM_H_
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|_MACHINE_PARAM_H_
 end_define
 
 begin_ifndef
@@ -585,7 +585,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_MACHINE_PARAM_H_ */
+comment|/* !_NO_NAMESPACE_POLLUTION */
 end_comment
 
 begin_endif
@@ -594,7 +594,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_NO_NAMESPACE_POLLUTION */
+comment|/* !_AMD64_INCLUDE_PARAM_H_ */
 end_comment
 
 end_unit
