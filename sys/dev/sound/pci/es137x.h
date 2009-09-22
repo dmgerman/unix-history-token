ef0eb56ae6592f8818c5bbf4cc9e0cb6d0758f5a
Joel Dahl <joel@FreeBSD.org>
2009-09-22 13:23:59 +0000

    Move es137x.c and es137x.h to a 2-clause BSD license.  Also move a few comments.
    Submitted by:	Joachim Kuebart
    Approved by:	core, Russell Cattelan <cattelan@thebarn.com>
    
    
    Former-commit-id: bf50a375a5c42915294c12000269845144c3a462

--- sys/dev/sound/pci/es137x.h
+++ sys/dev/sound/pci/es137x.h
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * This supports the ENSONIQ AudioPCI board based on the ES1370.  *  * Copyright (c) 1998 Joachim Kuebart<joki@kuebart.stuttgart.netsurf.de>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice immediately at the beginning of the file, without modification,  *    this list of conditions, and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Absolutely no warranty of function or purpose is made by the author  *    Joachim Kuebart.  * 4. Modifications may be freely made to this file if the above conditions  *    are met.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 1998 Joachim Kuebart<joachim.kuebart@gmx.net>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * $FreeBSD$  */
+end_comment
+
+begin_comment
+comment|/* This supports the ENSONIQ AudioPCI board based on the ES1370. */
 end_comment
 
 begin_ifndef
