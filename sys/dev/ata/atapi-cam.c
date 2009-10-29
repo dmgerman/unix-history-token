5e4baf0e0d5d3837f8fd1d669d03805be0fbf687
Alexander Motin <mav@FreeBSD.org>
2009-10-29 10:38:17 +0000

    MFC rev. 198487: Round timeout up when converting CAM milliseconds to ATA seconds.
    Former-commit-id: 6b6d26d33d4a5e88632b44c85b1b3927544e8544

--- sys/dev/ata/atapi-cam.c
+++ sys/dev/ata/atapi-cam.c
@@ -3376,13 +3376,16 @@ name|request
 operator|->
 name|timeout
 operator|=
+operator|(
 name|ccb_h
 operator|->
 name|timeout
+operator|+
+literal|999
+operator|)
 operator|/
 literal|1000
 expr_stmt|;
-comment|/* XXX lost granularity */
 name|request
 operator|->
 name|callback
@@ -3814,7 +3817,7 @@ block|{
 if|#
 directive|if
 literal|0
-block|static const int8_t ccb[16] = { ATAPI_REQUEST_SENSE, 0, 0, 0, 		    sizeof(struct atapi_sense), 0, 0, 0, 0, 0, 0, 		    0, 0, 0, 0, 0 };  		bcopy (ccb, request->u.atapi.ccb, sizeof ccb); 		request->data = (caddr_t)&csio->sense_data; 		request->bytecount = sizeof(struct atapi_sense); 		request->transfersize = min(request->bytecount, 65534); 		request->timeout = csio->ccb_h.timeout / 1000; 		request->retries = 2; 		request->flags = ATA_R_QUIET|ATA_R_ATAPI|ATA_R_IMMEDIATE; 		hcb->flags |= AUTOSENSE;  		ata_queue_request(request); 		return;
+block|static const int8_t ccb[16] = { ATAPI_REQUEST_SENSE, 0, 0, 0, 		    sizeof(struct atapi_sense), 0, 0, 0, 0, 0, 0, 		    0, 0, 0, 0, 0 };  		bcopy (ccb, request->u.atapi.ccb, sizeof ccb); 		request->data = (caddr_t)&csio->sense_data; 		request->bytecount = sizeof(struct atapi_sense); 		request->transfersize = min(request->bytecount, 65534); 		request->timeout = (csio->ccb_h.timeout + 999) / 1000; 		request->retries = 2; 		request->flags = ATA_R_QUIET|ATA_R_ATAPI|ATA_R_IMMEDIATE; 		hcb->flags |= AUTOSENSE;  		ata_queue_request(request); 		return;
 else|#
 directive|else
 comment|/* 		 * Use auto-sense data from the ATA layer, if it has 		 * issued a REQUEST SENSE automatically and that operation 		 * returned without error. 		 */
