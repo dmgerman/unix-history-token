a74efcb176dba6ef6b5e58907c03169fb958a1fc
John Baldwin <jhb@FreeBSD.org>
2009-10-29 16:00:27 +0000

    MFC 197439: Extract the code to find and map the MADT ACPI table during early kernel startup and genericize it so it can be reused to map other tables as well: - Add a routine to walk a list of ACPI subtables such as those used in the   APIC and SRAT tables in the MI acpi(4) driver. - Move the routines for mapping and unmapping an ACPI table as well as   mapping the RSDT or XSDT and searching for a table with a given signature   out into acpica_machdep.c for both amd64 and i386.
    Former-commit-id: 850058c8cdbabe6961e8f52bee7d986a3b7b54c7

--- sys/dev/acpica/acpivar.h
+++ sys/dev/acpica/acpivar.h
@@ -1091,6 +1091,24 @@ define|\
 value|ACPI_DEVINFO_PRESENT(x, ACPI_STA_PRESENT | ACPI_STA_FUNCTIONAL | \ 	    ACPI_STA_BATT_PRESENT)
 end_define
 
+begin_comment
+comment|/* Callback function type for walking subtables within a table. */
+end_comment
+
+begin_typedef
+typedef|typedef
+name|void
+name|acpi_subtable_handler
+parameter_list|(
+name|ACPI_SUBTABLE_HEADER
+modifier|*
+parameter_list|,
+name|void
+modifier|*
+parameter_list|)
+function_decl|;
+end_typedef
+
 begin_function_decl
 name|BOOLEAN
 name|acpi_DeviceIsPresent
@@ -1439,6 +1457,29 @@ name|res
 parameter_list|,
 name|u_int
 name|flags
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|acpi_walk_subtables
+parameter_list|(
+name|void
+modifier|*
+name|first
+parameter_list|,
+name|void
+modifier|*
+name|end
+parameter_list|,
+name|acpi_subtable_handler
+modifier|*
+name|handler
+parameter_list|,
+name|void
+modifier|*
+name|arg
 parameter_list|)
 function_decl|;
 end_function_decl
