27a74a356b7221b9d6ce7718dba268742705bbd7
Alan Cox <alc@FreeBSD.org>
2009-11-04 03:12:56 +0000

    Eliminate an unnecessary #include.  (This #include should have been removed in r188331 when vnode_pager_lock() was eliminated.)
    Former-commit-id: 053313bb6a1a6422687556d69fe217fe6fb455a8

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -144,12 +144,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<vm/vnode_pager.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<vm/vm_extern.h>
 end_include
 
