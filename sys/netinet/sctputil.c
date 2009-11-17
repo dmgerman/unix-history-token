5942aaa1ffc5efcdd5e6078da6be5cd96e7e5c32
Michael Tuexen <tuexen@FreeBSD.org>
2009-11-17 13:05:51 +0000

    Do not hold the lock longer than necessary.
    Approved by: rrs (mentor)
    MFC after: 1 month
    
    
    Former-commit-id: 1a32578e6a049c90ff79b72083b96e1174048076

--- sys/netinet/sctputil.c
+++ sys/netinet/sctputil.c
@@ -30946,6 +30946,9 @@ argument_list|,
 name|sctp_nxt_addr
 argument_list|)
 expr_stmt|;
+name|SCTP_IPI_ITERATOR_WQ_UNLOCK
+argument_list|()
+expr_stmt|;
 name|sctp_timer_start
 argument_list|(
 name|SCTP_TIMER_TYPE_ADDR_WQ
@@ -30971,9 +30974,6 @@ operator|*
 operator|)
 name|NULL
 argument_list|)
-expr_stmt|;
-name|SCTP_IPI_ITERATOR_WQ_UNLOCK
-argument_list|()
 expr_stmt|;
 return|return
 operator|(
