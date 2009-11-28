2aecf4e87b884771d1b2978898f44f06369b14c6
Ed Schouten <ed@FreeBSD.org>
2009-11-28 11:13:50 +0000

    Remove unneeded includes of <sys/termios.h>.
    Former-commit-id: ae9e243d40d6ba7e4f7a675744a67113ddb3f348

--- sys/dev/uart/uart_core.c
+++ sys/dev/uart/uart_core.c
@@ -121,12 +121,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/termios.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/resource.h>
 end_include
 
