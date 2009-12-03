2008e2df1b95ead8f9fdbae38963240c69fe5316
Edward Tomasz Napierala <trasz@FreeBSD.org>
2009-12-03 13:29:24 +0000

    Add change that was somehow missed in r192586.  It could manifest by incorrectly returning EINVAL from acl_valid(3) for applications linked against pre-8.0 libc.
    Former-commit-id: 4b19132d51be6687cecbe49c67936d2e93de2b6e

--- sys/kern/vfs_acl.c
+++ sys/kern/vfs_acl.c
@@ -731,7 +731,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Convert "old" type - ACL_TYPE_{ACCESS,DEFAULT}_OLD - into its "new"  * counterpart.  It's required for old (pre-NFS4 ACLs) libc to work  * with new kernel.  Fixing 'type' for old binaries with new libc  * is being done in lib/libc/posix1e/acl_support.c:_acl_type_unold().  */
+comment|/*  * Convert "old" type - ACL_TYPE_{ACCESS,DEFAULT}_OLD - into its "new"  * counterpart.  It's required for old (pre-NFSv4 ACLs) libc to work  * with new kernel.  Fixing 'type' for old binaries with new libc  * is being done in lib/libc/posix1e/acl_support.c:_acl_type_unold().  */
 end_comment
 
 begin_function
@@ -1312,7 +1312,10 @@ name|VOP_ACLCHECK
 argument_list|(
 name|vp
 argument_list|,
+name|acl_type_unold
+argument_list|(
 name|type
+argument_list|)
 argument_list|,
 name|inkernelacl
 argument_list|,
