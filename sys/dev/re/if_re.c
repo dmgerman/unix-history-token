50a91bcdc9f10232e7c01a4f13a39ec1df3fff9a
Pyun YongHyeon <yongari@FreeBSD.org>
2009-12-03 18:50:04 +0000

    MFC r199565:   Move interface reinitialization down after disabling WOL in resume   path.
    Former-commit-id: 3abadc307fdc890303cd9f1efa3e3acf4ad5d1ae

--- sys/dev/re/if_re.c
+++ sys/dev/re/if_re.c
@@ -13686,6 +13686,12 @@ literal|0x01
 argument_list|)
 expr_stmt|;
 block|}
+comment|/* 	 * Clear WOL matching such that normal Rx filtering 	 * wouldn't interfere with WOL patterns. 	 */
+name|re_clrwol
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 comment|/* reinitialize interface if necessary */
 if|if
 condition|(
@@ -13696,12 +13702,6 @@ operator|&
 name|IFF_UP
 condition|)
 name|re_init_locked
-argument_list|(
-name|sc
-argument_list|)
-expr_stmt|;
-comment|/* 	 * Clear WOL matching such that normal Rx filtering 	 * wouldn't interfere with WOL patterns. 	 */
-name|re_clrwol
 argument_list|(
 name|sc
 argument_list|)
