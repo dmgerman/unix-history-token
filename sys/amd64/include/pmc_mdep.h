632449b1f6388bb1141a8ada6db8b3c93f32c68c
Fabien Thomas <fabient@FreeBSD.org>
2009-12-03 19:37:52 +0000

    MFC 179886: Complete the merge after hwpmc backport.
    Make preparations for increasing the size of the kernel virtual
    address space on the amd64 architecture.  The amd64 architecture
    requires kernel code and global variables to reside in the highest 2GB
    of the 64-bit virtual address space.  Thus, KERNBASE cannot change.
    However, KERNBASE is sometimes used as the start of the kernel virtual
    address space.  Henceforth, VM_MIN_KERNEL_ADDRESS should be used
    instead.  Since KERNBASE and VM_MIN_KERNEL_ADDRESS are still the same
    address, there should be no visible effect from this change (yet).
    
    
    Former-commit-id: 4e942142852929ec70a7e3fb94cc5c833047ed16

--- sys/amd64/include/pmc_mdep.h
+++ sys/amd64/include/pmc_mdep.h
@@ -300,7 +300,7 @@ name|PMC_IN_KERNEL
 parameter_list|(
 name|va
 parameter_list|)
-value|(((va)>= DMAP_MIN_ADDRESS&&		\ 	(va)< DMAP_MAX_ADDRESS) || ((va)>= KERNBASE&&	\ 	(va)< VM_MAX_KERNEL_ADDRESS))
+value|(((va)>= DMAP_MIN_ADDRESS&&			\ 	(va)< DMAP_MAX_ADDRESS) || ((va)>= VM_MIN_KERNEL_ADDRESS&&	\ 	(va)< VM_MAX_KERNEL_ADDRESS))
 end_define
 
 begin_define
