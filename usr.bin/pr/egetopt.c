66dc327fb1b184533083b1fec219178a6c483ca8
Xin Li <delphij@FreeBSD.org>
2009-12-13 04:34:51 +0000

    egetopt.c does not use any stdlib.h definations, nor it referenced any symbol from other module, so remove reference of stdlib.h and extern.h.
    Verified with:	md5(1)
    
    
    Former-commit-id: abeb78fbc4d5eeea6a4a2453414a4008c3b32ad3

--- usr.bin/pr/egetopt.c
+++ usr.bin/pr/egetopt.c
@@ -59,19 +59,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<stdlib.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<string.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|"extern.h"
 end_include
 
 begin_comment
