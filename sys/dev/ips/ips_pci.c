0290318413f669928b30552b548e26eb13f6c140
Matthew Jacob <mjacob@FreeBSD.org>
2009-12-17 00:21:12 +0000

    Fix argument order in a call to mtx_init.
    MFC after:	1 week
    
    
    Former-commit-id: 7d9ec50cd1b768f546f1b910916a88ff821d7999

--- sys/dev/ips/ips_pci.c
+++ sys/dev/ips/ips_pci.c
@@ -768,9 +768,9 @@ name|queue_mtx
 argument_list|,
 literal|"IPS bioqueue lock"
 argument_list|,
-name|MTX_DEF
+name|NULL
 argument_list|,
-literal|0
+name|MTX_DEF
 argument_list|)
 expr_stmt|;
 name|sema_init
