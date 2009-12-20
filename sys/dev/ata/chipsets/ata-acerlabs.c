407df1f6515be77b1f6a9963babdb8aaaf7e3463
Marius Strobl <marius@FreeBSD.org>
2009-12-20 01:44:47 +0000

    MFC: r200482, r200485
    o Properly support M5229 revision 0xc7 and 0xc8:
      - These revisions no longer have cable detection capability.
      - The UDMA support bit of register 0x4b has been dropped without an
        replacement.
      - According to Linux it's crucial for working ATAPI DMA support to
        also set the reserved bit 1 of regsiter 0x53 with these revisions.
    o Only set ATA_CHECKS_CABLE for chip versions that actually support
      cable detection, i.e. neither for ALI_OLD nor for ALI_NEW revisions
      >= 0xc7.
    
    
    Former-commit-id: b62e87f2d8feb717a05969d70f728927c640d30f

--- sys/dev/ata/chipsets/ata-acerlabs.c
+++ sys/dev/ata/chipsets/ata-acerlabs.c
@@ -742,7 +742,17 @@ argument_list|,
 literal|1
 argument_list|)
 expr_stmt|;
-comment|/* enable cable detection and UDMA support on newer chips */
+comment|/* enable cable detection and UDMA support on revisions< 0xc7 */
+if|if
+condition|(
+name|ctlr
+operator|->
+name|chip
+operator|->
+name|chiprev
+operator|<
+literal|0xc7
+condition|)
 name|pci_write_config
 argument_list|(
 name|dev
@@ -763,7 +773,7 @@ argument_list|,
 literal|1
 argument_list|)
 expr_stmt|;
-comment|/* enable ATAPI UDMA mode */
+comment|/* enable ATAPI UDMA mode (even if we are going to do PIO) */
 name|pci_write_config
 argument_list|(
 name|dev
@@ -779,7 +789,19 @@ argument_list|,
 literal|1
 argument_list|)
 operator||
+operator|(
+name|ctlr
+operator|->
+name|chip
+operator|->
+name|chiprev
+operator|>=
+literal|0xc7
+condition|?
+literal|0x03
+else|:
 literal|0x01
+operator|)
 argument_list|,
 literal|1
 argument_list|)
@@ -909,6 +931,24 @@ condition|)
 return|return
 name|ENXIO
 return|;
+if|if
+condition|(
+name|ctlr
+operator|->
+name|chip
+operator|->
+name|cfg2
+operator|&
+name|ALI_NEW
+operator|&&
+name|ctlr
+operator|->
+name|chip
+operator|->
+name|chiprev
+operator|<
+literal|0xc7
+condition|)
 name|ch
 operator|->
 name|flags
@@ -1558,6 +1598,14 @@ operator|->
 name|cfg2
 operator|&
 name|ALI_NEW
+operator|&&
+name|ctlr
+operator|->
+name|chip
+operator|->
+name|chiprev
+operator|<
+literal|0xc7
 condition|)
 block|{
 if|if
