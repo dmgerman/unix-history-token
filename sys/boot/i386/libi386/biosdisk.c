d2d8c08df1065701129df9e79a1653f4f3e42349
Takahashi Yoshihiro <nyan@FreeBSD.org>
2009-12-24 12:19:52 +0000

    MFC: revision 200631
      Fix debug messages of bd_io().
    
    
    Former-commit-id: 2860f8fd04b4e9c21b7bbe817d5ad1b6c15ba1da

--- sys/boot/i386/libi386/biosdisk.c
+++ sys/boot/i386/libi386/biosdisk.c
@@ -6601,11 +6601,9 @@ name|write
 condition|)
 name|DEBUG
 argument_list|(
-literal|"%d sectors from %lld to %p (0x%x) %s"
+literal|"Write %d sector(s) from %p (0x%x) to %lld %s"
 argument_list|,
 name|x
-argument_list|,
-name|dblk
 argument_list|,
 name|p
 argument_list|,
@@ -6613,6 +6611,8 @@ name|VTOP
 argument_list|(
 name|p
 argument_list|)
+argument_list|,
+name|dblk
 argument_list|,
 name|result
 condition|?
@@ -6624,9 +6624,11 @@ expr_stmt|;
 else|else
 name|DEBUG
 argument_list|(
-literal|"%d sectors from %p (0x%x) to %lld %s"
+literal|"Read %d sector(s) from %lld to %p (0x%x) %s"
 argument_list|,
 name|x
+argument_list|,
+name|dblk
 argument_list|,
 name|p
 argument_list|,
@@ -6634,8 +6636,6 @@ name|VTOP
 argument_list|(
 name|p
 argument_list|)
-argument_list|,
-name|dblk
 argument_list|,
 name|result
 condition|?
