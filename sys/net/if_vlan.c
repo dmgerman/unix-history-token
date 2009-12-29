62bfef2b0acd55910d557c7fdc7a6a322b3303c7
John Baldwin <jhb@FreeBSD.org>
2009-12-29 13:35:18 +0000

    Change vlan interfaces to cope more usefully with the parent interface being renamed.  Previously the vlan interfaces would lose their configuration as if the parent interface had been physically removed.  Now vlan interfaces ignore rename events. - Add a new ifnet flag (IFF_RENAMING) that is set while an ifnet is being   renamed.  This flag can be checked in ifnet departure/arrival event   handlers to treat rename events differently. - Change the ifnet departure event handler in the if_vlan(4) driver to   ignore departure events due to a trunk interface being renamed.
    Reviewed by:	brooks, rwatson
    MFC after:	1 week
    
    
    Former-commit-id: 3485ec536a72e607fdf25bd80a5ba7ac53713bb3

--- sys/net/if_vlan.c
+++ sys/net/if_vlan.c
@@ -2364,7 +2364,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * A handler for network interface departure events.  * Track departure of trunks here so that we don't access invalid  * pointers or whatever if a trunk is ripped from under us, e.g.,  * by ejecting its hot-plug card.  */
+comment|/*  * A handler for network interface departure events.  * Track departure of trunks here so that we don't access invalid  * pointers or whatever if a trunk is ripped from under us, e.g.,  * by ejecting its hot-plug card.  However, if an ifnet is simply  * being renamed, then there's no need to tear down the state.  */
 end_comment
 
 begin_function
@@ -2399,6 +2399,16 @@ operator|->
 name|if_vlantrunk
 operator|==
 name|NULL
+condition|)
+return|return;
+comment|/* If the ifnet is just being renamed, don't do anything. */
+if|if
+condition|(
+name|ifp
+operator|->
+name|if_flags
+operator|&
+name|IFF_RENAMING
 condition|)
 return|return;
 name|VLAN_LOCK
