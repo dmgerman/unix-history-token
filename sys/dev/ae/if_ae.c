0b3a9fa194f15c712d86fdd15d18c8881a58d8a7
Gavin Atkinson <gavin@FreeBSD.org>
2010-01-02 13:07:51 +0000

    MFC r200993:  Small spelling fix, "Ethetnet" -> "Ethernet"
    Approved by:	ed (mentor)
    
    
    Former-commit-id: dd02c8fabe3262e1b94988e57a618f2a3fd6d88d

--- sys/dev/ae/if_ae.c
+++ sys/dev/ae/if_ae.c
@@ -5362,7 +5362,7 @@ name|sc
 operator|->
 name|dev
 argument_list|,
-literal|"Ethetnet address registers are invalid.\n"
+literal|"Ethernet address registers are invalid.\n"
 argument_list|)
 expr_stmt|;
 return|return
