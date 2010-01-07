b3d5697c9762e75e918b6af1b5b65564f4ffe131
Luigi Rizzo <luigi@FreeBSD.org>
2010-01-07 14:23:19 +0000

    ip_var.h now needs to be before ip_fw_private.h
    Former-commit-id: c1b1358655e9376b1baa4f75e0ce99a925a34ab6

--- sys/netgraph/ng_ipfw.c
+++ sys/netgraph/ng_ipfw.c
@@ -96,6 +96,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<netinet/ip_var.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<netinet/ip_fw.h>
 end_include
 
@@ -109,12 +115,6 @@ begin_include
 include|#
 directive|include
 file|<netinet/ip.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<netinet/ip_var.h>
 end_include
 
 begin_include
