e149cbaedd9c14959c16ce04f2dc634cb6a38068
Ruslan Ermilov <ru@FreeBSD.org>
2010-01-10 08:02:07 +0000

    Apply patches directly to sources.  Their effect is as follows:
    - Make one-true-awk respect locale's collating order in [a-z]
      bracket expressions, until a more complete fix (like handing
      BREs) is ready.
    
    - Don't require a space between -[fv] and its argument.
    
    
    Former-commit-id: 5e8a417df43edda6532791793e82b5ef6adec062

--- contrib/one-true-awk/run.c
+++ contrib/one-true-awk/run.c
@@ -3,6 +3,20 @@ begin_comment
 comment|/**************************************************************** Copyright (C) Lucent Technologies 1997 All Rights Reserved  Permission to use, copy, modify, and distribute this software and its documentation for any purpose and without fee is hereby granted, provided that the above copyright notice appear in all copies and that both that the copyright notice and this permission notice and warranty disclaimer appear in supporting documentation, and that the name Lucent Technologies or any of its entities not be used in advertising or publicity pertaining to distribution of the software without specific, written prior permission.  LUCENT DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL LUCENT OR ANY OF ITS ENTITIES BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. ****************************************************************/
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_define
 define|#
 directive|define
@@ -4271,7 +4285,7 @@ else|else
 block|{
 name|i
 operator|=
-name|strcmp
+name|strcoll
 argument_list|(
 name|getsval
 argument_list|(
