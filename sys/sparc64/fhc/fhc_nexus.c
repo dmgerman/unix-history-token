a92daf04dec69aa3803276342b8ee7cc3e313a6e
Marius Strobl <marius@FreeBSD.org>
2010-01-13 20:48:42 +0000

    MFC: r200815, r200816
    Provide and consume missing module dependency information.
    
    
    Former-commit-id: e387943a6e56ae6e8b06d0d6e68dffc923d863d1

--- sys/sparc64/fhc/fhc_nexus.c
+++ sys/sparc64/fhc/fhc_nexus.c
@@ -324,6 +324,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|fhc
+argument_list|,
+name|nexus
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_function
 specifier|static
 name|int
--- sys/sparc64/sparc64/nexus.c
+++ sys/sparc64/sparc64/nexus.c
@@ -436,6 +436,16 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|nexus
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_decl_stmt
 specifier|static
 name|char
