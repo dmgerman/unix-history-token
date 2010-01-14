6b78d0b0177f54804e58b4e3bc9cbed113d3a86a
Edwin Groothuis <edwin@FreeBSD.org>
2010-01-14 10:40:54 +0000

    Remove -d option, whois.nic.mil doesn't exist anymore.
    PR:		bin/142507
    Submitted by:	Dan Mahoney <danm@prime.gushi.org>
    MFC after:	1 week
    
    Former-commit-id: 041789e1f385197600f5b079b2ef43f9fe7a8eca

--- usr.bin/whois/whois.c
+++ usr.bin/whois/whois.c
@@ -172,13 +172,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|DNICHOST
-value|"whois.nic.mil"
-end_define
-
-begin_define
-define|#
-directive|define
 name|GNICHOST
 value|"whois.nic.gov"
 end_define
@@ -502,7 +495,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"aAbc:dfgh:iIklmp:QrR6"
+literal|"aAbc:fgh:iIklmp:QrR6"
 argument_list|)
 operator|)
 operator|!=
@@ -545,14 +538,6 @@ case|:
 name|country
 operator|=
 name|optarg
-expr_stmt|;
-break|break;
-case|case
-literal|'d'
-case|:
-name|host
-operator|=
-name|DNICHOST
 expr_stmt|;
 break|break;
 case|case
@@ -1830,7 +1815,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: whois [-aAbdfgiIklmQrR6] [-c country-code | -h hostname] "
+literal|"usage: whois [-aAbfgiIklmQrR6] [-c country-code | -h hostname] "
 literal|"[-p port] name ...\n"
 argument_list|)
 expr_stmt|;
