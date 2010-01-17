331111545f48d96ee3de55db13c946e0337086f7
Bjoern A. Zeeb <bz@FreeBSD.org>
2010-01-17 13:07:52 +0000

    Garbage collect references to the no longer implemented tcp_fasttimo().
    Discussed with:	rwatson
    MFC after:	5 days
    
    
    Former-commit-id: 9c5c644298076fdccbd475c7a6a7e93d7228d087

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -2815,15 +2815,6 @@ end_function_decl
 
 begin_function_decl
 name|void
-name|tcp_fasttimo
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
 name|tcp_init
 parameter_list|(
 name|void
