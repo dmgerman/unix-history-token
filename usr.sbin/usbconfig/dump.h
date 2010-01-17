55b540541c28b772554eef00f76f8ef3301904ea
Andrew Thompson <thompsa@FreeBSD.org>
2010-01-17 18:26:21 +0000

    MFC r201705
     Sync to p4
      - Add new quirks commands and the '-d' option optionally to specify the ugen device.
    
    
    Former-commit-id: 46a27397b4c1ad57ba21b11ed50961552ba9365e

--- usr.sbin/usbconfig/dump.h
+++ usr.sbin/usbconfig/dump.h
@@ -7,6 +7,18 @@ begin_comment
 comment|/*-  * Copyright (c) 2008 Hans Petter Selasky. All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_DUMP_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_DUMP_H_
+end_define
+
 begin_function_decl
 specifier|const
 name|char
@@ -39,6 +51,21 @@ name|dump_power_mode
 parameter_list|(
 name|uint8_t
 name|value
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|dump_string_by_index
+parameter_list|(
+name|struct
+name|libusb20_device
+modifier|*
+name|pdev
+parameter_list|,
+name|uint8_t
+name|index
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -108,6 +135,15 @@ name|all_cfg
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* _DUMP_H_ */
+end_comment
 
 end_unit
 
