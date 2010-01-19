0234cf96fa643d920b8a56addf2bf0f64714c423
Konstantin Belousov <kib@FreeBSD.org>
2010-01-19 19:54:18 +0000

    MFC r197932: Do not map segments of zero length.
    Tested by:	Mykola Dzham <i levsha me>
    
    
    Former-commit-id: 67154732e259e355c0fa4f3cfa4ae23b305d2ec0

--- sys/kern/imgact_elf.c
+++ sys/kern/imgact_elf.c
@@ -3140,6 +3140,15 @@ operator|.
 name|p_type
 operator|==
 name|PT_LOAD
+operator|&&
+name|phdr
+index|[
+name|i
+index|]
+operator|.
+name|p_memsz
+operator|!=
+literal|0
 condition|)
 block|{
 comment|/* Loadable segment */
@@ -3830,6 +3839,18 @@ case|case
 name|PT_LOAD
 case|:
 comment|/* Loadable segment */
+if|if
+condition|(
+name|phdr
+index|[
+name|i
+index|]
+operator|.
+name|p_memsz
+operator|==
+literal|0
+condition|)
+break|break;
 name|prot
 operator|=
 literal|0
