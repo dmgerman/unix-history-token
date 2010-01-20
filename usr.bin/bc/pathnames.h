02cf46f0db6e3a9c55391d90d4edf2007cca4b41
Gabor Kovesdan <gabor@FreeBSD.org>
2010-01-20 21:30:52 +0000

    Replace GNU bc/dc with BSDL versions ported from OpenBSD. They have a good compatibility level with the GNU counterparts and have shown to be mature enough. For now, the GNU versions aren't removed from the tree, just detached from the build.
    Sponsored by:		Google Summer of Code 2008
    Portbuild run by:	erwin
    Approved by:		delphij
    
    
    Former-commit-id: c74072ed62aa9841e40d21b305a06cbd75f7c440

---
+++ usr.bin/bc/pathnames.h
@@ -0,0 +1,29 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/*	$FreeBSD$							*/
+end_comment
+
+begin_comment
+comment|/*      $OpenBSD: pathnames.h,v 1.1 2003/09/25 19:32:44 otto Exp $	*/
+end_comment
+
+begin_comment
+comment|/*  * Copyright (c) 2003, Otto Moerbeek<otto@drijf.net>  *  * Permission to use, copy, modify, and distribute this software for any  * purpose with or without fee is hereby granted, provided that the above  * copyright notice and this permission notice appear in all copies.  *  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|_PATH_LIBB
+value|"/usr/share/misc/bc.library"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_DC
+value|"/usr/bin/dc"
+end_define
+
+end_unit
+
