72dc1f98116b72061a9d1fa624c5021fb4de2731
Joerg Wunsch <joerg@FreeBSD.org>
2010-01-23 07:54:06 +0000

    Overhaul of the pcii driver:
    . Properly allocate all IO space resources.  These cards scatter their
      IO addresses over a range of 0x1600 bytes, and they require an
      additional address for "special interrupt handling".
    
    . Implement the "special interrupt handling" per the GPIB-PCIIA
      Technical Reference Manual; this was apparently not declared for the
      clone card this driver has been originally implemented for, but it
      turned out to be needed for both, an original NI brand PCII/PCIIA
      card as well as the Axiom AX5488 clone.
    
    . Add some diagnostic messages for various resource allocation etc.
      failures during probe.
    
    . Add some comments about the structure of the IO address space that
      is used by these cards.
    
    MFC after:	1 day
    
    
    Former-commit-id: c323a63780ec032a6add28c4bb037a0197e865cf

--- sys/dev/ieee488/upd7210.h
+++ sys/dev/ieee488/upd7210.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2005 Poul-Henning Kamp<phk@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  *  * Locating an actual µPD7210 data book has proven quite impossible for me.  * There are a fair number of newer chips which are supersets of the µPD7210  * but they are particular eager to comprehensively mark what the extensions  * are and what is in the base set.  Some even give the registers and their  * bits new names.  *  * The following information is based on a description of the µPD7210 found  * in an old manual for a VME board which used the chip.  */
+comment|/*-  * Copyright (c) 2005 Poul-Henning Kamp<phk@FreeBSD.org>  * Copyright (c) 2010 Joerg Wunsch<joerg@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  *  * Locating an actual µPD7210 data book has proven quite impossible for me.  * There are a fair number of newer chips which are supersets of the µPD7210  * but they are particular eager to comprehensively mark what the extensions  * are and what is in the base set.  Some even give the registers and their  * bits new names.  *  * The following information is based on a description of the µPD7210 found  * in an old manual for a VME board which used the chip.  */
 end_comment
 
 begin_ifndef
@@ -63,11 +63,10 @@ index|[
 literal|8
 index|]
 decl_stmt|;
-name|u_int
-name|reg_offset
-index|[
-literal|8
-index|]
+name|struct
+name|resource
+modifier|*
+name|irq_clear_res
 decl_stmt|;
 name|int
 name|dmachan
