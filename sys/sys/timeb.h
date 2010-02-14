d17efd17421510b82711ec2d66e6066de5cfb491
Ed Schouten <ed@FreeBSD.org>
2010-02-14 12:00:42 +0000

    Make <sys/timeb.h> work without including <sys/types.h>.
    According to the last POSIX specification that contained <sys/timeb.h>,
    this header should also typedef time_t properly. Also add a proper
    comment to the final #endif.
    
    
    Former-commit-id: 24fbd5723b1070d3682fc13a4bc3e603bafc229f

--- sys/sys/timeb.h
+++ sys/sys/timeb.h
@@ -15,6 +15,36 @@ directive|define
 name|_SYS_TIMEB_H_
 end_define
 
+begin_include
+include|#
+directive|include
+file|<sys/_types.h>
+end_include
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_TIME_T_DECLARED
+end_ifndef
+
+begin_typedef
+typedef|typedef
+name|__time_t
+name|time_t
+typedef|;
+end_typedef
+
+begin_define
+define|#
+directive|define
+name|_TIME_T_DECLARED
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_comment
 comment|/* The ftime(2) system call structure -- deprecated. */
 end_comment
@@ -85,6 +115,10 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* !_SYS_TIMEB_H_ */
+end_comment
 
 end_unit
 
