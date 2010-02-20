6be4b15343aebb2555befb19a787f1f1017141ad
Ulrich Spoerlein <uqs@FreeBSD.org>
2010-02-20 10:19:19 +0000

    Fix common misspelling of hierarchy
    Pointed out by:		bf1783 at gmail
    Approved by:		np (cxgb), kientzle (tar, etc.), philip (mentor)
    
    
    Former-commit-id: 7e26ef6a5410f5e1e71cf2df9266ece01839a944

--- sys/fs/msdosfs/msdosfs_vnops.c
+++ sys/fs/msdosfs/msdosfs_vnops.c
@@ -4472,7 +4472,7 @@ name|dp
 operator|->
 name|de_fndcnt
 expr_stmt|;
-comment|/* 	 * If ".." must be changed (ie the directory gets a new 	 * parent) then the source directory must not be in the 	 * directory heirarchy above the target, as this would 	 * orphan everything below the source directory. Also 	 * the user must have write permission in the source so 	 * as to be able to change "..". We must repeat the call 	 * to namei, as the parent directory is unlocked by the 	 * call to doscheckpath(). 	 */
+comment|/* 	 * If ".." must be changed (ie the directory gets a new 	 * parent) then the source directory must not be in the 	 * directory hierarchy above the target, as this would 	 * orphan everything below the source directory. Also 	 * the user must have write permission in the source so 	 * as to be able to change "..". We must repeat the call 	 * to namei, as the parent directory is unlocked by the 	 * call to doscheckpath(). 	 */
 name|error
 operator|=
 name|VOP_ACCESS
