befc9deef1e61ee4f2a78ae73ee35b4f29fe84ce
Konstantin Belousov <kib@FreeBSD.org>
2010-02-28 17:19:22 +0000

    Mark msdosfs as mpsafe.
    Tested by:	pho
    MFC after:	3 weeks
    
    
    Former-commit-id: a5e8ed5c214c606989683d9f422b13da80f7ccc4

--- sys/fs/msdosfs/msdosfs_vfsops.c
+++ sys/fs/msdosfs/msdosfs_vfsops.c
@@ -3567,6 +3567,12 @@ name|mnt_flag
 operator||=
 name|MNT_LOCAL
 expr_stmt|;
+name|mp
+operator|->
+name|mnt_kern_flag
+operator||=
+name|MNTK_MPSAFE
+expr_stmt|;
 name|MNT_IUNLOCK
 argument_list|(
 name|mp
