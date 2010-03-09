b9ce03dbeabf8670d3e066db231e135c029756aa
Marcel Moolenaar <marcel@FreeBSD.org>
2010-03-09 02:08:02 +0000

    Remove inclusion of <i386/include/psl.h> While here move inclusion of <sys/lock.h> in a better place.
    Former-commit-id: 546726462103ae060cf14fde3dfd48b3d66c2b43

--- sys/ia64/ia64/vm_machdep.c
+++ sys/ia64/ia64/vm_machdep.c
@@ -28,6 +28,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/lock.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/malloc.h>
 end_include
 
@@ -130,12 +136,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/lock.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<vm/vm_kern.h>
 end_include
 
@@ -155,12 +155,6 @@ begin_include
 include|#
 directive|include
 file|<vm/vm_extern.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<i386/include/psl.h>
 end_include
 
 begin_function
