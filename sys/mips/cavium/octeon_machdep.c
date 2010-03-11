2568f3210a218c9953382cc095f5738348e85c59
Juli Mallett <jmallett@FreeBSD.org>
2010-03-11 22:25:53 +0000

    Don't force single user on Octeon anymore.
    Former-commit-id: f5c59f47e8a37619b16e97bf45a01af43c064322

--- sys/mips/cavium/octeon_machdep.c
+++ sys/mips/cavium/octeon_machdep.c
@@ -2847,10 +2847,6 @@ block|{
 name|uint64_t
 name|platform_counter_freq
 decl_stmt|;
-name|boothowto
-operator||=
-name|RB_SINGLE
-expr_stmt|;
 comment|/* Initialize pcpu stuff */
 name|mips_pcpu0_init
 argument_list|()
