49f6463b39b7f7b2c47d3821647b076c0b48b5c8
Marius Strobl <marius@FreeBSD.org>
2010-03-15 19:13:49 +0000

    MFC: r204222
    According to the Linux sungem driver, in case of Apple (K2) GMACs
    GEM_MIF_CONFIG_MDI0 cannot be trusted when the firmware has powered
    down the chip so the internal transceiver has to be hardcoded. This
    is also in line with the AppleGMACEthernet driver, which just doesn't
    distinguish between internal/external transceiver and MDIO/MDI1
    respectively in the first place. Tested by: Andreas Tobler
    
    
    Former-commit-id: 6c3fc9b4c5a13c7a68ff844eedd90c7830c79cfa

--- sys/dev/gem/if_gem.c
+++ sys/dev/gem/if_gem.c
@@ -1649,7 +1649,7 @@ name|gem_mediastatus
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* 	 * Fall back on an internal PHY if no external PHY was found. 	 */
+comment|/* 	 * Fall back on an internal PHY if no external PHY was found. 	 * Note that with Apple (K2) GMACs GEM_MIF_CONFIG_MDI0 can't be 	 * trusted when the firmware has powered down the chip. 	 */
 if|if
 condition|(
 name|error
@@ -1657,12 +1657,19 @@ operator|!=
 literal|0
 operator|&&
 operator|(
+operator|(
 name|v
 operator|&
 name|GEM_MIF_CONFIG_MDI0
 operator|)
 operator|!=
 literal|0
+operator|||
+name|GEM_IS_APPLE
+argument_list|(
+name|sc
+argument_list|)
+operator|)
 condition|)
 block|{
 name|v
