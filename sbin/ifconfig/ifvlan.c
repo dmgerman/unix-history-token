44ab8f222b0f6084eb6efef4d4cdf98b5d73d92d
Pyun YongHyeon <yongari@FreeBSD.org>
2010-03-22 19:50:33 +0000

    MFC r204150:   Add TSO support on VLAN in fconfig(8).
    Former-commit-id: c39e945875a1266a8d05d418e1c5d8daf66fb9a5

--- sbin/ifconfig/ifvlan.c
+++ sbin/ifconfig/ifvlan.c
@@ -876,6 +876,25 @@ name|IFCAP_VLAN_HWFILTER
 argument_list|,
 name|setifcap
 argument_list|)
+block|,
+name|DEF_CMD
+argument_list|(
+literal|"-vlanhwtso"
+argument_list|,
+operator|-
+name|IFCAP_VLAN_HWTSO
+argument_list|,
+name|setifcap
+argument_list|)
+block|,
+name|DEF_CMD
+argument_list|(
+literal|"vlanhwtso"
+argument_list|,
+name|IFCAP_VLAN_HWTSO
+argument_list|,
+name|setifcap
+argument_list|)
 block|, }
 decl_stmt|;
 end_decl_stmt
