9f0d96b0b47921bf2e6fbebd59ee471d12fe61e3
John Baldwin <jhb@FreeBSD.org>
2010-03-26 19:00:17 +0000

    MFC 205332: Use the same policy for rejecting / not-reject ACPI tables with incorrect checksums as the base acpi(4) driver.  This fixes a problem where the MADT parser would reject the MADT table during early boot causing the MP Table to be, but then the acpi(4) driver would attach and use non-SMP interrupt routing.
    Former-commit-id: 7eb2e867ac5ccf4a358102500278bc40de6173b2

--- sys/amd64/acpica/acpi_machdep.c
+++ sys/amd64/acpica/acpi_machdep.c
@@ -498,6 +498,11 @@ argument_list|,
 name|sig
 argument_list|)
 expr_stmt|;
+if|#
+directive|if
+operator|(
+name|ACPI_CHECKSUM_ABORT
+operator|)
 name|table_unmap
 argument_list|(
 name|table
@@ -510,6 +515,8 @@ operator|(
 name|NULL
 operator|)
 return|;
+endif|#
+directive|endif
 block|}
 return|return
 operator|(
--- sys/i386/acpica/acpi_machdep.c
+++ sys/i386/acpica/acpi_machdep.c
@@ -2871,6 +2871,11 @@ argument_list|,
 name|sig
 argument_list|)
 expr_stmt|;
+if|#
+directive|if
+operator|(
+name|ACPI_CHECKSUM_ABORT
+operator|)
 name|table_unmap
 argument_list|(
 name|table
@@ -2883,6 +2888,8 @@ operator|(
 name|NULL
 operator|)
 return|;
+endif|#
+directive|endif
 block|}
 return|return
 operator|(
