1cf0520ca90536fbc96c48661f54406f17e1228a
Jack F Vogel <jfv@FreeBSD.org>
2010-03-27 00:21:40 +0000

    Update the driver to Intel version 2.1.6 	- add some new hardware support for 82599 	- Big change to interrupt architecture, it now 	  uses a queue which contains an RX/TX pair as 	  the recipient of the interrupt. This will reduce 	  overall system interrupts/msix usage. 	- Improved RX mbuf handling: the old get_buf routine 	  is no longer synchronized with rxeof, this allows 	  the elimination of packet discards due to mbuf 	  allocation failure. 	- Much simplified and improved AIM code, it now 	  happens in the queue interrupt context and takes 	  into account both the traffic on the RX AND TX 	  side. 	- variety of small tweaks, like ring size, that have 	  been seen as performance improvements. 	- Thanks to those that provided feedback or suggested 	  changes, I hope I've caught all of them.
    Former-commit-id: b84610a366a1902021779514de6179a43ff71b09

--- sys/dev/ixgbe/ixgbe_api.c
+++ sys/dev/ixgbe/ixgbe_api.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************    Copyright (c) 2001-2009, Intel Corporation    All rights reserved.      Redistribution and use in source and binary forms, with or without    modification, are permitted provided that the following conditions are met:       1. Redistributions of source code must retain the above copyright notice,        this list of conditions and the following disclaimer.       2. Redistributions in binary form must reproduce the above copyright        notice, this list of conditions and the following disclaimer in the        documentation and/or other materials provided with the distribution.       3. Neither the name of the Intel Corporation nor the names of its        contributors may be used to endorse or promote products derived from        this software without specific prior written permission.      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   POSSIBILITY OF SUCH DAMAGE.  ******************************************************************************/
+comment|/******************************************************************************    Copyright (c) 2001-2010, Intel Corporation    All rights reserved.      Redistribution and use in source and binary forms, with or without    modification, are permitted provided that the following conditions are met:       1. Redistributions of source code must retain the above copyright notice,        this list of conditions and the following disclaimer.       2. Redistributions in binary form must reproduce the above copyright        notice, this list of conditions and the following disclaimer in the        documentation and/or other materials provided with the distribution.       3. Neither the name of the Intel Corporation nor the names of its        contributors may be used to endorse or promote products derived from        this software without specific prior written permission.      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   POSSIBILITY OF SUCH DAMAGE.  ******************************************************************************/
 end_comment
 
 begin_comment
@@ -220,6 +220,9 @@ case|:
 case|case
 name|IXGBE_DEV_ID_82599_CX4
 case|:
+case|case
+name|IXGBE_DEV_ID_82599_T3_LOM
+case|:
 name|hw
 operator|->
 name|mac
@@ -371,6 +374,44 @@ argument_list|,
 name|IXGBE_NOT_IMPLEMENTED
 argument_list|)
 return|;
+block|}
+end_function
+
+begin_comment
+comment|/**  *  ixgbe_enable_relaxed_ordering - Enables tx relaxed ordering,  *  which is disabled by default in ixgbe_start_hw();  *  *  @hw: pointer to hardware structure  *  *   Enable relaxed ordering;  **/
+end_comment
+
+begin_function
+name|void
+name|ixgbe_enable_relaxed_ordering
+parameter_list|(
+name|struct
+name|ixgbe_hw
+modifier|*
+name|hw
+parameter_list|)
+block|{
+if|if
+condition|(
+name|hw
+operator|->
+name|mac
+operator|.
+name|ops
+operator|.
+name|enable_relaxed_ordering
+condition|)
+name|hw
+operator|->
+name|mac
+operator|.
+name|ops
+operator|.
+name|enable_relaxed_ordering
+argument_list|(
+name|hw
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
