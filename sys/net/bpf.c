7c3a42826d885d2f73fec933489934319cdf54c7
Jung-uk Kim <jkim@FreeBSD.org>
2010-04-05 17:33:33 +0000

    MFC:	r205858
    Check the pointer to JIT binary filter before its de-allocation.
    
    
    Former-commit-id: 8aadd789dc02b6e77e882ede93a4a97a106491e7

--- sys/net/bpf.c
+++ sys/net/bpf.c
@@ -6387,6 +6387,8 @@ condition|(
 name|d
 operator|->
 name|bd_rfilter
+operator|!=
+name|NULL
 condition|)
 block|{
 name|free
@@ -6404,6 +6406,14 @@ expr_stmt|;
 ifdef|#
 directive|ifdef
 name|BPF_JITTER
+if|if
+condition|(
+name|d
+operator|->
+name|bd_bfilter
+operator|!=
+name|NULL
+condition|)
 name|bpf_destroy_jit_filter
 argument_list|(
 name|d
@@ -6419,6 +6429,8 @@ condition|(
 name|d
 operator|->
 name|bd_wfilter
+operator|!=
+name|NULL
 condition|)
 name|free
 argument_list|(
