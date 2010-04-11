2c866de2a3963516154ffb57e10cf48138732551
Tim Kientzle <kientzle@FreeBSD.org>
2010-04-11 18:44:42 +0000

    If a file is specifically both included and excluded, then:  * It is not extracted (because it is excluded)  * If it's not present in the archive, then an error is    reported (because the file was requested and not found)  * If it is present in the archive, no error is reported. Previously, this would always report an error because the exclusion prevented the entry from matching the inclusion.
    Also, tar is now more reluctant to report unmatched inclusions.
    Previously, "tar x file1 'file*'" against an archive that contained a
    single entry "file1" would match file1 and then report an error for
    the second pattern because it wasn't matched.  It now considers both
    inclusions to be matched and reports no error.
    
    
    Former-commit-id: 2dbb13ce4690bdea5d385fc49b3de42c94ac7923

--- usr.bin/tar/matching.c
+++ usr.bin/tar/matching.c
@@ -672,6 +672,63 @@ operator|(
 literal|0
 operator|)
 return|;
+comment|/* Mark off any unmatched inclusions. */
+comment|/* In particular, if a filename does appear in the archive and 	 * is explicitly included and excluded, then we don't report 	 * it as missing even though we don't extract it. 	 */
+name|matched
+operator|=
+name|NULL
+expr_stmt|;
+for|for
+control|(
+name|match
+operator|=
+name|matching
+operator|->
+name|inclusions
+init|;
+name|match
+operator|!=
+name|NULL
+condition|;
+name|match
+operator|=
+name|match
+operator|->
+name|next
+control|)
+block|{
+if|if
+condition|(
+name|match
+operator|->
+name|matches
+operator|==
+literal|0
+operator|&&
+name|match_inclusion
+argument_list|(
+name|match
+argument_list|,
+name|pathname
+argument_list|)
+condition|)
+block|{
+name|matching
+operator|->
+name|inclusions_unmatched_count
+operator|--
+expr_stmt|;
+name|match
+operator|->
+name|matches
+operator|++
+expr_stmt|;
+name|matched
+operator|=
+name|match
+expr_stmt|;
+block|}
+block|}
 comment|/* Exclusions take priority */
 for|for
 control|(
@@ -707,11 +764,19 @@ literal|1
 operator|)
 return|;
 block|}
-comment|/* Then check for inclusions */
+comment|/* It's not excluded and we found an inclusion above, so it's included. */
+if|if
+condition|(
 name|matched
-operator|=
+operator|!=
 name|NULL
-expr_stmt|;
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
+comment|/* We didn't find an unmatched inclusion, check the remaining ones. */
 for|for
 control|(
 name|match
@@ -731,8 +796,15 @@ operator|->
 name|next
 control|)
 block|{
+comment|/* We looked at previously-unmatched inclusions already. */
 if|if
 condition|(
+name|match
+operator|->
+name|matches
+operator|>
+literal|0
+operator|&&
 name|match_inclusion
 argument_list|(
 name|match
@@ -741,48 +813,7 @@ name|pathname
 argument_list|)
 condition|)
 block|{
-comment|/* 			 * If this pattern has never been matched, 			 * then we're done. 			 */
-if|if
-condition|(
 name|match
-operator|->
-name|matches
-operator|==
-literal|0
-condition|)
-block|{
-name|match
-operator|->
-name|matches
-operator|++
-expr_stmt|;
-name|matching
-operator|->
-name|inclusions_unmatched_count
-operator|--
-expr_stmt|;
-return|return
-operator|(
-literal|0
-operator|)
-return|;
-block|}
-comment|/* 			 * Otherwise, remember the match but keep checking 			 * in case we can tick off an unmatched pattern. 			 */
-name|matched
-operator|=
-name|match
-expr_stmt|;
-block|}
-block|}
-comment|/* 	 * We didn't find a pattern that had never been matched, but 	 * we did find a match, so count it and exit. 	 */
-if|if
-condition|(
-name|matched
-operator|!=
-name|NULL
-condition|)
-block|{
-name|matched
 operator|->
 name|matches
 operator|++
@@ -792,6 +823,7 @@ operator|(
 literal|0
 operator|)
 return|;
+block|}
 block|}
 comment|/* If there were inclusions, default is to exclude. */
 if|if
