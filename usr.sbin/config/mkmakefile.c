32099447781a4ef3f5e4886f7fdd23c4931a88af
Alexander Leidinger <netchild@FreeBSD.org>
2010-04-22 09:20:17 +0000

    Revert r206179 (by imp) and do something similar which is more consistent with all other corresponding CTF places by changing the corresponding code which is generated by config(8). Or in short, move the '@' from the variable definition to the use of the variable. [1]
    While I'm here break up a long line. [2]
    
    Discussed with:	imp [1,2], bde [2]
    
    
    Former-commit-id: d9afabe4800d229de80f8537036e4037170abb40

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -3736,7 +3736,7 @@ argument_list|(
 name|cmd
 argument_list|)
 argument_list|,
-literal|"${%s_%c%s}\n.if defined(NORMAL_CTFCONVERT)&& !empty(NORMAL_CTFCONVERT)\n\t${NORMAL_CTFCONVERT}\n.endif"
+literal|"${%s_%c%s}\n\t@${NORMAL_CTFCONVERT}"
 argument_list|,
 name|ftype
 argument_list|,
