0b536e07ef8e45fce139a805921a43669bc5283c
Rui Paulo <rpaulo@FreeBSD.org>
2010-04-28 15:15:06 +0000

    MFC r206617:   Make this code a little more portable by wrapping the mtx calls   into macros.
    Former-commit-id: 5c7a1b14a1dcda250c4720ce8d6bb26bd0eca196

--- sys/net80211/ieee80211_freebsd.h
+++ sys/net80211/ieee80211_freebsd.h
@@ -548,6 +548,61 @@ value|mtx_assert((&(_as)->as_lock), MA_OWNED)
 end_define
 
 begin_comment
+comment|/*  * Scan table definitions.  */
+end_comment
+
+begin_typedef
+typedef|typedef
+name|struct
+name|mtx
+name|ieee80211_scan_table_lock_t
+typedef|;
+end_typedef
+
+begin_define
+define|#
+directive|define
+name|IEEE80211_SCAN_TABLE_LOCK_INIT
+parameter_list|(
+name|_st
+parameter_list|,
+name|_name
+parameter_list|)
+define|\
+value|mtx_init(&(_st)->st_lock, _name, "802.11 scan table", MTX_DEF)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IEEE80211_SCAN_TABLE_LOCK_DESTROY
+parameter_list|(
+name|_st
+parameter_list|)
+value|mtx_destroy(&(_st)->st_lock)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IEEE80211_SCAN_TABLE_LOCK
+parameter_list|(
+name|_st
+parameter_list|)
+value|mtx_lock(&(_st)->st_lock)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IEEE80211_SCAN_TABLE_UNLOCK
+parameter_list|(
+name|_st
+parameter_list|)
+value|mtx_unlock(&(_st)->st_lock)
+end_define
+
+begin_comment
 comment|/*  * Node reference counting definitions.  *  * ieee80211_node_initref	initialize the reference count to 1  * ieee80211_node_incref	add a reference  * ieee80211_node_decref	remove a reference  * ieee80211_node_dectestref	remove a reference and return 1 if this  *				is the last reference, otherwise 0  * ieee80211_node_refcnt	reference count for printing (only)  */
 end_comment
 
