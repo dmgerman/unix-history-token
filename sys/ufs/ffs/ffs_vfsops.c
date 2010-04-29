b188de7d2e26ff0ad9968c9084859f54786df35d
Andriy Gapon <avg@FreeBSD.org>
2010-04-29 10:04:00 +0000

    ffs_vfsops: restore alphabetic order of options in ffs_opts
    The order was not correct only for nfsv4acls.
    ("no" prefix is ignored)
    
    MFC after:	1 week
    
    
    Former-commit-id: 9d51fb85802163e429fc508e70e9bbdaa1cbe4be

--- sys/ufs/ffs/ffs_vfsops.c
+++ sys/ufs/ffs/ffs_vfsops.c
@@ -549,6 +549,8 @@ literal|"from"
 block|,
 literal|"multilabel"
 block|,
+literal|"nfsv4acls"
+block|,
 literal|"snapshot"
 block|,
 literal|"nosuid"
@@ -560,8 +562,6 @@ block|,
 literal|"sync"
 block|,
 literal|"union"
-block|,
-literal|"nfsv4acls"
 block|,
 name|NULL
 block|}
