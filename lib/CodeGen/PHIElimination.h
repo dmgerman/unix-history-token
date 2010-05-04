83a19baf52b4ffecf4006595d24b12980f237add
Roman Divacky <rdivacky@FreeBSD.org>
2010-05-04 20:50:39 +0000

    Update LLVM to r103052.
    Former-commit-id: 59161dfae3225dd9151afbc76ca9074598c0c605

--- lib/CodeGen/PHIElimination.h
+++ lib/CodeGen/PHIElimination.h
@@ -311,6 +311,14 @@ return|return
 name|I
 return|;
 block|}
+name|bool
+name|EliminateRegSequences
+argument_list|(
+name|MachineFunction
+operator|&
+name|MF
+argument_list|)
+block|;
 typedef|typedef
 name|std
 operator|::
