38826263ee14854b6b4bde2b2db8827841bcdab0
Warner Losh <imp@FreeBSD.org>
2010-05-08 13:48:31 +0000

    MFC r181059:
    Cleanup for WARNS 3.
    
    
    Former-commit-id: 584a12eaa9dce77b948c0f264d32ad064d49cfe6

--- lib/libthread_db/arch/arm/libpthread_md.c
+++ lib/libthread_db/arch/arm/libpthread_md.c
@@ -20,19 +20,13 @@ end_expr_stmt
 begin_include
 include|#
 directive|include
-file|<string.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/types.h>
 end_include
 
 begin_include
 include|#
 directive|include
-file|<proc_service.h>
+file|<string.h>
 end_include
 
 begin_include
