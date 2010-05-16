8c6f7bfd3eac1da5e1a32f2eff523f7549f430f6
Jilles Tjoelker <jilles@FreeBSD.org>
2010-05-16 22:52:51 +0000

    MFC r207944: sh: Fix pathname expansion with quoted slashes like *\/.
    These are git commits 36f0fa8fcbc8c7b2b194addd29100fb40e73e4e9 and
    d6d06ff5c2ea0fa44becc5ef4340e5f2f15073e4 in dash.
    
    Because this is the first code I'm importing from dash to expand.c, add the
    Herbert Xu copyright notice which is in dash's expand.c.
    
    When pathname expanding *\/, the CTLESC representing the quoted state was
    erroneously taken as part of the * pathname component. This CTLESC was then
    seen by the pattern matching code as escaping the '\0' terminating the
    string.
    
    The code is slightly different because dash converts the CTLESC characters
    to backslashes and removes all the other CTL* characters to allow
    substituting glob(3).
    
    The effect of the bug was also slightly different from dash (where nothing
    matched at all). Because a CTLESC can escape a '\0' in some way, whether
    files were included despite the bug depended on memory that should not be
    read. In particular, on many machines /*\/ expanded to a strict subset of
    what /*/ expanded to.
    
    Example:
      echo /*"/null"
    
    This should print /dev/null, not /*/null.
    
    PR:		bin/146378
    Obtained from:	dash
    
    Former-commit-id: 18265a563d5f54f1cd95a1bed43c46fe5987fdbf

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  * Copyright (c) 1997-2005  *	Herbert Xu<herbert@gondor.apana.org.au>.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Kenneth Almquist.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_ifndef
@@ -5158,6 +5158,9 @@ decl_stmt|;
 name|int
 name|matchdot
 decl_stmt|;
+name|int
+name|esc
+decl_stmt|;
 name|metaflag
 operator|=
 literal|0
@@ -5172,9 +5175,18 @@ name|p
 operator|=
 name|name
 init|;
+name|esc
+operator|=
+literal|0
+operator|,
+operator|*
+name|p
 condition|;
 name|p
-operator|++
+operator|+=
+name|esc
+operator|+
+literal|1
 control|)
 block|{
 if|if
@@ -5332,7 +5344,8 @@ operator|==
 name|CTLQUOTEMARK
 condition|)
 continue|continue;
-elseif|else
+else|else
+block|{
 if|if
 condition|(
 operator|*
@@ -5340,13 +5353,15 @@ name|p
 operator|==
 name|CTLESC
 condition|)
-name|p
+name|esc
 operator|++
 expr_stmt|;
 if|if
 condition|(
-operator|*
 name|p
+index|[
+name|esc
+index|]
 operator|==
 literal|'/'
 condition|)
@@ -5360,8 +5375,11 @@ name|start
 operator|=
 name|p
 operator|+
+name|esc
+operator|+
 literal|1
 expr_stmt|;
+block|}
 block|}
 block|}
 if|if
@@ -5597,9 +5615,14 @@ literal|0
 expr_stmt|;
 operator|*
 name|endname
-operator|++
 operator|=
 literal|'\0'
+expr_stmt|;
+name|endname
+operator|+=
+name|esc
+operator|+
+literal|1
 expr_stmt|;
 block|}
 name|matchdot
@@ -5766,9 +5789,15 @@ condition|)
 name|endname
 index|[
 operator|-
+name|esc
+operator|-
 literal|1
 index|]
 operator|=
+name|esc
+condition|?
+name|CTLESC
+else|:
 literal|'/'
 expr_stmt|;
 block|}
