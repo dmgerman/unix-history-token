56c3b033503bfaab89b76c7680f9d6e3d1d15748
Rafal Jaworowski <raj@FreeBSD.org>
2010-05-18 21:23:51 +0000

    Provide missing members for Book-E pmap (and fix build).
    Former-commit-id: cae502e17a360be7d4f0d625870f74d95e9165da

--- sys/powerpc/include/pmap.h
+++ sys/powerpc/include/pmap.h
@@ -286,6 +286,13 @@ name|u_int
 name|pm_active
 decl_stmt|;
 comment|/* active on cpus */
+name|uint32_t
+name|pm_gen_count
+decl_stmt|;
+comment|/* generation count (pmap lock dropped) */
+name|u_int
+name|pm_retries
+decl_stmt|;
 name|int
 name|pm_refs
 decl_stmt|;
