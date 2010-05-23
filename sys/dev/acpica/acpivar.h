5f86076c2bb4641a04265cd74015a7850f7acc81
Alexander Motin <mav@FreeBSD.org>
2010-05-23 07:53:22 +0000

    Make table-based HPET identification more clever. Before creating fake device, make sure we have no real HPET device entry with same ID. As side effect, it potentially allows several HPETs to be attached. Use first of them for timecounting, rest (if ever present) could later be used as event sources.
    Former-commit-id: 8e7eefbab332923aac870918264c4caaa28159ed

--- sys/dev/acpica/acpivar.h
+++ sys/dev/acpica/acpivar.h
@@ -1454,6 +1454,21 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|BOOLEAN
+name|acpi_MatchHid
+parameter_list|(
+name|ACPI_HANDLE
+name|h
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|hid
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_struct
 struct|struct
 name|acpi_parse_resource_set
