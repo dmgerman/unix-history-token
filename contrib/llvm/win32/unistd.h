573bcbfb96200d327079311aec9c25586e5cf80c
Roman Divacky <rdivacky@FreeBSD.org>
2010-06-10 19:59:23 +0000

    Remove Xcode cmake win32 projects unittests from LLVM and clang.xcodeproj INPUTS win32 from clang.
    Requested by:	jkim
    Approved by:	ed (mentor)
    
    
    Former-commit-id: caeda2157a51d8e760ba81c9a0785f0d961b6a97

--- contrib/llvm/win32/unistd.h
+++
@@ -1,7 +0,0 @@
-begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_comment
-comment|// This file has to be here because it's #included from the auto generated FileLexer.cpp
-end_comment
-
-end_unit
-
