d070f85e8faf3dd355cc1ca10317c32daf99f70d
Jung-uk Kim <jkim@FreeBSD.org>
2010-06-11 23:38:25 +0000

    Apply band-aid around function-like macro fdrop() without turning it into a real (inline) function or applying void casting for all its consumers. In most of places, the "return value" is not checked nor assigned, which causes too many warnings for some smart compilers, i.e., clang.
    Found by:	clang
    
    
    Former-commit-id: eecf478e812e5ae840b8b539cdc03bf44c160123

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -1030,6 +1030,23 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function
+specifier|static
+name|__inline
+name|int
+name|_fnoop
+parameter_list|(
+name|void
+parameter_list|)
+block|{
+return|return
+operator|(
+literal|0
+operator|)
+return|;
+block|}
+end_function
+
 begin_define
 define|#
 directive|define
@@ -1051,7 +1068,7 @@ parameter_list|,
 name|td
 parameter_list|)
 define|\
-value|(refcount_release(&(fp)->f_count) ? _fdrop((fp), (td)) : 0)
+value|(refcount_release(&(fp)->f_count) ? _fdrop((fp), (td)) : _fnoop())
 end_define
 
 begin_decl_stmt
