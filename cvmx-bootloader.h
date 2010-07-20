d273814395b6f6fa732fc0518818c488108b7fc8
Juli Mallett <jmallett@FreeBSD.org>
2010-07-20 07:11:19 +0000

    Initial import of Cavium Networks Octeon Simple Executive, SDK version 1.9.0.
    Former-commit-id: 1c305b501145f696d3597fb9b5b2091caaa6f67c

---
+++ cvmx-bootloader.h
@@ -0,0 +1,339 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/***********************license start***************  *  Copyright (c) 2008 Cavium Networks (support@cavium.com). All rights  *  reserved.  *  *  *  Redistribution and use in source and binary forms, with or without  *  modification, are permitted provided that the following conditions are  *  met:  *  *      * Redistributions of source code must retain the above copyright  *        notice, this list of conditions and the following disclaimer.  *  *      * Redistributions in binary form must reproduce the above  *        copyright notice, this list of conditions and the following  *        disclaimer in the documentation and/or other materials provided  *        with the distribution.  *  *      * Neither the name of Cavium Networks nor the names of  *        its contributors may be used to endorse or promote products  *        derived from this software without specific prior written  *        permission.  *  *  TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"  *  AND WITH ALL FAULTS AND CAVIUM NETWORKS MAKES NO PROMISES, REPRESENTATIONS  *  OR WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH  *  RESPECT TO THE SOFTWARE, INCLUDING ITS CONDITION, ITS CONFORMITY TO ANY  *  REPRESENTATION OR DESCRIPTION, OR THE EXISTENCE OF ANY LATENT OR PATENT  *  DEFECTS, AND CAVIUM SPECIFICALLY DISCLAIMS ALL IMPLIED (IF ANY) WARRANTIES  *  OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR  *  PURPOSE, LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET  *  POSSESSION OR CORRESPONDENCE TO DESCRIPTION.  THE ENTIRE RISK ARISING OUT  *  OF USE OR PERFORMANCE OF THE SOFTWARE LIES WITH YOU.  *  *  *  For any questions regarding licensing please contact marketing@caviumnetworks.com  *  ***********************license end**************************************/
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__CVMX_BOOTLOADER__
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|__CVMX_BOOTLOADER__
+end_define
+
+begin_comment
+comment|/**  * @file  *  * Bootloader definitions that are shared with other programs  *  *<hr>$Revision: 41586 $<hr>  */
+end_comment
+
+begin_comment
+comment|/* The bootloader_header_t structure defines the header that is present ** at the start of binary u-boot images.  This header is used to locate the bootloader ** image in NAND, and also to allow verification of images for normal NOR booting. ** This structure is placed at the beginning of a bootloader binary image, and remains ** in the executable code. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|BOOTLOADER_HEADER_MAGIC
+value|0x424f4f54
+end_define
+
+begin_comment
+comment|/* "BOOT" in ASCII */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|BOOTLOADER_HEADER_COMMENT_LEN
+value|64
+end_define
+
+begin_define
+define|#
+directive|define
+name|BOOTLOADER_HEADER_VERSION_LEN
+value|64
+end_define
+
+begin_define
+define|#
+directive|define
+name|BOOTLOADER_HEADER_MAX_SIZE
+value|0x200
+end_define
+
+begin_comment
+comment|/* limited by the space to the next exception handler */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|BOOTLOADER_HEADER_CURRENT_MAJOR_REV
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|BOOTLOADER_HEADER_CURRENT_MINOR_REV
+value|1
+end_define
+
+begin_comment
+comment|/* offsets to struct bootloader_header fields for assembly use */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAGIC_OFFST
+value|8
+end_define
+
+begin_define
+define|#
+directive|define
+name|HCRC_OFFST
+value|12
+end_define
+
+begin_define
+define|#
+directive|define
+name|HLEN_OFFST
+value|16
+end_define
+
+begin_define
+define|#
+directive|define
+name|DLEN_OFFST
+value|24
+end_define
+
+begin_define
+define|#
+directive|define
+name|DCRC_OFFST
+value|28
+end_define
+
+begin_define
+define|#
+directive|define
+name|GOT_OFFST
+value|48
+end_define
+
+begin_define
+define|#
+directive|define
+name|LOOKUP_STEP
+value|8192
+end_define
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__ASSEMBLY__
+end_ifndef
+
+begin_typedef
+typedef|typedef
+struct|struct
+name|bootloader_header
+block|{
+name|uint32_t
+name|jump_instr
+decl_stmt|;
+comment|/* Jump to executable code following the                             ** header.  This allows this header to                             ** be (and remain) part of the executable image)                             */
+name|uint32_t
+name|nop_instr
+decl_stmt|;
+comment|/* Must be 0x0 */
+name|uint32_t
+name|magic
+decl_stmt|;
+comment|/* Magic number to identify header */
+name|uint32_t
+name|hcrc
+decl_stmt|;
+comment|/* CRC of all of header excluding this field */
+name|uint16_t
+name|hlen
+decl_stmt|;
+comment|/* Length of header in bytes */
+name|uint16_t
+name|maj_rev
+decl_stmt|;
+comment|/* Major revision */
+name|uint16_t
+name|min_rev
+decl_stmt|;
+comment|/* Minor revision */
+name|uint16_t
+name|board_type
+decl_stmt|;
+comment|/* Board type that the image is for */
+name|uint32_t
+name|dlen
+decl_stmt|;
+comment|/* Length of data (immediately following header) in bytes */
+name|uint32_t
+name|dcrc
+decl_stmt|;
+comment|/* CRC of data */
+name|uint64_t
+name|address
+decl_stmt|;
+comment|/* Mips virtual address */
+name|uint32_t
+name|flags
+decl_stmt|;
+name|uint16_t
+name|image_type
+decl_stmt|;
+comment|/* Defined in bootloader_image_t enum */
+name|uint16_t
+name|resv0
+decl_stmt|;
+comment|/* pad */
+comment|/* The next 4 fields are placed in compile-time, not by the utility */
+name|uint32_t
+name|got_address
+decl_stmt|;
+comment|/* compiled got address position in the image */
+name|uint32_t
+name|got_num_entries
+decl_stmt|;
+comment|/* number of got entries */
+name|uint32_t
+name|compiled_start
+decl_stmt|;
+comment|/* compaled start of the image address */
+name|uint32_t
+name|image_start
+decl_stmt|;
+comment|/* relocated start of image address */
+name|char
+name|comment_string
+index|[
+name|BOOTLOADER_HEADER_COMMENT_LEN
+index|]
+decl_stmt|;
+comment|/* Optional, for descriptive purposes */
+name|char
+name|version_string
+index|[
+name|BOOTLOADER_HEADER_VERSION_LEN
+index|]
+decl_stmt|;
+comment|/* Optional, for descriptive purposes */
+block|}
+name|__attribute__
+typedef|((
+name|packed
+typedef|))
+name|bootloader_header_t
+typedef|;
+end_typedef
+
+begin_comment
+comment|/* Defines for flag field */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|BL_HEADER_FLAG_FAILSAFE
+value|(1)
+end_define
+
+begin_typedef
+typedef|typedef
+enum|enum
+block|{
+name|BL_HEADER_IMAGE_UKNOWN
+init|=
+literal|0x0
+block|,
+name|BL_HEADER_IMAGE_STAGE2
+block|,
+comment|/* Binary bootloader stage2 image (NAND boot) */
+name|BL_HEADER_IMAGE_STAGE3
+block|,
+comment|/* Binary bootloader stage3 image (NAND boot)*/
+name|BL_HEADER_IMAGE_NOR
+block|,
+comment|/* Binary bootloader for NOR boot */
+name|BL_HEADER_IMAGE_PCIBOOT
+block|,
+comment|/* Binary bootloader for PCI boot */
+name|BL_HEADER_IMAGE_UBOOT_ENV
+block|,
+comment|/* Environment for u-boot */
+name|BL_HEADER_IMAGE_MAX
+block|,
+comment|/* Range for customer private use.  Will not be used by Cavium Networks */
+name|BL_HEADER_IMAGE_CUST_RESERVED_MIN
+init|=
+literal|0x1000
+block|,
+name|BL_HEADER_IMAGE_CUST_RESERVED_MAX
+init|=
+literal|0x1fff
+block|, }
+name|bootloader_image_t
+typedef|;
+end_typedef
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __ASSEMBLY__ */
+end_comment
+
+begin_comment
+comment|/* Maximum address searched for NAND boot images and environments.  This is used ** by stage1 and stage2. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|MAX_NAND_SEARCH_ADDR
+value|0x400000
+end_define
+
+begin_comment
+comment|/* Defines for RAM based environment set by the host or the previous bootloader ** in a chain boot configuration. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|U_BOOT_RAM_ENV_ADDR
+value|(0x1000)
+end_define
+
+begin_define
+define|#
+directive|define
+name|U_BOOT_RAM_ENV_SIZE
+value|(0x1000)
+end_define
+
+begin_define
+define|#
+directive|define
+name|U_BOOT_RAM_ENV_CRC_SIZE
+value|(0x4)
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __CVMX_BOOTLOADER__ */
+end_comment
+
+end_unit
+
---
+++ cvmx-sysinfo.h
@@ -0,0 +1,210 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/***********************license start***************  *  Copyright (c) 2003-2008 Cavium Networks (support@cavium.com). All rights  *  reserved.  *  *  *  Redistribution and use in source and binary forms, with or without  *  modification, are permitted provided that the following conditions are  *  met:  *  *      * Redistributions of source code must retain the above copyright  *        notice, this list of conditions and the following disclaimer.  *  *      * Redistributions in binary form must reproduce the above  *        copyright notice, this list of conditions and the following  *        disclaimer in the documentation and/or other materials provided  *        with the distribution.  *  *      * Neither the name of Cavium Networks nor the names of  *        its contributors may be used to endorse or promote products  *        derived from this software without specific prior written  *        permission.  *  *  TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"  *  AND WITH ALL FAULTS AND CAVIUM NETWORKS MAKES NO PROMISES, REPRESENTATIONS  *  OR WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH  *  RESPECT TO THE SOFTWARE, INCLUDING ITS CONDITION, ITS CONFORMITY TO ANY  *  REPRESENTATION OR DESCRIPTION, OR THE EXISTENCE OF ANY LATENT OR PATENT  *  DEFECTS, AND CAVIUM SPECIFICALLY DISCLAIMS ALL IMPLIED (IF ANY) WARRANTIES  *  OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR  *  PURPOSE, LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET  *  POSSESSION OR CORRESPONDENCE TO DESCRIPTION.  THE ENTIRE RISK ARISING OUT  *  OF USE OR PERFORMANCE OF THE SOFTWARE LIES WITH YOU.  *  *  *  For any questions regarding licensing please contact marketing@caviumnetworks.com  *  ***********************license end**************************************/
+end_comment
+
+begin_comment
+comment|/**  * @file  *  * This module provides system/board information obtained by the bootloader.  *  *<hr>$Revision: 41586 $<hr>  *  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__CVMX_SYSINFO_H__
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|__CVMX_SYSINFO_H__
+end_define
+
+begin_include
+include|#
+directive|include
+file|"cvmx-app-init.h"
+end_include
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__cplusplus
+end_ifdef
+
+begin_extern
+extern|extern
+literal|"C"
+block|{
+endif|#
+directive|endif
+define|#
+directive|define
+name|OCTEON_SERIAL_LEN
+value|20
+comment|/**  * Structure describing application specific information.  * __cvmx_app_init() populates this from the cvmx boot descriptor.  * This structure is private to simple executive applications, so  * no versioning is required.  *  * This structure must be provided with some fields set in order to use  * simple executive functions in other applications (Linux kernel, u-boot, etc.)  * The cvmx_sysinfo_minimal_initialize() function is provided to set the required values  * in these cases.  *  *  */
+typedef|typedef
+struct|struct
+block|{
+comment|/* System wide variables */
+name|uint64_t
+name|system_dram_size
+decl_stmt|;
+comment|/**< installed DRAM in system, in bytes */
+name|void
+modifier|*
+name|phy_mem_desc_ptr
+decl_stmt|;
+comment|/**< ptr to memory descriptor block */
+comment|/* Application image specific variables */
+name|uint64_t
+name|stack_top
+decl_stmt|;
+comment|/**< stack top address (virtual) */
+name|uint64_t
+name|heap_base
+decl_stmt|;
+comment|/**< heap base address (virtual) */
+name|uint32_t
+name|stack_size
+decl_stmt|;
+comment|/**< stack size in bytes */
+name|uint32_t
+name|heap_size
+decl_stmt|;
+comment|/**< heap size in bytes */
+name|uint32_t
+name|core_mask
+decl_stmt|;
+comment|/**< coremask defining cores running application */
+name|uint32_t
+name|init_core
+decl_stmt|;
+comment|/**< Deprecated, use cvmx_coremask_first_core() to select init core */
+name|uint64_t
+name|exception_base_addr
+decl_stmt|;
+comment|/**< exception base address, as set by bootloader */
+name|uint32_t
+name|cpu_clock_hz
+decl_stmt|;
+comment|/**< cpu clock speed in hz */
+name|uint32_t
+name|dram_data_rate_hz
+decl_stmt|;
+comment|/**< dram data rate in hz (data rate = 2 * clock rate */
+name|uint16_t
+name|board_type
+decl_stmt|;
+name|uint8_t
+name|board_rev_major
+decl_stmt|;
+name|uint8_t
+name|board_rev_minor
+decl_stmt|;
+name|uint8_t
+name|mac_addr_base
+index|[
+literal|6
+index|]
+decl_stmt|;
+name|uint8_t
+name|mac_addr_count
+decl_stmt|;
+name|char
+name|board_serial_number
+index|[
+name|OCTEON_SERIAL_LEN
+index|]
+decl_stmt|;
+comment|/* Several boards support compact flash on the Octeon boot bus.  The CF     ** memory spaces may be mapped to different addresses on different boards.     ** These values will be 0 if CF is not present.     ** Note that these addresses are physical addresses, and it is up to the application     ** to use the proper addressing mode (XKPHYS, KSEG0, etc.)*/
+name|uint64_t
+name|compact_flash_common_base_addr
+decl_stmt|;
+name|uint64_t
+name|compact_flash_attribute_base_addr
+decl_stmt|;
+comment|/* Base address of the LED display (as on EBT3000 board)     ** This will be 0 if LED display not present.     ** Note that this address is a physical address, and it is up to the application     ** to use the proper addressing mode (XKPHYS, KSEG0, etc.)*/
+name|uint64_t
+name|led_display_base_addr
+decl_stmt|;
+name|uint32_t
+name|dfa_ref_clock_hz
+decl_stmt|;
+comment|/**< DFA reference clock in hz (if applicable)*/
+name|uint32_t
+name|bootloader_config_flags
+decl_stmt|;
+comment|/**< configuration flags from bootloader */
+name|uint8_t
+name|console_uart_num
+decl_stmt|;
+comment|/**< Uart number used for console */
+block|}
+name|cvmx_sysinfo_t
+typedef|;
+comment|/**  * This function returns the system/board information as obtained  * by the bootloader.  *  *  * @return  Pointer to the boot information structure  *  */
+specifier|extern
+name|cvmx_sysinfo_t
+modifier|*
+name|cvmx_sysinfo_get
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+comment|/**  * This function is used in non-simple executive environments (such as Linux kernel, u-boot, etc.)  * to configure the minimal fields that are required to use  * simple executive files directly.  *  * Locking (if required) must be handled outside of this  * function  *  * @param phy_mem_desc_ptr  *                   Pointer to global physical memory descriptor (bootmem descriptor)  * @param board_type Octeon board type enumeration  *  * @param board_rev_major  *                   Board major revision  * @param board_rev_minor  *                   Board minor revision  * @param cpu_clock_hz  *                   CPU clock freqency in hertz  *  * @return 0: Failure  *         1: success  */
+specifier|extern
+name|int
+name|cvmx_sysinfo_minimal_initialize
+parameter_list|(
+name|void
+modifier|*
+name|phy_mem_desc_ptr
+parameter_list|,
+name|uint16_t
+name|board_type
+parameter_list|,
+name|uint8_t
+name|board_rev_major
+parameter_list|,
+name|uint8_t
+name|board_rev_minor
+parameter_list|,
+name|uint32_t
+name|cpu_clock_hz
+parameter_list|)
+function_decl|;
+ifdef|#
+directive|ifdef
+name|CVMX_BUILD_FOR_LINUX_USER
+comment|/**  * Initialize the sysinfo structure when running on  * Octeon under Linux userspace  */
+specifier|extern
+name|void
+name|cvmx_sysinfo_linux_userspace_initialize
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+endif|#
+directive|endif
+ifdef|#
+directive|ifdef
+name|__cplusplus
+block|}
+end_extern
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __CVMX_SYSINFO_H__ */
+end_comment
+
+end_unit
+
