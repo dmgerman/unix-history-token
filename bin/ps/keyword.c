c077d5d1fbb73a008fe9f7f7810a41715aa016b7
Edward Tomasz Napierala <trasz@FreeBSD.org>
2010-07-24 15:37:36 +0000

    Fix alignment for the 'flags' label, and make more room for 'tdev'.
    MFC after:	1 month
    
    
    Former-commit-id: 9a4f3f8ce15bfc74795a18d78173ecd9d00e21f9

--- bin/ps/keyword.c
+++ bin/ps/keyword.c
@@ -629,7 +629,7 @@ name|kvar
 block|,
 name|NULL
 block|,
-literal|7
+literal|8
 block|,
 name|KOFF
 argument_list|(
@@ -2147,7 +2147,7 @@ name|tdev
 block|,
 name|NULL
 block|,
-literal|4
+literal|5
 block|,
 literal|0
 block|,
