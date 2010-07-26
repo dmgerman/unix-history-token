1e5b38b98ab53836bdd46a04058b3e02b3130ad1
Konstantin Belousov <kib@FreeBSD.org>
2010-07-26 14:38:51 +0000

    Remove unneeded includes.
    Submitted by:	alc
    MFC after:	1 week
    
    
    Former-commit-id: c63ed0ad8d0b4671a8a611487948636e2c5014f2

--- sys/amd64/linux32/linux32_machdep.c
+++ sys/amd64/linux32/linux32_machdep.c
@@ -182,18 +182,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<vm/vm_extern.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<vm/vm_kern.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<vm/vm_map.h>
 end_include
 
