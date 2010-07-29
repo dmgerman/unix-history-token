13b0c0a4f6fdd807198c943b582396a6f66c6c96
Juli Mallett <jmallett@FreeBSD.org>
2010-07-29 02:32:21 +0000

    o) Subtract 64K from the default userland stack pointer.  GCC generate code    that with a 32-bit ABI on a system with 64-bit registers can attempt to    access an invalid (well, kernel) memory address rather than the intended    user address for stack-relative loads and stores.  Lowering the stack    pointer works around this. [1] o) Make TRAP_DEBUG code conditional on the trap_debug variable.  Make    trap_debug default to 0 instead of 1 now but make it possible to change it    at runtime using sysctl. o) Kill programs that attempt an unaligned access of a kernel address.  Note    that with some ABIs, calling useracc() is not sufficient since the register    may be 64-bit but vm_offset_t is 32-bit so a kernel address could be    truncated to what looks like a valid user address, allowing the user to    crash the kernel. o) Clean up unaligned access emulation to support unaligned 16-bit and 64-bit    accesses.  (For 16-bit accesses it was checking for user access to too much    memory (4 bytes) and there was no 64-bit support.)  This still lacks support    for unaligned load-linked and store-conditional.
    Reviewed by:	[1] gonzo
    
    
    Former-commit-id: 6d071d8e83455bd5a715556afca442e31f184d41

--- sys/mips/mips/pm_machdep.c
+++ sys/mips/mips/pm_machdep.c
@@ -2484,7 +2484,7 @@ name|trapframe
 argument_list|)
 argument_list|)
 expr_stmt|;
-comment|/* 	 * Make sp 64-bit aligned. 	 */
+comment|/* 	 * The stack pointer has to be aligned to accommodate the largest 	 * datatype at minimum.  This probably means it should be 16-byte 	 * aligned, but for now we're 8-byte aligning it. 	 */
 name|td
 operator|->
 name|td_frame
@@ -2508,6 +2508,24 @@ operator|-
 literal|1
 operator|)
 expr_stmt|;
+comment|/* 	 * If we're running o32 or n32 programs but have 64-bit registers, 	 * GCC may use stack-relative addressing near the top of user 	 * address space that, due to sign extension, will yield an 	 * invalid address.  For instance, if sp is 0x7fffff00 then GCC 	 * might do something like this to load a word from 0x7ffffff0: 	 * 	 * 	addu	sp, sp, 32768 	 * 	lw	t0, -32528(sp) 	 * 	 * On systems with 64-bit registers, sp is sign-extended to 	 * 0xffffffff80007f00 and the load is instead done from 	 * 0xffffffff7ffffff0. 	 * 	 * To prevent this, we subtract 64K from the stack pointer here. 	 * 	 * For consistency, we should just always do this unless we're 	 * running n64 programs.  For now, since we don't support 	 * COMPAT_FREEBSD32 on n64 kernels, we just do it unless we're 	 * running n64 kernels. 	 */
+if|#
+directive|if
+operator|!
+name|defined
+argument_list|(
+name|__mips_n64
+argument_list|)
+name|td
+operator|->
+name|td_frame
+operator|->
+name|sp
+operator|-=
+literal|65536
+expr_stmt|;
+endif|#
+directive|endif
 name|td
 operator|->
 name|td_frame
