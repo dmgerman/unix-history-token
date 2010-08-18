ccdf756b979edd55b1b27b2431e1c0a9a193a295
Andre Oppermann <andre@FreeBSD.org>
2010-08-18 18:05:54 +0000

    If a TCP connection has been idle for one retransmit timeout or more it must reset its congestion window back to the initial window.
    RFC3390 has increased the initial window from 1 segment to up to
    4 segments.
    
    The initial window increase of RFC3390 wasn't reflected into the
    restart window which remained at its original defaults of 4 segments
    for local and 1 segment for all other connections.  Both values are
    controllable through sysctl net.inet.tcp.local_slowstart_flightsize
    and net.inet.tcp.slowstart_flightsize.
    
    The increase helps TCP's slow start algorithm to open up the congestion
    window much faster.
    
    Reviewed by:	lstewart
    MFC after:	1 week
    
    
    Former-commit-id: 0bbda19853a148b2fc703ad9ece7b2856b8f26e5

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -2191,6 +2191,16 @@ name|VNET_DECLARE
 argument_list|(
 name|int
 argument_list|,
+name|tcp_do_rfc3390
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|VNET_DECLARE
+argument_list|(
+name|int
+argument_list|,
 name|tcp_do_newreno
 argument_list|)
 expr_stmt|;
@@ -2266,6 +2276,13 @@ define|#
 directive|define
 name|V_tcp_delack_enabled
 value|VNET(tcp_delack_enabled)
+end_define
+
+begin_define
+define|#
+directive|define
+name|V_tcp_do_rfc3390
+value|VNET(tcp_do_rfc3390)
 end_define
 
 begin_define
