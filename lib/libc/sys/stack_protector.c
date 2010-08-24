f27ea8da2ed69d7738efafdd5229b9308572a0a4
Konstantin Belousov <kib@FreeBSD.org>
2010-08-24 13:02:22 +0000

    Remove extra FreeBSD tag.
    MFC after:	3 days
    
    
    Former-commit-id: e4b189fd426daca6a87da2eb33b36eedade86280

--- lib/libc/sys/stack_protector.c
+++ lib/libc/sys/stack_protector.c
@@ -1,9 +1,5 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* $FreeBSD$ */
-end_comment
-
-begin_comment
 comment|/* $NetBSD: stack_protector.c,v 1.4 2006/11/22 17:23:25 christos Exp $	*/
 end_comment
 
