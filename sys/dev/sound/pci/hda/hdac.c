9042fd7e8576f800f3415acc529f2917723d6830
Jack F Vogel <jfv@FreeBSD.org>
2010-08-28 00:23:13 +0000

    Add Intel Cougar Point PCH HD Audio Controller ID
    MFC in a week
    
    
    Former-commit-id: f6f716853f4e5fc19f7c9f5552e7c12785590ea7

--- sys/dev/sound/pci/hda/hdac.c
+++ sys/dev/sound/pci/hda/hdac.c
@@ -295,6 +295,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|HDA_INTEL_CPT
+value|HDA_MODEL_CONSTRUCT(INTEL, 0x1c20)
+end_define
+
+begin_define
+define|#
+directive|define
 name|HDA_INTEL_82801F
 value|HDA_MODEL_CONSTRUCT(INTEL, 0x2668)
 end_define
@@ -2244,6 +2251,14 @@ name|hdac_devices
 index|[]
 init|=
 block|{
+block|{
+name|HDA_INTEL_CPT
+block|,
+literal|"Intel Cougar Point"
+block|,
+literal|0
+block|}
+block|,
 block|{
 name|HDA_INTEL_82801F
 block|,
