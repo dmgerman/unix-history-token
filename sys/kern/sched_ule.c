ca7d0150f237347966bde580c1d43b94cfb45412
Matthew Fleming <mdf@FreeBSD.org>
2010-09-02 16:23:05 +0000

    Fix UP build.
    MFC after:	2 weeks
    
    
    Former-commit-id: cd67564a159789d1a73c49cba9337a397548e149

--- sys/kern/sched_ule.c
+++ sys/kern/sched_ule.c
@@ -7767,6 +7767,9 @@ name|SRQ_OURSELF
 operator||
 name|SRQ_YIELDING
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|SMP
 if|if
 condition|(
 name|THREAD_CAN_MIGRATE
@@ -7795,6 +7798,8 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 if|if
 condition|(
 name|ts
