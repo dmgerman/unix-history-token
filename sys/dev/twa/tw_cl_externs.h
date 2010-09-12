67e2deb148a0d4f909360076c7a376dbbf312b93
Xin Li <delphij@FreeBSD.org>
2010-09-12 06:19:35 +0000

    MFC r212008, 212028, 212210: Vendor update to version 3.80.06.003 to fix panic with ZFS under heavy I/O load.
    PR:             kern/149968
    Submitted by:   LSI (Tom Couch)
    Reported by:    Kai Kockro <kkockro web de>
    Tested by:      Kai Kockro, jpaetzel
    
    Former-commit-id: 1c91aabac6572346bcd5c8c2eafb2475bfff911b

--- sys/dev/twa/tw_cl_externs.h
+++ sys/dev/twa/tw_cl_externs.h
@@ -261,6 +261,32 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+specifier|extern
+name|int
+name|twa_setup_intr
+parameter_list|(
+name|struct
+name|twa_softc
+modifier|*
+name|sc
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|extern
+name|int
+name|twa_teardown_intr
+parameter_list|(
+name|struct
+name|twa_softc
+modifier|*
+name|sc
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_comment
 comment|/* Send down a SCSI command to the firmware (usually, an internal Req Sense. */
 end_comment
