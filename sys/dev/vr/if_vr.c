df0cde8429ddd535244eed651b097b297ea25992
Pyun YongHyeon <yongari@FreeBSD.org>
2010-09-24 19:26:27 +0000

    MFC r211766:   vr_init_locked() will stop and reset the controller. Remove   unnecessary vr_stop()/vr_reset() calls.
    Former-commit-id: 4c3be613a7d12578b13e17a797e66d87c9e974d2

--- sys/dev/vr/if_vr.c
+++ sys/dev/vr/if_vr.c
@@ -7718,16 +7718,6 @@ operator|.
 name|num_restart
 operator|++
 expr_stmt|;
-name|vr_stop
-argument_list|(
-name|sc
-argument_list|)
-expr_stmt|;
-name|vr_reset
-argument_list|(
-name|sc
-argument_list|)
-expr_stmt|;
 name|sc
 operator|->
 name|vr_ifp
@@ -11126,16 +11116,6 @@ argument_list|(
 name|ifp
 argument_list|,
 literal|"watchdog timeout\n"
-argument_list|)
-expr_stmt|;
-name|vr_stop
-argument_list|(
-name|sc
-argument_list|)
-expr_stmt|;
-name|vr_reset
-argument_list|(
-name|sc
 argument_list|)
 expr_stmt|;
 name|ifp
