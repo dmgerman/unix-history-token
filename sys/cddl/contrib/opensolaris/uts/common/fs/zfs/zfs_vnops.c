041539dd9acac925f4f80b20eeaa62ac7964a342
Andriy Gapon <avg@FreeBSD.org>
2010-10-08 07:29:26 +0000

    MFC r212951: zfs_map_page/zfs_unmap_page: do not use sched_pin() and SFB_CPUPRIVATE
    Former-commit-id: 6c7f2221a69c2dd433702742a4da149503a8d44e

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
@@ -1211,9 +1211,6 @@ modifier|*
 name|sfp
 parameter_list|)
 block|{
-name|sched_pin
-argument_list|()
-expr_stmt|;
 operator|*
 name|sfp
 operator|=
@@ -1221,7 +1218,7 @@ name|sf_buf_alloc
 argument_list|(
 name|pp
 argument_list|,
-name|SFB_CPUPRIVATE
+literal|0
 argument_list|)
 expr_stmt|;
 return|return
@@ -1254,9 +1251,6 @@ name|sf_buf_free
 argument_list|(
 name|sf
 argument_list|)
-expr_stmt|;
-name|sched_unpin
-argument_list|()
 expr_stmt|;
 block|}
 end_function
