c0be6eb40815ca0259cfb3b5b21933a907647077
Bjoern A. Zeeb <bz@FreeBSD.org>
2010-10-15 14:33:46 +0000

    MFC r185963 (by csjp in 2008):
      Consider processes attaching/detaching from tun(4) devices as being link
      state changes.  This change modifies tunopen and tunclose to call the
      if_link_state_change() function.  Among other things, this will result in
      devd(8) receiving events from devctl(4) for linkup/link down.  This allows
      us to do several useful things, including initializing tunnel parameters
      and adding routes.
    
      Discussed on:	freebsd-net
    
    
    Former-commit-id: 6886e03fd7f1fc676b3e2a9f0f412f3151a2cdf6

--- sys/net/if_tun.c
+++ sys/net/if_tun.c
@@ -2145,6 +2145,13 @@ argument_list|(
 name|tp
 argument_list|)
 expr_stmt|;
+name|if_link_state_change
+argument_list|(
+name|ifp
+argument_list|,
+name|LINK_STATE_UP
+argument_list|)
+expr_stmt|;
 name|TUNDEBUG
 argument_list|(
 name|ifp
@@ -2380,6 +2387,13 @@ name|tun_mtx
 argument_list|)
 expr_stmt|;
 block|}
+name|if_link_state_change
+argument_list|(
+name|ifp
+argument_list|,
+name|LINK_STATE_DOWN
+argument_list|)
+expr_stmt|;
 name|funsetown
 argument_list|(
 operator|&
