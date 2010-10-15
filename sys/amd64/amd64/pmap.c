6c7c0df3e592d4ac98bd62d75e7901352ee0430e
Konstantin Belousov <kib@FreeBSD.org>
2010-10-15 17:56:51 +0000

    MFC r209862: For both i386 and amd64 pmap, - change the type of pm_active to cpumask_t, which it is; - in pmap_remove_pages(), compare with PCPU(curpmap), instead of   dereferencing the long chain of pointers [1]. For amd64 pmap, remove the unneeded checks for validity of curpmap in pmap_activate(), since curpmap should be always valid after r209789.
    Former-commit-id: e155823bd75c7ef1ecb3d9daa4386d04b865761c

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -17415,13 +17415,9 @@ if|if
 condition|(
 name|pmap
 operator|!=
-name|vmspace_pmap
+name|PCPU_GET
 argument_list|(
-name|curthread
-operator|->
-name|td_proc
-operator|->
-name|p_vmspace
+name|curpmap
 argument_list|)
 condition|)
 block|{
@@ -21686,11 +21682,6 @@ expr_stmt|;
 ifdef|#
 directive|ifdef
 name|SMP
-if|if
-condition|(
-name|oldpmap
-condition|)
-comment|/* XXX FIXME */
 name|atomic_clear_int
 argument_list|(
 operator|&
@@ -21719,11 +21710,6 @@ argument_list|)
 expr_stmt|;
 else|#
 directive|else
-if|if
-condition|(
-name|oldpmap
-condition|)
-comment|/* XXX FIXME */
 name|oldpmap
 operator|->
 name|pm_active
