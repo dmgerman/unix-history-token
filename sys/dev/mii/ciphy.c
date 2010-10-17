3b5e1fb674b33578dd1367e3a556770a419acff5
Marius Strobl <marius@FreeBSD.org>
2010-10-17 12:43:15 +0000

    MFC: r176773, r178598
    Recognize Cicada CS8204 and CS8244 PHYs.
    
    
    Former-commit-id: f7adad3f971a1151d44e4d275a18a607979496e3

--- sys/dev/mii/ciphy.c
+++ sys/dev/mii/ciphy.c
@@ -18,7 +18,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_comment
-comment|/*  * Driver for the Cicada CS8201 10/100/1000 copper PHY.  */
+comment|/*  * Driver for the Cicada/Vitesse CS/VSC8xxx 10/100/1000 copper PHY.  */
 end_comment
 
 begin_include
@@ -313,6 +313,20 @@ argument_list|(
 name|CICADA
 argument_list|,
 name|VSC8211
+argument_list|)
+block|,
+name|MII_PHY_DESC
+argument_list|(
+name|CICADA
+argument_list|,
+name|CS8204
+argument_list|)
+block|,
+name|MII_PHY_DESC
+argument_list|(
+name|CICADA
+argument_list|,
+name|CS8244
 argument_list|)
 block|,
 name|MII_PHY_DESC
@@ -1464,6 +1478,9 @@ name|model
 condition|)
 block|{
 case|case
+name|MII_MODEL_CICADA_CS8204
+case|:
+case|case
 name|MII_MODEL_CICADA_CS8201
 case|:
 comment|/* Turn off "aux mode" (whatever that means) */
@@ -1580,6 +1597,9 @@ block|}
 break|break;
 case|case
 name|MII_MODEL_CICADA_VSC8211
+case|:
+case|case
+name|MII_MODEL_CICADA_CS8244
 case|:
 case|case
 name|MII_MODEL_VITESSE_VSC8601
