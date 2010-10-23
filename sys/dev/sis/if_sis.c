a6788aeae81bf0338891ada44fbfdb373617e79b
Pyun YongHyeon <yongari@FreeBSD.org>
2010-10-23 00:41:26 +0000

    MFC r214089:   Correct handling of shared interrupt in sis_intr(). r212116 incorrectly   released a drver lock for shared interrupt case such that it caused   panic. While I'm here check whether driver is still running before   serving TX/RX handler.
      Reported by:	Jerahmy Pocott < QUAKENET1 <> optusnet dot com dot au >
      Tested by:	Jerahmy Pocott < QUAKENET1 <> optusnet dot com dot au >
    
    
    Former-commit-id: dcb2c9f5c46aebeef8157977eeb2f7cdbc3a7cec

--- sys/dev/sis/if_sis.c
+++ sys/dev/sis/if_sis.c
@@ -8404,6 +8404,7 @@ argument_list|(
 name|sc
 argument_list|)
 expr_stmt|;
+return|return;
 block|}
 comment|/* Disable interrupts. */
 name|CSR_WRITE_4
@@ -8428,6 +8429,19 @@ literal|0
 condition|;
 control|)
 block|{
+if|if
+condition|(
+operator|(
+name|ifp
+operator|->
+name|if_drv_flags
+operator|&
+name|IFF_DRV_RUNNING
+operator|)
+operator|==
+literal|0
+condition|)
+break|break;
 if|if
 condition|(
 name|status
@@ -8530,6 +8544,15 @@ name|SIS_ISR
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|ifp
+operator|->
+name|if_drv_flags
+operator|&
+name|IFF_DRV_RUNNING
+condition|)
+block|{
 comment|/* Re-enable interrupts. */
 name|CSR_WRITE_4
 argument_list|(
@@ -8556,6 +8579,7 @@ argument_list|(
 name|ifp
 argument_list|)
 expr_stmt|;
+block|}
 name|SIS_UNLOCK
 argument_list|(
 name|sc
