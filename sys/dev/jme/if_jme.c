e737c43ebcf3468bbfe3b40833b72e5f68e68b5b
Pyun YongHyeon <yongari@FreeBSD.org>
2010-10-24 21:14:01 +0000

    MFC r213844:   Make sure to not use stale ip/tcp header pointers. The ip/tcp   header parser uses m_pullup(9) to get access to mbuf chain.   m_pullup(9) can allocate new mbuf chain and free old one if the   space left in the mbuf chain is not enough to hold requested   contiguous bytes. Previously drivers can use stale ip/tcp header   pointer if m_pullup(9) returned new mbuf chain.
      Reported by:	Andrew Boyer (aboyer <> averesystems dot com)
    
    
    Former-commit-id: ea24008f72fd4f753dd6f7c9b807ffe65259ef35

--- sys/dev/jme/if_jme.c
+++ sys/dev/jme/if_jme.c
@@ -9060,6 +9060,26 @@ name|ENOBUFS
 operator|)
 return|;
 block|}
+comment|/* 		 * Reset IP checksum and recompute TCP pseudo 		 * checksum that NDIS specification requires. 		 */
+name|ip
+operator|=
+operator|(
+expr|struct
+name|ip
+operator|*
+operator|)
+operator|(
+name|mtod
+argument_list|(
+name|m
+argument_list|,
+name|char
+operator|*
+argument_list|)
+operator|+
+name|ip_off
+operator|)
+expr_stmt|;
 name|tcp
 operator|=
 operator|(
@@ -9079,7 +9099,6 @@ operator|+
 name|poff
 operator|)
 expr_stmt|;
-comment|/* 		 * Reset IP checksum and recompute TCP pseudo 		 * checksum that NDIS specification requires. 		 */
 name|ip
 operator|->
 name|ip_sum
