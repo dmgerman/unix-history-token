c957b559a7ec5c5f8723fc53c1b459f0bc434fe9
Lawrence Stewart <lstewart@FreeBSD.org>
2010-11-06 15:40:34 +0000

    MFC r213912:
    - Switch the "net.inet.tcp.reass.cursegments" and
      "net.inet.tcp.reass.maxsegments" sysctl variables to be based on UMA zone
      stats. The value returned by the cursegments sysctl is approximate owing to
      the way in which uma_zone_get_cur is implemented.
    
    - Discontinue use of V_tcp_reass_qsize as a global reassembly segment count
      variable in the reassembly implementation. The variable was used without
      proper synchronisation and was duplicating accounting done by UMA already. The
      lack of synchronisation was particularly problematic on SMP systems
      terminating many TCP sessions, resulting in poor TCP performance for
      connections with non-zero packet loss.
    
    The base code from r213912 was modified as part of this MFC in order to work
    correctly on FreeBSD 7.
    
    Sponsored by:	FreeBSD Foundation
    Reviewed by:	andre, gnn, rpaulo (as part of a larger patch)
    
    
    Former-commit-id: 625a607c7eb0f8779cca9e40ac0acb7fdf8f800e

--- sys/netinet/tcp_reass.c
+++ sys/netinet/tcp_reass.c
@@ -236,6 +236,26 @@ begin_comment
 comment|/* TCPDEBUG */
 end_comment
 
+begin_function_decl
+specifier|static
+name|int
+name|tcp_reass_sysctl_maxseg
+parameter_list|(
+name|SYSCTL_HANDLER_ARGS
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+specifier|static
+name|int
+name|tcp_reass_sysctl_qsize
+parameter_list|(
+name|SYSCTL_HANDLER_ARGS
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_expr_stmt
 name|SYSCTL_NODE
 argument_list|(
@@ -264,7 +284,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_expr_stmt
-name|SYSCTL_INT
+name|SYSCTL_PROC
 argument_list|(
 name|_net_inet_tcp_reass
 argument_list|,
@@ -278,6 +298,11 @@ operator|&
 name|tcp_reass_maxseg
 argument_list|,
 literal|0
+argument_list|,
+operator|&
+name|tcp_reass_sysctl_maxseg
+argument_list|,
+literal|"I"
 argument_list|,
 literal|"Global maximum number of TCP Segments in Reassembly Queue"
 argument_list|)
@@ -294,7 +319,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_expr_stmt
-name|SYSCTL_INT
+name|SYSCTL_PROC
 argument_list|(
 name|_net_inet_tcp_reass
 argument_list|,
@@ -308,6 +333,11 @@ operator|&
 name|tcp_reass_qsize
 argument_list|,
 literal|0
+argument_list|,
+operator|&
+name|tcp_reass_sysctl_qsize
+argument_list|,
+literal|"I"
 argument_list|,
 literal|"Global number of TCP Segments currently in Reassembly Queue"
 argument_list|)
@@ -543,9 +573,6 @@ operator|->
 name|t_segqlen
 operator|--
 expr_stmt|;
-name|tcp_reass_qsize
-operator|--
-expr_stmt|;
 block|}
 name|KASSERT
 argument_list|(
@@ -568,6 +595,70 @@ name|t_segqlen
 operator|)
 argument_list|)
 expr_stmt|;
+block|}
+end_function
+
+begin_function
+specifier|static
+name|int
+name|tcp_reass_sysctl_maxseg
+parameter_list|(
+name|SYSCTL_HANDLER_ARGS
+parameter_list|)
+block|{
+name|tcp_reass_maxseg
+operator|=
+name|uma_zone_get_max
+argument_list|(
+name|tcp_reass_zone
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+name|sysctl_handle_int
+argument_list|(
+name|oidp
+argument_list|,
+name|arg1
+argument_list|,
+name|arg2
+argument_list|,
+name|req
+argument_list|)
+operator|)
+return|;
+block|}
+end_function
+
+begin_function
+specifier|static
+name|int
+name|tcp_reass_sysctl_qsize
+parameter_list|(
+name|SYSCTL_HANDLER_ARGS
+parameter_list|)
+block|{
+name|tcp_reass_qsize
+operator|=
+name|uma_zone_get_cur
+argument_list|(
+name|tcp_reass_zone
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+name|sysctl_handle_int
+argument_list|(
+name|oidp
+argument_list|,
+name|arg1
+argument_list|,
+name|arg2
+argument_list|,
+name|req
+argument_list|)
+operator|)
+return|;
 block|}
 end_function
 
@@ -651,7 +742,7 @@ condition|)
 goto|goto
 name|present
 goto|;
-comment|/* 	 * Limit the number of segments in the reassembly queue to prevent 	 * holding on to too many segments (and thus running out of mbufs). 	 * Make sure to let the missing segment through which caused this 	 * queue.  Always keep one global queue entry spare to be able to 	 * process the missing segment. 	 */
+comment|/* 	 * Limit the number of segments in the reassembly queue to prevent 	 * holding on to too many segments (and thus running out of mbufs). 	 * Make sure to let the missing segment through which caused this 	 * queue. 	 */
 if|if
 condition|(
 name|th
@@ -662,19 +753,11 @@ name|tp
 operator|->
 name|rcv_nxt
 operator|&&
-operator|(
-name|tcp_reass_qsize
-operator|+
-literal|1
-operator|>=
-name|tcp_reass_maxseg
-operator|||
 name|tp
 operator|->
 name|t_segqlen
 operator|>=
 name|tcp_reass_maxqlen
-operator|)
 condition|)
 block|{
 name|tcp_reass_overflows
@@ -742,9 +825,6 @@ block|}
 name|tp
 operator|->
 name|t_segqlen
-operator|++
-expr_stmt|;
-name|tcp_reass_qsize
 operator|++
 expr_stmt|;
 comment|/* 	 * Find a segment which begins after this one does. 	 */
@@ -848,9 +928,6 @@ expr_stmt|;
 name|tp
 operator|->
 name|t_segqlen
-operator|--
-expr_stmt|;
-name|tcp_reass_qsize
 operator|--
 expr_stmt|;
 comment|/* 				 * Try to present any queued data 				 * at the left window edge to the user. 				 * This is needed after the 3-WHS 				 * completes. 				 */
@@ -989,9 +1066,6 @@ expr_stmt|;
 name|tp
 operator|->
 name|t_segqlen
-operator|--
-expr_stmt|;
-name|tcp_reass_qsize
 operator|--
 expr_stmt|;
 name|q
@@ -1183,9 +1257,6 @@ expr_stmt|;
 name|tp
 operator|->
 name|t_segqlen
-operator|--
-expr_stmt|;
-name|tcp_reass_qsize
 operator|--
 expr_stmt|;
 name|q
