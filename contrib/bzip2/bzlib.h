00a13bbe3df47f7136aa72b5e37043d9823826be
David E. O'Brien <obrien@FreeBSD.org>
2010-11-09 18:32:57 +0000

    Upgrade to Bzip2 version 1.0.6.
    Reviewed by: SO (cperciva)
    
    
    Former-commit-id: b789d5a225dd370a30660f8d12fcaddd36992d79

--- contrib/bzip2/bzlib.h
+++ contrib/bzip2/bzlib.h
@@ -16,7 +16,7 @@ comment|/*-------------------------------------------------------------*/
 end_comment
 
 begin_comment
-comment|/* ------------------------------------------------------------------    This file is part of bzip2/libbzip2, a program and library for    lossless, block-sorting data compression.     bzip2/libbzip2 version 1.0.5 of 10 December 2007    Copyright (C) 1996-2007 Julian Seward<jseward@bzip.org>     Please read the WARNING, DISCLAIMER and PATENTS sections in the     README file.     This program is released under the terms of the license contained    in the file LICENSE.    ------------------------------------------------------------------ */
+comment|/* ------------------------------------------------------------------    This file is part of bzip2/libbzip2, a program and library for    lossless, block-sorting data compression.     bzip2/libbzip2 version 1.0.6 of 6 September 2010    Copyright (C) 1996-2010 Julian Seward<jseward@bzip.org>     Please read the WARNING, DISCLAIMER and PATENTS sections in the     README file.     This program is released under the terms of the license contained    in the file LICENSE.    ------------------------------------------------------------------ */
 end_comment
 
 begin_ifndef
