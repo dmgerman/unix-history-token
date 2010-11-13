4b3b0742ba4ac089e5b82c62ef318804a74f8ce8
Bruce Cran <brucec@FreeBSD.org>
2010-11-13 01:21:55 +0000

    We still support PLIP devices, so mention them in addition to ethernet.
    Former-commit-id: 76cf4fd46434181cf808b6dccdbac4b06f7c81bc

--- usr.sbin/sysinstall/install.c
+++ usr.sbin/sysinstall/install.c
@@ -2853,7 +2853,7 @@ condition|(
 operator|!
 name|msgYesNo
 argument_list|(
-literal|"Would you like to configure any Ethernet network devices?"
+literal|"Would you like to configure any Ethernet or PLIP network devices?"
 argument_list|)
 condition|)
 block|{
