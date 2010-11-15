ce179ada9e4b2911f5726f53c517d9b4e1f7313c
Marius Strobl <marius@FreeBSD.org>
2010-11-15 21:58:10 +0000

    Convert drivers somehow missed in r200874 to multipass probing.
    Former-commit-id: e138047404986c8e63217bdc72bc55963fbff2d8

--- sys/sparc64/pci/psycho.c
+++ sys/sparc64/pci/psycho.c
@@ -723,7 +723,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
-name|DRIVER_MODULE
+name|EARLY_DRIVER_MODULE
 argument_list|(
 name|psycho
 argument_list|,
@@ -736,6 +736,8 @@ argument_list|,
 literal|0
 argument_list|,
 literal|0
+argument_list|,
+name|BUS_PASS_BUS
 argument_list|)
 expr_stmt|;
 end_expr_stmt
--- sys/sparc64/pci/schizo.c
+++ sys/sparc64/pci/schizo.c
@@ -710,7 +710,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
-name|DRIVER_MODULE
+name|EARLY_DRIVER_MODULE
 argument_list|(
 name|schizo
 argument_list|,
@@ -723,6 +723,8 @@ argument_list|,
 literal|0
 argument_list|,
 literal|0
+argument_list|,
+name|BUS_PASS_BUS
 argument_list|)
 expr_stmt|;
 end_expr_stmt
