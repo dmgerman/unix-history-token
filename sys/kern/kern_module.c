04abee0f520ebdbd8956dc427aa301b03ac2cc9f
Attilio Rao <attilio@FreeBSD.org>
2010-11-19 19:43:56 +0000

    Scan the list in reverse order for the shutdown handlers of loaded modules. This way, when there is a dependency between two modules, the handler of the latter probed runs first.
    This is a similar approach as the modules are unloaded in the same
    linkerfile.
    
    Sponsored by:	Sandvine Incorporated
    Submitted by:	Nima Misaghian <nmisaghian at sandvine dot com>
    MFC after:	1 week
    
    
    Former-commit-id: 399ee9cbc88af9a0fe2b9f0bf6f5bf4a506f40c8

--- sys/kern/kern_module.c
+++ sys/kern/kern_module.c
@@ -124,6 +124,7 @@ begin_typedef
 typedef|typedef
 name|TAILQ_HEAD
 argument_list|(
+argument|modulelst
 argument_list|,
 argument|module
 argument_list|)
@@ -365,11 +366,13 @@ argument_list|)
 expr_stmt|;
 name|MOD_SLOCK
 expr_stmt|;
-name|TAILQ_FOREACH
+name|TAILQ_FOREACH_REVERSE
 argument_list|(
 argument|mod
 argument_list|,
 argument|&modules
+argument_list|,
+argument|modulelst
 argument_list|,
 argument|link
 argument_list|)
