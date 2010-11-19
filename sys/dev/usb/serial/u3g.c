5e0982f679ba61204d7fc93ec659d3ab1fe1f126
Andrew Thompson <thompsa@FreeBSD.org>
2010-11-19 01:51:51 +0000

    MFC r215326
     Add a special INIT product ID used by some models of the HUAWEI
     K3765 datacard. After ejecting this device, it reappears using
     the normal K3765 ID. It does not switch automatically
    
    
    Former-commit-id: f635f58a47fde8b5faf50a994f1904af1ac97faf

--- sys/dev/usb/serial/u3g.c
+++ sys/dev/usb/serial/u3g.c
@@ -1834,6 +1834,15 @@ argument_list|)
 block|,
 name|U3G_DEV
 argument_list|(
+name|HUAWEI
+argument_list|,
+name|K3765_INIT
+argument_list|,
+name|U3GINIT_HUAWEISCSI
+argument_list|)
+block|,
+name|U3G_DEV
+argument_list|(
 name|KYOCERA2
 argument_list|,
 name|CDMA_MSM_K
