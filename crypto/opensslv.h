1fe426b6c053a04176b5159d8f4755ebe32b332c
Simon L. Nielsen <simon@FreeBSD.org>
2010-12-02 22:36:51 +0000

    Import OpenSSL 0.9.8q.
    Former-commit-id: 623724a1108dfb8b4c4caa595e856efdb1946b56

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -19,7 +19,7 @@ begin_define
 define|#
 directive|define
 name|OPENSSL_VERSION_NUMBER
-value|0x0090810f
+value|0x0090811f
 end_define
 
 begin_ifdef
@@ -32,7 +32,7 @@ begin_define
 define|#
 directive|define
 name|OPENSSL_VERSION_TEXT
-value|"OpenSSL 0.9.8p-fips 16 Nov 2010"
+value|"OpenSSL 0.9.8q-fips 2 Dec 2010"
 end_define
 
 begin_else
@@ -44,7 +44,7 @@ begin_define
 define|#
 directive|define
 name|OPENSSL_VERSION_TEXT
-value|"OpenSSL 0.9.8p 16 Nov 2010"
+value|"OpenSSL 0.9.8q 2 Dec 2010"
 end_define
 
 begin_endif
