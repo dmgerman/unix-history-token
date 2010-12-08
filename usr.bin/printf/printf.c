2fa453ecbbc3c1dbecd38b6342ddc1d4e0f8fa50
Jilles Tjoelker <jilles@FreeBSD.org>
2010-12-08 22:13:27 +0000

    printf: Remove support for building as a csh builtin.
    The #define BUILTIN was for building as a csh (not tcsh) builtin.
    Given that csh was replaced by tcsh years ago there is no point in keeping
    this.
    
    The #define SHELL is for building as an sh builtin and is in active use.
    
    This commit does not change the /bin/sh and /usr/bin/printf binaries.
    
    
    Former-commit-id: 6575b72385d500b308239701814bb1657d865ae5

--- usr.bin/printf/printf.c
+++ usr.bin/printf/printf.c
@@ -3,21 +3,11 @@ begin_comment
 comment|/*  * Copyright (c) 1989, 1993  *	The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by the University of  *	California, Berkeley and its contributors.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_if
-if|#
-directive|if
-operator|!
-name|defined
-argument_list|(
-name|BUILTIN
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
+begin_ifndef
+ifndef|#
+directive|ifndef
 name|SHELL
-argument_list|)
-end_if
+end_ifndef
 
 begin_ifndef
 ifndef|#
@@ -225,22 +215,11 @@ endif|#
 directive|endif
 end_endif
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|BUILTIN
-end_ifndef
-
 begin_include
 include|#
 directive|include
 file|<locale.h>
 end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_define
 define|#
@@ -391,35 +370,18 @@ name|gargv
 decl_stmt|;
 end_decl_stmt
 
-begin_decl_stmt
+begin_function
 name|int
-ifdef|#
-directive|ifdef
-name|BUILTIN
-name|progprintf
-argument_list|(
+name|main
+parameter_list|(
 name|int
 name|argc
-argument_list|,
+parameter_list|,
 name|char
-operator|*
+modifier|*
 name|argv
 index|[]
-argument_list|)
-else|#
-directive|else
-decl|main
-argument_list|(
-name|int
-name|argc
-argument_list|,
-name|char
-operator|*
-name|argv
-index|[]
-argument_list|)
-endif|#
-directive|endif
+parameter_list|)
 block|{
 name|size_t
 name|len
@@ -443,19 +405,9 @@ decl_stmt|,
 modifier|*
 name|start
 decl_stmt|;
-if|#
-directive|if
-operator|!
-name|defined
-argument_list|(
-name|BUILTIN
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
+ifndef|#
+directive|ifndef
 name|SHELL
-argument_list|)
 operator|(
 name|void
 operator|)
@@ -771,7 +723,7 @@ expr_stmt|;
 block|}
 comment|/* NOTREACHED */
 block|}
-end_decl_stmt
+end_function
 
 begin_function
 specifier|static
