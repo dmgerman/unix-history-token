5a459c32e8ca561bfc61bdfbb61b490357f901e0
Konstantin Belousov <kib@FreeBSD.org>
2010-12-13 10:40:16 +0000

    MFC r216231: Do not leak %rdx value in the previous image to the new image after execve(2).
    Approved by:	re (bz)
    
    
    Former-commit-id: 6dc08b129ead46b8404b3fb914df9213ffc3e17d

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -3920,6 +3920,15 @@ name|tf_flags
 operator|=
 name|TF_HASSEGS
 expr_stmt|;
+name|td
+operator|->
+name|td_retval
+index|[
+literal|1
+index|]
+operator|=
+literal|0
+expr_stmt|;
 comment|/* 	 * Reset the hardware debug registers if they were in use. 	 * They won't have any meaning for the newly exec'd process. 	 */
 if|if
 condition|(
