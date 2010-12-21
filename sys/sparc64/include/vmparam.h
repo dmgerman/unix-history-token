f585a416016419bab1fa701929a7ca3cc6df484c
Marius Strobl <marius@FreeBSD.org>
2010-12-21 21:32:17 +0000

    Revert r216080 so kmem_map is capped at 3/5 of the currently rather modest kernel address space in order to leave space for the buffer cache, pipes, thread stacks, etc on machines with more physical memory until we take advantage of ASI_ATOMIC_QUAD_LDD_PHYS on CPUs providing it so we don't need to lock the kernel TSB pages into the dTLB, basically making the entire 64-bit kernel address space available on relevant machines.
    Submitted by:	alc
    
    
    Former-commit-id: b0ebd235eb3e916b649cbea19d78f25650c91b9b

--- sys/sparc64/include/vmparam.h
+++ sys/sparc64/include/vmparam.h
@@ -484,7 +484,7 @@ begin_define
 define|#
 directive|define
 name|VM_KMEM_SIZE_MAX
-value|(VM_MAX_KERNEL_ADDRESS - VM_MIN_KERNEL_ADDRESS)
+value|((VM_MAX_KERNEL_ADDRESS - \     VM_MIN_KERNEL_ADDRESS + 1) * 3 / 5)
 end_define
 
 begin_endif
