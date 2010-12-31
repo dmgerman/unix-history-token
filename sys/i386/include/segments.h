33c74c9b0a800f8712bd0064d32e5239fb8ff129
Colin Percival <cperciva@FreeBSD.org>
2010-12-31 17:42:25 +0000

    Make i386_set_ldt work on i386/XEN, step 5/5.
    When cleaning up a thread, reset its LDT to the default LDT.
    
    Note: Casting the LDT pointer to an int and storing it in pc_currentldt is
    wildly bogus, but is harmless since pc_currentldt is a write-only variable.
    
    MFC after:	3 days
    
    
    Former-commit-id: 35530a5cc48af4965d01b28632295c22f3706c62

--- sys/i386/include/segments.h
+++ sys/i386/include/segments.h
@@ -1476,6 +1476,14 @@ end_ifdef
 
 begin_decl_stmt
 specifier|extern
+name|struct
+name|proc_ldt
+name|default_proc_ldt
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
 name|union
 name|descriptor
 modifier|*
