cd0ff65cc2832611917b8b4280fdd0b6ab08600e
Stanislav Sedov <stas@FreeBSD.org>
2011-01-06 07:13:23 +0000

    - Show textual representation of ioctl command in warning message   if ioctl(4) is failed besides the command number.
    Suggested by:	delphij
    MFC after:	2 weeks
    
    
    Former-commit-id: 65862a27a0696b4733f9d82372afd9cd1be8d7e7

--- usr.sbin/cpucontrol/cpucontrol.c
+++ usr.sbin/cpucontrol/cpucontrol.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2008 Stanislav Sedov<stas@FreeBSD.org>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2008-2011 Stanislav Sedov<stas@FreeBSD.org>.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
 begin_comment
@@ -765,6 +765,11 @@ name|fd
 decl_stmt|,
 name|error
 decl_stmt|;
+specifier|const
+name|char
+modifier|*
+name|command_name
+decl_stmt|;
 name|char
 modifier|*
 name|endptr
@@ -1027,6 +1032,10 @@ name|command
 operator|=
 name|CPUCTL_RDMSR
 expr_stmt|;
+name|command_name
+operator|=
+literal|"CPUCTL_RDMSR"
+expr_stmt|;
 break|break;
 case|case
 name|OP_WRITE
@@ -1034,6 +1043,10 @@ case|:
 name|command
 operator|=
 name|CPUCTL_WRMSR
+expr_stmt|;
+name|command_name
+operator|=
+literal|"CPUCTL_WRMSR"
 expr_stmt|;
 break|break;
 case|case
@@ -1043,6 +1056,10 @@ name|command
 operator|=
 name|CPUCTL_MSRSBIT
 expr_stmt|;
+name|command_name
+operator|=
+literal|"CPUCTL_MSRSBIT"
+expr_stmt|;
 break|break;
 case|case
 name|OP_AND
@@ -1050,6 +1067,10 @@ case|:
 name|command
 operator|=
 name|CPUCTL_MSRCBIT
+expr_stmt|;
+name|command_name
+operator|=
+literal|"CPUCTL_MSRCBIT"
 expr_stmt|;
 break|break;
 default|default:
@@ -1125,9 +1146,11 @@ name|WARN
 argument_list|(
 literal|0
 argument_list|,
-literal|"ioctl(%s, %lu)"
+literal|"ioctl(%s, %s (%lu))"
 argument_list|,
 name|dev
+argument_list|,
+name|command_name
 argument_list|,
 name|command
 argument_list|)
