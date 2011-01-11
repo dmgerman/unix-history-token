159b650065616bec959ae7ce27dbf2c3cdbf36fa
Dimitry Andric <dim@FreeBSD.org>
2011-01-11 19:13:29 +0000

    MFC r215212:
    Similar to r212647, remove the workaround in sys/net/vnet.h for an ld
    bug (incorrect placement of __start_SECNAME in some cases) that was
    fixed in r210245.
    
    There is already an UPDATING entry about needing a recent ld.
    
    
    Former-commit-id: 64dc1cebd097f3eb95b37544202ef931b572adad

--- sys/net/vnet.h
+++ sys/net/vnet.h
@@ -509,23 +509,6 @@ begin_comment
 comment|/*  * Virtual network stack memory allocator, which allows global variables to  * be automatically instantiated for each network stack instance.  */
 end_comment
 
-begin_asm
-asm|__asm__(
-if|#
-directive|if
-name|defined
-argument_list|(
-name|__arm__
-argument_list|)
-asm|".section " VNET_SETNAME ", \"aw\", %progbits\n"
-else|#
-directive|else
-asm|".section " VNET_SETNAME ", \"aw\", @progbits\n"
-endif|#
-directive|endif
-asm|"\t.p2align " __XSTRING(CACHE_LINE_SHIFT) "\n" 	"\t.previous");
-end_asm
-
 begin_define
 define|#
 directive|define
