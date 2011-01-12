6fe747440225218b393447a764af2781841b5d92
Pawel Jakub Dawidek <pjd@FreeBSD.org>
2011-01-12 14:13:50 +0000

    svn merge -c215331,215332,215676 svn+ssh://svn.freebsd.org/base/head/sbin/hastd sbin/hastd
    MFC r215331,r215332,r215676:
    
    r215676 (by brucec):
    
    Don't generate input() since it's not used.
    
    r215332:
    
    Move timeout.tv_sec initialization outside the loop - sigtimedwait(2) won't
    modify it.
    
    Submitted by:	Mikolaj Golub <to.my.trociny@gmail.com>
    
    r215331:
    
    1. Exit when we cannot create incoming connection.
    2. Improve logging to inform which connection can't be created.
    
    Submitted by:	[1] Mikolaj Golub <to.my.trociny@gmail.com>
    
    
    Former-commit-id: fd345baa0780114453ae337eb8258130eae259f4

--- sbin/hastd/primary.c
+++ sbin/hastd/primary.c
@@ -2202,7 +2202,7 @@ name|primary_exit
 argument_list|(
 name|EX_TEMPFAIL
 argument_list|,
-literal|"Unable to create connection to %s"
+literal|"Unable to create outgoing connection to %s"
 argument_list|,
 name|res
 operator|->
@@ -2527,11 +2527,11 @@ operator|<
 literal|0
 condition|)
 block|{
-name|pjdlog_errno
+name|primary_exit
 argument_list|(
-name|LOG_WARNING
+name|EX_TEMPFAIL
 argument_list|,
-literal|"Unable to create connection to %s"
+literal|"Unable to create incoming connection to %s"
 argument_list|,
 name|res
 operator|->
@@ -9332,6 +9332,12 @@ argument_list|)
 expr_stmt|;
 name|timeout
 operator|.
+name|tv_sec
+operator|=
+name|RETRY_SLEEP
+expr_stmt|;
+name|timeout
+operator|.
 name|tv_nsec
 operator|=
 literal|0
@@ -9428,12 +9434,6 @@ operator|=
 name|now
 expr_stmt|;
 block|}
-name|timeout
-operator|.
-name|tv_sec
-operator|=
-name|RETRY_SLEEP
-expr_stmt|;
 name|signo
 operator|=
 name|sigtimedwait
