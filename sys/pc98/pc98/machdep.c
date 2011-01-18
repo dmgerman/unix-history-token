933f2e1c5c2bd5f28cc9988a3b23f6e3592773df
Takahashi Yoshihiro <nyan@FreeBSD.org>
2011-01-18 15:35:13 +0000

    MFi386: revision 217515
    The mem_range_softc is defined in mem.c.
    
    
    Former-commit-id: adb36787ecddaf0a3464c87fa817abc3789a2702

--- sys/pc98/pc98/machdep.c
+++ sys/pc98/pc98/machdep.c
@@ -200,12 +200,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/memrange.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/msgbuf.h>
 end_include
 
@@ -1023,13 +1017,6 @@ begin_decl_stmt
 name|struct
 name|mtx
 name|icu_lock
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|struct
-name|mem_range_softc
-name|mem_range_softc
 decl_stmt|;
 end_decl_stmt
 
