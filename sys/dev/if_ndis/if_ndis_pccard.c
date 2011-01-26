88f82bd3e8e45ccc8de327bdbd3e67a0774a2dd0
John Baldwin <jhb@FreeBSD.org>
2011-01-26 20:36:20 +0000

    MFC 216486,216764: Use bus_alloc_resource_any() instead of bus_alloc_resource().
    Former-commit-id: 6cb4dcf4a436b85247eafe84e92494217aa45260

--- sys/dev/if_ndis/if_ndis_pccard.c
+++ sys/dev/if_ndis/if_ndis_pccard.c
@@ -771,7 +771,7 @@ name|sc
 operator|->
 name|ndis_res_io
 operator|=
-name|bus_alloc_resource
+name|bus_alloc_resource_any
 argument_list|(
 name|dev
 argument_list|,
@@ -781,13 +781,6 @@ operator|&
 name|sc
 operator|->
 name|ndis_io_rid
-argument_list|,
-literal|0
-argument_list|,
-operator|~
-literal|0
-argument_list|,
-literal|1
 argument_list|,
 name|RF_ACTIVE
 argument_list|)
@@ -862,7 +855,7 @@ name|sc
 operator|->
 name|ndis_irq
 operator|=
-name|bus_alloc_resource
+name|bus_alloc_resource_any
 argument_list|(
 name|dev
 argument_list|,
@@ -870,13 +863,6 @@ name|SYS_RES_IRQ
 argument_list|,
 operator|&
 name|rid
-argument_list|,
-literal|0
-argument_list|,
-operator|~
-literal|0
-argument_list|,
-literal|1
 argument_list|,
 name|RF_SHAREABLE
 operator||
--- sys/dev/mpt/mpt_pci.c
+++ sys/dev/mpt/mpt_pci.c
@@ -2004,7 +2004,7 @@ name|mpt
 operator|->
 name|pci_pio_reg
 operator|=
-name|bus_alloc_resource
+name|bus_alloc_resource_any
 argument_list|(
 name|dev
 argument_list|,
@@ -2014,13 +2014,6 @@ operator|&
 name|mpt
 operator|->
 name|pci_pio_rid
-argument_list|,
-literal|0
-argument_list|,
-operator|~
-literal|0
-argument_list|,
-literal|0
 argument_list|,
 name|RF_ACTIVE
 argument_list|)
@@ -2081,7 +2074,7 @@ name|mpt
 operator|->
 name|pci_reg
 operator|=
-name|bus_alloc_resource
+name|bus_alloc_resource_any
 argument_list|(
 name|dev
 argument_list|,
@@ -2091,13 +2084,6 @@ operator|&
 name|mpt
 operator|->
 name|pci_mem_rid
-argument_list|,
-literal|0
-argument_list|,
-operator|~
-literal|0
-argument_list|,
-literal|0
 argument_list|,
 name|RF_ACTIVE
 argument_list|)
