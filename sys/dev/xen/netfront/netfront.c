9bb7456644a1a464406a79e8dc46af560aa0aaaa
Justin T. Gibbs <gibbs@FreeBSD.org>
2011-01-29 02:36:45 +0000

    Fix bug in the netfront driver that caused excessive packet drops during receive processing.
    Remove unnecessary restrictions on the mbuf chain length built during an
    LRO receive.  This restriction was copied from the Linux netfront driver
    where the LRO implementation cannot handle more than 18 discontinuities.
    The FreeBSD implementation has no such restriction.
    
    MFC after: 1 week
    
    
    Former-commit-id: 40c8c903815be1da94822b5bcc29123328b34301

--- sys/dev/xen/netfront/netfront.c
+++ sys/dev/xen/netfront/netfront.c
@@ -5583,12 +5583,6 @@ operator|*
 name|cons
 decl_stmt|;
 name|int
-name|max
-init|=
-literal|5
-comment|/* MAX_TX_REQ_FRAGS + (rx->status<= RX_COPY_THRESHOLD) */
-decl_stmt|;
-name|int
 name|frags
 init|=
 literal|1
@@ -6143,42 +6137,6 @@ name|list
 operator|=
 name|m0
 expr_stmt|;
-if|if
-condition|(
-name|unlikely
-argument_list|(
-name|frags
-operator|>
-name|max
-argument_list|)
-condition|)
-block|{
-if|if
-condition|(
-name|net_ratelimit
-argument_list|()
-condition|)
-name|WPRINTK
-argument_list|(
-literal|"Too many frags\n"
-argument_list|)
-expr_stmt|;
-name|printf
-argument_list|(
-literal|"%s: too many frags %d> max %d\n"
-argument_list|,
-name|__func__
-argument_list|,
-name|frags
-argument_list|,
-name|max
-argument_list|)
-expr_stmt|;
-name|err
-operator|=
-name|E2BIG
-expr_stmt|;
-block|}
 operator|*
 name|cons
 operator|+=
@@ -6190,7 +6148,9 @@ operator|=
 name|pages_flipped
 expr_stmt|;
 return|return
+operator|(
 name|err
+operator|)
 return|;
 block|}
 end_function
