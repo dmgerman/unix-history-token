ae3ed9d3b8e0e4234c02c7a7a79a9d487e91eb83
Konstantin Belousov <kib@FreeBSD.org>
2011-02-12 13:17:14 +0000

    In checker, read journal by sectors.
    Due to UFS insistence to pretend that device sector size is 512 bytes,
    sector size is obtained from ioctl(DIOCGSECTORSIZE) for real devices,
    and from the label otherwise. The file images without label have to
    be made with 512 sector size.
    
    In collaboration with:	pho
    Reviewed by:	jeff
    Tested by:	bz, pho
    
    
    Former-commit-id: 5a7b139567eaee3be41f3f2668a3f40a1436e9b3

--- sbin/fsck_ffs/fsck.h
+++ sbin/fsck_ffs/fsck.h
@@ -992,6 +992,12 @@ comment|/* actual disk sector size */
 end_comment
 
 begin_decl_stmt
+name|long
+name|real_dev_bsize
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
 name|char
 name|nflag
 decl_stmt|;
