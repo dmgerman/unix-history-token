f749b2f62339d09ee6c5677aeb1a5c6a3168f7f0
Takahashi Yoshihiro <nyan@FreeBSD.org>
2011-02-16 15:20:37 +0000

    MFi386: revision 218713
      Apply a few small optimizations to boot2's code, to make it shrink a
      little further.
    
    
    Former-commit-id: 73b58e58ef4a7929c9dc60f86c4b39aaed45dfcd

--- sys/boot/pc98/boot2/boot2.c
+++ sys/boot/pc98/boot2/boot2.c
@@ -647,7 +647,7 @@ end_decl_stmt
 
 begin_decl_stmt
 specifier|static
-name|uint32_t
+name|uint16_t
 name|opts
 decl_stmt|;
 end_decl_stmt
@@ -1864,7 +1864,7 @@ name|i
 decl_stmt|;
 endif|#
 directive|endif
-name|int
+name|uint8_t
 name|autoboot
 decl_stmt|;
 name|ino_t
@@ -2349,11 +2349,12 @@ decl_stmt|,
 name|x
 decl_stmt|;
 name|int
-name|fmt
-decl_stmt|,
 name|i
 decl_stmt|,
 name|j
+decl_stmt|;
+name|uint8_t
+name|fmt
 decl_stmt|;
 if|if
 condition|(
