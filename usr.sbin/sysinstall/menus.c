11bfb8bc3d8987e351c758ba973c6b949f9250f9
Bruce Cran <brucec@FreeBSD.org>
2011-02-22 17:38:43 +0000

    MFC r218840:
    Remove the quotas option from the Startup Services menu.
    GENERIC has no support for quotas so this option has no effect.
    
    PR:           bin/123237
    Submitted by: Lawrence Mayer <lawmay3 at i12.com>
    
    Former-commit-id: 30eeb47a940e93be9a0c33b687573f3b0b93ed8d

--- usr.sbin/sysinstall/menus.c
+++ usr.sbin/sysinstall/menus.c
@@ -7500,20 +7500,6 @@ block|,
 endif|#
 directive|endif
 block|{
-literal|" quotas"
-block|,
-literal|"This host wishes to check quotas on startup."
-block|,
-name|dmenuVarCheck
-block|,
-name|dmenuToggleVariable
-block|,
-name|NULL
-block|,
-literal|"check_quotas=YES"
-block|}
-block|,
-block|{
 name|NULL
 block|}
 block|}
