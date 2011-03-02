ab28ade7ccb2dbc58aa5b0897064685a44838a5e
Dmitry Chagin <dchagin@FreeBSD.org>
2011-03-02 06:22:18 +0000

    MFC r218609: Remove unused since r134586 thr_exit1() declaration.
    Former-commit-id: f9b6e2675139102859b8b1c2d20b7fc104e46254

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -5203,15 +5203,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|void
-name|thr_exit1
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_endif
 endif|#
 directive|endif
