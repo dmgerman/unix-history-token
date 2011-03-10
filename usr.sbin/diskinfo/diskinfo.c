15f85ea0f4cdc032e7724c6c3656e34f10c67907
Xin Li <delphij@FreeBSD.org>
2011-03-10 01:02:53 +0000

    MFC r218432:
    Don't consider ioctl returning error as a fatal error and move on to next
    disk.  When multiple devices are specified, a non-supporting ioctl will
    cause diskinfo(8) to stop at the point which does not seem to be necessary.
    
    
    Former-commit-id: 56b12baeab23d672bfeaef7880392d372db82c8a

--- usr.sbin/diskinfo/diskinfo.c
+++ usr.sbin/diskinfo/diskinfo.c
@@ -158,6 +158,10 @@ decl_stmt|,
 name|fd
 decl_stmt|,
 name|error
+decl_stmt|,
+name|exitval
+init|=
+literal|0
 decl_stmt|;
 name|char
 name|buf
@@ -334,9 +338,10 @@ name|fd
 operator|<
 literal|0
 condition|)
-name|err
+block|{
+name|warn
 argument_list|(
-literal|1
+literal|"%s"
 argument_list|,
 name|argv
 index|[
@@ -344,6 +349,14 @@ name|i
 index|]
 argument_list|)
 expr_stmt|;
+name|exitval
+operator|=
+literal|1
+expr_stmt|;
+goto|goto
+name|out
+goto|;
+block|}
 name|error
 operator|=
 name|ioctl
@@ -360,10 +373,9 @@ if|if
 condition|(
 name|error
 condition|)
-name|err
+block|{
+name|warn
 argument_list|(
-literal|1
-argument_list|,
 literal|"%s: ioctl(DIOCGMEDIASIZE) failed, probably not a disk."
 argument_list|,
 name|argv
@@ -372,6 +384,14 @@ name|i
 index|]
 argument_list|)
 expr_stmt|;
+name|exitval
+operator|=
+literal|1
+expr_stmt|;
+goto|goto
+name|out
+goto|;
+block|}
 name|error
 operator|=
 name|ioctl
@@ -388,10 +408,9 @@ if|if
 condition|(
 name|error
 condition|)
-name|err
+block|{
+name|warn
 argument_list|(
-literal|1
-argument_list|,
 literal|"%s: DIOCGSECTORSIZE failed, probably not a disk."
 argument_list|,
 name|argv
@@ -400,6 +419,14 @@ name|i
 index|]
 argument_list|)
 expr_stmt|;
+name|exitval
+operator|=
+literal|1
+expr_stmt|;
+goto|goto
+name|out
+goto|;
+block|}
 name|error
 operator|=
 name|ioctl
@@ -709,6 +736,8 @@ argument_list|,
 name|sectorsize
 argument_list|)
 expr_stmt|;
+name|out
+label|:
 name|close
 argument_list|(
 name|fd
@@ -717,7 +746,7 @@ expr_stmt|;
 block|}
 name|exit
 argument_list|(
-literal|0
+name|exitval
 argument_list|)
 expr_stmt|;
 block|}
