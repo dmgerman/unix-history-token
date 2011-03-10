3ff00d773226167ec11aff29086c0b31b9745c1f
John Baldwin <jhb@FreeBSD.org>
2011-03-10 19:50:12 +0000

    Remove now-obsolete comment.
    Submitted by:	netchild
    MFC after:	1 week
    
    
    Former-commit-id: c71a010b61df0534066e27f619ac3e7b032c0442

--- sys/compat/linux/linux_futex.c
+++ sys/compat/linux/linux_futex.c
@@ -1913,7 +1913,7 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
-comment|/* XXX: linux verifies access here and returns EFAULT */
+comment|/* XXX: Linux verifies access here and returns EFAULT */
 switch|switch
 condition|(
 name|op
@@ -2458,7 +2458,6 @@ operator|->
 name|val3
 argument_list|)
 expr_stmt|;
-comment|/* 		 * XXX: Linux is able to cope with different addresses 		 * corresponding to the same mapped memory in the sleeping 		 * and waker process(es). 		 */
 ifdef|#
 directive|ifdef
 name|DEBUG
