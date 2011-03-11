affc3ad4a7b0589193e7eaefe74c89a45ce993e0
Marius Strobl <marius@FreeBSD.org>
2011-03-11 21:24:02 +0000

    Now that TLS is supported for sparc64 by both binutils 2.17.50 committed in r218822 and rtld(1) committed in r219533 turn on TLS support in GCC.
    Former-commit-id: 9941f4cbea910b1f0f4c7e0b0454a426517dd03e

--- gnu/usr.bin/cc/cc_tools/auto-host.h
+++ gnu/usr.bin/cc/cc_tools/auto-host.h
@@ -909,12 +909,6 @@ directive|if
 operator|!
 name|defined
 argument_list|(
-name|__sparc64__
-argument_list|)
-operator|&&
-operator|!
-name|defined
-argument_list|(
 name|__arm__
 argument_list|)
 end_if
