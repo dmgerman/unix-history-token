244ba8ea27070441ba213a7bf14a56e0092051e5
Fabien Thomas <fabient@FreeBSD.org>
2011-03-31 13:59:47 +0000

    Clearing the flag when preempting will let the preempted thread run too much time. This can finish in a scheduler deadlock with ping-pong between two threads.
    One sample of this is:
    - device lapic (to have a preemption point on critical_exit())
    - options DEVICE_POLLING with HZ>1499 (to have lapic freq = hardclock freq)
    - running a cpu intensive task (that does not enter the kernel)
    - only one CPU on SMP or no SMP.
    
    As requested by jhb@ 4BSD have received the same type of fix instead of
    propagating the flag to the new thread.
    
    Reviewed by:	jhb, jeff
    MFC after:	1 month
    
    
    Former-commit-id: 2021e00d8469740d044c76deace3e1d1870a4051

--- sys/kern/sched_ule.c
+++ sys/kern/sched_ule.c
@@ -7699,6 +7699,15 @@ name|td_oncpu
 operator|=
 name|NOCPU
 expr_stmt|;
+if|if
+condition|(
+operator|!
+operator|(
+name|flags
+operator|&
+name|SW_PREEMPT
+operator|)
+condition|)
 name|td
 operator|->
 name|td_flags
