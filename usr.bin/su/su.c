6645b8fa3b17b5cad4400d27eb1ff4c811ffafb0
Hajimu UMEMOTO <ume@FreeBSD.org>
2011-04-04 14:57:28 +0000

    MFC r220055: getlogin() may return NULL.
    Former-commit-id: 621d900e0f8ce158cc22125368e69e90be43152e

--- usr.bin/su/su.c
+++ usr.bin/su/su.c
@@ -352,6 +352,8 @@ name|struct
 name|passwd
 modifier|*
 name|pwd
+init|=
+name|NULL
 decl_stmt|;
 name|struct
 name|pam_conv
@@ -861,6 +863,12 @@ operator|=
 name|getlogin
 argument_list|()
 expr_stmt|;
+if|if
+condition|(
+name|username
+operator|!=
+name|NULL
+condition|)
 name|pwd
 operator|=
 name|getpwnam
@@ -870,10 +878,6 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|username
-operator|==
-name|NULL
-operator|||
 name|pwd
 operator|==
 name|NULL
