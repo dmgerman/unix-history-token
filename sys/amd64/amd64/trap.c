eea0a03b36d127198f3130e50e03a4926f394ecb
Ryan Stone <rstone@FreeBSD.org>
2011-04-16 00:10:55 +0000

    MFC r220453:  Add tunables that mirror the functionality of sysctls machdep.panic_on_nmi  and machdep.kdb_on_nmi.
    Approved by:	emaste (mentor)
    
    
    Former-commit-id: d8f1da5b1cb19e91d0c648681dbff0d9f48f0b74

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -579,6 +579,17 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|TUNABLE_INT
+argument_list|(
+literal|"machdep.kdb_on_nmi"
+argument_list|,
+operator|&
+name|kdb_on_nmi
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_endif
 endif|#
 directive|endif
@@ -610,6 +621,17 @@ argument_list|,
 literal|0
 argument_list|,
 literal|"Panic on NMI"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|TUNABLE_INT
+argument_list|(
+literal|"machdep.panic_on_nmi"
+argument_list|,
+operator|&
+name|panic_on_nmi
 argument_list|)
 expr_stmt|;
 end_expr_stmt
--- sys/i386/i386/trap.c
+++ sys/i386/i386/trap.c
@@ -662,6 +662,17 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|TUNABLE_INT
+argument_list|(
+literal|"machdep.kdb_on_nmi"
+argument_list|,
+operator|&
+name|kdb_on_nmi
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_endif
 endif|#
 directive|endif
@@ -693,6 +704,17 @@ argument_list|,
 literal|0
 argument_list|,
 literal|"Panic on NMI"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|TUNABLE_INT
+argument_list|(
+literal|"machdep.panic_on_nmi"
+argument_list|,
+operator|&
+name|panic_on_nmi
 argument_list|)
 expr_stmt|;
 end_expr_stmt
