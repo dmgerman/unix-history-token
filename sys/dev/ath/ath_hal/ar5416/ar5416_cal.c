f914c1ee4c6cea92cb1c5ad5739881335d5e00db
Adrian Chadd <adrian@FreeBSD.org>
2011-05-11 10:30:31 +0000

    Remove a now unneeded comment..
    Former-commit-id: 947df630ad96ad6815e0002fbab8fc5ce4a4cc12

--- sys/dev/ath/ath_hal/ar5416/ar5416_cal.c
+++ sys/dev/ath/ath_hal/ar5416/ar5416_cal.c
@@ -822,7 +822,6 @@ name|AH_TRUE
 argument_list|)
 expr_stmt|;
 comment|/*  	 * Do NF calibration after DC offset and other CALs. 	 * Per system engineers, noise floor value can sometimes be 20 dB 	 * higher than normal value if DC offset and noise floor cal are 	 * triggered at the same time. 	 */
-comment|/* XXX this actually kicks off a NF calibration -adrian */
 name|OS_REG_SET_BIT
 argument_list|(
 name|ah
