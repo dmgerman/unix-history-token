468145c2f8a54d9f6ea5f9f075b92c7024ede202
Bernhard Schmidt <bschmidt@FreeBSD.org>
2011-05-31 19:08:25 +0000

    Add module version to iwi/ipw/wpi and iwn.
    The version is used to check if a module is already preset, not setting
    it results in:
    
    can't re-use a leaf (ipw)!
    module_register: module pci/ipw already exists!
    Module pci/ipw failed to register: 17
    
    while trying to load the module due to an entry in loader.conf. With this
    commit we get the expected:
    
    module ipw already present!
    
    Reported by:	Dru Lavigne, bz
    Tested by:	bz
    MFC after:	1 week
    
    
    Former-commit-id: df7f7a4e8694faf6e315dcc1a0b12bd8960fa165

--- sys/dev/iwn/if_iwn.c
+++ sys/dev/iwn/if_iwn.c
@@ -3266,6 +3266,16 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|iwn
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|MODULE_DEPEND
 argument_list|(
 name|iwn
--- sys/dev/wpi/if_wpi.c
+++ sys/dev/wpi/if_wpi.c
@@ -1773,6 +1773,16 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|wpi
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_decl_stmt
 specifier|static
 specifier|const
