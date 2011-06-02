d92d3f31d079fcaf8539763b6522e6640dbb7b99
Ed Maste <emaste@FreeBSD.org>
2011-06-02 00:43:16 +0000

    There are a couple of structs in mfireg.h with members named 'class'. These cause problems when trying to include the header in a C++ project. Rename them to 'evt_class', and track the change in mfi and mfiutil.
    Submitted by:	Mark Johnston
    Sponsored by:	Sandvine Incorporated
    Reviewed by:	jhb@
    MFC after:	1 week
    
    
    Former-commit-id: 98e0601fedba63de5c4b171e79c76b5c73eba428

--- usr.sbin/mfiutil/mfi_evt.c
+++ usr.sbin/mfiutil/mfi_evt.c
@@ -1776,7 +1776,7 @@ name|format_locale
 argument_list|(
 name|detail
 operator|->
-name|class
+name|evt_class
 operator|.
 name|members
 operator|.
@@ -1787,11 +1787,11 @@ name|format_class
 argument_list|(
 name|detail
 operator|->
-name|class
+name|evt_class
 operator|.
 name|members
 operator|.
-name|class
+name|evt_class
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -2877,7 +2877,7 @@ name|filter
 operator|.
 name|members
 operator|.
-name|class
+name|evt_class
 operator|=
 name|MFI_EVT_CLASS_WARNING
 expr_stmt|;
@@ -2940,7 +2940,7 @@ name|filter
 operator|.
 name|members
 operator|.
-name|class
+name|evt_class
 argument_list|)
 operator|<
 literal|0
