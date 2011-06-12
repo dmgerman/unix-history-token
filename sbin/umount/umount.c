872ec295af2de9639a710ea3dfa4f3407b16b9be
Rick Macklem <rmacklem@FreeBSD.org>
2011-06-12 02:05:59 +0000

    MFC: r222466 Modify the umount(8) command so that it doesn't do a sync(2) syscall before unmount(2) for the "-f" case. This avoids a forced dismount from getting stuck for an NFS mountpoint in sync() when the server is not responsive. With this commit, forced dismounts should normally work for the NFS clients, but can take up to about 1minute to complete.
    Former-commit-id: d545336dcd33d83c71fb03a5c0bfb3bafe434d47

--- sbin/umount/umount.c
+++ sbin/umount/umount.c
@@ -422,10 +422,6 @@ name|struct
 name|addrinfo
 name|hints
 decl_stmt|;
-comment|/* Start disks transferring immediately. */
-name|sync
-argument_list|()
-expr_stmt|;
 name|all
 operator|=
 name|errs
@@ -546,6 +542,20 @@ expr_stmt|;
 name|argv
 operator|+=
 name|optind
+expr_stmt|;
+comment|/* Start disks transferring immediately. */
+if|if
+condition|(
+operator|(
+name|fflag
+operator|&
+name|MNT_FORCE
+operator|)
+operator|==
+literal|0
+condition|)
+name|sync
+argument_list|()
 expr_stmt|;
 if|if
 condition|(
