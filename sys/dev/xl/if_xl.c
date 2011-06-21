aa02f3ef75bbb37d16fc987bc3ec82903cfb9b55
Warner Losh <imp@FreeBSD.org>
2011-06-21 22:16:04 +0000

    My broken 'u' key scks!
    Former-commit-id: a81ed9d3901081586c5df841175a586faa991d4a

--- sys/dev/xl/if_xl.c
+++ sys/dev/xl/if_xl.c
@@ -1485,7 +1485,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Murphy's law says that it's possible the chip can wedge and  * the 'command in progress' bit may never clear. Hence, we wait  * only a finite amount of time to avoid getting caught in an  * infinite loop. Normally this delay routine would be a macro,  * but it isn't called during normal operation so we can afford  * to make it a function.  Spress warning when card gone.  */
+comment|/*  * Murphy's law says that it's possible the chip can wedge and  * the 'command in progress' bit may never clear. Hence, we wait  * only a finite amount of time to avoid getting caught in an  * infinite loop. Normally this delay routine would be a macro,  * but it isn't called during normal operation so we can afford  * to make it a function.  Supress warning when card gone.  */
 end_comment
 
 begin_function
