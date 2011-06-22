03624370d2a2297b6e6e0f3b8a302e58f3b55b73
Hans Petter Selasky <hselasky@FreeBSD.org>
2011-06-22 08:09:50 +0000

    MFC r219221: Remove dependency to ucom from ulpt.
    Former-commit-id: 229f9db0aa9b65fbadbb646121b2c4f476db100f

--- sys/dev/usb/serial/ulpt.c
+++ sys/dev/usb/serial/ulpt.c
@@ -3101,22 +3101,6 @@ expr_stmt|;
 end_expr_stmt
 
 begin_expr_stmt
-name|MODULE_DEPEND
-argument_list|(
-name|ulpt
-argument_list|,
-name|ucom
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_expr_stmt
 name|MODULE_VERSION
 argument_list|(
 name|ulpt
