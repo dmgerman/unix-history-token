42aef0b16909a27c97f87b7331185abe95704199
Andrey V. Elsukov <ae@FreeBSD.org>
2011-06-28 04:57:53 +0000

    MFC r223364:   When user specifies the bootcode with size smaller than VTOC_BOOTSIZE,   gpart_write_partcode_vtoc8 does access out of range of allocated memory.   Check size of bootcode before writing it.
      Pointed out by:	ru
    
    
    Former-commit-id: 579f743e213c31bcd87934abd68005a305f7eb95

--- sbin/geom/class/part/geom_part.c
+++ sbin/geom/class/part/geom_part.c
@@ -7595,6 +7595,20 @@ argument_list|)
 expr_stmt|;
 block|}
 else|else
+block|{
+if|if
+condition|(
+name|partsize
+operator|!=
+name|VTOC_BOOTSIZE
+condition|)
+name|errx
+argument_list|(
+name|EXIT_FAILURE
+argument_list|,
+literal|"invalid bootcode"
+argument_list|)
+expr_stmt|;
 name|gpart_write_partcode_vtoc8
 argument_list|(
 name|gp
@@ -7604,6 +7618,7 @@ argument_list|,
 name|partcode
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 elseif|else
 if|if
