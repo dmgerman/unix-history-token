9cd9977ba1feab766b2c5c45c896a8516632e16a
Jonathan Anderson <jonathan@FreeBSD.org>
2011-07-05 14:58:10 +0000

    Merge r223505 from HEAD to stable/7:
    --
    Tidy up a capabilities-related comment.
    
    This commit refers to an #ifdef that hasn't been merged [yet?]; remove it.
    
    Approved by: rwatson
    --
    
    
    Former-commit-id: c3cda5790c9087ec7e30ad74ab442166878d3485

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -15942,7 +15942,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Common filesystem object access control check routine.  Accepts a  * vnode's type, "mode", uid and gid, requested access mode, credentials,  * and optional call-by-reference privused argument allowing vaccess()  * to indicate to the caller whether privilege was used to satisfy the  * request (obsoleted).  Returns 0 on success, or an errno on failure.  *  * The ifdef'd CAPABILITIES version is here for reference, but is not  * actually used.  */
+comment|/*  * Common filesystem object access control check routine.  Accepts a  * vnode's type, "mode", uid and gid, requested access mode, credentials,  * and optional call-by-reference privused argument allowing vaccess()  * to indicate to the caller whether privilege was used to satisfy the  * request (obsoleted).  Returns 0 on success, or an errno on failure.  */
 end_comment
 
 begin_function
