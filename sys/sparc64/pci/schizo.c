d938af03032e745fdfe124dc98230d2568458ff0
Marius Strobl <marius@FreeBSD.org>
2011-07-12 17:55:34 +0000

    - Add a missing shift in schizo_get_timecount(). This happened to be non-fatal   as STX_CTRL_PERF_CNT_CNT0_SHIFT actually is zero, if we were using the   second counter in the upper 32 bits this would be required though as the MI   timecounter code doesn't support 64-bit counters/counter registers. - Remove a redundant NULL assignment from the timecounter initialization.
    Former-commit-id: ab481a1d73ebfefe8513dce4cf4da8c74107a19a

--- sys/sparc64/pci/schizo.c
+++ sys/sparc64/pci/schizo.c
@@ -2359,12 +2359,6 @@ name|schizo_get_timecount
 expr_stmt|;
 name|tc
 operator|->
-name|tc_poll_pps
-operator|=
-name|NULL
-expr_stmt|;
-name|tc
-operator|->
 name|tc_counter_mask
 operator|=
 name|STX_CTRL_PERF_CNT_MASK
@@ -7999,6 +7993,7 @@ name|tc_priv
 expr_stmt|;
 return|return
 operator|(
+operator|(
 name|SCHIZO_CTRL_READ_8
 argument_list|(
 name|sc
@@ -8011,6 +8006,9 @@ name|STX_CTRL_PERF_CNT_MASK
 operator|<<
 name|STX_CTRL_PERF_CNT_CNT0_SHIFT
 operator|)
+operator|)
+operator|>>
+name|STX_CTRL_PERF_CNT_CNT0_SHIFT
 operator|)
 return|;
 block|}
