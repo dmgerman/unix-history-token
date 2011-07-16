90776155ed876753fc14efd8af905445301dc4aa
Zack Kirsch <zack@FreeBSD.org>
2011-07-16 08:51:09 +0000

    Add DEXITCODE plumbing to NFS.
    Isilon has the concept of an in-memory exit-code ring that saves the last exit
    code of a function and allows for stack tracing. This is very helpful when
    debugging tough issues.
    
    This patch is essentially a no-op for BSD at this point, until we upstream
    the dexitcode logic itself. The patch adds DEXITCODE calls to every NFS
    function that returns an errno error code. A number of code paths were also
    reorganized to have single exit paths, to reduce code duplication.
    
    Submitted by:   David Kwan <dkwan@isilon.com>
    Reviewed by:    rmacklem
    Approved by:    zml (mentor)
    MFC after:      2 weeks
    
    
    Former-commit-id: 42912698d212509238dbaba7da4415029307f47f

--- sys/fs/nfs/nfsdport.h
+++ sys/fs/nfs/nfsdport.h
@@ -201,6 +201,30 @@ block|}
 struct|;
 end_struct
 
+begin_comment
+comment|/*  * These are NO-OPS for BSD until Isilon upstreams EXITCODE support.  * EXITCODE is an in-memory ring buffer that holds the routines failing status.  * This is a valuable tool to use when debugging and analyzing issues.  * In addition to recording a routine's failing status, it offers  * logging of routines for call stack tracing.  * EXITCODE should be used only in routines that return a true errno value, as  * that value will be formatted to a displayable errno string.  Routines that   * return regular int status that are not true errno should not set EXITCODE.  * If you want to log routine tracing, you can add EXITCODE(0) to any routine.  * NFS extended the EXITCODE with EXITCODE2 to record either the routine's  * exit errno status or the nd_repstat.  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NFSEXITCODE
+parameter_list|(
+name|error
+parameter_list|)
+end_define
+
+begin_define
+define|#
+directive|define
+name|NFSEXITCODE2
+parameter_list|(
+name|error
+parameter_list|,
+name|nd
+parameter_list|)
+end_define
+
 begin_define
 define|#
 directive|define
