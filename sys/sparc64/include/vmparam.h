11dc0c2ce56da629d34d5982156b163636c9b2a7
Marius Strobl <marius@FreeBSD.org>
2011-07-25 18:28:12 +0000

    MFC: r223378
    On machines where we don't need to lock the kernel TSB into the dTLB and
    thus may basically use the entire 64-bit kernel address space reduce
    VM_KMEM_SIZE_SCALE to 1 allowing kernel to use more memory.
    
    
    Former-commit-id: b231c35ea1db966c22f2affd25c59ceaaaa82669

--- sys/sparc64/include/vmparam.h
+++ sys/sparc64/include/vmparam.h
@@ -440,7 +440,7 @@ begin_define
 define|#
 directive|define
 name|VM_KMEM_SIZE_SCALE
-value|(3)
+value|(tsb_kernel_ldd_phys == 0 ? 3 : 1)
 end_define
 
 begin_endif
@@ -497,6 +497,13 @@ define|#
 directive|define
 name|UMA_MD_SMALL_ALLOC
 end_define
+
+begin_decl_stmt
+specifier|extern
+name|u_int
+name|tsb_kernel_ldd_phys
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 specifier|extern
