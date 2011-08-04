31169218adbb168dcfe82ace2ab55293de0391ab
Jonathan Anderson <jonathan@FreeBSD.org>
2011-08-04 14:20:13 +0000

    Flesh out the cap_test regression test.
    Add more regression testing, some of which is expected to fail until we
    commit more kernel implementation.
    
    Approved by: re (kib), mentor (rwatson)
    Sponsored by: Google Inc
    
    
    Former-commit-id: 4fdab79e7f08d80bfc957550e3a4d79758c703fb

--- tools/regression/security/cap_test/cap_test_sysctl.c
+++ tools/regression/security/cap_test/cap_test_sysctl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2008-2011 Robert N. M. Watson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2008-2011 Robert N. M. Watson  * Copyright (c) 2011 Jonathan Anderson  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -36,6 +36,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/errno.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/sysctl.h>
 end_include
 
@@ -49,12 +55,6 @@ begin_include
 include|#
 directive|include
 file|<err.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<errno.h>
 end_include
 
 begin_include
@@ -86,21 +86,24 @@ comment|/*  * Certain sysctls are permitted in capability mode, but most are not
 end_comment
 
 begin_function
-name|void
+name|int
 name|test_sysctl
 parameter_list|(
 name|void
 parameter_list|)
 block|{
 name|int
-name|error
-decl_stmt|,
 name|i
 decl_stmt|,
 name|oid
 index|[
 literal|2
 index|]
+decl_stmt|;
+name|int
+name|success
+init|=
+name|PASSED
 decl_stmt|;
 name|size_t
 name|len
@@ -126,8 +129,8 @@ argument_list|(
 name|i
 argument_list|)
 expr_stmt|;
-name|error
-operator|=
+name|CHECK
+argument_list|(
 name|sysctl
 argument_list|(
 name|oid
@@ -144,23 +147,15 @@ name|NULL
 argument_list|,
 literal|0
 argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|error
-condition|)
-name|warnx
-argument_list|(
-literal|"capmode and kern.osreldate failed error %d"
-argument_list|,
-name|errno
-argument_list|)
-expr_stmt|;
-name|exit
-argument_list|(
+operator|==
 literal|0
 argument_list|)
 expr_stmt|;
+return|return
+operator|(
+name|success
+operator|)
+return|;
 block|}
 end_function
 
