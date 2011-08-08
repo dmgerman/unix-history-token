fe7e0491087201f009f2552bce0aa287d609f575
Sergey Kandaurov <pluknet@FreeBSD.org>
2011-08-08 12:16:42 +0000

    MFC r224571:  Add missing MODULE_VERSION() definition.
    PR:		kern/159345
    
    Former-commit-id: e1ce32667706949732f3a032f3da7180c6345124

--- sys/net/if_lagg.c
+++ sys/net/if_lagg.c
@@ -1377,6 +1377,16 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|if_lagg
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_if
 if|#
 directive|if
