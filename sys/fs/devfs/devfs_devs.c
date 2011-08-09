65df3560ac6723d2fb6166329b0adf0b8e1a83de
Konstantin Belousov <kib@FreeBSD.org>
2011-08-09 20:53:33 +0000

    Do not update mountpoint generation counter to the value which was not yet acted upon by devfs_populate().
    Submitted by:	Kohji Okuno <okuno.kohji jp panasonic com>
    Approved by:	re (bz)
    MFC after:	1 week
    
    
    Former-commit-id: b246ee4af4323bb0f483b60fca78701dc6f6ae21

--- sys/fs/devfs/devfs_devs.c
+++ sys/fs/devfs/devfs_devs.c
@@ -3260,6 +3260,9 @@ modifier|*
 name|dm
 parameter_list|)
 block|{
+name|unsigned
+name|gen
+decl_stmt|;
 name|sx_assert
 argument_list|(
 operator|&
@@ -3270,13 +3273,17 @@ argument_list|,
 name|SX_XLOCKED
 argument_list|)
 expr_stmt|;
+name|gen
+operator|=
+name|devfs_generation
+expr_stmt|;
 if|if
 condition|(
 name|dm
 operator|->
 name|dm_generation
 operator|==
-name|devfs_generation
+name|gen
 condition|)
 return|return;
 while|while
@@ -3293,7 +3300,7 @@ name|dm
 operator|->
 name|dm_generation
 operator|=
-name|devfs_generation
+name|gen
 expr_stmt|;
 block|}
 end_function
