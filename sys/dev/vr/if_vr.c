56d8f9161de3de2feffac78ff65abee7b872e254
Marius Strobl <marius@FreeBSD.org>
2011-10-09 20:16:51 +0000

    Sprinkle const.
    Former-commit-id: 29733cec27c8dd366ada2ca2ecaca1c492502f90

--- sys/dev/vr/if_vr.c
+++ sys/dev/vr/if_vr.c
@@ -300,6 +300,7 @@ end_define
 
 begin_struct
 specifier|static
+specifier|const
 struct|struct
 name|vr_type
 block|{
@@ -312,11 +313,13 @@ decl_stmt|;
 name|int
 name|vr_quirks
 decl_stmt|;
+specifier|const
 name|char
 modifier|*
 name|vr_name
 decl_stmt|;
 block|}
+decl|const
 name|vr_devs
 index|[]
 init|=
@@ -969,6 +972,7 @@ end_function_decl
 
 begin_struct
 specifier|static
+specifier|const
 struct|struct
 name|vr_tx_threshold_table
 block|{
@@ -982,6 +986,7 @@ name|int
 name|value
 decl_stmt|;
 block|}
+decl|const
 name|vr_tx_threshold_tables
 index|[]
 init|=
@@ -2765,6 +2770,7 @@ end_comment
 
 begin_function
 specifier|static
+specifier|const
 name|struct
 name|vr_type
 modifier|*
@@ -2774,6 +2780,7 @@ name|device_t
 name|dev
 parameter_list|)
 block|{
+specifier|const
 name|struct
 name|vr_type
 modifier|*
@@ -2846,6 +2853,7 @@ name|device_t
 name|dev
 parameter_list|)
 block|{
+specifier|const
 name|struct
 name|vr_type
 modifier|*
@@ -2911,6 +2919,7 @@ name|ifnet
 modifier|*
 name|ifp
 decl_stmt|;
+specifier|const
 name|struct
 name|vr_type
 modifier|*
