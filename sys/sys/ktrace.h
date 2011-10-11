d16184208e64101b77f5cb4d388481f88a6ed2d2
Dag-Erling Smorgrav <des@FreeBSD.org>
2011-10-11 20:37:10 +0000

    Add a new trace point, KTRFAC_CAPFAIL, which traces capability check failures.  It is included in the default set for ktrace(1) and kdump(1).
    Former-commit-id: 8a1712120010bd725b46ae6af25ee6a09e7d9fd0

--- sys/sys/ktrace.h
+++ sys/sys/ktrace.h
@@ -446,6 +446,31 @@ value|11
 end_define
 
 begin_comment
+comment|/*  * KTR_CAPFAIL - trace capability check failures  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|KTR_CAPFAIL
+value|12
+end_define
+
+begin_struct
+struct|struct
+name|ktr_cap_fail
+block|{
+name|cap_rights_t
+name|cap_needed
+decl_stmt|;
+name|cap_rights_t
+name|cap_held
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_comment
 comment|/*  * KTR_DROP - If this bit is set in ktr_type, then at least one event  * between the previous record and this record was dropped.  */
 end_comment
 
@@ -542,6 +567,13 @@ define|#
 directive|define
 name|KTRFAC_PROCDTOR
 value|(1<<KTR_PROCDTOR)
+end_define
+
+begin_define
+define|#
+directive|define
+name|KTRFAC_CAPFAIL
+value|(1<<KTR_CAPFAIL)
 end_define
 
 begin_comment
@@ -766,6 +798,17 @@ name|void
 modifier|*
 parameter_list|,
 name|size_t
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|ktrcapfail
+parameter_list|(
+name|cap_rights_t
+parameter_list|,
+name|cap_rights_t
 parameter_list|)
 function_decl|;
 end_function_decl
