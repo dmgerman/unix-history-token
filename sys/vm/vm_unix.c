d2fc3182ecca2eff498a75857168c00a2d940578
Marcel Moolenaar <marcel@FreeBSD.org>
2011-10-13 16:20:10 +0000

    In sys_obreak() and when compiling for amd64 or ia64, when the process is ILP32 (i.e. i386) grant execute permissions by default. The JDK 1.4.x depends on being able to execute from the heap on i386.
    Former-commit-id: 9fff0c2481177fa3f83e3ff700068833e984f44a

--- sys/vm/vm_unix.c
+++ sys/vm/vm_unix.c
@@ -3,6 +3,12 @@ begin_comment
 comment|/*-  * Copyright (c) 1988 University of Utah.  * Copyright (c) 1991, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * the Systems Programming Group of the University of Utah Computer  * Science Department.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * from: Utah $Hdr: vm_unix.c 1.1 89/11/07$  *  *	@(#)vm_unix.c	8.1 (Berkeley) 6/11/93  */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|"opt_compat.h"
+end_include
+
 begin_comment
 comment|/*  * Traditional sbrk/grow interface to VM  */
 end_comment
@@ -55,6 +61,12 @@ begin_include
 include|#
 directive|include
 file|<sys/resourcevar.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/sysent.h>
 end_include
 
 begin_include
@@ -167,6 +179,8 @@ decl_stmt|,
 name|vmemlim
 decl_stmt|;
 name|int
+name|prot
+decl_stmt|,
 name|rv
 decl_stmt|;
 name|int
@@ -474,6 +488,43 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+name|prot
+operator|=
+name|VM_PROT_RW
+expr_stmt|;
+ifdef|#
+directive|ifdef
+name|COMPAT_FREEBSD32
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__amd64__
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__ia64__
+argument_list|)
+if|if
+condition|(
+name|SV_PROC_FLAG
+argument_list|(
+name|td
+operator|->
+name|td_proc
+argument_list|,
+name|SV_ILP32
+argument_list|)
+condition|)
+name|prot
+operator||=
+name|VM_PROT_EXECUTE
+expr_stmt|;
+endif|#
+directive|endif
+endif|#
+directive|endif
 name|rv
 operator|=
 name|vm_map_insert
@@ -491,7 +542,7 @@ name|old
 argument_list|,
 name|new
 argument_list|,
-name|VM_PROT_RW
+name|prot
 argument_list|,
 name|VM_PROT_ALL
 argument_list|,
