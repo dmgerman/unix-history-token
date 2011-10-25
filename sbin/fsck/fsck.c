d11d684b9c2fb0d2d907e5bd3a104d1dcedc4926
Maxim Sobolev <sobomax@FreeBSD.org>
2011-10-25 01:46:42 +0000

    Add new option -c to specify alternatve location of the /etc/fstab file.
    MFC after:	1 month
    
    
    Former-commit-id: 1283de2344e096a6a2e10d1bcdee7cf086f7969a

--- sbin/fsck/fsck.c
+++ sbin/fsck/fsck.c
@@ -423,6 +423,11 @@ index|[
 literal|3
 index|]
 decl_stmt|;
+specifier|const
+name|char
+modifier|*
+name|etc_fstab
+decl_stmt|;
 name|globopt
 index|[
 literal|0
@@ -449,6 +454,10 @@ operator|&
 name|opthead
 argument_list|)
 expr_stmt|;
+name|etc_fstab
+operator|=
+name|NULL
+expr_stmt|;
 while|while
 condition|(
 operator|(
@@ -460,7 +469,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"BCdvpfFnyl:t:T:"
+literal|"BCdvpfFnyl:t:T:c:"
 argument_list|)
 operator|)
 operator|!=
@@ -644,6 +653,14 @@ name|optarg
 expr_stmt|;
 break|break;
 case|case
+literal|'c'
+case|:
+name|etc_fstab
+operator|=
+name|optarg
+expr_stmt|;
+break|break;
+case|case
 literal|'?'
 case|:
 default|default:
@@ -659,6 +676,17 @@ expr_stmt|;
 name|argv
 operator|+=
 name|optind
+expr_stmt|;
+if|if
+condition|(
+name|etc_fstab
+operator|!=
+name|NULL
+condition|)
+name|setfstab
+argument_list|(
+name|etc_fstab
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
@@ -2683,7 +2711,7 @@ name|char
 name|common
 index|[]
 init|=
-literal|"[-Cdfnpvy] [-B | -F] [-T fstype:fsoptions] [-t fstype]"
+literal|"[-Cdfnpvy] [-B | -F] [-T fstype:fsoptions] [-t fstype] [-c fstab]"
 decl_stmt|;
 operator|(
 name|void
