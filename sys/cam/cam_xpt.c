c736b06796168d202782f5711db85ad15904f268
Marius Strobl <marius@FreeBSD.org>
2011-11-27 15:43:40 +0000

    For sparc64 also adjust the geometry of da(4) driven disks to not overflow the 16-bit cylinders field of the VTOC8 disk label (at around 502GB). The geometry chosen for disks above that limit allows to use disks up to 2TB, which is the limit of the extended VTOC8 format. The geometry used for disks smaller than the 16-bit cylinders limit stays the same as used by cam_calc_geometry(9) for extended translation. Thanks to Hans-Joerg Sirtl for providing hardware for testing this change.
    MFC after:	3 days
    
    
    Former-commit-id: 3c1ee670ed7cc36fe7f9b1167f9cf7e29d890a48

--- sys/cam/cam_xpt.c
+++ sys/cam/cam_xpt.c
@@ -215,6 +215,16 @@ end_include
 begin_include
 include|#
 directive|include
+file|<machine/md_var.h>
+end_include
+
+begin_comment
+comment|/* geometry translation */
+end_comment
+
+begin_include
+include|#
+directive|include
 file|<machine/stdarg.h>
 end_include
 
@@ -11086,10 +11096,18 @@ name|CAM_REQ_CMP
 expr_stmt|;
 break|break;
 block|}
-ifdef|#
-directive|ifdef
+if|#
+directive|if
+name|defined
+argument_list|(
 name|PC98
-comment|/* 		 * In a PC-98 system, geometry translation depens on 		 * the "real" device geometry obtained from mode page 4. 		 * SCSI geometry translation is performed in the 		 * initialization routine of the SCSI BIOS and the result 		 * stored in host memory.  If the translation is available 		 * in host memory, use it.  If not, rely on the default 		 * translation the device driver performs. 		 */
+argument_list|)
+operator|||
+name|defined
+argument_list|(
+name|__sparc64__
+argument_list|)
+comment|/* 		 * In a PC-98 system, geometry translation depens on 		 * the "real" device geometry obtained from mode page 4. 		 * SCSI geometry translation is performed in the 		 * initialization routine of the SCSI BIOS and the result 		 * stored in host memory.  If the translation is available 		 * in host memory, use it.  If not, rely on the default 		 * translation the device driver performs. 		 * For sparc64, we may need adjust the geometry of large 		 * disks in order to fit the limitations of the 16-bit 		 * fields of the VTOC8 disk label. 		 */
 if|if
 condition|(
 name|scsi_da_bios_params
