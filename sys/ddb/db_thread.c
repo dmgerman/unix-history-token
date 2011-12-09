f730c4abea3995b299e8e217d5a27720730e4ef0
Konstantin Belousov <kib@FreeBSD.org>
2011-12-09 20:41:54 +0000

    Typo.
    MFC after:	3 days
    
    
    Former-commit-id: 4f8a654f10195ad5f426a4c1efb6ee6a0fbe7901

--- sys/ddb/db_thread.c
+++ sys/ddb/db_thread.c
@@ -356,7 +356,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Lookup a thread based on a db expression address.  We assume that the  * address was parsed in hexadecimal.  We reparse the address in decimal  * first and try to treat it as a thread ID to find an associated thread.  * If that fails and check_pid is true, we terat the decimal value as a  * PID.  If that matches a process, we return the first thread in that  * process.  Otherwise, we treat the addr as a pointer to a thread.  */
+comment|/*  * Lookup a thread based on a db expression address.  We assume that the  * address was parsed in hexadecimal.  We reparse the address in decimal  * first and try to treat it as a thread ID to find an associated thread.  * If that fails and check_pid is true, we treat the decimal value as a  * PID.  If that matches a process, we return the first thread in that  * process.  Otherwise, we treat the addr as a pointer to a thread.  */
 end_comment
 
 begin_function
