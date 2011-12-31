07289b1334796c14902c3350246654bac01d26ea
Dimitry Andric <dim@FreeBSD.org>
2011-12-31 12:37:07 +0000

    In sys/dev/hwpmc/hwpmc_amd.c, fix a clang warning about invalid enum conversions.
    Reviewed by:	jkoshy
    MFC after:	1 week
    
    
    Former-commit-id: da9572cf2d513b5449c912bd7ed1692abd80d07a

--- sys/dev/hwpmc/hwpmc_amd.c
+++ sys/dev/hwpmc/hwpmc_amd.c
@@ -4083,13 +4083,6 @@ modifier|*
 name|name
 decl_stmt|;
 comment|/* 	 * The presence of hardware performance counters on the AMD 	 * Athlon, Duron or later processors, is _not_ indicated by 	 * any of the processor feature flags set by the 'CPUID' 	 * instruction, so we only check the 'instruction family' 	 * field returned by CPUID for instruction family>= 6. 	 */
-name|class
-operator|=
-name|cputype
-operator|=
-operator|-
-literal|1
-expr_stmt|;
 name|name
 operator|=
 name|NULL
@@ -4151,18 +4144,7 @@ operator|=
 literal|"K8"
 expr_stmt|;
 break|break;
-block|}
-if|if
-condition|(
-operator|(
-name|int
-operator|)
-name|cputype
-operator|==
-operator|-
-literal|1
-condition|)
-block|{
+default|default:
 operator|(
 name|void
 operator|)
