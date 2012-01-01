c57a23131fe772c312abb6fb1895cd40ab6b7d53
Dimitry Andric <dim@FreeBSD.org>
2012-01-01 23:49:11 +0000

    MFC r228714:
      In usr.sbin/timed, fix several issues with printf formats:
      - Cast time_t's to long, and print them with %ld.
      - Print ptrdiff_t's with %td.
      - Print ssize_t's and size_t's with %zd and %zu.
      - Print int32_t's with %d.
      Also, replace some int variables with the more appropriate size_t.
    
    MFC r228719:
    
      Some people pointed out long is 32-bit on some arches, while time_t is
      64-bit, so better cast time_t to intmax_t, and use the appropriate
      printf format strings.
    
    MFC r228720:
    
      Fix r228719; when you use intmax_t, you need stdint.h.
    
      Pointy hat to:	dim
    
    
    Former-commit-id: 342522e7679926441ef33c7c7d23ba7aadf120ad

--- usr.sbin/timed/timed/readmsg.c
+++ usr.sbin/timed/timed/readmsg.c
@@ -725,8 +725,11 @@ name|fprintf
 argument_list|(
 name|fd
 argument_list|,
-literal|"readmsg: wait %ld.%6ld at %s\n"
+literal|"readmsg: wait %jd.%6ld at %s\n"
 argument_list|,
+operator|(
+name|intmax_t
+operator|)
 name|rwait
 operator|.
 name|tv_sec
@@ -904,7 +907,7 @@ name|syslog
 argument_list|(
 name|LOG_NOTICE
 argument_list|,
-literal|"short packet (%u/%u bytes) from %s"
+literal|"short packet (%zd/%zu bytes) from %s"
 argument_list|,
 name|n
 argument_list|,
@@ -1947,7 +1950,7 @@ name|fprintf
 argument_list|(
 name|fd
 argument_list|,
-literal|"%s %d %-6u (%ld,%ld) %-15s %s\n"
+literal|"%s %d %-6u (%d,%d) %-15s %s\n"
 argument_list|,
 name|tsptype
 index|[
