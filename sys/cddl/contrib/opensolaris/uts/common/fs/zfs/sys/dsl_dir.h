3a0004c710399743c97afe9affaf274e92e50c31
Martin Matuska <mm@FreeBSD.org>
2012-01-05 09:39:29 +0000

    MFC r226676, r226678, r226700, r226705, r226706, r226707:
    MFC r226676 (pjd):
    Allow to rename file systems without remounting if it is possible.
    It is possible for file systems with 'mountpoint' preperty set to 'legacy'
    or 'none' - we don't have to change mount directory for them.
    Currently such file systems are unmounted on rename and not even mounted back.
    
    This introduces layering violation, as we need to update 'f_mntfromname'
    field in statfs structure related to mountpoint (for the dataset we are
    renaming and all its children).
    
    In my opinion it is worth it, as it allow to update FreeBSD in even cleaner
    way - in ZFS-only configuration root file system is ZFS file system with
    'mountpoint' property set to 'legacy'. If root dataset is named system/rootfs,
    we can snapshot it (system/rootfs@upgrade), clone it (system/oldrootfs),
    update FreeBSD and if it doesn't boot we can boot back from system/oldrootfs
    and rename it back to system/rootfs while it is mounted as /. Before it was
    not possible, because unmounting / was not possible.
    
    MFC r227768 (pjd):
    Include <sys/zfs_vfsops.h> only when compiling kernel module.
    
    MFC r226700 (pjd):
    Don't forget to rename mounted snapshots of the file system being renamed.
    
    MFC r226705 (pjd):
    Extend r226676 to allow rename without unmount even for file systems with
    non-legacy mountpoints. It is better to be able to rename such file systems and
    let them be mounted in old places until next reboot than using live CD, etc. to
    rename with remount.
    
    This is implemented by adding -u option to 'zfs rename'. If file system's
    mountpoint property is set to 'legacy' or 'none', there is no need to specify -u.
    
    Update zfs(8) manual page to reflect this addition.
    
    MFC r226706 (pjd):
    Update copyright to include myself.
    
    MFC r226707 (pjd):
    - Use better naming now that we allow to rename any mounted file system (not
      only legacy).
    - Update copyright to include myself.
    
    Approved by:	pjd
    
    
    Former-commit-id: 29cddaff4d879b7c0566dfc47be732a81aea90eb

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/dsl_dir.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/dsl_dir.h
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011 Pawel Jakub Dawidek<pawel@dawidek.net>.  * All rights reserved.  */
 end_comment
 
 begin_ifndef
@@ -549,6 +549,9 @@ specifier|const
 name|char
 modifier|*
 name|newname
+parameter_list|,
+name|int
+name|flags
 parameter_list|)
 function_decl|;
 name|int
