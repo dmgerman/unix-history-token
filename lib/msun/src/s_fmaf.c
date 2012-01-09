d3fc2bc3b1e51980cfba07945b02b1f34312012d
David Schultz <das@FreeBSD.org>
2012-01-09 04:55:55 +0000

    MFC various fma{,f,l} improvements:
    r226245 - refactoring
    r226371 - fix double-rounding bug
    r226373 - new math_private.h macros
    r226601 - fix nit in r226371
    
    
    Former-commit-id: 12d5300d9385b798cf118821bd1c4ba0f363f694

--- lib/msun/src/s_fmaf.c
+++ lib/msun/src/s_fmaf.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2005 David Schultz<das@FreeBSD.ORG>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2005-2011 David Schultz<das@FreeBSD.ORG>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_include
@@ -20,11 +20,23 @@ end_expr_stmt
 begin_include
 include|#
 directive|include
+file|<fenv.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"math.h"
 end_include
 
+begin_include
+include|#
+directive|include
+file|"math_private.h"
+end_include
+
 begin_comment
-comment|/*  * Fused multiply-add: Compute x * y + z with a single rounding error.  *  * A double has more than twice as much precision than a float, so  * direct double-precision arithmetic suffices.  *  * XXX We are relying on the compiler to convert from double to float  *     using the current rounding mode and with the appropriate  *     side-effects.  But on at least one platform (gcc 3.4.2/sparc64),  *     this appears to be too much to ask for.  The precision  *     reduction should be done manually.  */
+comment|/*  * Fused multiply-add: Compute x * y + z with a single rounding error.  *  * A double has more than twice as much precision than a float, so  * direct double-precision arithmetic suffices, except where double  * rounding occurs.  */
 end_comment
 
 begin_function
@@ -41,16 +53,122 @@ name|float
 name|z
 parameter_list|)
 block|{
-return|return
-operator|(
+name|double
+name|xy
+decl_stmt|,
+name|result
+decl_stmt|;
+name|uint32_t
+name|hr
+decl_stmt|,
+name|lr
+decl_stmt|;
+name|xy
+operator|=
 operator|(
 name|double
 operator|)
 name|x
 operator|*
 name|y
+expr_stmt|;
+name|result
+operator|=
+name|xy
 operator|+
 name|z
+expr_stmt|;
+name|EXTRACT_WORDS
+argument_list|(
+name|hr
+argument_list|,
+name|lr
+argument_list|,
+name|result
+argument_list|)
+expr_stmt|;
+comment|/* Common case: The double precision result is fine. */
+if|if
+condition|(
+operator|(
+name|lr
+operator|&
+literal|0x1fffffff
+operator|)
+operator|!=
+literal|0x10000000
+operator|||
+comment|/* not a halfway case */
+operator|(
+name|hr
+operator|&
+literal|0x7ff00000
+operator|)
+operator|==
+literal|0x7ff00000
+operator|||
+comment|/* NaN */
+name|result
+operator|-
+name|xy
+operator|==
+name|z
+operator|||
+comment|/* exact */
+name|fegetround
+argument_list|()
+operator|!=
+name|FE_TONEAREST
+condition|)
+comment|/* not round-to-nearest */
+return|return
+operator|(
+name|result
+operator|)
+return|;
+comment|/* 	 * If result is inexact, and exactly halfway between two float values, 	 * we need to adjust the low-order bit in the direction of the error. 	 */
+name|fesetround
+argument_list|(
+name|FE_TOWARDZERO
+argument_list|)
+expr_stmt|;
+specifier|volatile
+name|double
+name|vxy
+init|=
+name|xy
+decl_stmt|;
+comment|/* XXX work around gcc CSE bug */
+name|double
+name|adjusted_result
+init|=
+name|vxy
+operator|+
+name|z
+decl_stmt|;
+name|fesetround
+argument_list|(
+name|FE_TONEAREST
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|result
+operator|==
+name|adjusted_result
+condition|)
+name|SET_LOW_WORD
+argument_list|(
+name|adjusted_result
+argument_list|,
+name|lr
+operator|+
+literal|1
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+name|adjusted_result
 operator|)
 return|;
 block|}
