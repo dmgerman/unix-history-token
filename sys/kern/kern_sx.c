6c85909b4b0de17770a837f365896f6bf928e4d8
Andriy Gapon <avg@FreeBSD.org>
2012-01-15 21:58:51 +0000

    MFC r228430,228433: put sys/systm.h at its proper place or add it if missing
    Former-commit-id: 032a6d7f6972b63203b5715f636a6885ea644d41

--- sys/kern/kern_sx.c
+++ sys/kern/kern_sx.c
@@ -48,6 +48,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/systm.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/ktr.h>
 end_include
 
@@ -85,12 +91,6 @@ begin_include
 include|#
 directive|include
 file|<sys/sysctl.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/systm.h>
 end_include
 
 begin_if
