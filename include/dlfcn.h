a991c24f4c07d02e64c34af7a24d50d094922e2f
Konstantin Belousov <kib@FreeBSD.org>
2012-01-21 01:24:06 +0000

    MFC r229768: Implement fdlopen(3), an rtld interface to load shared object by file descriptor.
    Former-commit-id: 1b8b04b5eb013192f56e33832f1a9f837febe75c

--- include/dlfcn.h
+++ include/dlfcn.h
@@ -415,6 +415,18 @@ name|__BSD_VISIBLE
 end_if
 
 begin_function_decl
+name|void
+modifier|*
+name|fdlopen
+parameter_list|(
+name|int
+parameter_list|,
+name|int
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|int
 name|dladdr
 parameter_list|(
