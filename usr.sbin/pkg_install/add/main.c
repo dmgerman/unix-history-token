970b267afa78a2d66f4db4d7f4c23357e97bd0be
Eitan Adler <eadler@FreeBSD.org>
2012-01-28 19:48:44 +0000

    MFC r229831: 	- X11BASE has been deprecated for a long time and will die soon
    Approved by:	cperciva
    
    
    Former-commit-id: af91685a3bb3d0f2b2f3b8838da7657a50ab5e19

--- usr.sbin/pkg_install/add/main.c
+++ usr.sbin/pkg_install/add/main.c
@@ -1647,7 +1647,7 @@ name|setenv
 argument_list|(
 literal|"PATH"
 argument_list|,
-literal|"/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/X11R6/bin"
+literal|"/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 argument_list|,
 literal|1
 argument_list|)
