42c21f46c7904fdb9767a0f6bad234dd2032a245
Ken Merry <ken@FreeBSD.org>
2012-02-02 17:54:35 +0000

    Fix the netback driver build for i386.
    netback.c:	Add missing VM includes.
    
    xen/xenvar.h,
    xen/xenpmap.h:	Move some XENHVM macros from <machine/xen/xenpmap.h> to
    		<machine/xen/xenvar.h> on i386 to match the amd64 headers.
    
    conf/files:	Add netback to the build.
    
    Submitted by:	jhb
    MFC after:	3 days
    
    
    Former-commit-id: b768c4e1d6cb32b687610563098a86b5fbe160d6

--- sys/dev/xen/netback/netback.c
+++ sys/dev/xen/netback/netback.c
@@ -193,6 +193,18 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/vm_extern.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<vm/vm_kern.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<machine/_inttypes.h>
 end_include
 
