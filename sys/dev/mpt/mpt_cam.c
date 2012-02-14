30f54d5d093351638580465045d72fb247454cd9
Marius Strobl <marius@FreeBSD.org>
2012-02-14 01:15:01 +0000

    MFC: r231228
    Remove extra newlines from panic messages.
    
    
    Former-commit-id: 37a88f561a92c19292562e8b16baf48d3b4e1c1e

--- sys/dev/mpt/mpt_cam.c
+++ sys/dev/mpt/mpt_cam.c
@@ -6530,7 +6530,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -7761,7 +7761,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -8449,7 +8449,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -9491,7 +9491,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -12822,7 +12822,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -17081,7 +17081,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -21387,7 +21387,7 @@ operator|!=
 name|CAM_DIR_NONE
 argument_list|,
 operator|(
-literal|"dxfer_len %u but direction is NONE\n"
+literal|"dxfer_len %u but direction is NONE"
 operator|,
 name|csio
 operator|->
@@ -25980,7 +25980,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"zero ccb sts at %d\n"
+literal|"zero ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -26277,7 +26277,7 @@ operator|.
 name|status
 argument_list|,
 operator|(
-literal|"ZERO ccb sts at %d\n"
+literal|"ZERO ccb sts at %d"
 operator|,
 name|__LINE__
 operator|)
@@ -26572,7 +26572,7 @@ operator|==
 name|TGT_STATE_LOADING
 argument_list|,
 operator|(
-literal|"bad state 0x%x on reply to buffer post\n"
+literal|"bad state 0x%x on reply to buffer post"
 operator|,
 name|tgt
 operator|->
