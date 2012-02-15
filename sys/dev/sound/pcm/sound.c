afd892a96e182bc6efb1ddd985d9d9206384dd5d
Alexander Motin <mav@FreeBSD.org>
2012-02-15 14:31:45 +0000

    MFC r231762: Do not handle MOD_SHUTDOWN equally to MOD_UNLOAD in sound kernel module. MOD_SHUTDOWN is not an end of existence, and there is a life after it. In particular, code previously called on MOD_SHUTDOWN grabbed lock and deallocated unit numbering. That caused infinite wait loop if snd_uaudio tried to destroy its PCM device after that point.
    Former-commit-id: 141353cc73672a17d5b8deda23963e3ed14b41c5

--- sys/dev/sound/pcm/sound.c
+++ sys/dev/sound/pcm/sound.c
@@ -6572,9 +6572,6 @@ break|break;
 case|case
 name|MOD_UNLOAD
 case|:
-case|case
-name|MOD_SHUTDOWN
-case|:
 name|ret
 operator|=
 name|sndstat_acquire
@@ -6606,6 +6603,10 @@ operator|=
 name|NULL
 expr_stmt|;
 block|}
+break|break;
+case|case
+name|MOD_SHUTDOWN
+case|:
 break|break;
 default|default:
 name|ret
