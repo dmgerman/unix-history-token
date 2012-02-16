baf75cd443d1d0094a295c49c7d19b85c3bf529f
Hiroki Sato <hrs@FreeBSD.org>
2012-02-16 01:32:23 +0000

    MFC r222732,222771,222820,222824,222972,223752,224006,224144,224148,225519,22568 3,228990:
    - Implement RDNSS and DNSSL options (RFC 6106, IPv6 Router Advertisement
      Options for DNS Configuration).
    
    - rtadvd(8) now supports "noifprefix" to disable gathering on-link prefixes
      from interfaces when no "addr" is specified[2].  An entry in rtadvd.conf
      with "noifprefix" + no "addr" generates an RA message with no prefix
      information option.
    
    - rtadvd(8) now supports RTM_IFANNOUNCE message to fix crashes when an
      interface is added or removed.
    
    - Implement burst unsolicited RA sending into the internal RA timer framework
      when AdvSendAdvertisements and/or configuration entries are changed as
      described in RFC 4861 6.2.4.  This fixes issues that make termination of the
      rtadvd(8) daemon take very long time.
    
    - rtadvd(8) now accepts non-existent interfaces as well in the command line.
    
    - Add control socket support and rtadvctl(8) utility to show the RA information
      in rtadvd(8).  Dumping by SIGUSR1 has been removed in favor of it.
    
    
    Former-commit-id: 73e4886932358ba6bcc0b9f0a315e9a4947e2e95

--- usr.sbin/rtadvd/pathnames.h
+++ usr.sbin/rtadvd/pathnames.h
@@ -14,5 +14,19 @@ name|_PATH_RTADVDCONF
 value|"/etc/rtadvd.conf"
 end_define
 
+begin_define
+define|#
+directive|define
+name|_PATH_RTADVDPID
+value|"/var/run/rtadvd.pid"
+end_define
+
+begin_define
+define|#
+directive|define
+name|_PATH_CTRL_SOCK
+value|"/var/run/rtadvd.sock"
+end_define
+
 end_unit
 
