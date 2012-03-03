a7494748b3bc6130544648a5911226cc3f3316e2
Juli Mallett <jmallett@FreeBSD.org>
2012-03-03 21:39:12 +0000

    On MIPS, _ALIGN always aligns to 8 bytes, even for 32-bit binaries.  This might not be ideal, but is the ABI we've shipped so far.  Fix macros which reflect the results of _ALIGN on 32-bit MIPS to use the right alignment.
    This fixes sendmsg under COMPAT_FREEBSD32 on n64 MIPS kernels.
    
    
    Former-commit-id: e59db796becb0640cea88e239c880d29e295293f

--- sys/compat/freebsd32/freebsd32_misc.c
+++ sys/compat/freebsd32/freebsd32_misc.c
@@ -5590,12 +5590,35 @@ return|;
 block|}
 end_function
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__mips__
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|FREEBSD32_ALIGNBYTES
 value|(sizeof(int) - 1)
 end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|FREEBSD32_ALIGNBYTES
+value|(sizeof(long) - 1)
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
