23a50780f288f19a6d4ca7e3ed5df4f5d024d2f7
Andrey V. Elsukov <ae@FreeBSD.org>
2012-03-05 04:51:22 +0000

    MFC r231754:   Add additional check to EBR probe and create methods:   don't try probe and create  EBR scheme when parent partition type   is not "ebr". This fixes error messages about corrupted EBR for   some partitions where is actually another partition scheme.
      NOTE: if you have EBR on the partition with different than "ebr"
      (0x05) type, then you will lost access to partitions until it will be
      changed.
    
    MFC r231928:
      Add alias for the partition type 0x0f. Now "ebr" name is used for both
      types 0x05 and 0x0f, but 0x05 is preferred and used when partition is
      created with "gpart add -t ebr ...".
      This should keep EBR partitions accessible after r231754 for those,
      who have EBR on the partition with type 0x0f.
    
    
    Former-commit-id: 84092044d92637ba7ef3ad0319d01f5b4bc5818f

--- sys/geom/part/g_part_mbr.c
+++ sys/geom/part/g_part_mbr.c
@@ -642,6 +642,12 @@ name|G_PART_ALIAS_MS_FAT32
 block|}
 block|,
 block|{
+name|DOSPTYP_EXTLBA
+block|,
+name|G_PART_ALIAS_EBR
+block|}
+block|,
+block|{
 name|DOSPTYP_LDM
 block|,
 name|G_PART_ALIAS_MS_LDM_DATA
