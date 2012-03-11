c106d2733bc5032e66944e038b001c752499dece
Adrian Chadd <adrian@FreeBSD.org>
2012-03-11 19:08:56 +0000

    Upgrade the netgraph vlan node to support 802.1q, encapsulation type, PCP and CFI fields.
    * Ethernet_type for VLAN encapsulation is tunable, default is 0x8100;
    * PCP (Priority code point) and CFI (canonical format indicator) is
      tunable per VID;
    * Tunable encapsulation to support 802.1q
    * Encapsulation/Decapsulation code improvements
    
    New messages have been added for this netgraph node to support the
    new features.
    
    However, the legacy "vlan" id is still supported and compiled in by
    default.  It can be disabled in a future release.
    
    TODO:
    
    * Documentation
    * Examples
    
    PR:		kern/161908
    Submitted by:	Ivan <rozhuk.im@gmail.com>
    
    Former-commit-id: 19af2370c7ff311cba151c90359a9802a7fd6cae

--- sys/netgraph/ng_vlan.h
+++ sys/netgraph/ng_vlan.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2003 IPNET Internet Communication Company  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * Author: Ruslan Ermilov<ru@FreeBSD.org>  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2003 IPNET Internet Communication Company  * Copyright (c) 2011 - 2012 Rozhuk Ivan<rozhuk.im@gmail.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * Author: Ruslan Ermilov<ru@FreeBSD.org>  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -13,6 +13,17 @@ begin_define
 define|#
 directive|define
 name|_NETGRAPH_NG_VLAN_H_
+end_define
+
+begin_comment
+comment|/* Using "vlan" in addfilter and gettable messages. 2012.01 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NG_VLAN_USE_OLD_VLAN_NAME
+value|1
 end_define
 
 begin_comment
@@ -65,9 +76,37 @@ block|,
 name|NGM_VLAN_DEL_FILTER
 block|,
 name|NGM_VLAN_GET_TABLE
-block|}
+block|,
+name|NGM_VLAN_DEL_VID_FLT
+block|,
+name|NGM_VLAN_GET_DECAP
+block|,
+name|NGM_VLAN_SET_DECAP
+block|,
+name|NGM_VLAN_GET_ENCAP
+block|,
+name|NGM_VLAN_SET_ENCAP
+block|,
+name|NGM_VLAN_GET_ENCAP_PROTO
+block|,
+name|NGM_VLAN_SET_ENCAP_PROTO
+block|, }
 enum|;
 end_enum
+
+begin_define
+define|#
+directive|define
+name|VLAN_ENCAP_FROM_FILTER
+value|0x00000001
+end_define
+
+begin_define
+define|#
+directive|define
+name|VLAN_ENCAP_FROM_NOMATCH
+value|0x00000002
+end_define
 
 begin_comment
 comment|/* For NGM_VLAN_ADD_FILTER control message. */
@@ -78,14 +117,32 @@ struct|struct
 name|ng_vlan_filter
 block|{
 name|char
-name|hook
+name|hook_name
 index|[
 name|NG_HOOKSIZ
 index|]
 decl_stmt|;
-name|u_int16_t
+ifdef|#
+directive|ifdef
+name|NG_VLAN_USE_OLD_VLAN_NAME
+name|uint16_t
 name|vlan
 decl_stmt|;
+comment|/* VLAN - same as vid, oldname, deprecated. */
+endif|#
+directive|endif
+name|uint16_t
+name|vid
+decl_stmt|;
+comment|/* VID - VLAN Identifier. */
+name|uint8_t
+name|pcp
+decl_stmt|;
+comment|/* PCP - Priority Code Point. */
+name|uint8_t
+name|cfi
+decl_stmt|;
+comment|/* CFI - Canonical Format Indicator. */
 block|}
 struct|;
 end_struct
@@ -94,12 +151,35 @@ begin_comment
 comment|/* Keep this in sync with the above structure definition.  */
 end_comment
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|NG_VLAN_USE_OLD_VLAN_NAME
+end_ifdef
+
 begin_define
 define|#
 directive|define
 name|NG_VLAN_FILTER_FIELDS
-value|{				\ 	{ "hook",&ng_parse_hookbuf_type  },		\ 	{ "vlan",&ng_parse_uint16_type   },		\ 	{ NULL }						\ }
+value|{				\ 	{ "hook",&ng_parse_hookbuf_type	},		\ 	{ "vlan",&ng_parse_uint16_type	},		\ 	{ "vid",&ng_parse_uint16_type	},		\ 	{ "pcp",&ng_parse_uint8_type	},		\ 	{ "cfi",&ng_parse_uint8_type	},		\ 	{ NULL }						\ }
 end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|NG_VLAN_FILTER_FIELDS
+value|{				\ 	{ "hook",&ng_parse_hookbuf_type	},		\ 	{ "vid",&ng_parse_uint16_type	},		\ 	{ "pcp",&ng_parse_uint8_type	},		\ 	{ "cfi",&ng_parse_uint8_type	},		\ 	{ NULL }						\ }
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/* Structure returned by NGM_VLAN_GET_TABLE. */
