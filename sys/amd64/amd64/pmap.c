9e7c2b0758cd3e5e1e9454166dd8fa480b2ab8ba
Alan Cox <alc@FreeBSD.org>
2012-03-18 17:41:54 +0000

    Style fix to pmap_protect().
    Submitted by:	bde
    
    
    Former-commit-id: ffa8bba3a5dba8c398559da78c95b68eaaca1f91

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -13499,10 +13499,9 @@ name|pt_entry_t
 modifier|*
 name|pte
 decl_stmt|;
-name|int
-name|anychanged
-decl_stmt|;
 name|boolean_t
+name|anychanged
+decl_stmt|,
 name|pv_lists_locked
 decl_stmt|;
 if|if
@@ -13554,7 +13553,7 @@ name|resume
 label|:
 name|anychanged
 operator|=
-literal|0
+name|FALSE
 expr_stmt|;
 name|PMAP_LOCK
 argument_list|(
@@ -13746,7 +13745,7 @@ argument_list|)
 condition|)
 name|anychanged
 operator|=
-literal|1
+name|TRUE
 expr_stmt|;
 continue|continue;
 block|}
@@ -13984,7 +13983,7 @@ expr_stmt|;
 else|else
 name|anychanged
 operator|=
-literal|1
+name|TRUE
 expr_stmt|;
 block|}
 block|}
