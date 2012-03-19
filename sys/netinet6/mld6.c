3aa618cdf29a59b51424d53f1e56bfcf60e0ec5f
John Baldwin <jhb@FreeBSD.org>
2012-03-19 20:15:18 +0000

    MFC 225096: Fix if_addr_mtx recursion in mld6.
    mld_set_version() is called only from mld_v1_input_query() and
    mld_v2_input_query() both holding the if_addr_mtx lock, and then calling
    into mld_v2_cancel_link_timers() acquires it the second time, which results
    in mtx recursion. To avoid that, delay if_addr_mtx acquisition until after
    mld_set_version() is called; while here, further reduce locking scope
    to protect only the needed pieces: if_multiaddrs, in6m_lookup_locked().
    
    
    Former-commit-id: 045d884a08d486c52602b0eb83950fc176768945

--- sys/netinet6/mld6.c
+++ sys/netinet6/mld6.c
@@ -2495,11 +2495,6 @@ expr_stmt|;
 name|MLD_LOCK
 argument_list|()
 expr_stmt|;
-name|IF_ADDR_LOCK
-argument_list|(
-name|ifp
-argument_list|)
-expr_stmt|;
 comment|/* 	 * Switch to MLDv1 host compatibility mode. 	 */
 name|mli
 operator|=
@@ -2554,6 +2549,11 @@ condition|)
 name|timer
 operator|=
 literal|1
+expr_stmt|;
+name|IF_ADDR_LOCK
+argument_list|(
+name|ifp
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
@@ -3280,11 +3280,6 @@ expr_stmt|;
 name|MLD_LOCK
 argument_list|()
 expr_stmt|;
-name|IF_ADDR_LOCK
-argument_list|(
-name|ifp
-argument_list|)
-expr_stmt|;
 name|mli
 operator|=
 name|MLD_IFINFO
@@ -3411,6 +3406,11 @@ block|}
 else|else
 block|{
 comment|/* 		 * MLDv2 Group-specific or Group-and-source-specific Query. 		 * 		 * Group-source-specific queries are throttled on 		 * a per-group basis to defeat denial-of-service attempts. 		 * Queries for groups we are not a member of on this 		 * link are simply ignored. 		 */
+name|IF_ADDR_LOCK
+argument_list|(
+name|ifp
+argument_list|)
+expr_stmt|;
 name|inm
 operator|=
 name|in6m_lookup_locked
@@ -3429,9 +3429,16 @@ name|inm
 operator|==
 name|NULL
 condition|)
+block|{
+name|IF_ADDR_UNLOCK
+argument_list|(
+name|ifp
+argument_list|)
+expr_stmt|;
 goto|goto
 name|out_locked
 goto|;
+block|}
 if|if
 condition|(
 name|nsrc
@@ -3461,6 +3468,11 @@ argument_list|,
 literal|"%s: GS query throttled."
 argument_list|,
 name|__func__
+argument_list|)
+expr_stmt|;
+name|IF_ADDR_UNLOCK
+argument_list|(
+name|ifp
 argument_list|)
 expr_stmt|;
 goto|goto
@@ -3518,14 +3530,14 @@ operator|->
 name|mld_addr
 argument_list|)
 expr_stmt|;
-block|}
-name|out_locked
-label|:
 name|IF_ADDR_UNLOCK
 argument_list|(
 name|ifp
 argument_list|)
 expr_stmt|;
+block|}
+name|out_locked
+label|:
 name|MLD_UNLOCK
 argument_list|()
 expr_stmt|;
