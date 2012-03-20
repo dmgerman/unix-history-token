9ac99e7d0742cb40e3c30f020d1c374b215e2865
David Chisnall <theraven@FreeBSD.org>
2012-03-20 17:58:15 +0000

    Import new version of libcxxrt.  Now works correctly with libobjc2 to implement the unified exception model for Objective-C++.
    Approved by:	dim (mentor)
    
    
    Former-commit-id: 7b78c3900650820abcd15beea380c281faf20c54

--- contrib/libcxxrt/typeinfo.h
+++ contrib/libcxxrt/typeinfo.h
@@ -15,11 +15,168 @@ directive|include
 file|"abi_namespace.h"
 end_include
 
-begin_include
-include|#
-directive|include
-file|"typeinfo"
-end_include
+begin_decl_stmt
+name|namespace
+name|ABI_NAMESPACE
+block|{
+struct_decl|struct
+name|__class_type_info
+struct_decl|;
+block|}
+end_decl_stmt
+
+begin_decl_stmt
+name|namespace
+name|std
+block|{
+comment|/** 	  * Standard type info class.  The layout of this class is specified by the 	  * ABI.  The layout of the vtable is not, but is intended to be 	  * compatible with the GNU ABI. 	  * 	  * Unlike the GNU version, the vtable layout is considered semi-private. 	  */
+name|class
+name|type_info
+block|{
+name|public
+label|:
+comment|/** 		 * Virtual destructor.  This class must have one virtual function to 		 * ensure that it has a vtable. 		 */
+name|virtual
+operator|~
+name|type_info
+argument_list|()
+expr_stmt|;
+name|bool
+name|operator
+operator|==
+operator|(
+specifier|const
+name|type_info
+operator|&
+operator|)
+specifier|const
+expr_stmt|;
+name|bool
+name|operator
+operator|!=
+operator|(
+specifier|const
+name|type_info
+operator|&
+operator|)
+specifier|const
+expr_stmt|;
+name|bool
+name|before
+argument_list|(
+specifier|const
+name|type_info
+operator|&
+argument_list|)
+decl|const
+decl_stmt|;
+specifier|const
+name|char
+modifier|*
+name|name
+parameter_list|()
+function_decl|const;
+name|type_info
+argument_list|()
+expr_stmt|;
+name|private
+label|:
+name|type_info
+argument_list|(
+specifier|const
+name|type_info
+operator|&
+name|rhs
+argument_list|)
+expr_stmt|;
+name|type_info
+modifier|&
+name|operator
+init|=
+operator|(
+specifier|const
+name|type_info
+operator|&
+name|rhs
+operator|)
+decl_stmt|;
+specifier|const
+name|char
+modifier|*
+name|__type_name
+decl_stmt|;
+comment|/* 		 * The following functions are in this order to match the 		 * vtable layout of libsupc++.  This allows libcxxrt to be used 		 * with libraries that depend on this. 		 * 		 * These functions are in the public headers for libstdc++, so 		 * we have to assume that someone will probably call them and 		 * expect them to work.  Their names must also match the names used in 		 * libsupc++, so that code linking against this library can subclass 		 * type_info and correctly fill in the values in the vtables. 		 */
+name|public
+label|:
+comment|/** 		 * Catch function.  Allows external libraries to implement 		 * their own basic types.  This is used, for example, in the 		 * GNUstep Objective-C runtime to allow Objective-C types to be 		 * caught in G++ catch blocks. 		 * 		 * The outer parameter indicates the number of outer pointers 		 * in the high bits.  The low bit indicates whether the 		 * pointers are const qualified. 		 */
+name|virtual
+name|bool
+name|__do_catch
+argument_list|(
+specifier|const
+name|type_info
+operator|*
+name|thrown_type
+argument_list|,
+name|void
+operator|*
+operator|*
+name|thrown_object
+argument_list|,
+name|unsigned
+name|outer
+argument_list|)
+decl|const
+decl_stmt|;
+comment|/** 		 * Performs an upcast.  This is used in exception handling to 		 * cast from subclasses to superclasses.  If the upcast is 		 * possible, it returns true and adjusts the pointer.  If the 		 * upcast is not possible, it returns false and does not adjust 		 * the pointer. 		 */
+name|virtual
+name|bool
+name|__do_upcast
+argument_list|(
+specifier|const
+name|ABI_NAMESPACE
+operator|::
+name|__class_type_info
+operator|*
+name|target
+argument_list|,
+name|void
+operator|*
+operator|*
+name|thrown_object
+argument_list|)
+decl|const
+block|{
+return|return
+name|false
+return|;
+block|}
+comment|/** 		 * Returns true if this is some pointer type, false otherwise. 		 */
+name|virtual
+name|bool
+name|__is_pointer_p
+argument_list|()
+specifier|const
+block|{
+return|return
+name|false
+return|;
+block|}
+comment|/** 		 * Returns true if this is some function type, false otherwise. 		 */
+name|virtual
+name|bool
+name|__is_function_p
+argument_list|()
+specifier|const
+block|{
+return|return
+name|false
+return|;
+block|}
+block|}
+empty_stmt|;
+block|}
+end_decl_stmt
 
 begin_decl_stmt
 name|namespace
@@ -68,12 +225,23 @@ name|virtual
 operator|~
 name|__function_type_info
 argument_list|()
-block|; 	}
-decl_stmt|;
+block|;
+name|virtual
+name|bool
+name|__is_function_p
+argument_list|()
+specifier|const
+block|{
+return|return
+name|true
+return|;
+block|}
+expr|}
+block|;
 comment|/** 	 * Type info for enums. 	 */
-name|struct
+block|struct
 name|__enum_type_info
-range|:
+operator|:
 name|public
 name|std
 operator|::
@@ -84,11 +252,11 @@ operator|~
 name|__enum_type_info
 argument_list|()
 block|; 	}
-decl_stmt|;
+block|;
 comment|/** 	 * Base class for class type info.  Used only for tentative definitions. 	 */
-name|struct
+block|struct
 name|__class_type_info
-range|:
+operator|:
 name|public
 name|std
 operator|::
@@ -111,20 +279,28 @@ argument|const struct __class_type_info *other
 argument_list|)
 specifier|const
 block|;
-comment|/** 		 * Function returning whether a cast from this type to another type is 		 * possible. 		 */
 name|virtual
 name|bool
-name|can_cast_to
+name|__do_upcast
 argument_list|(
-argument|const struct __class_type_info *other
+argument|const __class_type_info *target
+argument_list|,
+argument|void **thrown_object
 argument_list|)
 specifier|const
-block|; 	}
-decl_stmt|;
+block|{
+return|return
+name|this
+operator|==
+name|target
+return|;
+block|}
+expr|}
+block|;
 comment|/** 	 * Single-inheritance class type info.  This is used for classes containing 	 * a single non-virtual base class at offset 0. 	 */
-name|struct
+block|struct
 name|__si_class_type_info
-range|:
+operator|:
 name|public
 name|__class_type_info
 block|{
@@ -139,6 +315,16 @@ operator|*
 name|__base_type
 block|;
 name|virtual
+name|bool
+name|__do_upcast
+argument_list|(
+argument|const ABI_NAMESPACE::__class_type_info *target
+argument_list|,
+argument|void **thrown_object
+argument_list|)
+specifier|const
+block|;
+name|virtual
 name|void
 operator|*
 name|cast_to
@@ -148,53 +334,45 @@ argument_list|,
 argument|const struct __class_type_info *other
 argument_list|)
 specifier|const
-block|;
-name|virtual
-name|bool
-name|can_cast_to
-argument_list|(
-argument|const struct __class_type_info *other
-argument_list|)
-specifier|const
 block|; 	}
-decl_stmt|;
+block|;
 comment|/** 	 * Type info for base classes.  Classes with multiple bases store an array 	 * of these, one for each superclass. 	 */
-struct|struct
+block|struct
 name|__base_class_type_info
 block|{
 specifier|const
 name|__class_type_info
-modifier|*
+operator|*
 name|__base_type
-decl_stmt|;
+block|;
 name|private
-label|:
+operator|:
 comment|/** 			 * The high __offset_shift bits of this store the (signed) offset 			 * of the base class.  The low bits store flags from 			 * __offset_flags_masks. 			 */
 name|long
 name|__offset_flags
-decl_stmt|;
+block|;
 comment|/** 			 * Flags used in the low bits of __offset_flags. 			 */
-enum|enum
+block|enum
 name|__offset_flags_masks
 block|{
 comment|/** This base class is virtual. */
 name|__virtual_mask
-init|=
+operator|=
 literal|0x1
 block|,
 comment|/** This base class is public. */
 name|__public_mask
-init|=
+operator|=
 literal|0x2
 block|,
 comment|/** The number of bits reserved for flags. */
 name|__offset_shift
-init|=
+operator|=
 literal|8
 block|}
-enum|;
+block|;
 name|public
-label|:
+operator|:
 comment|/** 			 * Returns the offset of the base class. 			 */
 name|long
 name|offset
@@ -253,12 +431,12 @@ operator|&
 name|__virtual_mask
 return|;
 block|}
-block|}
-struct|;
+expr|}
+block|;
 comment|/** 	 * Type info for classes with virtual bases or multiple superclasses. 	 */
-name|struct
+block|struct
 name|__vmi_class_type_info
-range|:
+operator|:
 name|public
 name|__class_type_info
 block|{
@@ -300,6 +478,16 @@ literal|0x2
 block|}
 block|;
 name|virtual
+name|bool
+name|__do_upcast
+argument_list|(
+argument|const ABI_NAMESPACE::__class_type_info *target
+argument_list|,
+argument|void **thrown_object
+argument_list|)
+specifier|const
+block|;
+name|virtual
 name|void
 operator|*
 name|cast_to
@@ -309,20 +497,12 @@ argument_list|,
 argument|const struct __class_type_info *other
 argument_list|)
 specifier|const
-block|;
-name|virtual
-name|bool
-name|can_cast_to
-argument_list|(
-argument|const struct __class_type_info *other
-argument_list|)
-specifier|const
 block|; 	}
-decl_stmt|;
+block|;
 comment|/** 	 * Base class used for both pointer and pointer-to-member type info. 	 */
-name|struct
+block|struct
 name|__pbase_type_info
-range|:
+operator|:
 name|public
 name|std
 operator|::
@@ -375,12 +555,34 @@ name|__incomplete_class_mask
 operator|=
 literal|0x10
 block|}
+block|;
+name|virtual
+name|bool
+name|__is_pointer_p
+argument_list|()
+specifier|const
+block|{
+return|return
+name|true
+return|;
+block|}
+name|virtual
+name|bool
+name|__do_catch
+argument_list|(
+argument|const type_info *thrown_type
+argument_list|,
+argument|void **thrown_object
+argument_list|,
+argument|unsigned outer
+argument_list|)
+specifier|const
 block|; 	}
-decl_stmt|;
+block|;
 comment|/** 	 * Pointer type info. 	 */
-name|struct
+block|struct
 name|__pointer_type_info
-range|:
+operator|:
 name|public
 name|__pbase_type_info
 block|{
@@ -389,11 +591,11 @@ operator|~
 name|__pointer_type_info
 argument_list|()
 block|; 	}
-decl_stmt|;
+block|;
 comment|/** 	 * Pointer to member type info. 	 */
-name|struct
+block|struct
 name|__pointer_to_member_type_info
-range|:
+operator|:
 name|public
 name|__pbase_type_info
 block|{
@@ -408,8 +610,7 @@ name|__class_type_info
 operator|*
 name|__context
 block|; 	}
-decl_stmt|;
-block|}
+block|;  }
 end_decl_stmt
 
 end_unit
