c382ef968aa924c8d232d46a9ff7232df22a4768
Gleb Smirnoff <glebius@FreeBSD.org>
2012-03-21 07:29:47 +0000

    Merge 228321 from head:   Fix double free.
      PR:           kern/163089
      Submitted by: Herbie Robinson <Herbie.Robinson stratus.com>
    
    
    Former-commit-id: d89fa8c46589617f3ad9db617b2b23d6ee6f3566

--- sys/netinet6/mld6.c
+++ sys/netinet6/mld6.c
@@ -11608,11 +11608,6 @@ argument_list|,
 name|m
 argument_list|)
 expr_stmt|;
-name|m_freem
-argument_list|(
-name|m
-argument_list|)
-expr_stmt|;
 name|IP6STAT_INC
 argument_list|(
 name|ip6s_odropped
