3aec952d860683804ad3f14fb3e4b0793dcf0e6b
John Baldwin <jhb@FreeBSD.org>
2012-03-23 18:22:38 +0000

    MFC 232367: Properly handle failures in igb_setup_msix() by returning 0 if MSI or MSI-X allocation fails.
    Former-commit-id: 5cb14f9007ffeb55e7178c0374070b106dac7fae

--- sys/dev/e1000/if_igb.c
+++ sys/dev/e1000/if_igb.c
@@ -12827,7 +12827,7 @@ argument_list|)
 expr_stmt|;
 return|return
 operator|(
-name|ENXIO
+literal|0
 operator|)
 return|;
 block|}
@@ -12896,18 +12896,25 @@ argument_list|)
 operator|==
 literal|0
 condition|)
+block|{
 name|device_printf
 argument_list|(
 name|adapter
 operator|->
 name|dev
 argument_list|,
-literal|"Using MSI interrupt\n"
+literal|" Using MSI interrupt\n"
 argument_list|)
 expr_stmt|;
 return|return
 operator|(
 name|msgs
+operator|)
+return|;
+block|}
+return|return
+operator|(
+literal|0
 operator|)
 return|;
 block|}
