80d798b6f6342fdc27193835e7d6e98fd6720984
Nathan Whitehorn <nwhitehorn@FreeBSD.org>
2012-04-05 00:53:21 +0000

    MFC 232980,233011,233017,233117,233434,233436,233454,233529,233530,233618:
    Major pmap performance, concurrency, and correctness improvements, mostly
    for the 64-bit PMAP module (64-bit-capable CPUs with either a 32-bit or
    64-bit kernel). Thanks to alc for his help and prodding.
    
    
    Former-commit-id: 4cf272666ea11f56dd33f795c7dbe96bc5eb584c

--- sys/powerpc/aim/mmu_oea.c
+++ sys/powerpc/aim/mmu_oea.c
@@ -8135,6 +8135,9 @@ name|struct
 name|pvo_entry
 modifier|*
 name|pvo
+decl_stmt|,
+modifier|*
+name|tpvo
 decl_stmt|;
 name|int
 name|pteidx
@@ -8201,13 +8204,15 @@ block|}
 block|}
 else|else
 block|{
-name|LIST_FOREACH
+name|LIST_FOREACH_SAFE
 argument_list|(
 argument|pvo
 argument_list|,
 argument|&pm->pmap_pvo
 argument_list|,
 argument|pvo_plink
+argument_list|,
+argument|tpvo
 argument_list|)
 block|{
 if|if
