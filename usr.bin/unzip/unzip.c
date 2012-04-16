343bccd00f8b6bc74e428d3f0133c533db1e41ba
Kevin Lo <kevlo@FreeBSD.org>
2012-04-16 01:20:17 +0000

    MFC r234206: Partially implement zipinfo (-Z) support. This fixes some test failures seen with perl 5.12 and 5.14.
    PR:	bin/166895
    Submitted by:	swills
    
    Former-commit-id: a7948b405a943eeddb47c7a7663c3bb9385371cd

--- usr.bin/unzip/unzip.c
+++ usr.bin/unzip/unzip.c
@@ -241,6 +241,17 @@ begin_comment
 comment|/* verbose/list */
 end_comment
 
+begin_decl_stmt
+specifier|static
+name|int
+name|Z1_opt
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* zipinfo mode list files only */
+end_comment
+
 begin_comment
 comment|/* time when unzip started */
 end_comment
@@ -260,6 +271,17 @@ begin_decl_stmt
 specifier|static
 name|int
 name|unzip_debug
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* zipinfo mode */
+end_comment
+
+begin_decl_stmt
+specifier|static
+name|int
+name|zipinfo_mode
 decl_stmt|;
 end_decl_stmt
 
@@ -3324,6 +3346,12 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+operator|!
+name|zipinfo_mode
+condition|)
+block|{
+if|if
+condition|(
 name|v_opt
 operator|==
 literal|1
@@ -3381,6 +3409,24 @@ argument_list|,
 name|buf
 argument_list|,
 literal|0U
+argument_list|,
+name|archive_entry_pathname
+argument_list|(
+name|e
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+else|else
+block|{
+if|if
+condition|(
+name|Z1_opt
+condition|)
+name|printf
+argument_list|(
+literal|"%s\n"
 argument_list|,
 name|archive_entry_pathname
 argument_list|(
@@ -3604,6 +3650,12 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
+name|zipinfo_mode
+condition|)
+block|{
+if|if
+condition|(
+operator|!
 name|p_opt
 operator|&&
 operator|!
@@ -3653,6 +3705,7 @@ literal|"--------  ------  ------- -----   ----   ----   ------    ----\n"
 argument_list|)
 expr_stmt|;
 block|}
+block|}
 name|total_size
 operator|=
 literal|0
@@ -3693,6 +3746,12 @@ argument_list|(
 name|ret
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+operator|!
+name|zipinfo_mode
+condition|)
+block|{
 if|if
 condition|(
 name|t_opt
@@ -3740,6 +3799,21 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+block|}
+else|else
+block|{
+if|if
+condition|(
+name|Z1_opt
+condition|)
+name|list
+argument_list|(
+name|a
+argument_list|,
+name|e
+argument_list|)
+expr_stmt|;
+block|}
 name|total_size
 operator|+=
 name|archive_entry_size
@@ -3751,6 +3825,11 @@ operator|++
 name|file_count
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|zipinfo_mode
+condition|)
+block|{
 if|if
 condition|(
 name|v_opt
@@ -3813,6 +3892,7 @@ else|:
 literal|""
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 name|ac
 argument_list|(
@@ -3892,7 +3972,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: unzip [-aCcfjLlnopqtuv] [-d dir] [-x pattern] zipfile\n"
+literal|"usage: unzip [-aCcfjLlnopqtuvZ1] [-d dir] [-x pattern] zipfile\n"
 argument_list|)
 expr_stmt|;
 name|exit
@@ -3937,7 +4017,7 @@ name|argc
 argument_list|,
 name|argv
 argument_list|,
-literal|"aCcd:fjLlnopqtuvx:"
+literal|"aCcd:fjLlnopqtuvx:Z1"
 argument_list|)
 operator|)
 operator|!=
@@ -3949,6 +4029,14 @@ condition|(
 name|opt
 condition|)
 block|{
+case|case
+literal|'1'
+case|:
+name|Z1_opt
+operator|=
+literal|1
+expr_stmt|;
+break|break;
 case|case
 literal|'a'
 case|:
@@ -4091,6 +4179,14 @@ name|optarg
 argument_list|)
 expr_stmt|;
 break|break;
+case|case
+literal|'Z'
+case|:
+name|zipinfo_mode
+operator|=
+literal|1
+expr_stmt|;
+break|break;
 default|default:
 name|usage
 argument_list|()
@@ -4195,6 +4291,26 @@ argument_list|,
 name|argv
 argument_list|)
 expr_stmt|;
+comment|/*  	 * When more of the zipinfo mode options are implemented, this 	 * will need to change. 	 */
+if|if
+condition|(
+name|zipinfo_mode
+operator|&&
+operator|!
+name|Z1_opt
+condition|)
+block|{
+name|printf
+argument_list|(
+literal|"Zipinfo mode needs additional options\n"
+argument_list|)
+expr_stmt|;
+name|exit
+argument_list|(
+literal|1
+argument_list|)
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|argc
