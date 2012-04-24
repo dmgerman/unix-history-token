2bf1d3061c0ec659dc90288ba3cdf5176f57a70f
David Schultz <das@FreeBSD.org>
2012-04-24 03:56:39 +0000

    MFC r234528, r234536:   Fix an infinite loop in fputws().
    Former-commit-id: 61008dfa772813c064d72068116e560e47071af2

--- lib/libc/stdio/fputws.c
+++ lib/libc/stdio/fputws.c
@@ -158,12 +158,12 @@ name|iov_base
 operator|=
 name|buf
 expr_stmt|;
-do|do
-block|{
 name|wsp
 operator|=
 name|ws
 expr_stmt|;
+do|do
+block|{
 name|nbytes
 operator|=
 name|__wcsnrtombs
@@ -227,7 +227,7 @@ goto|;
 block|}
 do|while
 condition|(
-name|ws
+name|wsp
 operator|!=
 name|NULL
 condition|)
