f4ec81670d3a1b4dfc6954bdff52a7ca6ecac606
Alexander Motin <mav@FreeBSD.org>
2012-05-12 13:55:36 +0000

    Add two functions xpt_batch_start() and xpt_batch_done() to the CAM SIM KPI to allow drivers to handle request completion directly without passing them to the CAM SWI thread removing extra context switch. Modify all ATA/SATA drivers to use them.
    Reviewed by:	gibbs, ken
    MFC after:	2 weeks
    
    
    Former-commit-id: 24a057ff71c7ec54f2cdc3a586e970b0611d4e89

--- sys/dev/ata/ata-all.c
+++ sys/dev/ata/ata-all.c
@@ -2961,6 +2961,13 @@ operator|->
 name|state_mtx
 argument_list|)
 expr_stmt|;
+name|xpt_batch_start
+argument_list|(
+name|ch
+operator|->
+name|sim
+argument_list|)
+expr_stmt|;
 endif|#
 directive|endif
 name|ata_interrupt_locked
@@ -2971,6 +2978,13 @@ expr_stmt|;
 ifdef|#
 directive|ifdef
 name|ATA_CAM
+name|xpt_batch_done
+argument_list|(
+name|ch
+operator|->
+name|sim
+argument_list|)
+expr_stmt|;
 name|mtx_unlock
 argument_list|(
 operator|&
