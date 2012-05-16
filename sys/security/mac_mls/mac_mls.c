e0fb40e984c9710e9f2223f4d5e2a345fc415bca
Christian Brueffer <brueffer@FreeBSD.org>
2012-05-16 19:47:02 +0000

    MFC: r234957
    Check vplabel for NULL before dereferencing it.  Fixes a panic
    when running atop with MAC_MLS enabled.
    
    
    Former-commit-id: 1c923e0736b1996e5dae779636b789bbd6b2f1c3

--- sys/security/mac_mls/mac_mls.c
+++ sys/security/mac_mls/mac_mls.c
@@ -8192,6 +8192,17 @@ operator|(
 literal|0
 operator|)
 return|;
+if|if
+condition|(
+name|vplabel
+operator|==
+name|NULL
+condition|)
+return|return
+operator|(
+literal|0
+operator|)
+return|;
 name|subj
 operator|=
 name|SLOT
