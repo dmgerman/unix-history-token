d12b3b9f8f694709bd5f9609939700dc831ccc7e
Dag-Erling Smorgrav <des@FreeBSD.org>
2012-06-16 09:17:21 +0000

    Backport sha512 support.
    Former-commit-id: fbb57af98199d2cac69f31dcc2f2a544d8b1b6bd

--- lib/libmd/mddriver.c
+++ lib/libmd/mddriver.c
@@ -1,6 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* MDDRIVER.C - test driver for MD2, MD4 and MD5  */
+comment|/* MDDRIVER.C - test driver for MD2, MD4 and MD5 */
+end_comment
+
+begin_comment
+comment|/* Copyright (C) 1990-2, RSA Data Security, Inc. Created 1990. All rights  * reserved.  *   * RSA Data Security, Inc. makes no representations concerning either the  * merchantability of this software or the suitability of this software for  * any particular purpose. It is provided "as is" without express or implied  * warranty of any kind.  *   * These notices must be retained in any copies of any part of this  * documentation and/or software. */
 end_comment
 
 begin_include
@@ -16,32 +20,6 @@ literal|"$FreeBSD$"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
-
-begin_comment
-comment|/* Copyright (C) 1990-2, RSA Data Security, Inc. Created 1990. All    rights reserved.     RSA Data Security, Inc. makes no representations concerning either    the merchantability of this software or the suitability of this    software for any particular purpose. It is provided "as is"    without express or implied warranty of any kind.     These notices must be retained in any copies of any part of this    documentation and/or software.  */
-end_comment
-
-begin_comment
-comment|/* The following makes MD default to MD5 if it has not already been      defined with C compiler flags.  */
-end_comment
-
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|MD
-end_ifndef
-
-begin_define
-define|#
-directive|define
-name|MD
-value|5
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 begin_include
 include|#
@@ -66,6 +44,28 @@ include|#
 directive|include
 file|<string.h>
 end_include
+
+begin_comment
+comment|/* The following makes MD default to MD5 if it has not already been defined  * with C compiler flags. */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|MD
+value|5
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_if
 if|#
@@ -146,7 +146,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* Digests a string and prints the result.  */
+comment|/* Digests a string and prints the result. */
 end_comment
 
 begin_function
@@ -154,12 +154,10 @@ specifier|static
 name|void
 name|MDString
 parameter_list|(
-name|string
-parameter_list|)
 name|char
 modifier|*
 name|string
-decl_stmt|;
+parameter_list|)
 block|{
 name|char
 name|buf
@@ -192,12 +190,15 @@ block|}
 end_function
 
 begin_comment
-comment|/* Digests a reference suite of strings and prints the results.  */
+comment|/* Digests a reference suite of strings and prints the results. */
 end_comment
 
 begin_function
+name|int
 name|main
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 name|printf
 argument_list|(
@@ -233,12 +234,14 @@ argument_list|)
 expr_stmt|;
 name|MDString
 argument_list|(
-literal|"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+literal|"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+literal|"abcdefghijklmnopqrstuvwxyz0123456789"
 argument_list|)
 expr_stmt|;
 name|MDString
 argument_list|(
-literal|"1234567890123456789012345678901234567890\ 1234567890123456789012345678901234567890"
+literal|"1234567890123456789012345678901234567890"
+literal|"1234567890123456789012345678901234567890"
 argument_list|)
 expr_stmt|;
 return|return
