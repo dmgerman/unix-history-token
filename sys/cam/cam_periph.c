d40a32f3715c516be22a0237bd33f4ee05118f0d
Alexander Motin <mav@FreeBSD.org>
2012-06-25 07:06:58 +0000

    MFC r237446: Don't print SCSI Queue Full and CAM_REQUEUE_REQ statuses as errors if they were handled and retried. They are part of normal operation for SCSI TCQ.
    Former-commit-id: 6215b30d760fb035cdb111399525aaaa1b7d586b

--- sys/cam/cam_periph.c
+++ sys/cam/cam_periph.c
@@ -6058,6 +6058,11 @@ name|error
 operator|=
 name|ERESTART
 expr_stmt|;
+operator|*
+name|print
+operator|=
+literal|0
+expr_stmt|;
 break|break;
 block|}
 comment|/* FALLTHROUGH */
@@ -7164,10 +7169,16 @@ literal|"Retry was blocked"
 expr_stmt|;
 block|}
 else|else
+block|{
 name|error
 operator|=
 name|ERESTART
 expr_stmt|;
+name|print
+operator|=
+literal|0
+expr_stmt|;
+block|}
 break|break;
 case|case
 name|CAM_RESRC_UNAVAIL
