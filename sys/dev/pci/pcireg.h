60b0081cb70944d2efd3229fa0223ab1da505fb5
John Baldwin <jhb@FreeBSD.org>
2012-06-28 21:24:09 +0000

    MFC 228161,230774,230822,236415: Add a new -e flag to pciconf(8)'s list mode to display PCI error details. Currently this dumps the status of any error bits in the PCI status register and PCI-express device status register.  It also lists any errors indicated by version 1 of PCI-express Advanced Error Reporting (AER).
    Former-commit-id: 04fb4bb4a4dfbbb7d0804ad0e87fa21cfa93e97d

--- sys/dev/pci/pcireg.h
+++ sys/dev/pci/pcireg.h
@@ -4316,6 +4316,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|PCIM_AER_UC_SURPRISE_LINK_DOWN
+value|0x00000020
+end_define
+
+begin_define
+define|#
+directive|define
 name|PCIM_AER_UC_POISONED_TLP
 value|0x00001000
 end_define
@@ -4445,6 +4452,13 @@ define|#
 directive|define
 name|PCIM_AER_COR_REPLAY_TIMEOUT
 value|0x00001000
+end_define
+
+begin_define
+define|#
+directive|define
+name|PCIM_AER_COR_ADVISORY_NF_ERROR
+value|0x00002000
 end_define
 
 begin_define
