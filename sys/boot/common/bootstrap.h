f88b996e331291009be99c2757b479b764856162
Andriy Gapon <avg@FreeBSD.org>
2012-06-29 09:56:22 +0000

    MFC r235153: sys/boot: add common CTASSERT definition
    Former-commit-id: ba07d422cfd4f9346421cf076de6e985f372aa83

--- sys/boot/common/bootstrap.h
+++ sys/boot/common/bootstrap.h
@@ -1524,6 +1524,55 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|CTASSERT
+end_ifndef
+
+begin_comment
+comment|/* Allow lint to override */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|CTASSERT
+parameter_list|(
+name|x
+parameter_list|)
+value|_CTASSERT(x, __LINE__)
+end_define
+
+begin_define
+define|#
+directive|define
+name|_CTASSERT
+parameter_list|(
+name|x
+parameter_list|,
+name|y
+parameter_list|)
+value|__CTASSERT(x, y)
+end_define
+
+begin_define
+define|#
+directive|define
+name|__CTASSERT
+parameter_list|(
+name|x
+parameter_list|,
+name|y
+parameter_list|)
+value|typedef char __assert ## y[(x) ? 1 : -1]
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_endif
 endif|#
 directive|endif
