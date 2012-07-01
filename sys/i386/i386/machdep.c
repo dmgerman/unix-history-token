b9d157f20bb121bcd7d66898e801201488aa78d9
Christian Brueffer <brueffer@FreeBSD.org>
2012-07-01 12:59:00 +0000

    Replace an unreachable panic() in vm86_getptr (been there for 13 years) with a KASSERT() behind the functions's only consumer.
    Suggested by:	kib
    Reviewed by:	kib
    CID:		4494
    Found with:	Coverity Prevent(tm)
    MFC after:	2 weeks
    
    Former-commit-id: f4e755bf114d590648b71cee1d5cc28b97b77ca6

--- sys/i386/i386/machdep.c
+++ sys/i386/i386/machdep.c
@@ -10588,6 +10588,8 @@ decl_stmt|,
 name|pa_indx
 decl_stmt|,
 name|da_indx
+decl_stmt|,
+name|res
 decl_stmt|;
 name|u_long
 name|physmem_tunable
@@ -10978,6 +10980,8 @@ name|PAGE_SHIFT
 operator|)
 argument_list|)
 expr_stmt|;
+name|res
+operator|=
 name|vm86_getptr
 argument_list|(
 operator|&
@@ -10997,6 +11001,17 @@ operator|&
 name|vmf
 operator|.
 name|vmf_di
+argument_list|)
+expr_stmt|;
+name|KASSERT
+argument_list|(
+name|res
+operator|!=
+literal|0
+argument_list|,
+operator|(
+literal|"vm86_getptr() failed: address not found"
+operator|)
 argument_list|)
 expr_stmt|;
 name|vmf
