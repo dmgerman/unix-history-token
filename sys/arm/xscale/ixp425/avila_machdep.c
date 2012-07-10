0184afd50cc66a7d1080294ffea0f32a5bbb734c
Warner Losh <imp@FreeBSD.org>
2012-07-10 01:49:50 +0000

    Remove some unused variables/externs that have been copied too many times...
    Former-commit-id: 22a94e3bcb6b8c3b4981d4cd58bd5972fc74909f

--- sys/arm/xscale/ixp425/avila_machdep.c
+++ sys/arm/xscale/ixp425/avila_machdep.c
@@ -416,22 +416,6 @@ decl_stmt|;
 end_decl_stmt
 
 begin_decl_stmt
-specifier|extern
-name|void
-modifier|*
-name|_end
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-specifier|extern
-name|int
-modifier|*
-name|end
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
 name|struct
 name|pcpu
 name|__pcpu
@@ -468,12 +452,6 @@ name|dump_avail
 index|[
 literal|4
 index|]
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-name|vm_offset_t
-name|physical_pages
 decl_stmt|;
 end_decl_stmt
 
