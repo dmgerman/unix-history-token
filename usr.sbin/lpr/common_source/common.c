e0c7fbae2d5fce8bd77e1e55260dba47cbe15cab
Jaakko Heinonen <jh@FreeBSD.org>
2012-07-17 09:34:52 +0000

    Make sure that arraysz is initialized to a value larger than zero. arraysz could get initialized to zero on ZFS because ZFS reports directory sizes differently compared to UFS.
    PR:		bin/169493
    Tested by:	swills
    MFC after:	2 weeks
    
    Former-commit-id: d4b4d007d7f28602fc5307f28b0d3cbbaf9dab16

--- usr.sbin/lpr/common_source/common.c
+++ usr.sbin/lpr/common_source/common.c
@@ -468,6 +468,16 @@ operator|/
 literal|24
 operator|)
 expr_stmt|;
+if|if
+condition|(
+name|arraysz
+operator|<
+literal|16
+condition|)
+name|arraysz
+operator|=
+literal|16
+expr_stmt|;
 name|queue
 operator|=
 operator|(
