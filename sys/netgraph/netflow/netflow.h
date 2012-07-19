fbb7eca684a3c1ef859612462cedd1ec682ed37e
Alexander V. Chernikov <melifaro@FreeBSD.org>
2012-07-19 11:43:31 +0000

    MFC r219182, r219229, r220769, r223706, r223787, r223822, r232921, r237162, r237163, r237164, r237226, r237227
    Merge netflow v9 support (kernel side)
    
    r219182 - Add support for NetFlow version 9 into ng_netflow(4) node.
    r219229 - Unbreak the build for no options INET6.
    r220769 - ng_netflow_cache_init() can be void.
    r223706 - Fix double free.
    r223787 - Eliminate flow6_hash_entry in favor of flow_hash_entry
    r223822 - Add missing unlocks.
    r232921 - Use rt_numfibs variable instead of compile-time RT_NUMFIBS.
    r237162 - Fix improper L4 header handling for IPv6 packets passed via DLT_RAW.
    r237163 - Set netflow v9 observation domain value to fib number instead of node id
    r237164 - Use time_uptime instead of getnanotime for accouting integer number of seconds.
    r237226 - Simplify IP pointer recovery in case of mbuf reallocation.
    r237227 - Make radix lookup on src and dst flow addresses optional
    
    No objection from:  glebius
    Approved by:        ae(mentor)
    
    
    Former-commit-id: cf94ae71e997b2a523f3ad582d6da7ac3753918f

--- sys/netgraph/netflow/netflow.h
+++ sys/netgraph/netflow/netflow.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2004 Gleb Smirnoff<glebius@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	 $SourceForge: netflow.h,v 1.8 2004/09/16 17:05:11 glebius Exp $  *	 $FreeBSD$  */
+comment|/*-  * Copyright (c) 2010-2011 Alexander V. Chernikov<melifaro@ipfw.ru>  * Copyright (c) 2004 Gleb Smirnoff<glebius@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	 $SourceForge: netflow.h,v 1.8 2004/09/16 17:05:11 glebius Exp $  *	 $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -26,7 +26,7 @@ value|15
 end_define
 
 begin_comment
-comment|/*  * More info can be found in these Cisco documents:  *  * Cisco IOS NetFlow, White Papers.  * http://www.cisco.com/en/US/products/ps6601/prod_white_papers_list.html  *  * Cisco CNS NetFlow Collection Engine User Guide, 5.0.2, NetFlow Export  * Datagram Formats.  * http://www.cisco.com/en/US/products/sw/netmgtsw/ps1964/products_user_guide_chapter09186a00803f3147.html#wp26453  *  */
+comment|/*  * More info can be found in these Cisco documents:  *  * Cisco IOS NetFlow, White Papers.  * http://www.cisco.com/en/US/products/ps6601/prod_white_papers_list.html  *  * Cisco CNS NetFlow Collection Engine User Guide, 5.0.2, NetFlow Export  * Datagram Formats.  * http://www.cisco.com/en/US/products/sw/netmgtsw/ps1964/products_user_guide_chapter09186a00803f3147.html#wp26453  *  * Cisco Systems NetFlow Services Export Version 9  * http://www.ietf.org/rfc/rfc3954.txt  *  */
 end_comment
 
 begin_define
@@ -41,6 +41,13 @@ define|#
 directive|define
 name|NETFLOW_V5
 value|5
+end_define
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9
+value|9
 end_define
 
 begin_struct
@@ -117,6 +124,44 @@ name|uint16_t
 name|pad
 decl_stmt|;
 comment|/* Pad to word boundary */
+block|}
+name|__attribute__
+argument_list|(
+operator|(
+name|__packed__
+operator|)
+argument_list|)
+struct|;
+end_struct
+
+begin_struct
+struct|struct
+name|netflow_v9_header
+block|{
+name|uint16_t
+name|version
+decl_stmt|;
+comment|/* NetFlow version */
+name|uint16_t
+name|count
+decl_stmt|;
+comment|/* Total number of records in packet */
+name|uint32_t
+name|sys_uptime
+decl_stmt|;
+comment|/* System uptime */
+name|uint32_t
+name|unix_secs
+decl_stmt|;
+comment|/* Current seconds since 0000 UTC 1970 */
+name|uint32_t
+name|seq_num
+decl_stmt|;
+comment|/* Sequence number */
+name|uint32_t
+name|source_id
+decl_stmt|;
+comment|/* Observation Domain id */
 block|}
 name|__attribute__
 argument_list|(
@@ -362,6 +407,736 @@ operator|)
 argument_list|)
 struct|;
 end_struct
+
+begin_comment
+comment|/* RFC3954 field definitions */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IN_BYTES
+value|1
+end_define
+
+begin_comment
+comment|/* Input bytes count for a flow. Default 4, can be 8 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IN_PKTS
+value|2
+end_define
+
+begin_comment
+comment|/* Incoming counter with number of packets associated with an IP Flow. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FLOWS
+value|3
+end_define
+
+begin_comment
+comment|/* Number of Flows that were aggregated. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_PROTOCOL
+value|4
+end_define
+
+begin_comment
+comment|/* IP protocol byte. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_TOS
+value|5
+end_define
+
+begin_comment
+comment|/* Type of service byte setting when entering the incoming interface. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_TCP_FLAGS
+value|6
+end_define
+
+begin_comment
+comment|/* TCP flags; cumulative of all the TCP flags seen in this Flow. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_L4_SRC_PORT
+value|7
+end_define
+
+begin_comment
+comment|/* TCP/UDP source port number. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV4_SRC_ADDR
+value|8
+end_define
+
+begin_comment
+comment|/* IPv4 source address. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_SRC_MASK
+value|9
+end_define
+
+begin_comment
+comment|/* The number of contiguous bits in the source subnet mask (i.e., the mask in slash notation). 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_INPUT_SNMP
+value|10
+end_define
+
+begin_comment
+comment|/* Input interface index. Default 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_L4_DST_PORT
+value|11
+end_define
+
+begin_comment
+comment|/* TCP/UDP destination port number. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV4_DST_ADDR
+value|12
+end_define
+
+begin_comment
+comment|/* IPv4 destination address. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_DST_MASK
+value|13
+end_define
+
+begin_comment
+comment|/* The number of contiguous bits in the destination subnet mask (i.e., the mask in slash notation). 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_OUTPUT_SNMP
+value|14
+end_define
+
+begin_comment
+comment|/* Output interface index. Default 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV4_NEXT_HOP
+value|15
+end_define
+
+begin_comment
+comment|/* IPv4 address of the next-hop router. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_SRC_AS
+value|16
+end_define
+
+begin_comment
+comment|/* Source BGP autonomous system number. Default 2, can be 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_DST_AS
+value|17
+end_define
+
+begin_comment
+comment|/* Destination BGP autonomous system number. Default 2, can be 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_BGP_IPV4_NEXT_HOP
+value|18
+end_define
+
+begin_comment
+comment|/* Next-hop router's IP address in the BGP domain. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MUL_DST_PKTS
+value|19
+end_define
+
+begin_comment
+comment|/* IP multicast outgoing packet counter for packets associated with IP flow. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MUL_DST_BYTES
+value|20
+end_define
+
+begin_comment
+comment|/* IP multicast outgoing Octet (byte) counter for the number of bytes associated with IP flow. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_LAST_SWITCHED
+value|21
+end_define
+
+begin_comment
+comment|/* sysUptime in msec at which the last packet of this Flow was switched. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FIRST_SWITCHED
+value|22
+end_define
+
+begin_comment
+comment|/* sysUptime in msec at which the first packet of this Flow was switched. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_OUT_BYTES
+value|23
+end_define
+
+begin_comment
+comment|/* Outgoing counter for the number of bytes associated with an IP Flow. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_OUT_PKTS
+value|24
+end_define
+
+begin_comment
+comment|/* Outgoing counter for the number of packets associated with an IP Flow. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_SRC_ADDR
+value|27
+end_define
+
+begin_comment
+comment|/* IPv6 source address. 16 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_DST_ADDR
+value|28
+end_define
+
+begin_comment
+comment|/* IPv6 destination address. 16 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_SRC_MASK
+value|29
+end_define
+
+begin_comment
+comment|/* Length of the IPv6 source mask in contiguous bits. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_DST_MASK
+value|30
+end_define
+
+begin_comment
+comment|/* Length of the IPv6 destination mask in contiguous bits. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_FLOW_LABEL
+value|31
+end_define
+
+begin_comment
+comment|/* IPv6 flow label as per RFC 2460 definition. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_ICMP_TYPE
+value|32
+end_define
+
+begin_comment
+comment|/* Internet Control Message Protocol (ICMP) packet type; reported as ICMP Type * 256 + ICMP code. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MUL_IGMP_TYPE
+value|33
+end_define
+
+begin_comment
+comment|/* Internet Group Management Protocol (IGMP) packet type. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_SAMPLING_INTERVAL
+value|34
+end_define
+
+begin_comment
+comment|/* When using sampled NetFlow, the rate at which packets are sampled; for example, a value of 100 indicates that one of every hundred packets is sampled. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_SAMPLING_ALGORITHM
+value|35
+end_define
+
+begin_comment
+comment|/* For sampled NetFlow platform-wide: 0x01 deterministic sampling 0x02 random sampling. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FLOW_ACTIVE_TIMEOUT
+value|36
+end_define
+
+begin_comment
+comment|/* Timeout value (in seconds) for active flow entries in the NetFlow cache. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FLOW_INACTIVE_TIMEOUT
+value|37
+end_define
+
+begin_comment
+comment|/* Timeout value (in seconds) for inactive Flow entries in the NetFlow cache. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_ENGINE_TYPE
+value|38
+end_define
+
+begin_comment
+comment|/* Type of Flow switching engine (route processor, linecard, etc...). 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_ENGINE_ID
+value|39
+end_define
+
+begin_comment
+comment|/* ID number of the Flow switching engine. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_TOTAL_BYTES_EXP
+value|40
+end_define
+
+begin_comment
+comment|/* Counter with for the number of bytes exported by the Observation Domain. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_TOTAL_PKTS_EXP
+value|41
+end_define
+
+begin_comment
+comment|/* Counter with for the number of packets exported by the Observation Domain. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_TOTAL_FLOWS_EXP
+value|42
+end_define
+
+begin_comment
+comment|/* Counter with for the number of flows exported by the Observation Domain. Default 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_TOP_LABEL_TYPE
+value|46
+end_define
+
+begin_comment
+comment|/* MPLS Top Label Type. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_TOP_LABEL_IP_ADDR
+value|47
+end_define
+
+begin_comment
+comment|/* Forwarding Equivalent Class corresponding to the MPLS Top Label. 4 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FLOW_SAMPLER_ID
+value|48
+end_define
+
+begin_comment
+comment|/* Identifier shown in "show flow-sampler". 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FLOW_SAMPLER_MODE
+value|49
+end_define
+
+begin_comment
+comment|/* The type of algorithm used for sampling data. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_FLOW_SAMPLER_RANDOM_INTERVAL
+value|50
+end_define
+
+begin_comment
+comment|/* Packet interval at which to sample. 4. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_DST_TOS
+value|55
+end_define
+
+begin_comment
+comment|/* Type of Service byte setting when exiting outgoing interface. 1. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_SRC_MAC
+value|56
+end_define
+
+begin_comment
+comment|/* Source MAC Address. 6 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_DST_MAC
+value|57
+end_define
+
+begin_comment
+comment|/* Destination MAC Address. 6 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_SRC_VLAN
+value|58
+end_define
+
+begin_comment
+comment|/* Virtual LAN identifier associated with ingress interface. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_DST_VLAN
+value|59
+end_define
+
+begin_comment
+comment|/* Virtual LAN identifier associated with egress interface. 2 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IP_PROTOCOL_VERSION
+value|60
+end_define
+
+begin_comment
+comment|/* Internet Protocol Version. Set to 4 for IPv4, set to 6 for IPv6. If not present in the template, then version 4 is assumed. 1. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_DIRECTION
+value|61
+end_define
+
+begin_comment
+comment|/* Flow direction: 0 - ingress flow 1 - egress flow. 1 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_NEXT_HOP
+value|62
+end_define
+
+begin_comment
+comment|/* IPv6 address of the next-hop router. 16 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_BGP_IPV6_NEXT_HOP
+value|63
+end_define
+
+begin_comment
+comment|/* Next-hop router in the BGP domain. 16 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_IPV6_OPTION_HEADERS
+value|64
+end_define
+
+begin_comment
+comment|/* Bit-encoded field identifying IPv6 option headers found in the flow */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_1
+value|70
+end_define
+
+begin_comment
+comment|/* MPLS label at position 1 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_2
+value|71
+end_define
+
+begin_comment
+comment|/* MPLS label at position 2 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_3
+value|72
+end_define
+
+begin_comment
+comment|/* MPLS label at position 3 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_4
+value|73
+end_define
+
+begin_comment
+comment|/* MPLS label at position 4 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_5
+value|74
+end_define
+
+begin_comment
+comment|/* MPLS label at position 5 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_6
+value|75
+end_define
+
+begin_comment
+comment|/* MPLS label at position 6 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_7
+value|76
+end_define
+
+begin_comment
+comment|/* MPLS label at position 7 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_8
+value|77
+end_define
+
+begin_comment
+comment|/* MPLS label at position 8 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_9
+value|78
+end_define
+
+begin_comment
+comment|/* MPLS label at position 9 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_FIELD_MPLS_LABEL_10
+value|79
+end_define
+
+begin_comment
+comment|/* MPLS label at position 10 in the stack. 3 */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NETFLOW_V9_MAX_RESERVED_FLOWSET
+value|0xFF
+end_define
+
+begin_comment
+comment|/* Clause 5.2 */
+end_comment
 
 end_unit
 
