9309fc6808066c7685adeeeb47c039592872ec9c
Simon L. Nielsen <simon@FreeBSD.org>
2012-08-06 21:33:11 +0000

    Fix named(8) DNSSEC validation Denial of Service.
    Security:	FreeBSD-SA-12:05.bind
    Security:	CVE-2012-3817
    Obtained from:	ISC
    Approved by:	so (simon)
    
    Former-commit-id: 1d0e66c520e6484f77ffae86b5f836d71c14f194

--- contrib/bind9/lib/dns/resolver.c
+++ contrib/bind9/lib/dns/resolver.c
@@ -32987,6 +32987,13 @@ name|hashval
 operator|=
 name|hashval
 expr_stmt|;
+name|bad
+operator|->
+name|expire
+operator|=
+operator|*
+name|expire
+expr_stmt|;
 name|isc_buffer_init
 argument_list|(
 operator|&
@@ -33100,6 +33107,7 @@ name|ISC_FALSE
 argument_list|)
 expr_stmt|;
 block|}
+else|else
 name|bad
 operator|->
 name|expire
