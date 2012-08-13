123f425ec4c4216067f6766cccc833260554877a
Ed Maste <emaste@FreeBSD.org>
2012-08-13 16:03:30 +0000

    Use array notation for consistency.
    Former-commit-id: 4fa02f5d4c91b9a7a578b875484c79c49623f127

--- sys/dev/re/if_re.c
+++ sys/dev/re/if_re.c
@@ -10536,7 +10536,10 @@ name|ifp
 argument_list|)
 operator|->
 name|rx_rings
-operator|->
+index|[
+literal|0
+index|]
+operator|.
 name|nr_kflags
 operator||=
 name|NKR_PENDINTR
@@ -10550,7 +10553,10 @@ name|ifp
 argument_list|)
 operator|->
 name|rx_rings
-operator|->
+index|[
+literal|0
+index|]
+operator|.
 name|si
 argument_list|,
 name|PI_NET
