b4f9097a3f06c8786b9f74fe8caa6e34e1e3cd6f
Randall R Stewart <rrs@FreeBSD.org>
2012-08-17 05:51:46 +0000

    Ok jhb, lets move the ifa_free() down to the bottom to assure that *all* tables and such are removed before we start to free. This won't protect the Hash in ip_input.c but in theory should protect any other uses that *do* use locks.
    MFC after:	1 week (or more)
    
    
    Former-commit-id: f648890e3799a8c01ded69e8d099f1969dc9780a

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -2545,8 +2545,6 @@ argument_list|(
 name|ifp
 argument_list|)
 expr_stmt|;
-comment|/*	ifa_free(&ia->ia_ifa);	- Double free?? */
-comment|/* if_addrhead */
 name|IN_IFADDR_WLOCK
 argument_list|()
 expr_stmt|;
@@ -2643,6 +2641,15 @@ operator|->
 name|ia_ifa
 argument_list|)
 expr_stmt|;
+name|ifa_free
+argument_list|(
+operator|&
+name|ia
+operator|->
+name|ia_ifa
+argument_list|)
+expr_stmt|;
+comment|/* if_addrhead */
 name|ifa_free
 argument_list|(
 operator|&
