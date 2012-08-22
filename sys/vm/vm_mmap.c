9de4f7d6234584c0f54c978ba5c786ba1d80c2e3
Konstantin Belousov <kib@FreeBSD.org>
2012-08-22 20:34:23 +0000

    MFC r239247: Adjust the r205536, by allowing a non-zero offset for anonymous mappings for a.out binaries. Apparently, a.out ld.so from FreeBSD 1.1.5.1 can issue such requests.
    Former-commit-id: 0d45f0f46e198180ebbca7b44f958fea4ac6574c

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -687,7 +687,16 @@ name|fp
 operator|=
 name|NULL
 expr_stmt|;
-comment|/* make sure mapping fits into numeric range etc */
+comment|/* 	 * Enforce the constraints. 	 * Mapping of length 0 is only allowed for old binaries. 	 * Anonymous mapping shall specify -1 as filedescriptor and 	 * zero position for new code. Be nice to ancient a.out 	 * binaries and correct pos for anonymous mapping, since old 	 * ld.so sometimes issues anonymous map requests with non-zero 	 * pos. 	 */
+if|if
+condition|(
+operator|!
+name|SV_CURPROC_FLAG
+argument_list|(
+name|SV_AOUT
+argument_list|)
+condition|)
+block|{
 if|if
 condition|(
 operator|(
@@ -696,12 +705,6 @@ operator|->
 name|len
 operator|==
 literal|0
-operator|&&
-operator|!
-name|SV_CURPROC_FLAG
-argument_list|(
-name|SV_AOUT
-argument_list|)
 operator|&&
 name|curproc
 operator|->
@@ -716,6 +719,8 @@ name|flags
 operator|&
 name|MAP_ANON
 operator|)
+operator|!=
+literal|0
 operator|&&
 operator|(
 name|uap
@@ -736,6 +741,24 @@ operator|(
 name|EINVAL
 operator|)
 return|;
+block|}
+else|else
+block|{
+if|if
+condition|(
+operator|(
+name|flags
+operator|&
+name|MAP_ANON
+operator|)
+operator|!=
+literal|0
+condition|)
+name|pos
+operator|=
+literal|0
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|flags
