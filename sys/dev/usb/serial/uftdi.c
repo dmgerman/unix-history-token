1f7fdd60f4920b88a27572428206f4d617a6fb31
Hans Petter Selasky <hselasky@FreeBSD.org>
2012-08-22 18:30:13 +0000

    Add new USB device ID.
    Submitted by:	Dmitry Luhtionov
    MFC after:	1 week
    
    
    Former-commit-id: e50478c5e0df86fc4953f22c8973660bf090877c

--- sys/dev/usb/serial/uftdi.c
+++ sys/dev/usb/serial/uftdi.c
@@ -3013,6 +3013,15 @@ name|UFTDI_DEV
 argument_list|(
 name|FTDI
 argument_list|,
+name|SERIAL_232RL
+argument_list|,
+name|UFTDI_TYPE_AUTO
+argument_list|)
+block|,
+name|UFTDI_DEV
+argument_list|(
+name|FTDI
+argument_list|,
 name|SERIAL_4232H
 argument_list|,
 name|UFTDI_TYPE_8U232AM
