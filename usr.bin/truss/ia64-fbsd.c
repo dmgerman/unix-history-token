81b80e11caf51c4a11727f53b001f676fb49cede
Andrey Zonov <zont@FreeBSD.org>
2012-08-23 12:20:29 +0000

    - Put arguments for print_syscall_ret() function in proper order.   Bug was introduced in r192025.
    Approved by:	kib (mentor)
    
    
    Former-commit-id: e995911cc7e64da90968289a1d47ab326bec501c

--- usr.bin/truss/ia64-fbsd.c
+++ usr.bin/truss/ia64-fbsd.c
@@ -1264,11 +1264,11 @@ name|s_args
 argument_list|,
 name|errorp
 argument_list|,
+name|retval
+argument_list|,
 name|fsc
 operator|.
 name|sc
-argument_list|,
-name|retval
 argument_list|)
 expr_stmt|;
 name|clear_fsc
