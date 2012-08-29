a4e764b53a997a68c41bab2533b618a5abfcb73e
John Baldwin <jhb@FreeBSD.org>
2012-08-29 20:49:36 +0000

    MFC 238310: Partially revert r217515 so that the mem_range_softc variable is always present on x86 kernels.  This fixes the build of kernels that include 'device acpi' but do not include 'device mem'.
    Former-commit-id: c3685a582a247d356ebdb49ce002ade7d9d11a30

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -212,6 +212,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/memrange.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/msgbuf.h>
 end_include
 
@@ -911,6 +917,13 @@ begin_decl_stmt
 name|struct
 name|mtx
 name|icu_lock
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+name|struct
+name|mem_range_softc
+name|mem_range_softc
 decl_stmt|;
 end_decl_stmt
 
