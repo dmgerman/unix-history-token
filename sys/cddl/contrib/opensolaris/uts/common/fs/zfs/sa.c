c9cf1330203e16b5f98ac5711a3c60d45e96f032
Andriy Gapon <avg@FreeBSD.org>
2012-09-11 07:07:52 +0000

    zfs: fix sa_modify_attrs handling of variable-sized attributes
    - skip length_idx index for a replaced variable-sized attribute
    - skip length_idx index for a removed variable-sized attribute
    - also re-arranged code to make sure that length_idx is always
      incremented for variable-sized attributes
    - additionally add an assertion that the number of actually produced
      attributes is the same as the expected number of resulting
      attributes
    
    In cooperation with:	Matthew Ahrens <mahrens@delphix.com>
    Tested by:	Trent Nelson <trent@snakebite.org>
    Reviewed by:	Matthew Ahrens <mahrens@delphix.com> (for upstream)
    To do:		get this upstreamed
    MFC after:	2 weeks
    
    
    Former-commit-id: d6c1317eae3754f160fe5041bde468a366cf79b2

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sa.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sa.c
@@ -8191,7 +8191,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * add/remove/replace a single attribute and then rewrite the entire set  * of attributes.  */
+comment|/*  * Add/remove a single attribute or replace a variable-sized attribute value  * with a value of a different size, and then rewrite the entire set  * of attributes.  * Same-length attribute value replacement (including fixed-length attributes)  * is handled more efficiently by the upper layers.  */
 end_comment
 
 begin_function
@@ -8634,18 +8634,26 @@ operator|==
 name|newattr
 condition|)
 block|{
+comment|/* duplicate attributes are not allowed */
+name|ASSERT
+argument_list|(
+name|action
+operator|==
+name|SA_REPLACE
+operator|||
+name|action
+operator|==
+name|SA_REMOVE
+argument_list|)
+expr_stmt|;
+comment|/* must be variable-sized to be replaced here */
 if|if
 condition|(
 name|action
 operator|==
-name|SA_REMOVE
+name|SA_REPLACE
 condition|)
 block|{
-name|j
-operator|++
-expr_stmt|;
-continue|continue;
-block|}
 name|ASSERT
 argument_list|(
 name|SA_REGISTERED_LEN
@@ -8656,13 +8664,6 @@ name|attr
 argument_list|)
 operator|==
 literal|0
-argument_list|)
-expr_stmt|;
-name|ASSERT
-argument_list|(
-name|action
-operator|==
-name|SA_REPLACE
 argument_list|)
 expr_stmt|;
 name|SA_ADD_BULK_ATTR
@@ -8680,6 +8681,7 @@ argument_list|,
 name|buflen
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 else|else
 block|{
@@ -8706,7 +8708,6 @@ operator|->
 name|sa_lengths
 index|[
 name|length_idx
-operator|++
 index|]
 expr_stmt|;
 block|}
@@ -8748,6 +8749,20 @@ name|length
 argument_list|)
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|SA_REGISTERED_LEN
+argument_list|(
+name|sa
+argument_list|,
+name|attr
+argument_list|)
+operator|==
+literal|0
+condition|)
+name|length_idx
+operator|++
+expr_stmt|;
 block|}
 if|if
 condition|(
@@ -8832,6 +8847,15 @@ name|buflen
 argument_list|)
 expr_stmt|;
 block|}
+name|ASSERT3U
+argument_list|(
+name|j
+argument_list|,
+operator|==
+argument_list|,
+name|attr_count
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|sa_build_layouts
