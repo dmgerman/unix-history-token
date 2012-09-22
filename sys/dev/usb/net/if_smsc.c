3fb7d8b479f9d0f183b349e6002eeb73b374c26a
Hans Petter Selasky <hselasky@FreeBSD.org>
2012-09-22 08:02:42 +0000

    Apply some more casting.
    Former-commit-id: 0a4756b5702ff3db1cdfd4a0dd6f9ff03828fb55

--- sys/dev/usb/net/if_smsc.c
+++ sys/dev/usb/net/if_smsc.c
@@ -1198,6 +1198,7 @@ block|{
 name|usb_ticks_t
 name|start_ticks
 decl_stmt|;
+specifier|const
 name|usb_ticks_t
 name|max_ticks
 init|=
@@ -1378,9 +1379,14 @@ block|}
 do|while
 condition|(
 operator|(
+call|(
+name|usb_ticks_t
+call|)
+argument_list|(
 name|ticks
 operator|-
 name|start_ticks
+argument_list|)
 operator|)
 operator|<
 name|max_ticks
@@ -4470,6 +4476,7 @@ decl_stmt|;
 name|usb_ticks_t
 name|start_ticks
 decl_stmt|;
+specifier|const
 name|usb_ticks_t
 name|max_ticks
 init|=
@@ -4561,9 +4568,14 @@ do|;
 if|if
 condition|(
 operator|(
+call|(
+name|usb_ticks_t
+call|)
+argument_list|(
 name|ticks
 operator|-
 name|start_ticks
+argument_list|)
 operator|)
 operator|>=
 name|max_ticks
