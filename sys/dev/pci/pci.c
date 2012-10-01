c658bac28aabacd868643a9a156a2d93eb374527
Gavin Atkinson <gavin@FreeBSD.org>
2012-10-01 15:47:13 +0000

    Merge the following from head:
      r240694
        Add PCI subclass for NVM Express devices.
    
      r240699, r240739
        Recognise NVM devices and pretty-print their name.
    
    
    Former-commit-id: 568fbde849bc1d38bcede6a45b6b0791cc31b119

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -16975,6 +16975,14 @@ literal|"SAS"
 block|}
 block|,
 block|{
+name|PCIC_STORAGE
+block|,
+name|PCIS_STORAGE_NVM
+block|,
+literal|"NVM"
+block|}
+block|,
+block|{
 name|PCIC_NETWORK
 block|,
 operator|-
