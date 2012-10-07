e7f936cdce22f5c41b354b072e02de3172f66d75
Warner Losh <imp@FreeBSD.org>
2012-10-07 02:08:19 +0000

    Improve a few comments.
    Former-commit-id: d17ef0c652585900703f5276c3a771e08dd4b553

--- sys/arm/at91/at91_rtc.c
+++ sys/arm/at91/at91_rtc.c
@@ -4,7 +4,7 @@ comment|/*-  * Copyright (c) 2006 M. Warner Losh.  All rights reserved.  * Copyr
 end_comment
 
 begin_comment
-comment|/*  * Driver for the at91 on-chip realtime clock.  *  * This driver does not currently support alarms, just date and time.  *  * Note that on an rm9200 the RTC is not your typical battery-driven clock that  * keeps time while the system is powered down.  In fact, it doesn't even  * survive a chip reset to keep time across a reboot.  About the only thing it  * might be good for is keeping time while the cpu clock is turned off for power  * savings.  On later chips, a battery backup feature is available.  */
+comment|/*  * Driver for the at91 on-chip realtime clock.  *  * This driver does not currently support alarms, just date and time.  *  * The RTC on the AT91RM9200 resets when the core rests, so it is useless as a  * source of time (except when the CPU clock is powered down to save power,  * which we don't currently do).  On AT91SAM9 chips, the RTC survives chip  * reset, and there's provisions for it to keep time via battery backup if the  * system loses power.  On those systems, we use it as a RTC.  We tell the two  * apart because the century field is 19 on AT91RM9200 on reset, or on AT91SAM9  * chips that haven't had their time properly set.  */
 end_comment
 
 begin_include
@@ -628,6 +628,10 @@ return|;
 block|}
 end_function
 
+begin_comment
+comment|/*  * Cannot support detach, since there's no clock_unregister function.  */
+end_comment
+
 begin_function
 specifier|static
 name|int
@@ -642,7 +646,6 @@ operator|(
 name|EBUSY
 operator|)
 return|;
-comment|/* XXX */
 block|}
 end_function
 
