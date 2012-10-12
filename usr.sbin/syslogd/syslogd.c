b685e09e0c07be63cc00bf637c8872f1152abf74
Eitan Adler <eadler@FreeBSD.org>
2012-10-12 02:10:26 +0000

    MFC r240389,r240409: - 	Add support for ipv6 addresses as destination - 	Add documentation for IPv6 support
    PR:		docs/171580
    Approved by:	cperciva (implicit)
    
    Former-commit-id: f5fea4f90438ffe367a984e0396b619148c87b2b

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -10371,6 +10371,11 @@ name|char
 modifier|*
 name|tp
 decl_stmt|;
+name|char
+name|endkey
+init|=
+literal|':'
+decl_stmt|;
 comment|/* 			 * scan forward to see if there is a port defined. 			 * so we can't use strlcpy.. 			 */
 name|i
 operator|=
@@ -10398,6 +10403,23 @@ expr_stmt|;
 name|p
 operator|++
 expr_stmt|;
+comment|/* 			 * an ipv6 address should start with a '[' in that case 			 * we should scan for a ']' 			 */
+if|if
+condition|(
+operator|*
+name|p
+operator|==
+literal|'['
+condition|)
+block|{
+name|p
+operator|++
+expr_stmt|;
+name|endkey
+operator|=
+literal|']'
+expr_stmt|;
+block|}
 while|while
 condition|(
 operator|*
@@ -10407,7 +10429,7 @@ operator|(
 operator|*
 name|p
 operator|!=
-literal|':'
+name|endkey
 operator|)
 operator|&&
 operator|(
@@ -10427,6 +10449,20 @@ name|p
 operator|++
 expr_stmt|;
 block|}
+if|if
+condition|(
+name|endkey
+operator|==
+literal|']'
+operator|&&
+operator|*
+name|p
+operator|==
+name|endkey
+condition|)
+name|p
+operator|++
+expr_stmt|;
 operator|*
 name|tp
 operator|=
