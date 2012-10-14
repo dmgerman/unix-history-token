94ccaef8c84e33c66c11faaeb1c9705e0c4f6277
Andriy Gapon <avg@FreeBSD.org>
2012-10-14 07:55:14 +0000

    MFC r241302: kvm_proclist: ignore processes in larvae state
    Former-commit-id: 1505866e858c88f04f6d9f8308c2bd4b1dcd3a01

--- lib/libkvm/kvm_proc.c
+++ lib/libkvm/kvm_proc.c
@@ -487,6 +487,15 @@ condition|(
 name|proc
 operator|.
 name|p_state
+operator|==
+name|PRS_NEW
+condition|)
+continue|continue;
+if|if
+condition|(
+name|proc
+operator|.
+name|p_state
 operator|!=
 name|PRS_ZOMBIE
 condition|)
