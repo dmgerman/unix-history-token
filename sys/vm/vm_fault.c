cf4bc156890ed153d8d9ec4d1338f928d515886d
Konstantin Belousov <kib@FreeBSD.org>
2012-10-24 18:32:37 +0000

    Commit the actual text provided by Alan, instead of the wrong update in r242011.
    MFC after:	1 week
    
    
    Former-commit-id: 55d75ffd9c51164d5104349766f79663c62c9fc8

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -4728,7 +4728,7 @@ operator|&=
 operator|~
 name|VM_PROT_WRITE
 expr_stmt|;
-comment|/* 	 * Loop through all of the pages in the entry's range, copying 	 * each one from the source object (it should be there) to the 	 * destination object.  Note that copied pages are not wired 	 * and marked dirty to prevent reclamation without saving the 	 * content into the swap file on pageout. 	 */
+comment|/* 	 * Loop through all of the virtual pages within the entry's 	 * range, copying each page from the source object to the 	 * destination object.  Since the source is wired, those pages 	 * must exist.  In contrast, the destination is pageable. 	 * Since the destination object does share any backing storage 	 * with the source object, all of its pages must be dirtied, 	 * regardless of whether they can be written. 	 */
 for|for
 control|(
 name|vaddr
