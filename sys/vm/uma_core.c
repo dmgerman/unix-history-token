d47be54562209cb39b1a4e7ca27e17ed161f560e
Eitan Adler <eadler@FreeBSD.org>
2012-10-25 03:29:09 +0000

    MFC r241825: 	Print flags as hex instead of an integer.
    PR:		kern/168210
    Approved by:	cperciva (implicit)
    
    Former-commit-id: 5f5098017f2d019e48a4095b17c82cc8b70a1706

--- sys/vm/uma_core.c
+++ sys/vm/uma_core.c
@@ -5651,7 +5651,7 @@ directive|ifdef
 name|UMA_DEBUG
 name|printf
 argument_list|(
-literal|"UMA: %s(%p) size %d(%d) flags %d ipers %d ppera %d out %d free %d\n"
+literal|"UMA: %s(%p) size %d(%d) flags %#x ipers %d ppera %d out %d free %d\n"
 argument_list|,
 name|zone
 operator|->
@@ -12610,7 +12610,7 @@ name|slab
 decl_stmt|;
 name|printf
 argument_list|(
-literal|"keg: %s(%p) size %d(%d) flags %d ipers %d ppera %d "
+literal|"keg: %s(%p) size %d(%d) flags %#x ipers %d ppera %d "
 literal|"out %d free %d limit %d\n"
 argument_list|,
 name|keg
@@ -12748,7 +12748,7 @@ name|i
 decl_stmt|;
 name|printf
 argument_list|(
-literal|"zone: %s(%p) size %d flags %d\n"
+literal|"zone: %s(%p) size %d flags %#x\n"
 argument_list|,
 name|zone
 operator|->
