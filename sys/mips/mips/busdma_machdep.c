2f132f9c5bb845f5d7525bf92a7fdacb5212b407
Adrian Chadd <adrian@FreeBSD.org>
2012-11-02 05:22:32 +0000

    Free the dma map -after- it's checked, not before. Or you'll be potentially referencing already-freed memory.
    Former-commit-id: 6c9da042983ac9ee0eafed2267e57fa4596b161f

--- sys/mips/mips/busdma_machdep.c
+++ sys/mips/mips/busdma_machdep.c
@@ -2448,11 +2448,6 @@ name|bus_dmamap_t
 name|map
 parameter_list|)
 block|{
-name|_busdma_free_dmamap
-argument_list|(
-name|map
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|STAILQ_FIRST
@@ -2502,6 +2497,11 @@ name|dmat
 operator|->
 name|map_count
 operator|--
+expr_stmt|;
+name|_busdma_free_dmamap
+argument_list|(
+name|map
+argument_list|)
 expr_stmt|;
 name|CTR2
 argument_list|(
