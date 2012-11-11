6d5b97b28a9e78c3884bf3df67201e891012f0d5
Attilio Rao <attilio@FreeBSD.org>
2012-11-11 23:25:47 +0000

    Tweak comments.
    In collabouration with:	alc
    
    
    Former-commit-id: 92f410f4f21ed749cf4d7c9b428dd9a952dee48a

--- sys/sys/_rwlock.h
+++ sys/sys/_rwlock.h
@@ -22,7 +22,7 @@ file|<machine/param.h>
 end_include
 
 begin_comment
-comment|/*  * Reader/writer lock.  *  * The layout of the first 2 members of struct rwlock* is considered fixed.  * More specifically, it is assumed that there is a member called rw_lock  * for every struct rwlock* and that other locking primitive structures are  * not allowed to use such name for their members.  * If this needs to change, the bits in the rwlock implementation might be  * modified appropriately.  */
+comment|/*  * Reader/writer lock.  *  * All reader/writer lock implementations must always have a member  * called rw_lock.  Other locking primitive structures are not allowed to  * use this name for their members.  * If this rule needs to change, the bits in the reader/writer lock  * implementation must be modified appropriately.  */
 end_comment
 
 begin_struct
@@ -42,7 +42,7 @@ struct|;
 end_struct
 
 begin_comment
-comment|/*  * Members of struct rwlock_padalign must mirror members of struct rwlock.  * rwlock_padalign rwlocks can use rwlock(9) KPI transparently, without  * modifies.  * When using pad-aligned rwlocks within structures, they should generally  * stay as the first member of the struct.  This is because otherwise the  * compiler can generate ever more padding for the struct to keep a correct  * alignment for the rwlock.  */
+comment|/*  * Members of struct rwlock_padalign must mirror members of struct rwlock.  * rwlock_padalign rwlocks can use the rwlock(9) API transparently without  * modification.  * Pad-aligned rwlocks used within structures should generally be the  * first member of the struct.  Otherwise, the compiler can generate  * additional padding for the struct to keep a correct alignment for  * the rwlock.  */
 end_comment
 
 begin_struct
