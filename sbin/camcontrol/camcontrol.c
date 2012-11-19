3da94d645c0c015d5eed0609ff2fbf77c280ae17
Ed Maste <emaste@FreeBSD.org>
2012-11-19 18:20:27 +0000

    MFC (part of) r241737 (ed):
      More -Wmissing-variable-declarations fixes.
    
      In addition to adding `static' where possible:
    
      ...
      - sbin/camcontrol: Move `verbose' into camcontrol.h and fix shadow warnings.
      ...
    
    
    Former-commit-id: b7ad1241e84f78883375c4f1be2f3de7ba66a478

--- sbin/camcontrol/camcontrol.c
+++ sbin/camcontrol/camcontrol.c
@@ -27932,12 +27932,12 @@ name|void
 name|usage
 parameter_list|(
 name|int
-name|verbose
+name|printlong
 parameter_list|)
 block|{
 name|fprintf
 argument_list|(
-name|verbose
+name|printlong
 condition|?
 name|stdout
 else|:
@@ -28004,7 +28004,7 @@ expr_stmt|;
 if|if
 condition|(
 operator|!
-name|verbose
+name|printlong
 condition|)
 return|return;
 ifndef|#
