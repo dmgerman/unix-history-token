ee5d000b4736f524e0be932b38ca41524d1e12ee
Andriy Gapon <avg@FreeBSD.org>
2012-11-24 12:34:44 +0000

    MFC r242862: zfs_ioc_destroy_snaps_nvl: remove disk device entries for zvol snapshots
    Former-commit-id: 045a95c3cc92139300618df96f9293b4e1662b3f

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
@@ -14047,6 +14047,14 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
+operator|(
+name|void
+operator|)
+name|zvol_remove_minor
+argument_list|(
+name|name
+argument_list|)
+expr_stmt|;
 block|}
 name|err
 operator|=
