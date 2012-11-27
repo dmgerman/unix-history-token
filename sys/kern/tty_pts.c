b76eb4c0df5b501b2b7893e711341bd8c8ede056
Mateusz Guzik <mjg@FreeBSD.org>
2012-11-27 01:35:29 +0000

    MFC r243234:
    Fix possible fp reference leak in posix_openpt
    
    
    Former-commit-id: f8bb43d61713f0d1d84a58f9ec5ea1d3b1d3042f

--- sys/kern/tty_pts.c
+++ sys/kern/tty_pts.c
@@ -3522,6 +3522,13 @@ argument_list|,
 name|td
 argument_list|)
 expr_stmt|;
+name|fdrop
+argument_list|(
+name|fp
+argument_list|,
+name|td
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 name|error
