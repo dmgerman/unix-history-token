d5fceb11740a80eaadd6bf97c6679c4e3a318397
Andriy Gapon <avg@FreeBSD.org>
2012-12-01 19:22:18 +0000

    MFC r243268: zfs_remove: set VV_NOSYNC flag if a node is unlinked
    Former-commit-id: e190f7e299a1ba03f7d624858f6e634e7c3636aa

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
@@ -8165,6 +8165,17 @@ argument_list|,
 name|tx
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+name|vp
+operator|->
+name|v_vflag
+operator||=
+name|VV_NOSYNC
+expr_stmt|;
+endif|#
+directive|endif
 block|}
 name|txtype
 operator|=
