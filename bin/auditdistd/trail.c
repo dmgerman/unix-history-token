42b313d424eb59fe93a6895d06e4e057fa448fb6
Robert N. M. Watson <rwatson@FreeBSD.org>
2012-12-01 00:02:31 +0000

    Merge a number of post-1.2-alpha2 changes to OpenBSM into the OpenBSM vendor area; these sort out various post-release issues, largely to do with integrating OpenBSM with the base FreeBSD build.  All of these changes will appear in a future 1.2-alpha3:
    Change 219846 on 2012/11/26 by rwatson@rwatson_cinnamon
    
            Update several instances of Apple Computer to Apple; a change made
            in the FreeBSD tree some years ago but not propagated to OpenBSM.
    
    Change 219845 on 2012/11/26 by rwatson@rwatson_cinnamon
    
            Remove Apple acknowledgement clause from file with Christian
            Peron copyright (with permission from Christian).
    
    Change 219836 on 2012/11/23 by rwatson@rwatson_cinnamon
    
            Replace further instances of <> with "" for local includes in
            auditdistd.
    
    Change 219834 on 2012/11/23 by rwatson@rwatson_cinnamon
    
            For current-directory headers, use #include "" rather than #include
            <>.
    
    Change 219832 on 2012/11/23 by rwatson@rwatson_cinnamon
    
            Be more consistent with the remainder of OpenBSM and include
            config/config.h rather than config.h.
    
            Don't include config.h from synch.h, which is included only from
            .c files that already include config.h.
    
    Change 219831 on 2012/11/23 by pjd@pjd_anger
    
            Add Xref to auditdistd(8).
    
            Suggested by:   rwatson
    
    Obtained from:	TrustedBSD Project
    Sponsored by:	The FreeBSD Foundation (auditdistd)
    
    
    Former-commit-id: b48ce0bc252165d99954cb44131f1e47cbf84380

--- bin/auditdistd/trail.c
+++ bin/auditdistd/trail.c
@@ -1,12 +1,12 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2012 The FreeBSD Foundation  * All rights reserved.  *  * This software was developed by Pawel Jakub Dawidek under sponsorship from  * the FreeBSD Foundation.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/trustedbsd/openbsm/bin/auditdistd/trail.c#1 $  */
+comment|/*-  * Copyright (c) 2012 The FreeBSD Foundation  * All rights reserved.  *  * This software was developed by Pawel Jakub Dawidek under sponsorship from  * the FreeBSD Foundation.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $P4: //depot/projects/trustedbsd/openbsm/bin/auditdistd/trail.c#3 $  */
 end_comment
 
 begin_include
 include|#
 directive|include
-file|"config.h"
+file|<config/config.h>
 end_include
 
 begin_include
@@ -163,7 +163,7 @@ end_endif
 begin_include
 include|#
 directive|include
-file|<pjdlog.h>
+file|"pjdlog.h"
 end_include
 
 begin_include
