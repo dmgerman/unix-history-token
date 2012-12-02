accf0c2b30bd553681d843b98935cac0bb19eea8
Eitan Adler <eadler@FreeBSD.org>
2012-12-02 17:54:52 +0000

    Add support for hdmi hda codec onboard nvidia gt 440 graphics card
    PR:		kern/174059
    Submitted by:	"4721@hushmail.com" <4721@hushmail.com>
    Approved by:	cperciva (implicit)
    MFC after:	2 weeks
    
    Former-commit-id: b143de9ed989bc838585cb1fe63a778edad38073

--- sys/dev/sound/pci/hda/hdacc.c
+++ sys/dev/sound/pci/hda/hdacc.c
@@ -2067,6 +2067,14 @@ literal|"NVIDIA GT240"
 block|}
 block|,
 block|{
+name|HDA_CODEC_NVIDIAGT440
+block|,
+literal|0
+block|,
+literal|"NVIDIA GT440"
+block|}
+block|,
+block|{
 name|HDA_CODEC_INTELIP
 block|,
 literal|0
