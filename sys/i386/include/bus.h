6f237e697473dd6619eba6d5ee9a8872517bd5d4
Jim Harris <jimharris@FreeBSD.org>
2012-12-06 22:33:31 +0000

    Add amd64 implementations for 8-byte bus_space routines.
    Submitted by:	Carl Delsey <carl.r.delsey@intel.com>
    Discussed with:	jhb, rwatson
    Reviewed by:	jimharris
    MFC after:	1 week
    
    
    Former-commit-id: 02ec204f70a1940cb2ab9fd65f434dbb4b0a2ff1

--- sys/i386/include/bus.h
+++ sys/i386/include/bus.h
@@ -7,11 +7,36 @@ begin_comment
 comment|/* $FreeBSD$ */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_MACHINE_BUS_H_
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_MACHINE_BUS_H_
+end_define
+
 begin_include
 include|#
 directive|include
 file|<x86/bus.h>
 end_include
+
+begin_comment
+comment|/*  * The functions:  *    bus_space_read_8  *    bus_space_read_region_8  *    bus_space_write_8  *    bus_space_write_multi_8  *    bus_space_write_region_8  *    bus_space_set_multi_8  *    bus_space_set_region_8  *    bus_space_copy_region_8  *    bus_space_read_multi_8  * are unimplemented for i386 because there is no way to do a 64-bit move in  * this architecture. It is possible to do two 32-bit moves, but this is  * not atomic and may have hardware dependencies that should be fully  * understood.  */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/*_MACHINE_BUS_H_*/
+end_comment
 
 end_unit
 
