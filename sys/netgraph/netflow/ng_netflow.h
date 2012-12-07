8a420ac511db4a5931dc51391bc638dc1743c72a
Alexander V. Chernikov <melifaro@FreeBSD.org>
2012-12-07 13:03:23 +0000

    MFC r241446,r241501
    Add NG_NETFLOW_V9INFO_TYPE command to be able to request netflowv9-specific
    data.
    
    Submitted by:	Dmitry Luhtionov <dmitryluhtionov at gmail.com>
    
    
    Former-commit-id: d321a0d69dbdf56f8a163527b791916ea7a4b4bb

--- sys/netgraph/netflow/ng_netflow.h
+++ sys/netgraph/netflow/ng_netflow.h
@@ -32,6 +32,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|NGM_NETFLOW_V9_COOKIE
+value|1349865386
+end_define
+
+begin_define
+define|#
+directive|define
 name|NG_NETFLOW_MAXIFACES
 value|USHRT_MAX
 end_define
@@ -140,6 +147,15 @@ init|=
 literal|9
 block|,
 comment|/* set outgoing interface MTU */
+name|NGM_NETFLOW_V9INFO
+init|=
+literal|10
+operator||
+name|NGM_READONLY
+operator||
+name|NGM_HASREPLY
+block|,
+comment|/* get v9 info */
 block|}
 enum|;
 end_enum
@@ -431,6 +447,30 @@ name|uint32_t
 name|nentries
 decl_stmt|;
 comment|/* number of records in response */
+block|}
+struct|;
+end_struct
+
+begin_comment
+comment|/* This structure is used in NGM_NETFLOW_V9INFO message */
+end_comment
+
+begin_struct
+struct|struct
+name|ng_netflow_v9info
+block|{
+name|uint16_t
+name|templ_packets
+decl_stmt|;
+comment|/* v9 template packets */
+name|uint16_t
+name|templ_time
+decl_stmt|;
+comment|/* v9 template time */
+name|uint16_t
+name|mtu
+decl_stmt|;
+comment|/* v9 MTU */
 block|}
 struct|;
 end_struct
@@ -946,6 +986,17 @@ value|{			\ 	{ "mtu",&ng_parse_uint16_type },	\ 	{ NULL }					\ }
 end_define
 
 begin_comment
+comment|/* Parse the v9info structure */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NG_NETFLOW_V9INFO_TYPE
+value|{			\ 	{ "v9 template packets",&ng_parse_uint16_type },\ 	{ "v9 template time",&ng_parse_uint16_type },\ 	{ "v9 MTU",&ng_parse_uint16_type },\ 	{ NULL }					\ }
+end_define
+
+begin_comment
 comment|/* Private hook data */
 end_comment
 
@@ -1152,6 +1203,7 @@ name|uint16_t
 name|maxfibs
 decl_stmt|;
 comment|/* number of allocated fibs */
+comment|/* Netflow v9 configuration options */
 comment|/* 	 * RFC 3954 clause 7.3 	 * "Both options MUST be configurable by the user on the Exporter." 	 */
 name|uint16_t
 name|templ_time
@@ -1361,6 +1413,19 @@ name|priv_p
 parameter_list|,
 name|struct
 name|ng_netflow_info
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|ng_netflow_copyv9info
+parameter_list|(
+name|priv_p
+parameter_list|,
+name|struct
+name|ng_netflow_v9info
 modifier|*
 parameter_list|)
 function_decl|;
