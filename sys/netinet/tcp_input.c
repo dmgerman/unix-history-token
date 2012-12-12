f2715b52c28628328245eacc80fd9b4f1c8ca987
Gleb Smirnoff <glebius@FreeBSD.org>
2012-12-12 17:41:21 +0000

    Fix a crash in tcp_input(), that happens when mbuf has a fwd_tag on it, but later after processing and freeing the tag, we need to jump back again to the findpcb label. Since the fwd_tag pointer wasn't NULL we tried to process and free the tag for second time.
    Reported & tested by:	Pawel Tyll <ptyll nitronet.pl>
    MFC after:		3 days
    
    
    Former-commit-id: f6c5031d598dc927f54ccb1023342309b5574b50

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -4069,6 +4069,10 @@ operator|&=
 operator|~
 name|M_IP_NEXTHOP
 expr_stmt|;
+name|fwd_tag
+operator|=
+name|NULL
+expr_stmt|;
 block|}
 elseif|else
 if|if
@@ -4262,6 +4266,10 @@ name|m_flags
 operator|&=
 operator|~
 name|M_IP_NEXTHOP
+expr_stmt|;
+name|fwd_tag
+operator|=
+name|NULL
 expr_stmt|;
 block|}
 else|else
