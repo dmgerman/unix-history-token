fd874aff9ae17a9592423cd61a2b9bb556d45e91
Robert N. M. Watson <rwatson@FreeBSD.org>
2012-12-15 14:59:00 +0000

    Merge OpenBSM 1.2-alpha3 from the vendor branch to 10-CURRENT; this version included various upstreamed patches from the FreeBSD base to make OpenBSM compile more easily with bmake, higher warning levels, clang, and several other loose ends.
    Obtained from:	TrustedBSD Project
    
    
    Former-commit-id: 03bd3c77506d952e610e5c6299335c971c2240af

--- contrib/openbsm/sys/bsm/audit_record.h
+++ contrib/openbsm/sys/bsm/audit_record.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2005-2009 Apple Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $P4: //depot/projects/trustedbsd/openbsm/sys/bsm/audit_record.h#10 $  */
+comment|/*-  * Copyright (c) 2005-2009 Apple Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1.  Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  * 2.  Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  * 3.  Neither the name of Apple Inc. ("Apple") nor the names of  *     its contributors may be used to endorse or promote products derived  *     from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $P4: //depot/projects/trustedbsd/openbsm/sys/bsm/audit_record.h#12 $  */
 end_comment
 
 begin_ifndef
@@ -1449,6 +1449,22 @@ end_function_decl
 begin_function_decl
 name|token_t
 modifier|*
+name|au_to_privset
+parameter_list|(
+name|char
+modifier|*
+name|privtypestr
+parameter_list|,
+name|char
+modifier|*
+name|privstr
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|token_t
+modifier|*
 name|au_to_process
 parameter_list|(
 name|au_id_t
@@ -2084,6 +2100,21 @@ name|au_to_trailer
 parameter_list|(
 name|int
 name|rec_size
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|token_t
+modifier|*
+name|au_to_upriv
+parameter_list|(
+name|char
+name|sorf
+parameter_list|,
+name|char
+modifier|*
+name|priv
 parameter_list|)
 function_decl|;
 end_function_decl
