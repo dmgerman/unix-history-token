552f834eab635b44c8dfd1f905373487c277d107
Eitan Adler <eadler@FreeBSD.org>
2012-12-20 18:13:04 +0000

    MFC r244037: 	Add check for failure of mkstemp and setenv.
    Approved by:	cperciva (implicit)
    
    
    Former-commit-id: 3f642316bef8af2c6de3d47b558c9fb41ee77c2e

--- usr.bin/fetch/fetch.c
+++ usr.bin/fetch/fetch.c
@@ -3180,6 +3180,8 @@ operator|!=
 name|NULL
 condition|)
 block|{
+if|if
+condition|(
 name|mkstemps
 argument_list|(
 name|tmppath
@@ -3191,7 +3193,22 @@ argument_list|)
 operator|+
 literal|1
 argument_list|)
+operator|==
+operator|-
+literal|1
+condition|)
+block|{
+name|warn
+argument_list|(
+literal|"%s: mkstemps()"
+argument_list|,
+name|path
+argument_list|)
 expr_stmt|;
+goto|goto
+name|failure
+goto|;
+block|}
 name|of
 operator|=
 name|fopen
@@ -4914,6 +4931,8 @@ name|N_filename
 operator|!=
 name|NULL
 condition|)
+if|if
+condition|(
 name|setenv
 argument_list|(
 literal|"NETRC"
@@ -4921,6 +4940,18 @@ argument_list|,
 name|N_filename
 argument_list|,
 literal|1
+argument_list|)
+operator|==
+operator|-
+literal|1
+condition|)
+name|err
+argument_list|(
+literal|1
+argument_list|,
+literal|"setenv: cannot set NETRC=%s"
+argument_list|,
+name|N_filename
 argument_list|)
 expr_stmt|;
 while|while
