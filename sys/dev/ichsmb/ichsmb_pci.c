3d945fa103190e1e0faedd571218e58c987c6c47
Jack F Vogel <jfv@FreeBSD.org>
2013-01-02 22:01:26 +0000

    Add Intel Lynx Point PCH SMBus Device IDs
    Former-commit-id: 606150fed316707eed730f64ae93fbc98ce74cbe

--- sys/dev/ichsmb/ichsmb_pci.c
+++ sys/dev/ichsmb/ichsmb_pci.c
@@ -256,6 +256,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|ID_LPT
+value|0x8c228086
+end_define
+
+begin_define
+define|#
+directive|define
 name|PCIS_SERIALBUS_SMBUS_PROGIF
 value|0x00
 end_define
@@ -675,6 +682,17 @@ argument_list|(
 name|dev
 argument_list|,
 literal|"Intel Panther Point SMBus controller"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|ID_LPT
+case|:
+name|device_set_desc
+argument_list|(
+name|dev
+argument_list|,
+literal|"Intel Lynx Point SMBus controller"
 argument_list|)
 expr_stmt|;
 break|break;
