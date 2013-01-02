d3e2b0325955849536c0c7e3d1b3f068a7cbab14
Jack F Vogel <jfv@FreeBSD.org>
2013-01-02 21:56:58 +0000

    Add Intel Lynx Point PCH HD Audio Device IDs
    Former-commit-id: 8fa5d5f743f1314d152da5ce6ed8d1351c422075

--- sys/dev/sound/pci/hda/hdac.h
+++ sys/dev/sound/pci/hda/hdac.h
@@ -147,6 +147,20 @@ end_define
 begin_define
 define|#
 directive|define
+name|HDA_INTEL_LPT1
+value|HDA_MODEL_CONSTRUCT(INTEL, 0x8c20)
+end_define
+
+begin_define
+define|#
+directive|define
+name|HDA_INTEL_LPT2
+value|HDA_MODEL_CONSTRUCT(INTEL, 0x8c21)
+end_define
+
+begin_define
+define|#
+directive|define
 name|HDA_INTEL_ALL
 value|HDA_MODEL_CONSTRUCT(INTEL, 0xffff)
 end_define
