e2fd964be59432f8e6015a8ca9528310effc929e
Warner Losh <imp@FreeBSD.org>
2013-01-11 21:19:45 +0000

    style(9) changes before I do more real changes.
    Former-commit-id: 7c22dce449048b15a82b8d849a87805e5857ad5d

--- sys/dev/atkbdc/atkbd.c
+++ sys/dev/atkbdc/atkbd.c
@@ -5996,10 +5996,10 @@ expr_stmt|;
 if|#
 directive|if
 literal|0
-block|c = get_controller_command_byte(kbdc);     if ((c == -1)  	|| !set_controller_command_byte(kbdc,              kbdc_get_device_mask(kbdc),             KBD_DISABLE_KBD_PORT | KBD_DISABLE_KBD_INT                 | KBD_DISABLE_AUX_PORT | KBD_DISABLE_AUX_INT)) {
+block|c = get_controller_command_byte(kbdc); 	if ((c == -1)  	    || !set_controller_command_byte(kbdc,  		kbdc_get_device_mask(kbdc), 		KBD_DISABLE_KBD_PORT | KBD_DISABLE_KBD_INT 		| KBD_DISABLE_AUX_PORT | KBD_DISABLE_AUX_INT)) {
 comment|/* CONTROLLER ERROR */
-block|kbdc_lock(kbdc, FALSE); 	splx(s); 	return EIO;     }
-comment|/*       * Now that the keyboard controller is told not to generate       * the keyboard and mouse interrupts, call `splx()' to allow       * the other tty interrupts. The clock interrupt may also occur,       * but the timeout routine (`scrn_timer()') will be blocked       * by the lock flag set via `kbdc_lock()'      */
+block|kbdc_lock(kbdc, FALSE); 		splx(s); 		return EIO; 	}
+comment|/*  	 * Now that the keyboard controller is told not to generate  	 * the keyboard and mouse interrupts, call `splx()' to allow  	 * the other tty interrupts. The clock interrupt may also occur,  	 * but the timeout routine (`scrn_timer()') will be blocked  	 * by the lock flag set via `kbdc_lock()' 	 */
 block|splx(s);
 endif|#
 directive|endif
@@ -6027,7 +6027,7 @@ if|#
 directive|if
 literal|0
 comment|/* restore the interrupts */
-block|if (!set_controller_command_byte(kbdc,             kbdc_get_device_mask(kbdc), 	    c& (KBD_KBD_CONTROL_BITS | KBD_AUX_CONTROL_BITS))) {
+block|if (!set_controller_command_byte(kbdc, kbdc_get_device_mask(kbdc), 	    c& (KBD_KBD_CONTROL_BITS | KBD_AUX_CONTROL_BITS))) {
 comment|/* CONTROLLER ERROR */
 block|}
 else|#
