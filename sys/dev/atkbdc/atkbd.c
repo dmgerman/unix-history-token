f2a932f696d5708483b7af822b45718cb21c011e
Warner Losh <imp@FreeBSD.org>
2013-01-28 23:16:47 +0000

    MFC: r245314 and r245315:      r245315 | imp | 2013-01-11 14:42:23 -0700 (Fri, 11 Jan 2013) | 4 lines
         Pass the device_t into atkbd_{probe,attach}_unit and get the
         controller unit and keyboard unit from there. It will be needed
         for other things in the future as well...
    
         r245314 | imp | 2013-01-11 14:19:45 -0700 (Fri, 11 Jan 2013) | 2 lines
    
         style(9) changes before I do more real changes.
    
    
    Former-commit-id: 3298a51881cfda0f9f5c3c6e912dae96321a9361

--- sys/dev/atkbdc/atkbd.c
+++ sys/dev/atkbdc/atkbd.c
@@ -213,11 +213,8 @@ begin_function
 name|int
 name|atkbd_probe_unit
 parameter_list|(
-name|int
-name|unit
-parameter_list|,
-name|int
-name|ctlr
+name|device_t
+name|dev
 parameter_list|,
 name|int
 name|irq
@@ -260,7 +257,13 @@ index|[
 literal|0
 index|]
 operator|=
-name|ctlr
+name|device_get_unit
+argument_list|(
+name|device_get_parent
+argument_list|(
+name|dev
+argument_list|)
+argument_list|)
 expr_stmt|;
 name|args
 index|[
@@ -278,7 +281,10 @@ operator|->
 name|probe
 call|)
 argument_list|(
-name|unit
+name|device_get_unit
+argument_list|(
+name|dev
+argument_list|)
 argument_list|,
 name|args
 argument_list|,
@@ -302,16 +308,13 @@ begin_function
 name|int
 name|atkbd_attach_unit
 parameter_list|(
-name|int
-name|unit
+name|device_t
+name|dev
 parameter_list|,
 name|keyboard_t
 modifier|*
 modifier|*
 name|kbd
-parameter_list|,
-name|int
-name|ctlr
 parameter_list|,
 name|int
 name|irq
@@ -333,6 +336,9 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|int
+name|unit
+decl_stmt|;
 name|sw
 operator|=
 name|kbd_get_switch
@@ -350,12 +356,25 @@ return|return
 name|ENXIO
 return|;
 comment|/* reset, initialize and enable the device */
+name|unit
+operator|=
+name|device_get_unit
+argument_list|(
+name|dev
+argument_list|)
+expr_stmt|;
 name|args
 index|[
 literal|0
 index|]
 operator|=
-name|ctlr
+name|device_get_unit
+argument_list|(
+name|device_get_parent
+argument_list|(
+name|dev
+argument_list|)
+argument_list|)
 expr_stmt|;
 name|args
 index|[
@@ -5996,10 +6015,10 @@ expr_stmt|;
 if|#
 directive|if
 literal|0
-block|c = get_controller_command_byte(kbdc);     if ((c == -1)  	|| !set_controller_command_byte(kbdc,              kbdc_get_device_mask(kbdc),             KBD_DISABLE_KBD_PORT | KBD_DISABLE_KBD_INT                 | KBD_DISABLE_AUX_PORT | KBD_DISABLE_AUX_INT)) {
+block|c = get_controller_command_byte(kbdc); 	if ((c == -1)  	    || !set_controller_command_byte(kbdc,  		kbdc_get_device_mask(kbdc), 		KBD_DISABLE_KBD_PORT | KBD_DISABLE_KBD_INT 		| KBD_DISABLE_AUX_PORT | KBD_DISABLE_AUX_INT)) {
 comment|/* CONTROLLER ERROR */
-block|kbdc_lock(kbdc, FALSE); 	splx(s); 	return EIO;     }
-comment|/*       * Now that the keyboard controller is told not to generate       * the keyboard and mouse interrupts, call `splx()' to allow       * the other tty interrupts. The clock interrupt may also occur,       * but the timeout routine (`scrn_timer()') will be blocked       * by the lock flag set via `kbdc_lock()'      */
+block|kbdc_lock(kbdc, FALSE); 		splx(s); 		return EIO; 	}
+comment|/*  	 * Now that the keyboard controller is told not to generate  	 * the keyboard and mouse interrupts, call `splx()' to allow  	 * the other tty interrupts. The clock interrupt may also occur,  	 * but the timeout routine (`scrn_timer()') will be blocked  	 * by the lock flag set via `kbdc_lock()' 	 */
 block|splx(s);
 endif|#
 directive|endif
@@ -6027,7 +6046,7 @@ if|#
 directive|if
 literal|0
 comment|/* restore the interrupts */
-block|if (!set_controller_command_byte(kbdc,             kbdc_get_device_mask(kbdc), 	    c& (KBD_KBD_CONTROL_BITS | KBD_AUX_CONTROL_BITS))) {
+block|if (!set_controller_command_byte(kbdc, kbdc_get_device_mask(kbdc), 	    c& (KBD_KBD_CONTROL_BITS | KBD_AUX_CONTROL_BITS))) {
 comment|/* CONTROLLER ERROR */
 block|}
 else|#
