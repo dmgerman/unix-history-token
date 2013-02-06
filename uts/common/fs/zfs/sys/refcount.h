0d32184ec2d8ad33632156f719116cac19a02134
Martin Matuska <mm@FreeBSD.org>
2013-02-06 08:14:58 +0000

    Update vendor-sys/illumos/dist to illumos-gate version 13937:6b4f289e7094
    Illumos ZFS issues:
     3468 mdb enhancements for zfs development (not relevant for FreeBSD)
    
    
    Former-commit-id: c49c5f6ca68e70b7175963beeb63ce4c7b02ba55

--- uts/common/fs/zfs/sys/refcount.h
+++ uts/common/fs/zfs/sys/refcount.h
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2012 by Delphix. All rights reserved.  */
 end_comment
 
 begin_ifndef
@@ -91,10 +91,10 @@ decl_stmt|;
 name|list_t
 name|rc_removed
 decl_stmt|;
-name|int64_t
+name|uint64_t
 name|rc_count
 decl_stmt|;
-name|int64_t
+name|uint64_t
 name|rc_removed_count
 decl_stmt|;
 block|}
