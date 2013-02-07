ea68082d7319296b21b4d57ee01fdd695b531042
Konstantin Belousov <kib@FreeBSD.org>
2013-02-07 15:34:22 +0000

    When vforked child is traced, the debugging events are not generated until child performs exec().  The behaviour is reasonable when a debugger is the real parent, because the parent is stopped until exec(), and sending a debugging event to the debugger would deadlock both parent and child.
    On the other hand, when debugger is not the parent of the vforked
    child, not sending debugging signals makes it impossible to debug
    across vfork.
    
    Fix the issue by declining generating debug signals only when vfork()
    was done and child called ptrace(PT_TRACEME).  Set a new process flag
    P_PPTRACE from the attach code for PT_TRACEME, if P_PPWAIT flag is
    set, which indicates that the process was created with vfork() and
    still did not execed. Check P_PPTRACE from issignal(), instead of
    refusing the trace outright for the P_PPWAIT case.  The scope of
    P_PPTRACE is exactly contained in the scope of P_PPWAIT.
    
    Found and tested by:  zont
    Reviewed by:	pluknet
    MFC after:	2 weeks
    
    
    Former-commit-id: e3bba38df82b087529e4c91d20d6c119e33d1cbb

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -852,6 +852,8 @@ operator|(
 name|P_TRACED
 operator||
 name|P_PPWAIT
+operator||
+name|P_PPTRACE
 operator|)
 expr_stmt|;
 comment|/* 	 * Stop the real interval timer.  If the handler is currently 	 * executing, prevent it from rearming itself and let it finish. 	 */
