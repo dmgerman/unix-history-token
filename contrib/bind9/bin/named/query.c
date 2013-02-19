02b2350229013f6abcde3879412f3457cdc9ff60
Bjoern A. Zeeb <bz@FreeBSD.org>
2013-02-19 13:27:20 +0000

    Fix Denial of Service vulnerability in named(8) with DNS64. [13:01]
    Fix Denial of Service vulnerability in libc's glob(3) functionality.
    [13:02]
    
    Security:	CVE-2012-5688
    Security:	FreeBSD-SA-13:01.bind
    Security:	CVE-2010-2632
    Security:	FreeBSD-SA-13:02.libc
    Approved by:	so (simon, bz)
    
    Former-commit-id: f0aae432726ce16874fb2a55449eeebb55b61df8

--- contrib/bind9/bin/named/query.c
+++ contrib/bind9/bin/named/query.c
@@ -23777,6 +23777,12 @@ name|ttl
 init|=
 name|ISC_UINT32_MAX
 decl_stmt|;
+name|dns_rdataset_init
+argument_list|(
+operator|&
+name|rdataset
+argument_list|)
+expr_stmt|;
 name|result
 operator|=
 name|dns_db_getoriginnode
@@ -23796,12 +23802,6 @@ condition|)
 goto|goto
 name|cleanup
 goto|;
-name|dns_rdataset_init
-argument_list|(
-operator|&
-name|rdataset
-argument_list|)
-expr_stmt|;
 name|result
 operator|=
 name|dns_db_findrdataset
