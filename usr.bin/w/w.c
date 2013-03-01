296be98d5303308eac39575a1dbafe83ba8bcd68
John Baldwin <jhb@FreeBSD.org>
2013-03-01 01:02:28 +0000

    MFC 245610,245633,245635: Include the thread name along with the command name when displaying the command name of a thread from a multi-threaded process that doesn't have an available argument list (such as kernel processes) and threads display is enabled via -H.
    Former-commit-id: 35098d523b562053c5239a45f2bd2018b508fe1c

--- usr.bin/w/w.c
+++ usr.bin/w/w.c
@@ -554,7 +554,10 @@ parameter_list|,
 name|char
 modifier|*
 parameter_list|,
-name|int
+name|char
+modifier|*
+parameter_list|,
+name|size_t
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -1591,6 +1594,8 @@ name|kp
 operator|->
 name|ki_comm
 argument_list|,
+name|NULL
+argument_list|,
 name|MAXCOMLEN
 argument_list|)
 expr_stmt|;
@@ -2150,6 +2155,8 @@ argument_list|,
 name|dkp
 operator|->
 name|ki_comm
+argument_list|,
+name|NULL
 argument_list|,
 name|MAXCOMLEN
 argument_list|)
