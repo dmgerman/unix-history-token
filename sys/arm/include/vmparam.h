74dd2aedd4cb0793a54988780bdd2a934ebab86d
Andrew Turner <andrew@FreeBSD.org>
2013-03-01 21:59:23 +0000

    Increase the maximum text size on ARM to 64MiB. Without this clang would be sent a SIGABRT when it is loaded as it is too large. This is the smallest power of two MiB value that allows us to execute clang.
    While here wrap it in an #ifndef to be consistent with the other
    architectures.
    
    Submitted by:	Daisuke Aoyama <aoyama at peach.ne.jp>
    
    
    Former-commit-id: fb3b3e024c6fd0b0357c5cab9da7a6479e0aba6c

--- sys/arm/include/vmparam.h
+++ sys/arm/include/vmparam.h
@@ -394,12 +394,23 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MAXTSIZ
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|MAXTSIZ
-value|(16*1024*1024)
+value|(64*1024*1024)
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifndef
 ifndef|#
