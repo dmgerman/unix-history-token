fbcadbdc3e9abcc30cca32a977032f2d5da197d6
Marius Strobl <marius@FreeBSD.org>
2013-03-02 17:14:53 +0000

    MFC: r246106, r246139
    Mark tftp_log() as __printflike() and deal with the fallout.
    
    
    Former-commit-id: 3c6ac931ecb5e173567faef50471502d6d559c4a

--- libexec/tftpd/tftp-utils.h
+++ libexec/tftpd/tftp-utils.h
@@ -474,8 +474,17 @@ name|message
 parameter_list|,
 modifier|...
 parameter_list|)
-function_decl|;
+function_decl|__printflike
+parameter_list|(
+function_decl|2
+operator|,
+function_decl|3
 end_function_decl
+
+begin_empty_stmt
+unit|)
+empty_stmt|;
+end_empty_stmt
 
 begin_comment
 comment|/*  * Performance figures  */
--- libexec/tftpd/tftpd.c
+++ libexec/tftpd/tftpd.c
@@ -143,6 +143,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<stdint.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<stdio.h>
 end_include
 
@@ -3540,12 +3546,18 @@ name|tftp_log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"Sent %d bytes in %d seconds"
+literal|"Sent %jd bytes in %jd seconds"
 argument_list|,
+operator|(
+name|intmax_t
+operator|)
 name|ts
 operator|.
 name|amount
 argument_list|,
+operator|(
+name|intmax_t
+operator|)
 name|time
 argument_list|(
 name|NULL
@@ -3703,8 +3715,11 @@ name|tftp_log
 argument_list|(
 name|LOG_INFO
 argument_list|,
-literal|"Download of %d bytes in %d blocks completed after %0.1f seconds\n"
+literal|"Download of %jd bytes in %d blocks completed after %0.1f seconds\n"
 argument_list|,
+operator|(
+name|intmax_t
+operator|)
 name|ts
 operator|.
 name|amount
