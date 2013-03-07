2343718d088148c8d82f259a6266a66aee7cff41
Michael Tuexen <tuexen@FreeBSD.org>
2013-03-07 20:37:57 +0000

    MFC r236522:
    Remove code which is not needed.
    
    
    Former-commit-id: 97472f0a650d71a7d9ceec56f5379e7e83cf5d84

--- sys/netinet/sctp_pcb.c
+++ sys/netinet/sctp_pcb.c
@@ -9215,15 +9215,6 @@ operator|->
 name|src_port
 expr_stmt|;
 comment|/* Get the scopes in properly to the sin6 addr's */
-comment|/* we probably don't need these operations */
-operator|(
-name|void
-operator|)
-name|sa6_recoverscope
-argument_list|(
-name|from6
-argument_list|)
-expr_stmt|;
 name|sa6_embedscope
 argument_list|(
 name|from6
@@ -9412,15 +9403,6 @@ operator|->
 name|dest_port
 expr_stmt|;
 comment|/* Get the scopes in properly to the sin6 addr's */
-comment|/* we probably don't need these operations */
-operator|(
-name|void
-operator|)
-name|sa6_recoverscope
-argument_list|(
-name|to6
-argument_list|)
-expr_stmt|;
 name|sa6_embedscope
 argument_list|(
 name|to6
