284c04c4b2650d65ca821e5df685ba4c020e88b9
Marius Strobl <marius@FreeBSD.org>
2013-03-08 12:57:58 +0000

    MFC: r247632
    - Complete r231621 (MFC'ed to stable/9 in r232092) by also blacklisting the
      bridge used by VMware for PCIe devices. While at it, update the comment
      now that we know that MSI-X doesn't work with ESXi 5.1 for Intel 82576
      either and the underlying issue is a bug in the MSI-X allocation code of
      the hypervisor.
      Reported by: Harald Schmalzbauer
    - Make the nomatch table const.
    
    
    Former-commit-id: 48707f9f9db5d9796e8508bf51dca9ca38f9c509

--- sys/dev/pci/pci.c
+++ sys/dev/pci/pci.c
@@ -1346,7 +1346,7 @@ block|,
 literal|0
 block|}
 block|,
-comment|/* 	 * MSI-X doesn't work with at least LSI SAS1068E passed through by 	 * VMware. 	 */
+comment|/* 	 * MSI-X allocation doesn't work properly for devices passed through 	 * by VMware up to at least ESXi 5.1. 	 */
 block|{
 literal|0x079015ad
 block|,
@@ -1357,6 +1357,18 @@ block|,
 literal|0
 block|}
 block|,
+comment|/* PCI/PCI-X */
+block|{
+literal|0x07a015ad
+block|,
+name|PCI_QUIRK_DISABLE_MSI
+block|,
+literal|0
+block|,
+literal|0
+block|}
+block|,
+comment|/* PCIe */
 comment|/* 	 * Some virtualization environments emulate an older chipset 	 * but support MSI just fine.  QEMU uses the Intel 82440. 	 */
 block|{
 literal|0x12378086
@@ -18022,6 +18034,7 @@ end_function
 
 begin_struct
 specifier|static
+specifier|const
 struct|struct
 block|{
 name|int
@@ -18030,6 +18043,7 @@ decl_stmt|;
 name|int
 name|subclass
 decl_stmt|;
+specifier|const
 name|char
 modifier|*
 name|desc
@@ -18755,13 +18769,15 @@ block|{
 name|int
 name|i
 decl_stmt|;
+specifier|const
 name|char
 modifier|*
 name|cp
 decl_stmt|,
 modifier|*
 name|scp
-decl_stmt|,
+decl_stmt|;
+name|char
 modifier|*
 name|device
 decl_stmt|;
@@ -18958,7 +18974,6 @@ argument_list|,
 literal|1
 argument_list|)
 expr_stmt|;
-return|return;
 block|}
 end_function
 
