95fa3ae1f5d0ed319615e49936bcbbf81f717835
Andrew Turner <andrew@FreeBSD.org>
2013-03-10 02:40:50 +0000

    Update how we read the stack pointer to work on both GCC and clang.
    Former-commit-id: 0cfbddf74f4cd27a034501b0c0009c3c80b3a766

--- sys/arm/arm/db_trace.c
+++ sys/arm/arm/db_trace.c
@@ -2418,14 +2418,11 @@ name|struct
 name|unwind_state
 name|state
 decl_stmt|;
-specifier|register
 name|uint32_t
 name|sp
-name|__asm__
-argument_list|(
-literal|"sp"
-argument_list|)
 decl_stmt|;
+comment|/* Read the stack pointer */
+asm|__asm __volatile("mov %0, sp" : "=&r" (sp));
 name|state
 operator|.
 name|registers
@@ -2448,9 +2445,6 @@ index|[
 name|SP
 index|]
 operator|=
-operator|(
-name|uint32_t
-operator|)
 name|sp
 expr_stmt|;
 name|state
