0ca6f1ade261298c17848c74d69cbd85702b43dc
Attilio Rao <attilio@FreeBSD.org>
2013-03-12 12:20:49 +0000

    Simplify vm_page_is_valid().
    Sponsored by:	EMC / Isilon storage division
    Reviewed by:	alc
    
    
    Former-commit-id: e240fc1a21f018b74eed525d06a3b19e78a11a4a

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -10436,13 +10436,14 @@ argument_list|,
 name|size
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
+return|return
+operator|(
 name|m
 operator|->
 name|valid
+operator|!=
+literal|0
 operator|&&
-operator|(
 operator|(
 name|m
 operator|->
@@ -10453,13 +10454,6 @@ operator|)
 operator|==
 name|bits
 operator|)
-condition|)
-return|return
-literal|1
-return|;
-else|else
-return|return
-literal|0
 return|;
 block|}
 comment|/*  * Set the page's dirty bits if the page is modified.  */
