e42e942c12e73e055089ff0123e4a79c794c60f8
Alexander V. Chernikov <melifaro@FreeBSD.org>
2013-03-19 12:42:14 +0000

    MFC r247626.
    Fix callout expiring dynamic rules.
    
    PR:		kern/175530
    Submitted by:	Vladimir Spiridenkov <vs@gtn.ru>
    
    Former-commit-id: 223dcca89394bb7c2803443e97fca6b722b94a84

--- sys/netpfil/ipfw/ip_fw_dynamic.c
+++ sys/netpfil/ipfw/ip_fw_dynamic.c
@@ -4539,14 +4539,14 @@ operator|=
 operator|&
 name|V_layer3_chain
 expr_stmt|;
-comment|/* Run keepalive checks every keepalive_interval iff ka is enabled */
+comment|/* Run keepalive checks every keepalive_period iff ka is enabled */
 if|if
 condition|(
 operator|(
 name|V_dyn_keepalive_last
 operator|+
-name|V_dyn_keepalive_interval
-operator|>=
+name|V_dyn_keepalive_period
+operator|<=
 name|time_uptime
 operator|)
 operator|&&
@@ -5675,7 +5675,7 @@ operator|=
 literal|1
 expr_stmt|;
 comment|/* do send keepalives */
-name|V_dyn_keepalive
+name|V_dyn_keepalive_last
 operator|=
 name|time_uptime
 expr_stmt|;
