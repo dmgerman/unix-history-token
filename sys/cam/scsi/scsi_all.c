e4bb6f9343ecfc2c8b595ca2eb9d263c21a007e6
Alexander Motin <mav@FreeBSD.org>
2013-04-11 06:34:41 +0000

    Do not sent 120 TEST UNIT READY requests on generic NOT READY statuses.
    Some failing disks tend to return vendor-specific ASC/ASCQ codes with
    NOT READY sense key.  It caused extremely long recovery attempts, repeating
    these 120 TURs (it takes at least 1 minute) for every I/O request.
    Instead of that use default error handling, doing just few retries.
    
    Reviewed by:	ken, gibbs
    MFC after:	1 month
    
    
    Former-commit-id: c3348e52519c497515f20f7c49096967330a8029

--- sys/cam/scsi/scsi_all.c
+++ sys/cam/scsi/scsi_all.c
@@ -3569,13 +3569,7 @@ block|,
 block|{
 name|SSD_KEY_NOT_READY
 block|,
-name|SS_TUR
-operator||
-name|SSQ_MANY
-operator||
-name|SSQ_DECREMENT_COUNT
-operator||
-name|EBUSY
+name|SS_RDEF
 block|,
 literal|"NOT READY"
 block|}
@@ -4302,7 +4296,7 @@ literal|0x04
 argument_list|,
 literal|0x00
 argument_list|,
-argument|SS_TUR | SSQ_MANY | SSQ_DECREMENT_COUNT | EIO
+argument|SS_RDEF
 argument_list|,
 literal|"Logical unit not ready, cause not reportable"
 argument_list|)
