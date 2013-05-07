3dbafe9b55752ae197f7ff5b4182324fe207b93b
David C Somayajulu <davidcs@FreeBSD.org>
2013-05-07 22:58:42 +0000

    1. Updated Copyright Information 2. Added Flash Read/Update Support 3. Fixed TSO Handling
    Submitted by: David C Somayajulu (davidcs@freebsd.org)
    Reviewed by: George Neville-Neil (gnn@freebsd.org)
    Approved by: George Neville-Neil (gnn@freebsd.org)
    
    
    Former-commit-id: 0e25d90bc74c439524d3a09558770585d4d5b3e7

--- sys/dev/qlxgb/qla_ioctl.h
+++ sys/dev/qlxgb/qla_ioctl.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c) 2010-2011 Qlogic Corporation  * All rights reserved.  *  *  Redistribution and use in source and binary forms, with or without  *  modification, are permitted provided that the following conditions  *  are met:  *  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  *  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE  *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  *  POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*  * Copyright (c) 2011-2013 Qlogic Corporation  * All rights reserved.  *  *  Redistribution and use in source and binary forms, with or without  *  modification, are permitted provided that the following conditions  *  are met:  *  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  *  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE  *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  *  POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_comment
@@ -75,6 +75,88 @@ name|qla_rd_flash_t
 typedef|;
 end_typedef
 
+begin_struct
+struct|struct
+name|qla_wr_flash
+block|{
+name|uint32_t
+name|off
+decl_stmt|;
+name|uint32_t
+name|size
+decl_stmt|;
+name|void
+modifier|*
+name|buffer
+decl_stmt|;
+name|uint32_t
+name|pattern
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_typedef
+typedef|typedef
+name|struct
+name|qla_wr_flash
+name|qla_wr_flash_t
+typedef|;
+end_typedef
+
+begin_struct
+struct|struct
+name|qla_erase_flash
+block|{
+name|uint32_t
+name|off
+decl_stmt|;
+name|uint32_t
+name|size
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_typedef
+typedef|typedef
+name|struct
+name|qla_erase_flash
+name|qla_erase_flash_t
+typedef|;
+end_typedef
+
+begin_struct
+struct|struct
+name|qla_rd_pci_ids
+block|{
+name|uint16_t
+name|ven_id
+decl_stmt|;
+name|uint16_t
+name|dev_id
+decl_stmt|;
+name|uint16_t
+name|subsys_ven_id
+decl_stmt|;
+name|uint16_t
+name|subsys_dev_id
+decl_stmt|;
+name|uint8_t
+name|rev_id
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_typedef
+typedef|typedef
+name|struct
+name|qla_rd_pci_ids
+name|qla_rd_pci_ids_t
+typedef|;
+end_typedef
+
 begin_comment
 comment|/*  * Read/Write Register  */
 end_comment
@@ -95,6 +177,39 @@ define|#
 directive|define
 name|QLA_RD_FLASH
 value|_IOWR('q', 2, qla_rd_flash_t)
+end_define
+
+begin_comment
+comment|/*  * Write Flash  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|QLA_WR_FLASH
+value|_IOWR('q', 3, qla_wr_flash_t)
+end_define
+
+begin_comment
+comment|/*  * Erase Flash  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|QLA_ERASE_FLASH
+value|_IOWR('q', 5, qla_erase_flash_t)
+end_define
+
+begin_comment
+comment|/*  * Read PCI IDs   */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|QLA_RD_PCI_IDS
+value|_IOWR('q', 6, qla_rd_pci_ids_t)
 end_define
 
 begin_endif
