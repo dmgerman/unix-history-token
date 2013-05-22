27fdd5d252d2d736cb1fffb7c10a50928c6e1482
Jung-uk Kim <jkim@FreeBSD.org>
2013-05-22 17:47:45 +0000

    Improve compatibility with old flex and fix build with GCC.
    Former-commit-id: 452d2a2fcbd88c4e6b41a2a5d6575b5d5ecea7e7

--- sbin/hastd/hast.h
+++ sbin/hastd/hast.h
@@ -756,26 +756,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|void
-name|yyerror
-parameter_list|(
-specifier|const
-name|char
-modifier|*
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|int
-name|yylex
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_endif
 endif|#
 directive|endif
