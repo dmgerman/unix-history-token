76e0785b87b13b720e3deba478f5c7464764cf88
Martin Matuska <mm@FreeBSD.org>
2013-05-23 22:52:21 +0000

    Update vendor/illumos/dist and vendor-sys/illumos/dist to illumos-gate 14031:e4eb37f33d60
    Illumos ZFS issues:
      3699 zfs hold or release of a non-existent snapshot does not output error
      3739 cannot set zfs quota or reservation on pool version < 22
    
    
    Former-commit-id: 372bd19132bdc4df3bd27f6364da70c0228267a8

--- uts/common/fs/zfs/dsl_prop.c
+++ uts/common/fs/zfs/dsl_prop.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2013 Martin Matuska. All rights reserved.  */
 end_comment
 
 begin_include
@@ -2580,25 +2580,6 @@ operator|<
 name|SPA_VERSION_RECVD_PROPS
 condition|)
 block|{
-name|zfs_prop_t
-name|prop
-init|=
-name|zfs_name_to_prop
-argument_list|(
-name|propname
-argument_list|)
-decl_stmt|;
-if|if
-condition|(
-name|prop
-operator|==
-name|ZFS_PROP_QUOTA
-operator|||
-name|prop
-operator|==
-name|ZFS_PROP_RESERVATION
-condition|)
-return|return;
 if|if
 condition|(
 name|source
