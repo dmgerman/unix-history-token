a3b0a087b637c5b8c839b41d3c03b519921a3995
Alan Cox <alc@FreeBSD.org>
2013-06-10 01:48:21 +0000

    Revise the interface between vm_object_madvise() and vm_page_dontneed() so that pointless calls to pmap_is_modified() can be easily avoided when performing madvise(..., MADV_FREE).
    Sponsored by:	EMC / Isilon Storage Division
    
    
    Former-commit-id: 9f17d235c361c06deb6d662a06112149ef0c12e0

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -1300,6 +1300,19 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|void
+name|vm_page_advise
+parameter_list|(
+name|vm_page_t
+name|m
+parameter_list|,
+name|int
+name|advice
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|vm_page_t
 name|vm_page_alloc
 parameter_list|(
@@ -1417,15 +1430,6 @@ end_function_decl
 begin_function_decl
 name|int
 name|vm_page_try_to_free
-parameter_list|(
-name|vm_page_t
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
-name|vm_page_dontneed
 parameter_list|(
 name|vm_page_t
 parameter_list|)
