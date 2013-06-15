04e1de60955e2aa647cc10f5a19e3ef777372e44
Mikolaj Golub <trociny@FreeBSD.org>
2013-06-15 08:07:47 +0000

    MFC r251490:
    Properly set curvnet context in lagg_port_setlladdr() task handler.
    
    Reported by:	Nikos Vassiliadis <nvass gmx.com>
    Submitted by:	zec
    Tested by:	Nikos Vassiliadis <nvass gmx.com>
    
    
    Former-commit-id: 8bc35734c0b8071e8f6254ad38c753db1d4ec4d5

--- sys/net/if_lagg.c
+++ sys/net/if_lagg.c
@@ -2883,6 +2883,13 @@ operator|->
 name|llq_ifp
 expr_stmt|;
 comment|/* Set the link layer address */
+name|CURVNET_SET
+argument_list|(
+name|ifp
+operator|->
+name|if_vnet
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|if_setlladdr
@@ -2895,6 +2902,9 @@ name|llq_lladdr
 argument_list|,
 name|ETHER_ADDR_LEN
 argument_list|)
+expr_stmt|;
+name|CURVNET_RESTORE
+argument_list|()
 expr_stmt|;
 if|if
 condition|(
