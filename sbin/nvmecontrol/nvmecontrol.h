c1dbeea702142c9bc46b3ead5fd264375c877150
Jim Harris <jimharris@FreeBSD.org>
2013-06-26 23:02:48 +0000

    Break out code related to the perftest command into a separate file.
    Sponsored by:	Intel
    MFC after:	3 days
    
    
    Former-commit-id: d01f613bba67671cb504497e3fb2bfc90364c3be

--- sbin/nvmecontrol/nvmecontrol.h
+++ sbin/nvmecontrol/nvmecontrol.h
@@ -138,6 +138,21 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|perftest
+parameter_list|(
+name|int
+name|argc
+parameter_list|,
+name|char
+modifier|*
+name|argv
+index|[]
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
