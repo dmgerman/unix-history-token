6c36b818b1eb2a00a919acb26a2b8c42de09d84c
Jim Harris <jimharris@FreeBSD.org>
2013-06-26 23:46:31 +0000

    Add generalized helper functions for printing hex data as part of nvmecontrol commands.
    Submitted by:	Joe Golio <joseph.golio@emc.com>
    Obtained from:	EMC / Isilon Storage Division
    MFC after:	3 days
    
    
    Former-commit-id: 204f7c00b252f56bc927bbe5c50b636a4aa7a71f

--- sbin/nvmecontrol/nvmecontrol.h
+++ sbin/nvmecontrol/nvmecontrol.h
@@ -182,6 +182,20 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|print_hex
+parameter_list|(
+name|void
+modifier|*
+name|data
+parameter_list|,
+name|uint32_t
+name|length
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
