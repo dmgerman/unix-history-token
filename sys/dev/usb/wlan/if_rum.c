7c1151f69961bc9a81a117e6d96f4488f1727d19
Rui Paulo <rpaulo@FreeBSD.org>
2013-07-04 20:57:15 +0000

    Set ic_cryptocaps to make sure wpa_supplicant works with WEP.
    Former-commit-id: e0a010fecc80a89003bf80653403e524cd3d9052

--- sys/dev/usb/wlan/if_rum.c
+++ sys/dev/usb/wlan/if_rum.c
@@ -3555,6 +3555,18 @@ operator||
 name|IEEE80211_C_WPA
 comment|/* 802.11i */
 expr_stmt|;
+name|ic
+operator|->
+name|ic_cryptocaps
+operator|=
+name|IEEE80211_CRYPTO_WEP
+operator||
+name|IEEE80211_CRYPTO_AES_CCM
+operator||
+name|IEEE80211_CRYPTO_TKIPMIC
+operator||
+name|IEEE80211_CRYPTO_TKIP
+expr_stmt|;
 name|bands
 operator|=
 literal|0
