4f7b7a3a52c5c70443e70028d4a92cb7ba0e9f20
Michael Tuexen <tuexen@FreeBSD.org>
2013-07-07 12:54:15 +0000

    MFC r242708, r242709: Improve PR-SCTP if used in combination with NR-SACK. Based on work done by Mohammad Rajiullah.
    Former-commit-id: 6fd4debaa4a6b0966886c7769f197cabc0d8e327

--- sys/netinet/sctp_output.c
+++ sys/netinet/sctp_output.c
@@ -49614,11 +49614,21 @@ argument_list|)
 block|{
 if|if
 condition|(
+operator|(
 name|at
 operator|->
 name|sent
 operator|!=
 name|SCTP_FORWARD_TSN_SKIP
+operator|)
+operator|&&
+operator|(
+name|at
+operator|->
+name|sent
+operator|!=
+name|SCTP_DATAGRAM_NR_MARKED
+operator|)
 condition|)
 block|{
 comment|/* no more to look at */
