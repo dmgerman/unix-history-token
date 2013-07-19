95c1b007aad7434f856f335e2faca249faa10145
Jack F Vogel <jfv@FreeBSD.org>
2013-07-19 21:37:40 +0000

    Add new Coleto Creek device support: SATA, SMBus, and Watchdog devices.
    MFC after: 1 week
    
    
    Former-commit-id: 56d03aa5e5215612e7b3fbfe5f6a04fcdaf1f030

--- sys/dev/ahci/ahci.c
+++ sys/dev/ahci/ahci.c
@@ -1401,6 +1401,16 @@ literal|0
 block|}
 block|,
 block|{
+literal|0x23a38086
+block|,
+literal|0x00
+block|,
+literal|"Intel Coleto Creek"
+block|,
+literal|0
+block|}
+block|,
+block|{
 literal|0x8c028086
 block|,
 literal|0x00
