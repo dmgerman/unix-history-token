91141fa0ff57d80914cffc31ac71eb562a2258bc
Pedro Giffuni <pfg@FreeBSD.org>
2013-07-25 16:04:55 +0000

    ext2fs: Drop a check that wan't supposed to be in r253651.
    MFC after:	1 month
    
    
    Former-commit-id: 87e0c40242125a4a938e910faddbe3de1796be1c

--- sys/fs/ext2fs/ext2_lookup.c
+++ sys/fs/ext2fs/ext2_lookup.c
@@ -647,25 +647,6 @@ name|NULL
 expr_stmt|;
 block|}
 comment|/* 	 * Avoid complications for partial directory entries by adjusting 	 * the i/o to end at a block boundary.  Don't give up (like ufs 	 * does) if the initial adjustment gives a negative count, since 	 * many callers don't supply a large enough buffer.  The correct 	 * size is a little larger than DIRBLKSIZ to allow for expansion 	 * of directory entries, but some callers just use 512. 	 */
-if|if
-condition|(
-name|uio
-operator|->
-name|uio_offset
-operator|<
-literal|0
-condition|)
-name|offset
-operator|=
-name|startoffset
-operator|=
-name|uio
-operator|->
-name|uio_offset
-operator|+
-name|DIRBLKSIZ
-expr_stmt|;
-else|else
 name|offset
 operator|=
 name|startoffset
