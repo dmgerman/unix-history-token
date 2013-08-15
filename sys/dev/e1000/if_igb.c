ed9055ce737327b5bac519943bd3bfa86e456fea
Jack F Vogel <jfv@FreeBSD.org>
2013-08-15 21:06:38 +0000

    MFC r254262  Further improve the msix setup, make sure pci_alloc_msix() gives us              the vectors we requested, and fall back to MSI when not, also release              any allocated resources before the fallback.
    Former-commit-id: 7369b961af67255d1680df0146d4231e47a3c8ed

--- sys/dev/e1000/if_igb.c
+++ sys/dev/e1000/if_igb.c
@@ -13685,6 +13685,7 @@ goto|;
 block|}
 if|if
 condition|(
+operator|(
 name|pci_alloc_msix
 argument_list|(
 name|dev
@@ -13694,6 +13695,13 @@ name|msgs
 argument_list|)
 operator|==
 literal|0
+operator|)
+operator|&&
+operator|(
+name|msgs
+operator|==
+name|want
+operator|)
 condition|)
 block|{
 name|device_printf
@@ -13719,7 +13727,12 @@ name|msgs
 operator|)
 return|;
 block|}
-comment|/* Fallback to MSI configuration */
+comment|/* 	** If MSIX alloc failed or provided us with 	** less than needed, free and fall through to MSI 	*/
+name|pci_release_msi
+argument_list|(
+name|dev
+argument_list|)
+expr_stmt|;
 name|msi
 label|:
 if|if
@@ -13777,7 +13790,7 @@ name|adapter
 operator|->
 name|dev
 argument_list|,
-literal|" Using MSI interrupt\n"
+literal|" Using an MSI interrupt\n"
 argument_list|)
 expr_stmt|;
 return|return
@@ -13786,7 +13799,15 @@ name|msgs
 operator|)
 return|;
 block|}
-comment|/* Default to a legacy interrupt */
+name|device_printf
+argument_list|(
+name|adapter
+operator|->
+name|dev
+argument_list|,
+literal|" Using a Legacy interrupt\n"
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 literal|0
