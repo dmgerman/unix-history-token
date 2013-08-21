3870d41303d1041f3917e6ca008efa0097bee0f6
Ian Lepore <ian@FreeBSD.org>
2013-08-21 14:33:02 +0000

    Add support for uarts other than the serial console in TI OMAP SoCs.
    The TI uart hardware is ns16550-compatible, except that before it can
    be used the clocks and power have to be enabled and a non-standard
    mode control register has to be set to put the device in uart mode
    (as opposed to irDa or other serial protocols).  This adds the extra
    code in an extension to the standard ns8250 probe routine, and the
    rest of the driver is just the standard ns8250 code.
    
    
    Former-commit-id: 22d9ed7ff7f7693ed513f3b9c7f1a5d289a7fb80

--- sys/dev/uart/uart.h
+++ sys/dev/uart/uart.h
@@ -247,6 +247,20 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|extern
+name|struct
+name|uart_class
+name|uart_ti8250_class
+name|__attribute__
+argument_list|(
+operator|(
+name|weak
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_ifdef
 ifdef|#
 directive|ifdef
