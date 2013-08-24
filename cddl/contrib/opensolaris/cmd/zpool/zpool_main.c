1ba30d2599051437e4e024df8285b06da3906cff
Xin Li <delphij@FreeBSD.org>
2013-08-24 00:54:47 +0000

    MFV r254751:
    Don't treat the parameter as a number (pool GUID) when there is
    error converting it from string, instead, treat it as the pool
    name.
    
    Illumos ZFS issues:
      1765 assert triggered in libzfs_import.c trying to import pool
           name beginning with a number
    
    
    Former-commit-id: 014d3b5ba085d1892ead2580f61900ba1a0d8561

--- cddl/contrib/opensolaris/cmd/zpool/zpool_main.c
+++ cddl/contrib/opensolaris/cmd/zpool/zpool_main.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc. All rights reserved.  * Copyright (c) 2012 by Delphix. All rights reserved.  * Copyright (c) 2012 by Frederik Wessels. All rights reserved.  * Copyright (c) 2012 Martin Matuska<mm@FreeBSD.org>. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc. All rights reserved.  * Copyright (c) 2012 by Delphix. All rights reserved.  * Copyright (c) 2012 by Frederik Wessels. All rights reserved.  * Copyright (c) 2012 Martin Matuska<mm@FreeBSD.org>. All rights reserved.  * Copyright (c) 2013 by Prasad Joshi (sTec). All rights reserved.  */
 end_comment
 
 begin_include
@@ -9324,6 +9324,7 @@ name|endptr
 operator|!=
 literal|'\0'
 condition|)
+block|{
 name|searchname
 operator|=
 name|argv
@@ -9331,6 +9332,11 @@ index|[
 literal|0
 index|]
 expr_stmt|;
+name|searchguid
+operator|=
+literal|0
+expr_stmt|;
+block|}
 name|found_config
 operator|=
 name|NULL
