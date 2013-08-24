78469b9061ccc636701864238fabba4c603d4a3a
Mark Johnston <markj@FreeBSD.org>
2013-08-24 21:13:38 +0000

    Rename the kld_unload event handler to kld_unload_try, and add a new kld_unload event handler which gets invoked after a linker file has been successfully unloaded. The kld_unload and kld_load event handlers are now invoked with the shared linker lock held, while kld_unload_try is invoked with the lock exclusively held.
    Convert hwpmc(4) to use these event handlers instead of having
    kern_kldload() and kern_kldunload() invoke hwpmc(4) hooks whenever files are
    loaded or unloaded. This has no functional effect, but simplifes the linker
    code somewhat.
    
    Reviewed by:	jhb
    
    
    Former-commit-id: 45ae5c43c572a959d23e8c4c4cbe8dc2de62f9b2

--- sys/cddl/dev/sdt/sdt.c
+++ sys/cddl/dev/sdt/sdt.c
@@ -267,7 +267,7 @@ end_function_decl
 begin_function_decl
 specifier|static
 name|void
-name|sdt_kld_unload
+name|sdt_kld_unload_try
 parameter_list|(
 name|void
 modifier|*
@@ -426,7 +426,7 @@ end_decl_stmt
 
 begin_decl_stmt
 name|eventhandler_tag
-name|sdt_kld_unload_tag
+name|sdt_kld_unload_try_tag
 decl_stmt|;
 end_decl_stmt
 
@@ -1335,7 +1335,7 @@ end_function
 begin_function
 specifier|static
 name|void
-name|sdt_kld_unload
+name|sdt_kld_unload_try
 parameter_list|(
 name|void
 modifier|*
@@ -1585,13 +1585,13 @@ argument_list|,
 name|EVENTHANDLER_PRI_ANY
 argument_list|)
 expr_stmt|;
-name|sdt_kld_unload_tag
+name|sdt_kld_unload_try_tag
 operator|=
 name|EVENTHANDLER_REGISTER
 argument_list|(
-name|kld_unload
+name|kld_unload_try
 argument_list|,
-name|sdt_kld_unload
+name|sdt_kld_unload_try
 argument_list|,
 name|NULL
 argument_list|,
@@ -1637,9 +1637,9 @@ argument_list|)
 expr_stmt|;
 name|EVENTHANDLER_DEREGISTER
 argument_list|(
-name|kld_unload
+name|kld_unload_try
 argument_list|,
-name|sdt_kld_unload_tag
+name|sdt_kld_unload_try_tag
 argument_list|)
 expr_stmt|;
 name|sdt_probe_func
