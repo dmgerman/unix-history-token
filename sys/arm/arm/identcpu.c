8046d5cf96dd1b8dfecbf101cd05c07f555e6f54
Zbigniew Bodek <zbb@FreeBSD.org>
2013-10-28 21:39:54 +0000

    Remove not working and deprecated PJ4Bv6 support
    Sheeva PJ4Bv6 - based chips were only prototypes for V7 class Armada
    SoC family. Current in-tree support for PJ4Bv6 will not work and also
    there should be no platforms in active use that would incorporate that
    CPU revision.
    
    
    Former-commit-id: daf7926ea709d9c6dd53bd5c83adf1ff2e886892

--- sys/arm/arm/identcpu.c
+++ sys/arm/arm/identcpu.c
@@ -1451,26 +1451,6 @@ name|generic_steppings
 block|}
 block|,
 block|{
-name|CPU_ID_MV88SV581X_V6
-block|,
-name|CPU_CLASS_MARVELL
-block|,
-literal|"Sheeva 88SV581x"
-block|,
-name|generic_steppings
-block|}
-block|,
-block|{
-name|CPU_ID_ARM_88SV581X_V6
-block|,
-name|CPU_CLASS_MARVELL
-block|,
-literal|"Sheeva 88SV581x"
-block|,
-name|generic_steppings
-block|}
-block|,
-block|{
 name|CPU_ID_MV88SV581X_V7
 block|,
 name|CPU_CLASS_MARVELL
@@ -1486,26 +1466,6 @@ block|,
 name|CPU_CLASS_MARVELL
 block|,
 literal|"Sheeva 88SV581x"
-block|,
-name|generic_steppings
-block|}
-block|,
-block|{
-name|CPU_ID_MV88SV584X_V6
-block|,
-name|CPU_CLASS_MARVELL
-block|,
-literal|"Sheeva 88SV584x"
-block|,
-name|generic_steppings
-block|}
-block|,
-block|{
-name|CPU_ID_ARM_88SV584X_V6
-block|,
-name|CPU_CLASS_MARVELL
-block|,
-literal|"Sheeva 88SV584x"
 block|,
 name|generic_steppings
 block|}
