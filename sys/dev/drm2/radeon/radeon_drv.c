1159faf9c04f5c5a1d6f69d51190682897036330
Jean-Sebastien Pedron <dumbbell@FreeBSD.org>
2013-10-29 19:50:40 +0000

    MFC r256771:
    drm/radeon: radeonkms depends on firmware(9)
    
    Submitted by:	tijl@
    Approved by:	re (kib)
    
    
    Former-commit-id: 042fe74ad05c94e4e97e1a31214028bce6f30da8

--- sys/dev/drm2/radeon/radeon_drv.c
+++ sys/dev/drm2/radeon/radeon_drv.c
@@ -2428,5 +2428,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|radeonkms
+argument_list|,
+name|firmware
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
