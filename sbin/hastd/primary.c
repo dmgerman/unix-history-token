db2345961d72135f41a980cf4b4e7b6ae2b7fe83
Mikolaj Golub <trociny@FreeBSD.org>
2013-10-31 20:30:26 +0000

    MFC r257154:
    Merging local and remote bitmaps must be protected by hr_amp lock.
    
    This is believed to fix hastd crashes, which might occur during
    synchronization, triggered by the failed assertion:
    
     Assertion failed: (amp->am_memtab[ext] > 0),
     function activemap_write_complete, file activemap.c, line 351.
    
    Approved by:	re (glebius)
    
    
    Former-commit-id: 8d228a05b0fd17566510bc980c1fef8a90bbf171

--- sbin/hastd/primary.c
+++ sbin/hastd/primary.c
@@ -3557,6 +3557,14 @@ goto|goto
 name|close
 goto|;
 block|}
+name|mtx_lock
+argument_list|(
+operator|&
+name|res
+operator|->
+name|hr_amp_lock
+argument_list|)
+expr_stmt|;
 comment|/* 		 * Merge local and remote bitmaps. 		 */
 name|activemap_merge
 argument_list|(
@@ -3575,14 +3583,6 @@ name|map
 argument_list|)
 expr_stmt|;
 comment|/* 		 * Now that we merged bitmaps from both nodes, flush it to the 		 * disk before we start to synchronize. 		 */
-name|mtx_lock
-argument_list|(
-operator|&
-name|res
-operator|->
-name|hr_amp_lock
-argument_list|)
-expr_stmt|;
 operator|(
 name|void
 operator|)
