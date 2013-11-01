1b9b3cfa1cca4c967cc11e0e41ac7cbf3c319b00
Marcel Moolenaar <marcel@FreeBSD.org>
2013-11-01 01:32:01 +0000

    Use LOG2_ID_PAGE_SIZE again for the identity mapping in regions 6 & 7. Make the default translation size the same as the PBVM page size to avoid inserting overlapping translations in the TC. That generally is very bad.
    Former-commit-id: 8d53648ad163dd1479f01e1aec5ae371b4db2941

--- sys/ia64/ia64/mp_machdep.c
+++ sys/ia64/ia64/mp_machdep.c
@@ -843,7 +843,7 @@ literal|8
 operator|)
 operator||
 operator|(
-name|PAGE_SHIFT
+name|LOG2_ID_PAGE_SIZE
 operator|<<
 literal|2
 operator|)
@@ -863,7 +863,7 @@ literal|8
 operator|)
 operator||
 operator|(
-name|PAGE_SHIFT
+name|LOG2_ID_PAGE_SIZE
 operator|<<
 literal|2
 operator|)
