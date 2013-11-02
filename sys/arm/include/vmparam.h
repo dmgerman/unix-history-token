b101b397b97e4e801c4dbb81ed8ceff6215e7541
Alan Cox <alc@FreeBSD.org>
2013-11-02 17:08:20 +0000

    Don't create a distinct free page pool for segregating allocations that are accessed through the direct map unless the kernel configuration actually includes a direct map.  Only a few configurations do, and for the rest the unnecessary free page pool is a small pessimization.
    Tested by:	zbb
    MFC after:	6 weeks
    
    
    Former-commit-id: a553130102b546b701463c0f71063d444fd90225

--- sys/arm/include/vmparam.h
+++ sys/arm/include/vmparam.h
@@ -196,8 +196,14 @@ name|VM_PHYSSEG_DENSE
 end_define
 
 begin_comment
-comment|/*  * Create three free page pools: VM_FREEPOOL_DEFAULT is the default pool  * from which physical pages are allocated and VM_FREEPOOL_DIRECT is  * the pool from which physical pages for small UMA objects are  * allocated.  */
+comment|/*  * Create two or three free page pools depending on the existence of a direct  * map: VM_FREEPOOL_DEFAULT is the default pool from which physical pages are  * allocated, and VM_FREEPOOL_DIRECT is the pool from which physical pages for  * small UMA objects are allocated.  */
 end_comment
+
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|ARM_USE_SMALL_ALLOC
+end_ifdef
 
 begin_define
 define|#
@@ -216,15 +222,46 @@ end_define
 begin_define
 define|#
 directive|define
-name|VM_FREEPOOL_DEFAULT
-value|0
+name|VM_FREEPOOL_DIRECT
+value|1
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_define
+define|#
+directive|define
+name|VM_NFREEPOOL
+value|2
+end_define
+
+begin_define
+define|#
+directive|define
+name|VM_FREEPOOL_CACHE
+value|1
 end_define
 
 begin_define
 define|#
 directive|define
 name|VM_FREEPOOL_DIRECT
-value|1
+value|0
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_define
+define|#
+directive|define
+name|VM_FREEPOOL_DEFAULT
+value|0
 end_define
 
 begin_comment
