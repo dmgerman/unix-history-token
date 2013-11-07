480e1949375d9ec0793b9d3b391fc3be3bbfa900
Steven G. Kargl <kargl@FreeBSD.org>
2013-11-07 21:20:34 +0000

    Fix bulding libm on platforms with LDBL_MANT_DIG == 53.
    Reported by:	ian
    
    
    Former-commit-id: 4597bc9cd23cce5842cdcd81a52bb4992aafa421

--- lib/msun/src/s_round.c
+++ lib/msun/src/s_round.c
@@ -136,5 +136,30 @@ block|}
 block|}
 end_function
 
+begin_if
+if|#
+directive|if
+operator|(
+name|LDBL_MANT_DIG
+operator|==
+literal|53
+operator|)
+end_if
+
+begin_expr_stmt
+name|__weak_reference
+argument_list|(
+name|round
+argument_list|,
+name|roundl
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 end_unit
 
