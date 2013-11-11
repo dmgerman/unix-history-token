d2dd9c1f06155e42e2f7eb104f39f425c468c115
Andrey V. Elsukov <ae@FreeBSD.org>
2013-11-11 10:44:38 +0000

    MFC r257084:   Initialize inc_fibnum for properly handling ICMP6_PACKET_TOO_BIG   errors in multifib environment.
      PR:		183265
    
    
    Former-commit-id: 38146875d8ac215a98e45434729e849e2b0f7270

--- sys/netinet6/icmp6.c
+++ sys/netinet6/icmp6.c
@@ -4931,6 +4931,15 @@ argument_list|)
 expr_stmt|;
 name|inc
 operator|.
+name|inc_fibnum
+operator|=
+name|M_GETFIB
+argument_list|(
+name|m
+argument_list|)
+expr_stmt|;
+name|inc
+operator|.
 name|inc_flags
 operator||=
 name|INC_ISIPV6
