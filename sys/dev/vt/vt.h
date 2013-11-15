805420ebc8864778e391943bf52958fa5684ad53
Aleksandr Rybalko <ray@FreeBSD.org>
2013-11-15 11:33:36 +0000

    Add VT_ALT_TO_ESC_HACK enabled by default. This will prepend ESC sequence before any chars when any of ALT keys is down. ! Not sure if it right way, but now it is possible to use Alt keys in vim.
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: 38fdc4d42b18f570521e14021fd461a212132de6

--- sys/dev/vt/vt.h
+++ sys/dev/vt/vt.h
@@ -122,6 +122,24 @@ endif|#
 directive|endif
 end_endif
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|VT_ALT_TO_ESC_HACK
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|VT_ALT_TO_ESC_HACK
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_define
 define|#
 directive|define
@@ -432,6 +450,11 @@ name|int
 name|vd_keyboard
 decl_stmt|;
 comment|/* (G) Keyboard index. */
+name|unsigned
+name|int
+name|vd_kbstate
+decl_stmt|;
+comment|/* (?) Device unit. */
 name|unsigned
 name|int
 name|vd_unit
@@ -1475,7 +1498,7 @@ parameter_list|,
 name|softc
 parameter_list|)
 define|\
-value|static struct terminal	driver ## _consterm;				\ static struct vt_window	driver ## _conswindow;				\ static struct vt_device	driver ## _consdev = {				\ 	.vd_driver =&driver,						\ 	.vd_softc = (softc),						\ 	.vd_flags = VDF_INVALID,					\ 	.vd_windows = { [VT_CONSWINDOW] =&driver ## _conswindow, },	\ 	.vd_curwindow =&driver ## _conswindow,				\ 	.vd_markedwin = NULL,						\ };									\ static term_char_t	driver ## _constextbuf[(width) * 		\ 	    (VBF_DEFAULT_HISTORY_SIZE)];				\ static term_char_t	*driver ## _constextbufrows[			\ 	    VBF_DEFAULT_HISTORY_SIZE];					\ static struct vt_window	driver ## _conswindow = {			\ 	.vw_number = VT_CONSWINDOW,					\ 	.vw_flags = VWF_CONSOLE,					\ 	.vw_buf = {							\ 		.vb_buffer = driver ## _constextbuf,			\ 		.vb_rows = driver ## _constextbufrows,			\ 		.vb_history_size = VBF_DEFAULT_HISTORY_SIZE,		\ 		.vb_curroffset = 0,					\ 		.vb_roffset = 0,					\ 		.vb_flags = VBF_STATIC,					\ 		.vb_mark_start = {					\ 			.tp_row = 0,					\ 			.tp_col = 0,					\ 		},							\ 		.vb_mark_end = {					\ 			.tp_row = 0,					\ 			.tp_col = 0,					\ 		},							\ 		.vb_scr_size = {					\ 			.tp_row = height,				\ 			.tp_col = width,				\ 		},							\ 	},								\ 	.vw_device =&driver ## _consdev,				\ 	.vw_terminal =&driver ## _consterm,				\ 	.vw_kbdmode = K_XLATE,						\ };									\ TERMINAL_DECLARE_EARLY(driver ## _consterm, vt_termclass,		\&driver ## _conswindow);						\ SYSINIT(vt_early_cons, SI_SUB_INT_CONFIG_HOOKS, SI_ORDER_ANY,		\     vt_upgrade,&driver ## _consdev)
+value|static struct terminal	driver ## _consterm;				\ static struct vt_window	driver ## _conswindow;				\ static struct vt_device	driver ## _consdev = {				\ 	.vd_driver =&driver,						\ 	.vd_softc = (softc),						\ 	.vd_flags = VDF_INVALID,					\ 	.vd_windows = { [VT_CONSWINDOW] =&driver ## _conswindow, },	\ 	.vd_curwindow =&driver ## _conswindow,				\ 	.vd_markedwin = NULL,						\ 	.vd_kbstate = 0,						\ };									\ static term_char_t	driver ## _constextbuf[(width) * 		\ 	    (VBF_DEFAULT_HISTORY_SIZE)];				\ static term_char_t	*driver ## _constextbufrows[			\ 	    VBF_DEFAULT_HISTORY_SIZE];					\ static struct vt_window	driver ## _conswindow = {			\ 	.vw_number = VT_CONSWINDOW,					\ 	.vw_flags = VWF_CONSOLE,					\ 	.vw_buf = {							\ 		.vb_buffer = driver ## _constextbuf,			\ 		.vb_rows = driver ## _constextbufrows,			\ 		.vb_history_size = VBF_DEFAULT_HISTORY_SIZE,		\ 		.vb_curroffset = 0,					\ 		.vb_roffset = 0,					\ 		.vb_flags = VBF_STATIC,					\ 		.vb_mark_start = {					\ 			.tp_row = 0,					\ 			.tp_col = 0,					\ 		},							\ 		.vb_mark_end = {					\ 			.tp_row = 0,					\ 			.tp_col = 0,					\ 		},							\ 		.vb_scr_size = {					\ 			.tp_row = height,				\ 			.tp_col = width,				\ 		},							\ 	},								\ 	.vw_device =&driver ## _consdev,				\ 	.vw_terminal =&driver ## _consterm,				\ 	.vw_kbdmode = K_XLATE,						\ };									\ TERMINAL_DECLARE_EARLY(driver ## _consterm, vt_termclass,		\&driver ## _conswindow);						\ SYSINIT(vt_early_cons, SI_SUB_INT_CONFIG_HOOKS, SI_ORDER_ANY,		\     vt_upgrade,&driver ## _consdev)
 end_define
 
 begin_comment
