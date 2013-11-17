daa3acd2df3418f6f4b65fc8577ffa06135f0cab
Jean-Sebastien Pedron <dumbbell@FreeBSD.org>
2013-11-17 16:07:52 +0000

    drm: Support DRM_CAP_TIMESTAMP_MONOTONIC capability
    This fixes DPMS with KDE and radeonkms. Without this, the display would
    freeze when the monitor is put into sleep state, and only resumes after
    several dozens of minutes once the monitor is powered on again.
    
    Tested by:	Mathias Picker <Mathias.Picker@virtual-earth.de>
    
    
    Former-commit-id: b43d3adf4a6b6b713329bd2b8241fa47a75e4526

--- sys/dev/drm2/drmP.h
+++ sys/dev/drm2/drmP.h
@@ -4451,6 +4451,14 @@ name|drm_timestamp_precision
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|extern
+name|unsigned
+name|int
+name|drm_timestamp_monotonic
+decl_stmt|;
+end_decl_stmt
+
 begin_comment
 comment|/* Device setup support (drm_drv.c) */
 end_comment
