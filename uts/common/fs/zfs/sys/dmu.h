93cf12624cc23845517ccf8e758e660aef797286
Andriy Gapon <avg@FreeBSD.org>
2013-11-20 10:58:25 +0000

    3964 L2ARC should always compress metadata buffers
    illumos/illumos-gate@e4be62a2b74a8f09bb669217a1a39eee069b13a1
    
    
    Former-commit-id: 603a5c449bcbd46b5b59902f7f691eeb8ecc483e

--- uts/common/fs/zfs/sys/dmu.h
+++ uts/common/fs/zfs/sys/dmu.h
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc. All rights reserved.  * Copyright (c) 2012, Joyent, Inc. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc. All rights reserved.  * Copyright (c) 2012, Joyent, Inc. All rights reserved.  * Copyright 2013 DEY Storage Systems, Inc.  */
 end_comment
 
 begin_comment
@@ -2818,6 +2818,10 @@ name|zfs_crc64_table
 index|[
 literal|256
 index|]
+decl_stmt|;
+specifier|extern
+name|int
+name|zfs_mdcomp_disable
 decl_stmt|;
 ifdef|#
 directive|ifdef
