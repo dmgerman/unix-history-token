ba4086f476ab975075ff22c184bf4459021dda31
Andriy Gapon <avg@FreeBSD.org>
2013-11-26 10:26:40 +0000

    MFC r229663: Allow to change vfs.zfs.arc_meta_limit at runtime.
    - Change vfs.zfs.arc_meta_used from CTLFLAG_RDTUN to CTLFLAG_RD,
    as it is not a tunable.
    
    MFC slacker:	pjd
    
    
    Former-commit-id: a6689fde69751d6a64c3d4a21038e8e1d90c59a2

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
@@ -1522,7 +1522,7 @@ name|OID_AUTO
 argument_list|,
 name|arc_meta_used
 argument_list|,
-name|CTLFLAG_RDTUN
+name|CTLFLAG_RD
 argument_list|,
 operator|&
 name|arc_meta_used
@@ -1543,7 +1543,7 @@ name|OID_AUTO
 argument_list|,
 name|arc_meta_limit
 argument_list|,
-name|CTLFLAG_RDTUN
+name|CTLFLAG_RW
 argument_list|,
 operator|&
 name|arc_meta_limit
