31b7429f67b3cdc78b84c7b2c2b6707446108e97
Andriy Gapon <avg@FreeBSD.org>
2013-11-29 15:39:39 +0000

    zfs: add zfs_freebsd_putpages
    this should be more optimal than writing pages one-by-one via zfs_write ->
    update_pages in the case of multi-page putpages call
    
    MFC after:	16 days
    
    
    Former-commit-id: 07e8c98726c53f5e214f946c1cc4acdb3a29cf12

--- sys/cddl/compat/opensolaris/sys/vm.h
+++ sys/cddl/compat/opensolaris/sys/vm.h
@@ -51,6 +51,22 @@ name|zfs_vm_pagerret_ok
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|extern
+specifier|const
+name|int
+name|zfs_vm_pagerput_sync
+decl_stmt|;
+end_decl_stmt
+
+begin_decl_stmt
+specifier|extern
+specifier|const
+name|int
+name|zfs_vm_pagerput_inval
+decl_stmt|;
+end_decl_stmt
+
 begin_function_decl
 name|void
 name|zfs_vmobject_assert_wlocked
