810a2f9e52a700ed9a4b5484b14c71b8ba4d3a47
Craig Rodrigues <rodrigc@FreeBSD.org>
2013-12-02 18:20:07 +0000

    MFC r258737
    In keg_dtor(), print out the keg name in the "Freed UMA keg was not empty"
    message printed to the console.  This makes it easier to track down
    the source of certain memory leaks.
    
    Suggested by: adrian
    
    
    Former-commit-id: 74ccac68707a244b67645515a360dcb876de8dc3

--- sys/vm/uma_core.c
+++ sys/vm/uma_core.c
@@ -6322,8 +6322,18 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"Freed UMA keg was not empty (%d items). "
+literal|"Freed UMA keg (%s) was not empty (%d items). "
 literal|" Lost %d pages of memory.\n"
+argument_list|,
+name|keg
+operator|->
+name|uk_name
+condition|?
+name|keg
+operator|->
+name|uk_name
+else|:
+literal|""
 argument_list|,
 name|keg
 operator|->
