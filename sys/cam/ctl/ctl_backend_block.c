503d2d594e011de6d841396c5b8821d48cbbffd4
Edward Tomasz Napierala <trasz@FreeBSD.org>
2013-12-03 18:04:14 +0000

    Properly report an error instead of panicing when user tries to create LUN backed by non-disk device, e.g. /dev/null.
    Reviewed by:	ken (earlier version)
    MFC after:	3 days
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: d8b439de70c65f53ad3100fc7ca58812f0a0e576

--- sys/cam/ctl/ctl_backend_block.c
+++ sys/cam/ctl/ctl_backend_block.c
@@ -6494,6 +6494,7 @@ break|break;
 case|case
 name|CTL_BE_BLOCK_NONE
 case|:
+break|break;
 default|default:
 name|panic
 argument_list|(
@@ -6577,6 +6578,7 @@ break|break;
 case|case
 name|CTL_BE_BLOCK_NONE
 case|:
+break|break;
 default|default:
 name|panic
 argument_list|(
@@ -6988,7 +6990,7 @@ operator|->
 name|error_str
 argument_list|)
 argument_list|,
-literal|"%s is not a disk or file"
+literal|"%s is not a disk or plain file"
 argument_list|,
 name|be_lun
 operator|->
