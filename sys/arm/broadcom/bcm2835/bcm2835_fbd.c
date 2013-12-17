5729c50642f26fbe7f87fff3797c1628b7ce7f8d
Aleksandr Rybalko <ray@FreeBSD.org>
2013-12-17 15:34:38 +0000

    Fix copyright and some style(9) things.
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: dceff73cb31444ebd39421371dc44819ba78b49f

--- sys/arm/broadcom/bcm2835/bcm2835_fbd.c
+++ sys/arm/broadcom/bcm2835/bcm2835_fbd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2012 Oleksandr Tymoshenko<gonzo@freebsd.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
+comment|/*-  * Copyright (c) 2012 Oleksandr Tymoshenko<gonzo@freebsd.org>  * Copyright (c) 2012, 2013 The FreeBSD Foundation  * All rights reserved.  *  * Portions of this software were developed by Oleksandr Rybalko  * under sponsorship from the FreeBSD Foundation.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  */
 end_comment
 
 begin_include
@@ -367,22 +367,16 @@ modifier|*
 name|arg
 parameter_list|)
 block|{
-name|struct
-name|bcmsc_softc
-modifier|*
-name|sc
-init|=
-name|arg
-decl_stmt|;
 specifier|volatile
 name|struct
 name|bcm_fb_config
 modifier|*
 name|fb_config
-init|=
+decl_stmt|;
+name|struct
+name|bcmsc_softc
+modifier|*
 name|sc
-operator|->
-name|fb_config
 decl_stmt|;
 name|struct
 name|fb_info
@@ -406,6 +400,16 @@ name|err
 init|=
 literal|0
 decl_stmt|;
+name|sc
+operator|=
+name|arg
+expr_stmt|;
+name|fb_config
+operator|=
+name|sc
+operator|->
+name|fb_config
+expr_stmt|;
 name|node
 operator|=
 name|ofw_bus_get_node
@@ -1146,7 +1150,7 @@ goto|goto
 name|fail
 goto|;
 block|}
-comment|/*  	 * We have to wait until interrupts are enabled.  	 * Mailbox relies on it to get data from VideoCore 	 */
+comment|/* 	 * We have to wait until interrupts are enabled. 	 * Mailbox relies on it to get data from VideoCore 	 */
 name|sc
 operator|->
 name|init_hook
