78270039b4b862b35c17c5784ccebae71d331b74
Baptiste Daroussin <bapt@FreeBSD.org>
2013-12-23 17:16:44 +0000

    Do not print a question in non-interractive mode but explain why boostrapping has failed and how to allow it to happen
    Reported by:	jmmv
    
    
    Former-commit-id: dcfec00c4b8dd922f0cb04d6e77d5eb7410d91ca

--- usr.sbin/pkg/pkg.c
+++ usr.sbin/pkg/pkg.c
@@ -3829,6 +3829,19 @@ literal|"Do you want to fetch and install it now? [y/N]: "
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|static
+specifier|const
+name|char
+name|non_interractive_message
+index|[]
+init|=
+literal|"The package management tool is not yet installed on your system.\n"
+literal|"Please set ASSUME_ALWAYS_YES=yes environement variable to be able to boostrap "
+literal|"in non-interractive (stdin not being a tty)\n"
+decl_stmt|;
+end_decl_stmt
+
 begin_function
 specifier|static
 name|int
@@ -4423,13 +4436,6 @@ operator|!
 name|yes
 condition|)
 block|{
-name|printf
-argument_list|(
-literal|"%s"
-argument_list|,
-name|confirmation_message
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 operator|!
@@ -4441,9 +4447,25 @@ name|stdin
 argument_list|)
 argument_list|)
 condition|)
+block|{
+name|fprintf
+argument_list|(
+name|stderr
+argument_list|,
+name|non_interractive_message
+argument_list|)
+expr_stmt|;
 name|exit
 argument_list|(
 name|EXIT_FAILURE
+argument_list|)
+expr_stmt|;
+block|}
+name|printf
+argument_list|(
+literal|"%s"
+argument_list|,
+name|confirmation_message
 argument_list|)
 expr_stmt|;
 if|if
