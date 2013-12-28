6c08aeb252ac3bb754a58de0dffed1a35dd7053a
Dimitry Andric <dim@FreeBSD.org>
2013-12-28 01:15:34 +0000

    MFC r259839:
    In sys/netinet/in_mcast.c, inm_is_ifp_detached() is only used whenever
    KTR is defined, so put it between #ifdef KTR guards.  This avoids a
    warning about a unused function if KTR is not enabled.
    
    
    Former-commit-id: ac051f7a830eb5e83e04680e57b3f1f8ca2f0c9d

--- sys/netinet/in_mcast.c
+++ sys/netinet/in_mcast.c
@@ -559,6 +559,12 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KTR
+end_ifdef
+
 begin_function_decl
 specifier|static
 name|int
@@ -571,6 +577,11 @@ modifier|*
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_function_decl
 specifier|static
@@ -925,6 +936,12 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|KTR
+end_ifdef
+
 begin_comment
 comment|/*  * Inline function which wraps assertions for a valid ifp.  * The ifnet layer will set the ifma's ifp pointer to NULL if the ifp  * is detached.  */
 end_comment
@@ -1003,6 +1020,11 @@ operator|)
 return|;
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/*  * Initialize an in_mfilter structure to a known state at t0, t1  * with an empty source filter list.  */
