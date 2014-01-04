e98abc51ef7da40e01a4f0dd480476c422a5e66d
Dimitry Andric <dim@FreeBSD.org>
2014-01-04 18:58:18 +0000

    MFC r260042:
    Remove superfluous old-style rcsid[] from if_my.c.  There is already an
    __FBSDID() at the top of the file.
    
    
    Former-commit-id: c6baf54da0c7c5dad6d23dde1658ab4dbd2692e1

--- sys/dev/my/if_my.c
+++ sys/dev/my/if_my.c
@@ -257,28 +257,6 @@ directive|include
 file|<dev/my/if_myreg.h>
 end_include
 
-begin_ifndef
-ifndef|#
-directive|ifndef
-name|lint
-end_ifndef
-
-begin_decl_stmt
-specifier|static
-specifier|const
-name|char
-name|rcsid
-index|[]
-init|=
-literal|"$Id: if_my.c,v 1.16 2003/04/15 06:37:25 mdodd Exp $"
-decl_stmt|;
-end_decl_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_comment
 comment|/*  * Various supported device vendors/types and their names.  */
 end_comment
