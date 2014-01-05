d49024d3d162c3d201e558b9a3a7b6fcc877ecdd
Alexander Motin <mav@FreeBSD.org>
2014-01-05 22:53:55 +0000

    MFC r258169: Add ID for Intel Avoton SMBus controller.
    Former-commit-id: 52faccc818eb110592f2d1a1e75045888fc2529a

--- sys/dev/ichsmb/ichsmb_pci.c
+++ sys/dev/ichsmb/ichsmb_pci.c
@@ -256,6 +256,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|ID_AVOTON
+value|0x1f3c8086
+end_define
+
+begin_define
+define|#
+directive|define
 name|ID_COLETOCRK
 value|0x23B08086
 end_define
@@ -689,6 +696,17 @@ argument_list|(
 name|dev
 argument_list|,
 literal|"Intel Panther Point SMBus controller"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|ID_AVOTON
+case|:
+name|device_set_desc
+argument_list|(
+name|dev
+argument_list|,
+literal|"Intel Avoton SMBus controller"
 argument_list|)
 expr_stmt|;
 break|break;
