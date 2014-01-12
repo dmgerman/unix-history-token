80b261f7739356c44e651ade33da15d719ab4450
Pedro Giffuni <pfg@FreeBSD.org>
2014-01-12 20:18:12 +0000

    Revert unrelated fix that went into r260569.
    The fix is good but the minimum MFC period for this change
    has not elapsed yet.
    
    
    Former-commit-id: cbc2f41b10fd3abe0b4136e92667b011d53c8667

--- sys/fs/ext2fs/ext2_inode_cnv.c
+++ sys/fs/ext2fs/ext2_inode_cnv.c
@@ -530,7 +530,9 @@ name|ip
 operator|->
 name|i_flags
 operator|=
-literal|0
+name|ei
+operator|->
+name|e2di_flags
 expr_stmt|;
 name|ip
 operator|->
@@ -886,7 +888,9 @@ name|ei
 operator|->
 name|e2di_flags
 operator|=
-literal|0
+name|ip
+operator|->
+name|i_flags
 expr_stmt|;
 name|ei
 operator|->
