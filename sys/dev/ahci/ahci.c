1f663b01740ba46463d1b39aa96ae6c0176126c5
Alexander Motin <mav@FreeBSD.org>
2014-01-17 17:16:49 +0000

    Add ID for one more ASMedia AHCI-compatible controller.
    Reported by:	ignace.peeters@gmail.com
    MFC after:	2 weeks
    
    
    Former-commit-id: 02ae153155a43d8ddc92f947b4a14a24a047384d

--- sys/dev/ahci/ahci.c
+++ sys/dev/ahci/ahci.c
@@ -909,6 +909,16 @@ literal|0
 block|}
 block|,
 block|{
+literal|0x06111b21
+block|,
+literal|0x00
+block|,
+literal|"ASMedia ASM2106"
+block|,
+literal|0
+block|}
+block|,
+block|{
 literal|0x06121b21
 block|,
 literal|0x00
