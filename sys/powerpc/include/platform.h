c64dc0580ad9dc40c3ce7d8f14da410640da5591
Justin Hibbits <jhibbits@FreeBSD.org>
2014-01-31 03:55:34 +0000

    Unbreak non-SMP builds.  This was broken by r259284.  Also, reorganize the code introduced in that revision a bit.
    Reviewed by:	nwhitehorn
    MFC after:	3 weeks
    
    
    Former-commit-id: 843c2bda29ad47e33f975a568c320c363afc48d5

--- sys/powerpc/include/platform.h
+++ sys/powerpc/include/platform.h
@@ -157,6 +157,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|platform_sleep
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
