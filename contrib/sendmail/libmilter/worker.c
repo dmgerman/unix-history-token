fd9fdfad154c1367f7f0eb87594cc701596df0d6
Gregory Shapiro <gshapiro@FreeBSD.org>
2014-02-01 22:27:40 +0000

    MFC: Merge sendmail 8.14.8
    Former-commit-id: dc8a2de0ff4f95d69b1c7422a8894ef2fba27e1f

--- contrib/sendmail/libmilter/worker.c
+++ contrib/sendmail/libmilter/worker.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  *  Copyright (c) 2003-2004, 2007, 2009-2012 Sendmail, Inc. and its suppliers.  *	All rights reserved.  *  * By using this file, you agree to the terms and conditions set  * forth in the LICENSE file which can be found at the top level of  * the sendmail distribution.  *  * Contributed by Jose Marcio Martins da Cruz - Ecole des Mines de Paris  *   Jose-Marcio.Martins@ensmp.fr  */
+comment|/*  *  Copyright (c) 2003-2004, 2007, 2009-2012 Proofpoint, Inc. and its suppliers.  *	All rights reserved.  *  * By using this file, you agree to the terms and conditions set  * forth in the LICENSE file which can be found at the top level of  * the sendmail distribution.  *  * Contributed by Jose Marcio Martins da Cruz - Ecole des Mines de Paris  *   Jose-Marcio.Martins@ensmp.fr  */
 end_comment
 
 begin_include
@@ -12,7 +12,7 @@ end_include
 begin_macro
 name|SM_RCSID
 argument_list|(
-literal|"@(#)$Id: worker.c,v 8.24 2012/03/13 15:37:46 ca Exp $"
+literal|"@(#)$Id: worker.c,v 8.25 2013/11/22 20:51:37 ca Exp $"
 argument_list|)
 end_macro
 
