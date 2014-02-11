5f56f7d147a0a4d31beab4d4e2a13ff3f9d5c497
Edward Tomasz Napierala <trasz@FreeBSD.org>
2014-02-11 10:58:32 +0000

    Empty data segment during Login Phase is rather unlikely, but it's not, by itself, a protocol error.  This fixes interoperability with OpenBSD initiator.
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: 44e3dc5d2bc3c144ca4f4c08dfaaf18c8147add3

--- usr.sbin/ctld/discovery.c
+++ usr.sbin/ctld/discovery.c
@@ -153,21 +153,6 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|request
-operator|->
-name|pdu_data_len
-operator|==
-literal|0
-condition|)
-name|log_errx
-argument_list|(
-literal|1
-argument_list|,
-literal|"received Text PDU with empty data segment"
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
 name|ntohl
 argument_list|(
 name|bhstr
