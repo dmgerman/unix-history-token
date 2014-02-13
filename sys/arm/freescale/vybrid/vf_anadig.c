5eca255b59013eba1016785a1c8fb6cedcd9ca78
Ruslan Bukin <br@FreeBSD.org>
2014-02-13 06:38:01 +0000

    Add function for configuring PLL4 (Audio) clock frequency output.
    Former-commit-id: 8cd339472efce7fddc67d03686ca9edb635fa2b6

--- sys/arm/freescale/vybrid/vf_anadig.c
+++ sys/arm/freescale/vybrid/vf_anadig.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2013 Ruslan Bukin<br@bsdpad.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2013-2014 Ruslan Bukin<br@bsdpad.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_comment
@@ -541,6 +541,20 @@ name|EN_USB_CLKS
 value|(1<< 6)
 end_define
 
+begin_define
+define|#
+directive|define
+name|PLL4_CTRL_DIV_SEL_S
+value|0
+end_define
+
+begin_define
+define|#
+directive|define
+name|PLL4_CTRL_DIV_SEL_M
+value|0x7f
+end_define
+
 begin_struct
 struct|struct
 name|anadig_softc
@@ -562,6 +576,14 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
+
+begin_decl_stmt
+name|struct
+name|anadig_softc
+modifier|*
+name|anadig_sc
+decl_stmt|;
+end_decl_stmt
 
 begin_decl_stmt
 specifier|static
@@ -755,6 +777,94 @@ block|}
 end_function
 
 begin_function
+name|uint32_t
+name|pll4_configure_output
+parameter_list|(
+name|uint32_t
+name|mfi
+parameter_list|,
+name|uint32_t
+name|mfn
+parameter_list|,
+name|uint32_t
+name|mfd
+parameter_list|)
+block|{
+name|struct
+name|anadig_softc
+modifier|*
+name|sc
+decl_stmt|;
+name|int
+name|reg
+decl_stmt|;
+name|sc
+operator|=
+name|anadig_sc
+expr_stmt|;
+comment|/* 	 * PLLout = Fsys * (MFI+(MFN/MFD)) 	 */
+name|reg
+operator|=
+name|READ4
+argument_list|(
+name|sc
+argument_list|,
+name|ANADIG_PLL4_CTRL
+argument_list|)
+expr_stmt|;
+name|reg
+operator|&=
+operator|~
+operator|(
+name|PLL4_CTRL_DIV_SEL_M
+operator|<<
+name|PLL4_CTRL_DIV_SEL_S
+operator|)
+expr_stmt|;
+name|reg
+operator||=
+operator|(
+name|mfi
+operator|<<
+name|PLL4_CTRL_DIV_SEL_S
+operator|)
+expr_stmt|;
+name|WRITE4
+argument_list|(
+name|sc
+argument_list|,
+name|ANADIG_PLL4_CTRL
+argument_list|,
+name|reg
+argument_list|)
+expr_stmt|;
+name|WRITE4
+argument_list|(
+name|sc
+argument_list|,
+name|ANADIG_PLL4_NUM
+argument_list|,
+name|mfn
+argument_list|)
+expr_stmt|;
+name|WRITE4
+argument_list|(
+name|sc
+argument_list|,
+name|ANADIG_PLL4_DENOM
+argument_list|,
+name|mfd
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+literal|0
+operator|)
+return|;
+block|}
+end_function
+
+begin_function
 specifier|static
 name|int
 name|anadig_attach
@@ -834,6 +944,10 @@ literal|0
 index|]
 argument_list|)
 expr_stmt|;
+name|anadig_sc
+operator|=
+name|sc
+expr_stmt|;
 comment|/* Enable USB PLLs */
 name|enable_pll
 argument_list|(
@@ -849,7 +963,7 @@ argument_list|,
 name|ANADIG_PLL7_CTRL
 argument_list|)
 expr_stmt|;
-comment|/* Enable other */
+comment|/* Enable other PLLs */
 name|enable_pll
 argument_list|(
 name|sc
