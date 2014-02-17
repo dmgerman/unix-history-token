c9fffa6345a8b5374ec0407bf967d2c0f4f6bdc4
Andriy Gapon <avg@FreeBSD.org>
2014-02-17 18:24:37 +0000

    MFC r260185: MFV r260155: 4391 panic system rather than corrupting pool if we hit bug 4390
    Former-commit-id: e97f17ae2642166a4f735e8a448d7ebb2b083fee

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/spa_misc.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/spa_misc.c
@@ -325,7 +325,7 @@ expr_stmt|;
 end_expr_stmt
 
 begin_comment
-comment|/*  * zfs_recover can be set to nonzero to attempt to recover from  * otherwise-fatal errors, typically caused by on-disk corruption.  When  * set, calls to zfs_panic_recover() will turn into warning messages.  */
+comment|/*  * zfs_recover can be set to nonzero to attempt to recover from  * otherwise-fatal errors, typically caused by on-disk corruption.  When  * set, calls to zfs_panic_recover() will turn into warning messages.  * This should only be used as a last resort, as it typically results  * in leaked space, or worse.  */
 end_comment
 
 begin_decl_stmt
