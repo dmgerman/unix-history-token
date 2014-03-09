2d950b352f6ada19d3c38c10837b7d1fe528f435
Warner Losh <imp@FreeBSD.org>
2014-03-09 02:28:30 +0000

    Remove bogus define that causes compile errors.  Most of the defines for SAM9X are going away soonish anyway (once FDT works), but until then...
    Former-commit-id: 5c2c49d843ff792f525a3de575afda138779f6df

--- sys/arm/at91/at91sam9x5reg.h
+++ sys/arm/at91/at91sam9x5reg.h
@@ -892,13 +892,6 @@ end_define
 begin_define
 define|#
 directive|define
-name|AT91SAM9X25_IRQ_AIC
-value|(-1)
-end_define
-
-begin_define
-define|#
-directive|define
 name|AT91SAM9X25_AIC_BASE
 value|0xffff000
 end_define
