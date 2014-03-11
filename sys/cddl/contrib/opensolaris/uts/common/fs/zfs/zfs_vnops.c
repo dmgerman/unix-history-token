8c3e2c224ca10fcc9653755e0e484dab20102bd5
Xin Li <delphij@FreeBSD.org>
2014-03-11 00:23:50 +0000

    MFV r262983:
    4638 Panic in ZFS via rfs3_setattr()/rfs3_write(): dirtying snapshot!
    
    illumos/illumos-gate@2144b121c08e0eb676cc6ca4662ebbc9f9c22fe3
    
    
    Former-commit-id: 787850cebef547253ad4bce77616616174498424

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vnops.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright 2013 Nexenta Systems, Inc.  All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright 2014 Nexenta Systems, Inc.  All rights reserved.  */
 end_comment
 
 begin_comment
@@ -3236,6 +3236,32 @@ argument_list|,
 literal|8
 argument_list|)
 expr_stmt|;
+comment|/* 	 * In a case vp->v_vfsp != zp->z_zfsvfs->z_vfs (e.g. snapshots) our 	 * callers might not be able to detect properly that we are read-only, 	 * so check it explicitly here. 	 */
+if|if
+condition|(
+name|zfsvfs
+operator|->
+name|z_vfs
+operator|->
+name|vfs_flag
+operator|&
+name|VFS_RDONLY
+condition|)
+block|{
+name|ZFS_EXIT
+argument_list|(
+name|zfsvfs
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+name|SET_ERROR
+argument_list|(
+name|EROFS
+argument_list|)
+operator|)
+return|;
+block|}
 comment|/* 	 * If immutable or not appending then return EPERM 	 */
 if|if
 condition|(
@@ -22164,6 +22190,32 @@ operator|(
 name|SET_ERROR
 argument_list|(
 name|EINVAL
+argument_list|)
+operator|)
+return|;
+block|}
+comment|/* 	 * In a case vp->v_vfsp != zp->z_zfsvfs->z_vfs (e.g. snapshots) our 	 * callers might not be able to detect properly that we are read-only, 	 * so check it explicitly here. 	 */
+if|if
+condition|(
+name|zfsvfs
+operator|->
+name|z_vfs
+operator|->
+name|vfs_flag
+operator|&
+name|VFS_RDONLY
+condition|)
+block|{
+name|ZFS_EXIT
+argument_list|(
+name|zfsvfs
+argument_list|)
+expr_stmt|;
+return|return
+operator|(
+name|SET_ERROR
+argument_list|(
+name|EROFS
 argument_list|)
 operator|)
 return|;
