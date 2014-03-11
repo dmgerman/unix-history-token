bb48ac7eb3d800d23d839dd770d78795d198ee88
Gleb Smirnoff <glebius@FreeBSD.org>
2014-03-11 15:17:04 +0000

    Merge r261169: Fix compilation with IGB_LEGACY_TX defined.
    PR:		185909
    
    Former-commit-id: 0c69d5c90eb800d87fa7a6c2626882c6c822b53b

--- sys/dev/e1000/if_igb.c
+++ sys/dev/e1000/if_igb.c
@@ -11091,6 +11091,9 @@ name|adapter
 operator|->
 name|queues
 decl_stmt|;
+ifndef|#
+directive|ifndef
+name|IGB_LEGACY_TX
 name|struct
 name|tx_ring
 modifier|*
@@ -11100,6 +11103,8 @@ name|adapter
 operator|->
 name|tx_rings
 decl_stmt|;
+endif|#
+directive|endif
 name|int
 name|error
 decl_stmt|,
