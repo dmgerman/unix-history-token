809a27444fd254ace7e51acdeba9f2cfd5459097
Simon J. Gerraty <sjg@FreeBSD.org>
2014-03-15 23:51:28 +0000

    Backport bmake to 9 so ports can rely on it being available. We only build/install it as bmake.
    Requested by: bapt
    Reviewed by:	marcel
    
    
    Former-commit-id: 6bfea0fccc2128ddc5b0257963fe7d7129d52354

---
+++ usr.bin/bmake/config.h
@@ -0,0 +1,1121 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
+comment|/* config.h.  Generated from config.h.in by configure.  */
+end_comment
+
+begin_comment
+comment|/* config.h.in.  Generated from configure.in by autoheader.  */
+end_comment
+
+begin_comment
+comment|/* Define if building universal (internal helper macro) */
+end_comment
+
+begin_comment
+comment|/* #undef AC_APPLE_UNIVERSAL_BUILD */
+end_comment
+
+begin_comment
+comment|/* Path of default shell */
+end_comment
+
+begin_comment
+comment|/* #undef DEFSHELL_CUSTOM */
+end_comment
+
+begin_comment
+comment|/* Shell spec to use by default */
+end_comment
+
+begin_comment
+comment|/* #undef DEFSHELL_INDEX */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the<ar.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_AR_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the declaration of `sys_siglist', and to 0 if you    don't. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_DECL_SYS_SIGLIST
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<dirent.h> header file, and it defines `DIR'.    */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_DIRENT_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `dirname' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_DIRNAME
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_DOPRNT */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the `err' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ERR
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `errx' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ERRX
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<err.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ERR_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<fcntl.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_FCNTL_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `fork' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_FORK
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `getcwd' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_GETCWD
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `getenv' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_GETENV
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `getopt' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_GETOPT
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `getwd' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_GETWD
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<inttypes.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_INTTYPES_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `killpg' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_KILLPG
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<memory.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_MEMORY_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `mmap' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_MMAP
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<ndir.h> header file, and it defines `DIR'. */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_NDIR_H */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the<paths.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_PATHS_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<poll.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_POLL_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `putenv' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_PUTENV
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<ranlib.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_RANLIB_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `realpath' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_REALPATH
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `select' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SELECT
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `setenv' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SETENV
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `setpgid' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SETPGID
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `setsid' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SETSID
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `sigaction' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SIGACTION
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `sigvec' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SIGVEC
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `snprintf' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SNPRINTF
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<stdint.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STDINT_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<stdlib.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STDLIB_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `strerror' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRERROR
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `stresep' function. */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_STRESEP */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the `strftime' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRFTIME
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<strings.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRINGS_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<string.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRING_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `strlcpy' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRLCPY
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `strsep' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRSEP
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `strtod' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRTOD
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `strtol' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRTOL
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if `struct stat' is a member of `st_rdev'. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_STRUCT_STAT_ST_RDEV
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if your `struct stat' has `st_rdev'. Deprecated, use    `HAVE_STRUCT_STAT_ST_RDEV' instead. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_ST_RDEV
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/dir.h> header file, and it defines `DIR'.    */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_SYS_DIR_H */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/mman.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_MMAN_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/ndir.h> header file, and it defines `DIR'.    */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_SYS_NDIR_H */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/select.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_SELECT_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/socket.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_SOCKET_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/stat.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_STAT_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/time.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_TIME_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/types.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_TYPES_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<sys/uio.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_UIO_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have<sys/wait.h> that is POSIX.1 compatible. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_SYS_WAIT_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<unistd.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_UNISTD_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `unsetenv' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_UNSETENV
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<utime.h> header file. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_UTIME_H
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `vfork' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_VFORK
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the<vfork.h> header file. */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE_VFORK_H */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the `vprintf' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_VPRINTF
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `vsnprintf' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_VSNPRINTF
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `wait3' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WAIT3
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `wait4' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WAIT4
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `waitpid' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WAITPID
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `warn' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WARN
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you have the `warnx' function. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WARNX
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if `fork' works. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WORKING_FORK
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if `vfork' works. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|HAVE_WORKING_VFORK
+value|1
+end_define
+
+begin_comment
+comment|/* define if your compiler has __attribute__ */
+end_comment
+
+begin_comment
+comment|/* #undef HAVE___ATTRIBUTE__ */
+end_comment
+
+begin_comment
+comment|/* Define to the address where bug reports for this package should be sent. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PACKAGE_BUGREPORT
+value|"sjg@NetBSD.org"
+end_define
+
+begin_comment
+comment|/* Define to the full name of this package. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PACKAGE_NAME
+value|"bmake"
+end_define
+
+begin_comment
+comment|/* Define to the full name and version of this package. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PACKAGE_STRING
+value|"bmake 20130706"
+end_define
+
+begin_comment
+comment|/* Define to the one symbol short name of this package. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PACKAGE_TARNAME
+value|"bmake"
+end_define
+
+begin_comment
+comment|/* Define to the home page for this package. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PACKAGE_URL
+value|""
+end_define
+
+begin_comment
+comment|/* Define to the version of this package. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PACKAGE_VERSION
+value|"20130706"
+end_define
+
+begin_comment
+comment|/* Define as the return type of signal handlers (`int' or `void'). */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|RETSIGTYPE
+value|void
+end_define
+
+begin_comment
+comment|/* Define to 1 if the `S_IS*' macros in<sys/stat.h> do not work properly. */
+end_comment
+
+begin_comment
+comment|/* #undef STAT_MACROS_BROKEN */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you have the ANSI C header files. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|STDC_HEADERS
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if you can safely include both<sys/time.h> and<time.h>. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TIME_WITH_SYS_TIME
+value|1
+end_define
+
+begin_comment
+comment|/* Define to 1 if your<sys/time.h> declares `struct tm'. */
+end_comment
+
+begin_comment
+comment|/* #undef TM_IN_SYS_TIME */
+end_comment
+
+begin_comment
+comment|/* Enable extensions on AIX 3, Interix.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_ALL_SOURCE
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_ALL_SOURCE
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Enable GNU extensions on systems that have them.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_GNU_SOURCE
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_GNU_SOURCE
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Enable threading extensions on Solaris.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_POSIX_PTHREAD_SEMANTICS
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_POSIX_PTHREAD_SEMANTICS
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Enable extensions on HP NonStop.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|_TANDEM_SOURCE
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|_TANDEM_SOURCE
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Enable general extensions on Solaris.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__EXTENSIONS__
+end_ifndef
+
+begin_define
+define|#
+directive|define
+name|__EXTENSIONS__
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most    significant byte first (like Motorola and SPARC, unlike Intel). */
+end_comment
+
+begin_if
+if|#
+directive|if
+name|defined
+name|AC_APPLE_UNIVERSAL_BUILD
+end_if
+
+begin_if
+if|#
+directive|if
+name|defined
+name|__BIG_ENDIAN__
+end_if
+
+begin_define
+define|#
+directive|define
+name|WORDS_BIGENDIAN
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|WORDS_BIGENDIAN
+end_ifndef
+
+begin_comment
+comment|/* #  undef WORDS_BIGENDIAN */
+end_comment
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* Define to 1 if on MINIX. */
+end_comment
+
+begin_comment
+comment|/* #undef _MINIX */
+end_comment
+
+begin_comment
+comment|/* Define to 2 if the system does not provide POSIX.1 features except with    this defined. */
+end_comment
+
+begin_comment
+comment|/* #undef _POSIX_1_SOURCE */
+end_comment
+
+begin_comment
+comment|/* Define to 1 if you need to in order for `stat' and other things to work. */
+end_comment
+
+begin_comment
+comment|/* #undef _POSIX_SOURCE */
+end_comment
+
+begin_comment
+comment|/* Define to empty if `const' does not conform to ANSI C. */
+end_comment
+
+begin_comment
+comment|/* #undef const */
+end_comment
+
+begin_comment
+comment|/* Define to `long int' if<sys/types.h> does not define. */
+end_comment
+
+begin_comment
+comment|/* #undef off_t */
+end_comment
+
+begin_comment
+comment|/* Define to `int' if<sys/types.h> does not define. */
+end_comment
+
+begin_comment
+comment|/* #undef pid_t */
+end_comment
+
+begin_comment
+comment|/* Define to `unsigned int' if<sys/types.h> does not define. */
+end_comment
+
+begin_comment
+comment|/* #undef size_t */
+end_comment
+
+begin_comment
+comment|/* Define as `fork' if `vfork' does not work. */
+end_comment
+
+begin_comment
+comment|/* #undef vfork */
+end_comment
+
+end_unit
+
