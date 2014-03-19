344c5aa49f211b6429a73aa03a7f2cad22eec584
Konstantin Belousov <kib@FreeBSD.org>
2014-03-19 13:09:38 +0000

    MFC r263095: Initialize paddr to handle the case of zero size.
    Former-commit-id: 8fefa7e44960e84c0d0903087951f970a2fdb517

--- sys/vm/device_pager.c
+++ sys/vm/device_pager.c
@@ -1851,6 +1851,11 @@ argument_list|(
 name|size
 argument_list|)
 expr_stmt|;
+name|paddr
+operator|=
+literal|0
+expr_stmt|;
+comment|/* Make paddr initialized for the case of size == 0. */
 for|for
 control|(
 name|off
