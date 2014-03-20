d5d39a02add2b7e2d1481dd9f3eb3637e4ed3070
Xin Li <delphij@FreeBSD.org>
2014-03-20 00:10:58 +0000

    MFC r256999 (smh):
    Added support for the 'zfs list -t snap' and 'zfs snap' aliases which are
    available under Oracle Solaris 11.
    
    This includes an update to the ZFS(8) man page to reflect all the
    available alias (snap, umount, and recv).
    
    Initial changes obtained from ZFS On Linux + fixes for man page and cmd
    help:
    https://github.com/zfsonlinux/zfs/commit/10b75496bb0cb7a7b8146c263164adc37f1d176a
    https://github.com/zfsonlinux/zfs/commit/cf81b00a73fe47fdb21586ac1cc179b734540973
    
    Obtained from:  https://github.com/zfsonlinux/zfs
    
    
    Former-commit-id: 9e43d77247955533ecdf05031059074ffea5edd9

--- cddl/contrib/opensolaris/cmd/zfs/zfs_main.c
+++ cddl/contrib/opensolaris/cmd/zfs/zfs_main.c
@@ -1308,9 +1308,9 @@ return|return
 operator|(
 name|gettext
 argument_list|(
-literal|"\treceive [-vnFu]<filesystem|volume|"
+literal|"\treceive|recv [-vnFu]<filesystem|volume|"
 literal|"snapshot>\n"
-literal|"\treceive [-vnFu] [-d | -e]<filesystem>\n"
+literal|"\treceive|recv [-vnFu] [-d | -e]<filesystem>\n"
 argument_list|)
 operator|)
 return|;
@@ -1383,7 +1383,7 @@ return|return
 operator|(
 name|gettext
 argument_list|(
-literal|"\tsnapshot [-r] [-o property=value] ... "
+literal|"\tsnapshot|snap [-r] [-o property=value] ... "
 literal|"<filesystem@snapname|volume@snapname> ...\n"
 argument_list|)
 operator|)
@@ -1395,7 +1395,7 @@ return|return
 operator|(
 name|gettext
 argument_list|(
-literal|"\tunmount [-f] "
+literal|"\tunmount|umount [-f] "
 literal|"<-a | filesystem|mountpoint>\n"
 argument_list|)
 operator|)
@@ -14173,6 +14173,8 @@ literal|"volume"
 block|,
 literal|"snapshot"
 block|,
+literal|"snap"
+block|,
 literal|"all"
 block|,
 name|NULL
@@ -14211,13 +14213,16 @@ break|break;
 case|case
 literal|2
 case|:
+case|case
+literal|3
+case|:
 name|types
 operator||=
 name|ZFS_TYPE_SNAPSHOT
 expr_stmt|;
 break|break;
 case|case
-literal|3
+literal|4
 case|:
 name|types
 operator|=
@@ -31530,6 +31535,22 @@ condition|)
 name|cmdname
 operator|=
 literal|"receive"
+expr_stmt|;
+comment|/* 		 * The 'snap' command is an alias for 'snapshot' 		 */
+if|if
+condition|(
+name|strcmp
+argument_list|(
+name|cmdname
+argument_list|,
+literal|"snap"
+argument_list|)
+operator|==
+literal|0
+condition|)
+name|cmdname
+operator|=
+literal|"snapshot"
 expr_stmt|;
 comment|/* 		 * Special case '-?' 		 */
 if|if
