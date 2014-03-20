68b2b1d6dc011ae7d1e9b5eb8f25f9b1fd3fa30a
Marcel Moolenaar <marcel@FreeBSD.org>
2014-03-20 00:38:17 +0000

    Use linker sets to provide build-time selection of the schemes that need to be supported. Each scheme provides data and callbacks for handling the scheme-specifics. To this end, put each scheme in its own source file.
    Former-commit-id: c507db0039242f8839cbbf20a9886c3a41d42c1b

--- mkimg.c
+++ mkimg.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2013 Juniper Networks, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2013,2014 Juniper Networks, Inc.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_include
@@ -16,6 +16,12 @@ literal|"$FreeBSD$"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
+
+begin_include
+include|#
+directive|include
+file|<sys/linker_set.h>
+end_include
 
 begin_include
 include|#
@@ -205,6 +211,15 @@ modifier|*
 name|why
 parameter_list|)
 block|{
+name|struct
+name|mkimg_scheme
+modifier|*
+name|s
+decl_stmt|,
+modifier|*
+modifier|*
+name|iter
+decl_stmt|;
 name|warnx
 argument_list|(
 literal|"error: %s"
@@ -285,55 +300,34 @@ argument_list|,
 literal|"    schemes:\n"
 argument_list|)
 expr_stmt|;
-name|fprintf
+name|SET_FOREACH
 argument_list|(
-name|stderr
+argument|iter
 argument_list|,
-literal|"\tapm\t-  Apple Partition Map\n"
+argument|schemes
 argument_list|)
+block|{
+name|s
+operator|=
+operator|*
+name|iter
 expr_stmt|;
 name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\tbsd\t-  BSD disk label\n"
-argument_list|)
-expr_stmt|;
-name|fprintf
-argument_list|(
-name|stderr
+literal|"\t%s\t-  %s\n"
 argument_list|,
-literal|"\tebr\t-  Extended Boot Record\n"
-argument_list|)
-expr_stmt|;
-name|fprintf
-argument_list|(
-name|stderr
+name|s
+operator|->
+name|name
 argument_list|,
-literal|"\tgpt\t-  GUID Partition Table\n"
+name|s
+operator|->
+name|description
 argument_list|)
 expr_stmt|;
-name|fprintf
-argument_list|(
-name|stderr
-argument_list|,
-literal|"\tmbr\t-  Master Boot Record\n"
-argument_list|)
-expr_stmt|;
-name|fprintf
-argument_list|(
-name|stderr
-argument_list|,
-literal|"\tpc98\t-  PC-9800 disk partitions\n"
-argument_list|)
-expr_stmt|;
-name|fprintf
-argument_list|(
-name|stderr
-argument_list|,
-literal|"\tvtoc8\t-  SMI VTOC8 disk labels\n"
-argument_list|)
-expr_stmt|;
+block|}
 name|fprintf
 argument_list|(
 name|stderr
@@ -1215,7 +1209,7 @@ condition|(
 name|scheme_selected
 argument_list|()
 operator|!=
-name|SCHEME_UNDEF
+name|NULL
 condition|)
 name|usage
 argument_list|(
@@ -1277,7 +1271,7 @@ condition|(
 name|scheme_selected
 argument_list|()
 operator|==
-name|SCHEME_UNDEF
+name|NULL
 condition|)
 name|usage
 argument_list|(
