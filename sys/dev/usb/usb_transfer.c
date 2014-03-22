0905488709161e08198ced57ade006b2b08a1a44
Hans Petter Selasky <hselasky@FreeBSD.org>
2014-03-22 17:20:10 +0000

    MFC r263159: Workaround for USB MIDI adapters which use non-supported values of wMaxPacketSize for BULK endpoints.
    Former-commit-id: 05ef5f2d72244f2ef4f369c7572fcd3a233438b8

--- sys/dev/usb/usb_transfer.c
+++ sys/dev/usb/usb_transfer.c
@@ -1231,6 +1231,9 @@ decl_stmt|;
 name|usb_frcount_t
 name|x
 decl_stmt|;
+name|uint16_t
+name|maxp_old
+decl_stmt|;
 name|uint8_t
 name|type
 decl_stmt|;
@@ -1597,6 +1600,13 @@ operator|->
 name|hc_max_packet_count
 expr_stmt|;
 block|}
+comment|/* store max packet size value before filtering */
+name|maxp_old
+operator|=
+name|xfer
+operator|->
+name|max_packet_size
+expr_stmt|;
 comment|/* filter "wMaxPacketSize" according to HC capabilities */
 if|if
 condition|(
@@ -1803,6 +1813,23 @@ index|]
 expr_stmt|;
 block|}
 block|}
+comment|/* 	 * Check if the max packet size was outside its allowed range 	 * and clamped to a valid value: 	 */
+if|if
+condition|(
+name|maxp_old
+operator|!=
+name|xfer
+operator|->
+name|max_packet_size
+condition|)
+name|xfer
+operator|->
+name|flags_int
+operator|.
+name|maxp_was_clamped
+operator|=
+literal|1
+expr_stmt|;
 comment|/* compute "max_frame_size" */
 name|usbd_update_max_frame_size
 argument_list|(
@@ -11876,6 +11903,32 @@ operator|(
 name|xfer
 operator|->
 name|isoc_time_complete
+operator|)
+return|;
+block|}
+end_function
+
+begin_comment
+comment|/*  * The following function returns non-zero if the max packet size  * field was clamped to a valid value. Else it returns zero.  */
+end_comment
+
+begin_function
+name|uint8_t
+name|usbd_xfer_maxp_was_clamped
+parameter_list|(
+name|struct
+name|usb_xfer
+modifier|*
+name|xfer
+parameter_list|)
+block|{
+return|return
+operator|(
+name|xfer
+operator|->
+name|flags_int
+operator|.
+name|maxp_was_clamped
 operator|)
 return|;
 block|}
