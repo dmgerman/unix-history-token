e83be770f8b3252dc2fefdf6af3326df0afae60c
Rui Paulo <rpaulo@FreeBSD.org>
2014-04-05 23:20:04 +0000

    Add fsl,imx53.
    Former-commit-id: 31285e903f54aead9c0c82a977f90e480f9327a2

--- sys/dev/ata/chipsets/ata-fsl.c
+++ sys/dev/ata/chipsets/ata-fsl.c
@@ -247,6 +247,14 @@ name|dev
 argument_list|,
 literal|"fsl,imx51-ata"
 argument_list|)
+operator|&&
+operator|!
+name|ofw_bus_is_compatible
+argument_list|(
+name|dev
+argument_list|,
+literal|"fsl,imx53-ata"
+argument_list|)
 condition|)
 return|return
 operator|(
