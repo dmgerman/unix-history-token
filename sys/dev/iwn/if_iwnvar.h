9b955b8275f79ce508f5433362643a37cc44884a
Marius Strobl <marius@FreeBSD.org>
2014-04-25 21:42:46 +0000

    MFC: r260053
    - There's no need to keep track of resource IDs.
    - Simplify MSI allocation and release. For a single one, we don't need to
      fiddle with the MSI count and pci_release_msi(9) is smart enough to just
      do nothing in case of INTx.
    - Don't allocate MSI as RF_SHAREABLE.
    - Use DEVMETHOD_END.
    - Use NULL instead of 0 for pointers.
    
    
    Former-commit-id: 167d1f8fd87411cbb90a8004691faa5668c454d3

--- sys/dev/iwn/if_iwnvar.h
+++ sys/dev/iwn/if_iwnvar.h
@@ -874,9 +874,6 @@ name|struct
 name|iwn_rx_ring
 name|rxq
 decl_stmt|;
-name|int
-name|mem_rid
-decl_stmt|;
 name|struct
 name|resource
 modifier|*
@@ -887,9 +884,6 @@ name|sc_st
 decl_stmt|;
 name|bus_space_handle_t
 name|sc_sh
-decl_stmt|;
-name|int
-name|irq_rid
 decl_stmt|;
 name|struct
 name|resource
