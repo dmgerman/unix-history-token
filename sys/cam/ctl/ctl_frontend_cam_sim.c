659c3a46d9fae3991bce5ecf255d22267b7ff006
Alexander Motin <mav@FreeBSD.org>
2014-05-08 08:21:52 +0000

    MFC r263811 (by trasz): Remove ctl_mem_pool.{c,h}.
    Former-commit-id: a2c659b736513efbc88c29bff972983d0bc77903

--- sys/cam/ctl/ctl_frontend_cam_sim.c
+++ sys/cam/ctl/ctl_frontend_cam_sim.c
@@ -174,12 +174,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<cam/ctl/ctl_mem_pool.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<cam/ctl/ctl_debug.h>
 end_include
 
