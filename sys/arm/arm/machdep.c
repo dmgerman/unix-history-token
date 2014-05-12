1f79f1a48f776ec6849951332ad851ca8c001b73
Ian Lepore <ian@FreeBSD.org>
2014-05-12 13:05:03 +0000

    Interrupts need to be disabled on entry to cpu_sleep() for ARM.  Given that and the need to be in a critical section when switching to idleclock mode for event timers, use spinlock_enter()/exit() to achieve both needs.
    The ARM WFI (wait for interrupt) instruction blocks until an interrupt is
    asserted, and it will unblock even if interrupts are masked, and it will
    unblock immediately if an interrupt is already pending.  It is necessary
    to execute it with interrupts disabled, otherwise the interrupt that
    should unblock it may occur and be serviced just prior to executing the
    instruction.  At that point the system is inappropriately asleep until
    the next timer tick or some other random interrupt happens.
    
    In general, interrupts need to be disabled continuously from the time the
    decision is made that there is no work to be done and sleeping is needed
    until actually going to sleep, to avoid a race where handling a new
    interrupt changes the basis for deciding there is no work to be done.
    
    Submitted by:	hps@ (in slightly different form)
    
    
    Former-commit-id: af013caf9ff745484b2799d0b7301889fc03b4ec

--- sys/arm/arm/machdep.c
+++ sys/arm/arm/machdep.c
@@ -1872,6 +1872,9 @@ argument_list|,
 name|curcpu
 argument_list|)
 expr_stmt|;
+name|spinlock_enter
+argument_list|()
+expr_stmt|;
 ifndef|#
 directive|ifndef
 name|NO_EVENTTIMERS
@@ -1881,9 +1884,6 @@ operator|!
 name|busy
 condition|)
 block|{
-name|critical_enter
-argument_list|()
-expr_stmt|;
 name|cpu_idleclock
 argument_list|()
 expr_stmt|;
@@ -1913,12 +1913,12 @@ block|{
 name|cpu_activeclock
 argument_list|()
 expr_stmt|;
-name|critical_exit
-argument_list|()
-expr_stmt|;
 block|}
 endif|#
 directive|endif
+name|spinlock_exit
+argument_list|()
+expr_stmt|;
 name|CTR2
 argument_list|(
 name|KTR_SPARE2
