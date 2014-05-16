6db9433c888b2056d85f372e0c1eb484f2c5d1f8
John Baldwin <jhb@FreeBSD.org>
2014-05-16 17:45:09 +0000

    Add definitions for more structured extended features as well as XSAVE Extended Features for AVX512 and MPX (Memory Protection Extensions).
    Obtained from:	Intel's Instruction Set Extensions Programming Reference
                    (March 2014)
    
    
    Former-commit-id: 7cfbbc616bb4db651c5df629f789811abaa356a3

--- sys/amd64/amd64/identcpu.c
+++ sys/amd64/amd64/identcpu.c
@@ -1203,13 +1203,13 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"\n  Standard Extended Features=0x%b"
+literal|"\n  Structured Extended Features=0x%b"
 argument_list|,
 name|cpu_stdext_feature
 argument_list|,
 literal|"\020"
 comment|/* RDFSBASE/RDGSBASE/WRFSBASE/WRGSBASE */
-literal|"\001GSFSBASE"
+literal|"\001FSGSBASE"
 literal|"\002TSCADJ"
 comment|/* Bit Manipulation Instructions */
 literal|"\004BMI1"
@@ -1221,17 +1221,27 @@ comment|/* Supervisor Mode Execution Prot. */
 literal|"\010SMEP"
 comment|/* Bit Manipulation Instructions */
 literal|"\011BMI2"
-literal|"\012ENHMOVSB"
+literal|"\012ERMS"
 comment|/* Invalidate Processor Context ID */
 literal|"\013INVPCID"
 comment|/* Restricted Transactional Memory */
 literal|"\014RTM"
+comment|/* Intel Memory Protection Extensions */
+literal|"\017MPX"
+comment|/* AVX512 Foundation */
+literal|"\021AVX512F"
 comment|/* Enhanced NRBG */
 literal|"\023RDSEED"
 comment|/* ADCX + ADOX */
 literal|"\024ADX"
 comment|/* Supervisor Mode Access Prevention */
 literal|"\025SMAP"
+literal|"\030CLFLUSHOPT"
+literal|"\032PROCTRACE"
+literal|"\033AVX512PF"
+literal|"\034AVX512ER"
+literal|"\035AVX512CD"
+literal|"\036SHA"
 argument_list|)
 expr_stmt|;
 block|}
