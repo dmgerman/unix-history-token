7285aca47957cf2df6a7be61ee057d8fbfc2b824
Peter Wemm <peter@FreeBSD.org>
2014-05-27 04:59:53 +0000

    Merge svn-1.8.8 -> 1.8.9
    Former-commit-id: 878676fc69684606a6501664dbbbf0d3d1fa9536

--- usr.bin/svn/svn_private_config.h
+++ usr.bin/svn/svn_private_config.h
@@ -358,7 +358,7 @@ begin_define
 define|#
 directive|define
 name|PACKAGE_STRING
-value|"subversion 1.8.8"
+value|"subversion 1.8.9"
 end_define
 
 begin_comment
@@ -391,7 +391,7 @@ begin_define
 define|#
 directive|define
 name|PACKAGE_VERSION
-value|"1.8.8"
+value|"1.8.9"
 end_define
 
 begin_comment
