a40c68b9cc3dd80b178e9fbffc73b995ec7e05e0
Marius Strobl <marius@FreeBSD.org>
2014-05-28 09:06:36 +0000

    MFC: r264177
    Make some unwise casts. On i386 these casts wind up being safe. Rather
    than disturb the API, go with these casts to shut gcc up.
    
    
    Former-commit-id: 15a93fb116c6ee3691699db67fcea70c5747e8cf

--- sys/dev/hyperv/netvsc/hv_rndis_filter.c
+++ sys/dev/hyperv/netvsc/hv_rndis_filter.c
@@ -6,6 +6,20 @@ end_comment
 begin_include
 include|#
 directive|include
+file|<sys/cdefs.h>
+end_include
+
+begin_expr_stmt
+name|__FBSDID
+argument_list|(
+literal|"$FreeBSD$"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_include
+include|#
+directive|include
 file|<sys/param.h>
 end_include
 
@@ -1408,7 +1422,10 @@ operator|(
 name|rndis_msg
 operator|*
 operator|)
-operator|(
+call|(
+name|uintptr_t
+call|)
+argument_list|(
 name|pkt
 operator|->
 name|page_buffers
@@ -1419,7 +1436,7 @@ operator|.
 name|pfn
 operator|<<
 name|PAGE_SHIFT
-operator|)
+argument_list|)
 expr_stmt|;
 name|rndis_hdr
 operator|=
