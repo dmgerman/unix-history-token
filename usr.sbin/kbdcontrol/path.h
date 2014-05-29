a3c590a1a1deb9c95cbd4dcf3bcb2c121b292fb6
Aleksandr Rybalko <ray@FreeBSD.org>
2014-05-29 14:39:25 +0000

    Enable kbdcontrol(1) to use maps from vt(4) keymaps dir /usr/share/vt/keymaps if vt(4) is present.
    MFC after:	7 days
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: d0a316b72f5fdae6fbc9cf375c40d0b29e45cfd2

--- usr.sbin/kbdcontrol/path.h
+++ usr.sbin/kbdcontrol/path.h
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
 begin_define
 define|#
 directive|define
@@ -18,6 +22,20 @@ define|#
 directive|define
 name|SCRNMAP_PATH
 value|"/usr/share/syscons/scrnmaps/"
+end_define
+
+begin_define
+define|#
+directive|define
+name|VT_KEYMAP_PATH
+value|"/usr/share/vt/keymaps/"
+end_define
+
+begin_define
+define|#
+directive|define
+name|VT_FONT_PATH
+value|"/usr/share/vt/fonts/"
 end_define
 
 end_unit
