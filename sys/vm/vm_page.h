91e58409576e73b5cc5851d510c321db57919da7
Alan Cox <alc@FreeBSD.org>
2014-06-07 17:12:26 +0000

    Add a page size field to struct vm_page.  Increase the page size field when a partially populated reservation becomes fully populated, and decrease this field when a fully populated reservation becomes partially populated.
    Use this field to simplify the implementation of pmap_enter_object() on
    amd64, arm, and i386.
    
    On all architectures where we support superpages, the cost of creating a
    superpage mapping is roughly the same as creating a base page mapping.  For
    example, both kinds of mappings entail the creation of a single PTE and PV
    entry.  With this in mind, use the page size field to make the
    implementation of vm_map_pmap_enter(..., MAP_PREFAULT_PARTIAL) a little
    smarter.  Previously, if MAP_PREFAULT_PARTIAL was specified to
    vm_map_pmap_enter(), that function would only map base pages.  Now, it will
    create up to 96 base page or superpage mappings.
    
    Reviewed by:	kib
    Sponsored by:	EMC / Isilon Storage Division
    
    
    Former-commit-id: f5d7a6863bdd48157472656062c134fba7761043

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -219,6 +219,10 @@ name|queue
 decl_stmt|;
 comment|/* page queue index (P,Q) */
 name|int8_t
+name|psind
+decl_stmt|;
+comment|/* pagesizes[] index (O) */
+name|int8_t
 name|segind
 decl_stmt|;
 name|uint8_t
@@ -1763,6 +1767,16 @@ end_function_decl
 begin_function_decl
 name|vm_page_t
 name|vm_page_prev
+parameter_list|(
+name|vm_page_t
+name|m
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|boolean_t
+name|vm_page_ps_is_valid
 parameter_list|(
 name|vm_page_t
 name|m
