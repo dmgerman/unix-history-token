5d870b1107909266a327191768b6ab1920347e93
Alexander V. Chernikov <melifaro@FreeBSD.org>
2014-06-14 22:47:25 +0000

    Move further to eliminate next pieces of number-assuming code inside tables.
    Kernel changes:
    * Add IP_FW_OBJ_FLUSH opcode (flush table based on its name/set)
    * Add IP_FW_OBJ_DUMP opcode (dumps table data based on its names/set)
    * Add IP_FW_OBJ_LISTSIZE / IP_FW_OBJ_LIST opcodes (get list of kernel tables)
    
    Userland changes:
    * move tables code to separate tables.c file
    * get rid of tables_max
    * switch "all"/list handling to new opcodes
    
    
    Former-commit-id: d8466a91936a974cbd1e24d58bf4b9cf913be3b7

--- sys/netinet/ip_fw.h
+++ sys/netinet/ip_fw.h
@@ -212,6 +212,28 @@ begin_comment
 comment|/* request info for one object */
 end_comment
 
+begin_define
+define|#
+directive|define
+name|IP_FW_OBJ_FLUSH
+value|94
+end_define
+
+begin_comment
+comment|/* flush data for given object */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|IP_FW_OBJ_DUMP
+value|95
+end_define
+
+begin_comment
+comment|/* dump all data for given object */
+end_comment
+
 begin_comment
 comment|/*  * The kernel representation of ipfw rules is made of a list of  * 'instructions' (for all practical purposes equivalent to BPF  * instructions), which specify which fields of the packet  * (or its metadata) should be analysed.  *  * Each instruction is stored in a structure which begins with  * "ipfw_insn", and can contain extra fields depending on the  * instruction type (listed below).  * Note that the code is written so that individual instructions  * have a size which is a multiple of 32 bits. This means that, if  * such structures contain pointers or other 64-bit entities,  * (there is just one instance now) they may end up unaligned on  * 64-bit architectures, so the must be handled with care.  *  * "enum ipfw_opcodes" are the opcodes supported. We can have up  * to 256 different opcodes. When adding new opcodes, they should  * be appended to the end of the opcode list before O_LAST_OPCODE,  * this will prevent the ABI from being broken, otherwise users  * will have to recompile ipfw(8) when they update the kernel.  */
 end_comment
@@ -1869,7 +1891,7 @@ value|1
 end_define
 
 begin_comment
-comment|/* IP_FW_OBJ_DEL, IP_FW_OBJ_INFO (followed by ipfw_xtable_info)  */
+comment|/*  * IP_FW_OBJ_DEL, IP_FW_OBJ_INFO (followed by ipfw_xtable_info),  * IP_FW_OBJ_DUMP (followed by ipfw_xtable_info and ipfw_table_xentry'xN )  */
 end_comment
 
 begin_typedef
