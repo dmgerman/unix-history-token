f01eadd4ff96742732710480b83f85dbd88e0d03
Hans Petter Selasky <hselasky@FreeBSD.org>
2014-06-16 20:43:43 +0000

    MFC r254120, r257862 and r267395: - Fix out of range shifting bug in bitops.h. - Make code a bit easier to read by adding parenthesis.
    Approved by:	re, gjb @
    
    
    Former-commit-id: 61d17ab526b7cb8301c5614e54e52efd2a7d197c

--- sys/ofed/include/linux/bitops.h
+++ sys/ofed/include/linux/bitops.h
@@ -1232,8 +1232,8 @@ end_function
 begin_define
 define|#
 directive|define
-name|NBINT
-value|(NBBY * sizeof(int))
+name|NBLONG
+value|(NBBY * sizeof(long))
 end_define
 
 begin_define
@@ -1246,7 +1246,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|atomic_set_int(&((volatile int *)(a))[(i)/NBINT], 1<< (i) % NBINT)
+value|atomic_set_long(&((volatile long *)(a))[(i)/NBLONG], 1UL<< ((i) % NBLONG))
 end_define
 
 begin_define
@@ -1259,7 +1259,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|atomic_clear_int(&((volatile int *)(a))[(i)/NBINT], 1<< (i) % NBINT)
+value|atomic_clear_long(&((volatile long *)(a))[(i)/NBLONG], 1UL<< ((i) % NBLONG))
 end_define
 
 begin_define
@@ -1272,7 +1272,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|!!(atomic_load_acq_int(&((volatile int *)(a))[(i)/NBINT])& 1<< ((i) % NBINT))
+value|!!(atomic_load_acq_long(&((volatile long *)(a))[(i)/NBLONG])&	\     (1UL<< ((i) % NBLONG)))
 end_define
 
 begin_function
@@ -1292,11 +1292,35 @@ block|{
 name|long
 name|val
 decl_stmt|;
+name|var
+operator|+=
+name|bit
+operator|/
+operator|(
+sizeof|sizeof
+argument_list|(
+name|long
+argument_list|)
+operator|*
+name|NBBY
+operator|)
+expr_stmt|;
+name|bit
+operator|%=
+sizeof|sizeof
+argument_list|(
+name|long
+argument_list|)
+operator|*
+name|NBBY
+expr_stmt|;
 name|bit
 operator|=
-literal|1
+operator|(
+literal|1UL
 operator|<<
 name|bit
+operator|)
 expr_stmt|;
 do|do
 block|{
@@ -1357,11 +1381,35 @@ block|{
 name|long
 name|val
 decl_stmt|;
+name|var
+operator|+=
+name|bit
+operator|/
+operator|(
+sizeof|sizeof
+argument_list|(
+name|long
+argument_list|)
+operator|*
+name|NBBY
+operator|)
+expr_stmt|;
+name|bit
+operator|%=
+sizeof|sizeof
+argument_list|(
+name|long
+argument_list|)
+operator|*
+name|NBBY
+expr_stmt|;
 name|bit
 operator|=
-literal|1
+operator|(
+literal|1UL
 operator|<<
 name|bit
+operator|)
 expr_stmt|;
 do|do
 block|{
