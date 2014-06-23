04b6dc60dbe561a95ef4c5fdf8c0bf0ef10b6dfb
Navdeep Parhar <np@FreeBSD.org>
2014-06-23 05:39:10 +0000

    MFC r267689:
    Consider the total number of descriptors available (and not just those
    that are ready to be reclaimed) when deciding whether to resume tx after
    a stall.
    
    
    Former-commit-id: 103152fbb7303610e7e34ef6f9153c4b406698c4

--- sys/dev/cxgbe/t4_sge.c
+++ sys/dev/cxgbe/t4_sge.c
@@ -9205,6 +9205,10 @@ condition|)
 block|{
 if|if
 condition|(
+name|eq
+operator|->
+name|avail
+operator|+
 name|can_reclaim
 operator|<
 name|tx_resume_threshold
@@ -9831,6 +9835,10 @@ condition|)
 block|{
 if|if
 condition|(
+name|eq
+operator|->
+name|avail
+operator|+
 name|can_reclaim
 operator|<
 name|tx_resume_threshold
@@ -10534,6 +10542,10 @@ parameter_list|)
 block|{
 return|return
 operator|(
+name|eq
+operator|->
+name|avail
+operator|+
 name|reclaimable
 argument_list|(
 name|eq
