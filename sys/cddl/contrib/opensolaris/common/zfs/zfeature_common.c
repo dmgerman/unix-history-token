4a8b97b0e8c52b2981bfe4b1894206350bbdac75
Xin Li <delphij@FreeBSD.org>
2014-07-01 22:31:09 +0000

    MFV r268121:
    4924 LZ4 Compression for metadata
    
    illumos/illumos-gate@b8289d24d866c1af02d7007348f7f057693c15d3
    
    MFC after:	2 weeks
    
    
    Former-commit-id: e224fbf1650841ad61e1349fe46367a6d733b6ee

--- sys/cddl/contrib/opensolaris/common/zfs/zfeature_common.c
+++ sys/cddl/contrib/opensolaris/common/zfs/zfeature_common.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.  * Copyright (c) 2013, Joyent, Inc. All rights reserved.  */
+comment|/*  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.  * Copyright (c) 2013, Joyent, Inc. All rights reserved.  * Copyright (c) 2014, Nexenta Systems, Inc. All rights reserved.  */
 end_comment
 
 begin_ifdef
@@ -662,7 +662,7 @@ name|B_FALSE
 argument_list|,
 name|B_FALSE
 argument_list|,
-name|B_FALSE
+name|B_TRUE
 argument_list|,
 name|NULL
 argument_list|)
