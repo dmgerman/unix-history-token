62596872a7f5d15b98bc0bd4594a6b4ea186d188
Alexander Motin <mav@FreeBSD.org>
2014-07-07 09:37:22 +0000

    Teach ctl_add_initiator() to dynamically allocate IIDs from pool.
    If port passed negative IID value, the function will try to allocate IID
    from the pool of unused, based on passed wwpn or name arguments.  It does
    all its best to make IID unique and persistent across reconnects.
    
    This makes persistent reservation properly work for iSCSI.  Previously,
    in case of reconnects, reservation could be unexpectedly lost, or even
    migrate between intiators.
    
    
    Former-commit-id: 313f2407616616989754eab43ad285269bb7a089

--- sys/cam/ctl/ctl_frontend.h
+++ sys/cam/ctl/ctl_frontend.h
@@ -187,6 +187,27 @@ define|\
 value|static int name ## _modevent(module_t mod, int type, void *data) \ 	{ \ 		switch (type) { \ 		case MOD_LOAD: \ 			ctl_frontend_register( \ 				(struct ctl_frontend *)data); \ 			break; \ 		case MOD_UNLOAD: \ 			printf(#name " module unload - not possible for this module type\n"); \ 			return EINVAL; \ 		default: \ 			return EOPNOTSUPP; \ 		} \ 		return 0; \ 	} \ 	static moduledata_t name ## _mod = { \ 		#name, \ 		name ## _modevent, \ 		(void *)&driver \ 	}; \ 	DECLARE_MODULE(name, name ## _mod, SI_SUB_CONFIGURE, SI_ORDER_FOURTH); \ 	MODULE_DEPEND(name, ctl, 1, 1, 1); \ 	MODULE_DEPEND(name, cam, 1, 1, 1)
 end_define
 
+begin_struct
+struct|struct
+name|ctl_wwpn_iid
+block|{
+name|int
+name|in_use
+decl_stmt|;
+name|time_t
+name|last_use
+decl_stmt|;
+name|uint64_t
+name|wwpn
+decl_stmt|;
+name|char
+modifier|*
+name|name
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
 begin_comment
 comment|/*  * The ctl_frontend structure is the registration mechanism between a FETD  * (Front End Target Driver) and the CTL layer.  Here is a description of  * the fields:  *  * port_type:		  This field tells CTL what kind of front end it is  *                        dealing with.  This field serves two purposes.  *                        The first is to let CTL know whether the frontend  *                        in question is inside the main CTL module (i.e.  *                        the ioctl front end), and therefore its module  *                        reference count shouldn't be incremented.  The  *                        CTL ioctl front end should continue to use the  *                        CTL_PORT_IOCTL argument as long as it is part of  *                        the main CTL module.  The second is to let CTL  *                        know what kind of front end it is dealing with, so  *                        it can return the proper inquiry data for that  *                        particular port.  *  * num_requested_ctl_io:  This is the number of ctl_io structures that the  *			  front end needs for its pool.  This should  * 			  generally be the maximum number of outstanding  *			  transactions that the FETD can handle.  The CTL  *			  layer will add a few to this to account for  *			  ctl_io buffers queued for pending sense data.  *			  (Pending sense only gets queued if the FETD  * 			  doesn't support autosense.  e.g. non-packetized  * 			  parallel SCSI doesn't support autosense.)  *  * port_name:		  A string describing the FETD.  e.g. "LSI 1030T U320"  *			  or whatever you want to use to describe the driver.  *  *  * physical_port:	  This is the physical port number of this  * 			  particular port within the driver/hardware.  This  * 			  number is hardware/driver specific.  * virtual_port:	  This is the virtual port number of this  * 			  particular port.  This is for things like NP-IV.  *   * port_online():	  This function is called, with onoff_arg as its  *			  argument, by the CTL layer when it wants the FETD  *			  to start responding to selections on the specified  * 			  target ID.  (targ_target)  *  * port_offline():	  This function is called, with onoff_arg as its  *			  argument, by the CTL layer when it wants the FETD  * 			  to stop responding to selection on the specified  * 			  target ID.  (targ_target)  *  * onoff_arg:		  This is supplied as an argument to port_online()  *			  and port_offline().  This is specified by the  *			  FETD.  *  * lun_enable():	  This function is called, with targ_lun_arg, a target  *			  ID and a LUN ID as its arguments, by CTL when it  *			  wants the FETD to enable a particular LUN.  If the  *			  FETD doesn't really know about LUNs, it should  *			  just ignore this call and return 0.  If the FETD  *			  cannot enable the requested LUN for some reason, the  *			  FETD should return non-zero status.  *  * lun_disable():	  This function is called, with targ_lun_arg, a target  *			  ID and LUN ID as its arguments, by CTL when it  *			  wants the FETD to disable a particular LUN.  If the  *			  FETD doesn't really know about LUNs, it should just  *			  ignore this call and return 0.  If the FETD cannot  *			  disable the requested LUN for some reason, the  *			  FETD should return non-zero status.  *  * targ_lun_arg:	  This is supplied as an argument to the targ/lun  *			  enable/disable() functions.  This is specified by  *			  the FETD.  *  * fe_datamove():	  This function is called one or more times per I/O  *			  by the CTL layer to tell the FETD to initiate a  *			  DMA to or from the data buffer(s) specified by  * 			  the passed-in ctl_io structure.  *  * fe_done():	  	  This function is called by the CTL layer when a  *			  particular SCSI I/O or task management command has  * 			  completed.  For SCSI I/O requests (CTL_IO_SCSI),  *			  sense data is always supplied if the status is  *			  CTL_SCSI_ERROR and the SCSI status byte is  *			  SCSI_STATUS_CHECK_COND.  If the FETD doesn't  *			  support autosense, the sense should be queued  *			  back to the CTL layer via ctl_queue_sense().  *  * fe_dump():		  This function, if it exists, is called by CTL  *			  to request a dump of any debugging information or  *			  state to the console.  *  * max_targets:		  The maximum number of targets that we can create  *			  per-port.  *  * max_target_id:	  The highest target ID that we can use.  *  * targ_port:		  The CTL layer assigns a "port number" to every  *			  FETD.  This port number should be passed back in  *			  in the header of every ctl_io that is queued to  *			  the CTL layer.  This enables us to determine  *			  which bus the command came in on.  *  * ctl_pool_ref:	  Memory pool reference used by the FETD in calls to  * 			  ctl_alloc_io().  *  * max_initiators:	  Maximum number of initiators that the FETD is  *			  allowed to have.  Initiators should be numbered  *			  from 0 to max_initiators - 1.  This value will  * 			  typically be 16, and thus not a problem for  * 			  parallel SCSI.  This may present issues for Fibre  *			  Channel.  *  * wwnn			  World Wide Node Name to be used by the FETD.  *			  Note that this is set *after* registration.  It  * 			  will be set prior to the online function getting  * 			  called.  *  * wwpn			  World Wide Port Name to be used by the FETD.  *			  Note that this is set *after* registration.  It  * 			  will be set prior to the online function getting  * 			  called.  *  * status:		  Used by CTL to keep track of per-FETD state.  *  * links:		  Linked list pointers, used by CTL.  The FETD  *			  shouldn't touch this field.  */
 end_comment
@@ -302,6 +323,12 @@ name|uint32_t
 name|max_initiators
 decl_stmt|;
 comment|/* passed back to FETD */
+name|struct
+name|ctl_wwpn_iid
+modifier|*
+name|wwpn_iid
+decl_stmt|;
+comment|/* used by CTL */
 name|uint64_t
 name|wwnn
 decl_stmt|;
@@ -460,7 +487,7 @@ parameter_list|(
 name|struct
 name|ctl_port
 modifier|*
-name|fe
+name|port
 parameter_list|,
 name|int
 name|master_SC
@@ -479,7 +506,7 @@ parameter_list|(
 name|struct
 name|ctl_port
 modifier|*
-name|fe
+name|port
 parameter_list|)
 function_decl|;
 end_function_decl
@@ -577,37 +604,45 @@ function_decl|;
 end_function_decl
 
 begin_comment
-comment|/*  * This routine adds an initiator to CTL's port database.  The WWPN should  * be the FC WWPN, if available.  The targ_port field should be the same as  * the targ_port passed back from CTL in the ctl_frontend structure above.  * The iid field should be the same as the iid passed in the nexus of each  * ctl_io from this initiator.  */
+comment|/*  * This routine adds an initiator to CTL's port database.  * The iid field should be the same as the iid passed in the nexus of each  * ctl_io from this initiator.  * The WWPN should be the FC WWPN, if available.  */
 end_comment
 
 begin_function_decl
 name|int
 name|ctl_add_initiator
 parameter_list|(
+name|struct
+name|ctl_port
+modifier|*
+name|port
+parameter_list|,
+name|int
+name|iid
+parameter_list|,
 name|uint64_t
 name|wwpn
 parameter_list|,
-name|int32_t
-name|targ_port
-parameter_list|,
-name|uint32_t
-name|iid
+name|char
+modifier|*
+name|name
 parameter_list|)
 function_decl|;
 end_function_decl
 
 begin_comment
-comment|/*  * This routine will remove an initiator from CTL's port database.  The  * targ_port field should be the same as the targ_port passed back in the  * ctl_frontend structure above.  The iid field should be the same as the  * iid passed in the nexus of each ctl_io from this initiator.  */
+comment|/*  * This routine will remove an initiator from CTL's port database.  * The iid field should be the same as the iid passed in the nexus of each  * ctl_io from this initiator.  */
 end_comment
 
 begin_function_decl
 name|int
 name|ctl_remove_initiator
 parameter_list|(
-name|int32_t
-name|targ_port
+name|struct
+name|ctl_port
+modifier|*
+name|port
 parameter_list|,
-name|uint32_t
+name|int
 name|iid
 parameter_list|)
 function_decl|;
