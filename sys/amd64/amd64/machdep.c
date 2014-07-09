784ee343fb0a87637bc0e47645321332e5b1b9f1
Konstantin Belousov <kib@FreeBSD.org>
2014-07-09 21:39:40 +0000

    For safety, ensure that any consumer of the set_regs() and ptrace_set_pc() use the correct return to userspace using iret.
    The signal return, PT_CONTINUE (which in fact uses signal return path)
    set the pcb flag already.  The setcontext(2) enforces iret return when
    %rip is incorrect.  Due to this, the change is redundand, but is made
    to ensure that no path which modifies context, forgets to set
    PCB_FULL_IRET.
    
    Inspired by:	CVE-2014-4699
    Reviewed by:	jhb
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    
    Former-commit-id: 8861cda451b4f6ea43e52a06296443f92e41a89a

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -10028,6 +10028,15 @@ name|tf_rip
 operator|=
 name|addr
 expr_stmt|;
+name|set_pcb_flags
+argument_list|(
+name|td
+operator|->
+name|td_pcb
+argument_list|,
+name|PCB_FULL_IRET
+argument_list|)
+expr_stmt|;
 return|return
 operator|(
 literal|0
@@ -10644,6 +10653,7 @@ name|tf_flags
 operator|=
 name|TF_HASSEGS
 expr_stmt|;
+block|}
 name|set_pcb_flags
 argument_list|(
 name|td
@@ -10653,7 +10663,6 @@ argument_list|,
 name|PCB_FULL_IRET
 argument_list|)
 expr_stmt|;
-block|}
 return|return
 operator|(
 literal|0
