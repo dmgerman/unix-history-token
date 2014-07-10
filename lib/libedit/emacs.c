570edbef095a58746e882a653e9d86b9852722b8
Pedro Giffuni <pfg@FreeBSD.org>
2014-07-10 17:52:17 +0000

    Sync some (mostly cosmetical) changes from NetBSD
    Makefile,v 1.37
    tc1.c v 1.3
    Rename TEST/test.c tc1.c
    
    common.c,v 1.23
    pass lint on _LP64.
    
    emacs.c,v 1.22
    pass lint on _LP64.
    
    filecomplete.h,v 1.8
    mv NetBSD ID back from 1.9 as we don't
    have the widecharacter support.
    
    prompt.c,v 1.14
    prompt.h,v 1.9
    term.h,v 1.20
    read.h,v 1.6
    Update NetBSD version strings
    
    sys.h,v 1.12
    Misc sun stuff.
    
    tty.c 1.31
    handle EINTR in the termios operations
    Allow a single process to control multiple ttys (for pthreads using _REENTRANT)
    using multiple EditLine objects.
    pass lint on _LP64.
    Don't depend on side effects inside an assert
    
    MFC after:	1 week
    Obtained from:	NetBSD
    
    
    Former-commit-id: 216dde045eb7dc059b5be90aedcd3c1fd17dd057

--- lib/libedit/emacs.c
+++ lib/libedit/emacs.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Christos Zoulas of Cornell University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$NetBSD: emacs.c,v 1.21 2006/03/06 21:11:56 christos Exp $  */
+comment|/*-  * Copyright (c) 1992, 1993  *	The Regents of the University of California.  All rights reserved.  *  * This code is derived from software contributed to Berkeley by  * Christos Zoulas of Cornell University.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *	$NetBSD: emacs.c,v 1.22 2009/02/15 21:55:23 christos Exp $  */
 end_comment
 
 begin_if
@@ -332,6 +332,10 @@ name|c_delafter
 argument_list|(
 name|el
 argument_list|,
+call|(
+name|int
+call|)
+argument_list|(
 name|cp
 operator|-
 name|el
@@ -339,6 +343,7 @@ operator|->
 name|el_line
 operator|.
 name|cursor
+argument_list|)
 argument_list|)
 expr_stmt|;
 comment|/* delete after dot */
@@ -490,6 +495,10 @@ name|c_insert
 argument_list|(
 name|el
 argument_list|,
+call|(
+name|int
+call|)
+argument_list|(
 name|el
 operator|->
 name|el_chared
@@ -505,6 +514,7 @@ operator|.
 name|c_kill
 operator|.
 name|buf
+argument_list|)
 argument_list|)
 expr_stmt|;
 comment|/* copy the chars */
@@ -785,6 +795,10 @@ name|c_delafter
 argument_list|(
 name|el
 argument_list|,
+call|(
+name|int
+call|)
+argument_list|(
 name|cp
 operator|-
 name|el
@@ -792,6 +806,7 @@ operator|->
 name|el_line
 operator|.
 name|cursor
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
@@ -851,6 +866,10 @@ name|c_delbefore
 argument_list|(
 name|el
 argument_list|,
+call|(
+name|int
+call|)
+argument_list|(
 name|cp
 operator|-
 name|el
@@ -860,6 +879,7 @@ operator|.
 name|c_kill
 operator|.
 name|mark
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|el
@@ -2050,9 +2070,14 @@ name|c_insert
 argument_list|(
 name|el
 argument_list|,
+call|(
+name|int
+call|)
+argument_list|(
 name|oldc
 operator|-
 name|cp
+argument_list|)
 argument_list|)
 expr_stmt|;
 for|for
