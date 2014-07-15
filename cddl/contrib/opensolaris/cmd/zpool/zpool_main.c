d644ab0a0b9193f9cce253cbc0d7ea791f72c901
Xin Li <delphij@FreeBSD.org>
2014-07-15 22:44:04 +0000

    MFV r268714:
    Improve extreme rewind import.
    
    When doing an "extreme rewind" import ("zpool import -XF"), we attempt
    to verify all data in the pool, essentially scrubbing the entire pool.
    The problem is that spa_load_verify_cb() issues an unbounded number of
    concurrent scrub i/os.  This can lead to all of memory being used for
    these zio's, wedging the system. Like normal scrub, we need to put a
    cap on the number of outstanding i/os, and have the traverse thread
    block when we reach this cap.
    
    For this purpose the cap can be very large (10,000) to optimize the
    elevator algorithm.  Three kernel tunables have been added:
    
    	vfs.zfs.spa_load_verify_maxinflight
    	vfs.zfs.spa_load_verify_metadata
    	vfs.zfs.spa_load_verify_data
    
    The latter two tunables controls whether metadata and/or user data
    when doing extreme rewind.
    
    Make 'zpool import -T' imply scrub.
    
    Make zpool import -T <txg> accept hexadecimal values for the txg when
    prefixed with 0x.
    
    Skip txg's for which there is no uberblock when doing extreme rewind.
    
    Skip reading all user data twice by skipping prefetches when doing
    extreme rewinds as we do not access via the ARC.
    
    Illumos issues:
      4970 need controls on i/o issued by zpool import -XF
      4971 zpool import -T should accept hex values
      4972 zpool import -T implies extreme rewind, and thus a scrub
      4973 spa_load_retry retries the same txg
      4974 spa_load_verify() reads all data twice
    
    MFC after:	2 weeks
    
    
    Former-commit-id: dddc1140d7cffd84e462a600a46cc7c66078430b

--- cddl/contrib/opensolaris/cmd/zpool/zpool_main.c
+++ cddl/contrib/opensolaris/cmd/zpool/zpool_main.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2012 by Frederik Wessels. All rights reserved.  * Copyright (c) 2012 Martin Matuska<mm@FreeBSD.org>. All rights reserved.  * Copyright (c) 2013 by Prasad Joshi (sTec). All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc. All rights reserved.  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.  * Copyright (c) 2012 by Frederik Wessels. All rights reserved.  * Copyright (c) 2012 Martin Matuska<mm@FreeBSD.org>. All rights reserved.  * Copyright (c) 2013 by Prasad Joshi (sTec). All rights reserved.  */
 end_comment
 
 begin_include
@@ -8926,7 +8926,7 @@ argument_list|,
 operator|&
 name|endptr
 argument_list|,
-literal|10
+literal|0
 argument_list|)
 expr_stmt|;
 if|if
