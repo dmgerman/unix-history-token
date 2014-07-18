53a806106863de87dcafdc7b3cdfb3f2556b5407
Navdeep Parhar <np@FreeBSD.org>
2014-07-18 00:31:06 +0000

    MFC r268706: cxgbe(4): Display CF facility correctly in the device log.
    Former-commit-id: 76d6a8a0caddce6e2489e35e03dad06b7dd5a453

--- sys/dev/cxgbe/t4_main.c
+++ sys/dev/cxgbe/t4_main.c
@@ -29334,6 +29334,12 @@ operator|=
 literal|"CORE"
 block|,
 index|[
+name|FW_DEVLOG_FACILITY_CF
+index|]
+operator|=
+literal|"CF"
+block|,
+index|[
 name|FW_DEVLOG_FACILITY_SCHED
 index|]
 operator|=
