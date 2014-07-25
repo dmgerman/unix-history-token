3e52528af1acf1c8d97b42936cc65201bb30a760
Edward Tomasz Napierala <trasz@FreeBSD.org>
2014-07-25 11:29:45 +0000

    Fix ctl(4) kldload failure that manifested like this:
    link_elf_obj: symbol icl_pdu_new_bhs undefined
    
    PR:		192031
    Submitted by:	Nils Beyer (earlier version)
    MFC after:	3 days
    Sponsored by:	FreeBSD Foundation
    
    Former-commit-id: 30da1fc7c5f5c91acc339e3891da59df33860f74

--- sys/cam/ctl/ctl_frontend_iscsi.c
+++ sys/cam/ctl/ctl_frontend_iscsi.c
@@ -917,6 +917,22 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|ctlcfiscsi
+argument_list|,
+name|icl
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 begin_function
 specifier|static
 name|struct
