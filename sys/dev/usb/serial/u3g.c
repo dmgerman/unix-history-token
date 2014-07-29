70595145e46f8301ba709ca53f5bb909953b4cb5
Hans Petter Selasky <hselasky@FreeBSD.org>
2014-07-29 22:07:52 +0000

    MFC r268929: Add new device ID.
    PR:		191959
    
    Former-commit-id: 975300f0d0ab34f77a80d3d2ddf718627ca330ff

--- sys/dev/usb/serial/u3g.c
+++ sys/dev/usb/serial/u3g.c
@@ -2280,6 +2280,15 @@ name|U3G_DEV
 argument_list|(
 name|NOVATEL
 argument_list|,
+name|MC679
+argument_list|,
+literal|0
+argument_list|)
+block|,
+name|U3G_DEV
+argument_list|(
+name|NOVATEL
+argument_list|,
 name|MC950D
 argument_list|,
 literal|0
