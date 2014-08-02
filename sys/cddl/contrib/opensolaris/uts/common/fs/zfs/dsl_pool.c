5914f7a2ccc5a08a719b9cf6097cc82748c5c83f
Xin Li <delphij@FreeBSD.org>
2014-08-02 03:56:06 +0000

    MFC r268855: MFV r268848:
    Instead of asserting all zio's be properly aligned, only assert
    on the logical ones.
    
    Cap uberblocks at 8k, otherwise with ashift=17, there would be
    only one uberblock.
    
    This fixes a problem that zdb would trip assert on pools with
    ashift >= 0xe (8k).
    
    While there, also change the code so it only attempt to condense
    space map unless the uncondensed size consumes greater than
    zfs_metaslab_condense_block_threshold blocks.
    
    Illumos issue:
      4958 zdb trips assert on pools with ashift >= 0xe
    
    
    Former-commit-id: 9bc96a283c101e9e0536b69e9d702111a735aa20

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dsl_pool.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/dsl_pool.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2013 Steven Hartland. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.  * Copyright (c) 2013 Steven Hartland. All rights reserved.  */
 end_comment
 
 begin_include
