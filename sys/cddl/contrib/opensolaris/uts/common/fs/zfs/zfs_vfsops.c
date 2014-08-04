71f4647263427c469166bddf6f18007b05476fad
Konstantin Belousov <kib@FreeBSD.org>
2014-08-04 01:16:08 +0000

    MFC r269189: Initialize zfs vnode v_hash when the vnode is allocated.
    Former-commit-id: 6167ea9ff406ab6b5a328e2a158f0e6291e9cdd7

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vfsops.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_vfsops.c
@@ -9456,16 +9456,6 @@ name|vpp
 operator|=
 name|NULL
 expr_stmt|;
-else|else
-operator|(
-operator|*
-name|vpp
-operator|)
-operator|->
-name|v_hash
-operator|=
-name|ino
-expr_stmt|;
 return|return
 operator|(
 name|err
