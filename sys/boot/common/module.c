55eb4ade91c8109b72a2845b6b9c7c9d43388cbc
Marcel Moolenaar <marcel@FreeBSD.org>
2014-08-05 23:41:40 +0000

    In file_loadraw() print the name of the file as well as its size so that we know what file is being loaded and how big the file is once complete. This has ELF modules and disk images emit the same output.
    Former-commit-id: d2ad4dffe030b4ca74eaf70ff74b94b81321538c

--- sys/boot/common/module.c
+++ sys/boot/common/module.c
@@ -60,6 +60,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/stdint.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"bootstrap.h"
 end_include
 
@@ -1810,6 +1816,13 @@ argument_list|,
 name|loadaddr
 argument_list|)
 expr_stmt|;
+name|printf
+argument_list|(
+literal|"%s "
+argument_list|,
+name|name
+argument_list|)
+expr_stmt|;
 name|laddr
 operator|=
 name|loadaddr
@@ -1885,6 +1898,20 @@ operator|+=
 name|got
 expr_stmt|;
 block|}
+name|printf
+argument_list|(
+literal|"size=%#jx\n"
+argument_list|,
+call|(
+name|uintmax_t
+call|)
+argument_list|(
+name|laddr
+operator|-
+name|loadaddr
+argument_list|)
+argument_list|)
+expr_stmt|;
 comment|/* Looks OK so far; create& populate control structure */
 name|fp
 operator|=
