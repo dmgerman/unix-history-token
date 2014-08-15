a613b2d176725aa86e123178ed5ddc53f197b348
Gleb Smirnoff <glebius@FreeBSD.org>
2014-08-15 14:02:24 +0000

    pf_map_addr() can fail and in this case we should drop the packet, otherwise bad consequences including a routing loop can occur.
    Move pf_set_rt_ifp() earlier in state creation sequence and
    inline it, cutting some extra code.
    
    PR:		183997
    Submitted by:	Kajetan Staszkiewicz <vegeta tuxpowered.net>
    Sponsored by:	InnoGames GmbH
    
    Former-commit-id: ee2dd62ed6948736e3693474424d9875ca132515

--- sys/netpfil/pf/pf.h
+++ sys/netpfil/pf/pf.h
@@ -759,8 +759,19 @@ end_comment
 begin_define
 define|#
 directive|define
-name|PFRES_MAX
+name|PFRES_MAPFAILED
 value|15
+end_define
+
+begin_comment
+comment|/* pf_map_addr() failed */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|PFRES_MAX
+value|16
 end_define
 
 begin_comment
@@ -771,7 +782,7 @@ begin_define
 define|#
 directive|define
 name|PFRES_NAMES
-value|{ \ 	"match", \ 	"bad-offset", \ 	"fragment", \ 	"short", \ 	"normalize", \ 	"memory", \ 	"bad-timestamp", \ 	"congestion", \ 	"ip-option", \ 	"proto-cksum", \ 	"state-mismatch", \ 	"state-insert", \ 	"state-limit", \ 	"src-limit", \ 	"synproxy", \ 	NULL \ }
+value|{ \ 	"match", \ 	"bad-offset", \ 	"fragment", \ 	"short", \ 	"normalize", \ 	"memory", \ 	"bad-timestamp", \ 	"congestion", \ 	"ip-option", \ 	"proto-cksum", \ 	"state-mismatch", \ 	"state-insert", \ 	"state-limit", \ 	"src-limit", \ 	"synproxy", \ 	"map-failed", \ 	NULL \ }
 end_define
 
 begin_comment
