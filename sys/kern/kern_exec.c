c7e02572354602473fae507499e7fa521f70a1ed
Mateusz Guzik <mjg@FreeBSD.org>
2014-08-17 07:22:40 +0000

    MFC r268636:
    Plug p_pptr null test in do_execve. It is always true.
    
    
    Former-commit-id: 77b9a9b5ad67f5527bd06cc503c706ae6572ec18

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -2895,15 +2895,9 @@ if|if
 condition|(
 name|p
 operator|->
-name|p_pptr
-operator|&&
-operator|(
-name|p
-operator|->
 name|p_flag
 operator|&
 name|P_PPWAIT
-operator|)
 condition|)
 block|{
 name|p
