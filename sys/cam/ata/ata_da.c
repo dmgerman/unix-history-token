486a313f7c90e14ad21b14ae0653efd306874e56
Sean Bruno <sbruno@FreeBSD.org>
2014-08-21 21:05:58 +0000

    Add the Samsung 843T as a 4k enabled drive
    Submitted by:	Jason Wolfe <jason@llnw.com>
    MFC after:	2 weeks
    Sponsored by:	Limelight Networks
    
    
    Former-commit-id: 248650ab024c1f4902beab6e19b0ee36f86c831e

--- sys/cam/ata/ata_da.c
+++ sys/cam/ata/ata_da.c
@@ -1342,6 +1342,24 @@ name|ADA_Q_4K
 block|}
 block|,
 block|{
+comment|/* 		 * Samsung 843T Series SSDs 		 * 4k optimised 		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_FIXED
+block|,
+literal|"*"
+block|,
+literal|"SAMSUNG MZ7WD*"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+name|ADA_Q_4K
+block|}
+block|,
+block|{
 comment|/* 		 * SuperTalent TeraDrive CT SSDs 		 * 4k optimised& trim only works in 4k requests + 4k aligned 		 */
 block|{
 name|T_DIRECT
--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -3144,6 +3144,24 @@ name|DA_Q_4K
 block|}
 block|,
 block|{
+comment|/* 		 * Samsung 843T Series SSDs 		 * 4k optimised 		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_FIXED
+block|,
+literal|"ATA"
+block|,
+literal|"SAMSUNG MZ7WD*"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+name|DA_Q_4K
+block|}
+block|,
+block|{
 comment|/* 		 * SuperTalent TeraDrive CT SSDs 		 * 4k optimised& trim only works in 4k requests + 4k aligned 		 */
 block|{
 name|T_DIRECT
