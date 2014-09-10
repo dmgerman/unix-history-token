498c480c5d347a04bbd0169fb8accdf0d52c4fbd
John Baldwin <jhb@FreeBSD.org>
2014-09-10 21:25:54 +0000

    To workaround an errata on certain Pentium Pro CPUs, i386 disables the local APIC in initializecpu() and re-enables it if the APIC code decides to use the local APIC after all.  Rework this workaround slightly so that initializecpu() won't re-disable the local APIC if it is called after the APIC code re-enables the local APIC.
    Former-commit-id: ad51d0d89d7a0309bd909b00add4ec274d79d3f6

--- sys/i386/include/md_var.h
+++ sys/i386/include/md_var.h
@@ -584,6 +584,15 @@ end_function_decl
 
 begin_function_decl
 name|void
+name|ppro_reenable_apic
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
 name|printcpuinfo
 parameter_list|(
 name|void
