8439e7320aef43252d392416f0a47daee7db480a
Konstantin Belousov <kib@FreeBSD.org>
2014-09-17 21:04:50 +0000

    The vm_mmap_cdev() explicitely converts absence of both MAP_SHARED and MAP_PRIVATE flags to MAP_SHARED.  Apparently, some code in tree, in particular, libgeom, relied on this behaviour, see r271721.  For regular file types, the absence of the flags is interpreted as MAP_PRIVATE, and libc nlist used this (fixed in r271723).
    Allow the implicit flags for legacy binaries.  Bump __FreeBSD_version
    to get the ABI note on new binaries to check for in mmap code.
    
    Remove the test for presence of one of the MAP_ANON, MAP_SHARED or
    MAP_PRIVATE flags before fget_mmap().  For MAP_ANON, we already verify
    that passed fd == -1.  For fd != -1, test after fget_mmap() (for newer
    binaries) covers the case.
    
    Reported by:	bdrewery, pho
    Reviewed by:	jhb
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: 3cc03bdbf974127fb719e1dd97f9808fec03ccf0

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -60,7 +60,7 @@ begin_define
 define|#
 directive|define
 name|__FreeBSD_version
-value|1100035
+value|1100036
 end_define
 
 begin_comment
@@ -108,6 +108,13 @@ define|#
 directive|define
 name|P_OSREL_MAP_ANON
 value|800104
+end_define
+
+begin_define
+define|#
+directive|define
+name|P_OSREL_MAP_FSTRICT
+value|1100036
 end_define
 
 begin_define
