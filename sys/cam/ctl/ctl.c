ce3aceffad6133031e00f993793030aeea479ec3
Alexander Motin <mav@FreeBSD.org>
2014-09-22 10:05:36 +0000

    Don't try to continue aborted commands if status was not set.
    Former-commit-id: dabf2b597e761dd635d6217ffd01c53af946f375

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -23539,6 +23539,17 @@ name|CTL_FLAG_IO_CONT
 operator|)
 operator|&&
 operator|(
+name|io
+operator|->
+name|io_hdr
+operator|.
+name|flags
+operator|&
+name|CTL_FLAG_ABORT
+operator|)
+operator|==
+literal|0
+operator|&&
 operator|(
 operator|(
 name|io
@@ -23551,9 +23562,7 @@ name|CTL_STATUS_MASK
 operator|)
 operator|==
 name|CTL_STATUS_NONE
-operator|)
 operator|||
-operator|(
 operator|(
 name|io
 operator|->
@@ -23565,7 +23574,6 @@ name|CTL_STATUS_MASK
 operator|)
 operator|==
 name|CTL_SUCCESS
-operator|)
 operator|)
 condition|)
 block|{
