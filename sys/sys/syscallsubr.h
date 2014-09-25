c98760a62ae33bc24fe0a6298f90764c2e374b03
Konstantin Belousov <kib@FreeBSD.org>
2014-09-25 21:07:19 +0000

    Fix fcntl(2) compat32 after r270691.  The copyin and copyout of the struct flock are done in the sys_fcntl(), which mean that compat32 used direct access to userland pointers.
    Move code from sys_fcntl() to new wrapper, kern_fcntl_freebsd(), which
    performs neccessary userland memory accesses, and use it from both
    native and compat32 fcntl syscalls.
    
    Reported by:	jhibbits
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 days
    
    
    Former-commit-id: 13f27537a17a49b46c2599d3285147c8e5371b1b

--- sys/sys/syscallsubr.h
+++ sys/sys/syscallsubr.h
@@ -727,6 +727,27 @@ end_function_decl
 
 begin_function_decl
 name|int
+name|kern_fcntl_freebsd
+parameter_list|(
+name|struct
+name|thread
+modifier|*
+name|td
+parameter_list|,
+name|int
+name|fd
+parameter_list|,
+name|int
+name|cmd
+parameter_list|,
+name|long
+name|arg
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
 name|kern_fhstat
 parameter_list|(
 name|struct
