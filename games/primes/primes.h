bb76c13abc420bcbe2745b52c3f9435bd873ae3e
Colin Percival <cperciva@FreeBSD.org>
2014-09-26 09:40:48 +0000

    Correctly enumerate primes between 4295098369 and 3825123056546413050.
    Prior to this commit, primes(6) relied solely on sieving with primes up
    to 65537, with the effect that composite numbers which are the product
    of two non-16-bit primes would be incorrectly identified as prime.  For
    example,
    # primes 1099511627800 1099511627820
    would output
    1099511627803
    1099511627807
    1099511627813
    when in fact only the first of those values is prime.
    
    This commit adds strong pseudoprime tests to validate the candidates
    which pass the initial sieving stage, using bases of 2, 3, 5, 7, 11,
    13, 17, 19, and 23.  Thanks to papers from C. Pomerance, J.L. Selfridge,
    and S.S. Wagstaff, Jr.; G. Jaeschke; and Y. Jiang and Y. Deng, we know
    that the smallest value which passes these tests is 3825123056546413051.
    
    At present we do not know how many strong pseudoprime tests are required
    to prove primality for values larger than 3825123056546413050, so we
    force primes(6) to stop at that point.
    
    Reviewed by:	jmg
    Relnotes:	primes(6) now correctly enumerates primes up to
    		3825123056546413050
    MFC after:	7 days
    Sponsored by:	EuroBSDCon devsummit
    
    Former-commit-id: aaefc1fc7fe412ad22d9438b7003ed9c86ea345e

--- games/primes/primes.h
+++ games/primes/primes.h
@@ -73,6 +73,17 @@ comment|/* largest prime in the prime array */
 end_comment
 
 begin_comment
+comment|/* Maximum size sieving alone can handle. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SIEVEMAX
+value|4295098368ULL
+end_define
+
+begin_comment
 comment|/*  * To avoid excessive sieves for small factors, we use the table below to  * setup our sieve blocks.  Each element represents an odd number starting  * with 1.  All non-zero elements are factors of 3, 5, 7, 11 and 13.  */
 end_comment
 
@@ -96,6 +107,30 @@ end_decl_stmt
 begin_comment
 comment|/* length of pattern array */
 end_comment
+
+begin_comment
+comment|/* Test for primality using strong pseudoprime tests. */
+end_comment
+
+begin_function_decl
+name|int
+name|isprime
+parameter_list|(
+name|ubig
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_comment
+comment|/* Maximum value which the SPSP code can handle. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|SPSPMAX
+value|3825123056546413050ULL
+end_define
 
 end_unit
 
