ae06bbf7831a196bba211ded7c3ba372cbdc5ae2
Alexander Motin <mav@FreeBSD.org>
2014-10-06 12:41:07 +0000

    MFC r271358: Fix array overrun, reported by Coverity.
    Former-commit-id: 542c8f5ef71e55ef077f768414160ae1a890af2e

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -35665,9 +35665,12 @@ name|softc
 operator|->
 name|ctl_ports
 index|[
+name|ctl_port_idx
+argument_list|(
 name|i
 operator|/
 name|CTL_MAX_INIT_PER_PORT
+argument_list|)
 index|]
 expr_stmt|;
 if|if
