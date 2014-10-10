4d6957691fe486c89ce67d14d79e1d708d105fbe
Garrett Cooper <ngie@FreeBSD.org>
2014-10-10 18:59:18 +0000

    Add missing #include <sys/time.h> for gettimeofday
    Sponsored by: EMC / Isilon Storage Division
    
    
    Former-commit-id: c4ba09b2a3693d26561b0599d171bfe6f37d1f2b

--- contrib/netbsd-tests/lib/libc/gen/t_time.c
+++ contrib/netbsd-tests/lib/libc/gen/t_time.c
@@ -21,6 +21,26 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__FreeBSD__
+argument_list|)
+end_if
+
+begin_include
+include|#
+directive|include
+file|<sys/time.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
