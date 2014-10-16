dcb4706b849ff6dce7d76fde4d9c11cf741020ba
Sean Bruno <sbruno@FreeBSD.org>
2014-10-16 20:33:04 +0000

    Add 4k quirks for PM853T Samsung SSD
    MFC after:	2 weeks
    Sponsored by:	Limelight Networks
    
    
    Former-commit-id: 82ddf7d65f02549977c22971ef17e7c247bf8cdb

--- sys/cam/ata/ata_da.c
+++ sys/cam/ata/ata_da.c
@@ -1360,6 +1360,24 @@ name|ADA_Q_4K
 block|}
 block|,
 block|{
+comment|/* 		 * Samsung PM853T Series SSDs 		 * 4k optimised 		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_FIXED
+block|,
+literal|"*"
+block|,
+literal|"SAMSUNG MZ7GE*"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+name|ADA_Q_4K
+block|}
+block|,
+block|{
 comment|/* 		 * SuperTalent TeraDrive CT SSDs 		 * 4k optimised& trim only works in 4k requests + 4k aligned 		 */
 block|{
 name|T_DIRECT
