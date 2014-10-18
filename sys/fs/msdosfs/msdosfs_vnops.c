0597f63a097e0969eca56869e627ecea8b181510
Konstantin Belousov <kib@FreeBSD.org>
2014-10-18 15:31:53 +0000

    MFC r272952: Do not set IN_ACCESS flag for read-only mounts.
    Former-commit-id: 30afa5dcfff8a268c2907522bf8d0c467586ccd3

--- sys/fs/msdosfs/msdosfs_vnops.c
+++ sys/fs/msdosfs/msdosfs_vnops.c
@@ -2857,7 +2857,11 @@ name|v_mount
 operator|->
 name|mnt_flag
 operator|&
+operator|(
 name|MNT_NOATIME
+operator||
+name|MNT_RDONLY
+operator|)
 operator|)
 operator|==
 literal|0
--- sys/ufs/ffs/ffs_vnops.c
+++ sys/ufs/ffs/ffs_vnops.c
@@ -2503,7 +2503,11 @@ name|v_mount
 operator|->
 name|mnt_flag
 operator|&
+operator|(
 name|MNT_NOATIME
+operator||
+name|MNT_RDONLY
+operator|)
 operator|)
 operator|==
 literal|0
