20eafdb5dc3ec6f711100973530999e95efd1a04
Garrett Cooper <ngie@FreeBSD.org>
2014-10-21 20:17:05 +0000

    Add sys/socket.h #include for bind(2), et al
    Sponsored by: EMC / Isilon Storage Division
    
    
    Former-commit-id: 8adb702309be0db54f6f1b487d5da0748675dc10

--- contrib/netbsd-tests/lib/libc/sys/t_connect.c
+++ contrib/netbsd-tests/lib/libc/sys/t_connect.c
@@ -49,6 +49,23 @@ directive|include
 file|<atf-c.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/socket.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_expr_stmt
 name|ATF_TC
 argument_list|(
