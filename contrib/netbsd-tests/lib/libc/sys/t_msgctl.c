b0f70a710808daa8388999ee450d307d95f126f6
Garrett Cooper <ngie@FreeBSD.org>
2014-10-23 07:22:18 +0000

    Add limits.h #include for INT_MAX
    Sponsored by: EMC / Isilon Storage Division
    
    
    Former-commit-id: 30ff990b7c17674c91ba295793ab0ce7f81d3b4b

--- contrib/netbsd-tests/lib/libc/sys/t_msgctl.c
+++ contrib/netbsd-tests/lib/libc/sys/t_msgctl.c
@@ -99,6 +99,23 @@ directive|include
 file|<unistd.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<limits.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_define
 define|#
 directive|define
