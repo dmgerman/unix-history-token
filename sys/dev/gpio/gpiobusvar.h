3be314836e6824f44f01d94b887f9b612cb7c6c7
Luiz Otavio O Souza <loos@FreeBSD.org>
2014-10-23 23:12:30 +0000

    Provide a working GPIOBUS_IVAR() macro for FDT systems.
    Former-commit-id: 59a2f5632fb84a98b3d2fca814581493d11a6878

--- sys/dev/gpio/gpiobusvar.h
+++ sys/dev/gpio/gpiobusvar.h
@@ -56,6 +56,27 @@ directive|include
 file|"gpio_if.h"
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|FDT
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|GPIOBUS_IVAR
+parameter_list|(
+name|d
+parameter_list|)
+value|(struct gpiobus_ivar *)				\&((struct ofw_gpiobus_devinfo *)device_get_ivars(d))->opd_dinfo
+end_define
+
+begin_else
+else|#
+directive|else
+end_else
+
 begin_define
 define|#
 directive|define
@@ -65,6 +86,11 @@ name|d
 parameter_list|)
 value|(struct gpiobus_ivar *) device_get_ivars(d)
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
