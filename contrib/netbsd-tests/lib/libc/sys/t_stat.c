9515e955d9109a8f25d934f8fd69688948d626f7
Garrett Cooper <ngie@FreeBSD.org>
2014-10-23 05:46:10 +0000

    Add netinet/in.h for struct sockaddr_in
    Sponsored by: EMC / Isilon Storage Division
    
    
    Former-commit-id: 666a062971378a22ce75f7463745c3633561ef4b

--- contrib/netbsd-tests/lib/libc/sys/t_stat.c
+++ contrib/netbsd-tests/lib/libc/sys/t_stat.c
@@ -93,6 +93,26 @@ directive|include
 file|<stdio.h>
 end_include
 
+begin_if
+if|#
+directive|if
+name|defined
+argument_list|(
+name|__FreeBSD__
+argument_list|)
+end_if
+
+begin_include
+include|#
+directive|include
+file|<netinet/in.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_decl_stmt
 specifier|static
 specifier|const
