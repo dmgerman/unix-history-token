9a9c36f1448e31a115d8e2d5d77f9a47c43bfd4d
Sean Bruno <sbruno@FreeBSD.org>
2014-10-29 22:17:45 +0000

    MFC r272720, 273061, 273062, 273063, 273064
    Implement PLPMTUD blackhole detection (RFC 4821), inspired by code
    from xnu sources.  If we encounter a network where ICMP is blocked
    the Needs Frag indicator may not propagate back to us.  Attempt to
    downshift the mss once to a preconfigured value.
    
    Note, this is turned off by default.
    
    
    Former-commit-id: 86b6d141e26d20da66adef64f2dd30eb5e3249f2

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -619,13 +619,21 @@ name|u_int
 name|t_tsomax
 decl_stmt|;
 comment|/* tso burst length limit */
+name|u_int
+name|t_pmtud_saved_maxopd
+decl_stmt|;
+comment|/* pre-blackhole MSS */
+name|u_int
+name|t_flags2
+decl_stmt|;
+comment|/* More tcpcb flags storage */
 name|uint32_t
 name|t_ispare
 index|[
-literal|8
+literal|6
 index|]
 decl_stmt|;
-comment|/* 5 UTO, 3 TBD */
+comment|/* 5 UTO, 1 TBD */
 name|void
 modifier|*
 name|t_pspare2
@@ -1149,6 +1157,43 @@ end_endif
 
 begin_comment
 comment|/* TCP_SIGNATURE */
+end_comment
+
+begin_comment
+comment|/*  * Flags for PLPMTU handling, t_flags2  */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TF2_PLPMTU_BLACKHOLE
+value|0x00000001
+end_define
+
+begin_comment
+comment|/* Possible PLPMTUD Black Hole. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TF2_PLPMTU_PMTUD
+value|0x00000002
+end_define
+
+begin_comment
+comment|/* Allowed to attempt PLPMTUD. */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|TF2_PLPMTU_MAXSEGSNT
+value|0x00000004
+end_define
+
+begin_comment
+comment|/* Last seg sent was full seg. */
 end_comment
 
 begin_comment
