ebc0db33e34ebe37b3660f70c1b6327ff25b4473
Ian Lepore <ian@FreeBSD.org>
2014-11-15 05:40:20 +0000

    Whitespace and comment tweaks, no functional changes.
    Former-commit-id: bfd19851fa723f9fca008f8c22cef6e2f60625e1

--- sys/arm/arm/busdma_machdep-v6.c
+++ sys/arm/arm/busdma_machdep-v6.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2012 Ian Lepore  * Copyright (c) 2010 Mark Tinguely  * Copyright (c) 2004 Olivier Houchard  * Copyright (c) 2002 Peter Grehan  * Copyright (c) 1997, 1998 Justin T. Gibbs.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *  From i386/busdma_machdep.c 191438 2009-04-23 20:24:19Z jhb  */
+comment|/*-  * Copyright (c) 2012-2014 Ian Lepore  * Copyright (c) 2010 Mark Tinguely  * Copyright (c) 2004 Olivier Houchard  * Copyright (c) 2002 Peter Grehan  * Copyright (c) 1997, 1998 Justin T. Gibbs.  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions, and the following disclaimer,  *    without modification, immediately at the beginning of the file.  * 2. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  *  From i386/busdma_machdep.c 191438 2009-04-23 20:24:19Z jhb  */
 end_comment
 
 begin_include
@@ -5521,7 +5521,7 @@ name|notyetbounceuser
 end_ifdef
 
 begin_comment
-comment|/* If busdma uses user pages, then the interrupt handler could 	 * be use the kernel vm mapping. Both bounce pages and sync list 	 * do not cross page boundaries. 	 * Below is a rough sequence that a person would do to fix the 	 * user page reference in the kernel vmspace. This would be 	 * done in the dma post routine. 	 */
+comment|/* If busdma uses user pages, then the interrupt handler could  * be use the kernel vm mapping. Both bounce pages and sync list  * do not cross page boundaries.  * Below is a rough sequence that a person would do to fix the  * user page reference in the kernel vmspace. This would be  * done in the dma post routine.  */
 end_comment
 
 begin_function
@@ -5550,7 +5550,7 @@ decl_stmt|;
 name|vm_offset_t
 name|va
 decl_stmt|;
-comment|/* each synclist entry is contained within a single page. 		 * 		 * this would be needed if BUS_DMASYNC_POSTxxxx was implemented 		*/
+comment|/*  	 * each synclist entry is contained within a single page. 	 * this would be needed if BUS_DMASYNC_POSTxxxx was implemented 	 */
 name|curaddr
 operator|=
 name|pmap_extract
