1f165d3972ae470626cbe02e9e4dc19985f73cda
Garrett Cooper <ngie@FreeBSD.org>
2014-11-16 20:37:46 +0000

    Alias isinff to isinf on FreeBSD
    isinf on FreeBSD automatically picks the appropriate type per math.h
    
    Sponsored by: EMC / Isilon Storage Division
    
    
    Former-commit-id: 3c0051575eefb61f021e8ce43723230c302202a9

--- contrib/netbsd-tests/lib/libm/t_pow.c
+++ contrib/netbsd-tests/lib/libm/t_pow.c
@@ -33,6 +33,24 @@ directive|include
 file|<math.h>
 end_include
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+end_ifdef
+
+begin_define
+define|#
+directive|define
+name|isinff
+value|isinf
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_comment
 comment|/*  * pow(3)  */
 end_comment
