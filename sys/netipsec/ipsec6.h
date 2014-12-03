5e4facde37f89ca8c92dd022dd388c0ed1b284eb
Andrey V. Elsukov <ae@FreeBSD.org>
2014-12-03 04:08:41 +0000

    Remove __P() macro.
    Suggested by:	kevlo
    Sponsored by:	Yandex LLC
    
    
    Former-commit-id: b21833d15136fc0ae488f513c058b9fef534d9af

--- sys/netipsec/ipsec6.h
+++ sys/netipsec/ipsec6.h
@@ -164,24 +164,21 @@ name|inpcb
 struct_decl|;
 end_struct_decl
 
-begin_decl_stmt
+begin_function_decl
 specifier|extern
 name|int
 name|ipsec6_in_reject
-name|__P
-argument_list|(
-operator|(
-expr|struct
+parameter_list|(
+name|struct
 name|mbuf
-operator|*
-operator|,
-expr|struct
+modifier|*
+parameter_list|,
+name|struct
 name|inpcb
-operator|*
-operator|)
-argument_list|)
-decl_stmt|;
-end_decl_stmt
+modifier|*
+parameter_list|)
+function_decl|;
+end_function_decl
 
 begin_struct_decl
 struct_decl|struct
