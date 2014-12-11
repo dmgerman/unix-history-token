afe984f279f1cb03b24190766ebad7b68828924b
Alexander Motin <mav@FreeBSD.org>
2014-12-11 00:25:26 +0000

    MFC r275478: Swap resource count scopes for used/available space.
    Used count should be reported as per-LUN, while available should not.
    
    
    Former-commit-id: 4fdcbb8db0c628748c0a3f584232dc812432f824

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -32619,9 +32619,9 @@ index|[
 literal|4
 index|]
 operator|=
-literal|0x01
+literal|0x02
 expr_stmt|;
-comment|/* per-LUN */
+comment|/* per-pool */
 name|data
 operator|+=
 name|phdr
@@ -32719,9 +32719,9 @@ index|[
 literal|4
 index|]
 operator|=
-literal|0x02
+literal|0x01
 expr_stmt|;
-comment|/* per-pool */
+comment|/* per-LUN */
 name|data
 operator|+=
 name|phdr
