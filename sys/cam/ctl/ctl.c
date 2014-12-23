e2760da3191d3921cc96f52e0fff7bdfc10a53bb
Alexander Motin <mav@FreeBSD.org>
2014-12-23 12:45:29 +0000

    MFC r275959: Report initiator id in portlist XML in more formalized way.
    Former-commit-id: 5669082dbda88b3b9533b359b14bf4797afa3b08

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -16663,7 +16663,7 @@ name|sbuf_printf
 argument_list|(
 name|sb
 argument_list|,
-literal|"\t<initiator>%u %s</initiator>\n"
+literal|"\t<initiator id=\"%u\">%s</initiator>\n"
 argument_list|,
 name|j
 argument_list|,
@@ -16684,7 +16684,7 @@ name|sbuf_printf
 argument_list|(
 name|sb
 argument_list|,
-literal|"\t<initiator>%u naa.%08jx</initiator>\n"
+literal|"\t<initiator id=\"%u\">naa.%08jx</initiator>\n"
 argument_list|,
 name|j
 argument_list|,
