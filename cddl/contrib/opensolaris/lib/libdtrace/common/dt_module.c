f43f7dc9f3f4f18b2e8dd72f257beb3f8cb740f0
Mark Johnston <markj@FreeBSD.org>
2014-12-26 22:49:27 +0000

    MFC r275011: The module load address always needs to be included when setting the dm_*_va fields of dt_module_t. Previously, this was only done on architectures where kernel modules have type ET_REL; this change fixes that. As a result, symbol name resolution in the stack() action now works properly for kernel modules on i386.
    Former-commit-id: ea3f963ac023c0e72d3fec8df53de98330b03136

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_module.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_module.c
@@ -5346,9 +5346,6 @@ condition|)
 continue|continue;
 if|if
 condition|(
-name|is_elf_obj
-operator|&&
-operator|(
 name|sh
 operator|.
 name|sh_type
@@ -5360,7 +5357,6 @@ operator|.
 name|sh_type
 operator|==
 name|SHT_NOBITS
-operator|)
 condition|)
 block|{
 name|alignmask
@@ -5386,6 +5382,10 @@ name|sh_addr
 operator|=
 name|mapbase
 expr_stmt|;
+if|if
+condition|(
+name|is_elf_obj
+condition|)
 name|dmp
 operator|->
 name|dm_sec_offsets
