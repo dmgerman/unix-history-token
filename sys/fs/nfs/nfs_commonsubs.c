0d4ccedeebdc138d67204bc9d067c433d9bee172
Peter Jeremy <peterj@FreeBSD.org>
2014-12-28 07:16:48 +0000

    MFH r275941: Adjust the test of a KASSERT to better match the intent.
    This assertion was added in r246213 as a guard against corrupted mbufs
    arriving from drivers, the key distinguishing factor of said mbufs being
    that they had a negative length. Given we're in a while loop specifically
    designed to skip over zero-length mbufs, panicking on a zero-length mbuf
    seems incorrect.
    
    Suggested by:	rmacklem
    MFH go-ahead:	benno
    Approved by:	grog (co-mentor)
    
    
    Former-commit-id: 06411f84936cf4207050768ab67db88adaac2508

--- sys/fs/nfs/nfs_commonsubs.c
+++ sys/fs/nfs/nfs_commonsubs.c
@@ -1320,11 +1320,11 @@ expr_stmt|;
 name|KASSERT
 argument_list|(
 name|len
-operator|>
+operator|>=
 literal|0
 argument_list|,
 operator|(
-literal|"len %d"
+literal|"len %d, corrupted mbuf?"
 operator|,
 name|len
 operator|)
