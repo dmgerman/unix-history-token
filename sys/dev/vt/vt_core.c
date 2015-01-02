b81eac0b87bcce51214edb2702ce987364d54b45
Hans Petter Selasky <hselasky@FreeBSD.org>
2015-01-02 13:35:10 +0000

    The "vt_suspend_flush_timer()" function is sometimes called locked which prevents us from doing a "callout_drain()" call. The callout in question has a lock associated with it and we are not freeing the callout. That means we can use the "callout_stop()" function to atomically stop the callback iff the "callout_stop()" function is called locked. This patch applies proper locking to "callout_stop()" and replaces a "callout_drain()" with a "callout_stop()".
    MFC after:    1 week
    
    
    Former-commit-id: 58d3b25dac08f34b2fe2510931f3cc3b44b29c07

--- sys/dev/vt/vt_core.c
+++ sys/dev/vt/vt_core.c
@@ -392,6 +392,18 @@ end_define
 begin_define
 define|#
 directive|define
+name|VT_LOCK_ASSERT
+parameter_list|(
+name|vd
+parameter_list|,
+name|what
+parameter_list|)
+value|mtx_assert(&(vd)->vd_lock, what)
+end_define
+
+begin_define
+define|#
+directive|define
 name|VT_UNIT
 parameter_list|(
 name|vw
@@ -1369,6 +1381,14 @@ modifier|*
 name|vd
 parameter_list|)
 block|{
+comment|/* 	 * As long as this function is called locked, callout_stop() 	 * has the same effect like callout_drain() with regard to 	 * preventing the callback function from executing. 	 */
+name|VT_LOCK_ASSERT
+argument_list|(
+name|vd
+argument_list|,
+name|MA_OWNED
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|!
@@ -1394,7 +1414,7 @@ literal|0
 argument_list|)
 condition|)
 return|return;
-name|callout_drain
+name|callout_stop
 argument_list|(
 operator|&
 name|vd
@@ -12661,7 +12681,17 @@ name|VDF_ASYNC
 condition|)
 block|{
 comment|/* Stop vt_flush periodic task. */
+name|VT_LOCK
+argument_list|(
+name|vd
+argument_list|)
+expr_stmt|;
 name|vt_suspend_flush_timer
+argument_list|(
+name|vd
+argument_list|)
+expr_stmt|;
+name|VT_UNLOCK
 argument_list|(
 name|vd
 argument_list|)
