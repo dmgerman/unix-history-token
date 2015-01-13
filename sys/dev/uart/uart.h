f0ec04b5c20b864cfd6dfa3da0d2572e199bd382
Ruslan Bukin <br@FreeBSD.org>
2015-01-13 15:04:28 +0000

    Rename Exynos UART driver. No functional change.
    Submitted by:	Michal Meloun <meloun@miracle.cz>
    
    
    Former-commit-id: 29659faba2e556b851c5d165d5e5a38d56f0fc26

--- sys/dev/uart/uart.h
+++ sys/dev/uart/uart.h
@@ -303,6 +303,20 @@ argument_list|)
 decl_stmt|;
 end_decl_stmt
 
+begin_decl_stmt
+specifier|extern
+name|struct
+name|uart_class
+name|uart_exynos4210_class
+name|__attribute__
+argument_list|(
+operator|(
+name|weak
+operator|)
+argument_list|)
+decl_stmt|;
+end_decl_stmt
+
 begin_ifdef
 ifdef|#
 directive|ifdef
