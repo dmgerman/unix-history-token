8a2c6c489a9f21893033f9e14ff1d79529cfb835
Ian Lepore <ian@FreeBSD.org>
2015-01-21 02:35:04 +0000

    Use the explicit member initializer style to init the bus_space struct. Fill in some formerly NULL members where the implementation function exists.  Add a dummy function that panics and use it as a placeholder for thigns that are still unimplemented.  Remove a few unused includes.
    Former-commit-id: cdfa616a1beefd20808850998d13a783fab1c391

--- sys/arm/arm/bus_space-v6.c
+++ sys/arm/arm/bus_space-v6.c
@@ -32,24 +32,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/bus.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/kernel.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/malloc.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/bus.h>
 end_include
 
@@ -73,6 +55,29 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_function
+specifier|static
+name|void
+name|bs_unimplemented
+parameter_list|(
+name|void
+parameter_list|)
+block|{
+name|panic
+argument_list|(
+literal|"unimplemented bus_space function called"
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
+begin_define
+define|#
+directive|define
+name|BS_UNIMPLEMENTED
+value|(void *)bs_unimplemented
+end_define
+
 begin_comment
 comment|/*  * The bus space tag.  This is constant for all instances, so  * we never have to explicitly "create" it.  */
 end_comment
@@ -84,181 +89,376 @@ name|bus_space
 name|_base_tag
 init|=
 block|{
-comment|/* cookie */
-operator|(
-name|void
-operator|*
-operator|)
-literal|0
+comment|/* privdata is whatever the implementer wants; unused in base tag */
+operator|.
+name|bs_privdata
+operator|=
+name|NULL
 block|,
 comment|/* mapping/unmapping */
+operator|.
+name|bs_map
+operator|=
 name|generic_bs_map
 block|,
+operator|.
+name|bs_unmap
+operator|=
 name|generic_bs_unmap
 block|,
+operator|.
+name|bs_subregion
+operator|=
 name|generic_bs_subregion
 block|,
 comment|/* allocation/deallocation */
+operator|.
+name|bs_alloc
+operator|=
 name|generic_bs_alloc
 block|,
+operator|.
+name|bs_free
+operator|=
 name|generic_bs_free
 block|,
 comment|/* barrier */
+operator|.
+name|bs_barrier
+operator|=
 name|generic_bs_barrier
 block|,
 comment|/* read (single) */
+operator|.
+name|bs_r_1
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_1, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_r_2
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_2, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_r_4
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_4, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_r_8
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_8, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
 comment|/* read multiple */
+operator|.
+name|bs_rm_1
+operator|=
 name|generic_bs_rm_1
 block|,
+operator|.
+name|bs_rm_2
+operator|=
 name|generic_armv4_bs_rm_2
 block|,
+operator|.
+name|bs_rm_4
+operator|=
 name|generic_bs_rm_4
 block|,
-name|NULL
+operator|.
+name|bs_rm_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* read region */
+operator|.
+name|bs_rr_1
+operator|=
 name|generic_bs_rr_1
 block|,
+operator|.
+name|bs_rr_2
+operator|=
 name|generic_armv4_bs_rr_2
 block|,
+operator|.
+name|bs_rr_4
+operator|=
 name|generic_bs_rr_4
 block|,
-name|NULL
+operator|.
+name|bs_rr_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* write (single) */
+operator|.
+name|bs_w_1
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_1, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_w_2
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_2, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_w_4
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_4, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_w_8
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_8, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
 comment|/* write multiple */
+operator|.
+name|bs_wm_1
+operator|=
 name|generic_bs_wm_1
 block|,
+operator|.
+name|bs_wm_2
+operator|=
 name|generic_armv4_bs_wm_2
 block|,
+operator|.
+name|bs_wm_4
+operator|=
 name|generic_bs_wm_4
 block|,
-name|NULL
+operator|.
+name|bs_wm_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* write region */
+operator|.
+name|bs_wr_1
+operator|=
 name|generic_bs_wr_1
 block|,
+operator|.
+name|bs_wr_2
+operator|=
 name|generic_armv4_bs_wr_2
 block|,
+operator|.
+name|bs_wr_4
+operator|=
 name|generic_bs_wr_4
 block|,
-name|NULL
+operator|.
+name|bs_wr_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* set multiple */
-name|NULL
+operator|.
+name|bs_sm_1
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
-name|NULL
+operator|.
+name|bs_sm_2
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
-name|NULL
+operator|.
+name|bs_sm_4
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
-name|NULL
+operator|.
+name|bs_sm_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* set region */
+operator|.
+name|bs_sr_1
+operator|=
 name|generic_bs_sr_1
 block|,
+operator|.
+name|bs_sr_2
+operator|=
 name|generic_armv4_bs_sr_2
 block|,
+operator|.
+name|bs_sr_4
+operator|=
 name|generic_bs_sr_4
 block|,
-name|NULL
+operator|.
+name|bs_sr_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* copy */
-name|NULL
+operator|.
+name|bs_c_1
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
+operator|.
+name|bs_c_2
+operator|=
 name|generic_armv4_bs_c_2
 block|,
-name|NULL
+operator|.
+name|bs_c_4
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
-name|NULL
+operator|.
+name|bs_c_8
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* read stream (single) */
+operator|.
+name|bs_r_1_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_1_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_r_2_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_2_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_r_4_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_4_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_r_8_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_r_8_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
 comment|/* read multiple stream */
-name|NULL
+operator|.
+name|bs_rm_1_s
+operator|=
+name|generic_bs_rm_1
 block|,
+operator|.
+name|bs_rm_2_s
+operator|=
 name|generic_armv4_bs_rm_2
 block|,
-comment|/* bus_space_read_multi_stream_2 */
-name|NULL
+operator|.
+name|bs_rm_4_s
+operator|=
+name|generic_bs_rm_4
 block|,
-name|NULL
+operator|.
+name|bs_rm_8_s
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* read region stream */
-name|NULL
+operator|.
+name|bs_rr_1_s
+operator|=
+name|generic_bs_rr_1
 block|,
-name|NULL
+operator|.
+name|bs_rr_2_s
+operator|=
+name|generic_bs_rr_2
 block|,
-name|NULL
+operator|.
+name|bs_rr_4_s
+operator|=
+name|generic_bs_rr_4
 block|,
-name|NULL
+operator|.
+name|bs_rr_8_s
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* write stream (single) */
+operator|.
+name|bs_w_1_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_1_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_w_2_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_2_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_w_4_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_4_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
+operator|.
+name|bs_w_8_s
+operator|=
 name|NULL
 block|,
-comment|/* bs_w_8_s, Use inline code in bus.h */
+comment|/* Use inline code in bus.h */
 comment|/* write multiple stream */
-name|NULL
+operator|.
+name|bs_wm_1_s
+operator|=
+name|generic_bs_wm_1
 block|,
+operator|.
+name|bs_wm_2_s
+operator|=
 name|generic_armv4_bs_wm_2
 block|,
-comment|/* bus_space_write_multi_stream_2 */
-name|NULL
+operator|.
+name|bs_wm_4_s
+operator|=
+name|generic_bs_wm_4
 block|,
-name|NULL
+operator|.
+name|bs_wm_8_s
+operator|=
+name|BS_UNIMPLEMENTED
 block|,
 comment|/* write region stream */
-name|NULL
+operator|.
+name|bs_wr_1_s
+operator|=
+name|generic_bs_wr_1
 block|,
-name|NULL
+operator|.
+name|bs_wr_2_s
+operator|=
+name|generic_bs_wr_4
 block|,
-name|NULL
+operator|.
+name|bs_wr_4_s
+operator|=
+name|generic_bs_wr_8
 block|,
-name|NULL
-block|}
+operator|.
+name|bs_wr_8_s
+operator|=
+name|BS_UNIMPLEMENTED
+block|, }
 decl_stmt|;
 end_decl_stmt
 
