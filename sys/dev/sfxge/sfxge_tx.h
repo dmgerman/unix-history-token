aea27f5c7eb3474f3ef728240c2b2062909d702c
Andrew Rybchenko <arybchik@FreeBSD.org>
2015-01-29 19:11:37 +0000

    sfxge: Separate software Tx queue limit for non-TCP traffic
    Add separate software Tx queue limit for non-TCP traffic to make total
    limit higher and avoid local drops of TCP packets because of no
    backpressure.
    There is no point to make non-TCP limit high since without backpressure
    UDP stream easily overflows any sensible limit.
    
    Split early drops statistics since it is better to have separate counter
    for each drop reason to make it unabmiguous.
    
    Add software Tx queue high watermark. The information is very useful to
    understand how big queues grow under traffic load.
    
    Sponsored by:   Solarflare Communications, Inc.
    Approved by:    gnn (mentor)
    
    
    Former-commit-id: 64f61be1efe35e17098cc275d12248cb17e650cd

--- sys/dev/sfxge/sfxge_tx.h
+++ sys/dev/sfxge/sfxge_tx.h
@@ -110,6 +110,13 @@ begin_define
 define|#
 directive|define
 name|SFXGE_TX_DPL_GET_PKT_LIMIT_DEFAULT
+value|(64 * 1024)
+end_define
+
+begin_define
+define|#
+directive|define
+name|SFXGE_TX_DPL_GET_NON_TCP_PKT_LIMIT_DEFAULT
 value|1024
 end_define
 
@@ -132,7 +139,12 @@ name|unsigned
 name|int
 name|std_get_max
 decl_stmt|;
-comment|/* Maximum number of packets 						 * in get list */
+comment|/* Maximum number  of packets 						 * in get list */
+name|unsigned
+name|int
+name|std_get_non_tcp_max
+decl_stmt|;
+comment|/* Maximum number 						 * of non-TCP packets 						 * in get list */
 name|unsigned
 name|int
 name|std_put_max
@@ -160,6 +172,16 @@ name|int
 name|std_get_count
 decl_stmt|;
 comment|/* Packets in get list. */
+name|unsigned
+name|int
+name|std_get_non_tcp_count
+decl_stmt|;
+comment|/* Non-TCP packets 						 * in get list */
+name|unsigned
+name|int
+name|std_get_hiwat
+decl_stmt|;
+comment|/* Packets in get list 						 * high watermark */
 block|}
 struct|;
 end_struct
@@ -428,7 +450,19 @@ name|drops
 decl_stmt|;
 name|unsigned
 name|long
-name|early_drops
+name|get_overflow
+decl_stmt|;
+name|unsigned
+name|long
+name|get_non_tcp_overflow
+decl_stmt|;
+name|unsigned
+name|long
+name|put_overflow
+decl_stmt|;
+name|unsigned
+name|long
+name|netdown_drops
 decl_stmt|;
 comment|/* The following fields change more often, and are used mostly 	 * on the completion path 	 */
 name|unsigned
