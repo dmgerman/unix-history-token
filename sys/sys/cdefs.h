51fba607b0b48d68fbc2e4539da59a19223d8179
Dimitry Andric <dim@FreeBSD.org>
2015-02-04 20:55:21 +0000

    Mark typedefs for manually implementing _Static_assert() as unused, so they won't show up unecessarily for -Wunused-local-typedefs.
    MFC after:	3 days
    
    
    Former-commit-id: a91032327d8d6d3e3200d4b2c74f962fd2e41ca2

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -1492,7 +1492,7 @@ name|x
 parameter_list|,
 name|y
 parameter_list|)
-value|typedef char __assert_ ## y[(x) ? 1 : -1]
+value|typedef char __assert_ ## y[(x) ? 1 : -1] \ 				__unused
 end_define
 
 begin_else
