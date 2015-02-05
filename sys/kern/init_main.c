d6415a20bdb32e00bc4f7e5b58ebf94179e85863
John-Mark Gurney <jmg@FreeBSD.org>
2015-02-05 07:51:38 +0000

    turn GEOM_UNCOMPRESS_DEBUG into a proper option so it can be specified in kernel config files..
    put VERBOSE_SYSINIT in it's own option header so the one file,
    init_main.c, can use it instead of requiring an entire kernel recompile
    to change one file..
    
    
    Former-commit-id: febb267e5b64b9038198d623af8ad4cb5654ea71

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -32,6 +32,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"opt_verbose_sysinit.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/param.h>
 end_include
 
