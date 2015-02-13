fdfbaec25b01d077dc8203e9a8798b7cc4396ad3
Hans Petter Selasky <hselasky@FreeBSD.org>
2015-02-13 07:48:54 +0000

    MFC r278477: Fix DMA address casts.
    Former-commit-id: 638707949baf61f8f0d5011b672a48e45ee3a9de

--- sys/dev/usb/controller/xhci.c
+++ sys/dev/usb/controller/xhci.c
@@ -2830,9 +2830,6 @@ argument_list|)
 expr_stmt|;
 name|addr
 operator|=
-operator|(
-name|uint64_t
-operator|)
 name|buf_res
 operator|.
 name|physaddr
@@ -5507,9 +5504,6 @@ expr_stmt|;
 comment|/* 	 * NOTE: The Event Ring Dequeue Pointer Register is 64-bit 	 * latched. That means to activate the register we need to 	 * write both the low and high double word of the 64-bit 	 * register. 	 */
 name|addr
 operator|=
-operator|(
-name|uint32_t
-operator|)
 name|buf_res
 operator|.
 name|physaddr
