b3534b5ab2902ebb1665d2a9c8fe395157adfff2
Ryan Stone <rstone@FreeBSD.org>
2015-03-10 23:27:13 +0000

    Fix SR-IOV passthrough devices to allow ppt to attach
    A late change to the SR-IOV infrastructure broke passthrough of
    VFs.  device_set_devclass() was being used to try to force the
    ppt driver to attach to the device, but this didn't work because
    the DF_FIXEDCLASS flag wasn't being set on the device, so the
    ppt driver probe routine would not match when it returned
    BUS_NOWILDCARD.  Fix this by adding a new device function that
    both sets the devclass and sets the DF_FIXEDCLASS flag, and use
    that to force the ppt driver to attach to VFs.
    
    Differential Revision: https://reviews.freebsd.org/D2041
    Reviewed by:	jhb
    MFC after:	3 weeks
    
    
    Former-commit-id: f4b0b9cc92320469e515be1c4db7475f9f0e2c14

--- sys/sys/bus.h
+++ sys/sys/bus.h
@@ -2900,6 +2900,21 @@ end_function_decl
 
 begin_function_decl
 name|int
+name|device_set_devclass_fixed
+parameter_list|(
+name|device_t
+name|dev
+parameter_list|,
+specifier|const
+name|char
+modifier|*
+name|classname
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|int
 name|device_set_driver
 parameter_list|(
 name|device_t
