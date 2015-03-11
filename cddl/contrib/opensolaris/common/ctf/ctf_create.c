da963eed78b64884ebac58a5db3b193d45892717
Mark Johnston <markj@FreeBSD.org>
2015-03-11 00:01:39 +0000

    When copying a type from a source CTF container to a destination container, ctf_add_type() first performs a by-name lookup of the type in the destination container. If this lookup returns a forward declaration for an enum, struct, or union, reset dst_type back to CTF_ERR, indicating that the source type is not in fact present in the destination container. This ensures that ctf_add_type() will also search the destination container's dynamic type list for the source type.
    Without this change, a pair of mutually recursive struct definitions could
    cause infinite recursion in ctf_add_type() if the destination container
    only contained forward declarations for the struct types: ctf_add_type()
    recursively calls itself on each struct member's type, and the forward
    declarations meant that the dynamic type list search would be skipped.
    
    MFC after:	2 weeks
    Sponsored by:	EMC / Isilon Storage Division
    
    
    Former-commit-id: bc7c33a2c3be15ee07602ba479e771270255003f

--- cddl/contrib/opensolaris/common/ctf/ctf_create.c
+++ cddl/contrib/opensolaris/common/ctf/ctf_create.c
@@ -6993,8 +6993,10 @@ operator|&&
 name|dst_kind
 operator|!=
 name|kind
-operator|&&
-operator|(
+condition|)
+block|{
+if|if
+condition|(
 name|dst_kind
 operator|!=
 name|CTF_K_FORWARD
@@ -7012,7 +7014,6 @@ name|kind
 operator|!=
 name|CTF_K_UNION
 operator|)
-operator|)
 condition|)
 return|return
 operator|(
@@ -7024,6 +7025,12 @@ name|ECTF_CONFLICT
 argument_list|)
 operator|)
 return|;
+else|else
+name|dst_type
+operator|=
+name|CTF_ERR
+expr_stmt|;
+block|}
 comment|/* 	 * If the non-empty name was not found in the appropriate hash, search 	 * the list of pending dynamic definitions that are not yet committed. 	 * If a matching name and kind are found, assume this is the type that 	 * we are looking for.  This is necessary to permit ctf_add_type() to 	 * operate recursively on entities such as a struct that contains a 	 * pointer member that refers to the same struct type. 	 */
 if|if
 condition|(
