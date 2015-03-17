5d4751f85fa9170a7a82d06d5443cecd8e4d1a26
Zbigniew Bodek <zbb@FreeBSD.org>
2015-03-17 18:59:47 +0000

    Introduce Annapurna Labs AHCI support
    Overview:
    * implemented quirk for forcing SATA interface enable
    * restore value to status register - this enables link autonegotiation
    
    Modifications:
    * devid:vendorid field
    * quirk for forcing PI setting (BIOS is doing that on PC-like systems)
    * write to capabilites field to enable phy link initialization
    
    Submitted by:  Wojciech Macek <wma@semihalf.com>
    Reviewed by:   imp, mav
    Obtained from: Semihalf
    
    
    Former-commit-id: d980fd62009b7218b48f4a588d00b964a3a2c9e0

--- sys/dev/ahci/ahci.h
+++ sys/dev/ahci/ahci.h
@@ -3142,9 +3142,23 @@ end_define
 begin_define
 define|#
 directive|define
+name|AHCI_Q_FORCE_PI
+value|0x00040000
+end_define
+
+begin_define
+define|#
+directive|define
+name|AHCI_Q_RESTORE_CAP
+value|0x00080000
+end_define
+
+begin_define
+define|#
+directive|define
 name|AHCI_Q_BIT_STRING
 define|\
-value|"\021"			\ 	"\001NOFORCE"		\ 	"\002NOPMP"		\ 	"\003NONCQ"		\ 	"\0041CH"		\ 	"\0052CH"		\ 	"\0064CH"		\ 	"\007EDGEIS"		\ 	"\010SATA2"		\ 	"\011NOBSYRES"		\ 	"\012NOAA"		\ 	"\013NOCOUNT"		\ 	"\014ALTSIG"		\ 	"\015NOMSI"		\ 	"\016ATI_PMP_BUG"	\ 	"\017MAXIO_64K"		\ 	"\020SATA1_UNIT0"	\ 	"\021ABAR0"		\ 	"\0221MSI"
+value|"\021"			\ 	"\001NOFORCE"		\ 	"\002NOPMP"		\ 	"\003NONCQ"		\ 	"\0041CH"		\ 	"\0052CH"		\ 	"\0064CH"		\ 	"\007EDGEIS"		\ 	"\010SATA2"		\ 	"\011NOBSYRES"		\ 	"\012NOAA"		\ 	"\013NOCOUNT"		\ 	"\014ALTSIG"		\ 	"\015NOMSI"		\ 	"\016ATI_PMP_BUG"	\ 	"\017MAXIO_64K"		\ 	"\020SATA1_UNIT0"	\ 	"\021ABAR0"		\ 	"\0221MSI"              \ 	"\022FORCE_PI"          \ 	"\023RESTORE_CAP"
 end_define
 
 begin_function_decl
