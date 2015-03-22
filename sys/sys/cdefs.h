2d63f46c3bc3b90167256f235be16e1ca9bea5fa
Pedro Giffuni <pfg@FreeBSD.org>
2015-03-22 23:42:21 +0000

    Small style(9) cleanup.
    Fix yet more issues, but certainly not all.
    
    Pointed out by:	bde
    
    
    Former-commit-id: cdbccd4335bb21025457d34cf04b72868def3611

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -264,8 +264,8 @@ name|defined
 argument_list|(
 name|__cplusplus
 argument_list|)
-expr|\
 operator|&&
+expr|\
 name|__INTEL_COMPILER
 operator|<
 literal|800
@@ -527,7 +527,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * The __CONCAT macro is used to concatenate parts of symbol names, e.g.  * with "#define	OLD(foo) __CONCAT(old,foo)", OLD(foo) produces oldfoo.  * The __CONCAT macro is a bit tricky to use if it must work in non-ANSI  * mode -- there must be no spaces between its arguments, and for nested  * __CONCAT's, all the __CONCAT's must be at the left.  __CONCAT can also  * concatenate double-quoted strings produced by the __STRING macro, but  * this only works with ANSI C.  *  * __XSTRING is like __STRING, but it expands any macros in its argument  * first.  It is only available with ANSI C.  */
+comment|/*  * The __CONCAT macro is used to concatenate parts of symbol names, e.g.  * with "#define OLD(foo) __CONCAT(old,foo)", OLD(foo) produces oldfoo.  * The __CONCAT macro is a bit tricky to use if it must work in non-ANSI  * mode -- there must be no spaces between its arguments, and for nested  * __CONCAT's, all the __CONCAT's must be at the left.  __CONCAT can also  * concatenate double-quoted strings produced by the __STRING macro, but  * this only works with ANSI C.  *  * __XSTRING is like __STRING, but it expands any macros in its argument  * first.  It is only available with ANSI C.  */
 end_comment
 
 begin_if
