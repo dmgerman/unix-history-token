fb8132ed53a20fa4bf6ae1157e55550b3ea16b5c
Neel Natu <neel@FreeBSD.org>
2015-04-06 03:16:20 +0000

    MFC r272481. Add new fields in the FADT, required by IASL 20140926-64.
    Former-commit-id: fe9a632834493abe6226838bd619a093dacff5c2

--- usr.sbin/bhyve/acpi.c
+++ usr.sbin/bhyve/acpi.c
@@ -1931,7 +1931,28 @@ name|EFPRINTF
 argument_list|(
 name|fp
 argument_list|,
-literal|"[0003]\t\tReserved : 000000\n"
+literal|"[0002]\t\tARM Flags (decoded below): 0000\n"
+argument_list|)
+expr_stmt|;
+name|EFPRINTF
+argument_list|(
+name|fp
+argument_list|,
+literal|"\t\t\tPSCI Compliant : 0\n"
+argument_list|)
+expr_stmt|;
+name|EFPRINTF
+argument_list|(
+name|fp
+argument_list|,
+literal|"\t\t\tMust use HVC for PSCI : 0\n"
+argument_list|)
+expr_stmt|;
+name|EFPRINTF
+argument_list|(
+name|fp
+argument_list|,
+literal|"[0001]\t\tFADT Minor Revision : 01\n"
 argument_list|)
 expr_stmt|;
 name|EFPRINTF
