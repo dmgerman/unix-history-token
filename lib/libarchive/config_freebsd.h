12a1352812d18fa6e28a2dceda4bbf5777605be6
Jilles Tjoelker <jilles@FreeBSD.org>
2015-04-25 21:25:00 +0000

    libarchive: Allow setting nanosecond timestamps.
    Former-commit-id: be972291a56de3d140e23486d3e37fb14aa7d9a4

--- lib/libarchive/config_freebsd.h
+++ lib/libarchive/config_freebsd.h
@@ -3,6 +3,12 @@ begin_comment
 comment|/*-  * Copyright (c) 2003-2007 Tim Kientzle  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
+begin_include
+include|#
+directive|include
+file|<osreldate.h>
+end_include
+
 begin_comment
 comment|/* FreeBSD 5.0 and later have ACL and extattr support. */
 end_comment
@@ -1337,6 +1343,33 @@ directive|define
 name|TIME_WITH_SYS_TIME
 value|1
 end_define
+
+begin_if
+if|#
+directive|if
+name|__FreeBSD_version
+operator|>=
+literal|1100056
+end_if
+
+begin_define
+define|#
+directive|define
+name|HAVE_FUTIMENS
+value|1
+end_define
+
+begin_define
+define|#
+directive|define
+name|HAVE_UTIMENSAT
+value|1
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_comment
 comment|/* FreeBSD 4 and earlier lack intmax_t/uintmax_t */
