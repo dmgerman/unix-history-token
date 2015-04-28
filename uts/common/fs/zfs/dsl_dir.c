8749eb3a2a4d43fe11961b72b022fc40ff319474
Andriy Gapon <avg@FreeBSD.org>
2015-04-28 07:14:08 +0000

    5610 zfs clone from different source and target pools produces coredump
    Author: Alexander Eremin <a.eremin@nexenta.com>
    Reviewed by: Josef 'Jeff' Sipek <josef.sipek@nexenta.com>
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Approved by: Dan McDonald <danmcd@omniti.com>
    
    illumos/illumos-gate@03b1c2971d24a9cd2c073d634f7e074fbd14e984
    
    
    Former-commit-id: c5651d05d05f1cb5fc9d8108ecbccaecb8e75fa1

--- uts/common/fs/zfs/dsl_dir.c
+++ uts/common/fs/zfs/dsl_dir.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2012, 2014 by Delphix. All rights reserved.  * Copyright (c) 2013 Martin Matuska. All rights reserved.  * Copyright (c) 2014 Joyent, Inc. All rights reserved.  * Copyright (c) 2014 Spectra Logic Corporation, All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2012, 2014 by Delphix. All rights reserved.  * Copyright (c) 2013 Martin Matuska. All rights reserved.  * Copyright (c) 2014 Joyent, Inc. All rights reserved.  * Copyright (c) 2014 Spectra Logic Corporation, All rights reserved.  * Copyright 2015 Nexenta Systems, Inc. All rights reserved.  */
 end_comment
 
 begin_include
@@ -1665,7 +1665,7 @@ return|return
 operator|(
 name|SET_ERROR
 argument_list|(
-name|EINVAL
+name|EXDEV
 argument_list|)
 operator|)
 return|;
