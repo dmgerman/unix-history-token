5280f7951c13a44304cea904a5e4231b932392ac
Luiz Otavio O Souza <loos@FreeBSD.org>
2015-05-05 00:27:55 +0000

    Enable DMA for sdhci on RPi 2 (BCM2836).
    Former-commit-id: 8fefd86ff4cad8d60ed0579e0db8b80983d91039

--- sys/arm/broadcom/bcm2835/bcm2835_sdhci.c
+++ sys/arm/broadcom/bcm2835/bcm2835_sdhci.c
@@ -217,40 +217,6 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* DMA doesn't yet work with the bcm3826 */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|SOC_BCM2836
-end_ifdef
-
-begin_define
-define|#
-directive|define
-name|PIO_MODE
-value|1
-end_define
-
-begin_else
-else|#
-directive|else
-end_else
-
-begin_define
-define|#
-directive|define
-name|PIO_MODE
-value|0
-end_define
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_decl_stmt
 specifier|static
 name|int
@@ -265,7 +231,7 @@ specifier|static
 name|int
 name|bcm2835_sdhci_pio_mode
 init|=
-name|PIO_MODE
+literal|0
 decl_stmt|;
 end_decl_stmt
 
