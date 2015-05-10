85d62351aa23ad23c40cdbe703d1e3e8d0a4f604
Thomas Quinot <thomas@FreeBSD.org>
2015-05-10 21:21:52 +0000

    Unbreak build following rev. 282726
    (Makefile.inc1): add dependency of xinstall on libmd to
    avoid failure of parallel bootstrap.
    
    (lib/libmd/*.h): do not redefine symbols if already
    defined as macros (libcrypt uses the same sources internally,
    redefining symbols with a prefix of its own).
    
    Fixes build failures caused by previous change to libmd.
    
    Reported by: ian
    Pointy hat to: thomas
    
    
    Former-commit-id: 36bac16646ee67d0b46f1b2038f786f68df9ac5f

--- lib/libmd/md5.h
+++ lib/libmd/md5.h
@@ -25,12 +25,29 @@ begin_comment
 comment|/* Ensure libmd symbols do not clash with libcrypto */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5Init
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|MD5Init
 value|_libmd_MD5Init
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5Update
+end_ifndef
 
 begin_define
 define|#
@@ -39,12 +56,34 @@ name|MD5Update
 value|_libmd_MD5Update
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5Pad
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|MD5Pad
 value|_libmd_MD5Pad
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5Final
+end_ifndef
 
 begin_define
 define|#
@@ -53,12 +92,34 @@ name|MD5Final
 value|_libmd_MD5Final
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5Transform
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|MD5Transform
 value|_libmd_MD5Transform
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5End
+end_ifndef
 
 begin_define
 define|#
@@ -67,12 +128,34 @@ name|MD5End
 value|_libmd_MD5End
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5File
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|MD5File
 value|_libmd_MD5File
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5FileChunk
+end_ifndef
 
 begin_define
 define|#
@@ -81,12 +164,28 @@ name|MD5FileChunk
 value|_libmd_MD5FileChunk
 end_define
 
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|MD5Data
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|MD5Data
 value|_libmd_MD5Data
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_endif
 endif|#
