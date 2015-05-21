46ac5e9f548adf9030c4a1640c47bb660da568f1
Hans Petter Selasky <hselasky@FreeBSD.org>
2015-05-21 07:41:03 +0000

    MFC r279726: Add DA_Q_NO_RC16 quirk for USB mass storage device.
    PR:		194062
    
    Former-commit-id: e96d6f5fdc3b6863426fac7890277acf71f87d61

--- sys/cam/scsi/scsi_da.c
+++ sys/cam/scsi/scsi_da.c
@@ -3276,6 +3276,24 @@ name|DA_Q_4K
 block|}
 block|,
 block|{
+comment|/* 		 * Hama Innostor USB-Stick  		 */
+block|{
+name|T_DIRECT
+block|,
+name|SIP_MEDIA_REMOVABLE
+block|,
+literal|"Innostor"
+block|,
+literal|"Innostor*"
+block|,
+literal|"*"
+block|}
+block|,
+comment|/*quirks*/
+name|DA_Q_NO_RC16
+block|}
+block|,
+block|{
 comment|/* 		 * MX-ES USB Drive by Mach Xtreme 		 */
 block|{
 name|T_DIRECT
