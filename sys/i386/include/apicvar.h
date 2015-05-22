aa0060dd851870daea97a157886fbd1e13259867
Wei Hu <whu@FreeBSD.org>
2015-05-22 11:20:59 +0000

    Fix a i386 build failure cause by commit r283280.
    Former-commit-id: 0d15ca7db7953d86954023c3246a672278ff8cc9

--- sys/i386/include/apicvar.h
+++ sys/i386/include/apicvar.h
@@ -1040,6 +1040,18 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|hv_vector_handler
+parameter_list|(
+name|struct
+name|trapframe
+modifier|*
+name|frame
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
