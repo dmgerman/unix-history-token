c6ff7e9ae84016b184edec3a7673c235e6c0ce94
Wei Hu <whu@FreeBSD.org>
2015-05-28 09:20:35 +0000

    MFC r283053
    Submitted by:	whu
    Reviewed by:	royger
    Approved by:	royger
    Sponsored by:	Microsoft OSTC
    Differential Revision:	https://reviews.freebsd.org/D2661
    
    
    Former-commit-id: e12e573bf468332b33875279cfd5184e86423123

--- sys/dev/hyperv/storvsc/hv_vstorage.h
+++ sys/dev/hyperv/storvsc/hv_vstorage.h
@@ -63,7 +63,7 @@ value|-1
 end_define
 
 begin_comment
-comment|/*  * Version history:  * V1 Beta                    0.1  * V1 RC< 2008/1/31          1.0  * V1 RC> 2008/1/31          2.0  */
+comment|/*  * Version history:  * V1 Beta                    0.1  * V1 RC< 2008/1/31          1.0  * V1 RC> 2008/1/31          2.0  * Win7: 4.2  * Win8: 5.1  */
 end_comment
 
 begin_define
@@ -155,7 +155,7 @@ begin_define
 define|#
 directive|define
 name|SENSE_BUFFER_SIZE
-value|0x12
+value|0x14
 end_define
 
 begin_define
@@ -164,6 +164,48 @@ directive|define
 name|MAX_DATA_BUFFER_LENGTH_WITH_PADDING
 value|0x14
 end_define
+
+begin_define
+define|#
+directive|define
+name|POST_WIN7_STORVSC_SENSE_BUFFER_SIZE
+value|0x14
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRE_WIN8_STORVSC_SENSE_BUFFER_SIZE
+value|0x12
+end_define
+
+begin_struct
+struct|struct
+name|vmscsi_win8_extension
+block|{
+comment|/* 	 * The following were added in Windows 8 	 */
+name|uint16_t
+name|reserve
+decl_stmt|;
+name|uint8_t
+name|queue_tag
+decl_stmt|;
+name|uint8_t
+name|queue_action
+decl_stmt|;
+name|uint32_t
+name|srb_flags
+decl_stmt|;
+name|uint32_t
+name|time_out_value
+decl_stmt|;
+name|uint32_t
+name|queue_sort_ey
+decl_stmt|;
+block|}
+name|__packed
+struct|;
+end_struct
 
 begin_struct
 struct|struct
@@ -230,6 +272,11 @@ decl_stmt|;
 block|}
 name|u
 union|;
+comment|/* 	 * The following was added in win8. 	 */
+name|struct
+name|vmscsi_win8_extension
+name|win8_extension
+decl_stmt|;
 block|}
 name|__packed
 struct|;
