6e58abecbc8d8bf4a667ac2e0188e89c3988ffb2
Michael Tuexen <tuexen@FreeBSD.org>
2015-05-29 12:25:11 +0000

    MFC r277337:
    Remove an unused variable.
    
    Reported by:	Coverity
    CID:		750999
    
    Former-commit-id: 70b6b634b62feb9e8bc250dec96ead8d945a31c0

--- sys/netinet/sctp_usrreq.c
+++ sys/netinet/sctp_usrreq.c
@@ -23743,11 +23743,6 @@ name|sockaddr
 modifier|*
 name|sa
 decl_stmt|;
-name|struct
-name|sctp_nets
-modifier|*
-name|net
-decl_stmt|;
 comment|/* FIXME MT: check correct? */
 name|SCTP_CHECK_AND_CAST
 argument_list|(
@@ -23796,15 +23791,6 @@ argument_list|(
 name|stcb
 argument_list|)
 expr_stmt|;
-name|net
-operator|=
-name|sctp_findnet
-argument_list|(
-name|stcb
-argument_list|,
-name|sa
-argument_list|)
-expr_stmt|;
 block|}
 name|SCTP_INP_RUNLOCK
 argument_list|(
@@ -23829,8 +23815,7 @@ name|inp
 argument_list|,
 name|sa
 argument_list|,
-operator|&
-name|net
+name|NULL
 argument_list|,
 name|NULL
 argument_list|,
