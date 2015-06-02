7ee45bb22cb3e4448ce689f683e9e728615e1d9b
Andrey V. Elsukov <ae@FreeBSD.org>
2015-06-02 03:14:42 +0000

    MFC r275392:   Remove route chaching support from ipsec code. It isn't used for some time.   * remove sa_route_union declaration and route_cache member from struct secashead;   * remove key_sa_routechange() call from ICMP and ICMPv6 code;   * simplify ip_ipsec_mtu();   * remove #include <net/route.h>;
    Sponsored by:	Yandex LLC
    
    
    Former-commit-id: 814cac1ee73cdb90c533fc74734e6a59dfc015a2

--- sys/netipsec/ipsec_input.c
+++ sys/netipsec/ipsec_input.c
@@ -108,12 +108,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<net/route.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<net/netisr.h>
 end_include
 
--- sys/netipsec/xform_ipip.c
+++ sys/netipsec/xform_ipip.c
@@ -90,12 +90,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<net/route.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<net/netisr.h>
 end_include
 
