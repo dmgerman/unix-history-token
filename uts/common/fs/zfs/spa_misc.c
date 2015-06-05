54cfa6cfa81dc84cb989e4d2dccc842fe50ba530
Andriy Gapon <avg@FreeBSD.org>
2015-06-05 16:49:21 +0000

    5818 zfs {ref}compressratio is incorrect with 4k sector size
    Author: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: George Wilson <george@delphix.com>
    Reviewed by: Richard Elling <richard.elling@richardelling.com>
    Reviewed by: Steven Hartland <killing@multiplay.co.uk>
    Reviewed by: Don Brady <dev.fs.zfs@gmail.com>
    Approved by: Albert Lee <trisk@omniti.com>
    
    illumos/illumos-gate@81cd5c555f505484180a62ca5a2fbb00d70c57d6
    
    
    Former-commit-id: 7cebc55398cccaca889045adb0441230fea05873

--- uts/common/fs/zfs/spa_misc.c
+++ uts/common/fs/zfs/spa_misc.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.  * Copyright (c) 2014 Spectra Logic Corporation, All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011, 2015 by Delphix. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.  * Copyright (c) 2014 Spectra Logic Corporation, All rights reserved.  */
 end_comment
 
 begin_include
@@ -2093,6 +2093,18 @@ operator|)
 operator|!=
 literal|0
 operator|)
+expr_stmt|;
+name|spa
+operator|->
+name|spa_min_ashift
+operator|=
+name|INT_MAX
+expr_stmt|;
+name|spa
+operator|->
+name|spa_max_ashift
+operator|=
+literal|0
 expr_stmt|;
 comment|/* 	 * As a pool is being created, treat all features as disabled by 	 * setting SPA_FEATURE_DISABLED for all entries in the feature 	 * refcount cache. 	 */
 for|for
--- uts/common/fs/zfs/sys/vdev_impl.h
+++ uts/common/fs/zfs/sys/vdev_impl.h
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2012, 2014 by Delphix. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011, 2015 by Delphix. All rights reserved.  */
 end_comment
 
 begin_ifndef
@@ -651,7 +651,7 @@ name|spa_aux_vdev_t
 modifier|*
 name|vdev_aux
 decl_stmt|;
-comment|/* for l2cache vdevs		*/
+comment|/* for l2cache and spares vdevs	*/
 name|zio_t
 modifier|*
 name|vdev_probe_zio
