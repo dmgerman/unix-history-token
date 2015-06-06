31a73d9d4bd6e1c368c2c021bd310a4596a3247b
Ruslan Bukin <br@FreeBSD.org>
2015-06-06 14:26:40 +0000

    Include a header required for vtophys().
    Former-commit-id: 76fb4c8a144c71d0278d477c777fc554fb5eb3ee

--- sys/arm/freescale/imx/imx6_sdma.c
+++ sys/arm/freescale/imx/imx6_sdma.c
@@ -108,6 +108,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<vm/pmap.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<dev/fdt/fdt_common.h>
 end_include
 
