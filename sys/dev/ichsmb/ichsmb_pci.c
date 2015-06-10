4f1026c28ffc372b76b0e028cdef9b97bd1cad53
Rui Paulo <rpaulo@FreeBSD.org>
2015-06-10 22:39:10 +0000

    ichsmb: add Intel Wellsburg device ID.
    Submitted by:	Michael Allen <mallen pi-coral.com>
    MFC after:	1 week
    Sponsored by:	Pi-Coral, Inc.
    
    
    Former-commit-id: 605b5f3aabbbcbf59970901c7545674e252a07c0

--- sys/dev/ichsmb/ichsmb_pci.c
+++ sys/dev/ichsmb/ichsmb_pci.c
@@ -298,6 +298,13 @@ end_define
 begin_define
 define|#
 directive|define
+name|ID_WELLSBURG
+value|0x8d228086
+end_define
+
+begin_define
+define|#
+directive|define
 name|PCIS_SERIALBUS_SMBUS_PROGIF
 value|0x00
 end_define
@@ -783,6 +790,17 @@ argument_list|(
 name|dev
 argument_list|,
 literal|"Intel Coleto Creek SMBus controller"
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|ID_WELLSBURG
+case|:
+name|device_set_desc
+argument_list|(
+name|dev
+argument_list|,
+literal|"Intel Wellsburg SMBus controller"
 argument_list|)
 expr_stmt|;
 break|break;
