1b54553ae94869277f5e300709e589145fe6ad1a
Andrew Rybchenko <arybchik@FreeBSD.org>
2015-06-18 15:41:09 +0000

    sfxge: use if_initbaudrate() to set interface baudrate
    It is required to cope with u_long limit for 10 Gbps in a right way.
    It is a direct commit to stable/10 since head differs (head does not
    have if_initbaudrate(), if_baudrate is simply uint64_t).
    
    Sponsored by: Solarflare Communications, Inc.
    Differential Revision: https://reviews.freebsd.org/D2849
    
    
    Former-commit-id: 91cbc490f960f338b75fd5c3e2c1a50085388a0b

--- sys/dev/sfxge/sfxge_port.c
+++ sys/dev/sfxge/sfxge_port.c
@@ -937,7 +937,7 @@ end_comment
 begin_decl_stmt
 specifier|static
 specifier|const
-name|u_long
+name|uint64_t
 name|sfxge_link_baudrate
 index|[
 name|EFX_LINK_NMODES
@@ -1002,14 +1002,9 @@ index|[
 name|EFX_LINK_10000FDX
 index|]
 operator|=
-name|MIN
-argument_list|(
 name|IF_Gbps
 argument_list|(
-literal|10ULL
-argument_list|)
-argument_list|,
-name|ULONG_MAX
+literal|10
 argument_list|)
 block|, }
 decl_stmt|;
@@ -1073,18 +1068,19 @@ else|:
 name|LINK_STATE_DOWN
 operator|)
 expr_stmt|;
+name|if_initbaudrate
+argument_list|(
 name|sc
 operator|->
 name|ifnet
-operator|->
-name|if_baudrate
-operator|=
+argument_list|,
 name|sfxge_link_baudrate
 index|[
 name|port
 operator|->
 name|link_mode
 index|]
+argument_list|)
 expr_stmt|;
 name|if_link_state_change
 argument_list|(
