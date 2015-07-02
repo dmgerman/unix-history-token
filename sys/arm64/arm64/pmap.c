49e2967db698de017eff00d80639dd366e940b27
Andrew Turner <andrew@FreeBSD.org>
2015-07-02 16:13:29 +0000

    Remove an unneeded define and old comment referencing amd64.
    Former-commit-id: 4ee4d59631e88d3e48b0d47a16d48f91a3f94767

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -7,12 +7,6 @@ begin_comment
 comment|/*-  * Copyright (c) 2003 Networks Associates Technology, Inc.  * All rights reserved.  *  * This software was developed for the FreeBSD Project by Jake Burkholder,  * Safeport Network Services, and Network Associates Laboratories, the  * Security Research Division of Network Associates, Inc. under  * DARPA/SPAWAR contract N66001-01-C-8035 ("CBOSS"), as part of the DARPA  * CHATS research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
-begin_define
-define|#
-directive|define
-name|AMD64_NPT_AWARE
-end_define
-
 begin_include
 include|#
 directive|include
@@ -12551,7 +12545,6 @@ operator|(
 name|FALSE
 operator|)
 return|;
-comment|/* 	 * NB:  The sequence of updating a page table followed by accesses 	 * to the corresponding pages used in the !DMAP case is subject to 	 * the situation described in the "AMD64 Architecture Programmer's 	 * Manual Volume 2: System Programming" rev. 3.23, "7.3.1 Special 	 * Coherency Considerations".  Therefore, issuing the INVLPG right 	 * after modifying the PTE bits is crucial. 	 */
 if|if
 condition|(
 operator|!
