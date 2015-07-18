2fcdfea71f20422c77f8274a4f547aa6429a49a6
John-Mark Gurney <jmg@FreeBSD.org>
2015-07-18 20:21:25 +0000

    other fixes to make boot2 compile for IXP...  Properly end the asm sections, and for some reason, main needs a prototype... If someone has a better fix, I'm all ears...
    Pointed out by:	Berislav Purgar
    
    
    Former-commit-id: 1b54e7d9c493a15f260b1aa98373b9a1fecd4744

--- sys/boot/arm/ixp425/boot2/boot2.c
+++ sys/boot/arm/ixp425/boot2/boot2.c
@@ -708,6 +708,15 @@ block|}
 block|}
 end_function
 
+begin_function_decl
+name|int
+name|main
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_function
 name|int
 name|main
