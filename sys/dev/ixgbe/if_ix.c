0b67d43a63d743618543790e7ed05b628f602214
Hiren Panchasara <hiren@FreeBSD.org>
2015-07-21 06:48:36 +0000

    Remove a couple of TUNABLE_INT() calls which are unnecessary after r267961. r267961 did remove them but they "reappeared" when ixgbe(4) rewrite happened in r280182.
    Sponsored by:		Limelight Networks
    
    
    Former-commit-id: 51d45e0432e622491a94c25b52eabd4bf2e2fad2

--- sys/dev/ixgbe/if_ix.c
+++ sys/dev/ixgbe/if_ix.c
@@ -1827,17 +1827,6 @@ decl_stmt|;
 end_decl_stmt
 
 begin_expr_stmt
-name|TUNABLE_INT
-argument_list|(
-literal|"hw.ixgbe.rx_process_limit"
-argument_list|,
-operator|&
-name|ixgbe_rx_process_limit
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_expr_stmt
 name|SYSCTL_INT
 argument_list|(
 name|_hw_ix
@@ -1871,17 +1860,6 @@ init|=
 literal|256
 decl_stmt|;
 end_decl_stmt
-
-begin_expr_stmt
-name|TUNABLE_INT
-argument_list|(
-literal|"hw.ixgbe.tx_process_limit"
-argument_list|,
-operator|&
-name|ixgbe_tx_process_limit
-argument_list|)
-expr_stmt|;
-end_expr_stmt
 
 begin_expr_stmt
 name|SYSCTL_INT
