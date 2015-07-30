c898b02f9b0348933a56ad77da66ad0c3171b8a7
Andrey V. Elsukov <ae@FreeBSD.org>
2015-07-30 10:26:43 +0000

    Build if_stf(4) module only when both INET and INET6 support are enabled.
    Former-commit-id: 838759b9c5ae24af8c40d9766b7fcbb657c30129

--- sys/net/if_stf.c
+++ sys/net/if_stf.c
@@ -18,18 +18,6 @@ end_comment
 begin_include
 include|#
 directive|include
-file|"opt_inet.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"opt_inet6.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/param.h>
 end_include
 
