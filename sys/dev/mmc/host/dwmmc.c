4e1470a46bfec556e8dcd0fef3d218c331b08e67
Andrew Turner <andrew@FreeBSD.org>
2015-08-07 08:57:58 +0000

    Attach dwmmc to the ofwbus, som devicetrees place it here.
    Sponsored by:	ABT Systems Ltd
    
    
    Former-commit-id: 9266139db1c938e13b9abc2a69ba76fbe650d7b0

--- sys/dev/mmc/host/dwmmc.c
+++ sys/dev/mmc/host/dwmmc.c
@@ -5627,5 +5627,23 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|DRIVER_MODULE
+argument_list|(
+name|dwmmc
+argument_list|,
+name|ofwbus
+argument_list|,
+name|dwmmc_driver
+argument_list|,
+name|dwmmc_devclass
+argument_list|,
+literal|0
+argument_list|,
+literal|0
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
