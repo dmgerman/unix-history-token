e5391d43fcbd1f3f800a9eb1babee757c0b01453
Alexander Motin <mav@FreeBSD.org>
2015-08-13 00:13:55 +0000

    MFV 286711: 6096 ZFS_SMB_ACL_RENAME needs to cleanup better
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed by: Gordon Ross <gordon.w.ross@gmail.com>
    Reviewed by: George Wilson <gwilson@zfsmail.com>
    Approved by: Robert Mustacchi <rm@joyent.com>
    
    illumos/illumos-gate@8f5190a540d64d2debee6664bbc740e4c38f5b7f
    
    
    Former-commit-id: 2e6f6c48504173f225f75b9fa53e5267914c77b4

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ioctl.c
@@ -22283,6 +22283,14 @@ argument_list|(
 name|vp
 argument_list|)
 expr_stmt|;
+name|VN_RELE
+argument_list|(
+name|ZTOV
+argument_list|(
+name|sharedir
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|ZFS_EXIT
 argument_list|(
 name|zfsvfs
