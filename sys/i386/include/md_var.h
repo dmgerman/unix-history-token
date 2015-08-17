1f2d381c1c2ebd382faed963d3cdd85ed8089cc3
Konstantin Belousov <kib@FreeBSD.org>
2015-08-17 18:33:16 +0000

    MFC r286228: Clear the IA32_MISC_ENABLE MSR bit on APs.
    Former-commit-id: e869059927dee814d70ca18a5f8ab592c6e2f463

--- sys/i386/include/md_var.h
+++ sys/i386/include/md_var.h
@@ -620,6 +620,15 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|bool
+name|intel_fix_cpuid
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|void
 name|i686_pagezero
 parameter_list|(
