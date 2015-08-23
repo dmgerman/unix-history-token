d5ae12d65ed38d10daa16f6a055d393d70b1db77
Jilles Tjoelker <jilles@FreeBSD.org>
2015-08-23 20:44:53 +0000

    sh: Don't create bad parse result when postponing a bad substitution error.
    An invalid substitution like ${var@} does not cause a parse error but is
    stored in the intermediate representation, to be written as part of the
    error message. If there is a CTL* byte in the stored part, this confuses
    some code such as the code to skip an unused alternative such as in
    ${var-alternative}.
    
    To keep things simple, do not store CTL* bytes.
    
    Found with afl-fuzz.
    
    MFC after:	1 week
    
    
    Former-commit-id: d3da68e386496b213d27b4fdb283ed33993a8c22

--- bin/sh/parser.c
+++ bin/sh/parser.c
@@ -8029,7 +8029,16 @@ argument_list|(
 literal|"Unexpected end of line in substitution"
 argument_list|)
 expr_stmt|;
-else|else
+elseif|else
+if|if
+condition|(
+name|BASESYNTAX
+index|[
+name|c
+index|]
+operator|!=
+name|CCTL
+condition|)
 name|USTPUTC
 argument_list|(
 name|c
@@ -8108,6 +8117,15 @@ argument_list|,
 name|out
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|BASESYNTAX
+index|[
+name|c
+index|]
+operator|!=
+name|CCTL
+condition|)
 name|STPUTC
 argument_list|(
 name|c
