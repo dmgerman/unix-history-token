3a99acec9f5ce3d35cced186c57ef30b51eb32ae
Tai-hwa Liang <avatar@FreeBSD.org>
2015-09-11 22:43:35 +0000

    Fixing a memory leak on module unloading.
    MFC after:	3 weeks
    
    
    Former-commit-id: a7406d751a266400640061bb345e49ee47193524

--- sys/gnu/fs/reiserfs/reiserfs_vfsops.c
+++ sys/gnu/fs/reiserfs/reiserfs_vfsops.c
@@ -4620,6 +4620,13 @@ condition|(
 literal|0
 condition|)
 do|;
+name|free
+argument_list|(
+name|ip
+argument_list|,
+name|M_REISERFSNODE
+argument_list|)
+expr_stmt|;
 name|pathrelse
 argument_list|(
 operator|&
