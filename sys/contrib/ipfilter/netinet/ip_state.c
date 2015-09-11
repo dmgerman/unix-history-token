b7835c0d1c3c55d7aae6f7d083ab2a83d93ee5b1
Cy Schubert <cy@FreeBSD.org>
2015-09-11 08:48:16 +0000

    Revert $FreeBSD$.
    Former-commit-id: c907301d70a39ecf2ab7a099d2ab360a6ceb360a

--- sys/contrib/ipfilter/netinet/ip_state.c
+++ sys/contrib/ipfilter/netinet/ip_state.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*	$NetBSD: ip_state.c,v 1.4 2012/12/20 21:42:28 christos Exp $	*/
+comment|/*	$FreeBSD$ */
 end_comment
 
 begin_comment
