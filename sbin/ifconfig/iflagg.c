5618c4819952abdad9a873792013f9a545b0580a
Garrett Cooper <ngie@FreeBSD.org>
2015-09-27 07:51:18 +0000

    Replace N #defines with nitems to simplify ifconfig code slightly
    MFC after: 1 week
    
    
    Former-commit-id: 92ef950320f240110a5c4abc79faafc6f8110db3

--- sbin/ifconfig/iflagg.c
+++ sbin/ifconfig/iflagg.c
@@ -1972,13 +1972,6 @@ parameter_list|(
 name|void
 parameter_list|)
 block|{
-define|#
-directive|define
-name|N
-parameter_list|(
-name|a
-parameter_list|)
-value|(sizeof(a) / sizeof(a[0]))
 name|int
 name|i
 decl_stmt|;
@@ -1990,7 +1983,7 @@ literal|0
 init|;
 name|i
 operator|<
-name|N
+name|nitems
 argument_list|(
 name|lagg_cmds
 argument_list|)
@@ -2013,9 +2006,6 @@ operator|&
 name|af_lagg
 argument_list|)
 expr_stmt|;
-undef|#
-directive|undef
-name|N
 block|}
 end_function
 
