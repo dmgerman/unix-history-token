d034603626e8dd6a7f4fed4942bae46ced610257
Hans Petter Selasky <hselasky@FreeBSD.org>
2015-10-08 08:30:40 +0000

    MFC r287775: Update TSO limits to include all headers.
    To make driver programming easier the TSO limits are changed to
    reflect the values used in the BUSDMA tag a network adapter driver is
    using. The TCP/IP network stack will subtract space for all linklevel
    and protocol level headers and ensure that the full mbuf chain passed
    to the network adapter fits within the given limits. See r287775
    for a more detailed description.
    
    Differential Revision:	https://reviews.freebsd.org/D3477
    Reviewed by:		rmacklem
    
    
    Former-commit-id: 16410b3e39478e6b08b8a80777aefe4de70d90ea

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -2962,6 +2962,8 @@ operator|(
 name|if_hw_tsomax
 operator|-
 name|hdrlen
+operator|-
+name|max_linkhdr
 operator|)
 expr_stmt|;
 if|if
@@ -3006,6 +3008,11 @@ operator|!=
 literal|0
 condition|)
 block|{
+comment|/* 				 * Subtract one segment for the LINK 				 * and TCP/IP headers mbuf that will 				 * be prepended to this mbuf chain 				 * after the code in this section 				 * limits the number of mbufs in the 				 * chain to if_hw_tsomaxsegcount. 				 */
+name|if_hw_tsomaxsegcount
+operator|-=
+literal|1
+expr_stmt|;
 name|max_len
 operator|=
 literal|0
