4e121c8c11ca4221fc4ea619920a42dd657b2e1c
Baptiste Daroussin <bapt@FreeBSD.org>
2015-10-13 20:45:29 +0000

    eliminate need for "print" definition
    By having space automatically classified as "print" type, we can
    eliminate the print section from ctype src files completely (they
    are just "graph" plus "<space>".
    
    Obtained from:	Dragonfly
    
    
    Former-commit-id: baf14f6f63d5653737d2e97d8f622acd9b6996a1

--- usr.bin/localedef/ctype.c
+++ usr.bin/localedef/ctype.c
@@ -1388,6 +1388,18 @@ name|ctype
 operator||=
 name|_ISBLANK
 expr_stmt|;
+if|if
+condition|(
+name|wc
+operator|==
+literal|' '
+condition|)
+name|ctn
+operator|->
+name|ctype
+operator||=
+name|_ISPRINT
+expr_stmt|;
 comment|/* 			 * Technically these settings are only 			 * required for the C locale.  However, it 			 * turns out that because of the historical 			 * version of isprint(), we need them for all 			 * locales as well.  Note that these are not 			 * necessarily valid punctation characters in 			 * the current language, but ispunct() needs 			 * to return TRUE for them. 			 */
 if|if
 condition|(
