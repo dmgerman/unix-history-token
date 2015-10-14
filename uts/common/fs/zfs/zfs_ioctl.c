ba90a957b769a386c255c1a6fad0dceadce07069
Alexander Motin <mav@FreeBSD.org>
2015-10-14 07:48:39 +0000

    6286 ZFS internal error when set large block on bootfs
    Reviewed by: Paul Dagnelie <pcd@delphix.com>
    Reviewed by: George Wilson <george.wilson@delphix.com>
    Reviewed by: Andriy Gapon <avg@FreeBSD.org>
    Approved by: Robert Mustacchi <rm@joyent.com>
    Author: Matthew Ahrens <mahrens@delphix.com>
    
    illumos/illumos-gate@6de9bb5603e65b16816b7ab29e39bac820e2da2b
    
    
    Former-commit-id: 479acec1a7a940ac36b98a267dabb279238053b9

--- uts/common/fs/zfs/zfs_ioctl.c
+++ uts/common/fs/zfs/zfs_ioctl.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Portions Copyright 2011 Martin Matuska  * Copyright 2015 Nexenta Systems, Inc.  All rights reserved.  * Copyright (c) 2014, Joyent, Inc. All rights reserved.  * Copyright (c) 2011, 2014 by Delphix. All rights reserved.  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.  * Copyright (c) 2013 Steven Hartland. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Portions Copyright 2011 Martin Matuska  * Copyright 2015 Nexenta Systems, Inc.  All rights reserved.  * Copyright (c) 2014, Joyent, Inc. All rights reserved.  * Copyright (c) 2011, 2015 by Delphix. All rights reserved.  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.  * Copyright (c) 2013 Steven Hartland. All rights reserved.  */
 end_comment
 
 begin_comment
@@ -16409,7 +16409,7 @@ return|return
 operator|(
 name|SET_ERROR
 argument_list|(
-name|EDOM
+name|ERANGE
 argument_list|)
 operator|)
 return|;
@@ -16429,7 +16429,7 @@ return|return
 operator|(
 name|SET_ERROR
 argument_list|(
-name|EDOM
+name|ERANGE
 argument_list|)
 operator|)
 return|;
