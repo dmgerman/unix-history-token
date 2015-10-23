5d5867be771dd74e3c46b8f7d869e32c4aabbf2e
Andriy Gapon <avg@FreeBSD.org>
2015-10-23 07:33:43 +0000

    MFC r288364: sdt module does not seem to actually use any symbol from opensolaris module
    Former-commit-id: fc82c98869fffd4e4c0cd498fc7fda96a1d499b1

--- sys/cddl/dev/sdt/sdt.c
+++ sys/cddl/dev/sdt/sdt.c
@@ -1818,21 +1818,5 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
-begin_expr_stmt
-name|MODULE_DEPEND
-argument_list|(
-name|sdt
-argument_list|,
-name|opensolaris
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|,
-literal|1
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
 end_unit
 
