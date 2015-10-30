b9633c988cb33d927764b48fd3e1dc939ba72a54
Xin Li <delphij@FreeBSD.org>
2015-10-30 00:46:52 +0000

    In pw_userlock, set 'name' to NULL when we encounter an all number string because it is also used as an indicator of whether a name or an UID is being used and we may have undefined results as 'name' may contain uninitialized stack contents.
    MFC after:	2 weeks
    
    
    Former-commit-id: 34842b6d2ca5aa49da60fa13537f20c8d38cc6a2

--- usr.sbin/pw/pw_user.c
+++ usr.sbin/pw/pw_user.c
@@ -1474,6 +1474,7 @@ index|]
 operator|==
 literal|'\0'
 condition|)
+block|{
 name|id
 operator|=
 name|pw_checkid
@@ -1483,6 +1484,11 @@ argument_list|,
 name|UID_MAX
 argument_list|)
 expr_stmt|;
+name|name
+operator|=
+name|NULL
+expr_stmt|;
+block|}
 else|else
 name|name
 operator|=
