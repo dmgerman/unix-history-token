6b624bf1bfac543133f5a811961df5762a7f2aa3
Dag-Erling Smorgrav <des@FreeBSD.org>
2015-11-26 23:05:40 +0000

    r291198 inadvertantly reverted a local patch for the default location of ssh-askpass and xauth, breaking X11 forwarding.
    Former-commit-id: 17efbcefdd144c28f4584a47753a27b619b2076b

--- crypto/openssh/pathnames.h
+++ crypto/openssh/pathnames.h
@@ -4,6 +4,10 @@ comment|/* $OpenBSD: pathnames.h,v 1.24 2013/12/06 13:39:49 markus Exp $ */
 end_comment
 
 begin_comment
+comment|/* $FreeBSD$ */
+end_comment
+
+begin_comment
 comment|/*  * Author: Tatu Ylonen<ylo@cs.hut.fi>  * Copyright (c) 1995 Tatu Ylonen<ylo@cs.hut.fi>, Espoo, Finland  *                    All rights reserved  *  * As far as I am concerned, the code I have written for this software  * can be used freely for any purpose.  Any derived versions of this  * software must be clearly marked as such, and if the derived work is  * incompatible with the protocol description in the RFC file, it must be  * called by a name other than "ssh" or "Secure Shell".  */
 end_comment
 
@@ -327,7 +331,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_SSH_ASKPASS_DEFAULT
-value|"/usr/X11R6/bin/ssh-askpass"
+value|"/usr/local/bin/ssh-askpass"
 end_define
 
 begin_endif
@@ -393,7 +397,7 @@ begin_define
 define|#
 directive|define
 name|_PATH_XAUTH
-value|"/usr/X11R6/bin/xauth"
+value|"/usr/local/bin/xauth"
 end_define
 
 begin_endif
