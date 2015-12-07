9f7417c3d7dc0ac3c34b2e7c31bcc5adcf5feedb
Ken Merry <ken@FreeBSD.org>
2015-12-07 21:04:27 +0000

    The ccb_xflags enumeration was removed from FreeBSD/head in r259397 (it contained the CAM_EXTLUN_VALID bit) and I added the same type name with a different set of values back in r291716.
    The old ccb_xflags enumeration still exists in FreeBSD stable/10.
    Shift all of the new values by one bit to avoid compatibility
    issues when merged to stable/10.
    
    MFC after:	3 days
    Sponsored by:	Spectra Logic
    
    
    Former-commit-id: 72f48875b74a459794172a4ab6a543c1cb013c4d

--- sys/cam/cam_ccb.h
+++ sys/cam/cam_ccb.h
@@ -399,17 +399,17 @@ enum|enum
 block|{
 name|CAM_USER_DATA_ADDR
 init|=
-literal|0x00000001
+literal|0x00000002
 block|,
 comment|/* Userspace data pointers */
 name|CAM_SG_FORMAT_IOVEC
 init|=
-literal|0x00000002
+literal|0x00000004
 block|,
 comment|/* iovec instead of busdma S/G*/
 name|CAM_UNMAPPED_BUF
 init|=
-literal|0x00000004
+literal|0x00000008
 comment|/* use unmapped I/O */
 block|}
 name|ccb_xflags
