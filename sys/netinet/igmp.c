b4e3d82e13141799c330b292c16e07ba8b6b8fcd
Andrey V. Elsukov <ae@FreeBSD.org>
2015-12-08 07:47:01 +0000

    MFC r291579:   In the same way fix the problem described in r291578 for IGMPv3.
      In case when router has a lot of multicast groups, the reply can take
      several packets due to MTU limitation.
      Also we have a limit IGMP_MAX_RESPONSE_BURST == 4, that limits the number
      of packets we send in one shot. Then we recalculate the timer value and
      schedule the remaining packets for sending.
      The problem is that when we call igmp_v3_dispatch_general_query() to send
      remaining packets, we queue new reply in the same mbuf queue. And when
      number of packets is bigger than IGMP_MAX_RESPONSE_BURST, we get endless
      reply of IGMPv3 reports.
      To fix this, add the check for remaining packets in the queue.
    
    
    Former-commit-id: 1f546d4a6533b7fb67b78c0f765b76267e3c047f

--- sys/netinet/igmp.c
+++ sys/netinet/igmp.c
@@ -12631,6 +12631,20 @@ name|igi_version
 operator|)
 argument_list|)
 expr_stmt|;
+comment|/* 	 * Check that there are some packets queued. If so, send them first. 	 * For large number of groups the reply to general query can take 	 * many packets, we should finish sending them before starting of 	 * queuing the new reply. 	 */
+if|if
+condition|(
+name|igi
+operator|->
+name|igi_gq
+operator|.
+name|ifq_head
+operator|!=
+name|NULL
+condition|)
+goto|goto
+name|send
+goto|;
 name|ifp
 operator|=
 name|igi
@@ -12776,6 +12790,8 @@ argument_list|(
 name|ifp
 argument_list|)
 expr_stmt|;
+name|send
+label|:
 name|loop
 operator|=
 operator|(
