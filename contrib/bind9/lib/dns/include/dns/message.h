0af102a67a7f817e48749612c1b349ed1e7bb888
Xin Li <delphij@FreeBSD.org>
2015-12-16 06:21:26 +0000

    Fix BIND remote denial of service vulnerability. [SA-15:27]
    Security:	FreeBSD-SA-15:27.bind
    Security:	CVE-2015-8000
    Approved by:	so
    
    Former-commit-id: 2eff06ef8cab3153d5ac8a4f4f60110709131261

--- contrib/bind9/lib/dns/include/dns/message.h
+++ contrib/bind9/lib/dns/include/dns/message.h
@@ -3,10 +3,6 @@ begin_comment
 comment|/*  * Copyright (C) 2004-2010, 2012, 2013  Internet Systems Consortium, Inc. ("ISC")  * Copyright (C) 1999-2003  Internet Software Consortium.  *  * Permission to use, copy, modify, and/or distribute this software for any  * purpose with or without fee is hereby granted, provided that the above  * copyright notice and this permission notice appear in all copies.  *  * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH  * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY  * AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,  * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM  * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE  * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR  * PERFORMANCE OF THIS SOFTWARE.  */
 end_comment
 
-begin_comment
-comment|/* $Id$ */
-end_comment
-
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -629,6 +625,18 @@ decl_stmt|;
 name|unsigned
 name|int
 name|free_saved
+range|:
+literal|1
+decl_stmt|;
+name|unsigned
+name|int
+name|tkey
+range|:
+literal|1
+decl_stmt|;
+name|unsigned
+name|int
+name|rdclass_set
 range|:
 literal|1
 decl_stmt|;
@@ -1937,6 +1945,24 @@ end_function_decl
 
 begin_comment
 comment|/*%<  * Built a opt record.  *  * Requires:  * \li   msg be a valid message.  * \li   opt to be a non NULL and *opt to be NULL.  *  * Returns:  * \li	 ISC_R_SUCCESS on success.  * \li	 ISC_R_NOMEMORY  * \li	 ISC_R_NOSPACE  * \li	 other.  */
+end_comment
+
+begin_function_decl
+name|void
+name|dns_message_setclass
+parameter_list|(
+name|dns_message_t
+modifier|*
+name|msg
+parameter_list|,
+name|dns_rdataclass_t
+name|rdclass
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_comment
+comment|/*%<  * Set the expected class of records in the response.  *  * Requires:  * \li   msg be a valid message with parsing intent.  */
 end_comment
 
 begin_macro
