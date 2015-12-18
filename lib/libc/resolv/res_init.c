f9fd7bff591948ea85061973468145e9057be675
Garrett Cooper <ngie@FreeBSD.org>
2015-12-18 07:00:42 +0000

    MFstable/10 r292430:
    MFC r292250:
    
    Allow users override `DEBUG` on the command line via DEBUG_FLAGS="-DDEBUG" with
    lib/libc/resolv by conditionalizing its definition
    
    Reviewed by: ume, vangyzen
    Differential Revision: https://reviews.freebsd.org/D4519
    
    
    Former-commit-id: 4f385cc9c586b2cfb1da0815aa85835736539d79

--- lib/libc/resolv/res_init.c
+++ lib/libc/resolv/res_init.c
@@ -272,11 +272,22 @@ directive|define
 name|RESOLVSORT
 end_define
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|DEBUG
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|DEBUG
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_ifdef
 ifdef|#
