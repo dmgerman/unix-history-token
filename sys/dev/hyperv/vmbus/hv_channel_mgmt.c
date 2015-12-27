ea3b24232903473d2ba5c22a75da6754c34093c8
Garrett Cooper <ngie@FreeBSD.org>
2015-12-27 21:03:39 +0000

    MFC r292661:
    Remove redundant vmbus_select_outgoing_channel declaration already handled
    in include/hyperv.h
    
    This unbreaks the gcc 4.2.1 kernel build of hyperv
    
    Differential Revision: https://reviews.freebsd.org/D4684
    Reviewed by: royger
    Sponsored by: EMC / Isilon Storage Division
    
    
    Former-commit-id: cbd0db3a60bc8fca84693c0107b86a8879bda760

--- sys/dev/hyperv/vmbus/hv_channel_mgmt.c
+++ sys/dev/hyperv/vmbus/hv_channel_mgmt.c
@@ -166,20 +166,6 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_function_decl
-name|struct
-name|hv_vmbus_channel
-modifier|*
-name|vmbus_select_outgoing_channel
-parameter_list|(
-name|struct
-name|hv_vmbus_channel
-modifier|*
-name|promary
-parameter_list|)
-function_decl|;
-end_function_decl
-
 begin_comment
 comment|/**  * Channel message dispatch table  */
 end_comment
