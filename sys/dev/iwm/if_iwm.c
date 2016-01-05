68fe39d2f8e628ad3ec16aa222bd9d6ff2dedd35
avos <avos@FreeBSD.org>
2016-01-05 20:09:26 +0000

    iwm: revert r293178
    This optimization is not proper (and causes kernel panic),
    since driver checks fw_status to optimize away parsing stage
    if it was already done.
    
    Reported by:	dchagin
    
    
    Former-commit-id: 3f6b7ef961f63a6437382c464575319333a48265

--- sys/dev/iwm/if_iwm.c
+++ sys/dev/iwm/if_iwm.c
@@ -9552,14 +9552,6 @@ argument_list|,
 name|ucode_type
 argument_list|)
 expr_stmt|;
-name|iwm_fw_info_free
-argument_list|(
-operator|&
-name|sc
-operator|->
-name|sc_fw
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|error
@@ -22936,6 +22928,16 @@ name|int
 name|do_net80211
 parameter_list|)
 block|{
+name|struct
+name|iwm_fw_info
+modifier|*
+name|fw
+init|=
+operator|&
+name|sc
+operator|->
+name|sc_fw
+decl_stmt|;
 name|device_t
 name|dev
 init|=
@@ -23023,6 +23025,20 @@ name|txq
 index|[
 name|i
 index|]
+argument_list|)
+expr_stmt|;
+comment|/* Free firmware */
+if|if
+condition|(
+name|fw
+operator|->
+name|fw_fp
+operator|!=
+name|NULL
+condition|)
+name|iwm_fw_info_free
+argument_list|(
+name|fw
 argument_list|)
 expr_stmt|;
 comment|/* Free scheduler */
