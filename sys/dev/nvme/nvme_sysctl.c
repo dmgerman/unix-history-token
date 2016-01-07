a6cc0bbac8eba561173117be89e5caf97f723b23
Jim Harris <jimharris@FreeBSD.org>
2016-01-07 20:32:04 +0000

    nvme: add hw.nvme.min_cpus_per_ioq tunable
    Due to FreeBSD system-wide limits on number of MSI-X vectors
    (https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=199321),
    it may be desirable to allocate fewer than the maximum number
    of vectors for an NVMe device, in order to save vectors for
    other devices (usually Ethernet) that can take better
    advantage of them and may be probed after NVMe.
    
    This tunable is expressed in terms of minimum number of CPUs
    per I/O queue instead of max number of queues per controller,
    to allow for a more even distribution of CPUs per queue.  This
    avoids cases where some number of CPUs have a dedicated queue,
    but other CPUs need to share queues.  Ideally the PR referenced
    above will eventually be fixed and the mechanism implemented
    here becomes obsolete anyways.
    
    While here, fix a bug in the CPUs per I/O queue calculation to
    properly account for the admin queue's MSI-X vector.
    
    Reviewed by:	gallatin
    MFC after:	3 days
    Sponsored by:	Intel
    
    
    Former-commit-id: 7a2516bcbb64ee7d56a9c88bfd8a90f842bbfdeb

--- sys/dev/nvme/nvme_sysctl.c
+++ sys/dev/nvme/nvme_sysctl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 2012-2013 Intel Corporation  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (C) 2012-2016 Intel Corporation  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_include
@@ -1106,6 +1106,28 @@ operator|=
 name|SYSCTL_CHILDREN
 argument_list|(
 name|ctrlr_tree
+argument_list|)
+expr_stmt|;
+name|SYSCTL_ADD_UINT
+argument_list|(
+name|ctrlr_ctx
+argument_list|,
+name|ctrlr_list
+argument_list|,
+name|OID_AUTO
+argument_list|,
+literal|"num_cpus_per_ioq"
+argument_list|,
+name|CTLFLAG_RD
+argument_list|,
+operator|&
+name|ctrlr
+operator|->
+name|num_cpus_per_ioq
+argument_list|,
+literal|0
+argument_list|,
+literal|"Number of CPUs assigned per I/O queue pair"
 argument_list|)
 expr_stmt|;
 name|SYSCTL_ADD_PROC
