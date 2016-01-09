8398d4258ed971dbdfcc27a847ad837fab6b7e53
Dmitry Chagin <dchagin@FreeBSD.org>
2016-01-09 17:54:37 +0000

    MFC r283492:
    Implement Linux specific syncfs() system call.
    
    
    Former-commit-id: 562b92ab3b69ce91df4257edef69e98f5542fd1d

--- sys/amd64/linux32/linux32_dummy.c
+++ sys/amd64/linux32/linux32_dummy.c
@@ -678,14 +678,6 @@ end_expr_stmt
 begin_expr_stmt
 name|DUMMY
 argument_list|(
-name|syncfs
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_expr_stmt
-name|DUMMY
-argument_list|(
 name|setns
 argument_list|)
 expr_stmt|;
