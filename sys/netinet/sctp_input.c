6f5031b1dc1e37afd4671a11a4d04bab1d6f3c49
Michael Tuexen <tuexen@FreeBSD.org>
2016-01-16 12:47:28 +0000

    MFC r286206: Don't take the port numbers for packets containing ABORT chunks from a freed mbuf. Just use them from the stcb.
    Former-commit-id: 34b9871894c0e3820ab2e478a070e0c003ce57dd

--- sys/netinet/sctp_input.c
+++ sys/netinet/sctp_input.c
@@ -30206,12 +30206,6 @@ name|offset
 argument_list|,
 name|length
 argument_list|,
-name|src
-argument_list|,
-name|dst
-argument_list|,
-name|sh
-argument_list|,
 name|inp
 argument_list|,
 name|stcb
@@ -30220,14 +30214,6 @@ name|net
 argument_list|,
 operator|&
 name|high_tsn
-argument_list|,
-name|mflowtype
-argument_list|,
-name|mflowid
-argument_list|,
-name|vrf_id
-argument_list|,
-name|port
 argument_list|)
 expr_stmt|;
 if|if
