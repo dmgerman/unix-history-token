a870e596d9a2a4d659f6de0519c6aefe4f7d06e1
Andrew Rybchenko <arybchik@FreeBSD.org>
2016-01-18 06:18:01 +0000

    sfxge: convert nvram set_version method to use partition id
    Submitted by:   Andy Moreton <amoreton at solarflare.com>
    Sponsored by:   Solarflare Communications, Inc.
    MFC after:      2 days
    
    
    Former-commit-id: f9135aeb89957a7fedc711c06cb4ddf8213d04b6

--- sys/dev/sfxge/common/efx_nvram.c
+++ sys/dev/sfxge/common/efx_nvram.c
@@ -56,9 +56,6 @@ comment|/* envo_test */
 endif|#
 directive|endif
 comment|/* EFSYS_OPT_DIAG */
-name|falcon_nvram_set_version
-block|,
-comment|/* envo_set_version */
 name|falcon_nvram_type_to_partn
 block|,
 comment|/* envo_type_to_partn */
@@ -83,6 +80,9 @@ comment|/* envo_partn_rw_finish */
 name|falcon_nvram_partn_get_version
 block|,
 comment|/* envo_partn_get_version */
+name|falcon_nvram_partn_set_version
+block|,
+comment|/* envo_partn_set_version */
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -117,9 +117,6 @@ comment|/* envo_test */
 endif|#
 directive|endif
 comment|/* EFSYS_OPT_DIAG */
-name|siena_nvram_set_version
-block|,
-comment|/* envo_set_version */
 name|siena_nvram_type_to_partn
 block|,
 comment|/* envo_type_to_partn */
@@ -144,6 +141,9 @@ comment|/* envo_partn_rw_finish */
 name|siena_nvram_partn_get_version
 block|,
 comment|/* envo_partn_get_version */
+name|siena_nvram_partn_set_version
+block|,
+comment|/* envo_partn_set_version */
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -180,9 +180,6 @@ comment|/* envo_test */
 endif|#
 directive|endif
 comment|/* EFSYS_OPT_DIAG */
-name|ef10_nvram_set_version
-block|,
-comment|/* envo_set_version */
 name|ef10_nvram_type_to_partn
 block|,
 comment|/* envo_type_to_partn */
@@ -207,6 +204,9 @@ comment|/* envo_partn_rw_finish */
 name|ef10_nvram_partn_get_version
 block|,
 comment|/* envo_partn_get_version */
+name|ef10_nvram_partn_set_version
+block|,
+comment|/* envo_partn_set_version */
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -1677,6 +1677,9 @@ name|enp
 operator|->
 name|en_envop
 decl_stmt|;
+name|uint32_t
+name|partn
+decl_stmt|;
 name|efx_rc_t
 name|rc
 decl_stmt|;
@@ -1741,13 +1744,14 @@ name|rc
 operator|=
 name|envop
 operator|->
-name|envo_set_version
+name|envo_type_to_partn
 argument_list|(
 name|enp
 argument_list|,
 name|type
 argument_list|,
-name|version
+operator|&
+name|partn
 argument_list|)
 operator|)
 operator|!=
@@ -1756,11 +1760,40 @@ condition|)
 goto|goto
 name|fail1
 goto|;
+if|if
+condition|(
+operator|(
+name|rc
+operator|=
+name|envop
+operator|->
+name|envo_partn_set_version
+argument_list|(
+name|enp
+argument_list|,
+name|partn
+argument_list|,
+name|version
+argument_list|)
+operator|)
+operator|!=
+literal|0
+condition|)
+goto|goto
+name|fail2
+goto|;
 return|return
 operator|(
 literal|0
 operator|)
 return|;
+name|fail2
+label|:
+name|EFSYS_PROBE
+argument_list|(
+name|fail2
+argument_list|)
+expr_stmt|;
 name|fail1
 label|:
 name|EFSYS_PROBE1
