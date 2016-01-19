060ca737f513eff41f8256b3de3852d5fa4e75e4
John Baldwin <jhb@FreeBSD.org>
2016-01-19 19:04:56 +0000

    MFC 292410,292443,293977: Exit cleanly if malloc() fails to allocate a buffer for a copy of the current MBR.
    PR:		205322
    Submitted by:	Alexander Kuleshov <kuleshovmail@gmail.com>
    
    Former-commit-id: 38b6117cbc57b1a9a6a244f6cda1eb31192b0ad1

--- usr.sbin/boot0cfg/boot0cfg.c
+++ usr.sbin/boot0cfg/boot0cfg.c
@@ -1758,6 +1758,9 @@ name|mbr_size
 operator|)
 return|;
 block|}
+if|if
+condition|(
+operator|(
 operator|*
 name|mbr
 operator|=
@@ -1767,6 +1770,18 @@ sizeof|sizeof
 argument_list|(
 name|buf
 argument_list|)
+argument_list|)
+operator|)
+operator|==
+name|NULL
+condition|)
+name|errx
+argument_list|(
+literal|1
+argument_list|,
+literal|"%s: unable to allocate MBR buffer"
+argument_list|,
+name|disk
 argument_list|)
 expr_stmt|;
 name|memcpy
