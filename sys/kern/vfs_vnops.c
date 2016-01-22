767c3bdad4c4d1ce7560235ed20eb5cf538de5f1
Konstantin Belousov <kib@FreeBSD.org>
2016-01-22 20:35:20 +0000

    The struct file f_advice member is overlaid with the devfs f_cdevpriv data.  If vnode bypass for devfs file failed, vn_read/vn_write are called and might try to dereference f_advice.  Limit the accesses to f_advice to VREG vnodes only, which is the type ensured by posix_fadvise().
    The f_advice for regular files is protected by mtxpool lock.  Recheck
    that f_advice is not NULL after lock is taken.
    
    Reported and tested by:	bde
    Sponsored by:	The FreeBSD Foundation
    MFC after:	3 weeks
    
    
    Former-commit-id: 7c0d63d9ba48cddd951a5668f3c6cee41a737128

--- sys/kern/vfs_vnops.c
+++ sys/kern/vfs_vnops.c
@@ -3581,6 +3581,14 @@ operator|->
 name|f_advice
 operator|==
 name|NULL
+operator|||
+name|fp
+operator|->
+name|f_vnode
+operator|->
+name|v_type
+operator|!=
+name|VREG
 condition|)
 return|return
 operator|(
@@ -3603,6 +3611,12 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
+name|fp
+operator|->
+name|f_advice
+operator|!=
+name|NULL
+operator|&&
 name|uio
 operator|->
 name|uio_offset
