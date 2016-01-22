aaf46f2af89966a36e7ea0ce6cebbd8665490b11
Dimitry Andric <dim@FreeBSD.org>
2016-01-22 21:18:04 +0000

    Vendor import of libc++ release_38 branch r258549: https://llvm.org/svn/llvm-project/libcxx/branches/release_38@258549
    Former-commit-id: 78fdc874a112136e4f9212585c20c95a453de8e0

--- src/include/atomic_support.h
+++ src/include/atomic_support.h
@@ -1,4 +1,36 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|//===----------------------------------------------------------------------===////
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|//                     The LLVM Compiler Infrastructure
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|// This file is dual licensed under the MIT and the University of Illinois Open
+end_comment
+
+begin_comment
+comment|// Source Licenses. See LICENSE.TXT for details.
+end_comment
+
+begin_comment
+comment|//
+end_comment
+
+begin_comment
+comment|//===----------------------------------------------------------------------===////
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
