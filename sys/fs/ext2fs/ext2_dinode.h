ff80d612015d6777e8b1983ac3cffebbcfd3acf2
Pedro Giffuni <pfg@FreeBSD.org>
2016-01-24 02:44:00 +0000

    Fix comment.
    Former-commit-id: ef00f61907eb53cccb43324ab89323331cafd592

--- sys/fs/ext2fs/ext2_dinode.h
+++ sys/fs/ext2fs/ext2_dinode.h
@@ -97,7 +97,7 @@ value|((ino_t)11)
 end_define
 
 begin_comment
-comment|/*  * Inode flags  * The system supports EXT2_IMMUTABLE, EXT2_APPEND and EXT2_NODUMP flags.  * The current implementation also uses EXT3_INDEX, EXT4_EXTENTS and  * EXT4_HUGE_FILE with some restrictions, imposed the lack of write  * support.  */
+comment|/*  * Inode flags  * The system supports EXT2_IMMUTABLE, EXT2_APPEND and EXT2_NODUMP flags.  * The current implementation also uses EXT3_INDEX, EXT4_EXTENTS and  * EXT4_HUGE_FILE with some restrictions imposed by the lack of write  * support.  */
 end_comment
 
 begin_define
