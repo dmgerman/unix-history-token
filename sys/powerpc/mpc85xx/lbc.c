d2af4086c96f9598ce8cb3a0e7fe576e7d2406e6
Glen Barber <gjb@FreeBSD.org>
2016-02-03 02:02:01 +0000

    MFH
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: 83fa5bd44aa088455b30b0ad59f0db0504a58f30

--- sys/powerpc/mpc85xx/lbc.c
+++ sys/powerpc/mpc85xx/lbc.c
@@ -438,7 +438,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_expr_stmt
-name|DRIVER_MODULE
+name|EARLY_DRIVER_MODULE
 argument_list|(
 name|lbc
 argument_list|,
@@ -451,6 +451,8 @@ argument_list|,
 literal|0
 argument_list|,
 literal|0
+argument_list|,
+name|BUS_PASS_BUS
 argument_list|)
 expr_stmt|;
 end_expr_stmt
