3045d44bd50421edfa8146b41d9d799504718430
Konstantin Belousov <kib@FreeBSD.org>
2016-02-08 19:24:13 +0000

    If libthr.so is dlopened without RTLD_GLOBAL flag, the libthr symbols do not participate in the global symbols namespace, but rtld locks are still replaced and functions are interposed.  In particular, __pthread_map_stacks_exec is resolved to the libc version.  If a library is loaded later, which requires adjustment of the stack protection mode, rtld calls into libc __pthread_map_stacks_exec due to the symbols scope.  The libc version might recurse into binder and recursively acquire rtld bind lock, causing the hang.
    Make libc __pthread_map_stacks_exec() interposed, which synchronizes
    rtld locks and version of the stack exec hook when libthr loaded,
    regardless of the symbol scope control or symbol resolution order.
    
    The __pthread_map_stacks_exec() symbol is removed from the private
    version in libthr since libc symbol now operates correctly in presence
    of libthr.
    
    Reported and tested by:	markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 weeks
    
    
    Former-commit-id: 40cb4490475a9283dce75a41304e8c1d04a5311b

--- lib/libc/include/libc_private.h
+++ lib/libc/include/libc_private.h
@@ -529,6 +529,8 @@ name|INTERPOS_wait6
 block|,
 name|INTERPOS_ppoll
 block|,
+name|INTERPOS_map_stacks_exec
+block|,
 name|INTERPOS_MAX
 block|}
 enum|;
@@ -1768,6 +1770,13 @@ name|__init_elf_aux_vector
 parameter_list|(
 name|void
 parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|__libc_map_stacks_exec
+parameter_list|()
 function_decl|;
 end_function_decl
 
