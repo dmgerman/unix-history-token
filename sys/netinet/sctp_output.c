22fbc7551b887c286fe9038c441f2799ca5f101d
Michael Tuexen <tuexen@FreeBSD.org>
2016-02-18 21:33:10 +0000

    Add protection code.
    MFC after:	3 days
    CID:		748858
    
    Former-commit-id: 5c848c1ca978d149397d0b9d1d021a87114939fa

--- sys/netinet/sctp_output.c
+++ sys/netinet/sctp_output.c
@@ -15498,6 +15498,11 @@ directive|ifdef
 name|INET
 if|if
 condition|(
+name|stcb
+condition|)
+block|{
+if|if
+condition|(
 operator|(
 name|retried
 operator|==
@@ -15553,6 +15558,7 @@ name|ipv4_local_scope
 operator|=
 literal|0
 expr_stmt|;
+block|}
 block|}
 endif|#
 directive|endif
