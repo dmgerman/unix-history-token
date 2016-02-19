bb413847bff2dd8324aa56de4d4f7f222c7516c1
skra <skra@FreeBSD.org>
2016-02-19 08:35:29 +0000

    Rename pmap.h to pmap-v4.h and remove pmap-v6.h include from it. Create new pmap.h which includes specific header according to __ARM_ARCH.
    Note that <machine/pmap.h> is included from <vm/pmap.h> so one common
    <machine/pmap.h> must exist.
    
    
    Former-commit-id: 103548d48a3e357834c1d28bad4e5d2f6de60327

--- sys/arm/include/pmap-v6.h
+++ sys/arm/include/pmap-v6.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_MACHINE_PMAP_H_
+name|_MACHINE_PMAP_V6_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_MACHINE_PMAP_H_
+name|_MACHINE_PMAP_V6_H_
 end_define
 
 begin_include
@@ -857,7 +857,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* !_MACHINE_PMAP_H_ */
+comment|/* !_MACHINE_PMAP_V6_H_ */
 end_comment
 
 end_unit
