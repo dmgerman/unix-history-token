b161afb681eed73401de1e500de37fd2200a63e4
skra <skra@FreeBSD.org>
2016-02-22 09:10:23 +0000

    As <machine/vm.h> is included from <vm/vm.h>, there is no need to include it explicitly when <vm/vm.h> is already included.
    Reviewed by:	alc, kib
    Differential Revision:	https://reviews.freebsd.org/D5380
    
    
    Former-commit-id: 34516362a2342d4f4587dd05dcaf4f615da3368e

--- sys/amd64/vmm/vmm.c
+++ sys/amd64/vmm/vmm.c
@@ -152,12 +152,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/vm.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/pcb.h>
 end_include
 
--- sys/dev/altera/avgen/altera_avgen_nexus.c
+++ sys/dev/altera/avgen/altera_avgen_nexus.c
@@ -110,12 +110,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<machine/vm.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<vm/vm.h>
 end_include
 
