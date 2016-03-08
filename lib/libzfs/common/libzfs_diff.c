9c48f4f3ffaa66b54b85a8f70e3cb0221ca605a2
Alexander Motin <mav@FreeBSD.org>
2016-03-08 18:51:12 +0000

    4448 zfs diff misprints unicode characters
    Reviewed by: Igor Kozhukhov <ikozhukhov@gmail.com>
    Reviewed by: Toomas Soome <tsoome@me.com>
    Approved by: Matthew Ahrens <mahrens@delphix.com>
    Author: Joshua M. Clulow <jmc@joyent.com>
    
    illumos/illumos-gate@b211eb9181f99c20acbf4c528f94cb44b4ca8c31
    
    
    Former-commit-id: 8802c25f5d1ef4650e9a8ca6896f2dd512052a9f

--- lib/libzfs/common/libzfs_diff.c
+++ lib/libzfs/common/libzfs_diff.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2015 Nexenta Systems, Inc. All rights reserved.  */
+comment|/*  * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2015 Nexenta Systems, Inc. All rights reserved.  * Copyright 2016 Joyent, Inc.  */
 end_comment
 
 begin_comment
@@ -549,29 +549,37 @@ modifier|*
 name|string
 parameter_list|)
 block|{
+name|char
+name|c
+decl_stmt|;
 while|while
 condition|(
+operator|(
+name|c
+operator|=
 operator|*
 name|string
+operator|++
+operator|)
+operator|!=
+literal|'\0'
 condition|)
 block|{
 if|if
 condition|(
-operator|*
-name|string
+name|c
 operator|>
 literal|' '
 operator|&&
-operator|*
-name|string
+name|c
 operator|!=
 literal|'\\'
 operator|&&
-operator|*
-name|string
+name|c
 operator|<
 literal|'\177'
 condition|)
+block|{
 operator|(
 name|void
 operator|)
@@ -581,12 +589,12 @@ name|fp
 argument_list|,
 literal|"%c"
 argument_list|,
-operator|*
-name|string
-operator|++
+name|c
 argument_list|)
 expr_stmt|;
+block|}
 else|else
+block|{
 operator|(
 name|void
 operator|)
@@ -596,11 +604,13 @@ name|fp
 argument_list|,
 literal|"\\%03o"
 argument_list|,
-operator|*
-name|string
-operator|++
+operator|(
+name|uint8_t
+operator|)
+name|c
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 block|}
 end_function
