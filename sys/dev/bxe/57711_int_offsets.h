7c59301d12e8b034297e2e1e379e9364bd22ec37
David C Somayajulu <davidcs@FreeBSD.org>
2016-03-09 21:40:00 +0000

    MFC r296071   Upgrade the firmware carried in driver and loaded during hardware   initialization (a.k.a STORM firmware) to version 7.13.1 (latest version)
    Former-commit-id: 19868579e6f2f205c41795a6d46153228845448d

--- sys/dev/bxe/57711_int_offsets.h
+++ sys/dev/bxe/57711_int_offsets.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2007-2014 QLogic Corporation. All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS'  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 2007-2017 QLogic Corporation. All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF  * THE POSSIBILITY OF SUCH DAMAGE.  */
 end_comment
 
 begin_include
@@ -24,7 +24,7 @@ name|struct
 name|iro
 name|e1h_iro_arr
 index|[
-literal|379
+literal|385
 index|]
 init|=
 block|{
@@ -1368,6 +1368,58 @@ block|}
 block|,
 comment|// TSTORM_ASSERT_LIST_INDEX_OFFSET
 block|{
+name|UNDEF_IRO
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|// TSTORM_MEASURE_PCI_LATENCY_CTRL_OFFSET
+block|{
+name|UNDEF_IRO
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|// TSTORM_MEASURE_PCI_LATENCY_DATA_OFFSET
+block|{
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|// TSTORM_AGG_MEASURE_PCI_LATENCY_INDEX
+block|{
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|// TSTORM_AGG_MEASURE_PCI_LATENCY_COMP_TYPE
+block|{
 literal|0xa080
 block|,
 literal|0x8
@@ -2693,6 +2745,32 @@ literal|0x8
 block|}
 block|,
 comment|// TSTORM_ACCEPT_CLASSIFY_FAILED_OFFSET
+block|{
+name|UNDEF_IRO
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|// TSTORM_ACCEPT_CLASSIFY_FAIL_E2_ENABLE_OFFSET(portId)
+block|{
+name|UNDEF_IRO
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|,
+literal|0x0
+block|}
+block|,
+comment|// TSTORM_ACCEPT_CLASSIFY_FAIL_E2_VNIC_OFFSET(portId)
 block|{
 name|UNDEF_IRO
 block|,
