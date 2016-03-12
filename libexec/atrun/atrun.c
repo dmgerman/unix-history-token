97970d775685c580bb988773d02845fef149ea20
Garrett Cooper <ngie@FreeBSD.org>
2016-03-12 19:09:51 +0000

    MFC r295079:
    Fix the type for hw.ncpu, so sysctlbyname doesn't consistently fail on
    64-bit architectures where sizeof(int) != sizeof(size_t).
    
    PR: 206758
    
    Former-commit-id: 9de5f40cf1e7c2170b915002a581c31cec33f77c

--- libexec/atrun/atrun.c
+++ libexec/atrun/atrun.c
@@ -1943,8 +1943,6 @@ ifdef|#
 directive|ifdef
 name|__FreeBSD__
 name|size_t
-name|ncpu
-decl_stmt|,
 name|ncpusz
 decl_stmt|;
 name|double
@@ -1952,6 +1950,9 @@ name|load_avg
 init|=
 operator|-
 literal|1
+decl_stmt|;
+name|int
+name|ncpu
 decl_stmt|;
 else|#
 directive|else
