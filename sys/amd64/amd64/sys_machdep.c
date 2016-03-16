482944751abc896b568789bbcc817effc080ad92
Gleb Smirnoff <glebius@FreeBSD.org>
2016-03-16 22:35:55 +0000

    Merge r296956:
      Due to invalid use of a signed intermediate value in the bounds checking
      during argument validity verification, unbound zero'ing of the process LDT
      and adjacent memory can be initiated from usermode.
    
    Submitted by:   CORE Security
    Patch by:       kib
    Security:       SA-16:15
    
    Former-commit-id: a488825a1c77898dac67a0fe0e2861c992debef5

--- sys/amd64/amd64/sys_machdep.c
+++ sys/amd64/amd64/sys_machdep.c
@@ -2964,11 +2964,12 @@ name|int
 name|error
 init|=
 literal|0
-decl_stmt|,
-name|i
 decl_stmt|;
+name|unsigned
 name|int
 name|largest_ld
+decl_stmt|,
+name|i
 decl_stmt|;
 name|struct
 name|mdproc
