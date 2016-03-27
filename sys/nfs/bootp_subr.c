4f35bd0cbf31c7c3b60b782be28da0b5b9093044
Ian Lepore <ian@FreeBSD.org>
2016-03-27 22:36:32 +0000

    Switch bootpc_adjust_interface() from returning int to void.  Its one caller doesn't check for errors, and all the errors that can happen result in it calling panic anyway, except for one that's really more of a warning (and is going to disappear on an upcoming commit anyway).
    Former-commit-id: 67627349cc3e9bbe927ec07813399ba64efe2b38

--- sys/nfs/bootp_subr.c
+++ sys/nfs/bootp_subr.c
@@ -1284,7 +1284,7 @@ end_function_decl
 
 begin_function_decl
 specifier|static
-name|int
+name|void
 name|bootpc_adjust_interface
 parameter_list|(
 name|struct
@@ -4682,7 +4682,7 @@ end_function
 
 begin_function
 specifier|static
-name|int
+name|void
 name|bootpc_adjust_interface
 parameter_list|(
 name|struct
@@ -4795,11 +4795,7 @@ argument_list|,
 name|td
 argument_list|)
 expr_stmt|;
-return|return
-operator|(
-literal|0
-operator|)
-return|;
+return|return;
 block|}
 name|printf
 argument_list|(
@@ -5116,18 +5112,9 @@ argument_list|,
 name|error
 argument_list|)
 expr_stmt|;
-return|return
-operator|(
-name|error
-operator|)
-return|;
+return|return;
 block|}
 block|}
-return|return
-operator|(
-literal|0
-operator|)
-return|;
 block|}
 end_function
 
