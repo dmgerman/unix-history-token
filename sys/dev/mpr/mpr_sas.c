05f7126e8d8c0afb508c9e9e9d96c056ddaeda58
Alexander Motin <mav@FreeBSD.org>
2016-04-08 10:00:07 +0000

    MFC r266615: Increase taskqueue thread priority from idle to PRIBIO.
    Idle priority is not even time-share, so if system is busy in any way,
    those events may never be executed.  Since in some cases system waits
    for events processed by that thread, that may cause deadlocks.
    
    
    Former-commit-id: 1b200206f2bdba554bba89f60d4a736a71ca5907

--- sys/dev/mpr/mpr_sas.c
+++ sys/dev/mpr/mpr_sas.c
@@ -4180,7 +4180,6 @@ operator|->
 name|ev_tq
 argument_list|)
 expr_stmt|;
-comment|/* Run the task queue with lowest priority */
 name|taskqueue_start_threads
 argument_list|(
 operator|&
@@ -4190,7 +4189,7 @@ name|ev_tq
 argument_list|,
 literal|1
 argument_list|,
-literal|255
+name|PRIBIO
 argument_list|,
 literal|"%s taskq"
 argument_list|,
