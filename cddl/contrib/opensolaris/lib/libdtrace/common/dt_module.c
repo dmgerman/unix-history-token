9e766e2e33df54d356a16441b109661d05221430
Mark Johnston <markj@FreeBSD.org>
2016-04-14 04:54:47 +0000

    MFC r297827: libdtrace: Add a missing unlock to an error handler.
    Former-commit-id: 6f2b956eef9ee61a6fc7acb5ab98a98b152ef658

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_module.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_module.c
@@ -3760,6 +3760,13 @@ argument_list|(
 literal|"failed to iterate objects\n"
 argument_list|)
 expr_stmt|;
+name|dt_proc_unlock
+argument_list|(
+name|dtp
+argument_list|,
+name|p
+argument_list|)
+expr_stmt|;
 name|dt_proc_release
 argument_list|(
 name|dtp
