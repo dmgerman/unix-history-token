4a86e9d4e940f07e23edd1113731113e83c76f58
Sean Bruno <sbruno@FreeBSD.org>
2016-04-19 16:48:14 +0000

    Plug memory leak in ctl(4) when ctl_copyin_args() is called with a non- null terminated ASCII string.
    PR:		207626
    Submitted by:	cturt@hardenedbsd.org
    MFC after:	2 days
    
    Former-commit-id: 91a4a9c9649066926c72b13eced761c5d2c60035

--- sys/cam/ctl/ctl.c
+++ sys/cam/ctl/ctl.c
@@ -14557,6 +14557,13 @@ argument_list|,
 name|i
 argument_list|)
 expr_stmt|;
+name|free
+argument_list|(
+name|tmpptr
+argument_list|,
+name|M_CTL
+argument_list|)
+expr_stmt|;
 goto|goto
 name|bailout
 goto|;
