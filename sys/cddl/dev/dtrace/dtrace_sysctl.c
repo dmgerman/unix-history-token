760b879229bef40cfabced6c35ee44443b99a19d
Mark Johnston <markj@FreeBSD.org>
2016-04-25 18:09:36 +0000

    Add a kern.dtrace.err_verbose sysctl to control dtrace_err_verbose.
    When this flag is turned on, DOF and DIF validation errors are printed to
    the kernel message buffer. This is useful for debugging.
    
    Also remove the debug.dtrace.debug sysctl, which has no effect.
    
    
    Former-commit-id: b7184bfa8e5d8645a22b0f76f11f8767dfa04be9

--- sys/cddl/dev/dtrace/dtrace_sysctl.c
+++ sys/cddl/dev/dtrace/dtrace_sysctl.c
@@ -3,53 +3,6 @@ begin_comment
 comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to the terms of the  * Common Development and Distribution License (the "License").  * You may not use this file except in compliance with the License.  *  * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE  * or http://www.opensolaris.org/os/licensing.  * See the License for the specific language governing permissions  * and limitations under the License.  *  * When distributing Covered Code, include this CDDL HEADER in each  * file and include the License file at usr/src/OPENSOLARIS.LICENSE.  * If applicable, add the following below this CDDL HEADER, with the  * fields enclosed by brackets "[]" replaced with your own identifying  * information: Portions Copyright [yyyy] [name of copyright owner]  *  * CDDL HEADER END  *  * $FreeBSD$  *  */
 end_comment
 
-begin_expr_stmt
-name|SYSCTL_NODE
-argument_list|(
-name|_debug
-argument_list|,
-name|OID_AUTO
-argument_list|,
-name|dtrace
-argument_list|,
-name|CTLFLAG_RD
-argument_list|,
-literal|0
-argument_list|,
-literal|"DTrace debug parameters"
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_decl_stmt
-name|int
-name|dtrace_debug
-init|=
-literal|0
-decl_stmt|;
-end_decl_stmt
-
-begin_expr_stmt
-name|SYSCTL_INT
-argument_list|(
-name|_debug_dtrace
-argument_list|,
-name|OID_AUTO
-argument_list|,
-name|debug
-argument_list|,
-name|CTLFLAG_RWTUN
-argument_list|,
-operator|&
-name|dtrace_debug
-argument_list|,
-literal|0
-argument_list|,
-literal|""
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
 begin_comment
 comment|/* Report registered DTrace providers. */
 end_comment
@@ -251,6 +204,24 @@ block|}
 end_function
 
 begin_expr_stmt
+name|SYSCTL_NODE
+argument_list|(
+name|_debug
+argument_list|,
+name|OID_AUTO
+argument_list|,
+name|dtrace
+argument_list|,
+name|CTLFLAG_RD
+argument_list|,
+literal|0
+argument_list|,
+literal|"DTrace debug parameters"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
 name|SYSCTL_PROC
 argument_list|(
 name|_debug_dtrace
@@ -290,6 +261,27 @@ argument_list|,
 literal|0
 argument_list|,
 literal|"DTrace parameters"
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_expr_stmt
+name|SYSCTL_INT
+argument_list|(
+name|_kern_dtrace
+argument_list|,
+name|OID_AUTO
+argument_list|,
+name|err_verbose
+argument_list|,
+name|CTLFLAG_RW
+argument_list|,
+operator|&
+name|dtrace_err_verbose
+argument_list|,
+literal|0
+argument_list|,
+literal|"print DIF and DOF validation errors to the message buffer"
 argument_list|)
 expr_stmt|;
 end_expr_stmt
