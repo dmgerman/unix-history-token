e634f07998fa6b5c3ea87ba158ed43998b1edbfa
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-04-26 05:08:55 +0000

    hyperv/hn: Avoid sub-channel creation callback.
    Since the sub-channel offers are synchronized, we can do our own
    channel setup without using the sub-channel creation callback.
    
    This paves the way to whack the sub-channel creation callback.
    
    MFC after:	1 week
    Sponsored by:	Microsoft OSTC
    
    
    Former-commit-id: eaff6777191c03acd441e06d52b7fdb35d8dc7ee

--- sys/dev/hyperv/netvsc/hv_net_vsc.h
+++ sys/dev/hyperv/netvsc/hv_net_vsc.h
@@ -2830,6 +2830,18 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|hv_nv_subchan_attach
+parameter_list|(
+name|struct
+name|hv_vmbus_channel
+modifier|*
+name|chan
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
