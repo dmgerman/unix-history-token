7199e63721ea28ec77c33cd67b6099c2556928db
Roger Pau Monne <royger@FreeBSD.org>
2016-05-02 16:14:55 +0000

    atrtc: export function to set RTC
    This is going to be used by the Xen clock on Dom0 in order to set the RTC of
    the host. The current logic in atrtc_settime is moved to atrtc_set and the
    unused device_t parameter is removed from the atrtc_set function call so it
    can be safely used by other callers.
    
    Sponsored by:		Citrix Systems R&D
    Reviewed by:		kib, jhb
    Differential revision:	https://reviews.freebsd.org/D6067
    
    
    Former-commit-id: e7db8f35e92493ffa9810f36b662da93dba17670

--- sys/isa/rtc.h
+++ sys/isa/rtc.h
@@ -700,6 +700,18 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|atrtc_set
+parameter_list|(
+name|struct
+name|timespec
+modifier|*
+name|ts
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
