4ea07ba9fe065fd5ff3a4c3d6c7b30de4448a9dc
Andrew Rybchenko <arybchik@FreeBSD.org>
2016-05-10 07:15:09 +0000

    sfxge(4): remove EFSYS_OPT_NVRAM_SFX7101
    Submitted by:   Andy Moreton <amoreton at solarflare.com>
    Sponsored by:   Solarflare Communications, Inc.
    MFC after:      1 week
    
    
    Former-commit-id: 8fe1f6c923d9fa7169ed8aa9a0af448b20f56d58

--- sys/dev/sfxge/common/efx_check.h
+++ sys/dev/sfxge/common/efx_check.h
@@ -49,13 +49,6 @@ end_comment
 begin_define
 define|#
 directive|define
-name|EFSYS_OPT_NVRAM_SFX7101
-value|(0)
-end_define
-
-begin_define
-define|#
-directive|define
 name|EFSYS_OPT_PHY_NULL
 value|(0)
 end_define
@@ -989,60 +982,22 @@ endif|#
 directive|endif
 end_endif
 
-begin_comment
-comment|/* Support NVRAM config for SFX7101 */
-end_comment
-
-begin_if
-if|#
-directive|if
+begin_ifdef
+ifdef|#
+directive|ifdef
 name|EFSYS_OPT_NVRAM_SFX7101
-end_if
-
-begin_if
-if|#
-directive|if
-operator|!
-name|EFSYS_OPT_NVRAM
-end_if
+end_ifdef
 
 begin_error
 error|#
 directive|error
-literal|"NVRAM_SFX7101 requires NVRAM"
+literal|"NVRAM_SFX7101 is obsolete and is not supported."
 end_error
 
 begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_if
-if|#
-directive|if
-operator|!
-name|EFSYS_OPT_FALCON
-end_if
-
-begin_error
-error|#
-directive|error
-literal|"NVRAM_SFX7101 requires FALCON"
-end_error
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_comment
-comment|/* EFSYS_OPT_NVRAM_SFX7101 */
-end_comment
 
 begin_ifdef
 ifdef|#
