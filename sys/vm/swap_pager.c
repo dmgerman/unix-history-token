6efd73cdd3290598fc9f98b5ee6a13720e694969
Konstantin Belousov <kib@FreeBSD.org>
2016-06-13 03:39:16 +0000

    Explicitely initialize sw_alloc_sx.  Currently it is not initialized but works due to zeroed out bss on startup.
    Reviewed by:	alc
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    Approved by:	re (hrs)
    
    
    Former-commit-id: f62b1f5d3fd8cd6d91b23c30d998e78295b93879

--- sys/vm/swap_pager.c
+++ sys/vm/swap_pager.c
@@ -2113,6 +2113,14 @@ expr_stmt|;
 name|sx_init
 argument_list|(
 operator|&
+name|sw_alloc_sx
+argument_list|,
+literal|"swspsx"
+argument_list|)
+expr_stmt|;
+name|sx_init
+argument_list|(
+operator|&
 name|swdev_syscall_lock
 argument_list|,
 literal|"swsysc"
