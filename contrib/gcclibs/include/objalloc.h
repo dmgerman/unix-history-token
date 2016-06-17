2feabca152ced46055a46f137a1182a0ffaf405d
Pedro Giffuni <pfg@FreeBSD.org>
2016-06-17 02:29:55 +0000

    MFC r301291: libiberty: prevent integer overflow.
    Take care of very old bug leading to heap-buffer overflow by
    processing certain file headers via bfd binary.
    
    PR:		200888
    Obtained from:	OpenBSD
    
    Former-commit-id: e4371973958f94c4e2b9bb178fac2807861e706d

--- contrib/gcclibs/include/objalloc.h
+++ contrib/gcclibs/include/objalloc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* objalloc.h -- routines to allocate memory for objects    Copyright 1997, 2001 Free Software Foundation, Inc.    Written by Ian Lance Taylor, Cygnus Solutions.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
+comment|/* objalloc.h -- routines to allocate memory for objects    Copyright 1997, 2001-2012 Free Software Foundation, Inc.    Written by Ian Lance Taylor, Cygnus Solutions.  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 end_comment
 
 begin_ifndef
@@ -226,7 +226,7 @@ parameter_list|,
 name|l
 parameter_list|)
 define|\
-value|__extension__								\   ({ struct objalloc *__o = (o);					\      unsigned long __len = (l);						\      if (__len == 0)							\        __len = 1;							\      __len = (__len + OBJALLOC_ALIGN - 1)&~ (OBJALLOC_ALIGN - 1);	\      (__len<= __o->current_space					\       ? (__o->current_ptr += __len,					\ 	 __o->current_space -= __len,					\ 	 (void *) (__o->current_ptr - __len))				\       : _objalloc_alloc (__o, __len)); })
+value|__extension__								\   ({ struct objalloc *__o = (o);					\      unsigned long __len = (l);						\      if (__len == 0)							\        __len = 1;							\      __len = (__len + OBJALLOC_ALIGN - 1)&~ (OBJALLOC_ALIGN - 1);	\      (__len != 0&& __len<= __o->current_space				\       ? (__o->current_ptr += __len,					\ 	 __o->current_space -= __len,					\ 	 (void *) (__o->current_ptr - __len))				\       : _objalloc_alloc (__o, __len)); })
 end_define
 
 begin_else
