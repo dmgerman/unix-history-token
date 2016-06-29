6619b3c4bb9466db34dddfcd63efbec8b7f82b60
Hans Petter Selasky <hselasky@FreeBSD.org>
2016-06-29 10:25:32 +0000

    MFC r302076: Update the definition for number of scratch pages to match the latest version of the XHCI specification. Make sure the code can handle the maximum number of allowed scratch pages.
    Submitted by:	Shichun_Ma@Dell.com
    
    
    Former-commit-id: ca8117f8c4978ff91e9b498cc0552e6a2c17d7e7

--- sys/dev/usb/controller/xhci.c
+++ sys/dev/usb/controller/xhci.c
@@ -1120,7 +1120,7 @@ argument_list|(
 name|bus
 argument_list|)
 decl_stmt|;
-name|uint8_t
+name|uint16_t
 name|i
 decl_stmt|;
 name|cb
@@ -1185,7 +1185,9 @@ literal|0
 init|;
 name|i
 operator|!=
-name|XHCI_MAX_SCRATCHPADS
+name|sc
+operator|->
+name|sc_noscratch
 condition|;
 name|i
 operator|++
