b55148b4c451097fbcc95b78cb8274a93b95752f
Eitan Adler <eadler@FreeBSD.org>
2016-07-09 04:56:28 +0000

    Add -P to pathchk usage
    Former-commit-id: abd80f7fe1dc74851a028c8a4253301fe94e31c6

--- usr.bin/pathchk/pathchk.c
+++ usr.bin/pathchk/pathchk.c
@@ -260,7 +260,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"usage: pathchk [-p] pathname ...\n"
+literal|"usage: pathchk [-Pp] pathname ...\n"
 argument_list|)
 expr_stmt|;
 name|exit
