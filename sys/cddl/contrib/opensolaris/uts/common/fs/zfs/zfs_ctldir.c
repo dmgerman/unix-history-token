095ae7ab116846b25cc7fd5fe37cbaac27d2b99e
Andriy Gapon <avg@FreeBSD.org>
2016-07-13 11:58:21 +0000

    MFC r286710: 6093 zfsctl_shares_lookup should only VN_RELE() on zfs_zget() success
    Former-commit-id: 1078001c82228de663264b78452b692bf1aa761c

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ctldir.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zfs_ctldir.c
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright 2015, OmniTI Computer Consulting, Inc. All rights reserved.  */
 end_comment
 
 begin_comment
@@ -5384,6 +5384,7 @@ operator|)
 operator|==
 literal|0
 condition|)
+block|{
 name|error
 operator|=
 name|VOP_LOOKUP
@@ -5406,6 +5407,7 @@ name|dzp
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
 name|ZFS_EXIT
 argument_list|(
 name|zfsvfs
