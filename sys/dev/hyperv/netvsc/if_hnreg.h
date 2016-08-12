925fc78d4b24415779c0f12e735d915b64986685
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-08-12 07:14:40 +0000

    hyperv/hn: Simplify NDIS configuration.
    MFC after:	1 week
    Sponsored by:	Microsoft
    Differential Revision:	https://reviews.freebsd.org/D7466
    
    
    Former-commit-id: 80ef15b102c578243b12574e35e81888f02b6e86

--- sys/dev/hyperv/netvsc/if_hnreg.h
+++ sys/dev/hyperv/netvsc/if_hnreg.h
@@ -48,6 +48,13 @@ name|HN_NVS_TYPE_INIT_RESP
 value|2
 end_define
 
+begin_define
+define|#
+directive|define
+name|HN_NVS_TYPE_NDIS_CONF
+value|125
+end_define
+
 begin_comment
 comment|/*  * Any size less than this one will _not_ work, e.g. hn_nvs_init  * only has 12B valid data, however, if only 12B data were sent,  * Hypervisor would never reply.  */
 end_comment
@@ -121,6 +128,67 @@ block|}
 name|__packed
 struct|;
 end_struct
+
+begin_comment
+comment|/* No reponse */
+end_comment
+
+begin_struct
+struct|struct
+name|hn_nvs_ndis_conf
+block|{
+name|uint32_t
+name|nvs_type
+decl_stmt|;
+comment|/* HN_NVS_TYPE_NDIS_CONF */
+name|uint32_t
+name|nvs_mtu
+decl_stmt|;
+name|uint32_t
+name|nvs_rsvd
+decl_stmt|;
+name|uint64_t
+name|nvs_caps
+decl_stmt|;
+comment|/* HN_NVS_NDIS_CONF_ */
+name|uint8_t
+name|nvs_rsvd1
+index|[
+literal|12
+index|]
+decl_stmt|;
+block|}
+name|__packed
+struct|;
+end_struct
+
+begin_expr_stmt
+name|CTASSERT
+argument_list|(
+sizeof|sizeof
+argument_list|(
+expr|struct
+name|hn_nvs_ndis_conf
+argument_list|)
+operator|>=
+name|HN_NVS_REQSIZE_MIN
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_define
+define|#
+directive|define
+name|HN_NVS_NDIS_CONF_SRIOV
+value|0x0004
+end_define
+
+begin_define
+define|#
+directive|define
+name|HN_NVS_NDIS_CONF_VLAN
+value|0x0008
+end_define
 
 begin_endif
 endif|#
