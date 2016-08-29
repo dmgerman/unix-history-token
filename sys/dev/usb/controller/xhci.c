bfa1648c6fba20d50f135b7ff638599f4fc58546
Hans Petter Selasky <hselasky@FreeBSD.org>
2016-08-29 08:55:59 +0000

    MFC r304629: Don't separate the status stage of the XHCI USB control transfers into its own job because this breaks the simplified QEMU XHCI TRB parser, which expects the complete USB control transfer as a series of back to back TRBs. The old behaviour is kept under #ifdef in case this change breaks enumeration of any USB devices.
    PR:		212021
    
    Former-commit-id: c14ad7f2005f0b3dcbaeaaa491b0e41f8d9a0137

--- sys/dev/usb/controller/xhci.c
+++ sys/dev/usb/controller/xhci.c
@@ -9933,6 +9933,9 @@ name|control_act
 condition|)
 block|{
 comment|/* 		 * Send a DATA1 message and invert the current 		 * endpoint direction. 		 */
+ifdef|#
+directive|ifdef
+name|XHCI_STEP_STATUS_STAGE
 name|temp
 operator|.
 name|step_td
@@ -9945,6 +9948,16 @@ operator|!=
 literal|0
 operator|)
 expr_stmt|;
+else|#
+directive|else
+name|temp
+operator|.
+name|step_td
+operator|=
+literal|0
+expr_stmt|;
+endif|#
+directive|endif
 name|temp
 operator|.
 name|direction
