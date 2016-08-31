fb2a2d9de83ff7ef9a66e466f9a6e5875ad20ada
Andrey Chernov <ache@FreeBSD.org>
2016-08-31 18:49:50 +0000

    'addrlen' does not matter when we need to find the first non-zero bit in the byte from the left and 'addrlen' already counted in 'lim'.
    PR:     212121
    Submitted by:   Herbie.Robinson@stratus.com
    MFC after:      7 days
    
    Former-commit-id: ef8851cf05eb0e26388037e9f37d165a17ed635c

--- lib/libc/net/getaddrinfo.c
+++ lib/libc/net/getaddrinfo.c
@@ -4873,11 +4873,13 @@ condition|)
 block|{
 while|while
 condition|(
+operator|(
 name|r
-operator|<
-name|addrlen
-operator|*
-literal|8
+operator|&
+literal|0x80
+operator|)
+operator|==
+literal|0
 condition|)
 block|{
 name|match
