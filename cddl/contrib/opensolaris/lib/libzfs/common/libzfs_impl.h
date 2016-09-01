5be566d4799005596c8692cfb169be9775373f82
Alexander Motin <mav@FreeBSD.org>
2016-09-01 13:29:53 +0000

    MFV r302643: 6902 speed up listing of snapshots if requesting name only and sorting by name
    This was our change from the beginning, so just reduce the upstream diff.
    
    
    Former-commit-id: 21b91852dac36ba697adf573f3f6e82a01ef8de4

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_impl.h
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_impl.h
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER SART  *  * The contents of this file are subject to th
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011 Pawel Jakub Dawidek<pawel@dawidek.net>.  * All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2013 Martin Matuska<mm@FreeBSD.org>. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright (c) 2011 Pawel Jakub Dawidek. All rights reserved.  * Copyright (c) 2013 by Delphix. All rights reserved.  * Copyright (c) 2013 Martin Matuska<mm@FreeBSD.org>. All rights reserved.  */
 end_comment
 
 begin_ifndef
