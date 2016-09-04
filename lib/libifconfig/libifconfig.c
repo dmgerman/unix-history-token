a1daab3a7248a0275292a9404b47cb1e2c0e11b8
Dimitry Andric <dim@FreeBSD.org>
2016-09-04 21:54:24 +0000

    Merge ^/head r305394 through r305396.
    Former-commit-id: 0a6832de682918317be44c79973e3df89d0a91f8

--- lib/libifconfig/libifconfig.c
+++ lib/libifconfig/libifconfig.c
@@ -4,7 +4,7 @@ comment|/*  * Copyright (c) 2016, Marie Helene Kvello-Aune  * All rights reserve
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 1983, 1993  *  The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*  * Copyright (c) 1983, 1993  *  The Regents of the University of California.  All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 4. Neither the name of the University nor the names of its contributors  *    may be used to endorse or promote products derived from this software  *    without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_include
@@ -94,8 +94,8 @@ literal|1
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifconfig_handle
+operator|*
+name|h
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -283,14 +283,18 @@ decl_stmt|;
 name|char
 modifier|*
 name|descr
-init|=
-name|NULL
 decl_stmt|;
 name|size_t
 name|descrlen
-init|=
-literal|64
 decl_stmt|;
+name|descr
+operator|=
+name|NULL
+expr_stmt|;
+name|descrlen
+operator|=
+literal|64
+expr_stmt|;
 name|memset
 argument_list|(
 operator|&
@@ -300,8 +304,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -539,8 +542,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -721,8 +723,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -829,8 +830,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -969,8 +969,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1062,8 +1061,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1157,8 +1155,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1250,8 +1247,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1342,8 +1338,7 @@ name|ifcap
 decl_stmt|;
 name|int
 name|flags
-decl_stmt|;
-name|int
+decl_stmt|,
 name|value
 decl_stmt|;
 name|memset
@@ -1355,8 +1350,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1513,8 +1507,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1612,8 +1605,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1700,8 +1692,7 @@ literal|0
 argument_list|,
 sizeof|sizeof
 argument_list|(
-expr|struct
-name|ifreq
+name|ifr
 argument_list|)
 argument_list|)
 expr_stmt|;
