66765f5d245790a9fd3d5e40aa4bd7a433b5277e
Bruce Evans <bde@FreeBSD.org>
2016-09-04 12:01:32 +0000

    Fix missing fmodl() on arches with 53-bit long doubles.
    PR:		199422, 211965
    MFC after:	1 week
    
    Former-commit-id: aaf89ea60c18340cad34b5919692b544d4d36566

--- lib/msun/src/e_fmod.c
+++ lib/msun/src/e_fmod.c
@@ -28,6 +28,12 @@ end_comment
 begin_include
 include|#
 directive|include
+file|<float.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"math.h"
 end_include
 
@@ -889,6 +895,31 @@ return|;
 comment|/* exact output */
 block|}
 end_function
+
+begin_if
+if|#
+directive|if
+operator|(
+name|LDBL_MANT_DIG
+operator|==
+literal|53
+operator|)
+end_if
+
+begin_expr_stmt
+name|__weak_reference
+argument_list|(
+name|fmod
+argument_list|,
+name|fmodl
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 end_unit
 
