9bf6b8f2457fff551766c9e36b4203b50179f60f
Pedro Giffuni <pfg@FreeBSD.org>
2016-09-08 15:25:41 +0000

    MFC r303893, r303894: misc replacements of comma with semicolon when pertinent.
    Uses of commas instead of a semicolons can easily go undetected. The comma
    can serve as a statement separator but this shouldn't be abused when
    statements are meant to be standalone.
    
    
    Former-commit-id: b16825429bd247f4155409b2c1647c8c9ce35c38

--- usr.sbin/ancontrol/ancontrol.c
+++ usr.sbin/ancontrol/ancontrol.c
@@ -3079,7 +3079,7 @@ name|stats
 operator|->
 name|an_rx_refresh_pkts
 argument_list|)
-operator|,
+expr_stmt|;
 name|printf
 argument_list|(
 literal|"Refresh frames transmitted:\t\t\t[ %u ]\n"
@@ -3088,7 +3088,7 @@ name|stats
 operator|->
 name|an_tx_refresh_pkts
 argument_list|)
-operator|,
+expr_stmt|;
 name|printf
 argument_list|(
 literal|"Poll frames received:\t\t\t\t[ %u ]\n"
