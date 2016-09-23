393ef6b79c40078df8fd2e74fa5c592c3806f4c9
Xin Li <delphij@FreeBSD.org>
2016-09-23 07:48:34 +0000

    Fix multiple OpenSSL vulnerabilitites.
    Approved by:	so
    Security:	FreeBSD-SA-16:26.openssl
    
    Former-commit-id: 0774b991f0f322526d3cc61a4c668d4f24558217

--- crypto/openssl/ssl/ssl_sess.c
+++ crypto/openssl/ssl/ssl_sess.c
@@ -2053,11 +2053,11 @@ name|err
 goto|;
 if|if
 condition|(
-name|session_id
-operator|+
-name|len
-operator|>
 name|limit
+operator|-
+name|session_id
+operator|<
+name|len
 condition|)
 block|{
 name|fatal
