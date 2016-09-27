3e91aef0c4db3aa8a4be1a62c7035368f3b07906
Luiz Otavio O Souza <loos@FreeBSD.org>
2016-09-27 17:25:06 +0000

    MFC r306205:
    Add the ID for the Huawei ME909S LTE modem.
    
    Submitted by:	svenauhagen at github
    Sponsored by:	Rubicon Communications, LLC (Netgate)
    
    
    Former-commit-id: 9b3846e693d82a43be5dda7490d8baeb719de7ad

--- sys/dev/usb/serial/u3g.c
+++ sys/dev/usb/serial/u3g.c
@@ -2042,6 +2042,15 @@ name|U3G_DEV
 argument_list|(
 name|HUAWEI
 argument_list|,
+name|ME909S
+argument_list|,
+name|U3GINIT_HUAWEISCSI2
+argument_list|)
+block|,
+name|U3G_DEV
+argument_list|(
+name|HUAWEI
+argument_list|,
 name|MOBILE
 argument_list|,
 name|U3GINIT_HUAWEI
