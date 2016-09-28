7e685499ef26dccd8c17814ccd58a8e15e02dccc
Alexander Motin <mav@FreeBSD.org>
2016-09-28 23:46:08 +0000

    7402 Create tunable to ignore hole_birth feature
    Until we can resolve the numerous hole_birth bugs that have cropped up
    recently, and come up with a way going forwards to protect users from
    corruption, we should disable the hole_birth feature.  Using a tunable
    allows those who are confident that their data is correct to continue to
    take advantage of the feature.
    
    Closes #188
    
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Author: Paul Dagnelie <pcd@delphix.com>
    
    Former-commit-id: 0cd86d10e97042c3c47e6d6756d51019959a3dd0

--- uts/common/fs/zfs/dmu_traverse.c
+++ uts/common/fs/zfs/dmu_traverse.c
@@ -107,6 +107,14 @@ begin_comment
 comment|/* 50MB */
 end_comment
 
+begin_decl_stmt
+name|boolean_t
+name|send_holes_without_birth_time
+init|=
+name|B_TRUE
+decl_stmt|;
+end_decl_stmt
+
 begin_typedef
 typedef|typedef
 struct|struct
@@ -994,6 +1002,9 @@ block|{
 comment|/* 		 * Since this block has a birth time of 0 it must be one of 		 * two things: a hole created before the 		 * SPA_FEATURE_HOLE_BIRTH feature was enabled, or a hole 		 * which has always been a hole in an object. 		 * 		 * If a file is written sparsely, then the unwritten parts of 		 * the file were "always holes" -- that is, they have been 		 * holes since this object was allocated.  However, we (and 		 * our callers) can not necessarily tell when an object was 		 * allocated.  Therefore, if it's possible that this object 		 * was freed and then its object number reused, we need to 		 * visit all the holes with birth==0. 		 * 		 * If it isn't possible that the object number was reused, 		 * then if SPA_FEATURE_HOLE_BIRTH was enabled before we wrote 		 * all the blocks we will visit as part of this traversal, 		 * then this hole must have always existed, so we can skip 		 * it.  We visit blocks born after (exclusive) td_min_txg. 		 * 		 * Note that the meta-dnode cannot be reallocated. 		 */
 if|if
 condition|(
+operator|!
+name|send_holes_without_birth_time
+operator|&&
 operator|(
 operator|!
 name|td
