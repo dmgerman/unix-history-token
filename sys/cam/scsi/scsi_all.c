b75adb89ba4d64e9066bdc1a82625f0dd865bdfe
Alexander Motin <mav@FreeBSD.org>
2016-10-05 00:45:58 +0000

    Do not retry on some security sense codes.
    MFC after:	1 week
    
    
    Former-commit-id: 453231236535f7c1a1ff54cab3c56306c00996de

--- sys/cam/scsi/scsi_all.c
+++ sys/cam/scsi/scsi_all.c
@@ -15667,9 +15667,8 @@ literal|0x74
 argument_list|,
 literal|0x71
 argument_list|,
-argument|SS_RDEF
+argument|SS_FATAL | EACCES
 argument_list|,
-comment|/* XXX TBD */
 literal|"Logical unit access not authorized"
 argument_list|)
 block|}
@@ -15682,9 +15681,8 @@ literal|0x74
 argument_list|,
 literal|0x79
 argument_list|,
-argument|SS_RDEF
+argument|SS_FATAL | EACCES
 argument_list|,
-comment|/* XXX TBD */
 literal|"Security conflict in translated device"
 argument_list|)
 block|}
