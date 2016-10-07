ad42b7532dd99486928d1448a8ae5c8c2f181587
Marcel Moolenaar <marcel@FreeBSD.org>
2016-10-07 15:42:20 +0000

    In order to allow mkimg(1) (and other tools) to become a build tool that can be compiled on various OSes (including on older versions of FreeBSD), make it possible to have it include the partitioning scheme definitions without pulling in FreeBSD specifics. In particular this means:  o  move the scheme definitions iand related defines to header files     under sys/disk,  o  make them (more) portable by using uint#_t (where applicable)     and renaming defines so that they at least have a good prefix,  o  make the new headers stand-alone so that they don't need FreeBSD     definitions, like struct uuid(*)  o  keep the original headers for compatibility, but rewrite them to     get the scheme definitions from <sys/disk/$scheme.h>.
    (*) since UUID/GUID type definitions are non-portable and the GPT
    scheme uses them, make it possible to have the scheme definitions
    use an external type by allowing consumers of the header to set
    GPT_UUID_TYPE. When GPT_UUID_TYPE has not been defined, the header
    will use it's own type definition, which is the same as struct uuid.
    The gpt_uuid_t typedef is created to abstract the details and allows
    consumers to refer to a single type.
    
    There is not conflict between the partitioning scheme headers and
    what is defined in them. All headers can be included in the same
    source files.
    
    Note: consumers of the old headers have not been changed yet. Such
    will be done if and when needed/beneficial.
    
    Reviewed by:	imp, jhb
    MFC after:	1 month
    Sponsored by:	Bracket Computing
    
    
    Former-commit-id: 8c542eea003c3cd964fb859b5f2e03b36bb4206b

--- sys/sys/disklabel.h
+++ sys/sys/disklabel.h
@@ -38,8 +38,14 @@ directive|include
 file|<sys/ioccom.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<sys/disk/bsd.h>
+end_include
+
 begin_comment
-comment|/*  * Disk description table, see disktab(5)  */
+comment|/* Disk description table, see disktab(5) */
 end_comment
 
 begin_define
@@ -50,42 +56,8 @@ value|"/etc/disktab"
 end_define
 
 begin_comment
-comment|/*  * Each disk has a label which includes information about the hardware  * disk geometry, filesystem partitions, and drive specific information.  * The label is in block 0 or 1, possibly offset from the beginning  * to leave room for a bootstrap, etc.  */
+comment|/*  * The label is in block 0 or 1, possibly offset from the beginning  * to leave room for a bootstrap, etc.  * XXX these should be defined per controller (or drive) elsewhere, not here!  * XXX in actuality it can't even be per controller or drive. It should be  * constant/fixed across storage hardware and CPU architectures. Disks can  * travel from one machine to another and a label created on one machine  * should be detectable and understood by the other.  */
 end_comment
-
-begin_comment
-comment|/* XXX these should be defined per controller (or drive) elsewhere, not here! */
-end_comment
-
-begin_if
-if|#
-directive|if
-name|defined
-argument_list|(
-name|__i386__
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|__amd64__
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|__arm__
-argument_list|)
-operator|||
-expr|\
-name|defined
-argument_list|(
-name|__powerpc__
-argument_list|)
-operator|||
-name|defined
-argument_list|(
-name|__mips__
-argument_list|)
-end_if
 
 begin_define
 define|#
@@ -109,16 +81,11 @@ begin_comment
 comment|/* offset of label in sector */
 end_comment
 
-begin_endif
-endif|#
-directive|endif
-end_endif
-
 begin_define
 define|#
 directive|define
 name|DISKMAGIC
-value|((u_int32_t)0x82564557)
+value|BSD_MAGIC
 end_define
 
 begin_comment
@@ -135,7 +102,7 @@ begin_define
 define|#
 directive|define
 name|MAXPARTITIONS
-value|8
+value|BSD_NPARTS_MIN
 end_define
 
 begin_endif
@@ -151,251 +118,43 @@ begin_define
 define|#
 directive|define
 name|BBSIZE
-value|8192
+value|BSD_BOOTBLOCK_SIZE
 end_define
 
 begin_define
 define|#
 directive|define
 name|LABEL_PART
-value|2
+value|BSD_PART_RAW
 end_define
-
-begin_comment
-comment|/* partition containing label */
-end_comment
 
 begin_define
 define|#
 directive|define
 name|RAW_PART
-value|2
+value|BSD_PART_RAW
 end_define
-
-begin_comment
-comment|/* partition containing whole disk */
-end_comment
 
 begin_define
 define|#
 directive|define
 name|SWAP_PART
-value|1
+value|BSD_PART_SWAP
 end_define
 
-begin_comment
-comment|/* partition normally containing swap */
-end_comment
-
-begin_struct
-struct|struct
-name|disklabel
-block|{
-name|u_int32_t
-name|d_magic
-decl_stmt|;
-comment|/* the magic number */
-name|u_int16_t
-name|d_type
-decl_stmt|;
-comment|/* drive type */
-name|u_int16_t
-name|d_subtype
-decl_stmt|;
-comment|/* controller/d_type specific */
-name|char
-name|d_typename
-index|[
-literal|16
-index|]
-decl_stmt|;
-comment|/* type name, e.g. "eagle" */
-name|char
-name|d_packname
-index|[
-literal|16
-index|]
-decl_stmt|;
-comment|/* pack identifier */
-comment|/* disk geometry: */
-name|u_int32_t
-name|d_secsize
-decl_stmt|;
-comment|/* # of bytes per sector */
-name|u_int32_t
-name|d_nsectors
-decl_stmt|;
-comment|/* # of data sectors per track */
-name|u_int32_t
-name|d_ntracks
-decl_stmt|;
-comment|/* # of tracks per cylinder */
-name|u_int32_t
-name|d_ncylinders
-decl_stmt|;
-comment|/* # of data cylinders per unit */
-name|u_int32_t
-name|d_secpercyl
-decl_stmt|;
-comment|/* # of data sectors per cylinder */
-name|u_int32_t
-name|d_secperunit
-decl_stmt|;
-comment|/* # of data sectors per unit */
-comment|/* 	 * Spares (bad sector replacements) below are not counted in 	 * d_nsectors or d_secpercyl.  Spare sectors are assumed to 	 * be physical sectors which occupy space at the end of each 	 * track and/or cylinder. 	 */
-name|u_int16_t
-name|d_sparespertrack
-decl_stmt|;
-comment|/* # of spare sectors per track */
-name|u_int16_t
-name|d_sparespercyl
-decl_stmt|;
-comment|/* # of spare sectors per cylinder */
-comment|/* 	 * Alternate cylinders include maintenance, replacement, configuration 	 * description areas, etc. 	 */
-name|u_int32_t
-name|d_acylinders
-decl_stmt|;
-comment|/* # of alt. cylinders per unit */
-comment|/* hardware characteristics: */
-comment|/* 	 * d_interleave, d_trackskew and d_cylskew describe perturbations 	 * in the media format used to compensate for a slow controller. 	 * Interleave is physical sector interleave, set up by the 	 * formatter or controller when formatting.  When interleaving is 	 * in use, logically adjacent sectors are not physically 	 * contiguous, but instead are separated by some number of 	 * sectors.  It is specified as the ratio of physical sectors 	 * traversed per logical sector.  Thus an interleave of 1:1 	 * implies contiguous layout, while 2:1 implies that logical 	 * sector 0 is separated by one sector from logical sector 1. 	 * d_trackskew is the offset of sector 0 on track N relative to 	 * sector 0 on track N-1 on the same cylinder.  Finally, d_cylskew 	 * is the offset of sector 0 on cylinder N relative to sector 0 	 * on cylinder N-1. 	 */
-name|u_int16_t
-name|d_rpm
-decl_stmt|;
-comment|/* rotational speed */
-name|u_int16_t
-name|d_interleave
-decl_stmt|;
-comment|/* hardware sector interleave */
-name|u_int16_t
-name|d_trackskew
-decl_stmt|;
-comment|/* sector 0 skew, per track */
-name|u_int16_t
-name|d_cylskew
-decl_stmt|;
-comment|/* sector 0 skew, per cylinder */
-name|u_int32_t
-name|d_headswitch
-decl_stmt|;
-comment|/* head switch time, usec */
-name|u_int32_t
-name|d_trkseek
-decl_stmt|;
-comment|/* track-to-track seek, usec */
-name|u_int32_t
-name|d_flags
-decl_stmt|;
-comment|/* generic flags */
+begin_define
 define|#
 directive|define
 name|NDDATA
-value|5
-name|u_int32_t
-name|d_drivedata
-index|[
-name|NDDATA
-index|]
-decl_stmt|;
-comment|/* drive-type specific information */
+value|BSD_NDRIVEDATA
+end_define
+
+begin_define
 define|#
 directive|define
 name|NSPARE
-value|5
-name|u_int32_t
-name|d_spare
-index|[
-name|NSPARE
-index|]
-decl_stmt|;
-comment|/* reserved for future use */
-name|u_int32_t
-name|d_magic2
-decl_stmt|;
-comment|/* the magic number (again) */
-name|u_int16_t
-name|d_checksum
-decl_stmt|;
-comment|/* xor of data incl. partitions */
-comment|/* filesystem and partition information: */
-name|u_int16_t
-name|d_npartitions
-decl_stmt|;
-comment|/* number of partitions in following */
-name|u_int32_t
-name|d_bbsize
-decl_stmt|;
-comment|/* size of boot area at sn0, bytes */
-name|u_int32_t
-name|d_sbsize
-decl_stmt|;
-comment|/* max size of fs superblock, bytes */
-struct|struct
-name|partition
-block|{
-comment|/* the partition table */
-name|u_int32_t
-name|p_size
-decl_stmt|;
-comment|/* number of sectors in partition */
-name|u_int32_t
-name|p_offset
-decl_stmt|;
-comment|/* starting sector */
-name|u_int32_t
-name|p_fsize
-decl_stmt|;
-comment|/* filesystem basic fragment size */
-name|u_int8_t
-name|p_fstype
-decl_stmt|;
-comment|/* filesystem type, see below */
-name|u_int8_t
-name|p_frag
-decl_stmt|;
-comment|/* filesystem fragments per block */
-name|u_int16_t
-name|p_cpg
-decl_stmt|;
-comment|/* filesystem cylinders per group */
-block|}
-name|d_partitions
-index|[
-name|MAXPARTITIONS
-index|]
-struct|;
-comment|/* actually may be more */
-block|}
-struct|;
-end_struct
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|CTASSERT
-end_ifdef
-
-begin_expr_stmt
-name|CTASSERT
-argument_list|(
-sizeof|sizeof
-argument_list|(
-expr|struct
-name|disklabel
-argument_list|)
-operator|==
-literal|148
-operator|+
-name|MAXPARTITIONS
-operator|*
-literal|16
-argument_list|)
-expr_stmt|;
-end_expr_stmt
-
-begin_endif
-endif|#
-directive|endif
-end_endif
+value|BSD_NSPARE
+end_define
 
 begin_function_decl
 specifier|static
@@ -479,164 +238,6 @@ return|;
 block|}
 end_function
 
-begin_comment
-comment|/* d_type values: */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_SMD
-value|1
-end_define
-
-begin_comment
-comment|/* SMD, XSMD; VAX hp/up */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_MSCP
-value|2
-end_define
-
-begin_comment
-comment|/* MSCP */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_DEC
-value|3
-end_define
-
-begin_comment
-comment|/* other DEC (rk, rl) */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_SCSI
-value|4
-end_define
-
-begin_comment
-comment|/* SCSI */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_ESDI
-value|5
-end_define
-
-begin_comment
-comment|/* ESDI interface */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_ST506
-value|6
-end_define
-
-begin_comment
-comment|/* ST506 etc. */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_HPIB
-value|7
-end_define
-
-begin_comment
-comment|/* CS/80 on HP-IB */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_HPFL
-value|8
-end_define
-
-begin_comment
-comment|/* HP Fiber-link */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_FLOPPY
-value|10
-end_define
-
-begin_comment
-comment|/* floppy */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_CCD
-value|11
-end_define
-
-begin_comment
-comment|/* concatenated disk */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_VINUM
-value|12
-end_define
-
-begin_comment
-comment|/* vinum volume */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_DOC2K
-value|13
-end_define
-
-begin_comment
-comment|/* Msys DiskOnChip */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_RAID
-value|14
-end_define
-
-begin_comment
-comment|/* CMU RAIDFrame */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|DTYPE_JFS2
-value|16
-end_define
-
-begin_comment
-comment|/* IBM JFS 2 */
-end_comment
-
 begin_ifdef
 ifdef|#
 directive|ifdef
@@ -702,307 +303,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/*  * Filesystem type and version.  * Used to interpret other filesystem-specific  * per-partition information.  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_UNUSED
-value|0
-end_define
-
-begin_comment
-comment|/* unused */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_SWAP
-value|1
-end_define
-
-begin_comment
-comment|/* swap */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_V6
-value|2
-end_define
-
-begin_comment
-comment|/* Sixth Edition */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_V7
-value|3
-end_define
-
-begin_comment
-comment|/* Seventh Edition */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_SYSV
-value|4
-end_define
-
-begin_comment
-comment|/* System V */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_V71K
-value|5
-end_define
-
-begin_comment
-comment|/* V7 with 1K blocks (4.1, 2.9) */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_V8
-value|6
-end_define
-
-begin_comment
-comment|/* Eighth Edition, 4K blocks */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_BSDFFS
-value|7
-end_define
-
-begin_comment
-comment|/* 4.2BSD fast filesystem */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_MSDOS
-value|8
-end_define
-
-begin_comment
-comment|/* MSDOS filesystem */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_BSDLFS
-value|9
-end_define
-
-begin_comment
-comment|/* 4.4BSD log-structured filesystem */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_OTHER
-value|10
-end_define
-
-begin_comment
-comment|/* in use, but unknown/unsupported */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_HPFS
-value|11
-end_define
-
-begin_comment
-comment|/* OS/2 high-performance filesystem */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_ISO9660
-value|12
-end_define
-
-begin_comment
-comment|/* ISO 9660, normally CD-ROM */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_BOOT
-value|13
-end_define
-
-begin_comment
-comment|/* partition contains bootstrap */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_VINUM
-value|14
-end_define
-
-begin_comment
-comment|/* Vinum drive */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_RAID
-value|15
-end_define
-
-begin_comment
-comment|/* RAIDFrame drive */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_FILECORE
-value|16
-end_define
-
-begin_comment
-comment|/* Acorn Filecore Filing System */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_EXT2FS
-value|17
-end_define
-
-begin_comment
-comment|/* ext2fs */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_NTFS
-value|18
-end_define
-
-begin_comment
-comment|/* Windows/NT file system */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_CCD
-value|20
-end_define
-
-begin_comment
-comment|/* concatenated disk component */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_JFS2
-value|21
-end_define
-
-begin_comment
-comment|/* IBM JFS2 */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_HAMMER
-value|22
-end_define
-
-begin_comment
-comment|/* DragonFlyBSD Hammer FS */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_HAMMER2
-value|23
-end_define
-
-begin_comment
-comment|/* DragonFlyBSD Hammer2 FS */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_UDF
-value|24
-end_define
-
-begin_comment
-comment|/* UDF */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_EFS
-value|26
-end_define
-
-begin_comment
-comment|/* SGI's Extent File system */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_ZFS
-value|27
-end_define
-
-begin_comment
-comment|/* Sun's ZFS */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|FS_NANDFS
-value|30
-end_define
-
-begin_comment
-comment|/* FreeBSD nandfs (NiLFS derived) */
-end_comment
 
 begin_ifdef
 ifdef|#
@@ -1097,65 +397,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/*  * flags shared by various drives:  */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|D_REMOVABLE
-value|0x01
-end_define
-
-begin_comment
-comment|/* removable media */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|D_ECC
-value|0x02
-end_define
-
-begin_comment
-comment|/* supports ECC */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|D_BADSECT
-value|0x04
-end_define
-
-begin_comment
-comment|/* supports bad sector forw. */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|D_RAMDISK
-value|0x08
-end_define
-
-begin_comment
-comment|/* disk emulator */
-end_comment
-
-begin_define
-define|#
-directive|define
-name|D_CHAIN
-value|0x10
-end_define
-
-begin_comment
-comment|/* can do back-back transfers */
-end_comment
 
 begin_comment
 comment|/*  * NB:<sys/disk.h> defines ioctls from 'd'/128 and up.  */
