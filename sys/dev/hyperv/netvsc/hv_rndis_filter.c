03319d3b694c207f7a9f840c5f5fbfb13661baaf
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-10-17 07:02:17 +0000

    MFC 304724,304728
    304724
        hyperv/hn: Remove the redundant rid setting for RNDIS HALT.
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D7595
    
    304728
        hyperv/hn: Log a warning for RESET_CMPLT.
    
        RESET is not used by the hn(4) at all, and RESET_CMPLT does not even
        have a rid to match with the pending requests.  So, let's put it
        onto an independent switch branch and log a warning about it.
    
        Sponsored by:   Microsoft
        Differential Revision:  https://reviews.freebsd.org/D7602
    
    
    Former-commit-id: e7741f8ac9acbcd5d7fe8ece7e625bf925e5c178

--- sys/dev/hyperv/netvsc/hv_rndis_filter.c
+++ sys/dev/hyperv/netvsc/hv_rndis_filter.c
@@ -1470,31 +1470,6 @@ expr_stmt|;
 block|}
 else|else
 block|{
-if|if
-condition|(
-name|response
-operator|->
-name|ndis_msg_type
-operator|==
-name|REMOTE_NDIS_RESET_CMPLT
-condition|)
-block|{
-comment|/* Does not have a request id field */
-name|request
-operator|->
-name|response_msg
-operator|.
-name|msg
-operator|.
-name|reset_complete
-operator|.
-name|status
-operator|=
-name|STATUS_BUFFER_OVERFLOW
-expr_stmt|;
-block|}
-else|else
-block|{
 name|request
 operator|->
 name|response_msg
@@ -1507,7 +1482,6 @@ name|status
 operator|=
 name|STATUS_BUFFER_OVERFLOW
 expr_stmt|;
-block|}
 block|}
 name|sema_post
 argument_list|(
@@ -2517,9 +2491,6 @@ case|case
 name|REMOTE_NDIS_SET_CMPLT
 case|:
 case|case
-name|REMOTE_NDIS_RESET_CMPLT
-case|:
-case|case
 name|REMOTE_NDIS_KEEPALIVE_CMPLT
 case|:
 name|hv_rf_receive_response
@@ -2542,10 +2513,28 @@ name|rndis_hdr
 argument_list|)
 expr_stmt|;
 break|break;
-default|default:
-name|printf
+case|case
+name|REMOTE_NDIS_RESET_CMPLT
+case|:
+comment|/* 		 * Reset completed, no rid. 		 * 		 * NOTE: 		 * RESET is not issued by hn(4), so this message should 		 * _not_ be observed. 		 */
+name|if_printf
 argument_list|(
-literal|"hv_rf_on_receive():  Unknown msg_type 0x%x\n"
+name|sc
+operator|->
+name|hn_ifp
+argument_list|,
+literal|"RESET CMPLT received\n"
+argument_list|)
+expr_stmt|;
+break|break;
+default|default:
+name|if_printf
+argument_list|(
+name|sc
+operator|->
+name|hn_ifp
+argument_list|,
+literal|"unknown RNDIS message 0x%x\n"
 argument_list|,
 name|rndis_hdr
 operator|->
@@ -4004,10 +3993,6 @@ name|rndis_request
 modifier|*
 name|request
 decl_stmt|;
-name|rndis_halt_request
-modifier|*
-name|halt
-decl_stmt|;
 name|int
 name|i
 decl_stmt|,
@@ -4048,39 +4033,6 @@ operator|->
 name|halt_complete_flag
 operator|=
 literal|0
-expr_stmt|;
-comment|/* Set up the rndis set */
-name|halt
-operator|=
-operator|&
-name|request
-operator|->
-name|request_msg
-operator|.
-name|msg
-operator|.
-name|halt_request
-expr_stmt|;
-name|halt
-operator|->
-name|request_id
-operator|=
-name|atomic_fetchadd_int
-argument_list|(
-operator|&
-name|device
-operator|->
-name|new_request_id
-argument_list|,
-literal|1
-argument_list|)
-expr_stmt|;
-comment|/* Increment to get the new value (call above returns old value) */
-name|halt
-operator|->
-name|request_id
-operator|+=
-literal|1
 expr_stmt|;
 name|ret
 operator|=
