512a9eaebe95ea7f4f14cb474be0f77ebb2fed3f
Konstantin Belousov <kib@FreeBSD.org>
2016-10-19 18:15:44 +0000

    Partial workaround for Intel PCI adapters reading past the end of the host-programmed DMA regions.  This change seemingly fixes the descriptor fetches, but the packet memory accesses are left problematic.
    Reviewed by:	emaste, erj, sbruno
    Sponsored by:	The FreeBSD Foundation
    MFC after:	2 weeks
    Differential revision:	https://reviews.freebsd.org/D8282
    
    
    Former-commit-id: c016b6250b4325150e08cd8cd686c3ab2de8749a

--- sys/dev/e1000/if_lem.c
+++ sys/dev/e1000/if_lem.c
@@ -3237,13 +3237,18 @@ name|report_tx_early
 operator|=
 literal|1
 expr_stmt|;
+comment|/* 	 * It seems that the descriptor DMA engine on some PCI cards 	 * fetches memory past the end of the last descriptor in the 	 * ring.  These reads are problematic when VT-d (DMAR) busdma 	 * is used.  Allocate the scratch space to avoid getting 	 * faults from DMAR, by requesting scratch memory for one more 	 * descriptor. 	 */
 name|tsize
 operator|=
 name|roundup2
 argument_list|(
+operator|(
 name|adapter
 operator|->
 name|num_tx_desc
+operator|+
+literal|1
+operator|)
 operator|*
 sizeof|sizeof
 argument_list|(
@@ -3302,13 +3307,18 @@ name|txdma
 operator|.
 name|dma_vaddr
 expr_stmt|;
+comment|/* 	 * See comment above txdma allocation for rationale behind +1. 	 */
 name|rsize
 operator|=
 name|roundup2
 argument_list|(
+operator|(
 name|adapter
 operator|->
 name|num_rx_desc
+operator|+
+literal|1
+operator|)
 operator|*
 sizeof|sizeof
 argument_list|(
