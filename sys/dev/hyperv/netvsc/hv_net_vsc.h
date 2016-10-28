c509e146440ec683895ffc7a4b02c372db3d9ee9
Sepherosa Ziehau <sephe@FreeBSD.org>
2016-10-28 05:31:34 +0000

    hyperv/hn: Change header guardian; in preparation for the upcoming rename.
    MFC after:	1 week
    Sponsored by:	Microsoft
    Differential Revision:	https://reviews.freebsd.org/D8352
    
    
    Former-commit-id: 5eafa75846f24611cf9fb870d513eb9aa169422e

--- sys/dev/hyperv/netvsc/hv_net_vsc.h
+++ sys/dev/hyperv/netvsc/hv_net_vsc.h
@@ -6,13 +6,13 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|__HV_NET_VSC_H__
+name|_HN_NVS_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|__HV_NET_VSC_H__
+name|_HN_NVS_H_
 end_define
 
 begin_struct_decl
@@ -430,7 +430,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* __HV_NET_VSC_H__ */
+comment|/* !_HN_NVS_H_ */
 end_comment
 
 end_unit
