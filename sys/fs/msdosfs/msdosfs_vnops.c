3044c0a7b269f4edf43fff90df18e3b2bd6b60d4
Konstantin Belousov <kib@FreeBSD.org>
2016-11-11 20:08:45 +0000

    MFC r308025: Enable vn_io_fault() deadlock avoidance for msdosfs.
    Former-commit-id: 695d0a172632ebeed340876b2949e0d2404bbded

--- sys/fs/msdosfs/msdosfs_vnops.c
+++ sys/fs/msdosfs/msdosfs_vnops.c
@@ -2820,7 +2820,7 @@ name|diff
 expr_stmt|;
 name|error
 operator|=
-name|uiomove
+name|vn_io_fault_uiomove
 argument_list|(
 name|bp
 operator|->
@@ -3453,6 +3453,7 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
+comment|/* 			 * This call to vfs_bio_clrbuf() ensures that 			 * even if vn_io_fault_uiomove() below faults, 			 * garbage from the newly instantiated buffer 			 * is not exposed to the userspace via mmap(). 			 */
 name|vfs_bio_clrbuf
 argument_list|(
 name|bp
@@ -3573,7 +3574,7 @@ comment|/* 		 * Should these vnode_pager_* functions be done on dir 		 * files? 
 comment|/* 		 * Copy the data from user space into the buf header. 		 */
 name|error
 operator|=
-name|uiomove
+name|vn_io_fault_uiomove
 argument_list|(
 name|bp
 operator|->
