b92bb4e631e8f08ffe05b87092d47c3cda25bd8d
Adrian Chadd <adrian@FreeBSD.org>
2016-11-15 01:34:38 +0000

    [mips] make UMTX_CHAINS configurable at compile time.
    The default (512) wastes quite a bit of space which doesn't really buy
    us much on highly embedded systems which don't take a lot of locks in
    parallel.
    
    This makes it at least build time configurable so people can experiment.
    
    
    Former-commit-id: db9d0a46428d80decfd34c283d50b97766b9af17

--- sys/kern/kern_umtx.c
+++ sys/kern/kern_umtx.c
@@ -557,12 +557,23 @@ name|GOLDEN_RATIO_PRIME
 value|2654404609U
 end_define
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|UMTX_CHAINS
+end_ifndef
+
 begin_define
 define|#
 directive|define
 name|UMTX_CHAINS
 value|512
 end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_define
 define|#
