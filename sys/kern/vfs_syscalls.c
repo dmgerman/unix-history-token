1e61ff5b9f376565f78084c2ff0c81d5f734243b
Konstantin Belousov <kib@FreeBSD.org>
2016-11-16 16:14:01 +0000

    MFC r308212: Allow some dotdot lookups in capability mode.
    Former-commit-id: 325f0ec660ff1ac10a9913dc3d565a6135bc025f

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -4832,9 +4832,13 @@ goto|;
 comment|/* 		 * Handle special fdopen() case. bleh. 		 * 		 * Don't do this for relative (capability) lookups; we don't 		 * understand exactly what would happen, and we don't think 		 * that it ever should. 		 */
 if|if
 condition|(
+operator|(
 name|nd
 operator|.
-name|ni_strictrelative
+name|ni_lcf
+operator|&
+name|NI_LCF_STRICTRELATIVE
+operator|)
 operator|==
 literal|0
 operator|&&
@@ -5033,11 +5037,15 @@ directive|ifdef
 name|CAPABILITIES
 if|if
 condition|(
+operator|(
 name|nd
 operator|.
-name|ni_strictrelative
-operator|==
-literal|1
+name|ni_lcf
+operator|&
+name|NI_LCF_STRICTRELATIVE
+operator|)
+operator|!=
+literal|0
 condition|)
 name|fcaps
 operator|=
