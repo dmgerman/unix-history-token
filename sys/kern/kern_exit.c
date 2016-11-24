e2763890739741c42c55e4588dcafec82c7ad6bd
Mateusz Guzik <mjg@FreeBSD.org>
2016-11-24 18:21:48 +0000

    wait: avoid relocking the child if proc_to_reap returns 1
    proc_to_reap would always unlock. However, if it returned 1, kern_wait6
    would immediately lock it again. Save the dance.
    
    Reviewed by:	kib
    
    
    Former-commit-id: 407b971726cbfa1f8a267f5e04303e0ed2b57d74

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -4201,11 +4201,6 @@ literal|1
 operator|)
 return|;
 block|}
-name|PROC_UNLOCK
-argument_list|(
-name|p
-argument_list|)
-expr_stmt|;
 return|return
 operator|(
 literal|1
@@ -4661,9 +4656,11 @@ literal|0
 operator|)
 return|;
 block|}
-name|PROC_LOCK
+name|PROC_LOCK_ASSERT
 argument_list|(
 name|p
+argument_list|,
+name|MA_OWNED
 argument_list|)
 expr_stmt|;
 name|PROC_SLOCK
@@ -5228,6 +5225,11 @@ index|[
 literal|0
 index|]
 operator|)
+argument_list|)
+expr_stmt|;
+name|PROC_UNLOCK
+argument_list|(
+name|p
 argument_list|)
 expr_stmt|;
 name|nfound
