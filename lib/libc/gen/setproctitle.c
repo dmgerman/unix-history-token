031fa2bdb6a5e2c6d2800492af1e617463b38085
Ed Maste <emaste@FreeBSD.org>
2016-11-29 01:08:09 +0000

    MFC r309006: remove unnecessary vm includes from setproctitle
    vm headers were needed only for the PS_STRINGS fallback, which was
    removed in r297888.
    
    
    Former-commit-id: 9c1f5b870168bb861b50b26882867ae1b2b7e0f4

--- lib/libc/gen/setproctitle.c
+++ lib/libc/gen/setproctitle.c
@@ -44,24 +44,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<vm/vm.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<vm/vm_param.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<vm/pmap.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<stdio.h>
 end_include
 
