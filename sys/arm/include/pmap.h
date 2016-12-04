64703b2008776aaf0cb33b43828b8c8e8ccfd8b5
mmel <mmel@FreeBSD.org>
2016-12-04 15:27:39 +0000

    Implement fake pmap_mapdev_attr() for ARMv6. This function is referenced, but never called from DRM2 code. Also, real behavior of pmap_mapdev_attr() in ARM world is unclear as we don't have any additional attribute for a device memory type.
    MFC after: 2 weeks
    
    
    Former-commit-id: 7614ab1998c538a0a840693affd27d2c1b707cce

--- sys/arm/include/pmap.h
+++ sys/arm/include/pmap.h
@@ -149,6 +149,33 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function
+specifier|static
+specifier|inline
+name|void
+modifier|*
+name|pmap_mapdev_attr
+parameter_list|(
+name|vm_paddr_t
+name|addr
+parameter_list|,
+name|vm_size_t
+name|size
+parameter_list|,
+name|int
+name|attr
+parameter_list|)
+block|{
+name|panic
+argument_list|(
+literal|"%s is not implemented yet!\n"
+argument_list|,
+name|__func__
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
 begin_struct_decl
 struct_decl|struct
 name|pcb
