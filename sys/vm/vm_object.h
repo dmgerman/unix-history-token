9e8efb79d4d855006c0d43b272d64b422ecaed10
Alan Cox <alc@FreeBSD.org>
2016-12-12 17:47:09 +0000

    Eliminate every mention of PG_CACHED pages from the comments in the machine- independent layer of the virtual memory system.  Update some of the nearby comments to eliminate redundancy and improve clarity.
    In vm/vm_reserv.c, do not use hyphens after adverbs ending in -ly per
    The Chicago Manual of Style.
    
    Update the comment in vm/vm_page.h defining the four types of page queues to
    reflect the elimination of PG_CACHED pages and the introduction of the
    laundry queue.
    
    Reviewed by:	kib, markj
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D8752
    
    
    Former-commit-id: 58dc1cecb90f5979473cde2bbc2662539a9eb4fa

--- sys/vm/vm_object.h
+++ sys/vm/vm_object.h
@@ -50,7 +50,7 @@ file|<vm/_vm_radix.h>
 end_include
 
 begin_comment
-comment|/*  *	Types defined:  *  *	vm_object_t		Virtual memory object.  *  *	The root of cached pages pool is protected by both the per-object lock  *	and the free pages queue mutex.  *	On insert in the cache radix trie, the per-object lock is expected  *	to be already held and the free pages queue mutex will be  *	acquired during the operation too.  *	On remove and lookup from the cache radix trie, only the free  *	pages queue mutex is expected to be locked.  *	These rules allow for reliably checking for the presence of cached  *	pages with only the per-object lock held, thereby reducing contention  *	for the free pages queue mutex.  *  * List of locks  *	(c)	const until freed  *	(o)	per-object lock   *	(f)	free pages queue mutex  *  */
+comment|/*  *	Types defined:  *  *	vm_object_t		Virtual memory object.  *  * List of locks  *	(c)	const until freed  *	(o)	per-object lock   *	(f)	free pages queue mutex  *  */
 end_comment
 
 begin_struct
