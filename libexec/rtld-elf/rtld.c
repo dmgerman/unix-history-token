1371efefca21ffaa40055563257b985c1bba2755
Bryan Drewery <bdrewery@FreeBSD.org>
2016-12-13 18:05:14 +0000

    Take write lock for rtld_bind before modifying obj_list in dl_iterate_phdr().
    This avoids a race with readers such as dladdr(3)/dlinfo(3)/dlsym(3) and
    the atexit(3) handler.  This race was introduced in r294373.
    
    Reviewed by:	markj, kib, kan
    MFC after:	2 weeks
    Sponsored by:	Dell EMC Isilon
    
    
    Former-commit-id: e7c4b8bd0134a6c9bef4a66c206ac9a4ad24c88e

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -18047,7 +18047,7 @@ operator|&
 name|phdr_lockstate
 argument_list|)
 expr_stmt|;
-name|rlock_acquire
+name|wlock_acquire
 argument_list|(
 name|rtld_bind_lock
 argument_list|,
@@ -18116,7 +18116,7 @@ argument_list|,
 name|param
 argument_list|)
 expr_stmt|;
-name|rlock_acquire
+name|wlock_acquire
 argument_list|(
 name|rtld_bind_lock
 argument_list|,
