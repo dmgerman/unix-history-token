a591bbfb2fd36f354bf82a645e507ce45f5bc390
Julian Elischer <julian@FreeBSD.org>
2016-12-27 08:31:40 +0000

    MFH: r309408
    Changes to allow the patching of packets with an offset (and other changes.. see man page)
    
    PR:	206185
    Submitted by:	Dmitry Vagin <daemon-hammer@ya.ru>
    MFC after:	 1 week
    Relnotes:	yes (also ng_checksum node)
    
    Former-commit-id: d38ce6af16fc5287bb1d42c0f92bf260eece56b8

--- sys/netgraph/ng_patch.h
+++ sys/netgraph/ng_patch.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (C) 2010 by Maxim Ignatenko<gelraen.ua@gmail.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2010 Maxim Ignatenko<gelraen.ua@gmail.com>  * Copyright (c) 2015 Dmitry Vagin<daemon.hammer@ya.ru>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -56,6 +56,24 @@ value|"out"
 end_define
 
 begin_comment
+comment|/* Checksum flags */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NG_PATCH_CSUM_IPV4
+value|(CSUM_IP|CSUM_TCP|CSUM_UDP|CSUM_SCTP)
+end_define
+
+begin_define
+define|#
+directive|define
+name|NG_PATCH_CSUM_IPV6
+value|(CSUM_TCP_IPV6|CSUM_UDP_IPV6|CSUM_SCTP_IPV6)
+end_define
+
+begin_comment
 comment|/* Netgraph commands understood by this node type */
 end_comment
 
@@ -73,6 +91,10 @@ block|,
 name|NGM_PATCH_CLR_STATS
 block|,
 name|NGM_PATCH_GETCLR_STATS
+block|,
+name|NGM_PATCH_GETDLT
+block|,
+name|NGM_PATCH_SETDLT
 block|}
 enum|;
 end_enum
@@ -131,13 +153,55 @@ block|}
 enum|;
 end_enum
 
+begin_comment
+comment|/* Parsing declarations */
+end_comment
+
+begin_define
+define|#
+directive|define
+name|NG_PATCH_CONFIG_TYPE
+value|{						\ 	{ "count",&ng_parse_uint32_type		},	\ 	{ "csum_flags",&ng_parse_uint64_type		},	\ 	{ "relative_offset",&ng_parse_uint32_type		},	\ 	{ "ops",&ng_patch_ops_array_type	},	\ 	{ NULL }							\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|NG_PATCH_OP_TYPE
+value|{				\ 	{ "offset",&ng_parse_uint32_type	},	\ 	{ "length",&ng_parse_uint16_type	},	\ 	{ "mode",&ng_parse_uint16_type	},	\ 	{ "value",&ng_parse_uint64_type	},	\ 	{ NULL }					\ }
+end_define
+
+begin_define
+define|#
+directive|define
+name|NG_PATCH_STATS_TYPE
+value|{				\ 	{ "Received",&ng_parse_uint64_type	},	\ 	{ "Patched",&ng_parse_uint64_type	},	\ 	{ "Dropped",&ng_parse_uint64_type	},	\ 	{ NULL }					\ }
+end_define
+
+begin_union
+union|union
+name|ng_patch_op_val
+block|{
+name|uint8_t
+name|v1
+decl_stmt|;
+name|uint16_t
+name|v2
+decl_stmt|;
+name|uint32_t
+name|v4
+decl_stmt|;
+name|uint64_t
+name|v8
+decl_stmt|;
+block|}
+union|;
+end_union
+
 begin_struct
 struct|struct
 name|ng_patch_op
 block|{
-name|uint64_t
-name|value
-decl_stmt|;
 name|uint32_t
 name|offset
 decl_stmt|;
@@ -148,16 +212,13 @@ comment|/* 1, 2, 4 or 8 (bytes) */
 name|uint16_t
 name|mode
 decl_stmt|;
+name|union
+name|ng_patch_op_val
+name|val
+decl_stmt|;
 block|}
 struct|;
 end_struct
-
-begin_define
-define|#
-directive|define
-name|NG_PATCH_OP_TYPE_INFO
-value|{				\ 	{ "value",&ng_parse_uint64_type	},	\ 	{ "offset",&ng_parse_uint32_type	},	\ 	{ "length",&ng_parse_uint16_type	},	\ 	{ "mode",&ng_parse_uint16_type	},	\ 	{ NULL }					\ }
-end_define
 
 begin_struct
 struct|struct
@@ -166,8 +227,11 @@ block|{
 name|uint32_t
 name|count
 decl_stmt|;
-name|uint32_t
+name|uint64_t
 name|csum_flags
+decl_stmt|;
+name|uint32_t
+name|relative_offset
 decl_stmt|;
 name|struct
 name|ng_patch_op
@@ -177,13 +241,6 @@ decl_stmt|;
 block|}
 struct|;
 end_struct
-
-begin_define
-define|#
-directive|define
-name|NG_PATCH_CONFIG_TYPE_INFO
-value|{					\ 	{ "count",&ng_parse_uint32_type		},	\ 	{ "csum_flags",&ng_parse_uint64_type		},	\ 	{ "ops",&ng_patch_ops_array_type		},	\ 	{ NULL }							\ }
-end_define
 
 begin_struct
 struct|struct
@@ -202,11 +259,28 @@ block|}
 struct|;
 end_struct
 
+begin_struct
+struct|struct
+name|ng_patch_vlan_header
+block|{
+name|u_int16_t
+name|tag
+decl_stmt|;
+name|u_int16_t
+name|etype
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
 begin_define
 define|#
 directive|define
-name|NG_PATCH_STATS_TYPE_INFO
-value|{			\ 	{ "Received",&ng_parse_uint64_type	},	\ 	{ "Patched",&ng_parse_uint64_type	},	\ 	{ "Dropped",&ng_parse_uint64_type	},	\ 	{ NULL }					\ }
+name|NG_PATCH_CONF_SIZE
+parameter_list|(
+name|count
+parameter_list|)
+value|(sizeof(struct ng_patch_config) + \ 	(count) * sizeof(struct ng_patch_op))
 end_define
 
 begin_endif
