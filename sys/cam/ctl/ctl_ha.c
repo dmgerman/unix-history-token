3012e1c044bf24fa408b5f725fc58202c8e58ecb
Alexander Motin <mav@FreeBSD.org>
2017-01-05 16:30:13 +0000

    Fix bootverbose affecting code logic in r294558.
    Reported by:	Jilles Tjoelker <jilles@stack.nl>
    
    
    Former-commit-id: 0e7d9208ff0e8b969fc924d15898b0db09e07214

--- sys/cam/ctl/ctl_ha.c
+++ sys/cam/ctl/ctl_ha.c
@@ -2226,10 +2226,12 @@ condition|(
 name|error
 operator|!=
 literal|0
-operator|&&
-name|bootverbose
 condition|)
 block|{
+if|if
+condition|(
+name|bootverbose
+condition|)
 name|printf
 argument_list|(
 literal|"%s: soconnect() error %d\n"
