a930576aa2effe8526f85828b909f22a745d0d9f
Adrian Chadd <adrian@FreeBSD.org>
2017-01-10 05:32:30 +0000

    [net80211] add CHAN_VHT2G/CHAN_VHT5G macros.
    Former-commit-id: 28abf798d624d17280301968bb5807cd8b972f7e

--- sys/net80211/_ieee80211.h
+++ sys/net80211/_ieee80211.h
@@ -1262,6 +1262,28 @@ end_define
 begin_define
 define|#
 directive|define
+name|IEEE80211_IS_CHAN_VHT_2GHZ
+parameter_list|(
+name|_c
+parameter_list|)
+define|\
+value|(IEEE80211_IS_CHAN_2GHZ(_c)&& \ 	 ((_c)->ic_flags& IEEE80211_CHAN_VHT) != 0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|IEEE80211_IS_CHAN_VHT_5GHZ
+parameter_list|(
+name|_c
+parameter_list|)
+define|\
+value|(IEEE80211_IS_CHAN_5GHZ(_c)&& \ 	 ((_c)->ic_flags& IEEE80211_CHAN_VHT) != 0)
+end_define
+
+begin_define
+define|#
+directive|define
 name|IEEE80211_IS_CHAN_VHT20
 parameter_list|(
 name|_c
