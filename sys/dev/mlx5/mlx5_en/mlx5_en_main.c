eef3eadb82b9780afa2e66a1c4b8e8b3b2a8fc27
Hans Petter Selasky <hselasky@FreeBSD.org>
2017-01-20 15:45:21 +0000

    Allow transmit packet bufring in software to be disabled.
    - Add new sysctl node to control the transmit packet bufring.
    
    - Add optimised version of the transmit routine which output packets
    directly to the DMA ring instead of using bufring in case the transmit
    lock is congested. This can reduce the number of taskswitches which in
    turn influence the overall system CPU usage, depending on the
    workload.
    
    - Add " TX" suffix to debug name for transmit mutexes to silence some
    witness warnings about aquiring duplicate locks having same name.
    
    MFC after:		1 week
    Sponsored by:		Mellanox Technologies
    Suggested by:		gallatin @
    
    
    Former-commit-id: 5d114e058f0f4fb4ef9d8efc7abb61ae4fec986d

--- sys/dev/mlx5/mlx5_en/mlx5_en_main.c
+++ sys/dev/mlx5/mlx5_en/mlx5_en_main.c
@@ -2149,6 +2149,12 @@ name|sq_stats
 operator|->
 name|dropped
 expr_stmt|;
+if|if
+condition|(
+name|sq_br
+operator|!=
+name|NULL
+condition|)
 name|tx_queue_dropped
 operator|+=
 name|sq_br
@@ -5035,6 +5041,18 @@ name|tc
 operator|=
 name|tc
 expr_stmt|;
+comment|/* check if we should allocate a second packet buffer */
+if|if
+condition|(
+name|priv
+operator|->
+name|params_ethtool
+operator|.
+name|tx_bufring_disable
+operator|==
+literal|0
+condition|)
+block|{
 name|sq
 operator|->
 name|br
@@ -5227,6 +5245,7 @@ argument_list|)
 expr_stmt|;
 endif|#
 directive|endif
+block|}
 name|snprintf
 argument_list|(
 name|buffer
@@ -5387,6 +5406,15 @@ operator|->
 name|uar
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|sq
+operator|->
+name|sq_tq
+operator|!=
+name|NULL
+condition|)
+block|{
 name|taskqueue_drain
 argument_list|(
 name|sq
@@ -5406,6 +5434,15 @@ operator|->
 name|sq_tq
 argument_list|)
 expr_stmt|;
+block|}
+if|if
+condition|(
+name|sq
+operator|->
+name|br
+operator|!=
+name|NULL
+condition|)
 name|buf_ring_free
 argument_list|(
 name|sq
@@ -7580,6 +7617,7 @@ argument_list|,
 literal|"mlx5tx"
 argument_list|,
 name|MTX_NETWORK_LOCK
+literal|" TX"
 argument_list|,
 name|MTX_DEF
 argument_list|)
@@ -7594,6 +7632,7 @@ argument_list|,
 literal|"mlx5comp"
 argument_list|,
 name|MTX_NETWORK_LOCK
+literal|" TX"
 argument_list|,
 name|MTX_DEF
 argument_list|)
