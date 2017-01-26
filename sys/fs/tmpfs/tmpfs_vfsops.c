9a5e66d59484fde0122f779e4b8662e10c0a34b3
Konstantin Belousov <kib@FreeBSD.org>
2017-01-26 11:00:57 +0000

    MFC r312414: Rename tmpfs_mount member allnode_lock to include namespace prefix.
    Former-commit-id: c86ebf16c8d76532f358533e593a7909599bf24b

--- sys/fs/tmpfs/tmpfs_vfsops.c
+++ sys/fs/tmpfs/tmpfs_vfsops.c
@@ -1023,7 +1023,7 @@ argument_list|(
 operator|&
 name|tmp
 operator|->
-name|allnode_lock
+name|tm_allnode_lock
 argument_list|,
 literal|"tmpfs allnode lock"
 argument_list|,
@@ -1089,7 +1089,7 @@ argument_list|,
 operator|&
 name|tmp
 operator|->
-name|allnode_lock
+name|tm_allnode_lock
 argument_list|)
 expr_stmt|;
 name|tmp
@@ -1558,7 +1558,7 @@ argument_list|(
 operator|&
 name|tmp
 operator|->
-name|allnode_lock
+name|tm_allnode_lock
 argument_list|)
 expr_stmt|;
 name|MPASS
