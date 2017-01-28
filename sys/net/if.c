17becd3918c1713e8aca6df477e6c73dd23859c7
dexuan <dexuan@FreeBSD.org>
2017-01-28 07:26:42 +0000

    ifnet: move the new ifnet_event EVENTHANDLER_DECLARE to net/if_var.h
    Thank glebius for pointing this out:
    "The network stuff shall not be added to sys/eventhandler.h"
    
    Reviewed by:	David_A_Bright_DELL.com, sephe, glebius
    Approved by:	sephe (mentor)
    MFC after:	2 weeks
    Sponsored by:	Microsoft
    Differential Revision:	https://reviews.freebsd.org/D9345
    
    
    Former-commit-id: bdfabd66b1b19fd07df5b862df031b7652f7c3ac

--- sys/net/if.c
+++ sys/net/if.c
@@ -174,12 +174,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/eventhandler.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<machine/stdarg.h>
 end_include
 
