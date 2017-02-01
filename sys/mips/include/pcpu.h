5fbb3c9475cf16cdda877b0e9c6ab6cba3989ae4
Jason A. Harmening <jah@FreeBSD.org>
2017-02-01 03:32:49 +0000

    Implement get_pcpu() for the remaining architectures and use it to replace pcpu_find(curcpu) in MI code.
    Former-commit-id: d0601d6436b85995b5e923422625b3e5a1ea968c

--- sys/mips/include/pcpu.h
+++ sys/mips/include/pcpu.h
@@ -159,6 +159,14 @@ end_define
 begin_define
 define|#
 directive|define
+name|get_pcpu
+parameter_list|()
+value|(pcpup)
+end_define
+
+begin_define
+define|#
+directive|define
 name|PCPU_ADD
 parameter_list|(
 name|member
