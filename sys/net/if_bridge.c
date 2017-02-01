2f707bb713a79b5f257d2f1306b167d464b34f6e
Kristof Provost <kp@FreeBSD.org>
2017-02-01 20:27:38 +0000

    MFC 312782
    bridge: Release the bridge lock when calling bridge_set_ifcap()
    
    This calls ioctl() handlers for the different interfaces in the bridge.
    These handlers expect to get called in an ioctl context where it's safe
    for them to sleep. We may not sleep with the bridge lock held.
    
    However, we still need to protect the interface list, to ensure it
    doesn't get changed while we iterate over it.
    Use BRIDGE_XLOCK(), which prevents bridge members from being removed.
    Adding bridge members is safe, because it uses LIST_INSERT_HEAD().
    
    This caused panics when adding xen interfaces to a bridge.
    
    PR:		216304
    Reviewed by:	ae
    Sponsored by:	RootBSD
    Differential Revision:	https://reviews.freebsd.org/D9290
    
    Former-commit-id: 9d7d51dcde0bf9c134e650b36e19253fecc665fe

--- sys/net/if_bridge.c
+++ sys/net/if_bridge.c
@@ -4625,6 +4625,11 @@ operator|->
 name|bif_savedcaps
 expr_stmt|;
 block|}
+name|BRIDGE_XLOCK
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 name|LIST_FOREACH
 argument_list|(
 argument|bif
@@ -4657,6 +4662,11 @@ name|enabled
 operator||=
 name|mask
 expr_stmt|;
+name|BRIDGE_UNLOCK
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 name|bridge_set_ifcap
 argument_list|(
 name|sc
@@ -4666,7 +4676,17 @@ argument_list|,
 name|enabled
 argument_list|)
 expr_stmt|;
+name|BRIDGE_LOCK
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 block|}
+name|BRIDGE_XDROP
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
@@ -4705,6 +4725,11 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|BRIDGE_UNLOCK_ASSERT
+argument_list|(
+name|sc
+argument_list|)
+expr_stmt|;
 name|bzero
 argument_list|(
 operator|&
