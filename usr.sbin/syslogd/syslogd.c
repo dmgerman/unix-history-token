3c36c20cab2aec6f0176a06bf6142ab67c797772
Garrett Cooper <ngie@FreeBSD.org>
2017-02-07 01:28:55 +0000

    Sort sys/ #includes some more
    MFC after:	1 week
    X-MFC with:	r313358
    Sponsored by:	Dell EMC Isilon
    
    
    Former-commit-id: ac1b92809511fbb73fc3f1ea6f43fb371351742f

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -194,25 +194,25 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/uio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<sys/wait.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<sys/time.h>
 end_include
 
 begin_include
 include|#
 directive|include
+file|<sys/uio.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<sys/un.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/wait.h>
 end_include
 
 begin_if
