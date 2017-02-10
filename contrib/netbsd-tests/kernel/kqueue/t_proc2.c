314358d4fb40be2aaed45a0d3935ebc6927e8809
Garrett Cooper <ngie@FreeBSD.org>
2017-02-10 02:29:39 +0000

    MFC r305468:
    Port contrib/netbsd-tests/kernel/kqueue/... as tests/sys/kqueue/...
    
    proc2_test must be skipped because the invariant tested
    (`ke.fflags & NOTE_TRACKERR`) doesn't pass.
    
    
    Former-commit-id: 3bb33c68e07e6473535aa1e3cfeaa8a5656ef4e1

--- contrib/netbsd-tests/kernel/kqueue/t_proc2.c
+++ contrib/netbsd-tests/kernel/kqueue/t_proc2.c
@@ -29,6 +29,23 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|__FreeBSD__
+end_ifdef
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
