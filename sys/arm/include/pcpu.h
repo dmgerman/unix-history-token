4fa576db6d6fadfa49d110e3c8d5f04c2a87b30a
Jason A. Harmening <jah@FreeBSD.org>
2017-02-15 10:35:46 +0000

    MFC r312610, r312792
    r312610:
     Like r310481 for i386, move the objects used to create temporary
     mappings for armv6 pmap zero and copy operations to the MD PCPU region.
     Change sysmap initialization to only allocate KVA pages for CPUs that
     are actually present.
    
     While here, collapse CMAP3 into CMAP2 (their use was mutually exclusive
     anyway) and "recover" some space in PCPU padding that has always been
     available due to 64-byte cacheline padding.
    
    r312792:
     Further cleanup of per-CPU armv6 pmap data:
    
     - Replace pcpu_find(curcpu) with get_pcpu(), which is much more direct.
    
     - Remove armv4 pcpu fields which I added in r286296 but never needed
       to use.
    
     - armv6 pc_qmap_addr was leftover from the old armv6 pmap
       implementation.  Rename it and put it to use in the new one.
    
    
    Former-commit-id: dce21f8fca08edd8230643d7f8e53efddb03bfbc

--- sys/arm/include/pcpu.h
+++ sys/arm/include/pcpu.h
@@ -27,6 +27,18 @@ directive|include
 file|<machine/cpuconf.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<sys/_lock.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<sys/_mutex.h>
+end_include
+
 begin_define
 define|#
 directive|define
@@ -62,7 +74,7 @@ define|#
 directive|define
 name|PCPU_MD_FIELDS
 define|\
-value|unsigned int pc_vfpsid;						\ 	unsigned int pc_vfpmvfr0;					\ 	unsigned int pc_vfpmvfr1;					\ 	struct pmap *pc_curpmap;					\ 	vm_offset_t pc_qmap_addr;					\ 	void *pc_qmap_pte;						\ 	unsigned int pc_dbreg[32];					\ 	int pc_dbreg_cmd;						\ 	char __pad[1]
+value|unsigned int pc_vfpsid;						\ 	unsigned int pc_vfpmvfr0;					\ 	unsigned int pc_vfpmvfr1;					\ 	struct pmap *pc_curpmap;					\ 	struct mtx pc_cmap_lock;					\ 	void *pc_cmap1_pte2p;						\ 	void *pc_cmap2_pte2p;						\ 	caddr_t pc_cmap1_addr;						\ 	caddr_t pc_cmap2_addr;						\ 	vm_offset_t pc_qmap_addr;					\ 	void *pc_qmap_pte2p;						\ 	unsigned int pc_dbreg[32];					\ 	int pc_dbreg_cmd;						\ 	char __pad[19]
 end_define
 
 begin_else
@@ -75,7 +87,7 @@ define|#
 directive|define
 name|PCPU_MD_FIELDS
 define|\
-value|vm_offset_t qmap_addr;						\ 	void *pc_qmap_pte;						\ 	char __pad[149]
+value|char __pad[157]
 end_define
 
 begin_endif
