50a7e83b3c416d717b79e95539ae4b0ea68dd39b
Alan Somers <asomers@FreeBSD.org>
2017-02-28 22:49:41 +0000

    MFC r311572, r311895, r311928, r311985, r312395, r312417
    r311572:
    Fix file descriptor leaks in cmp(1)
    
    Also, add a few test cases
    
    Reported by:	Coverity
    CID:		271624 275338
    Reviewed by:	ngie
    MFC after:	4 weeks
    Sponsored by:	Spectra Logic Corp
    Differential Revision:	https://reviews.freebsd.org/D9074
    
    r311895:
    Fix memory leaks during "tail -r" of an irregular file
    
    * Rewrite r_buf to use standard tail queues instead of a hand-rolled
      circular linked list. Free dynamic allocations when done.
    * Remove an optimization for the case where the file is a multiple of 128KB
      in size and there is a scarcity of memory.
    * Add ATF tests for "tail -r" and its variants.
    
    Reported by:	Valgrind
    Reviewed by:	ngie
    MFC after:	4 weeks
    Sponsored by:	Spectra Logic Corp
    Differential Revision:	https://reviews.freebsd.org/D9067
    
    r311928:
    Fix build of usr.bin/tail with GCC
    
    Submitted by:	pluknet
    Reported by:	pluknet
    MFC after:	27 days
    X-MFC-with:	311895
    Sponsored by:	Spectra Logic Corp
    
    r311985:
    Fix uninitialized variable CIDs in route6d
    
    The variables in question are actually return arguments, but it's still good
    form to initialize them.
    
    Reported by:	Coverity
    CID:		979679 979680
    MFC after:	4 weeks
    Sponsored by:	Spectra Logic Corp
    
    r312395:
    Fix several Coverity CIDs in devd
    
    CID 1362055, 1362054: File descriptor leaks during shutdown
    CID 1362013: Potential null-termination fail with long network device names
    CID 1362097: Uncaught exception during memory pressure
    CID 1362017, 1362016: Unchecked errors, possibly resulting in weird behavior
    	if two devd instances start at the same time.
    CID 1362015:	Unchecked error that will probably never fail
    
    Reported by:	Coverity
    CID:	1362055 1362054 1362013 1362097 1362017 1362016 1362015
    MFC after:	4 weeks
    Sponsored by:	Spectra Logic Corp
    
    r312417:
    Fix build of devd with GCC 4.2
    
    Reported by:	olivier
    Pointy-hat-to:	asomers
    MFC after:	27 days
    X-MFC-with:	312395
    Sponsored by:	Spectra Logic Corp
    
    Former-commit-id: bb5138f8f128eacd74e0f1de93a68c964c005d50

--- usr.sbin/route6d/route6d.c
+++ usr.sbin/route6d/route6d.c
@@ -5556,6 +5556,12 @@ name|msg_iovlen
 operator|=
 literal|1
 expr_stmt|;
+name|m
+operator|.
+name|msg_flags
+operator|=
+literal|0
+expr_stmt|;
 if|if
 condition|(
 operator|!
@@ -5923,6 +5929,12 @@ sizeof|sizeof
 argument_list|(
 name|cmsgbuf
 argument_list|)
+expr_stmt|;
+name|m
+operator|.
+name|msg_flags
+operator|=
+literal|0
 expr_stmt|;
 if|if
 condition|(
