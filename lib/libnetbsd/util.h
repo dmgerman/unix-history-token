c63c649621b7195df91f787ea50d1eaaf6006783
Garrett Cooper <ngie@FreeBSD.org>
2017-02-28 00:56:33 +0000

    Note: this change reintroduces r314020 after r314327, r314330, and r314332
    MFC r313404:
    
    Improve libnetbsd compatibility with NetBSD
    
    This change is being made to diff reduce/reduce duplication in
    contrib/netbsd-tests and to facilitate further porting of software from
    NetBSD
    
    Add the following headers:
    - sys/event.h:
    -- sys/types.h is required for kqueue on FreeBSD, but not NetBSD.
    - sys/types.h:
    -- NBBY is defined in sys/param.h on FreeBSD, not sys/types.h like on NetBSD.
       Pull in sys/param.h to have parity with NetBSD.
    - sys/wait.h:
    -- Define wrusage as __wrusage for parity with NetBSD typedef.
    - glob.h
    -- Define __gl_stat_t as "struct stat" for parity with NetBSD typedef.
    - pthread.h:
    -- Pull in pthread_np.h for _np functions defined separately on FreeBSD.
    
    Improve compatibility with NetBSD in the following headers:
    
    - sha1.h:
    -- define SHA1_CTX as SHA_CTX
    -- define SHA1Final as SHA1_Final
    - sha2.h:
    -- #include sha384 to pick up all of the SHA 384 bit macros and definitions.
    - util.h:
    -- Add sys/types.h to util.h to pollute the header for types used in
       flags_to_string and string_to_flags (u_long) as NetBSD doesn't require them
       for the functions.
    
    
    Former-commit-id: 409f320a79f6fa58506c6f2fd1b9a5beebff1c67

--- lib/libnetbsd/util.h
+++ lib/libnetbsd/util.h
@@ -10,14 +10,20 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|_UTIL_H_
+name|_LIBNETBSD_UTIL_H_
 end_ifndef
 
 begin_define
 define|#
 directive|define
-name|_UTIL_H_
+name|_LIBNETBSD_UTIL_H_
 end_define
+
+begin_include
+include|#
+directive|include
+file|<sys/types.h>
+end_include
 
 begin_include
 include|#
@@ -65,10 +71,6 @@ begin_endif
 endif|#
 directive|endif
 end_endif
-
-begin_comment
-comment|/* _UTIL_H_ */
-end_comment
 
 end_unit
 
