128d509babbe9fbf9fc2f2e95170ef46a93d2b49
Alexander Motin <mav@FreeBSD.org>
2017-03-02 07:55:47 +0000

    Execute last ZIO of log commit synchronously.
    For short transactions overhead of context switch can be too large.
    Skipping it gives significant latency reduction.  For large ones,
    including multiple ZIOs, latency is less critical, while throughput
    there may become limited by checksumming speed of single CPU core.
    To get best of both cases, execute last ZIO directly from calling
    thread context to save latency, while all others (if there are any)
    enqueue to taskqueues in traditional way.
    
    MFC after:	2 weeks
    Sponsored by:	iXsystems, Inc.
    
    
    Former-commit-id: 4f19ebebe872073ea82b7e20bcabad969cfeaaf8

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zil.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/zil.c
@@ -4424,6 +4424,9 @@ parameter_list|,
 name|lwb_t
 modifier|*
 name|lwb
+parameter_list|,
+name|boolean_t
+name|last
 parameter_list|)
 block|{
 name|lwb_t
@@ -4877,6 +4880,19 @@ operator|->
 name|lwb_nused
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|last
+condition|)
+name|lwb
+operator|->
+name|lwb_zio
+operator|->
+name|io_pipeline
+operator|&=
+operator|~
+name|ZIO_STAGE_ISSUE_ASYNC
+expr_stmt|;
 name|zio_nowait
 argument_list|(
 name|lwb
@@ -5091,6 +5107,8 @@ argument_list|(
 name|zilog
 argument_list|,
 name|lwb
+argument_list|,
+name|B_FALSE
 argument_list|)
 expr_stmt|;
 if|if
@@ -7300,6 +7318,8 @@ argument_list|(
 name|zilog
 argument_list|,
 name|lwb
+argument_list|,
+name|B_TRUE
 argument_list|)
 expr_stmt|;
 name|zilog
