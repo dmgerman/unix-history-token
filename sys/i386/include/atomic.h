bb4df4a9e67d9c58627627cf139aff68019735a0
Mateusz Guzik <mjg@FreeBSD.org>
2017-03-16 06:00:27 +0000

    MFC r311169,r311898,r312925,r312973,r312975,r313007,r313040,r313080,     r313254,r313341
    amd64: add atomic_fcmpset
    
    ==
    
    sparc64: add atomic_fcmpset
    
    ==
    
    Implement atomic_fcmpset_* for arm and arm64.
    
    ==
    
    Add atomic_fcmpset_*() inlines for powerpc
    
    Summary:
    atomic_fcmpset_*() is analogous to atomic_cmpset(), but saves off the read value
    from the target memory location into the 'old' pointer in the case of failure.
    
    ==
    
    i386: add atomic_fcmpset
    
    ==
    
    Don't retry a lost reservation in atomic_fcmpset()
    
    The desired behavior of atomic_fcmpset_() is to always exit on error.  Instead
    of retrying on lost reservation, leave the retry to the caller, and return
    
    ==
    
    Add atomic_fcmpset_*() inlines for MIPS
    
    atomic_fcmpset_*() is analogous to atomic_cmpset(), but saves off the
    read value from the target memory location into the 'old' pointer.
    
    ==
    
    i386: fixup fcmpset
    
    An incorrect output specifier was used which worked with clang by accident,
    but breaks with the in-tree gcc version.
    
    While here plug a whitespace nit.
    
    ==
    
    Implement atomic_fcmpset_*() for RISC-V.
    
    ==
    
    Use 64bit store instruction in atomic_fcmpset_64.
    
    
    Former-commit-id: 1a750fa46939884682e5acc53f3a05497b474a1f

--- sys/i386/include/atomic.h
+++ sys/i386/include/atomic.h
@@ -180,6 +180,25 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|int
+name|atomic_fcmpset_int
+parameter_list|(
+specifier|volatile
+name|u_int
+modifier|*
+name|dst
+parameter_list|,
+name|u_int
+modifier|*
+name|expect
+parameter_list|,
+name|u_int
+name|src
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|u_int
 name|atomic_fetchadd_int
 parameter_list|(
@@ -456,6 +475,77 @@ operator|,
 comment|/* 1 */
 literal|"+a"
 operator|(
+name|expect
+operator|)
+comment|/* 2 */
+operator|:
+literal|"r"
+operator|(
+name|src
+operator|)
+comment|/* 3 */
+operator|:
+literal|"memory"
+operator|,
+literal|"cc"
+block|)
+function|;
+end_function
+
+begin_return
+return|return
+operator|(
+name|res
+operator|)
+return|;
+end_return
+
+begin_function
+unit|}  static
+name|__inline
+name|int
+name|atomic_fcmpset_int
+parameter_list|(
+specifier|volatile
+name|u_int
+modifier|*
+name|dst
+parameter_list|,
+name|u_int
+modifier|*
+name|expect
+parameter_list|,
+name|u_int
+name|src
+parameter_list|)
+block|{
+name|u_char
+name|res
+decl_stmt|;
+asm|__asm __volatile(
+literal|"	"
+name|MPLOCKED
+literal|"		"
+literal|"	cmpxchgl %3,%1 ;	"
+literal|"       sete	%0 ;		"
+literal|"# atomic_cmpset_int"
+operator|:
+literal|"=q"
+operator|(
+name|res
+operator|)
+operator|,
+comment|/* 0 */
+literal|"+m"
+operator|(
+operator|*
+name|dst
+operator|)
+operator|,
+comment|/* 1 */
+literal|"+a"
+operator|(
+operator|*
 name|expect
 operator|)
 comment|/* 2 */
@@ -2555,6 +2645,20 @@ end_define
 begin_define
 define|#
 directive|define
+name|atomic_fcmpset_acq_int
+value|atomic_fcmpset_int
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_rel_int
+value|atomic_fcmpset_int
+end_define
+
+begin_define
+define|#
+directive|define
 name|atomic_set_acq_long
 value|atomic_set_barr_long
 end_define
@@ -2620,6 +2724,20 @@ define|#
 directive|define
 name|atomic_cmpset_rel_long
 value|atomic_cmpset_long
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_acq_long
+value|atomic_fcmpset_long
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_rel_long
+value|atomic_fcmpset_long
 end_define
 
 begin_define
@@ -2972,6 +3090,27 @@ end_define
 begin_define
 define|#
 directive|define
+name|atomic_fcmpset_32
+value|atomic_fcmpset_int
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_acq_32
+value|atomic_fcmpset_acq_int
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_rel_32
+value|atomic_fcmpset_rel_int
+end_define
+
+begin_define
+define|#
+directive|define
 name|atomic_swap_32
 value|atomic_swap_int
 end_define
@@ -3231,6 +3370,51 @@ name|new
 parameter_list|)
 define|\
 value|atomic_cmpset_rel_int((volatile u_int *)(dst), (u_int)(old), \ 	    (u_int)(new))
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_ptr
+parameter_list|(
+name|dst
+parameter_list|,
+name|old
+parameter_list|,
+name|new
+parameter_list|)
+define|\
+value|atomic_fcmpset_int((volatile u_int *)(dst), (u_int *)(old), (u_int)(new))
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_acq_ptr
+parameter_list|(
+name|dst
+parameter_list|,
+name|old
+parameter_list|,
+name|new
+parameter_list|)
+define|\
+value|atomic_fcmpset_acq_int((volatile u_int *)(dst), (u_int *)(old), \ 	    (u_int)(new))
+end_define
+
+begin_define
+define|#
+directive|define
+name|atomic_fcmpset_rel_ptr
+parameter_list|(
+name|dst
+parameter_list|,
+name|old
+parameter_list|,
+name|new
+parameter_list|)
+define|\
+value|atomic_fcmpset_rel_int((volatile u_int *)(dst), (u_int *)(old), \ 	    (u_int)(new))
 end_define
 
 begin_define
