e94d869c37b625d109960237acbe2477e9d459da
Dag-Erling Smorgrav <des@FreeBSD.org>
2017-03-17 14:18:52 +0000

    r308996 broke IP literals by assuming that a colon could only occur as a separator between host and port, and using strchr() to search for it. Rewrite fetch_resolve() so it handles bracketed literals correctly, and remove similar code elsewhere to avoid passing unbracketed literals to fetch_resolve().  Remove #ifdef INET6 so we still parse IP literals correctly even if we do not have the ability to connect to them.
    While there, fix an off-by-one error which caused HTTP 400 errors to be
    misinterpreted as redirects.
    
    PR:		217723
    MFC after:	1 week
    Reported by:	bapt, bz, cem, ngie
    
    Former-commit-id: bc8a2ef13dc07d8166abbeea94055e840f6554ac

--- lib/libfetch/fetch.c
+++ lib/libfetch/fetch.c
@@ -1830,9 +1830,6 @@ name|URL
 expr_stmt|;
 block|}
 comment|/* hostname */
-ifdef|#
-directive|ifdef
-name|INET6
 if|if
 condition|(
 operator|*
@@ -1882,8 +1879,6 @@ operator|=
 name|q
 operator|-
 name|p
-operator|-
-literal|2
 operator|)
 operator|>
 name|MAXHOSTNAMELEN
@@ -1898,7 +1893,6 @@ name|u
 operator|->
 name|host
 argument_list|,
-operator|++
 name|p
 argument_list|,
 name|i
@@ -1910,8 +1904,7 @@ name|q
 expr_stmt|;
 block|}
 else|else
-endif|#
-directive|endif
+block|{
 for|for
 control|(
 name|i
@@ -1955,6 +1948,7 @@ operator|=
 operator|*
 name|p
 expr_stmt|;
+block|}
 comment|/* port */
 if|if
 condition|(
@@ -2226,12 +2220,12 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"scheme:   [%s]\n"
-literal|"user:     [%s]\n"
-literal|"password: [%s]\n"
-literal|"host:     [%s]\n"
-literal|"port:     [%d]\n"
-literal|"document: [%s]\n"
+literal|"scheme:   \"%s\"\n"
+literal|"user:     \"%s\"\n"
+literal|"password: \"%s\"\n"
+literal|"host:     \"%s\"\n"
+literal|"port:     \"%d\"\n"
+literal|"document: \"%s\"\n"
 argument_list|,
 name|u
 operator|->
