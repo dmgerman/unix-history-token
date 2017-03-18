58875942e30b2e47c99eb80641121b278479fa2c
Alan Cox <alc@FreeBSD.org>
2017-03-18 05:38:10 +0000

    MFC r310083   Tidy up.  Mostly, remove or replace stale comments.  Most of the comments   in this file actually described the operation of the swap pager, not the   default pager.  Given that this is the wrong place to discuss the   implementation of the swap pager, it shouldn't come as a surprise that as   the swap pager evolved these comments became increasingly stale.  In   addition, apply some style fixes, like modernizing a few remaining old-   style function definitions.
    Former-commit-id: 34aa6486253b47f67f2fa10e43f4c1939e365487

--- sys/vm/default_pager.c
+++ sys/vm/default_pager.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1995, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * The default pager is responsible for supplying backing store to unbacked  * storage.  The backing store is usually swap so we just fall through to  * the swap routines.  However, since swap metadata has not been assigned,  * the swap routines assign and manage the swap backing store through the  * vm_page->swapblk field.  The object is only converted when the page is   * physically freed after having been cleaned and even then vm_page->swapblk  * is maintained whenever a resident page also has swap backing store.  */
+comment|/*-  * Copyright (c) 1995, David Greenman  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  * 3. All advertising materials mentioning features or use of this software  *    must display the following acknowledgement:  *	This product includes software developed by David Greenman.  * 4. The name of the author may not be used to endorse or promote products  *    derived from this software without specific prior written permission.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_include
@@ -212,7 +212,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*  * no_pager_alloc just returns an initialized object.  */
+comment|/*  * Return an initialized object.  */
 end_comment
 
 begin_function
@@ -327,7 +327,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * deallocate resources associated with default objects.   The default objects  * have no special resources allocated to them, but the vm_page's being used  * in this object might.  Still, we do not have to do anything - we will free  * the swapblk in the underlying vm_page's when we free the vm_page or  * garbage collect the vm_page cache list.  */
+comment|/*  * Deallocate resources associated with the object.  */
 end_comment
 
 begin_function
@@ -335,13 +335,11 @@ specifier|static
 name|void
 name|default_pager_dealloc
 parameter_list|(
-name|object
-parameter_list|)
 name|vm_object_t
 name|object
-decl_stmt|;
+parameter_list|)
 block|{
-comment|/* 	 * OBJT_DEFAULT objects have no special resources allocated to them. 	 */
+comment|/* Reserved swap is released by vm_object_destroy(). */
 name|object
 operator|->
 name|type
@@ -352,7 +350,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Load pages from backing store.  Since OBJT_DEFAULT is converted to  * OBJT_SWAP at the time a swap-backed vm_page_t is freed, we will never  * see a vm_page with assigned swap here.  */
+comment|/*  * Load pages from backing store.  */
 end_comment
 
 begin_function
@@ -379,6 +377,7 @@ modifier|*
 name|rahead
 parameter_list|)
 block|{
+comment|/* 	 * Since an OBJT_DEFAULT object is converted to OBJT_SWAP by the first 	 * call to the putpages method, this function will never be called on 	 * a vm_page with assigned swap. 	 */
 return|return
 operator|(
 name|VM_PAGER_FAIL
@@ -388,7 +387,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Store pages to backing store.  We should assign swap and initiate  * I/O.  We do not actually convert the object to OBJT_SWAP here.  The  * object will be converted when the written-out vm_page_t is moved from the  * cache to the free list.  */
+comment|/*  * Store pages to backing store.  */
 end_comment
 
 begin_function
@@ -414,6 +413,7 @@ modifier|*
 name|rtvals
 parameter_list|)
 block|{
+comment|/* The swap pager will convert the object to OBJT_SWAP. */
 name|swappagerops
 operator|.
 name|pgo_putpages
@@ -433,7 +433,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Tell us whether the backing store for the requested (object,index) is  * synchronized.  i.e. tell us whether we can throw the page away and   * reload it later.  So, for example, if we are in the process of writing  * the page to its backing store, or if no backing store has been assigned,  * it is not yet synchronized.  *  * It is possible to have fully-synchronized swap assigned without the  * object having been converted.  We just call swap_pager_haspage() to  * deal with it since it must already deal with it plus deal with swap  * meta-data structures.  */
+comment|/*  * Tell us whether the requested (object,index) is available from the object's  * backing store.  */
 end_comment
 
 begin_function
@@ -441,31 +441,26 @@ specifier|static
 name|boolean_t
 name|default_pager_haspage
 parameter_list|(
-name|object
-parameter_list|,
-name|pindex
-parameter_list|,
-name|before
-parameter_list|,
-name|after
-parameter_list|)
 name|vm_object_t
 name|object
-decl_stmt|;
+parameter_list|,
 name|vm_pindex_t
 name|pindex
-decl_stmt|;
+parameter_list|,
 name|int
 modifier|*
 name|before
-decl_stmt|;
+parameter_list|,
 name|int
 modifier|*
 name|after
-decl_stmt|;
+parameter_list|)
 block|{
+comment|/* An OBJT_DEFAULT object has no backing store. */
 return|return
+operator|(
 name|FALSE
+operator|)
 return|;
 block|}
 end_function
