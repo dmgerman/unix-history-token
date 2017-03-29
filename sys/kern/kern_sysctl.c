a7d195ea5a82f7d5bf8845de17669a7cef5e2238
Garrett Cooper <ngie@FreeBSD.org>
2017-03-29 08:00:11 +0000

    MFC r315699:
    Print out name of non-dynamic sysctl in sysctl_remove_oid_locked
    
    This will provide a slightly better smoking gun than just stating
    "can't remove non-dynamic nodes!" when calling sysctl_ctx_free(9)
    and sysctl_remove_{name,oid}(9) with a non-dynamic (likely
    static) sysctl.
    
    
    Former-commit-id: 195dba56e4b654a02727078d377cc5e2d5b72513

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -1487,7 +1487,11 @@ condition|)
 block|{
 name|printf
 argument_list|(
-literal|"can't remove non-dynamic nodes!\n"
+literal|"Warning: can't remove non-dynamic nodes (%s)!\n"
+argument_list|,
+name|oidp
+operator|->
+name|oid_name
 argument_list|)
 expr_stmt|;
 return|return
