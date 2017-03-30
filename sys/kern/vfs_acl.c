c418dd62a457be3912f0442d9b9be5a6829f863d
Robert N. M. Watson <rwatson@FreeBSD.org>
2017-03-30 22:00:58 +0000

    Add system-call argument auditing for ACL-related system calls.
    Obtained from:	TrustedBSD Project
    MFC after:	3 weeks
    Sponsored by:	DARPA, AFRL
    
    
    Former-commit-id: 9e8d15d471484d57a65a119be6be3ed1280ec43a

--- sys/kern/vfs_acl.c
+++ sys/kern/vfs_acl.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 1999-2006 Robert N. M. Watson  * All rights reserved.  *  * This software was developed by Robert Watson for the TrustedBSD Project.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
+comment|/*-  * Copyright (c) 1999-2006, 2016-2017 Robert N. M. Watson  * All rights reserved.  *  * This software was developed by Robert Watson for the TrustedBSD Project.  *  * Portions of this software were developed by BAE Systems, the University of  * Cambridge Computer Laboratory, and Memorial University under DARPA/AFRL  * contract FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent  * Computing (TC) research program.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  */
 end_comment
 
 begin_comment
@@ -121,6 +121,12 @@ begin_include
 include|#
 directive|include
 file|<sys/acl.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|<security/audit/audit.h>
 end_include
 
 begin_include
@@ -841,6 +847,11 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_VALUE
+argument_list|(
+name|type
+argument_list|)
+expr_stmt|;
 name|inkernelacl
 operator|=
 name|acl_alloc
@@ -898,6 +909,11 @@ argument_list|,
 name|LK_EXCLUSIVE
 operator||
 name|LK_RETRY
+argument_list|)
+expr_stmt|;
+name|AUDIT_ARG_VNODE1
+argument_list|(
+name|vp
 argument_list|)
 expr_stmt|;
 ifdef|#
@@ -1019,6 +1035,11 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_VALUE
+argument_list|(
+name|type
+argument_list|)
+expr_stmt|;
 name|inkernelacl
 operator|=
 name|acl_alloc
@@ -1035,6 +1056,11 @@ argument_list|,
 name|LK_EXCLUSIVE
 operator||
 name|LK_RETRY
+argument_list|)
+expr_stmt|;
+name|AUDIT_ARG_VNODE1
+argument_list|(
+name|vp
 argument_list|)
 expr_stmt|;
 ifdef|#
@@ -1159,6 +1185,11 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_VALUE
+argument_list|(
+name|type
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|vn_start_write
@@ -1191,6 +1222,11 @@ argument_list|,
 name|LK_EXCLUSIVE
 operator||
 name|LK_RETRY
+argument_list|)
+expr_stmt|;
+name|AUDIT_ARG_VNODE1
+argument_list|(
+name|vp
 argument_list|)
 expr_stmt|;
 ifdef|#
@@ -1268,7 +1304,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Given a vnode, check whether an ACL is appropriate for it  */
+comment|/*  * Given a vnode, check whether an ACL is appropriate for it  *  * XXXRW: No vnode lock held so can't audit vnode state...?  */
 end_comment
 
 begin_function
@@ -1403,6 +1439,8 @@ argument_list|,
 name|LOOKUP
 argument_list|,
 name|FOLLOW
+operator||
+name|AUDITVNODE1
 argument_list|,
 name|UIO_USERSPACE
 argument_list|,
@@ -1498,6 +1536,8 @@ argument_list|,
 name|LOOKUP
 argument_list|,
 name|NOFOLLOW
+operator||
+name|AUDITVNODE1
 argument_list|,
 name|UIO_USERSPACE
 argument_list|,
@@ -1593,6 +1633,8 @@ argument_list|,
 name|LOOKUP
 argument_list|,
 name|FOLLOW
+operator||
+name|AUDITVNODE1
 argument_list|,
 name|UIO_USERSPACE
 argument_list|,
@@ -1688,6 +1730,8 @@ argument_list|,
 name|LOOKUP
 argument_list|,
 name|NOFOLLOW
+operator||
+name|AUDITVNODE1
 argument_list|,
 name|UIO_USERSPACE
 argument_list|,
@@ -1779,6 +1823,13 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_FD
+argument_list|(
+name|uap
+operator|->
+name|filedes
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|getvnode
@@ -1873,6 +1924,13 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_FD
+argument_list|(
+name|uap
+operator|->
+name|filedes
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|getvnode
@@ -2149,6 +2207,13 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_FD
+argument_list|(
+name|uap
+operator|->
+name|filedes
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|getvnode
@@ -2429,6 +2494,13 @@ decl_stmt|;
 name|int
 name|error
 decl_stmt|;
+name|AUDIT_ARG_FD
+argument_list|(
+name|uap
+operator|->
+name|filedes
+argument_list|)
+expr_stmt|;
 name|error
 operator|=
 name|getvnode
