f265c9774f8a579d2aec8cbc137d72cd58744171
Justin Hibbits <jhibbits@FreeBSD.org>
2017-04-01 22:29:11 +0000

    Add a helper function to get system reference clock
    Many devices are clocked from the SoC's platform clock / 2.  Some device nodes
    include their own clock-frequency property, while others are dependent on the
    SoC's bus-frequency property instead.  To simplify, add a helper function to get
    this clock.
    
    
    Former-commit-id: 12fb5d29d7cd12c85f947e0f0d89e9e6461b2aba

--- sys/powerpc/mpc85xx/mpc85xx.h
+++ sys/powerpc/mpc85xx/mpc85xx.h
@@ -697,6 +697,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|uint32_t
+name|mpc85xx_get_system_clock
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
