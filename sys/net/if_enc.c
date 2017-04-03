a1f3622544175666aa4b3344dfaecc9e1953ebe0
Andrey V. Elsukov <ae@FreeBSD.org>
2017-04-03 12:48:56 +0000

    MFC r316012:   Declare module version.
    Former-commit-id: c38d567527f6d37d2fdb2171bf88cdfb11b95f59

--- sys/net/if_enc.c
+++ sys/net/if_enc.c
@@ -2137,5 +2137,15 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_VERSION
+argument_list|(
+name|if_enc
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
