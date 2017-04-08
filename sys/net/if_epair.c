3d813e8086da8c1690270c222ce2dfa011a46db6
Patrick Kelsey <pkelsey@FreeBSD.org>
2017-04-08 04:50:50 +0000

    Fixed typo in comment found while reading commit email for fix of other typo in same comment.
    ned -> need
    
    MFC after:	3 days
    
    
    Former-commit-id: fbd8fe48d8ae08e7351a1c6b97d4aac655ecaee1

--- sys/net/if_epair.c
+++ sys/net/if_epair.c
@@ -1729,7 +1729,7 @@ operator|==
 literal|0
 condition|)
 return|return;
-comment|/* 	 * We get packets here from ether_output via if_handoff() 	 * and ned to put them into the input queue of the oifp 	 * and call oifp->if_input() via netisr/epair_sintr(). 	 */
+comment|/* 	 * We get packets here from ether_output via if_handoff() 	 * and need to put them into the input queue of the oifp 	 * and call oifp->if_input() via netisr/epair_sintr(). 	 */
 name|oifp
 operator|=
 name|sc
