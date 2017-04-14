55c15b8ac4be5be515213c5058eb23db28bd5ba3
Andriy Gapon <avg@FreeBSD.org>
2017-04-14 15:09:30 +0000

    MFC r315852: zfs: add zio_buf_alloc_nowait and use it in vdev_queue_aggregate
    Former-commit-id: 4c3337b1e3a6e0faeac72525c60bbdd19ff7f19c

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h
@@ -1886,6 +1886,15 @@ parameter_list|)
 function_decl|;
 specifier|extern
 name|void
+modifier|*
+name|zio_buf_alloc_nowait
+parameter_list|(
+name|size_t
+name|size
+parameter_list|)
+function_decl|;
+specifier|extern
+name|void
 name|zio_buf_free
 parameter_list|(
 name|void
