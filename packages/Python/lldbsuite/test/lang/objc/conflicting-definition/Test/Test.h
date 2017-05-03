9e9fff1d90d29fb742666f1acbbb179af8bbdfce
Dimitry Andric <dim@FreeBSD.org>
2017-05-03 20:26:55 +0000

    Vendor import of lldb trunk r302069: https://llvm.org/svn/llvm-project/lldb/trunk@302069
    Former-commit-id: 52fd8de56a8a12201c1e6188e1f34d28c3d3398d

---
+++ packages/Python/lldbsuite/test/lang/objc/conflicting-definition/Test/Test.h
@@ -0,0 +1,35 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_empty
+empty|#import<Foundation/Foundation.h>
+end_empty
+
+begin_empty
+empty|#import<Test/Foo.h>
+end_empty
+
+begin_decl_stmt
+unit|@
+name|interface
+name|Test
+range|:
+name|NSObject
+block|{ @
+name|public
+name|CMTimeRange
+name|_range
+block|; }
+operator|-
+operator|(
+name|void
+operator|)
+name|doTest
+decl_stmt|;
+end_decl_stmt
+
+begin_macro
+unit|@
+name|end
+end_macro
+
+end_unit
+
