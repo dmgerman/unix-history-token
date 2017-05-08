718ad8d838fc7380c2e1b6d4edeed65802ea6dad
Ken Merry <ken@FreeBSD.org>
2017-05-08 14:48:39 +0000

    MFC r317745:
      Don't bother retrying errors for encrypted drives that are locked.
    
      sys/cam/scsi/scsi_all.c:
      	In the asc_table, if we get a 0x20,0x02 error ("Access denied -
      	no access rights"), don't bother retrying.  Instead, immediately
      	fail the command.
    
      	This is the error returned by Self Encrypting Drives (SED) when
      	they are locked.
    
    Sponsored by:	Spectra Logic
    
    
    Former-commit-id: 51f0f9e57902b8d4e5654585e74614d80d37aa1a

--- sys/cam/scsi/scsi_all.c
+++ sys/cam/scsi/scsi_all.c
@@ -8084,9 +8084,8 @@ literal|0x20
 argument_list|,
 literal|0x02
 argument_list|,
-argument|SS_RDEF
+argument|SS_FATAL | EPERM
 argument_list|,
-comment|/* XXX TBD */
 literal|"Access denied - no access rights"
 argument_list|)
 block|}
