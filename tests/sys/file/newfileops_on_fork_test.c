07bda6e2d11c5eb1a096c064c064776b9720159f
Garrett Cooper <ngie@FreeBSD.org>
2017-05-28 08:57:08 +0000

    Use main(void) instead of main(argc __unused, argv __unused)
    MFC after:	3 days
    Sponsored by:	Dell EMC Isilon
    
    
    Former-commit-id: f6e0a0ab1621a0b77e8a91d1bca4f7e7de7427b8

--- tests/sys/file/newfileops_on_fork_test.c
+++ tests/sys/file/newfileops_on_fork_test.c
@@ -243,15 +243,7 @@ begin_function
 name|int
 name|main
 parameter_list|(
-name|__unused
-name|int
-name|argc
-parameter_list|,
-name|__unused
-name|char
-modifier|*
-name|argv
-index|[]
+name|void
 parameter_list|)
 block|{
 name|struct
