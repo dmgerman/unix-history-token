c39f323ea95b951ca8dddf7a19bddccfca860a36
Cy Schubert <cy@FreeBSD.org>
2017-05-30 03:25:05 +0000

    MFC r318745:
    Remove redundant variable declaration.
    
    
    Former-commit-id: 7e9ef6553d689bbe3903574c49935eb636433c3d

--- sys/contrib/ipfilter/netinet/ip_nat.c
+++ sys/contrib/ipfilter/netinet/ip_nat.c
@@ -9041,14 +9041,6 @@ name|int
 name|getlock
 decl_stmt|;
 block|{
-ifdef|#
-directive|ifdef
-name|IPF_NAT6
-name|int
-name|i
-decl_stmt|;
-endif|#
-directive|endif
 if|if
 condition|(
 name|getlock
