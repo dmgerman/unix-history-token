562e74fe30a0c6115b0fd2847659f0e3b6833661
Cy Schubert <cy@FreeBSD.org>
2017-06-10 17:05:14 +0000

    Disable the -O (output fields) option in poollist() (ippool -l) for now. The option does not presently work. However, similar functions in ipfstat (for state) and ipnat (for nat) do work and provide outputs that can be easily parsed by shell scripts or subsequently loaded into CSV files.  The intention here is to return to this option to make it work. I suspect the problem is in printpoolfields.c.
    Former-commit-id: f56fdf8956ab7ebb01507632e342f702d33078ce

--- contrib/ipfilter/tools/ippool.c
+++ contrib/ipfilter/tools/ippool.c
@@ -609,7 +609,7 @@ name|fprintf
 argument_list|(
 name|stderr
 argument_list|,
-literal|"\t-l [-dv] [-m<name>] [-t<type>] [-O<fields>]\n"
+literal|"\t-l [-dv] [-m<name>] [-t<type>]\n"
 argument_list|)
 expr_stmt|;
 name|fprintf
@@ -3260,19 +3260,18 @@ literal|1
 return|;
 block|}
 break|break;
-case|case
-literal|'O'
-case|:
-name|pool_fields
-operator|=
-name|parsefields
-argument_list|(
-name|poolfields
-argument_list|,
-name|optarg
-argument_list|)
-expr_stmt|;
-break|break;
+if|#
+directive|if
+literal|0
+block|case 'O' :
+comment|/* XXX This option does not work. This function as  */
+comment|/* XXX used by state and nat can be used to format  */
+comment|/* XXX output especially useful for scripting. It   */
+comment|/* XXX is left here with the intention of making    */
+comment|/* XXX it work for the same purpose at some point.  */
+block|pool_fields = parsefields(poolfields, optarg); 			break;
+endif|#
+directive|endif
 case|case
 literal|'t'
 case|:
