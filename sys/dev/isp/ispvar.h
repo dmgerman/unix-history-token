40969e45c15d638858676d47b33b3af21dc9f0cb
Alexander Motin <mav@FreeBSD.org>
2017-07-03 15:56:45 +0000

    Switch fabric scans from GID_FT to GID_PT+GFF_ID/GFT_ID.
    Instead of using GID_FT SNS request to get list of registered FCP ports,
    use GID_PT to get list of all Nx_Ports, and then use GFF_ID and/or GFT_ID
    requests to find whether they are FCP and target capable.
    
    The problem with old approach is that GID_FT does not report ports without
    FC-4 type registered.  In particular it was impossible to boot OS from
    FreeBSD FC target using QLogic FC BIOS, since one does not register FC-4
    type even on new cards and so ignored by old code as incompatible.
    
    As a side bonus this allows initiator to skip pointless logins to other
    initiators by fetching that information from SNS instead.
    
    In case some switches do not implement GFF_ID/GFT_ID correctly, add sysctls
    to disable that functionality.  I handled broken GFF_ID of my Brocade 200E,
    but there may be other switches with different bugs.
    
    Linux also uses GID_PT, but GFF_ID is disabled by default there, and GFT_ID
    is not supported.
    
    Sponsored by:	iXsystems, Inc.
    
    
    Former-commit-id: 71928b099194f9828972d0459531acd9da79d080

--- sys/dev/isp/ispvar.h
+++ sys/dev/isp/ispvar.h
@@ -4,7 +4,7 @@ comment|/* $FreeBSD$ */
 end_comment
 
 begin_comment
-comment|/*-  *  Copyright (c) 1997-2009 by Matthew Jacob  *  All rights reserved.  *  *  Redistribution and use in source and binary forms, with or without  *  modification, are permitted provided that the following conditions  *  are met:  *  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  *  *  THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  *  ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE  *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  *  SUCH DAMAGE.  *  */
+comment|/*-  *  Copyright (c) 2009-2017 Alexander Motin<mav@FreeBSD.org>  *  Copyright (c) 1997-2009 by Matthew Jacob  *  All rights reserved.  *  *  Redistribution and use in source and binary forms, with or without  *  modification, are permitted provided that the following conditions  *  are met:  *  *  1. Redistributions of source code must retain the above copyright  *     notice, this list of conditions and the following disclaimer.  *  2. Redistributions in binary form must reproduce the above copyright  *     notice, this list of conditions and the following disclaimer in the  *     documentation and/or other materials provided with the distribution.  *  *  THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  *  ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE  *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  *  SUCH DAMAGE.  *  */
 end_comment
 
 begin_comment
@@ -1616,6 +1616,14 @@ decl_stmt|;
 name|uint8_t
 name|isp_retry_count
 decl_stmt|;
+name|int
+name|isp_use_gft_id
+decl_stmt|;
+comment|/* Use GFT_ID */
+name|int
+name|isp_use_gff_id
+decl_stmt|;
+comment|/* Use GFF_ID */
 comment|/* 	 * Current active WWNN/WWPN 	 */
 name|uint64_t
 name|isp_wwnn
