6b30fa89f31e2a48ded22d396e0ee05acb232a5d
Ed Maste <emaste@FreeBSD.org>
2017-07-06 18:30:52 +0000

    MF11 r320685: Update to ELF Tool Chain snapshot at r3561
    This update is primarily bug fixes in C++ symbol demangling, including:
    
    - rvalue reference
    - builtin type auto and decltype(auto)
    - revamped support for function return types
    - formatting fixes
    - omit void when its the only param
    - ref-qualifiers and others in function types
    - type qualifiers in pointer-to-member function types
    - incorrect handling regarding CV-qualifiers in function types
    - ref-qualifier found in nested-name
    - properly handle <name> ::= <substitute><template-args>
    - make sure that nested function name is not a substitute candidate
    - correctly handle expression in template args
    - skip unknown substitution abbreviations
    
    Also r320663 libelftc: bump version, tracking import in r320343
    
    Approved by:	re (gjb)
    Sponsored by:	The FreeBSD Foundation
    
    
    Former-commit-id: 08b8cee3068b2cd5e9ae26e6b34b15b3959720ab

--- contrib/elftoolchain/libelftc/_libelftc.h
+++ contrib/elftoolchain/libelftc/_libelftc.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2009 Kai Wang  * Copyright (c) 2007,2008 Hyogeol Lee<hyogeollee@gmail.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: _libelftc.h 3174 2015-03-27 17:13:41Z emaste $  */
+comment|/*-  * Copyright (c) 2009 Kai Wang  * Copyright (c) 2007,2008 Hyogeol Lee<hyogeollee@gmail.com>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer  *    in this position and unchanged.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  *  * $Id: _libelftc.h 3531 2017-06-05 05:08:43Z kaiwang27 $  */
 end_comment
 
 begin_ifndef
@@ -292,6 +292,20 @@ name|_str
 parameter_list|,
 name|size_t
 name|_len
+parameter_list|)
+function_decl|;
+name|bool
+name|vector_str_push_vector
+parameter_list|(
+name|struct
+name|vector_str
+modifier|*
+name|_dst
+parameter_list|,
+name|struct
+name|vector_str
+modifier|*
+name|_org
 parameter_list|)
 function_decl|;
 name|bool
