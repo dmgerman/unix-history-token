3b4a2312a2125e01faaf317bc30a06b73a0865ea
Konstantin Belousov <kib@FreeBSD.org>
2017-07-10 06:08:44 +0000

    MFC r320619: Resolve confusion between different error code spaces.
    Former-commit-id: a2529fe4a0f5558920f3f8b9705c50d9607f577d

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -4624,7 +4624,7 @@ block|}
 end_function
 
 begin_comment
-comment|/*  * Destroy old address space, and allocate a new stack  *	The new stack is only SGROWSIZ large because it is grown  *	automatically in trap.c.  */
+comment|/*  * Destroy old address space, and allocate a new stack.  *	The new stack is only sgrowsiz large because it is grown  *	automatically on a page fault.  */
 end_comment
 
 begin_function
@@ -4882,6 +4882,8 @@ expr_stmt|;
 if|if
 condition|(
 name|error
+operator|!=
+name|KERN_SUCCESS
 condition|)
 block|{
 name|vm_object_deallocate
@@ -4891,7 +4893,10 @@ argument_list|)
 expr_stmt|;
 return|return
 operator|(
+name|vm_mmap_to_errno
+argument_list|(
 name|error
+argument_list|)
 operator|)
 return|;
 block|}
@@ -5048,10 +5053,15 @@ expr_stmt|;
 if|if
 condition|(
 name|error
+operator|!=
+name|KERN_SUCCESS
 condition|)
 return|return
 operator|(
+name|vm_mmap_to_errno
+argument_list|(
 name|error
+argument_list|)
 operator|)
 return|;
 ifdef|#
