2a80aa37b701646b24164d473a0755e4e953979e
Konstantin Belousov <kib@FreeBSD.org>
2017-07-11 12:35:44 +0000

    Fix BIT_FLS().
    The iteration index is unsigned, so testing for larger than or equal
    to zero makes little sense.
    
    Submitted by:	Sebastian Huber <sebastian.huber@embedded-brains.de>
    MFC after:	3 days
    
    
    Former-commit-id: f00fc07fafdc6748c5cf60c2c746669e932dd7e8

--- sys/sys/bitset.h
+++ sys/sys/bitset.h
@@ -456,7 +456,7 @@ name|_s
 parameter_list|,
 name|p
 parameter_list|)
-value|__extension__ ({					\ 	__size_t __i;							\ 	int __bit;							\ 									\ 	__bit = 0;							\ 	for (__i = __bitset_words((_s)) - 1; __i>= 0; __i--) {		\ 		if ((p)->__bits[__i] != 0) {				\ 			__bit = flsl((p)->__bits[__i]);			\ 			__bit += __i * _BITSET_BITS;			\ 			break;						\ 		}							\ 	}								\ 	__bit;								\ })
+value|__extension__ ({					\ 	__size_t __i;							\ 	int __bit;							\ 									\ 	__bit = 0;							\ 	for (__i = __bitset_words((_s)); __i> 0; __i--) {		\ 		if ((p)->__bits[__i - 1] != 0) {			\ 			__bit = flsl((p)->__bits[__i - 1]);		\ 			__bit += (__i - 1) * _BITSET_BITS;		\ 			break;						\ 		}							\ 	}								\ 	__bit;								\ })
 end_define
 
 begin_define
