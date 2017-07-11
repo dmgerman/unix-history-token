dbd110b8281fc0c161c43b1971a8568b8d2a4892
Eric Joyner <erj@FreeBSD.org>
2017-07-11 21:25:07 +0000

    MFC r316541, r316544, and r320688 from HEAD to stable/11.
    r316541:
    Fix a double free in ixgbe_rxeof()
    
    r316544:
    Don't overwrite vf->flags variable at the end of ixgbe(4) ixgbe_add_vf()
    
    r320688:
    ixgbe(4): Update HEAD (p3) to 3.2.12-k
    
    Includes:
    
    - Support for X550EM devices.
    - Support for Bypass adapters.
    - Flow Director code moved to separate files
    - SR-IOV code moved to separate files
    - Netmap code moved to separate files
    
    Sponsored by:	Intel Corporation
    
    
    Former-commit-id: 830a553c090a7d62c3d26229503953f27aa25af8

--- sys/dev/ixgbe/ixgbe_82598.c
+++ sys/dev/ixgbe/ixgbe_82598.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************    Copyright (c) 2001-2015, Intel Corporation    All rights reserved.      Redistribution and use in source and binary forms, with or without    modification, are permitted provided that the following conditions are met:       1. Redistributions of source code must retain the above copyright notice,        this list of conditions and the following disclaimer.       2. Redistributions in binary form must reproduce the above copyright        notice, this list of conditions and the following disclaimer in the        documentation and/or other materials provided with the distribution.       3. Neither the name of the Intel Corporation nor the names of its        contributors may be used to endorse or promote products derived from        this software without specific prior written permission.      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   POSSIBILITY OF SUCH DAMAGE.  ******************************************************************************/
+comment|/******************************************************************************    Copyright (c) 2001-2017, Intel Corporation   All rights reserved.    Redistribution and use in source and binary forms, with or without   modification, are permitted provided that the following conditions are met:     1. Redistributions of source code must retain the above copyright notice,       this list of conditions and the following disclaimer.     2. Redistributions in binary form must reproduce the above copyright       notice, this list of conditions and the following disclaimer in the       documentation and/or other materials provided with the distribution.     3. Neither the name of the Intel Corporation nor the names of its       contributors may be used to endorse or promote products derived from       this software without specific prior written permission.    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   POSSIBILITY OF SUCH DAMAGE.  ******************************************************************************/
 end_comment
 
 begin_comment
@@ -3585,7 +3585,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  *  ixgbe_set_vfta_82598 - Set VLAN filter table  *  @hw: pointer to hardware structure  *  @vlan: VLAN id to write to VLAN filter  *  @vind: VMDq output index that maps queue to VLAN id in VFTA  *  @vlan_on: boolean flag to turn on/off VLAN in VFTA  *  *  Turn on/off specified VLAN in the VLAN filter table.  **/
+comment|/**  *  ixgbe_set_vfta_82598 - Set VLAN filter table  *  @hw: pointer to hardware structure  *  @vlan: VLAN id to write to VLAN filter  *  @vind: VMDq output index that maps queue to VLAN id in VFTA  *  @vlan_on: boolean flag to turn on/off VLAN in VFTA  *  @vlvf_bypass: boolean flag - unused  *  *  Turn on/off specified VLAN in the VLAN filter table.  **/
 end_comment
 
 begin_function
@@ -3605,6 +3605,9 @@ name|vind
 parameter_list|,
 name|bool
 name|vlan_on
+parameter_list|,
+name|bool
+name|vlvf_bypass
 parameter_list|)
 block|{
 name|u32
@@ -3619,6 +3622,11 @@ decl_stmt|;
 name|u32
 name|vftabyte
 decl_stmt|;
+name|UNREFERENCED_1PARAMETER
+argument_list|(
+name|vlvf_bypass
+argument_list|)
+expr_stmt|;
 name|DEBUGFUNC
 argument_list|(
 literal|"ixgbe_set_vfta_82598"
@@ -4378,7 +4386,7 @@ comment|/**  *  ixgbe_get_supported_physical_layer_82598 - Returns physical laye
 end_comment
 
 begin_function
-name|u32
+name|u64
 name|ixgbe_get_supported_physical_layer_82598
 parameter_list|(
 name|struct
@@ -4387,7 +4395,7 @@ modifier|*
 name|hw
 parameter_list|)
 block|{
-name|u32
+name|u64
 name|physical_layer
 init|=
 name|IXGBE_PHYSICAL_LAYER_UNKNOWN
--- sys/dev/ixgbe/ixgbe_dcb.c
+++ sys/dev/ixgbe/ixgbe_dcb.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/******************************************************************************    Copyright (c) 2001-2015, Intel Corporation    All rights reserved.      Redistribution and use in source and binary forms, with or without    modification, are permitted provided that the following conditions are met:       1. Redistributions of source code must retain the above copyright notice,        this list of conditions and the following disclaimer.       2. Redistributions in binary form must reproduce the above copyright        notice, this list of conditions and the following disclaimer in the        documentation and/or other materials provided with the distribution.       3. Neither the name of the Intel Corporation nor the names of its        contributors may be used to endorse or promote products derived from        this software without specific prior written permission.      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   POSSIBILITY OF SUCH DAMAGE.  ******************************************************************************/
+comment|/******************************************************************************    Copyright (c) 2001-2017, Intel Corporation   All rights reserved.    Redistribution and use in source and binary forms, with or without   modification, are permitted provided that the following conditions are met:     1. Redistributions of source code must retain the above copyright notice,       this list of conditions and the following disclaimer.     2. Redistributions in binary form must reproduce the above copyright       notice, this list of conditions and the following disclaimer in the       documentation and/or other materials provided with the distribution.     3. Neither the name of the Intel Corporation nor the names of its       contributors may be used to endorse or promote products derived from       this software without specific prior written permission.    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   POSSIBILITY OF SUCH DAMAGE.  ******************************************************************************/
 end_comment
 
 begin_comment
@@ -1500,6 +1500,9 @@ case|:
 case|case
 name|ixgbe_mac_X550EM_x
 case|:
+case|case
+name|ixgbe_mac_X550EM_a
+case|:
 if|#
 directive|if
 operator|!
@@ -1598,6 +1601,9 @@ name|ixgbe_mac_X550
 case|:
 case|case
 name|ixgbe_mac_X550EM_x
+case|:
+case|case
+name|ixgbe_mac_X550EM_a
 case|:
 if|#
 directive|if
@@ -1790,6 +1796,9 @@ case|:
 case|case
 name|ixgbe_mac_X550EM_x
 case|:
+case|case
+name|ixgbe_mac_X550EM_a
+case|:
 if|#
 directive|if
 operator|!
@@ -1953,6 +1962,9 @@ name|ixgbe_mac_X550
 case|:
 case|case
 name|ixgbe_mac_X550EM_x
+case|:
+case|case
+name|ixgbe_mac_X550EM_a
 case|:
 if|#
 directive|if
@@ -2135,6 +2147,9 @@ case|:
 case|case
 name|ixgbe_mac_X550EM_x
 case|:
+case|case
+name|ixgbe_mac_X550EM_a
+case|:
 if|#
 directive|if
 operator|!
@@ -2267,6 +2282,9 @@ case|:
 case|case
 name|ixgbe_mac_X550EM_x
 case|:
+case|case
+name|ixgbe_mac_X550EM_a
+case|:
 if|#
 directive|if
 operator|!
@@ -2353,6 +2371,9 @@ name|ixgbe_mac_X550
 case|:
 case|case
 name|ixgbe_mac_X550EM_x
+case|:
+case|case
+name|ixgbe_mac_X550EM_a
 case|:
 if|#
 directive|if
@@ -2560,6 +2581,9 @@ case|:
 case|case
 name|ixgbe_mac_X550EM_x
 case|:
+case|case
+name|ixgbe_mac_X550EM_a
+case|:
 if|#
 directive|if
 operator|!
@@ -2712,6 +2736,9 @@ case|:
 case|case
 name|ixgbe_mac_X550EM_x
 case|:
+case|case
+name|ixgbe_mac_X550EM_a
+case|:
 if|#
 directive|if
 operator|!
@@ -2839,6 +2866,9 @@ name|ixgbe_mac_X550
 case|:
 case|case
 name|ixgbe_mac_X550EM_x
+case|:
+case|case
+name|ixgbe_mac_X550EM_a
 case|:
 if|#
 directive|if
