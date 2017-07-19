d17c965396b5b09fc3ce8f23fad18a0a54160446
Garrett Cooper <ngie@FreeBSD.org>
2017-07-19 19:06:19 +0000

    MFC r316076:
    sys/boot/common: Make geli(4) support optional in MI sources
    
    This saves a negligible amount of memory for non-geli enabled
    bootloaders.
    
    
    Former-commit-id: a7f15d569b7e144435f9e40d6a4ba1025bcfba34

--- sys/boot/common/module.c
+++ sys/boot/common/module.c
@@ -777,6 +777,12 @@ return|;
 block|}
 end_function
 
+begin_ifdef
+ifdef|#
+directive|ifdef
+name|LOADER_GELI_SUPPORT
+end_ifdef
+
 begin_expr_stmt
 name|COMMAND_SET
 argument_list|(
@@ -974,6 +980,11 @@ operator|)
 return|;
 block|}
 end_function
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_function
 name|void
