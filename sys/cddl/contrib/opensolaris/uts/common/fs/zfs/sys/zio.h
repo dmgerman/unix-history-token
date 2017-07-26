e081b3fe02b195a40cbcb4b91356a5c6763f66c0
Alexander Motin <mav@FreeBSD.org>
2017-07-26 16:45:39 +0000

    MFC r318828: MFV r316917: 7968 multi-threaded spa_sync()
    illumos/illumos-gate@94c2d0eb22e9624151ee84a7edbf7178e1bf4087
    https://github.com/illumos/illumos-gate/commit/94c2d0eb22e9624151ee84a7edbf7178e1bf4087
    
    https://www.illumos.org/issues/7968
      spa_sync() iterates over all the dirty dnodes and processes each of them by
      calling dnode_sync(). If there are many dirty dnodes (e.g. because we created
      or removed a lot of files), the single thread of spa_sync() calling
      dnode_sync() can become a bottleneck. Additionally, if many dnodes are dirtied
      concurrently in open context (e.g. due to concurrent file creation), the
      os_lock will experience lock contention via dnode_setdirty().
      The solution is to track dirty dnodes on a multilist_t, and for spa_sync() to
      use separate threads to process each of the sublists in the multilist.
      On the concurrent file creation microbenchmark, the performance improvement
      from dnode_setdirty() is up to 7%. Additionally, the wall clock time spent in
      spa_sync() is reduced to 15%-40% of the single-threaded case. In terms of cost/
      reward, once the other bottlenecks are addressed, fixing this bug will provide
      a medium-large performance gain and require a medium amount of effort to
      implement.
    
    Reviewed by: Pavel Zakharov <pavel.zakharov@delphix.com>
    Reviewed by: Brad Lewis <brad.lewis@delphix.com>
    Reviewed by: Saso Kiselkov <saso.kiselkov@nexenta.com>
    Reviewed by: Brian Behlendorf <behlendorf1@llnl.gov>
    Approved by: Dan McDonald <danmcd@omniti.com>
    Author: Matthew Ahrens <mahrens@delphix.com>
    
    Former-commit-id: 5622f8fdaf2bf9b0f92b605e67de5ad1d35a5e3f

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/sys/zio.h
@@ -4,7 +4,7 @@ comment|/*  * CDDL HEADER START  *  * The contents of this file are subject to t
 end_comment
 
 begin_comment
-comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.  * Copyright (c) 2012, 2016 by Delphix. All rights reserved.  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.  */
+comment|/*  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.  * Copyright 2011 Nexenta Systems, Inc.  All rights reserved.  * Copyright (c) 2012, 2017 by Delphix. All rights reserved.  * Copyright (c) 2013 by Saso Kiselkov. All rights reserved.  */
 end_comment
 
 begin_ifndef
@@ -1207,7 +1207,7 @@ block|}
 struct|;
 specifier|extern
 name|int
-name|zio_timestamp_compare
+name|zio_bookmark_compare
 parameter_list|(
 specifier|const
 name|void
