c72beea85a0049928d185fbe3b473c7214698272
Bjoern A. Zeeb <bz@FreeBSD.org>
2017-07-27 13:03:36 +0000

    After inpcb route caching was put back in place there is no need for flowtable anymore (as flowtable was never considered to be useful in the forwarding path).
    Reviewed by:		np
    Differential Revision:	https://reviews.freebsd.org/D11448
    
    
    Former-commit-id: 2b0d850d0d704a9d3b5f710299ae6b299f152b5c

--- usr.bin/netstat/netstat.h
+++ usr.bin/netstat/netstat.h
@@ -1083,15 +1083,6 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
-name|void
-name|flowtable_stats
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
 name|char
 modifier|*
 name|routename
