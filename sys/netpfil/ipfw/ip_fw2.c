00e872af1438fc550114fcb3c7c334e0a8a2d6c8
Philip Paeps <philip@FreeBSD.org>
2017-08-01 13:40:37 +0000

    MFC r320941: Fix GRE over IPv6 tunnels with IPFW
    Previously, GRE packets in IPv6 tunnels would be dropped by IPFW (unless
    net.inet6.ip6.fw.deny_unknown_exthdrs was unset).
    
    PR:		220640
    Submitted by:	Kun Xie <kxie@xiplink.com>
    
    Former-commit-id: dc4046e0c3c95a03bf3fb52b99577b577432016f

--- sys/netpfil/ipfw/ip_fw2.c
+++ sys/netpfil/ipfw/ip_fw2.c
@@ -342,6 +342,16 @@ end_endif
 begin_include
 include|#
 directive|include
+file|<net/if_gre.h>
+end_include
+
+begin_comment
+comment|/* for struct gre_h */
+end_comment
+
+begin_include
+include|#
+directive|include
 file|<netpfil/ipfw/ip_fw_private.h>
 end_include
 
@@ -4766,6 +4776,22 @@ name|ulp
 argument_list|,
 expr|struct
 name|pim
+argument_list|)
+expr_stmt|;
+break|break;
+case|case
+name|IPPROTO_GRE
+case|:
+comment|/* RFC 1701 */
+comment|/* XXX GRE header check? */
+name|PULLUP_TO
+argument_list|(
+name|hlen
+argument_list|,
+name|ulp
+argument_list|,
+expr|struct
+name|gre_h
 argument_list|)
 expr_stmt|;
 break|break;
