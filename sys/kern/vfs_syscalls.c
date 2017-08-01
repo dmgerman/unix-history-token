c9f4555f54296a325959a1a974076a81c32276fc
Dmitry Chagin <dchagin@FreeBSD.org>
2017-08-01 03:40:19 +0000

    Implement proper Linux /dev/fd and /proc/self/fd behavior by adding Linux specific things to the native fdescfs file system.
    Unlike FreeBSD, the Linux fdescfs is a directory containing a symbolic
    links to the actual files, which the process has open.
    A readlink(2) call on this file returns a full path in case of regular file
    or a string in a special format (type:[inode], anon_inode:<file-type>, etc..).
    As well as in a FreeBSD, opening the file in the Linux fdescfs directory is
    equivalent to duplicating the corresponding file descriptor.
    
    Here we have mutually exclusive requirements:
    - in case of readlink(2) call fdescfs lookup() method should return VLNK
    vnode otherwise our kern_readlink() fail with EINVAL error;
    - in the other calls fdescfs lookup() method should return non VLNK vnode.
    
    For what new vnode v_flag VV_READLINK was added, which is set if fdescfs has beed
    mounted with linrdlnk option an modified kern_readlinkat() to properly handle it.
    
    For now For Linux ABI compatibility mount fdescfs volume with linrdlnk option:
    
        mount -t fdescfs -o linrdlnk null /compat/linux/dev/fd
    
    Reviewed by:	kib@
    MFC after:	1 week
    Relnotes:	yes
    
    Former-commit-id: 905bd6347371cc093f3c3557b1a6d77a9a06fb43

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -13124,6 +13124,16 @@ operator|->
 name|v_type
 operator|!=
 name|VLNK
+operator|&&
+operator|(
+name|vp
+operator|->
+name|v_vflag
+operator|&
+name|VV_READLINK
+operator|)
+operator|==
+literal|0
 condition|)
 name|error
 operator|=
