9d600d9e95905eec64a835ecb0bdb9b89ee1450d
Alexander Motin <mav@FreeBSD.org>
2017-08-05 08:36:04 +0000

    MFC r321921: Add compat shim part missed at r305197.
    This fixes compatibility between old kernel and new ZFS tools.
    It seems to be tradition to forget it.
    
    PR:		221112
    
    Former-commit-id: af03b9e32a0181d5dff8efcd20d4e15d3982d838

--- cddl/contrib/opensolaris/lib/libzfs/common/libzfs_compat.c
+++ cddl/contrib/opensolaris/lib/libzfs/common/libzfs_compat.c
@@ -183,6 +183,14 @@ name|zfs_ioctl_version
 condition|)
 block|{
 case|case
+name|ZFS_IOCVER_INLANES
+case|:
+name|cflag
+operator|=
+name|ZFS_CMD_COMPAT_INLANES
+expr_stmt|;
+break|break;
+case|case
 name|ZFS_IOCVER_RESUME
 case|:
 name|cflag
