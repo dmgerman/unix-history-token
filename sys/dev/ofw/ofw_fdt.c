bf22588073f6f89af6ba7843addea82c3b32e88b
mw <mw@FreeBSD.org>
2017-08-09 00:56:29 +0000

    Add support for "compatible" parameter in ofw_fdt_fixup
    Sometimes it's convenient to provide fixup to many boards
    that use the same SoC family (eg. Marvell Armada 38x).
    Instead of putting multiple entries in fdt_fixup_table,
    use one entry which refers to all boards with given SoC.
    
    Submitted by: Patryk Duda <pdk@semihalf.com>
    Reviewed by: nwhitehorn, cognet (mentor)
    Approved by: cognet (mentor)
    Obtained from: Semihalf
    Differential Revision: https://reviews.freebsd.org/D11878
    
    
    Former-commit-id: 0bd5dc8d2c9e5cf87cb6fc9d476c7f75b087cca4

--- sys/dev/ofw/ofw_fdt.c
+++ sys/dev/ofw/ofw_fdt.c
@@ -74,6 +74,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<dev/ofw/ofw_bus_subr.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|"ofw_if.h"
 end_include
 
@@ -2207,6 +2213,22 @@ name|FDT_MODEL_LEN
 argument_list|)
 operator|!=
 literal|0
+condition|)
+comment|/* 			 * Sometimes it's convenient to provide one 			 * fixup entry that refers to many boards. 			 * To handle this case, simply check if model 			 * is compatible parameter 			 */
+if|if
+condition|(
+operator|!
+name|ofw_bus_node_is_compatible
+argument_list|(
+name|root
+argument_list|,
+name|fdt_fixup_table
+index|[
+name|i
+index|]
+operator|.
+name|model
+argument_list|)
 condition|)
 continue|continue;
 if|if
