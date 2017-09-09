c87d15f6b243f8de47c21a68d3171b93b8a456d0
Konstantin Belousov <kib@FreeBSD.org>
2017-09-09 17:35:19 +0000

    Add a vm_page_change_lock() helper, the common code to not relock page lock if both old and new pages use the same underlying lock.  Convert existing places to use the helper instead of inlining it.  Use the optimization in vm_object_page_remove().
    Suggested and reviewed by:	alc, markj
    Sponsored by:	The FreeBSD Foundation
    MFC after:	1 week
    
    
    Former-commit-id: 69e48439ccd2fa524773e31f46082281bbd5c7ec

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -1609,6 +1609,22 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|void
+name|vm_page_change_lock
+parameter_list|(
+name|vm_page_t
+name|m
+parameter_list|,
+name|struct
+name|mtx
+modifier|*
+modifier|*
+name|mtx
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|vm_page_t
 name|vm_page_grab
 parameter_list|(
