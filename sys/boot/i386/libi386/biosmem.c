656fe251b621eb14515738ee10db1b35c119a94a
tsoome <tsoome@FreeBSD.org>
2017-09-14 16:42:29 +0000

    loader: biosmem.c cstyle cleanup
    No functional changes, just cleanup.
    
    Reviewed by:	allanjude, imp
    Differential Revision:	https://reviews.freebsd.org/D12370
    
    
    Former-commit-id: 78f2a4519883e6caa0b13c23b525b1009d8588ca

--- sys/boot/i386/libi386/biosmem.c
+++ sys/boot/i386/libi386/biosmem.c
@@ -146,16 +146,16 @@ begin_comment
 comment|/*  * Products in this list need quirks to detect  * memory correctly. You need both maker and product as  * reported by smbios.  */
 end_comment
 
+begin_comment
+comment|/* e820 might not return useful extended memory */
+end_comment
+
 begin_define
 define|#
 directive|define
 name|BQ_DISTRUST_E820_EXTMEM
 value|0x1
 end_define
-
-begin_comment
-comment|/* e820 might not return useful 					   extended memory */
-end_comment
 
 begin_struct
 struct|struct
@@ -243,6 +243,7 @@ condition|;
 operator|++
 name|i
 control|)
+block|{
 if|if
 condition|(
 name|smbios_match
@@ -279,6 +280,7 @@ operator|.
 name|quirk
 operator|)
 return|;
+block|}
 return|return
 operator|(
 literal|0
@@ -318,7 +320,7 @@ name|addr
 operator|=
 literal|0x15
 expr_stmt|;
-comment|/* int 0x15 function 0xe820*/
+comment|/* int 0x15 function 0xe820 */
 name|v86
 operator|.
 name|eax
@@ -466,7 +468,7 @@ operator||=
 name|B_EXTMEM_E820
 expr_stmt|;
 block|}
-comment|/* 	 * Look for the largest segment in 'extended' memory beyond 	 * 1MB but below 4GB. 	 */
+comment|/* 		 * Look for the largest segment in 'extended' memory beyond 		 * 1MB but below 4GB. 		 */
 if|if
 condition|(
 operator|(
@@ -500,7 +502,7 @@ name|smap
 operator|.
 name|length
 expr_stmt|;
-comment|/* 	     * If this segment crosses the 4GB boundary, truncate it. 	     */
+comment|/* 			 * If this segment crosses the 4GB boundary, 			 * truncate it. 			 */
 if|if
 condition|(
 name|smap
@@ -589,7 +591,7 @@ operator||=
 name|B_BASEMEM_12
 expr_stmt|;
 block|}
-comment|/* Fall back through several compatibility functions for extended memory */
+comment|/* 	 * Fall back through several compatibility functions for extended 	 * memory. 	 */
 if|if
 condition|(
 name|bios_extmem
@@ -609,7 +611,7 @@ name|addr
 operator|=
 literal|0x15
 expr_stmt|;
-comment|/* int 0x15 function 0xe801*/
+comment|/* int 0x15 function 0xe801 */
 name|v86
 operator|.
 name|eax
@@ -632,7 +634,7 @@ argument_list|)
 operator|)
 condition|)
 block|{
-comment|/* 	     * Clear high_heap; it may end up overlapping 	     * with the segment we're determining here. 	     * Let the default "steal stuff from top of 	     * bios_extmem" code below pick up on it. 	     */
+comment|/* 			 * Clear high_heap; it may end up overlapping 			 * with the segment we're determining here. 			 * Let the default "steal stuff from top of 			 * bios_extmem" code below pick up on it. 			 */
 name|high_heap_size
 operator|=
 literal|0
@@ -641,7 +643,7 @@ name|high_heap_base
 operator|=
 literal|0
 expr_stmt|;
-comment|/* 	     * %cx is the number of 1KiB blocks between 1..16MiB. 	     * It can only be up to 0x3c00; if it's smaller then 	     * there's a PC AT memory hole so we can't treat 	     * it as contiguous. 	     */
+comment|/* 			 * %cx is the number of 1KiB blocks between 1..16MiB. 			 * It can only be up to 0x3c00; if it's smaller then 			 * there's a PC AT memory hole so we can't treat 			 * it as contiguous. 			 */
 name|bios_extmem
 operator|=
 operator|(
@@ -714,7 +716,7 @@ name|addr
 operator|=
 literal|0x15
 expr_stmt|;
-comment|/* int 0x15 function 0x88*/
+comment|/* int 0x15 function 0x88 */
 name|v86
 operator|.
 name|eax
@@ -750,7 +752,7 @@ literal|0x100000
 operator|+
 name|bios_extmem
 expr_stmt|;
-comment|/*      * If we have extended memory and did not find a suitable heap      * region in the SMAP, use the last HEAP_MIN of 'extended' memory as a      * high heap candidate.      */
+comment|/* 	 * If we have extended memory and did not find a suitable heap 	 * region in the SMAP, use the last HEAP_MIN of 'extended' memory as a 	 * high heap candidate. 	 */
 if|if
 condition|(
 name|bios_extmem
