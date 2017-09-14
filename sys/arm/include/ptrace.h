19870cb531bd50449577836db8aca965bcb17409
John Baldwin <jhb@FreeBSD.org>
2017-09-14 15:03:43 +0000

    Add ptrace operations to fetch and store VFP registers.
    Reviewed by:	mmel, kib
    MFC after:	1 month
    Differential Revision:	https://reviews.freebsd.org/D12294
    
    
    Former-commit-id: 89c4fa41e6db99bc8c1c0f8245e73ed2d4c7c0f6

--- sys/arm/include/ptrace.h
+++ sys/arm/include/ptrace.h
@@ -19,6 +19,50 @@ directive|define
 name|_MACHINE_PTRACE_H_
 end_define
 
+begin_define
+define|#
+directive|define
+name|__HAVE_PTRACE_MACHDEP
+end_define
+
+begin_comment
+comment|/*  * Must match mcontext_vfp_t.  Note that mcontext_vfp_t does not  * include explicit padding.  */
+end_comment
+
+begin_struct
+struct|struct
+name|vfpreg
+block|{
+name|__uint64_t
+name|vfp_reg
+index|[
+literal|32
+index|]
+decl_stmt|;
+name|__uint32_t
+name|vfp_scr
+decl_stmt|;
+name|__uint32_t
+name|vfp_pad0
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_define
+define|#
+directive|define
+name|PT_GETVFPREGS
+value|(PT_FIRSTMACH + 0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PT_SETVFPREGS
+value|(PT_FIRSTMACH + 1)
+end_define
+
 begin_endif
 endif|#
 directive|endif
