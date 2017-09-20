3c719aa60e859445f0109f92d0acea6681b9f97b
Konstantin Belousov <kib@FreeBSD.org>
2017-09-20 08:51:03 +0000

    MFC r323558: Use existing tag name for the vm_object' memq.
    Former-commit-id: 51e52aaa8c9055553e93fba1d4bb0e295830a20e

--- sys/vm/vm_page.h
+++ sys/vm/vm_page.h
@@ -426,6 +426,12 @@ name|PQ_COUNT
 value|3
 end_define
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|VM_PAGE_HAVE_PGLIST
+end_ifndef
+
 begin_expr_stmt
 name|TAILQ_HEAD
 argument_list|(
@@ -435,6 +441,17 @@ name|vm_page
 argument_list|)
 expr_stmt|;
 end_expr_stmt
+
+begin_define
+define|#
+directive|define
+name|VM_PAGE_HAVE_PGLIST
+end_define
+
+begin_endif
+endif|#
+directive|endif
+end_endif
 
 begin_expr_stmt
 name|SLIST_HEAD
