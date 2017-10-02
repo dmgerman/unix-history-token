569e99d19f574bea1797366190c0735d21d82cda
Ian Lepore <ian@FreeBSD.org>
2017-10-02 01:03:18 +0000

    Enhance the interrupt capabilities of ti_pruss driver.
    The existing ti_pruss driver for the PRUSS Hardware provided by the AM335x
    ARM CPU has basic interrupt capabilities.  This updated driver provides some
    more options:
    
     - Sysctl based configuration for the interrupts (for some examples, see the
       test plan in the phabricator review cited below).
    
     - A device file (/dev/pruss0.irqN) for each enabled interrupt. This file
       can be read and the device blocks if no irq has happened or returns an
       uint64_t timestamp based on nanouptime().
    
     - Each interrupt device file provides kqueue-based event notification,
       blocking read(), or select().
    
    Submitted by:	Manuel Stuhn <freebsdnewbie@freenet.de>
    Differential Revision:	https://reviews.freebsd.org/D11959
    
    
    Former-commit-id: ad533b26af714ce444784d525a13ce8aae0ee7ed

--- sys/arm/ti/ti_pruss.h
+++ sys/arm/ti/ti_pruss.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2013 Rui Paulo<rpaulo@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2013 Rui Paulo<rpaulo@FreeBSD.org>  * Copyright (c) 2017 Manuel Stuehn  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  * POSSIBILITY OF SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -46,22 +46,120 @@ end_define
 begin_define
 define|#
 directive|define
-name|PRUSS_INTC_HIER
-value|0x1500
+name|PRUSS_INTC_GER
+value|(PRUSS_AM33XX_INTC + 0x0010)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SISR
+value|(PRUSS_AM33XX_INTC + 0x0020)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SICR
+value|(PRUSS_AM33XX_INTC + 0x0024)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_EISR
+value|(PRUSS_AM33XX_INTC + 0x0028)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_EICR
+value|(PRUSS_AM33XX_INTC + 0x002C)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_HIEISR
+value|(PRUSS_AM33XX_INTC + 0x0034)
 end_define
 
 begin_define
 define|#
 directive|define
 name|PRUSS_INTC_HIDISR
-value|0x0038
+value|(PRUSS_AM33XX_INTC + 0x0038)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SECR0
+value|(PRUSS_AM33XX_INTC + 0x0280)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SECR1
+value|(PRUSS_AM33XX_INTC + 0x0284)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_CMR_BASE
+value|(PRUSS_AM33XX_INTC + 0x0400)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_HMR_BASE
+value|(PRUSS_AM33XX_INTC + 0x0800)
 end_define
 
 begin_define
 define|#
 directive|define
 name|PRUSS_INTC_HIPIR_BASE
-value|0x0900
+value|(PRUSS_AM33XX_INTC + 0x0900)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SIPR0
+value|(PRUSS_AM33XX_INTC + 0x0D00)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SIPR1
+value|(PRUSS_AM33XX_INTC + 0x0D04)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SITR0
+value|(PRUSS_AM33XX_INTC + 0x0D80)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_SITR1
+value|(PRUSS_AM33XX_INTC + 0x0D84)
+end_define
+
+begin_define
+define|#
+directive|define
+name|PRUSS_INTC_HIER
+value|(PRUSS_AM33XX_INTC + 0x1500)
 end_define
 
 begin_endif
