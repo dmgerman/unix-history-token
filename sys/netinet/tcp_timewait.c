8411b8344be80dbb5143353de3021b35ffc80be6
Julien Charbon <jch@FreeBSD.org>
2017-10-02 09:45:17 +0000

    Forgotten bits in r324179:  Include sys/syslog.h if INVARIANTS is not defined
    MFC after:	1 week
    X-MFC with:	r324179
    Pointy hat to:	jch
    
    
    Former-commit-id: 92edbb7464f72366cb0858790cb60dad2fbd8c1c

--- sys/netinet/tcp_timewait.c
+++ sys/netinet/tcp_timewait.c
@@ -101,6 +101,23 @@ directive|include
 file|<sys/socketvar.h>
 end_include
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|INVARIANTS
+end_ifndef
+
+begin_include
+include|#
+directive|include
+file|<sys/syslog.h>
+end_include
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
 begin_include
 include|#
 directive|include
