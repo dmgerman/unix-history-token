9b691400ef54fa3b3a4679684bd883a1ab3b788b
Conrad E. Meyer <cem@FreeBSD.org>
2017-10-07 18:59:29 +0000

    random(4): Add missing source descriptions
    Add source descriptions missed in r260847, r303035.
    
    While here, convert the array to C99 initializers.
    
    Reviewed by:	delphij
    Approved by:	secteam (delphij)
    Sponsored by:	Dell EMC Isilon
    Differential Revision:	https://reviews.freebsd.org/D12618
    
    
    Former-commit-id: 1d443c7114288aa3878f100d1d5f0eacefaf3128

--- sys/sys/random.h
+++ sys/sys/random.h
@@ -166,7 +166,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  * Note: if you add or remove members of random_entropy_source, remember to also update the  * KASSERT regarding what valid members are in random_harvest_internal(), and remember the  * strings in the static array random_source_descr[] in random_harvestq.c.  *  * NOTE: complain loudly to markm@ or on the lists if this enum gets more than 32  * distinct values (0-31)! ENTROPYSOURCE may be == 32, but not> 32.  */
+comment|/*  * Note: if you add or remove members of random_entropy_source, remember to  * also update the strings in the static array random_source_descr[] in  * random_harvestq.c.  *  * NOTE: complain loudly to markm@ or on the lists if this enum gets more than 32  * distinct values (0-31)! ENTROPYSOURCE may be == 32, but not> 32.  */
 end_comment
 
 begin_enum
