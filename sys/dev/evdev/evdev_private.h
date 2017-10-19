9265c032f1a585d1be34ee0e2cc145106857c4c0
wulf <wulf@FreeBSD.org>
2017-10-19 20:16:40 +0000

    MFC r321397-r321399
    r321397:
    evdev: style(9), sort headers alphabetically
    
    r321398:
    evdev: Macroize common locking sequences
    
    r321399:
    Change my email address to wulf@FreeBSD.org in copyright headers.
    
    Approved by:	gonzo (mentor)
    
    
    Former-commit-id: 9c615dbf1da107ad03ddd2fa280616977ba39c24

--- sys/dev/evdev/evdev_private.h
+++ sys/dev/evdev/evdev_private.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*-  * Copyright (c) 2014 Jakub Wojciech Klama<jceel@FreeBSD.org>  * Copyright (c) 2015-2016 Vladimir Kondratyev<wulf@cicgroup.ru>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
+comment|/*-  * Copyright (c) 2014 Jakub Wojciech Klama<jceel@FreeBSD.org>  * Copyright (c) 2015-2016 Vladimir Kondratyev<wulf@FreeBSD.org>  * All rights reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in the  *    documentation and/or other materials provided with the distribution.  *  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  *  * $FreeBSD$  */
 end_comment
 
 begin_ifndef
@@ -24,7 +24,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/queue.h>
+file|<sys/kbio.h>
 end_include
 
 begin_include
@@ -36,7 +36,7 @@ end_include
 begin_include
 include|#
 directive|include
-file|<sys/kbio.h>
+file|<sys/queue.h>
 end_include
 
 begin_include
@@ -444,6 +444,26 @@ parameter_list|(
 name|evdev
 parameter_list|)
 value|mtx_assert((evdev)->ev_lock, MA_OWNED)
+end_define
+
+begin_define
+define|#
+directive|define
+name|EVDEV_ENTER
+parameter_list|(
+name|evdev
+parameter_list|)
+value|do {					\ 	if ((evdev)->ev_lock_type == EV_LOCK_INTERNAL)			\ 		EVDEV_LOCK(evdev);					\ 	else								\ 		EVDEV_LOCK_ASSERT(evdev);				\ } while (0)
+end_define
+
+begin_define
+define|#
+directive|define
+name|EVDEV_EXIT
+parameter_list|(
+name|evdev
+parameter_list|)
+value|do {					\ 	if ((evdev)->ev_lock_type == EV_LOCK_INTERNAL)			\ 		EVDEV_UNLOCK(evdev);					\ } while (0)
 end_define
 
 begin_struct
