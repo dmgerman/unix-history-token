bdcda95e81eb432b3999a835028eb1cd7a80cc4c
Hans Petter Selasky <hselasky@FreeBSD.org>
2017-11-17 15:43:29 +0000

    MFC r325614: Multiple fixes for using IPv6 link-local addresses with RDMA.
    1) Fail to resolve RDMA address if rtalloc1() returns the loopback
    device, lo0, as the gateway interface.
    
    2) Use ip_dev_find() and ip6_dev_find() to lookup network interfaces
    with matching IPv4 and IPv6 addresses, respectivly.
    
    3) In addr_resolve() make sure the "ifa" pointer is always set, also when
    the "ifp" is NULL. Else a NULL pointer access might happen trying to
    read from the "ifa" pointer later on.
    
    Sponsored by:	Mellanox Technologies
    
    
    Former-commit-id: 43961d0c21c848c2639c5d1ea2b590b73d62adc2

--- sys/ofed/drivers/infiniband/core/device.c
+++ sys/ofed/drivers/infiniband/core/device.c
@@ -2834,5 +2834,21 @@ argument_list|)
 expr_stmt|;
 end_expr_stmt
 
+begin_expr_stmt
+name|MODULE_DEPEND
+argument_list|(
+name|ibcore
+argument_list|,
+name|toecore
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
 end_unit
 
